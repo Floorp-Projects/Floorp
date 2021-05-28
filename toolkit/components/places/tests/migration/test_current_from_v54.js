818db1b45b6e7d6ea29e4259ecfabb1d4b9c940a
Marco Bonardo <mbonardo@mozilla.com>
2021-05-28 14:08:18 +0000

    Bug 1713073 - Add new places metadata tables. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D116084

---
+++ toolkit/components/places/tests/migration/test_current_from_v54.js
@@ -0,0 +1,25 @@
+add_task(async function setup() {
+  // Since this migration doesn't affect places.sqlite, we can reuse v43.
+  await setupPlacesDatabase("places_v54.sqlite");
+  await setupPlacesDatabase("favicons_v41.sqlite", "favicons.sqlite");
+});
+
+add_task(async function database_is_valid() {
+  // Accessing the database for the first time triggers migration.
+  Assert.equal(
+    PlacesUtils.history.databaseStatus,
+    PlacesUtils.history.DATABASE_STATUS_UPGRADED
+  );
+
+  let db = await PlacesUtils.promiseDBConnection();
+  Assert.equal(await db.getSchemaVersion(), CURRENT_SCHEMA_VERSION);
+
+  let count = (
+    await db.execute(`SELECT count(*) FROM moz_places_metadata`)
+  )[0].getResultByIndex(0);
+  Assert.equal(count, 0, "Empty table");
+  count = (
+    await db.execute(`SELECT count(*) FROM moz_places_metadata_search_queries`)
+  )[0].getResultByIndex(0);
+  Assert.equal(count, 0, "Empty table");
+});
