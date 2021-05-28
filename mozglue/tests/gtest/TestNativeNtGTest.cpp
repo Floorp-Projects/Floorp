9672ed905783681fc08cbca54136af0de5183273
Toshihito Kikuchi <tkikuchi@mozilla.com>
2021-05-28 15:32:29 +0000

    Bug 1695817 - Part 1: Make nt::GetLeafName accept nsAString r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D109302

--- mozglue/tests/gtest/TestNativeNtGTest.cpp
+++ mozglue/tests/gtest/TestNativeNtGTest.cpp
@@ -25,3 +25,39 @@ TEST(TestNativeNtGTest, GenerateDependentModuleSet)
   EXPECT_TRUE(dependentModules.Contains(u"MOZGLUE.dll"_ns));
   EXPECT_FALSE(dependentModules.Contains(u"xxx.dll"_ns));
 }
+
+TEST(TestNativeNtGTest, GetLeafName)
+{
+  nsAutoString str;
+  str = mozilla::nt::GetLeafName(u""_ns);
+  EXPECT_STREQ(str.get(), L"");
+  str = mozilla::nt::GetLeafName(u"\\"_ns);
+  EXPECT_STREQ(str.get(), L"");
+  str = mozilla::nt::GetLeafName(u"\\\\"_ns);
+  EXPECT_STREQ(str.get(), L"");
+  str = mozilla::nt::GetLeafName(u"abc\\def\\ghi"_ns);
+  EXPECT_STREQ(str.get(), L"ghi");
+  str = mozilla::nt::GetLeafName(u"abcdef"_ns);
+  EXPECT_STREQ(str.get(), L"abcdef");
+  str = mozilla::nt::GetLeafName(u"\\abcdef"_ns);
+  EXPECT_STREQ(str.get(), L"abcdef");
+
+  const auto kEntireText =
+      u"\\"_ns
+      u"\\\\abc"_ns
+      u"\\x\\y\\z"_ns
+      u"123\\456\\"_ns
+      u"789"_ns;
+  str = mozilla::nt::GetLeafName(Substring(kEntireText, 0, 0));
+  EXPECT_STREQ(str.get(), L"");
+  str = mozilla::nt::GetLeafName(Substring(kEntireText, 0, 1));
+  EXPECT_STREQ(str.get(), L"");
+  str = mozilla::nt::GetLeafName(Substring(kEntireText, 1, 5));
+  EXPECT_STREQ(str.get(), L"abc");
+  str = mozilla::nt::GetLeafName(Substring(kEntireText, 6, 6));
+  EXPECT_STREQ(str.get(), L"z");
+  str = mozilla::nt::GetLeafName(Substring(kEntireText, 12, 8));
+  EXPECT_STREQ(str.get(), L"");
+  str = mozilla::nt::GetLeafName(Substring(kEntireText, 20));
+  EXPECT_STREQ(str.get(), L"789");
+}
