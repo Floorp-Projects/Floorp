f00e87bcb6f2b1279aa1bd723f1e4e2dc13f8862
Butkovits Atila <abutkovits@mozilla.com>
2021-05-30 14:27:20 +0300

    Backed out changeset 229e905d571e (bug 1708735) for multiple regressions. CLOSED TREE

--- toolkit/components/extensions/test/browser/browser_ext_themes_arrowpanels.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_arrowpanels.js
@@ -56,7 +56,9 @@ add_task(async function test_popup_styling(browser, accDoc) {
       // Open the information arrow panel
       await openIdentityPopup();
 
-      let arrowContent = gIdentityHandler._identityPopup.panelContent;
+      let arrowContent = gIdentityHandler._identityPopup.shadowRoot.querySelector(
+        ".panel-arrowcontent"
+      );
       let arrowContentComputedStyle = window.getComputedStyle(arrowContent);
       // Ensure popup background color was set properly
       Assert.equal(
