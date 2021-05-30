f80600c6d2929f7c45b94be6473f2348b2542aa3
Emilio Cobos Alvarez <emilio@crisal.io>
2021-05-30 12:31:49 +0000

    Bug 1708735 - Make the arrowpanel code more generic. r=dao,zombie
    Share the concept of a panel content with all other menupopups / panels.
    
    This avoids importing global.css in the shadow tree, and renames the
    arrowcontent part to just "content", since we want to introduce a
    "content" part for other panels.
    
    This shouldn't change behavior but makes bug 1708136 a matter of
    tweaking a couple CSS rules and fixing up test failures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113990

--- toolkit/content/widgets/menupopup.js
+++ toolkit/content/widgets/menupopup.js
@@ -107,7 +107,7 @@
         <html:link rel="stylesheet" href="chrome://global/skin/global.css"/>
         <html:style>${this.styles}</html:style>
         <arrowscrollbox class="menupopup-arrowscrollbox"
-                        part="arrowscrollbox"
+                        part="arrowscrollbox content"
                         exportparts="scrollbox: arrowscrollbox-scrollbox"
                         flex="1"
                         orient="vertical"
@@ -136,7 +136,7 @@
         case "macosx":
           s += `
             :host(.in-menulist) arrowscrollbox {
-              padding: 0;
+              --panel-padding: 0;
             }
           `;
           break;
