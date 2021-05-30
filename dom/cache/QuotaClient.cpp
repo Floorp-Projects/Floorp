577fd6f3268a6158886a9341999b6d7ff55bb9ad
Jan Varga <jvarga@mozilla.com>
2021-05-30 11:08:02 +0000

    Bug 1708643 - CACHE: Replace QM_OR_ELSE_(WARN|LOG) with QM_OR_ELSE_(WARN|LOG)_IF in places where the fallback needs to be called conditionally; r=dom-storage-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D115150

--- dom/cache/QuotaClient.cpp
+++ dom/cache/QuotaClient.cpp
@@ -103,22 +103,17 @@ Result<UsageInfo, nsresult> GetBodyUsage(nsIFile& aMorgueDir,
           return false;
         };
 
-        // QM_OR_ELSE_WARN is not used here since we just want to log
+        // QM_OR_ELSE_WARN_IF is not used here since we just want to log
         // NS_ERROR_FILE_FS_CORRUPTED result and not spam the reports (even a
         // warning in the reports is not desired).
-        QM_TRY(QM_OR_ELSE_LOG(
+        QM_TRY(QM_OR_ELSE_LOG_IF(
             ToResult(BodyTraverseFiles(QuotaInfo{}, *bodyDir, getUsage,
                                        /* aCanRemoveFiles */ true,
                                        /* aTrackQuota */ false)),
-            ([](const nsresult rv) -> Result<Ok, nsresult> {
-              // We treat NS_ERROR_FILE_FS_CORRUPTED as if the
-              // directory did not exist at all.
-              if (rv == NS_ERROR_FILE_FS_CORRUPTED) {
-                return Ok{};
-              }
-
-              return Err(rv);
-            })));
+            IsSpecificError<NS_ERROR_FILE_FS_CORRUPTED>,
+            // We treat NS_ERROR_FILE_FS_CORRUPTED as if the
+            // directory did not exist at all.
+            ErrToDefaultOk<>));
         return usageInfo;
       }));
 }
