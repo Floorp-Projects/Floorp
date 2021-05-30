c946015d102c32ca73326755f57f7d5b692ccfdc
Emilio Cobos Alvarez <emilio@crisal.io>
2021-05-30 12:31:49 +0000

    Bug 1708735 - Make the arrowpanel code more generic. r=dao,zombie
    Share the concept of a panel content with all other menupopups / panels.
    
    This avoids importing global.css in the shadow tree, and renames the
    arrowcontent part to just "content", since we want to introduce a
    "content" part for other panels.
    
    This shouldn't change behavior but makes bug 1708136 a matter of
    tweaking a couple CSS rules and fixing up test failures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113990

--- toolkit/content/widgets/panel.js
+++ toolkit/content/widgets/panel.js
@@ -10,12 +10,11 @@
   class MozPanel extends MozElements.MozElementMixin(XULPopupElement) {
     static get markup() {
       return `
-      <html:link rel="stylesheet" href="chrome://global/skin/global.css"/>
       <vbox class="panel-arrowcontainer" flex="1">
         <box class="panel-arrowbox" part="arrowbox">
           <image class="panel-arrow" part="arrow"/>
         </box>
-        <box class="panel-arrowcontent" flex="1" part="arrowcontent"><html:slot/></box>
+        <html:slot part="content" />
       </vbox>
       `;
     }
@@ -59,7 +58,7 @@
 
     initialize() {
       // As an optimization, we don't slot contents if the panel is [hidden] in
-      // connecetedCallack this means we can avoid running this code at startup
+      // connectedCallack this means we can avoid running this code at startup
       // and only need to do it when a panel is about to be shown.
       // We then override the `hidden` setter and `removeAttribute` and call this
       // function if the node is about to be shown.
@@ -68,15 +67,22 @@
       }
 
       if (!this.isArrowPanel) {
-        this.shadowRoot.appendChild(document.createElement("slot"));
+        let slot = document.createElement("slot");
+        slot.part = "content";
+        this.shadowRoot.appendChild(slot);
       } else {
         this.shadowRoot.appendChild(this.constructor.fragment);
       }
     }
 
+    get panelContent() {
+      return this.shadowRoot.querySelector("[part=content]");
+    }
+
     get hidden() {
       return super.hidden;
     }
+
     set hidden(v) {
       if (!v) {
         this.initialize();
