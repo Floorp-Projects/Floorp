eebcb5d4a96a8222be1b225544490d0373da6838
Ryan Kelly <ryan@rfk.id.au>
2021-05-31 15:53:04 +1000

    [components] Update appservices to v77.0.2.
    This includes a fix for a failing runtime assertion which we believe
    to have been the underlying cause of a recent spike in Fenix crashes.

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -30,7 +30,7 @@ object Versions {
     const val disklrucache = "2.0.2"
     const val leakcanary = "2.4"
 
-    const val mozilla_appservices = "77.0.1"
+    const val mozilla_appservices = "77.0.2"
 
     const val mozilla_glean = "38.0.0"
 
