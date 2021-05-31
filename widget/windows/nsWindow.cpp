cdcf59a33939b2f95daaa5000f8efa643389b0ac
Kagami Sascha Rosylight <krosylight@mozilla.com>
2021-05-31 13:39:18 +0000

    Bug 1710317 - Part 1: Add MouseButton::eEraser r=aklotz,edgar
    Differential Revision: https://phabricator.services.mozilla.com/D115508

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -91,6 +91,7 @@
 #include "prenv.h"
 
 #include "mozilla/WidgetTraceEvent.h"
+#include "nsContentUtils.h"
 #include "nsISupportsPrimitives.h"
 #include "nsITheme.h"
 #include "nsIObserverService.h"
@@ -8605,6 +8606,17 @@ bool nsWindow::DispatchTouchEventFromWMPointer(
   return true;
 }
 
+static MouseButton PenFlagsToMouseButton(PEN_FLAGS aPenFlags) {
+  // Theoretically flags can be set together but they do not
+  if (aPenFlags & PEN_FLAG_BARREL) {
+    return MouseButton::eSecondary;
+  }
+  if (aPenFlags & PEN_FLAG_ERASER) {
+    return MouseButton::eEraser;
+  }
+  return MouseButton::ePrimary;
+}
+
 bool nsWindow::OnPointerEvents(UINT msg, WPARAM aWParam, LPARAM aLParam) {
   if (!mPointerEvents.ShouldHandleWinPointerMessages(msg, aWParam)) {
     return false;
@@ -8618,6 +8630,13 @@ bool nsWindow::OnPointerEvents(UINT msg, WPARAM aWParam, LPARAM aLParam) {
     // Don't consume the Windows WM_POINTER* messages
     return false;
   }
+
+  uint32_t pointerId = mPointerEvents.GetPointerId(aWParam);
+  POINTER_PEN_INFO penInfo{};
+  if (!mPointerEvents.GetPointerPenInfo(pointerId, &penInfo)) {
+    return false;
+  }
+
   // When dispatching mouse events with pen, there may be some
   // WM_POINTERUPDATE messages between WM_POINTERDOWN and WM_POINTERUP with
   // small movements. Those events will reset sLastMousePoint and reset
@@ -8641,8 +8660,7 @@ bool nsWindow::OnPointerEvents(UINT msg, WPARAM aWParam, LPARAM aLParam) {
       sLastPointerDownPoint.x = eventPoint.x;
       sLastPointerDownPoint.y = eventPoint.y;
       message = eMouseDown;
-      button = IS_POINTER_SECONDBUTTON_WPARAM(aWParam) ? MouseButton::eSecondary
-                                                       : MouseButton::ePrimary;
+      button = PenFlagsToMouseButton(penInfo.penFlags);
       sLastPenDownButton = button;
       sPointerDown = true;
     } break;
@@ -8681,23 +8699,19 @@ bool nsWindow::OnPointerEvents(UINT msg, WPARAM aWParam, LPARAM aLParam) {
     default:
       return false;
   }
-  uint32_t pointerId = mPointerEvents.GetPointerId(aWParam);
-  POINTER_PEN_INFO penInfo{};
-  mPointerEvents.GetPointerPenInfo(pointerId, &penInfo);
 
   // Windows defines the pen pressure is normalized to a range between 0 and
   // 1024. Convert it to float.
   float pressure = penInfo.pressure ? (float)penInfo.pressure / 1024 : 0;
-  int16_t buttons = sPointerDown ? button == MouseButton::ePrimary
-                                       ? MouseButtonsFlag::ePrimaryFlag
-                                       : MouseButtonsFlag::eSecondaryFlag
-                                 : MouseButtonsFlag::eNoButtons;
+  int16_t buttons = sPointerDown
+                        ? nsContentUtils::GetButtonsFlagForButton(button)
+                        : MouseButtonsFlag::eNoButtons;
   WinPointerInfo pointerInfo(pointerId, penInfo.tiltX, penInfo.tiltY, pressure,
                              buttons);
   pointerInfo.twist = penInfo.rotation;
 
   if (StaticPrefs::dom_w3c_pointer_events_scroll_by_pen_enabled() &&
-      DispatchTouchEventFromWMPointer(msg, aLParam, pointerInfo)) {
+      DispatchTouchEventFromWMPointer(msg, aLParam, pointerInfo, button)) {
     return true;
   }
 
