803140bbdbcb25c276774a0b5a501c12e7ffbf5e
Alexandre Poirot <poirot.alex@gmail.com>
2021-06-01 11:40:31 +0000

    Bug 1710582 - [devtools] Make refreshTab test helper wait for target switching. r=nchevobbe
    navigateTo contains all the logic to wait for panel updates on reload,
    as well as wait for the process of the new target.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114828

--- devtools/client/memory/test/browser/browser_memory_refresh_does_not_leak.js
+++ devtools/client/memory/test/browser/browser_memory_refresh_does_not_leak.js
@@ -54,7 +54,7 @@ const DESCRIPTION = {
 };
 
 this.test = makeMemoryTest(TEST_URL, async function({ tab, panel }) {
-  const front = panel.panelWin.gStore.getState().front;
+  let front = panel.panelWin.gStore.getState().front;
 
   const startWindows = await getWindowsInSnapshot(front);
   dumpn(
@@ -64,6 +64,9 @@ this.test = makeMemoryTest(TEST_URL, async function({ tab, panel }) {
   is(startWindows.length, 1);
 
   await refreshTab();
+
+  // Update the front as we may have switched to a new target and a new memory front
+  front = panel.panelWin.gStore.getState().front;
 
   const endWindows = await getWindowsInSnapshot(front);
   is(endWindows.length, 1);
