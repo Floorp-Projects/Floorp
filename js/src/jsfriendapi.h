61f95cd63f06440ab758b7315a89662e9fe9d155
Iain Ireland <iireland@mozilla.com>
2021-06-01 01:31:19 +0000

    Bug 1624792: Remove js::GlobalHasInstrumentation r=tcampbell,smaug
    This is no longer needed because we are removing the realm instrumentation code (which was only ever used for WebRR).
    
    Differential Revision: https://phabricator.services.mozilla.com/D116304

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -754,12 +754,6 @@ class MOZ_STACK_CLASS JS_FRIEND_API AutoAssertNoContentJS {
  */
 extern JS_FRIEND_API uint64_t GetGCHeapUsageForObjectZone(JSObject* obj);
 
-/**
- * Return whether a global object's realm has had instrumentation enabled by a
- * Debugger.
- */
-extern JS_FRIEND_API bool GlobalHasInstrumentation(JSObject* global);
-
 class JS_FRIEND_API CompartmentTransplantCallback {
  public:
   virtual JSObject* getObjectToTransplant(JS::Compartment* compartment) = 0;
