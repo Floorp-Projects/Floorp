344147f0fa3c27f814ab0128c122cb8f172e5ced
Iain Ireland <iireland@mozilla.com>
2021-06-01 01:31:20 +0000

    Bug 1624792: Remove instrumentation tests r=tcampbell
    This patch stack is based on the patches for bug 1554524 in more-or-less reverse order, with a few modifications so that the intermediate patches build. This is part 8.
    
    Depends on D116304
    
    Differential Revision: https://phabricator.services.mozilla.com/D116305

--- js/src/jit-test/tests/basic/evaluate-global-mismatch.js
+++ js/src/jit-test/tests/basic/evaluate-global-mismatch.js
@@ -13,22 +13,3 @@ assertErrorMessage(() => {
     global: g_discardSource,
   });
 }, g_discardSource.Error, "discardSource option mismatch");
-
-
-let g_instrumentation = newGlobal({newCompartment: true});
-let dbg_instrumentation = new Debugger();
-let gdbg_instrumentation = dbg_instrumentation.addDebuggee(g_instrumentation);
-
-gdbg_instrumentation.setInstrumentation(
-  gdbg_instrumentation.makeDebuggeeValue((kind, script, offset) => {
-  }),
-  ["breakpoint"]
-);
-
-assertErrorMessage(() => {
-  // Script is compiled with instrumentationKinds=breakpoint, and is executed
-  // in global with instrumentationKinds=0.
-  g_instrumentation.evaluate("function f() { (function(){})(); }; f();", {
-    global: g,
-  });
-}, g.Error, "instrumentationKinds mismatch");
