ec54d41dcf3c62e1078ed0937dad33cdf5e82c01
Sam Foster <sfoster@mozilla.com>
2021-06-02 22:52:04 +0000

    Bug 1682634 - Add a mochitest-chrome test for the screenshot flow and result dimensions. r=emalysz
    * Some helpers and a basic test for full page and visible page screenshots, using the clipboard to examine the result
    * The visible page test's assertions are marked todo, as we don't actually get the expected image dimensions currently. This is possibly bug 1711101?
    
    Differential Revision: https://phabricator.services.mozilla.com/D99854

---
+++ browser/extensions/screenshots/test/browser/browser_screenshots_dimensions.js
@@ -0,0 +1,107 @@
+add_task(async function test_fullPageScreenshot() {
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: TEST_GREEN_PAGE,
+    },
+    async browser => {
+      let helper = new ScreenshotsHelper(browser);
+      let contentInfo = await helper.getContentDimensions();
+      ok(contentInfo, "Got dimensions back from the content");
+
+      await helper.triggerUIFromToolbar();
+
+      await helper.clickUIElement(
+        helper.selector.preselectIframe,
+        helper.selector.fullPageButton
+      );
+
+      info("Waiting for the preview UI and the copy button");
+      await helper.waitForUIContent(
+        helper.selector.previewIframe,
+        helper.selector.copyButton
+      );
+
+      let deactivatedPromise = helper.waitForToolbarButtonDeactivation();
+      let clipboardChanged = waitForRawClipboardChange();
+
+      await helper.clickUIElement(
+        helper.selector.previewIframe,
+        helper.selector.copyButton
+      );
+
+      info("Waiting for clipboard change");
+      await clipboardChanged;
+
+      await deactivatedPromise;
+      info("Screenshots is deactivated");
+
+      let result = await getImageSizeFromClipboard(browser);
+      is(
+        result.width,
+        contentInfo.documentWidth,
+        "Got expected screenshot width"
+      );
+      is(
+        result.height,
+        contentInfo.documentHeight,
+        "Got expected screenshot height"
+      );
+    }
+  );
+});
+
+add_task(async function test_visiblePageScreenshot() {
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: TEST_GREEN_PAGE,
+    },
+    async browser => {
+      let helper = new ScreenshotsHelper(browser);
+      let contentInfo = await helper.getContentDimensions();
+      ok(contentInfo, "Got dimensions back from the content");
+
+      await helper.triggerUIFromToolbar();
+
+      await helper.clickUIElement(
+        helper.selector.preselectIframe,
+        helper.selector.visiblePageButton
+      );
+
+      info("Waiting for the preview UI and the copy button");
+      await helper.waitForUIContent(
+        helper.selector.previewIframe,
+        helper.selector.copyButton
+      );
+
+      let deactivatedPromise = helper.waitForToolbarButtonDeactivation();
+      let clipboardChanged = waitForRawClipboardChange();
+
+      await helper.clickUIElement(
+        helper.selector.previewIframe,
+        helper.selector.copyButton
+      );
+
+      info("Waiting for clipboard change");
+      await clipboardChanged;
+
+      await deactivatedPromise;
+      info("Screenshots is deactivated");
+
+      let result = await getImageSizeFromClipboard(browser);
+      info("result: " + JSON.stringify(result, null, 2));
+      info("contentInfo: " + JSON.stringify(contentInfo, null, 2));
+      todo_is(
+        result.width,
+        contentInfo.documentWidth,
+        "Got expected screenshot width"
+      );
+      todo_is(
+        result.height,
+        contentInfo.clientHeight,
+        "Got expected screenshot height"
+      );
+    }
+  );
+});
