d2c3c7ef3e87e7ccb9d12b112ad0e1e16a082760
Andi-Bogdan Postelnicu <andi@mozilla.com>
2021-06-02 14:38:01 +0000

    Bug 1707096 - fix the build for `clang-trunk`. r=jfkthame, a=jcristau
    With D100581 LLVM added support for -Wunused-but-set-parameter and -Wunused-but-set-variable.
    1. we shouldn't treat these warning as errors
    2. for harfbuzz we apply a patch that has also been upstreamed, #2995, since pragma directives are somehow borken in clang vs gcc.
    
    Differential Revision: https://phabricator.services.mozilla.com/D116604

--- gfx/harfbuzz/src/hb-subset-cff1.cc
+++ gfx/harfbuzz/src/hb-subset-cff1.cc
@@ -402,7 +402,7 @@ struct cff_subset_plan {
   void plan_subset_encoding (const OT::cff1::accelerator_subset_t &acc, hb_subset_plan_t *plan)
   {
     const Encoding *encoding = acc.encoding;
-    unsigned int  size0, size1, supp_size;
+    unsigned int  size0, size1;
     hb_codepoint_t  code, last_code = CFF_UNDEF_CODE;
     hb_vector_t<hb_codepoint_t> supp_codes;
 
@@ -412,7 +412,6 @@ struct cff_subset_plan {
       return;
     }
 
-    supp_size = 0;
     supp_codes.init ();
 
     subset_enc_num_codes = plan->num_output_glyphs () - 1;
@@ -448,7 +447,6 @@ struct cff_subset_plan {
 	  code_pair_t pair = { supp_codes[i], sid };
 	  subset_enc_supp_codes.push (pair);
 	}
-	supp_size += SuppEncoding::static_size * supp_codes.length;
       }
     }
     supp_codes.fini ();
