a096495c5445cc7c8ed29bfc931093fee33b4ddc
Neil Deakin <neil@mozilla.com>
2021-06-02 13:45:10 +0000

    Bug 1600059, remove some usages of waitForCondition in formautofill tests and replace with waitForMutationCondition and waiting for popup event notifications in the child process to see if this fixes the intermittent failures, r=mak
    This is the same issue as bug 1701078.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112979

--- browser/extensions/formautofill/test/browser/creditCard/browser_creditCard_fill_cancel_login.js
+++ browser/extensions/formautofill/test/browser/creditCard/browser_creditCard_fill_cancel_login.js
@@ -17,8 +17,12 @@ add_task(async function test_fill_creditCard_but_cancel_login() {
     async function(browser) {
       await openPopupOn(browser, "#cc-name");
       const ccItem = getDisplayedPopupItems(browser)[0];
+      let popupClosePromise = BrowserTestUtils.waitForPopupEvent(
+        browser.autoCompletePopup,
+        "hidden"
+      );
       await EventUtils.synthesizeMouseAtCenter(ccItem, {});
-      await Promise.all([osKeyStoreLoginShown, expectPopupClose(browser)]);
+      await Promise.all([osKeyStoreLoginShown, popupClosePromise]);
 
       await SpecialPowers.spawn(browser, [], async function() {
         is(content.document.querySelector("#cc-name").value, "", "Check name");
