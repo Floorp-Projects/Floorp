680ad752232a6f3b2e29ff46ad8f0fa3c4b54f95
Emilio Cobos Álvarez <emilio@crisal.io>
2021-06-03 08:15:24 +0000

    Bug 1481400 - Centralize keyboard activation code. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D116584

--- dom/html/nsGenericHTMLElement.cpp
+++ dom/html/nsGenericHTMLElement.cpp
@@ -16,6 +16,7 @@
 #include "mozilla/MouseEvents.h"
 #include "mozilla/PresShell.h"
 #include "mozilla/TextEditor.h"
+#include "mozilla/TextEvents.h"
 #include "mozilla/StaticPrefs_html5.h"
 #include "mozilla/StaticPrefs_layout.h"
 #include "mozilla/StaticPrefs_accessibility.h"
@@ -2433,6 +2434,44 @@ bool nsGenericHTMLElement::PerformAccesskey(bool aKeyCausesActivation,
   }
 
   return focused;
+}
+
+void nsGenericHTMLElement::HandleKeyboardActivation(
+    EventChainPostVisitor& aVisitor) {
+  const auto message = aVisitor.mEvent->mMessage;
+  if (message != eKeyUp && message != eKeyPress) {
+    return;
+  }
+
+  if (nsEventStatus_eIgnore != aVisitor.mEventStatus) {
+    return;
+  }
+
+  const WidgetKeyboardEvent* keyEvent = aVisitor.mEvent->AsKeyboardEvent();
+  bool shouldActivate = false;
+  switch (message) {
+    case eKeyPress:
+      shouldActivate = keyEvent->mKeyCode == NS_VK_RETURN;
+      if (keyEvent->mKeyCode == NS_VK_SPACE) {
+        // Consume 'space' key to prevent scrolling the page down.
+        aVisitor.mEventStatus = nsEventStatus_eConsumeNoDefault;
+      }
+      break;
+    case eKeyUp:
+      shouldActivate = keyEvent->mKeyCode == NS_VK_SPACE;
+      break;
+    default:
+      MOZ_ASSERT_UNREACHABLE("why didn't we bail out earlier?");
+      break;
+  }
+
+  if (!shouldActivate) {
+    return;
+  }
+
+  DispatchSimulatedClick(this, aVisitor.mEvent->IsTrusted(),
+                         aVisitor.mPresContext);
+  aVisitor.mEventStatus = nsEventStatus_eConsumeNoDefault;
 }
 
 nsresult nsGenericHTMLElement::DispatchSimulatedClick(
