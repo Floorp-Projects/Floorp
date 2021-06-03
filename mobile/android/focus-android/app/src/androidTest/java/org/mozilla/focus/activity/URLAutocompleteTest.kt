f40da180f6dd932bab59dab5f677c60eb2b44523
Oana Horvath <oana.horvath@softvision.ro>
2021-06-03 12:41:38 +0300

    [focus] For https://github.com/mozilla-mobile/focus-android/issues/4859: Re-enable disableAutocompleteForCustomSiteTest UI test

--- mobile/android/focus-android/app/src/androidTest/java/org/mozilla/focus/activity/URLAutocompleteTest.kt
+++ mobile/android/focus-android/app/src/androidTest/java/org/mozilla/focus/activity/URLAutocompleteTest.kt
@@ -4,7 +4,6 @@
 package org.mozilla.focus.activity
 
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -97,7 +96,6 @@ class URLAutocompleteTest {
         }
     }
 
-    @Ignore("Failing, see: https://github.com/mozilla-mobile/focus-android/issues/4859")
     @Test
     // Add custom autocompletion site, then disable autocomplete
     fun disableAutocompleteForCustomSiteTest() {
