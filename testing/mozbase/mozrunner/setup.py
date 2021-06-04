47d37f4cd4b01d8467aa5f6b457292a7c11497d0
James Graham <james@hoppipolla.co.uk>
2021-06-04 19:34:36 +0000

    Bug 1714529 - Update mozrunner version for release, r=ahal
    This bumps the mozprocess dependency version
    
    Differential Revision: https://phabricator.services.mozilla.com/D116824

--- testing/mozbase/mozrunner/setup.py
+++ testing/mozbase/mozrunner/setup.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = "mozrunner"
-PACKAGE_VERSION = "8.2.0"
+PACKAGE_VERSION = "8.2.1"
 
 desc = """Reliable start/stop/configuration of Mozilla Applications (Firefox, Thunderbird, etc.)"""
 
@@ -16,7 +16,7 @@ deps = [
     "mozfile>=1.2",
     "mozinfo>=0.7,<2",
     "mozlog>=6.0",
-    "mozprocess>=1.2.0,<2",
+    "mozprocess>=1.3.0,<2",
     "mozprofile~=2.3",
     "six>=1.13.0,<2",
 ]
