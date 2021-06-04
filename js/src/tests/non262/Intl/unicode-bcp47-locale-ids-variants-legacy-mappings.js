5b115c50bcc11545c45bda96e499a6593ecebf74
André Bargull <andre.bargull@gmail.com>
2021-06-04 17:42:26 +0000

    Bug 1686052 - Part 10: Use new language tag canonicalisation algorithm. r=tcampbell
    UTS #35 (version 38) overhauled the language tag canonicalisation algorithm
    and CLDR 38 also added new alias entries to replace some deprecated language
    subtags with a new, preferred form.
    
    Overview of canonicalisation changes:
    - Script aliases must now be processed. (Script alias data was already present
      in CLDR, but the previous canonicalisation algorithm never processed it.)
    - Sign language canonicalisation was added.
    - Grandfathered tags are now handled like any other tag. It is no longer
      required to perform exact matches, but instead individual subtags are
      compared. For example both "art-lojban" and "art-ZZ-lojban" are now
      canonicalised to "jbo" resp. "jbo-ZZ".
    
    
    Changes in make_intl_data.py:
    - Split `writeMappingsBinarySearchBody` from `writeMappingsBinarySearch` so it
      can be used in the new `writeSignLanguageMappingsFunction` function.
    - `writeMappingsBinarySearchBody` splits the `name` parameter in two distinct
      parameters `source_name` and `target_name`. This is also needed for the new
      `writeSignLanguageMappingsFunction` function.
    - Change `writeVariantTagMappings` to allow no replacements.
      - For example the alias entry `<languageAlias type="und_bokmal" replacement="und">`
        removes the "bokmal" variant subtags, but doesn't add any replacement tags.
    - Replace "grandfathered" with "legacy", because UTS and CLDR no longer use
      that term.
    - `readSupplementalData` was changed to collect all alias rules into a single
       dict. This matches how the new UTS #35 canonicalisation algorithm is
       specified. Later they are split into individual dictionaries for each subtag.
    
    Now that we no longer need to support old Python versions, we can use newer
    features like format strings. I've used them exclusively in new code and also
    replaced other `str.format()` calls in functions which were modified in this
    patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101398

--- js/src/tests/non262/Intl/unicode-bcp47-locale-ids-variants-legacy-mappings.js
+++ js/src/tests/non262/Intl/unicode-bcp47-locale-ids-variants-legacy-mappings.js
@@ -5,11 +5,43 @@
 // <https://www.unicode.org/reports/tr35/#Legacy_Variants>
 
 assertEq(Intl.getCanonicalLocales("sv-AALAND")[0], "sv-AX");
-assertEq(Intl.getCanonicalLocales("no-BOKMAL")[0], "nb-bokmal");
-assertEq(Intl.getCanonicalLocales("no-NYNORSK")[0], "nb-nynorsk");
+assertEq(Intl.getCanonicalLocales("no-BOKMAL")[0], "nb");
+assertEq(Intl.getCanonicalLocales("no-NYNORSK")[0], "nn");
 assertEq(Intl.getCanonicalLocales("en-POSIX")[0], "en-posix");
 assertEq(Intl.getCanonicalLocales("el-POLYTONI")[0], "el-polyton");
-assertEq(Intl.getCanonicalLocales("aa-SAAHO")[0], "aa-saaho");
+assertEq(Intl.getCanonicalLocales("aa-SAAHO")[0], "ssy");
+
+// Additional cases from CLDR, version 38.
+
+assertEq(Intl.getCanonicalLocales("aar-saaho")[0], "ssy");
+assertEq(Intl.getCanonicalLocales("arm-arevmda")[0], "hyw");
+assertEq(Intl.getCanonicalLocales("hy-arevmda")[0], "hyw");
+assertEq(Intl.getCanonicalLocales("hye-arevmda")[0], "hyw");
+
+for (let language of ["chi", "cmn", "zh", "zho"]) {
+    assertEq(Intl.getCanonicalLocales(language)[0], "zh");
+
+    let mapping = {
+        "guoyu-hakka": "hak",
+        "guoyu-xiang": "hsn",
+        "guoyu": "zh",
+        "hakka": "hak",
+        "xiang": "hsn",
+    };
+    for (let [variant, canonical] of Object.entries(mapping)) {
+        assertEq(Intl.getCanonicalLocales(`${language}-${variant}`)[0], canonical);
+    }
+}
+
+// Most legacy variant subtags are simply removed in contexts they don't apply.
+for (let variant of ["arevela", "arevmda", "bokmal", "hakka", "lojban", "nynorsk", "saaho", "xiang"]) {
+    assertEq(Intl.getCanonicalLocales(`en-${variant}`)[0], "en");
+}
+
+// Except these three, whose replacement is always applied.
+assertEq(Intl.getCanonicalLocales("en-aaland")[0], "en-AX");
+assertEq(Intl.getCanonicalLocales("en-heploc")[0], "en-alalc97");
+assertEq(Intl.getCanonicalLocales("en-polytoni")[0], "en-polyton");
 
 if (typeof reportCompare === "function")
     reportCompare(true, true);
