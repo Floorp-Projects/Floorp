50fcce3ed04f0d41a8f6ccc62f89126186d2436f
Noemi Erli <nerli@mozilla.com>
2021-06-07 19:50:30 +0300

    Backed out changeset 9e76c07577b1 (bug 1712047) for causing bustages in DrawTargetSkia.cpp CLOSED TREE

--- gfx/2d/DrawTargetSkia.cpp
+++ gfx/2d/DrawTargetSkia.cpp
@@ -273,7 +273,7 @@ static sk_sp<SkImage> GetSkImageForSurface(SourceSurface* aSurface,
     releaseProc = ReleaseTemporarySurface;
   }
 
-  DataSourceSurface* surf = dataSurface.take();
+  DataSourceSurface* surf = aSurface->GetDataSurface().take();
 
   // Skia doesn't support RGBX surfaces so ensure that the alpha value is opaque
   // white.
