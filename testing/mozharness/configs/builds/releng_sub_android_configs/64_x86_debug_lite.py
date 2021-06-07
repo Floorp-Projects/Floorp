3ce26cbc70b3485ed0d9d34409f4b7d3b59423c5
Agi Sferro <agi@sferro.dev>
2021-06-07 21:49:07 +0000

    Bug 1709640 - Add GeckoView Lite build variant. r=jmaher,aklotz,mhentges,glandium
    This patch adds a new GeckoView build variant dubbed "lite". We are in the
    process of adding some optional dependencies to GeckoView that are needed by
    Fenix (like Glean and Nimbus) which are likely not going to be used by anyone
    else at Mozilla. To avoid bloating third party consumers, we provide a
    geckoview-lite build that doesn't contain any optional dependency.
    
    The min SDK version of GeckoView (non-lite) is being increased to 21 to match
    Glean's min SDK. Given that Fenix's min SDK version is already 21 and that the
    lite version's min SDK is still at 16 this change is safe to do and will not
    incur in any loss of usability.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114370

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_debug_lite.py
@@ -0,0 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+config = {
+    "stage_platform": "android-x86-lite-debug",
+    "src_mozconfig": "mobile/android/config/mozconfigs/android-x86/debug-lite",
+    "debug_build": True,
+}
