9ec931b7a9547315e57e9583af8a4cf958a6a912
Eitan Isaacson <eitan@monotonous.org>
2021-06-08 22:21:05 +0000

    Bug 1709976 - Remove selection listeners from shutting down PresShell. r=Jamie, a=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D115839

--- accessible/base/SelectionManager.cpp
+++ accessible/base/SelectionManager.cpp
@@ -101,6 +101,24 @@ void SelectionManager::RemoveDocSelectionListener(PresShell* aPresShell) {
   // selection.
   Selection* spellSel = frameSel->GetSelection(SelectionType::eSpellCheck);
   spellSel->RemoveSelectionListener(this);
+
+  if (mCurrCtrlNormalSel) {
+    if (mCurrCtrlNormalSel->GetPresShell() == aPresShell) {
+      // Remove 'this' registered as selection listener for the normal selection
+      // if we are removing listeners for its PresShell.
+      mCurrCtrlNormalSel->RemoveSelectionListener(this);
+      mCurrCtrlNormalSel = nullptr;
+    }
+  }
+
+  if (mCurrCtrlSpellSel) {
+    if (mCurrCtrlSpellSel->GetPresShell() == aPresShell) {
+      // Remove 'this' registered as selection listener for the spellcheck
+      // selection if we are removing listeners for its PresShell.
+      mCurrCtrlSpellSel->RemoveSelectionListener(this);
+      mCurrCtrlSpellSel = nullptr;
+    }
+  }
 }
 
 void SelectionManager::ProcessTextSelChangeEvent(AccEvent* aEvent) {
