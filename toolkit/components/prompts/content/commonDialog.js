eb770bc00257952b1c9c286e3590db1f19552967
Michelle Goossens <michelle@masterwayz.nl>
2021-06-08 22:16:33 +0000

    Bug 1714349 - Remove browser.proton.modals.enabled pref r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D116903

--- toolkit/components/prompts/content/commonDialog.js
+++ toolkit/components/prompts/content/commonDialog.js
@@ -35,40 +35,38 @@ function commonDialogOnLoad() {
   }
   let title = { raw: args.title };
   let { promptPrincipal } = args;
-  if (PromptUtils.protonModals) {
-    if (promptPrincipal) {
-      if (promptPrincipal.isNullPrincipal) {
-        title = { l10nId: "common-dialog-title-null" };
-      } else if (promptPrincipal.isSystemPrincipal) {
-        title = { l10nId: "common-dialog-title-system" };
-        root.style.setProperty(
-          "--icon-url",
-          "url('chrome://branding/content/icon32.png')"
-        );
-      } else if (promptPrincipal.addonPolicy) {
-        title.raw = promptPrincipal.addonPolicy.name;
-      } else if (promptPrincipal.isContentPrincipal) {
-        try {
-          title.raw = promptPrincipal.URI.displayHostPort;
-        } catch (ex) {
-          // hostPort getter can throw, e.g. for about URIs.
-          title.raw = promptPrincipal.origin;
-        }
-        // hostPort can be empty for file URIs.
-        if (!title.raw) {
-          title.raw = promptPrincipal.prePath;
-        }
-      } else {
-        title = { l10nId: "common-dialog-title-unknown" };
+  if (promptPrincipal) {
+    if (promptPrincipal.isNullPrincipal) {
+      title = { l10nId: "common-dialog-title-null" };
+    } else if (promptPrincipal.isSystemPrincipal) {
+      title = { l10nId: "common-dialog-title-system" };
+      root.style.setProperty(
+        "--icon-url",
+        "url('chrome://branding/content/icon32.png')"
+      );
+    } else if (promptPrincipal.addonPolicy) {
+      title.raw = promptPrincipal.addonPolicy.name;
+    } else if (promptPrincipal.isContentPrincipal) {
+      try {
+        title.raw = promptPrincipal.URI.displayHostPort;
+      } catch (ex) {
+        // hostPort getter can throw, e.g. for about URIs.
+        title.raw = promptPrincipal.origin;
       }
-    } else if (args.authOrigin) {
-      title = { raw: args.authOrigin };
+      // hostPort can be empty for file URIs.
+      if (!title.raw) {
+        title.raw = promptPrincipal.prePath;
+      }
+    } else {
+      title = { l10nId: "common-dialog-title-unknown" };
     }
-    if (args.headerIconURL) {
-      root.style.setProperty("--icon-url", `url('${args.headerIconURL}')`);
-    }
-    dialog.setAttribute("buttonpack", "end");
+  } else if (args.authOrigin) {
+    title = { raw: args.authOrigin };
   }
+  if (args.headerIconURL) {
+    root.style.setProperty("--icon-url", `url('${args.headerIconURL}')`);
+  }
+  dialog.setAttribute("buttonpack", "end");
   // Fade and crop potentially long raw titles, e.g., origins and hostnames.
   title.shouldUseMaskFade = title.raw && (args.authOrigin || promptPrincipal);
   root.setAttribute("headertitle", JSON.stringify(title));
