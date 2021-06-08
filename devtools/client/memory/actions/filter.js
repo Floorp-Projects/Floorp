fd3528a1c8eac5f4d3d589ffb5d0e5d375c4ba52
Andrey Bienkowski <hexagonrecursion@gmail.com>
2021-06-08 18:09:00 +0000

    Bug 1365607 - Refactor devtools/client/memory/actions/filter.js r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D117174

--- devtools/client/memory/actions/filter.js
+++ devtools/client/memory/actions/filter.js
@@ -5,6 +5,7 @@
 
 const { actions } = require("devtools/client/memory/constants");
 const { refresh } = require("devtools/client/memory/actions/refresh");
+const { debounce } = require("devtools/shared/debounce");
 
 const setFilterString = (exports.setFilterString = function(filterString) {
   return {
@@ -17,21 +18,14 @@ const setFilterString = (exports.setFilterString = function(filterString) {
 // when the filter string is updated. This helps us avoid doing any work while
 // the user is still typing.
 const FILTER_INPUT_DEBOUNCE_MS = 250;
-
-// The timer id for the debounced census refresh.
-let timerId = null;
+const debouncedRefreshDispatcher = debounce(
+  (dispatch, heapWorker) => dispatch(refresh(heapWorker)),
+  FILTER_INPUT_DEBOUNCE_MS
+);
 
 exports.setFilterStringAndRefresh = function(filterString, heapWorker) {
   return ({ dispatch, getState }) => {
     dispatch(setFilterString(filterString));
-
-    if (timerId !== null) {
-      clearTimeout(timerId);
-    }
-
-    timerId = setTimeout(
-      () => dispatch(refresh(heapWorker)),
-      FILTER_INPUT_DEBOUNCE_MS
-    );
+    debouncedRefreshDispatcher(dispatch, heapWorker);
   };
 };
