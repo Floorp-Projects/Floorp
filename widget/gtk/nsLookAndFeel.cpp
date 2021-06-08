e1bff55faca90d8e043daa06f49df117941505f8
Emilio Cobos Álvarez <emilio@crisal.io>
2021-06-08 08:45:44 +0000

    Bug 1714103 - Simplify a condition in RecordLookAndFeelSpecificTelemetry. r=stransky
    Differential Revision: https://phabricator.services.mozilla.com/D116936

--- widget/gtk/nsLookAndFeel.cpp
+++ widget/gtk/nsLookAndFeel.cpp
@@ -1699,11 +1699,7 @@ void nsLookAndFeel::RecordLookAndFeelSpecificTelemetry() {
 
   // Whether the current Gtk theme uses something other than a solid color
   // background for scrollbar parts.
-  bool scrollbarUsesImage =
-      WidgetUsesImage(MOZ_GTK_SCROLLBAR_VERTICAL) ||
-      WidgetUsesImage(MOZ_GTK_SCROLLBAR_CONTENTS_VERTICAL) ||
-      WidgetUsesImage(MOZ_GTK_SCROLLBAR_TROUGH_VERTICAL) ||
-      WidgetUsesImage(MOZ_GTK_SCROLLBAR_THUMB_VERTICAL);
+  bool scrollbarUsesImage = !ShouldHonorThemeScrollbarColors();
   mozilla::Telemetry::ScalarSet(
       mozilla::Telemetry::ScalarID::WIDGET_GTK_THEME_SCROLLBAR_USES_IMAGES,
       scrollbarUsesImage);
