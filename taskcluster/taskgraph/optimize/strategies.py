92f88c284d858d6d8f80598a83a35e39cb373d95
Andrew Halberstadt <ahal@mozilla.com>
2021-06-08 20:03:36 +0000

    Bug 1690947 - [taskgraph] Return full status in util.taskcluster:status_task, r=taskgraph-reviewers,aki
    This moves the return value of the old task to a new 'state_task' function and
    updates consumers. We'll be using other parts of the status in later patches in
    the stack.
    
    Differential Revision: https://phabricator.services.mozilla.com/D115755

--- taskcluster/taskgraph/optimize/strategies.py
+++ taskcluster/taskgraph/optimize/strategies.py
@@ -36,8 +36,10 @@ class IndexSearch(OptimizationStrategy):
             try:
                 task_id = find_task_id(index_path)
                 status = status_task(task_id)
-                if status not in ("exception", "failed"):
-                    return task_id
+                if status.get("state") in ("exception", "failed"):
+                    continue
+
+                return task_id
             except KeyError:
                 # 404 will end up here and go on to the next index path
                 pass
