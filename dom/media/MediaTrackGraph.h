d7cdd6b1407326f4476784858f1dda1cdc421757
Chun-Min Chang <chun.m.chang@gmail.com>
2021-06-08 00:48:20 +0000

    Bug 1702646 - Share AudioSegments among AudioInputTrack r=padenot
    Each AudioInputTrack has its own AudioSegments storing the input audio
    data. When the AudioInputTrack is in pass-through mode, the AudioSegment
    is just the data de-interleaved from its raw data, without any audio
    processing magic applied on it. If there are multiple AudioInputTracks
    in pass-through mode exist in the same graph, then all of their
    AudioSegments are same.
    
    Before this patch, each of these AudioInputTracks allocates its own
    space to store its own AudioSegments even those data are same. This
    patch makes it possible for these AudioInputTracks to share the same
    AudioSegment data they need. By creating the AudioSegment in the
    NativeInputTrack, which is mapped to one specific device and is
    connected to the AudioInputTrack, the AudioInputTrack can fetch the
    AudioSegment data when they need and then append shared-references of
    the AudioChunk, inside the fetched AudioSegment, into their own
    AudioSegment. Therefore, we can have some AudioChunks created by the
    NativeInputTrack and shared among the AudioInputTracks in pass-through
    mode.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114801

--- dom/media/MediaTrackGraph.h
+++ dom/media/MediaTrackGraph.h
@@ -107,6 +107,14 @@ class AudioDataListenerInterface {
   virtual ~AudioDataListenerInterface() = default;
 
  public:
+  // Information for the interleaved buffer coming from the audio callbacks
+  struct BufferInfo {
+    AudioDataValue* mBuffer = nullptr;
+    size_t mFrames = 0;
+    uint32_t mChannels = 0;
+    TrackRate mRate = 0;
+  };
+
   /* These are for cubeb audio input & output streams: */
   /**
    * Output data to speakers, for use as the "far-end" data for echo
@@ -114,8 +122,7 @@ class AudioDataListenerInterface {
    * chunks.
    */
   virtual void NotifyOutputData(MediaTrackGraphImpl* aGraph,
-                                AudioDataValue* aBuffer, size_t aFrames,
-                                TrackRate aRate, uint32_t aChannels) = 0;
+                                BufferInfo aInfo) = 0;
   /**
    * An AudioCallbackDriver with an input stream signaling that it has stopped
    * for any reason and the AudioDataListener will not be notified of input data
@@ -127,8 +134,7 @@ class AudioDataListenerInterface {
    * guaranteed to be in any particular size chunks.
    */
   virtual void NotifyInputData(MediaTrackGraphImpl* aGraph,
-                               const AudioDataValue* aBuffer, size_t aFrames,
-                               TrackRate aRate, uint32_t aChannels,
+                               const BufferInfo aInfo,
                                uint32_t aAlreadyBuffered) = 0;
 
   /**
