106c1a2b0321f3f2d9959f28374d6877a8a816cc
Jan Varga <jvarga@mozilla.com>
2021-06-08 16:27:28 +0000

    Bug 1713820 - Add dedicated QM_LOG_ERROR_ENABLED identifier for conditional LogError compilation; r=dom-storage-reviewers,jstutte
    Differential Revision: https://phabricator.services.mozilla.com/D117132

---
+++ dom/quota/Config.h
@@ -0,0 +1,14 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef DOM_QUOTA_CONFIG_H_
+#define DOM_QUOTA_CONFIG_H_
+
+#if defined(EARLY_BETA_OR_EARLIER) || defined(DEBUG)
+#  define QM_LOG_ERROR_ENABLED
+#endif
+
+#endif  // DOM_QUOTA_CONFIG_H_
