4805e400f8b0a2eec03eed6e3286fff6143421d8
Luca Greco <lgreco@mozilla.com>
2021-06-09 19:20:41 +0000

    Bug 1682632 - part1.3: ExtensionEventManager webidl interface. r=baku,mixedpuppy
    WebIDL interface and c++ class to be used by all WebExtension API webidl bindings
    to expose the expected WebExtensions API events (e.g. browser.tabs.onUpdated etc.)
    
    Depends on D84682
    
    Differential Revision: https://phabricator.services.mozilla.com/D80604

---
+++ toolkit/components/extensions/webidl-api/ExtensionEventManager.cpp
@@ -0,0 +1,49 @@
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "ExtensionEventManager.h"
+
+#include "mozilla/dom/ExtensionEventManagerBinding.h"
+#include "nsIGlobalObject.h"
+
+namespace mozilla {
+namespace extensions {
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(ExtensionEventManager);
+NS_IMPL_CYCLE_COLLECTING_RELEASE(ExtensionEventManager)
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(ExtensionEventManager, mGlobal);
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(ExtensionEventManager)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_END
+
+ExtensionEventManager::ExtensionEventManager(nsIGlobalObject* aGlobal,
+                                             const nsAString& aNamespace,
+                                             const nsAString& aEventName)
+    : mGlobal(aGlobal), mAPINamespace(aNamespace), mEventName(aEventName) {
+  MOZ_DIAGNOSTIC_ASSERT(mGlobal);
+}
+
+JSObject* ExtensionEventManager::WrapObject(JSContext* aCx,
+                                            JS::Handle<JSObject*> aGivenProto) {
+  return dom::ExtensionEventManager_Binding::Wrap(aCx, this, aGivenProto);
+}
+
+nsIGlobalObject* ExtensionEventManager::GetParentObject() const {
+  return mGlobal;
+}
+
+bool ExtensionEventManager::HasListener(dom::Function& aCallback,
+                                        ErrorResult& aRv) const {
+  return false;
+}
+
+bool ExtensionEventManager::HasListeners(ErrorResult& aRv) const {
+  return false;
+}
+
+}  // namespace extensions
+}  // namespace mozilla
