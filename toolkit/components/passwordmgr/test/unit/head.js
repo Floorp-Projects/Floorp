7a15a3dacd5fefb002c73e651965e2b0c86ac2c9
Tim Giles <tgiles@mozilla.com>
2021-06-09 13:54:10 +0000

    Bug 1686071 - Add and fix tests due to new password generation feature. r=dimi
    Differential Revision: https://phabricator.services.mozilla.com/D114113

--- toolkit/components/passwordmgr/test/unit/head.js
+++ toolkit/components/passwordmgr/test/unit/head.js
@@ -53,6 +53,7 @@ const newPropertyBag = LoginHelper.newPropertyBag;
 const NEW_PASSWORD_HEURISTIC_ENABLED_PREF =
   "signon.generation.confidenceThreshold";
 const RELATED_REALMS_ENABLED_PREF = "signon.relatedRealms.enabled";
+const IMPROVED_PASSWORD_RULES_PREF = "signon.improvedPasswordRules.enabled";
 /**
  * All the tests are implemented with add_task, this starts them automatically.
  */
