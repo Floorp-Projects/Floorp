add43c91b7358a84878fad5d3d69f2a70710bdad
Nazım Can Altınova <canaltinova@gmail.com>
2021-06-09 11:22:55 +0000

    Bug 1690624 - Create empty ProfilerBindings header and cpp files r=gerald
    Differential Revision: https://phabricator.services.mozilla.com/D116514

---
+++ tools/profiler/public/ProfilerBindings.h
@@ -0,0 +1,21 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef ProfilerBindings_h
+#define ProfilerBindings_h
+
+/* FFI functions for Profiler Rust API to call into profiler */
+
+// Everything in here is safe to include unconditionally, implementations must
+// take !MOZ_GECKO_PROFILER into account.
+
+extern "C" {
+
+// TODO: Add the profiler API bindings here.
+
+}  // extern "C"
+
+#endif  // ProfilerBindings_h
