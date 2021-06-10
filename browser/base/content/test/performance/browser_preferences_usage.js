6b9285da0d91adace21469f03b125785885f5446
Nika Layzell <nika@thelayzells.com>
2021-06-10 01:36:35 +0000

    Bug 1713890 - Relax gfx.{content,canvas}.asure.backends pref usage checks, r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D117329

--- browser/base/content/test/performance/browser_preferences_usage.js
+++ browser/base/content/test/performance/browser_preferences_usage.js
@@ -238,11 +238,11 @@ add_task(async function navigate_around() {
       // https://bugzilla.mozilla.org/show_bug.cgi?id=1639497
       knownProblematicPrefs["gfx.canvas.azure.backends"] = {
         min: 100,
-        max: 101,
+        max: 110,
       };
       knownProblematicPrefs["gfx.content.azure.backends"] = {
         min: 100,
-        max: 101,
+        max: 110,
       };
       // The following 2 sandbox prefs are covered by
       // https://bugzilla.mozilla.org/show_bug.cgi?id=1639494
