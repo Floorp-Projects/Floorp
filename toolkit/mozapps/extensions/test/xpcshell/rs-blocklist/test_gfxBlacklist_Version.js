34138c4609a2161cf6a7e39457029af571c0186e
Rob Wu <rob@robwu.nl>
2021-06-10 14:15:05 +0000

    Bug 1714673 - Fix and re-enable test_gfxBlacklist_Version.js on Android r=jrmuizel
    The values for spoofVendorID/spoofDeviceID should match entries in
    test_gfxBlacklist_AllOS.json. These entries were added in
    https://hg.mozilla.org/mozilla-central/rev/21263fbfdcc2 but
    inadvertently not updated (because the test was disabled).
    Fixing that enables the test to use the expected entries.
    
    Several test expectations were no longer correct for Android and have
    been updated as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D116953

--- toolkit/mozapps/extensions/test/xpcshell/rs-blocklist/test_gfxBlacklist_Version.js
+++ toolkit/mozapps/extensions/test/xpcshell/rs-blocklist/test_gfxBlacklist_Version.js
@@ -19,8 +19,10 @@ async function run_test() {
   gfxInfo.QueryInterface(Ci.nsIGfxInfoDebug);
   gfxInfo.fireTestProcess();
 
+  // Save OS in variable since createAppInfo below will change it to "xpcshell".
+  const OS = Services.appinfo.OS;
   // Set the vendor/device ID, etc, to match the test file.
-  switch (Services.appinfo.OS) {
+  switch (OS) {
     case "WINNT":
       gfxInfo.spoofVendorID("0xabcd");
       gfxInfo.spoofDeviceID("0x1234");
@@ -38,8 +40,8 @@ async function run_test() {
       gfxInfo.spoofOSVersion(0xa0900);
       break;
     case "Android":
-      gfxInfo.spoofVendorID("abcd");
-      gfxInfo.spoofDeviceID("asdf");
+      gfxInfo.spoofVendorID("0xabcd");
+      gfxInfo.spoofDeviceID("0x1234");
       gfxInfo.spoofDriverVersion("5");
       break;
   }
@@ -81,10 +83,14 @@ async function run_test() {
     Assert.equal(status, Ci.nsIGfxInfo.FEATURE_STATUS_OK);
     Assert.equal(failureId.value, "");
 
-    // These four pass on Linux independent of the blocklist XML file as the
-    // try machines don't have support.
     status = gfxInfo.getFeatureStatus(Ci.nsIGfxInfo.FEATURE_OPENGL_LAYERS);
-    Assert.equal(status, Ci.nsIGfxInfo.FEATURE_BLOCKED_DRIVER_VERSION);
+    if (OS == "Android") {
+      // OpenGL layers are never blocklisted on Android, despite an entry in test_gfxBlacklist.json.
+      // https://searchfox.org/mozilla-central/rev/c1ec9ecbbc7eac698923ffd18c8594aa3e2e9da0/widget/android/GfxInfo.cpp#431-437
+      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_STATUS_OK);
+    } else {
+      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_BLOCKED_DRIVER_VERSION);
+    }
 
     status = gfxInfo.getFeatureStatus(
       Ci.nsIGfxInfo.FEATURE_WEBGL_OPENGL,
@@ -114,19 +120,35 @@ async function run_test() {
       Ci.nsIGfxInfo.FEATURE_WEBRTC_HW_ACCELERATION_H264,
       failureId
     );
-    Assert.equal(status, Ci.nsIGfxInfo.FEATURE_STATUS_OK);
+    if (OS == "Android" && status != Ci.nsIGfxInfo.FEATURE_STATUS_OK) {
+      // Hardware acceleration for H.264 varies by device.
+      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_BLOCKED_DEVICE);
+      Assert.equal(failureId.value, "FEATURE_FAILURE_WEBRTC_H264");
+    } else {
+      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_STATUS_OK);
+    }
 
     status = gfxInfo.getFeatureStatus(
       Ci.nsIGfxInfo.FEATURE_WEBRTC_HW_ACCELERATION_ENCODE,
       failureId
     );
-    Assert.equal(status, Ci.nsIGfxInfo.FEATURE_STATUS_OK);
+    if (OS == "Android" && status != Ci.nsIGfxInfo.FEATURE_STATUS_OK) {
+      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_BLOCKED_DEVICE);
+      Assert.equal(failureId.value, "FEATURE_FAILURE_WEBRTC_ENCODE");
+    } else {
+      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_STATUS_OK);
+    }
 
     status = gfxInfo.getFeatureStatus(
       Ci.nsIGfxInfo.FEATURE_WEBRTC_HW_ACCELERATION_DECODE,
       failureId
     );
-    Assert.equal(status, Ci.nsIGfxInfo.FEATURE_STATUS_OK);
+    if (OS == "Android" && status != Ci.nsIGfxInfo.FEATURE_STATUS_OK) {
+      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_BLOCKED_DEVICE);
+      Assert.equal(failureId.value, "FEATURE_FAILURE_WEBRTC_DECODE");
+    } else {
+      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_STATUS_OK);
+    }
 
     status = gfxInfo.getFeatureStatus(
       Ci.nsIGfxInfo.FEATURE_DIRECT3D_11_LAYERS,
