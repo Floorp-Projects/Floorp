3f4d8dfef603aad0c86cf2a6cd01f16a8578321d
Ryan Hunt <rhunt@eqrion.net>
2021-06-10 17:11:31 +0000

    Bug 1711073 - wasm: Add WasmInitExpr.h. r=jseward
    Moves InitExpr declarations from WasmTypes.h
    to WasmInitExpr.h. The definitions already lived in
    their own translation unit, so this is a header
    only change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D117043

--- js/src/wasm/WasmTypes.h
+++ js/src/wasm/WasmTypes.h
@@ -41,6 +41,7 @@
 #include "vm/NativeObject.h"
 #include "wasm/WasmBuiltins.h"
 #include "wasm/WasmConstants.h"
+#include "wasm/WasmInitExpr.h"
 #include "wasm/WasmSerialize.h"
 #include "wasm/WasmShareable.h"
 #include "wasm/WasmTypeDecls.h"
@@ -540,68 +541,6 @@ class BlockType {
   }
 
   bool operator!=(BlockType rhs) const { return !(*this == rhs); }
-};
-
-enum class InitExprKind {
-  None,
-  Literal,
-  Variable,
-};
-
-// A InitExpr describes a deferred initializer expression, used to initialize
-// a global or a table element offset. Such expressions are created during
-// decoding and actually executed on module instantiation.
-
-class InitExpr {
-  InitExprKind kind_;
-  // The bytecode for this constant expression if this is not a literal.
-  Bytes bytecode_;
-  // The value if this is a literal.
-  LitVal literal_;
-  // The value type of this constant expression in either case.
-  ValType type_;
-
- public:
-  InitExpr() : kind_(InitExprKind::None) {}
-
-  explicit InitExpr(LitVal literal)
-      : kind_(InitExprKind::Literal),
-        literal_(literal),
-        type_(literal.type()) {}
-
-  // Decode and validate a constant expression given at the current
-  // position of the decoder. Upon failure, the decoder contains the failure
-  // message or else the failure was an OOM.
-  static bool decodeAndValidate(Decoder& d, ModuleEnvironment* env,
-                                ValType expected, InitExpr* expr);
-
-  // Evaluate the constant expresssion with the given context. This may only
-  // fail due to an OOM, as all InitExpr's are required to have been validated.
-  bool evaluate(JSContext* cx, const ValVector& globalImportValues,
-                HandleWasmInstanceObject instanceObj,
-                MutableHandleVal result) const;
-
-  bool isLiteral() const { return kind_ == InitExprKind::Literal; }
-
-  // Gets the result of this expression if it was determined to be a literal.
-  LitVal literal() const {
-    MOZ_ASSERT(isLiteral());
-    return literal_;
-  }
-
-  // Get the type of the resulting value of this expression.
-  ValType type() const { return type_; }
-
-  // Allow moving, but not implicit copying
-  InitExpr(const InitExpr&) = delete;
-  InitExpr& operator=(const InitExpr&) = delete;
-  InitExpr(InitExpr&&) = default;
-  InitExpr& operator=(InitExpr&&) = default;
-
-  // Allow explicit cloning
-  [[nodiscard]] bool clone(const InitExpr& src);
-
-  WASM_DECLARE_SERIALIZABLE(InitExpr)
 };
 
 // CacheableChars is used to cacheably store UniqueChars.
