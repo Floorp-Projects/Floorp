9cb0429aa3ee5e3d82da0718aa7abae8e377b452
Olli Pettay <Olli.Pettay@helsinki.fi>
2021-06-10 19:54:45 +0000

    Bug 1715865 - Make BrowsingContext skippable, r=nika,mccr8
    One could optimize out MarkWrapperLive call if stored that wrapper is already live etc, but
    in practice that shouldn't matter too much.
    
    Differential Revision: https://phabricator.services.mozilla.com/D117456

--- docshell/base/BrowsingContext.h
+++ docshell/base/BrowsingContext.h
@@ -641,7 +641,7 @@ class BrowsingContext : public nsILoadContext, public nsWrapperCache {
   Nullable<WindowProxyHolder> GetWindow();
 
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
-  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(BrowsingContext)
+  NS_DECL_CYCLE_COLLECTION_SKIPPABLE_SCRIPT_HOLDER_CLASS(BrowsingContext)
   NS_DECL_NSILOADCONTEXT
 
   // Window APIs that are cross-origin-accessible (from the HTML spec).
