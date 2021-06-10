8aea134559400a3cf0e5aaa419d1af5fe0dda686
Jeff Muizelaar <jmuizelaar@mozilla.com>
2021-06-10 21:32:17 +0000

    Bug 1715895 - Disable sw-wr on non-x86 Linux in late beta and release. r=aosmond, a=pascalc
    This avoids bug 1714064 (SPARC) and bug 1714511 (ARM)
    
    Differential Revision: https://phabricator.services.mozilla.com/D117467

--- widget/gtk/GfxInfo.cpp
+++ widget/gtk/GfxInfo.cpp
@@ -758,12 +758,14 @@ const nsTArray<GfxDriverInfo>& GfxInfo::GetGfxDriverInfo() {
 
     ////////////////////////////////////
     // FEATURE_WEBRENDER_SOFTWARE - ALLOWLIST
+#  if defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || \
+      defined(__i386) || defined(__amd64__) || EARLY_BETA_OR_EARLIER
     APPEND_TO_DRIVER_BLOCKLIST(OperatingSystem::Linux, DeviceFamily::All,
                                nsIGfxInfo::FEATURE_WEBRENDER_SOFTWARE,
                                nsIGfxInfo::FEATURE_ALLOW_ALWAYS,
                                DRIVER_COMPARISON_IGNORED, V(0, 0, 0, 0),
                                "FEATURE_ROLLOUT_SOFTWARE_WR", "");
-
+#endif
     ////////////////////////////////////
     // FEATURE_WEBRENDER_COMPOSITOR
     APPEND_TO_DRIVER_BLOCKLIST(
