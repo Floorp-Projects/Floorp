9e61b4a2e9c034436b8a1c7fb5ef802c34fccdab
Paul Bone <pbone@mozilla.com>
2021-06-11 05:47:48 +0000

    Bug 1715705 - Ask the parent before doing a MEMORY_PRESSURE GC r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D117396

--- dom/base/nsJSEnvironment.h
+++ dom/base/nsJSEnvironment.h
@@ -107,6 +107,8 @@ class nsJSContext : public nsIScriptContext {
   static void PokeShrinkingGC();
   static void KillShrinkingGCTimer();
 
+  static void LowMemoryGC();
+
   static void MaybePokeCC();
   static void EnsureCCRunner(mozilla::TimeDuration aDelay,
                              mozilla::TimeDuration aBudget);
