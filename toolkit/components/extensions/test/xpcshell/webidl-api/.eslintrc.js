68aa2b00fe8ddabee83149a2166634a3711001a0
Luca Greco <lgreco@mozilla.com>
2021-06-11 18:58:11 +0000

    Bug 1682632 - part2.8: WebExtensions API request handling unit tests. r=baku,mixedpuppy
    - Added a new set of xpcshell-tests in toolkit/components/extensions/test/xpcshell/webidl-api
      for unit tests related to the WebIDL bindings API request forwarding
    
    Depends on D99886
    
    Differential Revision: https://phabricator.services.mozilla.com/D99887

---
+++ toolkit/components/extensions/test/xpcshell/webidl-api/.eslintrc.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+  env: {
+    // The tests in this folder are testing based on WebExtensions, so lets
+    // just define the webextensions environment here.
+    webextensions: true,
+  },
+};
