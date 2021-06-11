4c06787a227b9f46ae22b70611f1213891d72e03
Gregory Mierzwinski <gmierz2@outlook.com>
2021-06-11 15:52:47 +0000

    Bug 1708760 - Add a pageload test for about:welcome. r=perftest-reviewers,AlexandruIonescu
    Differential Revision: https://phabricator.services.mozilla.com/D117190

---
+++ testing/raptor/browsertime/welcome.js
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* eslint-env node */
+
+module.exports = async function(context, commands) {
+  context.log.info("Starting a first-install test");
+  let page_cycles = context.options.browsertime.page_cycles;
+
+  for (let count = 0; count < page_cycles; count++) {
+    context.log.info("Navigating to about:blank");
+    await commands.navigate("about:blank");
+
+    await commands.measure.start();
+    await commands.wait.byTime(1000);
+    await commands.navigate("about:welcome");
+    await commands.wait.byTime(2000);
+    await commands.measure.stop();
+
+    await commands.wait.byTime(2000);
+  }
+
+  context.log.info("First-install test ended.");
+  return true;
+};
