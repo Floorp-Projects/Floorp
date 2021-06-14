e4d7dd995a62ca5653527792b55c8f6ee6e74b02
Mats Palmgren <mats@mozilla.com>
2021-06-14 01:22:06 +0000

    Bug 1542807 part 3 - Add some style quirks for legacy ::markers created from list-style-type/list-style-image.  r=emilio
    Specifically:
    For "bullets", i.e. 'list-style-type:disc|circle|square|
    disclosure-closed|disclosure-open', we use a built-in font
    (-moz-bullet-font, which has glyphs for those symbols + space) to
    retain mostly backwards compatible rendering for those.  Authors may
    override that with an explicit 'font-family' ::marker style though.
    We also use this font for 'list-style-image' in case it would
    fallback to one of the above when the image fails to load (so that
    we get the same width space).
    
    When the -moz-bullet-font is used we also set 'font-synthesis' to
    avoid synthesizing italic/bold for this font. Authors may override
    this with an explicit ::marker declaration.
    
    We also set 'letter-spacing' and 'word-spacing' to the initial value
    for bullets for web-compat reasons.  Again, authors may override
    this with an explicit ::marker declaration. (This breaks backwards-
    compat slightly but makes us compatible with Chrome.  We used to
    ignore these for list-style-type:<string> too.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D111693

--- servo/components/style/values/specified/font.rs
+++ servo/components/style/values/specified/font.rs
@@ -1972,6 +1972,14 @@ impl FontSynthesis {
             style: true,
         }
     }
+    #[inline]
+    /// Get the 'none' value of font-synthesis
+    pub fn none() -> Self {
+        FontSynthesis {
+            weight: false,
+            style: false,
+        }
+    }
 }
 
 impl Parse for FontSynthesis {
