b4c92696a74d58a3f5a615da7bcd651f4a9fed83
Mike Hommey <mh+mozilla@glandium.org>
2021-06-15 09:26:00 +0000

    Bug 1716518 - Upgrade urlencoding to v1.3.3.
    Differential Revision: https://phabricator.services.mozilla.com/D117875
    
    Depends on D117874

--- third_party/rust/urlencoding/README.md
+++ third_party/rust/urlencoding/README.md
@@ -1,26 +1,16 @@
 # urlencoding
 
-[![Latest Version](https://img.shields.io/crates/v/urlencoding.svg)](https://crates.io/crates/urlencoding)
+[![Latest Version](https://img.shields.io/crates/v/urlencoding.svg)](https://lib.rs/crates/urlencoding)
 
-A Rust library for doing URL percentage encoding.
+A tiny Rust library for doing URL percentage encoding and decoding. It percent-encodes everything except alphanumerics and `-`, `_`, `.`, `~`.
 
-Installation
-============
+When decoding `+` is not treated as a space. Error recovery from incomplete percent-escapes follows the [WHATWG URL standard](https://url.spec.whatwg.org/).
 
-This crate can be downloaded through Cargo. To do so, add the following line to your `Cargo.toml` file, under `dependencies`:
-
-```toml
-urlencoding = "1.0.0"
-```
-
-Usage
-=====
+## Usage
 
 To encode a string, do the following:
 
 ```rust
-extern crate urlencoding;
-
 use urlencoding::encode;
 
 fn main() {
@@ -33,8 +23,6 @@ fn main() {
 To decode a string, it's only slightly different:
 
 ```rust
-extern crate urlencoding;
-
 use urlencoding::decode;
 
 fn main() {
@@ -44,7 +32,6 @@ fn main() {
 }
 ```
 
-License
-=======
+## License
 
 This project is licensed under the MIT license, Copyright (c) 2017 Bertram Truong. For more information see the `LICENSE` file.
--- third_party/rust/urlencoding/src/lib.rs
+++ third_party/rust/urlencoding/src/lib.rs
@@ -1,93 +1,18 @@
-use std::str;
-use std::string::FromUtf8Error;
+mod enc;
+pub use enc::encode;
+pub use enc::encode_binary;
+pub use enc::Encoded;
 
-pub fn encode(data: &str) -> String {
-    let mut escaped = String::new();
-    for b in data.as_bytes().iter() {
-        match *b as char {
-            // Accepted characters
-            'A'...'Z' | 'a'...'z' | '0'...'9' | '-' | '_' | '.' | '~' => escaped.push(*b as char),
-
-            // Everything else is percent-encoded
-            b => escaped.push_str(format!("%{:02X}", b as u32).as_str()),
-        };
-    }
-    return escaped;
-}
-
-pub fn decode(data: &str) -> Result<String, FromUrlEncodingError> {
-    validate_urlencoded_str(data)?;
-    let mut unescaped_bytes: Vec<u8> = Vec::new();
-    let mut bytes = data.bytes();
-    // If validate_urlencoded_str returned Ok, then we know
-    // every '%' is followed by 2 hex characters
-    while let Some(b) = bytes.next() {
-        match b as char {
-            '%' => {
-                let bytes_to_decode = &[bytes.next().unwrap(), bytes.next().unwrap()];
-                let hex_str = str::from_utf8(bytes_to_decode).unwrap();
-                unescaped_bytes.push(u8::from_str_radix(hex_str, 16).unwrap());
-            },
-            _ => {
-                // Assume whoever did the encoding intended what we got
-                unescaped_bytes.push(b);
-            }
-        }
-    }
-    String::from_utf8(unescaped_bytes).or_else(|e| Err(FromUrlEncodingError::Utf8CharacterError {
-        error: e,
-    }))
-}
-
-// Validates every '%' character is followed by exactly 2 hex
-// digits.
-fn validate_urlencoded_str(data: &str) -> Result<(), FromUrlEncodingError> {
-    let mut iter = data.char_indices();
-    while let Some((idx, chr)) = iter.next() {
-        match chr {
-            '%' => {
-                validate_percent_encoding(&mut iter, idx)?;
-            },
-            _ => continue,
-        }
-    }
-    Ok(())
-}
-
-// Validates the next two characters returned by the provided iterator are
-// hexadecimal digits.
-fn validate_percent_encoding(iter: &mut str::CharIndices, current_idx: usize) -> Result<(), FromUrlEncodingError> {
-    for _ in 0..2 {
-        match iter.next() {
-            // Only hex digits are valid
-            Some((_, c)) if c.is_digit(16) => {
-                continue
-            },
-            Some((i, c)) => return Err(FromUrlEncodingError::UriCharacterError {
-                character: c,
-                index: i,
-            }),
-            // We got a '%' without 2 characters after it, so mark the '%' as bad
-            None => return Err(FromUrlEncodingError::UriCharacterError {
-                character: '%',
-                index: current_idx,
-            }),
-        }
-    }
-    Ok(())
-}
-
-#[derive(Debug)]
-pub enum FromUrlEncodingError {
-    UriCharacterError { character: char, index: usize },
-    Utf8CharacterError { error: FromUtf8Error },
-}
+mod dec;
+pub use dec::decode;
+pub use dec::decode_binary;
+pub use dec::FromUrlEncodingError;
 
 #[cfg(test)]
 mod tests {
     use super::encode;
     use super::decode;
-    use super::FromUrlEncodingError;
+    use crate::dec::from_hex_digit;
 
     #[test]
     fn it_encodes_successfully() {
@@ -123,48 +48,35 @@ mod tests {
         assert_eq!(bad_encoded_string, decode(bad_encoded_string).unwrap());
     }
 
-    #[test]
-    fn it_decodes_unsuccessfuly_bad_percent_01() {
-        let bad_encoded_string = "this%2that";
-        let expected_idx = 6;
-        let expected_char = 't';
-
-        match decode(bad_encoded_string).unwrap_err() {
-            FromUrlEncodingError::UriCharacterError { index: i, character: c } => {
-                assert_eq!(expected_idx, i);
-                assert_eq!(expected_char, c)
-            },
-            _ => panic!()
-        }
-    }
 
     #[test]
-    fn it_decodes_unsuccessfuly_bad_percent_02() {
-        let bad_encoded_string = "this%20that%";
-        let expected_idx = 11;
-        let expected_char = '%';
+    fn misc() {
+        assert_eq!(3, from_hex_digit(b'3').unwrap());
+        assert_eq!(10, from_hex_digit(b'a').unwrap());
+        assert_eq!(15, from_hex_digit(b'F').unwrap());
+        assert_eq!(None, from_hex_digit(b'G'));
+        assert_eq!(None, from_hex_digit(9));
 
-        match decode(bad_encoded_string).unwrap_err() {
-            FromUrlEncodingError::UriCharacterError { index: i, character: c } => {
-                assert_eq!(expected_idx, i);
-                assert_eq!(expected_char, c)
-            },
-            _ => panic!()
-        }
-    }
-
-    #[test]
-    fn it_decodes_unsuccessfuly_bad_percent_03() {
-        let bad_encoded_string = "this%20that%2";
-        let expected_idx = 11;
-        let expected_char = '%';
-
-        match decode(bad_encoded_string).unwrap_err() {
-            FromUrlEncodingError::UriCharacterError { index: i, character: c } => {
-                assert_eq!(expected_idx, i);
-                assert_eq!(expected_char, c)
-            },
-            _ => panic!()
-        }
+        assert_eq!("pureascii", encode("pureascii"));
+        assert_eq!("pureascii", decode("pureascii").unwrap());
+        assert_eq!("", encode(""));
+        assert_eq!("", decode("").unwrap());
+        assert_eq!("%26a%25b%21c.d%3Fe", encode("&a%b!c.d?e"));
+        assert_eq!("%00", encode("\0"));
+        assert_eq!("%00x", encode("\0x"));
+        assert_eq!("x%00", encode("x\0"));
+        assert_eq!("x%00x", encode("x\0x"));
+        assert_eq!("aa%00%00bb", encode("aa\0\0bb"));
+        assert_eq!("\0", decode("\0").unwrap());
+        assert!(decode("%F0%0F%91%BE%20Hello%21").is_err());
+        assert_eq!("this that", decode("this%20that").unwrap());
+        assert_eq!("this that%", decode("this%20that%").unwrap());
+        assert_eq!("this that%2", decode("this%20that%2").unwrap());
+        assert_eq!("this that%%", decode("this%20that%%").unwrap());
+        assert_eq!("this that%2%", decode("this%20that%2%").unwrap());
+        assert_eq!("this%2that", decode("this%2that").unwrap());
+        assert_eq!("this%%2that", decode("this%%2that").unwrap());
+        assert_eq!("this%2x&that", decode("this%2x%26that").unwrap());
+        // assert_eq!("this%2&that", decode("this%2%26that").unwrap());
     }
 }
