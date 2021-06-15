bc81849ad1fd7becb30f4862a7ce38f7b6541de8
Dan Mosedale <dmose@mozilla.org>
2021-06-15 18:15:39 +0000

    Bug 1690377 - Upgrade Node 10 toolchain to Node 12, r=Standard8,mhentges
    Differential Revision: https://phabricator.services.mozilla.com/D117606

--- taskcluster/docker/recipes/install-node.sh
+++ taskcluster/docker/recipes/install-node.sh
@@ -3,14 +3,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This script installs Node v10.
+# This script installs Node v12.
 # XXX For now, this should match the version installed in
 # taskcluster/scripts/misc/repack-node.sh. Later we'll get the ESLint builder
 # to use the linux64-node toolchain directly.
 
-wget -O node.xz --progress=dot:mega https://nodejs.org/dist/v10.23.1/node-v10.23.1-linux-x64.tar.xz
-
-echo '207e5ec77ca655ba6fcde922d6b329acbb09898b0bd793ccfcce6c27a36fdff0  node.xz' | sha256sum -c
+wget -O node.xz --progress=dot:mega https://nodejs.org/download/release/v12.22.1/node-v12.22.1-linux-x64.tar.xz
+echo '8b537282c222ae4a40e019a52f769ca27b6640699bdde1510375e8d72da7d041' node.xz | sha256sum -c
 tar -C /usr/local -xJ --strip-components 1 < node.xz
 node -v  # verify
 npm -v
