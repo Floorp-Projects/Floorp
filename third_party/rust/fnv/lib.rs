d0426b9606c5b4d6c74e6a0c4b7655b0cdf6f3a0
Mike Hommey <mh+mozilla@glandium.org>
2021-06-15 21:24:42 +0000

    Bug 1716518 - Upgrade fnv to v1.0.7. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D117789

--- third_party/rust/fnv/lib.rs
+++ third_party/rust/fnv/lib.rs
@@ -21,53 +21,66 @@
 //! denial-of-service attacks, and can assume that its inputs are going to be
 //! small—a perfect use case for FNV.
 //!
-//!
-//! ## Using FNV in a `HashMap`
-//!
-//! The `FnvHashMap` type alias is the easiest way to use the standard library’s
-//! `HashMap` with FNV.
-//!
-//! ```rust
-//! use fnv::FnvHashMap;
-//!
-//! let mut map = FnvHashMap::default();
-//! map.insert(1, "one");
-//! map.insert(2, "two");
-//!
-//! map = FnvHashMap::with_capacity_and_hasher(10, Default::default());
-//! map.insert(1, "one");
-//! map.insert(2, "two");
-//! ```
-//!
-//! Note, the standard library’s `HashMap::new` and `HashMap::with_capacity`
-//! are only implemented for the `RandomState` hasher, so using `Default` to
-//! get the hasher is the next best option.
-//!
-//! ## Using FNV in a `HashSet`
-//!
-//! Similarly, `FnvHashSet` is a type alias for the standard library’s `HashSet`
-//! with FNV.
-//!
-//! ```rust
-//! use fnv::FnvHashSet;
-//!
-//! let mut set = FnvHashSet::default();
-//! set.insert(1);
-//! set.insert(2);
-//!
-//! set = FnvHashSet::with_capacity_and_hasher(10, Default::default());
-//! set.insert(1);
-//! set.insert(2);
-//! ```
+#![cfg_attr(feature = "std", doc = r#"
+
+## Using FNV in a `HashMap`
+
+The `FnvHashMap` type alias is the easiest way to use the standard library’s
+`HashMap` with FNV.
+
+```rust
+use fnv::FnvHashMap;
+
+let mut map = FnvHashMap::default();
+map.insert(1, "one");
+map.insert(2, "two");
+
+map = FnvHashMap::with_capacity_and_hasher(10, Default::default());
+map.insert(1, "one");
+map.insert(2, "two");
+```
+
+Note, the standard library’s `HashMap::new` and `HashMap::with_capacity`
+are only implemented for the `RandomState` hasher, so using `Default` to
+get the hasher is the next best option.
+
+## Using FNV in a `HashSet`
+
+Similarly, `FnvHashSet` is a type alias for the standard library’s `HashSet`
+with FNV.
+
+```rust
+use fnv::FnvHashSet;
+
+let mut set = FnvHashSet::default();
+set.insert(1);
+set.insert(2);
+
+set = FnvHashSet::with_capacity_and_hasher(10, Default::default());
+set.insert(1);
+set.insert(2);
+```
+"#)]
 //!
 //! [chongo]: http://www.isthe.com/chongo/tech/comp/fnv/index.html
 //! [faq]: https://www.rust-lang.org/en-US/faq.html#why-are-rusts-hashmaps-slow
-//! [graphs]: http://cglab.ca/~abeinges/blah/hash-rs/
+//! [graphs]: https://cglab.ca/~abeinges/blah/hash-rs/
 
+#![cfg_attr(not(feature = "std"), no_std)]
 
+#[cfg(all(not(feature = "std"), test))]
+extern crate alloc;
+
+#[cfg(feature = "std")]
 use std::default::Default;
+#[cfg(feature = "std")]
 use std::hash::{Hasher, BuildHasherDefault};
+#[cfg(feature = "std")]
 use std::collections::{HashMap, HashSet};
+#[cfg(not(feature = "std"))]
+use core::default::Default;
+#[cfg(not(feature = "std"))]
+use core::hash::{Hasher, BuildHasherDefault};
 
 /// An implementation of the Fowler–Noll–Vo hash function.
 ///
@@ -115,16 +128,21 @@ impl Hasher for FnvHasher {
 pub type FnvBuildHasher = BuildHasherDefault<FnvHasher>;
 
 /// A `HashMap` using a default FNV hasher.
+#[cfg(feature = "std")]
 pub type FnvHashMap<K, V> = HashMap<K, V, FnvBuildHasher>;
 
 /// A `HashSet` using a default FNV hasher.
+#[cfg(feature = "std")]
 pub type FnvHashSet<T> = HashSet<T, FnvBuildHasher>;
 
 
 #[cfg(test)]
 mod test {
     use super::*;
+    #[cfg(feature = "std")]
     use std::hash::Hasher;
+    #[cfg(not(feature = "std"))]
+    use alloc::vec::Vec;
 
     fn fnv1a(bytes: &[u8]) -> u64 {
         let mut hasher = FnvHasher::default();
