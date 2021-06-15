76fdd67526cee2210c2d262ce1993347bf627475
Paul Bone <pbone@mozilla.com>
2021-06-15 23:17:05 +0000

    Bug 1715705 - Ask the parent before doing a MEMORY_PRESSURE GC r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D117396

--- dom/base/nsJSEnvironment.cpp
+++ dom/base/nsJSEnvironment.cpp
@@ -317,18 +317,16 @@ nsJSEnvironmentObserver::Observe(nsISupports* aSubject, const char* aTopic,
         // slow and it likely won't help us anyway.
         return NS_OK;
       }
+      if (data.EqualsLiteral("heap-minimize")) {
+        // heap-minimize notifiers expect this to run synchronously
+        nsJSContext::DoLowMemoryGC();
+        return NS_OK;
+      }
       if (data.EqualsLiteral("low-memory")) {
         nsJSContext::SetLowMemoryState(true);
       }
-      nsJSContext::GarbageCollectNow(JS::GCReason::MEM_PRESSURE,
-                                     nsJSContext::NonIncrementalGC,
-                                     nsJSContext::ShrinkingGC);
-      nsJSContext::CycleCollectNow();
-      if (sScheduler.NeedsGCAfterCC()) {
-        nsJSContext::GarbageCollectNow(JS::GCReason::MEM_PRESSURE,
-                                       nsJSContext::NonIncrementalGC,
-                                       nsJSContext::ShrinkingGC);
-      }
+      // Asynchronously GC.
+      nsJSContext::LowMemoryGC();
     }
   } else if (!nsCRT::strcmp(aTopic, "memory-pressure-stop")) {
     nsJSContext::SetLowMemoryState(false);
@@ -1931,6 +1929,36 @@ void nsJSContext::PokeShrinkingGC() {
       &sShrinkingGCTimer, ShrinkingGCTimerFired, nullptr,
       StaticPrefs::javascript_options_compact_on_user_inactive_delay(),
       nsITimer::TYPE_ONE_SHOT_LOW_PRIORITY, "ShrinkingGCTimerFired");
+}
+
+// static
+void nsJSContext::DoLowMemoryGC() {
+  if (sShuttingDown) {
+    return;
+  }
+  nsJSContext::GarbageCollectNow(JS::GCReason::MEM_PRESSURE,
+                                 nsJSContext::NonIncrementalGC,
+                                 nsJSContext::ShrinkingGC);
+  nsJSContext::CycleCollectNow();
+  if (sScheduler.NeedsGCAfterCC()) {
+    nsJSContext::GarbageCollectNow(JS::GCReason::MEM_PRESSURE,
+                                   nsJSContext::NonIncrementalGC,
+                                   nsJSContext::ShrinkingGC);
+  }
+}
+
+// static
+void nsJSContext::LowMemoryGC() {
+  RefPtr<MayGCPromise> mbPromise = MayGCNow(JS::GCReason::MEM_PRESSURE);
+  if (!mbPromise) {
+    // Normally when the promise is null it means that IPC failed, that probably
+    // means that something bad happened, don't bother with the GC.
+    return;
+  }
+  mbPromise->Then(
+      GetMainThreadSerialEventTarget(), __func__,
+      [](bool aIgnored) { DoLowMemoryGC(); },
+      [](mozilla::ipc::ResponseRejectReason r) {});
 }
 
 // static
