fc2ac54f90bafc003e7c718295cc47ac7e0c768d
Mike Hommey <mh+mozilla@glandium.org>
2021-06-15 22:17:28 +0000

    Bug 1716518 - Upgrade profiling to v1.0.2. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D117838

--- third_party/rust/profiling/src/tracing_impl.rs
+++ third_party/rust/profiling/src/tracing_impl.rs
@@ -1,11 +1,11 @@
 #[macro_export]
 macro_rules! scope {
     ($name:expr) => {
-        let _span = $crate::tracing::span!(tracing::Level::INFO, $name);
+        let _span = $crate::tracing::span!($crate::tracing::Level::INFO, $name);
         let _span_entered = _span.enter();
     };
     ($name:expr, $data:expr) => {
-        let _span = $crate::tracing::span!(tracing::Level::INFO, $name, tag = $data);
+        let _span = $crate::tracing::span!($crate::tracing::Level::INFO, $name, tag = $data);
         let _span_entered = _span.enter();
     };
 }
