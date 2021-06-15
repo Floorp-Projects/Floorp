27d2f55329a3935ea1f8f0665a7c98aa44dad9b2
Mike Hommey <mh+mozilla@glandium.org>
2021-06-15 09:25:16 +0000

    Bug 1716518 - Upgrade sha-1 to v0.8.2.
    Differential Revision: https://phabricator.services.mozilla.com/D117858
    
    Depends on D117857

--- third_party/rust/sha-1/src/consts.rs
+++ third_party/rust/sha-1/src/consts.rs
@@ -2,16 +2,16 @@
 
 pub const STATE_LEN: usize = 5;
 
-#[cfg(not(feature = "asm"))]
+#[cfg(any(not(feature = "asm"), feature = "asm-aarch64"))]
 pub const BLOCK_LEN: usize = 16;
 
-#[cfg(not(feature = "asm"))]
+#[cfg(any(not(feature = "asm"), feature = "asm-aarch64"))]
 pub const K0: u32 = 0x5A827999u32;
-#[cfg(not(feature = "asm"))]
+#[cfg(any(not(feature = "asm"), feature = "asm-aarch64"))]
 pub const K1: u32 = 0x6ED9EBA1u32;
-#[cfg(not(feature = "asm"))]
+#[cfg(any(not(feature = "asm"), feature = "asm-aarch64"))]
 pub const K2: u32 = 0x8F1BBCDCu32;
-#[cfg(not(feature = "asm"))]
+#[cfg(any(not(feature = "asm"), feature = "asm-aarch64"))]
 pub const K3: u32 = 0xCA62C1D6u32;
 
 pub const H: [u32; STATE_LEN] = [
