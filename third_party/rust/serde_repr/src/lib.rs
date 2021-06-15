9a73e306761adcec1d8dadf7ba5851f13bc9ee9c
Mike Hommey <mh+mozilla@glandium.org>
2021-06-15 09:25:10 +0000

    Bug 1716518 - Upgrade serde_repr to v0.1.7.
    Differential Revision: https://phabricator.services.mozilla.com/D117856
    
    Depends on D117855

--- third_party/rust/serde_repr/src/lib.rs
+++ third_party/rust/serde_repr/src/lib.rs
@@ -1,3 +1,11 @@
+//! [![github]](https://github.com/dtolnay/serde-repr)&ensp;[![crates-io]](https://crates.io/crates/serde_repr)&ensp;[![docs-rs]](https://docs.rs/serde_repr)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logoColor=white&logo=data:image/svg+xml;base64,PHN2ZyByb2xlPSJpbWciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDUxMiA1MTIiPjxwYXRoIGZpbGw9IiNmNWY1ZjUiIGQ9Ik00ODguNiAyNTAuMkwzOTIgMjE0VjEwNS41YzAtMTUtOS4zLTI4LjQtMjMuNC0zMy43bC0xMDAtMzcuNWMtOC4xLTMuMS0xNy4xLTMuMS0yNS4zIDBsLTEwMCAzNy41Yy0xNC4xIDUuMy0yMy40IDE4LjctMjMuNCAzMy43VjIxNGwtOTYuNiAzNi4yQzkuMyAyNTUuNSAwIDI2OC45IDAgMjgzLjlWMzk0YzAgMTMuNiA3LjcgMjYuMSAxOS45IDMyLjJsMTAwIDUwYzEwLjEgNS4xIDIyLjEgNS4xIDMyLjIgMGwxMDMuOS01MiAxMDMuOSA1MmMxMC4xIDUuMSAyMi4xIDUuMSAzMi4yIDBsMTAwLTUwYzEyLjItNi4xIDE5LjktMTguNiAxOS45LTMyLjJWMjgzLjljMC0xNS05LjMtMjguNC0yMy40LTMzLjd6TTM1OCAyMTQuOGwtODUgMzEuOXYtNjguMmw4NS0zN3Y3My4zek0xNTQgMTA0LjFsMTAyLTM4LjIgMTAyIDM4LjJ2LjZsLTEwMiA0MS40LTEwMi00MS40di0uNnptODQgMjkxLjFsLTg1IDQyLjV2LTc5LjFsODUtMzguOHY3NS40em0wLTExMmwtMTAyIDQxLjQtMTAyLTQxLjR2LS42bDEwMi0zOC4yIDEwMiAzOC4ydi42em0yNDAgMTEybC04NSA0Mi41di03OS4xbDg1LTM4Ljh2NzUuNHptMC0xMTJsLTEwMiA0MS40LTEwMi00MS40di0uNmwxMDItMzguMiAxMDIgMzguMnYuNnoiPjwvcGF0aD48L3N2Zz4K
+//!
+//! <br>
+//!
 //! Derive `Serialize` and `Deserialize` that delegates to the underlying repr
 //! of a C-like enum.
 //!
@@ -26,7 +34,7 @@
 //! }
 //! ```
 
-#![recursion_limit = "128"]
+#![allow(clippy::single_match_else)]
 
 extern crate proc_macro;
 
@@ -55,6 +63,7 @@ pub fn derive_serialize(input: TokenStream) -> TokenStream {
 
     TokenStream::from(quote! {
         impl serde::Serialize for #ident {
+            #[allow(clippy::use_self)]
             fn serialize<S>(&self, serializer: S) -> core::result::Result<S::Ok, S::Error>
             where
                 S: serde::Serializer
@@ -78,7 +87,6 @@ pub fn derive_deserialize(input: TokenStream) -> TokenStream {
     let declare_discriminants = input.variants.iter().map(|variant| {
         let variant = &variant.ident;
         quote! {
-            #[allow(non_upper_case_globals)]
             const #variant: #repr = #ident::#variant as #repr;
         }
     });
@@ -115,12 +123,14 @@ pub fn derive_deserialize(input: TokenStream) -> TokenStream {
 
     TokenStream::from(quote! {
         impl<'de> serde::Deserialize<'de> for #ident {
+            #[allow(clippy::use_self)]
             fn deserialize<D>(deserializer: D) -> core::result::Result<Self, D::Error>
             where
                 D: serde::Deserializer<'de>,
             {
                 struct discriminant;
 
+                #[allow(non_upper_case_globals)]
                 impl discriminant {
                     #(#declare_discriminants)*
                 }
