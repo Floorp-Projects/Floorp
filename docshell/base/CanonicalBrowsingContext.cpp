9778a93dc801544459176e77e7bfa5a75e41b904
Kashav Madan <kshvmdn@gmail.com>
2021-06-16 14:05:53 +0000

    Bug 1716444 - Don't do final tab state flush for replaced contexts, r=farre,annyG
    If we're destroying the frame loader of a replaced browsing context we'll end up
    firing browser-shutdown-tabstate-updated for a tab that wasn't actually closed.
    
    This results in us cleaning up Session Store state earlier than expected, which
    means we drop future updates to SessionStoreInternal._closedTabs.
    
    Fixes browser_sessionHistory.js, browser_async_remove_tab.js, and possibly
    browser_491168.js for SHIP+BFCache.
    
    Differential Revision: https://phabricator.services.mozilla.com/D117944

--- docshell/base/CanonicalBrowsingContext.cpp
+++ docshell/base/CanonicalBrowsingContext.cpp
@@ -224,10 +224,15 @@ void CanonicalBrowsingContext::ReplacedBy(
   MOZ_ASSERT(!aNewContext->mWebProgress);
   MOZ_ASSERT(!aNewContext->mSessionHistory);
   MOZ_ASSERT(IsTop() && aNewContext->IsTop());
+
+  mIsReplaced = true;
+  aNewContext->mIsReplaced = false;
+
   if (mStatusFilter) {
     mStatusFilter->RemoveProgressListener(mDocShellProgressBridge);
     mStatusFilter = nullptr;
   }
+
   mWebProgress->ContextReplaced(aNewContext);
   aNewContext->mWebProgress = std::move(mWebProgress);
 
