ff74a9535257ad782569156012d5b9f8727c12e7
Marian-Vasile Laza <mlaza@mozilla.com>
2021-06-16 11:59:28 +0300

    Backed out 2 changesets (bug 1707884, bug 1716284) for causing xpshell failures on test_layout-reflows-observer.js CLOSED TREE
    Backed out changeset 58c3344a4f19 (bug 1707884)
    Backed out changeset 22adda3b4bb1 (bug 1716284)

--- devtools/server/actors/resources/index.js
+++ devtools/server/actors/resources/index.js
@@ -16,7 +16,6 @@ const TYPES = {
   NETWORK_EVENT: "network-event",
   STYLESHEET: "stylesheet",
   NETWORK_EVENT_STACKTRACE: "network-event-stacktrace",
-  REFLOW: "reflow",
   SOURCE: "source",
   THREAD_STATE: "thread-state",
   SERVER_SENT_EVENT: "server-sent-event",
@@ -75,9 +74,6 @@ const FrameTargetResources = augmentResourceDictionary({
   },
   [TYPES.NETWORK_EVENT_STACKTRACE]: {
     path: "devtools/server/actors/resources/network-events-stacktraces",
-  },
-  [TYPES.REFLOW]: {
-    path: "devtools/server/actors/resources/reflow",
   },
   [TYPES.SOURCE]: {
     path: "devtools/server/actors/resources/sources",
