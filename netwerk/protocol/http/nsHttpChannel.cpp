d90d8d2adc4489673b597999c25155672ce088a6
Mike Kaply <mozilla@kaply.com>
2021-06-16 14:24:43 +0000

    Bug 1695693 - Add support for Windows 10 SSO on Firefox. r=valentin,necko-reviewers,dveditz,johannh
    Differential Revision: https://phabricator.services.mozilla.com/D114540

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -134,6 +134,9 @@
 #include "mozilla/dom/SecFetch.h"
 #include "mozilla/net/TRRService.h"
 #include "mozilla/URLQueryStringStripper.h"
+#ifdef XP_WIN
+#  include "HttpWinUtils.h"
+#endif
 
 #ifdef MOZ_TASK_TRACER
 #  include "GeckoTaskTracer.h"
@@ -388,6 +391,19 @@ nsresult nsHttpChannel::PrepareToConnect() {
   LOG(("nsHttpChannel::PrepareToConnect [this=%p]\n", this));
 
   AddCookiesToRequest();
+
+#ifdef XP_WIN
+  // If Windows 10 SSO is enabled, we potentially add auth information to
+  // secure top level loads (DOCUMENTs) that aren't anonymous or
+  // private browsing.
+  if (StaticPrefs::network_http_windows10_sso_enabled() &&
+      mURI->SchemeIs("https") &&
+      mLoadInfo->GetExternalContentPolicyType() ==
+          ExtContentPolicy::TYPE_DOCUMENT &&
+      !(mLoadFlags & LOAD_ANONYMOUS) && !mPrivateBrowsing) {
+    AddWindowsSSO(this);
+  }
+#endif
 
   // notify "http-on-modify-request" observers
   CallOnModifyRequestObservers();
