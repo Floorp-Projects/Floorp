79cba7d9eab0b45731b00484c47f07698363b273
Robert Mader <robert.mader@posteo.de>
2021-06-17 19:50:59 +0000

    Bug 1713686 - Disbale redraw_on_invalidation in RenderCompositorSWGL on Wayland, r=lsalzman,gfx-reviewers
    This was introduced in D106246. Most likely it is only necessary
    on uncomposited legacy desktops. So on Linux only enable it for X11,
    leaving out Wayland.
    
    Removing it makes `RenderCompositorSWGL` behave (more) equally to
    `RenderCompositorOGLSWGL`, massively reducing overpaint on certain
    websites like testufo.com and improving frame rates on many low-end
    devices.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118192

--- gfx/webrender_bindings/RenderCompositorSWGL.cpp
+++ gfx/webrender_bindings/RenderCompositorSWGL.cpp
@@ -9,6 +9,10 @@
 #include "mozilla/gfx/Logging.h"
 #include "mozilla/widget/CompositorWidget.h"
 
+#ifdef MOZ_WIDGET_GTK
+#  include "mozilla/WidgetUtilsGtk.h"
+#endif
+
 namespace mozilla {
 using namespace gfx;
 
@@ -264,8 +268,14 @@ void RenderCompositorSWGL::GetCompositorCapabilities(
   // Always support a single update rect for SwCompositor
   aCaps->max_update_rects = 1;
 
-  // When the window contents may be damaged, we need to force a full redraw.
+  // On uncomposited desktops such as X11 without compositor or Window 7 with
+  // Aero disabled we need to force a full redraw when the window contents may
+  // be damaged.
+#ifdef MOZ_WIDGET_GTK
+  aCaps->redraw_on_invalidation = widget::GdkIsX11Display();
+#else
   aCaps->redraw_on_invalidation = true;
+#endif
 }
 
 }  // namespace wr
