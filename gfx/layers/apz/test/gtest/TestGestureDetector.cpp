d43d943370ff8da9f95fe3d2778c1c6de1b19b10
Marian-Vasile Laza <mlaza@mozilla.com>
2021-06-17 09:31:12 +0300

    Backed out 3 changesets (bug 1713715) for causing gtest failures in BlockingResourceBase.cpp
    Backed out changeset 03158c35a264 (bug 1713715)
    Backed out changeset a0be853c5042 (bug 1713715)
    Backed out changeset 5785ea7b467a (bug 1713715)

--- gfx/layers/apz/test/gtest/TestGestureDetector.cpp
+++ gfx/layers/apz/test/gtest/TestGestureDetector.cpp
@@ -579,8 +579,6 @@ TEST_F(APZCGestureDetectorTester, DoubleTap) {
   MakeApzcWaitForMainThread();
   MakeApzcZoomable();
 
-  apzc->Metrics().SetIsRootContent(true);
-
   EXPECT_CALL(*mcc, HandleTap(TapType::eSingleTap, LayoutDevicePoint(10, 10), 0,
                               apzc->GetGuid(), _))
       .Times(0);
