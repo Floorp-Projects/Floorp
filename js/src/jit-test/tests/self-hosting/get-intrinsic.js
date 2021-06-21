70eef8d0ef1fbc5d938d72d5e5db85fbe275ee39
Ted Campbell <tcampbell@mozilla.com>
2021-06-21 20:13:51 +0000

    Bug 1716901 - Use getIntrinsicValue to implement getSelfHostedValue. r=jandem
    Use the value from the intrinsics holder instead of making a new copy each time.
    This also lets us remove the self-hosting global in the future.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118365

---
+++ js/src/jit-test/tests/self-hosting/get-intrinsic.js
@@ -0,0 +1,19 @@
+
+var intrinsic_names = [
+    "IsConstructor",    // Implementation in C++
+    "ArrayIndexOf",     // Implementation in JS
+    "localeCache",      // Self-hosting variable
+];
+
+for (var name of intrinsic_names) {
+    // GetIntrinsic in same global should have consistent values
+    assertEq(getSelfHostedValue(name), getSelfHostedValue(name));
+
+    // Different globals shouldn't reuse intrinsics.
+    for (var newCompartment of [true, false]) {
+        let g = newGlobal({newCompartment});
+        let a = evaluate(`getSelfHostedValue("${name}")`, { global: g })
+        let b = getSelfHostedValue(name);
+        assertEq(a === b, false);
+    }
+}
