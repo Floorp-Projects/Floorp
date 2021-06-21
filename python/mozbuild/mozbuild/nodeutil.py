9f7ab60ea05394b07f049b60fcc9f5433d63ac85
Mitchell Hentges <mhentges@mozilla.com>
2021-06-21 16:47:55 +0000

    Bug 1716600: Remove looping from Node's find_executable() r=firefox-build-system-reviewers,nalexander
    Since we're no longer checking for both "nodejs" and "node",
    `find_executable()` can be simplified.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118376

--- python/mozbuild/mozbuild/nodeutil.py
+++ python/mozbuild/mozbuild/nodeutil.py
@@ -96,7 +96,7 @@ def find_node_executable(
     # "nodejs" is first in the tuple on the assumption that it's only likely to
     # exist on systems (probably linux distros) where there is a program in the path
     # called "node" that does something else.
-    return find_executable(["node"], min_version)
+    return find_executable("node", min_version)
 
 
 def find_npm_executable(min_version=NPM_MIN_VERSION):
@@ -106,30 +106,28 @@ def find_npm_executable(min_version=NPM_MIN_VERSION):
     version tuple. Both tuple entries will be None if a Node executable
     could not be resolved.
     """
-    return find_executable(["npm"], min_version, True)
+    return find_executable("npm", min_version, True)
 
 
-def find_executable(names, min_version, use_node_for_version_check=False):
+def find_executable(name, min_version, use_node_for_version_check=False):
     paths = find_node_paths()
 
     found_exe = None
-    for name in names:
-        exe = which(name, path=paths)
+    exe = which(name, path=paths)
 
-        if not exe:
-            continue
+    if not exe:
+        return None, None
 
-        if not found_exe:
-            found_exe = exe
+    if not found_exe:
+        found_exe = exe
 
-        # We always verify we can invoke the executable and its version is
-        # sane.
-        try:
-            version = check_executable_version(exe, use_node_for_version_check)
-        except (subprocess.CalledProcessError, ValueError):
-            continue
+    # Verify we can invoke the executable and its version is acceptable.
+    try:
+        version = check_executable_version(exe, use_node_for_version_check)
+    except (subprocess.CalledProcessError, ValueError):
+        return found_exe, None
 
-        if version >= min_version:
-            return exe, version.version
+    if version >= min_version:
+        return exe, version.version
 
     return found_exe, None
