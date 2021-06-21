1d14b0c651c358b41fe6baaf9875b7be98ae0a41
Harry Twyford <htwyford@mozilla.com>
2021-06-21 14:32:46 +0000

    Bug 1716025 - Part 2 - Add pref that can extend the metadata blocklist. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D117544

--- browser/components/places/tests/browser/interactions/browser_interactions_blocklist.js
+++ browser/components/places/tests/browser/interactions/browser_interactions_blocklist.js
@@ -8,15 +8,35 @@
 const ALLOWED_TEST_URL = "https://example.com/";
 const BLOCKED_TEST_URL = "https://example.com/browser";
 
+XPCOMUtils.defineLazyModuleGetters(this, {
+  InteractionsBlocklist: "resource:///modules/InteractionsBlocklist.jsm",
+});
+
 add_task(async function setup() {
+  let oldBlocklistValue = Services.prefs.getStringPref(
+    "places.interactions.customBlocklist",
+    "[]"
+  );
+  Services.prefs.setStringPref("places.interactions.customBlocklist", "[]");
+
   sinon.spy(Interactions, "_updateDatabase");
 
   registerCleanupFunction(() => {
     sinon.restore();
+    Services.prefs.setStringPref(
+      "places.interactions.customBlocklist",
+      oldBlocklistValue
+    );
   });
 });
-
-add_task(async function test() {
+/**
+ * Loads the blocked URL, then loads about:blank to trigger the end of the
+ * interaction with the blocked URL.
+ *
+ * @param {boolean} expectRecording
+ *   True if we expect the blocked URL to have been recorded in the database.
+ */
+async function loadBlockedUrl(expectRecording) {
   await BrowserTestUtils.withNewTab(ALLOWED_TEST_URL, async browser => {
     Interactions._pageViewStartTime = Cu.now() - 10000;
 
@@ -35,16 +55,52 @@ add_task(async function test() {
     BrowserTestUtils.loadURI(browser, "about:blank");
     await BrowserTestUtils.browserLoaded(browser, false, "about:blank");
 
-    // We should not have updated the database with BLOCKED_TEST_URL because it
-    // is blocklisted. We wait a little to make sure _updateDatabase is not
-    // going to fire.
-    // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
-    await new Promise(resolve => setTimeout(resolve, 500));
-    await assertDatabaseValues([
-      {
-        url: ALLOWED_TEST_URL,
-        totalViewTime: 10000,
-      },
-    ]);
+    if (expectRecording) {
+      await assertDatabaseValues([
+        {
+          url: ALLOWED_TEST_URL,
+          totalViewTime: 10000,
+        },
+        {
+          url: BLOCKED_TEST_URL,
+          totalViewTime: 20000,
+        },
+      ]);
+    } else {
+      // We should not have updated the database with BLOCKED_TEST_URL because it
+      // is blocklisted. We wait a little to make sure _updateDatabase is not
+      // going to fire.
+      // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+      await new Promise(resolve => setTimeout(resolve, 500));
+      await assertDatabaseValues([
+        {
+          url: ALLOWED_TEST_URL,
+          totalViewTime: 10000,
+        },
+      ]);
+    }
+    sinon.reset();
   });
+}
+
+add_task(async function test() {
+  info("Record BLOCKED_TEST_URL because it is not yet blocklisted.");
+  await loadBlockedUrl(true);
+
+  info("Add BLOCKED_TEST_URL to the blocklist and verify it is not recorded.");
+  let blockedRegex = /^(https?:\/\/)?example\.com\/browser/i;
+  InteractionsBlocklist.addRegexToBlocklist(blockedRegex);
+  Assert.equal(
+    Services.prefs.getStringPref("places.interactions.customBlocklist", "[]"),
+    JSON.stringify([blockedRegex.toString()])
+  );
+  await loadBlockedUrl(false);
+
+  info("Remove BLOCKED_TEST_URL from the blocklist and verify it is recorded.");
+  InteractionsBlocklist.removeRegexFromBlocklist(blockedRegex);
+  Assert.equal(
+    Services.prefs.getStringPref("places.interactions.customBlocklist", "[]"),
+    JSON.stringify([])
+  );
+  await loadBlockedUrl(true);
 });
