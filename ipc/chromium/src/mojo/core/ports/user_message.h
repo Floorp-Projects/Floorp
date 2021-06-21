cb2374ca8747ea7b0543aa2e2de68ea086ab829b
Nika Layzell <nika@thelayzells.com>
2021-06-21 21:53:06 +0000

    Bug 1706374 - Part 3: Get mojo/core/ports building in libxul, r=handyman
    This involves replacing a number of types normally provided by chromium's
    `base` with their XPCOM counterparts etc.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112767

--- ipc/chromium/src/mojo/core/ports/user_message.h
+++ ipc/chromium/src/mojo/core/ports/user_message.h
@@ -7,9 +7,6 @@
 
 #include <stddef.h>
 
-#include "base/component_export.h"
-#include "base/macros.h"
-
 namespace mojo {
 namespace core {
 namespace ports {
@@ -24,12 +21,15 @@ namespace ports {
 // |kUserMessageTypeInfo| and pass its address down to the UserMessage
 // constructor. The type of a UserMessage can then be dynamically inspected by
 // comparing |type_info()| to any subclass's |&kUserMessageTypeInfo|.
-class COMPONENT_EXPORT(MOJO_CORE_PORTS) UserMessage {
+class UserMessage {
  public:
   struct TypeInfo {};
 
   explicit UserMessage(const TypeInfo* type_info);
   virtual ~UserMessage();
+
+  UserMessage(const UserMessage&) = delete;
+  void operator=(const UserMessage&) = delete;
 
   const TypeInfo* type_info() const { return type_info_; }
 
@@ -47,8 +47,6 @@ class COMPONENT_EXPORT(MOJO_CORE_PORTS) UserMessage {
 
  private:
   const TypeInfo* const type_info_;
-
-  DISALLOW_COPY_AND_ASSIGN(UserMessage);
 };
 
 }  // namespace ports
