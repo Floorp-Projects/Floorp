c1853d27206d07f19844e3d6550b1f8916db13c3
Valentin Gosu <valentin.gosu@gmail.com>
2021-06-22 09:19:55 +0000

    Bug 1696138 - Make a copy of the host before calling getaddrinfo r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D118010

--- netwerk/dns/GetAddrInfo.cpp
+++ netwerk/dns/GetAddrInfo.cpp
@@ -350,7 +350,14 @@ nsresult GetAddrInfo(const nsACString& aHost, uint16_t aAddressFamily,
     return (*aAddrInfo)->Addresses().Length() ? NS_OK : NS_ERROR_UNKNOWN_HOST;
   }
 
-  nsAutoCString host(aHost);
+  nsAutoCString host;
+  if (StaticPrefs::network_dns_copy_string_before_call()) {
+    host = Substring(aHost.BeginReading(), aHost.Length());
+    MOZ_ASSERT(aHost.BeginReading() != host.BeginReading());
+  } else {
+    host = aHost;
+  }
+
   if (gNativeIsLocalhost) {
     // pretend we use the given host but use IPv4 localhost instead!
     host = "localhost"_ns;
