34be8e24a763e4986120feb3fc507e40112af930
Jan Varga <jvarga@mozilla.com>
2021-06-23 21:26:07 +0000

    Bug 1716568 - Move implementation details of InitializationInfo to a cpp file; r=dom-storage-reviewers,jstutte
    Differential Revision: https://phabricator.services.mozilla.com/D118131

---
+++ dom/quota/InitializationTypes.cpp
@@ -0,0 +1,61 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "InitializationTypes.h"
+
+#include "mozilla/Telemetry.h"
+#include "mozilla/TelemetryHistogramEnums.h"
+#include "nsString.h"
+
+namespace mozilla::dom::quota {
+
+namespace {
+
+// TODO: Use constexpr here once bug 1594094 is addressed.
+nsLiteralCString GetInitializationString(const Initialization aInitialization) {
+  switch (aInitialization) {
+    case Initialization::Storage:
+      return "Storage"_ns;
+    case Initialization::TemporaryStorage:
+      return "TemporaryStorage"_ns;
+    case Initialization::DefaultRepository:
+      return "DefaultRepository"_ns;
+    case Initialization::TemporaryRepository:
+      return "TemporaryRepository"_ns;
+    case Initialization::UpgradeStorageFrom0_0To1_0:
+      return "UpgradeStorageFrom0_0To1_0"_ns;
+    case Initialization::UpgradeStorageFrom1_0To2_0:
+      return "UpgradeStorageFrom1_0To2_0"_ns;
+    case Initialization::UpgradeStorageFrom2_0To2_1:
+      return "UpgradeStorageFrom2_0To2_1"_ns;
+    case Initialization::UpgradeStorageFrom2_1To2_2:
+      return "UpgradeStorageFrom2_1To2_2"_ns;
+    case Initialization::UpgradeStorageFrom2_2To2_3:
+      return "UpgradeStorageFrom2_2To2_3"_ns;
+    case Initialization::UpgradeFromIndexedDBDirectory:
+      return "UpgradeFromIndexedDBDirectory"_ns;
+    case Initialization::UpgradeFromPersistentStorageDirectory:
+      return "UpgradeFromPersistentStorageDirectory"_ns;
+
+    default:
+      MOZ_CRASH("Bad initialization value!");
+  }
+}
+
+}  // namespace
+
+void InitializationInfo::ReportFirstInitializationAttempt(
+    const Initialization aInitialization, const bool aSuccess) {
+  MOZ_ASSERT(!InitializationAttempted(aInitialization));
+
+  mInitializationAttempts |= aInitialization;
+
+  Telemetry::Accumulate(Telemetry::QM_FIRST_INITIALIZATION_ATTEMPT,
+                        GetInitializationString(aInitialization),
+                        static_cast<uint32_t>(aSuccess));
+}
+
+}  // namespace mozilla::dom::quota
