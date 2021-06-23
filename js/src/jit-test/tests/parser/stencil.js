7ccbaae172652fd6fe3eba6ec201cb0757e522f2
Tooru Fujisawa <arai_a@mac.com>
2021-06-23 05:15:13 +0000

    Bug 1717002 - Part 5: Add offThreadCompileToStencil and finishOffThreadCompileToStencil. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D118316

--- js/src/jit-test/tests/parser/stencil.js
+++ js/src/jit-test/tests/parser/stencil.js
@@ -1,16 +1,25 @@
-function test(script_str) {
-  const eval_f = eval;
   const options = {
     fileName: "compileToStencil-DATA.js",
     lineNumber: 1,
     forceFullParse: true,
   };
+
+function testMainThread(script_str) {
+  const eval_f = eval;
   const stencil = compileToStencil(script_str, options);
   const result = evalStencil(stencil, options);
   assertEq(result, eval_f(script_str));
 }
 
-test(`
+function testOffThread(script_str) {
+  const eval_f = eval;
+  const job = offThreadCompileToStencil(script_str, options);
+  const stencil = finishOffThreadCompileToStencil(job);
+  const result = evalStencil(stencil, options);
+  assertEq(result, eval_f(script_str));
+}
+
+testMainThread(`
 var a = 10;
 let b = 20, c = 30;
 const d = 40;
@@ -19,3 +28,15 @@ function f() {
 }
 f();
 `);
+
+if (helperThreadCount() > 0) {
+  testOffThread(`
+var a2 = 10;
+let b2 = 20, c2 = 30;
+const d2 = 40;
+function f2() {
+  return a2 + b2 + c2 + d2;
+}
+f2();
+`);
+}
