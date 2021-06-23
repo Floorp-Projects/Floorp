42b7f5a145805604eb457efb5aaba6c741c0c9af
Nihanth Subramanya <nhnt11@gmail.com>
2021-06-23 15:24:01 +0000

    Bug 1713036 - Use Remote Settings config in DoH preferences UI. r=jaws,preferences-reviewers,necko-reviewers,valentin
    Depends on D116798
    
    Differential Revision: https://phabricator.services.mozilla.com/D117960

--- netwerk/dns/TRRServiceBase.cpp
+++ netwerk/dns/TRRServiceBase.cpp
@@ -13,6 +13,7 @@
 #include "nsIDNSService.h"
 // Put DNSLogging.h at the end to avoid LOG being overwritten by other headers.
 #include "DNSLogging.h"
+#include "mozilla/StaticPrefs_network.h"
 
 namespace mozilla {
 namespace net {
@@ -85,7 +86,7 @@ void TRRServiceBase::CheckURIPrefs() {
   }
 
   // Otherwise just use the default value.
-  MaybeSetPrivateURI(mURIPref);
+  MaybeSetPrivateURI(mDefaultURIPref);
 }
 
 // static
@@ -144,6 +145,7 @@ void TRRServiceBase::OnTRRURIChange() {
   mURIPrefHasUserValue = Preferences::HasUserValue("network.trr.uri");
   Preferences::GetCString("network.trr.uri", mURIPref);
   Preferences::GetCString(kRolloutURIPref, mRolloutURIPref);
+  Preferences::GetCString("network.trr.default_provider_uri", mDefaultURIPref);
 
   CheckURIPrefs();
 }
