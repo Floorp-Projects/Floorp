5be222aba9285a909d9eca724e9de378b22cb613
Kashav Madan <kshvmdn@gmail.com>
2021-06-26 05:49:12 +0000

    Bug 1703692 - Use ChildOffset to build the offset path for flushes, r=nika
    We can't rely on the Children list since it may have been cleared on shutdown.
    Since we don't clear parent edges, walking the parent chain and using
    mChildOffset works.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118384

--- toolkit/components/sessionstore/SessionStoreRestoreData.cpp
+++ toolkit/components/sessionstore/SessionStoreRestoreData.cpp
@@ -19,20 +19,14 @@ bool SessionStoreRestoreData::IsEmpty() {
 
 SessionStoreRestoreData* SessionStoreRestoreData::FindDataForChild(
     BrowsingContext* aContext) {
-  nsTArray<uint32_t> offsets;
-  for (const BrowsingContext* current = aContext;
-       current && current->GetParent(); current = current->GetParent()) {
-    // Don't bother continuing if any frame in our chain was created
-    // dynamically.
-    if (current->ChildOffset() < 0) {
-      return nullptr;
-    }
-    offsets.AppendElement(current->ChildOffset());
+  nsTArray<uint32_t> path;
+  if (!aContext->GetOffsetPath(path)) {
+    return nullptr;
   }
 
   SessionStoreRestoreData* data = this;
 
-  for (uint32_t offset : Reversed(offsets)) {
+  for (uint32_t offset : Reversed(path)) {
     if (!data || data->mChildren.Length() <= offset ||
         !data->mChildren[offset] || data->mChildren[offset]->IsEmpty()) {
       return nullptr;
