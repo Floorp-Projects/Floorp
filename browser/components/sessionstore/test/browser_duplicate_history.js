004a79bae0b4277247c92f9435105a057b35afa5
Kashav Madan <kshvmdn@gmail.com>
2021-06-26 05:49:12 +0000

    Bug 1703692 - Update SessionStore and TabState{,Cache} functions to take a permanentKey, r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D118541

--- browser/components/sessionstore/test/browser_duplicate_history.js
+++ browser/components/sessionstore/test/browser_duplicate_history.js
@@ -20,14 +20,14 @@ add_task(async function() {
   ) {
     let tab = gBrowser.getTabForBrowser(aBrowser);
     await TabStateFlusher.flush(aBrowser);
-    let before = TabStateCache.get(aBrowser);
+    let before = TabStateCache.get(aBrowser.permanentKey);
 
     let newTab = SessionStore.duplicateTab(window, tab);
     await Promise.all([
       BrowserTestUtils.browserLoaded(newTab.linkedBrowser),
       TestUtils.topicObserved("sessionstore-debug-tab-restored"),
     ]);
-    let after = TabStateCache.get(newTab.linkedBrowser);
+    let after = TabStateCache.get(newTab.linkedBrowser.permanentKey);
 
     isnot(
       before.history.entries,
