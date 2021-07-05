e09f4b99eade4756a3d10fd1500b53bf1113b7ee
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2021-07-05 00:48:42 +0000

    Bug 1705976 - A test that double tapping inside OOP iframe doesn't zoom if the top level document disallows zooming. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D119042

--- gfx/layers/apz/test/mochitest/apz_test_native_event_utils.js
+++ gfx/layers/apz/test/mochitest/apz_test_native_event_utils.js
@@ -1455,15 +1455,19 @@ async function pinchZoomOutWithTouchAtCenter() {
 }
 
 // useTouchpad is only currently implemented on macOS
-async function doubleTapOn(element, x, y, useTouchpad) {
-  let transformEndPromise = promiseTransformEnd();
-
+function synthesizeDoubleTap(element, x, y, useTouchpad) {
   if (useTouchpad) {
     synthesizeNativeTouchpadDoubleTap(element, x, y);
   } else {
     synthesizeNativeTap(element, x, y);
     synthesizeNativeTap(element, x, y);
   }
+}
+// useTouchpad is only currently implemented on macOS
+async function doubleTapOn(element, x, y, useTouchpad) {
+  let transformEndPromise = promiseTransformEnd();
+
+  synthesizeDoubleTap(element, x, y, useTouchpad);
 
   // Wait for the APZ:TransformEnd to fire
   await transformEndPromise;
