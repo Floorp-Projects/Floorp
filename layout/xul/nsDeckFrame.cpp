ba5ded4486e838915c05a67c4af093581d931969
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2021-07-06 08:58:56 +0000

    Bug 1700245 - Use IsAncestorFrameCrossDocInProcess in PresShell::ClearMouseCapture(nsIFrame*). r=tnikkel
    Since bug 1717726 we've disallowed having remote <browser> or remote <iframe>
    inside nsDeckFrame which means `captureingFrame` in ClearMouseCapture(nsIFrame*)
    function will never be called in an out-of-process thus we can use
    IsAncestorFrameCrossDocInProcess there.
    
    Also add an assertions that the function gets called only with a child frame of
    nsDeckFrame since this function is used only inside nsDeckFrame.
    
    Depends on D119068
    
    Differential Revision: https://phabricator.services.mozilla.com/D119069

--- layout/xul/nsDeckFrame.cpp
+++ layout/xul/nsDeckFrame.cpp
@@ -77,7 +77,7 @@ void nsDeckFrame::Init(nsIContent* aContent, nsContainerFrame* aParent,
 void nsDeckFrame::ShowBox(nsIFrame* aBox) { Animate(aBox, true); }
 
 void nsDeckFrame::HideBox(nsIFrame* aBox) {
-  mozilla::PresShell::ClearMouseCapture(aBox);
+  PresShell::ClearMouseCapture(aBox);
   Animate(aBox, false);
 }
 
