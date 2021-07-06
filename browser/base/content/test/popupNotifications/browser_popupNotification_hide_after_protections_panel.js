5abe51e9908ff503e4e393dbd477035da075e5b3
Molly Howell <mhowell@mozilla.com>
2021-07-06 15:37:44 +0000

    Bug 1703608 - Hide doorhangers while protections panel is open. r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D117693

---
+++ browser/base/content/test/popupNotifications/browser_popupNotification_hide_after_protections_panel.js
@@ -0,0 +1,44 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+add_task(async function test_hide_popup_with_protections_panel_showing() {
+  await BrowserTestUtils.withNewTab(
+    "https://test1.example.com/",
+    async function(browser) {
+      // Request location permissions and wait for that prompt to appear.
+      let popupShownPromise = waitForNotificationPanel();
+      await SpecialPowers.spawn(browser, [], async function() {
+        content.navigator.geolocation.getCurrentPosition(() => {});
+      });
+      await popupShownPromise;
+
+      // Click on the icon for the protections panel, to show the panel.
+      popupShownPromise = BrowserTestUtils.waitForEvent(
+        window,
+        "popupshown",
+        true,
+        event => event.target == gProtectionsHandler._protectionsPopup
+      );
+      EventUtils.synthesizeMouseAtCenter(
+        document.getElementById("tracking-protection-icon-container"),
+        {}
+      );
+      await popupShownPromise;
+
+      // Make sure the location permission prompt closed.
+      Assert.ok(!PopupNotifications.isPanelOpen, "Geolocation popup is hidden");
+
+      // Close the protections panel.
+      let popupHidden = BrowserTestUtils.waitForEvent(
+        gProtectionsHandler._protectionsPopup,
+        "popuphidden"
+      );
+      gProtectionsHandler._protectionsPopup.hidePopup();
+      await popupHidden;
+
+      // Make sure the location permission prompt came back.
+      Assert.ok(PopupNotifications.isPanelOpen, "Geolocation popup is showing");
+    }
+  );
+});
