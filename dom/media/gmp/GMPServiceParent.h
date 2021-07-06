d38447f8c9c3563ded60780177ae90daa82f9478
Butkovits Atila <abutkovits@mozilla.com>
2021-07-06 21:40:33 +0300

    Backed out changeset 0bf78f953aac (bug 1705034) for causing Gtest failures.

--- dom/media/gmp/GMPServiceParent.h
+++ dom/media/gmp/GMPServiceParent.h
@@ -12,7 +12,6 @@
 #include "nsClassHashtable.h"
 #include "nsTHashMap.h"
 #include "mozilla/Atomics.h"
-#include "nsNetUtil.h"
 #include "nsIAsyncShutdown.h"
 #include "nsRefPtrHashtable.h"
 #include "nsThreadUtils.h"
@@ -64,8 +63,6 @@ class GeckoMediaPluginServiceParent final
   nsresult ForgetThisSiteNative(
       const nsAString& aSite, const mozilla::OriginAttributesPattern& aPattern);
 
-  nsresult ForgetThisBaseDomainNative(const nsAString& aBaseDomain);
-
   // Notifies that some user of this class is created/destroyed.
   void ServiceUserCreated(GMPServiceParent* aServiceParent);
   void ServiceUserDestroyed(GMPServiceParent* aServiceParent);
@@ -105,9 +102,8 @@ class GeckoMediaPluginServiceParent final
   void ClearNodeIdAndPlugin(nsIFile* aPluginStorageDir,
                             DirectoryFilter& aFilter);
   void ForgetThisSiteOnGMPThread(
-      const nsACString& aSite,
+      const nsACString& aOrigin,
       const mozilla::OriginAttributesPattern& aPattern);
-  void ForgetThisBaseDomainOnGMPThread(const nsACString& aBaseDomain);
   void ClearRecentHistoryOnGMPThread(PRTime aSince);
 
   already_AddRefed<GMPParent> GetById(uint32_t aPluginId);
@@ -212,12 +208,9 @@ class GeckoMediaPluginServiceParent final
   nsTArray<GMPServiceParent*> mServiceParents;
 };
 
-nsresult WriteToFile(nsIFile* aPath, const nsCString& aFileName,
-                     const nsCString& aData);
 nsresult ReadSalt(nsIFile* aPath, nsACString& aOutData);
 bool MatchOrigin(nsIFile* aPath, const nsACString& aSite,
                  const mozilla::OriginAttributesPattern& aPattern);
-bool MatchBaseDomain(nsIFile* aPath, const nsACString& aBaseDomain);
 
 class GMPServiceParent final : public PGMPServiceParent {
  public:
