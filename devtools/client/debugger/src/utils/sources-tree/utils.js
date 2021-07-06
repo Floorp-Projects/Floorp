d35f13fd80fe151b2b57e42b56e3292f02ecf5ab
Hubert Boma Manilla <hmanilla@mozilla.com>
2021-07-06 14:07:33 +0000

    Bug 1577878 - [devtools] Fix and test project search for remote frames r=ochameau
    Added fission test for project-search with remote frames
    Fixed getRelativePath util to show `(index)` when no path is provided e.g http://foo.com/.
    
    Moved the test for the getRelativePath from node tests to xpcshell test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118324

--- devtools/client/debugger/src/utils/sources-tree/utils.js
+++ devtools/client/debugger/src/utils/sources-tree/utils.js
@@ -176,14 +176,29 @@ export function createParentMap(tree) {
   return map;
 }
 
+/**
+ * Get the relative path of the url
+ * Does not include any query parameters or fragment parts
+ *
+ * @param string url
+ * @returns string path
+ */
 export function getRelativePath(url) {
   const { pathname } = parse(url);
   if (!pathname) {
     return url;
   }
   const index = pathname.indexOf("/");
-
-  return index !== -1 ? pathname.slice(index + 1) : "";
+  if (index !== -1) {
+    const path = pathname.slice(index + 1);
+    // If the path is empty this is likely the index file.
+    // e.g http://foo.com/
+    if (path == "") {
+      return "(index)";
+    }
+    return path;
+  }
+  return "";
 }
 
 export function getPathWithoutThread(path) {
