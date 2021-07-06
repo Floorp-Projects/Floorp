1d29ad9e89426c7133b977150209f7944d7e86a9
Gavin Lazar Suntop <gavin@gsuntop.com>
2021-07-06 21:03:38 +0000

    Bug 1710950 - fixing arrow keys r=thecount
    Differential Revision: https://phabricator.services.mozilla.com/D117684

--- browser/components/pocket/content/panels/js/saved.js
+++ browser/components/pocket/content/panels/js/saved.js
@@ -703,10 +703,10 @@ PKT_PANEL_OVERLAY.prototype = {
     document
       .querySelector(`body`)
       .append(
-        parser.parseFromString(
+        ...parser.parseFromString(
           Handlebars.templates.saved_shell(templateData),
           `text/html`
-        ).documentElement
+        ).body.childNodes
       );
 
     // Add in premium content (if applicable based on premium status)
@@ -716,12 +716,17 @@ PKT_PANEL_OVERLAY.prototype = {
     ) {
       let elSubshell = document.querySelector(`body .pkt_ext_subshell`);
 
-      let elPremiumShell = parser.parseFromString(
+      let elPremiumShellElements = parser.parseFromString(
         Handlebars.templates.saved_premiumshell(templateData),
         `text/html`
-      ).documentElement;
+      ).body.childNodes;
 
-      elSubshell.insertBefore(elPremiumShell, elSubshell.firstChild);
+      // Convert NodeList to Array and reverse it
+      elPremiumShellElements = [].slice.call(elPremiumShellElements).reverse();
+
+      elPremiumShellElements.forEach(el => {
+        elSubshell.insertBefore(el, elSubshell.firstChild);
+      });
     }
 
     // Initialize functionality for overlay
