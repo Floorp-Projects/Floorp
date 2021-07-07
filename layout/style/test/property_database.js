2f8c404c9c074ec8d4beecc5276266630defe159
Narcis Beleuzu <nbeleuzu@mozilla.com>
2021-07-08 00:37:51 +0300

    Backed out changeset 66a484400799 (bug 1718934) for mochitest failures on test_value_computation.html . CLOSED TREE

--- layout/style/test/property_database.js
+++ layout/style/test/property_database.js
@@ -5211,8 +5211,6 @@ var gCSSProperties = {
       "-moz-default-background-color",
       "-moz-hyperlinktext",
       "-moz-visitedhyperlinktext",
-      /* Needed for compat */
-      "-webkit-focus-ring-color",
     ],
     invalid_values: [
       "#f",
