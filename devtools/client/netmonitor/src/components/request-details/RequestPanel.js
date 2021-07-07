8185ca2c9dc5373f862a932eb099e3c4b4100d60
Sebastian Zartner <sebastianzartner@gmail.com>
2021-07-07 21:14:21 +0000

    Bug 1713301 - Fixed display of raw request or reponse data when clicking on search results. r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D116955

--- devtools/client/netmonitor/src/components/request-details/RequestPanel.js
+++ devtools/client/netmonitor/src/components/request-details/RequestPanel.js
@@ -74,7 +74,7 @@ class RequestPanel extends Component {
     super(props);
     this.state = {
       filterText: "",
-      rawRequestPayloadDisplayed: false,
+      rawRequestPayloadDisplayed: !!props.targetSearchResult,
     };
 
     this.toggleRawRequestPayload = this.toggleRawRequestPayload.bind(this);
@@ -97,6 +97,12 @@ class RequestPanel extends Component {
       "requestPostData",
     ]);
     updateFormDataSections(nextProps);
+
+    if (nextProps.targetSearchResult !== null) {
+      this.setState({
+        rawRequestPayloadDisplayed: !!nextProps.targetSearchResult,
+      });
+    }
   }
 
   /**
@@ -112,8 +118,7 @@ class RequestPanel extends Component {
       this.state.filterText !== nextState.filterText ||
       this.state.rawRequestPayloadDisplayed !==
         nextState.rawRequestPayloadDisplayed ||
-      (this.props.targetSearchResult !== nextProps.targetSearchResult &&
-        nextProps.targetSearchResult !== null)
+      this.props.targetSearchResult !== nextProps.targetSearchResult
     );
   }
 
