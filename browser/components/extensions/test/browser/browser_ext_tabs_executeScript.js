8fd4234a999a7a6d9e0e04acf364b3c10fd9c9da
Narcis Beleuzu <nbeleuzu@mozilla.com>
2021-07-07 23:15:46 +0300

    Backed out changeset 77f16565a279 (bug 1642444) for WP failures on iframe-cross-origin-print.sub.html CLOSED TREE

--- browser/components/extensions/test/browser/browser_ext_tabs_executeScript.js
+++ browser/components/extensions/test/browser/browser_ext_tabs_executeScript.js
@@ -30,21 +30,6 @@ add_task(async function testExecuteScript() {
 
   async function background() {
     try {
-      // This promise is meant to be resolved when browser.tabs.executeScript({file: "script.js"})
-      // is called and the content script does message back, registering the runtime.onMessage
-      // listener here is meant to prevent intermittent failures due to a race on executing the
-      // array of promises passed to the `await Promise.all(...)` below.
-      const promiseRuntimeOnMessage = new Promise(resolve => {
-        browser.runtime.onMessage.addListener(message => {
-          browser.test.assertEq(
-            "script ran",
-            message,
-            "Expected runtime message"
-          );
-          resolve();
-        });
-      });
-
       let [tab] = await browser.tabs.query({
         active: true,
         currentWindow: true,
@@ -401,7 +386,16 @@ add_task(async function testExecuteScript() {
           await browser.tabs.remove(tab.id);
         }),
 
-        promiseRuntimeOnMessage,
+        new Promise(resolve => {
+          browser.runtime.onMessage.addListener(message => {
+            browser.test.assertEq(
+              "script ran",
+              message,
+              "Expected runtime message"
+            );
+            resolve();
+          });
+        }),
       ]);
 
       browser.test.notifyPass("executeScript");
