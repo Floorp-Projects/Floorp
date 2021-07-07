8fac017c47fdc4d046399eacf4c36e03ed7caa44
mcarare <mihai.carare.dev@gmail.com>
2021-07-07 16:15:53 +0300

    [components] For https://github.com/mozilla-mobile/android-components/issues/9554: Use WindowInsetsControllerCompat to avoid null controllers.

--- mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/view/Window.kt
+++ mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/view/Window.kt
@@ -8,7 +8,6 @@ import android.os.Build
 import android.os.Build.VERSION.SDK_INT
 import android.view.Window
 import androidx.annotation.ColorInt
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import mozilla.components.support.utils.ColorUtils.isDark
 
@@ -17,7 +16,7 @@ import mozilla.components.support.utils.ColorUtils.isDark
  * If the color is light enough, a light status bar with dark icons will be used.
  */
 fun Window.setStatusBarTheme(@ColorInt color: Int) {
-    getWindowInsetsController()?.isAppearanceLightStatusBars =
+    getWindowInsetsController().isAppearanceLightStatusBars =
         isDark(color)
     statusBarColor = color
 }
@@ -27,7 +26,7 @@ fun Window.setStatusBarTheme(@ColorInt color: Int) {
  * If the color is light enough, a light navigation bar with dark icons will be used.
  */
 fun Window.setNavigationBarTheme(@ColorInt color: Int) {
-    getWindowInsetsController()?.isAppearanceLightNavigationBars =
+    getWindowInsetsController().isAppearanceLightNavigationBars =
         isDark(color)
 
     if (SDK_INT >= Build.VERSION_CODES.P) {
@@ -39,6 +38,6 @@ fun Window.setNavigationBarTheme(@ColorInt color: Int) {
 /**
  * Retrieves a {@link WindowInsetsControllerCompat} for the top-level window decor view.
  */
-fun Window.getWindowInsetsController(): WindowInsetsControllerCompat? {
-    return ViewCompat.getWindowInsetsController(this.decorView)
+fun Window.getWindowInsetsController(): WindowInsetsControllerCompat {
+    return WindowInsetsControllerCompat(this, this.decorView)
 }
