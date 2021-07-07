d4e847332bea0d13cdfa5fdb6d2a0e85472ccc69
Chris H-C <chutten@mozilla.com>
2021-07-07 14:39:41 +0000

    Bug 1713843 - Don't register prefs listeners in about:telemetry r=janerik
    unload listeners aren't reliable at the best of time, and for about: pages they
    seem to never fire in my testing. There is no weakref-based listener setup for
    prefs in JS and I don't want to build one, so this makes the prefs display in
    about:telemetry only update on render, not live.
    
    Differential Revision: https://phabricator.services.mozilla.com/D119285

--- toolkit/content/aboutTelemetry.js
+++ toolkit/content/aboutTelemetry.js
@@ -175,25 +175,7 @@ function removeAllChildNodes(node) {
 }
 
 var Settings = {
-  SETTINGS: [
-    // data upload
-    {
-      pref: PREF_FHR_UPLOAD_ENABLED,
-      defaultPrefValue: false,
-    },
-    // extended "Telemetry" recording
-    {
-      pref: PREF_TELEMETRY_ENABLED,
-      defaultPrefValue: false,
-    },
-  ],
-
   attachObservers() {
-    for (let s of this.SETTINGS) {
-      let setting = s;
-      Preferences.observe(setting.pref, this.render, this);
-    }
-
     let elements = document.getElementsByClassName("change-data-choices-link");
     for (let el of elements) {
       el.parentElement.addEventListener("click", function(event) {
@@ -213,12 +195,6 @@ var Settings = {
           }
         }
       });
-    }
-  },
-
-  detachObservers() {
-    for (let setting of this.SETTINGS) {
-      Preferences.ignore(setting.pref, this.render, this);
     }
   },
 
@@ -2296,15 +2272,6 @@ function setupListeners() {
 
   let search = document.getElementById("search");
   search.addEventListener("input", Search.searchHandler);
-
-  // Clean up observers when page is closed
-  window.addEventListener(
-    "unload",
-    function(aEvent) {
-      Settings.detachObservers();
-    },
-    { once: true }
-  );
 
   document
     .getElementById("captured-stacks-fetch-symbols")
