b4f317ed6c3d7b72d089128533ffd9b91fa02eb4
Thomas Wisniewski <twisniewski@mozilla.com>
2021-07-08 15:08:27 +0000

    Bug 1713690 - Improve the SmartBlock shimming for Google IMA3; r=denschub,webcompat-reviewers
    - adds an actual shim for Google IMA3, rather than shimming with an empty file
    - simplifies the AdSafeProtected IMA shim:
      - no longer needs to opt in to the original script for videos to play
      - it can now just be a basic stub shim for the API
    
    Differential Revision: https://phabricator.services.mozilla.com/D119337

--- browser/extensions/webcompat/shims/adsafeprotected-ima.js
+++ browser/extensions/webcompat/shims/adsafeprotected-ima.js
@@ -5,71 +5,15 @@
 "use strict";
 
 /**
- * Bug 1508639 - Shim Ad Safe Protected's Google IMA adapter
+ *
+ * Sites relying on Ad Safe Protected's adapter for Google IMA may
+ * have broken videos when the script is blocked. This shim stubs
+ * out the API to help mitigate major breakage.
  */
 
 if (!window.googleImaVansAdapter) {
-  const shimId = "AdSafeProtectedGoogleIMAAdapter";
-
-  const sendMessageToAddon = (function() {
-    const pendingMessages = new Map();
-    const channel = new MessageChannel();
-    channel.port1.onerror = console.error;
-    channel.port1.onmessage = event => {
-      const { messageId, response } = event.data;
-      const resolve = pendingMessages.get(messageId);
-      if (resolve) {
-        pendingMessages.delete(messageId);
-        resolve(response);
-      }
-    };
-    function reconnect() {
-      const detail = {
-        pendingMessages: [...pendingMessages.values()],
-        port: channel.port2,
-        shimId,
-      };
-      window.dispatchEvent(new CustomEvent("ShimConnects", { detail }));
-    }
-    window.addEventListener("ShimHelperReady", reconnect);
-    reconnect();
-    return function(message) {
-      const messageId =
-        Math.random()
-          .toString(36)
-          .substring(2) + Date.now().toString(36);
-      return new Promise(resolve => {
-        const payload = {
-          message,
-          messageId,
-          shimId,
-        };
-        pendingMessages.set(messageId, resolve);
-        channel.port1.postMessage(payload);
-      });
-    };
-  })();
-
   window.googleImaVansAdapter = {
-    init: () => {},
-    dispose: () => {},
+    init() {},
+    dispose() {},
   };
-
-  if (document.domain === "www.nhl.com") {
-    // Treat it as an opt-in when the user clicks on a video
-    // TODO: Improve this! It races to tell the bg script to unblock the ad from
-    // https://pubads.g.doubleclick.net/gampad/ads before the page loads them.
-    async function click(e) {
-      if (
-        e.isTrusted &&
-        e.target.closest(
-          "#video-player, .video-preview, article:not([data-video-url=''])"
-        )
-      ) {
-        document.documentElement.removeEventListener("click", click, true);
-        await sendMessageToAddon("optIn");
-      }
-    }
-    document.documentElement.addEventListener("click", click, true);
-  }
 }
