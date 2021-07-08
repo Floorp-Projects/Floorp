2b6016cc06e00b5a38cea9dbfe28d9ccc6fbfa9e
Ting-Yu Lin <tlin@mozilla.com>
2021-07-08 17:58:43 +0000

    Bug 1719555 Part 4 - Remove unused code subtracting block and padding from block-size for nsComboboxDisplayFrame. r=emilio
    This patch removed the unused code which introduced by Bug 1571764
    because Bug 1560824 moves the `padding-block` style to <select>'s UA
    rule, so now `nsComboboxDisplayFrame` doesn't have any padding in the
    block axis.
    
    Differential Revision: https://phabricator.services.mozilla.com/D119342

--- layout/forms/nsComboboxControlFrame.cpp
+++ layout/forms/nsComboboxControlFrame.cpp
@@ -1251,8 +1251,6 @@ void nsComboboxDisplayFrame::Reflow(nsPresContext* aPresContext,
   MOZ_ASSERT(aStatus.IsEmpty(), "Caller should pass a fresh reflow status!");
 
   ReflowInput state(aReflowInput);
-  WritingMode wm = aReflowInput.GetWritingMode();
-  LogicalMargin bp = state.ComputedLogicalBorderPadding(wm);
   if (state.ComputedBSize() == NS_UNCONSTRAINEDSIZE) {
     float inflation = nsLayoutUtils::FontSizeInflationFor(mComboBox);
     // We intentionally use the combobox frame's style here, which has
@@ -1262,15 +1260,12 @@ void nsComboboxDisplayFrame::Reflow(nsPresContext* aPresContext,
     nscoord lh = ReflowInput::CalcLineHeight(mComboBox->GetContent(),
                                              mComboBox->Style(), aPresContext,
                                              NS_UNCONSTRAINEDSIZE, inflation);
-    if (!mComboBox->StyleText()->mLineHeight.IsNormal()) {
-      // If the author specified a different line-height than normal, or a
-      // different appearance, subtract the border-padding from the
-      // comboboxdisplay frame, so as to respect that line-height rather than
-      // that line-height + 2px (from the UA sheet).
-      lh = std::max(0, lh - bp.BStartEnd(wm));
-    }
     state.SetComputedBSize(lh);
   }
+  const WritingMode wm = aReflowInput.GetWritingMode();
+  const LogicalMargin bp = state.ComputedLogicalBorderPadding(wm);
+  MOZ_ASSERT(bp.BStartEnd(wm) == 0,
+             "We shouldn't have border and padding in the block axis in UA!");
   nscoord inlineBp = bp.IStartEnd(wm);
   nscoord computedISize = mComboBox->mDisplayISize - inlineBp;
 
