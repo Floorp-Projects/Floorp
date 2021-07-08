8ee6c18a777640a59d3fab18b65a66af2ae967ce
Jamie Nicol <jnicol@mozilla.com>
2021-07-08 09:01:28 +0000

    Bug 1719367 - Hook up default clear color to RenderCompositorLayersSWGL. r=sotaro
    Fenix makes use of the default clear color by setting it to a dark
    color when the dark theme is enabled to avoid white flashes while
    pages are loading. In bug 1663732 we hooked this functionality up to
    webrender, however, it was only used by the draw compositor. Now with
    software webrender on Android, we sometimes use
    RenderCompositorLayersSWGL rather than the draw compositor, and users
    are therefore experiencing white flashes during page loads.
    
    This adds a clear_color argument to StartCompositing(), which
    RenderCompositorLayersSWGL uses to set the CompositorOGL's clear
    color. This patch also makes Renderer::clear_color not an Option
    type, as we only ever used it in Some form, and that simplifies the
    FFI.
    
    Differential Revision: https://phabricator.services.mozilla.com/D119272

--- gfx/webrender_bindings/RenderCompositor.cpp
+++ gfx/webrender_bindings/RenderCompositor.cpp
@@ -95,14 +95,14 @@ void wr_compositor_attach_external_image(void* aCompositor,
   compositor->AttachExternalImage(aId, aExternalImage);
 }
 
-void wr_compositor_start_compositing(void* aCompositor,
+void wr_compositor_start_compositing(void* aCompositor, wr::ColorF aClearColor,
                                      const wr::DeviceIntRect* aDirtyRects,
                                      size_t aNumDirtyRects,
                                      const wr::DeviceIntRect* aOpaqueRects,
                                      size_t aNumOpaqueRects) {
   RenderCompositor* compositor = static_cast<RenderCompositor*>(aCompositor);
-  compositor->StartCompositing(aDirtyRects, aNumDirtyRects, aOpaqueRects,
-                               aNumOpaqueRects);
+  compositor->StartCompositing(aClearColor, aDirtyRects, aNumDirtyRects,
+                               aOpaqueRects, aNumOpaqueRects);
 }
 
 void wr_compositor_end_frame(void* aCompositor) {
