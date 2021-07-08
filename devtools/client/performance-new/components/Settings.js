1d0551a9419cda7c1da2a5d3d873ede23141d4ea
Jon Coppeard <jcoppeard@mozilla.com>
2021-07-08 16:37:07 +0000

    Bug 1719661 - Replace JS helper thread option with TaskController thread option in about:profiling r=julienw,fluent-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D119394

--- devtools/client/performance-new/components/Settings.js
+++ devtools/client/performance-new/components/Settings.js
@@ -162,9 +162,10 @@ const threadColumns = [
       l10nId: "perftools-thread-dns-resolver",
     },
     {
-      name: "JS Helper",
-      id: "js-helper",
-      l10nId: "perftools-thread-js-helper",
+      // Threads that are part of XPCOM's TaskController thread pool.
+      name: "TaskController",
+      id: "task-controller",
+      l10nId: "perftools-thread-task-controller",
     },
   ],
 ];
