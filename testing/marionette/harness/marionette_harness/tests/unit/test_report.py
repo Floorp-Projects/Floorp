99e0642d4e0831a7449c8563a0189c4d84084d5c
Henrik Skupin <mail@hskupin.info>
2021-07-09 07:34:59 +0000

    Bug 1641226 - [marionette] Fix expectedFailure decorator usage. r=webdriver-reviewers,jdescottes
    As long as we make use of our own test status handling
    we cannot make use of the unexpectedError and
    unexpectedSuccess decorators from unittest.case.
    
    This patch just makes it possible to use both decorators
    again now that Marionette only runs with Python 3. Any
    other refactoring needs to happen at a later time.
    
    Differential Revision: https://phabricator.services.mozilla.com/D119319

--- testing/marionette/harness/marionette_harness/tests/unit/test_report.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_report.py
@@ -4,27 +4,26 @@
 
 from __future__ import absolute_import
 
-from marionette_harness import MarionetteTestCase, expectedFailure, skip
+from marionette_harness import (
+    expectedFailure,
+    MarionetteTestCase,
+    skip,
+    unexpectedSuccess,
+)
 
 
 class TestReport(MarionetteTestCase):
     def test_pass(self):
         assert True
 
-    def test_fail(self):
-        assert False
-
     @skip("Skip Message")
     def test_skip(self):
         assert False
 
     @expectedFailure
-    def test_expected_fail(self):
-        assert False
-
-    @expectedFailure
-    def test_unexpected_pass(self):
-        assert True
-
     def test_error(self):
         raise Exception()
+
+    @unexpectedSuccess
+    def test_unexpected_pass(self):
+        assert True
