3b5816e7f217fe49f6bca8feca411b22ad3525ce
Noemi Erli <nerli@mozilla.com>
2021-07-12 17:38:14 +0300

    Backed out 3 changesets (bug 970675) for causing failures in browser_privatebrowsing_popupblocker.js CLOSED TREE
    Backed out changeset 98f78b0795b7 (bug 970675)
    Backed out changeset ec59b5b4094a (bug 970675)
    Backed out changeset 5a1853ea9693 (bug 970675)

--- browser/components/preferences/dialogs/permissions.js
+++ browser/components/preferences/dialogs/permissions.js
@@ -223,15 +223,6 @@ var gPermissionManager = {
       return;
     }
 
-    // Skip private browsing session permissions.
-    if (
-      perm.principal.privateBrowsingId !==
-        Services.scriptSecurityManager.DEFAULT_PRIVATE_BROWSING_ID &&
-      perm.expireType === Services.perms.EXPIRE_SESSION
-    ) {
-      return;
-    }
-
     let p = new Permission(perm.principal, perm.type, perm.capability);
     this._permissions.set(p.origin, p);
   },
