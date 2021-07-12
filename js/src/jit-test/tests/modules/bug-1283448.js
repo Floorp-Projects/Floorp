2123ff214c819f913b38cb187c78244cdec4382e
Tooru Fujisawa <arai_a@mac.com>
2021-07-12 16:02:21 +0000

    Bug 1719459 - Part 2: Add shell ModuleObject wrapper to avoid unexpected operation. r=jonco a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D119566

--- js/src/jit-test/tests/modules/bug-1283448.js
+++ js/src/jit-test/tests/modules/bug-1283448.js
@@ -1,5 +1,3 @@
-// |jit-test| error: TypeError
-
 let a = registerModule('a', parseModule("var x = 1; export { x };"));
 let b = registerModule('b', parseModule("import { x as y } from 'a';"));
 a.__proto__ = {15: 1337};
