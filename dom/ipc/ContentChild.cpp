e3be4419305eb1fcc8c19067762b90c6a04a0f71
Jan Varga <jvarga@mozilla.com>
2021-07-13 11:16:00 +0000

    Bug 1591877 - Share the same cached pref value of dom.storage.next_gen across all relevant processes; r=dom-storage-reviewers,jstutte, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D119711

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -4330,6 +4330,13 @@ mozilla::ipc::IPCResult ContentChild::RecvDecoderSupportedMimeTypes(
   return IPC_OK();
 }
 
+mozilla::ipc::IPCResult ContentChild::RecvInitNextGenLocalStorageEnabled(
+    const bool& aEnabled) {
+  mozilla::dom::RecvInitNextGenLocalStorageEnabled(aEnabled);
+
+  return IPC_OK();
+}
+
 /* static */ void ContentChild::DispatchBeforeUnloadToSubtree(
     BrowsingContext* aStartingAt,
     const DispatchBeforeUnloadToSubtreeResolver& aResolver) {
