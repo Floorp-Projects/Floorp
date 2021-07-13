d280e414115487b1297b82768b6d1288a4763fff
Jan de Mooij <jdemooij@mozilla.com>
2021-07-13 09:31:33 +0000

    Bug 1719795 part 2 - Use SandboxPrivate::GetPrivate more. r=mccr8
    Depends on D119498
    
    Differential Revision: https://phabricator.services.mozilla.com/D119499

--- js/xpconnect/src/Sandbox.cpp
+++ js/xpconnect/src/Sandbox.cpp
@@ -435,29 +435,27 @@ static bool SandboxCloneInto(JSContext* cx, unsigned argc, Value* vp) {
 }
 
 static void sandbox_finalize(JSFreeOp* fop, JSObject* obj) {
-  nsIScriptObjectPrincipal* sop =
-      static_cast<nsIScriptObjectPrincipal*>(xpc_GetJSPrivate(obj));
-  if (!sop) {
-    // sop can be null if CreateSandboxObject fails in the middle.
+  SandboxPrivate* priv = SandboxPrivate::GetPrivate(obj);
+  if (!priv) {
+    // priv can be null if CreateSandboxObject fails in the middle.
     return;
   }
 
-  static_cast<SandboxPrivate*>(sop)->ForgetGlobalObject(obj);
+  priv->ForgetGlobalObject(obj);
   DestroyProtoAndIfaceCache(obj);
-  DeferredFinalize(sop);
+  DeferredFinalize(static_cast<nsIScriptObjectPrincipal*>(priv));
 }
 
 static size_t sandbox_moved(JSObject* obj, JSObject* old) {
   // Note that this hook can be called before the private pointer is set. In
   // this case the SandboxPrivate will not exist yet, so there is nothing to
   // do.
-  nsIScriptObjectPrincipal* sop =
-      static_cast<nsIScriptObjectPrincipal*>(xpc_GetJSPrivate(obj));
-  if (!sop) {
+  SandboxPrivate* priv = SandboxPrivate::GetPrivate(obj);
+  if (!priv) {
     return 0;
   }
 
-  return static_cast<SandboxPrivate*>(sop)->ObjectMoved(obj, old);
+  return priv->ObjectMoved(obj, old);
 }
 
 #define XPCONNECT_SANDBOX_CLASS_METADATA_SLOT \
@@ -1983,10 +1981,9 @@ nsresult xpc::EvalInSandbox(JSContext* cx, HandleObject sandboxArg,
     return NS_ERROR_INVALID_ARG;
   }
 
-  nsIScriptObjectPrincipal* sop =
-      static_cast<nsIScriptObjectPrincipal*>(xpc_GetJSPrivate(sandbox));
+  SandboxPrivate* priv = SandboxPrivate::GetPrivate(sandbox);
+  nsIScriptObjectPrincipal* sop = priv;
   MOZ_ASSERT(sop, "Invalid sandbox passed");
-  SandboxPrivate* priv = static_cast<SandboxPrivate*>(sop);
   nsCOMPtr<nsIPrincipal> prin = sop->GetPrincipal();
   NS_ENSURE_TRUE(prin, NS_ERROR_FAILURE);
 
