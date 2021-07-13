4146a3bdbe22d1f73197fcfbb53e0096070aadb0
Jed Davis <jld@mozilla.com>
2021-07-13 15:40:59 +0000

    Bug 1720072 - Allow the XRes perf metric to tolerate content processes not using X11. r=perftest-reviewers,AlexandruIonescu  a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D119661

--- testing/talos/talos/cmanager_linux.py
+++ testing/talos/talos/cmanager_linux.py
@@ -138,8 +138,8 @@ def GetXRes(pids):
             except ValueError:
                 print("Invalid data, not a float")
                 raise
-        else:
-            raise Exception("Could not find PID=%s in xrestop output" % pid)
+        # Content processes generally won't use X11 after bug 1635451,
+        # so failing to find the process isn't an error.
     return XRes
 
 
