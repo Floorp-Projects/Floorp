9e498248b4130323d4d52a234a35f38a0cbb9829
Tooru Fujisawa <arai_a@mac.com>
2021-07-13 11:52:41 +0000

    Bug 1708432 - Move JS_Get*PropertyDescriptor* to js/public/PropertyDescriptor.h. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D119617

--- dom/indexedDB/KeyPath.cpp
+++ dom/indexedDB/KeyPath.cpp
@@ -10,7 +10,8 @@
 #include "IndexedDBCommon.h"
 #include "Key.h"
 #include "ReportInternalError.h"
-#include "js/Array.h"  // JS::NewArrayObject
+#include "js/Array.h"               // JS::NewArrayObject
+#include "js/PropertyDescriptor.h"  // JS::PropertyDescriptor, JS_GetOwnUCPropertyDescriptor
 #include "mozilla/ResultExtensions.h"
 #include "mozilla/dom/BindingDeclarations.h"
 #include "mozilla/dom/Blob.h"
