18c35e2c9ba733f71dc856255482c5fc554112a3
Pascal Chevrel <pchevrel@mozilla.com>
2021-07-13 18:37:23 +0200

    Backed out changeset d39eb2d0e9d8 (bug 1713686) on beta for causing a regression (bug 1717351) a=backout

--- gfx/webrender_bindings/RenderCompositorSWGL.cpp
+++ gfx/webrender_bindings/RenderCompositorSWGL.cpp
@@ -9,10 +9,6 @@
 #include "mozilla/gfx/Logging.h"
 #include "mozilla/widget/CompositorWidget.h"
 
-#ifdef MOZ_WIDGET_GTK
-#  include "mozilla/WidgetUtilsGtk.h"
-#endif
-
 namespace mozilla {
 using namespace gfx;
 
@@ -269,14 +265,8 @@ void RenderCompositorSWGL::GetCompositorCapabilities(
   // Always support a single update rect for SwCompositor
   aCaps->max_update_rects = 1;
 
-  // On uncomposited desktops such as X11 without compositor or Window 7 with
-  // Aero disabled we need to force a full redraw when the window contents may
-  // be damaged.
-#ifdef MOZ_WIDGET_GTK
-  aCaps->redraw_on_invalidation = widget::GdkIsX11Display();
-#else
+  // When the window contents may be damaged, we need to force a full redraw.
   aCaps->redraw_on_invalidation = true;
-#endif
 }
 
 }  // namespace wr
