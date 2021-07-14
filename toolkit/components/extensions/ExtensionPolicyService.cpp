4e7eb3115137eafdad66926f3bfa65ec8dc4a272
Geoff Lankow <geoff@darktrojan.net>
2021-07-14 22:34:29 +0000

    Bug 1715434 - In Thunderbird, consider some extra message manager groups when injecting WebExt content scripts. r=zombie a=pascalc
    Thunderbird uses these extra groups for special handling of links, and they need to be considered the same as the browsers group for WebExtensions purposes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D119874

--- toolkit/components/extensions/ExtensionPolicyService.cpp
+++ toolkit/components/extensions/ExtensionPolicyService.cpp
@@ -283,7 +283,14 @@ RefPtr<Promise> ExtensionPolicyService::ExecuteContentScripts(
 // background & options pages, and xpcshell tests.
 static bool IsTabOrExtensionBrowser(dom::BrowsingContext* aBC) {
   const auto& group = aBC->Top()->GetMessageManagerGroup();
-  return group == u"browsers"_ns || group == u"webext-browsers"_ns;
+  bool rv = group == u"browsers"_ns || group == u"webext-browsers"_ns;
+
+#ifdef MOZ_THUNDERBIRD
+  // ...unless it's Thunderbird, which has extra groups for unrelated reasons.
+  rv = rv || group == u"single-site"_ns || group == u"single-page"_ns;
+#endif
+
+  return rv;
 }
 
 static nsTArray<RefPtr<dom::BrowsingContext>> GetAllInProcessContentBCs() {
