a171e12db68ca6f9eb875696a0c289d984e2071b
Masayuki Nakano <masayuki@d-toybox.com>
2021-07-14 01:20:19 +0000

    Bug 1665550 - part 3: Make `nsTextControlFrame` set the source node and selection of drag session to new ones when it's reframed r=smaug
    When `nsTextControlFrame` is reframed, `TextEditor`, anonymous `<div>`, its
    `Text` and the independent `Selection`s are deleted temporarily and recreated
    them.
    
    If users are dragging text in `<input>` or `<textarea>`, the drag session's
    source node is set to the anonymous text node in the element and the selection
    is set to the independent selection.  So, if the element is reframed during a
    drag, the source node is disconnected from the document and `EndDragSession`
    failed to dispatch `eDragEnd` event.
    
    Therefore, this patch makes `nsTextControlFrame` replaces the source node and
    selection when it's recreated and only when the drag session's original source
    node was in the text control element.  For checking which text control had the
    anonymous text node, this patch makes `nsTextControlFrame` replaces source
    node with the `<input>` or `<textarea>` element when the frame is destroyed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D119487

--- testing/mochitest/tests/SimpleTest/EventUtils.js
+++ testing/mochitest/tests/SimpleTest/EventUtils.js
@@ -3002,6 +3002,9 @@ function _computeSrcElementFromSrcSelection(aSrcSelection) {
  *          destWindow:   The window for dispatching event on destElement,
  *                        defaults to the current window object
  *          expectCancelDragStart:  Set to true if the test cancels "dragstart"
+ *          expectSrcElementDisconnected:
+ *                        Set to true if srcElement will be disconnected and
+ *                        "dragend" event won't be fired.
  *          logFunc:      Set function which takes one argument if you need
  *                        to log rect of target.  E.g., `console.log`.
  *        }
@@ -3023,6 +3026,7 @@ async function synthesizePlainDragAndDrop(aParams) {
     srcWindow = window,
     destWindow = window,
     expectCancelDragStart = false,
+    expectSrcElementDisconnected = false,
     logFunc,
   } = aParams;
   // Don't modify given dragEvent object because we modify dragEvent below and
@@ -3341,6 +3345,7 @@ async function synthesizePlainDragAndDrop(aParams) {
     await new Promise(r => setTimeout(r, 0));
 
     if (ds.getCurrentSession()) {
+      const sourceNode = ds.sourceNode;
       let dragEndEvent;
       function onDragEnd(aEvent) {
         dragEndEvent = aEvent;
@@ -3356,14 +3361,25 @@ async function synthesizePlainDragAndDrop(aParams) {
             'event target of "dragend" is not srcElement nor its descendant'
           );
         }
+        if (expectSrcElementDisconnected) {
+          throw new Error(
+            `"dragend" event shouldn't be fired when the source node is disconnected (the source node is ${
+              sourceNode?.isConnected ? "connected" : "null or disconnected"
+            })`
+          );
+        }
       }
       srcWindow.addEventListener("dragend", onDragEnd, { capture: true });
       try {
         ds.endDragSession(true, _parseModifiers(dragEvent));
-        if (!dragEndEvent) {
+        if (!expectSrcElementDisconnected && !dragEndEvent) {
           // eslint-disable-next-line no-unsafe-finally
           throw new Error(
-            '"dragend" event is not fired by nsIDragService.endDragSession()'
+            `"dragend" event is not fired by nsIDragService.endDragSession()${
+              ds.sourceNode && !ds.sourceNode.isConnected
+                ? "(sourceNode was disconnected)"
+                : ""
+            }`
           );
         }
       } finally {
