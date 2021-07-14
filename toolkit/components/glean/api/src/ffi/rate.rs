5983db45bf4d9747996cafd85055e5e7886a71ec
Chris H-C <chutten@mozilla.com>
2021-07-14 16:13:48 +0000

    Bug 1694496 MLA FFI for `rate` metric type r=janerik
    Differential Revision: https://phabricator.services.mozilla.com/D119786

---
+++ toolkit/components/glean/api/src/ffi/rate.rs
@@ -0,0 +1,44 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#![cfg(feature = "with_gecko")]
+
+use nsstring::nsACString;
+
+#[no_mangle]
+pub unsafe extern "C" fn fog_rate_add_to_numerator(id: u32, amount: i32) {
+    with_metric!(RATE_MAP, id, metric, metric.add_to_numerator(amount));
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn fog_rate_add_to_denominator(id: u32, amount: i32) {
+    with_metric!(RATE_MAP, id, metric, metric.add_to_denominator(amount));
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn fog_rate_test_has_value(id: u32, ping_name: &nsACString) -> bool {
+    with_metric!(RATE_MAP, id, metric, test_has!(metric, ping_name))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn fog_rate_test_get_value(
+    id: u32,
+    ping_name: &nsACString,
+    num: &mut i32,
+    den: &mut i32,
+) {
+    let (n, d) = with_metric!(RATE_MAP, id, metric, test_get!(metric, ping_name));
+    *num = n;
+    *den = d;
+}
+
+#[no_mangle]
+pub extern "C" fn fog_rate_test_get_error(
+    id: u32,
+    ping_name: &nsACString,
+    error_str: &mut nsACString,
+) -> bool {
+    let err = with_metric!(RATE_MAP, id, metric, test_get_errors!(metric, ping_name));
+    err.map(|err_str| error_str.assign(&err_str)).is_some()
+}
