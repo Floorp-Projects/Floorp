ce7a9eeed41995705b025fa3b0afcc695dd42f70
Cosmin Sabou <csabou@mozilla.com>
2021-07-16 02:49:45 +0300

    Backed out changeset 2c1257a95130 (bug 1720591) for causing build bustages complaining about patch_main. CLOSED TREE

--- build/mach_bootstrap.py
+++ build/mach_bootstrap.py
@@ -247,6 +247,10 @@ def bootstrap(topsrcdir):
         # The resource module is UNIX only.
         pass
 
+    from mozbuild.util import patch_main
+
+    patch_main()
+
     def resolve_repository():
         import mozversioncontrol
 
