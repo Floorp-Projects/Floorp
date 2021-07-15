292c1a1f604effae4af3040b34bfa50af4d0c0b7
Florian Quèze <florian@queze.net>
2021-07-15 22:04:23 +0000

    Bug 1720368 - Remove MOZ_GECKO_PROFILER ifdefs around profiler_init and profiler_shutdown calls, r=gerald.
    Differential Revision: https://phabricator.services.mozilla.com/D119809

--- js/xpconnect/src/XPCShellImpl.cpp
+++ js/xpconnect/src/XPCShellImpl.cpp
@@ -1078,10 +1078,8 @@ int XRE_XPCShellMain(int argc, char** argv, char** envp,
   // with the IOInterposer will be properly tracked.
   mozilla::IOInterposerInit ioInterposerGuard;
 
-#ifdef MOZ_GECKO_PROFILER
   char aLocal;
   profiler_init(&aLocal);
-#endif
 
 #ifdef MOZ_ASAN_REPORTER
   PR_SetEnv("MOZ_DISABLE_ASAN_REPORTER=1");
@@ -1440,11 +1438,9 @@ int XRE_XPCShellMain(int argc, char** argv, char** envp,
     CrashReporter::UnsetExceptionHandler();
   }
 
-#ifdef MOZ_GECKO_PROFILER
   // This must precede NS_LogTerm(), otherwise xpcshell return non-zero
   // during some tests, which causes failures.
   profiler_shutdown();
-#endif
 
   NS_LogTerm();
 
--- tools/profiler/public/GeckoProfiler.h
+++ tools/profiler/public/GeckoProfiler.h
@@ -27,6 +27,11 @@
 #include "mozilla/ProfilerMarkers.h"
 #include "mozilla/ProfilerState.h"
 
+enum class IsFastShutdown {
+  No,
+  Yes,
+};
+
 #ifndef MOZ_GECKO_PROFILER
 
 #  include "mozilla/UniquePtr.h"
@@ -80,6 +85,11 @@ static inline mozilla::UniquePtr<mozilla::ProfileChunkedBuffer>
 profiler_capture_backtrace() {
   return nullptr;
 }
+
+static inline void profiler_init(void* stackTop) {}
+
+static inline void profiler_shutdown(
+    IsFastShutdown aIsFastShutdown = IsFastShutdown::No) {}
 
 static inline void profiler_set_process_name(
     const nsACString& aProcessName, const nsACString* aETLDplus1 = nullptr) {}
@@ -184,11 +194,6 @@ void profiler_init_threadmanager();
 #  define AUTO_PROFILER_INIT mozilla::AutoProfilerInit PROFILER_RAII
 // Call this after the nsThreadManager is Init()ed
 #  define AUTO_PROFILER_INIT2 mozilla::AutoProfilerInit2 PROFILER_RAII
-
-enum class IsFastShutdown {
-  No,
-  Yes,
-};
 
 // Clean up the profiler module, stopping it if required. This function may
 // also save a shutdown profile if requested. No profiler calls should happen
