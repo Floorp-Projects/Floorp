7b0e100c2e5cf980cea732bffe7023f366e9c266
stransky <stransky@redhat.com>
2021-07-15 09:48:03 +0000

    Bug 1717451 [Linux] Guard logging code with MOZ_LOG_TEST macro, r=jhorak
    Depends on D119030
    
    Differential Revision: https://phabricator.services.mozilla.com/D119031

--- widget/gtk/nsWindow.h
+++ widget/gtk/nsWindow.h
@@ -57,6 +57,9 @@ extern mozilla::LazyLogModule gWidgetPopupLog;
 #  define LOGDRAG(args) MOZ_LOG(gWidgetDragLog, mozilla::LogLevel::Debug, args)
 #  define LOG_POPUP(args) \
     MOZ_LOG(gWidgetPopupLog, mozilla::LogLevel::Debug, args)
+#  define LOG_ENABLED()                                         \
+    (MOZ_LOG_TEST(gWidgetPopupLog, mozilla::LogLevel::Debug) || \
+     MOZ_LOG_TEST(gWidgetLog, mozilla::LogLevel::Debug))
 
 #else
 
@@ -64,6 +67,7 @@ extern mozilla::LazyLogModule gWidgetPopupLog;
 #  define LOGW(args)
 #  define LOGDRAG(args)
 #  define LOG_POPUP(args)
+#  define LOG_ENABLED() false
 
 #endif /* MOZ_LOGGING */
 
