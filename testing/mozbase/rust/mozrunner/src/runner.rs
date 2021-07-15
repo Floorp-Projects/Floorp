15bc18588a442dff9e6542fabad5b9c43d8b0ac8
Henrik Skupin <mail@hskupin.info>
2021-07-15 09:13:52 +0000

    Bug 1720502 - [mozrunner-rust] Only pass --foreground argument on MacOS. r=webdriver-reviewers,jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D119945

--- testing/mozbase/rust/mozrunner/src/runner.rs
+++ testing/mozbase/rust/mozrunner/src/runner.rs
@@ -288,7 +288,9 @@ impl Runner for FirefoxRunner {
                 Arg::Other(_) | Arg::None => {}
             }
         }
-        if !seen_foreground {
+        // -foreground is only supported on Mac, and shouldn't be passed
+        // to Firefox on other platforms (bug 1720502).
+        if cfg!(target_os = "macos") && !seen_foreground {
             cmd.arg("-foreground");
         }
         if !seen_no_remote {
