a2d1c64b79144aab7e2dafe74f2ec3e58c467c27
Matthew Gaudet <mgaudet@mozilla.com>
2021-07-16 15:31:44 +0000

    Bug 1720901 - Run test262-update.py to import new tests. r=arai
    Disable new failing tests that aren't disabled under a particular feature flag as well.
    
    Depends on D120075
    
    Differential Revision: https://phabricator.services.mozilla.com/D120076

---
+++ js/src/tests/test262/intl402/Locale/prototype/calendars/output-array.js
@@ -0,0 +1,18 @@
+// |reftest| skip -- Intl.Locale-info is not supported
+// Copyright 2021 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-intl.locale.prototype.calendars
+description: >
+    Checks that the return value of Intl.Locale.prototype.calendars is an Array.
+info: |
+  CalendarsOfLocale ( loc )
+  ...
+  5. Return ! CreateArrayFromListAndPreferred( list, preferred ).
+features: [Intl.Locale,Intl.Locale-info]
+---*/
+
+assert(Array.isArray(new Intl.Locale('en').calendars));
+
+reportCompare(0, 0);
