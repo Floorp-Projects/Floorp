b683cdb180a350533843ea87c52db3fd12718447
Agi Sferro <agi@sferro.dev>
2021-07-16 17:49:53 +0000

    Bug 1718341 - Rename androidx86_7_0.py to android-x86_64. r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D119219

--- testing/mozharness/configs/android/android_common.py
+++ testing/mozharness/configs/android/android_common.py
@@ -5,7 +5,7 @@
 # Shared/common mozharness configuration for Android unit tests.
 #
 # This configuration should be combined with platform-specific mozharness
-# configuration such as androidx86_7_0.py, android_hw, or similar.
+# configuration such as android-x86_64.py, android_hw, or similar.
 
 from __future__ import absolute_import
 import os
