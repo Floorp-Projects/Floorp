0122a0e82aec70ce4e6762f7f17d9a3d510c9522
shravanrn@gmail.com <shravanrn@gmail.com>
2021-07-16 02:38:40 +0000

    Bug 1713735 part 1 - Vendored files for wasm2c for use in rlbox r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D116441

---
+++ third_party/wasm2c/src/tools/wasm2c.cc
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2017 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cassert>
+#include <cinttypes>
+#include <cstdio>
+#include <cstdlib>
+
+#include "src/apply-names.h"
+#include "src/binary-reader.h"
+#include "src/binary-reader-ir.h"
+#include "src/error-formatter.h"
+#include "src/feature.h"
+#include "src/generate-names.h"
+#include "src/ir.h"
+#include "src/option-parser.h"
+#include "src/stream.h"
+#include "src/validator.h"
+#include "src/wast-lexer.h"
+
+#include "src/c-writer.h"
+
+using namespace wabt;
+
+static int s_verbose;
+static std::string s_infile;
+static std::string s_outfile;
+static Features s_features;
+static WriteCOptions s_write_c_options;
+static bool s_read_debug_names = true;
+static std::unique_ptr<FileStream> s_log_stream;
+
+static const char s_description[] =
+R"(  Read a file in the WebAssembly binary format, and convert it to
+  a C source file and header.
+
+examples:
+  # parse binary file test.wasm and write test.c and test.h
+  $ wasm2c test.wasm -o test.c
+
+  # parse test.wasm, write test.c and test.h, but ignore the debug names, if any
+  $ wasm2c test.wasm --no-debug-names -o test.c
+)";
+
+static void ParseOptions(int argc, char** argv) {
+  OptionParser parser("wasm2c", s_description);
+
+  parser.AddOption('v', "verbose", "Use multiple times for more info", []() {
+    s_verbose++;
+    s_log_stream = FileStream::CreateStderr();
+  });
+  parser.AddOption(
+      'o', "output", "FILENAME",
+      "Output file for the generated C source file, by default use stdout",
+      [](const char* argument) {
+        s_outfile = argument;
+        ConvertBackslashToSlash(&s_outfile);
+      });
+  parser.AddOption(
+      'n', "modname", "MODNAME",
+      "Unique name for the module being generated. Each wasm sandboxed module in a single application should have a unique name.",
+      [](const char* argument) {
+        s_write_c_options.mod_name = argument;
+      });
+  s_features.AddOptions(&parser);
+  parser.AddOption("no-debug-names", "Ignore debug names in the binary file",
+                   []() { s_read_debug_names = false; });
+  parser.AddArgument("filename", OptionParser::ArgumentCount::One,
+                     [](const char* argument) {
+                       s_infile = argument;
+                       ConvertBackslashToSlash(&s_infile);
+                     });
+  parser.Parse(argc, argv);
+
+  // TODO(binji): currently wasm2c doesn't support any non-default feature
+  // flags.
+  bool any_non_default_feature = false;
+#define WABT_FEATURE(variable, flag, default_, help) \
+  any_non_default_feature |= (s_features.variable##_enabled() != default_);
+#include "src/feature.def"
+#undef WABT_FEATURE
+
+  if (any_non_default_feature) {
+    fprintf(stderr, "wasm2c currently support only default feature flags.\n");
+    exit(1);
+  }
+}
+
+// TODO(binji): copied from binary-writer-spec.cc, probably should share.
+static string_view strip_extension(string_view s) {
+  string_view ext = s.substr(s.find_last_of('.'));
+  string_view result = s;
+
+  if (ext == ".c")
+    result.remove_suffix(ext.length());
+  return result;
+}
+
+int ProgramMain(int argc, char** argv) {
+  Result result;
+
+  InitStdio();
+  ParseOptions(argc, argv);
+
+  std::vector<uint8_t> file_data;
+  result = ReadFile(s_infile.c_str(), &file_data);
+  if (Succeeded(result)) {
+    Errors errors;
+    Module module;
+    const bool kStopOnFirstError = true;
+    const bool kFailOnCustomSectionError = true;
+    ReadBinaryOptions options(s_features, s_log_stream.get(),
+                              s_read_debug_names, kStopOnFirstError,
+                              kFailOnCustomSectionError);
+    result = ReadBinaryIr(s_infile.c_str(), file_data.data(), file_data.size(),
+                          options, &errors, &module);
+    if (Succeeded(result)) {
+      if (Succeeded(result)) {
+        ValidateOptions options(s_features);
+        result = ValidateModule(&module, &errors, options);
+        result |= GenerateNames(&module);
+      }
+
+      if (Succeeded(result)) {
+        /* TODO(binji): This shouldn't fail; if a name can't be applied
+         * (because the index is invalid, say) it should just be skipped. */
+        Result dummy_result = ApplyNames(&module);
+        WABT_USE(dummy_result);
+      }
+
+      if (Succeeded(result)) {
+        if (!s_outfile.empty()) {
+          std::string header_name =
+              strip_extension(s_outfile).to_string() + ".h";
+          FileStream c_stream(s_outfile.c_str());
+          FileStream h_stream(header_name);
+          result = WriteC(&c_stream, &h_stream, header_name.c_str(), &module,
+                          s_write_c_options);
+        } else {
+          FileStream stream(stdout);
+          result =
+              WriteC(&stream, &stream, "wasm.h", &module, s_write_c_options);
+        }
+      }
+    }
+    FormatErrorsToFile(errors, Location::Type::Binary);
+  }
+  return result != Result::Ok;
+}
+
+int main(int argc, char** argv) {
+  WABT_TRY
+  return ProgramMain(argc, argv);
+  WABT_CATCH_BAD_ALLOC_AND_EXIT
+}
+
---
+++ third_party/wasm2c/wasm2c/wasm-rt.h
@@ -0,0 +1,252 @@
+/*
+ * Copyright 2018 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef WASM_RT_H_
+#define WASM_RT_H_
+
+#include <stdint.h>
+#include <setjmp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Maximum stack depth before trapping. This can be configured by defining
+ * this symbol before including wasm-rt when building the generated c files,
+ * for example:
+ *
+ * ```
+ *   cc -c -DWASM_RT_MAX_CALL_STACK_DEPTH=100 my_module.c -o my_module.o
+ * ```
+ * */
+#ifndef WASM_RT_MAX_CALL_STACK_DEPTH
+#define WASM_RT_MAX_CALL_STACK_DEPTH 500
+#endif
+
+/** Check if we should use guard page model. This is enabled if
+ * --- the os is 64-bit
+ * --- the user has not asked for the use of explicit bounds checks
+ */
+#if !defined(WASM_USING_GUARD_PAGES)
+  #if !defined(WASM_USE_EXPLICIT_BOUNDS_CHECKS) && UINTPTR_MAX == 0xffffffffffffffff
+    #define WASM_USING_GUARD_PAGES 1
+  #else
+    #define WASM_USING_GUARD_PAGES 0
+  #endif
+#endif
+#if defined(_MSC_VER)
+#define WASM_RT_NO_RETURN __declspec(noreturn)
+#else
+#define WASM_RT_NO_RETURN __attribute__((noreturn))
+#endif
+
+/** Reason a trap occurred. Provide this to `wasm_rt_trap`. */
+typedef enum {
+  WASM_RT_TRAP_NONE,         /** No error. */
+  WASM_RT_TRAP_OOB,          /** Out-of-bounds access in linear memory. */
+  WASM_RT_TRAP_INT_OVERFLOW, /** Integer overflow on divide or truncation. */
+  WASM_RT_TRAP_DIV_BY_ZERO,  /** Integer divide by zero. */
+  WASM_RT_TRAP_INVALID_CONVERSION, /** Conversion from NaN to integer. */
+  WASM_RT_TRAP_UNREACHABLE,        /** Unreachable instruction executed. */
+  WASM_RT_TRAP_CALL_INDIRECT,      /** Invalid call_indirect, for any reason. */
+  WASM_RT_TRAP_EXHAUSTION,         /** Call stack exhausted. */
+} wasm_rt_trap_t;
+
+/** Value types. Used to define function signatures. */
+typedef enum {
+  WASM_RT_I32,
+  WASM_RT_I64,
+  WASM_RT_F32,
+  WASM_RT_F64,
+} wasm_rt_type_t;
+
+/** A function type for all `anyfunc` functions in a Table. All functions are
+ * stored in this canonical form, but must be cast to their proper signature to
+ * call. */
+typedef void (*wasm_rt_anyfunc_t)(void);
+
+/** A single element of a Table. */
+typedef struct {
+  /** The index as returned from `wasm_rt_register_func_type`. */
+  uint32_t func_type;
+  /** The function. The embedder must know the actual C signature of the
+   * function and cast to it before calling. */
+  wasm_rt_anyfunc_t func;
+} wasm_rt_elem_t;
+
+/** A Memory object. */
+typedef struct {
+  /** The linear memory data, with a byte length of `size`. */
+#if WASM_USING_GUARD_PAGES == 1
+  uint8_t* const data;
+#else
+  uint8_t* data;
+#endif
+  /** The current and maximum page count for this Memory object. If there is no
+   * maximum, `max_pages` is 0xffffffffu (i.e. UINT32_MAX). */
+  uint32_t pages, max_pages;
+  /** The current size of the linear memory, in bytes. */
+  uint32_t size;
+} wasm_rt_memory_t;
+
+/** A Table object. */
+typedef struct {
+  /** The table element data, with an element count of `size`. */
+  wasm_rt_elem_t* data;
+  /** The maximum element count of this Table object. If there is no maximum,
+   * `max_size` is 0xffffffffu (i.e. UINT32_MAX). */
+  uint32_t max_size;
+  /** The current element count of the table. */
+  uint32_t size;
+} wasm_rt_table_t;
+
+typedef struct wasm_func_type_t {
+  wasm_rt_type_t* params;
+  wasm_rt_type_t* results;
+  uint32_t param_count;
+  uint32_t result_count;
+} wasm_func_type_t;
+
+#define WASM2C_WASI_MAX_SETJMP_STACK 32
+#define WASM2C_WASI_MAX_FDS 32
+typedef struct wasm_sandbox_wasi_data {
+  wasm_rt_memory_t* heap_memory;
+
+  uint32_t tempRet0;
+
+  uint32_t next_setjmp_index;
+  jmp_buf setjmp_stack[WASM2C_WASI_MAX_SETJMP_STACK];
+
+  uint32_t main_argc;
+  char** main_argv;
+
+  int wasm_fd_to_native[WASM2C_WASI_MAX_FDS];
+  uint32_t next_wasm_fd;
+
+} wasm_sandbox_wasi_data;
+
+typedef void (*wasm_rt_sys_init_t)(void);
+typedef void* (*create_wasm2c_sandbox_t)(void);
+typedef void (*destroy_wasm2c_sandbox_t)(void* sbx_ptr);
+typedef void* (*lookup_wasm2c_nonfunc_export_t)(void* sbx_ptr, const char* name);
+typedef uint32_t (*lookup_wasm2c_func_index_t)(void* sbx_ptr, uint32_t param_count, uint32_t result_count, wasm_rt_type_t* types);
+typedef uint32_t (*add_wasm2c_callback_t)(void* sbx_ptr, uint32_t func_type_idx, void* func_ptr);
+typedef void (*remove_wasm2c_callback_t)(void* sbx_ptr, uint32_t callback_idx);
+
+typedef struct wasm2c_sandbox_funcs_t {
+  wasm_rt_sys_init_t wasm_rt_sys_init;
+  create_wasm2c_sandbox_t create_wasm2c_sandbox;
+  destroy_wasm2c_sandbox_t destroy_wasm2c_sandbox;
+  lookup_wasm2c_nonfunc_export_t lookup_wasm2c_nonfunc_export;
+  lookup_wasm2c_func_index_t lookup_wasm2c_func_index;
+  add_wasm2c_callback_t add_wasm2c_callback;
+  remove_wasm2c_callback_t remove_wasm2c_callback;
+} wasm2c_sandbox_funcs_t;
+
+/** Stop execution immediately and jump back to the call to `wasm_rt_try`.
+ *  The result of `wasm_rt_try` will be the provided trap reason.
+ *
+ *  This is typically called by the generated code, and not the embedder. */
+WASM_RT_NO_RETURN extern void wasm_rt_trap(wasm_rt_trap_t);
+
+/** Register a function type with the given signature. The returned function
+ * index is guaranteed to be the same for all calls with the same signature.
+ * The following varargs must all be of type `wasm_rt_type_t`, first the
+ * params` and then the `results`.
+ *
+ *  ```
+ *    // Register (func (param i32 f32) (result i64)).
+ *    wasm_rt_register_func_type(2, 1, WASM_RT_I32, WASM_RT_F32, WASM_RT_I64);
+ *    => returns 1
+ *
+ *    // Register (func (result i64)).
+ *    wasm_rt_register_func_type(0, 1, WASM_RT_I32);
+ *    => returns 2
+ *
+ *    // Register (func (param i32 f32) (result i64)) again.
+ *    wasm_rt_register_func_type(2, 1, WASM_RT_I32, WASM_RT_F32, WASM_RT_I64);
+ *    => returns 1
+ *  ``` */
+extern uint32_t wasm_rt_register_func_type(wasm_func_type_t** p_func_type_structs,
+                                           uint32_t* p_func_type_count,
+                                           uint32_t params,
+                                           uint32_t results,
+                                           wasm_rt_type_t* types);
+
+void wasm_rt_cleanup_func_types(wasm_func_type_t** p_func_type_structs,
+                               uint32_t* p_func_type_count);
+
+/** Initialize a Memory object with an initial page size of `initial_pages` and
+ * a maximum page size of `max_pages`.
+ *
+ *  ```
+ *    wasm_rt_memory_t my_memory;
+ *    // 1 initial page (65536 bytes), and a maximum of 2 pages.
+ *    wasm_rt_allocate_memory(&my_memory, 1, 2);
+ *  ``` */
+extern void wasm_rt_allocate_memory(wasm_rt_memory_t*,
+                                    uint32_t initial_pages,
+                                    uint32_t max_pages);
+
+extern void wasm_rt_deallocate_memory(wasm_rt_memory_t*);
+
+/** Grow a Memory object by `pages`, and return the previous page count. If
+ * this new page count is greater than the maximum page count, the grow fails
+ * and 0xffffffffu (UINT32_MAX) is returned instead.
+ *
+ *  ```
+ *    wasm_rt_memory_t my_memory;
+ *    ...
+ *    // Grow memory by 10 pages.
+ *    uint32_t old_page_size = wasm_rt_grow_memory(&my_memory, 10);
+ *    if (old_page_size == UINT32_MAX) {
+ *      // Failed to grow memory.
+ *    }
+ *  ``` */
+extern uint32_t wasm_rt_grow_memory(wasm_rt_memory_t*, uint32_t pages);
+
+/** Initialize a Table object with an element count of `elements` and a maximum
+ * page size of `max_elements`.
+ *
+ *  ```
+ *    wasm_rt_table_t my_table;
+ *    // 5 elemnets and a maximum of 10 elements.
+ *    wasm_rt_allocate_table(&my_table, 5, 10);
+ *  ``` */
+extern void wasm_rt_allocate_table(wasm_rt_table_t*,
+                                   uint32_t elements,
+                                   uint32_t max_elements);
+
+extern void wasm_rt_deallocate_table(wasm_rt_table_t*);
+
+extern void wasm_rt_expand_table(wasm_rt_table_t*);
+
+// One time init function for wasm runtime. Should be called once for the current process
+extern void wasm_rt_sys_init();
+
+// Initialize wasi for the given sandbox. Called prior to sandbox execution.
+extern void wasm_rt_init_wasi(wasm_sandbox_wasi_data*);
+
+extern void wasm_rt_cleanup_wasi(wasm_sandbox_wasi_data*);
+
+// Helper function that host can use to ensure wasm2c code is loaded correctly when using dynamic libraries
+extern void wasm2c_ensure_linked();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* WASM_RT_H_ */
