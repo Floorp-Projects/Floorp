c28fc1238562c9529a65576a20667cc3c6dd128c
Florian Quèze <florian@queze.net>
2021-07-16 15:26:23 +0000

    Bug 1720851 - fix includes in NetworkMarker.cpp, r=gerald,necko-reviewers,kershaw.
    Differential Revision: https://phabricator.services.mozilla.com/D120062

--- netwerk/protocol/http/NetworkMarker.cpp
+++ netwerk/protocol/http/NetworkMarker.cpp
@@ -4,6 +4,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "NetworkMarker.h"
+
+#include "HttpBaseChannel.h"
+#include "nsIChannelEventSink.h"
+
 namespace mozilla {
 namespace net {
 
