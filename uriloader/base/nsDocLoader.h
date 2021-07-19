3dc6e87585fac80969c58ee434cf818ef2560460
Csoregi Natalia <ncsoregi@mozilla.com>
2021-07-20 00:41:23 +0300

    Backed out changeset b6399ca8a2c1 (bug 1706347) for failures on iframe-cross-origin-print.sub.html. CLOSED TREE

--- uriloader/base/nsDocLoader.h
+++ uriloader/base/nsDocLoader.h
@@ -30,6 +30,7 @@
 
 namespace mozilla {
 namespace dom {
+class BrowserBridgeChild;
 class BrowsingContext;
 }  // namespace dom
 }  // namespace mozilla
@@ -54,6 +55,8 @@ class nsDocLoader : public nsIDocumentLoader,
                     public nsIChannelEventSink,
                     public nsISupportsPriority {
  public:
+  using BrowserBridgeChild = mozilla::dom::BrowserBridgeChild;
+
   NS_DECLARE_STATIC_IID_ACCESSOR(NS_THIS_DOCLOADER_IMPL_CID)
 
   nsDocLoader() : nsDocLoader(false) {}
@@ -133,9 +136,27 @@ class nsDocLoader : public nsIDocumentLoader,
     mTreatAsBackgroundLoad = false;
   };
 
-  uint32_t ChildCount() const { return mChildList.Length(); }
+  // Inform a parent docloader that a BrowserBridgeChild has been created for
+  // an OOP sub-document.
+  // (This is the OOP counterpart to ChildEnteringOnload below.)
+  void OOPChildLoadStarted(BrowserBridgeChild* aChild) {
+    MOZ_DIAGNOSTIC_ASSERT(!mOOPChildrenLoading.Contains(aChild));
+    mOOPChildrenLoading.AppendElement(aChild);
+  }
 
-  void OOPChildrenLoadingIsEmpty() { DocLoaderIsEmpty(true); }
+  // Inform a parent docloader that the BrowserBridgeChild for one of its
+  // OOP sub-documents is done calling its onload handler.
+  // (This is the OOP counterpart to ChildDoneWithOnload below.)
+  void OOPChildLoadDone(BrowserBridgeChild* aChild) {
+    // aChild will not be in the list if nsDocLoader::Stop() was called, since
+    // that clears mOOPChildrenLoading.  It also dispatches the 'load' event,
+    // so we don't need to call DocLoaderIsEmpty in that case.
+    if (mOOPChildrenLoading.RemoveElement(aChild)) {
+      DocLoaderIsEmpty(true);
+    }
+  }
+
+  uint32_t ChildCount() const { return mChildList.Length(); }
 
  protected:
   explicit nsDocLoader(bool aNotifyAboutBackgroundRequests);
@@ -352,6 +373,10 @@ class nsDocLoader : public nsIDocumentLoader,
   // DocLoaderIsEmpty calls (those coming from requests finishing in our
   // loadgroup) unless this is empty.
   nsCOMArray<nsIDocumentLoader> mChildrenInOnload;
+
+  // The OOP counterpart to mChildrenInOnload.
+  // Not holding strong refs here since we don't actually use the BBCs.
+  nsTArray<const BrowserBridgeChild*> mOOPChildrenLoading;
 
   int64_t GetMaxTotalProgress();
 
