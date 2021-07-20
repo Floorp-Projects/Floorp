b817b99e35bb757ced733b42c9b42f18c2abe49d
Jan de Mooij <jdemooij@mozilla.com>
2021-07-20 07:29:27 +0000

    Bug 1717438 part 3 - Use the InvalidatedTeleporting flag also for shadowed properties. r=tcampbell
    The flag is now used to guard against both cases teleporting has to watch out
    for: proto changes and shadowed properties.
    
    This lets us get rid of dictionary conversions and reshaping for ReshapeForShadowedProp.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118361

--- js/src/vm/NativeObject.cpp
+++ js/src/vm/NativeObject.cpp
@@ -1138,7 +1138,7 @@ static bool ReshapeForShadowedPropSlow(JSContext* cx, HandleNativeObject obj,
     }
 
     if (proto->as<NativeObject>().contains(cx, id)) {
-      return NativeObject::reshapeForShadowedProp(cx, proto.as<NativeObject>());
+      return JSObject::setInvalidatedTeleporting(cx, proto);
     }
 
     proto = proto->staticPrototype();
@@ -1162,15 +1162,6 @@ static MOZ_ALWAYS_INLINE bool ReshapeForShadowedProp(JSContext* cx,
   }
 
   return ReshapeForShadowedPropSlow(cx, obj.as<NativeObject>(), id);
-}
-
-/* static */
-bool NativeObject::reshapeForShadowedProp(JSContext* cx,
-                                          HandleNativeObject obj) {
-  if (!obj->inDictionaryMode()) {
-    return toDictionaryMode(cx, obj);
-  }
-  return generateNewDictionaryShape(cx, obj);
 }
 
 static bool ChangeProperty(JSContext* cx, HandleNativeObject obj, HandleId id,
