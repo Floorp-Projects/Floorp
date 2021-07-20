607e9c9a835567eed2e22c9818ee10be32e060eb
Agi Sferro <agi@sferro.dev>
2021-07-20 03:23:29 +0000

    Bug 1718341 - Rename arm-4.3 -> arm r=aklotz,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D119220

--- testing/mozharness/configs/android/android_pgo.py
+++ testing/mozharness/configs/android/android_pgo.py
@@ -5,7 +5,7 @@
 # Mozharness configuration for Android PGO.
 #
 # This configuration should be combined with platform-specific mozharness
-# configuration such as androidarm_4_3.py, or similar.
+# configuration such as androidarm.py, or similar.
 
 config = {
     "default_actions": [
