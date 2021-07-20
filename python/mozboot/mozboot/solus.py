fdce01f3f52f6cfad2bec04439290307d12d9eab
Agi Sferro <agisferro@gmail.com>
2021-07-20 03:23:31 +0000

    Bug 1718341 - Remove redundant override in MobileAndroidBootstrapper. r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D119998

--- python/mozboot/mozboot/solus.py
+++ python/mozboot/mozboot/solus.py
@@ -20,14 +20,8 @@ if sys.version_info < (3,):
 class SolusBootstrapper(LinuxBootstrapper, BaseBootstrapper):
     """Solus experimental bootstrapper."""
 
-    SYSTEM_PACKAGES = [
-        "nodejs",
-        "unzip",
-        "zip",
-    ]
-    SYSTEM_COMPONENTS = [
-        "system.devel",
-    ]
+    SYSTEM_PACKAGES = ["nodejs", "unzip", "zip"]
+    SYSTEM_COMPONENTS = ["system.devel"]
 
     BROWSER_PACKAGES = [
         "alsa-lib",
@@ -68,12 +62,6 @@ class SolusBootstrapper(LinuxBootstrapper, BaseBootstrapper):
     def install_browser_artifact_mode_packages(self, mozconfig_builder):
         self.ensure_browser_packages(artifact_mode=True)
 
-    def install_mobile_android_packages(self, mozconfig_builder):
-        self.ensure_mobile_android_packages(mozconfig_builder)
-
-    def install_mobile_android_artifact_mode_packages(self, mozconfig_builder):
-        self.ensure_mobile_android_packages(mozconfig_builder, artifact_mode=True)
-
     def ensure_browser_packages(self, artifact_mode=False):
         self.package_install(*self.BROWSER_PACKAGES)
 
@@ -81,7 +69,7 @@ class SolusBootstrapper(LinuxBootstrapper, BaseBootstrapper):
         # installed via ensure_browser_packages
         pass
 
-    def ensure_mobile_android_packages(self, mozconfig_builder, artifact_mode=False):
+    def install_mobile_android_packages(self, mozconfig_builder, artifact_mode=False):
         try:
             self.package_install(*self.MOBILE_ANDROID_COMMON_PACKAGES)
         except Exception as e:
@@ -90,7 +78,7 @@ class SolusBootstrapper(LinuxBootstrapper, BaseBootstrapper):
 
         # 2. Android pieces.
         self.ensure_java(mozconfig_builder)
-        super().ensure_mobile_android_packages(artifact_mode=artifact_mode)
+        super().install_mobile_android_packages(artifact_mode=artifact_mode)
 
     def _update_package_manager(self):
         pass
