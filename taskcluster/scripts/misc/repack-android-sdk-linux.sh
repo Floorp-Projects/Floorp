be84115bc0eb6c62560d9349a5b2009ebb6a6cbd
Agi Sferro <agi@sferro.dev>
2021-07-20 03:23:32 +0000

    Bug 1718341 - Update Android cmdtools to 4.0. r=nalexander,glandium
    The SDK Tools version we're currently using is deprecated and was last released
    in 2017. It's time for an upgrade!
    
    Differential Revision: https://phabricator.services.mozilla.com/D119226

--- taskcluster/scripts/misc/repack-android-sdk-linux.sh
+++ taskcluster/scripts/misc/repack-android-sdk-linux.sh
@@ -10,11 +10,7 @@ mkdir -p $HOME/artifacts $UPLOAD_DIR
 
 # Populate /builds/worker/.mozbuild/android-sdk-linux.
 cd $GECKO_PATH
-./mach python python/mozboot/mozboot/android.py --artifact-mode --no-interactive
-
-# It's nice to have the build logs include the state of the world upon
-# completion.
-/builds/worker/.mozbuild/android-sdk-linux/tools/bin/sdkmanager --list
+./mach python python/mozboot/mozboot/android.py --artifact-mode --no-interactive --list-packages
 
 tar cavf $UPLOAD_DIR/android-sdk-linux.tar.zst -C /builds/worker/.mozbuild android-sdk-linux
 
