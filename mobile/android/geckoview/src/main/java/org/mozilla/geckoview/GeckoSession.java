f1853c4d3df64f3c97b9933e452d4290b1d19e5e
Dylan Roeh <droeh@mozilla.com>
2021-07-20 20:28:28 +0000

    Bug 1543720 - Add support for Storage Access API in GeckoView. r=aklotz,geckoview-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D116143

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -5470,6 +5470,12 @@ public class GeckoSession {
         int PERMISSION_TRACKING = 7;
 
         /**
+         * Permission for third party frames to access first party cookies and storage. May be
+         * granted heuristically in some cases.
+         */
+        int PERMISSION_STORAGE_ACCESS = 8;
+
+        /**
          * Represents a content permission -- including the type of permission,
          * the present value of the permission, the URL the permission pertains to,
          * and other information.
@@ -5500,6 +5506,12 @@ public class GeckoSession {
             final public @NonNull String uri;
 
             /**
+             * The third party origin associated with the request; currently only used
+             * for storage access permission.
+             */
+            final public @Nullable String thirdPartyOrigin;
+
+            /**
              * A boolean indicating whether this content permission is associated with
              * private browsing.
              */
@@ -5525,6 +5537,7 @@ public class GeckoSession {
 
             protected ContentPermission() {
                 this.uri = "";
+                this.thirdPartyOrigin = null;
                 this.privateMode = false;
                 this.permission = PERMISSION_GEOLOCATION;
                 this.value = VALUE_ALLOW;
@@ -5539,6 +5552,13 @@ public class GeckoSession {
 
                 final String permission = bundle.getString("perm");
                 this.permission = convertType(permission);
+                if (permission.startsWith("3rdPartyStorage^")) {
+                    // Storage access permissions are stored with the key "3rdPartyStorage^https://foo.com"
+                    // where the third party origin is "https://foo.com".
+                    this.thirdPartyOrigin = permission.substring(16);
+                } else {
+                    this.thirdPartyOrigin = bundle.getString("thirdPartyOrigin");
+                }
 
                 this.value = bundle.getInt("value");
                 this.contextId = StorageController.retrieveUnsafeSessionContextId(bundle.getString("contextId"));
@@ -5595,6 +5615,8 @@ public class GeckoSession {
                     return PERMISSION_MEDIA_KEY_SYSTEM_ACCESS;
                 } else if ("trackingprotection".equals(type) || "trackingprotection-pb".equals(type)) {
                     return PERMISSION_TRACKING;
+                } else if ("storage-access".equals(type) || type.startsWith("3rdPartyStorage^")) {
+                    return PERMISSION_STORAGE_ACCESS;
                 } else {
                     return -1;
                 }
@@ -5619,6 +5641,8 @@ public class GeckoSession {
                         return "media-key-system-access";
                     case PERMISSION_TRACKING:
                         return privateMode ? "trackingprotection-pb" : "trackingprotection";
+                    case PERMISSION_STORAGE_ACCESS:
+                        return "storage-access";
                     default:
                         return "";
                 }
@@ -5641,8 +5665,9 @@ public class GeckoSession {
             }
 
             /* package */ @NonNull GeckoBundle toGeckoBundle() {
-                final GeckoBundle res = new GeckoBundle(5);
+                final GeckoBundle res = new GeckoBundle(7);
                 res.putString("uri", uri);
+                res.putString("thirdPartyOrigin", thirdPartyOrigin);
                 res.putString("principal", mPrincipal);
                 res.putBoolean("privateMode", privateMode);
                 res.putString("perm", convertType(permission, privateMode));
@@ -5902,7 +5927,9 @@ public class GeckoSession {
             PermissionDelegate.PERMISSION_XR,
             PermissionDelegate.PERMISSION_AUTOPLAY_INAUDIBLE,
             PermissionDelegate.PERMISSION_AUTOPLAY_AUDIBLE,
-            PermissionDelegate.PERMISSION_MEDIA_KEY_SYSTEM_ACCESS})
+            PermissionDelegate.PERMISSION_MEDIA_KEY_SYSTEM_ACCESS,
+            PermissionDelegate.PERMISSION_TRACKING,
+            PermissionDelegate.PERMISSION_STORAGE_ACCESS})
     /* package */ @interface Permission {}
 
     /**
