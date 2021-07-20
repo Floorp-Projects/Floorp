0cb0639db24cd94ed3823318dfcd0ebf1af9be13
Jan de Mooij <jdemooij@mozilla.com>
2021-07-20 15:05:15 +0000

    Bug 1721333 part 4 - Remove unused private slot from WeakMap jsapi-test. r=jonco
    Depends on D120309
    
    Differential Revision: https://phabricator.services.mozilla.com/D120310

--- js/src/jsapi-tests/testWeakMap.cpp
+++ js/src/jsapi-tests/testWeakMap.cpp
@@ -165,7 +165,7 @@ static size_t DelegateObjectMoved(JSObject* obj, JSObject* old) {
 
 JSObject* newKey() {
   static const JSClass keyClass = {
-      "keyWithDelegate", JSCLASS_HAS_PRIVATE | JSCLASS_HAS_RESERVED_SLOTS(1),
+      "keyWithDelegate", JSCLASS_HAS_RESERVED_SLOTS(1),
       JS_NULL_CLASS_OPS, JS_NULL_CLASS_SPEC,
       JS_NULL_CLASS_EXT, JS_NULL_OBJECT_OPS};
 
