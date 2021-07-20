7dbc09100431e970189e9d47c94189bdf8ec0d2d
stransky <stransky@redhat.com>
2021-07-20 13:33:04 +0000

    Bug 1673000 [Linux] Use Gtk color picker, r=jhorak
    Differential Revision: https://phabricator.services.mozilla.com/D120324

--- widget/gtk/nsColorPicker.h
+++ widget/gtk/nsColorPicker.h
@@ -12,10 +12,8 @@
 #include "nsIColorPicker.h"
 #include "nsString.h"
 
-// Don't activate the GTK3 color picker for now, because it is missing a few
-// things, mainly the ability to let the user select a color on the screen.
-// See bug 1198256.
-#undef ACTIVATE_GTK3_COLOR_PICKER
+// Enable Gtk3 system color picker.
+#define ACTIVATE_GTK3_COLOR_PICKER 1
 
 class nsIWidget;
 
