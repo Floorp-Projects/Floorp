0de0db4c330976c4679159451b7da1355484daec
Robert Mader <robert.mader@posteo.de>
2021-07-21 20:34:55 +0000

    Bug 1721036 - Delay buffer commits until the surface is ready,r=stransky
    When the `MozContainer` surface is not yet ready in `Commit()`,
    we need to queue the commit.
    Similar to `nsWindow` and `WaylandVsyncSource` we can do so by
    registering via a callback via `add_initial_draw_callback()`.
    
    This required some logic changes which let to clean ups and
    simplifications in the whole file (I'm sorry it got so big).
    
    Differential Revision: https://phabricator.services.mozilla.com/D120429

--- widget/gtk/WindowSurfaceWaylandMultiBuffer.h
+++ widget/gtk/WindowSurfaceWaylandMultiBuffer.h
@@ -38,38 +38,39 @@ class WindowSurfaceWaylandMB : public WindowSurface {
   // and we send it to wayland compositor in FrameCallbackHandler()/
   // FlushPendingCommits().
   already_AddRefed<DrawTarget> Lock(
-      const LayoutDeviceIntRegion& aRegion) override;
+      const LayoutDeviceIntRegion& aInvalidRegion) override;
   void Commit(const LayoutDeviceIntRegion& aInvalidRegion) final;
-  RefPtr<nsWaylandDisplay> GetWaylandDisplay() { return mWaylandDisplay; };
-
-  static void BufferReleaseCallbackHandler(void* aData, wl_buffer* aBuffer);
 
  private:
-  RefPtr<WaylandShmBuffer> GetWaylandBuffer();
+  void Commit(const MutexAutoLock& aProofOfLock,
+              const LayoutDeviceIntRegion& aInvalidRegion);
   RefPtr<WaylandShmBuffer> ObtainBufferFromPool(
-      const LayoutDeviceIntSize& aSize);
-  void ReturnBufferToPool(const RefPtr<WaylandShmBuffer>& aBuffer);
+      const MutexAutoLock& aProofOfLock, const LayoutDeviceIntSize& aSize);
+  void ReturnBufferToPool(const MutexAutoLock& aProofOfLock,
+                          const RefPtr<WaylandShmBuffer>& aBuffer);
   void EnforcePoolSizeLimit(const MutexAutoLock& aProofOfLock);
-  void PrepareBufferForFrame(const MutexAutoLock& aProofOfLock);
+  void CollectPendingSurfaces(const MutexAutoLock& aProofOfLock);
   void HandlePartialUpdate(const MutexAutoLock& aProofOfLock,
                            const LayoutDeviceIntRegion& aInvalidRegion);
-  void IncrementBufferAge();
-  void BufferReleaseCallbackHandler(wl_buffer* aBuffer);
+  void IncrementBufferAge(const MutexAutoLock& aProofOfLock);
 
   mozilla::Mutex mSurfaceLock;
 
   nsWindow* mWindow;
-  RefPtr<nsWaylandDisplay> mWaylandDisplay;
-  RefPtr<WaylandShmBuffer> mWaylandBuffer;
   LayoutDeviceIntSize mMozContainerSize;
 
-  // partial damage
-  RefPtr<WaylandShmBuffer> mPreviousWaylandBuffer;
-  LayoutDeviceIntRegion mPreviousInvalidRegion;
+  RefPtr<WaylandShmBuffer> mInProgressBuffer;
+  RefPtr<WaylandShmBuffer> mFrontBuffer;
+  LayoutDeviceIntRegion mFrontBufferInvalidRegion;
 
   // buffer pool
   nsTArray<RefPtr<WaylandShmBuffer>> mInUseBuffers;
+  nsTArray<RefPtr<WaylandShmBuffer>> mPendingBuffers;
   nsTArray<RefPtr<WaylandShmBuffer>> mAvailableBuffers;
+
+  // delayed commits
+  bool mFrameInProcess;
+  bool mCallbackRequested;
 };
 
 }  // namespace mozilla::widget
