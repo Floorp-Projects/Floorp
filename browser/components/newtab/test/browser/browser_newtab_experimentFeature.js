289d6ac0ebef7b5c9311700d3e5efefade4ce7f2
Andrei Oprea <andrei.br92@gmail.com>
2021-07-21 15:03:00 +0000

    Bug 1716562 - Use a pref for each property in the value field of an experiment r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D118000

--- browser/components/newtab/test/browser/browser_newtab_experimentFeature.js
+++ browser/components/newtab/test/browser/browser_newtab_experimentFeature.js
@@ -3,9 +3,6 @@
 
 "use strict";
 
-const { ExperimentManager } = ChromeUtils.import(
-  "resource://nimbus/lib/ExperimentManager.jsm"
-);
 const { ExperimentFakes } = ChromeUtils.import(
   "resource://testing-common/NimbusTestUtils.jsm"
 );
@@ -30,60 +27,21 @@ add_task(async function test_enroll_newNewtabExperience() {
     ok(newtabExperience, "Newtab experience is on by default");
   });
 
-  let recipe = ExperimentFakes.recipe(`foo${Date.now()}`, {
-    branches: [
-      {
-        slug: "treatment",
-        ratio: 1,
-        feature: {
-          featureId: "newtab",
-          enabled: true,
-          value: {
-            newNewtabExperienceEnabled: false,
-            customizationMenuEnabled: false,
-          },
-        },
-      },
-      {
-        slug: "control",
-        ratio: 1,
-        feature: {
-          featureId: "newtab",
-          enabled: true,
-          value: {
-            newNewtabExperienceEnabled: false,
-            customizationMenuEnabled: false,
-          },
-        },
-      },
-    ],
-    bucketConfig: {
-      namespace: "mstest-utils",
-      randomizationUnit: "normandy_id",
-      start: 0,
-      count: 1000,
-      total: 1000,
+  let doExperimentCleanup = await ExperimentFakes.enrollWithFeatureConfig({
+    featureId: "newtab",
+    value: {
+      enabled: true,
+      newNewtabExperienceEnabled: false,
+      customizationMenuEnabled: false,
     },
   });
-
-  await ExperimentManager.enroll(recipe);
-
-  await BrowserTestUtils.waitForCondition(
-    () => ExperimentManager.store.getAllActive().length === 1,
-    "Wait for enrollment to finish"
-  );
 
   await SpecialPowers.spawn(browser, [], () => {
     let newtabExperience = content.document.querySelector(".newtab-experience");
     ok(!newtabExperience, "Newtab experience deactive");
   });
 
-  await ExperimentManager.unenroll(recipe.slug, "cleanup");
-
-  await BrowserTestUtils.waitForCondition(
-    () => ExperimentManager.store.getAllActive().length === 0,
-    "Wait for enrollment to finish"
-  );
+  await doExperimentCleanup();
 
   await SpecialPowers.spawn(browser, [], () => {
     let newtabExperience = content.document.querySelector(".newtab-experience");
