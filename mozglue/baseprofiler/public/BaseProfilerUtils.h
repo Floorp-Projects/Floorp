b7f9ea60673efbc0843160738f8b30503680d885
Gerald Squelart <gsquelart@mozilla.com>
2021-07-21 04:16:57 +0000

    Bug 1721110 - Introduce BaseProfilerProcessId and BaseProfilerThreadId - r=florian
    These classes should replace the `int` type that is currently used to store process and thread ids. The next patches will start using them. Advantages:
    - Prevent type mismatches, e.g., giving a process id (or other number) to a function expecting a thread id.
    - Prevent nonsensical arithmetic operations.
    - Make the unspecified id more abstract, so it's more obvious and portable.
    - Make conversions to/from numbers (for display or storage) more visible.
    - Allow future changes of APIs using them less risky.
    - Allow future changes of the ids themselves (e.g., to be able to use bigger underlying types on some platforms, or even the opaque std::thread::id type.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D120221

--- mozglue/baseprofiler/public/BaseProfilerUtils.h
+++ mozglue/baseprofiler/public/BaseProfilerUtils.h
@@ -10,6 +10,106 @@
 // This header contains most process- and thread-related functions.
 // It is safe to include unconditionally.
 
+#include "mozilla/Assertions.h"
+
+#include <type_traits>
+
+namespace mozilla::baseprofiler {
+
+// Trivially-copyable class containing a process id. It may be left unspecified.
+class BaseProfilerProcessId {
+ public:
+  // Unspecified process id.
+  constexpr BaseProfilerProcessId() = default;
+
+  [[nodiscard]] constexpr bool IsSpecified() const {
+    return mProcessId != scUnspecified;
+  }
+
+  using NumberType = int;
+
+  // Get the process id as a number, which may be unspecified.
+  // This should only be used for serialization or logging.
+  [[nodiscard]] constexpr NumberType ToNumber() const { return mProcessId; }
+
+  // BaseProfilerProcessId from given number (which may be unspecified).
+  constexpr static BaseProfilerProcessId FromNumber(
+      const NumberType& aProcessId) {
+    return BaseProfilerProcessId{aProcessId};
+  }
+
+  [[nodiscard]] constexpr bool operator==(
+      const BaseProfilerProcessId& aOther) const {
+    return mProcessId == aOther.mProcessId;
+  }
+  [[nodiscard]] constexpr bool operator!=(
+      const BaseProfilerProcessId& aOther) const {
+    return mProcessId != aOther.mProcessId;
+  }
+
+ private:
+  constexpr explicit BaseProfilerProcessId(const NumberType& aProcessId)
+      : mProcessId(aProcessId) {}
+
+  static constexpr NumberType scUnspecified = 0;
+  NumberType mProcessId = scUnspecified;
+};
+
+// Check traits. These should satisfy usage in std::atomic.
+static_assert(std::is_trivially_copyable_v<BaseProfilerProcessId>);
+static_assert(std::is_copy_constructible_v<BaseProfilerProcessId>);
+static_assert(std::is_move_constructible_v<BaseProfilerProcessId>);
+static_assert(std::is_copy_assignable_v<BaseProfilerProcessId>);
+static_assert(std::is_move_assignable_v<BaseProfilerProcessId>);
+
+// Trivially-copyable class containing a thread id. It may be left unspecified.
+class BaseProfilerThreadId {
+ public:
+  // Unspecified thread id.
+  constexpr BaseProfilerThreadId() = default;
+
+  [[nodiscard]] constexpr bool IsSpecified() const {
+    return mThreadId != scUnspecified;
+  }
+
+  using NumberType = int;
+
+  // Get the thread id as a number, which may be unspecified.
+  // This should only be used for serialization or logging.
+  [[nodiscard]] constexpr NumberType ToNumber() const { return mThreadId; }
+
+  // BaseProfilerThreadId from given number (which may be unspecified).
+  constexpr static BaseProfilerThreadId FromNumber(
+      const NumberType& aThreadId) {
+    return BaseProfilerThreadId{aThreadId};
+  }
+
+  [[nodiscard]] constexpr bool operator==(
+      const BaseProfilerThreadId& aOther) const {
+    return mThreadId == aOther.mThreadId;
+  }
+  [[nodiscard]] constexpr bool operator!=(
+      const BaseProfilerThreadId& aOther) const {
+    return mThreadId != aOther.mThreadId;
+  }
+
+ private:
+  constexpr explicit BaseProfilerThreadId(const NumberType& aThreadId)
+      : mThreadId(aThreadId) {}
+
+  static constexpr NumberType scUnspecified = 0;
+  NumberType mThreadId = scUnspecified;
+};
+
+// Check traits. These should satisfy usage in std::atomic.
+static_assert(std::is_trivially_copyable_v<BaseProfilerThreadId>);
+static_assert(std::is_copy_constructible_v<BaseProfilerThreadId>);
+static_assert(std::is_move_constructible_v<BaseProfilerThreadId>);
+static_assert(std::is_copy_assignable_v<BaseProfilerThreadId>);
+static_assert(std::is_move_assignable_v<BaseProfilerThreadId>);
+
+}  // namespace mozilla::baseprofiler
+
 #ifndef MOZ_GECKO_PROFILER
 
 namespace mozilla::baseprofiler {
