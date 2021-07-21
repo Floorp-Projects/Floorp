0e8b9d451f9d374e3626ad828ace521569d0fe79
Agi Sferro <agisferro@gmail.com>
2021-07-21 18:43:06 +0000

    Bug 1716536 - Add missing License headers to mobile. r=aklotz
    This also moves some tests to the public domain according to the Mozilla
    License Policy: https://www.mozilla.org/en-US/MPL/license-policy/
    
    Differential Revision: https://phabricator.services.mozilla.com/D119824

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/TestRunnerApiEngine.java
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/TestRunnerApiEngine.java
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
 package org.mozilla.geckoview.test;
 
 import android.util.Log;
--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/WebPushUtils.java
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/WebPushUtils.java
@@ -1,8 +1,5 @@
-/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
- * vim: ts=4 sw=4 expandtab:
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
 
 package org.mozilla.geckoview.test;
 
