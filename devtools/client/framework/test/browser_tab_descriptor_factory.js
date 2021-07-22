eff3f83881c799be21990bec3bae4d5b6b6c69c1
Alexandre Poirot <poirot.alex@gmail.com>
2021-07-22 16:55:55 +0000

    Bug 1721789 - [devtools] Fix browser_tab_descriptor_factory.js with server side targets. r=jdescottes
    TabDescriptor.getTarget can only be used with a TargetCommand initialized by calling startListening.
    We need to have the watcher running to have any target being defined when server targets is enabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120588

--- devtools/client/framework/test/browser_tab_descriptor_factory.js
+++ devtools/client/framework/test/browser_tab_descriptor_factory.js
@@ -5,6 +5,8 @@
 
 // Test TabDescriptorFactory
 
+const { createCommandsDictionary } = require("devtools/shared/commands/index");
+
 add_task(async function() {
   await testTabDescriptorWithURL("data:text/html;charset=utf-8,foo");
 
@@ -28,6 +30,12 @@ async function testTabDescriptorWithURL(url) {
   );
   is(descriptor, secondDescriptor, "second descriptor is the same");
 
+  // We have to involve TargetCommand in order to have a function TabDescriptor.getTarget.
+  // With bug 1700909, all code should soon be migrated to use CommandsFactory instead of TabDescriptorFactory.
+  // Making this test irrelevant and this cryptic workaround useless.
+  const commands = await createCommandsDictionary(descriptor);
+  await commands.targetCommand.startListening();
+
   info("Wait for descriptor's target");
   const target = await descriptor.getTarget();
 
@@ -36,6 +44,9 @@ async function testTabDescriptorWithURL(url) {
 
   info("Destroy the descriptor");
   await descriptor.destroy();
+
+  info("Destroy the command");
+  await commands.destroy();
 
   gBrowser.removeCurrentTab();
 }
