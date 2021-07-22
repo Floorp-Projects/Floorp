a601cc9dcd05d0e2f5b7fe9c1ac116a062e05024
Alexandre Poirot <poirot.alex@gmail.com>
2021-07-22 08:14:26 +0000

    Bug 1713607 - [devtools] Emit DOCUMENT_EVENT will-navigate in case of bfcache navigations. r=nchevobbe,jdescottes
    Introduce a new watcher for DOCUMENT_EVENT, now running in the parent process.
    This watcher will only emit the "will-navigate" resource and we will stop emitting it from the
    existing watcher, running in the content process. (except for iframe switching. i.e. the iframe dropdown menu)
    
    We have to move it to the parent to better handle bfcache navigations
    and more generally all navigations which are initiated from the parent process.
    bfcacheInParent feature enabled many types of navigations to be controlled from the parent process.
    
    This was especially important to have this implementation in the parent
    because the navigation event may be fired too late in the content process.
    Leading to will-navigate being emitted *after* the new target we navigate to is notified to the client.
    
    Differential Revision: https://phabricator.services.mozilla.com/D116383

---
+++ devtools/server/actors/resources/parent-process-document-event.js
@@ -0,0 +1,126 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const {
+  TYPES: { DOCUMENT_EVENT },
+} = require("devtools/server/actors/resources/index");
+const { Ci } = require("chrome");
+const ChromeUtils = require("ChromeUtils");
+const {
+  getAllRemoteBrowsingContexts,
+} = require("devtools/server/actors/watcher/target-helpers/utils.js");
+const {
+  WILL_NAVIGATE_TIME_SHIFT,
+} = require("devtools/server/actors/webconsole/listeners/document-events");
+
+class ParentProcessDocumentEventWatcher {
+  /**
+   * Start watching, from the parent process, for DOCUMENT_EVENT's "will-navigate" event related to a given Watcher Actor.
+   *
+   * All other DOCUMENT_EVENT events are implemented from another watcher class, running in the content process.
+   * Note that this other content process watcher will also emit one special edgecase of will-navigate
+   * retlated to the iframe dropdown menu.
+   *
+   * We have to move listen for navigation in the parent to better handle bfcache navigations
+   * and more generally all navigations which are initiated from the parent process.
+   * 'bfcacheInParent' feature enabled many types of navigations to be controlled from the parent process.
+   *
+   * This was especially important to have this implementation in the parent
+   * because the navigation event may be fired too late in the content process.
+   * Leading to will-navigate being emitted *after* the new target we navigate to is notified to the client.
+   *
+   * @param WatcherActor watcherActor
+   *        The watcher actor from which we should observe document event
+   * @param Object options
+   *        Dictionary object with following attributes:
+   *        - onAvailable: mandatory function
+   *          This will be called for each resource.
+   */
+  async watch(watcherActor, { onAvailable }) {
+    this.watcherActor = watcherActor;
+    this.onAvailable = onAvailable;
+
+    // Get all the BrowsingContext to be watched by this toolbox
+    const allBrowsingContexts = this.watcherActor.browserElement
+      ? [this.watcherActor.browserElement.browsingContext]
+      : getAllRemoteBrowsingContexts();
+    // Filter browsing contexts to only have the top BrowsingContext of each tree of BrowsingContexts…
+    const topLevelBrowsingContexts = allBrowsingContexts.filter(
+      browsingContext => browsingContext.top == browsingContext
+    );
+
+    // Only register one WebProgressListener per BrowsingContext tree.
+    // We will be notified about children BrowsingContext navigations/state changes via the top level BrowsingContextWebProgressListener,
+    // and BrowsingContextWebProgress.browsingContext attribute will be updated dynamically everytime
+    // we get notified about a child BrowsingContext.
+    // Note that regular web page toolbox will only have one BrowsingContext tree, for the given tab.
+    // But the Browser Toolbox will have many trees to listen to, one per top-level Window, and also one per tab,
+    // as tabs's BrowsingContext context aren't children of their top level window!
+    //
+    // Also save the WebProgress and not the BrowsingContext because `BrowsingContext.webProgress` will be undefined in destroy(),
+    // while it is still valuable to call `webProgress.removeProgressListener`. Otherwise events keeps being notified!!
+    this.webProgresses = topLevelBrowsingContexts.map(
+      browsingContext => browsingContext.webProgress
+    );
+    this.webProgresses.forEach(webProgress => {
+      webProgress.addProgressListener(
+        this,
+        Ci.nsIWebProgress.NOTIFY_STATE_DOCUMENT
+      );
+    });
+  }
+
+  onStateChange(progress, request, flag, status) {
+    const isStart = flag & Ci.nsIWebProgressListener.STATE_START;
+    const isDocument = flag & Ci.nsIWebProgressListener.STATE_IS_DOCUMENT;
+    if (isDocument && isStart) {
+      const { browsingContext } = progress;
+
+      // Ignore navigation for same-process iframes
+      if (!browsingContext.currentWindowGlobal.isProcessRoot) {
+        return;
+      }
+      // Ignore if we are still on the initial document,
+      // as that's the navigation from it (about:blank) to the actual first location.
+      // The target isn't created yet.
+      if (browsingContext.currentWindowGlobal.isInitialDocument) {
+        return;
+      }
+
+      const newURI = request instanceof Ci.nsIChannel ? request.URI.spec : null;
+      this.onAvailable([
+        {
+          browsingContextID: browsingContext.id,
+          resourceType: DOCUMENT_EVENT,
+          name: "will-navigate",
+          time: Date.now() - WILL_NAVIGATE_TIME_SHIFT,
+          shouldBeIgnoredAsRedundantWithTargetAvailable: false,
+          isFrameSwitching: false,
+          newURI,
+        },
+      ]);
+    }
+  }
+
+  get QueryInterface() {
+    return ChromeUtils.generateQI([
+      "nsIWebProgressListener",
+      "nsISupportsWeakReference",
+    ]);
+  }
+
+  destroy() {
+    this.webProgresses.forEach(webProgress => {
+      webProgress.removeProgressListener(
+        this,
+        Ci.nsIWebProgress.NOTIFY_STATE_DOCUMENT
+      );
+    });
+    this.webProgresses = null;
+  }
+}
+
+module.exports = ParentProcessDocumentEventWatcher;
