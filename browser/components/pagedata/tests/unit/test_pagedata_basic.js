660210e5ce90dc8bbddc2eb1e057a7a9644c2633
Dave Townsend <dtownsend@oxymoronical.com>
2021-07-26 12:51:42 +0000

    Bug 1721647: Add a stub page data service API to enable parallel development. r=mak
    This adds the stub API for the page data service and some basic docs. The service can be used from
    xpcshell tests and the events respond with sane data. As there are no consumers currently the
    in-memory cache never clears.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120498

---
+++ browser/components/pagedata/tests/unit/test_pagedata_basic.js
@@ -0,0 +1,61 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/*
+ * Simply tests that the notification is dispatched when new page data is
+ * discovered.
+ */
+
+const { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
+
+XPCOMUtils.defineLazyModuleGetters(this, {
+  Services: "resource://gre/modules/Services.jsm",
+  PageDataService: "resource:///modules/PageDataService.jsm",
+});
+
+add_task(async function notifies() {
+  let url = Services.io.newURI("https://www.mozilla.org/");
+
+  Assert.equal(
+    PageDataService.getCached(url),
+    null,
+    "Should be no cached data."
+  );
+
+  let listener = () => {
+    Assert.ok(false, "Should not notify for no data.");
+  };
+
+  PageDataService.on("page-data", listener);
+
+  let pageData = await PageDataService.queueFetch(url);
+  Assert.equal(pageData.url.spec, "https://www.mozilla.org/");
+  Assert.equal(pageData.data.length, 0);
+
+  pageData = PageDataService.getCached(url);
+  Assert.equal(pageData.url.spec, "https://www.mozilla.org/");
+  Assert.equal(pageData.data.length, 0);
+
+  PageDataService.off("page-data", listener);
+
+  let promise = PageDataService.once("page-data");
+
+  PageDataService.pageDataDiscovered(url, [
+    {
+      type: "product",
+      data: {
+        price: 276,
+      },
+    },
+  ]);
+
+  pageData = await promise;
+  Assert.equal(pageData.url.spec, "https://www.mozilla.org/");
+  Assert.equal(pageData.data.length, 1);
+  Assert.equal(pageData.data[0].type, "product");
+
+  Assert.equal(PageDataService.getCached(url), pageData);
+  Assert.equal(await PageDataService.queueFetch(url), pageData);
+});
--- browser/docs/index.rst
+++ browser/docs/index.rst
@@ -14,6 +14,7 @@ This is the nascent documentation of the Firefox front-end code.
    components/newtab/docs/index
    installer/windows/installer/index
    /toolkit/mozapps/defaultagent/default-browser-agent/index
+   components/pagedata/docs/index
    components/newtab/content-src/asrouter/docs/index
    search/index
    base/sslerrorreport/index
