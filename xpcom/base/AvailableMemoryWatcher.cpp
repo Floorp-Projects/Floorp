66b55bf0122f23caeb313d6af3733fa2e98bf874
Haik Aftandilian <haftandilian@mozilla.com>
2021-07-27 16:17:43 +0000

    Bug 1714382 - Include the Mac memory pressure state in crash reports r=spohl,KrisWright
    Subscribe to memory pressure events on macOS and add crash report annotations to parent and content process crash reports that can be used to determine if the system was under memory pressure at the time of the crash.
    
    Include the memory pressure level reported via the DISPATCH_SOURCE_TYPE_MEMORYPRESSURE dispatch with timestamps of transitions, the memory pressure level as read from the kern.memorystatus_vm_pressure_level sysctl, and a measurement of the percentage of available memory in the system read from the kern.memorystatus_level sysctl.
    
    Differential Revision: https://phabricator.services.mozilla.com/D116725

--- xpcom/base/AvailableMemoryWatcher.cpp
+++ xpcom/base/AvailableMemoryWatcher.cpp
@@ -112,7 +112,7 @@ void nsAvailableMemoryWatcherBase::RecordTelemetryEventOnHighMemory() {
 
 // Define the fallback method for a platform for which a platform-specific
 // CreateAvailableMemoryWatcher() is not defined.
-#if !defined(XP_WIN)
+#if !defined(XP_WIN) && !defined(XP_MACOSX)
 already_AddRefed<nsAvailableMemoryWatcherBase> CreateAvailableMemoryWatcher() {
   RefPtr instance(new nsAvailableMemoryWatcherBase);
   return do_AddRef(instance);
