a3ebbd06a60dbb0c31a62d445636eec29f63247b
Julien Wajsberg <felash@gmail.com>
2021-07-27 15:12:53 +0000

    Bug 1717047 - [profiler, netwerk] Support all redirections in the parent process r=canaltinova,necko-reviewers,dragana
    This moves the adding of the end marker for redirects in nsHttpChannel
    to SetupReplacementChannel, so that all redirects are properly caught.
    Without that, some requests will show as unfinished in the profiler
    frontend.
    
    Some of the redirects are internal and we may be able to flag and ignore
    them in the frontend, but that's work for the future.
    
    Because all redirections get a "REDIRECT" end marker, including the
    internal redirection to the service worker's intercepted channel, we now
    need an additional "START" marker there as well.
    
    All existing profiler tests related to service workers needed to be
    updated because there's an extra redirect marker in all these requests,
    as well as several pairs of markers that all have the same id.
    
    This also adds a new profiler test for handling the http->https case
    that we wouldn't catch before this patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118836

--- netwerk/protocol/http/InterceptedHttpChannel.h
+++ netwerk/protocol/http/InterceptedHttpChannel.h
@@ -80,6 +80,11 @@ class InterceptedHttpChannel final
   RefPtr<nsInputStreamPump> mPump;
   TimeStamp mFinishResponseStart;
   TimeStamp mFinishResponseEnd;
+  TimeStamp mInterceptedChannelCreationTimestamp;
+
+  // For the profiler markers
+  TimeStamp mLastStatusReported;
+
   Atomic<int64_t> mProgress;
   int64_t mProgressReported;
   int64_t mSynthesizedStreamLength;
