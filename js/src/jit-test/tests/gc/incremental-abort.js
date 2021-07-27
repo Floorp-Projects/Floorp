7b50cdc815d8770123946949db20ecd49545f791
Jon Coppeard <jcoppeard@mozilla.com>
2021-07-27 08:03:14 +0000

    Bug 1721838 - Ensure we cancel the GC slice triggered when a background task finishes if we waited for the task on the main thread r=sfink
    If we don't do this we get an extra unnecessary GC slice.
    
    This bug was introduced because I changed GCRuntime::waitForBackgroundTask to
    have two places where we wait but the check only happened on one of them.
    
    I tidied up the test code but there are no actual changes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120829

--- js/src/jit-test/tests/gc/incremental-abort.js
+++ js/src/jit-test/tests/gc/incremental-abort.js
@@ -5,45 +5,46 @@
 gczeal(0);
 gc();
 
-function testAbort(zoneCount, objectCount, sliceCount, abortState)
+// Allocate objectCount objects in zoneCount zones and run a incremental
+// shrinking GC with slices with a work budget of sliceBudget until we reach
+// GC state abortState at which point, abort the GC.
+function testAbort(zoneCount, objectCount, sliceBudget, abortState)
 {
-    // Allocate objectCount objects in zoneCount zones and run a incremental
-    // shrinking GC.
 
-    var zones = [];
-    for (var i = 0; i < zoneCount; i++) {
-        var zone = newGlobal({newCompartment: true});
-        evaluate("var objects; " +
-                 "function makeObjectGraph(objectCount) { " +
-                 "    objects = []; " +
-                 "    for (var i = 0; i < objectCount; i++) " +
-                 "        objects.push({i: i}); " +
-                "}",
-                 { global: zone });
-        zone.makeObjectGraph(objectCount);
-        zones.push(zone);
+  var zones = [];
+  for (var i = 0; i < zoneCount; i++) {
+    var zone = newGlobal({newCompartment: true});
+    evaluate("var objects; " +
+             "function makeObjectGraph(objectCount) { " +
+             "    objects = []; " +
+             "    for (var i = 0; i < objectCount; i++) " +
+             "        objects.push({i: i}); " +
+             "}",
+             { global: zone });
+    zone.makeObjectGraph(objectCount);
+    zones.push(zone);
+  }
+
+  gc();
+
+  var didAbort = false;
+  startgc(sliceBudget, "shrinking");
+  assertEq(currentgc().isShrinking, true);
+  while (gcstate() !== "NotActive") {
+    if (gcstate() == abortState) {
+      abortgc();
+      didAbort = true;
+      break;
     }
 
-    gc();
+    gcslice(sliceBudget);
+  }
 
-    var didAbort = false;
-    startgc(sliceCount, "shrinking");
-    assertEq(currentgc().isShrinking, true);
-    while (gcstate() !== "NotActive") {
-        if (gcstate() == abortState) {
-            abortgc();
-            didAbort = true;
-            break;
-        }
+  assertEq(gcstate(), "NotActive");
+  if (abortState)
+    assertEq(didAbort, true);
 
-        gcslice(sliceCount);
-    }
-
-    assertEq(gcstate(), "NotActive");
-    if (abortState)
-        assertEq(didAbort, true);
-
-    return zones;
+  return zones;
 }
 
 gczeal(0);
@@ -51,5 +52,6 @@ testAbort(10, 10000, 10000);
 testAbort(10, 10000, 10000, "Mark");
 testAbort(10, 10000, 1000, "Sweep");
 testAbort(10, 10000, 10000, "Compact");
-// Note: we do not yield automatically before Finalize or Decommit, as they yield internally.
-// Thus, we may not witness an incremental state in this phase and cannot test it explicitly.
+// Note: we do not yield automatically before Finalize or Decommit, as they
+// yield internally.  Thus, we may not witness an incremental state in this
+// phase and cannot test it explicitly.
