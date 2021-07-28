2a1c2621cbf9a6028d74fa8941b19b56f54362a9
Alexandre Poirot <poirot.alex@gmail.com>
2021-07-28 11:42:27 +0000

    Bug 1722545 - [devtools] Disable server targets in browser_perf-docload.js/browser_perf-refresh.js. r=nchevobbe
    The old performance panel behaves quite differently in case of target switching.
    It will open a brand new profile for the next test, which confuses these tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120990

--- devtools/client/performance/test/browser_perf-refresh.js
+++ devtools/client/performance/test/browser_perf-refresh.js
@@ -21,6 +21,14 @@ const {
 } = require("devtools/client/performance/test/helpers/wait-utils");
 
 add_task(async function() {
+  // Run this test without server targets as reload would introduce a target switch
+  // with a new profile record which would clear previous page markers.
+  // This panel has now been deprecated, we might later get rid of this test
+  // once removing client side targets (bug 1721852)
+  await SpecialPowers.pushPrefEnv({
+    set: [["devtools.target-switching.server.enabled", false]],
+  });
+
   const { panel } = await initPerformanceInNewTab({
     url: SIMPLE_URL,
     win: window,
