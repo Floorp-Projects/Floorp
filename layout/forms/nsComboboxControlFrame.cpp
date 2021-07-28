8b74ff00e0e43e4f1435ecae1f044d1092b12cff
Ting-Yu Lin <tlin@mozilla.com>
2021-07-28 18:41:19 +0000

    Bug 1716212 Part 2 - Carry nsComboboxControlFrame's line-height to nsComboboxDisplayFrame when it has auto block-size. r=emilio
    The issue is that combobox display may contain some non-Latin characters
    that need extra block-size to display than the one line-height
    calculate by using a Latin font spec in combobox control's style.
    
    Before this patch, when a combobox control has unconstrained block-size,
    we set combobox display's block-size to combobox control's one
    line-height in Reflow(), which is intended to properly initialize
    `BlockReflowInput::mMinLineHeight` since combobox display has
    `line-height:-moz-block-height`. However, this simply prevents the
    combobox display from choosing a larger block-size after the reflow. See
    bug 1716212 comment 11 for an analysis.
    
    This patch fixes the issue by carrying combox control's computed line
    height to combobox display so that its computed block-size is still
    unconstrained so that it can accommodate taller characters in the
    display text.
    
    After this patch, <select><option> containing non-Latin characters
    should have the same block-size as <button>, and no characters should be
    clipped. Modified test_unstyled_control_height.html to test this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120877

--- layout/forms/nsComboboxControlFrame.cpp
+++ layout/forms/nsComboboxControlFrame.cpp
@@ -1245,18 +1245,13 @@ void nsComboboxDisplayFrame::Reflow(nsPresContext* aPresContext,
                                     const ReflowInput& aReflowInput,
                                     nsReflowStatus& aStatus) {
   MOZ_ASSERT(aStatus.IsEmpty(), "Caller should pass a fresh reflow status!");
+  MOZ_ASSERT(aReflowInput.mParentReflowInput &&
+                 aReflowInput.mParentReflowInput->mFrame == mComboBox,
+             "Combobox's frame tree is wrong!");
 
   ReflowInput state(aReflowInput);
   if (state.ComputedBSize() == NS_UNCONSTRAINEDSIZE) {
-    float inflation = nsLayoutUtils::FontSizeInflationFor(mComboBox);
-    // We intentionally use the combobox frame's style here, which has
-    // the 'line-height' specified by the author, if any.
-    // (This frame has 'line-height: -moz-block-height' in the UA
-    // sheet which is suitable when there's a specified block-size.)
-    nscoord lh = ReflowInput::CalcLineHeight(mComboBox->GetContent(),
-                                             mComboBox->Style(), aPresContext,
-                                             NS_UNCONSTRAINEDSIZE, inflation);
-    state.SetComputedBSize(lh);
+    state.SetLineHeight(state.mParentReflowInput->GetLineHeight());
   }
   const WritingMode wm = aReflowInput.GetWritingMode();
   const LogicalMargin bp = state.ComputedLogicalBorderPadding(wm);
