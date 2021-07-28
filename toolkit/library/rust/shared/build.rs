8c7bef10e4c31492b745ba3d7a5281d32b2aacb7
Mike Hommey <mh+mozilla@glandium.org>
2021-07-28 20:33:08 +0000

    Bug 1582382 - Simplify rust oom-hook related build script. r=firefox-build-system-reviewers,andi
    Bug 1715282 bumped the minimum rust version to 1.51, so we can remove
    anything that only applies to older versions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121031

--- toolkit/library/rust/shared/build.rs
+++ toolkit/library/rust/shared/build.rs
@@ -8,21 +8,12 @@ use rustc_version::{version, Version};
 
 fn main() {
     let ver = version().unwrap();
-    let mut bootstrap = false;
     let max_oom_hook_version = Version::parse("1.55.0-alpha").unwrap();
 
-    if ver >= Version::parse("1.28.0-alpha").unwrap() && ver < max_oom_hook_version {
+    if ver < max_oom_hook_version {
         println!("cargo:rustc-cfg=feature=\"oom_with_hook\"");
-        bootstrap = true;
     } else if std::env::var("MOZ_AUTOMATION").is_ok() {
         panic!("Builds on automation must use a version of rust for which we know how to hook OOM: want < {}, have {}",
                max_oom_hook_version, ver);
-    }
-
-    // This is a rather awful thing to do, but we're only doing it on
-    // versions of rustc that are not going to change the unstable APIs
-    // we use from under us, all being already released or beta.
-    if bootstrap && ver < Version::parse("1.50.0").unwrap() {
-        println!("cargo:rustc-env=RUSTC_BOOTSTRAP=1");
     }
 }
