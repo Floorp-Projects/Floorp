a8f1cd2ddd3b358bd644b9075e658fd17a5c05c5
Mark Banner <standard8@mozilla.com>
2021-07-28 15:26:48 +0000

    Bug 1722275 - Add a basic framework for collecting page data from loaded pages. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D120825

--- browser/components/pagedata/tests/unit/test_pagedata_basic.js
+++ browser/components/pagedata/tests/unit/test_pagedata_basic.js
@@ -24,22 +24,6 @@ add_task(async function notifies() {
     "Should be no cached data."
   );
 
-  let listener = () => {
-    Assert.ok(false, "Should not notify for no data.");
-  };
-
-  PageDataService.on("page-data", listener);
-
-  let pageData = await PageDataService.queueFetch(url);
-  Assert.equal(pageData.url, "https://www.mozilla.org/");
-  Assert.equal(pageData.data.length, 0);
-
-  pageData = PageDataService.getCached(url);
-  Assert.equal(pageData.url, "https://www.mozilla.org/");
-  Assert.equal(pageData.data.length, 0);
-
-  PageDataService.off("page-data", listener);
-
   let promise = PageDataService.once("page-data");
 
   PageDataService.pageDataDiscovered(url, [
@@ -51,11 +35,28 @@ add_task(async function notifies() {
     },
   ]);
 
-  pageData = await promise;
-  Assert.equal(pageData.url, "https://www.mozilla.org/");
-  Assert.equal(pageData.data.length, 1);
-  Assert.equal(pageData.data[0].type, "product");
+  let pageData = await promise;
+  Assert.equal(
+    pageData.url,
+    "https://www.mozilla.org/",
+    "Should have notified data for the expected url"
+  );
+  Assert.deepEqual(
+    pageData.data,
+    [
+      {
+        type: "product",
+        data: {
+          price: 276,
+        },
+      },
+    ],
+    "Should have returned the correct product data"
+  );
 
-  Assert.equal(PageDataService.getCached(url), pageData);
-  Assert.equal(await PageDataService.queueFetch(url), pageData);
+  Assert.deepEqual(
+    PageDataService.getCached(url),
+    pageData,
+    "Should return the same pageData from the cache as was notified."
+  );
 });
