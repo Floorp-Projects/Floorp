91205cacba47f6a1fa8259a80c8b03bf693b3e6d
Mike Hommey <mh+mozilla@glandium.org>
2021-07-28 20:33:09 +0000

    Bug 1582382 - Enable the new rust hooks in standalone spidermonkey. r=jandem
    - Enable the malloc hook via the moz_memory feature.
    - Install the rust oom hook during VM initialization.
    - And only install the panic hook when building spidermonkey standalone,
    because Gecko already takes care of it otherwise.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121034

--- js/src/vm/Initialization.cpp
+++ js/src/vm/Initialization.cpp
@@ -113,6 +113,7 @@ static void SetupCanonicalNaN() {
   } while (0)
 
 extern "C" void install_rust_panic_hook();
+extern "C" void install_rust_oom_hook();
 
 JS_PUBLIC_API const char* JS::detail::InitWithFailureDiagnostic(
     bool isDebugBuild) {
@@ -131,7 +132,11 @@ JS_PUBLIC_API const char* JS::detail::InitWithFailureDiagnostic(
 
   libraryInitState = InitState::Initializing;
 
+#ifdef JS_STANDALONE
+  // The rust hooks are initialized by Gecko on non-standalone builds.
   install_rust_panic_hook();
+  install_rust_oom_hook();
+#endif
 
   PRMJ_NowInit();
 
