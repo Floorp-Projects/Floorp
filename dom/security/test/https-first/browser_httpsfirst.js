0e31259b5935c2df7c46ede9ba7b11e53d3400b9
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2021-07-28 16:08:07 +0000

    Bug 1719272: Annotate failing browser/ tests to potentially enable https-first mode in Nightly r=webcompat-reviewers,denschub,webdriver-reviewers,preferences-reviewers,Gijs,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D119177

--- dom/security/test/https-first/browser_httpsfirst.js
+++ dom/security/test/https-first/browser_httpsfirst.js
@@ -27,6 +27,10 @@ async function runPrefTest(aURI, aDesc, aAssertURLStartsWith) {
 }
 
 add_task(async function() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["dom.security.https_first", false]],
+  });
+
   await runPrefTest(
     "http://example.com",
     "HTTPS-First disabled; Should not upgrade",
