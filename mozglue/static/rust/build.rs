d95d0e08900732cf8fbf5408de49dfb8901c35fa
Mike Hommey <mh+mozilla@glandium.org>
2021-07-28 20:33:08 +0000

    Bug 1582382 - Move rust malloc and oom hook to mozglue. r=firefox-build-system-reviewers,andi
    This is similar to what was done in bug 1594998 for the panic hook.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121032

--- mozglue/static/rust/build.rs
+++ mozglue/static/rust/build.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use rustc_version::{version, Version};
 use std::env;
 use std::path::PathBuf;
 
@@ -35,4 +36,14 @@ fn main() {
     build.file("wrappers.cpp");
     build.compile("wrappers");
     println!("cargo:rerun-if-changed=wrappers.cpp");
+
+    let ver = version().unwrap();
+    let max_oom_hook_version = Version::parse("1.55.0-alpha").unwrap();
+
+    if ver < max_oom_hook_version {
+        println!("cargo:rustc-cfg=feature=\"oom_with_hook\"");
+    } else if std::env::var("MOZ_AUTOMATION").is_ok() {
+        panic!("Builds on automation must use a version of rust for which we know how to hook OOM: want < {}, have {}",
+               max_oom_hook_version, ver);
+    }
 }
