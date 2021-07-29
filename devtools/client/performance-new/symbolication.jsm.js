5e26bd2b5872f24815fcbef640eb8cf7c4d2b515
Markus Stange <mstange.moz@gmail.com>
2021-07-29 20:28:41 +0000

    Bug 1721109 - Check all local files in the same loop, both files in objdirs and the regular absolute path of the library. r=canaltinova
    This may look odd now but it's a temporary step on the way to the final state.
    
    Depends on D120173
    
    Differential Revision: https://phabricator.services.mozilla.com/D120174

--- devtools/client/performance-new/symbolication.jsm.js
+++ devtools/client/performance-new/symbolication.jsm.js
@@ -59,52 +59,53 @@ async function doesFileExistAtPath(path) {
 }
 
 /**
- * Retrieve a symbol table from a binary on the host machine, by looking up
- * relevant build artifacts in the specified objdirs.
- * This is needed if the debuggee is a build running on a remote machine that
- * was compiled by the developer on *this* machine (the "host machine"). In
- * that case, the objdir will contain the compiled binary with full symbol and
- * debug information, whereas the binary on the device may not exist in
- * uncompressed form or may have been stripped of debug information and some
- * symbol information.
- * An objdir, or "object directory", is a directory on the host machine that's
- * used to store build artifacts ("object files") from the compilation process.
+ * Enumerate all paths at which we could find files with symbol information.
  *
  * @param {string[]} objdirs An array of objdir paths on the host machine
  *   that should be searched for relevant build artifacts.
- * @param {string} filename The file name of the binary.
- * @param {string} breakpadId The breakpad ID of the binary.
- * @returns {Promise<SymbolTableAsTuple>} The symbol table of the first encountered binary with a
- *   matching breakpad ID, in SymbolTableAsTuple format. An exception is thrown (the
- *   promise is rejected) if nothing was found.
+ * @param {Library} lib Information about the library.
+ * @returns {Array<{ path: string, debugPath: string }>}
  */
-async function getSymbolTableFromLocalBinary(objdirs, filename, breakpadId) {
+function getCandidatePaths(objdirs, lib) {
+  const { name, path, debugPath } = lib;
   const { OS } = lazy.OS();
   const candidatePaths = [];
+
+  // First, try to find a binary with a matching file name and breakpadId
+  // in one of the manually specified objdirs.
+  // This is needed if the debuggee is a build running on a remote machine that
+  // was compiled by the developer on *this* machine (the "host machine"). In
+  // that case, the objdir will contain the compiled binary with full symbol and
+  // debug information, whereas the binary on the device may not exist in
+  // uncompressed form or may have been stripped of debug information and some
+  // symbol information.
+  // An objdir, or "object directory", is a directory on the host machine that's
+  // used to store build artifacts ("object files") from the compilation process.
+  // This only works if the binary is one of the Gecko binaries and not
+  // a system library.
   for (const objdirPath of objdirs) {
     // Binaries are usually expected to exist at objdir/dist/bin/filename.
-    candidatePaths.push(OS.Path.join(objdirPath, "dist", "bin", filename));
+    candidatePaths.push({
+      path: OS.Path.join(objdirPath, "dist", "bin", name),
+      debugPath: OS.Path.join(objdirPath, "dist", "bin", name),
+    });
     // Also search in the "objdir" directory itself (not just in dist/bin).
     // If, for some unforeseen reason, the relevant binary is not inside the
     // objdirs dist/bin/ directory, this provides a way out because it lets the
     // user specify the actual location.
-    candidatePaths.push(OS.Path.join(objdirPath, filename));
+    candidatePaths.push({
+      path: OS.Path.join(objdirPath, name),
+      debugPath: OS.Path.join(objdirPath, name),
+    });
   }
 
-  for (const path of candidatePaths) {
-    if (await doesFileExistAtPath(path)) {
-      const { ProfilerGetSymbols } = lazy.ProfilerGetSymbols();
-      try {
-        return await ProfilerGetSymbols.getSymbolTable(path, path, breakpadId);
-      } catch (e) {
-        // ProfilerGetSymbols.getSymbolTable was unsuccessful. So either the
-        // file wasn't parseable or its contents didn't match the specified
-        // breakpadId, or some other error occurred.
-        // Advance to the next candidate path.
-      }
-    }
-  }
-  throw new Error("Could not find any matching binary.");
+  // Check the absolute paths of the library file(s) last.
+  // We do this after the objdir search because the library's path may point
+  // to a stripped binary, which will have fewer symbols than the original
+  // binaries in the objdir.
+  candidatePaths.push({ path, debugPath });
+
+  return candidatePaths;
 }
 
 /**
@@ -124,51 +125,58 @@ async function getSymbolTableFromLocalBinary(objdirs, filename, breakpadId) {
  *   should be searched for relevant build artifacts.
  * @param {PerfFront | undefined} perfFront - The perfFront for a remote debugging
  *   connection, or undefined when profiling this browser.
- * @return {Promise<SymbolTableAsTuple>}
+ * @return {Promise<SymbolTableAsTuple>} The symbol table of the first encountered binary with a
+ *   matching breakpad ID, in SymbolTableAsTuple format. An exception is thrown (the
+ *   promise is rejected) if nothing was found.
  */
 async function getSymbolTableMultiModal(lib, objdirs, perfFront = undefined) {
-  const { name, debugName, path, debugPath, breakpadId } = lib;
-  try {
-    // First, try to find a binary with a matching file name and breakpadId
-    // in one of the manually specified objdirs. If the profiled build was
-    // compiled locally, and matches the build artifacts in the objdir, then
-    // this gives the best results because the objdir build always has full
-    // symbol information.
-    // This only works if the binary is one of the Gecko binaries and not
-    // a system library.
-    return await getSymbolTableFromLocalBinary(objdirs, name, breakpadId);
-  } catch (errorWhenCheckingObjdirs) {
-    // Couldn't find a matching build in one of the objdirs. Search elsewhere.
+  const { debugName, breakpadId } = lib;
+
+  // First, enumerate the local paths at which we could find binaries (and, on
+  // Windows, PDB files) which could contain symbol information.
+  const candidatePaths = getCandidatePaths(objdirs, lib);
+
+  // Iterate over all the paths and try to get symbols from each entry.
+  const { ProfilerGetSymbols } = lazy.ProfilerGetSymbols();
+  for (const { path, debugPath } of candidatePaths) {
     if (await doesFileExistAtPath(path)) {
-      const { ProfilerGetSymbols } = lazy.ProfilerGetSymbols();
-      // This profile was probably obtained from this machine, and not from a
-      // different device (e.g. an Android phone). Dump symbols from the file
-      // on this machine directly.
-      return ProfilerGetSymbols.getSymbolTable(path, debugPath, breakpadId);
+      try {
+        return await ProfilerGetSymbols.getSymbolTable(
+          path,
+          debugPath,
+          breakpadId
+        );
+      } catch (e) {
+        // ProfilerGetSymbols.getSymbolTable was unsuccessful. So either the
+        // file wasn't parseable or its contents didn't match the specified
+        // breakpadId, or some other error occurred.
+        // Advance to the next candidate path.
+      }
     }
-    // No file exists at the path on this machine, which probably indicates
-    // that the profile was obtained on a different machine, i.e. the debuggee
-    // is truly remote (e.g. on an Android phone).
-    if (!perfFront) {
-      // No remote connection - we really needed the file at path.
-      throw new Error(
-        `Could not obtain symbols for the library ${debugName} ${breakpadId} ` +
-          `because there was no file at the given path "${path}". Furthermore, ` +
-          `looking for symbols in the given objdirs failed: ${errorWhenCheckingObjdirs.message}`
-      );
-    }
-    // Try to obtain the symbol table on the debuggee. We get into this
-    // branch in the following cases:
-    //  - Android system libraries
-    //  - Firefox binaries that have no matching equivalent on the host
-    //    machine, for example because the user didn't point us at the
-    //    corresponding objdir, or if the build was compiled somewhere
-    //    else, or if the build on the device is outdated.
-    // For now, the "debuggee" is never a Windows machine, which is why we don't
-    // need to pass the library's debugPath. (path and debugPath are always the
-    // same on non-Windows.)
-    return getSymbolTableFromDebuggee(perfFront, path, breakpadId);
   }
+
+  // No local file was found to obtain symbols from.
+  // This can happen if the  debuggee is truly remote (e.g. on an Android phone).
+  if (!perfFront) {
+    throw new Error(
+      `Could not obtain symbols for the library ${debugName} ${breakpadId} ` +
+        `because there was no matching file at any of the candidate paths: ${JSON.stringify(
+          candidatePaths
+        )}`
+    );
+  }
+
+  // Try to obtain the symbol table on the debuggee. We get into this
+  // branch in the following cases:
+  //  - Android system libraries
+  //  - Firefox binaries that have no matching equivalent on the host
+  //    machine, for example because the user didn't point us at the
+  //    corresponding objdir, or if the build was compiled somewhere
+  //    else, or if the build on the device is outdated.
+  // For now, the "debuggee" is never a Windows machine, which is why we don't
+  // need to pass the library's debugPath. (path and debugPath are always the
+  // same on non-Windows.)
+  return getSymbolTableFromDebuggee(perfFront, lib.path, breakpadId);
 }
 
 /**
@@ -265,9 +273,6 @@ function createLocalSymbolicationService(sharedLibraries, objdirs, perfFront) {
 
 module.exports = {
   createLocalSymbolicationService,
-  getSymbolTableFromDebuggee,
-  getSymbolTableFromLocalBinary,
-  getSymbolTableMultiModal,
 };
 
 // Object.keys() confuses the linting which expects a static array expression.
