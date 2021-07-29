b987c2c96df890ce987c87c18456ef2572524eb8
Ted Campbell <tcampbell@mozilla.com>
2021-07-29 01:53:35 +0000

    Bug 1688794 - Run self-hosted top-level in target realm instead of cloning. r=jandem
    The top-level script for selfhosted.js defines a few builtin values when it
    executes. Instead of relying on cloning these objects into normal realms, this
    patch now executes the script in the target realm itself. We run this script on
    demand when we are first looking up a builtin that is neither a C++ intrinsic,
    nor a self-hosted function defined in the stencil. In practice, this is not run
    except when certain Intl APIs are used in a realm.
    
    Work around the testGCOutOfMemory test now that there is no initial nursery
    usage during startup. This test case should be improved in future.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120546

--- js/src/frontend/Stencil.h
+++ js/src/frontend/Stencil.h
@@ -251,7 +251,10 @@ class ScopeStencil {
   // The kind determines the corresponding BaseParserScopeData.
   ScopeKind kind_{UINT8_MAX};
 
-  // True if this scope has enclosing scope.
+  // True if this scope has enclosing scope stencil. Otherwise, the enclosing
+  // scope will be read from CompilationInput while instantiating. Self-hosting
+  // is a special case and will use `emptyGlobalScope` when there is no
+  // enclosing scope stencil.
   static constexpr uint8_t HasEnclosing = 1 << 0;
 
   // If true, an environment Shape must be created. The shape itself may
