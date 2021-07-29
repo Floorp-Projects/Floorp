bd4783085feb03285de682f3ddfb632986f75e03
Markus Stange <mstange.moz@gmail.com>
2021-07-29 20:28:47 +0000

    Bug 1721109 - Move candidate path handling and lib info map lookup into the worker. r=canaltinova
    This prepares the worker for an upcoming update to a new profiler-get-symbols version;
    this way of doing things is a much better match for the new API.
    
    For requesting a single symbol table, this may look unnecessarily complicated, and it is.
    But it paves the way for requesting symbols for multiple libraries in a single message.
    In the future, rather than requesting a full symbol table for a single library, we will
    want to have a queryAPI endpoint which lets us request symbols for multiple libraries
    with a single message. This is just elaborate preparation for that step.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120188

--- devtools/client/performance-new/symbolication-worker.js
+++ devtools/client/performance-new/symbolication-worker.js
@@ -82,6 +82,69 @@ function createPlainErrorObject(e) {
   };
 }
 
+class PathHelper {
+  constructor(libInfoMap, objdirs) {
+    this._libInfoMap = libInfoMap;
+    this._objdirs = objdirs;
+  }
+
+  /**
+   * Enumerate all paths at which we could find files with symbol information.
+   *
+   * @param {string} debugName
+   * @param {string} breakpadId
+   * @returns {Array<{ path: string, debugPath: string }>}
+   */
+  getCandidatePaths(debugName, breakpadId) {
+    const key = `${debugName}:${breakpadId}`;
+    const lib = this._libInfoMap.get(key);
+    if (!lib) {
+      throw new Error(
+        `Could not find the library for "${debugName}", "${breakpadId}".`
+      );
+    }
+
+    const { name, path, debugPath } = lib;
+    const candidatePaths = [];
+
+    // First, try to find a binary with a matching file name and breakpadId
+    // in one of the manually specified objdirs.
+    // This is needed if the debuggee is a build running on a remote machine that
+    // was compiled by the developer on *this* machine (the "host machine"). In
+    // that case, the objdir will contain the compiled binary with full symbol and
+    // debug information, whereas the binary on the device may not exist in
+    // uncompressed form or may have been stripped of debug information and some
+    // symbol information.
+    // An objdir, or "object directory", is a directory on the host machine that's
+    // used to store build artifacts ("object files") from the compilation process.
+    // This only works if the binary is one of the Gecko binaries and not
+    // a system library.
+    for (const objdirPath of this._objdirs) {
+      // Binaries are usually expected to exist at objdir/dist/bin/filename.
+      candidatePaths.push({
+        path: OS.Path.join(objdirPath, "dist", "bin", name),
+        debugPath: OS.Path.join(objdirPath, "dist", "bin", name),
+      });
+      // Also search in the "objdir" directory itself (not just in dist/bin).
+      // If, for some unforeseen reason, the relevant binary is not inside the
+      // objdirs dist/bin/ directory, this provides a way out because it lets the
+      // user specify the actual location.
+      candidatePaths.push({
+        path: OS.Path.join(objdirPath, name),
+        debugPath: OS.Path.join(objdirPath, name),
+      });
+    }
+
+    // Check the absolute paths of the library file(s) last.
+    // We do this after the objdir search because the library's path may point
+    // to a stripped binary, which will have fewer symbols than the original
+    // binaries in the objdir.
+    candidatePaths.push({ path, debugPath });
+
+    return candidatePaths;
+  }
+}
+
 function getCompactSymbolTableFromPath(binaryPath, debugPath, breakpadId) {
   // Read the binary file into WASM memory.
   const binaryFile = OS.File.open(binaryPath, { read: true });
@@ -118,10 +181,35 @@ function getCompactSymbolTableFromPath(binaryPath, debugPath, breakpadId) {
   }
 }
 
+function getSymbolTable(debugName, breakpadId, libInfoMap, objdirs) {
+  const helper = new PathHelper(libInfoMap, objdirs);
+  const candidatePaths = helper.getCandidatePaths(debugName, breakpadId);
+
+  const errors = [];
+  for (const { path, debugPath } of candidatePaths) {
+    try {
+      return getCompactSymbolTableFromPath(path, debugPath, breakpadId);
+    } catch (e) {
+      // getCompactSymbolTableFromPath was unsuccessful. So either the
+      // file wasn't parseable or its contents didn't match the specified
+      // breakpadId, or some other error occurred.
+      // Advance to the next candidate path.
+      errors.push(e);
+    }
+  }
+
+  throw new Error(
+    `Could not obtain symbols for the library ${debugName} ${breakpadId} ` +
+      `because there was no matching file at any of the candidate paths: ${JSON.stringify(
+        candidatePaths
+      )}. Errors: ${errors.map(e => e.message).join(", ")}`
+  );
+}
+
 /** @param {MessageEvent<SymbolicationWorkerInitialMessage>} e */
 onmessage = async e => {
   try {
-    const { binaryPath, debugPath, breakpadId, module } = e.data;
+    const { debugName, breakpadId, libInfoMap, objdirs, module } = e.data;
 
     if (!(module instanceof WebAssembly.Module)) {
       throw new Error("invalid WebAssembly module");
@@ -130,11 +218,7 @@ onmessage = async e => {
     // Instantiate the WASM module.
     await wasm_bindgen(module);
 
-    const result = getCompactSymbolTableFromPath(
-      binaryPath,
-      debugPath,
-      breakpadId
-    );
+    const result = getSymbolTable(debugName, breakpadId, libInfoMap, objdirs);
     postMessage(
       { result },
       result.map(r => r.buffer)
