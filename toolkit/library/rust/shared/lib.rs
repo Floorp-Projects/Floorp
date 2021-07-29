a5ebee57bb900a79655315e0d1f0a75d011d5f28
Noemi Erli <nerli@mozilla.com>
2021-07-30 00:21:00 +0300

    Backed out 2 changesets (bug 1720676) for causing remote failures CLOSED TREE
    Backed out changeset 3c2599c00332 (bug 1720676)
    Backed out changeset b58937c59f63 (bug 1720676)

--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -77,6 +77,9 @@ extern crate fluent_ffi;
 #[cfg(not(target_os = "android"))]
 extern crate viaduct;
 
+#[cfg(feature = "remote")]
+extern crate remote;
+
 extern crate gecko_logger;
 
 #[cfg(feature = "oxidized_breakpad")]
