c0d2f2bba4a324c29ce8c089f8c09750e111f046
Nazım Can Altınova <canaltinova@gmail.com>
2021-07-30 21:49:22 +0000

    Bug 1652560 - Add gecko_profiler_label API for adding label frames from Rust code r=emilio,gerald
    Differential Revision: https://phabricator.services.mozilla.com/D120793

--- tools/profiler/public/ProfilerBindings.h
+++ tools/profiler/public/ProfilerBindings.h
@@ -4,18 +4,31 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* FFI functions for Profiler Rust API to call into profiler */
+
 #ifndef ProfilerBindings_h
 #define ProfilerBindings_h
 
-/* FFI functions for Profiler Rust API to call into profiler */
+#include <stdint.h>
+
+namespace mozilla {
+class AutoProfilerLabel;
+}  // namespace mozilla
+
+namespace JS {
+enum class ProfilingCategoryPair : uint32_t;
+}  // namespace JS
 
 // Everything in here is safe to include unconditionally, implementations must
 // take !MOZ_GECKO_PROFILER into account.
-
 extern "C" {
 
 void gecko_profiler_register_thread(const char* aName);
 void gecko_profiler_unregister_thread();
+
+void gecko_profiler_construct_label(mozilla::AutoProfilerLabel* aAutoLabel,
+                                    JS::ProfilingCategoryPair aCategoryPair);
+void gecko_profiler_destruct_label(mozilla::AutoProfilerLabel* aAutoLabel);
 
 }  // extern "C"
 
