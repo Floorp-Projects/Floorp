4c7d81406ef2ef0169375b63138f0a50bfd55b2f
Zibi Braniecki <zbraniecki@mozilla.com>
2021-07-30 18:07:32 +0000

    Bug 1723187 - Force sync load of a file if async load is in progress. r=emilio a=nightly-fix
    Differential Revision: https://phabricator.services.mozilla.com/D121401

--- intl/l10n/rust/l10nregistry-ffi/src/source.rs
+++ intl/l10n/rust/l10nregistry-ffi/src/source.rs
@@ -263,7 +263,10 @@ pub extern "C" fn l10nfilesource_fetch_file_sync(
     };
 
     *status = L10nFileSourceStatus::None;
-    if let Some(res) = source.fetch_file_sync(&locale, &path.to_utf8(), false) {
+    //XXX: Bug 1723191 - if we encounter a request for sync load while async load is in progress
+    //                   we will discard the async load and force the sync load instead.
+    //                   There may be a better option but we haven't had time to explore it.
+    if let Some(res) = source.fetch_file_sync(&locale, &path.to_utf8(), /* overload */ true) {
         Rc::into_raw(res)
     } else {
         std::ptr::null()
