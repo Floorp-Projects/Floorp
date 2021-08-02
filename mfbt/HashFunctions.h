619d3b95b43408ac4af5f91011c8c38142bb533e
André Bargull <andre.bargull@gmail.com>
2021-08-02 16:38:00 +0000

    Bug 1341265 - Part 1: Remove stale fixme comment. r=iain
    Bug 775896 has been fixed, so we can remove the fixme comment.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118967

--- mfbt/HashFunctions.h
+++ mfbt/HashFunctions.h
@@ -79,8 +79,6 @@ static const HashNumber kGoldenRatioU32 = 0x9E3779B9U;
  * Since the leftmost bits of the result are best, the hash bucket index is
  * computed by doing ScrambleHashCode(h) / (2^32/N) or the equivalent
  * right-shift, not ScrambleHashCode(h) % N or the equivalent bit-mask.
- *
- * FIXME: OrderedHashTable uses a bit-mask; see bug 775896.
  */
 constexpr HashNumber ScrambleHashCode(HashNumber h) {
   /*
