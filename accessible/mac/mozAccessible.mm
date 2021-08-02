f57cc302766c01847b16329ff1ebc9a56ea5e6e9
Morgan Reschenberg <mreschenberg@mozilla.com>
2021-08-02 19:41:21 +0000

    Bug 1720334: Represent checked/unchecked state with AXValue for treeitems r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D121215

--- accessible/mac/mozAccessible.mm
+++ accessible/mac/mozAccessible.mm
@@ -111,7 +111,7 @@ using namespace mozilla::a11y;
 static const uint64_t kCachedStates =
     states::CHECKED | states::PRESSED | states::MIXED | states::EXPANDED |
     states::CURRENT | states::SELECTED | states::TRAVERSED | states::LINKED |
-    states::HASPOPUP | states::BUSY | states::MULTI_LINE;
+    states::HASPOPUP | states::BUSY | states::MULTI_LINE | states::CHECKABLE;
 static const uint64_t kCacheInitialized = ((uint64_t)0x1) << 63;
 
 - (uint64_t)state {
