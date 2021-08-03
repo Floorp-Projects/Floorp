bb443ae08f98ec23f2c191cc6f7e52652adc4696
Travis Long <tlong@mozilla.com>
2021-08-03 06:56:06 -0500

    [components] Update appservices to v82.1.0

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -29,7 +29,7 @@ object Versions {
     const val disklrucache = "2.0.2"
     const val leakcanary = "2.4"
 
-    const val mozilla_appservices = "81.0.1"
+    const val mozilla_appservices = "82.1.0"
 
     const val mozilla_glean = "39.0.3"
 
