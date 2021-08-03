7b320ed8a6622951a412a91286ad1414c3c3b85f
Zibi Braniecki <zbraniecki@mozilla.com>
2021-08-03 16:25:16 +0000

    Bug 1613705 - [localization] part17: Fix racy tests to wait for l10n frame. r=platform-i18n-reviewers,dminor,application-update-reviewers,nalexander
    Depends on D116791
    
    Differential Revision: https://phabricator.services.mozilla.com/D116792

--- toolkit/components/certviewer/tests/browser/browser_checkNonEmptyFields.js
+++ toolkit/components/certviewer/tests/browser/browser_checkNonEmptyFields.js
@@ -6,6 +6,7 @@
 async function checkNonEmptyFields(url) {
   await BrowserTestUtils.withNewTab(url, async function(browser) {
     await SpecialPowers.spawn(browser, [], async function() {
+      await content.document.l10n.ready;
       let certificateSection = await ContentTaskUtils.waitForCondition(() => {
         return content.document.querySelector("certificate-section");
       }, "Certificate section found");
@@ -56,7 +57,10 @@ async function checkNonEmptyFields(url) {
           for (let infoItem of infoItems) {
             let item = infoItem.shadowRoot.querySelector(".info");
             let info = item.textContent;
-            Assert.notEqual(info, "", "Empty strings shouldn't be rendered");
+            Assert.ok(
+              parseInt(info.length) > 0,
+              "Empty strings shouldn't be rendered"
+            );
           }
         }
       }
@@ -74,7 +78,5 @@ add_task(async function test() {
 
   let urls = [url1, url2, url3];
 
-  for (let url of urls) {
-    await checkNonEmptyFields(url);
-  }
+  await Promise.all(urls.map(checkNonEmptyFields));
 });
