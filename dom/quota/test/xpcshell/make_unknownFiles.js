12b2d84659de797338c05771c3e8025d16912282
Jan Varga <jvarga@mozilla.com>
2021-08-03 10:46:28 +0000

    Bug 1621920 - Convert IDB client to use the new macro for warning about unknown stuff in .files directory and add handling of unknown directories in .files directory; r=dom-storage-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D119586

--- dom/quota/test/xpcshell/make_unknownFiles.js
+++ dom/quota/test/xpcshell/make_unknownFiles.js
@@ -74,7 +74,8 @@ async function testSteps() {
     createUnknownDirectoryIn(originRelativePath);
   }
 
-  // Unknown files in idb client directory and its subdirectories
+  // Unknown files in idb client directory and its subdirectories and unknown
+  // directory in .files directory
   {
     const request = indexedDB.openForPrincipal(principal, "myIndexedDB");
     await openDBRequestUpgradeNeeded(request);
@@ -91,6 +92,9 @@ async function testSteps() {
 
     createUnknownFileIn(`${originRelativePath}/idb`);
     createUnknownFileIn(
+      `${originRelativePath}/idb/2320029346mByDIdnedxe.files`
+    );
+    createUnknownDirectoryIn(
       `${originRelativePath}/idb/2320029346mByDIdnedxe.files`
     );
     createUnknownFileIn(
