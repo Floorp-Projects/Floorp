f142b49260d6cce397d7a5bd573921ce68a6e1d4
Harry Twyford <htwyford@mozilla.com>
2021-08-04 19:20:12 +0000

    Bug 1722395 - Fix placeholder string in handoff input in permanent private browsing mode. r=Standard8
    about:newtab doesn't load aboutPrivateBrowsing.ftl and conversely about:privatebrowsing doesn't load newtab.ftl. Since permanent private browsing mode uses about:newtab as its new tab, we need to make sure we load our strings from newtab.ftl in that case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121646

--- browser/components/search/content/contentSearchUI.js
+++ browser/components/search/content/contentSearchUI.js
@@ -47,7 +47,7 @@ this.ContentSearchUIController = (function() {
     this._idPrefix = idPrefix;
     this._healthReportKey = healthReportKey;
     this._searchPurpose = searchPurpose;
-    this._isPrivateWindow = false;
+    this._isPrivateEngine = false;
 
     let tableID = idPrefix + "searchSuggestionTable";
     this.input.autocomplete = "off";
@@ -636,13 +636,13 @@ this.ContentSearchUIController = (function() {
     _onMsgState(state) {
       // Not all state messages broadcast the windows' privateness info.
       if ("isPrivateWindow" in state) {
-        this._isPrivateWindow = state.isPrivateWindow;
+        this._isPrivateEngine = state.isPrivateEngine;
       }
 
       this.engines = state.engines;
 
       let currentEngine = state.currentEngine;
-      if (this._isPrivateWindow) {
+      if (this._isPrivateEngine) {
         currentEngine = state.currentPrivateEngine;
       }
 
@@ -662,7 +662,7 @@ this.ContentSearchUIController = (function() {
     },
 
     _onMsgCurrentEngine(engine) {
-      if (this._isPrivateWindow) {
+      if (this._isPrivateEngine) {
         return;
       }
       this.defaultEngine = engine;
@@ -670,7 +670,7 @@ this.ContentSearchUIController = (function() {
     },
 
     _onMsgCurrentPrivateEngine(engine) {
-      if (!this._isPrivateWindow) {
+      if (!this._isPrivateEngine) {
         return;
       }
       this.defaultEngine = engine;
