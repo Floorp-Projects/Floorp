3763c6780ee7a078c3d3fa9d3856739d26b6ac51
Cosmin Sabou <csabou@mozilla.com>
2021-08-05 19:02:13 +0300

    Backed out 2 changesets (bug 1723583, bug 1722449) for failures on browser_all_files_referenced.js. CLOSED TREE
    Backed out changeset 8eb9e75580b6 (bug 1722449)
    Backed out changeset 6819370bba6d (bug 1723583)

--- browser/components/screenshots/content/screenshots.js
+++ browser/components/screenshots/content/screenshots.js
@@ -8,37 +8,10 @@ class ScreenshotsUI extends HTMLElement {
   constructor() {
     super();
   }
-  connectedCallback() {
-    this.initialize();
-  }
-
-  initialize() {
-    if (this._initialized) {
-      return;
-    }
-    this._initialized = true;
-    let template = this.ownerDocument.getElementById(
-      "screenshots-dialog-template"
-    );
-    let templateContent = template.content;
-    this.appendChild(templateContent.cloneNode(true));
-
-    this._cancelButton = this.querySelector(".highlight-button-cancel");
-    this._cancelButton.addEventListener("click", this);
-    this._copyButton = this.querySelector(".highlight-button-copy");
-    this._copyButton.addEventListener("click", this);
-    this._downloadButton = this.querySelector(".highlight-button-download");
-    this._downloadButton.addEventListener("click", this);
-  }
-
-  handleEvent(event) {
-    if (event.type == "click" && event.target == this._cancelButton) {
-      //TODO hook up close functionality
-    } else if (event.type == "click" && event.target == this._copyButton) {
-      //TODO hook up copy functionality
-    } else if (event.type == "click" && event.target == this._downloadButton) {
-      //TODO hook up download functionality
-    }
+  get markup() {
+    return `
+    `;
   }
 }
-customElements.define("screenshots-ui", ScreenshotsUI);
+
+customElements.define("screenshots-div", ScreenshotsUI);
