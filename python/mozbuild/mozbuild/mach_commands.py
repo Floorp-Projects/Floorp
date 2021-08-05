5bd96bbb60a03e1f68827e8be3c44ed77f27aee4
Nick Alexander <nalexander@mozilla.com>
2021-08-05 17:21:50 +0000

    Bug 1724247 - Specify full publisher subject in MSIX repackages. r=bhearsum
    Installing the Nightly MSIX packages, signed with Mozilla's Nightly
    key, yields an error: "Error in parsing the app package."  Unpacking
    with `makeappx.exe` yields:
    ```
    MakeAppx : error: Error info: error 8007000B: The app manifest publisher name (CN=Mozilla Corporation) must match the subject name of the signing certificate (CN=Mozilla Corporation, OU=Firefox Engineering Operations, O=Mozilla Corporation, L=Mountain View, S=California, C=US).
    ```
    
    Previously, we allowed just the `CN` to vary; in this patch we make
    the publisher be the entire publisher subject, and we update the
    publisher details in the task definitions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121896

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -2177,10 +2177,10 @@ class Repackage(MachCommandBase):
         type=str,
         # This default is baked into enough places under `browser/` that we need
         # not extract a constant.
-        default="Mozilla Corporation",
+        default="CN=Mozilla Corporation",
         required=False,
         help="The Publisher string to use in the App Manifest."
-        + " It must match the CN on the certificate used for signing",
+        + " It must match the subject on the certificate used for signing",
     )
     @CommandArgument(
         "--makeappx",
