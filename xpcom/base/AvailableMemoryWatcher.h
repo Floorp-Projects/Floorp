f54ac2b7c453299f7b8c6f08ca0a7e4abfa0d141
Haik Aftandilian <haftandilian@mozilla.com>
2021-08-05 06:07:39 +0000

    Bug 1723604 - Patch 1 - Refactor the MacMemoryPressureLevel enum into a basic class r=mac-reviewers,bradwerth
    Differential Revision: https://phabricator.services.mozilla.com/D121794

--- xpcom/base/AvailableMemoryWatcher.h
+++ xpcom/base/AvailableMemoryWatcher.h
@@ -10,21 +10,11 @@
 #include "mozilla/TimeStamp.h"
 #include "mozilla/ipc/CrashReporterHost.h"
 #include "mozilla/UniquePtr.h"
+#include "MemoryPressureLevelMac.h"
 #include "nsCOMPtr.h"
 #include "nsIAvailableMemoryWatcherBase.h"
 
 namespace mozilla {
-
-#if defined(XP_MACOSX)
-// An internal representation of the Mac memory-pressure level constants.
-enum class MacMemoryPressureLevel {
-  Unset,
-  Unexpected,
-  Normal,
-  Warning,
-  Critical,
-};
-#endif
 
 // This class implements a platform-independent part to watch the system's
 // memory situation and invoke the registered callbacks when we detect
