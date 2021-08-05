7d8637b55953efbec9057cbe9557e7895aef3457
Nick Alexander <nalexander@mozilla.com>
2021-08-05 02:58:14 +0000

    Bug 1709697 - Pre: Allow to extract string(s) from "application.ini" in a `Finder`. r=firefox-build-system-reviewers,glandium
    This makes it possible to extract multiple strings from an
    `application.ini` in a package presented as an archive.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121039

--- python/mozbuild/mozbuild/repackaging/application_ini.py
+++ python/mozbuild/mozbuild/repackaging/application_ini.py
@@ -4,27 +4,65 @@
 
 from __future__ import absolute_import, print_function
 
-import os
+from six import string_types
 from six.moves import configparser
-import mozpack.path as mozpath
+from mozpack.files import FileFinder
 
 
-def get_application_ini_value(application_directory, section, value, fallback=None):
-    rc = None
-    for root, dirs, files in os.walk(application_directory):
-        if "application.ini" in files:
-            parser = configparser.ConfigParser()
-            parser.read(mozpath.join(root, "application.ini"))
+def get_application_ini_value(
+    finder_or_application_directory, section, value, fallback=None
+):
+    """Find string with given `section` and `value` in any `application.ini`
+    under given directory or finder.
+
+    If string is not found and `fallback` is given, find string with given
+    `section` and `fallback` instead.
+
+    Raises an `Exception` if no string is found."""
+
+    return next(
+        get_application_ini_values(
+            finder_or_application_directory,
+            dict(section=section, value=value, fallback=fallback),
+        )
+    )
+
+
+def get_application_ini_values(finder_or_application_directory, *args):
+    """Find multiple strings for given `section` and `value` pairs.
+    Additional `args` should be dictionaries with keys `section`, `value`,
+    and optional `fallback`.  Returns an iterable of strings, one for each
+    dictionary provided.
+
+    `fallback` is treated as with `get_application_ini_value`.
+
+    Raises an `Exception` if any string is not found."""
+
+    if isinstance(finder_or_application_directory, string_types):
+        finder = FileFinder(finder_or_application_directory)
+    else:
+        finder = finder_or_application_directory
+
+    # Packages usually have a top-level `firefox/` directory; search below it.
+    for p, f in finder.find("**/application.ini"):
+        data = f.open().read().decode("utf-8")
+        parser = configparser.ConfigParser()
+        parser.read_string(data)
+
+        for d in args:
+            rc = None
             try:
-                rc = parser.get(section, value)
+                rc = parser.get(d["section"], d["value"])
             except configparser.NoOptionError:
-                if not fallback:
+                if "fallback" not in d:
                     raise
                 else:
-                    rc = parser.get(section, fallback)
-            break
+                    rc = parser.get(d["section"], d["fallback"])
 
-    if rc is None:
-        raise Exception("Input package does not contain an application.ini file")
+            if rc is None:
+                raise Exception("Input does not contain an application.ini file")
 
-    return rc
+            yield rc
+
+        # Process only the first `application.ini`.
+        break
