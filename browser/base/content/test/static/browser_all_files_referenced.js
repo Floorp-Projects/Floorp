c9be904cffad7a7e4149afa4155dfc3b563121ed
Kajal Sah <kajalsah107@gmail.com>
2021-08-05 17:43:00 +0000

    Bug 1722449 - Adds markup to the tab dialog box for screenshot component. r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D120931

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -258,11 +258,7 @@ var whitelist = [
   // Bug 1721741:
   // (The references to these files are dynamically generated, so the test can't
   // find the references)
-  { file: "chrome://browser/content/screenshots/cancel.svg" },
   { file: "chrome://browser/content/screenshots/copied-notification.svg" },
-  { file: "chrome://browser/content/screenshots/copy.svg" },
-  { file: "chrome://browser/content/screenshots/download-white.svg" },
-  { file: "chrome://browser/content/screenshots/download.svg" },
   {
     file:
       "chrome://browser/content/screenshots/icon-welcome-face-without-eyes.svg",
