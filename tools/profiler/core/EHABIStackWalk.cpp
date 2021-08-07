e43d8d7087c932be7c5e257d9a8a2f3df733bf44
Markus Stange <mstange.moz@gmail.com>
2021-08-07 00:34:41 +0000

    Bug 1642516 - In the EHABI stack unwinder, make sure to report proper instruction addresses by masking of the bit that indicates ARM or Thumb mode. r=jld
    This will make it so that, for return addresses, symbolication can look up
    the address "returnAddress - 1" and get the correct line number (and inline
    stack) for the call instruction.
    Without the masking, returnAddress - 1 might still fall into the instruction
    *after* the call instruction, giving wrong line numbers / inline stacks.
    
    Differential Revision: https://phabricator.services.mozilla.com/D121930

--- tools/profiler/core/EHABIStackWalk.cpp
+++ tools/profiler/core/EHABIStackWalk.cpp
@@ -121,7 +121,15 @@ size_t EHABIStackWalk(const mcontext_t& aContext, void* stackBase, void** aSPs,
 
   while (count < aNumFrames) {
     uint32_t pc = state[R_PC], sp = state[R_SP];
-    aPCs[count] = reinterpret_cast<void*>(pc);
+
+    // ARM instructions are always aligned to 2 or 4 bytes.
+    // The last bit of the pc / lr indicates ARM or Thumb mode.
+    // We're only interested in the instruction address, so we mask off that
+    // bit.
+    constexpr uint32_t instrAddrMask = ~1;
+    uint32_t instrAddress = pc & instrAddrMask;
+
+    aPCs[count] = reinterpret_cast<void*>(instrAddress);
     aSPs[count] = reinterpret_cast<void*>(sp);
     count++;
 
