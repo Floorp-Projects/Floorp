3a2ae60d9686e6fd3acf53663b11d49ce5748080
Butkovits Atila <abutkovits@mozilla.com>
2021-08-07 12:08:15 +0300

    Backed out changeset c5618823c6b5 (bug 1716825) for causing Btime failures. CLOSED TREE

--- testing/raptor/browsertime/browsertime_pageload.js
+++ testing/raptor/browsertime/browsertime_pageload.js
@@ -6,8 +6,7 @@
 
 module.exports = async function(context, commands) {
   context.log.info("Starting a browsertime pageload");
-  let test_url = context.options.browsertime.test_url;
-  let secondary_url = context.options.browsertime.secondary_url;
+  let url = context.options.browsertime.url;
   let page_cycles = context.options.browsertime.page_cycles;
   let page_cycle_delay = context.options.browsertime.page_cycle_delay;
   let post_startup_delay = context.options.browsertime.post_startup_delay;
@@ -19,20 +18,14 @@ module.exports = async function(context, commands) {
   await commands.wait.byTime(post_startup_delay);
 
   for (let count = 0; count < page_cycles; count++) {
-    if (count !== 0 && secondary_url !== undefined) {
-      context.log.info("Navigating to secondary url:" + secondary_url);
-      await commands.navigate(secondary_url);
-    } else {
-      context.log.info("Navigating to about:blank");
-      await commands.navigate("about:blank");
-    }
+    context.log.info("Navigating to about:blank");
+    await commands.navigate("about:blank");
 
-    context.log.info("Navigating to primary url:" + test_url);
     context.log.info("Cycle %d, waiting for %d ms", count, page_cycle_delay);
     await commands.wait.byTime(page_cycle_delay);
 
     context.log.info("Cycle %d, starting the measure", count);
-    await commands.measure.start(test_url);
+    await commands.measure.start(url);
   }
   context.log.info("Browsertime pageload ended.");
   return true;
