2cffa3f5c45e666a26207d868ff11551e3581a46
Paul Zuehlcke <pbz@mozilla.com>
2021-08-09 16:50:42 +0000

    Bug 1724386 - Cache InternalStorageAllowedCheck on inner window. r=timhuang,nika
    Differential Revision: https://phabricator.services.mozilla.com/D120833

--- toolkit/components/antitracking/test/browser/browser_socialtracking.js
+++ toolkit/components/antitracking/test/browser/browser_socialtracking.js
@@ -121,6 +121,7 @@ runTest({
   ],
 });
 
+let numBlockEvents = Services.prefs.getBoolPref("privacy.antitracking.cacheStorageAllowedForWindow") ? 1 : 2;
 runTest({
   testName:
     "Socialtracking-annotation feature enabled and considered for tracking detection.",
@@ -132,7 +133,9 @@ runTest({
     [Ci.nsIWebProgressListener.STATE_COOKIES_LOADED, true, 1],
     [Ci.nsIWebProgressListener.STATE_COOKIES_LOADED_SOCIALTRACKER, true, 1],
     [Ci.nsIWebProgressListener.STATE_LOADED_SOCIALTRACKING_CONTENT, true, 2],
-    [Ci.nsIWebProgressListener.STATE_COOKIES_BLOCKED_SOCIALTRACKER, true, 2],
+    // If we cache the storage allowed decision, we will only get one block
+    // event per window and origin.
+    [Ci.nsIWebProgressListener.STATE_COOKIES_BLOCKED_SOCIALTRACKER, true, numBlockEvents],
   ],
 });
 
