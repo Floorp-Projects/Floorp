e899e654577cf18cdaff139ba05ad157f469e711
Frederik Braun <fbraun@mozilla.com>
2021-08-10 12:41:02 +0000

    Bug 1723998 - Refactor jshacks detection and adapt for use in system principal telemetry r=ckerschb,tjr
    Differential Revision: https://phabricator.services.mozilla.com/D121838

--- dom/security/nsContentSecurityUtils.h
+++ dom/security/nsContentSecurityUtils.h
@@ -46,6 +46,12 @@ class nsContentSecurityUtils {
                               uint64_t aWindowID, uint32_t aLineNumber,
                               uint32_t aColumnNumber);
 
+  // Helper function for various checks:
+  // This function detects profiles with userChrome.js or extension signatures
+  // disabled. We can't/won't enforce strong security for people with those
+  // hacks. The function will cache its result.
+  static void DetectJsHacks();
+
   // Helper function to query the HTTP Channel of a potential
   // multi-part channel. Mostly used for querying response headers
   static nsresult GetHttpChannelFromPotentialMultiPart(
