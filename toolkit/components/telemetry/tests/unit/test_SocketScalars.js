cc9333fc0c50efee914a96b8088292ea187fcefa
Rob Wu <rob@robwu.nl>
2021-08-10 12:14:04 +0000

    Bug 1716307 - Only run test_SocketScalars when there's a socket process r=janerik
    Differential Revision: https://phabricator.services.mozilla.com/D122012

--- toolkit/components/telemetry/tests/unit/test_SocketScalars.js
+++ toolkit/components/telemetry/tests/unit/test_SocketScalars.js
@@ -20,19 +20,14 @@ async function waitForSocketScalars() {
   await ContentTaskUtils.waitForCondition(() => {
     const scalars = Telemetry.getSnapshotForScalars("main", false);
     return Object.keys(scalars).includes("socket");
-  });
+  }, "Waiting for socket scalars to have been set");
 }
 
-add_task(async function() {
-  if (!Services.prefs.getBoolPref("network.process.enabled")) {
-    Assert.ok(
-      true,
-      "Test finished: no point to test telemetry from socket process with lanuching the process"
-    );
-    return;
-  }
-
-  do_test_pending();
+add_task(async function test_scalars_in_socket_process() {
+  Assert.ok(
+    Services.prefs.getBoolPref("network.process.enabled"),
+    "Socket process should be enabled"
+  );
 
   do_get_profile(true);
   await TelemetryController.testSetup();
@@ -42,6 +37,7 @@ add_task(async function() {
   // Once scalars are set by the socket process, they don't immediately get
   // sent to the parent process. Wait for the Telemetry IPC Timer to trigger
   // and batch send the data back to the parent process.
+  // Note: this requires the socket process to be enabled (see bug 1716307).
   await waitForSocketScalars();
 
   Assert.equal(
@@ -51,5 +47,4 @@ add_task(async function() {
     42,
     `${SOCKET_ONLY_UINT_SCALAR} must have the correct value (socket process).`
   );
-  do_test_finished();
 });
