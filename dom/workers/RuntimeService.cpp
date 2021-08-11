0ac98622fa4e19637122785ad079967a10a443e1
Kagami Sascha Rosylight <krosylight@mozilla.com>
2021-08-11 14:41:34 +0000

    Bug 1723050 - Part 33: Replace typedef by using in dom/workers/ r=dom-worker-reviewers,edenchuang
    Differential Revision: https://phabricator.services.mozilla.com/D121331

--- dom/workers/RuntimeService.cpp
+++ dom/workers/RuntimeService.cpp
@@ -167,7 +167,7 @@ struct PrefTraits;
 
 template <>
 struct PrefTraits<bool> {
-  typedef bool PrefValueType;
+  using PrefValueType = bool;
 
   static const PrefValueType kDefaultValue = false;
 
@@ -184,7 +184,7 @@ struct PrefTraits<bool> {
 
 template <>
 struct PrefTraits<int32_t> {
-  typedef int32_t PrefValueType;
+  using PrefValueType = int32_t;
 
   static inline PrefValueType Get(const char* aPref) {
     AssertIsOnMainThread();
@@ -203,7 +203,7 @@ T GetWorkerPref(const nsACString& aPref,
                 bool* aPresent = nullptr) {
   AssertIsOnMainThread();
 
-  typedef PrefTraits<T> PrefHelper;
+  using PrefHelper = PrefTraits<T>;
 
   T result;
   bool present = true;
