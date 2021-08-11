d6acdda1eae1c2e965fa5678a9e320c228966d94
Bryce Seager van Dyk <bvandyk@mozilla.com>
2021-08-11 19:07:23 +0000

    Bug 1724781 - Add org.mozilla.clearkey_with_protection_query keysystem to key system access code. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D122143

--- dom/media/VideoUtils.h
+++ dom/media/VideoUtils.h
@@ -46,6 +46,8 @@ class MediaContainerType;
 
 // EME Key System String.
 #define EME_KEY_SYSTEM_CLEARKEY "org.w3.clearkey"
+#define EME_KEY_SYSTEM_CLEARKEY_WITH_PROTECTION_QUERY \
+  "org.mozilla.clearkey_with_protection_query"
 #define EME_KEY_SYSTEM_WIDEVINE "com.widevine.alpha"
 
 /**
