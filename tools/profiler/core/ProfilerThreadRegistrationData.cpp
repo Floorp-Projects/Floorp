837c1d6209398d0d4aa43563ec0a5344581450fe
Gerald Squelart <gsquelart@mozilla.com>
2021-08-12 01:12:30 +0000

    Bug 1721939 - ProfilerThreadRegistrationData accessor sub-classes - r=canaltinova
    Non-virtual sub-classes of `ProfilerThreadRegistrationData` provide layers of public accessors to subsets of the data.
    Each level builds on the previous one and adds further access to more data, but always with the appropriate guards where necessary.
    These classes have protected constructors, so only some trusted classes (in later patches) will be able to construct them, and then give limited access depending on who asks (the owning thread or another one), and how much data they actually need.
    
    The hierarchy is, from base to most derived:
    - `ThreadRegistrationData` (previous patch)
    - `ThreadRegistrationUnlockedConstReader`
    - `ThreadRegistrationUnlockedConstReaderAndAtomicRW`
    - `ThreadRegistrationUnlockedRWForLockedProfiler`
    - `ThreadRegistrationUnlockedReaderAndAtomicRWOnThread`
    - `ThreadRegistrationLockedRWFromAnyThread`
    - `ThreadRegistrationLockedRWOnThread`
    - `ThreadRegistration::EmbeddedData` (next patch, as data member of `ThreadRegistration`)
    
    Tech detail: These classes need to be a single hierarchy so that the upcoming `ThreadRegistration` class can contain the most-derived class, and from there can publish references to base classes without relying on Undefined Behavior. (It's not allowed to have some object and give a reference to a sub-class, unless that object was *really* constructed as that sub-class at least, even if that sub-class only adds member functions!)
    And where appropriate, these references will come along with the required lock.
    
    For example:
    JSContext can only be written on the owning thread, through `ThreadRegistrationLockedRWOnThread` (which will be accessed through TLS = Thread Local Storage), which requires a per-thread lock.
    Thanks to that, JSContext can be read using `ThreadRegistrationUnlockedReaderAndAtomicRWOnThread`, without lock because we're on the owning thread, so there cannot be any write at the same time.
    Other threads will be able to read it as well, but they will only see it through `ThreadRegistrationLockedRWFromAnyThread`, which will require the per-thread lock, thereby preventing simultaneous writes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120816

--- tools/profiler/core/ProfilerThreadRegistrationData.cpp
+++ tools/profiler/core/ProfilerThreadRegistrationData.cpp
@@ -6,6 +6,11 @@
 
 #include "mozilla/ProfilerThreadRegistrationData.h"
 
+#include "mozilla/ProfilerMarkers.h"
+#include "js/AllocationRecording.h"
+#include "js/ProfilingStack.h"
+#include "js/TraceLoggerAPI.h"
+
 #if defined(XP_WIN)
 #  include <windows.h>
 #elif defined(XP_DARWIN)
@@ -32,6 +37,123 @@ ThreadRegistrationData::ThreadRegistrationData(const char* aName,
           aStackTop
 #endif
       ) {
+}
+
+// This is a simplified version of profiler_add_marker that can be easily passed
+// into the JS engine.
+static void profiler_add_js_marker(const char* aMarkerName,
+                                   const char* aMarkerText) {
+  PROFILER_MARKER_TEXT(
+      mozilla::ProfilerString8View::WrapNullTerminatedString(aMarkerName), JS,
+      {}, mozilla::ProfilerString8View::WrapNullTerminatedString(aMarkerText));
+}
+
+static void profiler_add_js_allocation_marker(JS::RecordAllocationInfo&& info) {
+  if (!profiler_can_accept_markers()) {
+    return;
+  }
+
+  struct JsAllocationMarker {
+    static constexpr mozilla::Span<const char> MarkerTypeName() {
+      return mozilla::MakeStringSpan("JS allocation");
+    }
+    static void StreamJSONMarkerData(
+        mozilla::baseprofiler::SpliceableJSONWriter& aWriter,
+        const mozilla::ProfilerString16View& aTypeName,
+        const mozilla::ProfilerString8View& aClassName,
+        const mozilla::ProfilerString16View& aDescriptiveTypeName,
+        const mozilla::ProfilerString8View& aCoarseType, uint64_t aSize,
+        bool aInNursery) {
+      if (aClassName.Length() != 0) {
+        aWriter.StringProperty("className", aClassName);
+      }
+      if (aTypeName.Length() != 0) {
+        aWriter.StringProperty(
+            "typeName",
+            NS_ConvertUTF16toUTF8(aTypeName.Data(), aTypeName.Length()));
+      }
+      if (aDescriptiveTypeName.Length() != 0) {
+        aWriter.StringProperty(
+            "descriptiveTypeName",
+            NS_ConvertUTF16toUTF8(aDescriptiveTypeName.Data(),
+                                  aDescriptiveTypeName.Length()));
+      }
+      aWriter.StringProperty("coarseType", aCoarseType);
+      aWriter.IntProperty("size", aSize);
+      aWriter.BoolProperty("inNursery", aInNursery);
+    }
+    static mozilla::MarkerSchema MarkerTypeDisplay() {
+      return mozilla::MarkerSchema::SpecialFrontendLocation{};
+    }
+  };
+
+  profiler_add_marker(
+      "JS allocation", geckoprofiler::category::JS,
+      mozilla::MarkerStack::Capture(), JsAllocationMarker{},
+      mozilla::ProfilerString16View::WrapNullTerminatedString(info.typeName),
+      mozilla::ProfilerString8View::WrapNullTerminatedString(info.className),
+      mozilla::ProfilerString16View::WrapNullTerminatedString(
+          info.descriptiveTypeName),
+      mozilla::ProfilerString8View::WrapNullTerminatedString(info.coarseType),
+      info.size, info.inNursery);
+}
+
+void ThreadRegistrationLockedRWFromAnyThread::SetIsBeingProfiled(
+    bool aIsBeingProfiled, const PSAutoLock&) {
+  mIsBeingProfiled = aIsBeingProfiled;
+}
+
+void ThreadRegistrationLockedRWOnThread::SetJSContext(JSContext* aJSContext) {
+  MOZ_ASSERT(aJSContext && !mJSContext);
+
+  mJSContext = aJSContext;
+
+  // We give the JS engine a non-owning reference to the ProfilingStack. It's
+  // important that the JS engine doesn't touch this once the thread dies.
+  js::SetContextProfilingStack(aJSContext, &ProfilingStackRef());
+}
+
+void ThreadRegistrationLockedRWOnThread::ClearJSContext() {
+  mJSContext = nullptr;
+}
+
+void ThreadRegistrationLockedRWOnThread::PollJSSampling() {
+  // We can't start/stop profiling until we have the thread's JSContext.
+  if (mJSContext) {
+    // It is possible for mJSSampling to go through the following sequences.
+    //
+    // - INACTIVE, ACTIVE_REQUESTED, INACTIVE_REQUESTED, INACTIVE
+    //
+    // - ACTIVE, INACTIVE_REQUESTED, ACTIVE_REQUESTED, ACTIVE
+    //
+    // Therefore, the if and else branches here aren't always interleaved.
+    // This is ok because the JS engine can handle that.
+    //
+    if (mJSSampling == ACTIVE_REQUESTED) {
+      mJSSampling = ACTIVE;
+      js::EnableContextProfilingStack(mJSContext, true);
+      if (JSTracerEnabled()) {
+        JS::StartTraceLogger(mJSContext);
+      }
+      if (JSAllocationsEnabled()) {
+        // TODO - This probability should not be hardcoded. See Bug 1547284.
+        JS::EnableRecordingAllocations(mJSContext,
+                                       profiler_add_js_allocation_marker, 0.01);
+      }
+      js::RegisterContextProfilingEventMarker(mJSContext,
+                                              profiler_add_js_marker);
+
+    } else if (mJSSampling == INACTIVE_REQUESTED) {
+      mJSSampling = INACTIVE;
+      js::EnableContextProfilingStack(mJSContext, false);
+      if (JSTracerEnabled()) {
+        JS::StopTraceLogger(mJSContext);
+      }
+      if (JSAllocationsEnabled()) {
+        JS::DisableRecordingAllocations(mJSContext);
+      }
+    }
+  }
 }
 
 }  // namespace mozilla::profiler
