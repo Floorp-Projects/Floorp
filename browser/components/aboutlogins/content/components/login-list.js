d95e6066b0dd5d29264bfd9458b81d6ea88632b0
Antonin LOUBIERE <pyjacpp@laposte.net>
2021-08-12 14:14:16 +0000

    Bug 1592682 - Add Section Headers in login list for date and alert sorts. r=jaws,tgiles,fluent-reviewers
    Add Section Headers in login list (about:logins) for date and alert sorts.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120788

--- browser/components/aboutlogins/content/components/login-list.js
+++ browser/components/aboutlogins/content/components/login-list.js
@@ -3,9 +3,16 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import LoginListItemFactory from "./login-list-item.js";
+import LoginListSectionFactory from "./login-list-section.js";
 import { recordTelemetryEvent } from "../aboutLoginsUtils.js";
 
 const collator = new Intl.Collator();
+const monthFormatter = new Intl.DateTimeFormat(undefined, { month: "long" });
+const yearMonthFormatter = new Intl.DateTimeFormat(undefined, {
+  year: "numeric",
+  month: "long",
+});
+const dayDuration = 24 * 60 * 60_000;
 const sortFnOptions = {
   name: (a, b) => collator.compare(a.title, b.title),
   "name-reverse": (a, b) => collator.compare(b.title, a.title),
@@ -32,6 +39,60 @@ const sortFnOptions = {
   },
 };
 
+const headersFnOptions = {
+  // TODO: name should use the ICU API, see Bug 1592834
+  // name: l =>
+  //   l.title.length && letterRegExp.test(l.title[0])
+  //     ? l.title[0].toUpperCase()
+  //     : "#",
+  // "name-reverse": l => headersFnOptions.name(l),
+  name: () => "",
+  "name-reverse": () => "",
+  "last-used": l => headerFromDate(l.timeLastUsed),
+  "last-changed": l => headerFromDate(l.timePasswordChanged),
+  alerts: (l, breachesByLoginGUID, vulnerableLoginsByLoginGUID) => {
+    const isBreached = breachesByLoginGUID && breachesByLoginGUID.has(l.guid);
+    const isVulnerable =
+      vulnerableLoginsByLoginGUID && vulnerableLoginsByLoginGUID.has(l.guid);
+    if (isBreached) {
+      return (
+        LoginListSectionFactory.ID_PREFIX + "about-logins-list-section-breach"
+      );
+    } else if (isVulnerable) {
+      return (
+        LoginListSectionFactory.ID_PREFIX +
+        "about-logins-list-section-vulnerable"
+      );
+    }
+    return (
+      LoginListSectionFactory.ID_PREFIX + "about-logins-list-section-nothing"
+    );
+  },
+};
+
+function headerFromDate(timestamp) {
+  let now = new Date();
+  now.setHours(0, 0, 0, 0); // reset to start of day
+  let date = new Date(timestamp);
+
+  if (now < date) {
+    return (
+      LoginListSectionFactory.ID_PREFIX + "about-logins-list-section-today"
+    );
+  } else if (now - dayDuration < date) {
+    return (
+      LoginListSectionFactory.ID_PREFIX + "about-logins-list-section-yesterday"
+    );
+  } else if (now - 7 * dayDuration < date) {
+    return LoginListSectionFactory.ID_PREFIX + "about-logins-list-section-week";
+  } else if (now.getFullYear() == date.getFullYear()) {
+    return monthFormatter.format(date);
+  } else if (now.getFullYear() - 1 == date.getFullYear()) {
+    return yearMonthFormatter.format(date);
+  }
+  return String(date.getFullYear());
+}
+
 export default class LoginList extends HTMLElement {
   constructor() {
     super();
@@ -39,6 +100,8 @@ export default class LoginList extends HTMLElement {
     this._loginGuidsSortedOrder = [];
     // A map of login GUID -> {login, listItem}.
     this._logins = {};
+    // A map of section header -> sectionItem
+    this._sections = {};
     this._filter = "";
     this._selectedGuid = null;
     this._blankLoginListItem = LoginListItemFactory.create({});
@@ -128,14 +191,40 @@ export default class LoginList extends HTMLElement {
       listItem.hidden = !visibleLoginGuids.has(listItem.dataset.guid);
     }
 
-    // Re-arrange the login-list-items according to their sort
-    for (let i = this._loginGuidsSortedOrder.length - 1; i >= 0; i--) {
-      let guid = this._loginGuidsSortedOrder[i];
-      let { listItem } = this._logins[guid];
-      this._list.insertBefore(
-        listItem,
-        this._blankLoginListItem.nextElementSibling
-      );
+    let sectionsKey = Object.keys(this._sections);
+    for (let sectionKey of sectionsKey) {
+      this._sections[sectionKey]._inUse = false;
+    }
+
+    if (this._loginGuidsSortedOrder.length) {
+      let section = null;
+      let currentHeader = null;
+      // Re-arrange the login-list-items according to their sort and
+      // create / re-arrange sections
+      for (let i = this._loginGuidsSortedOrder.length - 1; i >= 0; i--) {
+        let guid = this._loginGuidsSortedOrder[i];
+        let { listItem, _header } = this._logins[guid];
+
+        if (!listItem.hidden) {
+          if (currentHeader != _header) {
+            section = this.renderSectionHeader((currentHeader = _header));
+          }
+
+          section.insertBefore(
+            listItem,
+            section.firstElementChild.nextElementSibling
+          );
+        }
+      }
+    }
+
+    for (let sectionKey of sectionsKey) {
+      let section = this._sections[sectionKey];
+      if (section._inUse) {
+        continue;
+      }
+
+      section.hidden = true;
     }
 
     let activeDescendantId = this._list.getAttribute("aria-activedescendant");
@@ -165,6 +254,22 @@ export default class LoginList extends HTMLElement {
       // selected sort so the user's current task isn't interrupted.
       this._sortSelect.namedItem("alerts").hidden = false;
     }
+  }
+
+  renderSectionHeader(header) {
+    let section = this._sections[header];
+    if (!section) {
+      section = this._sections[header] = LoginListSectionFactory.create(header);
+    }
+
+    this._list.insertBefore(
+      section,
+      this._blankLoginListItem.nextElementSibling
+    );
+
+    section._inUse = true;
+    section.hidden = false;
+    return section;
   }
 
   handleEvent(event) {
@@ -209,6 +314,7 @@ export default class LoginList extends HTMLElement {
         break;
       }
       case "change": {
+        this._applyHeaders();
         this._applySortAndScrollToTop();
         const extra = { sort_key: this._sortSelect.value };
         recordTelemetryEvent({ object: "list", method: "sort", extra });
@@ -332,6 +438,7 @@ export default class LoginList extends HTMLElement {
       map[login.guid] = { login };
       return map;
     }, {});
+    this._applyHeaders();
     this._applySort();
     this._list.textContent = "";
     this._list.appendChild(this._blankLoginListItem);
@@ -420,6 +527,7 @@ export default class LoginList extends HTMLElement {
         const alertsSortOptionElement = this._sortSelect.namedItem("alerts");
         alertsSortOptionElement.hidden = false;
         this._sortSelect.selectedIndex = alertsSortOptionElement.index;
+        this._applyHeaders();
         this._applySortAndScrollToTop();
         this._selectFirstVisibleLogin();
       }
@@ -455,6 +563,7 @@ export default class LoginList extends HTMLElement {
       return;
     }
     this._sortSelect.value = sortDirection;
+    this._applyHeaders();
     this._applySortAndScrollToTop();
     this._selectFirstVisibleLogin();
   }
@@ -465,6 +574,7 @@ export default class LoginList extends HTMLElement {
   loginAdded(login) {
     this._logins[login.guid] = { login };
     this._loginGuidsSortedOrder.push(login.guid);
+    this._applyHeaders(false);
     this._applySort();
 
     // Add the list item and update any other related state that may pertain
@@ -486,10 +596,12 @@ export default class LoginList extends HTMLElement {
   loginModified(login) {
     this._logins[login.guid] = Object.assign(this._logins[login.guid], {
       login,
+      _header: null, // reset header
     });
+    this._applyHeaders(false);
     this._applySort();
-    let { listItem } = this._logins[login.guid];
-    LoginListItemFactory.update(listItem, login);
+    let loginObject = this._logins[login.guid];
+    LoginListItemFactory.update(loginObject.listItem, login);
 
     // Update any other related state that may pertain to the list item
     // such as breach alerts that may or may not now apply.
@@ -577,6 +689,20 @@ export default class LoginList extends HTMLElement {
     });
   }
 
+  _applyHeaders(updateAll = true) {
+    let headerFn = headersFnOptions[this._sortSelect.value];
+    for (let guid of this._loginGuidsSortedOrder) {
+      let login = this._logins[guid];
+      if (updateAll || !login._header) {
+        login._header = headerFn(
+          login.login,
+          this._breachesByLoginGUID,
+          this._vulnerableLoginsByLoginGUID
+        );
+      }
+    }
+  }
+
   _applySortAndScrollToTop() {
     this._applySort();
     this.render();
@@ -631,20 +757,43 @@ export default class LoginList extends HTMLElement {
     let activeDescendantId = this._list.getAttribute("aria-activedescendant");
     let activeDescendant =
       activeDescendantId && this.shadowRoot.getElementById(activeDescendantId);
-    if (!activeDescendant || activeDescendant.hidden) {
+    if (
+      !activeDescendant ||
+      activeDescendant.hidden ||
+      !activeDescendant.classList.contains("login-list-item")
+    ) {
       activeDescendant =
         this._list.querySelector(".login-list-item[data-guid]:not([hidden])") ||
         this._list.firstElementChild;
     }
     let newlyFocusedItem = null;
-    let previousItem = activeDescendant.previousElementSibling;
-    while (previousItem && previousItem.hidden) {
-      previousItem = previousItem.previousElementSibling;
-    }
-    let nextItem = activeDescendant.nextElementSibling;
-    while (nextItem && nextItem.hidden) {
-      nextItem = nextItem.nextElementSibling;
-    }
+    let previousItem = activeDescendant;
+    do {
+      previousItem =
+        (previousItem.tagName == "SECTION"
+          ? previousItem.lastElementChild
+          : previousItem.previousElementSibling) ||
+        (previousItem.parentElement.tagName == "SECTION" &&
+          previousItem.parentElement.previousElementSibling);
+    } while (
+      previousItem &&
+      (previousItem.hidden ||
+        !previousItem.classList.contains("login-list-item"))
+    );
+
+    let nextItem = activeDescendant;
+    do {
+      nextItem =
+        (nextItem.tagName == "SECTION"
+          ? nextItem.firstElementChild.nextElementSibling
+          : nextItem.nextElementSibling) ||
+        (nextItem.parentElement.tagName == "SECTION" &&
+          nextItem.parentElement.nextElementSibling);
+    } while (
+      nextItem &&
+      (nextItem.hidden || !nextItem.classList.contains("login-list-item"))
+    );
+
     if (event.type == "keydown") {
       switch (event.key) {
         case "ArrowDown": {
