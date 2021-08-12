ddabc1b810ce820ed33e55740f9217b43de8a87d
Andrew Osmond <aosmond@mozilla.com>
2021-08-12 20:28:53 +0000

    Bug 1725388 - Remove pref/envvars that can disable WebRender. r=jrmuizel
    MOZ_WEBRENDER=0 now does nothing -- you will either get HW-WR or SW-WR
    depending on the platform configuration. The pref
    gfx.webrender.force-legacy-layers is removed. This leaves no
    configuration option to disable WebRender.
    
    MOZ_WEBRENDER=1 will continue to force WR on, which will ensure in CI we
    get HW-WR unless gfx.webrender.software is true.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122474

--- gfx/thebes/gfxPlatform.h
+++ gfx/thebes/gfxPlatform.h
@@ -807,8 +807,6 @@ class gfxPlatform : public mozilla::layers::MemoryPressureListener {
   static bool WebRenderPrefEnabled();
   // you probably want to use gfxVars::UseWebRender() instead of this
   static bool WebRenderEnvvarEnabled();
-  // you probably want to use gfxVars::UseWebRender() instead of this
-  static bool WebRenderEnvvarDisabled();
 
   static const char* WebRenderResourcePathOverride();
 
