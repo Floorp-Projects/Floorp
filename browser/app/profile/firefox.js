d4075b13a4e42414957ed1c6c864b4a3f7c3a184
Marian-Vasile Laza <mlaza@mozilla.com>
2021-08-13 21:04:21 +0300

    Backed out changeset c734901eec31 (bug 1702715) for causing bc failures on browser_ext_devtools_network_targetSwitch.js. CLOSED TREE

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2143,7 +2143,7 @@ pref("devtools.browsertoolbox.fission", false);
 #endif
 
 // This preference will enable watching top-level targets from the server side.
-pref("devtools.target-switching.server.enabled", true);
+pref("devtools.target-switching.server.enabled", false);
 
 // Toolbox Button preferences
 pref("devtools.command-button-pick.enabled", true);
