349b267b61ff8ae032f6f3cd04d6814fa24957ef
Jan de Mooij <jdemooij@mozilla.com>
2021-08-13 09:20:37 +0000

    Bug 1725348 part 11 - Make ArrayObject::createArray more consistent with other allocation functions. r=jonco
    Rename to just "create" and add missing call to debugCheckNewObject for consistency
    with JSFunction, NativeObject, TypedObject.
    
    Depends on D122430
    
    Differential Revision: https://phabricator.services.mozilla.com/D122431

--- js/src/vm/ArrayObject.h
+++ js/src/vm/ArrayObject.h
@@ -42,11 +42,11 @@ class ArrayObject : public NativeObject {
   }
 
   // Make an array object with the specified initial state.
-  static inline ArrayObject* createArray(JSContext* cx, gc::AllocKind kind,
-                                         gc::InitialHeap heap,
-                                         HandleShape shape, uint32_t length,
-                                         AutoSetNewObjectMetadata& metadata,
-                                         gc::AllocSite* site = nullptr);
+  static inline ArrayObject* create(JSContext* cx, gc::AllocKind kind,
+                                    gc::InitialHeap heap, HandleShape shape,
+                                    uint32_t length,
+                                    AutoSetNewObjectMetadata& metadata,
+                                    gc::AllocSite* site = nullptr);
 };
 
 }  // namespace js
