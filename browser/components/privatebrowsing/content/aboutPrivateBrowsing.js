0aaae4a8632a94378592aa2ab95c78ba92e197bf
Emily McMinn <emcminn@mozilla.com>
2021-08-16 14:33:31 +0000

    Bug 1724019 - Update about:privatebrowsing CSS, markup and tests r=andreio
    Differential Revision: https://phabricator.services.mozilla.com/D121753

--- browser/components/privatebrowsing/content/aboutPrivateBrowsing.js
+++ browser/components/privatebrowsing/content/aboutPrivateBrowsing.js
@@ -101,17 +101,13 @@ async function renderPromo({
 
   const titleEl = document.getElementById("private-browsing-vpn-text");
   let linkEl = document.getElementById("private-browsing-vpn-link");
-  const promoHeaderEl = document.getElementById("promo-header");
   const infoContainerEl = document.querySelector(".info");
-  const promoImageLargeEl = document.querySelector(".promo-image-large img");
-  const promoImageSmallEl = document.querySelector(".promo-image-small img");
-
   // Setup the private browsing VPN link.
   const vpnPromoUrl =
     promoLinkUrl || RPMGetFormatURLPref("browser.privatebrowsing.vpnpromourl");
 
-  if (promoLinkType === "button") {
-    linkEl.classList.add("button");
+  if (promoLinkType === "link") {
+    linkEl.classList.remove("button");
   }
 
   if (vpnPromoUrl) {
@@ -137,22 +133,6 @@ async function renderPromo({
         container.remove();
         document.body.insertAdjacentElement("afterbegin", container);
     }
-  }
-
-  if (promoHeader) {
-    promoHeaderEl.innerText = promoHeader;
-  }
-
-  if (promoImageLarge) {
-    promoImageLargeEl.src = promoImageLarge;
-  } else {
-    promoImageLargeEl.parentNode.remove();
-  }
-
-  if (promoImageSmall) {
-    promoImageSmallEl.src = promoImageSmall;
-  } else {
-    promoImageSmallEl.parentNode.remove();
   }
 
   if (!promoTitleEnabled) {
