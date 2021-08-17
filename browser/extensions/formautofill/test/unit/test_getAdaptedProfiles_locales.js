9285e090f8da82a5f3f3b24f7fac8cfa4ecaf9f3
Tim Giles <tgiles@mozilla.com>
2021-08-17 15:58:48 +0000

    Bug 1687681: Add France specific CC expiry date transformation. r=dimi,sfoster
    Our previous regex did not parse "AAAA" or "AA" so I've added this to the transformation regex.
    For the year first regex, I added a separate regex because trying to bolt on the missing "AA" caused the resulting parsed result to drop characters for other locales ("yyyy" became "yyy" and the same with "aaaa").
    
    Remove backslash as a supported month/year divider when checking expiry date placeholders.
    
    Differential Revision: https://phabricator.services.mozilla.com/D120550

---
+++ browser/extensions/formautofill/test/unit/test_getAdaptedProfiles_locales.js
@@ -0,0 +1,138 @@
+/* Any copyright is dedicated to the Public Domain.
+http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/*
+ * Test to ensure locale specific placeholders for credit card fields are properly used
+ * to transform various values in the profile.
+ */
+
+"use strict";
+
+let FormAutofillHandler;
+add_task(async function() {
+  ({ FormAutofillHandler } = ChromeUtils.import(
+    "resource://autofill/FormAutofillHandler.jsm"
+  ));
+});
+
+const DEFAULT_CREDITCARD_RECORD = {
+  guid: "123",
+  "cc-exp-month": 1,
+  "cc-exp-year": 2025,
+  "cc-exp": "2025-01",
+};
+
+const FR_TESTCASES = [
+  {
+    description: "Use placeholder to adjust cc-exp format [mm/aa].",
+    document: `<form><input autocomplete="cc-number">
+               <input placeholder="mm/aa" autocomplete="cc-exp"></form>`,
+    profileData: [Object.assign({}, DEFAULT_CREDITCARD_RECORD)],
+    expectedResult: [
+      Object.assign({}, DEFAULT_CREDITCARD_RECORD, {
+        "cc-exp": "01/25",
+      }),
+    ],
+  },
+  {
+    description: "Use placeholder to adjust cc-exp format [mm / aa].",
+    document: `<form><input autocomplete="cc-number">
+               <input placeholder="mm / aa" autocomplete="cc-exp"></form>`,
+    profileData: [Object.assign({}, DEFAULT_CREDITCARD_RECORD)],
+    expectedResult: [
+      Object.assign({}, DEFAULT_CREDITCARD_RECORD, {
+        "cc-exp": "01/25",
+      }),
+    ],
+  },
+  {
+    description: "Use placeholder to adjust cc-exp format [MM / AA].",
+    document: `<form><input autocomplete="cc-number">
+               <input placeholder="MM / AA" autocomplete="cc-exp"></form>`,
+    profileData: [Object.assign({}, DEFAULT_CREDITCARD_RECORD)],
+    expectedResult: [
+      Object.assign({}, DEFAULT_CREDITCARD_RECORD, {
+        "cc-exp": "01/25",
+      }),
+    ],
+  },
+  {
+    description: "Use placeholder to adjust cc-exp format [mm / aaaa].",
+    document: `<form><input autocomplete="cc-number">
+               <input placeholder="mm / aaaa" autocomplete="cc-exp"></form>`,
+    profileData: [Object.assign({}, DEFAULT_CREDITCARD_RECORD)],
+    expectedResult: [
+      Object.assign({}, DEFAULT_CREDITCARD_RECORD, {
+        "cc-exp": "01/2025",
+      }),
+    ],
+  },
+  {
+    description: "Use placeholder to adjust cc-exp format [mm - aaaa].",
+    document: `<form><input autocomplete="cc-number">
+               <input placeholder="mm - aaaa" autocomplete="cc-exp"></form>`,
+    profileData: [Object.assign({}, DEFAULT_CREDITCARD_RECORD)],
+    expectedResult: [
+      Object.assign({}, DEFAULT_CREDITCARD_RECORD, {
+        "cc-exp": "01-2025",
+      }),
+    ],
+  },
+  {
+    description: "Use placeholder to adjust cc-exp format [aaaa-mm].",
+    document: `<form><input autocomplete="cc-number">
+               <input placeholder="aaaa-mm" autocomplete="cc-exp"></form>`,
+    profileData: [Object.assign({}, DEFAULT_CREDITCARD_RECORD)],
+    expectedResult: [
+      Object.assign({}, DEFAULT_CREDITCARD_RECORD, {
+        "cc-exp": "2025-01",
+      }),
+    ],
+  },
+];
+
+const TESTCASES = [FR_TESTCASES];
+
+for (let localeTests of TESTCASES) {
+  for (let testcase of localeTests) {
+    add_task(async function() {
+      info("Starting testcase: " + testcase.description);
+
+      let doc = MockDocument.createTestDocument(
+        "http://localhost:8080/test/",
+        testcase.document
+      );
+      let form = doc.querySelector("form");
+      let formLike = FormLikeFactory.createFromForm(form);
+      let handler = new FormAutofillHandler(formLike);
+
+      handler.collectFormFields();
+      handler.focusedInput = form.elements[0];
+      let adaptedRecords = handler.activeSection.getAdaptedProfiles(
+        testcase.profileData
+      );
+      Assert.deepEqual(adaptedRecords, testcase.expectedResult);
+
+      if (testcase.expectedOptionElements) {
+        testcase.expectedOptionElements.forEach((expectedOptionElement, i) => {
+          for (let field in expectedOptionElement) {
+            let select = form.querySelector(`[autocomplete=${field}]`);
+            let expectedOption = doc.getElementById(
+              expectedOptionElement[field]
+            );
+            Assert.notEqual(expectedOption, null);
+
+            let value = testcase.profileData[i][field];
+            let cache = handler.activeSection._cacheValue.matchingSelectOption.get(
+              select
+            );
+            let targetOption = cache[value] && cache[value].get();
+            Assert.notEqual(targetOption, null);
+
+            Assert.equal(targetOption, expectedOption);
+          }
+        });
+      }
+    });
+  }
+}
