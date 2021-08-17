1faa65436c2d02eff18247f8c6ca16a29f27914e
Harry Twyford <htwyford@mozilla.com>
2021-08-17 23:28:31 +0000

    Bug 1725468 - Add prototype monochromatic theme for UX iteration. r=Gijs,amy
    Differential Revision: https://phabricator.services.mozilla.com/D122515

--- toolkit/mozapps/extensions/content/aboutaddons.js
+++ toolkit/mozapps/extensions/content/aboutaddons.js
@@ -95,6 +95,10 @@ const BUILTIN_THEME_PREVIEWS = new Map([
     "firefox-alpenglow@mozilla.org",
     "chrome://mozapps/content/extensions/firefox-alpenglow.svg",
   ],
+  [
+    "firefox-monochromatic-purple@mozilla.org",
+    "chrome://mozapps/content/extensions/firefox-monochromatic-purple.svg",
+  ],
 ]);
 
 const PERMISSION_MASKS = {
