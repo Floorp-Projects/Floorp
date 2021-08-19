89a7c5d4f902b19b3d46b06b401344226767f313
John Lin <jolin@mozilla.com>
2021-08-19 19:01:20 +0000

    Bug 1665776 - p5: add VPx support to AndroidEncoderModule. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D121022

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/HardwareCodecCapabilityUtils.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/HardwareCodecCapabilityUtils.java
@@ -225,6 +225,7 @@ public final class HardwareCodecCapabilityUtils {
         return null;
     }
 
+    @WrapForJNI
     public static boolean hasHWVP8(final boolean aIsEncoder) {
         return getHWCodecCapability(VP8_MIME_TYPE, aIsEncoder);
     }
@@ -232,6 +233,11 @@ public final class HardwareCodecCapabilityUtils {
     @WrapForJNI
     public static boolean hasHWVP9(final boolean aIsEncoder) {
         return getHWCodecCapability(VP9_MIME_TYPE, aIsEncoder);
+    }
+
+    @WrapForJNI
+    public static boolean hasHWH264(final boolean aIsEncoder) {
+        return getHWCodecCapability(H264_MIME_TYPE, aIsEncoder);
     }
 
     @WrapForJNI(calledFrom = "gecko")
