2708698da8b0fa7be38181feabcf370883a2ab5a
André Bargull <andre.bargull@gmail.com>
2021-08-19 17:28:55 +0000

    Bug 1725632 - Part 4: Fix-up includes to js/src/jit in js/src/vm. r=jandem
    Depends on D122593
    
    Differential Revision: https://phabricator.services.mozilla.com/D122594

--- js/src/vm/TraceLogging.cpp
+++ js/src/vm/TraceLogging.cpp
@@ -14,7 +14,6 @@
 #include <utility>
 
 #include "jit/BaselineJIT.h"
-#include "jit/CompileWrappers.h"
 #include "jit/JitSpewer.h"
 #include "js/friend/ErrorMessages.h"  // js::GetErrorMessage, JSMSG_*
 #include "js/Printf.h"
