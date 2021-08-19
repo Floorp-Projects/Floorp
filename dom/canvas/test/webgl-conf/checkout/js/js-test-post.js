30a01e163fc34c303a4420b1bb62668e2183e54e
Jeff Gilbert <jgilbert@mozilla.com>
2021-08-19 18:37:58 +0000

    Bug 1724535 - Update WebGL CTS checkout. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D122657

--- dom/canvas/test/webgl-conf/checkout/js/js-test-post.js
+++ dom/canvas/test/webgl-conf/checkout/js/js-test-post.js
@@ -9,4 +9,19 @@ _addSpan('<br /><span class="pass">TEST COMPLETE</span>');
 if (_jsTestPreVerboseLogging) {
     _bufferedLogToConsole('TEST COMPLETE');
 }
+
+{
+    const e_results = document.createElement('div');
+    let fails_class = 'pass';
+    if (RESULTS.fail) {
+        fails_class = 'fail';
+    }
+    e_results.classList.add('pass');
+    e_results.innerHTML = `<p>TEST COMPLETE: ${RESULTS.pass} PASS, ` +
+      `<span class="${fails_class}">${RESULTS.fail} FAIL</span></p>`;
+
+    const e_desc = document.getElementById("description");
+    e_desc.appendChild(e_results);
+}
+
 notifyFinishedToHarness()
