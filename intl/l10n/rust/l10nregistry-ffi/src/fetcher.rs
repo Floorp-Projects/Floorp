b7112e0f555104bddc56ecf3bb58f3b449a3d63f
Zibi Braniecki <zbraniecki@mozilla.com>
2021-08-19 17:49:11 +0000

    Bug 1726535 - Restore the ja-JP-macos -> ja-JP-mac L10nRegistry path overload. r=jfkthame, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D123133

--- intl/l10n/rust/l10nregistry-ffi/src/fetcher.rs
+++ intl/l10n/rust/l10nregistry-ffi/src/fetcher.rs
@@ -4,7 +4,7 @@
 
 use l10nregistry::source::FileFetcher;
 
-use std::io;
+use std::{borrow::Cow, io};
 
 pub struct GeckoFileFetcher;
 
@@ -13,13 +13,30 @@ fn try_string_from_box_u8(input: Box<[u8]>) -> io::Result<String> {
         .map_err(|err| io::Error::new(io::ErrorKind::InvalidData, err.utf8_error()))
 }
 
+// For historical reasons we maintain a locale in Firefox with a codename `ja-JP-mac`.
+// This string is an invalid BCP-47 language tag, so we don't store it in Gecko, which uses
+// valid BCP-47 tags only, but rather keep that quirk local to Gecko L10nRegistry file fetcher.
+//
+// Here, if we encounter `ja-JP-macos` (valid BCP-47), we swap it for `ja-JP-mac`.
+//
+// See bug 1726586 for details, and source::get_locale_from_gecko.
+fn get_path_for_gecko<'s>(input: &'s str) -> Cow<'s, str> {
+    if input.contains("ja-JP-macos") {
+        input.replace("ja-JP-macos", "ja-JP-mac").into()
+    } else {
+        input.into()
+    }
+}
+
 #[async_trait::async_trait(?Send)]
 impl FileFetcher for GeckoFileFetcher {
     fn fetch_sync(&self, path: &str) -> io::Result<String> {
+        let path = get_path_for_gecko(path);
         crate::load::load_sync(path).and_then(try_string_from_box_u8)
     }
 
     async fn fetch(&self, path: &str) -> io::Result<String> {
+        let path = get_path_for_gecko(path);
         crate::load::load_async(path)
             .await
             .and_then(try_string_from_box_u8)
