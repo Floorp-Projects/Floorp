69cf4f563178b61998e09754e169cb698926dfb9
Jon Bauman <jbauman@mozilla.com>
2021-08-20 23:57:06 +0000

    Bug 1726830 - Update mp4parse-rust to c6ba5af. r=kinetik, a=RyanVM
    Update mp4parse_capi API to receive pixi data from parser
    
    There are some necessary changes in nsAVIFDecoder.cpp to accommodate the mp4parse_capi changes. Aside from the addition of `BitsPerChannelToBitDepth`, to facilitate a bit of logging, there should be no functional changes. This is a prerequisite to [[ https://bugzilla.mozilla.org/show_bug.cgi?id=1696045 | bug 1696045 ]], which will add telemetry around the `pixi` box.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123273

--- third_party/rust/mp4parse_capi/src/lib.rs
+++ third_party/rust/mp4parse_capi/src/lib.rs
@@ -316,16 +316,23 @@ pub struct Mp4parseParser {
 
 #[repr(C)]
 #[derive(Debug)]
+pub struct Mp4parseAvifImageItem {
+    pub coded_data: Mp4parseByteData,
+    pub bits_per_channel: Mp4parseByteData,
+}
+
+#[repr(C)]
+#[derive(Debug)]
 pub struct Mp4parseAvifImage {
-    pub primary_item: Mp4parseByteData,
+    pub primary_image: Mp4parseAvifImageItem,
     /// The size of the image; should never be null unless using permissive parsing
     pub spatial_extents: *const mp4parse::ImageSpatialExtentsProperty,
     pub nclx_colour_information: *const mp4parse::NclxColourInformation,
     pub icc_colour_information: Mp4parseByteData,
     pub image_rotation: mp4parse::ImageRotation,
     pub image_mirror: *const mp4parse::ImageMirror,
-    /// If no alpha item exists, `.length` will be 0 and `.data` will be null
-    pub alpha_item: Mp4parseByteData,
+    /// If no alpha item exists, members' `.length` will be 0 and `.data` will be null
+    pub alpha_image: Mp4parseAvifImageItem,
     pub premultiplied_alpha: bool,
 }
 
@@ -1022,8 +1029,8 @@ fn mp4parse_get_track_video_info_safe(
 /// pointer points to a valid `Mp4parseAvifParser`, and that the avif_image
 /// pointer points to a valid `Mp4parseAvifImage`. If there was not a previous
 /// successful call to `mp4parse_avif_read()`, no guarantees are made as to
-/// the state of `avif_image`. If `avif_image.alpha_item` is set to a
-/// positive `length` and non-null `data`, then the `avif_image` contains an
+/// the state of `avif_image`. If `avif_image.alpha_image.coded_data` is set to
+/// a positive `length` and non-null `data`, then the `avif_image` contains a
 /// valid alpha channel data. Otherwise, the image is opaque.
 #[no_mangle]
 pub unsafe extern "C" fn mp4parse_avif_get_image(
@@ -1047,17 +1054,25 @@ pub fn mp4parse_avif_get_image_safe(
 ) -> mp4parse::Result<Mp4parseAvifImage> {
     let context = parser.context();
 
+    let primary_image = Mp4parseAvifImageItem {
+        coded_data: Mp4parseByteData::with_data(context.primary_item_coded_data()),
+        bits_per_channel: Mp4parseByteData::with_data(context.primary_item_bits_per_channel()?),
+    };
+
+    // If there is no alpha present, all the `Mp4parseByteData`s will be zero length
+    let alpha_image = Mp4parseAvifImageItem {
+        coded_data: Mp4parseByteData::with_data(context.alpha_item_coded_data()),
+        bits_per_channel: Mp4parseByteData::with_data(context.alpha_item_bits_per_channel()?),
+    };
+
     Ok(Mp4parseAvifImage {
-        primary_item: Mp4parseByteData::with_data(context.primary_item()),
+        primary_image,
         spatial_extents: context.spatial_extents_ptr()?,
         nclx_colour_information: context.nclx_colour_information_ptr()?,
         icc_colour_information: Mp4parseByteData::with_data(context.icc_colour_information()?),
         image_rotation: context.image_rotation()?,
         image_mirror: context.image_mirror_ptr()?,
-        alpha_item: context
-            .alpha_item()
-            .map(Mp4parseByteData::with_data)
-            .unwrap_or_default(),
+        alpha_image,
         premultiplied_alpha: context.premultiplied_alpha,
     })
 }
