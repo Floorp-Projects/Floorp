1ffba5e8b943203cfd3b61ac8143cd18c9416643
Haik Aftandilian <haftandilian@mozilla.com>
2021-08-20 18:54:04 +0000

    Bug 1722758 - Patch 4 - Encode additional characters in the URL required for NSURL compatiblity r=mac-reviewers,mstange
    Perform additional encoding of URLs before converting to an NSURL as required per the `NSURL URLWithString:` method documentation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122653

--- widget/tests/unit/test_macsharingservice.js
+++ widget/tests/unit/test_macsharingservice.js
@@ -9,7 +9,37 @@ function test_getSharingProviders() {
   let sharingService = Cc["@mozilla.org/widget/macsharingservice;1"].getService(
     Ci.nsIMacSharingService
   );
-  let providers = sharingService.getSharingProviders("http://example.org");
+
+  // Ensure these URL's are accepted without error by the getSharingProviders()
+  // method. This does not test if the URL's are interpreted correctly by
+  // the platform implementation and does not test that the URL will be
+  // successfully shared to the target application if the shareURL method is
+  // used. It does indicate the Mac API's used to get the sharing providers
+  // successfully created a URL object for the URL provided and returned at
+  // least one provider.
+  let urls = [
+    "http://example.org",
+    "http://example.org/#",
+    "http://example.org/dkl??",
+    "http://example.org/dkl?a=b;c=d#thisisaref",
+    "http://example.org/dkl?a=b;c=d#thisisaref#double",
+    "http://example.org/#/",
+    "http://example.org/#/#",
+    "http://example.org/#/#/",
+    "http://example.org/foo/bar/x|page.html#this_is_a_fragment",
+    "http://example.org/page.html#this_is_a_fragment",
+    "http://example.org/page.html#this_is_a_fragment#and_another",
+    "http://example.org/foo/bar;#foo",
+    "http://example.org/a file with spaces.html",
+    "https://chat.mozilla.org/#/room/#macdev:mozilla.org",
+    "https://chat.mozilla.org/#/room/%23macdev:mozilla.org",
+  ];
+
+  urls.forEach(url => testGetSharingProvidersForUrl(sharingService, url));
+}
+
+function testGetSharingProvidersForUrl(sharingService, url) {
+  let providers = sharingService.getSharingProviders(url);
   Assert.greater(providers.length, 1, "There are providers returned");
   providers.forEach(provider => {
     Assert.ok("name" in provider, "Provider has name");
