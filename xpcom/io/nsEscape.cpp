a86c654e4d583d87c1a6c630096a1755c35279aa
Haik Aftandilian <haftandilian@mozilla.com>
2021-08-20 18:54:03 +0000

    Bug 1722758 - Patch 3 - Change netCharType to be an unsigned char array instead of int to save space r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D122652

--- xpcom/io/nsEscape.cpp
+++ xpcom/io/nsEscape.cpp
@@ -17,7 +17,7 @@
 static const char hexCharsUpper[] = "0123456789ABCDEF";
 static const char hexCharsUpperLower[] = "0123456789ABCDEFabcdef";
 
-static const int netCharType[256] =
+static const unsigned char netCharType[256] =
     // clang-format off
 /*  Bit 0       xalpha      -- the alphas
 **  Bit 1       xpalpha     -- as xalpha but
@@ -56,7 +56,7 @@ static const int netCharType[256] =
      ((C >= 'a' && C <= 'f') ? C - 'a' + 10 : 0)))
 // clang-format on
 
-#define IS_OK(C) (netCharType[((unsigned int)(C))] & (aFlags))
+#define IS_OK(C) (netCharType[((unsigned char)(C))] & (aFlags))
 #define HEX_ESCAPE '%'
 
 static const uint32_t ENCODE_MAX_LEN = 6;  // %uABCD
