6692fa0e63337deb61a0e3028c848f5ce1f1005f
Alexandru Michis <malexandru@mozilla.com>
2021-08-21 21:17:21 +0300

    Backed out changeset 6aaaf18f2aae (bug 1722567) for causing bc failures in browser_forget_async_closings.js
    CLOSED TREE

--- browser/base/content/test/tabs/browser_undo_close_tabs.js
+++ browser/base/content/test/tabs/browser_undo_close_tabs.js
@@ -26,12 +26,12 @@ add_task(async function withMultiSelectedTabs() {
   ok(tab2.multiselected, "Tab2 is multiselected");
   ok(tab3.multiselected, "Tab3 is multiselected");
   ok(tab4.multiselected, "Tab4 is multiselected");
-  is(gBrowser.multiSelectedTabsCount, 3, "Three multiselected tabs");
+  is(gBrowser.multiSelectedTabsCount, 3, "Two multiselected tabs");
 
   gBrowser.removeMultiSelectedTabs();
   await TestUtils.waitForCondition(
-    () => SessionStore.getLastClosedTabCount(window) == 3,
-    "wait for the multi selected tabs to close in SessionStore"
+    () => gBrowser.tabs.length == 2,
+    "wait for the multiselected tabs to close"
   );
   is(
     SessionStore.getLastClosedTabCount(window),
@@ -44,13 +44,6 @@ add_task(async function withMultiSelectedTabs() {
     () => gBrowser.tabs.length == 5,
     "wait for the tabs to reopen"
   );
-
-  is(
-    SessionStore.getLastClosedTabCount(window),
-    SessionStore.getClosedTabCount(window) ? 1 : 0,
-    "LastClosedTabCount should be reset"
-  );
-
   info("waiting for the browsers to finish loading");
   // Check that the tabs are restored in the correct order
   for (let tabId of [2, 3, 4]) {
@@ -63,67 +56,6 @@ add_task(async function withMultiSelectedTabs() {
         );
       }, "waiting for tab " + aTabId + " to load");
     });
-  }
-
-  gBrowser.removeAllTabsBut(initialTab);
-});
-
-add_task(async function withBothGroupsAndTab() {
-  let initialTab = gBrowser.selectedTab;
-  let tab1 = await addTab("https://example.com/1");
-  let tab2 = await addTab("https://example.com/2");
-  let tab3 = await addTab("https://example.com/3");
-
-  gBrowser.selectedTab = tab2;
-  await triggerClickOn(tab3, { shiftKey: true });
-
-  ok(!initialTab.multiselected, "InitialTab is not multiselected");
-  ok(!tab1.multiselected, "Tab1 is not multiselected");
-  ok(tab2.multiselected, "Tab2 is multiselected");
-  ok(tab3.multiselected, "Tab3 is multiselected");
-  is(gBrowser.multiSelectedTabsCount, 2, "Two multiselected tabs");
-
-  gBrowser.removeMultiSelectedTabs();
-  await TestUtils.waitForCondition(
-    () => gBrowser.tabs.length == 2,
-    "wait for the multiselected tabs to close"
-  );
-
-  is(
-    SessionStore.getLastClosedTabCount(window),
-    2,
-    "SessionStore should know how many tabs were just closed"
-  );
-
-  let tab4 = await addTab("http://example.com/4");
-
-  is(
-    SessionStore.getLastClosedTabCount(window),
-    2,
-    "LastClosedTabCount should be the same"
-  );
-
-  gBrowser.removeTab(tab4);
-
-  await TestUtils.waitForCondition(
-    () => SessionStore.getLastClosedTabCount(window) == 1,
-    "wait for the tab to close in SessionStore"
-  );
-
-  let count = 3;
-  for (let i = 0; i < 3; i++) {
-    is(
-      SessionStore.getLastClosedTabCount(window),
-      1,
-      "LastClosedTabCount should be one"
-    );
-    undoCloseTab();
-
-    await TestUtils.waitForCondition(
-      () => gBrowser.tabs.length == count,
-      "wait for the tabs to reopen"
-    );
-    count++;
   }
 
   gBrowser.removeAllTabsBut(initialTab);
