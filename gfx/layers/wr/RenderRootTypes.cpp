c46752adac161948ac8947f75bfc58a3c258e0a5
Glenn Watson <git@intuitionlibrary.com>
2021-08-22 20:54:07 +0000

    Bug 1724846 - Split DL spatial tree to separate payload r=gfx-reviewers,nical
    This will allow experimenting with different representations of
    the spatial tree (such as interning and/or providing stable
    indices during display list building). It may also simplify
    future changes to the public API to expose the spatial tree
    directly.
    
    As part of these changes, refactor how the debug representation
    for the capture format is (de)serialized, to make it simpler to
    add different payload vector types in future.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122183

--- gfx/layers/wr/RenderRootTypes.cpp
+++ gfx/layers/wr/RenderRootTypes.cpp
@@ -18,6 +18,7 @@ void IPDLParamTraits<mozilla::layers::DisplayListData>::Write(
   WriteIPDLParam(aMsg, aActor, aParam.mCommands);
   WriteIPDLParam(aMsg, aActor, std::move(aParam.mDLItems));
   WriteIPDLParam(aMsg, aActor, std::move(aParam.mDLCache));
+  WriteIPDLParam(aMsg, aActor, std::move(aParam.mDLSpatialTree));
   WriteIPDLParam(aMsg, aActor, aParam.mDLDesc);
   WriteIPDLParam(aMsg, aActor, aParam.mRemotePipelineIds);
   WriteIPDLParam(aMsg, aActor, aParam.mResourceUpdates);
@@ -34,6 +35,7 @@ bool IPDLParamTraits<mozilla::layers::DisplayListData>::Read(
       ReadIPDLParam(aMsg, aIter, aActor, &aResult->mCommands) &&
       ReadIPDLParam(aMsg, aIter, aActor, &aResult->mDLItems) &&
       ReadIPDLParam(aMsg, aIter, aActor, &aResult->mDLCache) &&
+      ReadIPDLParam(aMsg, aIter, aActor, &aResult->mDLSpatialTree) &&
       ReadIPDLParam(aMsg, aIter, aActor, &aResult->mDLDesc) &&
       ReadIPDLParam(aMsg, aIter, aActor, &aResult->mRemotePipelineIds) &&
       ReadIPDLParam(aMsg, aIter, aActor, &aResult->mResourceUpdates) &&
