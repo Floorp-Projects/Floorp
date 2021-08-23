0d6f7f1c2bbd12999dcf33011828ef408965793b
Jeff Gilbert <jgilbert@mozilla.com>
2021-08-23 23:59:26 +0000

    Bug 1726863 - Cache/elide GLContext::BindFramebuffer calls. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D123275

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -635,6 +635,7 @@ void WebGLContext::FinishInit() {
   // Initial setup.
 
   gl->mImplicitMakeCurrent = true;
+  gl->mElideDuplicateBindFramebuffers = true;
 
   const auto& size = mDefaultFB->mSize;
 
