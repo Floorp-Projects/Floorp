6d33922813935105e2fb5f479edd850b3c9ac54b
Julian Descottes <jdescottes@mozilla.com>
2021-08-23 07:59:07 +0000

    Bug 1725999 - [devtools] Remove backward compatibility code for ThreadConfigurationOptions r=bomsy
    Depends on D122751
    
    Differential Revision: https://phabricator.services.mozilla.com/D122758

--- devtools/shared/commands/thread-configuration/thread-configuration-command.js
+++ devtools/shared/commands/thread-configuration/thread-configuration-command.js
@@ -48,25 +48,17 @@ class ThreadConfigurationCommand {
       "thread"
     );
 
-    // @backward-compat { version 91 } Thread configuration actor now supports most thread options
-    // When `supportsThreadConfigurationOptions` is removed, this condition should no longer required.
-    // The code should run unconditionally.
-    if (
-      this._commands.targetCommand.rootFront.traits
-        .supportsThreadConfigurationOptions
-    ) {
-      // Lets always call reconfigure for all the target types that do not
-      // have target watcher support yet. e.g In the browser, even
-      // though `hasTargetWatcherSupport()` is true, only
-      // FRAME and CONTENT PROCESS targets use watcher actors,
-      // WORKER targets are supported via the legacy listerners.
-      threadFronts = threadFronts.filter(
-        threadFront =>
-          !this._commands.targetCommand.hasTargetWatcherSupport(
-            threadFront.targetFront.targetType
-          )
-      );
-    }
+    // Lets always call reconfigure for all the target types that do not
+    // have target watcher support yet. e.g In the browser, even
+    // though `hasTargetWatcherSupport()` is true, only
+    // FRAME and CONTENT PROCESS targets use watcher actors,
+    // WORKER targets are supported via the legacy listerners.
+    threadFronts = threadFronts.filter(
+      threadFront =>
+        !this._commands.targetCommand.hasTargetWatcherSupport(
+          threadFront.targetFront.targetType
+        )
+    );
 
     await Promise.all(
       threadFronts.map(threadFront => threadFront.reconfigure(configuration))
