0f28cbfa87a9c13401b9ea0eb086dbe9acef89d1
Jan de Mooij <jdemooij@mozilla.com>
2021-08-24 09:20:45 +0000

    Bug 1726533 part 4 - Store the shape instead of object for NewObject ops. r=arai
    We still allocate the object in the Stencil instantiation code, and then only
    use its shape. This will be fixed in a later patch to allocate only the shape.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123093

--- js/src/vm/Interpreter.cpp
+++ js/src/vm/Interpreter.cpp
@@ -5028,10 +5028,9 @@ bool js::OptimizeSpreadCall(JSContext* cx, HandleValue arg, bool* optimized) {
 
 JSObject* js::NewObjectOperation(JSContext* cx, HandleScript script,
                                  jsbytecode* pc) {
-  // Extract the template object, if one exists, and copy it.
   if (JSOp(*pc) == JSOp::NewObject) {
-    RootedPlainObject baseObject(cx, &script->getObject(pc)->as<PlainObject>());
-    return CopyTemplateObject(cx, baseObject, GenericObject);
+    RootedShape shape(cx, script->getShape(pc));
+    return PlainObject::createWithShape(cx, shape);
   }
 
   MOZ_ASSERT(JSOp(*pc) == JSOp::NewInit);
