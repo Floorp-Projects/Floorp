2616737707c16cdf9dcf1205e75f45a96773b226
André Bargull <andre.bargull@gmail.com>
2021-08-24 09:34:51 +0000

    Bug 1648137 - Part 15: Record the source of a number part. r=platform-i18n-reviewers,nordzilla
    Add `NumberPartSource` to `NumberPart` in preparation for part 18.
    
    Also changes the underlying type of `NumberPartType` to `int16_t` to ensure the
    size of `NumberPart` doesn't change on 32-bit platforms when adding
    `NumberPartSource`. (Note: `sizeof(NumberPart)` is 16 on 64-platforms with or
    without this change.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D119755

--- intl/components/src/NumberFormatFields.h
+++ intl/components/src/NumberFormatFields.h
@@ -24,6 +24,33 @@ struct NumberFormatField {
       : begin(begin), end(end), type(type) {}
 };
 
+struct NumberPartSourceMap {
+  struct Range {
+    uint32_t begin = 0;
+    uint32_t end = 0;
+  };
+
+  // Begin and end position of the start range.
+  Range start;
+
+  // Begin and end position of the end range.
+  Range end;
+
+  NumberPartSource source(uint32_t endIndex) {
+    if (start.begin < endIndex && endIndex <= start.end) {
+      return NumberPartSource::Start;
+    }
+    if (end.begin < endIndex && endIndex <= end.end) {
+      return NumberPartSource::End;
+    }
+    return NumberPartSource::Shared;
+  }
+
+  NumberPartSource source(const NumberFormatField& field) {
+    return source(field.end);
+  }
+};
+
 class NumberFormatFields {
   using FieldsVector = Vector<NumberFormatField, 16>;
 
@@ -33,6 +60,12 @@ class NumberFormatFields {
   [[nodiscard]] bool append(NumberPartType type, int32_t begin, int32_t end);
 
   [[nodiscard]] bool toPartsVector(size_t overallLength,
+                                   NumberPartVector& parts) {
+    return toPartsVector(overallLength, {}, parts);
+  }
+
+  [[nodiscard]] bool toPartsVector(size_t overallLength,
+                                   const NumberPartSourceMap& sourceMap,
                                    NumberPartVector& parts);
 };
 
