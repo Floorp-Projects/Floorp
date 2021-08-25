cf6738f50f7555f697f6429e66e8108b7f41aae0
Mirko Brodesser <mbrodesser@mozilla.com>
2021-08-25 07:29:55 +0000

    Bug 1727271: part 1) Simplify and document `nsContentPermissionUtils::ConvertArrayToPermissionRequest`. r=edgar
    Differential Revision: https://phabricator.services.mozilla.com/D123428

--- dom/base/nsContentPermissionHelper.h
+++ dom/base/nsContentPermissionHelper.h
@@ -60,7 +60,11 @@ class nsContentPermissionUtils {
   static uint32_t ConvertPermissionRequestToArray(
       nsTArray<PermissionRequest>& aSrcArray, nsIMutableArray* aDesArray);
 
-  static uint32_t ConvertArrayToPermissionRequest(
+  // Converts blindly, that is, strings are not matched against any list.
+  //
+  // @param aSrcArray needs to contain elements of type
+  // `nsIContentPermissionType`.
+  static void ConvertArrayToPermissionRequest(
       nsIArray* aSrcArray, nsTArray<PermissionRequest>& aDesArray);
 
   static nsresult CreatePermissionArray(const nsACString& aType,
