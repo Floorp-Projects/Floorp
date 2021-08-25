2f70492b7a9b8099f2e9cb8bb2199be4f08c77b7
Mike Hommey <mh+mozilla@glandium.org>
2021-08-25 07:39:02 +0000

    Bug 1726474 - Use clang.exe to build wasm files when the clang plugin is enabled. r=firefox-build-system-reviewers,andi
    Differential Revision: https://phabricator.services.mozilla.com/D123591

--- config/static-checking-config.mk
+++ config/static-checking-config.mk
@@ -9,8 +9,13 @@ ifdef ENABLE_CLANG_PLUGIN
 # Replace "clang-cl.exe" to "clang.exe --driver-mode=cl" to avoid loading the
 # module clang.exe again when load the plugin dll, which links to the import
 # library of clang.exe.
+# Likewise with "clang++.exe", replacing it with "clang.exe --driver-mode=g++",
+# when building .wasm files from source (we do need to keep clang++ when
+# building $(WASM_ARCHIVE)). Note we'd normally use $(CPPWASMOBJS), but it's
+# not defined yet when this file is included.
 ifeq ($(OS_ARCH),WINNT)
 CC := $(subst clang-cl.exe,clang.exe --driver-mode=cl,$(CC:.EXE=.exe))
 CXX := $(subst clang-cl.exe,clang.exe --driver-mode=cl,$(CXX:.EXE=.exe))
+$(notdir $(addsuffix .$(WASM_OBJ_SUFFIX),$(basename $(WASM_CPPSRCS)))): WASM_CXX := $(subst clang++.exe,clang.exe --driver-mode=g++,$(WASM_CXX:.EXE=.exe))
 endif
 endif
