16f6a2c91f0384120aeb8ea18754ff9d3e4ad347
Jeff Muizelaar <jmuizelaar@mozilla.com>
2021-08-25 01:11:45 +0000

    Bug 1727447 - Stop using PushGroupAndCopyBackground when painting masks. r=mattwoodrow
    We don't support COMPONENT_ALPHA anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123576

--- layout/svg/SVGIntegrationUtils.cpp
+++ layout/svg/SVGIntegrationUtils.cpp
@@ -952,18 +952,9 @@ void PaintMaskAndClipPathInternal(const PaintFramesParams& aParams,
     }
 
     if (shouldPushMask) {
-      if (aParams.layerManager &&
-          aParams.layerManager->GetRoot()->GetContentFlags() &
-              Layer::CONTENT_COMPONENT_ALPHA) {
-        context.PushGroupAndCopyBackground(
-            gfxContentType::COLOR_ALPHA,
-            opacityApplied ? 1.0 : maskUsage.opacity, maskSurface,
-            maskTransform);
-      } else {
-        context.PushGroupForBlendBack(gfxContentType::COLOR_ALPHA,
-                                      opacityApplied ? 1.0 : maskUsage.opacity,
-                                      maskSurface, maskTransform);
-      }
+      context.PushGroupForBlendBack(gfxContentType::COLOR_ALPHA,
+                                    opacityApplied ? 1.0 : maskUsage.opacity,
+                                    maskSurface, maskTransform);
     }
   }
 
