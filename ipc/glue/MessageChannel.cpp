7315c581cd8a68e295bd3736cae5404b7d80fde3
Nika Layzell <nika@thelayzells.com>
2021-08-25 18:30:25 +0000

    Bug 1715755 - Part 3: Don't defer messages if the interrupt stack is empty, r=handyman
    Previously this would misbehave as it couldn't get the interrupt stack.
    
    Depends on D123149
    
    Differential Revision: https://phabricator.services.mozilla.com/D123150

--- ipc/glue/MessageChannel.cpp
+++ ipc/glue/MessageChannel.cpp
@@ -2110,7 +2110,8 @@ bool MessageChannel::ShouldDeferInterruptMessage(const Message& aMsg,
   // MessageChannel.h. "Remote" stack depth means our side, and "local" means
   // the other side.
   if (aMsg.interrupt_remote_stack_depth_guess() ==
-      RemoteViewOfStackDepth(aStackDepth)) {
+          RemoteViewOfStackDepth(aStackDepth) ||
+      mInterruptStack.empty()) {
     return false;
   }
 
