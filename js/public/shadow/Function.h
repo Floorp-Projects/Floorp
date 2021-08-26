307c1a6d1c5a1f4a9b14125ae24fe1707c876000
Jon Coppeard <jcoppeard@mozilla.com>
2021-08-26 14:57:17 +0000

    Bug 1724031 - Part 2: Store a functions flags and argument count in a single 32-bit field r=jandem
    In preparation for storing this in a slot, this stores the two values together
    in the same field.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123082

--- js/public/shadow/Function.h
+++ js/public/shadow/Function.h
@@ -25,8 +25,7 @@ namespace shadow {
 
 struct Function {
   shadow::Object base;
-  uint16_t nargs;
-  uint16_t flags;
+  uint32_t flagsAndArgCount;
   /* Used only for natives */
   JSNative native;
   const JSJitInfo* jitinfo;
