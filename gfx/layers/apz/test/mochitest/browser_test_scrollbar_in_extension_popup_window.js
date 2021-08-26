21d8da452dffea166ccf8b4e2fd2cf7a809d03d9
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2021-08-26 05:24:29 +0000

    Bug 1723315 - Ensure there's no pending paint requests before taking a snapshot. r=sotaro
    There's no handy way to use functions in paint_listener.js inside extension's
    popup document's context, if paint_listener.js gets loaded with a script tag
    inside the document, it doesn't allow us using nsIDOMWindowUtils since it is in
    extension's privilege. Running a similar chunk of the function code in
    SpecialPowers.spawn is the simplest way to use the function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123588

--- gfx/layers/apz/test/mochitest/browser_test_scrollbar_in_extension_popup_window.js
+++ gfx/layers/apz/test/mochitest/browser_test_scrollbar_in_extension_popup_window.js
@@ -74,6 +74,33 @@ add_task(async () => {
 
   async function takeSnapshot(browserWin) {
     let browser = await openBrowserActionPanel(extension, browserWin, true);
+
+    // Ensure there's no pending paint requests.
+    // The below code is a simplified version of promiseAllPaintsDone in
+    // paint_listener.js.
+    await SpecialPowers.spawn(browser, [], async () => {
+      return new Promise(resolve => {
+        function waitForPaints() {
+          // Wait until paint suppression has ended
+          if (SpecialPowers.DOMWindowUtils.paintingSuppressed) {
+            dump`waiting for paint suppression to end...`;
+            content.window.setTimeout(waitForPaints, 0);
+            return;
+          }
+
+          if (SpecialPowers.DOMWindowUtils.isMozAfterPaintPending) {
+            dump`waiting for paint...`;
+            content.window.addEventListener("MozAfterPaint", waitForPaints, {
+              once: true,
+            });
+            return;
+          }
+          resolve();
+        }
+        waitForPaints();
+      });
+    });
+
     const snapshot = await SpecialPowers.spawn(browser, [], async () => {
       return SpecialPowers.snapshotWindowWithOptions(
         content.window,
