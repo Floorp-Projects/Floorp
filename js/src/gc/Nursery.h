81499a6d7bb460962d1dde7e57a0f95ea31dd4f5
Jon Coppeard <jcoppeard@mozilla.com>
2021-08-26 14:57:21 +0000

    Bug 1724031 - Part 11: Prevent gecko profiler from observing moved function pointers r=jandem
    Changing functions to use fixed slots creates a problem when the profiler
    tries to get the script for on-stack functions. Unfortunately this can happen
    during minor GC after the functions have been moved but before the profiler
    data pointing to them has been updated.
    
    The function data itself is mostly intact, but the object header (used for the
    shape) is not valid. As far as I can see this only affects the debug assertions
    around checking that slot numbers are valid. However the simplest fix is to
    suppress the profiler during the intial part of minor GC, until the roots have
    been traced (which will update the profiler data).
    
    The patch moves the code to trace the roots in a minor GC to a separate method
    and adds a guard to suppress the profiler.œ
    
    What do you think?  Alternatively we would have to add a load of 'unchecked'
    methods to a buch of accessors on JSFunction, and to NativeObject to get fixed
    slots without touching the shape.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123642

--- js/src/gc/Nursery.h
+++ js/src/gc/Nursery.h
@@ -66,6 +66,7 @@ class MapObject;
 class SetObject;
 
 namespace gc {
+class AutoGCSession;
 class AutoMaybeStartBackgroundAllocation;
 class AutoTraceSession;
 struct Cell;
@@ -704,6 +705,7 @@ class Nursery {
     size_t tenuredCells;
   };
   CollectionResult doCollection(JS::GCReason reason);
+  void traceRoots(gc::AutoGCSession& session, TenuringTracer& mover);
 
   size_t doPretenuring(JSRuntime* rt, JS::GCReason reason,
                        bool validPromotionRate, double promotionRate);
