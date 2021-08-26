82883ac180835f2521aafc906bdb9f434d813b33
Bryce Seager van Dyk <bvandyk@mozilla.com>
2021-08-26 18:37:02 +0000

    Bug 1726024 - Handle failure to get async shutdown service in GetShutdownBarrier. r=jib
    We will fail to get the service if this function is called and we're already in
    shutdown. Call sites for this function do not guard against doing so (and I'm
    not sure there's a nice way to check). So we need to handle failure in the
    function and have call sites check if this failed.
    
    An alternative function is added, MustGetShutdownBarrier, which maintains the
    original behaviour. Callers that still wish to crash on failure can instead opt
    to use this function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123001

--- dom/media/systemservices/MediaUtils.h
+++ dom/media/systemservices/MediaUtils.h
@@ -155,6 +155,9 @@ class Refcountable<bool> : public RefcountableBase {
 
 nsCOMPtr<nsIAsyncShutdownClient> GetShutdownBarrier();
 
+// Like GetShutdownBarrier but will release assert that the result is not null.
+nsCOMPtr<nsIAsyncShutdownClient> MustGetShutdownBarrier();
+
 class ShutdownBlocker : public nsIAsyncShutdownBlocker {
  public:
   ShutdownBlocker(const nsString& aName) : mName(aName) {}
