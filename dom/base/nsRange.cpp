6481b69a6c3e266145dbfd7606f74e20defde501
Jonathan Kew <jkew@mozilla.com>
2021-08-26 16:56:43 +0000

    Bug 1725555 - patch 2 - Enable the primary nsTextFrame to provide, and cache, an array of all the continuations in the chain. r=emilio
    This allows us to binary-search the continuations from nsRange::CollectClientRectsAndText,
    instead of linear-searching the linked list for every range we need to look up.
    
    Depends on D122998
    
    Differential Revision: https://phabricator.services.mozilla.com/D122999

--- dom/base/nsRange.cpp
+++ dom/base/nsRange.cpp
@@ -2647,10 +2647,40 @@ static nsresult GetPartialTextRect(RectCallback* aCallback,
   if (textFrame) {
     nsIFrame* relativeTo =
         nsLayoutUtils::GetContainingBlockForClientRect(textFrame);
-    for (nsTextFrame* f = textFrame; f;
-         f = static_cast<nsTextFrame*>(f->GetNextContinuation())) {
+
+    // Try to binary-search the list of continuations for the starting point.
+    // (GetContinuations is fallible; if it returns nullptr, we'll just start
+    // from the beginning.)
+    nsTArray<nsTextFrame*>* continuations = textFrame->GetContinuations();
+    nsTextFrame* f = textFrame;
+    if (continuations) {
+      size_t index;
+      if (BinarySearchIf(
+              *continuations, 0, continuations->Length(),
+              [=](nsTextFrame* aFrame) -> int {
+                if (aStartOffset < aFrame->GetContentOffset()) {
+                  return -1;
+                }
+                if (aStartOffset > aFrame->GetContentOffset()) {
+                  return 1;
+                }
+                return 0;
+              },
+              &index)) {
+        f = (*continuations)[index];
+      } else {
+        f = (*continuations)[index ? index - 1 : 0];
+      }
+    }
+
+    for (; f; f = static_cast<nsTextFrame*>(f->GetNextContinuation())) {
       int32_t fstart = f->GetContentOffset(), fend = f->GetContentEnd();
-      if (fend <= aStartOffset || fstart >= aEndOffset) continue;
+      if (fend <= aStartOffset) {
+        continue;
+      }
+      if (fstart >= aEndOffset) {
+        break;
+      }
 
       // Calculate the text content offsets we'll need if text is requested.
       int32_t textContentStart = fstart;
