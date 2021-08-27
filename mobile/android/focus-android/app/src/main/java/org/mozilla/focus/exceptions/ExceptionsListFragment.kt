0b61b1217fade5dea5835391af4864a7290ad45c
mcarare <mihai.carare.dev@gmail.com>
2021-08-27 18:17:16 +0300

    [focus] For https://github.com/mozilla-mobile/focus-android/issues/5241: Hide delete all websites button when in select mode.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/exceptions/ExceptionsListFragment.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/exceptions/ExceptionsListFragment.kt
@@ -17,6 +17,7 @@ import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.CompoundButton
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.ItemTouchHelper.SimpleCallback
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -112,6 +113,8 @@ open class ExceptionsListFragment : BaseSettingsLikeFragment(), CoroutineScope {
             LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         exceptionList.adapter = DomainListAdapter()
         exceptionList.setHasFixedSize(true)
+
+        removeAllExceptions.isVisible = !isSelectionMode()
 
         if (!isSelectionMode()) {
             itemTouchHelper.attachToRecyclerView(exceptionList)
