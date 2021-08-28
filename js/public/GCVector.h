7b3740670d353f81c012d37bd65a630c981081b2
Jon Coppeard <jcoppeard@mozilla.com>
2021-08-28 12:17:58 +0000

    Bug 1721562 - Remove unimplemented GCVector methods from MutableWrappedPtrOperations for GCVector r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D123654

--- js/public/GCVector.h
+++ js/public/GCVector.h
@@ -273,15 +273,6 @@ class MutableWrappedPtrOperations<JS::GCVector<T, Capacity, AllocPolicy>,
   [[nodiscard]] bool resize(size_t aNewLength) {
     return vec().resize(aNewLength);
   }
-  [[nodiscard]] bool growByUninitialized(size_t aIncr) {
-    return vec().growByUninitialized(aIncr);
-  }
-  void infallibleGrowByUninitialized(size_t aIncr) {
-    vec().infallibleGrowByUninitialized(aIncr);
-  }
-  [[nodiscard]] bool resizeUninitialized(size_t aNewLength) {
-    return vec().resizeUninitialized(aNewLength);
-  }
   void clear() { vec().clear(); }
   void clearAndFree() { vec().clearAndFree(); }
   template <typename U>
@@ -328,10 +319,6 @@ class MutableWrappedPtrOperations<JS::GCVector<T, Capacity, AllocPolicy>,
   }
   void popBack() { vec().popBack(); }
   T popCopy() { return vec().popCopy(); }
-  template <typename U>
-  T* insert(T* aP, U&& aVal) {
-    return vec().insert(aP, std::forward<U>(aVal));
-  }
   void erase(T* aT) { vec().erase(aT); }
   void erase(T* aBegin, T* aEnd) { vec().erase(aBegin, aEnd); }
   template <typename Pred>
