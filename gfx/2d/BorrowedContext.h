fe21b00004cae91dd7a97b3bb4d6742d59d25b6d
Andrew Osmond <aosmond@mozilla.com>
2021-08-30 16:35:49 +0000

    Bug 1728112 - Remove BorrowedCairoContext. r=jrmuizel
    This code is no longer used by anything.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123945

--- gfx/2d/BorrowedContext.h
+++ gfx/2d/BorrowedContext.h
@@ -15,57 +15,9 @@
 #  include "X11UndefineNone.h"
 #endif
 
-struct _cairo;
-typedef struct _cairo cairo_t;
-
 namespace mozilla {
 
 namespace gfx {
-
-/* This is a helper class that let's you borrow a cairo_t from a
- * DrawTargetCairo. This is used for drawing themed widgets.
- *
- * Callers should check the cr member after constructing the object
- * to see if it succeeded. The DrawTarget should not be used while
- * the context is borrowed. */
-class BorrowedCairoContext {
- public:
-  BorrowedCairoContext() : mCairo(nullptr), mDT(nullptr) {}
-
-  explicit BorrowedCairoContext(DrawTarget* aDT) : mDT(aDT) {
-    mCairo = BorrowCairoContextFromDrawTarget(aDT);
-  }
-
-  // We can optionally Init after construction in
-  // case we don't know what the DT will be at construction
-  // time.
-  cairo_t* Init(DrawTarget* aDT) {
-    MOZ_ASSERT(!mDT, "Can't initialize twice!");
-    mDT = aDT;
-    return mCairo = BorrowCairoContextFromDrawTarget(aDT);
-  }
-
-  // The caller needs to call Finish if cr is non-null when
-  // they are done with the context. This is currently explicit
-  // instead of happening implicitly in the destructor to make
-  // what's happening in the caller more clear. It also
-  // let's you resume using the DrawTarget in the same scope.
-  void Finish() {
-    if (mCairo) {
-      ReturnCairoContextToDrawTarget(mDT, mCairo);
-      mCairo = nullptr;
-    }
-  }
-
-  ~BorrowedCairoContext() { MOZ_ASSERT(!mCairo); }
-
-  cairo_t* mCairo;
-
- private:
-  static cairo_t* BorrowCairoContextFromDrawTarget(DrawTarget* aDT);
-  static void ReturnCairoContextToDrawTarget(DrawTarget* aDT, cairo_t* aCairo);
-  DrawTarget* mDT;
-};
 
 #ifdef MOZ_X11
 /* This is a helper class that let's you borrow an Xlib drawable from
