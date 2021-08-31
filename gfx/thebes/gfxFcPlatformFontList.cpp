5a5135002e8add608f8817fbc4852171df4a6a03
Csoregi Natalia <ncsoregi@mozilla.com>
2021-08-31 17:55:00 +0300

    Backed out changeset df6b2d7ac580 (bug 1728086) for failures on system-ui-ar.html. CLOSED TREE

--- gfx/thebes/gfxFcPlatformFontList.cpp
+++ gfx/thebes/gfxFcPlatformFontList.cpp
@@ -2219,12 +2219,7 @@ bool gfxFcPlatformFontList::GetStandardFamilyName(const nsCString& aFontName,
 void gfxFcPlatformFontList::AddGenericFonts(
     StyleGenericFontFamily aGenericType, nsAtom* aLanguage,
     nsTArray<FamilyAndGeneric>& aFamilyList) {
-  if (aGenericType == StyleGenericFontFamily::SystemUi) {
-    // system-ui maps to a system font family, no need to use fontconfig for
-    // it.
-    return gfxPlatformFontList::AddGenericFonts(aGenericType, aLanguage,
-                                                aFamilyList);
-  }
+  bool usePrefFontList = false;
 
   const char* generic = GetGenericName(aGenericType);
   NS_ASSERTION(generic, "weird generic font type");
@@ -2235,8 +2230,6 @@ void gfxFcPlatformFontList::AddGenericFonts(
   // By default, most font prefs on Linux map to "use fontconfig"
   // keywords. So only need to explicitly lookup font pref if
   // non-default settings exist
-  bool usePrefFontList = false;
-
   nsAutoCString genericToLookup(generic);
   if ((!mAlwaysUseFontconfigGenerics && aLanguage) ||
       aLanguage == nsGkAtoms::x_math) {
