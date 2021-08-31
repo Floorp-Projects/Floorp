d9dfd50c8b96cb0352563ddde87f9270d29e0154
Masayuki Nakano <masayuki@d-toybox.com>
2021-08-31 17:37:41 +0000

    Bug 1727844 - part 3: Rename `EditorUtils::IsContentPreformatted()` to `IsWhiteSpacePreformatted()` r=m_kato
    The method returns false even if linefeed characters are preformatted.
    So, it should be renamed to explain what it does clearer.
    
    And this renames `TextFragmentData::mIsPreformatted` and its accessors too.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123870

--- editor/libeditor/HTMLEditSubActionHandler.cpp
+++ editor/libeditor/HTMLEditSubActionHandler.cpp
@@ -1112,8 +1112,8 @@ EditActionResult HTMLEditor::HandleInsertText(
 
   // is our text going to be PREformatted?
   // We remember this so that we know how to handle tabs.
-  bool isPRE =
-      EditorUtils::IsContentPreformatted(*pointToInsert.ContainerAsContent());
+  const bool isWhiteSpaceCollapsible = !EditorUtils::IsWhiteSpacePreformatted(
+      *pointToInsert.ContainerAsContent());
 
   // turn off the edit listener: we know how to
   // build the "doc changed range" ourselves, and it's
@@ -1134,7 +1134,7 @@ EditActionResult HTMLEditor::HandleInsertText(
     // for efficiency, break out the pre case separately.  This is because
     // its a lot cheaper to search the input string for only newlines than
     // it is to search for both tabs and newlines.
-    if (isPRE || IsInPlaintextMode()) {
+    if (!isWhiteSpaceCollapsible || IsInPlaintextMode()) {
       while (pos != -1 &&
              pos < AssertedCast<int32_t>(aInsertionString.Length())) {
         int32_t oldPos = pos;
@@ -2115,14 +2115,14 @@ void HTMLEditor::ExtendRangeToDeleteWithNormalizingWhiteSpaces(
       precedingCharPoint.ContainerAsText() ==
           aStartToDelete.ContainerAsText() &&
       precedingCharPoint.IsCharASCIISpaceOrNBSP() &&
-      !EditorUtils::IsContentPreformatted(
+      !EditorUtils::IsWhiteSpacePreformatted(
           *precedingCharPoint.ContainerAsText());
   const bool maybeNormalizeFollowingWhiteSpaces =
       followingCharPoint.IsSet() && !followingCharPoint.IsEndOfContainer() &&
       (followingCharPoint.ContainerAsText() == aEndToDelete.ContainerAsText() ||
        removingLastCharOfStartNode) &&
       followingCharPoint.IsCharASCIISpaceOrNBSP() &&
-      !EditorUtils::IsContentPreformatted(
+      !EditorUtils::IsWhiteSpacePreformatted(
           *followingCharPoint.ContainerAsText());
 
   if (!maybeNormalizePrecedingWhiteSpaces &&
@@ -5807,7 +5807,7 @@ EditorDOMPoint HTMLEditor::GetCurrentHardLineEndPoint(
 
     // Check for newlines in pre-formatted text nodes.
     if (nextEditableContent->IsText() &&
-        EditorUtils::IsContentPreformatted(*nextEditableContent)) {
+        EditorUtils::IsWhiteSpacePreformatted(*nextEditableContent)) {
       nsAutoString textContent;
       nextEditableContent->GetAsText()->GetData(textContent);
       int32_t newlinePos = textContent.FindChar(nsCRT::LF);
