e6b64b97b154e7156a4133a5ee5d08a2537eafae
Andreas Pehrson <apehrson@mozilla.com>
2021-09-01 10:11:44 +0000

    Bug 1727563 - Implement TryLock methods and RAII helpers for RWLock. r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D124066

--- xpcom/threads/RWLock.cpp
+++ xpcom/threads/RWLock.cpp
@@ -46,6 +46,16 @@ RWLock::~RWLock() {
 }
 #endif
 
+bool RWLock::TryReadLockInternal() {
+#ifdef XP_WIN
+  return TryAcquireSRWLockShared(NativeHandle(mRWLock));
+#else
+  int rv = pthread_rwlock_tryrdlock(NativeHandle(mRWLock));
+  MOZ_RELEASE_ASSERT(rv == 0 || rv == EBUSY, "pthread_rwlock_tryrdlock failed");
+  return rv == 0;
+#endif
+}
+
 void RWLock::ReadLockInternal() {
 #ifdef XP_WIN
   AcquireSRWLockShared(NativeHandle(mRWLock));
@@ -61,6 +71,16 @@ void RWLock::ReadUnlockInternal() {
 #else
   MOZ_RELEASE_ASSERT(pthread_rwlock_unlock(NativeHandle(mRWLock)) == 0,
                      "pthread_rwlock_unlock failed");
+#endif
+}
+
+bool RWLock::TryWriteLockInternal() {
+#ifdef XP_WIN
+  return TryAcquireSRWLockExclusive(NativeHandle(mRWLock));
+#else
+  int rv = pthread_rwlock_trywrlock(NativeHandle(mRWLock));
+  MOZ_RELEASE_ASSERT(rv == 0 || rv == EBUSY, "pthread_rwlock_trywrlock failed");
+  return rv == 0;
 #endif
 }
 
