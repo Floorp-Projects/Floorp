05f592ee16e9997d186c2febb4472560b888f16a
Dão Gottwald <dao@mozilla.com>
2021-09-01 02:43:24 +0000

    Bug 1728368 - Simplify findbar overflow behavior. r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D124086

--- toolkit/content/widgets/findbar.js
+++ toolkit/content/widgets/findbar.js
@@ -65,10 +65,8 @@
         <image anonid="find-status-icon" class="find-status-icon" />
         <description anonid="find-status" control="findbar-textbox" class="findbar-label findbar-find-status" />
       </hbox>
-      <div id="findbar-close-container">
-        <toolbarbutton anonid="find-closebutton" class="findbar-closebutton tabbable close-icon"
-            data-l10n-id="findbar-find-button-close" oncommand="close();" />
-      </div>
+      <toolbarbutton anonid="find-closebutton" class="findbar-closebutton tabbable close-icon"
+        data-l10n-id="findbar-find-button-close" oncommand="close();"/>
       `;
     }
 
