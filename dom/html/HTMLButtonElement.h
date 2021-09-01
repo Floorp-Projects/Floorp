da8d018c080de07b35c81c50e8e07fcdd9ce33d7
Edgar Chen <echen@mozilla.com>
2021-09-01 16:51:58 +0000

    Bug 1728537 - Part 2: Move RestoreState out of nsIFormControl; r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D124180

--- dom/html/HTMLButtonElement.h
+++ dom/html/HTMLButtonElement.h
@@ -41,11 +41,11 @@ class HTMLButtonElement final : public nsGenericHTMLFormElementWithState,
 
   // nsGenericHTMLFormElement
   void SaveState() override;
+  bool RestoreState(PresState* aState) override;
 
   // overriden nsIFormControl methods
   NS_IMETHOD Reset() override;
   NS_IMETHOD SubmitNamesValues(FormData* aFormData) override;
-  bool RestoreState(PresState* aState) override;
   virtual bool IsDisabledForEvents(WidgetEvent* aEvent) override;
 
   virtual void FieldSetDisabledChanged(bool aNotify) override;
--- dom/html/HTMLTextAreaElement.h
+++ dom/html/HTMLTextAreaElement.h
@@ -58,12 +58,12 @@ class HTMLTextAreaElement final : public TextControlElement,
 
   // nsGenericHTMLFormElement
   void SaveState() override;
+  bool RestoreState(PresState* aState) override;
 
   // nsIFormControl
   MOZ_CAN_RUN_SCRIPT_BOUNDARY
   NS_IMETHOD Reset() override;
   NS_IMETHOD SubmitNamesValues(FormData* aFormData) override;
-  virtual bool RestoreState(PresState* aState) override;
   virtual bool IsDisabledForEvents(WidgetEvent* aEvent) override;
 
   virtual void FieldSetDisabledChanged(bool aNotify) override;
