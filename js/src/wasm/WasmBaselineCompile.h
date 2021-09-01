8e0c2babc81133ddd62f857372cca504c9c010aa
Lars T Hansen <lhansen@mozilla.com>
2021-09-01 09:11:50 +0000

    Bug 1727477 - Split baseline compiler into multiple files.  r=jseward
    This is a somewhat rough carving-up of the baseline compiler into
    multiple files, so as to lay the groundwork for easier maintenance and
    further abstraction / refactoring.
    
    The grand prizes here are
    
    (a) the factoring-out of the BaseCompiler type into a header file, WasmBCClass.h, and
    
    (b) the ability to isolate parts of functionality in suitably named files, such as WasmBCMemory.{cpp,h}, which holds all logic for memory access.
    
    With memory64 coming, this isolation will help us identify more
    abstractions and stay sane.  With further work, the compiler will
    become more maintainable.
    
    The existing file WasmBaselineCompile.h remains the API to the
    compiler, and has not changed materially.
    
    WasmBaselineCompile.cpp now has many out-of-line method definitions
    for the compiler but not the compiler class itself, nor all methods;
    this will likely be further carved up over time.
    
    Mostly, inline methods have been moved to -inl.h files, this is
    subject to further adjustment over time.
    
    There are many new files.  Every new file name starts with the prefix
    "WasmBC", for easy identification.  After that we have headers:
    
      Class -- the compiler object itself
      Defs -- basic configuration and a few shared definitions
      Frame -- stack frame and stack map abstractions
      Memory -- a few shared definitions pertaining to heap access
      RegDefs -- register and register allocation abstractions
      Stk -- the Stk value type
    
    Then inline files:
    
      Class-inl -- some inline defs for the compiler
      Codegen-inl -- inline defs for general code generation
      RegDefs-inl -- ditto for register allocator
      RegMgmt-inl -- ditto for register management
      StkMgmg-inl -- almost anything that pushes or pops
    
    Then new cpp files:
    
      Frame -- anything to do with stack frame or stack map
      Memory -- virtually all code for heap access
    
    The astute reader will observe the absence of WasmBCCodegen.cpp, for
    example.  It's my intent to create such a file later and move over
    many methods from WasmBaselineCompile.cpp.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123628

--- js/src/wasm/WasmBaselineCompile.h
+++ js/src/wasm/WasmBaselineCompile.h
@@ -36,6 +36,11 @@ namespace wasm {
     LifoAlloc& lifo, const FuncCompileInputVector& inputs, CompiledCode* code,
     UniqueChars* error);
 
+// BaseLocalIter iterates over a vector of types of locals and provides offsets
+// from the Frame address for those locals, and associated data.
+//
+// The implementation of BaseLocalIter is the property of the BaseStackFrame.
+// But it is also exposed for eg the debugger to use.
 class BaseLocalIter {
  private:
   using ConstValTypeRange = mozilla::Range<const ValType>;
