d5c0351afd13083950efafa22c969346a346889c
Cristian Tuns <ctuns@mozilla.com>
2021-09-01 10:13:16 -0400

    Backed out changeset 5f7e6d65a0a6 (Bug 1727073) for causing build bustages. CLOSED TREE

--- widget/gtk/ScreenHelperGTK.h
+++ widget/gtk/ScreenHelperGTK.h
@@ -81,7 +81,7 @@ class ScreenGetterWayland : public ScreenGetter {
 
   MonitorConfig* AddMonitorConfig(int aId);
   bool RemoveMonitorConfig(int aId);
-  already_AddRefed<Screen> MakeScreenWayland(gint aMonitor);
+  already_AddRefed<Screen> MakeScreenWayland(gint aMonitorNum);
 
   RefPtr<nsIScreen> GetScreenForWindow(nsWindow* aWindow);
   void GetScreenRectForWindow(nsWindow* aWindow, GdkRectangle* aRect);
@@ -91,7 +91,6 @@ class ScreenGetterWayland : public ScreenGetter {
 
  private:
   int GetMonitorForWindow(nsWindow* aWindow);
-  bool MonitorUsesNonIntegerScale(int aMonitor);
 
  private:
   void* mRegistry;
