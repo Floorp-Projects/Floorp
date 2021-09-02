bea0752f14c4c5426a5ed121a138911281994d71
aardgoose <angus.sawyer@gmail.com>
2021-09-02 00:29:28 +0000

    Bug 1367251 - ignore gl.UNPACK_FLIP_Y_WEBGL & UNPACK_PREMULTIPLY_ALPHA for textures uploading ImageBitmap data as per spec. r=jgilbert
    ***
    
    Differential Revision: https://phabricator.services.mozilla.com/D29469

--- dom/canvas/WebGLTextureUpload.cpp
+++ dom/canvas/WebGLTextureUpload.cpp
@@ -71,7 +71,9 @@ Maybe<TexUnpackBlobDesc> FromImageBitmap(const GLenum target, uvec3 size,
                                 imageSize,
                                 nullptr,
                                 {},
-                                surf});
+                                surf,
+                                {},
+                                false});
 }
 
 TexUnpackBlobDesc FromImageData(const GLenum target, uvec3 size,
@@ -918,10 +920,18 @@ void WebGLTexture::TexImage(uint32_t level, GLenum respecFormat,
   if (src.cpuData) {
     cpuDataView = Some(RawBuffer<>{src.cpuData->Data()});
   }
-  const auto srcViewDesc = webgl::TexUnpackBlobDesc{
-      src.imageTarget, src.size,      src.srcAlphaType, std::move(cpuDataView),
-      src.pboOffset,   src.imageSize, src.image,        src.sd,
-      src.dataSurf,    src.unpacking};
+  const auto srcViewDesc = webgl::TexUnpackBlobDesc{src.imageTarget,
+                                                    src.size,
+                                                    src.srcAlphaType,
+                                                    std::move(cpuDataView),
+                                                    src.pboOffset,
+                                                    src.imageSize,
+                                                    src.image,
+                                                    src.sd,
+                                                    src.dataSurf,
+                                                    src.unpacking,
+                                                    src.applyUnpackTransforms};
+
   const auto blob = webgl::TexUnpackBlob::Create(srcViewDesc);
   if (!blob) {
     MOZ_ASSERT(false);
