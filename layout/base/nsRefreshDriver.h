36beef3f4d969ac4082f538cedcff624ca3d29c2
Paul Bone <pbone@mozilla.com>
2021-09-02 01:17:27 +0000

    Bug 1727960 - Properly detect if nsRefreshDriver is ticking in IdleTaskRunner r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D123887

--- layout/base/nsRefreshDriver.h
+++ layout/base/nsRefreshDriver.h
@@ -368,6 +368,8 @@ class nsRefreshDriver final : public mozilla::layers::TransactionIdAllocator,
    */
   static mozilla::Maybe<mozilla::TimeStamp> GetNextTickHint();
 
+  static bool IsRegularRateTimerTicking();
+
   static void DispatchIdleTaskAfterTickUnlessExists(mozilla::Task* aTask);
   static void CancelIdleTask(mozilla::Task* aTask);
 
