b9794ea8af3f785493c13528f42e4c98a7ea27cd
Andreas Pehrson <apehrson@mozilla.com>
2021-09-02 12:19:23 +0200

    Bug 1654112 - Re-enable a now viable threading assert in webrtc::AudioReceiveStream. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D124374

--- third_party/libwebrtc/audio/audio_receive_stream.cc
+++ third_party/libwebrtc/audio/audio_receive_stream.cc
@@ -177,8 +177,7 @@ void AudioReceiveStream::Stop() {
 
 webrtc::AudioReceiveStream::Stats AudioReceiveStream::GetStats(
     bool get_and_clear_legacy_stats) const {
-  // TODO: Mozilla - currently we run stats on the STS thread
-  //RTC_DCHECK_RUN_ON(&worker_thread_checker_);
+  RTC_DCHECK_RUN_ON(&worker_thread_checker_);
   webrtc::AudioReceiveStream::Stats stats;
   stats.remote_ssrc = config_.rtp.remote_ssrc;
 
