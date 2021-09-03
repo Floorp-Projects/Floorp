3cbae7f729e78be496a7d05eea82ebf97fd50edc
Andrei Oprea <andrei.br92@gmail.com>
2021-09-03 13:41:21 +0000

    Bug 1728420 - New messaging surface Spotlight r=Mardak,dmose,nanj
    Differential Revision: https://phabricator.services.mozilla.com/D121913

--- browser/components/newtab/test/unit/asrouter/PanelTestProvider.test.js
+++ browser/components/newtab/test/unit/asrouter/PanelTestProvider.test.js
@@ -1,6 +1,7 @@
 import { PanelTestProvider } from "lib/PanelTestProvider.jsm";
 import update_schema from "content-src/asrouter/templates/OnboardingMessage/UpdateAction.schema.json";
 import whats_new_schema from "content-src/asrouter/templates/OnboardingMessage/WhatsNewMessage.schema.json";
+import spotlight_schema from "content-src/asrouter/templates/OnboardingMessage/Spotlight.schema.json";
 
 describe("PanelTestProvider", () => {
   let messages;
@@ -10,7 +11,7 @@ describe("PanelTestProvider", () => {
   it("should have a message", () => {
     // Careful: when changing this number make sure that new messages also go
     // through schema verifications.
-    assert.lengthOf(messages, 9);
+    assert.lengthOf(messages, 10);
   });
   it("should be a valid message", () => {
     const updateMessages = messages.filter(
@@ -28,6 +29,14 @@ describe("PanelTestProvider", () => {
       assert.jsonSchema(message.content, whats_new_schema);
       // Not part of `message.content` so it can't be enforced through schema
       assert.property(message, "order");
+    }
+  });
+  it("should be a valid message", () => {
+    const spotlightMessages = messages.filter(
+      ({ template }) => template === "spotlight"
+    );
+    for (let message of spotlightMessages) {
+      assert.jsonSchema(message.content, spotlight_schema);
     }
   });
 });
