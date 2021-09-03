9791b9d135b3c1dcfa0a3c5e461105baa0356a53
Glenn Watson <git@intuitionlibrary.com>
2021-09-03 02:09:57 +0000

    Bug 1728774 - Fix display item cache with retained DL builder. r=miko,gfx-reviewers
    Fix a bug where the display item cache was not being reused from
    frame to frame with retained display list builders.
    
    At the same time, make the capacity recycling on the display list
    serialization arrays a bit more realistic.
    
    Differential Revision: https://phabricator.services.mozilla.com/D124412

--- gfx/wr/webrender_api/src/display_list.rs
+++ gfx/wr/webrender_api/src/display_list.rs
@@ -2143,9 +2143,9 @@ impl DisplayListBuilder {
         // So don't let the spike of the first allocation make us allocate a large
         // contiguous buffer (with some likelihood of OOM, see bug 1531819).
         let next_capacity = DisplayListCapacity {
-            cache_size: self.payload.cache_data.len().min(1024),
-            items_size: self.payload.items_data.len().min(1024),
-            spatial_tree_size: self.payload.spatial_tree.len().min(1024),
+            cache_size: self.payload.cache_data.len().min(128 * 1024),
+            items_size: self.payload.items_data.len().min(512 * 1024),
+            spatial_tree_size: self.payload.spatial_tree.len().min(128 * 1024),
         };
         let payload = mem::replace(
             &mut self.payload,
