b12746eef7ec127ea3ecdc1c2288442153a76fcf
Jon Coppeard <jcoppeard@mozilla.com>
2021-09-06 12:12:07 +0000

    Bug 1727745 - Move ArgumentsObject templates from Realm to GlobalObjectData r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D123754

--- js/src/vm/GlobalObject.cpp
+++ js/src/vm/GlobalObject.cpp
@@ -1209,6 +1209,10 @@ void GlobalObjectData::trace(JSTracer* trc) {
   if (regExpStatics) {
     regExpStatics->trace(trc);
   }
+
+  TraceNullableEdge(trc, &mappedArgumentsTemplate, "mapped-arguments-template");
+  TraceNullableEdge(trc, &unmappedArgumentsTemplate,
+                    "unmapped-arguments-template");
 }
 
 void GlobalObjectData::addSizeOfIncludingThis(
