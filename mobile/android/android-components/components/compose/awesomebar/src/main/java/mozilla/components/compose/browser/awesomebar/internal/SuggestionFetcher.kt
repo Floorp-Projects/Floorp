5fc80752bda2ecd4edd8298a1ef4857859a087c0
Sebastian Kaspari <s.kaspari@gmail.com>
2021-09-06 11:43:41 +0200

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/10922: Port Facts from browser-awesomebar to compose-awesomebar.
    Fenix processes those facts and uses them for telemetry.

--- mobile/android/android-components/components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/internal/SuggestionFetcher.kt
+++ mobile/android/android-components/components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/internal/SuggestionFetcher.kt
@@ -4,12 +4,16 @@
 
 package mozilla.components.compose.browser.awesomebar.internal
 
+import android.os.SystemClock
 import androidx.compose.runtime.RememberObserver
 import androidx.compose.runtime.mutableStateOf
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import mozilla.components.compose.browser.awesomebar.AwesomeBarFacts
+import mozilla.components.compose.browser.awesomebar.AwesomeBarFacts.emitAwesomeBarFact
 import mozilla.components.concept.awesomebar.AwesomeBar
+import mozilla.components.support.base.facts.Action
 import mozilla.components.support.base.utils.NamedThreadFactory
 import java.util.concurrent.Executors
 
@@ -50,7 +54,25 @@ internal class SuggestionFetcher(
         provider: AwesomeBar.SuggestionProvider,
         text: String
     ) {
+        // At this point, we have a timing value for a provider.
+        // We have a choice here - we can try grouping different timings together for a
+        // single user input value, or we can just treat them as entirely independent values.
+        // These timings are correlated with each other in a sense that they act on the same
+        // inputs, and are executed at the same time. However, our goal here is to track performance
+        // of the providers. Each provider acts independently from another; recording their performance
+        // at an individual level will allow us to track that performance over time.
+        // Tracked value will be reflected both in perceived user experience (how quickly results from
+        // a provider show up), and in a purely technical interpretation of how quickly providers
+        // fulfill requests.
+        // Grouping also poses timing challenges - as user is typing, we're trying to cancel these
+        // provider requests. Given that each request can take an arbitrary amount of time to execute,
+        // grouping correctly becomes tricky and we run a risk of omitting certain values - or, of
+        // adding a bunch of complexity just for the sake of "correct grouping".
+        val start = SystemClock.elapsedRealtimeNanos()
         val suggestions = provider.onInputChanged(text)
+        val end = SystemClock.elapsedRealtimeNanos()
+        emitProviderQueryTimingFact(provider, timingNs = end - start)
+
         processResultFrom(provider, suggestions)
     }
 
@@ -81,4 +103,16 @@ internal class SuggestionFetcher(
     }
 
     override fun onRemembered() = Unit
+}
+
+@Suppress("MagicNumber")
+internal fun emitProviderQueryTimingFact(provider: AwesomeBar.SuggestionProvider, timingNs: Long) {
+    emitAwesomeBarFact(
+        Action.INTERACTION,
+        AwesomeBarFacts.Items.PROVIDER_DURATION,
+        metadata = mapOf(
+            // We only care about millisecond precision here, so convert from ns to ms before emitting.
+            AwesomeBarFacts.MetadataKeys.DURATION_PAIR to (provider to (timingNs / 1_000_000L))
+        )
+    )
 }
