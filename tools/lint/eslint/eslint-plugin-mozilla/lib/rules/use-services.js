01a583ac70f9ff20368d90c4b2b2285f96780d3b
Mark Banner <standard8@mozilla.com>
2021-09-06 09:53:51 +0000

    Bug 1602940 - Add nsIUUIDGenerator to the Javascript Services object. r=Gijs
    This also adds a commented out option for automatically fixing use-services in the ESLint rule. This cannot be enabled at the moment as it needs special treatment for chrome tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D124391

--- tools/lint/eslint/eslint-plugin-mozilla/lib/rules/use-services.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/rules/use-services.js
@@ -14,32 +14,46 @@ let servicesInterfaceMap = helpers.servicesData;
 // -----------------------------------------------------------------------------
 // Rule Definition
 // -----------------------------------------------------------------------------
-module.exports = function(context) {
-  // ---------------------------------------------------------------------------
-  // Public
-  //  --------------------------------------------------------------------------
-  return {
-    CallExpression(node) {
-      if (
-        !node.callee ||
-        !node.callee.property ||
-        node.callee.property.type != "Identifier" ||
-        node.callee.property.name != "getService" ||
-        node.arguments.length != 1 ||
-        !node.arguments[0].property ||
-        node.arguments[0].property.type != "Identifier" ||
-        !node.arguments[0].property.name ||
-        !(node.arguments[0].property.name in servicesInterfaceMap)
-      ) {
-        return;
-      }
+module.exports = {
+  meta: {
+    // fixable: "code",
+  },
+  create(context) {
+    return {
+      CallExpression(node) {
+        if (
+          !node.callee ||
+          !node.callee.property ||
+          node.callee.property.type != "Identifier" ||
+          node.callee.property.name != "getService" ||
+          node.arguments.length != 1 ||
+          !node.arguments[0].property ||
+          node.arguments[0].property.type != "Identifier" ||
+          !node.arguments[0].property.name ||
+          !(node.arguments[0].property.name in servicesInterfaceMap)
+        ) {
+          return;
+        }
 
-      let serviceName = servicesInterfaceMap[node.arguments[0].property.name];
-
-      context.report(
-        node,
-        `Use Services.${serviceName} rather than getService().`
-      );
-    },
-  };
+        let serviceName = servicesInterfaceMap[node.arguments[0].property.name];
+        context.report({
+          node,
+          message: `Use Services.${serviceName} rather than getService().`,
+          // This is not enabled by default as for mochitest plain tests we
+          // would need to replace with `SpecialPowers.Services.${serviceName}`.
+          // At the moment we do not have an easy way to detect that.
+          // fix(fixer) {
+          //   let sourceCode = context.getSourceCode();
+          //   return fixer.replaceTextRange(
+          //     [
+          //       sourceCode.getFirstToken(node.callee).range[0],
+          //       sourceCode.getLastToken(node).range[1],
+          //     ],
+          //     `Services.${serviceName}`
+          //   );
+          // },
+        });
+      },
+    };
+  },
 };
