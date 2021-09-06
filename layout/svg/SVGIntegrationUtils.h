e259e004de563b15a859b543625844ef27ee3df9
Sandor Molnar <smolnar@mozilla.com>
2021-09-06 19:31:31 +0300

    Backed out 3 changesets (bug 1729236) for causing reftest failures in layout/reftests/svg/filters/filter-giant.
    Backed out changeset 1333d00d417c (bug 1729236)
    Backed out changeset a3021a3499e1 (bug 1729236)
    Backed out changeset 49a5aa4c8eb7 (bug 1729236)

--- layout/svg/SVGIntegrationUtils.h
+++ layout/svg/SVGIntegrationUtils.h
@@ -151,8 +151,8 @@ class SVGIntegrationUtils final {
   struct MOZ_STACK_CLASS PaintFramesParams {
     gfxContext& ctx;
     nsIFrame* frame;
-    nsRect dirtyRect;
-    nsRect borderArea;
+    const nsRect& dirtyRect;
+    const nsRect& borderArea;
     nsDisplayListBuilder* builder;
     layers::LayerManager* layerManager;
     bool handleOpacity;  // If true, PaintMaskAndClipPath/ PaintFilter should
