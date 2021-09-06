2112b178baacba3d632b5aec0b31e437390c0d76
Ms2ger <Ms2ger@gmail.com>
2021-09-06 09:32:59 +0000

    Bug 1729029 - Ensure test fails when feature is off; r=lth a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D124601

--- testing/web-platform/tests/wasm/jsapi/function/constructor.tentative.any.js
+++ testing/web-platform/tests/wasm/jsapi/function/constructor.tentative.any.js
@@ -6,30 +6,37 @@ function addxy(x, y) {
 }
 
 test(() => {
+    assert_implements(WebAssembly.Function, "WebAssembly.Function is not implemented");
     var fun = new WebAssembly.Function({parameters: ["i32", "i32"], results: ["i32"]}, addxy);
     assert_true(fun instanceof WebAssembly.Function)
 }, "construct with JS function")
 
 test(() => {
+    assert_implements(WebAssembly.Function, "WebAssembly.Function is not implemented");
     assert_throws_js(TypeError, () => new WebAssembly.Function({parameters: []}, addxy))
 }, "fail with missing results")
 
 test(() => {
+    assert_implements(WebAssembly.Function, "WebAssembly.Function is not implemented");
     assert_throws_js(TypeError, () => new WebAssembly.Function({results: []}, addxy))
 }, "fail with missing parameters")
 
 test(() => {
+    assert_implements(WebAssembly.Function, "WebAssembly.Function is not implemented");
     assert_throws_js(TypeError, () => new WebAssembly.Function({parameters: [1], results: [true]}, addxy))
 }, "fail with non-string parameters & results")
 
 test(() => {
+    assert_implements(WebAssembly.Function, "WebAssembly.Function is not implemented");
     assert_throws_js(TypeError, () => new WebAssembly.Function({parameters: ["invalid"], results: ["invalid"]}, addxy))
 }, "fail with non-existent parameter and result type")
 
 test(() => {
+    assert_implements(WebAssembly.Function, "WebAssembly.Function is not implemented");
     assert_throws_js(TypeError, () => new WebAssembly.Function({parameters: [], results: []}, 72))
 }, "fail with non-function object")
 
-test(()  => {
+test(() => {
+    assert_implements(WebAssembly.Function, "WebAssembly.Function is not implemented");
     assert_throws_js(TypeError, () => new WebAssembly.Function({parameters: [], results: []}, {}))
 }, "fail to construct with non-callable object")
