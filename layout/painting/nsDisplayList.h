863831bae6114b75b875b4961cae1059bbeb0d8b
Matt Woodrow <mwoodrow@mozilla.com>
2021-09-06 23:12:04 +0000

    Bug 1728258 - Compute AnimatedGeometryRoot frames in RetainedDisplayListBuilder without needing the existing object stored on nsDisplayItem. r=miko,mstange
    Differential Revision: https://phabricator.services.mozilla.com/D124153

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -1501,7 +1501,7 @@ class nsDisplayListBuilder {
                                       OutOfFlowDisplayData)
 
   struct DisplayListBuildingData {
-    RefPtr<AnimatedGeometryRoot> mModifiedAGR = nullptr;
+    nsIFrame* mModifiedAGR = nullptr;
     nsRect mDirtyRect;
   };
   NS_DECLARE_FRAME_PROPERTY_DELETABLE(DisplayListBuildingRect,
@@ -2363,6 +2363,12 @@ class nsDisplayItem : public nsDisplayItemLink {
   }
 
   virtual bool CreatesStackingContextHelper() { return false; }
+
+  /**
+   * Returns true if this item can be moved asynchronously on the compositor,
+   * see RetainedDisplayListBuilder.cpp comments.
+   */
+  virtual bool CanMoveAsync() { return false; }
 
  protected:
   // This is never instantiated directly (it has pure virtual methods), so no
@@ -5615,6 +5621,8 @@ class nsDisplayStickyPosition : public nsDisplayOwnLayer {
 
   bool CreatesStackingContextHelper() override { return true; }
 
+  bool CanMoveAsync() override { return true; }
+
  private:
   NS_DISPLAY_ALLOW_CLONING()
 
@@ -6442,6 +6450,11 @@ class nsDisplayTransform : public nsPaintedDisplayItem {
                      bool aEnforceMinimumSize = true) const;
 
   void WriteDebugInfo(std::stringstream& aStream) override;
+
+  bool CanMoveAsync() override {
+    return EffectCompositor::HasAnimationsForCompositor(
+        mFrame, DisplayItemType::TYPE_TRANSFORM);
+  }
 
   /**
    * This item is an additional item as the boundary between parent
