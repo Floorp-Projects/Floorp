7e0f7ff7667c1bfe37e43da0a99e19785cd66d53
Matt Woodrow <mwoodrow@mozilla.com>
2021-09-06 23:12:02 +0000

    Bug 1728050 - Move opacity flattening to be part of WR DL serialization. r=miko
    Differential Revision: https://phabricator.services.mozilla.com/D124005

--- gfx/layers/wr/DisplayItemCache.h
+++ gfx/layers/wr/DisplayItemCache.h
@@ -100,9 +100,9 @@ class DisplayItemCache final {
   /**
    * Suppress display item caching. This doesn't clear any existing cached
    * items or change the underlying capacity, it just makes IsEnabled() return
-   * false. It is not meant to be flipped in the middle of a display list build,
-   * but rather set before the display list build starts to suppress use of the
-   * cache for that display list build.
+   * false.
+   * It will also make CanReuseItem return false for the duration of the
+   * suppression.
    */
   bool SetSuppressed(bool aSuppressed) {
     if (aSuppressed == mSuppressed) {
--- gfx/layers/wr/WebRenderCommandBuilder.cpp
+++ gfx/layers/wr/WebRenderCommandBuilder.cpp
@@ -2484,6 +2484,8 @@ Maybe<wr::ImageMask> WebRenderCommandBuilder::BuildWrMaskImage(
 
   nsPoint maskOffset = aMaskItem->ToReferenceFrame() - bounds.TopLeft();
 
+  bool shouldHandleOpacity = aBuilder.GetInheritedOpacity() != 1.0f;
+
   nsRect dirtyRect;
   // If this mask item is being painted for the first time, some members of
   // WebRenderMaskData are still default initialized. This is intentional.
@@ -2491,7 +2493,7 @@ Maybe<wr::ImageMask> WebRenderCommandBuilder::BuildWrMaskImage(
       !itemRect.IsEqualInterior(maskData->mItemRect) ||
       !(aMaskItem->Frame()->StyleSVGReset()->mMask == maskData->mMaskStyle) ||
       maskOffset != maskData->mMaskOffset || !sameScale ||
-      aMaskItem->ShouldHandleOpacity() != maskData->mShouldHandleOpacity) {
+      shouldHandleOpacity != maskData->mShouldHandleOpacity) {
     IntSize size = itemRect.Size().ToUnknownSize();
 
     if (!Factory::AllowedSurfaceSize(size)) {
@@ -2535,8 +2537,8 @@ Maybe<wr::ImageMask> WebRenderCommandBuilder::BuildWrMaskImage(
                            .PreScale(scale.width, scale.height));
 
     bool maskPainted = false;
-    bool maskIsComplete =
-        aMaskItem->PaintMask(aDisplayListBuilder, context, &maskPainted);
+    bool maskIsComplete = aMaskItem->PaintMask(
+        aDisplayListBuilder, context, shouldHandleOpacity, &maskPainted);
     if (!maskPainted) {
       return Nothing();
     }
@@ -2587,7 +2589,7 @@ Maybe<wr::ImageMask> WebRenderCommandBuilder::BuildWrMaskImage(
       maskData->mMaskOffset = maskOffset;
       maskData->mScale = scale;
       maskData->mMaskStyle = aMaskItem->Frame()->StyleSVGReset()->mMask;
-      maskData->mShouldHandleOpacity = aMaskItem->ShouldHandleOpacity();
+      maskData->mShouldHandleOpacity = shouldHandleOpacity;
     }
   }
 
