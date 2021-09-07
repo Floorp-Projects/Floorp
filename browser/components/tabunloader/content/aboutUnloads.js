c64a6c0b23e96ee199da9ebe857c85e7be130306
Toshihito Kikuchi <tkikuchi@mozilla.com>
2021-09-07 00:46:59 +0000

    Bug 1725257 - Part 3. Implement about:unloads.  r=NeilDeakin,fluent-reviewers,flod
    The page contains
    - a table to show the tabs and their attributes that `TabUnloader` considers
    - a button to trigger `TabUnloader.unloadLeastRecentlyUsedTab()`
    
    to visualize the behavior of `TabUnloader` and manually trigger it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D123988

--- browser/components/tabunloader/content/aboutUnloads.js
+++ browser/components/tabunloader/content/aboutUnloads.js
@@ -7,7 +7,118 @@ const { TabUnloader } = ChromeUtils.import(
   "resource:///modules/TabUnloader.jsm"
 );
 
-function onLoad() {}
+async function refreshData() {
+  const sortedTabs = await TabUnloader.getSortedTabs();
+  const tabTable = document.querySelector(".tab-table > tbody");
+  const getHost = uri => {
+    try {
+      return uri?.host;
+    } catch (e) {
+      return uri?.spec;
+    }
+  };
+  const updateTimestamp = () => {
+    document.l10n.setAttributes(
+      document.getElementById("label-last-updated"),
+      "about-unloads-last-updated",
+      { date: Date.now() }
+    );
+  };
+
+  // Reset the table
+  // Don't delete the first row showing the "no unloadable tab" message
+  while (tabTable.rows.length > 1) {
+    tabTable.deleteRow(1);
+  }
+  if (!sortedTabs.length) {
+    document.getElementById("button-unload").disabled = true;
+    document.getElementById("no-unloadable-tab-message").hidden = false;
+    updateTimestamp();
+    return;
+  }
+  document.getElementById(
+    "button-unload"
+  ).disabled = !TabUnloader.isDiscardable(sortedTabs[0]);
+  document.getElementById("no-unloadable-tab-message").hidden = true;
+
+  const fragmentRows = new DocumentFragment();
+  const templateRow = document.querySelector("template[name=tab-table-row]");
+
+  let ordinal = 0;
+  for (const tabInfo of sortedTabs) {
+    if (!("tab" in tabInfo)) {
+      continue;
+    }
+
+    const fragment = templateRow.content.cloneNode(true);
+    const row = fragment.querySelector("tr");
+
+    row.children[0].textContent = TabUnloader.isDiscardable(tabInfo)
+      ? ++ordinal
+      : "-";
+    row.children[1].textContent = getHost(
+      tabInfo.tab?.linkedBrowser?.currentURI
+    );
+    if ("lastAccessed" in tabInfo.tab) {
+      document.l10n.setAttributes(
+        row.children[2],
+        "about-unloads-last-accessed",
+        { date: tabInfo.tab.lastAccessed }
+      );
+    }
+    row.children[3].textContent = tabInfo.weight;
+    row.children[4].textContent = tabInfo.sortWeight;
+    if ("memory" in tabInfo) {
+      document.l10n.setAttributes(
+        row.children[5],
+        "about-unloads-memory-in-mb",
+        { mem: tabInfo.memory / 1024 / 1024 }
+      );
+    }
+
+    if (tabInfo.processes) {
+      for (const [pid, procEntry] of tabInfo.processes) {
+        if (pid < 0) {
+          // Tab is hosted by the main process
+          continue;
+        }
+
+        const procLabel = document.createElement("span");
+        const procInfo = procEntry.entryToProcessMap;
+
+        if (procEntry.isTopLevel) {
+          procLabel.classList.add("top-level-process");
+        }
+        if (procInfo.tabSet.size > 1) {
+          procLabel.classList.add("shared-process");
+        }
+        procLabel.textContent = pid;
+        document.l10n.setAttributes(
+          procLabel,
+          "about-unloads-memory-in-mb-tooltip",
+          { mem: procInfo.memory / 1024 / 1024 }
+        );
+        row.children[6].appendChild(procLabel);
+        row.children[6].appendChild(document.createTextNode(" "));
+      }
+    }
+
+    fragmentRows.appendChild(fragment);
+  }
+
+  tabTable.appendChild(fragmentRows);
+  updateTimestamp();
+}
+
+async function onLoad() {
+  document
+    .getElementById("button-unload")
+    .addEventListener("click", async () => {
+      await TabUnloader.unloadLeastRecentlyUsedTab();
+      await refreshData();
+    });
+  await refreshData();
+}
 
 try {
   document.addEventListener("DOMContentLoaded", onLoad, { once: true });
--- browser/locales/en-US/browser/aboutUnloads.ftl
+++ browser/locales/en-US/browser/aboutUnloads.ftl
@@ -6,3 +6,46 @@
 ### feature.
 
 about-unloads-page-title = Tab Unloading
+about-unloads-intro-1 =
+    { -brand-short-name } has a feature that automatically unloads tabs
+    to prevent the application from crashing due to insufficient memory
+    when the system’s available memory is low. The next tab to be unloaded is
+    chosen based on multiple attributes. This page shows how
+    { -brand-short-name } prioritizes tabs and which tab will be unloaded
+    when tab unloading is triggered.
+about-unloads-intro-2 =
+    Existing tabs are displayed in the table below in the same order used by
+    { -brand-short-name } to choose the next tab to unload. Process IDs are
+    displayed in <strong>bold</strong> when they are hosting the tab’s top
+    frame, and in <em>italic</em> when the process is shared between different
+    tabs. You can trigger tab unloading manually by clicking the <em>Unload</em>
+    button below.
+
+about-unloads-last-updated =
+    Last updated: { DATETIME($date,
+        year: "numeric", month: "numeric", day: "numeric",
+        hour: "numeric", minute: "numeric", second: "numeric",
+        hour12: "false") }
+about-unloads-button-unload = Unload
+  .title = Unload tab with the highest priority
+about-unloads-no-unloadable-tab = There are no unloadable tabs.
+
+about-unloads-column-priority = Priority
+about-unloads-column-host = Host
+about-unloads-column-last-accessed = Last Accessed
+about-unloads-column-weight = Base Weight
+  .title = Tabs are first sorted by this value, which derives from some special attributes such as playing a sound, WebRTC, etc.
+about-unloads-column-sortweight = Secondary Weight
+  .title = If available, tabs are sorted by this value after being sorted by the base weight. The value derives from tab’s memory usage and the count of processes.
+about-unloads-column-memory = Memory
+  .title = Tab’s estimated memory usage
+about-unloads-column-processes = Process IDs
+  .title = IDs of the processes hosting tab’s content
+
+about-unloads-last-accessed = { DATETIME($date,
+        year: "numeric", month: "numeric", day: "numeric",
+        hour: "numeric", minute: "numeric", second: "numeric",
+        hour12: "false") }
+about-unloads-memory-in-mb = { NUMBER($mem, maxFractionalUnits: 2) } MB
+about-unloads-memory-in-mb-tooltip =
+  .title = { NUMBER($mem, maxFractionalUnits: 2) } MB
