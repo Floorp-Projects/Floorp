b939b22e8ce6563cd6a2d9f6df0ad4e61ad59368
Nan Jiang <njiang028@gmail.com>
2021-09-08 19:29:41 +0000

    Bug 1729576 - Add various telemetry changes for Firefox Suggest r=adw
    * Add 'scenario' to impression and click pings
    * Only send 'search_query' and 'matched_keywords' for 'online' scenario
    
    Differential Revision: https://phabricator.services.mozilla.com/D124943

--- browser/components/urlbar/tests/browser/browser_urlbar_telemetry_quicksuggest.js
+++ browser/components/urlbar/tests/browser/browser_urlbar_telemetry_quicksuggest.js
@@ -46,6 +46,8 @@ const TELEMETRY_EVENT_CATEGORY = "contextservices.quicksuggest";
 const EXPERIMENT_PREF = "browser.urlbar.quicksuggest.enabled";
 const SUGGEST_PREF = "suggest.quicksuggest";
 
+const DEFAULT_SCENARIO = UrlbarPrefs.get("quickSuggestScenario");
+
 // Spy for the custom impression/click sender
 let spy;
 
@@ -108,6 +110,35 @@ add_task(async function impression() {
     assertCustomImpression(index);
   });
   await PlacesUtils.history.clear();
+});
+
+// Tests the impression scalar and the custom impression ping for "online" scenario.
+add_task(async function impression_online() {
+  await UrlbarTestUtils.withExperiment({
+    valueOverrides: {
+      quickSuggestScenario: "online",
+      quickSuggestEnabled: true,
+      quickSuggestShouldShowOnboardingDialog: false,
+    },
+    callback: async () => {
+      spy.resetHistory();
+      await BrowserTestUtils.withNewTab("about:blank", async () => {
+        await UrlbarTestUtils.promiseAutocompleteResultPopup({
+          window,
+          value: TEST_SEARCH_STRING,
+          fireInputEvent: true,
+        });
+        let index = 1;
+        await assertIsQuickSuggest(index);
+        await UrlbarTestUtils.promisePopupClose(window, () => {
+          EventUtils.synthesizeKey("KEY_Enter");
+        });
+        assertScalars({ [TELEMETRY_SCALARS.IMPRESSION]: index + 1 });
+        assertCustomImpression(index, "online");
+      });
+      await PlacesUtils.history.clear();
+    },
+  });
 });
 
 // Makes sure the impression scalar and the custom impression are not incremented
@@ -516,11 +547,17 @@ async function assertNoQuickSuggestResults() {
  *
  * @param {number} [index]
  *   The expected index of the Quick Suggest result.
+ * @param {string} [scenario]
+ *   The scenario of the Quick Suggest, should be one of "offline", "history", "online".
  */
-function assertCustomImpression(index) {
+function assertCustomImpression(index, scenario = DEFAULT_SCENARIO) {
   Assert.ok(spy.calledOnce, "Should send a custom impression ping");
   // Validate the impression ping
   let [payload, endpoint] = spy.firstCall.args;
+  let expectedSearchQuery = scenario === "online" ? TEST_SEARCH_STRING : "";
+  let expectedMatchedKeywords = scenario === "online" ? TEST_SEARCH_STRING : "";
+  let expectedScenario = scenario;
+
   Assert.ok(
     endpoint.includes(CONTEXTUAL_SERVICES_PING_TYPES.QS_IMPRESSION),
     "Should set the endpoint for QuickSuggest impression"
@@ -540,14 +577,15 @@ function assertCustomImpression(index) {
   Assert.equal(payload.position, index + 1, "Should set the position");
   Assert.equal(
     payload.search_query,
-    TEST_SEARCH_STRING,
-    "Should set the search_query to an empty string"
+    expectedSearchQuery,
+    "Should set the search_query"
   );
   Assert.equal(
     payload.matched_keywords,
-    TEST_SEARCH_STRING,
-    "Should set the matched_keywords to an empty string"
+    expectedMatchedKeywords,
+    "Should set the matched_keywords"
   );
+  Assert.equal(payload.scenario, expectedScenario, "Should set the scenario");
 }
 
 /**
@@ -585,6 +623,7 @@ function assertCustomClick(index) {
   );
   Assert.equal(payload.block_id, 1, "Should set the block_id");
   Assert.equal(payload.position, index + 1, "Should set the position");
+  Assert.equal(payload.scenario, DEFAULT_SCENARIO, "Should set the scenario");
 }
 
 /**
