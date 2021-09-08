235783d9c380132a6dfc9a6b5aa1b93050b69331
Cosmin Sabou <csabou@mozilla.com>
2021-09-08 07:43:48 +0300

    Backed out changeset 40f514ec207e (bug 1729406) for causing windows build bustages. CLOSED TREE

--- taskcluster/scripts/misc/exfiltrate-pdbstr.sh
+++ taskcluster/scripts/misc/exfiltrate-pdbstr.sh
@@ -6,4 +6,4 @@ set -x -e -v
 mkdir pdbstr
 cp "c:/Program Files (x86)/Windows Kits/10/Debuggers/$1/srcsrv/pdbstr.exe" pdbstr
 
-tar -c pdbstr | python3 $GECKO_PATH/taskcluster/scripts/misc/zstdpy > pdbstr.tar.zst
+tar -jcvf pdbstr.tar.bz2 pdbstr
