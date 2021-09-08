c122209bf0825ec5bc2f4e82240e481fef2be36a
Sebastian Hengst <archaeopteryx@coole-files.de>
2021-09-08 18:42:23 +0200

    Backed out changeset 5281f57107c7 (bug 1728423) for hangs in nsLayoutUtils::PaintFrame (bug 1729723). CLOSED TREE

--- toolkit/components/glean/metrics_index.py
+++ toolkit/components/glean/metrics_index.py
@@ -11,7 +11,6 @@ metrics_yamls = [
     "toolkit/components/glean/tests/test_metrics.yaml",
     "toolkit/mozapps/update/metrics.yaml",
     "browser/base/content/metrics.yaml",
-    "gfx/metrics.yaml",
 ]
 
 # The list of all Glean pings.yaml files, relative to the top src dir.
