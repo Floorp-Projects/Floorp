79340ff8b9c0aee6032ed36bc6073bf71c1ee93a
Julian Descottes <jdescottes@mozilla.com>
2021-09-08 08:20:29 +0000

    Bug 1729625 - [devtools] Remove beta label from Show Tabbing Order widget r=nchevobbe
    Depends on D124804
    
    Differential Revision: https://phabricator.services.mozilla.com/D124889

--- devtools/client/accessibility/components/DisplayTabbingOrder.js
+++ devtools/client/accessibility/components/DisplayTabbingOrder.js
@@ -21,7 +21,6 @@ const {
 class DisplayTabbingOrder extends PureComponent {
   static get propTypes() {
     return {
-      describedby: PropTypes.string,
       dispatch: PropTypes.func.isRequired,
       tabbingOrderDisplayed: PropTypes.bool.isRequired,
     };
@@ -46,7 +45,7 @@ class DisplayTabbingOrder extends PureComponent {
   }
 
   render() {
-    const { describedby, tabbingOrderDisplayed } = this.props;
+    const { tabbingOrderDisplayed } = this.props;
     return label(
       {
         className:
@@ -61,7 +60,6 @@ class DisplayTabbingOrder extends PureComponent {
         checked: tabbingOrderDisplayed,
         disabled: this.state.disabled,
         onChange: this.onChange,
-        "aria-describedby": describedby,
       }),
       L10N.getStr("accessibility.toolbar.displayTabbingOrder.label")
     );
