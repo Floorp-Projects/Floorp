c6a77011adc22dc20f4d66d999d23eb8da5ea56f
Csoregi Natalia <ncsoregi@mozilla.com>
2021-09-08 18:48:58 +0300

    Backed out changeset f42e32addd89 (bug 1534799) for causing failures on test_txtctrl.xhtml. CLOSED TREE

--- toolkit/content/widgets/moz-input-box.js
+++ toolkit/content/widgets/moz-input-box.js
@@ -8,6 +8,9 @@
 // a block to prevent accidentally leaking globals onto `window`.
 {
   const cachedFragments = {
+    get entities() {
+      return ["chrome://global/locale/textcontext.dtd"];
+    },
     get editMenuItems() {
       return `
       <menuitem data-l10n-id="text-action-undo" cmd="cmd_undo"></menuitem>
@@ -48,7 +51,8 @@
           <menupopup anonid="spell-dictionaries-menu" onpopupshowing="event.stopPropagation();" onpopuphiding="event.stopPropagation();"></menupopup>
         </menu>
       </menupopup>
-    `
+    `,
+        this.entities
       );
       return this.spellcheck;
     },
