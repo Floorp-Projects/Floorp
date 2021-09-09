297e1a351fc5bb73bcd7146095d076874cab0f26
Botond Ballo <botond@mozilla.com>
2021-09-09 05:15:53 +0000

    Bug 1593381 - Introduce a more generic mechanism for APZ test code to access private members of APZ types. r=hiro
    Differential Revision: https://phabricator.services.mozilla.com/D124290

---
+++ gfx/layers/apz/test/gtest/APZTestAccess.cpp
@@ -0,0 +1,20 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "APZTestAccess.h"
+#include "mozilla/layers/WebRenderScrollData.h"
+
+namespace mozilla {
+namespace layers {
+
+/*static*/
+void APZTestAccess::InitializeForTest(WebRenderLayerScrollData& aLayer,
+                                      int32_t aDescendantCount) {
+  aLayer.InitializeForTest(aDescendantCount);
+}
+
+}  // namespace layers
+}  // namespace mozilla
