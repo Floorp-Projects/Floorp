92447048b17d450685f4dc505f2f0ac967973023
Yury Delendik <ydelendik@mozilla.com>
2021-09-09 16:40:44 +0000

    Bug 1672343 - Ignore one of the SIMD shuffle operands if it is not used. r=lth
    Differential Revision: https://phabricator.services.mozilla.com/D124382

--- js/src/jit-test/tests/wasm/simd/shuffle-x86-ion-codegen.js
+++ js/src/jit-test/tests/wasm/simd/shuffle-x86-ion-codegen.js
@@ -66,14 +66,12 @@ codegenTestX64_v128xLITERAL_v128(
      ['i8x16.shuffle 16 16 16 0 1 2 3 4 5 6 7 8 9 10 11 12',
       '(v128.const i32x4 0 0 0 0)',
 `
-66 0f ef c9               pxor %xmm1, %xmm1
 66 0f 73 f8 03            pslldq \\$0x03, %xmm0`],
 
      // Shift right bytes, shifting in zeroes.  See above.
      ['i8x16.shuffle 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18',
       '(v128.const i32x4 0 0 0 0)',
 `
-66 0f ef c9               pxor %xmm1, %xmm1
 66 0f 73 d8 03            psrldq \\$0x03, %xmm0`]]);
 
 // SSE4.1 PBLENDVB instruction is using XMM0, checking if blend
