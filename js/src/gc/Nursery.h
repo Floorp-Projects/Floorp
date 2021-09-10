c23a79a449aad1cf99716cb13a597c54ea7b1c03
Jon Coppeard <jcoppeard@mozilla.com>
2021-09-10 07:35:01 +0000

    Bug 1601228 - Move tenuring implementation to a new source file r=sfink
    Splitting up GC.cpp will add more code to Marking.cpp so as a first step let's
    move all the tenuring code out of Marking.cpp into its own source file.
    
    This is all code motion, except I make MAX_DEDUPLICATABLE_STRING_LENGTH a
    constant.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125098

--- js/src/gc/Nursery.h
+++ js/src/gc/Nursery.h
@@ -64,6 +64,7 @@ class HeapSlot;
 class JSONPrinter;
 class MapObject;
 class SetObject;
+class TenuringTracer;
 
 namespace gc {
 class AutoGCSession;
@@ -107,75 +108,6 @@ class NurseryDecommitTask : public GCParallelTask {
 
   MainThreadOrGCTaskData<NurseryChunk*> partialChunk;
   MainThreadOrGCTaskData<size_t> partialCapacity;
-};
-
-class TenuringTracer final : public GenericTracer {
-  friend class Nursery;
-  Nursery& nursery_;
-
-  // Amount of data moved to the tenured generation during collection.
-  size_t tenuredSize;
-  // Number of cells moved to the tenured generation.
-  size_t tenuredCells;
-
-  // These lists are threaded through the Nursery using the space from
-  // already moved things. The lists are used to fix up the moved things and
-  // to find things held live by intra-Nursery pointers.
-  gc::RelocationOverlay* objHead;
-  gc::RelocationOverlay** objTail;
-  gc::StringRelocationOverlay* stringHead;
-  gc::StringRelocationOverlay** stringTail;
-
-  TenuringTracer(JSRuntime* rt, Nursery* nursery);
-
-  JSObject* onObjectEdge(JSObject* obj) override;
-  JSString* onStringEdge(JSString* str) override;
-  JS::Symbol* onSymbolEdge(JS::Symbol* sym) override;
-  JS::BigInt* onBigIntEdge(JS::BigInt* bi) override;
-  js::BaseScript* onScriptEdge(BaseScript* script) override;
-  js::Shape* onShapeEdge(Shape* shape) override;
-  js::RegExpShared* onRegExpSharedEdge(RegExpShared* shared) override;
-  js::BaseShape* onBaseShapeEdge(BaseShape* base) override;
-  js::GetterSetter* onGetterSetterEdge(GetterSetter* gs) override;
-  js::PropMap* onPropMapEdge(PropMap* map) override;
-  js::jit::JitCode* onJitCodeEdge(jit::JitCode* code) override;
-  js::Scope* onScopeEdge(Scope* scope) override;
-
- public:
-  Nursery& nursery() { return nursery_; }
-
-  void traverse(JS::Value* thingp);
-
-  // The store buffers need to be able to call these directly.
-  void traceObject(JSObject* src);
-  void traceObjectSlots(NativeObject* nobj, uint32_t start, uint32_t end);
-  void traceSlots(JS::Value* vp, uint32_t nslots);
-  void traceString(JSString* src);
-  void traceBigInt(JS::BigInt* src);
-
- private:
-  inline void insertIntoObjectFixupList(gc::RelocationOverlay* entry);
-  inline void insertIntoStringFixupList(gc::StringRelocationOverlay* entry);
-
-  template <typename T>
-  inline T* allocTenured(JS::Zone* zone, gc::AllocKind kind);
-  JSString* allocTenuredString(JSString* src, JS::Zone* zone,
-                               gc::AllocKind dstKind);
-
-  inline JSObject* movePlainObjectToTenured(PlainObject* src);
-  JSObject* moveToTenuredSlow(JSObject* src);
-  JSString* moveToTenured(JSString* src);
-  JS::BigInt* moveToTenured(JS::BigInt* src);
-
-  size_t moveElementsToTenured(NativeObject* dst, NativeObject* src,
-                               gc::AllocKind dstKind);
-  size_t moveSlotsToTenured(NativeObject* dst, NativeObject* src);
-  size_t moveStringToTenured(JSString* dst, JSString* src,
-                             gc::AllocKind dstKind);
-  size_t moveBigIntToTenured(JS::BigInt* dst, JS::BigInt* src,
-                             gc::AllocKind dstKind);
-
-  void traceSlots(JS::Value* vp, JS::Value* end);
 };
 
 // Classes with JSCLASS_SKIP_NURSERY_FINALIZE or Wrapper classes with
