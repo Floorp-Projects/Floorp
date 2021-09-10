85a8a8079d20243aaba9d7a1ccf40cfc10d0686c
Julien Wajsberg <felash@gmail.com>
2021-09-10 12:28:41 +0000

    Bug 1729428 - [devtools performance] In tests, load the real profiler UI in about:debugging instead of a blank page r=jdescottes
    Depends on D124972
    
    Differential Revision: https://phabricator.services.mozilla.com/D124973

--- devtools/client/aboutdebugging/test/browser/browser_aboutdebugging_profiler_dialog.js
+++ devtools/client/aboutdebugging/test/browser/browser_aboutdebugging_profiler_dialog.js
@@ -3,25 +3,33 @@
 
 "use strict";
 
+const RUNTIME_ID = "1337id";
+const DEVICE_NAME = "Fancy Phone";
+const RUNTIME_NAME = "Lorem ipsum";
+
 /**
  * Test opening and closing the profiler dialog.
  */
 add_task(async function() {
+  // This is a client to the current Firefox.
+  const clientWrapper = await createLocalClientWrapper();
+
   // enable USB devices mocks
   const mocks = new Mocks();
-  const usbClient = mocks.createUSBRuntime("1337id", {
-    deviceName: "Fancy Phone",
-    name: "Lorem ipsum",
+  mocks.createUSBRuntime(RUNTIME_ID, {
+    deviceName: DEVICE_NAME,
+    name: RUNTIME_NAME,
+    clientWrapper,
   });
 
   const { document, tab, window } = await openAboutDebugging();
 
   mocks.emitUSBUpdate();
-  await connectToRuntime("Fancy Phone", document);
-  await selectRuntime("Fancy Phone", "Lorem ipsum", document);
+  await connectToRuntime(DEVICE_NAME, document);
+  await selectRuntime(DEVICE_NAME, RUNTIME_NAME, document);
 
   info("Open the profiler dialog");
-  await openProfilerDialog(usbClient, document);
+  await openProfilerDialogWithRealClient(document);
   assertDialogVisible(document);
 
   info("Click on the close button and wait until the dialog disappears");
@@ -31,7 +39,7 @@ add_task(async function() {
   assertDialogHidden(document);
 
   info("Open the profiler dialog again");
-  await openProfilerDialog(usbClient, document);
+  await openProfilerDialogWithRealClient(document);
   assertDialogVisible(document);
 
   info("Click on the mask element and wait until the dialog disappears");
@@ -41,7 +49,7 @@ add_task(async function() {
   assertDialogHidden(document);
 
   info("Open the profiler dialog again");
-  await openProfilerDialog(usbClient, document);
+  await openProfilerDialogWithRealClient(document);
   assertDialogVisible(document);
 
   info("Navigate to this-firefox and wait until the dialog disappears");
@@ -50,9 +58,13 @@ add_task(async function() {
   assertDialogHidden(document);
 
   info("Select the remote runtime again, check the dialog is still hidden");
-  await selectRuntime("Fancy Phone", "Lorem ipsum", document);
+  await selectRuntime(DEVICE_NAME, RUNTIME_NAME, document);
   assertDialogHidden(document);
 
+  info("Remove USB runtime");
+  mocks.removeUSBRuntime(RUNTIME_ID);
+  mocks.emitUSBUpdate();
+  await waitUntilUsbDeviceIsUnplugged(DEVICE_NAME, document);
   await removeTab(tab);
 });
 
@@ -62,9 +74,28 @@ function assertDialogVisible(doc) {
 }
 
 function assertDialogHidden(doc) {
-  ok(!document.querySelector(".qa-profiler-dialog"), "Dialog is removed");
-  ok(
-    !document.querySelector(".qa-profiler-dialog-mask"),
-    "Dialog mask is removed"
+  ok(!doc.querySelector(".qa-profiler-dialog"), "Dialog is removed");
+  ok(!doc.querySelector(".qa-profiler-dialog-mask"), "Dialog mask is removed");
+}
+
+/**
+ * Open the performance profiler dialog with a real client.
+ */
+async function openProfilerDialogWithRealClient(doc) {
+  info("Click on the Profile Runtime button");
+  const profileButton = doc.querySelector(".qa-profile-runtime-button");
+  profileButton.click();
+
+  info("Wait for the rendering of the profiler UI");
+  const profilerIframe = await TestUtils.waitForCondition(
+    () => doc.querySelector(".profiler-dialog__frame"),
+    "Waiting for the profiler frame."
   );
+  await TestUtils.waitForCondition(
+    () =>
+      profilerIframe.contentDocument &&
+      profilerIframe.contentDocument.querySelector(".perf-presets"),
+    "Waiting for the rendering of the profiler UI."
+  );
+  return profilerIframe.contentDocument;
 }
