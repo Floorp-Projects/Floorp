4abd90b0540a2ba15f9f6ee37ddfa787bf25b71e
Emilio Cobos Álvarez <emilio@crisal.io>
2021-09-12 23:37:47 +0000

    Bug 1728187 - Make the non-native theme not use standins for system colors if forcing colors. r=morgan
    We allow using system colors when forcing colors so it makes sense to do this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125271

--- layout/style/PreferenceSheet.h
+++ layout/style/PreferenceSheet.h
@@ -37,12 +37,7 @@ struct PreferenceSheet {
     bool mUseDocumentColors = true;
 
     // Whether the non-native theme should use system colors for widgets.
-    // We only do that if we have a high-contrast theme _and_ we are overriding
-    // the document colors. Otherwise it causes issues when pages only override
-    // some of the system colors, specially in dark themes mode.
-    bool NonNativeThemeShouldUseSystemColors() const {
-      return mUseAccessibilityTheme && !mUseDocumentColors;
-    }
+    bool NonNativeThemeShouldUseSystemColors() const;
 
     void Load(bool aIsChrome);
   };
