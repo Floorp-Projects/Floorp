2078c80b8918b7c9eb173f6caeefb083b164b17b
Ted Campbell <tcampbell@mozilla.com>
2021-09-13 19:21:24 +0000

    Bug 1730426 - Add testcase for OOM during Bailout. r=jandem
    This oomTest covers snapshot recovery during a bailout, including inlined
    frames. Previous test coverage was spotty and was platform dependent.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125357

---
+++ js/src/jit-test/tests/ion/bailout-oom-01.js
@@ -0,0 +1,36 @@
+// |jit-test| --no-threads; --fast-warmup; skip-if: !('oomTest' in this)
+
+setJitCompilerOption("ion.warmup.trigger", 20);
+gczeal(0);
+
+var nonce = 0;
+
+function doTest() {
+    // Block Warp/Ion compile.
+    with ({}) {};
+
+    nonce += 1;
+
+    // Fresh function and script.
+    let fn = new Function("arg", `
+        /* {nonce} */
+        var r1 = [];
+        var r2 = [];
+        return (() => arg + 1)();
+    `);
+
+    // Warm up JITs.
+    for (var i = 0; i < 20; ++i) {
+        assertEq(fn(i), i + 1);
+    }
+
+    // Trigger bailout.
+    fn();
+}
+
+// Warmup doTest already.
+doTest();
+doTest();
+
+// OOM test the JIT compilation and bailout.
+oomTest(doTest);
