d3a34b89169363b1f91eb75d4c7bdc397efb2e0c
Matt Woodrow <mwoodrow@mozilla.com>
2021-09-13 05:01:36 +0000

    Bug 1729236 - Remove GetPaintRect. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D124590

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -2880,10 +2880,8 @@ class nsDisplayItem : public nsDisplayItemLink {
   virtual Maybe<nsRect> GetClipWithRespectToASR(
       nsDisplayListBuilder* aBuilder, const ActiveScrolledRoot* aASR) const;
 
-  const nsRect& GetPaintRect() const { return mBuildingRect; }
-
   virtual const nsRect& GetUntransformedPaintRect() const {
-    return GetPaintRect();
+    return GetBuildingRect();
   }
 
   nsRect GetPaintRect(nsDisplayListBuilder* aBuilder, gfxContext* aCtx);
@@ -6320,11 +6318,6 @@ class nsDisplayTransform : public nsPaintedDisplayItem {
   bool UntransformBuildingRect(nsDisplayListBuilder* aBuilder,
                                nsRect* aOutRect) const {
     return UntransformRect(aBuilder, GetBuildingRect(), aOutRect);
-  }
-
-  bool UntransformPaintRect(nsDisplayListBuilder* aBuilder,
-                            nsRect* aOutRect) const {
-    return UntransformRect(aBuilder, GetPaintRect(), aOutRect);
   }
 
   static gfx::Point3D GetDeltaToTransformOrigin(const nsIFrame* aFrame,
