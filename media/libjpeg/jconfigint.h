16a2c9e3260b0d42aeac5e2b2ecbca758027324b
Andrew Osmond <aosmond@mozilla.com>
2021-09-13 21:55:12 +0000

    Bug 1709303 - Part 1. Prepare scripts and patches for libjpeg-turbo update. r=jrmuizel,tnikkel
    media/libjpeg/1050342.diff is no longer necessary and a correction
    appears to have been made in the library. Chromium no longer uses this
    patch either.
    
    media/libjpeg/assembly-tables.diff and media/libjpeg/externalize-table.diff
    require significant changes in order to apply. This may be done in a
    future followup patch, but is deemed less important than updating
    libjpeg-turbo.
    
    With these patches, an update to libjpeg-turbo 2.1.1 should apply
    cleanly and build.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125458

--- media/libjpeg/jconfigint.h
+++ media/libjpeg/jconfigint.h
@@ -1,5 +1,5 @@
 /* libjpeg-turbo build number */
-#define BUILD "20191231"
+#define BUILD "20210810"
 
 /* Need to use Mozilla-specific function inlining. */
 #include "mozilla/Attributes.h"
@@ -9,7 +9,7 @@
 #define PACKAGE_NAME "libjpeg-turbo"
 
 /* Version number of package */
-#define VERSION "2.0.4"
+#define VERSION "2.1.1"
 
 /* The size of `size_t', as computed by sizeof. */
 #ifdef HAVE_64BIT_BUILD
@@ -34,4 +34,14 @@
 #elif (SIZEOF_SIZE_T == 4)
 #define HAVE_BITSCANFORWARD
 #endif
+#endif
+
+#if defined(__has_attribute)
+#if __has_attribute(fallthrough)
+#define FALLTHROUGH  __attribute__((fallthrough));
+#else
+#define FALLTHROUGH
+#endif
+#else
+#define FALLTHROUGH
 #endif
--- media/update-libjpeg.sh
+++ media/update-libjpeg.sh
@@ -17,17 +17,12 @@ tag=${2-HEAD}
 (cd $repo; git archive --prefix=media/libjpeg/ $tag) | (cd $srcdir/..; tar xf -)
 
 cd $srcdir/libjpeg
-cp win/jsimdcfg.inc simd/
 
-revert_files="1050342.diff assembly-tables.diff externalize-table.diff 1520760-avx2-detection.diff jconfig.h jconfigint.h jpeg_nbits_table.c moz.build MOZCHANGES mozilla.diff simd/jsimdcfg.inc"
+revert_files="jconfig.h jconfigint.h moz.build MOZCHANGES mozilla.diff"
 if test -d ${topsrcdir}/.hg; then
     hg revert --no-backup $revert_files
-elif test -d ${topsrcdir}/.git; then
+elif test -e ${topsrcdir}/.git; then
     git checkout HEAD -- $revert_files
 fi
 
 patch -p0 -i mozilla.diff
-patch -p0 -i 1050342.diff
-patch -p3 -i externalize-table.diff
-patch -p3 -i assembly-tables.diff
-patch -p0 -i 1520760-avx2-detection.diff
