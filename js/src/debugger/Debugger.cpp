0736ef7ca666bf980c8a5dd5c3dce9dedd6b2bfb
Steve Fink <sfink@mozilla.com>
2021-09-13 22:58:52 +0000

    Bug 1729602 - Remove the magic cx constructor handling in `Rooted<TypeConstructibleWithCx> foo(cx);` r=tcampbell,jonco
    Differential Revision: https://phabricator.services.mozilla.com/D124994

--- js/src/debugger/Debugger.cpp
+++ js/src/debugger/Debugger.cpp
@@ -5235,7 +5235,7 @@ class MOZ_STACK_CLASS Debugger::ScriptQuery : public Debugger::QueryBase {
       using RealmToScriptMap =
           GCHashMap<Realm*, BaseScript*, DefaultHasher<Realm*>>;
 
-      Rooted<RealmToScriptMap> innermostForRealm(cx);
+      Rooted<RealmToScriptMap> innermostForRealm(cx, cx);
 
       // Visit each candidate script and find innermost in each realm.
       for (BaseScript* script : scriptVector) {
