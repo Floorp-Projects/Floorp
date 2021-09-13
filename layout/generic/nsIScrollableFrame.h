28838a2dca7aa168743bc5dbbb4d960079174af6
Matt Woodrow <mwoodrow@mozilla.com>
2021-09-13 05:01:35 +0000

    Bug 1542929 - Remove mReferenceFrame from nsDisplayItem. r=miko
    Differential Revision: https://phabricator.services.mozilla.com/D124009

--- layout/generic/nsIScrollableFrame.h
+++ layout/generic/nsIScrollableFrame.h
@@ -476,7 +476,8 @@ class nsIScrollableFrame : public nsIScrollbarMediator {
    */
   virtual mozilla::Maybe<mozilla::layers::ScrollMetadata> ComputeScrollMetadata(
       mozilla::layers::WebRenderLayerManager* aLayerManager,
-      const nsIFrame* aContainerReferenceFrame) const = 0;
+      const nsIFrame* aItemFrame,
+      const nsPoint& aOffsetToReferenceFrame) const = 0;
 
   /**
    * Mark the scrollbar frames for reflow.
