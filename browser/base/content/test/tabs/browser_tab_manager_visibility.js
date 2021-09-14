7a63af58ef986c3459142e90c11d720bc45d8b81
Emilio Cobos Álvarez <emilio@crisal.io>
2021-09-14 10:43:49 +0000

    Bug 1729795 - Clean up openNewWindowWithFlushedXULCacheForMozSupports. r=jaws,extension-reviewers,zombie
    The only test that still needs it is browser_tab_manager_visibility.js.
    The rest look like proton leftovers.
    
    After bug 1729477 those sheets are stored in the shared style sheet
    cache, not in the XUL prototype cache, so remove that.
    
    Differential Revision: https://phabricator.services.mozilla.com/D124983

--- browser/base/content/test/tabs/browser_tab_manager_visibility.js
+++ browser/base/content/test/tabs/browser_tab_manager_visibility.js
@@ -11,7 +11,7 @@ const DUMMY_PAGE_PATH = "/browser/base/content/test/tabs/dummy_page.html";
 add_task(async function tab_manager_visibility_preference_on() {
   Services.prefs.setBoolPref("browser.tabs.tabmanager.enabled", true);
 
-  let newWindow = await BrowserTestUtils.openNewWindowWithFlushedXULCacheForMozSupports();
+  let newWindow = await BrowserTestUtils.openNewWindowWithFlushedCacheForMozSupports();
   await BrowserTestUtils.withNewTab(
     {
       gBrowser: newWindow.gBrowser,
@@ -33,7 +33,7 @@ add_task(async function tab_manager_visibility_preference_on() {
 add_task(async function tab_manager_visibility_preference_off() {
   Services.prefs.setBoolPref("browser.tabs.tabmanager.enabled", false);
 
-  let newWindow = await BrowserTestUtils.openNewWindowWithFlushedXULCacheForMozSupports();
+  let newWindow = await BrowserTestUtils.openNewWindowWithFlushedCacheForMozSupports();
   await BrowserTestUtils.withNewTab(
     {
       gBrowser: newWindow.gBrowser,
