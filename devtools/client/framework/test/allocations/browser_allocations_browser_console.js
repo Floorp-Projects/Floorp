f4dfe2d339232733e538719a57696a55a03437c6
Alexandre Poirot <poirot.alex@gmail.com>
2021-09-14 12:47:49 +0000

    Bug 1730108 - [devtools] Introduce a test to track memory while opening and closing the browser console. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D125154

---
+++ devtools/client/framework/test/allocations/browser_allocations_browser_console.js
@@ -0,0 +1,58 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Record allocations while opening and closing the Browser Console
+
+const TEST_URL =
+  "http://example.com/browser/devtools/client/framework/test/allocations/reloaded-page.html";
+
+const { require } = ChromeUtils.import("resource://devtools/shared/Loader.jsm");
+const {
+  BrowserConsoleManager,
+} = require("devtools/client/webconsole/browser-console-manager");
+
+async function testScript() {
+  // Open
+  await BrowserConsoleManager.toggleBrowserConsole();
+
+  // Reload the tab to make the test slightly more real
+  const hud = BrowserConsoleManager.getBrowserConsole();
+  const onTargetProcessed = hud.commands.targetCommand.once(
+    "processed-available-target"
+  );
+
+  gBrowser.reloadTab(gBrowser.selectedTab);
+
+  info("Wait for target of the new document to be fully processed");
+  await onTargetProcessed;
+
+  // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  // Close
+  await BrowserConsoleManager.toggleBrowserConsole();
+}
+
+add_task(async function() {
+  const tab = await addTab(TEST_URL);
+
+  // Run the test scenario first before recording in order to load all the
+  // modules. Otherwise they get reported as "still allocated" objects,
+  // whereas we do expect them to be kept in memory as they are loaded via
+  // the main DevTools loader, which keeps the module loaded until the
+  // shutdown of Firefox
+  await testScript();
+
+  // Now, run the test script. This time, we record this run.
+  await startRecordingAllocations();
+
+  for (let i = 0; i < 3; i++) {
+    await testScript();
+  }
+
+  await stopRecordingAllocations("browser-console");
+
+  gBrowser.removeTab(tab);
+});
