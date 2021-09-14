dd0fa11c4e6ab1b174f2bc66c1d0e4526ce1c4f6
Andreu Botella <abb@randomunok.com>
2021-09-14 09:48:19 +0000

    Bug 1728995 [wpt PR 30331] - Fix the remaining instances of mixed-case `WebIDL`, a=testonly
    Automatic update from web-platform-tests
    Fix the remaining instances of mixed-case `WebIDL`
    
    The `WebIDL` directory was renamed to lowercase `webidl` in #30327.
    This change fixes two remaining instances of mixed-case `WebIDL` in
    `update-built-tests.sh` and `tools/ci/jobs.py`.
    --
    
    wpt-commits: 8300a95a270ed8c9c4685b906d927c568559deef
    wpt-pr: 30331

--- testing/web-platform/tests/update-built-tests.sh
+++ testing/web-platform/tests/update-built-tests.sh
@@ -8,4 +8,4 @@ infrastructure/assumptions/tools/build.sh
 html/tools/build.sh
 python3 mimesniff/mime-types/resources/generated-mime-types.py
 python3 css/css-ui/tools/appearance-build-webkit-reftests.py
-python3 WebIDL/tools/generate-setlike.py
+python3 webidl/tools/generate-setlike.py
