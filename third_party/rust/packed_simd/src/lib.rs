42765085d5c9172781e1453ebf719559448f6a1e
Mike Hommey <mh+mozilla@glandium.org>
2021-09-15 07:53:21 +0000

    Bug 1730397 - Update packed_simd to a version that supports rustc >= 1.56. r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D125604

--- third_party/rust/packed_simd/src/lib.rs
+++ third_party/rust/packed_simd/src/lib.rs
@@ -199,8 +199,9 @@
 //!   Numeric casts are not very "precise": sometimes lossy, sometimes value
 //!   preserving, etc.
 
+#![cfg_attr(const_generics, feature(const_generics))]
+#![cfg_attr(not(const_generics), feature(adt_const_params))]
 #![feature(
-    const_generics,
     repr_simd,
     rustc_attrs,
     platform_intrinsics,
