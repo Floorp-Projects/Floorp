b228e7547d78d5c492a5c1957a5c5fe5ef9c5e61
Marian-Vasile Laza <mlaza@mozilla.com>
2021-09-15 11:42:13 +0300

    Backed out changeset 9af854b99136 (bug 1730397) as requested by the dev for causing SM bustages. CLOSED TREE

--- third_party/rust/packed_simd/src/lib.rs
+++ third_party/rust/packed_simd/src/lib.rs
@@ -199,9 +199,8 @@
 //!   Numeric casts are not very "precise": sometimes lossy, sometimes value
 //!   preserving, etc.
 
-#![cfg_attr(const_generics, feature(const_generics))]
-#![cfg_attr(not(const_generics), feature(adt_const_params))]
 #![feature(
+    const_generics,
     repr_simd,
     rustc_attrs,
     platform_intrinsics,
