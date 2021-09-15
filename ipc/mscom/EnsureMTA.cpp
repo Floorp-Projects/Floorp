7646447ee09cdd040756097d2efa58c0c1632644
Marian-Vasile Laza <mlaza@mozilla.com>
2021-09-15 14:12:55 +0300

    Backed out 7 changesets (bug 1546154) for causing build bustages on ContentProcess.cpp. CLOSED TREE
    Backed out changeset 0bd777eee249 (bug 1546154)
    Backed out changeset 611812ee62a2 (bug 1546154)
    Backed out changeset cef0aa18a3ab (bug 1546154)
    Backed out changeset cfda47fb0a46 (bug 1546154)
    Backed out changeset f3e005f5fedc (bug 1546154)
    Backed out changeset 7f50fca0c2cd (bug 1546154)
    Backed out changeset bbb0b25450f5 (bug 1546154)

--- ipc/mscom/EnsureMTA.cpp
+++ ipc/mscom/EnsureMTA.cpp
@@ -9,7 +9,6 @@
 #include "mozilla/Assertions.h"
 #include "mozilla/ClearOnShutdown.h"
 #include "mozilla/DebugOnly.h"
-#include "mozilla/mscom/COMWrappers.h"
 #include "mozilla/mscom/Utils.h"
 #include "mozilla/SchedulerGroup.h"
 #include "mozilla/StaticLocalPtr.h"
@@ -18,6 +17,14 @@
 
 #include "private/pprthred.h"
 
+#include <combaseapi.h>
+
+#if (NTDDI_VERSION < NTDDI_WIN8)
+// Win8+ API that we use very carefully
+DECLARE_HANDLE(CO_MTA_USAGE_COOKIE);
+HRESULT WINAPI CoIncrementMTAUsage(CO_MTA_USAGE_COOKIE* pCookie);
+#endif  // (NTDDI_VERSION < NTDDI_WIN8)
+
 namespace {
 
 class EnterMTARunnable : public mozilla::Runnable {
@@ -25,7 +32,7 @@ class EnterMTARunnable : public mozilla::Runnable {
   EnterMTARunnable() : mozilla::Runnable("EnterMTARunnable") {}
   NS_IMETHOD Run() override {
     mozilla::DebugOnly<HRESULT> hr =
-        mozilla::mscom::wrapped::CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+        ::CoInitializeEx(nullptr, COINIT_MULTITHREADED);
     MOZ_ASSERT(SUCCEEDED(hr));
     return NS_OK;
   }
@@ -45,7 +52,7 @@ class BackgroundMTAData {
     if (mThread) {
       mThread->Dispatch(
           NS_NewRunnableFunction("BackgroundMTAData::~BackgroundMTAData",
-                                 &mozilla::mscom::wrapped::CoUninitialize),
+                                 &::CoUninitialize),
           NS_DISPATCH_NORMAL);
       mThread->Shutdown();
     }
@@ -73,19 +80,28 @@ EnsureMTA::EnsureMTA() {
   // We intentionally don't check rv unless we need it when
   // CoIncremementMTAUsage is unavailable.
 
-  // Calling this function initializes the MTA without needing to explicitly
-  // create a thread and call CoInitializeEx to do it.
-  // We don't retain the cookie because once we've incremented the MTA, we
-  // leave it that way for the lifetime of the process.
-  CO_MTA_USAGE_COOKIE mtaCookie = nullptr;
-  HRESULT hr = wrapped::CoIncrementMTAUsage(&mtaCookie);
-  if (SUCCEEDED(hr)) {
-    if (NS_SUCCEEDED(rv)) {
-      // Start the persistent MTA thread (mostly) asynchronously.
-      Unused << GetPersistentMTAThread();
-    }
+  // This API is only available beginning with Windows 8. Even though this
+  // constructor will only be called once, we intentionally use
+  // StaticDynamicallyLinkedFunctionPtr here to hang onto the ole32 module.
+  static const StaticDynamicallyLinkedFunctionPtr<
+      decltype(&::CoIncrementMTAUsage)>
+      pCoIncrementMTAUsage(L"ole32.dll", "CoIncrementMTAUsage");
+  if (pCoIncrementMTAUsage) {
+    // Calling this function initializes the MTA without needing to explicitly
+    // create a thread and call CoInitializeEx to do it.
+    // We don't retain the cookie because once we've incremented the MTA, we
+    // leave it that way for the lifetime of the process.
+    CO_MTA_USAGE_COOKIE mtaCookie = nullptr;
+    HRESULT hr = pCoIncrementMTAUsage(&mtaCookie);
+    MOZ_ASSERT(SUCCEEDED(hr));
+    if (SUCCEEDED(hr)) {
+      if (NS_SUCCEEDED(rv)) {
+        // Start the persistent MTA thread (mostly) asynchronously.
+        Unused << GetPersistentMTAThread();
+      }
 
-    return;
+      return;
+    }
   }
 
   // In the fallback case, we simply initialize our persistent MTA thread.
@@ -112,8 +128,8 @@ EnsureMTA::CreateInstanceInternal(REFCLSID aClsid, REFIID aIid) {
   MOZ_ASSERT(IsCurrentThreadExplicitMTA());
 
   RefPtr<IUnknown> iface;
-  HRESULT hr = wrapped::CoCreateInstance(aClsid, nullptr, CLSCTX_INPROC_SERVER,
-                                         aIid, getter_AddRefs(iface));
+  HRESULT hr = ::CoCreateInstance(aClsid, nullptr, CLSCTX_INPROC_SERVER, aIid,
+                                  getter_AddRefs(iface));
   if (FAILED(hr)) {
     return CreateInstanceAgileRefPromise::CreateAndReject(hr, __func__);
   }
