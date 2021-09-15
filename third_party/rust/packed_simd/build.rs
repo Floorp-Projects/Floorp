40e1ffebcda8a454fd77c58b822392ea15945617
Marian-Vasile Laza <mlaza@mozilla.com>
2021-09-15 11:42:13 +0300

    Backed out changeset 9af854b99136 (bug 1730397) as requested by the dev for causing SM bustages. CLOSED TREE

--- third_party/rust/packed_simd/build.rs
+++ third_party/rust/packed_simd/build.rs
@@ -1,14 +1,8 @@
-use rustc_version::{version, Version};
-
 fn main() {
     println!("cargo:rustc-env=RUSTC_BOOTSTRAP=1");
     let target = std::env::var("TARGET")
         .expect("TARGET environment variable not defined");
     if target.contains("neon") {
         println!("cargo:rustc-cfg=libcore_neon");
-    }
-    let ver = version().unwrap();
-    if ver < Version::parse("1.56.0-alpha").unwrap() {
-        println!("cargo:rustc-cfg=const_generics");
     }
 }
