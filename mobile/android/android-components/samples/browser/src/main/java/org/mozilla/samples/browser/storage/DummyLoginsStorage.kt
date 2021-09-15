8c220b4b6ab279fee156206e9c3432fe4d2e9bdf
Ben Dean-Kawamura <ben@mozilla.com>
2021-09-15 16:48:31 -0400

    [components] Updating DummyLoginsStorage to using the new logins API
    Grisha suggested that we just use `SyncableLoginsStorage` directly and I
    think that would be a great idea.  However, I couldn't figure out how to
    get a test context to give it, so I just updated the dummy
    implementation.

--- mobile/android/android-components/samples/browser/src/main/java/org/mozilla/samples/browser/storage/DummyLoginsStorage.kt
+++ mobile/android/android-components/samples/browser/src/main/java/org/mozilla/samples/browser/storage/DummyLoginsStorage.kt
@@ -4,10 +4,21 @@
 
 package org.mozilla.samples.browser.storage
 
+import mozilla.components.concept.storage.EncryptedLogin
 import mozilla.components.concept.storage.Login
+import mozilla.components.concept.storage.LoginEntry
 import mozilla.components.concept.storage.LoginsStorage
 import org.json.JSONObject
 import java.util.UUID
+
+fun makeLogin(guid: String, entry: LoginEntry) = Login(
+    guid = guid,
+    origin = entry.origin,
+    httpRealm = entry.httpRealm,
+    formActionOrigin = entry.formActionOrigin,
+    username = entry.username,
+    password = entry.password,
+)
 
 /**
  * A dummy [LoginsStorage] that returns a fixed set of fake logins. Currently sample browser does
@@ -32,7 +43,7 @@ class DummyLoginsStorage : LoginsStorage {
 
     override suspend fun touch(guid: String) = Unit
 
-    override suspend fun delete(id: String): Boolean = logins.removeAll { login -> login.guid == id }
+    override suspend fun delete(guid: String): Boolean = logins.removeAll { login -> login.guid == guid }
 
     override suspend fun get(guid: String): Login? = logins.first { login -> login.guid == guid }
 
@@ -42,32 +53,72 @@ class DummyLoginsStorage : LoginsStorage {
 
     override suspend fun wipeLocal() = logins.clear()
 
-    override suspend fun add(login: Login): String {
+    override suspend fun add(entry: LoginEntry): EncryptedLogin {
         val uuid = UUID.randomUUID().toString()
-        logins.add(login.copy(guid = uuid))
-        return uuid
+        val login = makeLogin(uuid, entry)
+        logins.add(login)
+        return encryptLogin(login)
     }
 
-    override suspend fun update(login: Login) {
+    override suspend fun update(guid: String, entry: LoginEntry): EncryptedLogin {
+        var login: Login? = null;
         logins.map { current ->
-            if (current.guid == login.guid) {
-                login
+            if (current.guid == guid) {
+                login = makeLogin(guid, entry)
             } else {
                 current
             }
+        }
+        return encryptLogin(login!!)
+    }
+
+   // Simplified version of the findLoginToUpdate logic from application-services
+    override fun findLoginToUpdate(entry: LoginEntry): Login? {
+        return logins.firstOrNull { it.username == entry.username }
+    }
+
+    // Simplified version of the addOrUpdate logic from application-services
+    override suspend fun addOrUpdate(entry: LoginEntry): EncryptedLogin {
+        val existing = logins.firstOrNull {
+            it.origin == entry.origin && it.username == entry.username
+        }
+        if (existing == null) {
+            return add(entry)
+        } else {
+            return update(existing.guid, entry)
         }
     }
 
     override suspend fun importLoginsAsync(logins: List<Login>) = JSONObject()
 
-    override suspend fun ensureValid(login: Login) = Unit
-
     override suspend fun getByBaseDomain(origin: String): List<Login> {
         return logins.filter { login -> login.origin == origin }
     }
 
-    override suspend fun getPotentialDupesIgnoringUsername(login: Login): List<Login> {
-        return emptyList()
+    // "Encryption" for us is just serializing to JSON
+    private fun encryptLogin(login: Login): EncryptedLogin {
+        var obj = JSONObject()
+        obj.put("username", login.username)
+        obj.put("password", login.password)
+        return EncryptedLogin(
+            guid = login.guid,
+            origin = login.origin,
+            httpRealm = login.httpRealm,
+            formActionOrigin = login.formActionOrigin,
+            secFields = obj.toString(),
+        )
+    }
+
+    override fun decryptLogin(login: EncryptedLogin): Login {
+        val obj = JSONObject(login.secFields)
+        return Login(
+            guid = login.guid,
+            origin = login.origin,
+            httpRealm = login.httpRealm,
+            formActionOrigin = login.formActionOrigin,
+            username = obj.getString("username"),
+            password = obj.getString("password"),
+        )
     }
 
     override fun close() = Unit
