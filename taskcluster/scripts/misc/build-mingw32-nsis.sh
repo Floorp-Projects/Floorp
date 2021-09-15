116a7bc02b3d58398e7632b991678c9d7d2ff339
Mike Hommey <mh+mozilla@glandium.org>
2021-09-15 08:33:06 +0000

    Bug 1730825 - Build NSIS as a relocatable binary. r=firefox-build-system-reviewers,andi
    The reason the error mentioned in build-mingw32-nsis.sh happens is that
    the default mode NSIS builds in is a fully-installed mode, where it
    hardcodes the locations of its data files. This is why nsis needs to
    be used from the same place it's built for. But there's another mode,
    enabled with NSIS_CONFIG_CONST_DATA_PATH=no, that makes it relocatable,
    and makes it find its data files relatively to the nsis binary.
    
    However, there's a bug in the nsis build scripts, which makes the nsis
    binary installed in the destination directory instead of a bin/
    subdirectory, while the source code itself looks for data files relative
    to the parent directory of the directory that contains the executable.
    So we need to set PREFIX_BIN to force the executable to be installed in
    a bin/ subdirectory.
    
    There is also an issue in nsis itself when it's executed by anything
    other than a shell, which we patch out.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125638

--- taskcluster/scripts/misc/build-mingw32-nsis.sh
+++ taskcluster/scripts/misc/build-mingw32-nsis.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -x -e -v
 
-# We set the INSTALL_DIR to match the directory that it will run in exactly,
-# otherwise we get an NSIS error of the form:
-#   checking for NSIS version...
-#   DEBUG: Executing: `/home/worker/workspace/build/src/mingw32/
-#   DEBUG: The command returned non-zero exit status 1.
-#   DEBUG: Its error output was:
-#   DEBUG: | Error: opening stub "/home/worker/workspace/mingw32/
-#   DEBUG: | Error initalizing CEXEBuild: error setting
-#   ERROR: Failed to get nsis version.
-
 INSTALL_DIR=$MOZ_FETCHES_DIR/nsis
 
 mkdir -p $INSTALL_DIR
@@ -26,7 +16,16 @@ make -f win32/Makefile.gcc PREFIX=i686-w64-mingw32-
 
 cd ../nsis-3.07-src
 patch -p1 < $GECKO_PATH/build/win32/nsis-no-insert-timestamp.patch
-scons XGCC_W32_PREFIX=i686-w64-mingw32- ZLIB_W32=../zlib-1.2.11 SKIPUTILS="NSIS Menu" PREFIX=$INSTALL_DIR/ VERSION=3.07 install
+patch -p1 < $GECKO_PATH/build/win32/nsis-no-underscore.patch
+scons \
+  XGCC_W32_PREFIX=i686-w64-mingw32- \
+  ZLIB_W32=../zlib-1.2.11 \
+  SKIPUTILS="NSIS Menu" \
+  PREFIX_DEST=$INSTALL_DIR/ \
+  PREFIX_BIN=bin \
+  NSIS_CONFIG_CONST_DATA_PATH=no \
+  VERSION=3.07 \
+  install
 
 # --------------
 
