9f81851d108c4ccc827eb48617816e548021d850
John Lin <jolin@mozilla.com>
2021-09-15 15:59:43 +0000

    Bug 1665776 - p5: add VPx support to AndroidEncoderModule. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D121022

--- dom/media/platforms/android/AndroidEncoderModule.cpp
+++ dom/media/platforms/android/AndroidEncoderModule.cpp
@@ -6,6 +6,7 @@
 
 #include "AndroidDataEncoder.h"
 #include "MP4Decoder.h"
+#include "VPXDecoder.h"
 
 #include "mozilla/Logging.h"
 
@@ -17,14 +18,34 @@ extern LazyLogModule sPEMLog;
       ("AndroidEncoderModule(%p)::%s: " arg, this, __func__, ##__VA_ARGS__))
 
 bool AndroidEncoderModule::SupportsMimeType(const nsACString& aMimeType) const {
-  return MP4Decoder::IsH264(aMimeType);
+  return (MP4Decoder::IsH264(aMimeType) &&
+          java::HardwareCodecCapabilityUtils::HasHWH264(true /* encoder */)) ||
+         (VPXDecoder::IsVP8(aMimeType) &&
+          java::HardwareCodecCapabilityUtils::HasHWVP8(true /* encoder */)) ||
+         (VPXDecoder::IsVP9(aMimeType) &&
+          java::HardwareCodecCapabilityUtils::HasHWVP9(true /* encoder */));
 }
 
 already_AddRefed<MediaDataEncoder> AndroidEncoderModule::CreateVideoEncoder(
     const CreateEncoderParams& aParams) const {
-  RefPtr<MediaDataEncoder> encoder =
-      new AndroidDataEncoder(aParams.ToH264Config(), aParams.mTaskQueue);
-  return encoder.forget();
+  RefPtr<MediaDataEncoder> encoder;
+  switch (CreateEncoderParams::CodecTypeForMime(aParams.mConfig.mMimeType)) {
+    case MediaDataEncoder::CodecType::H264:
+      return MakeRefPtr<AndroidDataEncoder<MediaDataEncoder::H264Config>>(
+                 aParams.ToH264Config(), aParams.mTaskQueue)
+          .forget();
+    case MediaDataEncoder::CodecType::VP8:
+      return MakeRefPtr<AndroidDataEncoder<MediaDataEncoder::VP8Config>>(
+                 aParams.ToVP8Config(), aParams.mTaskQueue)
+          .forget();
+    case MediaDataEncoder::CodecType::VP9:
+      return MakeRefPtr<AndroidDataEncoder<MediaDataEncoder::VP9Config>>(
+                 aParams.ToVP9Config(), aParams.mTaskQueue)
+          .forget();
+    default:
+      AND_PEM_LOG("Unsupported MIME type:%s", aParams.mConfig.mMimeType.get());
+      return nullptr;
+  }
 }
 
 }  // namespace mozilla
