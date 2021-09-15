2d2fa35c9cb6ee29bfcb8c0100173d43850853b4
Ted Campbell <tcampbell@mozilla.com>
2021-09-15 17:34:25 +0000

    Bug 1730426 - Save exception before calling jit::BailoutIonToBaseline. r=jandem
    Save the exception state of the ExceptionHandlerBailout before calling the
    normal bailout code. If the the bailout fails, we drop the error that triggered
    the bailout and instead use the error that caused bailout to fail (which is
    probably OOM). This is implicitly what the previous code did, but this avoids
    implicitly clobbering exceptions.
    
    This also means we no longer run the RInstruction interpreter under while an
    exception is pending so we can remover a number of callWithABI exclusions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125360

--- js/src/jslibmath.h
+++ js/src/jslibmath.h
@@ -17,7 +17,7 @@
 namespace js {
 
 inline double NumberDiv(double a, double b) {
-  AutoUnsafeCallWithABI unsafe(UnsafeABIStrictness::AllowPendingExceptions);
+  AutoUnsafeCallWithABI unsafe;
   if (b == 0) {
     if (a == 0 || mozilla::IsNaN(a)) {
       return JS::GenericNaN();
@@ -32,7 +32,7 @@ inline double NumberDiv(double a, double b) {
 }
 
 inline double NumberMod(double a, double b) {
-  AutoUnsafeCallWithABI unsafe(UnsafeABIStrictness::AllowPendingExceptions);
+  AutoUnsafeCallWithABI unsafe;
   if (b == 0) {
     return JS::GenericNaN();
   }
