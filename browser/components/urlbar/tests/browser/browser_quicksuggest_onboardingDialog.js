cab35e68cd82fb508d658c1afd0afad6a6422b0e
Drew Willcoxon <adw@mozilla.com>
2021-09-15 15:39:48 +0000

    Bug 1730691 - Properly support tabbing through the elements in the Firefox Suggest onboarding dialog. r=dao, a=RyanVM
    Give `tabindex` attributes to each focusable element and handle enter keypresses
    (via the `dialogaccept` event) on them. The tab index ordering is:
    
    1. Accept button (initial focus)
    2. Settings button
    3. Learn more
    4. Not now
    
    Also give `#infoBody` a tab index of -1 so it doesn't receive focus. It does
    normally for some reason.
    
    The accept button still does not have a focus ring initially. When you hit tab
    the first time, the focus ring appear on the settings button. IMO it's better
    not to show the ring initially since most people won't be interacting with the
    dialog with the keyboard, and even those that do don't necessarily need to see
    it initially. If we do want it initially, calling `focus` on the button in a
    load event listener works.
    
    On Windows (and maybe Linux?), the arrow keys still move around the elements
    until the focus gets stuck on the Not now link. After that the arrow keys don't
    work anymore but the tab key still does. Not sure how to fix that part.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125616

--- browser/components/urlbar/tests/browser/browser_quicksuggest_onboardingDialog.js
+++ browser/components/urlbar/tests/browser/browser_quicksuggest_onboardingDialog.js
@@ -22,24 +22,26 @@ const TELEMETRY_EVENT_CATEGORY = "contextservices.quicksuggest";
 
 // When the accept button is clicked, the user should be opted in.
 add_task(async function accept() {
-  await doDialogTest(true, async () => {
-    let tabCount = gBrowser.tabs.length;
-    let dialogPromise = openDialog("accept");
+  await doDialogTest({
+    expectOptIn: true,
+    callback: async () => {
+      let tabCount = gBrowser.tabs.length;
+      let dialogPromise = openDialog("accept");
 
-    info("Calling maybeShowOnboardingDialog");
-    await UrlbarQuickSuggest.maybeShowOnboardingDialog();
+      info("Calling maybeShowOnboardingDialog");
+      await UrlbarQuickSuggest.maybeShowOnboardingDialog();
 
-    info("Waiting for dialog");
-    await dialogPromise;
+      info("Waiting for dialog");
+      await dialogPromise;
 
-    Assert.equal(
-      gBrowser.currentURI.spec,
-      "about:blank",
-      "Nothing loaded in the current tab"
-    );
-    Assert.equal(gBrowser.tabs.length, tabCount, "No news tabs were opened");
-
-    TelemetryTestUtils.assertEvents([
+      Assert.equal(
+        gBrowser.currentURI.spec,
+        "about:blank",
+        "Nothing loaded in the current tab"
+      );
+      Assert.equal(gBrowser.tabs.length, tabCount, "No news tabs were opened");
+    },
+    telemetryEvents: [
       {
         category: TELEMETRY_EVENT_CATEGORY,
         method: "enable_toggled",
@@ -55,117 +57,315 @@ add_task(async function accept() {
         method: "opt_in_dialog",
         object: "accept",
       },
-    ]);
+    ],
   });
 });
 
 // When the Not Now link is clicked, the user should remain opted out.
 add_task(async function notNow() {
-  await doDialogTest(false, async () => {
-    let tabCount = gBrowser.tabs.length;
-    let dialogPromise = openDialog("onboardingNotNow");
+  await doDialogTest({
+    expectOptIn: false,
+    callback: async () => {
+      let tabCount = gBrowser.tabs.length;
+      let dialogPromise = openDialog("onboardingNotNow");
 
-    info("Calling maybeShowOnboardingDialog");
-    await UrlbarQuickSuggest.maybeShowOnboardingDialog();
+      info("Calling maybeShowOnboardingDialog");
+      await UrlbarQuickSuggest.maybeShowOnboardingDialog();
 
-    info("Waiting for dialog");
-    await dialogPromise;
+      info("Waiting for dialog");
+      await dialogPromise;
 
-    Assert.equal(
-      gBrowser.currentURI.spec,
-      "about:blank",
-      "Nothing loaded in the current tab"
-    );
-    Assert.equal(gBrowser.tabs.length, tabCount, "No news tabs were opened");
-
-    TelemetryTestUtils.assertEvents([
+      Assert.equal(
+        gBrowser.currentURI.spec,
+        "about:blank",
+        "Nothing loaded in the current tab"
+      );
+      Assert.equal(gBrowser.tabs.length, tabCount, "No news tabs were opened");
+    },
+    telemetryEvents: [
       {
         category: TELEMETRY_EVENT_CATEGORY,
         method: "opt_in_dialog",
         object: "not_now",
       },
-    ]);
+    ],
   });
 });
 
-// When the Customize button is clicked, the user should remain opted out and
-// about:preferences#search should load.
-add_task(async function customize() {
-  await doDialogTest(false, async () => {
-    let dialogPromise = openDialog("extra1");
+// When the settings button is clicked, the user should remain opted out and
+// about:preferences should load.
+add_task(async function settings() {
+  await doDialogTest({
+    expectOptIn: false,
+    callback: async () => {
+      let dialogPromise = openDialog("extra1");
 
-    // about:preferences#search will load in the current tab since it's
-    // about:blank.
-    let loadPromise = BrowserTestUtils.browserLoaded(
-      gBrowser.selectedBrowser
-    ).then(() => info("Saw load"));
+      // about:preferences will load in the current tab since it's
+      // about:blank.
+      let loadPromise = BrowserTestUtils.browserLoaded(
+        gBrowser.selectedBrowser
+      ).then(() => info("Saw load"));
 
-    info("Calling maybeShowOnboardingDialog");
-    await UrlbarQuickSuggest.maybeShowOnboardingDialog();
+      info("Calling maybeShowOnboardingDialog");
+      await UrlbarQuickSuggest.maybeShowOnboardingDialog();
 
-    info("Waiting for dialog");
-    await dialogPromise;
+      info("Waiting for dialog");
+      await dialogPromise;
 
-    info("Waiting for load");
-    await loadPromise;
+      info("Waiting for load");
+      await loadPromise;
 
-    Assert.equal(
-      gBrowser.currentURI.spec,
-      "about:preferences#privacy",
-      "Current tab is about:preferences#privacy"
-    );
-
-    TelemetryTestUtils.assertEvents([
+      Assert.equal(
+        gBrowser.currentURI.spec,
+        "about:preferences#privacy",
+        "Current tab is about:preferences#privacy"
+      );
+    },
+    telemetryEvents: [
       {
         category: TELEMETRY_EVENT_CATEGORY,
         method: "opt_in_dialog",
         object: "settings",
       },
-    ]);
+    ],
   });
 });
 
 // When Learn More is clicked, the user should remain opted out and the support
 // URL should open in a new tab.
 add_task(async function learnMore() {
-  await doDialogTest(false, async () => {
-    let dialogPromise = openDialog("onboardingLearnMore");
-    let loadPromise = BrowserTestUtils.waitForNewTab(
-      gBrowser,
-      LEARN_MORE_URL
-    ).then(tab => {
-      info("Saw new tab");
-      return tab;
-    });
+  await doDialogTest({
+    expectOptIn: false,
+    callback: async () => {
+      let dialogPromise = openDialog("onboardingLearnMore");
+      let loadPromise = BrowserTestUtils.waitForNewTab(
+        gBrowser,
+        LEARN_MORE_URL
+      ).then(tab => {
+        info("Saw new tab");
+        return tab;
+      });
 
-    info("Calling maybeShowOnboardingDialog");
-    await UrlbarQuickSuggest.maybeShowOnboardingDialog();
+      info("Calling maybeShowOnboardingDialog");
+      await UrlbarQuickSuggest.maybeShowOnboardingDialog();
 
-    info("Waiting for dialog");
-    await dialogPromise;
+      info("Waiting for dialog");
+      await dialogPromise;
 
-    info("Waiting for new tab");
-    let tab = await loadPromise;
+      info("Waiting for new tab");
+      let tab = await loadPromise;
 
-    Assert.equal(gBrowser.selectedTab, tab, "Current tab is the new tab");
-    Assert.equal(
-      gBrowser.currentURI.spec,
-      LEARN_MORE_URL,
-      "Current tab is the support page"
-    );
-    BrowserTestUtils.removeTab(tab);
-
-    TelemetryTestUtils.assertEvents([
+      Assert.equal(gBrowser.selectedTab, tab, "Current tab is the new tab");
+      Assert.equal(
+        gBrowser.currentURI.spec,
+        LEARN_MORE_URL,
+        "Current tab is the support page"
+      );
+      BrowserTestUtils.removeTab(tab);
+    },
+    telemetryEvents: [
       {
         category: TELEMETRY_EVENT_CATEGORY,
         method: "opt_in_dialog",
         object: "learn_more",
       },
-    ]);
+    ],
   });
 });
 
-async function doDialogTest(expectOptIn, callback) {
+// The Esc key should behave like Not now: It should dismiss the dialog without
+// opting in.
+add_task(async function escKey() {
+  await doFocusTest({
+    tabKeyRepeat: 0,
+    expectedFocusID: "onboardingAcceptButton",
+    expectOptIn: false,
+    callback: async () => {
+      let tabCount = gBrowser.tabs.length;
+      EventUtils.synthesizeKey("KEY_Escape");
+      Assert.equal(
+        gBrowser.currentURI.spec,
+        "about:blank",
+        "Nothing loaded in the current tab"
+      );
+      Assert.equal(gBrowser.tabs.length, tabCount, "No news tabs were opened");
+    },
+    telemetryEvents: [
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "opt_in_dialog",
+        object: "not_now",
+      },
+    ],
+  });
+});
+
+// Tests tabbing through the dialog and pressing enter.
+// Tab key count: 0
+// Expected focused element: accept button
+add_task(async function focus_accept() {
+  await doFocusTest({
+    tabKeyRepeat: 0,
+    expectedFocusID: "onboardingAcceptButton",
+    expectOptIn: true,
+    callback: async () => {
+      EventUtils.synthesizeKey("KEY_Enter");
+    },
+    telemetryEvents: [
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "enable_toggled",
+        object: "enabled",
+      },
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "sponsored_toggled",
+        object: "enabled",
+      },
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "opt_in_dialog",
+        object: "accept",
+      },
+    ],
+  });
+});
+
+// Tests tabbing through the dialog and pressing enter.
+// Tab key count: 1
+// Expected focused element: settings button
+add_task(async function focus_settings() {
+  await doFocusTest({
+    tabKeyRepeat: 1,
+    expectedFocusID: "onboardingSettingsButton",
+    expectOptIn: false,
+    callback: async () => {
+      // about:preferences will load in the current tab since it's about:blank.
+      let loadPromise = BrowserTestUtils.browserLoaded(
+        gBrowser.selectedBrowser
+      ).then(() => info("Saw load"));
+
+      EventUtils.synthesizeKey("KEY_Enter");
+
+      info("Waiting for load");
+      await loadPromise;
+
+      Assert.equal(
+        gBrowser.currentURI.spec,
+        "about:preferences#privacy",
+        "Current tab is about:preferences#privacy"
+      );
+    },
+    telemetryEvents: [
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "opt_in_dialog",
+        object: "settings",
+      },
+    ],
+  });
+});
+
+// Tests tabbing through the dialog and pressing enter.
+// Tab key count: 2
+// Expected focused element: learn more link
+add_task(async function focus_learnMore() {
+  await doFocusTest({
+    tabKeyRepeat: 2,
+    expectedFocusID: "onboardingLearnMore",
+    expectOptIn: false,
+    callback: async () => {
+      let loadPromise = BrowserTestUtils.waitForNewTab(
+        gBrowser,
+        LEARN_MORE_URL
+      ).then(tab => {
+        info("Saw new tab");
+        return tab;
+      });
+
+      EventUtils.synthesizeKey("KEY_Enter");
+
+      info("Waiting for new tab");
+      let tab = await loadPromise;
+
+      Assert.equal(gBrowser.selectedTab, tab, "Current tab is the new tab");
+      Assert.equal(
+        gBrowser.currentURI.spec,
+        LEARN_MORE_URL,
+        "Current tab is the support page"
+      );
+      BrowserTestUtils.removeTab(tab);
+    },
+    telemetryEvents: [
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "opt_in_dialog",
+        object: "learn_more",
+      },
+    ],
+  });
+});
+
+// Tests tabbing through the dialog and pressing enter.
+// Tab key count: 3
+// Expected focused element: not now link
+add_task(async function focus_notNow() {
+  await doFocusTest({
+    tabKeyRepeat: 3,
+    expectedFocusID: "onboardingNotNow",
+    expectOptIn: false,
+    callback: async () => {
+      let tabCount = gBrowser.tabs.length;
+      EventUtils.synthesizeKey("KEY_Enter");
+      Assert.equal(
+        gBrowser.currentURI.spec,
+        "about:blank",
+        "Nothing loaded in the current tab"
+      );
+      Assert.equal(gBrowser.tabs.length, tabCount, "No news tabs were opened");
+    },
+    telemetryEvents: [
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "opt_in_dialog",
+        object: "not_now",
+      },
+    ],
+  });
+});
+
+// Tests tabbing through the dialog and pressing enter.
+// Tab key count: 4
+// Expected focused element: accept button (wraps around)
+add_task(async function focus_accept_wraparound() {
+  await doFocusTest({
+    tabKeyRepeat: 4,
+    expectedFocusID: "onboardingAcceptButton",
+    expectOptIn: true,
+    callback: async () => {
+      EventUtils.synthesizeKey("KEY_Enter");
+    },
+    telemetryEvents: [
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "enable_toggled",
+        object: "enabled",
+      },
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "sponsored_toggled",
+        object: "enabled",
+      },
+      {
+        category: TELEMETRY_EVENT_CATEGORY,
+        method: "opt_in_dialog",
+        object: "accept",
+      },
+    ],
+  });
+});
+
+async function doDialogTest({ expectOptIn, telemetryEvents, callback }) {
   await BrowserTestUtils.withNewTab("about:blank", async () => {
     // Set all the required prefs for showing the onboarding dialog.
     await SpecialPowers.pushPrefEnv({
@@ -196,13 +396,129 @@ async function doDialogTest(expectOptIn, callback) {
       "Sponsored pref enabled status"
     );
 
+    if (telemetryEvents) {
+      TelemetryTestUtils.assertEvents(telemetryEvents);
+    }
+
     await SpecialPowers.popPrefEnv();
   });
 }
 
-async function openDialog(button) {
+// Whether the tab key can move the focus. On macOS with full keyboard access
+// disabled (which is default), this will be false. See `canTabMoveFocus`.
+let gCanTabMoveFocus;
+
+async function doFocusTest({
+  tabKeyRepeat,
+  expectedFocusID,
+  expectOptIn,
+  telemetryEvents,
+  callback,
+}) {
+  if (AppConstants.platform == "macosx") {
+    if (gCanTabMoveFocus === undefined) {
+      gCanTabMoveFocus = await canTabMoveFocus();
+    }
+    if (!gCanTabMoveFocus) {
+      Assert.ok(true, "Skipping focus/tabbing test on Mac");
+      return;
+    }
+  }
+
+  await doDialogTest({
+    expectOptIn,
+    telemetryEvents,
+    callback: async () => {
+      let dialogPromise = BrowserTestUtils.promiseAlertDialogOpen(
+        null,
+        DIALOG_URI,
+        { isSubDialog: true }
+      );
+
+      let maybeShowPromise = UrlbarQuickSuggest.maybeShowOnboardingDialog();
+
+      let win = await dialogPromise;
+      if (win.document.readyState != "complete") {
+        await BrowserTestUtils.waitForEvent(win, "load");
+      }
+
+      let doc = win.document;
+
+      Assert.equal(
+        doc.activeElement.id,
+        "onboardingAcceptButton",
+        "Accept button is focused initially"
+      );
+
+      if (tabKeyRepeat) {
+        EventUtils.synthesizeKey("KEY_Tab", { repeat: tabKeyRepeat });
+      }
+      Assert.equal(
+        doc.activeElement.id,
+        expectedFocusID,
+        "Expected element is focused: " + expectedFocusID
+      );
+
+      await callback();
+      await maybeShowPromise;
+    },
+  });
+}
+
+async function openDialog(button = undefined) {
   await BrowserTestUtils.promiseAlertDialog(button, DIALOG_URI, {
     isSubDialog: true,
   });
   info("Saw dialog");
+}
+
+/**
+ * This is a real hacky way of determining whether the tab key can move focus.
+ * Windows and Linux both support it but macOS does not unless full keyboard
+ * access is enabled, so practically this is only useful on macOS. Gecko seems
+ * to know whether full keyboard access is enabled because it affects focus in
+ * Firefox and some code in nsXULElement.cpp and other places mention it, but
+ * there doesn't seem to be a way to access that information from JS. There is
+ * `Services.focus.elementIsFocusable`, but it returns true regardless of
+ * whether full access is enabled.
+ *
+ * So what we do here is open the dialog and synthesize a tab key. If the focus
+ * doesn't change, then we assume moving the focus via the tab key is not
+ * supported.
+ *
+ * Why not just always skip the focus tasks on Mac? Because individual
+ * developers (like the one writing this comment) may be running macOS with full
+ * keyboard access enabled and want to actually run the tasks on their machines.
+ *
+ * @returns {boolean}
+ */
+async function canTabMoveFocus() {
+  let canMove = false;
+  await doDialogTest({
+    expectOptIn: false,
+    callback: async () => {
+      let dialogPromise = BrowserTestUtils.promiseAlertDialogOpen(
+        null,
+        DIALOG_URI,
+        { isSubDialog: true }
+      );
+
+      let maybeShowPromise = UrlbarQuickSuggest.maybeShowOnboardingDialog();
+
+      let win = await dialogPromise;
+      if (win.document.readyState != "complete") {
+        await BrowserTestUtils.waitForEvent(win, "load");
+      }
+
+      let doc = win.document;
+      let { activeElement } = doc;
+      EventUtils.synthesizeKey("KEY_Tab");
+
+      canMove = activeElement != doc.activeElement;
+
+      EventUtils.synthesizeKey("KEY_Escape");
+      await maybeShowPromise;
+    },
+  });
+  return canMove;
 }
