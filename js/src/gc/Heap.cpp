fbfeb219031967dfb2d22e04162ad8a40856aef8
Jon Coppeard <jcoppeard@mozilla.com>
2021-09-16 07:31:29 +0000

    Bug 1730889 - Part 4: Move ArenaLists::foregroundFinalize to GCRuntime r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D125705

--- js/src/gc/Heap.cpp
+++ js/src/gc/Heap.cpp
@@ -241,6 +241,18 @@ Arena* ArenaLists::takeSweptEmptyArenas() {
   return arenas;
 }
 
+void ArenaLists::setIncrementalSweptArenas(AllocKind kind,
+                                           SortedArenaList& arenas) {
+  incrementalSweptArenaKind = kind;
+  incrementalSweptArenas.ref().clear();
+  incrementalSweptArenas = arenas.toArenaList();
+}
+
+void ArenaLists::clearIncrementalSweptArenas() {
+  incrementalSweptArenaKind = AllocKind::LIMIT;
+  incrementalSweptArenas.ref().clear();
+}
+
 void ArenaLists::checkGCStateNotInUse() {
   // Called before and after collection to check the state is as expected.
 #ifdef DEBUG
