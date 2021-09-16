1b65f558f170d062bdb5e61912404dc052736f8d
Dmitry Bezhetskov <dbezhetskov@igalia.com>
2021-09-16 05:09:24 +0000

    Bug 1639153 - Introduce indirect stubs to optimize call_indirect. r=lth
    This patch introduces indirect stubs.
    An indirect stub is a stub that takes care of any switching activities needed for call_indirect.
    Before this patch, we have to conservatively assume that any call_indirect's target can be from a foreign instance,
    but now we don't need to do this because now Table[i].code is replaced with the address of the corresponding indirect stub.
    
    There is one optimization - if we 100% sure that call_indirect's target belongs to our instance then we can omit indirect stub
    and use raw function call with signature check of course.
    We 100% sure that call_indirect's target is the same instance iff Table is private and we don't export it.
    
    However, even if a table is exported but not used we can still use the optimization but this logic for some next patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D117123

--- js/src/wasm/WasmIonCompile.cpp
+++ js/src/wasm/WasmIonCompile.cpp
@@ -1630,7 +1630,7 @@ class FunctionCompiler {
       callee = CalleeDesc::wasmTable(table, funcTypeId);
     }
 
-    CallSiteDesc desc(lineOrBytecode, CallSiteDesc::Dynamic);
+    CallSiteDesc desc(lineOrBytecode, CallSiteDesc::Indirect);
     ArgTypeVector args(funcType);
     ResultType resultType = ResultType::Vector(funcType.results());
     auto* ins = MWasmCall::New(alloc(), desc, callee, call.regArgs_,
@@ -1651,7 +1651,7 @@ class FunctionCompiler {
       return true;
     }
 
-    CallSiteDesc desc(lineOrBytecode, CallSiteDesc::Dynamic);
+    CallSiteDesc desc(lineOrBytecode, CallSiteDesc::Import);
     auto callee = CalleeDesc::import(globalDataOffset);
     ArgTypeVector args(funcType);
     ResultType resultType = ResultType::Vector(funcType.results());
