a50985f865941a3c37c5fa667ea287baf290e208
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-09-16 13:26:22 +0000

    Bug 1730449 - [devtools] Remove non-existent connectToRemoteFrame in NodeActor spec. r=ochameau,devtools-backward-compat-reviewers.
    The actor method was removed in https://hg.mozilla.org/mozilla-central/rev/103be18558b0.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125814

--- devtools/shared/specs/node.js
+++ devtools/shared/specs/node.js
@@ -144,16 +144,6 @@ const nodeSpec = generateActorSpec({
       request: {},
       response: RetVal("windowDimensions"),
     },
-    connectToRemoteFrame: {
-      request: {},
-      // We are passing a target actor form here.
-      // As we are manually fetching the form JSON via frame-connector.js connectToFrame,
-      // we are not instanciating a protocol.js front class and can't use proper type
-      // here and have automatic marshalling.
-      //
-      // Alex: Can we do something to address that??
-      response: RetVal("json"),
-    },
     waitForFrameLoad: {
       request: {},
       response: {},
