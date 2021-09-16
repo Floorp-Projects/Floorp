b1b4715c62a54141692660b9b6495f73a4341fbd
Kate Hudson <k88hudson@gmail.com>
2021-09-16 17:45:35 +0000

    Bug 1730926 - Split up hasExposure and exposureDescription in Nimbus manifest r=andreio
    Differential Revision: https://phabricator.services.mozilla.com/D125728

--- toolkit/components/nimbus/test/unit/test_FeatureManifest.js
+++ toolkit/components/nimbus/test/unit/test_FeatureManifest.js
@@ -17,18 +17,33 @@ XPCOMUtils.defineLazyGetter(this, "fetchSchema", async () => {
   return schema.definitions.Feature;
 });
 
-add_task(async function test_feature_manifest_is_valid() {
+async function validateManifestEntry(entry) {
   const ajv = new Ajv({ allErrors: true });
   const validate = ajv.compile(await fetchSchema);
+  return [validate(entry), validate.errors];
+}
 
+function throwValidationError(featureId, errors) {
+  throw new Error(
+    `The manifest entry for ${featureId} is not valid in tookit/components/nimbus/NimbusFeature.js: ` +
+      JSON.stringify(errors, undefined, 2)
+  );
+}
+
+add_task(async function test_feature_manifest_is_valid() {
   // Validate each entry in the feature manifest.
-  Object.keys(FeatureManifest).forEach(featureId => {
-    const valid = validate(FeatureManifest[featureId]);
+  for (const featureId in FeatureManifest) {
+    const entry = FeatureManifest[featureId];
+    const [valid, errors] = await validateManifestEntry(entry);
     if (!valid) {
-      throw new Error(
-        `The manifest entry for ${featureId} is not valid in tookit/components/nimbus/NimbusFeature.js: ` +
-          JSON.stringify(validate.errors, undefined, 2)
-      );
+      throwValidationError(featureId, errors);
     }
-  });
+
+    // Note: Because we're on an old version of ajv if/else isn't supported, see https://bugzilla.mozilla.org/show_bug.cgi?id=1730924
+    if (entry.hasExposure && !entry.exposureDescription) {
+      throwValidationError(featureId, {
+        message: "Should have required property 'exposureDescription'",
+      });
+    }
+  }
 });
