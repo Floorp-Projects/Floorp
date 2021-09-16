a3ef1fd32c3336422f868c54ce8ba49a11522bce
Christian Holler <choller@mozilla.com>
2021-09-16 09:42:50 +0000

    Bug 1720866 - Integrate the wasm-smith WebAssembly fuzzer. r=jandem,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D125173

--- js/src/fuzz-tests/testWasm.cpp
+++ js/src/fuzz-tests/testWasm.cpp
@@ -30,6 +30,11 @@ using namespace js::wasm;
 extern JS::PersistentRootedObject gGlobal;
 extern JSContext* gCx;
 
+static bool gIsWasmSmith = false;
+extern "C" {
+size_t gluesmith(uint8_t* data, size_t size, uint8_t* out, size_t maxsize);
+}
+
 static int testWasmInit(int* argc, char*** argv) {
   if (!wasm::HasSupport(gCx) ||
       !GlobalObject::getOrCreateConstructor(gCx, JSProto_WebAssembly)) {
@@ -37,6 +42,11 @@ static int testWasmInit(int* argc, char*** argv) {
   }
 
   return 0;
+}
+
+static int testWasmSmithInit(int* argc, char*** argv) {
+  gIsWasmSmith = true;
+  return testWasmInit(argc, argv);
 }
 
 static bool emptyNativeFunction(JSContext* cx, unsigned argc, Value* vp) {
@@ -124,8 +134,22 @@ static int testWasmFuzz(const uint8_t* buf, size_t size) {
     // Ensure we have no lingering exceptions from previous modules
     gCx->clearPendingException();
 
-    unsigned char moduleLen = buf[currentIndex];
-    currentIndex++;
+    uint16_t moduleLen;
+    if (gIsWasmSmith) {
+      // Jump over the optByte. Unlike with the regular format, for
+      // wasm-smith we are fixing this and use byte 0 as opt-byte.
+      // Eventually this will also be changed for the regular format.
+      if (!currentIndex) {
+        currentIndex++;
+      }
+
+      // Caller ensures the structural soundness of the input here
+      moduleLen = *((uint16_t*)&buf[currentIndex]);
+      currentIndex += 2;
+    } else {
+      moduleLen = buf[currentIndex];
+      currentIndex++;
+    }
 
     if (size - currentIndex < moduleLen) {
       moduleLen = size - currentIndex;
@@ -135,11 +159,16 @@ static int testWasmFuzz(const uint8_t* buf, size_t size) {
       continue;
     }
 
-    if (currentIndex == 1) {
+    if (currentIndex == 1 || (gIsWasmSmith && currentIndex == 3)) {
       // If this is the first module we are reading, we use the first
       // few bytes to tweak some settings. These are fixed anyway and
       // overwritten later on.
-      uint8_t optByte = (uint8_t)buf[currentIndex];
+      uint8_t optByte;
+      if (gIsWasmSmith) {
+        optByte = (uint8_t)buf[0];
+      } else {
+        optByte = (uint8_t)buf[currentIndex];
+      }
 
       // Note that IonPlatformSupport() and CraneliftPlatformSupport() do not
       // take into account whether those compilers support particular features
@@ -197,6 +226,15 @@ static int testWasmFuzz(const uint8_t* buf, size_t size) {
     // Expected header for a valid WebAssembly module
     uint32_t magic_header = 0x6d736100;
     uint32_t magic_version = 0x1;
+
+    if (gIsWasmSmith) {
+      // When using wasm-smith, magic values should already be there.
+      // Checking this to make sure the data passed is sane.
+      MOZ_RELEASE_ASSERT(*(uint32_t*)(&buf[currentIndex]) == magic_header,
+                         "Magic header mismatch!");
+      MOZ_RELEASE_ASSERT(*(uint32_t*)(&buf[currentIndex + 4]) == magic_version,
+                         "Magic version mismatch!");
+    }
 
     // We just skip over the first 8 bytes now because we fill them
     // with `magic_header` and `magic_version` anyway.
@@ -451,4 +489,68 @@ static int testWasmFuzz(const uint8_t* buf, size_t size) {
   return 0;
 }
 
+static int testWasmSmithFuzz(const uint8_t* buf, size_t size) {
+  // Define maximum sizes for the input to wasm-smith as well
+  // as the resulting modules. The input to output size factor
+  // of wasm-smith is somewhat variable but a factor of 4 seems
+  // to roughly work out. The logic below also assumes that these
+  // are powers of 2.
+  const size_t maxInputSize = 1024;
+  const size_t maxModuleSize = 4096;
+
+  size_t maxModules = size / maxInputSize + 1;
+
+  // We need 1 leading byte for options and 2 bytes for size per module
+  uint8_t* out =
+      new uint8_t[1 + maxModules * (maxModuleSize + sizeof(uint16_t))];
+
+  auto deleteGuard = mozilla::MakeScopeExit([&] { delete[] out; });
+
+  // Copy the opt-byte.
+  out[0] = buf[0];
+
+  size_t outIndex = 1;
+  size_t currentIndex = 1;
+
+  while (currentIndex < size) {
+    size_t remaining = size - currentIndex;
+
+    // We need to have at least a size and some byte to read.
+    if (remaining <= sizeof(uint16_t)) {
+      break;
+    }
+
+    // Determine size of the next input, limited to `maxInputSize`.
+    uint16_t inSize =
+        (*((uint16_t*)&buf[currentIndex]) & (maxInputSize - 1)) + 1;
+    remaining -= sizeof(uint16_t);
+    currentIndex += sizeof(uint16_t);
+
+    // Cap to remaining bytes.
+    inSize = remaining >= inSize ? inSize : remaining;
+
+    size_t outSize =
+        gluesmith((uint8_t*)&buf[currentIndex], inSize,
+                  out + outIndex + sizeof(uint16_t), maxModuleSize);
+
+    if (!outSize) {
+      break;
+    }
+
+    currentIndex += inSize;
+
+    // Write the size of the resulting module to our output buffer.
+    *(uint16_t*)(&out[outIndex]) = (uint16_t)outSize;
+    outIndex += sizeof(uint16_t) + outSize;
+  }
+
+  // If we lack at least one module, don't do anything.
+  if (outIndex == 1) {
+    return 0;
+  }
+
+  return testWasmFuzz(out, outIndex);
+}
+
 MOZ_FUZZING_INTERFACE_RAW(testWasmInit, testWasmFuzz, Wasm);
+MOZ_FUZZING_INTERFACE_RAW(testWasmSmithInit, testWasmSmithFuzz, WasmSmith);
