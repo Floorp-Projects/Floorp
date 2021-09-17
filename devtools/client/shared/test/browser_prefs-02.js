bb6b1b3635b069871d291faadd2f68b5744187ee
Julian Descottes <jdescottes@mozilla.com>
2021-09-17 14:24:50 +0000

    Bug 1729255 - [devtools] Store JSON preferences as String instead of Char in PrefsHelper r=bomsy
    JSON preferences used to be stored in string preferences, which is compatible with special characters.
    There should not be any data loss, char prefs seem to gracefully transition to string prefs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125877

--- devtools/client/shared/test/browser_prefs-02.js
+++ devtools/client/shared/test/browser_prefs-02.js
@@ -8,25 +8,16 @@
 const { PrefsHelper } = require("devtools/client/shared/prefs");
 
 function test() {
-  const originalJson = Services.prefs.getCharPref(
-    "devtools.performance.timeline.hidden-markers"
-  );
-  const originalFloat = Services.prefs.getCharPref(
-    "devtools.performance.memory.sample-probability"
-  );
-
-  const Prefs = new PrefsHelper("devtools.performance", {
-    float: ["Float", "memory.sample-probability"],
-    json: ["Json", "timeline.hidden-markers"],
+  const Prefs = new PrefsHelper("prefs.helper.test", {
+    float: ["Float", "float"],
+    json: ["Json", "json"],
+    jsonWithSpecialChar: ["Json", "jsonWithSpecialChar"],
   });
 
   Prefs.registerObserver();
 
-  // Float
-  Services.prefs.setCharPref(
-    "devtools.performance.timeline.hidden-markers",
-    '{"a":1}'
-  );
+  // JSON
+  Services.prefs.setCharPref("prefs.helper.test.json", '{"a":1}');
   is(Prefs.json.a, 1, "The JSON pref value is correctly casted on get.");
 
   Prefs.json = { b: 2 };
@@ -37,11 +28,31 @@ function test() {
   );
   is(Prefs.json.b, 2, "The JSON pref value is correctly casted on set (2).");
 
-  // Float
-  Services.prefs.setCharPref(
-    "devtools.performance.memory.sample-probability",
-    "3.14"
+  // JSON with special character
+  Services.prefs.setStringPref(
+    "prefs.helper.test.jsonWithSpecialChar",
+    `{"hello":"おはよう皆！"}`
   );
+  is(
+    Prefs.jsonWithSpecialChar.hello,
+    "おはよう皆！",
+    "The JSON pref value with a special character is correctly stored."
+  );
+
+  Prefs.jsonWithSpecialChar = { bye: "さよなら！" };
+  is(
+    Prefs.jsonWithSpecialChar.hello,
+    undefined,
+    "The JSON with the special characters pref value is correctly set. (1)"
+  );
+  is(
+    Prefs.jsonWithSpecialChar.bye,
+    "さよなら！",
+    "The JSON with the special characters pref value is correctly set. (2)"
+  );
+
+  // Float
+  Services.prefs.setCharPref("prefs.helper.test.float", "3.14");
   is(Prefs.float, 3.14, "The float pref value is correctly casted on get.");
 
   Prefs.float = 6.28;
@@ -49,13 +60,8 @@ function test() {
 
   Prefs.unregisterObserver();
 
-  Services.prefs.setCharPref(
-    "devtools.performance.timeline.hidden-markers",
-    originalJson
-  );
-  Services.prefs.setCharPref(
-    "devtools.performance.memory.sample-probability",
-    originalFloat
-  );
+  Services.prefs.clearUserPref("prefs.helper.test.float");
+  Services.prefs.clearUserPref("prefs.helper.test.json");
+  Services.prefs.clearUserPref("prefs.helper.test.jsonWithSpecialChar");
   finish();
 }
