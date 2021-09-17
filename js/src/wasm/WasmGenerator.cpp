b986eb543bdf48ea22870c50ced6ad21d74c1b27
Ryan Hunt <rhunt@eqrion.net>
2021-09-17 21:19:23 +0000

    Bug 1731121 - wasm: Make TypeContext refcounted and shareable. r=lth
    This commit makes TypeContext refcounted/shareable. This is so that
    we can change RttValue's representation to be:
      1. RefPtr<TypeContext> typeContext;
      2. uint32_t typeIndex;
    Effectively adding an explicit type context field to RttValue instead
    of using an implicit global TypeContext, as it is now.
    
    The big change here is that as TypeContext is now refcounted, it
    needs to be heap-allocated, and cannot be a plain member of
    ModuleEnvironment, as it is now. This adds some extra indirections.
    If this is unacceptable, then I think we can find a workaround.
    
    The next commit will implement the changes to RttValue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125889

--- js/src/wasm/WasmGenerator.cpp
+++ js/src/wasm/WasmGenerator.cpp
@@ -275,9 +275,9 @@ bool ModuleGenerator::init(Metadata* maybeAsmJSMetadata) {
     // Copy type definitions to metadata that are required at runtime,
     // allocating global data so that codegen can find the type id's at
     // runtime.
-    for (uint32_t typeIndex = 0; typeIndex < moduleEnv_->types.length();
+    for (uint32_t typeIndex = 0; typeIndex < moduleEnv_->types->length();
          typeIndex++) {
-      const TypeDef& typeDef = moduleEnv_->types[typeIndex];
+      const TypeDef& typeDef = (*moduleEnv_->types)[typeIndex];
       TypeIdDesc& typeId = moduleEnv_->typeIds[typeIndex];
 
       if (TypeIdDesc::isGlobal(typeDef)) {
@@ -308,12 +308,12 @@ bool ModuleGenerator::init(Metadata* maybeAsmJSMetadata) {
     if (moduleEnv_->functionReferencesEnabled()) {
       // Do a linear pass to create a map from src index to dest index.
       RenumberVector renumbering;
-      if (!renumbering.reserve(moduleEnv_->types.length())) {
+      if (!renumbering.reserve(moduleEnv_->types->length())) {
         return false;
       }
       for (uint32_t srcIndex = 0, destIndex = 0;
-           srcIndex < moduleEnv_->types.length(); srcIndex++) {
-        const TypeDef& typeDef = moduleEnv_->types[srcIndex];
+           srcIndex < moduleEnv_->types->length(); srcIndex++) {
+        const TypeDef& typeDef = (*moduleEnv_->types)[srcIndex];
         if (!TypeIdDesc::isGlobal(typeDef)) {
           renumbering.infallibleAppend(UINT32_MAX);
           continue;
