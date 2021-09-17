f61e803ae7488463f1c9c034f46134fbffd96788
Yury Delendik <ydelendik@mozilla.com>
2021-09-17 12:16:23 +0000

    Bug 1487113 - nsICacheInfoChannel.alternativeDataInputStream as attribute. r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D117360

--- netwerk/protocol/http/HttpChannelChild.h
+++ netwerk/protocol/http/HttpChannelChild.h
@@ -142,9 +142,6 @@ class HttpChannelChild final : public PHttpChannelChild,
   mozilla::ipc::IPCResult RecvOriginalCacheInputStreamAvailable(
       const Maybe<IPCStream>& aStream) override;
 
-  mozilla::ipc::IPCResult RecvAltDataCacheInputStreamAvailable(
-      const Maybe<IPCStream>& aStream) override;
-
   virtual void ActorDestroy(ActorDestroyReason aWhy) override;
 
   virtual void DoNotifyListenerCleanup() override;
@@ -197,7 +194,8 @@ class HttpChannelChild final : public PHttpChannelChild,
   void ProcessOnStartRequest(const nsHttpResponseHead& aResponseHead,
                              const bool& aUseResponseHead,
                              const nsHttpHeaderArray& aRequestHeaders,
-                             const HttpChannelOnStartRequestArgs& aArgs);
+                             const HttpChannelOnStartRequestArgs& aArgs,
+                             const HttpChannelAltDataStream& aAltData);
 
   // Callbacks while receiving OnTransportAndData/OnStopRequest/OnProgress/
   // OnStatus/FlushedForDiversion/DivertMessages on background IPC channel.
@@ -274,7 +272,7 @@ class HttpChannelChild final : public PHttpChannelChild,
   RefPtr<ChannelEventQueue> mEventQ;
 
   nsCOMPtr<nsIInputStreamReceiver> mOriginalInputStreamReceiver;
-  nsCOMPtr<nsIInputStreamReceiver> mAltDataInputStreamReceiver;
+  nsCOMPtr<nsIInputStream> mAltDataInputStream;
 
   // Used to ensure atomicity of mBgChild and mBgInitFailCallback
   Mutex mBgChildMutex{"HttpChannelChild::BgChildMutex"};
