28807d5a6e951f02df1cd01c7c4e3815781b9b10
Julian Descottes <jdescottes@mozilla.com>
2021-09-17 15:55:12 +0000

    Bug 1676427 - [devtools] Remove whitespace added by the webpack Banner plugin r=nchevobbe
    Depends on D125943
    
    The Banner including the license text was introducing a white space which fails some of our linters.
    The existing js files which are using the Banner plugin are probably skipped by the whitespace linter, but vendors.css is not.
    
    The whitespace is removed in the string passed to the Banner plugin in webpack.config.js.
    The rest of the diff comes from running `node devtools/client/debugger/bin/bundle.js`
    
    Differential Revision: https://phabricator.services.mozilla.com/D125969

--- devtools/client/debugger/webpack.config.js
+++ devtools/client/debugger/webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
       banner: `/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- `,
+`,
       raw: true,
     }),
     new ObjectRestSpreadPlugin(),
