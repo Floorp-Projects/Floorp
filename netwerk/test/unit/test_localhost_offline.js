4fce4f4f6db871796f0c3e9e57fcfc7582fd0db3
Kershaw Chang <kershaw@mozilla.com>
2021-09-17 12:53:03 +0000

    Bug 1731030 - Check if the status code is NS_ERROR_OFFLINE, r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D125809

--- netwerk/test/unit/test_localhost_offline.js
+++ netwerk/test/unit/test_localhost_offline.js
@@ -2,6 +2,7 @@
 
 const { HttpServer } = ChromeUtils.import("resource://testing-common/httpd.js");
 var httpServer = null;
+const body = "Hello";
 
 function makeChan(url) {
   let chan = NetUtil.newChannel({
@@ -13,6 +14,15 @@ function makeChan(url) {
   return chan;
 }
 
+function channelOpenPromise(chan, flags) {
+  return new Promise(resolve => {
+    function finish(req, buffer) {
+      resolve([req, buffer]);
+    }
+    chan.asyncOpen(new ChannelListener(finish, null, flags));
+  });
+}
+
 function makeURL(host) {
   return `http://${host}:${httpServer.identity.primaryPort}/`;
 }
@@ -21,25 +31,30 @@ add_task(async function test_localhost_offline() {
   Services.io.offline = true;
   Services.prefs.setBoolPref("network.disable-localhost-when-offline", false);
   let chan = makeChan(makeURL("127.0.0.1"));
-  await new Promise(resolve => {
-    chan.asyncOpen(new ChannelListener(resolve));
-  });
+  let [, resp] = await channelOpenPromise(chan);
+  Assert.equal(resp, body, "Should get correct response");
 
   chan = makeChan(makeURL("localhost"));
-  await new Promise(resolve => {
-    chan.asyncOpen(new ChannelListener(resolve));
-  });
+  [, resp] = await channelOpenPromise(chan);
+  Assert.equal(resp, body, "Should get response");
 
   Services.prefs.setBoolPref("network.disable-localhost-when-offline", true);
 
   chan = makeChan(makeURL("127.0.0.1"));
-  await new Promise(resolve => {
-    chan.asyncOpen(new ChannelListener(resolve, null, CL_EXPECT_FAILURE));
-  });
+  let [req] = await channelOpenPromise(
+    chan,
+    CL_ALLOW_UNKNOWN_CL | CL_EXPECT_FAILURE
+  );
+  req.QueryInterface(Ci.nsIHttpChannel);
+  Assert.equal(req.status, Cr.NS_ERROR_OFFLINE);
+
   chan = makeChan(makeURL("localhost"));
-  await new Promise(resolve => {
-    chan.asyncOpen(new ChannelListener(resolve, null, CL_EXPECT_FAILURE));
-  });
+  [req] = await channelOpenPromise(
+    chan,
+    CL_ALLOW_UNKNOWN_CL | CL_EXPECT_FAILURE
+  );
+  req.QueryInterface(Ci.nsIHttpChannel);
+  Assert.equal(req.status, Cr.NS_ERROR_OFFLINE);
 
   Services.prefs.clearUserPref("network.disable-localhost-when-offline");
   Services.io.offline = false;
@@ -47,11 +62,12 @@ add_task(async function test_localhost_offline() {
 
 function run_test() {
   httpServer = new HttpServer();
-  httpServer.registerPathHandler("/", response => {
+  httpServer.registerPathHandler("/", (request, response) => {
     response.seizePower();
     response.write("HTTP/1.1 200 OK\r\n");
+    response.write("Content-Length: " + body.length + "\r\n");
     response.write("\r\n");
-    response.write("Hello, world!");
+    response.write(body);
     response.finish();
   });
   httpServer.start(-1);
