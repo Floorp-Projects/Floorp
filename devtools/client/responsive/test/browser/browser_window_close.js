f1c59fa2b83d8538d594a568d67adca109a9e765
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-09-20 08:27:56 +0000

    Bug 1688445 - [devtools] Fix browser/browser_window_close.js intermittent. r=jdescottes.
    The test seemed to fail because the page was already loaded when we started
    awaiting for `BrowserTestUtils.browserLoaded`.
    This patch removes it and instead pass an `url` param to `BrowserTestUtils.waitForNewWindow`,
    which means we'll wait for the page to be loaded.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126063

--- devtools/client/responsive/test/browser/browser_window_close.js
+++ devtools/client/responsive/test/browser/browser_window_close.js
@@ -6,24 +6,35 @@
 addRDMTask(
   null,
   async function() {
-    const newWindowPromise = BrowserTestUtils.waitForNewWindow();
-    window.open("data:text/html;charset=utf-8,", "_blank", "noopener,all");
+    const NEW_WINDOW_URL =
+      "data:text/html;charset=utf-8,New window opened via window.open";
+    const newWindowPromise = BrowserTestUtils.waitForNewWindow({
+      // Passing the url param so the Promise will resolve once DOMContentLoaded is emitted
+      // on the new window tab
+      url: NEW_WINDOW_URL,
+    });
+    window.open(NEW_WINDOW_URL, "_blank", "noopener,all");
+
     const newWindow = await newWindowPromise;
+    ok(true, "Got new window");
 
+    info("Focus new window");
     newWindow.focus();
-    await BrowserTestUtils.browserLoaded(newWindow.gBrowser.selectedBrowser);
 
+    info("Open RDM");
     const tab = newWindow.gBrowser.selectedTab;
     const { ui } = await openRDM(tab);
     await waitForDeviceAndViewportState(ui);
 
-    // Close the window on a tab with an active responsive design UI and
-    // wait for the UI to gracefully shutdown.  This has leaked the window
-    // in the past.
     ok(
       ResponsiveUIManager.isActiveForTab(tab),
       "ResponsiveUI should be active for tab when the window is closed"
     );
+
+    // Close the window on a tab with an active responsive design UI and
+    // wait for the UI to gracefully shutdown.  This has leaked the window
+    // in the past.
+    info("Close the new window");
     const offPromise = once(ResponsiveUIManager, "off");
     await BrowserTestUtils.closeWindow(newWindow);
     await offPromise;
