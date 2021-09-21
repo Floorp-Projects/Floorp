568fbd349e84ed0a30af0ffda892bf5430e8b96c
André Bargull <andre.bargull@gmail.com>
2021-09-21 08:51:19 +0000

    Bug 1693576 - Part 1: Add "commonlyUsed" to Calendar and Collator. r=platform-i18n-reviewers,gregtatum
    Pass through the "commonlyUsed" parameter to ICU to be able to filter any
    uncommon keyword values.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125570

--- intl/components/src/Calendar.cpp
+++ intl/components/src/Calendar.cpp
@@ -89,10 +89,11 @@ SpanResult<char> Calendar::LegacyIdentifierToBcp47(const char* aIdentifier,
 
 /* static */
 Result<Calendar::Bcp47IdentifierEnumeration, ICUError>
-Calendar::GetBcp47KeywordValuesForLocale(const char* aLocale) {
+Calendar::GetBcp47KeywordValuesForLocale(const char* aLocale,
+                                         CommonlyUsed aCommonlyUsed) {
   UErrorCode status = U_ZERO_ERROR;
   UEnumeration* enumeration = ucal_getKeywordValuesForLocale(
-      "calendar", aLocale, /* commonlyUsed */ false, &status);
+      "calendar", aLocale, static_cast<bool>(aCommonlyUsed), &status);
 
   if (U_SUCCESS(status)) {
     return Bcp47IdentifierEnumeration(enumeration);
