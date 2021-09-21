11151d13b27e8bdfe7aab2c3b0b02eaf975a8f2b
Junjie Ying <yingjunj@msu.edu>
2021-09-21 00:03:49 +0000

    Bug 1719939 - Remove last fragments of code manually checking the browser.proton.enabled pref. r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D125345

--- browser/base/content/certerror/aboutNetError.js
+++ browser/base/content/certerror/aboutNetError.js
@@ -278,21 +278,7 @@ function initPage() {
 
   var err = gErrorCode;
   // List of error pages with an illustration.
-  let illustratedErrors = [
-    "malformedURI",
-    "dnsNotFound",
-    "connectionFailure",
-    "netInterrupt",
-    "netTimeout",
-    "netReset",
-    "netOffline",
-  ];
-  if (
-    illustratedErrors.includes(err) &&
-    !RPMGetBoolPref("browser.proton.enabled")
-  ) {
-    document.body.classList.add("illustrated", err);
-  }
+
   if (err == "blockedByPolicy") {
     document.body.classList.add("blocked");
   }
