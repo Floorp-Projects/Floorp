9c9e28cef16aaccecf2107bb29185837a6277c8e
Tooru Fujisawa <arai_a@mac.com>
2021-09-21 05:01:36 +0000

    Bug 1718529 - Part 2: Remove cloneAndExecuteScript testing function. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D121256

--- js/src/jit-test/tests/asm.js/testBug893519.js
+++
@@ -1,4 +0,0 @@
-// |jit-test| error:Error: AsmJS modules do not yet support cloning; skip-if: !isAsmJSCompilationAvailable()
-
-var g = newGlobal({newCompartment: true});
-cloneAndExecuteScript("function h() { function f() { 'use asm'; function g() { return 42 } return g } return f }", g);
--- js/src/jit-test/tests/basic/testLet.js
+++ js/src/jit-test/tests/basic/testLet.js
@@ -19,11 +19,6 @@ function test(str, arg, result)
     // test reflection logic
     Reflect.parse(got);
 
-    // test script cloning
-    var code = "(function (x) { " + str + " })";
-    var c = cloneAndExecuteScript(code, otherGlobal);
-    assertEq(c.toString(), eval(code).toString());
-
     var got = fun(arg);
     var expect = result;
     if (got !== expect) {
