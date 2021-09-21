02ee947437df11a6aebb4bf8b3f803e53c90320b
Marian-Vasile Laza <mlaza@mozilla.com>
2021-09-21 22:13:33 +0300

    Backed out changeset c0b5f3efa4d4 (bug 1731432) for causing reftest failures. CLOSED TREE

--- js/public/Id.h
+++ js/public/Id.h
@@ -262,14 +262,9 @@ struct BarrierMethods<jsid> {
     }
     return nullptr;
   }
-  static void writeBarriers(jsid* idp, jsid prev, jsid next) {
-    if (prev.isString()) {
-      JS::IncrementalPreWriteBarrier(JS::GCCellPtr(prev.toString()));
-      MOZ_ASSERT(!gc::IsInsideNursery(next.toString()));
-    }
-    if (prev.isSymbol()) {
-      JS::IncrementalPreWriteBarrier(JS::GCCellPtr(prev.toSymbol()));
-    }
+  static void postWriteBarrier(jsid* idp, jsid prev, jsid next) {
+    MOZ_ASSERT_IF(JSID_IS_STRING(next),
+                  !gc::IsInsideNursery(JSID_TO_STRING(next)));
   }
   static void exposeToJS(jsid id) {
     if (id.isGCThing()) {
