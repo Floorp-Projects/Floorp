e87afe3c34d1af9c0ba41368c0e8f6c01d665fd5
André Bargull <andre.bargull@gmail.com>
2021-09-21 08:51:20 +0000

    Bug 1693576 - Part 2: Add DateTimeFormat::GetAllowedHourCycles(). r=platform-i18n-reviewers,gregtatum
    ICU's public API only provides a function to return the preferred hour cycle
    (`udatpg_getDefaultHourCycle()`), whereas for `Intl.Locale` we want to be able
    to determine all allowed hour cycles. So we have to do it the hard way and
    directly read ICU resource bundles to get the data we need for this feature.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125571

--- intl/components/src/DateTimeFormat.h
+++ intl/components/src/DateTimeFormat.h
@@ -443,6 +443,19 @@ class DateTimeFormat final {
   static Maybe<DateTimeFormat::HourCycle> HourCycleFromPattern(
       Span<const char16_t> aPattern);
 
+  using HourCyclesVector = Vector<HourCycle, 4>;
+
+  /**
+   * Returns the allowed hour cycles for the input locale.
+   *
+   * NOTE: This function currently takes a language subtag and an optional
+   * region subtag. This is a restriction until bug 1719746 has migrated
+   * language tag processing into the unified Intl component. After bug 1719746,
+   * this function should be changed to accept a single locale tag.
+   */
+  static Result<HourCyclesVector, ICUError> GetAllowedHourCycles(
+      Span<const char> aLanguage, Maybe<Span<const char>> aRegion);
+
  private:
   explicit DateTimeFormat(UDateFormat* aDateFormat);
 
