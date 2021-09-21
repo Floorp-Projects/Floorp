7cdbee55521c211caf50d43dbafc885a89cda553
André Bargull <andre.bargull@gmail.com>
2021-09-21 15:42:16 +0000

    Bug 1731080 - Part 1: Add a String class to the unified Intl API. r=platform-i18n-reviewers,gregtatum
    The default constructor was deleted because all methods are static.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125838

---
+++ intl/components/gtest/TestString.cpp
@@ -0,0 +1,62 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#include "gtest/gtest.h"
+
+#include "mozilla/intl/String.h"
+#include "mozilla/Span.h"
+
+#include "TestBuffer.h"
+
+namespace mozilla::intl {
+
+static Result<std::u16string_view, ICUError> ToLocaleLowerCase(
+    const char* aLocale, const char16_t* aString,
+    TestBuffer<char16_t>& aBuffer) {
+  aBuffer.clear();
+
+  MOZ_TRY(String::ToLocaleLowerCase(aLocale, MakeStringSpan(aString), aBuffer));
+
+  return aBuffer.get_string_view();
+}
+
+static Result<std::u16string_view, ICUError> ToLocaleUpperCase(
+    const char* aLocale, const char16_t* aString,
+    TestBuffer<char16_t>& aBuffer) {
+  aBuffer.clear();
+
+  MOZ_TRY(String::ToLocaleUpperCase(aLocale, MakeStringSpan(aString), aBuffer));
+
+  return aBuffer.get_string_view();
+}
+
+TEST(IntlString, ToLocaleLowerCase)
+{
+  TestBuffer<char16_t> buf;
+
+  ASSERT_EQ(ToLocaleLowerCase("en", u"test", buf).unwrap(), u"test");
+  ASSERT_EQ(ToLocaleLowerCase("en", u"TEST", buf).unwrap(), u"test");
+
+  // Turkish dotless i.
+  ASSERT_EQ(ToLocaleLowerCase("tr", u"I", buf).unwrap(), u"ı");
+  ASSERT_EQ(ToLocaleLowerCase("tr", u"İ", buf).unwrap(), u"i");
+  ASSERT_EQ(ToLocaleLowerCase("tr", u"I\u0307", buf).unwrap(), u"i");
+}
+
+TEST(IntlString, ToLocaleUpperCase)
+{
+  TestBuffer<char16_t> buf;
+
+  ASSERT_EQ(ToLocaleUpperCase("en", u"test", buf).unwrap(), u"TEST");
+  ASSERT_EQ(ToLocaleUpperCase("en", u"TEST", buf).unwrap(), u"TEST");
+
+  // Turkish dotless i.
+  ASSERT_EQ(ToLocaleUpperCase("tr", u"i", buf).unwrap(), u"İ");
+  ASSERT_EQ(ToLocaleUpperCase("tr", u"ı", buf).unwrap(), u"I");
+
+  // Output can be longer than the input string.
+  ASSERT_EQ(ToLocaleUpperCase("en", u"Größenmaßstäbe", buf).unwrap(),
+            u"GRÖSSENMASSSTÄBE");
+}
+
+}  // namespace mozilla::intl
