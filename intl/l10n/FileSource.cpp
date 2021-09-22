7e6caf3eebbd9a10e56e98540d9e6c43083c941f
Dan Minor <dminor@mozilla.com>
2021-09-22 18:16:07 +0000

    Bug 1642415 - Use metasource in FileSource; r=platform-i18n-reviewers,extension-reviewers,gregtatum,zombie
    Differential Revision: https://phabricator.services.mozilla.com/D125239

--- intl/l10n/FileSource.cpp
+++ intl/l10n/FileSource.cpp
@@ -23,8 +23,8 @@ L10nFileSource::L10nFileSource(RefPtr<const ffi::FileSource> aRaw,
 /* static */
 already_AddRefed<L10nFileSource> L10nFileSource::Constructor(
     const GlobalObject& aGlobal, const nsACString& aName,
-    const nsTArray<nsCString>& aLocales, const nsACString& aPrePath,
-    const dom::FileSourceOptions& aOptions,
+    const nsACString& aMetaSource, const nsTArray<nsCString>& aLocales,
+    const nsACString& aPrePath, const dom::FileSourceOptions& aOptions,
     const Optional<Sequence<nsCString>>& aIndex, ErrorResult& aRv) {
   nsCOMPtr<nsIGlobalObject> global = do_QueryInterface(aGlobal.GetAsSupports());
 
@@ -35,11 +35,11 @@ already_AddRefed<L10nFileSource> L10nFileSource::Constructor(
   RefPtr<const ffi::FileSource> raw;
   if (aIndex.WasPassed()) {
     raw = dont_AddRef(ffi::l10nfilesource_new_with_index(
-        &aName, &aLocales, &aPrePath, aIndex.Value().Elements(),
+        &aName, &aMetaSource, &aLocales, &aPrePath, aIndex.Value().Elements(),
         aIndex.Value().Length(), allowOverrides, &status));
   } else {
-    raw = dont_AddRef(ffi::l10nfilesource_new(&aName, &aLocales, &aPrePath,
-                                              allowOverrides, &status));
+    raw = dont_AddRef(ffi::l10nfilesource_new(
+        &aName, &aMetaSource, &aLocales, &aPrePath, allowOverrides, &status));
   }
 
   if (PopulateError(aRv, status)) {
@@ -51,8 +51,9 @@ already_AddRefed<L10nFileSource> L10nFileSource::Constructor(
 /* static */
 already_AddRefed<L10nFileSource> L10nFileSource::CreateMock(
     const GlobalObject& aGlobal, const nsACString& aName,
-    const nsTArray<nsCString>& aLocales, const nsACString& aPrePath,
-    const nsTArray<L10nFileSourceMockFile>& aFS, ErrorResult& aRv) {
+    const nsACString& aMetaSource, const nsTArray<nsCString>& aLocales,
+    const nsACString& aPrePath, const nsTArray<L10nFileSourceMockFile>& aFS,
+    ErrorResult& aRv) {
   nsTArray<ffi::L10nFileSourceMockFile> fs(aFS.Length());
   for (const auto& file : aFS) {
     auto f = fs.AppendElement();
@@ -64,7 +65,7 @@ already_AddRefed<L10nFileSource> L10nFileSource::CreateMock(
   ffi::L10nFileSourceStatus status;
 
   RefPtr<const ffi::FileSource> raw(dont_AddRef(ffi::l10nfilesource_new_mock(
-      &aName, &aLocales, &aPrePath, &fs, &status)));
+      &aName, &aMetaSource, &aLocales, &aPrePath, &fs, &status)));
 
   if (PopulateError(aRv, status)) {
     return nullptr;
@@ -79,6 +80,10 @@ JSObject* L10nFileSource::WrapObject(JSContext* aCx,
 
 void L10nFileSource::GetName(nsCString& aRetVal) {
   ffi::l10nfilesource_get_name(mRaw.get(), &aRetVal);
+}
+
+void L10nFileSource::GetMetaSource(nsCString& aRetVal) {
+  ffi::l10nfilesource_get_metasource(mRaw.get(), &aRetVal);
 }
 
 void L10nFileSource::GetLocales(nsTArray<nsCString>& aRetVal) {
