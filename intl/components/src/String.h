6685fa570db2278d418472d7301c57ed2a188b83
criss <ccozmuta@mozilla.com>
2021-09-22 23:33:48 +0300

    Backed out 2 changesets (bug 1719544) for causing bustages on gtest.h:1445:11. CLOSED TREE
    Backed out changeset 4ab330369412 (bug 1719544)
    Backed out changeset b5d8d32d8326 (bug 1719544)

--- intl/components/src/String.h
+++ intl/components/src/String.h
@@ -16,7 +16,6 @@
 #include "unicode/uchar.h"
 #include "unicode/unorm2.h"
 #include "unicode/ustring.h"
-#include "unicode/utext.h"
 #include "unicode/utypes.h"
 
 namespace mozilla::intl {
@@ -176,72 +175,6 @@ class String final {
   static bool IsCaseIgnorable(char32_t codePoint) {
     return u_hasBinaryProperty(static_cast<UChar32>(codePoint),
                                UCHAR_CASE_IGNORABLE);
-  }
-
-  /**
-   * Return the NFC pairwise composition of the two input characters, if any;
-   * returns 0 (which we know is not a composed char!) if none exists.
-   */
-  static char32_t ComposePairNFC(char32_t a, char32_t b) {
-    // unorm2_getNFCInstance returns a static instance that does not have to be
-    // released here. If it fails, we just return 0 (no composition) always.
-    static UErrorCode status = U_ZERO_ERROR;
-    static const UNormalizer2* normalizer = unorm2_getNFCInstance(&status);
-    if (U_FAILURE(status)) {
-      return 0;
-    }
-    UChar32 ch = unorm2_composePair(normalizer, static_cast<UChar32>(a),
-                                    static_cast<UChar32>(b));
-    return ch < 0 ? 0 : static_cast<char32_t>(ch);
-  }
-
-  /**
-   * Put the "raw" (single-level) canonical decomposition of the input char, if
-   * any, into the provided buffer. Canonical decomps are never more than two
-   * chars in length (although full normalization may result in longer output
-   * due to recursion).
-   * Returns the length of the decomposition (0 if none, else 1 or 2).
-   */
-  static int DecomposeRawNFD(char32_t ab, char32_t decomp[2]) {
-    // unorm2_getNFCInstance returns a static instance that does not have to be
-    // released here. If it fails, we just return 0 (no decomposition) always.
-    // Although we are using it to query for a decomposition, the mode of the
-    // Normalizer2 is irrelevant here, so we may as well use the same singleton
-    // instance as ComposePairNFC.
-    static UErrorCode status = U_ZERO_ERROR;
-    static const UNormalizer2* normalizer = unorm2_getNFCInstance(&status);
-    if (U_FAILURE(status)) {
-      return 0;
-    }
-
-    // Canonical decompositions are never more than two Unicode characters,
-    // or a maximum of 4 utf-16 code units.
-    const unsigned MAX_DECOMP_LENGTH = 4;
-    UErrorCode error = U_ZERO_ERROR;
-    UChar decompUtf16[MAX_DECOMP_LENGTH];
-    int32_t len = unorm2_getRawDecomposition(normalizer,
-                                             static_cast<UChar32>(ab),
-                                             decompUtf16, MAX_DECOMP_LENGTH,
-                                             &error);
-    if (U_FAILURE(error) || len < 0) {
-      return 0;
-    }
-    UText text = UTEXT_INITIALIZER;
-    utext_openUChars(&text, decompUtf16, len, &error);
-    MOZ_ASSERT(U_SUCCESS(error));
-    UChar32 ch = UTEXT_NEXT32(&text);
-    len = 0;
-    if (ch != U_SENTINEL) {
-      decomp[0] = static_cast<char32_t>(ch);
-      ++len;
-      ch = UTEXT_NEXT32(&text);
-      if (ch != U_SENTINEL) {
-        decomp[1] = static_cast<char32_t>(ch);
-        ++len;
-      }
-    }
-    utext_close(&text);
-    return len;
   }
 };
 
