cd01a4f0ece7e0c7aab05771623b38db0aab36e8
Byron Campen <docfaraday@gmail.com>
2021-09-23 17:30:20 +0000

    Bug 1731302: Add a "relay-tls" type to our mochitest harness. r=ng
    Also, allow nICEr to use loopback on this test, like the other relay tests.
    
    Depends on D126020
    
    Differential Revision: https://phabricator.services.mozilla.com/D126021

--- dom/media/webrtc/tests/mochitests/pc.js
+++ dom/media/webrtc/tests/mochitests/pc.js
@@ -2280,6 +2280,10 @@ PeerConnectionWrapper.prototype = {
       candidateType = "relay-tcp";
     }
 
+    if (lCand.relayProtocol === "tls" && candidateType === "relay") {
+      candidateType = "relay-tls";
+    }
+
     if (expectedLocalCandidateType === "srflx" && candidateType === "prflx") {
       // Be forgiving of prflx when expecting srflx, since that can happen due
       // to timing.
