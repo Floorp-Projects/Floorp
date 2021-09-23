eb735ac57e701c05a1deac60d563150c33a6a0e7
Butkovits Atila <abutkovits@mozilla.com>
2021-09-23 13:06:40 +0300

    Backed out changeset 53b1fa0faa6d (bug 1696251) for breaking the static-analysis integration. a=backout

--- testing/tps/mach_commands.py
+++ testing/tps/mach_commands.py
@@ -5,31 +5,35 @@
 from __future__ import absolute_import, print_function
 import os
 
-from mach.decorators import Command, CommandArgument
+from mach.decorators import Command, CommandArgument, CommandProvider
+from mozbuild.base import MachCommandBase
 from mozpack.copier import Jarrer
 from mozpack.files import FileFinder
 
 
-@Command("tps-build", category="testing", description="Build TPS add-on.")
-@CommandArgument("--dest", default=None, help="Where to write add-on.")
-def build(command_context, dest):
-    src = os.path.join(
-        command_context.topsrcdir, "services", "sync", "tps", "extensions", "tps"
-    )
-    dest = os.path.join(
-        dest or os.path.join(command_context.topobjdir, "services", "sync"),
-        "tps.xpi",
-    )
+@CommandProvider
+class MachCommands(MachCommandBase):
+    @Command("tps-build", category="testing", description="Build TPS add-on.")
+    @CommandArgument("--dest", default=None, help="Where to write add-on.")
+    def build(self, command_context, dest):
+        """TPS tests for Sync."""
+        src = os.path.join(
+            command_context.topsrcdir, "services", "sync", "tps", "extensions", "tps"
+        )
+        dest = os.path.join(
+            dest or os.path.join(command_context.topobjdir, "services", "sync"),
+            "tps.xpi",
+        )
 
-    if not os.path.exists(os.path.dirname(dest)):
-        os.makedirs(os.path.dirname(dest))
+        if not os.path.exists(os.path.dirname(dest)):
+            os.makedirs(os.path.dirname(dest))
 
-    if os.path.isfile(dest):
-        os.unlink(dest)
+        if os.path.isfile(dest):
+            os.unlink(dest)
 
-    jarrer = Jarrer()
-    for p, f in FileFinder(src).find("*"):
-        jarrer.add(p, f)
-    jarrer.copy(dest)
+        jarrer = Jarrer()
+        for p, f in FileFinder(src).find("*"):
+            jarrer.add(p, f)
+        jarrer.copy(dest)
 
-    print("Built TPS add-on as %s" % dest)
+        print("Built TPS add-on as %s" % dest)
