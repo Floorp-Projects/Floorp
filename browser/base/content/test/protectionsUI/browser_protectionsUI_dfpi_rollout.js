64764ef32ea223ead5b88463ec9262f83376da95
Johann Hofmann <jhofmann@mozilla.com>
2021-09-23 22:10:50 +0000

    Bug 1732271 - Add a rollout pref for setting dFPI as the default cookieBehavior. r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D126476

---
+++ browser/base/content/test/protectionsUI/browser_protectionsUI_dfpi_rollout.js
@@ -0,0 +1,31 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+const PREF_DFPI_ENABLED_BY_DEFAULT =
+  "privacy.restrict3rdpartystorage.rollout.enabledByDefault";
+const COOKIE_BEHAVIOR_PREF = "network.cookie.cookieBehavior";
+
+const defaultPrefs = Services.prefs.getDefaultBranch("");
+const previousDefaultCB = defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF);
+
+registerCleanupFunction(function() {
+  defaultPrefs.setIntPref(COOKIE_BEHAVIOR_PREF, previousDefaultCB);
+});
+
+// Tests that the dFPI rollout pref updates the default cookieBehavior to 5
+add_task(async function testdFPIRolloutPref() {
+  defaultPrefs.setIntPref(
+    COOKIE_BEHAVIOR_PREF,
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER
+  );
+  Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, false);
+  is(
+    defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER
+  );
+  Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, true);
+  is(
+    defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN
+  );
+});
