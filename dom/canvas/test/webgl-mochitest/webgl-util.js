983932760bb5eec1053cb3c0ea6f370bbeeaceb5
Jeff Gilbert <jgilbert@mozilla.com>
2021-09-23 00:53:32 +0000

    Bug 1731677 - Remove cruft from webgl-mochitest/*. r=gfx-reviewers,lsalzman
    * Remove test_webgl_conformance because we're always conformant now.
    * Remove test_webgl_color_buffer_float because we have coverage in CTS
      now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126139

--- dom/canvas/test/webgl-mochitest/webgl-util.js
+++ dom/canvas/test/webgl-mochitest/webgl-util.js
@@ -1,62 +1,6 @@
 WebGLUtil = (function() {
   // ---------------------------------------------------------------------------
-  // Error handling (for obvious failures, such as invalid element ids)
-
-  function defaultErrorFunc(str) {
-    console.log("Error: " + str);
-  }
-
-  var gErrorFunc = defaultErrorFunc;
-  function setErrorFunc(func) {
-    gErrorFunc = func;
-  }
-
-  function error(str) {
-    gErrorFunc(str);
-  }
-
-  // ---------------------------------------------------------------------------
-  // Warning handling (for failures that may be intentional)
-
-  function defaultWarningFunc(str) {
-    console.log("Warning: " + str);
-  }
-
-  var gWarningFunc = defaultWarningFunc;
-  function setWarningFunc(func) {
-    gWarningFunc = func;
-  }
-
-  function warning(str) {
-    gWarningFunc(str);
-  }
-
-  // ---------------------------------------------------------------------------
   // WebGL helpers
-
-  function getWebGL(canvasId, requireConformant, attributes) {
-    // `requireConformant` will default to falsey if it is not supplied.
-
-    var canvas = document.getElementById(canvasId);
-
-    var gl = null;
-    try {
-      gl = canvas.getContext("webgl", attributes);
-    } catch (e) {}
-
-    if (!gl && !requireConformant) {
-      try {
-        gl = canvas.getContext("experimental-webgl", attributes);
-      } catch (e) {}
-    }
-
-    if (!gl) {
-      error("WebGL context could not be retrieved from '" + canvasId + "'.");
-      return null;
-    }
-
-    return gl;
-  }
 
   function withWebGL2(canvasId, callback, onFinished) {
     var run = function() {
@@ -72,12 +16,6 @@ WebGLUtil = (function() {
         onFinished();
         return;
       }
-
-      function errorFunc(str) {
-        ok(false, "Error: " + str);
-      }
-      setErrorFunc(errorFunc);
-      setWarningFunc(errorFunc);
 
       callback(gl);
       onFinished();
@@ -96,29 +34,16 @@ WebGLUtil = (function() {
     }
   }
 
-  function getContentFromElem(elem) {
-    var str = "";
-    var k = elem.firstChild;
-    while (k) {
-      if (k.nodeType == 3) {
-        str += k.textContent;
-      }
-
-      k = k.nextSibling;
-    }
-
-    return str;
-  }
-
   // Returns a valid shader, or null on errors.
   function createShaderById(gl, id) {
     var elem = document.getElementById(id);
     if (!elem) {
-      error("Failed to create shader from non-existent id '" + id + "'.");
-      return null;
+      throw new Error(
+        "Failed to create shader from non-existent id '" + id + "'."
+      );
     }
 
-    var src = getContentFromElem(elem);
+    var src = elem.innerHTML.trim();
 
     var shader;
     if (elem.type == "x-shader/x-fragment") {
@@ -126,8 +51,9 @@ WebGLUtil = (function() {
     } else if (elem.type == "x-shader/x-vertex") {
       shader = gl.createShader(gl.VERTEX_SHADER);
     } else {
-      error("Bad MIME type for shader '" + id + "': " + elem.type + ".");
-      return null;
+      throw new Error(
+        "Bad MIME type for shader '" + id + "': " + elem.type + "."
+      );
     }
 
     gl.shaderSource(shader, src);
@@ -153,7 +79,7 @@ WebGLUtil = (function() {
       str += "\nShader program info log:\n" + gl.getProgramInfoLog(prog);
       str += "\n\nVert shader log:\n" + gl.getShaderInfoLog(vs);
       str += "\n\nFrag shader log:\n" + gl.getShaderInfoLog(fs);
-      warning(str);
+      console.error(str);
       return null;
     }
 
@@ -161,11 +87,8 @@ WebGLUtil = (function() {
   }
 
   return {
-    setErrorFunc,
-    setWarningFunc,
-
-    getWebGL,
     withWebGL2,
+
     createShaderById,
     createProgramByIds,
 
