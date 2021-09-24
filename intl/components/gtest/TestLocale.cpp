1a63a04aabeadbc127b6616371cd5c16fead1ffe
André Bargull <andre.bargull@gmail.com>
2021-09-24 10:22:46 +0000

    Bug 1719748 - Part 6: Add an enumeration over the list of available locales. r=platform-i18n-reviewers,gregtatum
    Differential Revision: https://phabricator.services.mozilla.com/D126492

---
+++ intl/components/gtest/TestLocale.cpp
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#include "gtest/gtest.h"
+
+#include "mozilla/intl/Locale.h"
+
+#include <stdint.h>
+#include <string_view>
+
+namespace mozilla::intl {
+
+TEST(IntlLocale, GetAvailableLocales)
+{
+  using namespace std::literals;
+
+  int32_t english = 0;
+  int32_t german = 0;
+  int32_t chinese = 0;
+
+  // Since this list is dependent on ICU, and may change between upgrades, only
+  // test a subset of the available locales.
+  for (const char* locale : Locale::GetAvailableLocales()) {
+    if (locale == "en"sv) {
+      english++;
+    } else if (locale == "de"sv) {
+      german++;
+    } else if (locale == "zh"sv) {
+      chinese++;
+    }
+  }
+
+  // Each locale should be found exactly once.
+  ASSERT_EQ(english, 1);
+  ASSERT_EQ(german, 1);
+  ASSERT_EQ(chinese, 1);
+}
+
+}  // namespace mozilla::intl
---
+++ intl/components/src/Locale.h
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef intl_components_Locale_h_
+#define intl_components_Locale_h_
+
+#include "unicode/uloc.h"
+
+#include "mozilla/intl/ICU4CGlue.h"
+
+namespace mozilla::intl {
+
+class Locale final {
+ public:
+  Locale() = delete;
+
+  /**
+   * Returns an iterator over all supported locales.
+   *
+   * The returned strings are ICU locale identifiers and NOT BCP 47 language
+   * tags.
+   *
+   * Also see <https://unicode-org.github.io/icu/userguide/locale>.
+   */
+  static auto GetAvailableLocales() {
+    return AvailableLocalesEnumeration<uloc_countAvailable,
+                                       uloc_getAvailable>();
+  }
+};
+
+}  // namespace mozilla::intl
+
+#endif
