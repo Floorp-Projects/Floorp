1d4970d0bce264d4a3f357896b640847259bb88e
Alexandre Poirot <poirot.alex@gmail.com>
2021-09-27 09:06:27 +0000

    Bug 1732253 - [devtools] Share code for all panel's reload tests. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D126466

--- devtools/client/framework/test/allocations/browser_allocations_reload_inspector.js
+++ devtools/client/framework/test/allocations/browser_allocations_reload_inspector.js
@@ -3,63 +3,12 @@
 
 "use strict";
 
-// Record allocations while reloading the page with the DevTools opened
+// Record allocations while reloading the page with the inspector opened
 
-const TEST_URL =
-  "http://example.com/browser/devtools/client/framework/test/allocations/reloaded-page.html";
+/* import-globals-from reload-test.js */
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/devtools/client/framework/test/allocations/reload-test.js",
+  this
+);
 
-const { require } = ChromeUtils.import("resource://devtools/shared/Loader.jsm");
-const { gDevTools } = require("devtools/client/framework/devtools");
-
-async function testScript(toolbox) {
-  const onTargetSwitched = toolbox.commands.targetCommand.once(
-    "switched-target"
-  );
-  const onReloaded = toolbox.getCurrentPanel().once("reloaded");
-
-  gBrowser.reloadTab(gBrowser.selectedTab);
-
-  if (
-    toolbox.commands.targetCommand.targetFront.targetForm
-      .followWindowGlobalLifeCycle
-  ) {
-    info("Wait for target switched");
-    await onTargetSwitched;
-  }
-
-  info("Wait for panel reload");
-  await onReloaded;
-
-  // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
-  await new Promise(resolve => setTimeout(resolve, 1000));
-}
-
-add_task(async function() {
-  const tab = await addTab(TEST_URL);
-  const toolbox = await gDevTools.showToolboxForTab(tab, {
-    toolId: "inspector",
-  });
-
-  // Run the test scenario first before recording in order to load all the
-  // modules. Otherwise they get reported as "still allocated" objects,
-  // whereas we do expect them to be kept in memory as they are loaded via
-  // the main DevTools loader, which keeps the module loaded until the
-  // shutdown of Firefox
-  await testScript(toolbox);
-
-  await startRecordingAllocations({
-    alsoRecordContentProcess: true,
-  });
-
-  // Now, run the test script. This time, we record this run.
-  for (let i = 0; i < 10; i++) {
-    await testScript(toolbox);
-  }
-
-  await stopRecordingAllocations("reload-inspector", {
-    alsoRecordContentProcess: true,
-  });
-
-  await toolbox.destroy();
-  gBrowser.removeTab(tab);
-});
+add_task(createPanelReloadTest("reload-inspector", "inspector"));
---
+++ devtools/client/framework/test/allocations/reload-test.js
@@ -0,0 +1,80 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+/* import-globals-from head.js */
+/* eslint no-unused-vars: [2, {"vars": "local"}] */
+
+/**
+ * Generate a test Task to record allocation when reloading a test page
+ * while having one particular DevTools panel opened
+ *
+ * @param String recordName
+ *        Name of the test recorded into PerfHerder/Talos database
+ * @param String toolId
+ *        ID of the panel to open
+ */
+function createPanelReloadTest(recordName, toolId) {
+  return async function panelReloadTest() {
+    const TEST_URL =
+      "http://example.com/browser/devtools/client/framework/test/allocations/reloaded-page.html";
+
+    async function testScript(toolbox) {
+      const onTargetSwitched = toolbox.commands.targetCommand.once(
+        "switched-target"
+      );
+      const onReloaded = toolbox.getCurrentPanel().once("reloaded");
+
+      gBrowser.reloadTab(gBrowser.selectedTab);
+
+      if (
+        toolbox.commands.targetCommand.targetFront.targetForm
+          .followWindowGlobalLifeCycle
+      ) {
+        info("Wait for target switched");
+        await onTargetSwitched;
+      }
+
+      info("Wait for panel reload");
+      await onReloaded;
+
+      // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+
+    const tab = await addTab(TEST_URL);
+
+    const { require } = ChromeUtils.import(
+      "resource://devtools/shared/Loader.jsm"
+    );
+    const { gDevTools } = require("devtools/client/framework/devtools");
+    const toolbox = await gDevTools.showToolboxForTab(tab, {
+      toolId,
+    });
+
+    // Run the test scenario first before recording in order to load all the
+    // modules. Otherwise they get reported as "still allocated" objects,
+    // whereas we do expect them to be kept in memory as they are loaded via
+    // the main DevTools loader, which keeps the module loaded until the
+    // shutdown of Firefox
+    await testScript(toolbox);
+
+    await startRecordingAllocations({
+      alsoRecordContentProcess: true,
+    });
+
+    // Now, run the test script. This time, we record this run.
+    for (let i = 0; i < 10; i++) {
+      await testScript(toolbox);
+    }
+
+    await stopRecordingAllocations(recordName, {
+      alsoRecordContentProcess: true,
+    });
+
+    await toolbox.destroy();
+    gBrowser.removeTab(tab);
+  };
+}
