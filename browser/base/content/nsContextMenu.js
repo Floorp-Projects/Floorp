53a01f5fb9319c7237a1fa38db55dd1db4598ebb
Emma Malysz <emalysz@mozilla.com>
2021-09-27 17:30:35 +0000

    Bug 1725430: disable screenshot shortcut for extension when screenshots.browser.component.enabled is true. r=emalysz,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D125850

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -1301,7 +1301,11 @@ class nsContextMenu {
     if (SCREENSHOT_BROWSER_COMPONENT) {
       Services.obs.notifyObservers(window, "menuitem-screenshot", true);
     } else {
-      Services.obs.notifyObservers(null, "menuitem-screenshot-extension", true);
+      Services.obs.notifyObservers(
+        null,
+        "menuitem-screenshot-extension",
+        "contextMenu"
+      );
     }
   }
 
