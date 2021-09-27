8030703a7ca9044fb2b945160e17673d6423f1b8
criss <ccozmuta@mozilla.com>
2021-09-28 00:32:38 +0300

    Backed out 10 changesets (bug 1712151, bug 1724279, bug 1730712, bug 1717051, bug 1723031, bug 1731145) for causing failures on test_yaml.py Backed out changeset 7f64d538701b (bug 1723031) Backed out changeset 394152994966 (bug 1723031) Backed out changeset 9bfeb01bcc9a (bug 1723031) Backed out changeset 3d283616a57d (bug 1730712) Backed out changeset bc677b409650 (bug 1724279) Backed out changeset 784c94c2f528 (bug 1723031) Backed out changeset 6e1bde40e3b4 (bug 1723031) Backed out changeset 7adf7e2136a3 (bug 1712151) Backed out changeset 2aef162b9a1b (bug 1717051) Backed out changeset 9beeb6d3d95b (bug 1731145)

---
+++ python/mozbuild/mozbuild/requirements.py
@@ -0,0 +1,144 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+
+
+THUNDERBIRD_PYPI_ERROR = """
+Thunderbird requirements definitions cannot include PyPI packages.
+""".strip()
+
+
+class PthSpecifier:
+    def __init__(self, path):
+        self.path = path
+
+
+class PypiSpecifier:
+    def __init__(self, package_name, version, full_specifier):
+        self.package_name = package_name
+        self.version = version
+        self.full_specifier = full_specifier
+
+
+class PypiOptionalSpecifier:
+    def __init__(self, repercussion, package_name, version, full_specifier):
+        self.repercussion = repercussion
+        self.package_name = package_name
+        self.version = version
+        self.full_specifier = full_specifier
+
+
+class MachEnvRequirements:
+    """Requirements associated with a "virtualenv_packages.txt" definition
+
+    Represents the dependencies of a virtualenv. The source files consist
+    of colon-delimited fields. The first field
+    specifies the action. The remaining fields are arguments to that
+    action. The following actions are supported:
+
+    pth -- Adds the path given as argument to "mach.pth" under
+        the virtualenv site packages directory.
+
+    pypi -- Fetch the package, plus dependencies, from PyPI.
+
+    pypi-optional -- Attempt to install the package and dependencies from PyPI.
+        Continue using the virtualenv, even if the package could not be installed.
+
+    packages.txt -- Denotes that the specified path is a child manifest. It
+        will be read and processed as if its contents were concatenated
+        into the manifest being read.
+
+    thunderbird-packages.txt -- Denotes a Thunderbird child manifest.
+        Thunderbird child manifests are only activated when working on Thunderbird,
+        and they can cannot have "pypi" or "pypi-optional" entries.
+    """
+
+    def __init__(self):
+        self.requirements_paths = []
+        self.pth_requirements = []
+        self.pypi_requirements = []
+        self.pypi_optional_requirements = []
+
+    @classmethod
+    def from_requirements_definition(
+        cls, topsrcdir, is_thunderbird, requirements_definition
+    ):
+        requirements = cls()
+        _parse_mach_env_requirements(
+            requirements, requirements_definition, topsrcdir, is_thunderbird
+        )
+        return requirements
+
+
+def _parse_mach_env_requirements(
+    requirements_output, root_requirements_path, topsrcdir, is_thunderbird
+):
+    def _parse_requirements_line(line, is_thunderbird_packages_txt):
+        line = line.strip()
+        if not line or line.startswith("#"):
+            return
+
+        action, params = line.rstrip().split(":", maxsplit=1)
+        if action == "pth":
+            requirements_output.pth_requirements.append(PthSpecifier(params))
+        elif action == "packages.txt":
+            _parse_requirements_definition_file(
+                os.path.join(topsrcdir, params),
+                is_thunderbird_packages_txt,
+            )
+        elif action == "pypi":
+            if is_thunderbird_packages_txt:
+                raise Exception(THUNDERBIRD_PYPI_ERROR)
+
+            package_name, version = _parse_package_specifier(params)
+            requirements_output.pypi_requirements.append(
+                PypiSpecifier(package_name, version, params)
+            )
+        elif action == "pypi-optional":
+            if is_thunderbird_packages_txt:
+                raise Exception(THUNDERBIRD_PYPI_ERROR)
+
+            if len(params.split(":", maxsplit=1)) != 2:
+                raise Exception(
+                    "Expected pypi-optional package to have a repercussion "
+                    'description in the format "package:fallback explanation", '
+                    'found "{}"'.format(params)
+                )
+            package, repercussion = params.split(":")
+            package_name, version = _parse_package_specifier(package)
+            requirements_output.pypi_optional_requirements.append(
+                PypiOptionalSpecifier(repercussion, package_name, version, package)
+            )
+        elif action == "thunderbird-packages.txt":
+            if is_thunderbird:
+                _parse_requirements_definition_file(
+                    os.path.join(topsrcdir, params), is_thunderbird_packages_txt=True
+                )
+        else:
+            raise Exception("Unknown requirements definition action: %s" % action)
+
+    def _parse_requirements_definition_file(
+        requirements_path, is_thunderbird_packages_txt
+    ):
+        """Parse requirements file into list of requirements"""
+        assert os.path.isfile(requirements_path)
+        requirements_output.requirements_paths.append(requirements_path)
+
+        with open(requirements_path, "r") as requirements_file:
+            lines = [line for line in requirements_file]
+
+        for line in lines:
+            _parse_requirements_line(line, is_thunderbird_packages_txt)
+
+    _parse_requirements_definition_file(root_requirements_path, False)
+
+
+def _parse_package_specifier(specifier):
+    if len(specifier.split("==")) != 2:
+        raise Exception(
+            "Expected pypi package version to be pinned in the "
+            'format "package==version", found "{}"'.format(specifier)
+        )
+    return specifier.split("==")
