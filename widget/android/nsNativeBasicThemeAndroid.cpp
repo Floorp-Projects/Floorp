8696d678e8f2535375e0917eb58928ae5c5fa87f
Emilio Cobos Álvarez <emilio@crisal.io>
2021-09-27 13:56:49 +0000

    Bug 1730867 - Use native scrollbars on Android. r=mstange
    We only draw a scrollbar thumb, so it's easy enough (fair amount of
    boilerplate, though).
    
    For now I've done the trivial thing (keep painting a round thumb without
    other fancy things), as that's what we were doing, but in the future we
    should consider drawing a stroke around the thumb like cocoa does to add
    more contrast.
    
    This patch does change behavior in some cases. For example dark websites
    now get a lighter, easier to distinguish scrollbar, thanks to the
    scrollbar darkening code in nsNativeBasicTheme.cpp.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125826

--- widget/android/nsNativeBasicThemeAndroid.cpp
+++ widget/android/nsNativeBasicThemeAndroid.cpp
@@ -6,6 +6,84 @@
 #include "nsNativeBasicThemeAndroid.h"
 
 #include "mozilla/ClearOnShutdown.h"
+#include "nsStyleConsts.h"
+#include "nsIFrame.h"
+
+auto nsNativeBasicThemeAndroid::GetScrollbarSizes(nsPresContext* aPresContext,
+                                                  StyleScrollbarWidth aWidth,
+                                                  Overlay aOverlay)
+    -> ScrollbarSizes {
+  // We force auto-width scrollbars because scrollbars on android are already
+  // thin enough.
+  return nsNativeBasicTheme::GetScrollbarSizes(
+      aPresContext, StyleScrollbarWidth::Auto, aOverlay);
+}
+
+NS_IMETHODIMP
+nsNativeBasicThemeAndroid::GetMinimumWidgetSize(
+    nsPresContext* aPresContext, nsIFrame* aFrame, StyleAppearance aAppearance,
+    mozilla::LayoutDeviceIntSize* aResult, bool* aIsOverridable) {
+  if (!IsWidgetScrollbarPart(aAppearance)) {
+    return nsNativeBasicTheme::GetMinimumWidgetSize(
+        aPresContext, aFrame, aAppearance, aResult, aIsOverridable);
+  }
+
+  auto sizes =
+      GetScrollbarSizes(aPresContext, StyleScrollbarWidth::Auto, Overlay::Yes);
+  aResult->SizeTo(sizes.mHorizontal, sizes.mHorizontal);
+  MOZ_ASSERT(sizes.mHorizontal == sizes.mVertical);
+
+  *aIsOverridable = true;
+  return NS_OK;
+}
+
+template <typename PaintBackendData>
+void nsNativeBasicThemeAndroid::DoPaintScrollbarThumb(
+    PaintBackendData& aPaintData, const LayoutDeviceRect& aRect,
+    bool aHorizontal, nsIFrame* aFrame, const ComputedStyle& aStyle,
+    const EventStates& aElementState, const EventStates& aDocumentState,
+    UseSystemColors aUseSystemColors, DPIRatio aDpiRatio) {
+  // TODO(emilio): Maybe do like macOS and draw a stroke?
+  const auto color = ComputeScrollbarThumbColor(
+      aFrame, aStyle, aElementState, aDocumentState, aUseSystemColors);
+
+  // Draw the thumb rect centered in the scrollbar.
+  LayoutDeviceRect thumbRect(aRect);
+  if (aHorizontal) {
+    thumbRect.height *= 0.5f;
+    thumbRect.y += thumbRect.height * 0.5f;
+  } else {
+    thumbRect.width *= 0.5f;
+    thumbRect.x += thumbRect.width * 0.5f;
+  }
+
+  const LayoutDeviceCoord radius =
+      (aHorizontal ? thumbRect.height : thumbRect.width) / 2.0f;
+  PaintRoundedRectWithRadius(aPaintData, thumbRect, color,
+                             sRGBColor::White(0.0f), 0.0f, radius / aDpiRatio,
+                             aDpiRatio);
+}
+
+bool nsNativeBasicThemeAndroid::PaintScrollbarThumb(
+    DrawTarget& aDt, const LayoutDeviceRect& aRect, bool aHorizontal,
+    nsIFrame* aFrame, const ComputedStyle& aStyle,
+    const EventStates& aElementState, const EventStates& aDocumentState,
+    UseSystemColors aUseSystemColors, DPIRatio aDpiRatio) {
+  DoPaintScrollbarThumb(aDt, aRect, aHorizontal, aFrame, aStyle, aElementState,
+                        aDocumentState, aUseSystemColors, aDpiRatio);
+  return true;
+}
+
+bool nsNativeBasicThemeAndroid::PaintScrollbarThumb(
+    WebRenderBackendData& aWrData, const LayoutDeviceRect& aRect,
+    bool aHorizontal, nsIFrame* aFrame, const ComputedStyle& aStyle,
+    const EventStates& aElementState, const EventStates& aDocumentState,
+    UseSystemColors aUseSystemColors, DPIRatio aDpiRatio) {
+  DoPaintScrollbarThumb(aWrData, aRect, aHorizontal, aFrame, aStyle,
+                        aElementState, aDocumentState, aUseSystemColors,
+                        aDpiRatio);
+  return true;
+}
 
 already_AddRefed<nsITheme> do_GetBasicNativeThemeDoNotUseDirectly() {
   static mozilla::StaticRefPtr<nsITheme> gInstance;
