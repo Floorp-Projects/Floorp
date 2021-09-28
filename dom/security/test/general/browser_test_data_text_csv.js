6d57162ed464bd6bc1c00c622b718060eb109dd6
Katherine Patenio <kpatenio@mozilla.com>
2021-09-28 00:45:35 +0000

    Bug 1724319 - fix browser_test_data_text_csv when downloads pref is true. r=mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D125471

--- dom/security/test/general/browser_test_data_text_csv.js
+++ dom/security/test/general/browser_test_data_text_csv.js
@@ -27,10 +27,22 @@ function addWindowListener(aURL, aCallback) {
   });
 }
 
-add_task(async function() {
+function promisePanelOpened() {
+  if (DownloadsPanel.panel && DownloadsPanel.panel.state == "open") {
+    return Promise.resolve();
+  }
+  return BrowserTestUtils.waitForEvent(DownloadsPanel.panel, "popupshown");
+}
+
+// Note: remove this test once browser.download.improvements_to_download_panel is removed.
+add_task(async function test_with_downloads_improvement_pref_disabled() {
   await SpecialPowers.pushPrefEnv({
-    set: [["security.data_uri.block_toplevel_data_uri_navigations", true]],
+    set: [
+      ["security.data_uri.block_toplevel_data_uri_navigations", true],
+      ["browser.download.improvements_to_download_panel", false],
+    ],
   });
+
   let windowPromise = addWindowListener(
     "chrome://mozapps/content/downloads/unknownContentType.xhtml"
   );
@@ -44,4 +56,52 @@ add_task(async function() {
     "file name of download should match"
   );
   win.close();
+});
+
+add_task(async function test_with_downloads_improvement_pref_enabled() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["security.data_uri.block_toplevel_data_uri_navigations", true],
+      ["browser.download.improvements_to_download_panel", true],
+    ],
+  });
+  let downloadsPanelPromise = promisePanelOpened();
+  let downloadsPromise = Downloads.getList(Downloads.PUBLIC);
+  let expectedValue = "text/csv;foo,bar,foobar";
+
+  info("Loading URI for pref enabled");
+  BrowserTestUtils.loadURI(gBrowser, kTestURI);
+  info("Waiting for downloads panel to open");
+  await downloadsPanelPromise;
+  info("Getting downloads info after opening downloads panel");
+  let downloadList = await downloadsPromise;
+
+  is(DownloadsPanel.isPanelShowing, true, "DownloadsPanel should be open.");
+  is(
+    downloadList._downloads.length,
+    1,
+    "File should be successfully downloaded."
+  );
+
+  let [download] = downloadList._downloads;
+  is(download.contentType, "text/csv", "File contentType should be correct.");
+  is(
+    download.source.url,
+    `data:${expectedValue}`,
+    "File name should be correct."
+  );
+
+  info("Cleaning up downloads");
+  try {
+    if (Services.appinfo.OS === "WINNT") {
+      // We need to make the file writable to delete it on Windows.
+      await IOUtils.setPermissions(download.target.path, 0o600);
+    }
+    await IOUtils.remove(download.target.path);
+  } catch (ex) {
+    info("The file " + download.target.path + " is not removed, " + ex);
+  }
+
+  await downloadList.remove(download);
+  await download.finalize();
 });
