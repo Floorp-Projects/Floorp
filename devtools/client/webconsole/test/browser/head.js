6a660b7ee02fda49d7bdc8c4bb5715a87fb8ac99
Julian Descottes <jdescottes@mozilla.com>
2021-09-28 13:36:47 +0000

    Bug 1732748 - [devtools] Clear cookies in webconsole mochitest cleanup r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D126772

--- devtools/client/webconsole/test/browser/head.js
+++ devtools/client/webconsole/test/browser/head.js
@@ -48,6 +48,10 @@ const GA_PARAMS = `?${new URLSearchParams({
 const wcActions = require("devtools/client/webconsole/actions/index");
 
 registerCleanupFunction(async function() {
+  // Reset all cookies, tests loading sjs_slow-response-test-server.sjs will
+  // set a foo cookie which might have side effects on other tests.
+  Services.cookies.removeAll();
+
   Services.prefs.clearUserPref("devtools.webconsole.ui.filterbar");
 
   // Reset all filter prefs between tests. First flushPrefEnv in case one of the
