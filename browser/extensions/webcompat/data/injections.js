b39921c8abc2e839ba0537b46fbf9c266cda632d
Thomas Wisniewski <twisniewski@mozilla.com>
2021-09-28 17:47:04 +0000

    Bug 1720140 - Ship v27.0.0 of the WebCompat System Addon; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D126816

--- browser/extensions/webcompat/data/injections.js
+++ browser/extensions/webcompat/data/injections.js
@@ -247,20 +247,6 @@ const AVAILABLE_INJECTIONS = [
     },
   },
   {
-    id: "bug1645064",
-    platform: "desktop",
-    domain: "s-kanava.fi",
-    bug: "1645064",
-    contentScripts: {
-      matches: ["https://www.s-kanava.fi/*"],
-      css: [
-        {
-          file: "injections/css/bug1645064-s-kanava.fi-invisible-charts.css",
-        },
-      ],
-    },
-  },
-  {
     id: "bug1651917",
     platform: "android",
     domain: "teletrader.com",
@@ -471,6 +457,24 @@ const AVAILABLE_INJECTIONS = [
           file: "injections/css/bug1719870-lcbo.com-table-clearfix.css",
         },
       ],
+    },
+  },
+  {
+    id: "bug1722955",
+    platform: "android",
+    domain: "frontgate.com",
+    bug: "1722955",
+    contentScripts: {
+      matches: ["*://*.frontgate.com/*"],
+      js: [
+        {
+          file: "lib/ua_helpers.js",
+        },
+        {
+          file: "injections/js/bug1722955-frontgate.com-ua-override.js",
+        },
+      ],
+      allFrames: true,
     },
   },
   {
