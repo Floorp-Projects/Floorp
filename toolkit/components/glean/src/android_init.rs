e32eef881eaac7e6bd487c93bfe5da821796db18
Jan-Erik Rediger <jrediger@mozilla.com>
2021-09-28 17:19:14 +0000

    Bug 1732859 - Force re-export of Glean symbols in C++. r=chutten
    We previously tried to do this from Rust,
    but that seems to be unreliable and currently only works for x86{,_64}.
    
    The same trick however works from the C++ side: we reference one symbol
    (and re-export it) and all the other symbols show up as well.
    Now all `libxul.so` files build for Android targets export the `glean_*`
    symbols, which are used by the Glean Android SDK to call into Glean.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126777

--- toolkit/components/glean/src/android_init.rs
+++ toolkit/components/glean/src/android_init.rs
@@ -8,23 +8,6 @@ use nsstring::nsACString;
 // Needed for re-export.
 pub use glean_ffi;
 
-/// Workaround to force a re-export of the `no_mangle` symbols from `glean_ffi`
-///
-/// Due to how linking works and hides symbols the symbols from `glean_ffi` might not be
-/// re-exported and thus not usable.
-/// By forcing use of _at least one_ symbol in an exported function the functions will also be
-/// rexported.
-/// This is only required for debug builds (and `debug_assertions` is the closest thing we have to
-/// check that).
-/// In release builds we rely on LTO builds to take care of it.
-/// Our tests should ensure this actually happens.
-///
-/// See https://github.com/rust-lang/rust/issues/50007
-#[no_mangle]
-pub unsafe extern "C" fn _fog_force_reexport_donotcall() {
-    glean_ffi::glean_enable_logging();
-}
-
 /// On Android initialize the bare minimum of FOG.
 ///
 /// This does not initialize Glean, which should be handled by the bundling application.
