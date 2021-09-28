f43531930ba1c71cd17e889fd9155d05dc5d68f1
Mike Hommey <mh+mozilla@glandium.org>
2021-09-28 09:08:35 +0000

    Bug 1732824 - Update wasi-sdk to the latest trunk. r=firefox-build-system-reviewers,andi
    Differential Revision: https://phabricator.services.mozilla.com/D126749

--- taskcluster/scripts/misc/build-compiler-rt-wasi.sh
+++ taskcluster/scripts/misc/build-compiler-rt-wasi.sh
@@ -7,23 +7,6 @@ dir=${artifact%.tar.*}
 cd $MOZ_FETCHES_DIR/wasi-sdk
 LLVM_PROJ_DIR=$MOZ_FETCHES_DIR/llvm-project
 
-# https://github.com/WebAssembly/wasi-sdk/pull/189
-patch -p1 <<'EOF'
-diff --git a/Makefile b/Makefile
-index bde9936..b1f24fe 100644
---- a/Makefile
-+++ b/Makefile
-@@ -91,7 +91,7 @@ build/wasi-libc.BUILT: build/llvm.BUILT
- 		SYSROOT=$(BUILD_PREFIX)/share/wasi-sysroot
- 	touch build/wasi-libc.BUILT
- 
--build/compiler-rt.BUILT: build/llvm.BUILT
-+build/compiler-rt.BUILT: build/llvm.BUILT build/wasi-libc.BUILT
- 	# Do the build, and install it.
- 	mkdir -p build/compiler-rt
- 	cd build/compiler-rt && cmake -G Ninja \
-EOF
-
 # Build compiler-rt
 make \
   LLVM_PROJ_DIR=$LLVM_PROJ_DIR \
