5a33fc98dfde5619d3c6774b3ff3d38c936b692b
Mitchell Hentges <mhentges@mozilla.com>
2021-09-28 14:59:28 +0000

    Bug 1717051: Reuse "requirements" logic in mach_initialize r=ahal
    Rather than re-implementing it as `search_path()`, use the existing
    `MachEnvRequirements` tool to parse `mach_virtualenv_requirements.txt`
    
    Differential Revision: https://phabricator.services.mozilla.com/D126280

--- python/mozbuild/mozbuild/test/test_vendor.py
+++ python/mozbuild/mozbuild/test/test_vendor.py
@@ -37,6 +37,13 @@ def test_up_to_date_vendor():
         work_vendored = os.path.join(work_dir, "third_party", "python")
         shutil.copytree(existing_vendored, work_vendored)
 
+        # Copy "mach" module so that `VirtualenvManager` can populate itself.
+        # This is needed because "topsrcdir" is used in this test both for determining
+        # import paths and for acting as a "work dir".
+        existing_mach = os.path.join(topsrcdir, "python", "mach")
+        work_mach = os.path.join(work_dir, "python", "mach")
+        shutil.copytree(existing_mach, work_mach)
+
         # Run the vendoring process
         vendor = VendorPython(
             work_dir, None, Mock(), topobjdir=os.path.join(work_dir, "obj")
