e912fb246646237cfe4192f95c76caa9cac8cf98
Alexandru Michis <malexandru@mozilla.com>
2021-09-30 17:38:15 +0300

    Backed out changeset a4ebb978f792 (bug 1732674) for causing layout crashes (bug 1733047). a=backout

--- layout/generic/nsLineBox.h
+++ layout/generic/nsLineBox.h
@@ -1634,83 +1634,55 @@ nsLineList_const_reverse_iterator::operator=(
 
 class nsLineIterator final : public nsILineIterator {
  public:
-  nsLineIterator(const nsLineList& aLines, bool aRightToLeft)
-      : mLines(aLines), mRightToLeft(aRightToLeft) {
-    mIter = mLines.begin();
-    // If the list is empty, set mIndex to -1 so that we know the "current
-    // position" is invalid; GetLineAt() will return nullptr, and NextLine()
-    // will never advance the index or iterator from here.
-    if (mIter == mLines.end()) {
-      mIndex = -1;
-    }
-  }
-  ~nsLineIterator() = default;
+  nsLineIterator(nsLineList& aLines, bool aRightToLeft);
+  ~nsLineIterator();
 
-  void DisposeLineIterator() final { delete this; }
+  virtual void DisposeLineIterator() override;
 
-  int32_t GetNumLines() const final {
-    if (mNumLines < 0) {
-      mNumLines = mLines.size();  // This is O(N) in number of lines!
-    }
-    return mNumLines;
-  }
+  virtual int32_t GetNumLines() const override;
+  virtual bool GetDirection() override;
 
-  bool GetDirection() final { return mRightToLeft; }
-
-  // Note that this updates the iterator's current position!
-  mozilla::Result<LineInfo, nsresult> GetLine(int32_t aLineNumber) final;
-
-  int32_t FindLineContaining(nsIFrame* aFrame, int32_t aStartLine = 0) final;
-
+  mozilla::Result<LineInfo, nsresult> GetLine(
+      int32_t aLineNumber) const override;
+  virtual int32_t FindLineContaining(nsIFrame* aFrame,
+                                     int32_t aStartLine = 0) override;
   NS_IMETHOD FindFrameAt(int32_t aLineNumber, nsPoint aPos,
                          nsIFrame** aFrameFound, bool* aPosIsBeforeFirstFrame,
-                         bool* aPosIsAfterLastFrame) final;
+                         bool* aPosIsAfterLastFrame) const override;
 
   NS_IMETHOD CheckLineOrder(int32_t aLine, bool* aIsReordered,
                             nsIFrame** aFirstVisual,
-                            nsIFrame** aLastVisual) final;
+                            nsIFrame** aLastVisual) override;
 
  private:
   nsLineIterator() = delete;
   nsLineIterator(const nsLineIterator& aOther) = delete;
 
-  const nsLineBox* NextLine() {
-    if (mIter == mLines.end()) {
+  nsLineBox* PrevLine() {
+    if (0 == mIndex) {
       return nullptr;
     }
-    ++mIter;
-    ++mIndex;
-    return mIter.get();
+    return mLines[--mIndex];
   }
 
-  // Note that this updates the iterator's current position!
-  const nsLineBox* GetLineAt(int32_t aIndex) {
-    MOZ_ASSERT(aIndex >= 0, "invalid line index");
-    if (mIndex == aIndex) {
-      return mIter.get();
-    }
-    if (aIndex < 0) {
+  nsLineBox* NextLine() {
+    if (mIndex >= mNumLines - 1) {
       return nullptr;
     }
-    while (mIndex > aIndex) {
-      --mIter;
-      --mIndex;
-    }
-    while (mIndex < aIndex) {
-      if (mIter == mLines.end()) {
-        return nullptr;
-      }
-      ++mIter;
-      ++mIndex;
-    }
-    return mIter.get();
+    return mLines[++mIndex];
   }
 
-  const nsLineList& mLines;
-  nsLineList_const_iterator mIter;
-  int32_t mIndex = 0;
-  mutable int32_t mNumLines = -1;
-  const bool mRightToLeft;
+  nsLineBox* LineAt(int32_t aIndex) {
+    if ((aIndex < 0) || (aIndex >= mNumLines)) {
+      return nullptr;
+    }
+    return mLines[aIndex];
+  }
+
+  nsLineBox** mLines;
+  int32_t mIndex;
+  int32_t mNumLines;
+  bool mRightToLeft;
 };
 
 #endif /* nsLineBox_h___ */
