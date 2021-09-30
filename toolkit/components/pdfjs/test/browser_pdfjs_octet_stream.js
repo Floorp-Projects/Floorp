eeccc11c0bd68859f8fddccd522892e4740112a4
Sam Foster <sfoster@mozilla.com>
2021-09-30 22:35:15 +0000

    Bug 1724319 - Fix pdfjs component tests for when new download changes are enabled. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D127009

--- toolkit/components/pdfjs/test/browser_pdfjs_octet_stream.js
+++ toolkit/components/pdfjs/test/browser_pdfjs_octet_stream.js
@@ -46,11 +46,16 @@ add_task(async function test_octet_stream_opens_pdfjs() {
 });
 
 /**
- * Check that if the octet-stream thing is in a frame, we don't load it inside
- * PDF.js
+ * Check that when the improvements_to_download_panel pref is false and
+ * if the octet-stream thing is in a frame, we don't load it inside PDF.js
  */
 add_task(async function test_octet_stream_in_frame_downloads() {
-  await SpecialPowers.pushPrefEnv({ set: [["pdfjs.handleOctetStream", true]] });
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["pdfjs.handleOctetStream", true],
+      ["browser.download.improvements_to_download_panel", false],
+    ],
+  });
 
   let handlerInfo = gMIMEService.getFromTypeAndExtension(
     "application/pdf",
@@ -79,3 +84,115 @@ add_task(async function test_octet_stream_in_frame_downloads() {
     }
   );
 });
+
+/**
+ * Check that when the improvements_to_download_panel pref is true and
+ * if the octet-stream thing is in a frame, we don't load it inside PDF.js
+ */
+add_task(
+  async function test_octet_stream_in_frame_downloads_improvements_to_download_panel() {
+    await SpecialPowers.pushPrefEnv({
+      set: [
+        ["pdfjs.handleOctetStream", true],
+        ["browser.download.improvements_to_download_panel", true],
+      ],
+    });
+
+    let handlerInfo = gMIMEService.getFromTypeAndExtension(
+      "application/pdf",
+      "pdf"
+    );
+
+    // Make sure pdf.js is the default handler.
+    is(
+      handlerInfo.alwaysAskBeforeHandling,
+      false,
+      "pdf handler defaults to always-ask is false"
+    );
+    is(
+      handlerInfo.preferredAction,
+      Ci.nsIHandlerInfo.handleInternally,
+      "pdf handler defaults to internal"
+    );
+
+    let downloadsPanelPromise = BrowserTestUtils.waitForEvent(
+      DownloadsPanel.panel,
+      "popupshown"
+    );
+
+    // Once downloaded, the PDF will be opened as a file:// URI in a new tab
+    let previewTabPromise = BrowserTestUtils.waitForNewTab(
+      gBrowser,
+      url => {
+        let uri = NetUtil.newURI(url);
+        return uri.scheme == "file" && uri.spec.endsWith(".pdf");
+      },
+      false, // dont wait for load
+      true // any tab, not just the next one
+    );
+
+    await BrowserTestUtils.withNewTab(
+      { gBrowser, url: `data:text/html,<iframe src='${PDF_URL}'>` },
+      async function(newTabBrowser) {
+        // wait until downloadsPanel opens before continuing with test
+        info("Waiting for download panel to open");
+        await downloadsPanelPromise;
+        is(
+          DownloadsPanel.panel.state,
+          "open",
+          "Check the download panel state is 'open'"
+        );
+        let downloadList = await Downloads.getList(Downloads.PUBLIC);
+        let [download] = downloadList._downloads;
+
+        // Verify the downloaded PDF opened in a new tab,
+        // with its download file URI
+        info("Waiting for preview tab");
+        let previewTab = await previewTabPromise;
+        ok(previewTab, "PDF opened in a new tab");
+
+        is(
+          DownloadsPanel.isPanelShowing,
+          true,
+          "DownloadsPanel should be open."
+        );
+        is(
+          downloadList._downloads.length,
+          1,
+          "File should be successfully downloaded."
+        );
+
+        await BrowserTestUtils.removeTab(previewTab);
+
+        info("cleaning up downloads");
+        try {
+          if (Services.appinfo.OS === "WINNT") {
+            // We need to make the file writable to delete it on Windows.
+            await IOUtils.setPermissions(download.target.path, 0o600);
+          }
+          await IOUtils.remove(download.target.path);
+        } catch (error) {
+          info(
+            "The file " + download.target.path + " is not removed, " + error
+          );
+        }
+        await downloadList.remove(download);
+        await download.finalize();
+
+        if (DownloadsPanel.panel.state !== "closed") {
+          let hiddenPromise = BrowserTestUtils.waitForEvent(
+            DownloadsPanel.panel,
+            "popuphidden"
+          );
+          DownloadsPanel.hidePanel();
+          await hiddenPromise;
+        }
+        is(
+          DownloadsPanel.panel.state,
+          "closed",
+          "Check that the download panel is closed"
+        );
+      }
+    );
+  }
+);
