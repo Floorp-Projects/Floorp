1c0f7ffba9be9ba4ccb593a6eaec8d5a3f28eb2c
Jan-Erik Rediger <janerik@fnordig.de>
2021-09-30 10:53:21 +0200

    [components] Update to Glean v41.1.1

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -31,7 +31,7 @@ object Versions {
 
     const val mozilla_appservices = "85.2.0"
 
-    const val mozilla_glean = "41.1.0"
+    const val mozilla_glean = "41.1.1"
 
     const val material = "1.2.1"
 
