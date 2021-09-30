500591be65ede0f4a37d4bae144bfca7b4bb8d8c
Sam Foster <sfoster@mozilla.com>
2021-09-30 22:35:15 +0000

    Bug 1724319 - Fix pdfjs component tests for when new download changes are enabled. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D127009

--- toolkit/components/pdfjs/test/browser_pdfjs_savedialog.js
+++ toolkit/components/pdfjs/test/browser_pdfjs_savedialog.js
@@ -5,8 +5,15 @@ const RELATIVE_DIR = "toolkit/components/pdfjs/test/";
 const TESTROOT = "http://example.com/browser/" + RELATIVE_DIR;
 
 function test() {
+  // When the download panel improvements pref is true, we expect the PDF to be simply downloaded
+  // So it only makes sense to run this test with the pref set false
+  Services.prefs.setBoolPref(
+    "browser.download.improvements_to_download_panel",
+    false
+  );
   var oldAction = changeMimeHandler(Ci.nsIHandlerInfo.useSystemDefault, true);
   var tab = BrowserTestUtils.addTab(gBrowser, TESTROOT + "file_pdfjs_test.pdf");
+
   // Test: "Open with" dialog comes up when pdf.js is not selected as the default
   // handler.
   addWindowListener(
@@ -16,6 +23,9 @@ function test() {
 
   waitForExplicitFinish();
   registerCleanupFunction(function() {
+    Services.prefs.clearUserPref(
+      "browser.download.improvements_to_download_panel"
+    );
     changeMimeHandler(oldAction[0], oldAction[1]);
     gBrowser.removeTab(tab);
   });
