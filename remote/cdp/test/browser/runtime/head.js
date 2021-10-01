c9191f1092054f7d45a46815cf76a500ab4050e2
Henrik Skupin <mail@hskupin.info>
2021-10-01 18:49:28 +0000

    Bug 1733463 - [remote] Use "window.origin" for context's origin in "Runtime.executionContextCreated". r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D127243

--- remote/cdp/test/browser/runtime/head.js
+++ remote/cdp/test/browser/runtime/head.js
@@ -10,7 +10,8 @@ Services.scriptloader.loadSubScript(
   this
 );
 
-const BASE_PATH = "https://example.com/browser/remote/cdp/test/browser/runtime";
+const BASE_ORIGIN = "https://example.com";
+const BASE_PATH = `${BASE_ORIGIN}/browser/remote/cdp/test/browser/runtime`;
 const FRAMESET_SINGLE_URL = `${BASE_PATH}/doc_frameset_single.html`;
 const PAGE_FRAME_URL = `${BASE_PATH}/doc_frame.html`;
 const PAGE_URL = `${BASE_PATH}/doc_empty.html`;
