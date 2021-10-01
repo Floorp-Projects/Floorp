4ae245545249fcb90a43fa752bd47f3477bc41b3
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-10-01 08:19:29 +0000

    Bug 1732690 - [devtools] Fix GeometryHighlighter in iframes. r=jdescottes.
    The highlighter wasn't working on remote iframe as we were trying to retrieve it
    from the top-level inspector front.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126807

--- devtools/client/inspector/shared/highlighters-overlay.js
+++ devtools/client/inspector/shared/highlighters-overlay.js
@@ -1273,7 +1273,10 @@ class HighlightersOverlay {
    *        THe NodeFront of the element to highlight.
    */
   async showGeometryEditor(node) {
-    const highlighter = await this._getHighlighter("GeometryEditorHighlighter");
+    const highlighter = await this._getHighlighterTypeForNode(
+      "GeometryEditorHighlighter",
+      node
+    );
     if (!highlighter) {
       return;
     }
@@ -1291,14 +1294,19 @@ class HighlightersOverlay {
    * Hide the geometry editor highlighter.
    */
   async hideGeometryEditor() {
-    if (
-      !this.geometryEditorHighlighterShown ||
-      !this.highlighters.GeometryEditorHighlighter
-    ) {
+    if (!this.geometryEditorHighlighterShown) {
       return;
     }
 
-    await this.highlighters.GeometryEditorHighlighter.hide();
+    const highlighter = this.geometryEditorHighlighterShown.inspectorFront.getKnownHighlighter(
+      "GeometryEditorHighlighter"
+    );
+
+    if (!highlighter) {
+      return;
+    }
+
+    await highlighter.hide();
 
     this.emit("geometry-editor-highlighter-hidden");
     this.geometryEditorHighlighterShown = null;
