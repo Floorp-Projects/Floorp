dcc3c688bf0841dd9a061c46fc0fedf21e06a5af
Sarah Clements <sclements313@gmail.com>
2021-10-01 12:31:10 +0100

    [focus] Add nightly support (https://github.com/mozilla-mobile/focus-android/pull/5496)
    * Add nightly support
    
    * Switch cron tasks to taskgraph in taskcluster.yml
    
    * update target tasks
    don't run signing on every push or pr
    
    * switch nighty target task to detect attribute instead of label
    
    * rebase fix
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- mobile/android/focus-android/taskcluster/focus_android_taskgraph/target_tasks.py
+++ mobile/android/focus-android/taskcluster/focus_android_taskgraph/target_tasks.py
@@ -31,38 +31,11 @@ def target_tasks_default(full_task_graph, parameters, graph_config):
     return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
 
 
-@_target_task("nightly")
+@_target_task('nightly')
 def target_tasks_nightly(full_task_graph, parameters, graph_config):
     """Select the set of tasks required for a nightly build."""
-    return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
 
+    def filter(task):
+        return task.attributes.get("nightly-task", False)
 
-def _filter_fennec(fennec_type, task, parameters):
-    return task.attributes.get("build-type", "") == f"fennec-{fennec_type}"
-
-
-@_target_task("fennec-production")
-def target_tasks_fennec_nightly(full_task_graph, parameters, graph_config):
-    """Select the set of tasks required for a production build signed with the fennec key."""
-
-    return [l for l, t in full_task_graph.tasks.items() if _filter_fennec("production", t, parameters)]
-
-
-@_target_task("bump_android_components")
-def target_tasks_bump_android_components(full_task_graph, parameters, graph_config):
-    """Select the set of tasks required to update android components."""
-
-    def filter(task, parameters):
-        return task.attributes.get("bump-type", "") == "android-components"
-
-    return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
-
-
-@_target_task("screenshots")
-def target_tasks_screnshots(full_task_graph, parameters, graph_config):
-    """Select the set of tasks required to generate screenshots on a real device."""
-
-    def filter(task, parameters):
-        return task.attributes.get("screenshots", False)
-
-    return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
+    return [l for l, t in full_task_graph.tasks.items() if filter(t)]
