4e6622fb765e9d52d77915a624200db79461fdf6
Michelle Goossens <michelle@masterwayz.nl>
2021-10-01 19:25:28 +0000

    Bug 1727943 - Part 4: Migrate jittest and xpcshell Windows 10 CCov suites from AWS to Azure r=jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D127193

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -107,16 +107,6 @@ WINDOWS_WORKER_TYPES = {
         "virtual-with-gpu": "t-win64-aarch64-laptop",
         "hardware": "t-win64-aarch64-laptop",
     },
-    "windows10-64-ccov": {
-        "virtual": "t-win10-64",
-        "virtual-with-gpu": "t-win10-64-gpu-s",
-        "hardware": "t-win10-64-1803-hw",
-    },
-    "windows10-64-ccov-qr": {
-        "virtual": "t-win10-64",
-        "virtual-with-gpu": "t-win10-64-gpu-s",
-        "hardware": "t-win10-64-1803-hw",
-    },
     "windows10-64-devedition": {
         "virtual": "t-win10-64",
         "virtual-with-gpu": "t-win10-64-gpu-s",
