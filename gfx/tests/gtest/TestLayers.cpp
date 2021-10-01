e7ef20a10e8748934af2a8d38e442a29321ad7e1
Jeff Muizelaar <jmuizelaar@mozilla.com>
2021-10-01 00:08:40 +0000

    Bug 1733294 - Remove unused PaintedLayer. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D127026

--- gfx/tests/gtest/TestLayers.cpp
+++ gfx/tests/gtest/TestLayers.cpp
@@ -29,18 +29,6 @@ class TestContainerLayer : public ContainerLayer {
   }
 };
 
-class TestPaintedLayer : public PaintedLayer {
- public:
-  explicit TestPaintedLayer(LayerManager* aManager)
-      : PaintedLayer(aManager, nullptr) {}
-
-  virtual const char* Name() const { return "TestPaintedLayer"; }
-
-  virtual LayerType GetType() const { return TYPE_PAINTED; }
-
-  virtual void InvalidateRegion(const nsIntRegion& aRegion) { MOZ_CRASH(); }
-};
-
 class TestLayerManager : public LayerManager {
  public:
   TestLayerManager() : LayerManager() {}
@@ -55,10 +43,6 @@ class TestLayerManager : public LayerManager {
   virtual void GetBackendName(nsAString& aName) {}
   virtual LayersBackend GetBackendType() { return LayersBackend::LAYERS_BASIC; }
   virtual bool BeginTransaction(const nsCString& = nsCString()) { return true; }
-  virtual already_AddRefed<PaintedLayer> CreatePaintedLayer() {
-    RefPtr<PaintedLayer> layer = new TestPaintedLayer(this);
-    return layer.forget();
-  }
   virtual already_AddRefed<ColorLayer> CreateColorLayer() {
     MOZ_CRASH("Not implemented.");
   }
@@ -108,7 +92,6 @@ TEST(Layers, Transform)
 TEST(Layers, Type)
 {
   TestContainerLayer layer(nullptr);
-  ASSERT_EQ(nullptr, layer.AsPaintedLayer());
   ASSERT_EQ(nullptr, layer.AsRefLayer());
   ASSERT_EQ(nullptr, layer.AsColorLayer());
 }
@@ -150,192 +133,4 @@ TEST(Layers, UserData)
   EXPECT_CALL(*data1, Die());
   EXPECT_CALL(*data2, Die());
   EXPECT_CALL(*data3, Die());
-}
-
-static already_AddRefed<Layer> CreateLayer(char aLayerType,
-                                           LayerManager* aManager) {
-  RefPtr<Layer> layer = nullptr;
-  if (aLayerType == 'c') {
-    layer = aManager->CreateContainerLayer();
-  } else if (aLayerType == 't') {
-    layer = aManager->CreatePaintedLayer();
-  } else if (aLayerType == 'o') {
-    layer = aManager->CreateColorLayer();
-  }
-  return layer.forget();
-}
-
-already_AddRefed<Layer> CreateLayerTree(const char* aLayerTreeDescription,
-                                        nsIntRegion* aVisibleRegions,
-                                        const Matrix4x4* aTransforms,
-                                        RefPtr<LayerManager>& manager,
-                                        nsTArray<RefPtr<Layer> >& aLayersOut) {
-  aLayersOut.Clear();
-
-  if (!manager) {
-    manager = new TestLayerManager();
-  }
-
-  RefPtr<Layer> rootLayer = nullptr;
-  RefPtr<ContainerLayer> parentContainerLayer = nullptr;
-  RefPtr<Layer> lastLayer = nullptr;
-  int layerNumber = 0;
-  for (size_t i = 0; i < strlen(aLayerTreeDescription); i++) {
-    if (aLayerTreeDescription[i] == '(') {
-      if (!lastLayer) {
-        printf(
-            "Syntax error, likely '(' character isn't preceded by a "
-            "container.\n");
-        MOZ_CRASH();
-      }
-      parentContainerLayer = lastLayer->AsContainerLayer();
-      if (!parentContainerLayer) {
-        printf("Layer before '(' must be a container.\n");
-        MOZ_CRASH();
-      }
-    } else if (aLayerTreeDescription[i] == ')') {
-      parentContainerLayer = parentContainerLayer->GetParent();
-      lastLayer = nullptr;
-    } else {
-      RefPtr<Layer> layer =
-          CreateLayer(aLayerTreeDescription[i], manager.get());
-      if (aVisibleRegions) {
-        layer->SetVisibleRegion(
-            LayerIntRegion::FromUnknownRegion(aVisibleRegions[layerNumber]));
-        layer->SetEventRegions(EventRegions(aVisibleRegions[layerNumber]));
-      }
-      if (aTransforms) {
-        layer->SetBaseTransform(aTransforms[layerNumber]);
-      }
-      aLayersOut.AppendElement(layer);
-      layerNumber++;
-      if (rootLayer && !parentContainerLayer) {
-        MOZ_CRASH();
-      }
-      if (!rootLayer) {
-        rootLayer = layer;
-      }
-      if (parentContainerLayer) {
-        parentContainerLayer->InsertAfter(layer,
-                                          parentContainerLayer->GetLastChild());
-        layer->SetParent(parentContainerLayer);
-      }
-      lastLayer = layer;
-    }
-  }
-  if (rootLayer) {
-    rootLayer->ComputeEffectiveTransforms(Matrix4x4());
-    manager->SetRoot(rootLayer);
-  }
-  return rootLayer.forget();
-}
-
-TEST(Layers, LayerTree)
-{
-  const char* layerTreeSyntax = "c(c(tt))";
-  nsIntRegion layerVisibleRegion[] = {
-      nsIntRegion(IntRect(0, 0, 100, 100)),
-      nsIntRegion(IntRect(0, 0, 100, 100)),
-      nsIntRegion(IntRect(0, 0, 100, 100)),
-      nsIntRegion(IntRect(10, 10, 20, 20)),
-  };
-  Matrix4x4 transforms[] = {
-      Matrix4x4(),
-      Matrix4x4(),
-      Matrix4x4(),
-      Matrix4x4(),
-  };
-  nsTArray<RefPtr<Layer> > layers;
-
-  RefPtr<LayerManager> lm;
-  RefPtr<Layer> root = CreateLayerTree(layerTreeSyntax, layerVisibleRegion,
-                                       transforms, lm, layers);
-
-  // B2G g++ doesn't like ASSERT_NE with nullptr directly. It thinks it's
-  // an int.
-  Layer* nullLayer = nullptr;
-  ASSERT_NE(nullLayer, layers[0]->AsContainerLayer());
-  ASSERT_NE(nullLayer, layers[1]->AsContainerLayer());
-  ASSERT_NE(nullLayer, layers[2]->AsPaintedLayer());
-  ASSERT_NE(nullLayer, layers[3]->AsPaintedLayer());
-}
-
-static void ValidateTreePointers(Layer* aLayer) {
-  if (aLayer->GetNextSibling()) {
-    ASSERT_EQ(aLayer, aLayer->GetNextSibling()->GetPrevSibling());
-  } else if (aLayer->GetParent()) {
-    ASSERT_EQ(aLayer, aLayer->GetParent()->GetLastChild());
-  }
-  if (aLayer->GetPrevSibling()) {
-    ASSERT_EQ(aLayer, aLayer->GetPrevSibling()->GetNextSibling());
-  } else if (aLayer->GetParent()) {
-    ASSERT_EQ(aLayer, aLayer->GetParent()->GetFirstChild());
-  }
-  if (aLayer->GetFirstChild()) {
-    ASSERT_EQ(aLayer, aLayer->GetFirstChild()->GetParent());
-  }
-  if (aLayer->GetLastChild()) {
-    ASSERT_EQ(aLayer, aLayer->GetLastChild()->GetParent());
-  }
-}
-
-static void ValidateTreePointers(nsTArray<RefPtr<Layer> >& aLayers) {
-  for (uint32_t i = 0; i < aLayers.Length(); i++) {
-    ValidateTreePointers(aLayers[i]);
-  }
-}
-
-TEST(Layers, RepositionChild)
-{
-  const char* layerTreeSyntax = "c(ttt)";
-
-  nsTArray<RefPtr<Layer> > layers;
-  RefPtr<LayerManager> lm;
-  RefPtr<Layer> root =
-      CreateLayerTree(layerTreeSyntax, nullptr, nullptr, lm, layers);
-  ContainerLayer* parent = root->AsContainerLayer();
-  ValidateTreePointers(layers);
-
-  // tree is currently like this (using indexes into layers):
-  //   0
-  // 1 2 3
-  ASSERT_EQ(layers[2], layers[1]->GetNextSibling());
-  ASSERT_EQ(layers[3], layers[2]->GetNextSibling());
-  ASSERT_EQ(nullptr, layers[3]->GetNextSibling());
-
-  parent->RepositionChild(layers[1], layers[3]);
-  ValidateTreePointers(layers);
-
-  // now the tree is like this:
-  //   0
-  // 2 3 1
-  ASSERT_EQ(layers[3], layers[2]->GetNextSibling());
-  ASSERT_EQ(layers[1], layers[3]->GetNextSibling());
-  ASSERT_EQ(nullptr, layers[1]->GetNextSibling());
-
-  parent->RepositionChild(layers[3], layers[2]);
-  ValidateTreePointers(layers);
-
-  // no change
-  ASSERT_EQ(layers[3], layers[2]->GetNextSibling());
-  ASSERT_EQ(layers[1], layers[3]->GetNextSibling());
-  ASSERT_EQ(nullptr, layers[1]->GetNextSibling());
-
-  parent->RepositionChild(layers[3], layers[1]);
-  ValidateTreePointers(layers);
-
-  //   0
-  // 2 1 3
-  ASSERT_EQ(layers[1], layers[2]->GetNextSibling());
-  ASSERT_EQ(layers[3], layers[1]->GetNextSibling());
-  ASSERT_EQ(nullptr, layers[3]->GetNextSibling());
-
-  parent->RepositionChild(layers[3], nullptr);
-  ValidateTreePointers(layers);
-
-  //   0
-  // 3 2 1
-  ASSERT_EQ(layers[2], layers[3]->GetNextSibling());
-  ASSERT_EQ(layers[1], layers[2]->GetNextSibling());
-  ASSERT_EQ(nullptr, layers[1]->GetNextSibling());
 }
