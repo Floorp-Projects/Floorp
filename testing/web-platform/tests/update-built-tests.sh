54d801bfee429eb1e756480e1505d08535911eea
Simon Pieters <zcorpan@gmail.com>
2021-10-02 10:51:43 +0000

    Bug 1651513 [wpt PR 24521] - HTML: Add tentative tests for speculative HTML parsing, a=testonly
    Automatic update from web-platform-tests
    HTML: Add tentative tests for speculative HTML parsing
    
    See https://github.com/whatwg/html/issues/5624
    --
    
    wpt-commits: 9afacb73a04c1f33837eb0a7ffcd9ec16c9d477f
    wpt-pr: 24521

--- testing/web-platform/tests/update-built-tests.sh
+++ testing/web-platform/tests/update-built-tests.sh
@@ -9,3 +9,4 @@ html/tools/build.sh
 python3 mimesniff/mime-types/resources/generated-mime-types.py
 python3 css/css-ui/tools/appearance-build-webkit-reftests.py
 python3 webidl/tools/generate-setlike.py
+python3 html/syntax/speculative-parsing/tools/generate.py
