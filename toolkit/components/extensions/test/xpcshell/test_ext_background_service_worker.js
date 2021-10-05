0bc530a44ed2759814cf38efa648881d58c5d3b9
Luca Greco <lgreco@mozilla.com>
2021-10-05 17:05:41 +0000

    Bug 1688040 - part7.2: Listen for background service worker changes and emit internal event to ensure extension.wakeupBackground promise is resolved. r=zombie
    Depends on D106700
    
    Differential Revision: https://phabricator.services.mozilla.com/D106701

--- toolkit/components/extensions/test/xpcshell/test_ext_background_service_worker.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_background_service_worker.js
@@ -88,6 +88,17 @@ class TestWorkerWatcher extends EventEmitter {
     ChromeUtils.unregisterProcessActor(this.JS_ACTOR_NAME);
   }
 
+  getRegistration(extension) {
+    const swm = Cc["@mozilla.org/serviceworkers/manager;1"].getService(
+      Ci.nsIServiceWorkerManager
+    );
+
+    return swm.getRegistrationByPrincipal(
+      extension.extension.principal,
+      extension.extension.principal.spec
+    );
+  }
+
   watchExtensionServiceWorker(extension) {
     // These events are emitted by TestWatchExtensionWorkersParent.
     const promiseWorkerSpawned = this.waitForEvent("worker-spawned", extension);
@@ -103,6 +114,11 @@ class TestWorkerWatcher extends EventEmitter {
         Services.prefs.clearUserPref("dom.serviceWorkers.idle_timeout");
       });
       Services.prefs.setIntPref("dom.serviceWorkers.idle_timeout", 0);
+      const swReg = this.getRegistration(extension);
+      // If the active worker is already active, we have to make sure the new value
+      // set on the idle_timeout pref is picked up by ServiceWorkerPrivate::ResetIdleTimeout.
+      swReg.activeWorker?.attachDebugger();
+      swReg.activeWorker?.detachDebugger();
       return promiseWorkerTerminated;
     };
 
@@ -216,15 +232,7 @@ add_task(
       "The extension service worker has been terminated as expected"
     );
 
-    const swm = Cc["@mozilla.org/serviceworkers/manager;1"].getService(
-      Ci.nsIServiceWorkerManager
-    );
-
-    const swReg = swm.getRegistrationByPrincipal(
-      extension.extension.principal,
-      extension.extension.principal.spec
-    );
-
+    const swReg = testWorkerWatcher.getRegistration(extension);
     ok(swReg, "Got a service worker registration");
     ok(swReg?.activeWorker, "Got an active worker");
 
