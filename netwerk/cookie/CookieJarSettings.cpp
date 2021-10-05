f631f7ef60e37c01aedd63740bb45727eaca2456
Valentin Gosu <valentin.gosu@gmail.com>
2021-10-05 08:22:01 +0000

    Bug 1733356 - Fix non-unified build errors in netwerk/cookie/ r=necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D127401

--- netwerk/cookie/CookieJarSettings.cpp
+++ netwerk/cookie/CookieJarSettings.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/AntiTrackingUtils.h"
+#include "mozilla/BasePrincipal.h"
 #include "mozilla/ClearOnShutdown.h"
 #include "mozilla/ContentBlockingAllowList.h"
 #include "mozilla/dom/BrowsingContext.h"
@@ -21,6 +22,7 @@
 #  include "nsIProtocolHandler.h"
 #endif
 #include "nsIClassInfoImpl.h"
+#include "nsIChannel.h"
 #include "nsICookieManager.h"
 #include "nsICookieService.h"
 #include "nsIObjectInputStream.h"
