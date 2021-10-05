a19f20891a80882a8fc1dc51cb63da401eef4c96
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2021-10-05 12:42:15 +0000

    Bug 1734108 - Fix typo in LocaleCanonicalizer. r=platform-i18n-reviewers,dminor
    Differential Revision: https://phabricator.services.mozilla.com/D127530

--- intl/components/src/LocaleCanonicalizer.h
+++ intl/components/src/LocaleCanonicalizer.h
@@ -1,8 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#ifndef intl_components_Locale_h_
-#define intl_components_Locale_h_
+#ifndef intl_components_LocaleCanonicalizer_h_
+#define intl_components_LocaleCanonicalizer_h_
 
 #include "mozilla/intl/ICU4CGlue.h"
 #include "mozilla/Span.h"
