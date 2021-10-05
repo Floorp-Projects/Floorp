6e6f8a2fd44ff11c17653850cf7d4e61a374036a
Chris Martin <cmartin@mozilla.com>
2021-10-05 17:54:47 +0000

    Bug 1730309: Part 1 - Use Windows behavior for minimize and close buttons r=emk,emilio a=RyanVM
    Bug 1718629 did this for the maximize button.  This patch does the same for minimize and close, which we are switching to for consistent tooltip styling.
    
    Differential Revision: https://phabricator.services.mozilla.com/D125625

--- xpcom/ds/StaticAtoms.py
+++ xpcom/ds/StaticAtoms.py
@@ -1207,7 +1207,7 @@ STATIC_ATOMS = [
     Atom("time", "time"),
     Atom("title", "title"),
     Atom("titlebar", "titlebar"),
-    Atom("titlebar_max_button","titlebar-max-btn"),
+    Atom("titlebar_button","titlebar-btn"),
     Atom("titletip", "titletip"),
     Atom("token", "token"),
     Atom("tokenize", "tokenize"),
