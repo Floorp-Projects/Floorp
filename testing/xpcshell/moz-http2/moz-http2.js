55d0f70cedb91e272cb425cc9f462c1fa3d1c337
Butkovits Atila <abutkovits@mozilla.com>
2021-10-07 08:44:17 +0300

    Backed out changeset a34634271a66 (bug 1729831) for causing failures at test_odoh.js. CLOSED TREE

--- testing/xpcshell/moz-http2/moz-http2.js
+++ testing/xpcshell/moz-http2/moz-http2.js
@@ -1037,16 +1037,6 @@ function handleRequest(req, res) {
       } else {
         var b64encoded = Buffer.from(odohconfig).toString("base64");
         let packet = dnsPacket.decode(payload);
-        if (
-          u.query.failConfirmation == "true" &&
-          packet.questions[0].type == "NS" &&
-          packet.questions[0].name == "example.com"
-        ) {
-          res.writeHead(200);
-          res.write("<12bytes");
-          res.end("");
-          return;
-        }
         if (packet.questions[0].type == "HTTPS") {
           answers.push({
             name: packet.questions[0].name,
