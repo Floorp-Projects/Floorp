97fa6f5d39af9e19f42d37d68a2e3ce54be2b7a7
june wilde <jewilde@mozilla.com>
2021-10-07 07:55:30 +0000

    Bug 1670058 - support Global Privacy Control signal; r=smaug,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D126966

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -5997,6 +5997,7 @@ nsresult nsHttpChannel::BeginConnect() {
 
   SetOriginHeader();
   SetDoNotTrack();
+  SetGlobalPrivacyControl();
 
   OriginAttributes originAttributes;
   // Regular principal in case we have a proxy.
@@ -8701,6 +8702,17 @@ void nsHttpChannel::SetDoNotTrack() {
     DebugOnly<nsresult> rv =
         mRequestHead.SetHeader(nsHttp::DoNotTrack, "1"_ns, false);
     MOZ_ASSERT(NS_SUCCEEDED(rv));
+  }
+}
+
+void nsHttpChannel::SetGlobalPrivacyControl() {
+  MOZ_ASSERT(NS_IsMainThread(), "Must be called on the main thread");
+
+  if (StaticPrefs::privacy_globalprivacycontrol_enabled() &&
+      StaticPrefs::privacy_globalprivacycontrol_functionality_enabled()) {
+    // Send the header with a value of 1 to indicate opting-out
+    DebugOnly<nsresult> rv =
+        mRequestHead.SetHeader(nsHttp::GlobalPrivacyControl, "1"_ns, false);
   }
 }
 
