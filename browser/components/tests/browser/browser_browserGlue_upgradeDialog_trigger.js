e6345876a7ba6ff69f17133780b9e39f26e18c9f
Ed Lee <edilee@mozilla.com>
2021-10-07 03:59:56 +0000

    Bug 1734461 - Also check and reset newtab pref from "Firefox Home" upgrade spotlight checkbox r=pdahiya
    Add newtab pref checks and record events for when checkbox button is handled. Split up trigger tests to own file and share telemetry helpers from head.js.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127722

---
+++ browser/components/tests/browser/browser_browserGlue_upgradeDialog_trigger.js
@@ -0,0 +1,122 @@
+/* Any copyright is dedicated to the Public Domain.
+http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const { ExperimentFakes } = ChromeUtils.import(
+  "resource://testing-common/NimbusTestUtils.jsm"
+);
+const { NimbusFeatures, ExperimentAPI } = ChromeUtils.import(
+  "resource://nimbus/ExperimentAPI.jsm"
+);
+
+add_task(async function not_major_upgrade() {
+  Services.telemetry.clearEvents();
+
+  await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
+
+  AssertEvents("Not major upgrade for upgrade dialog requirements", [
+    "trigger",
+    "reason",
+    "not-major",
+  ]);
+});
+
+add_task(async function remote_disabled() {
+  mockWin7(false);
+  await ExperimentAPI.ready();
+  await ExperimentFakes.remoteDefaultsHelper({
+    feature: NimbusFeatures.upgradeDialog,
+    configuration: {
+      slug: "upgradeDialog_remoteDisabled",
+      variables: { enabled: false },
+      targeting: "true",
+    },
+  });
+
+  // Simulate starting from a previous version.
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.startup.homepage_override.mstone", "88.0"]],
+  });
+  Cc["@mozilla.org/browser/clh;1"].getService(Ci.nsIBrowserHandler).defaultArgs;
+
+  await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
+
+  AssertEvents("Feature disabled for upgrade dialog requirements", [
+    "trigger",
+    "reason",
+    "disabled",
+  ]);
+
+  // Re-enable back
+  await ExperimentFakes.remoteDefaultsHelper({
+    feature: NimbusFeatures.upgradeDialog,
+    configuration: {
+      slug: "upgradeDialog_remoteEnabled",
+      variables: { enabled: true },
+      targeting: "true",
+    },
+  });
+});
+
+add_task(async function enterprise_disabled() {
+  const defaultPrefs = Services.prefs.getDefaultBranch("");
+  const pref = "browser.aboutwelcome.enabled";
+  const orig = defaultPrefs.getBoolPref(pref, true);
+  defaultPrefs.setBoolPref(pref, false);
+
+  await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
+
+  AssertEvents("Welcome disabled like enterprise policy", [
+    "trigger",
+    "reason",
+    "no-welcome",
+  ]);
+  defaultPrefs.setBoolPref(pref, orig);
+});
+
+add_task(async function win7_excluded() {
+  mockWin7(true);
+
+  await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
+
+  AssertEvents("Not showing dialog for win7", ["trigger", "reason", "win7"]);
+});
+
+add_task(async function show_major_upgrade() {
+  mockWin7(false);
+
+  const promise = waitForDialog(async win => {
+    await BrowserTestUtils.waitForEvent(win, "ready");
+    win.close();
+  });
+  await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
+  await promise;
+
+  AssertEvents(
+    "Upgrade dialog opened and closed from major upgrade",
+    ["trigger", "reason", "satisfied"],
+    ["content", "show", "3-screens"],
+    ["content", "show", "upgrade-dialog-start-primary-button"],
+    ["content", "close", "external"]
+  );
+
+  BrowserTestUtils.removeTab(gBrowser.selectedTab);
+});
+
+add_task(async function dont_reshow() {
+  await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
+
+  AssertEvents("Shouldn't reshow for upgrade dialog requirements", [
+    "trigger",
+    "reason",
+    "already-shown",
+  ]);
+});
+
+registerCleanupFunction(() => {
+  Cc["@mozilla.org/browser/clh;1"].getService(
+    Ci.nsIBrowserHandler
+  ).majorUpgrade = false;
+  Services.prefs.clearUserPref("browser.startup.upgradeDialog.version");
+});
