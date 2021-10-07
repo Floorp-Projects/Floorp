32b69def248d4e448272874afdd211d4d6e697aa
Andrew Halberstadt <ahal@mozilla.com>
2021-10-07 19:27:20 +0000

    Bug 1734388 - Suppress 'tgdiff' failures on the base revision, r=taskgraph-reviewers,aki
    The 'tgdiff' task can fail if you are working on the 'taskgraph' module itself
    and the parent revision isn't compatible with your changes.
    
    This ensures the 'tgdiff' task stays green if there was a failure on the
    parent, but still turns orange if there was a failure on the current revision
    (since that is likely a legit error)
    
    Differential Revision: https://phabricator.services.mozilla.com/D127874

--- taskcluster/gecko_taskgraph/main.py
+++ taskcluster/gecko_taskgraph/main.py
@@ -420,6 +420,7 @@ def show_taskgraph(options):
             f"--label={options['graph_attr']}@{cur_ref}",
         ]
 
+        non_fatal_failures = []
         for spec in parameters:
             base_path = os.path.join(diffdir, f"{options['graph_attr']}_{base_ref}")
             cur_path = os.path.join(diffdir, f"{options['graph_attr']}_{cur_ref}")
@@ -429,6 +430,16 @@ def show_taskgraph(options):
                 params_name = Parameters.format_spec(spec)
                 base_path += f"_{params_name}"
                 cur_path += f"_{params_name}"
+
+            # We only capture errors when the 'base' generation fails. This is
+            # because if the 'current' generation passed, the failure is likely
+            # due to a difference in the set of revisions being tested and
+            # harmless. We'll still log a warning to notify that the diff is
+            # not available. But if the current generation failed, the error
+            # needs to be addressed.
+            if not os.path.isfile(base_path):
+                non_fatal_failures.append(os.path.basename(base_path))
+                continue
 
             try:
                 proc = subprocess.run(
@@ -454,6 +465,16 @@ def show_taskgraph(options):
                 # console in this case instead.
                 path=None if returncode == 0 else output_file,
                 params_spec=spec if len(parameters) > 1 else None,
+            )
+
+        if non_fatal_failures:
+            failstr = "\n  ".join(sorted(non_fatal_failures))
+            print(
+                "WARNING: Diff skipped for the following generation{s} "
+                "due to failures:\n  {failstr}".format(
+                    s="s" if len(non_fatal_failures) > 1 else "", failstr=failstr
+                ),
+                file=sys.stderr,
             )
 
         if options["format"] != "json":
