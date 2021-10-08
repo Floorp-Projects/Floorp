937632cada61a1e265a970e9cf313faed8919a41
Tim Chevalier <tjc@igalia.com>
2021-10-08 15:12:15 +0000

    Bug 1729563 - Add shell flag for change-array-by-copy methods r=mgaudet
    Add a build flag and shell flag for enabling change-array-by-copy methods.
    The flags currently have no effect.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127201

--- js/public/ContextOptions.h
+++ js/public/ContextOptions.h
@@ -51,6 +51,9 @@ class JS_PUBLIC_API ContextOptions {
         fuzzing_(false),
         privateClassFields_(false),
         privateClassMethods_(false),
+ #ifdef ENABLE_CHANGE_ARRAY_BY_COPY
+        changeArrayByCopy_(false),
+ #endif
         ergonomicBrandChecks_(false) {
   }
   // clang-format on
@@ -160,6 +163,14 @@ class JS_PUBLIC_API ContextOptions {
     ergonomicBrandChecks_ = enabled;
     return *this;
   }
+
+#ifdef ENABLE_CHANGE_ARRAY_BY_COPY
+  bool changeArrayByCopy() const { return changeArrayByCopy_; }
+  ContextOptions& setChangeArrayByCopy(bool enabled) {
+    changeArrayByCopy_ = enabled;
+    return *this;
+  }
+#endif
 
   bool classStaticBlocks() const { return classStaticBlocks_; }
   ContextOptions& setClassStaticBlocks(bool enabled) {
@@ -276,6 +287,9 @@ class JS_PUBLIC_API ContextOptions {
   bool fuzzing_ : 1;
   bool privateClassFields_ : 1;
   bool privateClassMethods_ : 1;
+#ifdef ENABLE_CHANGE_ARRAY_BY_COPY
+  bool changeArrayByCopy_ : 1;
+#endif
   bool ergonomicBrandChecks_ : 1;
   bool classStaticBlocks_ : 1;
 };
