0bbad2ac3a796429794b5672de2babf2c5efefb3
Cristian Tuns <ctuns@mozilla.com>
2021-10-11 12:10:06 -0400

    Backed out 2 changesets (bug 1732919) for causing mochitest failures on browser_webconsole_requestStorageAccess_errors.js. CLOSED TREE
    Backed out changeset 759d9b550cca (bug 1732919)
    Backed out changeset 2c86a1d595b8 (bug 1732919)

--- toolkit/components/antitracking/test/browser/browser_denyPermissionForTracker.js
+++ toolkit/components/antitracking/test/browser/browser_denyPermissionForTracker.js
@@ -37,7 +37,8 @@ AntiTracking._createTask({
     ok(document.cookie == "", "All is blocked");
 
     // requestStorageAccess should reject
-    SpecialPowers.wrap(document).notifyUserGestureActivation();
+    let dwu = SpecialPowers.getDOMWindowUtils(window);
+    let helper = dwu.setHandlingUserInput(true);
     await document
       .requestStorageAccess()
       .then(() => {
@@ -46,7 +47,7 @@ AntiTracking._createTask({
       .catch(() => {
         ok(true, "Should not grant storage access");
       });
-    SpecialPowers.wrap(document).clearUserGestureActivation();
+    helper.destruct();
   },
   extraPrefs: null,
   expectedBlockingNotifications:
