7a046130d664ccd94513133c3705d0311cd9a27d
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-10-11 16:30:04 +0000

    Bug 1734592 - [devtools] Remove Promise.jsm usage in devtools/client/inspector/rules. r=jdescottes.
    Differential Revision: https://phabricator.services.mozilla.com/D127809

--- devtools/client/inspector/rules/models/element-style.js
+++ devtools/client/inspector/rules/models/element-style.js
@@ -5,7 +5,6 @@
 "use strict";
 
 const Services = require("Services");
-const promise = require("promise");
 const Rule = require("devtools/client/inspector/rules/models/rule");
 const UserProperties = require("devtools/client/inspector/rules/models/user-properties");
 const {
@@ -129,7 +128,7 @@ class ElementStyle {
       })
       .then(entries => {
         if (this.destroyed || this.populated !== populated) {
-          return promise.resolve(undefined);
+          return Promise.resolve(undefined);
         }
 
         // Store the current list of rules (if any) during the population
@@ -167,7 +166,7 @@ class ElementStyle {
         // populate is often called after a setTimeout,
         // the connection may already be closed.
         if (this.destroyed) {
-          return promise.resolve(undefined);
+          return Promise.resolve(undefined);
         }
         return promiseWarn(e);
       });
