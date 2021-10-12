d429e2ebe640d7b8ae1e6fcd348ab1e09270c4e0
criss <ccozmuta@mozilla.com>
2021-10-12 14:22:54 +0300

    Backed out 2 changesets (bug 1734738) for causing failures on browser_aboutprofiling-threads.js
    Backed out changeset cb13dc106ff9 (bug 1734738)
    Backed out changeset 39004de98ca3 (bug 1734738)

--- devtools/client/performance-new/components/Settings.js
+++ devtools/client/performance-new/components/Settings.js
@@ -66,7 +66,6 @@ const {
   h3,
   section,
   p,
-  span,
 } = require("devtools/client/shared/vendor/react-dom-factories");
 const Range = createFactory(
   require("devtools/client/performance-new/components/Range")
@@ -265,7 +264,6 @@ class Settings extends PureComponent {
    */
   _renderThreadsColumns(threadDisplay, index) {
     const { threads } = this.props;
-    const areAllThreadsIncluded = threads.includes("*");
     return div(
       { className: "perf-settings-thread-column", key: index },
       threadDisplay.map(({ name, id, l10nId }) =>
@@ -274,11 +272,8 @@ class Settings extends PureComponent {
           { id: l10nId, attrs: { title: true }, key: name },
           label(
             {
-              className: `perf-settings-checkbox-label perf-settings-thread-label toggle-container-with-text ${
-                areAllThreadsIncluded
-                  ? "perf-settings-checkbox-label-disabled"
-                  : ""
-              }`,
+              className:
+                "perf-settings-checkbox-label perf-settings-thread-label toggle-container-with-text",
             },
             input({
               className: "perf-settings-checkbox",
@@ -287,10 +282,9 @@ class Settings extends PureComponent {
               // Do not localize the value, this is used internally by the profiler.
               value: name,
               checked: threads.includes(name),
-              disabled: areAllThreadsIncluded,
               onChange: this._handleThreadCheckboxChange,
             }),
-            span(null, name)
+            name
           )
         )
       )
