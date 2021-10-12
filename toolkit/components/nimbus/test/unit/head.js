b0d4c86fd7cf4180a0df2de27d051f391a2706db
Alexandru Michis <malexandru@mozilla.com>
2021-10-12 20:13:05 +0300

    Backed out changeset 837ba61b9975 (bug 1730924) for causing xpcshell failures in services/sync.
    CLOSED TREE

--- toolkit/components/nimbus/test/unit/head.js
+++ toolkit/components/nimbus/test/unit/head.js
@@ -3,7 +3,6 @@
 
 const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
-const { Ajv } = ChromeUtils.import("resource://testing-common/ajv-6.12.6.js");
 const { XPCOMUtils } = ChromeUtils.import(
   "resource://gre/modules/XPCOMUtils.jsm"
 );
