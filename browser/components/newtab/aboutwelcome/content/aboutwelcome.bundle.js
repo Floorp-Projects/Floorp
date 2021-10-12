aba06ecc303d4c0b24096b9fa7f6b40d83b3976e
Emily McMinn <emcminn@mozilla.com>
2021-10-12 20:50:48 +0000

    Bug 1734126 - Make sure the autoClose doesn't persist if we navigate away from the Thanks page. r=pdahiya, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D127892

--- browser/components/newtab/aboutwelcome/content/aboutwelcome.bundle.js
+++ browser/components/newtab/aboutwelcome/content/aboutwelcome.bundle.js
@@ -758,6 +758,7 @@ class MultiStageProtonScreen extends react__WEBPACK_IMPORTED_MODULE_0___default.
       content,
       totalNumberOfScreens: total
     } = this.props;
+    const windowObj = this.props.windowObj || window;
     const isWelcomeScreen = this.props.order === 0;
     const isLastScreen = this.props.order === total;
     const autoCloseTime = 20000; // Assign proton screen style 'screen-1' or 'screen-2' by checking
@@ -765,11 +766,14 @@ class MultiStageProtonScreen extends react__WEBPACK_IMPORTED_MODULE_0___default.
 
     const screenClassName = isWelcomeScreen ? "screen-0" : `${this.props.order === 1 ? `dialog-initial` : ``} ${this.props.order === total ? `dialog-last` : ``} screen-${this.props.order % 2 !== 0 ? 1 : 2}`;
 
-    if (this.props.order === total && autoClose) {
-      let currentURL = window.location.href;
+    if (isLastScreen && autoClose) {
+      let currentURL = windowObj.location.href;
       setTimeout(function () {
-        if (window.location.href === currentURL) {
-          window.location.href = "about:home";
+        // set the timer to close last screen and redirect to about:home after 20 seconds
+        const screenEl = windowObj.document.querySelector(".screen");
+
+        if (windowObj.location.href === currentURL && screenEl.className.includes("dialog-last")) {
+          windowObj.location.href = "about:home";
         }
       }, autoCloseTime);
     }
