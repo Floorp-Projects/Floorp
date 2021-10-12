e5c3e23693eb23f8f1fd0e7b2ebac89efdd30146
Olli Pettay <Olli.Pettay@helsinki.fi>
2021-10-12 11:23:17 +0000

    Bug 1734858, use the real index when updating the index after a bfcache load, r=peterv
    The issue is that when going back from a non-bfcacheable page to a bfcacheable page and the first page
    modifies requested index around the same time, UpdateIndex() doesn't have the right requested index. This is a variant of bug 1725680.
    The test case loads a non-bfcacheable page, then bfcachable, then another non-bfcacheable and then goes back and forward.
    Without the patch indexes go wrong and forward doesn't do anything.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127963

--- docshell/shistory/nsSHistory.cpp
+++ docshell/shistory/nsSHistory.cpp
@@ -1202,9 +1202,14 @@ static void FinishRestore(CanonicalBrowsingContext* aBrowsingContext,
 
   aEntry->SetFrameLoader(nullptr);
 
+  nsCOMPtr<nsISHistory> shistory = aEntry->GetShistory();
+  int32_t indexOfHistoryLoad =
+      shistory ? shistory->GetIndexOfEntry(aEntry) : -1;
+
   nsCOMPtr<nsFrameLoaderOwner> frameLoaderOwner =
       do_QueryInterface(aBrowsingContext->GetEmbedderElement());
-  if (frameLoaderOwner && aFrameLoader->GetMaybePendingBrowsingContext()) {
+  if (frameLoaderOwner && aFrameLoader->GetMaybePendingBrowsingContext() &&
+      indexOfHistoryLoad >= 0) {
     // Synthesize a STATE_START WebProgress state change event from here
     // in order to ensure emitting it on the BrowsingContext we navigate *from*
     // instead of the BrowsingContext we navigate *to*.
@@ -1255,7 +1260,8 @@ static void FinishRestore(CanonicalBrowsingContext* aBrowsingContext,
 
     // Assuming we still have the session history, update the index.
     if (loadingBC->GetSessionHistory()) {
-      loadingBC->GetSessionHistory()->UpdateIndex();
+      shistory->InternalSetRequestedIndex(indexOfHistoryLoad);
+      shistory->UpdateIndex();
     }
     loadingBC->HistoryCommitIndexAndLength();
     Unused << loadingBC->SetIsInBFCache(false);
