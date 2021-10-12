117ed88ef9d8ef8e309e6f1fc307aa2abf79052a
Lars T Hansen <lhansen@mozilla.com>
2021-10-12 15:07:33 +0000

    Bug 1735207 - Improve WasmExtendU32Index and WasmWrapU32Index. r=nbp
    WasmExtendU32Index and WasmWrapU32Index were not GVN-friendly and did
    not perform constant folding.  (The problem was introduced by the
    support for 4GB heaps and 64-bit bounds, which created these nodes.)
    As a consequence, these nodes impeded bounds check elimination, with
    the effect that BCE on 64-bit systems did nothing.  BCE on 32-bit
    systems was likely unaffected by the bug, as the max heap size on
    32-bit is 2GB.
    
    Make these nodes GVN-friendly and add appropriate constant folding.
    
    Add simple white-box test cases to test that the BCE works properly on
    64-bit and 32-bit both.
    
    Differential Revision: https://phabricator.services.mozilla.com/D128175

---
+++ js/src/jit-test/tests/wasm/bce-x86-ion-codegen.js
@@ -0,0 +1,55 @@
+// |jit-test| --wasm-compiler=ion; --spectre-mitigations=off; skip-if: !hasDisassembler() || wasmCompileMode() != "ion" || !getBuildConfiguration().x86 || getBuildConfiguration().simulator || getJitCompilerOptions()["ion.check-range-analysis"]; include:codegen-x86-test.js
+
+// Spectre mitigation is disabled above to make the generated code simpler to
+// match; ion.check-range-analysis makes a hash of the code and makes testing
+// pointless.
+
+// White-box testing of bounds check elimination on 32-bit systems.
+//
+// This is probably fairly brittle, but BCE (on 64-bit platforms) regressed (bug
+// 1735207) without us noticing, so it's not like we can do without these tests.
+//
+// See also bce-x64-ion-codegen.js.
+
+// Make sure the check for the second load is removed: the two load instructions
+// should appear back-to-back in the output.
+codegenTestX86_adhoc(
+`(module
+   (memory 1)
+   (func (export "f") (param i32) (result i32)
+     (local i32)
+     (local.set 1 (i32.add (local.get 0) (i32.const 4)))
+     (i32.load (local.get 1))
+     drop
+     (i32.load (local.get 1))))`,
+    'f', `
+3b ..                     cmp %e.., %e..
+0f 82 02 00 00 00         jb 0x00000000000000..
+0f 0b                     ud2
+8b .. ..                  movl \\(%r..,%r..,1\\), %e..
+8b .. ..                  movl \\(%r..,%r..,1\\), %eax`,
+    {no_prefix:true});
+
+// Make sure constant indices below the heap minimum do not require a bounds check.
+// The first movl from *rsi below loads the heap base from Tls, an x86-ism.
+codegenTestX86_adhoc(
+`(module
+   (memory 1)
+   (func (export "f") (result i32)
+     (i32.load (i32.const 16))))`,
+    'f', `
+8b ..                     movl \\(%rsi\\), %e..
+8b .. 10                  movl 0x10\\(%r..\\), %eax`);
+
+// Ditto, even at the very limit of the known heap, extending into the guard
+// page.  This is an OOB access, of course, but it needs no explicit bounds
+// check.
+codegenTestX86_adhoc(
+`(module
+   (memory 1)
+   (func (export "f") (result i32)
+     (i32.load (i32.const 65535))))`,
+    'f',
+`
+8b ..                     movl \\(%rsi\\), %e..
+8b .. ff ff 00 00         movl 0xFFFF\\(%r..\\), %eax`);
--- js/src/jit/WasmBCE.cpp
+++ js/src/jit/WasmBCE.cpp
@@ -52,9 +52,12 @@ bool jit::EliminateBoundsChecks(MIRGenerator* mir, MIRGraph& graph) {
           // result is above 2^31-1, but we don't care about that for BCE.
 
           if (addr->isConstant() &&
-              addr->toConstant()->type() == MIRType::Int32 &&
-              uint64_t(addr->toConstant()->toInt32()) <
-                  mir->minWasmHeapLength()) {
+              ((addr->toConstant()->type() == MIRType::Int32 &&
+                uint64_t(addr->toConstant()->toInt32()) <
+                    mir->minWasmHeapLength()) ||
+               (addr->toConstant()->type() == MIRType::Int64 &&
+                uint64_t(addr->toConstant()->toInt64()) <
+                    mir->minWasmHeapLength()))) {
             bc->setRedundant();
             if (JitOptions.spectreIndexMasking) {
               bc->replaceAllUsesWith(addr);
