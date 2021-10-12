c44c63311f1ce547f63869ddb76a9e7f65bb18c0
Paul Zuehlcke <pbz@mozilla.com>
2021-10-12 13:03:14 +0000

    Bug 1732919 - Update tests for new Storage Access API user activation behavior. r=anti-tracking-reviewers,timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D127152

--- toolkit/components/antitracking/test/browser/browser_storageAccessPrivilegeAPI.js
+++ toolkit/components/antitracking/test/browser/browser_storageAccessPrivilegeAPI.js
@@ -177,10 +177,9 @@ add_task(async function test_privilege_api_with_reject_tracker() {
 
   // Call the privilege API.
   await SpecialPowers.spawn(browser, [], async _ => {
-    // The privilege API requires a user gesture. So, we set the user handling
-    // flag before we call the API.
-    let dwu = SpecialPowers.getDOMWindowUtils(content);
-    let helper = dwu.setHandlingUserInput(true);
+    // The privilege API requires user activation. So, we set the user
+    // activation flag before we call the API.
+    content.document.notifyUserGestureActivation();
 
     try {
       await content.document.requestStorageAccessForOrigin(
@@ -190,7 +189,7 @@ add_task(async function test_privilege_api_with_reject_tracker() {
       ok(false, "The API shouldn't throw.");
     }
 
-    helper.destruct();
+    content.document.clearUserGestureActivation();
   });
 
   // Verify if the storage access permission is set correctly.
@@ -274,8 +273,7 @@ add_task(async function test_privilege_api_with_dFPI() {
   await SpecialPowers.spawn(browser, [], async _ => {
     // The privilege API requires a user gesture. So, we set the user handling
     // flag before we call the API.
-    let dwu = SpecialPowers.getDOMWindowUtils(content);
-    let helper = dwu.setHandlingUserInput(true);
+    content.document.notifyUserGestureActivation();
 
     try {
       await content.document.requestStorageAccessForOrigin(
@@ -285,7 +283,7 @@ add_task(async function test_privilege_api_with_dFPI() {
       ok(false, "The API shouldn't throw.");
     }
 
-    helper.destruct();
+    content.document.clearUserGestureActivation();
   });
 
   // Verify if the storage access permission is set correctly.
@@ -365,8 +363,7 @@ add_task(async function test_prompt() {
     let callAPIPromise = SpecialPowers.spawn(browser, [allow], async allow => {
       // The privilege API requires a user gesture. So, we set the user handling
       // flag before we call the API.
-      let dwu = SpecialPowers.getDOMWindowUtils(content);
-      let helper = dwu.setHandlingUserInput(true);
+      content.document.notifyUserGestureActivation();
       let isThrown = false;
 
       try {
@@ -379,7 +376,7 @@ add_task(async function test_prompt() {
 
       is(isThrown, !allow, `The API ${allow ? "shouldn't" : "should"} throw.`);
 
-      helper.destruct();
+      content.document.clearUserGestureActivation();
     });
 
     await shownPromise;
@@ -447,9 +444,7 @@ add_task(async function test_invalid_input() {
     }
     ok(isThrown, "The API should throw without user gesture.");
 
-    let dwu = SpecialPowers.getDOMWindowUtils(content);
-    let helper = dwu.setHandlingUserInput(true);
-
+    content.document.notifyUserGestureActivation();
     isThrown = false;
     try {
       await content.document.requestStorageAccessForOrigin();
@@ -458,6 +453,7 @@ add_task(async function test_invalid_input() {
     }
     ok(isThrown, "The API should throw with no input.");
 
+    content.document.notifyUserGestureActivation();
     isThrown = false;
     try {
       await content.document.requestStorageAccessForOrigin("");
@@ -467,6 +463,7 @@ add_task(async function test_invalid_input() {
     }
     ok(isThrown, "The API should throw with empty string.");
 
+    content.document.notifyUserGestureActivation();
     isThrown = false;
     try {
       await content.document.requestStorageAccessForOrigin("invalid url");
@@ -476,7 +473,7 @@ add_task(async function test_invalid_input() {
     }
     ok(isThrown, "The API should throw with invalid url.");
 
-    helper.destruct();
+    content.document.clearUserGestureActivation();
   });
 
   BrowserTestUtils.removeTab(tab);
