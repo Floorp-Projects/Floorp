714f3f274d5199f7d6dfdabb967373da5eaa93df
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2021-10-12 08:31:29 +0000

    Bug 1734687 - fix mochitest plain tests to pass with/without improvements_to_downloads changes, r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D127871

--- uriloader/exthandler/tests/mochitest/HelperAppLauncherDialog_chromeScript.js
+++ uriloader/exthandler/tests/mochitest/HelperAppLauncherDialog_chromeScript.js
@@ -2,17 +2,72 @@ const { ComponentUtils } = ChromeUtils.import(
   "resource://gre/modules/ComponentUtils.jsm"
 );
 
+const { Downloads } = ChromeUtils.import(
+  "resource://gre/modules/Downloads.jsm"
+);
+
+let gMIMEService = Cc["@mozilla.org/mime;1"].getService(Ci.nsIMIMEService);
+let gHandlerService = Cc["@mozilla.org/uriloader/handler-service;1"].getService(
+  Ci.nsIHandlerService
+);
+
 const HELPERAPP_DIALOG_CONTRACT = "@mozilla.org/helperapplauncherdialog;1";
 const HELPERAPP_DIALOG_CID = Components.ID(
   Cc[HELPERAPP_DIALOG_CONTRACT].number
 );
+
+let tmpDir = Services.dirsvc.get("TmpD", Ci.nsIFile);
+tmpDir.append("testsavedir" + Math.floor(Math.random() * 2 ** 32));
+// Create this dir if it doesn't exist (ignores existing dirs)
+try {
+  tmpDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0o777, true);
+} catch (ex) {
+  if (ex.result != Cr.NS_ERROR_FILE_ALREADY_EXISTS) {
+    throw ex;
+  }
+}
+Services.prefs.setIntPref("browser.download.folderList", 2);
+Services.prefs.setCharPref("browser.download.dir", tmpDir.path);
 
 const FAKE_CID = Services.uuid.generateUUID();
 /* eslint-env mozilla/frame-script */
 function HelperAppLauncherDialog() {}
 HelperAppLauncherDialog.prototype = {
   show(aLauncher, aWindowContext, aReason) {
-    sendAsyncMessage("suggestedFileName", aLauncher.suggestedFileName);
+    if (
+      Services.prefs.getBoolPref(
+        "browser.download.improvements_to_download_panel"
+      )
+    ) {
+      sendAsyncMessage("wrongAPICall", "show");
+    } else {
+      let f = tmpDir.clone();
+      f.append(aLauncher.suggestedFileName);
+      aLauncher.saveDestinationAvailable(f);
+      sendAsyncMessage("suggestedFileName", aLauncher.suggestedFileName);
+    }
+    aLauncher.cancel(Cr.NS_BINDING_ABORTED);
+  },
+  promptForSaveToFileAsync(
+    appLauncher,
+    parent,
+    filename,
+    extension,
+    forceSave
+  ) {
+    if (
+      !Services.prefs.getBoolPref(
+        "browser.download.improvements_to_download_panel"
+      )
+    ) {
+      sendAsyncMessage("wrongAPICall", "promptForSaveToFileAsync");
+    } else {
+      let f = tmpDir.clone();
+      f.append(filename);
+      appLauncher.saveDestinationAvailable(f);
+      sendAsyncMessage("suggestedFileName", filename);
+    }
+    appLauncher.cancel(Cr.NS_BINDING_ABORTED);
   },
   QueryInterface: ChromeUtils.generateQI(["nsIHelperAppLauncherDialog"]),
 };
@@ -25,12 +80,24 @@ registrar.registerFactory(
   ComponentUtils._getFactory(HelperAppLauncherDialog)
 );
 
-addMessageListener("unregister", function() {
+addMessageListener("unregister", async function() {
   registrar.registerFactory(
     HELPERAPP_DIALOG_CID,
     "",
     HELPERAPP_DIALOG_CONTRACT,
     null
   );
+  let list = await Downloads.getList(Downloads.ALL);
+  let downloads = await list.getAll();
+  for (let dl of downloads) {
+    await dl.refresh();
+    if (dl.target.exists || dl.target.partFileExists) {
+      dump("Finalizing download.\n");
+      await dl.finalize(true).catch(Cu.reportError);
+    }
+  }
+  await list.removeFinished();
+  dump("Clearing " + tmpDir.path + "\n");
+  tmpDir.remove(true);
   sendAsyncMessage("unregistered");
 });
