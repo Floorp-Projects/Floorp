ef9cc152fa6efb3ca00fddffc5529b68cd36d08e
Mark Banner <standard8@mozilla.com>
2021-10-12 15:15:18 +0000

    Bug 1735112 - Avoid loading PlacesUtils.jsm in early startup from MacTouchBar.js. r=harry
    Differential Revision: https://phabricator.services.mozilla.com/D128045

--- browser/components/touchbar/MacTouchBar.js
+++ browser/components/touchbar/MacTouchBar.js
@@ -12,7 +12,6 @@ const { XPCOMUtils } = ChromeUtils.import(
 XPCOMUtils.defineLazyModuleGetters(this, {
   AppConstants: "resource://gre/modules/AppConstants.jsm",
   BrowserWindowTracker: "resource:///modules/BrowserWindowTracker.jsm",
-  PlacesUtils: "resource://gre/modules/PlacesUtils.jsm",
   PrivateBrowsingUtils: "resource://gre/modules/PrivateBrowsingUtils.jsm",
   Services: "resource://gre/modules/Services.jsm",
   UrlbarTokenizer: "resource:///modules/UrlbarTokenizer.jsm",
@@ -571,7 +570,7 @@ class TouchBarInput {
     this._title = title;
   }
   get image() {
-    return this._image ? PlacesUtils.toURI(this._image) : null;
+    return this._image ? Services.io.newURI(this._image) : null;
   }
   set image(image) {
     this._image = image;
