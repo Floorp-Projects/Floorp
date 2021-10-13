971edb909bff5403cf27e5eb0af7e44415538d3c
Tim Chevalier <tjc@igalia.com>
2021-10-13 14:43:20 +0000

    Bug 1729563 - Implement change-array-by-copy methods r=mgaudet
    See https://tc39.es/proposal-change-array-by-copy/
    
        Added jit-test tests in change-array-by-copy.js
    
    Depends on: D127201
    
    Differential Revision: https://phabricator.services.mozilla.com/D126146

--- js/src/tests/non262/Array/unscopables.js
+++ js/src/tests/non262/Array/unscopables.js
@@ -20,19 +20,31 @@ assertDeepEq(desc2, {
 
 let keys = Reflect.ownKeys(Array_unscopables);
 
-assertDeepEq(keys, [
-    "at",
-    "copyWithin",
-    "entries",
-    "fill",
-    "find",
-    "findIndex",
-    "flat",
-    "flatMap",
-    "includes",
-    "keys",
-    "values"
-]);
+let expectedKeys = ["at",
+		    "copyWithin",
+		    "entries",
+		    "fill",
+		    "find",
+		    "findIndex",
+		    "flat",
+		    "flatMap",
+		    "includes",
+		    "keys",
+		    "values"];
+
+if (typeof getBuildConfiguration === "undefined") {
+  var getBuildConfiguration = SpecialPowers.Cu.getJSTestingFunctions().getBuildConfiguration;
+}
+
+if (typeof getRealmConfiguration === "undefined") {
+  var getRealmConfiguration = SpecialPowers.Cu.getJSTestingFunctions().getRealmConfiguration;
+}
+
+if (getBuildConfiguration()['change-array-by-copy'] && getRealmConfiguration()['change-array-by-copy']) {
+    expectedKeys.push("withAt", "withReversed", "withSorted", "withSpliced");
+}
+
+assertDeepEq(keys, expectedKeys);
 
 for (let key of keys)
     assertEq(Array_unscopables[key], true);
