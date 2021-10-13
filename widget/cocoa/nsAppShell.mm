bb45fd79a7ed06fc3aae490f2da80849b831e800
Jeff Muizelaar <jmuizelaar@mozilla.com>
2021-10-13 19:50:36 +0000

    Bug 1734705. Allow the event loop changes into EARLY_BETA. r=mstange
    This will let us get some more testing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D128401

--- widget/cocoa/nsAppShell.mm
+++ widget/cocoa/nsAppShell.mm
@@ -698,7 +698,7 @@ bool nsAppShell::ProcessNextNativeEvent(bool aMayWait) {
       // events and native events.  Without CGEvent coalescing, the native
       // event events can accumulate in the Carbon event queue which will
       // manifest as laggy scrolling.
-#ifdef NIGHTLY_BUILD
+#ifdef EARLY_BETA_OR_EARLIER
       eventProcessed = false;
       break;
 #else
