dd3d0cc92bc2f906f3dec28f2b82bc07d4d57266
Julien Wajsberg <felash@gmail.com>
2021-10-13 08:39:36 +0000

    Bug 1734738 - [aboutprofiling] Disable all threads checkboxes when the 'all threads' checkbox is checked r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D128100

--- devtools/client/performance-new/test/browser/browser_aboutprofiling-threads-behavior.js
+++ devtools/client/performance-new/test/browser/browser_aboutprofiling-threads-behavior.js
@@ -103,6 +103,8 @@ add_task(async function test() {
       "Asterisk was in the thread text input"
     );
 
+    is(styleThreadCheckbox.disabled, true, "The Style Thread is now disabled.");
+
     // Remove the asterisk
     setReactFriendlyInputValue(
       threadTextEl,
@@ -111,6 +113,7 @@ add_task(async function test() {
     threadTextEl.dispatchEvent(new Event("blur", { bubbles: true }));
 
     ok(!allThreadsCheckbox.checked, "The all threads checkbox is not checked.");
+    is(styleThreadCheckbox.disabled, false, "The Style Thread is now enabled.");
   });
 });
 
