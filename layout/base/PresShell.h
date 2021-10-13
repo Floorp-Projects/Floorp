377995ccb86ffcda0e455862923ae2887c7a634e
Timothy Nikkel <tnikkel@gmail.com>
2021-10-13 03:01:34 +0000

    Bug 1725569. Send visualViewport resize events for iframes. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D127941

--- layout/base/PresShell.h
+++ layout/base/PresShell.h
@@ -2967,6 +2967,7 @@ class PresShell final : public nsStubDocumentObserver,
   // needed for the synthetic mouse events.
   layers::ScrollableLayerGuid mMouseEventTargetGuid;
 
+  // Only populated on root content documents.
   nsSize mVisualViewportSize;
 
   // The focus information needed for async keyboard scrolling
