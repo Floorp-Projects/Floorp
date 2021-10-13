0019c467f038f4245df1e183bba27f6d44bba540
pestanoah <pestanoa@msu.edu>
2021-10-13 14:19:02 +0000

    Bug 1724284 - Renamed Browser Tools id to browserToolsMenu. r=mhowell,mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D125331

--- devtools/startup/tests/browser/browser_shim_disable_devtools.js
+++ devtools/startup/tests/browser/browser_shim_disable_devtools.js
@@ -110,12 +110,10 @@ add_task(async function() {
 
   info("Open the menubar Tools menu");
   const toolsMenuPopup = win.document.getElementById("menu_ToolsPopup");
-  // The "Browser Tools" menu still uses the id `webDeveloperMenu` even though it
-  // is no longer DevTools-specific.
-  const browserToolsMenu = win.document.getElementById("webDeveloperMenu");
+  const browserToolsMenu = win.document.getElementById("browserToolsMenu");
   ok(
     !browserToolsMenu.hidden,
-    "The Web Developer item of the tools menu is visible"
+    "The Browser Tools item of the tools menu is visible"
   );
 
   await simulateMenuOpen(toolsMenuPopup);
