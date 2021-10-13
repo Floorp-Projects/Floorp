5f31ca0d1196311022e3731646f46adccd3f2a4a
Alexandre Poirot <poirot.alex@gmail.com>
2021-10-13 09:17:56 +0000

    Bug 1539502 - [devtools] Prevent leaking all transient global while recording memory leaks. r=jdescottes
    This is two reason for leaking the globals:
    * The JIT/CachedIR that are introduced by trackingAllocationSites=true
    and can be fixed by calling minimizeMemoryUsage to purge CachedIR objects.
    * The Debugger::allocationsLog which relates to Debugger.Memory.drainAllocationLog
    and can be fixed by calling drainAllocationLog to clear allocationsLog.
    We also have to be careful about disable allocation site recording while doing the GCs.
    On start and on stop.
    
    Both were keeping strong references to the globals.
    
    In this patch, I'm also extending the coverage of the AllocationTracker
    to better assert what precise leaks are reported.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127777

--- devtools/shared/test-helpers/browser_allocation_tracker.js
+++ devtools/shared/test-helpers/browser_allocation_tracker.js
@@ -20,6 +20,10 @@ const TrackedObjects = loader.require(
   "devtools/shared/test-helpers/tracked-objects.jsm"
 );
 
+// This test record multiple times complete heap snapshot,
+// so that it can take a little bit to complete.
+requestLongerTimeout(2);
+
 add_task(async function() {
   // Use a sandbox to allocate test javascript object in order to avoid any
   // external noise
@@ -149,4 +153,98 @@ add_task(async function() {
   tracker.stop();
 
   TrackedObjects.clear();
+});
+
+add_task(async function() {
+  info("Test start and stop recording with tracked objects");
+
+  const sandbox = Cu.Sandbox(window);
+  const tracker = allocationTracker({ watchGlobal: sandbox });
+  await tracker.startRecordingAllocations("leaks");
+
+  Cu.evalInSandbox("this.foo = {};", sandbox, null, "sandbox.js", 1);
+
+  const record = await tracker.stopRecordingAllocations("leaks");
+  is(
+    record.objectsWithStack,
+    1,
+    "We get only one leaked objects, the foo object of the sandbox."
+  );
+  ok(
+    record.objectsWithoutStack > 10,
+    "We get an handful of objects without stacks. Most likely created by Memory API itself."
+  );
+
+  is(
+    record.leaks.length,
+    2,
+    "We get the one leak and the objects with missing stacks"
+  );
+  is(
+    record.leaks[0].src,
+    "UNKNOWN",
+    "First item is the objects with missing stacks"
+  );
+  // In theory the two following values should be equal,
+  // but they aren't always because of some dark matter around objects with missing stacks.
+  // `count` is computed out of `takeCensus`, while `objectsWithoutStack` uses `drainAllocationsLog`
+  // While the first go through the current GC graph, the second is a record of allocations over time,
+  // this probably explain why there is some subtle difference
+  ok(
+    record.leaks[0].count <= record.objectsWithoutStack,
+    "For now, the leak report intermittently assume there is less leaked objects than the summary"
+  );
+  is(record.leaks[1].src, "sandbox.js", "Second item if about our 'foo' leak");
+  is(record.leaks[1].count, 1, "We leak one object on this file");
+  is(record.leaks[1].lines.length, 1, "We leak from only one line");
+  is(record.leaks[1].lines[0], "1: 1", "On first line, we leak one object");
+  tracker.stop();
+
+  TrackedObjects.clear();
+});
+
+add_task(async function() {
+  info("Test that transient globals are not leaked");
+
+  const tracker = allocationTracker({ watchAllGlobals: true });
+
+  let sandboxBefore = Cu.Sandbox(window);
+  // We need to allocate at least one object from the global to reproduce the leak
+  Cu.evalInSandbox(
+    "this.foo = {};",
+    sandboxBefore,
+    null,
+    "sandbox-before.js",
+    1
+  );
+  const weakBefore = Cu.getWeakReference(sandboxBefore);
+  sandboxBefore = null;
+
+  await tracker.startRecordingAllocations();
+
+  ok(
+    !weakBefore.get(),
+    "Sandbox created before the record should have been freed by GCs done by startRecordingAllocations"
+  );
+
+  let sandboxDuring = Cu.Sandbox(window);
+  // We need to allocate at least one object from the global to reproduce the leak
+  Cu.evalInSandbox(
+    "this.bar = {};",
+    sandboxDuring,
+    null,
+    "sandbox-during.js",
+    1
+  );
+  const weakDuring = Cu.getWeakReference(sandboxDuring);
+  sandboxDuring = null;
+
+  await tracker.stopRecordingAllocations();
+
+  ok(
+    !weakDuring.get(),
+    "Sandbox should have been freed by GCs done by stopRecordingAllocations"
+  );
+
+  tracker.stop();
 });
