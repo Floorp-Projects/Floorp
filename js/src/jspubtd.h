fc2f89ea22a9112c0adc803c6ebe6aa929b53629
Marian-Vasile Laza <mlaza@mozilla.com>
2021-10-14 19:16:45 +0300

    Backed out 10 changesets (bug 725966) for causing build bustages on CodeGenerator.cpp.
    Backed out changeset 1708b6a2373d (bug 725966)
    Backed out changeset d085abe7302d (bug 725966)
    Backed out changeset d90d0eeda9a7 (bug 725966)
    Backed out changeset fc925bdde132 (bug 725966)
    Backed out changeset 7659142f5ede (bug 725966)
    Backed out changeset 5508f47f0571 (bug 725966)
    Backed out changeset 25723de19680 (bug 725966)
    Backed out changeset 5d8a68d1f36b (bug 725966)
    Backed out changeset 1b847f9f911a (bug 725966)
    Backed out changeset 0c2849b874cb (bug 725966)

--- js/src/jspubtd.h
+++ js/src/jspubtd.h
@@ -38,6 +38,7 @@ enum JSType {
   JSTYPE_STRING,    /* string */
   JSTYPE_NUMBER,    /* number */
   JSTYPE_BOOLEAN,   /* boolean */
+  JSTYPE_NULL,      /* null */
   JSTYPE_SYMBOL,    /* symbol */
   JSTYPE_BIGINT,    /* BigInt */
   JSTYPE_LIMIT
--- js/src/vm/Interpreter.cpp
+++ js/src/vm/Interpreter.cpp
@@ -808,7 +808,6 @@ bool js::HasInstance(JSContext* cx, HandleObject obj, HandleValue v, bool* bp) {
 }
 
 JSType js::TypeOfObject(JSObject* obj) {
-  AutoUnsafeCallWithABI unsafe;
   if (EmulatesUndefined(obj)) {
     return JSTYPE_UNDEFINED;
   }
