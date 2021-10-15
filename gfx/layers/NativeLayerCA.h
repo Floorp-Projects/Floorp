1977fd01d3b52d8e170c3ab414bbd7da3e647272
Brad Werth <bwerth@mozilla.com>
2021-10-15 15:20:09 +0000

    Bug 1735420: Make video layers check pixel formats to determine eligibility for layer specialization. r=mstange, a=RyanVM
    This ensures that we only do the extra work of EnqueueSurface when the video
    format is eligible for the low power detached mode.
    
    Differential Revision: https://phabricator.services.mozilla.com/D128420

--- gfx/layers/NativeLayerCA.h
+++ gfx/layers/NativeLayerCA.h
@@ -327,6 +327,8 @@ class NativeLayerCA : public NativeLayer {
     // changed.
     bool HasUpdate();
 
+    bool CanSpecializeSurface(IOSurfaceRef surface);
+
     // Lazily initialized by first call to ApplyChanges. mWrappingLayer is the
     // layer that applies the intersection of mDisplayRect and mClipRect (if
     // set), and mContentCALayer is the layer that hosts the IOSurface. We do
