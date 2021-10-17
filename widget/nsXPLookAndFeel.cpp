5122357c497684e01c5bb2d4a9bf8be1fe97a413
Emilio Cobos Álvarez <emilio@crisal.io>
2021-10-17 15:32:02 +0200

    Bug 1736163 - layout.css.prefers-color-scheme.content-override should trigger a look and feel update.
    MANUAL PUSH: Trivial orange fix CLOSED TREE

--- widget/nsXPLookAndFeel.cpp
+++ widget/nsXPLookAndFeel.cpp
@@ -430,6 +430,7 @@ static constexpr struct {
     {"browser.display.windows.native_menus"_ns},
     {"browser.proton.enabled"_ns},
     {"browser.proton.places-tooltip.enabled"_ns},
+    {"layout.css.prefers-color-scheme.content-override"_ns},
     // This affects not only the media query, but also the native theme, so we
     // need to re-layout.
     {"browser.theme.toolbar-theme"_ns, widget::ThemeChangeKind::AllBits},
