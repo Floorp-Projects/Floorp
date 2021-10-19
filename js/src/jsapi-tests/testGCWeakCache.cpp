2adab4bb085d8823c0615c3f6cfa755d825aec62
Jon Coppeard <jcoppeard@mozilla.com>
2021-10-19 14:43:31 +0000

    Bug 1736397 - Part 4: Replace sweeping with tracing weak edges for WeakCached HashSets r=sfink
    Depends on D128858
    
    Differential Revision: https://phabricator.services.mozilla.com/D128859

--- js/src/jsapi-tests/testGCWeakCache.cpp
+++ js/src/jsapi-tests/testGCWeakCache.cpp
@@ -136,8 +136,9 @@ struct ObjectEntry {
   HeapPtr<JSObject*> obj;
   explicit ObjectEntry(JSObject* o) : obj(o) {}
   bool operator==(const ObjectEntry& other) const { return obj == other.obj; }
-  bool needsSweep() {
-    return JS::GCPolicy<HeapPtr<JSObject*>>::needsSweep(&obj);
+  bool needsSweep() { return IsAboutToBeFinalized(&obj); }
+  bool traceWeak(JSTracer* trc) {
+    return TraceWeakEdge(trc, &obj, "ObjectEntry::obj");
   }
 };
 
@@ -173,8 +174,9 @@ struct NumberAndObjectEntry {
   bool operator==(const NumberAndObjectEntry& other) const {
     return number == other.number && obj == other.obj;
   }
-  bool needsSweep() {
-    return JS::GCPolicy<HeapPtr<JSObject*>>::needsSweep(&obj);
+  bool needsSweep() { return IsAboutToBeFinalized(&obj); }
+  bool traceWeak(JSTracer* trc) {
+    return TraceWeakEdge(trc, &obj, "NumberAndObjectEntry::obj");
   }
 };
 
