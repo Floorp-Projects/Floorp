538c9487e396329a28265d963aabbdc34a1ba1a4
Mike Hommey <mh+mozilla@glandium.org>
2021-10-19 23:31:04 +0000

    Bug 1736477 - Remove unused import_rule::PendingSheet. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D128823

--- servo/components/style/stylesheets/import_rule.rs
+++ servo/components/style/stylesheets/import_rule.rs
@@ -6,27 +6,17 @@
 //!
 //! [import]: https://drafts.csswg.org/css-cascade-3/#at-import
 
-use crate::context::QuirksMode;
 use crate::media_queries::MediaList;
 use crate::shared_lock::{DeepCloneParams, DeepCloneWithLock};
 use crate::shared_lock::{SharedRwLock, SharedRwLockReadGuard, ToCssWithGuard};
 use crate::str::CssStringWriter;
-use crate::stylesheets::{CssRule, Origin, StylesheetInDocument};
+use crate::stylesheets::{CssRule, StylesheetInDocument};
 use crate::stylesheets::layer_rule::LayerName;
 use crate::values::CssUrl;
 use cssparser::SourceLocation;
 use std::fmt::{self, Write};
 use style_traits::{CssWriter, ToCss};
 use to_shmem::{self, SharedMemoryBuilder, ToShmem};
-
-/// With asynchronous stylesheet parsing, we can't synchronously create a
-/// GeckoStyleSheet. So we use this placeholder instead.
-#[cfg(feature = "gecko")]
-#[derive(Clone, Debug)]
-pub struct PendingSheet {
-    origin: Origin,
-    quirks_mode: QuirksMode,
-}
 
 /// A sheet that is held from an import rule.
 #[cfg(feature = "gecko")]
@@ -36,7 +26,7 @@ pub enum ImportSheet {
     Sheet(crate::gecko::data::GeckoStyleSheet),
     /// An @import created while parsing off-main-thread, whose Gecko sheet has
     /// yet to be created and attached.
-    Pending(PendingSheet),
+    Pending,
 }
 
 #[cfg(feature = "gecko")]
@@ -47,11 +37,8 @@ impl ImportSheet {
     }
 
     /// Creates a pending ImportSheet for a load that has not started yet.
-    pub fn new_pending(origin: Origin, quirks_mode: QuirksMode) -> Self {
-        ImportSheet::Pending(PendingSheet {
-            origin,
-            quirks_mode,
-        })
+    pub fn new_pending() -> Self {
+        ImportSheet::Pending
     }
 
     /// Returns a reference to the GeckoStyleSheet in this ImportSheet, if it
@@ -65,7 +52,7 @@ impl ImportSheet {
                 }
                 Some(s)
             },
-            ImportSheet::Pending(_) => None,
+            ImportSheet::Pending => None,
         }
     }
 
@@ -100,7 +87,7 @@ impl DeepCloneWithLock for ImportSheet {
                 };
                 ImportSheet::Sheet(unsafe { GeckoStyleSheet::from_addrefed(clone) })
             },
-            ImportSheet::Pending(ref p) => ImportSheet::Pending(p.clone()),
+            ImportSheet::Pending => ImportSheet::Pending,
         }
     }
 }
