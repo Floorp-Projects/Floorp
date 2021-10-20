5e145d9d5696505a6e7e8f65626c5ad36954abb0
Gerald Squelart <gsquelart@mozilla.com>
2021-10-20 02:24:08 +0000

    Bug 1736489 - Always output the last counter sample before each change - r=florian
    The makes the graphs much more correct:
    If there were multiple skipped samples, the front-end would graph a long diagonal line between the samples before and after the skipped ones, making it look like the value gradually changed during that time.
    Instead, by forcing the output of values before a change, this shows a better graph, with a horizontal line while the value didn't change, and a more abrupt diagonal line between the last two sample times when the change actually happened.
    
    Also always output the very last sample, to clarify the final value (in case it was the same as previous ones.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D128841

--- tools/profiler/core/ProfileBufferEntry.cpp
+++ tools/profiler/core/ProfileBufferEntry.cpp
@@ -1600,12 +1600,14 @@ void ProfileBuffer::StreamCountersToJSON(SpliceableJSONWriter& aWriter,
           }
 
           aWriter.StartArrayProperty("data");
+          double previousSkippedTime = 0.0;
           uint64_t previousNumber = 0;
           int64_t previousCount = 0;
           for (size_t i = 0; i < size; i++) {
-            // Encode as deltas, and only encode if different than the last
-            // sample
-            if (i == 0 || samples[i].mNumber != previousNumber ||
+            // Encode as deltas, and only encode if different than the previous
+            // or next sample; Always write the first and last samples.
+            if (i == 0 || i == size - 1 ||
+                samples[i].mNumber != previousNumber ||
                 samples[i].mCount != previousCount) {
               if (i != 0 && samples[i].mTime >= samples[i - 1].mTime) {
                 MOZ_LOG(sFuzzyfoxLog, mozilla::LogLevel::Error,
@@ -1617,14 +1619,35 @@ void ProfileBuffer::StreamCountersToJSON(SpliceableJSONWriter& aWriter,
               MOZ_ASSERT(samples[i].mNumber - previousNumber <=
                          uint64_t(std::numeric_limits<int64_t>::max()));
 
+              int64_t numberDelta =
+                  static_cast<int64_t>(samples[i].mNumber - previousNumber);
+              int64_t countDelta = samples[i].mCount - previousCount;
+
+              if (previousSkippedTime != 0.0 &&
+                  (numberDelta != 0 || countDelta != 0)) {
+                // Write the last skipped sample, unless the new one is all
+                // zeroes (that'd be redundant) This is useful to know when a
+                // certain value was last sampled, so that the front-end graph
+                // will be more correct.
+                AutoArraySchemaWriter writer(aWriter);
+                writer.TimeMsElement(TIME, previousSkippedTime);
+                // The deltas are effectively zeroes, since no change happened
+                // between the last actually-written sample and the last skipped
+                // one.
+                writer.IntElement(NUMBER, 0);
+                writer.IntElement(COUNT, 0);
+              }
+
               AutoArraySchemaWriter writer(aWriter);
               writer.TimeMsElement(TIME, samples[i].mTime);
-              writer.IntElement(
-                  NUMBER,
-                  static_cast<int64_t>(samples[i].mNumber - previousNumber));
-              writer.IntElement(COUNT, samples[i].mCount - previousCount);
+              writer.IntElement(NUMBER, numberDelta);
+              writer.IntElement(COUNT, countDelta);
+
+              previousSkippedTime = 0.0;
               previousNumber = samples[i].mNumber;
               previousCount = samples[i].mCount;
+            } else {
+              previousSkippedTime = samples[i].mTime;
             }
           }
           aWriter.EndArray();   // data
