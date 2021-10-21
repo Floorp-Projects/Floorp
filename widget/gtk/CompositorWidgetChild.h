779d33862cf045441363689c573664840ff1a2d2
stransky <stransky@redhat.com>
2021-10-21 15:41:01 +0000

    Bug 1737065 [Linux] Implement EnableRendering() / DisableRendering() for GtkCompositorWidget r=rmader
    When GdkWindow is unmapped (during reparent, when GtkWindow is hidden) we need to clear Window handle at GtkCompositorWidget as it's no longer valid.
    A new GdkWindow/Window is created when we're mapped again.
    
    Differential Revision: https://phabricator.services.mozilla.com/D129167

--- widget/gtk/CompositorWidgetChild.h
+++ widget/gtk/CompositorWidgetChild.h
@@ -27,6 +27,8 @@ class CompositorWidgetChild final : public PCompositorWidgetChild,
   mozilla::ipc::IPCResult RecvUnobserveVsync() override;
 
   void NotifyClientSizeChanged(const LayoutDeviceIntSize& aClientSize) override;
+  void DisableRendering() override;
+  void EnableRendering(const uintptr_t aXWindow, const bool aShaped) override;
 
  private:
   RefPtr<CompositorVsyncDispatcher> mVsyncDispatcher;
