4cfad2e62f8fa64eb07f641df4e73cf4a8a1858e
Julian Descottes <jdescottes@mozilla.com>
2021-10-21 13:58:56 +0000

    Bug 1736678 - Ensure tests within devtools/client/webconsole are https-first compliant r=nchevobbe
    Depends on D128866
    
    Differential Revision: https://phabricator.services.mozilla.com/D128931

--- devtools/client/webconsole/test/browser/browser_webconsole_websocket.js
+++ devtools/client/webconsole/test/browser/browser_webconsole_websocket.js
@@ -6,18 +6,18 @@
 // Check that WebSocket connection failure messages are displayed. See Bug 603750.
 
 const TEST_URI =
-  "http://example.com/browser/devtools/client/webconsole/test/browser/test-websocket.html";
+  "https://example.com/browser/devtools/client/webconsole/test/browser/test-websocket.html";
 
 add_task(async function() {
   const hud = await openNewTabAndConsole(TEST_URI);
   await waitFor(
-    () => findMessage(hud, "ws://0.0.0.0:81", ".error"),
-    "Did not find error message for ws://0.0.0.0:81 connection",
+    () => findMessage(hud, "wss://0.0.0.0:81", ".error"),
+    "Did not find error message for wss://0.0.0.0:81 connection",
     500
   );
   await waitFor(
-    () => findMessage(hud, "ws://0.0.0.0:82", ".error"),
-    "Did not find error message for ws://0.0.0.0:82 connection",
+    () => findMessage(hud, "wss://0.0.0.0:82", ".error"),
+    "Did not find error message for wss://0.0.0.0:82 connection",
     500
   );
   ok(true, "WebSocket error messages are displayed in the console");
