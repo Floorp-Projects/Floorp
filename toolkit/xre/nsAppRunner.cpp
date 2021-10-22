6bc7261a9b9c183997f0b35c4db38d46e22db647
Jan-Erik Rediger <jrediger@mozilla.com>
2021-10-22 08:53:29 +0000

    Bug 1732928 - Mirror gecko.{version, build_id} metrics from EXTRACT to native Glean. r=chutten,jrmuizel
    With Glean enabled on Android we can move metrics from Geckoview Streaming[1]
    into proper native Glean metrics.
    For now we introduce new expiring metrics to allow validation with real data.
    We rely on GIFFT to mirror these back into legacy Telemetry probes, from
    where they will be pushed down the EXTRACT delegate.
    In analysis we will be able to check that the values match as expected.
    
    [1]: https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/start/report-gecko-telemetry-in-glean.html
    
    Differential Revision: https://phabricator.services.mozilla.com/D126801

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -29,6 +29,7 @@
 #include "mozilla/Utf8.h"
 #include "mozilla/intl/LocaleService.h"
 #include "mozilla/JSONWriter.h"
+#include "mozilla/glean/GleanMetrics.h"
 #include "BaseProfiler.h"
 
 #include "nsAppRunner.h"
@@ -5219,10 +5220,10 @@ nsresult XREMain::XRE_mainRun() {
 #endif /* MOZ_INSTRUMENT_EVENT_LOOP */
 
     // Send Telemetry about Gecko version and buildid
-    Telemetry::ScalarSet(Telemetry::ScalarID::GECKO_VERSION,
-                         NS_ConvertASCIItoUTF16(gAppData->version));
-    Telemetry::ScalarSet(Telemetry::ScalarID::GECKO_BUILD_ID,
-                         NS_ConvertASCIItoUTF16(gAppData->buildID));
+    nsAutoCString version(gAppData->version);
+    nsAutoCString buildID(gAppData->buildID);
+    mozilla::glean::geckoview_validation::version.Set(version);
+    mozilla::glean::geckoview_validation::build_id.Set(buildID);
 
 #if defined(MOZ_SANDBOX) && defined(XP_LINUX)
     // If we're on Linux, we now have information about the OS capabilities
