e2a3bd0a22885e9a9fc595de5d91b834c15e334a
Peter Van der Beken <peterv@propagandism.org>
2021-10-22 15:00:42 +0000

    Bug 1729329 - Convert nsCOMArrays in XSLT code to nsTArrays and remove impossible to hit OOM errors. r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D124680

--- dom/xslt/xpath/XPathResult.h
+++ dom/xslt/xpath/XPathResult.h
@@ -8,9 +8,9 @@
 
 #include "nsStubMutationObserver.h"
 #include "nsCOMPtr.h"
-#include "nsCOMArray.h"
-#include "nsIWeakReferenceUtils.h"
 #include "nsCycleCollectionParticipant.h"
+#include "nsIWeakReferenceUtils.h"
+#include "nsTArray.h"
 #include "mozilla/Attributes.h"
 #include "mozilla/ErrorResult.h"
 #include "nsString.h"
@@ -110,7 +110,7 @@ class XPathResult final : public nsIXPathResult,
       return nullptr;
     }
 
-    return mResultNodes.SafeObjectAt(0);
+    return mResultNodes.SafeElementAt(0);
   }
   bool InvalidIteratorState() const {
     return isIterator() && mInvalidIteratorState;
@@ -121,7 +121,7 @@ class XPathResult final : public nsIXPathResult,
       return 0;
     }
 
-    return (uint32_t)mResultNodes.Count();
+    return (uint32_t)mResultNodes.Length();
   }
   nsINode* IterateNext(ErrorResult& aRv);
   nsINode* SnapshotItem(uint32_t aIndex, ErrorResult& aRv) const {
@@ -130,7 +130,7 @@ class XPathResult final : public nsIXPathResult,
       return nullptr;
     }
 
-    return mResultNodes.SafeObjectAt(aIndex);
+    return mResultNodes.SafeElementAt(aIndex);
   }
 
   // nsIMutationObserver interface
@@ -168,7 +168,7 @@ class XPathResult final : public nsIXPathResult,
 
   nsCOMPtr<nsINode> mParent;
   RefPtr<txAExprResult> mResult;
-  nsCOMArray<nsINode> mResultNodes;
+  nsTArray<nsCOMPtr<nsINode>> mResultNodes;
   RefPtr<Document> mDocument;
   nsWeakPtr mContextNode;
   uint32_t mCurrentPos;
