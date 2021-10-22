b25940945ed0800968cc059423f0961b3d637e73
Michael Comella <michael.l.comella@gmail.com>
2021-10-22 12:22:34 -0700

    [components] For https://github.com/mozilla-mobile/android-components/issues/11161: add marker with duration.
    Like the previous commit, the upstream consumer needs to pass in a
    Profiler for this to work.

--- mobile/android/android-components/components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/internal/SuggestionFetcher.kt
+++ mobile/android/android-components/components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/internal/SuggestionFetcher.kt
@@ -16,6 +16,7 @@ import mozilla.components.concept.awesomebar.AwesomeBar
 import mozilla.components.concept.base.profiler.Profiler
 import mozilla.components.support.base.facts.Action
 import mozilla.components.support.base.utils.NamedThreadFactory
+import mozilla.components.support.utils.ThreadUtils
 import java.util.concurrent.Executors
 
 /**
@@ -47,7 +48,8 @@ internal class SuggestionFetcher(
         coroutineScope {
             groups.forEach { group ->
                 group.providers.forEach { provider ->
-                    launch(dispatcher) { fetchFrom(group, provider, text) }
+                    val profilerStartTime = profiler?.getProfilerTime() // DO NOT ADD ANYTHING ABOVE getProfilerTime.
+                    launch(dispatcher) { fetchFrom(group, provider, text, profilerStartTime) }
                 }
             }
         }
@@ -59,7 +61,8 @@ internal class SuggestionFetcher(
     private suspend fun fetchFrom(
         group: AwesomeBar.SuggestionProviderGroup,
         provider: AwesomeBar.SuggestionProvider,
-        text: String
+        text: String,
+        profilerStartTime: Double?,
     ) {
         // At this point, we have a timing value for a provider.
         // We have a choice here - we can try grouping different timings together for a
@@ -80,7 +83,7 @@ internal class SuggestionFetcher(
         val end = SystemClock.elapsedRealtimeNanos()
         emitProviderQueryTimingFact(provider, timingNs = end - start)
 
-        processResultFrom(group, provider, suggestions)
+        processResultFrom(group, provider, suggestions, profilerStartTime)
     }
 
     /**
@@ -90,7 +93,8 @@ internal class SuggestionFetcher(
     private fun processResultFrom(
         group: AwesomeBar.SuggestionProviderGroup,
         provider: AwesomeBar.SuggestionProvider,
-        suggestions: List<AwesomeBar.Suggestion>
+        suggestions: List<AwesomeBar.Suggestion>,
+        profilerStartTime: Double?,
     ) {
         val suggestionMap = state.value
 
@@ -104,6 +108,12 @@ internal class SuggestionFetcher(
         val updatedSuggestionMap = suggestionMap.toMutableMap()
         updatedSuggestionMap[group] = updatedSuggestions
         state.value = updatedSuggestionMap
+        val profilerEndTime = profiler?.getProfilerTime() // THIS MUST OCCUR RIGHT AFTER STATE UPDATE.
+
+        // Markers can only be added on the main thread right now.
+        ThreadUtils.postToMainThread {
+            profiler?.addMarker("Suggestion update", profilerStartTime, profilerEndTime, provider::class.simpleName)
+        }
     }
 
     override fun onAbandoned() {
