18e7fd5ca4d8b55c9b0760c5e0b125d45838a343
Hubert Boma Manilla <hmanilla@mozilla.com>
2021-10-25 13:57:44 +0000

    Bug 1735024 - [devtools] Fix the leaks from firefox data provider r=ochameau
    Fixes the leaks of the cached stacktrace info in stackTraceInfoByActorID.
    Prior to this fix, the stacktrace info in the cache is only removed when the stacktrace
    details panel is opened, meaning if the details panel is never opened the
    info is never cleared and leaks.
    This fixes the netmonitor and the webconsole as they both use the data provider
    
    ### Leak Info
    
    ####  Netmonitor
    Before: 151 leaked objects (14611 with missing allocation site) in the parent process
    After: -111 leaked objects (14150 with missing allocation site) in the parent process
    
    #### Webconsole
    Before: 271 leaked objects (13996 with missing allocation site) in the parent process
    After: 1 leaked objects (13810 with missing allocation site) in the parent process
    
    Differential Revision: https://phabricator.services.mozilla.com/D128015

--- devtools/client/webconsole/enhancers/message-cache-clearing.js
+++ devtools/client/webconsole/enhancers/message-cache-clearing.js
@@ -18,6 +18,10 @@ function enableMessagesCacheClearing(webConsoleUI) {
 
       if (webConsoleUI && action.type === MESSAGES_CLEAR) {
         webConsoleUI.clearMessagesCache();
+
+        // cleans up all the network data provider internal state
+        webConsoleUI.networkDataProvider?.destroy();
+
         if (webConsoleUI.hud?.toolbox) {
           webConsoleUI.hud.toolbox.setErrorCount(0);
         }
