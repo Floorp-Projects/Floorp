77233779938d87395de273895a608877e20cbbc4
Sean Feng <sefeng@mozilla.com>
2021-10-25 19:41:15 +0000

    Bug 1728864 - Focus the first focusable area with autofocus for delegates focus r=smaug
    Spec: https://github.com/whatwg/html/pull/6990
    
    Differential Revision: https://phabricator.services.mozilla.com/D128301

--- dom/base/ShadowRoot.cpp
+++ dom/base/ShadowRoot.cpp
@@ -756,6 +756,8 @@ void ShadowRoot::MaybeUnslotHostChild(nsIContent& aChild) {
 Element* ShadowRoot::GetFirstFocusable(bool aWithMouse) const {
   MOZ_ASSERT(DelegatesFocus(), "Why are we here?");
 
+  Element* potentialFocus = nullptr;
+
   for (nsINode* node = GetFirstChild(); node; node = node->GetNextNode(this)) {
     auto* el = Element::FromNode(*node);
     if (!el) {
@@ -763,22 +765,30 @@ Element* ShadowRoot::GetFirstFocusable(bool aWithMouse) const {
     }
     nsIFrame* frame = el->GetPrimaryFrame();
     if (frame && frame->IsFocusable(aWithMouse)) {
-      return el;
+      if (el->GetBoolAttr(nsGkAtoms::autofocus)) {
+        return el;
+      }
+      if (!potentialFocus) {
+        potentialFocus = el;
+      }
     }
-    ShadowRoot* shadow = el->GetShadowRoot();
-    if (shadow && shadow->DelegatesFocus()) {
-      if (Element* nested = shadow->GetFirstFocusable(aWithMouse)) {
-        return nested;
+    if (!potentialFocus) {
+      ShadowRoot* shadow = el->GetShadowRoot();
+      if (shadow && shadow->DelegatesFocus()) {
+        if (Element* nested = shadow->GetFirstFocusable(aWithMouse)) {
+          potentialFocus = nested;
+        }
       }
     }
   }
-  return nullptr;
+  return potentialFocus;
 }
 
 void ShadowRoot::MaybeSlotHostChild(nsIContent& aChild) {
   MOZ_ASSERT(aChild.GetParent() == GetHost());
   // Check to ensure that the child not an anonymous subtree root because even
-  // though its parent could be the host it may not be in the host's child list.
+  // though its parent could be the host it may not be in the host's child
+  // list.
   if (aChild.IsRootOfNativeAnonymousSubtree()) {
     return;
   }
