5ac2b54c2973fb541fcb68e0447c485101c33f4a
Jonathan Kew <jkew@mozilla.com>
2021-10-26 16:56:33 +0000

    Bug 1736868 - Pause the word-cache expiration timer when all caches are empty. r=lsalzman
    This avoids periodically waking up an otherwise-idle process once we've expired all the
    cached-word entries.
    
    Also reduce the font and shaped-word expiration times somewhat, so that processes will tend to
    become idle sooner. In practice if we don't reuse a given record within seconds, it's probably
    not critical to performance; the caches are primarily there for when the same font is used for
    many elements of a page, or a page is reflowed several times during load.
    
    Differential Revision: https://phabricator.services.mozilla.com/D129524

--- gfx/thebes/gfxFont.h
+++ gfx/thebes/gfxFont.h
@@ -309,7 +309,7 @@ class gfxFontCacheExpirationTracker
   }
 
  public:
-  enum { FONT_TIMEOUT_SECONDS = 10 };
+  enum { FONT_TIMEOUT_SECONDS = 5 };
 
   explicit gfxFontCacheExpirationTracker(nsIEventTarget* aEventTarget)
       : ExpirationTrackerImpl<gfxFont, 3, Lock, AutoLock>(
@@ -318,7 +318,7 @@ class gfxFontCacheExpirationTracker
 
 class gfxFontCache final : private gfxFontCacheExpirationTracker {
  public:
-  enum { SHAPED_WORD_TIMEOUT_SECONDS = 60 };
+  enum { SHAPED_WORD_TIMEOUT_SECONDS = 10 };
 
   explicit gfxFontCache(nsIEventTarget* aEventTarget);
   ~gfxFontCache();
@@ -361,6 +361,22 @@ class gfxFontCache final : private gfxFontCacheExpirationTracker {
 
   void FlushShapedWordCaches();
   void NotifyGlyphsChanged();
+
+  void RunWordCacheExpirationTimer() {
+    if (mWordCacheExpirationTimer && !mTimerRunning) {
+      mWordCacheExpirationTimer->InitWithNamedFuncCallback(
+          WordCacheExpirationTimerCallback, this,
+          SHAPED_WORD_TIMEOUT_SECONDS * 1000, nsITimer::TYPE_REPEATING_SLACK,
+          "gfxFontCache::WordCacheExpiration");
+      mTimerRunning = true;
+    }
+  }
+  void PauseWordCacheExpirationTimer() {
+    if (mWordCacheExpirationTimer && mTimerRunning) {
+      mWordCacheExpirationTimer->Cancel();
+      mTimerRunning = false;
+    }
+  }
 
   void AddSizeOfExcludingThis(mozilla::MallocSizeOf aMallocSizeOf,
                               FontCacheSizes* aSizes) const;
@@ -444,6 +460,7 @@ class gfxFontCache final : private gfxFontCacheExpirationTracker {
 
   static void WordCacheExpirationTimerCallback(nsITimer* aTimer, void* aCache);
   nsCOMPtr<nsITimer> mWordCacheExpirationTimer;
+  bool mTimerRunning = false;
 };
 
 class gfxTextPerfMetrics {
@@ -1841,8 +1858,9 @@ class gfxFont {
   }
 
   // Called by the gfxFontCache timer to increment the age of all the words,
-  // so that they'll expire after a sufficient period of non-use
-  void AgeCachedWords();
+  // so that they'll expire after a sufficient period of non-use.
+  // Returns true if the cache is now empty, otherwise false.
+  bool AgeCachedWords();
 
   // Discard all cached word records; called on memory-pressure notification.
   void ClearCachedWords() {
