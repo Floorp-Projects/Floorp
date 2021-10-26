0ff4fbc8b13a51b21f5246fe50dad2a3809783da
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-10-26 17:57:32 +0000

    Bug 1737571 - [devtools] Fix browser_resources_stylesheets_navigation.js when EFT is enabled. r=bomsy.
    The test had comments and disabled assertions to work around Bug 1725547.
    When EFT is enabled, the issue is fixed, so the test was failing.
    This patch modifies the tests to add some branches when EFT is enabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D129386

--- devtools/shared/commands/resource/tests/browser_resources_stylesheets_navigation.js
+++ devtools/shared/commands/resource/tests/browser_resources_stylesheets_navigation.js
@@ -115,16 +115,18 @@ add_task(async function() {
   info("Check that styleSheetChangeEventsEnabled persist after reloading");
   await reloadBrowser();
 
-  // ⚠️ We're only getting the stylesheets for the top-level document and the remote frames;
-  // the same-origin iframes stylesheets are missing.
-  // This should be fixed in Bug 1725547.
+  // ⚠️ When EFT is disabled, we're only getting the stylesheets for the top-level document
+  // and the remote frames; the same-origin iframes stylesheets are missing.
+  const expectedStylesheetResources = isEveryFrameTargetEnabled() ? 5 : 3;
   info(
     "Wait until we're notified about all the stylesheets (top-level document + iframe)"
   );
-  await waitFor(() => availableResources.length === 3);
+  await waitFor(
+    () => availableResources.length === expectedStylesheetResources
+  );
   is(
     availableResources.length,
-    3,
+    expectedStylesheetResources,
     "Retrieved the expected stylesheets after the page was reloaded"
   );
 
@@ -133,19 +135,27 @@ add_task(async function() {
   await assertResource(availableResources[0], {
     styleText: `.top-level-org{}`,
   });
-  // This should be uncommented as part of Bug 1725547.
-  // await assertResource(availableResources[1], {
-  //   styleText: `.frame-org-1{}`,
-  // });
-  // await assertResource(availableResources[2], {
-  //   styleText: `.frame-org-2{}`,
-  // });
-  await assertResource(availableResources[1], {
-    styleText: `.frame-com-1{}`,
-  });
-  await assertResource(availableResources[2], {
-    styleText: `.frame-com-new-bc{}`,
-  });
+  if (isEveryFrameTargetEnabled()) {
+    await assertResource(availableResources[1], {
+      styleText: `.frame-org-1{}`,
+    });
+    await assertResource(availableResources[2], {
+      styleText: `.frame-org-2{}`,
+    });
+    await assertResource(availableResources[3], {
+      styleText: `.frame-com-1{}`,
+    });
+    await assertResource(availableResources[4], {
+      styleText: `.frame-com-new-bc{}`,
+    });
+  } else {
+    await assertResource(availableResources[1], {
+      styleText: `.frame-com-1{}`,
+    });
+    await assertResource(availableResources[2], {
+      styleText: `.frame-com-new-bc{}`,
+    });
+  }
 
   is(
     await getDocumentStyleSheetChangeEventsEnabled(tab.linkedBrowser),
@@ -153,17 +163,18 @@ add_task(async function() {
     `styleSheetChangeEventsEnabled is still true on the top level document after reloading`
   );
 
-  // This should be uncommented as part of Bug 1725547.
-  // const bc = await SpecialPowers.spawn(
-  //   tab.linkedBrowser,
-  //   [],
-  //   () => content.document.querySelector("#same-origin-1").browsingContext
-  // );
-  // is(
-  //   await getDocumentStyleSheetChangeEventsEnabled(bc),
-  //   true,
-  //   `styleSheetChangeEventsEnabled is still true on the iframe after reloading`
-  // );
+  if (isEveryFrameTargetEnabled()) {
+    const bc = await SpecialPowers.spawn(
+      tab.linkedBrowser,
+      [],
+      () => content.document.querySelector("#same-origin-1").browsingContext
+    );
+    is(
+      await getDocumentStyleSheetChangeEventsEnabled(bc),
+      true,
+      `styleSheetChangeEventsEnabled is still true on the iframe after reloading`
+    );
+  }
 
   // clear availableResources so it's easier to test
   availableResources = [];
