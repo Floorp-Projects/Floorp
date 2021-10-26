2f26670584a8d2371b4265a51e1ce9813ab27496
André Bargull <andre.bargull@gmail.com>
2021-10-26 07:21:13 +0000

    Bug 1736805: Register written normalised length. r=gregtatum, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D129428

--- intl/components/src/String.h
+++ intl/components/src/String.h
@@ -150,6 +150,8 @@ class String final {
     // Copy the already normalized prefix.
     if (spanLength > 0) {
       PodCopy(aBuffer.data(), aString.data(), spanLength);
+
+      aBuffer.written(spanLength);
     }
 
     MOZ_TRY(FillBufferWithICUCall(
