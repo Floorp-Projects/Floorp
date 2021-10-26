ca100c5d7ad47e3c4e12162b4319c0cd736979b5
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-10-26 17:57:33 +0000

    Bug 1737756 - [devtools] Fix browser_inspector_highlighter-selector_02.js when EFT is enabled. r=bomsy.
    This patch ensures that we are using the target-dedicated highlighterTestFront to
    run some assertion on highlighters.
    
    Depends on D129481
    
    Differential Revision: https://phabricator.services.mozilla.com/D129483

--- devtools/client/inspector/test/browser_inspector_highlighter-selector_02.js
+++ devtools/client/inspector/test/browser_inspector_highlighter-selector_02.js
@@ -41,11 +41,7 @@ const TEST_DATA = [
 requestLongerTimeout(5);
 
 add_task(async function() {
-  const { inspector, highlighterTestFront } = await openInspectorForURL(
-    TEST_URL
-  );
-  const front = inspector.inspectorFront;
-  const highlighter = await front.getHighlighterByType("SelectorHighlighter");
+  const { inspector } = await openInspectorForURL(TEST_URL);
 
   for (const { inIframe, selector, containerCount } of TEST_DATA) {
     info(
@@ -63,6 +59,17 @@ add_task(async function() {
       contextNode = await getNodeFront("body", inspector);
     }
 
+    const inspectorFront = await contextNode.targetFront.getFront("inspector");
+    const highlighter = await inspectorFront.getHighlighterByType(
+      "SelectorHighlighter"
+    );
+    const highlighterTestFront = await getHighlighterTestFront(
+      inspector.toolbox,
+      {
+        target: contextNode.targetFront,
+      }
+    );
+
     await highlighter.show(contextNode, { selector });
 
     const nb = await highlighterTestFront.getSelectorHighlighterBoxNb(
@@ -72,7 +79,6 @@ add_task(async function() {
 
     is(nb, containerCount, "The correct number of highlighers were created");
     await highlighter.hide();
+    await highlighter.finalize();
   }
-
-  await highlighter.finalize();
 });
