42c21527d839350422e751f5bd4c1f78964ffae6
shravanrn@gmail.com <shravanrn@gmail.com>
2021-10-27 00:55:47 +0000

    Bug 1737718 - Configure wasm2c runtime to redirect crashes to MOZ_CRASH r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D129461

--- config/external/rlbox_wasm2c_sandbox/rlbox_wasm2c_thread_locals.cpp
+++ config/external/rlbox_wasm2c_sandbox/rlbox_wasm2c_thread_locals.cpp
@@ -5,6 +5,9 @@
 
 #ifdef MOZ_USING_WASM_SANDBOXING
 
+// For MOZ_CRASH_UNSAFE_PRINTF
+#  include "mozilla/Assertions.h"
+
 // Load general firefox configuration of RLBox
 #  include "mozilla/rlbox/rlbox_config.h"
 
@@ -16,5 +19,14 @@
 // members. We instead TLS storage via functions. This can be removed if the
 // MingW bug is fixed.
 RLBOX_WASM2C_SANDBOX_STATIC_VARIABLES();
+
+extern "C" {
+
+// Any error encountered by the wasm2c runtime or wasm sandboxed library code
+// is configured to call the below trap handler.
+void moz_wasm2c_trap_handler(const char* msg) {
+  MOZ_CRASH_UNSAFE_PRINTF("wasm2c crash: %s", msg);
+}
+}
 
 #endif
