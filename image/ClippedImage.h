12c304fa47d4d630cb065737cf41e89f34adb6ba
Andrew Osmond <aosmond@mozilla.com>
2021-10-27 01:24:31 +0000

    Bug 1711061 - Part 2. Add imgIContainer::GetImageProvider skeleton. r=tnikkel
    In later parts in this series, GetImageProvider will replace
    GetImageContainerAtSize. This will be a more specialized and lower
    overhead means to get a wr::ImageKey for a particular surface.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126596

--- image/ClippedImage.h
+++ image/ClippedImage.h
@@ -51,6 +51,11 @@ class ClippedImage : public ImageWrapper {
                           const Maybe<ImageIntRegion>& aRegion, uint32_t aFlags,
                           layers::ImageContainer** aOutContainer) override;
   NS_IMETHOD_(ImgDrawResult)
+  GetImageProvider(WindowRenderer* aRenderer, const gfx::IntSize& aSize,
+                   const Maybe<SVGImageContext>& aSVGContext,
+                   const Maybe<ImageIntRegion>& aRegion, uint32_t aFlags,
+                   WebRenderImageProvider** aProvider) override;
+  NS_IMETHOD_(ImgDrawResult)
   Draw(gfxContext* aContext, const nsIntSize& aSize, const ImageRegion& aRegion,
        uint32_t aWhichFrame, gfx::SamplingFilter aSamplingFilter,
        const Maybe<SVGImageContext>& aSVGContext, uint32_t aFlags,
