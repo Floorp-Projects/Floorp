6a67c931b0beb1fe62759c7742c1de1c713cbfda
Nika Layzell <nika@thelayzells.com>
2021-10-28 22:25:28 +0000

    Bug 1731597 - Part 1: Generalize nsBrowsingContextReadyCallback, r=annyG
    Differential Revision: https://phabricator.services.mozilla.com/D126993

--- toolkit/components/windowwatcher/nsOpenWindowInfo.cpp
+++ toolkit/components/windowwatcher/nsOpenWindowInfo.cpp
@@ -8,6 +8,7 @@
 #include "mozilla/OriginAttributes.h"
 #include "mozilla/dom/ToJSValue.h"
 #include "mozilla/dom/BrowserParent.h"
+#include "nsThreadUtils.h"
 
 NS_IMPL_ISUPPORTS(nsOpenWindowInfo, nsIOpenWindowInfo)
 
@@ -44,11 +45,11 @@ NS_IMETHODIMP nsOpenWindowInfo::GetScriptableOriginAttributes(
   return NS_OK;
 }
 
-const OriginAttributes& nsOpenWindowInfo::GetOriginAttributes() {
+const mozilla::OriginAttributes& nsOpenWindowInfo::GetOriginAttributes() {
   return mOriginAttributes;
 }
 
-BrowserParent* nsOpenWindowInfo::GetNextRemoteBrowser() {
+mozilla::dom::BrowserParent* nsOpenWindowInfo::GetNextRemoteBrowser() {
   return mNextRemoteBrowser;
 }
 
@@ -57,32 +58,48 @@ nsOpenWindowInfo::BrowsingContextReadyCallback() {
   return mBrowsingContextReadyCallback;
 }
 
+namespace {
+
+class nsBrowsingContextReadyCallback final
+    : public nsIBrowsingContextReadyCallback {
+ public:
+  NS_DECL_ISUPPORTS
+
+  explicit nsBrowsingContextReadyCallback(
+      std::function<void(mozilla::dom::BrowsingContext*)>&& aCallback)
+      : mCallback(aCallback) {}
+
+  NS_IMETHOD BrowsingContextReady(mozilla::dom::BrowsingContext* aBC) override {
+    if (!mCallback) {
+      return NS_OK;
+    }
+    // Spin the event loop to make sure everything's been initialized before we
+    // invoke the callback.
+    NS_DispatchToMainThread(NS_NewRunnableFunction(
+        "BrowsingContextReadyCallback",
+        [callback = std::move(mCallback), bc = RefPtr{aBC}] { callback(bc); }));
+    mCallback = nullptr;
+    return NS_OK;
+  }
+
+ private:
+  ~nsBrowsingContextReadyCallback() { BrowsingContextReady(nullptr); }
+
+  std::function<void(mozilla::dom::BrowsingContext*)> mCallback;
+};
+
 NS_IMPL_ISUPPORTS(nsBrowsingContextReadyCallback,
                   nsIBrowsingContextReadyCallback)
 
-nsBrowsingContextReadyCallback::nsBrowsingContextReadyCallback(
-    RefPtr<BrowsingContextCallbackReceivedPromise::Private> aPromise)
-    : mPromise(std::move(aPromise)) {}
+}  // namespace
 
-nsBrowsingContextReadyCallback::~nsBrowsingContextReadyCallback() {
-  if (mPromise) {
-    mPromise->Reject(NS_ERROR_FAILURE, __func__);
+nsresult nsOpenWindowInfo::OnBrowsingContextReady(
+    std::function<void(mozilla::dom::BrowsingContext*)>&& aCallback) {
+  if (mBrowsingContextReadyCallback) {
+    return NS_ERROR_ALREADY_INITIALIZED;
   }
-  mPromise = nullptr;
-}
-
-NS_IMETHODIMP nsBrowsingContextReadyCallback::BrowsingContextReady(
-    BrowsingContext* aBC) {
-  MOZ_DIAGNOSTIC_ASSERT(mPromise,
-                        "The 'browsing context ready' callback is null");
-  if (!mPromise) {
-    return NS_OK;
-  }
-  if (aBC) {
-    mPromise->Resolve(aBC, __func__);
-  } else {
-    mPromise->Reject(NS_ERROR_FAILURE, __func__);
-  }
-  mPromise = nullptr;
+  mBrowsingContextReadyCallback =
+      mozilla::MakeAndAddRef<nsBrowsingContextReadyCallback>(
+          std::move(aCallback));
   return NS_OK;
 }
