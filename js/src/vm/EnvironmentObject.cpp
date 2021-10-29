f154df7337f8f19526fa6d883d9ba0ab0ca3ac5d
Jan de Mooij <jdemooij@mozilla.com>
2021-10-29 18:19:16 +0000

    Bug 1738020 part 2 - Remove stale comment, fix assertion. r=tcampbell
    This likely hasn't been true for quite some time.
    
    Depends on D129628
    
    Differential Revision: https://phabricator.services.mozilla.com/D129629

--- js/src/vm/EnvironmentObject.cpp
+++ js/src/vm/EnvironmentObject.cpp
@@ -1015,9 +1015,8 @@ BlockLexicalEnvironmentObject* BlockLexicalEnvironmentObject::clone(
     return nullptr;
   }
 
-  // We can't assert that the clone has the same shape, because it could
-  // have been reshaped by ReshapeForShadowedProp.
-  MOZ_ASSERT(env->slotSpan() == copy->slotSpan());
+  MOZ_ASSERT(env->shape() == copy->shape());
+
   for (uint32_t i = JSSLOT_FREE(&class_); i < copy->slotSpan(); i++) {
     copy->setSlot(i, env->getSlot(i));
   }
