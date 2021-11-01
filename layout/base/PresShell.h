6ef7050581e45aecd641f3773fae34468e645af8
Emilio Cobos Álvarez <emilio@crisal.io>
2021-11-01 19:29:01 +0000

    Bug 1719150 - Push delayed mouse event to the right pres shell. r=smaug a=pascalc
    Otherwise if we get an event targeting a different suppressed pres shell
    while we're unsuppressing we might get stuck delaying the event over and
    over, see the links in comment 33.
    
    Not sure how to get a repro for this, the patch is written based on the
    pernosco session, but suggestions welcome.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130100

--- layout/base/PresShell.h
+++ layout/base/PresShell.h
@@ -2742,10 +2742,6 @@ class PresShell final : public nsStubDocumentObserver,
     already_AddRefed<PresShell> GetParentPresShellForEventHandling() {
       return mPresShell->GetParentPresShellForEventHandling();
     }
-    void PushDelayedEventIntoQueue(UniquePtr<DelayedEvent>&& aDelayedEvent) {
-      mPresShell->mDelayedEvents.AppendElement(std::move(aDelayedEvent));
-    }
-
     OwningNonNull<PresShell> mPresShell;
     AutoCurrentEventInfoSetter* mCurrentEventInfoSetter;
     static TimeStamp sLastInputCreated;
