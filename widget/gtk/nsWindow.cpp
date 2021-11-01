333dc73d8c002a2afbbebaab9b25372d011c6a7c
stransky <stransky@redhat.com>
2021-11-01 17:30:19 +0000

    Bug 1738639 [Wayland] Don't disable draw update for D&D widgets, r=jhorak
    Differential Revision: https://phabricator.services.mozilla.com/D130092

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -5134,9 +5134,6 @@ nsCString nsWindow::GetPopupTypeName() {
 // We do our best to persuade Gtk/Gdk to ignore all painting
 // to the widget.
 static void GtkWidgetDisableUpdates(GtkWidget* aWidget) {
-  // clear draw signal from widget class
-  GTK_WIDGET_GET_CLASS(aWidget)->draw = nullptr;
-
   // Clear exposure mask - it disabled synthesized events.
   GdkWindow* window = gtk_widget_get_window(aWidget);
   gdk_window_set_events(window, (GdkEventMask)(gdk_window_get_events(window) &
@@ -5631,6 +5628,7 @@ nsresult nsWindow::Create(nsIWidget* aParent, nsNativeWidget aNativeParent,
 
 #ifdef MOZ_WAYLAND
       if (mIsDragPopup && GdkIsWaylandDisplay()) {
+        LOG("  set commit to parent");
         moz_container_wayland_set_commit_to_parent(mContainer);
       }
 #endif
@@ -8370,11 +8368,13 @@ void nsWindow::SetDrawsInTitlebar(bool aState) {
 
   if (mIsPIPWindow && aState == mDrawInTitlebar) {
     gtk_window_set_decorated(GTK_WINDOW(mShell), !aState);
+    LOG("  set decoration for PIP %d", aState);
     return;
   }
 
-  if (!mShell || mGtkWindowDecoration == GTK_DECORATION_NONE ||
+  if (mGtkWindowDecoration == GTK_DECORATION_NONE ||
       aState == mDrawInTitlebar) {
+    LOG("  already set, quit");
     return;
   }
 
