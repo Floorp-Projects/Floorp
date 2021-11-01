b694877a911edeef76291030971707c147f32d32
OnuohaOluebube <onuohaoluebube05@gmail.com>
2021-11-01 16:56:25 +0000

    Bug 1460100 - Added ESLint rule to prevent use of nsIScriptableUnicodeConverter r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D129668

---
+++ tools/lint/eslint/eslint-plugin-mozilla/tests/reject-scriptableunicodeconverter.js
@@ -0,0 +1,33 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+var rule = require("../lib/rules/reject-scriptableunicodeconverter");
+var RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 8 } });
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+function invalidError() {
+  let message =
+    "Ci.nsIScriptableUnicodeConverter is deprecated. You should use TextEncoder or TextDecoder instead.";
+  return [{ message, type: "MemberExpression" }];
+}
+
+ruleTester.run("reject-scriptableunicodeconverter", rule, {
+  valid: ["TextEncoder", "TextDecoder"],
+  invalid: [
+    {
+      code: "Ci.nsIScriptableUnicodeConverter",
+      errors: invalidError(),
+    },
+  ],
+});
