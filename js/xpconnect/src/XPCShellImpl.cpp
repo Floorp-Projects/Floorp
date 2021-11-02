34f9cb2633995348622b405fc3a3eea99fa19cc0
Kershaw Chang <kershaw@mozilla.com>
2021-11-02 16:01:10 +0000

    Bug 1727180 - Call XRE_InitCommandLine for xpcshell, r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D124890

--- js/xpconnect/src/XPCShellImpl.cpp
+++ js/xpconnect/src/XPCShellImpl.cpp
@@ -1078,6 +1078,8 @@ int XRE_XPCShellMain(int argc, char** argv, char** envp,
   // with the IOInterposer will be properly tracked.
   mozilla::IOInterposerInit ioInterposerGuard;
 
+  XRE_InitCommandLine(argc, argv);
+
   char aLocal;
   profiler_init(&aLocal);
 
@@ -1443,6 +1445,8 @@ int XRE_XPCShellMain(int argc, char** argv, char** envp,
   profiler_shutdown();
 
   NS_LogTerm();
+
+  XRE_DeinitCommandLine();
 
   return result;
 }
