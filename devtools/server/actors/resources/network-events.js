02c1a22e99984ce91bd50bc3961f8a5ad41e3912
Alexandre Poirot <poirot.alex@gmail.com>
2021-11-02 10:57:23 +0000

    Bug 1735825 - [devtools] Introduce a context in Watcher actor to describe what context should be debugged. r=jdescottes
    This is only the beginning, focusing first on parent process usages.
    WatcherActor still expose browserElement shortcut as it is useful in many places.
    Context is kept JSON-serializable in order to later pass it through the SessionData.
    
    Differential Revision: https://phabricator.services.mozilla.com/D128490

--- devtools/server/actors/resources/network-events.js
+++ devtools/server/actors/resources/network-events.js
@@ -50,7 +50,7 @@ class NetworkEventWatcher {
     this.persist = false;
 
     this.listener = new NetworkObserver(
-      { browserId: watcherActor.browserId },
+      { browserId: this.browserId },
       { onNetworkEvent: this.onNetworkEvent.bind(this) }
     );
 
@@ -63,7 +63,7 @@ class NetworkEventWatcher {
   }
 
   get browserId() {
-    return this.watcherActor.browserId;
+    return this.watcherActor.context.browserId;
   }
 
   /**
@@ -161,8 +161,8 @@ class NetworkEventWatcher {
     // If the watcher is bound to one browser element (i.e. a tab), ignore
     // windowGlobals related to other browser elements
     if (
-      this.watcherActor.browserId &&
-      windowGlobal.browsingContext.browserId != this.watcherActor.browserId
+      this.watcherActor.context.type == "browser-element" &&
+      windowGlobal.browsingContext.browserId != this.browserId
     ) {
       return;
     }
