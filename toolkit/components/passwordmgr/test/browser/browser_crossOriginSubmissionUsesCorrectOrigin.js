78e2eab24311792258464487d43a413e17b67895
Dimi <dlee@mozilla.com>
2021-11-02 13:47:48 +0000

    Bug 1729640 - P3. Notify event when a form with password is submitted r=sfoster,tgiles,geckoview-reviewers,agi
    Notify "passwordmgr-form-submission-detected" when a form with password
    is submitted.
    The event should be notified regardless of whether the password manager
    decides to show the doorhanger or not. To support this, this patch
    also refactors _maybeSendFormInteractionMessage function to distinguish
    "onFormSubmit" event and "showDoorhanger" event.
    
    Differential Revision: https://phabricator.services.mozilla.com/D127104

--- toolkit/components/passwordmgr/test/browser/browser_crossOriginSubmissionUsesCorrectOrigin.js
+++ toolkit/components/passwordmgr/test/browser/browser_crossOriginSubmissionUsesCorrectOrigin.js
@@ -6,7 +6,7 @@
 function getDataFromNextSubmitMessage() {
   return new Promise(resolve => {
     LoginManagerParent.setListenerForTests((msg, data) => {
-      if (msg == "FormSubmit") {
+      if (msg == "ShowDoorhanger") {
         resolve(data);
       }
     });
