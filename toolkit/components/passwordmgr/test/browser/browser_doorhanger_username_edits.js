ae9a0d9ca1891879e686fd397e75e320fe8c7a8d
Csoregi Natalia <ncsoregi@mozilla.com>
2021-11-04 23:44:26 +0200

    Backed out 7 changesets (bug 1729640) for causing permafailures on browser_web_process_isolation.js. a=backout
    Backed out changeset 5b2a1799ce4e (bug 1729640)
    Backed out changeset fcd4b833ad73 (bug 1729640)
    Backed out changeset 5ff8d3adc6eb (bug 1729640)
    Backed out changeset b8fd0343ca42 (bug 1729640)
    Backed out changeset fbd6928e34ef (bug 1729640)
    Backed out changeset 4ce2eebab0c4 (bug 1729640)
    Backed out changeset c7c50c85f911 (bug 1729640)

--- toolkit/components/passwordmgr/test/browser/browser_doorhanger_username_edits.js
+++ toolkit/components/passwordmgr/test/browser/browser_doorhanger_username_edits.js
@@ -112,7 +112,7 @@ add_task(async function test_edit_username() {
         // Submit the form in the content page with the credentials from the test
         // case. This will cause the doorhanger notification to be displayed.
         info("Submitting the form");
-        let formSubmittedPromise = listenForTestNotification("ShowDoorhanger");
+        let formSubmittedPromise = listenForTestNotification("FormSubmit");
         let promiseShown = BrowserTestUtils.waitForEvent(
           PopupNotifications.panel,
           "popupshown",
