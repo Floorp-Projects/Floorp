b7e73c3487a81f8a6ee77b4368436edab9ef57e9
Agi Sferro <agi@sferro.dev>
2021-11-05 15:58:54 +0000

    Bug 1717506 - Handle out-of-order page{hide,show}. r=owlish
    With Fission BFCache, when navigating back the two pages might run in different
    processes, even though they belong to the same domain.
    
    This means that `pagehide` and `pageshow` might come out of order. For example,
    navigating from `a.html` to `b.html` and back, we might get
    
    ```
    pageshow b.html
    pageshow a.html
    pagehide b.html
    ```
    
    notice we get the `pageshow` event for `a.html` _before_ the `pagehide` event
    for `b.html`.
    
    Internally in our test framework, we use `pagehide` and `pageshow` to establish
    a connection between a page and the Java layer through a WebExtension port.
    
    Before this patch, we assumed tha all `pageshow` events will match a `pagehide`
    event in order, and thus disconnect the port early for `a.html` when getting
    the event for `b.html` in the above example.
    
    There is also a separate problem where if a port was established for a given
    web page and then a navigation was issued, we would hang waiting for a response
    from the page that navigated away (and thus, we would not be able to send
    messages anymore).
    
    To fix both of these issues, we keep track of all pending messages and discard
    them properly when a Port is disconnected, and make sure that the port that's
    disconnecting matches the one that's currently associated with the session (to
    ensure that we are not disconnecting because of a out-of-order port event).
    
    Differential Revision: https://phabricator.services.mozilla.com/D130376

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/BaseSessionTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/BaseSessionTest.kt
@@ -107,6 +107,7 @@ open class BaseSessionTest(noErrorCollector: Boolean = false) {
 
         const val TEST_ENDPOINT = GeckoSessionTestRule.TEST_ENDPOINT
         const val TEST_HOST = GeckoSessionTestRule.TEST_HOST
+        const val TEST_PORT = GeckoSessionTestRule.TEST_PORT
     }
 
     @get:Rule val sessionRule = GeckoSessionTestRule()
