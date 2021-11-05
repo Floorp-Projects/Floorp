b167837ca6485596b0ea5f1ce89beb60b5308a22
Masayuki Nakano <masayuki@d-toybox.com>
2021-11-05 10:14:35 +0000

    Bug 1739315 - part 1: Make `HTMLEditor::JoinNodesWithTransaction()` directly notify `TopLevelEditSubActionData::DidJoinNodes()` of joined position r=m_kato
    `TopLevelEditSubActionData::WillJoinNodes()` and
    `TopLevelEditSubActionData::DidJoinNodes()` are called only by
    `HTMLEditor::JoinNodesWithTransaction()`.  `WillJoinNodes()` assumes that
    all children or text data in `aLeftContent` is moved to head of `aRightContent`.
    Therefore, it just stores length of `aLeftContent` and `DidJoinNodes()` lets
    `AddPointToChangedRange()` know the joined point.
    
    Same value is stored by `HTMLEditor::JoinNodesWithTransaction()`.  Therefore,
    it can create same DOM point at calling `DidJoinNodes()` so that we can get
    rid of `WillJoinNodes()`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130348

--- editor/libeditor/HTMLEditor.cpp
+++ editor/libeditor/HTMLEditor.cpp
@@ -4591,10 +4591,7 @@ nsresult HTMLEditor::JoinNodesWithTransaction(nsINode& aLeftNode,
     return NS_ERROR_FAILURE;
   }
   // Find the number of children of the lefthand node
-  uint32_t oldLeftNodeLen = aLeftNode.Length();
-
-  TopLevelEditSubActionDataRef().WillJoinContents(*this, *aLeftNode.AsContent(),
-                                                  *aRightNode.AsContent());
+  const uint32_t oldLeftNodeLen = aLeftNode.Length();
 
   RefPtr<JoinNodeTransaction> transaction = JoinNodeTransaction::MaybeCreate(
       *this, *aLeftNode.AsContent(), *aRightNode.AsContent());
@@ -4616,8 +4613,8 @@ nsresult HTMLEditor::JoinNodesWithTransaction(nsINode& aLeftNode,
   NS_WARNING_ASSERTION(NS_SUCCEEDED(rvIgnored),
                        "RangeUpdater::SelAdjJoinNodes() failed, but ignored");
 
-  TopLevelEditSubActionDataRef().DidJoinContents(*this, *aLeftNode.AsContent(),
-                                                 *aRightNode.AsContent());
+  TopLevelEditSubActionDataRef().DidJoinContents(
+      *this, EditorRawDOMPoint(&aRightNode, oldLeftNodeLen));
 
   if (mInlineSpellChecker) {
     RefPtr<mozInlineSpellChecker> spellChecker = mInlineSpellChecker;
