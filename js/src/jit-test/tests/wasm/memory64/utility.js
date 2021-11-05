318608c50f20f04d3d3470e9a3f512b0f91c0247
Lars T Hansen <lhansen@mozilla.com>
2021-11-05 20:03:22 +0000

    Bug 1739576 - Make 8GB available. r=rhunt
    Tidy up MaxMemoryPages(IndexType::I64) so that it returns a value
    corresponding to the max buffer byte length.  Fix two asserts that
    were incorrect and that triggered when making this change.
    
    Add a test that the max memory pages for i64 is indeed at least 8GB.
    
    Drive-by fix to allow a test to OOM without reporting that as a
    failure.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130452

---
+++ js/src/jit-test/tests/wasm/memory64/utility.js
@@ -0,0 +1,23 @@
+// Assert that we get a useful amount of memory for memory64 on tier-1 64-bit
+// systems with our primary compilers.
+
+var conf = getBuildConfiguration();
+
+// Only 64-bit systems
+if (conf["pointer-byte-size"] != 8) {
+    quit(0);
+}
+
+// MIPS64 tops out below 2GB
+if (conf["mips64"]) {
+    quit(0);
+}
+
+// Cranelift tops out below 4GB
+if (wasmCompilersPresent().match(/cranelift/)) {
+    quit(0);
+}
+
+var eightGB = 8 * 1024 * 1024 * 1024;
+var pageSize = 65536;
+assertEq(wasmMaxMemoryPages("i64") >= eightGB / pageSize, true);
