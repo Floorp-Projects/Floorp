1456d987d4e15fd739381757231891be53e764eb
Botond Ballo <botond@mozilla.com>
2021-11-06 05:21:07 +0000

    Bug 1739754 - Fix an out-of-date commment. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D130532

--- gfx/layers/apz/src/APZCTreeManager.cpp
+++ gfx/layers/apz/src/APZCTreeManager.cpp
@@ -1606,7 +1606,7 @@ APZEventResult APZCTreeManager::ReceiveInputEvent(InputData& aEvent) {
 
       // If/when we enable support for pan inputs off-main-thread, we'll need
       // to duplicate this EventStateManager code or something. See the call to
-      // GetUserPrefsForWheelEvent in IAPZCTreeManager.cpp for why these fields
+      // GetUserPrefsForWheelEvent in APZInputBridge.cpp for why these fields
       // are stored separately.
       MOZ_ASSERT(NS_IsMainThread());
       WidgetWheelEvent wheelEvent = panInput.ToWidgetEvent(nullptr);
