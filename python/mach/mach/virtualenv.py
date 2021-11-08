31ddf389684a6eaffded48904308311c53e49bfa
Mitchell Hentges <mhentges@mozilla.com>
2021-11-08 21:20:45 +0000

    Bug 1740114: Virtualenv.py changes shouldn't require venv rebuilds r=ahal
    We now have more intelligent ways of checking if a venv is out-of-date:
    * Are the `pth` files correct?
    * Were any requirements files changed?
    
    Differential Revision: https://phabricator.services.mozilla.com/D130678

--- python/mach/mach/virtualenv.py
+++ python/mach/mach/virtualenv.py
@@ -172,7 +172,7 @@ class VirtualenvManager(VirtualenvHelper):
             "virtualenv",
             "version.py",
         )
-        deps = [__file__, virtualenv_package] + env_requirements.requirements_paths
+        deps = [virtualenv_package] + env_requirements.requirements_paths
 
         # Modifications to any of the following files mean the virtualenv should be
         # rebuilt:
