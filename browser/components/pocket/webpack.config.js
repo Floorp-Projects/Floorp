6c060946216849780734d3ad07f427e210dff23e
Scott <scott.downe@gmail.com>
2021-11-09 23:51:27 +0000

    Bug 1699515 - Switching to react for Save to Pocket panels. r=gvn
    Differential Revision: https://phabricator.services.mozilla.com/D128753

---
+++ browser/components/pocket/webpack.config.js
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* eslint-env node */
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./content/panels/js/main.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: `${__dirname}/content/panels/js/`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
