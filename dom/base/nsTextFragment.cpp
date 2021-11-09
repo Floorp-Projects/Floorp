f6541f3d0c1cf4352f1889b2f933f734efb5257b
Masayuki Nakano <masayuki@d-toybox.com>
2021-11-09 01:06:49 +0000

    Bug 1737009 - Make `nsTextFragment::SetTo()` check given string length first r=smaug,a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D129137

--- dom/base/nsTextFragment.cpp
+++ dom/base/nsTextFragment.cpp
@@ -196,6 +196,10 @@ static inline int32_t FirstNon8Bit(const char16_t* str, const char16_t* end) {
 
 bool nsTextFragment::SetTo(const char16_t* aBuffer, uint32_t aLength,
                            bool aUpdateBidi, bool aForce2b) {
+  if (MOZ_UNLIKELY(aLength > NS_MAX_TEXT_FRAGMENT_LENGTH)) {
+    return false;
+  }
+
   if (aForce2b && mState.mIs2b && !m2b->IsReadonly()) {
     uint32_t storageSize = m2b->StorageSize();
     uint32_t neededSize = aLength * sizeof(char16_t);
