8a64dddaf614b81a0a359b2e2d1aeb6575dcac2a
André Bargull <andre.bargull@gmail.com>
2021-11-09 09:50:54 +0000

    Bug 1740053 - Part 5: Fix a comment. r=jandem
    Depends on D130653
    
    Differential Revision: https://phabricator.services.mozilla.com/D130654

--- js/src/jit/VMFunctions.cpp
+++ js/src/jit/VMFunctions.cpp
@@ -131,7 +131,7 @@ struct TypeToPassInFloatReg<double> {
   static const uint32_t result = 1;
 };
 
-// Convert argument types to root types used by the gc, see MarkJitExitFrame.
+// Convert argument types to root types used by the gc, see TraceJitExitFrame.
 template <class T>
 struct TypeToRootType {
   static const uint32_t result = VMFunctionData::RootNone;
