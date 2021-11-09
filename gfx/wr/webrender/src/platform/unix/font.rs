6e549f82876221bef6b32d2521b46d98dcadd99a
Jonathan Kew <jkew@mozilla.com>
2021-11-09 17:06:52 +0000

    Bug 1738700 - Don't use FT_LOAD_COLOR for COLR-format fonts within WebRender, because color-font layers are handled at the Gecko level. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D130271

--- gfx/wr/webrender/src/platform/unix/font.rs
+++ gfx/wr/webrender/src/platform/unix/font.rs
@@ -478,12 +478,19 @@ impl FontContext {
             load_flags |= FT_LOAD_NO_BITMAP;
         }
 
-        load_flags |= FT_LOAD_COLOR;
+        let face_flags = unsafe { (*face).face_flags };
+        if (face_flags & (FT_FACE_FLAG_FIXED_SIZES as FT_Long)) != 0 {
+          // We only set FT_LOAD_COLOR if there are bitmap strikes;
+          // COLR (color-layer) fonts are handled internally by Gecko, and
+          // WebRender is just asked to paint individual layers.
+          load_flags |= FT_LOAD_COLOR;
+        }
+
         load_flags |= FT_LOAD_IGNORE_GLOBAL_ADVANCE_WIDTH;
 
         let (x_scale, y_scale) = font.transform.compute_scale().unwrap_or((1.0, 1.0));
         let req_size = font.size.to_f64_px();
-        let face_flags = unsafe { (*face).face_flags };
+
         let mut result = if (face_flags & (FT_FACE_FLAG_FIXED_SIZES as FT_Long)) != 0 &&
                             (face_flags & (FT_FACE_FLAG_SCALABLE as FT_Long)) == 0 &&
                             (load_flags & FT_LOAD_NO_BITMAP) == 0 {
