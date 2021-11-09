9ac28fde2221c7825a98384c8219597670910bd8
Butkovits Atila <abutkovits@mozilla.com>
2021-11-09 20:24:03 +0200

    Backed out 7 changesets (bug 1566998) for causing build bustages at Logging.h.
    Backed out changeset 1d0f667507f4 (bug 1566998)
    Backed out changeset dc39d8837aae (bug 1566998)
    Backed out changeset 24f76460c44b (bug 1566998)
    Backed out changeset b674d5f15f60 (bug 1566998)
    Backed out changeset f392ebf40d2a (bug 1566998)
    Backed out changeset ecbfa0721168 (bug 1566998)
    Backed out changeset d948fe16b8b0 (bug 1566998)

--- netwerk/test/unit/test_dns_disable_ipv4.js
+++ netwerk/test/unit/test_dns_disable_ipv4.js
@@ -6,21 +6,38 @@
 "use strict";
 
 var dns = Cc["@mozilla.org/network/dns-service;1"].getService(Ci.nsIDNSService);
-const gOverride = Cc["@mozilla.org/network/native-dns-override;1"].getService(
-  Ci.nsINativeDNSResolverOverride
+var ioService = Cc["@mozilla.org/network/io-service;1"].getService(
+  Ci.nsIIOService
 );
+
+var listener = {
+  onLookupComplete(inRequest, inRecord, inStatus) {
+    if (inStatus != Cr.NS_OK) {
+      Assert.equal(inStatus, Cr.NS_ERROR_UNKNOWN_HOST);
+      do_test_finished();
+      return;
+    }
+
+    while (true) {
+      try {
+        inRecord.QueryInterface(Ci.nsIDNSAddrRecord);
+        var answer = inRecord.getNextAddrAsString();
+        // If there is an answer it should be an IPv6  address
+        dump(answer);
+        Assert.ok(answer.includes(":"));
+      } catch (e) {
+        break;
+      }
+    }
+    do_test_finished();
+  },
+};
 
 const defaultOriginAttributes = {};
 
-add_task(async function test_none() {
-  let [, inRecord] = await new Promise(resolve => {
-    let listener = {
-      onLookupComplete(inRequest, inRecord, inStatus) {
-        resolve([inRequest, inRecord, inStatus]);
-      },
-      QueryInterface: ChromeUtils.generateQI(["nsIDNSListener"]),
-    };
-
+function run_test() {
+  do_test_pending();
+  try {
     dns.asyncResolve(
       "example.org",
       Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
@@ -30,40 +47,9 @@ add_task(async function test_none() {
       null,
       defaultOriginAttributes
     );
-  });
-
-  if (inRecord && inRecord.QueryInterface(Ci.nsIDNSAddrRecord)) {
-    while (inRecord.hasMore()) {
-      let nextIP = inRecord.getNextAddrAsString();
-      ok(nextIP.includes(":"), `${nextIP} should be IPv6`);
-    }
+  } catch (e) {
+    dump(e);
+    Assert.ok(false);
+    do_test_finished();
   }
-});
-
-add_task(async function test_some() {
-  dns.clearCache(true);
-  gOverride.addIPOverride("example.com", "1.1.1.1");
-  gOverride.addIPOverride("example.org", "::1:2:3");
-  let [, inRecord] = await new Promise(resolve => {
-    let listener = {
-      onLookupComplete(inRequest, inRecord, inStatus) {
-        resolve([inRequest, inRecord, inStatus]);
-      },
-      QueryInterface: ChromeUtils.generateQI(["nsIDNSListener"]),
-    };
-
-    dns.asyncResolve(
-      "example.org",
-      Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
-      Ci.nsIDNSService.RESOLVE_DISABLE_IPV4,
-      null, // resolverInfo
-      listener,
-      null,
-      defaultOriginAttributes
-    );
-  });
-
-  ok(inRecord.QueryInterface(Ci.nsIDNSAddrRecord));
-  equal(inRecord.getNextAddrAsString(), "::1:2:3");
-  equal(inRecord.hasMore(), false);
-});
+}
--- netwerk/test/unit/test_trr_cancel.js
+++ netwerk/test/unit/test_trr_cancel.js
@@ -7,6 +7,9 @@
 const dns = Cc["@mozilla.org/network/dns-service;1"].getService(
   Ci.nsIDNSService
 );
+const { TestUtils } = ChromeUtils.import(
+  "resource://testing-common/TestUtils.jsm"
+);
 
 trr_test_setup();
 registerCleanupFunction(async () => {
@@ -60,7 +63,7 @@ add_task(async function cancel_immediately() {
   let r1 = new TRRDNSListener("example.org", { expectedSuccess: false });
   let r2 = new TRRDNSListener("example.org", { expectedAnswer: "2.3.4.5" });
   r1.cancel();
-  let { inStatus } = await r1;
+  let [, , inStatus] = await r1;
   equal(inStatus, Cr.NS_ERROR_ABORT);
   await r2;
   equal(await trrServer.requestCount("example.org", "A"), 1);
@@ -71,9 +74,9 @@ add_task(async function cancel_immediately() {
   r2 = new TRRDNSListener("example.org", { expectedSuccess: false });
   r1.cancel();
   r2.cancel();
-  ({ inStatus } = await r1);
+  [, , inStatus] = await r1;
   equal(inStatus, Cr.NS_ERROR_ABORT);
-  ({ inStatus } = await r2);
+  [, , inStatus] = await r2;
   equal(inStatus, Cr.NS_ERROR_ABORT);
   await new Promise(resolve => do_timeout(50, resolve));
   equal(await trrServer.requestCount("example.org", "A"), 2);
@@ -97,7 +100,7 @@ add_task(async function cancel_delayed() {
   let r2 = new TRRDNSListener("example.com", { expectedAnswer: "1.1.1.1" });
   await new Promise(resolve => do_timeout(50, resolve));
   r1.cancel();
-  let { inStatus } = await r1;
+  let [, , inStatus] = await r1;
   equal(inStatus, Cr.NS_ERROR_ABORT);
   await r2;
 });
