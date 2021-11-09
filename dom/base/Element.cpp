fbdae21c4754703d44ee4453e76e21670dbc0902
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2021-11-09 09:19:24 +0000

    Bug 1739541 - Drop ScrollOrigin argument from ScrollByCSSPixels. r=botond
    All call sites of ScrollByCSSPixels specify ScrollOrigin::Relative.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130415

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -833,8 +833,7 @@ void Element::ScrollBy(const ScrollToOptions& aOptions) {
                                 ? ScrollMode::SmoothMsd
                                 : ScrollMode::Instant;
 
-    sf->ScrollByCSSPixels(scrollDelta, scrollMode,
-                          mozilla::ScrollOrigin::Relative);
+    sf->ScrollByCSSPixels(scrollDelta, scrollMode);
   }
 }
 
