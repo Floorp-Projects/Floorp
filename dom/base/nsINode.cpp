3e2146db6c8ce2ba80e4a49742b8758bd787e417
Emilio Cobos Álvarez <emilio@crisal.io>
2021-11-10 12:56:35 +0000

    Bug 1740230 - Allow matches() / querySelector() / etc on chrome docs to access chrome-only selectors. r=boris
    Without this some tests fail with the previous patch because code like:
    
      https://searchfox.org/mozilla-central/rev/267682a8f45221bf0bfe999d4a0239706a43bc56/browser/base/content/browser-gestureSupport.js#651
    
    starts throwing. Unfortunately I had missed that on my try run, because
    the error message didn't include that exception (it seemed like an
    intermittent browser-chrome failure instead).
    
    We could expose a ChromeOnly API for this, but this seems better. This
    fixes it trivially, and also removes the "no url data" situation from
    the selector parser, which is nice.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130818

--- dom/base/nsINode.cpp
+++ dom/base/nsINode.cpp
@@ -2867,7 +2867,15 @@ const RawServoSelectorList* nsINode::ParseSelectorList(
   RawServoSelectorList* list = cache.GetListOrInsertFrom(aSelectorString, [&] {
     // Note that we want to cache even if null was returned, because we
     // want to cache the "This is not a valid selector" result.
-    return Servo_SelectorList_Parse(&aSelectorString).Consume();
+    //
+    // NOTE(emilio): Off-hand, getting a CallerType here might seem like a
+    // better idea than using IsDocumentURISchemeChrome(), but that would mean
+    // that we'd need to key the selector cache by that.
+    // IsDocumentURISchemeChrome() gives us the same semantics as any inline
+    // style associated to a document, which seems reasonable.
+    return Servo_SelectorList_Parse(&aSelectorString,
+                                    doc->IsDocumentURISchemeChrome())
+        .Consume();
   });
 
   if (!list) {
