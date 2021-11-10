178e3e1cc405d9b967c1a1f7e5001462868fa19e
Neil Deakin <neil@mozilla.com>
2021-11-10 18:52:23 +0000

    Bug 1736864, add telemetry when the external helper service is invoked to handle a content type, specifying the action to take and some details about the data to download, r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D129150

--- uriloader/exthandler/nsExternalHelperAppService.h
+++ uriloader/exthandler/nsExternalHelperAppService.h
@@ -273,6 +273,10 @@ class nsExternalAppHandler final : public nsIStreamListener,
   void SetShouldCloseWindow() { mShouldCloseWindow = true; }
 
  protected:
+
+  // Record telemetry about a download that was attempted.
+  void RecordDownloadTelemetry(nsIChannel* aChannel, const char* aAction);
+
   bool IsDownloadSpam(nsIChannel* aChannel);
 
   ~nsExternalAppHandler();
