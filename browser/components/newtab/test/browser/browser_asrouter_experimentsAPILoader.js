9ee9fb92d35aaa0e3c9b59f60bcdc37ccc78bdb8
Andrei Oprea <andrei.br92@gmail.com>
2021-11-10 17:29:58 +0000

    Bug 1739666 - Messaging system should use feature.variables.enabled, not feature.enabled r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D130636

--- browser/components/newtab/test/browser/browser_asrouter_experimentsAPILoader.js
+++ browser/components/newtab/test/browser/browser_asrouter_experimentsAPILoader.js
@@ -10,7 +10,7 @@ const { RemoteSettingsExperimentLoader } = ChromeUtils.import(
 const { ExperimentAPI } = ChromeUtils.import(
   "resource://nimbus/ExperimentAPI.jsm"
 );
-const { ExperimentFakes } = ChromeUtils.import(
+const { ExperimentFakes, ExperimentTestUtils } = ChromeUtils.import(
   "resource://testing-common/NimbusTestUtils.jsm"
 );
 const { TelemetryFeed } = ChromeUtils.import(
@@ -20,8 +20,8 @@ const { TelemetryTestUtils } = ChromeUtils.import(
   "resource://testing-common/TelemetryTestUtils.jsm"
 );
 
-const EXPERIMENT_PAYLOAD = () =>
-  ExperimentFakes.recipe(
+const getCFRExperiment = async () => {
+  let recipe = ExperimentFakes.recipe(
     // In tests by default studies/experiments are turned off. We turn them on
     // to run the test and rollback at the end. Cleanup causes unenrollment so
     // for cases where the test runs multiple times we need unique ids.
@@ -35,120 +35,108 @@ const EXPERIMENT_PAYLOAD = () =>
         namespace: "mochitest",
         randomizationUnit: "normandy_id",
       },
-      branches: [
-        {
-          slug: "control",
-          ratio: 1,
-          feature: {
-            featureId: "cfr",
-            enabled: true,
-            value: {
-              id: "xman_test_message",
-              content: {
-                text: "This is a test CFR",
-                addon: {
-                  id: "954390",
-                  icon:
-                    "chrome://activity-stream/content/data/content/assets/cfr_fb_container.png",
-                  title: "Facebook Container",
-                  users: 1455872,
-                  author: "Mozilla",
-                  rating: 4.5,
-                  amo_url:
-                    "https://addons.mozilla.org/firefox/addon/facebook-container/",
-                },
-                buttons: {
-                  primary: {
-                    label: {
-                      string_id: "cfr-doorhanger-extension-ok-button",
-                    },
-                    action: {
-                      data: {
-                        url: null,
-                      },
-                      type: "INSTALL_ADDON_FROM_URL",
-                    },
-                  },
-                  secondary: [
-                    {
-                      label: {
-                        string_id: "cfr-doorhanger-extension-cancel-button",
-                      },
-                      action: {
-                        type: "CANCEL",
-                      },
-                    },
-                    {
-                      label: {
-                        string_id:
-                          "cfr-doorhanger-extension-never-show-recommendation",
-                      },
-                    },
-                    {
-                      label: {
-                        string_id:
-                          "cfr-doorhanger-extension-manage-settings-button",
-                      },
-                      action: {
-                        data: {
-                          origin: "CFR",
-                          category: "general-cfraddons",
-                        },
-                        type: "OPEN_PREFERENCES_PAGE",
-                      },
-                    },
-                  ],
-                },
-                category: "cfrAddons",
-                bucket_id: "CFR_M1",
-                info_icon: {
-                  label: {
-                    string_id: "cfr-doorhanger-extension-sumo-link",
-                  },
-                  sumo_path: "extensionrecommendations",
-                },
-                heading_text: "Welcome to the experiment",
-                notification_text: {
-                  string_id: "cfr-doorhanger-extension-notification2",
-                },
-              },
-              trigger: {
-                id: "openURL",
-                params: [
-                  "www.facebook.com",
-                  "facebook.com",
-                  "www.instagram.com",
-                  "instagram.com",
-                  "www.whatsapp.com",
-                  "whatsapp.com",
-                  "web.whatsapp.com",
-                  "www.messenger.com",
-                  "messenger.com",
-                ],
-              },
-              template: "cfr_doorhanger",
-              frequency: {
-                lifetime: 3,
-              },
-              targeting: "true",
-            },
-          },
-        },
-      ],
-      userFacingName: "About:Welcome Pull Factor Reinforcement",
-      isEnrollmentPaused: false,
-      experimentDocumentUrl:
-        "https://experimenter.services.mozilla.com/experiments/aboutwelcome-pull-factor-reinforcement/",
-      userFacingDescription:
-        "This study uses 4 different variants of about:welcome with a goal of testing new experiment framework and get insights on whether reinforcing pull-factors improves retention.",
     }
   );
+  let messageContent = {
+    id: "xman_test_message",
+    content: {
+      text: "This is a test CFR",
+      addon: {
+        id: "954390",
+        icon:
+          "chrome://activity-stream/content/data/content/assets/cfr_fb_container.png",
+        title: "Facebook Container",
+        users: 1455872,
+        author: "Mozilla",
+        rating: 4.5,
+        amo_url: "https://addons.mozilla.org/firefox/addon/facebook-container/",
+      },
+      buttons: {
+        primary: {
+          label: {
+            string_id: "cfr-doorhanger-extension-ok-button",
+          },
+          action: {
+            data: {
+              url: null,
+            },
+            type: "INSTALL_ADDON_FROM_URL",
+          },
+        },
+        secondary: [
+          {
+            label: {
+              string_id: "cfr-doorhanger-extension-cancel-button",
+            },
+            action: {
+              type: "CANCEL",
+            },
+          },
+          {
+            label: {
+              string_id: "cfr-doorhanger-extension-never-show-recommendation",
+            },
+          },
+          {
+            label: {
+              string_id: "cfr-doorhanger-extension-manage-settings-button",
+            },
+            action: {
+              data: {
+                origin: "CFR",
+                category: "general-cfraddons",
+              },
+              type: "OPEN_PREFERENCES_PAGE",
+            },
+          },
+        ],
+      },
+      category: "cfrAddons",
+      bucket_id: "CFR_M1",
+      info_icon: {
+        label: {
+          string_id: "cfr-doorhanger-extension-sumo-link",
+        },
+        sumo_path: "extensionrecommendations",
+      },
+      heading_text: "Welcome to the experiment",
+      notification_text: {
+        string_id: "cfr-doorhanger-extension-notification2",
+      },
+    },
+    trigger: {
+      id: "openURL",
+      params: [
+        "www.facebook.com",
+        "facebook.com",
+        "www.instagram.com",
+        "instagram.com",
+        "www.whatsapp.com",
+        "whatsapp.com",
+        "web.whatsapp.com",
+        "www.messenger.com",
+        "messenger.com",
+      ],
+    },
+    template: "cfr_doorhanger",
+    frequency: {
+      lifetime: 3,
+    },
+    targeting: "true",
+  };
+  recipe.branches[0].features[0].featureId = "cfr";
+  recipe.branches[0].features[0].value = messageContent;
+  recipe.branches[1].features[0].featureId = "cfr";
+  recipe.branches[1].features[0].value = messageContent;
+  await ExperimentTestUtils.validateExperiment(recipe);
+  return recipe;
+};
 
 const client = RemoteSettings("nimbus-desktop-experiments");
 
 // no `add_task` because we want to run this setup before each test not before
 // the entire test suite.
-async function setup(getPayload = EXPERIMENT_PAYLOAD) {
+async function setup(experiment) {
   await SpecialPowers.pushPrefEnv({
     set: [
       ["app.shield.optoutstudies.enabled", true],
@@ -159,15 +147,7 @@ async function setup(getPayload = EXPERIMENT_PAYLOAD) {
     ],
   });
 
-  await client.db.importChanges(
-    {},
-    42,
-    [
-      // Modify targeting to ensure the messages always show up
-      { ...getPayload() },
-    ],
-    { clear: true }
-  );
+  await client.db.importChanges({}, 42, [experiment], { clear: true });
 }
 
 async function cleanup() {
@@ -178,7 +158,8 @@ async function cleanup() {
 }
 
 add_task(async function test_loading_experimentsAPI() {
-  await setup();
+  let experiment = await getCFRExperiment();
+  await setup(experiment);
   // Fetch the new recipe from RS
   await RemoteSettingsExperimentLoader.updateRecipes();
   await BrowserTestUtils.waitForCondition(
@@ -206,7 +187,8 @@ add_task(async function test_loading_experimentsAPI() {
 });
 
 add_task(async function test_exposure_ping() {
-  await setup();
+  let experiment = await getCFRExperiment();
+  await setup(experiment);
   Services.telemetry.clearScalars();
   // Fetch the new recipe from RS
   await RemoteSettingsExperimentLoader.updateRecipes();
@@ -248,14 +230,11 @@ add_task(async function test_exposure_ping() {
 });
 
 add_task(async function test_featureless_experiment() {
-  const payload = () => {
-    let experiment = EXPERIMENT_PAYLOAD();
-    // Remove the feature property from the branch
-    experiment.branches.forEach(branch => delete branch.feature);
-    return experiment;
-  };
+  let experiment = await getCFRExperiment();
+  // Remove the feature property from the branch
+  experiment.branches.forEach(branch => delete branch.features);
   Assert.ok(ExperimentAPI._store.getAllActive().length === 0, "Empty store");
-  await setup(payload);
+  await setup(experiment);
   // Fetch the new recipe from RS
   await RemoteSettingsExperimentLoader.updateRecipes();
   // Enrollment was successful; featureless experiments shouldn't break anything
