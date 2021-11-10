3f5ef1a15571e23c677266446e93ae363a2a48c2
Lee Salzman <lsalzman@mozilla.com>
2021-11-10 22:16:13 +0000

    Bug 1738589 - Let Gecko decide if WR Mac font has color glyphs. r=jfkthame
    WR tries to normalize Mac FontInstances so that regardless of the font color
    we only need to generate one version of the glyph in the cache. This strategy
    backfires for fonts with color glyphs that are nominally supposed to ignore
    the font color, but may choose to opt-in to the font color per-glyph based
    on table data.
    
    This determination is problematic to do in WR itself, but Gecko knows whether
    a font has color glyphs ahead of time. So we let Gecko pass this data into
    ScaledFonts which can then transmit the knowledge of whether the font has
    color glyphs to the WR Mac font backend, which will then take appropriate
    action.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130819

--- gfx/2d/ScaledFontMac.cpp
+++ gfx/2d/ScaledFontMac.cpp
@@ -124,12 +124,14 @@ ScaledFontMac::ScaledFontMac(CGFontRef aFont,
                              const RefPtr<UnscaledFont>& aUnscaledFont,
                              Float aSize, bool aOwnsFont,
                              const DeviceColor& aFontSmoothingBackgroundColor,
-                             bool aUseFontSmoothing, bool aApplySyntheticBold)
+                             bool aUseFontSmoothing, bool aApplySyntheticBold,
+                             bool aHasColorGlyphs)
     : ScaledFontBase(aUnscaledFont, aSize),
       mFont(aFont),
       mFontSmoothingBackgroundColor(aFontSmoothingBackgroundColor),
       mUseFontSmoothing(aUseFontSmoothing),
-      mApplySyntheticBold(aApplySyntheticBold) {
+      mApplySyntheticBold(aApplySyntheticBold),
+      mHasColorGlyphs(aHasColorGlyphs) {
   if (!aOwnsFont) {
     // XXX: should we be taking a reference
     CGFontRetain(aFont);
@@ -143,12 +145,14 @@ ScaledFontMac::ScaledFontMac(CGFontRef aFont,
 ScaledFontMac::ScaledFontMac(CTFontRef aFont,
                              const RefPtr<UnscaledFont>& aUnscaledFont,
                              const DeviceColor& aFontSmoothingBackgroundColor,
-                             bool aUseFontSmoothing, bool aApplySyntheticBold)
+                             bool aUseFontSmoothing, bool aApplySyntheticBold,
+                             bool aHasColorGlyphs)
     : ScaledFontBase(aUnscaledFont, CTFontGetSize(aFont)),
       mCTFont(aFont),
       mFontSmoothingBackgroundColor(aFontSmoothingBackgroundColor),
       mUseFontSmoothing(aUseFontSmoothing),
-      mApplySyntheticBold(aApplySyntheticBold) {
+      mApplySyntheticBold(aApplySyntheticBold),
+      mHasColorGlyphs(aHasColorGlyphs) {
   mFont = CTFontCopyGraphicsFont(aFont, nullptr);
 
   CFRetain(mCTFont);
@@ -448,6 +452,9 @@ bool ScaledFontMac::GetWRFontInstanceOptions(
   if (mApplySyntheticBold) {
     options.flags |= wr::FontInstanceFlags::SYNTHETIC_BOLD;
   }
+  if (mHasColorGlyphs) {
+    options.flags |= wr::FontInstanceFlags::EMBEDDED_BITMAPS;
+  }
   options.bg_color = wr::ToColorU(mFontSmoothingBackgroundColor);
   options.synthetic_italics =
       wr::DegreesToSyntheticItalics(GetSyntheticObliqueAngle());
@@ -458,13 +465,18 @@ bool ScaledFontMac::GetWRFontInstanceOptions(
 ScaledFontMac::InstanceData::InstanceData(
     const wr::FontInstanceOptions* aOptions,
     const wr::FontInstancePlatformOptions* aPlatformOptions)
-    : mUseFontSmoothing(true), mApplySyntheticBold(false) {
+    : mUseFontSmoothing(true),
+      mApplySyntheticBold(false),
+      mHasColorGlyphs(false) {
   if (aOptions) {
     if (!(aOptions->flags & wr::FontInstanceFlags::FONT_SMOOTHING)) {
       mUseFontSmoothing = false;
     }
     if (aOptions->flags & wr::FontInstanceFlags::SYNTHETIC_BOLD) {
       mApplySyntheticBold = true;
+    }
+    if (aOptions->flags & wr::FontInstanceFlags::EMBEDDED_BITMAPS) {
+      mHasColorGlyphs = true;
     }
     if (aOptions->bg_color.a != 0) {
       mFontSmoothingBackgroundColor =
@@ -717,7 +729,8 @@ already_AddRefed<ScaledFont> UnscaledFontMac::CreateScaledFont(
     }
     scaledFont = new ScaledFontMac(
         font, this, instanceData.mFontSmoothingBackgroundColor,
-        instanceData.mUseFontSmoothing, instanceData.mApplySyntheticBold);
+        instanceData.mUseFontSmoothing, instanceData.mApplySyntheticBold,
+        instanceData.mHasColorGlyphs);
   } else {
     CGFontRef fontRef = mFont;
     if (aNumVariations > 0) {
@@ -731,7 +744,8 @@ already_AddRefed<ScaledFont> UnscaledFontMac::CreateScaledFont(
     scaledFont = new ScaledFontMac(fontRef, this, aGlyphSize, fontRef != mFont,
                                    instanceData.mFontSmoothingBackgroundColor,
                                    instanceData.mUseFontSmoothing,
-                                   instanceData.mApplySyntheticBold);
+                                   instanceData.mApplySyntheticBold,
+                                   instanceData.mHasColorGlyphs);
   }
   return scaledFont.forget();
 }
