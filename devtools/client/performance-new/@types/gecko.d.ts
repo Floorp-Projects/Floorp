b72813b6979bd72a21e206dacd7325ccbe59c394
Markus Stange <mstange.moz@gmail.com>
2021-11-10 21:20:34 +0000

    Bug 1625309 - Remove the frame script and instead use the WebChannel to expose the profile and symbolication. r=julienw
    Differential Revision: https://phabricator.services.mozilla.com/D121933

--- devtools/client/performance-new/@types/gecko.d.ts
+++ devtools/client/performance-new/@types/gecko.d.ts
@@ -291,7 +291,6 @@ declare namespace MockedExports {
      * Then add the file path to the KnownModules above.
      */
     import: <S extends keyof KnownModules>(module: S) => KnownModules[S];
-    createObjectIn: (content: ContentWindow) => object;
     exportFunction: (fn: Function, scope: object, options?: object) => void;
     cloneInto: (value: any, scope: object, options?: object) => void;
     isInAutomation: boolean;
