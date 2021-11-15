db4862b23ec1d253bfe273766093981d38289bf9
Julian Descottes <jdescottes@mozilla.com>
2021-11-15 08:44:46 +0000

    Bug 1737790 - [devtools] Color picker should fallback on getClosestBackgroundColor for elements without quads r=nchevobbe
    The getBackgroundColor already has fallback paths for several situations.
    We now use the fallback in case getAdjustedQuads returned an empty array.
    
    A browser mochitest is added to cover this issue as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130750

--- devtools/server/actors/inspector/utils.js
+++ devtools/server/actors/inspector/utils.js
@@ -468,15 +468,11 @@ async function getBackgroundColor({ rawNode: node, walker }) {
     };
   }
 
-  const bounds = getAdjustedQuads(
-    node.ownerGlobal,
-    node.firstChild,
-    "content"
-  )[0].bounds;
+  const quads = getAdjustedQuads(node.ownerGlobal, node.firstChild, "content");
 
   // Fall back to calculating contrast against closest bg if there are no bounds for text node.
   // Avoid creating doc walker by returning early.
-  if (!bounds) {
+  if (quads.length === 0 || !quads[0].bounds) {
     return {
       value: colorUtils.colorToRGBA(
         getClosestBackgroundColor(node),
@@ -485,6 +481,8 @@ async function getBackgroundColor({ rawNode: node, walker }) {
       ),
     };
   }
+
+  const bounds = quads[0].bounds;
 
   const docWalker = walker.getDocumentWalker(node);
   const firstChild = docWalker.firstChild();
