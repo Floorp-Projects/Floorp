793ef6b2aab761f0f5e6c0c4c3ea46379e469b29
Julian Descottes <jdescottes@mozilla.com>
2021-11-15 08:46:35 +0000

    Bug 1740438 - [devtools] Remove backward compatibility code for event-breakpoints trait r=devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D130844

--- devtools/server/actors/watcher.js
+++ devtools/server/actors/watcher.js
@@ -238,9 +238,6 @@ exports.WatcherActor = protocol.ActorClassWithSpec(watcherSpec, {
           [Resources.TYPES.SERVER_SENT_EVENT]: shouldEnableAllWatchers,
           [Resources.TYPES.WEBSOCKET]: shouldEnableAllWatchers,
         },
-
-        // @backward-compat { version 94 } Full support for event breakpoints via the watcher actor
-        "event-breakpoints": true,
       },
     };
   },
