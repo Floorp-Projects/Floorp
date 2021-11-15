bd1b5247c1d0a1a589f51239988abf30d73ff8a2
Jeff Muizelaar <jmuizelaar@mozilla.com>
2021-11-15 19:05:14 +0000

    Bug 1711133. Draw nothing for unitialized filters. r=mstange
    Currently we're falling back when hitting an uninitialized filter (e.g. a bad
    url) and the fallback path just draws nothing. We can do that just as well by
    drawing nothing ourselves.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130281

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -8241,11 +8241,17 @@ bool nsDisplayBackdropFilters::CreateWebRenderCommands(
   WrFiltersHolder wrFilters;
   Maybe<nsRect> filterClip;
   auto filterChain = mFrame->StyleEffects()->mBackdropFilters.AsSpan();
+  bool initialized = true;
   if (!SVGIntegrationUtils::CreateWebRenderCSSFilters(filterChain, mFrame,
                                                       wrFilters) &&
-      !SVGIntegrationUtils::BuildWebRenderFilters(mFrame, filterChain,
-                                                  wrFilters, filterClip)) {
+      !SVGIntegrationUtils::BuildWebRenderFilters(
+          mFrame, filterChain, wrFilters, filterClip, initialized)) {
     return false;
+  }
+
+  if (!initialized) {
+    // draw nothing
+    return true;
   }
 
   nsCSSRendering::ImageLayerClipState clip;
@@ -8352,12 +8358,18 @@ bool nsDisplayFilters::CreateWebRenderCommands(
 
   WrFiltersHolder wrFilters;
   Maybe<nsRect> filterClip;
+  bool initialized = true;
   auto filterChain = mFrame->StyleEffects()->mFilters.AsSpan();
   if (!SVGIntegrationUtils::CreateWebRenderCSSFilters(filterChain, mFrame,
                                                       wrFilters) &&
-      !SVGIntegrationUtils::BuildWebRenderFilters(mFrame, filterChain,
-                                                  wrFilters, filterClip)) {
+      !SVGIntegrationUtils::BuildWebRenderFilters(
+          mFrame, filterChain, wrFilters, filterClip, initialized)) {
     return false;
+  }
+
+  if (!initialized) {
+    // draw nothing
+    return true;
   }
 
   wr::WrStackingContextClip clip{};
