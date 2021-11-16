bda0bb4a059a3949fcca385fe7dc17b9c6c00e59
Dave Townsend <dtownsend@oxymoronical.com>
2021-11-16 10:29:17 +0000

    Bug 1740531: Move page data collection into PageDataSchema for easier testing. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D130881

---
+++ browser/components/pagedata/tests/unit/test_opengraph.js
@@ -0,0 +1,63 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Tests that the page data service can parse Open Graph metadata.
+ */
+
+add_task(async function test_type_website() {
+  await verifyPageData(
+    `
+      <html>
+      <head>
+        <title>Internet for people, not profit — Mozilla</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8"></meta>
+        <meta property="og:type" content="website">
+        <meta property="og:site_name" content="Mozilla">
+        <meta property="og:url" content="https://www.mozilla.org/">
+        <meta property="og:image" content="https://example.com/preview-image">
+        <meta property="og:title" content="Internet for people, not profit">
+        <!-- We expect the test will ignore tags the parser does not recognize. -->
+        <meta property="og:locale" content="en_CA">
+        <meta property="og:description" content="Mozilla is the not-for-profit behind the lightning fast Firefox browser. We put people over profit to give everyone more power online.">
+      </head>
+      <body>
+        <p>Test page</p>
+      </body>
+      </html>
+    `,
+    {
+      siteName: "Mozilla",
+      image: "https://example.com/preview-image",
+      data: {},
+    }
+  );
+});
+
+add_task(async function test_type_movie() {
+  await verifyPageData(
+    `
+      <html>
+      <head>
+        <title>Code Rush (TV Movie 2000)</title>
+        <meta property="og:url" content="https://www.imdb.com/title/tt0499004/"/>
+        <!-- Omitting og:site_name to test that the parser doesn't break on missing tags. -->
+        <meta property="og:title" content="Code Rush (TV Movie 2000) - IMDb"/>
+        <meta property="og:description" content="This is the description of the movie."/>
+        <meta property="og:type" content="video.movie"/>
+        <meta property="og:image" content="https://example.com/preview-code-rush"/>
+        <meta property="og:image:height" content="750"/>
+        <meta property="og:image:width" content="1000"/>
+      </head>
+      <body>
+        <p>Test page</p>
+      </body>
+      </html>
+    `,
+    {
+      image: "https://example.com/preview-code-rush",
+      data: {},
+    }
+  );
+});
---
+++ browser/components/pagedata/tests/unit/test_schemaorg.js
@@ -0,0 +1,91 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * Tests that the page data service can parse schema.org metadata.
+ */
+
+add_task(async function test_single_product_data() {
+  await verifyPageData(
+    `
+      <html>
+      <head>
+      <title>Product Info 1</title>
+      <meta http-equiv="Content-Type" content="text/html;charset=utf-8"></meta>
+      </head>
+      <body>
+        <div itemscope itemtype="https://schema.org/Product">
+          <img itemprop="image" src="bon-echo-microwave-17in.jpg" />
+          <a href="microwave.html" itemprop="url">
+            <span itemprop="name">Bon Echo Microwave</span>
+          </a>
+
+          <span itemprop="price" content="3.00">£3.00</span>
+          <span itemprop="priceCurrency" content="GBP"></span>
+
+          <span itemprop="gtin" content="13572468"></span>
+        </div>
+      </body>
+      </html>
+    `,
+    {
+      image: BASE_URL + "/bon-echo-microwave-17in.jpg",
+      data: {
+        [PageDataSchema.DATA_TYPE.PRODUCT]: {
+          name: "Bon Echo Microwave",
+          price: {
+            value: 3,
+            currency: "GBP",
+          },
+        },
+      },
+    }
+  );
+});
+
+add_task(async function test_single_multiple_data() {
+  await verifyPageData(
+    `
+      <html>
+      <head>
+      <title>Product Info 2</title>
+      <meta http-equiv="Content-Type" content="text/html;charset=utf-8"></meta>
+      </head>
+      <body>
+        <div itemscope itemtype="https://schema.org/Product">
+          <img itemprop="image" src="bon-echo-microwave-17in.jpg" />
+          <a href="microwave.html" itemprop="url">
+            <span itemprop="name">Bon Echo Microwave</span>
+          </a>
+
+          <span itemprop="price" content="3.00">£3.00</span>
+          <span itemprop="priceCurrency" content="GBP"></span>
+
+          <span itemprop="gtin" content="13572468"></span>
+        </div>
+        <div itemscope itemtype="http://schema.org/Product">
+          <img itemprop="image" src="gran-paradiso-toaster-17in.jpg" />
+          <a href="toaster.html" itemprop="url">
+            <span itemprop="name">Gran Paradiso Toaster</span>
+          </a>
+
+          <span itemprop="gtin" content="15263748"></span>
+        </div>
+      </body>
+      </html>
+    `,
+    {
+      image: BASE_URL + "/bon-echo-microwave-17in.jpg",
+      data: {
+        [PageDataSchema.DATA_TYPE.PRODUCT]: {
+          name: "Bon Echo Microwave",
+          price: {
+            value: 3,
+            currency: "GBP",
+          },
+        },
+      },
+    }
+  );
+});
