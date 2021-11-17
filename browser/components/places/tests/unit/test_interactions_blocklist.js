7e3e57ac8370886149e5e2547bc65e35a9c0ebe2
Brendan Dahl <brendan.dahl@gmail.com>
2021-11-17 22:00:04 +0000

    Bug 1741700 - Block interactions for oauth pages. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D131424

--- browser/components/places/tests/unit/test_interactions_blocklist.js
+++ browser/components/places/tests/unit/test_interactions_blocklist.js
@@ -17,12 +17,17 @@ let BLOCKED_URLS = [
   "https://mozilla.zoom.us/j/123456789",
   "https://yandex.az/search/?text=mozilla",
   "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&ch=&tn=baidu&bar=&wd=mozilla&rn=&fenlei=256&oq=&rsv_pq=970f2b8f001757b9&rsv_t=1f5d2V2o80HPdZtZnhodwkc7nZXTvDI1zwdPy%2FAeomnvFFGIrU1F3D9WoK4&rqlang=cn",
+  "https://accounts.google.com/o/oauth2/v2/auth/identifier/foobar",
+  "https://auth.mozilla.auth0.com/login/foobar",
+  "https://accounts.google.com/signin/oauth/consent/foobar",
+  "https://login.microsoftonline.com/common/oauth2/v2.0/authorize/foobar",
 ];
 
 let ALLOWED_URLS = [
   "https://example.com",
   "https://zoom.us/pricing",
   "https://www.google.ca/maps/place/Toronto,+ON/@43.7181557,-79.5181414,11z/data=!3m1!4b1!4m5!3m4!1s0x89d4cb90d7c63ba5:0x323555502ab4c477!8m2!3d43.653226!4d-79.3831843",
+  "https://example.com/https://auth.mozilla.auth0.com/login/foobar",
 ];
 
 // Tests that initializing InteractionsBlocklist loads the regexes from the
