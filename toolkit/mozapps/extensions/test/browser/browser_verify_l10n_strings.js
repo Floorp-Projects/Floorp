dbd64d955dd6af4c71409138fafea8a60aa6d67e
Harry Twyford <htwyford@mozilla.com>
2021-11-17 16:28:15 +0000

    Bug 1738325 - Uninstall expired themes from BuiltInThemes.jsm. r=dao
    This patch allows us to uninstall expired themes. It includes the uninstall logic in BuiltInThemes.ensureBuiltInThemes(), which runs on idle after startup and when about:addons is opened. It also changes aboutaddons.js so we don't show a blank Colorways section in about:addons if there are none installed. Finally, I removed the hidden pref gate controlling the Colorways feature. I initially included that to account for the possibility that we wouldn't finish the feature in time for 94 and we would've needed to uplift a simple patch to disable the dfeature. Since the feature has now shipped, there is no longer a need for the pref.
    
    After colorways expire, the user gets to keep the one they had installed going into the expiry, if any. This is information that would usually be associated with the profile. However, the themes are built-in (i.e. not in the profile). I get around this by saving a list of retained themes in a pref. This has drawbacks, such as making it possible to recover expired themes in about:config. I ran this by Product, and they're okay with it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130517

--- toolkit/mozapps/extensions/test/browser/browser_verify_l10n_strings.js
+++ toolkit/mozapps/extensions/test/browser/browser_verify_l10n_strings.js
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+XPCOMUtils.defineLazyModuleGetters(this, {
+  BuiltInThemes: "resource:///modules/BuiltInThemes.jsm",
+});
+
 // Maps add-on descriptors to updated Fluent IDs. Keep it in sync
 // with the list in XPIDatabase.jsm.
 const updatedAddonFluentIds = new Map([
@@ -38,24 +42,19 @@ add_task(async function test_ensure_bundled_addons_are_localized() {
     }
   }
 
-  // We verify colorway themes separately so we can more easily remove or modify
-  // this section when they are removed.
-  let colorwayBuiltInThemes = addons.filter(
-    addon =>
-      addon.isBuiltin &&
-      addon.type === "theme" &&
-      addon.id.endsWith("colorway@mozilla.org")
-  );
-  ok(!!colorwayBuiltInThemes.length, "Colorway themes should exist");
-  for (let colorwayTheme of colorwayBuiltInThemes) {
-    let l10nId = colorwayTheme.id.replace("@mozilla.org", "");
+  let colorwayThemes = Array.from(
+    BuiltInThemes.builtInThemeMap.keys()
+  ).filter(id => id.endsWith("colorway@mozilla.org"));
+  ok(!!colorwayThemes.length, "Colorway themes should exist");
+  for (let id of colorwayThemes) {
+    let l10nId = id.replace("@mozilla.org", "");
     let [, variantName] = l10nId.split("-", 2);
     let defaultFluentId = `extension-colorways-${variantName}-name`;
     let fluentId =
       updatedAddonFluentIds.get(defaultFluentId) || defaultFluentId;
     ok(
       bundle.hasMessage(fluentId),
-      `l10n id for ${colorwayTheme.id} \"name\" attribute should exist`
+      `l10n id for ${id} \"name\" attribute should exist`
     );
   }
 });
