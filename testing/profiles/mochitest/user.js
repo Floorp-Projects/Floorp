a64e50c9c167e154df1046eb08c3e5627394ea63
Niklas Goegge <ngogge@mozilla.com>
2021-11-17 11:04:34 +0000

    Bug 1617611: Enable SameSite=Lax by default in mochitests. r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D129162

--- testing/profiles/mochitest/user.js
+++ testing/profiles/mochitest/user.js
@@ -2,9 +2,6 @@
 /* globals user_pref */
 /* eslint quotes: 0 */
 
-// XXX: Bug 1617611 - Fix all the tests broken by "cookies SameSite=lax by default"
-user_pref("network.cookie.sameSite.laxByDefault", false);
-
 // Enable blocking access to storage from tracking resources by default.
 // We don't want to run mochitest using BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN (5 - aka Dynamic First Party Isolation) yet.
 user_pref("network.cookie.cookieBehavior", 4);
