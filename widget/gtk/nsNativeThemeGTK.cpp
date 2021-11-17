b5e40c1aab113e224440e72a646a104705965893
Emilio Cobos Álvarez <emilio@crisal.io>
2021-11-17 01:24:02 +0000

    Bug 1741499 - Remove HeadlessThemeGTK. r=spohl
    We can just use the non-native theme, which is much more complete than
    this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D131276

--- widget/gtk/nsNativeThemeGTK.cpp
+++ widget/gtk/nsNativeThemeGTK.cpp
@@ -4,7 +4,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "nsNativeThemeGTK.h"
-#include "HeadlessThemeGTK.h"
 #include "nsStyleConsts.h"
 #include "gtkdrawing.h"
 #include "ScreenHelperGTK.h"
@@ -2006,10 +2005,11 @@ already_AddRefed<nsITheme> do_GetNativeThemeDoNotUseDirectly() {
   static nsCOMPtr<nsITheme> inst;
 
   if (!inst) {
+    auto scrollbars = MakeUnique<ScrollbarDrawingGTK>();
     if (gfxPlatform::IsHeadless()) {
-      inst = new HeadlessThemeGTK();
+      inst = new nsNativeBasicTheme(std::move(scrollbars));
     } else {
-      inst = new nsNativeThemeGTK(MakeUnique<ScrollbarDrawingGTK>());
+      inst = new nsNativeThemeGTK(std::move(scrollbars));
     }
     ClearOnShutdown(&inst);
   }
