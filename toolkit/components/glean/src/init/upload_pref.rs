c3bbf0819e54c1b6a063508d06bb3e715938e0f8
Chris H-C <chutten@mozilla.com>
2021-11-18 19:03:24 +0000

    Bug 1737157 - Allow recording but not uploading in FOG tests r=janerik
    See https://firefox-source-docs.mozilla.org/toolkit/components/glean/dev/preferences.html#test-only-preferences
    for details.
    
    Differential Revision: https://phabricator.services.mozilla.com/D129553

--- toolkit/components/glean/src/init/upload_pref.rs
+++ toolkit/components/glean/src/init/upload_pref.rs
@@ -4,6 +4,7 @@
 
 use std::ffi::CStr;
 use std::os::raw::c_char;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 use nserror::{nsresult, NS_ERROR_FAILURE, NS_OK};
 use nsstring::{nsACString, nsCStr};
@@ -11,6 +12,10 @@ use xpcom::{
     interfaces::{nsIPrefBranch, nsISupports},
     RefPtr, XpCom,
 };
+
+/// Whether the current value of the localhost testing pref is permitting
+/// metric recording (even if upload is disabled).
+static RECORDING_ENABLED: AtomicBool = AtomicBool::new(false);
 
 // Partially cargo-culted from https://searchfox.org/mozilla-central/rev/598e50d2c3cd81cd616654f16af811adceb08f9f/security/manager/ssl/cert_storage/src/lib.rs#1192
 #[derive(xpcom)]
@@ -21,6 +26,10 @@ pub(crate) struct InitUploadPrefObserver {}
 #[allow(non_snake_case)]
 impl UploadPrefObserver {
     pub(crate) fn begin_observing() -> Result<(), nsresult> {
+        // Ensure we begin with the correct current value of RECORDING_ENABLED.
+        let recording_enabled = static_prefs::pref!("telemetry.fog.test.localhost_port") < 0;
+        RECORDING_ENABLED.store(recording_enabled, Ordering::SeqCst);
+
         // SAFETY: Everything here is self-contained.
         //
         // * We allocate the pref observer, created by the xpcom macro
@@ -37,6 +46,10 @@ impl UploadPrefObserver {
             (*pref_branch)
                 .AddObserverImpl(pref_nscstr, pref_obs.coerce(), false)
                 .to_result()?;
+            let pref_nscstr = &nsCStr::from("telemetry.fog.test.localhost_port") as &nsACString;
+            (*pref_branch)
+                .AddObserverImpl(pref_nscstr, pref_obs.coerce(), false)
+                .to_result()?;
         }
 
         Ok(())
@@ -50,7 +63,7 @@ impl UploadPrefObserver {
     ) -> nserror::nsresult {
         let topic = CStr::from_ptr(topic).to_str().unwrap();
         // Conversion utf16 to utf8 is messy.
-        // We should only ever observe changes to the one pref we want,
+        // We should only ever observe changes to one of the two prefs we want,
         // but just to be on the safe side let's assert.
 
         // cargo-culted from https://searchfox.org/mozilla-central/rev/598e50d2c3cd81cd616654f16af811adceb08f9f/security/manager/ssl/cert_storage/src/lib.rs#1606-1612
@@ -62,12 +75,23 @@ impl UploadPrefObserver {
             Err(_) => return NS_ERROR_FAILURE,
         };
         log::info!("Observed {:?}, {:?}", topic, pref_name);
+        debug_assert!(topic == "nsPref:changed");
         debug_assert!(
-            topic == "nsPref:changed" && pref_name == "datareporting.healthreport.uploadEnabled"
+            pref_name == "datareporting.healthreport.uploadEnabled"
+                || pref_name == "telemetry.fog.test.localhost_port"
         );
 
         let upload_enabled = static_prefs::pref!("datareporting.healthreport.uploadEnabled");
-        glean::set_upload_enabled(upload_enabled);
+        let recording_enabled = static_prefs::pref!("telemetry.fog.test.localhost_port") < 0;
+        if RECORDING_ENABLED.load(Ordering::SeqCst) && !recording_enabled {
+            // Whenever the test pref goes from permitting recording to forbidding it,
+            // ensure Glean is told to wipe the stores.
+            // This may send a "deletion-request" ping for a client_id that's never sent
+            // any other pings.
+            glean::set_upload_enabled(false);
+        }
+        RECORDING_ENABLED.store(recording_enabled, Ordering::SeqCst);
+        glean::set_upload_enabled(upload_enabled || recording_enabled);
         NS_OK
     }
 }
