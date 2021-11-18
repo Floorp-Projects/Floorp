a394a45fa9a8bd02508af1e6131856798e4a5cd2
Andrew Osmond <aosmond@mozilla.com>
2021-11-18 23:29:10 +0000

    Bug 1741997 - DMABufSurface::FenceDelete should not require GL context to close mSyncFd handle. r=stransky a=pascalc
    We can create DMABufSurface objects via deserialization without ever
    setting a GL context. We must ensure we close the file handle even if we
    don't have one.
    
    Differential Revision: https://phabricator.services.mozilla.com/D131561

--- widget/gtk/DMABufSurface.cpp
+++ widget/gtk/DMABufSurface.cpp
@@ -204,16 +204,16 @@ already_AddRefed<DMABufSurface> DMABufSurface::CreateDMABufSurface(
 }
 
 void DMABufSurface::FenceDelete() {
+  if (mSyncFd > 0) {
+    close(mSyncFd);
+    mSyncFd = -1;
+  }
+
   if (!mGL) {
     return;
   }
   const auto& gle = gl::GLContextEGL::Cast(mGL);
   const auto& egl = gle->mEgl;
-
-  if (mSyncFd > 0) {
-    close(mSyncFd);
-    mSyncFd = -1;
-  }
 
   if (mSync) {
     egl->fDestroySync(mSync);
