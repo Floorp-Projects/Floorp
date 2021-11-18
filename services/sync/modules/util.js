171dfc226e8d7cd787b4f7facc1d89a28290bcb9
Barret Rennie <barret@brennie.ca>
2021-11-18 18:17:13 +0000

    Bug 1736175 - Use PathUtils for generating paths in sync engines r=markh
    In bug 1649604, JSONFile was rewritten to use IOUtils and PathUtils for file IO
    and path management. This means that all path operations go through nsIFile
    methods. However, sync engines were generating paths that always contained a
    forward slash. If that file is a UNC path (i.e., if your profile is located on
    a network drive), all IOUtils and PathUtils operations on that drive will fail
    due to nsLocalFile::InitWithPath on Windows rejecting paths contaiing a forward
    slash with NS_ERROR_FILE_UNRECOGNIZED_PATH. This only occurred with UNC paths
    because OS.Path.normalize would normalize forward slashes to backslashes,
    except when the path is a UNC path. This meant that you could not use FxA sync
    with a profile on a network drive.
    
    Updating these engines to use PathUtils to join the paths instead of hardcoding
    forward slashes fixes this issue and allows sync to work with profiles on
    network drives.
    
    Differential Revision: https://phabricator.services.mozilla.com/D131166

--- services/sync/modules/util.js
+++ services/sync/modules/util.js
@@ -348,9 +348,11 @@ var Utils = {
     ).slice(0, SYNC_KEY_DECODED_LENGTH);
   },
 
-  jsonFilePath(filePath) {
-    return OS.Path.normalize(
-      OS.Path.join(OS.Constants.Path.profileDir, "weave", filePath + ".json")
+  jsonFilePath(...args) {
+    return PathUtils.join(
+      Services.dirsvc.get("ProfD", Ci.nsIFile).path,
+      "weave",
+      ...args
     );
   },
 
@@ -368,7 +370,7 @@ var Utils = {
    *        Promise resolved when the write has been performed.
    */
   async jsonLoad(filePath, that) {
-    let path = Utils.jsonFilePath(filePath);
+    let path = Utils.jsonFilePath(`${filePath}.json`);
 
     if (that._log && that._log.trace) {
       that._log.trace("Loading json from disk: " + filePath);
