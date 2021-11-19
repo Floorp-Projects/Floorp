e98256b22860b4879396cbc9fbf08e8a41ee6a1d
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2021-11-19 08:33:43 +0000

    Bug 1726804 - Refactor PROCESS_TYPE_* from nsICrashService r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D121389

---
+++ toolkit/components/crashes/gen_process_pings.py
@@ -0,0 +1,27 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from geckoprocesstypes import process_types
+
+
+def main(output):
+    output.write("""  let processPings = {""")
+
+    for p in process_types:
+        string_name = p.string_name
+        if p.string_name == "default":
+            string_name = "main"
+        output.write(
+            """
+    "%(proctype)s": %(crashping)s,"""
+            % {
+                "proctype": string_name,
+                "crashping": "true" if p.crash_ping else "false",
+            }
+        )
+    output.write(
+        """
+  };
+"""
+    )
