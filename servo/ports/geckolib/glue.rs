4036549369b8f51f344167533d96d929d59b071a
Emilio Cobos Álvarez <emilio@crisal.io>
2021-11-19 16:11:54 +0000

    Bug 1739464 - Use first generic rather than only generic to determine fallback font family. r=jfkthame
    This seems like more sensible behavior. We have another use of
    only_generic(), but that affects font sizing and other browsers agree
    with us there:
    
      <div style="font-family: monospace">Should be 13px</div>
      <div style="font-family: something, monospace">Should be 16px</div>
    
    So not touching that one.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130732

--- servo/ports/geckolib/glue.rs
+++ servo/ports/geckolib/glue.rs
@@ -4130,6 +4130,9 @@ pub extern "C" fn Servo_ComputedValues_EqualForCachedAnonymousContentStyle(
     // rules in minimal-xul.css, but which makes no difference for the
     // anonymous content subtrees we cache style for.
     differing_properties.remove(LonghandId::XLang);
+    // Similarly, -x-lang can influence the font-family fallback we have for
+    // the initial font-family so remove it as well.
+    differing_properties.remove(LonghandId::FontFamily);
 
     // Ignore any difference in pref-controlled, inherited properties.  These
     // properties may or may not be set by the 'all' declaration in the
