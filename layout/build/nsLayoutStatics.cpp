fcd2e0951869d43e7c2f3940fa2b1c663c5e949b
Deian Stefan <deian@cs.ucsd.edu>
2021-11-19 21:28:53 +0000

    Bug 1688452 - Retrofit nsExpatDriver to use RLBoxed libexpat r=tjr,peterv,bholley,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D104658

--- layout/build/nsLayoutStatics.cpp
+++ layout/build/nsLayoutStatics.cpp
@@ -128,6 +128,8 @@
 #include "RestoreTabContentObserver.h"
 #include "mozilla/intl/nsComplexBreaker.h"
 
+#include "nsRLBoxExpatDriver.h"
+
 using namespace mozilla;
 using namespace mozilla::net;
 using namespace mozilla::dom;
@@ -293,6 +295,8 @@ nsresult nsLayoutStatics::Initialize() {
   RestoreTabContentObserver::Initialize();
 
   ComplexBreaker::Initialize();
+
+  RLBoxExpatSandboxPool::Initialize();
 
   return NS_OK;
 }
