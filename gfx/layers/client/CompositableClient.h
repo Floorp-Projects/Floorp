1b44e05e9245b4aea49a6af9b15b6933385225a1
sotaro <sotaro.ikeda.g@gmail.com>
2021-11-19 07:41:29 +0000

    Bug 1741784 - Remove unused code around CompositableHost r=gfx-reviewers,nical
    Differential Revision: https://phabricator.services.mozilla.com/D131466

--- gfx/layers/client/CompositableClient.h
+++ gfx/layers/client/CompositableClient.h
@@ -53,11 +53,6 @@ class ContentClientRemoteBuffer;
  * Connect() creates the underlying IPDL actor (see CompositableChild) and the
  * corresponding CompositableHost on the other side.
  *
- * To do in-transaction texture transfer (the default), call
- * ShadowLayerForwarder::Attach(CompositableClient*, ShadowableLayer*). This
- * will let the LayerComposite on the compositor side know which
- * CompositableHost to use for compositing.
- *
  * To do async texture transfer (like async-video), the CompositableClient
  * should be created with a different CompositableForwarder (like
  * ImageBridgeChild) and attachment is done with
