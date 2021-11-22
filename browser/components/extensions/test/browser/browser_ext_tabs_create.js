6bd6cb5587a0638bd9be3c43b5d449dd1534dacd
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2021-11-22 14:26:32 +0000

    Bug 1742351: Update more browser/ tests to be compliant with https-first mode r=extension-reviewers,Gijs,robwu
    Differential Revision: https://phabricator.services.mozilla.com/D131773

--- browser/components/extensions/test/browser/browser_ext_tabs_create.js
+++ browser/components/extensions/test/browser/browser_ext_tabs_create.js
@@ -51,22 +51,22 @@ add_task(async function test_create_options() {
 
           let tests = [
             {
-              create: { url: "http://example.com/" },
-              result: { url: "http://example.com/" },
+              create: { url: "https://example.com/" },
+              result: { url: "https://example.com/" },
             },
             {
-              create: { url: "view-source:http://example.com/" },
-              result: { url: "view-source:http://example.com/" },
+              create: { url: "view-source:https://example.com/" },
+              result: { url: "view-source:https://example.com/" },
             },
             {
               create: { url: "blank.html" },
               result: { url: browser.runtime.getURL("bg/blank.html") },
             },
             {
-              create: { url: "http://example.com/", openInReaderMode: true },
+              create: { url: "https://example.com/", openInReaderMode: true },
               result: {
                 url: `about:reader?url=${encodeURIComponent(
-                  "http://example.com/"
+                  "https://example.com/"
                 )}`,
               },
             },
