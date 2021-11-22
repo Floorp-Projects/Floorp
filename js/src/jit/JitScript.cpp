4b7ac1ca93794bb99c48d34caaac4c057176e32a
André Bargull <andre.bargull@gmail.com>
2021-11-22 11:26:45 +0000

    Bug 1742102 - Part 3: Clean-up includes for JitScript. r=jandem
    "JitScript.h" was only including "TrialInlining.h" because the destructor
    needs to see the full definition of `InliningRoot`. When moving the
    destructor into the cpp-file, we can avoid that include. The constructor is
    already placed in the cpp-file, which gives a strong indication that it's not
    necessary to inline the destructor.
    
    Reducing the number of includes for "JitScript.h" is helpful, because that file
    is included in many other files through its transitive include in "JSScript-inl.h".
    And because "TrialInlining.h" includes "JSScript.h", it makes sense to avoid
    including "TrialInlining.h" unless absolutely necessary.
    
    Drive-by change:
    - Use IWYU to fix the includes in JitScript.h and JitScript-inl.h
    
    Differential Revision: https://phabricator.services.mozilla.com/D131612

--- js/src/jit/JitScript.cpp
+++ js/src/jit/JitScript.cpp
@@ -18,6 +18,7 @@
 #include "jit/JitFrames.h"
 #include "jit/JitSpewer.h"
 #include "jit/ScriptFromCalleeToken.h"
+#include "jit/TrialInlining.h"
 #include "vm/BytecodeUtil.h"
 #include "vm/FrameIter.h"  // js::OnlyJSJitFrameIter
 #include "vm/JSScript.h"
@@ -48,6 +49,20 @@ JitScript::JitScript(JSScript* script, Offset fallbackStubsOffset,
     setIonScriptImpl(script, IonDisabledScriptPtr);
   }
 }
+
+#ifdef DEBUG
+JitScript::~JitScript() {
+  // The contents of the stub space are removed and freed separately after the
+  // next minor GC. See prepareForDestruction.
+  MOZ_ASSERT(jitScriptStubSpace_.isEmpty());
+
+  // BaselineScript and IonScript must have been destroyed at this point.
+  MOZ_ASSERT(!hasBaselineScript());
+  MOZ_ASSERT(!hasIonScript());
+}
+#else
+JitScript::~JitScript() = default;
+#endif
 
 bool JSScript::createJitScript(JSContext* cx) {
   MOZ_ASSERT(!hasJitScript());
