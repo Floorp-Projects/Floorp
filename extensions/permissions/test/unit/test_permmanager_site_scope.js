94d822ee22e343fad709fe2ea29fe418a6c646fe
Benjamin VanderSloot <bvandersloot@mozilla.com>
2021-11-23 16:18:42 +0000

    Bug 1731739 part 2 - Use site as key for 3rdPartyStorage permissions, rather than origin   r=anti-tracking-reviewers,pbz,timhuang
    - Adding browser tests to verify correct behavior in integration
        - New test that fails on previous version: toolkit/components/antitracking/test/browser/browser_storageAccessScopeSameSiteWrite.js
    - Add the ability to store permission by site, use 3rdPartyStorage for this
    - No change is made to permission reads. These already proceed recursively, which eventually reach the site.
    - When fetching all permissions for a principal, also look for site-scoped permissions on its site's principal
    
    Differential Revision: https://phabricator.services.mozilla.com/D130675

---
+++ extensions/permissions/test/unit/test_permmanager_site_scope.js
@@ -0,0 +1,120 @@
+const TEST_SITE_URI = Services.io.newURI("http://example.com");
+const TEST_FQDN_1_URI = Services.io.newURI("http://test1.example.com");
+const TEST_FQDN_2_URI = Services.io.newURI("http://test2.example.com");
+const TEST_OTHER_URI = Services.io.newURI("http://example.net");
+const TEST_PERMISSION = "3rdPartyStorage^https://example.org";
+
+add_task(async function do_test() {
+  let pm = Services.perms;
+
+  let principal = Services.scriptSecurityManager.createContentPrincipal(
+    TEST_SITE_URI,
+    {}
+  );
+
+  let subdomain1Principal = Services.scriptSecurityManager.createContentPrincipal(
+    TEST_FQDN_1_URI,
+    {}
+  );
+
+  let subdomain2Principal = Services.scriptSecurityManager.createContentPrincipal(
+    TEST_FQDN_2_URI,
+    {}
+  );
+
+  let otherPrincipal = Services.scriptSecurityManager.createContentPrincipal(
+    TEST_OTHER_URI,
+    {}
+  );
+
+  // Set test permission for site
+  pm.addFromPrincipal(principal, TEST_PERMISSION, pm.ALLOW_ACTION);
+
+  // Check normal site permission
+  Assert.equal(
+    Ci.nsIPermissionManager.ALLOW_ACTION,
+    pm.testPermissionFromPrincipal(principal, TEST_PERMISSION)
+  );
+
+  // Check subdomain permission
+  Assert.equal(
+    Ci.nsIPermissionManager.ALLOW_ACTION,
+    pm.testPermissionFromPrincipal(subdomain1Principal, TEST_PERMISSION)
+  );
+
+  // Check other site permission
+  Assert.equal(
+    Ci.nsIPermissionManager.UNKNOWN_ACTION,
+    pm.testPermissionFromPrincipal(otherPrincipal, TEST_PERMISSION)
+  );
+
+  // Remove the permission from the site
+  pm.removeFromPrincipal(principal, TEST_PERMISSION);
+  Assert.equal(
+    pm.testPermissionFromPrincipal(principal, TEST_PERMISSION),
+    Ci.nsIPermissionManager.UNKNOWN_ACTION
+  );
+  Assert.equal(
+    pm.testPermissionFromPrincipal(subdomain1Principal, TEST_PERMISSION),
+    Ci.nsIPermissionManager.UNKNOWN_ACTION
+  );
+
+  // Set test permission for subdomain
+  pm.addFromPrincipal(subdomain1Principal, TEST_PERMISSION, pm.ALLOW_ACTION);
+
+  // Check normal site permission
+  Assert.equal(
+    Ci.nsIPermissionManager.ALLOW_ACTION,
+    pm.testPermissionFromPrincipal(principal, TEST_PERMISSION)
+  );
+
+  // Check subdomain permission
+  Assert.equal(
+    Ci.nsIPermissionManager.ALLOW_ACTION,
+    pm.testPermissionFromPrincipal(subdomain1Principal, TEST_PERMISSION)
+  );
+
+  // Check other subdomain permission
+  Assert.equal(
+    Ci.nsIPermissionManager.ALLOW_ACTION,
+    pm.testPermissionFromPrincipal(subdomain2Principal, TEST_PERMISSION)
+  );
+
+  // Check other site permission
+  Assert.equal(
+    Ci.nsIPermissionManager.UNKNOWN_ACTION,
+    pm.testPermissionFromPrincipal(otherPrincipal, TEST_PERMISSION)
+  );
+
+  // Check that subdomains include the site-scoped in the getAllForPrincipal
+  let sitePerms = pm.getAllForPrincipal(principal, TEST_PERMISSION);
+  let subdomain1Perms = pm.getAllForPrincipal(
+    subdomain1Principal,
+    TEST_PERMISSION
+  );
+  let subdomain2Perms = pm.getAllForPrincipal(
+    subdomain2Principal,
+    TEST_PERMISSION
+  );
+  let otherSitePerms = pm.getAllForPrincipal(otherPrincipal, TEST_PERMISSION);
+
+  Assert.equal(sitePerms.length, 1);
+  Assert.equal(subdomain1Perms.length, 1);
+  Assert.equal(subdomain2Perms.length, 1);
+  Assert.equal(otherSitePerms.length, 0);
+
+  // Remove the permission from the subdomain
+  pm.removeFromPrincipal(subdomain1Principal, TEST_PERMISSION);
+  Assert.equal(
+    pm.testPermissionFromPrincipal(principal, TEST_PERMISSION),
+    Ci.nsIPermissionManager.UNKNOWN_ACTION
+  );
+  Assert.equal(
+    pm.testPermissionFromPrincipal(subdomain1Principal, TEST_PERMISSION),
+    Ci.nsIPermissionManager.UNKNOWN_ACTION
+  );
+  Assert.equal(
+    pm.testPermissionFromPrincipal(subdomain2Principal, TEST_PERMISSION),
+    Ci.nsIPermissionManager.UNKNOWN_ACTION
+  );
+});
