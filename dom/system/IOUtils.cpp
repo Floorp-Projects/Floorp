2273b83e80606ce56d3aa0e74b4c35f0d36e54d3
Barret Rennie <barret@brennie.ca>
2021-11-23 14:33:27 +0000

    Bug 1742429 - On Windows, assert IOUtils/PathUtils paths do not contain forward slashes r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D131812

--- dom/system/IOUtils.cpp
+++ dom/system/IOUtils.cpp
@@ -32,6 +32,7 @@
 #include "mozilla/Utf8.h"
 #include "mozilla/dom/IOUtilsBinding.h"
 #include "mozilla/dom/Promise.h"
+#include "PathUtils.h"
 #include "nsCOMPtr.h"
 #include "nsError.h"
 #include "nsFileStreams.h"
@@ -56,14 +57,15 @@
 #  include "nsSystemInfo.h"
 #endif
 
-#define REJECT_IF_INIT_PATH_FAILED(_file, _path, _promise)               \
-  do {                                                                   \
-    if (nsresult _rv = (_file)->InitWithPath((_path)); NS_FAILED(_rv)) { \
-      (_promise)->MaybeRejectWithOperationError(                         \
-          FormatErrorMessage(_rv, "Could not parse path (%s)",           \
-                             NS_ConvertUTF16toUTF8(_path).get()));       \
-      return (_promise).forget();                                        \
-    }                                                                    \
+#define REJECT_IF_INIT_PATH_FAILED(_file, _path, _promise)            \
+  do {                                                                \
+    if (nsresult _rv = PathUtils::InitFileWithPath((_file), (_path)); \
+        NS_FAILED(_rv)) {                                             \
+      (_promise)->MaybeRejectWithOperationError(                      \
+          FormatErrorMessage(_rv, "Could not parse path (%s)",        \
+                             NS_ConvertUTF16toUTF8(_path).get()));    \
+      return (_promise).forget();                                     \
+    }                                                                 \
   } while (0)
 
 static constexpr auto SHUTDOWN_ERROR =
@@ -319,7 +321,7 @@ RefPtr<SyncReadFile> IOUtils::OpenFileForSyncReading(GlobalObject& aGlobal,
   MOZ_RELEASE_ASSERT(!NS_IsMainThread());
 
   nsCOMPtr<nsIFile> file = new nsLocalFile();
-  if (nsresult rv = file->InitWithPath(aPath); NS_FAILED(rv)) {
+  if (nsresult rv = PathUtils::InitFileWithPath(file, aPath); NS_FAILED(rv)) {
     aRv.ThrowOperationError(FormatErrorMessage(
         rv, "Could not parse path (%s)", NS_ConvertUTF16toUTF8(aPath).get()));
     return nullptr;
@@ -1875,8 +1877,8 @@ IOUtils::InternalWriteOpts::FromBinding(const WriteOptions& aOptions) {
 
   if (aOptions.mBackupFile.WasPassed()) {
     opts.mBackupFile = new nsLocalFile();
-    if (nsresult rv =
-            opts.mBackupFile->InitWithPath(aOptions.mBackupFile.Value());
+    if (nsresult rv = PathUtils::InitFileWithPath(opts.mBackupFile,
+                                                  aOptions.mBackupFile.Value());
         NS_FAILED(rv)) {
       return Err(IOUtils::IOError(rv).WithMessage(
           "Could not parse path of backupFile (%s)",
@@ -1886,7 +1888,8 @@ IOUtils::InternalWriteOpts::FromBinding(const WriteOptions& aOptions) {
 
   if (aOptions.mTmpPath.WasPassed()) {
     opts.mTmpFile = new nsLocalFile();
-    if (nsresult rv = opts.mTmpFile->InitWithPath(aOptions.mTmpPath.Value());
+    if (nsresult rv = PathUtils::InitFileWithPath(opts.mTmpFile,
+                                                  aOptions.mTmpPath.Value());
         NS_FAILED(rv)) {
       return Err(IOUtils::IOError(rv).WithMessage(
           "Could not parse path of temp file (%s)",
