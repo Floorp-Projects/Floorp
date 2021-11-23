84328498715c11f7c691d0907533263f0bd2dfcd
Lars T Hansen <lhansen@mozilla.com>
2021-11-23 07:55:31 +0000

    Bug 1742383 - Enable memory64 in Nightly. r=yury
    Push it out of the nest.
    
    Differential Revision: https://phabricator.services.mozilla.com/D131791

--- js/public/WasmFeatures.h
+++ js/public/WasmFeatures.h
@@ -136,15 +136,15 @@
                /* flag predicate     */ WasmSimdFlag(cx),                     \
                /* shell flag         */ "relaxed-simd",                       \
                /* preference name    */ "relaxed_simd")                       \
-  EXPERIMENTAL(/* capitalized name   */ Memory64,                             \
-               /* lower case name    */ memory64,                             \
-               /* compile predicate  */ WASM_MEMORY64_ENABLED,                \
-               /* compiler predicate */ BaselineAvailable(cx) ||              \
-                  IonAvailable(cx),                                           \
-               /* flag predicate     */ !IsFuzzingIon(cx) &&                  \
-                   !IsFuzzingCranelift(cx),                                   \
-               /* shell flag         */ "memory64",                           \
-               /* preference name    */ "memory64")                           \
+  DEFAULT(/* capitalized name   */ Memory64,                                  \
+          /* lower case name    */ memory64,                                  \
+          /* compile predicate  */ WASM_MEMORY64_ENABLED,                     \
+          /* compiler predicate */ BaselineAvailable(cx) ||                   \
+              IonAvailable(cx),                                               \
+          /* flag predicate     */ !IsFuzzingIon(cx) &&                       \
+              !IsFuzzingCranelift(cx),                                        \
+          /* shell flag         */ "memory64",                                \
+          /* preference name    */ "memory64")                                \
   EXPERIMENTAL(/* capitalized name   */ MozIntGemm,                           \
                /* lower case name    */ mozIntGemm,                           \
                /* compile predicate  */ WASM_MOZ_INTGEMM_ENABLED,             \
