9e8b7497c4bcfc0d653b736766cfbbaa28ba2a46
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-11-23 18:56:55 +0000

    Bug 1605330 - [devtools] Enable context selector on Nightly. r=ochameau.
    We take this opportunity to merge the browser toolbox and content toolbox preferences
    into a single one.
    
    Depends on D131600
    
    Differential Revision: https://phabricator.services.mozilla.com/D131770

--- devtools/client/webconsole/test/browser/browser_jsterm_evaluation_context_selector_iframe_picker.js
+++ devtools/client/webconsole/test/browser/browser_jsterm_evaluation_context_selector_iframe_picker.js
@@ -17,7 +17,7 @@ const TEST_URI = `https://example.com/document-builder.sjs?html=${encodeURICompo
 
 add_task(async function() {
   // Enable the context selector and the frames button.
-  await pushPref("devtools.contenttoolbox.webconsole.input.context", true);
+  await pushPref("devtools.webconsole.input.context", true);
   await pushPref("devtools.command-button-frames.enabled", true);
 
   const hud = await openNewTabAndConsole(TEST_URI);
