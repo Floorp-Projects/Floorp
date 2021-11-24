d04cb7f7ba22b58f91d7d221fd35e7afe97fd646
mcarare <mihai.carare.dev@gmail.com>
2021-11-24 17:19:28 +0200

    [focus] For https://github.com/mozilla-mobile/focus-android/issues/5653: Remove unused telemetry from main settings screen.
    Removed onSharedPreferenceChanged because there are no items that change in the screen,
    the screen is used exclusively for navigating to other settings screens.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/SettingsFragment.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/SettingsFragment.kt
@@ -4,7 +4,6 @@
 
 package org.mozilla.focus.settings
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
@@ -16,12 +15,11 @@ import org.mozilla.focus.R
 import org.mozilla.focus.ext.requireComponents
 import org.mozilla.focus.state.AppAction
 import org.mozilla.focus.state.Screen
-import org.mozilla.focus.telemetry.TelemetryWrapper
 import org.mozilla.focus.utils.AppConstants
 import org.mozilla.focus.utils.SupportUtils
 import org.mozilla.focus.whatsnew.WhatsNew
 
-class SettingsFragment : BaseSettingsFragment(), SharedPreferences.OnSharedPreferenceChangeListener {
+class SettingsFragment : BaseSettingsFragment() {
 
     override fun onCreatePreferences(bundle: Bundle?, s: String?) {
         setHasOptionsMenu(true)
@@ -33,14 +31,7 @@ class SettingsFragment : BaseSettingsFragment(), SharedPreferences.OnSharedPrefe
 
         (requireActivity() as AppCompatActivity).supportActionBar?.customView
 
-        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
-
         updateTitle(R.string.menu_settings)
-    }
-
-    override fun onPause() {
-        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
-        super.onPause()
     }
 
     override fun onPreferenceTreeClick(preference: androidx.preference.Preference): Boolean {
@@ -60,10 +51,6 @@ class SettingsFragment : BaseSettingsFragment(), SharedPreferences.OnSharedPrefe
         )
 
         return super.onPreferenceTreeClick(preference)
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        TelemetryWrapper.settingsEvent(key, sharedPreferences.all[key].toString())
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
