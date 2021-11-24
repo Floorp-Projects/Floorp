79af61265559596879d8bd1e70d56e2aaed1a32b
Haiyang Xu <hxu@mozilla.com>
2021-11-24 20:50:37 +0000

    Bug 1672140 - Rename cache::ClientMetadata to cache::CacheDirectoryMetadata; r=dom-storage-reviewers,janv
    Differential Revision: https://phabricator.services.mozilla.com/D131590

--- dom/cache/FileUtils.h
+++ dom/cache/FileUtils.h
@@ -31,13 +31,13 @@ nsresult BodyCreateDir(nsIFile& aBaseDir);
 // Note that this function can only be used during the initialization of the
 // database.  We're unlikely to be able to delete the DB successfully past
 // that point due to the file being in use.
-nsresult BodyDeleteDir(const ClientMetadata& aClientMetadata,
+nsresult BodyDeleteDir(const CacheDirectoryMetadata& aDirectoryMetadata,
                        nsIFile& aBaseDir);
 
 // Returns a Result with a success value with the body id and, optionally, the
 // copy context.
 Result<std::pair<nsID, nsCOMPtr<nsISupports>>, nsresult> BodyStartWriteStream(
-    const ClientMetadata& aClientMetadata, nsIFile& aBaseDir,
+    const CacheDirectoryMetadata& aDirectoryMetadata, nsIFile& aBaseDir,
     nsIInputStream& aSource, void* aClosure, nsAsyncCopyCallbackFun aCallback);
 
 void BodyCancelWrite(nsISupports& aCopyContext);
@@ -45,19 +45,19 @@ void BodyCancelWrite(nsISupports& aCopyContext);
 nsresult BodyFinalizeWrite(nsIFile& aBaseDir, const nsID& aId);
 
 Result<NotNull<nsCOMPtr<nsIInputStream>>, nsresult> BodyOpen(
-    const ClientMetadata& aClientMetadata, nsIFile& aBaseDir, const nsID& aId);
+    const CacheDirectoryMetadata& aDirectoryMetadata, nsIFile& aBaseDir,
+    const nsID& aId);
 
-nsresult BodyMaybeUpdatePaddingSize(const ClientMetadata& aClientMetadata,
-                                    nsIFile& aBaseDir, const nsID& aId,
-                                    uint32_t aPaddingInfo,
-                                    int64_t* aPaddingSizeInOut);
+nsresult BodyMaybeUpdatePaddingSize(
+    const CacheDirectoryMetadata& aDirectoryMetadata, nsIFile& aBaseDir,
+    const nsID& aId, uint32_t aPaddingInfo, int64_t* aPaddingSizeInOut);
 
-nsresult BodyDeleteFiles(const ClientMetadata& aClientMetadata,
+nsresult BodyDeleteFiles(const CacheDirectoryMetadata& aDirectoryMetadata,
                          nsIFile& aBaseDir, const nsTArray<nsID>& aIdList);
 
-nsresult BodyDeleteOrphanedFiles(const ClientMetadata& aClientMetadata,
-                                 nsIFile& aBaseDir,
-                                 const nsTArray<nsID>& aKnownBodyIdList);
+nsresult BodyDeleteOrphanedFiles(
+    const CacheDirectoryMetadata& aDirectoryMetadata, nsIFile& aBaseDir,
+    const nsTArray<nsID>& aKnownBodyIdList);
 
 // If aCanRemoveFiles is true, that means we are safe to touch the files which
 // can be accessed in other threads.
@@ -65,53 +65,54 @@ nsresult BodyDeleteOrphanedFiles(const ClientMetadata& aClientMetadata,
 // created by other threads. Note that if the files are not expected, we should
 // be safe to remove them in any case.
 template <typename Func>
-nsresult BodyTraverseFiles(const Maybe<ClientMetadata>& aClientMetadata,
-                           nsIFile& aBodyDir, const Func& aHandleFileFunc,
-                           bool aCanRemoveFiles, bool aTrackQuota = true);
+nsresult BodyTraverseFiles(
+    const Maybe<CacheDirectoryMetadata>& aDirectoryMetadata, nsIFile& aBodyDir,
+    const Func& aHandleFileFunc, bool aCanRemoveFiles, bool aTrackQuota = true);
 
 // XXX Remove this method when all callers properly wrap aClientMetadata with
 // Some/Nothing
 template <typename Func>
-nsresult BodyTraverseFiles(const ClientMetadata& aClientMetadata,
+nsresult BodyTraverseFiles(const CacheDirectoryMetadata& aDirectoryMetadata,
                            nsIFile& aBodyDir, const Func& aHandleFileFunc,
                            bool aCanRemoveFiles, bool aTrackQuota = true) {
-  return BodyTraverseFiles(Some(aClientMetadata), aBodyDir, aHandleFileFunc,
+  return BodyTraverseFiles(Some(aDirectoryMetadata), aBodyDir, aHandleFileFunc,
                            aCanRemoveFiles, aTrackQuota);
 }
 
-nsresult CreateMarkerFile(const ClientMetadata& aClientMetadata);
+nsresult CreateMarkerFile(const CacheDirectoryMetadata& aDirectoryMetadata);
 
-nsresult DeleteMarkerFile(const ClientMetadata& aClientMetadata);
+nsresult DeleteMarkerFile(const CacheDirectoryMetadata& aDirectoryMetadata);
 
-bool MarkerFileExists(const ClientMetadata& aClientMetadata);
+bool MarkerFileExists(const CacheDirectoryMetadata& aDirectoryMetadata);
 
-nsresult RemoveNsIFileRecursively(const Maybe<ClientMetadata>& aClientMetadata,
-                                  nsIFile& aFile, bool aTrackQuota = true);
+nsresult RemoveNsIFileRecursively(
+    const Maybe<CacheDirectoryMetadata>& aDirectoryMetadata, nsIFile& aFile,
+    bool aTrackQuota = true);
 
 // XXX Remove this method when all callers properly wrap aClientMetadata with
 // Some/Nothing
-inline nsresult RemoveNsIFileRecursively(const ClientMetadata& aClientMetadata,
-                                         nsIFile& aFile,
-                                         bool aTrackQuota = true) {
-  return RemoveNsIFileRecursively(Some(aClientMetadata), aFile, aTrackQuota);
+inline nsresult RemoveNsIFileRecursively(
+    const CacheDirectoryMetadata& aDirectoryMetadata, nsIFile& aFile,
+    bool aTrackQuota = true) {
+  return RemoveNsIFileRecursively(Some(aDirectoryMetadata), aFile, aTrackQuota);
 }
 
 // Delete a file that you think exists. If the file doesn't exist, an error
 // will not be returned, but warning telemetry will be generated! So only call
 // this on files that you know exist (idempotent usage, but it's not
 // recommended).
-nsresult RemoveNsIFile(const Maybe<ClientMetadata>& aClientMetadata,
+nsresult RemoveNsIFile(const Maybe<CacheDirectoryMetadata>& aDirectoryMetadata,
                        nsIFile& aFile, bool aTrackQuota = true);
 
 // XXX Remove this method when all callers properly wrap aClientMetadata with
 // Some/Nothing
-inline nsresult RemoveNsIFile(const ClientMetadata& aClientMetadata,
+inline nsresult RemoveNsIFile(const CacheDirectoryMetadata& aDirectoryMetadata,
                               nsIFile& aFile, bool aTrackQuota = true) {
-  return RemoveNsIFile(Some(aClientMetadata), aFile, aTrackQuota);
+  return RemoveNsIFile(Some(aDirectoryMetadata), aFile, aTrackQuota);
 }
 
-void DecreaseUsageForClientMetadata(const ClientMetadata& aClientMetadata,
-                                    int64_t aUpdatingSize);
+void DecreaseUsageForDirectoryMetadata(
+    const CacheDirectoryMetadata& aDirectoryMetadata, int64_t aUpdatingSize);
 
 /**
  * This function is used to check if the directory padding file is existed.
