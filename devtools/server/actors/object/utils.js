9cf2c0002fe29eb6b2a6afee3d85fff8210a9c86
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-11-25 15:03:19 +0000

    Bug 1741804 - [devtools] Fix getPropNamesFromObject + storage performance issues. r=jdescottes.
    The function, which is only used from previewers.js to retrieve
    the first then interesting properties of an object, has a special
    case for (local|session)Storage so we loop through their entries
    and not their properties.
    We're using storage#key to achieve this, but the problem is that
    the function is quite slow, and if the storage object has a lot
    of entries, it can end up freezing Firefox.
    In order to avoid such freeze, for storage object, we're not returning
    an array but an iterable object, which most of the time will only be
    called 10 times and prevent any perf issue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D131913

--- devtools/server/actors/object/utils.js
+++ devtools/server/actors/object/utils.js
@@ -327,29 +327,39 @@ function getPropsForEvent(className) {
  *
  * @param obj
  * @param rawObj
- * @returns {Array}
+ * @returns {Array|Iterable} If rawObj is localStorage/sessionStorage, we don't return an
+ *          array but an iterable object (with the proper `length` property) to avoid
+ *          performance issues.
  */
 function getPropNamesFromObject(obj, rawObj) {
-  let names = [];
-
   try {
     if (isStorage(obj)) {
       // local and session storage cannot be iterated over using
       // Object.getOwnPropertyNames() because it skips keys that are duplicated
       // on the prototype e.g. "key", "getKeys" so we need to gather the real
       // keys using the storage.key() function.
-      for (let j = 0; j < rawObj.length; j++) {
-        names.push(rawObj.key(j));
-      }
-    } else {
-      names = obj.getOwnPropertyNames();
+      // As the method is pretty slow, we return an iterator here, so we don't consume
+      // more than we need, especially since we're calling this from previewers in which
+      // we only need the first 10 entries for the preview (See Bug 1741804).
+
+      // Still return the proper number of entries.
+      const length = rawObj.length;
+      const iterable = { length };
+      iterable[Symbol.iterator] = function*() {
+        for (let j = 0; j < length; j++) {
+          yield rawObj.key(j);
+        }
+      };
+      return iterable;
     }
+
+    return obj.getOwnPropertyNames();
   } catch (ex) {
     // Calling getOwnPropertyNames() on some wrapped native prototypes is not
     // allowed: "cannot modify properties of a WrappedNative". See bug 952093.
   }
 
-  return names;
+  return [];
 }
 
 /**
