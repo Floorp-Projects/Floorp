768bd96e060e9912e29731553de2dc71449e8d99
Tim Huang <tihuang@mozilla.com>
2021-11-25 13:11:32 +0000

    Bug 1731982 - Part 9: Add a test to ensure that the dedicated/shared worker can be intercepted by a service worker. r=asuth
    The test verifies that if a dedicated/shared worker can be intercepted by a
    service worker.
    
    Differential Revision: https://phabricator.services.mozilla.com/D129058

---
+++ dom/serviceworkers/test/simple_fetch_worker.js
@@ -0,0 +1,18 @@
+// A simple worker script that forward intercepted url to the controlled window.
+
+function responseMsg(msg) {
+  self.clients
+    .matchAll({
+      includeUncontrolled: true,
+      type: "window",
+    })
+    .then(clients => {
+      if (clients && clients.length) {
+        clients[0].postMessage(msg);
+      }
+    });
+}
+
+onfetch = function(e) {
+  responseMsg(e.request.url);
+};
