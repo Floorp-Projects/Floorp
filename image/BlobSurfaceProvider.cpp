63aba5e38889bd31d831f4d36401746469c73b0a
Andrew Osmond <aosmond@mozilla.com>
2021-11-27 11:47:38 +0000

    Bug 1711061 - Part 8. Rename SourceSurfaceBlobImage.h/cpp to BlobSurfaceProvider.h/cpp. r=tnikkel
    Rename the file in preparation for the switch over to
    WebRenderImageProvider.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126601

--- image/BlobSurfaceProvider.cpp
+++ image/BlobSurfaceProvider.cpp
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "SourceSurfaceBlobImage.h"
+#include "BlobSurfaceProvider.h"
 #include "AutoRestoreSVGState.h"
 #include "ImageRegion.h"
 #include "SVGDocumentWrapper.h"
