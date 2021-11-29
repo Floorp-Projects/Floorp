c42ca6fd4a723aa64ac4797e83e557199c537413
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2021-11-29 18:06:32 +0000

    Bug 1742941: Update tests wtihin mobile/android/ to work with https-first enabled r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D132154

--- mobile/android/components/extensions/test/xpcshell/head.js
+++ mobile/android/components/extensions/test/xpcshell/head.js
@@ -16,6 +16,11 @@ XPCOMUtils.defineLazyModuleGetters(this, {
 // Remove this pref once bug 1535365 is fixed.
 Services.prefs.setBoolPref("extensions.webextensions.remote", false);
 
+// https_first automatically upgrades http to https, but the tests are not
+// designed to expect that. And it is not easy to change that because
+// nsHttpServer does not support https (bug 1742061). So disable https_first.
+Services.prefs.setBoolPref("dom.security.https_first", false);
+
 ExtensionTestUtils.init(this);
 
 Services.io.offline = true;
