5df3a0360afbbb85dd82367131ef2228bb9695e7
Hubert Boma Manilla <hmanilla@mozilla.com>
2021-11-29 16:41:42 +0000

    Bug 1690450 - [devtools] Add pref for blackboxing lines r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D132052

--- devtools/client/debugger/src/utils/prefs.js
+++ devtools/client/debugger/src/utils/prefs.js
@@ -72,6 +72,7 @@ if (isNode()) {
   pref("devtools.debugger.features.inline-preview", true);
   pref("devtools.debugger.features.overlay-step-buttons", true);
   pref("devtools.debugger.features.frame-step", true);
+  pref("devtools.debugger.features.blackbox-lines", false);
   pref("devtools.editor.tabsize", 2);
 }
 
@@ -153,6 +154,7 @@ export const features = new PrefsHelper("devtools.debugger.features", {
   inlinePreview: ["Bool", "inline-preview"],
   windowlessServiceWorkers: ["Bool", "windowless-service-workers"],
   frameStep: ["Bool", "frame-step"],
+  blackboxLines: ["Bool", "blackbox-lines"],
 });
 
 // Import the asyncStore already spawned by the TargetMixin class
