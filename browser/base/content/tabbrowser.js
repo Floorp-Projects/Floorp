a2ec84dc5cfcf74a875447af3b754f9cb287234c
Anny Gakhokidze <agakhokidze@mozilla.com>
2021-11-30 16:31:36 +0000

    Bug 1721217 - Part 5: Change the error code when we cancel loads due to another one starting, r=nika
    This allows us to move away from using IsNavigating field in parent-controlled
    paths.  Use a new distinct error code in cases when we cancel loads in
    Canonical BC due to another load starting. This way, we know to not reset the
    urlbar if we are doing another load.
    
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1721217#c10 for longer
    explanation of what is going on here.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126845

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -6403,9 +6403,24 @@
             // before the location changed.
 
             this.mBrowser.userTypedValue = null;
-
+            // When browser.tabs.documentchannel.parent-controlled pref and SHIP
+            // are enabled and a load gets cancelled due to another one
+            // starting, the error is NS_BINDING_CANCELLED_OLD_LOAD.
+            // When these prefs are not enabled, the error is different and
+            // that's why we still want to look at the isNavigating flag.
+            // We could add a workaround and make sure that in the alternative
+            // codepaths we would also omit the same error, but considering
+            // how we will be enabling fission by default soon, we can keep
+            // using isNavigating for now, and remove it when the
+            // parent-controlled pref and SHIP are enabled by default.
+            // Bug 1725716 has been filed to consider removing isNavigating
+            // field alltogether.
             let isNavigating = this.mBrowser.isNavigating;
-            if (this.mTab.selected && !isNavigating) {
+            if (
+              this.mTab.selected &&
+              aStatus != Cr.NS_BINDING_CANCELLED_OLD_LOAD &&
+              !isNavigating
+            ) {
               gURLBar.setURI();
             }
           } else if (isSuccessful) {
