e55a6be6cd37062a6a833c194dc1bb7c19d27151
Robert Mader <robert.mader@posteo.de>
2021-11-30 18:51:05 +0000

    Bug 1618914 - Fall back to ftruncate if posix_fallocate isn't supported by filesystem, r=stransky
    Rebase of a patch by Jan Beich <jbeich@FreeBSD.org>
    
    Differential Revision: https://phabricator.services.mozilla.com/D132482

--- widget/gtk/WaylandBuffer.cpp
+++ widget/gtk/WaylandBuffer.cpp
@@ -74,7 +74,10 @@ static int WaylandAllocateShmMemory(int aSize) {
   do {
     ret = posix_fallocate(fd, 0, aSize);
   } while (ret == EINTR);
-  if (ret != 0) {
+  if (ret == 0) {
+    return fd;
+  }
+  if (ret != ENODEV && ret != EINVAL && ret != EOPNOTSUPP) {
     NS_WARNING(
         nsPrintfCString("posix_fallocate() fails to allocate shm memory: %s",
                         strerror(ret))
@@ -82,7 +85,7 @@ static int WaylandAllocateShmMemory(int aSize) {
     close(fd);
     return -1;
   }
-#else
+#endif
   do {
     ret = ftruncate(fd, aSize);
   } while (ret < 0 && errno == EINTR);
@@ -93,7 +96,6 @@ static int WaylandAllocateShmMemory(int aSize) {
     close(fd);
     fd = -1;
   }
-#endif
 
   return fd;
 }
