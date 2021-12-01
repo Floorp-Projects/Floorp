c7975ad41489ce57cc4ce4cffed376b9d0f6e820
Jonatan Klemets <jonatan.r.klemets@gmail.com>
2021-12-01 18:26:11 +0000

    Bug 1722355 - Part 2: Run test262-update.py. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D126050

--- js/src/tests/test262/language/module-code/import-assertion-value-string-double.js
+++ js/src/tests/test262/language/module-code/import-assertion-value-string-double.js
@@ -1,4 +1,4 @@
-// |reftest| skip module -- import-assertions is not supported
+// |reftest| shell-option(--enable-import-assertions) skip-if(!xulRuntime.shell) module -- requires shell-options
 // Copyright (C) 2021 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
