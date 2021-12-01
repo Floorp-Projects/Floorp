398651278213dcc997f151342d223492a74a4839
Alexandru Michis <malexandru@mozilla.com>
2021-12-01 20:31:09 +0200

    Backed out 8 changesets (bug 1737198, bug 1743122) for causing xpcshell failures in unit/test_trr.js
    CLOSED TREE
    
    Backed out changeset 525922fbae63 (bug 1743122)
    Backed out changeset f370d8070271 (bug 1737198)
    Backed out changeset 84a05e5b5e8d (bug 1737198)
    Backed out changeset f7eeff0744f4 (bug 1737198)
    Backed out changeset ae860da29fd4 (bug 1737198)
    Backed out changeset 827456b1b2e8 (bug 1737198)
    Backed out changeset 2ebd361d8512 (bug 1737198)
    Backed out changeset c3b726205642 (bug 1737198)

--- netwerk/dns/TRR.cpp
+++ netwerk/dns/TRR.cpp
@@ -85,15 +85,14 @@ TRR::TRR(AHostResolver* aResolver, bool aPB)
 
 // to verify a domain
 TRR::TRR(AHostResolver* aResolver, nsACString& aHost, enum TrrType aType,
-         const nsACString& aOriginSuffix, bool aPB, bool aUseFreshConnection)
+         const nsACString& aOriginSuffix, bool aPB)
     : mozilla::Runnable("TRR"),
       mHost(aHost),
       mRec(nullptr),
       mHostResolver(aResolver),
       mType(aType),
       mPB(aPB),
-      mOriginSuffix(aOriginSuffix),
-      mUseFreshConnection(aUseFreshConnection) {
+      mOriginSuffix(aOriginSuffix) {
   MOZ_DIAGNOSTIC_ASSERT(XRE_IsParentProcess() || XRE_IsSocketProcess(),
                         "TRR must be in parent or socket process");
 }
@@ -168,10 +167,8 @@ bool TRR::MaybeBlockRequest() {
       return true;
     }
 
-    if (!StaticPrefs::network_trr_strict_native_fallback() &&
-        UseDefaultServer() &&
-        TRRService::Get()->IsTemporarilyBlocked(mHost, mOriginSuffix, mPB,
-                                                true)) {
+    if (UseDefaultServer() && TRRService::Get()->IsTemporarilyBlocked(
+                                  mHost, mOriginSuffix, mPB, true)) {
       if (mType == TRRTYPE_A) {
         // count only blocklist for A records to avoid double counts
         Telemetry::Accumulate(Telemetry::DNS_TRR_BLACKLISTED3,
@@ -267,15 +264,9 @@ nsresult TRR::SendHTTPRequest() {
     return rv;
   }
 
-  auto loadFlags = nsIRequest::LOAD_ANONYMOUS | nsIRequest::INHIBIT_CACHING |
-                   nsIRequest::LOAD_BYPASS_CACHE |
-                   nsIChannel::LOAD_BYPASS_URL_CLASSIFIER;
-  if (mUseFreshConnection) {
-    // Causes TRRServiceChannel to tell the connection manager
-    // to clear out any connection with the current conn info.
-    loadFlags |= nsIRequest::LOAD_FRESH_CONNECTION;
-  }
-  channel->SetLoadFlags(loadFlags);
+  channel->SetLoadFlags(
+      nsIRequest::LOAD_ANONYMOUS | nsIRequest::INHIBIT_CACHING |
+      nsIRequest::LOAD_BYPASS_CACHE | nsIChannel::LOAD_BYPASS_URL_CLASSIFIER);
   NS_ENSURE_SUCCESS(rv, rv);
 
   rv = channel->SetNotificationCallbacks(this);
--- netwerk/test/unit/test_odoh.js
+++ netwerk/test/unit/test_odoh.js
@@ -261,5 +261,3 @@ add_task(test_ipv6_trr_fallback);
 add_task(test_ipv4_trr_fallback);
 
 add_task(test_no_retry_without_doh);
-
-add_task(test_connection_reuse_and_cycling).skip(); // Bug 1742743
