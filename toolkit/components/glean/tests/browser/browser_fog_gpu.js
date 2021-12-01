9018ab0bb7b45c32e2e8f8e12c4de4c793453bc6
Chris H-C <chutten@mozilla.com>
2021-12-01 14:35:08 +0000

    Bug 1729026 - Test that GPU-process FOG metrics work r=Dexter,nical
    Differential Revision: https://phabricator.services.mozilla.com/D132406

---
+++ toolkit/components/glean/tests/browser/browser_fog_gpu.js
@@ -0,0 +1,36 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Glean's here on `window`, but eslint doesn't know that. bug 1715542.
+/* global Glean:false */
+
+add_task(async () => {
+  if (
+    !(await ChromeUtils.requestProcInfo()).children.some(p => p.type == "gpu")
+  ) {
+    ok(
+      true,
+      'No GPU process? No test. Try again with --setpref "layers.gpu-process.force-enabled=true".'
+    );
+    return;
+  }
+  ok(true, "GPU Process found: Let's test.");
+
+  is(
+    undefined,
+    Glean.testOnlyIpc.aCounter.testGetValue(),
+    "Ensure we begin without value."
+  );
+
+  let FOG = Cc["@mozilla.org/toolkit/glean;1"].createInstance(Ci.nsIFOG);
+  FOG.testTriggerGPUMetrics();
+  await FOG.testFlushAllChildren();
+
+  is(
+    45326, // See gfx/ipc/GPUParent.cpp's RecvTestTriggerMetrics().
+    Glean.testOnlyIpc.aCounter.testGetValue(),
+    "Ensure the GPU-process-set value shows up in the parent process."
+  );
+});
