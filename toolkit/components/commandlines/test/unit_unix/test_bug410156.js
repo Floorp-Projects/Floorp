8276b59a09ddad6e571eab58cd1600c42d3d778b
Nick Alexander <nalexander@mozilla.com>
2021-12-02 04:02:07 +0000

    Bug 1742879 - Let `Cu.createCommandLine` accept arguments and state. r=nika
    There's a lot of history in this area that mostly explains why this
    has not been supported in the past, leading to several awkward
    workarounds in tests.  But it doesn't appear that there's any reason
    to prevent tests from creating actual command lines, so that's what we
    allow here.
    
    I've elected to flesh out `Cu.createCommandLine` rather than expose
    `.init(..._)` directly since `Cu.createCommandLine` is already used in
    the relevant tests and is only used in tests.  And I've made the
    arguments required, rather than optional, so that consumers think
    about the arguments, in particular `state`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D132089

--- toolkit/components/commandlines/test/unit_unix/test_bug410156.js
+++ toolkit/components/commandlines/test/unit_unix/test_bug410156.js
@@ -3,7 +3,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 function run_test() {
-  var commandLine = Cu.createCommandLine();
+  var commandLine = Cu.createCommandLine(
+    [],
+    null,
+    Ci.nsICommandLine.STATE_INITIAL_LAUNCH
+  );
   var urlFile = do_get_file("../unit/data/test_bug410156.desktop");
   var uri = commandLine.resolveURI(urlFile.path);
   Assert.equal(uri.spec, "http://www.bug410156.com/");
