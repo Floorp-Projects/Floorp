a3db6e51999c8097c3a6538c38bac632a9c23055
Andi-Bogdan Postelnicu <andi@mozilla.com>
2021-12-05 19:04:18 +0000

    Bug 1519636 - Reformat recent changes to the Google coding style. r=sylvestre
    Updated with clang-format version 13.0.0 (taskcluster-cMIjs2JaRe6fqM2zK7ZACQ)
    
    Differential Revision: https://phabricator.services.mozilla.com/D132868

--- ipc/chromium/src/chrome/common/mach_ipc_mac.cc
+++ ipc/chromium/src/chrome/common/mach_ipc_mac.cc
@@ -20,12 +20,13 @@ struct MachSinglePortMessage {
 struct MachSinglePortMessageTrailer : MachSinglePortMessage {
   mach_msg_audit_trailer_t trailer;
 };
-}
+}  // namespace
 
 //==============================================================================
-kern_return_t MachSendPortSendRight(mach_port_t endpoint, mach_port_t attachment,
-                                    mozilla::Maybe<mach_msg_timeout_t> opt_timeout,
-                                    mach_msg_type_name_t endpoint_disposition) {
+kern_return_t MachSendPortSendRight(
+    mach_port_t endpoint, mach_port_t attachment,
+    mozilla::Maybe<mach_msg_timeout_t> opt_timeout,
+    mach_msg_type_name_t endpoint_disposition) {
   mach_msg_option_t opts = MACH_SEND_MSG;
   mach_msg_timeout_t timeout = MACH_MSG_TIMEOUT_NONE;
   if (opt_timeout) {
@@ -34,7 +35,8 @@ kern_return_t MachSendPortSendRight(mach_port_t endpoint, mach_port_t attachment
   }
 
   MachSinglePortMessage send_msg{};
-  send_msg.header.msgh_bits = MACH_MSGH_BITS(endpoint_disposition, 0) | MACH_MSGH_BITS_COMPLEX;
+  send_msg.header.msgh_bits =
+      MACH_MSGH_BITS(endpoint_disposition, 0) | MACH_MSGH_BITS_COMPLEX;
   send_msg.header.msgh_size = sizeof(send_msg);
   send_msg.header.msgh_remote_port = endpoint;
   send_msg.header.msgh_local_port = MACH_PORT_NULL;
@@ -45,16 +47,17 @@ kern_return_t MachSendPortSendRight(mach_port_t endpoint, mach_port_t attachment
   send_msg.data.disposition = MACH_MSG_TYPE_COPY_SEND;
   send_msg.data.type = MACH_MSG_PORT_DESCRIPTOR;
 
-  return mach_msg(&send_msg.header, opts, send_msg.header.msgh_size, 0, MACH_PORT_NULL, timeout,
-                  MACH_PORT_NULL);
+  return mach_msg(&send_msg.header, opts, send_msg.header.msgh_size, 0,
+                  MACH_PORT_NULL, timeout, MACH_PORT_NULL);
 }
 
 //==============================================================================
-kern_return_t MachReceivePortSendRight(const mozilla::UniqueMachReceiveRight& endpoint,
-                                       mozilla::Maybe<mach_msg_timeout_t> opt_timeout,
-                                       mozilla::UniqueMachSendRight* attachment,
-                                       audit_token_t* audit_token) {
-  mach_msg_option_t opts = MACH_RCV_MSG | MACH_RCV_TRAILER_TYPE(MACH_MSG_TRAILER_FORMAT_0) |
+kern_return_t MachReceivePortSendRight(
+    const mozilla::UniqueMachReceiveRight& endpoint,
+    mozilla::Maybe<mach_msg_timeout_t> opt_timeout,
+    mozilla::UniqueMachSendRight* attachment, audit_token_t* audit_token) {
+  mach_msg_option_t opts = MACH_RCV_MSG |
+                           MACH_RCV_TRAILER_TYPE(MACH_MSG_TRAILER_FORMAT_0) |
                            MACH_RCV_TRAILER_ELEMENTS(MACH_RCV_TRAILER_AUDIT);
   mach_msg_timeout_t timeout = MACH_MSG_TIMEOUT_NONE;
   if (opt_timeout) {
@@ -66,8 +69,9 @@ kern_return_t MachReceivePortSendRight(const mozilla::UniqueMachReceiveRight& en
   recv_msg.header.msgh_local_port = endpoint.get();
   recv_msg.header.msgh_size = sizeof(recv_msg);
 
-  kern_return_t kr = mach_msg(&recv_msg.header, opts, 0, recv_msg.header.msgh_size, endpoint.get(),
-                              timeout, MACH_PORT_NULL);
+  kern_return_t kr =
+      mach_msg(&recv_msg.header, opts, 0, recv_msg.header.msgh_size,
+               endpoint.get(), timeout, MACH_PORT_NULL);
   if (kr != KERN_SUCCESS) {
     return kr;
   }
