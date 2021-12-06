2d4a48fe5ff4c05c1639a0323c19bff0d7239375
mcarare <mihai.carare.dev@gmail.com>
2021-12-06 18:47:32 +0200

    [focus] For https://github.com/mozilla-mobile/focus-android/issues/5996: Remove unused methods for cookie option.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Settings.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Settings.kt
@@ -164,19 +164,6 @@ class Settings(
             context.getString(R.string.third_party_tracker)
         )!!
 
-    fun shouldBlockCookies(): Boolean =
-        shouldBlockCookiesValue() == resources.getString(
-            R.string.preference_privacy_should_block_cookies_yes_option2
-        )
-
-    fun shouldBlockThirdPartyCookies(): Boolean =
-        shouldBlockCookiesValue() == resources.getString(
-            R.string.preference_privacy_should_block_cookies_third_party_only_option
-        ) ||
-            shouldBlockCookiesValue() == resources.getString(
-                R.string.preference_privacy_should_block_cookies_yes_option2
-            )
-
     fun shouldShowFirstrun(): Boolean =
         !preferences.getBoolean(FirstrunFragment.FIRSTRUN_PREF, false)
 
