759137969857fe2db63de6c5f55413b02d5e2922
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2021-12-06 20:21:24 +0000

    Bug 1744533 - Fix nsICrashService refactoring regarding telemetry data r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D132956

--- toolkit/components/crashes/gen_process_pings.py
+++ toolkit/components/crashes/gen_process_pings.py
@@ -12,6 +12,8 @@ def main(output):
         string_name = p.string_name
         if p.string_name == "default":
             string_name = "main"
+        if p.string_name == "tab":
+            string_name = "content"
         output.write(
             """
     "%(proctype)s": %(crashping)s,"""
