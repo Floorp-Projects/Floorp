a31a8c2a1339a97379dd246d9913501ceebd08fc
Nika Layzell <nika@thelayzells.com>
2021-12-07 20:38:16 +0000

    Bug 1744604 - Part 3: Relax the alignas lint to be more accurate to the potential alignment issue, r=andi
    The platform in question which triggered this lint being added is the windows
    x86 ABI, which pre-msvc version 19.14 emitted an error when passing complex
    aggregate types as parameters due to not being able to align them.
    Unfortunately modern versions of Clang still have a bug in the implementation
    of the post-19.14 ABI and will pass types with incorrect alignments.
    
    This change makes the intended behaviour more clear and avoids linting against
    alignments <= the pointer alignment on all platforms, as well as alignment for
    types which will be re-aligned in the callee by clang.
    
    Finally, it also adds an exception for the `std::function` stdlib type which is
    16-byte aligned on macOS, but has a legal alignment on windows x86, and
    therefore should not be linted against as it will behave correctly on all
    platforms.
    
    In the future, if we update to a version of clang without this ABI bug, we may
    want to remove this lint entirely.
    
    Depends on D132996
    
    Differential Revision: https://phabricator.services.mozilla.com/D132997

--- build/clang-plugin/NonParamInsideFunctionDeclChecker.cpp
+++ build/clang-plugin/NonParamInsideFunctionDeclChecker.cpp
@@ -4,34 +4,70 @@
 
 #include "NonParamInsideFunctionDeclChecker.h"
 #include "CustomMatchers.h"
+#include "clang/Basic/TargetInfo.h"
 
 class NonParamAnnotation : public CustomTypeAnnotation {
 public:
   NonParamAnnotation() : CustomTypeAnnotation(moz_non_param, "non-param"){};
 
 protected:
+  // Helper for checking if a Decl has an explicitly specified alignment.
+  // Returns the alignment, in char units, of the largest alignment attribute,
+  // if it exceeds pointer alignment, and 0 otherwise.
+  unsigned checkExplicitAlignment(const Decl *D) const {
+    ASTContext &Context = D->getASTContext();
+    unsigned PointerAlign = Context.getTargetInfo().getPointerAlign(0);
+
+    // getMaxAlignment gets the largest alignment, in bits, specified by an
+    // alignment attribute directly on the declaration. If no alignment
+    // attribute is specified, it will return `0`.
+    unsigned MaxAlign = D->getMaxAlignment();
+    if (MaxAlign > PointerAlign) {
+      return Context.toCharUnitsFromBits(MaxAlign).getQuantity();
+    }
+    return 0;
+  }
+
   // Adding alignas(_) on a struct implicitly marks it as MOZ_NON_PARAM, due to
   // MSVC limitations which prevent passing explcitly aligned types by value as
   // parameters. This overload of hasFakeAnnotation injects fake MOZ_NON_PARAM
   // annotations onto these types.
   std::string getImplicitReason(const TagDecl *D,
                                 VisitFlags &ToVisit) const override {
-    // Check if the decl itself has an AlignedAttr on it.
-    for (const Attr *A : D->attrs()) {
-      if (isa<AlignedAttr>(A)) {
-        return "it has an alignas(_) annotation";
+    // Some stdlib types are known to have alignments over the pointer size on
+    // non-win32 platforms, but should not be linted against. Clear any
+    // annotations on those types.
+    if (!D->getASTContext().getTargetInfo().getCXXABI().isMicrosoft() &&
+        getDeclarationNamespace(D) == "std") {
+      StringRef Name = getNameChecked(D);
+      if (Name == "function") {
+        ToVisit = VISIT_NONE;
+        return "";
       }
     }
 
-    // Check if any of the decl's fields have an AlignedAttr on them.
+    // If the type doesn't have a destructor and can be passed in registers,
+    // clang will handle re-aligning it for us automatically, and we don't need
+    // to worry about the passed alignment.
+    auto RD = dyn_cast<CXXRecordDecl>(D);
+    if (RD && RD->canPassInRegisters()) {
+      return "";
+    }
+
+    // Check if the decl itself has an explicit alignment on it.
+    if (unsigned ExplicitAlign = checkExplicitAlignment(D)) {
+      return "it has an explicit alignment of '" +
+             std::to_string(ExplicitAlign) + "'";
+    }
+
+    // Check if any of the decl's fields have an explicit alignment on them.
     if (auto RD = dyn_cast<RecordDecl>(D)) {
       for (auto F : RD->fields()) {
-        for (auto A : F->attrs()) {
-          if (isa<AlignedAttr>(A)) {
-            return ("member '" + F->getName() +
-                    "' has an alignas(_) annotation")
-                .str();
-          }
+        if (unsigned ExplicitAlign = checkExplicitAlignment(F)) {
+          return ("member '" + F->getName() +
+                  "' has an explicit alignment of '" +
+                  std::to_string(ExplicitAlign) + "'")
+              .str();
         }
       }
     }
