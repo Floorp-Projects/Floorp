3cf336c072785c5cb9c7dfd80e8f0dcc1d4f224b
Paul Bone <pbone@mozilla.com>
2021-12-07 22:27:21 +0000

    Bug 1743713 - Fix a warning for FdPrintf in PHC r=glandium
    For a unified it is possible to include FdPrintf.cpp before FdPrintf.h which
    creates a compilation warning for FdPrintf's attributes.  FdPrintf.cpp
    should include FdPrintf.h so that the declaration always precedes the
    definition.
    
    Depends on D132913
    
    Differential Revision: https://phabricator.services.mozilla.com/D132914

--- memory/replace/logalloc/FdPrintf.cpp
+++ memory/replace/logalloc/FdPrintf.cpp
@@ -15,6 +15,7 @@
 #include <cstring>
 #include "mozilla/Assertions.h"
 #include "mozilla/Unused.h"
+#include "FdPrintf.h"
 
 /* Template class allowing a limited number of increments on a value */
 template <typename T>
