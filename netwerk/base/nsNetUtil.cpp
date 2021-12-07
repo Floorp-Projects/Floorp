e12fbe2fd030220a7c6a9b42dc013ba335622911
Valentin Gosu <valentin.gosu@gmail.com>
2021-12-07 22:52:20 +0000

    Bug 1738984 - Prevent NSS initialization and channel creation during shutdown r=keeler,a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D133065

--- netwerk/base/nsNetUtil.cpp
+++ netwerk/base/nsNetUtil.cpp
@@ -9,6 +9,7 @@
 
 #include "nsNetUtil.h"
 
+#include "mozilla/AppShutdown.h"
 #include "mozilla/Atomics.h"
 #include "mozilla/BasePrincipal.h"
 #include "mozilla/Components.h"
@@ -2706,6 +2707,11 @@ nsresult NS_GetFilenameFromDisposition(nsAString& aFilename,
 void net_EnsurePSMInit() {
   if (XRE_IsSocketProcess()) {
     EnsureNSSInitializedChromeOrContent();
+    return;
+  }
+
+  // Avoid a late initialization
+  if (AppShutdown::IsInOrBeyond(ShutdownPhase::AppShutdownNetTeardown)) {
     return;
   }
 
