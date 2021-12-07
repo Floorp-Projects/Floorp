54b71ea98ed8854fb3aff02b078b5595c0b28c26
Sebastian Kaspari <s.kaspari@gmail.com>
2021-12-07 18:50:35 +0100

    [components] Update to GeckoView 97.0

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "96.0.20211204095400"
+    const val version = "97.0.20211207040938"
 
     /**
      * GeckoView channel
