649a5b63d85ab9d9dc81a354eeeec208de7c5a64
Henri Sivonen <hsivonen@hsivonen.fi>
2021-12-08 11:34:20 +0000

    Bug 1701828 - meta charset rewrite. r=smaug
    Implements https://github.com/whatwg/html/issues/6962 . Improves performance
    when <meta charset> occurs in head but after the first kilobyte and aligns
    behavior better with WebKit and Blink.
    
    The main change is to avoid reloads when meta appears within head but
    after the first kilobyte. Prior to this change, Gecko reloaded in that
    case (in compliance with the spec!) even though WebKit and Blink did not.
    
    Differences from WebKit and Blink:
    
    * WebKit and Blink honor <meta charset> in <noscript>. This implementation
      does not.
    * WebKit and Blink look for meta as if the tree builder was unaware of
      foreign content. This implementation is foreign content-aware. This
      makes a difference for CDATA sections that contain a > before the meta
      as well as style and script elements within foreign content. This could
      happen if the CDATA section that has mysteriously been introduced around
      a what looks like a meta tag also contains another prior tag-looking
      run of text.
    * This implementation processes rel=preload and speculative loads that are
      seen before <meta charset> has been seen. WebKit and Blink instead first
      look for the meta and rewind before starting speculative parsing.
    * Unlike WebKit, if there is neither an honored meta nor syntax resembling
      an XML declaration, detection from content takes place (as in Blink).
    * Unlike Blink, if there is neither an honored meta nor syntax resembling
      an XML declaration, the detection from content is not dependent of network
      buffer boundaries.
    * Unlike Blink, detection from content can trigger a reload at the end of
      the stream if the guess made at that point differs from the first guess.
      (See below for the definition of the input to the first guess.)
    
    Differences from the old spec and Gecko previously:
    
    * Meta inside script and RCDATA elements is no longer honored.
    * Late meta is now ignored and no longer triggers a reload.
    * Later meta counts as early enough meta: In addition to the previous
      meta within the first 1024 bytes, now a meta that started within the first
      1024 bytes counts as early enough. Additionally, if by then there hasn't
      been a template start tag and head hasn't ended, meta occurring before the
      earlier of the end of the head or a template start tag counts as early
      enough.
    * Meta now counts as not-late even if the encoding label has numeric
      character reference escapes.
    * Syntax resembling an XML declaration longer than a kilobyte is honored if
      there is no honored meta.
    * If there is neither an honored meta nor syntax resembling an XML declaration,
      the initial chardetng scan is potentially longer than before: the first 1024
      bytes, the token spanning the 1024-byte boundary if there is such a token,
      and, if by then head hasn't ended and there hasn't been a template start tag
      until the end of the template start tag or the end of the token that causes
      head to end, ever comes first. However, if the token implying the end of the
      head is a text token, bytes only to the end of the previous non-text token is
      considered. (This definition avoids depending on network buffer boundaries.)
    * XML View Source now uses the code for syntax resembling an XML declaration
      instead of expat for extracting the internal encoding label.
    
    Reftest are added as both WPT and Gecko reftests in order to test both http:
    and file: URL scenarios. The Gecko tests retain the WPT <link> tags in order
    to use the exact same bytes.
    
    An encoding declaration has been added to a number of old tests that didn't
    intend to test the new speculation behavior especially in the context of
    https://bugzilla.mozilla.org/show_bug.cgi?id=1727750 .
    
    Differential Revision: https://phabricator.services.mozilla.com/D125808

--- dom/html/nsHTMLDocument.cpp
+++ dom/html/nsHTMLDocument.cpp
@@ -690,7 +690,6 @@ bool nsHTMLDocument::WillIgnoreCharsetOverride() {
     case kCharsetFromFinalAutoDetectionWouldNotHaveBeenUTF8DependedOnTLD:
     case kCharsetFromParentFrame:
     case kCharsetFromXmlDeclaration:
-    case kCharsetFromMetaPrescan:
     case kCharsetFromMetaTag:
     case kCharsetFromChannel:
       return false;
--- parser/html/nsHtml5Highlighter.h
+++ parser/html/nsHtml5Highlighter.h
@@ -31,6 +31,17 @@ class nsHtml5Highlighter {
   ~nsHtml5Highlighter();
 
   /**
+   * Set the op sink (for speculation).
+   */
+  void SetOpSink(nsAHtml5TreeOpSink* aOpSink);
+
+  /**
+   * Reset state to after generated head but before processing any of the input
+   * stream.
+   */
+  void Rewind();
+
+  /**
    * Starts the generated document.
    */
   void Start(const nsAutoString& aTitle);
@@ -146,6 +157,11 @@ class nsHtml5Highlighter {
    */
   void AddBase(nsHtml5String aValue);
 
+  /**
+   * Starts a wrapper around a run of characters.
+   */
+  void StartCharacters();
+
  private:
   /**
    * Starts a span with no class.
@@ -164,11 +180,6 @@ class nsHtml5Highlighter {
    * End the current <span> or <a> in the highlighter output.
    */
   void EndSpanOrA();
-
-  /**
-   * Starts a wrapper around a run of characters.
-   */
-  void StartCharacters();
 
   /**
    * Ends a wrapper around a run of characters.
@@ -317,7 +328,13 @@ class nsHtml5Highlighter {
   nsTArray<nsHtml5TreeOperation> mOpQueue;
 
   /**
-   * The tree op stage for the tree op executor.
+   * The tree op stage for the tree op executor or a speculation when looking
+   * for meta charset.
+   *
+   * The op sink is owned by the nsHtml5TreeOpExecutor, which outlives this
+   * object, because this object is owned by the nsHtml5Tokenizer instance that
+   * is owned by the nsHtml5StreamParser, which keeps the executor alive via
+   * nsHtml5Streamparser::mExecutorFlusher.
    */
   nsAHtml5TreeOpSink* mOpSink;
 
