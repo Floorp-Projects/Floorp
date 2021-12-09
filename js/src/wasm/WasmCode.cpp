0f908a96df5301121a78b5d5d36bc34b5a28c1eb
Lars T Hansen <lhansen@mozilla.com>
2021-12-09 14:06:02 +0000

    Bug 1744943 - Randomize stub placement to avoid icache thrashing. r=jseward
    When allocating individual stubs to a page, add a randomish amount of
    padding before the stub code to avoid thrashing the icache, which will
    happen when all the stubs are mapped to the same set.  See bug for
    a more detailed analysis.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133338

--- js/src/wasm/WasmCode.cpp
+++ js/src/wasm/WasmCode.cpp
@@ -18,6 +18,7 @@
 
 #include "wasm/WasmCode.h"
 
+#include "mozilla/Atomics.h"
 #include "mozilla/BinarySearch.h"
 #include "mozilla/EnumeratedRange.h"
 #include "mozilla/Sprintf.h"
@@ -690,6 +691,30 @@ void LazyStubSegment::addSizeOfMisc(MallocSizeOf mallocSizeOf, size_t* code,
   *data += mallocSizeOf(this);
 }
 
+// When allocating a single stub to a page, we should not always place the stub
+// at the beginning of the page as the stubs will tend to thrash the icache by
+// creating conflicts (everything ends up in the same cache set).  Instead,
+// locate stubs at different line offsets up to 3/4 the system page size (the
+// code allocation quantum).
+//
+// This may be called on background threads, hence the atomic.
+
+static void PadCodeForSingleStub(MacroAssembler& masm) {
+  // Assume 64B icache line size
+  static uint8_t zeroes[64];
+
+  // The counter serves only to spread the code out, it has no other meaning and
+  // can wrap around.
+  static mozilla::Atomic<uint32_t, mozilla::MemoryOrdering::ReleaseAcquire>
+      counter(0);
+
+  uint32_t maxPadLines = ((gc::SystemPageSize() * 3) / 4) / sizeof(zeroes);
+  uint32_t padLines = counter++ % maxPadLines;
+  for (uint32_t i = 0; i < padLines; i++) {
+    masm.appendRawCode(zeroes, sizeof(zeroes));
+  }
+}
+
 static constexpr unsigned LAZY_STUB_LIFO_DEFAULT_CHUNK_SIZE = 8 * 1024;
 
 bool LazyStubTier::createManyEntryStubs(const Uint32Vector& funcExportIndices,
@@ -702,6 +727,10 @@ bool LazyStubTier::createManyEntryStubs(const Uint32Vector& funcExportIndices,
   TempAllocator alloc(&lifo);
   JitContext jitContext(&alloc);
   WasmMacroAssembler masm(alloc);
+
+  if (funcExportIndices.length() == 1) {
+    PadCodeForSingleStub(masm);
+  }
 
   const MetadataTier& metadata = codeTier.metadata();
   const FuncExportVector& funcExports = metadata.funcExports;
@@ -883,6 +912,10 @@ bool LazyStubTier::createManyIndirectStubs(
   JitContext jitContext(&alloc);
   WasmMacroAssembler masm(alloc);
   AutoCreatedBy acb(masm, "LazyStubTier::createManyIndirectStubs");
+
+  if (targets.length() == 1) {
+    PadCodeForSingleStub(masm);
+  }
 
   CodeRangeVector codeRanges;
   for (const auto& target : targets) {
