c56b59efa9701018230d8dfa47606a8101ecb9a8
Alexandre Poirot <poirot.alex@gmail.com>
2021-12-09 14:00:39 +0000

    Bug 1740543 - Expose xpcom's GetErrorName() to JS. r=xpcom-reviewers,nika
    This will help JS codebase decipher what a given nsresult refers to.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130879

--- dom/base/ChromeUtils.h
+++ dom/base/ChromeUtils.h
@@ -88,6 +88,9 @@ class ChromeUtils {
                                 const ProfilerMarkerOptionsOrDouble& aOptions,
                                 const Optional<nsACString>& text);
 
+  static void GetXPCOMErrorName(GlobalObject& aGlobal, uint32_t aErrorCode,
+                                nsACString& aRetval);
+
   static void OriginAttributesToSuffix(
       GlobalObject& aGlobal, const dom::OriginAttributesDictionary& aAttrs,
       nsCString& aSuffix);
