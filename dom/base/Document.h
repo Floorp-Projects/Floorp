e1a3910e44b0945acfad6d6af341e9e6db7a845f
Andrew Osmond <aosmond@mozilla.com>
2021-12-09 19:25:22 +0000

    Bug 1738971 - Part 2. Add WorkerDocumentListener to get visibility events. r=dom-worker-reviewers,gfx-reviewers,jgilbert,smaug
    requestAnimationFrame callbacks are supposed to be delayed if the
    worker's owning document is no longer visible. This patch adds the
    ability for a WorkerGlobalScope to listen to changes in the visiblility
    state of the Document which owns the worker.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130263

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -276,6 +276,7 @@ enum class ViewportFitType : uint8_t;
 class WindowContext;
 class WindowGlobalChild;
 class WindowProxyHolder;
+class WorkerDocumentListener;
 class XPathEvaluator;
 class XPathExpression;
 class XPathNSResolver;
@@ -2126,6 +2127,9 @@ class Document : public nsINode,
    * aType >= FlushType::Style.
    */
   void FlushExternalResources(FlushType aType);
+
+  void AddWorkerDocumentListener(WorkerDocumentListener* aListener);
+  void RemoveWorkerDocumentListener(WorkerDocumentListener* aListener);
 
   // Triggers an update of <svg:use> element shadow trees.
   void UpdateSVGUseElementShadowTrees() {
@@ -5250,6 +5254,8 @@ class Document : public nsINode,
   RefPtr<ChromeObserver> mChromeObserver;
 
   RefPtr<HTMLAllCollection> mAll;
+
+  nsTHashSet<RefPtr<WorkerDocumentListener>> mWorkerListeners;
 
   // Pres shell resolution saved before entering fullscreen mode.
   float mSavedResolution;
