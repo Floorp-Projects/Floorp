9f874efe957225cb0985a6020c5293ef20770cfa
Emilio Cobos Álvarez <emilio@crisal.io>
2021-12-09 01:11:58 +0000

    Bug 1744195 - Centralize XUL state checks in nsNativeTheme::GetContentState. r=mstange
    This makes sure that the non-native theme and the native theme agree on
    what's e.g. disabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133195

--- widget/nsNativeTheme.h
+++ widget/nsNativeTheme.h
@@ -62,10 +62,6 @@ class nsNativeTheme : public nsITimerCallback, public nsINamed {
   bool IsWidgetStyled(nsPresContext* aPresContext, nsIFrame* aFrame,
                       mozilla::StyleAppearance aAppearance);
 
-  // Accessors to widget-specific state information
-
-  bool IsDisabled(nsIFrame* aFrame, mozilla::EventStates aEventStates);
-
   // RTL chrome direction
   static bool IsFrameRTL(nsIFrame* aFrame);
 
@@ -77,20 +73,6 @@ class nsNativeTheme : public nsITimerCallback, public nsINamed {
   }
 
   bool IsButtonTypeMenu(nsIFrame* aFrame);
-
-  // checkbox:
-  bool IsChecked(nsIFrame* aFrame) {
-    return GetCheckedOrSelected(aFrame, false);
-  }
-
-  // radiobutton:
-  bool IsSelected(nsIFrame* aFrame) {
-    return GetCheckedOrSelected(aFrame, true);
-  }
-
-  static bool IsFocused(nsIFrame* aFrame) {
-    return CheckBooleanAttr(aFrame, nsGkAtoms::focused);
-  }
 
   // scrollbar button:
   int32_t GetScrollbarButtonType(nsIFrame* aFrame);
@@ -147,8 +129,6 @@ class nsNativeTheme : public nsITimerCallback, public nsINamed {
   bool IsHorizontal(nsIFrame* aFrame);
 
   // progressbar:
-  bool IsIndeterminateProgress(nsIFrame* aFrame,
-                               mozilla::EventStates aEventStates);
   bool IsVerticalProgress(nsIFrame* aFrame);
 
   // meter:
@@ -172,9 +152,6 @@ class nsNativeTheme : public nsITimerCallback, public nsINamed {
   // Helpers for progressbar.
   static double GetProgressValue(nsIFrame* aFrame);
   static double GetProgressMaxValue(nsIFrame* aFrame);
-
-  bool GetCheckedOrSelected(nsIFrame* aFrame, bool aCheckSelected);
-  bool GetIndeterminate(nsIFrame* aFrame);
 
   bool QueueAnimatedContentForRefresh(nsIContent* aContent,
                                       uint32_t aMinimumFrameRate);
