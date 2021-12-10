57043306accb96c896832b432081225ce9549db1
Tooru Fujisawa <arai_a@mac.com>
2021-12-10 11:27:23 +0000

    Bug 1745351 - Remove JS::StencilCanLazilyParse. r=nbp
    Depends on D133463
    
    Differential Revision: https://phabricator.services.mozilla.com/D133464

--- js/public/experimental/JSStencil.h
+++ js/public/experimental/JSStencil.h
@@ -123,9 +123,6 @@ namespace JS {
 // decoding.
 extern JS_PUBLIC_API bool StencilIsBorrowed(Stencil* stencil);
 
-// Return true if the stencil is lazily parsed.
-extern JS_PUBLIC_API bool StencilCanLazilyParse(Stencil* stencil);
-
 extern JS_PUBLIC_API size_t SizeOfStencil(Stencil* stencil,
                                           mozilla::MallocSizeOf mallocSizeOf);
 
