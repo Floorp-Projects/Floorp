6606dcd5fcb1c5e944391a4c47d739dc578ea99b
Nihanth Subramanya <nhnt11@gmail.com>
2021-12-11 17:44:40 +0000

    Bug 1744900 - Make nsHostRecord::mTtl atomic. r=necko-reviewers,kershaw,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D133448

--- netwerk/dns/nsHostRecord.h
+++ netwerk/dns/nsHostRecord.h
@@ -176,7 +176,7 @@ class nsHostRecord : public mozilla::LinkedListElement<RefPtr<nsHostRecord>>,
   // but a request to refresh it will be made.
   mozilla::TimeStamp mGraceStart;
 
-  uint32_t mTtl = 0;
+  mozilla::Atomic<uint32_t, mozilla::Relaxed> mTtl{0};
 
   // The computed TRR mode that is actually used by the request.
   // It is set in nsHostResolver::NameLookup and is based on the mode of the
