0a28019a1d35a35cbba624acbd348328bd68f32b
Butkovits Atila <abutkovits@mozilla.com>
2021-12-14 06:30:49 +0200

    Backed out 2 changesets (bug 1737117) for causing bustages at nsAppRunner.cpp. CLOSED TREE
    Backed out changeset b78709bc0d0c (bug 1737117)
    Backed out changeset b3be1a6a5e3f (bug 1737117)

--- devtools/client/framework/browser-toolbox/test/browser_browser_toolbox_shouldprocessupdates.js
+++ devtools/client/framework/browser-toolbox/test/browser_browser_toolbox_shouldprocessupdates.js
@@ -41,7 +41,7 @@ add_task(async function() {
   ok(result.exists, "MOZ_TEST_PROCESS_UPDATES exists in subprocess");
   is(
     result.get,
-    "ShouldNotProcessUpdates(): DevToolsLaunching",
+    "!ShouldProcessUpdates()",
     "MOZ_TEST_PROCESS_UPDATES is correct in subprocess"
   );
 
