68d730f37805a4d35b143fdc6b0641a5842db1c9
MickeyMoz <sebastian@mozilla.com>
2021-12-14 19:32:54 +0000

    [focus] Update to Android-Components 97.0.20211214190058.

--- mobile/android/focus-android/buildSrc/src/main/java/AndroidComponents.kt
+++ mobile/android/focus-android/buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "97.0.20211214143345"
+    const val VERSION = "97.0.20211214190058"
 }
