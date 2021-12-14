43291aededd1667b8b32c374804c167b9c6fa0bf
Gabriele Svelto <gsvelto@mozilla.com>
2021-12-14 16:54:50 +0000

    Bug 1745950 - Remove the string-based KeepAliveIfHasListenersFor() implementation and convert all callers to use the atoms-based one instead r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D133748

--- dom/events/DOMEventTargetHelper.h
+++ dom/events/DOMEventTargetHelper.h
@@ -158,10 +158,8 @@ class DOMEventTargetHelper : public dom::EventTarget,
 
   virtual void LastRelease() {}
 
-  void KeepAliveIfHasListenersFor(const nsAString& aType);
   void KeepAliveIfHasListenersFor(nsAtom* aType);
 
-  void IgnoreKeepAliveIfHasListenersFor(const nsAString& aType);
   void IgnoreKeepAliveIfHasListenersFor(nsAtom* aType);
 
   void BindToOwner(nsIGlobalObject* aOwner);
@@ -176,10 +174,7 @@ class DOMEventTargetHelper : public dom::EventTarget,
   nsPIDOMWindowInner* MOZ_NON_OWNING_REF mOwnerWindow;
   bool mHasOrHasHadOwnerWindow;
 
-  struct {
-    nsTArray<nsString> mStrings;
-    nsTArray<RefPtr<nsAtom>> mAtoms;
-  } mKeepingAliveTypes;
+  nsTArray<RefPtr<nsAtom>> mKeepingAliveTypes;
 
   bool mIsKeptAlive;
 };
