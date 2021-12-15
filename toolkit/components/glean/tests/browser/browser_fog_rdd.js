768cb1333b8ba049400bdf0ba563738f261a9db7
Florian Quèze <florian@queze.net>
2021-12-15 16:27:57 +0000

    Bug 1745444 - Replace the arbitrary values in RecvTestTriggerMetrics with the PROCESS_TYPE_ constants, r=chutten.
    Differential Revision: https://phabricator.services.mozilla.com/D133509

--- toolkit/components/glean/tests/browser/browser_fog_rdd.js
+++ toolkit/components/glean/tests/browser/browser_fog_rdd.js
@@ -57,8 +57,8 @@ add_task(async () => {
   await Services.fog.testFlushAllChildren();
 
   is(
-    45327, // See dom/media/ipc/RDDParent.cpp's RecvTestTriggerMetrics().
     Glean.testOnlyIpc.aCounter.testGetValue(),
+    Ci.nsIXULRuntime.PROCESS_TYPE_RDD,
     "Ensure the RDD-process-set value shows up in the parent process."
   );
 
