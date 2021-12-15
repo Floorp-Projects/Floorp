70bda9f03829193fd808ad57ca5eee7c355cc1b3
Mike Hommey <mh+mozilla@glandium.org>
2021-12-15 21:40:22 +0000

    Bug 1744886 - Update cubeb-sys to 0.9.1. r=kinetik
    Differential Revision: https://phabricator.services.mozilla.com/D133853

--- third_party/rust/cubeb-sys/src/stream.rs
+++ third_party/rust/cubeb-sys/src/stream.rs
@@ -7,8 +7,8 @@ use callbacks::cubeb_device_changed_callback;
 use channel::cubeb_channel_layout;
 use device::cubeb_device;
 use format::cubeb_sample_format;
+use std::os::raw::{c_char, c_float, c_int, c_uint, c_void};
 use std::{fmt, mem};
-use std::os::raw::{c_float, c_int, c_uint, c_void, c_char};
 
 cubeb_enum! {
     pub enum cubeb_stream_prefs {
@@ -65,7 +65,8 @@ extern "C" {
     pub fn cubeb_stream_stop(stream: *mut cubeb_stream) -> c_int;
     pub fn cubeb_stream_get_position(stream: *mut cubeb_stream, position: *mut u64) -> c_int;
     pub fn cubeb_stream_get_latency(stream: *mut cubeb_stream, latency: *mut c_uint) -> c_int;
-    pub fn cubeb_stream_get_input_latency(stream: *mut cubeb_stream, latency: *mut c_uint) -> c_int;
+    pub fn cubeb_stream_get_input_latency(stream: *mut cubeb_stream, latency: *mut c_uint)
+        -> c_int;
     pub fn cubeb_stream_set_volume(stream: *mut cubeb_stream, volume: c_float) -> c_int;
     pub fn cubeb_stream_set_name(stream: *mut cubeb_stream, name: *const c_char) -> c_int;
     pub fn cubeb_stream_get_current_device(
