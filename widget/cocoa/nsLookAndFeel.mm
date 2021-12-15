9ea4f077719c048cd422bf76170c5cd9b0cc41bf
Emilio Cobos Álvarez <emilio@crisal.io>
2021-12-15 13:13:49 +0000

    Bug 1745360 - Back out one patch from bug bug 1743600 which was drive-by and apparently not correct in all macOS versions. a=dsmith
    MANUAL PUSH: Backout
    
    Differential Revision: https://phabricator.services.mozilla.com/D133887

--- widget/cocoa/nsLookAndFeel.mm
+++ widget/cocoa/nsLookAndFeel.mm
@@ -177,7 +177,11 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme, nscolor
       color = NS_RGB(0xFF, 0xFF, 0xFF);
       break;
     case ColorID::MozButtonactivetext:
-      color = GetColorFromNSColor(NSColor.selectedControlTextColor);
+      // Pre-macOS 12, pressed buttons were filled with the highlight color and the text was white.
+      // Starting with macOS 12, pressed (non-default) buttons are filled with medium gray and the
+      // text color is the same as in the non-pressed state.
+      color = nsCocoaFeatures::OnMontereyOrLater() ? GetColorFromNSColor(NSColor.controlTextColor)
+                                                   : NS_RGB(0xFF, 0xFF, 0xFF);
       break;
     case ColorID::Captiontext:
     case ColorID::Menutext:
