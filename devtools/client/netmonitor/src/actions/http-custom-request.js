993ac25bd28b8b33a0ee773b1227640d1c2ec9fe
Claudia <contatodaclau@gmail.com>
2021-12-15 15:59:05 +0000

    Bug 1744782 - [devtools] Add new toolbar button for the New Custom Request panel r=bomsy
    This is the first part of this bug, the next patch will include the context menu item
    
    Differential Revision: https://phabricator.services.mozilla.com/D133506

---
+++ devtools/client/netmonitor/src/actions/http-custom-request.js
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const {
+  OPEN_ACTION_BAR,
+  SELECT_ACTION_BAR_TAB,
+  PANELS,
+} = require("devtools/client/netmonitor/src/constants");
+
+/**
+ * Toggle visibility of New Custom Request panel in network panel
+ */
+function toggleHTTPCustomRequestPanel() {
+  return ({ dispatch, getState }) => {
+    const state = getState();
+
+    const shouldClose =
+      state.ui.networkActionOpen &&
+      state.ui.selectedActionBarTabId === PANELS.HTTP_CUSTOM_REQUEST;
+    dispatch({ type: OPEN_ACTION_BAR, open: !shouldClose });
+
+    dispatch({
+      type: SELECT_ACTION_BAR_TAB,
+      id: PANELS.HTTP_CUSTOM_REQUEST,
+    });
+  };
+}
+
+module.exports = {
+  toggleHTTPCustomRequestPanel,
+};
