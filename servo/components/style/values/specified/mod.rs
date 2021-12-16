e3ec8630b1c5f0c233120c181202a16e7209b8c2
Marian-Vasile Laza <mlaza@mozilla.com>
2021-12-16 04:51:13 +0200

    Backed out 4 changesets (bug 1746187) for causing xpcshell failures on test_css-properties-db.js.
    Backed out changeset fa4f889dd0d3 (bug 1746187)
    Backed out changeset e3fce2273109 (bug 1746187)
    Backed out changeset 6733677ad35d (bug 1746187)
    Backed out changeset a4375ff93414 (bug 1746187)

--- servo/components/style/values/specified/mod.rs
+++ servo/components/style/values/specified/mod.rs
@@ -90,7 +90,6 @@ pub use self::text::{InitialLetter, LetterSpacing, LineBreak, LineHeight, TextAl
 pub use self::text::{OverflowWrap, TextEmphasisPosition, TextEmphasisStyle, WordBreak};
 pub use self::text::{TextAlignKeyword, TextDecorationLine, TextOverflow, WordSpacing};
 pub use self::text::{TextDecorationLength, TextDecorationSkipInk, TextJustify, TextTransform};
-pub use self::text::HyphenateCharacter;
 pub use self::time::Time;
 pub use self::transform::{Rotate, Scale, Transform};
 pub use self::transform::{TransformOrigin, TransformStyle, Translate};
