fb92dcd19ccd53add17e2a7667e3418f3ed8fa36
Barret Rennie <barret@brennie.ca>
2021-12-16 04:47:19 +0000

    Bug 1744809 - Reduce boilerplate in IOUtils frontend methods r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D133832

--- dom/system/IOUtils.h
+++ dom/system/IOUtils.h
@@ -158,6 +158,10 @@ class IOUtils final {
   class EventQueue;
   class State;
 
+  template <typename Fn>
+  static already_AddRefed<Promise> WithPromiseAndState(GlobalObject& aGlobal,
+                                                       Fn aFn);
+
   /**
    * Dispatch a task on the event queue and resolve or reject the associated
    * promise based on the result.
