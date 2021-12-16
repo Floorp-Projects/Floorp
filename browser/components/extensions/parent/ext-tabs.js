bf78042576ca1c3602899f538d5b4aa41ff28631
Jonathan Watt <jwatt@jwatt.org>
2021-12-16 15:12:33 +0000

    Bug 1745452 - Remove the print progress dialog code. r=mstriemer,bobowen,webdriver-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D133500

--- browser/components/extensions/parent/ext-tabs.js
+++ browser/components/extensions/parent/ext-tabs.js
@@ -1356,7 +1356,6 @@ this.tabs = class extends ExtensionAPI {
                 printSettings.toFileName = picker.file.path;
 
                 printSettings.printSilent = true;
-                printSettings.showPrintProgress = false;
 
                 printSettings.outputFormat =
                   Ci.nsIPrintSettings.kOutputFormatPDF;
