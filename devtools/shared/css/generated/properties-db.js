f94b4ad3f57c2db6800a2e7422974d26e19edb3d
Marian-Vasile Laza <mlaza@mozilla.com>
2021-12-16 04:51:13 +0200

    Backed out 4 changesets (bug 1746187) for causing xpcshell failures on test_css-properties-db.js.
    Backed out changeset fa4f889dd0d3 (bug 1746187)
    Backed out changeset e3fce2273109 (bug 1746187)
    Backed out changeset 6733677ad35d (bug 1746187)
    Backed out changeset a4375ff93414 (bug 1746187)

--- devtools/shared/css/generated/properties-db.js
+++ devtools/shared/css/generated/properties-db.js
@@ -3094,7 +3094,6 @@ exports.CSS_PROPERTIES = {
       "text-underline-offset",
       "text-underline-position",
       "text-decoration-skip-ink",
-      "hyphenate-character",
       "cursor",
       "pointer-events",
       "-moz-user-input",
@@ -7275,20 +7274,6 @@ exports.CSS_PROPERTIES = {
       "min-content",
       "revert",
       "revert-layer",
-      "unset"
-    ]
-  },
-  "hyphenate-character": {
-    "isInherited": true,
-    "subproperties": [
-      "hyphenate-character"
-    ],
-    "supports": [],
-    "values": [
-      "auto",
-      "inherit",
-      "initial",
-      "revert",
       "unset"
     ]
   },
@@ -11636,10 +11621,6 @@ exports.PREFERENCES = [
   [
     "font-variation-settings",
     "layout.css.font-variations.enabled"
-  ],
-  [
-    "hyphenate-character",
-    "layout.css.hyphenate-character.enabled"
   ],
   [
     "justify-tracks",
