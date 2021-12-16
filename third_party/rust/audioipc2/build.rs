2d30cc124509a8f282c1dc92a0d3e4ad680b5ccc
Marian-Vasile Laza <mlaza@mozilla.com>
2021-12-16 06:08:19 +0200

    Backed out changeset 736011c1640c (bug 1726275) for causing build bustages. CLOSED TREE

--- third_party/rust/audioipc2/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    if std::env::var_os("CARGO_CFG_UNIX").is_some() {
-        cc::Build::new().file("src/cmsghdr.c").compile("cmsghdr");
-    }
-}
--- third_party/rust/ntapi/src/winapi_local/um/winioctl.rs
+++
@@ -1,4 +0,0 @@
-#[inline]
-pub(crate) const fn CTL_CODE(DeviceType: u32, Function: u32, Method: u32, Access: u32) -> u32 {
-    (DeviceType << 16) | (Access << 14) | (Function << 2) | Method
-}
