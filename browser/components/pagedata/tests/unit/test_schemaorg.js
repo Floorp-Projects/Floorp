e6dde4dd113a8a1357a88718aaaad35a1f924945
Dave Townsend <dtownsend@oxymoronical.com>
2021-12-16 16:27:52 +0000

    Bug 1746167: Support parsing JSON-LD page data. r=Standard8
    This is just a start and there are a number of JSON-LD features not currently
    handled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133878

--- browser/components/pagedata/tests/unit/test_schemaorg.js
+++ browser/components/pagedata/tests/unit/test_schemaorg.js
@@ -6,7 +6,7 @@
  * Tests that the page data service can parse schema.org metadata into PageData.
  */
 
-add_task(async function test_single_product_data() {
+add_task(async function test_single_product_microdata() {
   await verifyPageData(
     `
       <!DOCTYPE html>
@@ -58,7 +58,113 @@ add_task(async function test_single_product_data() {
   );
 });
 
-add_task(async function test_single_multiple_data() {
+add_task(async function test_single_product_json_ld() {
+  await verifyPageData(
+    `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <script type="application/ld+json">
+        {
+          "@context": "http://schema.org",
+          "@type": "Organization",
+          "employee": {
+            "@type": "Person",
+            "name": "Mr. Nested Name"
+          },
+          "name": "Mozilla"
+        }
+      </script>
+      <script type="application/ld+json">
+        {
+          "@context": "https://schema.org",
+          "@type": "Product",
+          "image": "bon-echo-microwave-17in.jpg",
+          "url": "microwave.html",
+          "name": "Bon Echo Microwave",
+          "offers": {
+            "@type": "Offer",
+            "price": "3.50",
+            "priceCurrency": "GBP"
+          },
+          "gtin": "13572468",
+          "description": "The most amazing microwave in the world"
+        }
+      </script>
+      </head>
+      <body>
+      </body>
+      </html>
+    `,
+    {
+      siteName: "Mozilla",
+      description: "The most amazing microwave in the world",
+      image: BASE_URL + "/bon-echo-microwave-17in.jpg",
+      data: {
+        [PageDataSchema.DATA_TYPE.PRODUCT]: {
+          name: "Bon Echo Microwave",
+          price: {
+            value: 3.5,
+            currency: "GBP",
+          },
+        },
+      },
+    }
+  );
+});
+
+add_task(async function test_single_product_combined() {
+  await verifyPageData(
+    `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <script type="application/ld+json">
+        {
+          "@context": "https://schema.org",
+          "@type": "Product",
+          "image": "bon-echo-microwave-17in.jpg",
+          "url": "microwave.html",
+          "name": "Bon Echo Microwave",
+          "offers": {
+            "@type": "Offer",
+            "price": "3.50",
+            "priceCurrency": "GBP"
+          },
+          "gtin": "13572468",
+          "description": "The most amazing microwave in the world"
+        }
+      </script>
+      </head>
+      <body>
+        <div itemscope itemtype="https://schema.org/Organization">
+          <div itemprop="employee" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Mr. Nested Name</span>
+          </div>
+
+          <span itemprop="name">Mozilla</span>
+        </div>
+      </body>
+      </html>
+    `,
+    {
+      siteName: "Mozilla",
+      description: "The most amazing microwave in the world",
+      image: BASE_URL + "/bon-echo-microwave-17in.jpg",
+      data: {
+        [PageDataSchema.DATA_TYPE.PRODUCT]: {
+          name: "Bon Echo Microwave",
+          price: {
+            value: 3.5,
+            currency: "GBP",
+          },
+        },
+      },
+    }
+  );
+});
+
+add_task(async function test_single_multiple_microdata() {
   await verifyPageData(
     `
       <!DOCTYPE html>
