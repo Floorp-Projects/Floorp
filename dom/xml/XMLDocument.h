2e132b3ea9a20c4bb3c548f014d5547f5a079bdb
Kagami Sascha Rosylight <krosylight@mozilla.com>
2021-12-17 13:27:52 +0000

    Bug 1539884 - Part 19: Mark XMLDocument::EndLoad as CAN_RUN_SCRIPT_BOUNDARY r=masayuki
    Depends on D134109
    
    Differential Revision: https://phabricator.services.mozilla.com/D134110

--- dom/xml/XMLDocument.h
+++ dom/xml/XMLDocument.h
@@ -42,7 +42,8 @@ class XMLDocument : public Document {
                                      bool aReset = true,
                                      nsIContentSink* aSink = nullptr) override;
 
-  virtual void EndLoad() override;
+  // TODO: Convert this to MOZ_CAN_RUN_SCRIPT (bug 1415230, bug 1535398)
+  MOZ_CAN_RUN_SCRIPT_BOUNDARY virtual void EndLoad() override;
 
   virtual nsresult Init() override;
 
