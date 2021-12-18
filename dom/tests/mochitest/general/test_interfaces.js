8ff2d1e61189edab83ef9f058d3b071c3af538a6
Emilio Cobos Álvarez <emilio@crisal.io>
2021-12-18 19:51:45 +0000

    Bug 1746737 - Layer interfaces are not nightly-only anymore.
    Differential Revision: https://phabricator.services.mozilla.com/D134230

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -283,9 +283,9 @@ var interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSKeyframesRule", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "CSSLayerBlockRule", insecureContext: true, nightly: true },
+  { name: "CSSLayerBlockRule", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "CSSLayerStatementRule", insecureContext: true, nightly: true },
+  { name: "CSSLayerStatementRule", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSMediaRule", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
