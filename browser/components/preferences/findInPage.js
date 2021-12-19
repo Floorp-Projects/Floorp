b32eae6796546594ea0c7a71ea0b4840d79a193d
Andrei Oprea <andrei.br92@gmail.com>
2021-12-19 19:58:35 +0000

    Bug 1745264 - More From Mozilla section should be searchable inside about:preferences r=preferences-reviewers,pdahiya,mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D133875

--- browser/components/preferences/findInPage.js
+++ browser/components/preferences/findInPage.js
@@ -568,9 +568,14 @@ var gSearchResultsPane = {
         this.listSearchTooltips.add(nodeObject);
       }
 
-      // If this is a node for an experimental feature option, add it to the list
-      // of subitems. The items that don't match the search term will be hidden.
-      if (child instanceof Element && child.classList.contains("featureGate")) {
+      // If this is a node for an experimental feature option or a Mozilla product item,
+      // add it to the list of subitems. The items that don't match the search term
+      // will be hidden.
+      if (
+        child instanceof Element &&
+        (child.classList.contains("featureGate") ||
+          child.classList.contains("mozilla-product-item"))
+      ) {
         this.subItems.set(child, result);
       }
     }
