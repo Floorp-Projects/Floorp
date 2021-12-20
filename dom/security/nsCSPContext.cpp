5bbe5dbac0942a454e698524d27df56cba4028cd
Rob Wu <rob@robwu.nl>
2021-12-20 13:46:46 +0000

    Bug 1735856 - Don't trigger violation events for frame-ancestors r=freddyb, a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D134145

--- dom/security/nsCSPContext.cpp
+++ dom/security/nsCSPContext.cpp
@@ -1442,8 +1442,12 @@ class CSPReportSenderRunnable final : public Runnable {
     }
 
     // 4) fire violation event
-    mCSPContext->FireViolationEvent(mTriggeringElement, mCSPEventListener,
-                                    init);
+    // A frame-ancestors violation has occurred, but we should not dispatch the
+    // violation event to a potentially cross-origin ancestor.
+    if (!mViolatedDirective.EqualsLiteral("frame-ancestors")) {
+      mCSPContext->FireViolationEvent(mTriggeringElement, mCSPEventListener,
+                                      init);
+    }
 
     return NS_OK;
   }
