2fc9dc8863a8e71cff27c8bfcc37ae5dff06ff9b
Byron Campen [:bwc] <docfaraday@gmail.com>
2021-12-20 17:07:11 +0000

    Bug 1714115: Re-apply fix from bug 1706261 that was lost in bug 1654112. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D134296

--- third_party/libwebrtc/rtc_base/platform_thread.cc
+++ third_party/libwebrtc/rtc_base/platform_thread.cc
@@ -142,7 +142,7 @@ bool PlatformThread::SetPriority(ThreadPriority priority) {
   RTC_DCHECK(spawned_thread_checker_.IsCurrent());
 
 #if defined(WEBRTC_WIN)
-  return SetThreadPriority(thread_, priority) != FALSE;
+  return SetThreadPriority(GetCurrentThread(), priority) != FALSE;
 #elif defined(__native_client__) || defined(WEBRTC_FUCHSIA)
   // Setting thread priorities is not supported in NaCl or Fuchsia.
   return true;
@@ -184,7 +184,7 @@ bool PlatformThread::SetPriority(ThreadPriority priority) {
       param.sched_priority = top_prio;
       break;
   }
-  return pthread_setschedparam(thread_, policy, &param) == 0;
+  return pthread_setschedparam(pthread_self(), policy, &param) == 0;
 #endif  // defined(WEBRTC_WIN)
 }
 
