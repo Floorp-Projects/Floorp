71490ef3ac4fb530788b94e1e4804f4d57700478
Chun-Min Chang <chun.m.chang@gmail.com>
2021-12-20 18:58:45 +0000

    Bug 1746893 - Relax AudioDriftCorrection's thread limitation r=padenot,pehrsons
    By the comment in `AudioDriftCorrection`: "The construction can happen
    in any thread", the `AudioDriftCorrection` class is ought to be created
    in any thread, but it's not true actually. The fact that ctor calls
    `Preferences::GetInt` makes it can only be called on main-thread, which
    is the thread `Preferences::GetInt` should be.
    
    To make `AudioDriftCorrection` work as what it's expected, the
    preference getter should be moved to the `AudioDriftCorrection`'s caller
    rather than `AudioDriftCorrection'`s ctor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122297

--- dom/media/CrossGraphPort.cpp
+++ dom/media/CrossGraphPort.cpp
@@ -8,6 +8,7 @@
 #include "AudioStreamTrack.h"
 #include "MediaTrackGraphImpl.h"
 #include "mozilla/Logging.h"
+#include "mozilla/Preferences.h"
 
 namespace mozilla {
 
@@ -159,7 +160,8 @@ CrossGraphReceiver::CrossGraphReceiver(TrackRate aSampleRate,
                                        TrackRate aTransmitterRate)
     : ProcessedMediaTrack(aSampleRate, MediaSegment::AUDIO,
                           static_cast<MediaSegment*>(new AudioSegment())),
-      mDriftCorrection(aTransmitterRate, aSampleRate) {}
+      mDriftCorrection(aTransmitterRate, aSampleRate,
+                       Preferences::GetInt("media.clockdrift.buffering", 50)) {}
 
 uint32_t CrossGraphReceiver::NumberOfChannels() const {
   return GetData<AudioSegment>()->MaxChannelCount();
