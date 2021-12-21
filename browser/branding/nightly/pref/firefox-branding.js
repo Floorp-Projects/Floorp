0b3bd225cd98410f3b918f07985bdccd11aeb848
Nicholas Rishel <nrishel@mozilla.com>
2021-12-21 20:57:08 +0000

    Bug 1746517 - Add query paramaters to signify link was clicked due to manual update for all builds. r=nalexander
    Depends on D133946
    
    Differential Revision: https://phabricator.services.mozilla.com/D134085

--- browser/branding/nightly/pref/firefox-branding.js
+++ browser/branding/nightly/pref/firefox-branding.js
@@ -13,7 +13,7 @@ pref("app.update.interval", 7200); // 2 hours
 pref("app.update.promptWaitTime", 43200);
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
-pref("app.update.url.manual", "https://www.mozilla.org/%LOCALE%/firefox/nightly/");
+pref("app.update.url.manual", "https://www.mozilla.org/%LOCALE%/firefox/nightly/?reason=manual-update");
 // A default value for the "More information about this update" link
 // supplied in the "An update is available" page of the update wizard.
 pref("app.update.url.details", "https://www.mozilla.org/%LOCALE%/firefox/nightly/notes/");
