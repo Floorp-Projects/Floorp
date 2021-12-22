d9a63c9302398d82fcc9c337fba8370ae52748e6
Erik Nordin <nordzilla@mozilla.com>
2021-12-22 19:08:38 +0000

    Bug 1743659 - Part 2: Update Gecko's Localization to use the new l10nregistry-rs API r=zbraniecki,dminor,eemeli,smaug,emilio?,eemeli?,zbraniecki
    - Updates Gecko's Fluent class to use the new ResourceId type,
      which can be either optional or required regarding a particular resource.
    - Adds JS tests verifying the new behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133579

--- intl/l10n/test/test_localization.js
+++ intl/l10n/test/test_localization.js
@@ -196,3 +196,118 @@ add_task(async function test_switch_to_async() {
   strictEqual(values[0], null);
   strictEqual(values[1], "Value2");
 });
+
+/**
+ * This test verifies that when a required resource is missing,
+ * we fallback entirely to the next locale for all entries.
+ */
+add_task(async function test_format_from_missing_required_resource() {
+  const l10nReg = new L10nRegistry();
+
+  const fs = [
+    { path: "/localization/de/browser/menu.ftl", source: `
+key-value = [de] Value1
+` },
+    { path: "/localization/de/browser/missing-in-en-US.ftl", source: `
+key-missing = [de] MissingValue
+` },
+    { path: "/localization/en-US/browser/menu.ftl", source: `
+key-value = [en] Value1
+` },
+  ];
+
+  const source = L10nFileSource.createMock("test", "app", ["de", "en-US"], "/localization/{locale}", fs);
+  l10nReg.registerSources([source]);
+
+  // returns correct contexts for [en-US, de]
+
+  let l10n = new Localization([
+    "/browser/menu.ftl",
+    "/browser/missing-in-en-US.ftl",
+  ], false, l10nReg, ["en-US", "de"]);
+
+  {
+    let values = await l10n.formatValues([
+      {id: "key-value"},
+      {id: "key-missing"},
+    ]);
+
+    strictEqual(values[0], "[de] Value1");
+    strictEqual(values[1], "[de] MissingValue");
+  }
+
+  // returns correct contexts for [de, en-US]
+
+  l10n = new Localization([
+    "/browser/menu.ftl",
+    {path: "/browser/missing-in-en-US.ftl", optional: false},
+  ], false, l10nReg, ["de", "en-US"]);
+
+  {
+    let values = await l10n.formatValues([
+      {id: "key-value"},
+      {id: "key-missing"},
+    ]);
+
+    strictEqual(values[0], "[de] Value1");
+    strictEqual(values[1], "[de] MissingValue");
+  }
+});
+
+/**
+ * This test verifies that when an optional resource is missing, we continue
+ * to populate entires from other resources in the same locale, and we only
+ * fallback entries from the missing optional resource to the next locale.
+ */
+add_task(async function test_format_from_missing_optional_resource() {
+  const l10nReg = new L10nRegistry();
+
+  const fs = [
+    { path: "/localization/de/browser/menu.ftl", source: `
+key-value = [de] Value1
+` },
+    { path: "/localization/de/browser/missing-in-en-US.ftl", source: `
+key-missing = [de] MissingValue
+` },
+    { path: "/localization/en-US/browser/menu.ftl", source: `
+key-value = [en] Value1
+` },
+  ];
+
+  const source = L10nFileSource.createMock("test", "app", ["de", "en-US"], "/localization/{locale}", fs);
+  l10nReg.registerSources([source]);
+
+  // returns correct contexts for [en-US, de]
+
+  let l10n = new Localization([
+    {path: "/browser/menu.ftl", optional: false},
+    {path: "/browser/missing-in-en-US.ftl", optional: true},
+  ], false, l10nReg, ["en-US", "de"]);
+
+  {
+    let values = await l10n.formatValues([
+      {id: "key-value"},
+      {id: "key-missing"},
+    ]);
+
+    strictEqual(values[0], "[en] Value1");
+    strictEqual(values[1], "[de] MissingValue");
+  }
+
+  // returns correct contexts for [de, en-US]
+
+  l10n = new Localization([
+    {path: "/browser/menu.ftl", optional: false},
+    {path: "/browser/missing-in-en-US.ftl", optional: true},
+  ], false, l10nReg, ["de", "en-US"]);
+
+  {
+    let values = await l10n.formatValues([
+      {id: "key-value"},
+      {id: "key-missing"},
+    ]);
+
+    strictEqual(values[0], "[de] Value1");
+    strictEqual(values[1], "[de] MissingValue");
+  }
+});
