7cfcfad32857c54608821800b9b67487aade826d
Daisuke Akatsuka <daisuke@birchill.co.jp>
2021-12-23 11:52:55 +0000

    Bug 1746832: Update MDN compat data. r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D134379

--- devtools/client/inspector/compatibility/test/browser/browser_compatibility_dynamic_js-dom-change.js
+++ devtools/client/inspector/compatibility/test/browser/browser_compatibility_dynamic_js-dom-change.js
@@ -12,11 +12,11 @@ const {
 
 // Test the behavior rules are dynamically added
 
-const ISSUE_CURSOR = {
+const ISSUE_BINDING = {
   type: COMPATIBILITY_ISSUE_TYPE.CSS_PROPERTY,
-  property: "cursor",
-  url: "https://developer.mozilla.org/docs/Web/CSS/cursor",
-  deprecated: false,
+  property: "-moz-binding",
+  url: "https://developer.mozilla.org/docs/Web/CSS/-moz-binding",
+  deprecated: true,
   experimental: false,
 };
 
@@ -32,7 +32,7 @@ const ISSUE_HYPHENS = {
 const TEST_URI = `
   <style>
     .child {
-      cursor: grab;
+      -moz-binding: none;
     }
   </style>
   <body></body>
@@ -62,8 +62,8 @@ add_task(async function() {
     inspector,
     selectedElementPane,
     allElementsPane,
-    [ISSUE_CURSOR],
-    [ISSUE_HYPHENS, ISSUE_CURSOR],
+    [ISSUE_BINDING],
+    [ISSUE_HYPHENS, ISSUE_BINDING],
     async function() {
       const doc = content.document;
       const parent = doc.querySelector("body");
