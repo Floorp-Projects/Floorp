29ae13db060a5cd57e83f13386efcae2d6dbed3b
Shane Caraveo <scaraveo@mozilla.com>
2021-12-23 18:48:09 +0000

    Bug 1733481 retry telemetry requests when a proxy fails r=chutten,robwu,necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D127170

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -5930,7 +5930,7 @@ void nsHttpChannel::MaybeResolveProxyAndBeginConnect() {
   // settings if we are never going to make a network connection.
   if (!mProxyInfo &&
       !(mLoadFlags & (LOAD_ONLY_FROM_CACHE | LOAD_NO_NETWORK_IO)) &&
-      !LoadBypassProxy() && NS_SUCCEEDED(ResolveProxy())) {
+      !BypassProxy() && NS_SUCCEEDED(ResolveProxy())) {
     return;
   }
 
