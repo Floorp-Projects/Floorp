46842b1f8b207c584a7d61d9f14086d12a0c7e3e
msub2 <msub2official@gmail.com>
2021-12-23 23:52:08 +0000

    Bug 1596921 - Use nsICanvasRenderingContextInternal::GetParentObject in WebGLContext::MakeXRCompatible. r=jgilbert
    Differential Revision: https://phabricator.services.mozilla.com/D133824

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -1456,15 +1456,7 @@ bool ClientWebGLContext::IsXRCompatible() const { return mXRCompatible; }
 already_AddRefed<dom::Promise> ClientWebGLContext::MakeXRCompatible(
     ErrorResult& aRv) {
   const FuncScope funcScope(*this, "MakeXRCompatible");
-  nsCOMPtr<nsIGlobalObject> global;
-  // TODO: Bug 1596921
-  // Should use nsICanvasRenderingContextInternal::GetParentObject
-  // once it has been updated to work in the offscreencanvas case
-  if (mCanvasElement) {
-    global = GetOwnerDoc()->GetScopeObject();
-  } else if (mOffscreenCanvas) {
-    global = mOffscreenCanvas->GetOwnerGlobal();
-  }
+  nsCOMPtr<nsIGlobalObject> global = GetParentObject();
   if (!global) {
     aRv.ThrowInvalidAccessError(
         "Using a WebGL context that is not attached to either a canvas or an "
