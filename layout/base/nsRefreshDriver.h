456bcc16e2bcf724a436f1889d84e90f7e20e9e5
Emilio Cobos Álvarez <emilio@crisal.io>
2021-12-30 15:14:22 +0000

    Bug 1745869 - Grant 1s of activity to hidden OOPIF iframes. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D134804

--- layout/base/nsRefreshDriver.h
+++ layout/base/nsRefreshDriver.h
@@ -277,7 +277,7 @@ class nsRefreshDriver final : public mozilla::layers::TransactionIdAllocator,
    */
   void Disconnect();
 
-  bool IsFrozen() { return mFreezeCount > 0; }
+  bool IsFrozen() const { return mFreezeCount > 0; }
 
   /**
    * Freeze the refresh driver.  It should stop delivering future
@@ -298,7 +298,7 @@ class nsRefreshDriver final : public mozilla::layers::TransactionIdAllocator,
    * Throttle or unthrottle the refresh driver.  This is done if the
    * corresponding presshell is hidden or shown.
    */
-  void SetThrottled(bool aThrottled);
+  void SetActivity(bool aIsActive, bool aIsInActiveTab);
 
   /**
    * Return the prescontext we were initialized with
@@ -472,6 +472,10 @@ class nsRefreshDriver final : public mozilla::layers::TransactionIdAllocator,
   void EnsureTimerStarted(EnsureTimerStartedFlags aFlags = eNone);
   void StopTimer();
 
+  bool ComputeShouldBeThrottled() const;
+  bool ShouldStopActivityGracePeriod() const;
+  void UpdateThrottledState();
+
   bool HasObservers() const;
   void AppendObserverDescriptionsToString(nsACString& aStr) const;
   // Note: This should only be called in the dtor of nsRefreshDriver.
@@ -547,6 +551,13 @@ class nsRefreshDriver final : public mozilla::layers::TransactionIdAllocator,
   mozilla::UniquePtr<mozilla::ProfileChunkedBuffer> mViewManagerFlushCause;
 
   bool mThrottled : 1;
+  bool mIsActive : 1;
+  bool mIsInActiveTab : 1;
+  // We grant a period of activity to out-of-process iframes that are in the
+  // foreground tab but inactive (hidden), in case they can set themselves up
+  // and get shown soon enough (see bug 1745869).
+  bool mIsGrantingActivityGracePeriod : 1;
+  bool mHasGrantedActivityGracePeriod : 1;
   bool mNeedToRecomputeVisibility : 1;
   bool mTestControllingRefreshes : 1;
 
@@ -600,6 +611,7 @@ class nsRefreshDriver final : public mozilla::layers::TransactionIdAllocator,
   mozilla::TimeStamp mNextThrottledFrameRequestTick;
   mozilla::TimeStamp mNextRecomputeVisibilityTick;
   mozilla::TimeStamp mBeforeFirstContentfulPaintTimerRunningLimit;
+  mozilla::TimeStamp mActivityGracePeriodStart;
 
   // separate arrays for each flush type we support
   ObserverArray mObservers[4];
