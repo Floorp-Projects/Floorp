2c4907c99f9d38c1c319fec1c11d6b089ccab1ad
Mike Hommey <mh+mozilla@glandium.org>
2021-12-30 00:54:02 +0000

    Bug 1747855 - Move --disable-updater to python configure. r=firefox-build-system-reviewers,geckoview-reviewers,mhentges,calu
    Differential Revision: https://phabricator.services.mozilla.com/D134769

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -4,7 +4,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 MOZ_APP_VENDOR=Mozilla
-MOZ_UPDATER=1
 
 if test "$OS_ARCH" = "WINNT"; then
   if ! test "$HAVE_64BIT_BUILD"; then
--- mobile/android/branding/nightly/configure.sh
+++ mobile/android/branding/nightly/configure.sh
@@ -3,4 +3,3 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 MOZ_APP_DISPLAYNAME="Firefox Nightly"
-MOZ_UPDATER=
