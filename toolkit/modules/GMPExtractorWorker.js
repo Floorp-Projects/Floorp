8fc39dd968ce0b7697dec6ac6f0437b84fbb1b1f
Barret Rennie <barret@brennie.ca>
2021-12-31 01:43:18 +0000

    Bug 1746675 - Port GMPExtractorWorker.jsm and GMPInstallManager.jsm to IOUtils r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D134471

--- toolkit/modules/GMPExtractorWorker.js
+++ toolkit/modules/GMPExtractorWorker.js
@@ -6,8 +6,6 @@
 
 "use strict";
 
-importScripts("resource://gre/modules/osfile.jsm");
-
 const FILE_ENTRY = "201: ";
 
 onmessage = async function(msg) {
@@ -43,31 +41,27 @@ onmessage = async function(msg) {
         };
         reader.readAsArrayBuffer(fileContents);
       });
-      let profileDirPath = OS.Constants.Path.profileDir;
-      let installToDirPath = OS.Path.join(
+      let profileDirPath = await PathUtils.getProfileDir();
+      let installToDirPath = PathUtils.join(
         profileDirPath,
-        msg.data.relativeInstallPath
+        ...msg.data.relativeInstallPath
       );
-      await OS.File.makeDir(installToDirPath, {
-        ignoreExisting: true,
-        unixMode: 0o755,
-        from: profileDirPath,
-      });
+      await IOUtils.makeDirectory(installToDirPath);
       // Do not extract into directories. Extract all files to the same
       // directory.
-      let destPath = OS.Path.join(installToDirPath, fileName);
-      await OS.File.writeAtomic(destPath, new Uint8Array(fileData), {
+      let destPath = PathUtils.join(installToDirPath, fileName);
+      await IOUtils.write(destPath, new Uint8Array(fileData), {
         tmpPath: destPath + ".tmp",
       });
       // Ensure files are writable and executable. Otherwise, we may be
       // unable to execute or uninstall them.
-      await OS.File.setPermissions(destPath, { unixMode: 0o700 });
-      if (OS.Constants.Sys.Name == "Darwin") {
+      await IOUtils.setPermissions(destPath, 0o700);
+      if (IOUtils.removeMacXAttr) {
         // If we're on MacOS Firefox will add the quarantine xattr to files it
         // downloads. In this case we want to clear that xattr so we can load
         // the CDM.
         try {
-          await OS.File.macRemoveXAttr(destPath, "com.apple.quarantine");
+          await IOUtils.removeMacXAttr(destPath, "com.apple.quarantine");
         } catch (e) {
           // Failed to remove the attribute. This could be because the profile
           // exists on a file system without xattr support.
