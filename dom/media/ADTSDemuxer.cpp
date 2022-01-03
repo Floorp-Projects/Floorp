0432b46db16b772fdfaf3a82f74b5ba2a0ace992
Paul Adenot <paul@paul.cx>
2022-01-03 17:27:16 +0000

    Bug 1746011 - Handle truncated ADTS stream. r=bryce, a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D133997

--- dom/media/ADTSDemuxer.cpp
+++ dom/media/ADTSDemuxer.cpp
@@ -718,8 +718,9 @@ int32_t ADTSTrackDemuxer::Read(uint8_t* aBuffer, int64_t aOffset,
 
   const int64_t streamLen = StreamLength();
   if (mInfo && streamLen > 0) {
+    int64_t max = streamLen > aOffset ? streamLen - aOffset : 0;
     // Prevent blocking reads after successful initialization.
-    aSize = std::min<int64_t>(aSize, streamLen - aOffset);
+    aSize = std::min<int64_t>(aSize, max);
   }
 
   uint32_t read = 0;
