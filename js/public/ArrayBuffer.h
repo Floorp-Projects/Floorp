df1c98251811e0bae63635b0ae448cc5c2f2765a
Matthew Gaudet <mgaudet@mozilla.com>
2022-01-04 17:58:47 +0000

    Bug 1746515 - Implement Array Buffer Clone r=sfink
    Add tests for ArrayBufferClone, as well as ArrayBufferCopyData.
    
    Adapt ArrayBufferCopyData to handle multiple compartments, and do runtime error checking.
    
    Differential Revision: https://phabricator.services.mozilla.com/D134080

--- js/public/ArrayBuffer.h
+++ js/public/ArrayBuffer.h
@@ -268,16 +268,32 @@ extern JS_PUBLIC_API void SetLargeArrayBuffersEnabled(bool enable);
 /**
  * Copy data from one array buffer to another.
  *
- * Both fromBuffer and toBuffer must be ArrayBufferObjectMaybeShared.
+ * Both fromBuffer and toBuffer must be (possibly wrapped)
+ * ArrayBufferObjectMaybeShared.
+ *
+ * This method may throw if the sizes don't match, or if unwrapping fails.
  *
  * The API for this is modelled on CopyDataBlockBytes from the spec:
  * https://tc39.es/ecma262/#sec-copydatablockbytes
  */
-extern JS_PUBLIC_API void ArrayBufferCopyData(JSContext* cx,
-                                              Handle<JSObject*> toBlock,
-                                              size_t toIndex,
-                                              Handle<JSObject*> fromBlock,
-                                              size_t fromIndex, size_t count);
+[[nodiscard]] extern JS_PUBLIC_API bool ArrayBufferCopyData(
+    JSContext* cx, Handle<JSObject*> toBlock, size_t toIndex,
+    Handle<JSObject*> fromBlock, size_t fromIndex, size_t count);
+
+/**
+ * Copy data from one array buffer to another.
+ *
+ * srcBuffer must be a (possibly wrapped) ArrayBufferObjectMaybeShared.
+ *
+ * This method may throw if unwrapping or allocation fails.
+ *
+ * The API for this is modelled on CloneArrayBuffer from the spec:
+ * https://tc39.es/ecma262/#sec-clonearraybuffer
+ */
+extern JS_PUBLIC_API JSObject* ArrayBufferClone(JSContext* cx,
+                                                Handle<JSObject*> srcBuffer,
+                                                size_t srcByteOffset,
+                                                size_t srcLength);
 
 }  // namespace JS
 
