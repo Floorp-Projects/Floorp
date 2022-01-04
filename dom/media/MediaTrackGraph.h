20e50da6765a62b0f7be55a0a198a29e0e9e8c1a
Randell Jesup <rjesup@wgate.com>
2022-01-04 13:01:14 +0000

    Bug 1746917: MediaTrackGraph cleanup r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D134319

--- dom/media/MediaTrackGraph.h
+++ dom/media/MediaTrackGraph.h
@@ -701,6 +701,8 @@ class SourceMediaTrack : public MediaTrack {
   void SetVolume(float aVolume);
   float GetVolumeLocked();
 
+  Mutex& GetMutex() { return mMutex; }
+
   friend class MediaTrackGraphImpl;
 
  protected:
