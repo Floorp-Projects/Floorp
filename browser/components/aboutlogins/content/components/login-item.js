02aad871935c3d96bb9e0896439a7804600f8273
Sergey Galich <sgalich@mozilla.com>
2022-01-04 20:10:35 +0000

    Bug 1748065 - Do not leak password length when password is concealed. r=tgiles
    Differential Revision: https://phabricator.services.mozilla.com/D134912

--- browser/components/aboutlogins/content/components/login-item.js
+++ browser/components/aboutlogins/content/components/login-item.js
@@ -242,9 +242,7 @@ export default class LoginItem extends HTMLElement {
       // In masked non-edit mode we use a different "display" element to render
       // the masked password so that one cannot simply remove/change
       // @type=password to reveal the real password.
-      this._passwordDisplayInput.value = " ".repeat(
-        this._login.password.length
-      );
+      this._passwordDisplayInput.value = "        ";
     }
 
     if (this.dataset.editing) {
@@ -845,11 +843,10 @@ export default class LoginItem extends HTMLElement {
 
     if (shouldEdit) {
       this._passwordInput.style.removeProperty("width");
-      this._passwordDisplayInput.style.removeProperty("width");
     } else {
       // Need to set a shorter width than -moz-available so the reveal checkbox
       // will still appear next to the password.
-      this._passwordDisplayInput.style.width = this._passwordInput.style.width =
+      this._passwordInput.style.width =
         (this._login.password || "").length + "ch";
     }
 
