6648e304517abf7d5cab9888285143a5fb499258
Iulian Moraru <imoraru@mozilla.com>
2022-01-04 06:41:18 +0200

    Backed out changeset f00f297558ab (bug 1745613) for causing bp-hybrid bustages on nsID. CLOSED TREE

--- xpcom/base/nsID.cpp
+++ xpcom/base/nsID.cpp
@@ -25,8 +25,7 @@
   }
   int len = static_cast<int>(aLength);
 
-  // Only try to use NSS on the main thread.
-  if (!NS_IsMainThread() || !NSS_IsInitialized()) {
+  if (!NSS_IsInitialized()) {
     return false;
   }
 
