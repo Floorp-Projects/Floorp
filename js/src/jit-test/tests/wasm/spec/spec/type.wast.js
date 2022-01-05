b104d78a99d26d016f7f8597fa5fec868a9d6336
Ryan Hunt <rhunt@eqrion.net>
2022-01-05 18:37:39 +0000

    Bug 1747450 - wasm: Update spec-tests. r=lth
    This commit did a fresh pull of all our vendored
    spec-test repositories. I updated all repositories instead
    of just the exception-handling one so that diffs between
    base and dependent proposals remained minimized.
    
    Differential Revision: https://phabricator.services.mozilla.com/D134631

--- js/src/jit-test/tests/wasm/spec/spec/type.wast.js
+++ js/src/jit-test/tests/wasm/spec/spec/type.wast.js
@@ -59,7 +59,7 @@ let $0 = instantiate(`(module
 // ./test/core/type.wast:43
 assert_malformed(
   () => instantiate(`(type (func (result i32) (param i32))) `),
-  `result before parameter`,
+  `unexpected token`,
 );
 
 // ./test/core/type.wast:47
