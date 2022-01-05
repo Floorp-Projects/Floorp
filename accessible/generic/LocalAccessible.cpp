df3b1159a6420fe96589bf2dc7bcc407172009e4
Eitan Isaacson <eitan@monotonous.org>
2022-01-05 22:30:53 +0000

    Bug 1747835 - P2: Cache text-input-type attribute for text and date/time inputs. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D134759

--- accessible/generic/LocalAccessible.cpp
+++ accessible/generic/LocalAccessible.cpp
@@ -3279,6 +3279,18 @@ already_AddRefed<AccAttributes> LocalAccessible::BundleFieldsForCache(
     // initial cache push.
     if (mContent->IsElement()) {
       fields->SetAttribute(nsGkAtoms::tag, mContent->NodeInfo()->NameAtom());
+
+      if (IsTextField() || IsDateTimeField()) {
+        // Cache text input types. Accessible is recreated if this changes,
+        // so it is considered immutable.
+        if (const nsAttrValue* attr =
+                mContent->AsElement()->GetParsedAttr(nsGkAtoms::type)) {
+          RefPtr<nsAtom> inputType = attr->GetAsAtom();
+          if (inputType) {
+            fields->SetAttribute(nsGkAtoms::textInputType, inputType);
+          }
+        }
+      }
     }
   }
 
