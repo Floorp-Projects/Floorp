d48a99e86c7bb1b00cd75181b54bb712eac77cdd
Neil Deakin <neil@mozilla.com>
2022-01-05 18:05:38 +0000

    Bug 1748483, use a controller for the commands in page info so that they are updated properly, r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D135067

--- browser/base/content/pageinfo/pageInfo.js
+++ browser/base/content/pageinfo/pageInfo.js
@@ -324,7 +324,14 @@ async function onLoadPageInfo() {
     window.arguments[0];
 
   // Init media view
-  document.getElementById("imagetree").view = gImageView;
+  let imageTree = document.getElementById("imagetree");
+  imageTree.view = gImageView;
+
+  imageTree.controllers.appendController(treeController);
+
+  document
+    .getElementById("metatree")
+    .controllers.appendController(treeController);
 
   // Select the requested tab, if the name is specified
   await loadTab(args);
@@ -1055,6 +1062,27 @@ function formatDate(datestr, unknown) {
   return dateTimeFormatter.format(date);
 }
 
+let treeController = {
+  supportsCommand(command) {
+    return command == "cmd_copy" || command == "cmd_selectAll";
+  },
+
+  isCommandEnabled(command) {
+    return true; // not worth checking for this
+  },
+
+  doCommand(command) {
+    switch (command) {
+      case "cmd_copy":
+        doCopy();
+        break;
+      case "cmd_selectAll":
+        document.activeElement.view.selection.selectAll();
+        break;
+    }
+  },
+};
+
 function doCopy() {
   if (!gClipboardHelper) {
     return;
@@ -1091,14 +1119,6 @@ function doSelectAllMedia() {
 
   if (tree) {
     tree.view.selection.selectAll();
-  }
-}
-
-function doSelectAll() {
-  var elem = document.commandDispatcher.focusedElement;
-
-  if (elem && elem.localName == "tree") {
-    elem.view.selection.selectAll();
   }
 }
 
