71f5e89cbb21e355d5b248693099097337a2c429
Joel Maher <jmaher@mozilla.com>
2022-01-06 14:19:33 +0000

    Bug 1743907 - remove webrender references from wpt/xpcshell. r=jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D135220

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -1203,12 +1203,6 @@ class XPCShellTests(object):
         else:
             self.env["MOZ_DISABLE_SOCKET_PROCESS"] = "1"
 
-        if self.enable_webrender:
-            self.env["MOZ_WEBRENDER"] = "1"
-            self.env["MOZ_ACCELERATED"] = "1"
-        else:
-            self.env["MOZ_WEBRENDER"] = "0"
-
     def buildEnvironment(self):
         """
         Create and returns a dictionary of self.env to include all the appropriate env
@@ -1524,7 +1518,6 @@ class XPCShellTests(object):
         self.mozInfo["serviceworker_e10s"] = True
 
         self.mozInfo["verify"] = options.get("verify", False)
-        self.mozInfo["webrender"] = self.enable_webrender
 
         self.mozInfo["socketprocess_networking"] = prefs.get(
             "network.http.network_access_on_socket_process.enabled", False
@@ -1643,7 +1636,6 @@ class XPCShellTests(object):
         self.failure_manifest = options.get("failure_manifest")
         self.threadCount = options.get("threadCount") or NUM_THREADS
         self.jscovdir = options.get("jscovdir")
-        self.enable_webrender = options.get("enable_webrender")
         self.headless = options.get("headless")
         self.runFailures = options.get("runFailures")
         self.timeoutAsPass = options.get("timeoutAsPass")
