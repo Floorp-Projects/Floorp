2823070215654e726b9d20a1d02e05c616cdfed8
Nika Layzell <nika@thelayzells.com>
2022-01-07 20:34:52 +0000

    Bug 1748719 - Make nsStringInputStream more flexible as to the backing data buffer, r=mccr8
    This change aims to make the way that the nsStringInputStream owns the backing
    data buffer more flexible. This has a few impacts, including allowing
    arbitrarily large payload sizes on 64-bit platforms, not requiring as complex
    checks around borrowed string buffers or nsTArray data storage, and supporting
    custom data ownership schemes such as those used by blobs.
    
    The new system uses a separate refcounted object internally to provide the
    contiguous backing buffer, with the nsStringInputStream wrapping it and
    providing the `nsIInputStream` interface and cursor. This also avoids issues
    around the buffer being mutated during reads, as mutating the `nsIInputStream`
    no longer mutates the actual data storage object.
    
    Differential Revision: https://phabricator.services.mozilla.com/D135162

--- dom/file/MemoryBlobImpl.h
+++ dom/file/MemoryBlobImpl.h
@@ -11,6 +11,7 @@
 #include "mozilla/LinkedList.h"
 #include "mozilla/StaticMutex.h"
 #include "mozilla/StaticPtr.h"
+#include "mozilla/StreamBufferSource.h"
 #include "nsCOMPtr.h"
 #include "nsICloneableInputStream.h"
 #include "nsIInputStream.h"
@@ -104,53 +105,31 @@ class MemoryBlobImpl final : public BaseBlobImpl {
     uint64_t mLength;
   };
 
-  class DataOwnerAdapter final : public nsIInputStream,
-                                 public nsISeekableStream,
-                                 public nsIIPCSerializableInputStream,
-                                 public nsICloneableInputStream {
+  class DataOwnerAdapter final : public StreamBufferSource {
     using DataOwner = MemoryBlobImpl::DataOwner;
 
    public:
-    static nsresult Create(DataOwner* aDataOwner, uint32_t aStart,
-                           uint32_t aLength, nsIInputStream** _retval);
+    static nsresult Create(DataOwner* aDataOwner, size_t aStart, size_t aLength,
+                           nsIInputStream** _retval);
 
-    NS_DECL_THREADSAFE_ISUPPORTS
-    NS_DECL_NSIIPCSERIALIZABLEINPUTSTREAM
+    Span<const char> Data() override { return mData; }
 
-    // These are mandatory.
-    NS_FORWARD_NSIINPUTSTREAM(mStream->)
-    NS_FORWARD_NSISEEKABLESTREAM(mSeekableStream->)
-    NS_FORWARD_NSITELLABLESTREAM(mSeekableStream->)
-    NS_FORWARD_NSICLONEABLEINPUTSTREAM(mCloneableInputStream->)
+    // This StreamBufferSource is owning, as the `mData` span references the
+    // immutable data buffer owned by `mDataOwner` which is being kept alive.
+    bool Owning() override { return true; }
+
+    // The memory usage from `DataOwner` is reported elsewhere, so we don't need
+    // to record it here.
+    size_t SizeOfExcludingThisEvenIfShared(MallocSizeOf) override { return 0; }
 
    private:
     ~DataOwnerAdapter() = default;
 
-    DataOwnerAdapter(DataOwner* aDataOwner, nsIInputStream* aStream,
-                     uint32_t aLength)
-        : mDataOwner(aDataOwner),
-          mStream(aStream),
-          mSeekableStream(do_QueryInterface(aStream)),
-          mSerializableInputStream(do_QueryInterface(aStream)),
-          mCloneableInputStream(do_QueryInterface(aStream)),
-          mLength(aLength) {
-      MOZ_ASSERT(
-          mSeekableStream && mSerializableInputStream && mCloneableInputStream,
-          "Somebody gave us the wrong stream!");
-    }
-
-    template <typename M>
-    void SerializeInternal(mozilla::ipc::InputStreamParams& aParams,
-                           FileDescriptorArray& aFileDescriptors,
-                           bool aDelayedStart, uint32_t aMaxSize,
-                           uint32_t* aSizeUsed, M* aManager);
+    DataOwnerAdapter(DataOwner* aDataOwner, Span<const char> aData)
+        : mDataOwner(aDataOwner), mData(aData) {}
 
     RefPtr<DataOwner> mDataOwner;
-    nsCOMPtr<nsIInputStream> mStream;
-    nsCOMPtr<nsISeekableStream> mSeekableStream;
-    nsCOMPtr<nsIIPCSerializableInputStream> mSerializableInputStream;
-    nsCOMPtr<nsICloneableInputStream> mCloneableInputStream;
-    uint32_t mLength;
+    Span<const char> mData;
   };
 
  private:
---
+++ xpcom/io/StreamBufferSource.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_StreamBufferSource_h
+#define mozilla_StreamBufferSource_h
+
+#include "mozilla/MemoryReporting.h"
+#include "nsString.h"
+#include "nsISupportsImpl.h"
+
+namespace mozilla {
+
+class StreamBufferSource {
+ public:
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(StreamBufferSource)
+
+  virtual Span<const char> Data() = 0;
+
+  virtual nsresult GetData(nsACString& aString) {
+    Span<const char> data = Data();
+    if (!aString.Assign(data.Elements(), data.Length(), fallible)) {
+      return NS_ERROR_OUT_OF_MEMORY;
+    }
+    return NS_OK;
+  }
+
+  virtual bool Owning() = 0;
+
+  virtual size_t SizeOfExcludingThisIfUnshared(MallocSizeOf aMallocSizeOf) {
+    return SizeOfExcludingThisEvenIfShared(aMallocSizeOf);
+  }
+  size_t SizeOfIncludingThisIfUnshared(MallocSizeOf aMallocSizeOf) {
+    if (mRefCnt > 1) {
+      return 0;
+    }
+    size_t n = aMallocSizeOf(this);
+    n += SizeOfExcludingThisIfUnshared(aMallocSizeOf);
+    return n;
+  }
+
+  virtual size_t SizeOfExcludingThisEvenIfShared(
+      MallocSizeOf aMallocSizeOf) = 0;
+  size_t SizeOfIncludingThisEvenIfShared(MallocSizeOf aMallocSizeOf) {
+    size_t n = aMallocSizeOf(this);
+    n += SizeOfExcludingThisEvenIfShared(aMallocSizeOf);
+    return n;
+  }
+
+ protected:
+  virtual ~StreamBufferSource() = default;
+};
+
+}  // namespace mozilla
+
+#endif  // mozilla_StreamBufferSource_h
