22b54fb91d667566cd6189a2bb9e1d84bd819c6b
Geoff Brown <gbrown@mozilla.com>
2022-01-07 15:34:02 +0000

    Bug 1744091 - Add taskgraph 'fission' transform; r=taskgraph-reviewers,jmaher
    Ensure setpref=fission.autostart set appropriately for task fission-ness. Otherwise, verify that fission enabling/disabling is appropriate for the task.
    
    Differential Revision: https://phabricator.services.mozilla.com/D134522

--- taskcluster/gecko_taskgraph/transforms/test/__init__.py
+++ taskcluster/gecko_taskgraph/transforms/test/__init__.py
@@ -396,6 +396,7 @@ def run_sibling_transforms(config, tasks):
         ("raptor", lambda t: t["suite"] == "raptor"),
         ("other", None),
         ("worker", None),
+        ("fission", None),
         # These transforms should always run last as there is never any
         # difference in configuration from one chunk to another (other than
         # chunk number).
