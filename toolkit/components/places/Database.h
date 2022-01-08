d09a64a119cc2e56fd66f2e63e08e288c1b0fb6b
Butkovits Atila <abutkovits@mozilla.com>
2022-01-08 02:50:53 +0200

    Backed out changeset 480b5274349b (bug 1748509) for causing indexdb issues. a=backout

--- toolkit/components/places/Database.h
+++ toolkit/components/places/Database.h
@@ -18,7 +18,7 @@
 
 // This is the schema version. Update it at any schema change and add a
 // corresponding migrateVxx method below.
-#define DATABASE_SCHEMA_VERSION 62
+#define DATABASE_SCHEMA_VERSION 61
 
 // Fired after Places inited.
 #define TOPIC_PLACES_INIT_COMPLETE "places-init-complete"
@@ -334,7 +334,6 @@ class Database final : public nsIObserver, public nsSupportsWeakReference {
   nsresult MigrateV59Up();
   nsresult MigrateV60Up();
   nsresult MigrateV61Up();
-  nsresult MigrateV62Up();
 
   void MigrateV52OriginFrecencies();
 
