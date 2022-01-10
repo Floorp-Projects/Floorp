292bd7f82253807726d549d6fc8c41c640201f2b
Sai Hemanth B <saihemanth9019@gmail.com>
2022-01-10 09:30:10 +0000

    Bug 1737968 - Replace lodash difference usage with filter. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D135443

--- devtools/client/debugger/src/components/PrimaryPanes/SourcesTree.js
+++ devtools/client/debugger/src/components/PrimaryPanes/SourcesTree.js
@@ -6,7 +6,6 @@
 import React, { Component } from "react";
 import classnames from "classnames";
 import { connect } from "../../utils/connect";
-import { difference } from "lodash";
 
 // Selectors
 import {
@@ -190,7 +189,9 @@ class SourcesTree extends Component {
   getSourcesGroups = item => {
     const sourcesAll = getAllSources(this.props);
     const sourcesInside = getSourcesInsideGroup(item, this.props);
-    const sourcesOuside = difference(sourcesAll, sourcesInside);
+    const sourcesOuside = sourcesAll.filter(
+      source => !sourcesInside.includes(source)
+    );
 
     return { sourcesInside, sourcesOuside };
   };
