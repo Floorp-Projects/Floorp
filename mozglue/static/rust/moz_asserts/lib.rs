8404ee38293e85462783bc96ba46aae1e8f5cf8e
Andi-Bogdan Postelnicu <andi@mozilla.com>
2022-01-11 09:16:57 +0000

    Bug 1617369 - Reformat recent rust changes with rustfmt r=emilio
    Updated with rustfmt 1.4.37-stable (f1edd04 2021-11-29)
    
    Differential Revision: https://phabricator.services.mozilla.com/D135527

--- mozglue/static/rust/moz_asserts/lib.rs
+++ mozglue/static/rust/moz_asserts/lib.rs
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-
 // Implementation note: It seems tempting to use if cfg!(feature = ...) directly
 // in the macro, but that doesn't work, as the feature would be evaluated
 // against the dependent crate.
