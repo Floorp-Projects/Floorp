a691cd2044463ac1837f66e0d9493eb580c4b742
Mark Banner <standard8@mozilla.com>
2022-01-11 14:32:52 +0000

    Bug 1748509 - Add builder fields to snapshot groups and more indices. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D135122

--- toolkit/components/places/Database.cpp
+++ toolkit/components/places/Database.cpp
@@ -1225,7 +1225,12 @@ nsresult Database::InitSchema(bool* aDatabaseMigrated) {
         NS_ENSURE_SUCCESS(rv, rv);
       }
 
-      // Firefox 97 uses schema version 61
+      if (currentSchemaVersion < 62) {
+        rv = MigrateV62Up();
+        NS_ENSURE_SUCCESS(rv, rv);
+      }
+
+      // Firefox 97 uses schema version 62
 
       // Schema Upgrades must add migration code here.
       // >>> IMPORTANT! <<<
@@ -1322,6 +1327,8 @@ nsresult Database::InitSchema(bool* aDatabaseMigrated) {
     rv = mMainConn->ExecuteSimpleSQL(
         CREATE_IDX_MOZ_PLACES_METADATA_PLACECREATED);
     NS_ENSURE_SUCCESS(rv, rv);
+    rv = mMainConn->ExecuteSimpleSQL(CREATE_IDX_MOZ_PLACES_METADATA_REFERRER);
+    NS_ENSURE_SUCCESS(rv, rv);
 
     // moz_places_metadata_search_queries
     rv = mMainConn->ExecuteSimpleSQL(CREATE_MOZ_PLACES_METADATA_SEARCH_QUERIES);
@@ -1330,10 +1337,16 @@ nsresult Database::InitSchema(bool* aDatabaseMigrated) {
     // moz_places_metadata_snapshots
     rv = mMainConn->ExecuteSimpleSQL(CREATE_MOZ_PLACES_METADATA_SNAPSHOTS);
     NS_ENSURE_SUCCESS(rv, rv);
+    rv = mMainConn->ExecuteSimpleSQL(
+        CREATE_IDX_MOZ_PLACES_METADATA_SNAPSHOTS_PINNNED);
+    NS_ENSURE_SUCCESS(rv, rv);
 
     // moz_places_metadata_snapshots_extra
     rv =
         mMainConn->ExecuteSimpleSQL(CREATE_MOZ_PLACES_METADATA_SNAPSHOTS_EXTRA);
+    NS_ENSURE_SUCCESS(rv, rv);
+    rv = mMainConn->ExecuteSimpleSQL(
+        CREATE_IDX_MOZ_PLACES_METADATA_SNAPSHOTS_EXTRA_TYPE);
     NS_ENSURE_SUCCESS(rv, rv);
 
     // moz_places_metadata_snapshots_groups
@@ -2363,6 +2376,36 @@ nsresult Database::MigrateV61Up() {
     rv = mMainConn->ExecuteSimpleSQL(CREATE_MOZ_PREVIEWS_TOMBSTONES);
     NS_ENSURE_SUCCESS(rv, rv);
   }
+  return NS_OK;
+}
+
+nsresult Database::MigrateV62Up() {
+  // Add builder columns if necessary.
+  nsCOMPtr<mozIStorageStatement> stmt;
+  nsresult rv = mMainConn->CreateStatement(
+      "SELECT builder FROM moz_places_metadata_snapshots_groups"_ns,
+      getter_AddRefs(stmt));
+  if (NS_FAILED(rv)) {
+    rv = mMainConn->ExecuteSimpleSQL(
+        "ALTER TABLE moz_places_metadata_snapshots_groups "
+        "ADD COLUMN builder TEXT NOT NULL "_ns);
+    NS_ENSURE_SUCCESS(rv, rv);
+    rv = mMainConn->ExecuteSimpleSQL(
+        "ALTER TABLE moz_places_metadata_snapshots_groups "
+        "ADD COLUMN builder_data TEXT "_ns);
+    NS_ENSURE_SUCCESS(rv, rv);
+  }
+
+  // Add indexes if necessary.
+  rv = mMainConn->ExecuteSimpleSQL(CREATE_IDX_MOZ_PLACES_METADATA_REFERRER);
+  NS_ENSURE_SUCCESS(rv, rv);
+  rv = mMainConn->ExecuteSimpleSQL(
+      CREATE_IDX_MOZ_PLACES_METADATA_SNAPSHOTS_PINNNED);
+  NS_ENSURE_SUCCESS(rv, rv);
+  rv = mMainConn->ExecuteSimpleSQL(
+      CREATE_IDX_MOZ_PLACES_METADATA_SNAPSHOTS_EXTRA_TYPE);
+  NS_ENSURE_SUCCESS(rv, rv);
+
   return NS_OK;
 }
 
