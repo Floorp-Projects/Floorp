a69152371485df49ab67e7fc50c8f8aa2a1558c9
Kershaw Chang <kershaw@mozilla.com>
2022-01-11 04:08:00 +0000

    Bug 1697539 - Test case, r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D135499

---
+++ dom/xhr/tests/worker_bug1697539.js
@@ -0,0 +1,19 @@
+onmessage = function(e) {
+  let xhr = new XMLHttpRequest();
+  let already_sent = false;
+  xhr.addEventListener("readystatechange", event => {
+    try {
+      event.originalTarget.send("test");
+    } catch (error) {
+      if (error.name == "InvalidStateError") {
+        if (!already_sent) {
+          postMessage(error.name);
+          already_sent = true;
+        }
+      }
+    }
+  });
+
+  xhr.open("POST", e.data, false);
+  xhr.send();
+};
