1ae5d4d8f6923f5ad5484a6f37df0ca8b73d7362
Jan-Erik Rediger <jrediger@mozilla.com>
2022-01-12 10:10:30 +0000

    Bug 1746254 - Use simple default hash state to avoid random seeding. r=chutten,padenot a=RyanVM
    This replaces the default-used `RandomState`[1] on the hashmaps used in
    FOG's IPC with a simple wrapper around `DefaultHasher`.
    
    `RandomState` uses `DefaultHasher` internally, which at the moment of
    this commit, uses `SipHasher13` internally.
    Our `HashState` does the same, but with default keys of `(0, 0)`
    (whereas `RandomState` uses some random keys).
    
    This will result in the same hashes used for the same values in
    different hashmaps.
    But this should be fine for the use here:
    The key for all those hash maps is a `MetricId`, which is a simple `u32`.
    It's already a unique identifier generated from defined metrics and not
    user-manipulated.
    Therefore we shouldn't need to worry about HashDos-resistance.
    
    [1]: https://doc.rust-lang.org/std/collections/hash_map/struct.RandomState.html
    
    Differential Revision: https://phabricator.services.mozilla.com/D135208

--- toolkit/components/glean/api/src/lib.rs
+++ toolkit/components/glean/api/src/lib.rs
@@ -12,6 +12,9 @@ pub extern crate uuid;
 // Re-exporting for use in user tests.
 pub use private::{DistributionData, ErrorType, RecordedEvent};
 
+use std::collections::hash_map::DefaultHasher;
+use std::hash::BuildHasher;
+
 // Must appear before `metrics` or its use of `ffi`'s macros will fail.
 #[macro_use]
 mod ffi;
@@ -24,3 +27,19 @@ pub mod ipc;
 
 #[cfg(test)]
 mod common_test;
+
+/// A simple hash state, which wraps `DefaultHasher` with default keys.
+///
+/// This will result in the same hashes for the same values but different hashmaps.
+/// Use it only when you can't rely on the random seed working,
+/// e.g. in a sandboxed Windows environment.
+#[derive(Debug, Default)]
+pub struct HashState;
+
+impl BuildHasher for HashState {
+    type Hasher = DefaultHasher;
+    #[inline]
+    fn build_hasher(&self) -> DefaultHasher {
+        DefaultHasher::new()
+    }
+}
