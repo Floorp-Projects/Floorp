1b7409dffbd3a3d7e81316ae97f2e01fb6820a41
Randell Jesup <rjesup@wgate.com>
2022-01-12 14:01:28 +0000

    Bug 1749214: nsHttpConnectionMgr cleanup r=kershaw,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D135459

--- netwerk/protocol/http/nsHttpConnectionMgr.h
+++ netwerk/protocol/http/nsHttpConnectionMgr.h
@@ -208,8 +208,15 @@ class nsHttpConnectionMgr final : public HttpConnectionMgrShell,
   //-------------------------------------------------------------------------
 
   ReentrantMonitor mReentrantMonitor{"nsHttpConnectionMgr.mReentrantMonitor"};
+  // This is used as a flag that we're shut down, and no new events should be
+  // dispatched.
   nsCOMPtr<nsIEventTarget> mSocketThreadTarget;
 
+  Atomic<bool, mozilla::Relaxed> mIsShuttingDown{false};
+
+  //-------------------------------------------------------------------------
+  // NOTE: these members are only accessed on the socket transport thread
+  //-------------------------------------------------------------------------
   // connection limits
   uint16_t mMaxUrgentExcessiveConns{0};
   uint16_t mMaxConns{0};
@@ -225,11 +232,6 @@ class nsHttpConnectionMgr final : public HttpConnectionMgrShell,
   uint32_t mThrottleHoldTime{0};
   TimeDuration mThrottleMaxTime;
   bool mBeConservativeForProxy{true};
-  Atomic<bool, mozilla::Relaxed> mIsShuttingDown{false};
-
-  //-------------------------------------------------------------------------
-  // NOTE: these members are only accessed on the socket transport thread
-  //-------------------------------------------------------------------------
 
   [[nodiscard]] bool ProcessPendingQForEntry(ConnectionEntry*,
                                              bool considerAll);
