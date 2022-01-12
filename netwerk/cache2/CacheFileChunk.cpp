6cd5050b51b86c48cca27f4b9eaaafdc21bd2b57
Randell Jesup <rjesup@wgate.com>
2022-01-12 13:54:01 +0000

    Bug 1747439: netwerk/cache2 cleanup r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D135167

--- netwerk/cache2/CacheFileChunk.cpp
+++ netwerk/cache2/CacheFileChunk.cpp
@@ -412,6 +412,7 @@ nsresult CacheFileChunk::Write(CacheFileHandle* aHandle,
 
 void CacheFileChunk::WaitForUpdate(CacheFileChunkListener* aCallback) {
   AssertOwnsLock();
+  mFile->AssertOwnsLock();  // For thread-safety analysis
 
   LOG(("CacheFileChunk::WaitForUpdate() [this=%p, listener=%p]", this,
        aCallback));
@@ -507,6 +508,7 @@ uint32_t CacheFileChunk::DataSize() const { return mBuf->DataSize(); }
 
 void CacheFileChunk::UpdateDataSize(uint32_t aOffset, uint32_t aLen) {
   AssertOwnsLock();
+  mFile->AssertOwnsLock();  // For thread-safety analysis
 
   // UpdateDataSize() is called only when we've written some data to the chunk
   // and we never write data anymore once some error occurs.
