3ddf9ed481210dd14de260be04a1e03d9e536fa9
Mike Kaply <mozilla@kaply.com>
2022-01-13 21:18:30 +0000

    Bug 1733497 - Remove region.properties. r=geckoview-reviewers,m_kato,flod
    Differential Revision: https://phabricator.services.mozilla.com/D134427

--- uriloader/exthandler/HandlerService.js
+++ uriloader/exthandler/HandlerService.js
@@ -48,6 +48,10 @@ XPCOMUtils.defineLazyServiceGetter(
   "@mozilla.org/mime;1",
   "nsIMIMEService"
 );
+XPCOMUtils.defineLazyModuleGetters(this, {
+  kHandlerList: "resource://gre/modules/handlers/HandlerList.jsm",
+  kHandlerListVersion: "resource://gre/modules/handlers/HandlerList.jsm",
+});
 
 function HandlerService() {
   // Observe handlersvc-json-replace so we can switch to the datasource
@@ -114,34 +118,17 @@ HandlerService.prototype = {
    * newer than the one in the data store.
    */
   _injectDefaultProtocolHandlersIfNeeded() {
-    let prefsDefaultHandlersVersion;
     try {
-      prefsDefaultHandlersVersion = Services.prefs.getComplexValue(
+      let defaultHandlersVersion = Services.prefs.getIntPref(
         "gecko.handlerService.defaultHandlersVersion",
-        Ci.nsIPrefLocalizedString
+        0
       );
-    } catch (ex) {
-      if (
-        ex instanceof Components.Exception &&
-        ex.result == Cr.NS_ERROR_UNEXPECTED
-      ) {
-        // This platform does not have any default protocol handlers configured.
-        return;
-      }
-      throw ex;
-    }
-
-    try {
-      prefsDefaultHandlersVersion = Number(prefsDefaultHandlersVersion.data);
-      let locale = Services.locale.appLocaleAsBCP47;
-
-      let defaultHandlersVersion =
-        this._store.data.defaultHandlersVersion[locale] || 0;
-      if (defaultHandlersVersion < prefsDefaultHandlersVersion) {
+      if (defaultHandlersVersion < kHandlerListVersion) {
         this._injectDefaultProtocolHandlers();
-        this._store.data.defaultHandlersVersion[
-          locale
-        ] = prefsDefaultHandlersVersion;
+        Services.prefs.setIntPref(
+          "gecko.handlerService.defaultHandlersVersion",
+          kHandlerListVersion
+        );
         // Now save the result:
         this._store.saveSoon();
       }
@@ -151,53 +138,13 @@ HandlerService.prototype = {
   },
 
   _injectDefaultProtocolHandlers() {
-    let schemesPrefBranch = Services.prefs.getBranch(
-      "gecko.handlerService.schemes."
-    );
-    let schemePrefList = schemesPrefBranch.getChildList("");
+    let locale = Services.locale.appLocaleAsBCP47;
 
-    let schemes = {};
-
-    // read all the scheme prefs into a hash
-    for (let schemePrefName of schemePrefList) {
-      let [scheme, handlerNumber, attribute] = schemePrefName.split(".");
-
-      try {
-        let attrData = schemesPrefBranch.getComplexValue(
-          schemePrefName,
-          Ci.nsIPrefLocalizedString
-        ).data;
-        if (!(scheme in schemes)) {
-          schemes[scheme] = {};
-        }
-
-        if (!(handlerNumber in schemes[scheme])) {
-          schemes[scheme][handlerNumber] = {};
-        }
-
-        schemes[scheme][handlerNumber][attribute] = attrData;
-      } catch (ex) {}
-    }
-
-    // Now drop any entries without a uriTemplate, or with a broken one.
-    // The Array.from calls ensure we can safely delete things without
-    // affecting the iterator.
-    for (let [scheme, handlerObject] of Array.from(Object.entries(schemes))) {
-      let handlers = Array.from(Object.entries(handlerObject));
-      let validHandlers = 0;
-      for (let [key, obj] of handlers) {
-        if (
-          !obj.uriTemplate ||
-          !obj.uriTemplate.startsWith("https://") ||
-          !obj.uriTemplate.toLowerCase().includes("%s")
-        ) {
-          delete handlerObject[key];
-        } else {
-          validHandlers++;
-        }
-      }
-      if (!validHandlers) {
-        delete schemes[scheme];
+    // Initialize handlers to default and update based on locale.
+    let localeHandlers = kHandlerList.default;
+    if (kHandlerList[locale]) {
+      for (let scheme in kHandlerList[locale].schemes) {
+        localeHandlers.schemes[scheme] = kHandlerList[locale].schemes[scheme];
       }
     }
 
@@ -210,7 +157,7 @@ HandlerService.prototype = {
     // All we're trying to do is insert some web apps into the list. We
     // don't care what's already in the file, we just want to do the
     // equivalent of appending into the database. So let's just go do that:
-    for (let scheme of Object.keys(schemes)) {
+    for (let scheme of Object.keys(localeHandlers.schemes)) {
       let existingSchemeInfo = this._store.data.schemes[scheme];
       if (!existingSchemeInfo) {
         // Haven't seen this scheme before. Default to asking which app the
@@ -226,8 +173,7 @@ HandlerService.prototype = {
         this._store.data.schemes[scheme] = existingSchemeInfo;
       }
       let { handlers } = existingSchemeInfo;
-      for (let handlerNumber of Object.keys(schemes[scheme])) {
-        let newHandler = schemes[scheme][handlerNumber];
+      for (let newHandler of localeHandlers.schemes[scheme].handlers) {
         // If there is already a handler registered with the same template
         // URL, ignore the new one:
         let matchingTemplate = handler =>
