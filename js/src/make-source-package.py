3242e1e8914a02e1cd4bcf9340317b86ff9e0343
Andre Natal <anatal@gmail.com>
2022-01-13 21:27:37 +0000

    Bug 1722102 - Adding vendor intgemm library r=yury,lth,firefox-build-system-reviewers,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D120770

--- js/src/make-source-package.py
+++ js/src/make-source-package.py
@@ -179,7 +179,7 @@ rsync_filter_list = """
 - /third_party/python/gyp
 + /third_party/python/**
 + /third_party/rust/**
-
++ /third_party/intgemm/**
 + /layout/tools/reftest/reftest/**
 
 + /testing/mach_commands.py
