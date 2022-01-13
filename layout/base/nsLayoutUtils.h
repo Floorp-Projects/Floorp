529d2dbd7696e7ab19a5f3a7132ff5defbffae7f
Ting-Yu Lin <tlin@mozilla.com>
2022-01-13 18:36:04 +0000

    Bug 1745113 Part 4 - Change AppUnitWidthOfString to take a Span parameter. r=jfkthame
    `Span` is more versatile than `const String&`, and existing callers passing a
    String into this function do not need any change.
    
    This patch makes the next part simpler.
    
    Differential Revision: https://phabricator.services.mozilla.com/D135642

--- layout/base/nsLayoutUtils.h
+++ layout/base/nsLayoutUtils.h
@@ -18,6 +18,7 @@
 #include "mozilla/SVGImageContext.h"
 #include "mozilla/ToString.h"
 #include "mozilla/TypedEnumBits.h"
+#include "mozilla/Span.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/WritingModes.h"
 #include "mozilla/layout/FrameChildList.h"
@@ -1654,11 +1655,11 @@ class nsLayoutUtils {
                                       DrawTarget* aDrawTarget) {
     return AppUnitWidthOfString(&aC, 1, aFontMetrics, aDrawTarget);
   }
-  static nscoord AppUnitWidthOfString(const nsString& aString,
+  static nscoord AppUnitWidthOfString(mozilla::Span<const char16_t> aString,
                                       nsFontMetrics& aFontMetrics,
                                       DrawTarget* aDrawTarget) {
-    return nsLayoutUtils::AppUnitWidthOfString(aString.get(), aString.Length(),
-                                               aFontMetrics, aDrawTarget);
+    return nsLayoutUtils::AppUnitWidthOfString(
+        aString.Elements(), aString.Length(), aFontMetrics, aDrawTarget);
   }
   static nscoord AppUnitWidthOfString(const char16_t* aString, uint32_t aLength,
                                       nsFontMetrics& aFontMetrics,
