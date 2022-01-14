b277a8bb9570d71f49a3e46881e1ba5cb8b22940
Nicolas Chevobbe <nchevobbe@mozilla.com>
2022-01-14 10:21:14 +0000

    Bug 1749550 - [devtools] Add distinct style to event badge in markup view when an event listener is disabled. r=ochameau.
    Differential Revision: https://phabricator.services.mozilla.com/D135680

--- devtools/client/inspector/markup/views/element-container.js
+++ devtools/client/inspector/markup/views/element-container.js
@@ -82,6 +82,20 @@ MarkupElementContainer.prototype = extend(MarkupContainer.prototype, {
       this.node
     );
 
+    // Add specific styling to the "event" badge when at least one event is disabled.
+    // The eventTooltip will take care of clearing the event listener when it's destroyed.
+    eventTooltip.on(
+      "event-tooltip-listener-toggled",
+      ({ hasDisabledEventListeners }) => {
+        const className = "has-disabled-events";
+        if (hasDisabledEventListeners) {
+          this.editor._eventBadge.classList.add(className);
+        } else {
+          this.editor._eventBadge.classList.remove(className);
+        }
+      }
+    );
+
     // Disable the image preview tooltip while we display the event details
     this.markup._disableImagePreviewTooltip();
     tooltip.once("hidden", () => {
