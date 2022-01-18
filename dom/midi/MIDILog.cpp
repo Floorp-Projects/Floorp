1c983272eadfc06c3de082e97e0bd434c5b341cd
Paul Adenot <paul@paul.cx>
2022-01-18 09:54:42 +0000

    Bug 1743462 - Add a log module for the Web MIDI API. r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D132387

---
+++ dom/midi/MIDILog.cpp
@@ -0,0 +1,54 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "MIDILog.h"
+
+#include "mozilla/dom/MIDITypes.h"
+#include "mozilla/dom/MIDIPortBinding.h"
+
+mozilla::LazyLogModule gWebMIDILog("WebMIDI");
+
+void LogMIDIMessage(nsTArray<mozilla::dom::MIDIMessage>& aMessages,
+                    const nsAString& aPortId,
+                    mozilla::dom::MIDIPortType aDirection) {
+  if (MOZ_LOG_TEST(gWebMIDILog, mozilla::LogLevel::Debug)) {
+    if (MOZ_LOG_TEST(gWebMIDILog, mozilla::LogLevel::Verbose)) {
+      for (auto& msg : aMessages) {
+        uint32_t byteCount = msg.data().Length();
+        nsAutoCString logMessage;
+        // Log long messages inline with the timestamp and the length, log
+        // longer messages a bit like xxd
+        logMessage.AppendPrintf(
+            "%s %s length=%u", NS_ConvertUTF16toUTF8(aPortId).get(),
+            aDirection == mozilla::dom::MIDIPortType::Input ? "->" : "<-",
+            byteCount);
+
+        if (byteCount <= 3) {
+          logMessage.AppendPrintf(" [");
+          // Regular messages
+          for (uint32_t i = 0; i < byteCount - 1; i++) {
+            logMessage.AppendPrintf("%x ", msg.data()[i]);
+          }
+          logMessage.AppendPrintf("%x]", msg.data()[byteCount - 1]);
+        } else {
+          // Longer messages
+          for (uint32_t i = 0; i < byteCount; i++) {
+            if (!(i % 8)) {
+              logMessage.AppendPrintf("\n%08u:\t", i);
+            }
+            logMessage.AppendPrintf("%x ", msg.data()[i]);
+          }
+        }
+        MOZ_LOG(gWebMIDILog, mozilla::LogLevel::Verbose,
+                ("%s", logMessage.get()));
+      }
+      return;
+    }
+    for (auto& msg : aMessages) {
+      LOG("%s %s %zu bytes", NS_ConvertUTF16toUTF8(aPortId).get(),
+          aDirection == mozilla::dom::MIDIPortType::Input ? "->" : "<-",
+          msg.data().Length());
+    }
+  }
+}
