a718b52a2425122e5a42cea5dc1f076a642c37fe
William Durand <will+git@drnd.me>
2022-01-18 16:33:57 +0000

    Bug 1736576 - Add support for multiple files in scripting.executeScript(). r=robwu
    Instead of trying to make `_execute()` in `ext-tabs-base.js` accept
    options for both the Scripting and Tabs APIs, we introduced an
    `execute()` function in `parent/ext-scripting.js` that is similar to
    `_execute()` but specific to the Scripting API options.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133872

--- toolkit/components/extensions/parent/ext-scripting.js
+++ toolkit/components/extensions/parent/ext-scripting.js
@@ -8,6 +8,70 @@
 
 var { ExtensionError } = ExtensionUtils;
 
+/**
+ * Inserts a script in the given tab, and returns a promise which resolves when
+ * the operation has completed.
+ *
+ * @param {TabBase} tab
+ *        The tab in which to perform the injection.
+ * @param {BaseContext} context
+ *        The extension context for which to perform the injection.
+ * @param {Object} details
+ *        The details object, specifying what to inject, where, and when.
+ *        Derived from the ScriptInjection type.
+ * @param {string} kind
+ *        The kind of data being injected. Possible choices: "js".
+ * @param {string} method
+ *        The name of the method which was called to trigger the injection.
+ *        Used to generate appropriate error messages on failure.
+ *
+ * @returns {Promise}
+ *        Resolves to the result of the execution, once it has completed.
+ */
+const execute = (tab, context, details, kind, method) => {
+  let options = {
+    jsPaths: [],
+    extensionId: context.extension.id,
+  };
+
+  // TODO: Bug 1750765 - Add test coverage for this option.
+  options.hasActiveTabPermission = tab.hasActiveTabPermission;
+  options.matches = tab.extension.allowedOrigins.patterns.map(
+    host => host.pattern
+  );
+
+  if (details.code) {
+    options[`${kind}Code`] = details.code;
+  }
+
+  if (details.files) {
+    for (const file of details.files) {
+      let url = context.uri.resolve(file);
+      if (!tab.extension.isExtensionURL(url)) {
+        return Promise.reject({
+          message: "Files to be injected must be within the extension",
+        });
+      }
+      options[`${kind}Paths`].push(url);
+    }
+  }
+
+  // TODO: Bug 1736574 - Add support for multiple frame IDs and `allFrames`.
+  if (details.frameId) {
+    options.frameID = details.frameId;
+  }
+
+  options.runAt = "document_idle";
+  options.wantReturnValue = true;
+
+  // TODO: Bug 1736579 - Configure options for CSS injection, e.g., `cssPaths`
+  // and `cssOrigin`.
+
+  // This function is derived from `_execute()` in `parent/ext-tabs-base.js`,
+  // make sure to keep both in sync when relevant.
+  return tab.queryContent("Execute", options);
+};
+
 this.scripting = class extends ExtensionAPI {
   getAPI(context) {
     const { extension } = context;
@@ -20,19 +84,11 @@ this.scripting = class extends ExtensionAPI {
 
           let tab = tabManager.get(tabId);
 
-          let executeScriptDetails = {
-            code: null,
-            file: null,
-            runAt: "document_idle",
-          };
-
-          if (details.files) {
-            // TODO bug 1736576: Support more than one file.
-            executeScriptDetails.file = details.files[0];
-          } else {
+          let executeDetails = {
             // Defined in `child/ext-scripting.js`.
-            executeScriptDetails.code = details.codeToExecute;
-          }
+            code: details.codeToExecute,
+            files: details.files,
+          };
 
           const promises = [];
 
@@ -42,9 +98,9 @@ this.scripting = class extends ExtensionAPI {
           }
 
           for (const frameId of frameIds) {
+            const details = { ...executeDetails, frameId };
             promises.push(
-              tab
-                .executeScript(context, { ...executeScriptDetails, frameId })
+              execute(tab, context, details, "js", "executeScript")
                 // We return `null` when the result value is falsey.
                 .then(results => ({ frameId, result: results[0] || null }))
                 .catch(error => ({ frameId, result: null, error }))
