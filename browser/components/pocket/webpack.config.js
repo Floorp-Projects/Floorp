7c161e72a9ee6bc79784c19ce3519d4b0ee3a7b9
Scott <scott.downe@gmail.com>
2022-01-19 02:03:31 +0000

    Bug 1750596 - Moving save to Pocket vendors into their own bundle. r=gvn
    Differential Revision: https://phabricator.services.mozilla.com/D136157

--- browser/components/pocket/webpack.config.js
+++ browser/components/pocket/webpack.config.js
@@ -27,4 +27,15 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: "vendor",
+          chunks: "all",
+        },
+      },
+    },
+  },
 };
