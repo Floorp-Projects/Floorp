5240cebb6dbfe0601d7e5de6f9c052291604141e
Cristian Tuns <ctuns@mozilla.com>
2022-01-19 07:24:42 -0500

    Backed out 2 changesets (bug 1745511) for causing mochitest failures on GeckoChildProcessHost.cpp CLOSED TREE
    Backed out changeset 620e4b9d8b61 (bug 1745511)
    Backed out changeset 0fadea77038a (bug 1745511)

--- toolkit/components/glean/api/src/ipc.rs
+++ toolkit/components/glean/api/src/ipc.rs
@@ -126,9 +126,6 @@ fn register_process_shutdown(process_type: u32) {
                 FOG_RegisterContentChildShutdown();
             };
         }
-        nsIXULRuntime::PROCESS_TYPE_GMPLUGIN => {
-            // GMP process shutdown is handled in GMPChild::ActorDestroy.
-        }
         nsIXULRuntime::PROCESS_TYPE_GPU => {
             // GPU process shutdown is handled in GPUParent::ActorDestroy.
         }
