e8279fdad6635e7c7049f3f826fd989bd9b4f68d
Chris Martin <cmartin@mozilla.com>
2022-01-20 16:36:46 +0000

    Bug 1751012 - Don't print to stdout in WebRender r=kvark
    Differential Revision: https://phabricator.services.mozilla.com/D136436

--- gfx/wr/webrender/src/platform/windows/font.rs
+++ gfx/wr/webrender/src/platform/windows/font.rs
@@ -254,9 +254,9 @@ impl FontContext {
                 let r = pixel[0];
                 let g = pixel[1];
                 let b = pixel[2];
-                print!("({}, {}, {}) ", r, g, b,);
+                debug!("({}, {}, {}) ", r, g, b,);
             }
-            println!();
+            debug!("");
         }
     }
 
