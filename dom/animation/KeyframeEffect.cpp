fea455afcd22b20e7ba9c7c912bcfe98240c8176
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2022-01-20 21:20:13 +0000

    Bug 1725908 - Disallow background color animations on <html> element running on the compositor. r=boris
    We don't support background color animations running on the compositor for
    nsDisplayCanvasBackgroundColor and nsDisplaySolidColor display items,
    <html> element is one of elements using the items.
    
    The reftest in this commit fails on opt builds, asserts an assertion [1]
    in the ctor of nsDisplayBackgroundColor on debug builds.
    
    [1] https://searchfox.org/mozilla-central/rev/080e18fa4748456003164f58b0d925b8c3826a67/layout/painting/nsDisplayList.h#4317
    
    Differential Revision: https://phabricator.services.mozilla.com/D135973

--- dom/animation/KeyframeEffect.cpp
+++ dom/animation/KeyframeEffect.cpp
@@ -2054,11 +2054,12 @@ KeyframeEffect::MatchForCompositor KeyframeEffect::IsMatchForCompositor(
     }
 
     // We don't yet support off-main-thread background-color animations on
-    // canvas frame or on <body> which genarate nsDisplayCanvasBackgroundColor
-    // or nsDisplaySolidColor display item.
+    // canvas frame or on <html> or <body> which genarate
+    // nsDisplayCanvasBackgroundColor or nsDisplaySolidColor display item.
     if (nsCSSRendering::IsCanvasFrame(aFrame) ||
         (aFrame->GetContent() &&
-         aFrame->GetContent()->IsHTMLElement(nsGkAtoms::body))) {
+         (aFrame->GetContent()->IsHTMLElement(nsGkAtoms::body) ||
+          aFrame->GetContent()->IsHTMLElement(nsGkAtoms::html)))) {
       return KeyframeEffect::MatchForCompositor::No;
     }
   }
