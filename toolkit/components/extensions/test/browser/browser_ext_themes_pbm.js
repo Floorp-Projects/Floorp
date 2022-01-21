097345510b50f17a623774056931d5d1ae02eaa1
Paul Zuehlcke <pbz@mozilla.com>
2022-01-21 15:47:37 +0000

    Bug 1749375 - Put PBM dark theme behind a pref. r=desktop-theme-reviewers,dao
    Differential Revision: https://phabricator.services.mozilla.com/D136394

--- toolkit/components/extensions/test/browser/browser_ext_themes_pbm.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_pbm.js
@@ -60,7 +60,12 @@ async function testWindowColorScheme({
 
 add_task(async function setup() {
   // Set system theme to light to ensure consistency across test machines.
-  await SpecialPowers.pushPrefEnv({ set: [["ui.systemUsesDarkTheme", 0]] });
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["browser.theme.dark-private-windows", true],
+      ["ui.systemUsesDarkTheme", 0],
+    ],
+  });
   // Ensure the built-in themes are initialized.
   await BuiltInThemes.ensureBuiltInThemes();
 
