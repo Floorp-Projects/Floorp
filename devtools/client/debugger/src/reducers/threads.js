8cb2f212ac1bd681d2503924cd6c8a7d15332bfa
Alexandre Poirot <poirot.alex@gmail.com>
2022-01-21 16:01:00 +0000

    Bug 1751031 - [devtools] Migrate all thread selectors to a selector module. r=bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D136398

--- devtools/client/debugger/src/reducers/threads.js
+++ devtools/client/debugger/src/reducers/threads.js
@@ -7,8 +7,6 @@
  * @module reducers/threads
  */
 
-import { createSelector } from "reselect";
-
 export function initialThreadsState() {
   return {
     threads: [],
@@ -52,53 +50,4 @@ export default function update(state = initialThreadsState(), action) {
     default:
       return state;
   }
-}
-
-export const getWorkerCount = state => getThreads(state).length;
-
-export function getWorkerByThread(state, thread) {
-  return getThreads(state).find(worker => worker.actor == thread);
-}
-
-function isMainThread(thread) {
-  return thread.isTopLevel;
-}
-
-export function getMainThread(state) {
-  return state.threads.threads.find(isMainThread);
-}
-
-export function getDebuggeeUrl(state) {
-  return getMainThread(state)?.url || "";
-}
-
-export const getThreads = createSelector(
-  state => state.threads.threads,
-  threads => threads.filter(thread => !isMainThread(thread))
-);
-
-export const getAllThreads = createSelector(
-  getMainThread,
-  getThreads,
-  (mainThread, threads) => {
-    const orderedThreads = Array.from(threads).sort((threadA, threadB) => {
-      if (threadA.name === threadB.name) {
-        return 0;
-      }
-      return threadA.name < threadB.name ? -1 : 1;
-    });
-    return [mainThread, ...orderedThreads].filter(Boolean);
-  }
-);
-
-export function getThread(state, threadActor) {
-  return getAllThreads(state).find(thread => thread.actor === threadActor);
-}
-
-// checks if a path begins with a thread actor
-// e.g "server1.conn0.child1/workerTarget22/context1/dbg-workers.glitch.me"
-export function startsWithThreadActor(state, path) {
-  const threadActors = getAllThreads(state).map(t => t.actor);
-  const match = path.match(new RegExp(`(${threadActors.join("|")})\/(.*)`));
-  return match?.[1];
 }
