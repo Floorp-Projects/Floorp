c78f0a7eb5bfe554f959969959db3e78a3fa914f
Scott <scott.downe@gmail.com>
2022-01-21 21:52:42 +0000

    Bug 1750607 - Pocket newtab card background and compact configs. r=gvn,andreio
    Differential Revision: https://phabricator.services.mozilla.com/D136160

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1479,7 +1479,10 @@ pref("browser.newtabpage.activity-stream.asrouter.useRemoteL10n", true);
 // These prefs control if Discovery Stream is enabled.
 pref("browser.newtabpage.activity-stream.discoverystream.enabled", true);
 pref("browser.newtabpage.activity-stream.discoverystream.hardcoded-basic-layout", false);
+// A preset compact layout, similar to setting a collection of prefs to build a complete layout.
 pref("browser.newtabpage.activity-stream.discoverystream.compactLayout.enabled", false);
+pref("browser.newtabpage.activity-stream.discoverystream.hideCardBackground.enabled", false);
+pref("browser.newtabpage.activity-stream.discoverystream.fourCardLayout.enabled", false);
 pref("browser.newtabpage.activity-stream.discoverystream.loadMore.enabled", false);
 pref("browser.newtabpage.activity-stream.discoverystream.lastCardMessage.enabled", false);
 pref("browser.newtabpage.activity-stream.discoverystream.newFooterSection.enabled", false);
