3e619f800801de400d3fa395b657c1e9d0b6da64
Sandor Molnar <smolnar@mozilla.com>
2022-01-22 05:49:37 +0200

    Backed out changeset 1e5f25dc1cf8 (bug 1751342) for causing browsertime failures. CLOSED TREE

--- third_party/rust/clap/examples/tutorial_builder/03_02_option.rs
+++
@@ -1,9 +0,0 @@
-use clap::{app_from_crate, arg};
-
-fn main() {
-    let matches = app_from_crate!()
-        .arg(arg!(-n --name <NAME>).required(false))
-        .get_matches();
-
-    println!("name: {:?}", matches.value_of("name"));
-}
