c7144b841e07b9f6bf358aa22e0488b08e65cbfd
Barret Rennie <barret@brennie.ca>
2022-01-25 12:16:17 +0000

    Bug 1745352 - Remove osfile.jsm from browser/components/extensions r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D133843

--- browser/components/extensions/parent/ext-pkcs11.js
+++ browser/components/extensions/parent/ext-pkcs11.js
@@ -7,7 +7,6 @@
 XPCOMUtils.defineLazyModuleGetters(this, {
   ctypes: "resource://gre/modules/ctypes.jsm",
   NativeManifests: "resource://gre/modules/NativeManifests.jsm",
-  OS: "resource://gre/modules/osfile.jsm",
 });
 
 XPCOMUtils.defineLazyServiceGetter(
@@ -16,6 +15,9 @@ XPCOMUtils.defineLazyServiceGetter(
   "@mozilla.org/security/pkcs11moduledb;1",
   "nsIPKCS11ModuleDB"
 );
+
+// eslint-disable-next-line mozilla/reject-importGlobalProperties
+Cu.importGlobalProperties(["PathUtils"]);
 
 var { DefaultMap } = ExtensionUtils;
 
@@ -38,12 +40,18 @@ this.pkcs11 = class extends ExtensionAPI {
       );
       if (hostInfo) {
         if (AppConstants.platform === "win") {
-          hostInfo.manifest.path = OS.Path.join(
-            OS.Path.dirname(hostInfo.path),
-            hostInfo.manifest.path
-          );
+          // If the path specified in the manifest is not an abslute path,
+          // translate it relative to manifest's directory.
+          if (!PathUtils.isAbsolute(hostInfo.manifest.path)) {
+            hostInfo.manifest.path = PathUtils.normalize(
+              PathUtils.joinRelative(
+                PathUtils.parent(hostInfo.path),
+                hostInfo.manifest.path
+              )
+            );
+          }
         }
-        let manifestLib = OS.Path.basename(hostInfo.manifest.path);
+        let manifestLib = PathUtils.filename(hostInfo.manifest.path);
         if (AppConstants.platform !== "linux") {
           manifestLib = manifestLib.toLowerCase(manifestLib);
         }
