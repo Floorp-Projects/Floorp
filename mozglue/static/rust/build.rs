cd850ac0ea99bdf89512c0778e7b0f89fe6f988d
Mike Hommey <mh+mozilla@glandium.org>
2022-01-25 21:29:49 +0000

    Bug 1751331 - Remove MOZ_DIST. r=firefox-build-system-reviewers,mhentges
    As mentioned in bug 1747354, the location of the dist directory is
    relied to be $topobjdir/dist, so just use that consistently rather
    than getting it from a separate variable for rust build scripts.
    
    Differential Revision: https://phabricator.services.mozilla.com/D136556

--- mozglue/static/rust/build.rs
+++ mozglue/static/rust/build.rs
@@ -7,16 +7,6 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let dist_path = {
-        let path = PathBuf::from(env::var_os("MOZ_DIST").unwrap());
-        if !path.is_absolute() || !path.is_dir() {
-            panic!(
-                "MOZ_DIST must be an absolute directory, was: {}",
-                path.display()
-            );
-        }
-        path
-    };
     let topobjdir = {
         let path = PathBuf::from(env::var_os("MOZ_TOPOBJDIR").unwrap());
         if !path.is_absolute() || !path.is_dir() {
@@ -31,7 +21,7 @@ fn main() {
     build.cpp(true);
     // For js-confdefs.h, see wrappers.cpp.
     build.include(topobjdir.join("js").join("src"));
-    build.include(dist_path.join("include"));
+    build.include(topobjdir.join("dist").join("include"));
     build.define("MOZ_HAS_MOZGLUE", None);
     build.file("wrappers.cpp");
     build.compile("wrappers");
