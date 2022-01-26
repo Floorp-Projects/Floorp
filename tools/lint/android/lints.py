202266fea40bf272008c88f3f36d4883ffa854ae
Agi Sferro <agi@sferro.dev>
2022-01-26 20:11:45 +0000

    Bug 1750231 - Suggest mach gradle instead of gradlew. r=calu
    Differential Revision: https://phabricator.services.mozilla.com/D136386

--- tools/lint/android/lints.py
+++ tools/lint/android/lints.py
@@ -160,7 +160,7 @@ def api_lint(config, **lintargs):
                 "path": mozpath.relpath(r["file"], topsrcdir),
                 "lineno": int(r["line"]),
                 "column": int(r.get("column") or 0),
-                "message": "Unexpected api change. Please run ./gradlew {} for more "
+                "message": "Unexpected api change. Please run ./mach gradle {} for more "
                 "information".format(
                     " ".join(lintargs["substs"]["GRADLE_ANDROID_API_LINT_TASKS"])
                 ),
