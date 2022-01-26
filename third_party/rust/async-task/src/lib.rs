41cfb9e36b2799ad21e7d5db45cea93f105b221b
Nika Layzell <nika@thelayzells.com>
2022-01-26 15:36:13 +0000

    Bug 1746533 - Part 1: Use custom vendored version of async-task, r=emilio
    This version adds a new `FallibleTask` wrapper type which allows polling the
    task without panicking.
    
    Differential Revision: https://phabricator.services.mozilla.com/D136945

--- third_party/rust/async-task/src/lib.rs
+++ third_party/rust/async-task/src/lib.rs
@@ -82,7 +82,7 @@ mod task;
 mod utils;
 
 pub use crate::runnable::{spawn, spawn_unchecked, Runnable};
-pub use crate::task::Task;
+pub use crate::task::{FallibleTask, Task};
 
 #[cfg(feature = "std")]
 pub use crate::runnable::spawn_local;
