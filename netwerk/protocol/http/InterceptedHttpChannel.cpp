30d30a533e36099fd71b0a558c5a6b69f02d8905
Julien Wajsberg <felash@gmail.com>
2022-01-26 17:26:24 +0000

    Bug 1639716 - [profiler, network markers] Add information about private browsing in network markers r=valentin,gerald,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D129819

--- netwerk/protocol/http/InterceptedHttpChannel.cpp
+++ netwerk/protocol/http/InterceptedHttpChannel.cpp
@@ -99,7 +99,8 @@ void InterceptedHttpChannel::AsyncOpenInternal() {
     profiler_add_network_marker(
         mURI, requestMethod, mPriority, mChannelId, NetworkLoadType::LOAD_START,
         mChannelCreationTimestamp, mLastStatusReported, 0, kCacheUnknown,
-        mLoadInfo->GetInnerWindowID());
+        mLoadInfo->GetInnerWindowID(),
+        mLoadInfo->GetOriginAttributes().mPrivateBrowsingId > 0);
   }
 
   // If an error occurs in this file we must ensure mListener callbacks are
@@ -518,11 +519,12 @@ InterceptedHttpChannel::Cancel(nsresult aStatus) {
     uint64_t size = 0;
     GetEncodedBodySize(&size);
 
-    profiler_add_network_marker(mURI, requestMethod, priority, mChannelId,
-                                NetworkLoadType::LOAD_CANCEL,
-                                mLastStatusReported, TimeStamp::Now(), size,
-                                kCacheUnknown, mLoadInfo->GetInnerWindowID(),
-                                &mTransactionTimings, std::move(mSource));
+    profiler_add_network_marker(
+        mURI, requestMethod, priority, mChannelId, NetworkLoadType::LOAD_CANCEL,
+        mLastStatusReported, TimeStamp::Now(), size, kCacheUnknown,
+        mLoadInfo->GetInnerWindowID(),
+        mLoadInfo->GetOriginAttributes().mPrivateBrowsingId > 0,
+        &mTransactionTimings, std::move(mSource));
   }
 
   MOZ_DIAGNOSTIC_ASSERT(NS_FAILED(aStatus));
@@ -693,13 +695,14 @@ InterceptedHttpChannel::ResetInterception(bool aBypass) {
     RefPtr<HttpBaseChannel> newBaseChannel = do_QueryObject(newChannel);
     MOZ_ASSERT(newBaseChannel,
                "The redirect channel should be a base channel.");
-    profiler_add_network_marker(mURI, requestMethod, priority, mChannelId,
-                                NetworkLoadType::LOAD_REDIRECT,
-                                mLastStatusReported, TimeStamp::Now(), size,
-                                kCacheUnknown, mLoadInfo->GetInnerWindowID(),
-                                &mTransactionTimings, std::move(mSource),
-                                Some(nsDependentCString(contentType.get())),
-                                mURI, flags, newBaseChannel->ChannelId());
+    profiler_add_network_marker(
+        mURI, requestMethod, priority, mChannelId,
+        NetworkLoadType::LOAD_REDIRECT, mLastStatusReported, TimeStamp::Now(),
+        size, kCacheUnknown, mLoadInfo->GetInnerWindowID(),
+        mLoadInfo->GetOriginAttributes().mPrivateBrowsingId > 0,
+        &mTransactionTimings, std::move(mSource),
+        Some(nsDependentCString(contentType.get())), mURI, flags,
+        newBaseChannel->ChannelId());
   }
 
   rv = SetupReplacementChannel(mURI, newChannel, true, flags);
@@ -1061,7 +1064,9 @@ InterceptedHttpChannel::OnStopRequest(nsIRequest* aRequest, nsresult aStatus) {
     profiler_add_network_marker(
         mURI, requestMethod, priority, mChannelId, NetworkLoadType::LOAD_STOP,
         mLastStatusReported, TimeStamp::Now(), size, kCacheUnknown,
-        mLoadInfo->GetInnerWindowID(), &mTransactionTimings, std::move(mSource),
+        mLoadInfo->GetInnerWindowID(),
+        mLoadInfo->GetOriginAttributes().mPrivateBrowsingId > 0,
+        &mTransactionTimings, std::move(mSource),
         Some(nsDependentCString(contentType.get())));
   }
 
