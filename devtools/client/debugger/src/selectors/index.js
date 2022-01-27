47cee65f87f031e41b3f3fa856cc8250a1e3c0b2
Alexandre Poirot <poirot.alex@gmail.com>
2022-01-27 08:31:42 +0000

    Bug 1751851 - [devtools] Migrate all source-tree selectors to a selector module. r=bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D136861

--- devtools/client/debugger/src/selectors/index.js
+++ devtools/client/debugger/src/selectors/index.js
@@ -10,7 +10,6 @@ export * from "../reducers/threads";
 export * from "../reducers/breakpoints";
 export * from "../reducers/pending-breakpoints";
 export * from "../reducers/ui";
-export * from "../reducers/source-tree";
 
 export {
   getSourceActor,
@@ -42,6 +41,7 @@ export {
 export * from "./preview";
 export * from "./project-text-search";
 export * from "./quick-open";
+export * from "./source-tree";
 export * from "./tabs";
 export * from "./threads";
 export {
