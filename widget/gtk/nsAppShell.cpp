4bc518fc470a18a8b741479a0bdc5371d6abe5b5
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2022-01-28 10:30:37 +0000

    Bug 1530052 - Use remoting name for GDK program name and class. r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D80371

--- widget/gtk/nsAppShell.cpp
+++ widget/gtk/nsAppShell.cpp
@@ -27,6 +27,8 @@
 #  include "nsIObserverService.h"
 #endif
 #include "gfxPlatform.h"
+#include "nsAppRunner.h"
+#include "mozilla/XREAppData.h"
 #include "ScreenHelperGTK.h"
 #include "HeadlessScreenHelper.h"
 #include "mozilla/widget/ScreenManager.h"
@@ -257,13 +259,9 @@ nsresult nsAppShell::Init() {
       // See https://bugzilla.gnome.org/show_bug.cgi?id=747634
       //
       // Only bother doing this for the parent process, since it's the one
-      // creating top-level windows. (At this point, a child process hasn't
-      // received the list of registered chrome packages, so the
-      // GetBrandShortName call would fail anyway.)
-      nsAutoString brandName;
-      mozilla::widget::WidgetUtils::GetBrandShortName(brandName);
-      if (!brandName.IsEmpty()) {
-        gdk_set_program_class(NS_ConvertUTF16toUTF8(brandName).get());
+      // creating top-level windows.
+      if (gAppData) {
+        gdk_set_program_class(gAppData->remotingName);
       }
     }
   }
