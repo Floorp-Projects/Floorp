229c7ffbc49fc0bb5ffb5175d49d5a534d6d640e
Mark Banner <standard8@mozilla.com>
2022-01-31 12:07:27 +0000

    Bug 1751275 - Add a snapshot monitor for triggering snapshot group builders. r=mossop
    Depends on D135706
    
    Differential Revision: https://phabricator.services.mozilla.com/D136526

---
+++ browser/components/places/tests/unit/interactions/test_snapshot_group_triggers.js
@@ -0,0 +1,144 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Tests for the SnapshotMonitor to check that it triggers builders at the
+ * correct times.
+ */
+
+const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
+
+const TEST_URLS = [
+  "https://example.com/1",
+  "https://example.com/2",
+  "https://example.com/3",
+  "https://example.com/4",
+  "https://example.com/5",
+];
+
+const fakeBuilder = {
+  rebuild: sinon.stub(),
+  update: sinon.stub(),
+};
+
+add_task(async function setup() {
+  await addInteractions(
+    TEST_URLS.map(url => {
+      return { url };
+    })
+  );
+
+  Services.prefs.setBoolPref("browser.places.interactions.enabled", true);
+
+  SnapshotMonitor.init();
+  SnapshotMonitor.testGroupBuilders = [fakeBuilder];
+});
+
+add_task(async function test_idle_daily() {
+  sinon.reset();
+
+  SnapshotMonitor.observe(null, "idle-daily");
+
+  Assert.ok(
+    fakeBuilder.rebuild.calledOnce,
+    "Should have triggered a full rebuild"
+  );
+  Assert.ok(
+    !fakeBuilder.update.called,
+    "Should not have triggered an intermediate update"
+  );
+});
+
+function assertUpdateCalled({ addedUrls = [], removedUrls = [] }) {
+  Assert.ok(
+    fakeBuilder.rebuild.notCalled,
+    "Should not have triggered a rebuild"
+  );
+  Assert.ok(
+    fakeBuilder.update.calledOnce,
+    "Should have triggered an intermediate update"
+  );
+  let arg = fakeBuilder.update.args[0][0];
+
+  Assert.deepEqual(
+    [...arg.addedUrls.values()],
+    addedUrls,
+    "Should have added the expected urls"
+  );
+  Assert.deepEqual(
+    [...arg.removedUrls.values()],
+    removedUrls,
+    "Should have removed the expected urls"
+  );
+}
+
+add_task(async function test_add_single_snapshot() {
+  sinon.reset();
+
+  let time = Date.now();
+  await new Promise(resolve => {
+    SnapshotMonitor.setTimerDelaysForTests({ added: 50, removed: 10000 });
+    fakeBuilder.update.callsFake(resolve);
+
+    Snapshots.add({ url: TEST_URLS[0] });
+  });
+
+  Assert.less(Date.now() - time, 300);
+
+  assertUpdateCalled({ addedUrls: [TEST_URLS[0]] });
+});
+
+add_task(async function test_add_multiple_snapshot() {
+  sinon.reset();
+
+  let time = Date.now();
+  await new Promise(resolve => {
+    SnapshotMonitor.setTimerDelaysForTests({ added: 50, removed: 10000 });
+    fakeBuilder.update.callsFake(resolve);
+
+    Snapshots.add({ url: TEST_URLS[1] });
+    Snapshots.add({ url: TEST_URLS[2] });
+  });
+
+  Assert.less(Date.now() - time, 300);
+
+  assertUpdateCalled({ addedUrls: [TEST_URLS[1], TEST_URLS[2]] });
+});
+
+add_task(async function test_remove_snapshots() {
+  sinon.reset();
+
+  let time = Date.now();
+  await new Promise(resolve => {
+    SnapshotMonitor.setTimerDelaysForTests({ added: 10000, removed: 50 });
+    fakeBuilder.update.callsFake(resolve);
+
+    Snapshots.delete(TEST_URLS[1]);
+    Snapshots.delete(TEST_URLS[2]);
+  });
+
+  Assert.less(Date.now() - time, 300);
+
+  assertUpdateCalled({ removedUrls: [TEST_URLS[1], TEST_URLS[2]] });
+});
+
+add_task(async function test_add_and_remove_snapshots() {
+  sinon.reset();
+
+  let time = Date.now();
+  await new Promise(resolve => {
+    SnapshotMonitor.setTimerDelaysForTests({ added: 10000, removed: 50 });
+    fakeBuilder.update.callsFake(resolve);
+
+    Snapshots.add({ url: TEST_URLS[3] });
+    Snapshots.add({ url: TEST_URLS[4] });
+    Snapshots.delete(TEST_URLS[0]);
+  });
+
+  Assert.less(Date.now() - time, 300);
+
+  assertUpdateCalled({
+    addedUrls: [TEST_URLS[3], TEST_URLS[4]],
+    removedUrls: [TEST_URLS[0]],
+  });
+});
