b03a36eaae5ae22b07df435b4b42a9a94c13cb28
ahochheiden <ahochheiden@mozilla.com>
2022-01-31 15:26:17 +0000

    Bug 1750693 - Removed `update_git_repo` function since it is unused r=firefox-build-system-reviewers,mhentges
    Differential Revision: https://phabricator.services.mozilla.com/D136644

--- python/mozversioncontrol/mozversioncontrol/repoupdate.py
+++ python/mozversioncontrol/mozversioncontrol/repoupdate.py
@@ -29,13 +29,3 @@ def update_mercurial_repo(
         subprocess.check_call(args + ["clone", repo, path])
 
     subprocess.check_call([hg, "update", "-r", revision], cwd=path)
-
-
-def update_git_repo(git, repo, path, revision="origin/master"):
-    """Ensure a Git repository exists at a path and is up to date."""
-    if os.path.exists(path):
-        subprocess.check_call([git, "fetch", "--all"], cwd=path)
-    else:
-        subprocess.check_call([git, "clone", repo, path])
-
-    subprocess.check_call([git, "checkout", revision], cwd=path)
