3d03f64988c34743989acad0bee018b2038b74d6
Benjamin VanderSloot <bvandersloot@mozilla.com>
2022-02-01 12:29:22 +0000

    Bug 1750007 - Make tests in dom/cache/test/ dFPI compatible, r=timhuang,asuth
    We are fixing mochitests that fail when network.cookie.cookieBehavior = 5, i.e. when we enable Total Cookie Protection.
    This is most often due to the test assuming that an origin will always have access to its storage state when embedded as
    a third party.
    
    In order to use the Cache API with TCP enabled, you need unpartitioned storage access.
    I add calls to get this permission for each test, wherever the pref environment is added.
    I also made the clearStorage function clear *both* the partitioned and unpartitioned storage.
    This is because the cache the ServiceWorker uses is in the partitioned storage.
    This would make the clearStorage ineffective for the ServiceWorker tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D137310

--- dom/cache/test/mochitest/driver.js
+++ dom/cache/test/mochitest/driver.js
@@ -17,7 +17,16 @@
 // when the returned promise is resolved.
 
 function runTests(testFile, order) {
-  function setupPrefs() {
+  async function setupPrefs() {
+    // Bug 1746646: Make mochitests work with TCP enabled (cookieBehavior = 5)
+    // Acquire storage access permission here so that the Cache API is avaialable
+    SpecialPowers.wrap(document).notifyUserGestureActivation();
+    await SpecialPowers.addPermission(
+      "storageAccessAPI",
+      true,
+      window.location.href
+    );
+    await SpecialPowers.wrap(document).requestStorageAccess();
     return SpecialPowers.pushPrefEnv({
       set: [
         ["dom.caches.enabled", true],
@@ -31,13 +40,21 @@ function runTests(testFile, order) {
 
   // adapted from dom/indexedDB/test/helpers.js
   function clearStorage() {
-    return new Promise(function(resolve, reject) {
+    var clearUnpartitionedStorage = new Promise(function(resolve, reject) {
       var qms = SpecialPowers.Services.qms;
       var principal = SpecialPowers.wrap(document).nodePrincipal;
       var request = qms.clearStoragesForPrincipal(principal);
       var cb = SpecialPowers.wrapCallback(resolve);
       request.callback = cb;
     });
+    var clearPartitionedStorage = new Promise(function(resolve, reject) {
+      var qms = SpecialPowers.Services.qms;
+      var principal = SpecialPowers.wrap(document).partitionedPrincipal;
+      var request = qms.clearStoragesForPrincipal(principal);
+      var cb = SpecialPowers.wrapCallback(resolve);
+      request.callback = cb;
+    });
+    return Promise.all([clearUnpartitionedStorage, clearPartitionedStorage]);
   }
 
   function loadScript(script) {
