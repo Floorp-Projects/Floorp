658e3c69574f2c616b369ac69a60c32da5cfbbb8
Valentin Gosu <valentin.gosu@gmail.com>
2022-02-01 21:08:03 +0000

    Bug 1750989 - Add Shaw to the TRR telemetry keys. r=kershaw, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D136378

--- netwerk/dns/TRRService.cpp
+++ netwerk/dns/TRRService.cpp
@@ -50,6 +50,7 @@ constexpr nsLiteralCString kTRRDomains[] = {
     "firefox.dns.nextdns.io"_ns,
     "private.canadianshield.cira.ca"_ns,
     "doh.xfinity.com"_ns,  // Steered clients
+    "dns.shaw.ca"_ns, // Steered clients
     // clang-format on
 };
 
