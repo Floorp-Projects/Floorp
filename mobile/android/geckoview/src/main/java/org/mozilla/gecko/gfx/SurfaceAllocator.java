d3e80e9854869aa9bafe983876d9ad67656534fb
Jamie Nicol <jnicol@mozilla.com>
2022-02-02 17:01:02 +0000

    Bug 1750234 - Handle GPU process crash when rendering video and webgl on Android. r=jgilbert,jolin
    Make the SurfaceAllocator save a reference to all Surfaces it has
    allocated. When the connection to the remote allocator dies, mark all
    of those surfaces as released. Ensure subsequent attempts to allocate
    new Surfaces will connect to the new compositor process (either GPU or
    parent).
    
    For webgl, when we detect we are using a Surface that has been
    released, clear the pool of Surfaces used for the swapchain so that
    we automatically reallocate new ones.
    
    For video, if we encounter an error in RemoteVideoDecoder and can see
    that Surface has been released, then report a
    NS_ERROR_DOM_MEDIA_NEED_NEW_DECODER in place of the potentially fatal
    error code we get from the java decoder. This will ensure a new
    decoder is created, which will in turn allocate a new Surface to
    decode in to.
    
    On the Android emulator (so potentially real devices too) the java
    decoder doesn't actually report any errors in response to the Surface
    being released. Therefore we also check for the Surface being released
    in ProcessOutput(), and manually raise the NEED_NEW_ENCODER error from
    there if so.
    
    Differential Revision: https://phabricator.services.mozilla.com/D133108

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/SurfaceAllocator.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/SurfaceAllocator.java
@@ -5,8 +5,10 @@
 
 package org.mozilla.gecko.gfx;
 
+import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
+import android.util.SparseArray;
 import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.annotation.WrapForJNI;
 import org.mozilla.gecko.process.GeckoProcessManager;
@@ -17,77 +19,115 @@ import org.mozilla.gecko.process.GeckoServiceChildProcess;
 
   private static ISurfaceAllocator sAllocator;
 
-  private static synchronized void ensureConnection() throws Exception {
+  // Keep a reference to all allocated Surfaces, so that we can release them if we lose the
+  // connection to the allocator service.
+  private static final SparseArray<GeckoSurface> sSurfaces = new SparseArray<GeckoSurface>();
+
+  private static synchronized void ensureConnection() {
     if (sAllocator != null) {
       return;
     }
 
-    if (GeckoAppShell.isParentProcess()) {
-      sAllocator = GeckoProcessManager.getInstance().getSurfaceAllocator();
-    } else {
-      sAllocator = GeckoServiceChildProcess.getSurfaceAllocator();
-    }
+    try {
+      if (GeckoAppShell.isParentProcess()) {
+        sAllocator = GeckoProcessManager.getInstance().getSurfaceAllocator();
+      } else {
+        sAllocator = GeckoServiceChildProcess.getSurfaceAllocator();
+      }
 
-    if (sAllocator == null) {
-      throw new Exception("Failed to connect to surface allocator service!");
+      if (sAllocator == null) {
+        Log.w(LOGTAG, "Failed to connect to RemoteSurfaceAllocator");
+        return;
+      }
+      sAllocator
+          .asBinder()
+          .linkToDeath(
+              new IBinder.DeathRecipient() {
+                @Override
+                public void binderDied() {
+                  Log.w(LOGTAG, "RemoteSurfaceAllocator died");
+                  synchronized (SurfaceAllocator.class) {
+                    // Our connection to the remote allocator has died, so all our surfaces are
+                    // invalid.  Release them all now. When their owners attempt to render in to
+                    // them they can detect they have been released and allocate new ones instead.
+                    for (int i = 0; i < sSurfaces.size(); i++) {
+                      sSurfaces.valueAt(i).release();
+                    }
+                    sSurfaces.clear();
+                    sAllocator = null;
+                  }
+                }
+              },
+              0);
+    } catch (final RemoteException e) {
+      Log.w(LOGTAG, "Failed to connect to RemoteSurfaceAllocator", e);
+      sAllocator = null;
     }
   }
 
   @WrapForJNI
-  public static GeckoSurface acquireSurface(
+  public static synchronized GeckoSurface acquireSurface(
       final int width, final int height, final boolean singleBufferMode) {
     try {
       ensureConnection();
 
+      if (sAllocator == null) {
+        Log.w(LOGTAG, "Failed to acquire GeckoSurface: not connected");
+        return null;
+      }
+
       if (singleBufferMode && !GeckoSurfaceTexture.isSingleBufferSupported()) {
         return null;
       }
+
       final GeckoSurface surface = sAllocator.acquireSurface(width, height, singleBufferMode);
+      sSurfaces.put(surface.getHandle(), surface);
+
       if (surface != null && !surface.inProcess()) {
         sAllocator.configureSync(surface.initSyncSurface(width, height));
       }
       return surface;
-    } catch (final Exception e) {
+    } catch (final RemoteException e) {
       Log.w(LOGTAG, "Failed to acquire GeckoSurface", e);
       return null;
     }
   }
 
   @WrapForJNI
-  public static void disposeSurface(final GeckoSurface surface) {
-    try {
-      ensureConnection();
-    } catch (final Exception e) {
-      Log.w(LOGTAG, "Failed to dispose surface, no connection");
+  public static synchronized void disposeSurface(final GeckoSurface surface) {
+    // If the surface has already been released (probably due to losing connection to the remote
+    // allocator) then there is nothing to do here.
+    if (surface.isReleased()) {
       return;
     }
 
-    // Release the SurfaceTexture on the other side
+    sSurfaces.remove(surface.getHandle());
+
+    // Release our Surface
+    surface.release();
+
+    if (sAllocator == null) {
+      return;
+    }
+
+    // Release the SurfaceTexture on the other side. If we have lost connection then do nothing, as
+    // there is nothing on the other side to release.
     try {
-      sAllocator.releaseSurface(surface.getHandle());
+      if (sAllocator != null) {
+        sAllocator.releaseSurface(surface.getHandle());
+      }
     } catch (final RemoteException e) {
       Log.w(LOGTAG, "Failed to release surface texture", e);
     }
-
-    // And now our Surface
-    try {
-      surface.release();
-    } catch (final Exception e) {
-      Log.w(LOGTAG, "Failed to release surface", e);
-    }
   }
 
-  public static void sync(final int upstream) {
+  public static synchronized void sync(final int upstream) {
+    // Sync from the SurfaceTexture on the other side. If we have lost connection then do nothing,
+    // as there is nothing on the other side to sync from.
     try {
-      ensureConnection();
-    } catch (final Exception e) {
-      Log.w(LOGTAG, "Failed to sync texture, no connection");
-      return;
-    }
-
-    // Release the SurfaceTexture on the other side
-    try {
-      sAllocator.sync(upstream);
+      if (sAllocator != null) {
+        sAllocator.sync(upstream);
+      }
     } catch (final RemoteException e) {
       Log.w(LOGTAG, "Failed to sync texture", e);
     }
