49f0e9a3f73090b8585378d90cf084cfeca82ffb
Csoregi Natalia <ncsoregi@mozilla.com>
2022-02-02 23:54:36 +0200

    Backed out changeset 951a331c6511 (bug 1748884) for causing failures on browser_text_tracks_webvtt_2.js. CLOSED TREE

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -409,7 +409,6 @@ pref("media.decoder-doctor.verbose", false);
 pref("media.decoder-doctor.new-issue-endpoint", "https://webcompat.com/issues/new");
 
 pref("media.videocontrols.picture-in-picture.enabled", false);
-pref("media.videocontrols.picture-in-picture.display-text-tracks.enabled", false);
 pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
 pref("media.videocontrols.picture-in-picture.video-toggle.always-show", false);
 pref("media.videocontrols.picture-in-picture.video-toggle.min-video-secs", 45);
