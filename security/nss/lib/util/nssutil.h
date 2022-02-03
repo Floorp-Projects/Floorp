0cd85f43fe993b37784fd6231e490faf5594eafc
John M. Schanck <jschanck@mozilla.com>
2022-02-03 19:29:01 +0000

    Bug 1748820 - land NSS NSS_3_75_RTM UPGRADE_NSS_RELEASE, r=bbeurdouche
    ```
    2022-02-03  John M. Schanck  <jschanck@mozilla.com>
    
    	* doc/rst/releases/index.rst:
    	Release notes for NSS 3.75
    	[27194d9a9ebd] [NSS_3_75_RTM] <NSS_3_75_BRANCH>
    
    2022-02-03  Benjamin Beurdouche  <bbeurdouche@mozilla.com>
    
    	* lib/nss/nss.h, lib/softoken/softkver.h, lib/util/nssutil.h:
    	Set version numbers to 3.75 final
    	[151f1680d540] <NSS_3_75_BRANCH>
    ```
    
    Differential Revision: https://phabricator.services.mozilla.com/D137783

--- security/nss/lib/util/nssutil.h
+++ security/nss/lib/util/nssutil.h
@@ -19,12 +19,12 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <Beta>]"
  */
-#define NSSUTIL_VERSION "3.75 Beta"
+#define NSSUTIL_VERSION "3.75"
 #define NSSUTIL_VMAJOR 3
 #define NSSUTIL_VMINOR 75
 #define NSSUTIL_VPATCH 0
 #define NSSUTIL_VBUILD 0
-#define NSSUTIL_BETA PR_TRUE
+#define NSSUTIL_BETA PR_FALSE
 
 SEC_BEGIN_PROTOS
 
