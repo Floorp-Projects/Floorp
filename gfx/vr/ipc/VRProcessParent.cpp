f2734135dd2e4d58523d00da6d9e2ed89b69ec5c
Jed Davis <jld@mozilla.com>
2022-02-04 18:03:40 +0000

    Bug 1750679. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D137359

--- gfx/vr/ipc/VRProcessParent.cpp
+++ gfx/vr/ipc/VRProcessParent.cpp
@@ -43,12 +43,6 @@ VRProcessParent::VRProcessParent(Listener* aListener)
 }
 
 VRProcessParent::~VRProcessParent() {
-  // Cancel all tasks. We don't want anything triggering after our caller
-  // expects this to go away.
-  {
-    MonitorAutoLock lock(mMonitor);
-    mTaskFactory.RevokeAll();
-  }
   MOZ_COUNT_DTOR(VRProcessParent);
 }
 
@@ -133,6 +127,13 @@ void VRProcessParent::Shutdown() {
 
 void VRProcessParent::DestroyProcess() {
   if (mLaunchThread) {
+    // Cancel all tasks. We don't want anything triggering after our caller
+    // expects this to go away.
+    {
+      MonitorAutoLock lock(mMonitor);
+      mTaskFactory.RevokeAll();
+    }
+
     mLaunchThread->Dispatch(NS_NewRunnableFunction("DestroyProcessRunnable",
                                                    [this] { Destroy(); }));
   }
