bcb952301ed98651743b9c3d1d9d4f7181fbf7d6
Mike Hommey <mh+mozilla@glandium.org>
2022-02-04 22:19:32 +0000

    Bug 1742972 - Upgrade lmdb-rkv-sys to 0.11.2. r=emilio
    It will stop creating a text_env section, which in turn will work around
    the linking problems on no-opt builds on arm64 mac.
    
    Differential Revision: https://phabricator.services.mozilla.com/D137902

--- third_party/rust/lmdb-rkv-sys/src/lib.rs
+++ third_party/rust/lmdb-rkv-sys/src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(warnings)]
 #![allow(non_camel_case_types)]
 #![allow(clippy::all)]
-#![doc(html_root_url = "https://docs.rs/lmdb-rkv-sys/0.11.0")]
+#![doc(html_root_url = "https://docs.rs/lmdb-rkv-sys/0.11.2")]
 
 extern crate libc;
 
