15ae4b1503a66c7b5ecb419350397544510e8213
Norisz Fay <nfay@mozilla.com>
2022-02-08 00:02:49 +0200

    Backed out changeset 370f46a043e4 (bug 1752907) for causing mochitest failures on browser_navigator.js

--- dom/workers/RuntimeService.h
+++ dom/workers/RuntimeService.h
@@ -178,7 +178,7 @@ class RuntimeService final : public nsIObserver {
 
   void MemoryPressureAllWorkers();
 
-  uint32_t ClampedHardwareConcurrency(bool aShouldResistFingerprinting) const;
+  uint32_t ClampedHardwareConcurrency() const;
 
   void CrashIfHanging();
 
