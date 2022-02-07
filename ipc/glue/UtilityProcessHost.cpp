01831135cc6d88f14ada499981576193fe492511
Dan Minor <dminor@mozilla.com>
2022-02-07 16:44:18 +0000

    Bug 1751936 - Update users of SharedPreferenceSerializer to pass ShouldSyncPreference; r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D137476

--- ipc/glue/UtilityProcessHost.cpp
+++ ipc/glue/UtilityProcessHost.cpp
@@ -6,6 +6,7 @@
 
 #include "UtilityProcessHost.h"
 
+#include "mozilla/dom/ContentParent.h"
 #include "mozilla/ipc/Endpoint.h"
 #include "mozilla/ipc/UtilityProcessManager.h"
 #include "mozilla/Telemetry.h"
@@ -59,7 +60,8 @@ bool UtilityProcessHost::Launch(StringVector aExtraOpts) {
   MOZ_ASSERT(mLaunchPhase == LaunchPhase::Unlaunched);
   MOZ_ASSERT(!mUtilityProcessParent);
 
-  mPrefSerializer = MakeUnique<ipc::SharedPreferenceSerializer>();
+  mPrefSerializer = MakeUnique<ipc::SharedPreferenceSerializer>(
+      dom::ContentParent::ShouldSyncPreference);
   if (!mPrefSerializer->SerializeToSharedMemory()) {
     return false;
   }
