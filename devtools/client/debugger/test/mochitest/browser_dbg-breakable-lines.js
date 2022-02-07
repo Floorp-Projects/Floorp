91a72e941f6124c853e964a5b5df31add3f6c2d9
Hubert Boma Manilla <hmanilla@mozilla.com>
2022-02-07 11:48:33 +0000

    Bug 1753159 - [devtools] Fix intermittent caused by not waiting for the source r=ochameau a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D137978

--- devtools/client/debugger/test/mochitest/browser_dbg-breakable-lines.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-breakable-lines.js
@@ -7,13 +7,12 @@ const TEST_URL = testServer.urlFor("index.html");
 
 // Assert the behavior of the gutter that grays out non-breakable lines
 add_task(async function testBreakableLinesOverReloads() {
-  const dbg = await initDebuggerWithAbsoluteURL(TEST_URL, "script.js");
+  const dbg = await initDebuggerWithAbsoluteURL(TEST_URL, "index.html", "script.js", "original.js");
 
   info("Assert breakable lines of the first html page load");
   await assertBreakableLines(dbg, "index.html", 20, [[16], [17]]);
 
   info("Assert breakable lines of the first original source file, original.js");
-  await waitForSource(dbg, "original.js");
   // The length of original.js is longer than the test file
   // because the sourcemap replaces the content of the original file
   // and appends a few lines with a "WEBPACK FOOTER" comment
@@ -23,12 +22,9 @@ add_task(async function testBreakableLinesOverReloads() {
   info("Assert breakable lines of the simple first load of script.js");
   await assertBreakableLines(dbg, "script.js", 3, [[1], [3]]);
 
-  info("Reload the page and assert that breakable lines get updated");
+  info("Reload the page, wait for sources and assert that breakable lines get updated");
   testServer.switchToNextVersion();
-  await reload(dbg);
-
-  info("Wait for script.js to be ready");
-  await waitForSelectedSource(dbg, "script.js");
+  await reload(dbg, "index.html", "script.js", "original.js");
 
   info("Assert breakable lines of the more complex second load of script.js");
   await assertBreakableLines(dbg, "script.js", 23, [[2], [13,23]]);
@@ -37,7 +33,6 @@ add_task(async function testBreakableLinesOverReloads() {
   await assertBreakableLines(dbg, "index.html", 21, [[15], [17]]);
 
   info("Assert breakable lines of the second orignal file");
-  await waitForSource(dbg, "original.js");
   // See first assertion about original.js,
   // the size of original.js doesn't match the size of the test file
   await assertBreakableLines(dbg, "original.js", 18, [[1,3], [8,11], [13]]);
