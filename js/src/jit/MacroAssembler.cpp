9034f0aee607cae1da28e5286e772ecb2c675780
Lars T Hansen <lhansen@mozilla.com>
2022-02-08 07:42:35 +0000

    Bug 1753061 - re-apply bug 1743586 part 2. r=rhunt
    This recovers the general improvements to bounds checking against known table
    sizes, which is good to have for code density anyway.
    
    Differential Revision: https://phabricator.services.mozilla.com/D137844

--- js/src/jit/MacroAssembler.cpp
+++ js/src/jit/MacroAssembler.cpp
@@ -3793,6 +3793,13 @@ std::pair<CodeOffset, uint32_t> MacroAssembler::wasmReserveStackChecked(
   return std::pair<CodeOffset, uint32_t>(trapInsnOffset, amount);
 }
 
+void MacroAssembler::loadWasmGlobalPtr(uint32_t globalDataOffset,
+                                       Register dest) {
+  loadPtr(Address(WasmTlsReg,
+                  offsetof(wasm::TlsData, globalArea) + globalDataOffset),
+          dest);
+}
+
 CodeOffset MacroAssembler::wasmCallImport(const wasm::CallSiteDesc& desc,
                                           const wasm::CalleeDesc& callee) {
   storePtr(WasmTlsReg,
@@ -3874,9 +3881,10 @@ CodeOffset MacroAssembler::wasmCallBuiltinInstanceMethod(
   return ret;
 }
 
-CodeOffset MacroAssembler::wasmCallIndirect(const wasm::CallSiteDesc& desc,
-                                            const wasm::CalleeDesc& callee,
-                                            bool needsBoundsCheck) {
+CodeOffset MacroAssembler::asmCallIndirect(const wasm::CallSiteDesc& desc,
+                                           const wasm::CalleeDesc& callee) {
+  MOZ_ASSERT(callee.which() == wasm::CalleeDesc::AsmJSTable);
+
   Register scratch = WasmTableCallScratchReg0;
   Register index = WasmTableCallIndexReg;
 
@@ -3888,25 +3896,30 @@ CodeOffset MacroAssembler::wasmCallIndirect(const wasm::CallSiteDesc& desc,
                     sizeof(wasm::FunctionTableElem) == 16,
                 "elements of function tables are two words");
 
-  if (callee.which() == wasm::CalleeDesc::AsmJSTable) {
-    // asm.js tables require no signature check, and have had their index
-    // masked into range and thus need no bounds check.
-    loadWasmGlobalPtr(callee.tableFunctionBaseGlobalDataOffset(), scratch);
-    if (sizeof(wasm::FunctionTableElem) == 8) {
-      computeEffectiveAddress(BaseIndex(scratch, index, TimesEight), scratch);
-    } else {
-      lshift32(Imm32(4), index);
-      addPtr(index, scratch);
-    }
-    loadPtr(Address(scratch, offsetof(wasm::FunctionTableElem, code)), scratch);
-    storePtr(WasmTlsReg,
-             Address(getStackPointer(), WasmCallerTlsOffsetBeforeCall));
-    storePtr(WasmTlsReg,
-             Address(getStackPointer(), WasmCalleeTlsOffsetBeforeCall));
-    return call(desc, scratch);
+  // asm.js tables require no signature check, and have had their index
+  // masked into range and thus need no bounds check.
+  loadWasmGlobalPtr(callee.tableFunctionBaseGlobalDataOffset(), scratch);
+  if (sizeof(wasm::FunctionTableElem) == 8) {
+    computeEffectiveAddress(BaseIndex(scratch, index, TimesEight), scratch);
+  } else {
+    lshift32(Imm32(4), index);
+    addPtr(index, scratch);
   }
+  loadPtr(Address(scratch, offsetof(wasm::FunctionTableElem, code)), scratch);
+  storePtr(WasmTlsReg,
+           Address(getStackPointer(), WasmCallerTlsOffsetBeforeCall));
+  storePtr(WasmTlsReg,
+           Address(getStackPointer(), WasmCalleeTlsOffsetBeforeCall));
+  return call(desc, scratch);
+}
 
+CodeOffset MacroAssembler::wasmCallIndirect(
+    const wasm::CallSiteDesc& desc, const wasm::CalleeDesc& callee,
+    bool needsBoundsCheck, mozilla::Maybe<uint32_t> tableSize) {
   MOZ_ASSERT(callee.which() == wasm::CalleeDesc::WasmTable);
+
+  Register scratch = WasmTableCallScratchReg0;
+  Register index = WasmTableCallIndexReg;
 
   // Write the functype-id into the ABI functype-id register.
   wasm::TypeIdDesc funcTypeId = callee.wasmTableSigId();
@@ -3925,10 +3938,15 @@ CodeOffset MacroAssembler::wasmCallIndirect(const wasm::CallSiteDesc& desc,
 
   // WebAssembly throws if the index is out-of-bounds.
   if (needsBoundsCheck) {
-    loadWasmGlobalPtr(callee.tableLengthGlobalDataOffset(), scratch);
-
     Label ok;
-    branch32(Assembler::Condition::Below, index, scratch, &ok);
+    if (tableSize.isSome()) {
+      branch32(Assembler::Condition::Below, index, Imm32(*tableSize), &ok);
+    } else {
+      branch32(Assembler::Condition::Above,
+               Address(WasmTlsReg, offsetof(wasm::TlsData, globalArea) +
+                                       callee.tableLengthGlobalDataOffset()),
+               index, &ok);
+    }
     wasmTrap(wasm::Trap::OutOfBounds, trapOffset);
     bind(&ok);
   }
