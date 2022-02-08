744025ce3990c2ea754c0d2eed0f8985f1634189
Lars T Hansen <lhansen@mozilla.com>
2022-02-08 07:42:28 +0000

    Bug 1753061 - revert bug 1742053 part 12. r=rhunt
    Differential Revision: https://phabricator.services.mozilla.com/D137634

--- js/src/wasm/WasmTable.cpp
+++ js/src/wasm/WasmTable.cpp
@@ -230,12 +230,10 @@ bool Table::fillFuncRef(Maybe<Tier> maybeTier, uint32_t index,
 
   Instance& instance = instanceObj->instance();
   Tier tier = maybeTier ? maybeTier.value() : instance.code().bestTier();
-  void* code = instance.ensureAndGetIndirectStub(tier, funcIndex);
+  void* code = instance.createIndirectStub(tier, funcIndex);
   if (!code) {
     return false;
   }
-
-  // Note, infallible beyond this point.
 
   for (uint32_t i = index, end = index + fillCount; i != end; i++) {
     setFuncRef(i, code, &instance);
