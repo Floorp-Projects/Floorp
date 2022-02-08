6c6e1755bf8216e1d4af2723f138100cff640bc2
Daniel Holbert <dholbert@cs.stanford.edu>
2022-02-08 21:22:18 +0000

    Bug 1754277: Fix non-unified build errors in layout/svg, and mark it as safe to build in non-unified mode. r=emilio
    Below are the build errors fixed here.
    
    layout/svg/SVGFEUnstyledLeafFrame.cpp:75:33: error: no type named 'SVGFEUnstyledElement' in namespace 'mozilla::dom'
    
    * Fixed this by including SVGFilters.h (which defines this type).
    
    layout/svg/SVGFEContainerFrame.cpp:83:12: error: unknown type name 'SVGFE'; did you mean 'dom::SVGFE'?
    layout/svg/SVGFELeafFrame.cpp:80:12: error: unknown type name 'SVGFE'; did you mean 'dom::SVGFE'?
    
    * Fixed these with a 'using' statement for the namespace.
    
    layout/svg/SVGGFrame.cpp:34:32: error: unknown type name 'SVGElement'
    
    * Fixed by including this type's header.
    
    layout/svg/FilterInstance.cpp:873:9: error: use of undeclared identifier 'nsLayoutUtils'
    layout/svg/SVGOuterSVGFrame.cpp:623:29: error: incomplete type 'nsLayoutUtils' named in nested name specifier
    layout/svg/SVGUseFrame.cpp:84:5: error: use of undeclared identifier 'nsLayoutUtils
    layout/svg/SVGViewportFrame.cpp:207:9: error: use of undeclared identifier 'nsLayoutUtils'
    
    * Fixed these by including this header.
    
    layout/svg/SVGOuterSVGFrame.cpp:88:20: error: unused function 'DependsOnIntrinsicSize' [-Werror,-Wunused-function]
    
    * Fixed by removing the unused function.
    
    layout/svg/SVGStopFrame.cpp:86:21: error: unknown type name 'SVGGradientFrame'
    
    * Fixed this by including this header.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138187

--- layout/svg/SVGFEUnstyledLeafFrame.cpp
+++ layout/svg/SVGFEUnstyledLeafFrame.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Keep in (case-insensitive) order:
+#include "mozilla/dom/SVGFilters.h"
 #include "mozilla/PresShell.h"
 #include "mozilla/SVGObserverUtils.h"
 #include "nsContainerFrame.h"
