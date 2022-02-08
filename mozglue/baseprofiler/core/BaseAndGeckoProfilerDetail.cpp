e63b8d4f7c41872349f504074312c0ddad965b6d
Gerald Squelart <gsquelart@mozilla.com>
2022-02-08 02:40:50 +0000

    Bug 1735397 - mozilla::profiler::detail::FilterHasPid and FiltersExcludePid - r=florian
    These will be used to check special thread filter rules around "pid:..." filters.
    
    Differential Revision: https://phabricator.services.mozilla.com/D135853

--- mozglue/baseprofiler/core/BaseAndGeckoProfilerDetail.cpp
+++ mozglue/baseprofiler/core/BaseAndGeckoProfilerDetail.cpp
@@ -6,8 +6,50 @@
 
 #include "mozilla/BaseAndGeckoProfilerDetail.h"
 
+#include <cstring>
+#include <string>
+#include <string_view>
+
 namespace mozilla::profiler::detail {
 
-;  // TODO Add code here.
+constexpr std::string_view scPidPrefix = "pid:";
+
+[[nodiscard]] MFBT_API bool FilterHasPid(
+    const char* aFilter, baseprofiler::BaseProfilerProcessId aPid) {
+  if (strncmp(aFilter, scPidPrefix.data(), scPidPrefix.length()) != 0) {
+    // The filter is not starting with "pid:".
+    return false;
+  }
+
+  const std::string pidString = std::to_string(aPid.ToNumber());
+  return pidString == (aFilter + scPidPrefix.length());
+}
+
+[[nodiscard]] MFBT_API bool FiltersExcludePid(
+    Span<const char* const> aFilters,
+    baseprofiler::BaseProfilerProcessId aPid) {
+  if (aFilters.empty()) {
+    return false;
+  }
+
+  // First, check if the list only contains "pid:..." strings.
+  for (const char* const filter : aFilters) {
+    if (strncmp(filter, scPidPrefix.data(), scPidPrefix.length()) != 0) {
+      // At least one filter is *not* a "pid:...", our pid is not excluded.
+      return false;
+    }
+  }
+
+  // Here, all filters start with "pid:". Check if the given pid is included.
+  const std::string pidString = std::to_string(aPid.ToNumber());
+  for (const char* const filter : aFilters) {
+    if (pidString == (filter + scPidPrefix.length())) {
+      // Our pid is present, so it's not excluded.
+      return false;
+    }
+  }
+  // Our pid was not in a list of only pids, so it's excluded.
+  return true;
+}
 
 }  // namespace mozilla::profiler::detail
