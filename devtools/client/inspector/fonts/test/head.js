92c00930410a05a449fe0242b74d26c24b3177a7
Sai Hemanth B <saihemanth9019@gmail.com>
2022-02-08 15:04:09 +0000

    Bug 1639179 - Add different preferences for 3 and 2-pane default tabs. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D137375

--- devtools/client/inspector/fonts/test/head.js
+++ devtools/client/inspector/fonts/test/head.js
@@ -15,6 +15,7 @@ Services.scriptloader.loadSubScript(
 Services.prefs.setCharPref("devtools.inspector.activeSidebar", "fontinspector");
 registerCleanupFunction(() => {
   Services.prefs.clearUserPref("devtools.inspector.activeSidebar");
+  Services.prefs.clearUserPref("devtools.inspector.selectedSidebar");
 });
 
 var nodeConstants = require("devtools/shared/dom-node-constants");
