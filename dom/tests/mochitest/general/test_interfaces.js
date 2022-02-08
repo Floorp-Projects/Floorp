e78cb0c280c1f2746703c2d7d31baf371a5de76c
Karl Dubost <kdubost@mozilla.com>
2022-02-08 15:02:33 +0000

    Bug 1637922. Switches to disable everywhere for netinfo. r=annevk,webidl,peterv
    Differential Revision: https://phabricator.services.mozilla.com/D126442

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -801,7 +801,7 @@ var interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "Navigator", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "NetworkInformation", insecureContext: true, desktop: false },
+  { name: "NetworkInformation", insecureContext: true, disabled: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "Node", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
