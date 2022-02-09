b7678a86ba98da8353d49e6716708b7e9b3d6a92
Eden Chuang <echuang@mozilla.com>
2022-02-09 07:14:26 +0000

    Bug 1750515 - Enable NavigationPreload API on Nightly. r=dom-worker-reviewers,smaug
    Depends on D137599
    
    Differential Revision: https://phabricator.services.mozilla.com/D136215

--- dom/serviceworkers/test/test_serviceworker_interfaces.js
+++ dom/serviceworkers/test/test_serviceworker_interfaces.js
@@ -226,6 +226,8 @@ var interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "NetworkInformation", android: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
+  "NavigationPreloadManager",
+  // IMPORTANT: Do not change this list without review from a DOM peer!
   "Notification",
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "NotificationEvent",
