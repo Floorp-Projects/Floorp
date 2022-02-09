cdc5972bff151334fbf0f76e3813b4419ec8d8e3
Daniel Holbert <dholbert@cs.stanford.edu>
2022-02-09 01:06:29 +0000

    Bug 1754315: Fix non-unified build errors in layout/painting, and mark it as safe to build in non-unified mode. r=boris
    Build errors fixed here:
    
    layout/painting/DottedCornerFinder.cpp:33:1: error: no template named 'nsTHashMap'
    (and similar for layout/painting/DashedCornerFinder.cpp)
    
    * Fixed by including nsTHashMap.h
    
    layout/painting/WindowRenderer.cpp:186:38: error: use of undeclared identifier 'Rect'; did you mean 'gfx::Rect'?
    layout/painting/WindowRenderer.cpp:111:18: error: unknown type name 'PersistentBufferProvider'; did you mean 'layers::PersistentBufferProvider'?
    (and more similar errors for e.g. `PersistentBufferProviderBasic::Create`, etc.)
    
    * Fixed by adding 'using' statements.
    
    layout/painting/WindowRenderer.cpp:117:8: error: use of undeclared identifier 'gfxPlatform'
    layout/painting/WindowRenderer.cpp:148:57: error: member access into incomplete type 'mozilla::dom::AnimationEffect'
    layout/painting/WindowRenderer.cpp:150:16: error: incomplete type 'mozilla::EffectSet' named in nested name specifier
    layout/painting/WindowRenderer.cpp:211:26: error: member access into incomplete type 'mozilla::nsDisplayList'
    
    * Fixed by including these types' headers.
    
    layout/painting/HitTestInfo.cpp:67:32: error: use of undeclared identifier 'CompositorHitTestFlags'; did you mean 'gfx::CompositorHitTestFlags'?
    
    * Fixed by adding 'using' statement.
    
    layout/painting/nsCSSRenderingGradients.cpp:772:26: error: incomplete type 'nsLayoutUtils' named in nested name specifier
    
    * Fixed by including nsLayoutUtils.h
    
    layout/painting/nsCSSRenderingGradients.h:106:29: error: unknown type name 'gfxPattern'
    
    * Fixed with a forward-decl.
    
    layout/painting/nsCSSRenderingGradients.h:107:30: error: unknown type name 'gfxRect'
    
    * Fixed with an include (can't use a forward-decl here; gfxRect is a typedef and hence can't really be forward-declared).
    
    Differential Revision: https://phabricator.services.mozilla.com/D138219

--- layout/painting/WindowRenderer.cpp
+++ layout/painting/WindowRenderer.cpp
@@ -6,8 +6,15 @@
 
 #include "WindowRenderer.h"
 
+#include "gfxPlatform.h"
 #include "mozilla/dom/Animation.h"  // for Animation
+#include "mozilla/dom/AnimationEffect.h"
+#include "mozilla/EffectSet.h"
 #include "mozilla/layers/PersistentBufferProvider.h"  // for PersistentBufferProviderBasic, PersistentBufferProvider (ptr only)
+#include "nsDisplayList.h"
+
+using namespace mozilla::gfx;
+using namespace mozilla::layers;
 
 namespace mozilla {
 
