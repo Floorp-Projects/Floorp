5e17bc5cd2f7f93535d34448388d9dd650ee43b9
Nika Layzell <nika@thelayzells.com>
2022-02-09 14:44:51 +0000

    Bug 1744352 - Part 1: Ensure we set InitialSandboxFlags when opening or replacing toplevel BrowsingContexts, r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D138209

--- docshell/base/BrowsingContext.h
+++ docshell/base/BrowsingContext.h
@@ -127,6 +127,8 @@ enum class ExplicitActiveStatus : uint8_t {
   FIELD(ShouldDelayMediaFromStart, bool)                                      \
   /* See nsSandboxFlags.h for the possible flags. */                          \
   FIELD(SandboxFlags, uint32_t)                                               \
+  /* The value of SandboxFlags when the BrowsingContext is first created.     \
+   * Used for sandboxing the initial about:blank document. */                 \
   FIELD(InitialSandboxFlags, uint32_t)                                        \
   /* A non-zero unique identifier for the browser element that is hosting     \
    * this                                                                     \
