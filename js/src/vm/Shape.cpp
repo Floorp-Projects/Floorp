37378c9b31c4a2196865296ab32d4f63cad020f0
Jan de Mooij <jdemooij@mozilla.com>
2022-02-10 09:13:18 +0000

    Bug 1754405 part 3 - Remove JSID_IS_VOID. r=evilpie
    Depends on D138273
    
    Differential Revision: https://phabricator.services.mozilla.com/D138274

--- js/src/vm/Shape.cpp
+++ js/src/vm/Shape.cpp
@@ -152,7 +152,7 @@ static void AssertValidCustomDataProp(NativeObject* obj, PropertyFlags flags) {
 /* static */
 bool NativeObject::addCustomDataProperty(JSContext* cx, HandleNativeObject obj,
                                          HandleId id, PropertyFlags flags) {
-  MOZ_ASSERT(!JSID_IS_VOID(id));
+  MOZ_ASSERT(!id.isVoid());
   MOZ_ASSERT(!id.isPrivateName());
   MOZ_ASSERT(!obj->containsPure(id));
 
@@ -275,7 +275,7 @@ bool NativeObject::addProperty(JSContext* cx, HandleNativeObject obj,
 
   // The object must not contain a property named |id|. The object must be
   // extensible, but allow private fields and sparsifying dense elements.
-  MOZ_ASSERT(!JSID_IS_VOID(id));
+  MOZ_ASSERT(!id.isVoid());
   MOZ_ASSERT(!obj->containsPure(id));
   MOZ_ASSERT_IF(!id.isPrivateName(),
                 obj->isExtensible() ||
@@ -390,7 +390,7 @@ bool NativeObject::addPropertyInReservedSlot(JSContext* cx,
   MOZ_ASSERT(slot < JSCLASS_RESERVED_SLOTS(obj->getClass()));
 
   // The object must not contain a property named |id| and must be extensible.
-  MOZ_ASSERT(!JSID_IS_VOID(id));
+  MOZ_ASSERT(!id.isVoid());
   MOZ_ASSERT(!obj->containsPure(id));
   MOZ_ASSERT(!id.isPrivateName());
   MOZ_ASSERT(obj->isExtensible());
@@ -463,7 +463,7 @@ static void AssertValidArrayIndex(NativeObject* obj, jsid id) {
 bool NativeObject::changeProperty(JSContext* cx, HandleNativeObject obj,
                                   HandleId id, PropertyFlags flags,
                                   uint32_t* slotOut) {
-  MOZ_ASSERT(!JSID_IS_VOID(id));
+  MOZ_ASSERT(!id.isVoid());
 
   AutoCheckShapeConsistency check(obj);
   AssertValidArrayIndex(obj, id);
@@ -583,7 +583,7 @@ bool NativeObject::changeCustomDataPropAttributes(JSContext* cx,
                                                   HandleNativeObject obj,
                                                   HandleId id,
                                                   PropertyFlags flags) {
-  MOZ_ASSERT(!JSID_IS_VOID(id));
+  MOZ_ASSERT(!id.isVoid());
 
   AutoCheckShapeConsistency check(obj);
   AssertValidArrayIndex(obj, id);
