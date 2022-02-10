75bda525198114c12ecc93c10bffc2b84d1fd57b
Masayuki Nakano <masayuki@d-toybox.com>
2022-02-10 00:39:42 +0000

    Bug 1754367 - Get rid of `WidgetGUIEvent::mPluginEvent` r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D138257

--- widget/gtk/nsGtkKeyUtils.cpp
+++ widget/gtk/nsGtkKeyUtils.cpp
@@ -1861,7 +1861,6 @@ void KeymapWrapper::InitKeyEvent(WidgetKeyboardEvent& aKeyEvent,
   // so link to the GdkEvent (which will vanish soon after return from the
   // event callback) to give plugins access to hardware_keycode and state.
   // (An XEvent would be nice but the GdkEvent is good enough.)
-  aKeyEvent.mPluginEvent.Copy(*aGdkKeyEvent);
   aKeyEvent.mTime = aGdkKeyEvent->time;
   aKeyEvent.mNativeKeyEvent = static_cast<void*>(aGdkKeyEvent);
   aKeyEvent.mIsRepeat =
