20dc7fc2b5b42e797fcdea83da6701dbf383d3b1
Morgan Reschenberg <mreschenberg@mozilla.com>
2022-02-10 20:46:54 +0000

    Bug 1750583: Create MOXRootGroup for all body elements without a document role r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D138174

--- accessible/mac/mozAccessible.mm
+++ accessible/mac/mozAccessible.mm
@@ -317,8 +317,7 @@ static const uint64_t kCacheInitialized = ((uint64_t)0x1) << 63;
   }
 
   id nativeParent = GetNativeFromGeckoAccessible(parent);
-  if (parent->Role() == roles::DOCUMENT &&
-      [nativeParent respondsToSelector:@selector(rootGroup)]) {
+  if ([nativeParent respondsToSelector:@selector(rootGroup)]) {
     // Before returning a WebArea as parent, check to see if
     // there is a generated root group that is an intermediate container.
     if (id<mozAccessible> rootGroup = [nativeParent rootGroup]) {
