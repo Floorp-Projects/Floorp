43763a72b3461574bd30a92d09e317ebdd60bf81
Julian Descottes <jdescottes@mozilla.com>
2022-02-10 09:12:46 +0000

    Bug 1752843 - [devtools] Wait for toolbox panels reload and properly close tabs. r=Honza, a=test-only
    After fixing the initial failing test by both waiting for DevTools reload propertly and closing all debug tabs, the following test started failing.
    Waiting for DevTools to properly update in this test seems to fix the intermittent(s?).
    
    Differential Revision: https://phabricator.services.mozilla.com/D138010

--- devtools/client/aboutdebugging/test/browser/browser_aboutdebugging_devtoolstoolbox_navigate_reload_button.js
+++ devtools/client/aboutdebugging/test/browser/browser_aboutdebugging_devtoolstoolbox_navigate_reload_button.js
@@ -9,19 +9,18 @@ function clickReload(devtoolsDocument) {
 
 // Test that ensures the remote page is reloaded when the button is clicked
 add_task(async function() {
-  await addTab("about:home");
+  const debug_tab = await addTab("about:home");
 
   const { document, tab, window } = await openAboutDebugging();
 
   // go to This Firefox and inspect the new tab
   info("Inspecting a new tab in This Firefox");
   await selectThisFirefoxPage(document, window.AboutDebugging.store);
-  const { devtoolsDocument, devtoolsWindow } = await openAboutDevtoolsToolbox(
-    document,
-    tab,
-    window,
-    "about:home"
-  );
+  const {
+    devtoolsDocument,
+    devtoolsTab,
+    devtoolsWindow,
+  } = await openAboutDevtoolsToolbox(document, tab, window, "about:home");
 
   info("Clicking reload button and waiting for requests to complete");
   const toolbox = getToolbox(devtoolsWindow);
@@ -29,13 +28,30 @@ add_task(async function() {
     onDomCompleteResource,
   } = await waitForNextTopLevelDomCompleteResource(toolbox.commands);
 
+  // Watch for navigation promises.
   const refreshes = Promise.all([
     onDomCompleteResource,
     toolbox.commands.client.waitForRequestsToSettle(),
     waitForAboutDebuggingRequests(window.AboutDebugging.store),
   ]);
+
+  // We cannot include this one in the Promise.all array, as it needs to be
+  // explicitly called after navigation started.
+  const waitForLoadedPanelsReload = await watchForLoadedPanelsReload(toolbox);
+
   clickReload(devtoolsDocument);
   await refreshes;
+  await waitForLoadedPanelsReload();
 
   ok(true, "Clicked refresh; both page and devtools reloaded");
+
+  await closeAboutDevtoolsToolbox(document, devtoolsTab, window);
+
+  info("Remove the debugged tab");
+  await removeTab(debug_tab);
+  await waitUntil(() => !findDebugTargetByText("about:home", document));
+  await waitForAboutDebuggingRequests(window.AboutDebugging.store);
+
+  info("Remove the about:debugging tab.");
+  await removeTab(tab);
 });
