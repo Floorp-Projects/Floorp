e7b4514c32606e5b672bdad843597826024177fb
Niklas Baumgardner <nbaumgardner@mozilla.com>
2022-02-10 15:23:44 +0000

    Bug 1749074 - Cancel screenshots on page unload. r=sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D135522

---
+++ browser/components/screenshots/tests/browser/browser_screenshots_page_unload.js
@@ -0,0 +1,79 @@
+/* Any copyright is dedicated to the Public Domain.
+  http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+add_task(async function test() {
+  CustomizableUI.addWidgetToArea(
+    "screenshot-button",
+    CustomizableUI.AREA_NAVBAR
+  );
+  let screenshotBtn = document.getElementById("screenshot-button");
+  Assert.ok(screenshotBtn, "The screenshots button was added to the nav bar");
+
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: TEST_PAGE,
+    },
+    async browser => {
+      await SpecialPowers.spawn(browser, [TEST_PAGE], url => {
+        let a = content.document.createElement("a");
+        a.id = "clickMe";
+        a.href = url;
+        a.textContent = "Click me to unload page";
+        content.document.querySelector("body").appendChild(a);
+      });
+
+      let helper = new ScreenshotsHelper(browser);
+
+      // click toolbar button so panel shows
+      helper.triggerUIFromToolbar();
+
+      let panel = gBrowser.selectedBrowser.ownerDocument.querySelector(
+        "#screenshotsPagePanel"
+      );
+      await BrowserTestUtils.waitForMutationCondition(
+        panel,
+        { attributes: true },
+        () => {
+          return BrowserTestUtils.is_visible(panel);
+        }
+      );
+      ok(BrowserTestUtils.is_visible(panel), "Panel buttons are visible");
+
+      await ContentTask.spawn(browser, null, async () => {
+        let screenshotsChild = content.windowGlobalChild.getActor(
+          "ScreenshotsComponent"
+        );
+        Assert.ok(screenshotsChild._overlay._initialized, "The overlay exists");
+      });
+
+      await SpecialPowers.spawn(browser, [], () => {
+        content.document.querySelector("#clickMe").click();
+      });
+
+      await BrowserTestUtils.waitForMutationCondition(
+        panel,
+        { attributes: true },
+        () => {
+          return BrowserTestUtils.is_hidden(panel);
+        }
+      );
+      ok(
+        BrowserTestUtils.is_hidden(panel),
+        "Panel buttons are hidden after page unload"
+      );
+
+      await ContentTask.spawn(browser, null, async () => {
+        let screenshotsChild = content.windowGlobalChild.getActor(
+          "ScreenshotsComponent"
+        );
+        Assert.ok(
+          !screenshotsChild._overlay._initialized,
+          "The overlay doesn't exist"
+        );
+      });
+    }
+  );
+});
