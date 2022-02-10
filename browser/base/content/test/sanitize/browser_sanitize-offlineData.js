567cd16d6e8d5d2913457c937e60e4946243c009
Hannah Peuckmann <hpeuckmann@mozilla.com>
2022-02-10 16:29:47 +0000

    Bug 1681701 - Enable exceptions in sanitization. r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D136463

--- browser/base/content/test/sanitize/browser_sanitize-offlineData.js
+++ browser/base/content/test/sanitize/browser_sanitize-offlineData.js
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
 // Bug 380852 - Delete permission manager entries in Clear Recent History
 
 const { Sanitizer } = ChromeUtils.import("resource:///modules/Sanitizer.jsm");
@@ -205,4 +208,65 @@ add_task(async function testWithRange() {
 
   // Clean up.
   await Sanitizer.sanitize(itemsToClear);
+});
+
+add_task(async function testExceptionsOnShutdown() {
+  await createData("example.org");
+  await createData("example.com");
+
+  // Set exception for example.org to not get cleaned
+  let originALLOW = "https://example.org";
+  PermissionTestUtils.add(
+    originALLOW,
+    "cookie",
+    Ci.nsICookiePermission.ACCESS_ALLOW
+  );
+
+  ok(
+    await hasIndexedDB("https://example.org"),
+    "We have indexedDB data for example.org"
+  );
+  ok(
+    SiteDataTestUtils.hasServiceWorkers("https://example.org"),
+    "We have serviceWorker data for example.org"
+  );
+
+  ok(
+    await hasIndexedDB("https://example.com"),
+    "We have indexedDB data for example.com"
+  );
+  ok(
+    SiteDataTestUtils.hasServiceWorkers("https://example.com"),
+    "We have serviceWorker data for example.com"
+  );
+
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      // clear cookies and site data is not activated
+      ["network.cookie.lifetimePolicy", Ci.nsICookieService.ACCEPT_NORMALLY],
+      ["browser.sanitizer.loglevel", "All"],
+      ["privacy.clearOnShutdown.offlineApps", true],
+      ["privacy.sanitize.sanitizeOnShutdown", true],
+    ],
+  });
+  // Clear it
+  await Sanitizer.runSanitizeOnShutdown();
+  // Data for example.org should not have been cleared
+  ok(
+    await hasIndexedDB("https://example.org"),
+    "We still have indexedDB data for example.org"
+  );
+  ok(
+    SiteDataTestUtils.hasServiceWorkers("https://example.org"),
+    "We still have serviceWorker data for example.org"
+  );
+  // Data for example.com should be cleared
+  ok(
+    !(await hasIndexedDB("https://example.com")),
+    "We don't have indexedDB data for example.com"
+  );
+  ok(
+    !SiteDataTestUtils.hasServiceWorkers("https://example.com"),
+    "We don't have serviceWorker data for example.com"
+  );
 });
