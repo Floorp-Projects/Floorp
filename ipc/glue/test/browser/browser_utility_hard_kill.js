8dc1a247acd82eced1c21576c6c4ce4e80fae18a
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2022-02-11 20:48:57 +0000

    Bug 1754572 - Fix hard_kill test by waiting on process death r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D138482

--- ipc/glue/test/browser/browser_utility_hard_kill.js
+++ ipc/glue/test/browser/browser_utility_hard_kill.js
@@ -20,9 +20,25 @@ add_task(async () => {
 });
 
 add_task(async () => {
+  SimpleTest.expectChildProcessCrash();
+
+  const utilityProcessGone = TestUtils.topicObserved("ipc:utility-shutdown");
+
   info("Hard kill Utility Process");
   const ProcessTools = Cc["@mozilla.org/processtools-service;1"].getService(
     Ci.nsIProcessToolsService
   );
   ProcessTools.kill(utilityPid);
+
+  info(`Waiting for utility process ${utilityPid} to go away.`);
+  let [subject, data] = await utilityProcessGone;
+  ok(
+    subject instanceof Ci.nsIPropertyBag2,
+    "Subject needs to be a nsIPropertyBag2 to clean up properly"
+  );
+  is(
+    parseInt(data, 10),
+    utilityPid,
+    `Should match the crashed PID ${utilityPid} with ${data}`
+  );
 });
