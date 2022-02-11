013d79e7e6df4fdd82bdaa7cc93ed95b54fca8c6
Marian-Vasile Laza <mlaza@mozilla.com>
2022-02-11 17:30:03 +0200

    Backed out 4 changesets (bug 1754474) for causing bc test failures. CLOSED TREE
    Backed out changeset c6dd8b681aca (bug 1754474)
    Backed out changeset 596955256cc6 (bug 1754474)
    Backed out changeset fc692b3c27a2 (bug 1754474)
    Backed out changeset 59eb7951a5eb (bug 1754474)

--- toolkit/components/glean/build_scripts/glean_parser_ext/run_glean_parser.py
+++ toolkit/components/glean/build_scripts/glean_parser_ext/run_glean_parser.py
@@ -7,6 +7,7 @@
 import cpp
 import js
 import os
+import re
 import rust
 import sys
 
@@ -48,7 +49,11 @@ def get_parser_options(moz_app_version):
     app_version_major = moz_app_version.split(".", 1)[0]
     return {
         "allow_reserved": False,
-        "expire_by_version": int(app_version_major),
+        "custom_is_expired": lambda expires: expires == "expired"
+        or expires != "never"
+        and int(expires) <= int(app_version_major),
+        "custom_validate_expires": lambda expires: expires in ("expired", "never")
+        or re.fullmatch(r"\d\d+", expires, flags=re.ASCII),
     }
 
 
