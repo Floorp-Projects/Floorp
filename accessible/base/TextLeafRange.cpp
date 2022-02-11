7039008a5d2bbb3412472f5464fd555b0c64be3f
James Teh <jteh@mozilla.com>
2022-02-11 06:27:02 +0000

    Bug 1730085 part 1: Fix some bugs in TextLeafPoint::FindBoundary for BOUNDARY_CHAR. r=eeejay
    These bugs cause problems for the upcoming implementation of BOUNDARY_LINE_END.
    
    1. Don't always return no character at the caret. Only do that if the caret is at the end of the line. This was always the intent; this was just a silly mistake.
    2. When moving to the next character in an Accessible, don't move past the last character. This was a simple off-by-one error.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138102

--- accessible/base/TextLeafRange.cpp
+++ accessible/base/TextLeafRange.cpp
@@ -6,6 +6,7 @@
 
 #include "TextLeafRange.h"
 
+#include "HyperTextAccessible-inl.h"
 #include "mozilla/a11y/Accessible.h"
 #include "mozilla/a11y/DocAccessible.h"
 #include "mozilla/a11y/DocAccessibleParent.h"
@@ -735,8 +736,10 @@ TextLeafPoint TextLeafPoint::FindBoundary(AccessibleTextBoundary aBoundaryType,
                                           bool aIncludeOrigin) const {
   if (IsCaret()) {
     if (aBoundaryType == nsIAccessibleText::BOUNDARY_CHAR) {
-      // The caret is at the end of the line. Return no character.
-      return ActualizeCaret(/* aAdjustAtEndOfLine */ false);
+      if (IsCaretAtEndOfLine()) {
+        // The caret is at the end of the line. Return no character.
+        return ActualizeCaret(/* aAdjustAtEndOfLine */ false);
+      }
     }
     return ActualizeCaret().FindBoundary(aBoundaryType, aDirection,
                                          aIncludeOrigin);
@@ -766,7 +769,7 @@ TextLeafPoint TextLeafPoint::FindBoundary(AccessibleTextBoundary aBoundaryType,
             // We've moved to the next leaf. That means we've set the offset
             // to 0, so we're already at the next character.
             boundary = searchFrom;
-          } else if (searchFrom.mOffset <
+          } else if (searchFrom.mOffset + 1 <
                      static_cast<int32_t>(
                          nsAccUtils::TextLength(searchFrom.mAcc))) {
             boundary.mAcc = searchFrom.mAcc;
