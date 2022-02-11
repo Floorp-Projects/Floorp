b5ab57cfc41a77cc358cd020fca36877f4f098bc
Marian-Vasile Laza <mlaza@mozilla.com>
2022-02-11 17:30:03 +0200

    Backed out 4 changesets (bug 1754474) for causing bc test failures. CLOSED TREE
    Backed out changeset c6dd8b681aca (bug 1754474)
    Backed out changeset 596955256cc6 (bug 1754474)
    Backed out changeset fc692b3c27a2 (bug 1754474)
    Backed out changeset 59eb7951a5eb (bug 1754474)

--- third_party/python/glean_parser/glean_parser/data_review.py
+++ third_party/python/glean_parser/glean_parser/data_review.py
@@ -45,7 +45,6 @@ def generate(
     # I tried [\W\Z] but it complained. So `|` it is.
     reobj = re.compile(f"\\W{bug}\\W|\\W{bug}$")
     durations = set()
-    responsible_emails = set()
     metrics_table = ""
     for category_name, metrics in all_objects.value.items():
         for metric in metrics.values():
@@ -62,9 +61,6 @@ def generate(
 
             durations.add(metric.expires)
 
-            if metric.expires == "never":
-                responsible_emails.update(metric.notification_emails)
-
     if len(durations) == 1:
         duration = next(iter(durations))
         if duration == "never":
@@ -76,12 +72,8 @@ def generate(
         collection_duration += f"{durations}"
 
     if "never" in durations:
-        collection_duration += "\n" + ", ".join(responsible_emails) + " "
+        collection_duration += "\n**TODO: identify at least one individual here** "
         collection_duration += "will be responsible for the permanent collections."
-
-    if len(durations) == 0:
-        print(f"I'm sorry, I couldn't find metrics matching the bug number {bug}.")
-        return 1
 
     # This template is pulled from
     # https://github.com/mozilla/data-review/blob/main/request.md
