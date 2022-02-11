90eb44f5e1bf10ad87e69410bbad02f25bcb6e6d
Sarah Clements <sclements@mozilla.com>
2022-02-11 16:19:52 +0000

    Bug 1747430 - remove tabIndicatorReplacesIcon in browser tabs css file r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D137775

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -1997,6 +1997,9 @@
         // process so the browser can no longer be considered to be
         // crashed.
         tab.removeAttribute("crashed");
+        // we call updatetabIndicatorAttr here, rather than _tabAttrModified, so as
+        // to be consistent with how "crashed" attribute changes are handled elsewhere
+        this.tabContainer.updateTabIndicatorAttr(tab);
       } else {
         aBrowser.sendMessageToActor(
           "Browser:AppTab",
@@ -6208,6 +6211,7 @@
             // process so the browser can no longer be considered to be
             // crashed.
             tab.removeAttribute("crashed");
+            gBrowser.tabContainer.updateTabIndicatorAttr(tab);
           } else {
             browser.sendMessageToActor(
               "Browser:AppTab",
@@ -6491,6 +6495,7 @@
           delete this.mBrowser.initialPageLoadedFromUserAction;
           // If the browser is loading it must not be crashed anymore
           this.mTab.removeAttribute("crashed");
+          gBrowser.tabContainer.updateTabIndicatorAttr(this.mTab);
         }
 
         if (this._shouldShowProgress(aRequest)) {
