b4e4facb69198a2364fc1b00d4eade668eb072a2
Kershaw Chang <kershaw@mozilla.com>
2022-02-11 10:55:54 +0000

    Bug 1600309 - P4: Avoid creating data bridge channel after crashes, r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D134906

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -187,6 +187,7 @@
 #include "nsIMemoryInfoDumper.h"
 #include "nsIMemoryReporter.h"
 #include "nsIObserverService.h"
+#include "nsIOService.h"
 #include "nsIScriptError.h"
 #include "nsIScriptSecurityManager.h"
 #include "nsJSEnvironment.h"
@@ -2009,6 +2010,11 @@ mozilla::ipc::IPCResult ContentChild::RecvNetworkLinkTypeChange(
     obs->NotifyObservers(nullptr, "contentchild:network-link-type-changed",
                          nullptr);
   }
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult ContentChild::RecvSocketProcessCrashed() {
+  nsIOService::IncreaseSocketProcessCrashCount();
   return IPC_OK();
 }
 
