ef83403475daa9141d5b46ea90518f9cb28e9d76
Chun-Min Chang <chun.m.chang@gmail.com>
2022-02-11 17:19:39 +0000

    Bug 1751963 - Apply audio settings before starting audio r=padenot
    The audio processing configurations should be set before starting audio
    device and audio processing, which requires to know the channel-count
    requested from user and then initializes the audio processing settings
    based on the channel count.
    
    Depends on D136921
    
    Differential Revision: https://phabricator.services.mozilla.com/D136922

--- dom/media/webrtc/MediaEngineWebRTCAudio.cpp
+++ dom/media/webrtc/MediaEngineWebRTCAudio.cpp
@@ -418,6 +418,8 @@ nsresult MediaEngineWebRTCMicrophoneSource::Start() {
     return NS_ERROR_NOT_AVAILABLE;
   }
 
+  ApplySettings(mCurrentPrefs);
+
   NS_DispatchToMainThread(NS_NewRunnableFunction(
       __func__, [inputProcessing = mInputProcessing, deviceID, track = mTrack,
                  principal = mPrincipal] {
@@ -429,8 +431,6 @@ nsresult MediaEngineWebRTCMicrophoneSource::Start() {
             track, inputProcessing, StartStopMessage::Start));
         track->ConnectDeviceInput(deviceID, inputProcessing, principal);
       }));
-
-  ApplySettings(mCurrentPrefs);
 
   MOZ_ASSERT(mState != kReleased);
   mState = kStarted;
