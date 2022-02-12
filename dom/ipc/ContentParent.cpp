3419e722757943a8272d9fdfe4c5e2051ad886f3
Cristian Tuns <ctuns@mozilla.com>
2022-02-12 05:00:04 -0500

    Backed out 4 changesets (bug 1600309) for causing networking crashes CLOSED TREE
    Backed out changeset ae7282e60012 (bug 1600309)
    Backed out changeset 0d8b02a8db17 (bug 1600309)
    Backed out changeset c7262d966a28 (bug 1600309)
    Backed out changeset e5503a84beea (bug 1600309)

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -648,7 +648,6 @@ static const char* sObserverTopics[] = {
     "cookie-changed",
     "private-cookie-changed",
     NS_NETWORK_LINK_TYPE_TOPIC,
-    "network:socket-process-crashed",
 };
 
 // PreallocateProcess is called by the PreallocatedProcessManager.
@@ -3672,8 +3671,6 @@ ContentParent::Observe(nsISupports* aSubject, const char* aTopic,
     }
   } else if (!strcmp(aTopic, NS_NETWORK_LINK_TYPE_TOPIC)) {
     UpdateNetworkLinkType();
-  } else if (!strcmp(aTopic, "network:socket-process-crashed")) {
-    Unused << SendSocketProcessCrashed();
   }
 
   return NS_OK;
