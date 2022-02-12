3c1e2e8eb4dac4a91a8f6600e121cd6ef9550ed7
Cristian Tuns <ctuns@mozilla.com>
2022-02-12 04:04:52 -0500

    Backed out changeset 3b7f350adae9 (bug 1754662) for causing build bustages on nsLayoutUtils.cpp CLOSED TREE

--- dom/base/nsDOMWindowUtils.cpp
+++ dom/base/nsDOMWindowUtils.cpp
@@ -2378,8 +2378,11 @@ nsDOMWindowUtils::SendQueryContentEvent(uint32_t aType, int64_t aOffset,
 
   if (message == eQueryCharacterAtPoint) {
     // Looking for the widget at the point.
-    nsIFrame* popupFrame = nsLayoutUtils::GetPopupFrameForPoint(
-        presContext->GetRootPresContext(), widget, pt);
+    WidgetQueryContentEvent dummyEvent(true, eQueryContentState, widget);
+    dummyEvent.Init(options);
+    InitEvent(dummyEvent, &pt);
+    nsIFrame* popupFrame = nsLayoutUtils::GetPopupFrameForEventCoordinates(
+        presContext->GetRootPresContext(), &dummyEvent);
 
     LayoutDeviceIntRect widgetBounds = widget->GetClientBounds();
     widgetBounds.MoveTo(0, 0);
