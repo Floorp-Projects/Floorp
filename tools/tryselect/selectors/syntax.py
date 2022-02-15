2399972c85c6346fb6c18268a7022845df3da04d
Joel Maher <jmaher@mozilla.com>
2022-02-15 16:47:57 +0000

    Bug 1754613 - split a11y tests out of mochitest-browser-chrome into mochitest-browser-a11y. r=releng-reviewers,Jamie,gbrown
    here is a try push:
    https://treeherder.mozilla.org/jobs?repo=try&group_state=expanded&tier=1%2C2%2C3&revision=aca12d2de75f1fb0eb25432358e5518a8e7f812d
    
    most of the bc failures fall into:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1742167
    
    I only tested bc to see if there were issues removing the `ba` tests from there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138571

--- tools/tryselect/selectors/syntax.py
+++ tools/tryselect/selectors/syntax.py
@@ -291,6 +291,7 @@ class AutoTry:
         "mochitest": ["mochitest-1", "mochitest-e10s-1"],
         "xpcshell": ["xpcshell"],
         "chrome": ["mochitest-o"],
+        "browser-a11y": ["mochitest-ba"],
         "browser-chrome": [
             "mochitest-browser-chrome-1",
             "mochitest-e10s-browser-chrome-1",
@@ -312,6 +313,7 @@ class AutoTry:
         "xpcshell": "xpcshell",
         "chrome": "mochitest-o",
         "browser-chrome": "mochitest-bc",
+        "browser-a11y": "mochitest-ba",
         "devtools-chrome": "mochitest-dt",
         "crashtest": "crashtest",
         "reftest": "reftest",
@@ -369,6 +371,9 @@ class AutoTry:
                 flavor = t["flavor"]
                 if "subsuite" in t and t["subsuite"] == "devtools":
                     flavor = "devtools-chrome"
+
+                if "subsuite" in t and t["subsuite"] == "a11y":
+                    flavor = "browser-a11y"
 
                 if flavor in ["crashtest", "reftest"]:
                     manifest_relpath = os.path.relpath(t["manifest"], self.topsrcdir)
