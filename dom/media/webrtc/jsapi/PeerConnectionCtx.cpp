b3db926f36792b455818963c3c3293807a163815
Byron Campen <docfaraday@gmail.com>
2022-02-15 15:37:02 +0000

    Bug 1402997: Move PCMedia's functionality to PCImpl. r=mjf
    Differential Revision: https://phabricator.services.mozilla.com/D138543

--- dom/media/webrtc/jsapi/PeerConnectionCtx.cpp
+++ dom/media/webrtc/jsapi/PeerConnectionCtx.cpp
@@ -421,7 +421,7 @@ void PeerConnectionCtx::EverySecondTelemetryCallback_m(nsITimer* timer,
   MOZ_ASSERT(PeerConnectionCtx::isActive());
 
   for (auto& idAndPc : GetInstance()->mPeerConnections) {
-    if (idAndPc.second->HasMedia()) {
+    if (idAndPc.second->IsActive()) {
       idAndPc.second->GetStats(nullptr, true)
           ->Then(
               GetMainThreadSerialEventTarget(), __func__,
