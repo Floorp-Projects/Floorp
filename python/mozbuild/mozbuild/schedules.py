09d91cc78e1c7d09131cd8df8fa4faa2b11c33ac
Joel Maher <jmaher@mozilla.com>
2022-02-15 16:47:57 +0000

    Bug 1754613 - split a11y tests out of mochitest-browser-chrome into mochitest-browser-a11y. r=releng-reviewers,Jamie,gbrown
    here is a try push:
    https://treeherder.mozilla.org/jobs?repo=try&group_state=expanded&tier=1%2C2%2C3&revision=aca12d2de75f1fb0eb25432358e5518a8e7f812d
    
    most of the bc failures fall into:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1742167
    
    I only tested bc to see if there were issues removing the `ba` tests from there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138571

--- python/mozbuild/mozbuild/schedules.py
+++ python/mozbuild/mozbuild/schedules.py
@@ -64,6 +64,7 @@ EXCLUSIVE_COMPONENTS = [
     # specific test suites
     "crashtest",
     "mochitest-a11y",
+    "mochitest-browser-a11y",
     "mochitest-browser-chrome",
     "mochitest-chrome",
     "mochitest-plain",
