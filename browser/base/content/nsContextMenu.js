881bb9214e2c6589c3c6788a6b9732418c5314d2
Emilio Cobos Álvarez <emilio@crisal.io>
2022-02-15 22:25:04 +0000

    Bug 1753836 - Fix context menus to deal with Event.screen* returning page CSS coordinates. r=Gijs
    If the page is zoomed, its devicePixelRatio will differ from the browser
    chrome's. Account for this by converting to device pixels before starting to
    scroll.
    
    Depends on D138036
    
    Differential Revision: https://phabricator.services.mozilla.com/D138037

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -71,15 +71,17 @@ function openContextMenu(aMessage, aBrowser, aActor) {
   // We don't have access to the original event here, as that happened in
   // another process. Therefore we synthesize a new MouseEvent to propagate the
   // inputSource to the subsequently triggered popupshowing event.
-  var newEvent = document.createEvent("MouseEvent");
+  let newEvent = document.createEvent("MouseEvent");
+  let screenX = context.screenXDevPx / window.devicePixelRatio;
+  let screenY = context.screenYDevPx / window.devicePixelRatio;
   newEvent.initNSMouseEvent(
     "contextmenu",
     true,
     true,
     null,
     0,
-    context.screenX,
-    context.screenY,
+    screenX,
+    screenY,
     0,
     0,
     false,
