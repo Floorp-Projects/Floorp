0f838c8e54b66a8a1f57d24d6bf78ec776d745e3
Byron Campen <docfaraday@gmail.com>
2022-02-15 15:37:02 +0000

    Bug 1402997: Some simplifications. r=mjf
    Depends on D138543
    
    Differential Revision: https://phabricator.services.mozilla.com/D138544

--- dom/media/webrtc/jsapi/WebrtcGlobalInformation.cpp
+++ dom/media/webrtc/jsapi/WebrtcGlobalInformation.cpp
@@ -95,7 +95,7 @@ GetStatsPromiseForThisProcess(const nsAString& aPcIdFilter) {
           !aPcIdFilter.EqualsASCII(aPc->GetIdAsAscii().c_str())) {
         return;
       }
-      if (!aPc->IsActive()) {
+      if (aPc->IsClosed()) {
         return;
       }
       promises.AppendElement(aPc->GetStats(nullptr, true));
