fca92b65eb0d537feef2f5fed3bc5b55154cf096
Kershaw Chang <kershaw@mozilla.com>
2022-02-15 19:18:11 +0000

    Bug 1754508 - P1: Make PNecko refcounted, r=necko-reviewers,dragana
    This is the rebased version of D138486.
    This patch is supposed to be landed on nightly and D138486 is for beta.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138755

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -115,7 +115,6 @@
 #include "mozilla/net/CookieServiceChild.h"
 #include "mozilla/net/DocumentChannelChild.h"
 #include "mozilla/net/HttpChannelChild.h"
-#include "mozilla/net/NeckoChild.h"
 #include "mozilla/widget/RemoteLookAndFeel.h"
 #include "mozilla/widget/ScreenManager.h"
 #include "mozilla/widget/WidgetMessageUtils.h"
@@ -1994,8 +1993,6 @@ mozilla::ipc::IPCResult ContentChild::RecvPScriptCacheConstructor(
   return IPC_OK();
 }
 
-PNeckoChild* ContentChild::AllocPNeckoChild() { return new NeckoChild(); }
-
 mozilla::ipc::IPCResult ContentChild::RecvNetworkLinkTypeChange(
     const uint32_t& aType) {
   mNetworkLinkType = aType;
@@ -2005,11 +2002,6 @@ mozilla::ipc::IPCResult ContentChild::RecvNetworkLinkTypeChange(
                          nullptr);
   }
   return IPC_OK();
-}
-
-bool ContentChild::DeallocPNeckoChild(PNeckoChild* necko) {
-  delete necko;
-  return true;
 }
 
 PPrintingChild* ContentChild::AllocPPrintingChild() {
