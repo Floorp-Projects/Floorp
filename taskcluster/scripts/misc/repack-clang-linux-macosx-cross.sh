736a38fc043fc6968da2bdf223af78a4c3c2ec9b
Mike Hommey <mh+mozilla@glandium.org>
2022-02-16 08:24:39 +0000

    Bug 1755415 - Encode the target in the compiler-rt artifact/directory. r=firefox-build-system-reviewers,mhentges
    This simplifies the setup for cross clang repacks.
    
    Also use `ninja install` to get all the relevant things in the artifact.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138745

--- taskcluster/scripts/misc/repack-clang-linux-macosx-cross.sh
+++ taskcluster/scripts/misc/repack-clang-linux-macosx-cross.sh
@@ -7,20 +7,20 @@ set -x -e -v
 cd $MOZ_FETCHES_DIR
 
 # We have a native linux64 toolchain in $MOZ_FETCHES_DIR/clang
-# We have a native aarch64 macos compiler-rt in $MOZ_FETCHES_DIR/aarch64/compiler-rt
-# We have a native x86_64 macos compiler-rt in $MOZ_FETCHES_DIR/x86_64/compiler-rt
+# We have a native aarch64 macos compiler-rt in $MOZ_FETCHES_DIR/compiler-rt-aarch64-apple-darwin
+# We have a native x86_64 macos compiler-rt in $MOZ_FETCHES_DIR/compiler-rt-x86_64-apple-darwin
 clang_lib=$(echo clang/lib/clang/*/lib)
 mkdir -p $clang_lib/darwin
-find {aarch64,x86_64}/compiler-rt/lib/darwin -type f -printf '%f\n' | sort -u | while read f; do
-  f=compiler-rt/lib/darwin/$f
-  if [ -f aarch64/$f -a -f x86_64/$f ]; then
+find compiler-rt-*/lib/darwin -type f -printf '%f\n' | sort -u | while read f; do
+  f=lib/darwin/$f
+  if [ -f compiler-rt-aarch64-apple-darwin/$f -a -f compiler-rt-x86_64-apple-darwin/$f ]; then
     # For compiler-rt files that exist on both ends, merge them
-    $MOZ_FETCHES_DIR/cctools/bin/lipo -create {aarch64,x86_64}/$f -output $clang_lib/darwin/${f##*/}
-  elif [ -f aarch64/$f ]; then
+    $MOZ_FETCHES_DIR/cctools/bin/lipo -create compiler-rt-{aarch64,x86_64}-apple-darwin/$f -output $clang_lib/darwin/${f##*/}
+  elif [ -f compiler-rt-aarch64-apple-darwin/$f ]; then
     # For compiler-rt files that exist on either end, copy the existing one
-    cp aarch64/$f $clang_lib/darwin
+    cp compiler-rt-aarch64-apple-darwin/$f $clang_lib/darwin
   else
-    cp x86_64/$f $clang_lib/darwin
+    cp compiler-rt-x86_64-apple-darwin/$f $clang_lib/darwin
   fi
 done
 
