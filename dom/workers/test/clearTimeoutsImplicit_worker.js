ea1d009c3f5e4f896695b7ce8e315302a78992e6
Jens Stutte <jstutte@mozilla.com>
2022-02-16 15:48:43 +0000

    Bug 1752120: Null out the mWorkerPrivate on WorkerGlobalScopeBase when a worker ends. r=dom-worker-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D138442

---
+++ dom/workers/test/clearTimeoutsImplicit_worker.js
@@ -0,0 +1,11 @@
+var count = 0;
+function timerFunction() {
+  if (++count == 30) {
+    postMessage("ready");
+    while (true) {}
+  }
+}
+
+for (var i = 0; i < 10; i++) {
+  setInterval(timerFunction, 500);
+}
