c9be3ed524726e2983bda6611dbac0330482bbec
Julien Cristau <jcristau@mozilla.com>
2022-02-16 10:28:45 +0100

    [focus] Override versionName for shipping builds
    https://github.com/mozilla-mobile/focus-android/pull/6399 makes us pass
    the versionName to gradlew for beta/release builds, but we need to tell
    gradle to use it and override the default.
    
    Code pretty much stolen from fenix.

---
+++ mobile/android/focus-android/buildSrc/src/main/java/Config.kt
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import org.gradle.api.Project
+
+object Config {
+    @JvmStatic
+    fun releaseVersionName(project: Project): String {
+        // Note: release builds must have the `versionName` set. However, the gradle ecosystem makes this hard to
+        // ergonomically validate (sometimes IDEs default to a release variant and mysteriously fail due to the
+        // validation, sometimes devs just need a release build and specifying project properties is annoying in IDEs),
+        // so instead we'll allow the `versionName` to silently default to an empty string.
+        return if (project.hasProperty("versionName")) project.property("versionName") as String else ""
+    }
+
+    @JvmStatic
+    fun nightlyVersionName(): String {
+        // Nightly versions use the Gecko/A-C major version and append "0.a1", e.g. with A-C 90.0.20210426143115
+        // the Nightly version will be 90.0a1
+        val majorVersion = AndroidComponents.VERSION.split(".")[0]
+        return "$majorVersion.0a1"
+    }
+}
