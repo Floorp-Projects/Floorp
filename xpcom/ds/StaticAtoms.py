7f78f5cd75ed691599683668ddd72a0505feb3d2
Timothy Nikkel <tnikkel@gmail.com>
2022-02-16 20:20:49 +0000

    Bug 1755290. Don't descend into the chrome tabbar when looking for an async scrollable element to create a display port on. r=botond,emilio
    A basic Firefox window has no element that WantAsyncScroll in the parent process, even if there are enough tabs to overflow the tabbar and make it scroll. This is because the underlying element is overflow hidden.
    
    The tabbar is an arrowscrollbox
    
    https://searchfox.org/mozilla-central/rev/e66593593f3b356901011ea0fcdf9979728e9ae8/browser/base/content/navigator-toolbox.inc.xhtml#56
    
    Which is implemented here
    
    https://searchfox.org/mozilla-central/rev/e66593593f3b356901011ea0fcdf9979728e9ae8/toolkit/content/widgets/arrowscrollbox.js#33
    
    using a <scrollbox> element. These are meant to be programtically scrolled but not user scrolled, hence they are declared overflow hidden here
    
    https://searchfox.org/mozilla-central/rev/e66593593f3b356901011ea0fcdf9979728e9ae8/toolkit/content/xul.css#540
    
    and they have been that way since the early 2000s, so doesn' seem likely to change. overflow: hidden element do not WantAsyncScroll
    
    https://searchfox.org/mozilla-central/rev/e66593593f3b356901011ea0fcdf9979728e9ae8/layout/generic/nsGfxScrollFrame.cpp#1691
    
    If the tabbar structure/name changes I left a comment there pointing out this code.
    
    In a session with a lot of tabs this code can be 15% of the total parent process time to open a new tab. With this patch it's pretty much zero.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138650

--- xpcom/ds/StaticAtoms.py
+++ xpcom/ds/StaticAtoms.py
@@ -1869,6 +1869,7 @@ STATIC_ATOMS = [
     Atom("countNonEmpty", "count-non-empty"),
     Atom("daysFromDate", "days-from-date"),
     Atom("secondsFromDateTime", "seconds-from-dateTime"),
+    Atom("tabbrowser_arrowscrollbox", "tabbrowser-arrowscrollbox"),
     # Simple gestures support
     Atom("onMozSwipeGestureMayStart", "onMozSwipeGestureMayStart"),
     Atom("onMozSwipeGestureStart", "onMozSwipeGestureStart"),
