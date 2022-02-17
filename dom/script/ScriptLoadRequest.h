6b6405cceceead857a8b3ed209af289444620d79
Yulia Startsev <ystartsev@mozilla.com>
2022-02-17 15:57:51 +0000

    Bug 1742437 - move DOM script context into own file and rename it; r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D138132

--- dom/script/ScriptLoadRequest.h
+++ dom/script/ScriptLoadRequest.h
@@ -28,6 +28,7 @@
 #include "nsIGlobalObject.h"
 #include "nsIScriptElement.h"
 #include "ScriptKind.h"
+#include "ScriptLoadContext.h"
 
 class nsICacheInfoChannel;
 
@@ -41,7 +42,7 @@ namespace dom {
 class Element;
 class ModuleLoadRequest;
 class ScriptLoadRequestList;
-class DOMScriptLoadContext;
+class ScriptLoadContext;
 
 /*
  * ScriptFetchOptions loosely corresponds to HTML's "script fetch options",
@@ -111,7 +112,7 @@ class ScriptFetchOptions {
  * ScriptLoadRequest and ScriptLoadContexts have a circular pointer.  A
  * ScriptLoadContext augments the loading of a ScriptLoadRequest by providing
  * additional information regarding the loading and evaluation behavior (see
- * the DOMScriptLoadContext class for details).  In terms of responsibility,
+ * the ScriptLoadContext class for details).  In terms of responsibility,
  * the ScriptLoadRequest represents "What" is being loaded, and the
  * ScriptLoadContext represents "How".
  *
@@ -139,7 +140,7 @@ class ScriptLoadRequest
   ScriptLoadRequest(ScriptKind aKind, nsIURI* aURI,
                     ScriptFetchOptions* aFetchOptions,
                     const SRIMetadata& aIntegrity, nsIURI* aReferrer,
-                    DOMScriptLoadContext* aContext);
+                    ScriptLoadContext* aContext);
 
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(ScriptLoadRequest)
@@ -265,7 +266,7 @@ class ScriptLoadRequest
 
   bool HasLoadContext() { return mLoadContext; }
 
-  DOMScriptLoadContext* GetLoadContext() {
+  ScriptLoadContext* GetLoadContext() {
     MOZ_ASSERT(mLoadContext);
     return mLoadContext;
   }
@@ -312,177 +313,7 @@ class ScriptLoadRequest
 
   // ScriptLoadContext for augmenting the load depending on the loading
   // context (DOM, Worker, etc.)
-  RefPtr<DOMScriptLoadContext> mLoadContext;
-};
-
-/*
- * DOM specific ScriptLoadContext.
- *
- * ScriptLoadContexts augment the loading of a ScriptLoadRequest. They
- * describe how a ScriptLoadRequests loading and evaluation needs to be
- * augmented, based on the information provided by the loading context. In
- * the case of the DOM, the ScriptLoadContext is used to identify how a script
- * should be loaded according to information found in the HTML document into
- * which it will be loaded. The following fields describe how the
- * ScriptLoadRequest will be loaded.
- *
- *    * mScriptMode
- *        stores the mode (Async, Sync, Deferred), and preload, which
- *        allows the ScriptLoader to decide if the script should be pushed
- *        offThread, or if the preloaded request should be used.
- *    * mScriptFromHead
- *        Set when the script tag is in the head, and should be treated as
- *        a blocking script
- *    * mIsInline
- *        Set for scripts whose bodies are inline in the html. In this case,
- *        the script does not need to be fetched first.
- *    * mIsXSLT
- *        Set if we are in an XSLT request.
- *    * TODO: mIsPreload (will be moved from ScriptFetchOptions)
- *        Set for scripts that are preloaded in a
- *        <link rel="preload" as="script"> element.
- *
- * In addition to describing how the ScriptLoadRequest will be loaded by the
- * DOM ScriptLoader, the ScriptLoadContext contains fields that facilitate
- * those custom behaviors, including support for offthread parsing, pointers
- * to runnables (for cancellation and cleanup if a script is parsed offthread)
- * and preload element specific controls.
- *
- */
-
-class DOMScriptLoadContext : public PreloaderBase {
- protected:
-  virtual ~DOMScriptLoadContext();
-
- public:
-  explicit DOMScriptLoadContext(Element* aElement,
-                                nsIGlobalObject* aWebExtGlobal = nullptr);
-
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
-  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(DOMScriptLoadContext)
-
-  void SetRequest(ScriptLoadRequest* aRequest);
-
-  // PreloaderBase
-  static void PrioritizeAsPreload(nsIChannel* aChannel);
-  virtual void PrioritizeAsPreload() override;
-
-  bool IsPreload() const;
-
-  // This will return nullptr in most cases,
-  // unless this is a module being imported by a WebExtension content script.
-  // In that case it's the Sandbox global executing that code.
-  nsIGlobalObject* GetWebExtGlobal() const;
-
-  bool CompileStarted() const {
-    return mRequest->InCompilingStage() ||
-           (mRequest->IsReadyToRun() && mWasCompiledOMT);
-  }
-
-  JS::OffThreadToken** OffThreadTokenPtr() {
-    return mOffThreadToken ? &mOffThreadToken : nullptr;
-  }
-
-  bool IsTracking() const { return mIsTracking; }
-  void SetIsTracking() {
-    MOZ_ASSERT(!mIsTracking);
-    mIsTracking = true;
-  }
-
-  void BlockOnload(Document* aDocument);
-
-  void MaybeUnblockOnload();
-
-  enum class ScriptMode : uint8_t {
-    eBlocking,
-    eDeferred,
-    eAsync,
-    eLinkPreload  // this is a load initiated by <link rel="preload"
-                  // as="script"> tag
-  };
-
-  void SetScriptMode(bool aDeferAttr, bool aAsyncAttr, bool aLinkPreload);
-
-  bool IsLinkPreloadScript() const {
-    return mScriptMode == ScriptMode::eLinkPreload;
-  }
-
-  bool IsBlockingScript() const { return mScriptMode == ScriptMode::eBlocking; }
-
-  bool IsDeferredScript() const { return mScriptMode == ScriptMode::eDeferred; }
-
-  bool IsAsyncScript() const { return mScriptMode == ScriptMode::eAsync; }
-
-  nsIScriptElement* GetScriptElement() const;
-
-  // Make this request a preload (speculative) request.
-  void SetIsPreloadRequest() {
-    MOZ_ASSERT(!GetScriptElement());
-    MOZ_ASSERT(!IsPreload());
-    mIsPreload = true;
-  }
-
-  // Make a preload request into an actual load request for the given element.
-  void SetIsLoadRequest(nsIScriptElement* aElement);
-
-  FromParser GetParserCreated() const {
-    nsIScriptElement* element = GetScriptElement();
-    if (!element) {
-      return NOT_FROM_PARSER;
-    }
-    return element->GetParserCreated();
-  }
-
-  // Used to output a string for the Gecko Profiler.
-  void GetProfilerLabel(nsACString& aOutString);
-
-  void MaybeCancelOffThreadScript();
-
-  TimeStamp mOffThreadParseStartTime;
-  TimeStamp mOffThreadParseStopTime;
-
-  ScriptMode mScriptMode;  // Whether this is a blocking, defer or async script.
-  bool mScriptFromHead;    // Synchronous head script block loading of other non
-                           // js/css content.
-  bool mIsInline;          // Is the script inline or loaded?
-  bool mInDeferList;       // True if we live in mDeferRequests.
-  bool mInAsyncList;       // True if we live in mLoadingAsyncRequests or
-                           // mLoadedAsyncRequests.
-  bool mIsNonAsyncScriptInserted;  // True if we live in
-                                   // mNonAsyncExternalScriptInsertedRequests
-  bool mIsXSLT;                    // True if we live in mXSLTRequests.
-  bool mInCompilingList;  // True if we are in mOffThreadCompilingRequests.
-  bool mIsTracking;       // True if the script comes from a source on our
-                          // tracking protection list.
-  bool mWasCompiledOMT;   // True if the script has been compiled off main
-                          // thread.
-
-  JS::OffThreadToken* mOffThreadToken;  // Off-thread parsing token.
-
-  Atomic<Runnable*> mRunnable;  // Runnable created when dispatching off thread
-                                // compile. Tracked here so that it can be
-                                // properly released during cancellation.
-
-  int32_t mLineNo;
-
-  // Set on scripts and top level modules.
-  bool mIsPreload;
-  nsCOMPtr<Element> mElement;
-
-  /* The Web Extension global -- Only used on DOM Modules.
-   *     Specifies a SandBox global with which to associate and run this script.
-   *     Propagated throughout the module tree if present.
-   */
-  nsCOMPtr<nsIGlobalObject> mWebExtGlobal;
-
-  RefPtr<ScriptLoadRequest> mRequest;
-
-  // Non-null if there is a document that this request is blocking from loading.
-  RefPtr<Document> mLoadBlockedDocument;
-
-  // For preload requests, we defer reporting errors to the console until the
-  // request is used.
-  nsresult mUnreportedPreloadError;
+  RefPtr<ScriptLoadContext> mLoadContext;
 };
 
 class ScriptLoadRequestList : private mozilla::LinkedList<ScriptLoadRequest> {
