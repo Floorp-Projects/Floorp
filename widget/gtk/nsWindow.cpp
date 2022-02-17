bfa09bee484faaa09132534cb55c3362fe9ba27a
Emilio Cobos Álvarez <emilio@crisal.io>
2022-02-17 22:19:40 +0000

    Bug 1755706 - Fix SetDrawsInTitlebar for PiP windows. r=stransky
    The previous code was not being called because it was only being called
    when redundant. The more complex SetDrawsInTitlebar code causes size
    changes which mess up PiP window positioning and restoration.
    
    The PiP and window positioning code had other bugs, but with those fixed I get
    perfect PiP on X11, which is nice.
    
    Depends on D138914
    
    Differential Revision: https://phabricator.services.mozilla.com/D138915

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -8485,15 +8485,15 @@ void nsWindow::SetDrawsInTitlebar(bool aState) {
   LOG("nsWindow::SetDrawsInTitlebar() State %d mGtkWindowDecoration %d\n",
       aState, (int)mGtkWindowDecoration);
 
-  if (mIsPIPWindow && aState == mDrawInTitlebar) {
-    gtk_window_set_decorated(GTK_WINDOW(mShell), !aState);
-    LOG("  set decoration for PIP %d", aState);
-    return;
-  }
-
   if (mGtkWindowDecoration == GTK_DECORATION_NONE ||
       aState == mDrawInTitlebar) {
     LOG("  already set, quit");
+    return;
+  }
+
+  if (mIsPIPWindow) {
+    gtk_window_set_decorated(GTK_WINDOW(mShell), !aState);
+    LOG("  set decoration for PIP %d", aState);
     return;
   }
 
