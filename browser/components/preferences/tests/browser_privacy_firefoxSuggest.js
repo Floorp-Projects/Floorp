ce7c4cb8fbb23d2aa61a4c5d7f7eeeebbc2b1fc7
Drew Willcoxon <adw@mozilla.com>
2022-02-17 01:52:26 +0000

    Bug 1755057 - Part 2: Don't show best match in the preferences UI if the best match feature is disabled. r=daisuke,Gijs,preferences-reviewers
    This is based on part 1 in D138583. It hides the best match toggle switch when
    the best match feature is disabled. The best match feature is a sub-feature of
    Firefox Suggest, so the toggle is also hidden if Suggest is disabled, along with
    all the other Suggest UI.
    
    Depends on D138583
    
    Differential Revision: https://phabricator.services.mozilla.com/D138584

--- browser/components/preferences/tests/browser_privacy_firefoxSuggest.js
+++ browser/components/preferences/tests/browser_privacy_firefoxSuggest.js
@@ -20,6 +20,7 @@ XPCOMUtils.defineLazyGetter(this, "QuickSuggestTestUtils", () => {
 });
 
 const CONTAINER_ID = "firefoxSuggestContainer";
+const BEST_MATCH_OPTION_BOX_ID = "firefoxSuggestBestMatchOptionBox";
 const BEST_MATCH_CHECKBOX_ID = "firefoxSuggestBestMatchToggle";
 const NONSPONSORED_CHECKBOX_ID = "firefoxSuggestNonsponsoredToggle";
 const SPONSORED_CHECKBOX_ID = "firefoxSuggestSponsoredToggle";
@@ -499,8 +500,133 @@ add_task(async function clickLearnMore() {
   await SpecialPowers.popPrefEnv();
 });
 
+// Tests the visibility of the best match toggle when the best match feature is
+// initially disabled and is then enabled via preferences.
+add_task(async function bestMatchVisibility_falseToTrue() {
+  await doBestMatchVisibilityTest(false, true);
+});
+
+// Tests the visibility of the best match toggle when the best match feature is
+// initially enabled and is then disabled via preferences.
+add_task(async function bestMatchVisibility_trueToFalse() {
+  await doBestMatchVisibilityTest(true, false);
+});
+
+/**
+ * Runs a test that checks the visibility of the Firefox Suggest best match
+ * toggle. It sets the best match feature pref, opens about:preferences and
+ * checks the visibility of the toggle, sets the feature pref again, and checks
+ * the visibility of the toggle again.
+ *
+ * @param {boolean} initialEnabled
+ *   Whether to enable the best match feature before about:preferences is
+ *   opened.
+ * @param {boolean} newEnabled
+ *   Whether to enable the best match feature after about:preferences is
+ *   opened.
+ */
+async function doBestMatchVisibilityTest(initialEnabled, newEnabled) {
+  info(
+    "Running best match visibility test: " +
+      JSON.stringify({ initialEnabled, newEnabled })
+  );
+
+  // Set the initial enabled status.
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.urlbar.bestMatch.enabled", initialEnabled]],
+  });
+
+  // Open prefs and check the initial visibility.
+  await openPreferencesViaOpenPreferencesAPI("privacy", { leaveOpen: true });
+
+  let doc = gBrowser.selectedBrowser.contentDocument;
+  let container = doc.getElementById(BEST_MATCH_OPTION_BOX_ID);
+  Assert.equal(
+    BrowserTestUtils.is_visible(container),
+    initialEnabled,
+    "The option box has the expected initial visibility"
+  );
+
+  // Set the new enabled status.
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.urlbar.bestMatch.enabled", newEnabled]],
+  });
+
+  // Check visibility again.
+  Assert.equal(
+    BrowserTestUtils.is_visible(container),
+    newEnabled,
+    "The option box has the expected visibility after setting enabled status"
+  );
+
+  // Clean up.
+  gBrowser.removeCurrentTab();
+  await SpecialPowers.popPrefEnv();
+  await SpecialPowers.popPrefEnv();
+}
+
+// Tests the visibility of the best match toggle when the best match feature is
+// enabled via a Nimbus experiment before about:preferences is opened.
+add_task(async function bestMatchVisibility_experiment_beforeOpen() {
+  await QuickSuggestTestUtils.withExperiment({
+    valueOverrides: {
+      bestMatchEnabled: true,
+    },
+    callback: async () => {
+      await openPreferencesViaOpenPreferencesAPI("privacy", {
+        leaveOpen: true,
+      });
+      let doc = gBrowser.selectedBrowser.contentDocument;
+      let container = doc.getElementById(BEST_MATCH_OPTION_BOX_ID);
+      Assert.ok(
+        BrowserTestUtils.is_visible(container),
+        "The option box is visible"
+      );
+      gBrowser.removeCurrentTab();
+    },
+  });
+});
+
+// Tests the visibility of the best match toggle when the best match feature is
+// enabled via a Nimbus experiment after about:preferences is opened.
+add_task(async function bestMatchVisibility_experiment_afterOpen() {
+  // Open prefs and check the initial visibility.
+  await openPreferencesViaOpenPreferencesAPI("privacy", { leaveOpen: true });
+  let doc = gBrowser.selectedBrowser.contentDocument;
+  let container = doc.getElementById(BEST_MATCH_OPTION_BOX_ID);
+  Assert.ok(
+    BrowserTestUtils.is_hidden(container),
+    "The option box is hidden initially"
+  );
+
+  // Install an experiment with best match enabled.
+  await QuickSuggestTestUtils.withExperiment({
+    valueOverrides: {
+      bestMatchEnabled: true,
+    },
+    callback: () => {
+      Assert.ok(
+        BrowserTestUtils.is_visible(container),
+        "The option box is visible after installing the experiment"
+      );
+    },
+  });
+
+  Assert.ok(
+    BrowserTestUtils.is_hidden(container),
+    "The option box is hidden again after the experiment is uninstalled"
+  );
+
+  gBrowser.removeCurrentTab();
+});
+
 // Check the pref and the checkbox for best match.
-add_task(async function bestMatch() {
+add_task(async function bestMatchToggle() {
+  // Enable the feature so that the toggle appears.
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.urlbar.bestMatch.enabled", true]],
+  });
+
   await openPreferencesViaOpenPreferencesAPI("privacy", { leaveOpen: true });
   const doc = gBrowser.selectedBrowser.contentDocument;
   const checkbox = doc.getElementById(BEST_MATCH_CHECKBOX_ID);
@@ -509,7 +635,7 @@ add_task(async function bestMatch() {
   info("Check if the checkbox stauts reflects the pref value");
   for (const isEnabled of [true, false]) {
     await SpecialPowers.pushPrefEnv({
-      set: [["browser.urlbar.bestMatch.enabled", isEnabled]],
+      set: [["browser.urlbar.suggest.bestmatch", isEnabled]],
     });
     assertCheckboxes({ [BEST_MATCH_CHECKBOX_ID]: isEnabled });
     await SpecialPowers.popPrefEnv();
@@ -525,14 +651,15 @@ add_task(async function bestMatch() {
     );
     Assert.ok(initialValue !== checkbox.checked);
     Assert.equal(
-      Services.prefs.getBoolPref("browser.urlbar.bestMatch.enabled"),
+      Services.prefs.getBoolPref("browser.urlbar.suggest.bestmatch"),
       checkbox.checked
     );
   }
 
   // Clean up.
-  Services.prefs.clearUserPref("browser.urlbar.bestMatch.enabled");
+  Services.prefs.clearUserPref("browser.urlbar.suggest.bestmatch");
   gBrowser.removeCurrentTab();
+  await SpecialPowers.popPrefEnv();
 });
 
 /**
