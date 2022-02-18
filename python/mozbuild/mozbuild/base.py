5c0368fef2691d616a96311bdde20ae766c03249
Mitchell Hentges <mhentges@mozilla.com>
2022-02-18 14:18:26 +0000

    Bug 1755530: Remove `python_executable_version()` r=glandium
    * The python2/python3 lookup logic is obsolete, we can always depend on
      `sys.executable` since Mach starts with a Python version check
    * `MozbuildObject`'s `python3` property was unused, which means that all
      of this removed code was dead anyways.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138824

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -38,7 +38,6 @@ from .mozconfig import (
     MozconfigLoadException,
     MozconfigLoader,
 )
-from .pythonutil import find_python3_executable
 from .util import (
     memoize,
     memoized_property,
@@ -546,25 +545,6 @@ class MozbuildObject(ProcessExecutionMixin):
             )
 
         return BuildReader(config, finder=finder)
-
-    @memoized_property
-    def python3(self):
-        """Obtain info about a Python 3 executable.
-
-        Returns a tuple of an executable path and its version (as a tuple).
-        Either both entries will have a value or both will be None.
-        """
-        # Search configured build info first. Then fall back to system.
-        try:
-            subst = self.substs
-
-            if "PYTHON3" in subst:
-                version = tuple(map(int, subst["PYTHON3_VERSION"].split(".")))
-                return subst["PYTHON3"], version
-        except BuildEnvironmentNotFoundException:
-            pass
-
-        return find_python3_executable()
 
     def is_clobber_needed(self):
         if not os.path.exists(self.topobjdir):
