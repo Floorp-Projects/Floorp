be3ad3d1b54795b0eead470cb0ef54ec3f379ba0
James Teh <jteh@mozilla.com>
2022-02-18 02:19:11 +0000

    Bug 1755604: Add Accessible::IsBefore/IsAncestorOf. r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D138743

--- accessible/base/TextLeafRange.cpp
+++ accessible/base/TextLeafRange.cpp
@@ -375,39 +375,7 @@ bool TextLeafPoint::operator<(const TextLeafPoint& aPoint) const {
   if (mAcc == aPoint.mAcc) {
     return mOffset < aPoint.mOffset;
   }
-
-  // Build the chain of parents.
-  Accessible* thisP = mAcc;
-  Accessible* otherP = aPoint.mAcc;
-  AutoTArray<Accessible*, 30> thisParents, otherParents;
-  do {
-    thisParents.AppendElement(thisP);
-    thisP = thisP->Parent();
-  } while (thisP);
-  do {
-    otherParents.AppendElement(otherP);
-    otherP = otherP->Parent();
-  } while (otherP);
-
-  // Find where the parent chain differs.
-  uint32_t thisPos = thisParents.Length(), otherPos = otherParents.Length();
-  for (uint32_t len = std::min(thisPos, otherPos); len > 0; --len) {
-    Accessible* thisChild = thisParents.ElementAt(--thisPos);
-    Accessible* otherChild = otherParents.ElementAt(--otherPos);
-    if (thisChild != otherChild) {
-      return thisChild->IndexInParent() < otherChild->IndexInParent();
-    }
-  }
-
-  // If the ancestries are the same length (both thisPos and otherPos are 0),
-  // we should have returned by now.
-  MOZ_ASSERT(thisPos != 0 || otherPos != 0);
-  // At this point, one of the ancestries is a superset of the other, so one of
-  // thisPos or otherPos should be 0.
-  MOZ_ASSERT(thisPos != otherPos);
-  // If the other Accessible is deeper than this one (otherPos > 0), this
-  // Accessible comes before the other.
-  return otherPos > 0;
+  return mAcc->IsBefore(aPoint.mAcc);
 }
 
 bool TextLeafPoint::IsEmptyLastLine() const {
