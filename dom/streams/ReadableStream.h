1c1d1b551ef50f589ba14af7544939486c830595
Iulian Moraru <imoraru@mozilla.com>
2022-02-18 02:51:13 +0200

    Backed out changeset 21d7dfa0dfee (bug 1734241) for causing bp-hybrid bustages on ReadableStreamPipeTo.cpp. CLOSED TREE

--- dom/streams/ReadableStream.h
+++ dom/streams/ReadableStream.h
@@ -14,7 +14,6 @@
 #include "mozilla/dom/BindingDeclarations.h"
 #include "mozilla/dom/QueuingStrategyBinding.h"
 #include "mozilla/dom/ReadableStreamController.h"
-#include "mozilla/dom/ReadableStreamBinding.h"
 #include "mozilla/dom/ReadableStreamDefaultController.h"
 #include "nsCycleCollectionParticipant.h"
 #include "nsWrapperCache.h"
@@ -32,7 +31,6 @@ class ReadableStreamDefaultReader;
 class ReadableStreamGenericReader;
 struct ReadableStreamGetReaderOptions;
 struct ReadIntoRequest;
-class WritableStream;
 
 using ReadableStreamReader =
     ReadableStreamDefaultReaderOrReadableStreamBYOBReader;
@@ -123,10 +121,6 @@ class ReadableStream final : public nsISupports, public nsWrapperCache {
 
   void GetReader(JSContext* aCx, const ReadableStreamGetReaderOptions& aOptions,
                  OwningReadableStreamReader& resultReader, ErrorResult& aRv);
-
-  MOZ_CAN_RUN_SCRIPT already_AddRefed<Promise> PipeTo(
-      WritableStream& aDestinaton, const StreamPipeOptions& aOptions,
-      ErrorResult& aRv);
 
   MOZ_CAN_RUN_SCRIPT void Tee(JSContext* aCx,
                               nsTArray<RefPtr<ReadableStream>>& aResult,
