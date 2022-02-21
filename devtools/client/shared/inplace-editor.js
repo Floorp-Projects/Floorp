b5cf3036cfce0d7239afd4ede99396894416d89d
Julian Descottes <jdescottes@mozilla.com>
2022-02-21 11:14:02 +0000

    Bug 1752780 - [devtools] Do not force units when incrementing unitless CSS properties r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D139144

--- devtools/client/shared/inplace-editor.js
+++ devtools/client/shared/inplace-editor.js
@@ -693,11 +693,16 @@ InplaceEditor.prototype = {
     // A DOM element is used to test the validity of various units. This is to
     // avoid having to do an async call to the server to get this information.
     const el = this.doc.createElement("div");
-    const units = ["px", "deg", "s"];
+
+    // Cycle through unitless (""), pixels, degrees and seconds.
+    const units = ["", "px", "deg", "s"];
     for (const unit of units) {
       const value = beforeValue + "1" + unit + afterValue;
       el.style.setProperty(this.property.name, "");
       el.style.setProperty(this.property.name, value);
+      // The property was set to `""` first, so if the value is no longer `""`,
+      // it means that the second `setProperty` call set a valid property and we
+      // can use this unit.
       if (el.style.getPropertyValue(this.property.name) !== "") {
         return unit;
       }
