44a6c895e28a93b3a3792bd1c1dd2b5ef9d6856f
Norisz Fay <nfay@mozilla.com>
2022-02-21 11:35:49 +0200

    Backed out 3 changesets (bug 1749380) for causing webrender crashes a=backout DONTBUILD
    Backed out changeset 1762adb371d4 (bug 1749380)
    Backed out changeset 2b42abbdb0df (bug 1749380)
    Backed out changeset 9fa6f71111d0 (bug 1749380)

--- gfx/wr/webrender/src/batch.rs
+++ gfx/wr/webrender/src/batch.rs
@@ -953,14 +953,8 @@ impl BatchBuilder {
                                 render_tasks,
                             ).unwrap();
 
-                            let prim_rect = ctx.data_stores.get_local_prim_rect(
-                                child_prim_instance,
-                                &ctx.prim_store.pictures,
-                                ctx.surfaces,
-                            );
-
                             let prim_header = PrimitiveHeader {
-                                local_rect: prim_rect,
+                                local_rect: pic.precise_local_rect,
                                 local_clip_rect: child_prim_info.combined_local_clip_rect,
                                 specific_prim_address: GpuCacheAddress::INVALID,
                                 transform_id: transforms
@@ -970,6 +964,11 @@ impl BatchBuilder {
                                         ctx.spatial_tree,
                                     ),
                             };
+
+                            let raster_config = pic
+                                .raster_config
+                                .as_ref()
+                                .expect("BUG: 3d primitive was not assigned a surface");
 
                             let child_pic_task_id = pic
                                 .primary_render_task_id
@@ -990,7 +989,7 @@ impl BatchBuilder {
 
                             let prim_header_index = prim_headers.push(&prim_header, z_id, [
                                 uv_rect_address.as_int(),
-                                BrushFlags::PERSPECTIVE_INTERPOLATION.bits() as i32,
+                                if raster_config.establishes_raster_root { 1 } else { 0 },
                                 0,
                                 child_clip_task_address.0 as i32,
                             ]);
@@ -1073,8 +1072,7 @@ impl BatchBuilder {
 
         let prim_rect = ctx.data_stores.get_local_prim_rect(
             prim_instance,
-            &ctx.prim_store.pictures,
-            ctx.surfaces,
+            ctx.prim_store,
         );
 
         let mut batch_features = BatchFeatures::empty();
@@ -1542,7 +1540,7 @@ impl BatchBuilder {
                 let prim_cache_address = gpu_cache.get_address(&ctx.globals.default_image_handle);
 
                 let prim_header = PrimitiveHeader {
-                    local_rect: prim_rect,
+                    local_rect: picture.precise_local_rect,
                     local_clip_rect: prim_info.combined_local_clip_rect,
                     specific_prim_address: prim_cache_address,
                     transform_id,
@@ -1564,12 +1562,16 @@ impl BatchBuilder {
                     Some(ref raster_config) => {
                         // If the child picture was rendered in local space, we can safely
                         // interpolate the UV coordinates with perspective correction.
-                        let brush_flags = BrushFlags::PERSPECTIVE_INTERPOLATION;
+                        let brush_flags = if raster_config.establishes_raster_root {
+                            BrushFlags::PERSPECTIVE_INTERPOLATION
+                        } else {
+                            BrushFlags::empty()
+                        };
 
                         let surface = &ctx.surfaces[raster_config.surface_index.0];
 
                         let mut is_opaque = prim_info.clip_task_index == ClipTaskIndex::INVALID
-                            && surface.is_opaque
+                            && surface.opaque_rect.contains_box(&surface.rect)
                             && transform_kind == TransformedRectKind::AxisAligned;
 
                         let pic_task_id = picture.primary_render_task_id.unwrap();
@@ -1584,7 +1586,7 @@ impl BatchBuilder {
                             PictureCompositeMode::Filter(ref filter) => {
                                 assert!(filter.is_visible());
                                 match filter {
-                                    Filter::Blur { .. } => {
+                                    Filter::Blur(..) => {
                                         let (clip_task_address, clip_mask_texture_id) = ctx.get_prim_clip_task_and_texture(
                                             prim_info.clip_task_index,
                                             render_tasks,
@@ -1823,7 +1825,7 @@ impl BatchBuilder {
 
                                             // These filters are handled via different paths.
                                             Filter::ComponentTransfer |
-                                            Filter::Blur { .. } |
+                                            Filter::Blur(..) |
                                             Filter::DropShadows(..) |
                                             Filter::Opacity(..) => unreachable!(),
                                         };
@@ -2110,7 +2112,7 @@ impl BatchBuilder {
                                 };
 
                                 let prim_header = PrimitiveHeader {
-                                    local_rect: prim_rect,
+                                    local_rect: picture.precise_local_rect,
                                     local_clip_rect: prim_info.combined_local_clip_rect,
                                     specific_prim_address: prim_cache_address,
                                     transform_id,
@@ -3041,8 +3043,9 @@ impl BatchBuilder {
                 );
 
                 let prim_cache_address = gpu_cache.get_address(&ctx.globals.default_image_handle);
+                let backdrop_picture = &ctx.prim_store.pictures[backdrop_pic_index.0];
                 let prim_header = PrimitiveHeader {
-                    local_rect: prim_rect,
+                    local_rect: backdrop_picture.precise_local_rect,
                     local_clip_rect: prim_info.combined_local_clip_rect,
                     transform_id,
                     specific_prim_address: prim_cache_address,
