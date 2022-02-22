e8626c4668ae5da10571e345fe5c4e8442907177
Csoregi Natalia <ncsoregi@mozilla.com>
2022-02-22 21:17:27 +0200

    Backed out 4 changesets (bug 1751010) for win bustages on nsWindow.cpp. CLOSED TREE
    Backed out changeset b68d86cc545d (bug 1751010)
    Backed out changeset b51b389bd0fd (bug 1751010)
    Backed out changeset f128c61e50eb (bug 1751010)
    Backed out changeset 1cbe75d59770 (bug 1751010)

--- widget/windows/WinTaskbar.h
+++ widget/windows/WinTaskbar.h
@@ -26,13 +26,10 @@ class WinTaskbar final : public nsIWinTaskbar {
   NS_DECL_THREADSAFE_ISUPPORTS
   NS_DECL_NSIWINTASKBAR
 
-  static bool GenerateAppUserModelID(nsAString& aAppUserModelId,
-                                     bool aPrivateBrowsing = false);
   // Registers the global app user model id for the instance.
   // See comments in WinTaskbar.cpp for more information.
   static bool RegisterAppUserModelID();
-  static bool GetAppUserModelID(nsAString& aDefaultGroupId,
-                                bool aPrivateBrowsing = false);
+  static bool GetAppUserModelID(nsAString& aDefaultGroupId);
 
  private:
   bool Initialize();
