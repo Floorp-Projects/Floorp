1ed0cf908a7689da1b9d6a696569d46a67e05992
Michael Froman <mfroman@mozilla.com>
2022-02-22 17:40:06 -0600

    Bug 1766646 - Vendor libwebrtc from dd7df5c989
    Upstream commit: https://webrtc.googlesource.com/src/+/dd7df5c989887648a582c5c8f164c6d016d9d5a9
        RTC_LOG: Internally, pass logging severity as template argument
    
        When the logging severity is statically known, passing it as a
        template argument instead of as a function argument saves space at the
        call site. libjingle_peerconnection_so.so shrinks by 5 kB for arm32 and
        9.5 kB for arm64.
    
        This is a repeat of the trick added in
        https://webrtc-review.googlesource.com/c/src/+/74480/, which was
        subsequently lost.
    
        Bug: webrtc:11968
        Change-Id: Ib6659d195d796b93e72b91d3d7c6b6e990aceb77
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/184934
        Commit-Queue: Karl Wiberg <kwiberg@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32157}

--- third_party/libwebrtc/rtc_base/logging.h
+++ third_party/libwebrtc/rtc_base/logging.h
@@ -495,6 +495,12 @@ class LogMessage {
   // |streams_| collection is empty, the LogMessage will be considered a noop
   // LogMessage.
   static bool IsNoop(LoggingSeverity severity);
+  // Version of IsNoop that uses fewer instructions at the call site, since the
+  // caller doesn't have to pass an argument.
+  template <LoggingSeverity S>
+  RTC_NO_INLINE static bool IsNoop() {
+    return IsNoop(S);
+  }
 #else
   // Next methods do nothing; no one will call these functions.
   LogMessage(const char* file, int line, LoggingSeverity sev) {}
@@ -534,6 +540,10 @@ class LogMessage {
   inline static int GetMinLogSeverity() { return 0; }
   inline static void ConfigureLogging(const char* params) {}
   static constexpr bool IsNoop(LoggingSeverity severity) { return true; }
+  template <LoggingSeverity S>
+  static constexpr bool IsNoop() {
+    return IsNoop(S);
+  }
 #endif  // RTC_LOG_ENABLED()
 
   // Enable dumping of AEC inputs and outputs.  Can be changed in mid-call
@@ -620,16 +630,18 @@ class LogMessage {
 // Logging Helpers
 //////////////////////////////////////////////////////////////////////
 
-#define RTC_LOG_FILE_LINE(sev, file, line)            \
-  !rtc::LogMessage::IsNoop(sev) &&                    \
-      ::rtc::webrtc_logging_impl::LogCall() &         \
-          ::rtc::webrtc_logging_impl::LogStreamer<>() \
-              << ::rtc::webrtc_logging_impl::LogMetadata(file, line, sev)
+#define RTC_LOG_FILE_LINE(sev, file, line)        \
+  ::rtc::webrtc_logging_impl::LogCall() &         \
+      ::rtc::webrtc_logging_impl::LogStreamer<>() \
+          << ::rtc::webrtc_logging_impl::LogMetadata(file, line, sev)
 
-#define RTC_LOG(sev) RTC_LOG_FILE_LINE(::rtc::sev, __FILE__, __LINE__)
+#define RTC_LOG(sev)                        \
+  !rtc::LogMessage::IsNoop<::rtc::sev>() && \
+      RTC_LOG_FILE_LINE(::rtc::sev, __FILE__, __LINE__)
 
 // The _V version is for when a variable is passed in.
-#define RTC_LOG_V(sev) RTC_LOG_FILE_LINE(sev, __FILE__, __LINE__)
+#define RTC_LOG_V(sev) \
+  !rtc::LogMessage::IsNoop(sev) && RTC_LOG_FILE_LINE(sev, __FILE__, __LINE__)
 
 // The _F version prefixes the message with the current function name.
 #if (defined(__GNUC__) && !defined(NDEBUG)) || defined(WANT_PRETTY_LOG_F)
