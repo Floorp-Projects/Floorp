9bf384f0b214efdec6dc4724d5d08334917ffe73
Jonathan Almeida <jalmeida@mozilla.com>
2022-02-23 00:34:59 -0700

    [focus] Set versionNameOverride when not null

--- mobile/android/focus-android/buildSrc/src/main/java/Config.kt
+++ mobile/android/focus-android/buildSrc/src/main/java/Config.kt
@@ -6,12 +6,12 @@ import org.gradle.api.Project
 
 object Config {
     @JvmStatic
-    fun releaseVersionName(project: Project): String {
+    fun releaseVersionName(project: Project): String? {
         // Note: release builds must have the `versionName` set. However, the gradle ecosystem makes this hard to
         // ergonomically validate (sometimes IDEs default to a release variant and mysteriously fail due to the
         // validation, sometimes devs just need a release build and specifying project properties is annoying in IDEs),
         // so instead we'll allow the `versionName` to silently default to an empty string.
-        return if (project.hasProperty("versionName")) project.property("versionName") as String else ""
+        return if (project.hasProperty("versionName")) project.property("versionName") as String else null
     }
 
     @JvmStatic
