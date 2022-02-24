9ddfe859cc4e9f7e769ece67795d04d156b86c2b
mcheang <mcheang@mozilla.com>
2022-02-24 10:53:53 +0000

    Bug 1754454 - update urlbar placeholder when search engine is removed on restart and idle. r=standard8 a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D139553

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4328,6 +4328,12 @@ const BrowserSearch = {
       },
       button
     );
+
+    // Update engine name in the placeholder to the new default engine name.
+    this._updateURLBarPlaceholderFromDefaultEngine(
+      PrivateBrowsingUtils.isWindowPrivate(window),
+      false
+    ).catch(Cu.reportError);
   },
 };
 
