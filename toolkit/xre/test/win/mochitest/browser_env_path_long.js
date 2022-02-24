033030df07a7872f3efcbc811794ae7c3fdbaf8c
Ray Kraesig <rkraesig@mozilla.com>
2022-02-24 19:37:16 +0000

    Bug 1753910 - avoid pathological PATH expansion  r=tkikuchi
    Unchecked, %PATH% may expand beyond the documented limits of Windows
    environment variables, and in fact, to a long enough string that
    processes inheriting this environment will fail to spawn due to
    insufficient memory.
    
    Avoid this by capping the length to that documented limit.
    
    (Additionally, write a mochitest to confirm that such paths do not
    prevent Firefox from starting.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D139072

---
+++ toolkit/xre/test/win/mochitest/browser_env_path_long.js
@@ -0,0 +1,15 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+// Test that the browser starts even when PATH would expand to a detrimentally
+// long value.
+add_task(async function test() {
+  await BrowserTestUtils.withNewTab({ gBrowser, url: "about:blank" }, function(
+    browser
+  ) {
+    ok(
+      true,
+      "Browser should start even with potentially pathologically long PATH."
+    );
+  });
+});
