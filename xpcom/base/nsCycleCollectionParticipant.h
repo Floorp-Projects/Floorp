2dc3e08ae74fe313a3c14ba0ea2db7c5bae1d9a6
Chris Peterson <cpeterson@mozilla.com>
2022-02-24 02:14:39 +0000

    Bug 1756504 - Remove cpp-virtual-final linter, a minor style check that doesn't diagnose real bugs. r=firefox-static-analysis-reviewers,sylvestre
    In bug 1436263, I added a cpp-virtual-final.yml linter to warn about virtual function declarations that included more than one virtual function specifier `virtual`, `final`, or `override`.
    
    I think we should remove this linter now because:
    
    * It's just a style check and doesn't diagnose a real bug. Including more than one virtual function specifier (`virtual`, `final`, or `override`) is harmless and unambiguous, just unnecessary extra code.
    * It has caused some engineer frustration because this style check caused their changeset to be backed out of autoland. Backing out and fixing these style issues are not a good use of sheriffs' or engineers' time.
    * It doesn't catch all virtual/final/override style issues because:
      * It can't analyze virtual function definitions that span multiple lines.
      * It doesn't check for `virtual void Foo() override` because  there are over 6000 cases already, so our code will never follow this style check consistently.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139454

--- xpcom/base/nsCycleCollectionParticipant.h
+++ xpcom/base/nsCycleCollectionParticipant.h
@@ -686,11 +686,8 @@ T* DowncastCCParticipant(void* aPtr) {
  * builds.
  */
 #ifdef DEBUG
-// clang-format off
-// Force this line to remain this way to make sure we don't trigger the
-// lint cpp-virtual-final. see bug 1505943
-#define NOT_INHERITED_CANT_OVERRIDE virtual void BaseCycleCollectable() final {}
-// clang-format on
+#  define NOT_INHERITED_CANT_OVERRIDE \
+    virtual void BaseCycleCollectable() final {}
 #else
 #  define NOT_INHERITED_CANT_OVERRIDE
 #endif
