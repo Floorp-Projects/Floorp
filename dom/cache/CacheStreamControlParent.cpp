3354e495da1771dc216f222bba435fd822e00f15
Jens Stutte <jstutte@mozilla.com>
2022-02-24 10:25:09 +0000

    Bug 1727526: Make AssertWillDelete check mStreamList and remove suspect candidates on ActorDestroy. r=dom-storage-reviewers,janv
    Differential Revision: https://phabricator.services.mozilla.com/D139569

--- dom/cache/CacheStreamControlParent.cpp
+++ dom/cache/CacheStreamControlParent.cpp
@@ -100,20 +100,23 @@ void CacheStreamControlParent::ActorDestroy(ActorDestroyReason aReason) {
     return;
   }
   mStreamList->GetManager().RemoveListener(this);
+#ifdef MOZ_DIAGNOSTIC_ASSERT_ENABLED
+  mStreamList->GetManager().RecordHaveDeletedCSCP(Id());
+#endif
   mStreamList->RemoveStreamControl(this);
   mStreamList->NoteClosedAll();
   mStreamList = nullptr;
 }
 
 void CacheStreamControlParent::AssertWillDelete() {
-  if (Id() != kFreedActorId) {
-    // If we cannot send, Send__delete__ will not do any cleanup.
-    // But if we are still unfreed, that might be wrong.
-    if (!CanSend()) {
-      MOZ_ASSERT(false, "Attempt to delete CSCP that cannot send.");
-      mStreamList->GetManager().RecordMayNotDeleteCSCP(Id());
-    }
+#ifdef MOZ_DIAGNOSTIC_ASSERT_ENABLED
+  // If we cannot send, Send__delete__ will not do any cleanup.
+  // But if we are still unfreed, that might be wrong.
+  if (mStreamList && !CanSend()) {
+    MOZ_ASSERT(false, "Attempt to delete blocking CSCP that cannot send.");
+    mStreamList->GetManager().RecordMayNotDeleteCSCP(Id());
   }
+#endif
 }
 
 mozilla::ipc::IPCResult CacheStreamControlParent::RecvOpenStream(
