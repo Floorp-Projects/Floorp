d5000aa769ab6dce41920c959480f632176dbfce
James Graham <james@hoppipolla.co.uk>
2022-02-24 14:10:17 +0000

    Bug 1756811 - Don't try to open devtools on android, r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D139492

--- testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/firefox.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/firefox.py
@@ -793,7 +793,8 @@ class FirefoxBrowser(Browser):
         if self._settings.get("special_powers", False):
             extensions.append(self.specialpowers_path)
         return ExecutorBrowser, {"marionette_port": self.instance.marionette_port,
-                                 "extensions": extensions}
+                                 "extensions": extensions,
+                                 "supports_devtools": True}
 
     def check_crash(self, process, test):
         dump_dir = os.path.join(self.instance.runner.profile.profile, "minidumps")
@@ -932,5 +933,6 @@ class FirefoxWdSpecBrowser(WebDriverBrowser):
 
     def executor_browser(self):
         cls, args = super().executor_browser()
+        args["supports_devtools"] = False
         args["profile"] = self.profile.profile
         return cls, args
