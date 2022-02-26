3ead9a30617c0e4f2d8dfc9be557a9e90dd1d76f
Emilio Cobos Álvarez <emilio@crisal.io>
2022-02-26 01:10:04 +0000

    Bug 1754266 - Teach browser_parsable_css.js about platform-specific queries. r=sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D139675

--- browser/base/content/test/static/browser_parsable_css.js
+++ browser/base/content/test/static/browser_parsable_css.js
@@ -279,8 +279,35 @@ function messageIsCSSError(msg) {
 let imageURIsToReferencesMap = new Map();
 let customPropsToReferencesMap = new Map();
 
+function neverMatches(mediaList) {
+  const perPlatformMediaQueryMap = {
+    macosx: ["(-moz-platform: macos)"],
+    win: [
+      "(-moz-platform: windows)",
+      "(-moz-platform: windows-win7)",
+      "(-moz-platform: windows-win8)",
+      "(-moz-platform: windows-win10)",
+    ],
+    linux: ["(-moz-platform: linux)"],
+    android: ["(-moz-platform: android)"],
+  };
+  for (let platform in perPlatformMediaQueryMap) {
+    if (platform === AppConstants.platform) {
+      continue;
+    }
+    if (perPlatformMediaQueryMap[platform].includes(mediaList.mediaText)) {
+      // This query only matches on another platform that isn't ours.
+      return true;
+    }
+  }
+  return false;
+}
+
 function processCSSRules(container) {
   for (let rule of container.cssRules) {
+    if (rule.media && neverMatches(rule.media)) {
+      continue;
+    }
     if (rule.styleSheet) {
       processCSSRules(rule.styleSheet); // @import
       continue;
