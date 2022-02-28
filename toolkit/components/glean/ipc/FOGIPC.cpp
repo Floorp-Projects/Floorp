2f0d17d2c04b52ca674af15db7f2a118ea28b60f
Bryce Seager van Dyk <bvandyk@mozilla.com>
2022-02-28 21:04:25 +0000

    Bug 1757347 - Further qualify mozilla::gmp namespace usage. r=florian
    This avoids lookup failures if we introduce glean metrics under 'gmp'. If we do
    introduce such metrics, they create a mozilla::glean::gmp namespace which
    confuses lookup for these lines.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139799

--- toolkit/components/glean/ipc/FOGIPC.cpp
+++ toolkit/components/glean/ipc/FOGIPC.cpp
@@ -222,8 +222,8 @@ void FlushAllChildData(
   }
 
   {
-    RefPtr<gmp::GeckoMediaPluginServiceParent> gmps(
-        gmp::GeckoMediaPluginServiceParent::GetSingleton());
+    RefPtr<mozilla::gmp::GeckoMediaPluginServiceParent> gmps(
+        mozilla::gmp::GeckoMediaPluginServiceParent::GetSingleton());
     // There can be multiple Gecko Media Plugin processes, but iterating
     // through them requires locking a mutex and the IPCs need to be sent
     // from a different thread, so it's better to let the
@@ -286,7 +286,7 @@ void SendFOGData(ipc::ByteBuf&& buf) {
       mozilla::dom::ContentChild::GetSingleton()->SendFOGData(std::move(buf));
       break;
     case GeckoProcessType_GMPlugin: {
-      gmp::SendFOGData(std::move(buf));
+      mozilla::gmp::SendFOGData(std::move(buf));
     } break;
     case GeckoProcessType_GPU:
       Unused << mozilla::gfx::GPUParent::GetSingleton()->SendFOGData(
@@ -333,8 +333,8 @@ void TestTriggerMetrics(uint32_t aProcessType,
                         const RefPtr<dom::Promise>& promise) {
   switch (aProcessType) {
     case nsIXULRuntime::PROCESS_TYPE_GMPLUGIN: {
-      RefPtr<gmp::GeckoMediaPluginServiceParent> gmps(
-          gmp::GeckoMediaPluginServiceParent::GetSingleton());
+      RefPtr<mozilla::gmp::GeckoMediaPluginServiceParent> gmps(
+          mozilla::gmp::GeckoMediaPluginServiceParent::GetSingleton());
       gmps->TestTriggerMetrics()->Then(
           GetCurrentSerialEventTarget(), __func__,
           [promise]() { promise->MaybeResolveWithUndefined(); },
