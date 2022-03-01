a43d6f6bf9164b56e3630be19dc7ccf7f0b46a40
Ben Hearsum <bhearsum@mozilla.com>
2022-03-01 14:16:48 +0000

    Bug 1751010: Use a unique icon for the Private Browsing taskbar pin r=mhowell
    This is subject to the same issue as an earlier patch in this series -- the icon will be wrong briefly at first launch, before the code in `BrowserContentHandler.jsm` replaces the default icon with the correct one.
    
    Differential Revision: https://phabricator.services.mozilla.com/D138601

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -158,6 +158,7 @@
 #include "mozilla/StaticPrefs_dom.h"
 #include "mozilla/StaticPrefs_gfx.h"
 #include "mozilla/StaticPrefs_layout.h"
+#include "nsNativeAppSupportWin.h"
 
 #include "nsIGfxInfo.h"
 #include "nsUXThemeConstants.h"
@@ -994,6 +995,10 @@ nsresult nsWindow::Create(nsIWidget* aParent, nsNativeWidget aNativeParent,
         }
       }
     }
+    HICON icon =
+        ::LoadIconW(::GetModuleHandleW(nullptr), MAKEINTRESOURCEW(IDI_PBMODE));
+    SetBigIcon(icon);
+    SetSmallIcon(icon);
   }
 
   mDeviceNotifyHandle = InputDeviceUtils::RegisterNotification(mWnd);
