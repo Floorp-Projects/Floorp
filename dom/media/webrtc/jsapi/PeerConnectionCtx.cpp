d5d869547e20b504df85802af43eba975ad6ca50
Byron Campen <docfaraday@gmail.com>
2022-03-01 22:16:49 +0000

    Bug 1755318: Fix issue where the order of PeerConnectionCtx shutdown and DataChannel shutdown was unpredictable. r=mjf
    When the pc.close() calls in PeerConnection.jsm were removed, tests that did
    not explicitly call pc.close() would result in situations where DataChannel
    shutdown ran first, which resulted in DataChannels hanging around after
    DataChannel ran its global shutdown code. So, we stop using xpcom-shutdown to
    prompt DataChannel to shut down, and instead use ShutdownBlockingTicket to
    ensure that shutdown does not proceed until DataChannel is cleaned up, which
    naturally results from the PeerConnectionCtx shutdown code.  However,
    ShutdownBlockingTicket blocks xpcom-will-shutdown, not xpcom-shutdown, which
    resulted in PeerConnectionCtx not getting an opportunity to run its shutdown
    code, which then led to shutdown hangs. So, PeerConnectionCtx runs its shutdown
    code at xpcom-will-shutdown.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139210

--- dom/media/webrtc/jsapi/PeerConnectionCtx.cpp
+++ dom/media/webrtc/jsapi/PeerConnectionCtx.cpp
@@ -207,7 +207,7 @@ class PeerConnectionCtxObserver : public nsIObserver {
 
     nsresult rv = NS_OK;
 
-    rv = observerService->AddObserver(this, NS_XPCOM_SHUTDOWN_OBSERVER_ID,
+    rv = observerService->AddObserver(this, NS_XPCOM_WILL_SHUTDOWN_OBSERVER_ID,
                                       false);
     MOZ_ALWAYS_SUCCEEDS(rv);
     rv = observerService->AddObserver(this, NS_IOSERVICE_OFFLINE_STATUS_TOPIC,
@@ -218,7 +218,7 @@ class PeerConnectionCtxObserver : public nsIObserver {
 
   NS_IMETHOD Observe(nsISupports* aSubject, const char* aTopic,
                      const char16_t* aData) override {
-    if (strcmp(aTopic, NS_XPCOM_SHUTDOWN_OBSERVER_ID) == 0) {
+    if (strcmp(aTopic, NS_XPCOM_WILL_SHUTDOWN_OBSERVER_ID) == 0) {
       CSFLogDebug(LOGTAG, "Shutting down PeerConnectionCtx");
       PeerConnectionCtx::Destroy();
 
@@ -229,7 +229,8 @@ class PeerConnectionCtxObserver : public nsIObserver {
       nsresult rv = observerService->RemoveObserver(
           this, NS_IOSERVICE_OFFLINE_STATUS_TOPIC);
       MOZ_ALWAYS_SUCCEEDS(rv);
-      rv = observerService->RemoveObserver(this, NS_XPCOM_SHUTDOWN_OBSERVER_ID);
+      rv = observerService->RemoveObserver(this,
+                                           NS_XPCOM_WILL_SHUTDOWN_OBSERVER_ID);
       MOZ_ALWAYS_SUCCEEDS(rv);
 
       // Make sure we're not deleted while still inside ::Observe()
@@ -257,7 +258,7 @@ class PeerConnectionCtxObserver : public nsIObserver {
         services::GetObserverService();
     if (observerService) {
       observerService->RemoveObserver(this, NS_IOSERVICE_OFFLINE_STATUS_TOPIC);
-      observerService->RemoveObserver(this, NS_XPCOM_SHUTDOWN_OBSERVER_ID);
+      observerService->RemoveObserver(this, NS_XPCOM_WILL_SHUTDOWN_OBSERVER_ID);
     }
   }
 };
@@ -537,7 +538,7 @@ static void GMPReady_m() {
 
 static void GMPReady() {
   GetMainThreadEventTarget()->Dispatch(WrapRunnableNM(&GMPReady_m),
-                                           NS_DISPATCH_NORMAL);
+                                       NS_DISPATCH_NORMAL);
 };
 
 void PeerConnectionCtx::initGMP() {
