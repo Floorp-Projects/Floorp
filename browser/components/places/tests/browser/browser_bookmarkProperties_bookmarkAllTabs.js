0c9d4992de63eccf69214c14bb9e3f014a4ca9a8
Butkovits Atila <abutkovits@mozilla.com>
2022-03-01 17:00:03 +0200

    Backed out changeset e84b2d8526fd (bug 1747689) for causing failures at browser_bookmarkProperties_bookmarkAllTabs.js. CLOSED TREE

--- browser/components/places/tests/browser/browser_bookmarkProperties_bookmarkAllTabs.js
+++ browser/components/places/tests/browser/browser_bookmarkProperties_bookmarkAllTabs.js
@@ -13,159 +13,61 @@ add_task(async function() {
     }
   });
 
-  add_task(async function test_bookmarkAllTabs_instantEditBookmark() {
-    await SpecialPowers.pushPrefEnv({
-      set: [["browser.bookmarks.editDialog.delayedApply.enabled", false]],
-    });
-    await withBookmarksDialog(
-      true,
-      function open() {
-        document.getElementById("Browser:BookmarkAllTabs").doCommand();
-      },
-      async dialog => {
-        let acceptBtn = dialog.document
-          .getElementById("bookmarkpropertiesdialog")
-          .getButton("accept");
-        Assert.ok(
-          !acceptBtn.disabled,
-          "InstantEditBookmark: Accept button is enabled"
-        );
+  await withBookmarksDialog(
+    true,
+    function open() {
+      document.getElementById("Browser:BookmarkAllTabs").doCommand();
+    },
+    async dialog => {
+      let acceptBtn = dialog.document
+        .getElementById("bookmarkpropertiesdialog")
+        .getButton("accept");
+      Assert.ok(!acceptBtn.disabled, "Accept button is enabled");
 
-        let namepicker = dialog.document.getElementById(
-          "editBMPanel_namePicker"
-        );
-        Assert.ok(
-          !namepicker.readOnly,
-          "InstantEditBookmark: Name field is writable"
-        );
-        let folderName = dialog.document
-          .getElementById("stringBundle")
-          .getString("bookmarkAllTabsDefault");
-        Assert.equal(
-          namepicker.value,
-          folderName,
-          "InstantEditBookmark: Name field is correct."
-        );
+      let namepicker = dialog.document.getElementById("editBMPanel_namePicker");
+      Assert.ok(!namepicker.readOnly, "Name field is writable");
+      let folderName = dialog.document
+        .getElementById("stringBundle")
+        .getString("bookmarkAllTabsDefault");
+      Assert.equal(namepicker.value, folderName, "Name field is correct.");
 
-        let promiseTitleChange = PlacesTestUtils.waitForNotification(
-          "bookmark-title-changed",
-          events => events.some(e => e.title === "folder"),
-          "places"
-        );
+      let promiseTitleChange = PlacesTestUtils.waitForNotification(
+        "bookmark-title-changed",
+        events => events.some(e => e.title === "folder"),
+        "places"
+      );
 
-        fillBookmarkTextField("editBMPanel_namePicker", "folder", dialog);
-        await promiseTitleChange;
+      fillBookmarkTextField("editBMPanel_namePicker", "folder", dialog);
+      await promiseTitleChange;
 
-        let folderPicker = dialog.document.getElementById(
-          "editBMPanel_folderMenuList"
-        );
+      let folderPicker = dialog.document.getElementById(
+        "editBMPanel_folderMenuList"
+      );
 
-        let defaultParentGuid = await PlacesUIUtils.defaultParentGuid;
-        // Check the initial state of the folder picker.
-        await TestUtils.waitForCondition(
-          () => folderPicker.getAttribute("selectedGuid") == defaultParentGuid,
-          "InstantEditBookmark: The folder is the expected one."
-        );
+      let defaultParentGuid = await PlacesUIUtils.defaultParentGuid;
+      // Check the initial state of the folder picker.
+      await TestUtils.waitForCondition(
+        () => folderPicker.getAttribute("selectedGuid") == defaultParentGuid,
+        "The folder is the expected one."
+      );
 
-        let savedItemId = dialog.gEditItemOverlay.itemId;
-        let savedItemGuid = await PlacesUtils.promiseItemGuid(savedItemId);
-        let entry = await PlacesUtils.bookmarks.fetch(savedItemGuid);
-        Assert.equal(
-          entry.parentGuid,
-          defaultParentGuid,
-          "InstantEditBookmark: Should have created the keyword in the right folder."
-        );
-      },
-      dialog => {
-        let savedItemId = dialog.gEditItemOverlay.itemId;
-        Assert.greater(savedItemId, 0, "Found the itemId");
-        return PlacesTestUtils.waitForNotification(
-          "bookmark-removed",
-          events => events.some(event => event.id === savedItemId),
-          "places"
-        );
-      }
-    );
-  });
-
-  add_task(async function test_bookmarkAllTabs_eitBookmark() {
-    await SpecialPowers.pushPrefEnv({
-      set: [["browser.bookmarks.editDialog.delayedApply.enabled", true]],
-    });
-    await withBookmarksDialog(
-      false,
-      function open() {
-        document.getElementById("Browser:BookmarkAllTabs").doCommand();
-      },
-      async dialog => {
-        let acceptBtn = dialog.document
-          .getElementById("bookmarkpropertiesdialog")
-          .getButton("accept");
-        Assert.ok(
-          !acceptBtn.disabled,
-          "EditBookmark: Accept button is enabled"
-        );
-
-        let namepicker = dialog.document.getElementById(
-          "editBMPanel_namePicker"
-        );
-        Assert.ok(!namepicker.readOnly, "EditBookmark: Name field is writable");
-        let folderName = dialog.document
-          .getElementById("stringBundle")
-          .getString("bookmarkAllTabsDefault");
-        Assert.equal(
-          namepicker.value,
-          folderName,
-          "EditBookmark: Name field is correct."
-        );
-
-        fillBookmarkTextField("editBMPanel_namePicker", "folder", dialog);
-
-        let folderPicker = dialog.document.getElementById(
-          "editBMPanel_folderMenuList"
-        );
-
-        let defaultParentGuid = await PlacesUIUtils.defaultParentGuid;
-        // Check the initial state of the folder picker.
-        await TestUtils.waitForCondition(
-          () => folderPicker.getAttribute("selectedGuid") == defaultParentGuid,
-          "EditBookmark: The folder is the expected one."
-        );
-
-        let savedItemId = dialog.gEditItemOverlay.itemId;
-        let savedItemGuid = await PlacesUtils.promiseItemGuid(savedItemId);
-        let entry = await PlacesUtils.bookmarks.fetch(savedItemGuid);
-        Assert.equal(
-          entry.parentGuid,
-          defaultParentGuid,
-          "EditBookmark: Should have created the keyword in the right folder."
-        );
-
-        EventUtils.synthesizeKey("VK_RETURN", {}, dialog);
-      }
-    );
-
-    let bookmark1 = await PlacesUtils.bookmarks.fetch({ url: "about:mozilla" });
-    let bookmark2 = await PlacesUtils.bookmarks.fetch({ url: "about:robots" });
-    let defaultParentGuid = await PlacesUIUtils.defaultParentGuid;
-
-    Assert.equal(
-      bookmark1.parentGuid,
-      bookmark2.parentGuid,
-      "EditBookmark: Both bookmarks created in same folder"
-    );
-    let folder = await PlacesUtils.bookmarks.fetch(bookmark1.parentGuid);
-    Assert.equal(
-      folder.parentGuid,
-      defaultParentGuid,
-      "EditBookmark: ParentGuid of bokmarks is default one as expected (toolbar)"
-    );
-    Assert.equal(
-      folder.title,
-      "folder",
-      "EditBookmark: folder title is expected one"
-    );
-
-    await PlacesUtils.bookmarks.eraseEverything();
-  });
+      let savedItemId = dialog.gEditItemOverlay.itemId;
+      let savedItemGuid = await PlacesUtils.promiseItemGuid(savedItemId);
+      let entry = await PlacesUtils.bookmarks.fetch(savedItemGuid);
+      Assert.equal(
+        entry.parentGuid,
+        defaultParentGuid,
+        "Should have created the keyword in the right folder."
+      );
+    },
+    dialog => {
+      let savedItemId = dialog.gEditItemOverlay.itemId;
+      Assert.greater(savedItemId, 0, "Found the itemId");
+      return PlacesTestUtils.waitForNotification(
+        "bookmark-removed",
+        events => events.some(event => event.id === savedItemId),
+        "places"
+      );
+    }
+  );
 });
