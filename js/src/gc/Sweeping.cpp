0af80bf1b632e7471c41b002da1d3fab7bc10bdd
Jon Coppeard <jcoppeard@mozilla.com>
2022-03-01 08:28:41 +0000

    Bug 1755725 - Part 1: Factor out crossZoneWrappers checks and do them before we calculate sweep groups r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D139835

--- js/src/gc/Sweeping.cpp
+++ js/src/gc/Sweeping.cpp
@@ -628,6 +628,12 @@ bool Compartment::findSweepGroupEdges() {
 }
 
 bool Zone::findSweepGroupEdges(Zone* atomsZone) {
+#ifdef DEBUG
+  if (FinalizationObservers* observers = finalizationObservers()) {
+    observers->checkTables();
+  }
+#endif
+
   // Any zone may have a pointer to an atom in the atoms zone, and these aren't
   // in the cross compartment map.
   if (atomsZone->wasGCStarted() && !addSweepGroupEdgeTo(atomsZone)) {
