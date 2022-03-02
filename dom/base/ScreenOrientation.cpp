cc62421bba2a8a4883d36b57435cd96b5cc634cd
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2022-03-02 03:48:14 +0000

    Bug 1753574 - Use previous orientation type to compare whether orientation type isn't changed. r=smaug
    Previous fix was not enough to check whether orientation type is same or not.
    
    If lock is resolved after `ScreenOrientation::MaybeChanged` is called,
    `LockOrientationTask::Run` resolves promise unfortunately.
    
    I should check previous orientation type instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D137972

--- dom/base/ScreenOrientation.cpp
+++ dom/base/ScreenOrientation.cpp
@@ -186,10 +186,18 @@ ScreenOrientation::LockOrientationTask::Run() {
     return NS_OK;
   }
 
+  BrowsingContext* bc = mDocument->GetBrowsingContext();
+  if (!bc) {
+    mPromise->MaybeResolveWithUndefined();
+    mDocument->ClearOrientationPendingPromise();
+    return NS_OK;
+  }
+
+  OrientationType previousOrientationType = bc->GetCurrentOrientationType();
   mScreenOrientation->LockDeviceOrientation(mOrientationLock, mIsFullscreen)
       ->Then(
           GetCurrentSerialEventTarget(), __func__,
-          [self = RefPtr{this}](
+          [self = RefPtr{this}, previousOrientationType](
               const GenericNonExclusivePromise::ResolveOrRejectValue& aValue) {
             if (self->mPromise->State() != Promise::PromiseState::Pending) {
               // mPromise is already resolved or rejected by
@@ -206,9 +214,13 @@ ScreenOrientation::LockOrientationTask::Run() {
               return;
             }
             if (aValue.IsResolve()) {
+              // LockDeviceOrientation won't change orientation, so change
+              // event isn't fired.
               if (BrowsingContext* bc = self->mDocument->GetBrowsingContext()) {
-                if (self->OrientationLockContains(
-                        bc->GetCurrentOrientationType()) ||
+                OrientationType currentOrientationType =
+                    bc->GetCurrentOrientationType();
+                if ((previousOrientationType == currentOrientationType &&
+                     self->OrientationLockContains(currentOrientationType)) ||
                     (self->mOrientationLock ==
                          hal::ScreenOrientation::Default &&
                      bc->GetCurrentOrientationAngle() == 0)) {
