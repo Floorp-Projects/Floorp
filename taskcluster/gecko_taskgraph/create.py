c63b7dcdc4892c2a3f0dfd435bb8218e0ed16560
Andrew Halberstadt <ahal@mozilla.com>
2022-03-02 20:19:55 +0000

    Bug 1754496 - [taskgraph] Use 'util/time.py' from vendored taskgraph, r=taskgraph-reviewers,aki
    Differential Revision: https://phabricator.services.mozilla.com/D138459

--- taskcluster/gecko_taskgraph/create.py
+++ taskcluster/gecko_taskgraph/create.py
@@ -11,9 +11,9 @@ import logging
 
 from slugid import nice as slugid
 from taskgraph.util.taskcluster import get_session, CONCURRENCY
+from taskgraph.util.time import current_json_time
 
 from gecko_taskgraph.util.parameterization import resolve_timestamps
-from gecko_taskgraph.util.time import current_json_time
 
 logger = logging.getLogger(__name__)
 
