4e84b5d5a8b082b389f4f0e7643a493f3e6a6abb
Sven Assmann <sven.assmann.it@gmail.com>
2022-03-03 21:24:56 +0000

    Bug 1651609 - Allow ESC key to exit picture-in-picture fullscreen mode. r=pip-reviewers,mhowell
    This fix handles the ESC key in 2 ways:
    - first when the PiP is in fullscreen mode, it would just leave the fullscreen mode
    - second when the PiP is not in fullscreen mode, it would handle it as same as pressing the close button of the PiP player
    - introduced a dedicated test for handling the ESC key for fullscreen and non fullscreen modes
    
    Differential Revision: https://phabricator.services.mozilla.com/D139621

--- toolkit/components/pictureinpicture/content/player.js
+++ toolkit/components/pictureinpicture/content/player.js
@@ -227,15 +227,15 @@ let Player = {
       case "keydown": {
         if (event.keyCode == KeyEvent.DOM_VK_TAB) {
           this.controls.setAttribute("keying", true);
-        } else if (
-          event.keyCode == KeyEvent.DOM_VK_ESCAPE &&
-          this.controls.hasAttribute("keying")
-        ) {
-          this.controls.removeAttribute("keying");
-
-          // We preventDefault to avoid exiting fullscreen if we happen
-          // to be in it.
+        } else if (event.keyCode == KeyEvent.DOM_VK_ESCAPE) {
           event.preventDefault();
+          if (this.isFullscreen) {
+            // We handle the ESC key, in fullscreen modus as intent to leave only the fullscreen mode
+            document.exitFullscreen();
+          } else {
+            // We handle the ESC key, as an intent to leave the picture-in-picture modus
+            this.onClose();
+          }
         } else if (
           Services.prefs.getBoolPref(KEYBOARD_CONTROLS_ENABLED_PREF, false) &&
           (event.keyCode != KeyEvent.DOM_VK_SPACE || !event.target.id)
@@ -305,7 +305,7 @@ let Player = {
 
   onDblClick(event) {
     if (event.target.id == "controls") {
-      if (document.fullscreenElement == document.body) {
+      if (this.isFullscreen) {
         document.exitFullscreen();
       } else {
         document.body.requestFullscreen();
@@ -326,10 +326,7 @@ let Player = {
       }
 
       case "close": {
-        this.actor.sendAsyncMessage("PictureInPicture:Pause", {
-          reason: "pip-closed",
-        });
-        this.closePipWindow({ reason: "close-button" });
+        this.onClose();
         break;
       }
 
@@ -350,6 +347,13 @@ let Player = {
         break;
       }
     }
+  },
+
+  onClose() {
+    this.actor.sendAsyncMessage("PictureInPicture:Pause", {
+      reason: "pip-closed",
+    });
+    this.closePipWindow({ reason: "close-button" });
   },
 
   onKeyDown(event) {
@@ -623,6 +627,15 @@ let Player = {
     const audioButton = document.getElementById("audio");
     let strId = "pictureinpicture-" + (isMuted ? "unmute" : "mute");
     document.l10n.setAttributes(audioButton, strId);
+  },
+
+  /**
+   * GET isFullscreen returns true if the video is running in fullscreen mode
+   *
+   * @returns {boolean}
+   */
+  get isFullscreen() {
+    return document.fullscreenElement == document.body;
   },
 
   /**
