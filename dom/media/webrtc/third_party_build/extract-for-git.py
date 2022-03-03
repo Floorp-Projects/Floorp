ae3ecc5898a48fe4dfe4e058012c9cccee4bd21e
Michael Froman <mfroman@mozilla.com>
2022-03-03 11:38:38 -0600

    Bug 1766646 - fix dom/media/webrtc/third_party_build/extract-for-git.py
    - account for path differences (third_party/libwebrtc/webrtc to third_party/libwebrtc)
      since last use.
    - run all mercurial commands with "HGPLAIN=1" environment variable
      to make sure aliases don't interfere with output.
    - account for additional, separately vendored directory that should
      not be included (third_party/libwebrtc/third_party).

--- dom/media/webrtc/third_party_build/extract-for-git.py
+++ dom/media/webrtc/third_party_build/extract-for-git.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import argparse
+import os
 import re
 import subprocess
 
@@ -10,10 +11,10 @@ import subprocess
 # be imported into Git. It filters out commits that are not part of upstream
 # code and rewrites the paths to match upstream. Finally, the commits are
 # combined into a mailbox file that can be applied with `git am`.
-LIBWEBRTC_DIR = "third_party/libwebrtc/webrtc"
+LIBWEBRTC_DIR = "third_party/libwebrtc"
 
 
-def build_commit_list(revset):
+def build_commit_list(revset, env):
     """Build commit list from the specified revset.
 
     The revset can be a single revision, e.g. 52bb9bb94661, or a range,
@@ -24,34 +25,38 @@ def build_commit_list(revset):
         ["hg", "log", "-r", revset, "-M", "--template", "{node}\n", LIBWEBRTC_DIR],
         capture_output=True,
         text=True,
+        env=env,
         cwd="../../../../",
     )
     return [line.strip() for line in res.stdout.strip().split("\n")]
 
 
-def extract_author_date(sha1):
+def extract_author_date(sha1, env):
     res = subprocess.run(
         ["hg", "log", "-r", sha1, "--template", "{author}|{date|isodate}"],
         capture_output=True,
         text=True,
+        env=env,
     )
     return res.stdout.split("|")
 
 
-def extract_description(sha1):
+def extract_description(sha1, env):
     res = subprocess.run(
         ["hg", "log", "-r", sha1, "--template", "{desc}"],
         capture_output=True,
         text=True,
+        env=env,
     )
     return res.stdout
 
 
-def extract_commit(sha1):
+def extract_commit(sha1, env):
     res = subprocess.run(
         ["hg", "log", "-r", sha1, "-pg", "--template", "\n"],
         capture_output=True,
         text=True,
+        env=env,
     )
     return res.stdout
 
@@ -66,18 +71,21 @@ def filter_nonwebrtc(commit):
         if (
             line.startswith("diff --git a/" + LIBWEBRTC_DIR)
             and not line.startswith("diff --git a/" + LIBWEBRTC_DIR + "/build")
+            and not line.startswith("diff --git a/" + LIBWEBRTC_DIR + "/third_party")
             and not line.endswith("moz.build")
         ):
             skipping = False
         elif line.startswith("diff --git"):
             skipping = True
+
         if not skipping:
             filtered.append(line)
     return "\n".join(filtered)
 
 
 def fixup_paths(commit):
-    return re.sub("third_party/libwebrtc/webrtc/", "", commit)
+    # make sure we only rewrite paths in the diff-related lines
+    return re.sub("( [ab])/" + LIBWEBRTC_DIR + "/", "\\1/", commit)
 
 
 def write_as_mbox(sha1, author, date, description, commit, ofile):
@@ -108,14 +116,19 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    # must run 'hg' with HGPLAIN=1 to ensure aliases don't interfere with
+    # command output.
+    env = os.environ.copy()
+    env["HGPLAIN"] = "1"
+
     for revset in args.revsets:
-        commits.extend(build_commit_list(revset))
+        commits.extend(build_commit_list(revset, env))
 
     with open("mailbox.patch", "w") as ofile:
         for sha1 in commits:
-            author, date = extract_author_date(sha1)
-            description = extract_description(sha1)
-            filtered_commit = filter_nonwebrtc(extract_commit(sha1))
+            author, date = extract_author_date(sha1, env)
+            description = extract_description(sha1, env)
+            filtered_commit = filter_nonwebrtc(extract_commit(sha1, env))
             if len(filtered_commit) == 0:
                 continue
             fixedup_commit = fixup_paths(filtered_commit)
