f48511a7b6bd757cf6389225cd4c4c45380c0e7a
Markus Stange <mstange.moz@gmail.com>
2022-03-03 04:11:15 +0000

    Bug 1749352 - Remove unused mFollowedByMomentum. r=botond
    Depends on D135922
    
    Differential Revision: https://phabricator.services.mozilla.com/D135923

--- widget/cocoa/nsChildView.mm
+++ widget/cocoa/nsChildView.mm
@@ -3247,19 +3247,6 @@ static gfx::IntPoint GetIntegerDeltaForEvent(NSEvent* aEvent) {
       panEvent.SetLineOrPageDeltas(lineOrPageDelta.x, lineOrPageDelta.y);
     }
 
-    if (panEvent.mType == PanGestureInput::PANGESTURE_END) {
-      // Check if there's a momentum start event in the event queue, so that we
-      // can annotate this event.
-      NSEvent* nextWheelEvent = [NSApp nextEventMatchingMask:NSEventMaskScrollWheel
-                                                   untilDate:[NSDate distantPast]
-                                                      inMode:NSDefaultRunLoopMode
-                                                     dequeue:NO];
-      if (nextWheelEvent &&
-          PanGestureTypeForEvent(nextWheelEvent) == PanGestureInput::PANGESTURE_MOMENTUMSTART) {
-        panEvent.mFollowedByMomentum = true;
-      }
-    }
-
     bool canTriggerSwipe = [self shouldConsiderStartingSwipeFromEvent:theEvent] &&
                            SwipeTracker::CanTriggerSwipe(panEvent);
     ;
