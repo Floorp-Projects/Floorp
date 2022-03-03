0d628cbf24ac55c74d441d95927109f59c141103
Jon Coppeard <jcoppeard@mozilla.com>
2022-03-03 17:49:18 +0000

    Bug 1643586 - Part 5: Replace heap state checks that rely on there being a JSContext for the thread r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D140192

--- js/src/gc/StoreBuffer.cpp
+++ js/src/gc/StoreBuffer.cpp
@@ -29,6 +29,20 @@ JS_PUBLIC_API void js::gc::UnlockStoreBuffer(StoreBuffer* sb) {
   sb->unlock();
 }
 
+#ifdef DEBUG
+void StoreBuffer::checkAccess() const {
+  // The GC runs tasks that may access the storebuffer in parallel and so must
+  // take a lock. The mutator may only access the storebuffer from the main
+  // thread.
+  if (runtime_->heapState() != JS::HeapState::Idle) {
+    MOZ_ASSERT(!CurrentThreadIsGCMarking());
+    lock_.assertOwnedByCurrentThread();
+  } else {
+    MOZ_ASSERT(CurrentThreadCanAccessRuntime(runtime_));
+  }
+}
+#endif
+
 bool StoreBuffer::WholeCellBuffer::init() {
   MOZ_ASSERT(!stringHead_);
   MOZ_ASSERT(!nonStringHead_);
