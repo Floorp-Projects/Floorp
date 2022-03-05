a5dbe66946e953e18948565518ca84c742990b56
Brad Werth <bwerth@mozilla.com>
2022-03-05 17:31:19 +0000

    Bug 1745492 Part 4: Replace IOSurface magic strings with CVPixelBuffer enums. r=media-playback-reviewers,alwu
    These values can be set with enums earlier in the pipeline, removing the need
    for magic strings. It also sets the BT2020 matrix when appropriate.
    
    Depends on D136823
    
    Differential Revision: https://phabricator.services.mozilla.com/D137082

--- dom/media/platforms/apple/AppleVTDecoder.cpp
+++ dom/media/platforms/apple/AppleVTDecoder.cpp
@@ -427,20 +427,27 @@ void AppleVTDecoder::OutputFrame(CVPixelBufferRef aImage,
     CVPixelBufferUnlockBaseAddress(aImage, kCVPixelBufferLock_ReadOnly);
   } else {
 #ifndef MOZ_WIDGET_UIKIT
+    // Set pixel buffer properties on aImage before we extract its surface.
+    // This ensures that we can use defined enums to set values instead
+    // of later setting magic CFSTR values on the surface itself.
+    if (mColorSpace == gfx::YUVColorSpace::BT601) {
+      CVBufferSetAttachment(aImage, kCVImageBufferYCbCrMatrixKey,
+                            kCVImageBufferYCbCrMatrix_ITU_R_601_4,
+                            kCVAttachmentMode_ShouldPropagate);
+    } else if (mColorSpace == gfx::YUVColorSpace::BT2020) {
+      CVBufferSetAttachment(aImage, kCVImageBufferYCbCrMatrixKey,
+                            kCVImageBufferYCbCrMatrix_ITU_R_2020,
+                            kCVAttachmentMode_ShouldPropagate);
+    } else {
+      CVBufferSetAttachment(aImage, kCVImageBufferYCbCrMatrixKey,
+                            kCVImageBufferYCbCrMatrix_ITU_R_709_2,
+                            kCVAttachmentMode_ShouldPropagate);
+    }
+
     CFTypeRefPtr<IOSurfaceRef> surface =
         CFTypeRefPtr<IOSurfaceRef>::WrapUnderGetRule(
             CVPixelBufferGetIOSurface(aImage));
     MOZ_ASSERT(surface, "Decoder didn't return an IOSurface backed buffer");
-
-    // Setup the correct YCbCr conversion matrix on the IOSurface, in case we
-    // pass this directly to CoreAnimation.
-    if (mColorSpace == gfx::YUVColorSpace::BT601) {
-      IOSurfaceSetValue(surface.get(), CFSTR("IOSurfaceYCbCrMatrix"),
-                        CFSTR("ITU_R_601_4"));
-    } else {
-      IOSurfaceSetValue(surface.get(), CFSTR("IOSurfaceYCbCrMatrix"),
-                        CFSTR("ITU_R_709_2"));
-    }
 
     RefPtr<MacIOSurface> macSurface = new MacIOSurface(std::move(surface));
     macSurface->SetYUVColorSpace(mColorSpace);
