cc00b6115c0cf60afac8acf80c9ac610cbb103be
surapunoyousei <ryosukeusuusu@gmail.com>
2022-03-06 01:16:38 +0900

    update notify

--- browser/extensions/floorp-system/background-script.js
+++ browser/extensions/floorp-system/background-script.js
@@ -1,11 +1,3 @@
-'use strict';
-browser.browserAction.onClicked.addListener(tab => {
-	browser.sessions.getRecentlyClosed(sessionInfos => {
-				for (let i = 0; i < sessionInfos.length; i++) {
-			let sessionInfo = sessionInfos[i];
-			if (sessionInfo.tab && sessionInfo.tab.windowId === tab.windowId) {
-				if (sessionInfo.tab.sessionId != null) {chrome.sessions.restore(sessionInfo.tab.sessionId);}else{}break;}}})});
-
 const API_END_POINT = "https://repo.ablaze.one/api/"
 
 const Notify = (url, now, latest) =>{
