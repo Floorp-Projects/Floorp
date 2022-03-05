7eb2cec4ea6ee67c245f1adb5aa0ac1514655b42
Ting-Yu Lin <tlin@mozilla.com>
2022-03-05 22:27:22 +0000

    Bug 1377072 - Minor cleanup related to overflow areas. r=dholbert
    None of these changes should change the behavior.
    
    - In `ConsiderChildOverflow()`, replace the legacy term "visual" overflow.
    
    - `GetOverflowAreasRelativeToSelf()`, `ScrollableOverflowRectRelativeToSelf()`,
      `InkOverflowRectRelativeToSelf()` can really be terser.
    
    - Replace `mRect.TopLeft()` with `Position()`, and
      `nsRect(nsPoint(0, 0), GetSize())` with `GetRectRelativeToSelf()`, making them
      shorter and clearer.
    
    - Move `GetOverflowRect()` to private section because it's an implementation
      detail of `InkOverflowRect()` and `ScrollableOverflowRect()`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D140437

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -3563,8 +3563,6 @@ class nsIFrame : public nsQueryFrame {
     return GetOverflowRect(mozilla::OverflowType::Scrollable);
   }
 
-  nsRect GetOverflowRect(mozilla::OverflowType aType) const;
-
   mozilla::OverflowAreas GetOverflowAreas() const;
 
   /**
@@ -5366,6 +5364,8 @@ class nsIFrame : public nsQueryFrame {
                                           bool aIsKeyboardSelect);
 
  private:
+  nsRect GetOverflowRect(mozilla::OverflowType aType) const;
+
   // Get a pointer to the overflow areas property attached to the frame.
   mozilla::OverflowAreas* GetOverflowAreasProperty() const {
     MOZ_ASSERT(mOverflow.mType == OverflowStorageType::Large);
