0f69edb2a5545f98e699cc0767fdff28caf7f2ec
Randell Jesup <rjesup@wgate.com>
2022-03-08 13:57:56 +0000

    Bug 1744043: Clean up nsJAR r=nika,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D132794

--- modules/libjar/test/unit/test_bug333423.js
+++ modules/libjar/test/unit/test_bug333423.js
@@ -15,6 +15,10 @@ function run_test() {
   );
   zipreader.open(file);
   zipreader.close();
-  var entries = zipreader.findEntries("*.*");
-  Assert.ok(!entries.hasMore()); // this shouldn't crash
+  // this should error out and not crash
+  Assert.throws(
+    () => zipreader.findEntries("*.*"),
+    /NS_ERROR_FAILURE/,
+    "Should error out on a closed zipreader"
+  );
 }
