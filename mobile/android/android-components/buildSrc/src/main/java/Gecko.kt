bf9fd5665bc37107db4d76475d37993b1bb59e7c
Gabriel Luong <gabriel.luong@gmail.com>
2022-03-08 09:18:10 -0500

    [components] Update to GeckoView 100.0

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "99.0.20220308092232"
+    const val version = "100.0.20220308100756"
 
     /**
      * GeckoView channel
