d1eb14906c3b44d3a5a48b53ae2daa3f70acbdde
Henrik Skupin <mail@hskupin.info>
2022-03-10 20:36:34 +0000

    Bug 1758909 - Improvements for test_preloader_telemetry.py r=dthayer
    This patch allows to correctly await observer notifications by
    using the "Execute Async Command" of Marionette which doesn't
    return immediately but when resolve() is called.
    
    Further it improves the checks for hits and misses by comparing
    numbers and not just booleans. This improves the failure output.
    
    Differential Revision: https://phabricator.services.mozilla.com/D140367

--- js/xpconnect/tests/marionette/test_preloader_telemetry.py
+++ js/xpconnect/tests/marionette/test_preloader_telemetry.py
@@ -5,7 +5,9 @@ from __future__ import absolute_import
 
 import time
 import os
+
 from marionette_harness import MarionetteTestCase
+
 
 LABELS_SCRIPT_PRELOADER_REQUESTS = {
     "Hit": "0",
@@ -18,11 +20,14 @@ class TestScriptPreloader(MarionetteTestCase):
     def test_preloader_requests_histogram(self):
         start_time = time.time()
         self.invalidate_caches()
+
         self.marionette.restart(clean=False, in_app=True)
+
         histogram = self.get_histogram("SCRIPT_PRELOADER_REQUESTS")
         misses = histogram.get(LABELS_SCRIPT_PRELOADER_REQUESTS["Miss"], 0)
         hits = histogram.get(LABELS_SCRIPT_PRELOADER_REQUESTS["Hit"], 0)
         child_hits = histogram.get(LABELS_SCRIPT_PRELOADER_REQUESTS["HitChild"], 0)
+
         self.assertMuchGreaterThan(misses, hits)
         self.assertMuchGreaterThan(misses, child_hits)
 
@@ -34,21 +39,23 @@ class TestScriptPreloader(MarionetteTestCase):
             start_time, "{}/startupCache/scriptCache-child.bin".format(profile)
         )
         self.marionette.restart(clean=False, in_app=True)
+
         histogram = self.get_histogram("SCRIPT_PRELOADER_REQUESTS")
         misses = histogram.get(LABELS_SCRIPT_PRELOADER_REQUESTS["Miss"], 0)
         hits = histogram.get(LABELS_SCRIPT_PRELOADER_REQUESTS["Hit"], 0)
         child_hits = histogram.get(LABELS_SCRIPT_PRELOADER_REQUESTS["HitChild"], 0)
+
         # This is just heuristic. We certainly want to be made aware if this ratio
         # changes and we didn't intend it to.
         self.assertSimilar(misses, hits)
         self.assertNotEqual(child_hits, 0)
 
     def assertMuchGreaterThan(self, lhs, rhs):
-        self.assertTrue(lhs > 4 * rhs)
+        self.assertGreater(lhs, 4 * rhs)
 
     def assertSimilar(self, lhs, rhs):
-        self.assertTrue(lhs < 2 * rhs)
-        self.assertTrue(lhs > rhs / 2)
+        self.assertLess(lhs, 2 * rhs)
+        self.assertGreater(lhs, rhs / 2)
 
     def wait_for_file_change(self, start_time, path):
         expires = time.time() + 20
@@ -64,24 +71,23 @@ class TestScriptPreloader(MarionetteTestCase):
 
     def wait_for_observer_notification(self, name):
         with self.marionette.using_context(self.marionette.CONTEXT_CHROME):
-            return self.marionette.execute_script(
+            return self.marionette.execute_async_script(
                 """
-                let [resolve] = arguments;
-                Services.obs.addObserver(() => resolve(), "{}");
-            """.format(
-                    name
-                )
+                const [name, resolve] = arguments;
+                Services.obs.addObserver(() => resolve(), name);
+            """,
+                script_args=(name,),
             )
 
     def get_histogram(self, name):
         with self.marionette.using_context(self.marionette.CONTEXT_CHROME):
             return self.marionette.execute_script(
                 """
-                let snapshot = Services.telemetry.getSnapshotForHistograms("main", true);
-                return snapshot.parent.{}.values;
-            """.format(
-                    name
-                )
+                const name = arguments[0];
+                const snapshot = Services.telemetry.getSnapshotForHistograms("main", true);
+                return snapshot.parent[name].values;
+            """,
+                script_args=(name,),
             )
 
     def invalidate_caches(self):
