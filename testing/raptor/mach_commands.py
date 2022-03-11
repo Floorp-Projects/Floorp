e3d6487b7aae437bc11841c18059aa531ce33e3b
Gregory Mierzwinski <gmierz2@outlook.com>
2022-03-11 13:12:58 +0000

    Bug 1758992 - Update matrix-react-bench to v1.1. r=perftest-reviewers,AlexandruIonescu
    Differential Revision: https://phabricator.services.mozilla.com/D140740

--- testing/raptor/mach_commands.py
+++ testing/raptor/mach_commands.py
@@ -28,7 +28,7 @@ from mozbuild.base import MachCommandConditions as Conditions
 HERE = os.path.dirname(os.path.realpath(__file__))
 
 BENCHMARK_REPOSITORY = "https://github.com/mozilla/perf-automation"
-BENCHMARK_REVISION = "7eef4a5eb4bd2d7558967e947b57cda33e287133"
+BENCHMARK_REVISION = "c51ca3fc73cd9a866c99357005cf1e21342a6a50"
 
 ANDROID_BROWSERS = ["geckoview", "refbrow", "fenix", "chrome-m"]
 
