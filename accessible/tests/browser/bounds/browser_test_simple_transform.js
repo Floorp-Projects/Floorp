cd3030c2752dd30b02d59cb2500194ce7a0dec1a
Morgan Reschenberg <mreschenberg@mozilla.com>
2022-03-14 19:52:50 +0000

    Bug 1756229: Cache scroll position r=Jamie,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D139190

--- accessible/tests/browser/bounds/browser_test_simple_transform.js
+++ accessible/tests/browser/bounds/browser_test_simple_transform.js
@@ -4,71 +4,20 @@
 
 "use strict";
 
-async function finishContentPaint(browser) {
-  await SpecialPowers.spawn(browser, [], () => {
-    return new Promise(function(r) {
-      content.requestAnimationFrame(() => content.setTimeout(r));
-    });
-  });
-}
-
-async function testContentBoundsInIframe(iframeDocAcc, id, browser) {
-  const acc = findAccessibleChildByID(iframeDocAcc, id);
-  const x = {};
-  const y = {};
-  const width = {};
-  const height = {};
-  acc.getBounds(x, y, width, height);
-
-  await invokeContentTask(
-    browser,
-    [id, x.value, y.value, width.value, height.value],
-    (_id, _x, _y, _width, _height) => {
-      const { Layout: LayoutUtils } = ChromeUtils.import(
-        "chrome://mochitests/content/browser/accessible/tests/browser/Layout.jsm"
-      );
-
-      let [
-        expectedX,
-        expectedY,
-        expectedWidth,
-        expectedHeight,
-      ] = LayoutUtils.getBoundsForDOMElm(_id, content.document);
-
-      ok(
-        _x >= expectedX - 5 || _x <= expectedX + 5,
-        "Got " + _x + ", accurate x for " + _id
-      );
-      ok(
-        _y >= expectedY - 5 || _y <= expectedY + 5,
-        "Got " + _y + ", accurate y for " + _id
-      );
-      ok(
-        _width >= expectedWidth - 5 || _width <= expectedWidth + 5,
-        "Got " + _width + ", accurate width for " + _id
-      );
-      ok(
-        _height >= expectedHeight - 5 || _height <= expectedHeight + 5,
-        "Got " + _height + ", accurate height for " + _id
-      );
-    }
-  );
-}
-
 // test basic translation
 addAccessibleTask(
   `<p id="translate">hello world</p>`,
   async function(browser, iframeDocAcc, contentDocAcc) {
     ok(iframeDocAcc, "IFRAME document accessible is present");
-    await testContentBoundsInIframe(iframeDocAcc, "translate", browser);
+    await testBoundsInContent(iframeDocAcc, "translate", browser);
 
     await invokeContentTask(browser, [], () => {
       let p = content.document.getElementById("translate");
       p.style = "transform: translate(100px, 100px);";
     });
 
-    await finishContentPaint(browser);
-    await testContentBoundsInIframe(iframeDocAcc, "translate", browser);
+    await waitForContentPaint(browser);
+    await testBoundsInContent(iframeDocAcc, "translate", browser);
   },
   { topLevel: true, iframe: true, remoteIframe: true }
 );
@@ -78,15 +27,15 @@ addAccessibleTask(
   `<p id="rotate">hello world</p>`,
   async function(browser, iframeDocAcc, contentDocAcc) {
     ok(iframeDocAcc, "IFRAME document accessible is present");
-    await testContentBoundsInIframe(iframeDocAcc, "rotate", browser);
+    await testBoundsInContent(iframeDocAcc, "rotate", browser);
 
     await invokeContentTask(browser, [], () => {
       let p = content.document.getElementById("rotate");
       p.style = "transform: rotate(-40deg);";
     });
 
-    await finishContentPaint(browser);
-    await testContentBoundsInIframe(iframeDocAcc, "rotate", browser);
+    await waitForContentPaint(browser);
+    await testBoundsInContent(iframeDocAcc, "rotate", browser);
   },
   { topLevel: true, iframe: true, remoteIframe: true }
 );
@@ -96,15 +45,15 @@ addAccessibleTask(
   `<p id="scale">hello world</p>`,
   async function(browser, iframeDocAcc, contentDocAcc) {
     ok(iframeDocAcc, "IFRAME document accessible is present");
-    await testContentBoundsInIframe(iframeDocAcc, "scale", browser);
+    await testBoundsInContent(iframeDocAcc, "scale", browser);
 
     await invokeContentTask(browser, [], () => {
       let p = content.document.getElementById("scale");
       p.style = "transform: scale(2);";
     });
 
-    await finishContentPaint(browser);
-    await testContentBoundsInIframe(iframeDocAcc, "scale", browser);
+    await waitForContentPaint(browser);
+    await testBoundsInContent(iframeDocAcc, "scale", browser);
   },
   { topLevel: true, iframe: true, remoteIframe: true }
 );
