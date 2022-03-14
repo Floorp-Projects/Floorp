25bcb1905aad4e7692bf38dac6ba20b980b23087
Morgan Reschenberg <mreschenberg@mozilla.com>
2022-03-14 19:52:50 +0000

    Bug 1757661: Make cached fields accessible in tests directly r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D139979

--- accessible/tests/browser/scroll/browser_test_scroll_bounds.js
+++ accessible/tests/browser/scroll/browser_test_scroll_bounds.js
@@ -7,28 +7,26 @@
 /* import-globals-from ../../mochitest/layout.js */
 loadScripts({ name: "layout.js", dir: MOCHITESTS_DIR });
 
-async function runTests(browser, docAcc) {
-  ok(docAcc, "iframe document acc is present");
-  await testBoundsInContent(docAcc, "square", browser);
-  await testBoundsInContent(docAcc, "rect", browser);
+const appUnitsPerDevPixel = 60;
 
-  await invokeContentTask(browser, [], () => {
-    content.document.getElementById("square").scrollIntoView();
-  });
+function testCachedScrollPosition(acc, expectedX, expectedY) {
+  let cachedPosition = "";
+  try {
+    cachedPosition = acc.cache.getStringProperty("scroll-position");
+  } catch (e) {
+    // `getStringProperty` will throw an exception if we ask for
+    // a domain it doesn't have -- catch the exception here to
+    // prevent it from taking down the whole test.
+    console.info("Unable to fetch scroll position from cache!");
+  }
 
-  await waitForContentPaint(browser);
-
-  await testBoundsInContent(docAcc, "square", browser);
-  await testBoundsInContent(docAcc, "rect", browser);
-
-  await invokeContentTask(browser, [], () => {
-    content.document.getElementById("rect").scrollIntoView();
-  });
-
-  await waitForContentPaint(browser);
-
-  await testBoundsInContent(docAcc, "square", browser);
-  await testBoundsInContent(docAcc, "rect", browser);
+  // The value we retrieve from the cache is in app units, but the values
+  // passed in are in pixels. Since the retrieved value is a string,
+  // and harder to modify, adjust our expected x and y values to match its units.
+  return (
+    cachedPosition ==
+    `${expectedX * appUnitsPerDevPixel}, ${expectedY * appUnitsPerDevPixel}`
+  );
 }
 
 /**
@@ -42,6 +40,81 @@ addAccessibleTask(
   <div id='rect' style='height:40px; width:200px; background:blue; margin-bottom:3400px'>
   </div>
   `,
-  runTests,
+  async function(browser, docAcc) {
+    ok(docAcc, "iframe document acc is present");
+    await testBoundsInContent(docAcc, "square", browser);
+    await testBoundsInContent(docAcc, "rect", browser);
+
+    await invokeContentTask(browser, [], () => {
+      content.document.getElementById("square").scrollIntoView();
+    });
+
+    await waitForContentPaint(browser);
+
+    await testBoundsInContent(docAcc, "square", browser);
+    await testBoundsInContent(docAcc, "rect", browser);
+
+    await invokeContentTask(browser, [], () => {
+      content.document.getElementById("rect").scrollIntoView();
+    });
+
+    await waitForContentPaint(browser);
+    await testBoundsInContent(docAcc, "square", browser);
+    await testBoundsInContent(docAcc, "rect", browser);
+  },
   { iframe: true, remoteIframe: true, chrome: true }
+);
+
+/**
+ * Test scroll offset on cached accessibles
+ */
+addAccessibleTask(
+  `
+  <div id='square' style='height:100px; width:100px; background:green; margin-top:3000px; margin-bottom:4000px;'>
+  </div>
+
+  <div id='rect' style='height:40px; width:200px; background:blue; margin-bottom:3400px'>
+  </div>
+  `,
+  async function(browser, docAcc) {
+    // We can only access the `cache` attribute of an accessible when
+    // the cache is enabled and we're in a remote browser. Verify
+    // both these conditions hold, and return early if they don't.
+    if (!isCacheEnabled || !browser.isRemoteBrowser) {
+      return;
+    }
+
+    ok(docAcc, "iframe document acc is present");
+    await untilCacheOk(
+      () => testCachedScrollPosition(docAcc, 0, 0),
+      "Correct initial scroll position."
+    );
+
+    await invokeContentTask(browser, [], () => {
+      content.document.getElementById("square").scrollIntoView();
+    });
+
+    await waitForContentPaint(browser);
+
+    // The only content to scroll over is `square`'s top margin
+    // so our scroll offset here should be 3000px
+    await untilCacheOk(
+      () => testCachedScrollPosition(docAcc, 0, 3000),
+      "Correct scroll position after first scroll."
+    );
+
+    await invokeContentTask(browser, [], () => {
+      content.document.getElementById("rect").scrollIntoView();
+    });
+
+    await waitForContentPaint(browser);
+    // We have to scroll over `square`'s top margin (3000px),
+    // `square` itself (100px), and `square`'s bottom margin (4000px).
+    // This should give us a 7100px offset.
+    await untilCacheOk(
+      () => testCachedScrollPosition(docAcc, 0, 7100),
+      "Correct final scroll position."
+    );
+  },
+  { iframe: true, remoteIframe: true }
 );
