f1448ea2792a4e46eb69eecf08f07ca2d4095857
Tooru Fujisawa <arai_a@mac.com>
2022-03-15 04:24:53 +0000

    Bug 1436400 - Part 12: Add testcases for bytecode encoding and module with import. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D140553

--- dom/base/test/.eslintrc.js
+++ dom/base/test/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   overrides: [
     {
-      files: "file_module_js_cache.js",
+      files: ["file_module_js_cache.js", "file_script_module_*.js"],
       parserOptions: {
         sourceType: "module",
       },
