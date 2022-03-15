2eebde76b00717371c836a404820a033bb765382
Nicolas Chevobbe <nchevobbe@mozilla.com>
2022-03-15 13:47:54 +0000

    Bug 1759136 - [devtools] Fix Reps TODOs for mode PropType. r=Honza.
    Differential Revision: https://phabricator.services.mozilla.com/D140836

--- devtools/client/shared/components/reps/reps/error.js
+++ devtools/client/shared/components/reps/reps/error.js
@@ -35,8 +35,7 @@ define(function(require, exports, module) {
    */
   ErrorRep.propTypes = {
     object: PropTypes.object.isRequired,
-    // @TODO Change this to Object.values when supported in Node's version of V8
-    mode: PropTypes.oneOf(Object.keys(MODE).map(key => MODE[key])),
+    mode: PropTypes.oneOf(Object.values(MODE)),
     // An optional function that will be used to render the Error stacktrace.
     renderStacktrace: PropTypes.func,
     shouldRenderTooltip: PropTypes.bool,
