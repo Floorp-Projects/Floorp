54ee3ee5428ccf6458f123c11028d9884a5d2610
Marian-Vasile Laza <mlaza@mozilla.com>
2022-03-15 15:25:14 -0700

    Backed out changeset 831481b0bc78 (bug 1759498) for causing bc failures on browser_popupNotification.js. CLOSED TREE

--- toolkit/content/widgets/popupnotification.js
+++ toolkit/content/widgets/popupnotification.js
@@ -29,8 +29,10 @@
         ".popup-notification-learnmore-link":
           "onclick=learnmoreclick,href=learnmoreurl",
         ".popup-notification-warning": "hidden=warninghidden,text=warninglabel",
-        ".popup-notification-secondary-button":
+        ".popup-notification-button-container > .popup-notification-secondary-button":
           "oncommand=secondarybuttoncommand,label=secondarybuttonlabel,accesskey=secondarybuttonaccesskey,hidden=secondarybuttonhidden,dropmarkerhidden",
+        ".popup-notification-button-container > toolbarseparator":
+          "hidden=dropmarkerhidden",
         ".popup-notification-dropmarker":
           "onpopupshown=dropmarkerpopupshown,hidden=dropmarkerhidden",
         ".popup-notification-dropmarker > menupopup": "oncommand=menucommand",
@@ -89,12 +91,13 @@
       </hbox>
       <hbox class="popup-notification-footer-container"></hbox>
       <hbox class="popup-notification-button-container panel-footer">
-        <button class="popup-notification-secondary-button"/>
-        <button type="menu" class="popup-notification-dropmarker" aria-label="&moreActionsButton.accessibleLabel;">
+        <button class="popup-notification-button popup-notification-secondary-button"></button>
+        <toolbarseparator></toolbarseparator>
+        <button type="menu" class="popup-notification-button popup-notification-dropmarker" aria-label="&moreActionsButton.accessibleLabel;">
           <menupopup position="after_end" aria-label="&moreActionsButton.accessibleLabel;">
           </menupopup>
         </button>
-        <button class="popup-notification-primary-button" label="&defaultButton.label;" accesskey="&defaultButton.accesskey;"/>
+        <button class="popup-notification-button popup-notification-primary-button" label="&defaultButton.label;" accesskey="&defaultButton.accesskey;"></button>
       </hbox>
       `;
     }
