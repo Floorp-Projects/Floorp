201953d2d277bb34527873c8ed65a1a27d6912f4
Nick Alexander <nalexander@mozilla.com>
2022-03-16 17:18:45 +0000

    Bug 1679443 - Add test ensuring profiles created by background tasks are (and remain) "slim". r=bytesized
    There are a few ways that we could test this.  We could use the
    profiler and "File IO" markers, a la
    https://searchfox.org/mozilla-central/source/browser/base/content/test/performance/browser_startup_content_mainthreadio.js.
    This would profile content that is transient, which could be good or
    bad -- temporary files for atomic writes would show up for example.
    But in fact there are profile contents created after the profiler is
    shut down (including `Telemetry.ShutdownTime.txt`), so this approach
    isn't sufficient.
    
    Therefore we do the simpler thing: we simply don't remove the
    temporary profile directory after the background task exits.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139909

---
+++ toolkit/components/backgroundtasks/tests/BackgroundTask_profile_is_slim.jsm
@@ -0,0 +1,33 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// A task that exercises various functionality to witness contents of
+// the temporary profile created during background tasks.  This will
+// be a dumping ground for functionality that writes to the profile.
+
+"use strict";
+
+var EXPORTED_SYMBOLS = ["runBackgroundTask"];
+
+Cu.importGlobalProperties(["fetch"]);
+
+const { EXIT_CODE } = ChromeUtils.import(
+  "resource://gre/modules/BackgroundTasksManager.jsm"
+).BackgroundTasksManager;
+
+async function runBackgroundTask(commandLine) {
+  console.error("runBackgroundTask: is_profile_slim");
+
+  // For now, just verify contents of profile after a network request.
+  if (commandLine.length != 1) {
+    console.error("Single URL argument required");
+    return 1;
+  }
+
+  let response = await fetch(commandLine.getArgument(0));
+  console.error(`response status code: ${response.status}`);
+
+  return response.ok ? EXIT_CODE.SUCCESS : 11;
+}
---
+++ toolkit/components/backgroundtasks/tests/xpcshell/test_backgroundtask_profile_is_slim.js
@@ -0,0 +1,145 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
+ * vim: sw=4 ts=4 sts=4 et
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Invoke `profile_is_slim` background task, have it *not* remove its
+// temporary profile, and verify that the temporary profile doesn't
+// contain unexpected files and/or directories.
+//
+// N.b.: the background task invocation is a production Firefox
+// running in automation; that means it can only connect to localhost
+// (and it is not configured to connect to mochi.test, etc).
+// Therefore we run our own server for it to connect to.
+
+const { HttpServer } = ChromeUtils.import("resource://testing-common/httpd.js");
+let server;
+
+setupProfileService();
+
+let successCount = 0;
+function success(metadata, response) {
+  response.setStatusLine(metadata.httpVersion, 200, "OK");
+  let str = "success";
+  response.write(str, str.length);
+  successCount += 1;
+}
+
+add_setup(() => {
+  server = new HttpServer();
+  server.registerPathHandler("/success", success);
+  server.start(-1);
+
+  registerCleanupFunction(async () => {
+    await server.stop();
+  });
+});
+
+add_task(async function test_backgroundtask_profile_is_slim() {
+  let profileService = Cc["@mozilla.org/toolkit/profile-service;1"].getService(
+    Ci.nsIToolkitProfileService
+  );
+
+  let profD = do_get_profile();
+  profD.append("test_profile_is_slim");
+  let profile = profileService.createUniqueProfile(
+    profD,
+    "test_profile_is_slim"
+  );
+
+  registerCleanupFunction(() => {
+    profile.remove(true);
+  });
+
+  let exitCode = await do_backgroundtask("profile_is_slim", {
+    extraArgs: [`http://localhost:${server.identity.primaryPort}/success`],
+    extraEnv: {
+      XRE_PROFILE_PATH: profile.rootDir.path,
+      MOZ_BACKGROUNDTASKS_NO_REMOVE_PROFILE: "1",
+    },
+  });
+
+  Assert.equal(
+    0,
+    exitCode,
+    "The fetch background task exited with exit code 0"
+  );
+  Assert.equal(
+    1,
+    successCount,
+    "The fetch background task reached the test server 1 time"
+  );
+
+  assertProfileIsSlim(profile.rootDir);
+});
+
+const expected = [
+  { relPath: "lock", condition: AppConstants.platform == "linux" },
+  {
+    relPath: ".parentlock",
+    condition:
+      AppConstants.platform == "linux" || AppConstants.platform == "macosx",
+  },
+  {
+    relPath: "parent.lock",
+    condition:
+      AppConstants.platform == "win" || AppConstants.platform == "macosx",
+  },
+  // Bug 1757229.
+  { relPath: "ShutdownDuration.json" },
+  { relPath: "Telemetry.ShutdownTime.txt" },
+  // TODO: verify that directory is empty.
+  { relPath: "cache2", isDirectory: true },
+  // Bug 1757252.
+  { relPath: "cert9.db" },
+  { relPath: "compatibility.ini" },
+  // Bug 1675829.
+  { relPath: "cookies.sqlite" },
+  { relPath: "crashes", isDirectory: true },
+  { relPath: "data", isDirectory: true },
+  // Bug 1757252.
+  { relPath: "key4.db" },
+  { relPath: "local", isDirectory: true },
+  { relPath: "minidumps", isDirectory: true },
+  // `mozinfo.json` is an artifact of the testing infrastructure.
+  { relPath: "mozinfo.json" },
+  { relPath: "pkcs11.txt" },
+  { relPath: "prefs.js" },
+  { relPath: "security_state", isDirectory: true },
+  // TODO: verify that directory is empty.
+  { relPath: "startupCache", isDirectory: true },
+  { relPath: "times.json" },
+];
+
+async function assertProfileIsSlim(profile) {
+  Assert.ok(profile.exists(), `Profile directory does exist: ${profile.path}`);
+
+  // We collect unexpected results, log them all, and then assert
+  // there are none to provide the most feedback per iteration.
+  let unexpected = [];
+
+  let typeLabel = { true: "directory", false: "file" };
+
+  for (let entry of profile.directoryEntries) {
+    // `getRelativePath` always uses '/' as path separator.
+    let relPath = entry.getRelativePath(profile);
+
+    info(`Witnessed directory entry '${relPath}'.`);
+
+    let expectation = expected.find(
+      it => it.relPath == relPath && (!("condition" in it) || it.condition)
+    );
+    if (!expectation) {
+      info(`UNEXPECTED: Directory entry '${relPath}' is NOT expected!`);
+      unexpected.push(relPath);
+    } else if (
+      typeLabel[!!expectation.isDirectory] != typeLabel[entry.isDirectory()]
+    ) {
+      info(`UNEXPECTED: Directory entry '${relPath}' is NOT expected type!`);
+      unexpected.push(relPath);
+    }
+  }
+
+  Assert.deepEqual([], unexpected, "Expected nothing to report");
+}
