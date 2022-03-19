c0c6d17864facaa8feede095424b6c5b92046d66
Nicholas Rishel <nrishel@mozilla.com>
2022-03-19 03:21:24 +0000

    Bug 1722777 - Pre: Move "skip windows" XPCShell test config for test_symlinks.js and test_bug476919.js from source to configuration file. r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D140427

--- xpcom/tests/unit/test_bug476919.js
+++ xpcom/tests/unit/test_bug476919.js
@@ -1,30 +1,25 @@
 /* global __LOCATION__ */
 
 function run_test() {
-  // skip this test on Windows
-  if (mozinfo.os != "win") {
-    var testDir = __LOCATION__.parent;
-    // create a test file, then symlink it, then check that we think it's a symlink
-    var targetFile = testDir.clone();
-    targetFile.append("target.txt");
-    if (!targetFile.exists()) {
-      targetFile.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0o644);
-    }
-
-    var link = testDir.clone();
-    link.append("link");
-    if (link.exists()) {
-      link.remove(false);
-    }
-
-    var ln = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
-    ln.initWithPath("/bin/ln");
-    var process = Cc["@mozilla.org/process/util;1"].createInstance(
-      Ci.nsIProcess
-    );
-    process.init(ln);
-    var args = ["-s", targetFile.path, link.path];
-    process.run(true, args, args.length);
-    Assert.ok(link.isSymlink());
+  var testDir = __LOCATION__.parent;
+  // create a test file, then symlink it, then check that we think it's a symlink
+  var targetFile = testDir.clone();
+  targetFile.append("target.txt");
+  if (!targetFile.exists()) {
+    targetFile.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0o644);
   }
+
+  var link = testDir.clone();
+  link.append("link");
+  if (link.exists()) {
+    link.remove(false);
+  }
+
+  var ln = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
+  ln.initWithPath("/bin/ln");
+  var process = Cc["@mozilla.org/process/util;1"].createInstance(Ci.nsIProcess);
+  process.init(ln);
+  var args = ["-s", targetFile.path, link.path];
+  process.run(true, args, args.length);
+  Assert.ok(link.isSymlink());
 }
