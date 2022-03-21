56c2f1570b96af1703ba90d1c9143214e71b9748
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-03-21 16:45:37 +0000

    Bug 1759690 - Part 4: Add a helper function for void callbacks r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D141392

--- dom/promise/Promise.h
+++ dom/promise/Promise.h
@@ -274,6 +274,13 @@ class Promise : public SupportsWeakPtr {
       const std::function<already_AddRefed<Promise>(
           JSContext*, JS::HandleValue, ErrorResult& aRv)>& aCallback);
 
+  // Similar to ThenCatchWithCycleCollectedArgs but doesn't care with return
+  // values of the callbacks and does not return a new promise.
+  template <typename ResolveCallback, typename RejectCallback, typename... Args>
+  void AddCallbacksWithCycleCollectedArgs(ResolveCallback&& aOnResolve,
+                                          RejectCallback&& aOnReject,
+                                          Args&&... aArgs);
+
   JSObject* PromiseObj() const { return mPromiseObj; }
 
   void AppendNativeHandler(PromiseNativeHandler* aRunnable);
