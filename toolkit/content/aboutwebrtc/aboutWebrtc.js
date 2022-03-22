eec8d53b79dbd854b2063dab6ef8f65350d749b0
Jan-Ivar Bruaroey <jib@mozilla.com>
2022-03-22 14:12:19 +0000

    Bug 1760687 - In about:webrtc RTP stats, fix wrong units shown (literally s/Kbps/KBps/ wo/changing numbers). r=ng DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D141691

--- toolkit/content/aboutwebrtc/aboutWebrtc.js
+++ toolkit/content/aboutwebrtc/aboutWebrtc.js
@@ -744,7 +744,7 @@ function renderTransportStats(
     }
   }
 
-  const estimateKbps = (timestamp, lastTimestamp, bytes, lastBytes) => {
+  const estimateKBps = (timestamp, lastTimestamp, bytes, lastBytes) => {
     if (!timestamp || !lastTimestamp || !bytes || !lastBytes) {
       return "0.0";
     }
@@ -787,12 +787,12 @@ function renderTransportStats(
     if (bytesReceived) {
       let s = ` (${(bytesReceived / 1024).toFixed(2)} Kb`;
       if (local && history) {
-        s += ` , ${estimateKbps(
+        s += ` , ${estimateKBps(
           timestamp,
           history[id].lastTimestamp,
           bytesReceived,
           history[id].lastBytesReceived
-        )} Kbps`;
+        )} KBps`;
       }
       s += ")";
       elements.push(renderText("span", s));
@@ -836,12 +836,12 @@ function renderTransportStats(
     if (bytesSent) {
       let s = ` (${(bytesSent / 1024).toFixed(2)} Kb`;
       if (local && history) {
-        s += `, ${estimateKbps(
+        s += `, ${estimateKBps(
           timestamp,
           history[id].lastTimestamp,
           bytesSent,
           history[id].lastBytesSent
-        )} Kbps`;
+        )} KBps`;
       }
       s += ")";
       elements.push(renderText("span", s));
