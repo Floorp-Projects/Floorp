93cb39c8cf55e318ebbafda9a71e8df33ff07fdc
Miko Mynttinen <mikokm@gmail.com>
2022-03-22 22:34:37 +0000

    Bug 1760847 - Deallocate linked list nodes when RemoveBottom() is called r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D141789

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -3245,7 +3245,10 @@ class nsDisplayList {
     }
 
     nsDisplayItem* bottom = mBottom->mValue;
-    mBottom = mBottom->mNext;
+
+    auto next = mBottom->mNext;
+    Deallocate(mBottom);
+    mBottom = next;
 
     if (!mBottom) {
       // No bottom item means no items at all.
