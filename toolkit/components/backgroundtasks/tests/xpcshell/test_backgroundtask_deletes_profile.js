1042182210fce475db2d475795ccfaae85ed6338
Nick Alexander <nalexander@mozilla.com>
2022-03-22 00:28:23 +0000

    Bug 1722777 - Test. r=bytesized
    Depends on D140429
    
    Differential Revision: https://phabricator.services.mozilla.com/D141069

--- toolkit/components/backgroundtasks/tests/xpcshell/test_backgroundtask_deletes_profile.js
+++ toolkit/components/backgroundtasks/tests/xpcshell/test_backgroundtask_deletes_profile.js
@@ -31,3 +31,97 @@ add_task(async function test_backgroundtask_deletes_profile() {
     `Temporary profile directory does not exist: ${profile}`
   );
 });
+
+let c = {
+  // See note about macOS and temporary directories below.
+  skip_if: () => AppConstants.platform == "macosx",
+};
+
+add_task(c, async function test_backgroundtask_cleans_up_stale_profiles() {
+  // Background tasks shutdown and removal raced with creating files in the
+  // temporary profile directory, leaving stale profile remnants on disk.  We
+  // try to incrementally clean up such remnants.  This test verifies that clean
+  // up succeeds as expected.  In the future we might test that locked profiles
+  // are ignored and that a failure during a removal does not stop other
+  // removals from being processed, etc.
+
+  // Background task temporary profiles are created in the OS temporary
+  // directory.  On Windows, we can put the temporary directory in a
+  // testing-specific location by setting TMP, per
+  // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-gettemppathw.
+  // On Linux, this works as well: see
+  // [GetSpecialSystemDirectory](https://searchfox.org/mozilla-central/source/xpcom/io/SpecialSystemDirectory.cpp).
+  // On macOS, we can't set the temporary directory in this manner so we skip
+  // this test.
+  let tmp = do_get_profile();
+  tmp.append("Temp");
+  tmp.createUnique(Ci.nsIFile.DIRECTORY_TYPE, 0o700);
+
+  // Get the "profile prefix" that the clean up process generates by fishing the
+  // profile directory from the testing task that provides the profile path.
+  let sentinel = Services.uuid.generateUUID().toString();
+  sentinel = sentinel.substring(1, sentinel.length - 1);
+
+  let stdoutLines = [];
+  let exitCode = await do_backgroundtask("unique_profile", {
+    extraArgs: [sentinel],
+    extraEnv: { TMP: tmp.path },
+    onStdoutLine: line => stdoutLines.push(line),
+  });
+  Assert.equal(0, exitCode);
+
+  let profile;
+  for (let line of stdoutLines) {
+    if (line.includes(sentinel)) {
+      let info = JSON.parse(line.split(sentinel)[1]);
+      profile = info[1];
+    }
+  }
+  Assert.ok(!!profile, `Found profile: ${profile}`);
+
+  Assert.ok(
+    profile.startsWith(tmp.path),
+    "Profile was created in test-specific temporary path"
+  );
+
+  let dir = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
+  dir.initWithPath(profile);
+
+  // Create a few "stale" profile directories.
+  let ps = [];
+  for (let i = 0; i < 3; i++) {
+    let p = dir.parent.clone();
+    p.append(`${dir.leafName}_${i}`);
+    p.create(Ci.nsIFile.DIRECTORY_TYPE, 0o700);
+    ps.push(p);
+
+    let f = p.clone();
+    f.append(`file_${i}`);
+    await IOUtils.writeJSON(f.path, {});
+  }
+
+  // Display logging for ease of debugging.
+  let moz_log = "BackgroundTasks:5";
+  let env = Cc["@mozilla.org/process/environment;1"].getService(
+    Ci.nsIEnvironment
+  );
+  if (env.exists("MOZ_LOG")) {
+    moz_log += `,${env.get("MOZ_LOG")}`;
+  }
+
+  // Invoke the task.
+  exitCode = await do_backgroundtask("unique_profile", {
+    extraArgs: [sentinel],
+    extraEnv: { TMP: tmp.path, MOZ_LOG: moz_log },
+  });
+  Assert.equal(0, exitCode);
+
+  // Verify none of the stale profile directories persist.
+  let es = []; // Expecteds.
+  let as = []; // Actuals.
+  for (let p of ps) {
+    as.push(!p.exists());
+    es.push(true);
+  }
+  Assert.deepEqual(es, as, "All stale profile directories were cleaned up.");
+});
