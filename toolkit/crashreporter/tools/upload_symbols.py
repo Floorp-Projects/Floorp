271beeebfe3d80e65d12acfef18ca454b73d05f6
Mitchell Hentges <mhentges@mozilla.com>
2022-03-22 19:44:13 +0000

    Bug 1759084: Don't redundantly activate site in upload_symbols.py r=ahal
    `upload_symbols.py` is always executed within a Mach site (otherwise
    activating the `common` command site would fail), therefore
    re-activating `common` is redundant.
    
    Note that `zstandard` is available from the system python, but only
    some upload-symbols jobs were using it - the "use system python"
    inference was not finding `zstandard` on Linux workers because they
    didn't have `pip` installed. So, I've explicitly set the "native
    packages source" to "SYSTEM" to force them to find and use it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D140982

--- toolkit/crashreporter/tools/upload_symbols.py
+++ toolkit/crashreporter/tools/upload_symbols.py
@@ -23,7 +23,6 @@ import redo
 import requests
 import shutil
 import sys
-from mozbuild.base import MozbuildObject
 
 log = logging.getLogger("upload-symbols")
 log.setLevel(logging.INFO)
@@ -57,9 +56,6 @@ def get_taskcluster_secret(secret_name):
 
 
 def main():
-    config = MozbuildObject.from_environment()
-    config.activate_virtualenv()
-
     logging.basicConfig()
     parser = argparse.ArgumentParser(
         description="Upload symbols in ZIP using token from Taskcluster secrets service."
@@ -90,8 +86,6 @@ def main():
         import gzip
         import tarfile
         import tempfile
-
-        config._ensure_zstd()
         import zstandard
 
         def prepare_zip_from(archive, tmpdir):
