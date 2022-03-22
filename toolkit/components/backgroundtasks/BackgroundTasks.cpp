777b32482d74bb91cf3edf09c3fa1e7901d6b9ea
Nicholas Rishel <nrishel@mozilla.com>
2022-03-22 00:28:23 +0000

    Bug 1722777 - Incrementally remove stale temporary profiles from background tasks which match the name of the current background task profile (before adding unique identifier). r=nalexander,mossop
    Depends on D140911
    
    Differential Revision: https://phabricator.services.mozilla.com/D140429

--- toolkit/components/backgroundtasks/BackgroundTasks.cpp
+++ toolkit/components/backgroundtasks/BackgroundTasks.cpp
@@ -8,6 +8,7 @@
 #include "nsIFile.h"
 #include "nsImportModule.h"
 #include "nsPrintfCString.h"
+#include "nsProfileLock.h"
 #include "nsXULAppAPI.h"
 #include "SpecialSystemDirectory.h"
 
@@ -170,10 +171,21 @@ nsresult BackgroundTasks::CreateTemporaryProfileDirectoryImpl(
     rv = GetSpecialSystemDirectory(OS_TemporaryDirectory, getter_AddRefs(file));
     NS_ENSURE_SUCCESS(rv, rv);
 
+    nsCString profilePrefix =
+        nsPrintfCString("%sBackgroundTask-%s-%s", MOZ_APP_VENDOR,
+                        aInstallHash.get(), mBackgroundTask.ref().get());
+
+    // Windows file cleanup is unreliable, so let's take a moment to clean up
+    // any prior background task profiles. We can continue if there was an error
+    // as creating a new temporary profile does not require cleaning up the old.
+    rv = RemoveStaleTemporaryProfileDirectories(file, profilePrefix);
+    if (NS_WARN_IF(NS_FAILED(rv))) {
+      MOZ_LOG(sBackgroundTasksLog, mozilla::LogLevel::Warning,
+              ("Error cleaning up stale temporary profile directories."));
+    }
+
     // The base path is /tmp/[vendor]BackgroundTask-[pathHash]-[taskName].
-    rv = file->AppendNative(nsPrintfCString("%sBackgroundTask-%s-%s",
-                                            MOZ_APP_VENDOR, aInstallHash.get(),
-                                            mBackgroundTask.ref().get()));
+    rv = file->AppendNative(profilePrefix);
     NS_ENSURE_SUCCESS(rv, rv);
 
     // Create a unique profile directory.  This can fail if there are too many
@@ -186,6 +198,73 @@ nsresult BackgroundTasks::CreateTemporaryProfileDirectoryImpl(
   }
 
   file.forget(aFile);
+  return NS_OK;
+}
+
+nsresult BackgroundTasks::RemoveStaleTemporaryProfileDirectories(
+    nsIFile* const aRoot, const nsCString& aPrefix) {
+  nsCOMPtr<nsIDirectoryEnumerator> entries;
+  nsresult rv = aRoot->GetDirectoryEntries(getter_AddRefs(entries));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<nsIFile> entry;
+  int removedProfiles = 0;
+  // Limit the number of stale temporary profiles we clean up so that we don't
+  // timeout the background task.
+  const int kMaxRemovedProfiles = 5;
+
+  // Loop over the temporary directory entries, deleting folders matching our
+  // profile prefix. Continue if there is an error interacting with the entry to
+  // more reliably make progress on cleaning up stale temporary profiles.
+  while (removedProfiles < kMaxRemovedProfiles &&
+         NS_SUCCEEDED(rv = entries->GetNextFile(getter_AddRefs(entry))) &&
+         entry) {
+    nsCString entryName;
+    rv = entry->GetNativeLeafName(entryName);
+    if (NS_FAILED(rv)) {
+      continue;
+    }
+
+    // Find profile folders matching our prefix.
+    if (aPrefix.Compare(entryName.get(), false, aPrefix.Length()) != 0) {
+      continue;
+    }
+
+    // Check if the profile is locked. If successful drop the lock so we can
+    // delete the folder. Background tasks' temporary profiles are not reused or
+    // remembered once released, so we don't need to hold this lock while
+    // deleting it.
+    nsProfileLock lock;
+    if (NS_FAILED(lock.Lock(entry, nullptr)) || NS_FAILED(lock.Unlock())) {
+      continue;
+    }
+
+    rv = entry->Remove(true);
+    if (NS_FAILED(rv)) {
+      if (MOZ_LOG_TEST(sBackgroundTasksLog, mozilla::LogLevel::Warning)) {
+        nsAutoString path;
+        if (NS_SUCCEEDED(entry->GetPath(path))) {
+          MOZ_LOG(sBackgroundTasksLog, mozilla::LogLevel::Warning,
+                  ("Error removing stale temporary profile directory: %s",
+                   NS_LossyConvertUTF16toASCII(path).get()));
+        }
+      }
+
+      continue;
+    }
+
+    if (MOZ_LOG_TEST(sBackgroundTasksLog, mozilla::LogLevel::Info)) {
+      nsAutoString path;
+      if (NS_SUCCEEDED(entry->GetPath(path))) {
+        MOZ_LOG(sBackgroundTasksLog, mozilla::LogLevel::Info,
+                ("Removed stale temporary profile directory: %s",
+                 NS_LossyConvertUTF16toASCII(path).get()));
+      }
+    }
+
+    removedProfiles++;
+  }
+
   return NS_OK;
 }
 
