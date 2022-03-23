670ba005e8cec6fb2ae36446907992454b0bb19c
Serge Guelton <sguelton@redhat.com>
2022-03-23 15:36:29 +0000

    Bug 1759344 - Cleanup mozglue/ includes r=andi
    Differential Revision: https://phabricator.services.mozilla.com/D141460

--- mozglue/misc/ConditionVariable_posix.cpp
+++ mozglue/misc/ConditionVariable_posix.cpp
@@ -9,9 +9,7 @@
 
 #include <errno.h>
 #include <pthread.h>
-#include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
 
 #include "mozilla/PlatformConditionVariable.h"
 #include "mozilla/PlatformMutex.h"
