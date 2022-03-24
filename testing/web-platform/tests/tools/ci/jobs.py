ee335ec5372ff2e06d8e0cace07bf30d1d68d32b
Philip Jägenstedt <philip@foolip.org>
2022-03-24 12:02:55 +0000

    Bug 1758713 [wpt PR 33128] - Clarify the status of the CSS build system, a=testonly
    Automatic update from web-platform-tests
    Clarify the status of the CSS build system (#33128)
    
    --
    
    wpt-commits: 1df73d78186ca9350f71c03a5dd4a369e03c703e
    wpt-pr: 33128

--- testing/web-platform/tests/tools/ci/jobs.py
+++ testing/web-platform/tests/tools/ci/jobs.py
@@ -30,7 +30,6 @@ job_path_map = {
     "resources_unittest": ["resources/", "tools/"],
     "tools_unittest": ["tools/"],
     "wptrunner_unittest": ["tools/"],
-    "build_css": ["css/"],
     "update_built": ["update-built-tests\\.sh",
                      "conformance-checkers/",
                      "css/css-ui/",
