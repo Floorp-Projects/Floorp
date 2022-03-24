287d1c7f2e9be581206f7595b5eb6fdba896721f
Dana Keeler <dkeeler@mozilla.com>
2022-03-24 21:34:21 +0000

    Bug 1088140 - support RSA-PSS signatures on certificates in the certificate verifier r=jschanck
    Differential Revision: https://phabricator.services.mozilla.com/D141780

--- security/certverifier/NSSCertDBTrustDomain.h
+++ security/certverifier/NSSCertDBTrustDomain.h
@@ -176,6 +176,11 @@ class NSSCertDBTrustDomain : public mozilla::pkix::TrustDomain {
       mozilla::pkix::Input signature,
       mozilla::pkix::Input subjectPublicKeyInfo) override;
 
+  virtual Result VerifyRSAPSSSignedData(
+      mozilla::pkix::Input data, mozilla::pkix::DigestAlgorithm digestAlgorithm,
+      mozilla::pkix::Input signature,
+      mozilla::pkix::Input subjectPublicKeyInfo) override;
+
   virtual Result CheckECDSACurveIsAcceptable(
       mozilla::pkix::EndEntityOrCA endEntityOrCA,
       mozilla::pkix::NamedCurve curve) override;
--- security/manager/ssl/CSTrustDomain.h
+++ security/manager/ssl/CSTrustDomain.h
@@ -50,6 +50,10 @@ class CSTrustDomain final : public mozilla::pkix::TrustDomain {
       mozilla::pkix::Input data, mozilla::pkix::DigestAlgorithm digestAlgorithm,
       mozilla::pkix::Input signature,
       mozilla::pkix::Input subjectPublicKeyInfo) override;
+  virtual Result VerifyRSAPSSSignedData(
+      mozilla::pkix::Input data, mozilla::pkix::DigestAlgorithm digestAlgorithm,
+      mozilla::pkix::Input signature,
+      mozilla::pkix::Input subjectPublicKeyInfo) override;
   virtual Result CheckECDSACurveIsAcceptable(
       mozilla::pkix::EndEntityOrCA endEntityOrCA,
       mozilla::pkix::NamedCurve curve) override;
