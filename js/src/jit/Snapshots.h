e01668976a2c42f370cd62be52121a644befed5e
Jan de Mooij <jdemooij@mozilla.com>
2022-03-24 10:08:18 +0000

    Bug 1760989 part 4 - Use ResumeMode to determine resumeAfter. r=iain
    We no longer have to encode this flag separately because it's redundant with
    the ResumeMode value.
    
    Differential Revision: https://phabricator.services.mozilla.com/D141855

--- js/src/jit/Snapshots.h
+++ js/src/jit/Snapshots.h
@@ -402,7 +402,7 @@ class RecoverWriter {
   uint32_t instructionsWritten_;
 
  public:
-  SnapshotOffset startRecover(uint32_t instructionCount, bool resumeAfter);
+  SnapshotOffset startRecover(uint32_t instructionCount);
 
   void writeInstruction(const MNode* rp);
 
@@ -496,10 +496,6 @@ class RecoverReader {
   // Number of instruction read.
   uint32_t numInstructionsRead_;
 
-  // True if we need to resume after the Resume Point instruction of the
-  // innermost frame.
-  bool resumeAfter_;
-
   // Space is reserved as part of the RecoverReader to avoid allocations of
   // data which is needed to decode the current instruction.
   RInstructionStorage rawData_;
@@ -525,8 +521,6 @@ class RecoverReader {
   const RInstruction* instruction() const {
     return reinterpret_cast<const RInstruction*>(rawData_.addr());
   }
-
-  bool resumeAfter() const { return resumeAfter_; }
 };
 
 }  // namespace jit
