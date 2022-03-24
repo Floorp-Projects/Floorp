071a017e1f2b0dd91a4220870ce7ed1a893923d1
Brad Werth <bwerth@mozilla.com>
2022-03-24 18:19:50 +0000

    Bug 1751217 Part 4: Make macOS declare video-dynamic-range: high with new-enough OS. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D141056

--- widget/LookAndFeel.h
+++ widget/LookAndFeel.h
@@ -324,6 +324,15 @@ class LookAndFeel {
     /** GTK menu radius */
     GtkMenuRadius,
 
+    /**
+     * Corresponding to dynamic-range.
+     * https://drafts.csswg.org/mediaqueries-5/#dynamic-range
+     * 0: Standard
+     * 1: High
+     */
+    DynamicRange,
+    VideoDynamicRange,
+
     /*
      * Not an ID; used to define the range of valid IDs.  Must be last.
      */
