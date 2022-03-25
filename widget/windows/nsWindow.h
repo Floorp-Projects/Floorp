ed5c762d1a7b08d57a454b56025e48fa179b428d
donal meehan <dmeehan@mozilla.com>
2022-03-25 09:09:29 -0400

    Backed out changeset c598a609660e (bug 1758357) for causing bug 1761408

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -667,6 +667,7 @@ class nsWindow final : public nsBaseWidget {
   static TriStateBool sCanQuit;
   static nsWindow* sCurrentWindow;
   static BOOL sIsOleInitialized;
+  static HCURSOR sCustomHCursor;
   static Cursor sCurrentCursor;
   static bool sSwitchKeyboardLayout;
   static bool sJustGotDeactivate;
