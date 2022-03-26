4a82829f2172e9ad9254e175fb794a90f7cbadc5
すらーぷの妖精 <73892113+surapunoyousei@users.noreply.github.com>
2022-03-26 23:19:29 +0900

    Update background.js

--- browser/extensions/undo-closed-tab/background.js
+++ browser/extensions/undo-closed-tab/background.js
@@ -1,7 +1,18 @@
-'use strict';
-browser.browserAction.onClicked.addListener(tab => {
-	browser.sessions.getRecentlyClosed(sessionInfos => {
-				for (let i = 0; i < sessionInfos.length; i++) {
-			let sessionInfo = sessionInfos[i];
-			if (sessionInfo.tab && sessionInfo.tab.windowId === tab.windowId) {
-				if (sessionInfo.tab.sessionId != null) {chrome.sessions.restore(sessionInfo.tab.sessionId);}else{}break;}}})});
+				function restoreMostRecent(session) {
+					if (!session.length) {
+					  return;
+					}
+					let sessionInfo = session[0];
+					if (sessionInfo.tab) {
+					  browser.sessions.restore(sessionInfo.tab.sessionId);
+					} else {
+					  browser.sessions.restore(sessionInfo.window.sessionId);
+					}
+				  }
+
+				  browser.browserAction.onClicked.addListener(function() {
+					let gettingSessions = browser.sessions.getRecentlyClosed({
+					  maxResults: 1
+					});
+					gettingSessions.then(restoreMostRecent);
+				  });
