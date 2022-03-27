eb060d764fd775ea7ec4fc6add20be1b09149437
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2022-03-27 23:40:24 +0000

    Bug 1760222 - Detect scroll-linked effects only in the same refresh driver's timestamp. r=botond
    With this change, `has-scroll-linked-effect` flag won't persist so that we can
    avoid choosing either the one-frame delayed scroll offset or the latest scroll
    offset in cases where there's no longer effective scroll linked effect. It will
    mitigate scroll jittering.
    
    Differential Revision: https://phabricator.services.mozilla.com/D141457

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -5870,7 +5870,8 @@ void ScrollFrameHelper::FireScrollEvent() {
   // Fire viewport scroll events at the document (where they
   // will bubble to the window)
   mozilla::layers::ScrollLinkedEffectDetector detector(
-      content->GetComposedDoc());
+      content->GetComposedDoc(),
+      presContext->RefreshDriver()->MostRecentRefresh());
   if (mIsRoot) {
     if (RefPtr<Document> doc = content->GetUncomposedDoc()) {
       // TODO: Bug 1506441
