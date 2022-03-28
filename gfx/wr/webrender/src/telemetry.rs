210d60531ba97d1e551af4d8c69b276cca399a05
Bas Schouten <bschouten@mozilla.com>
2022-03-28 07:21:14 +0000

    Bug 1751693: Add telemetry probe to measure glyph rasterization time and the required plumbing for WR. r=nical
    Differential Revision: https://phabricator.services.mozilla.com/D140681

---
+++ gfx/wr/webrender/src/telemetry.rs
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use glean::TimerId;
+#[cfg(feature = "gecko")]
+use fog::metrics::wr;
+
+pub struct Telemetry;
+
+/// Defines the interface for hooking up an external telemetry reporter to WR.
+#[cfg(not(feature = "gecko"))]
+impl Telemetry {
+    // Start rasterize glyph time collection
+    pub fn start_rasterize_glyphs_time() -> TimerId { return 0; }
+    // End rasterize glyph time collection
+    pub fn stop_and_accumulate_rasterize_glyphs_time(_id: TimerId) { }
+}
+
+#[cfg(feature = "gecko")]
+impl Telemetry {
+    pub fn start_rasterize_glyphs_time() -> TimerId { wr::rasterize_glyphs_time.start() }
+    pub fn stop_and_accumulate_rasterize_glyphs_time(id: TimerId) { wr::rasterize_glyphs_time.stop_and_accumulate(id); }
+}
