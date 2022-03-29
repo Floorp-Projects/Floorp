d853a2275f5cec7dcf468665fa343f6f6a80903f
John Schanck <jschanck@mozilla.com>
2022-03-29 18:48:37 +0000

    Bug 1607588 - Remove suppression for NSS shutdown race. r=decoder
    Differential Revision: https://phabricator.services.mozilla.com/D142351

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -263,10 +263,6 @@ extern "C" const char* __tsan_default_suppressions() {
          "race:VRShMem::PullBrowserState\n"
          "race:VRShMem::PushBrowserState\n"
 
-         // Bug 1607588
-         "race:nssToken_Destroy\n"
-         "race:nssSlot_GetToken\n"
-
          // Bug 1682951
          "race:storage::Connection::Release\n"
 
