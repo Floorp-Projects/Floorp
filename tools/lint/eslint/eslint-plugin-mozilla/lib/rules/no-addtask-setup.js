35d2a4ca1219c0f8536bc3fe7221ea8e4aca3f75
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2022-03-30 08:18:51 +0000

    Bug 1753696 - use an eslint rule to switch to add_setup(), r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D139542

---
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/rules/no-addtask-setup.js
@@ -0,0 +1,51 @@
+/**
+ * @fileoverview Reject `add_task(async function setup` or similar patterns in
+ * favour of add_setup.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+"use strict";
+
+// -----------------------------------------------------------------------------
+// Rule Definition
+// -----------------------------------------------------------------------------
+
+module.exports = {
+  meta: {
+    type: "suggestion",
+    fixable: "code",
+  },
+  create(context) {
+    return {
+      "Program > ExpressionStatement > CallExpression": function(node) {
+        let callee = node.callee;
+        if (callee.type === "Identifier" && callee.name === "add_task") {
+          let arg = node.arguments[0];
+          if (
+            arg.type !== "FunctionExpression" ||
+            !arg.id ||
+            arg.id.name !== "setup"
+          ) {
+            return;
+          }
+          context.report({
+            node,
+            message:
+              "Do not use add_task() for setup, use add_setup() instead.",
+            fix: fixer => {
+              let range = [node.callee.range[0], arg.id.range[1]];
+              let asyncOrNot = arg.async ? "async " : "";
+              return fixer.replaceTextRange(
+                range,
+                `add_setup(${asyncOrNot}function`
+              );
+            },
+          });
+        }
+      },
+    };
+  },
+};
