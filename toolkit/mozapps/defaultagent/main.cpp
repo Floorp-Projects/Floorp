8042ae553a71e035ca764565199df111785b4069
Nick Alexander <nalexander@mozilla.com>
2022-03-30 03:41:59 +0000

    Bug 1762100 - Part 1: Accommodate arbitrary ProgID roots in `set-default-browser-user-choice`. r=bhearsum
    This anticipates separating the FirefoxPDF-... handler from the
    FirefoxHTML-... handler.
    
    Differential Revision: https://phabricator.services.mozilla.com/D142301

--- toolkit/mozapps/defaultagent/main.cpp
+++ toolkit/mozapps/defaultagent/main.cpp
@@ -252,10 +252,12 @@ static bool CheckIfAppRanRecently(bool* aResult) {
 //   Actually performs the default agent task, which currently means generating
 //   and sending our telemetry ping and possibly showing a notification to the
 //   user if their browser has switched from Firefox to Edge with Blink.
-// set-default-browser-user-choice [app-user-model-id] [[.file1] ...]
+// set-default-browser-user-choice [app-user-model-id] [[.file1 ProgIDRoot1]
+// ...]
 //   Set the default browser via the UserChoice registry keys.  Additional
 //   optional file extensions to register can be specified as additional
-//   arguments.
+//   argument pairs: the first element is the file extension, the second element
+//   is the root of a ProgID, which will be suffixed with `-$AUMI`.
 int wmain(int argc, wchar_t** argv) {
   if (argc < 2 || !argv[1]) {
     return E_INVALIDARG;
