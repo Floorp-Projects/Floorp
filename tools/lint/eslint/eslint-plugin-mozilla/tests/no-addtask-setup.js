43d6391aa2b3c3595aa724b3aad086f40f80b2d1
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2022-03-30 08:18:51 +0000

    Bug 1753696 - use an eslint rule to switch to add_setup(), r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D139542

---
+++ tools/lint/eslint/eslint-plugin-mozilla/tests/no-addtask-setup.js
@@ -0,0 +1,67 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+var rule = require("../lib/rules/no-addtask-setup");
+var RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 9 } });
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+function callError(message) {
+  return [{ message, type: "CallExpression" }];
+}
+
+ruleTester.run("no-addtask-setup", rule, {
+  valid: [
+    "add_task(function() {});",
+    "add_task(function () {});",
+    "add_task(function foo() {});",
+    "add_task(async function() {});",
+    "add_task(async function () {});",
+    "add_task(async function foo() {});",
+    "something(function setup() {});",
+    "something(async function setup() {});",
+    "add_task(setup);",
+    "add_task(setup => {});",
+    "add_task(async setup => {});",
+  ],
+  invalid: [
+    {
+      code: "add_task(function setup() {});",
+      output: "add_setup(function() {});",
+      errors: callError(
+        "Do not use add_task() for setup, use add_setup() instead."
+      ),
+    },
+    {
+      code: "add_task(function setup () {});",
+      output: "add_setup(function () {});",
+      errors: callError(
+        "Do not use add_task() for setup, use add_setup() instead."
+      ),
+    },
+    {
+      code: "add_task(async function setup() {});",
+      output: "add_setup(async function() {});",
+      errors: callError(
+        "Do not use add_task() for setup, use add_setup() instead."
+      ),
+    },
+    {
+      code: "add_task(async function setup () {});",
+      output: "add_setup(async function () {});",
+      errors: callError(
+        "Do not use add_task() for setup, use add_setup() instead."
+      ),
+    },
+  ],
+});
