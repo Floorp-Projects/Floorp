8c157e4493aea12e558ac393bda2bdd1b69eb087
Marian-Vasile Laza <mlaza@mozilla.com>
2022-03-30 11:18:07 -0700

    Backed out 2 changesets (bug 1749928) for causing webgl failures. CLOSED TREE
    Backed out changeset d654d3fcfa8e (bug 1749928)
    Backed out changeset 49e91c2434f7 (bug 1749928)

--- testing/talos/talos/ffsetup.py
+++ testing/talos/talos/ffsetup.py
@@ -333,7 +333,7 @@ class FFSetup(object):
         self._init_env()
         self._init_profile()
         try:
-            if not self.debug_mode and not self.test_config["name"].startswith("damp"):
+            if not self.debug_mode and self.test_config["name"] != "damp":
                 self._run_profile()
         except BaseException:
             self.clean()
