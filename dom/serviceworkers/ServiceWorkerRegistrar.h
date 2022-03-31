7cd0bbc3b690dbfdc4cca7ecae4dcc20e492aade
Randell Jesup <rjesup@wgate.com>
2022-03-31 14:08:13 +0000

    Bug 1760662 - DOM worker thread-safety annotations r=dom-worker-reviewers,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D130594

--- dom/serviceworkers/ServiceWorkerRegistrar.h
+++ dom/serviceworkers/ServiceWorkerRegistrar.h
@@ -75,8 +75,8 @@ class ServiceWorkerRegistrar : public nsIObserver,
   nsresult WriteData(const nsTArray<ServiceWorkerRegistrationData>& aData);
   void DeleteData();
 
-  void RegisterServiceWorkerInternal(
-      const ServiceWorkerRegistrationData& aData);
+  void RegisterServiceWorkerInternal(const ServiceWorkerRegistrationData& aData)
+      REQUIRES(mMonitor);
 
   ServiceWorkerRegistrar();
   virtual ~ServiceWorkerRegistrar();
@@ -97,12 +97,14 @@ class ServiceWorkerRegistrar : public nsIObserver,
   bool IsSupportedVersion(const nsACString& aVersion) const;
 
  protected:
-  mozilla::Monitor mMonitor MOZ_UNANNOTATED;
+  mozilla::Monitor mMonitor;
 
   // protected by mMonitor.
-  nsCOMPtr<nsIFile> mProfileDir;
-  nsTArray<ServiceWorkerRegistrationData> mData;
-  bool mDataLoaded;
+  nsCOMPtr<nsIFile> mProfileDir GUARDED_BY(mMonitor);
+  // Read on mainthread, modified on background thread EXCEPT for
+  // ReloadDataForTest() AND for gtest, which modifies this on MainThread.
+  nsTArray<ServiceWorkerRegistrationData> mData GUARDED_BY(mMonitor);
+  bool mDataLoaded GUARDED_BY(mMonitor);
 
   // PBackground thread only
   uint32_t mDataGeneration;
