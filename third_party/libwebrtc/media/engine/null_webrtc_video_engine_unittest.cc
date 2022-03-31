a974dad072c99504e4830a48a63b4acb3db44393
Michael Froman <mfroman@mozilla.com>
2022-03-31 12:58:09 -0500

    Bug 1766646 - Vendor libwebrtc from 09ceed2165
    Upstream commit: https://webrtc.googlesource.com/src/+/09ceed2165137c4bea4e02e8d3db31970d0bf273
        Async audio processing API
    
        API to injecting a heavy audio processing operation into WebRTC audio capture pipeline
    
        Bug: webrtc:12003
        Change-Id: I9f6f58f468bd84efd0a9d53d703db6229a03959e
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/165788
        Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
        Commit-Queue: Olga Sharonova <olka@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32291}

--- third_party/libwebrtc/media/engine/null_webrtc_video_engine_unittest.cc
+++ third_party/libwebrtc/media/engine/null_webrtc_video_engine_unittest.cc
@@ -37,7 +37,7 @@ TEST(NullWebRtcVideoEngineTest, CheckInterface) {
       task_queue_factory.get(), adm,
       webrtc::MockAudioEncoderFactory::CreateUnusedFactory(),
       webrtc::MockAudioDecoderFactory::CreateUnusedFactory(), nullptr,
-      webrtc::AudioProcessingBuilder().Create(), trials);
+      webrtc::AudioProcessingBuilder().Create(), nullptr, trials);
 
   CompositeMediaEngine engine(std::move(audio_engine),
                               std::make_unique<NullWebRtcVideoEngine>());
