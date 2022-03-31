b2d66420679c88aab687202ba521f4e1ee7425e9
Randell Jesup <rjesup@wgate.com>
2022-03-31 14:08:13 +0000

    Bug 1760662 - DOM worker thread-safety annotations r=dom-worker-reviewers,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D130594

--- dom/serviceworkers/test/gtest/TestReadWrite.cpp
+++ dom/serviceworkers/test/gtest/TestReadWrite.cpp
@@ -37,7 +37,9 @@ class ServiceWorkerRegistrarTest : public ServiceWorkerRegistrar {
   }
 
   nsresult TestReadData() { return ReadData(); }
-  nsresult TestWriteData() { return WriteData(mData); }
+  nsresult TestWriteData() NO_THREAD_SAFETY_ANALYSIS {
+    return WriteData(mData);
+  }
   void TestDeleteData() { DeleteData(); }
 
   void TestRegisterServiceWorker(const ServiceWorkerRegistrationData& aData) {
