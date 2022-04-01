af2291acee7b78c2d69959e830dbd9cf34c7f639
Dan Minor <dminor@mozilla.com>
2022-04-01 18:57:40 +0000

    Bug 1761464 - Pass nsCString to RLBoxHunspell::Create; r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D142696

--- extensions/spellcheck/hunspell/glue/RLBoxHunspell.cpp
+++ extensions/spellcheck/hunspell/glue/RLBoxHunspell.cpp
@@ -15,17 +15,6 @@
 using namespace rlbox;
 using namespace mozilla;
 
-// Helper function for allocating and copying nsAutoCString into sandbox
-static tainted_hunspell<char*> allocStrInSandbox(
-    rlbox_sandbox_hunspell& aSandbox, const nsAutoCString& str) {
-  size_t size = str.Length() + 1;
-  tainted_hunspell<char*> t_str = aSandbox.malloc_in_sandbox<char>(size);
-  if (t_str) {
-    rlbox::memcpy(aSandbox, t_str, str.get(), size);
-  }
-  return t_str;
-}
-
 // Helper function for allocating and copying std::string into sandbox
 static tainted_hunspell<char*> allocStrInSandbox(
     rlbox_sandbox_hunspell& aSandbox, const std::string& str) {
@@ -38,8 +27,8 @@ static tainted_hunspell<char*> allocStrInSandbox(
 }
 
 /* static */
-RLBoxHunspell* RLBoxHunspell::Create(const nsAutoCString& affpath,
-                                     const nsAutoCString& dpath) {
+RLBoxHunspell* RLBoxHunspell::Create(const nsCString& affpath,
+                                     const nsCString& dpath) {
   MOZ_DIAGNOSTIC_ASSERT(NS_IsMainThread());
 
   mozilla::UniquePtr<rlbox_sandbox_hunspell, RLBoxDeleter> sandbox(
@@ -97,7 +86,7 @@ RLBoxHunspell* RLBoxHunspell::Create(const nsAutoCString& affpath,
 
 RLBoxHunspell::RLBoxHunspell(
     mozilla::UniquePtr<rlbox_sandbox_hunspell, RLBoxDeleter> aSandbox,
-    const nsAutoCString& affpath, const nsAutoCString& dpath)
+    const nsCString& affpath, const nsCString& dpath)
     : mSandbox(std::move(aSandbox)), mHandle(nullptr) {
   // Register callbacks
   mCreateFilemgr =
@@ -121,10 +110,11 @@ RLBoxHunspell::RLBoxHunspell(
   // Copy the affpath and dpath into the sandbox
   // These allocations should definitely succeed as these are first allocations
   // inside the sandbox.
-  tainted_hunspell<char*> t_affpath = allocStrInSandbox(*mSandbox, affpath);
+  tainted_hunspell<char*> t_affpath =
+      allocStrInSandbox(*mSandbox, affpath.get());
   MOZ_RELEASE_ASSERT(t_affpath);
 
-  tainted_hunspell<char*> t_dpath = allocStrInSandbox(*mSandbox, dpath);
+  tainted_hunspell<char*> t_dpath = allocStrInSandbox(*mSandbox, dpath.get());
   MOZ_RELEASE_ASSERT(t_dpath);
 
   // Create handle
