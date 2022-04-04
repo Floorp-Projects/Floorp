969b94a01d36de68dee521b9516b6218fa34ad7c
Andrew Osmond <aosmond@mozilla.com>
2022-04-04 16:42:02 +0000

    Bug 1753302 - Part 3. Add basic worker thread tests for WebGPU. r=gfx-reviewers,jgilbert
    Differential Revision: https://phabricator.services.mozilla.com/D141503

---
+++ dom/webgpu/mochitest/worker_wrapper.js
@@ -0,0 +1,33 @@
+ok(
+  SpecialPowers.getBoolPref("dom.webgpu.enabled"),
+  "WebGPU pref should be enabled."
+);
+ok(
+  SpecialPowers.getBoolPref("gfx.offscreencanvas.enabled"),
+  "OffscreenCanvas pref should be enabled."
+);
+SimpleTest.waitForExplicitFinish();
+
+const workerWrapperFunc = async function(worker_path, data, transfer) {
+  const worker = new Worker(worker_path);
+
+  const results = new Promise((resolve, reject) => {
+    worker.addEventListener("message", event => {
+      resolve(event.data);
+    });
+  });
+
+  worker.postMessage(data, transfer);
+  for (const result of await results) {
+    ok(result.value, result.message);
+  }
+};
+
+async function runWorkerTest(worker_path, data, transfer) {
+  try {
+    await workerWrapperFunc(worker_path, data, transfer);
+  } catch (e) {
+    ok(false, "Unhandled exception " + e);
+  }
+  SimpleTest.finish();
+}
