5d6b6caf5a62610ad6a25cf0914b8822d42ac64c
Stephen A Pohl <spohl.mozilla.bugs@gmail.com>
2022-04-04 17:30:10 +0000

    Bug 1745788: Add telemetry for situations when Firefox is running under App Translocation on macOS. r=mstange
    Differential Revision: https://phabricator.services.mozilla.com/D142109

--- toolkit/xre/MacRunFromDmgUtils.mm
+++ toolkit/xre/MacRunFromDmgUtils.mm
@@ -469,6 +469,8 @@ bool MaybeInstallAndRelaunch() {
     }
 
     Telemetry::ScalarSet(Telemetry::ScalarID::STARTUP_IS_RUN_FROM_DMG, isFromDmg);
+    Telemetry::ScalarSet(Telemetry::ScalarID::STARTUP_IS_RUN_FROM_APP_TRANSLOCATED_LOCATION,
+                         isTranslocated);
 
     if (!isFromDmg && !isTranslocated) {
       if (getenv("MOZ_INSTALLED_AND_RELAUNCHED_FROM_DMG")) {
