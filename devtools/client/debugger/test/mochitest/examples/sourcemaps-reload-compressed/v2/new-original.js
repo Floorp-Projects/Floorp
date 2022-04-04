e168059d8a59093cf3105c539aed5e009deabc28
Alexandre Poirot <poirot.alex@gmail.com>
2022-04-04 13:32:28 +0000

    Bug 1761921 - [devtools] Clarify the integration test page with many comments. r=bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D142318

--- devtools/client/debugger/test/mochitest/examples/sourcemaps-reload-compressed/v2/new-original.js
+++ devtools/client/debugger/test/mochitest/examples/sourcemaps-reload-compressed/v2/new-original.js
@@ -1,4 +1,4 @@
-// Pad the name with "_" in order to match the same length as removedOriginal
+// Pad the name with "_" in order to match the same length as v1/removed-original.js
 // and set the column breakpoints on the same column!
 window.____newOriginal = function ____newOriginal() {
   console.log("New original");
