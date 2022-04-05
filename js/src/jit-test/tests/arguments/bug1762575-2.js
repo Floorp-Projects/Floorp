fbd1fcd0f0427f3f52d181733985f1ba00cf7e8f
André Bargull <andre.bargull@gmail.com>
2022-04-05 16:15:25 +0000

    Bug 1762575 - Part 1: Use slow-path when any arguments element is overridden. r=iain
    `UnmappedArgumentsObject` doesn't mark an element as deleted when it is
    redefined through `Object.defineProperty()`, that means the current element
    value can only be read through `ArgumentsObject::element()` when the
    `ELEMENT_OVERRIDDEN_BIT` isn't set.
    
    Alternatively to this patch, we could also add a `ObjectOps::defineProperty`
    hook for `UnmappedArgumentsObject` to properly update the element value instead
    of redefining the property. But reassigning argument elements should be
    relatively uncommon, so it should be fine to just always take the slow-path
    when `ELEMENT_OVERRIDDEN_BIT` is set.
    
    The tests in "bug1762575-1.js" work on mapped arguments objects and already
    pass without this patch. "bug1762575-2.js" uses mapped arguments objects and
    only passes with this patch. "bug1762575-3.js" tests some implementation
    details when arguments object keep a strong reference to their elements.
    
    Differential Revision: https://phabricator.services.mozilla.com/D142666

---
+++ js/src/jit-test/tests/arguments/bug1762575-2.js
@@ -0,0 +1,50 @@
+// Tests on unmapped arguments objects.
+
+function ArraySlice() {
+  "use strict";
+  Object.defineProperty(arguments, 0, {value: 1});
+  var result = Array.prototype.slice.call(arguments);
+  assertEq(result[0], 1);
+}
+ArraySlice(0);
+
+function ArrayShift() {
+  "use strict";
+  Object.defineProperty(arguments, 0, {value: 1});
+  var result = Array.prototype.shift.call(arguments);
+  assertEq(result, 1);
+}
+ArrayShift(0);
+
+function ArrayPop() {
+  "use strict";
+  Object.defineProperty(arguments, 0, {value: 1});
+  var result = Array.prototype.pop.call(arguments);
+  assertEq(result, 1);
+}
+ArrayPop(0);
+
+function ArrayJoin() {
+  "use strict";
+  Object.defineProperty(arguments, 0, {value: 1});
+  var result = Array.prototype.join.call(arguments);
+  assertEq(result, "1");
+}
+ArrayJoin(0);
+
+function ArrayIncludes() {
+  "use strict";
+  Object.defineProperty(arguments, 0, {value: 1});
+  var result = Array.prototype.includes.call(arguments, 1);
+  assertEq(result, true);
+}
+ArrayIncludes(0);
+
+function FunctionApply() {
+  "use strict";
+  Object.defineProperty(arguments, 0, {value: 1});
+  var id = x => x;
+  var result = id.apply(null, arguments);
+  assertEq(result, 1);
+}
+FunctionApply(0);
