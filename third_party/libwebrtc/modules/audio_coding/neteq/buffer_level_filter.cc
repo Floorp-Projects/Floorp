d7f64b57803be676a7adb3b7ddb12c559998e963
Michael Froman <mfroman@mozilla.com>
2022-04-05 13:06:07 -0500

    Bug 1766646 - Vendor libwebrtc from ff9f6461b6
    Upstream commit: https://webrtc.googlesource.com/src/+/ff9f6461b60c568ba13d2c7253d3b541c2117c9e
        Revert "Reland "Refactor NetEq delay manager logic.""
    
        This reverts commit 2a7c57c34f323ee1977f6e7809ee23bfcf9a7459.
    
        Reason for revert: unexpected big changes in behavior.
    
        Original change's description:
        > Reland "Refactor NetEq delay manager logic."
        >
        > This is a reland of f8e62fcb14e37a5be4f1e4f599d34c8483fea8e9
        >
        > Original change's description:
        > > Refactor NetEq delay manager logic.
        > >
        > > - Removes dependence on sequence number for calculating target delay.
        > > - Changes target delay unit to milliseconds instead of number of
        > >   packets.
        > > - Moves acceleration/preemptive expand thresholds to decision logic.
        > >   Tests for this will be added in a follow up cl.
        > >
        > > Bug: webrtc:10333
        > > Change-Id: If690aae4abf41ef1d9353f0ff01fb7d121cf8a26
        > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/186265
        > > Commit-Queue: Jakob Ivarsson <jakobi@webrtc.org>
        > > Reviewed-by: Ivo Creusen <ivoc@webrtc.org>
        > > Cr-Commit-Position: refs/heads/master@{#32326}
        >
        > Bug: webrtc:10333
        > Change-Id: Iad5e7063f63b84762959ee5b412f5f14a7b2cd06
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/186943
        > Commit-Queue: Jakob Ivarsson <jakobi@webrtc.org>
        > Reviewed-by: Ivo Creusen <ivoc@webrtc.org>
        > Cr-Commit-Position: refs/heads/master@{#32332}
    
        TBR=ivoc@webrtc.org,jakobi@webrtc.org
    
        Change-Id: Iffda0e8a7b647392d8dfc6724d49439fa13d71b2
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Bug: webrtc:10333
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/187100
        Reviewed-by: Jakob Ivarsson <jakobi@webrtc.org>
        Commit-Queue: Jakob Ivarsson <jakobi@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32341}

--- third_party/libwebrtc/modules/audio_coding/neteq/buffer_level_filter.cc
+++ third_party/libwebrtc/modules/audio_coding/neteq/buffer_level_filter.cc
@@ -45,12 +45,12 @@ void BufferLevelFilter::Update(size_t buffer_size_samples,
       filtered_current_level - (int64_t{time_stretched_samples} * (1 << 8))));
 }
 
-void BufferLevelFilter::SetTargetBufferLevel(int target_buffer_level_ms) {
-  if (target_buffer_level_ms <= 20) {
+void BufferLevelFilter::SetTargetBufferLevel(int target_buffer_level) {
+  if (target_buffer_level <= 1) {
     level_factor_ = 251;
-  } else if (target_buffer_level_ms <= 60) {
+  } else if (target_buffer_level <= 3) {
     level_factor_ = 252;
-  } else if (target_buffer_level_ms <= 140) {
+  } else if (target_buffer_level <= 7) {
     level_factor_ = 253;
   } else {
     level_factor_ = 254;
