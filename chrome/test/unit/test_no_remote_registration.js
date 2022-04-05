81c1e273fd25816c5389848b832f0cf9b65dc977
Karnik Kanojia <karnikk1406120@gmail.com>
2022-04-05 20:39:36 +0000

    Bug 1763000 - Fix ESLint rule warnings for no-unused-vars in caps/tests/unit/test_no_remote_registration.js. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D142899

--- chrome/test/unit/test_no_remote_registration.js
+++ chrome/test/unit/test_no_remote_registration.js
@@ -3,7 +3,6 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-/* globals newAppInfo */
 
 var manifests = [do_get_file("data/test_no_remote_registration.manifest")];
 registerManifests(manifests);
