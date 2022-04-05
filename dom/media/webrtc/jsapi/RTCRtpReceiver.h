e2ececd04443e89621a6f596910b6d7a0ad875d3
Byron Campen <docfaraday@gmail.com>
2022-04-05 18:39:14 +0000

    Bug 1616937: Improve error handling during conduit updates. r=mjf
    Most of the errors we check for here should never happen, so crash/assert where
    appropriate. Also, handle the codec negotiation failure case outside of the
    conversion function, where we can log it better.
    
    Differential Revision: https://phabricator.services.mozilla.com/D142384

--- dom/media/webrtc/jsapi/RTCRtpReceiver.h
+++ dom/media/webrtc/jsapi/RTCRtpReceiver.h
@@ -92,7 +92,7 @@ class RTCRtpReceiver : public nsISupports, public nsWrapperCache {
 
   // This is called when we set an answer (ie; when the transport is finalized).
   void UpdateTransport();
-  nsresult UpdateConduit();
+  void UpdateConduit();
 
   // This is called when we set a remote description; may be an offer or answer.
   void UpdateStreams(StreamAssociationChanges* aChanges);
@@ -122,8 +122,8 @@ class RTCRtpReceiver : public nsISupports, public nsWrapperCache {
  private:
   virtual ~RTCRtpReceiver();
 
-  nsresult UpdateVideoConduit();
-  nsresult UpdateAudioConduit();
+  void UpdateVideoConduit();
+  void UpdateAudioConduit();
 
   std::string GetMid() const;
 
