5411b2a88c108a3fb4fef1e1eccafb08e900dd75
Alexandre Poirot <poirot.alex@gmail.com>
2022-04-06 07:17:40 +0000

    Bug 1762215 - [devtools] Migrate source actors reducer to a Map (instead of Resource API). r=bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D142542

--- devtools/client/debugger/src/selectors/source-actors.js
+++ devtools/client/debugger/src/selectors/source-actors.js
@@ -3,76 +3,103 @@
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
 import { asSettled } from "../utils/async-value";
-import {
-  hasResource,
-  getResource,
-  getMappedResource,
-  makeWeakQuery,
-  makeIdQuery,
-  makeReduceAllQuery,
-} from "../utils/resource";
 
-function resourceAsSourceActor({ breakableLines, ...sourceActor }) {
-  return sourceActor;
-}
-
+/**
+ * Tells if a given Source Actor is registered in the redux store
+ *
+ * @param {Object} state
+ * @param {String} id
+ *        Source Actor ID
+ * @return {Boolean}
+ */
 export function hasSourceActor(state, id) {
-  return hasResource(state.sourceActors, id);
-}
-
-export function getSourceActor(state, id) {
-  return getMappedResource(state.sourceActors, id, resourceAsSourceActor);
+  return state.sourceActors.has(id);
 }
 
 /**
- * Get all of the source actors for a set of IDs. Caches based on the identity
- * of "ids" when possible.
+ * Get the Source Actor object. See create.js:createSourceActor()
+ *
+ * @param {Object} state
+ * @param {String} id
+ *        Source Actor ID
+ * @return {Object}
+ *        The Source Actor object (if registered)
  */
-const querySourceActorsById = makeIdQuery(resourceAsSourceActor);
+export function getSourceActor(state, id) {
+  return state.sourceActors.get(id);
+}
 
 // Used by sources selectors
+/**
+ * Get many Source Actor objects. See create.js:createSourceActor()
+ *
+ * @param {Object} state
+ * @param {Array<String>} ids
+ *        List of Source Actor IDs
+ * @return {Array<Object>}
+ *        List of sources actors
+ */
 export function getSourceActors(state, ids) {
-  return querySourceActorsById(state.sourceActors, ids);
+  return ids.map(id => getSourceActor(state, id)).filter(source => !!source);
 }
 
-const querySourcesByThreadID = makeReduceAllQuery(
-  resourceAsSourceActor,
-  actors => {
-    return actors.reduce((acc, actor) => {
-      acc[actor.thread] = acc[actor.thread] || [];
-      acc[actor.thread].push(actor);
-      return acc;
-    }, {});
-  }
-);
 // Used by threads selectors
+/**
+ * Get all Source Actor objects for a given thread. See create.js:createSourceActor()
+ *
+ * @param {Object} state
+ * @param {Array<String>} ids
+ *        List of Thread IDs
+ * @return {Array<Object>}
+ */
 export function getSourceActorsForThread(state, ids) {
-  const sourcesByThread = querySourcesByThreadID(state.sourceActors);
-
-  let sources = [];
-  for (const id of Array.isArray(ids) ? ids : [ids]) {
-    sources = sources.concat(sourcesByThread[id] || []);
+  if (!Array.isArray(ids)) {
+    ids = [ids];
   }
-  return sources;
+  const actors = [];
+  for (const sourceActor of state.sourceActors.values()) {
+    if (ids.includes(sourceActor.thread)) {
+      actors.push(sourceActor);
+    }
+  }
+  return actors;
 }
 
+/**
+ * Get the list of all breakable lines for a given source actor.
+ *
+ * @param {Object} state
+ * @param {String} id
+ *        Source Actor ID
+ * @return {AsyncValue<Array<Number>>}
+ *        List of all the breakable lines.
+ */
 export function getSourceActorBreakableLines(state, id) {
-  const { breakableLines } = getResource(state.sourceActors, id);
+  const { breakableLines } = getSourceActor(state, id);
 
   return asSettled(breakableLines);
 }
 
 // Used by sources selectors
-export const getBreakableLinesForSourceActors = makeWeakQuery({
-  filter: (state, ids) => ids,
-  map: ({ breakableLines }) => breakableLines,
-  reduce: items =>
-    Array.from(
-      items.reduce((acc, item) => {
-        if (item && item.state === "fulfilled") {
-          acc = acc.concat(item.value);
-        }
-        return acc;
-      }, [])
-    ),
-});
+/**
+ * Get the list of all breakable lines for a set of source actors.
+ *
+ * This is typically used to fetch the breakable lines of HTML sources
+ * which are made of multiple source actors (one per inline script).
+ *
+ * @param {Object} state
+ * @param {Array<String>} ids
+ *        List of Source Actor IDs
+ * @return {AsyncValue<Array<Number>>}
+ *        List of all the breakable lines.
+ */
+export function getBreakableLinesForSourceActors(state, ids) {
+  const allBreakableLines = [];
+  for (const id of ids) {
+    const { breakableLines } = getSourceActor(state, id);
+    if (breakableLines && breakableLines.state == "fulfilled") {
+      allBreakableLines.push(...breakableLines.value);
+    }
+  }
+  return allBreakableLines;
+}
