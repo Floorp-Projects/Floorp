418c0de3daa656027c1ebf9c7acf1e105da8a718
Frederik Braun <fbraun@mozilla.com>
2022-04-06 14:36:47 +0000

    Bug 1577122 - mozJSComponentLoader::Import() should only work for internal URLs, and update some tests r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D140058

--- dom/indexedDB/test/unit/test_globalObjects_other.js
+++ dom/indexedDB/test/unit/test_globalObjects_other.js
@@ -14,7 +14,7 @@ function* testSteps() {
 
   // Test for IDBKeyRange and indexedDB availability in JS modules.
   const { GlobalObjectsModule } = ChromeUtils.import(
-    getSpec("GlobalObjectsModule.jsm")
+    "resource://test/GlobalObjectsModule.jsm"
   );
   let test = new GlobalObjectsModule();
   test.ok = ok;
