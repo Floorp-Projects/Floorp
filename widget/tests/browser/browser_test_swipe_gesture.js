cf44ee875e67e0288369963b8436d535fea2ff5d
Timothy Nikkel <tnikkel@gmail.com>
2022-04-06 09:51:17 +0000

    Bug 1757928. Add test. r=hiro
    Differential Revision: https://phabricator.services.mozilla.com/D142817

--- widget/tests/browser/browser_test_swipe_gesture.js
+++ widget/tests/browser/browser_test_swipe_gesture.js
@@ -52,12 +52,12 @@ function getPanHandler() {
 
 const NativePanHandler = getPanHandler();
 
-async function panRightToLeft(aElement, aX, aY) {
+async function panRightToLeft(aElement, aX, aY, aMultiplier) {
   await NativePanHandler.promiseNativePanEvent(
     aElement,
     aX,
     aY,
-    NativePanHandler.deltaOnRTL,
+    NativePanHandler.deltaOnRTL * aMultiplier,
     0,
     NativePanHandler.beginPhase
   );
@@ -73,7 +73,7 @@ async function panRightToLeft(aElement, aX, aY) {
     aElement,
     aX,
     aY,
-    NativePanHandler.deltaOnRTL,
+    NativePanHandler.deltaOnRTL * aMultiplier,
     0,
     NativePanHandler.updatePhase
   );
@@ -87,12 +87,17 @@ async function panRightToLeft(aElement, aX, aY) {
   );
 }
 
-async function panLeftToRight(aElement, aX, aY) {
+async function panLeftToRight(aElement, aX, aY, aMultiplier) {
+  await panLeftToRightBeginAndUpdate(aElement, aX, aY, aMultiplier);
+  await panLeftToRightEnd(aElement, aX, aY, aMultiplier);
+}
+
+async function panLeftToRightBeginAndUpdate(aElement, aX, aY, aMultiplier) {
   await NativePanHandler.promiseNativePanEvent(
     aElement,
     aX,
     aY,
-    -NativePanHandler.deltaOnRTL,
+    -NativePanHandler.deltaOnRTL * aMultiplier,
     0,
     NativePanHandler.beginPhase
   );
@@ -100,7 +105,7 @@ async function panLeftToRight(aElement, aX, aY) {
     aElement,
     aX,
     aY,
-    -NativePanHandler.deltaOnRTL,
+    -NativePanHandler.deltaOnRTL * aMultiplier,
     0,
     NativePanHandler.updatePhase
   );
@@ -108,10 +113,13 @@ async function panLeftToRight(aElement, aX, aY) {
     aElement,
     aX,
     aY,
-    -NativePanHandler.deltaOnRTL,
+    -NativePanHandler.deltaOnRTL * aMultiplier,
     0,
     NativePanHandler.updatePhase
   );
+}
+
+async function panLeftToRightEnd(aElement, aX, aY, aMultiplier) {
   await NativePanHandler.promiseNativePanEvent(
     aElement,
     aX,
@@ -156,7 +164,7 @@ add_task(async () => {
   });
 
   // Try to navigate forward.
-  await panRightToLeft(tab.linkedBrowser, 100, 100);
+  await panRightToLeft(tab.linkedBrowser, 100, 100, 1);
   // NOTE: The last endPhase shouldn't fire a wheel event since
   // its delta is zero.
   is(wheelEventCount, 3, "Received 3 wheel events");
@@ -175,7 +183,7 @@ add_task(async () => {
     tab.linkedBrowser,
     firstPage
   );
-  await panLeftToRight(tab.linkedBrowser, 100, 100);
+  await panLeftToRight(tab.linkedBrowser, 100, 100, 1);
   // NOTE: We only get a wheel event for the beginPhase, rest of events have
   // been captured by the swipe gesture module.
   is(wheelEventCount, 1, "Received a wheel event");
@@ -195,7 +203,7 @@ add_task(async () => {
     tab.linkedBrowser,
     secondPage
   );
-  await panRightToLeft(tab.linkedBrowser, 100, 100);
+  await panRightToLeft(tab.linkedBrowser, 100, 100, 1);
   is(wheelEventCount, 1, "Received a wheel event");
 
   await Promise.all([startLoadingPromise, stoppedLoadingPromise]);
@@ -209,7 +217,7 @@ add_task(async () => {
     event.preventDefault();
   };
   tab.linkedBrowser.addEventListener("wheel", wheelEventListener);
-  await panLeftToRight(tab.linkedBrowser, 100, 100);
+  await panLeftToRight(tab.linkedBrowser, 100, 100, 1);
   is(wheelEventCount, 3, "Received all wheel events");
 
   await waitForWhile();
@@ -224,12 +232,209 @@ add_task(async () => {
     set: [["widget.disable-swipe-tracker", true]],
   });
 
-  await panLeftToRight(tab.linkedBrowser, 100, 100);
+  await panLeftToRight(tab.linkedBrowser, 100, 100, 1);
   is(wheelEventCount, 3, "Received all wheel events");
 
   await waitForWhile();
   // Make sure any navigation didn't happen.
   is(tab.linkedBrowser.currentURI.spec, secondPage);
+
+  BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async () => {
+  // Set the default values for an OS that supports swipe to nav, except for
+  // whole-page-pixel-size which varies by OS, we vary it in differente tests
+  // in this file.
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["browser.gesture.swipe.left", "Browser:BackOrBackDuplicate"],
+      ["browser.gesture.swipe.eight", "Browser:ForwardOrForwardDuplicate"],
+      ["widget.disable-swipe-tracker", false],
+      ["widget.swipe.velocity-twitch-tolerance", 0.0000001],
+      ["widget.swipe.success-threshold", 0.25],
+      // Set the velocity-contribution to 0 so we can exactly control the
+      // values in the swipe tracker via the delta in the events that we send.
+      ["widget.swipe.success-velocity-contribution", 0.0],
+      ["widget.swipe.whole-page-pixel-size", 550.0],
+    ],
+  });
+
+  const firstPage = "about:about";
+  const secondPage = "about:mozilla";
+  const tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    firstPage,
+    true /* waitForLoad */
+  );
+
+  BrowserTestUtils.loadURI(tab.linkedBrowser, secondPage);
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser, false, secondPage);
+
+  // Make sure we can go back to the previous page.
+  ok(gBrowser.webNavigation.canGoBack);
+  // and we cannot go forward to the next page.
+  ok(!gBrowser.webNavigation.canGoForward);
+
+  let wheelEventCount = 0;
+  tab.linkedBrowser.addEventListener("wheel", () => {
+    wheelEventCount++;
+  });
+
+  // Send a pan that starts a navigate back but doesn't have enough delta to do
+  // anything. Don't send the pan end because we want to check the opacity
+  // before the MSD animation in SwipeTracker starts which can temporarily put
+  // us at 1 opacity.
+  await panLeftToRightBeginAndUpdate(tab.linkedBrowser, 100, 100, 0.9);
+
+  // Check both getComputedStyle instead of element.style.opacity because we use a transition on the opacity.
+  let computedOpacity = window
+    .getComputedStyle(gHistorySwipeAnimation._prevBox)
+    .getPropertyValue("opacity");
+  ok(
+    0.98 < computedOpacity && computedOpacity < 0.99,
+    "opacity of prevbox is not quite 1"
+  );
+  let opacity = gHistorySwipeAnimation._prevBox.style.opacity;
+  ok(0.98 < opacity && opacity < 0.99, "opacity of prevbox is not quite 1");
+
+  await panLeftToRightEnd(tab.linkedBrowser, 100, 100, 0.9);
+
+  // NOTE: We only get a wheel event for the beginPhase, rest of events have
+  // been captured by the swipe gesture module.
+  is(wheelEventCount, 1, "Received a wheel event");
+
+  await waitForWhile();
+  // Make sure any navigation didn't happen.
+  is(tab.linkedBrowser.currentURI.spec, secondPage);
+
+  // Try to navigate backward.
+  wheelEventCount = 0;
+  let startLoadingPromise = BrowserTestUtils.browserStarted(
+    tab.linkedBrowser,
+    firstPage
+  );
+  let stoppedLoadingPromise = BrowserTestUtils.browserStopped(
+    tab.linkedBrowser,
+    firstPage
+  );
+  await panLeftToRight(tab.linkedBrowser, 100, 100, 1);
+  // NOTE: We only get a wheel event for the beginPhase, rest of events have
+  // been captured by the swipe gesture module.
+  is(wheelEventCount, 1, "Received a wheel event");
+
+  // The element.style opacity will be 0 because we set it to 0 on successful navigation, however
+  // we have a tranisition on it so the computed style opacity will still be 1 because the transition hasn't started yet.
+  computedOpacity = window
+    .getComputedStyle(gHistorySwipeAnimation._prevBox)
+    .getPropertyValue("opacity");
+  ok(computedOpacity == 1, "computed opacity of prevbox is 1");
+  opacity = gHistorySwipeAnimation._prevBox.style.opacity;
+  ok(opacity == 0, "element.style opacity of prevbox 0");
+
+  // Make sure the gesture triggered going back to the previous page.
+  await Promise.all([startLoadingPromise, stoppedLoadingPromise]);
+
+  ok(gBrowser.webNavigation.canGoForward);
+
+  BrowserTestUtils.removeTab(tab);
+});
+
+// Same test as above but whole-page-pixel-size is increased and the multipliers passed to panLeftToRight correspondingly increased.
+add_task(async () => {
+  // Set the default values for an OS that supports swipe to nav, except for
+  // whole-page-pixel-size which varies by OS, we vary it in differente tests
+  // in this file.
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["browser.gesture.swipe.left", "Browser:BackOrBackDuplicate"],
+      ["browser.gesture.swipe.eight", "Browser:ForwardOrForwardDuplicate"],
+      ["widget.disable-swipe-tracker", false],
+      ["widget.swipe.velocity-twitch-tolerance", 0.0000001],
+      ["widget.swipe.success-threshold", 0.25],
+      // Set the velocity-contribution to 0 so we can exactly control the
+      // values in the swipe tracker via the delta in the events that we send.
+      ["widget.swipe.success-velocity-contribution", 0.0],
+      ["widget.swipe.whole-page-pixel-size", 1100.0],
+    ],
+  });
+
+  const firstPage = "about:about";
+  const secondPage = "about:mozilla";
+  const tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    firstPage,
+    true /* waitForLoad */
+  );
+
+  BrowserTestUtils.loadURI(tab.linkedBrowser, secondPage);
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser, false, secondPage);
+
+  // Make sure we can go back to the previous page.
+  ok(gBrowser.webNavigation.canGoBack);
+  // and we cannot go forward to the next page.
+  ok(!gBrowser.webNavigation.canGoForward);
+
+  let wheelEventCount = 0;
+  tab.linkedBrowser.addEventListener("wheel", () => {
+    wheelEventCount++;
+  });
+
+  // Send a pan that starts a navigate back but doesn't have enough delta to do
+  // anything. Don't send the pan end because we want to check the opacity
+  // before the MSD animation in SwipeTracker starts which can temporarily put
+  // us at 1 opacity.
+  await panLeftToRightBeginAndUpdate(tab.linkedBrowser, 100, 100, 1.8);
+
+  // Check both getComputedStyle instead of element.style.opacity because we use a transition on the opacity.
+  let computedOpacity = window
+    .getComputedStyle(gHistorySwipeAnimation._prevBox)
+    .getPropertyValue("opacity");
+  ok(
+    0.98 < computedOpacity && computedOpacity < 0.99,
+    "opacity of prevbox is not quite 1"
+  );
+  let opacity = gHistorySwipeAnimation._prevBox.style.opacity;
+  ok(0.98 < opacity && opacity < 0.99, "opacity of prevbox is not quite 1");
+
+  await panLeftToRightEnd(tab.linkedBrowser, 100, 100, 1.8);
+
+  // NOTE: We only get a wheel event for the beginPhase, rest of events have
+  // been captured by the swipe gesture module.
+  is(wheelEventCount, 1, "Received a wheel event");
+
+  await waitForWhile();
+  // Make sure any navigation didn't happen.
+  is(tab.linkedBrowser.currentURI.spec, secondPage);
+
+  // Try to navigate backward.
+  wheelEventCount = 0;
+  let startLoadingPromise = BrowserTestUtils.browserStarted(
+    tab.linkedBrowser,
+    firstPage
+  );
+  let stoppedLoadingPromise = BrowserTestUtils.browserStopped(
+    tab.linkedBrowser,
+    firstPage
+  );
+  await panLeftToRight(tab.linkedBrowser, 100, 100, 2);
+  // NOTE: We only get a wheel event for the beginPhase, rest of events have
+  // been captured by the swipe gesture module.
+  is(wheelEventCount, 1, "Received a wheel event");
+
+  // The element.style opacity will be 0 because we set it to 0 on successful navigation, however
+  // we have a tranisition on it so the computed style opacity will still be 1 because the transition hasn't started yet.
+  computedOpacity = window
+    .getComputedStyle(gHistorySwipeAnimation._prevBox)
+    .getPropertyValue("opacity");
+  ok(computedOpacity == 1, "computed opacity of prevbox is 1");
+  opacity = gHistorySwipeAnimation._prevBox.style.opacity;
+  ok(opacity == 0, "element.style opacity of prevbox 0");
+
+  // Make sure the gesture triggered going back to the previous page.
+  await Promise.all([startLoadingPromise, stoppedLoadingPromise]);
+
+  ok(gBrowser.webNavigation.canGoForward);
 
   BrowserTestUtils.removeTab(tab);
 });
