c9134dfe261393d0044f6568c4ac43fd58db8c33
Philip Jägenstedt <philip@foolip.org>
2022-04-07 19:08:38 +0000

    Bug 1762363 [wpt PR 33443] - Automatically enable MojoJS also on Chrome beta/stable, a=testonly
    Automatic update from web-platform-tests
    Automatically enable MojoJS also on Chrome beta/stable (#33443)
    
    --
    
    wpt-commits: 05dfc2fadacb192d68a7b273625c2a9b1fde5db2
    wpt-pr: 33443

--- testing/web-platform/tests/tools/wpt/run.py
+++ testing/web-platform/tests/tools/wpt/run.py
@@ -343,8 +343,7 @@ class Chrome(BrowserSetup):
         if kwargs["mojojs_path"]:
             kwargs["enable_mojojs"] = True
             logger.info("--mojojs-path is provided, enabling MojoJS")
-        # TODO(Hexcles): Enable this everywhere when Chrome 86 becomes stable.
-        elif browser_channel in self.experimental_channels:
+        else:
             try:
                 path = self.browser.install_mojojs(
                     dest=self.venv.path,
