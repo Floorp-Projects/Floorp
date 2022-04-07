aa50d873374d35b1f2750abfa25d8e2b5c95c180
Martin Robinson <mrobinson@igalia.com>
2022-04-07 10:38:27 +0000

    Bug 1762407 - Do not scroll content with `content-visibility: hidden` ancestors into view r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D142563

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -3654,7 +3654,7 @@ void PresShell::DoScrollContentIntoView() {
   NS_ASSERTION(mDidInitialize, "should have done initial reflow by now");
 
   nsIFrame* frame = mContentToScrollTo->GetPrimaryFrame();
-  if (!frame) {
+  if (!frame || frame->AncestorHidesContent()) {
     mContentToScrollTo->RemoveProperty(nsGkAtoms::scrolling);
     mContentToScrollTo = nullptr;
     return;
@@ -3725,6 +3725,10 @@ bool PresShell::ScrollFrameRectIntoView(nsIFrame* aFrame, const nsRect& aRect,
                                         ScrollAxis aVertical,
                                         ScrollAxis aHorizontal,
                                         ScrollFlags aScrollFlags) {
+  if (aFrame->AncestorHidesContent()) {
+    return false;
+  }
+
   bool didScroll = false;
   // This function needs to work even if rect has a width or height of 0.
   nsRect rect = aRect;
