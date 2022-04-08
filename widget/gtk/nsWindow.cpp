c2419fed6237b8f93a8147202608d482ad8293c3
Emilio Cobos Álvarez <emilio@crisal.io>
2022-04-08 13:19:52 +0000

    Bug 1756903 - Consider using client decorations everywhere. r=stransky
    I tested client all the way back to Ubuntu 18 LTS and seems to work
    fine, any reason not to try this?
    
    This fixes some weird overdrawing on the window corners on GNOME+X11.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139556

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -8945,28 +8945,12 @@ nsWindow::GtkWindowDecoration nsWindow::GetSystemGtkWindowDecoration() {
     if (!currentDesktop) {
       return GTK_DECORATION_NONE;
     }
-    // clang-format off
-    if (strstr(currentDesktop, "pop:GNOME") || // Bug 1629198
-        strstr(currentDesktop, "KDE") ||
-        strstr(currentDesktop, "Enlightenment") ||
-        strstr(currentDesktop, "LXDE") ||
-        strstr(currentDesktop, "openbox") ||
-        strstr(currentDesktop, "MATE") ||
-        strstr(currentDesktop, "X-Cinnamon") ||
-        strstr(currentDesktop, "Pantheon") ||
-        strstr(currentDesktop, "Deepin")) {
-      return GTK_DECORATION_CLIENT;
-    }
-    if (strstr(currentDesktop, "GNOME") ||
-        strstr(currentDesktop, "LXQt") ||
-        strstr(currentDesktop, "Unity")) {
-      return GTK_DECORATION_SYSTEM;
-    }
     if (strstr(currentDesktop, "i3")) {
       return GTK_DECORATION_NONE;
     }
-    // clang-format on
 
+    // Tested desktops: pop:GNOME, KDE, Enlightenment, LXDE, openbox, MATE,
+    // X-Cinnamon, Pantheon, Deepin, GNOME, LXQt, Unity.
     return GTK_DECORATION_CLIENT;
   }();
   return sGtkWindowDecoration;
