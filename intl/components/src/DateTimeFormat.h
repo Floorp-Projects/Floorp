f1f95f79bf3eaf8f995891e553c2189a7f3abc26
André Bargull <andre.bargull@gmail.com>
2022-04-09 11:52:52 +0000

    Bug 1763783 - Part 10: Remove no longer needed workarounds for ICU-21154 and ICU-21155. r=platform-i18n-reviewers,jfkthame
    ICU-21154 and ICU-21155 have been fixed in ICU 71, so we can remove this
    workaround.
    
    Depends on D143282
    
    Differential Revision: https://phabricator.services.mozilla.com/D143283

--- intl/components/src/DateTimeFormat.h
+++ intl/components/src/DateTimeFormat.h
@@ -419,8 +419,7 @@ class DateTimeFormat final {
    * plan to remove it.
    */
   template <typename B>
-  ICUResult GetOriginalSkeleton(B& aBuffer,
-                                Maybe<HourCycle> aHourCycle = Nothing()) {
+  ICUResult GetOriginalSkeleton(B& aBuffer) {
     static_assert(std::is_same_v<typename B::CharType, char16_t>);
     if (mOriginalSkeleton.length() == 0) {
       // Generate a skeleton from the resolved pattern, there was no originally
@@ -435,10 +434,6 @@ class DateTimeFormat final {
 
     if (!FillBuffer(mOriginalSkeleton, aBuffer)) {
       return Err(ICUError::OutOfMemory);
-    }
-    if (aHourCycle) {
-      DateTimeFormat::ReplaceHourSymbol(Span(aBuffer.data(), aBuffer.length()),
-                                        *aHourCycle);
     }
     return Ok();
   }
