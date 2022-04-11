e999070c0fd29b56db30a08e00afa4fa6da2297a
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2022-04-11 04:11:22 +0000

    Bug 1757431 - Update orientation lock information in browsing context when exiting full screen. r=smaug
    Orientation lock allows on full screen state only now. Then if exiting full
    screen, orientation lock is unlocked without `screen.orientation.unlock` call.
    
    Although we store orientation lock state in browsing context, we don't clear
    when exiting full screen. So when current content is re-activated by switching
    tab etc, we try to apply orientation lock state from browsing context
    unfortunately.
    
    So we should clear this state when exiting full screen even if `unlock` isn't
    called.
    
    Differential Revision: https://phabricator.services.mozilla.com/D142938

--- dom/base/ScreenOrientation.cpp
+++ dom/base/ScreenOrientation.cpp
@@ -702,6 +702,12 @@ ScreenOrientation::FullscreenEventListener::HandleEvent(Event* aEvent) {
     return NS_OK;
   }
 
+  BrowsingContext* bc = doc->GetBrowsingContext();
+  bc = bc ? bc->Top() : nullptr;
+  if (bc) {
+    bc->SetOrientationLock(hal::ScreenOrientation::None, IgnoreErrors());
+  }
+
   hal::UnlockScreenOrientation();
 
   target->RemoveSystemEventListener(u"fullscreenchange"_ns, this, true);
