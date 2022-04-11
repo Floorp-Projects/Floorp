9d22133f93d7c980e4cbdd1793bc62f314435f2f
Iulian Moraru <imoraru@mozilla.com>
2022-04-11 19:01:16 +0300

    Backed out changeset 75811190935c (bug 1713773) for causing leaks on mutex/nsthread.

--- xpcom/threads/nsThread.h
+++ xpcom/threads/nsThread.h
@@ -294,6 +294,7 @@ class nsThread : public nsIThreadInternal,
 
   static mozilla::OffTheBooksMutex& ThreadListMutex();
   static mozilla::LinkedList<nsThread>& ThreadList();
+  static void ClearThreadList();
 
   void AddToThreadList();
   void MaybeRemoveFromThreadList();
@@ -334,6 +335,8 @@ class nsThread : public nsIThreadInternal,
 
   // Set to true if this thread creates a JSRuntime.
   bool mCanInvokeJS;
+
+  bool mHasTLSEntry = false;
 
   // The time the currently running event spent in event queues, and
   // when it started running.  If no event is running, they are
