63432ff816b746b8d6a80176eedd38806c2eea0d
Steve Fink <sfink@mozilla.com>
2022-04-13 17:11:35 +0000

    Bug 1762771 - disallow gray marking when black objects are on the stack, again r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D143238

--- js/src/gc/Marking.cpp
+++ js/src/gc/Marking.cpp
@@ -1752,7 +1752,7 @@ GCMarker::MarkQueueProgress GCMarker::processMarkQueue() {
             markUntilBudgetExhausted(unlimited, DontReportMarkTime));
       } else if (js::StringEqualsLiteral(str, "set-color-gray")) {
         queueMarkColor = mozilla::Some(MarkColor::Gray);
-        if (gcrt.state() != State::Sweep) {
+        if (gcrt.state() != State::Sweep || hasBlackEntries()) {
           // Cannot mark gray yet, so continue with the GC.
           queuePos--;
           return QueueSuspended;
