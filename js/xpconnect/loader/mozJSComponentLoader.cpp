1e3e10268002354b1969901d78c890e61e384cb2
Tooru Fujisawa <arai_a@mac.com>
2022-04-13 11:41:11 +0000

    Bug 1752209 - Put cahe type into the cache key in StartupCache/ScriptPreloader. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D143576

--- js/xpconnect/loader/mozJSComponentLoader.cpp
+++ js/xpconnect/loader/mozJSComponentLoader.cpp
@@ -75,7 +75,8 @@ using namespace mozilla::loader;
 using namespace xpc;
 using namespace JS;
 
-#define JS_CACHE_PREFIX(aType) "jsloader/" aType
+#define JS_CACHE_PREFIX(aScopeType, aCompilationTarget) \
+  "jsloader/" aScopeType "/" aCompilationTarget
 
 /**
  * Buffer sizes for serialization and deserialization of scripts.
@@ -748,8 +749,8 @@ nsresult mozJSComponentLoader::ObjectForLocation(
   aInfo.EnsureResolvedURI();
 
   nsAutoCString cachePath;
-  rv = PathifyURI(JS_CACHE_PREFIX("non-syntactic"), aInfo.ResolvedURI(),
-                  cachePath);
+  rv = PathifyURI(JS_CACHE_PREFIX("non-syntactic", "script"),
+                  aInfo.ResolvedURI(), cachePath);
   NS_ENSURE_SUCCESS(rv, rv);
 
   JS::DecodeOptions decodeOptions;
