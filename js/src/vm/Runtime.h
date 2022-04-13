272e6bea25e5c10dbcbf35126185609ad0cfb319
Yulia Startsev <ystartsev@mozilla.com>
2022-04-13 16:29:36 +0000

    Bug 1763586 - Remove Element from LoadScript and child classes; r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D143139

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -319,8 +319,6 @@ struct JSRuntime {
   /* Call this to accumulate use counter data. */
   js::MainThreadData<JSSetUseCounterCallback> useCounterCallback;
 
-  js::MainThreadData<JSSourceElementCallback> sourceElementCallback;
-
  public:
   // Accumulates data for Firefox telemetry. |id| is the ID of a JS_TELEMETRY_*
   // histogram. |key| provides an additional key to identify the histogram.
@@ -331,9 +329,6 @@ struct JSRuntime {
 
   void setTelemetryCallback(JSRuntime* rt,
                             JSAccumulateTelemetryDataCallback callback);
-
-  void setSourceElementCallback(JSRuntime* rt,
-                                JSSourceElementCallback callback);
 
   // Sets the use counter for a specific feature, measuring the presence or
   // absence of usage of a feature on a specific web page and document which
