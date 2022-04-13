053dae1adea0bdb52db1fc8c5e76f9213acc6769
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-04-13 09:27:11 +0000

    Bug 1635428 - Part 3: Remove excluded() from files.py r=firefox-build-system-reviewers,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D112287

--- python/mozbuild/mozpack/files.py
+++ python/mozbuild/mozpack/files.py
@@ -363,22 +363,6 @@ class AbsoluteSymlinkFile(File):
 
         File.__init__(self, path)
 
-    @staticmethod
-    def excluded(dest):
-        if platform.system() != "Windows":
-            return False
-
-        # Exclude local resources from symlinking since the sandbox on Windows
-        # does not allow accessing reparse points. See bug 1695556.
-        from buildconfig import topobjdir
-
-        denylist = [("dist", "bin"), ("_tests", "modules")]
-        fulllist = [os.path.join(topobjdir, *paths) for paths in denylist]
-
-        fulldest = os.path.join(os.path.abspath(os.curdir), dest)
-
-        return mozpath.basedir(fulldest, fulllist) is not None
-
     def copy(self, dest, skip_if_older=True):
         assert isinstance(dest, six.string_types)
 
@@ -389,7 +373,7 @@ class AbsoluteSymlinkFile(File):
 
         # Handle the simple case where symlinks are definitely not supported by
         # falling back to file copy.
-        if not hasattr(os, "symlink") or AbsoluteSymlinkFile.excluded(dest):
+        if not hasattr(os, "symlink"):
             return File.copy(self, dest, skip_if_older=skip_if_older)
 
         # Always verify the symlink target path exists.
