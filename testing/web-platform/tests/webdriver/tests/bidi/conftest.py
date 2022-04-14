4123cbb1f15c3f3c2ceb24d27fcfd66b958b15c2
Julian Descottes <jdescottes@mozilla.com>
2022-04-14 15:24:23 +0000

    Bug 1764615 - [wdspec] new_tab fixture should restore the webdriver window_handle r=webdriver-reviewers,whimboo
    Alternatively, we could avoid updating the window_handle completely and run the test in a background tab.
    
    Differential Revision: https://phabricator.services.mozilla.com/D143680

--- testing/web-platform/tests/webdriver/tests/bidi/conftest.py
+++ testing/web-platform/tests/webdriver/tests/bidi/conftest.py
@@ -5,12 +5,16 @@ import pytest
 async def new_tab(bidi_session, current_session):
     # Open and focus a new tab to run the test in a foreground tab.
     context_id = current_session.new_window(type_hint="tab")
+    initial_window = current_session.window_handle
     current_session.window_handle = context_id
 
     # Retrieve the browsing context info for the new tab
     contexts = await bidi_session.browsing_context.get_tree(parent=context_id, max_depth=0)
     yield contexts[0]
 
+    # Restore the focus and current window for the WebDriver session before
+    # closing the tab.
+    current_session.window_handle = initial_window
     await bidi_session.browsing_context.close(context=contexts[0]["context"])
 
 
