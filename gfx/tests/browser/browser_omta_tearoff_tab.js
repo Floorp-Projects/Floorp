2db50930cd9c628b2714375f073f6cf23af14daa
Florian Quèze <florian@queze.net>
2022-04-14 22:06:32 +0000

    Bug 1764812 - Add a test ensuring vsync is disabled after tearing off a tab that has compositor animations, r=hiro.
    Differential Revision: https://phabricator.services.mozilla.com/D143748

---
+++ gfx/tests/browser/browser_omta_tearoff_tab.js
@@ -0,0 +1,32 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+var testPage =
+  "data:text/html,<head><style>body{animation: fadein 1s infinite;} @keyframes fadein{from{opacity: 0;}}</style><body>Text";
+
+add_task(async function test() {
+  let tab = BrowserTestUtils.addTab(gBrowser, testPage, {
+    skipAnimation: true,
+  });
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+
+  let promiseWin = BrowserTestUtils.waitForNewWindow();
+  let newWin = gBrowser.replaceTabWithWindow(tab);
+  await promiseWin;
+  Assert.ok(
+    ChromeUtils.vsyncEnabled(),
+    "vsync should be enabled as we have a tab with an animation"
+  );
+
+  newWin.close();
+  await TestUtils.waitForCondition(
+    () => !ChromeUtils.vsyncEnabled(),
+    "wait for vsync to be disabled"
+  );
+  Assert.ok(
+    !ChromeUtils.vsyncEnabled(),
+    "vsync should be disabled after closing window that contained an animated tab"
+  );
+});
