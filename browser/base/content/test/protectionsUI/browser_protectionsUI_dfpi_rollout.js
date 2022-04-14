8348f10f2bf29ff954175d9ca8723af43c3fd9b6
Paul Zuehlcke <pbz@mozilla.com>
2022-04-14 16:32:34 +0000

    Bug 1741597 - Revert cookie behavior to initial default value when dFPI rollout pref is set to false. r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D131348

--- browser/base/content/test/protectionsUI/browser_protectionsUI_dfpi_rollout.js
+++ browser/base/content/test/protectionsUI/browser_protectionsUI_dfpi_rollout.js
@@ -5,6 +5,12 @@ const { TelemetryTestUtils } = ChromeUtils.import(
   "resource://testing-common/TelemetryTestUtils.jsm"
 );
 
+ChromeUtils.defineModuleGetter(
+  this,
+  "BrowserGlue",
+  "resource:///modules/BrowserGlue.jsm"
+);
+
 const PREF_DFPI_ENABLED_BY_DEFAULT =
   "privacy.restrict3rdpartystorage.rollout.enabledByDefault";
 const COOKIE_BEHAVIOR_PREF = "network.cookie.cookieBehavior";
@@ -24,14 +30,22 @@ const SEARCH_PREFS_OPT_OUT = [
   ["browser.search.param.bing_ptag", "MOZZ0000000032"],
 ];
 
-registerCleanupFunction(function() {
+function cleanup() {
+  [...SEARCH_PREFS_OPT_IN, ...SEARCH_PREFS_OPT_OUT].forEach(([key]) =>
+    Services.prefs.clearUserPref(key)
+  );
+
+  [COOKIE_BEHAVIOR_PREF, PREF_DFPI_ENABLED_BY_DEFAULT].forEach(
+    Services.prefs.clearUserPref
+  );
+
+  BrowserGlue._defaultCookieBehaviorAtStartup = previousDefaultCB;
   defaultPrefs.setIntPref(COOKIE_BEHAVIOR_PREF, previousDefaultCB);
-  [
-    PREF_DFPI_ENABLED_BY_DEFAULT,
-    ...SEARCH_PREFS_OPT_IN,
-    ...SEARCH_PREFS_OPT_OUT,
-  ].forEach(([key]) => Services.prefs.clearUserPref(key));
-});
+
+  // Reset the rollout scalar back to 2 = unset. We have to simulate this on
+  // test cleanup, because BrowserGlue only sets this once initially.
+  Services.telemetry.scalarSet("privacy.dfpi_rollout_enabledByDefault", 2);
+}
 
 function testSearchPrefState(optIn) {
   let expectedPrefs = optIn ? SEARCH_PREFS_OPT_IN : SEARCH_PREFS_OPT_OUT;
@@ -68,6 +82,11 @@ function testTelemetryState(optIn) {
 // Tests that the dFPI rollout pref updates the default cookieBehavior to 5,
 // sets the correct search prefs and records telemetry.
 add_task(async function testdFPIRolloutPref() {
+  // The BrowserGlue code which computes this flag runs before we can set the
+  // default cookie behavior for this test. Thus we need to overwrite it in
+  // order for the opt-out to work correctly.
+  BrowserGlue._defaultCookieBehaviorAtStartup =
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER;
   defaultPrefs.setIntPref(
     COOKIE_BEHAVIOR_PREF,
     Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER
@@ -107,4 +126,77 @@ add_task(async function testdFPIRolloutPref() {
   );
   testSearchPrefState(true);
   testTelemetryState(true);
+
+  cleanup();
+});
+
+/**
+ * Setting the rollout pref to false should revert to the initial default cookie
+ * behavior, not always BEHAVIOR_REJECT_TRACKER.
+ */
+add_task(async function testdFPIRolloutPrefNonDefaultCookieBehavior() {
+  BrowserGlue._defaultCookieBehaviorAtStartup =
+    Ci.nsICookieService.BEHAVIOR_ACCEPT;
+  defaultPrefs.setIntPref(
+    COOKIE_BEHAVIOR_PREF,
+    Ci.nsICookieService.BEHAVIOR_ACCEPT
+  );
+
+  is(
+    Services.prefs.getIntPref(COOKIE_BEHAVIOR_PREF),
+    Ci.nsICookieService.BEHAVIOR_ACCEPT,
+    "Initial cookie behavior should be BEHAVIOR_ACCEPT"
+  );
+
+  Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, true);
+  is(
+    defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN,
+    "Default cookie behavior should be set to dFPI."
+  );
+
+  Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, false);
+  is(
+    defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
+    Ci.nsICookieService.BEHAVIOR_ACCEPT,
+    "Default cookie behavior should be set to BEHAVIOR_ACCEPT."
+  );
+
+  cleanup();
+});
+
+/**
+ * When a client already ships with dFPI enabled, toggling the rollout pref
+ * should not change cookie behavior.
+ */
+add_task(async function testdFPIRolloutPrefDFPIAlreadyEnabled() {
+  // Simulate TCP enabled by default.
+  BrowserGlue._defaultCookieBehaviorAtStartup =
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN;
+  defaultPrefs.setIntPref(
+    COOKIE_BEHAVIOR_PREF,
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN
+  );
+
+  is(
+    Services.prefs.getIntPref(COOKIE_BEHAVIOR_PREF),
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN,
+    "Initial cookie behavior should be BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN"
+  );
+
+  Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, true);
+  is(
+    defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN,
+    "Default cookie behavior should still be BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN."
+  );
+
+  Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, false);
+  is(
+    defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
+    Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN,
+    "Default cookie behavior should still be BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN."
+  );
+
+  cleanup();
 });
