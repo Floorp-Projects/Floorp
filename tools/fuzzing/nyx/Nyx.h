8e5b003502ff383f8518a67eb8467f2159f7b71b
Christian Holler <choller@mozilla.com>
2022-04-14 12:10:14 +0000

    Bug 1764259 - [Fuzzing] Nyx replay mode. r=truber
    Differential Revision: https://phabricator.services.mozilla.com/D143458

--- tools/fuzzing/nyx/Nyx.h
+++ tools/fuzzing/nyx/Nyx.h
@@ -9,6 +9,7 @@
 
 #include <stdint.h>
 #include <atomic>
+#include <list>
 
 #ifndef NYX_DISALLOW_COPY_AND_ASSIGN
 #  define NYX_DISALLOW_COPY_AND_ASSIGN(T) \
@@ -17,6 +18,11 @@
 #endif
 
 namespace mozilla {
+
+class MallocAllocPolicy;
+template <class T, size_t MinInlineCapacity, class AllocPolicy>
+class Vector;
+
 namespace fuzzing {
 
 class Nyx {
@@ -26,6 +32,7 @@ class Nyx {
   void start(void);
   bool started(void);
   bool is_enabled(const char* identifier);
+  bool is_replay();
   uint32_t get_data(uint8_t* data, uint32_t size);
   void release(uint32_t iterations = 1);
   void handle_event(const char* type, const char* file, int line,
@@ -33,6 +40,9 @@ class Nyx {
 
  private:
   std::atomic<bool> mInited;
+
+  std::atomic<bool> mReplayMode;
+  std::list<Vector<uint8_t, 0, MallocAllocPolicy>*> mReplayBuffers;
 
   Nyx();
   NYX_DISALLOW_COPY_AND_ASSIGN(Nyx);
