6818525c5ab714961cb79003231d1f8e612b5537
Michael Froman <mfroman@mozilla.com>
2022-04-15 14:06:14 -0500

    Bug 1766646 - Vendor libwebrtc from 8cc6695652
    Upstream commit: https://webrtc.googlesource.com/src/+/8cc6695652307929edfc877cd64b75cd9ec2d615
        Reformat python files checked by pylint (part 1/2).
    
        After recently changing .pylintrc (see [1]) we discovered that
        the presubmit check always checks all the python files when just
        one python file gets updated.
    
        This CL moves all these files one step closer to what the linter
        wants.
    
        Autogenerated with:
    
        # Added all the files under pylint control to ~/Desktop/to-reformat
        cat ~/Desktop/to-reformat | xargs sed -i '1i\\'
        git cl format --python --full
    
        This is part 1 out of 2. The second part will fix function names and
        will not be automated.
    
        [1] - https://webrtc-review.googlesource.com/c/src/+/186664
    
        No-Presubmit: True
        Bug: webrtc:12114
        Change-Id: Idfec4d759f209a2090440d0af2413a1ddc01b841
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/190980
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32530}

--- third_party/libwebrtc/sdk/android/PRESUBMIT.py
+++ third_party/libwebrtc/sdk/android/PRESUBMIT.py
@@ -6,23 +6,26 @@
 # in the file PATENTS.  All contributing project authors may
 # be found in the AUTHORS file in the root of the source tree.
 
+
 def CheckChangeOnUpload(input_api, output_api):
-  results = []
-  results.extend(CheckPatchFormatted(input_api, output_api))
-  return results
+    results = []
+    results.extend(CheckPatchFormatted(input_api, output_api))
+    return results
+
 
 def CheckPatchFormatted(input_api, output_api):
-  import git_cl
-  cmd = ['cl', 'format', '--dry-run', input_api.PresubmitLocalPath()]
-  code, _ = git_cl.RunGitWithCode(cmd, suppress_stderr=True)
-  if code == 2:
-    short_path = input_api.basename(input_api.PresubmitLocalPath())
-    full_path = input_api.os_path.relpath(input_api.PresubmitLocalPath(),
-                                          input_api.change.RepositoryRoot())
-    return [output_api.PresubmitPromptWarning(
-      'The %s directory requires source formatting. '
-      'Please run git cl format %s' %
-      (short_path, full_path))]
-  # As this is just a warning, ignore all other errors if the user
-  # happens to have a broken clang-format, doesn't use git, etc etc.
-  return []
+    import git_cl
+    cmd = ['cl', 'format', '--dry-run', input_api.PresubmitLocalPath()]
+    code, _ = git_cl.RunGitWithCode(cmd, suppress_stderr=True)
+    if code == 2:
+        short_path = input_api.basename(input_api.PresubmitLocalPath())
+        full_path = input_api.os_path.relpath(
+            input_api.PresubmitLocalPath(), input_api.change.RepositoryRoot())
+        return [
+            output_api.PresubmitPromptWarning(
+                'The %s directory requires source formatting. '
+                'Please run git cl format %s' % (short_path, full_path))
+        ]
+    # As this is just a warning, ignore all other errors if the user
+    # happens to have a broken clang-format, doesn't use git, etc etc.
+    return []
