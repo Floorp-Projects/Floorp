c5dd6ed3bfd6b338987506c702b527377a2f185c
Masayuki Nakano <masayuki@d-toybox.com>
2022-04-15 03:49:57 +0000

    Bug 1764238 - Backout the first and the second patch of bug 1758420 r=m_kato
    Due to breaking `mobile.twitter.com/explore` for Hebrew IME users on Android,
    this patch backs out the patches for bug 1753420 except the last patch which
    just rename some methods and variables.
    
    Differential Revision: https://phabricator.services.mozilla.com/D143684

--- dom/events/IMEContentObserver.cpp
+++ dom/events/IMEContentObserver.cpp
@@ -792,15 +792,6 @@ void IMEContentObserver::CharacterDataChanged(
              // node.
   }
 
-  // Let TextComposition have a change to update composition string range in
-  // the text node if the change is caused by the web apps.
-  if (mWidget && !IsEditorHandlingEventForComposition()) {
-    if (RefPtr<TextComposition> composition =
-            IMEStateManager::GetTextCompositionFor(mWidget)) {
-      composition->OnCharacterDataChanged(*aContent->AsText(), aInfo);
-    }
-  }
-
   if (!NeedsTextChangeNotification() ||
       !nsContentUtils::IsInSameAnonymousTree(mRootContent, aContent)) {
     return;
