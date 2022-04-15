e8e3023f2bdb3f2cda5f073297185977cf8a17e6
Alexandre Poirot <poirot.alex@gmail.com>
2022-04-15 07:37:16 +0000

    Bug 1747105 - [devtools] Cover SourceTree behavior when having many sources loaded from the same URL. r=bomsy
    I had to tune the versionized http helper to pass the right headers,
    while still ensuring that http cache is kept disabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D142348

--- devtools/client/debugger/test/mochitest/browser_dbg-features-source-tree.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-features-source-tree.js
@@ -22,6 +22,7 @@ const TEST_URL = testServer.urlFor("index.html");
 const INTEGRATION_TEST_PAGE_SOURCES = [
   "index.html",
   "script.js",
+  "onload.js",
   "test-functions.js",
   "query.js?x=1",
   "query.js?x=2",
@@ -30,9 +31,18 @@ const INTEGRATION_TEST_PAGE_SOURCES = [
   "replaced-bundle.js",
   "removed-original.js",
   "named-eval.js",
+  // Webpack generated some extra sources:
   "bootstrap 3b1a221408fdde86aa49",
   "bootstrap 6fda1f7ea9ecbc1a2d5b",
+  // There is 3 occurences, one per target (main thread, worker and iframe).
+  // But there is even more source actors (named evals and duplicated script tags).
+  "same-url.sjs",
+  "same-url.sjs",
 ];
+// The iframe one is only available when fission is enabled, or EFT
+if (isFissionEnabled() || isEveryFrameTargetEnabled()) {
+  INTEGRATION_TEST_PAGE_SOURCES.push("same-url.sjs");
+}
 
 /**
  * This test opens the SourceTree manually via click events on the nested source,
