e62b668f61de1cde9375e19d2d318aa346f9c273
Michael Froman <mfroman@mozilla.com>
2022-04-15 23:54:54 -0500

    Bug 1766646 - Vendor libwebrtc from 91e4156a8c
    Upstream commit: https://webrtc.googlesource.com/src/+/91e4156a8c0d2d70046417fcc2e13e2e1268ee0c
        Remove dependency on MessageHandlerAutoCleanup from cricket::Port.
    
        Bug: webrtc:11988
        Change-Id: I15335cf49365de1274f5d3051dcb2de97e9e263b
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/194562
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Tommi <tommi@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32673}

--- third_party/libwebrtc/p2p/base/port.cc
+++ third_party/libwebrtc/p2p/base/port.cc
@@ -188,6 +188,9 @@ void Port::Construct() {
 }
 
 Port::~Port() {
+  RTC_DCHECK_RUN_ON(thread_);
+  CancelPendingTasks();
+
   // Delete all of the remaining connections.  We copy the list up front
   // because each deletion will cause it to be modified.
 
@@ -609,6 +612,16 @@ rtc::DiffServCodePoint Port::StunDscpValue() const {
   return rtc::DSCP_NO_CHANGE;
 }
 
+void Port::set_timeout_delay(int delay) {
+  RTC_DCHECK_RUN_ON(thread_);
+  // Although this method is meant to only be used by tests, some downstream
+  // projects have started using it. Ideally we should update our tests to not
+  // require to modify this state and instead use a testing harness that allows
+  // adjusting the clock and then just use the kPortTimeoutDelay constant
+  // directly.
+  timeout_delay_ = delay;
+}
+
 bool Port::ParseStunUsername(const StunMessage* stun_msg,
                              std::string* local_ufrag,
                              std::string* remote_ufrag) const {
@@ -818,7 +831,14 @@ void Port::Prune() {
   thread_->Post(RTC_FROM_HERE, this, MSG_DESTROY_IF_DEAD);
 }
 
+// Call to stop any currently pending operations from running.
+void Port::CancelPendingTasks() {
+  RTC_DCHECK_RUN_ON(thread_);
+  thread_->Clear(this);
+}
+
 void Port::OnMessage(rtc::Message* pmsg) {
+  RTC_DCHECK_RUN_ON(thread_);
   RTC_DCHECK(pmsg->message_id == MSG_DESTROY_IF_DEAD);
   bool dead =
       (state_ == State::INIT || state_ == State::PRUNED) &&
