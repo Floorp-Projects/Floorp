9128c93a98fccf1ffdfc081f79b14f5192f54f42
Michael Froman <mfroman@mozilla.com>
2022-04-15 23:56:49 -0500

    Bug 1766646 - Vendor libwebrtc from 19526a69c7
    Upstream commit: https://webrtc.googlesource.com/src/+/19526a69c763d26f4699f14dd33681db8976a1b1
        Add RTC_NO_UNIQUE_ADDRESS to webrtc::VideoStreamAdapter.
    
        Bug: webrtc:11495
        Change-Id: I32a1bc75d7c578753c158a61eb6cb5ea7565629a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/194564
        Reviewed-by: Karl Wiberg <kwiberg@webrtc.org>
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32676}

--- third_party/libwebrtc/call/adaptation/video_stream_adapter.h
+++ third_party/libwebrtc/call/adaptation/video_stream_adapter.h
@@ -28,6 +28,7 @@
 #include "call/adaptation/video_stream_input_state_provider.h"
 #include "modules/video_coding/utility/quality_scaler.h"
 #include "rtc_base/experiments/balanced_degradation_settings.h"
+#include "rtc_base/system/no_unique_address.h"
 #include "rtc_base/thread_annotations.h"
 
 namespace webrtc {
@@ -214,7 +215,8 @@ class VideoStreamAdapter {
       const VideoStreamInputState& input_state) const
       RTC_RUN_ON(&sequence_checker_);
 
-  SequenceChecker sequence_checker_ RTC_GUARDED_BY(&sequence_checker_);
+  RTC_NO_UNIQUE_ADDRESS SequenceChecker sequence_checker_
+      RTC_GUARDED_BY(&sequence_checker_);
   // Gets the input state which is the basis of all adaptations.
   // Thread safe.
   VideoStreamInputStateProvider* input_state_provider_;
