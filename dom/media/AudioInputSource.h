502e1be819d3d66a77ece274c9eaf39eedac42f2
Chun-Min Chang <chun.m.chang@gmail.com>
2022-04-18 18:45:35 +0000

    Bug 1238038 - Create an input-only audio source r=padenot
    This patch creates an class managing an input-only audio stream within a
    task thread. This class will be used as a source generating the audio
    data to a NonNativeInputTrack in the following patch.
    
    Depends on D116535
    
    Differential Revision: https://phabricator.services.mozilla.com/D137911

--- dom/media/AudioInputSource.h
+++ dom/media/AudioInputSource.h
@@ -7,29 +7,67 @@
 #ifndef DOM_MEDIA_AudioInputSource_H_
 #define DOM_MEDIA_AudioInputSource_H_
 
+#include "AudioDriftCorrection.h"
 #include "AudioSegment.h"
+#include "CubebInputStream.h"
 #include "CubebUtils.h"
+#include "mozilla/ProfilerUtils.h"
+#include "mozilla/RefPtr.h"
+#include "mozilla/SPSCQueue.h"
+#include "mozilla/SharedThreadPool.h"
 
 namespace mozilla {
 
-// This is an interface to operate an input-only audio stream. Once users call
-// Start(), the audio data can be read by GetAudioSegment(). GetAudioSegment()
-// is not thread-safe, which means it can only be call in one specific thread.
-// The audio data is periodically produced by the underlying audio stream on its
-// callback thread.
-class AudioInputSource {
+// This is an interface to operate an input-only audio stream within a
+// cubeb-task thread on a specific thread. Once the class instance is created,
+// all its operations must be called on the same thread.
+//
+// The audio data is periodically produced by the underlying audio stream on the
+// stream's callback thread, and can be safely read by GetAudioSegment() on a
+// specific thread.
+class AudioInputSource : public CubebInputStream::Listener {
  public:
-  NS_INLINE_DECL_PURE_VIRTUAL_REFCOUNTING;
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(AudioInputSource, override);
 
   using Id = uint32_t;
 
-  // These functions should always be called in the same thread:
+  class EventListener {
+   public:
+    NS_INLINE_DECL_PURE_VIRTUAL_REFCOUNTING;
+
+    // This two events will be fired on main thread.
+    virtual void AudioDeviceChanged(Id aId) = 0;
+    enum class State { Started, Stopped, Drained, Error };
+    virtual void AudioStateCallback(Id aId, State aState) = 0;
+
+   protected:
+    EventListener() = default;
+    virtual ~EventListener() = default;
+  };
+
+  AudioInputSource(RefPtr<EventListener>&& aListener, Id aSourceId,
+                   CubebUtils::AudioDeviceID aDeviceId, uint32_t aChannelCount,
+                   bool aIsVoice, const PrincipalHandle& aPrincipalHandle,
+                   TrackRate aSourceRate, TrackRate aTargetRate,
+                   uint32_t aBufferMs);
+
+  // The following functions should always be called in the same thread: They
+  // are always run on MediaTrackGraph's graph thread.
   // Starts producing audio data.
-  virtual void Start() = 0;
+  virtual void Start();
   // Stops producing audio data.
-  virtual void Stop() = 0;
+  virtual void Stop();
   // Returns the AudioSegment with aDuration of data inside.
-  virtual AudioSegment GetAudioSegment(TrackTime aDuration) = 0;
+  virtual AudioSegment GetAudioSegment(TrackTime aDuration);
+
+  // CubebInputStream::Listener interface: These are used only for the
+  // underlying audio stream. No user should call these APIs.
+  // This will be fired on audio callback thread.
+  long DataCallback(const void* aBuffer, long aFrames) override;
+  // This can be fired on any thread.
+  void StateCallback(cubeb_state aState) override;
+  // This can be fired on any thread.
+  void DeviceChangedCallback() override;
 
   // Any threads:
   // The unique id of this source.
@@ -46,16 +84,38 @@ class AudioInputSource {
   const PrincipalHandle mPrincipalHandle;
 
  protected:
-  AudioInputSource(Id aId, CubebUtils::AudioDeviceID aDeviceId,
-                   uint32_t aChannelCount, TrackRate aRate, bool aIsVoice,
-                   const PrincipalHandle& aPrincipalHandle)
-      : mId(aId),
-        mDeviceId(aDeviceId),
-        mChannelCount(aChannelCount),
-        mRate(aRate),
-        mIsVoice(aIsVoice),
-        mPrincipalHandle(aPrincipalHandle) {}
-  virtual ~AudioInputSource() = default;
+  ~AudioInputSource() = default;
+
+ private:
+  // Underlying audio thread only.
+  bool CheckThreadIdChanged();
+
+  // Any thread.
+  const bool mSandboxed;
+
+  // Thread id of the underlying audio thread. Underlying audio thread only.
+  std::atomic<ProfilerThreadId> mAudioThreadId;
+
+  // Forward the underlying event from main thread.
+  const RefPtr<EventListener> mEventListener;
+
+  // Shared thread pool containing only one thread for cubeb operations.
+  // The cubeb operations: Start() and Stop() will be called on
+  // MediaTrackGraph's graph thread, which can be the cubeb stream's callback
+  // thread. Running cubeb operations within cubeb stream callback thread can
+  // cause the deadlock on Linux, so we dispatch those operations to the task
+  // thread.
+  const RefPtr<SharedThreadPool> mTaskThread;
+
+  // Correct the drift between the underlying audio stream and its reader.
+  AudioDriftCorrection mDriftCorrector;
+
+  // An input-only cubeb stream operated within mTaskThread.
+  UniquePtr<CubebInputStream> mStream;
+
+  // A single-producer-single-consumer lock-free queue whose data is produced by
+  // the audio callback thread and consumed by AudioInputSource's data reader.
+  SPSCQueue<AudioChunk> mSPSCQueue{30};
 };
 
 }  // namespace mozilla
