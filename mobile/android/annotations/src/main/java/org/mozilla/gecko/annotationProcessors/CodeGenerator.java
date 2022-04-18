366dbce5afdb00fb1f39d205247e8eb533d5f175
Jamie Nicol <jnicol@mozilla.com>
2022-04-18 18:11:07 +0000

    Bug 1762424 - Generate SDK bindings for android.view.SurfaceControl. r=agi
    And android.view.SurfaceControl$Transaction.
    
    In order to generate these bindings we must increase the maximum SDK
    version argument we pass to SDKProcessor to 29. However, doing so
    means that we now attempt to generate bindings for both Surface's
    Surface(SurfaceTexture) and Surface(SurfaceControl)
    constructors. These both translate in to C++ functions with identical
    signatures - Surface::New(jni::Object::Param) - causing a compilation
    failure.
    
    This patch therefore also allows the stubName property to override
    constructor names, and overrides the latter to
    Surface::FromSurfaceControl(), thereby avoiding the conflict.
    
    Differential Revision: https://phabricator.services.mozilla.com/D143484

--- mobile/android/annotations/src/main/java/org/mozilla/gecko/annotationProcessors/CodeGenerator.java
+++ mobile/android/annotations/src/main/java/org/mozilla/gecko/annotationProcessors/CodeGenerator.java
@@ -716,7 +716,7 @@ public class CodeGenerator {
     // Unpack the tuple and extract some useful fields from the Method..
     final Constructor<?> method = annotatedConstructor.getConstructor();
     final AnnotationInfo info = annotatedConstructor.mAnnotationInfo;
-    final String wrapperName = "New";
+    final String wrapperName = info.wrapperName.equals("<init>") ? "New" : info.wrapperName;
     final String uniqueName = getUniqueMethodName(wrapperName);
     final Class<?>[] argTypes = method.getParameterTypes();
     final Class<?> returnType = cls;
