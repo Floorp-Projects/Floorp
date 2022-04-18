d8175941d54231072600b7b7a0f2895b63249d35
Michael Froman <mfroman@mozilla.com>
2022-04-18 12:41:31 -0500

    Bug 1766646 - Vendor libwebrtc from 6e7167456b
    Upstream commit: https://webrtc.googlesource.com/src/+/6e7167456b5eba36c7985d6a74f1d191958d4e0f
        Adds experimental libvpx VP9 speed settings.
    
        Using the field trial WebRTC-VP9-PerformanceFlags, this CL allows you to
        configure the libvpx VP9 encoder with a list of flags to affect the
        quality vs speed tradeoff. This CL adds support for:
    
        * Speed (effort), for the temporal base layer frames
        * Speed for higher (non-base) layer frames
        * De-blocking (as part of the loopfilter) enabled for:
          0 = all frames
          1 = all but frames from the highest temporal layer
          2 = no frames
    
        Each entry in the list has a threshold in min number of pixels needed
        for settings in the entry to apply.
    
        Example: Two spatial layers (180p, 360p) with three temporal
        layers are configured. Field trial "WebRTC-VP9-PerformanceFlags" set to:
        "min_pixel_count:0|129600,base_layer_speed:5|8,high_layer_speed:7|8,deblock_mode:1|2"
        This translates to:
        S0:
          - TL0: Speed 5, deblocked
          - TL1: Speed 8, deblocked
          - TL2: Speed 8, not deblocked
        S1:
          - TL0: Speed 7, not deblocked
          - TL1: Speed 8, not deblocked
          - TL2: Speed 8, not deblocked
    
        Bug: webrtc:11551
        Change-Id: Ieef6816d3e0831ff53348ecc4a90260e2ef10422
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/188461
        Reviewed-by: Michael Horowitz <mhoro@webrtc.org>
        Reviewed-by: Sergey Silkin <ssilkin@webrtc.org>
        Commit-Queue: Erik Språng <sprang@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32749}

--- third_party/libwebrtc/modules/video_coding/codecs/vp9/vp9_impl.h
+++ third_party/libwebrtc/modules/video_coding/codecs/vp9/vp9_impl.h
@@ -115,7 +115,6 @@ class VP9EncoderImpl : public VP9Encoder {
   const VP9Profile profile_;
   bool inited_;
   int64_t timestamp_;
-  int cpu_speed_;
   uint32_t rc_max_intra_target_;
   vpx_codec_ctx_t* encoder_;
   vpx_codec_enc_cfg_t* config_;
@@ -198,11 +197,28 @@ class VP9EncoderImpl : public VP9Encoder {
       const WebRtcKeyValueConfig& trials);
   const bool external_ref_ctrl_;
 
-  const struct SpeedSettings {
-    bool enabled;
-    int layers[kMaxSpatialLayers];
-  } per_layer_speed_;
-  static SpeedSettings ParsePerLayerSpeed(const WebRtcKeyValueConfig& trials);
+  // Flags that can affect speed vs quality tradeoff, and are configureable per
+  // resolution ranges.
+  struct PerformanceFlags {
+    // Min number of pixels needed for this config to be valid.
+    int min_pixel_count = 0;
+
+    int base_layer_speed = -1;  // Speed setting for TL0.
+    int high_layer_speed = -1;  // Speed setting for TL1-TL3.
+    //  0 = deblock all temporal layers (TL)
+    //  1 = disable deblock for top-most TL
+    //  2 = disable deblock for all TLs
+    int deblock_mode = 0;
+  };
+  // Performance flags, ordered by |min_pixel_count|.
+  const std::vector<PerformanceFlags> performance_flags_;
+  // Caching of of |speed_configs_|, where index i maps to the resolution as
+  // specified in |codec_.spatialLayer[i]|.
+  std::vector<PerformanceFlags> performance_flags_by_spatial_index_;
+  void UpdatePerformanceFlags();
+  static std::vector<PerformanceFlags> ParsePerformanceFlagsFromTrials(
+      const WebRtcKeyValueConfig& trials);
+  static std::vector<PerformanceFlags> GetDefaultPerformanceFlags();
 
   int num_steady_state_frames_;
   // Only set config when this flag is set.
