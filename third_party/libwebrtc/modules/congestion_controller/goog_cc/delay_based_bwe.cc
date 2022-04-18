9d92177330c6d7bb33be8666ca754f1864d5e390
Michael Froman <mfroman@mozilla.com>
2022-04-18 11:37:19 -0500

    Bug 1766646 - Vendor libwebrtc from 0496a41211
    Upstream commit: https://webrtc.googlesource.com/src/+/0496a4121188a26013dca007bf6e9a7ab6d961b6
        Add class InterArrivalDelta to goog_cc
    
        This cl copies modules/remote_bitrate_estimator/inter_arrival.x to inter_arrival.h and interrival_delta.cc in goog_cc in the first patchset.
        In the following- this class is modified to use webrtc::Timestamp and webrtc::Timedelta in order to avoid having to use 24 bit time repressentation.
    
        Bug: none
        Change-Id: I9befe6e3e283cf7e21efa974ae33e8a83e26cbe6
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/194004
        Commit-Queue: Per Kjellander <perkj@webrtc.org>
        Reviewed-by: Christoffer Rodbro <crodbro@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32733}

--- third_party/libwebrtc/modules/congestion_controller/goog_cc/delay_based_bwe.cc
+++ third_party/libwebrtc/modules/congestion_controller/goog_cc/delay_based_bwe.cc
@@ -20,6 +20,7 @@
 #include "absl/strings/match.h"
 #include "api/rtc_event_log/rtc_event.h"
 #include "api/rtc_event_log/rtc_event_log.h"
+#include "api/units/time_delta.h"
 #include "logging/rtc_event_log/events/rtc_event_bwe_update_delay_based.h"
 #include "modules/congestion_controller/goog_cc/trendline_estimator.h"
 #include "modules/remote_bitrate_estimator/include/bwe_defines.h"
@@ -31,15 +32,7 @@
 namespace webrtc {
 namespace {
 constexpr TimeDelta kStreamTimeOut = TimeDelta::Seconds(2);
-constexpr int kTimestampGroupLengthMs = 5;
-constexpr int kAbsSendTimeFraction = 18;
-constexpr int kAbsSendTimeInterArrivalUpshift = 8;
-constexpr int kInterArrivalShift =
-    kAbsSendTimeFraction + kAbsSendTimeInterArrivalUpshift;
-constexpr int kTimestampGroupTicks =
-    (kTimestampGroupLengthMs << kInterArrivalShift) / 1000;
-constexpr double kTimestampToMs =
-    1000.0 / static_cast<double>(1 << kInterArrivalShift);
+constexpr TimeDelta kSendTimeGroupLength = TimeDelta::Millis(5);
 
 // This ssrc is used to fulfill the current API but will be removed
 // after the API has been changed.
@@ -181,12 +174,12 @@ void DelayBasedBwe::IncomingPacketFeedback(const PacketResult& packet_feedback,
   // Reset if the stream has timed out.
   if (last_seen_packet_.IsInfinite() ||
       at_time - last_seen_packet_ > kStreamTimeOut) {
-    video_inter_arrival_.reset(
-        new InterArrival(kTimestampGroupTicks, kTimestampToMs, true));
+    video_inter_arrival_ =
+        std::make_unique<InterArrivalDelta>(kSendTimeGroupLength);
     video_delay_detector_.reset(
         new TrendlineEstimator(key_value_config_, network_state_predictor_));
-    audio_inter_arrival_.reset(
-        new InterArrival(kTimestampGroupTicks, kTimestampToMs, true));
+    audio_inter_arrival_ =
+        std::make_unique<InterArrivalDelta>(kSendTimeGroupLength);
     audio_delay_detector_.reset(
         new TrendlineEstimator(key_value_config_, network_state_predictor_));
     active_delay_detector_ = video_delay_detector_.get();
@@ -211,7 +204,7 @@ void DelayBasedBwe::IncomingPacketFeedback(const PacketResult& packet_feedback,
 
   // As an alternative to ignoring small packets, we can separate audio and
   // video packets for overuse detection.
-  InterArrival* inter_arrival_for_packet = video_inter_arrival_.get();
+  auto* inter_arrival_for_packet = video_inter_arrival_.get();
   DelayIncreaseDetectorInterface* delay_detector_for_packet =
       video_delay_detector_.get();
   if (separate_audio_.enabled) {
@@ -232,25 +225,15 @@ void DelayBasedBwe::IncomingPacketFeedback(const PacketResult& packet_feedback,
     }
   }
 
-  uint32_t send_time_24bits =
-      static_cast<uint32_t>(
-          ((static_cast<uint64_t>(packet_feedback.sent_packet.send_time.ms())
-            << kAbsSendTimeFraction) +
-           500) /
-          1000) &
-      0x00FFFFFF;
-  // Shift up send time to use the full 32 bits that inter_arrival works with,
-  // so wrapping works properly.
-  uint32_t timestamp = send_time_24bits << kAbsSendTimeInterArrivalUpshift;
-
-  uint32_t timestamp_delta = 0;
-  int64_t recv_delta_ms = 0;
+  TimeDelta send_delta = TimeDelta::Zero();
+  TimeDelta recv_delta = TimeDelta::Zero();
   int size_delta = 0;
+
   bool calculated_deltas = inter_arrival_for_packet->ComputeDeltas(
-      timestamp, packet_feedback.receive_time.ms(), at_time.ms(),
-      packet_size.bytes(), &timestamp_delta, &recv_delta_ms, &size_delta);
-  double send_delta_ms = (1000.0 * timestamp_delta) / (1 << kInterArrivalShift);
-  delay_detector_for_packet->Update(recv_delta_ms, send_delta_ms,
+      packet_feedback.sent_packet.send_time, packet_feedback.receive_time,
+      at_time, packet_size.bytes(), &send_delta, &recv_delta, &size_delta);
+
+  delay_detector_for_packet->Update(recv_delta.ms(), send_delta.ms(),
                                     packet_feedback.sent_packet.send_time.ms(),
                                     packet_feedback.receive_time.ms(),
                                     packet_size.bytes(), calculated_deltas);
