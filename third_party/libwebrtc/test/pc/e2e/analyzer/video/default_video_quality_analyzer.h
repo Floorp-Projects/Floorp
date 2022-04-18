61fdb80aaa9b913a12cc6956ba5a7f77ddd5991a
Michael Froman <mfroman@mozilla.com>
2022-04-18 12:40:15 -0500

    Bug 1766646 - Vendor libwebrtc from 9c296e5b9e
    Upstream commit: https://webrtc.googlesource.com/src/+/9c296e5b9ebf8609e56ff21f117597eaffe53096
        Fix DVQA cpu usage when Stop is called multiple times
    
        Bug: webrtc:12247
        Change-Id: I946338e0ecf58f91c87c8638977a8bc52e648fd2
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/196083
        Commit-Queue: Andrey Logvin <landrey@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32747}

--- third_party/libwebrtc/test/pc/e2e/analyzer/video/default_video_quality_analyzer.h
+++ third_party/libwebrtc/test/pc/e2e/analyzer/video/default_video_quality_analyzer.h
@@ -237,6 +237,7 @@ class DefaultVideoQualityAnalyzer : public VideoQualityAnalyzerInterface {
   // obtained by calling GetKnownVideoStreams()
   std::map<StatsKey, StreamStats> GetStats() const;
   AnalyzerStats GetAnalyzerStats() const;
+  double GetCpuUsagePercent();
 
  private:
   struct FrameStats {
@@ -500,7 +501,6 @@ class DefaultVideoQualityAnalyzer : public VideoQualityAnalyzerInterface {
   void StopMeasuringCpuProcessTime();
   void StartExcludingCpuThreadTime();
   void StopExcludingCpuThreadTime();
-  double GetCpuUsagePercent();
 
   // TODO(titovartem) restore const when old constructor will be removed.
   DefaultVideoQualityAnalyzerOptions options_;
