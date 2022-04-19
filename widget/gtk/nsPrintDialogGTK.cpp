936e3f055556b635819d504b9c7461ec4f4fab9c
ganguin <ganguin>
2022-04-19 08:35:26 +0000

    Bug 1661450 - 1/8 Guard X11 specific code with MOZ_X11 r=stransky
    Add MOZ_X11 ifdefs on X11 specific code, mainly alongside GdkIsX11Display.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139526

--- widget/gtk/nsPrintDialogGTK.cpp
+++ widget/gtk/nsPrintDialogGTK.cpp
@@ -28,7 +28,10 @@
 #include "nsIObserverService.h"
 
 // for gdk_x11_window_get_xid
-#include <gdk/gdkx.h>
+#include <gdk/gdk.h>
+#ifdef MOZ_X11
+#  include <gdk/gdkx.h>
+#endif
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
