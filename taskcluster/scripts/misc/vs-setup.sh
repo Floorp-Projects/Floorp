ca3d248c272cc49b6a0f415b8fde59dab196bd45
Narcis Beleuzu <nbeleuzu@mozilla.com>
2022-04-20 03:03:26 +0300

    Backed out 2 changesets (bug 1758780) for jsreftest failure on fp-evaluation-order.js . CLOSED TREE
    Backed out changeset 10e2414392ae (bug 1758780)
    Backed out changeset 481062597411 (bug 1758780)

--- taskcluster/scripts/misc/vs-setup.sh
+++ taskcluster/scripts/misc/vs-setup.sh
@@ -2,14 +2,8 @@ VSDIR=vs
 VSPATH="${MOZ_FETCHES_DIR}/${VSDIR}"
 UNIX_VSPATH="$(cd ${MOZ_FETCHES_DIR} && pwd)/${VSDIR}"
 VCDIR=vc/tools/msvc/14.16.27023
-if [ ! -d "${VSPATH}/${VCDIR}" ]; then
-    VCDIR=vc/tools/msvc/14.29.30133
-fi
 SDKDIR="windows kits/10"
 SDK_VERSION=10.0.17134.0
-if [ ! -d "${VSPATH}/${SDKDIR}/lib/${SDK_VERSION}" ]; then
-    SDK_VERSION=10.0.19041.0
-fi
 
 export INCLUDE="${VSPATH}/${VCDIR}/include;${VSPATH}/${VCDIR}/atlmfc/include;${VSPATH}/${SDKDIR}/include/${SDK_VERSION}/ucrt;${VSPATH}/${SDKDIR}/include/${SDK_VERSION}/shared;${VSPATH}/${SDKDIR}/include/${SDK_VERSION}/um;${VSPATH}/${SDKDIR}/include/${SDK_VERSION}/winrt;${VSPATH}/dia sdk/include"
 export LIB="${VSPATH}/${VCDIR}/lib/x64;${VSPATH}/${VCDIR}/atlmfc/lib/x64;${VSPATH}/${SDKDIR}/lib/${SDK_VERSION}/um/x64;${VSPATH}/${SDKDIR}/lib/${SDK_VERSION}/ucrt/x64;${VSPATH}/dia sdk/lib/amd64"
