ee871dd92bd8afc3a6e6eb3f54703bf07b60f71e
Alexandre Poirot <poirot.alex@gmail.com>
2022-04-20 15:15:06 +0000

    Bug 1764505 - [devtools] Migrate Tool's definition isTargetSupport to isToolSupported. r=nchevobbe
    And pass a toolbox instead of only the toplevel target.
    It helps identify the browser toolbox and easily enable/disable tools for it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D143628

--- browser/components/extensions/parent/ext-devtools-panels.js
+++ browser/components/extensions/parent/ext-devtools-panels.js
@@ -156,7 +156,7 @@ class ParentDevToolsPanel extends BaseDevToolsPanel {
       // panelLabel is used to set the aria-label attribute (See Bug 1570645).
       panelLabel: title,
       tooltip: `DevTools Panel added by "${extensionName}" add-on.`,
-      isTargetSupported: target => target.isLocalTab,
+      isToolSupported: toolbox => toolbox.target.isLocalTab,
       build: (window, toolbox) => {
         if (toolbox !== this.toolbox) {
           throw new Error(
