fc2a9212474d1cf7037be79ee691f9c109816a28
ganguin <ganguin>
2022-04-20 09:32:07 +0000

    Bug 1661450 - 1/8 Guard X11 specific code with MOZ_X11 r=stransky
    Add MOZ_X11 ifdefs on X11 specific code, mainly alongside GdkIsX11Display.
    
    Differential Revision: https://phabricator.services.mozilla.com/D139526

--- widget/gtk/nsClipboard.cpp
+++ widget/gtk/nsClipboard.cpp
@@ -9,7 +9,9 @@
 
 #include "nsArrayUtils.h"
 #include "nsClipboard.h"
-#include "nsClipboardX11.h"
+#if defined(MOZ_X11)
+#  include "nsClipboardX11.h"
+#endif
 #if defined(MOZ_WAYLAND)
 #  include "nsClipboardWayland.h"
 #endif
@@ -202,16 +204,16 @@ nsClipboard::~nsClipboard() {
 NS_IMPL_ISUPPORTS(nsClipboard, nsIClipboard, nsIObserver)
 
 nsresult nsClipboard::Init(void) {
+#if defined(MOZ_X11)
   if (widget::GdkIsX11Display()) {
     mContext = new nsRetrievalContextX11();
-#if defined(MOZ_WAYLAND)
-  } else if (widget::GdkIsWaylandDisplay()) {
-    mContext = new nsRetrievalContextWayland();
-#endif
-  } else {
-    NS_WARNING("Missing nsRetrievalContext for nsClipboard!");
-    return NS_OK;
   }
+#endif
+#if defined(MOZ_WAYLAND)
+  if (widget::GdkIsWaylandDisplay()) {
+    mContext = new nsRetrievalContextWayland();
+  }
+#endif
 
   nsCOMPtr<nsIObserverService> os = mozilla::services::GetObserverService();
   if (os) {
