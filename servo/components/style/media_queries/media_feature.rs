b467bb5e099ba85c34294d03d0a175b8b9ceff6e
Butkovits Atila <abutkovits@mozilla.com>
2022-04-20 09:53:50 +0300

    Backed out changeset 3d29e3e5525b (bug 1765401) for causing reftest failures at mediaqueries. CLOSED TREE

--- servo/components/style/media_queries/media_feature.rs
+++ servo/components/style/media_queries/media_feature.rs
@@ -4,6 +4,7 @@
 
 //! Media features.
 
+use super::media_feature_expression::RangeOrOperator;
 use super::Device;
 use crate::parser::ParserContext;
 use crate::values::computed::Ratio;
@@ -16,7 +17,12 @@ use style_traits::ParseError;
 /// A generic discriminant for an enum value.
 pub type KeywordDiscriminant = u8;
 
-type MediaFeatureGetter<T> = fn(device: &Device) -> T;
+type MediaFeatureEvaluator<T> = fn(
+    device: &Device,
+    // null == no value was given in the query.
+    value: Option<T>,
+    range_or_operator: Option<RangeOrOperator>,
+) -> bool;
 
 /// Serializes a given discriminant.
 ///
@@ -35,14 +41,14 @@ pub type KeywordParser = for<'a, 'i, 't> fn(
 /// This determines the kind of values that get parsed, too.
 #[allow(missing_docs)]
 pub enum Evaluator {
-    Length(MediaFeatureGetter<CSSPixelLength>),
-    Integer(MediaFeatureGetter<u32>),
-    Float(MediaFeatureGetter<f32>),
-    BoolInteger(MediaFeatureGetter<bool>),
+    Length(MediaFeatureEvaluator<CSSPixelLength>),
+    Integer(MediaFeatureEvaluator<u32>),
+    Float(MediaFeatureEvaluator<f32>),
+    BoolInteger(MediaFeatureEvaluator<bool>),
     /// A non-negative number ratio, such as the one from device-pixel-ratio.
-    NumberRatio(MediaFeatureGetter<Ratio>),
+    NumberRatio(MediaFeatureEvaluator<Ratio>),
     /// A resolution.
-    Resolution(MediaFeatureGetter<Resolution>),
+    Resolution(MediaFeatureEvaluator<Resolution>),
     /// A keyword value.
     Enumerated {
         /// The parser to get a discriminant given a string.
@@ -54,8 +60,9 @@ pub enum Evaluator {
         serializer: KeywordSerializer,
         /// The evaluator itself. This is guaranteed to be called with a
         /// keyword that `parser` has produced.
-        evaluator: fn(&Device, Option<KeywordDiscriminant>) -> bool,
+        evaluator: MediaFeatureEvaluator<KeywordDiscriminant>,
     },
+    Ident(MediaFeatureEvaluator<Atom>),
 }
 
 /// A simple helper macro to create a keyword evaluator.
@@ -86,7 +93,14 @@ macro_rules! keyword_evaluator {
         fn __evaluate(
             device: &$crate::media_queries::Device,
             value: Option<$crate::media_queries::media_feature::KeywordDiscriminant>,
+            range_or_operator: Option<
+                $crate::media_queries::media_feature_expression::RangeOrOperator,
+            >,
         ) -> bool {
+            debug_assert!(
+                range_or_operator.is_none(),
+                "Since when do keywords accept ranges?"
+            );
             // This unwrap is ok because the only discriminants that get
             // back to us is the ones that `parse` produces.
             let value: Option<$keyword_type> =
