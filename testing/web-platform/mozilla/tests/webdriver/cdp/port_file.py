9e7378353061cb068b963d9535ce02d11fa30a37
Henrik Skupin <mail@hskupin.info>
2022-04-21 12:13:14 +0000

    Bug 1722090 - [remote] Move useful browser chrome tests for Remote Agent to wdspec. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D144213

---
+++ testing/web-platform/mozilla/tests/webdriver/cdp/port_file.py
@@ -0,0 +1,25 @@
+import os
+
+from support.network import websocket_request
+
+
+def test_devtools_active_port_file(browser):
+    current_browser = browser(use_cdp=True)
+
+    assert current_browser.remote_agent_port == "9222"
+    assert current_browser.debugger_address.startswith("/devtools/browser/")
+
+    port_file = os.path.join(current_browser.profile.profile, "DevToolsActivePort")
+    assert os.path.exists(port_file)
+
+    current_browser.quit(clean_profile=False)
+    assert not os.path.exists(port_file)
+
+
+def test_connect(browser):
+    current_browser = browser(use_cdp=True)
+
+    response = websocket_request(
+        current_browser.remote_agent_port, path=current_browser.debugger_address
+    )
+    assert response.status == 101
