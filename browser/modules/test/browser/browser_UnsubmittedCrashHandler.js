95e13b1839c994f02a669de845a47684565331aa
Gabriele Svelto <gsvelto@mozilla.com>
2022-04-21 12:43:11 +0000

    Bug 1702509 - Add an annotation describing how a crash report was submitted r=KrisWright
    Differential Revision: https://phabricator.services.mozilla.com/D143184

--- browser/modules/test/browser/browser_UnsubmittedCrashHandler.js
+++ browser/modules/test/browser/browser_UnsubmittedCrashHandler.js
@@ -137,9 +137,12 @@ function createPendingCrashReports(howMany, accessDate) {
  *
  * @param reportIDs (Array<string>)
  *        The IDs for the reports that we expect CrashSubmit to have sent.
+ * @param extraCheck (Function, optional)
+ *        A function that receives the annotations of the crash report and can
+ *        be used for checking them
  * @returns Promise
  */
-function waitForSubmittedReports(reportIDs) {
+function waitForSubmittedReports(reportIDs, extraCheck) {
   let promises = [];
   for (let reportID of reportIDs) {
     let promise = TestUtils.topicObserved(
@@ -149,23 +152,16 @@ function waitForSubmittedReports(reportIDs) {
           let propBag = subject.QueryInterface(Ci.nsIPropertyBag2);
           let dumpID = propBag.getPropertyAsAString("minidumpID");
           if (dumpID == reportID) {
+            if (extraCheck) {
+              let extra = propBag.getPropertyAsInterface(
+                "extra",
+                Ci.nsIPropertyBag2
+              );
+
+              extraCheck(extra);
+            }
+
             return true;
-          }
-          let extra = propBag.getPropertyAsInterface(
-            "extra",
-            Ci.nsIPropertyBag2
-          );
-          const blockedAnnotations = [
-            "ServerURL",
-            "TelemetryClientId",
-            "TelemetryServerURL",
-            "TelemetrySessionId",
-          ];
-          for (const key of blockedAnnotations) {
-            Assert.ok(
-              !extra.hasKey(key),
-              "The " + key + " annotation should have been stripped away"
-            );
           }
         }
         return false;
@@ -192,7 +188,7 @@ function waitForIgnoredReports(reportIDs) {
   for (let reportID of reportIDs) {
     let file = dir.clone();
     file.append(reportID + ".dmp.ignore");
-    promises.push(OS.File.exists(file.path));
+    promises.push(IOUtils.exists(file.path));
   }
   return Promise.all(promises);
 }
@@ -361,6 +357,23 @@ add_task(async function test_several_pending() {
  * Tests that the notification can submit a report.
  */
 add_task(async function test_can_submit() {
+  function extraCheck(extra) {
+    const blockedAnnotations = [
+      "ServerURL",
+      "TelemetryClientId",
+      "TelemetryServerURL",
+      "TelemetrySessionId",
+    ];
+    for (const key of blockedAnnotations) {
+      Assert.ok(
+        !extra.hasKey(key),
+        "The " + key + " annotation should have been stripped away"
+      );
+    }
+
+    Assert.equal(extra.get("SubmittedFrom"), "Infobar");
+  }
+
   let reportIDs = await createPendingCrashReports(1);
   let notification = await UnsubmittedCrashHandler.checkForUnsubmittedCrashReports();
   Assert.ok(notification, "There should be a notification");
@@ -372,8 +385,7 @@ add_task(async function test_can_submit() {
   );
   // ...which should be the first button.
   let submit = buttons[0];
-
-  let promiseReports = waitForSubmittedReports(reportIDs);
+  let promiseReports = waitForSubmittedReports(reportIDs, extraCheck);
   info("Sending crash report");
   submit.click();
   info("Sent!");
@@ -459,6 +471,16 @@ add_task(async function test_can_submit_always() {
     true,
     "The autoSubmit pref should have been set"
   );
+
+  // Create another report
+  reportIDs = await createPendingCrashReports(1);
+  let result = await UnsubmittedCrashHandler.checkForUnsubmittedCrashReports();
+
+  // Check that the crash was auto-submitted
+  Assert.equal(result, null, "The notification should not be shown");
+  promiseReports = await waitForSubmittedReports(reportIDs, extra => {
+    Assert.equal(extra.get("SubmittedFrom"), "Auto");
+  });
 
   // And revert back to default now.
   Services.prefs.clearUserPref(pref);
