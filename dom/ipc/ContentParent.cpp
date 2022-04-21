ae68ae2e2e91e543189172df6a1c0d2d46989ba5
Olli Pettay <Olli.Pettay@helsinki.fi>
2022-04-21 08:56:25 +0000

    Bug 1761728, MaybeDiscarded::IsNull doesn't mean the value isn't null, r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D144003

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -7412,8 +7412,11 @@ mozilla::ipc::IPCResult ContentParent::RecvSynchronizeLayoutHistoryState(
     return IPC_OK();
   }
 
-  SessionHistoryEntry* entry =
-      aContext.GetMaybeDiscarded()->Canonical()->GetActiveSessionHistoryEntry();
+  BrowsingContext* bc = aContext.GetMaybeDiscarded();
+  if (!bc) {
+    return IPC_OK();
+  }
+  SessionHistoryEntry* entry = bc->Canonical()->GetActiveSessionHistoryEntry();
   if (entry) {
     entry->SetLayoutHistoryState(aState);
   }
@@ -7509,8 +7512,12 @@ mozilla::ipc::IPCResult ContentParent::RecvSessionHistoryEntryWireframe(
     return IPC_OK();
   }
 
-  SessionHistoryEntry* entry =
-      aContext.GetMaybeDiscarded()->Canonical()->GetActiveSessionHistoryEntry();
+  BrowsingContext* bc = aContext.GetMaybeDiscarded();
+  if (!bc) {
+    return IPC_OK();
+  }
+
+  SessionHistoryEntry* entry = bc->Canonical()->GetActiveSessionHistoryEntry();
   if (entry) {
     entry->SetWireframe(Some(aWireframe));
   }
