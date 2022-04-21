6355f9495d1c41f5f593fc93850a72cc96458c28
Nika Layzell <nika@thelayzells.com>
2022-04-21 14:54:01 +0000

    Bug 1763413 - Stop disabling Fission in safe mode, r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D144198

--- toolkit/content/aboutSupport.js
+++ toolkit/content/aboutSupport.js
@@ -158,7 +158,6 @@ var snapshotFormatters = {
       disabledByE10sEnv: "fission-status-disabled-by-e10s-env",
       enabledByEnv: "fission-status-enabled-by-env",
       disabledByEnv: "fission-status-disabled-by-env",
-      disabledBySafeMode: "fission-status-disabled-by-safe-mode",
       enabledByDefault: "fission-status-enabled-by-default",
       disabledByDefault: "fission-status-disabled-by-default",
       enabledByUserPref: "fission-status-enabled-by-user-pref",
