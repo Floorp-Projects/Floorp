51ead39cf19b4c178918184df4efb50be2db8c30
Emilio Cobos Álvarez <emilio@crisal.io>
2022-04-25 08:23:51 +0000

    Bug 1766046 - cleanup animation-name. r=boris
    Make the representation the same between Gecko and Servo code. This will
    enable further clean-ups in the future.
    
    Make serialization be correct, serializing as identifier unless it's an
    invalid one (in which case we serialize as a string).
    
    This changes our stringification behavior in the specified style, but
    now it will match the computed style and be more correct over-all.
    
    Differential Revision: https://phabricator.services.mozilla.com/D144473

--- servo/tests/unit/style/parsing/animation.rs
+++ servo/tests/unit/style/parsing/animation.rs
@@ -11,20 +11,6 @@ use style::values::specified::AnimationIterationCount;
 use style_traits::ToCss;
 
 #[test]
-fn test_animation_name() {
-    use self::animation_name::single_value::SpecifiedValue as SingleValue;
-    let other_name = Atom::from("other-name");
-    assert_eq!(parse_longhand!(animation_name, "none"),
-               animation_name::SpecifiedValue(vec![SingleValue(None)]));
-    assert_eq!(parse_longhand!(animation_name, "other-name, none, 'other-name', \"other-name\""),
-               animation_name::SpecifiedValue(
-                   vec![SingleValue(Some(KeyframesName::Ident(CustomIdent(other_name.clone())))),
-                        SingleValue(None),
-                        SingleValue(Some(KeyframesName::QuotedString(other_name.clone()))),
-                        SingleValue(Some(KeyframesName::QuotedString(other_name.clone())))]));
-}
-
-#[test]
 fn test_animation_iteration() {
     assert_roundtrip_with_context!(AnimationIterationCount::parse, "0", "0");
     assert_roundtrip_with_context!(AnimationIterationCount::parse, "0.1", "0.1");
