d3a8ea0787103a5209789d034f8e1a5126ddf560
Michael Froman <mfroman@mozilla.com>
2022-04-25 18:47:30 -0500

    Bug 1766646 - Vendor libwebrtc from 9131313913
    Upstream commit: https://webrtc.googlesource.com/src/+/91313139131b3b28e6eac95a7d762d8f9420ce63
        RNN VAD: GRU layer isolated into rnn_gru.h/.cc
    
        Refactoring done to more easily and cleanly add SIMD optimizations and
        to remove `GatedRecurrentLayer` from the RNN VAD api.
    
        Bug: webrtc:10480
        Change-Id: Ie1dffdd9b19c57c03a0b634f6818c0780456a66c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/195445
        Commit-Queue: Alessio Bazzica <alessiob@webrtc.org>
        Reviewed-by: Jakob Ivarsson <jakobi@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32770}

--- third_party/libwebrtc/modules/audio_processing/agc2/rnn_vad/rnn_unittest.cc
+++ third_party/libwebrtc/modules/audio_processing/agc2/rnn_vad/rnn_unittest.cc
@@ -10,18 +10,10 @@
 
 #include "modules/audio_processing/agc2/rnn_vad/rnn.h"
 
-#include <array>
-#include <memory>
-#include <vector>
-
+#include "api/array_view.h"
 #include "modules/audio_processing/agc2/cpu_features.h"
-#include "modules/audio_processing/agc2/rnn_vad/test_utils.h"
-#include "modules/audio_processing/test/performance_timer.h"
-#include "rtc_base/checks.h"
-#include "rtc_base/logging.h"
-#include "rtc_base/numerics/safe_conversions.h"
+#include "modules/audio_processing/agc2/rnn_vad/common.h"
 #include "test/gtest.h"
-#include "third_party/rnnoise/src/rnn_vad_weights.h"
 
 namespace webrtc {
 namespace rnn_vad {
@@ -41,116 +33,6 @@ void WarmUpRnnVad(RnnVad& rnn_vad) {
   for (int i = 0; i < 10; ++i) {
     rnn_vad.ComputeVadProbability(kFeatures, /*is_silence=*/false);
   }
-}
-
-void TestGatedRecurrentLayer(
-    GatedRecurrentLayer& gru,
-    rtc::ArrayView<const float> input_sequence,
-    rtc::ArrayView<const float> expected_output_sequence) {
-  const int input_sequence_length = rtc::CheckedDivExact(
-      rtc::dchecked_cast<int>(input_sequence.size()), gru.input_size());
-  const int output_sequence_length = rtc::CheckedDivExact(
-      rtc::dchecked_cast<int>(expected_output_sequence.size()), gru.size());
-  ASSERT_EQ(input_sequence_length, output_sequence_length)
-      << "The test data length is invalid.";
-  // Feed the GRU layer and check the output at every step.
-  gru.Reset();
-  for (int i = 0; i < input_sequence_length; ++i) {
-    SCOPED_TRACE(i);
-    gru.ComputeOutput(
-        input_sequence.subview(i * gru.input_size(), gru.input_size()));
-    const auto expected_output =
-        expected_output_sequence.subview(i * gru.size(), gru.size());
-    ExpectNearAbsolute(expected_output, gru, 3e-6f);
-  }
-}
-
-// Gated recurrent units layer test data.
-constexpr int kGruInputSize = 5;
-constexpr int kGruOutputSize = 4;
-constexpr std::array<int8_t, 12> kGruBias = {96,   -99, -81, -114, 49,  119,
-                                             -118, 68,  -76, 91,   121, 125};
-constexpr std::array<int8_t, 60> kGruWeights = {
-    // Input 0.
-    124, 9, 1, 116,        // Update.
-    -66, -21, -118, -110,  // Reset.
-    104, 75, -23, -51,     // Output.
-    // Input 1.
-    -72, -111, 47, 93,   // Update.
-    77, -98, 41, -8,     // Reset.
-    40, -23, -43, -107,  // Output.
-    // Input 2.
-    9, -73, 30, -32,      // Update.
-    -2, 64, -26, 91,      // Reset.
-    -48, -24, -28, -104,  // Output.
-    // Input 3.
-    74, -46, 116, 15,    // Update.
-    32, 52, -126, -38,   // Reset.
-    -121, 12, -16, 110,  // Output.
-    // Input 4.
-    -95, 66, -103, -35,  // Update.
-    -38, 3, -126, -61,   // Reset.
-    28, 98, -117, -43    // Output.
-};
-constexpr std::array<int8_t, 48> kGruRecurrentWeights = {
-    // Output 0.
-    -3, 87, 50, 51,     // Update.
-    -22, 27, -39, 62,   // Reset.
-    31, -83, -52, -48,  // Output.
-    // Output 1.
-    -6, 83, -19, 104,  // Update.
-    105, 48, 23, 68,   // Reset.
-    23, 40, 7, -120,   // Output.
-    // Output 2.
-    64, -62, 117, 85,     // Update.
-    51, -43, 54, -105,    // Reset.
-    120, 56, -128, -107,  // Output.
-    // Output 3.
-    39, 50, -17, -47,   // Update.
-    -117, 14, 108, 12,  // Reset.
-    -7, -72, 103, -87,  // Output.
-};
-constexpr std::array<float, 20> kGruInputSequence = {
-    0.89395463f, 0.93224651f, 0.55788344f, 0.32341808f, 0.93355054f,
-    0.13475326f, 0.97370994f, 0.14253306f, 0.93710381f, 0.76093364f,
-    0.65780413f, 0.41657975f, 0.49403164f, 0.46843281f, 0.75138855f,
-    0.24517593f, 0.47657707f, 0.57064998f, 0.435184f,   0.19319285f};
-constexpr std::array<float, 16> kGruExpectedOutputSequence = {
-    0.0239123f,  0.5773077f,  0.f,         0.f,
-    0.01282811f, 0.64330572f, 0.f,         0.04863098f,
-    0.00781069f, 0.75267816f, 0.f,         0.02579715f,
-    0.00471378f, 0.59162533f, 0.11087593f, 0.01334511f};
-
-// Checks that the output of a GRU layer is within tolerance given test input
-// data.
-TEST(RnnVadTest, CheckGatedRecurrentLayer) {
-  GatedRecurrentLayer gru(kGruInputSize, kGruOutputSize, kGruBias, kGruWeights,
-                          kGruRecurrentWeights);
-  TestGatedRecurrentLayer(gru, kGruInputSequence, kGruExpectedOutputSequence);
-}
-
-TEST(RnnVadTest, DISABLED_BenchmarkGatedRecurrentLayer) {
-  GatedRecurrentLayer gru(kGruInputSize, kGruOutputSize, kGruBias, kGruWeights,
-                          kGruRecurrentWeights);
-
-  rtc::ArrayView<const float> input_sequence(kGruInputSequence);
-  static_assert(kGruInputSequence.size() % kGruInputSize == 0, "");
-  constexpr int input_sequence_length =
-      kGruInputSequence.size() / kGruInputSize;
-
-  constexpr int kNumTests = 10000;
-  ::webrtc::test::PerformanceTimer perf_timer(kNumTests);
-  for (int k = 0; k < kNumTests; ++k) {
-    perf_timer.StartTimer();
-    for (int i = 0; i < input_sequence_length; ++i) {
-      gru.ComputeOutput(
-          input_sequence.subview(i * gru.input_size(), gru.input_size()));
-    }
-    perf_timer.StopTimer();
-  }
-  RTC_LOG(LS_INFO) << (perf_timer.GetDurationAverage() / 1000) << " +/- "
-                   << (perf_timer.GetDurationStandardDeviation() / 1000)
-                   << " ms";
 }
 
 // Checks that the speech probability is zero with silence.
