9d9191e364f08bb89e7b430492b1d79ad041ec13
Butkovits Atila <abutkovits@mozilla.com>
2022-04-25 17:55:17 +0300

    Backed out 17 changesets (bug 1752332) for causing crashes at JS::AutoAssertNoGC::AutoAssertNoGC(JSContext*). CLOSED TREE
    Backed out changeset eaa4213b9e08 (bug 1752332)
    Backed out changeset 2d794b61fbf7 (bug 1752332)
    Backed out changeset 69cbec3e9a11 (bug 1752332)
    Backed out changeset 022a68e8d603 (bug 1752332)
    Backed out changeset 1ff8656b362d (bug 1752332)
    Backed out changeset 46ea5b4f9ad3 (bug 1752332)
    Backed out changeset e1dcb4c7cb88 (bug 1752332)
    Backed out changeset f9d6bc72406f (bug 1752332)
    Backed out changeset dd02b8ef0219 (bug 1752332)
    Backed out changeset 3e60b77153a3 (bug 1752332)
    Backed out changeset ce93b08837d2 (bug 1752332)
    Backed out changeset d165042105ea (bug 1752332)
    Backed out changeset 22b910308ecd (bug 1752332)
    Backed out changeset d2e748ccd01c (bug 1752332)
    Backed out changeset a89203990075 (bug 1752332)
    Backed out changeset b580c2a3bac0 (bug 1752332)
    Backed out changeset a2d5880b528f (bug 1752332)

--- dom/media/ipc/RDDProcessHost.cpp
+++ dom/media/ipc/RDDProcessHost.cpp
@@ -46,9 +46,9 @@ bool RDDProcessHost::Launch(StringVector aExtraOpts) {
   MOZ_ASSERT(mLaunchPhase == LaunchPhase::Unlaunched);
   MOZ_ASSERT(!mRDDChild);
 
-  mPrefSerializer = MakeUnique<ipc::SharedPreferenceSerializer>();
-  if (!mPrefSerializer->SerializeToSharedMemory(GeckoProcessType_RDD,
-                                                /* remoteType */ ""_ns)) {
+  mPrefSerializer = MakeUnique<ipc::SharedPreferenceSerializer>(
+      dom::ContentParent::ShouldSyncPreference);
+  if (!mPrefSerializer->SerializeToSharedMemory()) {
     return false;
   }
   mPrefSerializer->AddSharedPrefCmdLineArgs(*this, aExtraOpts);
