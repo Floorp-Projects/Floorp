cdbea95799c11009cb28944ed9c961beaf998c70
Michael Froman <mfroman@mozilla.com>
2022-04-26 17:05:00 -0500

    Bug 1766646 - Vendor libwebrtc from 9325d343e5
    Upstream commit: https://webrtc.googlesource.com/src/+/9325d343e57b2a2cf86b73080c71644e61b3c46d
        Enforcing return type handling on VoIP API.
    
        - This CL also affects some return type handling in Android Voip demo
        app due to changes in return type handling.
    
        Bug: webrtc:12193
        Change-Id: Id76faf7c871476ed1f2d08fb587211ae234ae8d3
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/196625
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Per Åhgren <peah@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Commit-Queue: Tim Na <natim@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32821}

--- third_party/libwebrtc/audio/voip/test/voip_core_unittest.cc
+++ third_party/libwebrtc/audio/voip/test/voip_core_unittest.cc
@@ -70,14 +70,18 @@ TEST_F(VoipCoreTest, BasicVoipCoreOperation) {
 
   auto channel = voip_core_->CreateChannel(&transport_, 0xdeadc0de);
 
-  voip_core_->SetSendCodec(channel, kPcmuPayload, kPcmuFormat);
-  voip_core_->SetReceiveCodecs(channel, {{kPcmuPayload, kPcmuFormat}});
+  EXPECT_EQ(voip_core_->SetSendCodec(channel, kPcmuPayload, kPcmuFormat),
+            VoipResult::kOk);
+  EXPECT_EQ(
+      voip_core_->SetReceiveCodecs(channel, {{kPcmuPayload, kPcmuFormat}}),
+      VoipResult::kOk);
 
   EXPECT_EQ(voip_core_->StartSend(channel), VoipResult::kOk);
   EXPECT_EQ(voip_core_->StartPlayout(channel), VoipResult::kOk);
 
-  voip_core_->RegisterTelephoneEventType(channel, kPcmuPayload,
-                                         kPcmuSampleRateHz);
+  EXPECT_EQ(voip_core_->RegisterTelephoneEventType(channel, kPcmuPayload,
+                                                   kPcmuSampleRateHz),
+            VoipResult::kOk);
 
   EXPECT_EQ(
       voip_core_->SendDtmfEvent(channel, kDtmfEventCode, kDtmfEventDurationMs),
@@ -125,7 +129,8 @@ TEST_F(VoipCoreTest, SendDtmfEventWithoutRegistering) {
 
   auto channel = voip_core_->CreateChannel(&transport_, 0xdeadc0de);
 
-  voip_core_->SetSendCodec(channel, kPcmuPayload, kPcmuFormat);
+  EXPECT_EQ(voip_core_->SetSendCodec(channel, kPcmuPayload, kPcmuFormat),
+            VoipResult::kOk);
 
   EXPECT_EQ(voip_core_->StartSend(channel), VoipResult::kOk);
   // Send Dtmf event without registering beforehand, thus payload
@@ -145,8 +150,10 @@ TEST_F(VoipCoreTest, SendDtmfEventWithoutRegistering) {
 TEST_F(VoipCoreTest, SendDtmfEventWithoutStartSend) {
   auto channel = voip_core_->CreateChannel(&transport_, 0xdeadc0de);
 
-  voip_core_->RegisterTelephoneEventType(channel, kPcmuPayload,
-                                         kPcmuSampleRateHz);
+  EXPECT_EQ(voip_core_->RegisterTelephoneEventType(channel, kPcmuPayload,
+                                                   kPcmuSampleRateHz),
+            VoipResult::kOk);
+
   // Send Dtmf event without calling StartSend beforehand, thus
   // Dtmf events cannot be sent and kFailedPrecondition is expected.
   EXPECT_EQ(
