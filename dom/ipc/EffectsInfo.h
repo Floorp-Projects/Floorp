9788952c5a45336a06ef1046148ed123e681b5b2
Razvan Cojocaru <rzvncj@gmail.com>
2022-04-26 03:12:38 +0000

    Bug 1733313 - Change EffectsInfo::mScaleX/Y into a Scale2D and rename to mRasterScale. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D144274

--- dom/ipc/EffectsInfo.h
+++ dom/ipc/EffectsInfo.h
@@ -22,18 +22,17 @@ class EffectsInfo {
   EffectsInfo() { *this = EffectsInfo::FullyHidden(); }
 
   static EffectsInfo VisibleWithinRect(
-      const nsRect& aVisibleRect, float aScaleX, float aScaleY,
+      const nsRect& aVisibleRect, const Scale2D& aRasterScale,
       const ParentLayerToScreenScale2D& aTransformToAncestorScale) {
-    return EffectsInfo{aVisibleRect, aScaleX, aScaleY,
-                       aTransformToAncestorScale};
+    return EffectsInfo{aVisibleRect, aRasterScale, aTransformToAncestorScale};
   }
   static EffectsInfo FullyHidden() {
-    return EffectsInfo{nsRect(), 1.0f, 1.0f, ParentLayerToScreenScale2D()};
+    return EffectsInfo{nsRect(), Scale2D(), ParentLayerToScreenScale2D()};
   }
 
   bool operator==(const EffectsInfo& aOther) {
-    return mVisibleRect == aOther.mVisibleRect && mScaleX == aOther.mScaleX &&
-           mScaleY == aOther.mScaleY &&
+    return mVisibleRect == aOther.mVisibleRect &&
+           mRasterScale == aOther.mRasterScale &&
            mTransformToAncestorScale == aOther.mTransformToAncestorScale;
   }
   bool operator!=(const EffectsInfo& aOther) { return !(*this == aOther); }
@@ -46,8 +45,7 @@ class EffectsInfo {
   // The desired scale factors to apply to rasterized content to match
   // transforms applied in ancestor browsers. This gets propagated into the
   // scale in StackingContextHelper.
-  float mScaleX;
-  float mScaleY;
+  Scale2D mRasterScale;
   // TransformToAncestorScale to be set on FrameMetrics. It includes CSS
   // transform scales and cumulative presshell resolution.
   ParentLayerToScreenScale2D mTransformToAncestorScale;
@@ -62,11 +60,10 @@ class EffectsInfo {
   // TabMessageUtils.
 
  private:
-  EffectsInfo(const nsRect& aVisibleRect, float aScaleX, float aScaleY,
+  EffectsInfo(const nsRect& aVisibleRect, const Scale2D& aRasterScale,
               const ParentLayerToScreenScale2D& aTransformToAncestorScale)
       : mVisibleRect(aVisibleRect),
-        mScaleX(aScaleX),
-        mScaleY(aScaleY),
+        mRasterScale(aRasterScale),
         mTransformToAncestorScale(aTransformToAncestorScale) {}
 };
 
