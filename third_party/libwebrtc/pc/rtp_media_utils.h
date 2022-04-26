c2bc651aca9cbe740efb27ae0397321998f88f94
Michael Froman <mfroman@mozilla.com>
2022-04-26 14:37:40 -0500

    Bug 1766646 - Vendor libwebrtc from b95d90b78a
    Upstream commit: https://webrtc.googlesource.com/src/+/b95d90b78a3491ef8e8aa0640dd521515ec881ca
        Rename UNIT_TEST to WEBRTC_UNIT_TEST
    
        Current name conflicts with upstream project code.
    
        Bug: webrtc:12247
        Change-Id: Ibd78273a75262772fc18fca688c29b9ba9525ce5
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/196653
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Commit-Queue: Andrey Logvin <landrey@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32813}

--- third_party/libwebrtc/pc/rtp_media_utils.h
+++ third_party/libwebrtc/pc/rtp_media_utils.h
@@ -49,13 +49,13 @@ RtpTransceiverDirection RtpTransceiverDirectionIntersection(
     RtpTransceiverDirection lhs,
     RtpTransceiverDirection rhs);
 
-#ifdef UNIT_TEST
+#ifdef WEBRTC_UNIT_TEST
 inline std::ostream& operator<<(  // no-presubmit-check TODO(webrtc:8982)
     std::ostream& os,             // no-presubmit-check TODO(webrtc:8982)
     RtpTransceiverDirection direction) {
   return os << RtpTransceiverDirectionToString(direction);
 }
-#endif  // UNIT_TEST
+#endif  // WEBRTC_UNIT_TEST
 
 }  // namespace webrtc
 
