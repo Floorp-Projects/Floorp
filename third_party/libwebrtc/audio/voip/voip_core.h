8ca7c611dd4d4b0c990c6135fa2da164d088f399
Michael Froman <mfroman@mozilla.com>
2022-04-27 14:12:18 -0500

    Bug 1766646 - Vendor libwebrtc from 37827c9058
    Upstream commit: https://webrtc.googlesource.com/src/+/37827c90582176943c87301b1120d50d0dfb1e58
        Revert "ChannelStatistics used for RTP stats in VoipStatistics."
    
        This reverts commit 444e04be6988fbdcc039d775481ac22481ff9ff4.
    
        Reason for revert: breaks downstream project
    
        Original change's description:
        > ChannelStatistics used for RTP stats in VoipStatistics.
        >
        > - Added local and remote RTP statistics query API.
        > - Change includes simplifying remote SSRC change handling
        >   via received RTP and RTCP packets.
        >
        > Bug: webrtc:11989
        > Change-Id: Ia3ee62c1191baaedc67e033ea3c661d8c9301abc
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/199060
        > Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        > Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        > Reviewed-by: Sam Zackrisson <saza@webrtc.org>
        > Commit-Queue: Tim Na <natim@webrtc.org>
        > Cr-Commit-Position: refs/heads/master@{#32954}
    
        TBR=mbonadei@webrtc.org,saza@webrtc.org,hta@webrtc.org,natim@webrtc.org
    
        Change-Id: I5ce6a698c1216c7d56e32fce3308c16daac852f4
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Bug: webrtc:11989
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/201460
        Reviewed-by: Alex Loiko <aleloi@google.com>
        Commit-Queue: Alex Loiko <aleloi@google.com>
        Cr-Commit-Position: refs/heads/master@{#32956}

--- third_party/libwebrtc/audio/voip/voip_core.h
+++ third_party/libwebrtc/audio/voip/voip_core.h
@@ -109,8 +109,6 @@ class VoipCore : public VoipEngine,
   // Implements VoipStatistics interfaces.
   VoipResult GetIngressStatistics(ChannelId channel_id,
                                   IngressStatistics& ingress_stats) override;
-  VoipResult GetChannelStatistics(ChannelId channe_id,
-                                  ChannelStatistics& channel_stats) override;
 
   // Implements VoipVolumeControl interfaces.
   VoipResult SetInputMuted(ChannelId channel_id, bool enable) override;
