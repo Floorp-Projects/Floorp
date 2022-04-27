304701dfe423860eb817c4f6e5ea065330edc8c1
Michael Froman <mfroman@mozilla.com>
2022-04-27 09:43:55 -0500

    Bug 1766646 - Vendor libwebrtc from 2412602b68
    Upstream commit: https://webrtc.googlesource.com/src/+/2412602b6801ae7495c7506fed47d573660c3545
        Using absl::optional for round trip time return type handling.
    
        No-Try: True
        Bug: webrtc:11989
        Change-Id: If2ed9b83468c03b82b372e64d8012e5786295476
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/197060
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Sam Zackrisson <saza@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32827}

--- third_party/libwebrtc/audio/voip/audio_ingress.h
+++ third_party/libwebrtc/audio/voip/audio_ingress.h
@@ -17,6 +17,7 @@
 #include <memory>
 #include <utility>
 
+#include "absl/types/optional.h"
 #include "api/array_view.h"
 #include "api/audio/audio_mixer.h"
 #include "api/rtp_headers.h"
@@ -78,10 +79,6 @@ class AudioIngress : public AudioMixer::Source {
     return output_audio_level_.TotalDuration();
   }
 
-  // Returns network round trip time (RTT) measued by RTCP exchange with
-  // remote media endpoint. RTT value -1 indicates that it's not initialized.
-  int64_t GetRoundTripTime();
-
   NetworkStatistics GetNetworkStatistics() const {
     NetworkStatistics stats;
     acm_receiver_.GetNetworkStatistics(&stats,
@@ -105,6 +102,10 @@ class AudioIngress : public AudioMixer::Source {
   }
 
  private:
+  // Returns network round trip time (RTT) measued by RTCP exchange with
+  // remote media endpoint. Returns absl::nullopt when it's not initialized.
+  absl::optional<int64_t> GetRoundTripTime();
+
   // Indicates AudioIngress status as caller invokes Start/StopPlaying.
   // If not playing, incoming RTP data processing is skipped, thus
   // producing no data to output device.
