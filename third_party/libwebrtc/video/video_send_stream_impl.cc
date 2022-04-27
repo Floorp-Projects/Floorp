2db4802771ce9858b667cb0feee8d92a89e8e34f
Michael Froman <mfroman@mozilla.com>
2022-04-27 12:24:12 -0500

    Bug 1766646 - Vendor libwebrtc from da06e8f6bd
    Upstream commit: https://webrtc.googlesource.com/src/+/da06e8f6bdb7bda271d9e68300f42e769d896542
        Do not proxy VideoSendStreamImpl::OnVideoLayersAllocationUpdated
    
        OnVideoLayersAllocationUpdated is handled on the encoder task queue in
        order to not race with OnEncodedImage callbacks.
    
        Bug: webrtc:12000
        Change-Id: I1c9a450cce819a7a0f8827aa0bb675c37350a0c2
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/200880
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Commit-Queue: Per Kjellander <perkj@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32935}

--- third_party/libwebrtc/video/video_send_stream_impl.cc
+++ third_party/libwebrtc/video/video_send_stream_impl.cc
@@ -481,15 +481,8 @@ void VideoSendStreamImpl::OnBitrateAllocationUpdated(
 
 void VideoSendStreamImpl::OnVideoLayersAllocationUpdated(
     VideoLayersAllocation allocation) {
-  if (!worker_queue_->IsCurrent()) {
-    auto ptr = weak_ptr_;
-    worker_queue_->PostTask([allocation = std::move(allocation), ptr] {
-      if (!ptr.get())
-        return;
-      ptr->OnVideoLayersAllocationUpdated(allocation);
-    });
-    return;
-  }
+  // OnVideoLayersAllocationUpdated is handled on the encoder task queue in
+  // order to not race with OnEncodedImage callbacks.
   rtp_video_sender_->OnVideoLayersAllocationUpdated(allocation);
 }
 
