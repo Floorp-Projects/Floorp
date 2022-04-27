2ef5924e836ab7c468b30ead34530c015e5080a6
Michael Froman <mfroman@mozilla.com>
2022-04-27 10:16:24 -0500

    Bug 1766646 - Vendor libwebrtc from 8592111e45
    Upstream commit: https://webrtc.googlesource.com/src/+/8592111e458d253e53806db65d23d21bfe9d9db4
        Add empty header to simplify Chromium migration.
    
        In order to land http://crrev.com/c/2593640 and make Chromium
        backwards compatible with [1], a new empty header needs to be created.
    
        [1] - https://webrtc-review.googlesource.com/c/src/+/196903
    
        Bug: webrtc:9987
        Change-Id: I48c4c47b56f9334ce329826c584c817919d59dad
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/197941
        Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32849}

---
+++ third_party/libwebrtc/rtc_base/async_resolver.h
@@ -0,0 +1,17 @@
+/*
+ *  Copyright 2008 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef RTC_BASE_ASYNC_RESOLVER_H_
+#define RTC_BASE_ASYNC_RESOLVER_H_
+
+// Placeholder header for the refactoring in:
+// https://webrtc-review.googlesource.com/c/src/+/196903
+
+#endif  // RTC_BASE_ASYNC_RESOLVER_H_
