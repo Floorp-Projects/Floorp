0d49156bd8376708f42abd97a7ef5ccb8573321e
Tom Ritter <tom@mozilla.com>
2022-04-27 12:57:35 +0000

    Bug 1752332: Rename ShouldSyncPreference to ShouldSanitizePreference r=KrisWright
    This simplifies the number of negations needed,
    and makes things easy to understand. I think
    anyway; I know that without renaming it I made
    several annoying-to-diagnose negation errors...
    
    Depends on D141411
    
    Differential Revision: https://phabricator.services.mozilla.com/D141412

--- dom/media/ipc/RDDProcessManager.cpp
+++ dom/media/ipc/RDDProcessManager.cpp
@@ -97,10 +97,9 @@ void RDDProcessManager::OnPreferenceChange(const char16_t* aData) {
   NS_LossyConvertUTF16toASCII strData(aData);
 
   // A pref changed. If it is useful to do so, inform child processes.
-  if (!ShouldSyncPreference(strData.Data(), false)) {
+  if (ShouldSanitizePreference(strData.Data(), false)) {
     return;
   }
-
   mozilla::dom::Pref pref(strData, /* isLocked */ false,
                           /* isSanitized */ false, Nothing(), Nothing());
   Preferences::GetPreference(&pref);
