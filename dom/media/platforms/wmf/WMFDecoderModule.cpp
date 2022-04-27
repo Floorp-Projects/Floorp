da94d6720cd14f73454a41b52901bcc28280cf45
Zaggy1024 <Zaggy1024@gmail.com>
2022-04-27 23:19:35 +0000

    Bug 1757823 - Use D3D11 APIs to check specific video codec profiles on Windows to determine hardware support. r=alwu
    This will enable hardware decoding for 10-bit VP9 and AV1 on supported systems.
    
    Differential Revision: https://phabricator.services.mozilla.com/D144258

--- dom/media/platforms/wmf/WMFDecoderModule.cpp
+++ dom/media/platforms/wmf/WMFDecoderModule.cpp
@@ -329,6 +329,12 @@ WMFStreamType WMFDecoderModule::GetStreamTypeFromMimeType(
   return WMFStreamType::Unknown;
 }
 
+bool WMFDecoderModule::SupportsColorDepth(
+    gfx::ColorDepth aColorDepth, DecoderDoctorDiagnostics* aDiagnostics) const {
+  // Color depth support can be determined by creating DX decoders.
+  return true;
+}
+
 media::DecodeSupportSet WMFDecoderModule::Supports(
     const SupportDecoderParams& aParams,
     DecoderDoctorDiagnostics* aDiagnostics) const {
