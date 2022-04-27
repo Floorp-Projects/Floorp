902470c2f0e988f65f9d157ee034b881d6167cd4
Meg Viar <lmegviar@gmail.com>
2022-04-27 15:12:41 +0000

    Bug 1764626 - Differentiate between about:welcome and Spotlight context when sending event telemetry r=Mardak
    Differential Revision: https://phabricator.services.mozilla.com/D143645

--- browser/components/newtab/content-src/lib/aboutwelcome-utils.js
+++ browser/components/newtab/content-src/lib/aboutwelcome-utils.js
@@ -2,6 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// If we're in a subdialog, then this is a spotlight modal
+const page = document.querySelector(":root[dialogroot=true]")
+  ? "spotlight"
+  : "about:welcome";
+
 export const AboutWelcomeUtils = {
   handleUserAction(action) {
     window.AWSendToParent("SPECIAL_ACTION", action);
@@ -9,7 +14,10 @@ export const AboutWelcomeUtils = {
   sendImpressionTelemetry(messageId, context) {
     window.AWSendEventTelemetry({
       event: "IMPRESSION",
-      event_context: context,
+      event_context: {
+        ...context,
+        page,
+      },
       message_id: messageId,
     });
   },
@@ -18,7 +26,7 @@ export const AboutWelcomeUtils = {
       event: eventName,
       event_context: {
         source: elementId,
-        page: "about:welcome",
+        page,
       },
       message_id: messageId,
     };
