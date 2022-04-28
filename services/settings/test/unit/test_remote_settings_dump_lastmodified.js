4e4cb627e7df4d83f406194ed03a8d44df8e0b8a
Mathieu Leplatre <mathieu@mozilla.com>
2022-04-28 06:21:19 +0000

    Bug 1719560 - Collect timestamps for all collections r=robwu
    Instead of having complex build scripts that have to introspect which dump is packaged on which platform and in which circumptances, the `last_modified.json` now contains the timestamps of all dumps files present in the source tree.
    
    Differential Revision: https://phabricator.services.mozilla.com/D144169

--- services/settings/test/unit/test_remote_settings_dump_lastmodified.js
+++ services/settings/test/unit/test_remote_settings_dump_lastmodified.js
@@ -1,16 +1,18 @@
 "use strict";
 
-const { AppConstants } = ChromeUtils.import(
-  "resource://gre/modules/AppConstants.jsm"
-);
 const { Utils } = ChromeUtils.import("resource://services-settings/Utils.jsm");
 
 Cu.importGlobalProperties(["fetch"]);
 
 async function getLocalDumpLastModified(bucket, collection) {
-  let res = await fetch(
-    `resource://app/defaults/settings/${bucket}/${collection}.json`
-  );
+  let res;
+  try {
+    res = await fetch(
+      `resource://app/defaults/settings/${bucket}/${collection}.json`
+    );
+  } catch (e) {
+    return -1;
+  }
   const { timestamp } = await res.json();
   ok(timestamp >= 0, `${bucket}/${collection} dump has timestamp`);
   return timestamp;
@@ -30,23 +32,24 @@ add_task(async function lastModified_of_non_existing_dump() {
 });
 
 add_task(async function lastModified_summary_is_correct() {
-  if (AppConstants.platform == "android") {
-    // TODO bug 1719560: When implemented, remove this condition.
-    equal(JSON.stringify(Utils._dumpStats), "{}", "No dumps on Android yet");
-    return;
-  }
   ok(Object.keys(Utils._dumpStats).length > 0, "Contains summary of dumps");
 
+  let checked = 0;
   for (let [identifier, lastModified] of Object.entries(Utils._dumpStats)) {
-    info(`Checking correctness of ${identifier}`);
     let [bucket, collection] = identifier.split("/");
+    let actual = await getLocalDumpLastModified(bucket, collection);
+    if (actual < 0) {
+      info(`${identifier} has no dump, skip.`);
+      continue;
+    }
+    info(`Checking correctness of ${identifier}`);
     equal(
       await Utils.getLocalDumpLastModified(bucket, collection),
       lastModified,
       `Expected last_modified value for ${identifier}`
     );
-
-    let actual = await getLocalDumpLastModified(bucket, collection);
     equal(lastModified, actual, `last_modified should match collection`);
+    checked++;
   }
+  ok(checked > 0, "At least one dump was packaged and checked.");
 });
