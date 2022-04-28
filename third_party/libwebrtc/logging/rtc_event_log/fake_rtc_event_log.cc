c990f796490b63fb4ab4d5eb9d873b53b588700c
Michael Froman <mfroman@mozilla.com>
2022-04-28 12:31:41 -0500

    Bug 1766646 - Vendor libwebrtc from d76dcbd963
    Upstream commit: https://webrtc.googlesource.com/src/+/d76dcbd9634f58f6a0fd5d6330890972d90789c1
        Simplify FakeRtcEventLog, delete rtc::Bind usage
    
        Bug: webrtc:11339
        Change-Id: I2a250934daf0a9114ef8c03464034b1efd8c4c35
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/201722
        Reviewed-by: Björn Terelius <terelius@webrtc.org>
        Commit-Queue: Niels Moller <nisse@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32990}

--- third_party/libwebrtc/logging/rtc_event_log/fake_rtc_event_log.cc
+++ third_party/libwebrtc/logging/rtc_event_log/fake_rtc_event_log.cc
@@ -11,7 +11,6 @@
 #include "logging/rtc_event_log/fake_rtc_event_log.h"
 
 #include "logging/rtc_event_log/events/rtc_event_ice_candidate_pair.h"
-#include "rtc_base/bind.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/logging.h"
 
@@ -33,9 +32,9 @@ void FakeRtcEventLog::StopLogging() {
 
 void FakeRtcEventLog::Log(std::unique_ptr<RtcEvent> event) {
   RtcEvent::Type rtc_event_type = event->GetType();
-  invoker_.AsyncInvoke<void>(
-      RTC_FROM_HERE, thread_,
-      rtc::Bind(&FakeRtcEventLog::IncrementEventCount, this, rtc_event_type));
+  invoker_.AsyncInvoke<void>(RTC_FROM_HERE, thread_, [this, rtc_event_type] {
+    ++count_[rtc_event_type];
+  });
 }
 
 }  // namespace webrtc
