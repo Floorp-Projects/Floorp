c29b73332c87accc16e605ae107bd3d49dbc08cd
Michael Froman <mfroman@mozilla.com>
2022-04-28 10:45:48 -0500

    Bug 1766646 - (fix-a0bb2ef2dc) add back VideoType enum values
    Used in (at least):
      dom/media/systemservices/objc_video_capture/device_info_objc.mm
      dom/media/systemservices/android_video_capture/device_info_android.cc

--- third_party/libwebrtc/common_video/libyuv/webrtc_libyuv.cc
+++ third_party/libwebrtc/common_video/libyuv/webrtc_libyuv.cc
@@ -27,6 +27,8 @@ size_t CalcBufferSize(VideoType type, int width, int height) {
   size_t buffer_size = 0;
   switch (type) {
     case VideoType::kI420:
+    case VideoType::kNV12:
+    case VideoType::kNV21:
     case VideoType::kIYUV:
     case VideoType::kYV12: {
       int half_width = (width + 1) >> 1;
@@ -34,7 +36,9 @@ size_t CalcBufferSize(VideoType type, int width, int height) {
       buffer_size = width * height + half_width * half_height * 2;
       break;
     }
+    case VideoType::kARGB4444:
     case VideoType::kRGB565:
+    case VideoType::kARGB1555:
     case VideoType::kYUY2:
     case VideoType::kUYVY:
       buffer_size = width * height * 2;
@@ -95,6 +99,8 @@ int ConvertVideoType(VideoType video_type) {
       return libyuv::FOURCC_YV12;
     case VideoType::kRGB24:
       return libyuv::FOURCC_24BG;
+    case VideoType::kABGR:
+      return libyuv::FOURCC_ABGR;
     case VideoType::kRGB565:
       return libyuv::FOURCC_RGBP;
     case VideoType::kYUY2:
@@ -103,10 +109,18 @@ int ConvertVideoType(VideoType video_type) {
       return libyuv::FOURCC_UYVY;
     case VideoType::kMJPEG:
       return libyuv::FOURCC_MJPG;
+    case VideoType::kNV21:
+      return libyuv::FOURCC_NV21;
+    case VideoType::kNV12:
+      return libyuv::FOURCC_NV12;
     case VideoType::kARGB:
       return libyuv::FOURCC_ARGB;
     case VideoType::kBGRA:
       return libyuv::FOURCC_BGRA;
+    case VideoType::kARGB4444:
+      return libyuv::FOURCC_R444;
+    case VideoType::kARGB1555:
+      return libyuv::FOURCC_RGBO;
   }
   RTC_NOTREACHED();
   return libyuv::FOURCC_ANY;
