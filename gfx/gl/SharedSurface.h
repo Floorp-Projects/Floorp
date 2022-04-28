ec93b0e3a607d7d1064eb93a30b541c21edecb95
Kelsey Gilbert <jgilbert@mozilla.com>
2022-04-28 19:12:08 +0000

    Bug 1703654 - Prototype display-p3 for WebGL canvas. r=lsalzman,emilio
    Enable (direct) external surface compositing for MacIOSurfaces.
    Works on Mac.
    
    Differential Revision: https://phabricator.services.mozilla.com/D144073

--- gfx/gl/SharedSurface.h
+++ gfx/gl/SharedSurface.h
@@ -60,9 +60,23 @@ struct PartialSharedSurfaceDesc {
   const SharedSurfaceType type;
   const layers::TextureType consumerType;
   const bool canRecycle;
+
+  bool operator==(const PartialSharedSurfaceDesc& rhs) const {
+    return gl == rhs.gl && type == rhs.type &&
+           consumerType == rhs.consumerType && canRecycle == rhs.canRecycle;
+  }
 };
 struct SharedSurfaceDesc : public PartialSharedSurfaceDesc {
   gfx::IntSize size = {};
+  gfx::ColorSpace2 colorSpace = gfx::ColorSpace2::UNKNOWN;
+
+  bool operator==(const SharedSurfaceDesc& rhs) const {
+    return PartialSharedSurfaceDesc::operator==(rhs) && size == rhs.size &&
+           colorSpace == rhs.colorSpace;
+  }
+  bool operator!=(const SharedSurfaceDesc& rhs) const {
+    return !(*this == rhs);
+  }
 };
 
 class SharedSurface {
@@ -166,8 +180,9 @@ class SurfaceFactory {
       const SharedSurfaceDesc&) = 0;
 
  public:
-  UniquePtr<SharedSurface> CreateShared(const gfx::IntSize& size) {
-    return CreateSharedImpl({mDesc, size});
+  UniquePtr<SharedSurface> CreateShared(const gfx::IntSize& size,
+                                        gfx::ColorSpace2 cs) {
+    return CreateSharedImpl({mDesc, size, cs});
   }
 };
 
