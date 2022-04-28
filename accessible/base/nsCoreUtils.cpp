2e1bd1704424870dc4519641370f69e4c7591b50
Morgan Reschenberg <mreschenberg@mozilla.com>
2022-04-28 16:58:19 +0000

    Bug 1763191: Add suppport for cached remote accessibles in coordinate conversion functions r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D142970

--- accessible/base/nsCoreUtils.cpp
+++ accessible/base/nsCoreUtils.cpp
@@ -7,8 +7,6 @@
 
 #include "nsIAccessibleTypes.h"
 
-#include "nsIBaseWindow.h"
-#include "nsIDocShellTreeOwner.h"
 #include "mozilla/dom/Document.h"
 #include "nsRange.h"
 #include "nsXULElement.h"
@@ -320,23 +318,6 @@ void nsCoreUtils::ConvertScrollTypeToPercents(uint32_t aScrollType,
   }
   *aVertical = ScrollAxis(whereY, whenY);
   *aHorizontal = ScrollAxis(whereX, whenX);
-}
-
-LayoutDeviceIntPoint nsCoreUtils::GetScreenCoordsForWindow(nsINode* aNode) {
-  LayoutDeviceIntPoint coords(0, 0);
-  nsCOMPtr<nsIDocShellTreeItem> treeItem(GetDocShellFor(aNode));
-  if (!treeItem) return coords;
-
-  nsCOMPtr<nsIDocShellTreeOwner> treeOwner;
-  treeItem->GetTreeOwner(getter_AddRefs(treeOwner));
-  if (!treeOwner) return coords;
-
-  nsCOMPtr<nsIBaseWindow> baseWindow = do_QueryInterface(treeOwner);
-  if (baseWindow) {
-    baseWindow->GetPosition(&coords.x, &coords.y);  // in device pixels
-  }
-
-  return coords;
 }
 
 already_AddRefed<nsIDocShell> nsCoreUtils::GetDocShellFor(nsINode* aNode) {
