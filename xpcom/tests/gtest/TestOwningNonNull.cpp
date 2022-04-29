dcdf8633ec35de5da367912aa74f13ac9821eb06
Emilio Cobos Álvarez <emilio@crisal.io>
2022-04-29 11:32:07 +0000

    Bug 1766768 - Fix move constructor of OwningNonNull(RefPtr<>&&) to actually move. r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D144987

---
+++ xpcom/tests/gtest/TestOwningNonNull.cpp
@@ -0,0 +1,27 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/OwningNonNull.h"
+#include "mozilla/RefCounted.h"
+#include "mozilla/RefPtr.h"
+#include "gtest/gtest.h"
+
+using namespace mozilla;
+
+struct OwnedRefCounted : public RefCounted<OwnedRefCounted> {
+  MOZ_DECLARE_REFCOUNTED_TYPENAME(OwnedRefCounted)
+
+  OwnedRefCounted() = default;
+
+ private:
+  ~OwnedRefCounted() = default;
+};
+
+TEST(OwningNonNull, Move)
+{
+  auto refptr = MakeRefPtr<OwnedRefCounted>();
+  OwningNonNull<OwnedRefCounted> owning(std::move(refptr));
+  EXPECT_FALSE(!!refptr);
+}
