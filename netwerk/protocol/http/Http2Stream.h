4b8a8160b2cc16fa2a3b55c2efacb8e843815737
edguloien <eguloien@mozilla.com>
2022-04-29 18:36:16 +0000

    Bug 1758524 (WIP) - Implement the http priority incremental flag: added lint fixes and test coverage. Differential Revision: https://phabricator.services.mozilla.com/D142694 r=necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D142965

--- netwerk/protocol/http/Http2Stream.h
+++ netwerk/protocol/http/Http2Stream.h
@@ -24,8 +24,7 @@ namespace mozilla {
 class OriginAttributes;
 }
 
-namespace mozilla {
-namespace net {
+namespace mozilla::net {
 
 class nsStandardURL;
 class Http2Session;
@@ -390,7 +389,6 @@ class Http2Stream : public nsAHttpSegmentReader,
   bool mIsWebsocket;
 };
 
-}  // namespace net
-}  // namespace mozilla
+}  // namespace mozilla::net
 
 #endif  // mozilla_net_Http2Stream_h
