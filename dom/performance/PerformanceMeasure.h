3baf13d0844b87fe78c67bb359db33013a38851b
Michael Comella <michael.l.comella@gmail.com>
2022-04-29 18:15:56 +0000

    Bug 1762482 - update PerformanceMeasure to User Timing L3. r=sefeng,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D143858

--- dom/performance/PerformanceMeasure.h
+++ dom/performance/PerformanceMeasure.h
@@ -15,9 +15,14 @@ namespace dom {
 // http://www.w3.org/TR/user-timing/#performancemeasure
 class PerformanceMeasure final : public PerformanceEntry {
  public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS_INHERITED(PerformanceMeasure,
+                                                         PerformanceEntry);
+
   PerformanceMeasure(nsISupports* aParent, const nsAString& aName,
                      DOMHighResTimeStamp aStartTime,
-                     DOMHighResTimeStamp aEndTime);
+                     DOMHighResTimeStamp aEndTime,
+                     const JS::Handle<JS::Value>& aDetail);
 
   virtual JSObject* WrapObject(JSContext* aCx,
                                JS::Handle<JSObject*> aGivenProto) override;
@@ -26,6 +31,8 @@ class PerformanceMeasure final : public PerformanceEntry {
 
   virtual DOMHighResTimeStamp Duration() const override { return mDuration; }
 
+  void GetDetail(JSContext* aCx, JS::MutableHandle<JS::Value> aRetval);
+
   size_t SizeOfIncludingThis(
       mozilla::MallocSizeOf aMallocSizeOf) const override;
 
@@ -33,6 +40,9 @@ class PerformanceMeasure final : public PerformanceEntry {
   virtual ~PerformanceMeasure();
   DOMHighResTimeStamp mStartTime;
   DOMHighResTimeStamp mDuration;
+
+ private:
+  JS::Heap<JS::Value> mDetail;
 };
 
 }  // namespace dom
