0882489862c0e6ce71f55d7e17ce296be8f2a8c2
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2022-04-29 13:44:12 +0000

    Bug 1756407 - Make about:restartrequired tests more robust r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D144953

--- browser/base/content/test/tabcrashed/browser_aboutRestartRequired_buildid_false-positive.js
+++ browser/base/content/test/tabcrashed/browser_aboutRestartRequired_buildid_false-positive.js
@@ -3,11 +3,11 @@
 // On debug builds, crashing tabs results in much thinking, which
 // slows down the test and results in intermittent test timeouts,
 // so we'll pump up the expected timeout for this test.
-requestLongerTimeout(5);
+requestLongerTimeout(2);
 
 SimpleTest.expectChildProcessCrash();
 
-add_task(async function test_browser_crashed_basic_event() {
+add_task(async function test_browser_crashed_false_positive_event() {
   info("Waiting for oop-browser-crashed event.");
 
   Services.telemetry.clearScalars();
@@ -17,15 +17,19 @@ add_task(async function test_browser_crashed_basic_event() {
     "Build ID mismatch false positive count should be undefined"
   );
 
+  ok(await ensureBuildID(), "System has correct platform.ini");
+  setBuildidMatchDontSendEnv();
   await forceCleanProcesses();
-  let eventPromise = getEventPromise("oop-browser-crashed", "basic");
-  let tab = await openNewTab(true);
+  let eventPromise = getEventPromise("oop-browser-crashed", "false-positive");
+  let tab = await openNewTab(false);
   await eventPromise;
+  unsetBuildidMatchDontSendEnv();
 
   is(
     getFalsePositiveTelemetry(),
-    undefined,
-    "Build ID mismatch false positive count should be undefined"
+    1,
+    "Build ID mismatch false positive count should be 1"
   );
+
   await closeTab(tab);
 });
