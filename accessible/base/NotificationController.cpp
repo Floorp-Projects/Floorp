7ccca95eacf610e8591505e395ed12e531f2d266
James Teh <jteh@mozilla.com>
2022-04-29 00:44:11 +0000

    Bug 1739560: Update line starts in the cache when they change. r=morgan
    If text or bounds change, it's very likely that line starts have changed too, so we push an update for them.
    However, if a non-rectangular text span changes its line wrapping without changing its text, the bounds might not change.
    In that case, we still get a bounds cache update request; we just determine not to push bounds.
    This happens a lot, though.
    To limit this, we compare against the cached first line start in LocalAccessible and push an update only if it's different.
    
    If text and bounds both change, we don't want to push two separate cache updates.
    We use queued cache updates to prevent this.
    This necessitated moving where we send queued cache updates so that we do it before firing mutation events, since clients might need the text to be up to date when handling those events.
    
    Differential Revision: https://phabricator.services.mozilla.com/D144949

--- accessible/base/NotificationController.cpp
+++ accessible/base/NotificationController.cpp
@@ -680,8 +680,6 @@ void NotificationController::WillRefresh(mozilla::TimeStamp aTime) {
 
   mDocument->ProcessPendingUpdates();
 
-  nsTArray<CacheData> cache;
-
   // Process rendered text change notifications.
   for (nsIContent* textNode : mTextHash) {
     LocalAccessible* textAcc = mDocument->GetAccessible(textNode);
@@ -747,10 +745,7 @@ void NotificationController::WillRefresh(mozilla::TimeStamp aTime) {
       TextUpdater::Run(mDocument, textAcc->AsTextLeaf(), text.mString);
       if (IPCAccessibilityActive() &&
           StaticPrefs::accessibility_cache_enabled_AtStartup()) {
-        RefPtr<AccAttributes> fields = textAcc->BundleFieldsForCache(
-            CacheDomain::Text, CacheUpdateType::Update);
-        uint64_t id = reinterpret_cast<uint64_t>(textAcc->UniqueID());
-        cache.AppendElement(CacheData(id, fields));
+        mDocument->QueueCacheUpdate(textAcc, CacheDomain::Text);
       }
       continue;
     }
@@ -780,12 +775,6 @@ void NotificationController::WillRefresh(mozilla::TimeStamp aTime) {
     }
   }
   mTextHash.Clear();
-
-  if (!cache.IsEmpty()) {
-    DocAccessibleChild* ipcDoc = mDocument->IPCDoc();
-    MOZ_ASSERT(ipcDoc);
-    ipcDoc->SendCache(CacheUpdateType::Update, cache, true);
-  }
 
   // Process content inserted notifications to update the tree.
   // Processing an insertion can indirectly run script (e.g. querying a XUL
@@ -893,6 +882,14 @@ void NotificationController::WillRefresh(mozilla::TimeStamp aTime) {
 
   mDocument->SendAccessiblesWillMove();
 
+  // Send any queued cache updates before we fire any mutation events so the
+  // cache is up to date when mutation events are fired. We do this after
+  // insertions (but not their events) so that cache updates dependent on the
+  // tree work correctly; e.g. line start calculation.
+  if (IPCAccessibilityActive() && mDocument) {
+    mDocument->ProcessQueuedCacheUpdates();
+  }
+
   CoalesceMutationEvents();
   ProcessMutationEvents();
   mEventGeneration = 0;
@@ -971,10 +968,6 @@ void NotificationController::WillRefresh(mozilla::TimeStamp aTime) {
       }
 #endif
     }
-  }
-
-  if (IPCAccessibilityActive() && mDocument) {
-    mDocument->ProcessQueuedCacheUpdates();
   }
 
   mObservingState = eRefreshObserving;
