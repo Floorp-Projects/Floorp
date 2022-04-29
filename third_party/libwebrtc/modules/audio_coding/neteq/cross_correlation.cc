f594c5d4fd26b3aeeab2314adca3319c138cea80
Michael Froman <mfroman@mozilla.com>
2022-04-28 21:45:36 -0500

    Bug 1766646 - Vendor libwebrtc from 6031b74664
    Upstream commit: https://webrtc.googlesource.com/src/+/6031b74664d0be40d7f913600d17f6d8d1323729
        Implement a Neon optimized function to find the argmax element in an array.
    
        Finding the array element with the largest argmax is a fairly common
        operation, so it makes sense to have a Neon optimized version. The
        implementation is done by first finding both the min and max value, and
        then returning whichever has the largest argmax.
    
        Bug: chromium:12355
        Change-Id: I088bd4f7d469b2424a7265de10fffb42764567a1
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/201622
        Commit-Queue: Ivo Creusen <ivoc@webrtc.org>
        Reviewed-by: Per Åhgren <peah@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33052}

--- third_party/libwebrtc/modules/audio_coding/neteq/cross_correlation.cc
+++ third_party/libwebrtc/modules/audio_coding/neteq/cross_correlation.cc
@@ -25,22 +25,23 @@ int CrossCorrelationWithAutoShift(const int16_t* sequence_1,
                                   size_t cross_correlation_length,
                                   int cross_correlation_step,
                                   int32_t* cross_correlation) {
-  // Find the maximum absolute value of sequence_1 and 2.
-  const int32_t max_1 =
-      abs(sequence_1[WebRtcSpl_MaxAbsIndexW16(sequence_1, sequence_1_length)]);
+  // Find the element that has the maximum absolute value of sequence_1 and 2.
+  // Note that these values may be negative.
+  const int16_t max_1 =
+      WebRtcSpl_MaxAbsElementW16(sequence_1, sequence_1_length);
   const int sequence_2_shift =
       cross_correlation_step * (static_cast<int>(cross_correlation_length) - 1);
   const int16_t* sequence_2_start =
       sequence_2_shift >= 0 ? sequence_2 : sequence_2 + sequence_2_shift;
   const size_t sequence_2_length =
       sequence_1_length + std::abs(sequence_2_shift);
-  const int32_t max_2 = abs(sequence_2_start[WebRtcSpl_MaxAbsIndexW16(
-      sequence_2_start, sequence_2_length)]);
+  const int16_t max_2 =
+      WebRtcSpl_MaxAbsElementW16(sequence_2_start, sequence_2_length);
 
   // In order to avoid overflow when computing the sum we should scale the
   // samples so that (in_vector_length * max_1 * max_2) will not overflow.
   const int64_t max_value =
-      max_1 * max_2 * static_cast<int64_t>(sequence_1_length);
+      abs(max_1 * max_2) * static_cast<int64_t>(sequence_1_length);
   const int32_t factor = max_value >> 31;
   const int scaling = factor == 0 ? 0 : 31 - WebRtcSpl_NormW32(factor);
 
