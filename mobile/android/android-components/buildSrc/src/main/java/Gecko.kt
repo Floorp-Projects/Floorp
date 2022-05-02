62579e1891eb3294c81bf597264974d7ea4b4983
Arturo Mejia <arturomejiamarmol@gmail.com>
2022-05-02 16:24:16 -0400

    [components] Update to GeckoView 102.0.0

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -9,12 +9,12 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "101.0.20220502145843"
+    const val version = "102.0.20220502144301"
 
     /**
      * GeckoView channel
      */
-    val channel = GeckoChannel.BETA
+    val channel = GeckoChannel.NIGHTLY
 }
 
 /**
