dc0ceddfa341911cf149d3ba1e25fb75bb06aafc
Michael Froman <mfroman@mozilla.com>
2022-05-02 15:21:32 -0500

    Bug 1766646 - Vendor libwebrtc from 483b31c231
    Upstream commit: https://webrtc.googlesource.com/src/+/483b31c23117a9af54c9867e11942e81b90621c2
        Reland "Enable Video-QualityScaling experiment by default"
    
        This time exclude iOS from the default behaviour.
    
        Bug: webrtc:12401
        Change-Id: Ib1f77123b72c3365591b56455332b3d97b307b26
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/205006
        Reviewed-by: Åsa Persson <asapersson@webrtc.org>
        Commit-Queue: Ilya Nikolaevskiy <ilnik@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33173}

--- third_party/libwebrtc/video/video_stream_encoder_unittest.cc
+++ third_party/libwebrtc/video/video_stream_encoder_unittest.cc
@@ -5691,6 +5691,8 @@ TEST_F(VideoStreamEncoderTest, RampsUpInQualityWhenBwIsHigh) {
 
 TEST_F(VideoStreamEncoderTest,
        QualityScalerAdaptationsRemovedWhenQualityScalingDisabled) {
+  webrtc::test::ScopedFieldTrials field_trials(
+      "WebRTC-Video-QualityScaling/Disabled/");
   AdaptingFrameForwarder source(&time_controller_);
   source.set_adaptation_enabled(true);
   video_stream_encoder_->SetSource(&source,
