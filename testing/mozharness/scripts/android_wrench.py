ad9f8c4db60556a148997b8a58513ff469331f21
Jamie Nicol <jnicol@mozilla.com>
2022-05-03 15:37:49 +0000

    Bug 1766990 - Redirect stdout and stderr to file for wrench tests on Android. r=gfx-reviewers,nical
    On Android long lines are truncated by logcat, meaning the base64
    images output by wrench's reftest harness get truncated. The glue code
    used by the old version of cargo-apk automatically split long lines,
    which we then stitched back together again when parsing the
    log. However, the glue used by the new cargo-apk no longer does this.
    
    While we could manually split the long strings ourselves, this method
    has always been unreliable. Instead, this patch makes us additionally
    write stdout and stderr to a file as well as logcat. The test runner
    script then pulls this file from the device and scrapes it rather than
    the logcat.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145076

--- testing/mozharness/scripts/android_wrench.py
+++ testing/mozharness/scripts/android_wrench.py
@@ -11,6 +11,7 @@ import enum
 import os
 import subprocess
 import sys
+import tempfile
 import time
 
 # load modules from parent dir
@@ -39,6 +40,14 @@ class AndroidWrench(TestingMixin, BaseScript, MozbaseMixin, AndroidMixin):
         # pre- and post-flight listeners, so we need _is_emulator be defined
         # before that happens. Doesn't need to be a real value though.
         self._is_emulator = None
+
+        # Directory for wrench input and output files. Note that we hard-code
+        # the path here, rather than using something like self.device.test_root,
+        # because it needs to be kept in sync with the path hard-coded inside
+        # the wrench source code.
+        self.wrench_dir = (
+            "/storage/emulated/0/Android/data/org.mozilla.wrench/files/wrench"
+        )
 
         super(AndroidWrench, self).__init__()
         if self.device_serial is None:
@@ -154,15 +163,12 @@ class AndroidWrench(TestingMixin, BaseScript, MozbaseMixin, AndroidMixin):
         return True
 
     def setup_sdcard(self, test_mode):
-        # Note that we hard-code /sdcard/wrench as the path here, rather than
-        # using something like self.device.test_root, because it needs to be
-        # kept in sync with the path hard-coded inside the wrench source code.
-        wrench_root = "/storage/emulated/0/Android/data/org.mozilla.wrench/files/wrench"
-        self.device.rm(wrench_root, recursive=True, force=True)
-        self.device.mkdir(wrench_root, parents=True)
+        self.device.rm(self.wrench_dir, recursive=True, force=True)
+        self.device.mkdir(self.wrench_dir, parents=True)
         if test_mode == TestMode.REFTEST:
             self.device.push(
-                self.query_abs_dirs()["abs_reftests_path"], wrench_root + "/reftests"
+                self.query_abs_dirs()["abs_reftests_path"],
+                self.wrench_dir + "/reftests",
             )
         args_file = os.path.join(self.query_abs_dirs()["abs_work_dir"], "wrench_args")
         with open(args_file, "w") as argfile:
@@ -178,7 +184,7 @@ class AndroidWrench(TestingMixin, BaseScript, MozbaseMixin, AndroidMixin):
                 argfile.write("--precache test_shaders")
             elif test_mode == TestMode.REFTEST:
                 argfile.write("reftest")
-        self.device.push(args_file, wrench_root + "/args")
+        self.device.push(args_file, self.wrench_dir + "/args")
 
     def run_tests(self, timeout):
         self.timed_screenshots(None)
@@ -193,55 +199,24 @@ class AndroidWrench(TestingMixin, BaseScript, MozbaseMixin, AndroidMixin):
             self._errored = True
             self.error("Wrench still running after timeout")
 
-    def scrape_logcat(self):
-        """Wrench will dump the test output to logcat, but for convenience we
-        want it to show up in the main log. So we scrape it out of the logcat
-        and dump it to our own log. Note that all output from wrench goes
-        through the cargo-apk glue stuff, which uses the RustStdoutStderr
-        tag on the output. Also it limits the line length to 512 bytes
-        (including the null terminator). For reftest unexpected-fail output
-        this means that the base64 image dump gets wrapped over multiple
-        lines, so part of what this function does is unwrap that so that the
-        resulting log is readable by the reftest analyzer."""
-
-        with open(self.logcat_path(), "r", encoding="utf-8") as f:
-            self.info("=== scraped logcat output ===")
-            tag = "RustStdoutStderr: "
-            long_line = None
+    def scrape_log(self):
+        """Wrench dumps stdout to a file rather than logcat because logcat
+        truncates long lines, and the base64 reftest images therefore get
+        truncated. In the past we split long lines and stitched them together
+        again, but this was unreliable. This scrapes the output file and dumps
+        it into our main log.
+        """
+        logfile = tempfile.NamedTemporaryFile()
+        self.device.pull(self.wrench_dir + "/stdout", logfile.name)
+        with open(logfile.name, "r", encoding="utf-8") as f:
+            self.info("=== scraped log output ===")
             for line in f:
-                tag_index = line.find(tag)
-                if tag_index == -1:
-                    # not a line we care about
-                    continue
-                line = line[tag_index + len(tag) :].rstrip()
-                if (
-                    long_line is None
-                    and "REFTEST " not in line
-                    and "panicked" not in line
-                ):
-                    # non-interesting line
-                    continue
-                if long_line is not None:
-                    # continuation of a wrapped line
-                    long_line += line
-                if len(line) >= 511:
-                    if long_line is None:
-                        # start of a new long line
-                        long_line = line
-                    # else "middle" of a long line that keeps going to the next line
-                    continue
-                # this line doesn't wrap over to the next, so we can
-                # print it
-                if long_line is not None:
-                    line = long_line
-                    long_line = None
                 if "UNEXPECTED-FAIL" in line or "panicked" in line:
                     self._errored = True
                     self.error(line)
                 else:
                     self.info(line)
-            self.info("=== end scraped logcat output ===")
-            self.info("(see logcat artifact for full logcat")
+            self.info("=== end scraped log output ===")
 
     def setup_emulator(self):
         avds_dir = self.query_abs_dirs()["abs_avds_dir"]
@@ -270,28 +245,34 @@ class AndroidWrench(TestingMixin, BaseScript, MozbaseMixin, AndroidMixin):
             self.setup_sdcard(TestMode.OPTIMIZED_SHADER_COMPILATION)
             self.info("Running optimized shader compilation tests...")
             self.run_tests(60)
+            self.info("Tests done; parsing log...")
+            self.scrape_log()
 
         if not self._errored:
             self.info("Setting up SD card...")
             self.setup_sdcard(TestMode.UNOPTIMIZED_SHADER_COMPILATION)
             self.info("Running unoptimized shader compilation tests...")
             self.run_tests(60)
+            self.info("Tests done; parsing log...")
+            self.scrape_log()
 
         if not self._errored:
             self.info("Setting up SD card...")
             self.setup_sdcard(TestMode.SHADER_TEST)
             self.info("Running shader tests...")
             self.run_tests(60 * 5)
+            self.info("Tests done; parsing log...")
+            self.scrape_log()
 
         if not self._errored:
             self.info("Setting up SD card...")
             self.setup_sdcard(TestMode.REFTEST)
             self.info("Running reftests...")
             self.run_tests(60 * 30)
+            self.info("Tests done; parsing log...")
+            self.scrape_log()
 
-        self.info("Tests done; parsing logcat...")
         self.logcat_stop()
-        self.scrape_logcat()
         self.info("All done!")
 
     def check_errors(self):
