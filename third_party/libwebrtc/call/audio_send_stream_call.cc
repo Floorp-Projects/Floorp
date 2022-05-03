ee388eec4c75ed0cf31ffddc74ddf80843c3a357
Michael Froman <mfroman@mozilla.com>
2022-05-02 19:04:27 -0500

    Bug 1766646 - Vendor libwebrtc from e44f24e199
    Upstream commit: https://webrtc.googlesource.com/src/+/e44f24e1992330ffbdf71950a1a31f61183a408b
        Handle longer AudioSendStream::Config strings
    
        Switch to using StringBuilder which suports a variable sized
        buffer.
    
        Bug: webrtc:12455
        Change-Id: I956d2385e6a26ce6fbb73869506d9d79de786a2e
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/206473
        Commit-Queue: Evan Shrubsole <eshr@google.com>
        Reviewed-by: Henrik Andreassson <henrika@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33215}

--- third_party/libwebrtc/call/audio_send_stream_call.cc
+++ third_party/libwebrtc/call/audio_send_stream_call.cc
@@ -27,8 +27,7 @@ AudioSendStream::Config::Config(Transport* send_transport)
 AudioSendStream::Config::~Config() = default;
 
 std::string AudioSendStream::Config::ToString() const {
-  char buf[1024];
-  rtc::SimpleStringBuilder ss(buf);
+  rtc::StringBuilder ss;
   ss << "{rtp: " << rtp.ToString();
   ss << ", rtcp_report_interval_ms: " << rtcp_report_interval_ms;
   ss << ", send_transport: " << (send_transport ? "(Transport)" : "null");
@@ -39,8 +38,8 @@ std::string AudioSendStream::Config::ToString() const {
   ss << ", has_dscp: " << (has_dscp ? "true" : "false");
   ss << ", send_codec_spec: "
      << (send_codec_spec ? send_codec_spec->ToString() : "<unset>");
-  ss << '}';
-  return ss.str();
+  ss << "}";
+  return ss.Release();
 }
 
 AudioSendStream::Config::Rtp::Rtp() = default;
