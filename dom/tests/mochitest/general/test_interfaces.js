4319d94cbdd42d6df3f6d50003ff934708716b1b
Tooru Fujisawa <arai_a@mac.com>
2022-05-03 06:32:25 +0000

    Bug 1765156 - Part 3: Use SpecialPowers.ChromeUtils.import in dom/. r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D144084

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -25,9 +25,8 @@
 // channel/OS) or one of the is* constants below (in cases when
 // exposure is affected by channel or OS in a nontrivial way).
 
-const { AppConstants } = SpecialPowers.Cu.import(
-  "resource://gre/modules/AppConstants.jsm",
-  {}
+const { AppConstants } = SpecialPowers.ChromeUtils.import(
+  "resource://gre/modules/AppConstants.jsm"
 );
 
 const isNightly = AppConstants.NIGHTLY_BUILD;
