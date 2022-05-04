de9fd2e9ae29ff5c07e3a63d1914b424a012446e
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2022-05-04 21:02:49 +0000

    Bug 1688879 - Part 1: Add Pref for import maps. r=jonco,yulia
    Add a pref "dom.importMaps.enabled", with default false.
    
    Differential Revision: https://phabricator.services.mozilla.com/D142069

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -3986,6 +3986,8 @@ class Document : public nsINode,
 
   bool ModuleScriptsEnabled();
 
+  bool ImportMapsEnabled();
+
   /**
    * Find the (non-anonymous) content in this document for aFrame. It will
    * be aFrame's content node if that content is in this document and not
