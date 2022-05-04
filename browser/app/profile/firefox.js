1032427dc3d75aca207fc98408ccf4a861c7c51b
Scott <scott.downe@gmail.com>
2022-05-04 17:33:19 +0000

    Bug 1767443 - Pocket newtab save to Pocket button directly to signup r=gvn,nanj
    Differential Revision: https://phabricator.services.mozilla.com/D145367

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1549,6 +1549,8 @@ pref("browser.newtabpage.activity-stream.discoverystream.spocs-endpoint", "");
 pref("browser.newtabpage.activity-stream.discoverystream.spocs-endpoint-query", "");
 pref("browser.newtabpage.activity-stream.discoverystream.sponsored-collections.enabled", false);
 
+pref("browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled", false);
+
 // List of regions that do not get stories, regardless of locale-list-config.
 pref("browser.newtabpage.activity-stream.discoverystream.region-stories-block", "FR");
 // List of locales that get stories, regardless of region-stories-config.
