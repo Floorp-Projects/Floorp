066b3a80a3a11c4c94e8cfc0cdbbfc1758fbd494
Harveer Singh <hsingh@mozilla.com>
2022-05-04 10:11:36 +0000

    Bug 1735124: sequence field in QM_TRY stack propagation now contains seq_no and thread_id information, r=jstutte,dom-storage-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D144988

--- dom/quota/QuotaCommon.cpp
+++ dom/quota/QuotaCommon.cpp
@@ -524,16 +524,25 @@ void LogError(const nsACString& aExpr, const Maybe<nsresult> aMaybeRv,
         res.AppendElement(EventExtraEntry{"result"_ns, nsCString{rvName}});
       }
 
-      // The sequence number is currently per-process, and we don't record the
-      // thread id. This is ok as long as we only record during storage
-      // initialization, and that happens (mostly) from a single thread. It's
-      // safe even if errors from multiple threads are interleaved, but the data
-      // will be hard to analyze then. In that case, we should record a pair of
-      // thread id and thread-local sequence number.
-      static Atomic<int32_t> sSequenceNumber{0};
+      // Here, we are generating thread local sequence number and thread Id
+      // information which could be useful for summarizing and categorizing
+      // log statistics in QM_TRY stack propagation scripts. Since, this is
+      // a thread local object, we do not need to worry about data races.
+      static MOZ_THREAD_LOCAL(uint32_t) sSequenceNumber;
+
+      // This would be initialized once, all subsequent calls would be a no-op.
+      MOZ_ALWAYS_TRUE(sSequenceNumber.init());
+
+      const auto currSeqNum = sSequenceNumber.get();
+      const auto threadId =
+          mozilla::baseprofiler::profiler_current_thread_id().ToNumber();
+      const auto threadIdAndSequence =
+          (static_cast<uint64_t>(threadId) << 32) | (currSeqNum & 0xFFFFFFFF);
 
       res.AppendElement(
-          EventExtraEntry{"seq"_ns, IntToCString(++sSequenceNumber)});
+          EventExtraEntry{"seq"_ns, IntToCString(threadIdAndSequence)});
+
+      sSequenceNumber.set(currSeqNum + 1);
 
       res.AppendElement(EventExtraEntry{"severity"_ns, severityString});
 
