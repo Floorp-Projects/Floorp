26ec9a519308a724d0b1f9b49e8dc0e9bb1fb897
Tom Ritter <tom@mozilla.com>
2022-05-04 15:01:22 +0000

    Bug 1763943: Do not bust on the debugger filename in the Browser Console r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D145452

--- dom/security/nsContentSecurityUtils.cpp
+++ dom/security/nsContentSecurityUtils.cpp
@@ -1381,7 +1381,11 @@ bool nsContentSecurityUtils::ValidateScriptFilename(JSContext* cx,
       // and this is the most reasonable. See 1727770
       u"about:downloads"_ns,
       // We think this is the same problem as about:downloads
-      u"about:preferences"_ns};
+      u"about:preferences"_ns,
+      // Browser console will give a filename of 'debugger' See 1763943
+      // Sometimes it's 'debugger eager eval code', other times just 'debugger
+      // eval code'
+      u"debugger"_ns};
 
   for (auto allowedFilenamePrefix : kAllowedFilenamesPrefix) {
     if (StringBeginsWith(filenameU, allowedFilenamePrefix)) {
