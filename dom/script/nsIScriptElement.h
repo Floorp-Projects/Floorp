0cb3316eb7f655d866fd557de943e17fea0bc0f6
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2022-05-05 16:03:16 +0000

    Bug 1688879 - Part 2: Add support for import maps in ScriptElement/ScriptLoader/ScriptKind. r=jonco,yulia,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D142070

--- dom/script/nsIScriptElement.h
+++ dom/script/nsIScriptElement.h
@@ -7,6 +7,7 @@
 #ifndef nsIScriptElement_h___
 #define nsIScriptElement_h___
 
+#include "js/loader/ScriptKind.h"
 #include "mozilla/AlreadyAddRefed.h"
 #include "mozilla/Assertions.h"
 #include "mozilla/CORSMode.h"
@@ -55,10 +56,10 @@ class nsIScriptElement : public nsIScriptLoaderObserver {
         mForceAsync(aFromParser == mozilla::dom::NOT_FROM_PARSER ||
                     aFromParser == mozilla::dom::FROM_PARSER_FRAGMENT),
         mFrozen(false),
-        mIsModule(false),
         mDefer(false),
         mAsync(false),
         mExternal(false),
+        mKind(JS::loader::ScriptKind::eClassic),
         mParserCreated(aFromParser == mozilla::dom::FROM_PARSER_FRAGMENT
                            ? mozilla::dom::NOT_FROM_PARSER
                            : aFromParser),
@@ -98,6 +99,7 @@ class nsIScriptElement : public nsIScriptLoaderObserver {
    * Freezes the return values of the following methods so that subsequent
    * modifications to the attributes don't change execution behavior:
    *  - GetScriptIsModule()
+   *  - GetScriptIsImportMap()
    *  - GetScriptDeferred()
    *  - GetScriptAsync()
    *  - GetScriptURI()
@@ -110,7 +112,15 @@ class nsIScriptElement : public nsIScriptLoaderObserver {
    */
   bool GetScriptIsModule() {
     MOZ_ASSERT(mFrozen, "Not ready for this call yet!");
-    return mIsModule;
+    return mKind == JS::loader::ScriptKind::eModule;
+  }
+
+  /**
+   * Is the script an import map. Currently only supported by HTML scripts.
+   */
+  bool GetScriptIsImportMap() {
+    MOZ_ASSERT(mFrozen, "Not ready for this call yet!");
+    return mKind == JS::loader::ScriptKind::eImportMap;
   }
 
   /**
@@ -166,10 +176,10 @@ class nsIScriptElement : public nsIScriptLoaderObserver {
 
     // Reset state set by FreezeExecutionAttrs().
     mFrozen = false;
-    mIsModule = false;
     mExternal = false;
     mAsync = false;
     mDefer = false;
+    mKind = JS::loader::ScriptKind::eClassic;
   }
 
   void SetCreatorParser(nsIParser* aParser);
@@ -297,11 +307,6 @@ class nsIScriptElement : public nsIScriptLoaderObserver {
   bool mFrozen;
 
   /**
-   * The effective moduleness.
-   */
-  bool mIsModule;
-
-  /**
    * The effective deferredness.
    */
   bool mDefer;
@@ -316,6 +321,11 @@ class nsIScriptElement : public nsIScriptLoaderObserver {
    * if the src attribute contained an invalid URL string.
    */
   bool mExternal;
+
+  /**
+   * The effective script kind.
+   */
+  JS::loader::ScriptKind mKind;
 
   /**
    * Whether this element was parser-created.
