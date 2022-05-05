91eeec369fb64d04676f0bd58e3788dfe243262c
Ryan Hunt <rhunt@eqrion.net>
2022-05-05 01:31:03 +0000

    Bug 1763645 - wasm: Add "cacheable POD" type traits, and use them. r=yury
    Many data types are copied directly to/from the serialized buffer. This
    is only safe if the data is "cacheable POD". This commit adds type traits
    and assertions for this that will be used with the new serialization code.
    
    A doc comment is added with the full details.
    
    Depends on D143401
    
    Differential Revision: https://phabricator.services.mozilla.com/D143402

--- js/src/wasm/WasmSerialize.h
+++ js/src/wasm/WasmSerialize.h
@@ -19,8 +19,104 @@
 #ifndef wasm_serialize_h
 #define wasm_serialize_h
 
+#include "mozilla/MacroForEach.h"
+#include "mozilla/Maybe.h"
+
+#include <cstdint>
+#include <type_traits>
+
 namespace js {
-namespace wasm {}  // namespace wasm
+namespace wasm {
+
+// [SMDOC] "Cacheable POD"
+//
+// Module serialization relies on copying simple structs to and from the
+// cache format. We need a way to ensure that we only do this on types that are
+// "safe". We call this "cacheable POD". Note: this is not the same thing as
+// "POD" as that may contain pointers, which are not cacheable.
+//
+// We define cacheable POD (C-POD) recursively upon types:
+//   1. any integer type is C-POD
+//   2. any floating point type is C-POD
+//   3. any enum type is C-POD
+//   4. any mozilla::Maybe<T> with T: C-POD is C-POD
+//   5. any T[N] with T: C-POD is C-POD
+//   6. any union where all fields are C-POD is C-POD
+//   7. any struct with the following conditions must is C-POD
+//      * every field's type must be C-POD
+//      * the parent type, if it exists, must also be C-POD
+//      * there must be no virtual methods
+//
+// There are no combination of C++ type traits at this time that can
+// automatically meet these criteria, so we are rolling our own system.
+//
+// We define a "IsCacheablePod" type trait, with builtin rules for cases (1-5).
+// The complex cases (6-7) are handled using manual declaration and checking
+// macros that must be used upon structs and unions that are considered
+// cacheable POD.
+//
+// See the following macros for details:
+//   - WASM_DECLARE_CACHEABLE_POD
+//   - WASM_CHECK_CACHEABLE_POD[_WITH_PARENT]
+
+// The IsCacheablePod type trait primary template. Contains the rules for
+// (cases 1-3).
+template <typename T>
+struct IsCacheablePod
+    : public std::conditional_t<std::is_arithmetic_v<T> || std::is_enum_v<T>,
+                                std::true_type, std::false_type> {};
+
+// Partial specialization for (case 4).
+template <typename T>
+struct IsCacheablePod<mozilla::Maybe<T>>
+    : public std::conditional_t<IsCacheablePod<T>::value, std::true_type,
+                                std::false_type> {};
+
+// Partial specialization for (case 5).
+template <typename T, size_t N>
+struct IsCacheablePod<T[N]>
+    : public std::conditional_t<IsCacheablePod<T>::value, std::true_type,
+                                std::false_type> {};
+
+template <class T>
+inline constexpr bool is_cacheable_pod = IsCacheablePod<T>::value;
+
+// Declare the type 'Type' to be cacheable POD. The definition of the type must
+// contain a WASM_CHECK_CACHEABLE_POD[_WITH_PARENT] to ensure all fields of the
+// type are cacheable POD.
+#define WASM_DECLARE_CACHEABLE_POD(Type)                                      \
+  static_assert(!std::is_polymorphic_v<Type>,                                 \
+                #Type "must not have virtual methods");                       \
+  } /* namespace wasm */                                                      \
+  } /* namespace js */                                                        \
+  template <>                                                                 \
+  struct js::wasm::IsCacheablePod<js::wasm::Type> : public std::true_type {}; \
+  namespace js {                                                              \
+  namespace wasm {
+
+// Helper: check each field's type to be cacheable POD
+#define WASM_CHECK_CACHEABLE_POD_FIELD_(Field)                    \
+  static_assert(js::wasm::IsCacheablePod<decltype(Field)>::value, \
+                #Field " must be cacheable pod");
+
+// Check every field in a type definition to ensure they are cacheable POD.
+#define WASM_CHECK_CACHEABLE_POD(Fields...) \
+  MOZ_FOR_EACH(WASM_CHECK_CACHEABLE_POD_FIELD_, (), (Fields))
+
+// Check every field in a type definition to ensure they are cacheable POD, and
+// check that the parent class is also cacheable POD.
+#define WASM_CHECK_CACHEABLE_POD_WITH_PARENT(Parent, Fields...) \
+  static_assert(js::wasm::IsCacheablePod<Parent>::value,        \
+                #Parent " must be cacheable pod");              \
+  MOZ_FOR_EACH(WASM_CHECK_CACHEABLE_POD_FIELD_, (), (Fields))
+
+// Allow fields that are not cacheable POD but are believed to be safe for
+// serialization due to some justification.
+#define WASM_ALLOW_NON_CACHEABLE_POD_FIELD(Field, Reason)          \
+  static_assert(!js::wasm::IsCacheablePod<decltype(Field)>::value, \
+                #Field " is not cacheable due to " Reason);
+
+}  // namespace wasm
 }  // namespace js
 
 #endif  // wasm_serialize_h
