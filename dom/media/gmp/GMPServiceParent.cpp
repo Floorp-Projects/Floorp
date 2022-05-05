e5f178ecd34d94dc9f4fd381c7653cd9c2373af9
Jeff Muizelaar <jmuizelaar@mozilla.com>
2022-05-05 19:24:18 +0000

    Bug 1768033 - Record more detailed telemetry in has_h264. r=alwu
    This should let us distinguish between pre-load, during-load
    and post-load
    
    Differential Revision: https://phabricator.services.mozilla.com/D145637

--- dom/media/gmp/GMPServiceParent.cpp
+++ dom/media/gmp/GMPServiceParent.cpp
@@ -569,7 +569,20 @@ void GeckoMediaPluginServiceParent::UpdateContentProcessGMPCapabilities(
   typedef mozilla::dom::GMPAPITags GMPAPITags;
   typedef mozilla::dom::ContentParent ContentParent;
 
-  bool has_h264 = false;
+  const uint32_t NO_H264 = 0;
+  const uint32_t HAS_H264 = 1;
+  const uint32_t NO_H264_1_DIR = 2;
+  const uint32_t NO_H264_2_PLUS_DIRS = 3;
+  const uint32_t NO_H264_DIR_IN_PROGRESS = 4;
+  uint32_t hasH264 = NO_H264;
+  if (mDirectoriesAdded == 1) {
+      hasH264 = NO_H264_1_DIR;
+  } else if (mDirectoriesAdded > 1) {
+      hasH264 = NO_H264_2_PLUS_DIRS;
+  }
+  if (mDirectoriesInProgress) {
+      hasH264 = NO_H264_DIR_IN_PROGRESS;
+  }
   nsTArray<GMPCapabilityData> caps;
   {
     MutexAutoLock lock(mMutex);
@@ -595,7 +608,7 @@ void GeckoMediaPluginServiceParent::UpdateContentProcessGMPCapabilities(
         x.capabilities().AppendElement(GMPAPITags(tag.mAPIName, tag.mAPITags));
         if (tag.mAPIName == nsLiteralCString(GMP_API_VIDEO_ENCODER) &&
             tag.mAPITags.Contains("h264"_ns)) {
-          has_h264 = true;
+          hasH264 = HAS_H264;
         }
       }
       caps.AppendElement(std::move(x));
@@ -603,7 +616,7 @@ void GeckoMediaPluginServiceParent::UpdateContentProcessGMPCapabilities(
   }
 
   Telemetry::Accumulate(Telemetry::MEDIA_GMP_UPDATE_CONTENT_PROCESS_HAS_H264,
-                        has_h264);
+                        hasH264);
 
   if (aContentProcess) {
     Unused << aContentProcess->SendGMPsChanged(caps);
@@ -713,6 +726,9 @@ RefPtr<GenericPromise> GeckoMediaPluginServiceParent::AsyncAddPluginDirectory(
     return GenericPromise::CreateAndReject(NS_ERROR_FAILURE, __func__);
   }
 
+  mDirectoriesAdded++;
+  mDirectoriesInProgress++;
+
   nsString dir(aDirectory);
   RefPtr<GeckoMediaPluginServiceParent> self = this;
   return InvokeAsync(thread, this, __func__,
@@ -725,14 +741,16 @@ RefPtr<GenericPromise> GeckoMediaPluginServiceParent::AsyncAddPluginDirectory(
                 "succeeded",
                 NS_ConvertUTF16toUTF8(dir).get());
             MOZ_ASSERT(NS_IsMainThread());
+            self->mDirectoriesInProgress--;
             self->UpdateContentProcessGMPCapabilities();
             return GenericPromise::CreateAndResolve(aVal, __func__);
           },
-          [dir](nsresult aResult) {
+          [dir, self](nsresult aResult) {
             GMP_LOG_DEBUG(
                 "GeckoMediaPluginServiceParent::AsyncAddPluginDirectory %s "
                 "failed",
                 NS_ConvertUTF16toUTF8(dir).get());
+            self->mDirectoriesInProgress--;
             return GenericPromise::CreateAndReject(aResult, __func__);
           });
 }
