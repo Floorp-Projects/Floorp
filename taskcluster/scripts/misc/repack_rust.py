c893adc6fc29ff2bc4a3d5ee77a43d9ad261e864
Mike Hommey <mh+mozilla@glandium.org>
2022-05-06 12:26:38 +0000

    Bug 1767917 - Remove rust compiler-builtins-hack. r=firefox-build-system-reviewers,andi
    The underlying issue in the LLVM gold plugin now has a proposed fix that we
    can use to remove the hack.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145539

--- taskcluster/scripts/misc/repack_rust.py
+++ taskcluster/scripts/misc/repack_rust.py
@@ -13,14 +13,12 @@ from __future__ import absolute_import, print_function
 
 import argparse
 import errno
-import glob
 import hashlib
 import os
 import shutil
 import subprocess
 from contextlib import contextmanager
 import tarfile
-import tempfile
 import textwrap
 
 import requests
@@ -459,7 +457,6 @@ def repack(
     targets,
     channel="stable",
     cargo_channel=None,
-    compiler_builtins_hack=False,
     patches=[],
 ):
     install_dir = "rustc"
@@ -509,60 +506,6 @@ def repack(
         for std in stds:
             install(os.path.basename(std["url"]), install_dir)
             pass
-    # Workaround for https://github.com/rust-lang/rust/issues/74657:
-    # Remove the .llvmbc and .llvmcmd sections (sections for the LLVM bitcode)
-    # from the compiler_builtins rlib.
-    hack_targets = ()
-    if compiler_builtins_hack:
-        hack_targets = (
-            "x86_64-unknown-linux-gnu",
-            "i686-unknown-linux-gnu",
-            "x86_64-linux-android",
-            "i686-linux-android",
-            "thumbv7neon-linux-androideabi",
-            "aarch64-linux-android",
-        )
-        llvm_bin = os.path.join(os.environ["MOZ_FETCHES_DIR"], "clang", "bin")
-    for t in hack_targets:
-        if t not in targets:
-            continue
-        for lib in glob.glob(
-            os.path.join(
-                install_dir, "lib", "rustlib", t, "lib", "libcompiler_builtins*"
-            )
-        ):
-            log("Postprocessing %s" % lib)
-            with tempfile.TemporaryDirectory() as d:
-                # Extract all the files from the .rlib
-                subprocess.check_call(
-                    [os.path.join(llvm_bin, "llvm-ar"), "x", os.path.abspath(lib)],
-                    cwd=d,
-                )
-                files = os.listdir(d)
-                for f in files:
-                    if not f.endswith(".o"):
-                        continue
-                    # For each .o file, remove the aforementioned sections.
-                    subprocess.check_call(
-                        [
-                            os.path.join(llvm_bin, "llvm-objcopy"),
-                            "-R",
-                            ".llvmbc",
-                            "-R",
-                            ".llvmcmd",
-                            f,
-                        ],
-                        cwd=d,
-                    )
-                # Create a new .rlib with the updated object files.
-                subprocess.check_call(
-                    [os.path.join(llvm_bin, "llvm-ar"), "crv", os.path.abspath(lib)]
-                    + files,
-                    cwd=d,
-                )
-                subprocess.check_call(
-                    [os.path.join(llvm_bin, "llvm-ranlib"), os.path.abspath(lib)], cwd=d
-                )
 
     log("Creating archive...")
     tar_file = install_dir + ".tar.zst"
@@ -689,11 +632,6 @@ def args():
         "--cargo-channel",
         help="Release channel version to use for cargo."
         " Defaults to the same as --channel.",
-    )
-    parser.add_argument(
-        "--compiler-builtins-hack",
-        action="store_true",
-        help="Enable workaround for " "https://github.com/rust-lang/rust/issues/74657.",
     )
     parser.add_argument(
         "--host",
