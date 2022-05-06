3009742293a46d43f025b965a9277842f03d80e5
Michael Froman <mfroman@mozilla.com>
2022-05-06 13:28:30 -0500

    Bug 1766646 - Vendor libwebrtc from 6512af04ba
    Upstream commit: https://webrtc.googlesource.com/src/+/6512af04bad7435ca2e51f4ba8fb069ba318e456
        Add root doc site definition for WebRTC documentation
    
        Bug: None
        Change-Id: I64f5a356e0358360bd8326c39c8b0a898b879ebf
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/208641
        Commit-Queue: Artem Titov <titovartem@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33360}

---
+++ third_party/libwebrtc/g3doc/g3doc.lua
@@ -0,0 +1,18 @@
+return {
+  theme = {
+    '@builtins/theme/ng.md'
+    -- We don't want to have more than h3 headings in the Table Of Content.
+    --toc_level = 3,
+  },
+
+  site = {
+    name = 'WebRTC C++ library',
+    home = '/g3doc/index.md',
+    logo = '/g3doc/logo.svg',
+    map  = '/g3doc/sitemap.md',
+  },
+
+  visibility = { '/...' },
+
+  freshness = {}
+}
