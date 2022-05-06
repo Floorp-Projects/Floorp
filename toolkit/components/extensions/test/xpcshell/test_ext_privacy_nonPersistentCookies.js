d8f780573683e931516aabe5cec0752ed90faaa2
William Durand <will+git@drnd.me>
2022-05-06 18:03:31 +0000

    Bug 1754924 - Deprecate nonPersistentCookies option from the privacy.websites.cookieConfig API. r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D145249

---
+++ toolkit/components/extensions/test/xpcshell/test_ext_privacy_nonPersistentCookies.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const { promiseShutdownManager, promiseStartupManager } = AddonTestUtils;
+
+AddonTestUtils.init(this);
+AddonTestUtils.overrideCertDB();
+AddonTestUtils.createAppInfo(
+  "xpcshell@tests.mozilla.org",
+  "XPCShell",
+  "1",
+  "42"
+);
+
+add_task(async function test_nonPersistentCookies_is_deprecated() {
+  await promiseStartupManager();
+
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      permissions: ["privacy"],
+    },
+    useAddonManager: "temporary",
+    async background() {
+      for (const nonPersistentCookies of [true, false]) {
+        await browser.privacy.websites.cookieConfig.set({
+          value: {
+            behavior: "reject_third_party",
+            nonPersistentCookies,
+          },
+        });
+      }
+
+      browser.test.sendMessage("background-done");
+    },
+  });
+
+  let { messages } = await promiseConsoleOutput(async () => {
+    await extension.startup();
+    await extension.awaitMessage("background-done");
+    await extension.unload();
+  });
+
+  const expectedMessage = /"'nonPersistentCookies' has been deprecated and it has no effect anymore."/;
+
+  AddonTestUtils.checkMessages(
+    messages,
+    {
+      expected: [{ message: expectedMessage }, { message: expectedMessage }],
+    },
+    true
+  );
+
+  await promiseShutdownManager();
+});
