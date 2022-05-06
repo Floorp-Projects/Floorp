804556edecabad55a4bb7097bfc5ed1f51331165
Nicolas Chevobbe <nchevobbe@mozilla.com>
2022-05-06 17:16:27 +0000

    Bug 1668219 - [devtools] Remove devtools/client/performance. r=julienw.
    Differential Revision: https://phabricator.services.mozilla.com/D145461

--- devtools/client/performance/test/helpers/urls.js
+++
@@ -1,9 +0,0 @@
-/* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
-"use strict";
-
-exports.EXAMPLE_URL =
-  "http://example.com/browser/devtools/client/performance/test";
-exports.SIMPLE_URL = `${exports.EXAMPLE_URL}/doc_simple-test.html`;
-// Used to test a page running on main process.
-exports.MAIN_PROCESS_URL = "about:robots";
