c79a63772a92b7aeafafd23e84d6b717ac39312d
Michael Froman <mfroman@mozilla.com>
2022-05-06 14:59:10 -0500

    Bug 1766646 - Vendor libwebrtc from 89cb65ed66
    Upstream commit: https://webrtc.googlesource.com/src/+/89cb65ed663a9000b9f7c90a78039bd85731e9ae
        [Battery]: Delay start of TaskQueuePacedSender.
    
        To avoid unnecessary repeating tasks, TaskQueuePacedSender is started
        only upon RtpTransportControllerSend::EnsureStarted().
    
        More specifically, the repeating task happens in
        TaskQueuePacedSender::MaybeProcessPackets() every 500ms, using a self
        task_queue_.PostDelayedTask().
    
        Bug: chromium:1152887
        Change-Id: I72c96d2c4b491d5edb45a30b210b3797165cbf48
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/208560
        Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
        Reviewed-by: Henrik Boström <hbos@webrtc.org>
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33421}

--- third_party/libwebrtc/call/rtp_transport_controller_send.cc
+++ third_party/libwebrtc/call/rtp_transport_controller_send.cc
@@ -133,9 +133,6 @@ RtpTransportControllerSend::RtpTransportControllerSend(
   initial_config_.key_value_config = trials;
   RTC_DCHECK(bitrate_config.start_bitrate_bps > 0);
 
-  pacer()->SetPacingRates(
-      DataRate::BitsPerSec(bitrate_config.start_bitrate_bps), DataRate::Zero());
-
   if (absl::StartsWith(trials->Lookup("WebRTC-LazyPacerStart"), "Disabled")) {
     EnsureStarted();
   }
@@ -499,6 +496,10 @@ void RtpTransportControllerSend::IncludeOverheadInPacedSender() {
 void RtpTransportControllerSend::EnsureStarted() {
   if (!use_task_queue_pacer_ && !process_thread_started_) {
     process_thread_started_ = true;
+    pacer()->SetPacingRates(
+        DataRate::BitsPerSec(
+            bitrate_configurator_.GetConfig().start_bitrate_bps),
+        DataRate::Zero());
     process_thread_->Start();
   }
 }
