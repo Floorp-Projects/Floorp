f2e743421949542995d2c6f32154c548c77c7489
Neil Deakin <neil@mozilla.com>
2022-05-06 10:00:57 +0000

    Bug 1746052, on Mac, set the always ask flag for the macbinary type as well, r=Gijs
    This test involves clicking on a link to a .bin file. In the Mac test environment, this is given the macbinary content type. This test assumes that it will be octet-stream (another valid content type for the bin extension) and sets the alwaysAskBeforeHandling flag for that, so the dialog never shows up. However, another subtest needs octet-stream to prompt as well, so set the always ask for both.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145547

--- uriloader/exthandler/tests/mochitest/browser_auto_close_window.js
+++ uriloader/exthandler/tests/mochitest/browser_auto_close_window.js
@@ -67,8 +67,22 @@ add_setup(async function() {
   mimeInfo.alwaysAskBeforeHandling = true;
   HandlerService.store(mimeInfo);
 
+  // On Mac, .bin is application/macbinary
+  let mimeInfoMac;
+  if (AppConstants.platform == "macosx") {
+    mimeInfoMac = MIMEService.getFromTypeAndExtension(
+      "application/macbinary",
+      "bin"
+    );
+    mimeInfoMac.alwaysAskBeforeHandling = true;
+    HandlerService.store(mimeInfoMac);
+  }
+
   registerCleanupFunction(() => {
     HandlerService.remove(mimeInfo);
+    if (mimeInfoMac) {
+      HandlerService.remove(mimeInfoMac);
+    }
   });
 });
 
