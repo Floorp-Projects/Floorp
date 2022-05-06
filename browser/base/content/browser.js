47543cc3115e626981cd2e0ddda5ef6bbd0872c3
Molnar Sandor <smolnar@mozilla.com>
2022-05-06 03:11:13 +0300

    Backed out changeset cf50b25d52a0 (bug 1749998) for causing browser-chrome failures in browser_downloads_panel_opens. CLOSED TREE

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -533,13 +533,6 @@ XPCOMUtils.defineLazyPreferenceGetter(
   false
 );
 
-XPCOMUtils.defineLazyPreferenceGetter(
-  this,
-  "gAlwaysOpenPanel",
-  "browser.download.alwaysOpenPanel",
-  true
-);
-
 customElements.setElementCreationCallback("translation-notification", () => {
   Services.scriptloader.loadSubScript(
     "chrome://browser/content/translation-notification.js",
@@ -7322,27 +7315,6 @@ var ToolbarContextMenu = {
   onDownloadsAutoHideChange(event) {
     let autoHide = event.target.getAttribute("checked") == "true";
     Services.prefs.setBoolPref("browser.download.autohideButton", autoHide);
-  },
-
-  updateDownloadsAlwaysOpenPanel(popup) {
-    let separator = document.getElementById(
-      "toolbarDownloadsAnchorMenuSeparator"
-    );
-    let checkbox = document.getElementById(
-      "toolbar-context-always-open-downloads-panel"
-    );
-    let isDownloads =
-      popup.triggerNode &&
-      ["downloads-button", "wrapper-downloads-button"].includes(
-        popup.triggerNode.id
-      );
-    separator.hidden = checkbox.hidden = !isDownloads;
-    checkbox.toggleAttribute("checked", gAlwaysOpenPanel);
-  },
-
-  onDownloadsAlwaysOpenPanelChange(event) {
-    let alwaysOpen = event.target.getAttribute("checked") == "true";
-    Services.prefs.setBoolPref("browser.download.alwaysOpenPanel", alwaysOpen);
   },
 
   _getUnwrappedTriggerNode(popup) {
