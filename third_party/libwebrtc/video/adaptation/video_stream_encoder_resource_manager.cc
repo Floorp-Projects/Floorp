5b9abbf73b1ba32ae7a419c737ff18bcdccf5b0f
Michael Froman <mfroman@mozilla.com>
2022-05-06 14:08:27 -0500

    Bug 1766646 - Vendor libwebrtc from 964a88fce1
    Upstream commit: https://webrtc.googlesource.com/src/+/964a88fce1adf9637594d169d7e72823fe344dea
        Prevent possible out-of-bounds access
    
        It read from simulcast_layers[0] even if vector is empty.
    
        Bug: none
        Change-Id: I293890feda70022beae4247ab10cf8182b4cf4a5
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/209706
        Reviewed-by: Åsa Persson <asapersson@webrtc.org>
        Commit-Queue: Sergey Silkin <ssilkin@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33391}

--- third_party/libwebrtc/video/adaptation/video_stream_encoder_resource_manager.cc
+++ third_party/libwebrtc/video/adaptation/video_stream_encoder_resource_manager.cc
@@ -720,16 +720,20 @@ bool VideoStreamEncoderResourceManager::IsSimulcast(
     const VideoEncoderConfig& encoder_config) {
   const std::vector<VideoStream>& simulcast_layers =
       encoder_config.simulcast_layers;
+  if (simulcast_layers.size() <= 1) {
+    return false;
+  }
 
-  bool is_simulcast = simulcast_layers.size() > 1;
-  bool is_lowest_layer_active = simulcast_layers[0].active;
+  if (simulcast_layers[0].active) {
+    // We can't distinguish between simulcast and singlecast when only the
+    // lowest spatial layer is active. Treat this case as simulcast.
+    return true;
+  }
+
   int num_active_layers =
       std::count_if(simulcast_layers.begin(), simulcast_layers.end(),
                     [](const VideoStream& layer) { return layer.active; });
-
-  // We can't distinguish between simulcast and singlecast when only the
-  // lowest spatial layer is active. Treat this case as simulcast.
-  return is_simulcast && (num_active_layers > 1 || is_lowest_layer_active);
+  return num_active_layers > 1;
 }
 
 }  // namespace webrtc
