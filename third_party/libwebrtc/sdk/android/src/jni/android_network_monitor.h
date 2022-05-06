e5329b6c197409a643e86a9a63c3c6f725416041
Michael Froman <mfroman@mozilla.com>
2022-05-06 16:59:49 -0500

    Bug 1766646 - Vendor libwebrtc from 6ca955a1dd
    Upstream commit: https://webrtc.googlesource.com/src/+/6ca955a1ddeee50d1ea3bef4d8ad5b7e97cc0dd1
        Reland "Fix problem with ipv4 over ipv6 on Android"
    
        This reverts commit 1e60490ddb11afecc796058802fbc80867d851d8.
    
        Reason for revert: Downstream project has been fixed (127.0.0.1 is not PII)
    
        Original change's description:
        > Revert "Fix problem with ipv4 over ipv6 on Android"
        >
        > This reverts commit da2fd2a2b25ee4bd7b383424cb26d51fb6cc7716,
        > as well as follow-up b7227a5a10f233cec04642f15a0233e7355bd340,
        > "Fix handling of partial match for GetVpnUnderlyingAdapterType".
        >
        > Reason for revert: Breaks downstream test.
        >
        > First change's description:
        > > Fix problem with ipv4 over ipv6 on Android
        > >
        > > This patch fixes a problem with using ipv4 over ipv6
        > > addresses on Android. These addresses are discovered
        > > using 'getifaddr' with interfaces called 'v4-wlan0' or
        > > 'v4-rmnet' but the Android API does not report them.
        > >
        > > This leads to failure when BasicPortAllocator tries
        > > to bind a socket to the ip-address, making the ipv4
        > > address unusable.
        > >
        > > This solution does the following
        > > 1) Insert BasicNetworkManager as NetworkBinderInterface
        > > rather than AndroidNetworkManager.
        > >
        > > 2) When SocketServer calls BindSocketToNetwork,
        > > BasicNetworkManager first lookup the interface name,
        > > and then calls AndroidNetworkManager.
        > >
        > > 3) AndroidNetworkManager will then first try to bind
        > > using the known ip-addresses, and if it can't find the network
        > > it will instead match the interface names.
        > >
        > > The patch has been tested on real android devices, and works fine.
        > > And everything is disabled by default, and is enabled by field trial.
        > >
        > > My plan is to rollout the feature, checking that it does not introduce
        > > any problems, and if so, enabled for all.
        > >
        > > Bug: webrtc:10707
        > > Change-Id: I7081ba43d4ce17077acfa5fbab44eda127ac3971
        > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/211003
        > > Commit-Queue: Jonas Oreland <jonaso@webrtc.org>
        > > Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        > > Cr-Commit-Position: refs/heads/master@{#33422}
        >
        > Second change's description:
        > > Fix handling of partial match for GetVpnUnderlyingAdapterType
        > >
        > > This is a followup to https://webrtc-review.googlesource.com/c/src/+/211003
        > > and fixes the problem pointed out by deadbeef@, thanks!
        > >
        > > Bug: webrtc:10707
        > > Change-Id: I8dea842b25ba15416353ce4002356183087873c7
        > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/211344
        > > Commit-Queue: Jonas Oreland <jonaso@webrtc.org>
        > > Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        > > Cr-Commit-Position: refs/heads/master@{#33436}
        >
        > TBR=hta@webrtc.org,jonaso@webrtc.org
        > NOTRY=True
        >
        > Bug: webrtc:10707
        > Change-Id: Ib13127fbf087c7f34ca0ccc6ce1805706f01d19d
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/211740
        > Reviewed-by: Taylor <deadbeef@webrtc.org>
        > Commit-Queue: Taylor <deadbeef@webrtc.org>
        > Cr-Commit-Position: refs/heads/master@{#33453}
    
        # Not skipping CQ checks because original CL landed > 1 day ago.
    
        Bug: webrtc:10707
        Change-Id: I0a11025c366c3127e2f57cd2cd2c33cc3877d1e3
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/211862
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Jonas Oreland <jonaso@webrtc.org>
        Commit-Queue: Jonas Oreland <jonaso@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33460}

--- third_party/libwebrtc/sdk/android/src/jni/android_network_monitor.h
+++ third_party/libwebrtc/sdk/android/src/jni/android_network_monitor.h
@@ -64,8 +64,7 @@ struct NetworkInformation {
   std::string ToString() const;
 };
 
-class AndroidNetworkMonitor : public rtc::NetworkMonitorInterface,
-                              public rtc::NetworkBinderInterface {
+class AndroidNetworkMonitor : public rtc::NetworkMonitorInterface {
  public:
   AndroidNetworkMonitor(JNIEnv* env,
                         const JavaRef<jobject>& j_application_context);
@@ -77,9 +76,14 @@ class AndroidNetworkMonitor : public rtc::NetworkMonitorInterface,
   void Start() override;
   void Stop() override;
 
+  // Does |this| NetworkMonitorInterface implement BindSocketToNetwork?
+  // Only Android returns true.
+  virtual bool SupportsBindSocketToNetwork() const override { return true; }
+
   rtc::NetworkBindingResult BindSocketToNetwork(
       int socket_fd,
-      const rtc::IPAddress& address) override;
+      const rtc::IPAddress& address,
+      const std::string& if_name) override;
   rtc::AdapterType GetAdapterType(const std::string& if_name) override;
   rtc::AdapterType GetVpnUnderlyingAdapterType(
       const std::string& if_name) override;
@@ -106,14 +110,18 @@ class AndroidNetworkMonitor : public rtc::NetworkMonitorInterface,
                                  jint preference);
 
   // Visible for testing.
-  absl::optional<NetworkHandle> FindNetworkHandleFromAddress(
-      const rtc::IPAddress& address) const;
+  absl::optional<NetworkHandle> FindNetworkHandleFromAddressOrName(
+      const rtc::IPAddress& address,
+      const std::string& ifname) const;
 
  private:
   void OnNetworkConnected_n(const NetworkInformation& network_info);
   void OnNetworkDisconnected_n(NetworkHandle network_handle);
   void OnNetworkPreference_n(NetworkType type,
                              rtc::NetworkPreference preference);
+
+  absl::optional<NetworkHandle> FindNetworkHandleFromIfname(
+      const std::string& ifname) const;
 
   const int android_sdk_int_;
   ScopedJavaGlobalRef<jobject> j_application_context_;
@@ -133,6 +141,14 @@ class AndroidNetworkMonitor : public rtc::NetworkMonitorInterface,
   bool find_network_handle_without_ipv6_temporary_part_
       RTC_GUARDED_BY(network_thread_) = false;
   bool surface_cellular_types_ RTC_GUARDED_BY(network_thread_) = false;
+
+  // NOTE: if bind_using_ifname_ is TRUE
+  // then the adapter name is used with substring matching as follows:
+  // An adapater name repored by android as 'wlan0'
+  // will be matched with 'v4-wlan0' ("v4-wlan0".find("wlan0") != npos).
+  // This applies to adapter_type_by_name_, vpn_underlying_adapter_type_by_name_
+  // and FindNetworkHandleFromIfname.
+  bool bind_using_ifname_ RTC_GUARDED_BY(network_thread_) = true;
   const rtc::scoped_refptr<PendingTaskSafetyFlag> safety_flag_
       RTC_PT_GUARDED_BY(network_thread_);
 };
