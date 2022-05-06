4b369db9736ebb65c1d4ca0562591495564efbf8
Michael Froman <mfroman@mozilla.com>
2022-05-06 13:25:18 -0500

    Bug 1766646 - Vendor libwebrtc from df6d4ca3f5
    Upstream commit: https://webrtc.googlesource.com/src/+/df6d4ca3f55f8073a6be8fb634541281e3ea0f69
        Prevent TURN from connecting to ports < 1024 (except 443)
    
        This should prevent TURN from being used in NAT slipstream attacks
        with native clients.
    
        Bug: webrtc:12497
        Change-Id: I3c33df92e97b8e6430b72a05790c137d89661093
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/208582
        Commit-Queue: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Philipp Hancke <philipp.hancke@googlemail.com>
        Reviewed-by: Jonas Oreland <jonaso@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33356}

--- third_party/libwebrtc/p2p/base/turn_port.cc
+++ third_party/libwebrtc/p2p/base/turn_port.cc
@@ -346,6 +346,15 @@ void TurnPort::PrepareAddress() {
     server_address_.address.SetPort(TURN_DEFAULT_PORT);
   }
 
+  if (!AllowedTurnPort(server_address_.address.port())) {
+    // This can only happen after a 300 ALTERNATE SERVER, since the port can't
+    // be created with a disallowed port number.
+    RTC_LOG(LS_ERROR) << "Attempt to start allocation with disallowed port# "
+                      << server_address_.address.port();
+    OnAllocateError(STUN_ERROR_SERVER_ERROR,
+                    "Attempt to start allocation to a disallowed port");
+    return;
+  }
   if (server_address_.address.IsUnresolvedIP()) {
     ResolveTurnAddress(server_address_.address);
   } else {
@@ -941,6 +950,21 @@ void TurnPort::Close() {
 
 rtc::DiffServCodePoint TurnPort::StunDscpValue() const {
   return stun_dscp_value_;
+}
+
+// static
+bool TurnPort::AllowedTurnPort(int port) {
+  // Port 443 is used for existing deployments. Ports above 1024 are assumed to
+  // be OK to use.
+  if (port == 443 || port >= 1024) {
+    return true;
+  }
+  // Allow any port if relevant field trial is set. This allows disabling the
+  // check.
+  if (webrtc::field_trial::IsEnabled("WebRTC-Turn-AllowSystemPorts")) {
+    return true;
+  }
+  return false;
 }
 
 void TurnPort::OnMessage(rtc::Message* message) {
--- third_party/libwebrtc/p2p/base/turn_port.h
+++ third_party/libwebrtc/p2p/base/turn_port.h
@@ -68,6 +68,12 @@ class TurnPort : public Port {
     if (credentials.username.size() > kMaxTurnUsernameLength) {
       return nullptr;
     }
+    // Do not connect to low-numbered ports. The default STUN port is 3478.
+    if (!AllowedTurnPort(server_address.address.port())) {
+      RTC_LOG(LS_ERROR) << "Attempt to use TURN to connect to port "
+                        << server_address.address.port();
+      return nullptr;
+    }
     // Using `new` to access a non-public constructor.
     return absl::WrapUnique(new TurnPort(
         thread, factory, network, socket, username, password, server_address,
@@ -111,6 +117,12 @@ class TurnPort : public Port {
       rtc::SSLCertificateVerifier* tls_cert_verifier = nullptr) {
     // Do basic parameter validation.
     if (credentials.username.size() > kMaxTurnUsernameLength) {
+      return nullptr;
+    }
+    // Do not connect to low-numbered ports. The default STUN port is 3478.
+    if (!AllowedTurnPort(server_address.address.port())) {
+      RTC_LOG(LS_ERROR) << "Attempt to use TURN to connect to port "
+                        << server_address.address.port();
       return nullptr;
     }
     // Using `new` to access a non-public constructor.
@@ -296,6 +308,7 @@ class TurnPort : public Port {
   typedef std::map<rtc::Socket::Option, int> SocketOptionsMap;
   typedef std::set<rtc::SocketAddress> AttemptedServerSet;
 
+  static bool AllowedTurnPort(int port);
   void OnMessage(rtc::Message* pmsg) override;
 
   bool CreateTurnClientSocket();
