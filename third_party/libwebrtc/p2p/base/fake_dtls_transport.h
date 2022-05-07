03c5b830a8860c3884905e64d8d1113fdadfd1ce
Michael Froman <mfroman@mozilla.com>
2022-05-07 09:15:42 -0500

    Bug 1766646 - Vendor libwebrtc from 92430888fd
    Upstream commit: https://webrtc.googlesource.com/src/+/92430888fd93e4aa7c8a7f8e46fe358ad619add3
        Add thread annotations to FakeIceTransport
    
        Bug: webrtc:12339
        Change-Id: I29f5c910c60155cbb48c686e77b02ad3aa761fb1
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/211665
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Niels Moller <nisse@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33500}

--- third_party/libwebrtc/p2p/base/fake_dtls_transport.h
+++ third_party/libwebrtc/p2p/base/fake_dtls_transport.h
@@ -55,9 +55,15 @@ class FakeDtlsTransport : public DtlsTransportInternal {
 
   // If this constructor is called, a new fake ICE transport will be created,
   // and this FakeDtlsTransport will take the ownership.
-  explicit FakeDtlsTransport(const std::string& name, int component)
+  FakeDtlsTransport(const std::string& name, int component)
       : FakeDtlsTransport(std::make_unique<FakeIceTransport>(name, component)) {
   }
+  FakeDtlsTransport(const std::string& name,
+                    int component,
+                    rtc::Thread* network_thread)
+      : FakeDtlsTransport(std::make_unique<FakeIceTransport>(name,
+                                                             component,
+                                                             network_thread)) {}
 
   ~FakeDtlsTransport() override {
     if (dest_ && dest_->dest_ == this) {
