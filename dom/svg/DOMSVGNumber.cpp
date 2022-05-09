58dc9a8c1ff7d3e0325789605e80c4356420a369
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-05-09 20:41:15 +0000

    Bug 1768189 - Part 32: Apply modernize-concat-nested-namespaces to dom/svg/DOMSVGAnimatedNumberList.cpp ... r=andi
    Depends on D145766
    
    Differential Revision: https://phabricator.services.mozilla.com/D145767

--- dom/svg/DOMSVGNumber.cpp
+++ dom/svg/DOMSVGNumber.cpp
@@ -16,8 +16,7 @@
 
 // See the architecture comment in DOMSVGAnimatedNumberList.h.
 
-namespace mozilla {
-namespace dom {
+namespace mozilla::dom {
 
 // We could use NS_IMPL_CYCLE_COLLECTION(, except that in Unlink() we need to
 // clear our list's weak ref to us to be safe. (The other option would be to
@@ -142,5 +141,4 @@ JSObject* DOMSVGNumber::WrapObject(JSContext* aCx,
   return SVGNumber_Binding::Wrap(aCx, this, aGivenProto);
 }
 
-}  // namespace dom
-}  // namespace mozilla
+}  // namespace mozilla::dom
