ea1fd1850467da86112b538114499c96fbbf6cb5
Bas Schouten <bschouten@mozilla.com>
2022-05-09 15:23:11 +0000

    Bug 1767257: Migrate existing WebRender telemetry probes to use Glean directly. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D145315

--- gfx/wr/webrender/src/telemetry.rs
+++ gfx/wr/webrender/src/telemetry.rs
@@ -5,6 +5,7 @@
 use glean::TimerId;
 #[cfg(feature = "gecko")]
 use fog::metrics::wr;
+use std::time::Duration;
 
 pub struct Telemetry;
 
@@ -15,10 +16,22 @@ impl Telemetry {
     pub fn start_rasterize_glyphs_time() -> TimerId { return 0; }
     // End rasterize glyph time collection
     pub fn stop_and_accumulate_rasterize_glyphs_time(_id: TimerId) { }
+    pub fn start_framebuild_time() -> TimerId { 0 }
+    pub fn stop_and_accumulate_framebuild_time(_id: TimerId) { }
+    pub fn record_scenebuild_time(_duration: Duration) { }
+    pub fn start_sceneswap_time() -> TimerId { 0 }
+    pub fn stop_and_accumulate_sceneswap_time(_id: TimerId) { }
+    pub fn cancel_sceneswap_time(_id: TimerId) { }
 }
 
 #[cfg(feature = "gecko")]
 impl Telemetry {
     pub fn start_rasterize_glyphs_time() -> TimerId { wr::rasterize_glyphs_time.start() }
     pub fn stop_and_accumulate_rasterize_glyphs_time(id: TimerId) { wr::rasterize_glyphs_time.stop_and_accumulate(id); }
+    pub fn start_framebuild_time() -> TimerId { wr::framebuild_time.start() }
+    pub fn stop_and_accumulate_framebuild_time(id: TimerId) { wr::framebuild_time.stop_and_accumulate(id); }
+    pub fn record_scenebuild_time(duration: Duration) { wr::scenebuild_time.accumulate_raw_duration(duration); }
+    pub fn start_sceneswap_time() -> TimerId { wr::sceneswap_time.start() }
+    pub fn stop_and_accumulate_sceneswap_time(id: TimerId) { wr::sceneswap_time.stop_and_accumulate(id); }
+    pub fn cancel_sceneswap_time(id: TimerId) { wr::sceneswap_time.cancel(id); }
 }
