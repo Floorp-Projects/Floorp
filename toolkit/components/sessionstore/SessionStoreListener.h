f5b32198c84b01ff72f0731fb69497e87091bc1e
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-05-09 20:41:21 +0000

    Bug 1768189 - Part 47: Apply modernize-concat-nested-namespaces to layout/style/MediaQueryList.h ... r=andi
    Depends on D145782
    
    Differential Revision: https://phabricator.services.mozilla.com/D145783

--- toolkit/components/sessionstore/SessionStoreListener.h
+++ toolkit/components/sessionstore/SessionStoreListener.h
@@ -16,8 +16,7 @@
 
 class nsITimer;
 
-namespace mozilla {
-namespace dom {
+namespace mozilla::dom {
 
 class ContentSessionStore {
  public:
@@ -110,7 +109,6 @@ class TabListener : public nsIDOMEventListener,
   uint32_t mEpoch;
 };
 
-}  // namespace dom
-}  // namespace mozilla
+}  // namespace mozilla::dom
 
 #endif  // mozilla_dom_SessionStoreListener_h
