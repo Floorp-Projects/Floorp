6e3796d71ea3ec98c67b60d46a53690965d24159
James Graham <james@hoppipolla.co.uk>
2022-05-09 15:33:30 +0000

    Bug 1767074 - Don't bind wptserve to external ip address for android, r=jdescottes
    This used to be used to get access to the server from the host, but
    hasn't been needed since we set up port forwarding using adb.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145135

--- testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/firefox_android.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/firefox_android.py
@@ -1,6 +1,5 @@
 import os
 
-import moznetwork
 from mozrunner import FennecEmulatorRunner, get_app_context
 
 from .base import (get_free_port,
@@ -94,10 +93,7 @@ def run_info_extras(**kwargs):
 
 
 def env_options():
-    # The server host is set to public localhost IP so that resources can be accessed
-    # from Android emulator
-    return {"server_host": moznetwork.get_ip(),
-            "bind_address": False,
+    return {"server_host": "127.0.0.1",
             "supports_debugger": True}
 
 
