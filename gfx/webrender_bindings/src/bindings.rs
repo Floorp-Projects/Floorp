fa2ede0ababa1b1ec63c4ada9d7da07979c5d951
Cosmin Sabou <csabou@mozilla.com>
2022-05-09 17:31:06 +0300

    Backed out changeset 50ba61f5cf8c (bug 1767257) for causing webrender bustages. CLOSED TREE

--- gfx/webrender_bindings/src/bindings.rs
+++ gfx/webrender_bindings/src/bindings.rs
@@ -512,6 +512,7 @@ extern "C" {
     #[allow(dead_code)]
     fn gfx_critical_error(msg: *const c_char);
     fn gfx_critical_note(msg: *const c_char);
+    fn record_telemetry_time(probe: TelemetryProbe, time_ns: u64);
     fn gfx_wr_set_crash_annotation(annotation: CrashAnnotation, value: *const c_char);
     fn gfx_wr_clear_crash_annotation(annotation: CrashAnnotation);
 }
@@ -547,8 +548,11 @@ impl RenderNotifier for CppNotifier {
         }
     }
 
-    fn new_frame_ready(&self, _: DocumentId, _scrolled: bool, composite_needed: bool) {
+    fn new_frame_ready(&self, _: DocumentId, _scrolled: bool, composite_needed: bool, render_time_ns: Option<u64>) {
         unsafe {
+            if let Some(time) = render_time_ns {
+                record_telemetry_time(TelemetryProbe::FrameBuildTime, time);
+            }
             if composite_needed {
                 wr_notifier_new_frame_ready(self.window_id);
             } else {
@@ -984,15 +988,17 @@ impl SceneBuilderHooks for APZCallbacks {
         gecko_profiler_start_marker("SceneBuilding");
     }
 
-    fn pre_scene_swap(&self) {
+    fn pre_scene_swap(&self, scenebuild_time: u64) {
         unsafe {
+            record_telemetry_time(TelemetryProbe::SceneBuildTime, scenebuild_time);
             apz_pre_scene_swap(self.window_id);
         }
     }
 
-    fn post_scene_swap(&self, _document_ids: &Vec<DocumentId>, info: PipelineInfo) {
+    fn post_scene_swap(&self, _document_ids: &Vec<DocumentId>, info: PipelineInfo, sceneswap_time: u64) {
         let mut info = WrPipelineInfo::new(&info);
         unsafe {
+            record_telemetry_time(TelemetryProbe::SceneSwapTime, sceneswap_time);
             apz_post_scene_swap(self.window_id, &info);
         }
 
