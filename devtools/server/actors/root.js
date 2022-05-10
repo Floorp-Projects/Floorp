2fd84ea13893bd698dcc00dcbcd1f2e86a037576
Nicolas Chevobbe <nchevobbe@mozilla.com>
2022-05-10 11:48:45 +0000

    Bug 1688231 - [devtools] Delete noPauseOnThreadActorAttach trait. r=bomsy.
    This was used for pre-86 servers, and we can now assume that all servers
    threads don't pause on attach.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145888

--- devtools/server/actors/root.js
+++ devtools/server/actors/root.js
@@ -142,9 +142,6 @@ exports.RootActor = protocol.ActorClassWithSpec(rootSpec, {
             "dom.worker.console.dispatch_events_to_main_thread"
           )
         : true,
-      // @backward-compat { version 86 } ThreadActor.attach no longer pauses the thread,
-      //                                 so that we no longer have to resume.
-      noPauseOnThreadActorAttach: true,
     };
   },
 
