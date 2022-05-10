d7035b5d1bb3378bbaafb0472b0be508e30242da
Nika Layzell <nika@thelayzells.com>
2022-05-10 22:27:42 +0000

    Bug 1754004 - Part 2: Remove the PFileDescriptorSet interface, r=asuth,necko-reviewers,kershaw
    This interface should no longer be required due to the changes in part 1
    limiting the complexity of IPCStream instances and limiting the number of file
    descriptors which a single stream can attach to a message.
    
    Removing this interface is necessary to serialize nsIInputStream instances over
    arbitrary toplevel protocols and non-protocol IPC in the future.
    
    Differential Revision: https://phabricator.services.mozilla.com/D141039

--- netwerk/base/nsFileStreams.h
+++ netwerk/base/nsFileStreams.h
@@ -155,8 +155,7 @@ class nsFileInputStream : public nsFileStreamBase,
  protected:
   virtual ~nsFileInputStream() = default;
 
-  void SerializeInternal(mozilla::ipc::InputStreamParams& aParams,
-                         FileDescriptorArray& aFileDescriptors);
+  void SerializeInternal(mozilla::ipc::InputStreamParams& aParams);
 
   nsresult SeekInternal(int32_t aWhence, int64_t aOffset,
                         bool aClearBuf = true);
