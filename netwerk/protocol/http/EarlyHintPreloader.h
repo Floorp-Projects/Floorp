5110c106972054a7033a7567084e6b76454a773d
Manuel Bucher <mbucher@mozilla.com>
2022-05-11 15:41:39 +0000

    Bug 1753730 - Add EarlyHintPreloader to load 103 Early Hint responses into the cache r=necko-reviewers,ckerschb,dragana,kershaw
    Currently only same origin requests are preloaded and preloads in the
    secure context. This may change in the future to match W3C decisions and
    Chromes behavior.
    
    Also only images get preloaded. This will change in the future to cover
    asset types.
    
    Currently the anchor isn't parsed correctly yet[1], so this will be
    fixed in a future patch.
    
    On non-2xx responses of the main document all ongoing preloads get
    canceled. Already completed preloads don't get affected and are in the
    cache.
    
    It is currently untested whether unused preloads don't have side effects.
    
    Another future patch should cover adding the preload to the devtools.
    
    [1]: https://datatracker.ietf.org/doc/html/rfc8288#section-3.2
    
    Differential Revision: https://phabricator.services.mozilla.com/D137885

---
+++ netwerk/protocol/http/EarlyHintPreloader.h
@@ -0,0 +1,96 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_net_EarlyHintPreloader_h
+#define mozilla_net_EarlyHintPreloader_h
+
+#include "mozilla/Maybe.h"
+#include "mozilla/PreloadHashKey.h"
+#include "nsIChannelEventSink.h"
+#include "nsIInterfaceRequestor.h"
+#include "nsIRedirectResultListener.h"
+#include "nsIStreamListener.h"
+#include "nsNetUtil.h"
+#include "nsRefPtrHashtable.h"
+
+class nsAttrValue;
+class nsICookieJarSettings;
+class nsIPrincipal;
+class nsIReferrerInfo;
+
+namespace mozilla::net {
+
+class EarlyHintPreloader;
+struct LinkHeader;
+
+// class keeping track of all ongoing early hints
+class OngoingEarlyHints final {
+ public:
+  NS_INLINE_DECL_REFCOUNTING(OngoingEarlyHints)
+  MOZ_DECLARE_REFCOUNTED_TYPENAME(OngoingEarlyHints)
+
+  OngoingEarlyHints() = default;
+
+  // returns whether a preload with that key already existed
+  bool Contains(const PreloadHashKey& aKey);
+  bool Add(const PreloadHashKey& aKey, RefPtr<EarlyHintPreloader> aPreloader);
+
+  void CancelAllOngoingPreloads();
+
+ private:
+  ~OngoingEarlyHints() = default;
+  nsRefPtrHashtable<PreloadHashKey, EarlyHintPreloader> mOngoingPreloads;
+};
+
+class EarlyHintPreloader final : public nsIStreamListener,
+                                 public nsIChannelEventSink,
+                                 public nsIRedirectResultListener,
+                                 public nsIInterfaceRequestor {
+ public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIREQUESTOBSERVER
+  NS_DECL_NSISTREAMLISTENER
+  NS_DECL_NSICHANNELEVENTSINK
+  NS_DECL_NSIREDIRECTRESULTLISTENER
+  NS_DECL_NSIINTERFACEREQUESTOR
+
+ public:
+  // Create and insert a preload into OngoingEarlyHints if the same preload
+  // wasn't already issued and the LinkHeader can be parsed correctly.
+  static void MaybeCreateAndInsertPreload(
+      OngoingEarlyHints* aOngoingEarlyHints, const LinkHeader& aHeader,
+      nsIURI* aBaseURI, nsIPrincipal* aTriggeringPrincipal,
+      nsICookieJarSettings* aCookieJarSettings);
+
+  // Should be called by the preloader service when the preload is not needed
+  // after all, because the final response returns a non-2xx status code.
+  nsresult CancelChannel(nsresult aStatus);
+
+ private:
+  explicit EarlyHintPreloader(nsIURI* aURI);
+  ~EarlyHintPreloader() = default;
+
+  static Maybe<PreloadHashKey> GenerateHashKey(ASDestination aAs, nsIURI* aURI,
+                                               nsIPrincipal* aPrincipal);
+
+  // call to start the preload
+  nsresult OpenChannel(nsIPrincipal* aTriggeringPrincipal,
+                       nsSecurityFlags aSecurityFlags,
+                       nsContentPolicyType aContentPolicyType,
+                       nsIReferrerInfo* aReferrerInfo,
+                       nsICookieJarSettings* aCookieJarSettings);
+
+  // keep opening uri to not preload cross origins on redirects for now
+  nsCOMPtr<nsIURI> mURI;
+  nsCOMPtr<nsIChannel> mChannel;
+  nsCOMPtr<nsIChannel> mRedirectChannel;
+};
+
+inline nsISupports* ToSupports(EarlyHintPreloader* aObj) {
+  return static_cast<nsIInterfaceRequestor*>(aObj);
+}
+
+}  // namespace mozilla::net
+
+#endif  // mozilla_net_EarlyHintPreloader_h
