0f1cdd6ceaff1269152f45dfb4b71a4bb42d0e8c
Mark Banner <standard8@mozilla.com>
2022-05-11 14:08:14 +0000

    Bug 1768031 - Add .mjs to ESLint rules. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D145700

--- tools/lint/eslint/eslint-plugin-mozilla/lib/environments/jsm.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/environments/jsm.js
@@ -12,10 +12,12 @@ module.exports = {
   globals: {
     // These globals are hard-coded and available in .jsm scopes.
     // https://searchfox.org/mozilla-central/rev/ed212c79cfe86357e9a5740082b9364e7f6e526f/js/xpconnect/loader/mozJSComponentLoader.cpp#134-140
+    // Although `debug` is allowed for jsm files, this is non-standard and something
+    // we don't want to allow in mjs files. Hence it is not included here.
     atob: false,
     btoa: false,
-    debug: false,
     dump: false,
+    Intl: false,
     // The WebAssembly global is available in most (if not all) contexts where
     // JS can run. It's definitely available in JSMs. So even if this is not
     // the perfect place to add it, it's not wrong, and we can move it later.
