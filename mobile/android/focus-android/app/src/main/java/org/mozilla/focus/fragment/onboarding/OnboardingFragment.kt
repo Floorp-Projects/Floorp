4de773841ac8e3f0a1cf945ef1d9ce21538c019f
Ionut Cristian Bedregeanu <ionut.bedregeanu@softvision.ro>
2022-05-11 10:49:46 +0300

    [focus] For https://github.com/mozilla-mobile/focus-android/issues/6998 - Align onboarding title to center

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/onboarding/OnboardingFragment.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/onboarding/OnboardingFragment.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
@@ -109,7 +110,8 @@ class OnboardingFragment : Fragment() {
                             top.linkTo(logo.bottom)
                             start.linkTo(parent.start)
                             end.linkTo(parent.end)
-                        }
+                        },
+                    textAlign = TextAlign.Center
                 )
                 Text(
                     text = stringResource(id = R.string.onboarding_description),
