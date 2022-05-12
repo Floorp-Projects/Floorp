b94f0dd0916b151ef93d025dfed891f0a7f9e25b
Matthew Gaudet <mgaudet@mozilla.com>
2022-05-12 17:56:25 +0000

    Bug 1769093 - Test262 Update: May 2022 r=yulia
    Differential Revision: https://phabricator.services.mozilla.com/D146219

--- js/src/tests/test262/built-ins/Temporal/PlainTime/from/argument-number.js
+++ js/src/tests/test262/built-ins/Temporal/PlainTime/from/argument-number.js
@@ -4,12 +4,29 @@
 
 /*---
 esid: sec-temporal.plaintime.from
-description: Number argument is converted to string
-includes: [compareArray.js, temporalHelpers.js]
+description: A number is converted to a string, then to Temporal.PlainTime
+includes: [temporalHelpers.js]
 features: [Temporal]
 ---*/
 
-const result = Temporal.PlainTime.from(1523);
-TemporalHelpers.assertPlainTime(result, 15, 23, 0, 0, 0, 0);
+const arg = 123456.987654321;
+
+const result = Temporal.PlainTime.from(arg);
+TemporalHelpers.assertPlainTime(result, 12, 34, 56, 987, 654, 321, "123456.987654321 is a valid ISO string for PlainTime");
+
+const numbers = [
+  1,
+  -123456.987654321,
+  1234567,
+  123456.9876543219,
+];
+
+for (const arg of numbers) {
+  assert.throws(
+    RangeError,
+    () => Temporal.PlainTime.from(arg),
+    `Number ${arg} does not convert to a valid ISO string for PlainTime`
+  );
+}
 
 reportCompare(0, 0);
