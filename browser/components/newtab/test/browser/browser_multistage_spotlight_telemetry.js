f44ba084149347843584b260aa7755fb5e27771d
Meg Viar <lmegviar@gmail.com>
2022-05-12 13:02:59 +0000

    Bug 1767511 - Many messaging-system/onboarding pings missing required event identifier r=Mardak
    Differential Revision: https://phabricator.services.mozilla.com/D145615

--- browser/components/newtab/test/browser/browser_multistage_spotlight_telemetry.js
+++ browser/components/newtab/test/browser/browser_multistage_spotlight_telemetry.js
@@ -10,19 +10,31 @@ const { BrowserWindowTracker } = ChromeUtils.import(
   "resource:///modules/BrowserWindowTracker.jsm"
 );
 
+const { AboutWelcomeTelemetry } = ChromeUtils.import(
+  "resource://activity-stream/aboutwelcome/lib/AboutWelcomeTelemetry.jsm"
+);
+
 async function waitForClick(selector, win) {
   await TestUtils.waitForCondition(() => win.document.querySelector(selector));
   win.document.querySelector(selector).click();
 }
 
-async function showDialog(dialogOptions) {
+function waitForDialog(callback = win => win.close()) {
+  return BrowserTestUtils.promiseAlertDialog(
+    null,
+    "chrome://browser/content/spotlight.html",
+    { callback, isSubDialog: true }
+  );
+}
+
+function showAndWaitForDialog(dialogOptions, callback) {
+  const promise = waitForDialog(callback);
   Spotlight.showSpotlightDialog(
     dialogOptions.browser,
     dialogOptions.message,
     dialogOptions.dispatchStub
   );
-  const [win] = await TestUtils.topicObserved("subdialog-loaded");
-  return win;
+  return promise;
 }
 
 add_task(async function send_spotlight_as_page_in_telemetry() {
@@ -31,20 +43,69 @@ add_task(async function send_spotlight_as_page_in_telemetry() {
   );
   let dispatchStub = sinon.stub();
   let browser = BrowserWindowTracker.getTopWindow().gBrowser.selectedBrowser;
-  let win = await showDialog({ message, browser, dispatchStub });
   let sandbox = sinon.createSandbox();
 
-  sandbox.stub(win, "AWSendEventTelemetry");
-  await waitForClick("button.secondary", win);
+  await showAndWaitForDialog({ message, browser, dispatchStub }, async win => {
+    let stub = sandbox.stub(win, "AWSendEventTelemetry");
+    await waitForClick("button.secondary", win);
+    Assert.equal(
+      stub.lastCall.args[0].event_context.page,
+      "spotlight",
+      "The value of event context page should be set to 'spotlight' in event telemetry"
+    );
+    win.close();
+  });
+
+  sandbox.restore();
+});
+
+add_task(async function send_dismiss_event_telemetry() {
+  const messageId = "PB_FOCUS_PROMO"; // a multistage spotlight promo with dismiss button
+  let message = (await PanelTestProvider.getMessages()).find(
+    m => m.id === messageId
+  );
+  let browser = BrowserWindowTracker.getTopWindow().gBrowser.selectedBrowser;
+  let sandbox = sinon.createSandbox();
+  let stub = sandbox.stub(AboutWelcomeTelemetry.prototype, "sendTelemetry");
+  // send without a dispatch function so that default is used
+  await showAndWaitForDialog({ message, browser }, async win => {
+    await waitForClick("button.dismiss-button", win);
+    win.close();
+  });
 
   Assert.equal(
-    win.AWSendEventTelemetry.lastCall.args[0].event_context.page,
-    "spotlight",
-    "The value of event context page should be set to 'spotlight' in event telemetry"
+    stub.lastCall.args[0].message_id,
+    messageId,
+    "A dismiss event is called with the correct message id"
   );
 
-  win.close();
-  registerCleanupFunction(() => {
-    sandbox.restore();
-  });
+  Assert.equal(
+    stub.lastCall.args[0].event,
+    "DISMISS",
+    "A dismiss event is called with a top level event field with value 'DISMISS'"
+  );
+
+  sandbox.restore();
 });
+
+add_task(
+  async function do_not_send_impression_telemetry_from_default_dispatch() {
+    // Don't send impression telemetry from the Spotlight default dispatch function
+    let message = (await PanelTestProvider.getMessages()).find(
+      m => m.id === "MULTISTAGE_SPOTLIGHT_MESSAGE"
+    );
+    let browser = BrowserWindowTracker.getTopWindow().gBrowser.selectedBrowser;
+    let sandbox = sinon.createSandbox();
+    let stub = sandbox.stub(AboutWelcomeTelemetry.prototype, "sendTelemetry");
+    // send without a dispatch function so that default is used
+    await showAndWaitForDialog({ message, browser });
+
+    Assert.equal(
+      stub.calledOn(),
+      false,
+      "No extra impression event was sent for multistage Spotlight"
+    );
+
+    sandbox.restore();
+  }
+);
