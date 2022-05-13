a0fea91aeac902443f3b15ad477e370b07eb6017
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2022-05-13 12:19:46 +0000

    Bug 1756003 part 2 - Compare concurrent and on-demand delazification. r=arai
    Adds a delazification mode, which spawn a concurrent depth-first delazification
    of all function to fill the stencil cache, to later compare cached results with
    stencils produced by on-demand delazification.
    
    The delazification mode also works when being used from a main-thread top-level
    compilation, in which case the delazification tasks are created and potentially
    blocking the main thread, while the delazification is happening.
    
    This way of doing help comparing on-demand delazification, based on instantiated
    stencils with concurrent delazification, based on stencils. Which is a good
    mode for fuzzing purposes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145992

--- js/src/vm/HelperThreads.h
+++ js/src/vm/HelperThreads.h
@@ -14,6 +14,7 @@
 #include "mozilla/Variant.h"
 #include "NamespaceImports.h"
 
+#include "js/experimental/JSStencil.h"
 #include "js/OffThreadScriptCompilation.h"
 #include "js/Transcoding.h"
 #include "js/UniquePtr.h"
@@ -205,6 +206,11 @@ void CancelOffThreadParses(JSRuntime* runtime);
 void CancelOffThreadDelazify(JSRuntime* runtime);
 
 /*
+ * Wait for all delazification to complete.
+ */
+void WaitForAllDelazifyTasks(JSRuntime* rt);
+
+/*
  * Start a parse/emit cycle for a stream of source. The characters must stay
  * alive until the compilation finishes.
  */
@@ -236,6 +242,12 @@ JS::OffThreadToken* StartOffThreadDecodeMultiStencils(
     JSContext* cx, const JS::DecodeOptions& options,
     JS::TranscodeSources& sources, JS::OffThreadCompileCallback callback,
     void* callbackData);
+
+// Start off-thread delazification task, to race the delazification of inner
+// functions.
+[[nodiscard]] bool StartOffThreadDelazification(
+    JSContext* cx, const JS::ReadOnlyCompileOptions& options,
+    const frontend::CompilationStencil& stencil);
 
 // Drain the task queues and wait for all helper threads to finish running.
 //
