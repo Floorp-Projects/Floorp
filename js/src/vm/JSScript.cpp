4335780b59ac04f31125bc34c6f6462e760fad08
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2022-05-13 16:41:29 +0000

    Bug 1756003 part 1 - ScriptSource carry the delazification mode. r=arai
    Previously, we only had a single mode of delazification, then we added the
    concurrent delazification, which could be mixed with on-demand delazification,
    by always checking for cached entries.
    
    Now, with the addition of a third mode which attempt to compare the outputs of
    on-demand delazification with concurrent delazification, we need to distinguish
    and carry this difference to the function responsible for running the
    delazification. However, this information is only available when we first run
    the delazification.
    
    This patch adds the JS::DelazificationOption (uint8_t) to the ScriptSource
    class, such that we can communicate the selected mode to the functions in charge
    of the delazification.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145991

--- js/src/vm/JSScript.cpp
+++ js/src/vm/JSScript.cpp
@@ -1859,6 +1859,7 @@ bool ScriptSource::initFromOptions(JSContext* cx,
   MOZ_ASSERT(!introducerFilename_);
 
   mutedErrors_ = options.mutedErrors();
+  delazificationMode_ = options.eagerDelazificationStrategy();
 
   startLine_ = options.lineno;
   introductionType_ = options.introductionType;
