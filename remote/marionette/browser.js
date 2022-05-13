20553049c76f97b7a0040f9195750664b18898f6
Henrik Skupin <mail@hskupin.info>
2022-05-13 09:45:18 +0000

    Bug 1533058 - [marionette] "WebDriver:NewWindow" for type "tab" has to open "about:blank". r=webdriver-reviewers,jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D139058

--- remote/marionette/browser.js
+++ remote/marionette/browser.js
@@ -249,17 +249,8 @@ browser.Context = class {
     switch (AppInfo.name) {
       case "Firefox":
         const opened = new EventPromise(this.window, "TabOpen");
-        this.window.BrowserOpenTab();
+        tab = TabManager.addTab({ focus, window: this.window });
         await opened;
-
-        tab = this.tabBrowser.selectedTab;
-
-        // The new tab is always selected by default. If focus is not wanted,
-        // the previously tab needs to be selected again.
-        if (!focus) {
-          this.tabBrowser.selectedTab = this.tab;
-        }
-
         break;
 
       default:
