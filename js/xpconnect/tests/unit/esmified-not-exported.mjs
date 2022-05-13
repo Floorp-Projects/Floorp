3ed52314c680f2c1820cead4360ab8bf9893aac9
Tooru Fujisawa <arai_a@mac.com>
2022-05-13 15:23:15 +0000

    Bug 1766761 - Part 4: Add a shim for Cu.import/ChromeUtils.import with ESM-ified module. r=jonco,Standard8
    This shim redirects Cu.import("foo.jsm") to ChromeUtils.importModule("foo.mjs")
    if foo.jsm is already renamed to foo.mjs.
    
    This de-optimizes ESM component's global `var`s, in order to expose all
    variables in `ModuleEnvironmentObject`, and return those variables from
    `Cu.improt`'s return value.
    
    In contrast to normal Cu.import, the return value of shim version exposes
    all non-exported lexical variables.
    Bug 1768060 patch aligns the normal Cu.import to expose them as well,
    so that `this.foo = foo;` hack can be removed in bug 1610653.
    
    Once not-in-tree `Cu.import` consumers are gone and `Cu.import` is removed,
    this shim and the de-optimization for the `var` can be removed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145784

---
+++ js/xpconnect/tests/unit/esmified-not-exported.mjs
@@ -0,0 +1,13 @@
+export var exportedVar = "exported var";
+export function exportedFunction() {
+  return "exported function";
+}
+export let exportedLet = "exported let";
+export const exportedConst = "exported const";
+
+var notExportedVar = "not exported var";
+function notExportedFunction() {
+  return "not exported function";
+}
+let notExportedLet = "not exported let";
+const notExportedConst = "not exported const";
