c7b7dd2b4639e0f29868067e0e1e6b42e49d2c4e
Bogdan Szekely <bszekely@mozilla.com>
2022-05-13 15:48:34 +0300

    Backed out 4 changesets (bug 1756003, bug 1764280) for causing build bustages on ScriptLoader.cpp. CLOSED TREE
    Backed out changeset 51b6c13941b6 (bug 1764280)
    Backed out changeset 399a9a92ff78 (bug 1756003)
    Backed out changeset 2dc65ba6edce (bug 1756003)
    Backed out changeset 5b982beda860 (bug 1756003)

--- js/src/jit-test/tests/self-test/delazification-mode-01.js
+++
@@ -1,9 +0,0 @@
-//|jit-test| --delazification-mode=concurrent-df; skip-if: helperThreadCount() === 0
-
-function foo() {
-    return "foo";
-}
-
-waitForStencilCache(foo);
-// false would be expected if threads are disabled.
-assertEq(isInStencilCache(foo), true);
