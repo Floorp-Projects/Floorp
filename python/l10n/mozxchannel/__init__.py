28cef61371130fbe90b7fb08d534063a102daa91
Francesco Lodolo (:flod) <flod@lodolo.net>
2022-05-16 16:55:45 +0000

    Bug 1768502 - Add esr102 to cross-channel configuration, drop esr91, r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D146412

--- python/l10n/mozxchannel/__init__.py
+++ python/l10n/mozxchannel/__init__.py
@@ -39,7 +39,7 @@ def get_default_config(topsrcdir, strings_path):
                     "central": "mozilla-central",
                     "beta": "releases/mozilla-beta",
                     "release": "releases/mozilla-release",
-                    "esr91": "releases/mozilla-esr91",
+                    "esr102": "releases/mozilla-esr102",
                 },
                 "config_files": [
                     "browser/locales/l10n.toml",
@@ -57,7 +57,7 @@ def get_default_config(topsrcdir, strings_path):
                     # ESR goes last (e.g. esr78 goes after esr91).
                     "comm": "comm-central",
                     "comm-beta": "releases/comm-beta",
-                    "comm-esr91": "releases/comm-esr91",
+                    "comm-esr102": "releases/comm-esr102",
                 },
                 "config_files": [
                     "comm/calendar/locales/l10n.toml",
