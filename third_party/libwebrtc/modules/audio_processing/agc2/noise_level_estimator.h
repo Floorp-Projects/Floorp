d4e3b012c39c996c0cd8fdd4a66022914c8f74ea
Michael Froman <mfroman@mozilla.com>
2022-05-16 12:40:33 -0500

    Bug 1766646 - Vendor libwebrtc from 70b775d77f
    Upstream commit: https://webrtc.googlesource.com/src/+/70b775d77f9de33cc4f48437e62bc9192a21001b
        AGC2 noise estimator code style improvements
    
        Code style improvements done in preparation for a bug fix (TODO added)
        which requires changes in the unit tests.
    
        Note that one expected value in the unit tests has been adjusted since
        the white noise generator is now instanced in each separate test and
        therefore, even if the seed remained the same, the generated sequences
        differ.
    
        Bug: webrtc:7494
        Change-Id: I497513b84f50b5c66cf6241a09946ce853eb1cd2
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/214122
        Commit-Queue: Alessio Bazzica <alessiob@webrtc.org>
        Reviewed-by: Ivo Creusen <ivoc@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33636}

--- third_party/libwebrtc/modules/audio_processing/agc2/noise_level_estimator.h
+++ third_party/libwebrtc/modules/audio_processing/agc2/noise_level_estimator.h
@@ -13,7 +13,6 @@
 
 #include "modules/audio_processing/agc2/signal_classifier.h"
 #include "modules/audio_processing/include/audio_frame_view.h"
-#include "rtc_base/constructor_magic.h"
 
 namespace webrtc {
 class ApmDataDumper;
@@ -21,6 +20,8 @@ class ApmDataDumper;
 class NoiseLevelEstimator {
  public:
   NoiseLevelEstimator(ApmDataDumper* data_dumper);
+  NoiseLevelEstimator(const NoiseLevelEstimator&) = delete;
+  NoiseLevelEstimator& operator=(const NoiseLevelEstimator&) = delete;
   ~NoiseLevelEstimator();
   // Returns the estimated noise level in dBFS.
   float Analyze(const AudioFrameView<const float>& frame);
@@ -28,14 +29,13 @@ class NoiseLevelEstimator {
  private:
   void Initialize(int sample_rate_hz);
 
+  ApmDataDumper* const data_dumper_;
   int sample_rate_hz_;
   float min_noise_energy_;
   bool first_update_;
   float noise_energy_;
   int noise_energy_hold_counter_;
   SignalClassifier signal_classifier_;
-
-  RTC_DISALLOW_COPY_AND_ASSIGN(NoiseLevelEstimator);
 };
 
 }  // namespace webrtc
