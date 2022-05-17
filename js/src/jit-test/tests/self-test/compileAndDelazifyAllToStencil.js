7b271bf56911b3cdc1e43c0dce1d303b4bfc370f
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2022-05-17 16:58:38 +0000

    Bug 1768630 - Replace CompileAndDelazifyToStencil by a CompileOption. r=arai
    Instead of having a function dedicated to parsing eagerly every function, while
    filling the stencil cache, we delegate to the code which would be used in
    production to do so.
    
    This is achieved by using instrumentation which creates DelazifyTasks for
    scripts which have a delazification mode which implies populating the stencil
    cache.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145995

--- js/src/jit-test/tests/self-test/compileAndDelazifyAllToStencil.js
+++
@@ -1,9 +0,0 @@
-//|jit-test| error:Error
-
-const options = {
-    fileName: "compileToStencil-DATA.js",
-    lineNumber: 1,
-    forceFullParse: true,
-};
-
-compileAndDelazifyAllToStencil(`(x => x)(1)`, options);
