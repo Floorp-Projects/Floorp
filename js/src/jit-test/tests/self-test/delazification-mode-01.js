5c2c48273bd786b780d30d9dcc3bb14295784187
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2022-05-17 10:53:04 +0000

    Bug 1756003 part 3 - Add JS shell command line option to set default delazification. r=arai
    This instrumentation is used to build test cases and help with fuzzing, by
    selecting a custom delazification type as the default mode for the top-level.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145993

---
+++ js/src/jit-test/tests/self-test/delazification-mode-01.js
@@ -0,0 +1,13 @@
+//|jit-test| --delazification-mode=concurrent-df; skip-if: helperThreadCount() === 0 || ('gczeal' in this && (gczeal(0), false))
+//
+// Note, we have to execute gczeal in the skip-if condition, before the current
+// script gets parsed and scheduled for delazification, as changing gc settings
+// while the helper thread is making allocation could cause intermittents.
+
+function foo() {
+    return "foo";
+}
+
+waitForStencilCache(foo);
+// false would be expected if threads are disabled.
+assertEq(isInStencilCache(foo), true);
