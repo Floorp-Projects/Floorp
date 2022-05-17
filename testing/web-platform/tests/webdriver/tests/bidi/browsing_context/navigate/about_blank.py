a5c0a9680fb6607ebb60a71de916ff8220c64139
Alexandra Borovova <aborovova@mozilla.com>
2022-05-17 15:40:25 +0000

    Bug 1763127 - Add a helper function to simplify wdspec tests for browsingContext.navigate. r=webdriver-reviewers,whimboo
    Depends on D146108
    
    Differential Revision: https://phabricator.services.mozilla.com/D146290

--- testing/web-platform/tests/webdriver/tests/bidi/browsing_context/navigate/about_blank.py
+++ testing/web-platform/tests/webdriver/tests/bidi/browsing_context/navigate/about_blank.py
@@ -1,73 +1,33 @@
 import pytest
 
+from . import navigate_and_assert
+
 pytestmark = pytest.mark.asyncio
 
 PAGE_ABOUT_BLANK = "about:blank"
 PAGE_EMPTY = "/webdriver/tests/bidi/browsing_context/navigate/support/empty.html"
 
+
 async def test_navigate_from_single_page(bidi_session, new_tab, url):
-    url_before = url(PAGE_EMPTY)
+    await navigate_and_assert(bidi_session, new_tab, url(PAGE_EMPTY))
+    await navigate_and_assert(bidi_session, new_tab, PAGE_ABOUT_BLANK)
 
-    result = await bidi_session.browsing_context.navigate(
-        context=new_tab["context"], url=url_before, wait="complete"
-    )
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=new_tab["context"], max_depth=0
-    )
-    assert contexts[0]["url"] == url_before
-
-    result = await bidi_session.browsing_context.navigate(
-        context=new_tab["context"], url=PAGE_ABOUT_BLANK, wait="complete"
-    )
-    assert result["url"] == PAGE_ABOUT_BLANK
-
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=new_tab["context"], max_depth=0
-    )
-    assert contexts[0]["url"] == PAGE_ABOUT_BLANK
 
 async def test_navigate_from_frameset(bidi_session, inline, new_tab, url):
     frame_url = url(PAGE_EMPTY)
     url_before = inline(f"<frameset><frame src='{frame_url}'/></frameset")
-    result = await bidi_session.browsing_context.navigate(
-        context=new_tab["context"], url=url_before, wait="complete"
-    )
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=new_tab["context"], max_depth=0
-    )
-    assert contexts[0]["url"] == url_before
+    await navigate_and_assert(bidi_session, new_tab, url_before)
 
-    result = await bidi_session.browsing_context.navigate(
-        context=new_tab["context"], url=PAGE_ABOUT_BLANK, wait="complete"
-    )
-    assert result["url"] == PAGE_ABOUT_BLANK
-
-    contexts = await bidi_session.browsing_context.get_tree(
-        root=new_tab["context"], max_depth=0
-    )
-    assert contexts[0]["url"] == PAGE_ABOUT_BLANK
+    await navigate_and_assert(bidi_session, new_tab, PAGE_ABOUT_BLANK)
 
 
 async def test_navigate_in_iframe(bidi_session, inline, new_tab):
     frame_start_url = inline("frame")
-    url_before = inline(
-        f"<iframe src='{frame_start_url}'></iframe>"
-    )
-    result = await bidi_session.browsing_context.navigate(
-        context=new_tab["context"], url=url_before, wait="complete"
-    )
-    assert result["url"] == url_before
-
-    contexts = await bidi_session.browsing_context.get_tree(root=new_tab["context"])
-    assert contexts[0]["url"] == url_before
+    url_before = inline(f"<iframe src='{frame_start_url}'></iframe>")
+    contexts = await navigate_and_assert(bidi_session, new_tab, url_before)
 
     assert len(contexts[0]["children"]) == 1
     frame = contexts[0]["children"][0]
-    frame_result = await bidi_session.browsing_context.navigate(
-        context=frame["context"], url=PAGE_ABOUT_BLANK, wait="complete"
-    )
-    assert frame_result["url"] == PAGE_ABOUT_BLANK
+    assert frame["url"] == frame_start_url
 
-    contexts = await bidi_session.browsing_context.get_tree(root=frame["context"])
-    assert len(contexts) == 1
-    assert contexts[0]["url"] == PAGE_ABOUT_BLANK
+    await navigate_and_assert(bidi_session, frame, PAGE_ABOUT_BLANK)
