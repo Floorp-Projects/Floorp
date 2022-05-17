3692ee34f11346d7a60e31bf709829c25e9dde6b
Agi Sferro <agi@sferro.dev>
2022-05-17 01:34:42 +0000

    Bug 1754607 - Adjust GeckoViewAutoFillChild for ActorDispatcher r=owlish
    The test failure in the bug is failing because the eventDispatcher in the actor
    was previously only available in the top-level-frame.
    
    In addition to adjusting for ActorDispatcher, this patch also makes every actor
    handle its own frame (rather than handling all the in-process children) which
    simplifies the code a bit.
    
    We also modify the test to use a third-party iframe so that the OOP iframe case
    is covered.
    
    Differential Revision: https://phabricator.services.mozilla.com/D141647

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/AutofillDelegateTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/AutofillDelegateTest.kt
@@ -8,7 +8,6 @@ import android.graphics.Matrix
 import android.os.Bundle
 import android.os.LocaleList
 import androidx.test.filters.MediumTest
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import android.util.Pair
 import android.util.SparseArray
 import android.view.View
@@ -18,22 +17,40 @@ import android.view.autofill.AutofillValue
 import org.hamcrest.Matchers.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 import org.mozilla.geckoview.Autofill
 import org.mozilla.geckoview.GeckoSession
-import org.mozilla.geckoview.test.rule.GeckoSessionTestRule
-import org.mozilla.geckoview.test.rule.GeckoSessionTestRule.AssertCalled
-import org.mozilla.geckoview.test.rule.GeckoSessionTestRule.WithDisplay
+import org.mozilla.geckoview.test.rule.GeckoSessionTestRule.*
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(Parameterized::class)
 @MediumTest
 class AutofillDelegateTest : BaseSessionTest() {
+
+    companion object {
+        @get:Parameterized.Parameters(name = "{0}")
+        @JvmStatic
+        val parameters: List<Array<out Any>> = listOf(
+            arrayOf("#inProcess"), arrayOf("#oop"))
+    }
+
+    @field:Parameterized.Parameter(0) @JvmField var iframe: String = ""
+
+    // Whether the iframe is loaded in-process (i.e. with the same origin as the
+    // outer html page) or out-of-process.
+    private val pageUrl by lazy {
+        when (iframe) {
+            "#inProcess" -> "http://example.org/tests/junit/forms_xorigin.html"
+            "#oop" -> createTestUrl(FORMS_XORIGIN_HTML_PATH)
+            else -> throw IllegalStateException()
+        }
+    }
 
     @Test fun autofillCommit() {
         sessionRule.setPrefsUntilTestEnd(mapOf(
                 "signon.rememberSignons" to true,
                 "signon.userInputRequiredToCapture.enabled" to false))
 
-        mainSession.loadTestPath(FORMS_HTML_PATH)
+        mainSession.loadUri(pageUrl)
         // Wait for the auto-fill nodes to populate.
         sessionRule.waitUntilCalled(object : Autofill.Delegate {
             // For the root document and the iframe document, each has a form group and
@@ -145,7 +162,7 @@ class AutofillDelegateTest : BaseSessionTest() {
     @Test fun autofill() {
         // Test parts of the Oreo auto-fill API; there is another autofill test in
         // SessionAccessibility for a11y auto-fill support.
-        mainSession.loadTestPath(FORMS_HTML_PATH)
+        mainSession.loadUri(pageUrl)
         // Wait for the auto-fill nodes to populate.
         sessionRule.waitUntilCalled(object : Autofill.Delegate {
             // For the root document and the iframe document, each has a form group and
@@ -163,34 +180,21 @@ class AutofillDelegateTest : BaseSessionTest() {
                 "#number1" to "24", "#tel1" to "42")
 
         // Set up promises to monitor the values changing.
-        val promises = autofills.flatMap { entry ->
+        val promises = autofills.map { entry ->
             // Repeat each test with both the top document and the iframe document.
-            arrayOf("document", "document.querySelector('#iframe').contentDocument").map { doc ->
-                mainSession.evaluatePromiseJS("""new Promise(resolve =>
-                    $doc.querySelector('${entry.key}').addEventListener(
-                      'input', event => {
-                        let eventInterface =
-                          event instanceof $doc.defaultView.InputEvent ? "InputEvent" :
-                          event instanceof $doc.defaultView.UIEvent ? "UIEvent" :
-                          event instanceof $doc.defaultView.Event ? "Event" : "Unknown";
-                        resolve([
-                          '${entry.key}',
-                          event.target.value,
-                          '${entry.value}',
-                          eventInterface
-                        ]);
-                }, { once: true }))""")
-            }
+            mainSession.evaluatePromiseJS("""
+                window.getDataForAllFrames('${entry.key}', '${entry.value}')
+                """)
         }
 
         val autofillValues = SparseArray<CharSequence>()
 
         // Perform auto-fill and return number of auto-fills performed.
-        fun checkAutofillChild(child: Autofill.Node) {
+        fun checkAutofillChild(child: Autofill.Node, domain: String) {
             // Seal the node info instance so we can perform actions on it.
-            if (child.children.count() > 0) {
+            if (child.children.isNotEmpty()) {
                 for (c in child.children) {
-                    checkAutofillChild(c!!)
+                    checkAutofillChild(c!!, child.domain)
                 }
             }
 
@@ -200,8 +204,12 @@ class AutofillDelegateTest : BaseSessionTest() {
 
             assertThat("Should have HTML tag",
                        child.tag, not(isEmptyOrNullString()))
-            assertThat("Web domain should match",
-                       child.domain, equalTo(GeckoSessionTestRule.TEST_ENDPOINT))
+            if (domain != "") {
+                assertThat(
+                    "Web domain should match its parent.",
+                    child.domain, equalTo(domain)
+                )
+            }
 
             if (child.inputType == Autofill.InputType.TEXT) {
                 assertThat("Input should be enabled", child.enabled, equalTo(true))
@@ -225,21 +233,29 @@ class AutofillDelegateTest : BaseSessionTest() {
         }
 
         val nodes = mainSession.autofillSession.root
-        checkAutofillChild(nodes)
+        checkAutofillChild(nodes, "")
 
         mainSession.autofill(autofillValues)
 
         // Wait on the promises and check for correct values.
-        for ((key, actual, expected, eventInterface) in promises.map { it.value.asJSList<String>() }) {
-            assertThat("Auto-filled value must match ($key)", actual, equalTo(expected))
-            assertThat("input event should be dispatched with InputEvent interface", eventInterface, equalTo("InputEvent"))
+        for (values in promises.map { it.value.asJsonArray() }) {
+            for (i in 0 until values.length()) {
+                val (key, actual, expected, eventInterface) = values.get(i).asJSList<String>()
+
+                assertThat("Auto-filled value must match ($key)", actual, equalTo(expected))
+                assertThat(
+                    "input event should be dispatched with InputEvent interface",
+                    eventInterface,
+                    equalTo("InputEvent")
+                )
+            }
         }
     }
 
     @Test fun autofillUnknownValue() {
         // Test parts of the Oreo auto-fill API; there is another autofill test in
         // SessionAccessibility for a11y auto-fill support.
-        mainSession.loadTestPath(FORMS_HTML_PATH)
+        mainSession.loadUri(pageUrl)
         // Wait for the auto-fill nodes to populate.
         sessionRule.waitUntilCalled(object : Autofill.Delegate {
             @AssertCalled(count = 1)
@@ -266,7 +282,7 @@ class AutofillDelegateTest : BaseSessionTest() {
     @WithDisplay(width = 100, height = 100)
     @Test fun autofillNavigation() {
         // Wait for the accessibility nodes to populate.
-        mainSession.loadTestPath(FORMS_HTML_PATH)
+        mainSession.loadUri(pageUrl)
         sessionRule.waitUntilCalled(object : Autofill.Delegate {
             @AssertCalled(count = 4)
             override fun onAutofill(session: GeckoSession,
@@ -410,7 +426,7 @@ class AutofillDelegateTest : BaseSessionTest() {
     @Test fun autofillActiveChange() {
         // We should blur the active autofill node if the session is set
         // inactive. Likewise, we should focus a node once we return.
-        mainSession.loadTestPath(FORMS_HTML_PATH)
+        mainSession.loadUri(pageUrl)
         // Wait for the auto-fill nodes to populate.
         sessionRule.waitUntilCalled(object : Autofill.Delegate {
             // For the root document and the iframe document, each has a form group and
@@ -500,267 +516,5 @@ class AutofillDelegateTest : BaseSessionTest() {
         // Each page has 3 nodes for autofill.
         assertThat("autofill hint count",
                    checkAutofillChild(root), equalTo(6))
-    }
-
-    class MockViewNode : ViewStructure() {
-        private var mClassName: String? = null
-        private var mEnabled = false
-        private var mVisibility = -1
-        private var mPackageName: String? = null
-        private var mTypeName: String? = null
-        private var mEntryName: String? = null
-        private var mAutofillType = -1
-        private var mAutofillHints: Array<String>? = null
-        private var mInputType = -1
-        private var mHtmlInfo: HtmlInfo? = null
-        private var mWebDomain: String? = null
-        private var mFocused = false
-        private var mFocusable = false
-
-        var children = ArrayList<MockViewNode?>()
-        var id = View.NO_ID
-        var height = 0
-        var width = 0
-
-        val className get() = mClassName
-        val htmlInfo get() = mHtmlInfo
-        val autofillHints get() = mAutofillHints
-        val autofillType get() = mAutofillType
-        val webDomain get() = mWebDomain
-        val isEnabled get() = mEnabled
-        val isFocused get() = mFocused
-        val isFocusable get() = mFocusable
-        val visibility get() = mVisibility
-        val inputType get() = mInputType
-
-        override fun setId(id: Int, packageName: String?, typeName: String?, entryName: String?) {
-            this.id = id
-            mPackageName = packageName
-            mTypeName = typeName
-            mEntryName = entryName
-        }
-
-        override fun setHint(hint: CharSequence?) {
-            TODO("not implemented")
-        }
-
-        override fun setElevation(elevation: Float) {
-            TODO("not implemented")
-        }
-
-        override fun getText(): CharSequence {
-            TODO("not implemented")
-        }
-
-        override fun setText(text: CharSequence?) {
-            TODO("not implemented")
-        }
-
-        override fun setText(text: CharSequence?, selectionStart: Int, selectionEnd: Int) {
-            TODO("not implemented")
-        }
-
-        override fun asyncCommit() {
-            TODO("not implemented")
-        }
-
-        override fun getChildCount(): Int = children.size
-
-        override fun setEnabled(state: Boolean) {
-            mEnabled = state
-        }
-
-        override fun setLocaleList(localeList: LocaleList?) {
-            TODO("not implemented")
-        }
-
-        override fun setDimens(left: Int, top: Int, scrollX: Int, scrollY: Int, width: Int, height: Int) {
-            this.width = width
-            this.height = height
-        }
-
-        override fun setChecked(state: Boolean) {
-            TODO("not implemented")
-        }
-
-        override fun setContextClickable(state: Boolean) {
-            TODO("not implemented")
-        }
-
-        override fun setAccessibilityFocused(state: Boolean) {
-            TODO("not implemented")
-        }
-
-        override fun setAlpha(alpha: Float) {
-            TODO("not implemented")
-        }
-
-        override fun setTransformation(matrix: Matrix?) {
-            TODO("not implemented")
-        }
-
-        override fun setClassName(className: String?) {
-            mClassName = className
-        }
-
-        override fun setLongClickable(state: Boolean) {
-            TODO("not implemented")
-        }
-
-        override fun newChild(index: Int): ViewStructure {
-            val child = MockViewNode()
-            children[index] = child
-            return child
-        }
-
-        override fun getHint(): CharSequence {
-            TODO("not implemented")
-        }
-
-        override fun setInputType(inputType: Int) {
-            mInputType = inputType
-        }
-
-        override fun setWebDomain(domain: String?) {
-            mWebDomain = domain
-        }
-
-        override fun setAutofillOptions(options: Array<out CharSequence>?) {
-            TODO("not implemented")
-        }
-
-        override fun setTextStyle(size: Float, fgColor: Int, bgColor: Int, style: Int) {
-            TODO("not implemented")
-        }
-
-        override fun setVisibility(visibility: Int) {
-            mVisibility = visibility
-        }
-
-        override fun getAutofillId(): AutofillId? {
-            TODO("not implemented")
-        }
-
-        override fun setHtmlInfo(htmlInfo: HtmlInfo) {
-            mHtmlInfo = htmlInfo
-        }
-
-        override fun setTextLines(charOffsets: IntArray?, baselines: IntArray?) {
-            TODO("not implemented")
-        }
-
-        override fun getExtras(): Bundle {
-            TODO("not implemented")
-        }
-
-        override fun setClickable(state: Boolean) {
-            TODO("not implemented")
-        }
-
-        override fun newHtmlInfoBuilder(tagName: String): HtmlInfo.Builder {
-            return MockHtmlInfoBuilder(tagName)
-        }
-
-        override fun getTextSelectionEnd(): Int {
-            TODO("not implemented")
-        }
-
-        override fun setAutofillId(id: AutofillId) {
-            TODO("not implemented")
-        }
-
-        override fun setAutofillId(parentId: AutofillId, virtualId: Int) {
-            TODO("not implemented")
-        }
-
-        override fun hasExtras(): Boolean {
-            TODO("not implemented")
-        }
-
-        override fun addChildCount(num: Int): Int {
-            TODO("not implemented")
-        }
-
-        override fun setAutofillType(type: Int) {
-            mAutofillType = type
-        }
-
-        override fun setActivated(state: Boolean) {
-            TODO("not implemented")
-        }
-
-        override fun setFocused(state: Boolean) {
-            mFocused = state
-        }
-
-        override fun getTextSelectionStart(): Int {
-            TODO("not implemented")
-        }
-
-        override fun setChildCount(num: Int) {
-            children = ArrayList()
-            for (i in 0 until num) {
-                children.add(null)
-            }
-        }
-
-        override fun setAutofillValue(value: AutofillValue?) {
-            TODO("not implemented")
-        }
-
-        override fun setAutofillHints(hint: Array<String>?) {
-            mAutofillHints = hint
-        }
-
-        override fun setContentDescription(contentDescription: CharSequence?) {
-            TODO("not implemented")
-        }
-
-        override fun setFocusable(state: Boolean) {
-            mFocusable = state
-        }
-
-        override fun setCheckable(state: Boolean) {
-            TODO("not implemented")
-        }
-
-        override fun asyncNewChild(index: Int): ViewStructure {
-            TODO("not implemented")
-        }
-
-        override fun setSelected(state: Boolean) {
-            TODO("not implemented")
-        }
-
-        override fun setDataIsSensitive(sensitive: Boolean) {
-            TODO("not implemented")
-        }
-
-        override fun setOpaque(opaque: Boolean) {
-            TODO("not implemented")
-        }
-    }
-
-    class MockHtmlInfoBuilder(tagName: String) : ViewStructure.HtmlInfo.Builder() {
-        val mTagName = tagName
-        val mAttributes: MutableList<Pair<String, String>> = mutableListOf()
-
-        override fun addAttribute(name: String, value: String): ViewStructure.HtmlInfo.Builder {
-            mAttributes.add(Pair(name, value))
-            return this
-        }
-
-        override fun build(): ViewStructure.HtmlInfo {
-            return MockHtmlInfo(mTagName, mAttributes)
-        }
-    }
-
-    class MockHtmlInfo(tagName: String, attributes: MutableList<Pair<String, String>>)
-            : ViewStructure.HtmlInfo() {
-        private val mTagName = tagName
-        private val mAttributes = attributes
-
-        override fun getTag() = mTagName
-        override fun getAttributes() = mAttributes
     }
 }
