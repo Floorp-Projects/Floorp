5ba05920fce26276051f2319e5c2b51e00a5bb2b
Michael Froman <mfroman@mozilla.com>
2022-05-16 21:12:36 -0500

    Bug 1766646 - Vendor libwebrtc from 9861f960c3
    Upstream commit: https://webrtc.googlesource.com/src/+/9861f960c320f9a35bdb2c795c068a4faed34862
        dcsctp: Add operators on TimeMs and DurationMs
    
        To be able to use them type-safely, they should support native
        operators (e.g. adding a time and a duration, or subtracting two time
        values), as the alternative is to manage them as numbers.
    
        Yes, this makes them behave a bit like absl::Time/absl::Duration.
    
        Bug: webrtc:12614
        Change-Id: I4dea12e33698a46e71fb549f44c06f2f381c9201
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/215143
        Reviewed-by: Tommi <tommi@webrtc.org>
        Commit-Queue: Victor Boivie <boivie@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33725}

--- third_party/libwebrtc/net/dcsctp/public/types.h
+++ third_party/libwebrtc/net/dcsctp/public/types.h
@@ -11,6 +11,8 @@
 #ifndef NET_DCSCTP_PUBLIC_TYPES_H_
 #define NET_DCSCTP_PUBLIC_TYPES_H_
 
+#include <cstdint>
+
 #include "net/dcsctp/public/strong_alias.h"
 
 namespace dcsctp {
@@ -29,10 +31,74 @@ using TimeoutID = StrongAlias<class TimeoutTag, uint64_t>;
 using IsUnordered = StrongAlias<class IsUnorderedTag, bool>;
 
 // Duration, as milliseconds. Overflows after 24 days.
-using DurationMs = StrongAlias<class DurationMsTag, int32_t>;
+class DurationMs : public StrongAlias<class DurationMsTag, int32_t> {
+ public:
+  constexpr explicit DurationMs(const UnderlyingType& v)
+      : StrongAlias<class DurationMsTag, int32_t>(v) {}
 
-// Current time, in milliseconds since a client-defined epoch.´
-using TimeMs = StrongAlias<class TimeMsTag, int64_t>;
+  // Convenience methods for working with time.
+  constexpr DurationMs& operator+=(DurationMs d) {
+    value_ += d.value_;
+    return *this;
+  }
+  constexpr DurationMs& operator-=(DurationMs d) {
+    value_ -= d.value_;
+    return *this;
+  }
+  template <typename T>
+  constexpr DurationMs& operator*=(T factor) {
+    value_ *= factor;
+    return *this;
+  }
+};
+
+constexpr inline DurationMs operator+(DurationMs lhs, DurationMs rhs) {
+  return lhs += rhs;
+}
+constexpr inline DurationMs operator-(DurationMs lhs, DurationMs rhs) {
+  return lhs -= rhs;
+}
+template <typename T>
+constexpr inline DurationMs operator*(DurationMs lhs, T rhs) {
+  return lhs *= rhs;
+}
+template <typename T>
+constexpr inline DurationMs operator*(T lhs, DurationMs rhs) {
+  return rhs *= lhs;
+}
+constexpr inline int32_t operator/(DurationMs lhs, DurationMs rhs) {
+  return lhs.value() / rhs.value();
+}
+
+// Represents time, in milliseconds since a client-defined epoch.
+class TimeMs : public StrongAlias<class TimeMsTag, int64_t> {
+ public:
+  constexpr explicit TimeMs(const UnderlyingType& v)
+      : StrongAlias<class TimeMsTag, int64_t>(v) {}
+
+  // Convenience methods for working with time.
+  constexpr TimeMs& operator+=(DurationMs d) {
+    value_ += *d;
+    return *this;
+  }
+  constexpr TimeMs& operator-=(DurationMs d) {
+    value_ -= *d;
+    return *this;
+  }
+};
+
+constexpr inline TimeMs operator+(TimeMs lhs, DurationMs rhs) {
+  return lhs += rhs;
+}
+constexpr inline TimeMs operator+(DurationMs lhs, TimeMs rhs) {
+  return rhs += lhs;
+}
+constexpr inline TimeMs operator-(TimeMs lhs, DurationMs rhs) {
+  return lhs -= rhs;
+}
+constexpr inline DurationMs operator-(TimeMs lhs, TimeMs rhs) {
+  return DurationMs(*lhs - *rhs);
+}
 
 }  // namespace dcsctp
 
