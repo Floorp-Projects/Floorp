b0d233f1a658236e7c70742f4346817b9dba5d14
Michael Froman <mfroman@mozilla.com>
2022-05-18 14:02:05 -0500

    Bug 1766646 - Vendor libwebrtc from 1366b0f841
    Upstream commit: https://webrtc.googlesource.com/src/+/1366b0f841c84d842e3ff5ab9050c2b39a96c2f2
        AsyncResolver: avoid hanging the WorkerThread.
    
        There's a problem where the destruction of the contained rtc::Thread
        will join the spawned thread blocked on getaddrinfo(). However,
        getaddrinfo() is sometimes slow and this behavior hinders packet traffic
        when it happens.
    
        Fix this by using the brand new detachable PlatformThread support.
    
        Fixed: b:181572711, webrtc:12659
        Change-Id: I0b7e0cca3b8b1b3ed22328d940b1bb95cacb5e24
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/214780
        Commit-Queue: Markus Handell <handellm@webrtc.org>
        Reviewed-by: Tommi <tommi@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33804}

--- third_party/libwebrtc/rtc_base/async_resolver.h
+++ third_party/libwebrtc/rtc_base/async_resolver.h
@@ -17,12 +17,13 @@
 #include <winsock2.h>  // NOLINT
 #endif
 
-#include <memory>
 #include <vector>
 
 #include "api/sequence_checker.h"
 #include "rtc_base/async_resolver_interface.h"
+#include "rtc_base/event.h"
 #include "rtc_base/ip_address.h"
+#include "rtc_base/ref_counted_object.h"
 #include "rtc_base/socket_address.h"
 #include "rtc_base/system/no_unique_address.h"
 #include "rtc_base/system/rtc_export.h"
@@ -52,6 +53,9 @@ class RTC_EXPORT AsyncResolver : public AsyncResolverInterface {
   const std::vector<IPAddress>& addresses() const;
 
  private:
+  // Fwd decl.
+  struct State;
+
   void ResolveDone(std::vector<IPAddress> addresses, int error)
       RTC_EXCLUSIVE_LOCKS_REQUIRED(sequence_checker_);
   void MaybeSelfDestruct();
@@ -59,11 +63,10 @@ class RTC_EXPORT AsyncResolver : public AsyncResolverInterface {
   SocketAddress addr_ RTC_GUARDED_BY(sequence_checker_);
   std::vector<IPAddress> addresses_ RTC_GUARDED_BY(sequence_checker_);
   int error_ RTC_GUARDED_BY(sequence_checker_);
-  webrtc::ScopedTaskSafety safety_ RTC_GUARDED_BY(sequence_checker_);
-  std::unique_ptr<Thread> popup_thread_ RTC_GUARDED_BY(sequence_checker_);
   bool recursion_check_ =
       false;  // Protects against SignalDone calling into Destroy.
   bool destroy_called_ = false;
+  scoped_refptr<State> state_;
   RTC_NO_UNIQUE_ADDRESS webrtc::SequenceChecker sequence_checker_;
 };
 
