7c412396d838a5416d13217d24fc69811cbb6f89
Michael Froman <mfroman@mozilla.com>
2022-05-18 16:59:38 -0500

    Bug 1766646 - Vendor libwebrtc from b2f8c1675d
    Upstream commit: https://webrtc.googlesource.com/src/+/b2f8c1675d74b860c63f97a38e63f3578505391d
        Use unordered_map in ReceiveStatisticsImpl
    
        In highly loaded media servers, ReceiveStatisticsImpl's use of std::map
        attributes to ~0.32% CPU. It needs to be able to iterate through the
        statisticians in order when reporting, but that is considered to be rare
        compared to how often they are looked up. So this commit adds a separate
        sorted set for just keeping track of the SSRCs, and letting the map of
        SSRC to Statisticians, be unordered.
    
        Bug: webrtc:12689
        Change-Id: I69fe41d96bca31b2e8d669b58b5c7afabceaa6a6
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/216385
        Commit-Queue: Victor Boivie <boivie@webrtc.org>
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33864}

--- third_party/libwebrtc/modules/rtp_rtcp/source/receive_statistics_impl.h
+++ third_party/libwebrtc/modules/rtp_rtcp/source/receive_statistics_impl.h
@@ -13,8 +13,8 @@
 
 #include <algorithm>
 #include <functional>
-#include <map>
 #include <memory>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -187,9 +187,12 @@ class ReceiveStatisticsImpl : public ReceiveStatistics {
       Clock* clock,
       int max_reordering_threshold)>
       stream_statistician_factory_;
-  uint32_t last_returned_ssrc_;
+  // The index within `all_ssrcs_` that was last returned.
+  size_t last_returned_ssrc_idx_;
+  std::vector<uint32_t> all_ssrcs_;
   int max_reordering_threshold_;
-  std::map<uint32_t, std::unique_ptr<StreamStatisticianImplInterface>>
+  std::unordered_map<uint32_t /*ssrc*/,
+                     std::unique_ptr<StreamStatisticianImplInterface>>
       statisticians_;
 };
 
