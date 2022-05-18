1daf11c0f67709dc21b390d83bb0be0147193d27
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2022-05-18 15:57:10 +0000

    Bug 1769992 - Enable Win32K Lockdown for Windows10 Fall Creators+ r=bobowen
    Differential Revision: https://phabricator.services.mozilla.com/D146716

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -1324,8 +1324,12 @@ bool SandboxBroker::SetSecurityLevelForUtilityProcess(
     SANDBOX_ENSURE_SUCCESS(result, "Failed to initialize signed policy rules.");
   }
 
-  result = AddWin32kLockdownPolicy(mPolicy, false);
-  SANDBOX_ENSURE_SUCCESS(result, "Failed to add the win32k lockdown policy");
+  // Win32k lockdown might not work on earlier versions
+  // Bug 1719212, 1769992
+  if (IsWin10FallCreatorsUpdateOrLater()) {
+    result = AddWin32kLockdownPolicy(mPolicy, false);
+    SANDBOX_ENSURE_SUCCESS(result, "Failed to add the win32k lockdown policy");
+  }
 
   mitigations = sandbox::MITIGATION_STRICT_HANDLE_CHECKS |
                 sandbox::MITIGATION_DLL_SEARCH_ORDER
