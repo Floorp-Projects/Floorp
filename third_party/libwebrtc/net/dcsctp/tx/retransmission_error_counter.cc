b202f8c3d9e1a82623608e338e148dd8fe0765d2
Michael Froman <mfroman@mozilla.com>
2022-05-18 20:57:13 -0500

    Bug 1766646 - Vendor libwebrtc from 3371638229
    Upstream commit: https://webrtc.googlesource.com/src/+/337163822955431120f69c3f6096ab26a7e9539f
        dcsctp: Use correct log severity
    
        As INFO is aliased to LS_INFO, this didn't trigger any warnings or
        compilation errors.
    
        Bug: None
        Change-Id: I1ed30c435d9ee6ea1b51d85a375d70135d3475e6
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/216689
        Reviewed-by: Florent Castelli <orphis@webrtc.org>
        Commit-Queue: Victor Boivie <boivie@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#33912}

--- third_party/libwebrtc/net/dcsctp/tx/retransmission_error_counter.cc
+++ third_party/libwebrtc/net/dcsctp/tx/retransmission_error_counter.cc
@@ -16,8 +16,8 @@ namespace dcsctp {
 bool RetransmissionErrorCounter::Increment(absl::string_view reason) {
   ++counter_;
   if (counter_ > limit_) {
-    RTC_DLOG(INFO) << log_prefix_ << reason
-                   << ", too many retransmissions, counter=" << counter_;
+    RTC_DLOG(LS_INFO) << log_prefix_ << reason
+                      << ", too many retransmissions, counter=" << counter_;
     return false;
   }
 
