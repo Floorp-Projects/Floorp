63d6cd55c60922d14518a731261b1a5e58f07904
Iain Ireland <iireland@mozilla.com>
2022-05-19 14:30:10 +0000

    Bug 1770048: Improve self-hosted new_List r=jandem,tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D146760

--- js/src/builtin/Array.h
+++ js/src/builtin/Array.h
@@ -131,6 +131,8 @@ extern JSObject* ArgumentsSliceDense(JSContext* cx, HandleObject obj,
                                      int32_t begin, int32_t end,
                                      HandleObject result);
 
+extern bool intrinsic_newList(JSContext* cx, unsigned argc, js::Value* vp);
+
 /*
  * Append the given (non-hole) value to the end of an array.  The array must be
  * a newborn array -- that is, one which has not been exposed to script for
