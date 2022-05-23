86ec9c28f7fe60429615fdad9b1689a54565d17c
Emilio Cobos Álvarez <emilio@crisal.io>
2022-05-23 20:43:15 +0000

    Bug 1472602 - Sticky/relative position fixes in nsTableWrapperFrame. r=dholbert
    Captions should be relatively-positioned (other browsers support this as
    well). This ensures we correctly save their normal positions and thus
    that the sticky scroll container knows how to reposition them correctly.
    
    Avoid registering inner table frames with the sticky scroll container to
    make the assertion sound in cases of sticky-positioned tables.
    
    Differential Revision: https://phabricator.services.mozilla.com/D147092

--- layout/generic/StickyScrollContainer.cpp
+++ layout/generic/StickyScrollContainer.cpp
@@ -34,6 +34,12 @@ StickyScrollContainer::~StickyScrollContainer() {
 // static
 StickyScrollContainer* StickyScrollContainer::GetStickyScrollContainerForFrame(
     nsIFrame* aFrame) {
+  if (MOZ_UNLIKELY(aFrame->IsTableFrame())) {
+    // position: sticky applies to both the table and the table wrapper frame,
+    // but we only want to apply it to the outer frame.
+    return nullptr;
+  }
+
   nsIScrollableFrame* scrollFrame = nsLayoutUtils::GetNearestScrollableFrame(
       aFrame->GetParent(), nsLayoutUtils::SCROLLABLE_SAME_DOC |
                                nsLayoutUtils::SCROLLABLE_STOP_AT_PAGE |
@@ -345,7 +351,10 @@ void StickyScrollContainer::GetScrollRanges(nsIFrame* aFrame,
 void StickyScrollContainer::PositionContinuations(nsIFrame* aFrame) {
   NS_ASSERTION(nsLayoutUtils::IsFirstContinuationOrIBSplitSibling(aFrame),
                "Should be starting from the first continuation");
-  nsPoint translation = ComputePosition(aFrame) - aFrame->GetNormalPosition();
+  bool hadProperty;
+  nsPoint translation =
+      ComputePosition(aFrame) - aFrame->GetNormalPosition(&hadProperty);
+  MOZ_ASSERT(hadProperty, "Sticky child without saved normal position?");
 
   // Move all continuation frames by the same amount.
   for (nsIFrame* cont = aFrame; cont;
