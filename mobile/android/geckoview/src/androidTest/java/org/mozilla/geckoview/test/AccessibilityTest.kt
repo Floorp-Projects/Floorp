bbe66f011f712369bc18b7f196f59202c1dacaec
Eitan Isaacson <eitan@monotonous.org>
2022-05-23 15:51:09 +0000

    Bug 1758540 - Pref on accessibility cache in Android. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D141638

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/AccessibilityTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/AccessibilityTest.kt
@@ -1389,9 +1389,6 @@ class AccessibilityTest : BaseSessionTest() {
     }
 
     @Test fun testRemoteAccessibilityFocusIframe() {
-        // TODO: Bug 1758540
-        assumeThat(sessionRule.env.isFission, equalTo(false))
-
         testAccessibilityFocusIframe(REMOTE_IFRAME);
     }
 
@@ -1438,9 +1435,6 @@ class AccessibilityTest : BaseSessionTest() {
 
     @Setting(key = Setting.Key.FULL_ACCESSIBILITY_TREE, value = "true")
     @Test fun testRemoteIframeTree() {
-        // TODO: Bug 1758540
-        assumeThat(sessionRule.env.isFission, equalTo(false))
-
         testIframeTree(REMOTE_IFRAME);
     }
 
