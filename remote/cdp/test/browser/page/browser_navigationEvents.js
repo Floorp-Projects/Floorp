42dc9793cb105fa6f0de4d5505eead0351484299
Julian Descottes <jdescottes@mozilla.com>
2022-05-23 19:06:12 +0000

    Bug 1636453 - [cdp] Support Page.navigate for hash navigation r=webdriver-reviewers,whimboo
    Depends on D146985
    
    The previous changeset changed the implementation of navigatedWithinDocument to
    be emitted in the proper scenarios (ie, same document navigation).
    
    However our Page.navigate implementation would still timeout waiting on a
    network request if you tried to programmatically perform a hash navigation.
    Meaning hash navigation was only working when triggered from the page.
    
    With this changeset, we try to detect hash navigations in Page.navigate and
    treat them as "network-less" navigations (same as for non http/https navigations)
    
    Differential Revision: https://phabricator.services.mozilla.com/D147076

--- remote/cdp/test/browser/page/browser_navigationEvents.js
+++ remote/cdp/test/browser/page/browser_navigationEvents.js
@@ -43,6 +43,11 @@ add_task(async function pageWithoutFrame({ client }) {
   info("Test Page.navigate");
   recordPromises();
 
+  let navigatedWithinDocumentResolved = false;
+  Page.navigatedWithinDocument().finally(
+    () => (navigatedWithinDocumentResolved = true)
+  );
+
   const url = RANDOM_ID_PAGE_URL;
   const { frameId } = await Page.navigate({ url });
   info("A new page has been requested");
@@ -90,6 +95,11 @@ add_task(async function pageWithoutFrame({ client }) {
     randomId2,
     "Test tab randomId has been updated after reload"
   );
+
+  ok(
+    !navigatedWithinDocumentResolved,
+    "navigatedWithinDocument never resolved during the test"
+  );
 });
 
 add_task(async function pageWithSingleFrame({ client }) {
@@ -112,6 +122,48 @@ add_task(async function pageWithSingleFrame({ client }) {
     frameId,
     "Received the correct frameId for the frameNavigated event"
   );
+});
+
+add_task(async function sameDocumentNavigation({ client }) {
+  await loadURL(PAGE_URL);
+
+  const { Page } = client;
+
+  // turn on navigation related events, such as DOMContentLoaded et al.
+  await Page.enable();
+  info("Page domain has been enabled");
+
+  const { frameTree } = await Page.getFrameTree();
+
+  info("Test Page.navigate for a same document navigation");
+  const onNavigatedWithinDocument = Page.navigatedWithinDocument();
+
+  let unexpectedEventResolved = false;
+  Promise.race([
+    Page.frameStartedLoading(),
+    Page.frameNavigated(),
+    Page.domContentEventFired(),
+    Page.loadEventFired(),
+    Page.frameStoppedLoading(),
+  ]).then(() => (unexpectedEventResolved = true));
+
+  const url = `${PAGE_URL}#some-hash`;
+  const { frameId } = await Page.navigate({ url });
+  ok(frameId, "Page.navigate returned a frameId");
+  is(
+    frameId,
+    frameTree.frame.id,
+    "The Page.navigate's frameId is the same than getFrameTree's one"
+  );
+
+  const event = await onNavigatedWithinDocument;
+  is(
+    event.frameId,
+    frameId,
+    "The navigatedWithinDocument frameId is the same as in Page.navigate"
+  );
+  is(event.url, url, "The navigatedWithinDocument url is the expected url");
+  ok(!unexpectedEventResolved, "No unexpected navigation event resolved.");
 });
 
 async function assertNavigationEvents({ url, frameId }) {
