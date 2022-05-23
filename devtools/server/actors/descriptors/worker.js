b8ab190cd3fc8268d85884b228e53b1eba7d914a
Alexandre Poirot <poirot.alex@gmail.com>
2022-05-23 08:38:01 +0000

    Bug 1770130 - [devtools] Get rid of WorkerDescriptorActor.attach. r=nchevobbe,devtools-backward-compat-reviewers
    We can immediately "attach" the descriptor as it gets instantiated.
    So that the frontend no longer need to call this before doing anything around the worker.
    
    I also renamed the descriptor front method "attach" method,
    in order to better highlight how special it is.
    Hopefully we can drop this once the worker targets are created by the server.
    i.e. when we enable service side worker targets for the browser toolbox,
    which includes supporting service workers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D146787

--- devtools/server/actors/descriptors/worker.js
+++ devtools/server/actors/descriptors/worker.js
@@ -45,7 +45,7 @@ const WorkerDescriptorActor = protocol.ActorClassWithSpec(
     initialize(conn, dbg) {
       protocol.Actor.prototype.initialize.call(this, conn);
       this._dbg = dbg;
-      this._attached = false;
+
       this._threadActor = null;
       this._transport = null;
 
@@ -53,6 +53,9 @@ const WorkerDescriptorActor = protocol.ActorClassWithSpec(
         onClose: this._onWorkerClose.bind(this),
         onError: this._onWorkerError.bind(this),
       };
+
+      this._dbg.addListener(this._dbgListener);
+      this._attached = true;
     },
 
     form() {
@@ -62,7 +65,11 @@ const WorkerDescriptorActor = protocol.ActorClassWithSpec(
         threadActor: this._threadActor,
         id: this._dbg.id,
         url: this._dbg.url,
-        traits: {},
+        traits: {
+          // @backward-compat { version 102 } WorkerDescriptor no longer implement attach method
+          //                  We can remove the trait once 102 is the release channel.
+          doNotAttach: true,
+        },
         type: this._dbg.type,
       };
       if (this._dbg.type === Ci.nsIWorkerDebugger.TYPE_SERVICE) {
@@ -83,22 +90,6 @@ const WorkerDescriptorActor = protocol.ActorClassWithSpec(
         }
       }
       return form;
-    },
-
-    attach() {
-      if (this._dbg.isClosed) {
-        return { error: "closed" };
-      }
-
-      if (!this._attached) {
-        this._dbg.addListener(this._dbgListener);
-        this._attached = true;
-      }
-
-      return {
-        type: "attached",
-        url: this._dbg.url,
-      };
     },
 
     detach() {
