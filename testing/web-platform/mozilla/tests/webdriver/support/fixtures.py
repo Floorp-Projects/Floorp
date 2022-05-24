83cd6b3574fa97e5e348d1df51153e94d9c3c657
Henrik Skupin <mail@hskupin.info>
2022-05-24 04:38:10 +0000

    Bug 1763124 - [wdspec] Add tests for erroneous requests for "browsingContext.navigate". r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D146909

--- testing/web-platform/mozilla/tests/webdriver/support/fixtures.py
+++ testing/web-platform/mozilla/tests/webdriver/support/fixtures.py
@@ -66,6 +66,18 @@ def browser(full_configuration):
 
 
 @pytest.fixture
+def custom_profile(configuration):
+    # Clone the known profile for automation preferences
+    firefox_options = configuration["capabilities"]["moz:firefoxOptions"]
+    _, profile_folder = firefox_options["args"]
+    profile = Profile.clone(profile_folder)
+
+    yield profile
+
+    profile.cleanup()
+
+
+@pytest.fixture
 def geckodriver(configuration):
     """Start a geckodriver instance directly."""
     driver = None
