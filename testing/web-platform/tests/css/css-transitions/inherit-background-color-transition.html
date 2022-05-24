dbuskariol-org/chromium

commit d7e0cc711958dfb3aa39f0f912db1f154617bd98

Check child_change instead of change for explicit inherit.
Additionally, re-ordered checks in ComputedStyle::ComputeDifference()
and removed the full ComputedStyle comparison which did extra
comparisons of inherited data.

Bug: 958393
Change-Id: Ib798a688cc27719417326d6b3a6e75ce3595fa50
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594575
Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
Commit-Queue: Rune Lillesveen <futhark@chromium.org>
Cr-Commit-Position: refs/heads/master@{#657180}

third_party/blink/renderer/core/dom/element.cc
@@ -2541,7 +2541,7 @@ StyleRecalcChange Element::RecalcOwnStyle(const StyleRecalcChange change) {
   }
 
   if (new_style) {
-    if (old_style && !change.RecalcChildren() &&
+    if (old_style && !child_change.RecalcChildren() &&
         old_style->HasChildDependentFlags())
       new_style->CopyChildDependentFlagsFrom(*old_style);
     if (RuntimeEnabledFeatures::LayoutNGEnabled())

third_party/blink/renderer/core/style/computed_style.cc
@@ -244,28 +244,23 @@ ComputedStyle::Difference ComputedStyle::ComputeDifference(
        old_style->IsDisplayLayoutCustomBox())) {
     return Difference::kDisplayAffectingDescendantStyles;
   }
-  bool independent_equal = old_style->IndependentInheritedEqual(*new_style);
-  bool non_independent_equal =
-      old_style->NonIndependentInheritedEqual(*new_style);
-  if (!independent_equal || !non_independent_equal) {
-    if (non_independent_equal && !old_style->HasExplicitlyInheritedProperties())
-      return Difference::kIndependentInherited;
+  if (!old_style->NonIndependentInheritedEqual(*new_style))
     return Difference::kInherited;
-  }
-
   if (!old_style->LoadingCustomFontsEqual(*new_style) ||
       old_style->JustifyItems() != new_style->JustifyItems())
     return Difference::kInherited;
-
-  if (*old_style == *new_style) {
+  bool non_inherited_equal = old_style->NonInheritedEqual(*new_style);
+  if (!non_inherited_equal && old_style->HasExplicitlyInheritedProperties()) {
+    return Difference::kInherited;
+  }
+  if (!old_style->IndependentInheritedEqual(*new_style))
+    return Difference::kIndependentInherited;
+  if (non_inherited_equal) {
+    DCHECK(*old_style == *new_style);
     if (PseudoStylesEqual(*old_style, *new_style))
       return Difference::kEqual;
     return Difference::kPseudoStyle;
   }
-
-  if (old_style->HasExplicitlyInheritedProperties())
-    return Difference::kInherited;
-
   if (new_style->HasAnyPublicPseudoStyles() ||
       old_style->HasAnyPublicPseudoStyles())
     return Difference::kPseudoStyle;

third_party/blink/web_tests/external/wpt/css/css-transitions/inherit-height-transition.html
@@ -1,0 +1,26 @@
+<!DOCTYPE html>
+<link rel="help" href="https://drafts.csswg.org/css-transitions/#starting">
+<script src="/resources/testharness.js"></script>
+<script src="/resources/testharnessreport.js"></script>
+<style>
+  div { height: inherit; }
+  #outer { height: 50px; }
+  #outer.collapsed {
+    height: 0px;
+    transition: height 0.01s;
+  }
+</style>
+<div id="outer">
+  <div>
+    <div id="inner">You should only see a flash of red.</div>
+  </div>
+</div>
+<script>
+  promise_test(t => {
+    outer.offsetTop;
+    outer.className = "collapsed";
+    return (new Promise((resolve) => outer.addEventListener("transitionend", resolve))).then(() => {
+      assert_equals(getComputedStyle(inner).height, "0px");
+    });
+  }, "Transitioned height, explicitly inherited down two DOM levels, should inherit correctly");
+</script>

Floorp-Projects/Floorp

commit 824bd0d09dc916e6a0bfddc9eda11fdc890b80ef

Bug 1769972 [wpt PR 34108] - Turn off independent inheritance if children have explicitly inherited properties., a=testonly
Automatic update from web-platform-tests
Turn off independent inheritance if children have explicitly inherited properties.

If any of our children have explicit inheritance of a non-inherited
property, we cannot mark it as using independently inherited properties
only, as some of the explicit properties might inherit (our diffing
considers inherited properties only). We used to gate this behind
a test of diffs in the non-inherited properties, but that would only
consider that difference on this element, not its children (and the
problem could propagate fro there to its grandchildren).

This seems to have been broken in a refactor in
d7e0cc711958dfb3aa39f0f912db1f154617bd98.

Fixed: 1325340
Change-Id: If6b790fea727ad68322cde7c4c2a342322283f68
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3650513
Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
Reviewed-by: Rune Lillesveen <futhark@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1005798}

--

wpt-commits: e4226161361351158d2486a7ef588cc2a3b74474
wpt-pr: 34108

testing/web-platform/tests/css/css-transitions/inherit-background-color-transition-ref.html
@@ -1,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+  <body style="color: black; background-color: white">
+    <div><div>PASS if black on white</div></div>
+  </body>
+</html>

testing/web-platform/tests/css/css-transitions/inherit-background-color-transition.html
@@ -1,0 +1,41 @@
+<!DOCTYPE html>
+<html id="html" class="reftest-wait">
+  <title>Verifies that 'background-color' from a transition inherits explicitly down if requested</title>
+  <link rel="help" href="https://crbug.com/1325340">
+  <link rel="match" href="inherit-background-color-transition-ref.html">
+  <script src="support/helper.js"></script>
+  <style>
+    body { transition: background-color 1s; color: white; background-color: black; }
+    .light { color: black; background-color: white; }
+  </style>
+</head>
+<body id="body">
+  <div style="background-color: inherit">
+    <div style="background-color: inherit" class="hoverdemo">PASS if black on white</div>
+  </div>
+  <script>
+    body.offsetTop;
+
+    async function run() {
+      let transitionEnd = new Promise((resolve) => {
+        body.addEventListener('transitionend', resolve);
+      });
+
+      // Trigger transition:
+      body.classList.toggle('light');
+
+      const transition = body.getAnimations()[0];
+      await transition.ready;
+      await waitForFrame();
+
+      // Expedite transition, but let it finish naturally.
+      transition.currentTime = transition.effect.getTiming().duration - 1;
+      await transitionEnd;
+
+      await waitForFrame();
+    }
+
+    run().then(() => html.classList.toggle('reftest-wait'));
+  </script>
+</body>
+</html>