01bf48b03697ca321408acaf4bdddbbd8e8095da
Michael Froman <mfroman@mozilla.com>
2022-05-25 11:14:27 -0500

    Bug 1766646 - Vendor libwebrtc from e6ee8fab7e
    Upstream commit: https://webrtc.googlesource.com/src/+/e6ee8fab7eac915b2b6abc9b71b6d33ad086f3d1
        Deprecate microsecond timestamps in RTC event log.
    
        (Microsecond timestamps are only used in the legacy wire-format,
        and the clocks only have microsecond resolution on some platforms.)
    
        Also convert structs on the parsing side to use a Timestamp instead
        of a uint64_t to represent the log time.
    
        Bug: webrtc:11933
        Change-Id: Ide5a0217d99f13f2e243115b163f13e0525648c7
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/219467
        Commit-Queue: Björn Terelius <terelius@webrtc.org>
        Reviewed-by: Christoffer Rodbro <crodbro@webrtc.org>
        Reviewed-by: Sebastian Jansson <srte@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34097}

--- third_party/libwebrtc/logging/rtc_event_log/events/rtc_event_generic_ack_received.h
+++ third_party/libwebrtc/logging/rtc_event_log/events/rtc_event_generic_ack_received.h
@@ -16,6 +16,7 @@
 
 #include "absl/types/optional.h"
 #include "api/rtc_event_log/rtc_event.h"
+#include "api/units/timestamp.h"
 
 namespace webrtc {
 
@@ -77,19 +78,19 @@ class RtcEventGenericAckReceived final : public RtcEvent {
 
 struct LoggedGenericAckReceived {
   LoggedGenericAckReceived() = default;
-  LoggedGenericAckReceived(int64_t timestamp_us,
+  LoggedGenericAckReceived(Timestamp timestamp,
                            int64_t packet_number,
                            int64_t acked_packet_number,
                            absl::optional<int64_t> receive_acked_packet_time_ms)
-      : timestamp_us(timestamp_us),
+      : timestamp(timestamp),
         packet_number(packet_number),
         acked_packet_number(acked_packet_number),
         receive_acked_packet_time_ms(receive_acked_packet_time_ms) {}
 
-  int64_t log_time_us() const { return timestamp_us; }
-  int64_t log_time_ms() const { return timestamp_us / 1000; }
+  int64_t log_time_us() const { return timestamp.us(); }
+  int64_t log_time_ms() const { return timestamp.ms(); }
 
-  int64_t timestamp_us;
+  Timestamp timestamp = Timestamp::MinusInfinity();
   int64_t packet_number;
   int64_t acked_packet_number;
   absl::optional<int64_t> receive_acked_packet_time_ms;
