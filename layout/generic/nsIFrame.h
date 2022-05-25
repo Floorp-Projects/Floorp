58955e14df9a1f650d1ed98c35eebccc93ec6de4
Emilio Cobos Álvarez <emilio@crisal.io>
2022-05-25 17:54:22 +0000

    Bug 1730284 - Use whether the embedder element intersects the viewport to decide whether to throttle in-process iframes. r=smaug
    This is more likely to be understandable by developers, matches other
    browsers more closely (see bug comments), and seems more in-line with
    what we do for OOP iframes.
    
    Add a pref to not do this throttling at all (which would match Chrome),
    though this is probably good enough for now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D146574

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -4460,11 +4460,6 @@ class nsIFrame : public nsQueryFrame {
   void UpdatePaintCountForPaintedPresShells();
 
   /**
-   * @return true if we painted @aPresShell during the last repaint.
-   */
-  bool DidPaintPresShell(mozilla::PresShell* aPresShell);
-
-  /**
    * Accessors for the absolute containing block.
    */
   bool IsAbsoluteContainer() const {
