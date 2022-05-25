01dbfdcf38eaf91ba6dd60edbc0db3b377b5c435
Emilio Cobos Álvarez <emilio@crisal.io>
2022-05-25 17:54:22 +0000

    Bug 1730284 - Factor out some IntersectionObserver code. r=smaug,sefeng
    Differential Revision: https://phabricator.services.mozilla.com/D146573

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -2459,7 +2459,11 @@ class nsContentUtils {
    * Returns the in-process subtree root document in a document hierarchy.
    * This could be a chrome document.
    */
-  static Document* GetInProcessSubtreeRootDocument(Document* aDoc);
+  static Document* GetInProcessSubtreeRootDocument(Document* aDoc) {
+    return const_cast<Document*>(
+        GetInProcessSubtreeRootDocument(const_cast<const Document*>(aDoc)));
+  }
+  static const Document* GetInProcessSubtreeRootDocument(const Document* aDoc);
 
   static void GetShiftText(nsAString& text);
   static void GetControlText(nsAString& text);
