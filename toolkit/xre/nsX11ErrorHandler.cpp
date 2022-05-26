20c681bc0e783e68ca38a2e24c5af388249737c9
Lee Salzman <lsalzman@mozilla.com>
2022-05-26 06:40:53 +0000

    Bug 1685375 - Remove ScopedXErrorHandler. r=jgilbert
    Avoid relying on X11 errors to detect failures where alternative means would
    suffice (i.e. checking results for failure or MakeCurrent failures). All other
    users of ScopedXErrorHandler outside of GLContextProviderGLX use it only to
    ignore errors rather than actually check the error result. Given those concerns,
    we also change the default X11 error handler to merely ignore errors rather than
    abort, such that X11 calls in Gecko no longer require an error trap by default.
    This also avoids contention with other libraries that may temporarily override
    the error handler such as GDK or Cairo since Gecko will never touch the handler
    after startup.
    
    Differential Revision: https://phabricator.services.mozilla.com/D147247

--- toolkit/xre/nsX11ErrorHandler.cpp
+++ toolkit/xre/nsX11ErrorHandler.cpp
@@ -105,14 +105,6 @@ int X11Error(Display* display, XErrorEvent* event) {
     }
   }
 
-  switch (XRE_GetProcessType()) {
-    case GeckoProcessType_Default:
-    case GeckoProcessType_Content:
-      CrashReporter::AppendAppNotesToCrashReport(notes);
-      break;
-    default:;  // crash report notes not supported.
-  }
-
 #ifdef DEBUG
   // The resource id is unlikely to be useful in a crash report without
   // context of other ids, but add it to the debug console output.
@@ -130,16 +122,18 @@ int X11Error(Display* display, XErrorEvent* event) {
 #  endif
 #endif
 
-  MOZ_CRASH_UNSAFE(notes.get());
+  NS_WARNING(notes.get());
+  return 0;
 }
 }
 
 void InstallX11ErrorHandler() {
   XSetErrorHandler(X11Error);
 
-  Display* display = mozilla::DefaultXDisplay();
-  NS_ASSERTION(display, "No X display");
   if (PR_GetEnv("MOZ_X_SYNC")) {
-    XSynchronize(display, X11True);
+    Display* display = mozilla::DefaultXDisplay();
+    if (display) {
+      XSynchronize(display, X11True);
+    }
   }
 }
