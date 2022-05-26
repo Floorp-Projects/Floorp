8933226e7e8ae53748e985e1707d2c50246b23d0
Michael Froman <mfroman@mozilla.com>
2022-05-26 17:09:27 -0500

    Bug 1766646 - Vendor libwebrtc from 4ea80f35f1
    Upstream commit: https://webrtc.googlesource.com/src/+/4ea80f35f1b6a74fcba182a1d769abe94b8513ca
        Disable PT based demuxing if MID header extension is present.
    
        We want to turn off PT based demux because SSRC-based endpoints that
        send media prematurely (which is a popular non-standard behavior still
        heavily in use) can otherwise get incorrect mappings and unsignalled
        ssrc issues because of the PT demux path.
    
        This CL disables PT based demuxing when the MID header extension is
        present on all m= sections in the SDP for that kind (audio/video), not
        caring if it was in the offer or answer. However if PT demuxing has been
        used in the past then it is always allowed. This ensures PT is off by
        default but that either offer or answer can enable PT and once it has
        been on it is also possible to get early media with PT.
    
        - Want PT-based demux? The MID header extension has to be removed in
          either the offer or the answer. Follow-up O/As allow PT demuxing if
          possible.
        - Want to use MID or SSRC demuxing? Great, you don't need PT-based demux
          and won't mind that we turned it off for you.
    
        The reason for disabling PT demux at offer time (if MID is present)
        instead of waiting for the SDP answer is because by the time the SDP
        answer arrives, early media could have triggered PT demux and caused
        incorrect mappings. The safe thing is to assume a spec-compliant
        endpoint until proven otherwise.
    
        However if PT demux is ever enabled, then from that point on we always
        allow PT-based demux in follow-up O/A exchanges. This ensures we don't
        drop packets in follow-up exchanges. The fact that PT-based demux is
        disabled during the initial offer should not matter because before the
        initial O/A exchange we don't have fingerprints.
    
        This change only affects Unified Plan and bundled groups. Existing test
        coverage ensuring we do not break legacy endpoints:
        [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/pc/peer_connection_integrationtest.cc;l=1156
        [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/web_tests/external/wpt/webrtc/protocol/rtp-demuxing.html;l=59
    
        UnsignaledStreamTest is also updated to test the interesting setups.
        A kill-switch is added in case we want to disable this change.
    
        Bug: webrtc:12814
        Change-Id: I807a82a543325753633aaef698e06cb4c9dfebaa
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/221101
        Commit-Queue: Henrik Boström <hbos@webrtc.org>
        Reviewed-by: Taylor Brandstetter <deadbeef@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34251}

--- third_party/libwebrtc/test/peer_scenario/tests/unsignaled_stream_test.cc
+++ third_party/libwebrtc/test/peer_scenario/tests/unsignaled_stream_test.cc
@@ -24,6 +24,31 @@ namespace webrtc {
 namespace test {
 namespace {
 
+enum class MidTestConfiguration {
+  // Legacy endpoint setup where PT demuxing is used.
+  kMidNotNegotiated,
+  // MID is negotiated but missing from packets. PT demuxing is disabled, so
+  // SSRCs have to be added to the SDP for WebRTC to forward packets correctly.
+  // Happens when client is spec compliant but the SFU isn't. Popular legacy.
+  kMidNegotiatedButMissingFromPackets,
+  // Fully spec-compliant: MID is present so we can safely drop packets with
+  // unknown MIDs.
+  kMidNegotiatedAndPresentInPackets,
+};
+
+// Gives the parameterized test a readable suffix.
+std::string TestParametersMidTestConfigurationToString(
+    testing::TestParamInfo<MidTestConfiguration> info) {
+  switch (info.param) {
+    case MidTestConfiguration::kMidNotNegotiated:
+      return "MidNotNegotiated";
+    case MidTestConfiguration::kMidNegotiatedButMissingFromPackets:
+      return "MidNegotiatedButMissingFromPackets";
+    case MidTestConfiguration::kMidNegotiatedAndPresentInPackets:
+      return "MidNegotiatedAndPresentInPackets";
+  }
+}
+
 class FrameObserver : public rtc::VideoSinkInterface<VideoFrame> {
  public:
   FrameObserver() : frame_observed_(false) {}
@@ -53,19 +78,24 @@ void set_ssrc(SessionDescriptionInterface* offer, size_t index, uint32_t ssrc) {
 
 }  // namespace
 
-TEST(UnsignaledStreamTest, ReplacesUnsignaledStreamOnCompletedSignaling) {
+class UnsignaledStreamTest
+    : public ::testing::Test,
+      public ::testing::WithParamInterface<MidTestConfiguration> {};
+
+TEST_P(UnsignaledStreamTest, ReplacesUnsignaledStreamOnCompletedSignaling) {
   // This test covers a scenario that might occur if a remote client starts
-  // sending media packets before negotiation has completed. These packets will
-  // trigger an unsignalled default stream to be created, and connects that to
-  // a default video sink.
-  // In some edge cases using unified plan, the default stream is create in a
-  // different transceiver to where the media SSRC will actually be used.
-  // This test verifies that the default stream is removed properly, and that
-  // packets are demuxed and video frames reach the desired sink.
+  // sending media packets before negotiation has completed. Depending on setup,
+  // these packets either get dropped or trigger an unsignalled default stream
+  // to be created, and connects that to a default video sink.
+  // In some edge cases using Unified Plan and PT demuxing, the default stream
+  // is create in a different transceiver to where the media SSRC will actually
+  // be used. This test verifies that the default stream is removed properly,
+  // and that packets are demuxed and video frames reach the desired sink.
+  const MidTestConfiguration kMidTestConfiguration = GetParam();
 
   // Defined before PeerScenario so it gets destructed after, to avoid use after
   // free.
-  PeerScenario s(*test_info_);
+  PeerScenario s(*::testing::UnitTest::GetInstance()->current_test_info());
 
   PeerScenarioClient::Config config = PeerScenarioClient::Config();
   // Disable encryption so that we can inject a fake early media packet without
@@ -93,14 +123,61 @@ TEST(UnsignaledStreamTest, ReplacesUnsignaledStreamOnCompletedSignaling) {
   std::atomic<bool> got_unsignaled_packet(false);
 
   // We will capture the media ssrc of the first added stream, and preemptively
-  // inject a new media packet using a different ssrc.
-  // This will create "default stream" for the second ssrc and connected it to
-  // the default video sink (not set in this test).
+  // inject a new media packet using a different ssrc. What happens depends on
+  // the test configuration.
+  //
+  // MidTestConfiguration::kMidNotNegotiated:
+  // - MID is not negotiated which means PT-based demuxing is enabled. Because
+  //   the packets have no MID, the second ssrc packet gets forwarded to the
+  //   first m= section. This will create a "default stream" for the second ssrc
+  //   and connect it to the default video sink (not set in this test). The test
+  //   verifies we can recover from this when we later get packets for the first
+  //   ssrc.
+  //
+  // MidTestConfiguration::kMidNegotiatedButMissingFromPackets:
+  // - MID is negotiated wich means PT-based demuxing is disabled. Because we
+  //   modify the packets not to contain the MID anyway (simulating a legacy SFU
+  //   that does not negotiate properly) unknown SSRCs are dropped but do not
+  //   otherwise cause any issues.
+  //
+  // MidTestConfiguration::kMidNegotiatedAndPresentInPackets:
+  // - MID is negotiated which means PT-based demuxing is enabled. In this case
+  //   the packets have the MID so they either get forwarded or dropped
+  //   depending on if the MID is known. The spec-compliant way is also the most
+  //   straight-forward one.
+
   uint32_t first_ssrc = 0;
   uint32_t second_ssrc = 0;
+  absl::optional<int> mid_header_extension_id = absl::nullopt;
 
   signaling.NegotiateSdp(
-      /* munge_sdp = */ {},
+      /* munge_sdp = */
+      [&](SessionDescriptionInterface* offer) {
+        // Obtain the MID header extension ID and if we want the
+        // MidTestConfiguration::kMidNotNegotiated setup then we remove the MID
+        // header extension through SDP munging (otherwise SDP is not modified).
+        for (cricket::ContentInfo& content_info :
+             offer->description()->contents()) {
+          std::vector<RtpExtension> header_extensions =
+              content_info.media_description()->rtp_header_extensions();
+          for (auto it = header_extensions.begin();
+               it != header_extensions.end(); ++it) {
+            if (it->uri == RtpExtension::kMidUri) {
+              // MID header extension found!
+              mid_header_extension_id = it->id;
+              if (kMidTestConfiguration ==
+                  MidTestConfiguration::kMidNotNegotiated) {
+                // Munge away the extension.
+                header_extensions.erase(it);
+              }
+              break;
+            }
+          }
+          content_info.media_description()->set_rtp_header_extensions(
+              std::move(header_extensions));
+        }
+        ASSERT_TRUE(mid_header_extension_id.has_value());
+      },
       /* modify_sdp = */
       [&](SessionDescriptionInterface* offer) {
         first_ssrc = get_ssrc(offer, 0);
@@ -113,9 +190,40 @@ TEST(UnsignaledStreamTest, ReplacesUnsignaledStreamOnCompletedSignaling) {
             if (ByteReader<uint32_t>::ReadBigEndian(&(packet.cdata()[8])) ==
                     first_ssrc &&
                 !got_unsignaled_packet) {
-              rtc::CopyOnWriteBuffer updated_buffer = packet.data;
-              ByteWriter<uint32_t>::WriteBigEndian(
-                  updated_buffer.MutableData() + 8, second_ssrc);
+              // Parse packet and modify the SSRC to simulate a second m=
+              // section that has not been negotiated yet.
+              std::vector<RtpExtension> extensions;
+              extensions.emplace_back(RtpExtension::kMidUri,
+                                      mid_header_extension_id.value());
+              RtpHeaderExtensionMap extensions_map(extensions);
+              RtpPacket parsed_packet;
+              parsed_packet.IdentifyExtensions(extensions_map);
+              ASSERT_TRUE(parsed_packet.Parse(packet.data));
+              parsed_packet.SetSsrc(second_ssrc);
+              // The MID extension is present if and only if it was negotiated.
+              // If present, we either want to remove it or modify it depending
+              // on setup.
+              switch (kMidTestConfiguration) {
+                case MidTestConfiguration::kMidNotNegotiated:
+                  EXPECT_FALSE(parsed_packet.HasExtension<RtpMid>());
+                  break;
+                case MidTestConfiguration::kMidNegotiatedButMissingFromPackets:
+                  EXPECT_TRUE(parsed_packet.HasExtension<RtpMid>());
+                  ASSERT_TRUE(parsed_packet.RemoveExtension(RtpMid::kId));
+                  break;
+                case MidTestConfiguration::kMidNegotiatedAndPresentInPackets:
+                  EXPECT_TRUE(parsed_packet.HasExtension<RtpMid>());
+                  // The simulated second m= section would have a different MID.
+                  // If we don't modify it here then |second_ssrc| would end up
+                  // being mapped to the first m= section which would cause SSRC
+                  // conflicts if we later add the same SSRC to a second m=
+                  // section. Hidden assumption: first m= section does not use
+                  // MID:1.
+                  ASSERT_TRUE(parsed_packet.SetExtension<RtpMid>("1"));
+                  break;
+              }
+              // Inject the modified packet.
+              rtc::CopyOnWriteBuffer updated_buffer = parsed_packet.Buffer();
               EmulatedIpPacket updated_packet(
                   packet.from, packet.to, updated_buffer, packet.arrival_time);
               send_node->OnPacketReceived(std::move(updated_packet));
@@ -152,6 +260,14 @@ TEST(UnsignaledStreamTest, ReplacesUnsignaledStreamOnCompletedSignaling) {
   EXPECT_TRUE(s.WaitAndProcess(&offer_exchange_done));
   EXPECT_TRUE(s.WaitAndProcess(&second_sink.frame_observed_));
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    UnsignaledStreamTest,
+    ::testing::Values(MidTestConfiguration::kMidNotNegotiated,
+                      MidTestConfiguration::kMidNegotiatedButMissingFromPackets,
+                      MidTestConfiguration::kMidNegotiatedAndPresentInPackets),
+    TestParametersMidTestConfigurationToString);
 
 }  // namespace test
 }  // namespace webrtc
