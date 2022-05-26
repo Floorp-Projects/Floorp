4af362a7f6253a0e924202907ba4995169dd2680
Brad Werth <bwerth@mozilla.com>
2022-05-26 15:24:12 +0000

    Bug 1731136 Part 1: Extend compositor to manage backdrop surfaces. r=gw
    This is all scaffolding that will be used by later parts of this patch
    series.
    
    Differential Revision: https://phabricator.services.mozilla.com/D128130

--- gfx/webrender_bindings/RenderCompositor.h
+++ gfx/webrender_bindings/RenderCompositor.h
@@ -131,6 +131,8 @@ class RenderCompositor {
                              wr::DeviceIntPoint aVirtualOffset,
                              wr::DeviceIntSize aTileSize, bool aIsOpaque) {}
   virtual void CreateExternalSurface(wr::NativeSurfaceId aId, bool aIsOpaque) {}
+  virtual void CreateBackdropSurface(wr::NativeSurfaceId aId,
+                                     wr::ColorF aColor) {}
   virtual void DestroySurface(NativeSurfaceId aId) {}
   virtual void CreateTile(wr::NativeSurfaceId, int32_t aX, int32_t aY) {}
   virtual void DestroyTile(wr::NativeSurfaceId, int32_t aX, int32_t aY) {}
