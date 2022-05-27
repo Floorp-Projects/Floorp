9be466636823993339c4f519c76b96c4f2a7508b
James Teh <jteh@mozilla.com>
2022-05-27 01:22:07 +0000

    Bug 1771060: Make use of the line cursor in IsLocalAccAtLineStart to improve performance. r=jfkthame
    For block frames, nsLineIterator doesn't use the line cursor, so switch to nsBlockInFlowLineIterator.
    We don't actually care about the line number here anyway.
    
    Differential Revision: https://phabricator.services.mozilla.com/D147237

--- accessible/base/TextLeafRange.cpp
+++ accessible/base/TextLeafRange.cpp
@@ -17,6 +17,7 @@
 #include "mozilla/intl/WordBreaker.h"
 #include "mozilla/StaticPrefs_layout.h"
 #include "nsAccUtils.h"
+#include "nsBlockFrame.h"
 #include "nsContentUtils.h"
 #include "nsIAccessiblePivot.h"
 #include "nsILineIterator.h"
@@ -181,6 +182,22 @@ static bool IsLocalAccAtLineStart(LocalAccessible* aAcc) {
     // If the blocks are different, that means there's nothing before us on the
     // same line, so we're at the start.
     return true;
+  }
+  if (nsBlockFrame* block = do_QueryFrame(thisBlock)) {
+    // If we have a block frame, it's faster for us to use
+    // BlockInFlowLineIterator because it uses the line cursor.
+    bool found = false;
+    block->SetupLineCursorForQuery();
+    nsBlockInFlowLineIterator prevIt(block, prevLineFrame, &found);
+    if (!found) {
+      // Error; play it safe.
+      return true;
+    }
+    found = false;
+    nsBlockInFlowLineIterator thisIt(block, thisLineFrame, &found);
+    // if the lines are different, that means there's nothing before us on the
+    // same line, so we're at the start.
+    return !found || prevIt.GetLine() != thisIt.GetLine();
   }
   nsAutoLineIterator it = prevBlock->GetLineIterator();
   MOZ_ASSERT(it, "GetLineIterator impl in line-container blocks is infallible");
