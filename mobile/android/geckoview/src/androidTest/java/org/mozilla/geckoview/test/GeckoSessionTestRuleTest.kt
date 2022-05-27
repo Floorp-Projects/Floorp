65a2647e01b041bd5ed5028469696bec19d13ce6
Andreea Pavel <apavel@mozilla.com>
2022-05-27 04:32:40 +0000

    Bug 1605001 - disable evaluateJS_canTimeout on debug r?#intermittent-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D147459

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/GeckoSessionTestRuleTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/GeckoSessionTestRuleTest.kt
@@ -1533,6 +1533,9 @@ class GeckoSessionTestRuleTest : BaseSessionTest(noErrorCollector = true) {
     @TimeoutMillis(1000)
     @Test(expected = UiThreadUtils.TimeoutException::class)
     fun evaluateJS_canTimeout() {
+        // disable test on debug for frequently failing Bug 1605001
+        assumeThat(sessionRule.env.isDebugBuild, equalTo(false))
+
         mainSession.loadTestPath(HELLO_HTML_PATH)
         mainSession.waitForPageStop()
         mainSession.delegateUntilTestEnd(object : PromptDelegate {
