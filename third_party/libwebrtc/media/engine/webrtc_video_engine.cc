dc89f5d628c683a21b28aceb300dc115094b7976
Michael Froman <mfroman@mozilla.com>
2022-05-27 17:33:02 -0500

    Bug 1766646 - Vendor libwebrtc from 62ec0f6130
    Upstream commit: https://webrtc.googlesource.com/src/+/62ec0f6130856599d9da4b4ad9e99f8e38c93bb9
        Add small cooldown to unsignalled ssrc stream creation.
    
        This CL adds a cooldown of 0.5 seconds where if the WebRtcVideoChannel
        created an unsignalled receive stream within that amount of time, if we
        receive even more unknown ssrcs we simply drop those RTP packets.
    
        This prevents getting into a state of spawning new decoders on every
        single packet which could happen e.g. if PT based demuxing is enabled
        and MIDs are missing from the packets.
    
        Bug: webrtc:12815
        Change-Id: Id7675fb0cbfbc72281dcfe030d1a35629df3eb9f
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/221520
        Reviewed-by: Taylor Brandstetter <deadbeef@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Henrik Boström <hbos@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34263}

--- third_party/libwebrtc/media/engine/webrtc_video_engine.cc
+++ third_party/libwebrtc/media/engine/webrtc_video_engine.cc
@@ -47,6 +47,7 @@ namespace cricket {
 namespace {
 
 const int kMinLayerSize = 16;
+constexpr int64_t kUnsignaledSsrcCooldownMs = rtc::kNumMillisecsPerSec / 2;
 
 const char* StreamTypeToString(
     webrtc::VideoSendStream::StreamStats::StreamType type) {
@@ -1565,6 +1566,7 @@ void WebRtcVideoChannel::ResetUnsignaledRecvStream() {
   RTC_DCHECK_RUN_ON(&thread_checker_);
   RTC_LOG(LS_INFO) << "ResetUnsignaledRecvStream.";
   unsignaled_stream_params_ = StreamParams();
+  last_unsignalled_ssrc_creation_time_ms_ = absl::nullopt;
 
   // Delete any created default streams. This is needed to avoid SSRC collisions
   // in Call's RtpDemuxer, in the case that |this| has created a default video
@@ -1767,7 +1769,23 @@ void WebRtcVideoChannel::OnPacketReceived(rtc::CopyOnWriteBuffer packet,
         if (demuxer_criteria_id_ != demuxer_criteria_completed_id_) {
           return;
         }
-
+        // Ignore unknown ssrcs if we recently created an unsignalled receive
+        // stream since this shouldn't happen frequently. Getting into a state
+        // of creating decoders on every packet eats up processing time (e.g.
+        // https://crbug.com/1069603) and this cooldown prevents that.
+        if (last_unsignalled_ssrc_creation_time_ms_.has_value()) {
+          int64_t now_ms = rtc::TimeMillis();
+          if (now_ms - last_unsignalled_ssrc_creation_time_ms_.value() <
+              kUnsignaledSsrcCooldownMs) {
+            // We've already created an unsignalled ssrc stream within the last
+            // 0.5 s, ignore with a warning.
+            RTC_LOG(LS_WARNING)
+                << "Another unsignalled ssrc packet arrived shortly after the "
+                << "creation of an unsignalled ssrc stream. Dropping packet.";
+            return;
+          }
+        }
+        // Let the unsignalled ssrc handler decide whether to drop or deliver.
         switch (unsignalled_ssrc_handler_->OnUnsignalledSsrc(this, ssrc)) {
           case UnsignalledSsrcHandler::kDropPacket:
             return;
@@ -1780,6 +1798,7 @@ void WebRtcVideoChannel::OnPacketReceived(rtc::CopyOnWriteBuffer packet,
             webrtc::PacketReceiver::DELIVERY_OK) {
           RTC_LOG(LS_WARNING) << "Failed to deliver RTP packet on re-delivery.";
         }
+        last_unsignalled_ssrc_creation_time_ms_ = rtc::TimeMillis();
       }));
 }
 
