58dce847c1908d0b0b20ad4d4dc8e9af73803505
Michael Froman <mfroman@mozilla.com>
2022-05-27 23:00:20 -0500

    Bug 1766646 - Vendor libwebrtc from a3796c8090
    Upstream commit: https://webrtc.googlesource.com/src/+/a3796c8090a524846d54cd6dd248200a42ac502c
        Revert the send-side bwe behavior to slow ramp-up on lifted REMB cap.
    
        The behavior was changed on https://webrtc-review.googlesource.com/c/src/+/219696. The revert is due to unknown implications for a downstream project. As REMB caps are not used with send-side bandwidth estimation it should be a noop.
    
        Bug: webrtc:12306
        Change-Id: Idecc49fda007f72512a8fc1e35d62e673b00df3d
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/222607
        Reviewed-by: Magnus Flodman <mflodman@webrtc.org>
        Commit-Queue: Christoffer Rodbro <crodbro@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34313}

--- third_party/libwebrtc/modules/congestion_controller/goog_cc/send_side_bandwidth_estimation.cc
+++ third_party/libwebrtc/modules/congestion_controller/goog_cc/send_side_bandwidth_estimation.cc
@@ -227,7 +227,7 @@ SendSideBandwidthEstimation::SendSideBandwidthEstimation(
       high_loss_threshold_(kDefaultHighLossThreshold),
       bitrate_threshold_(kDefaultBitrateThreshold),
       loss_based_bandwidth_estimation_(key_value_config),
-      disable_receiver_limit_caps_only_("Disabled") {
+      receiver_limit_caps_only_("Enabled") {
   RTC_DCHECK(event_log);
   if (BweLossExperimentIsEnabled()) {
     uint32_t bitrate_threshold_kbps;
@@ -240,7 +240,7 @@ SendSideBandwidthEstimation::SendSideBandwidthEstimation(
       bitrate_threshold_ = DataRate::KilobitsPerSec(bitrate_threshold_kbps);
     }
   }
-  ParseFieldTrial({&disable_receiver_limit_caps_only_},
+  ParseFieldTrial({&receiver_limit_caps_only_},
                   key_value_config->Lookup("WebRTC-Bwe-ReceiverLimitCapsOnly"));
 }
 
@@ -311,7 +311,7 @@ int SendSideBandwidthEstimation::GetMinBitrate() const {
 
 DataRate SendSideBandwidthEstimation::target_rate() const {
   DataRate target = current_target_;
-  if (!disable_receiver_limit_caps_only_)
+  if (receiver_limit_caps_only_)
     target = std::min(target, receiver_limit_);
   return std::max(min_bitrate_configured_, target);
 }
@@ -588,7 +588,7 @@ void SendSideBandwidthEstimation::UpdateMinHistory(Timestamp at_time) {
 
 DataRate SendSideBandwidthEstimation::GetUpperLimit() const {
   DataRate upper_limit = delay_based_limit_;
-  if (disable_receiver_limit_caps_only_)
+  if (!receiver_limit_caps_only_)
     upper_limit = std::min(upper_limit, receiver_limit_);
   return std::min(upper_limit, max_bitrate_configured_);
 }
