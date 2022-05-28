16d0222cab80a68d735351db194771f292f63868
Michael Froman <mfroman@mozilla.com>
2022-05-27 22:11:34 -0500

    Bug 1766646 - Vendor libwebrtc from 7a2db8acbe
    Upstream commit: https://webrtc.googlesource.com/src/+/7a2db8acbeec1a1dcb79873d223c1f6a892ef758
        Modify Bundle logic to not add & destroy extra transport at add-track
    
        If a bundle is established, then per JSEP, the offerer is required to
        include the new track in the bundle, and per BUNDLE, the answerer has
        to either accept the track as part of the bundle or reject the track;
        it cannot move it out of the group. So we will never need the transport.
    
        Bug: webrtc:12837
        Change-Id: I41cae74605fecf454900a958776b95607ccf3745
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/221601
        Reviewed-by: Henrik Boström <hbos@webrtc.org>
        Commit-Queue: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34290}

--- third_party/libwebrtc/pc/session_description.h
+++ third_party/libwebrtc/pc/session_description.h
@@ -488,6 +488,8 @@ class ContentGroup {
   bool HasContentName(const std::string& content_name) const;
   void AddContentName(const std::string& content_name);
   bool RemoveContentName(const std::string& content_name);
+  // for debugging
+  std::string ToString() const;
 
  private:
   std::string semantics_;
