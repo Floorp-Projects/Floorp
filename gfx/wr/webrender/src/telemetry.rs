d636a90843a83399758af92374334e40c70c4925
Bas Schouten <bschouten@mozilla.com>
2022-05-30 14:49:18 +0000

    Bug 1770974: Collect telemetry on blob rasterization times. r=nical
    Depends on D147224
    
    Differential Revision: https://phabricator.services.mozilla.com/D147226

--- gfx/wr/webrender/src/telemetry.rs
+++ gfx/wr/webrender/src/telemetry.rs
@@ -12,6 +12,7 @@ pub struct Telemetry;
 /// Defines the interface for hooking up an external telemetry reporter to WR.
 #[cfg(not(feature = "gecko"))]
 impl Telemetry {
+    pub fn record_rasterize_blobs_time(_duration: Duration) { }
     // Start rasterize glyph time collection
     pub fn start_rasterize_glyphs_time() -> TimerId { return 0; }
     // End rasterize glyph time collection
@@ -26,6 +27,7 @@ impl Telemetry {
 
 #[cfg(feature = "gecko")]
 impl Telemetry {
+    pub fn record_rasterize_blobs_time(duration: Duration) { wr::rasterize_blobs_time.accumulate_raw_duration(duration); }
     pub fn start_rasterize_glyphs_time() -> TimerId { wr::rasterize_glyphs_time.start() }
     pub fn stop_and_accumulate_rasterize_glyphs_time(id: TimerId) { wr::rasterize_glyphs_time.stop_and_accumulate(id); }
     pub fn start_framebuild_time() -> TimerId { wr::framebuild_time.start() }
