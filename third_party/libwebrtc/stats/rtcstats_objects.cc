f0e0dc80bb104ec14cca4165d9806c213c8df544
Michael Froman <mfroman@mozilla.com>
2022-05-30 19:14:41 -0500

    Bug 1766646 - Vendor libwebrtc from fe6580fb87
    Upstream commit: https://webrtc.googlesource.com/src/+/fe6580fb873a40fc2fe028288eff72bf4bef1eef
        Revert "Fix echo return loss stats and add to RTCAudioSourceStats."
    
        This reverts commit a27cfbffdfa0bf359628d2164db5b9d6321f9c9c.
    
        Reason for revert: WebRtcBrowserTest.RunsAudioVideoWebRTCCallInTwoTabsGetStatsPromise failing.
    
        Original change's description:
        > Fix echo return loss stats and add to RTCAudioSourceStats.
        >
        > This solves two problems:
        > * Echo return loss stats weren't being gathered in Chrome, because they
        >   need to be taken from the audio processor attached to the track
        >   rather than the audio send stream.
        > * The standardized location is in RTCAudioSourceStats, not
        >   RTCMediaStreamTrackStats. For now, will populate the stats in both
        >   locations.
        >
        > Bug: webrtc:12770
        > Change-Id: I47eaf7f2b50b914a1be84156aa831e27497d07e3
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/223182
        > Commit-Queue: Taylor Brandstetter <deadbeef@webrtc.org>
        > Reviewed-by: Henrik Boström <hbos@webrtc.org>
        > Cr-Commit-Position: refs/heads/master@{#34344}
    
        TBR=deadbeef@webrtc.org,hbos@webrtc.org,hbos@chromium.org,webrtc-scoped@luci-project-accounts.iam.gserviceaccount.com
    
        Change-Id: I6b2587d762f005adef67c0d5121f1b58c3b76688
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Bug: webrtc:12770
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/223068
        Reviewed-by: Evan Shrubsole <eshr@google.com>
        Reviewed-by: Henrik Boström <hbos@webrtc.org>
        Commit-Queue: Evan Shrubsole <eshr@google.com>
        Cr-Commit-Position: refs/heads/master@{#34352}

--- third_party/libwebrtc/stats/rtcstats_objects.cc
+++ third_party/libwebrtc/stats/rtcstats_objects.cc
@@ -987,9 +987,7 @@ RTCMediaSourceStats::~RTCMediaSourceStats() {}
 WEBRTC_RTCSTATS_IMPL(RTCAudioSourceStats, RTCMediaSourceStats, "media-source",
     &audio_level,
     &total_audio_energy,
-    &total_samples_duration,
-    &echo_return_loss,
-    &echo_return_loss_enhancement)
+    &total_samples_duration)
 // clang-format on
 
 RTCAudioSourceStats::RTCAudioSourceStats(const std::string& id,
@@ -1000,17 +998,13 @@ RTCAudioSourceStats::RTCAudioSourceStats(std::string&& id, int64_t timestamp_us)
     : RTCMediaSourceStats(std::move(id), timestamp_us),
       audio_level("audioLevel"),
       total_audio_energy("totalAudioEnergy"),
-      total_samples_duration("totalSamplesDuration"),
-      echo_return_loss("echoReturnLoss"),
-      echo_return_loss_enhancement("echoReturnLossEnhancement") {}
+      total_samples_duration("totalSamplesDuration") {}
 
 RTCAudioSourceStats::RTCAudioSourceStats(const RTCAudioSourceStats& other)
     : RTCMediaSourceStats(other),
       audio_level(other.audio_level),
       total_audio_energy(other.total_audio_energy),
-      total_samples_duration(other.total_samples_duration),
-      echo_return_loss(other.echo_return_loss),
-      echo_return_loss_enhancement(other.echo_return_loss_enhancement) {}
+      total_samples_duration(other.total_samples_duration) {}
 
 RTCAudioSourceStats::~RTCAudioSourceStats() {}
 
