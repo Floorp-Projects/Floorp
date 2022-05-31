c419d1fdfaf8a9ffa358b7b76930b14b1422d061
Emilio Cobos Álvarez <emilio@crisal.io>
2022-05-31 12:45:37 +0000

    Bug 1763035 - Don't inherit direction in cached scrollbar parts. r=jfkthame, a=RyanVM
    This is the most minimal fix for the bug. The issue is basically that
    XUL <tree>s have a non-anonymous scrollbar that doesn't match the rule
    above the one I'm modifying (which resets scrollbar directionality to
    ltr).
    
    Its _anonymous_ kids would inherit this rtl directionality and cache it,
    and the wrong directionality would be used for actual anonymous
    scrollbars, causing havoc later on due to the nsGfxScrollFrame reflow
    callbacks.
    
    This is wrong, anyways, there's no reason for inheriting the
    directionality of the scrollbar to begin with.
    
    Differential Revision: https://phabricator.services.mozilla.com/D147687

---
+++ browser/base/content/test/pageinfo/browser_pageinfo_rtl.js
@@ -0,0 +1,27 @@
+async function testPageInfo() {
+  await BrowserTestUtils.withNewTab("https://example.com", async function(
+    browser
+  ) {
+    let pageInfo = BrowserPageInfo();
+    await BrowserTestUtils.waitForEvent(pageInfo, "page-info-init");
+    is(
+      getComputedStyle(pageInfo.document.documentElement).direction,
+      "rtl",
+      "Should be RTL"
+    );
+    ok(true, "Didn't assert or crash");
+    pageInfo.close();
+  });
+}
+
+add_task(async function test_page_info_rtl() {
+  await SpecialPowers.pushPrefEnv({ set: [["intl.l10n.pseudo", "bidi"]] });
+
+  for (let useOverlayScrollbars of [0, 1]) {
+    info("Testing with overlay scrollbars: " + useOverlayScrollbars);
+    await SpecialPowers.pushPrefEnv({
+      set: [["ui.useOverlayScrollbars", useOverlayScrollbars]],
+    });
+    await testPageInfo();
+  }
+});
