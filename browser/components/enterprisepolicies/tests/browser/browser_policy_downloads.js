59aa2d17cc0ca0fccab819c72462be076a05b2fb
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2022-05-31 23:47:39 +0000

    Bug 1772001 - fix enterprise policy for using the temp folder for downloads, r=mkaply a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D147807

--- browser/components/enterprisepolicies/tests/browser/browser_policy_downloads.js
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_downloads.js
@@ -3,6 +3,8 @@
 
 "use strict";
 
+const UCT_URI = "chrome://mozapps/content/downloads/unknownContentType.xhtml";
+
 add_task(async function test_defaultdownload() {
   await setupPolicyEngineWithJson({
     policies: {
@@ -76,4 +78,71 @@ add_task(async function test_download() {
       "browser.download.useDownloadDir should be locked."
     );
   });
+});
+
+async function setDownloadDir() {
+  let tmpDir = PathUtils.join(
+    PathUtils.tempDir,
+    "testsavedir" + Math.floor(Math.random() * 2 ** 32)
+  );
+  // Create this dir if it doesn't exist (ignores existing dirs)
+  await IOUtils.makeDirectory(tmpDir);
+  registerCleanupFunction(async function() {
+    try {
+      await IOUtils.remove(tmpDir, { recursive: true });
+    } catch (e) {
+      Cu.reportError(e);
+    }
+  });
+  Services.prefs.setIntPref("browser.download.folderList", 2);
+  Services.prefs.setCharPref("browser.download.dir", tmpDir);
+  return tmpDir;
+}
+
+add_task(async function test_tmpdir_download() {
+  await setupPolicyEngineWithJson({
+    policies: {
+      StartDownloadsInTempDirectory: true,
+    },
+  });
+
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["browser.download.always_ask_before_handling_new_types", true],
+      ["browser.helperApps.deleteTempFileOnExit", true],
+    ],
+  });
+
+  let dlDir = new FileUtils.File(await setDownloadDir());
+  registerCleanupFunction(() => {
+    Services.prefs.clearUserPref("browser.download.dir");
+    Services.prefs.clearUserPref("browser.download.folderList");
+  });
+
+  // Wait for the download prompting dialog
+  let dialogPromise = BrowserTestUtils.domWindowOpenedAndLoaded(
+    null,
+    win => win.document.documentURI == UCT_URI
+  );
+
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url:
+        "https://example.com/browser/browser/components/downloads/test/browser/foo.txt",
+      waitForLoad: false,
+      waitForStop: true,
+    },
+    async function() {
+      let dialogWin = await dialogPromise;
+      let tempFile = dialogWin.dialog.mLauncher.targetFile;
+      isnot(
+        tempFile.parent.path,
+        dlDir.path,
+        "Should not have put temp file in the downloads dir."
+      );
+
+      dialogWin.close();
+    }
+  );
 });
