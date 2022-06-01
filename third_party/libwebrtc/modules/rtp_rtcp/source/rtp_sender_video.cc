c3d5b79a759807087ef9be8fb7156b459fc56929
Michael Froman <mfroman@mozilla.com>
2022-06-01 09:13:33 -0500

    Bug 1766646 - Vendor libwebrtc from 10ed32c114
    Upstream commit: https://webrtc.googlesource.com/src/+/10ed32c114fd24018790e92dfdf30932909a80a2
        do not require generic frame descriptor extension for FrameEncryptor
    
        as there are encryption schemes that preserve the payload structure
        well enough and do not require those extensions.
        This improves consistency as the webrtc-encoded-transform API
        (which does not use this synchronous codepath) does not require those
        header extensions either.
    
    
        BUG=webrtc:12995
    
        Change-Id: If237ca5d92e8871ac71c3d48fdd05127206395e6
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/226741
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Commit-Queue: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34537}

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_sender_video.cc
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_sender_video.cc
@@ -581,10 +581,6 @@ bool RTPSenderVideo::SendVideo(
   // TODO(benwright@webrtc.org) - Allocate enough to always encrypt inline.
   rtc::Buffer encrypted_video_payload;
   if (frame_encryptor_ != nullptr) {
-    if (!has_generic_descriptor) {
-      return false;
-    }
-
     const size_t max_ciphertext_size =
         frame_encryptor_->GetMaxCiphertextByteSize(cricket::MEDIA_TYPE_VIDEO,
                                                    payload.size());
