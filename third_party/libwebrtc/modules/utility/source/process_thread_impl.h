c2457bbd4bc6e8e9dd936e0bfd8c31a7e1cbc619
Michael Froman <mfroman@mozilla.com>
2022-06-01 13:34:48 -0500

    Bug 1766646 - Vendor libwebrtc from bbdecaff89
    Upstream commit: https://webrtc.googlesource.com/src/+/bbdecaff89c0ac532fbd913f60b16ba441a1c9a2
        Use backticks not vertical bars to denote variables in comments for /modules/utility
    
        Bug: webrtc:12338
        Change-Id: I8900efa25a5eebcf57ec894108e1212e1d5b8089
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/227098
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Artem Titov <titovartem@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34675}

--- third_party/libwebrtc/modules/utility/source/process_thread_impl.h
+++ third_party/libwebrtc/modules/utility/source/process_thread_impl.h
@@ -73,11 +73,11 @@ class ProcessThreadImpl : public ProcessThread {
     }
 
     int64_t run_at_ms;
-    // DelayedTask owns the |task|, but some delayed tasks must be removed from
+    // DelayedTask owns the `task`, but some delayed tasks must be removed from
     // the std::priority_queue, but mustn't be deleted. std::priority_queue does
     // not give non-const access to the values, so storing unique_ptr would
     // delete the task as soon as it is remove from the priority queue.
-    // Thus lifetime of the |task| is managed manually.
+    // Thus lifetime of the `task` is managed manually.
     QueuedTask* task;
   };
   typedef std::list<ModuleCallback> ModuleList;
