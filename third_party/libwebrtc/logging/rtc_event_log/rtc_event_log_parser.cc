70ebf2fb91088369eda87e542ec3de01f525e53e
Michael Froman <mfroman@mozilla.com>
2022-06-01 10:33:01 -0500

    Bug 1766646 - Vendor libwebrtc from 481698e6f5
    Upstream commit: https://webrtc.googlesource.com/src/+/481698e6f56f945c566bb7222ebc77c681bc040b
        Remove unnecessary conversions.
    
        `receive_timestamp` is already a `Timestamp`.
    
        Bug: None
        Change-Id: I334c6d591915115f3a577ac14183fc122517c313
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/227080
        Reviewed-by: Björn Terelius <terelius@webrtc.org>
        Commit-Queue: Fanny Linderborg <linderborg@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34582}

--- third_party/libwebrtc/logging/rtc_event_log/rtc_event_log_parser.cc
+++ third_party/libwebrtc/logging/rtc_event_log/rtc_event_log_parser.cc
@@ -2166,8 +2166,7 @@ std::vector<LoggedPacketInfo> ParsedRtcEventLog::GetPacketInfos(
           if (packet.received()) {
             receive_timestamp += TimeDelta::Micros(packet.delta_us());
             if (sent->reported_recv_time.IsInfinite()) {
-              sent->reported_recv_time =
-                  Timestamp::Millis(receive_timestamp.ms());
+              sent->reported_recv_time = receive_timestamp;
               sent->log_feedback_time = log_feedback_time;
             }
           } else {
