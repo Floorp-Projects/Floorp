a33e4df1bd529346737e891f6e2066f4d6aa09a8
Emilio Cobos Álvarez <emilio@crisal.io>
2022-06-01 20:56:39 +0000

    Bug 1772168 - Really ensure screenshot iframes are transparent. r=sfoster  a=pascalc
    Bug 1771487 fixed pages that followed the preferred color-scheme (e.g.,
    bugzilla or so).
    
    For pages that are forced-light, if you have a dark theme, the iframe
    would get a dark preferred color-scheme, which causes the same issue we
    were avoiding in the other case.
    
    Instead, make sure the screenshots frames don't have any color-scheme
    set (so they default to light), and explicitly set the color-scheme of
    the iframe so that it matches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D148039

--- browser/extensions/screenshots/selector/ui.js
+++ browser/extensions/screenshots/selector/ui.js
@@ -68,6 +68,8 @@ this.ui = (function() {
     el.style.margin = "0";
     el.scrolling = "no";
     el.style.clip = "auto";
+    el.style.backgroundColor = "transparent";
+    el.style.colorScheme = "light";
     return el;
   }
 
@@ -89,7 +91,6 @@ this.ui = (function() {
           this.element.id = "firefox-screenshots-selection-iframe";
           this.element.style.display = "none";
           this.element.style.setProperty("position", "absolute", "important");
-          this.element.style.setProperty("background-color", "transparent");
           this.element.setAttribute("role", "dialog");
           this.updateElementSize();
           this.element.addEventListener(
@@ -101,7 +102,6 @@ this.ui = (function() {
               // eslint-disable-next-line no-unsanitized/property
               this.document.documentElement.innerHTML = `
                <head>
-                <meta name="color-scheme" content="light dark">
                 <style>${substitutedCss}</style>
                 <title></title>
                </head>
@@ -242,7 +242,6 @@ this.ui = (function() {
           this.element = initializeIframe();
           this.element.id = "firefox-screenshots-preselection-iframe";
           this.element.style.setProperty("position", "fixed", "important");
-          this.element.style.setProperty("background-color", "transparent");
           this.element.style.width = "100%";
           this.element.style.height = "100%";
           this.element.setAttribute("role", "dialog");
@@ -256,7 +255,6 @@ this.ui = (function() {
               this.document.documentElement.innerHTML = `
                <head>
                 <link rel="localization" href="browser/screenshots.ftl">
-                <meta name="color-scheme" content="light dark">
                 <style>${substitutedCss}</style>
                 <title></title>
                </head>
@@ -386,7 +384,6 @@ this.ui = (function() {
           this.element.id = "firefox-screenshots-preview-iframe";
           this.element.style.display = "none";
           this.element.style.setProperty("position", "fixed", "important");
-          this.element.style.setProperty("background-color", "transparent");
           this.element.style.height = "100%";
           this.element.style.width = "100%";
           this.element.setAttribute("role", "dialog");
@@ -399,7 +396,6 @@ this.ui = (function() {
               this.document.documentElement.innerHTML = `
                 <head>
                   <link rel="localization" href="browser/screenshots.ftl">
-                  <meta name="color-scheme" content="light dark">
                   <style>${substitutedCss}</style>
                   <title></title>
                 </head>
