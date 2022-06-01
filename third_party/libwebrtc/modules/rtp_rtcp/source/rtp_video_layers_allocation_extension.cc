223c4206df027b9788622adc4fefcc56d85eb2d3
Michael Froman <mfroman@mozilla.com>
2022-05-31 22:48:25 -0500

    Bug 1766646 - Vendor libwebrtc from 6882a3f7d0
    Upstream commit: https://webrtc.googlesource.com/src/+/6882a3f7d04865975f4c5eeee76fe324f3141521
        Discard over large DataRates in VideoLayersAllocation rtp header extension
    
        Bug: b/193170077
        Change-Id: I427718daa70910dbaf7f2e1f3d88d3dce4f27c7a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/226561
        Reviewed-by: Emil Lundmark <lndmrk@webrtc.org>
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34520}

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_video_layers_allocation_extension.cc
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_video_layers_allocation_extension.cc
@@ -354,10 +354,13 @@ bool RtpVideoLayersAllocationExtension::Parse(
   // Target bitrates.
   for (auto& layer : allocation->active_spatial_layers) {
     for (DataRate& rate : layer.target_bitrate_per_temporal_layer) {
-      rate = DataRate::KilobitsPerSec(ReadLeb128(read_at, end));
-      if (read_at == nullptr) {
+      uint64_t bitrate_kbps = ReadLeb128(read_at, end);
+      // bitrate_kbps might represent larger values than DataRate type,
+      // discard unreasonably large values.
+      if (read_at == nullptr || bitrate_kbps > 1'000'000) {
         return false;
       }
+      rate = DataRate::KilobitsPerSec(bitrate_kbps);
     }
   }
 
