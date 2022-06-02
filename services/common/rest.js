32b67c9e27876766c38381d1a75b362c9694da54
Tooru Fujisawa <arai_a@mac.com>
2022-06-02 12:27:42 +0000

    Bug 1772097 - Part 2: Use plain object for lazy getter in services/common/. r=markh
    Differential Revision: https://phabricator.services.mozilla.com/D147917

--- services/common/rest.js
+++ services/common/rest.js
@@ -18,8 +18,10 @@ const { CommonUtils } = ChromeUtils.import(
   "resource://services-common/utils.js"
 );
 
+const lazy = {};
+
 ChromeUtils.defineModuleGetter(
-  this,
+  lazy,
   "CryptoUtils",
   "resource://services-crypto/utils.js"
 );
@@ -710,7 +712,7 @@ TokenAuthenticatedRESTRequest.prototype = {
   __proto__: RESTRequest.prototype,
 
   async dispatch(method, data) {
-    let sig = await CryptoUtils.computeHTTPMACSHA1(
+    let sig = await lazy.CryptoUtils.computeHTTPMACSHA1(
       this.authToken.id,
       this.authToken.key,
       method,
