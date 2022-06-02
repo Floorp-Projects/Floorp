a9194ca94828159c0bce523fa79a5a718b9a972f
Mike Hommey <mh+mozilla@glandium.org>
2022-06-02 06:54:25 +0000

    Bug 1772035 - Add cargo-vet toolchains. r=firefox-build-system-reviewers,andi
    We won't need all of them immediately, but while we're here, we might as
    well produce them all.
    
    Differential Revision: https://phabricator.services.mozilla.com/D147820

---
+++ taskcluster/scripts/misc/build-cargo-vet.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -x -e -v
+
+PROJECT=cargo-vet
+
+# Needed by osx-cross-linker.
+export TARGET=$1
+
+case "$TARGET" in
+x86_64-unknown-linux-gnu)
+    # Native Linux Build
+    EXE=
+    FEATURES="--features reqwest/native-tls-vendored"
+    export RUSTFLAGS="-Clinker=$MOZ_FETCHES_DIR/clang/bin/clang++ -C link-arg=--sysroot=$MOZ_FETCHES_DIR/sysroot-x86_64-linux-gnu -C link-arg=-fuse-ld=lld"
+    export CC=$MOZ_FETCHES_DIR/clang/bin/clang
+    export CFLAGS="--sysroot=$MOZ_FETCHES_DIR/sysroot-x86_64-linux-gnu"
+    ;;
+*-apple-darwin)
+    # Cross-compiling for Mac on Linux.
+    EXE=
+    FEATURES=
+    export PATH="$MOZ_FETCHES_DIR/clang/bin:$PATH"
+    export PATH="$MOZ_FETCHES_DIR/cctools/bin:$PATH"
+    export RUSTFLAGS="-C linker=$GECKO_PATH/taskcluster/scripts/misc/osx-cross-linker"
+    if test "$TARGET" = "aarch64-apple-darwin"; then
+        export MACOSX_DEPLOYMENT_TARGET=11.0
+    else
+        export MACOSX_DEPLOYMENT_TARGET=10.12
+    fi
+    export CC="$MOZ_FETCHES_DIR/clang/bin/clang"
+    export TARGET_CC="$CC -isysroot $MOZ_FETCHES_DIR/MacOSX11.0.sdk"
+    ;;
+x86_64-pc-windows-msvc)
+    # Cross-compiling for Windows on Linux.
+    EXE=.exe
+    FEATURES=
+    export CC=$MOZ_FETCHES_DIR/clang/bin/clang-cl
+    export TARGET_AR=$MOZ_FETCHES_DIR/clang/bin/llvm-lib
+
+    . $GECKO_PATH/taskcluster/scripts/misc/vs-setup.sh
+    export CARGO_TARGET_X86_64_PC_WINDOWS_MSVC_LINKER=$MOZ_FETCHES_DIR/clang/bin/lld-link
+    ;;
+esac
+
+PATH="$MOZ_FETCHES_DIR/rustc/bin:$PATH"
+
+cd $MOZ_FETCHES_DIR/$PROJECT
+
+cargo build --verbose --release --target "$TARGET" --bin $PROJECT $FEATURES
+
+mkdir ${PROJECT}
+cp target/$TARGET/release/${PROJECT}${EXE} ${PROJECT}/${PROJECT}${EXE}
+tar -acf ${PROJECT}.tar.zst ${PROJECT}
+mkdir -p $UPLOAD_DIR
+cp ${PROJECT}.tar.zst $UPLOAD_DIR
