f3e1d38e9dd45697ec00492755c8c1f9c1a17611
Michael Froman <mfroman@mozilla.com>
2022-06-02 16:27:26 -0500

    Bug 1766646 - Vendor libwebrtc from 3cd7a0ffddc
    Upstream commit: https://webrtc.googlesource.com/src/+/3cd7a0ffddc4cbc706bd0f5fbefb7b044e05f448
        Remove media/base/h264_profile_level_id.* and media/base/vp9_profile.h
    
        The content of these files was moved to api/video_codecs in
        https://webrtc.googlesource.com/src.git/+/c3fcee7c3a7714afc3e37d4753b40f4fdbc3653e
        but the original files could not be removed due to dependencies
        in downstream projects.
    
        Bug: chromium:1187565
        Change-Id: I414efa22102bfdea0765fa72a8cf8b0bd5c090db
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/229303
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Johannes Kron <kron@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#34869}

--- third_party/libwebrtc/modules/video_coding/codecs/h264/include/h264.h
+++ third_party/libwebrtc/modules/video_coding/codecs/h264/include/h264.h
@@ -16,8 +16,8 @@
 #include <string>
 #include <vector>
 
+#include "api/video_codecs/h264_profile_level_id.h"
 #include "media/base/codec.h"
-#include "media/base/h264_profile_level_id.h"
 #include "modules/video_coding/include/video_codec_interface.h"
 #include "rtc_base/system/rtc_export.h"
 
