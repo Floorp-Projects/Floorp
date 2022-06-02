5c91deebaee1161ec21004ff568f0a08eb4f77c8
Michael Froman <mfroman@mozilla.com>
2022-06-02 11:49:29 -0500

    Bug 1766646 - Vendor libwebrtc from ca0a08ab600
    Upstream commit: https://webrtc.googlesource.com/src/+/ca0a08ab600c8d7d00b94492122946ad837b1ef7
        Update remaining usage of VideoDecoder::InitDecode to Configure
    
        Bug: webrtc:13045
        Change-Id: I5253fddfd613cf0228fc3cd861b91e56558dd34a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/228947
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34777}

--- third_party/libwebrtc/test/testsupport/ivf_video_frame_generator.cc
+++ third_party/libwebrtc/test/testsupport/ivf_video_frame_generator.cc
@@ -38,19 +38,17 @@ IvfVideoFrameGenerator::IvfVideoFrameGenerator(const std::string& file_name)
       width_(file_reader_->GetFrameWidth()),
       height_(file_reader_->GetFrameHeight()) {
   RTC_CHECK(video_decoder_) << "No decoder found for file's video codec type";
-  VideoCodec codec_settings;
-  codec_settings.codecType = file_reader_->GetVideoCodecType();
-  codec_settings.width = file_reader_->GetFrameWidth();
-  codec_settings.height = file_reader_->GetFrameHeight();
+  VideoDecoder::Settings decoder_settings;
+  decoder_settings.set_codec_type(file_reader_->GetVideoCodecType());
+  decoder_settings.set_max_render_resolution(
+      {file_reader_->GetFrameWidth(), file_reader_->GetFrameHeight()});
   // Set buffer pool size to max value to ensure that if users of generator,
   // ex. test frameworks, will retain frames for quite a long time, decoder
   // won't crash with buffers pool overflow error.
-  codec_settings.buffer_pool_size = std::numeric_limits<int>::max();
+  decoder_settings.set_buffer_pool_size(std::numeric_limits<int>::max());
   RTC_CHECK_EQ(video_decoder_->RegisterDecodeCompleteCallback(&callback_),
                WEBRTC_VIDEO_CODEC_OK);
-  RTC_CHECK_EQ(
-      video_decoder_->InitDecode(&codec_settings, /*number_of_cores=*/1),
-      WEBRTC_VIDEO_CODEC_OK);
+  RTC_CHECK(video_decoder_->Configure(decoder_settings));
 }
 IvfVideoFrameGenerator::~IvfVideoFrameGenerator() {
   MutexLock lock(&lock_);
