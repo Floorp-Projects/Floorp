f80258595f1284c8f6508c8cd6db3c54d30a827e
Michael Froman <mfroman@mozilla.com>
2022-06-02 11:49:29 -0500

    Bug 1766646 - Vendor libwebrtc from ca0a08ab600
    Upstream commit: https://webrtc.googlesource.com/src/+/ca0a08ab600c8d7d00b94492122946ad837b1ef7
        Update remaining usage of VideoDecoder::InitDecode to Configure
    
        Bug: webrtc:13045
        Change-Id: I5253fddfd613cf0228fc3cd861b91e56558dd34a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/228947
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34777}

--- third_party/libwebrtc/video/video_receive_stream2.cc
+++ third_party/libwebrtc/video/video_receive_stream2.cc
@@ -130,10 +130,9 @@ RenderResolution InitialDecoderResolution() {
 // but logs messages to LS_ERROR.
 class NullVideoDecoder : public webrtc::VideoDecoder {
  public:
-  int32_t InitDecode(const webrtc::VideoCodec* codec_settings,
-                     int32_t number_of_cores) override {
+  bool Configure(const Settings& settings) override {
     RTC_LOG(LS_ERROR) << "Can't initialize NullVideoDecoder.";
-    return WEBRTC_VIDEO_CODEC_OK;
+    return true;
   }
 
   int32_t Decode(const webrtc::EncodedImage& input_image,
