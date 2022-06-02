052a67606b69e17c39f60a470b4fd289d1e8f4e1
ahochheiden <ahochheiden@mozilla.com>
2022-06-02 06:27:44 +0000

    Bug 1769631 - Remove 'U' from 'mode' parameters for various 'open' calls to ensure Python3.11 compatibility r=firefox-build-system-reviewers,glandium a=RyanVM
    The 'U' flag represents "universal newline". It has been deprecated
    since Python3.3. Since then "universal newline" is the default when a
    file is opened in text mode (not bytes). In Python3.11 using the 'U'
    flag throws errors. There should be no harm in removing 'U' from 'open'
    everywhere it is used, and doing allows the use of Python3.11.
    
    For more reading see: https://docs.python.org/3.11/whatsnew/3.11.html#changes-in-the-python-api
    
    Differential Revision: https://phabricator.services.mozilla.com/D147721

--- dom/base/usecounters.py
+++ dom/base/usecounters.py
@@ -8,7 +8,7 @@ import re
 
 def read_conf(conf_filename):
     # Can't read/write from a single StringIO, so make a new one for reading.
-    stream = open(conf_filename, "rU")
+    stream = open(conf_filename, "r")
 
     def parse_counters(stream):
         for line_num, line in enumerate(stream):
