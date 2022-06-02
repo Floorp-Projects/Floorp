b9ee29bc442a91311d5b9e71e58ef534c7984f71
Michael Froman <mfroman@mozilla.com>
2022-06-02 12:58:19 -0500

    Bug 1766646 - Vendor libwebrtc from 4d0760e7f9c
    Upstream commit: https://webrtc.googlesource.com/src/+/4d0760e7f9c236dd417ac8bf63ba7cfea0ea1652
        Add ability to mark video sources for screen casting in ObjC
    
        Bug: webrtc:13033
        Change-Id: If30a4889cd2cb0ecc5ee91eed2ee9b496a40c852
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/227295
        Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
        Commit-Queue: Kári Helgason <kthelgason@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#34818}

--- third_party/libwebrtc/sdk/objc/api/peerconnection/RTCPeerConnectionFactory.h
+++ third_party/libwebrtc/sdk/objc/api/peerconnection/RTCPeerConnectionFactory.h
@@ -60,6 +60,13 @@ RTC_OBJC_EXPORT
  */
 - (RTC_OBJC_TYPE(RTCVideoSource) *)videoSource;
 
+/** Initialize a generic RTCVideoSource with he posibility of marking
+ * it as usable for screen sharing. The RTCVideoSource should be
+ * passed to a RTCVideoCapturer implementation, e.g.
+ * RTCCameraVideoCapturer, in order to produce frames.
+ */
+- (RTC_OBJC_TYPE(RTCVideoSource) *)videoSourceForScreenCast:(BOOL)forScreenCast;
+
 /** Initialize an RTCVideoTrack with a source and an id. */
 - (RTC_OBJC_TYPE(RTCVideoTrack) *)videoTrackWithSource:(RTC_OBJC_TYPE(RTCVideoSource) *)source
                                                trackId:(NSString *)trackId;
--- third_party/libwebrtc/sdk/objc/native/src/objc_video_track_source.mm
+++ third_party/libwebrtc/sdk/objc/native/src/objc_video_track_source.mm
@@ -34,15 +34,18 @@
 
 namespace webrtc {
 
-ObjCVideoTrackSource::ObjCVideoTrackSource()
-    : AdaptedVideoTrackSource(/* required resolution alignment */ 2) {}
+ObjCVideoTrackSource::ObjCVideoTrackSource() : ObjCVideoTrackSource(false) {}
+
+ObjCVideoTrackSource::ObjCVideoTrackSource(bool is_screencast)
+    : AdaptedVideoTrackSource(/* required resolution alignment */ 2),
+      is_screencast_(is_screencast) {}
 
 ObjCVideoTrackSource::ObjCVideoTrackSource(RTCObjCVideoSourceAdapter *adapter) : adapter_(adapter) {
   adapter_.objCVideoTrackSource = this;
 }
 
 bool ObjCVideoTrackSource::is_screencast() const {
-  return false;
+  return is_screencast_;
 }
 
 absl::optional<bool> ObjCVideoTrackSource::needs_denoising() const {
