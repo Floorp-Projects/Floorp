86a7dfe50b3ac0ddaccce67f34ff3ceca94b4948
Dana Keeler <dkeeler@mozilla.com>
2022-06-03 23:26:28 +0000

    Bug 1769669 - require specifying the trusted root in content signature verifier r=jschanck,leplatrem,robwu,barret
    Before this patch, the content signature verifier
    (nsIContentSignatureVerifier/ContentSignatureVerifier) would identify the root
    it trusted based on the value of a preference. This patch changes the
    implementation to require a specified hard-coded root to trust as with add-on
    signature verification.
    
    Depends on D146644
    
    Differential Revision: https://phabricator.services.mozilla.com/D146645

--- toolkit/mozapps/extensions/test/xpcshell/head_cert_handling.js
+++ toolkit/mozapps/extensions/test/xpcshell/head_cert_handling.js
@@ -11,34 +11,6 @@
 
 "use strict";
 
-// Convert a string to an array of bytes consisting of the char code at each
-// index.
-function stringToArray(s) {
-  let a = [];
-  for (let i = 0; i < s.length; i++) {
-    a.push(s.charCodeAt(i));
-  }
-  return a;
-}
-
-// Commonly certificates are represented as PEM. The format is roughly as
-// follows:
-//
-// -----BEGIN CERTIFICATE-----
-// [some lines of base64, each typically 64 characters long]
-// -----END CERTIFICATE-----
-//
-// However, nsIX509CertDB.constructX509FromBase64 and related functions do not
-// handle input of this form. Instead, they require a single string of base64
-// with no newlines or BEGIN/END headers. This is a helper function to convert
-// PEM to the format that nsIX509CertDB requires.
-function pemToBase64(pem) {
-  return pem
-    .replace(/-----BEGIN CERTIFICATE-----/, "")
-    .replace(/-----END CERTIFICATE-----/, "")
-    .replace(/[\r\n]/g, "");
-}
-
 function readFile(file) {
   let fstream = Cc["@mozilla.org/network/file-input-stream;1"].createInstance(
     Ci.nsIFileInputStream
@@ -49,24 +21,6 @@ function readFile(file) {
     available > 0 ? NetUtil.readInputStreamToString(fstream, available) : "";
   fstream.close();
   return data;
-}
-
-function constructCertFromFile(filename) {
-  let certFile = do_get_file(filename, false);
-  let certBytes = readFile(certFile);
-  let certdb = Cc["@mozilla.org/security/x509certdb;1"].getService(
-    Ci.nsIX509CertDB
-  );
-  // Certs should always be in .pem format, don't bother with .dem handling.
-  return certdb.constructX509FromBase64(pemToBase64(certBytes));
-}
-
-function setCertRoot(filename) {
-  let cert = constructCertFromFile(filename);
-  Services.prefs.setCharPref(
-    "security.content.signature.root_hash",
-    cert.sha256Fingerprint
-  );
 }
 
 function loadCertChain(prefix, names) {
