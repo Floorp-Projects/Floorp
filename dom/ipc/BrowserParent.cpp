d0ae7eb0e40320de71046809f17530b1138beeb5
Masayuki Nakano <masayuki@d-toybox.com>
2022-06-06 14:30:42 +0000

    Bug 1772634 - part 1: Make `BrowserParent::RecvSynthesizeNativeMouseMove` relax r=smaug
    This is used by in the normal mode (i.e., not in the automation mode) to
    emulate a hover at locking or unlocking a pointer capture.  If it'd be possible,
    we should make it handle only in the remote process, but for now, we should
    make it relax for safer fix in the branches.  I think that it's acceptable
    because synthesizing only native mousemove events is safer than the other events.
    
    Differential Revision: https://phabricator.services.mozilla.com/D148364

--- dom/ipc/BrowserParent.cpp
+++ dom/ipc/BrowserParent.cpp
@@ -1928,8 +1928,8 @@ mozilla::ipc::IPCResult BrowserParent::RecvSynthesizeNativeMouseEvent(
 
 mozilla::ipc::IPCResult BrowserParent::RecvSynthesizeNativeMouseMove(
     const LayoutDeviceIntPoint& aPoint, const uint64_t& aObserverId) {
-  NS_ENSURE_TRUE(xpc::IsInAutomation(), IPC_FAIL(this, "Unexpected event"));
-
+  // This is used by pointer lock API.  So, even if it's not in the automation
+  // mode, we need to accept the request.
   AutoSynthesizedEventResponder responder(this, aObserverId, "mousemove");
   nsCOMPtr<nsIWidget> widget = GetWidget();
   if (widget) {
