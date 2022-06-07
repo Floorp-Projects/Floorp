7bcebb2935fb2dabcfb688eea8d6d79088375ba6
Mats Palmgren <mats@mozilla.com>
2022-06-07 09:30:01 +0000

    Bug 1590376 part 1 - Add a XULResizerElement and move all nsResizerFrame's event handling code there.  r=ntim,smaug
    I'm mostly moving the code verbatim, but I excluded a few bits
    that handled resizers inside menu popup frames, e.g.
    https://searchfox.org/mozilla-central/rev/7bb1cc6abf6634b2a20f71935e1e519e73402b63/layout/xul/nsResizerFrame.cpp#165-170
    I don't think we need that functionallity anymore and it
    simplifies the code to exclude it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D105926

--- layout/xul/nsResizerFrame.h
+++ layout/xul/nsResizerFrame.h
@@ -6,78 +6,21 @@
 #ifndef nsResizerFrame_h___
 #define nsResizerFrame_h___
 
-#include "mozilla/Attributes.h"
-#include "mozilla/EventForwards.h"
 #include "nsTitleBarFrame.h"
-
-class nsIBaseWindow;
 
 namespace mozilla {
 class PresShell;
 }  // namespace mozilla
 
 class nsResizerFrame final : public nsTitleBarFrame {
- protected:
-  typedef mozilla::LayoutDeviceIntPoint LayoutDeviceIntPoint;
-  typedef mozilla::LayoutDeviceIntRect LayoutDeviceIntRect;
-
-  struct Direction {
-    int8_t mHorizontal;
-    int8_t mVertical;
-  };
-
  public:
   NS_DECL_FRAMEARENA_HELPERS(nsResizerFrame)
 
   friend nsIFrame* NS_NewResizerFrame(mozilla::PresShell* aPresShell,
                                       ComputedStyle* aStyle);
 
-  explicit nsResizerFrame(ComputedStyle* aStyle, nsPresContext* aPresContext);
+  nsResizerFrame(ComputedStyle* aStyle, nsPresContext* aPresContext);
 
-  virtual nsresult HandleEvent(nsPresContext* aPresContext,
-                               mozilla::WidgetGUIEvent* aEvent,
-                               nsEventStatus* aEventStatus) override;
-
-  MOZ_CAN_RUN_SCRIPT_BOUNDARY
-  virtual void MouseClicked(mozilla::WidgetMouseEvent* aEvent) override;
-
- protected:
-  nsIContent* GetContentToResize(mozilla::PresShell* aPresShell,
-                                 nsIBaseWindow** aWindow);
-
-  Direction GetDirection();
-
-  /**
-   * Adjust the window position and size in a direction according to the mouse
-   * movement and the resizer direction. The minimum and maximum size is used
-   * to constrain the size.
-   *
-   * @param aPos left or top position
-   * @param aSize width or height
-   * @param aMinSize minimum width or height
-   * @param aMacSize maximum width or height
-   * @param aMovement the amount the mouse was moved
-   * @param aResizerDirection resizer direction returned by GetDirection
-   */
-  static void AdjustDimensions(int32_t* aPos, int32_t* aSize, int32_t aMinSize,
-                               int32_t aMaxSize, int32_t aMovement,
-                               int8_t aResizerDirection);
-
-  struct SizeInfo {
-    nsCString width, height;
-  };
-  static void SizeInfoDtorFunc(void* aObject, nsAtom* aPropertyName,
-                               void* aPropertyValue, void* aData);
-  static void ResizeContent(nsIContent* aContent, const Direction& aDirection,
-                            const SizeInfo& aSizeInfo,
-                            SizeInfo* aOriginalSizeInfo);
-  static void MaybePersistOriginalSize(nsIContent* aContent,
-                                       const SizeInfo& aSizeInfo);
-  static void RestoreOriginalSize(nsIContent* aContent);
-
- protected:
-  LayoutDeviceIntRect mMouseDownRect;
-  LayoutDeviceIntPoint mMouseDownPoint;
 };  // class nsResizerFrame
 
 #endif /* nsResizerFrame_h___ */
