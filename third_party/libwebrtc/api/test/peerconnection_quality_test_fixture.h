4d236efcc7bdb384aba08588e9c8e09baede125e
Michael Froman <mfroman@mozilla.com>
2022-06-06 21:35:38 -0500

    Bug 1766646 - Vendor libwebrtc from 79f82c1a576
    Upstream commit: https://webrtc.googlesource.com/src/+/79f82c1a5767f765e83c19927a517f5d1c43ad45
        [PCLF] introduce a Peer class which later will be used to perform in-call actions
    
        Bug: b/198796179
        Change-Id: Ic4ea2b8d03cbc524334d72ef5c8b3ad1ecd39359
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/231182
        Commit-Queue: Artem Titov <titovartem@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#34920}

--- third_party/libwebrtc/api/test/peerconnection_quality_test_fixture.h
+++ third_party/libwebrtc/api/test/peerconnection_quality_test_fixture.h
@@ -465,6 +465,13 @@ class PeerConnectionE2EQualityTestFixture {
     virtual void StopAndReportResults() = 0;
   };
 
+  // Represents single participant in call and can be used to perform different
+  // in-call actions. Might be extended in future.
+  class PeerHandle {
+   public:
+    virtual ~PeerHandle() = default;
+  };
+
   virtual ~PeerConnectionE2EQualityTestFixture() = default;
 
   // Add activity that will be executed on the best effort at least after
@@ -493,7 +500,13 @@ class PeerConnectionE2EQualityTestFixture {
   // `configurer` function will be used to configure peer in the call.
   virtual void AddPeer(rtc::Thread* network_thread,
                        rtc::NetworkManager* network_manager,
-                       rtc::FunctionView<void(PeerConfigurer*)> configurer) = 0;
+                       rtc::FunctionView<void(PeerConfigurer*)> configurer) {}
+  virtual PeerHandle* AddAndReturnPeer(
+      rtc::Thread* network_thread,
+      rtc::NetworkManager* network_manager,
+      rtc::FunctionView<void(PeerConfigurer*)> configurer) {
+    return nullptr;
+  }
   // Runs the media quality test, which includes setting up the call with
   // configured participants, running it according to provided `run_params` and
   // terminating it properly at the end. During call duration media quality
