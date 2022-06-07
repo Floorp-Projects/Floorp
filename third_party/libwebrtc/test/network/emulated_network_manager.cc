c77a3270a382b18530fb36d987eae6c222d0d404
Michael Froman <mfroman@mozilla.com>
2022-06-07 16:35:16 -0500

    Bug 1766646 - Vendor libwebrtc from 5e7a3aedf1f
    Upstream commit: https://webrtc.googlesource.com/src/+/5e7a3aedf1f3699d74ee111b48173a031acafb1f
        Let EmulatedNetworkManagerInterface own and expose a PacketSocketFactory
    
        So that applications don't need to construct it from the exposed
        network_thread.
    
        The EmulatedNetworkManagerInterface::network_thread() accessor is currently
        used as a way to get to emulation's SocketServer, and should be deleted
        when applications of the emulation framework have migrated away from
        that usage.
    
        Bug: webrtc:13145
        Change-Id: I3efa55d117cad8ac601c48a9d2d2aa62a121f9c9
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/231649
        Reviewed-by: Artem Titov <titovartem@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Niels Moller <nisse@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#34964}

--- third_party/libwebrtc/test/network/emulated_network_manager.cc
+++ third_party/libwebrtc/test/network/emulated_network_manager.cc
@@ -14,6 +14,7 @@
 #include <utility>
 
 #include "absl/memory/memory.h"
+#include "p2p/base/basic_packet_socket_factory.h"
 #include "test/network/fake_network_socket_server.h"
 
 namespace webrtc {
@@ -25,11 +26,17 @@ EmulatedNetworkManager::EmulatedNetworkManager(
     EndpointsContainer* endpoints_container)
     : task_queue_(task_queue),
       endpoints_container_(endpoints_container),
-      network_thread_(time_controller->CreateThread(
-          "net_thread",
-          std::make_unique<FakeNetworkSocketServer>(endpoints_container))),
       sent_first_update_(false),
-      start_count_(0) {}
+      start_count_(0) {
+  auto socket_server =
+      std::make_unique<FakeNetworkSocketServer>(endpoints_container);
+  packet_socket_factory_ =
+      std::make_unique<rtc::BasicPacketSocketFactory>(socket_server.get());
+  // Since we pass ownership of the socket server to `network_thread_`, we must
+  // arrange that it outlives `packet_socket_factory_` which refers to it.
+  network_thread_ =
+      time_controller->CreateThread("net_thread", std::move(socket_server));
+}
 
 void EmulatedNetworkManager::EnableEndpoint(EmulatedEndpointImpl* endpoint) {
   RTC_CHECK(endpoints_container_->HasEndpoint(endpoint))
