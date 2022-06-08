d5c604567d4bf89f19e5aaf76f4e289f5ca0c2ad
Michael Froman <mfroman@mozilla.com>
2022-06-08 14:07:25 -0500

    Bug 1766646 - Vendor libwebrtc from d92f8a86b3b
    Upstream commit: https://webrtc.googlesource.com/src/+/d92f8a86b3b0ca57ff8f8c3a71714a8b7800f103
        dcsctp: move HandoverReadinessStatus::ToString definition to build target public:socket
    
        This allows build targets that need only HandoverReadinessStatus
        to depend only on public:socket, and not on socket:dcsctp_socket.
    
        Bug: webrtc:13154
        Change-Id: I29f41910cdb5baed96b57fd7284b96fc50a56ba4
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/232331
        Reviewed-by: Victor Boivie <boivie@webrtc.org>
        Commit-Queue: Sergey Sukhanov <sergeysu@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#35037}

--- third_party/libwebrtc/net/dcsctp/socket/dcsctp_socket.cc
+++ third_party/libwebrtc/net/dcsctp/socket/dcsctp_socket.cc
@@ -138,57 +138,7 @@ TieTag MakeTieTag(DcSctpSocketCallbacks& cb) {
   return TieTag(static_cast<uint64_t>(tie_tag_upper) << 32 |
                 static_cast<uint64_t>(tie_tag_lower));
 }
-
-constexpr absl::string_view HandoverUnreadinessReasonToString(
-    HandoverUnreadinessReason reason) {
-  switch (reason) {
-    case HandoverUnreadinessReason::kWrongConnectionState:
-      return "WRONG_CONNECTION_STATE";
-    case HandoverUnreadinessReason::kSendQueueNotEmpty:
-      return "SEND_QUEUE_NOT_EMPTY";
-    case HandoverUnreadinessReason::kDataTrackerTsnBlocksPending:
-      return "DATA_TRACKER_TSN_BLOCKS_PENDING";
-    case HandoverUnreadinessReason::kReassemblyQueueDeliveredTSNsGap:
-      return "REASSEMBLY_QUEUE_DELIVERED_TSN_GAP";
-    case HandoverUnreadinessReason::kStreamResetDeferred:
-      return "STREAM_RESET_DEFERRED";
-    case HandoverUnreadinessReason::kOrderedStreamHasUnassembledChunks:
-      return "ORDERED_STREAM_HAS_UNASSEMBLED_CHUNKS";
-    case HandoverUnreadinessReason::kUnorderedStreamHasUnassembledChunks:
-      return "UNORDERED_STREAM_HAS_UNASSEMBLED_CHUNKS";
-    case HandoverUnreadinessReason::kRetransmissionQueueOutstandingData:
-      return "RETRANSMISSION_QUEUE_OUTSTANDING_DATA";
-    case HandoverUnreadinessReason::kRetransmissionQueueFastRecovery:
-      return "RETRANSMISSION_QUEUE_FAST_RECOVERY";
-    case HandoverUnreadinessReason::kRetransmissionQueueNotEmpty:
-      return "RETRANSMISSION_QUEUE_NOT_EMPTY";
-    case HandoverUnreadinessReason::kPendingStreamReset:
-      return "PENDING_STREAM_RESET";
-    case HandoverUnreadinessReason::kPendingStreamResetRequest:
-      return "PENDING_STREAM_RESET_REQUEST";
-  }
-}
 }  // namespace
-
-std::string HandoverReadinessStatus::ToString() const {
-  std::string result;
-  for (uint32_t bit = 1;
-       bit <= static_cast<uint32_t>(HandoverUnreadinessReason::kMax);
-       bit *= 2) {
-    auto flag = static_cast<HandoverUnreadinessReason>(bit);
-    if (Contains(flag)) {
-      if (!result.empty()) {
-        result.append(",");
-      }
-      absl::string_view s = HandoverUnreadinessReasonToString(flag);
-      result.append(s.data(), s.size());
-    }
-  }
-  if (result.empty()) {
-    result = "READY";
-  }
-  return result;
-}
 
 DcSctpSocket::DcSctpSocket(absl::string_view log_prefix,
                            DcSctpSocketCallbacks& callbacks,
