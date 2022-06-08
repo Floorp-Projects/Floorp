1c19d0dcbc3249ca85a3be98e42da67bd6743ac0
Emilio Cobos Álvarez <emilio@crisal.io>
2022-06-08 11:25:25 +0000

    Bug 1772968 - Clean up a bit more GetUnretargetedFullscreenElement usage. r=edgar
    Differential Revision: https://phabricator.services.mozilla.com/D148499

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -1891,13 +1891,13 @@ class Document : public nsINode,
   // This is called asynchronously by Document::AsyncRequestFullscreen()
   // to move this document into fullscreen mode if allowed.
   void RequestFullscreen(UniquePtr<FullscreenRequest> aRequest,
-                         bool applyFullScreenDirectly = false);
+                         bool aApplyFullscreenDirectly = false);
 
  private:
   void RequestFullscreenInContentProcess(UniquePtr<FullscreenRequest> aRequest,
-                                         bool applyFullScreenDirectly);
+                                         bool aApplyFullscreenDirectly);
   void RequestFullscreenInParentProcess(UniquePtr<FullscreenRequest> aRequest,
-                                        bool applyFullScreenDirectly);
+                                        bool aApplyFullscreenDirectly);
 
   // Pushes aElement onto the top layer
   void TopLayerPush(Element&);
