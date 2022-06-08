b8da4108ec427c47108fb7884f85fe001b66821f
Jan de Mooij <jdemooij@mozilla.com>
2022-06-08 06:25:26 +0000

    Bug 1393701 part 1 - Use a proxy in testWindowNonConfigurable.cpp. r=evilpie
    This lets us assert the `defineProperty` `ObjectOp` isn't used for arbitrary native objects.
    Making this a proxy also makes it more similar to the browser `WindowProxy`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D148518

--- js/src/jsapi-tests/testWindowNonConfigurable.cpp
+++ js/src/jsapi-tests/testWindowNonConfigurable.cpp
@@ -6,37 +6,40 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "js/PropertyAndElement.h"  // JS_DefineProperty
+#include "js/Proxy.h"
 #include "jsapi-tests/tests.h"
 
-static bool windowProxy_defineProperty(JSContext* cx, JS::HandleObject obj,
-                                       JS::HandleId id,
-                                       JS::Handle<JS::PropertyDescriptor> desc,
-                                       JS::ObjectOpResult& result) {
-  if (desc.hasConfigurable() && !desc.configurable()) {
-    result.failCantDefineWindowNonConfigurable();
-    return true;
+class WindowProxyHandler : public js::ForwardingProxyHandler {
+ public:
+  constexpr WindowProxyHandler() : js::ForwardingProxyHandler(&family) {}
+
+  static const char family;
+
+  virtual bool defineProperty(JSContext* cx, JS::HandleObject proxy,
+                              JS::HandleId id,
+                              JS::Handle<JS::PropertyDescriptor> desc,
+                              JS::ObjectOpResult& result) const override {
+    if (desc.hasConfigurable() && !desc.configurable()) {
+      result.failCantDefineWindowNonConfigurable();
+      return true;
+    }
+    return ForwardingProxyHandler::defineProperty(cx, proxy, id, desc, result);
   }
-
-  return NativeDefineProperty(cx, obj.as<js::NativeObject>(), id, desc, result);
-}
-
-static const js::ObjectOps windowProxy_objectOps = {
-    nullptr,                     // lookupProperty
-    windowProxy_defineProperty,  // defineProperty
-    nullptr,                     // hasProperty
-    nullptr,                     // getProperty
-    nullptr,                     // setProperty
-    nullptr,                     // getOwnPropertyDescriptor
-    nullptr,                     // deleteProperty
-    nullptr,                     // getElements
-    nullptr,                     // funToString
 };
+const char WindowProxyHandler::family = 0;
 
-static const JSClass windowProxy_class = {
-    "WindowProxy", 0, nullptr, nullptr, nullptr, &windowProxy_objectOps};
+static const JSClass windowProxy_class =
+    PROXY_CLASS_DEF("TestWindowProxy", JSCLASS_HAS_RESERVED_SLOTS(1));
+static const WindowProxyHandler windowProxy_handler;
 
 BEGIN_TEST(testWindowNonConfigurable) {
-  JS::RootedObject obj(cx, JS_NewObject(cx, &windowProxy_class));
+  JS::RootedObject wrapped(cx, JS_NewObject(cx, nullptr));
+  CHECK(wrapped);
+  JS::RootedValue wrappedVal(cx, JS::ObjectValue(*wrapped));
+  js::ProxyOptions options;
+  options.setClass(&windowProxy_class);
+  JS::RootedObject obj(cx, NewProxyObject(cx, &windowProxy_handler, wrappedVal,
+                                          nullptr, options));
   CHECK(obj);
   CHECK(JS_DefineProperty(cx, global, "windowProxy", obj, 0));
   JS::RootedValue v(cx);
