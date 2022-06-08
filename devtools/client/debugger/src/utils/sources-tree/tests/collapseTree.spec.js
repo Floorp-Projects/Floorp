2673337b05b271775919d9ddbdaff6e127a55d18
Alexandre Poirot <poirot.alex@gmail.com>
2022-06-08 15:27:33 +0000

    Bug 1771939 - [devtools] Move `formatTree` from production to test helpers. r=bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D147779

--- devtools/client/debugger/src/utils/sources-tree/tests/collapseTree.spec.js
+++ devtools/client/debugger/src/utils/sources-tree/tests/collapseTree.spec.js
@@ -2,11 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-import { makeMockDisplaySource } from "../../../utils/test-mockup";
+import { makeMockDisplaySource, formatTree } from "../../../utils/test-mockup";
 
 import {
   collapseTree,
-  formatTree,
   addToTree,
   createDirectoryNode,
 } from "../index";
