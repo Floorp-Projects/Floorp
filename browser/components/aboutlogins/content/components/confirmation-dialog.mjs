750d577f857a1d902830f2b166f0f7efbc86f0da
Mark Banner <standard8@mozilla.com>
2022-06-09 17:53:11 +0000

    Bug 1773468 - Change about:logins module files to use mjs extensions. r=sgalich
    Depends on D148771
    
    Differential Revision: https://phabricator.services.mozilla.com/D148772

--- browser/components/aboutlogins/content/components/confirmation-dialog.mjs
+++ browser/components/aboutlogins/content/components/confirmation-dialog.mjs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { setKeyboardAccessForNonDialogElements } from "../aboutLoginsUtils.js";
+import { setKeyboardAccessForNonDialogElements } from "../aboutLoginsUtils.mjs";
 
 export default class ConfirmationDialog extends HTMLElement {
   constructor() {
