80598b951af5749f78b8f7b88ce87b754b5a47da
Butkovits Atila <abutkovits@mozilla.com>
2022-06-09 17:15:41 +0300

    Backed out changeset 46c9d33f30a7 (bug 1773284) for causing devtools failures. CLOSED TREE

--- devtools/shared/protocol/Front/FrontClassWithSpec.js
+++ devtools/shared/protocol/Front/FrontClassWithSpec.js
@@ -4,8 +4,6 @@
 
 "use strict";
 
-const { Cu } = require("chrome");
-const ChromeUtils = require("ChromeUtils");
 var { Front } = require("devtools/shared/protocol/Front");
 
 /**
@@ -32,7 +30,6 @@ var generateRequestMethods = function(actorSpec, frontProto) {
         );
       }
 
-      const startTime = Cu.now();
       let packet;
       try {
         packet = spec.request.write(args, this);
@@ -62,11 +59,6 @@ var generateRequestMethods = function(actorSpec, frontProto) {
           console.error("Error reading response to: " + name + "\n" + ex);
           throw ex;
         }
-        ChromeUtils.addProfilerMarker(
-          "RDP Front",
-          startTime,
-          `${this.typeName}:${name}()`
-        );
         return ret;
       });
     };
