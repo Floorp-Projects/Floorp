d431fd0917ef1687c935c008bbd8b772bf677e6c
Simran Sandhu <f20171454@hyderabad.bits-pilani.ac.in>
2022-06-09 21:41:49 +0000

    Bug 1773124 - Make devtools/shared/natural-sort.js easier to read r=devtools-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D148545

--- devtools/shared/natural-sort.js
+++ devtools/shared/natural-sort.js
@@ -3,11 +3,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 /*
- * Natural Sort algorithm for Javascript - Version 0.8.1 - Released under MIT license
- * Author: Jim Palmer (based on chunking idea from Dave Koelle)
+ * Based on the Natural Sort algorithm for Javascript - Version 0.8.1 - adapted
+ * for Firefox DevTools and released under the MIT license.
  *
- * Includes pull request to move regexes out of main function for performance
- * increases.
+ * Author: Jim Palmer (based on chunking idea from Dave Koelle)
  *
  * Repository:
  *   https://github.com/overset/javascript-natural-sort/
@@ -15,33 +14,22 @@
 
 "use strict";
 
-var re = /(^([+\-]?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?(?=\D|\s|$))|^0x[\da-fA-F]+$|\d+)/g;
-var sre = /^\s+|\s+$/g; // trim pre-post whitespace
-var snre = /\s+/g; // normalize all whitespace to single ' ' character
-
-// eslint-disable-next-line
-var dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/;
-var hre = /^0x[0-9a-f]+$/i;
-var ore = /^0/;
-var b0re = /^\0/;
-var e0re = /\0$/;
-
-exports.naturalSortCaseSensitive = function naturalSortCaseSensitive(a, b) {
-  return naturalSort(a, b, false);
-};
-
-exports.naturalSortCaseInsensitive = function naturalSortCaseInsensitive(a, b) {
-  return naturalSort(a, b, true);
-};
+const tokenizeNumbersRx = /(^([+\-]?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?(?=\D|\s|$))|^0x[\da-fA-F]+$|\d+)/g;
+const dateRx = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/;
+const hexRx = /^0x[0-9a-f]+$/i;
+const startsWithNullRx = /^\0/;
+const endsWithNullRx = /\0$/;
+const whitespaceRx = /\s+/g;
+const startsWithZeroRx = /^0/;
 
 /**
  * Sort numbers, strings, IP Addresses, Dates, Filenames, version numbers etc.
  * "the way humans do."
  *
- * This function should only be called via naturalSortCaseSensitive and
+ * This function should only be called via naturalSortCaseSensitive or
  * naturalSortCaseInsensitive.
  *
- * e.g. [3, 2, 1, 10].sort(naturalSort)
+ * e.g. [3, 2, 1, 10].sort(naturalSortCaseSensitive)
  *
  * @param  {Object} a
  *         Passed in by Array.sort(a, b)
@@ -51,70 +39,89 @@ exports.naturalSortCaseInsensitive = function naturalSortCaseInsensitive(a, b) {
  *         Should the search be case insensitive?
  */
 // eslint-disable-next-line complexity
-function naturalSort(a, b, insensitive) {
-  // convert all to strings strip whitespace
-  const i = function(s) {
-    return ((insensitive && ("" + s).toLowerCase()) || "" + s).replace(sre, "");
-  };
-  const x = i(a) || "";
-  const y = i(b) || "";
-  // chunk/tokenize
-  const xN = x
-    .replace(re, "\0$1\0")
-    .replace(e0re, "")
-    .replace(b0re, "")
-    .split("\0");
-  const yN = y
-    .replace(re, "\0$1\0")
-    .replace(e0re, "")
-    .replace(b0re, "")
-    .split("\0");
-  // numeric, hex or date detection
-  const xD = parseInt(x.match(hre), 16) || (xN.length !== 1 && Date.parse(x));
-  const yD =
-    parseInt(y.match(hre), 16) || (xD && y.match(dre) && Date.parse(y)) || null;
-  const normChunk = function(s, l) {
-    // normalize spaces; find floats not starting with '0', string or 0 if
-    // not defined (Clint Priest)
-    return (
-      ((!s.match(ore) || l == 1) && parseFloat(s)) ||
-      s.replace(snre, " ").replace(sre, "") ||
-      0
-    );
-  };
-  let oFxNcL;
-  let oFyNcL;
+function naturalSort(a = "", b = "", insensitive = false) {
+  // Ensure we are working with trimmed strings
+  a = (a + "").trim();
+  b = (b + "").trim();
 
-  // first try and sort Hex codes or Dates
-  if (yD) {
-    if (xD < yD) {
+  if (insensitive) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+  }
+
+  // Chunk/tokenize - Here we split the strings into arrays or strings and
+  // numbers.
+  const aChunks = a
+    .replace(tokenizeNumbersRx, "\0$1\0")
+    .replace(startsWithNullRx, "")
+    .replace(endsWithNullRx, "")
+    .split("\0");
+  const bChunks = b
+    .replace(tokenizeNumbersRx, "\0$1\0")
+    .replace(startsWithNullRx, "")
+    .replace(endsWithNullRx, "")
+    .split("\0");
+
+  // Hex or date detection.
+  const aHexOrDate =
+    parseInt(a.match(hexRx), 16) || (aChunks.length !== 1 && Date.parse(a));
+  const bHexOrDate =
+    parseInt(b.match(hexRx), 16) ||
+    (aHexOrDate && b.match(dateRx) && Date.parse(b)) ||
+    null;
+
+  // Try and sort Hex codes or Dates.
+  if (bHexOrDate) {
+    if (aHexOrDate < bHexOrDate) {
       return -1;
-    } else if (xD > yD) {
+    } else if (aHexOrDate > bHexOrDate) {
       return 1;
     }
   }
 
-  // natural sorting through split numeric strings and default strings
-  // eslint-disable-next-line
-  for (let cLoc = 0, xNl = xN.length, yNl = yN.length, numS = Math.max(xNl, yNl); cLoc < numS; cLoc++) {
-    oFxNcL = normChunk(xN[cLoc] || "", xNl);
-    oFyNcL = normChunk(yN[cLoc] || "", yNl);
+  // Natural sorting through split numeric strings and default strings
+  const aChunksLength = aChunks.length;
+  const bChunksLength = bChunks.length;
+  const maxLen = Math.max(aChunksLength, bChunksLength);
 
-    // handle numeric vs string comparison - number < string - (Kyle Adams)
-    if (isNaN(oFxNcL) !== isNaN(oFyNcL)) {
-      return isNaN(oFxNcL) ? 1 : -1;
+  for (let i = 0; i < maxLen; i++) {
+    const aChunk = normalizeChunk(aChunks[i] || "", aChunksLength);
+    const bChunk = normalizeChunk(bChunks[i] || "", bChunksLength);
+
+    // Handle numeric vs string comparison - number < string
+    if (isNaN(aChunk) !== isNaN(bChunk)) {
+      return isNaN(aChunk) ? 1 : -1;
     }
-    // if unicode use locale comparison
-    // eslint-disable-next-line
-    if (/[^\x00-\x80]/.test(oFxNcL + oFyNcL) && oFxNcL.localeCompare) {
-      const comp = oFxNcL.localeCompare(oFyNcL);
+
+    // If unicode use locale comparison
+    // eslint-disable-next-line no-control-regex
+    if (/[^\x00-\x80]/.test(aChunk + bChunk) && aChunk.localeCompare) {
+      const comp = aChunk.localeCompare(bChunk);
       return comp / Math.abs(comp);
     }
-    if (oFxNcL < oFyNcL) {
+    if (aChunk < bChunk) {
       return -1;
-    } else if (oFxNcL > oFyNcL) {
+    } else if (aChunk > bChunk) {
       return 1;
     }
   }
   return null;
 }
+
+// Normalize spaces; find floats not starting with '0', string or 0 if not
+// defined
+const normalizeChunk = function(str, length) {
+  return (
+    ((!str.match(startsWithZeroRx) || length == 1) && parseFloat(str)) ||
+    str.replace(whitespaceRx, " ").trim() ||
+    0
+  );
+};
+
+exports.naturalSortCaseSensitive = function naturalSortCaseSensitive(a, b) {
+  return naturalSort(a, b, false);
+};
+
+exports.naturalSortCaseInsensitive = function naturalSortCaseInsensitive(a, b) {
+  return naturalSort(a, b, true);
+};
