91317935b064d0ddbc8af64ba0f8fb5bd166a95f
Molnar Sandor <smolnar@mozilla.com>
2022-06-09 22:02:36 +0300

    Backed out 2 changesets (bug 1773469, bug 1773468) for causing browser-chrome failures in test/static/browser_all_files_referenced.js CLOSED TREE
    Backed out changeset 65833ced5638 (bug 1773469)
    Backed out changeset ebc7b14f1ce5 (bug 1773468)

--- browser/components/aboutlogins/content/components/remove-logins-dialog.js
+++ browser/components/aboutlogins/content/components/remove-logins-dialog.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { setKeyboardAccessForNonDialogElements } from "../aboutLoginsUtils.mjs";
+import { setKeyboardAccessForNonDialogElements } from "../aboutLoginsUtils.js";
 
 export default class RemoveLoginsDialog extends HTMLElement {
   constructor() {
