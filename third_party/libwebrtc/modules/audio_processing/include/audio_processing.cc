534ac203d075a538d5c9b4b20a0bcf8305cec02a
Michael Froman <mfroman@mozilla.com>
2022-06-10 13:54:19 -0500

    Bug 1766646 - Vendor libwebrtc from b8ffdc4bb3
    Upstream commit: https://webrtc.googlesource.com/src/+/b8ffdc4bb3c8638511a92310bc8ad7e50acc1e2d
        APM: fix CaptureLevelAdjustment::operator==
    
        Bug: webrtc:7494
        Change-Id: I0ea13af2e23ed1490fa22d75d104bdd45b0452bf
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/233460
        Reviewed-by: Per Åhgren <peah@webrtc.org>
        Commit-Queue: Alessio Bazzica <alessiob@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#35142}

--- third_party/libwebrtc/modules/audio_processing/include/audio_processing.cc
+++ third_party/libwebrtc/modules/audio_processing/include/audio_processing.cc
@@ -110,7 +110,7 @@ bool Agc2Config::operator==(const Agc2Config& rhs) const {
 bool AudioProcessing::Config::CaptureLevelAdjustment::operator==(
     const AudioProcessing::Config::CaptureLevelAdjustment& rhs) const {
   return enabled == rhs.enabled && pre_gain_factor == rhs.pre_gain_factor &&
-         post_gain_factor && rhs.post_gain_factor &&
+         post_gain_factor == rhs.post_gain_factor &&
          analog_mic_gain_emulation == rhs.analog_mic_gain_emulation;
 }
 
