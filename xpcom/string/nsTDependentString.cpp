1393c2c723c7f360507cc4ba96d174d9e647da46
Nika Layzell <nika@thelayzells.com>
2022-06-10 21:12:09 +0000

    Bug 1772006 - Part 8: Remove unnecessary files from the string implementation, r=xpcom-reviewers,barret
    These files exist only due to the history of how strings were previously
    declared using macros, and aren't necessary anymore. The header files were not
    removed, as they may be depended on from outside of the module. Cleaning up the
    header situation is something which should probably happen in a follow-up.
    
    Differential Revision: https://phabricator.services.mozilla.com/D148302

--- xpcom/string/nsTDependentString.cpp
+++ xpcom/string/nsTDependentString.cpp
@@ -4,6 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "nsTDependentString.h"
+
 template <typename T>
 nsTDependentString<T>::nsTDependentString(const char_type* aStart,
                                           const char_type* aEnd)
