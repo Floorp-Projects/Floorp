5e9bc0b1f3ff781d9b5e4d6f7c3a72033542c6f4
Michael Froman <mfroman@mozilla.com>
2022-06-10 12:43:05 -0500

    Bug 1766646 - Vendor libwebrtc from 8970b49c756
    Upstream commit: https://webrtc.googlesource.com/src/+/8970b49c75693c943efa6c0d72fac02cd97a3ee6
        AgcManagerDirect: Add histograms for clipping prediction precision and recall
    
        Add histograms WebRTC.Audio.Agc.ClippingPredictor.Precision and WebRTC.Audio.Agc.ClippingPredictor.Recall. The histograms are defined in https://chromium-review.googlesource.com/c/chromium/src/+/3150271.
    
        Bug: webrtc:12774
        Change-Id: I1561ec7a61377c262f636d7aa3a5d5fd60a8839d
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/231460
        Commit-Queue: Hanna Silen <silen@webrtc.org>
        Reviewed-by: Alessio Bazzica <alessiob@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#35083}

--- third_party/libwebrtc/modules/audio_processing/agc/agc_manager_direct.cc
+++ third_party/libwebrtc/modules/audio_processing/agc/agc_manager_direct.cc
@@ -142,9 +142,25 @@ void LogClippingPredictorMetrics(const ClippingPredictorEvaluator& evaluator) {
                      << metrics->f1_score;
     RTC_DCHECK_GE(metrics->f1_score, 0.0f);
     RTC_DCHECK_LE(metrics->f1_score, 1.0f);
+    RTC_DCHECK_GE(metrics->precision, 0.0f);
+    RTC_DCHECK_LE(metrics->precision, 1.0f);
+    RTC_DCHECK_GE(metrics->recall, 0.0f);
+    RTC_DCHECK_LE(metrics->recall, 1.0f);
     RTC_HISTOGRAM_COUNTS_LINEAR(
         /*name=*/"WebRTC.Audio.Agc.ClippingPredictor.F1Score",
         /*sample=*/std::round(metrics->f1_score * 100.0f),
+        /*min=*/0,
+        /*max=*/100,
+        /*bucket_count=*/50);
+    RTC_HISTOGRAM_COUNTS_LINEAR(
+        /*name=*/"WebRTC.Audio.Agc.ClippingPredictor.Precision",
+        /*sample=*/std::round(metrics->precision * 100.0f),
+        /*min=*/0,
+        /*max=*/100,
+        /*bucket_count=*/50);
+    RTC_HISTOGRAM_COUNTS_LINEAR(
+        /*name=*/"WebRTC.Audio.Agc.ClippingPredictor.Recall",
+        /*sample=*/std::round(metrics->recall * 100.0f),
         /*min=*/0,
         /*max=*/100,
         /*bucket_count=*/50);
