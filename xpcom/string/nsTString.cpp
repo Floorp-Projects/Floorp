4b3c6384488b9755abeb92244eb34fff6e854931
Nika Layzell <nika@thelayzells.com>
2022-06-10 21:12:07 +0000

    Bug 1772006 - Part 4: Move basic string APIs to more appropriate .cpp files, r=xpcom-reviewers,barret
    Due to the history of how nsString was implemented, many APIs ended up being
    implemented in inappropriate files. This change moves simple definitions which
    won't require any changes for each type to happen within that type's .cpp file.
    
    This is necessary to eventually enable the directory to be built without
    unified builds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D148298

--- xpcom/string/nsTString.cpp
+++ xpcom/string/nsTString.cpp
@@ -4,6 +4,61 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "nsTString.h"
+#include "prdtoa.h"
+
+/**
+ * nsTString::SetCharAt
+ */
+
+template <typename T>
+bool nsTString<T>::SetCharAt(char16_t aChar, index_type aIndex) {
+  if (aIndex >= this->mLength) {
+    return false;
+  }
+
+  if (!this->EnsureMutable()) {
+    this->AllocFailed(this->mLength);
+  }
+
+  this->mData[aIndex] = char_type(aChar);
+  return true;
+}
+
+/**
+ * nsTString::ToDouble
+ */
+template <>
+double nsTString<char>::ToDouble(bool aAllowTrailingChars,
+                                 nsresult* aErrorCode) const {
+  double res = 0.0;
+  if (this->Length() > 0) {
+    char* conv_stopped;
+    const char* str = this->get();
+    // Use PR_strtod, not strtod, since we don't want locale involved.
+    res = PR_strtod(str, &conv_stopped);
+    if (aAllowTrailingChars && conv_stopped != str) {
+      *aErrorCode = NS_OK;
+    } else if (!aAllowTrailingChars && conv_stopped == str + this->Length()) {
+      *aErrorCode = NS_OK;
+    } else {
+      *aErrorCode = NS_ERROR_ILLEGAL_VALUE;
+    }
+  } else {
+    // The string was too short (0 characters)
+    *aErrorCode = NS_ERROR_ILLEGAL_VALUE;
+  }
+  return res;
+}
+
+template <>
+double nsTString<char16_t>::ToDouble(bool aAllowTrailingChars,
+                                     nsresult* aErrorCode) const {
+  NS_LossyConvertUTF16toASCII cString(BeginReading(), Length());
+  return aAllowTrailingChars ? cString.ToDoubleAllowTrailingChars(aErrorCode)
+                             : cString.ToDouble(aErrorCode);
+}
+
 template <typename T>
 void nsTString<T>::Rebind(const char_type* data, size_type length) {
   // If we currently own a buffer, release it.
