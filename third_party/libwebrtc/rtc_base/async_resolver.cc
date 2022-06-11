7bba5e06d35f36fd351afef27348c89a40df32e4
Michael Froman <mfroman@mozilla.com>
2022-06-11 13:13:36 -0500

    Bug 1766646 - Vendor libwebrtc from 08438fcd9a
    Upstream commit: https://webrtc.googlesource.com/src/+/08438fcd9afb2a535e910f8f4382d3debe87b0a1
        Remove a function for posting tasks to the global queue on MacOS/iOS and make it private.
    
        Since `PostTaskToGlobalQueue` is somewhat different from
        other TaskQueue APIs, there are concerns that it should not be
        a public API.
    
        Remove this from task_queue_gcd.h and make it a private static function
        of AsyncResolver.
    
        Bug: webrtc:13237
        Change-Id: Ib4aff296f894a4f3b051969d176369e13a10088f
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/234900
        Reviewed-by: Tommi <tommi@webrtc.org>
        Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
        Commit-Queue: Byoungchan Lee <daniel.l@hpcnt.com>
        Cr-Commit-Position: refs/heads/main@{#35236}

--- third_party/libwebrtc/rtc_base/async_resolver.cc
+++ third_party/libwebrtc/rtc_base/async_resolver.cc
@@ -41,10 +41,30 @@
 #include "rtc_base/third_party/sigslot/sigslot.h"  // for signal_with_thread...
 
 #if defined(WEBRTC_MAC) || defined(WEBRTC_IOS)
-#include "rtc_base/task_queue_gcd.h"
+#include <dispatch/dispatch.h>
 #endif
 
 namespace rtc {
+
+#if defined(WEBRTC_MAC) || defined(WEBRTC_IOS)
+namespace {
+
+void GlobalGcdRunTask(void* context) {
+  std::unique_ptr<webrtc::QueuedTask> task(
+      static_cast<webrtc::QueuedTask*>(context));
+  task->Run();
+}
+
+// Post a task into the system-defined global concurrent queue.
+void PostTaskToGlobalQueue(std::unique_ptr<webrtc::QueuedTask> task) {
+  dispatch_queue_global_t global_queue =
+      dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+  webrtc::QueuedTask* context = task.release();
+  dispatch_async_f(global_queue, context, &GlobalGcdRunTask);
+}
+
+}  // namespace
+#endif
 
 int ResolveHostname(const std::string& hostname,
                     int family,
@@ -152,8 +172,7 @@ void AsyncResolver::Start(const SocketAddress& addr) {
         }
       };
 #if defined(WEBRTC_MAC) || defined(WEBRTC_IOS)
-  webrtc::PostTaskToGlobalQueue(
-      webrtc::ToQueuedTask(std::move(thread_function)));
+  PostTaskToGlobalQueue(webrtc::ToQueuedTask(std::move(thread_function)));
 #else
   PlatformThread::SpawnDetached(std::move(thread_function), "AsyncResolver");
 #endif
