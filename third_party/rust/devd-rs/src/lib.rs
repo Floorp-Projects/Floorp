616733a444bace95bf5807dea05cc4c6c2f6abac
Mike Hommey <mh+mozilla@glandium.org>
2022-06-11 20:34:18 +0000

    Bug 1773399 - Update devd-rs to 0.3.4. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D148735

--- third_party/rust/devd-rs/src/lib.rs
+++ third_party/rust/devd-rs/src/lib.rs
@@ -1,13 +1,10 @@
-extern crate libc;
-#[macro_use]
-extern crate nom;
-
 pub mod data;
 pub mod parser;
 pub mod result;
 
 use io::{BufRead, BufReader};
 use libc::{c_int, connect, nfds_t, poll, pollfd, sockaddr_un, socket, AF_UNIX, POLLIN, SOCK_SEQPACKET};
+use std::cmp::Ordering;
 use std::os::unix::io::{FromRawFd, RawFd};
 use std::os::unix::net::UnixStream;
 use std::{io, mem, ptr};
@@ -15,10 +12,10 @@ use std::{io, mem, ptr};
 pub use data::*;
 pub use result::*;
 
-const SOCKET_PATH: &'static str = "/var/run/devd.seqpacket.pipe";
+const SOCKET_PATH: &str = "/var/run/devd.seqpacket.pipe";
 
-pub fn parse_devd_event(e: String) -> Result<Event> {
-    match parser::event(e.as_str()) {
+pub fn parse_devd_event(event: &str) -> Result<Event> {
+    match parser::event(event) {
         Ok((_, x)) => Ok(x),
         _ => Err(Error::Parse),
     }
@@ -28,6 +25,7 @@ pub fn parse_devd_event(e: String) -> Result<Event> {
 pub struct Context {
     sock: BufReader<UnixStream>,
     sockfd: RawFd,
+    buffer: String,
 }
 
 impl Context {
@@ -39,34 +37,45 @@ impl Context {
             }
             let mut sockaddr = sockaddr_un { sun_family: AF_UNIX as _, ..mem::zeroed() };
             ptr::copy_nonoverlapping(SOCKET_PATH.as_ptr(), sockaddr.sun_path.as_mut_ptr() as *mut u8, SOCKET_PATH.len());
-            if connect(sockfd, &sockaddr as *const sockaddr_un as *const _, (mem::size_of_val(&AF_UNIX) + SOCKET_PATH.len()) as _) < 0 {
+            if connect(
+                sockfd,
+                &sockaddr as *const sockaddr_un as *const _,
+                (mem::size_of_val(&AF_UNIX) + SOCKET_PATH.len()) as _,
+            ) < 0
+            {
                 return Err(io::Error::last_os_error().into());
             }
             Ok(Context {
                 sock: BufReader::new(UnixStream::from_raw_fd(sockfd)),
-                sockfd: sockfd,
+                sockfd,
+                buffer: String::new(),
             })
+        }
+    }
+
+    pub fn wait_for_event_raw_internal(&mut self, timeout_ms: usize) -> Result<&str> {
+        let mut fds = [pollfd { fd: self.sockfd, events: POLLIN, revents: 0 }];
+        let x = unsafe { poll((&mut fds).as_mut_ptr(), fds.len() as nfds_t, timeout_ms as c_int) };
+
+        match x.cmp(&0) {
+            Ordering::Less => Err(io::Error::last_os_error().into()),
+            Ordering::Equal => Err(Error::Timeout),
+            Ordering::Greater => {
+                self.buffer.clear();
+                self.sock.read_line(&mut self.buffer)?;
+                Ok(&self.buffer)
+            }
         }
     }
 
     /// Waits for an event using poll(), reads it but does not parse
     pub fn wait_for_event_raw(&mut self, timeout_ms: usize) -> Result<String> {
-        let mut fds = vec![pollfd { fd: self.sockfd, events: POLLIN, revents: 0 }];
-        let x = unsafe { poll((&mut fds).as_mut_ptr(), fds.len() as nfds_t, timeout_ms as c_int) };
-        if x < 0 {
-            Err(io::Error::last_os_error().into())
-        } else if x == 0 {
-            Err(Error::Timeout)
-        } else {
-            let mut s = String::new();
-            let _ = self.sock.read_line(&mut s);
-            Ok(s)
-        }
+        self.wait_for_event_raw_internal(timeout_ms).map(ToOwned::to_owned)
     }
 
     /// Waits for an event using poll(), reads and parses it
-    pub fn wait_for_event<'a>(&mut self, timeout_ms: usize) -> Result<Event> {
-        self.wait_for_event_raw(timeout_ms).and_then(parse_devd_event)
+    pub fn wait_for_event(&mut self, timeout_ms: usize) -> Result<Event> {
+        self.wait_for_event_raw_internal(timeout_ms).and_then(parse_devd_event)
     }
 
     /// Returns the devd socket file descriptor in case you want to select/poll on it together with
@@ -77,8 +86,8 @@ impl Context {
 
     /// Reads an event and parses it. Use when polling on the raw fd by yourself
     pub fn read_event(&mut self) -> Result<Event> {
-        let mut s = String::new();
-        let _ = self.sock.read_line(&mut s);
-        parse_devd_event(s)
+        self.buffer.clear();
+        self.sock.read_line(&mut self.buffer)?;
+        parse_devd_event(&self.buffer)
     }
 }
