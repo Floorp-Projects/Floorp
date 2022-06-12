fb8454bd6a0fb1472445806f0d42306dd720f13f
Michael Froman <mfroman@mozilla.com>
2022-06-11 21:38:52 -0500

    Bug 1766646 - Vendor libwebrtc from 3395107778
    Upstream commit: https://webrtc.googlesource.com/src/+/3395107778989ea66dedd047e8e2d2a16199056c
        Remove old definition of the macro RTC_NOTREACHED
    
        Bug: webrtc:9065
        Change-Id: I3cb166f217893808af5708ede69abaebb5bde28e
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/237809
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Artem Titov <titovartem@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#35356}

--- third_party/libwebrtc/rtc_base/checks.h
+++ third_party/libwebrtc/rtc_base/checks.h
@@ -445,8 +445,6 @@ RTC_NORETURN RTC_EXPORT void UnreachableCodeReached();
 
 #define RTC_UNREACHABLE_CODE_HIT false
 #define RTC_DCHECK_NOTREACHED() RTC_DCHECK(RTC_UNREACHABLE_CODE_HIT)
-// Deprecated. Use RTC_DCHECK_NOTREACHED instead
-#define RTC_NOTREACHED() RTC_DCHECK(RTC_UNREACHABLE_CODE_HIT)
 
 // Kills the process with an error message. Never returns. Use when you wish to
 // assert that a point in the code is never reached.
