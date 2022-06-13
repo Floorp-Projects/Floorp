18c9cc727481d0de51a1f769ca0e085fc518b924
Boris Chiou <boris.chiou@gmail.com>
2022-06-13 20:26:45 +0000

    Bug 1754897 - Part 4: Hook named scroll timelines to animation-timeline. r=emilio
    Basically, animation-timeline could be
    1. auto
    2. none
    3. <timeline-name>
    
    We extend the <timeline-name> to cover both @scroll-timeline rule and
    scroll-timeline-name property. We check @scroll-timeline rule first. If
    it doesn't exist, we check scroll-timeline-name of the element itself,
    the previous silbings, and their ancestors.
    
    Differential Revision: https://phabricator.services.mozilla.com/D146358

--- layout/style/nsAnimationManager.cpp
+++ layout/style/nsAnimationManager.cpp
@@ -215,16 +215,18 @@ static already_AddRefed<dom::AnimationTimeline> GetTimeline(
         // That's how we represent `none`.
         return nullptr;
       }
-      const auto* rule =
-          aPresContext->StyleSet()->ScrollTimelineRuleForName(name);
-      if (!rule) {
-        // Unknown timeline, so treat is as no timeline. Keep nullptr.
-        return nullptr;
+      // 1. Check @scroll-timeline rule.
+      if (const auto* rule =
+              aPresContext->StyleSet()->ScrollTimelineRuleForName(name)) {
+        // We do intentionally use the pres context's document for the owner of
+        // ScrollTimeline since it's consistent with what we do for
+        // KeyframeEffect instance.
+        return ScrollTimeline::FromRule(*rule, aPresContext->Document(),
+                                        aTarget);
       }
-      // We do intentionally use the pres context's document for the owner of
-      // ScrollTimeline since it's consistent with what we do for
-      // KeyframeEffect instance.
-      return ScrollTimeline::FromRule(*rule, aPresContext->Document(), aTarget);
+      // 2. Check scroll-timeline-name property.
+      return ScrollTimeline::FromNamedScroll(aPresContext->Document(), aTarget,
+                                             name);
     }
     case StyleAnimationTimeline::Tag::Scroll: {
       const auto& scroll = aStyleTimeline.AsScroll();
