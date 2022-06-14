93e710eb49e766948fb3bf83f69d6a7127f85c1b
Jon Coppeard <jcoppeard@mozilla.com>
2022-06-14 12:30:12 +0000

    Bug 1773778 - Part 12: Remove deprecated WeakHeapPtrDebugEnvironmentProxy typedef r=tcampbell
    Depends on D149121
    
    Differential Revision: https://phabricator.services.mozilla.com/D149122

--- js/src/gc/Barrier.h
+++ js/src/gc/Barrier.h
@@ -1234,13 +1234,10 @@ struct DefaultHasher<js::UnsafeBarePtr<T>> : js::UnsafeBarePtrHasher<T> {};
 
 namespace js {
 
-class DebugEnvironmentProxy;
-
 namespace jit {
 class JitCode;
 }  // namespace jit
 
-using WeakHeapPtrDebugEnvironmentProxy = WeakHeapPtr<DebugEnvironmentProxy*>;
 using WeakHeapPtrJitCode = WeakHeapPtr<jit::JitCode*>;
 using WeakHeapPtrSymbol = WeakHeapPtr<JS::Symbol*>;
 
