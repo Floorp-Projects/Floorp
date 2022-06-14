f8e1a5aabc8a8ee80bdd866c37047c23a82a8f92
Jon Coppeard <jcoppeard@mozilla.com>
2022-06-14 12:30:08 +0000

    Bug 1773778 - Part 2: Remove deprecated PreBarrieredId typedef r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D149112

--- js/src/gc/Barrier.h
+++ js/src/gc/Barrier.h
@@ -1241,7 +1241,6 @@ namespace jit {
 class JitCode;
 }  // namespace jit
 
-using PreBarrieredId = PreBarriered<jsid>;
 using PreBarrieredValue = PreBarriered<Value>;
 
 using GCPtrAtom = GCPtr<JSAtom*>;
