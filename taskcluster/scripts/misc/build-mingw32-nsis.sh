3195e73f24e3a156ce9a3e4b92a2dfdc2f458703
Mike Hommey <mh+mozilla@glandium.org>
2022-06-15 07:04:41 +0000

    Bug 1773221 - Link NSIS stubs with /fixed (aka disable relocations). r=firefox-build-system-reviewers,ahochheiden
    Differential Revision: https://phabricator.services.mozilla.com/D148882

--- taskcluster/scripts/misc/build-mingw32-nsis.sh
+++ taskcluster/scripts/misc/build-mingw32-nsis.sh
@@ -34,8 +34,15 @@ make -f win32/Makefile.gcc PREFIX=i686-w64-mingw32-
 cd ../nsis-3.07-src
 patch -p1 < $GECKO_PATH/build/win32/nsis-no-insert-timestamp.patch
 patch -p1 < $GECKO_PATH/build/win32/nsis-no-underscore.patch
-# --exclude-libs is not supported by lld, but is not required anyways
-sed -i 's/ -Wl,--exclude-libs,msvcrt.a//' SCons/Config/gnu
+# --exclude-libs is not supported by lld, but is not required anyways.
+# /fixed is passed by the build system when building with MSVC but not
+# when building with GCC/binutils. The build system doesn't really support
+# clang/lld, but apparently binutils and lld don't have the same defaults
+# related to this. Unfortunately, /fixed is necessary for the stubs to be
+# handled properly by the resource editor in NSIS, which doesn't handle
+# relocations, so we pass the equivalent flag to lld-link through lld through
+# clang.
+sed -i 's/-Wl,--exclude-libs,msvcrt.a/-Wl,-Xlink=-fixed/' SCons/Config/gnu
 # memcpy.c and memset.c are built with a C++ compiler so we need to
 # avoid their symbols being mangled.
 sed -i '2i extern "C"' SCons/Config/{memcpy,memset}.c
