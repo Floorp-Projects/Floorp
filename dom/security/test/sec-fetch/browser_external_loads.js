7d4052699239bcfc8dbef11f0036abf11b80b712
Niklas Goegge <ngogge@mozilla.com>
2022-06-15 19:01:02 +0000

    Bug 1738694: Add sec-fetch tests for openening new windows. r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D130860

--- dom/security/test/sec-fetch/browser_external_loads.js
+++ dom/security/test/sec-fetch/browser_external_loads.js
@@ -1,6 +1,11 @@
 "use strict";
 
-let gExpectedHeader = {};
+const TEST_PATH = getRootDirectory(gTestPath).replace(
+  "chrome://mochitests/content",
+  "https://example.com"
+);
+
+var gExpectedHeader = {};
 
 function checkSecFetchUser(subject, topic, data) {
   let channel = subject.QueryInterface(Ci.nsIHttpChannel);
@@ -27,7 +32,7 @@ function checkSecFetchUser(subject, topic, data) {
       );
     } catch (e) {
       if (expectedValue) {
-        ok(false, "required headers are set");
+        ok(false, `${header} should be set`);
       } else {
         ok(true, `${header} should not be set`);
       }
@@ -37,7 +42,10 @@ function checkSecFetchUser(subject, topic, data) {
 
 add_task(async function external_load() {
   waitForExplicitFinish();
+  Services.obs.addObserver(checkSecFetchUser, "http-on-stop-request");
 
+  // Simulate an external load in the *current* window with
+  // Ci.nsIBrowserDOMWindow.OPEN_EXTERNAL and the system principal.
   gExpectedHeader = {
     "sec-fetch-site": "none",
     "sec-fetch-mode": "navigate",
@@ -45,19 +53,105 @@ add_task(async function external_load() {
     "sec-fetch-user": "?1",
   };
 
-  Services.obs.addObserver(checkSecFetchUser, "http-on-stop-request");
-
   let loaded = BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
-  // Simulate an external load with Ci.nsIBrowserDOMWindow.OPEN_EXTERNAL and
-  // the system principal.
   window.browserDOMWindow.openURI(
-    makeURI("https://example.com"),
+    makeURI(`${TEST_PATH}file_dummy_link.html`),
     null,
     Ci.nsIBrowserDOMWindow.OPEN_CURRENTWINDOW,
     Ci.nsIBrowserDOMWindow.OPEN_EXTERNAL,
     Services.scriptSecurityManager.getSystemPrincipal()
   );
   await loaded;
+
+  // Open a link in a *new* window through the context menu.
+  gExpectedHeader = {
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-dest": "document",
+    "sec-fetch-user": "?1",
+  };
+
+  loaded = BrowserTestUtils.waitForNewWindow({
+    url: `${TEST_PATH}file_dummy_link_location.html`,
+  });
+  BrowserTestUtils.waitForEvent(document, "popupshown", false, event => {
+    document.getElementById("context-openlink").doCommand();
+    event.target.hidePopup();
+    return true;
+  });
+  BrowserTestUtils.synthesizeMouseAtCenter(
+    "#dummylink",
+    { type: "contextmenu", button: 2 },
+    gBrowser.selectedBrowser
+  );
+
+  let win = await loaded;
+  win.close();
+
+  // Simulate an external load in a *new* window with
+  // Ci.nsIBrowserDOMWindow.OPEN_EXTERNAL and the system principal.
+  gExpectedHeader = {
+    "sec-fetch-site": "none",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-dest": "document",
+    "sec-fetch-user": "?1",
+  };
+
+  loaded = BrowserTestUtils.waitForNewWindow({
+    url: "https://example.com/newwindow",
+  });
+  window.browserDOMWindow.openURI(
+    makeURI("https://example.com/newwindow"),
+    null,
+    Ci.nsIBrowserDOMWindow.OPEN_NEWWINDOW,
+    Ci.nsIBrowserDOMWindow.OPEN_EXTERNAL,
+    Services.scriptSecurityManager.getSystemPrincipal()
+  );
+  win = await loaded;
+  win.close();
+
+  // Open a *new* window through window.open without user activation.
+  gExpectedHeader = {
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-dest": "document",
+  };
+
+  loaded = BrowserTestUtils.waitForNewWindow({
+    url: "https://example.com/windowopen",
+  });
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [], () => {
+    content.window.open(
+      "https://example.com/windowopen",
+      "_blank",
+      "height=500,width=500"
+    );
+  });
+  win = await loaded;
+  win.close();
+
+  // Open a *new* window through window.open with user activation.
+  gExpectedHeader = {
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-dest": "document",
+    "sec-fetch-user": "?1",
+  };
+
+  loaded = BrowserTestUtils.waitForNewWindow({
+    url: "https://example.com/windowopen_withactivation",
+  });
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [], () => {
+    content.document.notifyUserGestureActivation();
+    content.window.open(
+      "https://example.com/windowopen_withactivation",
+      "_blank",
+      "height=500,width=500"
+    );
+    content.document.clearUserGestureActivation();
+  });
+  win = await loaded;
+  win.close();
 
   Services.obs.removeObserver(checkSecFetchUser, "http-on-stop-request");
   finish();
