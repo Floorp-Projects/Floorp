5e1b5eeebac247f037b6cd3364c0dbb6499cb00f
Mike Hommey <mh+mozilla@glandium.org>
2022-06-15 21:26:20 +0000

    Bug 1773399 - Update getrandom to 0.2.7. r=emilio,supply-chain-reviewers,bholley
    Differential Revision: https://phabricator.services.mozilla.com/D149326

--- third_party/rust/getrandom/src/wasi.rs
+++ third_party/rust/getrandom/src/wasi.rs
@@ -9,15 +9,11 @@
 //! Implementation for WASI
 use crate::Error;
 use core::num::NonZeroU32;
-use wasi::random_get;
+use wasi::wasi_snapshot_preview1::random_get;
 
 pub fn getrandom_inner(dest: &mut [u8]) -> Result<(), Error> {
-    unsafe {
-        random_get(dest.as_mut_ptr(), dest.len()).map_err(|e: wasi::Error| {
-            // convert wasi's Error into getrandom's NonZeroU32 error
-            // SAFETY: `wasi::Error` is `NonZeroU16` internally, so `e.raw_error()`
-            // will never return 0
-            NonZeroU32::new_unchecked(e.raw_error() as u32).into()
-        })
+    match unsafe { random_get(dest.as_mut_ptr() as i32, dest.len() as i32) } {
+        0 => Ok(()),
+        err => Err(unsafe { NonZeroU32::new_unchecked(err as u32) }.into()),
     }
 }
