9427886586fce5277098a8d0328a33454e2ae9f7
Marian-Vasile Laza <mlaza@mozilla.com>
2022-06-16 15:53:24 +0300

    Backed out changeset d70914c7f1f3 (bug 1730763) for causing web-platform reftest failures on direction-propagation-body-contain-root.html. CLOSED TREE

--- layout/painting/nsCSSRendering.cpp
+++ layout/painting/nsCSSRendering.cpp
@@ -1188,12 +1188,12 @@ nsIFrame* nsCSSRendering::FindBackgroundStyleFrame(nsIFrame* aForFrame) {
   // through to the content sink, which will call |StartLayout|
   // and thus |Initialize| on the pres shell.  See bug 119351
   // for the ugly details.
-  if (!bodyContent || aForFrame->StyleDisplay()->IsContainAny()) {
+  if (!bodyContent) {
     return aForFrame;
   }
 
   nsIFrame* bodyFrame = bodyContent->GetPrimaryFrame();
-  if (!bodyFrame || bodyFrame->StyleDisplay()->IsContainAny()) {
+  if (!bodyFrame) {
     return aForFrame;
   }
 
@@ -1242,29 +1242,26 @@ inline bool FindElementBackground(const nsIFrame* aForFrame,
   // was propagated to the viewport.
 
   nsIContent* content = aForFrame->GetContent();
-  if (!content || content->NodeInfo()->NameAtom() != nsGkAtoms::body) {
+  if (!content || content->NodeInfo()->NameAtom() != nsGkAtoms::body)
     return true;  // not frame for a "body" element
-  }
   // It could be a non-HTML "body" element but that's OK, we'd fail the
   // bodyContent check below
 
-  if (aForFrame->Style()->GetPseudoType() != PseudoStyleType::NotPseudo ||
-      aForFrame->StyleDisplay()->IsContainAny()) {
-    return true;  // A pseudo-element frame, or contained.
+  if (aForFrame->Style()->GetPseudoType() != PseudoStyleType::NotPseudo) {
+    return true;  // A pseudo-element frame.
   }
 
   // We should only look at the <html> background if we're in an HTML document
   Document* document = content->OwnerDoc();
 
   dom::Element* bodyContent = document->GetBodyElement();
-  if (bodyContent != content) {
+  if (bodyContent != content)
     return true;  // this wasn't the background that was propagated
-  }
 
   // This can be called even when there's no root element yet, during frame
   // construction, via nsLayoutUtils::FrameHasTransparency and
   // nsContainerFrame::SyncFrameViewProperties.
-  if (!aRootElementFrame || aRootElementFrame->StyleDisplay()->IsContainAny()) {
+  if (!aRootElementFrame) {
     return true;
   }
 
