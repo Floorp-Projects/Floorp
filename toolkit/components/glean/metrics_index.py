bf6385af57f0743d52c2114364076c9b80518a44
Travis Long <tlong@mozilla.com>
2022-06-17 19:12:32 +0000

    Bug 1773563 - Add Glean equivalents for current legacy telemetry events used by Nimbus r=barret
    Differential Revision: https://phabricator.services.mozilla.com/D149658

--- toolkit/components/glean/metrics_index.py
+++ toolkit/components/glean/metrics_index.py
@@ -21,6 +21,7 @@ metrics_yamls = [
     "toolkit/components/extensions/metrics.yaml",
     "toolkit/components/glean/metrics.yaml",
     "toolkit/components/glean/tests/test_metrics.yaml",
+    "toolkit/components/nimbus/metrics.yaml",
     "toolkit/components/processtools/metrics.yaml",
     "toolkit/components/search/metrics.yaml",
     "toolkit/components/telemetry/metrics.yaml",
