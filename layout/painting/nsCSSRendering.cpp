303d7f85fdd14abae7604e6052cf81f5d7f780cd
Daniel Holbert <dholbert@cs.stanford.edu>
2022-06-18 00:45:20 +0000

    Bug 1774879 part 4: Use const ComputedStyle* arg in nsCSSRendering::PaintStyleImageLayerWithSC, and update several callsites now that they can pass const variables. r=emilio
    This patch doesn't change behavior; it's just a const-correctness fix.
    
    Thanks to previous patches in this series, PaintStyleImageLayerWithSC's arg can
    become const, so let's make that change and opportunistically make the same
    change to the variables that callers pass in, where possible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149677

--- layout/painting/nsCSSRendering.cpp
+++ layout/painting/nsCSSRendering.cpp
@@ -1796,7 +1796,7 @@ ImgDrawResult nsCSSRendering::PaintStyleImageLayer(const PaintBGParams& aParams,
   MOZ_ASSERT(aParams.frame,
              "Frame is expected to be provided to PaintStyleImageLayer");
 
-  ComputedStyle* sc = FindBackground(aParams.frame);
+  const ComputedStyle* sc = FindBackground(aParams.frame);
   if (!sc) {
     // We don't want to bail out if moz-appearance is set on a root
     // node. If it has a parent content node, bail because it's not
@@ -2387,7 +2387,7 @@ static CompositionOp DetermineCompositionOp(
 
 ImgDrawResult nsCSSRendering::PaintStyleImageLayerWithSC(
     const PaintBGParams& aParams, gfxContext& aRenderingCtx,
-    ComputedStyle* aBackgroundSC, const nsStyleBorder& aBorder) {
+    const ComputedStyle* aBackgroundSC, const nsStyleBorder& aBorder) {
   MOZ_ASSERT(aParams.frame,
              "Frame is expected to be provided to PaintStyleImageLayerWithSC");
 
