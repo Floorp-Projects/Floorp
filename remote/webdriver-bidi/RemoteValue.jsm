2d613c83fca12ac3fb33915efeb493a43dd5749e
Alexandra Borovova <aborovova@mozilla.com>
2022-06-20 07:36:42 +0000

    Bug 1770751 - Add support for deserialization of complex objects with simple value fields. r=webdriver-reviewers,whimboo,jdescottes
    Depends on D149407
    
    Differential Revision: https://phabricator.services.mozilla.com/D148901

--- remote/webdriver-bidi/RemoteValue.jsm
+++ remote/webdriver-bidi/RemoteValue.jsm
@@ -23,6 +23,110 @@ XPCOMUtils.defineLazyGetter(lazy, "logger", () =>
 );
 
 /**
+ * Helper to validate if a date string follows Date Time String format.
+ *
+ * @see https://tc39.es/ecma262/#sec-date-time-string-format
+ *
+ * @param {string} dateString
+ *     String which needs to be validated.
+ *
+ * @throws {InvalidArgumentError}
+ *     If <var>dateString</var> doesn't follow the format.
+ */
+function checkDateTimeString(dateString) {
+  // Check if a date string follows a simplification of
+  // the ISO 8601 calendar date extended format.
+  const expandedYear = "[+-]\\d{6}";
+  const year = "\\d{4}";
+  const YYYY = `${expandedYear}|${year}`;
+  const MM = "\\d{2}";
+  const DD = "\\d{2}";
+  const date = `${YYYY}(?:-${MM})?(?:-${DD})?`;
+  const HH_mm = "\\d{2}:\\d{2}";
+  const SS = "\\d{2}";
+  const sss = "\\d{3}";
+  const TZ = `Z|[+-]${HH_mm}`;
+  const time = `T${HH_mm}(?::${SS}(?:\\.${sss})?(?:${TZ})?)?`;
+  const iso8601Format = new RegExp(`^${date}(?:${time})?$`);
+
+  // Check also if a date string is a valid date.
+  if (Number.isNaN(Date.parse(dateString)) || !iso8601Format.test(dateString)) {
+    throw new lazy.InvalidArgumentError(
+      `Expected "value" for Date to be a Date Time string, got ${dateString}`
+    );
+  }
+}
+
+/**
+ * Helper to deserialize value list.
+ *
+ * @see https://w3c.github.io/webdriver-bidi/#deserialize-value-list
+ *
+ * @param {Array} serializedValueList
+ *     List of serialized values.
+ *
+ * @return {Array} List of deserialized values.
+ *
+ * @throws {InvalidArgumentError}
+ *     If <var>serializedValueList</var> is not an array.
+ */
+function deserializeValueList(/* realm, */ serializedValueList) {
+  lazy.assert.array(
+    serializedValueList,
+    `Expected "serializedValueList" to be an array, got ${serializedValueList}`
+  );
+
+  const deserializedValues = [];
+
+  for (const item of serializedValueList) {
+    deserializedValues.push(deserialize(/*realm, */ item));
+  }
+
+  return deserializedValues;
+}
+
+/**
+ * Helper to deserialize key-value list.
+ *
+ * @see https://w3c.github.io/webdriver-bidi/#deserialize-key-value-list
+ *
+ * @param {Array} serializedKeyValueList
+ *     List of serialized key-value.
+ *
+ * @return {Array} List of deserialized key-value.
+ *
+ * @throws {InvalidArgumentError}
+ *     If <var>serializedKeyValueList</var> is not an array or
+ *     not an array of key-value arrays.
+ */
+function deserializeKeyValueList(/* realm, */ serializedKeyValueList) {
+  lazy.assert.array(
+    serializedKeyValueList,
+    `Expected "serializedKeyValueList" to be an array, got ${serializedKeyValueList}`
+  );
+
+  const deserializedKeyValueList = [];
+
+  for (const serializedKeyValue of serializedKeyValueList) {
+    if (!Array.isArray(serializedKeyValue) || serializedKeyValue.length != 2) {
+      throw new lazy.InvalidArgumentError(
+        `Expected key-value pair to be an array with 2 elements, got ${serializedKeyValue}`
+      );
+    }
+    const [serializedKey, serializedValue] = serializedKeyValue;
+    const deserializedKey =
+      typeof serializedKey == "string"
+        ? serializedKey
+        : deserialize(/* realm, */ serializedKey);
+    const deserializedValue = deserialize(/* realm, */ serializedValue);
+
+    deserializedKeyValueList.push([deserializedKey, deserializedValue]);
+  }
+
+  return deserializedKeyValueList;
+}
+
+/**
  * Deserialize a local value.
  *
  * @see https://w3c.github.io/webdriver-bidi/#deserialize-local-value
@@ -32,7 +136,7 @@ XPCOMUtils.defineLazyGetter(lazy, "logger", () =>
  *
  * @returns {Object} Deserialized representation of the value.
  */
-function deserialize(serializedValue) {
+function deserialize(/* realm, */ serializedValue) {
   const { objectId, type, value } = serializedValue;
 
   if (type !== undefined) {
@@ -92,6 +196,45 @@ function deserialize(serializedValue) {
           `Failed to deserialize value as BigInt: ${value}`
         );
       }
+
+    // Non-primitive protocol values
+    case "array":
+      return deserializeValueList(/* realm, */ value);
+    case "date":
+      // We want to support only Date Time String format,
+      // check if the value follows it.
+      checkDateTimeString(value);
+
+      return new Date(value);
+    case "map":
+      return new Map(deserializeKeyValueList(value));
+    case "object":
+      return Object.fromEntries(deserializeKeyValueList(value));
+    case "regexp":
+      lazy.assert.object(
+        value,
+        `Expected "value" for RegExp to be an object, got ${value}`
+      );
+      const { pattern, flags } = value;
+      lazy.assert.string(
+        pattern,
+        `Expected "pattern" for RegExp to be a string, got ${pattern}`
+      );
+      if (flags !== undefined) {
+        lazy.assert.string(
+          flags,
+          `Expected "flags" for RegExp to be a string, got ${flags}`
+        );
+      }
+      try {
+        return new RegExp(pattern, flags);
+      } catch (e) {
+        throw new lazy.InvalidArgumentError(
+          `Failed to deserialize value as RegExp: ${value}`
+        );
+      }
+    case "set":
+      return new Set(deserializeValueList(/* realm, */ value));
   }
 
   lazy.logger.warn(`Unsupported type for local value ${type}`);
--- remote/webdriver-bidi/test/xpcshell/test_RemoteValue.js
+++ remote/webdriver-bidi/test/xpcshell/test_RemoteValue.js
@@ -24,6 +24,17 @@ const PRIMITIVE_TYPES = [
 
 const REMOTE_SIMPLE_VALUES = [
   {
+    value: new RegExp(/foo/),
+    serialized: {
+      type: "regexp",
+      value: {
+        pattern: "foo",
+        flags: "",
+      },
+    },
+    deserializable: true,
+  },
+  {
     value: new RegExp(/foo/g),
     serialized: {
       type: "regexp",
@@ -32,6 +43,7 @@ const REMOTE_SIMPLE_VALUES = [
         flags: "g",
       },
     },
+    deserializable: true,
   },
   {
     value: new Date(1654004849000),
@@ -39,6 +51,7 @@ const REMOTE_SIMPLE_VALUES = [
       type: "date",
       value: "2022-05-31T13:47:29.000Z",
     },
+    deserializable: true,
   },
 ];
 
@@ -74,6 +87,7 @@ const REMOTE_COMPLEX_VALUES = [
         },
       ],
     },
+    deserializable: true,
   },
   {
     value: [1, [3, "4"]],
@@ -99,6 +113,7 @@ const REMOTE_COMPLEX_VALUES = [
         },
       ],
     },
+    deserializable: true,
   },
   {
     value: new Map(),
@@ -107,6 +122,7 @@ const REMOTE_COMPLEX_VALUES = [
       type: "map",
       value: [],
     },
+    deserializable: true,
   },
   {
     value: new Map([]),
@@ -115,6 +131,7 @@ const REMOTE_COMPLEX_VALUES = [
       type: "map",
       value: [],
     },
+    deserializable: true,
   },
   {
     value: new Map([
@@ -158,6 +175,7 @@ const REMOTE_COMPLEX_VALUES = [
         ],
       ],
     },
+    deserializable: true,
   },
   {
     value: new Set(),
@@ -166,6 +184,7 @@ const REMOTE_COMPLEX_VALUES = [
       type: "set",
       value: [],
     },
+    deserializable: true,
   },
   {
     value: new Set([]),
@@ -174,6 +193,7 @@ const REMOTE_COMPLEX_VALUES = [
       type: "set",
       value: [],
     },
+    deserializable: true,
   },
   {
     value: new Set([1, "2", true]),
@@ -199,6 +219,7 @@ const REMOTE_COMPLEX_VALUES = [
         { type: "boolean", value: true },
       ],
     },
+    deserializable: true,
   },
 ];
 
@@ -209,9 +230,9 @@ const { deserialize, serialize } = ChromeUtils.import(
 add_test(function test_deserializePrimitiveTypes() {
   for (const type of PRIMITIVE_TYPES) {
     const { value: expectedValue, serialized } = type;
-    const value = deserialize(serialized);
 
-    info(`Checking '${type}'`);
+    info(`Checking '${serialized.type}'`);
+    const value = deserialize(serialized);
 
     if (serialized.value == "NaN") {
       ok(Number.isNaN(value), `Got expected value for ${serialized}`);
@@ -227,6 +248,82 @@ add_test(function test_deserializePrimitiveTypes() {
   run_next_test();
 });
 
+add_test(function test_deserializeDateLocalValue() {
+  const validaDateStrings = [
+    "2009",
+    "2009-05",
+    "2009-05-19",
+    "2009T15:00",
+    "2009-05T15:00",
+    "2009-05-19T15:00",
+    "2009-05-19T15:00:15",
+    "2009-05-19T15:00:15.452",
+    "2009-05-19T15:00:15.452Z",
+    "2009-05-19T15:00:15.452+02:00",
+    "2009-05-19T15:00:15.452-02:00",
+    "-271821-04-20T00:00:00Z",
+    "+000000-01-01T00:00:00Z",
+  ];
+  for (const dateString of validaDateStrings) {
+    info(`Checking '${dateString}'`);
+    const value = deserialize({ type: "date", value: dateString });
+
+    Assert.equal(
+      value.getTime(),
+      new Date(dateString).getTime(),
+      `Got expected value for ${dateString}`
+    );
+  }
+
+  run_next_test();
+});
+
+add_test(function test_deserializeLocalValues() {
+  for (const type of REMOTE_SIMPLE_VALUES.concat(REMOTE_COMPLEX_VALUES)) {
+    const { value: expectedValue, serialized, deserializable } = type;
+
+    // Skip non deserializable cases
+    if (!deserializable) {
+      continue;
+    }
+
+    info(`Checking '${serialized.type}'`);
+    const value = deserialize(serialized);
+
+    let formattedValue = value;
+    let formattedExpectedValue = expectedValue;
+
+    // Format certain types for easier assertion
+    if (serialized.type == "map") {
+      Assert.equal(
+        Object.prototype.toString.call(expectedValue),
+        "[object Map]",
+        "Got expected type Map"
+      );
+
+      formattedValue = Array.from(value.values());
+      formattedExpectedValue = Array.from(expectedValue.values());
+    } else if (serialized.type == "set") {
+      Assert.equal(
+        Object.prototype.toString.call(expectedValue),
+        "[object Set]",
+        "Got expected type Set"
+      );
+
+      formattedValue = Array.from(value);
+      formattedExpectedValue = Array.from(expectedValue);
+    }
+
+    Assert.deepEqual(
+      formattedValue,
+      formattedExpectedValue,
+      "Got expected structure"
+    );
+  }
+
+  run_next_test();
+});
+
 add_test(function test_deserializePrimitiveTypesInvalidValues() {
   const invalidValues = [
     { type: "bigint", values: [undefined, null, false, "foo", [], {}] },
@@ -236,6 +333,145 @@ add_test(function test_deserializePrimitiveTypesInvalidValues() {
       values: [undefined, null, false, "43", [], {}],
     },
     { type: "string", values: [undefined, null, false, 42, [], {}] },
+  ];
+
+  for (const invalidValue of invalidValues) {
+    const { type, values } = invalidValue;
+
+    for (const value of values) {
+      info(`Checking '${type}' with value ${value}`);
+
+      Assert.throws(
+        () => deserialize({ type, value }),
+        /InvalidArgument/,
+        `Got expected error for type ${type} and value ${value}`
+      );
+    }
+  }
+
+  run_next_test();
+});
+
+add_test(function test_deserializeDateLocalValueInvalidValues() {
+  const invalidaDateStrings = [
+    "10",
+    "20009",
+    "+20009",
+    "2009-",
+    "2009-0",
+    "2009-15",
+    "2009-02-1",
+    "2009-02-50",
+    "2022-02-29",
+    "15:00",
+    "T15:00",
+    "9-05-19T15:00",
+    "2009-5-19T15:00",
+    "2009-05-1T15:00",
+    "2009-02-10T15",
+    "2009-05-19T15:",
+    "2009-05-19T1:00",
+    "2009-05-19T10:1",
+    "2022-06-31T15:00",
+    "2009-05-19T60:00",
+    "2009-05-19T15:70",
+    "2009-05-19T15:00.25",
+    "2009-05-19+10:00",
+    "2009-05-19Z",
+    "2009-05-19 15:00",
+    "2009-05-19t15:00Z",
+    "2009-05-19T15:00z",
+    "2009-05-19T15:00+01",
+    "2009-05-19T10:10+1:00",
+    "2009-05-19T10:10+01:1",
+    "2009-05-19T15:00+75:00",
+    "2009-05-19T15:00+02:80",
+    "2009-05-19T15:00-00:00",
+    "02009-05-19T15:00",
+  ];
+  for (const dateString of invalidaDateStrings) {
+    info(`Checking '${dateString}'`);
+
+    Assert.throws(
+      () => deserialize({ type: "date", value: dateString }),
+      /InvalidArgument/,
+      `Got expected error for date string: ${dateString}`
+    );
+  }
+
+  run_next_test();
+});
+
+add_test(function test_deserializeLocalValuesInvalidValues() {
+  const invalidValues = [
+    { type: "array", values: [undefined, null, false, 42, "foo", {}] },
+    {
+      type: "regexp",
+      values: [
+        undefined,
+        null,
+        false,
+        "foo",
+        42,
+        [],
+        {},
+        { pattern: null },
+        { pattern: 1 },
+        { pattern: true },
+        { pattern: "foo", flags: null },
+        { pattern: "foo", flags: 1 },
+        { pattern: "foo", flags: false },
+        { pattern: "foo", flags: "foo" },
+      ],
+    },
+    {
+      type: "date",
+      values: [
+        undefined,
+        null,
+        false,
+        "foo",
+        "05 October 2011 14:48 UTC",
+        "Tue Jun 14 2022 10:46:50 GMT+0200!",
+        42,
+        [],
+        {},
+      ],
+    },
+    {
+      type: "map",
+      values: [
+        undefined,
+        null,
+        false,
+        "foo",
+        42,
+        ["1"],
+        [[]],
+        [["1"]],
+        [{ "1": "2" }],
+        {},
+      ],
+    },
+    {
+      type: "set",
+      values: [undefined, null, false, "foo", 42, {}],
+    },
+    {
+      type: "object",
+      values: [
+        undefined,
+        null,
+        false,
+        "foo",
+        42,
+        {},
+        ["1"],
+        [[]],
+        [["1"]],
+        [{ "1": "2" }],
+      ],
+    },
   ];
 
   for (const invalidValue of invalidValues) {
