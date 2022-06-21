47e2fdc120db3c8b9342e83da3d86c3d38d4cab0
Chris H-C <chutten@mozilla.com>
2022-06-21 20:34:56 +0000

    Bug 1698184 - Test runtime registration of FOG metrics r=janerik
    Adds a test-only method to JS that permits the runtime registration of metrics.
    Also uses that to cover JOG with tests: registering and smoke-testing metrics
    of each metric type.
    (Events being a notable (temporary) exception)
    
    Instead of writing parsers, use serde_json for the optional extra metric args.
    
    Differential Revision: https://phabricator.services.mozilla.com/D143051

--- toolkit/components/glean/bindings/jog/src/lib.rs
+++ toolkit/components/glean/bindings/jog/src/lib.rs
@@ -3,7 +3,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use fog::factory;
-use fog::private::Lifetime;
+use fog::private::traits::HistogramType;
+use fog::private::{CommonMetricData, MemoryUnit, TimeUnit};
+#[cfg(feature = "with_gecko")]
+use nsstring::{nsACString, nsCString};
+use serde::Deserialize;
+use thin_vec::ThinVec;
 
 /// Activate the JOG runtime registrar.
 /// This is where the Artefact Build support happens.
@@ -11,24 +16,20 @@ use fog::private::Lifetime;
 /// returns whether it successfully found and processed metrics files.
 #[no_mangle]
 pub extern "C" fn jog_runtime_registrar() -> bool {
-    let _ = factory::create_and_register_metric(
-        "counter",
-        "category".into(),
-        "name".into(),
-        vec!["store1".into()],
-        Lifetime::Ping,
-        true,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-    );
     false
+}
+
+#[derive(Default, Deserialize)]
+struct ExtraMetricArgs {
+    time_unit: Option<TimeUnit>,
+    memory_unit: Option<MemoryUnit>,
+    allowed_extra_keys: Option<Vec<String>>,
+    range_min: Option<u64>,
+    range_max: Option<u64>,
+    bucket_count: Option<u64>,
+    histogram_type: Option<HistogramType>,
+    numerators: Option<Vec<CommonMetricData>>,
+    labels: Option<Vec<String>>,
 }
 
 /// Test-only method.
@@ -36,14 +37,65 @@ pub extern "C" fn jog_runtime_registrar() -> bool {
 /// Registers a metric.
 /// Doesn't check to see if it's been registered before.
 /// Doesn't check that it would pass schema validation if it were a real metric.
+///
+/// `extra_args` is a JSON-encoded string in a form that serde can read into an ExtraMetricArgs.
+///
+/// No effort has been made to make this pleasant to use, since it's for
+/// internal testing only (ie, the testing of JOG itself).
+#[cfg(feature = "with_gecko")]
 #[no_mangle]
-pub extern "C" fn jog_test_register_metric() {
-    // What information do we need for registration?
-    // Pretty much everything in CommonMetricData except `disabled` which we presume is `false`, so
-    // name, category, send_in_pings, lifetime
+pub extern "C" fn jog_test_register_metric(
+    metric_type: &nsACString,
+    category: &nsACString,
+    name: &nsACString,
+    send_in_pings: &ThinVec<nsCString>,
+    lifetime: &nsACString,
+    disabled: bool,
+    extra_args: &nsACString,
+) -> u32 {
+    log::warn!("Type: {:?}, Category: {:?}, Name: {:?}, SendInPings: {:?}, Lifetime: {:?}, Disabled: {}, ExtraArgs: {}",
+      metric_type, category, name, send_in_pings, lifetime, disabled, extra_args);
+    let ns_category = category;
+    let ns_name = name;
+    let metric_type = &metric_type.to_utf8();
+    let category = category.to_string();
+    let name = name.to_string();
+    let send_in_pings = send_in_pings.iter().map(|ping| ping.to_string()).collect();
+    let lifetime = serde_json::from_str(&lifetime.to_utf8())
+        .expect("Lifetime didn't deserialize happily. Is it valid JSON?");
 
-    // Then we need everything else we might need (cf glean_parser.util.extra_metric_args:
-    // time_unit, memory_unit, allowed_extra_keys, reason_codes, range_min, range_max, bucket_count, histogram_type, numerators
+    let extra_args: ExtraMetricArgs = if extra_args.is_empty() {
+        Default::default()
+    } else {
+        serde_json::from_str(&extra_args.to_utf8())
+            .expect("Extras didn't deserialize happily. Are they valid JSON?")
+    };
+    let metric = factory::create_and_register_metric(
+        metric_type,
+        category,
+        name,
+        send_in_pings,
+        lifetime,
+        disabled,
+        extra_args.time_unit,
+        extra_args.memory_unit,
+        extra_args.allowed_extra_keys,
+        extra_args.range_min,
+        extra_args.range_max,
+        extra_args.bucket_count,
+        extra_args.histogram_type,
+        extra_args.numerators,
+        extra_args.labels,
+    );
+    extern "C" {
+        fn JOG_RegisterMetric(category: &nsACString, name: &nsACString, metric: u32);
+    }
+    let metric = metric.unwrap();
+    // Safety: We're loaning to C++ the same nsCStrings they leant us.
+    unsafe {
+        JOG_RegisterMetric(ns_category, ns_name, metric);
+    }
+    metric
 }
 
 /// Test-only method.
