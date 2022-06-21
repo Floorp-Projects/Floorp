c895dbf245653f3e58372ce3220943287fa3f6ec
criss <ccozmuta@mozilla.com>
2022-06-21 22:25:58 +0300

    Backed out changeset d0771d3e5261 (bug 1775126) for causing mochitest failures on browser_report_site_issue.js. CLOSED TREE

--- browser/extensions/report-site-issue/test/browser/head.js
+++ browser/extensions/report-site-issue/test/browser/head.js
@@ -25,6 +25,49 @@ const NEW_ISSUE_PAGE = TEST_ROOT + "webcompat.html";
 
 const WC_ADDON_ID = "webcompat-reporter@mozilla.org";
 
+const WC_PAGE_ACTION_ID = "webcompat-reporter_mozilla_org";
+const WC_PAGE_ACTION_PANEL_ID =
+  "pageAction-panel-webcompat-reporter_mozilla_org";
+const WC_PAGE_ACTION_URLBAR_ID =
+  "pageAction-urlbar-webcompat-reporter_mozilla_org";
+
+const oldPAadd = PageActions.addAction;
+const placedSignal = "webext-page-action-placed";
+PageActions.addAction = function(action) {
+  oldPAadd.call(this, action);
+  if (action.id === WC_PAGE_ACTION_ID) {
+    Services.obs.notifyObservers(null, placedSignal);
+  }
+  return action;
+};
+const oldPAremoved = PageActions.onActionRemoved;
+const removedSignal = "webext-page-action-removed";
+PageActions.onActionRemoved = function(action) {
+  oldPAremoved.call(this, action);
+  if (action.id === WC_PAGE_ACTION_ID) {
+    Services.obs.notifyObservers(null, removedSignal);
+  }
+  return action;
+};
+
+function promisePageActionSignal(signal) {
+  return new Promise(done => {
+    const obs = function() {
+      Services.obs.removeObserver(obs, signal);
+      done();
+    };
+    Services.obs.addObserver(obs, signal);
+  });
+}
+
+function promisePageActionPlaced() {
+  return promisePageActionSignal(placedSignal);
+}
+
+function promisePageActionRemoved() {
+  return promisePageActionSignal(removedSignal);
+}
+
 async function promiseAddonEnabled() {
   const addon = await AddonManager.getAddonByID(WC_ADDON_ID);
   if (addon.isActive) {
@@ -37,57 +80,96 @@ async function promiseAddonEnabled() {
   if (!pref) {
     SpecialPowers.Services.prefs.setBoolPref(PREF_WC_REPORTER_ENABLED, true);
   }
+  await promisePageActionPlaced();
 }
 
-class HelpMenuHelper {
-  #popup = null;
+async function isPanelItemEnabled() {
+  const icon = document.getElementById(WC_PAGE_ACTION_PANEL_ID);
+  return icon && !icon.disabled;
+}
 
-  async open() {
-    this.popup = document.getElementById("menu_HelpPopup");
-    ok(this.popup, "Help menu should exist");
+async function isPanelItemDisabled() {
+  const icon = document.getElementById(WC_PAGE_ACTION_PANEL_ID);
+  return icon && icon.disabled;
+}
 
-    const menuOpen = BrowserTestUtils.waitForEvent(this.popup, "popupshown");
+async function isPanelItemPresent() {
+  return document.getElementById(WC_PAGE_ACTION_PANEL_ID) !== null;
+}
 
-    // This event-faking method was copied from browser_title_case_menus.js so
-    // this can be tested on MacOS (where the actual menus cannot be opened in
-    // tests, but we only need the help menu to populate itself).
-    this.popup.dispatchEvent(new MouseEvent("popupshowing", { bubbles: true }));
-    this.popup.dispatchEvent(new MouseEvent("popupshown", { bubbles: true }));
+async function isURLButtonPresent() {
+  return document.getElementById(WC_PAGE_ACTION_URLBAR_ID) !== null;
+}
 
-    await menuOpen;
-  }
-
-  async close() {
-    if (this.popup) {
-      const menuClose = BrowserTestUtils.waitForEvent(
-        this.popup,
-        "popuphidden"
+function openPageActions() {
+  let dwu = window.windowUtils;
+  return BrowserTestUtils.waitForCondition(() => {
+    // Wait for the main page action button to become visible.  It's hidden for
+    // some URIs, so depending on when this is called, it may not yet be quite
+    // visible.  It's up to the caller to make sure it will be visible.
+    info("Waiting for main page action button to have non-0 size");
+    let bounds = dwu.getBoundsWithoutFlushing(
+      BrowserPageActions.mainButtonNode
+    );
+    return bounds.width > 0 && bounds.height > 0;
+  })
+    .then(() => {
+      // Wait for the panel to become open, by clicking the button if necessary.
+      info("Waiting for main page action panel to be open");
+      if (BrowserPageActions.panelNode.state == "open") {
+        return Promise.resolve();
+      }
+      let shownPromise = promisePageActionPanelShown();
+      EventUtils.synthesizeMouseAtCenter(BrowserPageActions.mainButtonNode, {});
+      return shownPromise;
+    })
+    .then(() => {
+      // Wait for items in the panel to become visible.
+      return promisePageActionViewChildrenVisible(
+        BrowserPageActions.mainViewNode
       );
+    });
+}
 
-      // (Also copied from browser_title_case_menus.js)
-      // Just for good measure, we'll fire the popuphiding/popuphidden events
-      // after we close the menupopups.
-      this.popup.dispatchEvent(
-        new MouseEvent("popuphiding", { bubbles: true })
-      );
-      this.popup.dispatchEvent(
-        new MouseEvent("popuphidden", { bubbles: true })
-      );
-
-      await menuClose;
-      this.popup = null;
+function promisePageActionPanelShown() {
+  return new Promise(resolve => {
+    if (BrowserPageActions.panelNode.state == "open") {
+      executeSoon(resolve);
+      return;
     }
-  }
+    BrowserPageActions.panelNode.addEventListener(
+      "popupshown",
+      () => {
+        executeSoon(resolve);
+      },
+      { once: true }
+    );
+  });
+}
 
-  isItemHidden() {
-    const item = document.getElementById("help_reportSiteIssue");
-    return item && item.hidden;
-  }
+function promisePageActionViewChildrenVisible(panelViewNode) {
+  info(
+    "promisePageActionViewChildrenVisible waiting for a child node to be visible"
+  );
+  let dwu = window.windowUtils;
+  return BrowserTestUtils.waitForCondition(() => {
+    let bodyNode = panelViewNode.firstElementChild;
+    for (let childNode of bodyNode.children) {
+      let bounds = dwu.getBoundsWithoutFlushing(childNode);
+      if (bounds.width > 0 && bounds.height > 0) {
+        return true;
+      }
+    }
+    return false;
+  });
+}
 
-  isItemEnabled() {
-    const item = document.getElementById("help_reportSiteIssue");
-    return item && !item.hidden && !item.disabled;
-  }
+function pinToURLBar() {
+  PageActions.actionForID(WC_PAGE_ACTION_ID).pinnedToUrlbar = true;
+}
+
+function unpinFromURLBar() {
+  PageActions.actionForID(WC_PAGE_ACTION_ID).pinnedToUrlbar = false;
 }
 
 async function startIssueServer() {
