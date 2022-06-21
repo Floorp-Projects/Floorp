1171c51db568f75a65204c7f85a66a06e5922d2b
Jan de Mooij <jdemooij@mozilla.com>
2022-06-21 09:10:41 +0000

    Bug 1775065 - Use correct ExitFrameType for bailout exception. r=iain
    Also tidy up the code a bit: don't push an exit frame if there was no failure,
    because we'll immediately clobber it with the Baseline frames anyway.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149776

--- js/src/jit/MacroAssembler.cpp
+++ js/src/jit/MacroAssembler.cpp
@@ -2019,10 +2019,8 @@ void MacroAssembler::guardStringToInt32(Register str, Register output,
 
 void MacroAssembler::generateBailoutTail(Register scratch,
                                          Register bailoutInfo) {
-  loadJSContext(scratch);
-  enterFakeExitFrame(scratch, scratch, ExitFrameType::Bare);
-
-  branchIfFalseBool(ReturnReg, exceptionLabel());
+  Label bailoutFailed;
+  branchIfFalseBool(ReturnReg, &bailoutFailed);
 
   // Finish bailing out to Baseline.
   {
@@ -2091,6 +2089,17 @@ void MacroAssembler::generateBailoutTail(Register scratch,
     addToStackPtr(Imm32(ExitFrameLayout::SizeWithFooter()));
 
     jump(jitcodeReg);
+  }
+
+  bind(&bailoutFailed);
+  {
+    // jit::Bailout or jit::InvalidationBailout failed and returned false. The
+    // Ion frame has already been discarded and the stack pointer points to the
+    // JitFrameLayout header. Turn it into an ExitFrameLayout, similar to
+    // EnsureUnwoundJitExitFrame, and call the exception handler.
+    loadJSContext(scratch);
+    enterFakeExitFrame(scratch, scratch, ExitFrameType::UnwoundJit);
+    jump(exceptionLabel());
   }
 }
 
