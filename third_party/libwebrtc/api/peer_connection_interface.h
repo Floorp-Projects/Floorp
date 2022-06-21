bcf1f9017dc27c12eca75d8cc3cd076f66e52494
Michael Froman <mfroman@mozilla.com>
2022-06-21 12:49:28 -0500

    Bug 1766646 - Vendor libwebrtc from 1fe08e1abe5
    Upstream commit: https://webrtc.googlesource.com/src/+/1fe08e1abe5e9c9ebdf0a58344c0c273981406f3
        Remove unused 4-argument version of OnIceCandidateError.
    
        It has not been used since
        https://chromium-review.googlesource.com/c/chromium/src/+/1944346.
    
        Bug: webrtc:13446
        Change-Id: Ice9c418435bc7958562eb73524d7651a79508ab6
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/241440
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Henrik Boström <hbos@webrtc.org>
        Commit-Queue: Henrik Boström <hbos@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#35541}

--- third_party/libwebrtc/api/peer_connection_interface.h
+++ third_party/libwebrtc/api/peer_connection_interface.h
@@ -1297,14 +1297,6 @@ class PeerConnectionObserver {
 
   // Gathering of an ICE candidate failed.
   // See https://w3c.github.io/webrtc-pc/#event-icecandidateerror
-  // `host_candidate` is a stringified socket address.
-  virtual void OnIceCandidateError(const std::string& host_candidate,
-                                   const std::string& url,
-                                   int error_code,
-                                   const std::string& error_text) {}
-
-  // Gathering of an ICE candidate failed.
-  // See https://w3c.github.io/webrtc-pc/#event-icecandidateerror
   virtual void OnIceCandidateError(const std::string& address,
                                    int port,
                                    const std::string& url,
