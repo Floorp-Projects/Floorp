d9a0cc2cd8f9952657ce3bd1d9b65b89bb88d0bc
Michael Froman <mfroman@mozilla.com>
2022-06-21 21:28:11 -0500

    Bug 1766646 - Vendor libwebrtc from 46333dbf621
    Upstream commit: https://webrtc.googlesource.com/src/+/46333dbf6211ea197965c30fdbecbeb62bc81e5b
        Wire up proto-free event log format in encoder and parser.
    
        Encode ALR state events as an example. The ALR state unit tests pass with the new format, but the tests are not enabled in this CL since the other event types aren't encoded yet.
    
        Bug: webrtc:11933
        Change-Id: I3ba22778b55f24e2e2bd7d95bb9b17de29ef899f
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/234520
        Reviewed-by: Sebastian Jansson <srte@webrtc.org>
        Commit-Queue: Björn Terelius <terelius@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#35752}

--- third_party/libwebrtc/logging/rtc_event_log/encoder/rtc_event_log_encoder_unittest.cc
+++ third_party/libwebrtc/logging/rtc_event_log/encoder/rtc_event_log_encoder_unittest.cc
@@ -16,6 +16,7 @@
 
 #include "logging/rtc_event_log/encoder/rtc_event_log_encoder_legacy.h"
 #include "logging/rtc_event_log/encoder/rtc_event_log_encoder_new_format.h"
+#include "logging/rtc_event_log/encoder/rtc_event_log_encoder_v3.h"
 #include "logging/rtc_event_log/events/rtc_event_alr_state.h"
 #include "logging/rtc_event_log/events/rtc_event_audio_network_adaptation.h"
 #include "logging/rtc_event_log/events/rtc_event_audio_playout.h"
@@ -62,8 +63,7 @@ class RtcEventLogEncoderTest
         encoder_ = std::make_unique<RtcEventLogEncoderNewFormat>();
         break;
       case RtcEventLog::EncodingType::ProtoFree:
-        // TODO(terelius): Enable test once the format has been wired up.
-        RTC_CHECK_NOTREACHED();
+        encoder_ = std::make_unique<RtcEventLogEncoderV3>();
         break;
     }
     encoded_ =
@@ -1290,8 +1290,7 @@ class RtcEventLogEncoderSimpleTest
         encoder_ = std::make_unique<RtcEventLogEncoderNewFormat>();
         break;
       case RtcEventLog::EncodingType::ProtoFree:
-        // TODO(terelius): Enable test once the format has been wired up.
-        RTC_CHECK_NOTREACHED();
+        encoder_ = std::make_unique<RtcEventLogEncoderV3>();
         break;
     }
     encoded_ =
