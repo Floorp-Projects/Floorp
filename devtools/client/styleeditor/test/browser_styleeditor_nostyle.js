3f2ef09de8692c63d4276bfc9999ff25bef2cfa1
Nicolas Chevobbe <nchevobbe@mozilla.com>
2022-06-22 09:30:05 +0000

    Bug 1775411 - [devtools] Fix append new stylesheet link. r=ochameau.
    The link was missing an event listener, that we add in this patch.
    A test case is added to make sure the link does work.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149953

--- devtools/client/styleeditor/test/browser_styleeditor_nostyle.js
+++ devtools/client/styleeditor/test/browser_styleeditor_nostyle.js
@@ -8,7 +8,10 @@
 const TESTCASE_URI = TEST_BASE_HTTP + "nostyle.html";
 
 add_task(async function() {
-  const { panel } = await openStyleEditorForURL(TESTCASE_URI);
+  // Make enough room for the "append style sheet" link to not wrap,
+  // as it messes up with EvenEventUtils.synthesizeMouse
+  await pushPref("devtools.styleeditor.navSidebarWidth", 500);
+  const { panel, ui } = await openStyleEditorForURL(TESTCASE_URI);
   const { panelWindow } = panel;
 
   ok(
@@ -16,14 +19,39 @@ add_task(async function() {
     "style editor root element does not have 'loading' class name anymore"
   );
 
-  ok(
-    getRootElement(panel).querySelector(".empty.placeholder"),
+  const newButton = panelWindow.document.querySelector(
+    "toolbarbutton.style-editor-newButton"
+  );
+  ok(!newButton.hasAttribute("disabled"), "new style sheet button is enabled");
+
+  const importButton = panelWindow.document.querySelector(
+    ".style-editor-importButton"
+  );
+  ok(!importButton.hasAttribute("disabled"), "import button is enabled");
+
+  const emptyPlaceHolderEl = getRootElement(panel).querySelector(
+    ".empty.placeholder"
+  );
+  isnot(
+    emptyPlaceHolderEl.ownerGlobal.getComputedStyle(emptyPlaceHolderEl).display,
+    "none",
     "showing 'no style' indicator"
   );
 
-  let button = panelWindow.document.querySelector(".style-editor-newButton");
-  ok(!button.hasAttribute("disabled"), "new style sheet button is enabled");
+  info(
+    "Check that clicking on the append new stylesheet link do add a stylesheet"
+  );
+  const onEditorAdded = ui.once("editor-added");
+  const newLink = emptyPlaceHolderEl.querySelector("a.style-editor-newButton");
 
-  button = panelWindow.document.querySelector(".style-editor-importButton");
-  ok(!button.hasAttribute("disabled"), "import button is enabled");
+  // Use synthesizeMouse to also check that the element is visible
+  EventUtils.synthesizeMouseAtCenter(newLink, {}, newLink.ownerGlobal);
+  await onEditorAdded;
+
+  ok(true, "A stylesheet was added");
+  is(
+    emptyPlaceHolderEl.ownerGlobal.getComputedStyle(emptyPlaceHolderEl).display,
+    "none",
+    "The empty placeholder element is now hidden"
+  );
 });
