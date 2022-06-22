7c5e3a6fca301629296ca9beb622801db3f8d55c
Gabriele Svelto <gsvelto@mozilla.com>
2022-06-22 10:07:43 +0000

    Bug 1775117 - Fix Windows builds when using version 10.0.22621.0 of the Windows SDK r=handyman
    Differential Revision: https://phabricator.services.mozilla.com/D149868

--- widget/windows/WinUtils.cpp
+++ widget/windows/WinUtils.cpp
@@ -1835,6 +1835,26 @@ uint32_t WinUtils::GetMaxTouchPoints() {
   return 0;
 }
 
+// Starting with version 10.0.22621.0 of the Windows SDK the AR_STATE enum and
+// types are only defined when building for Windows 8 instead of Windows 7.
+#if (WDK_NTDDI_VERSION >= 0x0A00000C) && (WINVER < 0x0602)
+
+enum AR_STATE {
+  AR_ENABLED = 0x0,
+  AR_DISABLED = 0x1,
+  AR_SUPPRESSED = 0x2,
+  AR_REMOTESESSION = 0x4,
+  AR_MULTIMON = 0x8,
+  AR_NOSENSOR = 0x10,
+  AR_NOT_SUPPORTED = 0x20,
+  AR_DOCKED = 0x40,
+  AR_LAPTOP = 0x80
+};
+
+using PAR_STATE = enum AR_STATE*;
+
+#endif  // (WDK_NTDDI_VERSION >= 0x0A00000C) && (WINVER < 0x0602)
+
 /* static */
 POWER_PLATFORM_ROLE
 WinUtils::GetPowerPlatformRole() {
