f96b45db9eba303acceab3824a9816dc5fec7d02
Alexandre Poirot <poirot.alex@gmail.com>
2022-06-22 20:12:43 +0000

    Bug 1774583 - [devtools] Prevent clearing tab list when a target is destroyed. r=bomsy
    persistTabs should only be used when we want to clear the tabs list
    and have the persisted tab to be re-created (made visible, with a sourceId) when a new source matching the same url is created.
    
    Also cleanup a few things and avoid returning new state objects unless we really changed something.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149523

--- devtools/client/debugger/src/actions/tabs.js
+++ devtools/client/debugger/src/actions/tabs.js
@@ -17,27 +17,18 @@ import {
 } from "../selectors";
 
 export function updateTab(source, framework) {
-  const { url, id: sourceId, isOriginal, thread } = source;
-
   return {
     type: "UPDATE_TAB",
-    url,
+    source,
     framework,
-    isOriginal,
-    sourceId,
-    thread,
   };
 }
 
 export function addTab(source) {
-  const { url, id: sourceId, isOriginal, thread } = source;
-
   return {
     type: "ADD_TAB",
-    url,
-    isOriginal,
-    sourceId,
-    thread,
+    source,
+    framework: null,
   };
 }
 
