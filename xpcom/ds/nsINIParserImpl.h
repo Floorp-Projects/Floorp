0848527440168dd0bbf1d6fcfab531153371433b
Kris Maglione <maglione.k@gmail.com>
2022-06-23 23:05:35 +0000

    Bug 1773770: Part 4 - Migrate INI parser factory to static component registration. r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D149432

--- xpcom/ds/nsINIParserImpl.h
+++ xpcom/ds/nsINIParserImpl.h
@@ -8,25 +8,16 @@
 #define nsINIParserImpl_h__
 
 #include "nsIINIParser.h"
-#include "nsIFactory.h"
 #include "mozilla/Attributes.h"
-
-#define NS_INIPARSERFACTORY_CID                      \
-  {                                                  \
-    0xdfac10a9, 0xdd24, 0x43cf, {                    \
-      0xa0, 0x95, 0x6f, 0xfa, 0x2e, 0x4b, 0x6a, 0x6c \
-    }                                                \
-  }
 
 #define NS_INIPARSERFACTORY_CONTRACTID "@mozilla.org/xpcom/ini-parser-factory;1"
 
-class nsINIParserFactory final : public nsIINIParserFactory, public nsIFactory {
+class nsINIParserFactory final : public nsIINIParserFactory {
   ~nsINIParserFactory() = default;
 
  public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIINIPARSERFACTORY
-  NS_DECL_NSIFACTORY
 };
 
 #endif  // nsINIParserImpl_h__
