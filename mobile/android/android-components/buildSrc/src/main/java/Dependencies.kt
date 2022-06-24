4a71968c321c1f7dfe717964445ed9213ac34399
mcarare <mihai.carare.dev@gmail.com>
2022-06-24 16:47:31 +0300

    [components] For https://github.com/mozilla-mobile/android-components/issues/12381: Update Gradle, Gradle plugin and kotlin-dsl plugin.

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -17,7 +17,7 @@ object Versions {
 
     const val mockwebserver = "3.10.0"
 
-    const val android_gradle_plugin = "7.0.0"
+    const val android_gradle_plugin = "7.2.2"
     const val android_maven_publish_plugin = "3.6.2"
     const val lint = "27.0.1"
     const val detekt = "1.19.0"
