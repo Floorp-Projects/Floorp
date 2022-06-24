7a96f35e301cc6a0d28b05ac9d488c6af30d2cd6
James Teh <jteh@mozilla.com>
2022-06-24 05:28:53 +0000

    Bug 1773930: Expose xml-roles object attribute on cached RemoteAccessible. r=morgan
    This also changes LocalAccessible::Attributes to ignore the edge case of an empty role attribute when calculating xml-roles.
    This is consistent with how we handle the role attribute elsewhere (see aria::GetRoleMapIndex) and makes it easier to handle this consistently between local and remote.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149594

--- accessible/generic/LocalAccessible.cpp
+++ accessible/generic/LocalAccessible.cpp
@@ -1088,7 +1088,8 @@ already_AddRefed<AccAttributes> LocalAccessible::Attributes() {
   // 'xml-roles' attribute coming from ARIA.
   nsString xmlRoles;
   if (mContent->AsElement()->GetAttr(kNameSpaceID_None, nsGkAtoms::role,
-                                     xmlRoles)) {
+                                     xmlRoles) &&
+      !xmlRoles.IsEmpty()) {
     attributes->SetAttribute(nsGkAtoms::xmlroles, std::move(xmlRoles));
   } else if (nsAtom* landmark = LandmarkRole()) {
     // 'xml-roles' attribute for landmark.
@@ -3589,15 +3590,30 @@ already_AddRefed<AccAttributes> LocalAccessible::BundleFieldsForCache(
     if (mContent && mContent->IsElement()) {
       fields->SetAttribute(nsGkAtoms::tag, mContent->NodeInfo()->NameAtom());
 
+      dom::Element* el = mContent->AsElement();
       if (IsTextField() || IsDateTimeField()) {
         // Cache text input types. Accessible is recreated if this changes,
         // so it is considered immutable.
-        if (const nsAttrValue* attr =
-                mContent->AsElement()->GetParsedAttr(nsGkAtoms::type)) {
+        if (const nsAttrValue* attr = el->GetParsedAttr(nsGkAtoms::type)) {
           RefPtr<nsAtom> inputType = attr->GetAsAtom();
           if (inputType) {
             fields->SetAttribute(nsGkAtoms::textInputType, inputType);
           }
+        }
+      }
+
+      // Changing the role attribute currently re-creates the Accessible, so
+      // it's immutable in the cache.
+      if (const nsRoleMapEntry* roleMap = ARIARoleMap()) {
+        // Most of the time, the role attribute is a single, known role. We
+        // already send the map index, so we don't need to double up.
+        if (!el->AttrValueIs(kNameSpaceID_None, nsGkAtoms::role,
+                             roleMap->roleAtom, eIgnoreCase)) {
+          // Multiple roles or unknown roles are rare, so just send them as a
+          // string.
+          nsAutoString role;
+          el->GetAttr(kNameSpaceID_None, nsGkAtoms::role, role);
+          fields->SetAttribute(nsGkAtoms::role, std::move(role));
         }
       }
     }
