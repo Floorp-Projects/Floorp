0cc4872c4ce88fbaae59fb5c12a31f88980c49b1
Michael Froman <mfroman@mozilla.com>
2022-06-24 12:13:13 -0500

    Bug 1766646 - Vendor libwebrtc from 609b047b07
    Upstream commit: https://webrtc.googlesource.com/src/+/609b047b0758f8f1fba6d1d9ba4bb9094ae79489
        Add NetEq decision logic unit tests.
    
        - Add buffer level filter and delay manager mocks and make them
         injectable for easier testing.
        - Add a basic set of tests for simple cases and recently added features.
    
        Bug: webrtc:10333
        Change-Id: I8b6f73b8ad99ad6859ed1279086c0bd68b7687be
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/188623
        Commit-Queue: Jakob Ivarsson <jakobi@webrtc.org>
        Reviewed-by: Ivo Creusen <ivoc@webrtc.org>
        Cr-Commit-Position: refs/heads/master@{#32433}

---
+++ third_party/libwebrtc/modules/audio_coding/neteq/mock/mock_delay_manager.h
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef MODULES_AUDIO_CODING_NETEQ_MOCK_MOCK_DELAY_MANAGER_H_
+#define MODULES_AUDIO_CODING_NETEQ_MOCK_MOCK_DELAY_MANAGER_H_
+
+#include <memory>
+#include <utility>
+
+#include "api/neteq/tick_timer.h"
+#include "modules/audio_coding/neteq/delay_manager.h"
+#include "test/gmock.h"
+
+namespace webrtc {
+
+class MockDelayManager : public DelayManager {
+ public:
+  MockDelayManager(size_t max_packets_in_buffer,
+                   int base_minimum_delay_ms,
+                   int histogram_quantile,
+                   const TickTimer* tick_timer,
+                   std::unique_ptr<Histogram> histogram)
+      : DelayManager(max_packets_in_buffer,
+                     base_minimum_delay_ms,
+                     histogram_quantile,
+                     tick_timer,
+                     std::move(histogram)) {}
+  MOCK_METHOD(int, TargetDelayMs, (), (const));
+};
+
+}  // namespace webrtc
+#endif  // MODULES_AUDIO_CODING_NETEQ_MOCK_MOCK_DELAY_MANAGER_H_
