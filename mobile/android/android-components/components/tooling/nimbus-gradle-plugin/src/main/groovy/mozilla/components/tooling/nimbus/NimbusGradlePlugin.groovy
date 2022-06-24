b62db5d64758dac978d48a0d6a7823f815e70015
James Hugman <james@hugman.tv>
2022-06-24 18:01:00 +0100

    [components] Version bump for application-services nimbus-fml

--- mobile/android/android-components/components/tooling/nimbus-gradle-plugin/src/main/groovy/mozilla/components/tooling/nimbus/NimbusGradlePlugin.groovy
+++ mobile/android/android-components/components/tooling/nimbus-gradle-plugin/src/main/groovy/mozilla/components/tooling/nimbus/NimbusGradlePlugin.groovy
@@ -171,7 +171,7 @@ class NimbusPlugin implements Plugin<Project> {
         // a) this plugin is going to live in the AS repo (eventually)
         // See https://github.com/mozilla-mobile/android-components/issues/11422 for tying this
         // to a version that is specified in buildSrc/src/main/java/Dependencies.kt
-        return "93.1.0"
+        return "93.5.0"
     }
 
     // Try one or more hosts to download the given file.
