7304610d516e18cfa79d899b7b8381b37da9f43b
Botond Ballo <botond@mozilla.com>
2022-06-25 04:33:51 +0000

    Bug 1776530 - Rename APZUpdater::UsingWebRenderUpdaterThread() to IsConnectedToWebRender(). r=tnikkel
    The earlier name dates back to a time when we could use WebRender
    but not necessarily use a WebRender thread as the updater thread.
    
    Also add a comment to list the remaining situatins in which this
    function can return false.
    
    Differential Revision: https://phabricator.services.mozilla.com/D150308

--- gfx/layers/ipc/ContentCompositorBridgeParent.cpp
+++ gfx/layers/ipc/ContentCompositorBridgeParent.cpp
@@ -76,9 +76,9 @@ ContentCompositorBridgeParent::AllocPAPZCTreeManagerParent(
     // Note: we immediately call ClearTree since otherwise the APZCTM will
     // retain a reference to itself, through the checkerboard observer.
     LayersId dummyId{0};
-    const bool useWebRender = false;
+    const bool connectedToWebRender = false;
     RefPtr<APZCTreeManager> temp = new APZCTreeManager(dummyId);
-    RefPtr<APZUpdater> tempUpdater = new APZUpdater(temp, useWebRender);
+    RefPtr<APZUpdater> tempUpdater = new APZUpdater(temp, connectedToWebRender);
     tempUpdater->ClearTree(dummyId);
     return new APZCTreeManagerParent(aLayersId, temp, tempUpdater);
   }
