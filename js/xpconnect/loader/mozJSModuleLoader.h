50e3d3b12dff4a74ffdc085115560dde3f999050
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2022-06-25 13:45:03 +0000

    Bug 1775150 - Cleanup js/xpconnect includes r=sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D149803

--- js/xpconnect/loader/mozJSModuleLoader.h
+++ js/xpconnect/loader/mozJSModuleLoader.h
@@ -12,16 +12,15 @@
 #include "mozilla/FileLocation.h"
 #include "mozilla/MemoryReporting.h"
 #include "mozilla/StaticPtr.h"
-#include "mozilla/UniquePtr.h"
 #include "nsIMemoryReporter.h"
 #include "nsISupports.h"
 #include "nsIURI.h"
 #include "nsClassHashtable.h"
-#include "nsTHashMap.h"
 #include "jsapi.h"
 #include "js/experimental/JSStencil.h"
 
 #include "xpcpublic.h"
+#include "xpcIJSGetFactory.h"
 
 class nsIFile;
 class ModuleLoaderInfo;
