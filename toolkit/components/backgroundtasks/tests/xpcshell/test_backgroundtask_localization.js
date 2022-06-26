3039149f5acfcf0200e3926d317482a8a9d6a8e4
Nick Alexander <nalexander@mozilla.com>
2022-06-26 23:38:45 +0000

    Bug 1775393 - Enable Fluent localization in background tasks. r=mossop
    The chrome manifest flag parsing is not particularly flexible.  It's
    easiest to duplicate the manifest entries rather than further adjust
    the parser.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149950

---
+++ toolkit/components/backgroundtasks/tests/xpcshell/test_backgroundtask_localization.js
@@ -0,0 +1,28 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
+ * vim: sw=4 ts=4 sts=4 et
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+async function doOne(resource, id) {
+  let l10n = new Localization([resource], true);
+  let value = await l10n.formatValue(id);
+  Assert.ok(value, `${id} from ${resource} is not null: ${value}`);
+
+  let exitCode = await do_backgroundtask("localization", {
+    extraArgs: [resource, id, value],
+  });
+  Assert.equal(0, exitCode);
+}
+
+add_task(async function test_localization() {
+  // Verify that the `l10n-registry` category is processed and that localization
+  // works as expected in background tasks.  We can use any FTL resource and
+  // string identifier here as long as the value is short and can be passed as a
+  // command line argument safely (i.e., is ASCII).
+
+  // One from toolkit/.
+  await doOne("toolkit/global/commonDialog.ftl", "common-dialog-title-system");
+  // And one from browser/.
+  await doOne("browser/pageInfo.ftl", "not-set-date");
+});
