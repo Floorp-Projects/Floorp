ba53a694d0b5ed5a3f6c5604d5eea81819c09cca
Sean Feng <sefeng@mozilla.com>
2022-06-27 17:07:15 +0000

    Bug 1731778 - Implement COEP: credentialless for cache storage r=edenchuang
    Spec: https://fetch.spec.whatwg.org/#ref-for-cross-origin-resource-policy-internal-check
    
    This purpose of this patch is just to implement the spec.
    
    One noticeable thing I did in the patch is I made `CacheResponse` to
    include the `credentials mode` of the initial request. Consider the
    below scenario:
    
      1. Create a fetch request with a URL and a specific credential_mode, and put it into cache
      2. Call cache.match by using a URL, but without credential_mode
      3. cache.match() result should be filtered according to the initial request's credential_mode
    
    When applying the `response’s request-includes-credentials is true`
    check, the initial request's `credential_mode` is needed because
    `request-includes-credentials` is judged by the `credential_mode`.
    
    The rest of the changes are just normal spec alignments.
    
    Differential Revision: https://phabricator.services.mozilla.com/D147803

--- dom/fetch/InternalResponse.cpp
+++ dom/fetch/InternalResponse.cpp
@@ -196,6 +196,7 @@ SafeRefPtr<InternalResponse> InternalResponse::Clone(CloneType aCloneType) {
   clone->mPaddingSize = mPaddingSize;
 
   clone->mCacheInfoChannel = mCacheInfoChannel;
+  clone->mCredentialsMode = mCredentialsMode;
 
   if (mWrappedResponse) {
     clone->mWrappedResponse = mWrappedResponse->Clone(aCloneType);
