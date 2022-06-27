adc1089ee5d3b121a34d084a0ef54fa345b47d38
Jonathan Kew <jkew@mozilla.com>
2022-06-27 17:14:21 +0000

    Bug 1772225 - Implement a CGLayer-backed version of cairo_quartz_surface, to improve print/PDF output quality for pdf.js documents. r=jrmuizel, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D150128

--- gfx/cairo/cairo/src/cairo-quartz.h
+++ gfx/cairo/cairo/src/cairo-quartz.h
@@ -61,6 +61,12 @@ cairo_quartz_surface_create_for_data (unsigned char *data,
                                       unsigned int height,
                                       unsigned int stride);
 
+cairo_public cairo_surface_t *
+cairo_quartz_surface_create_cg_layer (cairo_surface_t *surface,
+                                      cairo_content_t content,
+                                      unsigned int width,
+                                      unsigned int height);
+
 cairo_public CGContextRef
 cairo_quartz_surface_get_cg_context (cairo_surface_t *surface);
 
