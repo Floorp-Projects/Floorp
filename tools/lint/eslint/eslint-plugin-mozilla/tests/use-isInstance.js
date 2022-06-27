c3ec76649f272c7f35675cf62d496c899e50c0f9
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-06-27 20:31:33 +0000

    Bug 1773740 - Part 3: Cover js files including ChromeUtils r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D148938

--- tools/lint/eslint/eslint-plugin-mozilla/tests/use-isInstance.js
+++ tools/lint/eslint/eslint-plugin-mozilla/tests/use-isInstance.js
@@ -34,6 +34,19 @@ const env = { browser: true };
 function mockChromeScript(code) {
   return {
     code,
+    filename: "foo.sys.mjs",
+    env,
+  };
+}
+
+/**
+ * A test case boilerplate simulating content script.
+ * @param {string} code
+ */
+function mockContentScript(code) {
+  return {
+    code,
+    filename: "foo.js",
     env,
   };
 }
@@ -54,6 +67,11 @@ ruleTester.run("use-isInstance", rule, {
     mockChromeScript("var File;file instanceof File;"),
     mockChromeScript("foo instanceof RandomGlobalThing;"),
     mockChromeScript("foo instanceof lazy.RandomGlobalThing;"),
+    mockContentScript("node instanceof Node;"),
+    mockContentScript("file instanceof File;"),
+    mockContentScript(
+      "SpecialPowers.ChromeUtils.import(''); file instanceof File;"
+    ),
   ],
   invalid: [
     {
@@ -61,32 +79,52 @@ ruleTester.run("use-isInstance", rule, {
       output: "Node.isInstance(node)",
       env,
       errors,
+      filename: "foo.sys.mjs",
     },
     {
       code: "text instanceof win.Text",
       output: "win.Text.isInstance(text)",
       errors,
+      filename: "foo.sys.mjs",
     },
     {
       code: "target instanceof this.contentWindow.HTMLAudioElement",
       output: "this.contentWindow.HTMLAudioElement.isInstance(target)",
       errors,
+      filename: "foo.sys.mjs",
     },
     {
       code: "target instanceof File",
       output: "File.isInstance(target)",
       env,
       errors,
+      filename: "foo.sys.mjs",
     },
     {
       code: "target instanceof win.File",
       output: "win.File.isInstance(target)",
       errors,
+      filename: "foo.sys.mjs",
     },
     {
       code: "window.arguments[0] instanceof window.XULElement",
       output: "window.XULElement.isInstance(window.arguments[0])",
       errors,
+      filename: "foo.sys.mjs",
+    },
+    {
+      code: "ChromeUtils.import(''); node instanceof Node",
+      output: "ChromeUtils.import(''); Node.isInstance(node)",
+      env,
+      errors,
+      filename: "foo.js",
+    },
+    {
+      code: "ChromeUtils.import(''); file instanceof File",
+      output: "ChromeUtils.import(''); File.isInstance(file)",
+      env,
+      errors,
+      filename: "foo.js",
     },
   ],
 });
