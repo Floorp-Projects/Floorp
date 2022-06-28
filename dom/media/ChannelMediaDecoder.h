0ab6ca8ac9beb898c128fb08dbc2bea8757fa9e7
alwu <alwu@mozilla.com>
2022-06-28 04:26:46 +0000

    Bug 1758789 - part13 : fallback to our state machine when the external engine fails to work. r=jolin
    This is important for testing because the media foundation doesn't support all the formats Firefox supports, we would need to fallback to the normal state machine in order to keep tests running without breaking them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D145402

--- dom/media/ChannelMediaDecoder.h
+++ dom/media/ChannelMediaDecoder.h
@@ -102,7 +102,8 @@ class ChannelMediaDecoder
   void DownloadProgressed();
 
   // Create a new state machine to run this decoder.
-  MediaDecoderStateMachineBase* CreateStateMachine();
+  MediaDecoderStateMachineBase* CreateStateMachine(
+      bool aDisableExternalEngine) override;
 
   nsresult Load(BaseMediaResource* aOriginal);
 
