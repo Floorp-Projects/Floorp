022fc119e769c5c3f29ed675cf65fa938e5dd5b9
pmcmanis <pmcmanis@mozilla.com>
2022-06-28 08:45:57 +0000

    Bug 1766515 - Implemented new glean Boolean error handling behavior on Labeled Booleans collected in Glean, and updated tests r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D149569

--- toolkit/components/glean/api/src/ffi/boolean.rs
+++ toolkit/components/glean/api/src/ffi/boolean.rs
@@ -17,6 +17,16 @@ pub extern "C" fn fog_boolean_test_get_value(id: u32, ping_name: &nsACString) ->
 }
 
 #[no_mangle]
+pub extern "C" fn fog_boolean_test_get_error(
+    id: u32,
+    ping_name: &nsACString,
+    error_str: &mut nsACString,
+) -> bool {
+    let err = with_metric!(BOOLEAN_MAP, id, metric, test_get_errors!(metric, ping_name));
+    err.map(|err_str| error_str.assign(&err_str)).is_some()
+}
+
+#[no_mangle]
 pub extern "C" fn fog_boolean_set(id: u32, value: bool) {
     with_metric!(BOOLEAN_MAP, id, metric, metric.set(value));
 }
