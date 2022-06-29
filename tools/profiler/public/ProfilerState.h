a002c9e1c9f50e469929fa7a195de35c6a36618c
Florian Quèze <florian@queze.net>
2022-06-29 21:24:12 +0000

    Bug 1774844 - Record per process power use on Apple Silicon, r=gerald.
    Differential Revision: https://phabricator.services.mozilla.com/D150511

--- tools/profiler/public/ProfilerState.h
+++ tools/profiler/public/ProfilerState.h
@@ -100,7 +100,9 @@
   MACRO(23, "processcpu", ProcessCPU,                                        \
         "Sample the CPU utilization of each process")                        \
                                                                              \
-  MACRO(24, "power", Power, "Sample energy meters. (Windows 11 only)")
+  MACRO(24, "power", Power,                                                  \
+        "Sample energy meters on Windows 11 and per process power use on "   \
+        "Apple Silicon")
 // *** Synchronize with lists in BaseProfilerState.h and geckoProfiler.json ***
 
 struct ProfilerFeature {
