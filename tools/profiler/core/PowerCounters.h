0675e9ba405c2cd242f1676d633c918aa3416774
Florian Queze <florian@queze.net>
2022-06-29 13:29:44 +0000

    Bug 1774844 - record EMI data as profiler counters, r=gerald.
    Differential Revision: https://phabricator.services.mozilla.com/D149653

---
+++ tools/profiler/core/PowerCounters.h
@@ -0,0 +1,40 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef TOOLS_POWERCOUNTERS_H_
+#define TOOLS_POWERCOUNTERS_H_
+
+#include "PlatformMacros.h"
+#include "mozilla/ProfilerCounts.h"
+#include "mozilla/UniquePtr.h"
+#include "mozilla/Vector.h"
+
+#if defined(GP_OS_windows)
+class PowerMeterDevice;
+#endif
+
+class PowerCounters {
+ public:
+#if defined(GP_OS_windows)
+  explicit PowerCounters();
+  ~PowerCounters();
+  void Sample();
+#else
+  explicit PowerCounters(){};
+  ~PowerCounters(){};
+  void Sample(){};
+#endif
+
+  using CountVector = mozilla::Vector<BaseProfilerCount*, 4>;
+  const CountVector& GetCounters() { return mCounters; }
+
+ private:
+  CountVector mCounters;
+
+#if defined(GP_OS_windows)
+  mozilla::Vector<mozilla::UniquePtr<PowerMeterDevice>> mPowerMeterDevices;
+#endif
+};
+
+#endif /* ndef TOOLS_POWERCOUNTERS_H_ */
