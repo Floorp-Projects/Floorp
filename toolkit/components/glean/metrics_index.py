6bc908b454c9f0c09990fe4d81d5942ed71c1116
mleclair <mleclair@mozilla.com>
2022-06-29 21:11:41 +0000

    Bug 1772124 - Added Glean metrics to track different type of resources for Early Hints r=dragana,mbucher,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D148741

--- toolkit/components/glean/metrics_index.py
+++ toolkit/components/glean/metrics_index.py
@@ -20,6 +20,7 @@ metrics_yamls = [
     "dom/media/metrics.yaml",
     "dom/metrics.yaml",
     "gfx/metrics.yaml",
+    "netwerk/protocol/http/metrics.yaml",
     "toolkit/components/extensions/metrics.yaml",
     "toolkit/components/glean/metrics.yaml",
     "toolkit/components/glean/tests/test_metrics.yaml",
