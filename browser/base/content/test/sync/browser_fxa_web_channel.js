4aff027f5b991408194dfdcd2c43b74da48b1155
Sam Foster <sfoster@mozilla.com>
2022-06-29 05:29:06 +0000

    Bug 1776087 - Add a fxa web channel command/handler to open firefoxview. r=markh
    * Export new COMMAND_FIREFOX_VIEW from FxAccountsCommon for the command string
    * Call a new openFirefoxView method when the feature pref is enabled
    * Pass along an entryPoint param - this is speculative: we don't handle this currently but its what the COMMAND_PAIR_PREFERENCES does and seems reasonable
    * New test to confirm that this openFirefoxView method will indeed get called when the right web channel message is received. Note, this doesnt confirm that FirefoxViewHandler.openTab actually results in fx-view being opened, but it will at least fail if that moves or gets renamed in the future
    
    Differential Revision: https://phabricator.services.mozilla.com/D150527

--- browser/base/content/test/sync/browser_fxa_web_channel.js
+++ browser/base/content/test/sync/browser_fxa_web_channel.js
@@ -202,6 +202,57 @@ var gTests = [
       );
     },
   },
+  {
+    desc:
+      "fxa web channel - firefox_view messages should call the openFirefoxView helper",
+    async run() {
+      // pref needs to be enabled for the fxa web channel to handle this command
+      await SpecialPowers.pushPrefEnv({
+        set: [["browser.tabs.firefox-view", true]],
+      });
+      let wasCalled = false;
+      let promiseMessageHandled = new Promise((resolve, reject) => {
+        let openFirefoxView = (browser, entryPoint) => {
+          wasCalled = true;
+          Assert.ok(
+            !!browser.ownerGlobal,
+            "openFirefoxView called with a browser argument"
+          );
+          Assert.equal(
+            typeof browser.ownerGlobal.FirefoxViewHandler.openTab,
+            "function",
+            "We can reach the openTab method"
+          );
+
+          client.tearDown();
+          resolve();
+        };
+
+        let client = new FxAccountsWebChannel({
+          content_uri: TEST_HTTP_PATH,
+          channel_id: TEST_CHANNEL_ID,
+          helpers: {
+            openFirefoxView,
+          },
+        });
+      });
+
+      await BrowserTestUtils.withNewTab(
+        {
+          gBrowser,
+          url: TEST_BASE_URL + "?firefox_view",
+        },
+        async function() {
+          try {
+            await promiseMessageHandled;
+          } finally {
+            SpecialPowers.popPrefEnv();
+          }
+        }
+      );
+      Assert.ok(wasCalled, "openFirefoxView did get called");
+    },
+  },
 ]; // gTests
 
 function makeObserver(aObserveTopic, aObserveFunc) {
