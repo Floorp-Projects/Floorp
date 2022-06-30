4578c6886f19b3da4601a23cc281d9c908883c8c
criss <ccozmuta@mozilla.com>
2022-06-30 05:28:11 +0300

    Backed out 3 changesets (bug 1771374) for causing bustages on Colorspaces.h. CLOSED TREE
    Backed out changeset 498c27656dc3 (bug 1771374)
    Backed out changeset 6f97bc39643f (bug 1771374)
    Backed out changeset b4fdb70808b6 (bug 1771374)

--- dom/canvas/WebGLIpdl.h
+++ dom/canvas/WebGLIpdl.h
@@ -248,12 +248,7 @@ struct ParamTraits<mozilla::WebGLContextOptions> final
   using T = mozilla::WebGLContextOptions;
 
   static bool Validate(const T& val) {
-    bool ok = true;
-    ok &= ValidateParam(val.powerPreference);
-    if (val.colorSpace) {
-      ok &= ValidateParam(*val.colorSpace);
-    }
-    return ok;
+    return ValidateParam(val.powerPreference) && ValidateParam(val.colorSpace);
   }
 };
 
