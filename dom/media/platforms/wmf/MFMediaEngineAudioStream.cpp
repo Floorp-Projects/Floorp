ee216457ab43cefc9ec2079236021dd79b5d8e3d
alwu <alwu@mozilla.com>
2022-06-30 18:14:44 +0000

    Bug 1758789 - part2 : create a custom media source. r=jolin
    Based on [1], we would need to create a custom media source and set it on the media engine. Media engine will interact with the source, and the streams in order to receive events and the decoded samples.
    
    [1] https://docs.microsoft.com/en-us/windows/win32/medfound/writing-a-custom-media-source
    
    Differential Revision: https://phabricator.services.mozilla.com/D144631

--- dom/media/platforms/wmf/MFMediaEngineAudioStream.cpp
+++ dom/media/platforms/wmf/MFMediaEngineAudioStream.cpp
@@ -4,15 +4,62 @@
 
 #include "MFMediaEngineAudioStream.h"
 
+#include "MFMediaEngineUtils.h"
+
 namespace mozilla {
 
-RefPtr<MediaDataDecoder::InitPromise> MFMediaEngineAudioStream::Init() {
-  // TODO : implement this by using MediaEngine API.
-  return InitPromise::CreateAndResolve(TrackType::kAudioTrack, __func__);
+#define LOGV(msg, ...)                          \
+  MOZ_LOG(gMFMediaEngineLog, LogLevel::Verbose, \
+          ("MFMediaStream=%p (%s), " msg, this, \
+           this->GetDescriptionName().get(), ##__VA_ARGS__))
+
+using Microsoft::WRL::ComPtr;
+using Microsoft::WRL::MakeAndInitialize;
+
+/* static */
+MFMediaEngineAudioStream* MFMediaEngineAudioStream::Create(
+    uint64_t aStreamId, const TrackInfo& aInfo, MFMediaSource* aParentSource) {
+  MOZ_ASSERT(aInfo.IsAudio());
+  MFMediaEngineAudioStream* stream;
+  if (FAILED(MakeAndInitialize<MFMediaEngineAudioStream>(
+          &stream, aStreamId, aInfo, aParentSource))) {
+    return nullptr;
+  }
+  return stream;
 }
 
-nsCString MFMediaEngineAudioStream::GetDescriptionName() const {
-  return "media engine audio stream"_ns;
+HRESULT MFMediaEngineAudioStream::CreateMediaType(const TrackInfo& aInfo,
+                                                  IMFMediaType** aMediaType) {
+  const AudioInfo& info = *aInfo.GetAsAudioInfo();
+  GUID subType = AudioMimeTypeToMediaFoundationSubtype(info.mMimeType);
+  NS_ENSURE_TRUE(subType != GUID_NULL, MF_E_TOPO_CODEC_NOT_FOUND);
+
+  // https://docs.microsoft.com/en-us/windows/win32/medfound/media-type-attributes
+  ComPtr<IMFMediaType> mediaType;
+  RETURN_IF_FAILED(wmf::MFCreateMediaType(&mediaType));
+  RETURN_IF_FAILED(mediaType->SetGUID(MF_MT_SUBTYPE, subType));
+  RETURN_IF_FAILED(mediaType->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Audio));
+  RETURN_IF_FAILED(
+      mediaType->SetUINT32(MF_MT_AUDIO_NUM_CHANNELS, info.mChannels));
+  RETURN_IF_FAILED(
+      mediaType->SetUINT32(MF_MT_AUDIO_SAMPLES_PER_SECOND, info.mRate));
+  uint64_t bitDepth = info.mBitDepth != 0 ? info.mBitDepth : 16;
+  RETURN_IF_FAILED(mediaType->SetUINT32(MF_MT_AUDIO_BITS_PER_SAMPLE, bitDepth));
+  LOGV("Created audio type, subtype=%s, channel=%" PRIu32 ", rate=%" PRIu32
+       ", bitDepth=%" PRIu64,
+       GUIDToStr(subType), info.mChannels, info.mRate, bitDepth);
+
+  *aMediaType = mediaType.Detach();
+  return S_OK;
 }
+
+bool MFMediaEngineAudioStream::HasEnoughRawData() const {
+  // If more than this much raw audio is queued, we'll hold off request more
+  // audio.
+  static const int64_t AMPLE_AUDIO_USECS = 2000000;
+  return mRawDataQueue.Duration() >= AMPLE_AUDIO_USECS;
+}
+
+#undef LOGV
 
 }  // namespace mozilla
