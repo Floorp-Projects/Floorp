bd31b518d0f8d8c0e5a956c3706f1b3fefb139da
Csoregi Natalia <ncsoregi@mozilla.com>
2022-06-30 17:25:47 +0300

    Backed out changeset 1491786f8402 (bug 1775194) for multiple reporting failures. CLOSED TREE

--- dom/serviceworkers/test/test_serviceworker_interfaces.js
+++ dom/serviceworkers/test/test_serviceworker_interfaces.js
@@ -273,6 +273,12 @@ var interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "ReadableStreamDefaultReader",
   // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "Report", nightly: true },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "ReportBody", nightly: true },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "ReportingObserver", nightly: true },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
   "Request",
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "Response",
