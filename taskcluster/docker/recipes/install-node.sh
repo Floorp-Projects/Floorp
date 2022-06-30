67cd5faa87b4cbfcaa20facbf4d284ea3a8d872c
Iulian Moraru <imoraru@mozilla.com>
2022-06-30 18:43:06 +0300

    Backed out 3 changesets (bug 1762571) for causing xpcshell failures on test_trr_httpssvc_wrap.js. CLOSED TREE
    Backed out changeset 9e6eb444acb4 (bug 1762571)
    Backed out changeset f24051722cee (bug 1762571)
    Backed out changeset 0ead2f6d0582 (bug 1762571)

--- taskcluster/docker/recipes/install-node.sh
+++ taskcluster/docker/recipes/install-node.sh
@@ -3,13 +3,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This script installs Node v16.
+# This script installs Node v12.
 # XXX For now, this should match the version installed in
 # taskcluster/scripts/misc/repack-node.sh. Later we'll get the ESLint builder
 # to use the linux64-node toolchain directly.
 
-wget -O node.xz --progress=dot:mega https://nodejs.org/download/release/v16.14.2/node-v16.14.2-linux-x64.tar.xz
-echo 'e40c6f81bfd078976d85296b5e657be19e06862497741ad82902d0704b34bb1b' node.xz | sha256sum -c
+wget -O node.xz --progress=dot:mega https://nodejs.org/download/release/v12.22.1/node-v12.22.1-linux-x64.tar.xz
+echo '8b537282c222ae4a40e019a52f769ca27b6640699bdde1510375e8d72da7d041' node.xz | sha256sum -c
 tar -C /usr/local -xJ --strip-components 1 < node.xz
 node -v  # verify
 npm -v
