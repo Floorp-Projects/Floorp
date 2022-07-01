281b84a80cfe5b8d0fd931fb986eebb9e4c7390d
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2022-07-01 01:35:32 +0000

    Bug 1530253 - Implement the logic to choose the best snap point for re-snap. r=botond
    Depends on D148861
    
    Differential Revision: https://phabricator.services.mozilla.com/D148862

--- gfx/layers/FrameMetrics.h
+++ gfx/layers/FrameMetrics.h
@@ -743,6 +743,16 @@ struct ScrollSnapInfo {
              mScrollSnapStop == aOther.mScrollSnapStop &&
              mTargetId == aOther.mTargetId;
     }
+    bool IsValidFor(const nsPoint& aDestination,
+                    const nsSize aSnapportSize) const {
+      nsPoint snapPoint(mSnapPositionX ? *mSnapPositionX : aDestination.x,
+                        mSnapPositionY ? *mSnapPositionY : aDestination.y);
+      nsRect snappedPort = nsRect(snapPoint, aSnapportSize);
+      // Ignore snap points if snapping to the point would leave the snap area
+      // outside of the snapport.
+      // https://drafts.csswg.org/css-scroll-snap-1/#snap-scope
+      return snappedPort.Intersects(mSnapArea);
+    }
   };
 
   CopyableTArray<SnapTarget> mSnapTargets;
