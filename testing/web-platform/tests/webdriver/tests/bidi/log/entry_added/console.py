32a29bfacbc3a916c48a9fe2a98016b8c31763dd
Alexandra Borovova <aborovova@mozilla.com>
2022-07-04 06:02:01 +0000

    Bug 1767260 - [wdspec] Add tests for context in log.entryAdded event. r=webdriver-reviewers,jdescottes
    Depends on D150406
    
    Differential Revision: https://phabricator.services.mozilla.com/D150559

--- testing/web-platform/tests/webdriver/tests/bidi/log/entry_added/console.py
+++ testing/web-platform/tests/webdriver/tests/bidi/log/entry_added/console.py
@@ -3,6 +3,8 @@ import time
 
 import pytest
 
+from webdriver.bidi.modules.script import ContextTarget
+
 from . import assert_console_entry
 
 
@@ -25,7 +27,12 @@ from . import assert_console_entry
     ],
 )
 async def test_text_with_argument_variation(
-    bidi_session, current_session, wait_for_event, log_argument, expected_text
+    bidi_session,
+    current_session,
+    wait_for_event,
+    log_argument,
+    expected_text,
+    top_context,
 ):
     await bidi_session.session.subscribe(events=["log.entryAdded"])
 
@@ -36,7 +43,7 @@ async def test_text_with_argument_variation(
 
     event_data = await on_entry_added
 
-    assert_console_entry(event_data, text=expected_text)
+    assert_console_entry(event_data, text=expected_text, context=top_context["context"])
 
 
 @pytest.mark.asyncio
@@ -103,21 +110,78 @@ async def test_timestamp(bidi_session, current_session, current_time, wait_for_e
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("new_context_method_name", ["refresh", "new_window"])
-async def test_new_context(
-    bidi_session, current_session, wait_for_event, new_context_method_name
+async def test_new_context_with_new_window(
+    bidi_session, current_session, wait_for_event, top_context
 ):
     await bidi_session.session.subscribe(events=["log.entryAdded"])
 
     on_entry_added = wait_for_event("log.entryAdded")
     current_session.execute_script("console.log('foo')")
     event_data = await on_entry_added
-    assert_console_entry(event_data, text="foo")
+    assert_console_entry(event_data, text="foo", context=top_context["context"])
 
-    new_context_method = getattr(current_session, new_context_method_name)
-    new_context_method()
+    new_window_handle = current_session.new_window()
+    current_session.window_handle = new_window_handle
+
+    on_entry_added = wait_for_event("log.entryAdded")
+    current_session.execute_script("console.log('foo_in_new_window')")
+    event_data = await on_entry_added
+    assert_console_entry(
+        event_data, text="foo_in_new_window", context=new_window_handle
+    )
+
+
+@pytest.mark.asyncio
+async def test_new_context_with_refresh(
+    bidi_session, current_session, wait_for_event, top_context
+):
+    await bidi_session.session.subscribe(events=["log.entryAdded"])
+
+    on_entry_added = wait_for_event("log.entryAdded")
+    current_session.execute_script("console.log('foo')")
+    event_data = await on_entry_added
+    assert_console_entry(event_data, text="foo", context=top_context["context"])
+
+    current_session.refresh()
 
     on_entry_added = wait_for_event("log.entryAdded")
     current_session.execute_script("console.log('foo_after_refresh')")
     event_data = await on_entry_added
-    assert_console_entry(event_data, text="foo_after_refresh")
+    assert_console_entry(
+        event_data, text="foo_after_refresh", context=top_context["context"]
+    )
+
+
+@pytest.mark.asyncio
+async def test_different_contexts(
+    bidi_session,
+    wait_for_event,
+    test_page_same_origin_frame,
+    top_context,
+):
+    await bidi_session.browsing_context.navigate(
+        context=top_context["context"], url=test_page_same_origin_frame, wait="complete"
+    )
+    contexts = await bidi_session.browsing_context.get_tree(root=top_context["context"])
+    assert len(contexts[0]["children"]) == 1
+    frame_context = contexts[0]["children"][0]
+
+    await bidi_session.session.subscribe(events=["log.entryAdded"])
+
+    on_entry_added = wait_for_event("log.entryAdded")
+    await bidi_session.script.evaluate(
+        expression="console.log('foo')",
+        target=ContextTarget(top_context["context"]),
+        await_promise=True,
+    )
+    event_data = await on_entry_added
+    assert_console_entry(event_data, text="foo", context=top_context["context"])
+
+    on_entry_added = wait_for_event("log.entryAdded")
+    await bidi_session.script.evaluate(
+        expression="console.log('bar')",
+        target=ContextTarget(frame_context["context"]),
+        await_promise=True,
+    )
+    event_data = await on_entry_added
+    assert_console_entry(event_data, text="bar", context=frame_context["context"])
