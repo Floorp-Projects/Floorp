b02d8613126bd0a744455498fc4708cb555ff90f
Gabriele Svelto <gsvelto@mozilla.com>
2022-07-04 14:00:11 +0000

    Bug 1774458 - Use undocumented, non-public adaptive spinlocks on macOS 10.15+, revert to user-space spinlocks on older versions r=pbone, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D149599

---
+++ memory/build/Mutex.cpp
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "Mutex.h"
+
+#if defined(XP_DARWIN)
+
+// static
+bool Mutex::UseUnfairLocks() {
+  if (__builtin_available(macOS 10.15, *)) {
+    return true;
+  }
+
+  return false;
+}
+
+// static
+bool Mutex::gFallbackToOSSpinLock = !UseUnfairLocks();
+
+#endif  // defined(XP_DARWIN)
