ba22c91145ffa610612faa63108f3dbe29d5ac7a
Edgar Chen <echen@mozilla.com>
2022-07-04 15:34:45 +0000

    Bug 1777517 - [Cocoa] Add clipboard logs; r=NeilDeakin
    Differential Revision: https://phabricator.services.mozilla.com/D150790

--- widget/nsBaseClipboard.h
+++ widget/nsBaseClipboard.h
@@ -6,9 +6,16 @@
 #ifndef nsBaseClipboard_h__
 #define nsBaseClipboard_h__
 
+#include "mozilla/Logging.h"
 #include "nsIClipboard.h"
 #include "nsITransferable.h"
 #include "nsCOMPtr.h"
+
+static mozilla::LazyLogModule sWidgetClipboardLog("WidgetClipboard");
+#define CLIPBOARD_LOG(...) \
+  MOZ_LOG(sWidgetClipboardLog, LogLevel::Debug, (__VA_ARGS__))
+#define CLIPBOARD_LOG_ENABLED() \
+  MOZ_LOG_TEST(sWidgetClipboardLog, LogLevel::Debug)
 
 class nsITransferable;
 class nsIClipboardOwner;
