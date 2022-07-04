64fa4b33098ebde24e194ac539c1f2479f394f14
Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>
2022-07-04 21:58:06 +0000

    Bug 1777173 [wpt PR 34641] - Align `browsingContext.contextCreated` tests with spec, a=testonly
    Automatic update from web-platform-tests
    Align `browsingContext.contextCreated` tests with spec (#34641)
    
    Co-authored-by: Julian Descottes <julian.descottes@gmail.com>
    Co-authored-by: Maksim Sadym <sadym@google.com>
    
    --
    
    wpt-commits: e351644422d3ffb680b195e6a590b61528db329a
    wpt-pr: 34641

--- testing/web-platform/tests/webdriver/tests/bidi/browsing_context/context_created/context_created.py
+++ testing/web-platform/tests/webdriver/tests/bidi/browsing_context/context_created/context_created.py
@@ -134,11 +134,13 @@ async def test_navigate_creates_iframes(bidi_session, top_context, test_page_mul
     children_info = root_info["children"]
     assert len(children_info) == 2
 
+    # Note: Live `browsingContext.contextCreated` events are always created with "about:blank":
+    # https://github.com/w3c/webdriver-bidi/issues/220#issuecomment-1145785349
     assert_browsing_context(
         events[0],
         children_info[0]["context"],
         children=None,
-        url=children_info[0]["url"],
+        url="about:blank",
         parent=root_info["context"],
     )
 
@@ -146,7 +148,7 @@ async def test_navigate_creates_iframes(bidi_session, top_context, test_page_mul
         events[1],
         children_info[1]["context"],
         children=None,
-        url=children_info[1]["url"],
+        url="about:blank",
         parent=root_info["context"],
     )
 
@@ -186,11 +188,13 @@ async def test_navigate_creates_nested_iframes(bidi_session, top_context, test_p
     assert len(child1_info["children"]) == 1
     child2_info = child1_info["children"][0]
 
+    # Note: `browsingContext.contextCreated` is always created with "about:blank":
+    # https://github.com/w3c/webdriver-bidi/issues/220#issuecomment-1145785349
     assert_browsing_context(
         events[0],
         child1_info["context"],
         children=None,
-        url=child1_info["url"],
+        url="about:blank",
         parent=root_info["context"],
     )
 
@@ -198,7 +202,7 @@ async def test_navigate_creates_nested_iframes(bidi_session, top_context, test_p
         events[1],
         child2_info["context"],
         children=None,
-        url=child2_info["url"],
+        url="about:blank",
         parent=child1_info["context"],
     )
 
