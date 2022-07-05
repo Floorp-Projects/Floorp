b169afbd0f4605ee1856565309f42df5dff8685d
Jim Blandy <jimb@red-bean.com>
2022-07-05 18:03:36 +0000

    Bug 1771255: Help Device::Cleanup cope when Device::mPromise's JSObject has been cleared. r=nical
    Differential Revision: https://phabricator.services.mozilla.com/D150624

--- dom/webgpu/Device.cpp
+++ dom/webgpu/Device.cpp
@@ -79,7 +79,8 @@ void Device::Cleanup() {
     mBridge->UnregisterDevice(mId);
   }
 
-  if (mLostPromise) {
+  // Cycle collection may have disconnected the promise object.
+  if (mLostPromise && mLostPromise->PromiseObj() != nullptr) {
     auto info = MakeRefPtr<DeviceLostInfo>(GetParentObject(),
                                            dom::GPUDeviceLostReason::Destroyed,
                                            u"Device destroyed"_ns);
