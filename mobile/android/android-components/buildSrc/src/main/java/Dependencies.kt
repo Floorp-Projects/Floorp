71b53d13af65b85beff28faba56667a245c0e3e3
mcarare <mihai.carare.dev@gmail.com>
2022-07-05 09:59:12 +0300

    [components] For https://github.com/mozilla-mobile/android-components/issues/12381: Sync android lint version to gradle plugin version.

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -19,7 +19,10 @@ object Versions {
 
     const val android_gradle_plugin = "7.2.2"
     const val android_maven_publish_plugin = "3.6.2"
-    const val lint = "27.0.1"
+
+    // This has to be synced to the gradlew plugin version. See
+    // http://googlesamples.github.io/android-custom-lint-rules/api-guide/example.md.html#example:samplelintcheckgithubproject/lintversion?
+    const val lint = "30.2.2"
     const val detekt = "1.19.0"
 
     const val sentry_legacy = "1.7.21"
