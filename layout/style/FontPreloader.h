640cf989f7960c0550531de826a850c377da987a
Andrew Osmond <aosmond@mozilla.com>
2022-07-07 21:24:08 +0000

    Bug 1072107 - Part 1. Expose method to load user fonts from workers. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D149244

--- layout/style/FontPreloader.h
+++ layout/style/FontPreloader.h
@@ -8,11 +8,19 @@
 #define FontPreloader_h_
 
 #include "mozilla/FetchPreloader.h"
+#include "nsContentSecurityManager.h"
+#include "nsIContentPolicy.h"
+#include "nsILoadInfo.h"
 
 class gfxUserFontEntry;
 struct gfxFontFaceSrc;
+class nsIHttpChannel;
+class nsIReferrerInfo;
 
 namespace mozilla {
+namespace dom {
+class WorkerPrivate;
+}
 
 class FontPreloader final : public FetchPreloader {
  public:
@@ -29,12 +37,29 @@ class FontPreloader final : public FetchPreloader {
       dom::Document* aDocument, nsILoadGroup* aLoadGroup,
       nsIInterfaceRequestor* aCallbacks, bool aIsPreload);
 
+  static nsresult BuildChannel(
+      nsIChannel** aChannel, nsIURI* aURI, const CORSMode aCORSMode,
+      const dom::ReferrerPolicy& aReferrerPolicy,
+      gfxUserFontEntry* aUserFontEntry, const gfxFontFaceSrc* aFontFaceSrc,
+      dom::WorkerPrivate* aWorkerPrivate, nsILoadGroup* aLoadGroup,
+      nsIInterfaceRequestor* aCallbacks, bool aIsPreload);
+
  protected:
   nsresult CreateChannel(nsIChannel** aChannel, nsIURI* aURI,
                          const CORSMode aCORSMode,
                          const dom::ReferrerPolicy& aReferrerPolicy,
                          dom::Document* aDocument, nsILoadGroup* aLoadGroup,
                          nsIInterfaceRequestor* aCallbacks) override;
+
+  static void BuildChannelFlags(
+      nsIURI* aURI, bool aIsPreload,
+      nsContentSecurityManager::CORSSecurityMapping& aCorsMapping,
+      nsSecurityFlags& aSecurityFlags, nsContentPolicyType& aContentPolicyType);
+
+  static nsresult BuildChannelSetup(nsIChannel* aChannel,
+                                    nsIHttpChannel* aHttpChannel,
+                                    nsIReferrerInfo* aReferrerInfo,
+                                    const gfxFontFaceSrc* aFontFaceSrc);
 };
 
 }  // namespace mozilla
