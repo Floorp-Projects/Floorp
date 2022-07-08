6882715e725b6a7cf31eeb02f7d4c5d2a1be7b70
lyavor <lyavor@mozilla.com>
2022-07-08 14:10:23 +0000

    Bug 1778467 - Update browser/base/content/test/sanitize/ test files to use https r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D151260

--- browser/base/content/test/sanitize/browser_sanitize-passwordDisabledHosts.js
+++ browser/base/content/test/sanitize/browser_sanitize-passwordDisabledHosts.js
@@ -6,10 +6,10 @@ add_task(async function() {
   await SpecialPowers.pushPrefEnv({ set: [["signon.rememberSignons", true]] });
 
   // Add a disabled host
-  Services.logins.setLoginSavingEnabled("http://example.com", false);
+  Services.logins.setLoginSavingEnabled("https://example.com", false);
   // Sanity check
   is(
-    Services.logins.getLoginSavingEnabled("http://example.com"),
+    Services.logins.getLoginSavingEnabled("https://example.com"),
     false,
     "example.com should be disabled for password saving since we haven't cleared that yet."
   );
@@ -19,7 +19,7 @@ add_task(async function() {
 
   // Make sure it's gone
   is(
-    Services.logins.getLoginSavingEnabled("http://example.com"),
+    Services.logins.getLoginSavingEnabled("https://example.com"),
     true,
     "example.com should be enabled for password saving again now that we've cleared."
   );
