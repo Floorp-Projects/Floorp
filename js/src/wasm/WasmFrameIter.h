5019e77a1ff0d29a38edda2abd44af3494557597
Jan de Mooij <jdemooij@mozilla.com>
2022-07-09 17:33:56 +0000

    Bug 1778466 part 4 - Simplify ProfilingFrameIterator::done. r=rhunt
    Depends on D151248
    
    Differential Revision: https://phabricator.services.mozilla.com/D151249

--- js/src/wasm/WasmFrameIter.h
+++ js/src/wasm/WasmFrameIter.h
@@ -192,7 +192,11 @@ class ProfilingFrameIterator {
                          const RegisterState& state);
 
   void operator++();
-  bool done() const { return !codeRange_ && exitReason_.isNone(); }
+
+  bool done() const {
+    MOZ_ASSERT_IF(!exitReason_.isNone(), codeRange_);
+    return !codeRange_;
+  }
 
   void* stackAddress() const {
     MOZ_ASSERT(!done());
