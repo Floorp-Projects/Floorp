e82dbf236f4af2bf35893a66c2938a6da7a71aab
Michael Froman <mfroman@mozilla.com>
2022-07-09 16:35:15 -0500

    Bug 1766646 - Vendor libwebrtc from a8b63626cab
    Upstream commit: https://webrtc.googlesource.com/src/+/a8b63626cab3af53bd748e5bd3b03938c6c14b51
        Fix race when checking for field trial from the encoder queue.
    
        Bug: webrtc:13633
        Change-Id: I9eed5b849ad8b9d739261a119b8bffb3033951be
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/249948
        Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
        Auto-Submit: Tomas Gunnarsson <tommi@webrtc.org>
        Reviewed-by: Henrik Boström <hbos@webrtc.org>
        Commit-Queue: Henrik Boström <hbos@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#35849}

--- third_party/libwebrtc/video/adaptation/video_stream_encoder_resource_manager.cc
+++ third_party/libwebrtc/video/adaptation/video_stream_encoder_resource_manager.cc
@@ -288,6 +288,8 @@ VideoStreamEncoderResourceManager::VideoStreamEncoderResourceManager(
       initial_frame_dropper_(
           std::make_unique<InitialFrameDropper>(quality_scaler_resource_)),
       quality_scaling_experiment_enabled_(QualityScalingExperiment::Enabled()),
+      pixel_limit_resource_experiment_enabled_(
+          field_trial::IsEnabled(kPixelLimitResourceFieldTrialName)),
       encoder_target_bitrate_bps_(absl::nullopt),
       quality_rampup_experiment_(
           QualityRampUpExperimentHelper::CreateIfEnabled(this, clock_)),
@@ -350,7 +352,7 @@ void VideoStreamEncoderResourceManager::MaybeInitializePixelLimitResource() {
   RTC_DCHECK_RUN_ON(encoder_queue_);
   RTC_DCHECK(adaptation_processor_);
   RTC_DCHECK(!pixel_limit_resource_);
-  if (!field_trial::IsEnabled(kPixelLimitResourceFieldTrialName)) {
+  if (!pixel_limit_resource_experiment_enabled_) {
     // The field trial is not running.
     return;
   }
