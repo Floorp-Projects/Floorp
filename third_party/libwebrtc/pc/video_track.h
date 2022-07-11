03063d0f2dfca029a4053edf40321e4759ca5b16
Michael Froman <mfroman@mozilla.com>
2022-07-11 15:24:24 -0500

    Bug 1766646 - Vendor libwebrtc from 962bf189618
    Upstream commit: https://webrtc.googlesource.com/src/+/962bf1896185c0d84232f2bfae492eeb04e1236d
        Use internal() in VideoTrack when invoking the source.
    
        This skips going through the proxy and potentially hide a thread hop
        should a regression occur.
    
        This CL contains a part of a previously reviewed, landed, reverted,
        relanded and re-reverted CL:
        https://webrtc-review.googlesource.com/c/src/+/250180
    
        Bug: webrtc:13540
        Change-Id: If098f5c04a263547fb53f44e9f9738b8e941a294
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/251861
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36026}

--- third_party/libwebrtc/pc/video_track.h
+++ third_party/libwebrtc/pc/video_track.h
@@ -54,6 +54,9 @@ class VideoTrack : public MediaStreamTrack<VideoTrackInterface>,
   MediaStreamTrackInterface::TrackState state() const override;
   std::string kind() const override;
 
+  // Direct access to the non-proxied source object for internal implementation.
+  VideoTrackSourceInterface* GetSourceInternal() const;
+
  protected:
   VideoTrack(
       const std::string& id,
