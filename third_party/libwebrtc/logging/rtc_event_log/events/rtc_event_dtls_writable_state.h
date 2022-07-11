d6cae654fcc4f47ede4515a1c4847b8392a8a6a8
Michael Froman <mfroman@mozilla.com>
2022-07-11 16:09:35 -0500

    Bug 1766646 - Vendor libwebrtc from 7a992e21b9c
    Upstream commit: https://webrtc.googlesource.com/src/+/7a992e21b9c3ff03aa34488a9c91fa580ca4faa6
        Reland "Refactor AnalyzerConfig to use Timestamps instead of microseconds."
    
        This is a reland of 43fb16921b29ecd3a2d87876dda75c575e05f66a
    
        Original change's description:
        > Refactor AnalyzerConfig to use Timestamps instead of microseconds.
        >
        > Add optional offset-to-UTC parameter to output. This allows aligning
        > the x-axis in the generated charts to other UTC-based logs.
        >
        > Bug: b/215140373
        > Change-Id: I65bcd295718acbb8c94e363907c1abc458067bfd
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/250203
        > Reviewed-by: Kristoffer Erlandsson <kerl@google.com>
        > Commit-Queue: Björn Terelius <terelius@webrtc.org>
        > Cr-Commit-Position: refs/heads/main@{#35992}
    
        Bug: b/215140373
        Change-Id: Id2b88cc4b8078a97275d49a617581cbbd02d2c6f
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/252380
        Reviewed-by: Kristoffer Erlandsson <kerl@google.com>
        Commit-Queue: Björn Terelius <terelius@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36066}

--- third_party/libwebrtc/logging/rtc_event_log/events/rtc_event_dtls_writable_state.h
+++ third_party/libwebrtc/logging/rtc_event_log/events/rtc_event_dtls_writable_state.h
@@ -28,6 +28,7 @@ struct LoggedDtlsWritableState {
 
   int64_t log_time_us() const { return timestamp.us(); }
   int64_t log_time_ms() const { return timestamp.ms(); }
+  Timestamp log_time() const { return timestamp; }
 
   Timestamp timestamp = Timestamp::MinusInfinity();
   bool writable;
