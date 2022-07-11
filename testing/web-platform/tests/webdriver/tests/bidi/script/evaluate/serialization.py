8c62f854e5f0de8a2efa4249e98d6523268102ba
Alexandra Borovova <lutien@users.noreply.github.com>
2022-07-11 22:59:20 +0000

    Bug 1777978 [wpt PR 34691] - Add tests for `script.evaluate` for serialization of complex objects with simple value field., a=testonly
    Automatic update from web-platform-tests
    Add tests for `script.evaluate` for serialization of complex objects with simple value field. (#34691)
    
    --
    
    wpt-commits: 6eb6639440c5345151d357ec43afe3742704b545
    wpt-pr: 34691

--- testing/web-platform/tests/webdriver/tests/bidi/script/evaluate/serialization.py
+++ testing/web-platform/tests/webdriver/tests/bidi/script/evaluate/serialization.py
@@ -9,6 +9,7 @@ from webdriver.bidi.modules.script import ContextTarget
         ("undefined", {"type": "undefined"}),
         ("null", {"type": "null"}),
         ("'foobar'", {"type": "string", "value": "foobar"}),
+        ("'2'", {"type": "string", "value": "2"}),
         ("Number.NaN", {"type": "number", "value": "NaN"}),
         ("-0", {"type": "number", "value": "-0"}),
         ("Infinity", {"type": "number", "value": "+Infinity"}),
@@ -21,6 +22,100 @@ from webdriver.bidi.modules.script import ContextTarget
     ],
 )
 async def test_primitive_values(bidi_session, top_context, expression, expected):
+    result = await bidi_session.script.evaluate(
+        expression=expression,
+        target=ContextTarget(top_context["context"]),
+        await_promise=True,
+    )
+
+    assert result == expected
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "expression, expected",
+    [
+        (
+            "new RegExp(/foo/g)",
+            {
+                "type": "regexp",
+                "value": {
+                    "pattern": "foo",
+                    "flags": "g",
+                },
+            },
+        ),
+        (
+            "new Date(1654004849000)",
+            {
+                "type": "date",
+                "value": "2022-05-31T13:47:29.000Z",
+            },
+        ),
+        (
+            "[1, 'foo', true, new RegExp(/foo/g), [1]]",
+            {
+                "type": "array",
+                "value": [
+                    {"type": "number", "value": 1},
+                    {"type": "string", "value": "foo"},
+                    {"type": "boolean", "value": True},
+                    {
+                        "type": "regexp",
+                        "value": {
+                            "pattern": "foo",
+                            "flags": "g",
+                        },
+                    },
+                    {"type": "array"},
+                ],
+            },
+        ),
+        (
+            "new Map([[1, 2], ['foo', 'bar'], [true, false], ['baz', [1]]])",
+            {
+                "type": "map",
+                "value": [
+                    [
+                        {"type": "number", "value": 1},
+                        {"type": "number", "value": 2},
+                    ],
+                    ["foo", {"type": "string", "value": "bar"}],
+                    [
+                        {"type": "boolean", "value": True},
+                        {"type": "boolean", "value": False},
+                    ],
+                    ["baz", {"type": "array"}],
+                ],
+            },
+        ),
+        (
+            "new Set([1, 'foo', true, [1], new Map([[1,2]])])",
+            {
+                "type": "set",
+                "value": [
+                    {"type": "number", "value": 1},
+                    {"type": "string", "value": "foo"},
+                    {"type": "boolean", "value": True},
+                    {"type": "array"},
+                    {"type": "map"},
+                ],
+            },
+        ),
+        (
+            "({'foo': {'bar': 'baz'}, 'qux': 'quux'})",
+            {
+                "type": "object",
+                "value": [
+                    ["foo", {"type": "object"}],
+                    ["qux", {"type": "string", "value": "quux"}],
+                ],
+            },
+        ),
+    ],
+    ids=["regexp", "date", "array", "map", "set", "object"],
+)
+async def test_remote_values(bidi_session, top_context, expression, expected):
     result = await bidi_session.script.evaluate(
         expression=expression,
         target=ContextTarget(top_context["context"]),
