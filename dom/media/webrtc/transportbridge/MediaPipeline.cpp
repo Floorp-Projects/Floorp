1fd8176925812987ce036a63252f8694bd7067af
Michael Froman <mfroman@mozilla.com>
2022-07-11 10:17:12 -0500

    Bug 1766646 (MOZ) - fixes for upstream removing implicit conversion from raw pointer to scoped_ref_ptr

--- dom/media/webrtc/transportbridge/MediaPipeline.cpp
+++ dom/media/webrtc/transportbridge/MediaPipeline.cpp
@@ -1522,8 +1522,8 @@ class MediaPipelineReceiveVideo::PipelineListener
       image = imageBuffer->GetNativeImage();
     } else {
       MOZ_ASSERT(aBuffer.type() == webrtc::VideoFrameBuffer::Type::kI420);
-      rtc::scoped_refptr<const webrtc::I420BufferInterface> i420 =
-          aBuffer.GetI420();
+      rtc::scoped_refptr<const webrtc::I420BufferInterface> i420(
+          aBuffer.GetI420());
 
       MOZ_ASSERT(i420->DataY());
       // Create a video frame using |buffer|.
