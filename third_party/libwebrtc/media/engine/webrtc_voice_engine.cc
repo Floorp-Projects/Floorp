a3f9a83eca1818795f72050b27d43da5e12b0423
Michael Froman <mfroman@mozilla.com>
2022-07-11 17:44:53 -0500

    Bug 1766646 - Vendor libwebrtc from ff05c5c805
    Upstream commit: https://webrtc.googlesource.com/src/+/ff05c5c805e2a7652507c3f610c66e4676268933
        audio/red: cleanup killswitch
    
        this has been enable by default since M96
    
        BUG=webrtc:11640
    
        Change-Id: I5d310d3929882007211eae12bc3ac1366107ca87
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/253400
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
        Cr-Commit-Position: refs/heads/main@{#36123}

--- third_party/libwebrtc/media/engine/webrtc_voice_engine.cc
+++ third_party/libwebrtc/media/engine/webrtc_voice_engine.cc
@@ -210,11 +210,6 @@ bool IsEnabled(const webrtc::WebRtcKeyValueConfig& config,
   return absl::StartsWith(config.Lookup(trial), "Enabled");
 }
 
-bool IsDisabled(const webrtc::WebRtcKeyValueConfig& config,
-                absl::string_view trial) {
-  return absl::StartsWith(config.Lookup(trial), "Disabled");
-}
-
 struct AdaptivePtimeConfig {
   bool enabled = false;
   webrtc::DataRate min_payload_bitrate = webrtc::DataRate::KilobitsPerSec(16);
@@ -311,8 +306,6 @@ WebRtcVoiceEngine::WebRtcVoiceEngine(
       audio_mixer_(audio_mixer),
       apm_(audio_processing),
       audio_frame_processor_(audio_frame_processor),
-      audio_red_for_opus_enabled_(
-          !IsDisabled(trials, "WebRTC-Audio-Red-For-Opus")),
       minimized_remsampling_on_mobile_trial_enabled_(
           IsEnabled(trials, "WebRTC-Audio-MinimizeResamplingOnMobile")) {
   // This may be called from any thread, so detach thread checkers.
@@ -738,7 +731,7 @@ std::vector<AudioCodec> WebRtcVoiceEngine::CollectCodecs(
 
       out.push_back(codec);
 
-      if (codec.name == kOpusCodecName && audio_red_for_opus_enabled_) {
+      if (codec.name == kOpusCodecName) {
         std::string redFmtp =
             rtc::ToString(codec.id) + "/" + rtc::ToString(codec.id);
         map_format({kRedCodecName, 48000, 2, {{"", redFmtp}}}, &out);
@@ -1311,9 +1304,7 @@ WebRtcVoiceMediaChannel::WebRtcVoiceMediaChannel(
       engine_(engine),
       call_(call),
       audio_config_(config.audio),
-      crypto_options_(crypto_options),
-      audio_red_for_opus_enabled_(
-          !IsDisabled(call->trials(), "WebRTC-Audio-Red-For-Opus")) {
+      crypto_options_(crypto_options) {
   network_thread_checker_.Detach();
   RTC_LOG(LS_VERBOSE) << "WebRtcVoiceMediaChannel::WebRtcVoiceMediaChannel";
   RTC_DCHECK(call);
@@ -1574,7 +1565,7 @@ bool WebRtcVoiceMediaChannel::SetRecvCodecs(
     }
     auto format = AudioCodecToSdpAudioFormat(codec);
     if (!IsCodec(codec, kCnCodecName) && !IsCodec(codec, kDtmfCodecName) &&
-        (!audio_red_for_opus_enabled_ || !IsCodec(codec, kRedCodecName)) &&
+        !IsCodec(codec, kRedCodecName) &&
         !engine()->decoder_factory_->IsSupportedDecoder(format)) {
       RTC_LOG(LS_ERROR) << "Unsupported codec: " << rtc::ToString(format);
       return false;
@@ -1760,19 +1751,17 @@ bool WebRtcVoiceMediaChannel::SetSendCodecs(
     }
   }
 
-  if (audio_red_for_opus_enabled_) {
-    // Loop through the codecs to find the RED codec that matches opus
-    // with respect to clockrate and number of channels.
-    size_t red_codec_position = 0;
-    for (const AudioCodec& red_codec : codecs) {
-      if (red_codec_position < send_codec_position &&
-          IsCodec(red_codec, kRedCodecName) &&
-          CheckRedParameters(red_codec, *send_codec_spec)) {
-        send_codec_spec->red_payload_type = red_codec.id;
-        break;
-      }
-      red_codec_position++;
+  // Loop through the codecs to find the RED codec that matches opus
+  // with respect to clockrate and number of channels.
+  size_t red_codec_position = 0;
+  for (const AudioCodec& red_codec : codecs) {
+    if (red_codec_position < send_codec_position &&
+        IsCodec(red_codec, kRedCodecName) &&
+        CheckRedParameters(red_codec, *send_codec_spec)) {
+      send_codec_spec->red_payload_type = red_codec.id;
+      break;
     }
+    red_codec_position++;
   }
 
   if (send_codec_spec_ != send_codec_spec) {
