f5ed9ad93d37bc8a775541d26164f057ba78180f
Tooru Fujisawa <arai_a@mac.com>
2022-07-11 15:09:14 +0000

    Bug 1777486 - Part 3: Migrate XPCOMUtils.jsm consumers with manual rewrite. r=kmag
    Migrate some files that's not covered by ./mach esmify
    
    Differential Revision: https://phabricator.services.mozilla.com/D151215

--- devtools/server/tests/xpcshell/test_safe-getter.js
+++ devtools/server/tests/xpcshell/test_safe-getter.js
@@ -22,8 +22,8 @@ function run_test() {
       enumerable: true
     });
 
-    const { XPCOMUtils } = ChromeUtils.import(
-      "resource://gre/modules/XPCOMUtils.jsm"
+    const { XPCOMUtils } = ChromeUtils.importESModule(
+      "resource://gre/modules/XPCOMUtils.sys.mjs"
     );
 
     // This is a CCW.
