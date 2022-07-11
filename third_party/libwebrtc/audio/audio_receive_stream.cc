80fd2f065d26b79b75116b43b6d7756b7c64e5b7
Michael Froman <mfroman@mozilla.com>
2022-07-11 16:40:42 -0500

    Bug 1766646 - Vendor libwebrtc from 4d278e2caf0
    Upstream commit: https://webrtc.googlesource.com/src/+/4d278e2caf07d6e627c9d5f67b20f6ecfb63cdd5
        Adopt absl::string_view in function parameters under audio/
    
        This is part of a large-scale effort to adopt absl::string_view
        throughout the WebRTC code base. As a first step, function parameters
        of type 'const std::string&' are being converted to absl::string_view.
    
        Bug: webrtc:13579
        Change-Id: Ib4618fad3bff2902cd3a4730506aca300949d76c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/252982
        Reviewed-by: Stefan Holmer <stefan@webrtc.org>
        Commit-Queue: Ali Tofigh <alito@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36102}

--- third_party/libwebrtc/audio/audio_receive_stream.cc
+++ third_party/libwebrtc/audio/audio_receive_stream.cc
@@ -454,9 +454,9 @@ void AudioReceiveStream::DeliverRtcp(const uint8_t* packet, size_t length) {
   channel_receive_->ReceivedRTCPPacket(packet, length);
 }
 
-void AudioReceiveStream::SetSyncGroup(const std::string& sync_group) {
+void AudioReceiveStream::SetSyncGroup(absl::string_view sync_group) {
   RTC_DCHECK_RUN_ON(&packet_sequence_checker_);
-  config_.sync_group = sync_group;
+  config_.sync_group = std::string(sync_group);
 }
 
 void AudioReceiveStream::SetLocalSsrc(uint32_t local_ssrc) {
