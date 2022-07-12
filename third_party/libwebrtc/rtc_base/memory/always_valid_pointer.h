dee7f025fd6cafe726d44808abc12163a7e95c16
Michael Froman <mfroman@mozilla.com>
2022-07-12 09:13:41 -0500

    Bug 1766646 - Vendor libwebrtc from 66eb789b41
    Upstream commit: https://webrtc.googlesource.com/src/+/66eb789b41232ecedcca61b987d0860f990938b1
        Add class for pointer or owned object.
    
        To be used as part of field trial conversion effort.
    
        Bug: webrtc:10335
        Change-Id: Iaeff520d5a83331926ead945c9e414716e61cac8
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/256013
        Reviewed-by: Niels Moller <nisse@webrtc.org>
        Reviewed-by: Tomas Gunnarsson <tommi@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Jonas Oreland <jonaso@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36259}

---
+++ third_party/libwebrtc/rtc_base/memory/always_valid_pointer.h
@@ -0,0 +1,40 @@
+/*
+ *  Copyright 2022 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef RTC_BASE_MEMORY_ALWAYS_VALID_POINTER_H_
+#define RTC_BASE_MEMORY_ALWAYS_VALID_POINTER_H_
+
+#include <memory>
+
+namespace webrtc {
+
+// This template allows the instantiation of a pointer to Interface in such a
+// way that if it is passed a null pointer, an object of class Default will be
+// created, which will be deallocated when the pointer is deleted.
+template <typename Interface, typename Default>
+class AlwaysValidPointer {
+ public:
+  explicit AlwaysValidPointer(Interface* pointer)
+      : owned_instance_(pointer ? nullptr : std::make_unique<Default>()),
+        pointer_(pointer ? pointer : owned_instance_.get()) {
+    RTC_DCHECK(pointer_);
+  }
+
+  Interface* get() { return pointer_; }
+  Interface* operator->() { return pointer_; }
+  Interface& operator*() { return *pointer_; }
+
+ private:
+  const std::unique_ptr<Interface> owned_instance_;
+  Interface* const pointer_;
+};
+
+}  // namespace webrtc
+
+#endif  // RTC_BASE_MEMORY_ALWAYS_VALID_POINTER_H_
