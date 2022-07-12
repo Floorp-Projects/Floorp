a29c5f9284c81279fecb8db8f1bb6146d7a95038
Michael Froman <mfroman@mozilla.com>
2022-07-12 10:12:08 -0500

    Bug 1766646 - Vendor libwebrtc from 824d8b0263
    Upstream commit: https://webrtc.googlesource.com/src/+/824d8b026329533a94aa5b1088474f750b6f2572
        Don't Prune a to-be-deleted connection object
    
        Bug: webrtc:13865
        Change-Id: I87c18c8a1dfc0284bef1e721a56ad61ab89dc1c3
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/256680
        Reviewed-by: Niels Moller <nisse@webrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36320}

--- third_party/libwebrtc/p2p/base/connection.cc
+++ third_party/libwebrtc/p2p/base/connection.cc
@@ -864,6 +864,21 @@ void Connection::FailAndDestroy() {
 
 void Connection::FailAndPrune() {
   RTC_DCHECK_RUN_ON(network_thread_);
+
+  // TODO(bugs.webrtc.org/13865): There's a circular dependency between Port
+  // and Connection. In some cases (Port dtor), a Connection object is deleted
+  // without using the `Destroy` method (pending_delete_ won't be raised and
+  // some functionality won't run as expected), while in other cases
+  // (AddOrReplaceConnection), the Connection object is deleted asynchronously
+  // via the `Destroy` method and in that case `pending_delete_` will be raised.
+  // However, in that case, there's a chance that the Port object gets
+  // deleted before the Connection object ends up being deleted. So, the
+  // Connection object holds on to a potentially bogus `port_` pointer.
+  // Here, we avoid such a potential, but the cleanup operations in Port
+  // need to be made consistent and safe.
+  if (pending_delete_)
+    return;
+
   set_state(IceCandidatePairState::FAILED);
   Prune();
 }
