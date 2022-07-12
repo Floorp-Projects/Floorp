7b695e5c3f5aee55c9fcb205466aeae510d8781a
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2022-07-12 14:46:53 +0000

    Bug 1742797 - Add a new IPC call to clear animation resources on WebRender, r=sotaro.
    Differential Revision: https://phabricator.services.mozilla.com/D151481

--- widget/nsBaseWidget.cpp
+++ widget/nsBaseWidget.cpp
@@ -1368,6 +1368,13 @@ void nsBaseWidget::ClearCachedWebrenderResources() {
   mWindowRenderer->AsWebRender()->ClearCachedResources();
 }
 
+void nsBaseWidget::ClearWebrenderAnimationResources() {
+  if (!mWindowRenderer || !mWindowRenderer->AsWebRender()) {
+    return;
+  }
+  mWindowRenderer->AsWebRender()->ClearAnimationResources();
+}
+
 bool nsBaseWidget::SetNeedFastSnaphot() {
   MOZ_ASSERT(XRE_IsParentProcess());
   MOZ_ASSERT(!mCompositorSession);
