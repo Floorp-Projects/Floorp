08a11b854ef06e38225ccd7f4fc12a62bcf61867
Michael Froman <mfroman@mozilla.com>
2022-07-12 09:41:25 -0500

    Bug 1766646 - Vendor libwebrtc from df59e53818
    Upstream commit: https://webrtc.googlesource.com/src/+/df59e5381827113c3eda208a92ce97cce7022c17
        Reland "Pacer: Reduce TQ wake up and improve packet size estimation"
    
        Update `early_execute_margin` after process packets, and the test case.
    
        Original change's description:
        >Pacer: Reduce TQ wake up and improve packet size estimation
        >
        >The TQ Pacer schedules delayed task according to target time of
        >PacingController. It drains all valid ProcessPackets() in single loop,
        >denies retired scheduled tasks, and round up the timeout to 1ms.
        >
        >This CL also improves packet size estimation in TQ Pacer by removing
        >zero initialization, and introduces `include_overhead_` configuration.
        >
        >Tests:
        >1. webrtc_perf_tests: MaybeProcessPackets() calls
        >  2075147 -> 2007995
        >
        >2. module_unittests: MaybeProcessPackets() calls
        >  203393 -> 183563
        >
        >3. peerconnection_unittests: MaybeProcessPackets() calls
        >  66713-> 64333
        >
        >Bug: webrtc:13417, webrtc:13437
        >Change-Id: I18eb0a36dbe063c606b1f27014df74a65ebfc486
        >Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/242962
        >Reviewed-by: Erik Språng <sprang@webrtc.org>
        >Reviewed-by: Henrik Boström <hbos@webrtc.org>
        >Commit-Queue: Erik Språng <sprang@webrtc.org>
        >Cr-Commit-Position: refs/heads/main@{#36179}
    
        Bug: webrtc:13417, webrtc:13437
        Change-Id: I79f2554cf02364b67ce7073698611a3ae337a73b
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/256145
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Reviewed-by: Henrik Boström <hbos@webrtc.org>
        Owners-Override: Erik Språng <sprang@webrtc.org>
        Commit-Queue: Erik Språng <sprang@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36283}

--- third_party/libwebrtc/modules/pacing/task_queue_paced_sender.h
+++ third_party/libwebrtc/modules/pacing/task_queue_paced_sender.h
@@ -14,9 +14,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include <functional>
 #include <memory>
-#include <queue>
 #include <vector>
 
 #include "absl/base/attributes.h"
@@ -31,7 +29,6 @@
 #include "modules/pacing/rtp_packet_pacer.h"
 #include "modules/rtp_rtcp/source/rtp_packet_to_send.h"
 #include "rtc_base/numerics/exp_filter.h"
-#include "rtc_base/synchronization/mutex.h"
 #include "rtc_base/task_queue.h"
 #include "rtc_base/thread_annotations.h"
 
@@ -40,6 +37,8 @@ class Clock;
 
 class TaskQueuePacedSender : public RtpPacketPacer, public RtpPacketSender {
  public:
+  static const int kNoPacketHoldback;
+
   // The `hold_back_window` parameter sets a lower bound on time to sleep if
   // there is currently a pacer queue and packets can't immediately be
   // processed. Increasing this reduces thread wakeups at the expense of higher
@@ -163,6 +162,7 @@ class TaskQueuePacedSender : public RtpPacketPacer, public RtpPacketSender {
 
   // Filtered size of enqueued packets, in bytes.
   rtc::ExpFilter packet_size_ RTC_GUARDED_BY(task_queue_);
+  bool include_overhead_ RTC_GUARDED_BY(task_queue_);
 
   mutable Mutex stats_mutex_;
   Stats current_stats_ RTC_GUARDED_BY(stats_mutex_);
