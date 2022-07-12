42e5f2543470853a14e5516d6331792fea14d927
Michael Froman <mfroman@mozilla.com>
2022-07-11 22:59:07 -0500

    Bug 1766646 - Vendor libwebrtc from e66e6a845b
    Upstream commit: https://webrtc.googlesource.com/src/+/e66e6a845b50f212ebb60234446cfc9db897879c
        Apply lower bound of delay based estimate in AimdRateControl::ClampBitrate
    
        This move the functionality of applying the lower bound of a network estimate to AimdRateControl::ClampBitrate instead of ChangeBitrate.
        The purpose is to be able to also clamp probe estimates set by AimdRateControl::SetEstimate as well.
    
        Bug: none
        Change-Id: I6a4d64d2e98bb99da06010e2edaf20dc42880e37
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/255823
        Reviewed-by: Per Kjellander <perkj@webrtc.org>
        Commit-Queue: Per Kjellander <perkj@webrtc.org>
        Reviewed-by: Diep Bui <diepbp@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36219}

--- third_party/libwebrtc/modules/remote_bitrate_estimator/aimd_rate_control_unittest.cc
+++ third_party/libwebrtc/modules/remote_bitrate_estimator/aimd_rate_control_unittest.cc
@@ -255,7 +255,7 @@ TEST(AimdRateControlTest, SetEstimateIncreaseBweInAlr) {
             2 * kInitialBitrateBps);
 }
 
-TEST(AimdRateControlTest, SetEstimateClampedByNetworkEstimate) {
+TEST(AimdRateControlTest, SetEstimateUpperLimitedByNetworkEstimate) {
   auto states = CreateAimdRateControlStates(/*send_side=*/true);
   NetworkStateEstimate network_estimate;
   network_estimate.link_capacity_upper = DataRate::KilobitsPerSec(400);
@@ -263,6 +263,17 @@ TEST(AimdRateControlTest, SetEstimateClampedByNetworkEstimate) {
   SetEstimate(states, 500'000);
   EXPECT_EQ(states.aimd_rate_control->LatestEstimate(),
             network_estimate.link_capacity_upper);
+}
+
+TEST(AimdRateControlTest, SetEstimateLowerLimitedByNetworkEstimate) {
+  auto states = CreateAimdRateControlStates(/*send_side=*/true);
+  NetworkStateEstimate network_estimate;
+  network_estimate.link_capacity_lower = DataRate::KilobitsPerSec(400);
+  states.aimd_rate_control->SetNetworkStateEstimate(network_estimate);
+  SetEstimate(states, 100'000);
+  // 0.85 is default backoff factor. (`beta_`)
+  EXPECT_EQ(states.aimd_rate_control->LatestEstimate(),
+            network_estimate.link_capacity_lower * 0.85);
 }
 
 TEST(AimdRateControlTest, SetEstimateIgnoresNetworkEstimatesLowerThanCurrent) {
