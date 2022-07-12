46b687c216096c18a0c154cfb02d79a627993d9d
Michael Froman <mfroman@mozilla.com>
2022-07-11 21:52:08 -0500

    Bug 1766646 - Vendor libwebrtc from 9848890590
    Upstream commit: https://webrtc.googlesource.com/src/+/9848890590b0d619a421b6c03fa7cd70edc9be18
        dcsctp: Remove handover_state from RRSendQueue
    
        It's not used as a constructor parameter, but instead invoked by calling
        RestoreFromState. Removing this parameter avoids confusion.
    
        Bug: None
        Change-Id: I3bb8a0135808e3ae010be6d37439513517f71832
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/254820
        Auto-Submit: Victor Boivie <boivie@webrtc.org>
        Reviewed-by: Sergey Sukhanov <sergeysu@webrtc.org>
        Commit-Queue: Sergey Sukhanov <sergeysu@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36188}

--- third_party/libwebrtc/net/dcsctp/tx/rr_send_queue.h
+++ third_party/libwebrtc/net/dcsctp/tx/rr_send_queue.h
@@ -47,8 +47,7 @@ class RRSendQueue : public SendQueue {
               size_t buffer_size,
               std::function<void(StreamID)> on_buffered_amount_low,
               size_t total_buffered_amount_low_threshold,
-              std::function<void()> on_total_buffered_amount_low,
-              const DcSctpSocketHandoverState* handover_state = nullptr);
+              std::function<void()> on_total_buffered_amount_low);
 
   // Indicates if the buffer is full. Note that it's up to the caller to ensure
   // that the buffer is not full prior to adding new items to it.
