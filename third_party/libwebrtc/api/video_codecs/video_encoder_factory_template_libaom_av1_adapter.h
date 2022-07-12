7428b500e9e237e459b365759869cd15dfcce1df
Michael Froman <mfroman@mozilla.com>
2022-07-11 22:56:17 -0500

    Bug 1766646 - Vendor libwebrtc from 6160ca53d1
    Upstream commit: https://webrtc.googlesource.com/src/+/6160ca53d113a4b6c6cb77f78a6d724344dfe8ef
        New VideoEncoderFactoryTemplate.
    
        The VideoEncoderFactoryTemplate takes encoder implementations as template arguments, making it possible to easily implement a VideoEncoderFactory only using the implementations required for the particular application. This will replace the BuiltinVideoEncoderFactory.
    
        Change-Id: Ifb0e93d0d4491664fb7f7acf085190d8a90ddc0e
        Bug: webrtc:13573
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/251904
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36216}

---
+++ third_party/libwebrtc/api/video_codecs/video_encoder_factory_template_libaom_av1_adapter.h
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2022 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef API_VIDEO_CODECS_VIDEO_ENCODER_FACTORY_TEMPLATE_LIBAOM_AV1_ADAPTER_H_
+#define API_VIDEO_CODECS_VIDEO_ENCODER_FACTORY_TEMPLATE_LIBAOM_AV1_ADAPTER_H_
+
+#include <memory>
+#include <vector>
+
+#include "modules/video_coding/codecs/av1/libaom_av1_encoder.h"
+#include "modules/video_coding/svc/create_scalability_structure.h"
+
+namespace webrtc {
+struct LibaomAv1EncoderTemplateAdapter {
+  static std::vector<SdpVideoFormat> SupportedFormats() {
+    return {SdpVideoFormat("AV1")};
+  }
+
+  static std::unique_ptr<VideoEncoder> CreateEncoder(
+      const SdpVideoFormat& format) {
+    return CreateLibaomAv1Encoder();
+  }
+
+  static bool IsScalabilityModeSupported(absl::string_view scalability_mode) {
+    // For libaom AV1, the scalability mode is supported if we can create the
+    // scalability structure.
+    return ScalabilityStructureConfig(scalability_mode) != absl::nullopt;
+  }
+};
+
+}  // namespace webrtc
+
+#endif  // API_VIDEO_CODECS_VIDEO_ENCODER_FACTORY_TEMPLATE_LIBAOM_AV1_ADAPTER_H_
