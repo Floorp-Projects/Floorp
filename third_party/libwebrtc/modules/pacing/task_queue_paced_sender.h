2508a905ba3364223903383fa87d34dbf8482a83
Michael Froman <mfroman@mozilla.com>
2022-07-12 09:05:01 -0500

    Bug 1766646 - Vendor libwebrtc from 16af378884
    Upstream commit: https://webrtc.googlesource.com/src/+/16af378884f9996ab1f8ce4629c910cb82ec54b4
        Remove deprecated TaskQueuePacedSender constructor.
    
        Bug: webrtc:13417
        Change-Id: I1486aedf2069d70c515fad97c665c7a5a94b6c38
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/256017
        Auto-Submit: Erik Språng <sprang@webrtc.org>
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36253}

--- third_party/libwebrtc/modules/pacing/task_queue_paced_sender.h
+++ third_party/libwebrtc/modules/pacing/task_queue_paced_sender.h
@@ -40,17 +40,6 @@ class Clock;
 
 class TaskQueuePacedSender : public RtpPacketPacer, public RtpPacketSender {
  public:
-  // TODO(bugs.webrtc.org/13417): Remove when downstream usage is gone.
-  ABSL_DEPRECATED("Use the other version instead.")
-  TaskQueuePacedSender(
-      Clock* clock,
-      PacingController::PacketSender* packet_sender,
-      RtcEventLog* event_log,
-      const WebRtcKeyValueConfig* field_trials,
-      TaskQueueFactory* task_queue_factory,
-      TimeDelta max_hold_back_window = PacingController::kMinSleepTime,
-      int max_hold_back_window_in_packets = -1);
-
   // The `hold_back_window` parameter sets a lower bound on time to sleep if
   // there is currently a pacer queue and packets can't immediately be
   // processed. Increasing this reduces thread wakeups at the expense of higher
