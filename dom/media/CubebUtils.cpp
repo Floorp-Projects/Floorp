980eeae29f293b4f3af8b8a7216b8ee906dfd6b8
Bogdan Szekely <bszekely@mozilla.com>
2022-07-12 16:03:26 +0300

    Backed out changeset 2fa9fd80ad28 (bug 1680975) for causing build bustages on CubebUtils.cpp CLOSED TREE

--- dom/media/CubebUtils.cpp
+++ dom/media/CubebUtils.cpp
@@ -25,8 +25,6 @@
 #include "prdtoa.h"
 #include <algorithm>
 #include <stdint.h>
-#include <unistd.h>
-
 #ifdef MOZ_WIDGET_ANDROID
 #  include "mozilla/java/GeckoAppShellWrappers.h"
 #endif
@@ -64,8 +62,7 @@
 #define PREF_AUDIOIPC_SHM_AREA_SIZE "media.audioipc.shm_area_size"
 
 #if (defined(XP_LINUX) && !defined(MOZ_WIDGET_ANDROID)) || \
-    defined(XP_MACOSX) || defined(XP_FREEBSD) ||           \
-    (defined(XP_WIN) && !defined(_ARM64_))
+    defined(XP_MACOSX) || (defined(XP_WIN) && !defined(_ARM64_))
 #  define MOZ_CUBEB_REMOTING
 #endif
 
