dc0bdd6fe558b5ac7dc9e9a8bc264e7e87884114
Cosmin Sabou <csabou@mozilla.com>
2022-07-13 12:36:32 +0300

    Backed out 3 changesets (bug 1750972) for potential site breakage.
    Backed out changeset 90d51383665c (bug 1750972)
    Backed out changeset 3b7d15317952 (bug 1750972)
    Backed out changeset b1a8adcb9099 (bug 1750972)

--- netwerk/cookie/test/browser/browser_sameSiteConsole.js
+++ netwerk/cookie/test/browser/browser_sameSiteConsole.js
@@ -6,7 +6,10 @@ const SAMESITE_TOP_PAGE = SAMESITE_DOMAIN + SAMESITE_PATH + "sameSite.sjs";
 
 add_task(async _ => {
   await SpecialPowers.pushPrefEnv({
-    set: [["network.cookie.sameSite.laxByDefault", true]],
+    set: [
+      ["network.cookie.sameSite.laxByDefault", true],
+      ["network.cookie.sameSite.noneRequiresSecure", true],
+    ],
   });
 
   const expected = [];
