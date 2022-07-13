2da4be8e4ce800639357c1120671d3bfd232d492
Michael Froman <mfroman@mozilla.com>
2022-07-13 13:59:38 -0500

    Bug 1766646 - Vendor libwebrtc from f8775cf269
    Upstream commit: https://webrtc.googlesource.com/src/+/f8775cf269ee1495a357ac4e4f03ebe9f47e1651
        Fix drawing of local mouse in CRD on ChromeOS
    
        ChromeOS will use DPI (see crrev.com/c/3322917), but the
        DesktopAndCursorComposer assumed pixels were used.
    
        Test: Manually ensured it works
        Bug: b/208370410
        Change-Id: I5fee50d408fd204273946009e6653d4e60d1e458
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/259502
        Reviewed-by: Alexander Cooper <alcooper@chromium.org>
        Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
        Cr-Commit-Position: refs/heads/main@{#36597}

--- third_party/libwebrtc/modules/desktop_capture/desktop_frame.cc
+++ third_party/libwebrtc/modules/desktop_capture/desktop_frame.cc
@@ -112,7 +112,7 @@ DesktopRect DesktopFrame::rect() const {
 float DesktopFrame::scale_factor() const {
   float scale = 1.0f;
 
-#if defined(WEBRTC_MAC)
+#if defined(WEBRTC_MAC) || defined(CHROMEOS)
   // At least on Windows the logical and physical pixel are the same
   // See http://crbug.com/948362.
   if (!dpi().is_zero() && dpi().x() == dpi().y())
