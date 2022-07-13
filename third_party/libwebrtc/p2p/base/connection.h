4a57399ef9e4291b8dbb0c008965c6c5f747543c
Michael Froman <mfroman@mozilla.com>
2022-07-13 13:45:27 -0500

    Bug 1766646 - Vendor libwebrtc from 6b1d626b04
    Upstream commit: https://webrtc.googlesource.com/src/+/6b1d626b043cea37d01b6a72ca1086fe4680a052
        Remove deprecated ProxyConnection ctor and make NewWeakPtr protected.
    
        Bug: webrtc:13892
        Change-Id: Icad20a0f6d304c23106dd880f3bfd2c7142929c9
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/258601
        Reviewed-by: Niels Moller <nisse@webrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36583}

--- third_party/libwebrtc/p2p/base/connection.h
+++ third_party/libwebrtc/p2p/base/connection.h
@@ -472,9 +472,6 @@ class ProxyConnection : public Connection {
                   size_t index,
                   const Candidate& remote_candidate);
 
-  // TODO(tommi): Remove this ctor once it's no longer needed.
-  ProxyConnection(Port* port, size_t index, const Candidate& remote_candidate);
-
   int Send(const void* data,
            size_t size,
            const rtc::PacketOptions& options) override;
