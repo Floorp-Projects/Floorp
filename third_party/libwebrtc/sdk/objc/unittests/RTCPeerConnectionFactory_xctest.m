15957ddf2b69db4c10c94fb073a217a2fcfd0531
Michael Froman <mfroman@mozilla.com>
2022-07-13 17:01:33 -0500

    Bug 1766646 - Vendor libwebrtc from 61afb34d3f
    Upstream commit: https://webrtc.googlesource.com/src/+/61afb34d3fb84f61cba5eef81dbd2cfb12b091e2
        Disable iOS tests relying on senderWithKind.
    
        Bug: webrtc:13989
        Change-Id: I1a299e8942ad3db251d4563d62946985f2d7af96
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/259822
        Reviewed-by: Peter Hanspers <peterhanspers@webrtc.org>
        Commit-Queue: Jeremy Leconte <jleconte@google.com>
        Cr-Commit-Position: refs/heads/main@{#36624}

--- third_party/libwebrtc/sdk/objc/unittests/RTCPeerConnectionFactory_xctest.m
+++ third_party/libwebrtc/sdk/objc/unittests/RTCPeerConnectionFactory_xctest.m
@@ -131,6 +131,8 @@
   XCTAssertTrue(true, "Expect test does not crash");
 }
 
+// TODO(crbug.com/webrtc/13989): Remove call to CreateSender in senderWithKind.
+#if !TARGET_IPHONE_SIMULATOR
 - (void)testRTCRtpSenderLifetime {
   @autoreleasepool {
     RTC_OBJC_TYPE(RTCConfiguration) *config = [[RTC_OBJC_TYPE(RTCConfiguration) alloc] init];
@@ -204,6 +206,7 @@
 
   XCTAssertTrue(true, "Expect test does not crash");
 }
+#endif
 
 - (void)testAudioSourceLifetime {
   @autoreleasepool {
