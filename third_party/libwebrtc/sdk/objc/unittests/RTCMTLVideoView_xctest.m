b59153c324129ae48d6d4f1cc52e6e1c8e227d9b
Michael Froman <mfroman@mozilla.com>
2022-07-14 11:06:01 -0500

    Bug 1766646 - Vendor libwebrtc from 49ad2d787a
    Upstream commit: https://webrtc.googlesource.com/src/+/49ad2d787a63e8406322b6af2b83766b9e5f2978
        Disable tests with 'method was not invoked' error on simulator
    
        https://chromium-swarm.appspot.com/task?id=5a663a0bbd1e4210
    
        Bug: webrtc:13990
        Change-Id: I8f6cc03e4ef4de389fa44f580be22649660c0524
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/259770
        Commit-Queue: Jeremy Leconte <jleconte@google.com>
        Reviewed-by: Peter Hanspers <peterhanspers@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36698}

--- third_party/libwebrtc/sdk/objc/unittests/RTCMTLVideoView_xctest.m
+++ third_party/libwebrtc/sdk/objc/unittests/RTCMTLVideoView_xctest.m
@@ -131,6 +131,8 @@
   [self.classMock verify];
 }
 
+// TODO(crbug.com/webrtc/13990): method was not invoked: drawFrame.
+#if !TARGET_IPHONE_SIMULATOR
 - (void)testRTCVideoRenderFrameCallbackI420 {
   // given
   OCMStub([self.classMock isMetalAvailable]).andReturn(YES);
@@ -255,6 +257,7 @@
 
   [self.rendererNV12Mock verify];
 }
+#endif
 
 - (void)testReportsSizeChangesToDelegate {
   OCMStub([self.classMock isMetalAvailable]).andReturn(YES);
