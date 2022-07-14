c1beac13958c50d38230e6a538570be4c16e7a09
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2022-07-14 02:19:11 +0000

    Bug 1774537 - Propagate last snap target id(s) from APZ to the content. r=botond
    Depends on D149495
    
    Differential Revision: https://phabricator.services.mozilla.com/D149496

--- gfx/layers/apz/util/APZCCallbackHelper.cpp
+++ gfx/layers/apz/util/APZCCallbackHelper.cpp
@@ -129,7 +129,9 @@ static CSSPoint ScrollFrameTo(nsIScrollableFrame* aFrame,
   // request because we'll clobber that one, which is bad.
   bool scrollInProgress = APZCCallbackHelper::IsScrollInProgress(aFrame);
   if (!scrollInProgress) {
-    aFrame->ScrollToCSSPixelsForApz(targetScrollPosition);
+    ScrollSnapTargetIds snapTargetIds = aRequest.GetLastSnapTargetIds();
+    aFrame->ScrollToCSSPixelsForApz(targetScrollPosition,
+                                    std::move(snapTargetIds));
     geckoScrollPosition = CSSPoint::FromAppUnits(aFrame->GetScrollPosition());
     aSuccessOut = true;
   }
@@ -387,7 +389,9 @@ void APZCCallbackHelper::UpdateRootFrame(const RepaintRequest& aRequest) {
         nsLayoutUtils::FindScrollableFrameFor(aRequest.GetScrollId());
     CSSPoint currentScrollPosition =
         CSSPoint::FromAppUnits(sf->GetScrollPosition());
-    sf->ScrollToCSSPixelsForApz(currentScrollPosition);
+    ScrollSnapTargetIds snapTargetIds = aRequest.GetLastSnapTargetIds();
+    sf->ScrollToCSSPixelsForApz(currentScrollPosition,
+                                std::move(snapTargetIds));
   }
 
   // Do this as late as possible since scrolling can flush layout. It also
