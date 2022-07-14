4a54a60be4f776e16a9581aad580e266e090ff2a
Michael Froman <mfroman@mozilla.com>
2022-07-14 15:25:48 -0500

    Bug 1766646 - Vendor libwebrtc from 45361f78ed
    Upstream commit: https://webrtc.googlesource.com/src/+/45361f78ed18c350b3edcaef19ae4c7cf167e95b
        Calculate video stream max bitrate using expression.
    
        This replaces the ealier table-based caps.
        Apart from the VGA cap (now 1600kbps instead of 1700kbps), or if using
        "in between" resolutions, the caps are unchanged - but now cover high
        resolutions better.
    
        Bug: webrtc:14017
        Change-Id: I8649b528495d6c917e38ea8cb1a272df6c464c03
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/260940
        Commit-Queue: Erik Språng <sprang@webrtc.org>
        Reviewed-by: Per Kjellander <perkj@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36776}

--- third_party/libwebrtc/media/engine/webrtc_video_engine.cc
+++ third_party/libwebrtc/media/engine/webrtc_video_engine.cc
@@ -343,24 +343,30 @@ bool IsCodecDisabledForSimulcast(const std::string& codec_name,
   return false;
 }
 
-// The selected thresholds for QVGA and VGA corresponded to a QP around 10.
-// The change in QP declined above the selected bitrates.
+// Calculates some reasonable max bitrate thresholds based on resolution.
 static int GetMaxDefaultVideoBitrateKbps(int width,
                                          int height,
                                          bool is_screenshare) {
-  int max_bitrate;
-  if (width * height <= 320 * 240) {
-    max_bitrate = 600;
-  } else if (width * height <= 640 * 480) {
-    max_bitrate = 1700;
-  } else if (width * height <= 960 * 540) {
-    max_bitrate = 2000;
-  } else {
-    max_bitrate = 2500;
-  }
+  double pixel_count = width * height;
+  // This expression uses a rectangular hyperbola plus a small exponential
+  // correction function, with parameters selected so the curve fits well to
+  // values expected for common resolutions.
+  //
+  // Some examples:
+  //  320x240 => 600
+  //  640x480 => 1600
+  //  960x540 => 2000
+  //  1280x720 => 2500
+  //  1920x1080 => 3300
+  double max_kbps = (3000 * pixel_count) / (310000 + pixel_count) +
+                    0.0003 * std::pow(pixel_count, 1.005);
+  // Round to nearest 100kbps.
+  int max_bitrate_kbps =
+      100 * std::max(1, static_cast<int>(std::round(max_kbps / 100)));
+
   if (is_screenshare)
-    max_bitrate = std::max(max_bitrate, 1200);
-  return max_bitrate;
+    max_bitrate_kbps = std::max(max_bitrate_kbps, 1200);
+  return max_bitrate_kbps;
 }
 
 // Returns its smallest positive argument. If neither argument is positive,
