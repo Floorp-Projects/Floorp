7a0354d08cc0c0abc4a20cbf7ca810b5624ff774
Michael Froman <mfroman@mozilla.com>
2022-07-14 13:40:49 -0500

    Bug 1766646 - Vendor libwebrtc from 1bcd827e9b
    Upstream commit: https://webrtc.googlesource.com/src/+/1bcd827e9b85e82ce13bd7cc27c5a4b90ba9c139
        Merged FrameBuffer3 {Next,Last}DecodableTemporalUnitRtpTimestamp() function.
    
        Bug: webrtc:13343
        Change-Id: Ic21eddd38466e6b5fd8b912b3ee2d9dc47a0ba35
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/260981
        Reviewed-by: Evan Shrubsole <eshr@webrtc.org>
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36756}

--- third_party/libwebrtc/test/fuzzers/frame_buffer3_fuzzer.cc
+++ third_party/libwebrtc/test/fuzzers/frame_buffer3_fuzzer.cc
@@ -39,7 +39,7 @@ void FuzzOneInput(const uint8_t* data, size_t size) {
   SeqNumUnwrapper<uint16_t, kFrameIdLength> unwrapper;
 
   while (helper.BytesLeft() > 0) {
-    int action = helper.ReadOrDefaultValue<uint8_t>(0) % 7;
+    int action = helper.ReadOrDefaultValue<uint8_t>(0) % 6;
 
     switch (action) {
       case 0: {
@@ -51,22 +51,18 @@ void FuzzOneInput(const uint8_t* data, size_t size) {
         break;
       }
       case 2: {
-        buffer.NextDecodableTemporalUnitRtpTimestamp();
+        buffer.DecodableTemporalUnitsInfo();
         break;
       }
       case 3: {
-        buffer.LastDecodableTemporalUnitRtpTimestamp();
-        break;
-      }
-      case 4: {
         buffer.ExtractNextDecodableTemporalUnit();
         break;
       }
-      case 5: {
+      case 4: {
         buffer.DropNextDecodableTemporalUnit();
         break;
       }
-      case 6: {
+      case 5: {
         auto frame = std::make_unique<FuzzyFrameObject>();
         frame->SetTimestamp(helper.ReadOrDefaultValue<uint32_t>(0));
         int64_t wire_id =
