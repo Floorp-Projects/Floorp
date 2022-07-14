b4ef462fc107bbe8780cd9645882cf3f74464434
Aaron Train <aaron.train@gmail.com>
2022-07-14 14:23:15 -0400

    [components] No issue: Disable Glean UI tests for intermittents

--- mobile/android/android-components/samples/glean/src/androidTest/java/org/mozilla/samples/glean/pings/BaselinePingTest.kt
+++ mobile/android/android-components/samples/glean/src/androidTest/java/org/mozilla/samples/glean/pings/BaselinePingTest.kt
@@ -18,6 +18,7 @@ import okhttp3.mockwebserver.RecordedRequest
 import org.json.JSONObject
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -107,6 +108,7 @@ class BaselinePingTest {
         return null
     }
 
+    @Ignore("Intermittent: https://github.com/mozilla-mobile/android-components/issues/12359")
     @Test
     fun validateBaselinePing() {
         // Wait for the app to be idle/ready.
