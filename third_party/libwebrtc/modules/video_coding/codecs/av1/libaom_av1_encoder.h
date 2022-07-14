af149cdca92c2292a07d7efae45ce90e56b27a18
Michael Froman <mfroman@mozilla.com>
2022-07-14 13:42:35 -0500

    Bug 1766646 - Vendor libwebrtc from 30ec725b6e
    Upstream commit: https://webrtc.googlesource.com/src/+/30ec725b6ef252c7b7d87765d67c251db8211cf6
        Auxiliary liboam AV1 encoder settings.
    
        Bug: none
        Change-Id: I03e01ffbed2ec98953650847600016e4f80fdb50
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/260861
        Reviewed-by: Sergey Silkin <ssilkin@webrtc.org>
        Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36758}

--- third_party/libwebrtc/modules/video_coding/codecs/av1/libaom_av1_encoder.h
+++ third_party/libwebrtc/modules/video_coding/codecs/av1/libaom_av1_encoder.h
@@ -10,13 +10,22 @@
 #ifndef MODULES_VIDEO_CODING_CODECS_AV1_LIBAOM_AV1_ENCODER_H_
 #define MODULES_VIDEO_CODING_CODECS_AV1_LIBAOM_AV1_ENCODER_H_
 
+#include <map>
 #include <memory>
 
 #include "absl/strings/string_view.h"
 #include "api/video_codecs/video_encoder.h"
 
 namespace webrtc {
+struct LibaomAv1EncoderAuxConfig {
+  // A map of max pixel count --> cpu speed.
+  std::map<int, int> max_pixel_count_to_cpu_speed;
+};
+
 std::unique_ptr<VideoEncoder> CreateLibaomAv1Encoder();
+std::unique_ptr<VideoEncoder> CreateLibaomAv1Encoder(
+    const LibaomAv1EncoderAuxConfig& aux_config);
+
 }  // namespace webrtc
 
 #endif  // MODULES_VIDEO_CODING_CODECS_AV1_LIBAOM_AV1_ENCODER_H_
