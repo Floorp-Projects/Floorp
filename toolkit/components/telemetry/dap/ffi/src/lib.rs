8fdda7f74f62d15fad4f7e79ab311f8bc185b67e
Simon Friedberger <simon@mozilla.com>
2022-07-14 14:39:19 +0000

    Bug 1775035 - Add Rust and XPCOM scaffolding for DAPTelemetry. r=emilio
    Rust is required to use the external libprio-rs Rust library.
    C++ XPCOM is added because writing XPCOM objects in Rust is still
    difficult.
    
    Differential Revision: https://phabricator.services.mozilla.com/D151805

---
+++ toolkit/components/telemetry/dap/ffi/src/lib.rs
@@ -0,0 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#[no_mangle]
+pub extern "C" fn dap_ffi_test() {
+    println!("Dummytest.");
+}
