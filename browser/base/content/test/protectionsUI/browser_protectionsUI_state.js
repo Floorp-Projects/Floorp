aa358f51502f093cb95ce7c0034ba4a89ff8e7b3
Thomas Wisniewski <twisniewski@mozilla.com>
2022-07-15 13:26:17 +0000

    Bug 1762462 - Make tests in browser/base/content/test/protectionsUI/ reflect Always Partitioning Storage; r=mossop
    Depends on D143627
    
    Differential Revision: https://phabricator.services.mozilla.com/D148042

--- browser/base/content/test/protectionsUI/browser_protectionsUI_state.js
+++ browser/base/content/test/protectionsUI/browser_protectionsUI_state.js
@@ -16,6 +16,8 @@
 
 const TP_PREF = "privacy.trackingprotection.enabled";
 const TP_PB_PREF = "privacy.trackingprotection.pbmode.enabled";
+const APS_PREF =
+  "privacy.partition.always_partition_third_party_non_cookie_storage";
 const TPC_PREF = "network.cookie.cookieBehavior";
 const DTSCBN_PREF = "dom.testing.sync-content-blocking-notifications";
 const BENIGN_PAGE =
@@ -272,6 +274,8 @@ async function testContentBlocking(tab) {
 }
 
 add_task(async function testNormalBrowsing() {
+  await SpecialPowers.pushPrefEnv({ set: [[APS_PREF, false]] });
+
   await UrlClassifierTestUtils.addTestTrackers();
 
   Services.prefs.setBoolPref(DTSCBN_PREF, true);
@@ -310,7 +314,10 @@ add_task(async function testNormalBrowsing() {
 
 add_task(async function testPrivateBrowsing() {
   await SpecialPowers.pushPrefEnv({
-    set: [["dom.security.https_first_pbm", false]],
+    set: [
+      ["dom.security.https_first_pbm", false],
+      [APS_PREF, false],
+    ],
   });
   let privateWin = await BrowserTestUtils.openNewBrowserWindow({
     private: true,
@@ -351,6 +358,8 @@ add_task(async function testPrivateBrowsing() {
 });
 
 add_task(async function testThirdPartyCookies() {
+  await SpecialPowers.pushPrefEnv({ set: [[APS_PREF, false]] });
+
   await UrlClassifierTestUtils.addTestTrackers();
   gTrackingPageURL = COOKIE_PAGE;
 
