fda2d5af3f5e38aae41c5dab31efb78ba3372f9e
Michael Froman <mfroman@mozilla.com>
2022-07-14 21:50:45 -0500

    Bug 1766646 - Vendor libwebrtc from adaf511221
    Upstream commit: https://webrtc.googlesource.com/src/+/adaf511221e26305167936e6ebb22f50aa900439
        Extend UnitBase multiplication to support size_t
    
        Bug: None
        Change-Id: I8dcb85cdb2819df54d4cb0cae59b77d7d629123a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/260941
        Reviewed-by: Sebastian Jansson <srte@webrtc.org>
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36828}

--- third_party/libwebrtc/api/units/time_delta_unittest.cc
+++ third_party/libwebrtc/api/units/time_delta_unittest.cc
@@ -187,14 +187,6 @@ TEST(TimeDeltaTest, MathOperations) {
   EXPECT_EQ((delta_a + delta_b).ms(), kValueA + kValueB);
   EXPECT_EQ((delta_a - delta_b).ms(), kValueA - kValueB);
 
-  const int32_t kInt32Value = 123;
-  const double kFloatValue = 123.0;
-  EXPECT_EQ((TimeDelta::Micros(kValueA) * kValueB).us(), kValueA * kValueB);
-  EXPECT_EQ((TimeDelta::Micros(kValueA) * kInt32Value).us(),
-            kValueA * kInt32Value);
-  EXPECT_EQ((TimeDelta::Micros(kValueA) * kFloatValue).us(),
-            kValueA * kFloatValue);
-
   EXPECT_EQ((delta_b / 10).ms(), kValueB / 10);
   EXPECT_EQ(delta_b / delta_a, static_cast<double>(kValueB) / kValueA);
 
@@ -206,6 +198,26 @@ TEST(TimeDeltaTest, MathOperations) {
   EXPECT_EQ(mutable_delta, TimeDelta::Millis(kValueA + kValueB));
   mutable_delta -= TimeDelta::Millis(kValueB);
   EXPECT_EQ(mutable_delta, TimeDelta::Millis(kValueA));
+}
+
+TEST(TimeDeltaTest, MultiplyByScalar) {
+  const TimeDelta kValue = TimeDelta::Micros(267);
+  const int64_t kInt64 = 450;
+  const int32_t kInt32 = 123;
+  const size_t kUnsignedInt = 125;
+  const double kFloat = 123.0;
+
+  EXPECT_EQ((kValue * kInt64).us(), kValue.us() * kInt64);
+  EXPECT_EQ(kValue * kInt64, kInt64 * kValue);
+
+  EXPECT_EQ((kValue * kInt32).us(), kValue.us() * kInt32);
+  EXPECT_EQ(kValue * kInt32, kInt32 * kValue);
+
+  EXPECT_EQ((kValue * kUnsignedInt).us(), kValue.us() * int64_t{kUnsignedInt});
+  EXPECT_EQ(kValue * kUnsignedInt, kUnsignedInt * kValue);
+
+  EXPECT_DOUBLE_EQ((kValue * kFloat).us(), kValue.us() * kFloat);
+  EXPECT_EQ(kValue * kFloat, kFloat * kValue);
 }
 
 TEST(TimeDeltaTest, InfinityOperations) {
