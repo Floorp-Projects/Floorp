fa9742e187eed48a88e9a1383fc00fe843ccf224
Michael Froman <mfroman@mozilla.com>
2022-07-14 22:05:04 -0500

    Bug 1766646 - Vendor libwebrtc from edcb25b623
    Upstream commit: https://webrtc.googlesource.com/src/+/edcb25b623b2e8e74ca35d82c1efa4eedea940b7
        Migrate RemoteNtpTimeEstimator to more precise time representations
    
        Reland of https://webrtc-review.googlesource.com/c/src/+/261311
    
        Bug: webrtc:13757
        Change-Id: I34a58100b8fadfe3dbea9ffce71829b7670daad8
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/261726
        Reviewed-by: Jakob Ivarsson‎ <jakobi@webrtc.org>
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36838}

--- third_party/libwebrtc/video/rtp_video_stream_receiver2.cc
+++ third_party/libwebrtc/video/rtp_video_stream_receiver2.cc
@@ -1065,12 +1065,13 @@ bool RtpVideoStreamReceiver2::DeliverRtcp(const uint8_t* rtcp_packet,
       clock_->CurrentNtpInMilliseconds() - received_ntp.ToMs();
   // Don't use old SRs to estimate time.
   if (time_since_received <= 1) {
-    ntp_estimator_.UpdateRtcpTimestamp(rtt, ntp_secs, ntp_frac, rtp_timestamp);
-    absl::optional<int64_t> remote_to_local_clock_offset_ms =
-        ntp_estimator_.EstimateRemoteToLocalClockOffsetMs();
-    if (remote_to_local_clock_offset_ms.has_value()) {
+    ntp_estimator_.UpdateRtcpTimestamp(
+        TimeDelta::Millis(rtt), NtpTime(ntp_secs, ntp_frac), rtp_timestamp);
+    absl::optional<int64_t> remote_to_local_clock_offset =
+        ntp_estimator_.EstimateRemoteToLocalClockOffset();
+    if (remote_to_local_clock_offset.has_value()) {
       capture_clock_offset_updater_.SetRemoteToLocalClockOffset(
-          Int64MsToQ32x32(*remote_to_local_clock_offset_ms));
+          *remote_to_local_clock_offset);
     }
   }
 
