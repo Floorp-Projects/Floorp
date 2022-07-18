760769c04875797967ffbeb8239d4a0d367cc82f
surapunoyousei <ryosukeusuusu@gmail.com>
2022-07-18 23:41:26 +0900

    Add Floorp pecific pereferences

---
+++ browser/app/profile/000-floorp.js
@@ -0,0 +1,176 @@
+pref("floorp.hide.tabbrowser-tab.enable", false);
+pref("floorp.optimized.msbutton.ope", false); 
+pref("floorp.optimized.verticaltab", false); 
+pref("floorp.horizontal.tab.position.shift", false);
+pref("floorp.Tree-type.verticaltab.optimization", false);
+pref("floorp.bookmarks.bar.focus.mode", false);
+pref("floorp.material.effect.enable", false);
+pref("floorp.browser.user.interface", 3);
+pref("floorp.browser.tabbar.settings", 1);
+pref("floorp.enable.auto.restart", false);
+pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
+pref("browser.newtabpage.activity-stream.feeds.section.highlights", true);
+pref("floorp.memory.free.button", true);
+pref("floorp.entable.deepl.translate", false);
+pref("floorp.multitab.bottommode", false);
+pref("view_source.editor.path", "C:\\Windows\\System32\\notepad.exe");
+pref("browser.search.separatePrivateDefault", true);
+pref("browser.search.separatePrivateDefault.ui.enabled", true);
+pref("browser.urlbar.update2.engineAliasRefresh", true);
+pref("services.sync.engine.prefs", false);
+pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.feeds.snippets", false, locked);
+pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsored", false, locked);
+pref("services.sync.prefs.sync.browser.newtabpage.activity-stream.showSponsoredTopSites", false, locked);
+pref("services.sync.telemetry.maxPayloadCount", "0", locked);
+pref("services.sync.telemetry.submissionInterval", "0", locked);
+pref("services.sync.prefs.sync.browser.startup.page", false, locked);
+pref("services.sync.prefs.sync.browser.tabs.warnOnClose", false, locked);
+pref("toolkit.legacyUserProfileCustomizations.stylesheets" ,true);
+pref("browser.tabs.warnOnClose", true);
+pref("browser.tabs.warnOnCloseOtherTabs", true);
+pref("browser.shell.checkDefaultBrowser", false, locked);
+pref("extensions.getAddons.showPane", false);
+pref("app.normandy.api_url", "");
+pref("app.normandy.enabled", true);
+pref("layout.css.backdrop-filter.enabled", true);
+pref("svg.context-properties.content.enabled", true, locked);
+pref("image.avif.enabled", true, locked);
+pref("image.jxl.enabled", true, locked);
+pref("extensions.blocklist.url", "https://blocklist.addons.mozilla.org/blocklist/3/%APP_ID%/%APP_VERSION%/");
+pref("extensions.blocklist.enabled", true);
+pref("services.blocklist.update_enabled",	true);
+pref("extensions.blocklist.url", "https://blocklist.addons.mozilla.org/blocklist/3/%APP_ID%/%APP_VERSION%/");
+pref("extensions.pocket.enabled", false);
+pref("security.csp.enable", true);
+pref("breakpad.reportURL", "", locked);
+pref("browser.tabs.crashReporting.sendReport", false);
+pref("browser.crashReports.unsubmittedCheck.enabled",	false);
+pref("xpinstall.signatures.required", false);
+pref("toolkit.telemetry.archive.enabled", false, locked);
+pref("toolkit.telemetry.bhrPing.enabled", false, locked);
+pref("toolkit.telemetry.enabled", false, locked);
+pref("toolkit.telemetry.firstShutdownPing.enabled", false, locked);
+pref("toolkit.telemetry.geckoview.streaming", false, locked);
+pref("toolkit.telemetry.newProfilePing.enabled", false, locked);
+pref("toolkit.telemetry.pioneer-new-studies-available", false, locked);
+pref("toolkit.telemetry.reportingpolicy.firstRun", false, locked);
+pref("toolkit.telemetry.server", "", locked);
+pref("toolkit.telemetry.shutdownPingSender.enabled", false, locked);
+pref("toolkit.telemetry.shutdownPingSender.enabledFirstSession", false, locked);
+pref("toolkit.telemetry.testing.overrideProductsCheck", false, locked);
+pref("toolkit.telemetry.unified", false, locked);
+pref("toolkit.telemetry.updatePing.enabled", false, locked);
+pref("app.shield.optoutstudies.enabled", false, locked);
+pref("dom.maxHardwareConcurrency",	2);
+pref("browser.search.geoSpecificDefaults", false);
+pref("browser.urlbar.trimURLs", false);
+pref("dom.network.enabled", false);
+pref("media.peerconnection.enabled", true);
+pref("media.peerconnection.ice.default_address_only", true);
+pref("media.peerconnection.ice.no_host", true);
+pref("privacy.resistFingerprinting.block_mozAddonManager", true);
+pref("privacy.trackingprotection.origin_telemetry.enabled", false, locked);
+pref("privacy.userContext.enabled", true);
+pref("privacy.userContext.ui.enabled", true);
+pref("trailhead.firstrun.branches", "", locked);
+pref("extensions.webcompat-reporter.enabled", false);
+pref("browser.startup.page", 3);
+pref("browser.tabs.closeWindowWithLastTab", false);
+pref("general.config.obscure_value", 0);
+pref("floorp.enable.multitab", false);
+pref("font.name-list.sans-serif.ja", "Noto Sans JP, Meiryo, Yu Gothic, MS PGothic, MS Gothic, Yu Mincho, MS PMincho, MS Mincho")
+pref("extensions.htmlaboutaddons.recommendations.enabled", false, locked);
+pref("datareporting.policy.dataSubmissionEnable", false, locked);
+pref("datareporting.healthreport.uploadEnabled", false, locked);
+pref("general.config.sandbox_enabled", false);
+pref("toolkit.legacyUserProfileCustomizations.script", false);
+pref("browser.proton.places-tooltip.enabled", true);
+pref("svg.context-properties.content.enabled", true);
+pref("layout.css.color-mix.enabled", true);
+pref("layout.css.backdrop-filter.enabled", true);
+pref("browser.compactmode.show", true);
+pref("userChrome.tab.connect_to_window",          true); 
+pref("userChrome.tab.color_like_toolbar",         true); 
+pref("userChrome.tab.photon_like_padding",        true); // Photon
+
+// pref("userChrome.tab.dynamic_separtor",        true); // Original, Proton
+pref("userChrome.tab.static_separator",           true); // Photon
+
+// pref("userChrome.tab.newtab_button_like_tab",  true); // Original
+pref("userChrome.tab.newtab_button_smaller",      true); // Photon
+// pref("userChrome.tab.newtab_button_proton",    true); // Proton
+
+// pref("userChrome.icon.panel_full",             true); // Original, Proton
+pref("userChrome.icon.panel_photon",              true); // Photon
+
+// Photon Only
+pref("userChrome.tab.photon_like_contextline",    true);
+pref("userChrome.rounding.square_tab",            true);
+pref("userChrome.compatibility.theme",       true);
+pref("userChrome.compatibility.os",          true);
+pref("userChrome.theme.built_in_contrast",   true);
+pref("userChrome.theme.system_default",      true);
+pref("userChrome.theme.proton_color",        true);
+pref("userChrome.theme.proton_chrome",       true); // Need proton_color
+pref("userChrome.theme.fully_color",         true); // Need proton_color
+pref("userChrome.theme.fully_dark",          true); // Need proton_color
+
+pref("userChrome.decoration.cursor",         true);
+pref("userChrome.decoration.field_border",   true);
+pref("userChrome.decoration.download_panel", true);
+pref("userChrome.decoration.animate",        true);
+
+pref("userChrome.padding.tabbar_width",      true);
+pref("userChrome.padding.tabbar_height",     true);
+pref("userChrome.padding.toolbar_button",    true);
+pref("userChrome.padding.navbar_width",      true);
+pref("userChrome.padding.urlbar",            true);
+pref("userChrome.padding.bookmarkbar",       true);
+pref("userChrome.padding.infobar",           true);
+pref("userChrome.padding.menu",              true);
+pref("userChrome.padding.bookmark_menu",     true);
+pref("userChrome.padding.global_menubar",    true);
+pref("userChrome.padding.panel",             true);
+pref("userChrome.padding.popup_panel",       true);
+
+pref("userChrome.tab.multi_selected",        true);
+pref("userChrome.tab.unloaded",              true);
+pref("userChrome.tab.letters_cleary",        true);
+pref("userChrome.tab.close_button_at_hover", true);
+pref("userChrome.tab.sound_hide_label",      true);
+pref("userChrome.tab.sound_with_favicons",   true);
+pref("userChrome.tab.pip",                   true);
+pref("userChrome.tab.container",             true);
+pref("userChrome.tab.crashed",               true);
+
+pref("userChrome.fullscreen.overlap",        true);
+
+pref("userChrome.icon.library",              true);
+pref("userChrome.icon.panel",                true);
+pref("userChrome.icon.menu",                 true);
+pref("userChrome.icon.context_menu",         true);
+pref("userChrome.icon.global_menu",          true);
+pref("userChrome.icon.global_menubar",       true);
+
+// -- User Content -------------------------------------------------------------
+pref("userContent.player.ui",             true);
+pref("userContent.player.icon",           true);
+pref("userContent.player.noaudio",        true);
+pref("userContent.player.size",           true);
+pref("userContent.player.click_to_play",  true);
+pref("userContent.player.animate",        true);
+
+pref("userContent.newTab.field_border",   true);
+pref("userContent.newTab.full_icon",      true);
+pref("userContent.newTab.animate",        true);
+pref("userContent.newTab.pocket_to_last", true);
+pref("userContent.newTab.searchbar",      true);
+
+pref("userContent.page.illustration",     true);
+pref("userContent.page.proton_color",     true);
+pref("userContent.page.dark_mode",        true); // Need proton_color
+pref("userContent.page.proton",           true); // Need proton_color
+
+// ** Useful Options ***********************************************************
+// Integrated calculator at urlbar
+pref("browser.urlbar.suggest.calculator", true);
