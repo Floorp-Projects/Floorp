16a3ae5aad6ab6db9b9ec556e55c2a3c7070d9f0
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2022-07-19 10:04:06 +0000

    Bug 1780076 - fix ordering of multiple setup tasks in xpcshell tests, r=nalexander a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D152119

--- testing/xpcshell/head.js
+++ testing/xpcshell/head.js
@@ -1814,7 +1814,10 @@ function run_next_test() {
 
   function frontLoadSetups() {
     _gTests.sort(([propsA, funcA], [propsB, funcB]) => {
-      return propsB.isSetup ? 1 : 0;
+      if (propsB.isSetup === propsA.isSetup) {
+        return 0;
+      }
+      return propsB.isSetup ? 1 : -1;
     });
   }
 
