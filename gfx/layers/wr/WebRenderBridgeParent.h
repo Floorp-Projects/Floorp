f8e27399194edaede197367486fb812715b8119f
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2022-07-19 19:56:21 +0000

    Bug 1742797 - Add a new IPC call to clear animation resources on WebRender, r=sotaro.
    Differential Revision: https://phabricator.services.mozilla.com/D151481

--- gfx/layers/wr/WebRenderBridgeParent.h
+++ gfx/layers/wr/WebRenderBridgeParent.h
@@ -141,6 +141,7 @@ class WebRenderBridgeParent final : public PWebRenderBridgeParent,
       const LayersObserverEpoch& aChildEpoch) override;
 
   mozilla::ipc::IPCResult RecvClearCachedResources() override;
+  mozilla::ipc::IPCResult RecvClearAnimationResources() override;
   mozilla::ipc::IPCResult RecvInvalidateRenderedFrame() override;
   mozilla::ipc::IPCResult RecvScheduleComposite(
       const wr::RenderReasons& aReasons) override;
