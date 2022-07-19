845ebe4b4ebf302b90bb2d96c42b61cc107e017f
Tooru Fujisawa <arai_a@mac.com>
2022-07-19 20:25:38 +0000

    Bug 1780158 - Exclude JSOp::SetAliasedVar .generator from Debugger.Script.getEffectfulOffsets. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D152155

--- js/src/jit-test/tests/debug/breakpoint-dot-generator.js
+++ js/src/jit-test/tests/debug/breakpoint-dot-generator.js
@@ -32,3 +32,4 @@ script.setBreakpoint(6, {});
 // Offset 1 shouldn't be exposed.
 assertEq(script.getPossibleBreakpoints().some(p => p.offset == 1), false);
 assertEq(script.getAllColumnOffsets().some(p => p.offset == 1), false);
+assertEq(script.getEffectfulOffsets().includes(1), false);
