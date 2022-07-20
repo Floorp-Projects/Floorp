ce7010131150d32c78ff197cc3cdf2ff30053b7b
Matthew Gaudet <mgaudet@mozilla.com>
2022-07-20 20:12:00 +0000

    Bug 1769849 - Improve ShadowRealm.evaluate errors r=evilpie
    Conditionally attempt to extract .name and .message and use for issuing error.
    
    Differential Revision: https://phabricator.services.mozilla.com/D151849

---
+++ js/src/tests/non262/ShadowRealms/error.js
@@ -0,0 +1,48 @@
+// |reftest| shell-option(--enable-shadow-realms) skip-if(!xulRuntime.shell)
+
+let sr = new ShadowRealm();
+
+try {
+    sr.evaluate("throw new Error('hi')");
+    assertEq(true, false, "Should have thrown");
+} catch (e) {
+    assertEq(e instanceof TypeError, true, "Correct type of error")
+    assertEq(/Error: hi/.test(e.message), true, "Should have included information from thrown error");
+}
+
+try {
+    sr.evaluate("throw new Error('∂å∂')");
+    assertEq(true, false, "Should have thrown");
+} catch (e) {
+    assertEq(e instanceof TypeError, true, "Correct type of error")
+    assertEq(/Error: ∂å∂/.test(e.message), true, "Should have included information from thrown error, UTF-8 Pass through.");
+}
+
+try {
+    sr.evaluate("throw {name: 'Hello', message: 'goodbye'}");
+    assertEq(true, false, "Should have thrown");
+} catch (e) {
+    assertEq(e instanceof TypeError, true, "Correct type of error")
+    assertEq(/uncaught exception: Object/.test(e.message), true, "Should get generic fillin message, non-string");
+}
+
+try {
+    sr.evaluate("throw {name: 10, message: 11}");
+    assertEq(true, false, "Should have thrown");
+} catch (e) {
+    assertEq(e instanceof TypeError, true, "Correct type of error")
+    assertEq(/uncaught exception: Object/.test(e.message), true, "Should get generic fillin message, non-string");
+}
+
+
+try {
+    sr.evaluate("throw { get name() { return 'holy'; }, get message() { return 'smokes' } }");
+    assertEq(true, false, "Should have thrown");
+} catch (e) {
+    assertEq(e instanceof TypeError, true, "Correct type of error")
+    assertEq(/uncaught exception: Object/.test(e.message), true, "Should get generic error message, getters");
+}
+
+
+if (typeof reportCompare === 'function')
+    reportCompare(true, true);
