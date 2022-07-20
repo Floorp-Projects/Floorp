eb3aca8baef347bdaec64f8c203408eeee6cd947
Alexandra Borovova <lutien@users.noreply.github.com>
2022-07-20 10:51:41 +0000

    Bug 1779190 [wpt PR 34800] - Add tests for script.callFunction for serialization of complex objects with simple value field., a=testonly
    Automatic update from web-platform-tests
    Add tests for script.callFunction for serialization of complex objects with simple value field. (#34800)
    
    --
    
    wpt-commits: 247522a0d1ee1054bbc5965d64c39bdc8ca14d62
    wpt-pr: 34800

--- testing/web-platform/tests/webdriver/tests/bidi/script/call_function/result.py
+++ testing/web-platform/tests/webdriver/tests/bidi/script/call_function/result.py
@@ -30,3 +30,118 @@ async def test_primitive_values(bidi_session, top_context, expression, expected)
     )
 
     assert result == expected
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "expression, expected",
+    [
+        ("(Symbol('foo'))", {"type": "symbol"}),
+        (
+            "[1, 'foo', true, new RegExp(/foo/g), [1]]",
+            {
+                "type": "array",
+                "value": [
+                    {"type": "number", "value": 1},
+                    {"type": "string", "value": "foo"},
+                    {"type": "boolean", "value": True},
+                    {
+                        "type": "regexp",
+                        "value": {
+                            "pattern": "foo",
+                            "flags": "g",
+                        },
+                    },
+                    {"type": "array"},
+                ],
+            },
+        ),
+        (
+            "({'foo': {'bar': 'baz'}, 'qux': 'quux'})",
+            {
+                "type": "object",
+                "value": [
+                    ["foo", {"type": "object"}],
+                    ["qux", {"type": "string", "value": "quux"}],
+                ],
+            },
+        ),
+        ("(()=>{})", {"type": "function"}),
+        ("(function(){})", {"type": "function"}),
+        ("(async ()=>{})", {"type": "function"}),
+        ("(async function(){})", {"type": "function"}),
+        (
+            "new RegExp(/foo/g)",
+            {"type": "regexp", "value": {"pattern": "foo", "flags": "g"}},
+        ),
+        (
+            "new Date(1654004849000)",
+            {"type": "date", "value": "2022-05-31T13:47:29.000Z"},
+        ),
+        (
+            "new Map([[1, 2], ['foo', 'bar'], [true, false], ['baz', [1]]])",
+            {
+                "type": "map",
+                "value": [
+                    [
+                        {"type": "number", "value": 1},
+                        {"type": "number", "value": 2},
+                    ],
+                    ["foo", {"type": "string", "value": "bar"}],
+                    [
+                        {"type": "boolean", "value": True},
+                        {"type": "boolean", "value": False},
+                    ],
+                    ["baz", {"type": "array"}],
+                ],
+            },
+        ),
+        (
+            "new Set([1, 'foo', true, [1], new Map([[1,2]])])",
+            {
+                "type": "set",
+                "value": [
+                    {"type": "number", "value": 1},
+                    {"type": "string", "value": "foo"},
+                    {"type": "boolean", "value": True},
+                    {"type": "array"},
+                    {"type": "map"},
+                ],
+            },
+        ),
+        ("new WeakMap()", {"type": "weakmap"}),
+        ("new WeakSet()", {"type": "weakset"}),
+        ("new Error('SOME_ERROR_TEXT')", {"type": "error"}),
+        # TODO(sadym): add `iterator` test.
+        # TODO(sadym): add `generator` test.
+        # TODO(sadym): add `proxy` test.
+        ("Promise.resolve()", {"type": "promise"}),
+        ("new Int32Array()", {"type": "typedarray"}),
+        ("new ArrayBuffer()", {"type": "arraybuffer"}),
+        (
+            "document.createElement('div')",
+            {
+                "type": "node",
+                "value": {
+                    "attributes": {},
+                    "childNodeCount": 0,
+                    "children": [],
+                    "localName": "div",
+                    "namespaceURI": "http://www.w3.org/1999/xhtml",
+                    "nodeName": "",
+                    "nodeType": 1,
+                    "nodeValue": "",
+                },
+            },
+        ),
+        ("window", {"type": "window"}),
+    ],
+)
+async def test_remote_values(bidi_session, top_context, expression, expected):
+    result = await bidi_session.script.call_function(
+        function_declaration=f"() => {expression}",
+        await_promise=False,
+        target=ContextTarget(top_context["context"]),
+    )
+
+    assert result == expected
