28123cf49894322353d33e19caa311655aec3164
Mark Banner <standard8@mozilla.com>
2022-07-20 10:36:52 +0000

    Bug 1780074 - Migrate browser/components/newtab to ESM. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D152113

---
+++ browser/components/newtab/lib/OnboardingMessageProvider.sys.mjs
@@ -0,0 +1,417 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+/* globals Localization */
+
+const { XPCOMUtils } = ChromeUtils.importESModule(
+  "resource://gre/modules/XPCOMUtils.sys.mjs"
+);
+
+const lazy = {};
+
+XPCOMUtils.defineLazyModuleGetters(lazy, {
+  ShellService: "resource:///modules/ShellService.jsm",
+});
+
+const L10N = new Localization([
+  "branding/brand.ftl",
+  "browser/branding/brandings.ftl",
+  "browser/branding/sync-brand.ftl",
+  "browser/newtab/onboarding.ftl",
+]);
+
+const ONBOARDING_MESSAGES = () => [
+  {
+    id: "FXA_ACCOUNTS_BADGE",
+    template: "toolbar_badge",
+    content: {
+      delay: 10000, // delay for 10 seconds
+      target: "fxa-toolbar-menu-button",
+    },
+    targeting: "false",
+    trigger: { id: "toolbarBadgeUpdate" },
+  },
+  {
+    id: "PROTECTIONS_PANEL_1",
+    template: "protections_panel",
+    content: {
+      title: { string_id: "cfr-protections-panel-header" },
+      body: { string_id: "cfr-protections-panel-body" },
+      link_text: { string_id: "cfr-protections-panel-link-text" },
+      cta_url: `${Services.urlFormatter.formatURLPref(
+        "app.support.baseURL"
+      )}etp-promotions?as=u&utm_source=inproduct`,
+      cta_type: "OPEN_URL",
+    },
+    trigger: { id: "protectionsPanelOpen" },
+  },
+  {
+    id: "FX_100_UPGRADE",
+    template: "spotlight",
+    targeting: "false",
+    content: {
+      template: "multistage",
+      id: "FX_100_UPGRADE",
+      transitions: true,
+      screens: [
+        {
+          id: "UPGRADE_PIN_FIREFOX",
+          content: {
+            logo: {
+              imageURL:
+                "chrome://activity-stream/content/data/content/assets/heart.webp",
+              height: "73px",
+            },
+            has_noodles: true,
+            title: {
+              fontSize: "36px",
+              string_id: "fx100-upgrade-thanks-header",
+            },
+            title_style: "fancy shine",
+            background:
+              "url('chrome://activity-stream/content/data/content/assets/confetti.svg') top / 100% no-repeat var(--in-content-page-background)",
+            subtitle: {
+              string_id: "fx100-upgrade-thanks-keep-body",
+            },
+            primary_button: {
+              label: {
+                string_id: "fx100-thank-you-pin-primary-button-label",
+              },
+              action: {
+                navigate: true,
+                type: "PIN_FIREFOX_TO_TASKBAR",
+              },
+            },
+            secondary_button: {
+              label: {
+                string_id: "mr1-onboarding-set-default-secondary-button-label",
+              },
+              action: {
+                navigate: true,
+              },
+            },
+          },
+        },
+      ],
+    },
+  },
+  {
+    id: "PB_NEWTAB_FOCUS_PROMO",
+    type: "default",
+    template: "pb_newtab",
+    groups: ["pbNewtab"],
+    content: {
+      infoBody: "fluent:about-private-browsing-info-description-simplified",
+      infoEnabled: true,
+      infoIcon: "chrome://global/skin/icons/indicator-private-browsing.svg",
+      infoLinkText: "fluent:about-private-browsing-learn-more-link",
+      infoTitle: "",
+      infoTitleEnabled: false,
+      promoEnabled: true,
+      promoType: "FOCUS",
+      promoHeader: "fluent:about-private-browsing-focus-promo-header-c",
+      promoImageLarge: "chrome://browser/content/assets/focus-promo.png",
+      promoLinkText: "fluent:about-private-browsing-focus-promo-cta",
+      promoLinkType: "button",
+      promoSectionStyle: "below-search",
+      promoTitle: "fluent:about-private-browsing-focus-promo-text-c",
+      promoTitleEnabled: true,
+      promoButton: {
+        action: {
+          type: "SHOW_SPOTLIGHT",
+          data: {
+            content: {
+              id: "FOCUS_PROMO",
+              template: "multistage",
+              modal: "tab",
+              backdrop: "transparent",
+              screens: [
+                {
+                  id: "DEFAULT_MODAL_UI",
+                  content: {
+                    logo: {
+                      imageURL:
+                        "chrome://browser/content/assets/focus-logo.svg",
+                      height: "48px",
+                    },
+                    title: {
+                      string_id: "spotlight-focus-promo-title",
+                    },
+                    subtitle: {
+                      string_id: "spotlight-focus-promo-subtitle",
+                    },
+                    dismiss_button: {
+                      action: {
+                        navigate: true,
+                      },
+                    },
+                    ios: {
+                      action: {
+                        data: {
+                          args:
+                            "https://app.adjust.com/167k4ih?campaign=firefox-desktop&adgroup=pb&creative=focus-omc172&redirect=https%3A%2F%2Fapps.apple.com%2Fus%2Fapp%2Ffirefox-focus-privacy-browser%2Fid1055677337",
+                          where: "tabshifted",
+                        },
+                        type: "OPEN_URL",
+                        navigate: true,
+                      },
+                    },
+                    android: {
+                      action: {
+                        data: {
+                          args:
+                            "https://app.adjust.com/167k4ih?campaign=firefox-desktop&adgroup=pb&creative=focus-omc172&redirect=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dorg.mozilla.focus",
+                          where: "tabshifted",
+                        },
+                        type: "OPEN_URL",
+                        navigate: true,
+                      },
+                    },
+                    tiles: {
+                      type: "mobile_downloads",
+                      data: {
+                        QR_code: {
+                          image_url:
+                            "chrome://browser/content/assets/focus-qr-code.svg",
+                          alt_text: {
+                            string_id: "spotlight-focus-promo-qr-code",
+                          },
+                          image_overrides: {
+                            de:
+                              "chrome://browser/content/assets/klar-qr-code.svg",
+                          },
+                        },
+                        marketplace_buttons: ["ios", "android"],
+                      },
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+    priority: 2,
+    frequency: {
+      custom: [
+        {
+          cap: 3,
+          period: 604800000, // Max 3 per week
+        },
+      ],
+      lifetime: 12,
+    },
+    targeting: "!(region in [ 'DE', 'AT', 'CH'] && localeLanguageCode == 'en')",
+  },
+  {
+    id: "PB_NEWTAB_KLAR_PROMO",
+    type: "default",
+    template: "pb_newtab",
+    groups: ["pbNewtab"],
+    content: {
+      infoBody: "fluent:about-private-browsing-info-description-simplified",
+      infoEnabled: true,
+      infoIcon: "chrome://global/skin/icons/indicator-private-browsing.svg",
+      infoLinkText: "fluent:about-private-browsing-learn-more-link",
+      infoTitle: "",
+      infoTitleEnabled: false,
+      promoEnabled: true,
+      promoType: "FOCUS",
+      promoHeader: "fluent:about-private-browsing-focus-promo-header-c",
+      promoImageLarge: "chrome://browser/content/assets/focus-promo.png",
+      promoLinkText: "Download Firefox Klar",
+      promoLinkType: "button",
+      promoSectionStyle: "below-search",
+      promoTitle:
+        "Firefox Klar clears your history every time while blocking ads and trackers.",
+      promoTitleEnabled: true,
+      promoButton: {
+        action: {
+          type: "SHOW_SPOTLIGHT",
+          data: {
+            content: {
+              id: "KLAR_PROMO",
+              template: "multistage",
+              modal: "tab",
+              backdrop: "transparent",
+              screens: [
+                {
+                  id: "DEFAULT_MODAL_UI",
+                  order: 0,
+                  content: {
+                    logo: {
+                      imageURL:
+                        "chrome://browser/content/assets/focus-logo.svg",
+                      height: "48px",
+                    },
+                    title: "Get Firefox Klar",
+                    subtitle: {
+                      string_id: "spotlight-focus-promo-subtitle",
+                    },
+                    dismiss_button: {
+                      action: {
+                        navigate: true,
+                      },
+                    },
+                    ios: {
+                      action: {
+                        data: {
+                          args:
+                            "https://app.adjust.com/a8bxj8j?campaign=firefox-desktop&adgroup=pb&creative=focus-omc172&redirect=https%3A%2F%2Fapps.apple.com%2Fde%2Fapp%2Fklar-by-firefox%2Fid1073435754",
+                          where: "tabshifted",
+                        },
+                        type: "OPEN_URL",
+                        navigate: true,
+                      },
+                    },
+                    android: {
+                      action: {
+                        data: {
+                          args:
+                            "https://app.adjust.com/a8bxj8j?campaign=firefox-desktop&adgroup=pb&creative=focus-omc172&redirect=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dorg.mozilla.klar",
+                          where: "tabshifted",
+                        },
+                        type: "OPEN_URL",
+                        navigate: true,
+                      },
+                    },
+                    tiles: {
+                      type: "mobile_downloads",
+                      data: {
+                        QR_code: {
+                          image_url:
+                            "chrome://browser/content/assets/klar-qr-code.svg",
+                          alt_text: {
+                            string_id: "spotlight-focus-promo-qr-code",
+                          },
+                        },
+                        marketplace_buttons: ["ios", "android"],
+                      },
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+    priority: 2,
+    frequency: {
+      custom: [
+        {
+          cap: 3,
+          period: 604800000, // Max 3 per week
+        },
+      ],
+      lifetime: 12,
+    },
+    targeting: "region in [ 'DE', 'AT', 'CH'] && localeLanguageCode == 'en'",
+  },
+  {
+    id: "PB_NEWTAB_INFO_SECTION",
+    template: "pb_newtab",
+    content: {
+      promoEnabled: false,
+      infoEnabled: true,
+      infoIcon: "",
+      infoTitle: "",
+      infoBody: "fluent:about-private-browsing-info-description-private-window",
+      infoLinkText: "fluent:about-private-browsing-learn-more-link",
+      infoTitleEnabled: false,
+    },
+    targeting: "true",
+  },
+];
+
+export const OnboardingMessageProvider = {
+  async getExtraAttributes() {
+    const [header, button_label] = await L10N.formatMessages([
+      { id: "onboarding-welcome-header" },
+      { id: "onboarding-start-browsing-button-label" },
+    ]);
+    return { header: header.value, button_label: button_label.value };
+  },
+  async getMessages() {
+    const messages = await this.translateMessages(await ONBOARDING_MESSAGES());
+    return messages;
+  },
+  async getUntranslatedMessages() {
+    // This is helpful for jsonSchema testing - since we are localizing in the provider
+    const messages = await ONBOARDING_MESSAGES();
+    return messages;
+  },
+  async translateMessages(messages) {
+    let translatedMessages = [];
+    for (const msg of messages) {
+      let translatedMessage = { ...msg };
+
+      // If the message has no content, do not attempt to translate it
+      if (!translatedMessage.content) {
+        translatedMessages.push(translatedMessage);
+        continue;
+      }
+
+      // Translate any secondary buttons separately
+      if (msg.content.secondary_button) {
+        const [secondary_button_string] = await L10N.formatMessages([
+          { id: msg.content.secondary_button.label.string_id },
+        ]);
+        translatedMessage.content.secondary_button.label =
+          secondary_button_string.value;
+      }
+      if (msg.content.header) {
+        const [header_string] = await L10N.formatMessages([
+          { id: msg.content.header.string_id },
+        ]);
+        translatedMessage.content.header = header_string.value;
+      }
+      translatedMessages.push(translatedMessage);
+    }
+    return translatedMessages;
+  },
+  async _doesAppNeedPin() {
+    const needPin = await lazy.ShellService.doesAppNeedPin();
+    return needPin;
+  },
+  async _doesAppNeedDefault() {
+    let checkDefault = Services.prefs.getBoolPref(
+      "browser.shell.checkDefaultBrowser",
+      false
+    );
+    let isDefault = await lazy.ShellService.isDefaultBrowser();
+    return checkDefault && !isDefault;
+  },
+  async getUpgradeMessage() {
+    let message = (await OnboardingMessageProvider.getMessages()).find(
+      ({ id }) => id === "FX_100_UPGRADE"
+    );
+    let { content } = message;
+    let pinScreen = content.screens?.find(
+      screen => screen.id === "UPGRADE_PIN_FIREFOX"
+    );
+    const needPin = await this._doesAppNeedPin();
+    const needDefault = await this._doesAppNeedDefault();
+    // If user doesn't need pin, update screen to set "default" or "get started" configuration
+    if (!needPin && pinScreen) {
+      let primary = pinScreen.content.primary_button;
+      if (needDefault) {
+        pinScreen.id = "UPGRADE_ONLY_DEFAULT";
+        primary.label.string_id =
+          "mr1-onboarding-set-default-only-primary-button-label";
+        primary.action.type = "SET_DEFAULT_BROWSER";
+      } else {
+        pinScreen.id = "UPGRADE_GET_STARTED";
+        pinScreen.content.subtitle.string_id = "fx100-upgrade-thank-you-body";
+        primary.label.string_id = "mr2-onboarding-start-browsing-button-label";
+        pinScreen.auto_advance = "primary_button";
+        delete primary.action.type;
+        delete pinScreen.content.secondary_button;
+      }
+    }
+
+    return message;
+  },
+};
---
+++ browser/components/newtab/lib/RemoteImages.sys.mjs
@@ -0,0 +1,422 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const { JSONFile } = ChromeUtils.import("resource://gre/modules/JSONFile.jsm");
+const { PromiseUtils } = ChromeUtils.import(
+  "resource://gre/modules/PromiseUtils.jsm"
+);
+const { RemoteSettings } = ChromeUtils.import(
+  "resource://services-settings/remote-settings.js"
+);
+
+const lazy = {};
+
+ChromeUtils.defineModuleGetter(
+  lazy,
+  "Downloader",
+  "resource://services-settings/Attachments.jsm"
+);
+
+ChromeUtils.defineModuleGetter(
+  lazy,
+  "KintoHttpClient",
+  "resource://services-common/kinto-http-client.js"
+);
+
+const RS_SERVER_PREF = "services.settings.server";
+const RS_MAIN_BUCKET = "main";
+const RS_COLLECTION = "ms-images";
+const RS_DOWNLOAD_MAX_RETRIES = 2;
+
+export const REMOTE_IMAGES_PATH = PathUtils.join(
+  PathUtils.localProfileDir,
+  "settings",
+  RS_MAIN_BUCKET,
+  RS_COLLECTION
+);
+
+export const REMOTE_IMAGES_DB_PATH = PathUtils.join(
+  REMOTE_IMAGES_PATH,
+  "db.json"
+);
+
+const IMAGE_EXPIRY_DURATION = 30 * 24 * 60 * 60; // 30 days in seconds.
+
+class _RemoteImages {
+  #dbPromise;
+
+  constructor() {
+    this.#dbPromise = null;
+
+    RemoteSettings(RS_COLLECTION).on("sync", () => this.#onSync());
+
+    // Ensure we migrate all our images to a JSONFile database.
+    this.withDb(() => {});
+  }
+
+  /**
+   * Load the database from disk.
+   *
+   * If the database does not yet exist, attempt a migration from legacy Remote
+   * Images (i.e., image files in |REMOTE_IMAGES_PATH|).
+   *
+   * @returns {Promise<JSONFile>} A promise that resolves with the database
+   *                              instance.
+   */
+  async #loadDb() {
+    let db;
+
+    if (!(await IOUtils.exists(REMOTE_IMAGES_DB_PATH))) {
+      db = await this.#migrate();
+    } else {
+      db = new JSONFile({ path: REMOTE_IMAGES_DB_PATH });
+      await db.load();
+    }
+
+    return db;
+  }
+
+  /**
+   * Reset the RemoteImages database
+   *
+   * NB: This is only meant to be used by unit tests.
+   *
+   * @returns {Promise<void>} A promise that resolves when the database has been
+   *                          reset.
+   */
+  reset() {
+    return this.withDb(async db => {
+      // We must reset |#dbPromise| *before* awaiting because if we do not, then
+      // another function could call withDb() while we are awaiting and get a
+      // promise that will resolve to |db| instead of getting null and forcing a
+      // db reload.
+      this.#dbPromise = null;
+      await db.finalize();
+    });
+  }
+
+  /*
+   * Execute |fn| with the RemoteSettings database.
+   *
+   * This ensures that only one caller can have a handle to the database at any
+   * given time (unless it is leaked through assignment from within |fn|). This
+   * prevents re-entrancy issues with multiple calls to cleanup() and calling
+   * cleanup while loading images.
+   *
+   * @param fn The function to call with the database.
+   */
+  async withDb(fn) {
+    const dbPromise = this.#dbPromise ?? this.#loadDb();
+
+    const { resolve, promise } = PromiseUtils.defer();
+    // NB: Update |#dbPromise| before awaiting anything so that the next call to
+    //     |withDb()| will see the new value of |#dbPromise|.
+    this.#dbPromise = promise;
+
+    const db = await dbPromise;
+
+    try {
+      return await fn(db);
+    } finally {
+      resolve(db);
+    }
+  }
+
+  /**
+   * Patch a reference to a remote image in a message with a blob URL.
+   *
+   * @param message     The remote image reference to be patched.
+   * @param replaceWith The property name that will be used to store the blob
+   *                    URL on |message|.
+   *
+   * @return A promise that resolves with an unloading function for the patched
+   *         URL, or rejects with an error.
+   *
+   *         If the message isn't patched (because there isn't a remote image)
+   *         then the promise will resolve to null.
+   */
+  async patchMessage(message, replaceWith = "imageURL") {
+    if (!!message && !!message.imageId) {
+      const { imageId } = message;
+      const urls = await this.load(imageId);
+
+      if (urls.size) {
+        const blobURL = urls.get(imageId);
+
+        delete message.imageId;
+        message[replaceWith] = blobURL;
+
+        return () => this.unload(urls);
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Load remote images.
+   *
+   * If the images have not been previously downloaded, then they will be
+   * downloaded from RemoteSettings.
+   *
+   * @param {...string} imageIds The image IDs to load.
+   *
+   * @returns {object} An object mapping image Ids to blob: URLs.
+   *                   If an image could not be loaded, it will not be present
+   *                   in the returned object.
+   *
+   *                   After the caller is finished with the images, they must call
+   *                   |RemoteImages.unload()| on the object.
+   */
+  load(...imageIds) {
+    return this.withDb(async db => {
+      // Deduplicate repeated imageIds by using a Map.
+      const urls = new Map(imageIds.map(key => [key, undefined]));
+
+      await Promise.all(
+        Array.from(urls.keys()).map(async imageId => {
+          try {
+            urls.set(imageId, await this.#loadImpl(db, imageId));
+          } catch (e) {
+            Cu.reportError(`Could not load image ID ${imageId}: ${e}`);
+            urls.delete(imageId);
+          }
+        })
+      );
+
+      return urls;
+    });
+  }
+
+  async #loadImpl(db, imageId) {
+    const recordId = this.#getRecordId(imageId);
+
+    let blob;
+    if (db.data.images[recordId]) {
+      // We have previously fetched this image, we can load it from disk.
+      try {
+        blob = await this.#readFromDisk(db, recordId);
+      } catch (e) {
+        if (
+          !(
+            e instanceof Components.Exception &&
+            e.name === "NS_ERROR_FILE_NOT_FOUND"
+          )
+        ) {
+          throw e;
+        }
+      }
+
+      // Fall back to downloading if we cannot read it from disk.
+    }
+
+    if (typeof blob === "undefined") {
+      blob = await this.#download(db, recordId);
+    }
+
+    return URL.createObjectURL(blob);
+  }
+
+  /**
+   * Unload URLs returned by RemoteImages
+   *
+   * @param {Map<string, string>} urls The result of calling |RemoteImages.load()|.
+   **/
+  unload(urls) {
+    for (const url of urls.keys()) {
+      URL.revokeObjectURL(url);
+    }
+  }
+
+  #onSync() {
+    return this.withDb(async db => {
+      await this.#cleanup(db);
+
+      const recordsById = await RemoteSettings(RS_COLLECTION)
+        .db.list()
+        .then(records =>
+          Object.assign({}, ...records.map(record => ({ [record.id]: record })))
+        );
+
+      await Promise.all(
+        Object.values(db.data.images)
+          .filter(
+            entry => recordsById[entry.recordId]?.attachment.hash !== entry.hash
+          )
+          .map(entry => this.#download(db, entry.recordId, { refresh: true }))
+      );
+    });
+  }
+
+  forceCleanup() {
+    return this.withDb(db => this.#cleanup(db));
+  }
+
+  /**
+   * Clean up all files that haven't been touched in 30d.
+   *
+   * @returns {Promise<undefined>} A promise that resolves once cleanup has
+   *                               finished.
+   */
+  async #cleanup(db) {
+    const now = Date.now();
+    await Promise.all(
+      Object.values(db.data.images)
+        .filter(entry => now - entry.lastLoaded >= IMAGE_EXPIRY_DURATION)
+        .map(entry => {
+          const path = PathUtils.join(REMOTE_IMAGES_PATH, entry.recordId);
+          delete db.data.images[entry.recordId];
+
+          return IOUtils.remove(path).catch(e => {
+            Cu.reportError(
+              `Could not remove remote image ${entry.recordId}: ${e}`
+            );
+          });
+        })
+    );
+
+    db.saveSoon();
+  }
+
+  /**
+   * Return the record ID from an image ID.
+   *
+   * Prior to Firefox 101, imageIds were of the form ${recordId}.${extension} so
+   * that we could infer the mimetype.
+   *
+   * @returns The RemoteSettings record ID.
+   */
+  #getRecordId(imageId) {
+    const idx = imageId.lastIndexOf(".");
+    if (idx === -1) {
+      return imageId;
+    }
+    return imageId.substring(0, idx);
+  }
+
+  /**
+   * Read the image from disk
+   *
+   * @param {JSONFile} db The RemoteImages database.
+   * @param {string} recordId The record ID of the image.
+   *
+   * @returns A promise that resolves to a blob, or rejects with an Error.
+   */
+  async #readFromDisk(db, recordId) {
+    const path = PathUtils.join(REMOTE_IMAGES_PATH, recordId);
+
+    try {
+      const blob = await File.createFromFileName(path, {
+        type: db.data.images[recordId].mimetype,
+      });
+      db.data.images[recordId].lastLoaded = Date.now();
+
+      return blob;
+    } catch (e) {
+      // If we cannot read the file from disk, delete the entry.
+      delete db.data.images[recordId];
+
+      throw e;
+    } finally {
+      db.saveSoon();
+    }
+  }
+
+  /**
+   * Download an image from RemoteSettings.
+   *
+   * @param {JSONFile} db The RemoteImages database.
+   * @param {string} recordId The record ID of the image.
+   * @param {object} options Options for downloading the image.
+   * @param {boolean} options.refresh Whether or not the image is being
+   *                                  downloaded because it is out of sync with
+   *                                  Remote Settings. If true, the blob will be
+   *                                  written to disk and not returned.
+   *                                  Additionally, the |lastLoaded| field will
+   *                                  not be updated in its db entry.
+   *
+   * @returns A promise that resolves with a Blob of the image data or rejects
+   *          with an Error.
+   */
+  async #download(db, recordId, { refresh = false } = {}) {
+    const client = new lazy.KintoHttpClient(
+      Services.prefs.getStringPref(RS_SERVER_PREF)
+    );
+
+    const record = await client
+      .bucket(RS_MAIN_BUCKET)
+      .collection(RS_COLLECTION)
+      .getRecord(recordId);
+
+    const downloader = new lazy.Downloader(RS_MAIN_BUCKET, RS_COLLECTION);
+    const arrayBuffer = await downloader.downloadAsBytes(record.data, {
+      retries: RS_DOWNLOAD_MAX_RETRIES,
+    });
+
+    // Cache to disk.
+    const path = PathUtils.join(REMOTE_IMAGES_PATH, recordId);
+
+    // We do not await this promise because any other attempt to interact with
+    // the file via IOUtils will have to synchronize via the IOUtils event queue
+    // anyway.
+    IOUtils.write(path, new Uint8Array(arrayBuffer));
+
+    const { mimetype, hash } = record.data.attachment;
+
+    if (refresh) {
+      Object.assign(db.data.images[recordId], { mimetype, hash });
+    } else {
+      db.data.images[recordId] = {
+        recordId,
+        mimetype,
+        hash,
+        lastLoaded: Date.now(),
+      };
+    }
+
+    db.saveSoon();
+
+    if (!refresh) {
+      return new Blob([arrayBuffer], { type: record.data.attachment.mimetype });
+    }
+
+    return undefined;
+  }
+
+  /**
+   * Migrate from a file-based store to an index-based store.
+   */
+  async #migrate() {
+    let children;
+    try {
+      children = await IOUtils.getChildren(REMOTE_IMAGES_PATH);
+
+      // Delete all previously cached entries.
+      await Promise.all(
+        children.map(async path => {
+          try {
+            await IOUtils.remove(path);
+          } catch (e) {
+            Cu.reportError(`RemoteImages could not delete ${path}: ${e}`);
+          }
+        })
+      );
+    } catch (e) {
+      if (!(DOMException.isInstance(e) && e.name === "NotFoundError")) {
+        throw e;
+      }
+    }
+
+    await IOUtils.makeDirectory(REMOTE_IMAGES_PATH);
+    const db = new JSONFile({ path: REMOTE_IMAGES_DB_PATH });
+    db.data = {
+      version: 1,
+      images: {},
+    };
+    db.saveSoon();
+    return db;
+  }
+}
+
+export const RemoteImages = new _RemoteImages();
