ce2d56136ab0ea90c090916c4e61893e53b57193
Brad Werth <bwerth@mozilla.com>
2022-07-20 20:00:26 +0000

    Bug 1737682 Part 1: Log whether or not we are hitting the detached state. r=mstange
    There are 10 enum cases that we track internally, one of which isn't
    emitted as telemetry. These cases are 0-indexed to match the enum values.
    
    0 / NotVideo: Not used for telemetry. No video is showing.
    
    1 / LowPower: We are showing exactly one video and we believe we are
    hitting the video low power mode. We don't use "Success" because of name
    collision in the telemetry generation.
    
    2 / FailMultipleVideo: There is more than one video visible.
    
    3 / FailWindowed: The video is being viewed in windowed mode, not
    fullscreen mode, so low power mode is not possible.
    
    4 / FailOverlaid: The video has something on top of it (like captions).
    
    5 / FailBacking: The layer directly underneath the video does not cover
    the window or does not have a black background.
    
    6 / FailMacOSVersion: The system is running a too-early version of macOS.
    
    7 / FailPref: The user has disabled the
    `gfx.core-animation.specialize-video` pref.
    
    8 / FailSurface: The video is encoded in such a way we can't decode it to
    a qualifying pixel format.
    
    9 / FailEnqueue: The video didn't enqueue properly, and we fell back to a
    non-video display path.
    
    Differential Revision: https://phabricator.services.mozilla.com/D129453

--- gfx/layers/NativeLayerCA.h
+++ gfx/layers/NativeLayerCA.h
@@ -42,6 +42,22 @@ namespace layers {
 class NativeLayerRootSnapshotterCA;
 class SurfacePoolHandleCA;
 
+enum class VideoLowPowerType {
+  // These must be kept synchronized with the telemetry histogram enums.
+  NotVideo,           // Never emitted as telemetry. No video is visible.
+  LowPower,           // As best we can tell, we are in the "detached",
+                      // low-power compositing mode. We don't use "Success"
+                      // because of name collision with telemetry generation.
+  FailMultipleVideo,  // There is more than one video visible.
+  FailWindowed,       // The window is not fullscreen.
+  FailOverlaid,       // Something is on top of the video (likely captions).
+  FailBacking,        // The layer behind the video is not full-coverage black.
+  FailMacOSVersion,   // macOS version does not meet requirements.
+  FailPref,           // Pref is not set.
+  FailSurface,        // Surface is not eligible.
+  FailEnqueue,        // Enqueueing the video didn't work.
+};
+
 // NativeLayerRootCA is the CoreAnimation implementation of the NativeLayerRoot
 // interface. A NativeLayerRootCA is created by the widget around an existing
 // CALayer with a call to CreateForCALayer - this CALayer is the root of the
@@ -122,7 +138,8 @@ class NativeLayerRootCA : public NativeLayerRoot {
       gfx::DeviceColor aColor) override;
 
   void SetWindowIsFullscreen(bool aFullscreen);
-  void NoteMouseMoveAtTime(const TimeStamp& aTime);
+
+  VideoLowPowerType CheckVideoLowPower();
 
  protected:
   explicit NativeLayerRootCA(CALayer* aLayer);
@@ -164,6 +181,12 @@ class NativeLayerRootCA : public NativeLayerRoot {
   // Updated by the layer's view's window to match the fullscreen state
   // of that window.
   bool mWindowIsFullscreen = false;
+
+  // How many times have we committed since the last time we emitted
+  // telemetry?
+  unsigned int mTelemetryCommitCount = 0;
+  static const unsigned int TELEMETRY_COMMIT_PERIOD =
+      600;  // 10 seconds at 60fps
 };
 
 class RenderSourceNLRS;
