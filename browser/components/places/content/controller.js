c9b826a4b1290f1c0bcfd15ac2594d438fc98b80
Mark Banner <standard8@mozilla.com>
2022-07-20 10:36:49 +0000

    Bug 1779982 - Use explicit imports in controller.js rather than telling ESLint about imports from modules. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D152003

--- browser/components/places/content/controller.js
+++ browser/components/places/content/controller.js
@@ -3,10 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+XPCOMUtils.defineLazyModuleGetters(this, {
+  PlacesUtils: "resource://gre/modules/PlacesUtils.jsm",
+  PlacesUIUtils: "resource:///modules/PlacesUIUtils.jsm",
+  PlacesTransactions: "resource://gre/modules/PlacesTransactions.jsm",
+});
 /* import-globals-from /browser/base/content/utilityOverlay.js */
-/* import-globals-from ../PlacesUIUtils.jsm */
-/* import-globals-from /toolkit/components/places/PlacesUtils.jsm */
-/* import-globals-from /toolkit/components/places/PlacesTransactions.jsm */
 /* import-globals-from ./places.js */
 
 /**
