2b0d1828385a0eddfa11a8ff4a22c2b41a16fb65
Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>
2022-07-20 10:52:01 +0000

    Bug 1779211 [wpt PR 34803] - Add `resultOwnership` tests for `script.evaluate`, a=testonly
    Automatic update from web-platform-tests
    Add `resultOwnership` tests for `script.evaluate` (#34803)
    
    * Add `resultOwnership` tests for `script.evaluate`
    * Reuse `assert_handle` in `call_function` and `evaluate`
    * Update tests to not expect `handle`.
    
    Co-authored-by: Maksim Sadym <sadym@google.com>
    --
    
    wpt-commits: 65f347778aabd3c87b7248cd436bc8160b6e1bd5
    wpt-pr: 34803

--- testing/web-platform/tests/webdriver/tests/bidi/script/evaluate/result_ownership.py
+++ testing/web-platform/tests/webdriver/tests/bidi/script/evaluate/result_ownership.py
@@ -9,8 +9,8 @@ from .. import assert_handle
                          [("root", True), ("none", False), (None, False)])
 async def test_throw_exception(bidi_session, top_context, result_ownership, should_contain_handle):
     with pytest.raises(ScriptEvaluateResultException) as exception:
-        await bidi_session.script.call_function(
-            function_declaration='()=>{throw {a:1}}',
+        await bidi_session.script.evaluate(
+            expression='throw {a:1}',
             await_promise=False,
             result_ownership=result_ownership,
             target=ContextTarget(top_context["context"]))
@@ -23,8 +23,8 @@ async def test_throw_exception(bidi_session, top_context, result_ownership, shou
                          [("root", True), ("none", False), (None, False)])
 async def test_invalid_script(bidi_session, top_context, result_ownership, should_contain_handle):
     with pytest.raises(ScriptEvaluateResultException) as exception:
-        await bidi_session.script.call_function(
-            function_declaration="))) !!@@## some invalid JS script (((",
+        await bidi_session.script.evaluate(
+            expression="))) !!@@## some invalid JS script (((",
             await_promise=False,
             result_ownership=result_ownership,
             target=ContextTarget(top_context["context"]))
@@ -37,8 +37,8 @@ async def test_invalid_script(bidi_session, top_context, result_ownership, shoul
                          [("root", True), ("none", False), (None, False)])
 async def test_rejected_promise(bidi_session, top_context, result_ownership, should_contain_handle):
     with pytest.raises(ScriptEvaluateResultException) as exception:
-        await bidi_session.script.call_function(
-            function_declaration="()=>{return Promise.reject({a:1})}",
+        await bidi_session.script.evaluate(
+            expression="Promise.reject({a:1})",
             await_promise=True,
             result_ownership=result_ownership,
             target=ContextTarget(top_context["context"]))
@@ -51,8 +51,8 @@ async def test_rejected_promise(bidi_session, top_context, result_ownership, sho
 @pytest.mark.parametrize("result_ownership, should_contain_handle",
                          [("root", True), ("none", False), (None, False)])
 async def test_return_value(bidi_session, top_context, await_promise, result_ownership, should_contain_handle):
-    result = await bidi_session.script.call_function(
-        function_declaration="async function(){return {a:1}}",
+    result = await bidi_session.script.evaluate(
+        expression="Promise.resolve({a:1})",
         await_promise=await_promise,
         result_ownership=result_ownership,
         target=ContextTarget(top_context["context"]))
