d890aedad545115d4138ce4ab57e7aa2e6f8d228
Mathieu Leplatre <mathieu@mozilla.com>
2022-07-21 14:48:51 +0000

    Bug 1777225 - Clear last_etag preference when server responds with error r=gbeckley
    Differential Revision: https://phabricator.services.mozilla.com/D150765

--- services/settings/remote-settings.js
+++ services/settings/remote-settings.js
@@ -315,6 +315,9 @@ function remoteSettingsFunction() {
         reportStatus = lazy.UptakeTelemetry.STATUS.CONTENT_ERROR;
       } else if (/Server/.test(e.message)) {
         reportStatus = lazy.UptakeTelemetry.STATUS.SERVER_ERROR;
+        // If the server replied with bad request, clear the last ETag
+        // value to unblock the next run of synchronization.
+        lazy.gPrefs.clearUserPref(PREF_SETTINGS_LAST_ETAG);
       } else if (/Timeout/.test(e.message)) {
         reportStatus = lazy.UptakeTelemetry.STATUS.TIMEOUT_ERROR;
       } else if (/NetworkError/.test(e.message)) {
