6fa56c5349a8b1e5c7e185145bb7c3c09257fdcf
smolnar <smolnar@mozilla.com>
2022-07-21 05:07:34 +0300

    Backed out changeset 81082e189b59 (bug 1780445) for causing node-new-tab failure.  CLOSED TREE

--- browser/components/newtab/test/browser/browser_asrouter_bug1761522.js
+++ browser/components/newtab/test/browser/browser_asrouter_bug1761522.js
@@ -17,8 +17,6 @@ const { RemoteSettings } = ChromeUtils.import(
   "resource://services-settings/remote-settings.js"
 );
 
-// This pref is used to override the Remote Settings server URL in tests.
-// See SERVER_URL in services/settings/Utils.jsm for more details.
 const RS_SERVER_PREF = "services.settings.server";
 
 const FLUENT_CONTENT = "asrouter-test-string = Test Test Test\n";
