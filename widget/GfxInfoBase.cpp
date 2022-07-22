48d6546a13014accffd3fdc1816e4b4b4499a59b
az <azebrowski@mozilla.com>
2022-07-22 23:08:08 +0000

    Bug 1766308 - Add gfxVars::CodecSupportInfo to store user-readable codec support string r=media-playback-reviewers,alwu
    Differential Revision: https://phabricator.services.mozilla.com/D147386

--- widget/GfxInfoBase.cpp
+++ widget/GfxInfoBase.cpp
@@ -1864,6 +1864,12 @@ GfxInfoBase::GetTargetFrameRate(uint32_t* aTargetFrameRate) {
 }
 
 NS_IMETHODIMP
+GfxInfoBase::GetCodecSupportInfo(nsACString& aCodecSupportInfo) {
+  aCodecSupportInfo.Assign(gfx::gfxVars::CodecSupportInfo());
+  return NS_OK;
+}
+
+NS_IMETHODIMP
 GfxInfoBase::GetIsHeadless(bool* aIsHeadless) {
   *aIsHeadless = gfxPlatform::IsHeadless();
   return NS_OK;
