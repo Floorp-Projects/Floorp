e081ed6ccdbb94c59e1d50de34d13703557472bf
Mark Banner <standard8@mozilla.com>
2022-07-25 10:54:13 +0000

    Bug 1781026 - Fix xpcshell no-unused-vars warnings in testing/xpcshell/example/. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D152621

--- testing/xpcshell/example/unit/test_get_file.js
+++ testing/xpcshell/example/unit/test_get_file.js
@@ -3,8 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var subscriptLoaded = false;
-
 function run_test() {
   var lf = do_get_file("file.txt");
   Assert.ok(lf.exists());
