9fb46a49e9e65c1326df36ac0e64edbd2892bd29
surapunoyousei <ryosukeusuusu@gmail.com>
2022-07-25 16:43:15 +0900

    delete experiment extensions

--- browser/extensions/floorp-system-mac/background-script.js
+++ browser/extensions/floorp-system-mac/background-script.js
@@ -7,28 +7,3 @@
         for (let tab of tabs) {
           initializePageAction(tab);
   }});
-
- let downloads = {};
-  browser.downloads.onCreated.addListener(
-    async function checkfordownload(file) {
-    let pref = await browser.aboutConfigPrefs.getPref("floorp.download.notification");
-    if(pref===1||pref===3){
-      browser.notifications.create({
-        "type": "basic",
-        "iconUrl": "chrome://branding/content/about-logo.png",
-        "title": chrome.i18n.getMessage("start"),
-        "message": file.filename
-    });}
-    downloads[file.id] = file.filename;
-});
-  
- browser.downloads.onChanged.addListener(
-    async function checkfordownloadfinish(file) {
-    let pref = await browser.aboutConfigPrefs.getPref("floorp.download.notification");
-    if (file.state.current == "complete"&& pref===2 || pref===3) {
-        browser.notifications.create({
-            "type": "basic",
-            "iconUrl": "chrome://branding/content/about-logo.png",
-            "title": chrome.i18n.getMessage("finish"),
-            "message": downloads[file.id]
- });}});
