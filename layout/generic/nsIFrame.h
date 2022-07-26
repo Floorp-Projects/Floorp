fe32378bf5e28edb51a54589f09789837e86f384
Emilio Cobos Álvarez <emilio@crisal.io>
2022-07-26 15:06:30 +0000

    Bug 1781034 - Remove layout debugger's visual debugging. r=layout-reviewers,dshin
    Differential Revision: https://phabricator.services.mozilla.com/D152628

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -5503,14 +5503,6 @@ class nsIFrame : public nsQueryFrame {
   static void DisplayReflowShutdown();
 
   static mozilla::LazyLogModule sFrameLogModule;
-
-  // Show frame borders when rendering
-  static void ShowFrameBorders(bool aEnable);
-  static bool GetShowFrameBorders();
-
-  // Show frame border of event target
-  static void ShowEventTargetFrameBorder(bool aEnable);
-  static bool GetShowEventTargetFrameBorder();
 #endif
 };
 
