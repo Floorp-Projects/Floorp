e14db45e7a44277046479d8567b44f0f6fcc81e7
Shane Caraveo <scaraveo@mozilla.com>
2022-07-26 19:39:14 +0000

    Bug 1711168 support extension matching in webAccessibleResources r=zombie,smaug,rpl
    Differential Revision: https://phabricator.services.mozilla.com/D115114

--- toolkit/components/extensions/test/xpcshell/test_ext_web_accessible_resources_matches.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_web_accessible_resources_matches.js
@@ -1,4 +1,5 @@
 "use strict";
+
 Services.prefs.setBoolPref("extensions.manifestV3.enabled", true);
 
 const server = createHttpServer({ hosts: ["example.com", "example.org"] });
@@ -11,10 +12,77 @@ let image = atob(
 const IMAGE_ARRAYBUFFER = Uint8Array.from(image, byte => byte.charCodeAt(0))
   .buffer;
 
+add_task(async function test_web_accessible_resources_matching() {
+  let extension = await ExtensionTestUtils.loadExtension({
+    manifest: {
+      manifest_version: 3,
+      web_accessible_resources: [
+        {
+          resources: ["/accessible.html"],
+        },
+      ],
+    },
+  });
+
+  await Assert.rejects(
+    extension.startup(),
+    /web_accessible_resources requires one of "matches" or "extension_ids"/,
+    "web_accessible_resources object format incorrect"
+  );
+
+  extension = await ExtensionTestUtils.loadExtension({
+    manifest: {
+      manifest_version: 3,
+      web_accessible_resources: [
+        {
+          resources: ["/accessible.html"],
+          matches: ["http://example.com/data/*"],
+        },
+      ],
+    },
+  });
+
+  await extension.startup();
+  ok(true, "web_accessible_resources with matches loads");
+  await extension.unload();
+
+  extension = await ExtensionTestUtils.loadExtension({
+    manifest: {
+      manifest_version: 3,
+      web_accessible_resources: [
+        {
+          resources: ["/accessible.html"],
+          extension_ids: ["foo@mochitest"],
+        },
+      ],
+    },
+  });
+
+  await extension.startup();
+  ok(true, "web_accessible_resources with extensions loads");
+  await extension.unload();
+
+  extension = await ExtensionTestUtils.loadExtension({
+    manifest: {
+      manifest_version: 3,
+      web_accessible_resources: [
+        {
+          resources: ["/accessible.html"],
+          matches: ["http://example.com/data/*"],
+          extension_ids: ["foo@mochitest"],
+        },
+      ],
+    },
+  });
+
+  await extension.startup();
+  ok(true, "web_accessible_resources with matches and extensions loads");
+  await extension.unload();
+});
+
 add_task(async function test_web_accessible_resources() {
   async function contentScript() {
     let canLoad = window.location.href.startsWith("http://example.com");
-
     let urls = [
       {
         name: "iframe",
@@ -132,6 +200,181 @@ add_task(async function test_web_accessible_resources() {
   page = await ExtensionTestUtils.loadContentPage("http://example.org/data/");
 
   await extension.awaitMessage("complete");
+
+  await page.close();
+  await extension.unload();
+});
+
+async function pageScript() {
+  function test_element_src(data) {
+    return new Promise(resolve => {
+      let elem = document.createElement(data.elem);
+      let elemContext =
+        data.content_context && elem.wrappedJSObject
+          ? elem.wrappedJSObject
+          : elem;
+      elemContext.setAttribute("src", data.url);
+      elem.addEventListener(
+        "load",
+        () => {
+          browser.test.log(`got load event for ${data.url}`);
+          resolve(true);
+        },
+        { once: true }
+      );
+      elem.addEventListener(
+        "error",
+        () => {
+          browser.test.log(`got error event for ${data.url}`);
+          resolve(false);
+        },
+        { once: true }
+      );
+      document.body.appendChild(elem);
+    });
+  }
+  browser.test.onMessage.addListener(async msg => {
+    browser.test.log(`testing ${JSON.stringify(msg)}`);
+    let loaded = await test_element_src(msg);
+    browser.test.assertEq(loaded, msg.shouldLoad, `${msg.name} loaded`);
+    browser.test.sendMessage("web-accessible-resources");
+  });
+  browser.test.sendMessage("page-loaded");
+}
+
+add_task(async function test_web_accessible_resources_extensions() {
+  let other = ExtensionTestUtils.loadExtension({
+    manifest: {
+      applications: { gecko: { id: "other@mochitest" } },
+    },
+    files: {
+      "page.js": pageScript,
+
+      "page.html": `<html><head>
+          <meta charset="utf-8">
+          <script src="page.js"></script>
+        </head></html>`,
+    },
+  });
+
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      manifest_version: 3,
+      applications: { gecko: { id: "this@mochitest" } },
+      web_accessible_resources: [
+        {
+          resources: ["/image.png"],
+          extension_ids: ["other@mochitest"],
+        },
+      ],
+    },
+
+    files: {
+      "image.png": IMAGE_ARRAYBUFFER,
+      "inaccessible.png": IMAGE_ARRAYBUFFER,
+      "page.js": pageScript,
+
+      "page.html": `<html><head>
+          <meta charset="utf-8">
+          <script src="page.js"></script>
+        </head></html>`,
+    },
+  });
+
+  await extension.startup();
+  let extensionUrl = `moz-extension://${extension.uuid}/`;
+
+  await other.startup();
+  let pageUrl = `moz-extension://${other.uuid}/page.html`;
+
+  let page = await ExtensionTestUtils.loadContentPage(pageUrl);
+  await other.awaitMessage("page-loaded");
+
+  other.sendMessage({
+    name: "accessible resource",
+    elem: "img",
+    url: `${extensionUrl}image.png`,
+    shouldLoad: true,
+  });
+  await other.awaitMessage("web-accessible-resources");
+
+  other.sendMessage({
+    name: "inaccessible resource",
+    elem: "img",
+    url: `${extensionUrl}inaccessible.png`,
+    shouldLoad: false,
+  });
+  await other.awaitMessage("web-accessible-resources");
+
+  await page.close();
+
+  // test that the extension may load it's own web accessible resource
+  page = await ExtensionTestUtils.loadContentPage(`${extensionUrl}page.html`);
+  await extension.awaitMessage("page-loaded");
+
+  extension.sendMessage({
+    name: "accessible resource",
+    elem: "img",
+    url: `${extensionUrl}image.png`,
+    shouldLoad: true,
+  });
+  await extension.awaitMessage("web-accessible-resources");
+
+  await page.close();
+  await extension.unload();
+  await other.unload();
+});
+
+// test that a web page not in matches cannot load the resource
+add_task(async function test_web_accessible_resources_inaccessible() {
+  let extension = ExtensionTestUtils.loadExtension({
+    temporarilyInstalled: true,
+    manifest: {
+      manifest_version: 3,
+      applications: { gecko: { id: "web@mochitest" } },
+      content_scripts: [
+        {
+          matches: ["http://example.com/data/*"],
+          js: ["page.js"],
+          run_at: "document_idle",
+        },
+      ],
+      web_accessible_resources: [
+        {
+          resources: ["/image.png"],
+          extension_ids: ["some_other_ext@mochitest"],
+        },
+      ],
+      host_permissions: ["*://example.com/*"],
+      granted_host_permissions: true,
+    },
+
+    files: {
+      "image.png": IMAGE_ARRAYBUFFER,
+      "page.js": pageScript,
+
+      "page.html": `<html><head>
+          <meta charset="utf-8">
+          <script src="page.js"></script>
+        </head></html>`,
+    },
+  });
+
+  await extension.startup();
+  let extensionUrl = `moz-extension://${extension.uuid}/`;
+  let page = await ExtensionTestUtils.loadContentPage(
+    "http://example.com/data/"
+  );
+  await extension.awaitMessage("page-loaded");
+
+  extension.sendMessage({
+    name: "cannot access resource",
+    elem: "img",
+    url: `${extensionUrl}image.png`,
+    content_context: true,
+    shouldLoad: false,
+  });
+  await extension.awaitMessage("web-accessible-resources");
 
   await page.close();
   await extension.unload();
