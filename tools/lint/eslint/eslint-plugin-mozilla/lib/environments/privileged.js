d1d74242efa62c455d9741e642681cdef02af207
Tooru Fujisawa <arai_a@mac.com>
2022-07-26 02:46:32 +0000

    Bug 1780543 - Part 11: Update document for environment. r=Standard8
    Depends on D152648
    
    Differential Revision: https://phabricator.services.mozilla.com/D152649

--- tools/lint/eslint/eslint-plugin-mozilla/lib/environments/privileged.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/environments/privileged.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview Defines the environment for jsm files.
+ * @fileoverview Defines the environment for privileges JS files.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
