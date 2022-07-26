504df3e923c028b3237206407f38d7b91e7f2b94
Shane Caraveo <scaraveo@mozilla.com>
2022-07-26 19:39:14 +0000

    Bug 1711168 allow extension pages to be loaded as top level tabs by other extensions r=rpl
    Ensure extensions can manage tabs and sessions that include tabs from other extensions.  The parent
    patch to this introduces cross-extension access validation.  However that breaks this specific use case
    that we need to continue supporting.  This patch modifies three extension apis, tab.create/update and
    windows.create to allow the creation of extension tabs which cannot be otherwise accessed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D151766

--- browser/components/extensions/parent/ext-tabs.js
+++ browser/components/extensions/parent/ext-tabs.js
@@ -611,6 +611,24 @@ this.tabs = class extends ExtensionAPIPersistent {
       return tab;
     }
 
+    function setContentTriggeringPrincipal(url, browser, options) {
+      // For urls that we want to allow an extension to open in a tab, but
+      // that it may not otherwise have access to, we set the triggering
+      // principal to the url that is being opened.  This is used for newtab,
+      // about: and moz-extension: protocols.
+      // We also prevent discarded, or lazy tabs by setting allowInheritPrincipal to false.
+      options.allowInheritPrincipal = false;
+      options.triggeringPrincipal = Services.scriptSecurityManager.createContentPrincipal(
+        Services.io.newURI(url),
+        {
+          userContextId: options.userContextId,
+          privateBrowsingId: PrivateBrowsingUtils.isBrowserPrivate(browser)
+            ? 1
+            : 0,
+        }
+      );
+    }
+
     let tabsApi = {
       tabs: {
         onActivated: new EventManager({
@@ -706,9 +724,11 @@ this.tabs = class extends ExtensionAPIPersistent {
             }
           }).then(window => {
             let url;
-            let principal = context.principal;
 
-            let options = {};
+            let options = {
+              allowInheritPrincipal: true,
+              triggeringPrincipal: context.principal,
+            };
             if (createProperties.cookieStoreId) {
               // May throw if validation fails.
               options.userContextId = getUserContextIdForCookieStoreId(
@@ -721,7 +741,10 @@ this.tabs = class extends ExtensionAPIPersistent {
             if (createProperties.url !== null) {
               url = context.uri.resolve(createProperties.url);
 
-              if (!context.checkLoadURL(url, { dontReportErrors: true })) {
+              if (
+                !url.startsWith("moz-extension://") &&
+                !context.checkLoadURL(url, { dontReportErrors: true })
+              ) {
                 return Promise.reject({ message: `Illegal URL: ${url}` });
               }
 
@@ -731,30 +754,10 @@ this.tabs = class extends ExtensionAPIPersistent {
             } else {
               url = window.BROWSER_NEW_TAB_URL;
             }
-            // Only set allowInheritPrincipal on discardable urls as it
-            // will override creating a lazy browser.  Setting triggeringPrincipal
-            // will ensure other cases are handled, but setting it may prevent
-            // creating about and data urls.
             let discardable = url && !url.startsWith("about:");
-            if (!discardable) {
-              // Make sure things like about:blank and data: URIs never inherit,
-              // and instead always get a NullPrincipal.
-              options.allowInheritPrincipal = false;
-              // Falling back to content here as about: requires it, however is safe.
-              principal = Services.scriptSecurityManager.createContentPrincipal(
-                Services.io.newURI(url),
-                {
-                  userContextId: options.userContextId,
-                  privateBrowsingId: PrivateBrowsingUtils.isBrowserPrivate(
-                    window.gBrowser
-                  )
-                    ? 1
-                    : 0,
-                }
-              );
-            } else {
-              options.allowInheritPrincipal = true;
-              options.triggeringPrincipal = context.principal;
+            // Handle moz-ext separately from the discardable flag to retain prior behavior.
+            if (!discardable || url?.startsWith("moz-extension://")) {
+              setContentTriggeringPrincipal(url, window.gBrowser, options);
             }
 
             tabListener.initTabReady();
@@ -814,7 +817,6 @@ this.tabs = class extends ExtensionAPIPersistent {
               });
             }
 
-            options.triggeringPrincipal = principal;
             let nativeTab = window.gBrowser.addTab(url, options);
 
             if (active) {
@@ -888,16 +890,21 @@ this.tabs = class extends ExtensionAPIPersistent {
           if (updateProperties.url !== null) {
             let url = context.uri.resolve(updateProperties.url);
 
-            if (!context.checkLoadURL(url, { dontReportErrors: true })) {
-              return Promise.reject({ message: `Illegal URL: ${url}` });
-            }
-
             let options = {
               flags: updateProperties.loadReplace
                 ? Ci.nsIWebNavigation.LOAD_FLAGS_REPLACE_HISTORY
                 : Ci.nsIWebNavigation.LOAD_FLAGS_NONE,
               triggeringPrincipal: context.principal,
             };
+
+            if (!context.checkLoadURL(url, { dontReportErrors: true })) {
+              // We allow loading top level tabs for "other" extensions.
+              if (url.startsWith("moz-extension://")) {
+                setContentTriggeringPrincipal(url, tabbrowser, options);
+              } else {
+                return Promise.reject({ message: `Illegal URL: ${url}` });
+              }
+            }
 
             let browser = nativeTab.linkedBrowser;
             if (nativeTab.linkedPanel) {
--- browser/components/extensions/test/browser/browser_ext_windows_create_url.js
+++ browser/components/extensions/test/browser/browser_ext_windows_create_url.js
@@ -3,112 +3,171 @@
 "use strict";
 
 add_task(async function testWindowCreate() {
+  let pageExt = ExtensionTestUtils.loadExtension({
+    manifest: {
+      applications: { gecko: { id: "page@mochitest" } },
+      protocol_handlers: [
+        {
+          protocol: "ext+foo",
+          name: "a foo protocol handler",
+          uriTemplate: "page.html?val=%s",
+        },
+      ],
+    },
+    files: {
+      "page.html": `<html><head>
+          <meta charset="utf-8">
+        </head></html>`,
+    },
+  });
+  await pageExt.startup();
+
+  async function background(OTHER_PAGE) {
+    browser.test.log(`== using ${OTHER_PAGE}`);
+    const EXTENSION_URL = browser.runtime.getURL("test.html");
+    const EXT_PROTO = "ext+bar:foo";
+    const OTHER_PROTO = "ext+foo:bar";
+
+    let windows = new (class extends Map {
+      // eslint-disable-line new-parens
+      get(id) {
+        if (!this.has(id)) {
+          let window = {
+            tabs: new Map(),
+          };
+          window.promise = new Promise(resolve => {
+            window.resolvePromise = resolve;
+          });
+
+          this.set(id, window);
+        }
+
+        return super.get(id);
+      }
+    })();
+
+    browser.tabs.onUpdated.addListener((tabId, changed, tab) => {
+      if (changed.status == "complete" && tab.url !== "about:blank") {
+        let window = windows.get(tab.windowId);
+        window.tabs.set(tab.index, tab);
+
+        if (window.tabs.size === window.expectedTabs) {
+          browser.test.log("resolving a window load");
+          window.resolvePromise(window);
+        }
+      }
+    });
+
+    async function create(options) {
+      browser.test.log(`creating window for ${options.url}`);
+      let window = await browser.windows.create(options);
+      let win = windows.get(window.id);
+      win.id = window.id;
+
+      win.expectedTabs = Array.isArray(options.url) ? options.url.length : 1;
+
+      return win.promise;
+    }
+
+    function createFail(options) {
+      return browser.windows
+        .create(options)
+        .then(() => {
+          browser.test.fail(`window opened with ${options.url}`);
+        })
+        .catch(() => {
+          browser.test.succeed(`window could not open with ${options.url}`);
+        });
+    }
+
+    let TEST_SETS = [
+      {
+        name: "Single protocol URL in this extension",
+        url: EXT_PROTO,
+        expect: [`${EXTENSION_URL}?val=ext%2Bbar%3Afoo`],
+      },
+      {
+        name: "Single, relative URL",
+        url: "test.html",
+        expect: [EXTENSION_URL],
+      },
+      {
+        name: "Single, absolute, extension URL",
+        url: EXTENSION_URL,
+        expect: [EXTENSION_URL],
+      },
+      {
+        name: "Single, absolute, other extension URL",
+        url: OTHER_PAGE,
+        expect: [OTHER_PAGE],
+      },
+      {
+        name: "Single protocol URL in other extension",
+        url: OTHER_PROTO,
+        expect: [`${OTHER_PAGE}?val=ext%2Bfoo%3Abar`],
+      },
+      {
+        name: "multiple urls",
+        url: [EXT_PROTO, "test.html", EXTENSION_URL, OTHER_PROTO],
+        expect: [
+          `${EXTENSION_URL}?val=ext%2Bbar%3Afoo`,
+          EXTENSION_URL,
+          EXTENSION_URL,
+          `${OTHER_PAGE}?val=ext%2Bfoo%3Abar`,
+        ],
+      },
+    ];
+    try {
+      let windows = await Promise.all(
+        TEST_SETS.map(t => create({ url: t.url }))
+      );
+
+      TEST_SETS.forEach((test, i) => {
+        test.expect.forEach((expectUrl, x) => {
+          browser.test.assertEq(
+            expectUrl,
+            windows[i].tabs.get(x)?.url,
+            TEST_SETS[i].name
+          );
+        });
+      });
+
+      Promise.all(windows.map(({ id }) => browser.windows.remove(id))).then(
+        () => {
+          browser.test.notifyPass("window-create-url");
+        }
+      );
+
+      // Expecting to fail when opening windows with multiple urls that includes
+      // other extension urls.
+      await Promise.all([createFail({ url: [EXTENSION_URL, OTHER_PAGE] })]);
+    } catch (e) {
+      browser.test.fail(`${e} :: ${e.stack}`);
+      browser.test.notifyFail("window-create-url");
+    }
+  }
+
   let extension = ExtensionTestUtils.loadExtension({
     manifest: {
       permissions: ["tabs"],
+      protocol_handlers: [
+        {
+          protocol: "ext+bar",
+          name: "a bar protocol handler",
+          uriTemplate: "test.html?val=%s",
+        },
+      ],
     },
 
-    background: async function() {
-      const EXTENSION_URL = browser.runtime.getURL("test.html");
-      const REMOTE_URL = browser.runtime.getURL("test.html");
-
-      let windows = new (class extends Map {
-        // eslint-disable-line new-parens
-        get(id) {
-          if (!this.has(id)) {
-            let window = {
-              tabs: new Map(),
-            };
-            window.promise = new Promise(resolve => {
-              window.resolvePromise = resolve;
-            });
-
-            this.set(id, window);
-          }
-
-          return super.get(id);
-        }
-      })();
-
-      browser.tabs.onUpdated.addListener((tabId, changed, tab) => {
-        if (changed.status == "complete" && tab.url !== "about:blank") {
-          let window = windows.get(tab.windowId);
-          window.tabs.set(tab.index, tab);
-
-          if (window.tabs.size === window.expectedTabs) {
-            window.resolvePromise(window);
-          }
-        }
-      });
-
-      async function create(options) {
-        let window = await browser.windows.create(options);
-        let win = windows.get(window.id);
-        win.id = window.id;
-
-        win.expectedTabs = Array.isArray(options.url) ? options.url.length : 1;
-
-        return win.promise;
-      }
-
-      try {
-        let windows = await Promise.all([
-          create({ url: REMOTE_URL }),
-          create({ url: "test.html" }),
-          create({ url: EXTENSION_URL }),
-          create({ url: [REMOTE_URL, "test.html", EXTENSION_URL] }),
-        ]);
-        browser.test.assertEq(
-          REMOTE_URL,
-          windows[0].tabs.get(0).url,
-          "Single, absolute, remote URL"
-        );
-
-        browser.test.assertEq(
-          REMOTE_URL,
-          windows[1].tabs.get(0).url,
-          "Single, relative URL"
-        );
-
-        browser.test.assertEq(
-          REMOTE_URL,
-          windows[2].tabs.get(0).url,
-          "Single, absolute, extension URL"
-        );
-
-        browser.test.assertEq(
-          REMOTE_URL,
-          windows[3].tabs.get(0).url,
-          "url[0]: Absolute, remote URL"
-        );
-        browser.test.assertEq(
-          EXTENSION_URL,
-          windows[3].tabs.get(1).url,
-          "url[1]: Relative URL"
-        );
-        browser.test.assertEq(
-          EXTENSION_URL,
-          windows[3].tabs.get(2).url,
-          "url[2]: Absolute, extension URL"
-        );
-
-        Promise.all(windows.map(({ id }) => browser.windows.remove(id))).then(
-          () => {
-            browser.test.notifyPass("window-create-url");
-          }
-        );
-      } catch (e) {
-        browser.test.fail(`${e} :: ${e.stack}`);
-        browser.test.notifyFail("window-create-url");
-      }
-    },
+    background: `(${background})("moz-extension://${pageExt.uuid}/page.html")`,
 
     files: {
-      "test.html": `<DOCTYPE html><html><head><meta charset="utf-8"></head></html>`,
+      "test.html": `<!DOCTYPE html><html><head><meta charset="utf-8"></head><body></body></html>`,
     },
   });
 
   await extension.startup();
   await extension.awaitFinish("window-create-url");
   await extension.unload();
+  await pageExt.unload();
 });
