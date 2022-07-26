b3d2bbce2788ffbe8b5a1e55afde5f9b434da0e9
Tooru Fujisawa <arai_a@mac.com>
2022-07-26 02:46:28 +0000

    Bug 1780543 - Part 1: Use eslint-env mozilla/frame-script consistently in frame script. r=Standard8,webdriver-reviewers,geckoview-reviewers,jdescottes,calu
    Differential Revision: https://phabricator.services.mozilla.com/D152426

--- remote/cdp/sessions/frame-script.js
+++ remote/cdp/sessions/frame-script.js
@@ -2,11 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* eslint-env mozilla/frame-script */
+
 "use strict";
 
 const { ContentProcessSession } = ChromeUtils.import(
   "chrome://remote/content/cdp/sessions/ContentProcessSession.jsm"
 );
 
-/* global content, docShell */
 new ContentProcessSession(this, docShell.browsingContext, content, docShell);
