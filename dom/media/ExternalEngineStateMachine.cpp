0e10ee707d3679a78dcd6cd33c1f112c02ae568f
alwu <alwu@mozilla.com>
2022-07-26 05:20:10 +0000

    Bug 1778038 - part2 : reply engine data request after finishing reader seek. r=jolin
    We should request new data after finishing seeking on the reader,
    otherwise it would send incorrect data to the remote media engine.
    
    Differential Revision: https://phabricator.services.mozilla.com/D151584

--- dom/media/ExternalEngineStateMachine.cpp
+++ dom/media/ExternalEngineStateMachine.cpp
@@ -912,11 +912,15 @@ void ExternalEngineStateMachine::NotifyEventInternal(
       break;
     case ExternalEngineEvent::RequestForAudio:
       mHasEnoughAudio = false;
-      RunningEngineUpdate(MediaData::Type::AUDIO_DATA);
+      if (ShouldRunEngineUpdateForRequest()) {
+        RunningEngineUpdate(MediaData::Type::AUDIO_DATA);
+      }
       break;
     case ExternalEngineEvent::RequestForVideo:
       mHasEnoughVideo = false;
-      RunningEngineUpdate(MediaData::Type::VIDEO_DATA);
+      if (ShouldRunEngineUpdateForRequest()) {
+        RunningEngineUpdate(MediaData::Type::VIDEO_DATA);
+      }
       break;
     case ExternalEngineEvent::AudioEnough:
       mHasEnoughAudio = true;
@@ -928,6 +932,16 @@ void ExternalEngineStateMachine::NotifyEventInternal(
       MOZ_ASSERT_UNREACHABLE("Undefined event!");
       break;
   }
+}
+
+bool ExternalEngineStateMachine::ShouldRunEngineUpdateForRequest() {
+  // Running engine update will request new data, which could be run on
+  // `RunningEngine` or `SeekingData` state. However, in `SeekingData` we should
+  // only request new data after finishing reader seek, otherwise the reader
+  // would start requesting data from a wrong position.
+  return mState.IsRunningEngine() ||
+         (mState.AsSeekingData() &&
+          !mState.AsSeekingData()->mWaitingReaderSeeked);
 }
 
 void ExternalEngineStateMachine::NotifyErrorInternal(
