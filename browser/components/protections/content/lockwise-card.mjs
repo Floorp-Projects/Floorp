8ada4a4369dda2fa787d72c30ecc322cc6de254b
Tooru Fujisawa <arai_a@mac.com>
2022-07-26 02:46:30 +0000

    Bug 1780543 - Part 4: Add mozilla/remote-page environment. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D152429

--- browser/components/protections/content/lockwise-card.mjs
+++ browser/components/protections/content/lockwise-card.mjs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* eslint-env mozilla/frame-script */
+/* eslint-env mozilla/remote-page */
 
 const HOW_IT_WORKS_URL_PREF = RPMGetFormatURLPref(
   "browser.contentblocking.report.lockwise.how_it_works.url"
