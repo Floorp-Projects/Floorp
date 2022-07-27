7180b02c1bc509600470b01da3d22efff64127dc
Jed Davis <jld@mozilla.com>
2022-07-27 19:41:05 +0000

    Bug 1779312 - Replace uses of strerror in Linux sandbox code. r=glandium
    `strerror` is async signal unsafe, and we're using it in contexts where
    that's a problem: in particular in the child process after `clone()`ing,
    where it can deadlock if it takes locks the parents' other threads had
    held (or cause other undefined behavior), but also in the SIGSYS handler
    if it's nested inside an async signal.  It's also thread-unsafe.
    
    This is mostly a mechanical replacement with the new `SANDBOX_LOG_ERRNO`
    or `SANDBOX_LOG_WITH_ERROR`; two messages had the error string in the
    middle and have been adjusted.
    
    Differential Revision: https://phabricator.services.mozilla.com/D152099

--- security/sandbox/linux/reporter/SandboxReporter.cpp
+++ security/sandbox/linux/reporter/SandboxReporter.cpp
@@ -50,14 +50,14 @@ bool SandboxReporter::Init() {
   int fds[2];
 
   if (0 != socketpair(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0, fds)) {
-    SANDBOX_LOG("SandboxReporter: socketpair failed: %s", strerror(errno));
+    SANDBOX_LOG_ERRNO("SandboxReporter: socketpair failed");
     return false;
   }
   mClientFd = fds[0];
   mServerFd = fds[1];
 
   if (!PlatformThread::Create(0, this, &mThread)) {
-    SANDBOX_LOG("SandboxReporter: thread creation failed: %s", strerror(errno));
+    SANDBOX_LOG_ERRNO("SandboxReporter: thread creation failed");
     close(mClientFd);
     close(mServerFd);
     mClientFd = mServerFd = -1;
@@ -259,7 +259,7 @@ void SandboxReporter::ThreadMain(void) {
       if (errno == EINTR) {
         continue;
       }
-      SANDBOX_LOG("SandboxReporter: recvmsg: %s", strerror(errno));
+      SANDBOX_LOG_ERRNO("SandboxReporter: recvmsg");
     }
     if (recvd <= 0) {
       break;
