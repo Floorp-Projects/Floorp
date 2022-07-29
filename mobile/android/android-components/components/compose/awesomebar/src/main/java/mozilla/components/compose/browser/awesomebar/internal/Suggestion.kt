047aac87dfab3ff79eeb42ca9f8f9452530ff298
Roger Yang <royang@mozilla.com>
2022-07-29 13:32:11 -0400

    [components] No issue: Ensure suggestion icons are cropped correctly

--- mobile/android/android-components/components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/internal/Suggestion.kt
+++ mobile/android/android-components/components/compose/awesomebar/src/main/java/mozilla/components/compose/browser/awesomebar/internal/Suggestion.kt
@@ -17,13 +17,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -135,8 +138,10 @@ private fun SuggestionIcon(
             contentDescription = null,
             modifier = Modifier
                 .padding(top = 8.dp)
+                .clip(RoundedCornerShape(2.dp))
                 .width(24.dp)
-                .height(24.dp)
+                .height(24.dp),
+            contentScale = ContentScale.Crop,
         )
         if (indicator != null) {
             Image(
