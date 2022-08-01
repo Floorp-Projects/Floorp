fcc69dc1822918a2b2288b1ac2228f0ffdbb6ddb
Ting-Yu Lin <tlin@mozilla.com>
2022-08-01 23:01:14 +0000

    Bug 1782602 - No need to consider margin when computing -moz-column-content's origin. r=emilio
    `-moz-column-content` is the anonymous frame under column set, and it has no
    margin per its style in ua.css.
    
    Differential Revision: https://phabricator.services.mozilla.com/D153404

--- layout/generic/nsColumnSetFrame.cpp
+++ layout/generic/nsColumnSetFrame.cpp
@@ -678,13 +678,12 @@ nsColumnSetFrame::ColumnBalanceData nsColumnSetFrame::ReflowChildren(
       // really big float in the first column and text in following
       // columns would flow around it.
 
-      // Reflow the frame
-      const auto childMargin = kidReflowInput.ComputedLogicalMargin(wm);
-      const LogicalPoint origin(wm, childOrigin.I(wm) + childMargin.IStart(wm),
-                                childOrigin.B(wm) + childMargin.BStart(wm));
+      MOZ_ASSERT(kidReflowInput.ComputedLogicalMargin(wm).IsAllZero(),
+                 "-moz-column-content has no margin!");
       aStatus.Reset();
       ReflowChild(child, PresContext(), kidDesiredSize, kidReflowInput, wm,
-                  origin, containerSize, ReflowChildFlags::Default, aStatus);
+                  childOrigin, containerSize, ReflowChildFlags::Default,
+                  aStatus);
 
       reflowNext = aStatus.NextInFlowNeedsReflow();
 
