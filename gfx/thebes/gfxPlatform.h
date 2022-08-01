cdd592888efb38f565425e195409f3c94693314e
Kelsey Gilbert <kelsey.gilbert@mozilla.com>
2022-08-01 19:49:27 +0000

    Bug 1771374 - Disable color management based on pref in DCLayerTree. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D150276

--- gfx/thebes/gfxPlatform.h
+++ gfx/thebes/gfxPlatform.h
@@ -1030,4 +1030,6 @@ class gfxPlatform : public mozilla::layers::MemoryPressureListener {
   const gfxSkipChars kEmptySkipChars;
 };
 
+CMSMode GfxColorManagementMode();
+
 #endif /* GFX_PLATFORM_H */
