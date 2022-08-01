6fb2bc28595c7704b230c3e068d3917824fd8862
Paul Zuehlcke <pbz@mozilla.com>
2022-08-01 13:40:41 +0000

    Bug 1781982 - Remove privacy.dfpi_rollout_enabledByDefault scalar. r=anti-tracking-reviewers,timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D153343

--- browser/base/content/test/protectionsUI/browser_protectionsUI_dfpi_rollout.js
+++ browser/base/content/test/protectionsUI/browser_protectionsUI_dfpi_rollout.js
@@ -25,26 +25,6 @@ function cleanup() {
 
   BrowserGlue._defaultCookieBehaviorAtStartup = previousDefaultCB;
   defaultPrefs.setIntPref(COOKIE_BEHAVIOR_PREF, previousDefaultCB);
-
-  // Reset the rollout scalar back to 2 = unset. We have to simulate this on
-  // test cleanup, because BrowserGlue only sets this once initially.
-  Services.telemetry.scalarSet("privacy.dfpi_rollout_enabledByDefault", 2);
-}
-
-function testTelemetryState(optIn) {
-  let expectedValue;
-  if (optIn == null) {
-    expectedValue = 2;
-  } else {
-    expectedValue = optIn ? 1 : 0;
-  }
-
-  TelemetryTestUtils.assertScalar(
-    TelemetryTestUtils.getProcessScalars("parent"),
-    "privacy.dfpi_rollout_enabledByDefault",
-    expectedValue,
-    "Scalar should have correct value"
-  );
 }
 
 // Tests that the dFPI rollout pref updates the default cookieBehavior to 5,
@@ -60,36 +40,29 @@ add_task(async function testdFPIRolloutPref() {
     Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER
   );
 
-  // Test the unset state of the pref.
-  testTelemetryState(null);
-
   Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, false);
   is(
     defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
     Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER
   );
-  testTelemetryState(false);
 
   Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, true);
   is(
     defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
     Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN
   );
-  testTelemetryState(true);
 
   Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, false);
   is(
     defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
     Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER
   );
-  testTelemetryState(false);
 
   Services.prefs.setBoolPref(PREF_DFPI_ENABLED_BY_DEFAULT, true);
   is(
     defaultPrefs.getIntPref(COOKIE_BEHAVIOR_PREF),
     Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN
   );
-  testTelemetryState(true);
 
   cleanup();
 });
