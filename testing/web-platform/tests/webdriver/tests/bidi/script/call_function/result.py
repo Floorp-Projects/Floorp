638b5e29ebd2e2fad56e3c727bf347679d6ac6d8
Julian Descottes <jdescottes@mozilla.com>
2022-08-02 10:31:10 +0000

    Bug 1780836 [wpt PR 34940] - Create dedicated tests for script.callFunction awaitPromise, a=testonly
    Automatic update from web-platform-tests
    Create dedicated tests for script.callFunction awaitPromise
    
    --
    
    wpt-commits: f946202a7a59fe703dbead2eed5b77b68c26e3f6
    wpt-pr: 34940

--- testing/web-platform/tests/webdriver/tests/bidi/script/call_function/result.py
+++ testing/web-platform/tests/webdriver/tests/bidi/script/call_function/result.py
@@ -2,8 +2,9 @@ import pytest
 
 from webdriver.bidi.modules.script import ContextTarget
 
+pytestmark = pytest.mark.asyncio
 
-@pytest.mark.asyncio
+@pytest.mark.parametrize("await_promise", [True, False])
 @pytest.mark.parametrize(
     "expression, expected",
     [
@@ -22,17 +23,21 @@ from webdriver.bidi.modules.script import ContextTarget
         ("42n", {"type": "bigint", "value": "42"}),
     ],
 )
-async def test_primitive_values(bidi_session, top_context, expression, expected):
+async def test_primitive_values(bidi_session, top_context, await_promise, expression, expected):
+    function_declaration = f"()=>{expression}"
+    if await_promise:
+        function_declaration = "async" + function_declaration
+
     result = await bidi_session.script.call_function(
-        function_declaration=f"() => {expression}",
-        await_promise=False,
+        function_declaration=function_declaration,
+        await_promise=await_promise,
         target=ContextTarget(top_context["context"]),
     )
 
     assert result == expected
 
 
-@pytest.mark.asyncio
+@pytest.mark.parametrize("await_promise", [True, False])
 @pytest.mark.parametrize(
     "expression, expected",
     [
@@ -115,7 +120,6 @@ async def test_primitive_values(bidi_session, top_context, expression, expected)
         # TODO(sadym): add `iterator` test.
         # TODO(sadym): add `generator` test.
         # TODO(sadym): add `proxy` test.
-        ("Promise.resolve()", {"type": "promise"}),
         ("new Int32Array()", {"type": "typedarray"}),
         ("new ArrayBuffer()", {"type": "arraybuffer"}),
         (
@@ -137,11 +141,35 @@ async def test_primitive_values(bidi_session, top_context, expression, expected)
         ("window", {"type": "window"}),
     ],
 )
-async def test_remote_values(bidi_session, top_context, expression, expected):
+async def test_remote_values(bidi_session, top_context, await_promise, expression, expected):
+    function_declaration = f"()=>{expression}"
+    if await_promise:
+        function_declaration = "async" + function_declaration
+
     result = await bidi_session.script.call_function(
-        function_declaration=f"() => {expression}",
-        await_promise=False,
+        function_declaration=function_declaration,
+        await_promise=await_promise,
         target=ContextTarget(top_context["context"]),
     )
 
     assert result == expected
+
+
+async def test_remote_value_promise_await(bidi_session, top_context):
+    result = await bidi_session.script.call_function(
+        function_declaration="()=>Promise.resolve(42)",
+        await_promise=True,
+        target=ContextTarget(top_context["context"]),
+    )
+
+    assert result == {"type": "number", "value": 42}
+
+
+async def test_remote_value_promise_no_await(bidi_session, top_context):
+    result = await bidi_session.script.call_function(
+        function_declaration="()=>Promise.resolve(42)",
+        await_promise=False,
+        target=ContextTarget(top_context["context"]),
+    )
+
+    assert result == {"type": "promise"}
