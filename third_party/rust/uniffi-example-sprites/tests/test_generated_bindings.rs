99aeaa01aa5bf852672c4c382f3d84864facf9b7
criss <ccozmuta@mozilla.com>
2022-08-03 00:01:58 +0300

    Backed out 2 changesets (bug 1766045) for causing Bp-hybrid failures on UniFFIGeneratedScaffolding.cpp
    Backed out changeset 5708672d0e83 (bug 1766045)
    Backed out changeset cc77fd5ec7a8 (bug 1766045)

--- third_party/rust/uniffi-example-sprites/tests/test_generated_bindings.rs
+++
@@ -1,9 +0,0 @@
-uniffi_macros::build_foreign_language_testcases!(
-    ["src/sprites.udl",],
-    [
-        "tests/bindings/test_sprites.py",
-        "tests/bindings/test_sprites.rb",
-        "tests/bindings/test_sprites.kts",
-        "tests/bindings/test_sprites.swift",
-    ]
-);
--- third_party/rust/uniffi-example-todolist/tests/test_generated_bindings.rs
+++
@@ -1,9 +0,0 @@
-uniffi_macros::build_foreign_language_testcases!(
-    ["src/todolist.udl",],
-    [
-        "tests/bindings/test_todolist.kts",
-        "tests/bindings/test_todolist.swift",
-        "tests/bindings/test_todolist.rb",
-        "tests/bindings/test_todolist.py"
-    ]
-);
