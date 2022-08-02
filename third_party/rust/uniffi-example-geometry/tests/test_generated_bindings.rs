f27248d28df8cb52b5adb085a4911d59c9f06c42
criss <ccozmuta@mozilla.com>
2022-08-03 00:01:58 +0300

    Backed out 2 changesets (bug 1766045) for causing Bp-hybrid failures on UniFFIGeneratedScaffolding.cpp
    Backed out changeset 5708672d0e83 (bug 1766045)
    Backed out changeset cc77fd5ec7a8 (bug 1766045)

--- third_party/rust/uniffi-example-geometry/tests/test_generated_bindings.rs
+++
@@ -1,9 +0,0 @@
-uniffi_macros::build_foreign_language_testcases!(
-    ["src/geometry.udl",],
-    [
-        "tests/bindings/test_geometry.py",
-        "tests/bindings/test_geometry.rb",
-        "tests/bindings/test_geometry.kts",
-        "tests/bindings/test_geometry.swift",
-    ]
-);
