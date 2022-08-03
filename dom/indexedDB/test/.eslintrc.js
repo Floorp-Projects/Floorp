783d9855a66a78efcbb80892baa09d9c34deff57
Mark Banner <standard8@mozilla.com>
2022-08-03 11:16:20 +0000

    Bug 1782008 - Remove now unnecessary ESLint test definitions from other .eslintrc.js files. r=mossop,media-playback-reviewers,alwu
    Differential Revision: https://phabricator.services.mozilla.com/D153216

--- dom/indexedDB/test/.eslintrc.js
+++ dom/indexedDB/test/.eslintrc.js
@@ -1,12 +1,6 @@
 "use strict";
 
 module.exports = {
-  extends: [
-    "plugin:mozilla/browser-test",
-    "plugin:mozilla/chrome-test",
-    "plugin:mozilla/mochitest-test",
-  ],
-  plugins: ["mozilla"],
   rules: {
     "no-shadow": "off",
   },
