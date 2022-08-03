1e3410cbddf8670b0189be58ebb3fe2281d96653
Mark Banner <standard8@mozilla.com>
2022-08-03 11:16:20 +0000

    Bug 1782008 - Remove now unnecessary .eslintrc.js files. r=webcompat-reviewers,extension-reviewers,media-playback-reviewers,pip-reviewers,denschub,rpl,alwu,mossop
    Differential Revision: https://phabricator.services.mozilla.com/D152736

--- toolkit/mozapps/update/tests/data/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-// The files in this directory are shared by mochitest-browser-chrome,
-// mochitest-chrome, and xpcshell tests. At this time it is simpler to just use
-// plugin:mozilla/xpcshell-test for eslint.
-
-"use strict";
-
-module.exports = {
-  extends: ["plugin:mozilla/xpcshell-test"],
-};
