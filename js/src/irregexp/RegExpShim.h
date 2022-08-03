b009be8b930b847cd54dde8056da39b4017d666e
Iain Ireland <iireland@mozilla.com>
2022-08-03 18:06:50 +0000

    Bug 1779849: Update RegExpStack code r=mgaudet
    While refactoring this code, upstream V8 removed their own need to have `Isolate` as a friend class of `RegExpStack`. We need to be able to reach inside RegExpStack for a couple of reasons, so we use `ExternalReference`, which is still a friend class (to support ExternalReference::TopOfRegExpStack, which is used in V8's macroassembler code).
    
    Since ExternalReference already exists, I'm also using it for the memory accounting code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D152907

--- js/src/irregexp/RegExpShim.h
+++ js/src/irregexp/RegExpShim.h
@@ -1036,7 +1036,6 @@ class Isolate {
 
   //********** Isolate code **********//
   RegExpStack* regexp_stack() const { return regexpStack_; }
-  byte* top_of_regexp_stack() const;
 
   // This is called from inside no-GC code. Instead of suppressing GC
   // to allocate the error, we return false from Execute and call
@@ -1166,6 +1165,13 @@ class StackLimitCheck {
  private:
   JSContext* cx_;
   js::AutoCheckRecursionLimit recursion_;
+};
+
+class ExternalReference {
+ public:
+  static const void* TopOfRegexpStack(Isolate* isolate);
+  static size_t SizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf,
+                                    RegExpStack* regexpStack);
 };
 
 class Code : public HeapObject {
