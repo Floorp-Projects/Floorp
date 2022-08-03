34c68ad6f44bf8287d927d30d1f3c032207a1d33
Mark Banner <standard8@mozilla.com>
2022-08-03 07:29:08 +0000

    Bug 1782523 - Update NodeJS to latest versions for the branches. r=firefox-build-system-reviewers,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D153445

--- taskcluster/docker/recipes/install-node.sh
+++ taskcluster/docker/recipes/install-node.sh
@@ -8,8 +8,8 @@
 # taskcluster/scripts/misc/repack-node.sh. Later we'll get the ESLint builder
 # to use the linux64-node toolchain directly.
 
-wget -O node.xz --progress=dot:mega https://nodejs.org/download/release/v16.14.2/node-v16.14.2-linux-x64.tar.xz
-echo 'e40c6f81bfd078976d85296b5e657be19e06862497741ad82902d0704b34bb1b' node.xz | sha256sum -c
+wget -O node.xz --progress=dot:mega https://nodejs.org/dist/v16.16.0/node-v16.16.0-linux-x64.tar.xz
+echo 'edcb6e9bb049ae365611aa209fc03c4bfc7e0295dbcc5b2f1e710ac70384a8ec' node.xz | sha256sum -c
 tar -C /usr/local -xJ --strip-components 1 < node.xz
 node -v  # verify
 npm -v
