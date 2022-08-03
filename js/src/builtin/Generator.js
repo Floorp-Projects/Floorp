b42f2eb105040c4d8833144ab0745a38d18c9f02
André Bargull <andre.bargull@gmail.com>
2022-08-03 06:37:57 +0000

    Bug 1782212 - Part 5: Add curly brackets to js/src/builtin/Generator.js. r=tcampbell
    Depends on D153175
    
    Differential Revision: https://phabricator.services.mozilla.com/D153176

--- js/src/builtin/Generator.js
+++ js/src/builtin/Generator.js
@@ -8,64 +8,76 @@ function GeneratorNext(val) {
     // single call. It's also inlined in Baseline.
 
     if (!IsSuspendedGenerator(this)) {
-        if (!IsObject(this) || !IsGeneratorObject(this))
+        if (!IsObject(this) || !IsGeneratorObject(this)) {
             return callFunction(CallGeneratorMethodIfWrapped, this, val, "GeneratorNext");
+        }
 
-        if (GeneratorObjectIsClosed(this))
+        if (GeneratorObjectIsClosed(this)) {
             return { value: undefined, done: true };
+        }
 
-        if (GeneratorIsRunning(this))
+        if (GeneratorIsRunning(this)) {
             ThrowTypeError(JSMSG_NESTING_GENERATOR);
+        }
     }
 
     try {
         return resumeGenerator(this, val, "next");
     } catch (e) {
-        if (!GeneratorObjectIsClosed(this))
+        if (!GeneratorObjectIsClosed(this)) {
             GeneratorSetClosed(this);
+        }
         throw e;
     }
 }
 
 function GeneratorThrow(val) {
     if (!IsSuspendedGenerator(this)) {
-        if (!IsObject(this) || !IsGeneratorObject(this))
+        if (!IsObject(this) || !IsGeneratorObject(this)) {
             return callFunction(CallGeneratorMethodIfWrapped, this, val, "GeneratorThrow");
+        }
 
-        if (GeneratorObjectIsClosed(this))
+        if (GeneratorObjectIsClosed(this)) {
             throw val;
+        }
 
-        if (GeneratorIsRunning(this))
+        if (GeneratorIsRunning(this)) {
             ThrowTypeError(JSMSG_NESTING_GENERATOR);
+        }
     }
 
     try {
         return resumeGenerator(this, val, "throw");
     } catch (e) {
-        if (!GeneratorObjectIsClosed(this))
+        if (!GeneratorObjectIsClosed(this)) {
             GeneratorSetClosed(this);
+        }
         throw e;
     }
 }
 
 function GeneratorReturn(val) {
     if (!IsSuspendedGenerator(this)) {
-        if (!IsObject(this) || !IsGeneratorObject(this))
+        if (!IsObject(this) || !IsGeneratorObject(this)) {
             return callFunction(CallGeneratorMethodIfWrapped, this, val, "GeneratorReturn");
+        }
 
-        if (GeneratorObjectIsClosed(this))
+        if (GeneratorObjectIsClosed(this)) {
             return { value: val, done: true };
+        }
 
-        if (GeneratorIsRunning(this))
+        if (GeneratorIsRunning(this)) {
             ThrowTypeError(JSMSG_NESTING_GENERATOR);
+        }
     }
 
     try {
         var rval = { value: val, done: true };
         return resumeGenerator(this, rval, "return");
     } catch (e) {
-        if (!GeneratorObjectIsClosed(this))
+        if (!GeneratorObjectIsClosed(this)) {
             GeneratorSetClosed(this);
+        }
         throw e;
     }
 }
@@ -76,10 +88,12 @@ function InterpretGeneratorResume(gen, val, kind) {
     // forceInterpreter() call below compiles to a bytecode op that prevents us
     // from JITing this script.
     forceInterpreter();
-    if (kind === "next")
+    if (kind === "next") {
        return resumeGenerator(gen, val, "next");
-    if (kind === "throw")
+    }
+    if (kind === "throw") {
        return resumeGenerator(gen, val, "throw");
+    }
     assert(kind === "return", "Invalid resume kind");
     return resumeGenerator(gen, val, "return");
 }
