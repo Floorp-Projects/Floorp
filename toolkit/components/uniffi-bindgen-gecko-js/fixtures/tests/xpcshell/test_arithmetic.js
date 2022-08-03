1edbb25daae89aca5a6bd2e0846fdd9676ebcc79
Ben Dean-Kawamura <bdk@mozilla.com>
2022-08-03 13:48:29 +0000

    Bug 1766045 - Adding xpcshell tests for our UniFFI fixtures r=teshaq,markh
    To test the bindings generation, we generate JS bindings for a set of test
    Rust crates, then run xpcshell tests against those bindings.
    
    Currently these tests only run when the `--enable-uniffi-fixtures`
    option is set, so they need to be run manually be a dev.  I'd love to
    create a system where these can optionally run in CI, but I'm not sure
    how to do that.
    
    Differential Revision: https://phabricator.services.mozilla.com/D144469

---
+++ toolkit/components/uniffi-bindgen-gecko-js/fixtures/tests/xpcshell/test_arithmetic.js
@@ -0,0 +1,48 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+const Arithmetic = ChromeUtils.import(
+  "resource://gre/modules/components-utils/Arithmetic.jsm"
+);
+
+add_task(async function() {
+  Assert.ok(Arithmetic.IntegerOverflow);
+  Assert.equal(await Arithmetic.add(2, 4), 6);
+  Assert.equal(await Arithmetic.add(4, 8), 12);
+  // For other backends we would have this test:
+  // await Assert.rejects(
+  //     Arithmetic.add(18446744073709551615, 1),
+  //     Arithmetic.IntegerOverflow,
+  //     "add() should throw IntegerOverflow")
+  //
+  // However, this doesn't work because JS number values are actually 64-bit
+  // floats, and that number is greater than the maximum "safe" integer.
+  //
+  // Instead, let's test that we reject numbers that are that big
+  await Assert.rejects(
+    Arithmetic.add(Number.MAX_SAFE_INTEGER + 1, 0),
+    /TypeError/,
+    "add() should throw TypeError when an input is > MAX_SAFE_INTEGER"
+  );
+
+  Assert.equal(await Arithmetic.sub(4, 2), 2);
+  Assert.equal(await Arithmetic.sub(8, 4), 4);
+  await Assert.rejects(
+    Arithmetic.sub(0, 1),
+    Arithmetic.IntegerOverflow,
+    "sub() should throw IntegerOverflow"
+  );
+
+  Assert.equal(await Arithmetic.div(8, 4), 2);
+  // Can't test this, because we don't allow Rust panics in FF
+  // Assert.rejects(
+  //     Arithmetic.div(8, 0),
+  //     (e) => Assert.equal(e, Arithmetic.UniFFIInternalError),
+  //     "Divide by 0 should throw UniFFIInternalError")
+  //
+  Assert.ok(await Arithmetic.equal(2, 2));
+  Assert.ok(await Arithmetic.equal(4, 4));
+
+  Assert.ok(!(await Arithmetic.equal(2, 4)));
+  Assert.ok(!(await Arithmetic.equal(4, 8)));
+});
---
+++ toolkit/components/uniffi-bindgen-gecko-js/fixtures/tests/xpcshell/test_type_checking.js
@@ -0,0 +1,47 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+const Arithmetic = ChromeUtils.import(
+  "resource://gre/modules/components-utils/Arithmetic.jsm"
+);
+const Geometry = ChromeUtils.import(
+  "resource://gre/modules/components-utils/Geometry.jsm"
+);
+
+const { TodoList } = ChromeUtils.import(
+  "resource://gre/modules/components-utils/Todolist.jsm"
+);
+const { Stringifier } = ChromeUtils.import(
+  "resource://gre/modules/components-utils/Rondpoint.jsm"
+);
+
+add_task(async function() {
+  // Test our "type checking", which at this point is checking that
+  // the correct number of arguments are passed and that pointer
+  // arguments are of the correct type.
+
+  await Assert.rejects(
+    Arithmetic.add(2),
+    /TypeError/,
+    "add() call missing argument"
+  );
+  Assert.throws(
+    () => Geometry.Point(0.0),
+    /TypeError/,
+    "Point constructor missing argument"
+  );
+
+  const todo = await TodoList.init();
+  const stringifier = await Stringifier.init();
+  await todo.getEntries(); // OK
+  todo.ptr = stringifier.ptr;
+
+  try {
+    await todo.getEntries(); // the pointer is incorrect, should throw
+    Assert.fail("Should have thrown the pointer was an incorrect pointer");
+  } catch (e) {
+    // OK
+    // For some reason Assert.throws() can't seem to catch the "TypeError"s thrown
+    // from C++
+  }
+});
