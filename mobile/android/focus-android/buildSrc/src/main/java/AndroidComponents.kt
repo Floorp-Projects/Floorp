a385fadc7b1b5a8760e2e47d2e18879280ad66c1
MickeyMoz <sebastian@mozilla.com>
2022-08-04 15:23:41 +0000

    [focus] Update to Android-Components 105.0.20220804143845.

--- mobile/android/focus-android/buildSrc/src/main/java/AndroidComponents.kt
+++ mobile/android/focus-android/buildSrc/src/main/java/AndroidComponents.kt
@@ -3,5 +3,5 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 object AndroidComponents {
-    const val VERSION = "105.0.20220803190103"
+    const val VERSION = "105.0.20220804143845"
 }
