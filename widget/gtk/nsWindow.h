6ba579374b5279883a73f59a0eade2960e5dd35c
stransky <stransky@redhat.com>
2022-08-04 09:26:38 +0000

    Bug 1782778 [Linux] Rename PauseCompositor/ResumeCompositor to PauseCompositorFlickering/ResumeCompositorFlickering r=emilio
    Rename PauseCompositor/ResumeCompositor to make it clear that we use it to supress flickering only.
    
    Differential Revision: https://phabricator.services.mozilla.com/D153572

--- widget/gtk/nsWindow.h
+++ widget/gtk/nsWindow.h
@@ -438,9 +438,9 @@ class nsWindow final : public nsBaseWidget {
   } WindowCompositorState;
 
   // Pause compositor to avoid rendering artifacts from content process.
-  void ResumeCompositor();
+  void ResumeCompositorFlickering();
   void ResumeCompositorFromCompositorThread();
-  void PauseCompositor();
+  void PauseCompositorFlickering();
   bool IsWaitingForCompositorResume();
 
  protected:
