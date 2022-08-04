2269969ac9421d0bceef86aa58bf93e956f75f7f
stransky <stransky@redhat.com>
2022-08-04 09:26:38 +0000

    Bug 1782778 [Linux] Rename PauseCompositor/ResumeCompositor to PauseCompositorFlickering/ResumeCompositorFlickering r=emilio
    Rename PauseCompositor/ResumeCompositor to make it clear that we use it to supress flickering only.
    
    Differential Revision: https://phabricator.services.mozilla.com/D153572

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -4923,7 +4923,7 @@ void nsWindow::OnScaleChanged() {
   // produces flickering.
   // Re-enable compositor again when remote content is updated or
   // timeout happens.
-  PauseCompositor();
+  PauseCompositorFlickering();
 
   // Force scale factor recalculation
   mWindowScaleFactorChanged = true;
@@ -6101,12 +6101,6 @@ void nsWindow::NativeMoveResize(bool aMoved, bool aResized) {
   }
 }
 
-static int WindowResumeCompositor(void* data) {
-  nsWindow* window = static_cast<nsWindow*>(data);
-  window->ResumeCompositor();
-  return true;
-}
-
 // We pause compositor to avoid rendering of obsoleted remote content which
 // produces flickering.
 // Re-enable compositor again when remote content is updated or
@@ -6116,7 +6110,7 @@ static int WindowResumeCompositor(void* data) {
 // in milliseconds.
 #define COMPOSITOR_PAUSE_TIMEOUT (1000)
 
-void nsWindow::PauseCompositor() {
+void nsWindow::PauseCompositorFlickering() {
   bool pauseCompositor = (mWindowType == eWindowType_toplevel) &&
                          mCompositorState == COMPOSITOR_ENABLED &&
                          mCompositorWidgetDelegate && !mIsDestroyed;
@@ -6124,7 +6118,7 @@ void nsWindow::PauseCompositor() {
     return;
   }
 
-  LOG("nsWindow::PauseCompositor()");
+  LOG("nsWindow::PauseCompositorFlickering()");
 
   if (mCompositorPauseTimeoutID) {
     g_source_remove(mCompositorPauseTimeoutID);
@@ -6136,7 +6130,13 @@ void nsWindow::PauseCompositor() {
     remoteRenderer->SendPause();
     mCompositorState = COMPOSITOR_PAUSED_FLICKERING;
     mCompositorPauseTimeoutID = (int)g_timeout_add(
-        COMPOSITOR_PAUSE_TIMEOUT, &WindowResumeCompositor, this);
+        COMPOSITOR_PAUSE_TIMEOUT,
+        [](void* data) -> gint {
+          nsWindow* window = static_cast<nsWindow*>(data);
+          window->ResumeCompositorFlickering();
+          return true;
+        },
+        this);
   }
 }
 
@@ -6144,10 +6144,10 @@ bool nsWindow::IsWaitingForCompositorResume() {
   return mCompositorState == COMPOSITOR_PAUSED_FLICKERING;
 }
 
-void nsWindow::ResumeCompositor() {
+void nsWindow::ResumeCompositorFlickering() {
   MOZ_RELEASE_ASSERT(NS_IsMainThread());
 
-  LOG("nsWindow::ResumeCompositor()\n");
+  LOG("nsWindow::ResumeCompositorFlickering()\n");
 
   if (mIsDestroyed || !IsWaitingForCompositorResume()) {
     LOG("  early quit\n");
@@ -6167,8 +6167,9 @@ void nsWindow::ResumeCompositor() {
 }
 
 void nsWindow::ResumeCompositorFromCompositorThread() {
-  nsCOMPtr<nsIRunnable> event = NewRunnableMethod(
-      "nsWindow::ResumeCompositor", this, &nsWindow::ResumeCompositor);
+  nsCOMPtr<nsIRunnable> event =
+      NewRunnableMethod("nsWindow::ResumeCompositorFlickering", this,
+                        &nsWindow::ResumeCompositorFlickering);
   NS_DispatchToMainThread(event.forget());
 }
 
