50d176de6292d9f18853172de0f7157ba36e39d9
Steve Fink <sfink@mozilla.com>
2022-08-08 21:41:06 +0000

    Bug 1782558 - Fix overflow when rounding up to power of two during aggressive Vector growth r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D153681

---
+++ js/src/jit-test/tests/bug1782558-veclen.js
@@ -0,0 +1,14 @@
+// |jit-test| skip-if: getBuildConfiguration()["pointer-byte-size"] > 4
+
+// On 64-bit, this will allocate 2G temporary strings for keys while
+// stringifying the Array, which takes a rather long time and doesn't have the
+// potential of the problematic overflowing anyway.
+
+try {
+    let x = [];
+    x.length = Math.pow(2, 32) - 1;
+    x + 1;
+    assertEq(true, false, "overflow expected");
+} catch (e) {
+    assertEq((e + "").includes("InternalError: allocation size overflow"), true);
+}
