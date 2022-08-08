185725960a9f12b2da692cbfbd3f1a2e3f010fc8
surapunoyousei <ryosukeusuusu@gmail.com>
2022-08-08 22:58:02 +0900

    Resolve macOS default font problem

--- browser/app/profile/000-floorp.js
+++ browser/app/profile/000-floorp.js
@@ -192,9 +192,11 @@ pref("floorp.enable.multitab", false);
 
 //font
 
+#ifdef XP_WIN
 pref("font.name-list.serif.ja", "Noto Sans JP, Yu Mincho, MS PMincho, MS Mincho, Meiryo, Yu Gothic, MS PGothic, MS Gothic");
 pref("font.name-list.sans-serif.ja", "Noto Sans JP, Meiryo, Yu Gothic, MS PGothic, MS Gothic, Yu Mincho, MS PMincho, MS Mincho");
 pref("font.name-list.monospace.ja", "Noto Sans JP, MS Gothic, MS Mincho, Meiryo, Yu Gothic, Yu Mincho, MS PGothic, MS PMincho");
+#endif
 
 /*-----------------------------------------------------------------------------------all.js の設定-----------------------------------------------------------------------------------*/
 
