f1e111c6dca6e04c2f5407f6b7d77aad66736212
Butkovits Atila <abutkovits@mozilla.com>
2022-08-08 19:33:03 +0300

    Backed out changeset 48d2a85f55af (bug 1783606) for causing failure at browser_inspector-iframe.js. CLOSED TREE

--- devtools/client/fronts/node.js
+++ devtools/client/fronts/node.js
@@ -320,15 +320,7 @@ class NodeFront extends FrontClassWithSpec(nodeSpec) {
   // parent-process only Browser Toolbox).
   get childrenUnavailable() {
     return (
-      // If form.useChildTargetToFetchChildren is true, it means the node HAS
-      // children in another target.
-      // Note: useChildTargetToFetchChildren might be undefined, force
-      // conversion to boolean. See Bug 1783613 to try and improve this.
-      !!this._form.useChildTargetToFetchChildren &&
-      // But if useChildTargetToFetchChildren is false, it means the client
-      // configuration prevents from displaying such children.
-      // This is the only case where children are considered as unavailable:
-      // they exist, but can't be retrieved by configuration.
+      this._form.useChildTargetToFetchChildren &&
       !this.useChildTargetToFetchChildren
     );
   }
@@ -351,7 +343,7 @@ class NodeFront extends FrontClassWithSpec(nodeSpec) {
       return false;
     }
 
-    return !!this._form.useChildTargetToFetchChildren;
+    return this._form.useChildTargetToFetchChildren;
   }
   get hasEventListeners() {
     return this._form.hasEventListeners;
