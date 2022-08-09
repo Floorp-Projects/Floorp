da7303f3b5b1c51fa0941cd88af36e22376cd1d3
André Bargull <andre.bargull@gmail.com>
2022-08-09 09:50:33 +0000

    Bug 1783703 - Part 13: Add an optimised code path for dense elements to Array.prototype.with. r=mgaudet
    We should provide an optimisation for dense elements when the code is already
    in C++.
    
    Differential Revision: https://phabricator.services.mozilla.com/D153994

---
+++ js/src/tests/non262/Array/with-dense.js
@@ -0,0 +1,103 @@
+// |reftest| shell-option(--enable-change-array-by-copy) skip-if(!Array.prototype.with)
+
+const indices = [
+  -10, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 10,
+];
+
+const arrays = [
+  // Dense no holes.
+  [],
+  [1],
+  [1,2],
+  [1,2,3],
+  [1,2,3,4],
+  [1,2,3,4,5,6,7,8],
+
+  // Dense trailing holes.
+  [,],
+  [1,,],
+  [1,2,,],
+  [1,2,3,,],
+  [1,2,3,4,,],
+  [1,2,3,4,5,6,7,8,,],
+
+  // Dense leading holes.
+  [,],
+  [,1],
+  [,1,2],
+  [,1,2,3],
+  [,1,2,3,4],
+  [,1,2,3,4,5,6,7,8],
+
+  // Dense with holes.
+  [1,,3],
+  [1,2,,4],
+  [1,,3,,5,6,,8],
+];
+
+const objects = arrays.map(array => {
+  let obj = {
+    length: array.length,
+  };
+  for (let i = 0; i < array.length; ++i) {
+    if (i in array) {
+      obj[i] = array[i];
+    }
+  }
+  return obj;
+});
+
+const objectsWithLargerDenseInitializedLength = arrays.map(array => {
+  let obj = {
+    length: array.length,
+  };
+  for (let i = 0; i < array.length; ++i) {
+    if (i in array) {
+      obj[i] = array[i];
+    }
+  }
+
+  // Add some extra dense elements after |length|.
+  for (let i = 0; i < 5; ++i) {
+    obj[array.length + i] = "extra";
+  }
+
+  return obj;
+});
+
+const thisValues = [
+  ...arrays,
+  ...objects,
+  ...objectsWithLargerDenseInitializedLength,
+];
+
+const replacement = {};
+
+for (let thisValue of thisValues) {
+  for (let index of indices) {
+    let actualIndex = index;
+    if (actualIndex < 0) {
+      actualIndex += thisValue.length;
+    }
+
+    if (actualIndex < 0 || actualIndex >= thisValue.length) {
+      continue;
+    }
+
+    let res = Array.prototype.with.call(thisValue, index, replacement);
+    assertEq(res.length, thisValue.length);
+
+    for (let i = 0; i < thisValue.length; ++i) {
+      assertEq(Object.hasOwn(res, i), true);
+
+      if (i === actualIndex) {
+        assertEq(res[i], replacement);
+      } else {
+        assertEq(res[i], thisValue[i]);
+      }
+    }
+  }
+}
+
+if (typeof reportCompare === "function")
+  reportCompare(0, 0);
