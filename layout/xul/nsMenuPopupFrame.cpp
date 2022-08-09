027e7eb37bd98fe036b91bb7b478a5436994e0e8
Emilio Cobos Álvarez <emilio@crisal.io>
2022-08-09 10:42:39 +0000

    Bug 1783500 - Account for negative popup margins when anchoring to a large rect. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D154074

--- layout/xul/nsMenuPopupFrame.cpp
+++ layout/xul/nsMenuPopupFrame.cpp
@@ -1451,9 +1451,6 @@ nsresult nsMenuPopupFrame::SetPopupPosition(nsIFrame* aAnchorFrame,
   // size will be 0.
   nsRect anchorRect;
 
-  // Width of the parent, used when aSizedToPopup is true.
-  int32_t parentWidth = 0;
-
   bool anchored = IsAnchored();
   if (anchored || aSizedToPopup) {
     // In order to deal with transforms, we need the root prescontext:
@@ -1486,9 +1483,6 @@ nsresult nsMenuPopupFrame::SetPopupPosition(nsIFrame* aAnchorFrame,
 
       anchorRect = ComputeAnchorRect(rootPresContext, aAnchorFrame);
     }
-
-    // The width is needed when aSizedToPopup is true
-    parentWidth = anchorRect.width;
   }
 
   // Set the popup's size to the preferred size. Below, this size will be
@@ -1500,12 +1494,18 @@ nsresult nsMenuPopupFrame::SetPopupPosition(nsIFrame* aAnchorFrame,
                  "preferred size of popup not set");
     nsSize newSize = mPrefSize;
     if (aSizedToPopup) {
-      newSize.width = parentWidth;
+      nsMargin rawMargin;
+      if (StyleMargin()->GetMargin(rawMargin)) {
+        rawMargin.EnsureAtMost(nsMargin());
+      }
+
+      newSize.width = anchorRect.width - rawMargin.LeftRight();
       // If we're anchoring to a rect, and the rect is smaller than the
       // preferred size of the popup, change its width accordingly.
       if (mAnchorType == MenuPopupAnchorType_Rect) {
-        newSize.width = std::max(parentWidth, mPrefSize.width);
+        newSize.width = std::max(newSize.width, mPrefSize.width);
       }
+
       // Pref size is already constrained by LayoutPopup().
       ConstrainSizeForWayland(newSize);
     }
