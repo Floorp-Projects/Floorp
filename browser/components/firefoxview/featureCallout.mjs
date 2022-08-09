8b881305feb24a6be872dc6d2811a73c54264952
Meg Viar <lmegviar@gmail.com>
2022-08-09 15:48:39 +0000

    Bug 1779423 - Add configurable positioning to Feature Callout surface r=Mardak
    Differential Revision: https://phabricator.services.mozilla.com/D152824

--- browser/components/firefoxview/featureCallout.mjs
+++ browser/components/firefoxview/featureCallout.mjs
@@ -48,7 +48,6 @@ function _addCalloutLinkElements() {
 }
 
 let CURRENT_SCREEN;
-let POSITION_HANDLER;
 let CONFIG;
 
 const CONTAINER_ID = "root";
@@ -65,6 +64,7 @@ const MESSAGES = [
         parent_selector: "#tabpickup-steps",
         content: {
           position: "callout",
+          arrow_position: "top",
           title: "Hop between devices with tab pickup",
           subtitle:
             "Quickly grab open tabs from your phone and open them here for maximum flow.",
@@ -90,6 +90,7 @@ const MESSAGES = [
         parent_selector: "#recently-closed-tabs-container",
         content: {
           position: "callout",
+          arrow_position: "bottom",
           title: "Get back your closed tabs in a snap",
           subtitle:
             "All your closed tabs will magically show up here. Never worry about accidentally closing a site again.",
@@ -111,6 +112,7 @@ const MESSAGES = [
         parent_selector: "#colorways",
         content: {
           position: "callout",
+          arrow_position: "end",
           title: "Add a splash of color",
           subtitle:
             "Paint your browser with colorways, only in Firefox. Choose the shade that speaks to you.",
@@ -143,9 +145,17 @@ function _createContainer() {
   return container;
 }
 
+/**
+ * Set callout's position relative to parent element
+ */
 function _positionCallout() {
-  let container = document.getElementById(CONTAINER_ID);
-  let parentEl = document.querySelector(CURRENT_SCREEN?.parent_selector);
+  const positions = ["top", "bottom", "left", "right"];
+  const container = document.getElementById("root");
+  const parentEl = document.querySelector(CURRENT_SCREEN?.parent_selector);
+  const arrowPosition = CURRENT_SCREEN?.content?.arrow_position || "top";
+  const margin = 15;
+  // Is the document layout right to left?
+  const RTL = document.dir === "rtl";
 
   if (!container || !parentEl) {
     return;
@@ -155,37 +165,86 @@ function _positionCallout() {
     const rect = el.getBoundingClientRect();
     return {
       left: rect.left + window.scrollX,
+      right: rect.right + window.scrollX,
+      top: rect.top + window.scrollY,
       bottom: rect.bottom + window.scrollY,
     };
   }
-  // Set callout's position relative to parent element
-  const margin = 15;
-  let containerTop = getOffset(parentEl).bottom - margin;
-  container.style.top = `${Math.min(
-    window.innerHeight - container.offsetHeight - margin,
-    containerTop
-  )}px`;
-  let leftOffset = (parentEl.offsetWidth - container.offsetWidth) / 2;
-  let containerLeft = getOffset(parentEl).left + leftOffset;
-  container.style.left = `${Math.min(
-    window.innerWidth - container.offsetWidth - margin,
-    Math.max(containerLeft, margin)
-  )}px`;
+
+  function clearPosition() {
+    positions.forEach(position => {
+      container.style[position] = "unset";
+    });
+  }
+
+  function centerHorizontally() {
+    let sideOffset = (parentEl.offsetWidth - container.offsetWidth) / 2;
+    let containerSide = RTL
+      ? window.innerWidth - getOffset(parentEl).right + sideOffset
+      : getOffset(parentEl).left + sideOffset;
+    container.style[RTL ? "right" : "left"] = `${Math.max(
+      containerSide,
+      margin
+    )}px`;
+  }
+
+  // Position callout relative to a parent element
+  const positioners = {
+    top() {
+      let containerTop = getOffset(parentEl).bottom - margin;
+      container.style.top = `${Math.min(
+        window.innerHeight - container.offsetHeight - margin,
+        containerTop
+      )}px`;
+      centerHorizontally(container, parentEl);
+    },
+    // Point to an element below the callout
+    bottom() {
+      // Container wrapper doesn't have a height, so use height of screen element
+      let containerHeight = container.querySelector(".screen").clientHeight;
+      let containerTop = getOffset(parentEl).top - containerHeight + margin;
+      container.style.top = `${Math.max(containerTop, 0)}px`;
+      centerHorizontally(container, parentEl);
+    },
+    // Point to an element to the right of the callout
+    left() {
+      let containerLeft = getOffset(parentEl).right - margin;
+      container.style.left = `${Math.min(
+        window.innerWidth - container.offsetWidth - margin,
+        containerLeft
+      )}px`;
+      container.style.top = `${getOffset(parentEl).top}px`;
+    },
+    // Point to an element to the left of the callout
+    right() {
+      let containerLeft =
+        getOffset(parentEl).left - container.offsetWidth + margin;
+      container.style.left = `${Math.max(containerLeft, margin)}px`;
+      container.style.top = `${getOffset(parentEl).top}px`;
+    },
+  };
+
+  clearPosition(container);
+
+  if (["start", "end"].includes(arrowPosition)) {
+    if (RTL) {
+      positioners[arrowPosition === "start" ? "right" : "left"]();
+    } else {
+      positioners[arrowPosition === "start" ? "left" : "right"]();
+    }
+  } else {
+    positioners[arrowPosition]();
+  }
 }
 
 function _addPositionListeners() {
-  POSITION_HANDLER = () => {
-    _positionCallout(CURRENT_SCREEN.parent_selector);
-  };
-  window.addEventListener("scroll", POSITION_HANDLER);
-  window.addEventListener("resize", POSITION_HANDLER);
+  window.addEventListener("scroll", _positionCallout);
+  window.addEventListener("resize", _positionCallout);
 }
 
 function _removePositionListeners() {
-  if (POSITION_HANDLER) {
-    window.removeEventListener("scroll", POSITION_HANDLER);
-    window.removeEventListener("resize", POSITION_HANDLER);
-  }
+  window.removeEventListener("scroll", _positionCallout);
+  window.removeEventListener("resize", _positionCallout);
 }
 
 function _setupWindowFunctions() {
@@ -269,6 +328,5 @@ export async function showFeatureCallout(messageId) {
   // This results in rendering the Feature Callout
   await _addScriptsAndRender(container);
 
-  // Add handlers for repositioning callout
   _addPositionListeners();
 }
