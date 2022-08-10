fd50d837b5135675eca2070971e18856fcf8f496
MickeyMoz <sebastian@mozilla.com>
2022-08-10 02:14:00 +0000

    [components] Update Glean to 51.1.0.

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -33,7 +33,7 @@ object Versions {
     const val mozilla_appservices = "93.8.0"
 
     // DO NOT MODIFY MANUALLY. This is auto-updated along with GeckoView.
-    const val mozilla_glean = "51.0.1"
+    const val mozilla_glean = "51.1.0"
 
     const val material = "1.2.1"
 
