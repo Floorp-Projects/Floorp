ec510030e29cc6353f8d7bc5d1f0da4a169a1d6c
Jason Prickett <jprickett@mozilla.com>
2022-08-10 18:16:53 +0000

    Bug 1774067 - Adding about welcome mobile download screen in MR onboarding.  r=pdahiya,mviar
    Differential Revision: https://phabricator.services.mozilla.com/D152295

---
+++ browser/components/newtab/test/unit/aboutwelcome/HeroImage.test.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { HeroImage } from "content-src/aboutwelcome/components/HeroImage";
+
+describe("HeroImage component", () => {
+  const imageUrl = "https://example.com";
+  const imageHeight = "100px";
+  const imageAlt = "Alt text";
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <HeroImage url={imageUrl} alt={imageAlt} height={imageHeight} />
+    );
+  });
+
+  it("should render HeroImage component", () => {
+    assert.ok(wrapper.exists());
+  });
+
+  it("should render an image element with src prop", () => {
+    let imgEl = wrapper.find("img");
+    assert.strictEqual(imgEl.prop("src"), imageUrl);
+  });
+
+  it("should render image element with alt text prop", () => {
+    let imgEl = wrapper.find("img");
+    assert.equal(imgEl.prop("alt"), imageAlt);
+  });
+
+  it("should render an image with a set height prop", () => {
+    let imgEl = wrapper.find("img");
+    assert.propertyVal(imgEl.prop("style"), "height", imageHeight);
+  });
+
+  it("should not render HeroImage component", () => {
+    wrapper.setProps({ url: null });
+    assert.ok(wrapper.isEmptyRender());
+  });
+});
