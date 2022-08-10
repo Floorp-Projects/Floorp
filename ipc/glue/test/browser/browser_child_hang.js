f15242afde8cf28ee95f8b90df8c7bf3d71333e0
Jed Davis <jld@mozilla.com>
2022-08-10 21:01:32 +0000

    Bug 1658072 - Rewrite the Unix implementation of IPC process termination handling. r=nika
    This patch rewrites the Unix backend of ProcessWatcher for two reasons:
    
    1. To remove the use of libevent's signal handling, which has concurrency
       bugs that can't be easily fixed upstream (see Bugzilla for details)
    
    2. To simplify the code in general; in particular, the new version has one
       place where the process and its exit status are consumed from the OS
    
    The new implementation uses the same pipe-to-self technique as libevent
    (and which we use elsewhere) to deal with async signal safety.  Unlike
    the previous version, there is a single object which manages all
    monitored child processes rather than one each.  (Previously, this
    multiplexing was done inside libevent.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D141309

---
+++ ipc/glue/test/browser/browser_child_hang.js
@@ -0,0 +1,37 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+//
+// Try to open a tab.  This provides code coverage for a few things,
+// although currently there's no automated functional test of correctness:
+//
+// * On opt builds, when the tab is closed and the process exits, it
+//   will hang for 3s and the parent will kill it after 2s.
+//
+// * On debug[*] builds, the parent process will wait until the
+//   process exits normally; but also, on browser shutdown, the
+//   preallocated content processes will block parent shutdown in
+//   WillDestroyCurrentMessageLoop.
+//
+// [*] Also sanitizer and code coverage builds.
+//
+
+add_task(async function() {
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: "https://example.com/",
+      forceNewProcess: true,
+    },
+    async function(browser) {
+      // browser.frameLoader.remoteTab.osPid is the child pid; once we
+      // have a way to get notifications about child process termination
+      // events, that could be useful.
+      ok(true, "Browser isn't broken");
+    }
+  );
+  // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+  await new Promise(resolve => setTimeout(resolve, 4000));
+  ok(true, "Still running after child process (hopefully) exited");
+});
