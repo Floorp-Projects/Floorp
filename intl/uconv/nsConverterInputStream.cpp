d8da4692373d57ee55b3962140480b6f9b21e647
Nika Layzell <nika@thelayzells.com>
2022-08-11 17:37:20 +0000

    Bug 1170668 - Improve short read handling in nsConverterInputStream, r=hsivonen
    This patch changes how nsConverterInputStream handles passing data
    through to the underlying unicode converter in order to make it more
    reliably handle propagating errors and deal with short reads from the
    underlying input stream.
    
    This was done by making the code continuously read within the Fill
    method until at least one character has been decoded from the input
    stream, so that we don't spuriously communicate an EOF to the caller due
    to a short read not producing enough bytes for the decoder to produce a
    UTF-16 character.
    
    In addition, while making this change it became easier to signal to
    the decoder about the final read from the input stream, meaning that
    partial characters at the end of the stream will now generate a
    replacement character, rather than being ignored.
    
    Differential Revision: https://phabricator.services.mozilla.com/D152682

--- intl/uconv/nsConverterInputStream.cpp
+++ intl/uconv/nsConverterInputStream.cpp
@@ -105,39 +105,38 @@ nsConverterInputStream::ReadSegments(nsWriteUnicharSegmentFun aWriter,
                                      void* aClosure, uint32_t aCount,
                                      uint32_t* aReadCount) {
   NS_ASSERTION(mUnicharDataLength >= mUnicharDataOffset, "unsigned madness");
-  uint32_t bytesToWrite = mUnicharDataLength - mUnicharDataOffset;
-  nsresult rv;
-  if (0 == bytesToWrite) {
+  uint32_t codeUnitsToWrite = mUnicharDataLength - mUnicharDataOffset;
+  if (0 == codeUnitsToWrite) {
     // Fill the unichar buffer
-    bytesToWrite = Fill(&rv);
-    if (bytesToWrite <= 0) {
+    codeUnitsToWrite = Fill(&mLastErrorCode);
+    if (codeUnitsToWrite == 0) {
       *aReadCount = 0;
-      return rv;
-    }
-    if (NS_FAILED(rv)) {
-      return rv;
+      return mLastErrorCode;
     }
   }
 
-  if (bytesToWrite > aCount) bytesToWrite = aCount;
+  if (codeUnitsToWrite > aCount) {
+    codeUnitsToWrite = aCount;
+  }
 
-  uint32_t bytesWritten;
-  uint32_t totalBytesWritten = 0;
+  uint32_t codeUnitsWritten;
+  uint32_t totalCodeUnitsWritten = 0;
 
-  while (bytesToWrite) {
-    rv = aWriter(this, aClosure, mUnicharData.Elements() + mUnicharDataOffset,
-                 totalBytesWritten, bytesToWrite, &bytesWritten);
+  while (codeUnitsToWrite) {
+    nsresult rv =
+        aWriter(this, aClosure, mUnicharData.Elements() + mUnicharDataOffset,
+                totalCodeUnitsWritten, codeUnitsToWrite, &codeUnitsWritten);
     if (NS_FAILED(rv)) {
       // don't propagate errors to the caller
       break;
     }
 
-    bytesToWrite -= bytesWritten;
-    totalBytesWritten += bytesWritten;
-    mUnicharDataOffset += bytesWritten;
+    codeUnitsToWrite -= codeUnitsWritten;
+    totalCodeUnitsWritten += codeUnitsWritten;
+    mUnicharDataOffset += codeUnitsWritten;
   }
 
-  *aReadCount = totalBytesWritten;
+  *aReadCount = totalCodeUnitsWritten;
 
   return NS_OK;
 }
@@ -166,7 +165,7 @@ nsConverterInputStream::ReadString(uint32_t aCount, nsAString& aString,
 }
 
 uint32_t nsConverterInputStream::Fill(nsresult* aErrorCode) {
-  if (nullptr == mInput) {
+  if (!mInput) {
     // We already closed the stream!
     *aErrorCode = NS_BASE_STREAM_CLOSED;
     return 0;
@@ -179,54 +178,72 @@ uint32_t nsConverterInputStream::Fill(nsresult* aErrorCode) {
     return 0;
   }
 
-  // We assume a many to one conversion and are using equal sizes for
-  // the two buffers.  However if an error happens at the very start
-  // of a byte buffer we may end up in a situation where n bytes lead
-  // to n+1 unicode chars.  Thus we need to keep track of the leftover
-  // bytes as we convert.
-
-  uint32_t nb;
-  *aErrorCode = NS_FillArray(mByteData, mInput, mLeftOverBytes, &nb);
-  if (nb == 0 && mLeftOverBytes == 0) {
-    // No more data
-    *aErrorCode = NS_OK;
-    return 0;
-  }
-
-  NS_ASSERTION(uint32_t(nb) + mLeftOverBytes == mByteData.Length(),
-               "mByteData is lying to us somewhere");
-
-  // Now convert as much of the byte buffer to unicode as possible
-  auto src = AsBytes(Span(mByteData));
-  auto dst = Span(mUnicharData);
   // mUnicharData.Length() is the buffer length, not the fill status.
   // mUnicharDataLength reflects the current fill status.
   mUnicharDataLength = 0;
   // Whenever we convert, mUnicharData is logically empty.
   mUnicharDataOffset = 0;
-  // Truncation from size_t to uint32_t below is OK, because the sizes
-  // are bounded by the lengths of mByteData and mUnicharData.
-  uint32_t result;
-  size_t read;
-  size_t written;
-  // The design of this class is fundamentally bogus in that trailing
-  // errors are ignored. Always passing false as the last argument to
-  // Decode* calls below.
-  if (mErrorsAreFatal) {
-    std::tie(result, read, written) =
-        mConverter->DecodeToUTF16WithoutReplacement(src, dst, false);
-  } else {
-    std::tie(result, read, written, std::ignore) =
-        mConverter->DecodeToUTF16(src, dst, false);
+
+  // Continue trying to read from the source stream until we successfully decode
+  // a character or encounter an error, as returning `0` here implies that the
+  // stream is complete.
+  //
+  // If the converter has been cleared, we've fully consumed the stream, and
+  // want to report EOF.
+  while (mUnicharDataLength == 0 && mConverter) {
+    // We assume a many to one conversion and are using equal sizes for
+    // the two buffers.  However if an error happens at the very start
+    // of a byte buffer we may end up in a situation where n bytes lead
+    // to n+1 unicode chars.  Thus we need to keep track of the leftover
+    // bytes as we convert.
+
+    uint32_t nb;
+    *aErrorCode = NS_FillArray(mByteData, mInput, mLeftOverBytes, &nb);
+    if (NS_FAILED(*aErrorCode)) {
+      return 0;
+    }
+
+    NS_ASSERTION(uint32_t(nb) + mLeftOverBytes == mByteData.Length(),
+                 "mByteData is lying to us somewhere");
+
+    // If `NS_FillArray` failed to read any new bytes, this is the last read,
+    // and we're at the end of the stream.
+    bool last = (nb == 0);
+
+    // Now convert as much of the byte buffer to unicode as possible
+    auto src = AsBytes(Span(mByteData));
+    auto dst = Span(mUnicharData);
+
+    // Truncation from size_t to uint32_t below is OK, because the sizes
+    // are bounded by the lengths of mByteData and mUnicharData.
+    uint32_t result;
+    size_t read;
+    size_t written;
+    if (mErrorsAreFatal) {
+      std::tie(result, read, written) =
+          mConverter->DecodeToUTF16WithoutReplacement(src, dst, last);
+    } else {
+      std::tie(result, read, written, std::ignore) =
+          mConverter->DecodeToUTF16(src, dst, last);
+    }
+    mLeftOverBytes = mByteData.Length() - read;
+    mUnicharDataLength = written;
+    // Clear `mConverter` if we reached the end of the stream, as we can't
+    // call methods on it anymore. This will also signal EOF to the caller
+    // through the loop condition.
+    if (last) {
+      MOZ_ASSERT(mLeftOverBytes == 0,
+                 "Failed to read all bytes on the last pass?");
+      mConverter = nullptr;
+    }
+    // If we got a decode error, we're done.
+    if (result != kInputEmpty && result != kOutputFull) {
+      MOZ_ASSERT(mErrorsAreFatal, "How come DecodeToUTF16() reported error?");
+      *aErrorCode = NS_ERROR_UDEC_ILLEGALINPUT;
+      return 0;
+    }
   }
-  mLeftOverBytes = mByteData.Length() - read;
-  mUnicharDataLength = written;
-  if (result == kInputEmpty || result == kOutputFull) {
-    *aErrorCode = NS_OK;
-  } else {
-    MOZ_ASSERT(mErrorsAreFatal, "How come DecodeToUTF16() reported error?");
-    *aErrorCode = NS_ERROR_UDEC_ILLEGALINPUT;
-  }
+  *aErrorCode = NS_OK;
   return mUnicharDataLength;
 }
 
