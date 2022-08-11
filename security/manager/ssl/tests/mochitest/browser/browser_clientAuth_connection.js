eee4180016dc25234b2376de6c273ab23f659604
Barret Rennie <barret@brennie.ca>
2022-08-11 17:11:36 +0000

    Bug 1772923 - Port osfile.jsm usage to IOUtils in security/manager/ r=keeler
    Differential Revision: https://phabricator.services.mozilla.com/D153711

--- security/manager/ssl/tests/mochitest/browser/browser_clientAuth_connection.js
+++ security/manager/ssl/tests/mochitest/browser/browser_clientAuth_connection.js
@@ -307,12 +307,10 @@ add_task(async function testClearPrivateBrowsingState() {
 // Test that 3rd party certificates are taken into account when filtering client
 // certificates based on the acceptible CA list sent by the server.
 add_task(async function testCertFilteringWithIntermediate() {
-  let intermediateBytes = await OS.File.read(
+  let intermediateBytes = await IOUtils.readUTF8(
     getTestFilePath("intermediate.pem")
   ).then(
-    data => {
-      let decoder = new TextDecoder();
-      let pem = decoder.decode(data);
+    pem => {
       let base64 = pemToBase64(pem);
       let bin = atob(base64);
       let bytes = [];
