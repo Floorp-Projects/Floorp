90d0f750f72b5713c79674d1c16282656ad4aa1c
surapunoyousei <ryosukeusuusu@gmail.com>
2022-08-11 18:59:35 +0900

    Add random background image for new tab

---
+++ browser/base/content/newtabbg.js
@@ -0,0 +1,51 @@
+/*-- This Source Code Form is subject to the terms of the Mozilla Public
+   - License, v. 2.0. If a copy of the MPL was not distributed with this file,
+   - You can obtain one at http://mozilla.org/MPL/2.0/.*/ 
+
+window.setTimeout( function(){
+  const imgpass = "https://images.unsplash.com/";
+  const imgfile = [];
+     imgfile[0] = 'photo-1510020553968-30f966e1ec9e?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDE3ODY5MQ&ixlib=rb-1.2.1&q=85&w=1920';
+     imgfile[1] = 'photo-1459664018906-085c36f472af?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDE3ODY5MQ&ixlib=rb-1.2.1&q=85&w=1920';
+     imgfile[2] = 'photo-1459664018906-085c36f472af?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDE3ODY5MQ&ixlib=rb-1.2.1&q=85&w=1920';
+     imgfile[3] = 'photo-1509180756080-d110bee1e772?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDE3ODY5MQ&ixlib=rb-1.2.1&q=85&w=1920';
+     imgfile[4] = 'photo-1507720708252-1ddeb1dbff34?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDE3ODY5MQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[5] = 'photo-1498588543704-e0d466ddcfe5?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDE3ODY5MQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[6] = 'photo-1507398341856-436d32b7ce65?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[7] = 'photo-1499678329028-101435549a4e?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[8] = 'photo-1533610067042-1cee3c403282?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[9] = 'photo-1505598439340-9a5cdc676547?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[10] = 'photo-1497996377197-e4b9024658a4?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[11] = 'photo-1483192683197-083ca7511846?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[12] = 'photo-1505699261378-c372af38134c?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[13] = 'photo-1484278786775-527ac0d0b608?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[14] = 'photo-1501917018566-8186ec861d99?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[15] = 'photo-1554336903-a5f31529dd4b?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTkzMQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[16] = 'photo-1504121449080-76ab9eb5bd2b?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTk5Mg&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[17] = 'photo-1503197979108-c824168d51a8?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTk5Mg&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[18] = 'photo-1470217957101-da7150b9b681?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTk5Mg&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[19] = 'photo-1443891238287-325a8fddd0f7?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTk5Mg&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[20] = 'photo-1492095664363-4ca82097ec8a?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTk5Mg&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[21] = 'photo-1475767239063-103c472f2c63?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTk5Mg&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[22] = 'photo-1466853817435-05b43fe45b39?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNTk5Mg&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[23] = 'photo-1452698325353-b90e60289e87?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjA1NQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[24] = 'photo-1518627675569-e9d4fb90cdb5?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjA1NQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[25] = 'photo-1478145046317-39f10e56b5e9?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjA1NQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[26] = 'photo-1461301214746-1e109215d6d3?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjE3NA&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[27] = 'photo-1502582877126-512f3c5b0a64?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjA1NQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[28] = 'photo-1494564256121-aada9f29f988?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjA1NQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[29] = 'photo-1515179423979-11466ac39a24?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjA1NQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[30] = 'photo-1435777940218-be0b632d06db?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjA1NQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[31] = 'photo-1516442052782-a2ae429edde7?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjA1NQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[32] = 'photo-1492269682833-cd80f8a20b08?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjA1NQ&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[33] = 'photo-1510279770292-4b34de9f5c23?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjEzMA&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[35] = 'photo-1453280339213-efb07f95531b?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjEzMA&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[36] = 'photo-1460388052839-a52677720738?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjEzMA&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[37] = 'photo-1502688020178-2b376c868918?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjEzMA&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[38] = 'photo-1501707305551-9b2adda5e527?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjEzMA&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[39] = 'photo-1451337516015-6b6e9a44a8a3?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjE3NA&ixlib=rb-1.2.1&q=85&w=1920'
+     imgfile[40] = 'photo-1498637841888-108c6b723fcb?ixid=MnwxMTI1OHwwfDF8cmFuZG9tfHx8fHx8fHx8MTY2MDIwNjE3NA&ixlib=rb-1.2.1&q=85&w=1920'
+  const n = Math.floor(Math.random() * imgfile.length);
+  document.getElementById("background").style.backgroundImage = 'url(' + imgpass + imgfile[n] + ')'
+}, 100
+)
