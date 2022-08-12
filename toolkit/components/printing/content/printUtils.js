bf03607de06560ef02488be574b0ed5c9eadc525
Barret Rennie <barret@brennie.ca>
2022-08-12 18:37:31 +0000

    Bug 1776307 - Port osfile.jsm usage to IOUtils in toolkit/components/printing r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D150205

--- toolkit/components/printing/content/printUtils.js
+++ toolkit/components/printing/content/printUtils.js
@@ -62,8 +62,6 @@ ChromeUtils.defineModuleGetter(
   "resource://gre/modules/SharedPromptUtils.jsm"
 );
 
-const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
-
 var PrintUtils = {
   SAVE_TO_PDF_PRINTER: "Mozilla Save to PDF",
 
@@ -321,11 +319,14 @@ var PrintUtils = {
         // (including using DownloadPaths.sanitize!).
         // For now, the following is for consistency with the behavior
         // prior to bug 1669149 part 3.
-        let dest = await OS.File.getCurrentDirectory();
+        let dest = undefined;
+        try {
+          dest = Services.dirsvc.get("CurWorkD", Ci.nsIFile).path;
+        } catch (e) {}
         if (!dest) {
-          dest = OS.Constants.Path.homeDir;
+          dest = Services.dirsvc.get("Home", Ci.nsIFile).path;
         }
-        settings.toFileName = OS.Path.join(dest || "", "mozilla.pdf");
+        settings.toFileName = PathUtils.join(dest, "mozilla.pdf");
       }
 
       if (useSystemDialog) {
