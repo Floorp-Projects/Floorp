cdd3c2a6931ebd47439b124f98a6c45e0a6ab2c1
Dan Robertson <drobertson@mozilla.com>
2022-08-15 13:52:12 +0000

    Bug 1771423 - Set Content-Type to an empty string for an empty blob with no type. r=kershaw,smaug,necko-reviewers
    Set the Content-Type to an empty string for an blob when the type was not
    given. Ensure that the channel Content-Type is not overriden in the case
    that it was previously set and the blob type is not valid.
    
    Depends on D147568
    
    Differential Revision: https://phabricator.services.mozilla.com/D147481

--- netwerk/ipc/DocumentLoadListener.cpp
+++ netwerk/ipc/DocumentLoadListener.cpp
@@ -246,7 +246,8 @@ class ParentProcessDocumentOpenInfo final : public nsDocumentOpenInfo,
     // The one exception is nsUnknownDecoder, which works in the parent
     // (and we need to know what the content type is before we can
     // decide if it will be handled in the parent), so we run that here.
-    if (mContentType.LowerCaseEqualsASCII(UNKNOWN_CONTENT_TYPE)) {
+    if (mContentType.LowerCaseEqualsASCII(UNKNOWN_CONTENT_TYPE) ||
+        mContentType.IsEmpty()) {
       return nsDocumentOpenInfo::TryStreamConversion(aChannel);
     }
 
