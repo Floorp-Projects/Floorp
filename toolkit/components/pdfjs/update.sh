82e827cbddc38be8a6e532c2f16c3da3541726e6
Tom Ritter <tom@mozilla.com>
2022-08-15 18:36:09 +0000

    Bug 1783724: Fix nits in PDF.js's update script r=Snuffleupagus
    Differential Revision: https://phabricator.services.mozilla.com/D154009

--- toolkit/components/pdfjs/update.sh
+++ toolkit/components/pdfjs/update.sh
@@ -35,10 +35,8 @@ popd || exit
 
 
 mkdir -p "$ROOT/build/mozcentral/browser/extensions/pdfjs/"
-touch "$ROOT/build/mozcentral/browser/extensions/pdfjs/README.mozilla"
 
 cp "$ROOT/build/mozcentral/browser/extensions/pdfjs/content/LICENSE" "$GECKO_PATH/toolkit/components/pdfjs/"
-cp "$ROOT/build/mozcentral/browser/extensions/pdfjs/content/README.mozilla" "$GECKO_PATH/toolkit/components/pdfjs/"
 cp "$ROOT/build/mozcentral/browser/extensions/pdfjs/content/PdfJsDefaultPreferences.jsm" "$GECKO_PATH/toolkit/components/pdfjs/content/PdfJsDefaultPreferences.jsm"
 rsync -a -v --delete "$ROOT/build/mozcentral/browser/extensions/pdfjs/content/build/" "$GECKO_PATH/toolkit/components/pdfjs/content/build/"
 rsync -a -v --delete "$ROOT/build/mozcentral/browser/extensions/pdfjs/content/web/" "$GECKO_PATH/toolkit/components/pdfjs/content/web/"
