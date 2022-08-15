7b0f010adff3cab4237cc02954f565a9e594e269
Julian Seward <jseward@acm.org>
2022-08-15 10:08:38 +0000

    Bug 1774840 - Implement new GC instructions in baseline (Part 2).  r=rhunt.
    In the existing GC array implementation, change some names that are `length`
    or `elementsLength` to be `numElements`, to remove ambiguity about whether
    those quantities are "number of bytes" or "number of array elements".  No
    functional change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D154229

--- js/src/wasm/WasmInstance.h
+++ js/src/wasm/WasmInstance.h
@@ -449,7 +449,8 @@ class alignas(16) Instance {
   static void* structNew(Instance* instance, void* structDescr);
   static void* exceptionNew(Instance* instance, JSObject* tag);
   static int32_t throwException(Instance* instance, JSObject* exn);
-  static void* arrayNew(Instance* instance, uint32_t length, void* arrayDescr);
+  static void* arrayNew(Instance* instance, uint32_t numElements,
+                        void* arrayDescr);
   static int32_t refTest(Instance* instance, void* refPtr, void* rttPtr);
   static int32_t intrI8VecMul(Instance* instance, uint32_t dest, uint32_t src1,
                               uint32_t src2, uint32_t len, uint8_t* memBase);
