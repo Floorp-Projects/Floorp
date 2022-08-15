e24e779171c0c403c144848c3c4f4e8b4940edb2
Paul Bone <pbone@mozilla.com>
2022-08-15 03:48:47 +0000

    Bug 1781728 - Clamp WebGL mipmap levels r=jgilbert
    Differential Revision: https://phabricator.services.mozilla.com/D154524

--- dom/canvas/WebGLTexture.cpp
+++ dom/canvas/WebGLTexture.cpp
@@ -670,15 +670,6 @@ static R Clamp(const T val, const R min, const R max) {
   return static_cast<R>(val);
 }
 
-template <typename T, typename A, typename B>
-static void ClampSelf(T* const out, const A min, const B max) {
-  if (*out < min) {
-    *out = T{min};
-  } else if (*out > max) {
-    *out = T{max};
-  }
-}
-
 void WebGLTexture::ClampLevelBaseAndMax() {
   if (!mImmutable) return;
 
@@ -687,8 +678,18 @@ void WebGLTexture::ClampLevelBaseAndMax() {
   //  `[0, levels-1]`, `level_max` is then clamped to the range `
   //  `[level_base, levels-1]`, where `levels` is the parameter passed to
   //   TexStorage* for the texture object."
-  ClampSelf(&mBaseMipmapLevel, 0u, mImmutableLevelCount - 1u);
-  ClampSelf(&mMaxMipmapLevel, mBaseMipmapLevel, mImmutableLevelCount - 1u);
+  MOZ_ASSERT(mImmutableLevelCount > 0);
+  uint32_t new_base = Clamp(mBaseMipmapLevel, 0u, mImmutableLevelCount - 1u);
+  uint32_t new_max =
+      Clamp(mMaxMipmapLevel, new_base, mImmutableLevelCount - 1u);
+  if (new_base != mBaseMipmapLevel) {
+    mBaseMipmapLevelDirty = true;
+  }
+  mBaseMipmapLevel = new_base;
+  if (new_max != mMaxMipmapLevel) {
+    mMaxMipmapLevelDirty = true;
+  }
+  mMaxMipmapLevel = new_max;
 
   // Note: This means that immutable textures are *always* texture-complete!
 }
@@ -727,6 +728,10 @@ bool WebGLTexture::BindTexture(TexTarget texTarget) {
   }
 
   return true;
+}
+
+static constexpr GLint ClampMipmapLevelForDriver(uint32_t level) {
+  return Clamp(level, uint8_t{0}, WebGLTexture::kMaxLevelCount);
 }
 
 void WebGLTexture::GenerateMipmap() {
@@ -806,21 +811,33 @@ void WebGLTexture::GenerateMipmap() {
   gl::GLContext* gl = mContext->gl;
 
   if (gl->WorkAroundDriverBugs()) {
-    // Don't generate mipmaps when the base level is out of range:
-    if (!(mImmutable && mBaseMipmapLevel >= mImmutableLevelCount)) {
-      // bug 696495 - to work around failures in the texture-mips.html test on
-      // various drivers, we set the minification filter before calling
-      // glGenerateMipmap. This should not carry a significant performance
-      // overhead so we do it unconditionally.
-      //
-      // note that the choice of GL_NEAREST_MIPMAP_NEAREST really matters. See
-      // Chromium bug 101105.
-      gl->fTexParameteri(mTarget.get(), LOCAL_GL_TEXTURE_MIN_FILTER,
-                         LOCAL_GL_NEAREST_MIPMAP_NEAREST);
-      gl->fGenerateMipmap(mTarget.get());
-      gl->fTexParameteri(mTarget.get(), LOCAL_GL_TEXTURE_MIN_FILTER,
-                         mSamplingState.minFilter.get());
+    // If we first set GL_TEXTURE_BASE_LEVEL to a number such as 20, then set
+    // MGL_TEXTURE_MAX_LEVEL to a smaller number like 8, our copy of the base
+    // level will be lowered, but we do not update the driver until now, before
+    // calling glGenerateMipmap().
+    if (mBaseMipmapLevelDirty) {
+      gl->fTexParameteri(mTarget.get(), LOCAL_GL_TEXTURE_BASE_LEVEL,
+                         ClampMipmapLevelForDriver(mBaseMipmapLevel));
+      mBaseMipmapLevelDirty = false;
     }
+    if (mMaxMipmapLevelDirty) {
+      gl->fTexParameteri(mTarget.get(), LOCAL_GL_TEXTURE_MAX_LEVEL,
+                         ClampMipmapLevelForDriver(mMaxMipmapLevel));
+      mMaxMipmapLevelDirty = false;
+    }
+
+    // bug 696495 - to work around failures in the texture-mips.html test on
+    // various drivers, we set the minification filter before calling
+    // glGenerateMipmap. This should not carry a significant performance
+    // overhead so we do it unconditionally.
+    //
+    // note that the choice of GL_NEAREST_MIPMAP_NEAREST really matters. See
+    // Chromium bug 101105.
+    gl->fTexParameteri(mTarget.get(), LOCAL_GL_TEXTURE_MIN_FILTER,
+                       LOCAL_GL_NEAREST_MIPMAP_NEAREST);
+    gl->fGenerateMipmap(mTarget.get());
+    gl->fTexParameteri(mTarget.get(), LOCAL_GL_TEXTURE_MIN_FILTER,
+                       mSamplingState.minFilter.get());
   } else {
     gl->fGenerateMipmap(mTarget.get());
   }
@@ -1032,17 +1049,16 @@ void WebGLTexture::TexParameter(TexTarget texTarget, GLenum pname,
     case LOCAL_GL_TEXTURE_BASE_LEVEL: {
       mBaseMipmapLevel = clamped.i;
       ClampLevelBaseAndMax();
-      const auto forDriver =
-          Clamp(mBaseMipmapLevel, uint8_t{0}, kMaxLevelCount);
-      clamped = FloatOrInt(forDriver);
+      clamped = FloatOrInt(ClampMipmapLevelForDriver(mBaseMipmapLevel));
+      mBaseMipmapLevelDirty = false;
       break;
     }
 
     case LOCAL_GL_TEXTURE_MAX_LEVEL: {
       mMaxMipmapLevel = clamped.i;
       ClampLevelBaseAndMax();
-      const auto forDriver = Clamp(mMaxMipmapLevel, uint8_t{0}, kMaxLevelCount);
-      clamped = FloatOrInt(forDriver);
+      clamped = FloatOrInt(ClampMipmapLevelForDriver(mMaxMipmapLevel));
+      mMaxMipmapLevelDirty = false;
       break;
     }
 
