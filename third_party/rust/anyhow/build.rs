5c118ab478d160a5734d660d64b08d321a01ce4e
Mike Hommey <mh+mozilla@glandium.org>
2022-08-16 20:30:27 +0000

    Bug 1785002 - Update anyhow to 1.0.61. r=emilio,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D154712

--- third_party/rust/anyhow/build.rs
+++ third_party/rust/anyhow/build.rs
@@ -65,13 +65,15 @@ fn main() {
 }
 
 fn compile_probe() -> Option<ExitStatus> {
+    println!("cargo:rerun-if-env-changed=RUSTC_WRAPPER");
+
     let rustc = env::var_os("RUSTC")?;
     let out_dir = env::var_os("OUT_DIR")?;
     let probefile = Path::new(&out_dir).join("probe.rs");
     fs::write(&probefile, PROBE).ok()?;
 
     // Make sure to pick up Cargo rustc configuration.
-    let mut cmd = if let Some(wrapper) = env::var_os("CARGO_RUSTC_WRAPPER") {
+    let mut cmd = if let Some(wrapper) = env::var_os("RUSTC_WRAPPER") {
         let mut cmd = Command::new(wrapper);
         // The wrapper's first argument is supposed to be the path to rustc.
         cmd.arg(rustc);
@@ -88,6 +90,10 @@ fn compile_probe() -> Option<ExitStatus> {
         .arg("--out-dir")
         .arg(out_dir)
         .arg(probefile);
+
+    if let Some(target) = env::var_os("TARGET") {
+        cmd.arg("--target").arg(target);
+    }
 
     // If Cargo wants to set RUSTFLAGS, use that.
     if let Ok(rustflags) = env::var("CARGO_ENCODED_RUSTFLAGS") {
