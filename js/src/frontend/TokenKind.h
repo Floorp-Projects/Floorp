6488fa7963959570f6eb33d52692e9b177ca34ec
Dan Minor <dminor@mozilla.com>
2022-08-16 12:54:41 +0000

    Bug 1783738 - Make 'accessor' a contextual keyword; r=mgaudet
    This should be a contextual keyword to avoid conflicting with existing
    uses of the identifier. It's inserted out of alphabetical order to
    avoid having to make the range conditional on whether ENABLE_DECORATORS
    is defined.
    
    Differential Revision: https://phabricator.services.mozilla.com/D154188

--- js/src/frontend/TokenKind.h
+++ js/src/frontend/TokenKind.h
@@ -123,13 +123,14 @@
   MACRO(Import, "keyword 'import'")                                    \
   MACRO(Class, "keyword 'class'")                                      \
   MACRO(Extends, "keyword 'extends'")                                  \
-  IF_DECORATORS(MACRO(Accessor, "keyword 'accessor'"))                 \
   MACRO(Super, "keyword 'super'")                                      \
   RANGE(KeywordLast, Super)                                            \
                                                                        \
   /* contextual keywords */                                            \
   MACRO(As, "'as'")                                                    \
   RANGE(ContextualKeywordFirst, As)                                    \
+  /* TODO: Move to alphabetical order when IF_DECORATORS is removed */ \
+  IF_DECORATORS(MACRO(Accessor, "'accessor'"))                         \
   MACRO(Assert, "'assert'")                                            \
   MACRO(Async, "'async'")                                              \
   MACRO(Await, "'await'")                                              \
