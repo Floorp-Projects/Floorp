d6e685317865855a5173b683f2d45ccb9621a2fb
Mike Hommey <mh+mozilla@glandium.org>
2022-08-16 21:47:43 +0000

    Bug 1785002 - Force update hashbrown to 0.12.3. r=emilio,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D154762

--- third_party/rust/hashbrown/src/lib.rs
+++ third_party/rust/hashbrown/src/lib.rs
@@ -21,7 +21,8 @@
         allocator_api,
         slice_ptr_get,
         nonnull_slice_from_raw_parts,
-        maybe_uninit_array_assume_init
+        maybe_uninit_array_assume_init,
+        build_hasher_simple_hash_one
     )
 )]
 #![allow(
@@ -30,7 +31,9 @@
     clippy::must_use_candidate,
     clippy::option_if_let_else,
     clippy::redundant_else,
-    clippy::manual_map
+    clippy::manual_map,
+    clippy::missing_safety_doc,
+    clippy::missing_errors_doc
 )]
 #![warn(missing_docs)]
 #![warn(rust_2018_idioms)]
@@ -126,20 +129,6 @@ pub enum TryReserveError {
         /// The layout of the allocation request that failed.
         layout: alloc::alloc::Layout,
     },
-}
-
-/// The error type for [`RawTable::get_each_mut`](crate::raw::RawTable::get_each_mut),
-/// [`HashMap::get_each_mut`], and [`HashMap::get_each_key_value_mut`].
-#[cfg(feature = "nightly")]
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub enum UnavailableMutError {
-    /// The requested entry is not present in the table.
-    Absent,
-    /// The requested entry is present, but a mutable reference to it was already created and
-    /// returned from this call to `get_each_mut` or `get_each_key_value_mut`.
-    ///
-    /// Includes the index of the existing mutable reference in the returned array.
-    Duplicate(usize),
 }
 
 /// Wrapper around `Bump` which allows it to be used as an allocator for
