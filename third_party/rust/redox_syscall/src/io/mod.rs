f8658f7955b1d89e7e293d0381f168c659df5fae
Mike Hommey <mh+mozilla@glandium.org>
2022-08-16 21:01:51 +0000

    Bug 1785002 - Update redox_syscall to 0.2.16. r=emilio,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D154740

--- third_party/rust/redox_syscall/src/io/mod.rs
+++ third_party/rust/redox_syscall/src/io/mod.rs
@@ -4,12 +4,12 @@ pub use self::dma::*;
 pub use self::io::*;
 pub use self::mmio::*;
 
-#[cfg(target_arch = "x86_64")]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub use self::pio::*;
 
 mod dma;
 mod io;
 mod mmio;
 
-#[cfg(target_arch = "x86_64")]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod pio;
