200923a5c83f89956809236752d4850c0a947478
Jesse Schwartzentruber <truber@mozilla.com>
2022-08-16 08:57:21 +0000

    Bug 1784495 - Switch AFL toolchain to original Nyx toolchain. r=decoder,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D154555

--- taskcluster/scripts/misc/build-afl.sh
+++ taskcluster/scripts/misc/build-afl.sh
@@ -3,17 +3,17 @@
 set -e -x
 
 artifact=$(basename "$TOOLCHAIN_ARTIFACT")
-dir=${artifact%.tar.*}
+dir="${artifact%.tar.*}"
 scripts="$(realpath "${0%/*}")"
 
-cd "$MOZ_FETCHES_DIR/AFLplusplus"
-make -f GNUmakefile afl-showmap \
+cd "$MOZ_FETCHES_DIR/AFL"
+patch -p1 -i "$scripts/afl-nyx.patch"
+make afl-showmap \
     CC="$MOZ_FETCHES_DIR/clang/bin/clang"
-make -f GNUmakefile.llvm install \
-    DESTDIR="$dir" \
+make -C llvm_mode install \
+    DESTDIR="../$dir" \
     PREFIX=/ \
     LLVM_CONFIG="$MOZ_FETCHES_DIR/clang/bin/llvm-config"
-rm -rf "$dir/share"
 
 tar caf "$artifact" "$dir"
 
