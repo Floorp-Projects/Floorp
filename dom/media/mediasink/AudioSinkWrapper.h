afe87eb3ef866cdd3a007d96e6f259edde41d2b6
Cristian Tuns <ctuns@mozilla.com>
2022-08-16 08:45:40 -0400

    Backed out 2 changesets (bug 1780150) for causing build bustages on AudioSinkWrapper.h CLOSED TREE
    Backed out changeset d060eced6ecf (bug 1780150)
    Backed out changeset 9c2c6af6a7cf (bug 1780150)

--- dom/media/mediasink/AudioSinkWrapper.h
+++ dom/media/mediasink/AudioSinkWrapper.h
@@ -141,7 +141,6 @@ class AudioSinkWrapper : public MediaSink {
   bool mAudioEnded;
   MozPromiseRequestHolder<EndedPromise> mAudioSinkEndedPromise;
   MediaQueue<AudioData>& mAudioQueue;
-  Maybe<TimeUnit> mLastPacketEndTime;
 };
 
 }  // namespace mozilla
