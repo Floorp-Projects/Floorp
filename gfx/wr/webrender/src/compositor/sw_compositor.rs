c4c5bc5245ea11cfcda0dfc71a9e642dd7812398
Jamie Nicol <jnicol@mozilla.com>
2022-08-17 09:58:51 +0000

    Bug 1784051 - Delete SwCompositor's shared depth texture when final surface is destroyed. r=lsalzman
    And lazily create it when first surface is created. This prevents us
    unnecessarily holding on to that memory when a compositor has no
    surfaces, ie when the tab is backgrounded on Android. While each depth
    texture may only be a few megabytes, this quickly adds up on Android
    where each tab has its own compositor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D154636

--- gfx/wr/webrender/src/compositor/sw_compositor.rs
+++ gfx/wr/webrender/src/compositor/sw_compositor.rs
@@ -728,7 +728,10 @@ pub struct SwCompositor {
     /// This depth texture must be big enough to accommodate the largest used
     /// tile size for any surface. The maximum requested tile size is tracked
     /// to ensure that this depth texture is at least that big.
-    depth_id: u32,
+    /// This is initialized when the first surface is created and freed when
+    /// the last surface is destroyed, to ensure compositors with no surfaces
+    /// are not holding on to extra memory.
+    depth_id: Option<u32>,
     /// Instance of the SwComposite thread, only created if we are not relying
     /// on a native RenderCompositor.
     composite_thread: Option<Arc<SwCompositeThread>>,
@@ -744,7 +747,6 @@ impl SwCompositor {
         compositor: Box<dyn MappableCompositor>,
         use_native_compositor: bool,
     ) -> Self {
-        let depth_id = gl.gen_textures(1)[0];
         // Only create the SwComposite thread if we're not using a native render
         // compositor. Thus, we are compositing into the main software framebuffer,
         // which benefits from compositing asynchronously while updating tiles.
@@ -766,7 +768,7 @@ impl SwCompositor {
                 y: 0,
             },
             max_tile_size: DeviceIntSize::zero(),
-            depth_id,
+            depth_id: None,
             composite_thread,
             locked_framebuffer: None,
             is_compositing: false,
@@ -1155,6 +1157,9 @@ impl Compositor for SwCompositor {
             self.max_tile_size.width.max(tile_size.width),
             self.max_tile_size.height.max(tile_size.height),
         );
+        if self.depth_id.is_none() {
+            self.depth_id = Some(self.gl.gen_textures(1)[0]);
+        }
         self.surfaces.insert(id, SwSurface::new(tile_size, is_opaque));
     }
 
@@ -1177,6 +1182,11 @@ impl Compositor for SwCompositor {
         if self.use_native_compositor {
             self.compositor.destroy_surface(id);
         }
+        if self.surfaces.is_empty() {
+            if let Some(depth_id) = self.depth_id.take() {
+                self.gl.delete_textures(&[depth_id]);
+            }
+        }
     }
 
     fn deinit(&mut self) {
@@ -1188,7 +1198,9 @@ impl Compositor for SwCompositor {
             self.deinit_surface(surface);
         }
 
-        self.gl.delete_textures(&[self.depth_id]);
+        if let Some(depth_id) = self.depth_id.take() {
+            self.gl.delete_textures(&[depth_id]);
+        }
 
         if self.use_native_compositor {
             self.compositor.deinit();
@@ -1219,7 +1231,7 @@ impl Compositor for SwCompositor {
                 gl::DRAW_FRAMEBUFFER,
                 gl::DEPTH_ATTACHMENT,
                 gl::TEXTURE_2D,
-                self.depth_id,
+                self.depth_id.expect("depth texture should be initialized"),
                 0,
             );
             self.gl.bind_framebuffer(gl::DRAW_FRAMEBUFFER, prev_fbo[0] as gl::GLuint);
@@ -1310,7 +1322,7 @@ impl Compositor for SwCompositor {
                 // last time it was supplied, instead simply reusing the buffer if the max
                 // tile size is not bigger than what was previously allocated.
                 self.gl.set_texture_buffer(
-                    self.depth_id,
+                    self.depth_id.expect("depth texture should be initialized"),
                     gl::DEPTH_COMPONENT,
                     valid_rect.width(),
                     valid_rect.height(),
