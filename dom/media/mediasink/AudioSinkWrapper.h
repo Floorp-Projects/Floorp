1e9b3850e336a256362a7f1657f6da47590689d0
Paul Adenot <paul@paul.cx>
2022-08-17 13:05:36 +0000

    Bug 1780150 - Cap currentTime to the end time of the last packet when a video with an audio track is muted. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D152405

--- dom/media/mediasink/AudioSinkWrapper.h
+++ dom/media/mediasink/AudioSinkWrapper.h
@@ -141,6 +141,7 @@ class AudioSinkWrapper : public MediaSink {
   bool mAudioEnded;
   MozPromiseRequestHolder<EndedPromise> mAudioSinkEndedPromise;
   MediaQueue<AudioData>& mAudioQueue;
+  Maybe<media::TimeUnit> mLastPacketEndTime;
 };
 
 }  // namespace mozilla
