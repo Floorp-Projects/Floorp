6c14dd11837bb574a82578c81889feb842cef297
Paul Adenot <paul@paul.cx>
2022-08-17 16:29:33 +0000

    Bug 1765480 - Conditionally include bsf, codec and parser list with CONFIG_* macros. r=alwu
    Depends on D150973
    
    Differential Revision: https://phabricator.services.mozilla.com/D151996

--- media/ffvpx/libavcodec/codec_list.c
+++ media/ffvpx/libavcodec/codec_list.c
@@ -1,6 +1,20 @@
 static const FFCodec * const codec_list[] = {
+#if CONFIG_VP8_DECODER
     &ff_vp8_decoder,
+#endif
+#if CONFIG_VP9_DECODER
     &ff_vp9_decoder,
+#endif
+#if CONFIG_FLAC_DECODER
     &ff_flac_decoder,
+#endif
+#if CONFIG_MP3_DECODER
     &ff_mp3_decoder,
+#endif
+#if CONFIG_LIBDAV1D
+    &ff_libdav1d_decoder,
+#endif
+#if CONFIG_AV1_DECODER
+    &ff_av1_decoder,
+#endif
     NULL };
