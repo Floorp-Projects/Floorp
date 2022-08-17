e874573e31f049a4c7c00b096b6c044a38ad2d0d
Cosmin Sabou <csabou@mozilla.com>
2022-08-17 15:55:57 +0300

    Backed out changeset b7a953f0120a (bug 784513) for landing with the wrong bug number.

--- js/src/jit-test/tests/decorators/syntax.js
+++ js/src/jit-test/tests/decorators/syntax.js
@@ -18,15 +18,6 @@ Reflect.parse("@dec1('a') @(() => {}) @dec3 class c {@((a, b, c) => {}) @dec2('a
 Reflect.parse("x = @dec class { #x }");
 Reflect.parse("x = (class A { }, @dec class { })");
 Reflect.parse("@dec1 class A extends @dec2 class B extends @dec3 class {} {} {}");
-Reflect.parse("class c {@dec1.dec2.dec3 method() {};}");
-Reflect.parse("class c {@dec1 @dec2.dec3.dec4 @dec5 method() {};}");
-Reflect.parse("class c {@dec1('a') @dec2.dec3.dec4 @dec5 method() {};}");
-Reflect.parse("class c {@dec1('a') @dec2.dec3.dec4('b', 'c') @dec5 method() {};}");
-Reflect.parse("class c {@dec1('a') @dec2.dec3.dec4('b', 'c') @dec5.dec6 method() {};}");
-Reflect.parse("@dec1.dec2 class c {}");
-Reflect.parse("@dec1.dec2 @dec3.dec4 @dec5 class c {}");
-Reflect.parse("@dec1.dec2('a') @(() => {}) @dec4 class c {}");
-Reflect.parse("@dec1.dec2('a') @(() => {}) @dec4 class c {@((a, b, c) => {}) @dec5.dec6('a', 'b') @dec7 method(a, b) {};}");
 
 assertThrowsInstanceOf(() => Reflect.parse("class c {@ method() {};}"), SyntaxError);
 assertThrowsInstanceOf(() => Reflect.parse("class c {@((a, b, c => {}) method(a, b) {};}"), SyntaxError);
@@ -36,6 +27,5 @@ assertThrowsInstanceOf(() => Reflect.parse("@dec1 f(a) {}"), SyntaxError);
 assertThrowsInstanceOf(() => Reflect.parse("@dec1 () => {}"), SyntaxError);
 assertThrowsInstanceOf(() => Reflect.parse("@class class { x; }"), SyntaxError);
 assertThrowsInstanceOf(() => Reflect.parse("@for class { x; }"), SyntaxError);
-assertThrowsInstanceOf(() => Reflect.parse("class c {@dec1. method() {};}"), SyntaxError);
-assertThrowsInstanceOf(() => Reflect.parse("@dec1. class c {method() {};}"), SyntaxError);
-assertThrowsInstanceOf(() => Reflect.parse("@dec1.(a) class c {method() {};}"), SyntaxError);
+
+if (typeof reportCompare === "function") reportCompare(0, 0);
