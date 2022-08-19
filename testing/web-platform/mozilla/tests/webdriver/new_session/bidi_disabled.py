0962fc7a80f22e3cd5dca672ce7ec62e563f20be
Henrik Skupin <mail@hskupin.info>
2022-08-19 18:48:48 +0000

    Bug 1769994 - [wdspec] Re-enable WebDriver BiDi after CDP-only test. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D154148

--- testing/web-platform/mozilla/tests/webdriver/new_session/bidi_disabled.py
+++ testing/web-platform/mozilla/tests/webdriver/new_session/bidi_disabled.py
@@ -9,11 +9,25 @@ def test_marionette_fallback_webdriver_session(configuration, geckodriver):
     prefs.update({"remote.active-protocols": 2})
     config["capabilities"]["moz:firefoxOptions"]["prefs"] = prefs
 
-    driver = geckodriver(config=config)
-    driver.new_session()
+    try:
+        driver = geckodriver(config=config)
+        driver.new_session()
 
-    assert driver.session.capabilities.get("webSocketUrl") is None
+        assert driver.session.capabilities.get("webSocketUrl") is None
 
-    # Sanity check that Marionette works as expected and by default returns
-    # at least one window handle
-    assert len(driver.session.handles) >= 1
+        # Sanity check that Marionette works as expected and by default returns
+        # at least one window handle
+        assert len(driver.session.handles) >= 1
+
+    finally:
+        driver.stop()
+
+        # WebDriver BiDi has to be re-enabled. Because we cannot easily
+        # get rid of the value let geckodriver overwrite it with the current
+        # default.
+        prefs.update({"remote.active-protocols": 3})
+
+        driver = geckodriver(config=config)
+        driver.new_session()
+
+        assert driver.session.capabilities.get("webSocketUrl") is not None
