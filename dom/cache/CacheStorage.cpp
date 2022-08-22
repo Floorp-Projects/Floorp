8c2824e47af1c730ae5f1455dbf4621cf3387762
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-08-22 20:48:34 +0000

    Bug 1784590 - Part 2: Add use counters for unsupported APIs in PBM r=asuth
    Depends on D154578
    
    Differential Revision: https://phabricator.services.mozilla.com/D154579

--- dom/cache/CacheStorage.cpp
+++ dom/cache/CacheStorage.cpp
@@ -301,7 +301,8 @@ already_AddRefed<Promise> CacheStorage::Match(
     const MultiCacheQueryOptions& aOptions, ErrorResult& aRv) {
   NS_ASSERT_OWNINGTHREAD(CacheStorage);
 
-  if (!HasStorageAccess()) {
+  if (!HasStorageAccess(eUseCounter_custom_PrivateBrowsingCachesMatch,
+                        UseCounterWorker::Custom_PrivateBrowsingCachesMatch)) {
     aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
     return nullptr;
   }
@@ -339,7 +340,8 @@ already_AddRefed<Promise> CacheStorage::Has(const nsAString& aKey,
                                             ErrorResult& aRv) {
   NS_ASSERT_OWNINGTHREAD(CacheStorage);
 
-  if (!HasStorageAccess()) {
+  if (!HasStorageAccess(eUseCounter_custom_PrivateBrowsingCachesHas,
+                        UseCounterWorker::Custom_PrivateBrowsingCachesHas)) {
     aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
     return nullptr;
   }
@@ -367,7 +369,8 @@ already_AddRefed<Promise> CacheStorage::Open(const nsAString& aKey,
                                              ErrorResult& aRv) {
   NS_ASSERT_OWNINGTHREAD(CacheStorage);
 
-  if (!HasStorageAccess()) {
+  if (!HasStorageAccess(eUseCounter_custom_PrivateBrowsingCachesOpen,
+                        UseCounterWorker::Custom_PrivateBrowsingCachesOpen)) {
     aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
     return nullptr;
   }
@@ -395,7 +398,8 @@ already_AddRefed<Promise> CacheStorage::Delete(const nsAString& aKey,
                                                ErrorResult& aRv) {
   NS_ASSERT_OWNINGTHREAD(CacheStorage);
 
-  if (!HasStorageAccess()) {
+  if (!HasStorageAccess(eUseCounter_custom_PrivateBrowsingCachesDelete,
+                        UseCounterWorker::Custom_PrivateBrowsingCachesDelete)) {
     aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
     return nullptr;
   }
@@ -422,7 +426,8 @@ already_AddRefed<Promise> CacheStorage::Delete(const nsAString& aKey,
 already_AddRefed<Promise> CacheStorage::Keys(ErrorResult& aRv) {
   NS_ASSERT_OWNINGTHREAD(CacheStorage);
 
-  if (!HasStorageAccess()) {
+  if (!HasStorageAccess(eUseCounter_custom_PrivateBrowsingCachesKeys,
+                        UseCounterWorker::Custom_PrivateBrowsingCachesKeys)) {
     aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
     return nullptr;
   }
@@ -553,13 +558,21 @@ OpenMode CacheStorage::GetOpenMode() const {
   return mNamespace == CHROME_ONLY_NAMESPACE ? OpenMode::Eager : OpenMode::Lazy;
 }
 
-bool CacheStorage::HasStorageAccess() const {
+bool CacheStorage::HasStorageAccess(UseCounter aLabel,
+                                    UseCounterWorker aLabelWorker) const {
   NS_ASSERT_OWNINGTHREAD(CacheStorage);
   if (NS_WARN_IF(!mGlobal)) {
     return false;
   }
 
   StorageAccess access = mGlobal->GetStorageAccess();
+  if (access == StorageAccess::ePrivateBrowsing) {
+    if (NS_IsMainThread()) {
+      SetUseCounter(mGlobal->GetGlobalJSObject(), aLabel);
+    } else {
+      SetUseCounter(aLabelWorker);
+    }
+  }
   return access > StorageAccess::ePrivateBrowsing ||
          (StaticPrefs::
               privacy_partition_always_partition_third_party_non_cookie_storage() &&
