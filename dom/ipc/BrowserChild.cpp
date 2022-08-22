5edc9f7783b8cd7ad271a97acc70f49dc52facb0
Nika Layzell <nika@thelayzells.com>
2022-08-22 15:38:14 +0000

    Bug 1781129 - Part 1: Use BigBuffer for IPCDataTransfer, r=edgar
    The IPCDataTransfer type is used to transfer Clipboard/Drag & Drop payloads
    over IPC to allow them to be written to or read from the relevant system
    interfaces. Previously, the system which was used was somewhat complex, and
    tried to use Shmem in some cases to store buffers out of line. Now that
    BigBuffer is available, it can be simplified substantially.
    
    In addition, this change removed the memory buffer overload of GetSurfaceData,
    as the only consumer was using it to immediately send the payload over IPC as a
    nsCString. It was changed to instead use `BigBuffer` as that is more efficient
    in a large buffer situation, and reduces the number of required copies.
    
    Differential Revision: https://phabricator.services.mozilla.com/D151852

--- dom/ipc/BrowserChild.cpp
+++ dom/ipc/BrowserChild.cpp
@@ -2253,7 +2253,7 @@ mozilla::ipc::IPCResult BrowserChild::RecvPasteTransferable(
 
   rv = nsContentUtils::IPCTransferableToTransferable(
       aDataTransfer, aIsPrivateData, aRequestingPrincipal, aContentPolicyType,
-      true /* aAddDataFlavor */, trans, this);
+      true /* aAddDataFlavor */, trans);
   NS_ENSURE_SUCCESS(rv, IPC_OK());
 
   nsCOMPtr<nsIDocShell> ourDocShell = do_GetInterface(WebNavigation());
