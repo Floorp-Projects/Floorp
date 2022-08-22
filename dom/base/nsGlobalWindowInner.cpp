5170f947e349cfbe9b6ec77d1ff7a8f2c50b4f2c
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-08-22 18:48:12 +0000

    Bug 1784590 - Part 1: Remove hide_in_pbmode prefs r=asuth
    It turns out that websites break with different reasons when hiding things. At this point we want to stop revising the hack further and instead gather the data about how many websites are currently affected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D154578

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -3290,14 +3290,6 @@ bool nsGlobalWindowInner::CachesEnabled(JSContext* aCx, JSObject*) {
   if (!StaticPrefs::dom_caches_enabled()) {
     return false;
   }
-  if (StaticPrefs::dom_caches_hide_in_pbmode_enabled()) {
-    if (const nsCOMPtr<nsIGlobalObject> global =
-            xpc::CurrentNativeGlobal(aCx)) {
-      if (global->GetStorageAccess() == StorageAccess::ePrivateBrowsing) {
-        return false;
-      }
-    }
-  }
   if (!JS::GetIsSecureContext(js::GetContextRealm(aCx))) {
     return StaticPrefs::dom_caches_testing_enabled() ||
            StaticPrefs::dom_serviceWorkers_testing_enabled();
@@ -5099,12 +5091,6 @@ Storage* nsGlobalWindowInner::GetLocalStorage(ErrorResult& aError) {
 
 IDBFactory* nsGlobalWindowInner::GetIndexedDB(JSContext* aCx,
                                               ErrorResult& aError) {
-  if (!IDBFactory::IsEnabled(aCx, AsGlobal()->GetGlobalJSObject())) {
-    // Let window.indexedDB be an attribute with a null value, to prevent
-    // undefined identifier error
-    return nullptr;
-  }
-
   if (!mIndexedDB) {
     // This may keep mIndexedDB null without setting an error.
     auto res = IDBFactory::CreateForWindow(this);
