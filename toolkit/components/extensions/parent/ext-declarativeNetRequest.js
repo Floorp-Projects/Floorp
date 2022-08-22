11eb482bec47a14d97e9584d0ecb6b66e340e1cd
Rob Wu <rob@robwu.nl>
2022-08-22 20:44:26 +0000

    Bug 1745762 - Add registration of DNR session rules r=rpl
    This patch offers the Rule type and updateSessionRules and
    getSessionRules to register rules. The actual evaluation of rules and
    most of the associated validation is not part of this patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D154801

--- toolkit/components/extensions/parent/ext-declarativeNetRequest.js
+++ toolkit/components/extensions/parent/ext-declarativeNetRequest.js
@@ -6,10 +6,47 @@
 
 "use strict";
 
+ChromeUtils.defineESModuleGetters(this, {
+  ExtensionDNR: "resource://gre/modules/ExtensionDNR.sys.mjs",
+});
+
+var { ExtensionError } = ExtensionUtils;
+
 this.declarativeNetRequest = class extends ExtensionAPI {
+  onShutdown() {
+    ExtensionDNR.clearRuleManager(this.extension);
+  }
   getAPI(context) {
+    const { extension } = this;
+
     return {
       declarativeNetRequest: {
+        updateSessionRules({ removeRuleIds, addRules }) {
+          const ruleManager = ExtensionDNR.getRuleManager(extension);
+          let ruleValidator = new ExtensionDNR.RuleValidator(
+            ruleManager.getSessionRules()
+          );
+          if (removeRuleIds) {
+            ruleValidator.removeRuleIds(removeRuleIds);
+          }
+          if (addRules) {
+            ruleValidator.addRules(addRules);
+          }
+          let failures = ruleValidator.getFailures();
+          if (failures.length) {
+            throw new ExtensionError(failures[0].message);
+          }
+          ruleManager.setSessionRules(ruleValidator.getValidatedRules());
+        },
+
+        getSessionRules() {
+          // ruleManager.getSessionRules() returns an array of Rule instances.
+          // When these are structurally cloned (to send them to the child),
+          // the enumerable public fields of the class instances are copied to
+          // plain objects, as desired.
+          return ExtensionDNR.getRuleManager(extension).getSessionRules();
+        },
+
         async testMatchOutcome(request) {
           // TODO bug 1745758: Implement rule evaluation engine.
           // Since rule registration has not been implemented yet, the result
