5555b317dc40c6843df3f8fd8fead833ecfa9768
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-08-22 18:48:12 +0000

    Bug 1784590 - Part 2: Add use counters for unsupported APIs in PBM r=asuth
    Depends on D154578
    
    Differential Revision: https://phabricator.services.mozilla.com/D154579

--- dom/cache/CacheStorage.h
+++ dom/cache/CacheStorage.h
@@ -95,7 +95,7 @@ class CacheStorage final : public nsISupports,
 
   OpenMode GetOpenMode() const;
 
-  bool HasStorageAccess() const;
+  bool HasStorageAccess(UseCounter aLabel, UseCounterWorker aLabelWorker) const;
 
   const Namespace mNamespace;
   nsCOMPtr<nsIGlobalObject> mGlobal;
