3f2e9ceb1ca33f44c8a463409629d16d9fc74c3a
Mike Hommey <mh+mozilla@glandium.org>
2022-08-23 23:55:47 +0000

    Bug 1785002 - Update fs-err to 2.8.1. r=emilio,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D155327

--- third_party/rust/fs-err/src/file.rs
+++ third_party/rust/fs-err/src/file.rs
@@ -284,6 +284,25 @@ mod unix {
                 .map_err(|err| self.error(err, ErrorKind::WriteAt))
         }
     }
+
+    #[cfg(feature = "io_safety")]
+    mod io_safety {
+        use std::os::unix::io::{AsFd, BorrowedFd, OwnedFd};
+
+        #[cfg_attr(docsrs, doc(cfg(feature = "io_safety")))]
+        impl AsFd for crate::File {
+            fn as_fd(&self) -> BorrowedFd<'_> {
+                self.file().as_fd()
+            }
+        }
+
+        #[cfg_attr(docsrs, doc(cfg(feature = "io_safety")))]
+        impl From<crate::File> for OwnedFd {
+            fn from(file: crate::File) -> Self {
+                file.into_parts().0.into()
+            }
+        }
+    }
 }
 
 #[cfg(windows)]
@@ -323,6 +342,25 @@ mod windows {
     impl IntoRawHandle for crate::File {
         fn into_raw_handle(self) -> RawHandle {
             self.file.into_raw_handle()
+        }
+    }
+
+    #[cfg(feature = "io_safety")]
+    mod io_safety {
+        use std::os::windows::io::{AsHandle, BorrowedHandle, OwnedHandle};
+
+        #[cfg_attr(docsrs, doc(cfg(feature = "io_safety")))]
+        impl AsHandle for crate::File {
+            fn as_handle(&self) -> BorrowedHandle<'_> {
+                self.file().as_handle()
+            }
+        }
+
+        #[cfg_attr(docsrs, doc(cfg(feature = "io_safety")))]
+        impl From<crate::File> for OwnedHandle {
+            fn from(file: crate::File) -> Self {
+                file.into_parts().0.into()
+            }
         }
     }
 }
