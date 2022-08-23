b755efe6a07c52a7a9ebbacc05f80e2bb70b3430
Hannah Peuckmann <hpeuckmann@mozilla.com>
2022-08-23 15:05:36 +0000

    Bug 1609578 - Tests for Not now permission option. r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D154069

--- browser/base/content/test/webrtc/browser_devices_get_user_media_queue_request.js
+++ browser/base/content/test/webrtc/browser_devices_get_user_media_queue_request.js
@@ -8,13 +8,16 @@ const permissionError =
 const badDeviceError =
   "error: NotReadableError: Failed to allocate videosource";
 
+const { webrtcUI } = ChromeUtils.import("resource:///modules/webrtcUI.jsm");
+
 var gTests = [
   {
-    desc: "test queueing deny audio behind allow video",
+    desc: "test 'Not now' label queueing audio twice behind allow video",
     run: async function testQueuingDenyAudioBehindAllowVideo() {
       let observerPromise = expectObserverCalled("getUserMedia:request");
       let promise = promisePopupNotificationShown("webRTC-shareDevices");
       await promiseRequestDevice(false, true);
+      await promiseRequestDevice(true, false);
       await promiseRequestDevice(true, false);
       await promise;
       promise = promisePopupNotificationShown("webRTC-shareDevices");
@@ -28,6 +31,11 @@ var gTests = [
       );
       let observerPromise2 = expectObserverCalled("recording-device-events");
       await promiseMessage("ok", () => {
+        is(
+          PopupNotifications.panel.firstElementChild.secondaryButton.label,
+          "Block",
+          "We offer Block because of no active camera/mic device"
+        );
         PopupNotifications.panel.firstElementChild.button.click();
       });
 
@@ -43,21 +51,115 @@ var gTests = [
       await checkSharingUI({ audio: false, video: true });
 
       await promise;
+      promise = promisePopupNotificationShown("webRTC-shareDevices");
+      await observerPromise;
+      observerPromise = expectObserverCalled("getUserMedia:request");
+      checkDeviceSelectors(["microphone"]);
+
+      observerPromise1 = expectObserverCalled("getUserMedia:response:deny");
+      await promiseMessage(permissionError, () => {
+        is(
+          PopupNotifications.panel.firstElementChild.secondaryButton.label,
+          "Not now",
+          "We offer Not now option because of an allowed camera/mic device"
+        );
+        activateSecondaryAction(kActionDeny);
+      });
+
+      await observerPromise1;
+
+      await promise;
       await observerPromise;
       checkDeviceSelectors(["microphone"]);
 
       observerPromise = expectObserverCalled("getUserMedia:response:deny");
       await promiseMessage(permissionError, () => {
+        is(
+          PopupNotifications.panel.firstElementChild.secondaryButton.label,
+          "Not now",
+          "We offer Not now option again because of an allowed camera/mic device"
+        );
         activateSecondaryAction(kActionDeny);
       });
 
       await observerPromise;
-      SitePermissions.removeFromPrincipal(
-        null,
-        "microphone",
-        gBrowser.selectedBrowser
-      );
 
+      // Clean up the active camera in the activePerms map
+      webrtcUI.activePerms.delete(gBrowser.selectedBrowser.outerWindowID);
+
+      // close all streams
+      await closeStream();
+    },
+  },
+
+  {
+    desc:
+      "test 'Not now'/'Block' label queueing microphone behind screen behind allow camera",
+    run: async function testQueuingAudioAndScreenBehindAllowVideo() {
+      let observerPromise = expectObserverCalled("getUserMedia:request");
+      let promise = promisePopupNotificationShown("webRTC-shareDevices");
+      await promiseRequestDevice(false, true);
+      await promiseRequestDevice(false, true, null, "screen");
+      await promiseRequestDevice(true, false);
+      await promise;
+      promise = promisePopupNotificationShown("webRTC-shareDevices");
+      checkDeviceSelectors(["camera"]);
+      await observerPromise;
+
+      observerPromise = expectObserverCalled("getUserMedia:request");
+      let observerPromise1 = expectObserverCalled(
+        "getUserMedia:response:allow"
+      );
+      let observerPromise2 = expectObserverCalled("recording-device-events");
+      await promiseMessage("ok", () => {
+        is(
+          PopupNotifications.panel.firstElementChild.secondaryButton.label,
+          "Block",
+          "We offer Block because of no active camera/mic device"
+        );
+        PopupNotifications.panel.firstElementChild.button.click();
+      });
+      await observerPromise1;
+      await observerPromise2;
+      Assert.deepEqual(
+        await getMediaCaptureState(),
+        { video: true },
+        "expected camera to be shared"
+      );
+      await checkSharingUI({ audio: false, video: true });
+
+      await promise;
+      promise = promisePopupNotificationShown("webRTC-shareDevices");
+      await observerPromise;
+      observerPromise = expectObserverCalled("getUserMedia:request");
+      checkDeviceSelectors(["screen"]);
+      observerPromise = expectObserverCalled("getUserMedia:response:deny");
+      await promiseMessage(permissionError, () => {
+        is(
+          PopupNotifications.panel.firstElementChild.secondaryButton.label,
+          "Block",
+          "We offer Block option because we are not asking for cam or mic"
+        );
+        activateSecondaryAction(kActionDeny);
+      });
+      await observerPromise;
+
+      await promise;
+      await observerPromise;
+      checkDeviceSelectors(["microphone"]);
+      observerPromise = expectObserverCalled("getUserMedia:response:deny");
+      await promiseMessage(permissionError, () => {
+        is(
+          PopupNotifications.panel.firstElementChild.secondaryButton.label,
+          "Not now",
+          "We offer Not now option because we are asking for mic and cam is already active"
+        );
+        activateSecondaryAction(kActionDeny);
+      });
+      await observerPromise;
+
+      // Clean up
+      webrtcUI.activePerms.delete(gBrowser.selectedBrowser.outerWindowID);
       // close all streams
       await closeStream();
     },
@@ -168,6 +270,9 @@ var gTests = [
       );
       await indicator;
       await checkSharingUI({ audio: true, video: false });
+
+      // Clean up the active microphone in the activePerms map
+      webrtcUI.activePerms.delete(gBrowser.selectedBrowser.outerWindowID);
 
       // close all streams
       await closeStream();
