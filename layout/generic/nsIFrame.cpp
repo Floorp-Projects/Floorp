310b1a4bb11d418f34650da94d3c7fa55d078c40
Martin Robinson <mrobinson@igalia.com>
2022-08-24 13:30:55 +0000

    Bug 1767262 - Lazily layout content that is hidden via `content-visibility: hidden` r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D145949

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -6747,6 +6747,10 @@ void nsIFrame::DidReflow(nsPresContext* aPresContext,
                          const ReflowInput* aReflowInput) {
   NS_FRAME_TRACE(NS_FRAME_TRACE_CALLS, ("nsIFrame::DidReflow"));
 
+  if (GetInFlowParent() && GetInFlowParent()->IsContentHiddenForLayout()) {
+    return;
+  }
+
   SVGObserverUtils::InvalidateDirectRenderingObservers(
       this, SVGObserverUtils::INVALIDATE_REFLOW);
 
@@ -6857,6 +6861,11 @@ bool nsIFrame::IsContentHidden() const {
   }
 
   return IsFrameOfType(nsIFrame::eReplaced) || !StyleDisplay()->IsInlineFlow();
+}
+
+bool nsIFrame::IsContentHiddenForLayout() const {
+  return IsContentHidden() &&
+         !PresShell()->IsForcingLayoutForHiddenContent(this);
 }
 
 bool nsIFrame::AncestorHidesContent() const {
