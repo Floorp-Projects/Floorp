9246a773e0321f309b0b7e513573dccfb7fd4197
Jon Coppeard <jcoppeard@mozilla.com>
2022-08-24 09:52:44 +0000

    Bug 1785772 - Part 3: Optimise checking multiple zone states at once using a bitmask r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D154970

--- js/src/gc/GCAPI.cpp
+++ js/src/gc/GCAPI.cpp
@@ -740,6 +740,8 @@ const char* StateName(JS::Zone::GCState state) {
       return "Finished";
     case JS::Zone::Compact:
       return "Compact";
+    case JS::Zone::Limit:
+      break;
   }
   MOZ_CRASH("Invalid Zone::GCState enum value");
 }
