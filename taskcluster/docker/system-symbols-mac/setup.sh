64bc2380d351ffe5ef47decc652e54df0d9572a1
Gabriele Svelto <gsvelto@mozilla.com>
2022-08-25 09:45:39 +0000

    Bug 1787087 - Remove breakpad's dump_syms from the macOS system symbols scrapers and use the Rust one instead r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D155561

--- taskcluster/docker/system-symbols-mac/setup.sh
+++ taskcluster/docker/system-symbols-mac/setup.sh
@@ -19,20 +19,6 @@ make "$ncpu" dmg-bin hfsplus
 cp dmg/dmg hfs/hfsplus /builds/worker/bin
 strip /builds/worker/bin/dmg /builds/worker/bin/hfsplus
 
-cd $WORK
-git clone --depth=1 --single-branch https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH=$PATH:$PWD/depot_tools
-mkdir breakpad
-cd breakpad
-fetch breakpad
-cd src
-touch README
-./configure
-make "$ncpu" src/tools/mac/dump_syms/dump_syms_mac
-# `make install` is broken because there are two dump_syms binaries.
-cp src/tools/mac/dump_syms/dump_syms_mac /builds/worker/bin
-strip /builds/worker/bin/dump_syms_mac
-
 pip3 install --no-cache-dir git+https://github.com/gabrielesvelto/reposado
 
 python3 /usr/local/bin/repoutil --configure <<EOF
