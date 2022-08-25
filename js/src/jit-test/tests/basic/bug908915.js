63462e3c9e12c5c434f78ff0780d435af360cccf
Butkovits Atila <abutkovits@mozilla.com>
2022-08-25 23:10:01 +0300

    Backed out changeset eb01cc5511fc (bug 1782450) for causing Bug 1787244. CLOSED TREE

--- js/src/jit-test/tests/basic/bug908915.js
+++ js/src/jit-test/tests/basic/bug908915.js
@@ -12,9 +12,6 @@ var ignorelist = {
     'terminate': true,
     'nukeAllCCWs': true,
     'cacheIRHealthReport': true,
-    'getInnerMostEnvironmentObject': true,
-    'getEnclosingEnvironmentObject': true,
-    'getEnvironmentObjectType' : true,
 };
 
 function f(y) {}
