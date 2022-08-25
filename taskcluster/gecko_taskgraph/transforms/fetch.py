61a68feb9dbf8230d828f4f0239b9a120ae83b35
Cosmin Sabou <csabou@mozilla.com>
2022-08-25 07:39:26 +0300

    Backed out 2 changesets (bug 1649987) for causing Bug 1787098, artifact related exceptions. a=backout
    Backed out changeset 77c769965781 (bug 1649987)
    Backed out changeset 971139c802a7 (bug 1649987)

--- taskcluster/gecko_taskgraph/transforms/fetch.py
+++ taskcluster/gecko_taskgraph/transforms/fetch.py
@@ -123,13 +123,13 @@ def make_task(config, jobs):
         if alias:
             attributes["fetch-alias"] = alias
 
-        task_expires = "2 days" if attributes.get("cached_task") is False else expires
-
         task = {
             "attributes": attributes,
             "name": name,
             "description": job["description"],
-            "expires-after": task_expires,
+            "expires-after": "2 days"
+            if attributes.get("cached_task") is False
+            else expires,
             "label": "fetch-%s" % name,
             "run-on-projects": [],
             "treeherder": {
@@ -154,7 +154,6 @@ def make_task(config, jobs):
                         "type": "directory",
                         "name": artifact_prefix,
                         "path": "/builds/worker/artifacts",
-                        "expires-after": task_expires,
                     }
                 ],
             },
--- taskcluster/gecko_taskgraph/transforms/task.py
+++ taskcluster/gecko_taskgraph/transforms/task.py
@@ -26,6 +26,7 @@ from taskgraph.util.schema import (
     taskref_or_string,
     validate_schema,
 )
+from taskgraph.util.time import value_of
 from taskgraph.util.treeherder import split_symbol
 from voluptuous import Any, Required, Optional, Extra, Match, All, NotIn
 
@@ -38,7 +39,6 @@ from gecko_taskgraph.util.partners import get_partners_to_be_published
 from gecko_taskgraph.util.scriptworker import BALROG_ACTIONS, get_release_config
 from gecko_taskgraph.util.signed_artifacts import get_signed_artifacts
 from gecko_taskgraph.util.workertypes import get_worker_type, worker_type_implementation
-from gecko_taskgraph.transforms.job.common import get_expiration
 
 RUN_TASK = os.path.join(GECKO, "taskcluster", "scripts", "run-task")
 
@@ -91,7 +91,6 @@ task_description_schema = Schema(
         # (e.g., "14 days").  Defaults are set based on the project.
         Optional("expires-after"): str,
         Optional("deadline-after"): str,
-        Optional("expiration-policy"): str,
         # custom routes for this task; the default treeherder routes will be added
         # automatically
         Optional("routes"): [str],
@@ -387,7 +386,6 @@ def verify_index(config, index):
                 # name of the produced artifact (root of the names for
                 # type=directory)
                 "name": str,
-                "expires-after": str,
             }
         ],
         # environment variables
@@ -402,7 +400,7 @@ def verify_index(config, index):
         # the exit status code(s) that indicates the caches used by the task
         # should be purged
         Optional("purge-caches-exit-status"): [int],
-        # Whether any artifacts are assigned to this worker
+        # Wether any artifacts are assigned to this worker
         Optional("skip-artifacts"): bool,
     },
 )
@@ -517,20 +515,11 @@ def build_docker_worker_payload(config, task, task_def):
 
     if "artifacts" in worker:
         artifacts = {}
-        expires_policy = get_expiration(
-            config, task.get("expiration-policy", "default")
-        )
         for artifact in worker["artifacts"]:
-            expires = artifact.get("expires-after", expires_policy)
-            # NOTE: this is pure ASCII string comparison, not semantic dates
-            # It just so happens that "1 day" < "1 month" < "1 year" in ASCII
-            # If this format changes at all, we need to refactor this
-            if expires > task_def["expires"]["relative-datestamp"]:
-                expires = task_def["expires"]["relative-datestamp"]
             artifacts[artifact["name"]] = {
                 "path": artifact["path"],
                 "type": artifact["type"],
-                "expires": {"relative-datestamp": expires},
+                "expires": task_def["expires"],  # always expire with the task
             }
         payload["artifacts"] = artifacts
 
@@ -644,7 +633,6 @@ def build_docker_worker_payload(config, task, task_def):
                 "path": str,
                 # if not specified, path is used for artifact name
                 Optional("name"): str,
-                "expires-after": str,
             }
         ],
         # Directories and/or files to be mounted.
@@ -750,13 +738,10 @@ def build_generic_worker_payload(config, task, task_def):
 
     artifacts = []
 
-    expires_policy = get_expiration(config, task.get("expiration-policy", "default"))
     for artifact in worker.get("artifacts", []):
-        expires = artifact.get("expires-after", expires_policy)
         a = {
             "path": artifact["path"],
             "type": artifact["type"],
-            "expires": {"relative-datestamp": expires},
         }
         if "name" in artifact:
             a["name"] = artifact["name"]
@@ -1821,28 +1806,6 @@ def try_task_config_routes(config, tasks):
 
 
 @transforms.add
-def set_task_and_artifact_expiry(config, jobs):
-    """Set the default expiry for tasks and their artifacts.
-
-    These values are read from ci/config.yml
-    """
-    for job in jobs:
-        expires = get_expiration(config, job.get("expiration-policy", "default"))
-        if "expires-after" not in job:
-            job["expires-after"] = expires
-
-        if "artifacts" in job["worker"]:
-            for a in job["worker"]["artifacts"]:
-                # NOTE: There is a small risk here that artifact expiry won't
-                #   match task expiry. So far this has not happened, but we
-                #   should compare them here in the future and set them equal.
-                if "expires-after" not in a:
-                    a["expires-after"] = expires
-
-        yield job
-
-
-@transforms.add
 def build_task(config, tasks):
     for task in tasks:
         level = str(config.params["level"])
@@ -1910,6 +1873,14 @@ def build_task(config, tasks):
                     branch_rev,
                 )
             )
+
+        if "expires-after" in task:
+            if is_try(config.params):
+                delta = value_of(task["expires-after"])
+                if delta.days >= 28:
+                    task["expires-after"] = "28 days"
+        else:
+            task["expires-after"] = "28 days" if is_try(config.params) else "1 year"
 
         if "deadline-after" not in task:
             task["deadline-after"] = "1 day"
