0d8398a8d46a9a6bbca7dfab6fda78189fe06f1c
criss <ccozmuta@mozilla.com>
2022-08-26 02:25:46 +0300

    Backed out 6 changesets (bug 1787306) for causing Toolchains bustages. CLOSED TREE
    Backed out changeset bcee96aa7df1 (bug 1787306)
    Backed out changeset b5be37b86b21 (bug 1787306)
    Backed out changeset 7e518de15dbb (bug 1787306)
    Backed out changeset 07d938340e27 (bug 1787306)
    Backed out changeset 9b8971442df5 (bug 1787306)
    Backed out changeset 4d719c2b2711 (bug 1787306)

--- third_party/rust/serial_test/tests/tests.rs
+++ third_party/rust/serial_test/tests/tests.rs
@@ -1,8 +1,8 @@
-use serial_test::local_serial_core;
+use serial_test::serial_core;
 
 #[test]
 fn test_empty_serial_call() {
-    local_serial_core("beta", || {
+    serial_core("beta", || {
         println!("Bar");
     });
 }
