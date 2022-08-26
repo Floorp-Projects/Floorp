31ae3016ca426f790116234980fb4bc725fcce4d
Emily McMinn <emcminn@mozilla.com>
2022-08-26 14:30:11 +0000

    Bug 1779427 - Turn on Firefox View feature tour by default r=mviar
    Differential Revision: https://phabricator.services.mozilla.com/D155482

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2718,4 +2718,4 @@ pref("browser.places.snapshots.expiration.userManaged.days", 420);
 
 // If the user has seen the Firefox View feature tour this value reflects the tour
 // message id, the id of the last screen they saw, and whether they completed the tour
-pref("browser.firefox-view.feature-tour", "{\"message\":\"FIREFOX_VIEW_FEATURE_TOUR\",\"screen\":\"\",\"complete\":true}");
+pref("browser.firefox-view.feature-tour", "{\"message\":\"FIREFOX_VIEW_FEATURE_TOUR\",\"screen\":\"FEATURE_CALLOUT_1\",\"complete\":false}");
