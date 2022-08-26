6425a6ace0da14b3032ad4b9e664dc676915d005
Nika Layzell <nika@thelayzells.com>
2022-08-26 16:08:06 +0000

    Bug 1781129 - Part 3: Remove Shmem overload of GetSurfaceData, r=edgar
    After the previous changes there was only one consumer left of the Shmem
    version of GetSurfaceData, which could easily be changed to use BigBuffer,
    removing the need for that overload.
    
    After that consumer is removed, the interface was also simplified as the
    generic logic in the implementation was no longer necessary.
    
    Differential Revision: https://phabricator.services.mozilla.com/D151854

--- widget/nsDragServiceProxy.cpp
+++ widget/nsDragServiceProxy.cpp
@@ -65,22 +65,15 @@ nsresult nsDragServiceProxy::InvokeDragSessionImpl(
       if (dataSurface) {
         size_t length;
         int32_t stride;
-        Maybe<Shmem> maybeShm = nsContentUtils::GetSurfaceData(
-            *dataSurface, &length, &stride, child);
-        if (maybeShm.isNothing()) {
-          return NS_ERROR_FAILURE;
-        }
-
-        auto surfaceData = maybeShm.value();
-
-        // Save the surface data to shared memory.
-        if (!surfaceData.IsReadable() || !surfaceData.get<char>()) {
+        auto surfaceData =
+            nsContentUtils::GetSurfaceData(*dataSurface, &length, &stride);
+        if (surfaceData.isNothing()) {
           NS_WARNING("Failed to create shared memory for drag session.");
           return NS_ERROR_FAILURE;
         }
 
         mozilla::Unused << child->SendInvokeDragSession(
-            std::move(dataTransfers), aActionType, Some(std::move(surfaceData)),
+            std::move(dataTransfers), aActionType, std::move(surfaceData),
             stride, dataSurface->GetFormat(), dragRect, principal, csp, csArgs,
             mSourceWindowContext);
         StartDragSession();
