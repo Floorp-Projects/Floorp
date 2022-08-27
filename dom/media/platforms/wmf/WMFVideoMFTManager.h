4866adddf9aaa89b2c83ca007f8bb82c100ebf5c
Iulian Moraru <imoraru@mozilla.com>
2022-08-27 03:09:49 +0300

    Backed out changeset b1648cab08f0 (bug 1767360) for causing bc failures on browser_resizeVideo.js. CLOSED TREE

--- dom/media/platforms/wmf/WMFVideoMFTManager.h
+++ dom/media/platforms/wmf/WMFVideoMFTManager.h
@@ -81,15 +81,7 @@ class WMFVideoMFTManager : public MFTManager {
   const VideoInfo mVideoInfo;
   const gfx::IntSize mImageSize;
   const WMFStreamType mStreamType;
-
-  // The size we ask from the IMFMediaType which might include paddings. This is
-  // only used for softwate decoding.
-  gfx::IntSize mSoftwareImageSize;
-
-  // The display size we ask from the IMFMediaType, which might be different
-  // from the video info. This is only used for softwate decoding.
-  gfx::IntSize mSoftwareDisplaySize;
-
+  gfx::IntSize mDecodedImageSize;
   uint32_t mVideoStride;
   Maybe<gfx::YUVColorSpace> mColorSpace;
   gfx::ColorRange mColorRange;
