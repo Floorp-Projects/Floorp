219b2cf6d4406572ca7b9fe9712ca4863fd8457e
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2022-08-29 18:12:10 +0000

    Bug 1775424 - Part 2: Impl import.meta.resolve in js shell. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D154791

--- js/src/jit-test/tests/modules/import-meta.js
+++ js/src/jit-test/tests/modules/import-meta.js
@@ -20,6 +20,11 @@ assertEq(get(), import.meta);
 assertEq("url" in import.meta, true);
 assertEq(import.meta.url.endsWith("import-meta.js"), true);
 
+assertEq("resolve" in import.meta, true);
+
+assertEq(import.meta.resolve("./x"),
+         import.meta.url.replace("import-meta.js", "x"));
+
 import getOtherMetaObject from "exportImportMeta.js";
 
 let otherImportMeta = getOtherMetaObject();
@@ -30,11 +35,13 @@ assertEq(otherImportMeta.url.endsWith("exportImportMeta.js"), true);
 
 assertEq(Object.isExtensible(import.meta), true);
 
-var desc = Object.getOwnPropertyDescriptor(import.meta, "url");
-assertEq(desc.writable, true);
-assertEq(desc.enumerable, true);
-assertEq(desc.configurable, true);
-assertEq(desc.value, import.meta.url);
+for (const name of Reflect.ownKeys(import.meta)) {
+  const desc = Object.getOwnPropertyDescriptor(import.meta, name);
+  assertEq(desc.writable, true);
+  assertEq(desc.enumerable, true);
+  assertEq(desc.configurable, true);
+  assertEq(desc.value, import.meta[name]);
+}
 
 // The import.meta object's prototype is null.
 assertEq(Object.getPrototypeOf(import.meta), null);
@@ -47,12 +54,14 @@ assertEq(import.meta.newProp, 42);
 
 let found = new Set(Reflect.ownKeys(import.meta));
 
-assertEq(found.size, 2);
+assertEq(found.size, 3);
 assertEq(found.has("url"), true);
 assertEq(found.has("newProp"), true);
+assertEq(found.has("resolve"), true);
 
 delete import.meta.url;
 delete import.meta.newProp;
+delete import.meta.resolve;
 
 found = new Set(Reflect.ownKeys(import.meta));
 assertEq(found.size, 0);
--- js/src/shell/ModuleLoader.h
+++ js/src/shell/ModuleLoader.h
@@ -31,6 +31,7 @@ class ModuleLoader {
                                          HandleObject moduleRequest);
   static bool GetImportMetaProperties(JSContext* cx, HandleValue privateValue,
                                       HandleObject metaObject);
+  static bool ImportMetaResolve(JSContext* cx, unsigned argc, Value* vp);
   static bool ImportModuleDynamically(JSContext* cx,
                                       HandleValue referencingPrivate,
                                       HandleObject moduleRequest,
@@ -48,6 +49,10 @@ class ModuleLoader {
                                   HandleObject moduleRequest);
   bool populateImportMeta(JSContext* cx, HandleValue privateValue,
                           HandleObject metaObject);
+  bool importMetaResolve(JSContext* cx,
+                         JS::Handle<JS::Value> referencingPrivate,
+                         JS::Handle<JSString*> specifier,
+                         JS::MutableHandle<JSString*> urlOut);
   bool dynamicImport(JSContext* cx, HandleValue referencingPrivate,
                      HandleObject moduleRequest, HandleObject promise);
   bool doDynamicImport(JSContext* cx, HandleValue referencingPrivate,
@@ -62,6 +67,8 @@ class ModuleLoader {
                            HandleObject module);
   JSLinearString* resolve(JSContext* cx, HandleObject moduleRequestArg,
                           HandleValue referencingInfo);
+  JSLinearString* resolve(JSContext* cx, HandleString specifier,
+                          HandleValue referencingInfo);
   bool getScriptPath(JSContext* cx, HandleValue privateValue,
                      MutableHandle<JSLinearString*> pathOut);
   JSLinearString* normalizePath(JSContext* cx, Handle<JSLinearString*> path);
@@ -71,6 +78,14 @@ class ModuleLoader {
   // The following are used for pinned atoms which do not need rooting.
   JSAtom* loadPathStr = nullptr;
   JSAtom* pathSeparatorStr = nullptr;
+
+  // The slot stored in ImportMetaResolve function.
+  enum { ModulePrivateSlot = 0, SlotCount };
+
+  // The number of args in ImportMetaResolve.
+  static const uint32_t ImportMetaResolveNumArgs = 1;
+  // The index of the 'specifier' argument in ImportMetaResolve.
+  static const uint32_t ImportMetaResolveSpecifierArg = 0;
 } JS_HAZ_NON_GC_POINTER;
 
 }  // namespace shell
