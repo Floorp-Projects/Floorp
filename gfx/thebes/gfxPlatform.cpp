231b2dfa788d656118818f62f0a4d75dd3c5837e
sotaro <sotaro.ikeda.g@gmail.com>
2022-08-29 13:54:48 +0000

    Bug 1787293 - Revert Bug 1780138. r=jrmuizel, a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D155833

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -2759,7 +2759,7 @@ void gfxPlatform::InitWebRenderConfig() {
                                  "FEATURE_FAILURE_WR_NO_GFX_INFO"_ns);
         useHwVideoZeroCopy = false;
       } else {
-        if (status != nsIGfxInfo::FEATURE_STATUS_OK) {
+        if (status != nsIGfxInfo::FEATURE_ALLOW_ALWAYS) {
           FeatureState& feature =
               gfxConfig::GetFeature(Feature::HW_DECODED_VIDEO_ZERO_COPY);
           feature.DisableByDefault(FeatureStatus::Blocked,
@@ -2796,7 +2796,7 @@ void gfxPlatform::InitWebRenderConfig() {
                                  "FEATURE_FAILURE_WR_NO_GFX_INFO"_ns);
         reuseDecoderDevice = false;
       } else {
-        if (status != nsIGfxInfo::FEATURE_STATUS_OK) {
+        if (status != nsIGfxInfo::FEATURE_ALLOW_ALWAYS) {
           FeatureState& feature =
               gfxConfig::GetFeature(Feature::REUSE_DECODER_DEVICE);
           feature.DisableByDefault(FeatureStatus::Blocked,
