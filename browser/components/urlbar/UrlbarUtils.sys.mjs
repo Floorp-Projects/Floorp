394e9a4eaf2b59444abbda3518051fdb685469f7
Marian-Vasile Laza <mlaza@mozilla.com>
2022-08-30 18:59:43 +0300

    Backed out changeset 654379382e64 (bug 1783154) for causing bc failures on browser_restoreEmptyInput.js. CLOSED TREE

--- browser/components/urlbar/UrlbarUtils.sys.mjs
+++ browser/components/urlbar/UrlbarUtils.sys.mjs
@@ -1210,8 +1210,6 @@ export var UrlbarUtils = {
         if (result.providerName == "TabToSearch") {
           // This is the onboarding result.
           return "tabtosearch";
-        } else if (result.providerName == "quickactions") {
-          return "quickaction";
         }
         return "dynamic";
     }
