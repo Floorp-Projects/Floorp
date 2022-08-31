278eaced00bea22cd58c24196e5b7c40f4195a12
Brenden Hyde <bhyde@mozilla.com>
2022-08-31 22:56:10 +0000

    Bug 1649987 - Begin overhaul of artifact retention tactics. r=jmaher
    Amend: Make comment about potential for logs expiring earlier than task.
    
    Differential Revision: https://phabricator.services.mozilla.com/D156131

--- taskcluster/gecko_taskgraph/transforms/task.py
+++ taskcluster/gecko_taskgraph/transforms/task.py
@@ -17,6 +17,7 @@ from copy import deepcopy
 
 import attr
 from mozbuild.util import memoize
+from taskcluster.utils import fromNow
 from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.keyed_by import evaluate_keyed_by
 from taskgraph.util.schema import (
@@ -26,7 +27,6 @@ from taskgraph.util.schema import (
     taskref_or_string,
     validate_schema,
 )
-from taskgraph.util.time import value_of
 from taskgraph.util.treeherder import split_symbol
 from voluptuous import Any, Required, Optional, Extra, Match, All, NotIn
 
@@ -39,6 +39,7 @@ from gecko_taskgraph.util.partners import get_partners_to_be_published
 from gecko_taskgraph.util.scriptworker import BALROG_ACTIONS, get_release_config
 from gecko_taskgraph.util.signed_artifacts import get_signed_artifacts
 from gecko_taskgraph.util.workertypes import get_worker_type, worker_type_implementation
+from gecko_taskgraph.transforms.job.common import get_expiration
 
 RUN_TASK = os.path.join(GECKO, "taskcluster", "scripts", "run-task")
 
@@ -91,6 +92,7 @@ task_description_schema = Schema(
         # (e.g., "14 days").  Defaults are set based on the project.
         Optional("expires-after"): str,
         Optional("deadline-after"): str,
+        Optional("expiration-policy"): str,
         # custom routes for this task; the default treeherder routes will be added
         # automatically
         Optional("routes"): [str],
@@ -386,6 +388,7 @@ def verify_index(config, index):
                 # name of the produced artifact (root of the names for
                 # type=directory)
                 "name": str,
+                "expires-after": str,
             }
         ],
         # environment variables
@@ -400,7 +403,7 @@ def verify_index(config, index):
         # the exit status code(s) that indicates the caches used by the task
         # should be purged
         Optional("purge-caches-exit-status"): [int],
-        # Wether any artifacts are assigned to this worker
+        # Whether any artifacts are assigned to this worker
         Optional("skip-artifacts"): bool,
     },
 )
@@ -515,11 +518,17 @@ def build_docker_worker_payload(config, task, task_def):
 
     if "artifacts" in worker:
         artifacts = {}
+        expires_policy = get_expiration(
+            config, task.get("expiration-policy", "default")
+        )
         for artifact in worker["artifacts"]:
+            art_exp = artifact.get("expires-after", expires_policy)
+            task_exp = task_def["expires"]["relative-datestamp"]
+            expires = art_exp if fromNow(art_exp) < fromNow(task_exp) else task_exp
             artifacts[artifact["name"]] = {
                 "path": artifact["path"],
                 "type": artifact["type"],
-                "expires": task_def["expires"],  # always expire with the task
+                "expires": {"relative-datestamp": expires},
             }
         payload["artifacts"] = artifacts
 
@@ -633,6 +642,7 @@ def build_docker_worker_payload(config, task, task_def):
                 "path": str,
                 # if not specified, path is used for artifact name
                 Optional("name"): str,
+                "expires-after": str,
             }
         ],
         # Directories and/or files to be mounted.
@@ -738,10 +748,15 @@ def build_generic_worker_payload(config, task, task_def):
 
     artifacts = []
 
+    expires_policy = get_expiration(config, task.get("expiration-policy", "default"))
     for artifact in worker.get("artifacts", []):
+        art_exp = artifact.get("expires-after", expires_policy)
+        task_exp = task_def["expires"]["relative-datestamp"]
+        expires = art_exp if fromNow(art_exp) < fromNow(task_exp) else task_exp
         a = {
             "path": artifact["path"],
             "type": artifact["type"],
+            "expires": {"relative-datestamp": expires},
         }
         if "name" in artifact:
             a["name"] = artifact["name"]
@@ -1808,6 +1823,34 @@ def try_task_config_routes(config, tasks):
 
 
 @transforms.add
+def set_task_and_artifact_expiry(config, jobs):
+    """Set the default expiry for tasks and their artifacts.
+
+    These values are read from ci/config.yml
+    """
+    for job in jobs:
+        expires = get_expiration(config, job.get("expiration-policy", "default"))
+        if "expires-after" not in job:
+            job["expires-after"] = expires
+        task_expiry = job["expires-after"]
+
+        if "artifacts" in job["worker"]:
+            for a in job["worker"]["artifacts"]:
+                if "expires-after" not in a:
+                    a["expires-after"] = expires
+
+                # By using > instead of >=, there's a chance of mismatch
+                #   where the artifact expires sooner than the task.
+                #   There is no chance, however, of mismatch where artifacts
+                #   expire _after_ the task.
+                # Currently this leads to some build tasks having logs
+                #   that expire in 1 year while the task expires in 3 years.
+                if fromNow(a["expires-after"]) > fromNow(task_expiry):
+                    a["expires-after"] = task_expiry
+        yield job
+
+
+@transforms.add
 def build_task(config, tasks):
     for task in tasks:
         level = str(config.params["level"])
@@ -1874,14 +1917,6 @@ def build_task(config, tasks):
                     branch_rev,
                 )
             )
-
-        if "expires-after" in task:
-            if is_try(config.params):
-                delta = value_of(task["expires-after"])
-                if delta.days >= 28:
-                    task["expires-after"] = "28 days"
-        else:
-            task["expires-after"] = "28 days" if is_try(config.params) else "1 year"
 
         if "deadline-after" not in task:
             task["deadline-after"] = "1 day"
