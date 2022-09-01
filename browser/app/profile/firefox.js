d2e96de8c9787f73b8ad576e075cb599637cfed3
Narcis Beleuzu <nbeleuzu@mozilla.com>
2022-09-02 00:34:30 +0300

    Backed out changeset aa77f15a38d9 (bug 1779427) for assertion failure on DOMJSProxyHandler.cpp . CLOSED TREE

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2720,6 +2720,6 @@ pref("browser.places.snapshots.expiration.userManaged.days", 420);
 
 // If the user has seen the Firefox View feature tour this value reflects the tour
 // message id, the id of the last screen they saw, and whether they completed the tour
-pref("browser.firefox-view.feature-tour", "{\"message\":\"FIREFOX_VIEW_FEATURE_TOUR\",\"screen\":\"FEATURE_CALLOUT_1\",\"complete\":false}");
+pref("browser.firefox-view.feature-tour", "{\"message\":\"FIREFOX_VIEW_FEATURE_TOUR\",\"screen\":\"\",\"complete\":true}");
 // Number of times the user visited about:firefoxview
 pref("browser.firefox-view.view-count", 0);
