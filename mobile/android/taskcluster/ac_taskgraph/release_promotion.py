a32bc283d7587b1e6b02deefceeb2ead595150cb
Johan Lorenzo <jlorenzo@mozilla.com>
2022-09-01 11:12:07 +0200

    Bug 1786780 - part 1: Enable release promotion on the new monorepo (and its staging counterpart)

--- mobile/android/taskcluster/ac_taskgraph/release_promotion.py
+++ mobile/android/taskcluster/ac_taskgraph/release_promotion.py
@@ -15,7 +15,8 @@ from taskgraph.parameters import Parameters
 from taskgraph.util.taskgraph import find_decision_task, find_existing_tasks_from_previous_kinds
 
 RELEASE_PROMOTION_PROJECTS = (
-    "https://github.com/mozilla-mobile/android-components", "https://github.com/mozilla-releng/staging-android-components"
+    "https://github.com/mozilla-mobile/firefox-android",
+    "https://github.com/mozilla-releng/staging-firefox-android",
 )
 
 
