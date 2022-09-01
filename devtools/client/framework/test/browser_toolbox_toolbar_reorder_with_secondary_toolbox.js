aa440f1a2c0906e1b490e0e597859d48438dc13a
Alexandre Poirot <poirot.alex@gmail.com>
2022-09-01 08:41:50 +0000

    Bug 1787409 - [devtools] Resolve toolbox's opening only once the meatball menu is updated. r=nchevobbe
    _buildTabs/_setInitialMeatballState wasn't awaited for.
    So that preference front request might be pending when closing the toolbox too quickly in tests.
    
    We should wait for all RDP requests to be completed before resolving Toolbox.open.
    
    Differential Revision: https://phabricator.services.mozilla.com/D155872

--- devtools/client/framework/test/browser_toolbox_toolbar_reorder_with_secondary_toolbox.js
+++ devtools/client/framework/test/browser_toolbox_toolbar_reorder_with_secondary_toolbox.js
@@ -28,12 +28,6 @@ add_task(async function() {
   Services.prefs.setCharPref("devtools.toolbox.selectedTool", "memory");
   const toolbox = await gDevToolsBrowser.openContentProcessToolbox(gBrowser);
 
-  // A RDP request is made during toolbox opening and this request isn't awaited for
-  // during the call to gDevTools.showToolbox. This relates to the autohide menu.
-  // Await for this request to be finished and the DOM elements relating to it to be disabled
-  // before trying to close the toolbox.
-  await waitForDOM(toolbox.win.document, "#toolbox-meatball-menu-noautohide");
-
   info(
     "Check whether the value of devtools.toolbox.tabsOrder was not affected after closed"
   );
