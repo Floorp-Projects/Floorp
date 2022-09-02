86540ed2e717940b70239f98445888b25f15f66c
Dana Keeler <dkeeler@mozilla.com>
2022-09-02 22:26:37 +0000

    Bug 1788856 - initialize NSS as needed in nsNSSCertificate r=jschanck
    Previously, instantiating an nsIX509Cert (implemented by nsNSSCertificate)
    would cause NSS to be initialized. However, if 'new nsNSSCertificate()' was
    called directly (rather than going through XPCOM), NSS would not be
    initialized. This didn't seem to be a problem until bug 1787942 changed how
    nsITransportSecurityInfo was sent between processes for PHttpChannel and
    HttpChannelOnStartRequestArgs (namely, by using the direct IPC support rather
    than first serializing to a string, sending it over IPC, and then deserializing
    it). That direct IPC implementation uses 'new nsNSSCertificate()', which is now
    a problem.
    
    nsNSSCertificate used to make extensive use of NSS, which warranted ensuring
    NSS was initialized before creating one at all. Now, as of bug 1748341, the
    cases where nsNSSCertificate uses NSS are limited and clearly delineated.
    Accordinly, this change makes it so nsNSSCertificate only initializes NSS if
    and when it needs it, rather than relying on the XPCOM boilerplate to
    initialize NSS first.
    
    Differential Revision: https://phabricator.services.mozilla.com/D156353

--- security/manager/ssl/nsNSSCertificate.cpp
+++ security/manager/ssl/nsNSSCertificate.cpp
@@ -78,6 +78,11 @@ UniqueCERTCertificate nsNSSCertificate::GetOrInstantiateCert() {
   if (maybeCert.isSome()) {
     return UniqueCERTCertificate(CERT_DupCertificate((*maybeCert).get()));
   }
+
+  if (!EnsureNSSInitializedChromeOrContent()) {
+    return nullptr;
+  }
+
   SECItem derItem = {siBuffer, mDER.Elements(),
                      static_cast<unsigned int>(mDER.Length())};
   UniqueCERTCertificate cert(CERT_NewTempCertificate(
@@ -450,6 +455,11 @@ nsNSSCertificate::GetSerialNumber(nsAString& _serialNumber) {
 nsresult nsNSSCertificate::GetCertificateHash(nsAString& aFingerprint,
                                               SECOidTag aHashAlg) {
   aFingerprint.Truncate();
+
+  if (!EnsureNSSInitializedChromeOrContent()) {
+    return NS_ERROR_NOT_AVAILABLE;
+  }
+
   nsTArray<uint8_t> digestArray;
   nsresult rv =
       Digest::DigestBuf(aHashAlg, mDER.Elements(), mDER.Length(), digestArray);
@@ -504,6 +514,10 @@ NS_IMETHODIMP
 nsNSSCertificate::GetSha256SubjectPublicKeyInfoDigest(
     nsACString& aSha256SPKIDigest) {
   aSha256SPKIDigest.Truncate();
+
+  if (!EnsureNSSInitializedChromeOrContent()) {
+    return NS_ERROR_NOT_AVAILABLE;
+  }
 
   pkix::Input certInput;
   pkix::Result result = certInput.Init(mDER.Elements(), mDER.Length());
