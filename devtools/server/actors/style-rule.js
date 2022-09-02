059dc02e8c3f2bc71052733a1e884f5529187899
Nicolas Chevobbe <nchevobbe@mozilla.com>
2022-09-02 18:00:27 +0000

    Bug 1499653 - [devtools] Display @supports condition on top of rules. r=jdescottes.
    Since we already had a test case for nested at-rules in
    browser_rules_container-queries.js, I'm moving it to a
    new test where we can test all the different at-rules that
    can be nested (including `@supports`).
    
    Differential Revision: https://phabricator.services.mozilla.com/D156313

--- devtools/server/actors/style-rule.js
+++ devtools/server/actors/style-rule.js
@@ -318,30 +318,32 @@ const StyleRuleActor = protocol.ActorClassWithSpec(styleRuleSpec, {
     // Go through all ancestor so we can build an array of all the media queries and
     // layers this rule is in.
     for (const ancestorRule of this.ancestorRules) {
+      const ruleClassName = ChromeUtils.getClassName(ancestorRule.rawRule);
       if (
-        ancestorRule.type === CSSRule.MEDIA_RULE &&
+        ruleClassName === "CSSMediaRule" &&
         ancestorRule.rawRule.media?.length
       ) {
         form.ancestorData.push({
           type: "media",
           value: Array.from(ancestorRule.rawRule.media).join(", "),
         });
-      } else if (
-        ChromeUtils.getClassName(ancestorRule.rawRule) === "CSSLayerBlockRule"
-      ) {
+      } else if (ruleClassName === "CSSLayerBlockRule") {
         form.ancestorData.push({
           type: "layer",
           value: ancestorRule.rawRule.name,
         });
-      } else if (
-        ChromeUtils.getClassName(ancestorRule.rawRule) === "CSSContainerRule"
-      ) {
+      } else if (ruleClassName === "CSSContainerRule") {
         form.ancestorData.push({
           type: "container",
           // Send containerName and containerQuery separately (instead of conditionText)
           // so the client has more flexibility to display the information.
           containerName: ancestorRule.rawRule.containerName,
           containerQuery: ancestorRule.rawRule.containerQuery,
+        });
+      } else if (ruleClassName === "CSSSupportsRule") {
+        form.ancestorData.push({
+          type: "supports",
+          conditionText: ancestorRule.rawRule.conditionText,
         });
       }
     }
