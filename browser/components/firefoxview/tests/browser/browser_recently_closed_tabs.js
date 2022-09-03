c34cb724c08a116e54947647a710bf7e200f7ac9
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2022-09-03 09:53:55 +0000

    Bug 1787565 - update recently closed tab entries in Firefox View based on delayed tab navigation information from session history, r=sclements
    I don't love this solution but it appears to work and I can't think of anything better.
    
    Differential Revision: https://phabricator.services.mozilla.com/D155920

--- browser/components/firefoxview/tests/browser/browser_recently_closed_tabs.js
+++ browser/components/firefoxview/tests/browser/browser_recently_closed_tabs.js
@@ -64,14 +64,9 @@ add_task(async function test_empty_list() {
     },
     async browser => {
       const { document } = browser.contentWindow;
-      const closedObjectsChanged = TestUtils.topicObserved(
-        "sessionstore-closed-objects-changed"
-      );
-
+      let container = document.querySelector("#collapsible-tabs-container");
       ok(
-        document
-          .querySelector("#collapsible-tabs-container")
-          .classList.contains("empty-container"),
+        container.classList.contains("empty-container"),
         "collapsible container should have correct styling when the list is empty"
       );
 
@@ -85,12 +80,15 @@ add_task(async function test_empty_list() {
       const tab1 = await add_new_tab(URLs[0]);
 
       await close_tab(tab1);
-      await closedObjectsChanged;
 
+      // The UI update happens asynchronously as we learn of the new closed tab.
+      await BrowserTestUtils.waitForMutationCondition(
+        container,
+        { attributeFilter: ["class"] },
+        () => !container.classList.contains("empty-container")
+      );
       ok(
-        !document
-          .querySelector("#collapsible-tabs-container")
-          .classList.contains("empty-container"),
+        !container.classList.contains("empty-container"),
         "collapsible container should have correct styling when the list is not empty"
       );
 
@@ -288,10 +286,15 @@ add_task(async function test_max_list_items() {
       await close_tab(tab);
       await closedObjectsChanged;
 
+      let firstListItem = document.querySelector("ol.closed-tabs-list")
+        .firstChild;
+      await BrowserTestUtils.waitForMutationCondition(
+        firstListItem,
+        { characterData: true, childList: true, subtree: true },
+        () => firstListItem.textContent.includes(".org")
+      );
       ok(
-        document
-          .querySelector("ol.closed-tabs-list")
-          .firstChild.textContent.includes("example.org"),
+        firstListItem.textContent.includes("example.org"),
         "first list item in recently-closed-tabs-list should have been updated"
       );
 
@@ -391,4 +394,56 @@ add_task(async function test_arrow_keys() {
       ok(list[0].matches(":focus"), "The first link is still focused");
     }
   );
+});
+
+add_task(async function test_switch_before_closing() {
+  clearHistory();
+
+  const INITIAL_URL = "https://example.org/iwilldisappear";
+  const FINAL_URL = "https://example.com/ishouldappear";
+  await withFirefoxView({}, async function(browser) {
+    let gBrowser = browser.getTabBrowser();
+    let newTab = await BrowserTestUtils.openNewForegroundTab(
+      gBrowser,
+      INITIAL_URL
+    );
+
+    // Switch back to FxView:
+    await BrowserTestUtils.switchTab(
+      gBrowser,
+      gBrowser.getTabForBrowser(browser)
+    );
+
+    // Update the tab we opened to a different site:
+    let loadPromise = BrowserTestUtils.browserLoaded(
+      newTab.linkedBrowser,
+      null,
+      FINAL_URL
+    );
+    BrowserTestUtils.loadURI(newTab.linkedBrowser, FINAL_URL);
+    await loadPromise;
+
+    // Close the added tab
+    BrowserTestUtils.removeTab(newTab);
+
+    const { document } = browser.contentWindow;
+    const tabsList = document.querySelector("ol.closed-tabs-list");
+    await BrowserTestUtils.waitForMutationCondition(
+      tabsList,
+      { childList: true },
+      () => !!tabsList.children.length
+    );
+    info("A tab appeared in the list, ensure it has the right URL.");
+    let urlBit = tabsList.firstElementChild.querySelector(".closed-tab-li-url");
+    await BrowserTestUtils.waitForMutationCondition(
+      urlBit,
+      { characterData: true, attributeFilter: ["title"] },
+      () => urlBit.textContent.includes(".com")
+    );
+    is(
+      urlBit.textContent,
+      "example.com",
+      "Item should end up with the correct URL."
+    );
+  });
 });
