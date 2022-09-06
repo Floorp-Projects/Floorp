b469ba2bf975128580aa12fdbbf25beeeb31e559
Morgan Rae Reschenberg <mreschenberg@mozilla.com>
2022-09-06 19:27:16 +0000

    Bug 1787282: [Part 1] Modify relations caching infrastructure for rels with no implicit partner r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D156126

--- accessible/ipc/RemoteAccessibleBase.cpp
+++ accessible/ipc/RemoteAccessibleBase.cpp
@@ -746,6 +746,11 @@ nsTArray<bool> RemoteAccessibleBase<Derived>::PreProcessRelations(
       }
     }
 
+    if (!data.mReverseType) {
+      updateTracker.AppendElement(false);
+      continue;
+    }
+
     nsStaticAtom* const relAtom = data.mAtom;
     auto newRelationTargets =
         aFields->GetAttribute<nsTArray<uint64_t>>(relAtom);
@@ -756,7 +761,7 @@ nsTArray<bool> RemoteAccessibleBase<Derived>::PreProcessRelations(
     // if we've recieved a DeleteEntry(). Only do this if mCachedFields is
     // initialized. If mCachedFields is not initialized, we still need to
     // construct the update array so we correctly handle reverse rels in
-    // PostProcessRelations
+    // PostProcessRelations.
     if ((shouldAddNewImplicitRels ||
          aFields->GetAttribute<DeleteEntry>(relAtom)) &&
         mCachedFields) {
@@ -768,7 +773,7 @@ nsTArray<bool> RemoteAccessibleBase<Derived>::PreProcessRelations(
               Document()->mReverseRelations.LookupOrInsert(id);
           // Then fetch its reverse relation's ID list
           nsTArray<uint64_t>& reverseRelIDs = reverseRels.LookupOrInsert(
-              static_cast<uint64_t>(data.mReverseType));
+              static_cast<uint64_t>(*data.mReverseType));
           //  There might be other reverse relations stored for this acc, so
           //  remove our ID instead of deleting the array entirely.
           DebugOnly<bool> removed = reverseRelIDs.RemoveElement(ID());
@@ -801,8 +806,10 @@ void RemoteAccessibleBase<Derived>::PostProcessRelations(
       for (uint64_t id : newIDs) {
         nsTHashMap<nsUint64HashKey, nsTArray<uint64_t>>& relations =
             Document()->mReverseRelations.LookupOrInsert(id);
+        MOZ_ASSERT(data.mReverseType,
+                   "Updating implicit rels, but no implicit rel exists?");
         nsTArray<uint64_t>& ids =
-            relations.LookupOrInsert(static_cast<uint64_t>(data.mReverseType));
+            relations.LookupOrInsert(static_cast<uint64_t>(*data.mReverseType));
         ids.AppendElement(ID());
       }
     }
