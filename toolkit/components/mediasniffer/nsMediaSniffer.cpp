92bde6526c5dc9a12ace15e385ae7ed4c83f33d8
Paul Adenot <paul@paul.cx>
2022-09-06 13:30:08 +0000

    Bug 1787159 - Fix sniffing for AAC in ADTS with big AAC frames. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D155585

--- toolkit/components/mediasniffer/nsMediaSniffer.cpp
+++ toolkit/components/mediasniffer/nsMediaSniffer.cpp
@@ -30,6 +30,9 @@ static const uint32_t MAX_BYTES_SNIFFED = 512;
 // bitstream.
 // This is 320kbps * 144 / 32kHz + 1 padding byte + 4 bytes of capture pattern.
 static const uint32_t MAX_BYTES_SNIFFED_MP3 = 320 * 144 / 32 + 1 + 4;
+// Multi-channel low sample-rate AAC packets can be huge, have a higher maximum
+// size.
+static const uint32_t MAX_BYTES_SNIFFED_ADTS = 8096;
 
 NS_IMPL_ISUPPORTS(nsMediaSniffer, nsIContentSniffer)
 
@@ -232,7 +235,7 @@ nsMediaSniffer::GetMIMETypeFromContent(nsIRequest* aRequest,
     return NS_OK;
   }
 
-  if (MatchesADTS(aData, clampedLength)) {
+  if (MatchesADTS(aData, std::min(aLength, MAX_BYTES_SNIFFED_ADTS))) {
     aSniffedType.AssignLiteral(AUDIO_AAC);
     return NS_OK;
   }
