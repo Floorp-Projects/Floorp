ec92ab90c9186bd7fa4600b3bd6a24169319610f
Jon Coppeard <jcoppeard@mozilla.com>
2022-09-07 09:49:59 +0000

    Bug 1785804 - Part 6: Inline TraceEdge methods for non-tagged pointers r=sfink
    For non-tagged pointer types, DoCallback is now simple enough that we can
    define this inline in Tracing.h and most TraceEdge methods become a virtual
    method call.
    
    Depends on D156563
    
    Differential Revision: https://phabricator.services.mozilla.com/D156564

--- js/src/gc/Marking.h
+++ js/src/gc/Marking.h
@@ -145,19 +145,6 @@ inline void CheckGCThingAfterMovingGC(const WeakHeapPtr<T*>& t);
 
 } /* namespace gc */
 
-// Debugging functions to check tracing invariants.
-#ifdef DEBUG
-template <typename T>
-void CheckTracedThing(JSTracer* trc, T* thing);
-template <typename T>
-void CheckTracedThing(JSTracer* trc, const T& thing);
-#else
-template <typename T>
-inline void CheckTracedThing(JSTracer* trc, T* thing) {}
-template <typename T>
-inline void CheckTracedThing(JSTracer* trc, const T& thing) {}
-#endif
-
 } /* namespace js */
 
 #endif /* gc_Marking_h */
