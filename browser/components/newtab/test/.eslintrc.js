9c5ebcfa46dee6196f3f8419b7c2fa3776330efe
lyavor <lyavor@mozilla.com>
2022-09-07 08:45:12 +0000

    Bug 1709150 - Consider linter rule to make https the default for all kind of tests we add. r=Standard8,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D149394

--- browser/components/newtab/test/.eslintrc.js
+++ browser/components/newtab/test/.eslintrc.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-commonjs */
+// This config doesn't inhert from top-level eslint config  Bug 1780031
 
+const xpcshellTestPaths = ["./unit*/**", "./xpcshell/**"];
 module.exports = {
   env: {
     mocha: true,
@@ -16,4 +18,24 @@ module.exports = {
     "react/jsx-no-bind": 0,
     "require-await": 0,
   },
+  overrides: [
+    {
+      // Exempt all files without a 'test' string in their path name since no-insecure-url
+      // is focussing on the test base
+      files: "*",
+      excludedFiles: ["**/test**", "**/test*/**", "Test*/**"],
+      rules: {
+        "@microsoft/sdl/no-insecure-url": "off",
+      },
+    },
+    {
+      // Disable "no-insecure-url" for all xpcshell test
+      files: xpcshellTestPaths.map(path => `${path}`),
+      rules: {
+        // As long "new HttpServer()" does not support https there is no reason to log warnings
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=1742061
+        "@microsoft/sdl/no-insecure-url": "off",
+      },
+    },
+  ],
 };
