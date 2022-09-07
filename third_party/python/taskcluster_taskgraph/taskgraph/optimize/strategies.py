9a4802651ebad44ea78b43e7f53d51b97448e85a
Johan Lorenzo <jlorenzo@mozilla.com>
2022-09-07 09:13:40 +0000

    Bug 1784232 - Bump taskgraph to v3.0.0 r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D155978

--- third_party/python/taskcluster_taskgraph/taskgraph/optimize/strategies.py
+++ third_party/python/taskcluster_taskgraph/taskgraph/optimize/strategies.py
@@ -50,13 +50,8 @@ class IndexSearch(OptimizationStrategy):
 @register_strategy("skip-unless-changed")
 class SkipUnlessChanged(OptimizationStrategy):
     def should_remove_task(self, task, params, file_patterns):
-        if params.get("repository_type") != "hg":
-            raise RuntimeError(
-                "SkipUnlessChanged optimization only works with mercurial repositories"
-            )
-
-        # pushlog_id == -1 - this is the case when run from a cron.yml job
-        if params.get("pushlog_id") == -1:
+        # pushlog_id == -1 - this is the case when run from a cron.yml job or on a git repository
+        if params.get("repository_type") == "hg" and params.get("pushlog_id") == -1:
             return False
 
         changed = files_changed.check(params, file_patterns)
