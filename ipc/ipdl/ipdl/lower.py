d263dec049d74abc0c75add4cfd96d9d13229125
Nika Layzell <nika@thelayzells.com>
2022-09-07 23:38:45 +0000

    Bug 1789440 - Track reply message IDs for MessageChannel async replies, r=ipc-reviewers,mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D156569

--- ipc/ipdl/ipdl/lower.py
+++ ipc/ipdl/ipdl/lower.py
@@ -4859,7 +4859,7 @@ class _GenerateProtocolActorCode(ipdl.ast.Visitor):
             $*{prologue}
 
             UniquePtr<MessageChannel::UntypedCallbackHolder> untypedCallback =
-                GetIPCChannel()->PopCallback(${msgvar});
+                GetIPCChannel()->PopCallback(${msgvar}, Id());
 
             typedef MessageChannel::CallbackHolder<${resolvetype}> CallbackHolder;
             auto* callback = static_cast<CallbackHolder*>(untypedCallback.get());
@@ -5413,6 +5413,7 @@ class _GenerateProtocolActorCode(ipdl.ast.Visitor):
                         send,
                         args=[
                             ExprMove(msgexpr),
+                            ExprVar(md.pqReplyId()),
                             ExprMove(resolvefn),
                             ExprMove(rejectfn),
                         ],
