9566bebcc129ec937cc0321acddc2b3fca7a1cc6
Brad Werth <bwerth@mozilla.com>
2022-09-08 18:02:03 +0000

    Bug 1787561 Part 1: Make GPUVideoImage track color depth, and make RemoteImageHolder pass it through. r=media-playback-reviewers,gfx-reviewers,alwu,aosmond
    This ensures that the Image that the client receives has the color depth
    information that was sent to the GPU, even though the client can't query
    the GPU directly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D155902

--- dom/media/ipc/RemoteImageHolder.cpp
+++ dom/media/ipc/RemoteImageHolder.cpp
@@ -22,11 +22,17 @@ RemoteImageHolder::RemoteImageHolder() = default;
 RemoteImageHolder::RemoteImageHolder(layers::IGPUVideoSurfaceManager* aManager,
                                      layers::VideoBridgeSource aSource,
                                      const gfx::IntSize& aSize,
+                                     const gfx::ColorDepth& aColorDepth,
                                      const layers::SurfaceDescriptor& aSD)
-    : mSource(aSource), mSize(aSize), mSD(Some(aSD)), mManager(aManager) {}
+    : mSource(aSource),
+      mSize(aSize),
+      mColorDepth(aColorDepth),
+      mSD(Some(aSD)),
+      mManager(aManager) {}
 RemoteImageHolder::RemoteImageHolder(RemoteImageHolder&& aOther)
     : mSource(aOther.mSource),
       mSize(aOther.mSize),
+      mColorDepth(aOther.mColorDepth),
       mSD(std::move(aOther.mSD)),
       mManager(aOther.mManager) {
   aOther.mSD = Nothing();
@@ -113,7 +119,7 @@ already_AddRefed<layers::Image> RemoteImageHolder::TransferToImage(
     SurfaceDescriptorRemoteDecoder remoteSD =
         static_cast<const SurfaceDescriptorGPUVideo&>(*mSD);
     remoteSD.source() = Some(mSource);
-    image = new GPUVideoImage(mManager, remoteSD, mSize);
+    image = new GPUVideoImage(mManager, remoteSD, mSize, mColorDepth);
   }
   mSD = Nothing();
   mManager = nullptr;
@@ -139,6 +145,7 @@ RemoteImageHolder::~RemoteImageHolder() {
     RemoteImageHolder&& aParam) {
   WriteIPDLParam(aWriter, aActor, aParam.mSource);
   WriteIPDLParam(aWriter, aActor, aParam.mSize);
+  WriteIPDLParam(aWriter, aActor, aParam.mColorDepth);
   WriteIPDLParam(aWriter, aActor, aParam.mSD);
   // Empty this holder.
   aParam.mSD = Nothing();
@@ -150,6 +157,7 @@ RemoteImageHolder::~RemoteImageHolder() {
     RemoteImageHolder* aResult) {
   if (!ReadIPDLParam(aReader, aActor, &aResult->mSource) ||
       !ReadIPDLParam(aReader, aActor, &aResult->mSize) ||
+      !ReadIPDLParam(aReader, aActor, &aResult->mColorDepth) ||
       !ReadIPDLParam(aReader, aActor, &aResult->mSD)) {
     return false;
   }
