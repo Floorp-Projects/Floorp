7f44f16781092ef597d1221f762f8acd92b92dd4
André Bargull <andre.bargull@gmail.com>
2022-09-08 13:47:03 +0000

    Bug 1341937 - Part 14: Remove a misleading comment. r=jandem
    The template object is always present, so a comment mentioning it's only
    conditionally present is a bit misleading.
    
    Differential Revision: https://phabricator.services.mozilla.com/D156790

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -7195,7 +7195,6 @@ void CodeGenerator::visitNewNamedLambdaObject(LNewNamedLambdaObject* lir) {
   Register tempReg = ToRegister(lir->temp0());
   const CompileInfo& info = lir->mir()->block()->info();
 
-  // If we have a template object, we can inline call object creation.
   using Fn =
       js::NamedLambdaObject* (*)(JSContext*, HandleFunction, gc::InitialHeap);
   OutOfLineCode* ool = oolCallVM<Fn, NamedLambdaObject::createTemplateObject>(
