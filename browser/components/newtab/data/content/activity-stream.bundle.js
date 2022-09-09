3c4d15f5c456c5ab2150642db8f412fd5508c6a2
Gavin Suntop <gavin@gsuntop.com>
2022-09-09 17:13:26 +0000

    Bug 1787521 -  Adjust button for newtab save to pocket action r=thecount,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D155995

--- browser/components/newtab/data/content/activity-stream.bundle.js
+++ browser/components/newtab/data/content/activity-stream.bundle.js
@@ -7761,11 +7761,11 @@ class _DSCard extends (external_React_default()).PureComponent {
     }, this.props.context_type === "pocket" ? /*#__PURE__*/external_React_default().createElement((external_React_default()).Fragment, null, /*#__PURE__*/external_React_default().createElement("span", {
       className: "story-badge-icon icon icon-pocket"
     }), /*#__PURE__*/external_React_default().createElement("span", {
-      "data-l10n-id": "newtab-pocket-saved-to-pocket"
+      "data-l10n-id": "newtab-pocket-saved"
     })) : /*#__PURE__*/external_React_default().createElement((external_React_default()).Fragment, null, /*#__PURE__*/external_React_default().createElement("span", {
       className: "story-badge-icon icon icon-pocket-save"
     }), /*#__PURE__*/external_React_default().createElement("span", {
-      "data-l10n-id": "newtab-pocket-save-to-pocket"
+      "data-l10n-id": "newtab-pocket-save"
     }))), /*#__PURE__*/external_React_default().createElement(DSLinkMenu, {
       id: this.props.id,
       index: this.props.pos,
