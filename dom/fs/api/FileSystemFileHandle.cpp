e0343cbce73f35f399a6fb0dadf848ff9818b21f
Randell Jesup <rjesup@wgate.com>
2022-09-10 02:15:04 +0000

    Bug 1761370: Add SyncAccessHandles to OPFS r=dom-storage-reviewers,jari,janv
    Differential Revision: https://phabricator.services.mozilla.com/D145642

--- dom/fs/api/FileSystemFileHandle.cpp
+++ dom/fs/api/FileSystemFileHandle.cpp
@@ -5,8 +5,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "FileSystemFileHandle.h"
-#include "fs/FileSystemRequestHandler.h"
 
+#include "fs/FileSystemRequestHandler.h"
 #include "js/StructuredClone.h"
 #include "js/TypeDecls.h"
 #include "mozilla/ErrorResult.h"
@@ -77,7 +77,7 @@ already_AddRefed<Promise> FileSystemFileHandle::CreateSyncAccessHandle(
     return nullptr;
   }
 
-  promise->MaybeReject(NS_ERROR_NOT_IMPLEMENTED);
+  mRequestHandler->GetAccessHandle(mManager, mMetadata, promise);
 
   return promise.forget();
 }
