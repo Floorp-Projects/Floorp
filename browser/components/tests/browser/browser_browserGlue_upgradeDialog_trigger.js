c412d65b913004aadd9d2cf45713a38302d6e860
Barret Rennie <barret@brennie.ca>
2022-09-12 20:37:16 +0000

    Bug 1784100 - Override upgradeDialog.enabled feature with majorRelease 2022 r=Mardak
    Differential Revision: https://phabricator.services.mozilla.com/D156147

--- browser/components/tests/browser/browser_browserGlue_upgradeDialog_trigger.js
+++ browser/components/tests/browser/browser_browserGlue_upgradeDialog_trigger.js
@@ -13,9 +13,29 @@ const { OnboardingMessageProvider } = ChromeUtils.import(
   "resource://activity-stream/lib/OnboardingMessageProvider.jsm"
 );
 
-add_task(async function not_major_upgrade() {
-  Services.telemetry.clearEvents();
+XPCOMUtils.defineLazyServiceGetters(this, {
+  BrowserHandler: ["@mozilla.org/browser/clh;1", "nsIBrowserHandler"],
+});
 
+add_setup(() => {
+  Services.telemetry.clearEvents();
+});
+
+async function forceMajorUpgrade() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.startup.homepage_override.mstone", "88.0"]],
+  });
+
+  void BrowserHandler.defaultArgs;
+
+  return async () => {
+    await SpecialPowers.popPrefEnv();
+    BrowserHandler.majorUpgrade = false;
+    Services.prefs.clearUserPref("browser.startup.upgradeDialog.version");
+  };
+}
+
+add_task(async function not_major_upgrade() {
   await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
 
   AssertEvents("Not major upgrade for upgrade dialog requirements", [
@@ -35,10 +55,7 @@ add_task(async function remote_disabled() {
   });
 
   // Simulate starting from a previous version.
-  await SpecialPowers.pushPrefEnv({
-    set: [["browser.startup.homepage_override.mstone", "88.0"]],
-  });
-  Cc["@mozilla.org/browser/clh;1"].getService(Ci.nsIBrowserHandler).defaultArgs;
+  let cleanupUpgrade = await forceMajorUpgrade();
 
   await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
 
@@ -49,6 +66,7 @@ add_task(async function remote_disabled() {
   ]);
 
   await doCleanup();
+  await cleanupUpgrade();
 });
 
 add_task(async function enterprise_disabled() {
@@ -57,6 +75,8 @@ add_task(async function enterprise_disabled() {
   const orig = defaultPrefs.getBoolPref(pref, true);
   defaultPrefs.setBoolPref(pref, false);
 
+  let cleanupUpgrade = await forceMajorUpgrade();
+
   await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
 
   AssertEvents("Welcome disabled like enterprise policy", [
@@ -64,6 +84,8 @@ add_task(async function enterprise_disabled() {
     "reason",
     "no-welcome",
   ]);
+
+  await cleanupUpgrade();
   defaultPrefs.setBoolPref(pref, orig);
 });
 
@@ -72,6 +94,8 @@ add_task(async function show_major_upgrade() {
   const pref = "browser.startup.upgradeDialog.enabled";
   const orig = defaultPrefs.getBoolPref(pref, true);
   defaultPrefs.setBoolPref(pref, true);
+
+  let cleanupUpgrade = await forceMajorUpgrade();
 
   await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
   const [win] = await TestUtils.topicObserved("subdialog-loaded");
@@ -86,10 +110,7 @@ add_task(async function show_major_upgrade() {
   ]);
 
   await BrowserTestUtils.removeTab(gBrowser.selectedTab);
-  defaultPrefs.setBoolPref(pref, orig);
-});
 
-add_task(async function dont_reshow() {
   await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
 
   AssertEvents("Shouldn't reshow for upgrade dialog requirements", [
@@ -97,11 +118,87 @@ add_task(async function dont_reshow() {
     "reason",
     "already-shown",
   ]);
+
+  defaultPrefs.setBoolPref(pref, orig);
+  await cleanupUpgrade();
 });
 
-registerCleanupFunction(() => {
-  Cc["@mozilla.org/browser/clh;1"].getService(
-    Ci.nsIBrowserHandler
-  ).majorUpgrade = false;
-  Services.prefs.clearUserPref("browser.startup.upgradeDialog.version");
+add_task(async function test_mr2022_upgradeDialogEnabled() {
+  const FALLBACK_PREF = "browser.startup.upgradeDialog.enabled";
+
+  async function runMajorReleaseTest(
+    { onboarding = undefined, enabled = undefined, fallbackPref = undefined },
+    expected
+  ) {
+    info("Testing upgradeDialog with:");
+    info(`  majorRelease2022.onboarding=${onboarding}`);
+    info(`  upgradeDialog.enabled=${enabled}`);
+    info(`  ${FALLBACK_PREF}=${fallbackPref}`);
+
+    let mr2022Cleanup = async () => {};
+    let upgradeDialogCleanup = async () => {};
+
+    if (typeof onboarding !== "undefined") {
+      mr2022Cleanup = await ExperimentFakes.enrollWithFeatureConfig({
+        featureId: "majorRelease2022",
+        value: { onboarding },
+      });
+    }
+
+    if (typeof enabled !== "undefined") {
+      upgradeDialogCleanup = await ExperimentFakes.enrollWithFeatureConfig({
+        featureId: "upgradeDialog",
+        value: { enabled },
+      });
+    }
+
+    if (typeof fallbackPref !== "undefined") {
+      await SpecialPowers.pushPrefEnv({
+        set: [[FALLBACK_PREF, fallbackPref]],
+      });
+    }
+
+    const cleanupForcedUpgrade = await forceMajorUpgrade();
+
+    try {
+      await BROWSER_GLUE._maybeShowDefaultBrowserPrompt();
+      AssertEvents(`Upgrade dialog ${expected ? "shown" : "not shown"}`, [
+        "trigger",
+        "reason",
+        expected ? "satisfied" : "disabled",
+      ]);
+
+      if (expected) {
+        const [win] = await TestUtils.topicObserved("subdialog-loaded");
+        win.close();
+        await BrowserTestUtils.removeTab(gBrowser.selectedTab);
+      }
+    } finally {
+      await cleanupForcedUpgrade();
+      if (typeof fallbackPref !== "undefined") {
+        await SpecialPowers.popPrefEnv();
+      }
+      await upgradeDialogCleanup();
+      await mr2022Cleanup();
+    }
+  }
+
+  await runMajorReleaseTest({ onboarding: true }, true);
+  await runMajorReleaseTest({ onboarding: true, enabled: false }, true);
+  await runMajorReleaseTest({ onboarding: true, fallbackPref: false }, true);
+
+  await runMajorReleaseTest({ onboarding: false }, false);
+  await runMajorReleaseTest({ onboarding: false, enabled: true }, false);
+  await runMajorReleaseTest({ onboarding: false, fallbackPref: true }, false);
+
+  await runMajorReleaseTest({ enabled: true }, true);
+  await runMajorReleaseTest({ enabled: true, fallbackPref: false }, true);
+  await runMajorReleaseTest({ fallbackPref: true }, true);
+
+  await runMajorReleaseTest({ enabled: false }, false);
+  await runMajorReleaseTest({ enabled: false, fallbackPref: true }, false);
+  await runMajorReleaseTest({ fallbackPref: false }, false);
+
+  // Test the default configuration.
+  await runMajorReleaseTest({}, true);
 });
