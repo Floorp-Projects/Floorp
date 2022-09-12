2b093483cbe023cd2d8d58a5512a591acf5e7f7c
Sarah Clements <sclements@mozilla.com>
2022-09-12 09:30:11 +0000

    WIP Bug 1784363 - Update the fxa entrypoint to use fx-view r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D156081

--- browser/components/firefoxview/firefox-view-tabs-setup-manager.sys.mjs
+++ browser/components/firefoxview/firefox-view-tabs-setup-manager.sys.mjs
@@ -420,7 +420,7 @@ export const TabsSetupFlowManager = new (class {
 
   async openFxAPairDevice(window) {
     const url = await lazy.fxAccounts.constructor.config.promisePairingURI({
-      entrypoint: "fxa_app_menu", // Bug 1784363 - update to "fx-view" when available
+      entrypoint: "fx-view",
     });
     openTabInWindow(window, url, true);
     Services.telemetry.recordEvent("firefoxview", "fxa_mobile", "sync", null, {
