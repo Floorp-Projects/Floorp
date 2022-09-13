67c5b63cae3c15bc968a0dd0c59ff9369d05d15c
Eden Chuang <echuang@mozilla.com>
2022-09-13 09:03:41 +0000

    Bug 1715547 - Remove ServiceWorker::Inner r=dom-worker-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D139000

--- dom/serviceworkers/ServiceWorkerChild.cpp
+++ dom/serviceworkers/ServiceWorkerChild.cpp
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "ServiceWorkerChild.h"
-#include "RemoteServiceWorkerImpl.h"
+#include "ServiceWorker.h"
 #include "mozilla/dom/WorkerCommon.h"
 #include "mozilla/dom/WorkerRef.h"
 
@@ -46,13 +46,13 @@ RefPtr<ServiceWorkerChild> ServiceWorkerChild::Create() {
 ServiceWorkerChild::ServiceWorkerChild()
     : mOwner(nullptr), mTeardownStarted(false) {}
 
-void ServiceWorkerChild::SetOwner(RemoteServiceWorkerImpl* aOwner) {
+void ServiceWorkerChild::SetOwner(ServiceWorker* aOwner) {
   MOZ_DIAGNOSTIC_ASSERT(!mOwner);
   MOZ_DIAGNOSTIC_ASSERT(aOwner);
   mOwner = aOwner;
 }
 
-void ServiceWorkerChild::RevokeOwner(RemoteServiceWorkerImpl* aOwner) {
+void ServiceWorkerChild::RevokeOwner(ServiceWorker* aOwner) {
   MOZ_DIAGNOSTIC_ASSERT(mOwner);
   MOZ_DIAGNOSTIC_ASSERT(aOwner == mOwner);
   mOwner = nullptr;
