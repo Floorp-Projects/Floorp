9b2db7bcce935dc8c127bccdad5bd5d30b4f21f6
Luca Greco <lgreco@mozilla.com>
2022-09-13 23:42:19 +0000

    Bug 1790115 - allow static theme resources in other extensions also in private browsing windows. r=mixedpuppy, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D156986

--- toolkit/components/extensions/test/browser/browser_ext_themes_getCurrent_differentExt.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_getCurrent_differentExt.js
@@ -2,25 +2,25 @@
 
 // This test checks whether browser.theme.getCurrent() works correctly when theme
 // does not originate from extension querying the theme.
-
-add_task(async function test_getcurrent() {
-  const theme = ExtensionTestUtils.loadExtension({
-    manifest: {
-      theme: {
-        images: {
-          theme_frame: "image1.png",
-        },
-        colors: {
-          frame: ACCENT_COLOR,
-          tab_background_text: TEXT_COLOR,
-        },
+const THEME = {
+  manifest: {
+    theme: {
+      images: {
+        theme_frame: "image1.png",
+      },
+      colors: {
+        frame: ACCENT_COLOR,
+        tab_background_text: TEXT_COLOR,
       },
     },
-    files: {
-      "image1.png": BACKGROUND,
-    },
-  });
+  },
+  files: {
+    "image1.png": BACKGROUND,
+  },
+};
 
+add_task(async function test_getcurrent() {
+  const theme = ExtensionTestUtils.loadExtension(THEME);
   const extension = ExtensionTestUtils.loadExtension({
     background() {
       browser.theme.onUpdated.addListener(() => {
@@ -80,4 +80,70 @@ add_task(async function test_getcurrent() {
   );
 
   await extension.unload();
+});
+
+add_task(async function test_getcurrent_privateBrowsing() {
+  const theme = ExtensionTestUtils.loadExtension(THEME);
+
+  const extension = ExtensionTestUtils.loadExtension({
+    incognitoOverride: "spanning",
+    manifest: {
+      sidebar_action: {
+        default_panel: "sidebar.html",
+      },
+    },
+    files: {
+      "sidebar.html": `<!DOCTYPE html>
+        <html>
+          <body>
+            Test Extension Sidebar
+            <script src="sidebar.js"></script>
+          </body>
+        </html>
+      `,
+      "sidebar.js": function() {
+        browser.theme.getCurrent().then(theme => {
+          if (!theme?.images) {
+            browser.test.fail(
+              `Missing expected images from theme.getCurrent result`
+            );
+            return;
+          }
+
+          // Try to access the theme_frame image
+          fetch(theme.images.theme_frame)
+            .then(() => {
+              browser.test.sendMessage("theme-image", { success: true });
+            })
+            .catch(e => {
+              browser.test.sendMessage("theme-image", {
+                success: false,
+                error: e.message,
+              });
+            });
+        });
+      },
+    },
+  });
+
+  await extension.startup();
+  await theme.startup();
+
+  const privateWin = await BrowserTestUtils.openNewBrowserWindow({
+    private: true,
+  });
+
+  const { ExtensionCommon } = ChromeUtils.import(
+    "resource://gre/modules/ExtensionCommon.jsm"
+  );
+  const { makeWidgetId } = ExtensionCommon;
+  privateWin.SidebarUI.show(`${makeWidgetId(extension.id)}-sidebar-action`);
+
+  let imageLoaded = extension.awaitMessage("theme-image");
+  Assert.deepEqual(await imageLoaded, { success: true }, "theme image loaded");
+
+  await extension.unload();
+  await theme.unload();
+
+  await BrowserTestUtils.closeWindow(privateWin);
 });
