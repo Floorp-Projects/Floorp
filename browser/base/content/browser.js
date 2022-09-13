dd70dcbbdc1f08f80d1e4e258ad977825aab2813
surapunoyousei <ryosukeusuusu@gmail.com>
2022-09-13 22:19:09 +0900

    [FB:028] Dual Theme | Add Dual Theme
     https://github.com/Floorp-Projects/Floorp/pull/34

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1741,6 +1741,7 @@ var gBrowserInit = {
   },
 
   onLoad() {
+    Services.prefs.addObserver("floorp.dualtheme.theme",this.onBeforeInitialXULLayout.bind(this))
     gBrowser.addEventListener("DOMUpdateBlockedPopups", gPopupBlockerObserver);
 
     window.addEventListener("AppCommand", HandleAppCommandEvent, true);
