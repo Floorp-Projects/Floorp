005c0cce86a6fbe1a163001f255fe0603128c6b0
Dale Harvey <dale@arandomurl.com>
2022-09-14 13:06:31 +0000

    Bug 1790681 - Enable separatePrivateDefault by default. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D157314

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3897,7 +3897,7 @@ pref("browser.search.log", false);
 pref("browser.search.update", true);
 pref("browser.search.suggest.enabled", true);
 pref("browser.search.suggest.enabled.private", false);
-pref("browser.search.separatePrivateDefault", false);
+pref("browser.search.separatePrivateDefault", true);
 pref("browser.search.separatePrivateDefault.ui.enabled", false);
 pref("browser.search.removeEngineInfobar.enabled", true);
 
