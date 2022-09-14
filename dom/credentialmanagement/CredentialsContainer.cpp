302dc2cb7b26a4b5d2db201c4bc11f214e6481fb
Benjamin VanderSloot <bvandersloot@mozilla.com>
2022-09-14 02:02:44 +0000

    Bug 1782081 - Only call WebAuthN code when requesting a `publicKey` credential, r=dveditz,emilio
    This may look unnecessary now, but opens up the Credential Manager API to be used by FedCM
    and any other Credential variants.
    I also opened up the API to allow rejecting with different error types.
    And there are some tests to support this
    
    Differential Revision: https://phabricator.services.mozilla.com/D153587

--- dom/credentialmanagement/CredentialsContainer.cpp
+++ dom/credentialmanagement/CredentialsContainer.cpp
@@ -4,14 +4,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/dom/Credential.h"
 #include "mozilla/dom/CredentialsContainer.h"
+#include "mozilla/dom/IdentityCredential.h"
 #include "mozilla/dom/Promise.h"
+#include "mozilla/StaticPrefs_security.h"
 #include "mozilla/dom/WebAuthnManager.h"
 #include "mozilla/dom/WindowGlobalChild.h"
 #include "mozilla/dom/WindowContext.h"
 #include "nsContentUtils.h"
 #include "nsFocusManager.h"
 #include "nsIDocShell.h"
+#include "nsGlobalWindowInner.h"
 
 namespace mozilla::dom {
 
@@ -23,22 +27,42 @@ NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(CredentialsContainer)
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END
 
-already_AddRefed<Promise> CreateAndReject(nsPIDOMWindowInner* aParent,
-                                          ErrorResult& aRv) {
+already_AddRefed<Promise> CreatePromise(nsPIDOMWindowInner* aParent,
+                                        ErrorResult& aRv) {
   MOZ_ASSERT(aParent);
-
   nsCOMPtr<nsIGlobalObject> global = do_QueryInterface(aParent);
   if (NS_WARN_IF(!global)) {
     aRv.Throw(NS_ERROR_FAILURE);
     return nullptr;
   }
-
   RefPtr<Promise> promise = Promise::Create(global, aRv);
   if (NS_WARN_IF(aRv.Failed())) {
     return nullptr;
   }
+  return promise.forget();
+}
 
-  promise->MaybeReject(NS_ERROR_DOM_NOT_ALLOWED_ERR);
+already_AddRefed<Promise> CreateAndRejectWithNotAllowed(
+    nsPIDOMWindowInner* aParent, ErrorResult& aRv) {
+  MOZ_ASSERT(aParent);
+  RefPtr<Promise> promise = CreatePromise(aParent, aRv);
+  if (!promise) {
+    return nullptr;
+  }
+  promise->MaybeRejectWithNotAllowedError(
+      "CredentialContainer request is not allowed."_ns);
+  return promise.forget();
+}
+
+already_AddRefed<Promise> CreateAndRejectWithNotSupported(
+    nsPIDOMWindowInner* aParent, ErrorResult& aRv) {
+  MOZ_ASSERT(aParent);
+  RefPtr<Promise> promise = CreatePromise(aParent, aRv);
+  if (!promise) {
+    return nullptr;
+  }
+  promise->MaybeRejectWithNotSupportedError(
+      "CredentialContainer request is not supported."_ns);
   return promise.forget();
 }
 
@@ -106,32 +130,95 @@ JSObject* CredentialsContainer::WrapObject(JSContext* aCx,
 
 already_AddRefed<Promise> CredentialsContainer::Get(
     const CredentialRequestOptions& aOptions, ErrorResult& aRv) {
-  if (!IsSameOriginWithAncestors(mParent) || !IsInActiveTab(mParent)) {
-    return CreateAndReject(mParent, aRv);
+  uint64_t totalOptions = 0;
+  if (aOptions.mPublicKey.WasPassed() &&
+      StaticPrefs::security_webauth_webauthn()) {
+    totalOptions += 1;
+  }
+  if (aOptions.mIdentity.WasPassed() &&
+      StaticPrefs::dom_security_credentialmanagement_identity_enabled()) {
+    totalOptions += 1;
+  }
+  if (totalOptions > 1) {
+    return CreateAndRejectWithNotSupported(mParent, aRv);
   }
 
-  EnsureWebAuthnManager();
-  return mManager->GetAssertion(aOptions.mPublicKey, aOptions.mSignal, aRv);
+  if (aOptions.mPublicKey.WasPassed() &&
+      StaticPrefs::security_webauth_webauthn()) {
+    if (!IsSameOriginWithAncestors(mParent) || !IsInActiveTab(mParent)) {
+      return CreateAndRejectWithNotAllowed(mParent, aRv);
+    }
+
+    EnsureWebAuthnManager();
+    return mManager->GetAssertion(aOptions.mPublicKey.Value(), aOptions.mSignal,
+                                  aRv);
+  }
+
+  if (aOptions.mIdentity.WasPassed() &&
+      StaticPrefs::dom_security_credentialmanagement_identity_enabled()) {
+    RefPtr<Promise> promise = CreatePromise(mParent, aRv);
+
+    IdentityCredential::DiscoverFromExternalSource(
+        mParent, aOptions, IsSameOriginWithAncestors(mParent))
+        ->Then(
+            GetCurrentSerialEventTarget(), __func__,
+            [promise](RefPtr<IdentityCredential> credential) {
+              promise->MaybeResolve(credential);
+            },
+            [promise](nsresult error) { promise->MaybeReject(error); });
+
+    return promise.forget();
+  }
+
+  return CreateAndRejectWithNotSupported(mParent, aRv);
 }
 
 already_AddRefed<Promise> CredentialsContainer::Create(
     const CredentialCreationOptions& aOptions, ErrorResult& aRv) {
-  if (!IsSameOriginWithAncestors(mParent) || !IsInActiveTab(mParent)) {
-    return CreateAndReject(mParent, aRv);
+  // Count the types of options provided. Must not be >1.
+  uint64_t totalOptions = 0;
+  if (aOptions.mPublicKey.WasPassed() &&
+      StaticPrefs::security_webauth_webauthn()) {
+    totalOptions += 1;
+  }
+  if (totalOptions > 1) {
+    return CreateAndRejectWithNotSupported(mParent, aRv);
   }
 
-  EnsureWebAuthnManager();
-  return mManager->MakeCredential(aOptions.mPublicKey, aOptions.mSignal, aRv);
+  if (aOptions.mPublicKey.WasPassed() &&
+      StaticPrefs::security_webauth_webauthn()) {
+    if (!IsSameOriginWithAncestors(mParent) || !IsInActiveTab(mParent)) {
+      return CreateAndRejectWithNotAllowed(mParent, aRv);
+    }
+
+    EnsureWebAuthnManager();
+    return mManager->MakeCredential(aOptions.mPublicKey.Value(),
+                                    aOptions.mSignal, aRv);
+  }
+
+  return CreateAndRejectWithNotSupported(mParent, aRv);
 }
 
 already_AddRefed<Promise> CredentialsContainer::Store(
     const Credential& aCredential, ErrorResult& aRv) {
-  if (!IsSameOriginWithAncestors(mParent) || !IsInActiveTab(mParent)) {
-    return CreateAndReject(mParent, aRv);
+  nsString type;
+  aCredential.GetType(type);
+  if (type.EqualsLiteral("public-key") &&
+      StaticPrefs::security_webauth_webauthn()) {
+    if (!IsSameOriginWithAncestors(mParent) || !IsInActiveTab(mParent)) {
+      return CreateAndRejectWithNotAllowed(mParent, aRv);
+    }
+
+    EnsureWebAuthnManager();
+    return mManager->Store(aCredential, aRv);
   }
 
-  EnsureWebAuthnManager();
-  return mManager->Store(aCredential, aRv);
+  if (type.EqualsLiteral("identity") &&
+      StaticPrefs::dom_security_credentialmanagement_identity_enabled()) {
+    return CreateAndRejectWithNotSupported(mParent, aRv);
+  }
+
+  return CreateAndRejectWithNotSupported(mParent, aRv);
 }
 
 already_AddRefed<Promise> CredentialsContainer::PreventSilentAccess(
