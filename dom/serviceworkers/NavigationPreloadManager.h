875b4ba1b7f5570c602665782bb212cb8c0a72fb
Butkovits Atila <abutkovits@mozilla.com>
2022-09-14 22:53:43 +0300

    Backed out 4 changesets (bug 1715547) for frequent autoland crashes. CLOSED TREE
    Backed out changeset 64e584ac229b (bug 1715547)
    Backed out changeset b02cb05ea06c (bug 1715547)
    Backed out changeset 6d0a9db1638c (bug 1715547)
    Backed out changeset 93250012e5ac (bug 1715547)

--- dom/serviceworkers/NavigationPreloadManager.h
+++ dom/serviceworkers/NavigationPreloadManager.h
@@ -30,13 +30,11 @@ class NavigationPreloadManager final : public nsISupports,
 
   static bool IsEnabled(JSContext* aCx, JSObject* aGlobal);
 
-  explicit NavigationPreloadManager(
-      RefPtr<ServiceWorkerRegistration>& aServiceWorkerRegistration);
+  NavigationPreloadManager(nsCOMPtr<nsIGlobalObject>&& aGlobal,
+                           RefPtr<ServiceWorkerRegistration::Inner>& aInner);
 
   // Webidl binding
-  nsIGlobalObject* GetParentObject() const {
-    return mServiceWorkerRegistration->GetParentObject();
-  }
+  nsIGlobalObject* GetParentObject() const { return mGlobal; }
 
   JSObject* WrapObject(JSContext* aCx,
                        JS::Handle<JSObject*> aGivenProto) override;
@@ -57,7 +55,8 @@ class NavigationPreloadManager final : public nsISupports,
   // General method for Enable()/Disable()
   already_AddRefed<Promise> SetEnabled(bool aEnabled, ErrorResult& aError);
 
-  RefPtr<ServiceWorkerRegistration> mServiceWorkerRegistration;
+  nsCOMPtr<nsIGlobalObject> mGlobal;
+  RefPtr<ServiceWorkerRegistration::Inner> mInner;
 };
 
 }  // namespace mozilla::dom
