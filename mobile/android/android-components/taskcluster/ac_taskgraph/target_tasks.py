778a7a975de3eb76565fd4d9d3e4c16e245104f1
Julien Cristau <jcristau@mozilla.com>
2022-09-15 17:08:45 +0200

    [components] Trigger a nightly graph when the geckoview major version changes
    Fixes https://bugzilla.mozilla.org/show_bug.cgi?id=1791748

--- mobile/android/android-components/taskcluster/ac_taskgraph/target_tasks.py
+++ mobile/android/android-components/taskcluster/ac_taskgraph/target_tasks.py
@@ -3,10 +3,13 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
+import re
 from redo import retry
 
-from taskgraph.target_tasks import _target_task
+from mozilla_version.maven import MavenVersion
+from taskgraph.target_tasks import _target_task, target_tasks_default
 from taskgraph.util.taskcluster import find_task_id
+from taskgraph.util.vcs import get_repository
 
 
 def index_exists(index_path, reason=""):
@@ -47,3 +50,32 @@ def target_tasks_release(full_task_graph, parameters, graph_config):
         return task.attributes.get("build-type", "") == "release"
 
     return [l for l, t in full_task_graph.tasks.items() if filter(t, parameters)]
+
+
+@_target_task("default")
+def target_tasks_ac_default(full_task_graph, parameters, graph_config):
+    def filter(task):
+        # Trigger the nightly cron hook when the GV major version changes
+        if task.kind != "trigger-nightly":
+            return True
+        repo = get_repository(os.getcwd())
+        if get_gv_version(repo, parameters["base_rev"]) != get_gv_version(
+            repo, parameters["head_rev"]
+        ):
+            return True
+        return False
+
+    return [
+        l
+        for l, t in full_task_graph.tasks.items()
+        if l in target_tasks_default(full_task_graph, parameters, graph_config)
+        and filter(t)
+    ]
+
+
+def get_gv_version(repo, revision):
+    gecko_kt = repo.run("show", f"{revision}:buildSrc/src/main/java/Gecko.kt")
+    match = re.search(r'version = "([^"]*)"', gecko_kt, re.MULTILINE)
+    if not match:
+        raise Exception(f"Couldn't parse geckoview version on commit {revision}")
+    return MavenVersion.parse(match.group(1)).major_number
