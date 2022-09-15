30dc7484b53ec6e8074856074e09bdbc8e84f013
Michael Froman <mfroman@mozilla.com>
2022-09-15 17:16:00 +0000

    Bug 1790824 - improve macOS compatibility for libwebrtc fast forward scripts. r=dbaker
    Differential Revision: https://phabricator.services.mozilla.com/D157494

--- dom/media/webrtc/third_party_build/loop-ff.sh
+++ dom/media/webrtc/third_party_build/loop-ff.sh
@@ -4,7 +4,7 @@
 LOOP_OUTPUT_LOG=~/log-loop-ff.txt
 
 # Print an Error message if `set -eE` causes the script to exit due to a failed command
-trap 'echo "*** ERROR *** $? $LINENO loop-ff did not complete successfully!" |& tee -a $LOOP_OUTPUT_LOG' ERR 
+trap 'echo "*** ERROR *** $? $LINENO loop-ff did not complete successfully!" 2>&1| tee -a $LOOP_OUTPUT_LOG' ERR
 
 source dom/media/webrtc/third_party_build/use_config_env.sh
 
@@ -59,42 +59,42 @@ MOZ_LIBWEBRTC_NEXT_BASE=`cd $MOZ_LIBWEBRTC_SRC ; \
 git log --oneline --ancestry-path $MOZ_LIBWEBRTC_BASE^..main \
  | tail -2 | head -1 | awk '{print$1;}'`
 
-echo "============ loop ff ============" |& tee -a $LOOP_OUTPUT_LOG
-echo "===loop-ff=== Moving from moz-libwebrtc commit $MOZ_LIBWEBRTC_BASE to $MOZ_LIBWEBRTC_NEXT_BASE" |& tee -a $LOOP_OUTPUT_LOG
-bash dom/media/webrtc/third_party_build/fast-forward-libwebrtc.sh |& tee -a $LOOP_OUTPUT_LOG
+echo "============ loop ff ============" 2>&1| tee -a $LOOP_OUTPUT_LOG
+echo "===loop-ff=== Moving from moz-libwebrtc commit $MOZ_LIBWEBRTC_BASE to $MOZ_LIBWEBRTC_NEXT_BASE" 2>&1| tee -a $LOOP_OUTPUT_LOG
+bash dom/media/webrtc/third_party_build/fast-forward-libwebrtc.sh 2>&1| tee -a $LOOP_OUTPUT_LOG
 #bash dom/media/webrtc/third_party_build/fast-forward-libwebrtc.sh
 
 MOZ_CHANGED=`hg diff -c tip --stat | egrep -ve "README.moz-ff-commit|README.mozilla|files changed," | wc -l` || echo -n "0" > /dev/null
 GIT_CHANGED=`cd $MOZ_LIBWEBRTC_SRC ; git show --oneline --name-only $MOZ_LIBWEBRTC_NEXT_BASE | wc -l`
 let GIT_CHANGED--
-echo "===loop-ff=== Verify number of files changed MOZ($MOZ_CHANGED) GIT($GIT_CHANGED)" |& tee -a $LOOP_OUTPUT_LOG
+echo "===loop-ff=== Verify number of files changed MOZ($MOZ_CHANGED) GIT($GIT_CHANGED)" 2>&1| tee -a $LOOP_OUTPUT_LOG
 if [ $MOZ_CHANGED -ne $GIT_CHANGED ]; then
-  echo "MOZ_CHANGED $MOZ_CHANGED should equal GIT_CHANGED $GIT_CHANGED" |& tee -a $LOOP_OUTPUT_LOG
+  echo "MOZ_CHANGED $MOZ_CHANGED should equal GIT_CHANGED $GIT_CHANGED" 2>&1| tee -a $LOOP_OUTPUT_LOG
   exit 1
 fi
 
 MODIFIED_BUILD_RELATED_FILE_CNT=`hg diff -c tip --stat \
     --include 'third_party/libwebrtc/**BUILD.gn' \
     --include 'third_party/libwebrtc/webrtc.gni' \
-    | wc -l`
-echo "===loop-ff=== Modified BUILD.gn (or webrtc.gni) files: $MODIFIED_BUILD_RELATED_FILE_CNT" |& tee -a $LOOP_OUTPUT_LOG
+    | wc -l | tr -d " "`
+echo "===loop-ff=== Modified BUILD.gn (or webrtc.gni) files: $MODIFIED_BUILD_RELATED_FILE_CNT" 2>&1| tee -a $LOOP_OUTPUT_LOG
 if [ "x$MODIFIED_BUILD_RELATED_FILE_CNT" != "x0" ]; then
-  echo "===loop-ff=== Regenerate build files" |& tee -a $LOOP_OUTPUT_LOG
+  echo "===loop-ff=== Regenerate build files" 2>&1| tee -a $LOOP_OUTPUT_LOG
   ./mach python python/mozbuild/mozbuild/gn_processor.py \
-      dom/media/webrtc/third_party_build/gn-configs/webrtc.json |& tee -a $LOOP_OUTPUT_LOG
+      dom/media/webrtc/third_party_build/gn-configs/webrtc.json 2>&1| tee -a $LOOP_OUTPUT_LOG
 
   MOZ_BUILD_CHANGE_CNT=`hg status third_party/libwebrtc \
-      --include 'third_party/libwebrtc/**moz.build' | wc -l`
+      --include 'third_party/libwebrtc/**moz.build' | wc -l | tr -d " "`
   if [ "x$MOZ_BUILD_CHANGE_CNT" != "x0" ]; then
-    echo "===loop-ff=== Detected modified moz.build files, commiting" |& tee -a $LOOP_OUTPUT_LOG
+    echo "===loop-ff=== Detected modified moz.build files, commiting" 2>&1| tee -a $LOOP_OUTPUT_LOG
   fi
 
   export UPSTREAM_COMMIT=$MOZ_LIBWEBRTC_NEXT_BASE
-  bash dom/media/webrtc/third_party_build/commit-build-file-changes.sh |& tee -a $LOOP_OUTPUT_LOG
+  bash dom/media/webrtc/third_party_build/commit-build-file-changes.sh 2>&1| tee -a $LOOP_OUTPUT_LOG
 fi
 
-echo "===loop-ff=== Test build" |& tee -a $LOOP_OUTPUT_LOG
-./mach build |& tee -a $LOOP_OUTPUT_LOG
+echo "===loop-ff=== Test build" 2>&1| tee -a $LOOP_OUTPUT_LOG
+./mach build 2>&1| tee -a $LOOP_OUTPUT_LOG
 
 if [ ! "x$MOZ_STOP_AFTER_COMMIT" = "x" ]; then
 if [ $MOZ_LIBWEBRTC_NEXT_BASE = $MOZ_STOP_AFTER_COMMIT ]; then
@@ -104,4 +104,4 @@ fi
 
 done
 
-echo "===loop-ff=== Completed fast-foward to $MOZ_STOP_AFTER_COMMIT" |& tee -a $LOOP_OUTPUT_LOG
+echo "===loop-ff=== Completed fast-foward to $MOZ_STOP_AFTER_COMMIT" 2>&1| tee -a $LOOP_OUTPUT_LOG
