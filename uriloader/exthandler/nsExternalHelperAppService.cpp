96cb8d5a4a1f755fb5ac262ea1baae098c1c62e9
Marco Bonardo <mbonardo@mozilla.com>
2022-09-16 09:47:05 +0000

    Bug 1789500 - filename sanitization may cut surrogate pairs when truncating. r=NeilDeakin, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D157516

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -3598,7 +3598,10 @@ void nsExternalHelperAppService::SanitizeFileName(nsAString& aFileName,
           nextChar = '_';
         }
 
-        lastNonTrimmable = int32_t(outFileName.Length()) + 1;
+        // Don't truncate surrogate pairs in the middle.
+        lastNonTrimmable =
+            int32_t(outFileName.Length()) +
+            (NS_IS_HIGH_SURROGATE(H_SURROGATE(nextChar)) ? 2 : 1);
       }
     }
 
