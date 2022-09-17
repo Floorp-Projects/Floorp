46eda328460fada4b5a26a3aff47a97051296458
surapunoyousei <ryosukeusuusu@gmail.com>
2022-09-17 18:58:02 +0900

    [FB:008]: Floorp Home | Convert to webp

--- browser/components/newtab/content-src/components/Background/Background.jsx
+++ browser/components/newtab/content-src/components/Background/Background.jsx
@@ -3,6 +3,6 @@ import React, { useState } from "react";
 const imgLength = 100;
 
 export function Background() {
-  const [imgSrc] = useState(`chrome://browser/skin/newtabbg-${Math.floor(Math.random() * imgLength)}.jpg`);
+  const [imgSrc] = useState(`chrome://browser/skin/newtabbg-${Math.floor(Math.random() * imgLength)}.webp`);
   return <div id="background" style={{background: `url(${imgSrc})`}} />
 }
