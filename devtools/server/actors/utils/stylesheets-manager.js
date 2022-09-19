840bc3b872d55df4fd9565f2f78fa83f8383f7fd
Nicolas Chevobbe <nchevobbe@mozilla.com>
2022-09-19 11:51:59 +0000

    Bug 1790595 - [devtools] Fix performance regression in _getMediaRules. r=jdescottes.
    Differential Revision: https://phabricator.services.mozilla.com/D157206

--- devtools/server/actors/utils/stylesheets-manager.js
+++ devtools/server/actors/utils/stylesheets-manager.js
@@ -582,11 +582,19 @@ class StyleSheetsManager extends EventEmitter {
     this._mqlList = [];
 
     const styleSheetRules = await this._getCSSRules(styleSheet);
+    const document = styleSheet.associatedDocument;
+    const win = document?.ownerGlobal;
+    const CSSGroupingRule = win?.CSSGroupingRule;
 
     // We need to go through nested rules to extract all the rules we're interested in
     const rules = [];
     const traverseRules = ruleList => {
       for (const rule of ruleList) {
+        // Don't go further if the rule can't hold other rules (e.g. not a @media, @supports, …)
+        if (!CSSGroupingRule || !CSSGroupingRule.isInstance(rule)) {
+          continue;
+        }
+
         if (rule.type === CSSRule.MEDIA_RULE) {
           rules.push(rule);
         }
@@ -602,8 +610,7 @@ class StyleSheetsManager extends EventEmitter {
       let matches = false;
 
       try {
-        const window = styleSheet.ownerNode.ownerGlobal;
-        const mql = window.matchMedia(rule.media.mediaText);
+        const mql = win.matchMedia(rule.media.mediaText);
         matches = mql.matches;
         mql.onchange = this._onMatchesChange.bind(this, resourceId, index);
         this._mqlList.push(mql);
