191cf56977fa4ac2dedfcf1e11efa1967ef992a4
Dragana Damjanovic <dd.mozilla@gmail.com>
2022-09-20 21:18:22 +0000

    Bug 1785775 - Add tests for the cert verification with and without proxy r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D154972

--- netwerk/test/unit/test_servers.js
+++ netwerk/test/unit/test_servers.js
@@ -196,6 +196,7 @@ add_task(async function test_https_proxy() {
     Ci.nsIX509CertDB
   );
   addCertFromFile(certdb, "http2-ca.pem", "CTu,u,u");
+  addCertFromFile(certdb, "proxy-ca.pem", "CTu,u,u");
 
   let proxy = new NodeHTTPSProxyServer();
   await proxy.start();
@@ -244,6 +245,7 @@ add_task(async function test_http2_proxy() {
     Ci.nsIX509CertDB
   );
   addCertFromFile(certdb, "http2-ca.pem", "CTu,u,u");
+  addCertFromFile(certdb, "proxy-ca.pem", "CTu,u,u");
 
   let proxy = new NodeHTTP2ProxyServer();
   await proxy.start();
