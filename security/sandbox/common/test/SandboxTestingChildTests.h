1a82371659d439f493c8e89e82999f541e47002f
Jed Davis <jld@mozilla.com>
2022-09-21 17:57:54 +0000

    Bug 1780312 - Part 2: Allow fstatfs in the Linux RDD sandbox policy. r=gcp
    As discussed in the last patch, allowing `fstatfs` will also make
    `statfs` work on any path that the process could open for reading
    (subject to sandbox policy).
    
    Differential Revision: https://phabricator.services.mozilla.com/D157542

--- security/sandbox/common/test/SandboxTestingChildTests.h
+++ security/sandbox/common/test/SandboxTestingChildTests.h
@@ -23,6 +23,7 @@
 #    include <sys/prctl.h>
 #    include <sys/resource.h>
 #    include <sys/socket.h>
+#    include <sys/statfs.h>
 #    include <sys/syscall.h>
 #    include <sys/sysmacros.h>
 #    include <sys/time.h>
@@ -463,6 +464,11 @@ void RunTestsContent(SandboxTestingChild* child) {
                      });
   }
 
+  child->ErrnoTest("statfs"_ns, true, [] {
+    struct statfs sf;
+    return statfs("/usr/share", &sf);
+  });
+
 #    ifdef MOZ_X11
   // Check that X11 access is blocked (bug 1129492).
   // This will fail if security.sandbox.content.headless is turned off.
@@ -636,6 +642,11 @@ void RunTestsRDD(SandboxTestingChild* child) {
   // used by fbdev) and numbers starting from 200 (0xc8).
   child->ErrnoValueTest("ioctl_nvidia"_ns, ENOTTY,
                         [] { return ioctl(0, 0x46c8, nullptr); });
+
+  child->ErrnoTest("statfs"_ns, true, [] {
+    struct statfs sf;
+    return statfs("/usr/share", &sf);
+  });
 
 #  elif XP_MACOSX
   RunMacTestLaunchProcess(child);
