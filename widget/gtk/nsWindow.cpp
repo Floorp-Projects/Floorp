976d2b3f62a799591951ee878cef7d7100d7c454
Norisz Fay <nfay@mozilla.com>
2022-09-21 16:20:18 +0300

    Backed out changeset ddd9b4ac6f84 (bug 1791752) for causing multiple failures CLOSED TREE

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -8776,7 +8776,6 @@ void nsWindow::SetCompositorWidgetDelegate(CompositorWidgetDelegate* delegate) {
     MOZ_ASSERT(mCompositorWidgetDelegate,
                "nsWindow::SetCompositorWidgetDelegate called with a "
                "non-PlatformCompositorWidgetDelegate");
-    MOZ_DIAGNOSTIC_ASSERT(mCompositorWidgetDelegate->AsGtkCompositorWidget());
     if (mIsMapped) {
       ConfigureCompositor();
     }
