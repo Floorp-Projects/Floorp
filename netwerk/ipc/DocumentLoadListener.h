891aaaa24a311ec9110e163299355d28177813e3
Manuel Bucher <manuel@mozilla.com>
2022-09-21 08:52:02 +0000

    Bug 1771867 - Move structs to NeckoCommon to reuse them in Early Hint Preloader code r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D157492

--- netwerk/ipc/DocumentLoadListener.h
+++ netwerk/ipc/DocumentLoadListener.h
@@ -484,25 +484,6 @@ class DocumentLoadListener : public nsIInterfaceRequestor,
                        LogMimeTypeMismatchParams>;
   nsTArray<SecurityWarningFunction> mSecurityWarningFunctions;
 
-  struct OnStartRequestParams {
-    nsCOMPtr<nsIRequest> request;
-  };
-  struct OnDataAvailableParams {
-    nsCOMPtr<nsIRequest> request;
-    nsCString data;
-    uint64_t offset;
-    uint32_t count;
-  };
-  struct OnStopRequestParams {
-    nsCOMPtr<nsIRequest> request;
-    nsresult status;
-  };
-  struct OnAfterLastPartParams {
-    nsresult status;
-  };
-  using StreamListenerFunction =
-      mozilla::Variant<OnStartRequestParams, OnDataAvailableParams,
-                       OnStopRequestParams, OnAfterLastPartParams>;
   // TODO Backtrack this.
   // The set of nsIStreamListener functions that got called on this
   // listener, so that we can replay them onto the replacement channel's
