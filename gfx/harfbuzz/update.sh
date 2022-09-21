c658575de510e11014d9c48104cf2264fbc1cdec
Ryan VanderMeulen <ryanvm@gmail.com>
2022-09-21 19:19:27 +0000

    Bug 1791290 - Update HarfBuzz to 5.2.0. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D157590

--- gfx/harfbuzz/update.sh
+++ gfx/harfbuzz/update.sh
@@ -5,7 +5,7 @@
 
 MY_TEMP_DIR=`mktemp -d -t harfbuzz_update.XXXXXX` || exit 1
 
-VERSION=5.1.0
+VERSION=5.2.0
 
 git clone https://github.com/harfbuzz/harfbuzz ${MY_TEMP_DIR}/harfbuzz
 git -C ${MY_TEMP_DIR}/harfbuzz checkout ${VERSION}
