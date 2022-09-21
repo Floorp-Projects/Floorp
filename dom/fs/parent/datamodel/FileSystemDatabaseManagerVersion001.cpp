9afd6972b03ff7642393d33b1a2230770772c9d0
Randell Jesup <rjesup@wgate.com>
2022-09-21 08:29:01 +0000

    Bug 1791384: Don't allow overwrite of files/directories using move() r=dom-storage-reviewers,jari
    Depends on D150663
    
    Differential Revision: https://phabricator.services.mozilla.com/D157643

--- dom/fs/parent/datamodel/FileSystemDatabaseManagerVersion001.cpp
+++ dom/fs/parent/datamodel/FileSystemDatabaseManagerVersion001.cpp
@@ -705,6 +705,26 @@ Result<bool, QMResult> FileSystemDatabaseManagerVersion001::MoveEntry(
   }
 #endif
 
+  // Are we actually renaming?
+  if ((aHandle.childName() == aNewDesignation.childName()) &&
+      (aHandle.parentId() == aNewDesignation.parentId())) {
+    return true;
+  }
+
+  // XXX Note: the spec doesn't mention this case.  The WPT tests assume
+  // that you can overwrite using move().
+
+  // If the destination file exists, fail explicitly.  Spec author plans to
+  // revise the spec
+  QM_TRY_UNWRAP(exists, DoesFileExist(mConnection, aNewDesignation));
+  if (exists) {
+    return Err(QMResult(NS_ERROR_DOM_NO_MODIFICATION_ALLOWED_ERR));
+  }
+  QM_TRY_UNWRAP(exists, DoesDirectoryExist(mConnection, aNewDesignation));
+  if (exists) {
+    return Err(QMResult(NS_ERROR_DOM_NO_MODIFICATION_ALLOWED_ERR));
+  }
+
   const nsLiteralCString updateEntryParentQuery =
       "UPDATE Entries "
       "SET parent = :parent "
