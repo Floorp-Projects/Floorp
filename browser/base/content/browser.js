6233170f3ae5a04ec421bfb908ccfff7f35cb719
kaonasi_biwa(加尾梨 毘和) <85387717+kaonasi-biwa@users.noreply.github.com>
2022-09-21 20:17:25 +0900

    Fix DualTheme`s Bug(2/2)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1745,7 +1745,6 @@ var gBrowserInit = {
   },
 
   onLoad() {
-    Services.prefs.addObserver("floorp.dualtheme.theme",this.onBeforeInitialXULLayout.bind(this))
     gBrowser.addEventListener("DOMUpdateBlockedPopups", gPopupBlockerObserver);
 
     window.addEventListener("AppCommand", HandleAppCommandEvent, true);
