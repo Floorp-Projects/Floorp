dc5452d857c18103f43044cd3a60b37be677af17
Michael Froman <mfroman@mozilla.com>
2022-09-22 14:15:18 -0500

    Bug 1790097 - Vendor libwebrtc from c397fc62d8
    Upstream commit: https://webrtc.googlesource.com/src/+/c397fc62d84f4aa13a7b2a963811a72af376e1b9
        Use string_view to pass track ids to constructors
    
        Bug: webrtc:13579
        Change-Id: Icbd08d5fba9d150295675d730b7261d23992488c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/264441
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Niels Moller <nisse@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37035}

--- third_party/libwebrtc/pc/audio_track.cc
+++ third_party/libwebrtc/pc/audio_track.cc
@@ -17,12 +17,12 @@ namespace webrtc {
 
 // static
 rtc::scoped_refptr<AudioTrack> AudioTrack::Create(
-    const std::string& id,
+    absl::string_view id,
     const rtc::scoped_refptr<AudioSourceInterface>& source) {
   return rtc::make_ref_counted<AudioTrack>(id, source);
 }
 
-AudioTrack::AudioTrack(const std::string& label,
+AudioTrack::AudioTrack(absl::string_view label,
                        const rtc::scoped_refptr<AudioSourceInterface>& source)
     : MediaStreamTrack<AudioTrackInterface>(label), audio_source_(source) {
   if (audio_source_) {
