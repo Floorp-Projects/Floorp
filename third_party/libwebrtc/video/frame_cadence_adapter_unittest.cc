c490ecd44b51a4f6591ffabaebd038e016fcb58b
Michael Froman <mfroman@mozilla.com>
2022-09-22 11:12:10 -0500

    Bug 1790097 - Vendor libwebrtc from f7f0b2108f
    Upstream commit: https://webrtc.googlesource.com/src/+/f7f0b2108fed0471423bee53d1cf2b113cccb525
        ZeroHz: Repeat refresh frame requests until a frame is received.
    
        When MediaStreamVideoSource::RequestRefreshFrame is called, the
        capturer most often emits a refresh frame. Due to various
        conditions such as for example timing of prior delivery,
        these frames can be dropped at various places in the input
        pipeline into WebRTC.
    
        This change ensures the frame cadence adapter repeatedly
        requests refresh frames at max fps frequency until one is
        received, in which case the requests cease.
    
        Fixed: chromium:1324120
        Change-Id: I90f85d31b132b6c441aa1c28c5eff85e3dc365ad
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/263520
        Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
        Commit-Queue: Markus Handell <handellm@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#36998}

--- third_party/libwebrtc/video/frame_cadence_adapter_unittest.cc
+++ third_party/libwebrtc/video/frame_cadence_adapter_unittest.cc
@@ -169,7 +169,6 @@ TEST(FrameCadenceAdapterTest,
 
 TEST(FrameCadenceAdapterTest, FrameRateFollowsMaxFpsWhenZeroHertzActivated) {
   ZeroHertzFieldTrialEnabler enabler;
-  MockCallback callback;
   GlobalSimulatedTimeController time_controller(Timestamp::Millis(0));
   auto adapter = CreateAdapter(enabler, time_controller.GetClock());
   adapter->Initialize(nullptr);
@@ -186,7 +185,6 @@ TEST(FrameCadenceAdapterTest, FrameRateFollowsMaxFpsWhenZeroHertzActivated) {
 TEST(FrameCadenceAdapterTest,
      FrameRateFollowsRateStatisticsAfterZeroHertzDeactivated) {
   ZeroHertzFieldTrialEnabler enabler;
-  MockCallback callback;
   GlobalSimulatedTimeController time_controller(Timestamp::Millis(0));
   auto adapter = CreateAdapter(enabler, time_controller.GetClock());
   adapter->Initialize(nullptr);
@@ -372,8 +370,8 @@ TEST(FrameCadenceAdapterTest, RequestsRefreshFrameOnKeyFrameRequestWhenNew) {
   adapter->SetZeroHertzModeEnabled(
       FrameCadenceAdapterInterface::ZeroHertzModeParams{});
   adapter->OnConstraintsChanged(VideoTrackSourceConstraints{0, 10});
-  time_controller.AdvanceTime(TimeDelta::Zero());
   EXPECT_CALL(callback, RequestRefreshFrame);
+  time_controller.AdvanceTime(TimeDelta::Zero());
   adapter->ProcessKeyFrameRequest();
 }
 
@@ -390,6 +388,28 @@ TEST(FrameCadenceAdapterTest, IgnoresKeyFrameRequestShortlyAfterFrame) {
   time_controller.AdvanceTime(TimeDelta::Zero());
   EXPECT_CALL(callback, RequestRefreshFrame).Times(0);
   adapter->ProcessKeyFrameRequest();
+}
+
+TEST(FrameCadenceAdapterTest, RequestsRefreshFramesUntilArrival) {
+  ZeroHertzFieldTrialEnabler enabler;
+  MockCallback callback;
+  GlobalSimulatedTimeController time_controller(Timestamp::Millis(0));
+  auto adapter = CreateAdapter(enabler, time_controller.GetClock());
+  adapter->Initialize(&callback);
+  adapter->SetZeroHertzModeEnabled(
+      FrameCadenceAdapterInterface::ZeroHertzModeParams{});
+  constexpr int kMaxFps = 10;
+  adapter->OnConstraintsChanged(VideoTrackSourceConstraints{0, kMaxFps});
+
+  // We should see max_fps + 1 refresh frame requests during the one second we
+  // wait until we send a single frame, after which refresh frame requests
+  // should cease (we should see no such requests during a second).
+  EXPECT_CALL(callback, RequestRefreshFrame).Times(kMaxFps + 1);
+  time_controller.AdvanceTime(TimeDelta::Seconds(1));
+  Mock::VerifyAndClearExpectations(&callback);
+  adapter->OnFrame(CreateFrame());
+  EXPECT_CALL(callback, RequestRefreshFrame).Times(0);
+  time_controller.AdvanceTime(TimeDelta::Seconds(1));
 }
 
 class FrameCadenceAdapterSimulcastLayersParamTest
