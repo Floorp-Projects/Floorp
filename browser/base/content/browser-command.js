97f70e5d5bc62dbb7aa994965d30376b4756d529
regularenthropy <89523758+regularenthropy@users.noreply.github.com>
2022-09-23 21:56:16 +0200

    set-all-favicons now uses sidebar amount const

--- browser/base/content/browser-command.js
+++ browser/base/content/browser-command.js
@@ -516,7 +516,7 @@ function setTabbarMode() {
 }
 
 function setAllfavicons() {
-  for (let sbar_id = 1; sbar_id < 20; sbar_id++) {
+  for (let sbar_id = 1; sbar_id <= SIDEBAR_AMOUNT; sbar_id++) {
     var sbar_favicon = Services.prefs.getStringPref(`floorp.browser.sidebar2.customurl${sbar_id}`)
     document.getElementById(`select-CustomURL${sbar_id}`).style.listStyleImage = `url(http://www.google.com/s2/favicons?domain=${sbar_favicon}`;
   }
