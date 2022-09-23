568730f86d787d9ec65950c3aeda4308cc8195e6
Csoregi Natalia <ncsoregi@mozilla.com>
2022-09-23 19:17:30 +0300

    Backed out changeset 6f1d00d07032 (bug 1767040) for causing build bustages. CLOSED TREE

--- toolkit/components/glean/bindings/GleanPings.cpp
+++ toolkit/components/glean/bindings/GleanPings.cpp
@@ -56,14 +56,7 @@ bool GleanPings::DefineGleanPings(JSContext* aCx,
 
 already_AddRefed<GleanPing> GleanPings::NamedGetter(const nsAString& aName,
                                                     bool& aFound) {
-  aFound = false;
-
-  NS_ConvertUTF16toUTF8 pingName(aName);
-  Maybe<uint32_t> pingId = JOG::GetPing(pingName);
-  if (pingId.isNothing() && !JOG::AreRuntimeMetricsComprehensive()) {
-    pingId = PingByNameLookup(pingName);
-  }
-
+  Maybe<uint32_t> pingId = PingByNameLookup(NS_ConvertUTF16toUTF8(aName));
   if (pingId.isNothing()) {
     aFound = false;
     return nullptr;
