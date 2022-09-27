275b06e759f9ab92e0ed18fa4f3c7975c1be5b8e
Andrew McCreight <continuation@gmail.com>
2022-09-27 21:27:17 +0000

    Bug 1792354, part 1 - DeCOM nsGZFileWriter. r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D158131

--- xpcom/base/nsGZFileWriter.h
+++ xpcom/base/nsGZFileWriter.h
@@ -7,13 +7,22 @@
 #ifndef nsGZFileWriter_h
 #define nsGZFileWriter_h
 
-#include "nsIGZFileWriter.h"
+#include "nsISupportsImpl.h"
 #include "zlib.h"
+#include "nsDependentString.h"
+#include <stdio.h>
 
 /**
- * A simple class for writing .gz files.
+ * A simple class for writing to a .gz file.
+ *
+ * Note that the file that this interface produces has a different format than
+ * what you'd get if you compressed your data as a gzip stream and dumped the
+ * result to a file.
+ *
+ * The standard gunzip tool cannot decompress a raw gzip stream, but can handle
+ * the files produced by this interface.
  */
-class nsGZFileWriter final : public nsIGZFileWriter {
+class nsGZFileWriter final {
   virtual ~nsGZFileWriter();
 
  public:
@@ -21,21 +30,44 @@ class nsGZFileWriter final : public nsIGZFileWriter {
 
   explicit nsGZFileWriter(Operation aMode = Create);
 
-  NS_DECL_ISUPPORTS
-  NS_DECL_NSIGZFILEWRITER
+  NS_INLINE_DECL_REFCOUNTING(nsGZFileWriter)
 
   /**
-   * nsIGZFileWriter exposes two non-virtual overloads of Write().  We
-   * duplicate them here so that you can call these overloads on a pointer
-   * to the concrete nsGZFileWriter class.
+   * Initialize this object.  We'll write our gzip'ed data to the given file,
+   * overwriting its contents if the file exists.
+   *
+   * Init() will return an error if called twice.  It's an error to call any
+   * other method on this interface without first calling Init().
    */
-  [[nodiscard]] nsresult Write(const char* aStr) {
-    return nsIGZFileWriter::Write(aStr);
+  [[nodiscard]] nsresult Init(nsIFile* aFile);
+
+  /**
+   * Alternate version of Init() for use when the file is already opened;
+   * e.g., with a FileDescriptor passed over IPC.
+   */
+  [[nodiscard]] nsresult InitANSIFileDesc(FILE* aFile);
+
+  /**
+   * Write the given string to the file.
+   */
+  [[nodiscard]] nsresult Write(const nsACString& aStr);
+
+  /**
+   * Write |length| bytes of |str| to the file.
+   */
+  [[nodiscard]] nsresult Write(const char* aStr, uint32_t aLen) {
+    return Write(nsDependentCSubstring(aStr, aLen));
   }
 
-  [[nodiscard]] nsresult Write(const char* aStr, uint32_t aLen) {
-    return nsIGZFileWriter::Write(aStr, aLen);
-  }
+  /**
+   * Close this nsGZFileWriter. This method will be run when the underlying
+   * object is destroyed, so it's not strictly necessary to explicitly call it
+   * from your code.
+   *
+   * It's an error to call this method twice, and it's an error to call Write()
+   * after Finish() has been called.
+   */
+  nsresult Finish();
 
  private:
   Operation mMode;
