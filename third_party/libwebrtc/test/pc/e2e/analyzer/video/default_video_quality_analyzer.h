b890b93b7fbc443be947a930d99bcf9e957aa610
Michael Froman <mfroman@mozilla.com>
2022-09-27 14:38:37 -0500

    Bug 1790097 - Vendor libwebrtc from 71f406e52b
    Upstream commit: https://webrtc.googlesource.com/src/+/71f406e52b7a571b092ba1525de8cc15065fea70
        [DVQA] Extract StreamState into separate file
    
        Bug: b/231397778
        Change-Id: Ic7207cd17e8179151824a2257b3173481e9f4f6e
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/265399
        Commit-Queue: Artem Titov <titovartem@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37187}

--- third_party/libwebrtc/test/pc/e2e/analyzer/video/default_video_quality_analyzer.h
+++ third_party/libwebrtc/test/pc/e2e/analyzer/video/default_video_quality_analyzer.h
@@ -37,6 +37,7 @@
 #include "test/pc/e2e/analyzer/video/default_video_quality_analyzer_frames_comparator.h"
 #include "test/pc/e2e/analyzer/video/default_video_quality_analyzer_internal_shared_objects.h"
 #include "test/pc/e2e/analyzer/video/default_video_quality_analyzer_shared_objects.h"
+#include "test/pc/e2e/analyzer/video/default_video_quality_analyzer_stream_state.h"
 #include "test/pc/e2e/analyzer/video/multi_head_queue.h"
 #include "test/pc/e2e/analyzer/video/names_collection.h"
 #include "test/testsupport/perf_test.h"
@@ -105,77 +106,6 @@ class DefaultVideoQualityAnalyzer : public VideoQualityAnalyzerInterface {
   std::map<std::string, std::vector<uint16_t>> GetStreamFrames() const;
 
  private:
-  // Represents a current state of video stream.
-  class StreamState {
-   public:
-    StreamState(size_t owner,
-                size_t peers_count,
-                bool enable_receive_own_stream,
-                Timestamp stream_started_time)
-        : owner_(owner),
-          enable_receive_own_stream_(enable_receive_own_stream),
-          stream_started_time_(stream_started_time),
-          frame_ids_(enable_receive_own_stream ? peers_count + 1
-                                               : peers_count) {}
-
-    size_t owner() const { return owner_; }
-    Timestamp stream_started_time() const { return stream_started_time_; }
-
-    void PushBack(uint16_t frame_id) { frame_ids_.PushBack(frame_id); }
-    // Crash if state is empty. Guarantees that there can be no alive frames
-    // that are not in the owner queue
-    uint16_t PopFront(size_t peer);
-    bool IsEmpty(size_t peer) const {
-      return frame_ids_.IsEmpty(GetPeerQueueIndex(peer));
-    }
-    // Crash if state is empty.
-    uint16_t Front(size_t peer) const {
-      return frame_ids_.Front(GetPeerQueueIndex(peer)).value();
-    }
-
-    // When new peer is added - all current alive frames will be sent to it as
-    // well. So we need to register them as expected by copying owner_ head to
-    // the new head.
-    void AddPeer() { frame_ids_.AddHead(GetAliveFramesQueueIndex()); }
-
-    size_t GetAliveFramesCount() const {
-      return frame_ids_.size(GetAliveFramesQueueIndex());
-    }
-    uint16_t MarkNextAliveFrameAsDead();
-
-    void SetLastRenderedFrameTime(size_t peer, Timestamp time);
-    absl::optional<Timestamp> last_rendered_frame_time(size_t peer) const;
-
-   private:
-    // Returns index of the `frame_ids_` queue which is used for specified
-    // `peer_index`.
-    size_t GetPeerQueueIndex(size_t peer_index) const;
-
-    // Returns index of the `frame_ids_` queue which is used to track alive
-    // frames for this stream. The frame is alive if it contains VideoFrame
-    // payload in `captured_frames_in_flight_`.
-    size_t GetAliveFramesQueueIndex() const;
-
-    // Index of the owner. Owner's queue in `frame_ids_` will keep alive frames.
-    const size_t owner_;
-    const bool enable_receive_own_stream_;
-    const Timestamp stream_started_time_;
-    // To correctly determine dropped frames we have to know sequence of frames
-    // in each stream so we will keep a list of frame ids inside the stream.
-    // This list is represented by multi head queue of frame ids with separate
-    // head for each receiver. When the frame is rendered, we will pop ids from
-    // the corresponding head until id will match with rendered one. All ids
-    // before matched one can be considered as dropped:
-    //
-    // | frame_id1 |->| frame_id2 |->| frame_id3 |->| frame_id4 |
-    //
-    // If we received frame with id frame_id3, then we will pop frame_id1 and
-    // frame_id2 and consider that frames as dropped and then compare received
-    // frame with the one from `captured_frames_in_flight_` with id frame_id3.
-    MultiHeadQueue<uint16_t> frame_ids_;
-    std::map<size_t, Timestamp> last_rendered_frame_time_;
-  };
-
   enum State { kNew, kActive, kStopped };
 
   struct ReceiverFrameStats {
