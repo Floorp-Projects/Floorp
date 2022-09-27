02caa482963aa9b991a116716dbed4c39962c12a
Mark Banner <standard8@mozilla.com>
2022-09-27 08:34:23 +0000

    Bug 1782523 - Update NodeJS to latest versions for the branches on ESR. r=nalexander, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D158185

--- taskcluster/docker/recipes/install-node.sh
+++ taskcluster/docker/recipes/install-node.sh
@@ -8,8 +8,8 @@
 # taskcluster/scripts/misc/repack-node.sh. Later we'll get the ESLint builder
 # to use the linux64-node toolchain directly.
 
-wget -O node.xz --progress=dot:mega https://nodejs.org/download/release/v12.22.1/node-v12.22.1-linux-x64.tar.xz
-echo '8b537282c222ae4a40e019a52f769ca27b6640699bdde1510375e8d72da7d041' node.xz | sha256sum -c
+wget -O node.xz --progress=dot:mega https://nodejs.org/dist/v12.22.12/node-v12.22.12-linux-x64.tar.xz
+echo 'e6d052364bfa2c17da92cf31794100cfd709ba147415ddaeed2222eec9ca1469' node.xz | sha256sum -c
 tar -C /usr/local -xJ --strip-components 1 < node.xz
 node -v  # verify
 npm -v
