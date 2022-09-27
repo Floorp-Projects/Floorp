9f8d549e639aa9c7e8426d7c8783372307cfc5ba
Jonathan Kew <jkew@mozilla.com>
2022-09-27 22:06:51 +0000

    Bug 1792214 - Apply OTS patch from https://github.com/khaledhosny/ots/pull/249 to work around Core Text and FreeType failures with variable COLR fonts that lack a gvar table. r=gfx-reviewers,jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D158141

--- gfx/ots/src/gvar.h
+++ gfx/ots/src/gvar.h
@@ -16,14 +16,26 @@ namespace ots {
 class OpenTypeGVAR : public Table {
  public:
   explicit OpenTypeGVAR(Font* font, uint32_t tag)
-      : Table(font, tag, tag) { }
+      : Table(font, tag, tag), m_ownsData(false) { }
+
+  virtual ~OpenTypeGVAR() {
+    if (m_ownsData) {
+      delete[] m_data;
+    }
+  }
 
   bool Parse(const uint8_t* data, size_t length);
   bool Serialize(OTSStream* out);
 
+#ifdef OTS_SYNTHESIZE_MISSING_GVAR
+  bool InitEmpty();
+#endif
+
  private:
   const uint8_t *m_data;
   size_t m_length;
+
+  bool m_ownsData;
 };
 
 }  // namespace ots
