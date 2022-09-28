f8e730c431f029f247ac3c2596000967d11797ba
Andrew McCreight <continuation@gmail.com>
2022-09-28 15:17:47 +0000

    Bug 1792574, part 4 - deCOM nsIMemory. r=xpcom-reviewers,nika
    I left the implementation in nsMemoryImpl.cpp so that the
    diff looks reasonable.
    
    I also got rid of the weird singleton runnable and replaced
    it with NS_NewRunnableFunction.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158214

--- xpcom/base/nsMemory.h
+++ xpcom/base/nsMemory.h
@@ -9,29 +9,54 @@
 
 #include "nsError.h"
 
-class nsIMemory;
+/**
+ *
+ * A client that wishes to be notified of low memory situations (for
+ * example, because the client maintains a large memory cache that
+ * could be released when memory is tight) should register with the
+ * observer service (see nsIObserverService) using the topic
+ * "memory-pressure".  There are specific types of notifications
+ * that can occur.  These types will be passed as the |aData|
+ * parameter of the of the "memory-pressure" notification:
+ *
+ * "low-memory"
+ * This will be passed as the extra data when the pressure
+ * observer is being asked to flush for low-memory conditions.
+ *
+ * "low-memory-ongoing"
+ * This will be passed when we continue to be in a low-memory
+ * condition and we want to flush caches and do other cheap
+ * forms of memory minimization, but heavy handed approaches like
+ * a GC are unlikely to succeed.
+ *
+ * "heap-minimize"
+ * This will be passed as the extra data when the pressure
+ * observer is being asked to flush because of a heap minimize
+ * call.
+ */
 
-#define NS_MEMORY_CONTRACTID "@mozilla.org/xpcom/memory-service;1"
-#define NS_MEMORY_CID                                \
-  { /* 30a04e40-38e7-11d4-8cf5-0060b0fc14a3 */       \
-    0x30a04e40, 0x38e7, 0x11d4, {                    \
-      0x8c, 0xf5, 0x00, 0x60, 0xb0, 0xfc, 0x14, 0xa3 \
-    }                                                \
-  }
+// This is implemented in nsMemoryImpl.cpp.
+
+namespace nsMemory {
 
 /**
- * Static helper routines to manage memory. These routines allow easy access
- * to xpcom's built-in (global) nsIMemory implementation, without needing
- * to go through the service manager to get it. However this requires clients
- * to link with the xpcom DLL.
- *
- * This class is not threadsafe and is intented for use only on the main
- * thread.
+ * Attempts to shrink the heap.
+ * @param immediate - if true, heap minimization will occur
+ *   immediately if the call was made on the main thread. If
+ *   false, the flush will be scheduled to happen when the app is
+ *   idle.
+ * @throws NS_ERROR_FAILURE if 'immediate' is set and the call
+ *   was not on the application's main thread.
  */
-class nsMemory {
- public:
-  static nsresult HeapMinimize(bool aImmediate);
-  static nsIMemory* GetGlobalMemoryService();  // AddRefs
-};
+nsresult HeapMinimize(bool aImmediate);
+
+/**
+ * This predicate can be used to determine if the platform is a "low-memory"
+ * platform. Callers may use this to dynamically tune their behaviour
+ * to favour reduced memory usage at the expense of performance. The value
+ * returned by this function will not change over the lifetime of the process.
+ */
+bool IsLowMemoryPlatform();
+}  // namespace nsMemory
 
 #endif  // nsMemory_h__
--- xpcom/base/nsMemoryImpl.cpp
+++ xpcom/base/nsMemoryImpl.cpp
@@ -4,15 +4,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "nsMemoryImpl.h"
+#include "nsMemory.h"
 #include "nsThreadUtils.h"
 
 #include "nsIObserver.h"
 #include "nsIObserverService.h"
+#include "nsIRunnable.h"
 #include "nsISimpleEnumerator.h"
 
 #include "nsCOMPtr.h"
 #include "mozilla/Services.h"
+#include "mozilla/Atomics.h"
 
 #ifdef ANDROID
 #  include <stdio.h>
@@ -23,87 +25,39 @@
 #  define LOW_MEMORY_THRESHOLD_KB (384 * 1024)
 #endif
 
-static nsMemoryImpl sGlobalMemory;
+static mozilla::Atomic<bool> sIsFlushing;
+static PRIntervalTime sLastFlushTime = 0;
 
-NS_IMPL_QUERY_INTERFACE(nsMemoryImpl, nsIMemory)
-
-NS_IMETHODIMP
-nsMemoryImpl::HeapMinimize(bool aImmediate) {
-  return FlushMemory(u"heap-minimize", aImmediate);
-}
-
-NS_IMETHODIMP
-nsMemoryImpl::IsLowMemoryPlatform(bool* aResult) {
+// static
+bool nsMemory::IsLowMemoryPlatform() {
 #ifdef ANDROID
   static int sLowMemory =
       -1;  // initialize to unknown, lazily evaluate to 0 or 1
   if (sLowMemory == -1) {
     sLowMemory = 0;  // assume "not low memory" in case file operations fail
-    *aResult = false;
 
     // check if MemTotal from /proc/meminfo is less than LOW_MEMORY_THRESHOLD_KB
     FILE* fd = fopen("/proc/meminfo", "r");
     if (!fd) {
-      return NS_OK;
+      return false;
     }
     uint64_t mem = 0;
     int rv = fscanf(fd, "MemTotal: %" PRIu64 " kB", &mem);
     if (fclose(fd)) {
-      return NS_OK;
+      return false;
     }
     if (rv != 1) {
-      return NS_OK;
+      return false;
     }
     sLowMemory = (mem < LOW_MEMORY_THRESHOLD_KB) ? 1 : 0;
   }
-  *aResult = (sLowMemory == 1);
+  return (sLowMemory == 1);
 #else
-  *aResult = false;
+  return false;
 #endif
-  return NS_OK;
 }
 
-/*static*/
-nsresult nsMemoryImpl::Create(const nsIID& aIID, void** aResult) {
-  return sGlobalMemory.QueryInterface(aIID, aResult);
-}
-
-nsresult nsMemoryImpl::FlushMemory(const char16_t* aReason, bool aImmediate) {
-  if (aImmediate) {
-    // They've asked us to run the flusher *immediately*. We've
-    // got to be on the UI main thread for us to be able to do
-    // that...are we?
-    if (!NS_IsMainThread()) {
-      NS_ERROR("can't synchronously flush memory: not on UI thread");
-      return NS_ERROR_FAILURE;
-    }
-  }
-
-  bool lastVal = sIsFlushing.exchange(true);
-  if (lastVal) {
-    return NS_OK;
-  }
-
-  PRIntervalTime now = PR_IntervalNow();
-
-  // Run the flushers immediately if we can; otherwise, proxy to the
-  // UI thread and run 'em asynchronously.
-  nsresult rv = NS_OK;
-  if (aImmediate) {
-    RunFlushers(aReason);
-  } else {
-    // Don't broadcast more than once every 1000ms to avoid being noisy
-    if (PR_IntervalToMicroseconds(now - sLastFlushTime) > 1000) {
-      sFlushEvent.mReason = aReason;
-      rv = NS_DispatchToMainThread(&sFlushEvent);
-    }
-  }
-
-  sLastFlushTime = now;
-  return rv;
-}
-
-void nsMemoryImpl::RunFlushers(const char16_t* aReason) {
+static void RunFlushers(const char16_t* aReason) {
   nsCOMPtr<nsIObserverService> os = mozilla::services::GetObserverService();
   if (os) {
     // Instead of:
@@ -135,25 +89,43 @@ void nsMemoryImpl::RunFlushers(const char16_t* aReason) {
   sIsFlushing = false;
 }
 
-// XXX need NS_IMPL_STATIC_ADDREF/RELEASE
-NS_IMETHODIMP_(MozExternalRefCountType)
-nsMemoryImpl::FlushEvent::AddRef() { return 2; }
-NS_IMETHODIMP_(MozExternalRefCountType)
-nsMemoryImpl::FlushEvent::Release() { return 1; }
-NS_IMPL_QUERY_INTERFACE(nsMemoryImpl::FlushEvent, nsIRunnable)
+static nsresult FlushMemory(const char16_t* aReason, bool aImmediate) {
+  if (aImmediate) {
+    // They've asked us to run the flusher *immediately*. We've
+    // got to be on the UI main thread for us to be able to do
+    // that...are we?
+    if (!NS_IsMainThread()) {
+      NS_ERROR("can't synchronously flush memory: not on UI thread");
+      return NS_ERROR_FAILURE;
+    }
+  }
 
-NS_IMETHODIMP
-nsMemoryImpl::FlushEvent::Run() {
-  sGlobalMemory.RunFlushers(mReason);
-  return NS_OK;
+  bool lastVal = sIsFlushing.exchange(true);
+  if (lastVal) {
+    return NS_OK;
+  }
+
+  PRIntervalTime now = PR_IntervalNow();
+
+  // Run the flushers immediately if we can; otherwise, proxy to the
+  // UI thread and run 'em asynchronously.
+  nsresult rv = NS_OK;
+  if (aImmediate) {
+    RunFlushers(aReason);
+  } else {
+    // Don't broadcast more than once every 1000ms to avoid being noisy
+    if (PR_IntervalToMicroseconds(now - sLastFlushTime) > 1000) {
+      nsCOMPtr<nsIRunnable> runnable(NS_NewRunnableFunction(
+          "FlushMemory",
+          [reason = aReason]() -> void { RunFlushers(reason); }));
+      NS_DispatchToMainThread(runnable.forget());
+    }
+  }
+
+  sLastFlushTime = now;
+  return rv;
 }
 
-mozilla::Atomic<bool> nsMemoryImpl::sIsFlushing;
-
-PRIntervalTime nsMemoryImpl::sLastFlushTime = 0;
-
-nsMemoryImpl::FlushEvent nsMemoryImpl::sFlushEvent;
-
-nsresult NS_GetMemoryManager(nsIMemory** aResult) {
-  return sGlobalMemory.QueryInterface(NS_GET_IID(nsIMemory), (void**)aResult);
+nsresult nsMemory::HeapMinimize(bool aImmediate) {
+  return FlushMemory(u"heap-minimize", aImmediate);
 }
