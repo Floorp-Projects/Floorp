55f6b28b14227f686b68dbd14b6d6e69749f4dfa
regularenthropy <89523758+regularenthropy@users.noreply.github.com>
2022-09-28 11:03:20 +0200

    clarify rest-mode function

--- browser/base/content/browser-command.js
+++ browser/base/content/browser-command.js
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-function bosshascomming() {
+function enableRestMode() {
   if (Services.prefs.getBoolPref("floorp.browser.rest.mode", false)) {
     var Tag = document.createElement("style");
     Tag.innerText = `*{display:none !important;}`
