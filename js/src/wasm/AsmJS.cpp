262355d715d701fd0bba2ee43261116ac99e10a9
Tooru Fujisawa <arai_a@mac.com>
2022-09-28 07:40:16 +0000

    Bug 1786494 - Part 8: Use ErrorContext for checking errors in asm.js. r=bthrall
    Differential Revision: https://phabricator.services.mozilla.com/D157448

--- js/src/wasm/AsmJS.cpp
+++ js/src/wasm/AsmJS.cpp
@@ -7075,9 +7075,7 @@ bool js::InstantiateAsmJS(JSContext* cx, unsigned argc, JS::Value* vp) {
 /*****************************************************************************/
 // Top-level js::CompileAsmJS
 
-static bool NoExceptionPending(JSContext* cx) {
-  return cx->isHelperThreadContext() || !cx->isExceptionPending();
-}
+static bool NoExceptionPending(ErrorContext* ec) { return !ec->hadErrors(); }
 
 static bool SuccessfulValidation(frontend::ParserBase& parser,
                                  unsigned compilationTime) {
@@ -7166,7 +7164,7 @@ static bool DoCompileAsmJS(JSContext* cx, ErrorContext* ec,
 
   // Various conditions disable asm.js optimizations.
   if (!EstablishPreconditions(cx, parser)) {
-    return NoExceptionPending(cx);
+    return NoExceptionPending(ec);
   }
 
   // "Checking" parses, validates and compiles, producing a fully compiled
@@ -7175,7 +7173,7 @@ static bool DoCompileAsmJS(JSContext* cx, ErrorContext* ec,
   SharedModule module =
       CheckModule(cx, ec, stackLimit, parserAtoms, parser, stmtList, &time);
   if (!module) {
-    return NoExceptionPending(cx);
+    return NoExceptionPending(ec);
   }
 
   // Finished! Save the ref-counted module on the FunctionBox. When JSFunctions
@@ -7183,14 +7181,14 @@ static bool DoCompileAsmJS(JSContext* cx, ErrorContext* ec,
   FunctionBox* funbox = parser.pc_->functionBox();
   MOZ_ASSERT(funbox->isInterpreted());
   if (!funbox->setAsmJSModule(module)) {
-    return NoExceptionPending(cx);
+    return NoExceptionPending(ec);
   }
 
   // Success! Write to the console with a "warning" message indicating
   // total compilation time.
   *validated = true;
   SuccessfulValidation(parser, time);
-  return NoExceptionPending(cx);
+  return NoExceptionPending(ec);
 }
 
 bool js::CompileAsmJS(JSContext* cx, ErrorContext* ec,
