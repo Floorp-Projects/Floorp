3f5c587f1dabcc13fc5bfffcf20d11fe767dab13
Yannis Juglaret <yjuglaret@mozilla.com>
2022-09-29 15:29:14 +0000

    Bug 1766432 - Part 2: Propagate custom definition for PROCESS_MITIGATION_DYNAMIC_CODE_POLICY to third-party. r=bobowen
    Differential Revision: https://phabricator.services.mozilla.com/D157904

--- security/sandbox/chromium/sandbox/win/src/process_mitigations.cc
+++ security/sandbox/chromium/sandbox/win/src/process_mitigations.cc
@@ -17,6 +17,8 @@
 #include "sandbox/win/src/sandbox_rand.h"
 #include "sandbox/win/src/win_utils.h"
 
+#include "mozilla/MozProcessMitigationDynamicCodePolicy.h"
+
 namespace {
 
 // API defined in libloaderapi.h >= Win8.
@@ -231,7 +233,7 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
     // Verify caller is not accidentally setting both mutually exclusive
     // policies.
     DCHECK(!(flags & MITIGATION_DYNAMIC_CODE_DISABLE_WITH_OPT_OUT));
-    PROCESS_MITIGATION_DYNAMIC_CODE_POLICY policy = {};
+    MOZ_PROCESS_MITIGATION_DYNAMIC_CODE_POLICY policy = {};
     policy.ProhibitDynamicCode = true;
 
     if (!set_process_mitigation_policy(ProcessDynamicCodePolicy, &policy,
@@ -307,7 +309,7 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
     // Verify caller is not accidentally setting both mutually exclusive
     // policies.
     DCHECK(!(flags & MITIGATION_DYNAMIC_CODE_DISABLE));
-    PROCESS_MITIGATION_DYNAMIC_CODE_POLICY policy = {};
+    MOZ_PROCESS_MITIGATION_DYNAMIC_CODE_POLICY policy = {};
     policy.ProhibitDynamicCode = true;
     policy.AllowThreadOptOut = true;
 
