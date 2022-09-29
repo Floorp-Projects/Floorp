7d97f198987e2a51580cd63039d5504a2f17ef38
Niklas Baumgardner <nbaumgardner@mozilla.com>
2022-09-29 03:08:06 +0000

    Bug 1792441 - Keep window open when restoring with hidden tabs. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D158233

--- browser/components/sessionstore/test/browser_firefoxView_selected_restore.js
+++ browser/components/sessionstore/test/browser_firefoxView_selected_restore.js
@@ -47,7 +47,7 @@ function promiseSessionStoreLoads(numberOfLoads) {
   });
 }
 
-add_task(async function test() {
+add_task(async function test_firefox_view_selected_tab() {
   let fxViewBtn = document.getElementById("firefox-view-button");
   ok(fxViewBtn, "Got the Firefox View button");
   fxViewBtn.click();
@@ -55,6 +55,38 @@ add_task(async function test() {
   await BrowserTestUtils.browserLoaded(
     window.FirefoxViewHandler.tab.linkedBrowser
   );
+
+  let allTabsRestored = promiseSessionStoreLoads(1);
+
+  _LastSession.setState(state);
+
+  is(gBrowser.tabs.length, 2, "Number of tabs is 2");
+
+  ss.restoreLastSession();
+
+  await allTabsRestored;
+
+  ok(
+    window.FirefoxViewHandler.tab.selected,
+    "The Firefox View tab is selected and the browser window did not close"
+  );
+  is(gBrowser.tabs.length, 3, "Number of tabs is 3");
+
+  gBrowser.removeTab(window.FirefoxViewHandler.tab);
+  gBrowser.removeTab(gBrowser.selectedTab);
+});
+
+add_task(async function test_firefox_view_previously_selected() {
+  let fxViewBtn = document.getElementById("firefox-view-button");
+  ok(fxViewBtn, "Got the Firefox View button");
+  fxViewBtn.click();
+
+  await BrowserTestUtils.browserLoaded(
+    window.FirefoxViewHandler.tab.linkedBrowser
+  );
+
+  let tab = gBrowser.tabs[1];
+  gBrowser.selectedTab = tab;
 
   let allTabsRestored = promiseSessionStoreLoads(1);
 
