2b3568d41295bfe1d40bb528a4514e0cfdc7e9bb
Ryan VanderMeulen <ryanvm@gmail.com>
2022-09-30 01:45:45 +0000

    Bug 1792227 - Update node to v16.17.1. r=firefox-build-system-reviewers,ahochheiden
    Differential Revision: https://phabricator.services.mozilla.com/D158039

--- taskcluster/docker/recipes/install-node.sh
+++ taskcluster/docker/recipes/install-node.sh
@@ -8,8 +8,8 @@
 # taskcluster/scripts/misc/repack-node.sh. Later we'll get the ESLint builder
 # to use the linux64-node toolchain directly.
 
-wget -O node.xz --progress=dot:mega https://nodejs.org/dist/v16.16.0/node-v16.16.0-linux-x64.tar.xz
-echo 'edcb6e9bb049ae365611aa209fc03c4bfc7e0295dbcc5b2f1e710ac70384a8ec' node.xz | sha256sum -c
+wget -O node.xz --progress=dot:mega https://nodejs.org/dist/v16.17.1/node-v16.17.1-linux-x64.tar.xz
+echo '06ba2eb34aa385967f5f58c87a44753f83212f6cccea892b33f80a2e7fda8384' node.xz | sha256sum -c
 tar -C /usr/local -xJ --strip-components 1 < node.xz
 node -v  # verify
 npm -v
