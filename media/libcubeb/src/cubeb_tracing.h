5919ebbd73f50a45f23250d500489fe5a7b994df
Paul Adenot <paul@paul.cx>
2022-10-01 07:59:44 +0000

    Bug 1791900 - Change cubeb vendoring manifest, swapping out cubeb_tracing.h with something that uses MicroGeckoProfiler.h. r=cubeb-reviewers,kinetik
    Differential Revision: https://phabricator.services.mozilla.com/D157879

--- media/libcubeb/src/cubeb_tracing.h
+++ media/libcubeb/src/cubeb_tracing.h
@@ -1,23 +1,27 @@
-/*
- * Copyright © 2022 Mozilla Foundation
- *
- * This program is made available under an ISC-style license.  See the
- * accompanying file LICENSE for details.
- */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef CUBEB_TRACING_H
 #define CUBEB_TRACING_H
 
-/* Empty header to allow hooking up a frame profiler. */
+#include <MicroGeckoProfiler.h>
 
-// To be called once on a thread to register for tracing.
-#define CUBEB_REGISTER_THREAD(name)
-// To be called once before a registered threads exits.
-#define CUBEB_UNREGISTER_THREAD()
+#define CUBEB_REGISTER_THREAD(name)               \
+  do {                                            \
+    char stacktop;                                \
+    uprofiler_register_thread(name, &stacktop);   \
+  } while (0)
+
+#define CUBEB_UNREGISTER_THREAD() uprofiler_unregister_thread()
+
 // Insert a tracing marker, with a particular name.
 // Phase can be 'x': instant marker, start time but no duration
 //              'b': beginning of a marker with a duration
 //              'e': end of a marker with a duration
-#define CUBEB_TRACE(name, phase)
+#define CUBEB_TRACE(name, phase) \
+  uprofiler_simple_event_marker(name, phase, 0, NULL, NULL, NULL)
 
 #endif // CUBEB_TRACING_H
