2594c0a077b41367f61ca8989b6ca4309fc40a34
Jonathan Kew <jkew@mozilla.com>
2022-10-05 00:35:24 +0000

    Bug 1793611 - Handle degenerate (0-length) color line in extend-pad mode for radial gradients. r=lsalzman
    Includes reftest; there's fuzz at the color transitions, when compared to a CSS-backgrounds simulation,
    but in the case of "real" failure there'd be many more pixels of mismatch, and max-difference would be 255.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158579

--- gfx/thebes/COLRFonts.cpp
+++ gfx/thebes/COLRFonts.cpp
@@ -666,6 +666,8 @@ struct ColorLineT {
     }
     // Normalize stops to the range 0.0 .. 1.0, and return the original
     // start & end.
+    // Note that if all stops are at the same offset, no normalization
+    // will be done.
     *aFirstStop = aStops[0].offset;
     *aLastStop = aStops.LastElement().offset;
     if ((*aLastStop > *aFirstStop) &&
@@ -1043,16 +1045,25 @@ struct PaintRadialGradient : public PaintPatternBase {
         if (aColorLine->extend != T::EXTEND_PAD) {
           return MakeUnique<ColorPattern>(DeviceColor());
         }
-      } else {
-        // Adjust centers along the vector between them, and scale radii for
-        // gradient line defined from 0.0 to 1.0.
-        Point vec = c2 - c1;
-        c1 += vec * firstStop;
-        c2 -= vec * (1.0f - lastStop);
-        float deltaR = r2 - r1;
-        r1 = r1 + deltaR * firstStop;
-        r2 = r2 - deltaR * (1.0f - lastStop);
+        // For extend-pad, when the color line is zero-length, we add a "fake"
+        // color stop to ensure we'll maintain the orientation of the cone,
+        // otherwise when we adjust circles to account for the normalized color
+        // stops, the centers will coalesce and the cone or cylinder collapses.
+        for (auto& gs : stopArray) {
+          gs.offset = 0.0f;
+        }
+        stopArray.AppendElement(
+            GradientStop{1.0f, stopArray.LastElement().color});
+        lastStop += 1.0f;
       }
+      // Adjust centers along the vector between them, and scale radii for
+      // gradient line defined from 0.0 to 1.0.
+      Point vec = c2 - c1;
+      c1 += vec * firstStop;
+      c2 -= vec * (1.0f - lastStop);
+      float deltaR = r2 - r1;
+      r1 = r1 + deltaR * firstStop;
+      r2 = r2 - deltaR * (1.0f - lastStop);
     }
     if ((r1 < 0.0f || r2 < 0.0f) && aColorLine->extend == T::EXTEND_PAD) {
       // For EXTEND_PAD, we can restrict the gradient definition to just its
@@ -1119,6 +1130,9 @@ struct PaintRadialGradient : public PaintPatternBase {
       c2 += vec;
     }
     RefPtr stops = aColorLine->MakeGradientStops(aState, stopArray);
+    if (!stops) {
+      return MakeUnique<ColorPattern>(DeviceColor());
+    }
     return MakeUnique<RadialGradientPattern>(c1, c2, r1, r2, std::move(stops),
                                              Matrix::Scaling(1.0, -1.0));
   }
