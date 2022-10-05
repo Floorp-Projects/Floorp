e93ae40c82069ba892d710d3b566e7ac68c15087
Dana Keeler <dkeeler@mozilla.com>
2022-10-05 20:15:02 +0000

    Bug 1716082 - clear all ongoing connections when removing certificate error overrides r=jschanck
    Differential Revision: https://phabricator.services.mozilla.com/D158613

--- security/manager/ssl/nsCertOverrideService.cpp
+++ security/manager/ssl/nsCertOverrideService.cpp
@@ -180,7 +180,6 @@ nsresult nsCertOverrideService::Init() {
   // attempt to read/write any settings file. Otherwise, we would end up
   // reading/writing the wrong settings file after a profile change.
   if (observerService) {
-    observerService->AddObserver(this, "profile-before-change", true);
     observerService->AddObserver(this, "profile-do-change", true);
     // simulate a profile change so we read the current profile's settings file
     Observe(nullptr, "profile-do-change", nullptr);
@@ -193,13 +192,7 @@ nsresult nsCertOverrideService::Init() {
 NS_IMETHODIMP
 nsCertOverrideService::Observe(nsISupports*, const char* aTopic,
                                const char16_t* aData) {
-  // check the topic
-  if (!nsCRT::strcmp(aTopic, "profile-before-change")) {
-    // The profile is about to change,
-    // or is going away because the application is shutting down.
-
-    RemoveAllFromMemory();
-  } else if (!nsCRT::strcmp(aTopic, "profile-do-change")) {
+  if (!nsCRT::strcmp(aTopic, "profile-do-change")) {
     // The profile has already changed.
     // Now read from the new profile location.
     // we also need to update the cached file location
@@ -220,18 +213,21 @@ nsCertOverrideService::Observe(nsISupports*, const char* aTopic,
   return NS_OK;
 }
 
-void nsCertOverrideService::RemoveAllFromMemory() {
-  MutexAutoLock lock(mMutex);
-  mSettingsTable.Clear();
-}
-
 void nsCertOverrideService::RemoveAllTemporaryOverrides() {
   MutexAutoLock lock(mMutex);
+  bool removedAny = false;
   for (auto iter = mSettingsTable.Iter(); !iter.Done(); iter.Next()) {
     nsCertOverrideEntry* entry = iter.Get();
     if (entry->mSettings->mIsTemporary) {
       entry->mSettings->mCert = nullptr;
       iter.Remove();
+      removedAny = true;
+    }
+  }
+  if (removedAny) {
+    nsCOMPtr<nsIObserverService> os = mozilla::services::GetObserverService();
+    if (os) {
+      os->NotifyObservers(nullptr, "net:cancel-all-connections", nullptr);
     }
   }
   // no need to write, as temporaries are never written to disk
@@ -608,6 +604,11 @@ nsCertOverrideService::ClearValidityOverride(
     return NS_ERROR_NOT_AVAILABLE;
   }
 
+  nsCOMPtr<nsIObserverService> os = mozilla::services::GetObserverService();
+  if (os) {
+    os->NotifyObservers(nullptr, "net:cancel-all-connections", nullptr);
+  }
+
   return NS_OK;
 }
 NS_IMETHODIMP
@@ -639,6 +640,11 @@ nsCertOverrideService::ClearAllOverrides() {
     nss->ClearSSLExternalAndInternalSessionCache();
   } else {
     return NS_ERROR_NOT_AVAILABLE;
+  }
+
+  nsCOMPtr<nsIObserverService> os = mozilla::services::GetObserverService();
+  if (os) {
+    os->NotifyObservers(nullptr, "net:cancel-all-connections", nullptr);
   }
 
   return NS_OK;
