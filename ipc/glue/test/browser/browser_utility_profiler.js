ba35ad5a229102a864b1e7eb6bbda95b5cc84402
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2022-10-06 06:14:09 +0000

    Bug 1788596 - Kill audioDecoder_Generic if hanging r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D157479

--- ipc/glue/test/browser/browser_utility_profiler.js
+++ ipc/glue/test/browser/browser_utility_profiler.js
@@ -13,7 +13,7 @@ Services.scriptloader.loadSubScript(
 // When running full suite, previous audio decoding tests might have left some
 // running and this might interfere with our testing
 add_setup(async function ensureNoExistingProcess() {
-  await utilityProcessTest().stopProcess();
+  await killPendingUtilityProcess();
 });
 
 add_task(async () => {
