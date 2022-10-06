31c4c60c08aaf183d53202ceb7893d9be5bad414
Chun-Min Chang <chun.m.chang@gmail.com>
2022-10-06 00:37:23 +0000

    Bug 1774300 - Implement VideoFrame Constructor for OffscreenCanvas r=padenot
    This patch allow constructing a VideoFrame from a OffscreenCanvas.
    
    Depends on D149331
    
    Differential Revision: https://phabricator.services.mozilla.com/D149332

--- dom/media/webcodecs/VideoFrame.h
+++ dom/media/webcodecs/VideoFrame.h
@@ -88,8 +88,8 @@ class VideoFrame final : public nsISupports, public nsWrapperCache {
       const GlobalObject& global, HTMLVideoElement& videoElement,
       const VideoFrameInit& init, ErrorResult& aRv);
   static already_AddRefed<VideoFrame> Constructor(
-      const GlobalObject& global, OffscreenCanvas& offscreenCanvas,
-      const VideoFrameInit& init, ErrorResult& aRv);
+      const GlobalObject& aGlobal, OffscreenCanvas& aOffscreenCanvas,
+      const VideoFrameInit& aInit, ErrorResult& aRv);
   static already_AddRefed<VideoFrame> Constructor(const GlobalObject& aGlobal,
                                                   ImageBitmap& aImageBitmap,
                                                   const VideoFrameInit& aInit,
