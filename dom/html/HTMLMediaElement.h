b919002a7060697bf14a06bb3bd2f7a4b655e18d
Chun-Min Chang <chun.m.chang@gmail.com>
2022-10-06 00:37:24 +0000

    Bug 1774300 - Implement VideoFrame Constructor for HTMLVideoElement r=padenot
    This patch makes it possible to construct a VideoFrame from a
    HTMLVideoElement. The necessity of the paired arguments in this
    constructor is verified in the WebCodecs' wpt.
    
    Depends on D150019
    
    Differential Revision: https://phabricator.services.mozilla.com/D150525

--- dom/html/HTMLMediaElement.h
+++ dom/html/HTMLMediaElement.h
@@ -797,6 +797,7 @@ class HTMLMediaElement : public nsGenericHTMLElement,
     CREATE_PATTERN,
     CREATE_IMAGEBITMAP,
     CAPTURE_STREAM,
+    CREATE_VIDEOFRAME,
   };
   void LogVisibility(CallerAPI aAPI);
 
