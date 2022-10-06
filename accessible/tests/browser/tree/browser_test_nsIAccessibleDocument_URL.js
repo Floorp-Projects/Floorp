a0952fcc673dc86ade26a18eb8c092788dd936f6
Masayuki Nakano <masayuki@d-toybox.com>
2022-10-06 10:04:45 +0000

    Bug 1793534 - part 2: Make the accessibility module handle exposable URIs which do not contain user-pass section r=Jamie
    We shouldn't expose the user-pass section of document URI to another apps since
    it's sensitive data, but non-malicious a11y API users must not need it.
    
    Additionally, `data` and `blob` URI may be too big (e.g., can be some mega
    bytes) and they may need to allocate too big string multiple times in the heap
    per URI.  Therefore, the accessibility module should stop handling URI as-is if
    the document URI is one of them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158735

---
+++ accessible/tests/browser/tree/browser_test_nsIAccessibleDocument_URL.js
@@ -0,0 +1,55 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+async function promiseEventDocumentLoadComplete(expectedURL) {
+  return new Promise(resolve => {
+    waitForEvent(EVENT_DOCUMENT_LOAD_COMPLETE, event => {
+      try {
+        if (
+          event.accessible.QueryInterface(nsIAccessibleDocument).URL ==
+          expectedURL
+        ) {
+          resolve(event.accessible.QueryInterface(nsIAccessibleDocument));
+          return true;
+        }
+        return false;
+      } catch (e) {
+        return false;
+      }
+    });
+  });
+}
+
+add_task(async function testInDataURI() {
+  const kURL = "data:text/html,Some text";
+  const waitForDocumentLoadComplete = promiseEventDocumentLoadComplete("");
+  await BrowserTestUtils.withNewTab(kURL, async browser => {
+    is(
+      (await waitForDocumentLoadComplete).URL,
+      "",
+      "nsIAccessibleDocument.URL shouldn't return data URI"
+    );
+  });
+});
+
+add_task(async function testInHTTPSURIContainingPrivateThings() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["network.auth.confirmAuth.enabled", false]],
+  });
+  const kURL =
+    "https://username:password@example.com/browser/toolkit/content/tests/browser/file_empty.html?query=some#ref";
+  const kURLWithoutUserPass =
+    "https://example.com/browser/toolkit/content/tests/browser/file_empty.html?query=some#ref";
+  const waitForDocumentLoadComplete = promiseEventDocumentLoadComplete(
+    kURLWithoutUserPass
+  );
+  await BrowserTestUtils.withNewTab(kURL, async browser => {
+    is(
+      (await waitForDocumentLoadComplete).URL,
+      kURLWithoutUserPass,
+      "nsIAccessibleDocument.URL shouldn't contain user/pass section"
+    );
+  });
+});
