31c539349771e1429353febff81b0254fa24191c
ahochheiden <ahochheiden@mozilla.com>
2022-10-06 18:02:00 +0000

    Bug 1793828 - Also use `normcase` in addition to `normpath` for `site_package_dirs` r=firefox-build-system-reviewers,nalexander
    This will resolve issues of drive letter uppercase/lowercase mismatch
    causing the venv/site to be considered 'out-of'date'.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158703

--- python/mach/mach/site.py
+++ python/mach/mach/site.py
@@ -780,12 +780,12 @@ class PythonVirtualenv:
         relative_path = path.relative_to(data_path)
 
         # Path to virtualenv's "site-packages" directory for provided sysconfig path
-        return os.path.normpath(Path(self.prefix) / relative_path)
+        return os.path.normpath(os.path.normcase(Path(self.prefix) / relative_path))
 
     def site_packages_dirs(self):
         dirs = []
         if sys.platform.startswith("win"):
-            dirs.append(os.path.normpath(self.prefix))
+            dirs.append(os.path.normpath(os.path.normcase(self.prefix)))
         purelib = self.resolve_sysconfig_packages_path("purelib")
         platlib = self.resolve_sysconfig_packages_path("platlib")
 
