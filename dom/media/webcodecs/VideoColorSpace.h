4fb53b6bf386d50b342c5bfff0b8f41f7f290c6b
Chun-Min Chang <chun.m.chang@gmail.com>
2022-10-06 00:37:20 +0000

    Bug 1774300 - Implement VideoColorSpace r=padenot,jgilbert
    This patch implements the VideoColorSpace created in D144771.
    
    Depends on D144772
    
    Differential Revision: https://phabricator.services.mozilla.com/D145395

--- dom/media/webcodecs/VideoColorSpace.h
+++ dom/media/webcodecs/VideoColorSpace.h
@@ -11,40 +11,33 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/ErrorResult.h"
 #include "mozilla/dom/BindingDeclarations.h"
+#include "mozilla/dom/VideoColorSpaceBinding.h"
 #include "nsCycleCollectionParticipant.h"
 #include "nsWrapperCache.h"
 
-class nsIGlobalObject;  // Add to make it buildable.
-
-namespace mozilla {
-namespace dom {
-
-enum class VideoColorPrimaries : uint8_t;           // Add to make it buildable.
-enum class VideoTransferCharacteristics : uint8_t;  // Add to make it buildable.
-enum class VideoMatrixCoefficients : uint8_t;       // Add to make it buildable.
-struct VideoColorSpaceInit;                         // Add to make it buildable.
-
-}  // namespace dom
-}  // namespace mozilla
+class nsIGlobalObject;
 
 namespace mozilla::dom {
 
-class VideoColorSpace final
-    : public nsISupports /* or NonRefcountedDOMObject if this is a
-                            non-refcounted object */
-    ,
-      public nsWrapperCache /* Change wrapperCache in the binding configuration
-                               if you don't want this */
-{
+class VideoColorSpace final : public nsISupports, public nsWrapperCache {
  public:
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_CYCLE_COLLECTION_WRAPPERCACHE_CLASS(VideoColorSpace)
 
  public:
-  VideoColorSpace();
+  VideoColorSpace(nsIGlobalObject* aParent, const VideoColorSpaceInit& aInit);
 
  protected:
-  ~VideoColorSpace();
+  ~VideoColorSpace() = default;
+
+ private:
+  template <class T>
+  static Nullable<T> ToNullable(const Optional<T>& aInput) {
+    if (aInput.WasPassed()) {
+      return Nullable<T>(aInput.Value());
+    }
+    return Nullable<T>();
+  }
 
  public:
   // This should return something that eventually allows finding a
@@ -56,18 +49,26 @@ class VideoColorSpace final
                        JS::Handle<JSObject*> aGivenProto) override;
 
   static already_AddRefed<VideoColorSpace> Constructor(
-      const GlobalObject& global, const VideoColorSpaceInit& init,
+      const GlobalObject& aGlobal, const VideoColorSpaceInit& aInit,
       ErrorResult& aRv);
 
-  Nullable<VideoColorPrimaries> GetPrimaries() const;
+  Nullable<VideoColorPrimaries> GetPrimaries() const {
+    return ToNullable(mInit.mPrimaries);
+  }
 
-  Nullable<VideoTransferCharacteristics> GetTransfer() const;
+  Nullable<VideoTransferCharacteristics> GetTransfer() const {
+    return ToNullable(mInit.mTransfer);
+  }
 
-  Nullable<VideoMatrixCoefficients> GetMatrix() const;
+  Nullable<VideoMatrixCoefficients> GetMatrix() const {
+    return ToNullable(mInit.mMatrix);
+  }
 
-  Nullable<bool> GetFullRange() const;
+  Nullable<bool> GetFullRange() const { return ToNullable(mInit.mFullRange); }
 
-  void ToJSON(JSContext* cx, JS::MutableHandle<JSObject*> aRetVal);
+ private:
+  nsCOMPtr<nsIGlobalObject> mParent;
+  const VideoColorSpaceInit mInit;
 };
 
 }  // namespace mozilla::dom
