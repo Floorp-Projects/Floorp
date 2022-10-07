bec652a9104189a294d4d2619279f318e9342e97
Bernard Igiri <bigiri@mozilla.com>
2022-10-07 20:21:49 +0000

    Bug 1780239 - Fix screen reader order of Colorway Landmark in FxView r=Gijs
    Added dynamic DOM reordering to FX View so that the order of announcement in screen readers and tab sorting, matches the visual changes made by the responsive CSS layout.
    
    Differential Revision: https://phabricator.services.mozilla.com/D157256

---
+++ browser/components/firefoxview/tests/browser/browser_media_query_dom_sorting.js
@@ -0,0 +1,70 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+const WIDE_WINDOW_WIDTH = 1100;
+const NARROW_WINDOW_WIDTH = 900;
+
+function getTestElements(doc) {
+  return {
+    recentlyClosedTabs: doc.getElementById("recently-closed-tabs-container"),
+    colorwayLandmark: doc.getElementById("colorway-landmark"),
+  };
+}
+
+function isColorwayLandmarkBeforeRecentlyClosedTabs(document) {
+  const recentlyClosedTabs = document.getElementById(
+    "recently-closed-tabs-container"
+  );
+  const colorwayLandmark = document.getElementById("colorway-landmark");
+  return recentlyClosedTabs.previousElementSibling === colorwayLandmark;
+}
+
+async function resizeWindow(win, width) {
+  const resizePromise = BrowserTestUtils.waitForEvent(win, "resize");
+  win.windowUtils.ensureDirtyRootFrame();
+  info("Resizing window...");
+  win.resizeTo(width, win.outerHeight);
+  await resizePromise;
+}
+
+add_task(async function media_query_less_than_65em() {
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: "about:firefoxview",
+    },
+    async browser => {
+      const win = browser.contentWindow;
+      const { recentlyClosedTabs, colorwayLandmark } = getTestElements(
+        win.document
+      );
+      await resizeWindow(win, NARROW_WINDOW_WIDTH);
+      is(
+        recentlyClosedTabs.previousSibling,
+        colorwayLandmark,
+        "colorway landmark has been positioned before recently closed tabs"
+      );
+    }
+  );
+});
+
+add_task(async function media_query_more_than_65em() {
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: "about:firefoxview",
+    },
+    async browser => {
+      const win = browser.contentWindow;
+      const { recentlyClosedTabs, colorwayLandmark } = getTestElements(
+        win.document
+      );
+      await resizeWindow(win, WIDE_WINDOW_WIDTH);
+      is(
+        recentlyClosedTabs.nextSibling,
+        colorwayLandmark,
+        "colorway landmark has been positioned after recently closed tabs"
+      );
+    }
+  );
+});
