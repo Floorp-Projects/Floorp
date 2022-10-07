9fb696409d0d24d2d93c6e7626489c04a496a95a
pmcmanis <pmcmanis@mozilla.com>
2022-10-07 20:19:49 +0000

    Bug 1793786 - Update Glean version to 51.4.0 r=chutten,supply-chain-reviewers,webdriver-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D158759

--- third_party/rust/uniffi_bindgen/src/macro_metadata/ci.rs
+++ third_party/rust/uniffi_bindgen/src/macro_metadata/ci.rs
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use crate::interface::ComponentInterface;
+use anyhow::anyhow;
 use uniffi_meta::Metadata;
 
 /// Add Metadata items to the ComponentInterface
@@ -18,9 +19,32 @@ pub fn add_to_ci(
     metadata_items: Vec<Metadata>,
 ) -> anyhow::Result<()> {
     for item in metadata_items {
+        let (item_desc, crate_name) = match &item {
+            Metadata::Func(meta) => (
+                format!("function `{}`", meta.name),
+                meta.module_path.first().unwrap(),
+            ),
+            Metadata::Method(meta) => (
+                format!("method `{}.{}`", meta.self_name, meta.name),
+                meta.module_path.first().unwrap(),
+            ),
+        };
+
+        let ns = iface.namespace();
+        if crate_name != ns {
+            return Err(anyhow!("Found {item_desc} from crate `{crate_name}`.")
+                .context(format!(
+                    "Main crate is expected to be named `{ns}` based on the UDL namespace."
+                ))
+                .context("Mixing symbols from multiple crates is not supported yet."));
+        }
+
         match item {
             Metadata::Func(meta) => {
                 iface.add_function_definition(meta.into())?;
+            }
+            Metadata::Method(meta) => {
+                iface.add_method_definition(meta)?;
             }
         }
     }
