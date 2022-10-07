f048ad0ec2ba2c79c7f0752c6b8597fe42e02b69
Michael Froman <mfroman@mozilla.com>
2022-10-07 12:21:13 -0500

    Bug 1790097 - Vendor libwebrtc from 2b46a5870b
    Upstream commit: https://webrtc.googlesource.com/src/+/2b46a5870b46d6d093deecdedc899744ae89f2cf
        Add proxy access to some methods in Obj-C SDK
    
        Most calls to C++ PeerConnection and related classes are proxied
        to internal threads in WebRTC. However, there is no such thing
        in the Obj-C SDK.
        It would be nice to proxy methods in the Obj-C SDK as well.
    
        RTCMediaStream and RTCVideoTrack have NSMutableArray members,
        and it can throw NSRangeException when it has race conditions,
        so that it would be a good starting point.
    
        Also, remove some NSAsserts as its condition isn't a fatal error,
        and it doesn't affect the production already.
    
        Bug: None
        Change-Id: I10b44a9c773d62a5c04c254986733a6b67d51617
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/262840
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Commit-Queue: Daniel.L (Byoungchan) Lee <daniel.l@hpcnt.com>
        Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37283}

--- third_party/libwebrtc/sdk/objc/api/peerconnection/RTCMediaStream.mm
+++ third_party/libwebrtc/sdk/objc/api/peerconnection/RTCMediaStream.mm
@@ -10,8 +10,6 @@
 
 #import "RTCMediaStream+Private.h"
 
-#include <vector>
-
 #import "RTCAudioTrack+Private.h"
 #import "RTCMediaStreamTrack+Private.h"
 #import "RTCPeerConnectionFactory+Private.h"
@@ -20,8 +18,9 @@
 
 @implementation RTC_OBJC_TYPE (RTCMediaStream) {
   RTC_OBJC_TYPE(RTCPeerConnectionFactory) * _factory;
-  NSMutableArray *_audioTracks;
-  NSMutableArray *_videoTracks;
+  rtc::Thread *_signalingThread;
+  NSMutableArray *_audioTracks /* accessed on _signalingThread */;
+  NSMutableArray *_videoTracks /* accessed on _signalingThread */;
   rtc::scoped_refptr<webrtc::MediaStreamInterface> _nativeMediaStream;
 }
 
@@ -36,10 +35,18 @@
 }
 
 - (NSArray<RTC_OBJC_TYPE(RTCAudioTrack) *> *)audioTracks {
+  if (!_signalingThread->IsCurrent()) {
+    return _signalingThread->Invoke<NSArray<RTC_OBJC_TYPE(RTCAudioTrack) *> *>(
+        RTC_FROM_HERE, [self]() { return self.audioTracks; });
+  }
   return [_audioTracks copy];
 }
 
 - (NSArray<RTC_OBJC_TYPE(RTCVideoTrack) *> *)videoTracks {
+  if (!_signalingThread->IsCurrent()) {
+    return _signalingThread->Invoke<NSArray<RTC_OBJC_TYPE(RTCVideoTrack) *> *>(
+        RTC_FROM_HERE, [self]() { return self.videoTracks; });
+  }
   return [_videoTracks copy];
 }
 
@@ -48,33 +55,52 @@
 }
 
 - (void)addAudioTrack:(RTC_OBJC_TYPE(RTCAudioTrack) *)audioTrack {
+  if (!_signalingThread->IsCurrent()) {
+    return _signalingThread->Invoke<void>(
+        RTC_FROM_HERE, [audioTrack, self]() { return [self addAudioTrack:audioTrack]; });
+  }
   if (_nativeMediaStream->AddTrack(audioTrack.nativeAudioTrack)) {
     [_audioTracks addObject:audioTrack];
   }
 }
 
 - (void)addVideoTrack:(RTC_OBJC_TYPE(RTCVideoTrack) *)videoTrack {
+  if (!_signalingThread->IsCurrent()) {
+    return _signalingThread->Invoke<void>(
+        RTC_FROM_HERE, [videoTrack, self]() { return [self addVideoTrack:videoTrack]; });
+  }
   if (_nativeMediaStream->AddTrack(videoTrack.nativeVideoTrack)) {
     [_videoTracks addObject:videoTrack];
   }
 }
 
 - (void)removeAudioTrack:(RTC_OBJC_TYPE(RTCAudioTrack) *)audioTrack {
+  if (!_signalingThread->IsCurrent()) {
+    return _signalingThread->Invoke<void>(
+        RTC_FROM_HERE, [audioTrack, self]() { return [self removeAudioTrack:audioTrack]; });
+  }
   NSUInteger index = [_audioTracks indexOfObjectIdenticalTo:audioTrack];
-  NSAssert(index != NSNotFound,
-           @"|removeAudioTrack| called on unexpected RTC_OBJC_TYPE(RTCAudioTrack)");
-  if (index != NSNotFound &&
-      _nativeMediaStream->RemoveTrack(audioTrack.nativeAudioTrack)) {
+  if (index == NSNotFound) {
+    RTC_LOG(LS_INFO) << "|removeAudioTrack| called on unexpected RTC_OBJC_TYPE(RTCAudioTrack)";
+    return;
+  }
+  if (_nativeMediaStream->RemoveTrack(audioTrack.nativeAudioTrack)) {
     [_audioTracks removeObjectAtIndex:index];
   }
 }
 
 - (void)removeVideoTrack:(RTC_OBJC_TYPE(RTCVideoTrack) *)videoTrack {
+  if (!_signalingThread->IsCurrent()) {
+    return _signalingThread->Invoke<void>(
+        RTC_FROM_HERE, [videoTrack, self]() { return [self removeVideoTrack:videoTrack]; });
+  }
   NSUInteger index = [_videoTracks indexOfObjectIdenticalTo:videoTrack];
-  NSAssert(index != NSNotFound,
-           @"|removeVideoTrack| called on unexpected RTC_OBJC_TYPE(RTCVideoTrack)");
-  if (index != NSNotFound &&
-      _nativeMediaStream->RemoveTrack(videoTrack.nativeVideoTrack)) {
+  if (index == NSNotFound) {
+    RTC_LOG(LS_INFO) << "|removeVideoTrack| called on unexpected RTC_OBJC_TYPE(RTCVideoTrack)";
+    return;
+  }
+
+  if (_nativeMediaStream->RemoveTrack(videoTrack.nativeVideoTrack)) {
     [_videoTracks removeObjectAtIndex:index];
   }
 }
@@ -98,6 +124,7 @@
   NSParameterAssert(nativeMediaStream);
   if (self = [super init]) {
     _factory = factory;
+    _signalingThread = factory.signalingThread;
 
     webrtc::AudioTrackVector audioTracks = nativeMediaStream->GetAudioTracks();
     webrtc::VideoTrackVector videoTracks = nativeMediaStream->GetVideoTracks();
