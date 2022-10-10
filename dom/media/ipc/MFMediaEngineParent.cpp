9956dbbaf2a8ebd3b6e74f4bc92254f0fa4e1169
alwu <alwu@mozilla.com>
2022-10-10 20:13:29 +0000

    Bug 1791892 - part1 : return nullptr for the stream request if the media source has been shutdowned. r=jolin
    Differential Revision: https://phabricator.services.mozilla.com/D157882

--- dom/media/ipc/MFMediaEngineParent.cpp
+++ dom/media/ipc/MFMediaEngineParent.cpp
@@ -302,8 +302,11 @@ void MFMediaEngineParent::NotifyError(MF_MEDIA_ENGINE_ERR aError,
 
 MFMediaEngineStreamWrapper* MFMediaEngineParent::GetMediaEngineStream(
     TrackType aType, const CreateDecoderParams& aParam) {
+  // Has been shutdowned.
+  if (!mMediaSource) {
+    return nullptr;
+  }
   LOG("Create a media engine decoder for %s", TrackTypeToStr(aType));
-  MOZ_ASSERT(mMediaSource);
   if (aType == TrackType::kAudioTrack) {
     auto* stream = mMediaSource->GetAudioStream();
     return new MFMediaEngineStreamWrapper(stream, stream->GetTaskQueue(),
