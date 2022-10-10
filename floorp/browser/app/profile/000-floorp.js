4285d70323b645faae20eb5e48fab2728247d535
surapunoyousei <ryosukeusuusu@gmail.com>
2022-10-10 12:37:28 +0900

    [FB] Browser Manager Sidebar | Set manager tool width

--- floorp/browser/app/profile/000-floorp.js
+++ floorp/browser/app/profile/000-floorp.js
@@ -97,6 +97,11 @@ pref("floorp.browser.sidebar2.customurl17", "");
 pref("floorp.browser.sidebar2.customurl18", "");
 pref("floorp.browser.sidebar2.customurl19", "");
 
+pref("floorp.browser.sidebar2.width.mode0", 600);	
+pref("floorp.browser.sidebar2.width.mode3", 415);
+pref("floorp.browser.sidebar2.width.mode4", 415);
+pref("floorp.browser.sidebar2.width.mode5", 450);
+
 //container tab for sidebar2 webpanel
 pref("floorp.browser.sidebar2.customurl0.usercontext", 0);
 pref("floorp.browser.sidebar2.customurl1.usercontext", 0);
