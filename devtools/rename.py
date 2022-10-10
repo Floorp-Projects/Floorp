890d1747be396e74a5e87c73e1177b8c7ef151c4
Alexandre Poirot <poirot.alex@gmail.com>
2022-10-10 11:44:59 +0000

    Bug 1793760 - [devtools] Use same URL for development version of react. r=jdescottes
    When using --enable-debug-js-modules in mozconfig,
    today, we ship the modules with different filename and so different URLs.
    
    This won't work once the modules will become ES Modules
    as we would load the modules from a fixed URL and there won't be
    any way to hack the module loader anymore.
    
    So, we should rather have the build system to ship the right file
    (production or dev version), using the same filename and URL.
    
    Unfortunately, I didn't find any easier way to rename a file from moz.build
    
    Differential Revision: https://phabricator.services.mozilla.com/D158665

---
+++ devtools/rename.py
@@ -0,0 +1,11 @@
+# -*- Mode: python; indent-tabs-mode: nil; tab-width: 40 -*-
+# vim: set filetype=python:
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Put the content of `filenames[0]` file into `output` file pointer
+def main(output, *filenames):
+    with open(filenames[0], "r", encoding="utf-8") as f:
+        content = f.read()
+        output.write(content)
