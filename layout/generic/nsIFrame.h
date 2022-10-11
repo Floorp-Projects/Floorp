47d951e48eaade7d443c35ef414eebdbcf2f0f5e
Ting-Yu Lin <tlin@mozilla.com>
2022-10-11 21:01:27 +0000

    Bug 1794456 Part 1 - Reimplement reverse iterator for nsFrameList. r=emilio
    If we traverse a frame list via Iterator::operator--() repeatedly, when it
    reaches the beginning of the list (mCurrent's previous sibiling is null), it
    will start over from the end of the list. This behavior can be surprising, so it
    is removed. Also, remove Iterator::mList since it's no longer needed.
    
    We don't really need to rely on mozilla::ReverseIterator to implement
    nsFrameList::reverse_iterator. We can abstract frame traversal operations as a
    template parameter, and implement reverse_iterator directly. This is more
    performant since mozilla::ReverseIterator makes a copy of the original iterator
    when dereferencing the reverse iterator.
    
    Iterator(nullptr) is now served as a end iterator for both the iterator and
    reverse_iterator.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158984

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -5733,18 +5733,22 @@ inline void nsFrameList::Enumerator::Next() {
 // Operators of nsFrameList::Iterator
 // ---------------------------------------------------
 
-inline nsFrameList::Iterator& nsFrameList::Iterator::operator++() {
-  mCurrent = mCurrent->GetNextSibling();
-  return *this;
+inline nsIFrame* nsFrameList::ForwardFrameTraversal::Next(nsIFrame* aFrame) {
+  MOZ_ASSERT(aFrame);
+  return aFrame->GetNextSibling();
+}
+inline nsIFrame* nsFrameList::ForwardFrameTraversal::Prev(nsIFrame* aFrame) {
+  MOZ_ASSERT(aFrame);
+  return aFrame->GetPrevSibling();
 }
 
-inline nsFrameList::Iterator& nsFrameList::Iterator::operator--() {
-  if (!mCurrent) {
-    mCurrent = mList.LastChild();
-  } else {
-    mCurrent = mCurrent->GetPrevSibling();
-  }
-  return *this;
+inline nsIFrame* nsFrameList::BackwardFrameTraversal::Next(nsIFrame* aFrame) {
+  MOZ_ASSERT(aFrame);
+  return aFrame->GetPrevSibling();
+}
+inline nsIFrame* nsFrameList::BackwardFrameTraversal::Prev(nsIFrame* aFrame) {
+  MOZ_ASSERT(aFrame);
+  return aFrame->GetNextSibling();
 }
 
 #endif /* nsIFrame_h___ */
