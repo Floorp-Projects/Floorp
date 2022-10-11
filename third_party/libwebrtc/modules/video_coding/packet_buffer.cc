bdb826a6ecded3f12aa2f57a099266966ca716bc
Michael Froman <mfroman@mozilla.com>
2022-10-10 19:02:37 -0500

    Bug 1790097 - Vendor libwebrtc from 3f659b1b3c
    Upstream commit: https://webrtc.googlesource.com/src/+/3f659b1b3ca857ba843efafbb97cf44f1e7df556
        Continue looking for frames after padding packets
    
        In H264, reordered packets can cause a frame following padding to become stuck in the packet buffer.
        A minimal example:
        _, P, 1  - padding packet p and frame 1. Frame 1 has not been returned because of missing packet 0
        0, P, 1  - when packet 0 arrives, FindFrames will stop incrementing i when it sees padding packet P, and frame 1 will never be returned
    
        Bug: webrtc:14216
        Change-Id: I78b76df9709fa8593c5025d647e2b868af3749f2
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/266465
        Reviewed-by: Philip Eliasson <philipel@webrtc.org>
        Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37357}

--- third_party/libwebrtc/modules/video_coding/packet_buffer.cc
+++ third_party/libwebrtc/modules/video_coding/packet_buffer.cc
@@ -107,6 +107,10 @@ PacketBuffer::InsertResult PacketBuffer::InsertPacket(
 
   UpdateMissingPackets(seq_num);
 
+  received_padding_.erase(
+      received_padding_.begin(),
+      received_padding_.lower_bound(seq_num - (buffer_.size() / 4)));
+
   result.packets = FindFrames(seq_num);
   return result;
 }
@@ -142,11 +146,11 @@ uint32_t PacketBuffer::ClearTo(uint16_t seq_num) {
   first_seq_num_ = seq_num;
 
   is_cleared_to_first_seq_num_ = true;
-  auto clear_to_it = missing_packets_.upper_bound(seq_num);
-  if (clear_to_it != missing_packets_.begin()) {
-    --clear_to_it;
-    missing_packets_.erase(missing_packets_.begin(), clear_to_it);
-  }
+  missing_packets_.erase(missing_packets_.begin(),
+                         missing_packets_.lower_bound(seq_num));
+
+  received_padding_.erase(received_padding_.begin(),
+                          received_padding_.lower_bound(seq_num));
 
   return num_cleared_packets;
 }
@@ -158,6 +162,7 @@ void PacketBuffer::Clear() {
 PacketBuffer::InsertResult PacketBuffer::InsertPadding(uint16_t seq_num) {
   PacketBuffer::InsertResult result;
   UpdateMissingPackets(seq_num);
+  received_padding_.insert(seq_num);
   result.packets = FindFrames(static_cast<uint16_t>(seq_num + 1));
   return result;
 }
@@ -175,6 +180,7 @@ void PacketBuffer::ClearInternal() {
   is_cleared_to_first_seq_num_ = false;
   newest_inserted_seq_num_.reset();
   missing_packets_.clear();
+  received_padding_.clear();
 }
 
 bool PacketBuffer::ExpandBufferSize() {
@@ -223,7 +229,18 @@ bool PacketBuffer::PotentialNewFrame(uint16_t seq_num) const {
 std::vector<std::unique_ptr<PacketBuffer::Packet>> PacketBuffer::FindFrames(
     uint16_t seq_num) {
   std::vector<std::unique_ptr<PacketBuffer::Packet>> found_frames;
-  for (size_t i = 0; i < buffer_.size() && PotentialNewFrame(seq_num); ++i) {
+  auto start = seq_num;
+
+  for (size_t i = 0; i < buffer_.size(); ++i) {
+    if (received_padding_.find(seq_num) != received_padding_.end()) {
+      seq_num += 1;
+      continue;
+    }
+
+    if (!PotentialNewFrame(seq_num)) {
+      break;
+    }
+
     size_t index = seq_num % buffer_.size();
     buffer_[index]->continuous = true;
 
@@ -363,6 +380,8 @@ std::vector<std::unique_ptr<PacketBuffer::Packet>> PacketBuffer::FindFrames(
 
         missing_packets_.erase(missing_packets_.begin(),
                                missing_packets_.upper_bound(seq_num));
+        received_padding_.erase(received_padding_.lower_bound(start),
+                                received_padding_.upper_bound(seq_num));
       }
     }
     ++seq_num;
