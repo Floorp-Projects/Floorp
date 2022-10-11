45ce9b2fbe44c909e506bef708ce47c058674f26
Marian-Vasile Laza <mlaza@mozilla.com>
2022-10-12 02:16:57 +0300

    Backed out changeset be17ad2c9d94 (bug 1794634) for causing mochitest failures on browser_rootgroup.js. CLOSED TREE

--- accessible/ipc/other/DocAccessibleChild.h
+++ accessible/ipc/other/DocAccessibleChild.h
@@ -82,6 +82,9 @@ class DocAccessibleChild : public DocAccessibleChildBase {
   virtual mozilla::ipc::IPCResult RecvLandmarkRole(
       const uint64_t& aID, nsString* aLandmark) override;
 
+  virtual mozilla::ipc::IPCResult RecvARIARoleAtom(const uint64_t& aID,
+                                                   nsString* aRole) override;
+
   virtual mozilla::ipc::IPCResult RecvGroupPosition(
       const uint64_t& aID, int32_t* aLevel, int32_t* aSimilarItemsInGroup,
       int32_t* aPositionInGroup) override;
