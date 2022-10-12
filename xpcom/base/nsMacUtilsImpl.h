3489c06d265fb5daabf7facec45c6c34335fe63b
Andrew McCreight <continuation@gmail.com>
2022-10-12 16:19:01 +0000

    Bug 1792567, part 2 - Turn nsMacUtilsImpl into a namespace. r=spohl
    All of the private methods and fields get moved into the .cpp file,
    and reordered where needed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158204

--- xpcom/base/nsMacUtilsImpl.h
+++ xpcom/base/nsMacUtilsImpl.h
@@ -19,65 +19,36 @@ using mozilla::StaticMutex;
 
 class nsIFile;
 
-class nsMacUtilsImpl final {
- public:
-  nsMacUtilsImpl() {}
+namespace nsMacUtilsImpl {
 
-  // Return the repo directory and the repo object directory respectively.
-  // These should only be used on Mac developer builds to determine the path
-  // to the repo or object directory.
-  static nsresult GetRepoDir(nsIFile** aRepoDir);
-  static nsresult GetObjDir(nsIFile** aObjDir);
+// Return the repo directory and the repo object directory respectively.
+// These should only be used on Mac developer builds to determine the path
+// to the repo or object directory.
+nsresult GetRepoDir(nsIFile** aRepoDir);
+nsresult GetObjDir(nsIFile** aObjDir);
 
 #if defined(MOZ_SANDBOX) || defined(__aarch64__)
-  static bool GetAppPath(nsCString& aAppPath);
+bool GetAppPath(nsCString& aAppPath);
 #endif /* MOZ_SANDBOX || __aarch64__ */
 
 #if defined(MOZ_SANDBOX) && defined(DEBUG)
-  static nsresult GetBloatLogDir(nsCString& aDirectoryPath);
-  static nsresult GetDirectoryPath(const char* aPath,
-                                   nsCString& aDirectoryPath);
+nsresult GetBloatLogDir(nsCString& aDirectoryPath);
+nsresult GetDirectoryPath(const char* aPath, nsCString& aDirectoryPath);
 #endif /* MOZ_SANDBOX && DEBUG */
 
-  static void EnableTCSMIfAvailable();
-  static bool IsTCSMAvailable();
-  static uint32_t GetPhysicalCPUCount();
-  static nsresult GetArchitecturesForBinary(const char* aPath,
-                                            uint32_t* aArchMask);
+void EnableTCSMIfAvailable();
+bool IsTCSMAvailable();
+uint32_t GetPhysicalCPUCount();
+nsresult GetArchitecturesForBinary(const char* aPath, uint32_t* aArchMask);
 
 #if defined(__aarch64__)
-  // Pre-translate binaries to avoid translation delays when launching
-  // x64 child process instances for the first time. i.e. on first launch
-  // after installation or after an update. Translations are cached so
-  // repeated launches of the binaries do not encounter delays.
-  static int PreTranslateXUL();
-  static int PreTranslateBinary(nsCString aBinaryPath);
+// Pre-translate binaries to avoid translation delays when launching
+// x64 child process instances for the first time. i.e. on first launch
+// after installation or after an update. Translations are cached so
+// repeated launches of the binaries do not encounter delays.
+int PreTranslateXUL();
+int PreTranslateBinary(nsCString aBinaryPath);
 #endif
-
- private:
-  ~nsMacUtilsImpl() {}
-#if defined(MOZ_SANDBOX) || defined(__aarch64__)
-  // Cache the appDir returned from GetAppPath to avoid doing I/O
-  static StaticAutoPtr<nsCString> sCachedAppPath
-      MOZ_GUARDED_BY(sCachedAppPathMutex);
-  // For thread safe setting/checking of sCachedAppPath
-  static StaticMutex sCachedAppPathMutex;
-  // Utility method to call ClearOnShutdown() on the main thread
-  static nsresult ClearCachedAppPathOnShutdown();
-#endif
-
-#if defined(__aarch64__)
-  // Limit XUL translation to one attempt
-  static std::atomic<bool> sIsXULTranslated;
-#endif
-
-  enum TCSMStatus { TCSM_Unknown = 0, TCSM_Available, TCSM_Unavailable };
-  static mozilla::Atomic<nsMacUtilsImpl::TCSMStatus> sTCSMStatus;
-
-  static nsresult EnableTCSM();
-#if defined(DEBUG)
-  static bool IsTCSMEnabled();
-#endif
-};
+}  // namespace nsMacUtilsImpl
 
 #endif /* nsMacUtilsImpl_h___ */
