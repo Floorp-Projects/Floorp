2f3feb0bdd2719678f8fdb973650dc5a31526db3
Zachary Svoboda <cczac1@gmail.com>
2022-10-13 13:50:39 +0000

    Bug 1327754 - Automatically focus json viewer scroll panel r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D158893

---
+++ devtools/client/jsonview/test/browser_jsonview_initial_focus.js
@@ -0,0 +1,33 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const VALID_JSON_URL = URL_ROOT + "valid_json.json";
+
+add_task(async function() {
+  info("Test focus JSON view started");
+  await addJsonViewTab(VALID_JSON_URL);
+
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [], async () => {
+    const scroller = content.document.getElementById("json-scrolling-panel");
+    ok(scroller, "Found the scrollable area");
+    is(
+      content.document.activeElement,
+      scroller,
+      "Scrollable area initially focused"
+    );
+  });
+  await reloadBrowser();
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [], async () => {
+    const scroller = await ContentTaskUtils.waitForCondition(
+      () => content.document.getElementById("json-scrolling-panel"),
+      "Wait for the panel to be loaded"
+    );
+    is(
+      content.document.activeElement,
+      scroller,
+      "Scrollable area focused after reload"
+    );
+  });
+});
