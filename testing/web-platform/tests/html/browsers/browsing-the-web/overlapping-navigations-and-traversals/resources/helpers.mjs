b11fd50e6456cd5c98d445210fd07735d5e715db
Domenic Denicola <d@domenic.me>
2022-10-13 16:14:17 +0000

    Bug 1794361 [wpt PR 36364] - Update overlapping nav and traversal tests to match the spec rewrite, a=testonly
    Automatic update from web-platform-tests
    Update overlapping nav and traversal tests to match the spec rewrite
    
    The results are still not 100% certain, especially for overlapping traversals. But we can mark these as non-tentative for now, with the understanding (documented in README.md) that we can update them later as we update the spec.
    --
    
    wpt-commits: 663bc1e45a0f6922a77af78fbef3f8e4d1902dd8
    wpt-pr: 36364

--- testing/web-platform/tests/html/browsers/browsing-the-web/overlapping-navigations-and-traversals/resources/helpers.mjs
+++ testing/web-platform/tests/html/browsers/browsing-the-web/overlapping-navigations-and-traversals/resources/helpers.mjs
@@ -35,11 +35,18 @@ export function waitForPopstate(obj) {
 // This is used when we want to end the test by asserting some load doesn't
 // happen, but we're not sure how long to wait. We could just wait a long-ish
 // time (e.g. a second), but that makes the tests slow. Instead, assume that
-// network loads take roughly the same time, so by waiting for 2x the duration
-// of a separate iframe load, we would have caught any problems.
+// network loads take roughly the same time. Then, you can use this function to
+// wait a small multiple of the duration of a separate iframe load; this should
+// be long enough to catch any problems.
 export async function waitForPotentialNetworkLoads(t) {
   const before = performance.now();
+
+  // Sometimes we're doing something, like a traversal, which cancels our first
+  // attempt at iframe loading. In that case we bail out after 100 ms and try
+  // again. (Better ideas welcome...)
+  await Promise.race([createIframe(t), delay(t, 100)]);
   await createIframe(t);
+
   const after = performance.now();
   await delay(t, after - before);
 }
