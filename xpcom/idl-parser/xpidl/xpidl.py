b3d18d7faca122ebf024cc600ee96b7fe9a5f3e3
Andrew McCreight <continuation@gmail.com>
2022-10-13 23:41:13 +0000

    Bug 1795102, part 2 - Make size_is checking use NameMap. r=nika
    This also changes the location from the location of the method to the
    parameter which seems better.
    
    Make the other error message use p.location, and change the error
    message to say "size_is" instead of "is_size".
    
    Differential Revision: https://phabricator.services.mozilla.com/D159317

--- xpcom/idl-parser/xpidl/xpidl.py
+++ xpcom/idl-parser/xpidl/xpidl.py
@@ -1341,22 +1341,11 @@ class Method(object):
                     self.location,
                 )
             if p.size_is:
-                found_size_param = False
-                for size_param in self.params:
-                    if p.size_is == size_param.name:
-                        found_size_param = True
-                        if (
-                            getBuiltinOrNativeTypeName(size_param.realtype)
-                            != "unsigned long"
-                        ):
-                            raise IDLError(
-                                "is_size parameter must have type 'unsigned long'",
-                                self.location,
-                            )
-                if not found_size_param:
+                size_param = self.namemap.get(p.size_is, p.location)
+                if getBuiltinOrNativeTypeName(size_param.realtype) != "unsigned long":
                     raise IDLError(
-                        "could not find is_size parameter '%s'" % p.size_is,
-                        self.location,
+                        "size_is parameter must have type 'unsigned long'",
+                        p.location,
                     )
 
     def isScriptable(self):
