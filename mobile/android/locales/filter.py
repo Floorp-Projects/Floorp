f3cd148033caf89d9b95358fa440c2dc66ce7653
Eemeli Aro <eemeli@mozilla.com>
2022-10-13 16:28:32 +0000

    Bug 1760033 - Remove aboutReader.properties and its remaining strings as unused. r=Gijs,geckoview-reviewers,m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D158577

--- mobile/android/locales/filter.py
+++ mobile/android/locales/filter.py
@@ -20,7 +20,6 @@ def test(mod, path, entity=None):
     if mod == "toolkit":
         # keep this file list in sync with jar.mn
         if path in (
-            "chrome/global/aboutReader.properties",
             "chrome/global/commonDialogs.properties",
             "chrome/global/intl.properties",
             "chrome/global/intl.css",
