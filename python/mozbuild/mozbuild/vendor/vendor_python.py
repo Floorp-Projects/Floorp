80c1cc599a6a429b785a0f80f56f2fee6bcfc71b
Alex Hochheiden <ahochheiden@mozilla.com>
2022-10-13 00:40:06 +0000

    Bug 1766497: Remove unused `virtualenv` package r=ahal
    Now that Python's stdlib `venv` is used, `virtualenv` is no longer
    needed.
    
    Since mozharness depends on the wheels for `setuptools` and `pip`, and
    they were vendored within `virtualenv` we must move them somewhere else.
    
    I've chosen to add `third_party/python/venv/wheels` to hold those wheels
    now that `virtualenv` is no longer there.
    
    Depends on D145786
    
    Differential Revision: https://phabricator.services.mozilla.com/D145787

--- python/mozbuild/mozbuild/vendor/vendor_python.py
+++ python/mozbuild/mozbuild/vendor/vendor_python.py
@@ -163,9 +163,8 @@ def _purge_vendor_dir(vendor_dir):
         "dlmanager",
         # gyp's package on PyPI doesn't have any downloadable files.
         "gyp",
-        # We manage installing "virtualenv" package manually, and we have a custom
-        # "virtualenv.py" entry module.
-        "virtualenv",
+        # We keep some wheels vendored in "_venv" for use in Mozharness
+        "_venv",
         # We manage vendoring "vsdownload" with a moz.yaml file (there is no module
         # on PyPI).
         "vsdownload",
