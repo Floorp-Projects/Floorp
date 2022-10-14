f912039ee9458293a6128180b1f37ca7912e4931
Julian Descottes <jdescottes@mozilla.com>
2022-10-14 20:43:03 +0000

    Bug 1765619 - [webdriver-bidi] Add gecko specific webdriver test for browsingContext.create context argument r=webdriver-reviewers,whimboo
    Depends on D143653
    
    Differential Revision: https://phabricator.services.mozilla.com/D143654

---
+++ testing/web-platform/mozilla/tests/webdriver/bidi/browsing_context/create/type_hint.py
@@ -0,0 +1,32 @@
+import pytest
+
+from tests.support.asserts import assert_success
+
+from . import using_context
+
+pytestmark = pytest.mark.asyncio
+
+
+def count_window_handles(session):
+    with using_context(session, "chrome"):
+        response = session.transport.send(
+            "GET", "session/{session_id}/window/handles".format(**vars(session))
+        )
+        chrome_handles = assert_success(response)
+        return len(chrome_handles)
+
+
+@pytest.mark.parametrize("type_hint", ["tab", "window"])
+async def test_type_hint(bidi_session, current_session, type_hint):
+    assert len(await bidi_session.browsing_context.get_tree()) == 1
+    assert count_window_handles(current_session) == 1
+
+    await bidi_session.browsing_context.create(type_hint=type_hint)
+
+    if type_hint == "window":
+        expected_window_count = 2
+    else:
+        expected_window_count = 1
+
+    assert len(await bidi_session.browsing_context.get_tree()) == 2
+    assert count_window_handles(current_session) == expected_window_count
