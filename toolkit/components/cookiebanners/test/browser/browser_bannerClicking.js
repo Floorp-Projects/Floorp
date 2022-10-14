57ba55ad0790431ea6f03c39140f916847fc4005
Tim Huang <tihuang@mozilla.com>
2022-10-14 19:59:44 +0000

    Bug 1788225 - Part 4: Add tests for cookie banner domain preferences. r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D157870

--- toolkit/components/cookiebanners/test/browser/browser_bannerClicking.js
+++ toolkit/components/cookiebanners/test/browser/browser_bannerClicking.js
@@ -3,6 +3,9 @@
 
 "use strict";
 
+// TODO: Split up the test into multiple tests (Bug 1795238)
+requestLongerTimeout(3);
+
 const TEST_DOMAIN_A = "example.com";
 const TEST_DOMAIN_B = "example.org";
 const TEST_DOMAIN_C = "example.net";
@@ -682,4 +685,130 @@ add_task(async function test_clicking_global_rules_precedence() {
     // applies, opt-in means the domain specific rule applies.
     expected: "OptIn",
   });
+});
+
+/**
+ * Test that domain preference takes precedence over pref settings.
+ */
+add_task(async function test_domain_preference() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["cookiebanners.service.mode", Ci.nsICookieBannerService.MODE_REJECT],
+      [
+        "cookiebanners.service.mode.privateBrowsing",
+        Ci.nsICookieBannerService.MODE_REJECT,
+      ],
+    ],
+  });
+
+  insertTestRules();
+
+  for (let testPBM of [false, true]) {
+    let testWin = window;
+    if (testPBM) {
+      testWin = await BrowserTestUtils.openNewBrowserWindow({
+        private: true,
+      });
+    }
+
+    info(
+      "Make sure the example.org follows the pref setting when there is no domain preference."
+    );
+    await openPageAndVerify({
+      win: testWin,
+      domain: TEST_DOMAIN_B,
+      testURL: TEST_PAGE_B,
+      visible: true,
+      expected: "NoClick",
+    });
+
+    info("Set the domain preference of example.org to MODE_REJECT_OR_ACCEPT");
+    let uri = Services.io.newURI(TEST_ORIGIN_B);
+    Services.cookieBanners.setDomainPref(
+      uri,
+      Ci.nsICookieBannerService.MODE_REJECT_OR_ACCEPT,
+      testPBM
+    );
+
+    info(
+      "Verify if domain preference takes precedence over then the pref setting for example.org"
+    );
+    await openPageAndVerify({
+      win: testWin,
+      domain: TEST_DOMAIN_B,
+      testURL: TEST_PAGE_B,
+      visible: false,
+      expected: "OptIn",
+    });
+
+    Services.cookieBanners.removeAllDomainPrefs(testPBM);
+
+    if (testPBM) {
+      await BrowserTestUtils.closeWindow(testWin);
+    }
+  }
+});
+
+/**
+ * Test that domain preference works on the top-level domain.
+ */
+add_task(async function test_domain_preference_iframe() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["cookiebanners.service.mode", Ci.nsICookieBannerService.MODE_REJECT],
+      [
+        "cookiebanners.service.mode.privateBrowsing",
+        Ci.nsICookieBannerService.MODE_REJECT,
+      ],
+    ],
+  });
+
+  insertTestRules();
+
+  for (let testPBM of [false, true]) {
+    let testWin = window;
+    if (testPBM) {
+      testWin = await BrowserTestUtils.openNewBrowserWindow({
+        private: true,
+      });
+    }
+
+    info(
+      "Make sure the example.org follows the pref setting when there is no domain preference for the top-level example.net."
+    );
+    await openIframeAndVerify({
+      win: testWin,
+      domain: TEST_DOMAIN_B,
+      testURL: TEST_PAGE_B,
+      visible: true,
+      expected: "NoClick",
+    });
+
+    info(
+      "Set the domain preference of the top-level domain to MODE_REJECT_OR_ACCEPT"
+    );
+    let uri = Services.io.newURI(TEST_ORIGIN_C);
+    Services.cookieBanners.setDomainPref(
+      uri,
+      Ci.nsICookieBannerService.MODE_REJECT_OR_ACCEPT,
+      testPBM
+    );
+
+    info(
+      "Verify if domain preference takes precedence over then the pref setting for top-level example.net"
+    );
+    await openIframeAndVerify({
+      win: testWin,
+      domain: TEST_DOMAIN_B,
+      testURL: TEST_PAGE_B,
+      visible: false,
+      expected: "OptIn",
+    });
+
+    Services.cookieBanners.removeAllDomainPrefs(testPBM);
+
+    if (testPBM) {
+      await BrowserTestUtils.closeWindow(testWin);
+    }
+  }
 });
