380282429386acee6fd451cc5183db4c56378924
Lee Salzman <lsalzman@mozilla.com>
2022-10-14 20:06:20 +0000

    Bug 1793769 - Avoid recreating the main DrawTargetWebgl SharedContext. r=aosmond
    For canvas users that rapidly create and destroy canvases, we may end up creating
    a new SharedContext (and hence ClientWebGLContext) if there are no more canvases
    left between destruction and creation. To work around this, just keep alive the
    SharedContext for the main thread (other threads are unfortunately a bit tricky
    to support) so that canvas creation remains fast in this instance.
    
    Differential Revision: https://phabricator.services.mozilla.com/D158904

--- dom/canvas/DrawTargetWebgl.cpp
+++ dom/canvas/DrawTargetWebgl.cpp
@@ -7,6 +7,7 @@
 #include "DrawTargetWebglInternal.h"
 #include "SourceSurfaceWebgl.h"
 
+#include "mozilla/ClearOnShutdown.h"
 #include "mozilla/StaticPrefs_gfx.h"
 #include "mozilla/gfx/Blur.h"
 #include "mozilla/gfx/DrawTargetSkia.h"
@@ -247,6 +248,10 @@ DrawTargetWebgl::SharedContext::~SharedContext() {
   if (sSharedContext.init() && sSharedContext.get() == this) {
     sSharedContext.set(nullptr);
   }
+  // Detect context loss before deletion.
+  if (mWebgl) {
+    mWebgl->ActiveTexture(LOCAL_GL_TEXTURE0);
+  }
   ClearAllTextures();
   UnlinkSurfaceTextures();
   UnlinkGlyphCaches();
@@ -335,6 +340,8 @@ static Atomic<bool> sContextInitError(false);
 MOZ_THREAD_LOCAL(DrawTargetWebgl::SharedContext*)
 DrawTargetWebgl::sSharedContext;
 
+RefPtr<DrawTargetWebgl::SharedContext> DrawTargetWebgl::sMainSharedContext;
+
 // Try to initialize a new WebGL context. Verifies that the requested size does
 // not exceed the available texture limits and that shader creation succeeded.
 bool DrawTargetWebgl::Init(const IntSize& size, const SurfaceFormat format) {
@@ -357,6 +364,15 @@ bool DrawTargetWebgl::Init(const IntSize& size, const SurfaceFormat format) {
     }
 
     sSharedContext.set(mSharedContext.get());
+
+    if (NS_IsMainThread()) {
+      // Keep the shared context alive for the main thread by adding a ref.
+      // Ensure the ref will get cleared on shutdown so it doesn't leak.
+      if (!sMainSharedContext) {
+        ClearOnShutdown(&sMainSharedContext);
+      }
+      sMainSharedContext = mSharedContext;
+    }
   } else {
     mSharedContext = sharedContext;
   }
