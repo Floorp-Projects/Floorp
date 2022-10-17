1dd3c455a233ab7cd594ae2c22399041f7667821
Michael Froman <mfroman@mozilla.com>
2022-10-17 14:20:32 -0500

    Bug 1790097 - Vendor libwebrtc from 4f15246683
    Upstream commit: https://webrtc.googlesource.com/src/+/4f152466831caa261e9be043f9bc9dcb5e08c2a2
        dcsctp: Support lifecycle events in send queue
    
        The send queue is responsible for generating lifecycle events for all
        messages that are still in the queue. Because, if they are still in the
        queue, that means that the last fragment of the message hasn't been sent
        yet (because then it would have been in the retransmission queue
        instead). And if the last fragment hasn't been sent, the send queue is
        responsible for generating the
        `OnLifecycleMessageExpired(/*maybe_sent=*/false)` event.
    
        Bug: webrtc:5696
        Change-Id: Icd5956d6aa0f392cae54f2a05bd20728d9f7f0a6
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/264144
        Commit-Queue: Victor Boivie <boivie@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37419}

--- third_party/libwebrtc/net/dcsctp/tx/send_queue.h
+++ third_party/libwebrtc/net/dcsctp/tx/send_queue.h
@@ -34,6 +34,10 @@ class SendQueue {
     // Partial reliability - RFC3758
     MaxRetransmits max_retransmissions = MaxRetransmits::NoLimit();
     TimeMs expires_at = TimeMs::InfiniteFuture();
+
+    // Lifecycle - set for the last fragment, and `LifecycleId::NotSet()` for
+    // all other fragments.
+    LifecycleId lifecycle_id = LifecycleId::NotSet();
   };
 
   virtual ~SendQueue() = default;
