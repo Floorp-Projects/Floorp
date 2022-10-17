e41ffa1f2500b3dfc5c969272b9f38b044d16c3e
Michael Froman <mfroman@mozilla.com>
2022-10-17 15:47:30 -0500

    Bug 1790097 - Vendor libwebrtc from 11fdb08282
    Upstream commit: https://webrtc.googlesource.com/src/+/11fdb082828171240102d3827826c1704396c08e
        Implement RTCInboundRTPStreamStats.JitterBufferTargetDelay
    
        This CL also removes the existing non-standard implementation of the metric.
    
        Bug: webrtc:14147, webrtc:11789
        Change-Id: I70fd1c451dfd59380fe5ce959086f37b31697c16
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/265360
        Reviewed-by: Artem Titov <titovartem@webrtc.org>
        Commit-Queue: Ivo Creusen <ivoc@webrtc.org>
        Reviewed-by: Sam Zackrisson <saza@webrtc.org>
        Reviewed-by: Henrik Boström <hbos@webrtc.org>
        Reviewed-by: Ivo Creusen <ivoc@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37441}

--- third_party/libwebrtc/pc/rtc_stats_integrationtest.cc
+++ third_party/libwebrtc/pc/rtc_stats_integrationtest.cc
@@ -655,8 +655,6 @@ class RTCStatsReportVerifier {
       verifier.TestMemberIsUndefined(media_stream_track.interruption_count);
       verifier.TestMemberIsUndefined(
           media_stream_track.total_interruption_duration);
-      verifier.TestMemberIsUndefined(
-          media_stream_track.jitter_buffer_target_delay);
     } else {
       RTC_DCHECK_EQ(*media_stream_track.kind, RTCMediaStreamTrackKind::kAudio);
       // The type of the referenced media source depends on kind.
@@ -667,8 +665,6 @@ class RTCStatsReportVerifier {
             media_stream_track.jitter_buffer_delay);
         verifier.TestMemberIsNonNegative<uint64_t>(
             media_stream_track.jitter_buffer_emitted_count);
-        verifier.TestMemberIsNonNegative<double>(
-            media_stream_track.jitter_buffer_target_delay);
         verifier.TestMemberIsPositive<double>(media_stream_track.audio_level);
         verifier.TestMemberIsPositive<double>(
             media_stream_track.total_audio_energy);
@@ -703,8 +699,6 @@ class RTCStatsReportVerifier {
         verifier.TestMemberIsUndefined(media_stream_track.jitter_buffer_delay);
         verifier.TestMemberIsUndefined(
             media_stream_track.jitter_buffer_emitted_count);
-        verifier.TestMemberIsUndefined(
-            media_stream_track.jitter_buffer_target_delay);
         verifier.TestMemberIsUndefined(media_stream_track.audio_level);
         verifier.TestMemberIsUndefined(media_stream_track.total_audio_energy);
         verifier.TestMemberIsUndefined(
@@ -850,6 +844,7 @@ class RTCStatsReportVerifier {
           inbound_stream.inserted_samples_for_deceleration);
       verifier.TestMemberIsUndefined(
           inbound_stream.removed_samples_for_acceleration);
+      verifier.TestMemberIsUndefined(inbound_stream.jitter_buffer_target_delay);
       verifier.TestMemberIsUndefined(inbound_stream.audio_level);
       verifier.TestMemberIsUndefined(inbound_stream.total_audio_energy);
       verifier.TestMemberIsUndefined(inbound_stream.total_samples_duration);
@@ -873,6 +868,8 @@ class RTCStatsReportVerifier {
           inbound_stream.inserted_samples_for_deceleration);
       verifier.TestMemberIsNonNegative<uint64_t>(
           inbound_stream.removed_samples_for_acceleration);
+      verifier.TestMemberIsNonNegative<double>(
+          inbound_stream.jitter_buffer_target_delay);
       verifier.TestMemberIsPositive<double>(inbound_stream.audio_level);
       verifier.TestMemberIsPositive<double>(inbound_stream.total_audio_energy);
       verifier.TestMemberIsPositive<double>(
