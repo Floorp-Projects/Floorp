ee8f84fa37f953fbae5808404ce66d86c1f2360d
Jamie Nicol <jnicol@mozilla.com>
2022-10-17 18:52:01 +0000

    Bug 1795614 - Avoid invalidating render targets on Mali-G710. r=gfx-reviewers,jgilbert a=pascalc
    In bug 1787520 we discovered a driver bug affecting Mali-G78 devices,
    and added a workaround for it - avoiding invalidating no-longer-needed
    render targets at the end of each render pass. It now transpires this
    bug also affects Mali-G710 GPUs, so this patch applies the same
    workaround for those devices too.
    
    Differential Revision: https://phabricator.services.mozilla.com/D159515

--- gfx/wr/webrender/src/device/gl.rs
+++ gfx/wr/webrender/src/device/gl.rs
@@ -1793,7 +1793,7 @@ impl Device {
         // On Mali-G78 devices with a driver version v1.r36p0 we have seen that invalidating render
         // targets can result in image corruption, perhaps due to subsequent reuses of the render
         // target not correctly reinitializing them to a valid state. See bug 1787520.
-        if renderer_name.starts_with("Mali-G78") {
+        if renderer_name.starts_with("Mali-G78") || renderer_name.starts_with("Mali-G710") {
             match parse_mali_version(&version_string) {
                 Some(version) if version >= (1, 36, 0) => supports_render_target_invalidate = false,
                 _ => {}
