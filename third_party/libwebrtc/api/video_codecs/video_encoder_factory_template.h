f73af2c13e52c6f35980386330a8cfbe39fabfdf
Michael Froman <mfroman@mozilla.com>
2022-10-17 18:07:21 -0500

    Bug 1790097 - Vendor libwebrtc from 02bfcf5132
    Upstream commit: https://webrtc.googlesource.com/src/+/02bfcf513280188ac8fb2280bdb9c680f9c8cbfd
        Compare only SdpVideoFormat::name and SdpVideoFormat::parameters in the VideoEncoderFactoryTemplate.
    
        Since https://webrtc-review.googlesource.com/c/src/+/267780 supported scalability modes are also used to compare for equality between SdpVideoFormats(?).
    
        Bug: webrtc:14267, webrtc:13573
        Change-Id: I2f3c2fca93bac6fadd222f776f672c9bd3f1de0a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/268304
        Reviewed-by: Tomas Gunnarsson <tommi@webrtc.org>
        Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37510}

--- third_party/libwebrtc/api/video_codecs/video_encoder_factory_template.h
+++ third_party/libwebrtc/api/video_codecs/video_encoder_factory_template.h
@@ -16,6 +16,7 @@
 #include <vector>
 
 #include "absl/algorithm/container.h"
+#include "api/array_view.h"
 #include "api/video_codecs/video_encoder.h"
 #include "api/video_codecs/video_encoder_factory.h"
 #include "modules/video_coding/svc/scalability_mode_util.h"
@@ -62,9 +63,14 @@ class VideoEncoderFactoryTemplate : public VideoEncoderFactory {
   }
 
  private:
-  template <typename V>
-  bool IsFormatSupported(const SdpVideoFormat& format) const {
-    return absl::c_count(V::SupportedFormats(), format) > 0;
+  bool IsFormatInList(
+      const SdpVideoFormat& format,
+      rtc::ArrayView<const SdpVideoFormat> supported_formats) const {
+    return absl::c_any_of(
+        supported_formats, [&](const SdpVideoFormat& supported_format) {
+          return supported_format.name == format.name &&
+                 supported_format.parameters == format.parameters;
+        });
   }
 
   template <typename V>
@@ -83,7 +89,7 @@ class VideoEncoderFactoryTemplate : public VideoEncoderFactory {
   void GetSupportedFormatsInternal(std::vector<SdpVideoFormat>& formats) const {
     auto supported_formats = V::SupportedFormats();
     for (const auto& format : supported_formats) {
-      if (absl::c_count(formats, format) == 0) {
+      if (!IsFormatInList(format, formats)) {
         formats.push_back(format);
       }
     }
@@ -96,7 +102,7 @@ class VideoEncoderFactoryTemplate : public VideoEncoderFactory {
   template <typename V, typename... Vs>
   std::unique_ptr<VideoEncoder> CreateVideoEncoderInternal(
       const SdpVideoFormat& format) {
-    if (IsFormatSupported<V>(format)) {
+    if (IsFormatInList(format, V::SupportedFormats())) {
       return V::CreateEncoder(format);
     }
 
@@ -111,7 +117,7 @@ class VideoEncoderFactoryTemplate : public VideoEncoderFactory {
   CodecSupport QueryCodecSupportInternal(
       const SdpVideoFormat& format,
       const absl::optional<std::string>& scalability_mode) const {
-    if (IsFormatSupported<V>(format)) {
+    if (IsFormatInList(format, V::SupportedFormats())) {
       return {.is_supported = IsScalabilityModeSupported<V>(scalability_mode)};
     }
 
