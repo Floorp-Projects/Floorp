299ce96ba4305623dc545278b832bb7af273247f
Emilio Cobos Álvarez <emilio@crisal.io>
2022-10-17 20:59:59 +0000

    Bug 1795260 - Test that the searchbar doesn't overflow the toolbar even with a big user-imposed size. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D159521

---
+++ browser/components/customizableui/test/browser_1795260_searchbar_overflow_toolbar.js
@@ -0,0 +1,24 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const WIDGET_ID = "search-container";
+
+registerCleanupFunction(() => CustomizableUI.reset());
+
+add_task(async function test_syncPreferenceWithWidget() {
+  // Move the searchbar to the nav bar.
+  CustomizableUI.addWidgetToArea(WIDGET_ID, CustomizableUI.AREA_NAVBAR);
+
+  let container = document.getElementById(WIDGET_ID);
+  // Set a disproportionately large width, which could be from a saved bigger
+  // window, or what not.
+  container.style.width = window.innerWidth * 2 + "px";
+
+  // Stuff shouldn't overflow.
+  ok(
+    container.getBoundingClientRect().width < window.innerWidth,
+    "Searchbar shouldn't overflow"
+  );
+});
