66636645b5cb7da3a516047d5184c3db87fe22de
Evan <eliang@calstatela.edu>
2022-10-17 17:22:38 +0000

    Bug 1790227 -Coverted DecoderDoctor JSM modules to ESM modules. r=NeilDeakin,media-playback-reviewers,chunmin.
    Differential Revision: https://phabricator.services.mozilla.com/D158406

--- dom/media/doctor/DecoderDoctorDiagnostics.cpp
+++ dom/media/doctor/DecoderDoctorDiagnostics.cpp
@@ -242,8 +242,8 @@ enum class ReportParam : uint8_t {
 };
 
 struct NotificationAndReportStringId {
-  // Notification type, handled by DecoderDoctorChild.jsm and
-  // DecoderDoctorParent.jsm.
+  // Notification type, handled by DecoderDoctorChild.sys.mjs and
+  // DecoderDoctorParent.sys.mjs.
   dom::DecoderDoctorNotificationType mNotificationType;
   // Console message id. Key in dom/locales/.../chrome/dom/dom.properties.
   const char* mReportStringId;
