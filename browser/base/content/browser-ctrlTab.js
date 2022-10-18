58bf0f428ff7bb71494962b6feb0f8c883579663
Emilio Cobos Álvarez <emilio@crisal.io>
2022-10-18 14:53:59 +0000

    Bug 1795890 - Fix Ctrl+Tab labels to not overflow. r=dao
    Make them use CSS text-overflow rather than XUL <label crop>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D159597

--- browser/base/content/browser-ctrlTab.js
+++ browser/base/content/browser-ctrlTab.js
@@ -240,7 +240,6 @@ var ctrlTab = {
 
     let label = (preview._label = document.createXULElement("label"));
     label.className = "ctrlTab-label plain";
-    label.setAttribute("crop", "end");
     previewInner.appendChild(label);
 
     return preview;
@@ -281,7 +280,7 @@ var ctrlTab = {
       canvas.style.maxHeight = canvasHeight + "px";
       canvas.appendChild(tabPreviews.get(aTab));
 
-      aPreview._label.setAttribute("value", aTab.label);
+      aPreview._label.textContent = aTab.label;
       aPreview.setAttribute("tooltiptext", aTab.label);
       if (aTab.image) {
         aPreview._favicon.setAttribute("src", aTab.image);
@@ -291,7 +290,7 @@ var ctrlTab = {
       aPreview.hidden = false;
     } else {
       aPreview.hidden = true;
-      aPreview._label.removeAttribute("value");
+      aPreview._label.textContent = "";
       aPreview.removeAttribute("tooltiptext");
       aPreview._favicon.removeAttribute("src");
     }
