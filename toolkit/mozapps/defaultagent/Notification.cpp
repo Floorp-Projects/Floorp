6508fd4311f27e6b98066eaed7f34b719c9b5734
Kirk Steuber <ksteuber@mozilla.com>
2022-10-18 18:23:00 +0000

    Bug 1795937 - Backed out changeset a22f5ae169a4 (Bug 1776069) r=nalexander
    Depends on D159619
    
    Differential Revision: https://phabricator.services.mozilla.com/D159620

--- toolkit/mozapps/defaultagent/Notification.cpp
+++ toolkit/mozapps/defaultagent/Notification.cpp
@@ -99,24 +99,6 @@ static bool GetPrefSetDefaultBrowserUserChoice() {
       .valueOr(false);
 }
 
-static bool SetNeverShowNotificationAgain() {
-  // Unprefixed because, if someone asks not to be shown the message again, they
-  // don't want to say that once per installation.
-  return !RegistrySetValueBool(IsPrefixed::Unprefixed,
-                               L"NeverShowNotificationAgain", true)
-              .isErr();
-}
-
-static bool GetNeverShowNotificationAgain() {
-  // We only ever store a true value, so no value stored is equivalent to false.
-  // But, on error, err on the side of caution rather than potentially showing
-  // a message to someone who asked never to see on again.
-  return RegistryGetValueBool(IsPrefixed::Unprefixed,
-                              L"NeverShowNotificationAgain")
-      .unwrapOr(mozilla::Some(true))
-      .valueOr(false);
-}
-
 struct ToastStrings {
   mozilla::UniquePtr<wchar_t[]> text1;
   mozilla::UniquePtr<wchar_t[]> text2;
@@ -397,9 +379,6 @@ class ToastHandler : public WinToastLib::IWinToastHandler {
         // SetFollowupNotificationRequestTime, there will be no followup
         // notification.
         activitiesPerformed.action = NotificationAction::DismissedByButton;
-        if (!mIsLocalizedNotification) {
-          SetNeverShowNotificationAgain();
-        }
       }
     } else if ((actionIndex == 1 && !mIsLocalizedNotification) ||
                (actionIndex == 0 && mIsLocalizedNotification)) {
@@ -633,11 +612,6 @@ NotificationActivities MaybeShowNotification(
   if (!mozilla::IsWin10OrLater()) {
     // Notifications aren't shown in versions prior to Windows 10 because the
     // notification API we want isn't available.
-    return activitiesPerformed;
-  }
-
-  bool neverShowNotificationAgain = GetNeverShowNotificationAgain();
-  if (neverShowNotificationAgain) {
     return activitiesPerformed;
   }
 
