d8658dc7dd0e209e344cfb33b36a62f9d5bdceba
William Durand <will+git@drnd.me>
2022-10-18 08:06:08 +0000

    Bug 1793925 - Warn about complex versioning formats used in manifest. r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D158834

--- testing/profiles/valgrind/user.js
+++ testing/profiles/valgrind/user.js
@@ -2,10 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Preferences file for profileserver.
+// Preferences file for valgrind.
 /* globals user_pref */
-// Turn off budget throttling for the profile server
-user_pref("dom.timeout.enable_budget_timer_throttling", false);
 // TODO: Bug 1795750 - Re-enable this pref when we have a new version of the
 // Quitter XPI with a simpler version format.
 user_pref("extensions.webextensions.warnings-as-errors", false);
