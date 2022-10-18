5a52df916d70ca8e859221cfc04439511621af88
Dana Keeler <dkeeler@mozilla.com>
2022-10-18 00:18:08 +0000

    Bug 1793841 - don't serialize securityInfo in browser_aboutCertError_exception.js r=pbz
    This test only needs the bytes of the server certificate, not the entire
    nsITransportSecurityInfo of the connection.
    
    Differential Revision: https://phabricator.services.mozilla.com/D157994

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -294,10 +294,6 @@ XPCOMUtils.defineLazyServiceGetters(this, {
   ],
   Favicons: ["@mozilla.org/browser/favicon-service;1", "nsIFaviconService"],
   gDNSService: ["@mozilla.org/network/dns-service;1", "nsIDNSService"],
-  gSerializationHelper: [
-    "@mozilla.org/network/serialization-helper;1",
-    "nsISerializationHelper",
-  ],
   WindowsUIUtils: ["@mozilla.org/windows-ui-utils;1", "nsIWindowsUIUtils"],
   BrowserHandler: ["@mozilla.org/browser/clh;1", "nsIBrowserHandler"],
 });
@@ -3641,19 +3637,6 @@ function BrowserReloadWithFlags(reloadFlags) {
       "BrowserTab"
     );
   }
-}
-
-function getSecurityInfo(securityInfoAsString) {
-  if (!securityInfoAsString) {
-    return null;
-  }
-
-  let securityInfo = gSerializationHelper.deserializeObject(
-    securityInfoAsString
-  );
-  securityInfo.QueryInterface(Ci.nsITransportSecurityInfo);
-
-  return securityInfo;
 }
 
 // TODO: can we pull getPEMString in from pippki.js instead of
