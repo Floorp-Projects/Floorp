25fe6781f21954b06572e02ea395f8683419d672
Sean Feng <sefeng@mozilla.com>
2022-10-19 13:30:59 +0000

    Bug 1783497 - Don't throw if chrome URL canonicalization fails r=smaug
    Looks like this is not needed and making a web-compat issue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D156985

--- chrome/nsChromeProtocolHandler.cpp
+++ chrome/nsChromeProtocolHandler.cpp
@@ -82,7 +82,7 @@ nsChromeProtocolHandler::GetProtocolFlags(uint32_t* result) {
   // and "chrome://navigator/content/navigator.xul".
 
   rv = nsChromeRegistry::Canonify(surl);
-  if (NS_FAILED(rv)) return rv;
+  mozilla::Unused << NS_WARN_IF(NS_FAILED(rv));
 
   surl.forget(result);
   return NS_OK;
@@ -98,21 +98,9 @@ nsChromeProtocolHandler::NewChannel(nsIURI* aURI, nsILoadInfo* aLoadInfo,
 
   MOZ_ASSERT(aResult, "Null out param");
 
-#ifdef DEBUG
-  // Check that the uri we got is already canonified
-  nsresult debug_rv;
   nsCOMPtr<nsIURI> debugURL = aURI;
-  debug_rv = nsChromeRegistry::Canonify(debugURL);
-  if (NS_SUCCEEDED(debug_rv)) {
-    bool same;
-    debug_rv = aURI->Equals(debugURL, &same);
-    if (NS_SUCCEEDED(debug_rv)) {
-      NS_ASSERTION(same,
-                   "Non-canonified chrome uri passed to "
-                   "nsChromeProtocolHandler::NewChannel!");
-    }
-  }
-#endif
+  rv = nsChromeRegistry::Canonify(debugURL);
+  NS_ENSURE_SUCCESS(rv, rv);
 
   nsCOMPtr<nsIChannel> result;
 
---
+++ chrome/test/unit/test_create_channel_chrome_url.js
@@ -0,0 +1,36 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+"use strict";
+
+const { NetUtil } = ChromeUtils.import("resource://gre/modules/NetUtil.jsm");
+
+function testURL(url) {
+  Services.io.newChannelFromURI(
+    NetUtil.newURI(url),
+    null, // aLoadingNode
+    Services.scriptSecurityManager.getSystemPrincipal(),
+    null, // aTriggeringPrincipal
+    Ci.nsILoadInfo.SEC_ALLOW_CROSS_ORIGIN_SEC_CONTEXT_IS_NULL,
+    Ci.nsIContentPolicy.TYPE_OTHER
+  );
+}
+
+add_task(async function test_create_channel_with_chrome_url() {
+  try {
+    testURL("chrome://path");
+    Assert.ok(false);
+  } catch (e) {
+    // Chrome url fails canonicalization
+    Assert.equal(e.result, Cr.NS_ERROR_FAILURE);
+  }
+
+  try {
+    testURL("chrome://path/path/path");
+    Assert.ok(false);
+  } catch (e) {
+    // Chrome url passes canonicalization
+    Assert.equal(e.result, Cr.NS_ERROR_FILE_NOT_FOUND);
+  }
+});
