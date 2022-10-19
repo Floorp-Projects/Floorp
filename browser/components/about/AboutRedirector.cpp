21affe65f8daceec916e5640a4c8f3ad19c391c2
surapunoyousei <ryosukeusuusu@gmail.com>
2022-10-19 17:35:00 +0900

    [FB] Fix  | Fix some issues

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -50,7 +50,7 @@ static const RedirEntry kRedirMap[] = {
          nsIAboutModule::URI_CAN_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
          nsIAboutModule::HIDE_FROM_ABOUTABOUT},
      // Floorp Contributor
-    {"contributors", "chrome://browser/skin/contributor.html",
+    {"contributors", "chrome://browser/skin/contributors.html",
      nsIAboutModule::ALLOW_SCRIPT | nsIAboutModule::URI_MUST_LOAD_IN_CHILD |
          nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS |
          nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
