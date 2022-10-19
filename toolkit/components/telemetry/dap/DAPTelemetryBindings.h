24ef71bd2cc82c94c89f88b11176cb80f91b0782
Simon Friedberger <simon@mozilla.com>
2022-10-19 21:42:43 +0000

    Bug 1791018 - Add DAP FFI layer. r=mt,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D157477

---
+++ toolkit/components/telemetry/dap/DAPTelemetryBindings.h
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef DAPTelemetryBindings_h
+#define DAPTelemetryBindings_h
+
+#include "DAPTelemetry.h"
+#include "mozilla/dap_ffi_generated.h"
+
+#endif  // DAPTelemetryBindings_h
