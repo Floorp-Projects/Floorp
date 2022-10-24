b69cf1793e84e1738f9b2496b364874b214bab97
Eitan Isaacson <eitan@monotonous.org>
2022-10-24 16:03:06 +0000

    Bug 1796876 - Allow selected option and list to be null. r=morgan
    HTMLComboboxListAccessible::SelectedOption can return null, so we need
    to prepare for that. Same with HTMLComboboxListAccessible::List when
    the combo box's child is removed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D159961

--- accessible/generic/LocalAccessible.cpp
+++ accessible/generic/LocalAccessible.cpp
@@ -3258,13 +3258,12 @@ already_AddRefed<AccAttributes> LocalAccessible::BundleFieldsForCache(
               static_cast<HTMLComboboxAccessible*>(acc);
           HTMLComboboxListAccessible* list = combobox->List();
           LocalAccessible* currItem = combobox->SelectedOption();
-          MOZ_ASSERT(list && currItem);
           // Preserve hittesting order by adding the item, then
           // the list, and finally the combobox itself.
-          if (inViewAccs.EnsureInserted(currItem)) {
+          if (currItem && inViewAccs.EnsureInserted(currItem)) {
             viewportCache.AppendElement(currItem->ID());
           }
-          if (inViewAccs.EnsureInserted(list)) {
+          if (list && inViewAccs.EnsureInserted(list)) {
             viewportCache.AppendElement(list->ID());
           }
         }
