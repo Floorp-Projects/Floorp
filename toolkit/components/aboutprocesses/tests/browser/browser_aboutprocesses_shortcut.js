6c84b0850a85f238593edfbd0301458c8e527837
Florian Quèze <florian@queze.net>
2022-10-24 14:42:27 +0000

    Bug 1796096 - Add a shift+esc keyboard shortcut to quickly open about:processes, r=mconley.
    Differential Revision: https://phabricator.services.mozilla.com/D159688

---
+++ toolkit/components/aboutprocesses/tests/browser/browser_aboutprocesses_shortcut.js
@@ -0,0 +1,16 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+add_task(async function() {
+  // Wait for the browser to be ready to receive keyboard events.
+  if (!gBrowser.selectedBrowser.hasLayers) {
+    await BrowserTestUtils.waitForEvent(window, "MozLayerTreeReady");
+  }
+
+  EventUtils.synthesizeKey("KEY_Escape", { shiftKey: true });
+
+  await BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
+  Assert.equal(gBrowser.selectedBrowser.currentURI.spec, "about:processes");
+});
