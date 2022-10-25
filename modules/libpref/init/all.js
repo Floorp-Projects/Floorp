84e7faf97d19743f2ba4210182637f5ed044ee73
Masayuki Nakano <masayuki@d-toybox.com>
2022-10-25 07:06:54 +0000

    Bug 1796044 - part 1: Make `intl.ime.hack.set_input_scope_of_url_bar_to_default` a static pref r=m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D159804

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2538,30 +2538,6 @@ pref("font.size.monospace.x-math", 13);
   // Switch the keyboard layout per window
   pref("intl.keyboard.per_window_layout", false);
 
-  // Whether Gecko sets input scope of the URL bar to IS_DEFAULT when black
-  // listed IMEs are active.  If you use tablet mode mainly and you want to
-  // use touch keyboard for URL when you set focus to the URL bar, you can
-  // set this to false.  Then, you'll see, e.g., ".com" key on the keyboard.
-  // However, if you set this to false, such IMEs set its open state to "closed"
-  // when you set focus to the URL bar.  I.e., input mode is automatically
-  // changed to English input mode.
-  // Black listed IMEs:
-  //   - Microsoft IME for Japanese
-  //   - Google Japanese Input
-  //   - Microsoft Bopomofo
-  //   - Microsoft ChangJie
-  //   - Microsoft Phonetic
-  //   - Microsoft Quick
-  //   - Microsoft New ChangJie
-  //   - Microsoft New Phonetic
-  //   - Microsoft New Quick
-  //   - Microsoft Pinyin
-  //   - Microsoft Pinyin New Experience Input Style
-  //   - Microsoft Wubi
-  //   - Microsoft IME for Korean (except on Win7)
-  //   - Microsoft Old Hangul
-  pref("intl.ime.hack.set_input_scope_of_url_bar_to_default", true);
-
   // Enable/Disable TSF support.
   pref("intl.tsf.enable", true);
 
