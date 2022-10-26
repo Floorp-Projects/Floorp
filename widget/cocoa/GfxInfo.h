990a6a6bb4ed45f7be4c8ee66d29890eebfcbb53
Andrew Osmond <aosmond@mozilla.com>
2022-10-26 20:29:13 +0000

    Bug 1797580 - Fix broken assumptions with allowlists and the downloadable blocklist. r=jrmuizel
    Ensure that when we evaluate the downloadable blocklist, we actually
    only use the downloadable blocklist. We should not include any platform
    specific checks in these prefs, as it causes confusion about why the
    prefs were set in the first place. Allowlisted features should be
    ignored when evaluating the downloadable blocklist; if we wish to
    override the ALLOW/ALLOW_QUALIFIED/DENIED statuses, we should use OK or
    BLOCKED_DEVICE or similar instead.
    
    This caused allowlisted features (like WebRender) to be taken away from
    users in the most recent nightly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D160408

--- widget/cocoa/GfxInfo.h
+++ widget/cocoa/GfxInfo.h
@@ -67,6 +67,7 @@ class GfxInfo : public GfxInfoBase {
  protected:
   virtual ~GfxInfo() {}
 
+  OperatingSystem GetOperatingSystem() override;
   virtual nsresult GetFeatureStatusImpl(
       int32_t aFeature, int32_t* aStatus, nsAString& aSuggestedDriverVersion,
       const nsTArray<GfxDriverInfo>& aDriverInfo, nsACString& aFailureId,
