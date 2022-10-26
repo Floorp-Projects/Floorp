662f6b04afb95279bade5249c424702ea45ae276
serge-sans-paille <sguelton@mozilla.com>
2022-10-26 06:16:43 +0000

    Bug 1796839 - Remove python2 dependency r=glandium
    gn no longer requires python2, we should be fine with python3 here.
    
    Differential Revision: https://phabricator.services.mozilla.com/D159956

--- taskcluster/scripts/misc/build-gn-linux.sh
+++ taskcluster/scripts/misc/build-gn-linux.sh
@@ -8,13 +8,6 @@ export CC=gcc
 export CXX=g++
 export LDFLAGS=-lrt
 
-# Gn build scripts use #!/usr/bin/env python, which will be python 2.6 on
-# the worker and cause failures. Work around this by putting python2.7
-# ahead of it in PATH.
-mkdir -p $WORKSPACE/python_bin
-ln -s /usr/bin/python2.7 $WORKSPACE/python_bin/python
-export PATH=$WORKSPACE/python_bin:$PATH
-
 cd $GECKO_PATH
 
 . taskcluster/scripts/misc/build-gn-common.sh
