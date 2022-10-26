250e27cadbfc40384061371aa24e4f303f88ecc8
Kirk Steuber <ksteuber@mozilla.com>
2022-10-26 07:44:32 +0000

    Bug 1727820 - Change consumers and implementation of AUS.downloadUpdate r=bhearsum
    This patch updates all consumers of nsIApplicationUpdateService.downloadUpdate to accomodate the changes made to that API earlier in the stack.
    
    It also updates the implementation to match that API.
    
    Differential Revision: https://phabricator.services.mozilla.com/D159296

--- toolkit/mozapps/update/tests/unit_aus_update/languagePackUpdates.js
+++ toolkit/mozapps/update/tests/unit_aus_update/languagePackUpdates.js
@@ -265,7 +265,7 @@ add_task(async function testRedownload() {
   gAUS.addDownloadListener(listener);
 
   let bestUpdate = gAUS.selectUpdate(updates);
-  gAUS.downloadUpdate(bestUpdate, false);
+  await gAUS.downloadUpdate(bestUpdate, false);
 
   await waitForEvent("update-downloaded");
 
