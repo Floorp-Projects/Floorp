a26edb7f0f77d1281101e53a628a61702cea3d7c
Dan Mosedale <dmosedale@mozillla.com>
2022-10-26 21:22:58 +0000

    Bug 1797553 - Add fxms-message-1 to default FxMS features, r=negin, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D160402

--- browser/components/newtab/test/browser/browser_asrouter_experimentsAPILoader.js
+++ browser/components/newtab/test/browser/browser_asrouter_experimentsAPILoader.js
@@ -116,12 +116,12 @@ const MESSAGE_CONTENT = {
   targeting: "true",
 };
 
-const getCFRExperiment = async () => {
+const getExperiment = async feature => {
   let recipe = ExperimentFakes.recipe(
     // In tests by default studies/experiments are turned off. We turn them on
     // to run the test and rollback at the end. Cleanup causes unenrollment so
     // for cases where the test runs multiple times we need unique ids.
-    `test_xman_cfr_${Date.now()}`,
+    `test_xman_${feature}_${Date.now()}`,
     {
       id: "xman_test_message",
       bucketConfig: {
@@ -133,13 +133,17 @@ const getCFRExperiment = async () => {
       },
     }
   );
-  recipe.branches[0].features[0].featureId = "cfr";
+  recipe.branches[0].features[0].featureId = feature;
   recipe.branches[0].features[0].value = MESSAGE_CONTENT;
-  recipe.branches[1].features[0].featureId = "cfr";
+  recipe.branches[1].features[0].featureId = feature;
   recipe.branches[1].features[0].value = MESSAGE_CONTENT;
-  recipe.featureIds = ["cfr"];
+  recipe.featureIds = [feature];
   await ExperimentTestUtils.validateExperiment(recipe);
   return recipe;
+};
+
+const getCFRExperiment = async () => {
+  return getExperiment("cfr");
 };
 
 const getLegacyCFRExperiment = async () => {
@@ -207,6 +211,29 @@ add_task(async function test_loading_experimentsAPI() {
   Assert.ok(
     telemetryFeedInstance.isInCFRCohort,
     "Telemetry should return true"
+  );
+
+  // Reload the provider
+  await ASRouter._updateMessageProviders();
+  // Wait to load the messages from the messaging-experiments provider
+  await ASRouter.loadMessagesFromAllProviders();
+
+  Assert.ok(
+    ASRouter.state.messages.find(m => m.id === "xman_test_message"),
+    "Experiment message found in ASRouter state"
+  );
+
+  await cleanup();
+});
+
+add_task(async function test_loading_fxms_message_1_feature() {
+  let experiment = await getExperiment("fxms-message-1");
+  await setup(experiment);
+  // Fetch the new recipe from RS
+  await RemoteSettingsExperimentLoader.updateRecipes();
+  await BrowserTestUtils.waitForCondition(
+    () => ExperimentAPI.getExperiment({ featureId: "fxms-message-1" }),
+    "ExperimentAPI should return an experiment"
   );
 
   // Reload the provider
