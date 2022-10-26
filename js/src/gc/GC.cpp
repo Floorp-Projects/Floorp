17aa456919b48c74d7533c2c8a963edcb72a1ab3
Jon Coppeard <jcoppeard@mozilla.com>
2022-10-26 11:09:39 +0000

    Bug 1796901 - Clear realm incremental marking state at the start of GC rather than at the end r=jandem, a=dmeehan
    GC can be aborted in several states and zones GCRuntime::finishCollection
    doesn't always get called. It's easier to clear this state in a signle place at
    the start.
    
    Differential Revision: https://phabricator.services.mozilla.com/D160056

--- js/src/gc/GC.cpp
+++ js/src/gc/GC.cpp
@@ -2721,6 +2721,10 @@ void GCRuntime::beginMarkPhase(AutoGCSession& session) {
     // the collecting arena lists.
     zone->arenas.mergeArenasFromCollectingLists();
     zone->arenas.moveArenasToCollectingLists();
+
+    for (RealmsInZoneIter realm(zone); !realm.done(); realm.next()) {
+      realm->clearAllocatedDuringGC();
+    }
   }
 
   if (rt->isBeingDestroyed()) {
@@ -2851,9 +2855,6 @@ void GCRuntime::finishCollection() {
   for (GCZonesIter zone(this); !zone.done(); zone.next()) {
     zone->changeGCState(Zone::Finished, Zone::NoGC);
     zone->notifyObservingDebuggers();
-    for (RealmsInZoneIter realm(zone); !realm.done(); realm.next()) {
-      realm->clearAllocatedDuringGC();
-    }
   }
 
 #ifdef JS_GC_ZEAL
