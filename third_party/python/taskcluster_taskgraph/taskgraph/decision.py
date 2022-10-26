7a7f5a170129ae9bbeca290657cd8535fc0e6acc
Mike Hommey <mh+mozilla@glandium.org>
2022-10-26 09:15:03 +0000

    Bug 1797406 - Ignore DONTBUILD on toolchains project branch. r=taskgraph-reviewers,jcristau
    The workflow for the toolchains project branch is that it's going to be
    synchronized with mozilla-central, and each push is expected to trigger
    new builds of the current (at the time of the push) clang upstream
    trunk. If the tip of mozilla-central happens to have DONTBUILD, we still
    want everything to start up.
    
    Differential Revision: https://phabricator.services.mozilla.com/D160314

--- third_party/python/taskcluster_taskgraph/taskgraph/decision.py
+++ third_party/python/taskcluster_taskgraph/taskgraph/decision.py
@@ -224,8 +224,13 @@ def get_decision_parameters(graph_config, options):
 
     # ..but can be overridden by the commit message: if it contains the special
     # string "DONTBUILD" and this is an on-push decision task, then use the
-    # special 'nothing' target task method.
-    if "DONTBUILD" in commit_message and options["tasks_for"] == "hg-push":
+    # special 'nothing' target task method. (except on the toolchains project,
+    # where we ignore "DONTBUILD").
+    if (
+        "DONTBUILD" in commit_message
+        and options["tasks_for"] == "hg-push"
+        and project != "toolchains"
+    ):
         parameters["target_tasks_method"] = "nothing"
 
     if options.get("optimize_target_tasks") is not None:
