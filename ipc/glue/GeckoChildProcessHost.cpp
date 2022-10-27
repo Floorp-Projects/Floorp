6518fa7259f53ac252c456de6b0a8f26db0f0d35
Jed Davis <jld@mozilla.com>
2022-10-27 22:26:07 +0000

    Bug 1797391 - Avoid process handle use-after-close in GeckoChildProcessHost dtor. r=nika
    ProcessWatcher takes ownership of the handle and may close it
    immediately if the process has already exited, so that needs to
    happen last; currently, because GetProcessId returns 0 for errors,
    DeregisterChildCrashAnnotationFileDescriptor will be passed 0 in that
    case, and will silently fail and leak resources.
    
    Differential Revision: https://phabricator.services.mozilla.com/D160295

--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -450,17 +450,6 @@ GeckoChildProcessHost::~GeckoChildProcessHost()
 
   MOZ_COUNT_DTOR(GeckoChildProcessHost);
 
-  if (mChildProcessHandle != 0) {
-    ProcessWatcher::EnsureProcessTerminated(
-        mChildProcessHandle
-#ifdef NS_FREE_PERMANENT_DATA
-        // If we're doing leak logging, shutdown can be slow.
-        ,
-        false  // don't "force"
-#endif
-    );
-  }
-
 #if defined(MOZ_WIDGET_COCOA)
   if (mChildTask != MACH_PORT_NULL)
     mach_port_deallocate(mach_task_self(), mChildTask);
@@ -474,7 +463,18 @@ GeckoChildProcessHost::~GeckoChildProcessHost()
     CrashReporter::DeregisterChildCrashAnnotationFileDescriptor(
         mChildProcessHandle);
 #endif
+
+    ProcessWatcher::EnsureProcessTerminated(
+        mChildProcessHandle
+#ifdef NS_FREE_PERMANENT_DATA
+        // If we're doing leak logging, shutdown can be slow.
+        ,
+        false  // don't "force"
+#endif
+    );
+    mChildProcessHandle = 0;
   }
+
 #if defined(MOZ_SANDBOX) && defined(XP_WIN)
   if (mSandboxBroker) {
     mSandboxBroker->Shutdown();
