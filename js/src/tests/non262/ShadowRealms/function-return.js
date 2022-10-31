ab56330d9212d88d03a37808b3d5b93b308cc7e6
André Bargull <andre.bargull@gmail.com>
2022-10-31 08:52:35 +0000

    Bug 1797750 - Part 8: Don't attempt to use the current name property for the toString output. r=mgaudet
    The previous code assumed that the "name" property is always a string, but
    user-code can redefine it to any value, which could easily lead to an exploit.
    
    Furthermore the spec requires that the computed string matches the `NativeFunction`
    production, so even if we only append the "name" when it's a string value, we'd
    first have to parse the string as an identifier to determine if the string has to
    be escaped or not. This is non-trivial, so instead follow the `toString` output
    of bound functions and return an unnamed function source code string.
    
    Depends on D160510
    
    Differential Revision: https://phabricator.services.mozilla.com/D160511

--- js/src/tests/non262/ShadowRealms/function-return.js
+++ js/src/tests/non262/ShadowRealms/function-return.js
@@ -1,21 +1,73 @@
 // |reftest| shell-option(--enable-shadow-realms) skip-if(!xulRuntime.shell)
 
+// The output of Function.prototype.toString must match the |NativeFunction| production.
+// https://tc39.es/ecma262/#sec-function.prototype.tostring
+//
+// NativeFunction :
+//   function NativeFunctionAccessor? PropertyName[~Yield, ~Await]? ( FormalParameters[~Yield, ~Await] ) { [ native code ] }
+//
+// NativeFunctionAccessor :
+//   get
+//   set
+
+function assertMatchesNativeFunction(f) {
+  var source = f.toString();
+
+  // Starts with "function".
+  assertEq(/^function\b/.test(source), true);
+
+  // Remove the optional |NativeFunctionAccessor| part.
+  var nativeAccesorRe = /^(?<start>\s*function)(?<accessor>\s+[gs]et)(?<end>\s+[^(].*)$/;
+  var match = nativeAccesorRe.exec(source);
+  if (match) {
+    source = match.groups.start + match.groups.end;
+  }
+
+  // The body must include the string "[native code".
+  var closeCurly = source.lastIndexOf("}");
+  var openCurly = source.lastIndexOf("{");
+  assertEq(openCurly < closeCurly, true);
+
+  var body = source.slice(openCurly + 1, closeCurly);
+  assertEq(/^\s*\[native code\]\s*$/.test(body), true);
+
+  // Verify |PropertyName| and |FormalParameters| are syntactically correct by parsing the source
+  // code. But we first need to replace the "[native code]" substring.
+  source = source.slice(0, openCurly) + "{}";
+
+  // Also prepend "void" to parse the function source code as a FunctionExpression, because we
+  // don't necessarily have a name part.
+  source = "void " + source;
+
+  try {
+    Function(source);
+  } catch {
+    assertEq(true, false, `${source} doesn't match NativeFunction`);
+  }
+}
+
 let sr = new ShadowRealm();
 var f = sr.evaluate("function f() { }; f");
 
-assertEq(/native code/.test(f.toString()), true)
-assertEq(/function f/.test(f.toString()), true)
+assertMatchesNativeFunction(f);
 
 f.name = "koala"
-
-assertEq(/function koala/.test(f.toString()), false)
+assertMatchesNativeFunction(f);
 
 Object.defineProperty(f, "name", { writable: true, value: "koala" });
-assertEq(/function koala/.test(f.toString()), true)
+assertMatchesNativeFunction(f);
 
 f.name = "panda"
-assertEq(/function panda/.test(f.toString()), true)
+assertMatchesNativeFunction(f);
 
+f.name = "has whitespace, therefore shouldn't match the PropertyName production";
+assertMatchesNativeFunction(f);
+
+f.name = 123;
+assertMatchesNativeFunction(f);
+
+Object.defineProperty(f, "name", { get() { throw new Error("unexpected side-effect"); } });
+assertMatchesNativeFunction(f);
 
 if (typeof reportCompare === 'function')
     reportCompare(true, true);
