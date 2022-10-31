6431dc4bfad3a3895c90748ae7147795f762441a
Stanca Serban <sstanca@mozilla.com>
2022-10-31 11:35:31 +0200

    Backed out 11 changesets (bug 1797750) for causing spidermonkey failures on WrappedFunctionObject.cpp. CLOSED TREE
    Backed out changeset 6d7bcd4a08bb (bug 1797750)
    Backed out changeset c93b57fe1b66 (bug 1797750)
    Backed out changeset 4e551959f63a (bug 1797750)
    Backed out changeset 55b2f301b941 (bug 1797750)
    Backed out changeset 8787060a075a (bug 1797750)
    Backed out changeset 9c02299f0d56 (bug 1797750)
    Backed out changeset 544c58f111c2 (bug 1797750)
    Backed out changeset 947432f9bd0d (bug 1797750)
    Backed out changeset bec9fda2f8ce (bug 1797750)
    Backed out changeset 3a6e1feb1ebc (bug 1797750)
    Backed out changeset 4e673df3058a (bug 1797750)

--- js/src/builtin/WrappedFunctionObject.cpp
+++ js/src/builtin/WrappedFunctionObject.cpp
@@ -6,8 +6,6 @@
 
 #include "builtin/WrappedFunctionObject.h"
 
-#include <string_view>
-
 #include "jsapi.h"
 
 #include "builtin/ShadowRealm.h"
@@ -30,8 +28,6 @@ using namespace JS;
 // GetWrappedValue ( callerRealm: a Realm Record, value: unknown )
 bool js::GetWrappedValue(JSContext* cx, Realm* callerRealm, Handle<Value> value,
                          MutableHandle<Value> res) {
-  cx->check(value);
-
   // Step 2. Return value (Reordered)
   if (!value.isObject()) {
     res.set(value);
@@ -53,124 +49,119 @@ bool js::GetWrappedValue(JSContext* cx, Realm* callerRealm, Handle<Value> value,
 
 // [[Call]]
 // https://tc39.es/proposal-shadowrealm/#sec-wrapped-function-exotic-objects-call-thisargument-argumentslist
-// https://tc39.es/proposal-shadowrealm/#sec-ordinary-wrapped-function-call
 static bool WrappedFunction_Call(JSContext* cx, unsigned argc, Value* vp) {
   CallArgs args = CallArgsFromVp(argc, vp);
 
   Rooted<JSObject*> callee(cx, &args.callee());
   MOZ_ASSERT(callee->is<WrappedFunctionObject>());
 
-  Handle<WrappedFunctionObject*> fun = callee.as<WrappedFunctionObject>();
-
-  // PrepareForWrappedFunctionCall is a no-op in our implementation, because
-  // we've already entered the correct realm.
-  MOZ_ASSERT(cx->realm() == fun->realm());
-
-  // The next steps refer to the OrdinaryWrappedFunctionCall operation.
+  Handle<WrappedFunctionObject*> F = callee.as<WrappedFunctionObject>();
 
   // 1. Let target be F.[[WrappedTargetFunction]].
-  Rooted<JSObject*> target(cx, fun->getTargetFunction());
+  Rooted<JSObject*> target(cx, F->getTargetFunction());
 
   // 2. Assert: IsCallable(target) is true.
   MOZ_ASSERT(IsCallable(ObjectValue(*target)));
 
-  // 3. Let callerRealm be F.[[Realm]].
-  Rooted<Realm*> callerRealm(cx, fun->realm());
-
-  // 4. NOTE: Any exception objects produced after this point are associated
-  //    with callerRealm.
-  //
-  // Implicit in our implementation, because |callerRealm| is already the
-  // current realm.
-
-  // 5. Let targetRealm be ? GetFunctionRealm(target).
+  // 3. Let targetRealm be ? GetFunctionRealm(target).
   Rooted<Realm*> targetRealm(cx, GetFunctionRealm(cx, target));
   if (!targetRealm) {
     return false;
   }
-
-  // 6. Let wrappedArgs be a new empty List.
-  InvokeArgs wrappedArgs(cx);
-  if (!wrappedArgs.init(cx, args.length())) {
+  // 4. Let callerRealm be ? GetFunctionRealm(F).
+  Rooted<Realm*> callerRealm(cx, GetFunctionRealm(cx, F));
+  if (!callerRealm) {
     return false;
   }
 
-  // 7. For each element arg of argumentsList, do
-  //     a. Let wrappedValue be ? GetWrappedValue(targetRealm, arg).
-  //     b. Append wrappedValue to wrappedArgs.
-  Rooted<Value> element(cx);
-  for (size_t i = 0; i < args.length(); i++) {
-    element = args.get(i);
-    if (!GetWrappedValue(cx, targetRealm, element, &element)) {
+  // 5. NOTE: Any exception objects produced after this point are associated
+  //    with callerRealm.
+  Rooted<Value> result(cx);
+  {
+    Rooted<JSObject*> global(cx, JS::GetRealmGlobalOrNull(callerRealm));
+    MOZ_RELEASE_ASSERT(
+        global, "global is null; executing in a realm that's being GC'd?");
+    AutoRealm ar(cx, global);
+
+    // https://searchfox.org/mozilla-central/source/js/public/CallAndConstruct.h#57-73
+    // 6. Let wrappedArgs be a new empty List.
+    RootedVector<Value> wrappedArgs(cx);
+
+    // 7. For each element arg of argumentsList, do
+    //     a. Let wrappedValue be ? GetWrappedValue(targetRealm, arg).
+    //     b. Append wrappedValue to wrappedArgs.
+    Rooted<Value> element(cx);
+    for (size_t i = 0; i < args.length(); i++) {
+      element = args.get(i);
+      if (!GetWrappedValue(cx, targetRealm, element, &element)) {
+        return false;
+      }
+
+      if (!wrappedArgs.append(element)) {
+        return false;
+      }
+    }
+
+    // 8. Let wrappedThisArgument to ? GetWrappedValue(targetRealm,
+    // thisArgument).
+    Rooted<Value> wrappedThisArgument(cx);
+    if (!GetWrappedValue(cx, targetRealm, args.thisv(), &wrappedThisArgument)) {
       return false;
     }
 
-    wrappedArgs[i].set(element);
-  }
+    // 9. Let result be the Completion Record of Call(target,
+    //    wrappedThisArgument, wrappedArgs).
+    if (!JS::Call(cx, wrappedThisArgument, target, wrappedArgs, &result)) {
+      // 11. Else (reordered);
+      //     a. Throw a TypeError exception.
+      ReportPotentiallyDetailedMessage(
+          cx, JSMSG_SHADOW_REALM_WRAPPED_EXECUTION_FAILURE_DETAIL,
+          JSMSG_SHADOW_REALM_WRAPPED_EXECUTION_FAILURE);
+      return false;
+    }
 
-  // 8. Let wrappedThisArgument to ? GetWrappedValue(targetRealm,
-  // thisArgument).
-  Rooted<Value> wrappedThisArgument(cx);
-  if (!GetWrappedValue(cx, targetRealm, args.thisv(), &wrappedThisArgument)) {
-    return false;
-  }
-
-  // 9. Let result be the Completion Record of Call(target,
-  //    wrappedThisArgument, wrappedArgs).
-  Rooted<Value> targetValue(cx, ObjectValue(*target));
-  Rooted<Value> result(cx);
-  if (!js::Call(cx, targetValue, wrappedThisArgument, wrappedArgs, &result)) {
-    // 11. Else (reordered);
-    //     a. Throw a TypeError exception.
-    ReportPotentiallyDetailedMessage(
-        cx, JSMSG_SHADOW_REALM_WRAPPED_EXECUTION_FAILURE_DETAIL,
-        JSMSG_SHADOW_REALM_WRAPPED_EXECUTION_FAILURE);
-    return false;
-  }
-
-  // 10. If result.[[Type]] is normal or result.[[Type]] is return, then
-  //     a. Return ? GetWrappedValue(callerRealm, result.[[Value]]).
-  if (!GetWrappedValue(cx, callerRealm, result, args.rval())) {
-    return false;
+    // 10. If result.[[Type]] is normal or result.[[Type]] is return, then
+    //     a. Return ? GetWrappedValue(callerRealm, result.[[Value]]).
+    if (!GetWrappedValue(cx, callerRealm, result, args.rval())) {
+      return false;
+    }
   }
 
   return true;
 }
 
-static bool CopyNameAndLength(JSContext* cx, HandleObject fun,
-                              HandleObject target) {
+static bool CopyNameAndLength(JSContext* cx, HandleObject F,
+                              HandleObject Target, char* prefix = nullptr,
+                              unsigned argCount = 0) {
   // 1. If argCount is undefined, then set argCount to 0 (implicit)
-  constexpr int32_t argCount = 0;
-
   // 2. Let L be 0.
-  double length = 0;
+  double L = 0;
 
   // 3. Let targetHasLength be ? HasOwnProperty(Target, "length").
-  //
+
+  Rooted<jsid> length(cx, NameToId(cx->names().length));
+  Rooted<jsid> name(cx, NameToId(cx->names().name));
   // Try to avoid invoking the resolve hook.
-  // Also see JSFunction::finishBoundFunctionInit().
-  if (target->is<JSFunction>() &&
-      !target->as<JSFunction>().hasResolvedLength()) {
+  if (Target->is<JSFunction>() &&
+      !Target->as<JSFunction>().hasResolvedLength()) {
     Rooted<Value> targetLen(cx);
-    if (!JSFunction::getUnresolvedLength(cx, target.as<JSFunction>(),
+    if (!JSFunction::getUnresolvedLength(cx, Target.as<JSFunction>(),
                                          &targetLen)) {
       return false;
     }
 
-    length = std::max(0.0, targetLen.toNumber() - argCount);
+    L = std::max(0.0, targetLen.toNumber() - argCount);
   } else {
-    Rooted<jsid> lengthId(cx, NameToId(cx->names().length));
-
     bool targetHasLength;
-    if (!HasOwnProperty(cx, target, lengthId, &targetHasLength)) {
+    if (!HasOwnProperty(cx, Target, length, &targetHasLength)) {
       return false;
     }
-
+    // https://searchfox.org/mozilla-central/source/js/src/vm/JSFunction.cpp#1298
     // 4. If targetHasLength is true, then
     if (targetHasLength) {
       //     a. Let targetLen be ? Get(Target, "length").
       Rooted<Value> targetLen(cx);
-      if (!GetProperty(cx, target, target, lengthId, &targetLen)) {
+      if (!GetProperty(cx, Target, Target, length, &targetLen)) {
         return false;
       }
 
@@ -182,31 +173,21 @@ static bool CopyNameAndLength(JSContext* cx, HandleObject fun,
       //             2. Assert: targetLenAsInt is finite.
       //             3. Set L to max(targetLenAsInt - argCount, 0).
       if (targetLen.isNumber()) {
-        length = std::max(0.0, JS::ToInteger(targetLen.toNumber()) - argCount);
+        L = std::max(0.0, JS::ToInteger(targetLen.toNumber()) - argCount);
       }
     }
   }
 
   // 5. Perform ! SetFunctionLength(F, L).
-  Rooted<Value> rootedLength(cx, NumberValue(length));
-  if (!DefineDataProperty(cx, fun, cx->names().length, rootedLength,
-                          JSPROP_READONLY)) {
+  Rooted<Value> rootedL(cx, DoubleValue(L));
+  if (!JS_DefinePropertyById(cx, F, length, rootedL, JSPROP_READONLY)) {
     return false;
   }
 
   // 6. Let targetName be ? Get(Target, "name").
-  //
-  // Try to avoid invoking the resolve hook.
   Rooted<Value> targetName(cx);
-  if (target->is<JSFunction>() && !target->as<JSFunction>().hasResolvedName()) {
-    if (!JSFunction::getUnresolvedName(cx, target.as<JSFunction>(),
-                                       &targetName)) {
-      return false;
-    }
-  } else {
-    if (!GetProperty(cx, target, target, cx->names().name, &targetName)) {
-      return false;
-    }
+  if (!GetProperty(cx, Target, Target, cx->names().name, &targetName)) {
+    return false;
   }
 
   // 7. If Type(targetName) is not String, set targetName to the empty String.
@@ -214,26 +195,47 @@ static bool CopyNameAndLength(JSContext* cx, HandleObject fun,
     targetName = StringValue(cx->runtime()->emptyString);
   }
 
+  Rooted<JSString*> targetString(cx, targetName.toString());
+  Rooted<jsid> targetNameId(cx);
+  if (!JS_StringToId(cx, targetString, &targetNameId)) {
+    return false;
+  }
+
   // 8. Perform ! SetFunctionName(F, targetName, prefix).
-  return DefineDataProperty(cx, fun, cx->names().name, targetName,
-                            JSPROP_READONLY);
+  // (inlined and specialized from js::SetFunctionName)
+  Rooted<JSAtom*> funName(cx, IdToFunctionName(cx, targetNameId));
+  if (!funName) {
+    return false;
+  }
+  Rooted<Value> rootedFunName(cx, StringValue(funName));
+  return JS_DefinePropertyById(cx, F, name, rootedFunName, JSPROP_READONLY);
 }
 
 static JSString* ToStringOp(JSContext* cx, JS::HandleObject obj,
                             bool isToSource) {
-  // Return an unnamed native function to match the behavior of bound
-  // functions.
-  //
-  // NOTE: The current value of the "name" property can be any value, it's not
-  // necessarily a string value. It can also be an accessor property which could
-  // lead to executing side-effects, which isn't allowed per the spec, cf.
-  // <https://tc39.es/ecma262/#sec-function.prototype.tostring>. Even if it's a
-  // data property with a string value, we'd still need to validate the string
-  // can be parsed as a |PropertyName| production before using it as part of the
-  // output.
-  constexpr std::string_view nativeCode = "function () {\n    [native code]\n}";
+  Rooted<jsid> name(cx, NameToId(cx->names().name));
+  Rooted<Value> nameVal(cx);
+  if (!JS_GetPropertyById(cx, obj, name, &nameVal)) {
+    return nullptr;
+  }
 
-  return NewStringCopy<CanGC>(cx, nativeCode);
+  MOZ_ASSERT(nameVal.isString());
+  Rooted<JSString*> nameStr(cx, nameVal.toString());
+
+  JSStringBuilder out(cx);
+  if (!out.append("function ")) {
+    return nullptr;
+  }
+
+  if (!out.append(nameStr)) {
+    return nullptr;
+  }
+
+  if (!out.append("() {\n    [native code]\n}")) {
+    return nullptr;
+  }
+
+  return out.finishString();
 }
 
 static const JSClassOps classOps = {
@@ -270,62 +272,63 @@ const JSClass WrappedFunctionObject::class_ = {
     &classOps,
     JS_NULL_CLASS_SPEC,
     JS_NULL_CLASS_EXT,
-    &objOps,
-};
+    &objOps};
+
+JSObject* GetRealmFunctionPrototype(JSContext* cx, Realm* realm) {
+  CHECK_THREAD(cx);
+  Rooted<GlobalObject*> global(cx, realm->maybeGlobal());
+  MOZ_RELEASE_ASSERT(global);
+  return GlobalObject::getOrCreateFunctionPrototype(cx, global);
+}
 
 // WrappedFunctionCreate ( callerRealm: a Realm Record, Target: a function
 // object)
 bool js::WrappedFunctionCreate(JSContext* cx, Realm* callerRealm,
                                HandleObject target, MutableHandle<Value> res) {
-  cx->check(target);
+  // Ensure that the function object has the correct realm by allocating it
+  // into that realm.
+  Rooted<JSObject*> global(cx, JS::GetRealmGlobalOrNull(callerRealm));
+  MOZ_RELEASE_ASSERT(global,
+                     "global is null; executing in a realm that's being GC'd?");
+  AutoRealm ar(cx, global);
 
-  WrappedFunctionObject* wrapped = nullptr;
-  {
-    // Ensure that the function object has the correct realm by allocating it
-    // into that realm.
-    Rooted<JSObject*> global(cx, callerRealm->maybeGlobal());
-    MOZ_RELEASE_ASSERT(
-        global, "global is null; executing in a realm that's being GC'd?");
-    AutoRealm ar(cx, global);
+  MOZ_ASSERT(target);
 
-    MOZ_ASSERT(target);
-
-    // Target *could* be a function from another compartment.
-    Rooted<JSObject*> maybeWrappedTarget(cx, target);
-    if (!cx->compartment()->wrap(cx, &maybeWrappedTarget)) {
-      return false;
-    }
-
-    // 1. Let internalSlotsList be the internal slots listed in Table 2, plus
-    // [[Prototype]] and [[Extensible]].
-    // 2. Let wrapped be ! MakeBasicObject(internalSlotsList).
-    // 3. Set wrapped.[[Prototype]] to
-    //    callerRealm.[[Intrinsics]].[[%Function.prototype%]].
-    wrapped = NewBuiltinClassInstance<WrappedFunctionObject>(cx);
-    if (!wrapped) {
-      return false;
-    }
-
-    // 4. Set wrapped.[[Call]] as described in 2.1 (implicit in JSClass call
-    // hook)
-    // 5. Set wrapped.[[WrappedTargetFunction]] to Target.
-    wrapped->setTargetFunction(*maybeWrappedTarget);
-    // 6. Set wrapped.[[Realm]] to callerRealm. (implicitly the realm of
-    //    wrapped, which we assured with the AutoRealm
-
-    MOZ_ASSERT(wrapped->realm() == callerRealm);
-  }
-
-  // Wrap |wrapped| to the current compartment.
-  RootedObject obj(cx, wrapped);
-  if (!cx->compartment()->wrap(cx, &obj)) {
+  // Target *could* be a function from another realm
+  Rooted<JSObject*> maybeWrappedTarget(cx, target);
+  if (!JS_WrapObject(cx, &maybeWrappedTarget)) {
     return false;
   }
 
+  // 1. Let internalSlotsList be the internal slots listed in Table 2, plus
+  // [[Prototype]] and [[Extensible]].
+  // 2. Let wrapped be ! MakeBasicObject(internalSlotsList).
+  // 3. Set wrapped.[[Prototype]] to
+  //    callerRealm.[[Intrinsics]].[[%Function.prototype%]].
+  Rooted<JSObject*> functionPrototype(
+      cx, GetRealmFunctionPrototype(cx, callerRealm));
+  MOZ_ASSERT(cx->compartment() == functionPrototype->compartment());
+
+  Rooted<WrappedFunctionObject*> wrapped(
+      cx,
+      NewObjectWithGivenProto<WrappedFunctionObject>(cx, functionPrototype));
+  if (!wrapped) {
+    return false;
+  }
+
+  // 4. Set wrapped.[[Call]] as described in 2.1 (implicit in JSClass call
+  // hook)
+  // 5. Set wrapped.[[WrappedTargetFunction]] to Target.
+  wrapped->setTargetFunction(*maybeWrappedTarget);
+  // 6. Set wrapped.[[Realm]] to callerRealm. (implicitly the realm of
+  //    wrapped, which we assured with the AutoRealm
+
+  MOZ_ASSERT(wrapped->realm() == callerRealm);
+
   // 7. Let result be CopyNameAndLength(wrapped, Target).
-  if (!CopyNameAndLength(cx, obj, target)) {
+  if (!CopyNameAndLength(cx, wrapped, maybeWrappedTarget)) {
     // 8. If result is an Abrupt Completion, throw a TypeError exception.
-    cx->clearPendingException();
+    JS_ClearPendingException(cx);
 
     JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
                               JSMSG_SHADOW_REALM_WRAP_FAILURE);
@@ -333,6 +336,6 @@ bool js::WrappedFunctionCreate(JSContext* cx, Realm* callerRealm,
   }
 
   // 9. Return wrapped.
-  res.set(ObjectValue(*obj));
+  res.set(ObjectValue(*wrapped));
   return true;
 }
