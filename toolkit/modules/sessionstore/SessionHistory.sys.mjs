4644f63efd9592a2c68075cf782869989b645e0a
Mark Banner <standard8@mozilla.com>
2022-10-31 15:15:33 +0000

    Bug 1798058 - Convert toolkit/modules/sessionstore/ to ES modules. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D160681

--- toolkit/modules/sessionstore/SessionHistory.sys.mjs
+++ toolkit/modules/sessionstore/SessionHistory.sys.mjs
@@ -2,10 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-"use strict";
-
-var EXPORTED_SYMBOLS = ["SessionHistory"];
-
 const lazy = {};
 
 ChromeUtils.defineESModuleGetters(lazy, {
@@ -19,7 +15,7 @@ function debug(msg) {
 /**
  * The external API exported by this module.
  */
-var SessionHistory = Object.freeze({
+export var SessionHistory = Object.freeze({
   isEmpty(docShell) {
     return SessionHistoryInternal.isEmpty(docShell);
   },
