a9a311076c7cd78f94a54aec1ec0121efd9cbbce
André Bargull <andre.bargull@gmail.com>
2022-10-31 07:08:47 +0000

    Bug 1765357 - Part 5: Remove top_level_await preference. r=mgaudet
    Depends on D160615
    
    Differential Revision: https://phabricator.services.mozilla.com/D160616

--- js/src/tests/user.js
+++ js/src/tests/user.js
@@ -31,7 +31,6 @@ user_pref("browser.webapps.checkForUpdates", 0);
 user_pref("javascript.options.weakrefs", true);
 user_pref("javascript.options.experimental.weakrefs.expose_cleanupSome", true);
 user_pref("javascript.options.experimental.iterator_helpers", true);
-user_pref("javascript.options.experimental.top_level_await", true);
 user_pref("javascript.options.experimental.enable_change_array_by_copy", false);
 user_pref("javascript.options.experimental.enable_new_set_methods", false);
 user_pref("javascript.options.experimental.import_assertions", true);
