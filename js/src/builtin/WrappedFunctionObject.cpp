40bea278491d297462a5a0f6daba323296e65a1b
André Bargull <andre.bargull@gmail.com>
2022-10-31 08:52:35 +0000

    Bug 1797750 - Part 7: Use NewBuiltinClassInstance instead of explicitly passing the prototype. r=mgaudet
    `NewBuiltinClassInstance` automatically selects the correct prototype.
    
    Depends on D160509
    
    Differential Revision: https://phabricator.services.mozilla.com/D160510

--- js/src/builtin/WrappedFunctionObject.cpp
+++ js/src/builtin/WrappedFunctionObject.cpp
@@ -276,13 +276,6 @@ const JSClass WrappedFunctionObject::class_ = {
     &objOps,
 };
 
-JSObject* GetRealmFunctionPrototype(JSContext* cx, Realm* realm) {
-  CHECK_THREAD(cx);
-  Rooted<GlobalObject*> global(cx, realm->maybeGlobal());
-  MOZ_RELEASE_ASSERT(global);
-  return GlobalObject::getOrCreateFunctionPrototype(cx, global);
-}
-
 // WrappedFunctionCreate ( callerRealm: a Realm Record, Target: a function
 // object)
 bool js::WrappedFunctionCreate(JSContext* cx, Realm* callerRealm,
@@ -307,13 +300,8 @@ bool js::WrappedFunctionCreate(JSContext* cx, Realm* callerRealm,
   // 2. Let wrapped be ! MakeBasicObject(internalSlotsList).
   // 3. Set wrapped.[[Prototype]] to
   //    callerRealm.[[Intrinsics]].[[%Function.prototype%]].
-  Rooted<JSObject*> functionPrototype(
-      cx, GetRealmFunctionPrototype(cx, callerRealm));
-  MOZ_ASSERT(cx->compartment() == functionPrototype->compartment());
-
   Rooted<WrappedFunctionObject*> wrapped(
-      cx,
-      NewObjectWithGivenProto<WrappedFunctionObject>(cx, functionPrototype));
+      cx, NewBuiltinClassInstance<WrappedFunctionObject>(cx));
   if (!wrapped) {
     return false;
   }
