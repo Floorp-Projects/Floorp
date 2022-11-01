abe71a513312c441f1364d23eb51cc7d5df01613
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-11-01 12:05:24 +0000

    Bug 1798369 - Part 3: Esmify dom/network/tests/tcpsocket_test.jsm r=necko-reviewers,kershaw
    Depends on D160825
    
    Differential Revision: https://phabricator.services.mozilla.com/D160826

---
+++ dom/network/tests/tcpsocket_test.sys.mjs
@@ -0,0 +1,12 @@
+export var createSocket = function(host, port, options) {
+  return new TCPSocket(host, port, options);
+};
+
+export var createServer = function(port, options, backlog) {
+  return new TCPServerSocket(port, options, backlog);
+};
+
+// See test_tcpsocket_client_and_server_basics.html's version for rationale.
+export var socketCompartmentInstanceOfArrayBuffer = function(obj) {
+  return obj instanceof ArrayBuffer;
+};
