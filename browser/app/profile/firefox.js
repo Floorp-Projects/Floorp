2787704900ff83852e784dc4df9194b34ee66e42
Paul Zuehlcke <pbz@mozilla.com>
2022-11-01 13:34:56 +0000

    Bug 1795041 - Add a cookie banner handling per-site toggle to the protection panel. r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D160254

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2749,6 +2749,8 @@ pref("browser.pdfjs.feature-tour", "{\"screen\":\"FEATURE_CALLOUT_1\",\"complete
 // StaticPrefList.yaml for a description of the prefs.
 #ifdef NIGHTLY_BUILD
   pref("cookiebanners.service.mode.privateBrowsing", 1);
+  // Enables the cookie banner desktop UI.
+  pref("cookiebanners.ui.desktop.enabled", true);
 #endif
 
 // We only want to enable this pref for Desktop nightlies.
