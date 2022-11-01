7168eb355f90d1534e1a866c8937d52931711493
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-11-01 20:46:35 +0900

    [FB] Webpanel | If the Listener is still there after 3 minutes, it is discarded.

--- floorp/browser/extensions/webextensions/webpanel-ua/background.js
+++ floorp/browser/extensions/webextensions/webpanel-ua/background.js
@@ -3,11 +3,7 @@ const ua = "Mozilla/5.0 (Android 12; Mobile; rv:102.0) Gecko/102.0 Firefox/102.0
 browser.webRequestExt.onBeforeRequest_webpanel_requestId.addListener(function(requestId){
   function listener(e) {
     if (e.requestId == requestId) {
-      browser.webRequest.onBeforeSendHeaders.removeListener(
-        listener,
-        {urls: ["<all_urls>"]},
-        ["blocking", "requestHeaders"]
-      )
+      browser.webRequest.onBeforeSendHeaders.removeListener(listener);
       e.requestHeaders.forEach(function (header) {
         if (header.name.toLowerCase() === "user-agent") {
             header.value = ua;
@@ -20,5 +16,10 @@ browser.webRequestExt.onBeforeRequest_webpanel_requestId.addListener(function(re
     listener,
     {urls: ["<all_urls>"]},
     ["blocking", "requestHeaders"]
-  )
+  );
+  setTimeout(function(){
+    if (browser.webRequest.onBeforeSendHeaders.hasListener(listener)) {
+      browser.webRequest.onBeforeSendHeaders.removeListener(listener);
+    }
+  }, 180 * 1000);
 })
