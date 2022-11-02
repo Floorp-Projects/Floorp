f4b35ff076e9d643ff843530fac5e32f6d290beb
Yury Delendik <ydelendik@mozilla.com>
2022-11-02 13:58:57 +0000

    Bug 1797158 - Initial import of Relaxed SIMD spec tests. r=rhunt
    Differential Revision: https://phabricator.services.mozilla.com/D160109

---
+++ js/src/jit-test/tests/wasm/spec/relaxed-simd/relaxed_laneselect.wast.js
@@ -0,0 +1,176 @@
+/* Copyright 2021 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// ./test/core/relaxed-simd/relaxed_laneselect.wast
+
+// ./test/core/relaxed-simd/relaxed_laneselect.wast:3
+let $0 = instantiate(`(module
+    (func (export "i8x16.relaxed_laneselect") (param v128 v128 v128) (result v128) (i8x16.relaxed_laneselect (local.get 0) (local.get 1) (local.get 2)))
+    (func (export "i16x8.relaxed_laneselect") (param v128 v128 v128) (result v128) (i8x16.relaxed_laneselect (local.get 0) (local.get 1) (local.get 2)))
+    (func (export "i32x4.relaxed_laneselect") (param v128 v128 v128) (result v128) (i8x16.relaxed_laneselect (local.get 0) (local.get 1) (local.get 2)))
+    (func (export "i64x2.relaxed_laneselect") (param v128 v128 v128) (result v128) (i8x16.relaxed_laneselect (local.get 0) (local.get 1) (local.get 2)))
+)`);
+
+// ./test/core/relaxed-simd/relaxed_laneselect.wast:10
+assert_return(
+  () =>
+    invoke($0, `i8x16.relaxed_laneselect`, [
+      i8x16([
+        0x0,
+        0x1,
+        0x12,
+        0x12,
+        0x4,
+        0x5,
+        0x6,
+        0x7,
+        0x8,
+        0x9,
+        0xa,
+        0xb,
+        0xc,
+        0xd,
+        0xe,
+        0xf,
+      ]),
+      i8x16([
+        0x10,
+        0x11,
+        0x34,
+        0x34,
+        0x14,
+        0x15,
+        0x16,
+        0x17,
+        0x18,
+        0x19,
+        0x1a,
+        0x1b,
+        0x1c,
+        0x1d,
+        0x1e,
+        0x1f,
+      ]),
+      i8x16([
+        0xff,
+        0x0,
+        0xf0,
+        0xf,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+        0x0,
+      ]),
+    ]),
+  [either(
+    i8x16([
+      0x0,
+      0x11,
+      0x14,
+      0x32,
+      0x14,
+      0x15,
+      0x16,
+      0x17,
+      0x18,
+      0x19,
+      0x1a,
+      0x1b,
+      0x1c,
+      0x1d,
+      0x1e,
+      0x1f,
+    ]),
+    i8x16([
+      0x0,
+      0x11,
+      0x12,
+      0x34,
+      0x14,
+      0x15,
+      0x16,
+      0x17,
+      0x18,
+      0x19,
+      0x1a,
+      0x1b,
+      0x1c,
+      0x1d,
+      0x1e,
+      0x1f,
+    ]),
+  )],
+);
+
+// ./test/core/relaxed-simd/relaxed_laneselect.wast:17
+assert_return(
+  () =>
+    invoke($0, `i16x8.relaxed_laneselect`, [
+      i16x8([0x0, 0x1, 0x1234, 0x1234, 0x4, 0x5, 0x6, 0x7]),
+      i16x8([0x8, 0x9, 0x5678, 0x5678, 0xc, 0xd, 0xe, 0xf]),
+      i16x8([0xffff, 0x0, 0xff00, 0xff, 0x0, 0x0, 0x0, 0x0]),
+    ]),
+  [either(
+    i16x8([0x0, 0x9, 0x1278, 0x5634, 0xc, 0xd, 0xe, 0xf]),
+    i16x8([0x0, 0x9, 0x1234, 0x5678, 0xc, 0xd, 0xe, 0xf]),
+  )],
+);
+
+// ./test/core/relaxed-simd/relaxed_laneselect.wast:24
+assert_return(
+  () =>
+    invoke($0, `i32x4.relaxed_laneselect`, [
+      i32x4([0x0, 0x1, 0x12341234, 0x12341234]),
+      i32x4([0x4, 0x5, 0x56785678, 0x56785678]),
+      i32x4([0xffffffff, 0x0, 0xffff0000, 0xffff]),
+    ]),
+  [either(
+    i32x4([0x0, 0x5, 0x12345678, 0x56781234]),
+    i32x4([0x0, 0x5, 0x12341234, 0x56785678]),
+  )],
+);
+
+// ./test/core/relaxed-simd/relaxed_laneselect.wast:31
+assert_return(
+  () =>
+    invoke($0, `i64x2.relaxed_laneselect`, [
+      i64x2([0x0n, 0x1n]),
+      i64x2([0x2n, 0x3n]),
+      i64x2([0xffffffffffffffffn, 0x0n]),
+    ]),
+  [either(i64x2([0x0n, 0x3n]), i64x2([0x0n, 0x3n]))],
+);
+
+// ./test/core/relaxed-simd/relaxed_laneselect.wast:38
+assert_return(
+  () =>
+    invoke($0, `i64x2.relaxed_laneselect`, [
+      i64x2([0x1234123412341234n, 0x1234123412341234n]),
+      i64x2([0x5678567856785678n, 0x5678567856785678n]),
+      i64x2([0xffffffff00000000n, 0xffffffffn]),
+    ]),
+  [either(
+    i64x2([0x1234123456785678n, 0x5678567812341234n]),
+    i64x2([0x1234123412341234n, 0x5678567856785678n]),
+  )],
+);
