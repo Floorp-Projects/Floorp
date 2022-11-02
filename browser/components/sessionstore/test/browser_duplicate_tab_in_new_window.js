c61d80412bd316c7dcd466a5d025a0448581706d
Andreas Farre <farre@mozilla.com>
2022-11-02 13:45:46 +0000

    Bug 1798232 - Make sure to flush session store when moving tabs to new window. r=dao
    When moving a tab to a new window, the tab state needs to be flushed
    and then copied over. Copying the current state could possibly miss
    out on any buffered changes. Unfortunately we can't wait for the flush
    to complete and then move, so we need to force the old state on top of
    the new.
    
    Differential Revision: https://phabricator.services.mozilla.com/D160887

---
+++ browser/components/sessionstore/test/browser_duplicate_tab_in_new_window.js
@@ -0,0 +1,36 @@
+ChromeUtils.defineModuleGetter(
+  this,
+  "SessionStore",
+  "resource:///modules/sessionstore/SessionStore.jsm"
+);
+
+add_task(async function() {
+  await BrowserTestUtils.withNewTab("https://example.com", async function(
+    aBrowser
+  ) {
+    BrowserTestUtils.loadURI(aBrowser, "https://example.org");
+    await BrowserTestUtils.browserLoaded(aBrowser);
+
+    let windowOpened = BrowserTestUtils.waitForNewWindow("https://example.org");
+    let newWindow = gBrowser.replaceTabWithWindow(
+      gBrowser.getTabForBrowser(aBrowser)
+    );
+    await windowOpened;
+    let newTab = SessionStore.duplicateTab(
+      newWindow,
+      newWindow.gBrowser.selectedTab
+    );
+
+    await BrowserTestUtils.browserLoaded(newTab.linkedBrowser);
+
+    await SpecialPowers.spawn(
+      newTab.linkedBrowser,
+      ["https://example.org"],
+      async ORIGIN => {
+        is(content.window.origin, ORIGIN);
+      }
+    );
+
+    BrowserTestUtils.closeWindow(newWindow);
+  });
+});
