f5db28b11a0bca86b6d8e8516d3c37fc598afd67
Sandor Molnar <smolnar@mozilla.com>
2022-11-03 00:41:43 +0200

    Backed out 2 changesets (bug 1798363) for causing android mochitest failures in dom/serviceworkers/test/test_https_origin_after_redirect_cached.html CLOSED TREE
    Backed out changeset 2b6cf2df8e21 (bug 1798363)
    Backed out changeset fe72217f6916 (bug 1798363)

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/ContentBlocking.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/ContentBlocking.java
@@ -275,12 +275,10 @@ public class ContentBlocking {
     /* package */ final Pref<Boolean> mSbPhishing =
         new Pref<Boolean>("browser.safebrowsing.phishing.enabled", true);
     /* package */ final Pref<Integer> mCookieBehavior =
-        new Pref<Integer>(
-            "network.cookie.cookieBehavior", CookieBehavior.ACCEPT_FIRST_PARTY_AND_ISOLATE_OTHERS);
+        new Pref<Integer>("network.cookie.cookieBehavior", CookieBehavior.ACCEPT_NON_TRACKERS);
     /* package */ final Pref<Integer> mCookieBehaviorPrivateMode =
         new Pref<Integer>(
-            "network.cookie.cookieBehavior.pbmode",
-            CookieBehavior.ACCEPT_FIRST_PARTY_AND_ISOLATE_OTHERS);
+            "network.cookie.cookieBehavior.pbmode", CookieBehavior.ACCEPT_NON_TRACKERS);
     /* package */ final Pref<Boolean> mCookiePurging =
         new Pref<Boolean>("privacy.purge_trackers.enabled", false);
 
@@ -1283,7 +1281,7 @@ public class ContentBlocking {
   @IntDef({
     CookieBehavior.ACCEPT_ALL, CookieBehavior.ACCEPT_FIRST_PARTY,
     CookieBehavior.ACCEPT_NONE, CookieBehavior.ACCEPT_VISITED,
-    CookieBehavior.ACCEPT_NON_TRACKERS, CookieBehavior.ACCEPT_FIRST_PARTY_AND_ISOLATE_OTHERS
+    CookieBehavior.ACCEPT_NON_TRACKERS
   })
   public @interface CBCookieBehavior {}
 
