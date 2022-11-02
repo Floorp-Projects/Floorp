68b65a3c90e230cfaa52c71cae5a99233df531fc
Barret Rennie <barret@brennie.ca>
2022-11-02 02:08:54 +0000

    Bug 1541508 - Use Services.env in dom/ r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D160138

--- dom/credentialmanagement/tests/browser/browser_active_document.js
+++ dom/credentialmanagement/tests/browser/browser_active_document.js
@@ -109,11 +109,8 @@ add_task(async function test_background_window() {
 });
 
 add_task(async function test_minimized() {
-  let env = Cc["@mozilla.org/process/environment;1"].getService(
-    Ci.nsIEnvironment
-  );
   // Minimizing windows doesn't supported in headless mode.
-  if (env.get("MOZ_HEADLESS")) {
+  if (Services.env.get("MOZ_HEADLESS")) {
     return;
   }
 
