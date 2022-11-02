2c65481dfbf44fcb818354a3278fcf6301bb3838
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2022-11-02 00:13:57 +0000

    Bug 1790301 - Allow Unicode-DFS-2016 license to import ICU4X 1.0. r=mhoye,glandium
    By bug 1715232, we added ICU4X license SHA256 hash to whitelist. But when
    releasing ICU4X 1.0, license becomes Unicode-DFS-2016 like ICU4C. So I would
    like to allow this license to import ICU4X 1.0.
    
    Also, due to dependencies, some crates still use old ICU4X license. Although it
    will be fixed by 1.1 release, we keep this hash.
    
    Differential Revision: https://phabricator.services.mozilla.com/D160588

--- python/mozbuild/mozbuild/vendor/vendor_rust.py
+++ python/mozbuild/mozbuild/vendor/vendor_rust.py
@@ -280,6 +280,7 @@ Please commit or stash these changes before vendoring, or re-run with `--ignore-
         "ISC",
         "MIT",
         "MPL-2.0",
+        "Unicode-DFS-2016",
         "Unlicense",
         "Zlib",
     ]
@@ -332,22 +333,9 @@ Please commit or stash these changes before vendoring, or re-run with `--ignore-
         # we're whitelisting this fuchsia crate because it doesn't get built in the final
         # product but has a license-file that needs ignoring
         "fuchsia-cprng": "03b114f53e6587a398931762ee11e2395bfdba252a329940e2c8c9e81813845b",
-        # ICU4X crates, see comment above.
-        "fixed_decimal": ICU4X_LICENSE_SHA256,
-        "icu_plurals": ICU4X_LICENSE_SHA256,
-        "icu_datetime": ICU4X_LICENSE_SHA256,
-        "icu_decimal": ICU4X_LICENSE_SHA256,
-        "icu_locale_canonicalizer": ICU4X_LICENSE_SHA256,
-        "icu_locid": ICU4X_LICENSE_SHA256,
-        "icu_locid_macros": ICU4X_LICENSE_SHA256,
-        "icu_provider": ICU4X_LICENSE_SHA256,
-        "icu_provider_macros": ICU4X_LICENSE_SHA256,
-        "icu": ICU4X_LICENSE_SHA256,
-        "icu_uniset": ICU4X_LICENSE_SHA256,
-        "litemap": ICU4X_LICENSE_SHA256,
-        "writeable": ICU4X_LICENSE_SHA256,
-        "yoke": ICU4X_LICENSE_SHA256,
+        # Old ICU4X crates for ICU4X 1.0, see comment above.
         "yoke-derive": ICU4X_LICENSE_SHA256,
+        "zerofrom-derive": ICU4X_LICENSE_SHA256,
     }
 
     @staticmethod
