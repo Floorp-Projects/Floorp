a3048d0b1b8c8a2d58cff268c776830a456217d2
Ryan Hunt <rhunt@eqrion.net>
2022-11-03 15:07:48 +0000

    Bug 1774827 - wasm: Use TypeDef for runtime casting and remove remnants of rtt.sub. r=yury
    RttValue used to contain parents/children for rtt.sub and this was used
    for runtime casting. We have all this information in TypeDef. This commit
    basically turns RttValue into a thin wrapper around a *TypeDef. It may make
    sense to remove RttValue and replace it with *TypeDef eventually.
    
    Differential Revision: https://phabricator.services.mozilla.com/D159775

--- js/src/wasm/WasmGcObject.h
+++ js/src/wasm/WasmGcObject.h
@@ -11,7 +11,6 @@
 #include "mozilla/Maybe.h"
 
 #include "gc/Allocator.h"
-#include "gc/WeakMap.h"
 #include "vm/ArrayBufferObject.h"
 #include "vm/JSObject.h"
 #include "wasm/WasmTypeDef.h"
@@ -36,15 +35,11 @@ class RttValue : public NativeObject {
   enum Slot {
     TypeContext = 0,  // Manually refcounted reference to TypeContext
     TypeDef = 1,      // Raw pointer to TypeDef owned by TypeContext
-    Parent = 2,       // Parent rtt for runtime casting
-    Children = 3,     // Child rtts for rtt.sub caching
     // Maximum number of slots
-    SlotCount = 4,
+    SlotCount = 2,
   };
 
   static RttValue* rttCanon(JSContext* cx, const wasm::TypeHandle& handle);
-  static RttValue* rttSub(JSContext* cx, js::Handle<RttValue*> parent,
-                          js::Handle<RttValue*> subCanon);
 
   bool isNewborn() { return getReservedSlot(Slot::TypeContext).isUndefined(); }
 
@@ -57,21 +52,7 @@ class RttValue : public NativeObject {
         .toPrivate();
   }
 
-  wasm::TypeHandle typeHandle() const {
-    return wasm::TypeHandle(typeContext(), typeDef());
-  }
-
   wasm::TypeDefKind kind() const { return typeDef().kind(); }
-
-  RttValue* parent() const {
-    return (RttValue*)getReservedSlot(Slot::Parent).toObjectOrNull();
-  }
-
-  ObjectWeakMap* maybeChildren() const {
-    return (ObjectWeakMap*)getReservedSlot(Slot::Children).toPrivate();
-  }
-  ObjectWeakMap& children() const { return *maybeChildren(); }
-  bool ensureChildren(JSContext* cx);
 
   // PropOffset is a uint32_t that is used to carry information about the
   // location of an value from RttValue::lookupProperty to
@@ -108,7 +89,6 @@ class RttValue : public NativeObject {
     return lookupProperty(cx, object, id, &offset, &type);
   }
 
-  static void trace(JSTracer* trc, JSObject* obj);
   static void finalize(JS::GCContext* gcx, JSObject* obj);
 };
 
