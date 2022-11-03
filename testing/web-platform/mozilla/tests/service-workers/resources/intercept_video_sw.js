d3fc243eece2d66c2960edd3d3ec08278297641c
Butkovits Atila <abutkovits@mozilla.com>
2022-11-03 20:39:02 +0200

    Backed out changeset 1840f6c848d9 (bug 1762078) for causing linting failures. CLOSED TREE

--- testing/web-platform/mozilla/tests/service-workers/resources/intercept_video_sw.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-self.addEventListener('fetch', event => {
-  if (event.request.url.indexOf('fetch_video.py') !== -1) {
-    // respond with a non-video
-    event.respondWith(new Response('intercepted'));
-  }
-});
