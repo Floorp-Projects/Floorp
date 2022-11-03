9db1038956e7a08a166facb24ab65bf355e40778
Alexandre Poirot <poirot.alex@gmail.com>
2022-11-03 14:05:44 +0000

    Bug 1797274 - [devtools] Track source-map gecko API performance. r=perftest-reviewers,bomsy,jdescottes,sparky
    We can't test source-map package performance yet as it isn't exposed
    and only loaded within a webpack bundle.
    
    But it would be nice to also cover the code on top of the source-map package,
    the "source map loader".
    
    Differential Revision: https://phabricator.services.mozilla.com/D160199

---
+++ testing/talos/talos/tests/devtools/addon/content/tests/source-map/source-map-loader.js
@@ -0,0 +1,64 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+/**
+ * This test cover the performance of the "Source Map Loader".
+ * The layer on top of source-map npm package, specific to mozilla-central/gecko
+ * from devtools/client/shared/source-map-loader.
+ */
+
+const { testSetup, testTeardown, runTest } = require("damp-test/tests/head");
+
+// source-map module has to be loaded via the browser loader in order to use the Worker API
+const { BrowserLoader } = ChromeUtils.import(
+  "resource://devtools/shared/loader/browser-loader.js"
+);
+const { require: browserRequire } = BrowserLoader({
+  baseURI: "resource://devtools/client/shared/",
+  window: dampWindow,
+});
+const sourceMap = browserRequire("devtools/client/shared/source-map/index");
+
+const SOURCE_MAP_WORKER =
+  "resource://devtools/client/shared/source-map/worker.js";
+const SOURCE_MAP_WORKER_ASSETS =
+  "resource://devtools/client/shared/source-map/assets/";
+
+module.exports = async function() {
+  await testSetup("data:text/html,source-map");
+
+  sourceMap.startSourceMapWorker(SOURCE_MAP_WORKER, SOURCE_MAP_WORKER_ASSETS);
+  const fakeGeneratedSource = {
+    id: "fake-id",
+    url:
+      "http://example.com/tests/devtools/addon/content/tests/source-map/angular-min.js",
+    sourceMapBaseURL:
+      "http://example.com/tests/devtools/addon/content/tests/source-map/",
+    sourceMapURL: "angular-min.js.map",
+  };
+
+  {
+    const test = runTest(`source-map-loader.init.DAMP`);
+    await sourceMap.getOriginalURLs(fakeGeneratedSource);
+    test.done();
+  }
+
+  {
+    const test = runTest(`source-map-loader.getOriginalLocation.DAMP`);
+    for (let x = 0; x < 1000; x++) {
+      await sourceMap.getOriginalLocation({
+        sourceId: fakeGeneratedSource.id,
+        line: 10,
+        column: 0,
+        sourceUrl: fakeGeneratedSource.url,
+      });
+    }
+    test.done();
+  }
+  sourceMap.clearSourceMaps();
+
+  await testTeardown();
+};
