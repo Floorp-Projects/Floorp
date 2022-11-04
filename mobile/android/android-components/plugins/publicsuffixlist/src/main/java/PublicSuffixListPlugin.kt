3cf2c7473401e629975fd53bab4c4d237a4901b2
Gabriel Luong <gabriel.luong@gmail.com>
2022-11-04 15:35:55 -0400

    Bug 1800540 - Refactor PublicSuffixListPlugin into a plugin using composite build

--- mobile/android/android-components/plugins/publicsuffixlist/src/main/java/PublicSuffixListPlugin.kt
+++ mobile/android/android-components/plugins/publicsuffixlist/src/main/java/PublicSuffixListPlugin.kt
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package plugins
-
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okio.ByteString
