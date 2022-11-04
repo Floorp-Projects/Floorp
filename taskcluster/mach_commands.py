f1096b53ec40f290d99fc1b4a96c5d75a78ea4ad
Stanca Serban <sstanca@mozilla.com>
2022-11-04 11:22:09 +0200

    Backed out 3 changesets (bug 1759030) for causing Bug 1799074. CLOSED TREE
    Backed out changeset 9c52daf6117c (bug 1759030)
    Backed out changeset 9940b1cf6881 (bug 1759030)
    Backed out changeset 3f2b2e009c00 (bug 1759030)

--- taskcluster/mach_commands.py
+++ taskcluster/mach_commands.py
@@ -331,7 +331,7 @@ def setup_logging(command_context, quiet=False, verbose=True):
 def show_actions(command_context, options):
     import gecko_taskgraph
     import gecko_taskgraph.actions
-    from taskgraph.generator import TaskGraphGenerator
+    import gecko_taskgraph.generator
     from taskgraph.parameters import parameters_loader
 
     try:
@@ -340,7 +340,7 @@ def show_actions(command_context, options):
         )
         parameters = parameters_loader(options["parameters"])
 
-        tgg = TaskGraphGenerator(
+        tgg = gecko_taskgraph.generator.TaskGraphGenerator(
             root_dir=options.get("root"),
             parameters=parameters,
         )
