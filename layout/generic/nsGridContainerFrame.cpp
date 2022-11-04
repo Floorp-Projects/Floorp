38289fb83c6830f85af6dcf218afb1b4482f135f
Tiaan Louw <tlouw@mozilla.com>
2022-11-04 14:05:48 +0000

    Bug 1799111 - Don't apply auto min sizing when spanning flexible tracks. r=emilio
    This is the first part of the bug.  A clarification in the spec states
    that a grid item spanning multiple tracks, which include a flexible
    track, should not participate in auto min sizing calculation and should
    have a width of 0.
    
    https://w3c.github.io/csswg-drafts/css-grid-1/#min-size-auto
    
    Differential Revision: https://phabricator.services.mozilla.com/D160900

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -6247,6 +6247,7 @@ void nsGridContainerFrame::Tracks::ResolveIntrinsicSize(
 
       // Check if we need to apply "Automatic Minimum Size" and cache it.
       if ((state & TrackSize::eAutoMinSizing) &&
+          !(state & TrackSize::eFlexMaxSizing) &&
           gridItem.ShouldApplyAutoMinSize(wm, mAxis, aPercentageBasis)) {
         gridItem.mState[mAxis] |= ItemState::eApplyAutoMinSize;
       }
