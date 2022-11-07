cc5f9b1030dddd375a5d4cf7b0290aa7c71ba5dc
Joshua Marshall <jmarshall@mozilla.com>
2022-11-07 14:41:32 +0000

    Bug 1762078 - Ensure service workers don't intercept crossorigin media requests r=edenchuang
    ***
    Bug 1762078 - Blocking SerivceWorker interception for no_cors cross-origin range request. r=dom-worker-reviewers,karlt
    ***
    Bug 1762078 - Lint python file.  Fix video file.
    
    Differential Revision: https://phabricator.services.mozilla.com/D159315

---
+++ testing/web-platform/mozilla/tests/service-workers/resources/fetch_video.py
@@ -0,0 +1,14 @@
+import os
+
+
+def main(request, response):
+    filename = os.path.join(request.doc_root, u"media", u"2x2-green.ogv")
+    body = open(filename, "rb").read()
+    length = len(body)
+    headers = [
+        (b"Content-Type", b"video/ogg"),
+        (b"Accept-Ranges", b"bytes"),
+        (b"Content-Length", b"%d" % length),
+        (b"Content-Range", b"bytes 0-%d/%d" % (length - 1, length)),
+    ]
+    return headers, body
