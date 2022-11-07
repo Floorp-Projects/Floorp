7f4cd8099511efd6ece6ab610679717b7dfa344e
Paul Bone <pbone@mozilla.com>
2022-11-07 22:36:36 +0000

    Bug 1760920 - pt 2b. Worker debugger can query the doc group r=dom-worker-reviewers,asuth,smaug
    I want to remove CollectMemoryInfo(BrowsingContext*, _), but asuth suggested
    that a lot of this code was questionable.  Now it should be thread safe and
    measure the correct memory.
    
    Differential Revision: https://phabricator.services.mozilla.com/D146673

--- dom/workers/WorkerDebugger.cpp
+++ dom/workers/WorkerDebugger.cpp
@@ -500,7 +500,6 @@ void WorkerDebugger::ReportErrorToDebuggerOnMainThread(
 
 RefPtr<PerformanceInfoPromise> WorkerDebugger::ReportPerformanceInfo() {
   AssertIsOnMainThread();
-  RefPtr<BrowsingContext> top;
   RefPtr<WorkerDebugger> self = this;
 
 #if defined(XP_WIN)
@@ -510,7 +509,6 @@ RefPtr<PerformanceInfoPromise> WorkerDebugger::ReportPerformanceInfo() {
 #endif
   bool isTopLevel = false;
   uint64_t windowID = mWorkerPrivate->WindowID();
-  PerformanceMemoryInfo memoryInfo;
 
   // Walk up to our containing page and its window
   WorkerPrivate* wp = mWorkerPrivate;
@@ -521,7 +519,7 @@ RefPtr<PerformanceInfoPromise> WorkerDebugger::ReportPerformanceInfo() {
   if (win) {
     BrowsingContext* context = win->GetBrowsingContext();
     if (context) {
-      top = context->Top();
+      RefPtr<BrowsingContext> top = context->Top();
       if (top && top->GetCurrentWindowContext()) {
         windowID = top->GetCurrentWindowContext()->OuterWindowId();
         isTopLevel = context->IsTop();
@@ -548,40 +546,59 @@ RefPtr<PerformanceInfoPromise> WorkerDebugger::ReportPerformanceInfo() {
   // Firefox.
   FallibleTArray<CategoryDispatch> items;
 
+  if (mWorkerPrivate->GetParent()) {
+    // We cannot properly measure the memory usage of nested workers
+    // (https://phabricator.services.mozilla.com/D146673#4948924)
+    return PerformanceInfoPromise::CreateAndResolve(
+        PerformanceInfo(url, pid, windowID, duration, perfId, true, isTopLevel,
+                        PerformanceMemoryInfo(), items),
+        __func__);
+  }
+
   CategoryDispatch item =
       CategoryDispatch(DispatchCategory::Worker.GetValue(), count);
   if (!items.AppendElement(item, fallible)) {
     NS_ERROR("Could not complete the operation");
   }
 
-  if (!isTopLevel) {
+  // Switch to the worker thread to gather the JS Runtime's memory usage.
+  RefPtr<WorkerPrivate::JSMemoryUsagePromise> memoryUsagePromise =
+      mWorkerPrivate->GetJSMemoryUsage();
+  if (!memoryUsagePromise) {
+    // The worker is shutting down, so we don't count the JavaScript memory.
     return PerformanceInfoPromise::CreateAndResolve(
         PerformanceInfo(url, pid, windowID, duration, perfId, true, isTopLevel,
-                        memoryInfo, items),
+                        PerformanceMemoryInfo(), items),
         __func__);
   }
 
   // We need to keep a ref on workerPrivate, passed to the promise,
-  // to make sure it's still aloive when collecting the info
-  // (and CheckedUnsafePtr does not convert directly to RefPtr).
+  // to make sure it's still alive when collecting the info, and we can't do
+  // this in WorkerPrivate::GetJSMemoryUsage() since that could cause it to be
+  // freed on the worker thread.
+  // Because CheckedUnsafePtr does not convert directly to RefPtr, we have an
+  // extra step here.
   WorkerPrivate* workerPtr = mWorkerPrivate;
   RefPtr<WorkerPrivate> workerRef = workerPtr;
-  RefPtr<AbstractThread> mainThread = AbstractThread::MainThread();
 
-  return CollectMemoryInfo(top, mainThread)
-      ->Then(
-          mainThread, __func__,
-          [workerRef, url, pid, perfId, windowID, duration, isTopLevel,
-           items = std::move(items)](const PerformanceMemoryInfo& aMemoryInfo) {
-            return PerformanceInfoPromise::CreateAndResolve(
-                PerformanceInfo(url, pid, windowID, duration, perfId, true,
-                                isTopLevel, aMemoryInfo, items),
-                __func__);
-          },
-          [workerRef]() {
-            return PerformanceInfoPromise::CreateAndReject(NS_ERROR_FAILURE,
-                                                           __func__);
-          });
+  // This captures an unused reference to memoryUsagePromise because the worker
+  // can be released while this promise is still alive.
+  return memoryUsagePromise->Then(
+      GetCurrentSerialEventTarget(), __func__,
+      [url, pid, perfId, windowID, duration, isTopLevel,
+       items = std::move(items), _w = std::move(workerRef),
+       memoryUsagePromise](uint64_t jsMem) {
+        PerformanceMemoryInfo memInfo;
+        memInfo.GCHeapUsage() = jsMem;
+        return PerformanceInfoPromise::CreateAndResolve(
+            PerformanceInfo(url, pid, windowID, duration, perfId, true,
+                            isTopLevel, memInfo, items),
+            __func__);
+      },
+      []() {
+        return PerformanceInfoPromise::CreateAndReject(NS_ERROR_FAILURE,
+                                                       __func__);
+      });
 }
 
 }  // namespace mozilla::dom
