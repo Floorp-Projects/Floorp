3d38bdb78e5b48e49c4d283eaeb5462a14c725ad
trickypr <trickypr@icloud.com>
2022-11-07 12:33:29 +0000

    Bug 1510561 - Part 12: Apply `plugin:mozilla/valid-jsdoc` to `browser/extensions/report-site-issue`. r=webcompat-reviewers,twisniewski
    Depends on D161386
    
    Differential Revision: https://phabricator.services.mozilla.com/D161387

--- browser/extensions/report-site-issue/.eslintrc.js
+++ browser/extensions/report-site-issue/.eslintrc.js
@@ -5,29 +5,13 @@
 "use strict";
 
 module.exports = {
+  extends: ["plugin:mozilla/valid-jsdoc"],
+
   rules: {
     // Rules from the mozilla plugin
     "mozilla/balanced-listeners": "error",
     "mozilla/no-aArgs": "error",
     "mozilla/var-only-at-top-level": "error",
-
-    "valid-jsdoc": [
-      "error",
-      {
-        prefer: {
-          return: "returns",
-        },
-        preferType: {
-          Boolean: "boolean",
-          Number: "number",
-          String: "string",
-          bool: "boolean",
-        },
-        requireParamDescription: false,
-        requireReturn: false,
-        requireReturnDescription: false,
-      },
-    ],
 
     // No expressions where a statement is expected
     "no-unused-expressions": "error",
