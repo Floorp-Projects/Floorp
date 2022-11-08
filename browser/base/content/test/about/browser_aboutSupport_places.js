ddec285d64b89ab99d26cc2e6d62359e70fd5052
Jonathan Sudiaman <jsudiaman@mozilla.com>
2022-11-08 20:44:05 +0000

    Bug 1799540 - Android about:support Graphics and Media sections are blank r=Standard8
    `aboutSupport.js` was also missing a check for `AppConstants.MOZ_PLACES`.
    
    As Kelsey pointed out, it may be good to have some protective code when iterating through `snapshotFormatters` to avoid breaking the entire page when one of the formatters errors out.
    
    Differential Revision: https://phabricator.services.mozilla.com/D161593

---
+++ browser/base/content/test/about/browser_aboutSupport_places.js
@@ -0,0 +1,48 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+add_task(async function test_places_db_stats_table() {
+  await BrowserTestUtils.withNewTab(
+    { gBrowser, url: "about:support" },
+    async function(browser) {
+      const [
+        initialToggleText,
+        toggleTextAfterShow,
+        toggleTextAfterHide,
+      ] = await SpecialPowers.spawn(browser, [], async function() {
+        const toggleButton = content.document.getElementById(
+          "place-database-stats-toggle"
+        );
+        const getToggleText = () =>
+          content.document.l10n.getAttributes(toggleButton).id;
+        const toggleTexts = [];
+        const table = content.document.getElementById(
+          "place-database-stats-tbody"
+        );
+        await ContentTaskUtils.waitForCondition(
+          () => table.style.display === "none",
+          "Stats table is hidden initially"
+        );
+        toggleTexts.push(getToggleText());
+        toggleButton.click();
+        await ContentTaskUtils.waitForCondition(
+          () => table.style.display === "",
+          "Stats table is shown after first toggle"
+        );
+        toggleTexts.push(getToggleText());
+        toggleButton.click();
+        await ContentTaskUtils.waitForCondition(
+          () => table.style.display === "none",
+          "Stats table is hidden after second toggle"
+        );
+        toggleTexts.push(getToggleText());
+        return toggleTexts;
+      });
+      Assert.equal(initialToggleText, "place-database-stats-show");
+      Assert.equal(toggleTextAfterShow, "place-database-stats-hide");
+      Assert.equal(toggleTextAfterHide, "place-database-stats-show");
+    }
+  );
+});
