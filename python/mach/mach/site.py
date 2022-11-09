5d900ed76a299ec20e4081fb17e49feefbdcc7c7
ahochheiden <ahochheiden@mozilla.com>
2022-11-09 22:17:48 +0000

    Bug 1795158 - Apply `os.path.realpath` to `topsrcdir` and `topobjdir` in `MozbuildObject` and in `site.py` r=firefox-build-system-reviewers,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D161553

--- python/mach/mach/site.py
+++ python/mach/mach/site.py
@@ -783,12 +783,16 @@ class PythonVirtualenv:
         relative_path = path.relative_to(data_path)
 
         # Path to virtualenv's "site-packages" directory for provided sysconfig path
-        return os.path.normpath(os.path.normcase(Path(self.prefix) / relative_path))
+        return os.path.normpath(
+            os.path.normcase(os.path.realpath(Path(self.prefix) / relative_path))
+        )
 
     def site_packages_dirs(self):
         dirs = []
         if sys.platform.startswith("win"):
-            dirs.append(os.path.normpath(os.path.normcase(self.prefix)))
+            dirs.append(
+                os.path.normpath(os.path.normcase(os.path.realpath(self.prefix)))
+            )
         purelib = self.resolve_sysconfig_packages_path("purelib")
         platlib = self.resolve_sysconfig_packages_path("platlib")
 
