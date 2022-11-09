715505ef920e0306828a558aeebacab5e2a0514f
Emilio Cobos Álvarez <emilio@crisal.io>
2022-11-09 14:19:22 +0000

    Bug 1799580 - Remove display: -moz-popup. r=TYLin
    We already rely on it applying only to XULPopupElement's. Make it
    explicit and remove the CSS value.
    
    Differential Revision: https://phabricator.services.mozilla.com/D161552

--- layout/style/nsStyleConsts.h
+++ layout/style/nsStyleConsts.h
@@ -91,8 +91,6 @@ enum class StyleDisplay : uint16_t {
       StyleDisplayFrom(StyleDisplayOutside::Block, StyleDisplayInside::MozBox),
   MozInlineBox =
       StyleDisplayFrom(StyleDisplayOutside::Inline, StyleDisplayInside::MozBox),
-  MozPopup =
-      StyleDisplayFrom(StyleDisplayOutside::XUL, StyleDisplayInside::MozPopup),
 };
 // The order of the StyleDisplay values isn't meaningful.
 bool operator<(const StyleDisplay&, const StyleDisplay&) = delete;
