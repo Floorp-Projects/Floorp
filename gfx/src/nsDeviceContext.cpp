b7db06382dd32d2be49db32225e587c4ec87993c
Emilio Cobos Álvarez <emilio@crisal.io>
2022-11-09 07:59:46 +0000

    Bug 1799627 - Remove unnecessary fallback to primary screen in nsDeviceContext dpi code. r=tnikkel,layout-reviewers
    nsIWidget::GetDPI no longer returns negative values.
    
    Differential Revision: https://phabricator.services.mozilla.com/D161570

--- gfx/src/nsDeviceContext.cpp
+++ gfx/src/nsDeviceContext.cpp
@@ -75,10 +75,6 @@ void nsDeviceContext::SetDPI() {
     mAppUnitsPerDevPixelAtUnitFullZoom =
         NS_lround((AppUnitsPerCSSPixel() * 96) / dpi);
   } else {
-    RefPtr<const widget::Screen> primaryScreen =
-        ScreenManager::GetSingleton().GetPrimaryScreen();
-    MOZ_ASSERT(primaryScreen);
-
     // A value of -1 means use the maximum of 96 and the system DPI.
     // A value of 0 means use the system DPI. A positive value is used as the
     // DPI. This sets the physical size of a device pixel and thus controls the
@@ -87,13 +83,8 @@ void nsDeviceContext::SetDPI() {
     if (prefDPI > 0) {
       dpi = prefDPI;
     } else if (mWidget) {
-      // PuppetWidget could return -1 if the value's not available yet.
       dpi = mWidget->GetDPI();
-      // In case that the widget returns -1, use the primary screen's
-      // value as default.
-      if (dpi < 0) {
-        dpi = primaryScreen->GetDPI();
-      }
+      MOZ_ASSERT(dpi > 0);
       if (prefDPI < 0) {
         dpi = std::max(96.0f, dpi);
       }
