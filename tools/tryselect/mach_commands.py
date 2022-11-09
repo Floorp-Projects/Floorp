183cf57af8ec7892d952a8acbd9a5a18abb53c0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2022-11-09 13:49:11 +0000

    Bug 1768859 - Rename 'push' parameter to 'dry_run' following the changes from c52884b73e612a7202a951f15f3775f6c705e5a4. r=aryx DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D161674

--- tools/tryselect/mach_commands.py
+++ tools/tryselect/mach_commands.py
@@ -331,7 +331,7 @@ def try_fuzzy(command_context, **kwargs):
         # If saving preset without -q/--query, allow user to use the
         # interface to build the query.
         kwargs_copy = kwargs.copy()
-        kwargs_copy["push"] = False
+        kwargs_copy["dry_run"] = True
         kwargs_copy["save"] = None
         kwargs["query"] = run(command_context, save_query=True, **kwargs_copy)
         if not kwargs["query"]:
