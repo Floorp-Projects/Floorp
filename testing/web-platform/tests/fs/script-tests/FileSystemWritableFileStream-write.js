e18b55d891ff6b9ec46830c47c268ded49599270
Austin Sullivan <asully@chromium.org>
2022-11-10 15:52:24 +0000

    Bug 1798760 [wpt PR 36785] - FSA: Use strict JS in all WPT script files, a=testonly
    Automatic update from web-platform-tests
    FSA: Use strict JS in all WPT script files
    
    'use strict' is currently inconsistently used across the various FS
    WPTs.
    
    Change-Id: I9f132055ced6c5023beea5b421935e046144d82c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3999251
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1066644}
    
    --
    
    wpt-commits: 0872ef848702fc8097acc655e77953428bccdcc1
    wpt-pr: 36785

--- testing/web-platform/tests/fs/script-tests/FileSystemWritableFileStream-write.js
+++ testing/web-platform/tests/fs/script-tests/FileSystemWritableFileStream-write.js
@@ -1,3 +1,5 @@
+'use strict';
+
 directory_test(async (t, root) => {
   const handle = await createEmptyFile(t, 'empty_blob', root);
   const stream = await cleanup_writable(t, await handle.createWritable());
