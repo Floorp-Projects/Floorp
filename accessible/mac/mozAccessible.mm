5f89314af6880cbccc4dbb6372915f3a774b761c
Marian-Vasile Laza <mlaza@mozilla.com>
2022-11-10 22:49:02 +0200

    Backed out changeset 8003d7c416e1 (bug 1798950) for causing mochitest failures on browser_text_leaf.js. CLOSED TREE

--- accessible/mac/mozAccessible.mm
+++ accessible/mac/mozAccessible.mm
@@ -571,10 +571,8 @@ struct RoleDescrComparator {
 
   nsAutoString title;
   mGeckoAccessible->Name(title);
-  if (nsCoreUtils::IsWhitespaceString(title)) {
-    return @"";
-  }
 
+  title.CompressWhitespace();
   return nsCocoaUtils::ToNSString(title);
 
   NS_OBJC_END_TRY_BLOCK_RETURN(nil);
