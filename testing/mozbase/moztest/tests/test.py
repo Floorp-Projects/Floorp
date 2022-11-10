fc0d2bf2c15ccefbaef26a883363e3d17b3ff6d3
Geoff Brown <gbrown@mozilla.com>
2022-11-10 15:15:58 +0000

    Bug 1487153 - Convert moztest tests to pytest; r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D161632

--- testing/mozbase/moztest/tests/test.py
+++ testing/mozbase/moztest/tests/test.py
@@ -6,52 +6,50 @@ from __future__ import absolute_import
 
 import math
 import time
-import unittest
 
 import mozunit
-
+import pytest
 from moztest.results import TestContext, TestResult, TestResultCollection
 
 
-class Result(unittest.TestCase):
-    def test_results(self):
-        self.assertRaises(
-            AssertionError, lambda: TestResult("test", result_expected="hello")
-        )
-        t = TestResult("test")
-        self.assertRaises(ValueError, lambda: t.finish(result="good bye"))
-
-    def test_time(self):
-        now = time.time()
-        t = TestResult("test")
-        time.sleep(1)
-        t.finish("PASS")
-        duration = time.time() - now
-        self.assertTrue(math.fabs(duration - t.duration) < 1)
-
-    def test_custom_time(self):
-        t = TestResult("test", time_start=0)
-        t.finish(result="PASS", time_end=1000)
-        self.assertEqual(t.duration, 1000)
+def test_results():
+    with pytest.raises(AssertionError):
+        TestResult("test", result_expected="hello")
+    t = TestResult("test")
+    with pytest.raises(ValueError):
+        t.finish(result="good bye")
 
 
-class Collection(unittest.TestCase):
-    def setUp(self):
-        c1 = TestContext("host1")
-        c2 = TestContext("host2")
-        c3 = TestContext("host2")
-        c4 = TestContext("host1")
+def test_time():
+    now = time.time()
+    t = TestResult("test")
+    time.sleep(1)
+    t.finish("PASS")
+    duration = time.time() - now
+    assert math.fabs(duration - t.duration) < 1
 
-        t1 = TestResult("t1", context=c1)
-        t2 = TestResult("t2", context=c2)
-        t3 = TestResult("t3", context=c3)
-        t4 = TestResult("t4", context=c4)
 
-        self.collection = TestResultCollection("tests")
-        self.collection.extend([t1, t2, t3, t4])
+def test_custom_time():
+    t = TestResult("test", time_start=0)
+    t.finish(result="PASS", time_end=1000)
+    assert t.duration == 1000
 
-    def test_unique_contexts(self):
-        self.assertEqual(len(self.collection.contexts), 2)
+
+def test_unique_contexts():
+    c1 = TestContext("host1")
+    c2 = TestContext("host2")
+    c3 = TestContext("host2")
+    c4 = TestContext("host1")
+
+    t1 = TestResult("t1", context=c1)
+    t2 = TestResult("t2", context=c2)
+    t3 = TestResult("t3", context=c3)
+    t4 = TestResult("t4", context=c4)
+
+    collection = TestResultCollection("tests")
+    collection.extend([t1, t2, t3, t4])
+
+    assert len(collection.contexts) == 2
 
 
 if __name__ == "__main__":
