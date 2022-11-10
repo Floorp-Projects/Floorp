fec05974fef977eefd24d0f30b39dab69e7e24f8
Julian Descottes <jdescottes@mozilla.com>
2022-11-10 08:07:53 +0000

    Bug 1788773 - [devtools] Migrate CacheEntry to a JavaScript class r=ochameau
    CacheEntry was still created as a function, migrate it to a regular class.
    
    Differential Revision: https://phabricator.services.mozilla.com/D161314

--- devtools/shared/platform/CacheEntry.sys.mjs
+++ devtools/shared/platform/CacheEntry.sys.mjs
@@ -10,105 +10,105 @@ ChromeUtils.defineESModuleGetters(lazy, {
 });
 
 /**
- * Module to fetch cache objects from CacheStorageService
- * and return them as an object.
+ * Flag for cache session being initialized.
  */
-export const CacheEntry = {
-  /**
-   * Flag for cache session being initialized.
-   */
-  isCacheSessionInitialized: false,
-  /**
-   * Cache session object.
-   */
-  cacheSession: null,
+let isCacheSessionInitialized = false;
 
-  /**
-   * Initializes our cache session / cache storage session.
-   */
-  initializeCacheSession(request) {
-    try {
-      const cacheService = Services.cache2;
-      if (cacheService) {
-        let loadContext = lazy.NetworkHelper.getRequestLoadContext(request);
-        if (!loadContext) {
-          // Get default load context if we can't fetch.
-          loadContext = Services.loadContextInfo.default;
-        }
-        this.cacheSession = cacheService.diskCacheStorage(loadContext);
-        this.isCacheSessionInitialized = true;
+/**
+ * Cache session object.
+ */
+let cacheSession = null;
+
+/**
+ * Initializes our cache session / cache storage session.
+ */
+function initializeCacheSession(request) {
+  try {
+    const cacheService = Services.cache2;
+    if (cacheService) {
+      let loadContext = lazy.NetworkHelper.getRequestLoadContext(request);
+      if (!loadContext) {
+        // Get default load context if we can't fetch.
+        loadContext = Services.loadContextInfo.default;
       }
-    } catch (e) {
-      this.isCacheSessionInitialized = false;
+      cacheSession = cacheService.diskCacheStorage(loadContext);
+      isCacheSessionInitialized = true;
     }
-  },
+  } catch (e) {
+    isCacheSessionInitialized = false;
+  }
+}
 
-  /**
-   * Parses a cache descriptor returned from the backend into a
-   * usable object.
-   *
-   * @param Object descriptor The descriptor from the backend.
-   */
-  parseCacheDescriptor(descriptor) {
-    const descriptorObj = {};
-    try {
-      if (descriptor.storageDataSize) {
-        descriptorObj.dataSize = descriptor.storageDataSize;
+/**
+ * Parses a cache entry returned from the backend to build a response cache
+ * object.
+ *
+ * @param {nsICacheEntry} cacheEntry
+ *     The cache entry from the backend.
+ *
+ * @returns {Object}
+ *     A responseCache object expected by RDP.
+ */
+function buildResponseCacheObject(cacheEntry) {
+  const cacheObject = {};
+  try {
+    if (cacheEntry.storageDataSize) {
+      cacheObject.dataSize = cacheEntry.storageDataSize;
+    }
+  } catch (e) {
+    // We just need to handle this in case it's a js file of 0B.
+  }
+  if (cacheEntry.expirationTime) {
+    cacheObject.expires = cacheEntry.expirationTime;
+  }
+  if (cacheEntry.fetchCount) {
+    cacheObject.fetchCount = cacheEntry.fetchCount;
+  }
+  if (cacheEntry.lastFetched) {
+    cacheObject.lastFetched = cacheEntry.lastFetched;
+  }
+  if (cacheEntry.lastModified) {
+    cacheObject.lastModified = cacheEntry.lastModified;
+  }
+  if (cacheEntry.deviceID) {
+    cacheObject.device = cacheEntry.deviceID;
+  }
+  return cacheObject;
+}
+
+/**
+ * Does the fetch for the cache entry from the session.
+ *
+ * @param string request
+ *        The request object.
+ * @param Function onCacheEntryAvailable
+ *        callback function.
+ */
+export function getCacheEntry(request, onCacheEntryAvailable) {
+  if (!isCacheSessionInitialized) {
+    initializeCacheSession(request);
+  }
+
+  if (cacheSession) {
+    cacheSession.asyncOpenURI(
+      request.URI,
+      "",
+      Ci.nsICacheStorage.OPEN_SECRETLY,
+      {
+        onCacheEntryCheck: entry => {
+          return Ci.nsICacheEntryOpenCallback.ENTRY_WANTED;
+        },
+        onCacheEntryAvailable: (cacheEntry, isnew, status) => {
+          if (cacheEntry) {
+            const cacheObject = buildResponseCacheObject(cacheEntry);
+            onCacheEntryAvailable(cacheObject);
+          } else {
+            onCacheEntryAvailable(null);
+          }
+        },
       }
-    } catch (e) {
-      // We just need to handle this in case it's a js file of 0B.
-    }
-    if (descriptor.expirationTime) {
-      descriptorObj.expires = descriptor.expirationTime;
-    }
-    if (descriptor.fetchCount) {
-      descriptorObj.fetchCount = descriptor.fetchCount;
-    }
-    if (descriptor.lastFetched) {
-      descriptorObj.lastFetched = descriptor.lastFetched;
-    }
-    if (descriptor.lastModified) {
-      descriptorObj.lastModified = descriptor.lastModified;
-    }
-    if (descriptor.deviceID) {
-      descriptorObj.device = descriptor.deviceID;
-    }
-    return descriptorObj;
-  },
-
-  /**
-   * Does the fetch for the cache descriptor from the session.
-   *
-   * @param string request
-   *        The request object.
-   * @param Function onCacheDescriptorAvailable
-   *        callback function.
-   */
-  getCacheEntry(request, onCacheDescriptorAvailable) {
-    if (!this.isCacheSessionInitialized) {
-      this.initializeCacheSession(request);
-    }
-    if (this.cacheSession) {
-      this.cacheSession.asyncOpenURI(
-        request.URI,
-        "",
-        Ci.nsICacheStorage.OPEN_SECRETLY,
-        {
-          onCacheEntryCheck: entry => {
-            return Ci.nsICacheEntryOpenCallback.ENTRY_WANTED;
-          },
-          onCacheEntryAvailable: (descriptor, isnew, status) => {
-            if (descriptor) {
-              const descriptorObj = this.parseCacheDescriptor(descriptor);
-              onCacheDescriptorAvailable(descriptorObj);
-            } else {
-              onCacheDescriptorAvailable(null);
-            }
-          },
-        }
-      );
-    } else {
-      onCacheDescriptorAvailable(null);
-    }
-  },
-};
+    );
+  } else {
+    onCacheEntryAvailable(null);
+  }
+}
