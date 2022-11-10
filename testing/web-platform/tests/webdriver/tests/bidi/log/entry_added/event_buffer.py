0a7eb35bfda51c72ce06fd8cdca3a34a08f62519
Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>
2022-11-10 15:53:43 +0000

    Bug 1796769 [wpt PR 36588] - Don't clear cache on Context refresh, a=testonly
    Automatic update from web-platform-tests
    Don't clear cache on Context refresh (#36588)
    
    * Don't clear cache on Context refresh.
    
    Co-authored-by: Maksim Sadym <sadym@google.com>
    --
    
    wpt-commits: 75a804e18765a3a428fd5b8b62ffcb269c2f41d4
    wpt-pr: 36588

--- testing/web-platform/tests/webdriver/tests/bidi/log/entry_added/event_buffer.py
+++ testing/web-platform/tests/webdriver/tests/bidi/log/entry_added/event_buffer.py
@@ -8,19 +8,13 @@ from . import assert_base_entry, create_log
 @pytest.mark.asyncio
 @pytest.mark.parametrize("log_type", ["console_api_log", "javascript_error"])
 async def test_console_log_cached_messages(
-    bidi_session, wait_for_event, log_type, top_context
+    bidi_session, wait_for_event, log_type, new_tab
 ):
     # Unsubscribe in case previous tests subscribed to log.entryAdded
     await bidi_session.session.unsubscribe(events=["log.entryAdded"])
 
-    # Refresh to make sure no events are cached for the current window global
-    # from previous tests.
-    await bidi_session.browsing_context.navigate(
-        context=top_context["context"], url=top_context["url"], wait="complete"
-    )
-
     # Log a message before subscribing
-    expected_text = await create_log(bidi_session, top_context, log_type, "cached_message")
+    expected_text = await create_log(bidi_session, new_tab, log_type, "cached_message")
 
     # Track all received log.entryAdded events in the events array
     events = []
@@ -31,43 +25,37 @@ async def test_console_log_cached_messages(
     remove_listener = bidi_session.add_event_listener("log.entryAdded", on_event)
 
     # Subscribe
-    on_entry_added = wait_for_event("log.entryAdded")
     await bidi_session.session.subscribe(events=["log.entryAdded"])
-    await on_entry_added
+    # Cached events are emitted before the subscribe command is finished.
     assert len(events) == 1
 
     # Check the log.entryAdded event received has the expected properties.
-    assert_base_entry(events[0], text=expected_text, context=top_context["context"])
+    assert_base_entry(events[0], text=expected_text, context=new_tab["context"])
 
     # Unsubscribe and re-subscribe
     await bidi_session.session.unsubscribe(events=["log.entryAdded"])
     await bidi_session.session.subscribe(events=["log.entryAdded"])
 
-    # Wait for some time to catch all messages.
-    await asyncio.sleep(0.5)
-
     # Check that the cached event was not re-emitted.
     assert len(events) == 1
 
     on_entry_added = wait_for_event("log.entryAdded")
-    expected_text = await create_log(bidi_session, top_context, log_type, "live_message")
+    expected_text = await create_log(bidi_session, new_tab, log_type, "live_message")
     await on_entry_added
 
     # Check that we only received the live message.
     assert len(events) == 2
-    assert_base_entry(events[1], text=expected_text, context=top_context["context"])
+    assert_base_entry(events[1], text=expected_text, context=new_tab["context"])
 
     # Unsubscribe, log a message and re-subscribe
     await bidi_session.session.unsubscribe(events=["log.entryAdded"])
-    expected_text = await create_log(bidi_session, top_context, log_type, "cached_message_2")
+    expected_text = await create_log(bidi_session, new_tab, log_type, "cached_message_2")
 
-    on_entry_added = wait_for_event("log.entryAdded")
     await bidi_session.session.subscribe(events=["log.entryAdded"])
-    await on_entry_added
 
     # Check that only the newly cached event was emitted
     assert len(events) == 3
-    assert_base_entry(events[2], text=expected_text, context=top_context["context"])
+    assert_base_entry(events[2], text=expected_text, context=new_tab["context"])
 
     remove_listener()
 
@@ -75,16 +63,10 @@ async def test_console_log_cached_messages(
 @pytest.mark.asyncio
 @pytest.mark.parametrize("log_type", ["console_api_log", "javascript_error"])
 async def test_console_log_cached_message_after_refresh(
-    bidi_session, wait_for_event, top_context, log_type
+    bidi_session, wait_for_event, new_tab, log_type
 ):
     # Unsubscribe in case previous tests subscribed to log.entryAdded
     await bidi_session.session.unsubscribe(events=["log.entryAdded"])
-
-    # Refresh to make sure no events are cached for the current window global
-    # from previous tests.
-    await bidi_session.browsing_context.navigate(
-        context=top_context["context"], url=top_context["url"], wait="complete"
-    )
 
     # Track all received log.entryAdded events in the events array
     events = []
@@ -95,21 +77,17 @@ async def test_console_log_cached_message_after_refresh(
     remove_listener = bidi_session.add_event_listener("log.entryAdded", on_event)
 
     # Log a message, refresh, log another message and subscribe
-    await create_log(bidi_session, top_context, log_type, "missed_message")
+    expected_text_1 = await create_log(bidi_session, new_tab, log_type, "cached_message_1")
     await bidi_session.browsing_context.navigate(
-        context=top_context["context"], url=top_context["url"], wait="complete"
+        context=new_tab["context"], url=new_tab["url"], wait="complete"
     )
-    expected_text = await create_log(bidi_session, top_context, log_type, "cached_message")
+    expected_text_2 = await create_log(bidi_session, new_tab, log_type, "cached_message_2")
 
-    on_entry_added = wait_for_event("log.entryAdded")
     await bidi_session.session.subscribe(events=["log.entryAdded"])
-    await on_entry_added
-
-    # Wait for some time to catch all messages.
-    await asyncio.sleep(0.5)
 
     # Check that only the cached message was retrieved.
-    assert len(events) == 1
-    assert_base_entry(events[0], text=expected_text)
+    assert len(events) == 2
+    assert_base_entry(events[0], text=expected_text_1)
+    assert_base_entry(events[1], text=expected_text_2)
 
     remove_listener()
