60e139bf6ced005f6e8567bb9833ab88edabfc6b
Kershaw Chang <kershaw@mozilla.com>
2022-11-10 12:46:43 +0000

    Bug 1755660 - Add tests for testing HTTP/2 to the origin server through a HTTP/2 proxy, r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D150627

--- netwerk/test/unit/test_http2_with_proxy.js
+++ netwerk/test/unit/test_http2_with_proxy.js
@@ -1,58 +1,15 @@
-/* import-globals-from http2_test_common.js */
+// test HTTP/2 with a HTTP/2 prooxy
 
-const { HttpServer } = ChromeUtils.import("resource://testing-common/httpd.js");
+"use strict";
+
+/* import-globals-from http2_test_common.js */
+/* import-globals-from head_servers.js */
 
 var concurrent_channels = [];
-var httpserv = null;
-var httpserv2 = null;
 
 var loadGroup;
 var serverPort;
-
-function altsvcHttp1Server(metadata, response) {
-  response.setStatusLine(metadata.httpVersion, 200, "OK");
-  response.setHeader("Content-Type", "text/plain", false);
-  response.setHeader("Connection", "close", false);
-  response.setHeader("Alt-Svc", 'h2=":' + serverPort + '"', false);
-  var body = "this is where a cool kid would write something neat.\n";
-  response.bodyOutputStream.write(body, body.length);
-}
-
-function h1ServerWK(metadata, response) {
-  response.setStatusLine(metadata.httpVersion, 200, "OK");
-  response.setHeader("Content-Type", "application/json", false);
-  response.setHeader("Connection", "close", false);
-  response.setHeader("Cache-Control", "no-cache", false);
-  response.setHeader("Access-Control-Allow-Origin", "*", false);
-  response.setHeader("Access-Control-Allow-Method", "GET", false);
-
-  var body = '["http://foo.example.com:' + httpserv.identity.primaryPort + '"]';
-  response.bodyOutputStream.write(body, body.length);
-}
-
-function altsvcHttp1Server2(metadata, response) {
-  // this server should never be used thanks to an alt svc frame from the
-  // h2 server.. but in case of some async lag in setting the alt svc route
-  // up we have it.
-  response.setStatusLine(metadata.httpVersion, 200, "OK");
-  response.setHeader("Content-Type", "text/plain", false);
-  response.setHeader("Connection", "close", false);
-  var body = "hanging.\n";
-  response.bodyOutputStream.write(body, body.length);
-}
-
-function h1ServerWK2(metadata, response) {
-  response.setStatusLine(metadata.httpVersion, 200, "OK");
-  response.setHeader("Content-Type", "application/json", false);
-  response.setHeader("Connection", "close", false);
-  response.setHeader("Cache-Control", "no-cache", false);
-  response.setHeader("Access-Control-Allow-Origin", "*", false);
-  response.setHeader("Access-Control-Allow-Method", "GET", false);
-
-  var body =
-    '["http://foo.example.com:' + httpserv2.identity.primaryPort + '"]';
-  response.bodyOutputStream.write(body, body.length);
-}
+var proxy;
 
 add_setup(async function setup() {
   var env = Cc["@mozilla.org/process/environment;1"].getService(
@@ -64,7 +21,6 @@ add_setup(async function setup() {
 
   // Set to allow the cert presented by our H2 server
   do_get_profile();
-
   Services.prefs.setIntPref("network.http.speculative-parallel-limit", 0);
 
   // The moz-http2 cert is for foo.example.com and is signed by http2-ca.pem
@@ -75,6 +31,7 @@ add_setup(async function setup() {
     Ci.nsIX509CertDB
   );
   addCertFromFile(certdb, "http2-ca.pem", "CTu,u,u");
+  addCertFromFile(certdb, "proxy-ca.pem", "CTu,u,u");
 
   Services.prefs.setBoolPref("network.http.http2.enabled", true);
   Services.prefs.setBoolPref("network.http.http2.allow-push", true);
@@ -93,25 +50,13 @@ add_setup(async function setup() {
     Ci.nsILoadGroup
   );
 
-  httpserv = new HttpServer();
-  httpserv.registerPathHandler("/altsvc1", altsvcHttp1Server);
-  httpserv.registerPathHandler("/.well-known/http-opportunistic", h1ServerWK);
-  httpserv.start(-1);
-  httpserv.identity.setPrimary(
-    "http",
-    "foo.example.com",
-    httpserv.identity.primaryPort
+  Services.prefs.setStringPref(
+    "services.settings.server",
+    `data:text/html,test`
   );
 
-  httpserv2 = new HttpServer();
-  httpserv2.registerPathHandler("/altsvc2", altsvcHttp1Server2);
-  httpserv2.registerPathHandler("/.well-known/http-opportunistic", h1ServerWK2);
-  httpserv2.start(-1);
-  httpserv2.identity.setPrimary(
-    "http",
-    "foo.example.com",
-    httpserv2.identity.primaryPort
-  );
+  proxy = new NodeHTTP2ProxyServer();
+  await proxy.start();
 });
 
 registerCleanupFunction(async () => {
@@ -124,8 +69,8 @@ registerCleanupFunction(async () => {
   Services.prefs.clearUserPref(
     "network.cookieJarSettings.unblocked_for_testing"
   );
-  await httpserv.stop();
-  await httpserv2.stop();
+
+  await proxy.stop();
 });
 
 // hack - the header test resets the multiplex object on the server,
@@ -137,12 +82,12 @@ add_task(async function do_test_http2_post_big() {
   const { httpProxyConnectResponseCode } = await test_http2_post_big(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_basic() {
   const { httpProxyConnectResponseCode } = await test_http2_basic(serverPort);
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_concurrent() {
@@ -150,7 +95,7 @@ add_task(async function do_test_http2_concurrent() {
     concurrent_channels,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_concurrent_post() {
@@ -158,19 +103,19 @@ add_task(async function do_test_http2_concurrent_post() {
     concurrent_channels,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_basic_unblocked_dep() {
   const { httpProxyConnectResponseCode } = await test_http2_basic_unblocked_dep(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_nospdy() {
   const { httpProxyConnectResponseCode } = await test_http2_nospdy(serverPort);
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push1() {
@@ -178,7 +123,7 @@ add_task(async function do_test_http2_push1() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push2() {
@@ -186,7 +131,7 @@ add_task(async function do_test_http2_push2() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push3() {
@@ -194,7 +139,7 @@ add_task(async function do_test_http2_push3() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push4() {
@@ -202,7 +147,7 @@ add_task(async function do_test_http2_push4() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push5() {
@@ -210,7 +155,7 @@ add_task(async function do_test_http2_push5() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push6() {
@@ -218,23 +163,14 @@ add_task(async function do_test_http2_push6() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
-});
-
-add_task(async function do_test_http2_altsvc() {
-  const { httpProxyConnectResponseCode } = await test_http2_altsvc(
-    httpserv.identity.primaryPort,
-    httpserv2.identity.primaryPort,
-    false
-  );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_doubleheader() {
   const { httpProxyConnectResponseCode } = await test_http2_doubleheader(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_xhr() {
@@ -243,57 +179,57 @@ add_task(async function do_test_http2_xhr() {
 
 add_task(async function do_test_http2_header() {
   const { httpProxyConnectResponseCode } = await test_http2_header(serverPort);
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_invalid_response_header() {
   const {
     httpProxyConnectResponseCode,
   } = await test_http2_invalid_response_header(serverPort);
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_cookie_crumbling() {
   const { httpProxyConnectResponseCode } = await test_http2_cookie_crumbling(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_multiplex() {
   var values = await test_http2_multiplex(serverPort);
-  Assert.equal(values[0].httpProxyConnectResponseCode, -1);
-  Assert.equal(values[1].httpProxyConnectResponseCode, -1);
+  Assert.equal(values[0].httpProxyConnectResponseCode, 200);
+  Assert.equal(values[1].httpProxyConnectResponseCode, 200);
   Assert.notEqual(values[0].streamID, values[1].streamID);
 });
 
 add_task(async function do_test_http2_big() {
   const { httpProxyConnectResponseCode } = await test_http2_big(serverPort);
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_huge_suspended() {
   const { httpProxyConnectResponseCode } = await test_http2_huge_suspended(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_post() {
   const { httpProxyConnectResponseCode } = await test_http2_post(serverPort);
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_empty_post() {
   const { httpProxyConnectResponseCode } = await test_http2_empty_post(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_patch() {
   const { httpProxyConnectResponseCode } = await test_http2_patch(serverPort);
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_pushapi_1() {
@@ -301,7 +237,7 @@ add_task(async function do_test_http2_pushapi_1() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 0);
 });
 
 add_task(async function do_test_http2_continuations() {
@@ -309,28 +245,28 @@ add_task(async function do_test_http2_continuations() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 0);
 });
 
 add_task(async function do_test_http2_blocking_download() {
   const { httpProxyConnectResponseCode } = await test_http2_blocking_download(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_illegalhpacksoft() {
   const { httpProxyConnectResponseCode } = await test_http2_illegalhpacksoft(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_illegalhpackhard() {
   const { httpProxyConnectResponseCode } = await test_http2_illegalhpackhard(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_folded_header() {
@@ -338,21 +274,21 @@ add_task(async function do_test_http2_folded_header() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_empty_data() {
   const { httpProxyConnectResponseCode } = await test_http2_empty_data(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_status_phrase() {
   const { httpProxyConnectResponseCode } = await test_http2_status_phrase(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_doublepush() {
@@ -360,7 +296,7 @@ add_task(async function do_test_http2_doublepush() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_disk_cache_push() {
@@ -368,7 +304,7 @@ add_task(async function do_test_http2_disk_cache_push() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_h11required_stream() {
@@ -378,35 +314,45 @@ add_task(async function do_test_http2_h11required_stream() {
   const { httpProxyConnectResponseCode } = await test_http2_h11required_stream(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_h11required_session() {
   const { httpProxyConnectResponseCode } = await test_http2_h11required_session(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_retry_rst() {
   const { httpProxyConnectResponseCode } = await test_http2_retry_rst(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_wrongsuite_tls12() {
+  // For this test we need to start HTTPS 1.1 proxy because HTTP/2 proxy cannot be used.
+  proxy.unregisterFilter();
+  let proxyHttp1 = new NodeHTTPSProxyServer();
+  await proxyHttp1.start();
+  proxyHttp1.registerFilter();
+  registerCleanupFunction(() => {
+    proxyHttp1.stop();
+  });
   const { httpProxyConnectResponseCode } = await test_http2_wrongsuite_tls12(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
+  proxyHttp1.unregisterFilter();
+  proxy.registerFilter();
 });
 
 add_task(async function do_test_http2_wrongsuite_tls13() {
   const { httpProxyConnectResponseCode } = await test_http2_wrongsuite_tls13(
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push_firstparty1() {
@@ -414,7 +360,7 @@ add_task(async function do_test_http2_push_firstparty1() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push_firstparty2() {
@@ -422,7 +368,7 @@ add_task(async function do_test_http2_push_firstparty2() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push_firstparty3() {
@@ -430,7 +376,7 @@ add_task(async function do_test_http2_push_firstparty3() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push_userContext1() {
@@ -438,7 +384,7 @@ add_task(async function do_test_http2_push_userContext1() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push_userContext2() {
@@ -446,7 +392,7 @@ add_task(async function do_test_http2_push_userContext2() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
 
 add_task(async function do_test_http2_push_userContext3() {
@@ -454,5 +400,5 @@ add_task(async function do_test_http2_push_userContext3() {
     loadGroup,
     serverPort
   );
-  Assert.equal(httpProxyConnectResponseCode, -1);
+  Assert.equal(httpProxyConnectResponseCode, 200);
 });
