d80d4faa0ee2b5b5bfc30df819f69290c41055bb
stransky <stransky@redhat.com>
2022-11-10 20:00:06 +0000

    Bug 1793507 [FFVPX] Add av1_frame_split filter for AV1 VA-API decode r=alwu
    Depends on D161785
    
    Differential Revision: https://phabricator.services.mozilla.com/D161786

--- media/ffvpx/libavcodec/bsf_list.c
+++ media/ffvpx/libavcodec/bsf_list.c
@@ -1,8 +1,11 @@
 #include "config_components.h"
 
 static const FFBitStreamFilter * const bitstream_filters[] = {
-    &ff_null_bsf,
 #if CONFIG_VP9_SUPERFRAME_SPLIT_BSF
     &ff_vp9_superframe_split_bsf,
 #endif
+#if CONFIG_AV1_VAAPI_HWACCEL
+    &ff_av1_frame_split_bsf,
+#endif
+    &ff_null_bsf,
     NULL };
