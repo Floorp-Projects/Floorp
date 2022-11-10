ad2dfe247c3e16a9ece8d052650c0e751672a171
Marian-Vasile Laza <mlaza@mozilla.com>
2022-11-11 01:42:31 +0200

    Backed out changeset 12f5f2d2aac9 (bug 1785125) for causing mochitest failures on test_basics.html. CLOSED TREE

--- dom/fs/api/FileSystemManager.cpp
+++ dom/fs/api/FileSystemManager.cpp
@@ -89,18 +89,13 @@ void FileSystemManager::BeginRequest(
     std::function<void(const RefPtr<FileSystemManagerChild>&)>&& aSuccess,
     std::function<void(nsresult)>&& aFailure) {
   MOZ_ASSERT(!mShutdown);
-  MOZ_ASSERT(mGlobal);
-
-  // Check if we're allowed to use storage
-  if (mGlobal->GetStorageAccess() < StorageAccess::eSessionScoped) {
-    aFailure(NS_ERROR_DOM_SECURITY_ERR);
-    return;
-  }
 
   if (mBackgroundRequestHandler->FileSystemManagerChildStrongRef()) {
     aSuccess(mBackgroundRequestHandler->FileSystemManagerChildStrongRef());
     return;
   }
+
+  MOZ_ASSERT(mGlobal);
 
   QM_TRY_INSPECT(const auto& principalInfo, GetPrincipalInfo(mGlobal), QM_VOID,
                  [&aFailure](nsresult rv) { aFailure(rv); });
