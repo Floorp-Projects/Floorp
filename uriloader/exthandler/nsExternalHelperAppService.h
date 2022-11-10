1f436298d655aae85b35fc7931a7719c69aad8c5
Neil Deakin <neil@mozilla.com>
2022-11-10 20:44:31 +0000

    Bug 1796395, remove now unneeded downloads.helpertype telemetry probe, r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D161825

--- uriloader/exthandler/nsExternalHelperAppService.h
+++ uriloader/exthandler/nsExternalHelperAppService.h
@@ -313,9 +313,6 @@ class nsExternalAppHandler final : public nsIStreamListener,
   void SetShouldCloseWindow() { mShouldCloseWindow = true; }
 
  protected:
-  // Record telemetry about a download that was attempted.
-  void RecordDownloadTelemetry(nsIChannel* aChannel, const char* aAction);
-
   bool IsDownloadSpam(nsIChannel* aChannel);
 
   ~nsExternalAppHandler();
