eae0527456977aebe43b9a242fd2d40c987fba1c
Butkovits Atila <abutkovits@mozilla.com>
2022-11-11 12:56:24 +0200

    Bug 1800048 - Lint fix. a=fix

--- accessible/tests/browser/e10s/browser_caching_relations.js
+++ accessible/tests/browser/e10s/browser_caching_relations.js
@@ -219,7 +219,9 @@ addAccessibleTask(
     chrome: true,
     // IA2 doesn't have a LINKS_TO relation and Windows non-cached
     // RemoteAccessible uses IA2, so we can't run these tests in this case.
-    topLevel: !isWinNoCache, iframe: !isWinNoCache, remoteIframe: !isWinNoCache
+    topLevel: !isWinNoCache,
+    iframe: !isWinNoCache,
+    remoteIframe: !isWinNoCache,
   }
 );
 
