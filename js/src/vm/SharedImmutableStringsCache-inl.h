21dbe211cc22029fc77fbe77145ee3ef35163bbb
Tooru Fujisawa <arai_a@mac.com>
2022-11-11 06:30:41 +0000

    Bug 1786834 - Part 1: Move SharedImmutableStringsCache out of JSRuntime and make it singleton. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D160204

--- js/src/vm/SharedImmutableStringsCache-inl.h
+++ js/src/vm/SharedImmutableStringsCache-inl.h
@@ -14,11 +14,10 @@ namespace js {
 template <typename IntoOwnedChars>
 [[nodiscard]] SharedImmutableString SharedImmutableStringsCache::getOrCreate(
     const char* chars, size_t length, IntoOwnedChars intoOwnedChars) {
-  MOZ_ASSERT(inner_);
   MOZ_ASSERT(chars);
   Hasher::Lookup lookup(Hasher::hashLongString(chars, length), chars, length);
 
-  auto locked = inner_->lock();
+  auto locked = inner_.lock();
   auto entry = locked->set.lookupForAdd(lookup);
   if (!entry) {
     OwnedChars ownedChars(intoOwnedChars());
@@ -27,7 +26,7 @@ template <typename IntoOwnedChars>
     }
     MOZ_ASSERT(ownedChars.get() == chars ||
                memcmp(ownedChars.get(), chars, length) == 0);
-    auto box = StringBox::Create(std::move(ownedChars), length, this->inner_);
+    auto box = StringBox::Create(std::move(ownedChars), length, &inner_);
     if (!box || !locked->set.add(entry, std::move(box))) {
       return SharedImmutableString();
     }
@@ -42,13 +41,12 @@ template <typename IntoOwnedTwoByteChars>
 SharedImmutableStringsCache::getOrCreate(
     const char16_t* chars, size_t length,
     IntoOwnedTwoByteChars intoOwnedTwoByteChars) {
-  MOZ_ASSERT(inner_);
   MOZ_ASSERT(chars);
   auto hash = Hasher::hashLongString(reinterpret_cast<const char*>(chars),
                                      length * sizeof(char16_t));
   Hasher::Lookup lookup(hash, chars, length);
 
-  auto locked = inner_->lock();
+  auto locked = inner_.lock();
   auto entry = locked->set.lookupForAdd(lookup);
   if (!entry) {
     OwnedTwoByteChars ownedTwoByteChars(intoOwnedTwoByteChars());
@@ -60,7 +58,7 @@ SharedImmutableStringsCache::getOrCreate(
         memcmp(ownedTwoByteChars.get(), chars, length * sizeof(char16_t)) == 0);
     OwnedChars ownedChars(reinterpret_cast<char*>(ownedTwoByteChars.release()));
     auto box = StringBox::Create(std::move(ownedChars),
-                                 length * sizeof(char16_t), this->inner_);
+                                 length * sizeof(char16_t), &inner_);
     if (!box || !locked->set.add(entry, std::move(box))) {
       return SharedImmutableTwoByteString();
     }
