d573a8f81c7624a601a7ae414d6a25b854d8b355
Emilio Cobos Álvarez <emilio@crisal.io>
2022-11-14 17:21:48 +0000

    Bug 1800416 - Ensure setting fullscreen actually ends up with a fullscreen sizemode. r=mhowell, a=dsmith
    When minimizing a fullscreen window, frame state is kept with
    mFullscreenMode = true but mSizeMode = nsSizeMode_Minimized.
    
    Calling EnsureSizeMode(nsSizeMode_Fullscreen) in this state would call
    EnsureFullscreenMode(true), but that'd bail out without actually
    setting the fullscreen sizemode, causing confusion.
    
    Before the regressing patch that was papered over because
    nsSizeMode_Fullscreen was passed explicitly to OnSizeModeChange(), but
    the underlying state was already wrong before my patch.
    
    Add some comments and documentation to the fullscreen-relevant members,
    since initially I was rather confused about this code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D161982

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -474,7 +474,12 @@ class nsWindow final : public nsBaseWidget {
 
     nsSizeMode mSizeMode = nsSizeMode_Normal;
     nsSizeMode mLastSizeMode = nsSizeMode_Normal;
+    // The old size mode before going into fullscreen mode. This should never
+    // be nsSizeMode_Fullscreen.
     nsSizeMode mOldSizeMode = nsSizeMode_Normal;
+    // Whether we're in fullscreen. We need to keep this state out of band,
+    // rather than just using mSizeMode, because a window can be minimized
+    // while fullscreen, and we don't store the fullscreen state anywhere else.
     bool mFullscreenMode = false;
     nsWindow* mWindow;
   };
