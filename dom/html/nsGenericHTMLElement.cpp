8b667f27db66956106b979f6862fcb6d08fe64c5
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-11-14 14:44:14 +0000

    Bug 1799565 - Allow pointer events on disabled form elements on Nightly r=smaug
    Corresponds to the latest consensus and also matches what Chrome shipped behind `--enable-blink-features=SendMouseEventsDisabledFormControls`.
    
    Imported the portion of tests that is directly impacted here from https://github.com/web-platform-tests/wpt/pull/32381. Others are not directly impacted and thus I'd like to land them separately since there are still some mismatching behavior around `button` element.
    
    Differential Revision: https://phabricator.services.mozilla.com/D161537

--- dom/html/nsGenericHTMLElement.cpp
+++ dom/html/nsGenericHTMLElement.cpp
@@ -1968,6 +1968,12 @@ bool nsGenericHTMLFormElement::IsElementDisabledForEvents(WidgetEvent* aEvent,
     case eLegacyMouseLineOrPageScroll:
     case eLegacyMousePixelScroll:
       return false;
+    case ePointerDown:
+    case ePointerUp:
+    case ePointerCancel:
+    case ePointerGotCapture:
+    case ePointerLostCapture:
+      return !StaticPrefs::dom_forms_always_allow_pointer_events_enabled();
     default:
       break;
   }
