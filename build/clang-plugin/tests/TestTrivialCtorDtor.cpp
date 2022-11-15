53274a1b7c2e2e27002d309e1f50f19643fa6e13
Chris Peterson <cpeterson@mozilla.com>
2022-11-15 03:15:13 +0000

    Bug 1800294 - clang-plugin: expect constexpr constructor error when compiling as C++17 but not C++20. r=firefox-static-analysis-reviewers,andi
    C++20 allows non-trivial constexpr constructors, so this BadUnfortunateError check passes unexpectedly when compiled as C++20:
    
    > error: 'error' diagnostics expected but not seen: build/clang-plugin/tests/TestTrivialCtorDtor.cpp Line 81: class 'BadUnfortunateError' must have trivial constructors and destructors
    
    This constexpr constructor check was added in bug 1226376.
    
    Differential Revision: https://phabricator.services.mozilla.com/D161951

--- build/clang-plugin/tests/TestTrivialCtorDtor.cpp
+++ build/clang-plugin/tests/TestTrivialCtorDtor.cpp
@@ -74,10 +74,15 @@ struct MOZ_TRIVIAL_CTOR_DTOR OkConstExprConstructorInMember {
   OkConstExprConstructor m;
 };
 
+#if __cplusplus >= 202002L
+struct
+#else
 // XXX: This error is unfortunate, but is unlikely to come up in real code.
 // In this situation, it should be possible to define a constexpr constructor
 // which explicitly initializes the members.
-struct MOZ_TRIVIAL_CTOR_DTOR BadUnfortunateError { // expected-error {{class 'BadUnfortunateError' must have trivial constructors and destructors}}
+struct // expected-error {{class 'BadUnfortunateError' must have trivial constructors and destructors}}
+#endif
+MOZ_TRIVIAL_CTOR_DTOR BadUnfortunateError {
   OkConstExprConstructor m;
   void *n;
 };
