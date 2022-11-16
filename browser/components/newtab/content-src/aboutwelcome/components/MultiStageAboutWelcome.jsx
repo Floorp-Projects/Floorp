0bb01d196748eba2f3595809e4b983a3543920b8
negin <nsauermann@mozilla.com>
2022-11-16 17:10:34 +0000

    Bug 1797851 - Add telemetry to video section in onboarding r=pdahiya,emcminn,a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D161704

--- browser/components/newtab/content-src/aboutwelcome/components/MultiStageAboutWelcome.jsx
+++ browser/components/newtab/content-src/aboutwelcome/components/MultiStageAboutWelcome.jsx
@@ -253,8 +253,17 @@ export const SecondaryCTA = props => {
 };
 
 export const OnboardingVideo = props => {
-  const vidUrl = props.content?.video_url;
-  const autoplay = props.content?.autoPlay;
+  const vidUrl = props.content.video_url;
+  const autoplay = props.content.autoPlay;
+
+  const handleVideoAction = event => {
+    props.handleAction({
+      currentTarget: {
+        value: event,
+      },
+    });
+  };
+
   return (
     <div>
       <video // eslint-disable-line jsx-a11y/media-has-caption
@@ -263,8 +272,8 @@ export const OnboardingVideo = props => {
         src={vidUrl}
         width="604px"
         height="340px"
-        value="video_container"
-        onEnded={props.handleAction}
+        onPlay={() => handleVideoAction("video_start")}
+        onEnded={() => handleVideoAction("video_end")}
       >
         <source src={vidUrl}></source>
       </video>
