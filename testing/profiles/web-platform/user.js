b0c6f3fda09c5f3011569dd567c8f97438254228
Frederik Braun <fbraun@mozilla.com>
2022-11-16 08:35:58 +0000

    Bug 1775194 - disable reporting API r=ckerschb,emilio,freddyb
    This disables the outdated, incorrect implementation of the
    Reporting API. The current implementation was only enabled
    on Nightly builds, but given its current state it does not
    even make sense there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D149873

--- testing/profiles/web-platform/user.js
+++ testing/profiles/web-platform/user.js
@@ -74,6 +74,8 @@ user_pref("layout.css.prefers-color-scheme.content-override", 1);
 // Force OffscreenCanvas support
 user_pref("gfx.offscreencanvas.enabled", true);
 user_pref("dom.workers.requestAnimationFrame", true);
+// A lot of tests use the Reporting API for observing things
+user_pref("dom.reporting.enabled", true);
 user_pref("layout.css.font-loading-api.workers.enabled", true);
 // Enable WebDriver BiDi experimental commands and events during tests.
 user_pref("remote.experimental.enabled", true);
