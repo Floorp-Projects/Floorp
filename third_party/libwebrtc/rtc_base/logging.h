5e58876aec67e23133f419c977894f72cec29798
Michael Froman <mfroman@mozilla.com>
2022-11-17 16:40:47 -0600

    Bug 1800920 - Vendor libwebrtc from 082d270b31
    Upstream commit: https://webrtc.googlesource.com/src/+/082d270b317a0cb56e4325abee0d1e21766f6e4f
        Remove leftover from previous cleanup.
    
        See https://webrtc-review.googlesource.com/c/src/+/269180.
    
        A default ctor was left behind in the version without logs enabled.
    
        Bug: None
        Change-Id: I27826928fe702c1d50f6ed823def9f5f0bb9aee8
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/269248
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Auto-Submit: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Tomas Gunnarsson <tommi@webrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37594}

--- third_party/libwebrtc/rtc_base/logging.h
+++ third_party/libwebrtc/rtc_base/logging.h
@@ -512,14 +512,6 @@ class LogMessage {
   LogMessage(const char* file, int line, LoggingSeverity sev, const char* tag) {
   }
 #endif
-  // DEPRECATED - DO NOT USE - PLEASE USE THE MACROS INSTEAD OF THE CLASS.
-  // Android code should use the 'const char*' version since tags are static
-  // and we want to avoid allocating a std::string copy per log line.
-  ABSL_DEPRECATED("Use RTC_LOG macros instead of accessing this class directly")
-  LogMessage(const char* file,
-             int line,
-             LoggingSeverity sev,
-             absl::string_view tag) {}
   ~LogMessage() = default;
 
   inline void AddTag(const char* tag) {}
