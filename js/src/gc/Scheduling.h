3885a5e85d3bc986003b2d42ebde045b4f7dea6a
Jon Coppeard <jcoppeard@mozilla.com>
2022-11-17 17:56:08 +0000

    Bug 1801076 - Increase small heap incremental limit r=sfink
    This change to delay reaching the GC urgent threshold while the heap is small
    improved our Jetstream2 score by 2% on MacOS in testing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162282

--- js/src/gc/Scheduling.h
+++ js/src/gc/Scheduling.h
@@ -360,7 +360,7 @@ static const size_t GCMinNurseryBytes = 256 * 1024;
  *
  * This must be greater than 1.3 to maintain performance on splay-latency.
  */
-static const double SmallHeapIncrementalLimit = 1.40;
+static const double SmallHeapIncrementalLimit = 1.50;
 
 /* JSGC_LARGE_HEAP_INCREMENTAL_LIMIT */
 static const double LargeHeapIncrementalLimit = 1.10;
