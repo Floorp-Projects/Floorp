e5b5005ffe902c27aeab5936b511d0b93962bf94
Mark Banner <standard8@mozilla.com>
2022-11-17 12:00:26 +0000

    Bug 1799314 - Convert consumers of testing modules to import ES modules direct (remote). r=webdriver-reviewers,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D161903

--- remote/marionette/sync.sys.mjs
+++ remote/marionette/sync.sys.mjs
@@ -403,7 +403,7 @@ export class DebounceCallback {
 /**
  * Wait for a message to be fired from a particular message manager.
  *
- * This method has been duplicated from BrowserTestUtils.jsm.
+ * This method has been duplicated from BrowserTestUtils.sys.mjs.
  *
  * @param {nsIMessageManager} messageManager
  *     The message manager that should be used.
@@ -451,7 +451,7 @@ export function waitForMessage(
 /**
  * Wait for the specified observer topic to be observed.
  *
- * This method has been duplicated from TestUtils.jsm.
+ * This method has been duplicated from TestUtils.sys.mjs.
  *
  * Because this function is intended for testing, any error in checkFn
  * will cause the returned promise to be rejected instead of waiting for
