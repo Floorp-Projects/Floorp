381a3186e8497bc34158ea5c7a238d54fdb8b1ea
Dave Townsend <dtownsend@oxymoronical.com>
2022-11-17 14:44:57 +0000

    Bug 1792138: Show the extension's name in permission prompts for opening external links. r=ckerschb,robwu,fluent-reviewers,pbz,flod
    In order to handle the content script case correctly we must expose the
    contentScriptAddonPolicy to JavaScript. With that we can always see what
    extension is trying to perform an action and use its name rather than internal
    ID in the dialog.
    
    Differential Revision: https://phabricator.services.mozilla.com/D161282

--- toolkit/mozapps/handling/content/permissionDialog.js
+++ toolkit/mozapps/handling/content/permissionDialog.js
@@ -23,6 +23,8 @@ let dialog = {
 
     this._handlerInfo = handler.QueryInterface(Ci.nsIHandlerInfo);
     this._principal = principal?.QueryInterface(Ci.nsIPrincipal);
+    this._addonPolicy =
+      this._principal?.addonPolicy ?? this._principal?.contentScriptAddonPolicy;
     this._browsingContext = browsingContext;
     this._outArgs = outArgs.QueryInterface(Ci.nsIWritablePropertyBag);
     this._preferredHandlerName = preferredHandlerName;
@@ -82,6 +84,13 @@ let dialog = {
    * the triggering principal and the preferred application handler.
    */
   get l10nDescriptionId() {
+    if (this._addonPolicy) {
+      if (this._preferredHandlerName) {
+        return "permission-dialog-description-extension-app";
+      }
+      return "permission-dialog-description-extension";
+    }
+
     if (this._principal?.schemeIs("file")) {
       if (this._preferredHandlerName) {
         return "permission-dialog-description-file-app";
@@ -116,6 +125,9 @@ let dialog = {
       return null;
     }
 
+    if (this._addonPolicy) {
+      return "permission-dialog-remember-extension";
+    }
     if (this._principal.schemeIs("file")) {
       return "permission-dialog-remember-file";
     }
@@ -168,6 +180,7 @@ let dialog = {
     document.l10n.setAttributes(description, this.l10nDescriptionId, {
       host,
       scheme,
+      extension: this._addonPolicy?.name,
       appName: this._preferredHandlerName,
     });
 
