e9074e73cdb131bd412a9dd5aad9575953e9d58b
Kagami Sascha Rosylight <krosylight@mozilla.com>
2022-11-18 20:08:47 +0000

    Bug 1801338 - Esmify toolkit/components/cleardata r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D162422

--- toolkit/components/cleardata/PrincipalsCollector.sys.mjs
+++ toolkit/components/cleardata/PrincipalsCollector.sys.mjs
@@ -2,11 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-"use strict";
-
-const { XPCOMUtils } = ChromeUtils.importESModule(
-  "resource://gre/modules/XPCOMUtils.sys.mjs"
-);
+import { XPCOMUtils } from "resource://gre/modules/XPCOMUtils.sys.mjs";
 
 const lazy = {};
 
@@ -45,7 +41,7 @@ function log(msg) {
  *
  *  There is currently no `refresh` method, though you are free to add one.
  */
-class PrincipalsCollector {
+export class PrincipalsCollector {
   /**
    * Creates a new PrincipalsCollector.
    */
@@ -166,5 +162,3 @@ class PrincipalsCollector {
     return principals;
   }
 }
-
-const EXPORTED_SYMBOLS = ["PrincipalsCollector"];
