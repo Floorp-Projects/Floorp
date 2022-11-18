c2c15e751c51bb121bc240ccef847185bec872ea
Michael Froman <mfroman@mozilla.com>
2022-11-18 16:44:48 -0600

    Bug 1800920 - Vendor libwebrtc from 53cc9adfe2
    Upstream commit: https://webrtc.googlesource.com/src/+/53cc9adfe200aeb49dd6b3968447cd8fb87c8706
        Keep microseconds when computing e2e test time results
    
        Using ms() was converting the value to an int before putting it into a
        double, causing the microseconds to be dropped. This has the most impact
        on decode time metrics which are ofter less than 1ms.
    
        Bug: webrtc:14339
        Change-Id: Ie8401ba5a46eb3b35e8a699acfdad2dcd32a8240
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/271163
        Reviewed-by: Artem Titov <titovartem@webrtc.org>
        Commit-Queue: Evan Shrubsole <eshr@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37734}

--- third_party/libwebrtc/test/pc/e2e/analyzer/video/default_video_quality_analyzer_frames_comparator_test.cc
+++ third_party/libwebrtc/test/pc/e2e/analyzer/video/default_video_quality_analyzer_frames_comparator_test.cc
@@ -13,11 +13,9 @@
 #include <map>
 #include <vector>
 
-#include "api/test/create_frame_generator.h"
 #include "api/units/timestamp.h"
 #include "rtc_base/strings/string_builder.h"
 #include "system_wrappers/include/clock.h"
-#include "system_wrappers/include/sleep.h"
 #include "test/gtest.h"
 #include "test/pc/e2e/analyzer/video/default_video_quality_analyzer_cpu_measurer.h"
 #include "test/pc/e2e/analyzer/video/default_video_quality_analyzer_shared_objects.h"
@@ -55,7 +53,8 @@ FrameStats FrameStatsWith10msDeltaBetweenPhasesAnd10x10Frame(
   frame_stats.encoded_time = captured_time + TimeDelta::Millis(20);
   frame_stats.received_time = captured_time + TimeDelta::Millis(30);
   frame_stats.decode_start_time = captured_time + TimeDelta::Millis(40);
-  frame_stats.decode_end_time = captured_time + TimeDelta::Millis(50);
+  // Decode time is in microseconds.
+  frame_stats.decode_end_time = captured_time + TimeDelta::Micros(40010);
   frame_stats.rendered_time = captured_time + TimeDelta::Millis(60);
   frame_stats.used_encoder = Vp8CodecForOneFrame(1, frame_stats.encoded_time);
   frame_stats.used_decoder =
@@ -125,7 +124,7 @@ TEST(DefaultVideoQualityAnalyzerFramesComparatorTest,
   EXPECT_DOUBLE_EQ(
       GetFirstOrDie(stats.at(stats_key).total_delay_incl_transport_ms), 60.0);
   EXPECT_DOUBLE_EQ(GetFirstOrDie(stats.at(stats_key).encode_time_ms), 10.0);
-  EXPECT_DOUBLE_EQ(GetFirstOrDie(stats.at(stats_key).decode_time_ms), 10.0);
+  EXPECT_DOUBLE_EQ(GetFirstOrDie(stats.at(stats_key).decode_time_ms), 0.01);
   EXPECT_DOUBLE_EQ(GetFirstOrDie(stats.at(stats_key).receive_to_render_time_ms),
                    30.0);
   EXPECT_DOUBLE_EQ(
