bfcad78fe7ac9b3df0fb3efa1aa54009b4635105
Michael Froman <mfroman@mozilla.com>
2022-11-18 12:17:22 -0600

    Bug 1800920 - Vendor libwebrtc from a0e090ff5a
    Upstream commit: https://webrtc.googlesource.com/src/+/a0e090ff5a2f2f5cfea874cc720e778412e81776
        Added an API to disable VolumeLogger on Android.
    
        Change-Id: Ib16c9e02fe18e1d6628f2192a21c53515753bcde
        Bug: webrtc:14321
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/270621
        Reviewed-by: Xavier Lepaul‎ <xalep@webrtc.org>
        Commit-Queue: Xavier Lepaul‎ <xalep@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37693}

--- third_party/libwebrtc/sdk/android/src/java/org/webrtc/audio/WebRtcAudioTrack.java
+++ third_party/libwebrtc/sdk/android/src/java/org/webrtc/audio/WebRtcAudioTrack.java
@@ -154,19 +154,20 @@ class WebRtcAudioTrack {
   @CalledByNative
   WebRtcAudioTrack(Context context, AudioManager audioManager) {
     this(context, audioManager, null /* audioAttributes */, null /* errorCallback */,
-        null /* stateCallback */, false /* useLowLatency */);
+        null /* stateCallback */, false /* useLowLatency */, true /* enableVolumeLogger */);
   }
 
   WebRtcAudioTrack(Context context, AudioManager audioManager,
       @Nullable AudioAttributes audioAttributes, @Nullable AudioTrackErrorCallback errorCallback,
-      @Nullable AudioTrackStateCallback stateCallback, boolean useLowLatency) {
+      @Nullable AudioTrackStateCallback stateCallback, boolean useLowLatency,
+      boolean enableVolumeLogger) {
     threadChecker.detachThread();
     this.context = context;
     this.audioManager = audioManager;
     this.audioAttributes = audioAttributes;
     this.errorCallback = errorCallback;
     this.stateCallback = stateCallback;
-    this.volumeLogger = new VolumeLogger(audioManager);
+    this.volumeLogger = enableVolumeLogger ? new VolumeLogger(audioManager) : null;
     this.useLowLatency = useLowLatency;
     Logging.d(TAG, "ctor" + WebRtcAudioUtils.getThreadInfo());
   }
@@ -266,7 +267,9 @@ class WebRtcAudioTrack {
   @CalledByNative
   private boolean startPlayout() {
     threadChecker.checkIsOnValidThread();
-    volumeLogger.start();
+    if (volumeLogger != null) {
+      volumeLogger.start();
+    }
     Logging.d(TAG, "startPlayout");
     assertTrue(audioTrack != null);
     assertTrue(audioThread == null);
@@ -298,7 +301,9 @@ class WebRtcAudioTrack {
   @CalledByNative
   private boolean stopPlayout() {
     threadChecker.checkIsOnValidThread();
-    volumeLogger.stop();
+    if (volumeLogger != null) {
+      volumeLogger.stop();
+    }
     Logging.d(TAG, "stopPlayout");
     assertTrue(audioThread != null);
     logUnderrunCount();
