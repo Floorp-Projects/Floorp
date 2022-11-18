a8887c10de26684b500c84b74c2299c530ed4512
Norisz Fay <nfay@mozilla.com>
2022-11-18 21:54:48 +0200

    Backed out changeset 40534e8e5252 (bug 1798589) for causing python failures

--- testing/mozharness/configs/repackage/osx_signed.py
+++ testing/mozharness/configs/repackage/osx_signed.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from __future__ import absolute_import
 import os
 
 config = {
@@ -9,7 +10,4 @@ config = {
     "locale": os.environ.get("LOCALE"),
     # ToolTool
     "tooltool_cache": os.environ.get("TOOLTOOL_CACHE"),
-    # TODO: `create_pkg` doesn't require this.
-    #       We need to refactor `create_dmg` to remove this requirement
-    "run_configure": True,
 }
