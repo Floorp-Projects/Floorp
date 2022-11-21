37e6c60be45263c64b2e564efa0fcfa0fedb77d8
Tom Ritter <tom@mozilla.com>
2022-11-21 19:35:16 +0000

    Bug 1800927: Fix the Updatebot linter and offending moz.yaml files r=ahal,webdriver-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D162223

--- tools/lint/test/test_updatebot.py
+++ tools/lint/test/test_updatebot.py
@@ -1,25 +1,35 @@
+import os
 import mozunit
 
 LINTER = "updatebot"
 
 
 def test_basic(lint, paths):
-    results = lint(paths())
+    results = []
+
+    for p in paths():
+        for (root, dirs, files) in os.walk(p):
+            for f in files:
+                if f == ".yamllint":
+                    continue
+
+                filepath = os.path.join(root, f)
+                result = lint(filepath, testing=True)
+                if result:
+                    results.append(result)
 
     assert len(results) == 2
 
     i = 0
-    assert results[i].level == "error"
-    assert "cargo-mismatch.yaml" in results[i].relpath
-    assert "wasn't found in Cargo.lock" in results[i].message
-
-    i += 1
-    assert results[i].level == "error"
-    assert "no-revision.yaml" in results[i].relpath
+    assert "no-revision.yaml" in results[i][0].path
     assert (
         'If "vendoring" is present, "revision" must be present in "origin"'
-        in results[i].message
+        in results[i][0].message
     )
+
+    i += 1
+    assert "cargo-mismatch.yaml" in results[i][0].path
+    assert "wasn't found in Cargo.lock" in results[i][0].message
 
 
 if __name__ == "__main__":
