3d738084cca5f5c1f1fa1dc68252c3170904d718
Nicolas Chevobbe <nchevobbe@mozilla.com>
2022-11-21 16:10:38 +0000

    Bug 1801199 - Record install telemetry event for SitePermsAddons. r=rpl.
    This is done by calling `AddonManagerPrivate.callInstallListeners` when we handle
    `SitePermsAddonInstall`'s specific listeners.
    We take this as an opportunity to remove support for the `onDownloadEnded` event
    as it didn't do anything and isn't something we want to record for SitePermsAddon.
    We also set `SitePermsAddonInstall#installId` which is used in `AMTelemetry`.
    
    Assertions on registered telemetry events are added in browser_midi_permission_gated.js,
    and a test case is added to ensure cancelling the event from the second popup works
    as expected, since it wasn't checked before.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162412

--- toolkit/mozapps/extensions/internal/SitePermsAddonProvider.sys.mjs
+++ toolkit/mozapps/extensions/internal/SitePermsAddonProvider.sys.mjs
@@ -295,10 +295,13 @@ class SitePermsAddonInstalling extends SitePermsAddonWrapper {
   }
 }
 
+// Numeric id included in the install telemetry events to correlate multiple events related
+// to the same install or update flow.
+let nextInstallId = 0;
+
 class SitePermsAddonInstall {
   #listeners = new Set();
   #installEvents = {
-    DOWNLOAD_ENDED: "onDownloadEnded",
     INSTALL_CANCELLED: "onInstallCancelled",
     INSTALL_ENDED: "onInstallEnded",
     INSTALL_FAILED: "onInstallFailed",
@@ -316,6 +319,7 @@ class SitePermsAddonInstall {
       this.principal.siteOriginNoSuffix,
       this
     );
+    this.installId = ++nextInstallId;
   }
 
   get installTelemetryInfo() {
@@ -344,7 +348,7 @@ class SitePermsAddonInstall {
           // to install/uninstall/enable/disable addons.  We may need to
           // do that here in the future.
           this.#callInstallListeners(this.#installEvents.INSTALL_FAILED);
-        } else {
+        } else if (this.state !== lazy.AddonManager.STATE_CANCELLED) {
           this.cancel();
         }
         return;
@@ -376,11 +380,17 @@ class SitePermsAddonInstall {
       return;
     }
 
-    this.#callInstallListeners(this.#installEvents.DOWNLOAD_ENDED);
     this.checkPrompt();
   }
 
   cancel() {
+    // This method can be called if the install is already cancelled.
+    // We don't want to go further in such case as it would lead to duplicated Telemetry events.
+    if (this.state == lazy.AddonManager.STATE_CANCELLED) {
+      console.error("SitePermsAddonInstall#cancel called twice on ", this);
+      return;
+    }
+
     this.state = lazy.AddonManager.STATE_CANCELLED;
     this.#callInstallListeners(this.#installEvents.INSTALL_CANCELLED);
   }
@@ -418,16 +428,11 @@ class SitePermsAddonInstall {
       return;
     }
 
-    for (const listener of this.#listeners) {
-      try {
-        listener[eventName]?.(this);
-      } catch (e) {
-        console.warn(
-          `SitePermsAddonInstall threw exception when calling listener callback for event "${eventName}":`,
-          e
-        );
-      }
-    }
+    lazy.AddonManagerPrivate.callInstallListeners(
+      eventName,
+      Array.from(this.#listeners),
+      this
+    );
   }
 }
 
