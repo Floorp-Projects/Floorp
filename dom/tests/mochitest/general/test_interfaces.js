40626cb8ddb55bd48c5ce671c3ba4b667f2bfeb7
Emilio Cobos Álvarez <emilio@crisal.io>
2022-11-21 08:01:11 +0000

    Bug 1801123 - Enable container queries on nightly. r=dshin
    There's only one worrying timeout
    (css/css-contain/container-queries/inline-size-bfc-floats.html), but it
    isn't a "hard" hang, but an invalidation loop.
    
    Let's enable this by default on nightly to get some feedback while we
    work on it and the remaining issues that block enabling them by default.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162292

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -277,6 +277,8 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSConditionRule", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "CSSContainerRule", insecureContext: true, nightly: true },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSCounterStyleRule", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSFontFaceRule", insecureContext: true },
