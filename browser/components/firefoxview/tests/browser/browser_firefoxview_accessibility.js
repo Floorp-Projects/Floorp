d6f904e4a2bf01c6e47c320d247055a3310fbe89
Hanna Jones <hjones@mozilla.com>
2022-11-21 15:56:49 +0000

    Bug 1800478 - fix intermittent failure in fx view accessibility tests r=tgiles
    The switch to using `withFirefoxView` caused another onboarding prompt to show. After the fx view tabstrip button has been clicked for the 3rd time we display a new message. This wasn't happening before because the pref that controls this is tied to that button click, so didn't get incremented when we were opening about:firefoxview in a new tab.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162427

--- browser/components/firefoxview/tests/browser/browser_firefoxview_accessibility.js
+++ browser/components/firefoxview/tests/browser/browser_firefoxview_accessibility.js
@@ -9,6 +9,15 @@
  * chrome.
  */
 
+add_setup(async function setup() {
+  // Make sure the prompt to connect FxA doesn't show
+  // Without resetting the view-count pref it gets surfaced after
+  // the third click on the fx view toolbar button.
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.firefox-view.view-count", 0]],
+  });
+});
+
 add_task(async function test_keyboard_focus_after_tab_pickup_opened() {
   // Reset various things touched by other tests in this file so that
   // we have a sufficiently clean environment.
