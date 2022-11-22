721ed4d92f43cffa982aacd4ad87f4fd2553c5a5
Michael Froman <mfroman@mozilla.com>
2022-11-22 15:54:41 +0000

    Bug 1801722 - pt2 - properly find our moz-libwebrtc base commit when commit stack is on top of release branch. r=dbaker DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D162615

--- dom/media/webrtc/third_party_build/use_config_env.sh
+++ dom/media/webrtc/third_party_build/use_config_env.sh
@@ -17,6 +17,14 @@ function find_base_commit()
   # read the last line of README.moz-ff-commit to retrieve our current base
   # commit in moz-libwebrtc
   MOZ_LIBWEBRTC_BASE=`tail -1 third_party/libwebrtc/README.moz-ff-commit`
+  echo "prelim MOZ_LIBWEBRTC_BASE: $MOZ_LIBWEBRTC_BASE"
+  # if we've advanced into a chrome release branch, we need to adjust the
+  # MOZ_LIBWEBRTC_BASE to the last common commit so we can now advance up
+  # the trunk commits.
+  MOZ_LIBWEBRTC_BASE=`cd $MOZ_LIBWEBRTC_SRC ; git merge-base $MOZ_LIBWEBRTC_BASE $MOZ_GIT_RELEASE_BRANCH`
+  # now make it a short hash
+  MOZ_LIBWEBRTC_BASE=`cd $MOZ_LIBWEBRTC_SRC ; git rev-parse --short $MOZ_LIBWEBRTC_BASE`
+  echo "adjusted MOZ_LIBWEBRTC_BASE: $MOZ_LIBWEBRTC_BASE"
 }
 
 function find_next_commit()
