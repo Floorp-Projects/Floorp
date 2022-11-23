09c2389d60356d9fc7c207d6051250a7a67e7175
Jon Coppeard <jcoppeard@mozilla.com>
2022-11-23 13:51:07 +0000

    Bug 1802087 - Part 2: Don't update null entries in the script GC things vector r=jandem
    This refactors the code to move the update into the GC.
    
    Depends on D162820
    
    Differential Revision: https://phabricator.services.mozilla.com/D162821

--- js/src/gc/Marking.cpp
+++ js/src/gc/Marking.cpp
@@ -640,6 +640,28 @@ void js::TraceGCCellPtrRoot(JSTracer* trc, JS::GCCellPtr* thingp,
   }
 }
 
+void js::TraceManuallyBarrieredGCCellPtr(JSTracer* trc, JS::GCCellPtr* thingp,
+                                         const char* name) {
+  Cell* thing = thingp->asCell();
+  if (!thing) {
+    return;
+  }
+
+  Cell* traced = MapGCThingTyped(thing, thing->getTraceKind(),
+                                 [trc, name](auto t) -> Cell* {
+                                   TraceManuallyBarrieredEdge(trc, &t, name);
+                                   return t;
+                                 });
+
+  if (!traced) {
+    // If we are clearing edges, also erase the type. This happens when using
+    // ClearEdgesTracer.
+    *thingp = JS::GCCellPtr();
+  } else if (traced != thingp->asCell()) {
+    *thingp = JS::GCCellPtr(traced, thingp->kind());
+  }
+}
+
 template <typename T>
 inline bool TraceTaggedPtrEdge(JSTracer* trc, T* thingp, const char* name) {
   // Return true by default. For some types the lambda below won't be called.
