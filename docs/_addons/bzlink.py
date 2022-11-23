f8c8f296440b2d248d2b618bfe72802ed177d024
Marco Castelluccio <mcastelluccio@mozilla.com>
2022-11-23 12:58:26 +0000

    Bug 1790816 - Reformat docs/ with isort. r=linter-reviewers,Standard8 DONTBUILD
    # ignore-this-changeset
    
    Differential Revision: https://phabricator.services.mozilla.com/D162665

--- docs/_addons/bzlink.py
+++ docs/_addons/bzlink.py
@@ -2,9 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from sphinx.transforms import SphinxTransform
-from docutils.nodes import reference, Text, paragraph
 import re
+
+from docutils.nodes import Text, paragraph, reference
+from sphinx.transforms import SphinxTransform
 
 
 class ConvertBugsToLinks(SphinxTransform):
