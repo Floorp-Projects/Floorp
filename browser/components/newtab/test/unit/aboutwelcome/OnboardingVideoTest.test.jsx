1779c1057fb3e789635dee3bb4963494f1b9384a
negin <nsauermann@mozilla.com>
2022-11-23 17:50:13 +0000

    Bug 1800126 - [Cleanup] Refactor OnboardingVideo in its own component r=yozhang
    [try](https://treeherder.mozilla.org/jobs?repo=try&revision=50e8d3a693d5a8cf6a6f3bf601c9cd241f5145e6)
    
    Differential Revision: https://phabricator.services.mozilla.com/D162420

---
+++ browser/components/newtab/test/unit/aboutwelcome/OnboardingVideoTest.test.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { mount } from "enzyme";
+import { OnboardingVideo } from "content-src/aboutwelcome/components/OnboardingVideo";
+
+describe("OnboardingVideo component", () => {
+  let sandbox;
+
+  beforeEach(async () => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  const SCREEN_PROPS = {
+    content: {
+      title: "Test title",
+      video_container: {
+        video_url: "test url",
+      },
+    },
+  };
+
+  it("should handle video_start action when video is played", () => {
+    const handleAction = sandbox.stub();
+    const wrapper = mount(
+      <OnboardingVideo handleAction={handleAction} {...SCREEN_PROPS} />
+    );
+    wrapper.find("video").simulate("play");
+    assert.calledWith(handleAction, {
+      currentTarget: { value: "video_start" },
+    });
+  });
+  it("should handle video_end action when video has completed playing", () => {
+    const handleAction = sandbox.stub();
+    const wrapper = mount(
+      <OnboardingVideo handleAction={handleAction} {...SCREEN_PROPS} />
+    );
+    wrapper.find("video").simulate("ended");
+    assert.calledWith(handleAction, {
+      currentTarget: { value: "video_end" },
+    });
+  });
+});
