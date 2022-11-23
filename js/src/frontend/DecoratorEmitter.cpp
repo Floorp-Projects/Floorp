824b58b5f4cf75ec466cef7b8b12e984f85d8095
Dan Minor <dminor@mozilla.com>
2022-11-23 16:47:13 +0000

    Bug 1793959 - Add DecoratorEmitter; r=mgaudet
    This adds a stub DecoratorEmitter to the build system.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162139

---
+++ js/src/frontend/DecoratorEmitter.cpp
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "frontend/DecoratorEmitter.h"
+
+#include "frontend/BytecodeEmitter.h"
+
+using namespace js;
+using namespace js::frontend;
+
+DecoratorEmitter::DecoratorEmitter(BytecodeEmitter* bce) {}
