5b31ae9796ecca272eef114630fbbcab84f33058
Marco Castelluccio <mcastelluccio@mozilla.com>
2022-11-23 12:59:26 +0000

    Bug 1790816 - Reformat intl/ with isort. r=linter-reviewers,Standard8 DONTBUILD
    # ignore-this-changeset
    
    Differential Revision: https://phabricator.services.mozilla.com/D162645

--- intl/icu_sources_data.py
+++ intl/icu_sources_data.py
@@ -10,8 +10,7 @@
 # This script expects to be run from `update-icu.sh` after the in-tree
 # copy of ICU has been updated.
 
-from __future__ import absolute_import
-from __future__ import print_function
+from __future__ import absolute_import, print_function
 
 import glob
 import multiprocessing
