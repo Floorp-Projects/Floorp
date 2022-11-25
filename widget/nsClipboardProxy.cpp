e887a32947029858a2a381236c058b91b70494ed
Stanca Serban <sstanca@mozilla.com>
2022-11-25 18:35:18 +0200

    Backed out changeset dab070a6ba77 (bug 1802125) for causing wpt failures on  /clipboard-apis. CLOSED TREE

--- widget/nsClipboardProxy.cpp
+++ widget/nsClipboardProxy.cpp
@@ -135,17 +135,9 @@ RefPtr<GenericPromise> nsClipboardProxy::AsyncGetData(
       ->Then(
           GetMainThreadSerialEventTarget(), __func__,
           /* resolve */
-          [promise,
-           transferable](const IPCDataTransferOrError& ipcDataTransferOrError) {
-            if (ipcDataTransferOrError.type() ==
-                IPCDataTransferOrError::Tnsresult) {
-              promise->Reject(ipcDataTransferOrError.get_nsresult(), __func__);
-              return;
-            }
-
+          [promise, transferable](const IPCDataTransfer& ipcDataTransfer) {
             nsresult rv = nsContentUtils::IPCTransferableToTransferable(
-                ipcDataTransferOrError.get_IPCDataTransfer(),
-                false /* aAddDataFlavor */, transferable,
+                ipcDataTransfer, false /* aAddDataFlavor */, transferable,
                 false /* aFilterUnknownFlavors */);
             if (NS_FAILED(rv)) {
               promise->Reject(rv, __func__);
