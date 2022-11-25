a43d144d883283a546ae0eb4eb543729c8fca989
Mike Hommey <mh+mozilla@glandium.org>
2022-11-25 02:18:48 +0000

    Bug 1802271 - Stop installing python3-dev packages. r=firefox-build-system-reviewers,ahochheiden
    With psutil now coming with wheels on Linux, we don't need to be able to
    build python modules out of the box.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162954

--- python/mozboot/mozboot/debian.py
+++ python/mozboot/mozboot/debian.py
@@ -16,7 +16,6 @@ class DebianBootstrapper(LinuxBootstrapper, BaseBootstrapper):
     # Ubuntu).
     COMMON_PACKAGES = [
         "build-essential",
-        "libpython3-dev",
         "m4",
         "unzip",
     ]
