e8abee45de77a52952b81dc5ad1fd5289672dcf7
André Bargull <andre.bargull@gmail.com>
2022-11-29 12:17:27 +0000

    Bug 1802497 - Part 1: Add missing MOZ_EXPORT for ARM simulator functions. r=jandem
    The next part adds a new cpp-file, which changes unified bundles. Without this
    change we'll then get a "visibility does not match previous declaratio" error.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163094

--- js/src/jit/arm/Simulator-arm.cpp
+++ js/src/jit/arm/Simulator-arm.cpp
@@ -49,7 +49,7 @@
 
 extern "C" {
 
-int64_t __aeabi_idivmod(int x, int y) {
+MOZ_EXPORT int64_t __aeabi_idivmod(int x, int y) {
   // Run-time ABI for the ARM architecture specifies that for |INT_MIN / -1|
   // "an implementation is (sic) may return any convenient value, possibly the
   // original numerator."
@@ -67,7 +67,7 @@ int64_t __aeabi_idivmod(int x, int y) {
   return (int64_t(hi) << 32) | lo;
 }
 
-int64_t __aeabi_uidivmod(int x, int y) {
+MOZ_EXPORT int64_t __aeabi_uidivmod(int x, int y) {
   uint32_t lo = uint32_t(x) / uint32_t(y);
   uint32_t hi = uint32_t(x) % uint32_t(y);
   return (int64_t(hi) << 32) | lo;
