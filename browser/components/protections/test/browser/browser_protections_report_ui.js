7ac86c275dd5f5fe0b0a70f6c42eb4032664be48
Barret Rennie <barret@brennie.ca>
2022-11-29 21:02:39 +0000

    Bug 1803176 - Port osfile.jsm usage to IOUtils in browser/components/protections/ r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D163347

--- browser/components/protections/test/browser/browser_protections_report_ui.js
+++ browser/components/protections/test/browser/browser_protections_report_ui.js
@@ -4,7 +4,6 @@
 
 // Note: This test may cause intermittents if run at exactly midnight.
 
-const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
 const { Sqlite } = ChromeUtils.importESModule(
   "resource://gre/modules/Sqlite.sys.mjs"
 );
@@ -20,7 +19,7 @@ XPCOMUtils.defineLazyServiceGetter(
 );
 
 XPCOMUtils.defineLazyGetter(this, "DB_PATH", function() {
-  return OS.Path.join(OS.Constants.Path.profileDir, "protections.sqlite");
+  return PathUtils.join(PathUtils.profileDir, "protections.sqlite");
 });
 
 const SQL = {
