4693b4dca859fd6f825d18436f344315d086dda2
Olli Pettay <Olli.Pettay@helsinki.fi>
2022-11-30 15:38:00 +0000

    Bug 1798780, ensure we don't evict the current frameloader, r=peterv, a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D163391

---
+++ docshell/test/browser/browser_bug1798780.js
@@ -0,0 +1,51 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// The test loads an initial page and then another page which does enough
+// fragment navigations so that when going back to the initial page and then
+// forward to the last page, the initial page is evicted from the bfcache.
+add_task(async function testBFCacheEviction() {
+  // Make an unrealistic large timeout.
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.sessionhistory.contentViewerTimeout", 86400]],
+  });
+
+  const uri = "data:text/html,initial page";
+  const uri2 =
+    getRootDirectory(gTestPath).replace(
+      "chrome://mochitests/content",
+      "https://example.com"
+    ) + "dummy_page.html";
+
+  await BrowserTestUtils.withNewTab({ gBrowser, url: uri }, async function(
+    browser
+  ) {
+    BrowserTestUtils.loadURI(browser, uri2);
+    await BrowserTestUtils.browserLoaded(browser);
+
+    let awaitPageShow = BrowserTestUtils.waitForContentEvent(
+      browser,
+      "pageshow"
+    );
+    await SpecialPowers.spawn(browser, [], async function() {
+      content.location.hash = "1";
+      content.location.hash = "2";
+      content.location.hash = "3";
+      content.history.go(-4);
+    });
+
+    await awaitPageShow;
+
+    let awaitPageShow2 = BrowserTestUtils.waitForContentEvent(
+      browser,
+      "pageshow"
+    );
+    await SpecialPowers.spawn(browser, [], async function() {
+      content.history.go(4);
+    });
+    await awaitPageShow2;
+    ok(true, "Didn't time out.");
+  });
+});
