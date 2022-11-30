2807c779769ac07cdb8d2d4a6289e7dcf18ef24e
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-12-01 02:07:05 +0900

    [FB] Extension | Rewrite webcompat (floorp-system)

---
+++ floorp/browser/extensions/webextensions/floorp-system/shared/webcompat.js
@@ -0,0 +1,32 @@
+const WEBCOMPATS = [
+  {
+    "matches": ["*://*.youtube.com/*"],
+    "js": [
+      { file: "webcompat/fix-youtube-comment.js" }
+    ],
+    "platforms": ["win"], // "mac", "win", "android", "cros", "linux", "openbsd", "fuchsia"
+  }
+]
+
+let REGISTED_CONTENT_SCRIPTS = [];
+
+async function regist_webcompat_contentScripts() {
+  let platform = (await browser.runtime.getPlatformInfo()).os;
+  for (let WEBCOMPAT of WEBCOMPATS) {
+    if (WEBCOMPAT.platforms.includes(platform)) {
+      let registeredContentScript = await browser.contentScripts.register(WEBCOMPAT);
+      REGISTED_CONTENT_SCRIPTS.push(registeredContentScript);
+    }
+  }
+}
+
+async function unregist_webcompat_contentScripts() {
+  for (let REGISTED_CONTENT_SCRIPT of REGISTED_CONTENT_SCRIPTS) {
+    REGISTED_CONTENT_SCRIPT.unregister();
+  }
+}
+
+(async() => {
+  //unregist_webcompat_contentScripts();
+  await regist_webcompat_contentScripts();
+})();
