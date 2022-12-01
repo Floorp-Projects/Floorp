2a4800917389b3336618399dc2004fd720d275a4
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-12-01 16:25:06 +0900

    [FB] Extension | Fix variable scope.

--- floorp/browser/extensions/webextensions/floorp-system/shared/webpanel-ua.js
+++ floorp/browser/extensions/webextensions/floorp-system/shared/webpanel-ua.js
@@ -1,25 +1,27 @@
-const ua = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36 Edg/107.0.1418.26";
+(function(){
+  const ua = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36 Edg/107.0.1418.26";
 
-browser.webRequestExt.onBeforeRequest_webpanel_requestId.addListener(function(requestId){
-  function listener(e) {
-    if (e.requestId == requestId) {
-      browser.webRequest.onBeforeSendHeaders.removeListener(listener);
-      e.requestHeaders.forEach(function (header) {
-        if (header.name.toLowerCase() === "user-agent") {
-            header.value = ua;
-        }
-      });
-      return {requestHeaders: e.requestHeaders};
+  browser.webRequestExt.onBeforeRequest_webpanel_requestId.addListener(function(requestId){
+    function listener(e) {
+      if (e.requestId == requestId) {
+        browser.webRequest.onBeforeSendHeaders.removeListener(listener);
+        e.requestHeaders.forEach(function (header) {
+          if (header.name.toLowerCase() === "user-agent") {
+              header.value = ua;
+          }
+        });
+        return {requestHeaders: e.requestHeaders};
+      }
     }
-  }
-  browser.webRequest.onBeforeSendHeaders.addListener(
-    listener,
-    {urls: ["<all_urls>"]},
-    ["blocking", "requestHeaders"]
-  );
-  setTimeout(function(){
-    if (browser.webRequest.onBeforeSendHeaders.hasListener(listener)) {
-      browser.webRequest.onBeforeSendHeaders.removeListener(listener);
-    }
-  }, 180 * 1000);
-})
+    browser.webRequest.onBeforeSendHeaders.addListener(
+      listener,
+      {urls: ["<all_urls>"]},
+      ["blocking", "requestHeaders"]
+    );
+    setTimeout(function(){
+      if (browser.webRequest.onBeforeSendHeaders.hasListener(listener)) {
+        browser.webRequest.onBeforeSendHeaders.removeListener(listener);
+      }
+    }, 180 * 1000);
+  })
+})();
