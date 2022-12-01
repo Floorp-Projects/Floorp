5a9a8b5aca52a3d717fd36ef78ec4de175808d82
Arturo Mejia <arturomejiamarmol@gmail.com>
2022-12-01 17:32:32 -0500

    [focus] Bug 1803117 - disable by default cookieBannerHandlingModePrivateBrowsing.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Settings.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Settings.kt
@@ -449,7 +449,7 @@ class Settings(
     fun getCurrentCookieBannerOptionFromSharePref(): CookieBannerOption {
         val optionValue = preferences.getString(
             context.getString(R.string.pref_key_cookie_banner_settings),
-            context.getString(CookieBannerOption.CookieBannerRejectAll().prefKeyId),
+            context.getString(CookieBannerOption.CookieBannerDisabled().prefKeyId),
         )
         return when (optionValue) {
             context.getString(CookieBannerOption.CookieBannerDisabled().prefKeyId) ->
