6d8dbc8d9cecc9bea568412a02ec424ece9dcb76
Nika Layzell <nika@thelayzells.com>
2022-12-01 15:43:19 +0000

    Bug 1793463 - Part 5: Stop using contractids to fetch protocol handlers, r=necko-reviewers,xpcom-reviewers,webdriver-reviewers,whimboo,valentin,kmag
    This patch replaces the previous ContractID-based lookup system for protocol
    handlers, and replaces it with a new custom system in nsIOService. It will be
    pre-populated with non-overridable static protocol handlers using the
    StaticComponents infrastructure added in the previous part, and callers can
    also dynamically register new protocol handlers at runtime.
    
    This new system is intended to provide access to the default port and
    non-dynamic protocol flags off-main-thread, by requiring these values to be
    provided up-front as constants, rather than getting them from the xpcom
    interface. The data is then guarded by an RWLock.
    
    Callers which look up specific handlers by their contractID are not changed, as
    the contract IDs for existing handlers have not been changed, so the lookup
    will still succeed.
    
    This change as-implemented breaks the nsGIOProtocolHandler on Linux, as it
    removes the special code which would try to use that handler for some
    protocols. This will be fixed in a later part by making the
    nsGIOProtocolHandler use the dynamic registration APIs to register and
    un-register protocol handlers at runtime in response to the GIO pref.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162804

--- netwerk/base/nsProtocolProxyService.cpp
+++ netwerk/base/nsProtocolProxyService.cpp
@@ -2017,6 +2017,7 @@ void nsProtocolProxyService::LoadHostFilters(const nsACString& aFilters) {
 
 nsresult nsProtocolProxyService::GetProtocolInfo(nsIURI* uri,
                                                  nsProtocolInfo* info) {
+  AssertIsOnMainThread();
   MOZ_ASSERT(uri, "URI is null");
   MOZ_ASSERT(info, "info is null");
 
@@ -2028,14 +2029,10 @@ nsresult nsProtocolProxyService::GetProtocolInfo(nsIURI* uri,
   nsCOMPtr<nsIIOService> ios = do_GetIOService(&rv);
   if (NS_FAILED(rv)) return rv;
 
-  nsCOMPtr<nsIProtocolHandler> handler;
-  rv = ios->GetProtocolHandler(info->scheme.get(), getter_AddRefs(handler));
+  rv = ios->GetDynamicProtocolFlags(uri, &info->flags);
   if (NS_FAILED(rv)) return rv;
 
-  rv = handler->DoGetProtocolFlags(uri, &info->flags);
-  if (NS_FAILED(rv)) return rv;
-
-  rv = handler->GetDefaultPort(&info->defaultPort);
+  rv = ios->GetDefaultPort(info->scheme.get(), &info->defaultPort);
   return rv;
 }
 
