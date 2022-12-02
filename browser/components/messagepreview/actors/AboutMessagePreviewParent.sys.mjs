5f1c1066dfaf43e4144e2923f2c7ebbb1dee9c17
Emily McMinn <emcminn@mozilla.com>
2022-12-02 16:45:00 +0000

    Bug 1803220 - Support the Spotlight component on about:messagepreview r=barret
    Differential Revision: https://phabricator.services.mozilla.com/D163530

--- browser/components/messagepreview/actors/AboutMessagePreviewParent.sys.mjs
+++ browser/components/messagepreview/actors/AboutMessagePreviewParent.sys.mjs
@@ -10,14 +10,20 @@ const lazy = {};
 
 XPCOMUtils.defineLazyModuleGetters(lazy, {
   InfoBar: "resource://activity-stream/lib/InfoBar.jsm",
+  Spotlight: "resource://activity-stream/lib/Spotlight.jsm",
 });
 
 export class AboutMessagePreviewParent extends JSWindowActorParent {
   showInfoBar(message) {
     const browser = this.browsingContext.topChromeWindow.gBrowser
       .selectedBrowser;
-
     lazy.InfoBar.showInfoBarMessage(browser, message, () => {});
+  }
+
+  showSpotlight(message) {
+    const browser = this.browsingContext.topChromeWindow.gBrowser
+      .selectedBrowser;
+    lazy.Spotlight.showSpotlightDialog(browser, message, () => {});
   }
 
   async showMessage(data) {
@@ -45,7 +51,9 @@ export class AboutMessagePreviewParent extends JSWindowActorParent {
       case "infobar":
         this.showInfoBar(message);
         return;
-
+      case "spotlight":
+        this.showSpotlight(message);
+        return;
       default:
         console.error(`Unsupported message template ${message.template}`);
     }
