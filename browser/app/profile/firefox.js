9385ccd0713e285552af9e73969a14f8d9305150
Cosmin Sabou <csabou@mozilla.com>
2022-12-02 23:41:08 +0200

    Backed out changeset 0406b118f155 (bug 1803566) for bc failures on browser_glean_telemetry_abandonment_groups.js. CLOSED TREE

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -404,7 +404,7 @@ pref("browser.urlbar.suggest.calculator",           false);
   pref("browser.urlbar.suggest.quickactions", true);
   pref("browser.urlbar.shortcuts.quickactions", true);
   pref("browser.urlbar.quickactions.showPrefs", true);
-  pref("browser.urlbar.quickactions.showInZeroPrefix", false);
+  pref("browser.urlbar.quickactions.showInZeroPrefix", true);
 #endif
 
 // Feature gate pref for weather suggestions in the urlbar.
