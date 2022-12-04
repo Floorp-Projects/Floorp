fb5e7a67af09332c91d3bc2f4cc0d989f87c4aa7
Oriol Brufau <oriol-bugzilla@hotmail.com>
2022-12-04 20:36:08 +0000

    Bug 1803928 - Make mContain and mEffectiveContainment private. r=emilio
    Depends on D163779
    
    Differential Revision: https://phabricator.services.mozilla.com/D163783

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -1290,15 +1290,17 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleDisplay {
   // Saved mDisplay for position:absolute/fixed and float:left/right; otherwise
   // equal to mDisplay.
   mozilla::StyleDisplay mOriginalDisplay;
-  mozilla::StyleContain mContain;
   // Equal to mContain plus any implicit containment from mContentVisibility and
   // mContainerType.
-  mozilla::StyleContain mEffectiveContainment;
   mozilla::StyleContentVisibility mContentVisibility;
   mozilla::StyleContainerType mContainerType;
 
  private:
   mozilla::StyleAppearance mAppearance;
+  mozilla::StyleContain mContain;
+  // Equal to mContain plus any implicit containment from mContentVisibility and
+  // mContainerType.
+  mozilla::StyleContain mEffectiveContainment;
 
  public:
   mozilla::StyleAppearance mDefaultAppearance;
@@ -1725,6 +1727,8 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleDisplay {
   inline bool
   IsFixedPosContainingBlockForContainLayoutAndPaintSupportingFrames() const;
   inline bool IsFixedPosContainingBlockForTransformSupportingFrames() const;
+
+  mozilla::ContainSizeAxes GetContainSizeAxes(const nsIFrame& aFrame) const;
 };
 
 struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleTable {
