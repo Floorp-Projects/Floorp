5292063db1724326c8b020ee9a8313f1b955dd23
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2022-12-04 23:41:42 +0000

    Bug 1801782 - Drop a redundant pan update phase in panRightToLeft. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D163181

--- widget/tests/browser/browser_test_swipe_gesture.js
+++ widget/tests/browser/browser_test_swipe_gesture.js
@@ -935,7 +935,7 @@ add_task(async () => {
   await panRightToLeft(tab.linkedBrowser, 100, 100, 1);
   // NOTE: The last endPhase shouldn't fire a wheel event since
   // its delta is zero.
-  is(wheelEventCount, 3, "Received 3 wheel events");
+  is(wheelEventCount, 2, "Received 2 wheel events");
 
   await waitForWhile();
   // Make sure any navigation didn't happen.
