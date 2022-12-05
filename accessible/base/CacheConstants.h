c3c870d29f29e12761f2b5ade66317372d35a113
Morgan Rae Reschenberg <mreschenberg@mozilla.com>
2022-12-05 23:42:52 +0000

    Bug 1803814: Remove `Maybe` wrapper from RelationData's mReverseType member r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D163858

--- accessible/base/CacheConstants.h
+++ accessible/base/CacheConstants.h
@@ -54,7 +54,7 @@ struct RelationData {
   nsStaticAtom* const mAtom;
   nsStaticAtom* const mValidTag;
   RelationType mType;
-  Maybe<RelationType> mReverseType;
+  RelationType mReverseType;
 };
 
 /**
@@ -70,17 +70,17 @@ struct RelationData {
  */
 static constexpr RelationData kRelationTypeAtoms[] = {
     {nsGkAtoms::aria_labelledby, nullptr, RelationType::LABELLED_BY,
-     Some(RelationType::LABEL_FOR)},
+     RelationType::LABEL_FOR},
     {nsGkAtoms::_for, nsGkAtoms::label, RelationType::LABEL_FOR,
-     Some(RelationType::LABELLED_BY)},
+     RelationType::LABELLED_BY},
     {nsGkAtoms::aria_controls, nullptr, RelationType::CONTROLLER_FOR,
-     Some(RelationType::CONTROLLED_BY)},
+     RelationType::CONTROLLED_BY},
     {nsGkAtoms::_for, nsGkAtoms::output, RelationType::CONTROLLED_BY,
-     Some(RelationType::CONTROLLER_FOR)},
+     RelationType::CONTROLLER_FOR},
     {nsGkAtoms::aria_describedby, nullptr, RelationType::DESCRIBED_BY,
-     Some(RelationType::DESCRIPTION_FOR)},
+     RelationType::DESCRIPTION_FOR},
     {nsGkAtoms::aria_flowto, nullptr, RelationType::FLOWS_TO,
-     Some(RelationType::FLOWS_FROM)},
+     RelationType::FLOWS_FROM},
 };
 
 }  // namespace a11y
--- accessible/ipc/RemoteAccessibleBase.cpp
+++ accessible/ipc/RemoteAccessibleBase.cpp
@@ -945,11 +945,6 @@ nsTArray<bool> RemoteAccessibleBase<Derived>::PreProcessRelations(
       }
     }
 
-    if (!data.mReverseType) {
-      updateTracker.AppendElement(false);
-      continue;
-    }
-
     nsStaticAtom* const relAtom = data.mAtom;
     auto newRelationTargets =
         aFields->GetAttribute<nsTArray<uint64_t>>(relAtom);
@@ -979,7 +974,7 @@ nsTArray<bool> RemoteAccessibleBase<Derived>::PreProcessRelations(
             // the following assert, we don't have parity on implicit/explicit
             // rels and something is wrong.
             nsTArray<uint64_t>& reverseRelIDs = reverseRels->LookupOrInsert(
-                static_cast<uint64_t>(*data.mReverseType));
+                static_cast<uint64_t>(data.mReverseType));
             //  There might be other reverse relations stored for this acc, so
             //  remove our ID instead of deleting the array entirely.
             DebugOnly<bool> removed = reverseRelIDs.RemoveElement(ID());
@@ -1013,10 +1008,8 @@ void RemoteAccessibleBase<Derived>::PostProcessRelations(
       for (uint64_t id : newIDs) {
         nsTHashMap<nsUint64HashKey, nsTArray<uint64_t>>& relations =
             Document()->mReverseRelations.LookupOrInsert(id);
-        MOZ_ASSERT(data.mReverseType,
-                   "Updating implicit rels, but no implicit rel exists?");
         nsTArray<uint64_t>& ids =
-            relations.LookupOrInsert(static_cast<uint64_t>(*data.mReverseType));
+            relations.LookupOrInsert(static_cast<uint64_t>(data.mReverseType));
         ids.AppendElement(ID());
       }
     }
