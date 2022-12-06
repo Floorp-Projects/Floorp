ca5abb1e48ac0225d003dfd88d12123c0ddad758
Valentin Gosu <valentin.gosu@gmail.com>
2022-12-06 10:36:10 +0000

    Bug 1797279 - Make PSocketProcess.ClearSessionCache return async when done r=kershaw,necko-reviewers
    Originally these changes were part of D162103.
    
    Differential Revision: https://phabricator.services.mozilla.com/D162152

--- netwerk/ipc/SocketProcessChild.h
+++ netwerk/ipc/SocketProcessChild.h
@@ -104,7 +104,8 @@ class SocketProcessChild final : public PSocketProcessChild {
   Maybe<RefPtr<BackgroundDataBridgeParent>> GetAndRemoveDataBridge(
       uint64_t aChannelId);
 
-  mozilla::ipc::IPCResult RecvClearSessionCache();
+  mozilla::ipc::IPCResult RecvClearSessionCache(
+      ClearSessionCacheResolver&& aResolve);
 
   already_AddRefed<PTRRServiceChild> AllocPTRRServiceChild(
       const bool& aCaptiveIsPassed, const bool& aParentalControlEnabled,
