955e3d440e11c6e15b5bf331d92f8459d96bdfce
Emily McDonough <emcdonough@mozilla.com>
2022-12-07 21:14:52 +0000

    Bug 1787947 - pref on CSS named pages in Nightly r=dholbert
    Also standardize having named pages enabled for css-page WPT
    
    Differential Revision: https://phabricator.services.mozilla.com/D163638

--- devtools/shared/css/generated/properties-db.js
+++ devtools/shared/css/generated/properties-db.js
@@ -9667,6 +9667,21 @@ exports.CSS_PROPERTIES = {
       "unset"
     ]
   },
+  "page": {
+    "isInherited": false,
+    "subproperties": [
+      "page"
+    ],
+    "supports": [],
+    "values": [
+      "auto",
+      "inherit",
+      "initial",
+      "revert",
+      "revert-layer",
+      "unset"
+    ]
+  },
   "page-break-after": {
     "isInherited": false,
     "subproperties": [
