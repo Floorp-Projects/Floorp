ee2217bb25aee76895c0a5b81ac3d44f6a181afe
Jon Coppeard <jcoppeard@mozilla.com>
2022-12-07 12:58:52 +0000

    Bug 1802897 - Part 8: Allow accessing the atom marking bitmap from more than one thread r=sfink
    This would otherwise assert during parallel marking. It's safe to check the
    mark bits concurrently as we don't mutate anything.
    
    This adds 'readonlyThreadsafe' methods to SparseBitmap along the same lines as
    those in mozilla::HashMap.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163491

--- js/src/gc/AtomMarking.cpp
+++ js/src/gc/AtomMarking.cpp
@@ -212,7 +212,7 @@ bool AtomMarkingRuntime::atomIsMarked(Zone* zone, T* thing) {
   }
 
   size_t bit = GetAtomBit(&thing->asTenured());
-  return zone->markedAtoms().getBit(bit);
+  return zone->markedAtoms().readonlyThreadsafeGetBit(bit);
 }
 
 template bool AtomMarkingRuntime::atomIsMarked(Zone* zone, JSAtom* thing);
