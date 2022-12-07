6c10ebd5085e80fcef0e0f851be905c93fa6974f
Emilio Cobos Álvarez <emilio@crisal.io>
2022-12-07 18:47:53 +0000

    Bug 1802314 - Use non-native tooltips in the cocoa non-native theme too. r=spohl
    We implemented a basic ThemeGeometry-based tooltip in
    https://hg.mozilla.org/integration/autoland/rev/e8d65866112f, but now
    that we have non-native theme support for these it seems ok to just use
    it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163023

--- widget/Theme.cpp
+++ widget/Theme.cpp
@@ -1580,9 +1580,15 @@ LayoutDeviceIntSize Theme::GetMinimumWidgetSize(nsPresContext* aPresContext,
 
 nsITheme::Transparency Theme::GetWidgetTransparency(
     nsIFrame* aFrame, StyleAppearance aAppearance) {
-  return GetScrollbarDrawing()
-      .GetScrollbarPartTransparency(aFrame, aAppearance)
-      .valueOr(eUnknownTransparency);
+  if (auto scrollbar = GetScrollbarDrawing().GetScrollbarPartTransparency(
+          aFrame, aAppearance)) {
+    return *scrollbar;
+  }
+  if (aAppearance == StyleAppearance::Tooltip) {
+    // We draw a rounded rect, so we need transparency.
+    return eTransparent;
+  }
+  return eUnknownTransparency;
 }
 
 NS_IMETHODIMP
