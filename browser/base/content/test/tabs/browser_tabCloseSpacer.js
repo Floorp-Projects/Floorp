16b5e3c28fa6f30b068215bcd158ac3a0e0134c2
Cosmin Sabou <csabou@mozilla.com>
2022-12-08 01:08:59 +0200

    Backed out changeset e9692ac46be2 (bug 1791438) for causing bc failures on browser_tabReorder_overflow. CLOSED TREE

--- browser/base/content/test/tabs/browser_tabCloseSpacer.js
+++ browser/base/content/test/tabs/browser_tabCloseSpacer.js
@@ -14,7 +14,7 @@ add_task(async function() {
   let downButton = gBrowser.tabContainer.arrowScrollbox._scrollButtonDown;
   let closingTabsSpacer = gBrowser.tabContainer._closingTabsSpacer;
 
-  await BrowserTestUtils.overflowTabs(registerCleanupFunction, window);
+  await overflowTabs();
   ok(
     gBrowser.tabContainer.hasAttribute("overflow"),
     "Tab strip should be overflowing"
@@ -57,10 +57,37 @@ add_task(async function() {
     gBrowser.tabContainer.hasAttribute("using-closing-tabs-spacer"),
     "using spacer"
   );
-
   is(downButton.clientWidth, 0, "down button has no width");
   isnot(closingTabsSpacer.clientWidth, 0, "spacer has some width");
 });
+
+async function overflowTabs() {
+  let arrowScrollbox = gBrowser.tabContainer.arrowScrollbox;
+  const originalSmoothScroll = arrowScrollbox.smoothScroll;
+  arrowScrollbox.smoothScroll = false;
+  registerCleanupFunction(() => {
+    arrowScrollbox.smoothScroll = originalSmoothScroll;
+  });
+
+  let width = ele => ele.getBoundingClientRect().width;
+  let tabMinWidth = parseInt(
+    getComputedStyle(gBrowser.selectedTab, null).minWidth
+  );
+  let tabCountForOverflow = Math.ceil(
+    (width(arrowScrollbox) / tabMinWidth) * 1.1
+  );
+  while (gBrowser.tabs.length < tabCountForOverflow) {
+    BrowserTestUtils.addTab(gBrowser, "about:blank", {
+      skipAnimation: true,
+      index: 0,
+    });
+  }
+
+  // Make sure scrolling finished.
+  await new Promise(resolve => {
+    arrowScrollbox.addEventListener("scrollend", resolve, { once: true });
+  });
+}
 
 function getLastCloseButton() {
   let lastTab = gBrowser.tabs[gBrowser.tabs.length - 1];
