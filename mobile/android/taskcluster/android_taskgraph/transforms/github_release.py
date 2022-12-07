ae29d6d804adce0af0b94c4788ee168586d75f4c
Johan Lorenzo <jlorenzo@mozilla.com>
2022-12-07 15:30:21 +0100

    Bug 1799696 - part 8: Migrate github-release task over

--- mobile/android/taskcluster/android_taskgraph/transforms/github_release.py
+++ mobile/android/taskcluster/android_taskgraph/transforms/github_release.py
@@ -34,15 +34,47 @@ def resolve_keys(config, tasks):
 def build_worker_definition(config, tasks):
     for task in tasks:
         worker_definition = {
-            # We don't want to upload 10gb of artifacts to the release; let's
-            # just create a release as a tag.
-            "artifact-map": [],
+            "artifact-map": _build_artifact_map(task),
             "git-tag": config.params["head_tag"],
             "git-revision": config.params["head_rev"],
-            "release-name": task["worker"]["release-name"],
             "release-name": task["worker"]["release-name"].format(version=config.params["version"]),
         }
 
         task["worker"].update(worker_definition)
+
+        yield task
+
+
+def _build_artifact_map(task):
+    artifact_map = []
+    if not task["attributes"].get("apks"):
+        # We don't want to upload 10gb of artifacts to the release; let's
+        # just create a release as a tag.
+        return artifact_map
+
+    github_names_per_path = {
+        apk_metadata["name"]: apk_metadata["github-name"]
+        for apk_metadata in task["attributes"]["apks"].values()
+    }
+
+    for upstream_artifact_metadata in task["worker"]["upstream-artifacts"]:
+        artifacts = {"paths": {}, "taskId": upstream_artifact_metadata["taskId"]}
+        for path in upstream_artifact_metadata["paths"]:
+            artifacts["paths"][path] = {
+                "destinations": [github_names_per_path[path]]
+            }
+
+        artifact_map.append(artifacts)
+
+    return artifact_map
+
+
+@transforms.add
+def remove_dependent_tasks(config, tasks):
+    for task in tasks:
+        try:
+            del task["dependent-tasks"]
+        except KeyError:
+            pass
 
         yield task
