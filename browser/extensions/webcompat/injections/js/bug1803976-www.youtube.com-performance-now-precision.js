32893bf27b88d4b88b871d86151a7b9df6dfc9ea
Ksenia Berezina <kberezina@mozilla.com>
2022-12-08 20:29:53 +0000

    Bug 1776678 - Ship v109.0.0 of the WebCompat System Addon r=webcompat-reviewers,twisniewski
    Differential Revision: https://phabricator.services.mozilla.com/D164256

---
+++ browser/extensions/webcompat/injections/js/bug1803976-www.youtube.com-performance-now-precision.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * Bug 1803976 - When attempting to go back on youtube.com, the content remains the same
+ *
+ * If consecutive session history entries had history.state.entryTime set to same value,
+ * back button doesn't work as expected. The entryTime value is coming from performance.now()
+ * and modifying its return value slightly to make sure two close consecutive calls don't
+ * get the same result helped with resolving the issue.
+ */
+
+/* globals exportFunction */
+
+console.info(
+  "performance.now precision has been modified for compatibility reasons. See https://bugzilla.mozilla.org/show_bug.cgi?id=1756970 for details."
+);
+
+const origPerf = performance.wrappedJSObject;
+const origNow = origPerf.now;
+
+let counter = 0;
+let previousVal = 0;
+
+Object.defineProperty(window.performance.wrappedJSObject, "now", {
+  value: exportFunction(function() {
+    let originalVal = origNow.call(origPerf);
+    if (originalVal === previousVal) {
+      originalVal += 0.00000003 * ++counter;
+    } else {
+      previousVal = originalVal;
+      counter = 0;
+    }
+    return originalVal;
+  }, window),
+});
