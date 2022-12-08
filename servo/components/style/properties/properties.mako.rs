1332dcab6039e49b8ed970382fc550f6d4a800e6
Jonathan Kew <jkew@mozilla.com>
2022-12-08 10:16:34 +0000

    Bug 1804492 - Remove 'font-palette' from the 'font' shorthand. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D164128

--- servo/components/style/properties/properties.mako.rs
+++ servo/components/style/properties/properties.mako.rs
@@ -2606,7 +2606,7 @@ pub struct SourcePropertyDeclaration {
 
 // This is huge, but we allocate it on the stack and then never move it,
 // we only pass `&mut SourcePropertyDeclaration` references around.
-size_of_test!(SourcePropertyDeclaration, 664);
+size_of_test!(SourcePropertyDeclaration, 632);
 
 impl SourcePropertyDeclaration {
     /// Create one. It’s big, try not to move it around.
