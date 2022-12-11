8f4e8142e8926b4c142a7d4a33b3b542d0034ca7
Mike Hommey <mh+mozilla@glandium.org>
2022-12-11 21:00:01 +0000

    Bug 1804167 - Use sysroot when building AFL instrumentation toolchain. r=decoder
    clang is built using the sysroot, and not using the sysroot to build the
    afl-llvm-pass plugin can lead to ABI differences that lead to crashes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163905

--- taskcluster/scripts/misc/build-afl.sh
+++ taskcluster/scripts/misc/build-afl.sh
@@ -10,6 +10,9 @@ cd "$MOZ_FETCHES_DIR/AFL"
 patch -p1 -i "$scripts/afl-nyx.patch"
 make afl-showmap \
     CC="$MOZ_FETCHES_DIR/clang/bin/clang"
+# -O3 -funroll-loops as per llvm_mode/Makefile
+CFLAGS="-O3 -funroll-loops --sysroot $MOZ_FETCHES_DIR/sysroot" \
+CXXFLAGS="-O3 -funroll-loops --sysroot $MOZ_FETCHES_DIR/sysroot" \
 make -C llvm_mode install \
     DESTDIR="../$dir" \
     PREFIX=/ \
