2b68755d6438d562f42a897e03ae3fd8c984c470
James Graham <james@hoppipolla.co.uk>
2022-12-12 13:44:50 +0000

    Bug 1800086 - Move capture.sys.mjs to shared module, r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D161801

--- remote/shared/Capture.sys.mjs
+++ remote/shared/Capture.sys.mjs
@@ -10,9 +10,7 @@ ChromeUtils.defineESModuleGetters(lazy, {
   Log: "chrome://remote/content/shared/Log.sys.mjs",
 });
 
-XPCOMUtils.defineLazyGetter(lazy, "logger", () =>
-  lazy.Log.get(lazy.Log.TYPES.MARIONETTE)
-);
+XPCOMUtils.defineLazyGetter(lazy, "logger", () => lazy.Log.get());
 
 const CONTEXT_2D = "2d";
 const BG_COLOUR = "rgb(255,255,255)";
