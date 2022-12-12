f566128d6e8c87bd1a448226f473cb65324f8ac9
Sandor Molnar <smolnar@mozilla.com>
2022-12-13 01:52:06 +0200

    Backed out 9 changesets (bug 1796585, bug 1796152, bug 1797284, bug 1796554, bug 1800076, bug 1802349, bug 1797497) for causing mochitest failures in xpcom/base/nsTraceRefcnt.cpp. CLOSED TREE
    Backed out changeset 6002e93434a2 (bug 1802349)
    Backed out changeset 000cd664c157 (bug 1800076)
    Backed out changeset e3534e639e2c (bug 1796554)
    Backed out changeset 1d22428d764c (bug 1797497)
    Backed out changeset b0dbe35d48be (bug 1797284)
    Backed out changeset b415d4b85ab6 (bug 1796585)
    Backed out changeset dc1c2ffbc38e (bug 1796554)
    Backed out changeset c44bae1f03f5 (bug 1796152)
    Backed out changeset 3eab5e71b1c7 (bug 1796152)

--- devtools/client/performance-new/popup/background.jsm.js
+++ devtools/client/performance-new/popup/background.jsm.js
@@ -452,7 +452,6 @@ function getPrefPostfix(pageContext) {
   switch (pageContext) {
     case "devtools":
     case "aboutprofiling":
-    case "aboutlogging":
       // Don't use any postfix on the prefs.
       return "";
     case "devtools-remote":
