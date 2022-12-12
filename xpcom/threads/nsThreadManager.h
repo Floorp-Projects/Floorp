b284401b35a6a34b0e48acb415c2097d7af0e0e9
Jens Stutte <jstutte@mozilla.com>
2022-12-12 15:21:28 +0000

    Bug 1768581 - Part 4: Postpone main nsThread destruction after full XPCOM shutdown. r=xpcom-reviewers,nika
    Some debug infrastructure like MOZ_WEAKPTR_INIT_THREAD_SAFETY_CHECK  that can apparently be triggered by nsComponentManagerImpl::gComponentManager->FreeService() seem to rely on the existence of a serial event target even if they do not post any events. So it seems sound to keep a representation of the main thread as nsThread object until after final XPCOM shutdown.
    But nsThreadManager::ShutdownMainThread() does more, it processes the last round of events, removes the thread's observer  and closes down the background event target. We do not want to move these operations to happen later than before, such that we split the nsThread release into a separate function and move that together with AbstractThread::ShutdownMainThread() behind FreeService().
    
    Depends on D160628
    
    Differential Revision: https://phabricator.services.mozilla.com/D162497

--- xpcom/threads/nsThreadManager.h
+++ xpcom/threads/nsThreadManager.h
@@ -37,8 +37,13 @@ class nsThreadManager : public nsIThreadManager {
   void ShutdownNonMainThreads();
 
   // Finish shutting down all threads. This function must be called after
-  // ShutdownNonMainThreads and will take the main thread out of commission.
+  // ShutdownNonMainThreads and will delete the BackgroundEventTarget and
+  // take the main thread event target out of commission, but without
+  // releasing the underlying nsThread object.
   void ShutdownMainThread();
+
+  // Release the underlying main thread nsThread object.
+  void ReleaseMainThread();
 
   // Called by nsThread to inform the ThreadManager it exists.  This method
   // must be called when the given thread is the current thread.
