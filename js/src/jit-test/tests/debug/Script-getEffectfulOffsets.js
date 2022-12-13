53ccb68bfa2cc19d224edf14319cdb37f7878e72
Tooru Fujisawa <arai_a@mac.com>
2022-12-13 09:41:47 +0000

    Bug 1797736 - Part 1: Mark JSOp::InitAliasedLexical with hops == 0 as non-effectful. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D161017

--- js/src/jit-test/tests/debug/Script-getEffectfulOffsets.js
+++ js/src/jit-test/tests/debug/Script-getEffectfulOffsets.js
@@ -20,14 +20,48 @@ ${code}
   assertEq(numEffectfulOperations, expected);
 }
 
-test("a.f = 0", 2);
-test("a.f++", 2);
-test("return a.f", 1);
-test("a[b] = 0", 2);
-test("a[b]++", 2);
-test("return a[b]", 1);
-test("a = 0", 1);
-test("d = 0", 2);
-test("with (a) { b = 0; }", 5);
-test("let o = {}; ({x: o.x} = { x: 10 })", 2);
-test("var x", 1);
+const base = 1;
+test("", base);
+
+test("a.f = 0", base + 1);
+test("a.f++", base + 1);
+test("return a.f", base + 0);
+test("a[b] = 0", base + 1);
+test("a[b]++", base + 1);
+test("return a[b]", base + 0);
+test("a = 0", base + 0);
+test("d = 0", base + 1);
+test("with (a) { b = 0; }", base + 4);
+test("let o = {}; ({x: o.x} = { x: 10 })", base + 1);
+test("var x", base + 0);
+
+// d is not closed over, and "let d = 0;" uses InitLexical,
+// which is non-effectful.
+test(`
+let d = 10;
+`, base + 0);
+
+// d is closed over, and "let d = 0;" uses InitAliasedLexical with hops == 0,
+// which is non-effectful.
+test(`
+let d = 10;
+function g() {
+  d;
+}
+`, base + 0);
+
+// Private accessor uses InitAliasedLexical with hops == 1,
+// which is currently marked as effectful.
+// Please fix this test if it's marked as non-effectful in the future.
+test(`
+class B {
+  set #x(x) {}
+}
+`, base + 1);
+
+test(`
+class B {
+  get #x() {}
+  set #x(x) {}
+}
+`, base + 2);
