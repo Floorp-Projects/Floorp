b8213704faa461e3a98cb8738df8fec5bd687998
Tooru Fujisawa <arai_a@mac.com>
2022-12-13 09:41:47 +0000

    Bug 1797736 - Part 2: Remove obsolete comment about async function and microtask checkpoints. r=tcampbell
    Eager evaluation already supports async function, and supporting it there isn't
    necessarily related to microtask checkpoints.
    
    Depends on D161017
    
    Differential Revision: https://phabricator.services.mozilla.com/D161018

--- js/src/debugger/Script.cpp
+++ js/src/debugger/Script.cpp
@@ -1424,8 +1424,6 @@ static bool BytecodeIsEffectful(JSScript* script, size_t offset) {
     case JSOp::SetFunName:
     case JSOp::MutateProto:
     case JSOp::DynamicImport:
-      // Treat async functions as effectful so that microtask checkpoints
-      // won't run.
     case JSOp::InitialYield:
     case JSOp::Yield:
       return true;
