f3fe71564330af5a4305458eedb9974ae216dcf5
Tim Huang <tihuang@mozilla.com>
2022-12-13 09:10:33 +0000

    Bug 1797079 - Part 3: Add tests for the reload telemetry. r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D163930

--- toolkit/components/cookiebanners/test/browser/browser_cookiebanner_telemetry.js
+++ toolkit/components/cookiebanners/test/browser/browser_cookiebanner_telemetry.js
@@ -56,6 +56,49 @@ function verifyLookUpTelemetry(probe, expected) {
 }
 
 /**
+ * A helper function to verify the reload telemetry.
+ *
+ * @param {Number} length The expected length of the telemetry array.
+ * @param {Number} idx The index of the telemetry to be verified.
+ * @param {Object} expected An object that describe the expected value.
+ */
+function verifyReloadTelemetry(length, idx, expected) {
+  let events = Glean.cookieBanners.reload.testGetValue();
+
+  is(events.length, length, "There is a expected number of reload events.");
+
+  let event = events[idx];
+
+  let { noRule, hasCookieRule, hasClickRule } = expected;
+  is(event.name, "reload", "The reload event has the correct name");
+  is(event.extra.no_rule, noRule, "The extra field 'no_rule' is expected");
+  is(
+    event.extra.has_cookie_rule,
+    hasCookieRule,
+    "The extra field 'has_cookie_rule' is expected"
+  );
+  is(
+    event.extra.has_click_rule,
+    hasClickRule,
+    "The extra field 'has_click_rule' is expected"
+  );
+}
+
+/**
+ * A helper function to reload the browser and wait until it loads.
+ *
+ * @param {Browser} browser The browser object.
+ * @param {String} url The URL to be loaded.
+ */
+async function reloadBrowser(browser, url) {
+  let reloaded = BrowserTestUtils.browserLoaded(browser, false, url);
+
+  // Reload as a user.
+  window.BrowserReload();
+
+  await reloaded;
+}
+/**
  * A helper function to open the testing page for look up telemetry.
  *
  * @param {browser} browser The browser element
@@ -83,7 +126,7 @@ async function openLookUpTelemetryTestPage(browser, testInTop, page, domain) {
   await clickFinishPromise;
 }
 
-add_setup(function() {
+add_setup(async function() {
   // Clear telemetry before starting telemetry test.
   Services.fog.testResetFOG();
 
@@ -103,6 +146,8 @@ add_setup(function() {
     // Clear cookies that have been set during testing.
     await SiteDataTestUtils.clear();
   });
+
+  await clickTestSetup();
 });
 
 add_task(async function test_service_mode_telemetry() {
@@ -157,7 +202,6 @@ add_task(async function test_rule_lookup_telemetry_no_rule() {
       ],
     ],
   });
-  await clickTestSetup();
 
   // Clear out all rules.
   Services.cookieBanners.resetRules(false);
@@ -237,7 +281,6 @@ add_task(async function test_rule_lookup_telemetry() {
       ],
     ],
   });
-  await clickTestSetup();
   insertTestClickRules();
 
   // Open a tab for testing.
@@ -262,7 +305,7 @@ add_task(async function test_rule_lookup_telemetry() {
         true,
         `cookieConsent_${TEST_DOMAIN_A}_1`,
         "optOut1",
-        "." + TEST_DOMAIN_A,
+        null,
         "/",
         3600,
         "",
@@ -276,7 +319,7 @@ add_task(async function test_rule_lookup_telemetry() {
         false,
         `cookieConsent_${TEST_DOMAIN_A}_2`,
         "optIn2",
-        TEST_DOMAIN_A,
+        null,
         "/",
         3600,
         "",
@@ -297,7 +340,7 @@ add_task(async function test_rule_lookup_telemetry() {
         false,
         `cookieConsent_${TEST_DOMAIN_B}_1`,
         "optIn1",
-        TEST_DOMAIN_B,
+        null,
         "/",
         3600,
         "UNSET",
@@ -437,4 +480,271 @@ add_task(async function test_rule_lookup_telemetry() {
   }
 
   BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async function test_reload_telemetry_no_rule() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [
+        "cookiebanners.service.mode",
+        Ci.nsICookieBannerService.MODE_REJECT_OR_ACCEPT,
+      ],
+    ],
+  });
+
+  // Clear out all rules.
+  Services.cookieBanners.resetRules(false);
+
+  // Open a tab for testing.
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    TEST_ORIGIN_A
+  );
+
+  // Make sure there is no reload event at the beginning.
+  let events = Glean.cookieBanners.reload.testGetValue();
+  ok(!events, "No reload event at the beginning.");
+
+  // Trigger the reload
+  await reloadBrowser(tab.linkedBrowser, TEST_ORIGIN_A + "/");
+
+  // Check the telemetry
+  verifyReloadTelemetry(1, 0, {
+    noRule: "true",
+    hasCookieRule: "false",
+    hasClickRule: "false",
+  });
+
+  BrowserTestUtils.removeTab(tab);
+  Services.fog.testResetFOG();
+});
+
+add_task(async function test_reload_telemetry() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [
+        "cookiebanners.service.mode",
+        Ci.nsICookieBannerService.MODE_REJECT_OR_ACCEPT,
+      ],
+    ],
+  });
+  insertTestClickRules();
+
+  // Open a tab for testing.
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    TEST_ORIGIN_A
+  );
+
+  // Make sure there is no reload event at the beginning.
+  let events = Glean.cookieBanners.reload.testGetValue();
+  ok(!events, "No reload event at the beginning.");
+
+  // Trigger the reload
+  await reloadBrowser(tab.linkedBrowser, TEST_ORIGIN_A + "/");
+
+  // Check the telemetry
+  verifyReloadTelemetry(1, 0, {
+    noRule: "false",
+    hasCookieRule: "false",
+    hasClickRule: "true",
+  });
+
+  // Add a both click rule and cookie rule for another domain.
+  let cookieRule = Cc["@mozilla.org/cookie-banner-rule;1"].createInstance(
+    Ci.nsICookieBannerRule
+  );
+  cookieRule.domains = [TEST_DOMAIN_B];
+
+  Services.cookieBanners.insertRule(cookieRule);
+  cookieRule.addCookie(
+    false,
+    `cookieConsent_${TEST_DOMAIN_B}_1`,
+    "optIn1",
+    null,
+    "/",
+    3600,
+    "UNSET",
+    false,
+    false,
+    true,
+    0,
+    0
+  );
+  cookieRule.addClickRule(
+    "div#banner",
+    false,
+    Ci.nsIClickRule.RUN_ALL,
+    null,
+    null,
+    "button#optIn"
+  );
+
+  // Load the page with another origin.
+  BrowserTestUtils.loadURI(tab.linkedBrowser, TEST_ORIGIN_B);
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+
+  // Trigger the reload
+  await reloadBrowser(tab.linkedBrowser, TEST_ORIGIN_B + "/");
+
+  // Check the telemetry
+  verifyReloadTelemetry(2, 1, {
+    noRule: "false",
+    hasCookieRule: "true",
+    hasClickRule: "true",
+  });
+
+  BrowserTestUtils.removeTab(tab);
+  Services.fog.testResetFOG();
+});
+
+add_task(async function test_reload_telemetry_mode_disabled() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [
+        "cookiebanners.service.mode",
+        Ci.nsICookieBannerService.MODE_REJECT_OR_ACCEPT,
+      ],
+    ],
+  });
+  insertTestClickRules();
+
+  // Disable the cookie banner service.
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["cookiebanners.service.mode", Ci.nsICookieBannerService.MODE_DISABLED],
+    ],
+  });
+
+  // Open a tab for testing.
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    TEST_ORIGIN_A
+  );
+
+  // Trigger the reload
+  await reloadBrowser(tab.linkedBrowser, TEST_ORIGIN_A + "/");
+
+  // Check the telemetry. The reload telemetry should report no rule given that
+  // the service is disabled.
+  verifyReloadTelemetry(1, 0, {
+    noRule: "true",
+    hasCookieRule: "false",
+    hasClickRule: "false",
+  });
+
+  BrowserTestUtils.removeTab(tab);
+  Services.fog.testResetFOG();
+});
+
+add_task(async function test_reload_telemetry_mode_reject() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["cookiebanners.service.mode", Ci.nsICookieBannerService.MODE_REJECT],
+    ],
+  });
+  insertTestClickRules();
+
+  // Open a tab for testing.
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    TEST_ORIGIN_A
+  );
+
+  // Trigger the reload
+  await reloadBrowser(tab.linkedBrowser, TEST_ORIGIN_A + "/");
+
+  // Check the telemetry. The reload telemetry should report there is click rule
+  // for the domain has opt-out rule.
+  verifyReloadTelemetry(1, 0, {
+    noRule: "false",
+    hasCookieRule: "false",
+    hasClickRule: "true",
+  });
+
+  // Load the page with the domain only has opt-in click rule.
+  BrowserTestUtils.loadURI(tab.linkedBrowser, TEST_ORIGIN_B);
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+
+  // Trigger the reload
+  await reloadBrowser(tab.linkedBrowser, TEST_ORIGIN_B + "/");
+
+  // Check the telemetry. It should report there is no rule because the domain
+  // only has an opt-in click rule.
+  verifyReloadTelemetry(2, 1, {
+    noRule: "true",
+    hasCookieRule: "false",
+    hasClickRule: "false",
+  });
+
+  BrowserTestUtils.removeTab(tab);
+  Services.fog.testResetFOG();
+});
+
+add_task(async function test_reload_telemetry_iframe() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [
+        "cookiebanners.service.mode",
+        Ci.nsICookieBannerService.MODE_REJECT_OR_ACCEPT,
+      ],
+    ],
+  });
+
+  // Clear out all rules.
+  Services.cookieBanners.resetRules(false);
+
+  // Insert a click rule for an iframe case. And add a cookie rule for the same
+  // domain. We shouldn't report there is a cookie rule for iframe because
+  // cookie rules are top-level only.
+  let cookieRule = Cc["@mozilla.org/cookie-banner-rule;1"].createInstance(
+    Ci.nsICookieBannerRule
+  );
+  cookieRule.domains = [TEST_DOMAIN_A];
+  Services.cookieBanners.insertRule(cookieRule);
+
+  cookieRule.addClickRule(
+    "div#banner",
+    false,
+    Ci.nsIClickRule.RUN_CHILD,
+    null,
+    null,
+    "button#optIn"
+  );
+  cookieRule.addCookie(
+    false,
+    `cookieConsent_${TEST_DOMAIN_A}_1`,
+    "optIn1",
+    null,
+    "/",
+    3600,
+    "UNSET",
+    false,
+    false,
+    true,
+    0,
+    0
+  );
+
+  // Open a tab for testing.
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    TEST_ORIGIN_C + TEST_PATH + "file_iframe_banner.html"
+  );
+
+  // Trigger the reload
+  await reloadBrowser(
+    tab.linkedBrowser,
+    TEST_ORIGIN_C + TEST_PATH + "file_iframe_banner.html"
+  );
+
+  // Check the telemetry
+  verifyReloadTelemetry(1, 0, {
+    noRule: "false",
+    hasCookieRule: "false",
+    hasClickRule: "true",
+  });
+
+  BrowserTestUtils.removeTab(tab);
+  Services.fog.testResetFOG();
 });
