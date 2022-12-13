65ae57556e94126c7a94635c369e13b4b2a15813
Christian Sadilek <christian.sadilek@gmail.com>
2022-12-13 11:53:42 -0500

    Revert "Bug 1799668 - part 15: Fix Focus build warning"
    This reverts commit f3a08e91e6f3affdb876f7148fb18b00b6c68a2e.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/UrlInputFragment.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/UrlInputFragment.kt
@@ -37,6 +37,7 @@ import org.mozilla.focus.R
 import org.mozilla.focus.activity.MainActivity
 import org.mozilla.focus.databinding.FragmentUrlinputBinding
 import org.mozilla.focus.ext.defaultSearchEngineName
+import org.mozilla.focus.ext.isSearch
 import org.mozilla.focus.ext.requireComponents
 import org.mozilla.focus.ext.settings
 import org.mozilla.focus.input.InputToolbarIntegration
