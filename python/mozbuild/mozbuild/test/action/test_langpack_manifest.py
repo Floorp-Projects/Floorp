b27f5e805928cb96d09287ea80f8f779e503f6ef
Francesco Lodolo (:flod) <flod@lodolo.net>
2022-12-13 10:12:06 +0000

    Bug 1805297 - Update name and description for language packs to fit within manifest.json limits, r=eemeli a=RyanVM
    manifest.json has character limits for name (45) and description (132).
    
    Language packs now use:
    
    name: Language: {native_name} ({english_name})
    description: {app} Language Pack for {native_name} ({locale_code}) – {english_name}
    
    The English name is omitted if identical to the native name, and it's omitted from the name if the resulting name exceeds the character limit.
    
    Longer name or description are always truncated.
    
    Differential Revision: https://phabricator.services.mozilla.com/D164535

--- python/mozbuild/mozbuild/test/action/test_langpack_manifest.py
+++ python/mozbuild/mozbuild/test/action/test_langpack_manifest.py
@@ -57,11 +57,64 @@ langpack-contributors = { "" }
         )
 
         data = json.loads(manifest)
-        self.assertEqual(data["name"], "Language Pack: Suomi (Finnish)")
+        self.assertEqual(data["name"], "Language: Suomi (Finnish)")
+        self.assertEqual(
+            data["description"], "Firefox Language Pack for Suomi (fi) – Finnish"
+        )
         self.assertEqual(
             data["author"], "Suomennosprojekti (contributors: Joe Smith, Mary White)"
         )
         self.assertEqual(data["version"], "57.0.1buildid20210928.100000")
+
+    def test_manifest_truncated_name(self):
+        ctx = {
+            "langpack-creator": "Mozilla.org / Softcatalà",
+            "langpack-contributors": "Joe Smith, Mary White",
+        }
+        os.environ["MOZ_BUILD_DATE"] = "20210928100000"
+        manifest = langpack_manifest.create_webmanifest(
+            "ca-valencia",
+            "57.0.1",
+            "57.0",
+            "57.0.*",
+            "Firefox",
+            "/var/vcs/l10n-central",
+            "langpack-ca-valencia@firefox.mozilla.og",
+            ctx,
+            {},
+        )
+
+        data = json.loads(manifest)
+        self.assertEqual(data["name"], "Language: Català (Valencià)")
+        self.assertEqual(
+            data["description"],
+            "Firefox Language Pack for Català (Valencià) (ca-valencia) – Catalan, Valencian",
+        )
+
+    def test_manifest_name_untranslated(self):
+        ctx = {
+            "langpack-creator": "Mozilla.org",
+            "langpack-contributors": "Joe Smith, Mary White",
+        }
+        os.environ["MOZ_BUILD_DATE"] = "20210928100000"
+        manifest = langpack_manifest.create_webmanifest(
+            "en-US",
+            "57.0.1",
+            "57.0",
+            "57.0.*",
+            "Firefox",
+            "/var/vcs/l10n-central",
+            "langpack-ca-valencia@firefox.mozilla.og",
+            ctx,
+            {},
+        )
+
+        data = json.loads(manifest)
+        self.assertEqual(data["name"], "Language: English (US)")
+        self.assertEqual(
+            data["description"],
+            "Firefox Language Pack for English (US) (en-US)",
+        )
 
     def test_manifest_without_contributors(self):
         ctx = {
@@ -81,8 +134,24 @@ langpack-contributors = { "" }
         )
 
         data = json.loads(manifest)
-        self.assertEqual(data["name"], "Language Pack: Suomi (Finnish)")
+        self.assertEqual(data["name"], "Language: Suomi (Finnish)")
+        self.assertEqual(
+            data["description"], "Firefox Language Pack for Suomi (fi) – Finnish"
+        )
         self.assertEqual(data["author"], "Suomennosprojekti")
+
+    def test_manifest_truncation(self):
+        locale = (
+            "Long locale code that will be truncated and will cause both "
+            "the name and the description to exceed the maximum number of "
+            "characters allowed in manifest.json"
+        )
+        title, description = langpack_manifest.get_title_and_description(
+            "Firefox", locale
+        )
+
+        self.assertEqual(len(title), 45)
+        self.assertEqual(len(description), 132)
 
 
 if __name__ == "__main__":
