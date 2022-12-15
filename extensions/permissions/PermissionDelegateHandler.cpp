1c70c5402cd29815bac0f6f018a37d64f1f2ab05
William Durand <will+git@drnd.me>
2022-12-15 10:24:17 +0000

    Bug 1437171 - Add Web MIDI support in `navigator.permissions.query()`. r=webidl,smaug,pbz
    Differential Revision: https://phabricator.services.mozilla.com/D164036

--- extensions/permissions/PermissionDelegateHandler.cpp
+++ extensions/permissions/PermissionDelegateHandler.cpp
@@ -38,6 +38,8 @@ static const DelegateInfo sPermissionsMap[] = {
     {"persistent-storage", nullptr, DelegatePolicy::ePersistDeniedCrossOrigin},
     {"vibration", nullptr, DelegatePolicy::ePersistDeniedCrossOrigin},
     {"midi", nullptr, DelegatePolicy::eDelegateUseIframeOrigin},
+    // Like "midi" but with sysex support.
+    {"midi-sysex", nullptr, DelegatePolicy::eDelegateUseIframeOrigin},
     {"storage-access", nullptr, DelegatePolicy::eDelegateUseIframeOrigin},
     {"camera", u"camera", DelegatePolicy::eDelegateUseFeaturePolicy},
     {"microphone", u"microphone", DelegatePolicy::eDelegateUseFeaturePolicy},
