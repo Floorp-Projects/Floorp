d6961758e595c17cede0621dab08d6a14d306a19
Barret Rennie <barret@brennie.ca>
2022-12-15 01:52:09 +0000

    Bug 1775849 - Import messages from messaging-system-inflight-assets as test cases for schema validation r=dmose
    Differential Revision: https://phabricator.services.mozilla.com/D161629

---
+++ browser/components/newtab/test/xpcshell/test_InflightAssetsMessageProvider.js
@@ -0,0 +1,41 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+const { InflightAssetsMessageProvider } = ChromeUtils.import(
+  "resource://testing-common/InflightAssetsMessageProvider.jsm"
+);
+
+const MESSAGE_VALIDATORS = {};
+let EXPERIMENT_VALIDATOR;
+
+add_setup(async function setup() {
+  const validators = await makeValidators();
+
+  EXPERIMENT_VALIDATOR = validators.experimentValidator;
+  Object.assign(MESSAGE_VALIDATORS, validators.messageValidators);
+});
+
+add_task(function test_InflightAssetsMessageProvider() {
+  const messages = InflightAssetsMessageProvider.getMessages();
+
+  for (const message of messages) {
+    const validator = MESSAGE_VALIDATORS[message.template];
+    Assert.ok(
+      typeof validator !== "undefined",
+      typeof validator !== "undefined"
+        ? `Schema validator found for ${message.template}`
+        : `No schema validator found for template ${message.template}. Please update this test to add one.`
+    );
+
+    assertValidates(
+      validator,
+      message,
+      `Message ${message.id} validates as ${message.template} template`
+    );
+    assertValidates(
+      EXPERIMENT_VALIDATOR,
+      message,
+      `Message ${message.id} validates as a MessagingExperiment`
+    );
+  }
+});
