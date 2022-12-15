18aae4e8e09480dd36f75a99613feae6176a45af
Mugurell <Mugurell@users.noreply.github.com>
2022-12-15 17:04:39 +0200

    Bug 1800268 - Update Focus to use the new API for autocomplete suggestions

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/input/InputToolbarIntegration.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/input/InputToolbarIntegration.kt
@@ -15,11 +15,11 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.mapNotNull
 import mozilla.components.browser.domains.autocomplete.CustomDomainsProvider
-import mozilla.components.browser.domains.autocomplete.DomainAutocompleteResult
 import mozilla.components.browser.domains.autocomplete.ShippedDomainsProvider
 import mozilla.components.browser.toolbar.BrowserToolbar
 import mozilla.components.compose.cfr.CFRPopup
 import mozilla.components.compose.cfr.CFRPopupProperties
+import mozilla.components.concept.toolbar.AutocompleteResult
 import mozilla.components.concept.toolbar.Toolbar
 import mozilla.components.lib.state.ext.flowScoped
 import mozilla.components.support.base.feature.LifecycleAwareFeature
@@ -85,7 +85,7 @@ class InputToolbarIntegration(
         }
 
         toolbar.setAutocompleteListener { text, delegate ->
-            var result: DomainAutocompleteResult? = null
+            var result: AutocompleteResult? = null
             if (useCustomDomainProvider) {
                 result = customDomainsProvider.getAutocompleteSuggestion(text)
             }
@@ -96,7 +96,7 @@ class InputToolbarIntegration(
 
             if (result != null) {
                 delegate.applyAutocompleteResult(
-                    mozilla.components.concept.toolbar.AutocompleteResult(
+                    AutocompleteResult(
                         result.input,
                         result.text,
                         result.url,
