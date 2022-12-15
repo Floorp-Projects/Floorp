d7a76ec24273eba76b7dc6b8a0f38a8e67697c67
Stanca Serban <sstanca@mozilla.com>
2022-12-15 16:17:34 +0200

    Backed out changeset 3bea0e8a2bc0 (bug 1805457) as requested by jonco. CLOSED TREE

--- js/src/vm/JSFunction.cpp
+++ js/src/vm/JSFunction.cpp
@@ -640,7 +640,7 @@ inline void JSFunction::trace(JSTracer* trc) {
   MOZ_ASSERT(!getFixedSlot(NativeJitInfoOrInterpretedScriptSlot).isGCThing());
   if (isInterpreted() && hasBaseScript()) {
     if (BaseScript* script = baseScript()) {
-      TraceManuallyBarrieredEdge(trc, &script, "JSFunction script");
+      TraceManuallyBarrieredEdge(trc, &script, "script");
       // Self-hosted scripts are shared with workers but are never relocated.
       // Skip unnecessary writes to prevent the possible data race.
       if (baseScript() != script) {
@@ -649,14 +649,14 @@ inline void JSFunction::trace(JSTracer* trc) {
       }
     }
   }
-  // wasm/asm.js exported functions need to keep WasmInstanceObject alive,
+  // wasm/asm.js exported functions need to keep WasmInstantObject alive,
   // access it via WASM_INSTANCE_SLOT extended slot.
   if (isAsmJSNative() || isWasm()) {
     const Value& v = getExtendedSlot(FunctionExtended::WASM_INSTANCE_SLOT);
     if (!v.isUndefined()) {
-      auto* instance = static_cast<js::wasm::Instance*>(v.toPrivate());
-      WasmInstanceObject* obj = instance->objectUnbarriered();
-      TraceManuallyBarrieredEdge(trc, &obj, "JSFunction wasm instance");
+      js::wasm::Instance* instance =
+          static_cast<js::wasm::Instance*>(v.toPrivate());
+      instance->trace(trc);
     }
   }
 }
