256789074f0b25ef8277aa416985bc111d366300
Ben Hearsum <bhearsum@mozilla.com>
2022-12-15 00:45:13 +0000

    Bug 1804894: Don't write postSigningData in MSIX builds r=nalexander
    This is only done by the Return-to-AMO flow, which is not supported in MSIX anyways.
    
    Also, disable the RTAMO test for MSIX builds while we're here.
    
    Differential Revision: https://phabricator.services.mozilla.com/D164666

--- browser/components/attribution/test/browser/browser_AttributionCode_telemetry.js
+++ browser/components/attribution/test/browser/browser_AttributionCode_telemetry.js
@@ -40,20 +40,27 @@ add_task(async function test_parse_error() {
   );
 
   // Write an invalid file to trigger a decode error
-  await AttributionCode.deleteFileAsync();
-  AttributionCode._clearCache();
-  // Empty string is valid on macOS.
-  await AttributionCode.writeAttributionFile(
-    AppConstants.platform == "macosx" ? "invalid" : ""
-  );
-  result = await AttributionCode.getAttrDataAsync();
-  Assert.deepEqual(result, {}, "Should have failed to parse");
+  // Skip this for MSIX packages though - we can't write or delete
+  // the attribution file there, everything happens in memory instead.
+  if (
+    AppConstants.platform === "win" &&
+    !Services.sysinfo.getProperty("hasWinPackageId")
+  ) {
+    await AttributionCode.deleteFileAsync();
+    AttributionCode._clearCache();
+    // Empty string is valid on macOS.
+    await AttributionCode.writeAttributionFile(
+      AppConstants.platform == "macosx" ? "invalid" : ""
+    );
+    result = await AttributionCode.getAttrDataAsync();
+    Assert.deepEqual(result, {}, "Should have failed to parse");
 
-  // `assertHistogram` also ensures that `read_error` index 0 is 0
-  // as we should not have recorded telemetry from the previous `getAttrDataAsync` call
-  TelemetryTestUtils.assertHistogram(histogram, INDEX_DECODE_ERROR, 1);
-  // Reset
-  histogram.clear();
+    // `assertHistogram` also ensures that `read_error` index 0 is 0
+    // as we should not have recorded telemetry from the previous `getAttrDataAsync` call
+    TelemetryTestUtils.assertHistogram(histogram, INDEX_DECODE_ERROR, 1);
+    // Reset
+    histogram.clear();
+  }
 });
 
 add_task(async function test_read_error() {
