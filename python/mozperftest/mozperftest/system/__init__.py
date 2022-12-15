b03ffa1cb426a71896861b176d71e15220183829
Stanca Serban <sstanca@mozilla.com>
2022-12-15 16:25:29 +0200

    Backed out changeset 311eb6a3a3ed (bug 1779822) for coverage related failures. CLOSED TREE

--- python/mozperftest/mozperftest/system/__init__.py
+++ python/mozperftest/mozperftest/system/__init__.py
@@ -2,16 +2,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from mozperftest.layers import Layers
+from mozperftest.system.proxy import ProxyRunner
 from mozperftest.system.android import AndroidDevice
-from mozperftest.system.android_startup import AndroidStartUp
+from mozperftest.system.profile import Profile
 from mozperftest.system.macos import MacosDevice
 from mozperftest.system.pingserver import PingServer
-from mozperftest.system.profile import Profile
-from mozperftest.system.proxy import ProxyRunner
 
 
 def get_layers():
-    return PingServer, Profile, ProxyRunner, AndroidDevice, MacosDevice, AndroidStartUp
+    return PingServer, Profile, ProxyRunner, AndroidDevice, MacosDevice
 
 
 def pick_system(env, flavor, mach_cmd):
@@ -27,9 +26,7 @@ def pick_system(env, flavor, mach_cmd):
             ),
         )
     if flavor == "mobile-browser":
-        return Layers(
-            env, mach_cmd, (Profile, ProxyRunner, AndroidDevice, AndroidStartUp)
-        )
+        return Layers(env, mach_cmd, (Profile, ProxyRunner, AndroidDevice))
     if flavor == "webpagetest":
         return Layers(env, mach_cmd, (Profile,))
     raise NotImplementedError(flavor)
