8a24ca3d3fa76e30b187d9acb9d53648e4bb3ed4
Ryan VanderMeulen <ryanvm@gmail.com>
2022-12-16 17:18:56 -0500

    Bug 1806042 - Backout bug 1790163 and bug 1473911 because they depend on the ICU update.

--- netwerk/dns/nsIDNService.h
+++ netwerk/dns/nsIDNService.h
@@ -23,10 +23,6 @@ class nsIPrefBranch;
 // nsIDNService
 //-----------------------------------------------------------------------------
 
-namespace mozilla::net {
-enum ScriptCombo : int32_t;
-}
-
 class nsIDNService final : public nsIIDNService {
  public:
   NS_DECL_THREADSAFE_ISUPPORTS
@@ -148,8 +144,7 @@ class nsIDNService final : public nsIIDNService {
    * For the "Moderately restrictive" profile, Latin is also allowed
    *  with other scripts except Cyrillic and Greek
    */
-  bool illegalScriptCombo(mozilla::intl::Script script,
-                          mozilla::net::ScriptCombo& savedScript)
+  bool illegalScriptCombo(mozilla::intl::Script script, int32_t& savedScript)
       MOZ_REQUIRES_SHARED(mLock);
 
   /**
