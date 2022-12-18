c8097879b7ddfb010df1a20c83fd5498c423271d
Kershaw Chang <kershaw@mozilla.com>
2022-12-18 12:44:38 +0000

    Bug 1740692 - Establish a speculative connection when receiving rel=preconnect in 103 response, r=necko-reviewers,manuel,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D163572

---
+++ netwerk/protocol/http/EarlyHintPreconnect.h
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_net_EarlyHintPreconnect_h
+#define mozilla_net_EarlyHintPreconnect_h
+
+#include "nsNetUtil.h"
+
+namespace mozilla::net {
+
+class EarlyHintPreconnect final {
+ public:
+  static void MaybePreconnect(const LinkHeader& aHeader, nsIURI* aBaseURI,
+                              nsIPrincipal* aPrincipal);
+
+  EarlyHintPreconnect() = delete;
+  EarlyHintPreconnect(const EarlyHintPreconnect&) = delete;
+  EarlyHintPreconnect& operator=(const EarlyHintPreconnect&) = delete;
+};
+
+}  // namespace mozilla::net
+
+#endif  // mozilla_net_EarlyHintPreconnect_h
