1939e2e7c1dd175dacc2b90ee756e7e12b94e0c4
alwu <alwu@mozilla.com>
2022-12-19 20:57:56 +0000

    Bug 1773551 - part7 : add mochitests for checking autoplay policy. r=media-playback-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D164854

---
+++ dom/media/autoplay/test/browser/browser_autoplay_policy_detection_global_and_site_sticky.js
@@ -0,0 +1,168 @@
+/**
+ * This test checks whether Autoplay Policy Detection API works correctly under
+ * different situations of having global permission set for block autoplay
+ * along with different site permission setting. This test only checks the
+ * sticky user gesture blocking model.
+ */
+"use strict";
+
+const { PermissionTestUtils } = ChromeUtils.import(
+  "resource://testing-common/PermissionTestUtils.jsm"
+);
+
+// We can't set site permission on 'about:blank' so we use an empty page.
+const PAGE_URL = GetTestWebBasedURL("file_empty.html");
+
+add_setup(async function setSharedPrefs() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["dom.media.autoplay-policy-detection.enabled", true],
+      ["media.autoplay.blocking_policy", 0],
+      ["media.autoplay.block-webaudio", true],
+    ],
+  });
+});
+
+add_task(async function testGlobalPermissionIsAllowed() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["media.autoplay.default", SpecialPowers.Ci.nsIAutoplay.ALLOWED]],
+  });
+  let tab = await createTabAndSetupPolicyAssertFunc(PAGE_URL);
+  PermissionTestUtils.add(
+    tab.linkedBrowser.currentURI,
+    "autoplay-media",
+    Services.perms.DENY_ACTION
+  );
+  await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
+    info("site permission blocks audible autoplay");
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed-muted",
+      resultForElement: "allowed-muted",
+      resultForContextType: "disallowed",
+      resultForContext: "disallowed",
+    });
+  });
+  PermissionTestUtils.add(
+    tab.linkedBrowser.currentURI,
+    "autoplay-media",
+    Ci.nsIAutoplay.BLOCKED_ALL
+  );
+  await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
+    info("site permission blocks all autoplay");
+    content.assertAutoplayPolicy({
+      resultForElementType: "disallowed",
+      resultForElement: "disallowed",
+      resultForContextType: "disallowed",
+      resultForContext: "disallowed",
+    });
+
+    info(
+      "activate document by using user gesture, all autoplay will be allowed"
+    );
+    content.document.notifyUserGestureActivation();
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed",
+      resultForElement: "allowed",
+      resultForContextType: "allowed",
+      resultForContext: "allowed",
+    });
+  });
+  PermissionTestUtils.remove(tab.linkedBrowser.currentURI, "autoplay-media");
+  BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async function testGlobalPermissionIsBlocked() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["media.autoplay.default", SpecialPowers.Ci.nsIAutoplay.BLOCKED]],
+  });
+  let tab = await createTabAndSetupPolicyAssertFunc(PAGE_URL);
+  PermissionTestUtils.add(
+    tab.linkedBrowser.currentURI,
+    "autoplay-media",
+    Services.perms.ALLOW_ACTION
+  );
+  await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
+    info("site permission allows all autoplay");
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed",
+      resultForElement: "allowed",
+      resultForContextType: "allowed",
+      resultForContext: "allowed",
+    });
+  });
+  PermissionTestUtils.add(
+    tab.linkedBrowser.currentURI,
+    "autoplay-media",
+    Ci.nsIAutoplay.BLOCKED_ALL
+  );
+  await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
+    info("site permission blocks all autoplay");
+    content.assertAutoplayPolicy({
+      resultForElementType: "disallowed",
+      resultForElement: "disallowed",
+      resultForContextType: "disallowed",
+      resultForContext: "disallowed",
+    });
+
+    info(
+      "activate document by using user gesture, all autoplay will be allowed"
+    );
+    content.document.notifyUserGestureActivation();
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed",
+      resultForElement: "allowed",
+      resultForContextType: "allowed",
+      resultForContext: "allowed",
+    });
+  });
+  PermissionTestUtils.remove(tab.linkedBrowser.currentURI, "autoplay-media");
+  BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async function testGlobalPermissionIsBlockedAll() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["media.autoplay.default", SpecialPowers.Ci.nsIAutoplay.BLOCKED_ALL]],
+  });
+  let tab = await createTabAndSetupPolicyAssertFunc(PAGE_URL);
+  PermissionTestUtils.add(
+    tab.linkedBrowser.currentURI,
+    "autoplay-media",
+    Services.perms.ALLOW_ACTION
+  );
+  await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
+    info("site permission allows all autoplay");
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed",
+      resultForElement: "allowed",
+      resultForContextType: "allowed",
+      resultForContext: "allowed",
+    });
+  });
+  PermissionTestUtils.add(
+    tab.linkedBrowser.currentURI,
+    "autoplay-media",
+    Services.perms.DENY_ACTION
+  );
+  await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
+    info("site permission blocks audible autoplay");
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed-muted",
+      resultForElement: "allowed-muted",
+      resultForContextType: "disallowed",
+      resultForContext: "disallowed",
+    });
+
+    info(
+      "activate document by using user gesture, all autoplay will be allowed"
+    );
+    content.document.notifyUserGestureActivation();
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed",
+      resultForElement: "allowed",
+      resultForContextType: "allowed",
+      resultForContext: "allowed",
+    });
+  });
+  PermissionTestUtils.remove(tab.linkedBrowser.currentURI, "autoplay-media");
+  BrowserTestUtils.removeTab(tab);
+});
---
+++ dom/media/autoplay/test/browser/browser_autoplay_policy_detection_global_sticky.js
@@ -0,0 +1,105 @@
+/**
+ * This test checks whether Autoplay Policy Detection API works correctly under
+ * different situations of having global permission set for block autoplay.
+ * This test only checks the sticky user gesture blocking model.
+ */
+"use strict";
+
+add_setup(async function setSharedPrefs() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["dom.media.autoplay-policy-detection.enabled", true],
+      ["media.autoplay.blocking_policy", 0],
+      ["media.autoplay.block-webaudio", true],
+    ],
+  });
+});
+
+add_task(async function testGlobalPermissionIsAllowed() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["media.autoplay.default", SpecialPowers.Ci.nsIAutoplay.ALLOWED]],
+  });
+  let tab = await createTabAndSetupPolicyAssertFunc("about:blank");
+  await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
+    info("global setting allows any autoplay");
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed",
+      resultForElement: "allowed",
+      resultForContextType: "allowed",
+      resultForContext: "allowed",
+    });
+  });
+  BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async function testGlobalPermissionIsBlocked() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["media.autoplay.default", SpecialPowers.Ci.nsIAutoplay.BLOCKED]],
+  });
+  let tab = await createTabAndSetupPolicyAssertFunc("about:blank");
+  await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
+    info(
+      "global setting allows inaudible autoplay but audible autoplay is still not allowed"
+    );
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed-muted",
+      resultForElement: "allowed-muted",
+      resultForContextType: "disallowed",
+      resultForContext: "disallowed",
+    });
+
+    info("tweaking video's muted attribute won't change the result");
+    content.video.muted = true;
+    is(
+      "allowed-muted",
+      content.navigator.getAutoplayPolicy(content.video),
+      "getAutoplayPolicy(video) returns correct value"
+    );
+    content.video.muted = false;
+    is(
+      "allowed-muted",
+      content.navigator.getAutoplayPolicy(content.video),
+      "getAutoplayPolicy(video) returns correct value"
+    );
+
+    info(
+      "activate document by using user gesture, all autoplay will be allowed"
+    );
+    content.document.notifyUserGestureActivation();
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed",
+      resultForElement: "allowed",
+      resultForContextType: "allowed",
+      resultForContext: "allowed",
+    });
+  });
+  BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async function testGlobalPermissionIsBlockedAll() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["media.autoplay.default", SpecialPowers.Ci.nsIAutoplay.BLOCKED_ALL]],
+  });
+  let tab = await createTabAndSetupPolicyAssertFunc("about:blank");
+  await SpecialPowers.spawn(tab.linkedBrowser, [], _ => {
+    info("global setting doesn't allow any autoplay");
+    content.assertAutoplayPolicy({
+      resultForElementType: "disallowed",
+      resultForElement: "disallowed",
+      resultForContextType: "disallowed",
+      resultForContext: "disallowed",
+    });
+
+    info(
+      "activate document by using user gesture, all autoplay will be allowed"
+    );
+    content.document.notifyUserGestureActivation();
+    content.assertAutoplayPolicy({
+      resultForElementType: "allowed",
+      resultForElement: "allowed",
+      resultForContextType: "allowed",
+      resultForContext: "allowed",
+    });
+  });
+  BrowserTestUtils.removeTab(tab);
+});
