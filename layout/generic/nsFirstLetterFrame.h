750f0bb4004f2e8f9cac143fb43d81915224b61f
Cristian Tuns <ctuns@mozilla.com>
2022-12-19 12:34:05 -0500

    Backed out 3 changesets (bug 290125) for causing build bustages on nsFirstLetterFrame.cpp CLOSED TREE
    Backed out changeset 0b6b18ea2634 (bug 290125)
    Backed out changeset d8297eee88e7 (bug 290125)
    Backed out changeset 3f341b8efb86 (bug 290125)

--- layout/generic/nsFirstLetterFrame.h
+++ layout/generic/nsFirstLetterFrame.h
@@ -79,10 +79,6 @@ class nsFirstLetterFrame final : public nsContainerFrame {
                                            nsIFrame** aContinuation,
                                            bool aIsFluid);
 
-  // Whether to use tight glyph bounds for a floating first-letter frame,
-  // or "loose" bounds based on font metrics rather than individual glyphs.
-  bool UseTightBounds() const;
-
  protected:
   nscoord mBaseline;
 
