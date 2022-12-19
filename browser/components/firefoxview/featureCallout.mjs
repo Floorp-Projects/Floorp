eb72a57ac6a18424f4b4c6c93720aa10bf9470e8
Stanca Serban <sstanca@mozilla.com>
2022-12-19 21:41:40 +0200

    Backed out changeset a92af276c945 (bug 1805930) for causing bustages in packager.mk. CLOSED TREE

--- browser/components/firefoxview/featureCallout.mjs
+++ browser/components/firefoxview/featureCallout.mjs
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/*eslint-env browser*/
-
-import { XPCOMUtils } from "resource://gre/modules/XPCOMUtils.sys.mjs";
+const { XPCOMUtils } = ChromeUtils.importESModule(
+  "resource://gre/modules/XPCOMUtils.sys.mjs"
+);
 
 const TRANSITION_MS = 500;
 const CONTAINER_ID = "root";
