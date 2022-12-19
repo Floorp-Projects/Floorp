f3b7d96ebff81d5dcdb5749ba7539afa57433a5e
Emilio Cobos Álvarez <emilio@crisal.io>
2022-12-19 16:44:36 +0000

    Bug 1806390 - Make nsContentUtils::GetLocalizedString / FormatLocalizedString safe to call from workers. r=yulia,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D165017

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -3433,7 +3433,6 @@ class nsContentUtils {
   static nsTArray<RefPtr<nsAtom>>* sUserDefinedEvents;
 
   static nsIStringBundleService* sStringBundleService;
-  static nsIStringBundle* sStringBundles[PropertiesFile_COUNT];
   class nsContentUtilsReporter;
 
   static nsIContentPolicy* sContentPolicyService;
