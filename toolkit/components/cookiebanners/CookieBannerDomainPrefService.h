281a74e29ac7d8a6bb4bb7f7636ba5eb267a74b1
Tim Huang <tihuang@mozilla.com>
2022-12-19 20:06:15 +0000

    Bug 1803601 - Part 1: Adding a Services.cookieBanners.setDomainPrefAndPersistInPrivateBrowsing() to allow persist cookie banner domain pref for the private browsing. r=pbz
    This patch adds a new function setDomainPrefAndPersistInPrivateBrowsing()
    to nsICookieBannerService. This function allows persisting domain pref
    in the private browsing mode.
    
    Differential Revision: https://phabricator.services.mozilla.com/D164797

--- toolkit/components/cookiebanners/CookieBannerDomainPrefService.h
+++ toolkit/components/cookiebanners/CookieBannerDomainPrefService.h
@@ -38,7 +38,8 @@ class CookieBannerDomainPrefService final : public nsIAsyncShutdownBlocker,
   // Set the preference for the given domain.
   [[nodiscard]] nsresult SetPref(const nsACString& aDomain,
                                  nsICookieBannerService::Modes aMode,
-                                 bool aIsPrivate);
+                                 bool aIsPrivate,
+                                 bool aPersistInPrivateBrowsing);
 
   // Remove the preference for the given domain.
   [[nodiscard]] nsresult RemovePref(const nsACString& aDomain, bool aIsPrivate);
@@ -54,6 +55,7 @@ class CookieBannerDomainPrefService final : public nsIAsyncShutdownBlocker,
   CookieBannerDomainPrefService()
       : mIsInitialized(false),
         mIsContentPrefLoaded(false),
+        mIsPrivateContentPrefLoaded(false),
         mIsShuttingDown(false) {}
 
   // Indicates whether the service is initialized.
@@ -62,21 +64,47 @@ class CookieBannerDomainPrefService final : public nsIAsyncShutdownBlocker,
   // Indicates whether the first reading of content pref completed.
   bool mIsContentPrefLoaded;
 
+  // Indicates whether the first reading of content pref for the private
+  // browsing completed.
+  bool mIsPrivateContentPrefLoaded;
+
   // Indicates whether we are shutting down.
   bool mIsShuttingDown;
 
+  // A class to represent the domain pref. It's consist of the service mode and
+  // a boolean to indicated if the domain pref persists in the disk.
+  class DomainPrefData final : public nsISupports {
+   public:
+    NS_DECL_ISUPPORTS
+
+    explicit DomainPrefData(nsICookieBannerService::Modes aMode,
+                            bool aIsPersistent)
+        : mMode(aMode), mIsPersistent(aIsPersistent) {}
+
+   private:
+    ~DomainPrefData() = default;
+
+    friend class CookieBannerDomainPrefService;
+
+    nsICookieBannerService::Modes mMode;
+    bool mIsPersistent;
+  };
+
   // Map of the per site preference keyed by domain.
-  nsTHashMap<nsCStringHashKey, nsICookieBannerService::Modes> mPrefs;
+  nsTHashMap<nsCStringHashKey, RefPtr<DomainPrefData>> mPrefs;
 
   // Map of the per site preference for private windows keyed by domain.
-  nsTHashMap<nsCStringHashKey, nsICookieBannerService::Modes> mPrefsPrivate;
+  nsTHashMap<nsCStringHashKey, RefPtr<DomainPrefData>> mPrefsPrivate;
 
   // A helper function that will wait until the initialization of the content
   // pref completed.
-  void EnsureInitCompleted();
+  void EnsureInitCompleted(bool aIsPrivate);
 
   nsresult AddShutdownBlocker();
   nsresult RemoveShutdownBlocker();
+
+  nsresult RemoveContentPrefForDomain(const nsACString& aDomain,
+                                      bool aIsPrivate);
 
   class BaseContentPrefCallback : public nsIContentPrefCallback2 {
    public:
@@ -99,8 +127,11 @@ class CookieBannerDomainPrefService final : public nsIAsyncShutdownBlocker,
     NS_DECL_NSICONTENTPREFCALLBACK2
 
     explicit InitialLoadContentPrefCallback(
-        CookieBannerDomainPrefService* aService)
-        : BaseContentPrefCallback(aService) {}
+        CookieBannerDomainPrefService* aService, bool aIsPrivate)
+        : BaseContentPrefCallback(aService), mIsPrivate(aIsPrivate) {}
+
+   private:
+    bool mIsPrivate;
   };
 
   class WriteContentPrefCallback final : public BaseContentPrefCallback {
