8103a818db8c90f24a4f15b0c0deea80b23e9f4f
Alexandre Poirot <poirot.alex@gmail.com>
2022-12-20 19:09:55 +0000

    Bug 1805378 - [devtools] Reuse toolbox parser worker from the webconsole. r=bomsy
    Note that we still have to instantiate a Webconsole dedicated one in case of Browser Console,
    which won't have any toolbox offhand.
    
    Differential Revision: https://phabricator.services.mozilla.com/D164545

--- devtools/client/webconsole/webconsole.js
+++ devtools/client/webconsole/webconsole.js
@@ -323,6 +323,12 @@ class WebConsole {
   }
 
   get parserService() {
+    // If we have a toolbox, we could reuse the parser already instantiated for the debugger.
+    // Note that we won't have a toolbox when running the Browser Console...
+    if (this.toolbox) {
+      return this.toolbox.parserService;
+    }
+
     if (this._parserService) {
       return this._parserService;
     }
@@ -332,9 +338,7 @@ class WebConsole {
     } = require("resource://devtools/client/debugger/src/workers/parser/index.js");
 
     this._parserService = new ParserDispatcher();
-    this._parserService.start(
-      "resource://devtools/client/debugger/dist/parser-worker.js"
-    );
+    this._parserService.start();
     return this._parserService;
   }
 
