36e6bda07af05b8305c24ebac978f43800cc6f0d
Marian-Vasile Laza <mlaza@mozilla.com>
2022-12-20 22:52:57 +0200

    Backed out changeset 4be804d1c686 (bug 1806500) for causing mochitest failures on test_has_permissions.html. CLOSED TREE

--- uriloader/exthandler/tests/mochitest/browser_shows_where_to_save_dialog.js
+++ uriloader/exthandler/tests/mochitest/browser_shows_where_to_save_dialog.js
@@ -273,7 +273,7 @@ async function setupFilePickerDirectory() {
     try {
       await IOUtils.remove(saveDir.path, { recursive: true });
     } catch (e) {
-      console.error(e);
+      Cu.reportError(e);
     }
   });
 
