55a93fba0fa9d17881188cfc7e75998bbda08e3b
Alexandre Poirot <poirot.alex@gmail.com>
2022-12-20 19:09:55 +0000

    Bug 1805378 - [devtools] Convert WorkerDispatcher to ES Class. r=bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D164546

--- devtools/client/debugger/babel.config.js
+++ devtools/client/debugger/babel.config.js
@@ -21,6 +21,7 @@ module.exports = {
         /[/\\]node_modules[/\\]devtools-/,
         /[/\\]node_modules[/\\]react-aria-components[/\\]/,
         "../../shared",
+        "../shared/worker-utils.js",
       ],
       presets: [
         "@babel/preset-react",
