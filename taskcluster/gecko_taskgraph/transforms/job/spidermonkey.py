3a0277ed6cc1f07e9169e47e68e181b95ebe924c
Csoregi Natalia <ncsoregi@mozilla.com>
2022-12-20 04:38:47 +0200

    Backed out changeset 00838fbbc755 (bug 1806073) for causing clang bustage. CLOSED TREE

--- taskcluster/gecko_taskgraph/transforms/job/spidermonkey.py
+++ taskcluster/gecko_taskgraph/transforms/job/spidermonkey.py
@@ -102,7 +102,7 @@ def generic_worker_spidermonkey(config, job, taskdesc):
 
     run["using"] = "run-task"
     run["command"] = [
-        "c:\\mozilla-build\\msys2\\usr\\bin\\bash.exe "  # string concat
+        "c:\\mozilla-build\\msys\\bin\\bash.exe "  # string concat
         '"./src/taskcluster/scripts/builder/%s"' % script
     ]
 
