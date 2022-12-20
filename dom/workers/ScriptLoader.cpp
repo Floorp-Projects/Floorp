1f63ccd52939e33f4224729e3154a290d24a01e1
Yulia Startsev <ystartsev@mozilla.com>
2022-12-20 20:56:12 +0000

    Bug 1247687 - Implement InitModuleLoader methods for WorkerScriptLoader and WorkerGlobalScope; r=jonco
    This implements a method to initialize the moduleLoader for workers. This will initialize only once, for all worker types (module and classic).
    
    Depends on D147324
    
    Differential Revision: https://phabricator.services.mozilla.com/D147326

--- dom/workers/ScriptLoader.cpp
+++ dom/workers/ScriptLoader.cpp
@@ -411,8 +411,20 @@ WorkerScriptLoader::WorkerScriptLoader(
   }
 
   nsIGlobalObject* global = GetGlobal();
-
   mController = global->GetController();
+  // Set up the module loader, if it has not been yet.
+  // TODO: Implement this for classic scripts when dynamic imports are added.
+  if (aWorkerPrivate->WorkerType() == WorkerType::Module) {
+    InitModuleLoader();
+  }
+}
+
+void WorkerScriptLoader::InitModuleLoader() {
+  mWorkerRef->Private()->AssertIsOnWorkerThread();
+  RefPtr<WorkerModuleLoader> moduleLoader =
+      new WorkerModuleLoader(this, GetGlobal(), mSyncLoopTarget.get());
+  static_cast<WorkerGlobalScopeBase*>(GetGlobal())
+      ->InitModuleLoader(moduleLoader);
 }
 
 void WorkerScriptLoader::CreateScriptRequests(
