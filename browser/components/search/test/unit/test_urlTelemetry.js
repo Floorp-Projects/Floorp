919ff11c51cafb5a1e6d636c167774005770e56f
Stephanie Cunnane <scunnane@mozilla.com>
2022-12-20 20:34:08 +0000

    Bug 1806722 - Remove SERP telemetry for Yahoo - pre-110 clients only. r=standard8, a=test-only

--- browser/components/search/test/unit/test_urlTelemetry.js
+++ browser/components/search/test/unit/test_urlTelemetry.js
@@ -65,18 +65,6 @@ const TESTS = [
     expectedSearchCountEntry: "google.in-content:organic:none",
   },
   {
-    title: "Yahoo organic",
-    trackingUrl:
-      "https://search.yahoo.com/search?p=test&fr=yfp-t&fp=1&toggle=1&cop=mss&ei=UTF-8",
-    expectedSearchCountEntry: "yahoo.in-content:organic:none",
-  },
-  {
-    title: "Yahoo organic UK",
-    trackingUrl:
-      "https://uk.search.yahoo.com/search?p=test&fr=yfp-t&fp=1&toggle=1&cop=mss&ei=UTF-8",
-    expectedSearchCountEntry: "yahoo.in-content:organic:none",
-  },
-  {
     title: "Bing search access point",
     trackingUrl: "https://www.bing.com/search?q=test&pc=MOZI&form=MOZLBR",
     expectedSearchCountEntry: "bing.in-content:sap:MOZI",
