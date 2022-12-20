524810d78a0aa3febfbfe7f10797bf12fe70c683
Sylvestre Ledru <sledru@mozilla.com>
2022-12-20 18:28:50 +0000

    Bug 1763971 - bump MarkupSafe in tree r=firefox-source-docs-reviewers,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D165092

--- third_party/python/MarkupSafe/setup.py
+++ third_party/python/MarkupSafe/setup.py
@@ -1,25 +1,13 @@
-from __future__ import print_function
-
-import io
-import re
+import os
+import platform
 import sys
 from distutils.errors import CCompilerError
 from distutils.errors import DistutilsExecError
 from distutils.errors import DistutilsPlatformError
 
 from setuptools import Extension
-from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.build_ext import build_ext
-
-with io.open("README.rst", "rt", encoding="utf8") as f:
-    readme = f.read()
-
-with io.open("src/markupsafe/__init__.py", "rt", encoding="utf8") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
-
-is_jython = "java" in sys.platform
-is_pypy = hasattr(sys, "pypy_version_info")
 
 ext_modules = [Extension("markupsafe._speedups", ["src/markupsafe/_speedups.c"])]
 
@@ -52,42 +40,6 @@ class ve_build_ext(build_ext):
 def run_setup(with_binary):
     setup(
         name="MarkupSafe",
-        version=version,
-        url="https://palletsprojects.com/p/markupsafe/",
-        project_urls={
-            "Documentation": "https://markupsafe.palletsprojects.com/",
-            "Code": "https://github.com/pallets/markupsafe",
-            "Issue tracker": "https://github.com/pallets/markupsafe/issues",
-        },
-        license="BSD-3-Clause",
-        author="Armin Ronacher",
-        author_email="armin.ronacher@active-4.com",
-        maintainer="The Pallets Team",
-        maintainer_email="contact@palletsprojects.com",
-        description="Safely add untrusted strings to HTML/XML markup.",
-        long_description=readme,
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Environment :: Web Environment",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: BSD License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.4",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-            "Topic :: Text Processing :: Markup :: HTML",
-        ],
-        packages=find_packages("src"),
-        package_dir={"": "src"},
-        include_package_data=True,
-        python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
         cmdclass={"build_ext": ve_build_ext},
         ext_modules=ext_modules if with_binary else [],
     )
@@ -100,7 +52,11 @@ def show_message(*lines):
     print("=" * 74)
 
 
-if not (is_pypy or is_jython):
+supports_speedups = platform.python_implementation() not in {"PyPy", "Jython"}
+
+if os.environ.get("CIBUILDWHEEL", "0") == "1" and supports_speedups:
+    run_setup(True)
+elif supports_speedups:
     try:
         run_setup(True)
     except BuildFailed:
