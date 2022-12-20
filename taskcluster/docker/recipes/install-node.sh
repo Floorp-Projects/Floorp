2de9934652ec311322d50d1ee160431c23442450
Mark Banner <standard8@mozilla.com>
2022-12-20 07:56:04 +0000

    Bug 1806497 - Update node to v16.19.0. r=firefox-build-system-reviewers,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D165065

--- taskcluster/docker/recipes/install-node.sh
+++ taskcluster/docker/recipes/install-node.sh
@@ -8,8 +8,8 @@
 # taskcluster/scripts/misc/repack-node.sh. Later we'll get the ESLint builder
 # to use the linux64-node toolchain directly.
 
-wget -O node.xz --progress=dot:mega https://nodejs.org/dist/v16.17.1/node-v16.17.1-linux-x64.tar.xz
-echo '06ba2eb34aa385967f5f58c87a44753f83212f6cccea892b33f80a2e7fda8384' node.xz | sha256sum -c
+wget -O node.xz --progress=dot:mega https://nodejs.org/dist/v16.19.0/node-v16.19.0-linux-x64.tar.xz
+echo 'c88b52497ab38a3ddf526e5b46a41270320409109c3f74171b241132984fd08f' node.xz | sha256sum -c
 tar -C /usr/local -xJ --strip-components 1 < node.xz
 node -v  # verify
 npm -v
