4fa7d2d32d1cd7dbf190562e9c9309dc20815db4
Daisuke Akatsuka <daisuke@birchill.co.jp>
2022-12-20 00:55:36 +0000

    Bug 1805717: Introduce search_mode property. r=mak,jteow
    Differential Revision: https://phabricator.services.mozilla.com/D164794

---
+++ browser/components/urlbar/tests/browser/browser_glean_telemetry_engagement_search_mode.js
@@ -0,0 +1,109 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test for the following data of engagement telemetry.
+// - search_mode
+
+/* import-globals-from head-glean.js */
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/browser/components/urlbar/tests/browser/head-glean.js",
+  this
+);
+
+add_setup(async function() {
+  await setup();
+});
+
+add_task(async function search_mode_search_not_mode() {
+  await doTest(async browser => {
+    await openPopup("x");
+    await doEnter();
+
+    assertEngagementTelemetry([{ search_mode: "" }]);
+  });
+});
+
+add_task(async function search_mode_search_engine() {
+  await doTest(async browser => {
+    await openPopup("x");
+    await UrlbarTestUtils.enterSearchMode(window);
+    await doEnter();
+
+    assertEngagementTelemetry([{ search_mode: "search_engine" }]);
+  });
+});
+
+add_task(async function search_mode_bookmarks() {
+  await doTest(async browser => {
+    await PlacesUtils.bookmarks.insert({
+      parentGuid: PlacesUtils.bookmarks.unfiledGuid,
+      url: "https://example.com/bookmark",
+      title: "bookmark",
+    });
+
+    await openPopup("bookmark");
+    await UrlbarTestUtils.enterSearchMode(window, {
+      source: UrlbarUtils.RESULT_SOURCE.BOOKMARKS,
+    });
+    await selectRowByURL("https://example.com/bookmark");
+    await doEnter();
+
+    assertEngagementTelemetry([{ search_mode: "bookmarks" }]);
+  });
+});
+
+add_task(async function search_mode_history() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.urlbar.autoFill", false]],
+  });
+
+  await doTest(async browser => {
+    await PlacesTestUtils.addVisits("https://example.com/test");
+
+    await openPopup("example");
+    await UrlbarTestUtils.enterSearchMode(window, {
+      source: UrlbarUtils.RESULT_SOURCE.HISTORY,
+    });
+    await selectRowByURL("https://example.com/test");
+    await doEnter();
+
+    assertEngagementTelemetry([{ search_mode: "history" }]);
+  });
+
+  await SpecialPowers.popPrefEnv();
+});
+
+add_task(async function search_mode_tabs() {
+  const tab = BrowserTestUtils.addTab(gBrowser, "https://example.com/");
+
+  await doTest(async browser => {
+    await openPopup("example");
+    await UrlbarTestUtils.enterSearchMode(window, {
+      source: UrlbarUtils.RESULT_SOURCE.TABS,
+    });
+    await selectRowByProvider("Places");
+    EventUtils.synthesizeKey("KEY_Enter");
+    await BrowserTestUtils.waitForCondition(() => gBrowser.selectedTab === tab);
+
+    assertEngagementTelemetry([{ search_mode: "tabs" }]);
+  });
+
+  BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async function search_mode_actions() {
+  await doTest(async browser => {
+    await openPopup("add");
+    await UrlbarTestUtils.enterSearchMode(window, {
+      source: UrlbarUtils.RESULT_SOURCE.ACTIONS,
+    });
+    await selectRowByProvider("quickactions");
+    const onLoad = BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
+    doClickSubButton(".urlbarView-quickaction-row[data-key=addons]");
+    await onLoad;
+
+    assertEngagementTelemetry([{ search_mode: "actions" }]);
+  });
+});
