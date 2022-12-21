e97286add359b071539b418e29e1066dae562bc8
Julian Descottes <jdescottes@mozilla.com>
2022-12-21 08:52:55 +0000

    Bug 1806419 - [devtools] Add test helper to wait for a specific target and use it in bfcache color simulation test r=nchevobbe
    See detailed comment on the bug for the investigation
    
    Differential Revision: https://phabricator.services.mozilla.com/D165168

--- devtools/client/inspector/rules/test/browser_rules_color_scheme_simulation_bfcache.js
+++ devtools/client/inspector/rules/test/browser_rules_color_scheme_simulation_bfcache.js
@@ -57,8 +57,25 @@ add_task(async function testBfCacheNavigationAfterClosingDevTools() {
   await waitFor(async () => isSimulationEnabled());
   is(await isSimulationEnabled(), true, "color scheme simulation is enabled");
 
+  // Wait for the iframe target to be processed before destroying the toolbox,
+  // to avoid unhandled promise rejections.
+  // The iframe URL starts with https://example.org/document-builder.sjs
+  let onIframeProcessed;
+
+  // Do not wait for the additional target in the noeft-nofis flavor.
+  const isNoEFTNoFis = !isFissionEnabled() && !isEveryFrameTargetEnabled();
+  if (!isNoEFTNoFis) {
+    const iframeURL = "https://example.org/document-builder.sjs";
+    onIframeProcessed = waitForTargetProcessed(toolbox.commands, targetFront =>
+      targetFront.url.startsWith(iframeURL)
+    );
+  }
+
   info("Navigate to a different URL");
   await navigateTo(TEST_URI + "?someparameter");
+
+  info("Wait for the iframe target to be processed by target-command");
+  await onIframeProcessed;
 
   info("Close DevTools to disable the simulation");
   await toolbox.destroy();
