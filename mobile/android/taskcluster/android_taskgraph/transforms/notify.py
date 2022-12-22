95ad913c35f0a6e1acd18130b808042d028fdefd
Johan Lorenzo <jlorenzo@mozilla.com>
2022-12-22 11:01:31 +0100

    Bug 1806884 - part 2: Make `release-notify-push` explain what happened to Android-Components

---
+++ mobile/android/taskcluster/android_taskgraph/transforms/notify.py
@@ -0,0 +1,24 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from taskgraph.transforms.base import TransformSequence
+from taskgraph.util.schema import resolve_keyed_by
+
+
+transforms = TransformSequence()
+
+
+@transforms.add
+def resolve_keys(config, tasks):
+    for task in tasks:
+        for key in ("notifications.message", "notifications.emails"):
+            resolve_keyed_by(
+                task,
+                key,
+                item_name=task["name"],
+                **{
+                    'level': config.params["level"],
+                }
+            )
+        yield task
