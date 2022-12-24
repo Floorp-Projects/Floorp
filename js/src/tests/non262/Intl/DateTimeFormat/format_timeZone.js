337faccc249c69f14dfcda779169e6958a28eec6
Jonathan Kew <jkew@mozilla.com>
2022-12-24 10:35:10 +0000

    Bug 1806042 - Update tests to expect normal spaces rather than U+202F/2009 in formatted date/time strings. r=gregtatum
    We'll need to revert this when we're ready to disable the workaround.
    
    Depends on D165408
    
    Differential Revision: https://phabricator.services.mozilla.com/D165409

--- js/src/tests/non262/Intl/DateTimeFormat/format_timeZone.js
+++ js/src/tests/non262/Intl/DateTimeFormat/format_timeZone.js
@@ -22,24 +22,24 @@ const tzMapper = [
 const tests = [
     {
         timeZone: "UTC",
-        result: "12/6/2012, 12:00:00\u202FPM",
+        result: "12/6/2012, 12:00:00 PM",
     },
     {
         timeZone: "America/Los_Angeles",
-        result: "12/6/2012, 4:00:00\u202FAM",
+        result: "12/6/2012, 4:00:00 AM",
     },
     {
         timeZone: "America/New_York",
         options: tzNameFormatOptions,
-        result: "12/6/2012, 7:00:00\u202FAM EST",
+        result: "12/6/2012, 7:00:00 AM EST",
     },
     {
         timeZone: "America/Caracas",
-        result: "12/6/2012, 7:30:00\u202FAM",
+        result: "12/6/2012, 7:30:00 AM",
     },
     {
         timeZone: "Europe/London",
-        result: "12/6/2012, 12:00:00\u202FPM",
+        result: "12/6/2012, 12:00:00 PM",
     },
     {
         timeZone: "Africa/Casablanca",
@@ -53,7 +53,7 @@ const tests = [
     },
     {
         timeZone: "Asia/Kathmandu",
-        result: "12/6/2012, 5:45:00\u202FPM",
+        result: "12/6/2012, 5:45:00 PM",
     },
     {
         timeZone: "Asia/Bangkok",
@@ -67,27 +67,27 @@ const tests = [
     },
     {
         timeZone: "Australia/Lord_Howe",
-        result: "12/6/2012, 11:00:00\u202FPM",
+        result: "12/6/2012, 11:00:00 PM",
     },
     {
         timeZone: "Australia/Lord_Howe",
         date: Date.UTC(2012, 7-1, 6, 12, 0, 0),
-        result: "7/6/2012, 10:30:00\u202FPM",
+        result: "7/6/2012, 10:30:00 PM",
     },
     {
         timeZone: "Pacific/Kiritimati",
         date: Date.UTC(1978, 12-1, 6, 12, 0, 0),
-        result: "12/6/1978, 1:20:00\u202FAM",
+        result: "12/6/1978, 1:20:00 AM",
     },
     {
         timeZone: "Africa/Monrovia",
         date: Date.UTC(1971, 12-1, 6, 12, 0, 0),
-        result: "12/6/1971, 11:15:30\u202FAM",
+        result: "12/6/1971, 11:15:30 AM",
     },
     {
         timeZone: "Asia/Riyadh",
         date: Date.UTC(1946, 12-1, 6, 12, 0, 0),
-        result: "12/6/1946, 3:06:52\u202FPM",
+        result: "12/6/1946, 3:06:52 PM",
     },
 ];
 
