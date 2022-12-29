36928e3fb8ee74a6a0c82392397176158dfef32c
Mike Hommey <mh+mozilla@glandium.org>
2022-12-29 00:05:49 +0000

    Bug 1807780 - Use -ivfsoverlay instead of liblowercase. r=firefox-build-system-reviewers,ahochheiden
    Because this involves affecting the MSVC + Windows SDK packs, we need to
    do this for toolchain tasks as well as Firefox builds in a single pass.
    
    The MSVC + Windows SDK packs are altered to keep the original case
    instead of lowercasing everything (except .lib files), and contain
    an overlay file that we now automatically use from the WINSYSROOT.
    
    This requires adjusting some paths to match what the original case
    is, as well as removing everything related to the use of liblowercase,
    which conflicts with the use of the overlay file. People using
    liblowercase locally will still have a working setup as long as they
    don't set WINSYSROOT (it's new, so they won't have it), and don't have
    an overlay file in there (which they only would  if they ran the new
    pack_vs.py).
    
    Differential Revision: https://phabricator.services.mozilla.com/D165596

--- build/vs/pack_vs.py
+++ build/vs/pack_vs.py
@@ -40,28 +40,64 @@ if __name__ == "__main__":
         unpacked = tmpdir / "unpack"
         extractPackages(selected, dl_cache, unpacked)
         stem = Path(Path(args.output.stem).stem)
+        vfs = {}
         # Create an archive containing all the paths in lowercase form for
         # cross-compiles.
-        with open(args.output, "wb") as f:
-            with ZstdCompressor().stream_writer(f) as z:
-                with tarfile.open(mode="w|", fileobj=z) as tar:
-                    for subpath, dest in (
-                        ("VC", "vc"),
-                        ("Program Files/Windows Kits/10", "windows kits/10"),
-                        ("DIA SDK", "dia sdk"),
-                    ):
-                        subpath = unpacked / subpath
-                        dest = Path(dest)
-                        for root, dirs, files in os.walk(subpath):
-                            relpath = Path(root).relative_to(subpath)
-                            for f in files:
-                                path = Path(root) / f
-                                info = tar.gettarinfo(path)
-                                with open(path, "rb") as fh:
-                                    info.name = str(stem / dest / relpath / f).lower()
-                                    # Set executable flag on .exe files, the Firefox build
-                                    # system wants it.
-                                    if info.name.endswith(".exe"):
-                                        info.mode |= (info.mode & 0o444) >> 2
-                                    print("Adding", info.name)
-                                    tar.addfile(info, fh)
+        with ZstdCompressor().stream_writer(open(args.output, "wb")) as z, tarfile.open(
+            mode="w|", fileobj=z
+        ) as tar:
+            for subpath in ("VC", "Program Files/Windows Kits/10", "DIA SDK"):
+                dest = subpath
+                if dest.startswith("Program Files/"):
+                    dest = dest[len("Program Files/") :]
+                subpath = unpacked / subpath
+                dest = Path(dest)
+                for root, dirs, files in os.walk(subpath):
+                    relpath = Path(root).relative_to(subpath)
+                    for f in files:
+                        path = Path(root) / f
+                        info = tar.gettarinfo(path)
+                        with open(path, "rb") as fh:
+                            lower_f = f.lower()
+                            # Ideally, we'd use the overlay for .libs too but as of
+                            # writing it's still impractical to use, so lowercase
+                            # them for now, that'll be enough.
+                            if lower_f.endswith(".lib"):
+                                f = lower_f
+                            info.name = str(stem / dest / relpath / f)
+                            # Set executable flag on .exe files, the Firefox build
+                            # system wants it.
+                            if lower_f.endswith(".exe"):
+                                info.mode |= (info.mode & 0o444) >> 2
+                            print("Adding", info.name)
+                            tar.addfile(info, fh)
+                            if lower_f.endswith((".h", ".idl")):
+                                vfs.setdefault(str(dest / relpath), []).append(f)
+            # Create an overlay file for use with clang's -ivfsoverlay flag.
+            overlay = {
+                "version": 0,
+                "case-sensitive": False,
+                "root-relative": "overlay-dir",
+                "overlay-relative": True,
+                "roots": [
+                    {
+                        "name": p,
+                        "type": "directory",
+                        "contents": [
+                            {
+                                "name": f,
+                                "type": "file",
+                                "external-contents": f"{p}/{f}",
+                            }
+                            for f in files
+                        ],
+                    }
+                    for p, files in vfs.items()
+                ],
+            }
+            overlay_yaml = tmpdir / "overlay.yaml"
+            with overlay_yaml.open("w") as fh:
+                fh.write(yaml.dump(overlay))
+            info = tar.gettarinfo(overlay_yaml)
+            info.name = str(stem / "overlay.yaml")
+            tar.addfile(info, overlay_yaml.open("rb"))
