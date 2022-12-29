d0f0355ad69f2ed17481d88b7fcafeb701be6c7e
Mike Hommey <mh+mozilla@glandium.org>
2022-12-29 21:08:08 +0000

    Bug 1806766 - Update unicode-segmentation to 1.10.0. r=emilio,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D165637

--- third_party/rust/unicode-segmentation/src/testdata.rs
+++ third_party/rust/unicode-segmentation/src/testdata.rs
@@ -12,7 +12,7 @@
 
 #![allow(missing_docs, non_upper_case_globals, non_snake_case)]
 // official Unicode test data
-// http://www.unicode.org/Public/14.0.0/ucd/auxiliary/GraphemeBreakTest.txt
+// http://www.unicode.org/Public/15.0.0/ucd/auxiliary/GraphemeBreakTest.txt
 pub const TEST_SAME: &'static [(&'static str, &'static [&'static str])] = &[
     ("\u{20}\u{20}", &["\u{20}", "\u{20}"]),
     ("\u{20}\u{308}\u{20}", &["\u{20}\u{308}", "\u{20}"]),
@@ -886,7 +886,7 @@ pub const TEST_DIFF: &'static [(
 ];
 
 // official Unicode test data
-// http://www.unicode.org/Public/14.0.0/ucd/auxiliary/WordBreakTest.txt
+// http://www.unicode.org/Public/15.0.0/ucd/auxiliary/WordBreakTest.txt
 pub const TEST_WORD: &'static [(&'static str, &'static [&'static str])] = &[
     ("\u{1}\u{1}", &["\u{1}", "\u{1}"]),
     ("\u{1}\u{308}\u{1}", &["\u{1}\u{308}", "\u{1}"]),
@@ -4917,7 +4917,7 @@ pub const TEST_WORD: &'static [(&'static str, &'static [&'static str])] = &[
 ];
 
 // official Unicode test data
-// http://www.unicode.org/Public/14.0.0/ucd/auxiliary/SentenceBreakTest.txt
+// http://www.unicode.org/Public/15.0.0/ucd/auxiliary/SentenceBreakTest.txt
 pub const TEST_SENTENCE: &'static [(&'static str, &'static [&'static str])] = &[
         ("\u{1}\u{1}", &["\u{1}\u{1}"]), ("\u{1}\u{308}\u{1}", &["\u{1}\u{308}\u{1}"]),
         ("\u{1}\u{d}", &["\u{1}\u{d}"]), ("\u{1}\u{308}\u{d}", &["\u{1}\u{308}\u{d}"]),
