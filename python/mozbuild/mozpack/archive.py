c15bc53a01571310196467f2404cd044d91d3e24
Mike Hommey <mh+mozilla@glandium.org>
2022-12-30 08:21:31 +0000

    Bug 1807872 - Make tarball generation more deterministic across versions of python. r=firefox-build-system-reviewers,andi
    Differential Revision: https://phabricator.services.mozilla.com/D165647

--- python/mozbuild/mozpack/archive.py
+++ python/mozbuild/mozpack/archive.py
@@ -13,6 +13,38 @@ from .files import BaseFile, File
 DEFAULT_MTIME = 1451606400
 
 
+# Python 3.9 contains this change:
+#  https://github.com/python/cpython/commit/674935b8caf33e47c78f1b8e197b1b77a04992d2
+# which changes the output of tar creation compared to earlier versions.
+# As this code is used to generate tar files that are meant to be deterministic
+# across versions of python (specifically, it's used as part of computing the hash
+# of docker images, which needs to be identical between CI (which uses python 3.8),
+# and developer environments (using arbitrary versions of python, at this point,
+# most probably more recent than 3.9)).
+# What we do is subblass TarInfo so that if used on python >= 3.9, it reproduces the
+# behavior from python < 3.9.
+# Here's how it goes:
+# - the behavior in python >= 3.9 is the same as python < 3.9 when the type encoded
+# in the tarinfo is CHRTYPE or BLKTYPE.
+# - the value of the type is only compared in the context of choosing which behavior
+# to take
+# - we replace the type with the same value (so that using the value has no changes)
+# but that pretends to be the same as CHRTYPE so that the condition that enables the
+# old behavior is taken.
+class HackedType(bytes):
+    def __eq__(self, other):
+        if other == tarfile.CHRTYPE:
+            return True
+        return self == other
+
+
+class TarInfo(tarfile.TarInfo):
+    @staticmethod
+    def _create_header(info, format, encoding, errors):
+        info["type"] = HackedType(info["type"])
+        return tarfile.TarInfo._create_header(info, format, encoding, errors)
+
+
 def create_tar_from_files(fp, files):
     """Create a tar file deterministically.
 
@@ -35,7 +67,7 @@ def create_tar_from_files(fp, files):
             if not isinstance(f, BaseFile):
                 f = File(f)
 
-            ti = tarfile.TarInfo(archive_path)
+            ti = TarInfo(archive_path)
             ti.mode = f.mode or 0o0644
             ti.type = tarfile.REGTYPE
 
--- python/mozbuild/mozpack/test/test_archive.py
+++ python/mozbuild/mozpack/test/test_archive.py
@@ -6,7 +6,6 @@ import hashlib
 import os
 import shutil
 import stat
-import sys
 import tarfile
 import tempfile
 import unittest
@@ -111,16 +110,8 @@ class TestArchive(unittest.TestCase):
             with open(tp, "wb") as fh:
                 create_tar_from_files(fh, files)
 
-            # Output should be deterministic. Checking for Python version because of Bug 1735396.
-            if sys.version_info.major == 3 and sys.version_info.minor <= 8:
-                self.assertEqual(
-                    file_hash(tp), "01cd314e277f060e98c7de6c8ea57f96b3a2065c"
-                )
-            # The else statement below assumes Python 3.9 and newer.
-            else:
-                self.assertEqual(
-                    file_hash(tp), "50fe1aaadf0d44abb69bdea174b4dd6cdb4f4898"
-                )
+            # Output should be deterministic.
+            self.assertEqual(file_hash(tp), "01cd314e277f060e98c7de6c8ea57f96b3a2065c")
 
             with tarfile.open(tp, "r") as tf:
                 self._verify_basic_tarfile(tf)
@@ -159,14 +150,7 @@ class TestArchive(unittest.TestCase):
             with open(gp, "wb") as fh:
                 create_tar_gz_from_files(fh, files)
 
-            if sys.version_info.major == 3 and sys.version_info.minor <= 8:
-                self.assertEqual(
-                    file_hash(gp), "7c4da5adc5088cdf00911d5daf9a67b15de714b7"
-                )
-            else:
-                self.assertEqual(
-                    file_hash(gp), "766117873af9754915ee93c56243616d000d7e5e"
-                )
+            self.assertEqual(file_hash(gp), "7c4da5adc5088cdf00911d5daf9a67b15de714b7")
 
             with tarfile.open(gp, "r:gz") as tf:
                 self._verify_basic_tarfile(tf)
@@ -183,14 +167,7 @@ class TestArchive(unittest.TestCase):
             with open(gp, "wb") as fh:
                 create_tar_gz_from_files(fh, files, filename="foobar")
 
-            if sys.version_info.major == 3 and sys.version_info.minor <= 8:
-                self.assertEqual(
-                    file_hash(gp), "721e00083c17d16df2edbddf40136298c06d0c49"
-                )
-            else:
-                self.assertEqual(
-                    file_hash(gp), "059916c8e6b6f22be774dc56aabb3819460fee53"
-                )
+            self.assertEqual(file_hash(gp), "721e00083c17d16df2edbddf40136298c06d0c49")
 
             with tarfile.open(gp, "r:gz") as tf:
                 self._verify_basic_tarfile(tf)
@@ -207,14 +184,7 @@ class TestArchive(unittest.TestCase):
             with open(bp, "wb") as fh:
                 create_tar_bz2_from_files(fh, files)
 
-            if sys.version_info.major == 3 and sys.version_info.minor <= 8:
-                self.assertEqual(
-                    file_hash(bp), "eb5096d2fbb71df7b3d690001a6f2e82a5aad6a7"
-                )
-            else:
-                self.assertEqual(
-                    file_hash(bp), "600883e25722fdde5eb78c6c40955ab80b7ac114"
-                )
+            self.assertEqual(file_hash(bp), "eb5096d2fbb71df7b3d690001a6f2e82a5aad6a7")
 
             with tarfile.open(bp, "r:bz2") as tf:
                 self._verify_basic_tarfile(tf)
