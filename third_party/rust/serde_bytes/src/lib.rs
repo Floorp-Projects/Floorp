ddb6586ca780beeb6d419ca74c4ef22a1be449fb
Mike Hommey <mh+mozilla@glandium.org>
2022-12-30 00:10:28 +0000

    Bug 1806766 - Update serde_bytes to 0.11.8. r=emilio,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D165707

--- third_party/rust/serde_bytes/src/lib.rs
+++ third_party/rust/serde_bytes/src/lib.rs
@@ -27,7 +27,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_bytes/0.11.7")]
+#![doc(html_root_url = "https://docs.rs/serde_bytes/0.11.8")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
 #![allow(
