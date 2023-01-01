83f941efb1dbb80e7239a20eacd7b16e30156e13
Jeff Muizelaar <jmuizelaar@mozilla.com>
2023-01-01 00:46:16 +0000

    Bug 1808099 - Make wr_glyph_rasterizer's glean dependency optional. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D165762

--- gfx/wr/wr_glyph_rasterizer/src/telemetry.rs
+++ gfx/wr/wr_glyph_rasterizer/src/telemetry.rs
@@ -2,9 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#[cfg(feature = "gecko")]
 use glean::TimerId;
 #[cfg(feature = "gecko")]
 use fog::metrics::wr;
+
+#[cfg(not(feature = "gecko"))]
+pub struct TimerId;
 
 pub struct Telemetry;
 
@@ -12,7 +16,7 @@ pub struct Telemetry;
 #[cfg(not(feature = "gecko"))]
 impl Telemetry {
     // Start rasterize glyph time collection
-    pub fn start_rasterize_glyphs_time() -> TimerId { return TimerId { id: 0 }; }
+    pub fn start_rasterize_glyphs_time() -> TimerId { return TimerId {}; }
     // End rasterize glyph time collection
     pub fn stop_and_accumulate_rasterize_glyphs_time(_id: TimerId) { }
 }
