f7b23def1c12504f484d6c50bb74132f0c929be3
Henrik Skupin <mail@hskupin.info>
2023-01-01 20:24:00 +0000

    Bug 1692468 - [marionette] Improve unit tests for element references. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D164740

---
+++ testing/marionette/harness/marionette_harness/tests/unit/test_element_id.py
@@ -0,0 +1,55 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import re
+from urllib.parse import quote
+
+from marionette_driver.by import By
+from marionette_driver.errors import NoSuchElementException, InvalidSelectorException
+from marionette_driver.marionette import HTMLElement
+
+from marionette_harness import MarionetteTestCase
+
+
+def inline(doc):
+    return "data:text/html;charset=utf-8,{}".format(quote(doc))
+
+
+id_html = inline("<p id=foo></p>")
+
+
+class TestElementID(MarionetteTestCase):
+    def setUp(self):
+        MarionetteTestCase.setUp(self)
+        self.marionette.timeout.implicit = 0
+
+    def test_id_is_valid_uuid(self):
+        self.marionette.navigate(id_html)
+        el = self.marionette.find_element(By.TAG_NAME, "p")
+        uuid_regex = re.compile(
+            "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
+        )
+        self.assertIsNotNone(
+            re.search(uuid_regex, el.id),
+            "UUID for the WebElement is not valid. ID is {}".format(el.id),
+        )
+
+    def test_id_identical_for_the_same_element(self):
+        self.marionette.navigate(id_html)
+        found = self.marionette.find_element(By.ID, "foo")
+        self.assertIsInstance(found, HTMLElement)
+
+        found_again = self.marionette.find_element(By.ID, "foo")
+        self.assertEqual(found_again, found)
+
+    def test_id_unique_per_session(self):
+        self.marionette.navigate(id_html)
+        found = self.marionette.find_element(By.ID, "foo")
+        self.assertIsInstance(found, HTMLElement)
+
+        self.marionette.delete_session()
+        self.marionette.start_session()
+
+        found_again = self.marionette.find_element(By.ID, "foo")
+        self.assertNotEqual(found_again.id, found.id)
---
+++ testing/marionette/harness/marionette_harness/tests/unit/test_element_id_chrome.py
@@ -0,0 +1,88 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from marionette_driver.by import By
+from marionette_driver.errors import NoSuchElementException
+from marionette_driver.marionette import HTMLElement
+
+from marionette_harness import MarionetteTestCase, parameterized, WindowManagerMixin
+
+
+PAGE_XHTML = "chrome://remote/content/marionette/test_no_xul.xhtml"
+PAGE_XUL = "chrome://remote/content/marionette/test.xhtml"
+
+
+class TestElementIDChrome(WindowManagerMixin, MarionetteTestCase):
+    def setUp(self):
+        super(TestElementIDChrome, self).setUp()
+
+        self.marionette.set_context("chrome")
+
+    def tearDown(self):
+        self.close_all_windows()
+
+        super(TestElementIDChrome, self).tearDown()
+
+    @parameterized("XUL", PAGE_XUL)
+    @parameterized("XHTML", PAGE_XHTML)
+    def test_id_identical_for_the_same_element(self, chrome_url):
+        win = self.open_chrome_window(chrome_url)
+        self.marionette.switch_to_window(win)
+
+        found_el = self.marionette.find_element(By.ID, "textInput")
+        self.assertEqual(HTMLElement, type(found_el))
+
+        found_el_new = self.marionette.find_element(By.ID, "textInput")
+        self.assertEqual(found_el_new.id, found_el.id)
+
+    @parameterized("XUL", PAGE_XUL)
+    @parameterized("XHTML", PAGE_XHTML)
+    def test_id_unique_per_session(self, chrome_url):
+        win = self.open_chrome_window(chrome_url)
+        self.marionette.switch_to_window(win)
+
+        found_el = self.marionette.find_element(By.ID, "textInput")
+        self.assertEqual(HTMLElement, type(found_el))
+
+        self.marionette.delete_session()
+        self.marionette.start_session()
+
+        self.marionette.set_context("chrome")
+        self.marionette.switch_to_window(win)
+
+        found_el_new = self.marionette.find_element(By.ID, "textInput")
+        self.assertNotEqual(found_el_new.id, found_el.id)
+
+    @parameterized("XUL", PAGE_XUL)
+    @parameterized("XHTML", PAGE_XHTML)
+    def test_id_no_such_element_in_another_chrome_window(self, chrome_url):
+        original_handle = self.marionette.current_window_handle
+
+        win = self.open_chrome_window(chrome_url)
+        self.marionette.switch_to_window(win)
+
+        found_el = self.marionette.find_element(By.ID, "textInput")
+        self.assertEqual(HTMLElement, type(found_el))
+
+        self.marionette.switch_to_window(original_handle)
+
+        with self.assertRaises(NoSuchElementException):
+            found_el.get_property("localName")
+
+    @parameterized("XUL", PAGE_XUL)
+    @parameterized("XHTML", PAGE_XHTML)
+    def test_id_removed_when_chrome_window_is_closed(self, chrome_url):
+        original_handle = self.marionette.current_window_handle
+
+        win = self.open_chrome_window(chrome_url)
+        self.marionette.switch_to_window(win)
+
+        found_el = self.marionette.find_element(By.ID, "textInput")
+        self.assertEqual(HTMLElement, type(found_el))
+
+        self.marionette.close_chrome_window()
+        self.marionette.switch_to_window(original_handle)
+
+        with self.assertRaises(NoSuchElementException):
+            found_el.get_property("localName")
