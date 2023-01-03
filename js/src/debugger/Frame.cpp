5c5cfd204c4fd1148a1fff5c3b209a2bb2e0d151
Jan de Mooij <jdemooij@mozilla.com>
2023-01-03 16:01:11 +0000

    Bug 1808171 part 5 - Use PlainObject for Debugger.Frame prototype object. r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D165800

--- js/src/debugger/Frame.cpp
+++ js/src/debugger/Frame.cpp
@@ -171,14 +171,7 @@ bool ScriptedOnPopHandler::onPop(JSContext* cx, Handle<DebuggerFrame*> frame,
 
 size_t ScriptedOnPopHandler::allocSize() const { return sizeof(*this); }
 
-// The Debugger.Frame.prototype object also has a class of
-// DebuggerFrame::class_ so we differentiate instances from the prototype
-// based on the presence of an owner debugger.
-bool js::DebuggerFrame::isInstance() const {
-  return !getReservedSlot(OWNER_SLOT).isUndefined();
-}
 js::Debugger* js::DebuggerFrame::owner() const {
-  MOZ_ASSERT(isInstance());
   JSObject* dbgobj = &getReservedSlot(OWNER_SLOT).toObject();
   return Debugger::fromJSObject(dbgobj);
 }
@@ -227,7 +220,7 @@ bool DebuggerFrame::hasAnyHooks() const {
 NativeObject* DebuggerFrame::initClass(JSContext* cx,
                                        Handle<GlobalObject*> global,
                                        HandleObject dbgCtor) {
-  return InitClass(cx, dbgCtor, &class_, nullptr, "Frame", construct, 0,
+  return InitClass(cx, dbgCtor, nullptr, nullptr, "Frame", construct, 0,
                    properties_, methods_, nullptr, nullptr);
 }
 
@@ -1250,18 +1243,7 @@ DebuggerFrame* DebuggerFrame::check(JSContext* cx, HandleValue thisv) {
     return nullptr;
   }
 
-  Rooted<DebuggerFrame*> frame(cx, &thisobj->as<DebuggerFrame>());
-
-  // Forbid Debugger.Frame.prototype, which is of class DebuggerFrame::class_
-  // but isn't really a working Debugger.Frame object.
-  if (!frame->isInstance()) {
-    JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
-                              JSMSG_INCOMPATIBLE_PROTO, "Debugger.Frame",
-                              "method", "prototype object");
-    return nullptr;
-  }
-
-  return frame;
+  return &thisobj->as<DebuggerFrame>();
 }
 
 struct MOZ_STACK_CLASS DebuggerFrame::CallData {
