9eb32e822ddbc2ba71700f10d82283ab412dea87
Oriol Brufau <oriol-bugzilla@hotmail.com>
2023-01-03 13:09:08 +0000

    Bug 1375328 - Allow single quotes and backticks to quote strings in reps. r=nchevobbe
    This will benefit strings coming from outerHTML or JSON.stringify(), which tend
    to contain lots of double quotes that had to be escaped.
    
    That's the same approach as Chromium, except that it prioritizes double quotes
    over single ones to reduce the change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D165809

--- devtools/client/shared/components/test/node/components/reps/string.test.js
+++ devtools/client/shared/components/test/node/components/reps/string.test.js
@@ -71,8 +71,8 @@ const testCases = [
       useQuotes: true,
     },
     result:
-      '"\\t\\n\\r\\"\'\\\\\\u001f\\u009f\\ufeff\\ufffe\\ud8000\\u2063' +
-      '\\ufffc\\u2028\\ueeee\ufffd"',
+      "`\\t\\n\\r\"'\\\\\\u001f\\u009f\\ufeff\\ufffe\\ud8000\\u2063" +
+      "\\ufffc\\u2028\\ueeee\ufffd`",
   },
   {
     name: "testUnpairedSurrogate",
@@ -168,6 +168,70 @@ const testCases = [
       transformEmptyString: false,
     },
     result: `""`,
+  },
+  {
+    name: "testQuotingSingleQuote",
+    props: {
+      object: "'",
+      useQuotes: true,
+    },
+    result: `"'"`,
+  },
+  {
+    name: "testQuotingDoubleQuote",
+    props: {
+      object: '"',
+      useQuotes: true,
+    },
+    result: `'"'`,
+  },
+  {
+    name: "testQuotingBacktick",
+    props: {
+      object: "`",
+      useQuotes: true,
+    },
+    result: '"`"',
+  },
+  {
+    name: "testQuotingSingleAndDoubleQuotes",
+    props: {
+      object: "'\"",
+      useQuotes: true,
+    },
+    result: "`'\"`",
+  },
+  {
+    name: "testQuotingSingleAndDoubleQuotesAnd${",
+    props: {
+      object: "'\"${",
+      useQuotes: true,
+    },
+    result: '"\'\\"${"',
+  },
+  {
+    name: "testQuotingSingleQuoteAndBacktick",
+    props: {
+      object: "'`",
+      useQuotes: true,
+    },
+    result: '"\'`"',
+  },
+  {
+    name: "testQuotingDoubleQuoteAndBacktick",
+    props: {
+      object: '"`',
+      useQuotes: true,
+    },
+    result: "'\"`'",
+  },
+  {
+    name: "testQuotingSingleAndDoubleQuotesAndBacktick",
+    props: {
+      object: "'\"`",
+      useQuotes: true,
+    },
+    result: '"\'\\"`"',
   },
 ];
 
