a5c21c51600ff38be2e332483e29e3c84ebcd36d
Zach Hoffman <zach@zrhoffman.net>
2023-01-03 15:30:19 +0000

    Bug 1808304 - Represent FirstChild, FirstOfType, LastChild, LastOfType, OnlyChild, and OnlyOfType as functionless Nth variants r=emilio
    Like bug 1808226, this doesn't change any behavior, it just simplifies matching.
    
    I also added a WPT JavaScript test for :only-of-type, since dedicated WPT
    JavScript tests already exist for the other pseudo-classes this patch
    touches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D165859

--- servo/components/selectors/builder.rs
+++ servo/components/selectors/builder.rs
@@ -310,16 +310,10 @@ where
             Component::AttributeInNoNamespace { .. } |
             Component::AttributeInNoNamespaceExists { .. } |
             Component::AttributeOther(..) |
-            Component::FirstChild |
-            Component::LastChild |
-            Component::OnlyChild |
             Component::Root |
             Component::Empty |
             Component::Scope |
             Component::Nth(..) |
-            Component::FirstOfType |
-            Component::LastOfType |
-            Component::OnlyOfType |
             Component::NonTSPseudoClass(..) => {
                 specificity.class_like_selectors += 1;
             },
