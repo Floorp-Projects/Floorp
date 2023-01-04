7d1473de0cced9d232096583e695e768121e4d6e
Dan Baker <dbaker@mozilla.com>
2023-01-04 15:13:49 -0700

    Bug 1806510 - Vendor libwebrtc from c6c346da61
    Upstream commit: https://webrtc.googlesource.com/src/+/c6c346da617983922057cceb1b77f2a9e38fdfdb
        Remove usage of rtc::MessageHandler in pc/remote_audio_source
    
        Bug: webrtc:9702
        Change-Id: Ibef43b8c1b61afe4cf4e79a7c6549af6d5bff93f
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/272546
        Reviewed-by: Florent Castelli <orphis@webrtc.org>
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#37859}

--- third_party/libwebrtc/pc/remote_audio_source.cc
+++ third_party/libwebrtc/pc/remote_audio_source.cc
@@ -14,15 +14,15 @@
 
 #include <memory>
 #include <string>
+#include <utility>
 
 #include "absl/algorithm/container.h"
 #include "api/scoped_refptr.h"
 #include "api/sequence_checker.h"
+#include "api/task_queue/task_queue_base.h"
 #include "rtc_base/checks.h"
-#include "rtc_base/location.h"
 #include "rtc_base/logging.h"
 #include "rtc_base/strings/string_format.h"
-#include "rtc_base/thread.h"
 
 namespace webrtc {
 
@@ -51,9 +51,9 @@ class RemoteAudioSource::AudioDataProxy : public AudioSinkInterface {
 };
 
 RemoteAudioSource::RemoteAudioSource(
-    rtc::Thread* worker_thread,
+    TaskQueueBase* worker_thread,
     OnAudioChannelGoneAction on_audio_channel_gone_action)
-    : main_thread_(rtc::Thread::Current()),
+    : main_thread_(TaskQueueBase::Current()),
       worker_thread_(worker_thread),
       on_audio_channel_gone_action_(on_audio_channel_gone_action),
       state_(MediaSourceInterface::kInitializing) {
@@ -163,24 +163,18 @@ void RemoteAudioSource::OnAudioChannelGone() {
   if (on_audio_channel_gone_action_ != OnAudioChannelGoneAction::kEnd) {
     return;
   }
-  // Called when the audio channel is deleted.  It may be the worker thread
-  // in libjingle or may be a different worker thread.
-  // This object needs to live long enough for the cleanup logic in OnMessage to
-  // run, so take a reference to it as the data. Sometimes the message may not
-  // be processed (because the thread was destroyed shortly after this call),
-  // but that is fine because the thread destructor will take care of destroying
-  // the message data which will release the reference on RemoteAudioSource.
-  main_thread_->Post(RTC_FROM_HERE, this, 0,
-                     new rtc::ScopedRefMessageData<RemoteAudioSource>(this));
-}
-
-void RemoteAudioSource::OnMessage(rtc::Message* msg) {
-  RTC_DCHECK_RUN_ON(main_thread_);
-  sinks_.clear();
-  SetState(MediaSourceInterface::kEnded);
-  // Will possibly delete this RemoteAudioSource since it is reference counted
-  // in the message.
-  delete msg->pdata;
+  // Called when the audio channel is deleted. It may be the worker thread or
+  // may be a different task queue.
+  // This object needs to live long enough for the cleanup logic in the posted
+  // task to run, so take a reference to it. Sometimes the task may not be
+  // processed (because the task queue was destroyed shortly after this call),
+  // but that is fine because the task queue destructor will take care of
+  // destroying task which will release the reference on RemoteAudioSource.
+  rtc::scoped_refptr<RemoteAudioSource> thiz(this);
+  main_thread_->PostTask([thiz = std::move(thiz)] {
+    thiz->sinks_.clear();
+    thiz->SetState(MediaSourceInterface::kEnded);
+  });
 }
 
 }  // namespace webrtc
