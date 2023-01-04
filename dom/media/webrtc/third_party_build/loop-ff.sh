3bdbd7ce0e2e0bbc98b51f6c7916f0a64112bc72
Michael Froman <mfroman@mozilla.com>
2023-01-04 17:25:43 +0000

    Bug 1808347 - pt1 - rename MOZ_GIT_RELEASE_BRANCH to MOZ_TARGET_UPSTREAM_BRANCH_HEAD. r=dbaker DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D165876

--- dom/media/webrtc/third_party_build/loop-ff.sh
+++ dom/media/webrtc/third_party_build/loop-ff.sh
@@ -35,8 +35,8 @@ if [ "x$MOZ_LIBWEBRTC_COMMIT" = "x" ]; then
 fi
 
 if [ "x$MOZ_STOP_AFTER_COMMIT" = "x" ]; then
-  MOZ_STOP_AFTER_COMMIT=`cd $MOZ_LIBWEBRTC_SRC ; git show $MOZ_GIT_RELEASE_BRANCH --format='%h' --name-only | head -1`
-  echo "No MOZ_STOP_AFTER_COMMIT variable defined - stopping at $MOZ_GIT_RELEASE_BRANCH"
+  MOZ_STOP_AFTER_COMMIT=`cd $MOZ_LIBWEBRTC_SRC ; git show $MOZ_TARGET_UPSTREAM_BRANCH_HEAD --format='%h' --name-only | head -1`
+  echo "No MOZ_STOP_AFTER_COMMIT variable defined - stopping at $MOZ_TARGET_UPSTREAM_BRANCH_HEAD"
 fi
 
 if [ "x$SKIP_NEXT_REVERT_CHK" = "x" ]; then
--- dom/media/webrtc/third_party_build/use_config_env.sh
+++ dom/media/webrtc/third_party_build/use_config_env.sh
@@ -28,7 +28,7 @@ function find_base_commit()
   # if we've advanced into a chrome release branch, we need to adjust the
   # MOZ_LIBWEBRTC_BASE to the last common commit so we can now advance up
   # the trunk commits.
-  MOZ_LIBWEBRTC_BASE=`cd $MOZ_LIBWEBRTC_SRC ; git merge-base $MOZ_LIBWEBRTC_BASE $MOZ_GIT_RELEASE_BRANCH`
+  MOZ_LIBWEBRTC_BASE=`cd $MOZ_LIBWEBRTC_SRC ; git merge-base $MOZ_LIBWEBRTC_BASE $MOZ_TARGET_UPSTREAM_BRANCH_HEAD`
   # now make it a short hash
   MOZ_LIBWEBRTC_BASE=`cd $MOZ_LIBWEBRTC_SRC ; git rev-parse --short $MOZ_LIBWEBRTC_BASE`
   echo "adjusted MOZ_LIBWEBRTC_BASE: $MOZ_LIBWEBRTC_BASE"
@@ -38,6 +38,6 @@ function find_next_commit()
 {
   # identify the next commit above our current base commit
   MOZ_LIBWEBRTC_NEXT_BASE=`cd $MOZ_LIBWEBRTC_SRC ; \
-  git log --oneline --ancestry-path $MOZ_LIBWEBRTC_BASE^..$MOZ_GIT_RELEASE_BRANCH \
+  git log --oneline --ancestry-path $MOZ_LIBWEBRTC_BASE^..$MOZ_TARGET_UPSTREAM_BRANCH_HEAD \
    | tail -2 | head -1 | awk '{print $1;}'`
 }
