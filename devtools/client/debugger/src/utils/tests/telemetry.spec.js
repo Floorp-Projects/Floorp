567fa911b641e180a69b2de52b4d52ac4728d9b3
Alexandre Poirot <poirot.alex@gmail.com>
2023-01-04 18:28:34 +0000

    Bug 1807898 - [devtools] Stop passing a sessionID to Toolbox. r=devtools-reviewers,nchevobbe
    Telemetry class now self-compute the session ID attribute
    and we no longer need to pass one down to Toolbox.
    
    Differential Revision: https://phabricator.services.mozilla.com/D165717

--- devtools/client/debugger/src/utils/tests/telemetry.spec.js
+++ devtools/client/debugger/src/utils/tests/telemetry.spec.js
@@ -2,32 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-jest.mock("devtools/client/shared/telemetry", () => {
-  function MockTelemetry() {}
-  MockTelemetry.prototype.recordEvent = jest.fn();
-
-  return MockTelemetry;
-});
-
-const Telemetry = require("devtools/client/shared/telemetry");
-
 import { recordEvent } from "../telemetry";
-
-const telemetry = new Telemetry();
 
 describe("telemetry.recordEvent()", () => {
   it("Receives the correct telemetry information", () => {
     recordEvent("foo", { bar: 1 });
 
-    expect(telemetry.recordEvent).toHaveBeenCalledWith(
-      "foo",
-      "debugger",
-      null,
-      {
-        // eslint-disable-next-line camelcase
-        session_id: -1,
-        bar: 1,
-      }
-    );
+    expect(window.dbg._telemetry.events.foo).toStrictEqual([{ bar: 1 }]);
   });
 });
