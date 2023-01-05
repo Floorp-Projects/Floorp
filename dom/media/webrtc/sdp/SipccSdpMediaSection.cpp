5c2f937be81c916d69d2b967cf5812954c27f7e5
Byron Campen [:bwc] <docfaraday@gmail.com>
2023-01-05 20:35:27 +0000

    Bug 1807193: Remove direction validation for simulcast in sipcc code. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D165991

--- dom/media/webrtc/sdp/SipccSdpMediaSection.cpp
+++ dom/media/webrtc/sdp/SipccSdpMediaSection.cpp
@@ -242,14 +242,6 @@ bool SipccSdpMediaSection::ValidateSimulcast(sdp_t* sdp, uint16_t level,
 bool SipccSdpMediaSection::ValidateSimulcastVersions(
     sdp_t* sdp, uint16_t level, const SdpSimulcastAttribute::Versions& versions,
     sdp::Direction direction, InternalResults& results) const {
-  if (versions.IsSet() && !(direction & GetDirectionAttribute().mValue)) {
-    results.AddParseError(sdp_get_media_line_number(sdp, level),
-                          "simulcast attribute has a direction that is "
-                          "inconsistent with the direction of this media "
-                          "section.");
-    return false;
-  }
-
   for (const SdpSimulcastAttribute::Version& version : versions) {
     for (const SdpSimulcastAttribute::Encoding& encoding : version.choices) {
       const SdpRidAttributeList::Rid* ridAttr = FindRid(encoding.rid);
