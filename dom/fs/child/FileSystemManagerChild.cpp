0d895cfaf99eb46be61657363264209a359d84bd
Jan Varga <jvarga@mozilla.com>
2023-01-06 19:45:26 +0000

    Bug 1806362 - Recreate FileSystemManagerChild after it was destroyed; r=dom-storage-reviewers,jari
    The actor is destroyed during origin clearing or resetting.
    
    Depends on D163635
    
    Differential Revision: https://phabricator.services.mozilla.com/D165069

--- dom/fs/child/FileSystemManagerChild.cpp
+++ dom/fs/child/FileSystemManagerChild.cpp
@@ -7,11 +7,20 @@
 #include "FileSystemManagerChild.h"
 
 #include "FileSystemAccessHandleChild.h"
+#include "FileSystemBackgroundRequestHandler.h"
 #include "FileSystemWritableFileStreamChild.h"
 #include "mozilla/dom/FileSystemSyncAccessHandle.h"
 #include "mozilla/dom/FileSystemWritableFileStream.h"
 
 namespace mozilla::dom {
+
+void FileSystemManagerChild::SetBackgroundRequestHandler(
+    FileSystemBackgroundRequestHandler* aBackgroundRequestHandler) {
+  MOZ_ASSERT(aBackgroundRequestHandler);
+  MOZ_ASSERT(!mBackgroundRequestHandler);
+
+  mBackgroundRequestHandler = aBackgroundRequestHandler;
+}
 
 #ifdef DEBUG
 bool FileSystemManagerChild::AllSyncAccessHandlesClosed() const {
@@ -82,6 +91,13 @@ FileSystemManagerChild::AllocPFileSystemWritableFileStreamChild() {
                      aValues) { resolver(NS_OK); });
 
   return IPC_OK();
+}
+
+void FileSystemManagerChild::ActorDestroy(ActorDestroyReason aWhy) {
+  if (mBackgroundRequestHandler) {
+    mBackgroundRequestHandler->ClearActor();
+    mBackgroundRequestHandler = nullptr;
+  }
 }
 
 }  // namespace mozilla::dom
