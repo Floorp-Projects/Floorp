4294bb8c4f386fc3eb3015fa961cf3269930e5af
Mark Banner <standard8@mozilla.com>
2023-01-07 10:59:06 +0000

    Bug 1808941 - Enable ESLint rules no-restricted-globals and strict for newtab. r=aminomancer
    The 'strict' rule is only enabled for mjs files, as per Mozilla's recommended config.
    
    Depends on D166196
    
    Differential Revision: https://phabricator.services.mozilla.com/D166197

--- browser/components/newtab/common/Actions.sys.mjs
+++ browser/components/newtab/common/Actions.sys.mjs
@@ -1,7 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-"use strict";
 
 export const MAIN_MESSAGE_TYPE = "ActivityStream:Main";
 export const CONTENT_MESSAGE_TYPE = "ActivityStream:Content";
