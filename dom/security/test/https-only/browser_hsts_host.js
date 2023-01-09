b364a19bd90152ca4d3b5bf2725796a9c4d1f26a
Narcis Beleuzu <nbeleuzu@mozilla.com>
2023-01-09 22:51:57 +0200

    Backed out changeset 30d7636620f8 (bug 1804684) as req by smaug

--- dom/security/test/https-only/browser_hsts_host.js
+++ dom/security/test/https-only/browser_hsts_host.js
@@ -16,7 +16,6 @@ add_task(async function see_hsts_header() {
       "https://example.com"
     ) + "hsts_headers.sjs";
   Services.obs.addObserver(observer, "http-on-examine-response");
-  info("Load first time with HSTS header");
   await BrowserTestUtils.loadURI(gBrowser.selectedBrowser, setHstsUrl);
 
   await BrowserTestUtils.waitForCondition(() => readMessage);
@@ -42,16 +41,12 @@ add_task(async function() {
     ) + "hsts_headers.sjs";
 
   // 1. Upgrade page to https://
-  let tab = await BrowserTestUtils.openNewForegroundTab(
-    gBrowser,
-    RESOURCE_LINK
-  );
+  await BrowserTestUtils.loadURI(gBrowser.selectedBrowser, RESOURCE_LINK);
 
   await BrowserTestUtils.waitForCondition(() => testFinished);
 
   // Clean up
   Services.console.unregisterListener(onNewMessage);
-  BrowserTestUtils.removeTab(tab);
 });
 
 add_task(async function() {
@@ -65,7 +60,6 @@ add_task(async function() {
 
   Services.obs.addObserver(observer, "http-on-examine-response");
   // reset hsts header
-  info("RESET HSTS header");
   await BrowserTestUtils.loadURI(gBrowser.selectedBrowser, clearHstsUrl);
   await BrowserTestUtils.waitForCondition(() => readMessage);
   // Clean up
