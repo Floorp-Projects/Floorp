8d0ccf7d902b06a92102ed756c22ed971a504cc6
Mike Hommey <mh+mozilla@glandium.org>
2023-01-09 22:29:40 +0000

    Bug 1807874 - Restore the use of forked libdmg-hfsplus for system symbols. r=gsvelto
    The toolchain one has problems with some of the dmgs we scrape.
    
    Differential Revision: https://phabricator.services.mozilla.com/D165648

--- taskcluster/docker/system-symbols-mac/setup.sh
+++ taskcluster/docker/system-symbols-mac/setup.sh
@@ -11,6 +11,14 @@ cd xar/xar
 make "$ncpu" && make install
 
 cd $WORK
+git clone --depth=1 --single-branch -b system-symbols-mac https://github.com/gabrielesvelto/libdmg-hfsplus.git
+cd libdmg-hfsplus
+cmake .
+make "$ncpu" dmg-bin hfsplus
+# `make install` installs way too much stuff
+cp dmg/dmg hfs/hfsplus /builds/worker/bin
+strip /builds/worker/bin/dmg /builds/worker/bin/hfsplus
+
 pip3 install --no-cache-dir git+https://github.com/mozilla/reposado
 
 python3 /usr/local/bin/repoutil --configure <<EOF
