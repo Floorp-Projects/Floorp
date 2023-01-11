64379462df48c042f77f54ba2c711dd780c48324
Gregory Pappas <gp3033@protonmail.com>
2023-01-11 14:44:36 +0000

    Bug 1708686 - Show extension icons in about:performance r=florian
    Differential Revision: https://phabricator.services.mozilla.com/D165789

--- toolkit/components/aboutperformance/content/aboutPerformance.js
+++ toolkit/components/aboutperformance/content/aboutPerformance.js
@@ -372,7 +372,12 @@ var State = {
           continue;
         }
         name = `${addon.name} (${addon.id})`;
-        image = "chrome://mozapps/skin/extensions/extension.svg";
+        image =
+          AddonManager.getPreferredIconURL(
+            addon.extension.manifest,
+            32,
+            window
+          ) || "chrome://mozapps/skin/extensions/extension.svg";
         type = gSystemAddonIds.has(addon.id) ? "system-addon" : "addon";
       } else if (id == 0 && !tab.isWorker) {
         name = { id: "ghost-windows" };
