1d5db6b6cfa0ed1d8599c44ed67c409d89f07917
Emilio Cobos Álvarez <emilio@crisal.io>
2023-01-11 22:16:38 +0000

    Bug 1792057 - Make sure to focus inner text field when creating / rebuilding edit fields in datetime widget. r=smaug
    The blur is a regression from bug 1740989, but it's really uncovering a
    regression from bug 1729342, sorta. Before that, we used to focus the
    inner text field on rebuild via focusInnerTextBox().
    
    One could argue that the focus fixup rule should really deal with this,
    but I guess since content can't really unattach shadow DOM, it's less of
    a general issue and more of an implementation detail.
    
    The test uncovers another pre-existing issue (switching from date ->
    test was also broken, and we left a bogus ElementState::FOCUS state in
    the element).
    
    Differential Revision: https://phabricator.services.mozilla.com/D158167

--- dom/base/nsFocusManager.cpp
+++ dom/base/nsFocusManager.cpp
@@ -865,71 +865,84 @@ nsresult nsFocusManager::ContentRemoved(Document* aDocument,
   // shadow-including inclusive ancestor of the currently focused element,
   // reset the focus within that window.
   Element* content = window->GetFocusedElement();
-  if (content &&
-      nsContentUtils::ContentIsHostIncludingDescendantOf(content, aContent)) {
-    window->SetFocusedElement(nullptr);
-
-    // if this window is currently focused, clear the global focused
-    // element as well, but don't fire any events.
-    if (window->GetBrowsingContext() == GetFocusedBrowsingContext()) {
-      mFocusedElement = nullptr;
-    } else {
-      // Check if the node that was focused is an iframe or similar by looking
-      // if it has a subdocument. This would indicate that this focused iframe
-      // and its descendants will be going away. We will need to move the
-      // focus somewhere else, so just clear the focus in the toplevel window
-      // so that no element is focused.
-      //
-      // The Fission case is handled in FlushAndCheckIfFocusable().
-      Document* subdoc = aDocument->GetSubDocumentFor(content);
-      if (subdoc) {
-        nsCOMPtr<nsIDocShell> docShell = subdoc->GetDocShell();
-        if (docShell) {
-          nsCOMPtr<nsPIDOMWindowOuter> childWindow = docShell->GetWindow();
-          if (childWindow &&
-              IsSameOrAncestor(childWindow, GetFocusedBrowsingContext())) {
-            if (XRE_IsParentProcess()) {
-              nsCOMPtr<nsPIDOMWindowOuter> activeWindow = mActiveWindow;
-              ClearFocus(activeWindow);
-            } else {
-              BrowsingContext* active = GetActiveBrowsingContext();
-              if (active) {
-                if (active->IsInProcess()) {
-                  nsCOMPtr<nsPIDOMWindowOuter> activeWindow =
-                      active->GetDOMWindow();
-                  ClearFocus(activeWindow);
-                } else {
-                  mozilla::dom::ContentChild* contentChild =
-                      mozilla::dom::ContentChild::GetSingleton();
-                  MOZ_ASSERT(contentChild);
-                  contentChild->SendClearFocus(active);
-                }
-              }  // no else, because ClearFocus does nothing with nullptr
-            }
-          }
-        }
-      }
-    }
-
-    // Notify the editor in case we removed its ancestor limiter.
-    if (content->IsEditable()) {
-      nsCOMPtr<nsIDocShell> docShell = aDocument->GetDocShell();
-      if (docShell) {
-        RefPtr<HTMLEditor> htmlEditor = docShell->GetHTMLEditor();
-        if (htmlEditor) {
-          RefPtr<Selection> selection = htmlEditor->GetSelection();
-          if (selection && selection->GetFrameSelection() &&
-              content == selection->GetFrameSelection()->GetAncestorLimiter()) {
-            htmlEditor->FinalizeSelection();
-          }
-        }
-      }
-    }
-
-    NotifyFocusStateChange(content, nullptr, 0, /* aGettingFocus = */ false,
-                           false);
+  if (!content) {
+    return NS_OK;
   }
 
+  if (!nsContentUtils::ContentIsHostIncludingDescendantOf(content, aContent)) {
+    return NS_OK;
+  }
+
+  Element* newFocusedElement = [&]() -> Element* {
+    if (auto* sr = ShadowRoot::FromNode(aContent)) {
+      if (sr->IsUAWidget() && sr->Host()->IsHTMLElement(nsGkAtoms::input)) {
+        return sr->Host();
+      }
+    }
+    return nullptr;
+  }();
+
+  window->SetFocusedElement(newFocusedElement);
+
+  // if this window is currently focused, clear the global focused
+  // element as well, but don't fire any events.
+  if (window->GetBrowsingContext() == GetFocusedBrowsingContext()) {
+    mFocusedElement = newFocusedElement;
+  } else if (Document* subdoc = aDocument->GetSubDocumentFor(content)) {
+    // Check if the node that was focused is an iframe or similar by looking if
+    // it has a subdocument. This would indicate that this focused iframe
+    // and its descendants will be going away. We will need to move the focus
+    // somewhere else, so just clear the focus in the toplevel window so that no
+    // element is focused.
+    //
+    // The Fission case is handled in FlushAndCheckIfFocusable().
+    if (nsCOMPtr<nsIDocShell> docShell = subdoc->GetDocShell()) {
+      nsCOMPtr<nsPIDOMWindowOuter> childWindow = docShell->GetWindow();
+      if (childWindow &&
+          IsSameOrAncestor(childWindow, GetFocusedBrowsingContext())) {
+        if (XRE_IsParentProcess()) {
+          nsCOMPtr<nsPIDOMWindowOuter> activeWindow = mActiveWindow;
+          ClearFocus(activeWindow);
+        } else {
+          BrowsingContext* active = GetActiveBrowsingContext();
+          if (active) {
+            if (active->IsInProcess()) {
+              nsCOMPtr<nsPIDOMWindowOuter> activeWindow =
+                  active->GetDOMWindow();
+              ClearFocus(activeWindow);
+            } else {
+              mozilla::dom::ContentChild* contentChild =
+                  mozilla::dom::ContentChild::GetSingleton();
+              MOZ_ASSERT(contentChild);
+              contentChild->SendClearFocus(active);
+            }
+          }  // no else, because ClearFocus does nothing with nullptr
+        }
+      }
+    }
+  }
+
+  // Notify the editor in case we removed its ancestor limiter.
+  if (content->IsEditable()) {
+    if (nsCOMPtr<nsIDocShell> docShell = aDocument->GetDocShell()) {
+      if (RefPtr<HTMLEditor> htmlEditor = docShell->GetHTMLEditor()) {
+        RefPtr<Selection> selection = htmlEditor->GetSelection();
+        if (selection && selection->GetFrameSelection() &&
+            content == selection->GetFrameSelection()->GetAncestorLimiter()) {
+          htmlEditor->FinalizeSelection();
+        }
+      }
+    }
+  }
+
+  if (!newFocusedElement) {
+    NotifyFocusStateChange(content, newFocusedElement, 0,
+                           /* aGettingFocus = */ false, false);
+  } else {
+    // We should already have the right state, which is managed by the <input>
+    // widget.
+    MOZ_ASSERT(newFocusedElement->State().HasState(ElementState::FOCUS));
+  }
   return NS_OK;
 }
 
@@ -1336,9 +1349,8 @@ void nsFocusManager::NotifyFocusStateChange(Element* aElement,
   // active state at moving focus.
   if (RefPtr<nsPresContext> presContext =
           aElement->GetPresContext(Element::PresContextFor::eForComposedDoc)) {
-    RefPtr<EventStateManager> esm =
-        presContext ? presContext->EventStateManager() : nullptr;
-    HTMLInputElement* activeInputElement =
+    RefPtr<EventStateManager> esm = presContext->EventStateManager();
+    auto* activeInputElement =
         HTMLInputElement::FromNodeOrNull(esm->GetActiveContent());
     if (activeInputElement &&
         (activeInputElement->ControlType() == FormControlType::InputCheckbox ||
