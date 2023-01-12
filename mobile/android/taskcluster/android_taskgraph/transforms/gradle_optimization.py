f41b950d3a28c37f48b7950e1658d1d4cd4353e7
Johan Lorenzo <jlorenzo@mozilla.com>
2023-01-12 15:19:24 +0100

    Bug 1809859 - Let focus UI tests run when A-C dependencies and gradle… (https://github.com/mozilla-mobile/firefox-android/pull/532)
    * Bug 1809859 - Let focus UI tests run when A-C dependencies and gradle config are changed
    
    * Apply suggestions from code review
    
    Co-authored-by: Julien Cristau <julien+github@cristau.org>
    
    Co-authored-by: Julien Cristau <julien+github@cristau.org>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- mobile/android/taskcluster/android_taskgraph/transforms/gradle_optimization.py
+++ mobile/android/taskcluster/android_taskgraph/transforms/gradle_optimization.py
@@ -9,25 +9,33 @@ transforms = TransformSequence()
 @transforms.add
 def add_components_optimization(config, tasks):
     for task in tasks:
-        if _is_task_related_to_android_components(task):
-            build_type = task["attributes"]["build-type"]
-            if build_type not in ("nightly", "release"):
-                optimization = task.setdefault("optimization", {})
-                skip_unless_changed = optimization.setdefault("skip-unless-changed", [])
-                skip_unless_changed.extend([
-                    "android-components/build.gradle",
-                    "android-components/settings.gradle",
-                    "android-components/buildSrc.*",
-                    "android-components/gradle.properties",
-                    "android-components/gradle/wrapper/gradle-wrapper.properties",
-                    "android-components/plugins/dependencies/**",
-                ])
+        attributes = task.get("attributes", {})
+        # TODO bug 1806454 - Use a single attribute instead of 2. This is a historical
+        # discrepancy where A-C are labeled by build-types but APKs by release ones.
+        #
+        # The monorepo migration made this discrepancy more obvious compared to when
+        # A-C and APKs lived in different repos
+        build_type = attributes.get("build-type", "")
+        release_type = attributes.get("release-type", "")
+
+        # We want to optimize away tasks on all android-components and APKs as long as
+        # these tasks are not labeled nightly, beta, or release.
+        #
+        # Any change that impacts all a-c (e.g. a change in the a-c gradle config)
+        # should also trigger APK builds and tests.
+        if all(type_ not in ("nightly", "beta", "release") for type_ in (build_type, release_type)):
+            optimization = task.setdefault("optimization", {})
+            skip_unless_changed = optimization.setdefault("skip-unless-changed", [])
+            skip_unless_changed.extend([
+                "android-components/build.gradle",
+                "android-components/settings.gradle",
+                "android-components/buildSrc.*",
+                "android-components/gradle.properties",
+                "android-components/gradle/wrapper/gradle-wrapper.properties",
+                "android-components/plugins/dependencies/**",
+            ])
 
         yield task
-
-
-def _is_task_related_to_android_components(task):
-    return bool(task.get("attributes", {}).get("component", ""))
 
 
 @transforms.add
