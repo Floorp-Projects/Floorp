51fa5725c5ec81fa189cf44a0c513bcbffc0b90d
Eden Chuang <echuang@mozilla.com>
2023-01-12 17:24:28 +0000

    Bug 1351231 - PFetch protocol declaration and implementation. r=dom-worker-reviewers,jesup
    Depends on D138813
    
    Differential Revision: https://phabricator.services.mozilla.com/D142436

--- ipc/glue/BackgroundParentImpl.cpp
+++ ipc/glue/BackgroundParentImpl.cpp
@@ -20,6 +20,7 @@
 #include "mozilla/dom/ContentParent.h"
 #include "mozilla/dom/DOMTypes.h"
 #include "mozilla/dom/EndpointForReportParent.h"
+#include "mozilla/dom/FetchParent.h"
 #include "mozilla/dom/FileCreatorParent.h"
 #include "mozilla/dom/FileSystemManagerParentFactory.h"
 #include "mozilla/dom/FileSystemRequestParent.h"
@@ -1481,6 +1482,10 @@ BackgroundParentImpl::AllocPLockManagerParent(
     const ContentPrincipalInfo& aPrincipalInfo, const nsID& aClientId) {
   return MakeAndAddRef<mozilla::dom::locks::LockManagerParent>(aPrincipalInfo,
                                                                aClientId);
+}
+
+already_AddRefed<dom::PFetchParent> BackgroundParentImpl::AllocPFetchParent() {
+  return MakeAndAddRef<dom::FetchParent>();
 }
 
 already_AddRefed<mozilla::net::PWebSocketConnectionParent>
