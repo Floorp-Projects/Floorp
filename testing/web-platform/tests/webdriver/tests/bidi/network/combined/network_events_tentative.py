43b08dcd95eb8c213c8235aaef1aa3a92c075b3a
Julian Descottes <jdescottes@mozilla.com>
2023-01-12 10:50:17 +0000

    Bug 1809290 - [wdspec] Add BiDi network event tests subscribing to a single context r=webdriver-reviewers,whimboo
    Depends on D166374
    
    Differential Revision: https://phabricator.services.mozilla.com/D166395

--- testing/web-platform/tests/webdriver/tests/bidi/network/combined/network_events_tentative.py
+++ testing/web-platform/tests/webdriver/tests/bidi/network/combined/network_events_tentative.py
@@ -63,3 +63,62 @@ async def test_same_request_id(
         before_request_sent_events[0]["request"]["request"]
         == response_completed_events[0]["request"]["request"]
     )
+
+
+@pytest.mark.asyncio
+async def test_subscribe_to_one_context(
+    bidi_session, top_context, wait_for_event, url, fetch, setup_network_test
+):
+    other_context = await bidi_session.browsing_context.create(type_hint="tab")
+    await bidi_session.browsing_context.navigate(
+        context=other_context["context"],
+        url=url(PAGE_EMPTY_HTML),
+        wait="complete",
+    )
+
+    network_events = await setup_network_test(
+        events=[
+            "network.beforeRequestSent",
+            "network.responseStarted",
+            "network.responseCompleted",
+        ],
+        contexts=[top_context["context"]],
+    )
+
+    # Perform a fetch request in the subscribed context and wait for the response completed event.
+    text_url = url(PAGE_EMPTY_TEXT)
+    on_response_completed = wait_for_event("network.responseCompleted")
+    await fetch(text_url, context=top_context)
+    await on_response_completed
+
+    assert len(network_events["network.beforeRequestSent"]) == 1
+    assert len(network_events["network.responseStarted"]) == 1
+    assert len(network_events["network.responseCompleted"]) == 1
+
+    # Check the received events have the correct context.
+    expected_request = {"method": "GET", "url": text_url}
+    expected_response = {"url": text_url}
+    assert_before_request_sent_event(
+        network_events["network.beforeRequestSent"][0],
+        expected_request=expected_request,
+        context=top_context["context"],
+    )
+    assert_response_event(
+        network_events["network.responseStarted"][0],
+        expected_response=expected_response,
+        context=top_context["context"],
+    )
+    assert_response_event(
+        network_events["network.responseCompleted"][0],
+        expected_response=expected_response,
+        context=top_context["context"],
+    )
+
+    # Perform another fetch request in the other context.
+    await fetch(text_url, context=other_context)
+    await asyncio.sleep(0.5)
+
+    # Check that no other event was received.
+    assert len(network_events["network.beforeRequestSent"]) == 1
+    assert len(network_events["network.responseStarted"]) == 1
+    assert len(network_events["network.responseCompleted"]) == 1
