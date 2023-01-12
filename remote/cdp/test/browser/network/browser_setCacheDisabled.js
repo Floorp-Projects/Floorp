4c903ed9aeda3236233f3eafa2a9362f83558647
Julian Descottes <jdescottes@mozilla.com>
2023-01-12 09:02:59 +0000

    Bug 1775791 - [cdp] Wait for progress listener to be ready before starting requests in browser_setCacheDisabled.js r=webdriver-reviewers,whimboo
    We should wait for the progress listener to be setup otherwise we are racing against the ContentTask.spawn
    
    Differential Revision: https://phabricator.services.mozilla.com/D166577

--- remote/cdp/test/browser/network/browser_setCacheDisabled.js
+++ remote/cdp/test/browser/network/browser_setCacheDisabled.js
@@ -13,30 +13,31 @@ add_task(async function cacheEnabledAfterDisabled({ client }) {
   await Network.setCacheDisabled({ cacheDisabled: true });
   await Network.setCacheDisabled({ cacheDisabled: false });
 
-  const checkPromise = checkLoadFlags(LOAD_NORMAL, TEST_PAGE);
+  await watchLoadFlags(LOAD_NORMAL, TEST_PAGE);
   await loadURL(TEST_PAGE);
-  await checkPromise;
+  await waitForLoadFlags();
 });
 
 add_task(async function cacheEnabledByDefault({ Network }) {
-  const checkPromise = checkLoadFlags(LOAD_NORMAL, TEST_PAGE);
+  await watchLoadFlags(LOAD_NORMAL, TEST_PAGE);
   await loadURL(TEST_PAGE);
-  await checkPromise;
+  await waitForLoadFlags();
 });
 
 add_task(async function cacheDisabled({ client }) {
   const { Network } = client;
   await Network.setCacheDisabled({ cacheDisabled: true });
 
-  const checkPromise = checkLoadFlags(
-    LOAD_BYPASS_CACHE | INHIBIT_CACHING,
-    TEST_PAGE
-  );
+  await watchLoadFlags(LOAD_BYPASS_CACHE | INHIBIT_CACHING, TEST_PAGE);
   await loadURL(TEST_PAGE);
-  await checkPromise;
+  await waitForLoadFlags();
 });
 
-function checkLoadFlags(flags, url) {
+// This helper will resolve when the content-process progressListener is started
+// and ready to monitor requests.
+// The promise itself will resolve when the progressListener will detect the
+// expected flags for the provided url.
+function watchLoadFlags(flags, url) {
   return ContentTask.spawn(
     gBrowser.selectedBrowser,
     { flags, url },
@@ -114,9 +115,18 @@ function checkLoadFlags(flags, url) {
         ]),
       };
 
-      await new Promise(resolve => {
+      // Store the promise that should be awaited for on the current window.
+      content.resolveCheckLoadFlags = new Promise(resolve => {
         RequestWatcher.init(docShell, flags, url, resolve);
       });
     }
   );
+}
+
+// Wait for the latest promise created in-content by watchLoadFlags to resolve.
+function waitForLoadFlags() {
+  return ContentTask.spawn(gBrowser.selectedBrowser, {}, async () => {
+    await content.resolveCheckLoadFlags;
+    delete content.resolveCheckLoadFlags;
+  });
 }
