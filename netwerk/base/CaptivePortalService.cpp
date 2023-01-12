c004c1e8215164f3bc9cf1b93bdbafd8f3a1d894
Stanca Serban <sstanca@mozilla.com>
2023-01-12 14:25:59 +0200

    Backed out changeset 40079b294d34 (bug 1800603) for causing Xpcshell failures in test_ext_captivePortal.js. CLOSED TREE

--- netwerk/base/CaptivePortalService.cpp
+++ netwerk/base/CaptivePortalService.cpp
@@ -65,10 +65,6 @@ nsresult CaptivePortalService::PerformCheck() {
   if (AppShutdown::IsInOrBeyond(ShutdownPhase::AppShutdownConfirmed)) {
     return NS_ERROR_ILLEGAL_DURING_SHUTDOWN;
   }
-  if (!mCanUseJS) {
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   MOZ_ASSERT(XRE_GetProcessType() == GeckoProcessType_Default);
   nsresult rv;
   if (!mCaptivePortalDetector) {
@@ -137,7 +133,6 @@ nsresult CaptivePortalService::Initialize() {
     observerService->AddObserver(this, kAbortCaptivePortalLoginEvent, true);
     observerService->AddObserver(this, kCaptivePortalLoginSuccessEvent, true);
     observerService->AddObserver(this, NS_XPCOM_SHUTDOWN_OBSERVER_ID, true);
-    observerService->AddObserver(this, "app-startup", true);
   }
 
   LOG(("Initialized CaptivePortalService\n"));
@@ -338,9 +333,6 @@ CaptivePortalService::Observe(nsISupports* aSubject, const char* aTopic,
     mSlackCount = 0;
   } else if (!strcmp(aTopic, NS_XPCOM_SHUTDOWN_OBSERVER_ID)) {
     Stop();
-    return NS_OK;
-  } else if (!strcmp(aTopic, "app-startup")) {
-    mCanUseJS = true;
     return NS_OK;
   }
 
