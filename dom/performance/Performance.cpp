e88447b75eed59f9b9b19b33eec18a4bae92b1ac
Sean Feng <sefeng@mozilla.com>
2023-01-13 16:09:12 +0000

    Bug 1808841 - Make resource timing entires always be queued regardless the size of the buffer r=valentin
    Somehow we have a bug in Gecko such that if the resource timing buffer
    is full, no further resource entries will be queued unless you clear
    or increase the buffer size. This is wrong because the spec specifies
    the entry should always be queued regardless the size of the buffer.
    
    Spec: https://w3c.github.io/resource-timing/#dfn-mark-resource-timing
    
    Differential Revision: https://phabricator.services.mozilla.com/D166723

--- dom/performance/Performance.cpp
+++ dom/performance/Performance.cpp
@@ -856,6 +856,8 @@ void Performance::InsertResourceEntry(PerformanceEntry* aEntry) {
     return;
   }
 
+  QueueEntry(aEntry);
+
   /*
    * Let new entry be the input PerformanceEntry to be added.
    *
@@ -868,7 +870,6 @@ void Performance::InsertResourceEntry(PerformanceEntry* aEntry) {
      * Increase resource timing buffer current size by 1.
      */
     mResourceEntries.InsertElementSorted(aEntry, PerformanceEntryComparator());
-    QueueEntry(aEntry);
     return;
   }
 
