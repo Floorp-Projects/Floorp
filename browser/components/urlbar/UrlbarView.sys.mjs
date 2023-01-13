e7a8bffbe0859ecfdbd5b71fe5e9478409aa91b9
Dão Gottwald <dao@mozilla.com>
2023-01-13 16:03:06 +0000

    Bug 1809705 - Implement "Learn more about Firefox Suggest" menu item in the urlbar result menu. r=adw,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D166578

--- browser/components/urlbar/UrlbarView.sys.mjs
+++ browser/components/urlbar/UrlbarView.sys.mjs
@@ -38,6 +38,11 @@ const ZERO_PREFIX_SCALAR_ABANDONMENT = "urlbar.zeroprefix.abandonment";
 const ZERO_PREFIX_SCALAR_ENGAGEMENT = "urlbar.zeroprefix.engagement";
 const ZERO_PREFIX_SCALAR_EXPOSURE = "urlbar.zeroprefix.exposure";
 
+const RESULT_MENU_COMMANDS = {
+  BLOCK: "block",
+  LEARN_MORE: "learn-more",
+};
+
 const getBoundsWithoutFlushing = element =>
   element.ownerGlobal.windowUtils.getBoundsWithoutFlushing(element);
 
@@ -777,7 +782,6 @@ export class UrlbarView {
       "popuphidden",
       () => {
         anchor.toggleAttribute("open", false);
-        this.#resultMenuResult = null;
       },
       { once: true }
     );
@@ -1295,33 +1299,25 @@ export class UrlbarView {
       item._buttons.get("tip").textContent = result.payload.buttonText;
     }
 
-    if (result.payload.isBlockable && !lazy.UrlbarPrefs.get("resultMenu")) {
-      this.#addRowButton(item, {
-        name: "block",
-        l10n: result.payload.blockL10n,
-      });
-    }
-    if (result.payload.helpUrl) {
-      this.#addRowButton(item, {
-        name: "help",
-        url: result.payload.helpUrl,
-        l10n: result.payload.helpL10n,
-      });
-    }
-    if (lazy.UrlbarPrefs.get("resultMenu")) {
-      let menuCommands = [];
-      if (result.source == lazy.UrlbarUtils.RESULT_SOURCE.HISTORY) {
-        menuCommands.push("remove-from-history");
-      } else if (result.payload.isBlockable) {
-        menuCommands.push("dismiss-firefox-suggest");
-      }
-      if (menuCommands.length) {
-        this.#resultMenuCommands.set(result, menuCommands);
+    if (!lazy.UrlbarPrefs.get("resultMenu")) {
+      if (result.payload.isBlockable) {
         this.#addRowButton(item, {
-          name: "menu",
-          l10n: { id: "urlbar-result-menu-button" },
+          name: "block",
+          l10n: result.payload.blockL10n,
         });
       }
+      if (result.payload.helpUrl) {
+        this.#addRowButton(item, {
+          name: "help",
+          url: result.payload.helpUrl,
+          l10n: result.payload.helpL10n,
+        });
+      }
+    } else if (this.#getResultMenuCommands(result)) {
+      this.#addRowButton(item, {
+        name: "menu",
+        l10n: { id: "urlbar-result-menu-button" },
+      });
     }
   }
 
@@ -1382,8 +1378,10 @@ export class UrlbarView {
       // always need updating.
       provider.getViewTemplate ||
       oldResult.isBestMatch != result.isBestMatch ||
-      !!result.payload.helpUrl != item._buttons.has("help") ||
-      !!result.payload.isBlockable != item._buttons.has("block") ||
+      (!lazy.UrlbarPrefs.get("resultMenu") &&
+        (!!result.payload.helpUrl != item._buttons.has("block") ||
+          !!result.payload.isBlockable != item._buttons.has("block"))) ||
+      !!this.#getResultMenuCommands(result) != item._buttons.has("menu") ||
       !lazy.ObjectUtils.deepEqual(
         oldResult.payload.buttons,
         result.payload.buttons
@@ -2508,6 +2506,55 @@ export class UrlbarView {
     }
   }
 
+  /**
+   * @param {UrlbarResult} result
+   *   The result to get menu commands for.
+   * @returns {Map}
+   *   Map of menu commands available for the result, null if there are none.
+   */
+  #getResultMenuCommands(result) {
+    if (!lazy.UrlbarPrefs.get("resultMenu")) {
+      return null;
+    }
+    if (this.#resultMenuCommands.has(result)) {
+      return this.#resultMenuCommands.get(result);
+    }
+    let commands = new Map();
+    if (result.source == lazy.UrlbarUtils.RESULT_SOURCE.HISTORY) {
+      commands.set(RESULT_MENU_COMMANDS.BLOCK, {
+        l10n: { id: "urlbar-result-menu-remove-from-history" },
+      });
+    }
+    if (result.payload.isBlockable) {
+      commands.set(RESULT_MENU_COMMANDS.BLOCK, {
+        l10n: result.payload.blockL10n,
+      });
+    }
+    if (result.payload.helpUrl) {
+      commands.set(RESULT_MENU_COMMANDS.LEARN_MORE, {
+        l10n: result.payload.helpL10n,
+      });
+    }
+    let rv = commands.size ? commands : null;
+    this.#resultMenuCommands.set(result, rv);
+    return rv;
+  }
+
+  #populateResultMenu() {
+    this.resultMenu.textContent = "";
+    for (const [command, data] of this.#getResultMenuCommands(
+      this.#resultMenuResult
+    )) {
+      let menuitem = this.document.createElementNS(
+        "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
+        "menuitem"
+      );
+      menuitem.dataset.command = command;
+      this.#setElementL10n(menuitem, data.l10n);
+      this.resultMenu.appendChild(menuitem);
+    }
+  }
+
   // Event handlers below.
 
   on_SelectedOneOffButtonChanged() {
@@ -2779,11 +2826,14 @@ export class UrlbarView {
   on_command(event) {
     if (event.currentTarget == this.resultMenu) {
       let result = this.#resultMenuResult;
+      this.#resultMenuResult = null;
       let menuitem = event.target;
       switch (menuitem.dataset.command) {
-        case "remove-from-history":
-        case "dismiss-firefox-suggest":
+        case RESULT_MENU_COMMANDS.BLOCK:
           this.controller.handleDeleteEntry(null, result);
+          break;
+        case RESULT_MENU_COMMANDS.LEARN_MORE:
+          this.window.openTrustedLinkIn(result.payload.helpUrl, "tab");
           break;
       }
     }
@@ -2791,12 +2841,7 @@ export class UrlbarView {
 
   on_popupshowing(event) {
     if (event.currentTarget == this.resultMenu) {
-      let availableCommands = this.#resultMenuCommands.get(
-        this.#resultMenuResult
-      );
-      for (let menuitem of this.resultMenu.querySelectorAll("[data-command]")) {
-        menuitem.hidden = !availableCommands.includes(menuitem.dataset.command);
-      }
+      this.#populateResultMenu();
     }
   }
 }
