616c0ce867bba527c61843df72859cc3f3de9580
Julian Descottes <jdescottes@mozilla.com>
2023-01-14 19:00:28 +0000

    Bug 1810302 - [wdspec] Remove the response_completed_tentative 304 test case r=Honza
    We should handle cached requests in https://bugzilla.mozilla.org/show_bug.cgi?id=1806802 and this test case seems very unstable on beta + android
    
    Differential Revision: https://phabricator.services.mozilla.com/D166867

--- testing/web-platform/tests/webdriver/tests/bidi/network/__init__.py
+++ testing/web-platform/tests/webdriver/tests/bidi/network/__init__.py
@@ -204,7 +204,6 @@ HTTP_STATUS_AND_STATUS_TEXT = [
     (301, "Moved Permanently"),
     (302, "Found"),
     (303, "See Other"),
-    (304, "Not Modified"),
     (305, "Use Proxy"),
     (307, "Temporary Redirect"),
     (400, "Bad Request"),
