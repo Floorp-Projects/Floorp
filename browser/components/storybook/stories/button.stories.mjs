20d0dec41b6d18eb95d26aace33805be0643c049
Mark Striemer <mstriemer@mozilla.com>
2023-01-16 21:39:56 +0000

    Bug 1803151 - Use our vendored lit.all.mjs in storybook r=hjones
    Differential Revision: https://phabricator.services.mozilla.com/D166393

--- browser/components/storybook/stories/button.stories.mjs
+++ browser/components/storybook/stories/button.stories.mjs
@@ -2,8 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { html } from "lit";
-import { classMap } from "lit/directives/class-map.js";
+import { classMap, html } from "lit.all.mjs";
 
 export default {
   title: "Design System/Atoms/Button",
