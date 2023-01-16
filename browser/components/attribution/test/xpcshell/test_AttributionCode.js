91a47e126772cfb50423d22e65de5662872b53e0
Ben Hearsum <bhearsum@mozilla.com>
2023-01-16 19:32:05 +0000

    Bug 1807924: make attribution tests work against MSIX builds r=nalexander
    Two notable things here:
    - Small refactor of AttributionCode.jsm to make it more testable (allowing `getCampaignId` to be easily stubbed out)
    - An ugly skip of one of the "invalid" attribution code tests -- for reasons noted in the comments. Ultimately, this points to maybe a refactor or larger change being needed, but I don't think it's worth blocking getting these tests on.
    
    Differential Revision: https://phabricator.services.mozilla.com/D166688

--- browser/components/attribution/test/xpcshell/test_AttributionCode.js
+++ browser/components/attribution/test/xpcshell/test_AttributionCode.js
@@ -3,6 +3,12 @@
  */
 "use strict";
 
+const { AppConstants } = ChromeUtils.importESModule(
+  "resource://gre/modules/AppConstants.sys.mjs"
+);
+
+const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
+
 add_task(async () => {
   await setupStubs();
 });
@@ -12,14 +18,34 @@ add_task(async () => {
  * to make sure we reject bad ones and accept good ones.
  */
 add_task(async function testValidAttrCodes() {
+  let currentCode = null;
   for (let entry of validAttrCodes) {
+    currentCode = entry.code;
+    // Attribution for MSIX builds works quite differently than regular Windows
+    // builds: the attribution codes come from a Windows API that we've wrapped
+    // with an XPCOM class. We don't have a way to inject codes into the build,
+    // so instead we mock out our XPCOM class and return the desired values from
+    // there. (A potential alternative is to have MSIX tests rely on attribution
+    // files, but that more or less invalidates the tests.)
+    if (
+      AppConstants.platform === "win" &&
+      Services.sysinfo.getProperty("hasWinPackageId")
+    ) {
+      // In real life, the attribution codes returned from Microsoft APIs
+      // are not URI encoded, and the AttributionCode code that deals with
+      // them expects that - so we have to simulate that as well.
+      sinon
+        .stub(AttributionCode, "msixCampaignId")
+        .get(() => decodeURIComponent(currentCode));
+    } else {
+      await AttributionCode.writeAttributionFile(currentCode);
+    }
     AttributionCode._clearCache();
-    await AttributionCode.writeAttributionFile(entry.code);
     let result = await AttributionCode.getAttrDataAsync();
     Assert.deepEqual(
       result,
       entry.parsed,
-      "Parsed code should match expected value, code was: " + entry.code
+      "Parsed code should match expected value, code was: " + currentCode
     );
   }
   AttributionCode._clearCache();
@@ -29,11 +55,35 @@ add_task(async function testValidAttrCodes() {
  * Make sure codes with various formatting errors are not seen as valid.
  */
 add_task(async function testInvalidAttrCodes() {
+  let currentCode = null;
   for (let code of invalidAttrCodes) {
+    currentCode = code;
+
+    if (
+      AppConstants.platform === "win" &&
+      Services.sysinfo.getProperty("hasWinPackageId")
+    ) {
+      if (code.includes("not set")) {
+        // One of the "invalid" codes that we test is an unescaped one.
+        // This is valid for most platforms, but we actually _expect_
+        // unescaped codes for MSIX builds, so that particular test is not
+        // valid for this case.
+        continue;
+      }
+
+      sinon
+        .stub(AttributionCode, "msixCampaignId")
+        .get(() => decodeURIComponent(currentCode));
+    } else {
+      await AttributionCode.writeAttributionFile(currentCode);
+    }
     AttributionCode._clearCache();
-    await AttributionCode.writeAttributionFile(code);
     let result = await AttributionCode.getAttrDataAsync();
-    Assert.deepEqual(result, {}, "Code should have failed to parse: " + code);
+    Assert.deepEqual(
+      result,
+      {},
+      "Code should have failed to parse: " + currentCode
+    );
   }
   AttributionCode._clearCache();
 });
@@ -42,7 +92,14 @@ add_task(async function testInvalidAttrCodes() {
  * Test the cache by deleting the attribution data file
  * and making sure we still get the expected code.
  */
-add_task(async function testDeletedFile() {
+let condition = {
+  // MSIX attribution codes are not cached by us, thus this test is
+  // unnecessary for those builds.
+  skip_if: () =>
+    AppConstants.platform === "win" &&
+    Services.sysinfo.getProperty("hasWinPackageId"),
+};
+add_task(condition, async function testDeletedFile() {
   // Set up the test by clearing the cache and writing a valid file.
   await AttributionCode.writeAttributionFile(validAttrCodes[0].code);
   let result = await AttributionCode.getAttrDataAsync();
