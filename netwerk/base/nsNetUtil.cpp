e9530912819ec7134cd52f84cb63e7e6748d13dc
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2023-01-16 14:46:14 +0000

    Bug 1724718 - put missing chrome/resource check from jar channels in a better place so we catch all cases, r=valentin,florian,necko-reviewers
    This also re-adds the TYPE_FETCH content policy for file existence checks
    from tests so they don't cause crashes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D122167

--- netwerk/base/nsNetUtil.cpp
+++ netwerk/base/nsNetUtil.cpp
@@ -3864,6 +3864,20 @@ void CheckForBrokenChromeURL(nsILoadInfo* aLoadInfo, nsIURI* aURI) {
   nsCString spec;
   aURI->GetSpec(spec);
 
+#ifdef ANDROID
+  // Various toolkit files use this and are shipped on android, but
+  // info-pages.css and aboutLicense.css are not - bug 1808987
+  if (StringEndsWith(spec, "info-pages.css"_ns) ||
+      StringEndsWith(spec, "aboutLicense.css"_ns) ||
+      // Error page CSS is also missing: bug 1810039
+      StringEndsWith(spec, "aboutNetError.css"_ns) ||
+      StringEndsWith(spec, "error-pages.css"_ns) ||
+      // Used by an extension installation test - bug 1809650
+      StringBeginsWith(spec, "resource://android/assets/web_extensions/"_ns)) {
+    return;
+  }
+#endif
+
   // DTD files from gre may not exist when requested by tests.
   if (StringBeginsWith(spec, "resource://gre/res/dtd/"_ns)) {
     return;
