3888410cb3acc21b5d6d6f9d2b36f7feb1a7e454
Nika Layzell <nika@thelayzells.com>
2023-01-16 23:14:10 +0000

    Bug 1809753 - Part 1: Remove quantum-dom nsIThread::EventTarget methods, r=mccr8
    We aren't likely to try to make these changes any time soon, so cleaning out
    these unnecessary methods which just return `this` will simplify things.
    
    I was unable to find any calls to the `.eventTarget` getter in JS, which makes
    sense, as the nsIThread type is only really used in JS as a wrapper around the
    main thread in older code. Because of that, it has been removed as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D166605

--- dom/cache/Manager.cpp
+++ dom/cache/Manager.cpp
@@ -1959,8 +1959,7 @@ void Manager::Init(Maybe<Manager&> aOldManager) {
   // per Manager now, this lets us cleanly call Factory::Remove() once the
   // Context goes away.
   SafeRefPtr<Context> ref = Context::Create(
-      SafeRefPtrFromThis(), mIOThread->SerialEventTarget(),
-      MakeSafeRefPtr<SetupAction>(),
+      SafeRefPtrFromThis(), mIOThread, MakeSafeRefPtr<SetupAction>(),
       aOldManager ? SomeRef(*aOldManager->mContext) : Nothing());
   mContext = ref.unsafeGetRawPtr();
 }
