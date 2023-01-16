e5196ef85af2d7b383b7547c097f3933f26bc14d
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2023-01-16 15:06:24 +0900

    [FB] Tabbar | Moving the code.

--- floorp/browser/base/content/browser-tabbar.js
+++ floorp/browser/base/content/browser-tabbar.js
@@ -3,19 +3,57 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- setTabbarMode();
- 
-//-------------------------------------------------------------------------Observer----------------------------------------------------------------------------
+//-------------------------------------------------------------------------Tabbar----------------------------------------------------------------------------
 
- Services.prefs.addObserver("floorp.browser.tabbar.settings", function(){
-   document.getElementById("tabbardesgin")?.remove();
-   document.getElementById("tabbar-script")?.remove();
-   document.getElementById("navigator-toolbox").insertBefore(document.getElementById("titlebar"), document.getElementById("navigator-toolbox").firstChild);
-   document.getElementById("TabsToolbar").before(document.getElementById("toolbar-menubar"));
-
-   setTabbarMode();
+function setTabbarMode() {
+  const tabbarCSS = {
+    HideTabBrowser: "@import url(chrome://browser/skin/tabbar/hide-tabbrowser.css);",
+    VerticalTab: "@import url(chrome://browser/skin/tabbar/verticaltab.css);",
+    BottomTabs: "@import url(chrome://browser/skin/tabbar/tabs_on_bottom.css);",
+    WindowBottomTabs: "@import url(chrome://browser/skin/tabbar/tabbar_on_window_bottom.css);"
   }
- )
+  const tabbarPref = Services.prefs.getIntPref("floorp.browser.tabbar.settings");
+  var Tag = document.createElement("style");
+  Tag.setAttribute("id", "tabbardesgin");
+  switch (tabbarPref) {
+    case 1:
+      //hide tabbrowser
+      Tag.innerText = tabbarCSS.HideTabBrowser
+      document.head.insertAdjacentElement('beforeend', Tag);
+      break;
+    case 2:
+      // vertical tab CSS
+      Tag.innerText = tabbarCSS.VerticalTab
+      document.head.insertAdjacentElement('beforeend', Tag);
+      window.setTimeout(function () {
+        document.getElementById("titlebar").before(document.getElementById("toolbar-menubar"));
+      }, 500);
+      break;
+    case 3:
+      //tabs_on_bottom
+      Tag.innerText = tabbarCSS.BottomTabs
+      document.head.insertAdjacentElement('beforeend', Tag);
+      break;
+    case 4:
+      Tag.innerText = tabbarCSS.WindowBottomTabs
+      document.head.insertAdjacentElement('beforeend', Tag);
+      var script = document.createElement("script");
+      script.setAttribute("id", "tabbar-script");
+      script.src = "chrome://browser/skin/tabbar/tabbar_on_window_bottom.js";
+      document.head.appendChild(script);
+      break;
+  }
+}
+
+setTabbarMode();
+Services.prefs.addObserver("floorp.browser.tabbar.settings", function () {
+  document.getElementById("tabbardesgin")?.remove();
+  document.getElementById("tabbar-script")?.remove();
+  document.getElementById("navigator-toolbox").insertBefore(document.getElementById("titlebar"), document.getElementById("navigator-toolbox").firstChild);
+  document.getElementById("TabsToolbar").before(document.getElementById("toolbar-menubar"));
+
+  setTabbarMode();
+});
 
 //-------------------------------------------------------------------------Multirow-tabs----------------------------------------------------------------------------
 
