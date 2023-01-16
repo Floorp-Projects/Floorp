ce5bae69ff35a2bf4e84dca0789918a924361999
Mike Conley <mconley@mozilla.com>
2023-01-16 22:25:40 +0000

    Bug 1803447 - Implement progress page for the new migration wizard. r=hjones,kpatenio,mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D165090

--- browser/components/migration/content/migration-wizard-constants.mjs
+++ browser/components/migration/content/migration-wizard-constants.mjs
@@ -15,4 +15,25 @@ export const MigrationWizardConstants = Object.freeze({
     PROGRESS: "progress",
     SAFARI_PERMISSION: "safari-permission",
   }),
+
+  /**
+   * Returns a mapping of a resource type to a string used to identify
+   * the associated resource group in the wizard via a data-resource-type
+   * attribute. The keys are used to set which items should be shown and
+   * in what state in #onShowingProgress.
+   *
+   * @type {Object<string, string>}
+   */
+  DISPLAYED_RESOURCE_TYPES: Object.freeze({
+    // The DISPLAYED_RESOURCE_TYPES should have their keys match those
+    // in MigrationUtils.resourceTypes.
+
+    // COOKIE resource migration is going to be removed, so we don't include
+    // it here.
+    HISTORY: "history",
+    FORMDATA: "form-autofill",
+    PASSWORDS: "logins-and-passwords",
+    BOOKMARKS: "bookmarks",
+    // We don't yet show OTHERDATA or SESSION resources.
+  }),
 });
