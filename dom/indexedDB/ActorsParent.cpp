e6def0dd30e98b33d406b47b8ae86d4fa08ea795
Nika Layzell <nika@thelayzells.com>
2023-01-16 19:22:04 +0000

    Bug 1806751 - Perform indexeddb database maintenance on a TaskQueue, r=asuth,janv,dom-storage-reviewers
    After adding some assertions to GetCurrentSerialEventTarget and running tests,
    I was able to find this caller which could lead to the crashes here. By running
    database maintenance on a TaskQueue the captured event target will be one which
    accepts and processes dispatches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D166609

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -13776,8 +13776,14 @@ nsresult Maintenance::BeginDatabaseMaintenance() {
           MOZ_ASSERT(threadPool);
         }
 
+        // Perform database maintenance on a TaskQueue, as database connections
+        // require a serial event target when being opened in order to allow
+        // memory pressure notifications to clear caches (bug 1806751).
+        const auto taskQueue = TaskQueue::Create(
+            do_AddRef(threadPool), "IndexedDB Database Maintenance");
+
         MOZ_ALWAYS_SUCCEEDS(
-            threadPool->Dispatch(databaseMaintenance, NS_DISPATCH_NORMAL));
+            taskQueue->Dispatch(databaseMaintenance, NS_DISPATCH_NORMAL));
 
         RegisterDatabaseMaintenance(databaseMaintenance);
       }
