c5f6fe3cecc37bb777ca13f8b99d28f59c9247ae
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2023-01-18 01:16:33 +0900

    [FB] UI | Rewrite `browser-UI-custom.js`

--- floorp/browser/base/content/browser-UI-custom.js
+++ floorp/browser/base/content/browser-UI-custom.js
@@ -6,205 +6,189 @@
 //import utils
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
-if (Services.prefs.getBoolPref("floorp.material.effect.enable", false)) {    
-  var Tag = document.createElement("style");
-  Tag.innerText = `@import url(chrome://browser/skin/options/micaforeveryone.css)`
-  document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-  Tag.setAttribute("id", "micaforeveryone");
+const UICustomPrefHandler = function(pref, callback) {
+  let prefValue = Services.prefs.getBoolPref(pref, false);
+  try {
+    callback({
+      pref: pref,
+      prefValue: prefValue,
+      reason: "init",
+    });
+  } catch (e) { console.error(e) }
+  Services.prefs.addObserver(pref, function() {
+    let prefValue = Services.prefs.getBoolPref(pref, false);
+    try {
+      callback({
+        pref: pref,
+        prefValue: prefValue,
+        reason: "changed",
+      });
+    } catch (e) { console.error(e) }
+  });
 }
 
-Services.prefs.addObserver("floorp.material.effect.enable", function(){
- if (Services.prefs.getBoolPref("floorp.material.effect.enable", false)) {
-   var Tag = document.createElement("style");
-   Tag.innerText = `@import url(chrome://browser/skin/options/micaforeveryone.css)`
-   document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-   Tag.setAttribute("id", "micaforeveryone");
- }
- else {
-   document.getElementById("micaforeveryone").remove();
- }});
+// prefs
+UICustomPrefHandler("floorp.material.effect.enable", function(event) {
+  if (event.prefValue) {
+    let Tag = document.createElement("style");
+    Tag.innerText = `@import url(chrome://browser/skin/options/micaforeveryone.css)`;
+    Tag.setAttribute("id", "floorp-micaforeveryone");
+    document.head.appendChild(Tag);
+  } else {
+    document.getElementById("floorp-micaforeveryone")?.remove();
+  }
+});
 
-  if (Services.prefs.getBoolPref("floorp.Tree-type.verticaltab.optimization", false)) {    
+UICustomPrefHandler("floorp.Tree-type.verticaltab.optimization", function(event) {
+  if (event.prefValue) {
+    let Tag = document.createElement("style");
+    Tag.innerText = `@import url(chrome://browser/skin/options/treestyletab.css)`;
+    Tag.setAttribute("id", "floorp-optimizefortreestyletab");
+    document.head.appendChild(Tag);
+  } else {
+    document.getElementById("floorp-optimizefortreestyletab")?.remove();
+  }
+});
+
+UICustomPrefHandler("floorp.optimized.msbutton.ope", function(event) {
+  if (event.prefValue) {
+    let Tag = document.createElement("style");
+    Tag.innerText = `@import url(chrome://browser/skin/options/msbutton.css)`;
+    Tag.setAttribute("id", "floorp-optimizedmsbuttonope");
+    document.head.appendChild(Tag);
+  } else {
+    document.getElementById("floorp-optimizedmsbuttonope")?.remove();
+  }
+});
+
+UICustomPrefHandler("floorp.bookmarks.bar.focus.mode", function(event) {
+  if (event.prefValue) {
+    let Tag = document.createElement("style");
+    Tag.innerText = `@import url(chrome://browser/skin/options/bookmarkbar_autohide.css)`;
+    Tag.setAttribute("id", "floorp-bookmarkbarfocus");
+    document.head.appendChild(Tag);
+  } else {
+    document.getElementById("floorp-bookmarkbarfocus")?.remove();
+  }
+});
+
+UICustomPrefHandler("floorp.bookmarks.fakestatus.mode", function(event) {
+  if (event.prefValue) {
+    setTimeout(function() {
+      document.getElementById("browser-bottombox")
+        .after(document.getElementById("PersonalToolbar"));
+    }, event.reason === "init" ? 250 : 1);
+  } else {
+    if (event.reason === "changed") {
+      //Fix for the bug that bookmarksbar is on the navigation toolbar when the pref is cahaned to false
+      if (!Services.prefs.getBoolPref("floorp.navbar.bottom", false)) {
+        document.getElementById("navigator-toolbox")
+          .appendChild(document.getElementById("nav-bar"));
+      }
+      document.getElementById("navigator-toolbox")
+        .appendChild(document.getElementById("PersonalToolbar"));
+    }
+  }
+});
+
+UICustomPrefHandler("floorp.search.top.mode", function(event) {
+  if (event.prefValue) {
+    let Tag = document.createElement("style");
+    Tag.innerText = `@import url(chrome://browser/skin/options/move_page_inside_searchbar.css)`;
+    Tag.setAttribute("id", "floorp-searchbartop");
+    document.head.appendChild(Tag);
+  } else {
+    document.getElementById("floorp-searchbartop")?.remove();
+  }
+});
+
+UICustomPrefHandler("floorp.legacy.dlui.enable", function(event) {
+  if (event.prefValue) {
+    let Tag = document.createElement("style");
+    Tag.innerText = `@import url(chrome://browser/skin/options/browser-custum-dlmgr.css)`;
+    Tag.setAttribute("id", "floorp-dlmgrcss");
+    document.head.appendChild(Tag);
+  } else {
+    document.getElementById("floorp-dlmgrcss")?.remove();
+  }
+});
+
+UICustomPrefHandler("floorp.downloading.red.color", function(event) {
+  if (event.prefValue) {
+    let Tag = document.createElement("style");
+    Tag.innerText = `@import url(chrome://browser/skin/options/downloading-redcolor.css`;
+    Tag.setAttribute("id", "floorp-dlredcolor");
+    document.head.appendChild(Tag);
+  } else {
+    document.getElementById("floorp-dlredcolor")?.remove();
+  }
+});
+
+UICustomPrefHandler("floorp.navbar.bottom", function(event) {
+  if (event.prefValue) {
     var Tag = document.createElement("style");
-    Tag.innerText = `@import url(chrome://browser/skin/options/treestyletab.css)`
-    document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-    Tag.setAttribute("id", "treestyletabopti");
-  }
-
-  Services.prefs.addObserver("floorp.Tree-type.verticaltab.optimization", function(){
-    if (Services.prefs.getBoolPref("floorp.Tree-type.verticaltab.optimization", false)) {
-      var Tag = document.createElement("style");
-      Tag.innerText = `@import url(chrome://browser/skin/options/treestyletab.css)`
-      document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-      Tag.setAttribute("id", "treestyletabopti");
-    }
-    else {
-      document.getElementById("treestyletabopti").remove();
-  }});
-
-  if (Services.prefs.getBoolPref("floorp.optimized.msbutton.ope", false)) {    
-    var Tag = document.createElement("style");
-    Tag.innerText = `@import url(chrome://browser/skin/options/msbutton.css)`
-    document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-    Tag.setAttribute("id", "optimizedmsbuttonope");
-  }
- Services.prefs.addObserver("floorp.optimized.msbutton.ope", function(){
-   if (Services.prefs.getBoolPref("floorp.optimized.msbutton.ope", false)) {
-     var Tag = document.createElement("style");
-     Tag.innerText = `@import url(chrome://browser/skin/options/msbutton.css)`
-     document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-     Tag.setAttribute("id", "optimizedmsbuttonope");
-   }
-   else {
-    document.getElementById("optimizedmsbuttonope").remove();
-  }});
-
-  if (Services.prefs.getBoolPref("floorp.bookmarks.bar.focus.mode", false)) {    
-    var Tag = document.createElement("style");
-    Tag.innerText = `@import url(chrome://browser/skin/options/bookmarkbar_autohide.css)`
-    document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-    Tag.setAttribute("id", "bookmarkbarfocus");
-  }
-   Services.prefs.addObserver("floorp.bookmarks.bar.focus.mode", function(){
-     if (Services.prefs.getBoolPref("floorp.bookmarks.bar.focus.mode", false)) {
-     var Tag = document.createElement("style");
-     Tag.innerText = `@import url(chrome://browser/skin/options/bookmarkbar_autohide.css)`
-     document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-     Tag.setAttribute("id", "bookmarkbarfocus");
-    }
-  else {
-   document.getElementById("bookmarkbarfocus").remove();
-  }});
-
-
-if (Services.prefs.getBoolPref("floorp.bookmarks.fakestatus.mode", false)) {
-  window.setTimeout(function(){document.body.appendChild(document.getElementById('PersonalToolbar'));}, 250);
-  }
-
- Services.prefs.addObserver("floorp.bookmarks.fakestatus.mode", function(){
-   if (Services.prefs.getBoolPref("floorp.bookmarks.fakestatus.mode", false)) {
-    document.getElementById("browser-bottombox").after(document.getElementById("PersonalToolbar"));
-   }
-   else {
-    //Fix for the bug that bookmarksbar is on the navigation toolbar when the pref is cahaned to false
-    if (!Services.prefs.getBoolPref("floorp.navbar.bottom", false)) {
-      document.getElementById("navigator-toolbox").appendChild(document.getElementById("nav-bar"));
-    }
-    document.getElementById("navigator-toolbox").appendChild(document.getElementById("PersonalToolbar"));
-   }
- })
-
-  if (Services.prefs.getBoolPref("floorp.search.top.mode", false)) {
-     var Tag = document.createElement("style");
-     Tag.innerText = `@import url(chrome://browser/skin/options/move_page_inside_searchbar.css)` 
-     document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-     Tag.setAttribute("id", "searchbartop");
-  }
-  Services.prefs.addObserver("floorp.search.top.mode", function(){
-    if (Services.prefs.getBoolPref("floorp.search.top.mode", false)) {
-      var Tag = document.createElement("style");
-      Tag.innerText = `@import url(chrome://browser/skin/options/move_page_inside_searchbar.css)` 
-      document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-      Tag.setAttribute("id", "searchbartop");
-    }
-    else {
-      document.getElementById("searchbartop").remove();
-    }
-   }
- )
- if (Services.prefs.getBoolPref("floorp.legacy.dlui.enable", false)) {    
-  var Tag = document.createElement("style");
-  Tag.innerText = `@import url(chrome://browser/skin/options/browser-custum-dlmgr.css)`
-  document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-  Tag.setAttribute("id", "dlmgrcss");
-}
-Services.prefs.addObserver("floorp.legacy.dlui.enable", function(){
- if (Services.prefs.getBoolPref("floorp.legacy.dlui.enable", false)) {
-   var Tag = document.createElement("style");
-   Tag.innerText = `@import url(chrome://browser/skin/options/browser-custum-dlmgr.css)`
-   document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-   Tag.setAttribute("id", "dlmgrcss");
- }
- else {
-   document.getElementById("dlmgrcss").remove();
- }});
-
- if (Services.prefs.getBoolPref("floorp.downloading.red.color", false)) {
-  var Tag = document.createElement("style");
-  Tag.innerText = `@import url(chrome://browser/skin/options/downloading-redcolor.css`
-  document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-  Tag.setAttribute("id", "dlredcolor");
-}
-Services.prefs.addObserver("floorp.downloading.red.color", function(){
- if (Services.prefs.getBoolPref("floorp.downloading.red.color", false)) {
-   var Tag = document.createElement("style");
-   Tag.innerText = `@import url(chrome://browser/skin/options/downloading-redcolor.css`
-   document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-   Tag.setAttribute("id", "dlredcolor");
- }
- else {
-   document.getElementById("dlredcolor").remove();
- }});
-
- if (Services.prefs.getBoolPref("floorp.navbar.bottom", false)) {
-   var Tag = document.createElement("style");
-   Tag.setAttribute("id", "navvarcss");
-   Tag.innerText = `@import url(chrome://browser/skin/options/navbar-botttom.css)`
-   document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend', Tag);
-   document.getElementById("browser-bottombox").after(document.getElementById("nav-bar"));
- }
-  Services.prefs.addObserver("floorp.navbar.bottom", function () {
-    if (Services.prefs.getBoolPref("floorp.navbar.bottom", false)) {
-      var Tag = document.createElement("style");
-      Tag.setAttribute("id", "navvarcss");
-      Tag.innerText = `@import url(chrome://browser/skin/options/navbar-botttom.css)`
-      document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend', Tag);
-      document.getElementById("browser-bottombox").after(document.getElementById("nav-bar"));
-    } else {
-      document.getElementById("navvarcss").remove();
-
+    Tag.setAttribute("id", "floorp-navvarcss");
+    Tag.innerText = `@import url(chrome://browser/skin/options/navbar-botttom.css)`;
+    document.head.appendChild(Tag);
+    document.getElementById("browser-bottombox")
+      .after(document.getElementById("nav-bar"));
+  } else {
+    document.getElementById("floorp-navvarcss")?.remove();
+    if (event.reason === "changed") {
       //Fix for the bug that bookmarksbar is on the navigation toolbar when the pref is cahaned to false
 
-      document.getElementById("navigator-toolbox").appendChild(document.getElementById("nav-bar"));
+      document.getElementById("navigator-toolbox")
+        .appendChild(document.getElementById("nav-bar"));
 
       if (!Services.prefs.getBoolPref("floorp.bookmarks.fakestatus.mode", false)) {
-        document.getElementById("navigator-toolbox").appendChild(document.getElementById("PersonalToolbar"));
+        document.getElementById("navigator-toolbox")
+          .appendChild(document.getElementById("PersonalToolbar"));
       }
     }
-  });
+  }
+});
 
-  Services.prefs.addObserver("floorp.disable.fullscreen.notification", function(){
-    if (Services.prefs.getBoolPref("floorp.disable.fullscreen.notification", false)) {
-      var Tag = document.createElement("style");
-      Tag.innerText = `@import url(chrome://browser/skin/options/disableFullScreenNotification.css)`
-      document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-      Tag.setAttribute("id", "DFSN");
-    }
-    else {
-      document.getElementById("DFSN").remove();
-  }});
-
- /*------------------------------------------- sidebar -------------------------------------------*/
-
- if (Services.prefs.getBoolPref("floorp.browser.sidebar.right", false)) {    
-  var Tag = document.createElement("style");
-  Tag.innerText = `.browser-sidebar2 {-moz-box-ordinal-group: 10 !important;}#sidebar-select-box{-moz-box-ordinal-group: 15 !important;} #sidebar-splitter2 {-moz-box-ordinal-group: 9 !important;}`;
-  document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-  Tag.setAttribute("id", "sidebar2css");
-}
-Services.prefs.addObserver("floorp.browser.sidebar.right", function(){
-  if (Services.prefs.getBoolPref("floorp.browser.sidebar.right", false)) {
+UICustomPrefHandler("floorp.disable.fullscreen.notification", function(event) {
+  if (event.prefValue) {
     var Tag = document.createElement("style");
-    Tag.innerText = `.browser-sidebar2 {-moz-box-ordinal-group: 10 !important;}#sidebar-select-box{-moz-box-ordinal-group: 15 !important;} #sidebar-splitter2 {-moz-box-ordinal-group: 9 !important;}`;
-    document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag);
-    Tag.setAttribute("id", "sidebar2css");
+    Tag.innerText = `@import url(chrome://browser/skin/options/disableFullScreenNotification.css)`;
+    Tag.setAttribute("id", "floorp-DFSN");
+    document.head.appendChild(Tag);
+  } else {
+    document.getElementById("floorp-DFSN")?.remove();
   }
-  else {
-    document.getElementById("sidebar2css").remove();
-  }});
+});
 
-  if (!Services.prefs.getBoolPref("floorp.browser.sidebar.enable", false)) {
-      var Tag = document.createElement("style");
-      Tag.innerText = `#sidebar-button2, #wrapper-sidebar-button2, .browser-sidebar2, #sidebar-select-box  {display: none !important;}`;
-      document.getElementsByTagName("head")[0].insertAdjacentElement("beforeend",Tag)
+/*------------------------------------------- sidebar -------------------------------------------*/
+
+UICustomPrefHandler("floorp.browser.sidebar.right", function(event) {
+  if (event.prefValue) {
+    var Tag = document.createElement("style");
+    Tag.textContent = `
+    .browser-sidebar2 {
+      -moz-box-ordinal-group: 10 !important;
+    }
+    #sidebar-select-box{
+      -moz-box-ordinal-group: 15 !important;
+    }
+    #sidebar-splitter2 {
+      -moz-box-ordinal-group: 9 !important;
+    }`;
+    Tag.setAttribute("id", "floorp-sidebar2css");
+    document.head.appendChild(Tag);
+  } else {
+    document.getElementById("floorp-sidebar2css")?.remove();
   }
+});
+
+if (!Services.prefs.getBoolPref("floorp.browser.sidebar.enable", false)) {
+  var Tag = document.createElement("style");
+  Tag.textContent = `
+  #sidebar-button2,
+  #wrapper-sidebar-button2,
+  .browser-sidebar2,
+  #sidebar-select-box {
+    display: none !important;
+  }`;
+  document.head.appendChild(Tag);
+}
