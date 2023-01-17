fcf6aca513b8ddef38d128a2d593742b59aaaec0
Mike Conley <mconley@mozilla.com>
2023-01-17 20:30:58 +0000

    Bug 1809498 - Make browser.migrate.content-modal.enabled also control opening the new migration-dialog.html document. r=niklas
    Differential Revision: https://phabricator.services.mozilla.com/D166838

---
+++ browser/components/migration/tests/browser/browser_dialog_open.js
@@ -0,0 +1,53 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests that we can open the migration dialog in a TabDialogBox when
+ * calling MigrationUtils.showMigrationWizard within a tabbrowser window
+ * execution context.
+ */
+add_task(async function test_migration_dialog_open_in_tab_dialog_box() {
+  let wizardReady = BrowserTestUtils.waitForEvent(window, "MigrationWizard:Ready");
+  let dialogPromise = TestUtils.topicObserved("subdialog-loaded");
+  MigrationUtils.showMigrationWizard(window, {});
+  await dialogPromise;
+  await wizardReady;
+
+  Assert.ok(true, "Migration dialog was opened");
+
+  let dialogClosed = BrowserTestUtils.waitForEvent(window, "dialogclose");
+  EventUtils.sendKey("ESCAPE");
+  await dialogClosed;
+});
+
+/**
+ * Tests that we can open the migration dialog in a stand-alone window
+ * when calling MigrationUtils.showMigrationWizard with a null opener
+ * argument, or a non-tabbrowser window context.
+ */
+add_task(async function test_migration_dialog_open_in_xul_window() {
+  let firstWindowOpened = BrowserTestUtils.domWindowOpened();
+  MigrationUtils.showMigrationWizard(null, {});
+  let firstDialogWin = await firstWindowOpened;
+
+  await BrowserTestUtils.waitForEvent(firstDialogWin, "MigrationWizard:Ready");
+
+  Assert.ok(true, "Migration dialog was opened");
+
+  // Now open a second migration dialog, using the first as the window
+  // argument.
+
+  let secondWindowOpened = BrowserTestUtils.domWindowOpened();
+  MigrationUtils.showMigrationWizard(firstDialogWin, {});
+  let secondDialogWin = await secondWindowOpened;
+
+  await BrowserTestUtils.waitForEvent(secondDialogWin, "MigrationWizard:Ready");
+
+  for (let dialogWin of [firstDialogWin, secondDialogWin]) {
+    let dialogClosed = BrowserTestUtils.domWindowClosed(dialogWin);
+    dialogWin.close();
+    await dialogClosed;
+  }
+});
