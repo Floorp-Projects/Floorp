f23f44668abcc8e933bfe56356c54cdd66d081f6
Jon Coppeard <jcoppeard@mozilla.com>
2023-01-17 17:06:50 +0000

    Bug 1809861 - Part 1: Simplify and clean up off-main thread script compilation r=smaug
    The main change in this patch is to prevent access to main-thread objects
    while off-thread. This is done by using nsMainThreadPtrHandle to wrap main
    thread pointers in the runnable. This prevents access to their targets when
    off thread and ensure they are only released on the main thread.
    
    This means that mRunnable is now only accessed on the main thread and so it
    doesn't need to be atomic and can be a normal RefPtr. We also don't need to
    leak a reference to it in AttemptOffThreadScriptCompile.
    
    This also requires that timing data is moved to the runnable.
    
    Cancellation should always have happened by unlink or destruction of
    ScriptLoadContext so handling for that is removed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D166667

--- dom/script/ScriptLoadContext.h
+++ dom/script/ScriptLoadContext.h
@@ -153,9 +153,6 @@ class ScriptLoadContext : public JS::loader::LoadContextBase,
 
   void MaybeCancelOffThreadScript();
 
-  TimeStamp mOffThreadParseStartTime;
-  TimeStamp mOffThreadParseStopTime;
-
   ScriptMode mScriptMode;  // Whether this is a blocking, defer or async script.
   bool mScriptFromHead;    // Synchronous head script block loading of other non
                            // js/css content.
@@ -172,11 +169,13 @@ class ScriptLoadContext : public JS::loader::LoadContextBase,
   bool mWasCompiledOMT;   // True if the script has been compiled off main
                           // thread.
 
-  JS::OffThreadToken* mOffThreadToken;  // Off-thread parsing token.
+  // Off-thread parsing token. Set at the start of off-thread parsing and
+  // cleared when the result of the parse is used.
+  JS::OffThreadToken* mOffThreadToken;
 
-  Atomic<Runnable*> mRunnable;  // Runnable created when dispatching off thread
-                                // compile. Tracked here so that it can be
-                                // properly released during cancellation.
+  // Runnable that is dispatched to the main thread when off-thread compilation
+  // completes.
+  RefPtr<Runnable> mRunnable;
 
   int32_t mLineNo;
 
