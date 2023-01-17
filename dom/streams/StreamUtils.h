40587564abda9b1a458e5978f006ecd4d78ac077
Kagami Sascha Rosylight <krosylight@mozilla.com>
2023-01-17 14:59:28 +0000

    Bug 1809895 - Part 1: Move PromisifyAlgorithm to StreamUtils.h r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D166805

--- dom/streams/StreamUtils.h
+++ dom/streams/StreamUtils.h
@@ -6,6 +6,9 @@
 #define mozilla_dom_StreamUtils_h
 
 #include "mozilla/ErrorResult.h"
+#include "mozilla/dom/Promise.h"
+
+class nsIGlobalObject;
 
 namespace mozilla::dom {
 
@@ -13,6 +16,37 @@ struct QueuingStrategy;
 
 double ExtractHighWaterMark(const QueuingStrategy& aStrategy,
                             double aDefaultHWM, ErrorResult& aRv);
+
+// Promisification algorithm, shared among:
+// Step 2 and 3 of https://streams.spec.whatwg.org/#readablestream-set-up
+// Step 2 and 3 of
+// https://streams.spec.whatwg.org/#readablestream-set-up-with-byte-reading-support
+// Step 2 and 3 of https://streams.spec.whatwg.org/#writablestream-set-up
+// Step 5 and 6 of https://streams.spec.whatwg.org/#transformstream-set-up
+template <typename T>
+MOZ_CAN_RUN_SCRIPT static already_AddRefed<Promise> PromisifyAlgorithm(
+    nsIGlobalObject* aGlobal, T aFunc, mozilla::ErrorResult& aRv) {
+  // Step 1. Let result be the result of running (algorithm). If this throws an
+  // exception e, return a promise rejected with e.
+  RefPtr<Promise> result;
+  if constexpr (!std::is_same<decltype(aFunc(aRv)), void>::value) {
+    result = aFunc(aRv);
+  } else {
+    aFunc(aRv);
+  }
+
+  if (aRv.Failed()) {
+    return Promise::CreateRejectedWithErrorResult(aGlobal, aRv);
+  }
+
+  // Step 2. If result is a Promise, then return result.
+  if (result) {
+    return result.forget();
+  }
+
+  // Step 3. Return a promise resolved with undefined.
+  return Promise::CreateResolvedWithUndefined(aGlobal, aRv);
+}
 
 }  // namespace mozilla::dom
 
