3fe587525a1d498972b97f137a302a4644e232f7
alwu <alwu@mozilla.com>
2023-01-17 20:00:49 +0000

    Bug 1809985 - part1 : add a test to ensure that playback can be recovered from crash. r=jolin
    Differential Revision: https://phabricator.services.mozilla.com/D166721

---
+++ dom/media/test/browser/wmfme/browser_wmfme_crash.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/**
+ * This test aims to ensure that the media engine playback will recover from a
+ * crash and keep playing without any problem.
+ */
+add_task(async function setupTestingPref() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["media.wmf.media-engine.enabled", true],
+      ["media.wmf.media-engine.channel-decoder.enabled", true],
+    ],
+  });
+});
+
+const VIDEO_PAGE = GetTestWebBasedURL("file_video.html");
+
+add_task(async function testPlaybackRecoveryFromCrash() {
+  info(`Create a tab and load test page`);
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    window.gBrowser,
+    "about:blank"
+  );
+  BrowserTestUtils.loadURI(tab.linkedBrowser, VIDEO_PAGE);
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+
+  await playVideo(tab);
+
+  info("Ensure video is running via the media engine framework");
+  await assertRunningProcessAndDecoderName(tab, {
+    expectedProcess: "Utility MF Media Engine CDM",
+    expectedDecoder: "media engine video stream",
+  });
+
+  const pidBeforeCrash = await getMFCDMProcessId();
+  await crashUtilityProcess(pidBeforeCrash);
+
+  info("The CDM process should be recreated which makes media keep playing");
+  await assertRunningProcessAndDecoderName(tab, {
+    expectedProcess: "Utility MF Media Engine CDM",
+    expectedDecoder: "media engine video stream",
+  });
+
+  const pidAfterCrash = await getMFCDMProcessId();
+  isnot(
+    pidBeforeCrash,
+    pidAfterCrash,
+    `new process ${pidAfterCrash} is not previous crashed one ${pidBeforeCrash}`
+  );
+
+  BrowserTestUtils.removeTab(tab);
+});
