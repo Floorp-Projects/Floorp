3a611b468ee6e3be4f8a5ee44e5fcfb950ab9b4e
Noemi Erli <nerli@mozilla.com>
2023-01-18 15:47:04 +0200

    Backed out changeset c7c04a2cfaf9 (bug 1810734) for causing Bug 1810985 CLOSED TREE

--- python/mozbuild/mozbuild/action/langpack_manifest.py
+++ python/mozbuild/mozbuild/action/langpack_manifest.py
@@ -423,7 +423,7 @@ def get_version_maybe_buildid(version):
 #                'base_path': 'browser/'
 #            }
 #        },
-#        'browser_specific_settings': {
+#        'applications': {
 #            'gecko':  {
 #                'strict_min_version': '57.0',
 #                'strict_max_version': '57.0.*',
@@ -454,7 +454,7 @@ def create_webmanifest(
     manifest = {
         "langpack_id": main_locale,
         "manifest_version": 2,
-        "browser_specific_settings": {
+        "applications": {
             "gecko": {
                 "id": langpack_eid,
                 "strict_min_version": min_app_ver,
