2e17b930aa4d8f1ce66edef8cc54863c44a6454e
Marco Bonardo <mbonardo@mozilla.com>
2023-01-18 10:18:18 +0000

    Bug 1750317 - Address bar may cut the search string in rare circumstances. r=adw
    The bug covers a case where, usually due to a busy system, events may be
    delayed causing the urlbar bufferer to handle Enter before the current context
    has notified any result to the view. In that case we may serve a partial
    search string.
    The patch makes the events bufferer wait for the onQueryResults notification
    rather than just relying on results being present in the context object
    
    Differential Revision: https://phabricator.services.mozilla.com/D166928

---
+++ browser/components/urlbar/tests/browser/browser_bufferer_onQueryResults.js
@@ -0,0 +1,82 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+// This test covers a race condition of input events followed by Enter.
+// The test is putting the event bufferer in a situation where a new query has
+// already results in the context object, but onQueryResults has not been
+// invoked yet. The EventBufferer should wait for onQueryResults to proceed,
+// otherwise the view cannot yet contain the updated query string and we may
+// end up searching for a partial string.
+
+add_setup(async function() {
+  sandbox = sinon.createSandbox();
+  await SearchTestUtils.promiseNewSearchEngine({
+    url: getRootDirectory(gTestPath) + "searchSuggestionEngine.xml",
+    setAsDefault: true,
+  });
+  // To reproduce the race condition it's important to disable any provider
+  // having `deferUserSelection` == true;
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.urlbar.suggest.engines", false]],
+  });
+  await PlacesUtils.history.clear();
+  registerCleanupFunction(async () => {
+    await PlacesUtils.history.clear();
+    sandbox.restore();
+  });
+});
+
+add_task(async function test() {
+  let tab = await BrowserTestUtils.openNewForegroundTab({
+    gBrowser,
+    opening: "about:robots",
+  });
+
+  let defer = PromiseUtils.defer();
+  let waitFirstSearchResults = PromiseUtils.defer();
+  let count = 0;
+  let original = gURLBar.controller.notify;
+  sandbox.stub(gURLBar.controller, "notify").callsFake(async (msg, context) => {
+    if (context?.deferUserSelectionProviders.size) {
+      Assert.ok(false, "Any provider deferring selection should be disabled");
+    }
+    if (msg == "onQueryResults") {
+      waitFirstSearchResults.resolve();
+      count++;
+    }
+    // Delay any events after the second onQueryResults call.
+    if (count >= 2) {
+      await defer.promise;
+    }
+    return original.call(gURLBar.controller, msg, context);
+  });
+
+  gURLBar.focus();
+  gURLBar.select();
+  EventUtils.synthesizeKey("t", {});
+  await waitFirstSearchResults.promise;
+  EventUtils.synthesizeKey("e", {});
+
+  let promiseLoaded = BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
+  EventUtils.synthesizeKey("KEY_Enter", {});
+
+  let context = await UrlbarTestUtils.promiseSearchComplete(window);
+  await TestUtils.waitForCondition(
+    () => context.results.length,
+    "Waiting for any result in the QueryContext"
+  );
+  info("Simulate a request to replay deferred events at this point");
+  gURLBar.eventBufferer.replayDeferredEvents(true);
+
+  defer.resolve();
+  await promiseLoaded;
+
+  Assert.equal(
+    gBrowser.selectedBrowser.currentURI.spec,
+    "http://mochi.test:8888/?terms=" + gURLBar.value
+  );
+
+  BrowserTestUtils.removeTab(tab);
+  sandbox.restore();
+});
