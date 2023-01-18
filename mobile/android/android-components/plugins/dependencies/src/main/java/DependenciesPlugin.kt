ef76ae1aabab8de904dc424dd95dd880a8b127d2
Jan-Erik Rediger <jrediger@mozilla.com>
2023-01-18 17:08:25 +0100

    Update to application-services 96.3.0

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -43,7 +43,7 @@ object Versions {
     const val disklrucache = "2.0.2"
     const val leakcanary = "2.10"
 
-    const val mozilla_appservices = "96.2.1"
+    const val mozilla_appservices = "96.3.0"
 
     // DO NOT MODIFY MANUALLY. This is auto-updated along with GeckoView.
     const val mozilla_glean = "52.0.0"
