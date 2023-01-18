1a91aa79d93034a3ee94e5c54a673ff5d8657ff6
Eden Chuang <echuang@mozilla.com>
2023-01-18 00:50:20 +0000

    Bug 1351231 - Integrate FetchChild into Fetch.cpp r=dom-worker-reviewers,jesup
    Depends on D142437
    
    Differential Revision: https://phabricator.services.mozilla.com/D142704

--- dom/fetch/FetchChild.h
+++ dom/fetch/FetchChild.h
@@ -5,15 +5,26 @@
 #ifndef mozilla_dom_fetchChild_h__
 #define mozilla_dom_fetchChild_h__
 
+#include "mozilla/dom/AbortSignal.h"
+#include "mozilla/dom/AbortFollower.h"
+#include "mozilla/dom/FlippedOnce.h"
 #include "mozilla/dom/PFetchChild.h"
+#include "nsIConsoleReportCollector.h"
+#include "nsIContentSecurityPolicy.h"
+#include "nsISupports.h"
 
 namespace mozilla::dom {
 
-class FetchChild final : public PFetchChild {
+class FetchObserver;
+class ThreadSafeWorkerRef;
+class Promise;
+class WorkerPrivate;
+
+class FetchChild final : public PFetchChild, public AbortFollower {
   friend class PFetchChild;
 
  public:
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(FetchChild, override);
+  NS_DECL_THREADSAFE_ISUPPORTS
 
   mozilla::ipc::IPCResult Recv__delete__(const nsresult&& aResult);
 
@@ -29,10 +40,35 @@ class FetchChild final : public PFetchChild {
 
   mozilla::ipc::IPCResult RecvOnCSPViolationEvent(const nsAString& aJSon);
 
+  void SetCSPEventListener(nsICSPEventListener* aListener);
+
+  static RefPtr<FetchChild> Create(WorkerPrivate* aWorkerPrivate,
+                                   RefPtr<Promise> aPromise,
+                                   RefPtr<AbortSignalImpl> aSignalImpl,
+                                   RefPtr<FetchObserver> aObserver);
+
+  FetchChild(RefPtr<Promise>&& aPromise, RefPtr<AbortSignalImpl>&& aSignalImpl,
+             RefPtr<FetchObserver>&& aObserver);
+
+  // AbortFollower
+  void RunAbortAlgorithm() override;
+
+  void DoFetchOp(const FetchOpArgs& aArgs);
+
  private:
   ~FetchChild() = default;
 
+  // WorkerPrivate shutdown callback.
+  void Shutdown();
   void ActorDestroy(ActorDestroyReason aReason) override;
+
+  RefPtr<ThreadSafeWorkerRef> mWorkerRef;
+  RefPtr<Promise> mPromise;
+  RefPtr<AbortSignalImpl> mSignalImpl;
+  RefPtr<FetchObserver> mFetchObserver;
+  nsCOMPtr<nsICSPEventListener> mCSPEventListener;
+  nsCOMPtr<nsIConsoleReportCollector> mReporter;
+  FlippedOnce<false> mIsShutdown;
 };
 
 }  // namespace mozilla::dom
