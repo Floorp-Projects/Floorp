c36891d32e60bc02e00ef6f22b75d0e85d794a2e
André Bargull <andre.bargull@gmail.com>
2023-01-18 19:32:27 +0000

    Bug 1810688 - Part 19: Directly fail for values in [-0.5,-0) in round{Double,Float32}ToInt32 for x86-shared. r=jandem
    Port the changes from part 17 to x86-shared.
    
    Differential Revision: https://phabricator.services.mozilla.com/D167121

--- js/src/jit/x86-shared/MacroAssembler-x86-shared.cpp
+++ js/src/jit/x86-shared/MacroAssembler-x86-shared.cpp
@@ -1913,11 +1913,11 @@ void MacroAssembler::roundFloat32ToInt32(FloatRegister src, Register dest,
   loadConstantFloat32(GetBiggestNumberLessThan(0.5f), temp);
   branchFloat(Assembler::DoubleLessThanOrEqual, src, scratch, &negativeOrZero);
   {
-    // Input is non-negative. Add the biggest float less than 0.5 and truncate,
-    // rounding down (because if the input is the biggest float less than 0.5,
-    // adding 0.5 would undesirably round up to 1). Note that we have to add the
-    // input to the temp register because we're not allowed to modify the input
-    // register.
+    // Input is strictly positive or NaN. Add the biggest float less than 0.5
+    // and truncate, rounding down (because if the input is the biggest float
+    // less than 0.5, adding 0.5 would undesirably round up to 1). Note that we
+    // have to add the input to the temp register because we're not allowed to
+    // modify the input register.
     addFloat32(src, temp);
     truncateFloat32ToInt32(temp, dest, fail);
     jump(&end);
@@ -1940,48 +1940,37 @@ void MacroAssembler::roundFloat32ToInt32(FloatRegister src, Register dest,
   // Input is negative.
   bind(&negative);
   {
-    // Inputs in ]-0.5; 0] need to be added 0.5, other negative inputs need to
-    // be added the biggest double less than 0.5.
-    Label loadJoin;
+    // Inputs in [-0.5, 0) are rounded to -0. Fail.
     loadConstantFloat32(-0.5f, scratch);
-    branchFloat(Assembler::DoubleLessThan, src, scratch, &loadJoin);
-    loadConstantFloat32(0.5f, temp);
-    bind(&loadJoin);
+    branchFloat(Assembler::DoubleGreaterThanOrEqual, src, scratch, fail);
+
+    // Other negative inputs need the biggest float less than 0.5 added.
+    //
+    // The result is stored in the temp register (currently contains the biggest
+    // float less than 0.5).
+    addFloat32(src, temp);
 
     if (HasSSE41()) {
-      // Add 0.5 and round toward -Infinity. The result is stored in the temp
-      // register (currently contains 0.5).
-      addFloat32(src, temp);
+      // Round toward -Infinity.
       vroundss(X86Encoding::RoundDown, temp, scratch);
 
       // Truncate.
       truncateFloat32ToInt32(scratch, dest, fail);
-
-      // If the result is positive zero, then the actual result is -0. Fail.
-      // Otherwise, the truncation will have produced the correct negative
-      // integer.
-      branchTest32(Assembler::Zero, dest, dest, fail);
     } else {
-      addFloat32(src, temp);
       // Round toward -Infinity without the benefit of ROUNDSS.
-      {
-        // If input + 0.5 >= 0, input is a negative number >= -0.5 and the
-        // result is -0.
-        branchFloat(Assembler::DoubleGreaterThanOrEqual, temp, scratch, fail);
 
-        // Truncate and round toward zero.
-        // This is off-by-one for everything but integer-valued inputs.
-        truncateFloat32ToInt32(temp, dest, fail);
+      // Truncate and round toward zero.
+      // This is off-by-one for everything but integer-valued inputs.
+      truncateFloat32ToInt32(temp, dest, fail);
 
-        // Test whether the truncated double was integer-valued.
-        convertInt32ToFloat32(dest, scratch);
-        branchFloat(Assembler::DoubleEqualOrUnordered, temp, scratch, &end);
+      // Test whether the truncated float was integer-valued.
+      convertInt32ToFloat32(dest, scratch);
+      branchFloat(Assembler::DoubleEqualOrUnordered, temp, scratch, &end);
 
-        // Input is not integer-valued, so we rounded off-by-one in the
-        // wrong direction. Correct by subtraction.
-        subl(Imm32(1), dest);
-        // Cannot overflow: output was already checked against INT_MIN.
-      }
+      // Input is not integer-valued, so we rounded off-by-one in the
+      // wrong direction. Correct by subtraction.
+      subl(Imm32(1), dest);
+      // Cannot overflow: output was already checked against INT_MIN.
     }
   }
 
@@ -1999,11 +1988,11 @@ void MacroAssembler::roundDoubleToInt32(FloatRegister src, Register dest,
   loadConstantDouble(GetBiggestNumberLessThan(0.5), temp);
   branchDouble(Assembler::DoubleLessThanOrEqual, src, scratch, &negativeOrZero);
   {
-    // Input is positive. Add the biggest double less than 0.5 and truncate,
-    // rounding down (because if the input is the biggest double less than 0.5,
-    // adding 0.5 would undesirably round up to 1). Note that we have to add the
-    // input to the temp register because we're not allowed to modify the input
-    // register.
+    // Input is strictly positive or NaN. Add the biggest double less than 0.5
+    // and truncate, rounding down (because if the input is the biggest double
+    // less than 0.5, adding 0.5 would undesirably round up to 1). Note that we
+    // have to add the input to the temp register because we're not allowed to
+    // modify the input register.
     addDouble(src, temp);
     truncateDoubleToInt32(temp, dest, fail);
     jump(&end);
@@ -2026,48 +2015,37 @@ void MacroAssembler::roundDoubleToInt32(FloatRegister src, Register dest,
   // Input is negative.
   bind(&negative);
   {
-    // Inputs in ]-0.5; 0] need to be added 0.5, other negative inputs need to
-    // be added the biggest double less than 0.5.
-    Label loadJoin;
+    // Inputs in [-0.5, 0) are rounded to -0. Fail.
     loadConstantDouble(-0.5, scratch);
-    branchDouble(Assembler::DoubleLessThan, src, scratch, &loadJoin);
-    loadConstantDouble(0.5, temp);
-    bind(&loadJoin);
+    branchDouble(Assembler::DoubleGreaterThanOrEqual, src, scratch, fail);
+
+    // Other negative inputs need the biggest double less than 0.5 added.
+    //
+    // The result is stored in the temp register (currently contains the biggest
+    // double less than 0.5).
+    addDouble(src, temp);
 
     if (HasSSE41()) {
-      // Add 0.5 and round toward -Infinity. The result is stored in the temp
-      // register (currently contains 0.5).
-      addDouble(src, temp);
+      // Round toward -Infinity.
       vroundsd(X86Encoding::RoundDown, temp, scratch);
 
       // Truncate.
       truncateDoubleToInt32(scratch, dest, fail);
-
-      // If the result is positive zero, then the actual result is -0. Fail.
-      // Otherwise, the truncation will have produced the correct negative
-      // integer.
-      branchTest32(Assembler::Zero, dest, dest, fail);
     } else {
-      addDouble(src, temp);
       // Round toward -Infinity without the benefit of ROUNDSD.
-      {
-        // If input + 0.5 >= 0, input is a negative number >= -0.5 and the
-        // result is -0.
-        branchDouble(Assembler::DoubleGreaterThanOrEqual, temp, scratch, fail);
 
-        // Truncate and round toward zero.
-        // This is off-by-one for everything but integer-valued inputs.
-        truncateDoubleToInt32(temp, dest, fail);
+      // Truncate and round toward zero.
+      // This is off-by-one for everything but integer-valued inputs.
+      truncateDoubleToInt32(temp, dest, fail);
 
-        // Test whether the truncated double was integer-valued.
-        convertInt32ToDouble(dest, scratch);
-        branchDouble(Assembler::DoubleEqualOrUnordered, temp, scratch, &end);
+      // Test whether the truncated double was integer-valued.
+      convertInt32ToDouble(dest, scratch);
+      branchDouble(Assembler::DoubleEqualOrUnordered, temp, scratch, &end);
 
-        // Input is not integer-valued, so we rounded off-by-one in the
-        // wrong direction. Correct by subtraction.
-        subl(Imm32(1), dest);
-        // Cannot overflow: output was already checked against INT_MIN.
-      }
+      // Input is not integer-valued, so we rounded off-by-one in the
+      // wrong direction. Correct by subtraction.
+      subl(Imm32(1), dest);
+      // Cannot overflow: output was already checked against INT_MIN.
     }
   }
 
