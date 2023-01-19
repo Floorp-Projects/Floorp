6b04a8db7878d15903a35d745561b51244abd316
Emilio Cobos Álvarez <emilio@crisal.io>
2023-01-19 14:33:13 +0000

    Bug 1809742 - Remove non_native_theme from mozinfo/web-platform metadata. r=dshin
    Depends on D166601
    
    Differential Revision: https://phabricator.services.mozilla.com/D166620

--- python/mozbuild/mozbuild/mozinfo.py
+++ python/mozbuild/mozbuild/mozinfo.py
@@ -96,7 +96,6 @@ def build_dict(config, env=os.environ):
     d["artifact"] = substs.get("MOZ_ARTIFACT_BUILDS") == "1"
     d["ccov"] = substs.get("MOZ_CODE_COVERAGE") == "1"
     d["cc_type"] = substs.get("CC_TYPE")
-    d["non_native_theme"] = True
     d["domstreams"] = substs.get("MOZ_DOM_STREAMS") == "1"
     d["isolated_process"] = (
         substs.get("MOZ_ANDROID_CONTENT_SERVICE_ISOLATED_PROCESS") == "1"
