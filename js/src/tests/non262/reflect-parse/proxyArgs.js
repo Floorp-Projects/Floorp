d210868e9485fceed82fa3b26528c1b78ad9d8e7
Iain Ireland <iireland@mozilla.com>
2023-01-19 18:41:23 +0000

    Bug 1800629: Remove custom builders from Reflect.parse r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D167215

--- js/src/tests/non262/reflect-parse/proxyArgs.js
+++ js/src/tests/non262/reflect-parse/proxyArgs.js
@@ -10,13 +10,4 @@ assertEq(Reflect.parse("0;", opts).loc !== null, true);
 delete opts.loc;
 assertEq(Reflect.parse("0;", opts).loc !== null, true);  // default is true
 
-// Proxy builder
-var builder = {
-    program: function (body) { return body.join(); },
-    expressionStatement: function (expr) { return expr + ";" },
-    literal: function (val) { return "" + val; }
-};
-opts = {builder: new Proxy(builder, {})};
-assertEq(Reflect.parse("0xff;", opts), "255;");
-
 reportCompare(0, 0, 'ok');
