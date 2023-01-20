3aa4e8a29408e5ea79869ae4d998fb385f2fcf4c
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-01-20 06:46:54 +0000

    Bug 1804886 - [devtools] Move pretty-fast into debugger/src/workers/pretty-print. r=ochameau.
    I disabled indivual eslint rules for the file and I'll
    fix them in individual commits in this stack
    
    Differential Revision: https://phabricator.services.mozilla.com/D166554

--- devtools/client/debugger/src/workers/pretty-print/tests/test.js
+++ devtools/client/debugger/src/workers/pretty-print/tests/test.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-"use strict";
-
 /*
  * Copyright 2013 Mozilla Foundation and contributors
  * Licensed under the New BSD license. See LICENSE.md or:
