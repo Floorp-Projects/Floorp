c9251a08748ef6ae18fe4cdd89c765c141fbe10f
Jan-Erik Rediger <jrediger@mozilla.com>
2023-01-20 09:28:30 +0000

    Bug 1808586 - Upgrade to Glean v52.0.1 r=chutten,supply-chain-reviewers
    Depends on D167245
    
    Differential Revision: https://phabricator.services.mozilla.com/D167247

--- third_party/rust/glean-core/src/database/mod.rs
+++ third_party/rust/glean-core/src/database/mod.rs
@@ -5,13 +5,16 @@
 use std::collections::btree_map::Entry;
 use std::collections::BTreeMap;
 use std::fs;
+use std::io;
 use std::num::NonZeroU64;
 use std::path::Path;
 use std::str;
 use std::sync::RwLock;
 
+use crate::ErrorKind;
+
 use rkv::migrator::Migrator;
-use rkv::StoreOptions;
+use rkv::{MigrateError, StoreError, StoreOptions};
 
 /// Unwrap a `Result`s `Ok` value or do the specified action.
 ///
@@ -81,8 +84,6 @@ fn delete_lmdb_database(path: &Path) {
 /// without migrating data.
 /// This is a no-op if no LMDB database file exists.
 pub fn migrate(path: &Path, dst_env: &Rkv) {
-    use rkv::{MigrateError, StoreError};
-
     log::debug!("Migrating files in {}", path.display());
 
     // Shortcut if no data to migrate is around.
@@ -708,7 +709,22 @@ impl Database {
             writer.commit()?;
             Ok(())
         });
+
         if let Err(e) = res {
+            // We try to clear everything.
+            // If there was no data to begin with we encounter a `NotFound` error.
+            // There's no point in logging that.
+            if let ErrorKind::Rkv(StoreError::IoError(ioerr)) = e.kind() {
+                if let io::ErrorKind::NotFound = ioerr.kind() {
+                    log::debug!(
+                        "Could not clear store for lifetime {:?}: {:?}",
+                        lifetime,
+                        ioerr
+                    );
+                    return;
+                }
+            }
+
             log::warn!("Could not clear store for lifetime {:?}: {:?}", lifetime, e);
         }
     }
