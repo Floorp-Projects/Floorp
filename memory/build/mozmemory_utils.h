29bcae0d93310c2c01d233053dcd36c37766835c
Ray Kraesig <rkraesig@mozilla.com>
2023-01-20 01:50:54 +0000

    Bug 1804499 - [3/4] Expose stall-and-retry code beyond mozjemalloc.cpp  r=glandium
    Extract the stall-and-retry logic from mozjemalloc into a header. Write
    a unit test for it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D164107

---
+++ memory/build/mozmemory_utils.h
@@ -0,0 +1,67 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozjemalloc_utils_h
+#define mozjemalloc_utils_h
+
+#include <optional>
+#include <type_traits>
+
+namespace mozilla {
+
+namespace detail {
+// Helper for StallAndRetry error messages.
+template <typename T>
+constexpr bool is_std_optional = false;
+template <typename T>
+constexpr bool is_std_optional<std::optional<T>> = true;
+}  // namespace detail
+
+struct StallSpecs {
+  // Maximum number of retry-attempts before giving up.
+  size_t maxAttempts;
+  // Delay time between successive events.
+  size_t delayMs;
+
+  // Retry a fallible operation until it succeeds or until we've run out of
+  // retries.
+  //
+  // Note that this invokes `aDelayFunc` immediately upon being called! It's
+  // intended for use in the unhappy path, after an initial attempt has failed.
+  //
+  // The function type here may be read:
+  // ```
+  // fn StallAndRetry<R>(
+  //     delay_func: impl Fn(usize) -> (),
+  //     operation: impl Fn() -> Option<R>,
+  // ) -> Option<R>;
+  // ```
+  //
+  template <typename DelayFunc, typename OpFunc>
+  auto StallAndRetry(DelayFunc&& aDelayFunc, OpFunc&& aOperation) const
+      -> decltype(aOperation()) {
+    {
+      // Explicit typecheck for OpFunc, to provide an explicit error message.
+      using detail::is_std_optional;
+      static_assert(is_std_optional<decltype(aOperation())>,
+                    "aOperation() must return std::optional");
+
+      // (clang's existing error messages suffice for aDelayFunc.)
+    }
+
+    for (size_t i = 0; i < maxAttempts; ++i) {
+      aDelayFunc(delayMs);
+      if (const auto opt = aOperation()) {
+        return opt;
+      }
+    }
+    return std::nullopt;
+  }
+};
+
+}  // namespace mozilla
+
+#endif  // mozjemalloc_utils_h
