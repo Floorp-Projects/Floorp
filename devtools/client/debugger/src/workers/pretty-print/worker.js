d907575b234e8d07aa15c04d77d87fdbfcec4360
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-01-20 06:46:54 +0000

    Bug 1804886 - [devtools] Move pretty-fast into debugger/src/workers/pretty-print. r=ochameau.
    I disabled indivual eslint rules for the file and I'll
    fix them in individual commits in this stack
    
    Differential Revision: https://phabricator.services.mozilla.com/D166554

--- devtools/client/debugger/src/workers/pretty-print/worker.js
+++ devtools/client/debugger/src/workers/pretty-print/worker.js
@@ -2,9 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-import prettyFast from "pretty-fast";
-
 import { workerHandler } from "devtools/client/shared/worker-utils";
+import { prettyFast } from "./pretty-fast";
 
 function prettyPrint({ url, indent, sourceText }) {
   const prettified = prettyFast(sourceText, {
