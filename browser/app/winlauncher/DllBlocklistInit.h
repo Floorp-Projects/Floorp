6d4ceee1b9ff7ba3ce69007f21e6f0b86971275f
Greg Stoll <gstoll@mozilla.com>
2023-01-20 18:00:45 +0000

    Bug 1804023 - Part 1: make blocklisting initialization take process type instead of a series of booleans r=gsvelto
    This is just refactoring in preparation for the next patches
    
    Differential Revision: https://phabricator.services.mozilla.com/D167398

--- browser/app/winlauncher/DllBlocklistInit.h
+++ browser/app/winlauncher/DllBlocklistInit.h
@@ -12,14 +12,15 @@
 #if defined(MOZ_LAUNCHER_PROCESS)
 #  include "mozilla/LauncherRegistryInfo.h"
 #endif
+#include "mozilla/ProcessType.h"
 #include "mozilla/WinHeaderOnlyUtils.h"
 
 namespace mozilla {
 
 LauncherVoidResultWithLineInfo InitializeDllBlocklistOOP(
     const wchar_t* aFullImagePath, HANDLE aChildProcess,
-    const IMAGE_THUNK_DATA* aCachedNtdllThunk, const bool aIsUtilityProcess,
-    const bool aIsSocketProcess);
+    const IMAGE_THUNK_DATA* aCachedNtdllThunk,
+    const GeckoProcessType aProcessType);
 
 LauncherVoidResultWithLineInfo InitializeDllBlocklistOOPFromLauncher(
     const wchar_t* aFullImagePath, HANDLE aChildProcess,
