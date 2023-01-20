780ef82c885600f82b4ec03cc284818cc546ddf6
Tiaan Louw <tlouw@mozilla.com>
2023-01-20 10:55:51 +0000

    Bug 1352757 - Add lab(), lch(), oklab(), oklch() to specified colors. r=supply-chain-reviewers,emilio
    Use new changes from cssparser and use the new lab/lch/oklab/oklch color
    formats.
    
    Introduced a new color type AbsoluteColor.  It represents any kind of
    color that has absolute numerical values.  It is also tied to a color
    space and therefore can be trivially converted to another color space.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163579

--- third_party/rust/cssparser/src/size_of_tests.rs
+++ third_party/rust/cssparser/src/size_of_tests.rs
@@ -4,56 +4,49 @@
 
 use crate::cow_rc_str::CowRcStr;
 use crate::tokenizer::Token;
-use std::borrow::Cow;
 
 macro_rules! size_of_test {
-    ($testname: ident, $t: ty, $expected_size: expr) => {
+    ($testname: ident, $t: ty, $expected_min_size: expr, $expected_max_size: expr) => {
         #[test]
         fn $testname() {
             let new = ::std::mem::size_of::<$t>();
-            let old = $expected_size;
-            if new < old {
+            if new < $expected_min_size {
                 panic!(
                     "Your changes have decreased the stack size of {} from {} to {}. \
                      Good work! Please update the expected size in {}.",
                     stringify!($t),
-                    old,
+                    $expected_min_size,
                     new,
                     file!()
                 )
-            } else if new > old {
+            } else if new > $expected_max_size {
                 panic!(
                     "Your changes have increased the stack size of {} from {} to {}. \
                      Please consider choosing a design which avoids this increase. \
                      If you feel that the increase is necessary, update the size in {}.",
                     stringify!($t),
-                    old,
+                    $expected_max_size,
                     new,
                     file!()
                 )
             }
         }
     };
+    ($testname: ident, $t: ty, $expected_size: expr) => {
+        size_of_test!($testname, $t, $expected_size, $expected_size);
+    };
 }
 
 // Some of these assume 64-bit
 size_of_test!(token, Token, 32);
-size_of_test!(std_cow_str, Cow<'static, str>, 32);
+size_of_test!(std_cow_str, std::borrow::Cow<'static, str>, 24, 32);
 size_of_test!(cow_rc_str, CowRcStr, 16);
 
 size_of_test!(tokenizer, crate::tokenizer::Tokenizer, 72);
-size_of_test!(
-    parser_input,
-    crate::parser::ParserInput,
-    if cfg!(rustc_has_pr45225) { 136 } else { 144 }
-);
+size_of_test!(parser_input, crate::parser::ParserInput, 136);
 size_of_test!(parser, crate::parser::Parser, 16);
 size_of_test!(source_position, crate::SourcePosition, 8);
 size_of_test!(parser_state, crate::ParserState, 24);
 
-size_of_test!(basic_parse_error, crate::BasicParseError, 48);
-size_of_test!(
-    parse_error_lower_bound,
-    crate::ParseError<()>,
-    if cfg!(rustc_has_pr45225) { 48 } else { 56 }
-);
+size_of_test!(basic_parse_error, crate::BasicParseError, 40, 48);
+size_of_test!(parse_error_lower_bound, crate::ParseError<()>, 40, 48);
