89488e7ee66aa1b62edb127256c84f0b86a67883
Timothy Nikkel <tnikkel@gmail.com>
2023-01-20 11:55:14 +0000

    Bug 1810563. Ignore CSP on SVG images in srcset in addition to src. r=smaug
    This is the same fix as bug 1262842 but extended to imageset.
    
    Differential Revision: https://phabricator.services.mozilla.com/D167319

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -3745,7 +3745,9 @@ nsresult Document::InitCSP(nsIChannel* aChannel) {
   // served with a CSP might block internally applied inline styles.
   nsCOMPtr<nsILoadInfo> loadInfo = aChannel->LoadInfo();
   if (loadInfo->GetExternalContentPolicyType() ==
-      ExtContentPolicy::TYPE_IMAGE) {
+          ExtContentPolicy::TYPE_IMAGE ||
+      loadInfo->GetExternalContentPolicyType() ==
+          ExtContentPolicy::TYPE_IMAGESET) {
     return NS_OK;
   }
 
