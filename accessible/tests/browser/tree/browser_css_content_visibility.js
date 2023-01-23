3e19701eaa9e76a82006552abb30b0b04ac3c5d9
Jihye Hong <jihye@igalia.com>
2023-01-23 09:19:43 +0000

    Bug 1793747 - Omit the element hidden by 'content-visibility:hidden' from the accessibility tree r=emilio,Jamie
    There is an issue that when the element is hidden because its ancestor has 'content-visibility:hidden', it still appears in the accessibility tree.
    Depending on CSS Containment Spec (https://www.w3.org/TR/css-contain-2/#cv-a11y),
    it should be omitted from the accessibility tree.
    This change fixes the issue above.
    
    The approach is not to create the node in accessibility tree if the element has any ancestor specified with 'content-visibility:hidden'.
    
    This patch can be tested such as:
    (1) Check if the accessibility tree is created correctly
      ./mach test accessible/tests/browser/tree/browser_css_content_visibility.js
    (2) Check if the accessibility tree is updated correctly
      ./mach test accessible/tests/browser/e10s/browser_treeupdate_csscontentvisibility.js
    
    Differential Revision: https://phabricator.services.mozilla.com/D159879

---
+++ accessible/tests/browser/tree/browser_css_content_visibility.js
@@ -0,0 +1,115 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+/* import-globals-from ../../mochitest/role.js */
+loadScripts({ name: "role.js", dir: MOCHITESTS_DIR });
+
+const snippet1 = `
+  <style>
+  #container {
+    width: 150px;
+    height: 150px;
+    background: lightblue;
+  }
+  .hidden {
+    content-visibility: hidden;
+  }
+  .auto {
+    content-visibility: auto;
+  }
+  </style>
+  <div id="container">
+    <div class="hidden" id="hidden-target">
+      hidden target
+      <div id="hidden-child">
+        hidden child
+      </div>
+    </div>
+    <div class="auto" id="auto-target">
+      auto target
+      <div id="auto-child">
+        auto child
+      </div>
+    </div>
+  </div>
+  `;
+
+// Check if the element specified with `content-visibility` property is accessible
+addAccessibleTask(
+  snippet1,
+  async function(browser, accDoc) {
+    const container = findAccessibleChildByID(accDoc, "container");
+    ok(
+      findAccessibleChildByID(container, "hidden-target"),
+      "hidden-target is accessible"
+    );
+    ok(
+      findAccessibleChildByID(container, "auto-target"),
+      "auto-target is accessible"
+    );
+
+    // The test checks if the child element of the element specified with
+    // `content-visibility: hidden` is ignored from a11y tree
+    let target = findAccessibleChildByID(accDoc, "hidden-target");
+    ok(
+      !findAccessibleChildByID(target, "hidden-child"),
+      "Children of hidden-target is not accessible"
+    );
+
+    // The test checks if the child element of the element specified with
+    // `content-visibility: auto` is showen in a11y tree
+    target = findAccessibleChildByID(accDoc, "auto-target");
+    ok(
+      findAccessibleChildByID(target, "auto-child"),
+      "Children of auto-target is accessible"
+    );
+  },
+  { iframe: true, remoteIframe: true, chrome: true }
+);
+
+// Check if the element having `display: contents` and a child of `content-visibility: hidden` element isn't accessible
+const snippet2 = `
+  <style>
+  #target {
+    width: 150px;
+    height: 150px;
+    background-color: lightblue;
+  }
+  #child {
+    width: 100px;
+    height: 100px;
+    background-color: lightgreen;
+  }
+  #grandchild {
+    width: 50px;
+    height: 50px;
+    background-color: red;
+  }
+  .hidden {
+    content-visibility: hidden;
+  }
+  .display-contents {
+    display: contents;
+  }
+  </style>
+  <div id="target" class="hidden">
+    <div id="child" class="display-contents">
+      <div id="grandchild"></div>
+    </div>
+  </div>
+  `;
+
+addAccessibleTask(
+  snippet2,
+  async function(browser, accDoc) {
+    const target = findAccessibleChildByID(accDoc, "target");
+    ok(
+      !findAccessibleChildByID(target, "child"),
+      "Element having `display: contents` and a child of `content-visibility: hidden` element isn't accessible"
+    );
+    testAccessibleTree(target, { SECTION: [] });
+  },
+  { iframe: true, remoteIframe: true, chrome: true }
+);
