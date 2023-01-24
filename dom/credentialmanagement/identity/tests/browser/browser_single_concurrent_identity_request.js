d19ae89fd8cc2cccbd392760e5b685e8491ae217
Benjamin VanderSloot <bvandersloot@mozilla.com>
2023-01-24 15:09:25 +0000

    Bug 1804727, part 2 - Prevent concurrent "identity" Credential requests - r=jschanck
    This is a requirement of the FedCM spec.
    
    Requesting review from you because I believe you are the only other person actively working on Credential Management API things and this touches the `navigator.credentials` object implementation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D167157

---
+++ dom/credentialmanagement/identity/tests/browser/browser_single_concurrent_identity_request.js
@@ -0,0 +1,51 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const TEST_URL = "https://example.com/";
+
+add_task(async function test_concurrent_identity_credential() {
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, TEST_URL);
+
+  let requestCredential = async function() {
+    let promise = content.navigator.credentials.get({
+      identity: {
+        providers: [
+          {
+            configURL:
+              "https://example.net/tests/dom/credentialmanagement/identity/tests/browser/server_manifest.json",
+            clientId: "browser",
+            nonce: "nonce",
+          },
+        ],
+      },
+    });
+    try {
+      return await promise;
+    } catch (err) {
+      return err;
+    }
+  };
+
+  ContentTask.spawn(tab.linkedBrowser, null, requestCredential);
+
+  let secondRequest = ContentTask.spawn(
+    tab.linkedBrowser,
+    null,
+    requestCredential
+  );
+
+  let concurrentResponse = await secondRequest;
+  ok(concurrentResponse, "expect a result from the second request.");
+  ok(concurrentResponse.name, "expect a DOMException which must have a name.");
+  is(
+    concurrentResponse.name,
+    "InvalidStateError",
+    "Expected 'InvalidStateError', but got '" + concurrentResponse.name + "'"
+  );
+
+  // Close tabs.
+  await BrowserTestUtils.removeTab(tab);
+});
