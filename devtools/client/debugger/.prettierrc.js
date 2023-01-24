3540312153f4c03188ebb74a0576e82b55e2093d
Mark Banner <standard8@mozilla.com>
2023-01-24 22:53:55 +0000

    Bug 1811820 - Change prettier config file from json to common js format. r=cmkm,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D167530

--- devtools/client/debugger/.prettierrc.js
+++ devtools/client/debugger/.prettierrc.js
@@ -2,23 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const fs = require("fs");
+"use strict";
+
+// eslint-disable-next-line mozilla/reject-relative-requires
+const topLevelPrettier = require("../../../.prettierrc.js");
 
 module.exports = {
-  ...JSON.parse(fs.readFileSync(__dirname + "/../../../.prettierrc")),
+  ...topLevelPrettier,
   overrides: [
     {
-      files: [
-        "src/**/*.js",
-        "packages/*/src/**/*.js",
-      ],
+      files: ["src/**/*.js", "packages/*/src/**/*.js"],
       options: {
         // The debugger uses Babel 7 and some newer Flow features.
         // Unfortunately, Prettier has not yet adopted a version of Babel's
         // parser with this fix: https://github.com/babel/babel/pull/9891
         // That necessitates us to override to config to explicitly tell
         // Prettier that our files contain Flowtype annotations.
-        "parser": "babel-flow"
+        parser: "babel-flow",
       },
     },
   ],
