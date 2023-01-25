313d1d8eb2a38c11d00fd4ccfe85c2e0ca970efc
Tim Chevalier <tjc@igalia.com>
2023-01-25 16:40:10 +0000

    Bug 1811789 - Fix order of arguments in TypedArrayWith() r=mgaudet
    The order of arguments when calling `CallTypedArrayMethodIfWrapped()` in
    the self-hosted `TypedArrayWith()` function was wrong.
    
    Differential Revision: https://phabricator.services.mozilla.com/D167738

---
+++ js/src/jit-test/tests/arrays/bug-1811789.js
@@ -0,0 +1,14 @@
+// |jit-test| --enable-change-array-by-copy; skip-if: !getBuildConfiguration()['change-array-by-copy']
+
+load(libdir + "asserts.js");
+
+function f5() { } 
+
+function f0() {
+    const v15 = new Uint8Array();
+    f5 &&= v15;
+    const v17 = new Uint8Array();
+    const v16 = wrapWithProto(v17, f5);
+    v16.with();
+}
+assertThrowsInstanceOf(() => f0(), RangeError);
