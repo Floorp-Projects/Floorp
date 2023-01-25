197e363e8fcc16670a6c03b0d81148e78f1f81cf
Tim Giles <tgiles@mozilla.com>
2023-01-25 18:56:15 +0000

    Bug 1801929 - Replace 'Use a Primary Password' link with support-link. r=settings-reviewers,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D167426

--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -2364,12 +2364,6 @@ var gPrivacyPane = {
     checkbox.disabled =
       (noMP && !Services.policies.isAllowed("createMasterPassword")) ||
       (!noMP && !Services.policies.isAllowed("removeMasterPassword"));
-
-    let learnMoreLink = document.getElementById("primaryPasswordLearnMoreLink");
-    let learnMoreURL =
-      Services.urlFormatter.formatURLPref("app.support.baseURL") +
-      "primary-password-stored-logins";
-    learnMoreLink.setAttribute("href", learnMoreURL);
   },
 
   /**
