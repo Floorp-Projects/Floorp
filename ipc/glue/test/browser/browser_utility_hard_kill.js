7851590d6e0451ed76eed4eb5675b246559f215c
Cristian Tuns <ctuns@mozilla.com>
2023-01-25 15:33:33 -0500

    Backed out 7 changesets (bug 1704500) for causing build bustages CLOSED TREE
    Backed out changeset e48723d8d42e (bug 1704500)
    Backed out changeset 897b985f9298 (bug 1704500)
    Backed out changeset c2474d1c0046 (bug 1704500)
    Backed out changeset d4508f8ba7df (bug 1704500)
    Backed out changeset 241be693628d (bug 1704500)
    Backed out changeset 341ec2465c7d (bug 1704500)
    Backed out changeset 0d1c0dcd7645 (bug 1704500)

--- ipc/glue/test/browser/browser_utility_hard_kill.js
+++ ipc/glue/test/browser/browser_utility_hard_kill.js
@@ -4,10 +4,27 @@
 "use strict";
 
 add_task(async () => {
-  await startUtilityProcess(["unknown"]);
+  const utilityPid = await startUtilityProcess();
 
   SimpleTest.expectChildProcessCrash();
 
+  const utilityProcessGone = TestUtils.topicObserved("ipc:utility-shutdown");
+
   info("Hard kill Utility Process");
-  await cleanUtilityProcessShutdown("unknown", true /* preferKill */);
+  const ProcessTools = Cc["@mozilla.org/processtools-service;1"].getService(
+    Ci.nsIProcessToolsService
+  );
+  ProcessTools.kill(utilityPid);
+
+  info(`Waiting for utility process ${utilityPid} to go away.`);
+  let [subject, data] = await utilityProcessGone;
+  ok(
+    subject instanceof Ci.nsIPropertyBag2,
+    "Subject needs to be a nsIPropertyBag2 to clean up properly"
+  );
+  is(
+    parseInt(data, 10),
+    utilityPid,
+    `Should match the crashed PID ${utilityPid} with ${data}`
+  );
 });
