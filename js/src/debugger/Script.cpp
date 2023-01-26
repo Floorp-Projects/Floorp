a52d8ea295d7eba3889d1ca1ce6e18cfdc4990cf
Iain Ireland <iireland@mozilla.com>
2023-01-26 19:36:32 +0000

    Bug 1811199: Skip synthetic for-of catch blocks in isInCatchScope r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D167855

--- js/src/debugger/Script.cpp
+++ js/src/debugger/Script.cpp
@@ -2245,15 +2245,25 @@ class DebuggerScript::IsInCatchScopeMatcher {
       return false;
     }
 
+    MOZ_ASSERT(!isInCatch_);
     for (const TryNote& tn : script->trynotes()) {
-      if (tn.start <= offset_ && offset_ < tn.start + tn.length &&
-          tn.kind() == TryNoteKind::Catch) {
+      bool inRange = tn.start <= offset_ && offset_ < tn.start + tn.length;
+      if (inRange && tn.kind() == TryNoteKind::Catch) {
         isInCatch_ = true;
+      } else if (isInCatch_) {
+        // For-of loops generate a synthetic catch block to handle
+        // closing the iterator when throwing an exception. The
+        // debugger should ignore these synthetic catch blocks, so
+        // we skip any Catch trynote that is immediately followed
+        // by a ForOf trynote.
+        if (inRange && tn.kind() == TryNoteKind::ForOf) {
+          isInCatch_ = false;
+          continue;
+        }
         return true;
       }
     }
 
-    isInCatch_ = false;
     return true;
   }
   ReturnType match(Handle<WasmInstanceObject*> instance) {
---
+++ js/src/jit-test/tests/debug/onExceptionUnwind-17.js
@@ -0,0 +1,58 @@
+// Test behavior of isInCatchScope on frame from loop iterator
+
+load(libdir + "asserts.js");
+
+var g = newGlobal({newCompartment: true});
+var dbg = Debugger(g);
+var hit = false;
+dbg.onExceptionUnwind = function (frame, exc) {
+    // onExceptionUnwind is called multiple times as the stack is unwound.
+    // Only check the first hit.
+    assertEq(frame instanceof Debugger.Frame, true);
+    assertEq(exc, 16);
+    if (!hit) {
+        hit = true;
+        {
+            assertEq(frame.type, "call");
+            assertEq(frame.callee.name, "foo");
+            assertEq(frame.older.type, "call");
+            const { lineNumber, columnNumber } = frame.script.getOffsetMetadata(frame.offset);
+            assertEq(lineNumber, 3);
+            assertEq(columnNumber, 4);
+
+            const isInCatchScope = frame.script.isInCatchScope(frame.offset);
+            assertEq(isInCatchScope, false);
+        }
+
+        {
+            const { older } = frame;
+            assertEq(older.type, "call");
+            assertEq(older.callee.name, "f");
+            assertEq(older.type, "call");
+            const { lineNumber, columnNumber } = older.script.getOffsetMetadata(older.offset);
+            assertEq(lineNumber, 8);
+            assertEq(columnNumber, 6);
+
+            const isInCatchScope = older.script.isInCatchScope(older.offset);
+            assertEq(isInCatchScope, true);
+        }
+    }
+};
+
+g.eval(
+`function f() {
+  function foo() {
+    throw 16; // <= first frame on exception is here
+  }
+
+  try {
+    for (const _ of [1]) {
+      foo(); // <= second frame, "older" is here
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+`);
+assertThrowsValue(function () { g.eval("f();"); }, 16);
+assertEq(hit, true);
