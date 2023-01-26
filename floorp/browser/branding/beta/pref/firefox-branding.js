4aaa475d1c9254637d38570e627f7a11759459d8
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2023-01-26 16:02:55 +0900

    [FB] branding (beta) | Correct incorrect syntax.

--- floorp/browser/branding/beta/pref/firefox-branding.js
+++ floorp/browser/branding/beta/pref/firefox-branding.js
@@ -16,17 +16,10 @@ pref("app.update.promptWaitTime", 691200);
 // app.update.url.details: a default value for the "More information about this
 // update" link supplied in the "An update is available" page of the update
 // wizard.
-#if MOZ_UPDATE_CHANNEL == beta
-  pref("app.update.url.manual", "https://floorp.ablaze.one");
-  pref("app.update.url.details", "https://blog.ablaze.one/category/ablaze/ablaze-project/floorp-beta/");
-  pref("app.releaseNotesURL", "https://blog.ablaze.one/category/ablaze/ablaze-project/floorp-beta/");
-  pref("app.releaseNotesURL.aboutDialog", "https://blog.ablaze.one/category/ablaze/ablaze-project/floorp-beta/");
-#else
-  pref("app.update.url.manual", "https://floorp.ablaze.one");
-  pref("app.update.url.details", "https://blog.ablaze.one/category/ablaze/ablaze-project/floorp/");
-  pref("app.releaseNotesURL", "https://blog.ablaze.one/category/ablaze/ablaze-project/floorp/");
-  pref("app.releaseNotesURL.aboutDialog", "https://blog.ablaze.one/category/ablaze/ablaze-project/floorp/");
-#endif
+pref("app.update.url.manual", "https://floorp.ablaze.one");
+pref("app.update.url.details", "https://blog.ablaze.one/category/ablaze/ablaze-project/floorp/");
+pref("app.releaseNotesURL", "https://blog.ablaze.one/category/ablaze/ablaze-project/floorp/");
+pref("app.releaseNotesURL.aboutDialog", "https://blog.ablaze.one/category/ablaze/ablaze-project/floorp/");
 
 // The number of days a binary is permitted to be old
 // without checking for an update.  This assumes that
