d2f89584f38ffce9dfd4124bf876c1e95490e9ea
Alexandre Poirot <poirot.alex@gmail.com>
2023-01-26 16:32:57 +0000

    Bug 1811230 - [devtools] Re-remove extensions.unifiedExtensions.enabled which was reintroduced by mistake. r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D167951

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -70,9 +70,6 @@ pref("extensions.langpacks.signatures.required", true);
 pref("xpinstall.signatures.required", true);
 pref("xpinstall.signatures.devInfoURL", "https://wiki.mozilla.org/Addons/Extension_Signing");
 
-// Enable the unified extensions UI by default.
-pref("extensions.unifiedExtensions.enabled", true);
-
 // Dictionary download preference
 pref("browser.dictionaries.download.url", "https://addons.mozilla.org/%LOCALE%/firefox/language-tools/");
 
