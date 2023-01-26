599fee960963f349a4ec066026643ff8442d87d4
Csoregi Natalia <ncsoregi@mozilla.com>
2023-01-26 19:21:30 +0200

    Backed out changeset 13175567352f (bug 1811953) for failures on browser_screenshots_drag_scroll_test.js. CLOSED TREE

--- browser/components/screenshots/tests/browser/head.js
+++ browser/components/screenshots/tests/browser/head.js
@@ -202,15 +202,6 @@ class ScreenshotsHelper {
     });
   }
 
-  getWindowPosition() {
-    return ContentTask.spawn(this.browser, [], () => {
-      return {
-        scrollX: content.window.scrollX,
-        scrollY: content.window.scrollY,
-      };
-    });
-  }
-
   clickDownloadButton() {
     mouse.click(this.endX - 60, this.endY + 30);
   }
