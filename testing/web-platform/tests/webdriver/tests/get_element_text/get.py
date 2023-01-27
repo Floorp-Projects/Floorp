e8ce1c1638dc10bd597e19b4754bc6a913212888
Henrik Skupin <mail@hskupin.info>
2023-01-27 10:31:49 +0000

    Bug 1811545 - [wdspec] Enhance stale_element fixture for shadow root nodes. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D167542

--- testing/web-platform/tests/webdriver/tests/get_element_text/get.py
+++ testing/web-platform/tests/webdriver/tests/get_element_text/get.py
@@ -71,7 +71,7 @@ def test_no_such_element_from_other_frame(session, url, closed):
 
 @pytest.mark.parametrize("as_frame", [False, True], ids=["top_context", "child_context"])
 def test_stale_element_reference(session, stale_element, as_frame):
-    element = stale_element("<input>", "input", as_frame=as_frame)
+    element = stale_element("input#text", as_frame=as_frame)
 
     response = get_element_text(session, element.id)
     assert_error(response, "stale element reference")
