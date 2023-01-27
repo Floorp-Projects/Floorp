d244c7915717eccf4e2578edff0ff71384253279
Henrik Skupin <mail@hskupin.info>
2023-01-27 10:31:49 +0000

    Bug 1811545 - [wdspec] Update test_no_such_element_from_other_frame tests for get_test_page fixture. r=webdriver-reviewers,Sasha,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D167543

--- testing/web-platform/tests/webdriver/tests/get_element_text/get.py
+++ testing/web-platform/tests/webdriver/tests/get_element_text/get.py
@@ -53,19 +53,20 @@ def test_no_such_element_from_other_window_handle(session, inline, closed):
 
 
 @pytest.mark.parametrize("closed", [False, True], ids=["open", "closed"])
-def test_no_such_element_from_other_frame(session, url, closed):
-    session.url = url("/webdriver/tests/support/html/subframe.html")
+def test_no_such_element_from_other_frame(session, get_test_page, closed):
+    session.url = get_test_page(as_frame=True)
 
-    frame = session.find.css("#delete-frame", all=False)
+    frame = session.find.css("iframe", all=False)
     session.switch_frame(frame)
 
-    button = session.find.css("#remove-parent", all=False)
-    if closed:
-        button.click()
+    element = session.find.css("div", all=False)
 
     session.switch_frame("parent")
 
-    response = get_element_text(session, button.id)
+    if closed:
+        session.execute_script("arguments[0].remove();", args=[frame])
+
+    response = get_element_text(session, element.id)
     assert_error(response, "no such element")
 
 
