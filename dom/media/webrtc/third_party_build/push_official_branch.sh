c000bd99d9973d78474e1d997b31e333a2e9b6fb
Michael Froman <mfroman@mozilla.com>
2023-01-28 01:32:26 +0000

    Bug 1813249 - pt3 - add script to push the official branch to moz-libwebrtc. r=dbaker DONTBUILD
    Depends on D168170
    
    Differential Revision: https://phabricator.services.mozilla.com/D168171

---
+++ dom/media/webrtc/third_party_build/push_official_branch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script is a simple helper script that creates a branch name
+# in moz-libwebrtc and pushes it to origin.  You will need permissions
+# to the mozilla fork of libwebrtc in order to complete this operation.
+# The repo is: https://github.com/mozilla/libwebrtc/
+#
+# Note: this should only be run after elm has been merged to mozilla-central
+
+function show_error_msg()
+{
+  echo "*** ERROR *** $? line $1 $0 did not complete successfully!"
+  echo "$ERROR_HELP"
+}
+
+# Print an Error message if `set -eE` causes the script to exit due to a failed command
+trap 'show_error_msg $LINENO' ERR
+
+source dom/media/webrtc/third_party_build/use_config_env.sh
+
+ERROR_HELP=""
+
+# After this point:
+# * eE: All commands should succeed.
+# * o pipefail: All stages of all pipes should succeed.
+set -eEo pipefail
+
+echo "Creating branch '$MOZ_LIBWEBRTC_OFFICIAL_BRANCH'"
+git branch $MOZ_LIBWEBRTC_OFFICIAL_BRANCH
+echo "Pushing the branch to https://github.com/mozilla/libwebrtc"
+git push origin $MOZ_LIBWEBRTC_OFFICIAL_BRANCH
+
+echo "Please add this new branch information to Bug $MOZ_FASTFORWARD_BUG"
+echo "https://github.com/mozilla/libwebrtc/tree/$MOZ_LIBWEBRTC_OFFICIAL_BRANCH"
