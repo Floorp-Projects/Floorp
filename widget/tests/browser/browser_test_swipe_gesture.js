be0f5469b1bbb0a3d18a36f02b9979e496d9a242
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2023-01-31 21:33:43 +0000

    Bug 1784772 - Forcibly trigger transition and await either transitionstart or transitioncancel event. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D168311

--- widget/tests/browser/browser_test_swipe_gesture.js
+++ widget/tests/browser/browser_test_swipe_gesture.js
@@ -578,7 +578,21 @@ add_task(async () => {
   await panLeftToRightBegin(tab.linkedBrowser, 100, 100, 100);
 
   ok(gHistorySwipeAnimation._prevBox != null, "should have prevbox");
-  let transitionPromise = new Promise(resolve => {
+  let transitionCancelPromise = new Promise(resolve => {
+    gHistorySwipeAnimation._prevBox.addEventListener(
+      "transitioncancel",
+      event => {
+        if (
+          event.propertyName == "opacity" &&
+          event.target == gHistorySwipeAnimation._prevBox
+        ) {
+          resolve();
+        }
+      },
+      { once: true }
+    );
+  });
+  let transitionStartPromise = new Promise(resolve => {
     gHistorySwipeAnimation._prevBox.addEventListener(
       "transitionstart",
       event => {
@@ -601,7 +615,7 @@ add_task(async () => {
 
   ok(gBrowser.webNavigation.canGoForward);
 
-  await transitionPromise;
+  await Promise.any([transitionStartPromise, transitionCancelPromise]);
 
   await TestUtils.waitForCondition(() => {
     return (
@@ -623,7 +637,20 @@ add_task(async () => {
   await panRightToLeftBegin(tab.linkedBrowser, 100, 100, 100);
 
   ok(gHistorySwipeAnimation._nextBox != null, "should have nextbox");
-  transitionPromise = new Promise(resolve => {
+  transitionCancelPromise = new Promise(resolve => {
+    gHistorySwipeAnimation._nextBox.addEventListener(
+      "transitioncancel",
+      event => {
+        if (
+          event.propertyName == "opacity" &&
+          event.target == gHistorySwipeAnimation._nextBox
+        ) {
+          resolve();
+        }
+      }
+    );
+  });
+  transitionStartPromise = new Promise(resolve => {
     gHistorySwipeAnimation._nextBox.addEventListener(
       "transitionstart",
       event => {
@@ -645,7 +672,7 @@ add_task(async () => {
 
   ok(gBrowser.webNavigation.canGoBack);
 
-  await transitionPromise;
+  await Promise.any([transitionStartPromise, transitionCancelPromise]);
 
   await TestUtils.waitForCondition(() => {
     return (
