17219d5d22266229eaf6bdccdad5f95240d33bc6
Luca Greco <lgreco@mozilla.com>
2023-01-31 20:42:17 +0000

    Bug 1748313 - ExtensionSetting webidl api bindings r=willdurand,webidl,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D167596

---
+++ toolkit/components/extensions/webidl-api/ExtensionSetting.cpp
@@ -0,0 +1,54 @@
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "ExtensionSetting.h"
+#include "ExtensionEventManager.h"
+
+#include "mozilla/dom/ExtensionSettingBinding.h"
+#include "nsIGlobalObject.h"
+
+namespace mozilla::extensions {
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(ExtensionSetting);
+NS_IMPL_CYCLE_COLLECTING_RELEASE(ExtensionSetting)
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(ExtensionSetting, mGlobal,
+                                      mExtensionBrowser, mOnChangeEventMgr);
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(ExtensionSetting)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_END
+
+ExtensionSetting::ExtensionSetting(nsIGlobalObject* aGlobal,
+                                   ExtensionBrowser* aExtensionBrowser,
+                                   const nsAString& aNamespace)
+    : mGlobal(aGlobal),
+      mExtensionBrowser(aExtensionBrowser),
+      mAPINamespace(aNamespace) {
+  MOZ_DIAGNOSTIC_ASSERT(mGlobal);
+  MOZ_DIAGNOSTIC_ASSERT(mExtensionBrowser);
+}
+
+/* static */
+bool ExtensionSetting::IsAllowed(JSContext* aCx, JSObject* aGlobal) {
+  return true;
+}
+
+JSObject* ExtensionSetting::WrapObject(JSContext* aCx,
+                                       JS::Handle<JSObject*> aGivenProto) {
+  return dom::ExtensionSetting_Binding::Wrap(aCx, this, aGivenProto);
+}
+
+nsIGlobalObject* ExtensionSetting::GetParentObject() const { return mGlobal; }
+
+ExtensionEventManager* ExtensionSetting::OnChange() {
+  if (!mOnChangeEventMgr) {
+    mOnChangeEventMgr = CreateEventManager(u"onChange"_ns);
+  }
+
+  return mOnChangeEventMgr;
+}
+
+}  // namespace mozilla::extensions
