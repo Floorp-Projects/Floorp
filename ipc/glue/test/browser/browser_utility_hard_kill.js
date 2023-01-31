511a568bfe36f821bfe11df48dafb4f53a0da550
David Parks <daparks@mozilla.com>
2023-01-31 21:10:28 +0000

    Bug 1704500: Add utility process test helpers r=gerard-majax
    Also makes the existing utility process test functions a bit more general.
    
    Depends on D162943
    
    Differential Revision: https://phabricator.services.mozilla.com/D162944

--- ipc/glue/test/browser/browser_utility_hard_kill.js
+++ ipc/glue/test/browser/browser_utility_hard_kill.js
@@ -4,27 +4,10 @@
 "use strict";
 
 add_task(async () => {
-  const utilityPid = await startUtilityProcess();
+  await startUtilityProcess(["unknown"]);
 
   SimpleTest.expectChildProcessCrash();
 
-  const utilityProcessGone = TestUtils.topicObserved("ipc:utility-shutdown");
-
   info("Hard kill Utility Process");
-  const ProcessTools = Cc["@mozilla.org/processtools-service;1"].getService(
-    Ci.nsIProcessToolsService
-  );
-  ProcessTools.kill(utilityPid);
-
-  info(`Waiting for utility process ${utilityPid} to go away.`);
-  let [subject, data] = await utilityProcessGone;
-  ok(
-    subject instanceof Ci.nsIPropertyBag2,
-    "Subject needs to be a nsIPropertyBag2 to clean up properly"
-  );
-  is(
-    parseInt(data, 10),
-    utilityPid,
-    `Should match the crashed PID ${utilityPid} with ${data}`
-  );
+  await cleanUtilityProcessShutdown("unknown", true /* preferKill */);
 });
