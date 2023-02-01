5c4b357b0c432913d187bc07604f85dc88bbd9e0
Mike Hommey <mh+mozilla@glandium.org>
2023-02-01 10:59:37 +0000

    Bug 1812974 - Stop using cctools when building pkgconf. r=firefox-build-system-reviewers,ahochheiden
    Differential Revision: https://phabricator.services.mozilla.com/D168038

--- taskcluster/scripts/misc/build-pkgconf.sh
+++ taskcluster/scripts/misc/build-pkgconf.sh
@@ -6,32 +6,33 @@ PROJECT=pkgconf
 
 cd ${MOZ_FETCHES_DIR}/${PROJECT}
 
+export PATH="$MOZ_FETCHES_DIR/clang/bin:$PATH"
+
 case "$1" in
 x86_64-unknown-linux-gnu)
-    ./configure --disable-shared CC="$MOZ_FETCHES_DIR/clang/bin/clang --sysroot=$MOZ_FETCHES_DIR/sysroot-x86_64-linux-gnu"
+    CC="clang --sysroot=$MOZ_FETCHES_DIR/sysroot-x86_64-linux-gnu"
     EXE=
     ;;
 x86_64-apple-darwin)
-    export LD_LIBRARY_PATH="$MOZ_FETCHES_DIR/clang/lib"
-    export PATH="$MOZ_FETCHES_DIR/clang/bin:$MOZ_FETCHES_DIR/cctools/bin:$PATH"
     export MACOSX_DEPLOYMENT_TARGET=10.12
-    ./configure --disable-shared CC="clang --target=x86_64-apple-darwin -isysroot $MOZ_FETCHES_DIR/MacOSX13.0.sdk" --host=x86_64-apple-darwin
+    TARGET=$1
+    CC="clang --target=$TARGET -isysroot $MOZ_FETCHES_DIR/MacOSX13.0.sdk"
     EXE=
     ;;
 aarch64-apple-darwin)
-    export LD_LIBRARY_PATH="$MOZ_FETCHES_DIR/clang/lib"
-    export PATH="$MOZ_FETCHES_DIR/clang/bin:$MOZ_FETCHES_DIR/cctools/bin:$PATH"
     export MACOSX_DEPLOYMENT_TARGET=11.0
-    ./configure --disable-shared CC="clang --target=aarch64-apple-darwin -isysroot $MOZ_FETCHES_DIR/MacOSX13.0.sdk" --host=aarch64-apple-darwin
+    TARGET=$1
+    CC="clang --target=$TARGET -isysroot $MOZ_FETCHES_DIR/MacOSX13.0.sdk"
     EXE=
     ;;
 x86_64-pc-windows-gnu)
-    export PATH="$MOZ_FETCHES_DIR/clang/bin:$PATH"
-    ./configure --disable-shared CC=x86_64-w64-mingw32-clang AR=llvm-ar RANLIB=llvm-ranlib CPPFLAGS=-DPKGCONFIG_IS_STATIC=1 --host=x86_64-w64-mingw32
+    TARGET=x86_64-w64-mingw32
+    CC="x86_64-w64-mingw32-clang -DPKGCONFIG_IS_STATIC=1"
     EXE=.exe
     ;;
 esac
 
+./configure --disable-shared CC="$CC" AR=llvm-ar RANLIB=llvm-ranlib LDFLAGS=-fuse-ld=lld ${TARGET:+--host=$TARGET}
 make -j$(nproc) V=1
 
 mv ${PROJECT}${EXE} ${PROJECT}_tmp
