0f9c5dc6eb0b80ef700d502ba4f54c2eaefba92f
Dan Baker <dbaker@mozilla.com>
2023-02-01 16:27:05 -0700

    Bug 1812154 - Vendor libwebrtc from fb409c975f
    Upstream commit: https://webrtc.googlesource.com/src/+/fb409c975f770d2de9e019a7087163ad31d768ed
        Do not use gtest_parallell for fuchsia
    
        Bug: b/232740856
        Change-Id: If1191c1e90b7eb18acd8e393f3927ddee4b92b34
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/277181
        Reviewed-by: Jeremy Leconte <jleconte@google.com>
        Commit-Queue: Christoffer Jansson <jansson@google.com>
        Cr-Commit-Position: refs/heads/main@{#38234}

--- third_party/libwebrtc/tools_webrtc/mb/mb.py
+++ third_party/libwebrtc/tools_webrtc/mb/mb.py
@@ -52,6 +52,7 @@ class WebRTCMetaBuildWrapper(mb.MetaBuildWrapper):
     test_type = isolate_map[target]['type']
 
     is_android = 'target_os="android"' in vals['gn_args']
+    is_fuchsia = 'target_os="fuchsia"' in vals['gn_args']
     is_linux = self.platform.startswith('linux') and not is_android
     is_ios = 'target_os="ios"' in vals['gn_args']
 
@@ -85,6 +86,8 @@ class WebRTCMetaBuildWrapper(mb.MetaBuildWrapper):
           '--logcat-output-file', '${ISOLATED_OUTDIR}/logcats',
           '--store-tombstones'
       ]
+    elif is_fuchsia:
+      cmdline += ['bin/run_%s' % target]
     elif is_ios:
       cmdline += [
           vpython_exe, '../../tools_webrtc/flags_compatibility.py',
