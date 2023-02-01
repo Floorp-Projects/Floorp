7ef5f4f8ba0d2270f9d1266109a35a1d22bf7b57
Mike Hommey <mh+mozilla@glandium.org>
2023-02-01 15:09:40 +0000

    Bug 1812974 - Stop using cctools when building gn. r=firefox-build-system-reviewers,ahochheiden
    The hardcoded use of libtool was removed upstream in 2019, replaced with
    the use of AR, so we can drop the related hack.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168036

--- taskcluster/scripts/misc/build-gn-macosx.sh
+++ taskcluster/scripts/misc/build-gn-macosx.sh
@@ -5,25 +5,19 @@ set -e -v
 
 WORKSPACE=$HOME/workspace
 
-CROSS_CCTOOLS_PATH=$MOZ_FETCHES_DIR/cctools
 CROSS_SYSROOT=$MOZ_FETCHES_DIR/MacOSX13.0.sdk
 export MACOSX_DEPLOYMENT_TARGET=10.12
 
 export CC=$MOZ_FETCHES_DIR/clang/bin/clang
 export CXX=$MOZ_FETCHES_DIR/clang/bin/clang++
 export AR=$MOZ_FETCHES_DIR/clang/bin/llvm-ar
-export CFLAGS="-target x86_64-apple-darwin -mlinker-version=137 -B ${CROSS_CCTOOLS_PATH}/bin -isysroot ${CROSS_SYSROOT} -I${CROSS_SYSROOT}/usr/include -iframework ${CROSS_SYSROOT}/System/Library/Frameworks"
+export CFLAGS="-target x86_64-apple-darwin -isysroot ${CROSS_SYSROOT} -I${CROSS_SYSROOT}/usr/include -iframework ${CROSS_SYSROOT}/System/Library/Frameworks"
 export CXXFLAGS="-stdlib=libc++ ${CFLAGS}"
-export LDFLAGS="${CXXFLAGS} -Wl,-syslibroot,${CROSS_SYSROOT} -Wl,-dead_strip"
+export LDFLAGS="-fuse-ld=lld ${CXXFLAGS} -Wl,-syslibroot,${CROSS_SYSROOT} -Wl,-dead_strip"
 
 # We patch tools/gn/bootstrap/bootstrap.py to detect this.
 export MAC_CROSS=1
 
 cd $GECKO_PATH
-
-# The ninja templates used to bootstrap gn have hard-coded references to
-# 'libtool', make sure we find the right one.
-ln -s $CROSS_CCTOOLS_PATH/bin/x86_64-apple-darwin-libtool $CROSS_CCTOOLS_PATH/bin/libtool
-export PATH=$CROSS_CCTOOLS_PATH/bin:$PATH
 
 . taskcluster/scripts/misc/build-gn-common.sh
