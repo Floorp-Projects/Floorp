a44e4c2c4ddc08b9f1181213c4467b4537907aaa
Dan Baker <dbaker@mozilla.com>
2023-02-01 16:23:12 -0700

    Bug 1812154 - Vendor libwebrtc from 7cd7bbe0d2
    Upstream commit: https://webrtc.googlesource.com/src/+/7cd7bbe0d28d28c31ba630d545ce998ffe14bbf2
        Move VideoEncoderConfig from api/ into video/config
    
        This cl is a preparation for https://webrtc-review.googlesource.com/c/src/+/277002
        so that downstream tests can be modified first.
    
        Bug: webrtc:14451
        Change-Id: I03b613db695c533bd77ab4bd2f8b35677094443c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/277003
        Commit-Queue: Jonas Oreland <jonaso@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Tomas Gunnarsson <tommi@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#38231}

---
+++ third_party/libwebrtc/video/config/video_encoder_config.h
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (c) 2022 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef VIDEO_CONFIG_VIDEO_ENCODER_CONFIG_H_
+#define VIDEO_CONFIG_VIDEO_ENCODER_CONFIG_H_
+
+#include "api/video_codecs/video_encoder_config.h"
+
+#endif  // VIDEO_CONFIG_VIDEO_ENCODER_CONFIG_H_
