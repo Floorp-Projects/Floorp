60b978d0e154ee7748633fe0d6fd59f2c0f4cf05
Dan Baker <dbaker@mozilla.com>
2023-02-01 14:59:14 -0700

    Bug 1812154 - Vendor libwebrtc from 0c01606ab2
    Upstream commit: https://webrtc.googlesource.com/src/+/0c01606ab2ac2f4869a4fd26da216ea301a4884a
        Add CapturerID for X11 and Wayland for telemetry
    
        Chrome Remote Desktop will support both X11 and Wayland desktop
        capturers in the near future and we'd like to differentiate between
        the two in our video frame stats and telemetry.  I beleive other
        products are in a similar position so I would like to add a capturer
        ID to the frames generated by the capturer classes.
    
        Bug: chromium:1366062
        Change-Id: If27c35ad6ef89b6396120982edc4dd0cf2a1e51c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/276081
        Commit-Queue: Joe Downing <joedow@google.com>
        Reviewed-by: Alexander Cooper <alcooper@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#38185}

--- third_party/libwebrtc/modules/desktop_capture/linux/wayland/base_capturer_pipewire.cc
+++ third_party/libwebrtc/modules/desktop_capture/linux/wayland/base_capturer_pipewire.cc
@@ -138,6 +138,7 @@ void BaseCapturerPipeWire::CaptureFrame() {
   // TODO(julien.isorce): http://crbug.com/945468. Set the icc profile on
   // the frame, see ScreenCapturerX11::CaptureFrame.
 
+  frame->set_capturer_id(DesktopCapturerId::kWaylandCapturerLinux);
   callback_->OnCaptureResult(Result::SUCCESS, std::move(frame));
 }
 
