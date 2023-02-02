5a855f774ebe5c673e7bdd9a49f14ce5fa6cd9ce
Dan Baker <dbaker@mozilla.com>
2023-02-02 08:52:14 -0700

    Bug 1812154 - Vendor libwebrtc from 0c4563c0c4
    Upstream commit: https://webrtc.googlesource.com/src/+/0c4563c0c4ee24affaaad5d06c403921f4ba01e3
        Remove the libaom av1 decoder.
    
        Bug: webrtc:14267
        Change-Id: I95a416b25fa20d4dea6896e05beb59789621f1fa
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/268305
        Reviewed-by: Sergey Silkin <ssilkin@webrtc.org>
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#38253}

--- third_party/libwebrtc/media/engine/internal_decoder_factory.cc
+++ third_party/libwebrtc/media/engine/internal_decoder_factory.cc
@@ -16,7 +16,6 @@
 #include "api/video_codecs/video_codec.h"
 #include "media/base/codec.h"
 #include "media/base/media_constants.h"
-#include "modules/video_coding/codecs/av1/libaom_av1_decoder.h"
 #include "modules/video_coding/codecs/h264/include/h264.h"
 #include "modules/video_coding/codecs/vp8/include/vp8.h"
 #include "modules/video_coding/codecs/vp9/include/vp9.h"
@@ -51,12 +50,8 @@ std::vector<SdpVideoFormat> InternalDecoderFactory::GetSupportedFormats()
   for (const SdpVideoFormat& h264_format : SupportedH264DecoderCodecs())
     formats.push_back(h264_format);
 
-//  bool isDav1dEnabled =
-//      kDav1dIsIncluded && !field_trial::IsDisabled(kDav1dFieldTrial);
-//  if (kIsLibaomAv1DecoderSupported || isDav1dEnabled) {
+//  if (kDav1dIsIncluded && !field_trial::IsDisabled(kDav1dFieldTrial)) {
 //    formats.push_back(SdpVideoFormat(cricket::kAv1CodecName));
-//  }
-//  if (isDav1dEnabled) {
 //    formats.push_back(SdpVideoFormat(
 //        cricket::kAv1CodecName,
 //        {{kAV1FmtpProfile, AV1ProfileToString(AV1Profile::kProfile1).data()}}));
@@ -102,11 +97,6 @@ std::unique_ptr<VideoDecoder> InternalDecoderFactory::CreateVideoDecoder(
       kDav1dIsIncluded && !field_trial::IsDisabled(kDav1dFieldTrial)) {
     return CreateDav1dDecoder();
   }
-
-  //if (absl::EqualsIgnoreCase(format.name, cricket::kAv1CodecName) &&
-  //    kIsLibaomAv1DecoderSupported) {
-  //  return CreateLibaomAv1Decoder();
-  //}
 
   RTC_DCHECK_NOTREACHED();
   return nullptr;
