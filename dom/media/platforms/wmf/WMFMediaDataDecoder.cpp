d122f63eb6eb84a68f80eceef9540337a2c51f38
alwu <alwu@mozilla.com>
2023-02-02 00:01:09 +0000

    Bug 1811901 - part3 : fix build error for unified build. r=jolin
    Differential Revision: https://phabricator.services.mozilla.com/D168014

--- dom/media/platforms/wmf/WMFMediaDataDecoder.cpp
+++ dom/media/platforms/wmf/WMFMediaDataDecoder.cpp
@@ -13,6 +13,7 @@
 #include "mozilla/SyncRunnable.h"
 #include "mozilla/TaskQueue.h"
 #include "mozilla/Telemetry.h"
+#include "mozilla/WindowsVersion.h"
 #include "nsTArray.h"
 
 #define LOG(...) MOZ_LOG(sPDMLog, mozilla::LogLevel::Debug, (__VA_ARGS__))
