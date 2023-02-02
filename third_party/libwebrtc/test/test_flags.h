d96848a98646710009765ed3b3346a442b797c3b
Dan Baker <dbaker@mozilla.com>
2023-02-02 10:39:07 -0700

    Bug 1812154 - Vendor libwebrtc from 9a92b8a546
    Upstream commit: https://webrtc.googlesource.com/src/+/9a92b8a546b45929c7cc0ed5f11486472365d4ef
        Add flag to export in new MetricSet proto format
    
        Bug: b/246095034
        Change-Id: I7e879ab9d47267788793a65a75fba401bf9aa38a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/277561
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Commit-Queue: Artem Titov <titovartem@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#38259}

--- third_party/libwebrtc/test/test_flags.h
+++ third_party/libwebrtc/test/test_flags.h
@@ -18,5 +18,6 @@
 ABSL_DECLARE_FLAG(std::string, force_fieldtrials);
 ABSL_DECLARE_FLAG(std::vector<std::string>, plot);
 ABSL_DECLARE_FLAG(std::string, isolated_script_test_perf_output);
+ABSL_DECLARE_FLAG(std::string, webrtc_test_metrics_output_path);
 
 #endif  // TEST_TEST_FLAGS_H_
