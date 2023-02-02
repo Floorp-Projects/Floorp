e42c7877fe17f799415b20b497df922e4bea06c4
Gregory Pappas <gp3033@protonmail.com>
2023-02-02 21:47:27 +0000

    Bug 1811274 - Add disposition property to browser.search.search r=robwu
    Depends on D164700
    
    Differential Revision: https://phabricator.services.mozilla.com/D168206

--- browser/components/extensions/test/browser/browser_ext_search.js
+++ browser/components/extensions/test/browser/browser_ext_search.js
@@ -7,7 +7,7 @@ const { AddonTestUtils } = ChromeUtils.import(
 );
 
 const SEARCH_TERM = "test";
-const SEARCH_URL = "https://localhost/?q={searchTerms}";
+const SEARCH_URL = "https://example.org/?q={searchTerms}";
 
 AddonTestUtils.initMochitest(this);
 
@@ -183,4 +183,169 @@ add_task(async function test_search_default_engine() {
     EXPECTED_ORIGIN,
     "Search request's should be associated with the originating extension."
   );
+});
+
+add_task(async function test_search_disposition() {
+  async function background() {
+    let resolvers = {};
+
+    function tabListener(tabId, changeInfo, tab) {
+      if (tab.url == "about:blank") {
+        // Ignore events related to the initial tab open.
+        return;
+      }
+
+      if (changeInfo.status === "complete") {
+        let query = new URL(tab.url).searchParams.get("q");
+        let resolver = resolvers[query];
+        browser.test.assertTrue(resolver, `Found resolver for ${tab.url}`);
+        browser.test.assertTrue(
+          resolver.resolve,
+          `${query} was not resolved yet`
+        );
+        resolver.resolve({
+          tabId,
+          windowId: tab.windowId,
+        });
+        resolver.resolve = null; // resolve can be used only once.
+      }
+    }
+    browser.tabs.onUpdated.addListener(tabListener);
+
+    async function awaitSearchResult(args) {
+      resolvers[args.query] = {};
+      resolvers[args.query].promise = new Promise(
+        _resolve => (resolvers[args.query].resolve = _resolve)
+      );
+      await browser.search.search({ ...args, engine: "Search Test" });
+      let searchResult = await resolvers[args.query].promise;
+      return searchResult;
+    }
+
+    const firstTab = await browser.tabs.create({
+      active: true,
+      url: "about:blank",
+    });
+
+    // Search in new tab (testing default disposition)
+    let result = await awaitSearchResult({
+      query: "DefaultDisposition",
+    });
+    browser.test.assertFalse(
+      result.tabId === firstTab.id,
+      "Query ran in new tab"
+    );
+    browser.test.assertEq(
+      result.windowId,
+      firstTab.windowId,
+      "Query ran in current window"
+    );
+    await browser.tabs.remove(result.tabId); // Cleanup
+
+    // Search in new tab
+    result = await awaitSearchResult({
+      query: "NewTab",
+      disposition: "NEW_TAB",
+    });
+    browser.test.assertFalse(
+      result.tabId === firstTab.id,
+      "Query ran in new tab"
+    );
+    browser.test.assertEq(
+      result.windowId,
+      firstTab.windowId,
+      "Query ran in current window"
+    );
+    await browser.tabs.remove(result.tabId); // Cleanup
+
+    // Search in current tab
+    result = await awaitSearchResult({
+      query: "CurrentTab",
+      disposition: "CURRENT_TAB",
+    });
+    browser.test.assertDeepEq(
+      {
+        tabId: firstTab.id,
+        windowId: firstTab.windowId,
+      },
+      result,
+      "Query ran in current tab in current window"
+    );
+
+    // Search in a specific tab
+    let newTab = await browser.tabs.create({
+      active: false,
+      url: "about:blank",
+    });
+    result = await awaitSearchResult({
+      query: "SpecificTab",
+      tabId: newTab.id,
+    });
+    browser.test.assertDeepEq(
+      {
+        tabId: newTab.id,
+        windowId: firstTab.windowId,
+      },
+      result,
+      "Query ran in specific tab in current window"
+    );
+    await browser.tabs.remove(newTab.id); // Cleanup
+
+    // Search in a new window
+    result = await awaitSearchResult({
+      query: "NewWindow",
+      disposition: "NEW_WINDOW",
+    });
+    browser.test.assertFalse(
+      result.windowId === firstTab.windowId,
+      "Query ran in new window"
+    );
+    await browser.windows.remove(result.windowId); // Cleanup
+    await browser.tabs.remove(firstTab.id); // Cleanup
+
+    // Make sure tabId and disposition can't be used together
+    await browser.test.assertRejects(
+      browser.search.search({
+        query: " ",
+        tabId: 1,
+        disposition: "NEW_WINDOW",
+      }),
+      "Cannot set both 'disposition' and 'tabId'",
+      "Should not be able to set both tabId and disposition"
+    );
+
+    // Make sure we reject if an invalid tabId is used
+    await browser.test.assertRejects(
+      browser.search.search({
+        query: " ",
+        tabId: Number.MAX_SAFE_INTEGER,
+      }),
+      /Invalid tab ID/,
+      "Should not be able to set an invalid tabId"
+    );
+
+    browser.test.notifyPass("disposition");
+  }
+  let searchExtension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      chrome_settings_overrides: {
+        search_provider: {
+          name: "Search Test",
+          search_url: "https://example.org/?q={searchTerms}",
+        },
+      },
+    },
+    useAddonManager: "temporary",
+  });
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      permissions: ["search", "tabs"],
+    },
+    background,
+  });
+  await searchExtension.startup();
+  await extension.startup();
+  await extension.awaitFinish("disposition");
+  await extension.unload();
+  await searchExtension.unload();
 });
