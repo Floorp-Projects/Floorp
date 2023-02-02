074eb93c716128f0efa3d4968212744a042d2f58
alwu <alwu@mozilla.com>
2023-02-02 00:01:09 +0000

    Bug 1811901 - part1 : implement conditional forward method on MediaEventForwarder. r=jolin
    Differential Revision: https://phabricator.services.mozilla.com/D168013

--- dom/media/MediaEventSource.h
+++ dom/media/MediaEventSource.h
@@ -564,6 +564,19 @@ class MediaEventForwarder : public MediaEventSource<Es...> {
         }));
   }
 
+  template <typename Function>
+  void ForwardIf(MediaEventSource<Es...>& aSource, Function&& aFunction) {
+    // Forwarding a rawptr `this` here is fine, since DisconnectAll disconnect
+    // all mListeners synchronously and prevents this handler from running.
+    mListeners.AppendElement(aSource.Connect(
+        mEventTarget, [this, func = aFunction](ArgType<Es>&&... aEvents) {
+          if (!func()) {
+            return;
+          }
+          this->NotifyInternal(std::forward<ArgType<Es>...>(aEvents)...);
+        }));
+  }
+
   void DisconnectAll() {
     for (auto& l : mListeners) {
       l.Disconnect();
