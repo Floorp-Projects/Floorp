304c4fef4c64db14925ca85d5afdaafb6014e684
Mike Conley <mconley@mozilla.com>
2023-02-03 21:36:35 +0000

    Bug 1814202 - Make browser_contextmenu_badiframe.js more reliable. r=NeilDeakin
    The test had a race condition in it where contextmenu events had a chance of
    not targeting the right document if a subframe hadn't loaded and presented
    in time. This patch uses BrowserTestUtils.browserLoaded and requestAnimationFrame
    to help ensure that the subframes have both loaded and presented.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168802

--- browser/base/content/test/contextMenu/browser_contextmenu_badiframe.js
+++ browser/base/content/test/contextMenu/browser_contextmenu_badiframe.js
@@ -14,34 +14,44 @@ var testPage =
   '"></frameset>';
 
 async function openTestPage() {
-  // Waiting for the error page to load in the subframe
-  await BrowserTestUtils.openNewForegroundTab(gBrowser, testPage, true, true);
-  await BrowserTestUtils.waitForCondition(() => {
-    return (
-      gBrowser.selectedBrowser.browsingContext.children[1].currentWindowGlobal
-        .documentURI.spec != "about:blank"
-    );
-  });
-
-  await SpecialPowers.spawn(
-    gBrowser.selectedBrowser.browsingContext.children[1],
-    [],
-    () => {
-      if (!content.document.body.classList.length) {
-        return new Promise(resolve => {
-          content.addEventListener(
-            "AboutNetErrorLoad",
-            () => {
-              resolve();
-            },
-            { once: true }
-          );
-        });
-      }
-
-      return undefined;
-    }
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    "about:blank",
+    true,
+    true
   );
+  let browser = tab.linkedBrowser;
+
+  // The test page has a top-level document and two subframes. One of
+  // those subframes is an error page, which doesn't fire a load event.
+  // We'll use BrowserTestUtils.browserLoaded and have it wait for all
+  // 3 loads before resolving.
+  let expectedLoads = 3;
+  let pageAndIframesLoaded = BrowserTestUtils.browserLoaded(
+    browser,
+    true /* includeSubFrames */,
+    url => {
+      expectedLoads--;
+      return !expectedLoads;
+    },
+    true /* maybeErrorPage */
+  );
+  BrowserTestUtils.loadURIString(browser, testPage);
+  await pageAndIframesLoaded;
+
+  // Make sure both the top-level document and the iframe documents have
+  // had a chance to present. We need this so that the context menu event
+  // gets dispatched properly.
+  for (let bc of [
+    ...browser.browsingContext.children,
+    browser.browsingContext,
+  ]) {
+    await SpecialPowers.spawn(bc, [], async function() {
+      await new Promise(resolve => {
+        content.requestAnimationFrame(resolve);
+      });
+    });
+  }
 }
 
 async function selectFromFrameMenu(frameNumber, menuId) {
@@ -74,7 +84,17 @@ async function selectFromFrameMenu(frameNumber, menuId) {
   frameItem.openMenu(true);
   await subPopupShownPromise;
 
+  let subPopupHiddenPromise = BrowserTestUtils.waitForEvent(
+    framePopup,
+    "popuphidden"
+  );
+  let contextMenuHiddenPromise = BrowserTestUtils.waitForEvent(
+    contextMenu,
+    "popuphidden"
+  );
   contextMenu.activateItem(document.getElementById(menuId));
+  await subPopupHiddenPromise;
+  await contextMenuHiddenPromise;
 }
 
 add_task(async function testOpenFrame() {
@@ -113,7 +133,11 @@ add_task(async function testOpenFrameInTab() {
 
     info("open frame in tab for " + expectedResultURI);
 
-    let newTabPromise = BrowserTestUtils.waitForNewTab(gBrowser, null, false);
+    let newTabPromise = BrowserTestUtils.waitForNewTab(
+      gBrowser,
+      expectedResultURI,
+      false
+    );
     await selectFromFrameMenu(frameNumber, "context-openframeintab");
     let newTab = await newTabPromise;
 
