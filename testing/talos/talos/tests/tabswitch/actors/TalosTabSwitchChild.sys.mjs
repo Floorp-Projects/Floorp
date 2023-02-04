c280c3dbc24485c2ca414a6e612c2e23eff0581e
Neil Deakin <neil@mozilla.com>
2023-02-04 14:36:48 +0000

    Bug 1812590, revert back to using a process script for setting up the about:tabswitch module as on some configurations it isn't set up before the page needs to create the about tabswitch channel, r=mconley,perftest-reviewers,kshampur
    Differential Revision: https://phabricator.services.mozilla.com/D168739

--- testing/talos/talos/tests/tabswitch/actors/TalosTabSwitchChild.sys.mjs
+++ testing/talos/talos/tests/tabswitch/actors/TalosTabSwitchChild.sys.mjs
@@ -2,23 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { ComponentUtils } = ChromeUtils.import(
-  "resource://gre/modules/ComponentUtils.jsm"
-);
-
 const { RemotePageChild } = ChromeUtils.import(
   "resource://gre/actors/RemotePageChild.jsm"
 );
-
-const WEBEXTENSION_ID = "tabswitch-talos@mozilla.org";
-const ABOUT_PAGE_NAME = "tabswitch";
-const Registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
-const UUID = "0f459ab4-b4ba-4741-ac89-ee47dea07adb";
-const ABOUT_PATH_PATH = "content/test.html";
-
-const { WebExtensionPolicy } = Cu.getGlobalForObject(Services);
-
-let factory;
 
 export class TalosTabSwitchChild extends RemotePageChild {
   actorCreated() {
@@ -40,49 +26,8 @@ export class TalosTabSwitchChild extends RemotePageChild {
   handleEvent(event) {}
 
   receiveMessage(message) {
-    if (message.name == "Tabswitch:Teardown") {
-      this.teardown();
-    } else if (message.name == "GarbageCollect") {
+    if (message.name == "GarbageCollect") {
       this.contentWindow.windowUtils.garbageCollect();
     }
   }
-
-  teardown() {
-    Registrar.unregisterFactory(Components.ID(UUID), this._factory);
-    factory = null;
-  }
 }
-
-function setupTabSwitch() {
-  let extensionPolicy = WebExtensionPolicy.getByID(WEBEXTENSION_ID);
-  let aboutPageURI = extensionPolicy.getURL(ABOUT_PATH_PATH);
-
-  class TabSwitchAboutModule {
-    constructor() {
-      this.QueryInterface = ChromeUtils.generateQI(["nsIAboutModule"]);
-    }
-    newChannel(aURI, aLoadInfo) {
-      let uri = Services.io.newURI(aboutPageURI);
-      let chan = Services.io.newChannelFromURIWithLoadInfo(uri, aLoadInfo);
-      chan.originalURI = aURI;
-      return chan;
-    }
-    getURIFlags(aURI) {
-      return (
-        Ci.nsIAboutModule.ALLOW_SCRIPT |
-        Ci.nsIAboutModule.URI_MUST_LOAD_IN_CHILD
-      );
-    }
-  }
-
-  factory = ComponentUtils.generateSingletonFactory(TabSwitchAboutModule);
-
-  Registrar.registerFactory(
-    Components.ID(UUID),
-    "",
-    `@mozilla.org/network/protocol/about;1?what=${ABOUT_PAGE_NAME}`,
-    factory
-  );
-}
-
-setupTabSwitch();
