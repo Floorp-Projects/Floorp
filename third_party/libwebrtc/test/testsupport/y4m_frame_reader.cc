c9fcbeb53ab386a7d7fa01af13bc17b553263da8
Dan Baker <dbaker@mozilla.com>
2023-02-04 13:35:08 -0700

    Bug 1812154 - Vendor libwebrtc from b984c07dab
    Upstream commit: https://webrtc.googlesource.com/src/+/b984c07dab529f5bbc52cad18e5c5a9de2278f0a
        Fix y4m frame reader to support any resolution
    
        Bug: b/240540204
        Change-Id: I7069bb0105ea3c6aa66e9d73f5f63ac5ec470733
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/278620
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Commit-Queue: Artem Titov <titovartem@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#38337}

--- third_party/libwebrtc/test/testsupport/y4m_frame_reader.cc
+++ third_party/libwebrtc/test/testsupport/y4m_frame_reader.cc
@@ -15,6 +15,7 @@
 #include "api/scoped_refptr.h"
 #include "api/video/i420_buffer.h"
 #include "rtc_base/logging.h"
+#include "rtc_base/strings/string_builder.h"
 #include "test/testsupport/file_utils.h"
 #include "test/testsupport/frame_reader.h"
 
@@ -22,10 +23,19 @@ namespace webrtc {
 namespace test {
 namespace {
 
-// Size of header: "YUV4MPEG2 W50 H20 F30:1 C420\n"
-const size_t kFileHeaderSize = 29;
+// Size of header: "YUV4MPEG2 WXXXXXX HXXXXXX FXXX:1 C420\n"
+// We allocate up to 6 digits for width and height and up to 3 digits for FPS.
+const size_t kFileHeaderMaxSize = 38;
+// Size of header: "YUV4MPEG2 WX HX FX:1 C420\n"
+const size_t kFileHeaderMinSize = 26;
 // Size of header: "FRAME\n"
 const size_t kFrameHeaderSize = 6;
+
+std::string GetExpectedHeaderPrefix(int width, int height) {
+  rtc::StringBuilder out;
+  out << "YUV4MPEG2 W" << width << " H" << height << " F";
+  return out.str();
+}
 
 }  // namespace
 
@@ -34,7 +44,7 @@ Y4mFrameReaderImpl::Y4mFrameReaderImpl(std::string input_filename,
                                        int height)
     : YuvFrameReaderImpl(input_filename, width, height) {
   frame_length_in_bytes_ += kFrameHeaderSize;
-  buffer_ = new uint8_t[kFileHeaderSize];
+  buffer_ = new char[kFileHeaderMaxSize];
 }
 Y4mFrameReaderImpl::~Y4mFrameReaderImpl() {
   delete[] buffer_;
@@ -48,28 +58,35 @@ bool Y4mFrameReaderImpl::Init() {
   }
   input_file_ = fopen(input_filename_.c_str(), "rb");
   if (input_file_ == nullptr) {
-    RTC_LOG(LS_ERROR) << "Couldn't open input file: "
-                      << input_filename_.c_str();
+    RTC_LOG(LS_ERROR) << "Couldn't open input file: " << input_filename_;
     return false;
   }
   size_t source_file_size = GetFileSize(input_filename_);
   if (source_file_size <= 0u) {
-    RTC_LOG(LS_ERROR) << "Input file " << input_filename_.c_str()
-                      << " is empty.";
+    RTC_LOG(LS_ERROR) << "Input file " << input_filename_ << " is empty.";
     return false;
   }
-  if (fread(buffer_, 1, kFileHeaderSize, input_file_) < kFileHeaderSize) {
+  char* c_file_header = fgets(buffer_, kFileHeaderMaxSize, input_file_);
+  std::string file_header(c_file_header);
+  if (file_header.size() < kFileHeaderMinSize) {
     RTC_LOG(LS_ERROR) << "Couldn't read Y4M header from input file: "
-                      << input_filename_.c_str();
+                      << input_filename_;
+    return false;
+  }
+  if (file_header.find(GetExpectedHeaderPrefix(input_width_, input_height_)) !=
+      0) {
+    RTC_LOG(LS_ERROR) << "Couldn't read Y4M file: " << input_filename_
+                      << ". Input file has different resolution, expected: "
+                      << GetExpectedHeaderPrefix(input_width_, input_height_)
+                      << "[0-9]?:1 C420; got: " << file_header;
     return false;
   }
 
-  number_of_frames_ = static_cast<int>((source_file_size - kFileHeaderSize) /
+  number_of_frames_ = static_cast<int>((source_file_size - file_header.size()) /
                                        frame_length_in_bytes_);
 
   if (number_of_frames_ == 0) {
-    RTC_LOG(LS_ERROR) << "Input file " << input_filename_.c_str()
-                      << " is too small.";
+    RTC_LOG(LS_ERROR) << "Input file " << input_filename_ << " is too small.";
   }
   return true;
 }
@@ -82,7 +99,7 @@ rtc::scoped_refptr<I420Buffer> Y4mFrameReaderImpl::ReadFrame() {
   if (fread(buffer_, 1, kFrameHeaderSize, input_file_) < kFrameHeaderSize &&
       ferror(input_file_)) {
     RTC_LOG(LS_ERROR) << "Couldn't read frame header from input file: "
-                      << input_filename_.c_str();
+                      << input_filename_;
     return nullptr;
   }
   return YuvFrameReaderImpl::ReadFrame();
