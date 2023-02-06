89054880a783526dd9e296d0f509b82b544b80cc
André Bargull <andre.bargull@gmail.com>
2023-02-06 13:46:26 +0000

    Bug 1812148: Ensure ballast for MDebug{Enter,Leave}GCUnsafeRegion. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D168047

---
+++ js/src/jit-test/tests/auto-regress/bug1812148.js
@@ -0,0 +1,49 @@
+const f = (x) => 1;
+for (let v4 = 0; v4 < 100; v4++) {
+    for (const v8 of "arguments") {
+        v8 == "a";
+        Symbol.for().description;
+        Symbol.for();
+        Symbol.for();
+    }
+    const v16 = Symbol.search;
+    let v17 = v16.description;
+    Symbol.for("arguments");
+    const v19 = v16.description;
+    v17 = 0;
+    const v21 = Symbol.for(v19).description;
+    Symbol.for(v21);
+    Symbol.for(v21);
+    const v25 = Symbol.for().description;
+    Symbol.for();
+    Symbol.for();
+    Symbol.for();
+    Symbol.for().description;
+    Symbol.for();
+    Symbol.for();
+    Symbol.for().description;
+    Symbol.for();
+    Symbol.for(v25).description;
+    Symbol.for();
+    Symbol.for();
+    Symbol.for();
+    Symbol.for();
+    const v42 = (1024)[v16];
+    const v43 = Symbol();
+    Symbol.for(v19).description;
+    Symbol.for();
+    Symbol.for();
+    Symbol.for(v21).description;
+    Symbol.for();
+    Symbol.for();
+    Symbol.for();
+    Symbol.for();
+    Symbol.for();
+    Symbol.for();
+    Symbol.for();
+    Symbol[v43] ||= v42;
+}
+f(0);
+f(0);
+f(0);
+f(0);
