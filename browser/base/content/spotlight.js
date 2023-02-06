33657f726d9fd86335a2639645f9443e657aab93
hanna alemu <halemu@mozilla.com>
2023-02-06 22:36:28 +0000

    Bug 1768730 - [Cleanup] Remove legacy spotlight messaging surface r=Mardak,fluent-reviewers,desktop-theme-reviewers,barret
    Differential Revision: https://phabricator.services.mozilla.com/D165254

--- browser/base/content/spotlight.js
+++ browser/base/content/spotlight.js
@@ -7,112 +7,14 @@ const { document: gDoc, XPCOMUtils } = browser.ownerGlobal;
 
 XPCOMUtils.defineLazyModuleGetters(this, {
   AboutWelcomeParent: "resource:///actors/AboutWelcomeParent.jsm",
-  RemoteL10n: "resource://activity-stream/lib/RemoteL10n.jsm",
 });
 
-const [CONFIG, PARAMS] = window.arguments[0];
-
-function cloneTemplate(id) {
-  return document.getElementById(id).content.cloneNode(true);
-}
+const CONFIG = window.arguments[0];
 
 function addStylesheet(href) {
   const link = document.head.appendChild(document.createElement("link"));
   link.rel = "stylesheet";
   link.href = href;
-}
-
-/**
- * Render content based on Spotlight-specific templates.
- */
-async function renderSpotlight(ready) {
-  const { template, logo = {}, body, extra = {} } = CONFIG;
-
-  // Add Spotlight styles
-  addStylesheet("chrome://browser/skin/spotlight.css");
-
-  // Apply desired message template.
-  const clone = cloneTemplate(template);
-  document.body.classList.add(template);
-
-  // Render logo element.
-  let imageEl = clone.querySelector(".logo");
-  if (logo.imageURL) {
-    imageEl.src = logo.imageURL;
-  } else {
-    imageEl.style.visibility = "hidden";
-  }
-  imageEl.style.height = imageEl.style.width = logo.size;
-
-  // Set text data of an element by class name with local/remote as configured.
-  const setText = (className, config) => {
-    const el = clone.querySelector(`.${className}`);
-    if (!config.label) {
-      el.remove();
-      return;
-    }
-
-    el.appendChild(
-      RemoteL10n.createElement(document, "span", { content: config.label })
-    );
-    el.style.fontSize = config.size;
-  };
-
-  // Render main body text elements.
-  Object.entries(body).forEach(entry => setText(...entry));
-
-  // Optionally apply and render extra behaviors.
-  const { expanded } = extra;
-  if (expanded) {
-    // Add the expanded behavior to the main text content.
-    clone
-      .querySelector("#content")
-      .append(cloneTemplate("extra-content-expanded"));
-    setText("expanded", expanded);
-
-    // Initialize state and handle toggle events.
-    const toggleBtn = clone.querySelector("#learn-more-toggle");
-    const toggle = () => {
-      const toExpand = !!toggleBtn.dataset.l10nId?.includes("collapsed");
-      document.l10n.setAttributes(
-        toggleBtn,
-        toExpand
-          ? "spotlight-learn-more-expanded"
-          : "spotlight-learn-more-collapsed"
-      );
-      toggleBtn.setAttribute("aria-expanded", toExpand);
-    };
-    toggleBtn.addEventListener("click", toggle);
-    toggle();
-  }
-
-  document.body.appendChild(clone);
-
-  let primaryBtn = document.getElementById("primary");
-  let secondaryBtn = document.getElementById("secondary");
-  if (primaryBtn) {
-    primaryBtn.addEventListener("click", () => {
-      PARAMS.primaryBtn = true;
-      window.close();
-    });
-
-    // If we just call focus() at some random time, it'll cause a flush,
-    // which slows things down unnecessarily, so instead we use rAF...
-    requestAnimationFrame(() => {
-      primaryBtn.focus({ focusVisible: false });
-    });
-  }
-  if (secondaryBtn) {
-    secondaryBtn.addEventListener("click", () => {
-      PARAMS.secondaryBtn = true;
-      window.close();
-    });
-  }
-
-  // Wait for translations to load before getting sizing information.
-  await document.l10n.ready;
-  await document.l10n.translateElements(clone.children);
-  requestAnimationFrame(() => requestAnimationFrame(ready));
 }
 
 /**
@@ -175,10 +77,7 @@ function renderMultistage(ready) {
 document.mozSubdialogReady = new Promise(resolve =>
   document.addEventListener(
     "DOMContentLoaded",
-    () =>
-      (CONFIG.template === "multistage" ? renderMultistage : renderSpotlight)(
-        resolve
-      ),
+    () => renderMultistage(resolve),
     {
       once: true,
     }
