ebf51b6be852a5d852c0e504f234aac79c9d2abd
arcade <arcade>
2023-02-06 15:37:55 +0000

    Bug 1810343 - skip implementation check of PR_GetPhysicalMemorySize on DragonFly BSD r=stransky
    Differential Revision: https://phabricator.services.mozilla.com/D168350

--- image/SurfaceCache.cpp
+++ image/SurfaceCache.cpp
@@ -1622,7 +1622,9 @@ void SurfaceCache::Initialize() {
   // Compute the size of the surface cache.
   uint64_t memorySize = PR_GetPhysicalMemorySize();
   if (memorySize == 0) {
+#if !defined(__DragonFly__)
     MOZ_ASSERT_UNREACHABLE("PR_GetPhysicalMemorySize not implemented here");
+#endif
     memorySize = 256 * 1024 * 1024;  // Fall back to 256MB.
   }
   uint64_t proposedSize = memorySize / surfaceCacheSizeFactor;
