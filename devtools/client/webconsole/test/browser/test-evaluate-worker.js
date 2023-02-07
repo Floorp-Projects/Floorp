db0a3e3594c62199bdad94d7c94a20776e083e50
Tooru Fujisawa <arai_a@mac.com>
2023-02-07 07:36:16 +0000

    Bug 1806598 - Part 6.5: Do not use WorkerGlobalScope.prototype.location and WorkerLocation.prototype.href in eager evaluation test. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D169035

--- devtools/client/webconsole/test/browser/test-evaluate-worker.js
+++ devtools/client/webconsole/test/browser/test-evaluate-worker.js
@@ -1,5 +1,11 @@
 "use strict";
 
+// Given WorkerGlobalScope.prototype.location and WorkerLocation.prototype.href
+// getters are not in eager-evaluation allowlist (see bug 1815381),
+// store the value here and use location_href variable in the test.
+// eslint-disable-next-line no-unused-vars
+var location_href = globalThis.location.href;
+
 self.addEventListener("message", ({ data }) => foo(data));
 
 function foo(data) {
