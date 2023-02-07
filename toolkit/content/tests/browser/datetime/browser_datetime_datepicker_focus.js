666166a02c22b479196dff6fff57820c8460d3c2
Anna Yeddi <ayeddi@mozilla.com>
2023-02-07 00:07:55 +0000

    Bug 1813951 - Updating the high intermittent browser_datetime_datepicker_focus.js tests. r=kcochrane
    Providing additional checks across the testcase to ensure the expected events are fired on expected elements and, if the behavior is not as expected, to get more granular information about the step which actually fails on remote.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168679

--- toolkit/content/tests/browser/datetime/browser_datetime_datepicker_focus.js
+++ toolkit/content/tests/browser/datetime/browser_datetime_datepicker_focus.js
@@ -32,6 +32,7 @@ add_task(async function test_focus_after_selection() {
   await helper.openPicker(
     `data:text/html, <input type="date" value=${inputValue}>`
   );
+  let browser = helper.tab.linkedBrowser;
 
   info("Test behavior when selection is done on the calendar grid");
 
@@ -45,20 +46,60 @@ add_task(async function test_focus_after_selection() {
     "The calendar is updated to show the second previous month (2022-10)."
   );
 
+  // 2022-10-24:
+  const focusedDayEl = getDayEl(24);
+
+  Assert.ok(
+    focusedDayEl.matches(":focus"),
+    "An expected focusable day within a calendar grid is focused"
+  );
+
   let closed = helper.promisePickerClosed();
 
   // Make a selection and close the picker
-  EventUtils.synthesizeKey(" ");
+  EventUtils.synthesizeKey(" ", {});
+
+  // Check the focus is returned to main browser window when a panel is closed
+  await SpecialPowers.spawn(browser, [], async () => {
+    const body = content.document.body;
+    // Testing the focus position within content:
+    Assert.deepEqual(
+      body,
+      content.document.activeElement,
+      `The main content's <body> received programmatic focus`
+    );
+  });
 
   await closed;
+
+  Assert.equal(
+    helper.panel.state,
+    "closed",
+    "Panel is closed when the selection is made"
+  );
 
   let ready = helper.waitForPickerReady();
 
   // Move the keyboard focus to the input field and reopen the picker
-  EventUtils.synthesizeKey("KEY_Tab");
-  EventUtils.synthesizeKey(" ");
+  EventUtils.synthesizeKey("KEY_Tab", {});
+
+  // Check the focus is returned to the Calendar button
+  await SpecialPowers.spawn(browser, [], async () => {
+    const input = content.document.querySelector("input");
+    // Testing the focus position within content:
+    Assert.deepEqual(
+      input,
+      content.document.activeElement,
+      `The input field includes programmatic focus`
+    );
+  });
+
+  // Reopen the picker
+  EventUtils.synthesizeKey(" ", {});
 
   await ready;
+
+  Assert.equal(helper.panel.state, "open", "Panel is reopened");
 
   // Move focus from 2022-10-24 to 2022-12-12 by week
   // Changing 2 month views along the way:
@@ -66,8 +107,7 @@ add_task(async function test_focus_after_selection() {
 
   // 2022-12-12:
   const focusedDay = getDayEl(12);
-
-  let monthYearEl = helper.getElement(MONTH_YEAR);
+  const monthYearEl = helper.getElement(MONTH_YEAR);
 
   await BrowserTestUtils.waitForMutationCondition(
     monthYearEl,
@@ -117,7 +157,7 @@ add_task(async function test_focus_after_selection() {
   );
 
   // Make a selection, close the month picker
-  EventUtils.synthesizeKey(" ");
+  EventUtils.synthesizeKey(" ", {});
 
   Assert.ok(
     BrowserTestUtils.is_hidden(helper.getElement(MONTH_YEAR_VIEW)),
