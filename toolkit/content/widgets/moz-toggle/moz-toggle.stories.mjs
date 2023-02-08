507566f9c2af29dd07d5899c1a3e0fc21c20a81a
Hanna Jones <hjones@mozilla.com>
2023-02-08 00:09:06 +0000

    Bug 1799471 - Part 1: add description slot to moz-toggle r=tgiles,mstriemer
    This patch updates `moz-toggle` to accept a `description` slot that can be used to change the description markup in cases where we need to display a support link or some other content.
    
    I'm not totally convinced using a slot is the right approach since it might be making the toggle markup a little too configurable. One alternative is we could accept another property that we use to append a support link to the description, but I don't love that either. I don't want our components sprouting tons of properties to handle edge cases. Open to thoughts/feedback on this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D167880

--- toolkit/content/widgets/moz-toggle/moz-toggle.stories.mjs
+++ toolkit/content/widgets/moz-toggle/moz-toggle.stories.mjs
@@ -1,10 +1,11 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* eslint-disable import/no-unassigned-import */
 
 import { html, ifDefined } from "../vendor/lit.all.mjs";
-// eslint-disable-next-line import/no-unassigned-import
 import "./moz-toggle.mjs";
+import "../moz-support-link/moz-support-link.mjs";
 
 export default {
   title: "Design System/Experiments/MozToggle",
@@ -16,7 +17,14 @@ export default {
   },
 };
 
-const Template = ({ pressed, disabled, label, description, ariaLabel }) => html`
+const Template = ({
+  pressed,
+  disabled,
+  label,
+  description,
+  ariaLabel,
+  hasSupportLink,
+}) => html`
   <div style="max-width: 400px">
     <moz-toggle
       ?pressed=${pressed}
@@ -24,20 +32,30 @@ const Template = ({ pressed, disabled, label, description, ariaLabel }) => html`
       label=${ifDefined(label)}
       description=${ifDefined(description)}
       aria-label=${ifDefined(ariaLabel)}
-    ></moz-toggle>
+    >
+      ${hasSupportLink
+        ? html`
+            <a
+              is="moz-support-link"
+              support-page="addons"
+              slot="support-link"
+            ></a>
+          `
+        : ""}
+    </moz-toggle>
   </div>
 `;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Toggle = Template.bind({});
+Toggle.args = {
   pressed: true,
   disabled: false,
   ariaLabel: "This is the aria-label",
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  ...Default.args,
+export const ToggleDisabled = Template.bind({});
+ToggleDisabled.args = {
+  ...Toggle.args,
   disabled: true,
 };
 
@@ -46,10 +64,17 @@ WithLabel.args = {
   pressed: true,
   disabled: false,
   label: "This is the label",
+  hasSupportLink: false,
 };
 
 export const WithDescription = Template.bind({});
 WithDescription.args = {
   ...WithLabel.args,
-  description: "This is the description",
+  description: "This is the description.",
+};
+
+export const WithSupportLink = Template.bind({});
+WithSupportLink.args = {
+  ...WithDescription.args,
+  hasSupportLink: true,
 };
