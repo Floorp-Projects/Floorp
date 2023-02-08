dfb5e9019cf48c7cb255c139368d330f4663ebee
Marian-Vasile Laza <mlaza@mozilla.com>
2023-02-08 21:17:51 +0200

    Backed out changeset 7d610bc570cd (bug 1813162) as requested by the dev, issue with the patch. CLOSED TREE

--- browser/components/search/test/browser/browser_search_telemetry_sources.js
+++ browser/components/search/test/browser/browser_search_telemetry_sources.js
@@ -77,7 +77,6 @@ add_setup(async function() {
       ],
       // Ensure to add search suggestion telemetry as search_suggestion not search_formhistory.
       ["browser.urlbar.maxHistoricalSearchSuggestions", 0],
-      ["browser.search.serpEventTelemetry.enabled", true],
     ],
   });
   // Enable local telemetry recording for the duration of the tests.
@@ -135,15 +134,6 @@ async function track_ad_click(
     }
   );
 
-  assertImpressionEvents([
-    {
-      provider: "example",
-      tagged: "true",
-      partner_code: "ff",
-      source: expectedScalarSource,
-    },
-  ]);
-
   let pageLoadPromise = BrowserTestUtils.waitForLocationChange(gBrowser);
   await SpecialPowers.spawn(tab.linkedBrowser, [], () => {
     content.document.getElementById("ad1").click();
@@ -162,18 +152,7 @@ async function track_ad_click(
     }
   );
 
-  assertImpressionEvents([
-    {
-      provider: "example",
-      tagged: "true",
-      partner_code: "ff",
-      source: expectedScalarSource,
-    },
-  ]);
-
   await cleanupFn();
-
-  Services.fog.testResetFOG();
 }
 
 add_task(async function test_source_urlbar() {
