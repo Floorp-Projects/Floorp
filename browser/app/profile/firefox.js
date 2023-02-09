59d8bc827b248347c40213f9ab145c5097e58d75
Stephanie Cunnane <scunnane@mozilla.com>
2023-02-09 04:03:37 +0000

    Bug 1813162 - Implement the SERP impression event. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D168172

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -675,6 +675,9 @@ pref("browser.search.separatePrivateDefault.ui.enabled", false);
 // The maximum amount of times the private default banner is shown.
 pref("browser.search.separatePrivateDefault.ui.banner.max", 0);
 
+// Enables search SERP telemetry (impressions, engagements and abandonment)
+pref("browser.search.serpEventTelemetry.enabled", false);
+
 // Enables the display of the Mozilla VPN banner in private browsing windows
 pref("browser.privatebrowsing.vpnpromourl", "https://vpn.mozilla.org/?utm_source=firefox-browser&utm_medium=firefox-%CHANNEL%-browser&utm_campaign=private-browsing-vpn-link");
 
