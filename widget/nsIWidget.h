f4429149ec54dbfd726b40488917a03fc751ff50
Brad Werth <bwerth@mozilla.com>
2023-02-09 23:44:19 +0000

    Bug 1768495 Part 1: Add a method nsIWidget::PauseOrResumeCompositor, and override it in nsCocoaWindow. r=gfx-reviewers,emilio,ahale
    This change does not affect behavior, it's just prepatory for a later
    caller to the newly-defined method.
    
    Differential Revision: https://phabricator.services.mozilla.com/D169344

--- widget/nsIWidget.h
+++ widget/nsIWidget.h
@@ -1974,6 +1974,11 @@ class nsIWidget : public nsISupports {
   virtual CompositorBridgeChild* GetRemoteRenderer() { return nullptr; }
 
   /**
+   * If there is a remote renderer, pause or resume it.
+   */
+  virtual void PauseOrResumeCompositor(bool aPause);
+
+  /**
    * Clear WebRender resources
    */
   virtual void ClearCachedWebrenderResources() {}
