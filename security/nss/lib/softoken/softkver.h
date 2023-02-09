cecf49bb4f225aa38a7065f54f1b58ca2df46653
John Schanck <jschanck@mozilla.com>
2023-02-09 21:53:21 +0000

    Bug 1815979 - land NSS NSS_3_87_1_RTM UPGRADE_NSS_RELEASE, r=keeler a=pascalc
    2023-02-09  John M. Schanck  <jschanck@mozilla.com>
    
    	* doc/rst/releases/nss_3_87_1.rst:
    	Documentation: release notes for NSS 3.87.1
    	[670ab97009df] [NSS_3_87_1_RTM] <NSS_3_87_1_BRANCH>
    
    	* lib/nss/nss.h, lib/softoken/softkver.h, lib/util/nssutil.h:
    	Set version numbers to 3.87.1 final
    	[4cac949cd2e6] <NSS_3_87_1_BRANCH>
    
    	* lib/pkcs12/p12d.c, lib/pkcs12/p12t.h, lib/pkcs12/p12tmpl.c:
    	Bug 1804640 - improve handling of unknown PKCS#12 safe bag types.
    	r=rrelyea
    
    	[62f6b3e9024d] <NSS_3_87_1_BRANCH>
    
    2023-01-05  Dennis Jackson  <djackson@mozilla.com>
    
    	* .hgtags:
    	Added tag NSS_3_87_RTM for changeset 41b6d2645ead
    	[52b4b7d3d3eb] <NSS_3_87_BRANCH>
    
    Differential Revision: https://phabricator.services.mozilla.com/D169384

--- security/nss/lib/softoken/softkver.h
+++ security/nss/lib/softoken/softkver.h
@@ -17,10 +17,10 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define SOFTOKEN_VERSION "3.87" SOFTOKEN_ECC_STRING
+#define SOFTOKEN_VERSION "3.87.1" SOFTOKEN_ECC_STRING
 #define SOFTOKEN_VMAJOR 3
 #define SOFTOKEN_VMINOR 87
-#define SOFTOKEN_VPATCH 0
+#define SOFTOKEN_VPATCH 1
 #define SOFTOKEN_VBUILD 0
 #define SOFTOKEN_BETA PR_FALSE
 
