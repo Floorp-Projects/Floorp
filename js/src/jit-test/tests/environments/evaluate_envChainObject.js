5cd5cacd3ed80b9396c8a6f1ec4ee7178afef5ee
Tooru Fujisawa <arai_a@mac.com>
2023-02-10 02:35:17 +0000

    Bug 1814880 - Disallow all unqualified variables objects in envChainObject option in evaluate function. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D169173

--- js/src/jit-test/tests/environments/evaluate_envChainObject.js
+++ js/src/jit-test/tests/environments/evaluate_envChainObject.js
@@ -1,3 +1,5 @@
+load(libdir + "asserts.js");
+
 // Test with envChainObject in current global.
 {
     let global = newGlobal();
@@ -12,3 +14,19 @@
     assertEq(envChainObject.a, "test2");
     assertEq(evaluate("a", { global, envChainObject }), "test2");
 }
+
+// Unqualified variables objects are not allowed.
+
+if (!isProxy(evalcx(""))) {
+  // if --more-compartments option is not give, evalcx returns sandbox,
+  // which is unqualified variables object.
+  assertThrowsInstanceOf(() => {
+    evaluate("10", { envChainObject: evalcx("") });
+  }, Error);
+}
+
+// evalReturningScope returns NonSyntacticVariablesObject, which is unqualified
+// variables object.
+assertThrowsInstanceOf(() => {
+  evaluate("10", { envChainObject: evalReturningScope("1") });
+}, Error);
