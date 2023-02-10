283e6f0db0adf26a6ab0a8cfba40cbc183bde909
Tooru Fujisawa <arai_a@mac.com>
2023-02-10 01:07:18 +0000

    Bug 1813744 - Use lossy conversion in shell disassemble function. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D169294

---
+++ js/src/jit-test/tests/basic/disassemble-filename.js
@@ -0,0 +1,7 @@
+// Invalid UTF-8 should be replaced with U+FFFD.
+const out = evaluate(`
+disassemble();
+`, {
+  fileName: String.fromCharCode(3823486100),
+});
+assertEq(out.includes(`"file": "\uFFFD",`), true);
