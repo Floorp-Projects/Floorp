fb74df23899665b0424dc76a12efafc51e828718
Cristian Tuns <ctuns@mozilla.com>
2023-02-09 21:31:43 -0500

    Backed out 3 changesets (bug 1768495) for causing reftest failures in 1478035.html CLOSED TREE
    Backed out changeset 6a48a9418fb3 (bug 1768495)
    Backed out changeset ac0c0d68e31d (bug 1768495)
    Backed out changeset 74c1903a5df6 (bug 1768495)

--- widget/cocoa/nsCocoaWindow.h
+++ widget/cocoa/nsCocoaWindow.h
@@ -354,7 +354,8 @@ class nsCocoaWindow final : public nsBaseWidget, public nsPIWidgetCocoa {
 
   bool InFullScreenMode() const { return mInFullScreenMode; }
 
-  void PauseOrResumeCompositor(bool aPause) override;
+  void PauseCompositor();
+  void ResumeCompositor();
 
   bool AsyncPanZoomEnabled() const override;
 
