929f809c4bb3fc3b8a917b97eb58d0c6a9ef5acf
Kagami Sascha Rosylight <krosylight@mozilla.com>
2023-02-13 18:51:50 +0000

    Bug 1781588 - Part 4: Migrate tests for custom data r=smaug
    Porting only the `data` test since there is a behavior mismatch for `icon`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D169319

--- dom/notification/test/mochitest/NotificationTest.js
+++ dom/notification/test/mochitest/NotificationTest.js
@@ -47,35 +47,6 @@ var NotificationTest = (function() {
     })(tests);
   }
 
-  var fakeCustomData = (function() {
-    var buffer = new ArrayBuffer(2);
-    new DataView(buffer).setInt16(0, 42, true);
-    var canvas = document.createElement("canvas");
-    canvas.width = canvas.height = 100;
-    var context = canvas.getContext("2d");
-
-    var map = new Map();
-    var set = new Set();
-    map.set("test", 42);
-    set.add(4);
-    set.add(2);
-
-    return {
-      primitives: {
-        a: 123,
-        b: "test",
-        c: true,
-        d: [1, 2, 3],
-      },
-      date: new Date(2013, 2, 1, 1, 10),
-      regexp: new RegExp("[^.]+"),
-      arrayBuffer: buffer,
-      imageData: context.createImageData(100, 100),
-      map,
-      set,
-    };
-  })();
-
   // NotificationTest API
   return {
     run(tests, callback) {
@@ -120,33 +91,12 @@ var NotificationTest = (function() {
 
     info,
 
-    customDataMatches(dataObj) {
-      var url = "http://www.domain.com";
-      try {
-        return (
-          JSON.stringify(dataObj.primitives) ===
-            JSON.stringify(fakeCustomData.primitives) &&
-          dataObj.date.toDateString() === fakeCustomData.date.toDateString() &&
-          dataObj.regexp.exec(url)[0].substr(7) === "www" &&
-          new Int16Array(dataObj.arrayBuffer)[0] === 42 &&
-          JSON.stringify(dataObj.imageData.data) ===
-            JSON.stringify(fakeCustomData.imageData.data) &&
-          dataObj.map.get("test") == 42 &&
-          dataObj.set.has(4) &&
-          dataObj.set.has(2)
-        );
-      } catch (e) {
-        return false;
-      }
-    },
-
     payload: {
       body: "Body",
       tag: "fakeTag",
       icon: "icon.jpg",
       lang: "en-US",
       dir: "ltr",
-      data: fakeCustomData,
     },
   };
 })();
