1ed73fdbeae1344ba38401d0ad257e65c11893f7
Tom Schuster <tschuster@mozilla.com>
2023-02-13 16:31:25 +0000

    Bug 1811855 - Annotate KeyboardEvent ShouldResistFingerprinting efficiency check. r=tjr
    Depends on D167563
    
    Differential Revision: https://phabricator.services.mozilla.com/D167564

--- dom/events/KeyboardEvent.cpp
+++ dom/events/KeyboardEvent.cpp
@@ -378,14 +378,14 @@ void KeyboardEvent::InitKeyboardEventJS(
 
 bool KeyboardEvent::ShouldResistFingerprinting(CallerType aCallerType) {
   // There are five situations we don't need to spoof this keyboard event.
-  //   1. The pref privcy.resistFingerprinting' is false, we fast return here
-  //      since we don't need to do any QI of following codes.
+  //   1. The pref privcy.resistFingerprinting' is false, we fast return here.
   //   2. This event is initialized by scripts.
   //   3. This event is from Numpad.
   //   4. This event is in the system group.
   //   5. The caller type is system.
-  if (!nsContentUtils::ShouldResistFingerprinting() || mInitializedByJS ||
-      aCallerType == CallerType::System || mEvent->mFlags.mInSystemGroup ||
+  if (!nsContentUtils::ShouldResistFingerprinting("Efficiency Check") ||
+      mInitializedByJS || aCallerType == CallerType::System ||
+      mEvent->mFlags.mInSystemGroup ||
       mEvent->AsKeyboardEvent()->mLocation ==
           KeyboardEvent_Binding::DOM_KEY_LOCATION_NUMPAD) {
     return false;
