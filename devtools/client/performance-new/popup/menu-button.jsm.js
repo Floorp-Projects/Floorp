1d943bb39a713cf2e215e9f4fad1e9aa5711a15c
Julien Wajsberg <felash@gmail.com>
2023-02-13 16:46:40 +0000

    Bug 1811853 - Rename panel.jsm.js to logic.jsm.js, to better reflect its content r=canaltinova
    Differential Revision: https://phabricator.services.mozilla.com/D167849

--- devtools/client/performance-new/popup/menu-button.jsm.js
+++ devtools/client/performance-new/popup/menu-button.jsm.js
@@ -22,9 +22,9 @@ const lazy = createLazyLoaders({
     ChromeUtils.import("resource:///modules/CustomizableUI.jsm"),
   CustomizableWidgets: () =>
     ChromeUtils.import("resource:///modules/CustomizableWidgets.jsm"),
-  PopupPanel: () =>
+  PopupLogic: () =>
     ChromeUtils.import(
-      "resource://devtools/client/performance-new/popup/panel.jsm.js"
+      "resource://devtools/client/performance-new/popup/logic.jsm.js"
     ),
   Background: () =>
     ChromeUtils.import(
@@ -110,7 +110,7 @@ function initialize(toggleProfilerKeyShortcuts) {
   /**
    * This is mutable state that will be shared between panel displays.
    *
-   * @type {import("devtools/client/performance-new/popup/panel.jsm.js").State}
+   * @type {import("devtools/client/performance-new/popup/logic.jsm.js").State}
    */
   const panelState = {
     cleanup: [],
@@ -166,7 +166,7 @@ function initialize(toggleProfilerKeyShortcuts) {
           // The popup logic is stored in a separate script so it doesn't have
           // to be parsed at browser startup, and will only be lazily loaded
           // when the popup is viewed.
-          const { initializePopup } = lazy.PopupPanel();
+          const { initializePopup } = lazy.PopupLogic();
 
           initializePopup(panelState, event.target);
         } catch (error) {
