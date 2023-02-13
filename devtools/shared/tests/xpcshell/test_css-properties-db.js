909626c3f497045c300dd9407d66e6f237e9b1ad
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-02-13 12:38:29 +0000

    Bug 1815877 - [devtools] Don't include CSS property preferences in devtools/shared/css/generated/properties-db.js. r=ochameau.
    This was only used in a XPCShell test, where we can
    directly use InspectorUtils.getCSSPropertyPrefs() instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D169338

--- devtools/shared/tests/xpcshell/test_css-properties-db.js
+++ devtools/shared/tests/xpcshell/test_css-properties-db.js
@@ -22,8 +22,8 @@
 const {
   PSEUDO_ELEMENTS,
   CSS_PROPERTIES,
-  PREFERENCES,
 } = require("resource://devtools/shared/css/generated/properties-db.js");
+const PREFERENCES = InspectorUtils.getCSSPropertyPrefs();
 const {
   generateCssProperties,
 } = require("resource://devtools/server/actors/css-properties.js");
@@ -44,14 +44,6 @@ function run_test() {
     PSEUDO_ELEMENTS,
     InspectorUtils.getCSSPseudoElementNames(),
     "The pseudo elements match on the client and platform. " +
-      propertiesErrorMessage
-  );
-
-  const prefs = InspectorUtils.getCSSPropertyPrefs();
-  deepEqual(
-    PREFERENCES,
-    prefs.map(({ name, pref }) => [name, pref]),
-    "The preferences match on the client and platform. " +
       propertiesErrorMessage
   );
 
@@ -177,12 +169,12 @@ function getKeyMismatches(a, b) {
  * @return {Boolean|undefined}
  */
 function getPreference(propertyName) {
-  const preference = PREFERENCES.find(([prefPropertyName, preferenceKey]) => {
-    return prefPropertyName === propertyName && !!preferenceKey;
+  const preference = PREFERENCES.find(({ name, pref }) => {
+    return name === propertyName && !!pref;
   });
 
   if (preference) {
-    return Preferences.get(preference[1]);
+    return Preferences.get(preference.pref);
   }
   return undefined;
 }
