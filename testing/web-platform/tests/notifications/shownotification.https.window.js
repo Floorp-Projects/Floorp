fa9b78fb1d82043a182a785ed7d9f26a9c769a69
Kagami Sascha Rosylight <krosylight@mozilla.com>
2023-02-13 18:51:50 +0000

    Bug 1781588 - Part 3: Migrate tests for different SW registrations r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D169054

--- testing/web-platform/tests/notifications/shownotification.https.window.js
+++ testing/web-platform/tests/notifications/shownotification.https.window.js
@@ -15,6 +15,7 @@ function reset() {
 async function registerSw() {
   await reset();
   const reg = await navigator.serviceWorker.register("noop-sw.js");
+  add_completion_callback(() => reg.unregister());
   await navigator.serviceWorker.ready;
   return reg;
 }
@@ -70,3 +71,27 @@ promise_test(async t => {
   const notifications = await registration.getNotifications();
   assert_equals(notifications.length, 3, "Should return three notifications");
 }, "fetching multiple notifications");
+
+// https://notifications.spec.whatwg.org/#dom-serviceworkerregistration-getnotifications
+// Step 5.2: Let notifications be a list of all notifications in the list of
+// notifications ... whose service worker registration is this ...
+promise_test(async t => {
+  t.add_cleanup(cleanup);
+  const another = await navigator.serviceWorker.register("noop-sw.js", { scope: "./scope" });
+  await registration.showNotification("Hello");
+  const notifications = await another.getNotifications();
+  assert_equals(notifications.length, 0, "Should return no notification");
+}, "fetching from another registration")
+
+// https://notifications.spec.whatwg.org/#non-persistent-notification
+// A non-persistent notification is a notification without an associated
+// service worker registration.
+promise_test(async t => {
+  t.add_cleanup(cleanup);
+  const nonPersistent = new Notification("Non-persistent");
+  t.add_cleanup(() => nonPersistent.close());
+  await registration.showNotification("Hello");
+  const notifications = await registration.getNotifications();
+  assert_equals(notifications.length, 1, "Should return a notification");
+  assert_equals(notifications[0].title, "Hello", "Title should match");
+}, "fetching only persistent notifications")
