4aaeba411c89b2cd1b58bfda3bc9e0cca8533f39
Mike Conley <mconley@mozilla.com>
2023-02-14 21:58:45 +0000

    Bug 1816563 - Make it possible to supply a list of manifest files that will error on failing the test-manifest-alpha check. r=ahal
    Depends on D169731
    
    Differential Revision: https://phabricator.services.mozilla.com/D169732

--- tools/lint/test-manifest-alpha/__init__.py
+++ tools/lint/test-manifest-alpha/__init__.py
@@ -3,18 +3,33 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
+import sys
 
+import yaml
 from manifestparser import TestManifest
 from mozlint import result
 from mozlint.pathutils import expand_exclusions
+from mozpack import path as mozpath
 
 # Since this linter matches on all files with .ini extensions, we can omit
 # those extensions from this allowlist, which makes it easier to match
 # against variants like "mochitest-serviceworker.ini".
 FILENAME_ALLOWLIST = ["mochitest", "browser", "chrome", "a11y"]
 
+here = os.path.abspath(os.path.dirname(__file__))
+ERROR_LEVEL_MANIFESTS_PATH = os.path.join(here, "error-level-manifests.yml")
+
 
 def lint(paths, config, fix=None, **lintargs):
+    try:
+        with open(ERROR_LEVEL_MANIFESTS_PATH) as f:
+            error_level_manifests = yaml.safe_load(f)
+    except (IOError, ValueError) as e:
+        print("{}: error:\n  {}".format(ERROR_LEVEL_MANIFESTS_PATH, e), file=sys.stderr)
+        sys.exit(1)
+
+    topsrcdir = lintargs["root"]
+
     results = []
     file_names = list(expand_exclusions(paths, config, lintargs["root"]))
 
@@ -29,15 +44,27 @@ def lint(paths, config, fix=None, **lintargs):
         sorted_test_names = sorted(test_names)
 
         if test_names != sorted_test_names:
+            rel_file_path = mozpath.relpath(file_name, topsrcdir)
+            level = "warning"
+
+            if (mozpath.normsep(rel_file_path) in error_level_manifests) or (
+                any(
+                    mozpath.match(rel_file_path, e)
+                    for e in error_level_manifests
+                    if "*" in e
+                )
+            ):
+                level = "error"
+
             res = {
-                "path": file_name,
+                "path": rel_file_path,
                 "lineno": 0,
                 "column": 0,
                 "message": (
                     "The mochitest test manifest is not in alphabetical order. "
                     "Expected ordering: \n\n%s\n\n" % "\n".join(sorted_test_names)
                 ),
-                "level": "warning",
+                "level": level,
             }
             results.append(result.from_config(config, **res))
 
