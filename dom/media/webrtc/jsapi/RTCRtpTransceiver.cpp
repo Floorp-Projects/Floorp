bab968be3782a422256afe5377744bf458f8af9b
Byron Campen <docfaraday@gmail.com>
2023-02-15 20:23:26 +0000

    Bug 1265827: Implement RTCPeerConnectionState. r=jib,webidl,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D168143

--- dom/media/webrtc/jsapi/RTCRtpTransceiver.cpp
+++ dom/media/webrtc/jsapi/RTCRtpTransceiver.cpp
@@ -226,17 +226,6 @@ void RTCRtpTransceiver::Init(const RTCRtpTransceiverInit& aInit,
     InitConduitControl();
   }
 
-  auto self = nsMainThreadPtrHandle<RTCRtpTransceiver>(
-      new nsMainThreadPtrHolder<RTCRtpTransceiver>(
-          "RTCRtpTransceiver::RTCRtpTransceiver::self", this, false));
-  mStsThread->Dispatch(
-      NS_NewRunnableFunction("RTCRtpTransceiver::RTCRtpTransceiver", [self] {
-        self->mTransportHandler->SignalStateChange.connect(
-            self.get(), &RTCRtpTransceiver::UpdateDtlsTransportState);
-        self->mTransportHandler->SignalRtcpStateChange.connect(
-            self.get(), &RTCRtpTransceiver::UpdateDtlsTransportState);
-      }));
-
   mSender->SetStreams(aInit.mStreams);
   mDirection = aInit.mDirection;
 }
@@ -251,23 +240,6 @@ void RTCRtpTransceiver::SetDtlsTransport(dom::RTCDtlsTransport* aDtlsTransport,
 
 void RTCRtpTransceiver::RollbackToStableDtlsTransport() {
   mDtlsTransport = mLastStableDtlsTransport;
-}
-
-void RTCRtpTransceiver::UpdateDtlsTransportState(
-    const std::string& aTransportId, TransportLayer::State aState) {
-  if (!GetMainThreadSerialEventTarget()->IsOnCurrentThread()) {
-    GetMainThreadSerialEventTarget()->Dispatch(
-        WrapRunnable(this, &RTCRtpTransceiver::UpdateDtlsTransportState,
-                     aTransportId, aState),
-        NS_DISPATCH_NORMAL);
-    return;
-  }
-
-  if (!mDtlsTransport) {
-    return;
-  }
-
-  mDtlsTransport->UpdateState(aState);
 }
 
 void RTCRtpTransceiver::InitAudio() {
@@ -885,13 +857,7 @@ void RTCRtpTransceiver::StopImpl() {
   mSender->Stop();
   mReceiver->Stop();
 
-  auto self = nsMainThreadPtrHandle<RTCRtpTransceiver>(
-      new nsMainThreadPtrHolder<RTCRtpTransceiver>(
-          "RTCRtpTransceiver::StopImpl::self", this, false));
-  mStsThread->Dispatch(NS_NewRunnableFunction(__func__, [self] {
-    self->disconnect_all();
-    self->mTransportHandler = nullptr;
-  }));
+  mTransportHandler = nullptr;
 }
 
 bool RTCRtpTransceiver::IsVideo() const { return mIsVideo; }
