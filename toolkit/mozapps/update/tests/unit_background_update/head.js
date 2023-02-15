776b3b470536307f16768d1c694cb5ddab6df37d
Max Christian Pohle <mpohle@mozilla.com>
2023-02-15 21:40:00 +0000

    Bug 1795471 - Add metrics to understand why users do not use background update, r=nalexander
    Add metrics to help understand how many users are not using background
    update on Windows.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168754

--- toolkit/mozapps/update/tests/unit_background_update/head.js
+++ toolkit/mozapps/update/tests/unit_background_update/head.js
@@ -16,3 +16,43 @@ const do_backgroundtask = BackgroundTasksTestUtils.do_backgroundtask.bind(
 const setupProfileService = BackgroundTasksTestUtils.setupProfileService.bind(
   BackgroundTasksTestUtils
 );
+
+// Helper function to register a callback to catch a Glean ping before its
+// submission. The function returns all string_list items, but not as they
+// appear in the ping itself, but as full text representation, which is the
+// value of the corresponding field. This makes the test more unique, because
+// the values often contain chars, which are not allowed in glean metric labels
+//
+// @returns: an array which contains all glean metrics, but as full text
+//           representation from the BackgroundUpdate.REASON object => its
+//           values, see description for further details.
+//
+async function checkGleanPing() {
+  let retval = ["EMPTY"];
+  let ping_submitted = false;
+
+  const { maybeSubmitBackgroundUpdatePing } = ChromeUtils.import(
+    "resource://gre/modules/backgroundtasks/BackgroundTask_backgroundupdate.jsm"
+  );
+  const { BackgroundUpdate } = ChromeUtils.import(
+    "resource://gre/modules/BackgroundUpdate.jsm"
+  );
+
+  GleanPings.backgroundUpdate.testBeforeNextSubmit(_ => {
+    ping_submitted = true;
+    retval = Glean.backgroundUpdate.reasonsToNotUpdate.testGetValue().map(v => {
+      return BackgroundUpdate.REASON[v];
+    });
+    Assert.ok(Array.isArray(retval));
+    return retval;
+  });
+  await maybeSubmitBackgroundUpdatePing();
+  Assert.ok(ping_submitted, "Glean ping successfully submitted");
+
+  // The metric has `lifetime: application` set, but when testing we do not
+  // want to keep the results around and avoid, that one test can influence
+  // another. That is why we clear this string_list.
+  Glean.backgroundUpdate.reasonsToNotUpdate.set([]);
+
+  return retval;
+}
---
+++ toolkit/mozapps/update/tests/unit_background_update/test_backgroundupdate_reason.js
@@ -0,0 +1,66 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
+ * vim: sw=2 ts=2 sts=2 et
+ * this source code form is subject to the terms of the mozilla public license,
+ * v. 2.0. if a copy of the mpl was not distributed with this file, you can
+ * obtain one at http://mozilla.org/mpl/2.0/. */
+
+"use strict";
+
+const { BackgroundUpdate } = ChromeUtils.import(
+  "resource://gre/modules/BackgroundUpdate.jsm"
+);
+
+// These tests use per-installation prefs, and those are a shared resource, so
+// they require some non-trivial setup.
+setupTestCommon(null);
+standardInit();
+
+add_setup(function test_setup() {
+  // FOG needs a profile directory to put its data in.
+  do_get_profile();
+
+  // We need to initialize it once, otherwise operations will be stuck in the
+  // pre-init queue.
+  Services.fog.initializeFOG();
+});
+
+// Because we want to use the keys from REASON as strings and send these with
+// Glean, we have to make sure, that they meet the requirements for `String
+// Lists` and are not too long.
+add_task(async function test_reasons_length() {
+  for (const key of Object.keys(BackgroundUpdate.REASON)) {
+    Glean.backgroundUpdate.reasonsToNotUpdate.add(key);
+    // No exception means success.
+    Assert.ok(
+      Array.isArray(Glean.backgroundUpdate.reasonsToNotUpdate.testGetValue()),
+      "Glean allows the name of the reason to be '" + key + "'"
+    );
+  }
+});
+
+// The string list in Glean can overflow and has a hard limit of 20 entries.
+// This test toggles a switch to reach this limit and fails if this causes an
+// exception, because we want to avoid that statistical data collection can have
+// an negative impact on the success rate of background updates.
+add_task(async function test_reasons_overflow() {
+  let prev = await UpdateUtils.getAppUpdateAutoEnabled();
+  try {
+    for (let i = 1; i <= 21; i++) {
+      await UpdateUtils.setAppUpdateAutoEnabled(false);
+      await BackgroundUpdate._reasonsToNotUpdateInstallation();
+      await UpdateUtils.setAppUpdateAutoEnabled(true);
+      await BackgroundUpdate._reasonsToNotUpdateInstallation();
+      Assert.ok(true, "Overflow test successful for run #" + i);
+    }
+  } finally {
+    ok(true, "resetting AppUpdateAutoEnabled to " + prev);
+    await UpdateUtils.setAppUpdateAutoEnabled(prev);
+  }
+});
+
+add_task(() => {
+  // `setupTestCommon()` calls `do_test_pending()`; this calls
+  // `do_test_finish()`.  The `add_task` schedules this to run after all the
+  // other tests have completed.
+  doTestFinish();
+});
