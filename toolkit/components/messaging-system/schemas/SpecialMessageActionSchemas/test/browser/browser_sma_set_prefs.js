4814fda3ff2a19bd8a2765cc21a07ea1fab0bdc4
Jason Prickett <jprickett@mozilla.com>
2023-02-16 18:23:38 +0000

    Bug 1806424 - Added ability for special message actions to set any pref with messaging-system-action prefix r=omc-reviewers,mviar
    Differential Revision: https://phabricator.services.mozilla.com/D169406

--- toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_set_prefs.js
+++ toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_set_prefs.js
@@ -5,12 +5,17 @@
 
 const HOMEPAGE_PREF = "browser.startup.homepage";
 const PRIVACY_SEGMENTATION_PREF = "browser.dataFeatureRecommendations.enabled";
+const MESSAGING_ACTION_PREF = "special-message-testpref";
 
-const PREFS = [HOMEPAGE_PREF, PRIVACY_SEGMENTATION_PREF];
+const PREFS_TO_CLEAR = [
+  HOMEPAGE_PREF,
+  PRIVACY_SEGMENTATION_PREF,
+  `messaging-system-action.${MESSAGING_ACTION_PREF}`,
+];
 
 add_setup(async function() {
   registerCleanupFunction(async () => {
-    PREFS.forEach(pref => Services.prefs.clearUserPref(pref));
+    PREFS_TO_CLEAR.forEach(pref => Services.prefs.clearUserPref(pref));
   });
 });
 
@@ -102,5 +107,61 @@ add_task(async function test_clear_homepage_pref() {
   Assert.ok(
     !Services.prefs.prefHasUserValue(HOMEPAGE_PREF),
     `${HOMEPAGE_PREF} pref successfully updated`
+  );
+});
+
+// Set a pref not listed in "allowed prefs"
+add_task(async function test_set_messaging_system_pref() {
+  const action = {
+    type: "SET_PREF",
+    data: {
+      pref: {
+        name: MESSAGING_ACTION_PREF,
+        value: true,
+      },
+    },
+  };
+
+  await SMATestUtils.executeAndValidateAction(action);
+
+  Assert.equal(
+    Services.prefs.getBoolPref(
+      `messaging-system-action.${MESSAGING_ACTION_PREF}`
+    ),
+    true,
+    `messaging-system-action.${MESSAGING_ACTION_PREF} pref successfully updated to correct value`
+  );
+});
+
+// Clear a pref not listed in "allowed prefs" that was initially set by
+// the SET_PREF special messaging action
+add_task(async function test_clear_messaging_system_pref() {
+  Services.prefs.setBoolPref(
+    `messaging-system-action.${MESSAGING_ACTION_PREF}`,
+    true
+  );
+  Assert.ok(
+    Services.prefs.prefHasUserValue(
+      `messaging-system-action.${MESSAGING_ACTION_PREF}`
+    ),
+    "Test setup ok"
+  );
+
+  const action = {
+    type: "SET_PREF",
+    data: {
+      pref: {
+        name: MESSAGING_ACTION_PREF,
+      },
+    },
+  };
+
+  await SMATestUtils.executeAndValidateAction(action);
+
+  Assert.ok(
+    !Services.prefs.prefHasUserValue(
+      `messaging-system-action.${MESSAGING_ACTION_PREF}`
+    ),
+    `messaging-system-action.${MESSAGING_ACTION_PREF} pref successfully cleared`
   );
 });
