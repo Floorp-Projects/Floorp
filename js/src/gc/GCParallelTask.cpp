9a72dc4266edc706a77b16e2694a8d72362e1349
Paul Bone <pbone@mozilla.com>
2023-02-16 04:11:55 +0000

    Bug 1816340 - pt 1. Add a locked version of runFromMainThread() r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D169578

--- js/src/gc/GCParallelTask.cpp
+++ js/src/gc/GCParallelTask.cpp
@@ -64,8 +64,7 @@ void js::GCParallelTask::startOrRunIfIdle(AutoLockHelperThreadState& lock) {
   joinWithLockHeld(lock);
 
   if (!CanUseExtraThreads()) {
-    AutoUnlockHelperThreadState unlock(lock);
-    runFromMainThread();
+    runFromMainThread(lock);
     return;
   }
 
@@ -96,8 +95,7 @@ void js::GCParallelTask::joinWithLockHeld(AutoLockHelperThreadState& lock,
     // and run it from the main thread. This stops us from blocking here when
     // the helper threads are busy with other tasks.
     cancelDispatchedTask(lock);
-    AutoUnlockHelperThreadState unlock(lock);
-    runFromMainThread();
+    runFromMainThread(lock);
   } else {
     // Otherwise wait for the task to complete.
     joinNonIdleTask(deadline, lock);
@@ -149,13 +147,17 @@ static inline TimeDuration TimeSince(TimeStamp prev) {
   return now - prev;
 }
 
-void js::GCParallelTask::runFromMainThread() {
+void js::GCParallelTask::runFromMainThread(AutoLockHelperThreadState& lock) {
   assertIdle();
   MOZ_ASSERT(js::CurrentThreadCanAccessRuntime(gc->rt));
-  AutoLockHelperThreadState lock;
   state_ = State::Running;
   runTask(gc->rt->gcContext(), lock);
   state_ = State::Idle;
+}
+
+void js::GCParallelTask::runFromMainThread() {
+  AutoLockHelperThreadState lock;
+  runFromMainThread(lock);
 }
 
 class MOZ_RAII AutoGCContext {
