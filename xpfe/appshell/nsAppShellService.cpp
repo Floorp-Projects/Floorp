ddb6e64e1eb450e876c8e1b23bbf590104378782
Andreas Pehrson <apehrson@mozilla.com>
2023-02-16 14:50:23 +0000

    Bug 1816436 - Narrow down the pipMask on GTK. r=stransky,mconley
    With this patch the webrtcIndicator is no longer considered a PIP window on GTK.
    This makes the webrtcIndicator *not* show a resize cursor over most of its
    surface.
    
    Differential Revision: https://phabricator.services.mozilla.com/D169783

--- xpfe/appshell/nsAppShellService.cpp
+++ xpfe/appshell/nsAppShellService.cpp
@@ -583,7 +583,8 @@ nsresult nsAppShellService::JustCreateTopWindow(
   // Linux/Gtk PIP window support. It's Chrome Toplevel window, always on top
   // and without any bar.
   uint32_t pipMask = nsIWebBrowserChrome::CHROME_ALWAYS_ON_TOP |
-                     nsIWebBrowserChrome::CHROME_OPENAS_CHROME;
+                     nsIWebBrowserChrome::CHROME_OPENAS_CHROME |
+                     nsIWebBrowserChrome::CHROME_WINDOW_RESIZE;
   uint32_t barMask = nsIWebBrowserChrome::CHROME_MENUBAR |
                      nsIWebBrowserChrome::CHROME_TOOLBAR |
                      nsIWebBrowserChrome::CHROME_LOCATIONBAR |
