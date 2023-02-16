42f40dc755a3c28ed98bf6ec463ab3ac792ff31d
Marian-Vasile Laza <mlaza@mozilla.com>
2023-02-16 11:16:47 +0200

    Backed out changeset 375f7fa64366 (bug 1816946) for bc failures on browser_aboutNetError_trr.js. CLOSED TREE

--- browser/base/content/test/about/browser_aboutNetError_trr.js
+++ browser/base/content/test/about/browser_aboutNetError_trr.js
@@ -8,7 +8,6 @@ function resetPrefs() {
   Services.prefs.clearUserPref("network.trr.mode");
   Services.prefs.clearUserPref("network.dns.native-is-localhost");
   Services.prefs.setIntPref("network.proxy.type", oldProxyType);
-  Services.prefs.clearUserPref("network.trr.display_fallback_warning");
 }
 
 // This test makes sure that the Add exception button only shows up
@@ -63,60 +62,6 @@ add_task(async function exceptionButtonTRROnly() {
     );
   });
 
-  BrowserTestUtils.removeTab(gBrowser.selectedTab);
-  resetPrefs();
-});
-
-add_task(async function exceptionButtonTRROnly() {
-  Services.prefs.setBoolPref("network.dns.native-is-localhost", true);
-  Services.prefs.setIntPref("network.proxy.type", 0);
-  // We need to disable proxy, otherwise TRR isn't used for name resolution.
-  Services.prefs.setIntPref("network.trr.mode", Ci.nsIDNSService.MODE_TRRFIRST);
-  Services.prefs.setBoolPref("network.trr.display_fallback_warning", true);
-  registerCleanupFunction(resetPrefs);
-
-  let browser;
-  let pageLoaded;
-  await BrowserTestUtils.openNewForegroundTab(
-    gBrowser,
-    () => {
-      gBrowser.selectedTab = BrowserTestUtils.addTab(
-        gBrowser,
-        "https://does-not-exist.test"
-      );
-      browser = gBrowser.selectedBrowser;
-      pageLoaded = BrowserTestUtils.waitForErrorPage(browser);
-    },
-    false
-  );
-
-  info("Loading and waiting for the net error");
-  await pageLoaded;
-
-  await SpecialPowers.spawn(browser, [], function() {
-    const doc = content.document;
-    ok(
-      doc.documentURI.startsWith("about:neterror"),
-      "Should be showing error page"
-    );
-
-    const titleEl = doc.querySelector(".title-text");
-    const actualDataL10nID = titleEl.getAttribute("data-l10n-id");
-    is(
-      actualDataL10nID,
-      "dns-not-found-native-fallback-title",
-      "Correct error page title is set"
-    );
-
-    let description = doc.getElementById("nativeFallbackDescription");
-    Assert.equal(
-      description.getAttribute("data-l10n-id"),
-      "neterror-dns-not-found-native-fallback-not-confirmed2",
-      "Unexpected content"
-    );
-  });
-
-  // await new Promise(resolve => {});
   BrowserTestUtils.removeTab(gBrowser.selectedTab);
   resetPrefs();
 });
