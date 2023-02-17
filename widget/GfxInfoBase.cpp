a4e6d7fee529a2292b585d041067cd200fb7557b
sotaro <sotaro.ikeda.g@gmail.com>
2023-02-17 01:47:02 +0000

    Bug 1817269 - Backout Bug 1798245 r=jrmuizel,gfx-reviewers a=pascalc
    Bug 1798245 causes Bug 1817269 on Fx 110.
    
    Differential Revision: https://phabricator.services.mozilla.com/D170151

--- widget/GfxInfoBase.cpp
+++ widget/GfxInfoBase.cpp
@@ -1254,7 +1254,8 @@ bool GfxInfoBase::DoesDriverVendorMatch(const nsAString& aBlocklistVendor,
 }
 
 bool GfxInfoBase::IsFeatureAllowlisted(int32_t aFeature) const {
-  return aFeature == nsIGfxInfo::FEATURE_HW_DECODED_VIDEO_ZERO_COPY;
+  return aFeature == nsIGfxInfo::FEATURE_VIDEO_OVERLAY ||
+         aFeature == nsIGfxInfo::FEATURE_HW_DECODED_VIDEO_ZERO_COPY;
 }
 
 nsresult GfxInfoBase::GetFeatureStatusImpl(
