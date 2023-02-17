3c84c4738cb179e121048c2e40debdaf66702590
typeling1578 <typeling1578@gmail.com>
2023-02-18 00:04:02 +0900

    [FB] Floorp System | Set Firefox's user agent to that of Windows.

--- floorp/browser/extensions/webextensions/floorp-system/shared/webcompat_useragent.js
+++ floorp/browser/extensions/webextensions/floorp-system/shared/webcompat_useragent.js
@@ -1,4 +1,4 @@
-const FIREFOX_STABLE_UA = "Mozilla/5.0 (X11; Linux x86_64; rv:110.0) Gecko/20100101 Firefox/110.0";
+const FIREFOX_STABLE_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0";
 const CHROME_STABLE_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36";
 
 const WEBCOMPATS_USERAGENT = [
