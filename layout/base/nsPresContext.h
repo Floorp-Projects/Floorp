06583109014dcae27f24cda6a9e1fb4543e9ead4
Emilio Cobos Álvarez <emilio@crisal.io>
2023-02-17 21:15:06 +0000

    Bug 1812868 - Tweak RDM and theme setup. r=mstange
    This is in order to make nsPresContext::UseOverlayScrollbars() thread-safe for
    stylo usage.
    
    Document::GetBrowsingContext() does ref-counting of main-thread-only
    objects, and we don't want that.
    
    Similarly nsPresContext::Theme() mutates the pres context which is not
    something we can do from multiple threads.
    
    Reviewed in: https://phabricator.services.mozilla.com/D168148

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -573,6 +573,7 @@ class nsPresContext : public nsISupports, public mozilla::SupportsWeakPtr {
   }
   void SetFullZoom(float aZoom);
   void SetOverrideDPPX(float);
+  void SetInRDMPane(bool aInRDMPane);
 
  public:
   float GetFullZoom() { return mFullZoom; }
@@ -824,18 +825,7 @@ class nsPresContext : public nsISupports, public mozilla::SupportsWeakPtr {
    */
   uint32_t GetBidi() const;
 
-  /*
-   * Obtain a native theme for rendering our widgets (both form controls and
-   * html)
-   *
-   * Guaranteed to return non-null.
-   */
-  nsITheme* Theme() MOZ_NONNULL_RETURN {
-    if (MOZ_LIKELY(mTheme)) {
-      return mTheme;
-    }
-    return EnsureTheme();
-  }
+  nsITheme* Theme() const MOZ_NONNULL_RETURN;
 
   void RecomputeTheme();
 
@@ -1352,6 +1342,9 @@ class nsPresContext : public nsISupports, public mozilla::SupportsWeakPtr {
 
   unsigned mIsVisual : 1;
 
+  // Are we in the RDM pane?
+  unsigned mInRDMPane : 1;
+
   unsigned mHasWarnedAboutTooLargeDashedOrDottedRadius : 1;
 
   // Have we added quirk.css to the style set?
@@ -1383,7 +1376,7 @@ class nsPresContext : public nsISupports, public mozilla::SupportsWeakPtr {
 
   void LastRelease();
 
-  nsITheme* EnsureTheme();
+  void EnsureTheme();
 
 #ifdef DEBUG
  private:
