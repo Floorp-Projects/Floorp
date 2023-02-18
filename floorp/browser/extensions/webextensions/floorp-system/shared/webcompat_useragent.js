844af0a80d30fb521fc0071c7aeeef9b499c1051
typeling1578 <typeling1578@gmail.com>
2023-02-18 14:47:27 +0900

    [FB] Floorp System | Match the User-Agent to the platform on which it is running.

--- floorp/browser/extensions/webextensions/floorp-system/shared/webcompat_useragent.js
+++ floorp/browser/extensions/webextensions/floorp-system/shared/webcompat_useragent.js
@@ -1,7 +1,15 @@
 const FLOORP_WEBCOMPAT_ENABLED_PREF = "floorp.webcompat.enabled";
 
-const FIREFOX_STABLE_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0";
-const CHROME_STABLE_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36";
+const FIREFOX_STABLE_UA = {
+    "win": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0",
+    "linux": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0",
+    "mac": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/110.0",
+}
+const CHROME_STABLE_UA = {
+    "win": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+    "linux": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+    "mac": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+};
 
 const WEBCOMPATS_USERAGENT = [
     {
@@ -28,6 +36,7 @@ let enabled = browser.aboutConfigPrefs.getBoolPref(FLOORP_WEBCOMPAT_ENABLED_PREF
 browser.aboutConfigPrefs.onPrefChange.addListener(function() {
     enabled = browser.aboutConfigPrefs.getBoolPref(FLOORP_WEBCOMPAT_ENABLED_PREF);
 }, FLOORP_WEBCOMPAT_ENABLED_PREF);
+let platformInfo = browser.runtime.getPlatformInfo();
 
 for (let WEBCOMPAT_USERAGENT of WEBCOMPATS_USERAGENT) {
     if (WEBCOMPAT_USERAGENT.platforms.includes("all") ||
@@ -36,9 +45,10 @@ for (let WEBCOMPAT_USERAGENT of WEBCOMPATS_USERAGENT) {
             async function(e) {
                 if (!await enabled) return;
                 if (e.tabId === -1) return;
+                if (typeof WEBCOMPAT_USERAGENT["ua"][(await platformInfo).os] === "undefined") return;
                 for (let requestHeader of e.requestHeaders) {
                     if (requestHeader.name.toLowerCase() === "user-agent") {
-                        requestHeader.value = WEBCOMPAT_USERAGENT["ua"];
+                        requestHeader.value = WEBCOMPAT_USERAGENT["ua"][(await platformInfo).os];
                     }
                 }
                 return { requestHeaders: e.requestHeaders };
