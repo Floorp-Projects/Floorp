a236fc7b775a3f994e76e7f85eea1771909db49e
Ryosuke Asano <ryosukeusuusu@gmail.com>
2023-02-19 10:14:27 +0900

    [FB] Floorp System | Add UA override for "Bing.com"

--- floorp/browser/extensions/webextensions/floorp-system/shared/webcompat_useragent.js
+++ floorp/browser/extensions/webextensions/floorp-system/shared/webcompat_useragent.js
@@ -12,6 +12,13 @@
         "mac": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
     };
 
+    const MSEDGE_STABLE_UA = {
+        "win": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.49",
+        "linux": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.49",
+        "mac": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.49"
+    };
+
+
     const WEBCOMPATS_USERAGENT = [
         {
             "matches": [
@@ -29,6 +36,12 @@
                 "*://*.picnet8.jp/*",
             ],
             "ua": CHROME_STABLE_UA,
+        },
+        {
+            "matches": [
+                "*://bing.com/*", // This make Bing search engine's chat work.
+            ],
+            "ua": MSEDGE_STABLE_UA,
         },
     ];
 
