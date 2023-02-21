9c53358a0fa2a0ad317ae3dab559fb685be6b4c9
Marian-Vasile Laza <mlaza@mozilla.com>
2023-02-21 14:49:58 +0200

    Backed out changeset 37d1ee9374e0 (bug 1816957) for wpt failures on idlharness.https.any.worker.html. CLOSED TREE

--- dom/serviceworkers/test/test_serviceworker_interfaces.js
+++ dom/serviceworkers/test/test_serviceworker_interfaces.js
@@ -147,8 +147,6 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "Crypto",
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  "CryptoKey",
-  // IMPORTANT: Do not change this list without review from a DOM peer!
   "CustomEvent",
   // IMPORTANT: Do not change this list without review from a DOM peer!
   "Directory",
--- dom/workers/test/test_worker_interfaces.js
+++ dom/workers/test/test_worker_interfaces.js
@@ -152,8 +152,6 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "Crypto", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "CryptoKey" },
-  // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CustomEvent", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "DedicatedWorkerGlobalScope", insecureContext: true },
