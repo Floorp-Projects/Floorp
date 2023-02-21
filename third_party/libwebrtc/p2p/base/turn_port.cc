be55f9299593db5aa83434352b7cc2c493b44ce8
Andreas Pehrson <apehrson@mozilla.com>
2023-02-21 21:12:35 +0100

    Bug 1817024 - Vendor libwebrtc from 3c5a1bf8f3
    Upstream commit: https://webrtc.googlesource.com/src/+/3c5a1bf8f32a75a67d09262032f035af44500d91
        [TurnPort] Update CreateOrRefreshEntry function, step 2.
    
        This removes the deprecated method in favor of the new one
        introduced in
        https://webrtc-review.googlesource.com/c/src/+/279440
    
        Bug: chromium:1374310
        Change-Id: I662d5cad9a48d18d9425e4d2754a966df68f7056
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/279441
        Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
        Reviewed-by: Jonas Oreland <jonaso@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#38411}

--- third_party/libwebrtc/p2p/base/turn_port.cc
+++ third_party/libwebrtc/p2p/base/turn_port.cc
@@ -1207,11 +1207,6 @@ bool TurnPort::EntryExists(TurnEntry* e) {
   return absl::c_linear_search(entries_, e);
 }
 
-bool TurnPort::CreateOrRefreshEntry(const rtc::SocketAddress& addr,
-                                    int channel_number) {
-  return CreateOrRefreshEntry(addr, channel_number, "");
-}
-
 bool TurnPort::CreateOrRefreshEntry(Connection* conn, int channel_number) {
   return CreateOrRefreshEntry(conn->remote_candidate().address(),
                               channel_number,
