f60372530813291d52846fb0379d2d0099e022c0
Sean Feng <sefeng@mozilla.com>
2023-02-22 20:17:00 +0000

    Bug 1809518 - Use stencil parsing to do the Javascript check for ORB in Utility Process r=farre,smaug,tcampbell
    This patch allows JS Validator to parse the incoming data into
    stencil to verify if its a Javascript file.
    
    Differential Revision: https://phabricator.services.mozilla.com/D166484

--- dom/ipc/JSValidatorChild.cpp
+++ dom/ipc/JSValidatorChild.cpp
@@ -5,7 +5,17 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/dom/JSValidatorChild.h"
+#include "js/JSON.h"
+#include "mozilla/dom/JSOracleChild.h"
+
 #include "mozilla/ipc/Endpoint.h"
+
+#include "js/experimental/JSStencil.h"
+#include "js/SourceText.h"
+#include "js/Exception.h"
+#include "js/GlobalObject.h"
+#include "js/CompileOptions.h"
+#include "js/RealmOptions.h"
 
 using namespace mozilla::dom;
 
@@ -72,16 +82,42 @@ void JSValidatorChild::Resolve(ValidatorResult aResult) {
 }
 
 JSValidatorChild::ValidatorResult JSValidatorChild::ShouldAllowJS() const {
-  // mSourceBytes could be empty when
-  // 1. No OnDataAvailable calls
-  // 2. Failed to allocate shmem
-
   // The empty document parses as JavaScript, so for clarity we have a condition
   // separately for that.
   if (mSourceBytes.IsEmpty()) {
     return ValidatorResult::JavaScript;
   }
 
+  JSContext* cx = JSOracleChild::JSContext();
+  if (!cx) {
+    return ValidatorResult::Failure;
+  }
+
+  JSObject* global = JSOracleChild::JSObject();
+  if (!global) {
+    return ValidatorResult::Failure;
+  }
+
+  JS::SourceText<Utf8Unit> srcBuf;
+  if (!srcBuf.init(cx, mSourceBytes.BeginReading(), mSourceBytes.Length(),
+                   JS::SourceOwnership::Borrowed)) {
+    JS_ClearPendingException(cx);
+    return ValidatorResult::Failure;
+  }
+
+  JSAutoRealm ar(cx, global);
+
+  // Parse to JavaScript
+  RefPtr<JS::Stencil> stencil =
+      CompileGlobalScriptToStencil(cx, JS::CompileOptions(cx), srcBuf);
+
+  if (!stencil) {
+    JS_ClearPendingException(cx);
+    return ValidatorResult::Other;
+  }
+
+  // The stencil parsing lacks the JSON support, so we have to do the
+  // JSON parsing separately.
   if (StringBeginsWith(NS_ConvertUTF8toUTF16(mSourceBytes), u"{"_ns)) {
     return ValidatorResult::JSON;
   }
