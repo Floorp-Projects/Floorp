58d22da98a17052b08ed8f3ddd91ff6c21412423
Jon Coppeard <jcoppeard@mozilla.com>
2023-02-22 08:56:28 +0000

    Bug 1817757 - Part 1: Remove support for parallel marking from workers r=sfink
    Depends on D170374
    
    Differential Revision: https://phabricator.services.mozilla.com/D170375

--- js/src/gc/GC.cpp
+++ js/src/gc/GC.cpp
@@ -1042,7 +1042,8 @@ bool GCRuntime::setParameter(JSGCParamKey key, uint32_t value,
       compactingEnabled = value != 0;
       break;
     case JSGC_PARALLEL_MARKING_ENABLED:
-      parallelMarkingEnabled = value != 0;
+      // Not supported on workers.
+      parallelMarkingEnabled = rt->isMainRuntime() && value != 0;
       updateMarkersVector();
       break;
     case JSGC_INCREMENTAL_WEAKMAP_ENABLED:
