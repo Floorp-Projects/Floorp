a6d90e9080869708fd0f1ea598bbe0a94e628ffa
Greg Tatum <tatum.creative@gmail.com>
2023-02-23 21:54:59 +0000

    Bug 1813783 - Test the LTR and RTL behavior in about:translations; r=nordzilla
    Differential Revision: https://phabricator.services.mozilla.com/D168566

--- toolkit/components/translations/tests/browser/browser_about_translations.js
+++ toolkit/components/translations/tests/browser/browser_about_translations.js
@@ -219,3 +219,67 @@ add_task(async function test_about_translations_translations() {
     },
   });
 });
+
+add_task(async function test_about_translations_language_directions() {
+  await openAboutTranslations({
+    languagePairs: [
+      // English (en) is LTR and Arabic (ar) is RTL.
+      { fromLang: "en", toLang: "ar" },
+      { fromLang: "ar", toLang: "en" },
+    ],
+    runInPage: async ({ selectors }) => {
+      const { document, window } = content;
+
+      /** @type {HTMLSelectElement} */
+      const fromSelect = document.querySelector(selectors.fromLanguageSelect);
+      /** @type {HTMLSelectElement} */
+      const toSelect = document.querySelector(selectors.toLanguageSelect);
+      /** @type {HTMLTextAreaElement} */
+      const translationTextarea = document.querySelector(
+        selectors.translationTextarea
+      );
+      /** @type {HTMLDivElement} */
+      const translationResult = document.querySelector(
+        selectors.translationResult
+      );
+
+      fromSelect.value = "en";
+      fromSelect.dispatchEvent(new Event("input"));
+      toSelect.value = "ar";
+      toSelect.dispatchEvent(new Event("input"));
+      translationTextarea.value = "This text starts as LTR.";
+      translationTextarea.dispatchEvent(new Event("input"));
+
+      is(
+        window.getComputedStyle(translationTextarea).direction,
+        "ltr",
+        "The English input is LTR"
+      );
+      is(
+        window.getComputedStyle(translationResult).direction,
+        "rtl",
+        "The Arabic results are RTL"
+      );
+
+      toSelect.value = "";
+      toSelect.dispatchEvent(new Event("input"));
+      fromSelect.value = "ar";
+      fromSelect.dispatchEvent(new Event("input"));
+      toSelect.value = "en";
+      toSelect.dispatchEvent(new Event("input"));
+      translationTextarea.value = "This text starts as RTL.";
+      translationTextarea.dispatchEvent(new Event("input"));
+
+      is(
+        window.getComputedStyle(translationTextarea).direction,
+        "rtl",
+        "The Arabic input is RTL"
+      );
+      is(
+        window.getComputedStyle(translationResult).direction,
+        "ltr",
+        "The English results are LTR"
+      );
+    },
+  });
+});
