f4032fb46c6f4adf7bd0e60d49aa0c20963e80b2
Michael Froman <mfroman@mozilla.com>
2023-02-23 22:25:58 +0000

    Bug 1818386 - teach vendor-libwebrtc.py how to exclude specific files from vendoring. r=ng DONTBUILD
    In the past, we have deleted a group of unused java files in our github patch stack.
    That causes rebase conflicts when upstream edits any of those java files.  Teaching
    our vendoring script how to exclude those files will allow us to remove that patch
    from our patch stack (avoid rebase conflicts) while also not including them in our
    builds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D170815

--- dom/media/webrtc/third_party_build/vendor-libwebrtc.py
+++ dom/media/webrtc/third_party_build/vendor-libwebrtc.py
@@ -32,6 +32,31 @@ LIBWEBRTC_UNUSED_IN_FIREFOX = [
     "presubmit_test_mocks.py",
     "pylintrc",
     "style-guide.md",
+    # Only the camera code under sdk/android/api/org/webrtc is used, so
+    # we remove a bunch of other java files that aren't useful.
+    "sdk/android/api/org/webrtc/CandidatePairChangeEvent.java",
+    "sdk/android/api/org/webrtc/DefaultVideoEncoderFactory.java",
+    "sdk/android/api/org/webrtc/HardwareVideoEncoderFactory.java",
+    "sdk/android/api/org/webrtc/IceCandidate.java",
+    "sdk/android/api/org/webrtc/MediaStream.java",
+    "sdk/android/api/org/webrtc/NetworkChangeDetector.java",
+    "sdk/android/api/org/webrtc/NetworkChangeDetectorFactory.java",
+    "sdk/android/api/org/webrtc/NetworkControllerFactoryFactory.java",
+    "sdk/android/api/org/webrtc/NetworkMonitor.java",
+    "sdk/android/api/org/webrtc/NetworkMonitorAutoDetect.java",
+    "sdk/android/api/org/webrtc/NetworkStatePredictorFactoryFactory.java",
+    "sdk/android/api/org/webrtc/PeerConnection.java",
+    "sdk/android/api/org/webrtc/PeerConnectionDependencies.java",
+    "sdk/android/api/org/webrtc/PeerConnectionFactory.java",
+    "sdk/android/api/org/webrtc/RTCStats.java",
+    "sdk/android/api/org/webrtc/RTCStatsCollectorCallback.java",
+    "sdk/android/api/org/webrtc/RTCStatsReport.java",
+    "sdk/android/api/org/webrtc/RtcCertificatePem.java",
+    "sdk/android/api/org/webrtc/RtpParameters.java",
+    "sdk/android/api/org/webrtc/RtpReceiver.java",
+    "sdk/android/api/org/webrtc/RtpSender.java",
+    "sdk/android/api/org/webrtc/RtpTransceiver.java",
+    "sdk/android/src/java/org/webrtc/JniHelper.java",
 ]
 
 
@@ -144,9 +169,11 @@ def unpack(target):
         pass
     with tarfile.open(target_archive) as t:
         safe_extract(t, path=target_path)
-    libwebrtc_used_in_firefox = os.listdir(target_path)
 
     if target == "libwebrtc":
+        # use the top level directories from the tarfile and
+        # delete those directories in LIBWEBRTC_DIR
+        libwebrtc_used_in_firefox = os.listdir(target_path)
         for path in libwebrtc_used_in_firefox:
             try:
                 shutil.rmtree(os.path.join(LIBWEBRTC_DIR, path))
@@ -160,17 +187,26 @@ def unpack(target):
             # GitHub packs everything inside a separate directory
             target_path = os.path.join(target_path, os.listdir(target_path)[0])
 
-        # remove the exceptions we don't want to vendor into our tree
+        # remove the top level exceptions we don't want to vendor into our tree
         libwebrtc_used_in_firefox = [
             path
             for path in libwebrtc_used_in_firefox
             if (path not in LIBWEBRTC_UNUSED_IN_FIREFOX)
         ]
 
+        # move all the top level entries from the tarfile to LIBWEBRTC_DIR
         for path in libwebrtc_used_in_firefox:
             shutil.move(
                 os.path.join(target_path, path), os.path.join(LIBWEBRTC_DIR, path)
             )
+
+        # remove any non-top-level entries in LIBWEBRTC_UNUSED_IN_FIREFOX
+        for path in LIBWEBRTC_UNUSED_IN_FIREFOX:
+            try:
+                os.remove(os.path.join(LIBWEBRTC_DIR, path))
+            except OSError:
+                # we can ignore errors from trying to delete directories
+                pass
     elif target == "build":
         try:
             shutil.rmtree(os.path.join(LIBWEBRTC_DIR, "build"))
