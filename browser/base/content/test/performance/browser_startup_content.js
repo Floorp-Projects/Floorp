9bc153d0ffbdedcd27b24ad33320e12c50577e3f
Cosmin Sabou <csabou@mozilla.com>
2023-02-24 16:08:12 +0200

    Backed out 3 changesets (bug 1818026) for causing mochitest plain failures on test_SVGStyleElement.xhtml.
    Backed out changeset 808a73bbb614 (bug 1818026)
    Backed out changeset 9b7dbaeb6deb (bug 1818026)
    Backed out changeset 62e5f2fd2775 (bug 1818026)

--- browser/base/content/test/performance/browser_startup_content.js
+++ browser/base/content/test/performance/browser_startup_content.js
@@ -34,6 +34,7 @@ const known_scripts = {
     "resource:///actors/AboutReaderChild.sys.mjs",
     "resource:///actors/BrowserTabChild.sys.mjs",
     "resource:///actors/LinkHandlerChild.jsm",
+    "resource:///actors/PageStyleChild.jsm",
     "resource:///actors/SearchSERPTelemetryChild.jsm",
     "resource://gre/actors/ContentMetaChild.jsm",
     "resource://gre/modules/Readerable.jsm",
