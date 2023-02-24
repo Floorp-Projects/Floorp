c1e39616ff4e800e844ea1ea9c3b7b242c899f73
Andreas Pehrson <apehrson@mozilla.com>
2023-02-24 17:01:24 +0100

    Bug 1817024 - Vendor libwebrtc from c71b34235e
    Upstream commit: https://webrtc.googlesource.com/src/+/c71b34235eb0f1f8c7cd3b66a01bd003c1299e00
        [M109] Resolve TURN hostname specific to network family behind field trial.
    
        Already implemented for STUN hostname resolution, but TURN port resolves hostnames separately. Reusing the field trial key reserved in bugs.webrtc.org/14334 but with a new parameter so as to not affect ongoing rollouts.
    
        (cherry picked from commit def85594ea34dec2c1cd8fd58835430c224899c7)
    
        No-Try: True
        Bug: chromium:1385639, webrtc:14319, webrtc:14131
        Change-Id: Idf771fb2f0de7849f8b701be8ee05a98b8d242f3
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/285981
        Reviewed-by: Jonas Oreland <jonaso@webrtc.org>
        Commit-Queue: Sameer Vijaykar <samvi@google.com>
        Cr-Original-Commit-Position: refs/heads/main@{#38811}
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/287941
        Reviewed-by: Per Kjellander <perkj@webrtc.org>
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/branch-heads/5414@{#2}
        Cr-Branched-From: a3e51df5f3377bee71b4dcdd1ee8f50c520a9ea8-refs/heads/main@{#38608}

--- third_party/libwebrtc/p2p/base/turn_port.cc
+++ third_party/libwebrtc/p2p/base/turn_port.cc
@@ -26,12 +26,33 @@
 #include "rtc_base/async_packet_socket.h"
 #include "rtc_base/byte_order.h"
 #include "rtc_base/checks.h"
+#include "rtc_base/experiments/field_trial_parser.h"
 #include "rtc_base/logging.h"
 #include "rtc_base/net_helpers.h"
 #include "rtc_base/socket_address.h"
 #include "rtc_base/strings/string_builder.h"
 
 namespace cricket {
+
+namespace {
+
+bool ResolveTurnHostnameForFamily(const webrtc::FieldTrialsView& field_trials) {
+  // Bug fix for TURN hostname resolution on IPv6.
+  // Field trial key reserved in bugs.webrtc.org/14334
+  static constexpr char field_trial_name[] =
+      "WebRTC-IPv6NetworkResolutionFixes";
+  if (!field_trials.IsEnabled(field_trial_name)) {
+    return false;
+  }
+
+  webrtc::FieldTrialParameter<bool> resolve_turn_hostname_for_family(
+      "ResolveTurnHostnameForFamily", /*default_value=*/false);
+  webrtc::ParseFieldTrial({&resolve_turn_hostname_for_family},
+                          field_trials.Lookup(field_trial_name));
+  return resolve_turn_hostname_for_family;
+}
+
+}  // namespace
 
 using ::webrtc::SafeTask;
 using ::webrtc::TaskQueueBase;
@@ -798,7 +819,7 @@ void TurnPort::ResolveTurnAddress(const rtc::SocketAddress& address) {
   RTC_LOG(LS_INFO) << ToString() << ": Starting TURN host lookup for "
                    << address.ToSensitiveString();
   resolver_ = socket_factory()->CreateAsyncDnsResolver();
-  resolver_->Start(address, [this] {
+  auto callback = [this] {
     // If DNS resolve is failed when trying to connect to the server using TCP,
     // one of the reason could be due to DNS queries blocked by firewall.
     // In such cases we will try to connect to the server with hostname,
@@ -829,7 +850,13 @@ void TurnPort::ResolveTurnAddress(const rtc::SocketAddress& address) {
     }
     server_address_.address = resolved_address;
     PrepareAddress();
-  });
+  };
+  // TODO(bugs.webrtc.org/14733): remove duplicate resolution with STUN port.
+  if (ResolveTurnHostnameForFamily(field_trials())) {
+    resolver_->Start(address, Network()->family(), std::move(callback));
+  } else {
+    resolver_->Start(address, std::move(callback));
+  }
 }
 
 void TurnPort::OnSendStunPacket(const void* data,
