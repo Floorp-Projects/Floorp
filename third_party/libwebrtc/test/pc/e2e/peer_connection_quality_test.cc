e086256de5fd422c7aea117d642e46e914c58b14
Andreas Pehrson <apehrson@mozilla.com>
2023-02-24 12:49:19 +0100

    Bug 1817024 - Vendor libwebrtc from 2af96059a3
    Upstream commit: https://webrtc.googlesource.com/src/+/2af96059a314edcbffa07c147ce386759c0f5813
        [PCLF] Add infra metrics to the AnalyzingVideoSink
    
        Bug: b/240540204
        Change-Id: If3f5436d701336b0bc122477c61b97b5dc28f422
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/282001
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Commit-Queue: Artem Titov <titovartem@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#38561}

--- third_party/libwebrtc/test/pc/e2e/peer_connection_quality_test.cc
+++ third_party/libwebrtc/test/pc/e2e/peer_connection_quality_test.cc
@@ -473,7 +473,8 @@ void PeerConnectionE2EQualityTest::OnTrackCallback(
   // track->kind() is kVideoKind.
   auto* video_track = static_cast<VideoTrackInterface*>(track.get());
   std::unique_ptr<rtc::VideoSinkInterface<VideoFrame>> video_sink =
-      video_quality_analyzer_injection_helper_->CreateVideoSink(peer_name);
+      video_quality_analyzer_injection_helper_->CreateVideoSink(
+          peer_name, peer_subscription, /*report_infra_stats=*/false);
   video_track->AddOrUpdateSink(video_sink.get(), rtc::VideoSinkWants());
   output_video_sinks_.push_back(std::move(video_sink));
 }
