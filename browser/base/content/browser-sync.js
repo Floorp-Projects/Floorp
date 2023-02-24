10c8b70062810d15708e4d937de7a32d9d4ac3bd
Mike Conley <mconley@mozilla.com>
2023-02-24 22:04:03 +0000

    Bug 1818570 - Fix entrypoint for FxA toolbarbutton and include experiment / variation slugs when opening FxA page. r=Mardak,markh, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D170790

--- browser/base/content/browser-sync.js
+++ browser/base/content/browser-sync.js
@@ -9,6 +9,11 @@ const { UIState } = ChromeUtils.import("resource://services-sync/UIState.jsm");
 
 ChromeUtils.defineModuleGetter(
   this,
+  "ExperimentAPI",
+  "resource://nimbus/ExperimentAPI.jsm"
+);
+ChromeUtils.defineModuleGetter(
+  this,
   "FxAccounts",
   "resource://gre/modules/FxAccounts.jsm"
 );
@@ -799,9 +804,26 @@ var gSync = {
     let fxaStatus = document.documentElement.getAttribute("fxastatus");
 
     if (fxaStatus == "not_configured") {
-      this.openFxAEmailFirstPageFromFxaMenu(
-        PanelMultiView.getViewNode(document, "PanelUI-fxa")
-      );
+      let extraParams = {};
+      let fxaButtonVisibilityExperiment =
+        ExperimentAPI.getExperimentMetaData({
+          featureId: "fxaButtonVisibility",
+        }) ??
+        ExperimentAPI.getRolloutMetaData({
+          featureId: "fxaButtonVisibility",
+        });
+      if (fxaButtonVisibilityExperiment) {
+        extraParams = {
+          entrypoint_experiment: fxaButtonVisibilityExperiment.slug,
+          entrypoint_variation: fxaButtonVisibilityExperiment.branch.slug,
+        };
+      }
+
+      let panel =
+        anchor.id == "appMenu-fxa-label2"
+          ? PanelMultiView.getViewNode(document, "PanelUI-fxa")
+          : undefined;
+      this.openFxAEmailFirstPageFromFxaMenu(panel, extraParams);
       PanelUI.hide();
       return;
     }
@@ -1197,21 +1219,24 @@ var gSync = {
     }
   },
 
-  async openFxAEmailFirstPage(entryPoint) {
+  async openFxAEmailFirstPage(entryPoint, extraParams = {}) {
     if (!(await FxAccounts.canConnectAccount())) {
       return;
     }
-    const url = await FxAccounts.config.promiseConnectAccountURI(entryPoint);
+    const url = await FxAccounts.config.promiseConnectAccountURI(
+      entryPoint,
+      extraParams
+    );
     switchToTabHavingURI(url, true, { replaceQueryString: true });
   },
 
-  async openFxAEmailFirstPageFromFxaMenu(panel = undefined) {
+  async openFxAEmailFirstPageFromFxaMenu(panel = undefined, extraParams = {}) {
     this.emitFxaToolbarTelemetry("login", panel);
     let entryPoint = "fxa_discoverability_native";
     if (panel) {
       entryPoint = "fxa_app_menu";
     }
-    this.openFxAEmailFirstPage(entryPoint);
+    this.openFxAEmailFirstPage(entryPoint, extraParams);
   },
 
   async openFxAManagePage(entryPoint) {
