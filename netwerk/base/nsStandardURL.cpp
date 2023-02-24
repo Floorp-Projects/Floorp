5c6dc581a80ee5540d0191883eb8f9b09b7802fd
Paul Adenot <paul@paul.cx>
2023-02-24 14:41:13 +0000

    Bug 1733356 - Fix build, remove unused global variable in nsStandardURL.cpp. r=sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D170898

--- netwerk/base/nsStandardURL.cpp
+++ netwerk/base/nsStandardURL.cpp
@@ -50,7 +50,6 @@ namespace mozilla {
 namespace net {
 
 static NS_DEFINE_CID(kThisImplCID, NS_THIS_STANDARDURL_IMPL_CID);
-static NS_DEFINE_CID(kStandardURLCID, NS_STANDARDURL_CID);
 
 // This will always be initialized and destroyed on the main thread, but
 // can be safely used on other threads.
