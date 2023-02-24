416798e36047f3d092ce2366a6f946b3cb02c9a6
Andreas Pehrson <apehrson@mozilla.com>
2023-02-24 11:40:44 +0100

    Bug 1817024 - Vendor libwebrtc from 24e0337846
    Upstream commit: https://webrtc.googlesource.com/src/+/24e0337846f37a22d975fd4e2c9ec1d28f569ca1
        Make disable_ipv6 ABSL_DEPRECATED.
    
        // All tests pass, infra failure unrelated
        NOTRY=True
    
        Bug: webrtc:14608
        Change-Id: Ie16dcf9dc66e687f0befef42c7d8e914696af191
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/280760
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Henrik Boström <hbos@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#38502}

--- third_party/libwebrtc/pc/peer_connection.cc
+++ third_party/libwebrtc/pc/peer_connection.cc
@@ -297,7 +297,7 @@ bool PeerConnectionInterface::RTCConfiguration::operator==(
     RtcpMuxPolicy rtcp_mux_policy;
     std::vector<rtc::scoped_refptr<rtc::RTCCertificate>> certificates;
     int ice_candidate_pool_size;
-    bool disable_ipv6;
+    bool DEPRECATED_disable_ipv6;
     bool disable_ipv6_on_wifi;
     int max_ipv6_networks;
     bool disable_link_local_networks;
@@ -365,7 +365,8 @@ bool PeerConnectionInterface::RTCConfiguration::operator==(
          certificates == o.certificates &&
          prioritize_most_likely_ice_candidate_pairs ==
              o.prioritize_most_likely_ice_candidate_pairs &&
-         media_config == o.media_config && disable_ipv6 == o.disable_ipv6 &&
+         media_config == o.media_config &&
+         DEPRECATED_disable_ipv6 == o.DEPRECATED_disable_ipv6 &&
          disable_ipv6_on_wifi == o.disable_ipv6_on_wifi &&
          max_ipv6_networks == o.max_ipv6_networks &&
          disable_link_local_networks == o.disable_link_local_networks &&
@@ -2094,7 +2095,7 @@ PeerConnection::InitializePortAllocator_n(
                           cricket::PORTALLOCATOR_ENABLE_IPV6_ON_WIFI;
   // If the disable-IPv6 flag was specified, we'll not override it
   // by experiment.
-  if (configuration.disable_ipv6) {
+  if (configuration.DEPRECATED_disable_ipv6) {
     port_allocator_flags &= ~(cricket::PORTALLOCATOR_ENABLE_IPV6);
   } else if (trials().IsDisabled("WebRTC-IPv6Default")) {
     port_allocator_flags &= ~(cricket::PORTALLOCATOR_ENABLE_IPV6);
