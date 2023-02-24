8e5acf8a38509209e0f4375e798bf4d27924bb83
Bob Owen <bobowencode@gmail.com>
2023-02-24 08:44:42 +0000

    Bug 1772089 p6: Fix content sandbox file system test. r=haik
    The content temp dir is no longer defined for macOS and Windows.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168597

--- security/sandbox/test/browser_content_sandbox_utils.js
+++ security/sandbox/test/browser_content_sandbox_utils.js
@@ -297,12 +297,12 @@ function fileInHomeDir() {
 
 // Returns a file object for a new file in the content temp dir (.../<UUID>).
 function fileInTempDir() {
-  let contentTempKey = "ContentTmpD";
+  let contentTempKey = "TmpD";
 
   // get the content temp dir, make sure it exists
   let ctmp = Services.dirsvc.get(contentTempKey, Ci.nsIFile);
-  Assert.ok(ctmp.exists(), "Content temp dir exists");
-  Assert.ok(ctmp.isDirectory(), "Content temp dir is a directory");
+  Assert.ok(ctmp.exists(), "Temp dir exists");
+  Assert.ok(ctmp.isDirectory(), "Temp dir is a directory");
 
   // build a file object for a new file in content temp
   let tempFile = ctmp.clone();
