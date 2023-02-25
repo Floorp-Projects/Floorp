c2d19b0612b58c5b9252662e027c53633676d1bf
Csoregi Natalia <ncsoregi@mozilla.com>
2023-02-25 15:29:32 +0200

    Backed out 2 changesets (bug 1777849) for causing webgl crashes. CLOSED TREE
    Backed out changeset af3c076c00d6 (bug 1777849)
    Backed out changeset 536f32f1cf47 (bug 1777849)

--- widget/gtk/GfxInfo.h
+++ widget/gtk/GfxInfo.h
@@ -113,7 +113,6 @@ class GfxInfo final : public GfxInfoBase {
   bool mHasMultipleGPUs;
   bool mGlxTestError;
   bool mIsVAAPISupported;
-  bool mIsX11ThreadSafe;
 
   void AddCrashReportAnnotations();
 };
