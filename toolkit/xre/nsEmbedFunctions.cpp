dd7df2875530144f5ad5fe3d8d9170d3a200d07f
Yannis Juglaret <yjuglaret@mozilla.com>
2023-02-27 15:00:57 +0000

    Bug 1788004 - Implement a hook-based fallback for BCryptGenRandom to mitigate Rust panics. r=cmartin
    BCryptGenRandom can be broken, but the Rust stdlib and the getrandom
    crate rely on it, and this is a source of crashes which are Rust
    panics. This happens the most on Windows 7 after bcryptprimitives.dll
    fails to load (see bug 1788004).
    
    To mitigate these crashes, we hook BCryptGenRandom if we detect that it
    is broken, and install a fallback based on RtlGenRandom. We only protect
    calls that use BCRYPT_USE_SYSTEM_PREFERRED_RNG; so code that relies on
    using BCryptOpenAlgorithmProvider and doesn't have its own fallback can
    still fail.
    
    We will hopefully remove this hook when the Rust stdlib and the
    getrandom crate both have their own RtlGenRandom-based fallback.
    
    Differential Revision: https://phabricator.services.mozilla.com/D170662

--- toolkit/xre/nsEmbedFunctions.cpp
+++ toolkit/xre/nsEmbedFunctions.cpp
@@ -25,6 +25,7 @@
 #  endif
 #  include "mozilla/ScopeExit.h"
 #  include "mozilla/WinDllServices.h"
+#  include "mozilla/WindowsBCryptInitialization.h"
 #  include "WinUtils.h"
 #  ifdef ACCESSIBILITY
 #    include "mozilla/GeckoArgs.h"
@@ -534,24 +535,19 @@ nsresult XRE_InitChildProcess(int aArgc, char* aArgv[],
       break;
   }
 
-#if defined(MOZ_SANDBOX) && defined(XP_WIN)
+#if defined(XP_WIN)
+#  if defined(MOZ_SANDBOX)
   if (aChildData->sandboxBrokerServices) {
     SandboxBroker::Initialize(aChildData->sandboxBrokerServices);
     SandboxBroker::GeckoDependentInitialize();
   }
+#  endif  // defined(MOZ_SANDBOX)
 
-  // Call BCryptGenRandom() to pre-load bcryptPrimitives.dll while the current
-  // thread still has an unrestricted impersonation token. We need to perform
-  // that operation to warmup the BCryptGenRandom() call that is used by
-  // others, especially rust.  See bug 1746524, bug 1751094, bug 1751177
-  UCHAR buffer[32];
-  NTSTATUS status = BCryptGenRandom(NULL,            // hAlgorithm
-                                    buffer,          // pbBuffer
-                                    sizeof(buffer),  // cbBuffer
-                                    BCRYPT_USE_SYSTEM_PREFERRED_RNG  // dwFlags
-  );
-  MOZ_RELEASE_ASSERT(status == STATUS_SUCCESS);
-#endif  // defined(MOZ_SANDBOX) && defined(XP_WIN)
+  {
+    DebugOnly<bool> result = mozilla::WindowsBCryptInitialization();
+    MOZ_ASSERT(result);
+  }
+#endif  // defined(XP_WIN)
 
   {
     // This is a lexical scope for the MessageLoop below.  We want it
