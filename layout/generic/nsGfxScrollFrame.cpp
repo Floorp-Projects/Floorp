0ca9566fa0dcab34e50eb5b69bc826a5c63c1554
Emilio Cobos Álvarez <emilio@crisal.io>
2023-02-27 10:03:51 +0000

    Bug 1818927 - Remove XUL clickthrough attribute. r=dholbert
    It only has an effect on macOS, and it's only used for
    scrollbars/resizers/trees. We already hard-coded <browser>
    to behave as never so do the same consistently, see bug 1519905.
    
    This reduces noise when inspecting scrollbars (and it's simpler).
    
    No behavior change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D171011

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -5669,7 +5669,6 @@ already_AddRefed<Element> ScrollFrameHelper::MakeScrollbar(
 
   e->SetAttr(kNameSpaceID_None, nsGkAtoms::orient, kOrientValues[aVertical],
              false);
-  e->SetAttr(kNameSpaceID_None, nsGkAtoms::clickthrough, u"always"_ns, false);
 
   if (mIsRoot) {
     e->SetProperty(nsGkAtoms::docLevelNativeAnonymousContent,
@@ -5846,8 +5845,6 @@ nsresult ScrollFrameHelper::CreateAnonymousContent(
         NS_WARNING("only resizable types should have resizers");
     }
     mResizerContent->SetAttr(kNameSpaceID_None, nsGkAtoms::dir, dir, false);
-    mResizerContent->SetAttr(kNameSpaceID_None, nsGkAtoms::clickthrough,
-                             u"always"_ns, false);
     aElements.AppendElement(mResizerContent);
   }
 
