56581979cb5af9dd2b33a31a1610b2757ee09b3e
Sean Feng <sefeng@mozilla.com>
2023-02-28 15:28:01 +0000

    Bug 1809518 - Recreate JSOracleParent instance when Utility Process crashes r=farre
    Top-level actor can only be bound to one process, we can't reuse the
    existing one to bind to a different process.
    
    So if the utility process crashes and we've created a new one as a
    replacement, we need to have a new JSOracleParent for the connection,
    using the old one won't work.
    
    This patch creates a new one upon the destruction of the existing one
    if the reason for the destruction is AbnormalShutdown.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168482

--- dom/ipc/JSOracleParent.cpp
+++ dom/ipc/JSOracleParent.cpp
@@ -28,7 +28,16 @@ void JSOracleParent::WithJSOracle(
       });
 }
 
-void JSOracleParent::ActorDestroy(ActorDestroyReason aReason) {}
+void JSOracleParent::ActorDestroy(ActorDestroyReason aReason) {
+  // Given an actor can only be bound to one process,
+  // if the utility process crashes and we create a new one,
+  // we can't reuse the same JSOracleParent instance for it,
+  // so we always create a new JSOracleParent to replace
+  // the existing one.
+  if (aReason == ActorDestroyReason::AbnormalShutdown) {
+    sOracleSingleton = new JSOracleParent();
+  }
+}
 
 /* static */
 JSOracleParent* JSOracleParent::GetSingleton() {
