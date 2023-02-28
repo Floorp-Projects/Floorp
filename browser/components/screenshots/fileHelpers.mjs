1fcaac69184e0ccd9c67a6665a4e52e5d07ad9d4
Neil Deakin <neil@mozilla.com>
2023-02-28 18:57:24 +0000

    Bug 1815062, modify callers of DownloadPaths.sanitize so that some use the allowInvalidFilenames argument, r=Gijs
    Callers that use the filename to assign the default filename in a filepicker or save without a filepicker should use the default arguments, callers that validate a filename entered from a filepicker should supply false for compressWhitespaces, and true for allowInvalidFilenames
    
    Differential Revision: https://phabricator.services.mozilla.com/D171002

--- browser/components/screenshots/fileHelpers.mjs
+++ browser/components/screenshots/fileHelpers.mjs
@@ -154,7 +154,11 @@ function getMIMEInfoForType(aMIMEType, aExtension) {
 
 // This is only used after the user has entered a filename.
 function validateFileName(aFileName) {
-  let processed = lazy.DownloadPaths.sanitize(aFileName) || "_";
+  let processed =
+    lazy.DownloadPaths.sanitize(aFileName, {
+      compressWhitespaces: false,
+      allowInvalidFilenames: true,
+    }) || "_";
   if (AppConstants.platform == "android") {
     // If a large part of the filename has been sanitized, then we
     // will use a default filename instead
