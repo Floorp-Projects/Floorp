77e9fabc2619e06d751f72dd28e14dc20b6d088b
Kash Shampur <kshampur@mozilla.com>
2023-02-28 18:30:05 +0000

    Bug 1816421 - Disable mobile fission performance tests on all branches. r=perftest-reviewers,sparky
    This patch should disable fenix/geckoview *fission* performance tests only.
    The tests are still available to select through Try.
    
    Differential Revision: https://phabricator.services.mozilla.com/D171133

--- taskcluster/gecko_taskgraph/target_tasks.py
+++ taskcluster/gecko_taskgraph/target_tasks.py
@@ -785,10 +785,16 @@ def target_tasks_general_perf_testing(full_task_graph, parameters, graph_config)
                 return True
             # Select fenix resource usage tests
             if "fenix" in try_name:
+                # Bug 1816421 disable fission perf tests
+                if "-fis" in try_name:
+                    return False
                 if "-power" in try_name:
                     return True
             # Select geckoview resource usage tests
             if "geckoview" in try_name:
+                # Bug 1816421 disable fission perf tests
+                if "-fis" in try_name:
+                    return False
                 # Run cpu+memory, and power tests
                 cpu_n_memory_task = "-cpu" in try_name and "-memory" in try_name
                 power_task = "-power" in try_name
