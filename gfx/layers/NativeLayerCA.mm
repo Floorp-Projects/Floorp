b304366310e1d54d2e31d4d786a394fe271b00eb
Brad Werth <bwerth@mozilla.com>
2023-03-01 20:57:58 +0000

    Bug 1808155 Part 2: Make VIDEO_LOG note events that will cause the video layer to rebuild. r=mstange
    There are only a few things that could cause the video layer to be
    rebuilt:
    1) The mMutatedSpecializeVideo flag being set.
    2) A new layer object being created to host for the video.
    
    This patch more thoroughly documents instances of case 1 by adding
    VIDEO_LOG message to anything that sets the mMutatedSpecializeVideo flag.
    
    It also makes some attempt to identify case 2 by adding a VIDEO_LOG
    message whenever we destroy a layer that has ever displayed a video
    texture. If a log message like this is followed by a message that a new
    video layer is created, it is a strong signal that we are unexpectedly
    throwing away our external surface handles when we'd like to keep using
    them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D171301

--- gfx/layers/NativeLayerCA.mm
+++ gfx/layers/NativeLayerCA.mm
@@ -788,6 +788,11 @@ NativeLayerCA::NativeLayerCA(gfx::DeviceColor aColor)
 }
 
 NativeLayerCA::~NativeLayerCA() {
+#ifdef NIGHTLY_BUILD
+  if (mHasEverAttachExternalImage && StaticPrefs::gfx_core_animation_specialize_video_log()) {
+    NSLog(@"VIDEO_LOG: ~NativeLayerCA: %p is being destroyed after hosting video.", this);
+  }
+#endif
   if (mInProgressLockedIOSurface) {
     mInProgressLockedIOSurface->Unlock(false);
     mInProgressLockedIOSurface = nullptr;
@@ -829,6 +834,11 @@ void NativeLayerCA::AttachExternalImage(wr::RenderTextureHost* aExternalImage) {
   bool oldSpecializeVideo = mSpecializeVideo;
   mSpecializeVideo = ShouldSpecializeVideo(lock);
   bool changedSpecializeVideo = (mSpecializeVideo != oldSpecializeVideo);
+#ifdef NIGHTLY_BUILD
+  if (changedSpecializeVideo && StaticPrefs::gfx_core_animation_specialize_video_log()) {
+    NSLog(@"VIDEO_LOG: AttachExternalImage: %p is forcing a video layer rebuild.", this);
+  }
+#endif
 
   bool oldIsDRM = mIsDRM;
   mIsDRM = aExternalImage->IsFromDRMSource();
@@ -910,14 +920,25 @@ bool NativeLayerCA::ShouldSpecializeVideo(const MutexAutoLock& aProofOfLock) {
 }
 
 void NativeLayerCA::SetRootWindowIsFullscreen(bool aFullscreen) {
+  if (mRootWindowIsFullscreen == aFullscreen) {
+    return;
+  }
+
   MutexAutoLock lock(mMutex);
 
   mRootWindowIsFullscreen = aFullscreen;
 
   bool oldSpecializeVideo = mSpecializeVideo;
   mSpecializeVideo = ShouldSpecializeVideo(lock);
+  bool changedSpecializeVideo = (mSpecializeVideo != oldSpecializeVideo);
 
-  if (mSpecializeVideo != oldSpecializeVideo) {
+  if (changedSpecializeVideo) {
+#ifdef NIGHTLY_BUILD
+    if (StaticPrefs::gfx_core_animation_specialize_video_log()) {
+      NSLog(@"VIDEO_LOG: SetRootWindowIsFullscreen: %p is forcing a video layer rebuild.", this);
+    }
+#endif
+
     ForAllRepresentations([&](Representation& r) { r.mMutatedSpecializeVideo = true; });
   }
 }
@@ -1648,6 +1669,11 @@ bool NativeLayerCA::Representation::ApplyChanges(
   if (mWrappingCALayer && mMutatedSpecializeVideo) {
     // Since specialize video changes the way we construct our wrapping and content layers,
     // we have to scrap them if this value has changed.
+#ifdef NIGHTLY_BUILD
+    if (aIsVideo && StaticPrefs::gfx_core_animation_specialize_video_log()) {
+      NSLog(@"VIDEO_LOG: Scrapping existing video layer.");
+    }
+#endif
     [mContentCALayer release];
     mContentCALayer = nil;
     [mOpaquenessTintLayer release];
