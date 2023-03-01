75eeb39d7fd968082fb7a9018e7f618bbc63b2c4
Mike Kaply <mozilla@kaply.com>
2023-03-01 20:52:55 +0000

    Bug 1812339 - Always use /etc directory on Linux for policy. r=stransky, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D167860

--- toolkit/components/enterprisepolicies/EnterprisePoliciesParent.sys.mjs
+++ toolkit/components/enterprisepolicies/EnterprisePoliciesParent.sys.mjs
@@ -529,8 +529,12 @@ class JSONPoliciesProvider {
     let configFile = null;
 
     if (AppConstants.platform == "linux" && AppConstants.MOZ_SYSTEM_POLICIES) {
-      let systemConfigFile = Services.dirsvc.get("SysConfD", Ci.nsIFile);
-      systemConfigFile.append("policies");
+      let systemConfigFile = Cc["@mozilla.org/file/local;1"].createInstance(
+        Ci.nsIFile
+      );
+      systemConfigFile.initWithPath(
+        "/etc/" + Services.appinfo.name.toLowerCase() + "/policies"
+      );
       systemConfigFile.append(POLICIES_FILENAME);
       if (systemConfigFile.exists()) {
         return systemConfigFile;
