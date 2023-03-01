5d0b234e0abee8c7d47878f1107b395af6b06e6c
Bastien Abadie <abadie@teklia.com>
2023-03-01 15:20:26 +0000

    Bug 1819101 - Add JSON summary to taskgraph diff output with warnings support, r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D171111

---
+++ taskcluster/scripts/misc/summarize-tgdiff.py
@@ -0,0 +1,45 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import argparse
+import json
+import pathlib
+
+
+def run():
+
+    parser = argparse.ArgumentParser(
+        description="Classify output of taskgraph for CI analsyis"
+    )
+    parser.add_argument(
+        "path",
+        type=pathlib.Path,
+        help="Folder containing all the TXT files from taskgraph target.",
+    )
+    parser.add_argument(
+        "threshold",
+        type=int,
+        help="Minimum number of lines to trigger a warning on taskgraph output.",
+    )
+    args = parser.parse_args()
+
+    out = {"files": {}, "status": "OK", "threshold": args.threshold}
+    for path in args.path.glob("*.txt"):
+
+        with path.open() as f:
+            nb = len(list(filter(None, f.readlines())))
+
+        out["files"][path.stem] = {
+            "nb": nb,
+            "status": "WARNING" if nb >= args.threshold else "OK",
+        }
+
+        if nb >= args.threshold:
+            out["status"] = "WARNING"
+
+    (args.path / "summary.json").write_text(json.dumps(out, sort_keys=True, indent=4))
+
+
+if __name__ == "__main__":
+    run()
