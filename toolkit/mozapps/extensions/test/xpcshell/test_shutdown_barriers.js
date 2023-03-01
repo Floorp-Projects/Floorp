4f7c6781b52a54f060120300df62cbc61b5a6a9c
Jens Stutte <jstutte@mozilla.com>
2023-03-01 17:57:16 +0000

    Bug 1814104 - Do not shutdown XPIProvider before it is fully started. r=rpl, a=dmeehan
    If the parent process is very short-living, we can find us shutting down
    while the startup had not yet finished. This seems to confuse things up
    to the point to have a shutdown hang. There seems to be no easy/reliable
    way to interrupt startup, so we should just wait for it to finish.
    Note that we can just use the already present `quitApplicationGranted`
    blocker as earliest possible shutdown phase to block.
    
    Differential Revision: https://phabricator.services.mozilla.com/D170382

--- toolkit/mozapps/extensions/test/xpcshell/test_shutdown_barriers.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_shutdown_barriers.js
@@ -3,9 +3,16 @@
  */
 "use strict";
 
-add_task(async function test_shutdown_barriers() {
-  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "66");
+XPCOMUtils.defineLazyModuleGetters(this, {
+  Management: "resource://gre/modules/Extension.jsm",
+  XPIProvider: "resource://gre/modules/addons/XPIProvider.jsm",
+});
 
+AddonTestUtils.init(this);
+AddonTestUtils.overrideCertDB();
+createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "42");
+
+add_task(async function test_shutdown_barriers() {
   await promiseStartupManager();
 
   const ID = "thing@xpcshell.addons.mozilla.org";
@@ -68,4 +75,39 @@ add_task(async function test_shutdown_barriers() {
     2,
     "Both shutdown blockers ran before manager shutdown completed"
   );
+});
+
+// Regression test for Bug 1814104.
+add_task(async function test_wait_addons_startup_before_granting_quit() {
+  await promiseStartupManager();
+
+  const extensions = [];
+  for (let i = 0; i < 20; i++) {
+    extensions.push(
+      ExtensionTestUtils.loadExtension({
+        useAddonManager: "permanent",
+        background() {},
+        manifest: {
+          browser_specific_settings: {
+            gecko: { id: `test-extension-${i}@mozilla` },
+          },
+        },
+      })
+    );
+  }
+
+  info("Wait for all test extension to have been started once");
+  await Promise.all(extensions.map(ext => ext.startup()));
+  await promiseShutdownManager();
+
+  info("Test early shutdown while enabled addons are still being started");
+
+  Management.on("startup", (_evt, extension) => {
+    ok(
+      !XPIProvider._closing,
+      `Unxpected addon startup for "${extension.id}" after XPIProvider have been closed and shutting down`
+    );
+  });
+  promiseStartupManager();
+  await promiseShutdownManager();
 });
