59fbf1585c555cd85a936d3be353424851d08998
Barret Rennie <barret@brennie.ca>
2023-03-02 05:30:46 +0000

    Bug 1802551 - Port osfile.jsm usage to IOUtils in TelemetryStorage.jsm r=chutten
    Now that IO is all handled by IOUtils, TelemetryControllerParent needs to use
    the IOUtils-specific `sendTelemetry` shutdown client to ensure its IO can run
    during shutdown.
    
    Differential Revision: https://phabricator.services.mozilla.com/D170773

--- toolkit/components/telemetry/app/TelemetryControllerParent.sys.mjs
+++ toolkit/components/telemetry/app/TelemetryControllerParent.sys.mjs
@@ -886,7 +886,7 @@ var Impl = {
       this._testMode ? 0 : undefined
     );
 
-    AsyncShutdown.sendTelemetry.addBlocker(
+    IOUtils.sendTelemetry.addBlocker(
       "TelemetryController: shutting down",
       () => this.shutdown(),
       () => this._getState()
