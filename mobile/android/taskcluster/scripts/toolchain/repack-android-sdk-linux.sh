83bf5d2128851666670cf6e81592c060bea7dcb8
Christian Sadilek <christian.sadilek@gmail.com>
2023-03-02 14:10:24 -0500

    Bug 1819989 - Remove JDK8 from base docker image
    Only use JDK8 for android-build image, which is used by our
    external gradle dependecies tasks. Nexus still requires Java8.

--- mobile/android/taskcluster/scripts/toolchain/repack-android-sdk-linux.sh
+++ mobile/android/taskcluster/scripts/toolchain/repack-android-sdk-linux.sh
@@ -2,12 +2,11 @@
 
 export ANDROID_SDK_ROOT=$MOZ_FETCHES_DIR
 
-# For the Android build system we want Java 11. However this version of sdkmanager requires Java 8.
-JAVA8PATH="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/:$PATH"
+JAVA11PATH="/usr/lib/jvm/java-11-openjdk-amd64/bin/:$PATH"
 
-yes | PATH=$JAVA8PATH "${ANDROID_SDK_ROOT}/tools/bin/sdkmanager" --licenses
+yes | PATH=$JAVA11PATH "${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager" --licenses --sdk_root="${ANDROID_SDK_ROOT}"
 
 # It's nice to have the build logs include the state of the world upon completion.
-PATH=$JAVA8PATH "${ANDROID_SDK_ROOT}/tools/bin/sdkmanager" --list
+PATH=$JAVA11PATH "${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager" --list --sdk_root="${ANDROID_SDK_ROOT}"
 
 tar cf - -C "$ANDROID_SDK_ROOT" . --transform 's,^\./,android-sdk-linux/,' | xz > "$UPLOAD_DIR/android-sdk-linux.tar.xz"
