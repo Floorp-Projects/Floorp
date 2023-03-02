0c3c16d11a21d42325a3cb301eb88bda99e60783
Andrew McCreight <continuation@gmail.com>
2023-03-02 23:08:50 +0000

    Bug 1819952 - Give some WebRTC Destroy runnables better names. r=bwc
    Differential Revision: https://phabricator.services.mozilla.com/D171486

--- dom/media/webrtc/transport/nr_socket_prsock.cpp
+++ dom/media/webrtc/transport/nr_socket_prsock.cpp
@@ -1096,8 +1096,8 @@ void NrUdpSocketIpc::Destroy() {
   // destroy_i also dispatches back to STS to call ReleaseUse, to avoid shutting
   // down the IO thread before close() runs.
   // We use a NonOwning runnable because our refcount has already gone to 0.
-  io_thread_->Dispatch(
-      NewNonOwningRunnableMethod(__func__, this, &NrUdpSocketIpc::destroy_i));
+  io_thread_->Dispatch(NewNonOwningRunnableMethod(
+      "NrUdpSocketIpc::Destroy", this, &NrUdpSocketIpc::destroy_i));
 #endif
 }
 
