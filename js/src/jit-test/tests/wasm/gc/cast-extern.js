603038ae207b9901cff332130a78f624bde728ce
Ryan Hunt <rhunt@eqrion.net>
2023-03-03 20:28:23 +0000

    Bug 1817782 - wasm: Test that we're only casting WasmGcObject. r=jseward
    The casting instructions currently assume that the input anyref is
    just a WasmGcObject (either struct or array). However any JS Value
    can become an anyref through extern.internalize. These values do
    not have a typedef field and must checked for while casting.
    
    This adds extra bloat to our casting instructions that we should
    find a way to remove. Two options:
      1. Move the typedef from the object to the shape. This would
         add an extra indirection to casting though.
      2. Box non-WasmGcObject's in a WasmStructObject when extern.
         internalize is called.
    
    Differential Revision: https://phabricator.services.mozilla.com/D170708

---
+++ js/src/jit-test/tests/wasm/gc/cast-extern.js
@@ -0,0 +1,54 @@
+// |jit-test| skip-if: !wasmGcEnabled()
+
+// Casting an external value is an expected failure
+let { refCast, refTest, brOnCast, brOnCastFail } = wasmEvalText(`
+  (module
+    (; give this struct a unique identity to avoid conflict with
+       the struct type defined in wasm.js ;)
+    (type (struct i64 i32 i64 i32))
+
+    (func (export "refTest") (param externref) (result i32)
+      local.get 0
+      extern.internalize
+      ref.test 0
+    )
+    (func (export "refCast") (param externref) (result i32)
+      local.get 0
+      extern.internalize
+      ref.cast 0
+      drop
+      i32.const 0
+    )
+    (func (export "brOnCast") (param externref) (result i32)
+      (block (result (ref 0))
+        local.get 0
+        extern.internalize
+        br_on_cast 0 0
+        drop
+        i32.const 0
+        br 1
+      )
+      drop
+      i32.const 1
+    )
+    (func (export "brOnCastFail") (param externref) (result i32)
+      (block (result anyref)
+        local.get 0
+        extern.internalize
+        br_on_cast_fail 0 0
+        drop
+        i32.const 1
+        br 1
+      )
+      drop
+      i32.const 0
+    )
+  )
+`).exports;
+
+for (let v of WasmNonNullExternrefValues) {
+  assertErrorMessage(() => refCast(v), WebAssembly.RuntimeError, /bad cast/);
+  assertEq(refTest(v), 0);
+  assertEq(brOnCast(v), 0);
+  assertEq(brOnCastFail(v), 0);
+}
