7a1d1d37a46631ebf916589a82ca6fd9c8292248
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2023-03-03 11:58:30 +0000

    Bug 1819445 - fix logging for string exceptions, r=willdurand
    Differential Revision: https://phabricator.services.mozilla.com/D171458

--- testing/mochitest/tests/SimpleTest/SimpleTest.js
+++ testing/mochitest/tests/SimpleTest/SimpleTest.js
@@ -26,14 +26,21 @@ let isSameOrigin = function(w) {
 };
 let isXOrigin = !isSameOrigin(window);
 
+// Note: duplicated in browser-test.js . See also bug 1820150.
 function isErrorObj(err) {
   // It'd be nice if we had either `Error.isError(err)` or `Error.isInstance(err)`
   // but we don't, so do it ourselves:
   if (!err) {
     return false;
   }
-  let glob = SpecialPowers.Cu.getGlobalForObject(err);
-  return err instanceof glob.Error;
+  try {
+    let glob = SpecialPowers.Cu.getGlobalForObject(err);
+    return err instanceof glob.Error;
+  } catch {
+    // getGlobalForObject can be upset if it doesn't get passed an object.
+    // Just do a standard instanceof check using this global and cross fingers:
+  }
+  return err instanceof Error;
 }
 
 // In normal test runs, the window that has a TestRunner in its parent is
@@ -2139,7 +2146,10 @@ var add_task = (function() {
           } catch (ex) {
             try {
               let serializedEx;
-              if (isErrorObj(ex)) {
+              if (
+                typeof ex == "string" ||
+                (typeof ex == "object" && isErrorObj(ex))
+              ) {
                 serializedEx = `${ex}`;
               } else {
                 serializedEx = JSON.stringify(ex);
