d9fd22feaadade07cf520761fd498c0db2d4d982
Lee Salzman <lsalzman@mozilla.com>
2023-03-05 03:11:08 +0000

    Bug 1777849 - Require HW-WR for Accelerated Canvas2D. r=jrmuizel, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D171638

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -3134,6 +3134,11 @@ static void AcceleratedCanvas2DPrefChangeCallback(const char*, void*) {
   if (kIsAndroid && !gfxConfig::IsEnabled(Feature::GPU_PROCESS)) {
     feature.Disable(FeatureStatus::Blocked, "Disabled by GPU Process disabled",
                     "FEATURE_FAILURE_DISABLED_BY_GPU_PROCESS_DISABLED"_ns);
+  } else if (!gfxConfig::IsEnabled(Feature::WEBRENDER)) {
+    // There isn't much benefit to accelerating Canvas2D if we can't accelerate
+    // WebRender itself.
+    feature.Disable(FeatureStatus::Blocked, "Disabled by Software WebRender",
+                    "FEATURE_FAILURE_DISABLED_BY_SOFTWARE_WEBRENDER"_ns);
   }
 
   // Check if blocklisted despite the default pref.
