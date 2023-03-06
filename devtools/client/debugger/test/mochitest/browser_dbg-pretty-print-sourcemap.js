ad25c358f850a997f308983ab6d0a10da6e64f4d
Alexandre Poirot <poirot.alex@gmail.com>
2023-03-06 11:37:12 +0000

    Bug 1818494 - [devtools] Make source actor objects immutable. r=devtools-reviewers,bomsy
    Move off from source actor the mutable "sourceMapURL" attribute.
    Instead have a mutable Set to store the source actors relating to a valid source map.
    
    This will be important as we start sharing source actor objects
    and so it is important to have them be immutable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D170880

--- devtools/client/debugger/test/mochitest/browser_dbg-pretty-print-sourcemap.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-pretty-print-sourcemap.js
@@ -88,9 +88,8 @@ add_task(async () => {
   // The source may be reported as pretty printable while we are fetching the sourcemap,
   // but once the sourcemap is reported to be failing, we no longer report to be pretty printable.
   await waitFor(
-    () =>
-      dbg.selectors.getSourceActorsForSource(source1.id)[0].sourceMapURL === "",
-    "Wait for the selector source to clear its sourceMapURL"
+    () => !dbg.selectors.isSourceWithMap(source1.id),
+    "Wait for the selector to report the source to be source-map less"
   );
 
   assertPrettyPrintButton(dbg, L10N.getStr("sourceTabs.prettyPrint"), false);
