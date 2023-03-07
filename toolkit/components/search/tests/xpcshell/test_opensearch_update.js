f8d2724ce0c0db86be36366bddd7e7aeeb83e33e
Mark Banner <standard8@mozilla.com>
2023-03-07 09:34:25 +0000

    Bug 1819093 - Rework test_opensearch_update.js to avoid having to manually set an update URL. r=mcheang
    Differential Revision: https://phabricator.services.mozilla.com/D171325

--- toolkit/components/search/tests/xpcshell/test_opensearch_update.js
+++ toolkit/components/search/tests/xpcshell/test_opensearch_update.js
@@ -5,31 +5,41 @@
 
 "use strict";
 
+const KEYWORD = "keyword";
+
 add_task(async function setup() {
-  useHttpServer();
+  let server = useHttpServer("");
+  server.registerContentType("sjs", "sjs");
   await AddonTestUtils.promiseStartupManager();
   await Services.search.init();
 });
 
-add_task(async function test_engineUpdate() {
-  const KEYWORD = "keyword";
-  const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
-
+add_task(async function test_installEngine() {
+  const engineData = {
+    baseURL: gDataUrl,
+    name: "original engine",
+    method: "GET",
+    updateFile: "opensearch/simple.xml",
+  };
   let engine = await SearchTestUtils.promiseNewSearchEngine({
-    url: `${gDataUrl}engine.xml`,
+    url: `${gDataUrl}data/engineMaker.sjs?${JSON.stringify(engineData)}`,
   });
 
   engine.alias = KEYWORD;
   await Services.search.moveEngine(engine, 0);
 
   Assert.ok(
-    !Services.search.getEngineByName("A second test engine"),
+    !!Services.search.getEngineByName("original engine"),
+    "Should be able to get the engine by the original name"
+  );
+  Assert.ok(
+    !Services.search.getEngineByName("simple"),
     "Should not be able to get the engine by the new name"
   );
+});
 
-  // can't have an accurate updateURL in the file since we can't know the test
-  // server origin, so manually set it
-  engine.wrappedJSObject._updateURL = gDataUrl + "engine2.xml";
+add_task(async function test_engineUpdate() {
+  const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
 
   let promiseUpdate = SearchTestUtils.promiseSearchNotification(
     SearchUtils.MODIFIED_TYPE.CHANGED,
@@ -38,30 +48,27 @@ add_task(async function test_engineUpdate() {
 
   // set last update to 8 days ago, since the default interval is 7, then
   // trigger an update
+  let engine = Services.search.getEngineByName("original engine");
   engine.wrappedJSObject.setAttr("updateexpir", Date.now() - ONE_DAY_IN_MS * 8);
   Services.search.QueryInterface(Ci.nsITimerCallback).notify(null);
 
-  let changedEngine = await promiseUpdate;
+  await promiseUpdate;
 
-  Assert.equal(
-    engine.name,
-    "A second test engine",
-    "Should have update the engines name"
-  );
+  Assert.equal(engine.name, "simple", "Should have updated the engine's name");
 
-  Assert.equal(changedEngine.alias, KEYWORD, "Keyword not cleared by update");
+  Assert.equal(engine.alias, KEYWORD, "Should have kept the keyword");
   Assert.equal(
-    changedEngine.wrappedJSObject.getAttr("order"),
+    engine.wrappedJSObject.getAttr("order"),
     1,
-    "Order not cleared by update"
+    "Should have kept the order"
   );
 
   Assert.ok(
-    !!Services.search.getEngineByName("A second test engine"),
+    !!Services.search.getEngineByName("simple"),
     "Should be able to get the engine by the new name"
   );
   Assert.ok(
-    !Services.search.getEngineByName("Test search engine"),
+    !Services.search.getEngineByName("original engine"),
     "Should not be able to get the engine by the old name"
   );
 });
