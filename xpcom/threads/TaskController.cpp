4f432961ae5f26c17f28a7cfa995e9fd6ee87df3
Florian Quèze <florian@queze.net>
2023-03-07 12:37:03 +0000

    Bug 1817498 - Unlock the TaskController mGraphMutex mutex before calling into BackgroundHangThread APIs, r=bas.
    Differential Revision: https://phabricator.services.mozilla.com/D171260

--- xpcom/threads/TaskController.cpp
+++ xpcom/threads/TaskController.cpp
@@ -467,7 +467,14 @@ void TaskController::ProcessPendingMTTask(bool aMayWait) {
       break;
     }
 
-    BackgroundHangMonitor().NotifyWait();
+#ifdef MOZ_ENABLE_BACKGROUND_HANG_MONITOR
+    // Unlock before calling into the BackgroundHangMonitor API as it uses
+    // the timer API.
+    {
+      MutexAutoUnlock unlock(mGraphMutex);
+      BackgroundHangMonitor().NotifyWait();
+    }
+#endif
 
     {
       // ProcessNextEvent will also have attempted to wait, however we may have
@@ -477,7 +484,12 @@ void TaskController::ProcessPendingMTTask(bool aMayWait) {
       mMainThreadCV.Wait();
     }
 
-    BackgroundHangMonitor().NotifyActivity();
+#ifdef MOZ_ENABLE_BACKGROUND_HANG_MONITOR
+    {
+      MutexAutoUnlock unlock(mGraphMutex);
+      BackgroundHangMonitor().NotifyActivity();
+    }
+#endif
   }
 
   if (mMayHaveMainThreadTask) {
