cc7167e6e359bf028d587e624fb1662cd7fc44b8
Hubert Boma Manilla <hmanilla@mozilla.com>
2023-03-07 16:26:41 +0000

    Bug 1790542 - [devtools] Convert browser_source_map-reload.js test from using .sjs files to createTestHTTPServer r=ochameau,devtools-reviewers
    .sjs files depend on redirects to load static files.
    In D168648 we block redirects on sourceMappingUrl, so this browser_source_map-reload.js
    fails because we use .sjs to serve the content.
    
    This patch refactors to no longer use .sjs files
    
    Differential Revision: https://phabricator.services.mozilla.com/D169739

---
+++ devtools/client/framework/test/reload/v2/code_reload_2.js
@@ -0,0 +1,10 @@
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+function f() {
+  console.log("The second version of the script");
+}
+
+f();
