d62d1259bbcd082310770b2d2214df0ff65d8d87
oana.horvath <ohorvath@mozilla.com>
2023-03-08 14:12:55 +0200

    Bug 1821024 - Disable blocking openDownloadedFileTest

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/DownloadTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/DownloadTest.kt
@@ -7,6 +7,7 @@ package org.mozilla.fenix.ui
 import androidx.core.net.toUri
 import org.junit.After
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.customannotations.SmokeTest
@@ -166,6 +167,7 @@ class DownloadTest {
         }
     }
 
+    @Ignore("failing, see https://bugzilla.mozilla.org/show_bug.cgi?id=1821024")
     @SmokeTest
     @Test
     fun openDownloadedFileTest() {
