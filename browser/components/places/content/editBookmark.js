2b5c052fd8cfdadf992a0e4f96d986c06e2b8546
Jonathan Sudiaman <jsudiaman@mozilla.com>
2023-03-08 13:34:34 +0000

    Bug 1820349 - Intermittently, the editbookmark dialogue won't appear when the star button is clicked r=mak
    Avoid breaking if `window.arguments[0]` is `null`.
    
    https://treeherder.mozilla.org/jobs?repo=try&revision=4875199ca6018fd8c9557b90c3d1ebda75765488
    
    Differential Revision: https://phabricator.services.mozilla.com/D171758

--- browser/components/places/content/editBookmark.js
+++ browser/components/places/content/editBookmark.js
@@ -428,7 +428,9 @@ var gEditItemOverlay = {
       }
 
       this._bookmarkState = this.makeNewStateObject({
+        children: aInfo.node?.children,
         index: aInfo.node?.index,
+        isFolder: aInfo.node != null && PlacesUtils.nodeIsFolder(aInfo.node),
       });
       if (isBookmark || bulkTagging) {
         await this._initAllTags();
@@ -656,12 +658,6 @@ var gEditItemOverlay = {
       if (this._paneInfo.isBookmark) {
         options.tags = this._element("tagsField").value;
         options.keyword = this._keyword;
-      }
-
-      const dialogInfo = window.arguments[0];
-      if (typeof dialogInfo === "object" && dialogInfo.type === "folder") {
-        options.isFolder = true;
-        options.children = dialogInfo.URIList;
       }
 
       if (this._paneInfo.bulkTagging) {
