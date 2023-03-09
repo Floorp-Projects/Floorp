293d6ce17a8c28ad0491cd2d49a152dda9a569b0
Emilio Cobos Álvarez <emilio@crisal.io>
2023-03-09 11:43:41 +0000

    Bug 1817219 - Remove ContentUrlOnImageContent telemetry. r=layout-reviewers,tnikkel
    Bug 1484928 is fixed, we should've removed this then.
    
    Differential Revision: https://phabricator.services.mozilla.com/D172106

--- layout/generic/nsImageFrame.h
+++ layout/generic/nsImageFrame.h
@@ -321,8 +321,6 @@ class nsImageFrame : public nsAtomicContainerFrame, public nsIReflowCallback {
   nsRect PredictedDestRect(const nsRect& aFrameContentBox);
 
  private:
-  void MaybeRecordContentUrlOnImageTelemetry();
-
   // random helpers
   inline void SpecToURI(const nsAString& aSpec, nsIURI** aURI);
 
