972f76028cdc862cf8029dd4cc70901d07e752f3
ganna <annhermy@gmail.com>
2023-03-13 16:13:19 +0000

    Bug 1821159 - Convert browser/actors/PointerLock* JSMs to ESMs. r=kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D172090

--- browser/actors/PointerLockChild.sys.mjs
+++ browser/actors/PointerLockChild.sys.mjs
@@ -1,11 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-"use strict";
 
-var EXPORTED_SYMBOLS = ["PointerLockChild"];
-
-class PointerLockChild extends JSWindowActorChild {
+export class PointerLockChild extends JSWindowActorChild {
   handleEvent(event) {
     switch (event.type) {
       case "MozDOMPointerLock:Entered":
