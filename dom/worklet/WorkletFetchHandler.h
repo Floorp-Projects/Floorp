a8ac1e579240a3f843c248366432cbbca1bd465a
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2023-03-13 22:59:35 +0000

    Bug 1572644 - Part 1-1: Move WorkletFetchHandler to its own file. r=jonco
    This is a refactoring to move WorkletFetchHandler to its own file, so
    later other classes can include its header.
    
    Differential Revision: https://phabricator.services.mozilla.com/D166539

---
+++ dom/worklet/WorkletFetchHandler.h
@@ -0,0 +1,66 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_WorkletFetchHandler_h
+#define mozilla_dom_WorkletFetchHandler_h
+
+#include "mozilla/dom/PromiseNativeHandler.h"
+#include "nsIStreamLoader.h"
+
+namespace mozilla::dom {
+class Worklet;
+struct WorkletOptions;
+
+class WorkletFetchHandler final : public PromiseNativeHandler,
+                                  public nsIStreamLoaderObserver {
+ public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+
+  static already_AddRefed<Promise> Fetch(Worklet* aWorklet, JSContext* aCx,
+                                         const nsAString& aModuleURL,
+                                         const WorkletOptions& aOptions,
+                                         ErrorResult& aRv);
+
+  virtual void ResolvedCallback(JSContext* aCx, JS::Handle<JS::Value> aValue,
+                                ErrorResult& aRv) override;
+  NS_IMETHOD
+  OnStreamComplete(nsIStreamLoader* aLoader, nsISupports* aContext,
+                   nsresult aStatus, uint32_t aStringLen,
+                   const uint8_t* aString) override;
+
+  virtual void RejectedCallback(JSContext* aCx, JS::Handle<JS::Value> aValue,
+                                ErrorResult& aRv) override;
+
+  const nsCString& URL() const { return mURL; }
+
+  void ExecutionFailed(nsresult aRv);
+
+  void ExecutionSucceeded();
+
+ private:
+  WorkletFetchHandler(Worklet* aWorklet, const nsACString& aURL,
+                      Promise* aPromise);
+
+  ~WorkletFetchHandler() = default;
+
+  void AddPromise(Promise* aPromise);
+
+  void RejectPromises(nsresult aResult);
+
+  void ResolvePromises();
+
+  RefPtr<Worklet> mWorklet;
+  nsTArray<RefPtr<Promise>> mPromises;
+
+  enum { ePending, eRejected, eResolved } mStatus;
+
+  nsresult mErrorStatus;
+
+  nsCString mURL;
+};
+
+}  // namespace mozilla::dom
+#endif  // mozilla_dom_WorkletFetchHandler_h
