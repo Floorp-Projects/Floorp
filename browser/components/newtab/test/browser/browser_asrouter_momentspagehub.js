42e4196b345b828f094c12b7e9d01cfcb8d09d55
Russell Chan <rchan@mozilla.com>
2023-03-13 16:30:14 +0000

    Bug 1817531 - Prevent some ASRouter tests from unecessarily repeatedly reloading messages r=omc-reviewers,aminomancer
    Differential Revision: https://phabricator.services.mozilla.com/D171487

--- browser/components/newtab/test/browser/browser_asrouter_momentspagehub.js
+++ browser/components/newtab/test/browser/browser_asrouter_momentspagehub.js
@@ -44,11 +44,12 @@ add_task(async function test_with_rs_messages() {
   );
   // Reload the provider
   await ASRouter._updateMessageProviders();
+  await ASRouter.loadMessagesFromAllProviders();
   // Wait to load the WNPanel messages
-  await BrowserTestUtils.waitForCondition(async () => {
-    await ASRouter.loadMessagesFromAllProviders();
-    return ASRouter.state.messages.length > initialMessageCount;
-  }, "Messages did not load");
+  await BrowserTestUtils.waitForCondition(
+    () => ASRouter.state.messages.length > initialMessageCount,
+    "Messages did not load"
+  );
 
   await MomentsPageHub.messageRequest({
     triggerId: "momentsUpdate",
@@ -79,10 +80,11 @@ add_task(async function test_with_rs_messages() {
 
   let prevLength = ASRouter.state.messages.length;
   // Wait to load the messages
-  await BrowserTestUtils.waitForCondition(async () => {
-    await ASRouter.loadMessagesFromAllProviders();
-    return ASRouter.state.messages.length > prevLength;
-  }, "Messages did not load");
+  await ASRouter.loadMessagesFromAllProviders();
+  await BrowserTestUtils.waitForCondition(
+    () => ASRouter.state.messages.length > prevLength,
+    "Messages did not load"
+  );
 
   await MomentsPageHub.messageRequest({
     triggerId: "momentsUpdate",
@@ -103,10 +105,11 @@ add_task(async function test_with_rs_messages() {
   await client.db.clear();
   // Wait to reset the WNPanel messages from state
   const previousMessageCount = ASRouter.state.messages.length;
-  await BrowserTestUtils.waitForCondition(async () => {
-    await ASRouter.loadMessagesFromAllProviders();
-    return ASRouter.state.messages.length < previousMessageCount;
-  }, "ASRouter messages should have been removed");
+  await ASRouter.loadMessagesFromAllProviders();
+  await BrowserTestUtils.waitForCondition(
+    () => ASRouter.state.messages.length < previousMessageCount,
+    "ASRouter messages should have been removed"
+  );
   await SpecialPowers.popPrefEnv();
   // Reload the provider
   await ASRouter._updateMessageProviders();
