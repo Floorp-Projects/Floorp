ae6d734d0abe60303485c61e619a05bff0a4e6a0
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-03-13 11:26:03 +0000

    Bug 1821302 - Convert toolkit/components/corroborator/ to ES modules updated r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D172237

--- toolkit/components/corroborator/Corroborate.sys.mjs
+++ toolkit/components/corroborator/Corroborate.sys.mjs
@@ -2,9 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { XPCOMUtils } = ChromeUtils.importESModule(
-  "resource://gre/modules/XPCOMUtils.sys.mjs"
-);
+import { XPCOMUtils } from "resource://gre/modules/XPCOMUtils.sys.mjs";
 
 const lazy = {};
 
@@ -12,12 +10,10 @@ XPCOMUtils.defineLazyServiceGetters(lazy, {
   gCertDB: ["@mozilla.org/security/x509certdb;1", "nsIX509CertDB"],
 });
 
-var EXPORTED_SYMBOLS = ["Corroborate"];
-
 /**
  * Tools for verifying internal files in Mozilla products.
  */
-const Corroborate = {
+export const Corroborate = {
   async init() {},
 
   /**
