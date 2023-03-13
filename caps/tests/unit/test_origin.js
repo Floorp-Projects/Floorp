76e21b89d0a289892ccf2284537870ec46e9eb16
june wilde <jewilde@mozilla.com>
2023-03-13 14:52:04 +0000

    Bug 1776755 - Skip ordering ExpandedPrincipals to speed up creation; r=nika,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D161450

--- caps/tests/unit/test_origin.js
+++ caps/tests/unit/test_origin.js
@@ -127,7 +127,7 @@ function run_test() {
   // nsEP origins should be in lexical order.
   Assert.equal(
     ep.origin,
-    `[Expanded Principal [${exampleOrg.origin}, ${exampleCom.origin}, ${nullPrin.origin}]]`
+    `[Expanded Principal [${exampleCom.origin}, ${nullPrin.origin}, ${exampleOrg.origin}]]`
   );
 
   // Make sure createContentPrincipal does what the rest of gecko does.
