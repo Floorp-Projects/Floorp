5d6d52e17919fa1beb345da087898ecd793416fc
YUKI "Piro" Hiroshi <yuki@clear-code.com>
2023-03-14 09:38:29 +0000

    Bug 1811326 - Place windows larger than screen inside screen always r=willdurand
    Differential Revision: https://phabricator.services.mozilla.com/D170156

--- browser/components/extensions/test/browser/browser_ext_windows_update.js
+++ browser/components/extensions/test/browser/browser_ext_windows_update.js
@@ -273,13 +273,21 @@ add_task(async function testPositionBoundaryCheck() {
         left: screen.availWidth * 100,
         top: screen.availHeight * 100,
       });
-      await browser.test.sendMessage("too-large");
+      await browser.test.sendMessage("too-large-coordinates");
       await waitMessage();
       await browser.windows.update(win.id, {
         left: -screen.availWidth * 100,
         top: -screen.availHeight * 100,
       });
-      await browser.test.sendMessage("too-small");
+      await browser.test.sendMessage("too-small-coordinates");
+      await waitMessage();
+      await browser.windows.update(win.id, {
+        left: 0,
+        top: 0,
+        width: screen.availWidth + 10,
+        height: screen.availHeight + 10,
+      });
+      await browser.test.sendMessage("too-large-size");
     },
   });
 
@@ -348,16 +356,24 @@ add_task(async function testPositionBoundaryCheck() {
   };
 
   extension.sendMessage("continue");
-  await extension.awaitMessage("too-large");
+  await extension.awaitMessage("too-large-coordinates");
   actualRect.right = win.screenX + win.outerWidth;
   actualRect.bottom = win.screenY + win.outerHeight;
 
   extension.sendMessage("continue");
-  await extension.awaitMessage("too-small");
+  await extension.awaitMessage("too-small-coordinates");
   actualRect.top = win.screenY;
   actualRect.left = win.screenX;
 
   isRectContained(actualRect, maxRect);
+
+  extension.sendMessage("continue");
+  await extension.awaitMessage("too-large-size");
+  is(
+    `${win.screenX},${win.screenY}`,
+    "0,0",
+    "window larger than available area of the screen is aligned at top-left of the screen"
+  );
 
   await extension.unload();
   await BrowserTestUtils.closeWindow(win);
