340ed0946d2d9448151460dbee3e40a05b9c04db
Yulia <ystartsev@mozilla.com>
2023-03-14 18:16:33 +0000

    Bug 1540913 - Part 7: Add test to verify correct behavior of script loader shutdown; r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D171861

---
+++ dom/workers/test/dynamicImport_nested.mjs
@@ -0,0 +1,8 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+await import("./dynamicImport_postMessage.mjs");
+
+export const message = "first";
