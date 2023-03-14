c8fd0473789b9925d8aaa0b36c662a5100011f13
ganna <annhermy@gmail.com>
2023-03-14 18:30:22 +0000

    Bug 1821162 - Convert SwitchDocumentDirectionChild.jsm and PromptParent.jsm in browser/actors to ESMs r=kpatenio,bnasar
    Differential Revision: https://phabricator.services.mozilla.com/D172153

--- browser/actors/SwitchDocumentDirectionChild.sys.mjs
+++ browser/actors/SwitchDocumentDirectionChild.sys.mjs
@@ -2,11 +2,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-"use strict";
 
-var EXPORTED_SYMBOLS = ["SwitchDocumentDirectionChild"];
-
-class SwitchDocumentDirectionChild extends JSWindowActorChild {
+export class SwitchDocumentDirectionChild extends JSWindowActorChild {
   receiveMessage(message) {
     if (message.name == "SwitchDocumentDirection") {
       let docShell = this.manager.browsingContext.docShell;
