0eb16e5599d41c4b7a0096758e1ea5ee940a1882
Yulia <ystartsev@mozilla.com>
2023-03-14 18:16:33 +0000

    Bug 1540913 - Part 7: Add test to verify correct behavior of script loader shutdown; r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D171861

---
+++ dom/workers/test/dynamicImport_worker.js
@@ -0,0 +1,16 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+onmessage = function(event) {
+  switch (event.data) {
+    case "start":
+      import("./dynamicImport_nested.mjs").then(m => postMessage(m.message));
+      import("./dynamicImport_immediate.mjs").then(m => postMessage(m.message));
+      break;
+    default:
+      throw new Error("Bad message: " + event.data);
+      break;
+  }
+};
