973c0d03c3e6607d9af7c03c9e8e35c97b2e5b63
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-03-14 08:55:01 +0000

    Bug 1636599 - Generate artifact for reviewbot from devtools-node-test-runner.js . r=jdescottes,ahal,andi,devtools-reviewers.
    Differential Revision: https://phabricator.services.mozilla.com/D171534

--- devtools/client/bin/devtools-node-test-runner.js
+++ devtools/client/bin/devtools-node-test-runner.js
@@ -15,10 +15,12 @@
  */
 
 const { execFileSync } = require("child_process");
+const { writeFileSync } = require("fs");
 const { chdir } = require("process");
 const path = require("path");
-
 const os = require("os");
+
+const REPOSITORY_ROOT = __dirname.replace("devtools/client/bin", "");
 
 // All Windows platforms report "win32", even for 64bit editions.
 const isWin = os.platform() === "win32";
@@ -89,38 +91,119 @@ function execOut(...args) {
   return { out: out.toString(), err: err && err.toString() };
 }
 
-function getErrors(suite, out, err) {
+function getErrors(suite, out, err, testPath) {
   switch (SUITES[suite].type) {
     case TEST_TYPES.JEST:
       return getJestErrors(out, err);
     case TEST_TYPES.TYPESCRIPT:
-      return getTypescriptErrors(out, err);
+      return getTypescriptErrors(out, err, testPath);
     default:
       throw new Error("Unsupported suite type: " + SUITES[suite].type);
   }
 }
+
+const JEST_ERROR_SUMMARY_REGEX = /\s●\s/;
 
 function getJestErrors(out, err) {
   // The string out has extra content before the JSON object starts.
   const jestJsonOut = out.substring(out.indexOf("{"), out.lastIndexOf("}") + 1);
   const results = JSON.parse(jestJsonOut);
 
-  // The individual failing tests are jammed into the same message string :/
-  return results.testResults.reduce((p, testResult) => {
-    const failures = testResult.message
-      .split("\n")
-      .filter(l => l.includes("●"));
-    return p.concat(failures);
-  }, []);
+  /**
+   * We don't have individual information, but a multiple line string in testResult.message,
+   * which looks like
+   *
+   *  ● Simple function
+   *
+   *        expect(received).toEqual(expected) // deep equality
+   *
+   *        Expected: false
+   *        Received: true
+   *
+   *          391 |       url: "test.js",
+   *          392 |     });
+   *        > 393 |     expect(true).toEqual(false);
+   *              |                  ^
+   *          394 |     expect(actual.code).toMatchSnapshot();
+   *          395 |
+   *          396 |     const smc = await new SourceMapConsumer(actual.map.toJSON());
+   *
+   *          at Object.<anonymous> (src/workers/pretty-print/tests/prettyFast.spec.js:393:18)
+   *          at asyncGeneratorStep (src/workers/pretty-print/tests/prettyFast.spec.js:7:103)
+   *          at _next (src/workers/pretty-print/tests/prettyFast.spec.js:9:194)
+   *          at src/workers/pretty-print/tests/prettyFast.spec.js:9:364
+   *          at Object.<anonymous> (src/workers/pretty-print/tests/prettyFast.spec.js:9:97)
+   *
+   */
+
+  const errors = [];
+  for (const testResult of results.testResults) {
+    if (testResult.status != "failed") {
+      continue;
+    }
+    let currentError;
+    let errorLine;
+
+    const lines = testResult.message.split("\n");
+    lines.forEach((line, i) => {
+      if (line.match(JEST_ERROR_SUMMARY_REGEX) || i == lines.length - 1) {
+        // This is the name of the test, if we were gathering information from a previous
+        // error, we add it to the errors
+        if (currentError) {
+          errors.push({
+            // The file should be relative from the repository
+            file: testResult.name.replace(REPOSITORY_ROOT, ""),
+            line: errorLine,
+            // we don't have information for the column
+            column: 0,
+            message: currentError.trim(),
+          });
+        }
+
+        // Handle the new error
+        currentError = line;
+      } else {
+        // We put any line that is not a test name in the error message as it may be
+        // valuable for the user.
+        currentError += "\n" + line;
+
+        // The actual line of the error is marked with " > XXX |"
+        const res = line.match(/> (?<line>\d+) \|/);
+        if (res) {
+          errorLine = parseInt(res.groups.line, 10);
+        }
+      }
+    });
+  }
+
+  return errors;
 }
 
-function getTypescriptErrors(out, err) {
+function getTypescriptErrors(out, err, testPath) {
+  console.log(out);
   // Typescript error lines look like:
   //   popup/panel.jsm.js(103,7): error TS2531: Object is possibly 'null'.
   // Which means:
   //   {file_path}({line},{col}): error TS{error_code}: {message}
-  const tsErrorRegex = /error TS\d+\:/;
-  return out.split("\n").filter(l => tsErrorRegex.test(l));
+  const tsErrorRegex = /(?<file>(\w|\/|\.)+)\((?<line>\d+),(?<column>\d+)\): (?<message>error TS\d+\:.*)/;
+  const errors = [];
+  for (const line of out.split("\n")) {
+    const res = line.match(tsErrorRegex);
+    if (!res) {
+      continue;
+    }
+    // TypeScript gives us the path from the directory the command is executed in, so we
+    // need to prepend the directory path.
+    const fileAbsPath = testPath + res.groups.file;
+    errors.push({
+      // The file should be relative from the repository.
+      file: fileAbsPath.replace(REPOSITORY_ROOT, ""),
+      line: parseInt(res.groups.line, 10),
+      column: parseInt(res.groups.column, 10),
+      message: res.groups.message.trim(),
+    });
+  }
+  return errors;
 }
 
 function runTests() {
@@ -144,6 +227,10 @@ function runTests() {
     );
     return false;
   }
+
+  const artifactArg = process.argv.find(arg => arg.includes("artifact="));
+  const artifactFilePath = artifactArg && artifactArg.split("=")[1];
+  const artifactErrors = {};
 
   const failedSuites = [];
   const suites = suite == "all" ? SUITES : { [suite]: SUITES[suite] };
@@ -180,15 +267,34 @@ function runTests() {
     }
 
     console.log("[devtools-node-test-runner] Parse errors from the test logs");
-    const errors = getErrors(suiteName, out, err) || [];
+    const errors = getErrors(suiteName, out, err, testPath) || [];
     if (errors.length) {
       failedSuites.push(suiteName);
     }
     for (const error of errors) {
+      if (!artifactErrors[error.file]) {
+        artifactErrors[error.file] = [];
+      }
+      artifactErrors[error.file].push({
+        path: error.file,
+        line: error.line,
+        column: error.column,
+        level: "error",
+        message: error.message,
+        analyzer: suiteName,
+      });
+
       console.log(
-        `TEST-UNEXPECTED-FAIL | ${suiteData.type} | ${suiteName} | ${error}`
+        `TEST-UNEXPECTED-FAIL | ${suiteData.type} | ${suiteName} | ${error.file}:${error.line}: ${error.message}`
       );
     }
+  }
+
+  if (artifactFilePath) {
+    console.log(
+      `[devtools-node-test-runner] Writing artifact to ${artifactFilePath}`
+    );
+    writeFileSync(artifactFilePath, JSON.stringify(artifactErrors, null, 2));
   }
 
   const success = failedSuites.length === 0;
