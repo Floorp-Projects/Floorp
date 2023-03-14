1e611134636fe55792f2253a7f6b587d965c4c51
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2023-03-14 23:13:15 +0000

    Bug 1804249 - Add telemetry for audio decoding process type r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D163951

---
+++ ipc/glue/test/browser/browser_audio_telemetry_rdd.js
@@ -0,0 +1,26 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* import-globals-from head-telemetry.js */
+
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/ipc/glue/test/browser/head-telemetry.js",
+  this
+);
+
+add_setup(async function testNoTelemetry() {
+  await Telemetry.clearScalars();
+});
+
+add_task(async function testAudioDecodingInRDD() {
+  await runTest({ expectUtility: false, expectRDD: true });
+});
+
+add_task(async function testRDDTelemetry() {
+  const codecs = ["vorbis", "mp3", "aac", "flac"];
+  await verifyNoTelemetryForProcess("utility", codecs);
+  // TODO: Bug 1804291
+  // await verifyTelemetryForProcess("rdd", codecs);
+});
