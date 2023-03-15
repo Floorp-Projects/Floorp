e32aa3fe6a9b98d13821e097815cd1f7f9486eed
Erich Gubler <erichdongubler@gmail.com>
2023-03-15 20:13:45 +0000

    Bug 1720941, part 1: build(ci): integrate WebGPU CTS into CI r=webgpu-reviewers,taskgraph-reviewers,jmaher,jimb
    Vendor in WebGPU CTS so that we can run it against our implementation of WebGPU. This patch includes:
    
    1. Some linting configuration tweaks, to silence issues that weren't straightforward to fix in CTS upstream.
    2. Some WPT runner configuration to enable preferences, and skip MacOS testing altogether (since it's not targeted by our current work).
    3. A new Rust binary crate that, when run from a development environment:
        1. Creates a vendored copy of WebGPU CTS in `<gecko>/dom/webgpu/tests/cts/checkout/`.
        2. Generates private Web Platform Tests (see also `<gecko>/testing/web-platform/docs/index.rst`). To do this, we:
            1. Use upstream test generation via `npm` scripts, which creates a single `cts.https.html` file with thousands of variants.
            2. Chunk the `cts.https.html` file into parts that Taskcluster can distribute without timing out individual jobs. IMO, this is the most likely part of these changes to need further iteration, because:
                1. We currently naively divide tests by number of variant, and have made no effort to empirically prove that we're dividing execution time of chunks.
                2. There is currently no stability in the distribution of tests per chunk. Test variants are essentially a flattened list of the tree structure used to organize tests. Adding a few tests that end up being placed in the middle of the list can cause the chunk with new tests _and all subsequent chunks_ to have their contents changed, which will probably cause a disproportionate number of line changes to review.:
            3. Fix `script` tag(s) so they actually work with the WPT test runner for private tests (viz., in the `testing/web-platform/mozilla` directory).
    
    The output of the new vendoring binary is designed make the above steps transparent. N.B. that no actual vendoring in of files has happened yet; the next patch will add the results of running this script.
    
    Differential Revision: https://phabricator.services.mozilla.com/D169951

---
+++ dom/webgpu/tests/cts/vendor/src/fs.rs
@@ -0,0 +1,310 @@
+use std::{
+    ffi::OsStr,
+    fmt::{self, Display},
+    fs,
+    ops::Deref,
+    path::{Path, PathBuf, StripPrefixError},
+};
+
+use miette::{ensure, Context, IntoDiagnostic};
+
+#[derive(Debug)]
+pub(crate) struct FileRoot {
+    nickname: &'static str,
+    path: PathBuf,
+}
+
+impl FileRoot {
+    pub(crate) fn new<P>(nickname: &'static str, path: P) -> miette::Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref();
+        Ok(Self {
+            nickname,
+            path: dunce::canonicalize(path)
+                .map_err(miette::Report::msg)
+                .wrap_err_with(|| format!("failed to canonicalize {path:?}"))?,
+        })
+    }
+
+    pub(crate) fn nickname(&self) -> &str {
+        self.nickname
+    }
+
+    pub(crate) fn try_child<P>(&self, path: P) -> Result<Child<'_>, StripPrefixError>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref();
+        if path.is_absolute() {
+            path.strip_prefix(&self.path)?;
+        }
+        Ok(Child {
+            root: self,
+            path: self.path.join(path),
+        })
+    }
+
+    #[track_caller]
+    pub(crate) fn child<P>(&self, path: P) -> Child<'_>
+    where
+        P: AsRef<Path>,
+    {
+        self.try_child(path)
+            .into_diagnostic()
+            .wrap_err("invariant violation: `path` is absolute and not a child of this file root")
+            .unwrap()
+    }
+
+    fn removed_dir<P>(&self, path: P) -> miette::Result<Child<'_>>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref();
+        let child = self.child(path);
+        if child.exists() {
+            log::info!("removing old contents of {child}…",);
+            log::trace!("removing directory {:?}", &*child);
+            fs::remove_dir_all(&*child)
+                .map_err(miette::Report::msg)
+                .wrap_err_with(|| format!("failed to remove old contents of {child}"))?;
+        }
+        Ok(child)
+    }
+
+    fn removed_file<P>(&self, path: P) -> miette::Result<Child<'_>>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref();
+        let child = self.child(path);
+        if child.exists() {
+            log::info!("removing old copy of {child}…",);
+            fs::remove_file(&*child)
+                .map_err(miette::Report::msg)
+                .wrap_err_with(|| format!("failed to remove old copy of {child}"))?;
+        }
+        Ok(child)
+    }
+
+    pub(crate) fn regen_dir<P>(
+        &self,
+        path: P,
+        gen: impl FnOnce(&Child<'_>) -> miette::Result<()>,
+    ) -> miette::Result<Child<'_>>
+    where
+        P: AsRef<Path>,
+    {
+        let child = self.removed_dir(path)?;
+        gen(&child)?;
+        ensure!(
+            child.is_dir(),
+            "{} was not regenerated for an unknown reason",
+            child,
+        );
+        Ok(child)
+    }
+
+    pub(crate) fn regen_file<P>(
+        &self,
+        path: P,
+        gen: impl FnOnce(&Child<'_>) -> miette::Result<()>,
+    ) -> miette::Result<Child<'_>>
+    where
+        P: AsRef<Path>,
+    {
+        let child = self.removed_file(path)?;
+        gen(&child)?;
+        ensure!(
+            child.is_file(),
+            "{} was not regenerated for an unknown reason",
+            child,
+        );
+        Ok(child)
+    }
+}
+
+impl Deref for FileRoot {
+    type Target = Path;
+
+    fn deref(&self) -> &Self::Target {
+        &self.path
+    }
+}
+
+impl AsRef<Path> for FileRoot {
+    fn as_ref(&self) -> &Path {
+        &self.path
+    }
+}
+
+impl AsRef<OsStr> for FileRoot {
+    fn as_ref(&self) -> &OsStr {
+        self.path.as_os_str()
+    }
+}
+
+impl Display for FileRoot {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Self { nickname, path } = self;
+        write!(f, "`{}` (AKA `<{nickname}>`)", path.display())
+    }
+}
+
+pub(crate) struct Child<'a> {
+    root: &'a FileRoot,
+    /// NOTE: This is always an absolute path that is a child of the `root`.
+    path: PathBuf,
+}
+
+impl Child<'_> {
+    pub(crate) fn relative_path(&self) -> &Path {
+        let Self { root, path } = self;
+        path.strip_prefix(root).unwrap()
+    }
+
+    pub(crate) fn try_child<P>(&self, path: P) -> Result<Self, StripPrefixError>
+    where
+        P: AsRef<Path>,
+    {
+        let child_path = path.as_ref();
+        let Self { root, path } = self;
+
+        if child_path.is_absolute() {
+            child_path.strip_prefix(path)?;
+        }
+        Ok(Child {
+            root,
+            path: path.join(child_path),
+        })
+    }
+
+    #[track_caller]
+    pub(crate) fn child<P>(&self, path: P) -> Child<'_>
+    where
+        P: AsRef<Path>,
+    {
+        self.try_child(path)
+            .into_diagnostic()
+            .wrap_err("invariant violation: `path` is absolute and not a child of this child")
+            .unwrap()
+    }
+}
+
+impl Deref for Child<'_> {
+    type Target = Path;
+
+    fn deref(&self) -> &Self::Target {
+        &self.path
+    }
+}
+
+impl AsRef<Path> for Child<'_> {
+    fn as_ref(&self) -> &Path {
+        &self.path
+    }
+}
+
+impl AsRef<OsStr> for Child<'_> {
+    fn as_ref(&self) -> &OsStr {
+        self.path.as_os_str()
+    }
+}
+
+impl Display for Child<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "`<{}>{}{}`",
+            self.root.nickname(),
+            std::path::MAIN_SEPARATOR,
+            self.relative_path().display()
+        )
+    }
+}
+
+pub(crate) fn existing_file<P>(path: P) -> P
+where
+    P: AsRef<Path>,
+{
+    let p = path.as_ref();
+    assert!(p.is_file(), "{p:?} does not exist as a file");
+    path
+}
+
+pub(crate) fn copy_dir<P, Q>(source: P, dest: Q) -> miette::Result<()>
+where
+    P: Display + AsRef<Path>,
+    Q: Display + AsRef<Path>,
+{
+    log::debug!(
+        "copy-merging directories from {} into {}",
+        source.as_ref().display(),
+        dest.as_ref().display(),
+    );
+    ::dircpy::copy_dir(&source, &dest)
+        .into_diagnostic()
+        .wrap_err_with(|| format!("failed to copy files from {source} to {dest}"))
+}
+
+pub(crate) fn read_to_string<P>(path: P) -> miette::Result<String>
+where
+    P: AsRef<Path>,
+{
+    fs::read_to_string(&path)
+        .into_diagnostic()
+        .wrap_err_with(|| {
+            format!(
+                "failed to read UTF-8 string from path {}",
+                path.as_ref().display()
+            )
+        })
+}
+
+pub(crate) fn copy<P1, P2>(from: P1, to: P2) -> miette::Result<u64>
+where
+    P1: AsRef<Path>,
+    P2: AsRef<Path>,
+{
+    fs::copy(&from, &to).into_diagnostic().wrap_err_with(|| {
+        format!(
+            "failed to copy {} to {}",
+            from.as_ref().display(),
+            to.as_ref().display()
+        )
+    })
+}
+
+pub(crate) fn create_dir_all<P>(path: P) -> miette::Result<()>
+where
+    P: AsRef<Path>,
+{
+    fs::create_dir_all(&path)
+        .into_diagnostic()
+        .wrap_err_with(|| {
+            format!(
+                "failed to create directories leading up to {}",
+                path.as_ref().display()
+            )
+        })
+}
+
+pub(crate) fn remove_file<P>(path: P) -> miette::Result<()>
+where
+    P: AsRef<Path>,
+{
+    fs::remove_file(&path)
+        .into_diagnostic()
+        .wrap_err_with(|| format!("failed to remove file at path {}", path.as_ref().display()))
+}
+
+pub(crate) fn write<P, C>(path: P, contents: C) -> miette::Result<()>
+where
+    P: AsRef<Path>,
+    C: AsRef<[u8]>,
+{
+    fs::write(&path, &contents)
+        .into_diagnostic()
+        .wrap_err_with(|| format!("failed to write to path {}", path.as_ref().display()))
+}
