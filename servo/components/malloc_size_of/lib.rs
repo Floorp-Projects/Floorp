3ebebc48614431ef78bc45924c15945e601c844e
David Shin <dshin@mozilla.com>
2023-03-15 16:39:33 +0000

    Bug 1820835: `:has` relative selector matching, with no caching/filtering. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D172019

--- servo/components/malloc_size_of/lib.rs
+++ servo/components/malloc_size_of/lib.rs
@@ -725,8 +725,8 @@ where
                 selector.size_of(ops)
             },
             Component::Is(ref list) |
-            Component::Where(ref list) |
-            Component::Has(ref list) => list.size_of(ops),
+            Component::Where(ref list) => list.size_of(ops),
+            Component::Has(ref relative_selectors) => relative_selectors.size_of(ops),
             Component::NthOf(ref nth_of_data) => nth_of_data.size_of(ops),
             Component::PseudoElement(ref pseudo) => (*pseudo).size_of(ops),
             Component::Combinator(..) |
