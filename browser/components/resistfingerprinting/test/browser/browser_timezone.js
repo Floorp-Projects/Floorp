20509e99cfc6b34dde98e8ef77b00718abcce90b
Tom Schuster <tschuster@mozilla.com>
2023-03-15 13:53:56 +0000

    Bug 1709867 - Test time zone spoofing exemptions. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D169902

--- browser/components/resistfingerprinting/test/browser/browser_timezone.js
+++ browser/components/resistfingerprinting/test/browser/browser_timezone.js
@@ -3,58 +3,154 @@
  *               UTC timezone after fingerprinting resistance is enabled.
  */
 
-add_setup(async function() {
-  await SpecialPowers.pushPrefEnv({
-    set: [["privacy.resistFingerprinting", true]],
-  });
-});
+async function verifySpoofed() {
+  ok(true, "Running on " + content.location.origin);
 
-add_task(async function test_timezone() {
-  // Load a page and verify the timezone.
-  let tab = await BrowserTestUtils.openNewForegroundTab(
-    gBrowser,
-    TEST_PATH + "file_dummy.html"
+  SpecialPowers.Cu.getJSTestingFunctions().setTimeZone("PST8PDT");
+  is(
+    Intl.DateTimeFormat("en-US").resolvedOptions().timeZone,
+    "PST8PDT",
+    "Default time zone should have changed"
   );
 
-  await SpecialPowers.spawn(tab.linkedBrowser, [], async function() {
-    let dateObj = new Date();
-    let dateString = dateObj.toString();
-
+  // Running in content:
+  function test() {
+    let date = new Date();
     ok(
-      dateString.endsWith("(Coordinated Universal Time)"),
-      "The date string is in UTC timezone."
+      date.toString().endsWith("(Coordinated Universal Time)"),
+      "The date toString() is in UTC timezone."
+    );
+    ok(
+      date.toTimeString().endsWith("(Coordinated Universal Time)"),
+      "The date toTimeString() is in UTC timezone."
+    );
+    let dateTimeFormat = Intl.DateTimeFormat("en-US", {
+      dateStyle: "full",
+      timeStyle: "full",
+    });
+    is(
+      dateTimeFormat.resolvedOptions().timeZone,
+      "UTC",
+      "The Intl.DateTimeFormat is in UTC timezone."
+    );
+    ok(
+      dateTimeFormat.format(date).endsWith("Coordinated Universal Time"),
+      "The Intl.DateTimeFormat is formatting with the UTC timezone."
     );
     is(
-      dateObj.getFullYear(),
-      dateObj.getUTCFullYear(),
+      date.getFullYear(),
+      date.getUTCFullYear(),
       "The full year reports in UTC timezone."
     );
     is(
-      dateObj.getMonth(),
-      dateObj.getUTCMonth(),
+      date.getMonth(),
+      date.getUTCMonth(),
       "The month reports in UTC timezone."
     );
+    is(date.getDate(), date.getUTCDate(), "The month reports in UTC timezone.");
+    is(date.getDay(), date.getUTCDay(), "The day reports in UTC timezone.");
     is(
-      dateObj.getDate(),
-      dateObj.getUTCDate(),
-      "The month reports in UTC timezone."
-    );
-    is(
-      dateObj.getDay(),
-      dateObj.getUTCDay(),
-      "The day reports in UTC timezone."
-    );
-    is(
-      dateObj.getHours(),
-      dateObj.getUTCHours(),
+      date.getHours(),
+      date.getUTCHours(),
       "The hours reports in UTC timezone."
     );
+    is(date.getTimezoneOffset(), 0, "The difference with UTC timezone is 0.");
+  }
+
+  // Run test in the context of the page.
+  Cu.exportFunction(is, content, { defineAs: "is" });
+  Cu.exportFunction(ok, content, { defineAs: "ok" });
+  content.eval(`(${test})()`);
+}
+
+add_task(async function test_timezone() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["privacy.resistFingerprinting", true]],
+  });
+
+  // Load a page and verify the timezone.
+  let tab = await BrowserTestUtils.openNewForegroundTab({
+    gBrowser,
+    opening: TEST_PATH + "file_dummy.html",
+    forceNewProcess: true,
+  });
+
+  await SpecialPowers.spawn(tab.linkedBrowser, [], verifySpoofed);
+
+  BrowserTestUtils.removeTab(tab);
+
+  await SpecialPowers.popPrefEnv();
+});
+
+// Verify that exempted domain is not spoofed.
+add_task(async function test_timezone_exempt() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["privacy.resistFingerprinting.testGranularityMask", 0x4],
+      ["privacy.resistFingerprinting.exemptedDomains", "example.net"],
+      ["privacy.resistFingerprinting", true],
+    ],
+  });
+
+  let tab = await BrowserTestUtils.openNewForegroundTab({
+    gBrowser,
+    opening: TEST_PATH + "file_dummy.html",
+    forceNewProcess: true,
+  });
+
+  await SpecialPowers.spawn(tab.linkedBrowser, [], async function() {
+    SpecialPowers.Cu.getJSTestingFunctions().setTimeZone("PST8PDT");
     is(
-      dateObj.getTimezoneOffset(),
-      0,
-      "The difference with UTC timezone is 0."
+      Intl.DateTimeFormat("en-US").resolvedOptions().timeZone,
+      "PST8PDT",
+      "Default time zone should have changed"
     );
+
+    function test() {
+      let date = new Date(0);
+      ok(
+        date.toString().endsWith("(Pacific Standard Time)"),
+        "The date toString() is in PT timezone"
+      );
+
+      is(
+        Intl.DateTimeFormat("en-US").resolvedOptions().timeZone,
+        "PST8PDT",
+        "Content should use default time zone"
+      );
+    }
+
+    // Run test in the context of the page.
+    Cu.exportFunction(is, content, { defineAs: "is" });
+    Cu.exportFunction(ok, content, { defineAs: "ok" });
+    content.eval(`(${test})()`);
   });
 
   BrowserTestUtils.removeTab(tab);
+
+  await SpecialPowers.popPrefEnv();
+});
+
+// Verify that we are still spoofing for domains not `exemptedDomains` list.
+add_task(async function test_timezone_exempt_wrong_domain() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["privacy.resistFingerprinting.testGranularityMask", 0x4],
+      ["privacy.resistFingerprinting.exemptedDomains", "example.net"],
+      ["privacy.resistFingerprinting", true],
+    ],
+  });
+
+  let tab = await BrowserTestUtils.openNewForegroundTab({
+    gBrowser,
+    opening:
+      TEST_PATH.replace("example.net", "example.org") + "file_dummy.html",
+    forceNewProcess: true,
+  });
+
+  await SpecialPowers.spawn(tab.linkedBrowser, [], verifySpoofed);
+
+  BrowserTestUtils.removeTab(tab);
+
+  await SpecialPowers.popPrefEnv();
 });
