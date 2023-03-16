4e5fb65a8fa45a40886592a5ed09d6104986bc36
Andrew McCreight <continuation@gmail.com>
2023-03-16 14:56:59 +0000

    Bug 1822608 - Make PIDs for utility processes consistent with other processes. r=gerard-majax
    Other processes print their pid like (pid 12345), so change utility processes to be consistent.
    
    Differential Revision: https://phabricator.services.mozilla.com/D172744

--- ipc/glue/UtilityProcessChild.cpp
+++ ipc/glue/UtilityProcessChild.cpp
@@ -194,8 +194,8 @@ mozilla::ipc::IPCResult UtilityProcessChild::RecvRequestMemoryReport(
     const uint32_t& aGeneration, const bool& aAnonymize,
     const bool& aMinimizeMemoryUsage, const Maybe<FileDescriptor>& aDMDFile,
     const RequestMemoryReportResolver& aResolver) {
-  nsPrintfCString processName("Utility (pid: %" PRIPID
-                              ", sandboxingKind: %" PRIu64 ")",
+  nsPrintfCString processName("Utility (pid %" PRIPID
+                              ", sandboxingKind %" PRIu64 ")",
                               base::GetCurrentProcId(), mSandbox);
 
   mozilla::dom::MemoryReportRequestClient::Start(
