10748a3dbeeb5cadb65d45b95407c517cdf00d8e
Barret Rennie <barret@brennie.ca>
2023-03-16 00:23:55 +0000

    Bug 1821243 - Update to jsonschema 4.17.3 r=ahochheiden
    We are using features from modern JSONSchema drafts, like dependantRequired, in
    the Firefox Messaging System schemas. jsonschema 3.x does not support these
    features. Experimenter is also being updated to 4.17.3 and we want to keep our
    validation tests in sync with Experimenter.
    
    Differential Revision: https://phabricator.services.mozilla.com/D172457

--- third_party/python/jsonschema/jsonschema/exceptions.py
+++ third_party/python/jsonschema/jsonschema/exceptions.py
@@ -1,19 +1,20 @@
 """
 Validation errors, and some surrounding helpers.
 """
+from __future__ import annotations
+
 from collections import defaultdict, deque
+from pprint import pformat
+from textwrap import dedent, indent
+import heapq
 import itertools
-import pprint
-import textwrap
 
 import attr
 
 from jsonschema import _utils
-from jsonschema.compat import PY3, iteritems
 
-
-WEAK_MATCHES = frozenset(["anyOf", "oneOf"])
-STRONG_MATCHES = frozenset()
+WEAK_MATCHES: frozenset[str] = frozenset(["anyOf", "oneOf"])
+STRONG_MATCHES: frozenset[str] = frozenset()
 
 _unset = _utils.Unset()
 
@@ -31,6 +32,7 @@ class _Error(Exception):
         schema=_unset,
         schema_path=(),
         parent=None,
+        type_checker=_unset,
     ):
         super(_Error, self).__init__(
             message,
@@ -54,45 +56,42 @@ class _Error(Exception):
         self.instance = instance
         self.schema = schema
         self.parent = parent
+        self._type_checker = type_checker
 
         for error in context:
             error.parent = self
 
     def __repr__(self):
-        return "<%s: %r>" % (self.__class__.__name__, self.message)
+        return f"<{self.__class__.__name__}: {self.message!r}>"
 
-    def __unicode__(self):
+    def __str__(self):
         essential_for_verbose = (
             self.validator, self.validator_value, self.instance, self.schema,
         )
         if any(m is _unset for m in essential_for_verbose):
             return self.message
 
-        pschema = pprint.pformat(self.schema, width=72)
-        pinstance = pprint.pformat(self.instance, width=72)
-        return self.message + textwrap.dedent("""
-
-            Failed validating %r in %s%s:
-            %s
-
-            On %s%s:
-            %s
-            """.rstrip()
-        ) % (
-            self.validator,
-            self._word_for_schema_in_error_message,
-            _utils.format_as_index(list(self.relative_schema_path)[:-1]),
-            _utils.indent(pschema),
-            self._word_for_instance_in_error_message,
-            _utils.format_as_index(self.relative_path),
-            _utils.indent(pinstance),
+        schema_path = _utils.format_as_index(
+            container=self._word_for_schema_in_error_message,
+            indices=list(self.relative_schema_path)[:-1],
         )
+        instance_path = _utils.format_as_index(
+            container=self._word_for_instance_in_error_message,
+            indices=self.relative_path,
+        )
+        prefix = 16 * " "
 
-    if PY3:
-        __str__ = __unicode__
-    else:
-        def __str__(self):
-            return unicode(self).encode("utf-8")
+        return dedent(
+            f"""\
+            {self.message}
+
+            Failed validating {self.validator!r} in {schema_path}:
+                {indent(pformat(self.schema, width=72), prefix).lstrip()}
+
+            On {instance_path}:
+                {indent(pformat(self.instance, width=72), prefix).lstrip()}
+            """.rstrip(),
+        )
 
     @classmethod
     def create_from(cls, other):
@@ -118,8 +117,21 @@ class _Error(Exception):
         path.extendleft(reversed(parent.absolute_schema_path))
         return path
 
-    def _set(self, **kwargs):
-        for k, v in iteritems(kwargs):
+    @property
+    def json_path(self):
+        path = "$"
+        for elem in self.absolute_path:
+            if isinstance(elem, int):
+                path += "[" + str(elem) + "]"
+            else:
+                path += "." + elem
+        return path
+
+    def _set(self, type_checker=None, **kwargs):
+        if type_checker is not None and self._type_checker is _unset:
+            self._type_checker = type_checker
+
+        for k, v in kwargs.items():
             if getattr(self, k) is _unset:
                 setattr(self, k, v)
 
@@ -129,6 +141,20 @@ class _Error(Exception):
             "path", "schema_path", "instance", "schema", "parent",
         )
         return dict((attr, getattr(self, attr)) for attr in attrs)
+
+    def _matches_type(self):
+        try:
+            expected = self.schema["type"]
+        except (KeyError, TypeError):
+            return False
+
+        if isinstance(expected, str):
+            return self._type_checker.is_type(self.instance, expected)
+
+        return any(
+            self._type_checker.is_type(self.instance, expected_type)
+            for expected_type in expected
+        )
 
 
 class ValidationError(_Error):
@@ -169,14 +195,8 @@ class UndefinedTypeCheck(Exception):
     def __init__(self, type):
         self.type = type
 
-    def __unicode__(self):
-        return "Type %r is unknown to this type checker" % self.type
-
-    if PY3:
-        __str__ = __unicode__
-    else:
-        def __str__(self):
-            return unicode(self).encode("utf-8")
+    def __str__(self):
+        return f"Type {self.type!r} is unknown to this type checker"
 
 
 class UnknownType(Exception):
@@ -189,23 +209,18 @@ class UnknownType(Exception):
         self.instance = instance
         self.schema = schema
 
-    def __unicode__(self):
-        pschema = pprint.pformat(self.schema, width=72)
-        pinstance = pprint.pformat(self.instance, width=72)
-        return textwrap.dedent("""
-            Unknown type %r for validator with schema:
-            %s
+    def __str__(self):
+        prefix = 16 * " "
+
+        return dedent(
+            f"""\
+            Unknown type {self.type!r} for validator with schema:
+                {indent(pformat(self.schema, width=72), prefix).lstrip()}
 
             While checking instance:
-            %s
-            """.rstrip()
-        ) % (self.type, _utils.indent(pschema), _utils.indent(pinstance))
-
-    if PY3:
-        __str__ = __unicode__
-    else:
-        def __str__(self):
-            return unicode(self).encode("utf-8")
+                {indent(pformat(self.instance, width=72), prefix).lstrip()}
+            """.rstrip(),
+        )
 
 
 class FormatError(Exception):
@@ -218,17 +233,11 @@ class FormatError(Exception):
         self.message = message
         self.cause = self.__cause__ = cause
 
-    def __unicode__(self):
+    def __str__(self):
         return self.message
 
-    if PY3:
-        __str__ = __unicode__
-    else:
-        def __str__(self):
-            return self.message.encode("utf-8")
 
-
-class ErrorTree(object):
+class ErrorTree:
     """
     ErrorTrees make it easier to check which validations failed.
     """
@@ -258,10 +267,10 @@ class ErrorTree(object):
         """
         Retrieve the child tree one level down at the given ``index``.
 
-        If the index is not in the instance that this tree corresponds to and
-        is not known by this tree, whatever error would be raised by
-        ``instance.__getitem__`` will be propagated (usually this is some
-        subclass of `exceptions.LookupError`.
+        If the index is not in the instance that this tree corresponds
+        to and is not known by this tree, whatever error would be raised
+        by ``instance.__getitem__`` will be propagated (usually this is
+        some subclass of `LookupError`.
         """
 
         if self._instance is not _unset and index not in self:
@@ -288,7 +297,9 @@ class ErrorTree(object):
         return self.total_errors
 
     def __repr__(self):
-        return "<%s (%s total errors)>" % (self.__class__.__name__, len(self))
+        total = len(self)
+        errors = "error" if total == 1 else "errors"
+        return f"<{self.__class__.__name__} ({total} total {errors})>"
 
     @property
     def total_errors(self):
@@ -296,7 +307,7 @@ class ErrorTree(object):
         The total number of errors in the entire tree, including children.
         """
 
-        child_errors = sum(len(tree) for _, tree in iteritems(self._contents))
+        child_errors = sum(len(tree) for _, tree in self._contents.items())
         return len(self.errors) + child_errors
 
 
@@ -306,19 +317,25 @@ def by_relevance(weak=WEAK_MATCHES, strong=STRONG_MATCHES):
 
     Arguments:
         weak (set):
-            a collection of validator names to consider to be "weak".
-            If there are two errors at the same level of the instance
-            and one is in the set of weak validator names, the other
-            error will take priority. By default, :validator:`anyOf` and
-            :validator:`oneOf` are considered weak validators and will
-            be superseded by other same-level validation errors.
+            a collection of validation keywords to consider to be
+            "weak".  If there are two errors at the same level of the
+            instance and one is in the set of weak validation keywords,
+            the other error will take priority. By default, :kw:`anyOf`
+            and :kw:`oneOf` are considered weak keywords and will be
+            superseded by other same-level validation errors.
 
         strong (set):
-            a collection of validator names to consider to be "strong"
+            a collection of validation keywords to consider to be
+            "strong"
     """
     def relevance(error):
         validator = error.validator
-        return -len(error.path), validator not in weak, validator in strong
+        return (
+            -len(error.path),
+            validator not in weak,
+            validator in strong,
+            not error._matches_type(),
+        )
     return relevance
 
 
@@ -333,20 +350,20 @@ def best_match(errors, key=relevance):
     `ValidationError.path` is shorter) are considered better matches,
     since they indicate "more" is wrong with the instance.
 
-    If the resulting match is either :validator:`oneOf` or :validator:`anyOf`,
-    the *opposite* assumption is made -- i.e. the deepest error is picked,
-    since these validators only need to match once, and any other errors may
-    not be relevant.
+    If the resulting match is either :kw:`oneOf` or :kw:`anyOf`, the
+    *opposite* assumption is made -- i.e. the deepest error is picked,
+    since these keywords only need to match once, and any other errors
+    may not be relevant.
 
     Arguments:
-        errors (collections.Iterable):
+        errors (collections.abc.Iterable):
 
             the errors to select from. Do not provide a mixture of
             errors from different validation attempts (i.e. from
             different instances or schemas), since it won't produce
             sensical output.
 
-        key (collections.Callable):
+        key (collections.abc.Callable):
 
             the key to use when sorting errors. See `relevance` and
             transitively `by_relevance` for more details (the default is
@@ -370,5 +387,10 @@ def best_match(errors, key=relevance):
     best = max(itertools.chain([best], errors), key=key)
 
     while best.context:
-        best = min(best.context, key=key)
+        # Calculate the minimum via nsmallest, because we don't recurse if
+        # all nested errors have the same relevance (i.e. if min == max == all)
+        smallest = heapq.nsmallest(2, best.context, key=key)
+        if len(smallest) == 2 and key(smallest[0]) == key(smallest[1]):
+            return best
+        best = smallest[0]
     return best
