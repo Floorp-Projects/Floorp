fca5e6e6729cdc9afab558e87b9492f1ee627cbe
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-03-16 15:46:58 +0000

    Bug 1819102 - Move OpenSearch engine update url handling entirely into OpenSearchEngine. r=Standard8
    Also removes a redundant check from engineUpdateService.
    
    Differential Revision: https://phabricator.services.mozilla.com/D172561

--- toolkit/components/search/SearchService.sys.mjs
+++ toolkit/components/search/SearchService.sys.mjs
@@ -3554,11 +3554,6 @@ var engineUpdateService = {
     let testEngine = null;
     let updateURI = engine._updateURI;
     if (updateURI) {
-      if (engine.isAppProvided && !updateURI.schemeIs("https")) {
-        lazy.logConsole.debug("Invalid scheme for default engine update");
-        return;
-      }
-
       lazy.logConsole.debug("updating", engine.name, updateURI.spec);
       testEngine = new lazy.OpenSearchEngine();
       testEngine._engineToUpdate = engine;
