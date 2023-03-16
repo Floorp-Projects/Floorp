b8d8c83472bc49a2892daeaa358589ce0b3dc6d1
Otto Länd <bind-autoland@mozilla.com>
2023-03-16 14:04:03 +0000

    Bug 1806794, 1806802: apply code formatting via Lando
    # ignore-this-changeset

--- testing/web-platform/tests/webdriver/tests/support/http_handlers/cached.py
+++ testing/web-platform/tests/webdriver/tests/support/http_handlers/cached.py
@@ -5,10 +5,10 @@ def main(request, response):
     status = int(request.GET.get(b"status", None))
     # For redirects, a "location" get parameter can indicate the redirected url
     if status == 301 and b"location" in request.GET:
-        response.headers.set(b"Location", request.GET.first(b"location"));
+        response.headers.set(b"Location", request.GET.first(b"location"))
 
     response.status = status
     response.headers.set(b"Content-Type", "text/plain")
-    response.headers.set(b"Expires", "Thu, 01 Dec 2100 20:00:00 GMT");
-    response.headers.set(b"Cache-Control", "max-age=3600");
+    response.headers.set(b"Expires", "Thu, 01 Dec 2100 20:00:00 GMT")
+    response.headers.set(b"Cache-Control", "max-age=3600")
     return "Cached HTTP Response"
