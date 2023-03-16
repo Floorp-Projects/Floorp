7e70b63cd31613a3ac5c66a73f2f260d1f30a3c5
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2023-03-16 07:51:01 +0000

    Bug 1822433 - Snap to the scroll destination in the case of `intended-end-position` scroll operation if there's no snap target points. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D172627

--- layout/generic/ScrollSnap.cpp
+++ layout/generic/ScrollSnap.cpp
@@ -124,10 +124,18 @@ CalcSnapPoints::CalcSnapPoints(ScrollUnit aUnit, ScrollSnapFlags aSnapFlags,
 
 SnapTarget CalcSnapPoints::GetBestEdge() const {
   return SnapTarget{
-      nsPoint(mTrackerOnX.mEdgeFound ? mTrackerOnX.mBestEdge.mPosition
-                                     : mStartPos.x,
-              mTrackerOnY.mEdgeFound ? mTrackerOnY.mBestEdge.mPosition
-                                     : mStartPos.y),
+      nsPoint(
+          mTrackerOnX.mEdgeFound ? mTrackerOnX.mBestEdge.mPosition
+          // In the case of IntendedEndPosition (i.e. the destination point is
+          // explicitely specied, e.g. scrollTo) use the destination point if we
+          // didn't find any candidates.
+          : !(mSnapFlags & ScrollSnapFlags::IntendedDirection) ? mDestination.x
+                                                               : mStartPos.x,
+          mTrackerOnY.mEdgeFound ? mTrackerOnY.mBestEdge.mPosition
+          // Same as above X axis case, use the destination point if we didn't
+          // find any candidates.
+          : !(mSnapFlags & ScrollSnapFlags::IntendedDirection) ? mDestination.y
+                                                               : mStartPos.y),
       ScrollSnapTargetIds{mTrackerOnX.mTargetIds, mTrackerOnY.mTargetIds}};
 }
 
