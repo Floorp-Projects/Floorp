0a2e58159503aedb1c08091866f891de56e11816
mcarare <48995920+mcarare@users.noreply.github.com>
2023-03-17 12:12:54 +0200

    Bug 1819123 - Refactor IntentUtilsTest to remove Robolectric.

--- mobile/android/focus-android/app/src/test/java/org/mozilla/focus/utils/IntentUtilsTest.kt
+++ mobile/android/focus-android/app/src/test/java/org/mozilla/focus/utils/IntentUtilsTest.kt
@@ -7,22 +7,32 @@ import android.app.PendingIntent
 import android.os.Build
 import org.junit.Assert
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
 
-@RunWith(RobolectricTestRunner::class)
 class IntentUtilsTest {
 
     @Test
-    @Config(sdk = [Build.VERSION_CODES.LOLLIPOP_MR1])
     fun `given a build version lower than 23, when defaultIntentPendingFlags is called, then flag 0 should be returned`() {
+        setFinalStaticValue(Build.VERSION::class.java.getField("SDK_INT"), 22)
+
         Assert.assertEquals(0, IntentUtils.defaultIntentPendingFlags)
     }
 
     @Test
-    @Config(sdk = [Build.VERSION_CODES.M])
     fun `given a build version bigger than 22, when defaultIntentPendingFlags is called, then flag FLAG_IMMUTABLE should be returned`() {
+        setFinalStaticValue(Build.VERSION::class.java.getField("SDK_INT"), 23)
         Assert.assertEquals(PendingIntent.FLAG_IMMUTABLE, IntentUtils.defaultIntentPendingFlags)
+    }
+
+    @Throws(Exception::class)
+    fun setFinalStaticValue(field: Field, newValue: Any) {
+        field.isAccessible = true
+
+        val modifiersField = Field::class.java.getDeclaredField("modifiers")
+        modifiersField.isAccessible = true
+        modifiersField.setInt(field, field.modifiers and Modifier.FINAL.inv())
+
+        field.set(null, newValue)
     }
 }
