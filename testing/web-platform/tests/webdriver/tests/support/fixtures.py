8b20979fce1234a1f4adc705501a0fcaa5542654
Thiago Perrotta <tperrotta@chromium.org>
2023-03-20 20:19:48 +0000

    Bug 1819243 [wpt PR 38732] - BiDi: consolidate BiDi fixtures in webdriver/tests/support/fixtures_bidi, a=testonly
    Automatic update from web-platform-tests
    BiDi: consolidate BiDi fixtures in webdriver/tests/support/fixtures_bidi
    
    This fixtures file is already globally included in
    webdriver/tests/conftest.py via `pytest_plugins`.
    
    Stop having webdriver/tests/bidi/conftest.py be a duplicate of the
    support fixture.
    
    --
    
    wpt-commits: 969b345fb0c50a5fff56ea3a081629fd95c24ff9
    wpt-pr: 38732

--- testing/web-platform/tests/webdriver/tests/support/fixtures.py
+++ testing/web-platform/tests/webdriver/tests/support/fixtures.py
@@ -238,7 +238,6 @@ def iframe(inline):
     return iframe
 
 
-
 @pytest.fixture
 def get_test_page(iframe, inline):
     def get_test_page(
@@ -326,6 +325,53 @@ def get_test_page(iframe, inline):
             return inline(page_data)
 
     return get_test_page
+
+
+@pytest.fixture
+def test_origin(url):
+    return url("")
+
+
+@pytest.fixture
+def test_alt_origin(url):
+    return url("", domain="alt")
+
+
+@pytest.fixture
+def test_page(inline):
+    return inline("<div>foo</div>")
+
+
+@pytest.fixture
+def test_page2(inline):
+    return inline("<div>bar</div>")
+
+
+@pytest.fixture
+def test_page_cross_origin(inline):
+    return inline("<div>bar</div>", domain="alt")
+
+
+@pytest.fixture
+def test_page_multiple_frames(inline, test_page, test_page2):
+    return inline(
+        f"<iframe src='{test_page}'></iframe><iframe src='{test_page2}'></iframe>"
+    )
+
+
+@pytest.fixture
+def test_page_nested_frames(inline, test_page_same_origin_frame):
+    return inline(f"<iframe src='{test_page_same_origin_frame}'></iframe>")
+
+
+@pytest.fixture
+def test_page_cross_origin_frame(inline, test_page_cross_origin):
+    return inline(f"<iframe src='{test_page_cross_origin}'></iframe>")
+
+
+@pytest.fixture
+def test_page_same_origin_frame(inline, test_page):
+    return inline(f"<iframe src='{test_page}'></iframe>")
 
 
 @pytest.fixture
