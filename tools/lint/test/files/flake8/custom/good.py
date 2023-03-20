8be49d9c521bcf22d45e19c36b09c82f917b577a
Andrew Halberstadt <ahal@mozilla.com>
2023-03-20 13:06:27 +0000

    Bug 1811850 - [lint] Replace flake8 linter with ruff, r=linter-reviewers,sylvestre
    Ruff is a very fast linter implemented in Rust and it can act as a drop-in
    replacement for flake8. When running the same set of rules across all files
    in mozilla-central (without mozlint), flake8 takes 900 seconds whereas ruff
    takes 0.9 seconds.
    
    Ruff also implements rules from other popular Python linters such as pylint,
    isort and pyupgrade. There are even plans to implement feature parity with
    black in the future. Ultimately, it can become our one stop shop for all Python
    linting and formatting.
    
    This stack will swap out all our Python lint tools for ruff (excluding black
    for now).
    
    Differential Revision: https://phabricator.services.mozilla.com/D172313

--- tools/lint/test/files/flake8/custom/good.py
+++
@@ -1,5 +0,0 @@
-# Unused import
-import distutils
-
-print("This is a line that is over 80 characters but under 100. It shouldn't fail.")
-print("This is a line that is over not only 80, but 100 characters. It should also not cause a failure.")
---
+++ tools/lint/test/files/ruff/bad.py
@@ -0,0 +1,4 @@
+import distutils
+
+if not "foo" in "foobar":
+    print("oh no!")
