c0c8d317120aa953d81d5696ba3f66fc5d795462
Tom Schuster <tschuster@mozilla.com>
2023-03-20 18:52:26 +0000

    Bug 1818250 - Cut Site-Specific Zoom over to fine-grained RFP. r=mconley,tjr
    Depends on D172271
    
    Differential Revision: https://phabricator.services.mozilla.com/D172272

--- browser/components/resistfingerprinting/test/browser/browser_bug1369357_site_specific_zoom_level.js
+++ browser/components/resistfingerprinting/test/browser/browser_bug1369357_site_specific_zoom_level.js
@@ -1,16 +1,16 @@
 "use strict";
 
-const testPage =
-  "http://example.net/browser/browser/components/resistFingerprinting/test/browser/file_dummy.html";
+const PATH_NET = TEST_PATH + "file_dummy.html";
+const PATH_ORG = PATH_NET.replace("example.net", "example.org");
 
 add_task(async function() {
   let tab1, tab1Zoom, tab2, tab2Zoom, tab3, tab3Zoom;
 
-  tab1 = await BrowserTestUtils.openNewForegroundTab(gBrowser, testPage);
+  tab1 = await BrowserTestUtils.openNewForegroundTab(gBrowser, PATH_NET);
   await FullZoom.enlarge();
   tab1Zoom = ZoomManager.getZoomForBrowser(tab1.linkedBrowser);
 
-  tab2 = await BrowserTestUtils.openNewForegroundTab(gBrowser, testPage);
+  tab2 = await BrowserTestUtils.openNewForegroundTab(gBrowser, PATH_NET);
   tab2Zoom = ZoomManager.getZoomForBrowser(tab2.linkedBrowser);
 
   is(
@@ -23,7 +23,7 @@ add_task(async function() {
     set: [["privacy.resistFingerprinting", true]],
   });
 
-  tab3 = await BrowserTestUtils.openNewForegroundTab(gBrowser, testPage);
+  tab3 = await BrowserTestUtils.openNewForegroundTab(gBrowser, PATH_NET);
   tab3Zoom = ZoomManager.getZoomForBrowser(tab3.linkedBrowser);
 
   isnot(
@@ -37,4 +37,42 @@ add_task(async function() {
   BrowserTestUtils.removeTab(tab1);
   BrowserTestUtils.removeTab(tab2);
   BrowserTestUtils.removeTab(tab3);
+
+  await SpecialPowers.popPrefEnv();
+});
+
+add_task(async function exempt_domain() {
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, PATH_NET);
+  await FullZoom.enlarge();
+  const netZoomOriginal = ZoomManager.getZoomForBrowser(tab.linkedBrowser);
+  is(netZoomOriginal, 1.1, "Initial zoom is 110%");
+  await BrowserTestUtils.removeTab(tab);
+
+  tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, PATH_ORG);
+  await FullZoom.enlarge();
+  const orgZoomOriginal = ZoomManager.getZoomForBrowser(tab.linkedBrowser);
+  is(orgZoomOriginal, 1.1, "Initial zoom is 110%");
+  await BrowserTestUtils.removeTab(tab);
+
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["privacy.resistFingerprinting.testGranularityMask", 4],
+      ["privacy.resistFingerprinting.exemptedDomains", "example.net"],
+      ["privacy.resistFingerprinting", true],
+    ],
+  });
+
+  tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, PATH_NET);
+  const netZoom = ZoomManager.getZoomForBrowser(tab.linkedBrowser);
+  is(netZoom, 1.1, "exempted example.net tab should have kept zoom level");
+  await FullZoom.reset();
+  await BrowserTestUtils.removeTab(tab);
+
+  tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, PATH_ORG);
+  const orgZoom = ZoomManager.getZoomForBrowser(tab.linkedBrowser);
+  is(orgZoom, 1.0, "example.org tab has its zoom reset to default 100%");
+  await FullZoom.reset();
+  await BrowserTestUtils.removeTab(tab);
+
+  await SpecialPowers.popPrefEnv();
 });
