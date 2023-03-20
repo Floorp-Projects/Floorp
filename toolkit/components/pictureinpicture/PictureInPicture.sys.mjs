cf586cf7dfec601378ed4eb0b8c1cf433cd454a3
Emilio Cobos Álvarez <emilio@crisal.io>
2023-03-20 17:21:38 +0000

    Bug 1823350 - Unify how we identify PiP windows on Linux/Windows. r=mconley,stransky
    This way we don't end up without titlebars in other alwaysontop windows.
    
    Differential Revision: https://phabricator.services.mozilla.com/D172992

--- toolkit/components/pictureinpicture/PictureInPicture.sys.mjs
+++ toolkit/components/pictureinpicture/PictureInPicture.sys.mjs
@@ -18,12 +18,7 @@ ChromeUtils.defineESModuleGetters(lazy, {
 import { Rect, Point } from "resource://gre/modules/Geometry.sys.mjs";
 
 const PLAYER_URI = "chrome://global/content/pictureinpicture/player.xhtml";
-var PLAYER_FEATURES =
-  "chrome,titlebar=yes,alwaysontop,lockaspectratio,resizable";
-/* Don't use dialog on Gtk as it adds extra border and titlebar to PIP window */
-if (!AppConstants.MOZ_WIDGET_GTK) {
-  PLAYER_FEATURES += ",dialog";
-}
+const PLAYER_FEATURES = "chrome,alwaysontop,lockaspectratio,resizable,dialog";
 const WINDOW_TYPE = "Toolkit:PictureInPicture";
 const PIP_ENABLED_PREF = "media.videocontrols.picture-in-picture.enabled";
 const TOGGLE_ENABLED_PREF =
