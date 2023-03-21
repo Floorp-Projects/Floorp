ca430868caccc56444ddaa8f2d471c0bbcc8a3ec
MickeyMoz <sebastian@mozilla.com>
2023-03-21 01:31:43 +0000

    Update Glean to 52.4.2.

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -46,7 +46,7 @@ object Versions {
     const val mozilla_appservices = "97.2.0"
 
     // DO NOT MODIFY MANUALLY. This is auto-updated along with GeckoView.
-    const val mozilla_glean = "52.3.0"
+    const val mozilla_glean = "52.4.2"
 
     const val material = "1.2.1"
     const val ksp = "1.0.9"
