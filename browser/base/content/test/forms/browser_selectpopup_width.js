112128bc78866fe47dfc9761f82e3f998e33862c
Emilio Cobos Álvarez <emilio@crisal.io>
2023-03-21 11:24:07 +0000

    Bug 1823552 - When fixing the position of a followanchor="false" popup, don't lose the anchor rect altogether. r=stransky
    Otherwise we lose the anchor constraints which can change our layout.
    The only thing we want is to stop following the anchor _node_, so we can
    just convert it to an anchor rect instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173135

--- browser/base/content/test/forms/browser_selectpopup_width.js
+++ browser/base/content/test/forms/browser_selectpopup_width.js
@@ -6,6 +6,12 @@ const PAGE = `
 </select>
 `;
 
+function tick() {
+  return new Promise(r =>
+    requestAnimationFrame(() => requestAnimationFrame(r))
+  );
+}
+
 add_task(async function() {
   const url = "data:text/html," + encodeURI(PAGE);
   await BrowserTestUtils.withNewTab(
@@ -16,6 +22,23 @@ add_task(async function() {
     async function(browser) {
       let popup = await openSelectPopup("click");
       let arrowSB = popup.shadowRoot.querySelector(".menupopup-arrowscrollbox");
+      is(
+        arrowSB.getBoundingClientRect().width,
+        600,
+        "Should be the right size"
+      );
+
+      // Trigger a layout change that would cause us to layout the popup again,
+      // and change our menulist to be zero-size so that the anchor rect
+      // codepath is used. We should still use the anchor rect to expand our
+      // size.
+      await tick();
+
+      popup.closest("menulist").style.width = "0";
+      popup.style.minWidth = "2px";
+
+      await tick();
+
       is(
         arrowSB.getBoundingClientRect().width,
         600,
