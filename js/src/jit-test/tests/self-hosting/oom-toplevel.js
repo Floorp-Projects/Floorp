b9ef9c007c8f10d1515fd3e7378d0c9e16ddda19
Ted Campbell <tcampbell@mozilla.com>
2023-03-21 21:23:33 +0000

    Bug 1727364 - Add testcase. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D123995

---
+++ js/src/jit-test/tests/self-hosting/oom-toplevel.js
@@ -0,0 +1,25 @@
+// |jit-test| skip-if: !('oomAtAllocation' in this)
+
+function code(n) {
+    return `
+        // Trigger top-level execution with an OOM in the middle.
+        oomAtAllocation(${n});
+        try { getSelfHostedValue("numberFormatCache") } catch (e) { }
+        resetOOMFailure();
+
+        // Read current value of "dateTimeFormatCache".
+        var initVal = getSelfHostedValue("dateTimeFormatCache");
+        assertEq(typeof initVal, "object");
+
+        // Retrigger top-level execution by reading a later value in the file.
+        // Then compare that "dateTimeFormatCache" was not clobbered.
+        getSelfHostedValue("collatorCache");
+        assertEq(initVal, getSelfHostedValue("dateTimeFormatCache"));
+        `;
+}
+
+// We cannot use `oomTest` here because of divergence issues from things like
+// `RegisterShapeCache` absorbing OOMs.
+for (var i = 1; i < 300; ++i) {
+    evaluate(code(i), { global: newGlobal() });
+}
