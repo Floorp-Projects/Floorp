ab185ca2b8552c1e204ec2ce6b97796670d76224
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-03-21 17:27:49 +0000

    Bug 1823399 - Add startColumn option to Debugger.Object.createSource. r=tcampbell.
    Differential Revision: https://phabricator.services.mozilla.com/D173023

--- js/src/jit-test/tests/debug/Object-createSource.js
+++ js/src/jit-test/tests/debug/Object-createSource.js
@@ -1,6 +1,6 @@
 // createSource creates new sources.
 
-let g = newGlobal({newCompartment: true});
+let g = newGlobal({ newCompartment: true });
 let dbg = new Debugger(g);
 let gdbg = dbg.addDebuggee(g);
 
@@ -8,11 +8,13 @@ let source = gdbg.createSource({
   text: "x = 3",
   url: "foo.js",
   startLine: 3,
+  startColumn: 42,
   sourceMapURL: "sourceMapURL",
   isScriptElement: true,
 });
 assertEq(source.text, "x = 3");
 assertEq(source.url, "foo.js");
 assertEq(source.startLine, 3);
+assertEq(source.startColumn, 42);
 assertEq(source.sourceMapURL, "sourceMapURL");
 assertEq(source.introductionType, "inlineScript");
