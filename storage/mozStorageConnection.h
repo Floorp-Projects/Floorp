1786dc2a9c76b743cb70cc6104b6440cdc4d33a0
Marco Bonardo <mbonardo@mozilla.com>
2023-03-22 15:36:37 +0000

    Bug 1813986 - Add an asyncVacuum() method to storage async connections, and let VacuumManager use it. r=asuth
    Add asyncVacuum to mozIStorageAsyncConnection, that dispatches a runnable to
    the helper thread, where it will execute a full or incremental vacuum, depending
    on the connection auto_vacuum value.
    It also supports vacuuming attached schemas.
    asyncVacuum() supports changing both the page_size and auto_vacuum.
    Change mozIStorageVacuumParticipant to return a mozIStorageAsyncConnection and
    allow specifying whether incremental vacuum should be enabled.
    Change vacuumManager notification from heavy-io-task to vacuum-begin and vacuum-end
    since the original proposal of notifying heavy IO didn't take off.
    Cleanup test_vacuum to be able to use instances of the test VacuumParticipant,
    that means we can remove the no more necessary registerESM hack.
    Fix Places History as the only cpp consumer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168298

--- storage/mozStorageConnection.h
+++ storage/mozStorageConnection.h
@@ -317,6 +317,25 @@ class Connection final : public mozIStorageConnection,
    */
   void RecordQueryStatus(int srv);
 
+  /**
+   * Returns the number of pages in the free list that can be removed.
+   *
+   * A database may use chunked growth to reduce filesystem fragmentation, then
+   * Sqlite will allocate and release multiple pages in chunks. We want to
+   * preserve the chunked space to reduce the likelihood of fragmentation,
+   * releasing free pages only when there's a large amount of them. This can be
+   * used to decide if it's worth vacuuming the database and how many pages can
+   * be vacuumed in case of incremental vacuum.
+   * Note this returns 0, and asserts, in case of errors.
+   */
+  int32_t RemovablePagesInFreeList(const nsACString& aSchemaName);
+
+  /**
+   * Whether the statement currently running on the helper thread can be
+   * interrupted.
+   */
+  Atomic<bool> mIsStatementOnHelperThreadInterruptible;
+
  private:
   ~Connection();
   nsresult initializeInternal();
@@ -483,6 +502,11 @@ class Connection final : public mozIStorageConnection,
    * sharedAsyncExecutionMutex.
    */
   bool mConnectionClosed;
+
+  /**
+   * Stores the growth increment chunk size, set through SetGrowthIncrement().
+   */
+  Atomic<int32_t> mGrowthChunkSize;
 };
 
 /**
