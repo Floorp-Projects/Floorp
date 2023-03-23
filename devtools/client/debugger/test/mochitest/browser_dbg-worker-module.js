3175a7491dcc75666db643af9f72722b20dc846c
Yulia <ystartsev@mozilla.com>
2023-03-23 10:15:09 +0000

    Bug 1822937 - Add devtools test to ensure we are no longer crashing; r=ochameau,devtools-reviewers
    The test times out when it is unsuccessful.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173277

---
+++ devtools/client/debugger/test/mochitest/browser_dbg-worker-module.js
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// Test that worker modules do not crash when run in the debugger.
+
+"use strict";
+
+add_task(async function() {
+  const dbg = await initDebugger("doc-module-worker.html");
+  // reload the page
+  await navigate(dbg, "doc-module-worker.html", "doc-module-worker.html");
+
+  ok(true, "didn't crash");
+  // TODO: bug 1816933 -- we can test actual functionality:
+  //  await waitForThreadCount(dbg, 1);
+  //  const workers = dbg.selectors.getThreads();
+  //  ok(workers.length == 1, "Got one worker");
+});
