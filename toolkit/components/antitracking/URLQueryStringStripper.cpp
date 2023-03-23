21e40e8dad7a812e11664a86cef54a0251b43438
Paul Zuehlcke <pbz@mozilla.com>
2023-03-23 10:36:55 +0000

    Bug 1821648 - Implement nsIObserver in URLQueryStringStripper to fix warning. r=anti-tracking-reviewers,timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D173294

--- toolkit/components/antitracking/URLQueryStringStripper.cpp
+++ toolkit/components/antitracking/URLQueryStringStripper.cpp
@@ -31,8 +31,8 @@ static const char kQueryStrippingEnabledPBMPref[] =
 
 namespace mozilla {
 
-NS_IMPL_ISUPPORTS(URLQueryStringStripper, nsIURLQueryStringStripper,
-                  nsIURLQueryStrippingListObserver)
+NS_IMPL_ISUPPORTS(URLQueryStringStripper, nsIObserver,
+                  nsIURLQueryStringStripper, nsIURLQueryStrippingListObserver)
 
 // static
 already_AddRefed<URLQueryStringStripper>
@@ -249,6 +249,18 @@ URLQueryStringStripper::TestGetStripList(nsACString& aStripList) {
                    [](auto& aResult, const auto& aValue) {
                      aResult.Append(NS_ConvertUTF16toUTF8(aValue));
                    });
+  return NS_OK;
+}
+
+/* nsIObserver */
+NS_IMETHODIMP
+URLQueryStringStripper::Observe(nsISupports*, const char* aTopic,
+                                const char16_t*) {
+  // Since this class is created at profile-after-change by the Category
+  // Manager, it's expected to implement nsIObserver; however, we have nothing
+  // interesting to do here.
+  MOZ_ASSERT(strcmp(aTopic, "profile-after-change") == 0);
+
   return NS_OK;
 }
 
