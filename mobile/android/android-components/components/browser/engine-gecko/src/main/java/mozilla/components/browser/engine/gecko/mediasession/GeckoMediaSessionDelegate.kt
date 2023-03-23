61b7e4271251f061a238f7079809effb3a774c73
Roger Yang <royang@mozilla.com>
2023-03-23 15:01:57 -0400

    Bug 1796357 - Remove media meta data bitmap workaround

--- mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/mediasession/GeckoMediaSessionDelegate.kt
+++ mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/mediasession/GeckoMediaSessionDelegate.kt
@@ -15,7 +15,6 @@ import org.mozilla.geckoview.MediaSession as GeckoViewMediaSession
 
 private const val ARTWORK_RETRIEVE_TIMEOUT = 1000L
 private const val ARTWORK_IMAGE_SIZE = 48
-private const val ARTWORK_ERROR_SIZE = 1
 
 internal class GeckoMediaSessionDelegate(
     private val engineSession: GeckoEngineSession,
@@ -41,18 +40,9 @@ internal class GeckoMediaSessionDelegate(
         val getArtwork: (suspend () -> Bitmap?)? = metaData.artwork?.let {
             {
                 try {
-                    var bitmap = withTimeoutOrNull(ARTWORK_RETRIEVE_TIMEOUT) {
+                    withTimeoutOrNull(ARTWORK_RETRIEVE_TIMEOUT) {
                         it.getBitmap(ARTWORK_IMAGE_SIZE).await()
                     }
-
-                    /* workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=1697255 */
-                    if (bitmap != null &&
-                        (bitmap.height <= ARTWORK_ERROR_SIZE || bitmap.width <= ARTWORK_ERROR_SIZE)
-                    ) {
-                        bitmap = null
-                    }
-
-                    bitmap
                 } catch (e: ImageProcessingException) {
                     null
                 }
