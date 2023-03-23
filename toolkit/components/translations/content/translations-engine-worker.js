3907826d4217909520f5a2f82915033363d621e2
Greg Tatum <tatum.creative@gmail.com>
2023-03-23 22:00:35 +0000

    Bug 1823702 - Add a trace logging level; r=nordzilla
    Logging message translations is useful, but really really verbose. This
    lets us adjust the granularity a bit more.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173193

--- toolkit/components/translations/content/translations-engine-worker.js
+++ toolkit/components/translations/content/translations-engine-worker.js
@@ -14,9 +14,14 @@
 importScripts("chrome://global/content/translations/bergamot-translator.js");
 
 // Respect the preference "browser.translations.logLevel".
-let _isLoggingEnabled = false;
+let _loggingLevel = "Error";
 function log(...args) {
-  if (_isLoggingEnabled) {
+  if (_loggingLevel !== "Error" && _loggingLevel !== "Warn") {
+    console.log("Translations:", ...args);
+  }
+}
+function trace(...args) {
+  if (_loggingLevel === "Trace" || _loggingLevel === "All") {
     console.log("Translations:", ...args);
   }
 }
@@ -60,7 +65,7 @@ async function handleInitializationMessage({ data }) {
       fromLanguage,
       toLanguage,
       enginePayload,
-      isLoggingEnabled,
+      logLevel,
       innerWindowId,
     } = data;
 
@@ -71,9 +76,9 @@ async function handleInitializationMessage({ data }) {
       throw new Error('Worker initialization missing "toLanguage"');
     }
 
-    if (isLoggingEnabled) {
+    if (logLevel) {
       // Respect the "browser.translations.logLevel" preference.
-      _isLoggingEnabled = true;
+      _loggingLevel = logLevel;
     }
 
     let engine;
@@ -141,6 +146,16 @@ function handleMessages(engine) {
               isHTML,
               innerWindowId
             );
+
+            // This logging level can be very verbose and slow, so only do it under the
+            // "Trace" level, which is the most verbose. Set the logging level to "Info" to avoid
+            // these, and get all of the other logs.
+            trace("Translation complete", {
+              messageBatch,
+              translations,
+              isHTML,
+              innerWindowId,
+            });
 
             postMessage({
               type: "translation-response",
