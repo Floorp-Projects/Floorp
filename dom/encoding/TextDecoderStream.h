af4cc69b54905c207bc548a7aece0c13ec1d9148
Iulian Moraru <imoraru@mozilla.com>
2023-03-25 00:11:49 +0200

    Backed out changeset e5ef47700fe9 (bug 1823619) for causing mochitest failures related to CompressionStream. CLOSED TREE

--- dom/encoding/TextDecoderStream.h
+++ dom/encoding/TextDecoderStream.h
@@ -69,9 +69,6 @@ class TextDecoderStream final : public nsISupports,
   RefPtr<TransformStream> mStream;
 };
 
-Span<const uint8_t> ExtractSpanFromBufferSource(
-    JSContext* aCx, JS::Handle<JS::Value> aBufferSource, ErrorResult& aRv);
-
 }  // namespace mozilla::dom
 
 #endif  // DOM_ENCODING_TEXTDECODERSTREAM_H_
