10f56470595f995d4405d80862df92881b8ecaeb
Julian Descottes <jdescottes@mozilla.com>
2023-03-24 16:14:44 +0000

    Bug 1815460 - [devtools] Merge addRequestPostData, addResponseHeaders, addResponseCookies into addResponseStart r=webdriver-reviewers,ochameau,bomsy,devtools-reviewers,whimboo
    Depends on D169361
    
    Differential Revision: https://phabricator.services.mozilla.com/D168538

--- remote/shared/listeners/NetworkEventRecord.sys.mjs
+++ remote/shared/listeners/NetworkEventRecord.sys.mjs
@@ -108,24 +108,38 @@ export class NetworkEventRecord {
    *
    * Required API for a NetworkObserver event owner.
    *
-   * @param {Object} response
-   *     The response information.
-   * @param {string} rawHeaders
-   *     The raw headers source.
+   *
+   * @param {object} options
+   * @param {nsIChannel} options.channel
+   *     The channel.
+   * @param {boolean} options.fromCache
+   * @param {string} options.rawHeaders
    */
-  addResponseStart(response, rawHeaders) {
+  addResponseStart(options) {
+    const { channel, fromCache, rawHeaders = "" } = options;
+    const { headers } = lazy.NetworkUtils.fetchResponseHeadersAndCookies(
+      channel
+    );
+
+    const headersSize = rawHeaders.length;
     this.#responseData = {
       ...this.#responseData,
-      bodySize: response.bodySize,
-      bytesReceived: response.transferredSize,
-      fromCache: this.#fromCache || response.fromCache,
-      // Note: at this point we only have access to the headers size. Parsed
-      // headers will be added in addResponseHeaders.
-      headersSize: response.headersSize,
-      protocol: response.protocol,
-      status: parseInt(response.status),
-      statusText: response.statusText,
+      bodySize: 0,
+      bytesReceived: headersSize,
+      fromCache: this.#fromCache || !!fromCache,
+      headers,
+      headersSize,
+      mimeType: this.#getMimeType(),
+      protocol: lazy.NetworkUtils.getProtocol(channel),
+      status: channel.responseStatus,
+      statusText: channel.responseStatusText,
     };
+
+    // This should be triggered when all headers have been received, matching
+    // the WebDriverBiDi response started trigger in `4.6. HTTP-network fetch`
+    // from the fetch specification, based on the PR visible at
+    // https://github.com/whatwg/fetch/pull/1540
+    this.#emitResponseStarted();
   }
 
   /**
@@ -141,40 +155,6 @@ export class NetworkEventRecord {
    *     True if the corresponding channel raced the cache and network requests.
    */
   addSecurityInfo(info, isRacing) {}
-
-  /**
-   * Add network response headers.
-   *
-   * Required API for a NetworkObserver event owner.
-   *
-   * @param {Array} headers
-   *     The response headers array.
-   */
-  addResponseHeaders(headers) {
-    this.#responseData.headers = headers;
-
-    // The mimetype info should also be available on the wrapped channel after
-    // headers have been parsed.
-    this.#responseData.mimeType = this.#getMimeType();
-
-    // This should be triggered when all headers have been received, matching
-    // the WebDriverBiDi response started trigger in `4.6. HTTP-network fetch`
-    // from the fetch specification, based on the PR visible at
-    // https://github.com/whatwg/fetch/pull/1540
-    this.#emitResponseStarted();
-  }
-
-  /**
-   * Add network response cookies.
-   *
-   * Required API for a NetworkObserver event owner.
-   *
-   * Not used for RemoteAgent.
-   *
-   * @param {Array} cookies
-   *     The response cookies array.
-   */
-  addResponseCookies(cookies) {}
 
   /**
    * Add network event timings.
