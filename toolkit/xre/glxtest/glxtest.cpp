f0df4240f854a08c807cc790f917df1d1dc23078
stransky <stransky@redhat.com>
2023-03-27 10:16:46 +0000

    Bug 1787182 [Linux] Implement glxtest test as binary r=emilio
    glxtest is run later when Firefox already spawns threads. Recently glxtest runs in forked process
    which doesn't work correctly in multi-thread environment, so we need to move glxtest to different binary file
    and launch it as stand alone code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173486

--- toolkit/xre/glxtest/glxtest.cpp
+++ toolkit/xre/glxtest/glxtest.cpp
@@ -24,15 +24,14 @@
 #include <dlfcn.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <vector>
+#include <stdint.h>
+#include <string.h>
 
 #if defined(MOZ_ASAN) || defined(FUZZING)
 #  include <signal.h>
 #endif
-
-#include "mozilla/Unused.h"
-#include "nsAppRunner.h"  // for IsWaylandEnabled on IsX11EGLEnabled
-#include "stdint.h"
-#include "nsExceptionHandler.h"
 
 #ifdef __SUNPRO_CC
 #  include <stdio.h>
@@ -42,11 +41,6 @@
 #  include "X11/Xlib.h"
 #  include "X11/Xutil.h"
 #  include <X11/extensions/Xrandr.h>
-#endif
-
-#ifdef MOZ_WAYLAND
-#  include "mozilla/widget/mozwayland.h"
-#  include "prlink.h"
 #endif
 
 #ifdef MOZ_X11
@@ -167,6 +161,10 @@ typedef struct _drmDevice {
 
 #define EXIT_FAILURE_BUFFER_TOO_SMALL 2
 
+// Print VA-API test results to stdout and logging to stderr
+#define OUTPUT_PIPE 1
+#define LOG_PIPE 2
+
 namespace mozilla {
 namespace widget {
 // the read end of the pipe, which will be used by GfxInfo
@@ -176,15 +174,10 @@ extern pid_t glxtest_pid;
 }  // namespace widget
 }  // namespace mozilla
 
-// the write end of the pipe, which we're going to write to
-static int write_end_of_the_pipe = -1;
-
 // our buffer, size and used length
 static char* glxtest_buf = nullptr;
 static int glxtest_bufsize = 0;
 static int glxtest_length = 0;
-
-static char* glxtest_render_device_path = nullptr;
 
 // C++ standard collides with C standard in that it doesn't allow casting void*
 // to function pointer types. So the work-around is to convert first to size_t.
@@ -225,14 +218,9 @@ static void record_warning(const char* str) {
 }
 
 static void record_flush() {
-  mozilla::Unused << write(write_end_of_the_pipe, glxtest_buf, glxtest_length);
-  if (auto* debugFile = getenv("MOZ_GFX_DEBUG_FILE")) {
-    auto fd = open(debugFile, O_CREAT | O_WRONLY | O_TRUNC,
-                   S_IRUSR | S_IRGRP | S_IROTH);
-    if (fd != -1) {
-      mozilla::Unused << write(fd, glxtest_buf, glxtest_length);
-      close(fd);
-    }
+  (void)write(OUTPUT_PIPE, glxtest_buf, glxtest_length);
+  if (getenv("MOZ_GFX_DEBUG")) {
+    (void)write(LOG_PIPE, glxtest_buf, glxtest_length);
   }
 }
 
@@ -244,7 +232,6 @@ static int x_error_handler(Display*, XErrorEvent* ev) {
       ev->error_code, ev->request_code, ev->minor_code);
   record_flush();
   _exit(EXIT_FAILURE);
-  return 0;
 }
 #endif
 
@@ -253,24 +240,6 @@ static int x_error_handler(Display*, XErrorEvent* ev) {
 // memory leak errors because we run it inside a short lived fork and we don't
 // care about leaking memory
 extern "C" {
-
-static void close_logging() {
-  // we want to redirect to /dev/null stdout, stderr, and while we're at it,
-  // any PR logging file descriptors. To that effect, we redirect all positive
-  // file descriptors up to what open() returns here. In particular, 1 is stdout
-  // and 2 is stderr.
-  int fd = open("/dev/null", O_WRONLY);
-  for (int i = 1; i < fd; i++) {
-    dup2(fd, i);
-  }
-  close(fd);
-
-  if (getenv("MOZ_AVOID_OPENGL_ALTOGETHER")) {
-    const char* msg = "ERROR\nMOZ_AVOID_OPENGL_ALTOGETHER envvar set";
-    mozilla::Unused << write(write_end_of_the_pipe, msg, strlen(msg));
-    exit(EXIT_FAILURE);
-  }
-}
 
 #define PCI_FILL_IDENT 0x0001
 #define PCI_FILL_CLASS 0x0020
@@ -363,7 +332,6 @@ static void get_pci_status() {
 #ifdef MOZ_WAYLAND
 static void set_render_device_path(const char* render_device_path) {
   record_value("DRM_RENDERDEVICE\n%s\n", render_device_path);
-  glxtest_render_device_path = strdup(render_device_path);
 }
 
 static bool device_has_name(const drmDevice* device, const char* name) {
@@ -720,14 +688,26 @@ static void get_xrandr_info(Display* dpy) {
 
   Window root = RootWindow(dpy, DefaultScreen(dpy));
   XRRProviderResources* pr = XRRGetProviderResources(dpy, root);
+  if (!pr) {
+    return;
+  }
   XRRScreenResources* res = XRRGetScreenResourcesCurrent(dpy, root);
+  if (!res) {
+    XRRFreeProviderResources(pr);
+    return;
+  }
   if (pr->nproviders != 0) {
     record_value("DDX_DRIVER\n");
     for (int i = 0; i < pr->nproviders; i++) {
       XRRProviderInfo* info = XRRGetProviderInfo(dpy, res, pr->providers[i]);
-      record_value("%s%s", info->name, i == pr->nproviders - 1 ? ";\n" : ";");
+      if (info) {
+        record_value("%s%s", info->name, i == pr->nproviders - 1 ? ";\n" : ";");
+        XRRFreeProviderInfo(info);
+      }
     }
   }
+  XRRFreeScreenResources(res);
+  XRRFreeProviderResources(pr);
 }
 
 static void glxtest() {
@@ -743,6 +723,7 @@ static void glxtest() {
 
   if (!glXGetProcAddress) {
     record_error("no glXGetProcAddress");
+    dlclose(libgl);
     return;
   }
 
@@ -779,6 +760,7 @@ static void glxtest() {
       !glXCreateContext || !glXMakeCurrent || !glXDestroyContext ||
       !glGetString) {
     record_error(LIBGL_FILENAME " missing methods");
+    dlclose(libgl);
     return;
   }
 
@@ -786,12 +768,14 @@ static void glxtest() {
   Display* dpy = XOpenDisplay(nullptr);
   if (!dpy) {
     record_error("Unable to open a connection to the X server");
+    dlclose(libgl);
     return;
   }
 
   ///// Check that the GLX extension is present /////
   if (!glXQueryExtension(dpy, nullptr, nullptr)) {
     record_error("GLX extension missing");
+    dlclose(libgl);
     return;
   }
 
@@ -808,6 +792,7 @@ static void glxtest() {
     vInfo = glXChooseVisual(dpy, DefaultScreen(dpy), attribs2);
     if (!vInfo) {
       record_error("No visuals found");
+      dlclose(libgl);
       return;
     }
   }
@@ -949,9 +934,21 @@ static bool x11_egltest() {
 
 #ifdef MOZ_WAYLAND
 static void wayland_egltest() {
+  static auto sWlDisplayConnect = (struct wl_display * (*)(const char*))
+      dlsym(RTLD_DEFAULT, "wl_display_connect");
+  static auto sWlDisplayRoundtrip =
+      (int (*)(struct wl_display*))dlsym(RTLD_DEFAULT, "wl_display_roundtrip");
+  static auto sWlDisplayDisconnect = (void (*)(struct wl_display*))dlsym(
+      RTLD_DEFAULT, "wl_display_disconnect");
+
+  if (!sWlDisplayConnect || !sWlDisplayRoundtrip || !sWlDisplayDisconnect) {
+    record_error("Missing Wayland libraries");
+    return;
+  }
+
   // NOTE: returns false to fall back to X11 when the Wayland socket doesn't
   // exist but fails with record_error if something actually went wrong
-  struct wl_display* dpy = wl_display_connect(nullptr);
+  struct wl_display* dpy = sWlDisplayConnect(nullptr);
   if (!dpy) {
     record_error("Could not connect to wayland socket");
     return;
@@ -963,14 +960,14 @@ static void wayland_egltest() {
 
   // This is enough to crash some broken NVIDIA prime + Wayland setups, see
   // https://github.com/NVIDIA/egl-wayland/issues/41 and bug 1768260.
-  wl_display_roundtrip(dpy);
+  sWlDisplayRoundtrip(dpy);
 
-  wl_display_disconnect(dpy);
+  sWlDisplayDisconnect(dpy);
   record_value("TEST_TYPE\nEGL\n");
 }
 #endif
 
-int childgltest() {
+int childgltest(bool aWayland) {
   enum { bufsize = 2048 };
   char buf[bufsize];
 
@@ -983,18 +980,18 @@ int childgltest() {
   get_pci_status();
 
 #ifdef MOZ_WAYLAND
-  if (IsWaylandEnabled()) {
+  if (aWayland) {
     wayland_egltest();
-  } else
+  }
 #endif
-  {
 #ifdef MOZ_X11
+  if (!aWayland) {
     // TODO: --display command line argument is not properly handled
     if (!x11_egltest()) {
       glxtest();
     }
-#endif
   }
+#endif
 
   // Finally write buffered data to the pipe.
   record_flush();
@@ -1009,44 +1006,48 @@ int childgltest() {
 
 }  // extern "C"
 
-/** \returns true in the child glxtest process, false in the parent process */
-bool fire_glxtest_process() {
-  int pfd[2];
-  if (pipe(pfd) == -1) {
-    perror("pipe");
-    return false;
-  }
-  pid_t pid = fork();
-  if (pid < 0) {
-    perror("fork");
-    close(pfd[0]);
-    close(pfd[1]);
-    return false;
-  }
-  // The child exits early to avoid running the full shutdown sequence and avoid
-  // conflicting with threads we have already spawned (like the profiler).
-  if (pid == 0) {
-    close(pfd[0]);
-    write_end_of_the_pipe = pfd[1];
-    close_logging();
-    // This process is expected to be crashy, and we
-    // don't want the user to see its crashes. That's the whole reason for
-    // doing this in a separate process.
-    if (CrashReporter::GetEnabled()) {
-      CrashReporter::UnsetExceptionHandler();
-    }
-#if defined(MOZ_ASAN) || defined(FUZZING)
-    // If handle_segv=1 (default), then glxtest crash will print a sanitizer
-    // report which can confuse the harness in fuzzing automation.
-    signal(SIGSEGV, SIG_DFL);
-#endif
-    int rv = childgltest();
-    close(pfd[1]);
-    _exit(rv);
-  }
+static void PrintUsage() {
+  printf(
+      "Firefox OpenGL probe utility\n"
+      "\n"
+      "usage: glxtest [options]\n"
+      "\n"
+      "Options:\n"
+      "\n"
+      "  -h --help                 show this message\n"
+      "  -w --wayland              probe OpenGL/EGL on Wayland (default is "
+      "X11)\n"
+      "\n");
+}
 
-  close(pfd[1]);
-  mozilla::widget::glxtest_pipe = pfd[0];
-  mozilla::widget::glxtest_pid = pid;
-  return false;
+int main(int argc, char** argv) {
+  struct option longOptions[] = {{"help", no_argument, NULL, 'h'},
+                                 {"wayland", no_argument, NULL, 'w'},
+                                 {NULL, 0, NULL, 0}};
+  const char* shortOptions = "hw";
+  int c;
+  bool wayland = false;
+  while ((c = getopt_long(argc, argv, shortOptions, longOptions, NULL)) != -1) {
+    switch (c) {
+      case 'w':
+        wayland = true;
+        break;
+      case 'h':
+        PrintUsage();
+        return 0;
+      default:
+        break;
+    }
+  }
+  if (getenv("MOZ_AVOID_OPENGL_ALTOGETHER")) {
+    const char* msg = "ERROR\nMOZ_AVOID_OPENGL_ALTOGETHER envvar set";
+    (void)write(OUTPUT_PIPE, msg, strlen(msg));
+    exit(EXIT_FAILURE);
+  }
+#if defined(MOZ_ASAN) || defined(FUZZING)
+  // If handle_segv=1 (default), then glxtest crash will print a sanitizer
+  // report which can confuse the harness in fuzzing automation.
+  signal(SIGSEGV, SIG_DFL);
+#endif
+  return childgltest(wayland);
 }
