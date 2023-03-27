c37cf867a08cc9f57e416fea5285f909aa969959
Ziran Sun <zsun@igalia.com>
2023-03-27 12:04:58 +0000

    Bug 1824668 - Make sure "beforetoggle" event is not fired for element removal. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D173672

--- dom/html/nsGenericHTMLElement.h
+++ dom/html/nsGenericHTMLElement.h
@@ -158,7 +158,9 @@ class nsGenericHTMLElement : public nsGenericHTMLElementBase {
   /** Returns true if the event has been cancelled. */
   MOZ_CAN_RUN_SCRIPT bool FireBeforeToggle(bool aIsOpen);
   MOZ_CAN_RUN_SCRIPT void ShowPopover(ErrorResult& aRv);
-  MOZ_CAN_RUN_SCRIPT void HidePopoverInternal(bool aFireEvents,
+  MOZ_CAN_RUN_SCRIPT_BOUNDARY void HidePopoverWithoutRunningScript();
+  MOZ_CAN_RUN_SCRIPT void HidePopoverInternal(bool aFocusPreviousElement,
+                                              bool aFireEvents,
                                               ErrorResult& aRv);
   MOZ_CAN_RUN_SCRIPT void HidePopover(ErrorResult& aRv);
   MOZ_CAN_RUN_SCRIPT void TogglePopover(bool force, ErrorResult& aRv);
