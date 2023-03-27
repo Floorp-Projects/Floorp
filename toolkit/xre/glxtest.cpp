a2a7af54b547313e970de01c1affe8437eddc2bf
Iulian Moraru <imoraru@mozilla.com>
2023-03-27 14:18:06 +0300

    Backed out 5 changesets (bug 1787182) for causing leaks on linux asan. CLOSED TREE
    Backed out changeset 34056df4e1d5 (bug 1787182)
    Backed out changeset fea9dc3de652 (bug 1787182)
    Backed out changeset 7e710b6500f0 (bug 1787182)
    Backed out changeset fe70bf105370 (bug 1787182)
    Backed out changeset 70439dbc72fd (bug 1787182)

--- toolkit/xre/glxtest.cpp
+++ toolkit/xre/glxtest.cpp
@@ -24,14 +24,14 @@
 #include <dlfcn.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <getopt.h>
-#include <vector>
-#include <stdint.h>
-#include <string.h>
 
 #if defined(MOZ_ASAN) || defined(FUZZING)
 #  include <signal.h>
 #endif
+
+#include "mozilla/Unused.h"
+#include "nsAppRunner.h"  // for IsWaylandEnabled on IsX11EGLEnabled
+#include "stdint.h"
 
 #ifdef __SUNPRO_CC
 #  include <stdio.h>
@@ -41,6 +41,13 @@
 #  include "X11/Xlib.h"
 #  include "X11/Xutil.h"
 #  include <X11/extensions/Xrandr.h>
+#endif
+
+#ifdef MOZ_WAYLAND
+#  include "mozilla/widget/mozwayland.h"
+#  include "mozilla/widget/xdg-output-unstable-v1-client-protocol.h"
+#  include "prlink.h"
+#  include "va/va.h"
 #endif
 
 #ifdef MOZ_X11
@@ -161,10 +168,6 @@ typedef struct _drmDevice {
 
 #define EXIT_FAILURE_BUFFER_TOO_SMALL 2
 
-// Print VA-API test results to stdout and logging to stderr
-#define OUTPUT_PIPE 1
-#define LOG_PIPE 2
-
 namespace mozilla {
 namespace widget {
 // the read end of the pipe, which will be used by GfxInfo
@@ -174,10 +177,26 @@ extern pid_t glxtest_pid;
 }  // namespace widget
 }  // namespace mozilla
 
+#ifdef MOZ_WAYLAND
+// bits to use decoding childvaapitest() return values.
+constexpr int CODEC_HW_H264 = 1 << 4;
+constexpr int CODEC_HW_VP8 = 1 << 5;
+constexpr int CODEC_HW_VP9 = 1 << 6;
+constexpr int CODEC_HW_AV1 = 1 << 7;
+
+#  define NVIDIA_VENDOR_STRING "NVIDIA Corporation"
+static bool run_vaapi_test = false;
+#endif
+
+// the write end of the pipe, which we're going to write to
+static int write_end_of_the_pipe = -1;
+
 // our buffer, size and used length
 static char* glxtest_buf = nullptr;
 static int glxtest_bufsize = 0;
 static int glxtest_length = 0;
+
+static char* glxtest_render_device_path = nullptr;
 
 // C++ standard collides with C standard in that it doesn't allow casting void*
 // to function pointer types. So the work-around is to convert first to size_t.
@@ -218,9 +237,14 @@ static void record_warning(const char* str) {
 }
 
 static void record_flush() {
-  (void)write(OUTPUT_PIPE, glxtest_buf, glxtest_length);
-  if (getenv("MOZ_GFX_DEBUG")) {
-    (void)write(LOG_PIPE, glxtest_buf, glxtest_length);
+  mozilla::Unused << write(write_end_of_the_pipe, glxtest_buf, glxtest_length);
+  if (auto* debugFile = getenv("MOZ_GFX_DEBUG_FILE")) {
+    auto fd = open(debugFile, O_CREAT | O_WRONLY | O_TRUNC,
+                   S_IRUSR | S_IRGRP | S_IROTH);
+    if (fd != -1) {
+      mozilla::Unused << write(fd, glxtest_buf, glxtest_length);
+      close(fd);
+    }
   }
 }
 
@@ -232,6 +256,7 @@ static int x_error_handler(Display*, XErrorEvent* ev) {
       ev->error_code, ev->request_code, ev->minor_code);
   record_flush();
   _exit(EXIT_FAILURE);
+  return 0;
 }
 #endif
 
@@ -240,6 +265,24 @@ static int x_error_handler(Display*, XErrorEvent* ev) {
 // memory leak errors because we run it inside a short lived fork and we don't
 // care about leaking memory
 extern "C" {
+
+static void close_logging() {
+  // we want to redirect to /dev/null stdout, stderr, and while we're at it,
+  // any PR logging file descriptors. To that effect, we redirect all positive
+  // file descriptors up to what open() returns here. In particular, 1 is stdout
+  // and 2 is stderr.
+  int fd = open("/dev/null", O_WRONLY);
+  for (int i = 1; i < fd; i++) {
+    dup2(fd, i);
+  }
+  close(fd);
+
+  if (getenv("MOZ_AVOID_OPENGL_ALTOGETHER")) {
+    const char* msg = "ERROR\nMOZ_AVOID_OPENGL_ALTOGETHER envvar set";
+    mozilla::Unused << write(write_end_of_the_pipe, msg, strlen(msg));
+    exit(EXIT_FAILURE);
+  }
+}
 
 #define PCI_FILL_IDENT 0x0001
 #define PCI_FILL_CLASS 0x0020
@@ -332,6 +375,7 @@ static void get_pci_status() {
 #ifdef MOZ_WAYLAND
 static void set_render_device_path(const char* render_device_path) {
   record_value("DRM_RENDERDEVICE\n%s\n", render_device_path);
+  glxtest_render_device_path = strdup(render_device_path);
 }
 
 static bool device_has_name(const drmDevice* device, const char* name) {
@@ -574,6 +618,13 @@ static bool get_egl_gl_status(EGLDisplay dpy,
     return false;
   }
 
+#ifdef MOZ_WAYLAND
+  // Enable VA-API testing on EGL/non-NVIDIA setup (Bug 1799747).
+  if (!strstr((const char*)vendorString, NVIDIA_VENDOR_STRING)) {
+    run_vaapi_test = true;
+  }
+#endif
+
   EGLDeviceEXT device;
   if (eglQueryDisplayAttribEXT(dpy, EGL_DEVICE_EXT, (EGLAttrib*)&device) ==
       EGL_TRUE) {
@@ -688,26 +739,14 @@ static void get_xrandr_info(Display* dpy) {
 
   Window root = RootWindow(dpy, DefaultScreen(dpy));
   XRRProviderResources* pr = XRRGetProviderResources(dpy, root);
-  if (!pr) {
-    return;
-  }
   XRRScreenResources* res = XRRGetScreenResourcesCurrent(dpy, root);
-  if (!res) {
-    XRRFreeProviderResources(pr);
-    return;
-  }
   if (pr->nproviders != 0) {
     record_value("DDX_DRIVER\n");
     for (int i = 0; i < pr->nproviders; i++) {
       XRRProviderInfo* info = XRRGetProviderInfo(dpy, res, pr->providers[i]);
-      if (info) {
-        record_value("%s%s", info->name, i == pr->nproviders - 1 ? ";\n" : ";");
-        XRRFreeProviderInfo(info);
-      }
+      record_value("%s%s", info->name, i == pr->nproviders - 1 ? ";\n" : ";");
     }
   }
-  XRRFreeScreenResources(res);
-  XRRFreeProviderResources(pr);
 }
 
 static void glxtest() {
@@ -723,7 +762,6 @@ static void glxtest() {
 
   if (!glXGetProcAddress) {
     record_error("no glXGetProcAddress");
-    dlclose(libgl);
     return;
   }
 
@@ -760,7 +798,6 @@ static void glxtest() {
       !glXCreateContext || !glXMakeCurrent || !glXDestroyContext ||
       !glGetString) {
     record_error(LIBGL_FILENAME " missing methods");
-    dlclose(libgl);
     return;
   }
 
@@ -768,14 +805,12 @@ static void glxtest() {
   Display* dpy = XOpenDisplay(nullptr);
   if (!dpy) {
     record_error("Unable to open a connection to the X server");
-    dlclose(libgl);
     return;
   }
 
   ///// Check that the GLX extension is present /////
   if (!glXQueryExtension(dpy, nullptr, nullptr)) {
     record_error("GLX extension missing");
-    dlclose(libgl);
     return;
   }
 
@@ -792,7 +827,6 @@ static void glxtest() {
     vInfo = glXChooseVisual(dpy, DefaultScreen(dpy), attribs2);
     if (!vInfo) {
       record_error("No visuals found");
-      dlclose(libgl);
       return;
     }
   }
@@ -934,21 +968,9 @@ static bool x11_egltest() {
 
 #ifdef MOZ_WAYLAND
 static void wayland_egltest() {
-  static auto sWlDisplayConnect = (struct wl_display * (*)(const char*))
-      dlsym(RTLD_DEFAULT, "wl_display_connect");
-  static auto sWlDisplayRoundtrip =
-      (int (*)(struct wl_display*))dlsym(RTLD_DEFAULT, "wl_display_roundtrip");
-  static auto sWlDisplayDisconnect = (void (*)(struct wl_display*))dlsym(
-      RTLD_DEFAULT, "wl_display_disconnect");
-
-  if (!sWlDisplayConnect || !sWlDisplayRoundtrip || !sWlDisplayDisconnect) {
-    record_error("Missing Wayland libraries");
-    return;
-  }
-
   // NOTE: returns false to fall back to X11 when the Wayland socket doesn't
   // exist but fails with record_error if something actually went wrong
-  struct wl_display* dpy = sWlDisplayConnect(nullptr);
+  struct wl_display* dpy = wl_display_connect(nullptr);
   if (!dpy) {
     record_error("Could not connect to wayland socket");
     return;
@@ -960,14 +982,246 @@ static void wayland_egltest() {
 
   // This is enough to crash some broken NVIDIA prime + Wayland setups, see
   // https://github.com/NVIDIA/egl-wayland/issues/41 and bug 1768260.
-  sWlDisplayRoundtrip(dpy);
+  wl_display_roundtrip(dpy);
 
-  sWlDisplayDisconnect(dpy);
+  wl_display_disconnect(dpy);
   record_value("TEST_TYPE\nEGL\n");
+}
+
+static constexpr struct {
+  VAProfile mVAProfile;
+  nsLiteralCString mName;
+} kVAAPiProfileName[] = {
+#  define MAP(v) \
+    { VAProfile##v, nsLiteralCString(#v) }
+    MAP(H264ConstrainedBaseline),
+    MAP(H264Main),
+    MAP(H264High),
+    MAP(VP8Version0_3),
+    MAP(VP9Profile0),
+    MAP(VP9Profile2),
+    MAP(AV1Profile0),
+    MAP(AV1Profile1),
+#  undef MAP
+};
+
+static const char* VAProfileName(VAProfile aVAProfile) {
+  for (const auto& profile : kVAAPiProfileName) {
+    if (profile.mVAProfile == aVAProfile) {
+      return profile.mName.get();
+    }
+  }
+  return nullptr;
+}
+
+int childvaapitest() {
+  int renderDeviceFD = -1;
+  VAProfile* profiles = nullptr;
+  VAEntrypoint* entryPoints = nullptr;
+  PRLibrary* libDrm = nullptr;
+  VADisplay display = nullptr;
+
+  auto autoRelease = mozilla::MakeScopeExit([&] {
+    if (renderDeviceFD > -1) {
+      close(renderDeviceFD);
+    }
+    delete[] profiles;
+    delete[] entryPoints;
+    if (display) {
+      vaTerminate(display);
+    }
+    if (libDrm) {
+      PR_UnloadLibrary(libDrm);
+    }
+  });
+
+  renderDeviceFD = open(glxtest_render_device_path, O_RDWR);
+  if (renderDeviceFD == -1) {
+    return 3;
+  }
+
+  PRLibSpec lspec;
+  lspec.type = PR_LibSpec_Pathname;
+  const char* libName = "libva-drm.so.2";
+  lspec.value.pathname = libName;
+  libDrm = PR_LoadLibraryWithFlags(lspec, PR_LD_NOW | PR_LD_LOCAL);
+  if (!libDrm) {
+    return 4;
+  }
+
+  static auto sVaGetDisplayDRM =
+      (void* (*)(int fd))PR_FindSymbol(libDrm, "vaGetDisplayDRM");
+  if (!sVaGetDisplayDRM) {
+    return 5;
+  }
+
+  display = sVaGetDisplayDRM(renderDeviceFD);
+  if (!display) {
+    return 6;
+  }
+
+  int major, minor;
+  VAStatus status = vaInitialize(display, &major, &minor);
+  if (status != VA_STATUS_SUCCESS) {
+    return 7;
+  }
+
+  int maxProfiles = vaMaxNumProfiles(display);
+  int maxEntryPoints = vaMaxNumEntrypoints(display);
+  if (MOZ_UNLIKELY(maxProfiles <= 0 || maxEntryPoints <= 0)) {
+    return 8;
+  }
+
+  profiles = new VAProfile[maxProfiles];
+  int numProfiles = 0;
+  status = vaQueryConfigProfiles(display, profiles, &numProfiles);
+  if (status != VA_STATUS_SUCCESS) {
+    return 9;
+  }
+  numProfiles = std::min(numProfiles, maxProfiles);
+
+  entryPoints = new VAEntrypoint[maxEntryPoints];
+  int codecs = 0;
+  bool foundProfile = false;
+  for (int p = 0; p < numProfiles; p++) {
+    VAProfile profile = profiles[p];
+
+    // Check only supported profiles
+    if (!VAProfileName(profile)) {
+      continue;
+    }
+
+    int numEntryPoints = 0;
+    status = vaQueryConfigEntrypoints(display, profile, entryPoints,
+                                      &numEntryPoints);
+    if (status != VA_STATUS_SUCCESS) {
+      continue;
+    }
+    numEntryPoints = std::min(numEntryPoints, maxEntryPoints);
+
+    for (int entry = 0; entry < numEntryPoints; entry++) {
+      if (entryPoints[entry] != VAEntrypointVLD) {
+        continue;
+      }
+      VAConfigID config = VA_INVALID_ID;
+      status = vaCreateConfig(display, profile, entryPoints[entry], nullptr, 0,
+                              &config);
+      if (status == VA_STATUS_SUCCESS) {
+        const char* profstr = VAProfileName(profile);
+        // VAProfileName returns null on failure, making the below calls safe
+        if (!strncmp(profstr, "H264", 4)) {
+          codecs |= CODEC_HW_H264;
+        } else if (!strncmp(profstr, "VP8", 3)) {
+          codecs |= CODEC_HW_VP8;
+        } else if (!strncmp(profstr, "VP9", 3)) {
+          codecs |= CODEC_HW_VP9;
+        } else if (!strncmp(profstr, "AV1", 3)) {
+          codecs |= CODEC_HW_AV1;
+        } else {
+          char warnbuf[128];
+          SprintfBuf(warnbuf, 128, "VA-API test unknown profile: %s", profstr);
+          record_warning(warnbuf);
+        }
+        vaDestroyConfig(display, config);
+        foundProfile = true;
+      }
+    }
+  }
+  if (foundProfile) {
+    return codecs;
+  }
+  return 10;
+}
+
+static void vaapitest() {
+  if (!glxtest_render_device_path) {
+    return;
+  }
+
+  pid_t vaapitest_pid = fork();
+  if (vaapitest_pid == 0) {
+#  if defined(MOZ_ASAN) || defined(FUZZING)
+    // If handle_segv=1 (default), then glxtest crash will print a sanitizer
+    // report which can confuse the harness in fuzzing automation.
+    signal(SIGSEGV, SIG_DFL);
+#  endif
+    int vaapirv = childvaapitest();
+    _exit(vaapirv);
+  } else if (vaapitest_pid > 0) {
+    int vaapitest_status = 0;
+    bool wait_for_vaapitest_process = true;
+
+    while (wait_for_vaapitest_process) {
+      if (waitpid(vaapitest_pid, &vaapitest_status, 0) == -1) {
+        wait_for_vaapitest_process = false;
+        record_warning(
+            "VA-API test failed: waiting for VA-API process failed.");
+      } else if (WIFEXITED(vaapitest_status) || WIFSIGNALED(vaapitest_status)) {
+        wait_for_vaapitest_process = false;
+      }
+    }
+
+    if (WIFEXITED(vaapitest_status)) {
+      // Note that WEXITSTATUS only returns least significant 8 bits
+      // of the exit code, despite returning type int.
+      int exitcode = WEXITSTATUS(vaapitest_status);
+      int codecs = exitcode & 0b1111'0000;
+      exitcode &= 0b0000'1111;
+      switch (exitcode) {
+        case 0:
+          char codecstr[80];
+          record_value("VAAPI_SUPPORTED\nTRUE\n");
+          SprintfBuf(codecstr, 80, "%d", codecs);
+          record_value("VAAPI_HWCODECS\n");
+          record_value(codecstr);
+          record_value("\n");
+          break;
+        case 3:
+          record_warning(
+              "VA-API test failed: opening render device path failed.");
+          break;
+        case 4:
+          record_warning(
+              "VA-API test failed: missing or old libva-drm library.");
+          break;
+        case 5:
+          record_warning("VA-API test failed: missing vaGetDisplayDRM.");
+          break;
+        case 6:
+          record_warning("VA-API test failed: failed to get vaGetDisplayDRM.");
+          break;
+        case 7:
+          record_warning(
+              "VA-API test failed: failed to initialise VAAPI connection.");
+          break;
+        case 8:
+          record_warning(
+              "VA-API test failed: wrong VAAPI profiles/entry point nums.");
+          break;
+        case 9:
+          record_warning("VA-API test failed: vaQueryConfigProfiles() failed.");
+          break;
+        case 10:
+          record_warning(
+              "VA-API test failed: no supported VAAPI profile found.");
+          break;
+        default:
+          record_warning(
+              "VA-API test failed: Something unexpected went wrong.");
+          break;
+      }
+    } else {
+      record_warning(
+          "VA-API test failed: process crashed. Please check your VA-API "
+          "drivers.");
+    }
+  } else {
+    record_warning("VA-API test failed: Could not fork process.");
+  }
 }
 #endif
 
-int childgltest(bool aWayland) {
+int childgltest() {
   enum { bufsize = 2048 };
   char buf[bufsize];
 
@@ -980,16 +1234,22 @@ int childgltest(bool aWayland) {
   get_pci_status();
 
 #ifdef MOZ_WAYLAND
-  if (aWayland) {
+  if (IsWaylandEnabled()) {
     wayland_egltest();
-  }
+  } else
 #endif
+  {
 #ifdef MOZ_X11
-  if (!aWayland) {
     // TODO: --display command line argument is not properly handled
     if (!x11_egltest()) {
       glxtest();
     }
+#endif
+  }
+
+#ifdef MOZ_WAYLAND
+  if (run_vaapi_test) {
+    vaapitest();
   }
 #endif
 
@@ -1006,48 +1266,38 @@ int childgltest(bool aWayland) {
 
 }  // extern "C"
 
-static void PrintUsage() {
-  printf(
-      "Firefox OpenGL probe utility\n"
-      "\n"
-      "usage: glxtest [options]\n"
-      "\n"
-      "Options:\n"
-      "\n"
-      "  -h --help                 show this message\n"
-      "  -w --wayland              probe OpenGL/EGL on Wayland (default is "
-      "X11)\n"
-      "\n");
-}
-
-int main(int argc, char** argv) {
-  struct option longOptions[] = {{"help", no_argument, NULL, 'h'},
-                                 {"wayland", no_argument, NULL, 'w'},
-                                 {NULL, 0, NULL, 0}};
-  const char* shortOptions = "hw";
-  int c;
-  bool wayland = false;
-  while ((c = getopt_long(argc, argv, shortOptions, longOptions, NULL)) != -1) {
-    switch (c) {
-      case 'w':
-        wayland = true;
-        break;
-      case 'h':
-        PrintUsage();
-        return 0;
-      default:
-        break;
-    }
+/** \returns true in the child glxtest process, false in the parent process */
+bool fire_glxtest_process() {
+  int pfd[2];
+  if (pipe(pfd) == -1) {
+    perror("pipe");
+    return false;
   }
-  if (getenv("MOZ_AVOID_OPENGL_ALTOGETHER")) {
-    const char* msg = "ERROR\nMOZ_AVOID_OPENGL_ALTOGETHER envvar set";
-    (void)write(OUTPUT_PIPE, msg, strlen(msg));
-    exit(EXIT_FAILURE);
+  pid_t pid = fork();
+  if (pid < 0) {
+    perror("fork");
+    close(pfd[0]);
+    close(pfd[1]);
+    return false;
   }
+  // The child exits early to avoid running the full shutdown sequence and avoid
+  // conflicting with threads we have already spawned (like the profiler).
+  if (pid == 0) {
+    close(pfd[0]);
+    write_end_of_the_pipe = pfd[1];
+    close_logging();
 #if defined(MOZ_ASAN) || defined(FUZZING)
-  // If handle_segv=1 (default), then glxtest crash will print a sanitizer
-  // report which can confuse the harness in fuzzing automation.
-  signal(SIGSEGV, SIG_DFL);
+    // If handle_segv=1 (default), then glxtest crash will print a sanitizer
+    // report which can confuse the harness in fuzzing automation.
+    signal(SIGSEGV, SIG_DFL);
 #endif
-  return childgltest(wayland);
+    int rv = childgltest();
+    close(pfd[1]);
+    _exit(rv);
+  }
+
+  close(pfd[1]);
+  mozilla::widget::glxtest_pipe = pfd[0];
+  mozilla::widget::glxtest_pid = pid;
+  return false;
 }
