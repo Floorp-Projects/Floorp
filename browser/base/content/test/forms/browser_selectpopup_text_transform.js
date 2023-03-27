a08027f2cc8c67283c88542b0b99982190ee3e12
Lata <imlata1111@gmail.com>
2023-03-27 21:46:02 +0000

    Bug 1790780 - The options of a select do not honor text-transform. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D173607

---
+++ browser/base/content/test/forms/browser_selectpopup_text_transform.js
@@ -0,0 +1,40 @@
+const PAGE = `
+<!doctype html>
+<select style="text-transform: uppercase">
+  <option>abc</option>
+  <option>defg</option>
+</select>
+`;
+
+add_task(async function() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["dom.forms.select.customstyling", true]],
+  });
+  const url = "data:text/html," + encodeURI(PAGE);
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url,
+    },
+    async function(browser) {
+      let popup = await openSelectPopup("click");
+      let menuitems = popup.querySelectorAll("menuitem");
+      is(menuitems[0].textContent, "abc", "Option text should be lowercase");
+      is(menuitems[1].textContent, "defg", "Option text should be lowercase");
+
+      let optionStyle = getComputedStyle(menuitems[0]);
+      is(
+        optionStyle.textTransform,
+        "uppercase",
+        "Option text should be transformed to uppercase"
+      );
+
+      optionStyle = getComputedStyle(menuitems[1]);
+      is(
+        optionStyle.textTransform,
+        "uppercase",
+        "Option text should be transformed to uppercase"
+      );
+    }
+  );
+});
