69f594b843b9bf77a117f612a591d23eed21e19f
Bobby Holley <bobbyholley@gmail.com>
2023-03-27 19:56:47 +0000

    Bug 1824812 — Add more shopee domains to the site permissions blocklist. r=rpl, a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D173752

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4182,7 +4182,7 @@ pref("cookiebanners.listService.testSkipRemoteSettings", false);
 
 // The domains we will block from installing SitePermsAddons. Comma-separated
 // full domains: any subdomains of the domains listed will also be allowed.
-pref("dom.sitepermsaddon-provider.separatedBlocklistedDomains", "shopee.co.th,alipay.com,miravia.es");
+pref("dom.sitepermsaddon-provider.separatedBlocklistedDomains", "shopee.co.th,shopee.tw,shopee.co.id,shopee.com.my,shopee.vn,shopee.ph,shopee.sg,shopee.com.br,shopee.com,shopee.cn,shopee.io,shopee.pl,shopee.com.mx,shopee.com.co,shopee.cl,shopee.kr,shopee.es,shopee.in,alipay.com,miravia.es");
 
 // Log level for logger in URLQueryStrippingListService
 pref("privacy.query_stripping.listService.logLevel", "Error");
