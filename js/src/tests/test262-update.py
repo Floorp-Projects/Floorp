9a443b2ec8358d761d65994dca1ec4f828d1e1f2
André Bargull <andre.bargull@gmail.com>
2023-03-28 15:29:18 +0000

    Bug 1824802 - Part 2: Update module loading to use importlib. r=mgaudet
    Update to use "importlib" to get rid of the deprecation warning about the "imp" module.
    
    Depends on D173822
    
    Differential Revision: https://phabricator.services.mozilla.com/D173823

--- js/src/tests/test262-update.py
+++ js/src/tests/test262-update.py
@@ -73,17 +73,30 @@ def loadTest262Parser(test262Dir):
     """
     Loads the test262 test record parser.
     """
-    import imp
+    import importlib.machinery
+    import importlib.util
 
-    fileObj = None
-    try:
-        moduleName = "parseTestRecord"
-        packagingDir = os.path.join(test262Dir, "tools", "packaging")
-        (fileObj, pathName, description) = imp.find_module(moduleName, [packagingDir])
-        return imp.load_module(moduleName, fileObj, pathName, description)
-    finally:
-        if fileObj:
-            fileObj.close()
+    packagingDir = os.path.join(test262Dir, "tools", "packaging")
+    moduleName = "parseTestRecord"
+
+    # Create a FileFinder to load Python source files.
+    loader_details = (
+        importlib.machinery.SourceFileLoader,
+        importlib.machinery.SOURCE_SUFFIXES,
+    )
+    finder = importlib.machinery.FileFinder(packagingDir, loader_details)
+
+    # Find the module spec.
+    spec = finder.find_spec(moduleName)
+    if spec is None:
+        raise RuntimeError("Can't find parseTestRecord module")
+
+    # Create and execute the module.
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    # Return the executed module
+    return module
 
 
 def tryParseTestFile(test262parser, source, testName):
