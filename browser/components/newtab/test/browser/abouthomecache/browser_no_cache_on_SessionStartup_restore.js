b28b0eb08994236021f519081dda1f0d0c03acfc
Mike Conley <mconley@mozilla.com>
2023-03-28 13:12:12 +0000

    Bug 1824094 - Don't read from the about:home startup cache if the last session will be recovered. r=Gijs
    We were choosing not to read from the cache if the user has configured the browser
    to restore the last session, but there's also the case where the last session might
    be restored regardless - for example, to recover from a parent-process crash, after
    an update applies, or if using the browser restart shortcut for developers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173782

---
+++ browser/components/newtab/test/browser/abouthomecache/browser_no_cache_on_SessionStartup_restore.js
@@ -0,0 +1,34 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
+
+/**
+ * Tests that if the browser is restoring the last session, not because
+ * it has been configured to, but because of a special condition (recovery
+ * or after applying an update), that we ignore the about:home startup
+ * cache, since it's unlikely to be the first thing loaded.
+ */
+add_task(async function test_no_cache_on_SessionStartup_restore() {
+  let sandbox = sinon.createSandbox();
+  registerCleanupFunction(() => {
+    sandbox.restore();
+  });
+
+  // Let's fake out the condition where SessionStartup thinks it's going to
+  // provide a restored session on startup.
+  sandbox.stub(SessionStartup, "willRestore").callsFake(() => {
+    return true;
+  });
+
+  await withFullyLoadedAboutHome(async browser => {
+    await simulateRestart(browser);
+
+    await ensureDynamicAboutHome(
+      browser,
+      AboutHomeStartupCache.CACHE_RESULT_SCALARS.NOT_LOADING_ABOUTHOME
+    );
+  });
+});
