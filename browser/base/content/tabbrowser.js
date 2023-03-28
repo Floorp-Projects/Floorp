7cf9a86c67b66893c3be50f241ecf5c92cbb9691
Shane Hughes <shughes@mozilla.com>
2023-03-28 00:38:42 +0000

    Bug 1818829 - Implement Feature Callout theme configuration. r=jprickett,omc-reviewers,fxview-reviewers,sfoster
    Add logic to apply theme colors to Feature Callout based on where it's
    going to show. We can use in-content CSS properties for Firefox View and
    other themed system pages, but not for PDF.js, nor for any callouts we
    might show in the browser chrome in the future. For the browser chrome
    in general, we can use the lightweight theme properties directly, in the
    same way the chrome frontend does. But PDF.js is a special case, since
    although it exists in the chrome, it's meant to appear like it's in the
    PDF.js viewer. And the PDF.js viewer has its own theme totally
    independent of everything else. So this dynamically applies themes from
    different sources.
    
    This also fixes the bug where the PDF.js color scheme could mismatch the
    PDF.js viewer if the browser theme and system color scheme don't match,
    e.g. where system color scheme is light but a dark theme is installed,
    or vice versa. For PDF.js specifically, we can use the
    -moz-content-prefers-color-scheme media query to follow the color scheme
    as it exists in the PDF.js viewer page instead of the color scheme in
    the chrome window where the Feature Callout actually exists.
    
    It also adds or modifies some colors that were previously missing or
    different from the prototype, fixes the illegibility of buttons in HCM
    and forced colors mode, and makes some other minor color changes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173088

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -404,6 +404,7 @@
         browser,
         prefName: "browser.pdfjs.feature-tour",
         page: "chrome",
+        theme: { preset: "pdfjs", simulateContent: true },
       });
     },
     _setupInitialBrowserAndTab() {
