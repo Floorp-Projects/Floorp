fdf91a1ed149ac6f05da98dd1ce91c6d11c3d131
Cathy Lu <calu@mozilla.com>
2023-03-28 21:25:05 +0000

    Bug 1815310 - Support persistent events for mobile tabs API r=extension-reviewers,robwu,amejiamarmol,geckoview-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D172746

--- mobile/android/components/extensions/ext-tabs.js
+++ mobile/android/components/extensions/ext-tabs.js
@@ -83,11 +83,162 @@ const tabListener = {
   },
 };
 
-this.tabs = class extends ExtensionAPI {
+this.tabs = class extends ExtensionAPIPersistent {
+  tabEventRegistrar({ event, listener }) {
+    const { extension } = this;
+    const { tabManager } = extension;
+    return ({ fire }) => {
+      const listener2 = (eventName, eventData, ...args) => {
+        if (!tabManager.canAccessTab(eventData.nativeTab)) {
+          return;
+        }
+
+        listener(fire, eventData, ...args);
+      };
+
+      tabTracker.on(event, listener2);
+      return {
+        unregister() {
+          tabTracker.off(event, listener2);
+        },
+        convert(_fire) {
+          fire = _fire;
+        },
+      };
+    };
+  }
+
+  PERSISTENT_EVENTS = {
+    onActivated({ fire, context }, params) {
+      const listener = (eventName, event) => {
+        const { windowId, tabId, isPrivate } = event;
+        if (isPrivate && !context.privateBrowsingAllowed) {
+          return;
+        }
+        // In GeckoView each window has only one tab, so previousTabId is omitted.
+        fire.async({ windowId, tabId });
+      };
+
+      mobileWindowTracker.on("tab-activated", listener);
+      return {
+        unregister() {
+          mobileWindowTracker.off("tab-activated", listener);
+        },
+        convert(_fire, _context) {
+          fire = _fire;
+          context = _context;
+        },
+      };
+    },
+    onCreated: this.tabEventRegistrar({
+      event: "tab-created",
+      listener: (fire, event) => {
+        const { tabManager } = this.extension;
+        fire.async(tabManager.convert(event.nativeTab));
+      },
+    }),
+    onRemoved: this.tabEventRegistrar({
+      event: "tab-removed",
+      listener: (fire, event) => {
+        fire.async(event.tabId, {
+          windowId: event.windowId,
+          isWindowClosing: event.isWindowClosing,
+        });
+      },
+    }),
+    onUpdated({ fire, context }, params) {
+      const { tabManager } = this.extension;
+      const restricted = ["url", "favIconUrl", "title"];
+
+      function sanitize(tab, changeInfo) {
+        const result = {};
+        let nonempty = false;
+        for (const prop in changeInfo) {
+          // In practice, changeInfo contains at most one property from
+          // restricted. Therefore it is not necessary to cache the value
+          // of tab.hasTabPermission outside the loop.
+          if (!restricted.includes(prop) || tab.hasTabPermission) {
+            nonempty = true;
+            result[prop] = changeInfo[prop];
+          }
+        }
+        return [nonempty, result];
+      }
+
+      const fireForTab = (tab, changed) => {
+        const [needed, changeInfo] = sanitize(tab, changed);
+        if (needed) {
+          fire.async(tab.id, changeInfo, tab.convert());
+        }
+      };
+
+      const listener = event => {
+        const needed = [];
+        let nativeTab;
+        switch (event.type) {
+          case "pagetitlechanged": {
+            const window = getBrowserWindow(event.target.ownerGlobal);
+            nativeTab = window.tab;
+
+            needed.push("title");
+            break;
+          }
+
+          case "DOMAudioPlaybackStarted":
+          case "DOMAudioPlaybackStopped": {
+            const window = event.target.ownerGlobal;
+            nativeTab = window.tab;
+            needed.push("audible");
+            break;
+          }
+        }
+
+        if (!nativeTab) {
+          return;
+        }
+
+        const tab = tabManager.getWrapper(nativeTab);
+        const changeInfo = {};
+        for (const prop of needed) {
+          changeInfo[prop] = tab[prop];
+        }
+
+        fireForTab(tab, changeInfo);
+      };
+
+      const statusListener = ({ browser, status, url }) => {
+        const { tab } = browser.ownerGlobal;
+        if (tab) {
+          const changed = { status };
+          if (url) {
+            changed.url = url;
+          }
+
+          fireForTab(tabManager.wrapTab(tab), changed);
+        }
+      };
+
+      windowTracker.addListener("status", statusListener);
+      windowTracker.addListener("pagetitlechanged", listener);
+
+      return {
+        unregister() {
+          windowTracker.removeListener("status", statusListener);
+          windowTracker.removeListener("pagetitlechanged", listener);
+        },
+        convert(_fire, _context) {
+          fire = _fire;
+          context = _context;
+        },
+      };
+    },
+  };
+
   getAPI(context) {
     const { extension } = context;
-
     const { tabManager } = extension;
+    const extensionApi = this;
+    const module = "tabs";
 
     function getTabOrActive(tabId) {
       if (tabId !== null) {
@@ -143,49 +294,25 @@ this.tabs = class extends ExtensionAPI {
       });
     }
 
-    const self = {
+    return {
       tabs: {
         onActivated: new EventManager({
           context,
-          name: "tabs.onActivated",
-          register: fire => {
-            const listener = (eventName, event) => {
-              const { windowId, tabId, isPrivate } = event;
-              if (isPrivate && !context.privateBrowsingAllowed) {
-                return;
-              }
-              fire.async({
-                windowId,
-                tabId,
-                // In GeckoView each window has only one tab, so previousTabId is omitted.
-              });
-            };
-
-            mobileWindowTracker.on("tab-activated", listener);
-            return () => {
-              mobileWindowTracker.off("tab-activated", listener);
-            };
-          },
+          module,
+          event: "onActivated",
+          extensionApi,
         }).api(),
 
         onCreated: new EventManager({
           context,
-          name: "tabs.onCreated",
-          register: fire => {
-            const listener = (eventName, event) => {
-              fire.async(tabManager.convert(event.nativeTab));
-            };
-
-            tabTracker.on("tab-created", listener);
-            return () => {
-              tabTracker.off("tab-created", listener);
-            };
-          },
+          module,
+          event: "onCreated",
+          extensionApi,
         }).api(),
 
         /**
          * Since multiple tabs currently can't be highlighted, onHighlighted
-         * essentially acts an alias for self.tabs.onActivated but returns
+         * essentially acts an alias for tabs.onActivated but returns
          * the tabId in an array to match the API.
          *
          * @see  https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/Tabs/onHighlighted
@@ -201,6 +328,9 @@ this.tabs = class extends ExtensionAPI {
           }
         ),
 
+        // Some events below are not be persisted because they are not implemented.
+        // They do not have an "extensionApi" property with an entry in
+        // PERSISTENT_EVENTS, but instead an empty "register" method.
         onAttached: new EventManager({
           context,
           name: "tabs.onAttached",
@@ -219,20 +349,9 @@ this.tabs = class extends ExtensionAPI {
 
         onRemoved: new EventManager({
           context,
-          name: "tabs.onRemoved",
-          register: fire => {
-            const listener = (eventName, event) => {
-              fire.async(event.tabId, {
-                windowId: event.windowId,
-                isWindowClosing: event.isWindowClosing,
-              });
-            };
-
-            tabTracker.on("tab-removed", listener);
-            return () => {
-              tabTracker.off("tab-removed", listener);
-            };
-          },
+          module,
+          event: "onRemoved",
+          extensionApi,
         }).api(),
 
         onReplaced: new EventManager({
@@ -253,85 +372,9 @@ this.tabs = class extends ExtensionAPI {
 
         onUpdated: new EventManager({
           context,
-          name: "tabs.onUpdated",
-          register: fire => {
-            const restricted = ["url", "favIconUrl", "title"];
-
-            function sanitize(tab, changeInfo) {
-              const result = {};
-              let nonempty = false;
-              for (const prop in changeInfo) {
-                // In practice, changeInfo contains at most one property from
-                // restricted. Therefore it is not necessary to cache the value
-                // of tab.hasTabPermission outside the loop.
-                if (!restricted.includes(prop) || tab.hasTabPermission) {
-                  nonempty = true;
-                  result[prop] = changeInfo[prop];
-                }
-              }
-              return [nonempty, result];
-            }
-
-            const fireForTab = (tab, changed) => {
-              const [needed, changeInfo] = sanitize(tab, changed);
-              if (needed) {
-                fire.async(tab.id, changeInfo, tab.convert());
-              }
-            };
-
-            const listener = event => {
-              const needed = [];
-              let nativeTab;
-              switch (event.type) {
-                case "pagetitlechanged": {
-                  const window = getBrowserWindow(event.target.ownerGlobal);
-                  nativeTab = window.tab;
-
-                  needed.push("title");
-                  break;
-                }
-
-                case "DOMAudioPlaybackStarted":
-                case "DOMAudioPlaybackStopped": {
-                  const window = event.target.ownerGlobal;
-                  nativeTab = window.tab;
-                  needed.push("audible");
-                  break;
-                }
-              }
-
-              if (!nativeTab) {
-                return;
-              }
-
-              const tab = tabManager.getWrapper(nativeTab);
-              const changeInfo = {};
-              for (const prop of needed) {
-                changeInfo[prop] = tab[prop];
-              }
-
-              fireForTab(tab, changeInfo);
-            };
-
-            const statusListener = ({ browser, status, url }) => {
-              const { tab } = browser.ownerGlobal;
-              if (tab) {
-                const changed = { status };
-                if (url) {
-                  changed.url = url;
-                }
-
-                fireForTab(tabManager.wrapTab(tab), changed);
-              }
-            };
-
-            windowTracker.addListener("status", statusListener);
-            windowTracker.addListener("pagetitlechanged", listener);
-            return () => {
-              windowTracker.removeListener("status", statusListener);
-              windowTracker.removeListener("pagetitlechanged", listener);
-            };
-          },
+          module,
+          event: "onUpdated",
+          extensionApi,
         }).api(),
 
         async create({
@@ -541,6 +584,5 @@ this.tabs = class extends ExtensionAPI {
         },
       },
     };
-    return self;
   }
 };
