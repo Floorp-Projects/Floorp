5b9f05f747ba52b446b308a65f7bfd53469dd881
Botond Ballo <botond@mozilla.com>
2023-03-30 03:59:05 +0000

    Bug 1818967 - Remove DisplayPortMargins::mScale which is no longer used. r=hiro
    Differential Revision: https://phabricator.services.mozilla.com/D173578

--- layout/base/DisplayPortUtils.h
+++ layout/base/DisplayPortUtils.h
@@ -71,25 +71,18 @@ struct DisplayPortMargins {
   // were saved.
   CSSPoint mLayoutOffset;
 
-  // The scale required to convert between the CSS cordinates of
-  // mVisualOffset and mLayoutOffset, and the Screen coordinates of mMargins.
-  CSSToScreenScale2D mScale;
-
   // Create displayport margins requested by APZ, relative to an async visual
   // offset provided by APZ.
   static DisplayPortMargins FromAPZ(const ScreenMargin& aMargins,
                                     const CSSPoint& aVisualOffset,
-                                    const CSSPoint& aLayoutOffset,
-                                    const CSSToScreenScale2D& aScale);
+                                    const CSSPoint& aLayoutOffset);
 
   // Create displayport port margins for the given scroll frame.
   // This is for use in cases where we don't have async scroll information from
   // APZ to use to adjust the margins. The visual and layout offset are set
-  // based on the main thread's view of them. If a scale isn't provided, one
-  // is computed based on the main thread's knowledge.
-  static DisplayPortMargins ForScrollFrame(
-      nsIScrollableFrame* aScrollFrame, const ScreenMargin& aMargins,
-      const Maybe<CSSToScreenScale2D>& aScale = Nothing());
+  // based on the main thread's view of them.
+  static DisplayPortMargins ForScrollFrame(nsIScrollableFrame* aScrollFrame,
+                                           const ScreenMargin& aMargins);
 
   // Convenience version of the above that takes a content element.
   static DisplayPortMargins ForContent(nsIContent* aContent,
