9efbe898812be1e53ecc007db50cd927d96bb2bb
Andreas Pehrson <apehrson@mozilla.com>
2023-03-30 10:36:33 +0000

    Bug 1817724 - In VideoCaptureAvFoundation use MediaEvent to handle frames. r=webrtc-reviewers,ng
    Hypothetically if the frame callback is doing something leading to crashes, this
    patch should at least affect those symptoms, giving us more clues to where the
    problem lies.
    
    This patch changes away from calling the CamerasParent callback directly on the
    frame queue, to notifying a MediaEvent owned by the VideoCaptureAdapter. The
    frames are then handled on the video capture thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D171962

--- dom/media/systemservices/objc_video_capture/video_capture_avfoundation.h
+++ dom/media/systemservices/objc_video_capture/video_capture_avfoundation.h
@@ -11,17 +11,25 @@
 
 #include "api/scoped_refptr.h"
 #include "api/sequence_checker.h"
+#include "MediaEventSource.h"
 #include "modules/video_capture/video_capture_impl.h"
 #include "mozilla/Maybe.h"
 #include "PerformanceRecorder.h"
 
-@class VideoCaptureAdapter;
+@interface VideoCaptureAdapter : NSObject <RTCVideoCapturerDelegate> {
+ @public
+  mozilla::MediaEventProducerExc<__strong RTCVideoFrame* _Nullable> frameEvent;
+}
+@end
 
 namespace webrtc::videocapturemodule {
 
 /**
  * VideoCaptureImpl implementation of the libwebrtc ios/mac sdk camera backend.
- * Single threaded except for OnFrame() that happens on a platform callback thread.
+ * Single threaded API. Callbacks run on the API thread.
+ *
+ * Internally callbacks come on a dedicated dispatch queue. We bounce them via a MediaEvent to the
+ * API thread.
  */
 class VideoCaptureAvFoundation : public VideoCaptureImpl {
  public:
@@ -34,19 +42,16 @@ class VideoCaptureAvFoundation : public VideoCaptureImpl {
 
   // Starts capturing synchronously. Idempotent. If an existing capture is live and another
   // capability is requested we'll restart the underlying backend with the new capability.
-  int32_t StartCapture(const VideoCaptureCapability& aCapability) MOZ_EXCLUDES(api_lock_) override;
+  int32_t StartCapture(const VideoCaptureCapability& aCapability) override;
   // Stops capturing synchronously. Idempotent.
-  int32_t StopCapture() MOZ_EXCLUDES(api_lock_) override;
-  bool CaptureStarted() MOZ_EXCLUDES(api_lock_) override;
+  int32_t StopCapture() override;
+  bool CaptureStarted() override;
   int32_t CaptureSettings(VideoCaptureCapability& aSettings) override;
 
-  // Callback. This can be called on any thread.
-  int32_t OnFrame(__strong RTCVideoFrame* _Nonnull aFrame) MOZ_EXCLUDES(api_lock_);
+  // Callback on the mChecker thread.
+  void OnFrame(__strong RTCVideoFrame* _Nonnull aFrame);
 
-  void SetTrackingId(uint32_t aTrackingIdProcId) MOZ_EXCLUDES(api_lock_) override;
-
-  // Registers the current thread with the profiler if not already registered.
-  void MaybeRegisterCallbackThread();
+  void SetTrackingId(uint32_t aTrackingIdProcId) override;
 
  private:
   // Control thread checker.
@@ -54,26 +59,21 @@ class VideoCaptureAvFoundation : public VideoCaptureImpl {
   AVCaptureDevice* _Nonnull const mDevice RTC_GUARDED_BY(mChecker);
   VideoCaptureAdapter* _Nonnull const mAdapter RTC_GUARDED_BY(mChecker);
   RTCCameraVideoCapturer* _Nonnull const mCapturer RTC_GUARDED_BY(mChecker);
-  // If capture has started, this is the capability it was started for. Written on the mChecker
-  // thread only.
-  mozilla::Maybe<VideoCaptureCapability> mCapability MOZ_GUARDED_BY(api_lock_);
+  // If capture has started, this is the capability it was started for.
+  mozilla::Maybe<VideoCaptureCapability> mCapability RTC_GUARDED_BY(mChecker);
   // The image type that mCapability maps to. Set in lockstep with mCapability.
-  mozilla::Maybe<mozilla::CaptureStage::ImageType> mImageType MOZ_GUARDED_BY(api_lock_);
-  // Id string uniquely identifying this capture source. Written on the mChecker thread only.
-  mozilla::Maybe<mozilla::TrackingId> mTrackingId MOZ_GUARDED_BY(api_lock_);
-  // Adds frame specific markers to the profiler while mTrackingId is set. Callback thread only.
-  mozilla::PerformanceRecorderMulti<mozilla::CaptureStage> mCaptureRecorder;
-  mozilla::PerformanceRecorderMulti<mozilla::CopyVideoStage> mConversionRecorder;
-  std::atomic<ProfilerThreadId> mCallbackThreadId;
+  mozilla::Maybe<mozilla::CaptureStage::ImageType> mImageType RTC_GUARDED_BY(mChecker);
+  // Id string uniquely identifying this capture source.
+  mozilla::Maybe<mozilla::TrackingId> mTrackingId RTC_GUARDED_BY(mChecker);
+  // Handle for the mAdapter frame event. mChecker thread only.
+  mozilla::MediaEventListener mFrameListener RTC_GUARDED_BY(mChecker);
+  // Adds frame specific markers to the profiler while mTrackingId is set.
+  mozilla::PerformanceRecorderMulti<mozilla::CaptureStage> mCaptureRecorder
+      RTC_GUARDED_BY(mChecker);
+  mozilla::PerformanceRecorderMulti<mozilla::CopyVideoStage> mConversionRecorder
+      RTC_GUARDED_BY(mChecker);
 };
 
 }  // namespace webrtc::videocapturemodule
-
-@interface VideoCaptureAdapter : NSObject <RTCVideoCapturerDelegate> {
-  webrtc::Mutex _mutex;
-  webrtc::videocapturemodule::VideoCaptureAvFoundation* _Nullable _capturer RTC_GUARDED_BY(_mutex);
-}
-- (void)setCapturer:(webrtc::videocapturemodule::VideoCaptureAvFoundation* _Nullable)capturer;
-@end
 
 #endif
