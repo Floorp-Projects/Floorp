233615040447ce208e3f8d5fce5cd4b0f0ef8606
Nika Layzell <nika@thelayzells.com>
2023-03-30 15:14:00 +0000

    Bug 1538754 - Consistently watch the nsIAsyncOutputStream for closure, r=asuth,saschanaz
    Before these changes, there were a number of cases where it would be
    possible for the pipe to be closed or otherwise encounter an error, and
    for there to be a very large or potentially indefinite delay before that
    error is reported to the ReadableStream.
    
    This patch avoids changing too much of the structure of the existing
    FetchStreamReader's design, while avoiding the potential pitfalls, and
    ensuring that the stream is being consistently watched.
    
    A more polished patch in the future may be able to perform a more
    efficient and direct adaptation from a `ReadableStream` or
    `WritableStream` to the `nsIAsyncInputStream` and `nsIAsyncOutputStream`
    types, but that was not tackled here.
    
    Differential Revision: https://phabricator.services.mozilla.com/D168481

--- dom/fetch/FetchStreamReader.h
+++ dom/fetch/FetchStreamReader.h
@@ -34,8 +34,12 @@ class FetchStreamReader final : public nsIOutputStreamCallback {
                          FetchStreamReader** aStreamReader,
                          nsIInputStream** aInputStream);
 
+  MOZ_CAN_RUN_SCRIPT
   void ChunkSteps(JSContext* aCx, JS::Handle<JS::Value> aChunk,
                   ErrorResult& aRv);
+  MOZ_CAN_RUN_SCRIPT
+  void CloseSteps(JSContext* aCx, ErrorResult& aRv);
+  MOZ_CAN_RUN_SCRIPT
   void ErrorSteps(JSContext* aCx, JS::Handle<JS::Value> aError,
                   ErrorResult& aRv);
 
@@ -59,6 +63,12 @@ class FetchStreamReader final : public nsIOutputStreamCallback {
 
   nsresult WriteBuffer();
 
+  // Attempt to copy data from mBuffer into mPipeOut. Returns `true` if data was
+  // written, and AsyncWait callbacks or FetchReadRequest calls have been set up
+  // to write more data in the future, and `false` otherwise.
+  MOZ_CAN_RUN_SCRIPT
+  bool Process(JSContext* aCx);
+
   void ReportErrorToConsole(JSContext* aCx, JS::Handle<JS::Value> aValue);
 
   nsCOMPtr<nsIGlobalObject> mGlobal;
@@ -72,10 +82,11 @@ class FetchStreamReader final : public nsIOutputStreamCallback {
   RefPtr<ReadableStreamDefaultReader> mReader;
 
   nsTArray<uint8_t> mBuffer;
-  uint32_t mBufferRemaining;
-  uint32_t mBufferOffset;
+  uint32_t mBufferRemaining = 0;
+  uint32_t mBufferOffset = 0;
 
-  bool mStreamClosed;
+  bool mHasOutstandingReadRequest = false;
+  bool mStreamClosed = false;
 };
 
 }  // namespace mozilla::dom
