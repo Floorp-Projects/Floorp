9466ef24c43b2cf9ac07208edbc6d7be46e68e22
Tooru Fujisawa <arai_a@mac.com>
2023-03-30 10:14:01 +0000

    Bug 1825095 - Skip getBacktrace-invalid.js test on coverage. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D174071

--- js/src/jit-test/tests/basic/getBacktrace-invalid.js
+++ js/src/jit-test/tests/basic/getBacktrace-invalid.js
@@ -1,3 +1,6 @@
+// |jit-test| skip-if: isLcovEnabled()
+// Skip this test on Lcov because the invalid filename breaks the test harness.
+
 // Some filename handling code still uses latin1, and some characters are
 // replaced with REPLACEMENT CHARACTER (U+FFFD).
 //
