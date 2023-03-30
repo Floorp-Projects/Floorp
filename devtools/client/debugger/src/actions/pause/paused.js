5b7c84585c9fad60892876d5b2574d7b25f84489
Alexandre Poirot <poirot.alex@gmail.com>
2023-03-30 21:26:23 +0000

    Bug 1781941 - [devtools] Wait for original sources when mapping frames. r=devtools-reviewers,bomsy
    With the new location object having direct references to source object,
    we now have to wait for sources objects to be registered in the reducers
    before trying to instantiate the related frame locations.
    
    Also fix a typo in waitForSourceToBeRegisteredInStore.
    
    Last but not least, we no longer need to wait for the source related to
    the selected frame as mapFrames will do it.
    Now we have a strong guaranty that frame.location will be set
    with a valid source object. That for both generated *and* original sources.
    
    Differential Revision: https://phabricator.services.mozilla.com/D173838

--- devtools/client/debugger/src/actions/pause/paused.js
+++ devtools/client/debugger/src/actions/pause/paused.js
@@ -9,7 +9,6 @@ import {
   getThreadContext,
 } from "../../selectors";
 
-import { waitForSourceToBeRegisteredInStore } from "../../client/firefox/create";
 import { mapFrames, fetchFrames } from ".";
 import { removeBreakpoint } from "../breakpoints";
 import { evaluateExpressions } from "../expressions";
@@ -49,14 +48,15 @@ export function paused(pauseInfo) {
     // display it and update the UI to be paused.
     // But we then fetch all the other frames:
     await dispatch(fetchFrames(cx));
-    // And map them to original source locations:
+    // And map them to original source locations.
+    // Note that this will wait for all related original sources to be loaded in the reducers.
+    // So this step may pause for a little while.
     await dispatch(mapFrames(cx));
 
     // If we paused on a particular frame, automatically select the related source
     // and highlight the paused line
     const selectedFrame = getSelectedFrame(getState(), thread);
     if (selectedFrame) {
-      await waitForSourceToBeRegisteredInStore(selectedFrame.location.sourceId);
       await dispatch(selectSpecificLocation(cx, selectedFrame.location));
     }
 
