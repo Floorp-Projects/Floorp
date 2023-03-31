7767b3f604ce00bba8f84d1850fb16a7a0341381
serge-sans-paille <sguelton@mozilla.com>
2023-03-31 13:29:47 +0000

    Bug 1825335 - Make toolkit/components/reputationservice buildable outside of a unified build environment r=andi
    Depends on D173978
    
    Differential Revision: https://phabricator.services.mozilla.com/D173980

--- toolkit/components/reputationservice/LoginReputationIPC.cpp
+++ toolkit/components/reputationservice/LoginReputationIPC.cpp
@@ -6,6 +6,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "LoginReputationIPC.h"
+#include "LoginReputation.h"
 #include "mozilla/Components.h"
 #include "mozilla/Unused.h"
 
