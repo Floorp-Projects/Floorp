d591250bf7c376e0afd932578a39c91fa7b17ef9
sotaro <sotaro.ikeda.g@gmail.com>
2023-03-31 04:08:40 +0000

    Bug 1824696 - Initialize gfxGradientCache in GPU process only when remote canvas is enabled r=gfx-reviewers,lsalzman
    On Android, remote canvas is disabled. gfxGradientCache does not need to be initialized.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174058

--- gfx/ipc/GPUParent.cpp
+++ gfx/ipc/GPUParent.cpp
@@ -201,7 +201,6 @@ bool GPUParent::Init(mozilla::ipc::UntypedEndpoint&& aEndpoint,
   apz::InitializeGlobalState();
   LayerTreeOwnerTracker::Initialize();
   CompositorBridgeParent::InitializeStatics();
-  gfxGradientCache::Init();
   mozilla::ipc::SetThisProcessName("GPU Process");
 
   return true;
@@ -285,6 +284,10 @@ mozilla::ipc::IPCResult GPUParent::RecvInit(
   // We bypass gfxPlatform::Init, so we must initialize any relevant libraries
   // here that would normally be initialized there.
   SkGraphics::Init();
+
+  if (gfxVars::RemoteCanvasEnabled()) {
+    gfxGradientCache::Init();
+  }
 
 #if defined(XP_WIN)
   if (gfxConfig::IsEnabled(Feature::D3D11_COMPOSITING)) {
