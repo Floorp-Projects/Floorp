85f8edb30ab5a81e3cdfdc51a5240d76e81610d3
Lee Salzman <lsalzman@mozilla.com>
2023-03-31 07:51:00 +0000

    Bug 1816775 - Handle OP_CLEAR in DrawTargetD2D1. r=aosmond
    Depends on D172956
    
    Differential Revision: https://phabricator.services.mozilla.com/D174142

--- gfx/2d/DrawTargetD2D1.h
+++ gfx/2d/DrawTargetD2D1.h
@@ -193,6 +193,7 @@ class DrawTargetD2D1 : public DrawTarget {
                                          bool aClipIsComplex);
   bool PrepareForDrawing(CompositionOp aOp, const Pattern& aPattern);
   void FinalizeDrawing(CompositionOp aOp, const Pattern& aPattern);
+  bool MaybeClearRect(CompositionOp aOp, const Rect& aBounds);
   void FlushTransformToDC() {
     if (mTransformDirty) {
       mDC->SetTransform(D2DMatrix(mTransform));
@@ -233,8 +234,8 @@ class DrawTargetD2D1 : public DrawTarget {
   already_AddRefed<ID2D1Brush> CreateTransparentBlackBrush();
   already_AddRefed<ID2D1SolidColorBrush> GetSolidColorBrush(
       const D2D_COLOR_F& aColor);
-  already_AddRefed<ID2D1Brush> CreateBrushForPattern(const Pattern& aPattern,
-                                                     Float aAlpha = 1.0f);
+  already_AddRefed<ID2D1Brush> CreateBrushForPattern(
+      const Pattern& aPattern, const DrawOptions& aOptions);
 
   void PushClipGeometry(ID2D1Geometry* aGeometry,
                         const D2D1_MATRIX_3X2_F& aTransform,
