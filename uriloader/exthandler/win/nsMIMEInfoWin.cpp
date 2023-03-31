52c07068f3ba3c746e35113501e25559942879fe
serge-sans-paille <sguelton@mozilla.com>
2023-03-31 13:29:44 +0000

    Bug 1825325 - Make uriloader/exthandler buildable outside of a unified build environment r=andi
    Depends on D173969
    
    Differential Revision: https://phabricator.services.mozilla.com/D173970

--- uriloader/exthandler/win/nsMIMEInfoWin.cpp
+++ uriloader/exthandler/win/nsMIMEInfoWin.cpp
@@ -5,9 +5,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "nsArrayEnumerator.h"
+#include "nsComponentManagerUtils.h"
 #include "nsCOMArray.h"
 #include "nsLocalFile.h"
 #include "nsMIMEInfoWin.h"
+#include "nsIMIMEService.h"
 #include "nsNetUtil.h"
 #include <windows.h>
 #include <shellapi.h>
@@ -98,7 +100,8 @@ nsMIMEInfoWin::LaunchWithFile(nsIFile* aFile) {
 
   if (mPreferredAction == useSystemDefault) {
     nsCOMPtr<nsIFile> defaultApp = GetDefaultApplication();
-    if (defaultApp && StaticPrefs::browser_pdf_launchDefaultEdgeAsApp()) {
+    if (defaultApp &&
+        mozilla::StaticPrefs::browser_pdf_launchDefaultEdgeAsApp()) {
       // Since Edgium is the default handler for PDF and other kinds of files,
       // if we're using the OS default and it's Edgium prefer its app mode so it
       // operates as a viewer (without browser toolbars). Bug 1632277.
@@ -295,7 +298,8 @@ nsresult nsMIMEInfoWin::LoadUriInternal(nsIURI* aURL) {
     // Ask the shell/urlmon to parse |utf16Spec| to avoid malformed URLs.
     // Failure is indicative of a potential security issue so we should
     // bail out if so.
-    LauncherResult<_bstr_t> validatedUri = UrlmonValidateUri(utf16Spec.get());
+    mozilla::LauncherResult<_bstr_t> validatedUri =
+        mozilla::UrlmonValidateUri(utf16Spec.get());
     if (validatedUri.isErr()) {
       return NS_ERROR_FAILURE;
     }
