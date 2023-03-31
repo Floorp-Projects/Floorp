9d46e0fa6205b46fba7cc6470ed6d6a23029570c
serge-sans-paille <sguelton@mozilla.com>
2023-03-31 13:29:45 +0000

    Bug 1825328 - Make media/libcubeb/*test buildable outside of a unified build environment r=andi
    Depends on D173971
    
    Differential Revision: https://phabricator.services.mozilla.com/D173972

--- media/libcubeb/test/common.h
+++ media/libcubeb/test/common.h
@@ -28,7 +28,7 @@ ARRAY_LENGTH(T(&)[N])
   return N;
 }
 
-void delay(unsigned int ms)
+inline void delay(unsigned int ms)
 {
 #if defined(_WIN32)
   Sleep(ms);
@@ -48,7 +48,7 @@ typedef struct {
   uint32_t const layout;
 } layout_info;
 
-int has_available_input_device(cubeb * ctx)
+inline int has_available_input_device(cubeb * ctx)
 {
   cubeb_device_collection devices;
   int input_device_available = 0;
@@ -80,7 +80,7 @@ int has_available_input_device(cubeb * ctx)
   return !!input_device_available;
 }
 
-void print_log(const char * msg, ...)
+inline void print_log(const char * msg, ...)
 {
   va_list args;
   va_start(args, msg);
@@ -91,7 +91,7 @@ void print_log(const char * msg, ...)
 /** Initialize cubeb with backend override.
  *  Create call cubeb_init passing value for CUBEB_BACKEND env var as
  *  override. */
-int common_init(cubeb ** ctx, char const * ctx_name)
+inline int common_init(cubeb ** ctx, char const * ctx_name)
 {
 #ifdef ENABLE_NORMAL_LOG
   if (cubeb_set_log_callback(CUBEB_LOG_NORMAL, print_log) != CUBEB_OK) {
