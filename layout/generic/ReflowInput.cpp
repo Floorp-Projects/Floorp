d705127c5cd3dd379b4d0167b550b887feb08810
Ting-Yu Lin <tlin@mozilla.com>
2023-04-01 04:48:53 +0000

    Bug 1825434 - Use containing block's inline-size as percentage basis when computing abspos hypothetical position. r=AlaskanEmily
    The containing block's block-size (`blockContentSize.BSize(wm)`) can be
    unconstrained if the block has `position:relative` at the time we call the
    `CalculateBorderPaddingMargin()`. That triggers the warning further down in the
    stack in `ComputeCBDependentValue`.
    
    However, the percentage margin and padding should resolve against the containing
    block's inline-size rather than the block-size. Hence this patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174340

--- layout/generic/ReflowInput.cpp
+++ layout/generic/ReflowInput.cpp
@@ -1296,7 +1296,7 @@ void ReflowInput::CalculateHypotheticalPosition(
       // value calculated using the absolute containing block width
       nscoord insideBoxBSizing, dummy;
       CalculateBorderPaddingMargin(eLogicalAxisBlock,
-                                   blockContentSize.BSize(wm),
+                                   blockContentSize.ISize(wm),
                                    &insideBoxBSizing, &dummy);
       boxISize.emplace(
           ComputeISizeValue(wm, blockContentSize,
