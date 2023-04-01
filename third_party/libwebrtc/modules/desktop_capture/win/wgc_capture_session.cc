848d4439cdbb7fa38658946d93c57dd707140a72
Byron Campen <docfaraday@gmail.com>
2023-03-31 22:11:02 -0500

    Bug 1822194 - Vendor libwebrtc from 3f2a3b19e3
    Upstream commit: https://webrtc.googlesource.com/src/+/3f2a3b19e3de1531fed98a6d8295f89a79f6d969
        [DesktopCapture]: Allow toggling the visibility of the cursor
    
        This is a change needed to implement (cursor: 'never') https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackSettings/cursor
        constraint.
    
        Bug: chromium:1007177
        Change-Id: Id7fae62de180d46a3874856978a3fda559aa6477
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/282861
        Commit-Queue: Alexander Cooper <alcooper@chromium.org>
        Reviewed-by: Alexander Cooper <alcooper@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#38770}

--- third_party/libwebrtc/modules/desktop_capture/win/wgc_capture_session.cc
+++ third_party/libwebrtc/modules/desktop_capture/win/wgc_capture_session.cc
@@ -105,7 +105,7 @@ WgcCaptureSession::~WgcCaptureSession() {
   RemoveEventHandlers();
 }
 
-HRESULT WgcCaptureSession::StartCapture() {
+HRESULT WgcCaptureSession::StartCapture(const DesktopCaptureOptions& options) {
   RTC_DCHECK_RUN_ON(&sequence_checker_);
   RTC_DCHECK(!is_capture_started_);
 
@@ -185,6 +185,15 @@ HRESULT WgcCaptureSession::StartCapture() {
   if (FAILED(hr)) {
     RecordStartCaptureResult(StartCaptureResult::kCreateCaptureSessionFailed);
     return hr;
+  }
+
+  if (!options.prefer_cursor_embedded()) {
+    ComPtr<ABI::Windows::Graphics::Capture::IGraphicsCaptureSession2> session2;
+    if (SUCCEEDED(session_->QueryInterface(
+            ABI::Windows::Graphics::Capture::IID_IGraphicsCaptureSession2,
+            &session2))) {
+      session2->put_IsCursorCaptureEnabled(false);
+    }
   }
 
   hr = session_->StartCapture();
