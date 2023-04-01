ba7a74099f10bb845008b9ee88e9ba3183b16765
Byron Campen <docfaraday@gmail.com>
2023-03-31 19:53:41 -0500

    Bug 1822194 - Vendor libwebrtc from e03862bcbf
    Upstream commit: https://webrtc.googlesource.com/src/+/e03862bcbfbd03e06f6798df3d4b599e22961272
        Avoid replaying excessive padding generation.
    
        If the real-time clock makes a sudden and large shift forward while
        there is at least one packet in the queue, the pacer would previously
        try to "replay" the behavior it would have done while it was asleep.
        This can lead to grand bursts of padding packets, which is
        undesireable.
        This CL mitigates this problem by forwarding the internal state clock
        to (now - 50ms) if there is nothing to do but generate padding.
    
        Bug: webrtc:11340, b/258509536
        Change-Id: I5b8a130d938dd2566f72c1946c139f50e099e5a9
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/285380
        Reviewed-by: Philip Eliasson <philipel@webrtc.org>
        Commit-Queue: Erik Språng <sprang@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#38752}

--- third_party/libwebrtc/modules/pacing/pacing_controller.cc
+++ third_party/libwebrtc/modules/pacing/pacing_controller.cc
@@ -33,7 +33,6 @@ constexpr TimeDelta kCongestedPacketInterval = TimeDelta::Millis(500);
 // The maximum debt level, in terms of time, capped when sending packets.
 constexpr TimeDelta kMaxDebtInTime = TimeDelta::Millis(500);
 constexpr TimeDelta kMaxElapsedTime = TimeDelta::Seconds(2);
-constexpr TimeDelta kTargetPaddingDuration = TimeDelta::Millis(5);
 
 bool IsDisabled(const FieldTrialsView& field_trials, absl::string_view key) {
   return absl::StartsWith(field_trials.Lookup(key), "Disabled");
@@ -50,6 +49,9 @@ const TimeDelta PacingController::kMaxExpectedQueueLength =
 const TimeDelta PacingController::kPausedProcessInterval =
     kCongestedPacketInterval;
 const TimeDelta PacingController::kMinSleepTime = TimeDelta::Millis(1);
+const TimeDelta PacingController::kTargetPaddingDuration = TimeDelta::Millis(5);
+const TimeDelta PacingController::kMaxPaddingReplayDuration =
+    TimeDelta::Millis(50);
 const TimeDelta PacingController::kMaxEarlyProbeProcessing =
     TimeDelta::Millis(1);
 
@@ -438,6 +440,14 @@ void PacingController::ProcessPackets() {
         GetPendingPacket(pacing_info, target_send_time, now);
     if (rtp_packet == nullptr) {
       // No packet available to send, check if we should send padding.
+      if (now - target_send_time > kMaxPaddingReplayDuration) {
+        // The target send time is more than `kMaxPaddingReplayDuration` behind
+        // the real-time clock. This can happen if the clock is adjusted forward
+        // without `ProcessPackets()` having been called at the expected times.
+        target_send_time = now - kMaxPaddingReplayDuration;
+        last_process_time_ = std::max(last_process_time_, target_send_time);
+      }
+
       DataSize padding_to_add = PaddingToAdd(recommended_probe_size, data_sent);
       if (padding_to_add > DataSize::Zero()) {
         std::vector<std::unique_ptr<RtpPacketToSend>> padding_packets =
