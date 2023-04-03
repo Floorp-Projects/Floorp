396238772940f981924b8055c3cdb936c38136ef
Mike Conley <mconley@mozilla.com>
2023-04-03 13:46:32 +0000

    Bug 1825867 - Make browser_sma_show_migration_wizard.js pass with both the old and new migration wizards. r=aminomancer
    Differential Revision: https://phabricator.services.mozilla.com/D174333

--- toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_show_migration_wizard.js
+++ toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_show_migration_wizard.js
@@ -3,37 +3,95 @@
 
 "use strict";
 
+/**
+ * We currently support both the legacy and the new migration wizard. The
+ * legacy migration wizard opens in a top-level XUL dialog. The new migration
+ * wizard opens as an HTML dialog within about:preferences.
+ *
+ * This variable is true if the new migration wizard that loads in
+ * about:preferences is being used.
+ *
+ * The legacy wizard codepaths can be removed once bug 1824851 lands.
+ */
+const USING_LEGACY_WIZARD = !Services.prefs.getBoolPref(
+  "browser.migrate.content-modal.enabled",
+  false
+);
+
+/**
+ * A helper function for this test that returns a Promise that resolves
+ * once either the legacy or new migration wizard appears.
+ *
+ * @returns {Promise<Element>}
+ *   Resolves to the dialog window in the legacy case, and the
+ *   about:preferences tab otherwise.
+ */
+async function waitForWizard() {
+  if (USING_LEGACY_WIZARD) {
+    return BrowserTestUtils.waitForCondition(
+      () => Services.wm.getMostRecentWindow("Browser:MigrationWizard"),
+      "Wait for migration wizard to open"
+    );
+  }
+
+  let wizardReady = BrowserTestUtils.waitForEvent(
+    window,
+    "MigrationWizard:Ready"
+  );
+  let wizardTab = await BrowserTestUtils.waitForNewTab(gBrowser, url => {
+    return url.startsWith("about:preferences");
+  });
+  await wizardReady;
+
+  return wizardTab;
+}
+
+/**
+ * Closes the migration wizard.
+ *
+ * @param {Element} wizardWindowOrTab
+ *   The XUL dialog window for the migration wizard in the legacy case, and
+ *   the about:preferences tab otherwise.
+ * @returns {Promise<undefined>}
+ */
+function closeWizard(wizardWindowOrTab) {
+  if (USING_LEGACY_WIZARD) {
+    return BrowserTestUtils.closeWindow(wizardWindowOrTab);
+  }
+
+  return BrowserTestUtils.removeTab(wizardWindowOrTab);
+}
+
+add_setup(async () => {
+  // Load the initial tab at example.com. This makes it so that if
+  // USING_LEGACY_WIZARD is false, we'll load the about:preferences
+  // page in a new tab rather than overtaking the initial one. This
+  // makes it easier to be consistent with closing and opening
+  // behaviours between the two kinds of migration wizards.
+  let browser = gBrowser.selectedBrowser;
+  BrowserTestUtils.loadURIString(browser, "https://example.com");
+  await BrowserTestUtils.browserLoaded(browser);
+});
+
 add_task(async function test_SHOW_MIGRATION_WIZARD() {
-  let migratorOpen = TestUtils.waitForCondition(() => {
-    let win = Services.wm.getMostRecentWindow("Browser:MigrationWizard");
-    return win && win.document && win.document.readyState == "complete";
-  }, "Migrator window loaded");
+  let wizardOpened = waitForWizard();
 
   SMATestUtils.executeAndValidateAction({ type: "SHOW_MIGRATION_WIZARD" });
 
-  await migratorOpen;
-  let migratorWindow = Services.wm.getMostRecentWindow(
-    "Browser:MigrationWizard"
-  );
-  ok(migratorWindow, "Migrator window opened");
-  await BrowserTestUtils.closeWindow(migratorWindow);
+  let wizard = await wizardOpened;
+  ok(wizard, "Migration wizard opened");
+  closeWizard(wizard);
 });
 
 add_task(async function test_SHOW_MIGRATION_WIZARD_WITH_SOURCE() {
-  let migratorOpen = TestUtils.waitForCondition(() => {
-    let win = Services.wm.getMostRecentWindow("Browser:MigrationWizard");
-    return win && win.document && win.document.readyState == "complete";
-  }, "Migrator window loaded");
+  let wizardOpened = waitForWizard();
 
   SMATestUtils.executeAndValidateAction({
     type: "SHOW_MIGRATION_WIZARD",
     data: { source: "chrome" },
   });
 
-  await migratorOpen;
-  let migratorWindow = Services.wm.getMostRecentWindow(
-    "Browser:MigrationWizard"
-  );
-  ok(migratorWindow, "Migrator window opened when source param specified");
-  await BrowserTestUtils.closeWindow(migratorWindow);
+  let wizard = await wizardOpened;
+  ok(wizard, "Migrator window opened when source param specified");
+  closeWizard(wizard);
 });
