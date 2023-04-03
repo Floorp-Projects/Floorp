eb574864ded27292c770c8d87f2a7513ebe74990
Mark Banner <standard8@mozilla.com>
2023-04-03 10:23:53 +0000

    Bug 1825739 - Replace generic ESLint disable statements with specific and prettier ones in other places. r=mossop
    Depends on D174228
    
    Differential Revision: https://phabricator.services.mozilla.com/D174229

--- dom/base/test/common_postMessages.js
+++ dom/base/test/common_postMessages.js
@@ -162,7 +162,7 @@ function create_wasmModule() {
       print(t)
     '
   */
-  // eslint-disable-next-line
+  // prettier-ignore
   const fooModuleCode = new Uint8Array([0,97,115,109,1,0,0,0,1,5,1,96,0,1,127,3,2,1,0,7,7,1,3,102,111,111,0,0,10,6,1,4,0,65,42,11,0,13,4,110,97,109,101,1,6,1,0,3,102,111,111]);
 
   WebAssembly.compile(fooModuleCode).then(
