ea2e7d9d89a6fce6717af81e94fe9cbb6b249053
Valentin Gosu <valentin.gosu@gmail.com>
2023-04-05 17:13:53 +0000

    Bug 1610741 - DoH Settings UI: Show error reason in Not Available label r=Gijs,kershaw,fluent-reviewers,necko-reviewers,settings-reviewers,flod
    - The patch saves the confirmation channel's status and skipReason in the TRR Service
    - The UI queries the status and displays the stringified version in the Not Available (reason) label
    - Introduces doh-status-label CSS rule so the Learn-More link isn't off center
    
    Differential Revision: https://phabricator.services.mozilla.com/D174110

--- netwerk/dns/TRR.cpp
+++ netwerk/dns/TRR.cpp
@@ -950,7 +950,7 @@ void TRR::ReportStatus(nsresult aStatusCode) {
   // it as failed; otherwise it can cause the confirmation to fail.
   if (UseDefaultServer() && aStatusCode != NS_ERROR_ABORT) {
     // Bad content is still considered "okay" if the HTTP response is okay
-    TRRService::Get()->RecordTRRStatus(aStatusCode);
+    TRRService::Get()->RecordTRRStatus(this);
   }
 }
 
@@ -999,7 +999,7 @@ TRR::OnStopRequest(nsIRequest* aRequest, nsresult aStatusCode) {
     }
   }
 
-  ReportStatus(aStatusCode);
+  auto scopeExit = MakeScopeExit([&] { ReportStatus(aStatusCode); });
 
   nsresult rv = NS_OK;
   // if status was "fine", parse the response and pass on the answer
