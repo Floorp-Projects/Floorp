b9c0fd39fdeaef1a0d29a1188962da13e727cadf
Valentin Gosu <valentin.gosu@gmail.com>
2023-04-05 17:13:52 +0000

    Bug 1610741 - Remove old DoH UI r=Gijs,settings-reviewers,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D173940

--- browser/components/preferences/dialogs/connection.js
+++ browser/components/preferences/dialogs/connection.js
@@ -7,10 +7,6 @@
 /* import-globals-from /toolkit/content/preferencesBindings.js */
 /* import-globals-from ../extensionControlled.js */
 
-ChromeUtils.defineESModuleGetters(this, {
-  DoHConfigController: "resource:///modules/DoHConfig.sys.mjs",
-});
-
 document
   .getElementById("ConnectionsDialog")
   .addEventListener("dialoghelp", window.top.openPrefsHelp);
@@ -35,16 +31,7 @@ Preferences.addAll([
   { id: "pref.advanced.proxies.disable_button.reload", type: "bool" },
   { id: "network.proxy.backup.ssl", type: "string" },
   { id: "network.proxy.backup.ssl_port", type: "int" },
-  { id: "network.trr.mode", type: "int" },
-  { id: "network.trr.uri", type: "string" },
-  { id: "network.trr.custom_uri", type: "string" },
-  { id: "doh-rollout.disable-heuristics", type: "bool" },
-  { id: "doh-rollout.skipHeuristicsCheck", type: "bool" },
 ]);
-
-const DoHConfigObserver = () => {
-  gConnectionsDialog.initDnsOverHttpsUI();
-};
 
 window.addEventListener(
   "DOMContentLoaded",
@@ -57,40 +44,6 @@ window.addEventListener(
       "change",
       gConnectionsDialog.updateDNSPref.bind(gConnectionsDialog)
     );
-
-    Preferences.get("network.trr.uri").on("change", () => {
-      gConnectionsDialog.updateDnsOverHttpsUI();
-    });
-
-    Services.obs.addObserver(
-      DoHConfigObserver,
-      DoHConfigController.kConfigUpdateTopic
-    );
-    window.addEventListener(
-      "unload",
-      e => {
-        Services.obs.removeObserver(
-          DoHConfigObserver,
-          DoHConfigController.kConfigUpdateTopic
-        );
-      },
-      { once: true }
-    );
-
-    // XXX: We can't init the DNS-over-HTTPs UI until the onsyncfrompreference for network.trr.mode
-    //      has been called. The uiReady promise will be resolved after the first call to
-    //      readDnsOverHttpsMode and the subsequent call to initDnsOverHttpsUI has happened.
-    gConnectionsDialog.uiReady = new Promise(resolve => {
-      gConnectionsDialog._areTrrPrefsReady = false;
-      gConnectionsDialog._handleTrrPrefsReady = resolve;
-    }).then(async () => {
-      // awaiting this ensures that initDnsOverHttpsUI() is called after
-      // execution has been returned to the caller of _handleTrrPrefsReady,
-      // which is the checkbox value reading path. This ensures the checkbox
-      // gets checked, then initDnsOverHttpsUI() is called, then the uiReady
-      // promise resolves, preventing intermittent failures in tests.
-      await gConnectionsDialog.initDnsOverHttpsUI();
-    });
 
     document
       .getElementById("disableProxyExtension")
@@ -114,37 +67,6 @@ window.addEventListener(
 
 var gConnectionsDialog = {
   beforeAccept(event) {
-    let dnsOverHttpsResolverChoice = document.getElementById(
-      "networkDnsOverHttpsResolverChoices"
-    ).value;
-    let writeURIandMode = uri => {
-      Services.prefs.setStringPref("network.trr.uri", uri);
-      // When writing the URI, also write the mode. This is needed in addition
-      // to the mode reacting in realtime to the checkbox state because of the
-      // case when the checkbox was ticked due to the rollout being enabled at
-      // the time of clicking "Accept".
-      Services.prefs.setIntPref(
-        "network.trr.mode",
-        this.writeDnsOverHttpsMode()
-      );
-    };
-    // We treat clicking "Accept" as a user choice, and set both the TRR
-    // URI and mode here. This will cause DoHController to permanently
-    // disable heuristics and the values at the time of accept will persist.
-    // This includes the case when no changes were made.
-    if (dnsOverHttpsResolverChoice == "custom") {
-      let customValue = document
-        .getElementById("networkCustomDnsOverHttpsInput")
-        .value.trim();
-      if (customValue) {
-        writeURIandMode(customValue);
-      } else {
-        writeURIandMode(DoHConfigController.currentConfig.fallbackProviderURI);
-      }
-    } else {
-      writeURIandMode(dnsOverHttpsResolverChoice);
-    }
-
     var proxyTypePref = Preferences.get("network.proxy.type");
     if (proxyTypePref.value == 2) {
       this.doAutoconfigURLFixup();
@@ -427,194 +349,9 @@ var gConnectionsDialog = {
     }
   },
 
-  get dnsOverHttpsResolvers() {
-    let providers = DoHConfigController.currentConfig.providerList;
-    // if there's no default, we'll hold its position with an empty string
-    let defaultURI = DoHConfigController.currentConfig.fallbackProviderURI;
-    let defaultIndex = providers.findIndex(p => p.uri == defaultURI);
-    if (defaultIndex == -1 && defaultURI) {
-      // the default value for the pref isn't included in the resolvers list
-      // so we'll make a stub for it. Without an id, we'll have to use the url as the label
-      providers.unshift({ uri: defaultURI });
-    }
-    return providers;
-  },
-
-  isDnsOverHttpsLocked() {
-    return Services.prefs.prefIsLocked("network.trr.mode");
-  },
-
-  isDnsOverHttpsEnabled() {
-    // We consider DoH enabled if:
-    // 1. network.trr.mode has a user-set value equal to 2 or 3.
-    // 2. network.trr.mode is 0, and DoH heuristics are enabled
-    let trrPref = Preferences.get("network.trr.mode");
-    if (trrPref.value > 0) {
-      return trrPref.value == 2 || trrPref.value == 3;
-    }
-
-    let rolloutEnabled = DoHConfigController.currentConfig.enabled;
-    let heuristicsDisabled =
-      Preferences.get("doh-rollout.disable-heuristics").value ||
-      Preferences.get("doh-rollout.skipHeuristicsCheck").value;
-    return rolloutEnabled && !heuristicsDisabled;
-  },
-
-  readDnsOverHttpsMode() {
-    // called to update checked element property to reflect current pref value
-    let enabled = this.isDnsOverHttpsEnabled();
-    let uriPref = Preferences.get("network.trr.uri");
-    uriPref.updateControlDisabledState(!enabled || this.isDnsOverHttpsLocked());
-    // this is the first signal we get when the prefs are available, so
-    // lazy-init if appropriate
-    if (!this._areTrrPrefsReady) {
-      this._areTrrPrefsReady = true;
-      this._handleTrrPrefsReady();
-    } else {
-      this.updateDnsOverHttpsUI();
-    }
-    return enabled;
-  },
-
-  writeDnsOverHttpsMode() {
-    // called to update pref with user change
-    let trrModeCheckbox = document.getElementById("networkDnsOverHttps");
-
-    let trrModeCurrent = Preferences.get("network.trr.mode").value;
-    if (trrModeCheckbox.checked) {
-      //Check if the user has set the value themself through about:config.
-      if (trrModeCurrent == Ci.nsIDNSService.MODE_TRRONLY) {
-        return Ci.nsIDNSService.MODE_TRRONLY;
-      }
-      // we treat checked/enabled as mode 2
-      return Ci.nsIDNSService.MODE_TRRFIRST;
-    }
-
-    return Ci.nsIDNSService.MODE_TRROFF;
-  },
-
-  updateDnsOverHttpsUI() {
-    // init and update of the UI must wait until the pref values are ready
-    if (!this._areTrrPrefsReady) {
-      return;
-    }
-    let [menu, customInput] = this.getDnsOverHttpsControls();
-    let customDohContainer = document.getElementById(
-      "customDnsOverHttpsContainer"
-    );
-    let customURI = Preferences.get("network.trr.custom_uri").value;
-    let currentURI = Preferences.get("network.trr.uri").value;
-    let resolvers = this.dnsOverHttpsResolvers;
-    let isCustom = menu.value == "custom";
-
-    if (this.isDnsOverHttpsEnabled()) {
-      this.toggleDnsOverHttpsUI(false);
-      if (isCustom) {
-        // if the current and custom_uri values mismatch, update the uri pref
-        if (
-          currentURI &&
-          !customURI &&
-          !resolvers.find(r => r.uri == currentURI)
-        ) {
-          Services.prefs.setStringPref("network.trr.custom_uri", currentURI);
-        }
-      }
-    } else {
-      this.toggleDnsOverHttpsUI(true);
-    }
-
-    if (!menu.disabled && isCustom) {
-      customDohContainer.hidden = false;
-      customInput.disabled = false;
-      customInput.scrollIntoView();
-    } else {
-      customDohContainer.hidden = true;
-      customInput.disabled = true;
-    }
-
-    // The height has likely changed, find our SubDialog and tell it to resize.
-    requestAnimationFrame(() => {
-      let dialogs = window.opener.gSubDialog._dialogs;
-      let dialog = dialogs.find(d => d._frame.contentDocument == document);
-      if (dialog) {
-        dialog.resizeVertically();
-      }
-    });
-  },
-
-  getDnsOverHttpsControls() {
-    return [
-      document.getElementById("networkDnsOverHttpsResolverChoices"),
-      document.getElementById("networkCustomDnsOverHttpsInput"),
-      document.getElementById("networkDnsOverHttpsResolverChoicesLabel"),
-      document.getElementById("networkCustomDnsOverHttpsInputLabel"),
-    ];
-  },
-
-  toggleDnsOverHttpsUI(disabled) {
-    for (let element of this.getDnsOverHttpsControls()) {
-      element.disabled = disabled;
-    }
-  },
-
-  initDnsOverHttpsUI() {
-    let resolvers = this.dnsOverHttpsResolvers;
-    let defaultURI = DoHConfigController.currentConfig.fallbackProviderURI;
-    let currentURI = Preferences.get("network.trr.uri").value;
-    let menu = document.getElementById("networkDnsOverHttpsResolverChoices");
-
-    // populate the DNS-Over-HTTPs resolver list
-    menu.removeAllItems();
-    for (let resolver of resolvers) {
-      let item = menu.appendItem(undefined, resolver.uri);
-      if (resolver.uri == defaultURI) {
-        document.l10n.setAttributes(
-          item,
-          "connection-dns-over-https-url-item-default",
-          {
-            name: resolver.UIName || resolver.uri,
-          }
-        );
-      } else {
-        item.label = resolver.UIName || resolver.uri;
-      }
-    }
-    let lastItem = menu.appendItem(undefined, "custom");
-    document.l10n.setAttributes(
-      lastItem,
-      "connection-dns-over-https-url-custom"
-    );
-
-    // set initial selection in the resolver provider picker
-    let selectedIndex = currentURI
-      ? resolvers.findIndex(r => r.uri == currentURI)
-      : 0;
-    if (selectedIndex == -1) {
-      // select the last "Custom" item
-      selectedIndex = menu.itemCount - 1;
-    }
-    menu.selectedIndex = selectedIndex;
-
-    if (this.isDnsOverHttpsLocked()) {
-      // disable all the options and the checkbox itself to disallow enabling them
-      this.toggleDnsOverHttpsUI(true);
-      document.getElementById("networkDnsOverHttps").disabled = true;
-    } else {
-      this.toggleDnsOverHttpsUI(false);
-      this.updateDnsOverHttpsUI();
-      document.getElementById("networkDnsOverHttps").disabled = false;
-    }
-  },
-
   registerSyncPrefListeners() {
     function setSyncFromPrefListener(element_id, callback) {
       Preferences.addSyncFromPrefListener(
-        document.getElementById(element_id),
-        callback
-      );
-    }
-    function setSyncToPrefListener(element_id, callback) {
-      Preferences.addSyncToPrefListener(
         document.getElementById(element_id),
         callback
       );
@@ -640,12 +377,6 @@ var gConnectionsDialog = {
     );
     setSyncFromPrefListener("networkProxySOCKS_Port", () =>
       this.readProxyProtocolPref("socks", true)
-    );
-    setSyncFromPrefListener("networkDnsOverHttps", () =>
-      this.readDnsOverHttpsMode()
-    );
-    setSyncToPrefListener("networkDnsOverHttps", () =>
-      this.writeDnsOverHttpsMode()
     );
   },
 };
