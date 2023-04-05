a01c274a41c4ef94f0d31e5c413b1324e8734413
Manuel Bucher <manuel@mozilla.com>
2023-04-05 14:02:00 +0000

    Bug 1818607 - browser_103_asset.js: Request longer timeout for osx test verify chaos mode 10 runs r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D174748

--- netwerk/test/browser/browser_103_assets.js
+++ netwerk/test/browser/browser_103_assets.js
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// On debug osx test machine, verify chaos mode takes slightly too long
+requestLongerTimeout(2);
+
 Services.prefs.setBoolPref("network.early-hints.enabled", true);
 
 const { request_count_checking } = ChromeUtils.importESModule(
