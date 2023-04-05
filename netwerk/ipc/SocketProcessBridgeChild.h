5211fc05b5c6c94931d85a26357e8ee0f53af456
Nika Layzell <nika@thelayzells.com>
2023-04-05 21:33:43 +0000

    Bug 1824465 - Part 19: Make PSocketProcessBridge refcounted, r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D173734

--- netwerk/ipc/SocketProcessBridgeChild.h
+++ netwerk/ipc/SocketProcessBridgeChild.h
@@ -31,19 +31,16 @@ class SocketProcessBridgeChild final : public PSocketProcessBridgeChild,
   void ActorDestroy(ActorDestroyReason aWhy) override;
   void DeferredDestroy();
   bool IsShuttingDown() const { return mShuttingDown; };
-  bool Inited() const { return mInited; };
   ProcessId SocketProcessPid() const { return mSocketProcessPid; };
 
  private:
   DISALLOW_COPY_AND_ASSIGN(SocketProcessBridgeChild);
   static bool Create(Endpoint<PSocketProcessBridgeChild>&& aEndpoint);
-  explicit SocketProcessBridgeChild(
-      Endpoint<PSocketProcessBridgeChild>&& aEndpoint);
+  explicit SocketProcessBridgeChild();
   virtual ~SocketProcessBridgeChild();
 
   static StaticRefPtr<SocketProcessBridgeChild> sSocketProcessBridgeChild;
   bool mShuttingDown;
-  bool mInited = false;
   ProcessId mSocketProcessPid;
 };
 
