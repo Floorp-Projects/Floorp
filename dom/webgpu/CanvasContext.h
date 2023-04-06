76543054fb67a32a47aa273267b8d86efd9794a7
Kelsey Gilbert <kelsey.gilbert@mozilla.com>
2023-04-06 00:11:01 +0000

    Bug 1814091 - Move CanvasContext.getPreferredFormat to GPU.getPreferredCanvasFormat. r=gfx-reviewers,jimb,emilio
    This reflects a change in the spec.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174792

--- dom/webgpu/CanvasContext.h
+++ dom/webgpu/CanvasContext.h
@@ -85,7 +85,6 @@ class CanvasContext final : public nsICanvasRenderingContextInternal,
   void Configure(const dom::GPUCanvasConfiguration& aDesc);
   void Unconfigure();
 
-  dom::GPUTextureFormat GetPreferredFormat(Adapter& aAdapter) const;
   RefPtr<Texture> GetCurrentTexture(ErrorResult& aRv);
   void MaybeQueueSwapChainPresent();
   void SwapChainPresent();
