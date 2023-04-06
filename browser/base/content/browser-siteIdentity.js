6c6f408cc49f9fffc75eeb7267d73c91648d7635
Csoregi Natalia <ncsoregi@mozilla.com>
2023-04-06 18:40:06 +0300

    Backed out 2 changesets (bug 1814261) as per request. CLOSED TREE
    Backed out changeset 8640c4a18d9e (bug 1814261)
    Backed out changeset c00c8fc88dbd (bug 1814261)

--- browser/base/content/browser-siteIdentity.js
+++ browser/base/content/browser-siteIdentity.js
@@ -166,7 +166,6 @@ var gIdentityHandler = {
 
   _popupInitialized: false,
   _initializePopup() {
-    window.ensureCustomElements("moz-support-link");
     if (!this._popupInitialized) {
       let wrapper = document.getElementById("template-identity-popup");
       wrapper.replaceWith(wrapper.content);
@@ -929,6 +928,9 @@ var gIdentityHandler = {
 
     // Update "Learn More" for Mixed Content Blocking and Insecure Login Forms.
     let baseURL = Services.urlFormatter.formatURLPref("app.support.baseURL");
+    this._identityPopupMixedContentLearnMore.forEach(e =>
+      e.setAttribute("href", baseURL + "mixed-content")
+    );
 
     this._identityPopupCustomRootLearnMore.setAttribute(
       "href",
