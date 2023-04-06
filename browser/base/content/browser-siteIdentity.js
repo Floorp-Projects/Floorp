364a94aedc7007651d8750ee25a453e90667f42b
Noah <osuolale49@gmail.com>
2023-04-06 06:00:16 +0000

    Bug 1814261 - Use moz-support-link in Mixed Content Blocking. r=tgiles,flod
    Differential Revision: https://phabricator.services.mozilla.com/D173620

--- browser/base/content/browser-siteIdentity.js
+++ browser/base/content/browser-siteIdentity.js
@@ -166,6 +166,7 @@ var gIdentityHandler = {
 
   _popupInitialized: false,
   _initializePopup() {
+    window.ensureCustomElements("moz-support-link");
     if (!this._popupInitialized) {
       let wrapper = document.getElementById("template-identity-popup");
       wrapper.replaceWith(wrapper.content);
@@ -928,9 +929,6 @@ var gIdentityHandler = {
 
     // Update "Learn More" for Mixed Content Blocking and Insecure Login Forms.
     let baseURL = Services.urlFormatter.formatURLPref("app.support.baseURL");
-    this._identityPopupMixedContentLearnMore.forEach(e =>
-      e.setAttribute("href", baseURL + "mixed-content")
-    );
 
     this._identityPopupCustomRootLearnMore.setAttribute(
       "href",
