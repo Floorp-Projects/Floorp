0a8bcec64455117cd59d4e9aea238a47bd3c3a19
Valentin Gosu <valentin.gosu@gmail.com>
2023-04-11 09:57:28 +0000

    Bug 1610741 - DoH Settings UI: Show error reason in Not Available label r=Gijs,kershaw,fluent-reviewers,necko-reviewers,settings-reviewers,flod
    - The patch saves the confirmation channel's status and skipReason in the TRR Service
    - The UI queries the status and displays the stringified version in the Not Available (reason) label
    - Introduces doh-status-label CSS rule so the Learn-More link isn't off center
    
    Differential Revision: https://phabricator.services.mozilla.com/D174110

--- netwerk/dns/TRR.h
+++ netwerk/dns/TRR.h
@@ -76,6 +76,7 @@ class TRR : public Runnable,
 
   RequestPurpose Purpose() { return mPurpose; }
   void SetPurpose(RequestPurpose aPurpose) { mPurpose = aPurpose; }
+  TRRSkippedReason SkipReason() const { return mTRRSkippedReason; }
 
  protected:
   virtual ~TRR() = default;
