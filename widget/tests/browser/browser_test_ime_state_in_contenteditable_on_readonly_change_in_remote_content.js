8741d45586a7411de9224b248679ebfd19991ba7
Masayuki Nakano <masayuki@d-toybox.com>
2023-04-11 23:26:06 +0000

    Bug 1806756 - part 7: Rewrite IME state tests of text controls in an editing host to make it possible to run in remote content r=m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D171202

--- widget/tests/browser/browser_test_ime_state_in_contenteditable_on_readonly_change_in_remote_content.js
+++ widget/tests/browser/browser_test_ime_state_in_contenteditable_on_readonly_change_in_remote_content.js
@@ -128,6 +128,79 @@ add_task(async function() {
         );
         tester.clear();
       })();
+
+      await (async function test_ime_state_of_text_controls_in_contenteditable_on_readonly_change() {
+        const tester = new IMEStateOfTextControlInContentEditableOnReadonlyChangeTester();
+        await SpecialPowers.spawn(browser, [], () => {
+          content.document.body.innerHTML = "<div contenteditable></div>";
+          content.wrappedJSObject.runner = content.wrappedJSObject.createIMEStateOfTextControlInContentEditableOnReadonlyChangeTester();
+        });
+        for (
+          let index = 0;
+          index <
+          IMEStateOfTextControlInContentEditableOnReadonlyChangeTester.numberOfTextControlTypes;
+          index++
+        ) {
+          const expectedDataOfInitialization = await SpecialPowers.spawn(
+            browser,
+            [index],
+            aIndex => {
+              const editingHost = content.document.querySelector("div");
+              return content.wrappedJSObject.runner.prepareToRun(
+                aIndex,
+                editingHost,
+                content.window
+              );
+            }
+          );
+          tester.checkResultOfPreparation(
+            expectedDataOfInitialization,
+            window,
+            tipWrapper
+          );
+          const expectedDataOfMakingParentEditingHost = await SpecialPowers.spawn(
+            browser,
+            [],
+            () => {
+              return content.wrappedJSObject.runner.runToMakeParentEditingHost();
+            }
+          );
+          tester.checkResultOfMakingParentEditingHost(
+            expectedDataOfMakingParentEditingHost
+          );
+          const expectedDataOfMakingHTMLEditorReadonly = await SpecialPowers.spawn(
+            browser,
+            [],
+            () => {
+              return content.wrappedJSObject.runner.runToMakeHTMLEditorReadonly();
+            }
+          );
+          tester.checkResultOfMakingHTMLEditorReadonly(
+            expectedDataOfMakingHTMLEditorReadonly
+          );
+          const expectedDataOfMakingHTMLEditorEditable = await SpecialPowers.spawn(
+            browser,
+            [],
+            () => {
+              return content.wrappedJSObject.runner.runToMakeHTMLEditorEditable();
+            }
+          );
+          tester.checkResultOfMakingHTMLEditorEditable(
+            expectedDataOfMakingHTMLEditorEditable
+          );
+          const expectedDataOfMakingParentNonEditable = await SpecialPowers.spawn(
+            browser,
+            [],
+            () => {
+              return content.wrappedJSObject.runner.runToMakeParentNonEditingHost();
+            }
+          );
+          tester.checkResultOfMakingParentNonEditable(
+            expectedDataOfMakingParentNonEditable
+          );
+          tester.clear();
+        }
+      })();
     }
   );
 });
