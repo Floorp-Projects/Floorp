b6fd003b4c530067b642f8dc42cf8756b123ebfb
Masayuki Nakano <masayuki@d-toybox.com>
2023-04-11 23:26:04 +0000

    Bug 1806756 - part 3: Make `runPluginTest` in `test_ime_state_in_parent.html` run as both mochitest-chrome and mochitest-browser-chrome r=m_kato
    We won't support plugins.  Therefore, the tests won't be maintained, thus,
    I just copied them instead of creating complicated shared JS file.
    
    Differential Revision: https://phabricator.services.mozilla.com/D171198

---
+++ widget/tests/browser/browser_test_ime_state_in_plugin_in_remote_content.js
@@ -0,0 +1,119 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* import-globals-from ../file_ime_state_test_helper.js */
+
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/widget/tests/browser/file_ime_state_test_helper.js",
+  this
+);
+add_task(async function() {
+  await BrowserTestUtils.withNewTab(
+    "https://example.com/browser/toolkit/content/tests/browser/file_empty.html",
+    async function(browser) {
+      const tipWrapper = new TIPWrapper(window);
+      ok(
+        tipWrapper.isAvailable(),
+        "TextInputProcessor should've been initialized"
+      );
+
+      await SpecialPowers.spawn(browser, [], () => {
+        content.wrappedJSObject.waitForIMEContentObserverSendingNotifications = () => {
+          return new content.window.Promise(resolve =>
+            content.window.requestAnimationFrame(() =>
+              content.window.requestAnimationFrame(resolve)
+            )
+          );
+        };
+        content.document.body.innerHTML =
+          '<input><object type="application/x-test"></object>';
+      });
+
+      await SpecialPowers.spawn(browser, [], () => {
+        content.document.activeElement?.blur();
+        return content.wrappedJSObject.waitForIMEContentObserverSendingNotifications();
+      });
+      is(
+        window.windowUtils.IMEStatus,
+        Ci.nsIDOMWindowUtils.IME_STATUS_DISABLED,
+        "IME enabled state should be disabled when no element has focus"
+      );
+      ok(
+        !tipWrapper.IMEHasFocus,
+        "IME should not have focus when no element has focus"
+      );
+
+      await SpecialPowers.spawn(browser, [], () => {
+        content.document.querySelector("object").focus();
+        return content.wrappedJSObject.waitForIMEContentObserverSendingNotifications();
+      });
+      is(
+        window.windowUtils.IMEStatus,
+        Ci.nsIDOMWindowUtils.IME_STATUS_DISABLED,
+        "IME enabled state should be disabled when an <object> for plugin has focus"
+      );
+      ok(
+        !tipWrapper.IMEHasFocus,
+        "IME enabled state should not have focus when an <object> for plugin has focus"
+      );
+
+      await SpecialPowers.spawn(browser, [], () => {
+        content.document.querySelector("object").blur();
+        return content.wrappedJSObject.waitForIMEContentObserverSendingNotifications();
+      });
+      is(
+        window.windowUtils.IMEStatus,
+        Ci.nsIDOMWindowUtils.IME_STATUS_DISABLED,
+        "IME enabled state should be disabled when an <object> for plugin gets blurred"
+      );
+      ok(
+        !tipWrapper.IMEHasFocus,
+        "IME should not have focus when an <object> for plugin gets blurred"
+      );
+
+      await SpecialPowers.spawn(browser, [], () => {
+        content.document.querySelector("object").focus();
+        return content.wrappedJSObject.waitForIMEContentObserverSendingNotifications();
+      });
+      is(
+        window.windowUtils.IMEStatus,
+        Ci.nsIDOMWindowUtils.IME_STATUS_DISABLED,
+        "IME enabled state should be disabled when an <object> for plugin gets focused again"
+      );
+      ok(
+        !tipWrapper.IMEHasFocus,
+        "IME should not have focus when an <object> for plugin gets focused again"
+      );
+
+      await SpecialPowers.spawn(browser, [], () => {
+        content.document.querySelector("object").remove();
+        return content.wrappedJSObject.waitForIMEContentObserverSendingNotifications();
+      });
+      is(
+        window.windowUtils.IMEStatus,
+        Ci.nsIDOMWindowUtils.IME_STATUS_DISABLED,
+        "IME enabled state should be disabled when focused <object> for plugin is removed from the document"
+      );
+      ok(
+        !tipWrapper.IMEHasFocus,
+        "IME should not have focus when focused <object> for plugin is removed from the document"
+      );
+
+      await SpecialPowers.spawn(browser, [], () => {
+        content.document.querySelector("input").focus();
+        return content.wrappedJSObject.waitForIMEContentObserverSendingNotifications();
+      });
+      is(
+        window.windowUtils.IMEStatus,
+        Ci.nsIDOMWindowUtils.IME_STATUS_ENABLED,
+        "IME enabled state should be enabled after <input> gets focus"
+      );
+      ok(
+        tipWrapper.IMEHasFocus,
+        "IME should have focus after <input> gets focus"
+      );
+    }
+  );
+});
