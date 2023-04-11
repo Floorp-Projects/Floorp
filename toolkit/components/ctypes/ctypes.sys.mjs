30ac5c0e5d9733865b036ab00ee1ee36f80ef89e
Tooru Fujisawa <arai_a@mac.com>
2023-04-11 01:25:14 +0000

    Bug 1821308 - Part 1: Convert ctypes.jsm to ESM. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D173810

--- toolkit/components/ctypes/ctypes.sys.mjs
+++ toolkit/components/ctypes/ctypes.sys.mjs
@@ -3,20 +3,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var EXPORTED_SYMBOLS = ["ctypes"];
-
 /*
  * This is the js module for ctypes. Import it like so:
- *   const { ctypes } = ChromeUtils.import("resource://gre/modules/ctypes.jsm");
+ *   const { ctypes } =
+ *     ChromeUtils.importESModule("resource://gre/modules/ctypes.sys.mjs");
  *
  * This will create a 'ctypes' object, which provides an interface to describe
  * and instantiate C types and call C functions from a dynamic library.
- *
- * For documentation on the API, see:
- * https://developer.mozilla.org/en/js-ctypes/js-ctypes_reference
- *
  */
 
 // Initialize the ctypes object. You do not need to do this yourself.
 const init = Cc["@mozilla.org/jsctypes;1"].createInstance();
 init();
+
+export const ctypes = globalThis.ctypes;
