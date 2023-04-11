7c169a03b107ef7c623a9f4be38b914cfcadf455
Masayuki Nakano <masayuki@d-toybox.com>
2023-04-11 23:26:05 +0000

    Bug 1806756 - part 6: Rewrite the first part of `runComplexContenteditableTests` in `test_ime_state_in_parent.html` to make it possible to run in remote content r=m_kato
    Although it sets `nsIEditor.flags`, but this is still available with
    `document.execCommand("contentReadOnly", false, "true")` only in the release
    channel and the late-beta builds (see bug 1782852 for the reason why we cannot
    delete it completely).  Therefore, we should test this in remote content too.
    
    Differential Revision: https://phabricator.services.mozilla.com/D171201

---
+++ widget/tests/browser/browser_test_ime_state_in_contenteditable_on_readonly_change_in_remote_content.js
@@ -0,0 +1,133 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* import-globals-from ../file_ime_state_test_helper.js */
+/* import-globals-from ../file_test_ime_state_in_contenteditable_on_readonly_change.js */
+
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/widget/tests/browser/file_ime_state_test_helper.js",
+  this
+);
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/widget/tests/browser/file_test_ime_state_in_contenteditable_on_readonly_change.js",
+  this
+);
+add_task(async function() {
+  await BrowserTestUtils.withNewTab(
+    "https://example.com/browser/widget/tests/browser/file_ime_state_tests.html",
+    async function(browser) {
+      const tipWrapper = new TIPWrapper(window);
+      ok(
+        tipWrapper.isAvailable(),
+        "TextInputProcessor should've been initialized"
+      );
+
+      await (async function test_ime_state_in_contenteditable_on_readonly_change() {
+        const expectedDataOfInitialization = await SpecialPowers.spawn(
+          browser,
+          [],
+          () => {
+            content.document.body.innerHTML = "<div contenteditable><br></div>";
+            content.wrappedJSObject.runner = content.wrappedJSObject.createIMEStateInContentEditableOnReadonlyChangeTester();
+            const editingHost = content.document.querySelector(
+              "div[contenteditable]"
+            );
+            return content.wrappedJSObject.runner.prepareToRun(
+              editingHost,
+              editingHost,
+              content.window
+            );
+          }
+        );
+        const tester = new IMEStateInContentEditableOnReadonlyChangeTester();
+        tester.checkResultOfPreparation(
+          expectedDataOfInitialization,
+          window,
+          tipWrapper
+        );
+        const expectedDataOfReadonly = await SpecialPowers.spawn(
+          browser,
+          [],
+          () => {
+            return content.wrappedJSObject.runner.runToMakeHTMLEditorReadonly();
+          }
+        );
+        tester.checkResultOfMakingHTMLEditorReadonly(expectedDataOfReadonly);
+        const expectedDataOfEditable = await SpecialPowers.spawn(
+          browser,
+          [],
+          () => {
+            return content.wrappedJSObject.runner.runToMakeHTMLEditorEditable();
+          }
+        );
+        tester.checkResultOfMakingHTMLEditorEditable(expectedDataOfEditable);
+        const expectedDataOfFinalization = await SpecialPowers.spawn(
+          browser,
+          [],
+          () => {
+            return content.wrappedJSObject.runner.runToRemoveContentEditableAttribute();
+          }
+        );
+        tester.checkResultOfRemovingContentEditableAttribute(
+          expectedDataOfFinalization
+        );
+        tester.clear();
+      })();
+
+      await (async function test_ime_state_in_button_in_contenteditable_on_readonly_change() {
+        const expectedDataOfInitialization = await SpecialPowers.spawn(
+          browser,
+          [],
+          () => {
+            content.document.body.innerHTML =
+              "<div contenteditable><br><button>button</button></div>";
+            content.wrappedJSObject.runner = content.wrappedJSObject.createIMEStateInContentEditableOnReadonlyChangeTester();
+            const editingHost = content.document.querySelector(
+              "div[contenteditable]"
+            );
+            return content.wrappedJSObject.runner.prepareToRun(
+              editingHost,
+              editingHost.querySelector("button"),
+              content.window
+            );
+          }
+        );
+        const tester = new IMEStateInContentEditableOnReadonlyChangeTester();
+        tester.checkResultOfPreparation(
+          expectedDataOfInitialization,
+          window,
+          tipWrapper
+        );
+        const expectedDataOfReadonly = await SpecialPowers.spawn(
+          browser,
+          [],
+          () => {
+            return content.wrappedJSObject.runner.runToMakeHTMLEditorReadonly();
+          }
+        );
+        tester.checkResultOfMakingHTMLEditorReadonly(expectedDataOfReadonly);
+        const expectedDataOfEditable = await SpecialPowers.spawn(
+          browser,
+          [],
+          () => {
+            return content.wrappedJSObject.runner.runToMakeHTMLEditorEditable();
+          }
+        );
+        tester.checkResultOfMakingHTMLEditorEditable(expectedDataOfEditable);
+        const expectedDataOfFinalization = await SpecialPowers.spawn(
+          browser,
+          [],
+          () => {
+            return content.wrappedJSObject.runner.runToRemoveContentEditableAttribute();
+          }
+        );
+        tester.checkResultOfRemovingContentEditableAttribute(
+          expectedDataOfFinalization
+        );
+        tester.clear();
+      })();
+    }
+  );
+});
