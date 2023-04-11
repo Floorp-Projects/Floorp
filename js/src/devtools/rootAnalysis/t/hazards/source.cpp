27c3916effcb64f638b43a02fe8827a508e914a2
Steve Fink <sfink@mozilla.com>
2023-04-11 23:08:43 +0000

    Bug 1820855 - [hazards] Make BFS_upwards a little simpler with respect to reaching the entry point, and comment. r=jonco
    This was necessary for a change that I ended up not needing, but the main point of this patch is the commentary.
    
    Differential Revision: https://phabricator.services.mozilla.com/D171895

--- js/src/devtools/rootAnalysis/t/hazards/source.cpp
+++ js/src/devtools/rootAnalysis/t/hazards/source.cpp
@@ -71,6 +71,8 @@ void GC() {
 
 extern void usecell(Cell*);
 
+extern bool flipcoin();
+
 void suppressedFunction() {
   GC();  // Calls GC, but is always called within AutoSuppressGC
 }
@@ -412,10 +414,14 @@ void safevals() {
     use(safe18);
   }
   {
-    Cell* unsafe19 = &cell;
-    void (*f)() = GC;
-    f();
-    use(unsafe19);
+    // A use after a GC, but not before. (This does not initialize safe19 by
+    // setting it to a value, because assignment would start its live range, and
+    // this test is to see if a variable with no known live range start requires
+    // a use before the GC or not. It should.)
+    Cell* safe19;
+    GC();
+    extern void initCellPtr(Cell**);
+    initCellPtr(&safe19);
   }
 }
 
