8bb1df8d1e51be94a7aac1af2a67c3a6ea921178
Dennis Schubert <mail@dennis-schubert.de>
2023-04-12 22:37:28 +0200

    Bug 1805410 - Ship v113.0.0 of the WebCompat System Addon.

--- mobile/android/android-components/components/feature/webcompat/src/main/assets/extensions/webcompat/injections/js/bug1818818-fastclick-legacy-shim.js
+++ mobile/android/android-components/components/feature/webcompat/src/main/assets/extensions/webcompat/injections/js/bug1818818-fastclick-legacy-shim.js
@@ -6,7 +6,7 @@
  * The patch is applied on sites using older version of FastClick library.
  * This allows to disable FastClick and fix various breakage caused
  * by the library.
- **/
+ */
 
 /* globals exportFunction */
 
