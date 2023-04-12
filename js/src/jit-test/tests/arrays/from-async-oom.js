f53817c4355f9a3255c037810aba8f03ff3db0fd
Matthew Gaudet <mgaudet@mozilla.com>
2023-04-12 16:03:25 +0000

    Bug 1797333 - Expand testing for Array.fromAsync r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D175176

---
+++ js/src/jit-test/tests/arrays/from-async-oom.js
@@ -0,0 +1,26 @@
+// |jit-test| --enable-array-from-async; skip-if: !Array.fromAsync|| !('oomTest' in this)
+
+// Basic Smoke Test
+async function* asyncGen(n) {
+    for (let i = 0; i < n; i++) {
+        yield i * 2;
+    }
+}
+
+function test() {
+    Array.fromAsync(asyncGen(4)).then((x) => {
+        assertEq(Array.isArray(x), true);
+        assertEq(x.length, 4);
+        assertEq(x[0], 0);
+        assertEq(x[1], 2);
+        assertEq(x[2], 4);
+        assertEq(x[3], 6);
+        done = true;
+    }
+    );
+
+    drainJobQueue();
+}
+
+oomTest(test);
+
