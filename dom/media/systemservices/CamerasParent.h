2597d0c0834c5546957ca7c54bccdf54659f21f1
Andreas Pehrson <apehrson@mozilla.com>
2023-04-12 07:16:20 +0000

    Bug 1800215 - In CamerasParent use sEngines only through a local reference. r=karlt
    This patch makes sEngines a refcounted array that is used on the IPC background
    thread only. This is to assign it in lockstep with sVideoCaptureThread and
    remove any doubt on whether a race exists on sEngines after cleaning up the
    singletons, for instance one could be imagined if another CamerasParent instance
    is created before the runnable to clean up sEngines has run on the video capture
    thread. The new CamerasParent instance would be accessing sEngines on another
    (new) video capture thread.
    
    This also makes CamerasParent instances access sEngines only through a local
    reference, mEngines. The array elements (the engines themselves) are only
    accessed on the video capture thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174256

--- dom/media/systemservices/CamerasParent.h
+++ dom/media/systemservices/CamerasParent.h
@@ -56,6 +56,7 @@ class CamerasParent final : public PCamerasParent,
       CamerasParent, mPBackgroundEventTarget)
 
  public:
+  class VideoEngineArray;
   friend DeliverFrameRunnable;
 
   static already_AddRefed<CamerasParent> Create();
@@ -134,6 +135,9 @@ class CamerasParent final : public PCamerasParent,
 
   // Local copy of sVideoCaptureThread. Guaranteed alive if non-null.
   const nsCOMPtr<nsISerialEventTarget> mVideoCaptureThread;
+
+  // Reference to same VideoEngineArray as sEngines. Video capture thread only.
+  const RefPtr<VideoEngineArray> mEngines;
 
   // image buffers
   ShmemPool mShmemPool;
