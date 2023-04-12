5a3ba671313b13b7f9de4ef1c5a4ea1d36508d7f
Andreas Pehrson <apehrson@mozilla.com>
2023-04-12 07:16:16 +0000

    Bug 1800215 - In CamerasParent use a ShutdownBlockingTicket instead of manual blocker shuffling on main thread. r=karlt
    This patch reworks the handling of the shutdown blocker by outsourcing it to
    ShutdownBlockingTicket. The logic of creating a ShutdownBlockingTicket is
    equivalent to our old behavior, in that we know synchronously off the main
    thread whether we were able to add the blocker.
    
    If shutdown is too far gone, creating a ShutdownBlockingTicket will fail, and
    CamerasParent sends a delete message without spinning up the video capture
    thread (which requires a shutdown blocker to tear down safely before shutdown is
    complete).
    
    Differential Revision: https://phabricator.services.mozilla.com/D166065

--- dom/media/systemservices/CamerasParent.h
+++ dom/media/systemservices/CamerasParent.h
@@ -65,9 +65,10 @@ class InputObserver : public webrtc::VideoInputFeedBack {
 
 class DeliverFrameRunnable;
 
-class CamerasParent final : public PCamerasParent,
-                            public nsIAsyncShutdownBlocker {
-  NS_DECL_THREADSAFE_ISUPPORTS
+class CamerasParent final : public PCamerasParent {
+  using ShutdownMozPromise = media::ShutdownBlockingTicket::ShutdownMozPromise;
+
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(CamerasParent)
 
  public:
   friend DeliverFrameRunnable;
@@ -131,13 +132,8 @@ class CamerasParent final : public PCamerasParent,
   void StopIPC();
   void StopVideoCapture();
   nsresult DispatchToVideoCaptureThread(RefPtr<Runnable> event);
-  NS_IMETHOD BlockShutdown(nsIAsyncShutdownClient*) override;
-  NS_IMETHOD GetName(nsAString& aName) override {
-    aName = mName;
-    return NS_OK;
-  }
-  NS_IMETHOD GetState(nsIPropertyBag**) override { return NS_OK; }
-  static nsString GetNewName();
+
+  void OnShutdown();
 
   // sEngines will be accessed by VideoCapture thread only
   // sNumOfCamerasParents, sNumOfOpenCamerasParentEngines, and
@@ -157,7 +153,11 @@ class CamerasParent final : public PCamerasParent,
   static base::Thread* sVideoCaptureThread;
 
   nsTArray<CallbackHelper*> mCallbacks;
-  nsString mName;
+  // If existent, blocks xpcom shutdown while alive.
+  // Note that this makes a reference cycle that gets broken in ActorDestroy().
+  const UniquePtr<media::ShutdownBlockingTicket> mShutdownBlocker;
+  // Tracks the mShutdownBlocker shutdown handler. mPBackgroundEventTarget only.
+  MozPromiseRequestHolder<ShutdownMozPromise> mShutdownRequest;
 
   // image buffers
   ShmemPool mShmemPool;
