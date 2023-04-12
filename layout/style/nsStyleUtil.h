059b18171dd5f88639521fcbd54c5754e0e397f5
Jonathan Kew <jkew@mozilla.com>
2023-04-12 16:05:11 +0000

    Bug 1121792 - Use more complete BCP47/RFC4647 language-range matching rather than simple prefix-dash match for the :lang() pseudo. r=emilio
    This is a Selectors-4 enhancement to the spec for the :lang() pseudo-class.
    It seems Safari has been shipping this behavior for some time.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174999

--- layout/style/nsStyleUtil.h
+++ layout/style/nsStyleUtil.h
@@ -37,6 +37,9 @@ class nsStyleUtil {
                                const nsAString& aSelectorValue,
                                const nsStringComparator& aComparator);
 
+  static bool LangTagCompare(const nsACString& aAttributeValue,
+                             const nsACString& aSelectorValue);
+
   static bool ValueIncludes(const nsAString& aValueList,
                             const nsAString& aValue,
                             const nsStringComparator& aComparator);
