f4eed392d82385a54993c51fc99f3adcb7d470c4
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-04-12 10:23:45 +0000

    Bug 1826502 - [devtools] Create a small utils similar to the classnames node package. r=devtools-reviewers,ochameau.
    Differential Revision: https://phabricator.services.mozilla.com/D174953

---
+++ devtools/client/shared/test/xpcshell/test_classnames.js
@@ -0,0 +1,53 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests utility function in `classnames.js`
+ */
+
+const classnames = require("resource://devtools/client/shared/classnames.js");
+
+add_task(async function() {
+  Assert.equal(
+    classnames(),
+    "",
+    "Returns an empty string when called with no params"
+  );
+  Assert.equal(
+    classnames(null, undefined, false),
+    "",
+    "Returns an empty string when called with only falsy params"
+  );
+  Assert.equal(
+    classnames("hello"),
+    "hello",
+    "Returns expected result when string is passed"
+  );
+  Assert.equal(
+    classnames("hello", "", "world"),
+    "hello world",
+    "Doesn't add extra spaces for empty strings"
+  );
+  Assert.equal(
+    classnames("hello", null, undefined, false, "world"),
+    "hello world",
+    "Doesn't add extra spaces for falsy values"
+  );
+  Assert.equal(
+    classnames("hello", { nice: true, blue: 42, world: {} }),
+    "hello nice blue world",
+    "Add property key when property value is truthy"
+  );
+  Assert.equal(
+    classnames("hello", { nice: false, blue: null, world: false }),
+    "hello",
+    "Does not add property key when property value is falsy"
+  );
+  Assert.equal(
+    classnames("hello", { nice: true }, { blue: true }, "world"),
+    "hello nice blue world",
+    "Handles multiple objects"
+  );
+});
