53481047f1594ef1f7bfdfde404627a8d67a5bd1
Dão Gottwald <dao@mozilla.com>
2023-04-13 17:37:19 +0000

    Bug 1827904 - Enable the result menu by default on all channels. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D175362

--- browser/components/urlbar/UrlbarPrefs.sys.mjs
+++ browser/components/urlbar/UrlbarPrefs.sys.mjs
@@ -63,7 +63,7 @@ const PREF_URLBAR_DEFAULTS = new Map([
 
   // Whether best match results can be blocked. This pref is a fallback for the
   // Nimbus variable `bestMatchBlockingEnabled`.
-  ["bestMatch.blockingEnabled", false],
+  ["bestMatch.blockingEnabled", true],
 
   // Whether the best match feature is enabled.
   ["bestMatch.enabled", true],
@@ -272,7 +272,7 @@ const PREF_URLBAR_DEFAULTS = new Map([
 
   // Whether the usual non-best-match quick suggest results can be blocked. This
   // pref is a fallback for the Nimbus variable `quickSuggestBlockingEnabled`.
-  ["quicksuggest.blockingEnabled", false],
+  ["quicksuggest.blockingEnabled", true],
 
   // Global toggle for whether the quick suggest feature is enabled, i.e.,
   // sponsored and recommended results related to the user's search string.
@@ -343,7 +343,7 @@ const PREF_URLBAR_DEFAULTS = new Map([
   ["quicksuggest.allowPositionInSuggestions", true],
 
   // Enable three-dot options button and menu for eligible results.
-  ["resultMenu", false],
+  ["resultMenu", true],
 
   // Allow the result menu button to be reached with the Tab key.
   ["resultMenu.keyboardAccessible", true],
