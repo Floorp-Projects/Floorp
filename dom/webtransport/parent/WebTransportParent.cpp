6701056d727c640a17813cb24360123a166cc732
Narcis Beleuzu <nbeleuzu@mozilla.com>
2023-04-14 23:11:43 +0300

    Backed out changeset 75d28b61c792 (bug 1818763) for multiple wpt failures. CLOSED TREE

--- dom/webtransport/parent/WebTransportParent.cpp
+++ dom/webtransport/parent/WebTransportParent.cpp
@@ -548,21 +548,6 @@ WebTransportParent::OnIncomingBidirectionalStreamAvailable(
   return NS_OK;
 }
 
-::mozilla::ipc::IPCResult WebTransportParent::RecvGetMaxDatagramSize(
-    GetMaxDatagramSizeResolver&& aResolver) {
-  LOG(("WebTransportParent RecvGetMaxDatagramSize"));
-  MOZ_ASSERT(mSocketThread->IsOnCurrentThread());
-  MOZ_ASSERT(mWebTransport);
-  MOZ_ASSERT(!mMaxDatagramSizeResolver);
-
-  mMaxDatagramSizeResolver = std::move(aResolver);
-  // maximum datagram size for the session is returned from network stack
-  // synchronously via WebTransportSessionEventListener::OnMaxDatagramSize
-  // interface
-  mWebTransport->GetMaxDatagramSize();
-  return IPC_OK();
-}
-
 // The promise sent in this request will be resolved
 // in OnOutgoingDatagramOutCome which is called synchronously from
 // WebTransportSessionProxy::SendDatagram
@@ -571,6 +556,7 @@ WebTransportParent::OnIncomingBidirectionalStreamAvailable(
     OutgoingDatagramResolver&& aResolver) {
   LOG(("WebTransportParent sending datagram"));
   MOZ_ASSERT(mSocketThread->IsOnCurrentThread());
+  MOZ_ASSERT(!mOutgoingDatagramResolver);
   MOZ_ASSERT(mWebTransport);
 
   Unused << aExpirationTime;
@@ -638,11 +624,8 @@ WebTransportParent::OnOutgoingDatagramOutCome(
 }
 
 NS_IMETHODIMP WebTransportParent::OnMaxDatagramSize(uint64_t aSize) {
-  LOG(("Max datagram size is %" PRIu64, aSize));
-  MOZ_ASSERT(mSocketThread->IsOnCurrentThread());
-  MOZ_ASSERT(mMaxDatagramSizeResolver);
-  mMaxDatagramSizeResolver(aSize);
-  mMaxDatagramSizeResolver = nullptr;
+  // XXX -  we need to pass this to  WebTransportChild via IPC
+  // See Bug 1818763
   return NS_OK;
 }
 }  // namespace mozilla::dom
