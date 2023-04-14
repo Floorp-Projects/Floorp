608d29b3f7d72d448389d0c9a4aa594cfe819dae
Mathew Hodson <mathew.hodson@gmail.com>
2023-04-14 08:48:38 +0000

    Bug 1824606 - Convert reflect.jsm to ESM. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D175200

--- toolkit/components/reflect/reflect.sys.mjs
+++ toolkit/components/reflect/reflect.sys.mjs
@@ -3,26 +3,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var EXPORTED_SYMBOLS = ["Reflect"];
-
 /*
  * This is the js module for Reflect. Import it like so:
- *   const { Reflect } = ChromeUtils.import(
- *     "resource://gre/modules/reflect.jsm"
+ *   const { Reflect } = ChromeUtils.importESModule(
+ *     "resource://gre/modules/reflect.sys.mjs"
  *   );
  *
  * This will create a 'Reflect' object, which provides an interface to the
  * SpiderMonkey parser API.
- *
- * For documentation on the API, see:
- * https://developer.mozilla.org/en/SpiderMonkey/Parser_API
- *
  */
 
-// Initialize the ctypes object. You do not need to do this yourself.
+// Initialize the Reflect object. You do not need to do this yourself.
 const init = Cc["@mozilla.org/jsreflect;1"].createInstance();
 init();
 
 // Reflect is a standard built-in defined on the shared global.
-// Export it via a lexical variable on the per-JSM global.
-const Reflect = globalThis.Reflect;
+export const Reflect = globalThis.Reflect;
