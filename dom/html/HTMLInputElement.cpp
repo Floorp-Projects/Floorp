c10ddbf008a39f4459923f7538f7e1af893e5ed5
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2023-04-15 02:30:35 +0000

    Bug 1827771 - Clear the revealed state when input type is changed from password. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D175516

--- dom/html/HTMLInputElement.cpp
+++ dom/html/HTMLInputElement.cpp
@@ -4416,6 +4416,12 @@ void HTMLInputElement::HandleTypeChange(FormControlType aNewType,
     fm->NeedsFlushBeforeEventHandling(this);
   }
 
+  if (oldType == FormControlType::InputPassword &&
+      State().HasState(ElementState::REVEALED)) {
+    // Modify the state directly to avoid dispatching events.
+    RemoveStates(ElementState::REVEALED);
+  }
+
   if (aNewType == FormControlType::InputFile ||
       oldType == FormControlType::InputFile) {
     if (aNewType == FormControlType::InputFile) {
