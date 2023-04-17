1964f0792a901f838b9f6654ff3170a34872eb54
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-04-17 09:12:42 +0000

    Bug 1479876 - [devtools] Add another exception to browser_console_clear_cache.js. r=devtools-reviewers,bomsy.
    Differential Revision: https://phabricator.services.mozilla.com/D175489

--- devtools/client/webconsole/test/browser/browser_console_clear_cache.js
+++ devtools/client/webconsole/test/browser/browser_console_clear_cache.js
@@ -14,9 +14,6 @@ add_task(async function() {
 
   await addTab(TEST_URI);
   let hud = await BrowserConsoleManager.toggleBrowserConsole();
-  // builtin-modules warning messages seem to be emitted late and causes the test to fail,
-  // so we filter those messages out (Bug 1479876)
-  await setFilterState(hud, { text: "-builtin-modules.js" });
 
   const CACHED_MESSAGE = "CACHED_MESSAGE";
   await logTextInContentAndWaitForMessage(hud, CACHED_MESSAGE);
@@ -31,7 +28,21 @@ add_task(async function() {
   // Check that there are no other messages logged (see Bug 1457478).
   // Log a message to make sure the console handled any prior log.
   await logTextInContentAndWaitForMessage(hud, "after clear");
-  const messages = hud.ui.outputNode.querySelectorAll(".message");
+  const messages = Array.from(
+    hud.ui.outputNode.querySelectorAll(".message")
+  ).filter(el => {
+    const location = el.querySelector(".frame-link-source");
+    // builtin-modules and RemoteSettingsComponents.sys.mjs messages seem to be emitted
+    // late and causes the test to fail, so we filter those messages out (Bug 1479876)
+    if (
+      location &&
+      (location.includes("builtin-module.js") ||
+        location.includes("RemoteSettingsComponents.sys.mjs"))
+    ) {
+      return false;
+    }
+    return true;
+  });
   is(messages.length, 1, "There is only the new message in the output");
 
   info("Close and re-open the browser console");
