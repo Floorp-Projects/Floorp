c8bd4a00bfc72190205c4eb7b500933481b37356
serge-sans-paille <sguelton@mozilla.com>
2023-04-17 10:11:56 +0000

    Bug 1826754 - Make toolkit/crashreporter/client buildable outside of a unified build environment r=andi
    Differential Revision: https://phabricator.services.mozilla.com/D174880

--- toolkit/crashreporter/client/crashreporter_linux.cpp
+++ toolkit/crashreporter/client/crashreporter_linux.cpp
@@ -76,7 +76,7 @@ static string Escape(const string& str) {
   return ret;
 }
 
-static bool WriteStrings(ostream& out, const string& header,
+static bool WriteStrings(std::ostream& out, const string& header,
                          StringTable& strings, bool escape) {
   out << "[" << header << "]" << std::endl;
   for (const auto& iter : strings) {
@@ -95,7 +95,7 @@ static bool WriteStrings(ostream& out, const string& header,
 
 static bool WriteStringsToFile(const string& path, const string& header,
                                StringTable& strings, bool escape) {
-  ofstream* f = UIOpenWrite(path, ios::trunc);
+  std::ofstream* f = UIOpenWrite(path, ios::trunc);
   bool success = false;
   if (f->is_open()) {
     success = WriteStrings(*f, header, strings, escape);
