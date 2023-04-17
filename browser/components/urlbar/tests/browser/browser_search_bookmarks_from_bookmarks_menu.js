d15d55c518451015b16a325bd09c07fc4eaaff12
Marc Seibert <mseibert@mozilla.com>
2023-04-17 10:31:35 +0000

    Bug 1736937 - Add "Search bookmarks" option in bookmarks menu. r=dao
    https://treeherder.mozilla.org/jobs?repo=try&revision=f1324632dfb7201ad71fddb074ec7e008c32d153
    
    Differential Revision: https://phabricator.services.mozilla.com/D175604

---
+++ browser/components/urlbar/tests/browser/browser_search_bookmarks_from_bookmarks_menu.js
@@ -0,0 +1,55 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+add_task(async function searchBookmarksFromBooksmarksMenu() {
+  // Add Button to toolbar
+  CustomizableUI.addWidgetToArea(
+    "bookmarks-menu-button",
+    CustomizableUI.AREA_NAVBAR,
+    0
+  );
+  let bookmarksMenuButton = document.getElementById("bookmarks-menu-button");
+  ok(bookmarksMenuButton, "Bookmarks Menu Button added");
+
+  // Open Bookmarks-Menu-Popup
+  let bookmarksMenuPopup = document.getElementById("BMB_bookmarksPopup");
+  let PopupShownPromise = BrowserTestUtils.waitForEvent(
+    bookmarksMenuPopup,
+    "popupshown"
+  );
+  EventUtils.synthesizeMouseAtCenter(bookmarksMenuButton, {
+    type: "mousedown",
+  });
+  await PopupShownPromise;
+  ok(true, "Bookmarks Menu Popup shown");
+
+  // Click on 'Search Bookmarks'
+  let searchBookmarksButton = document.getElementById("BMB_searchBookmarks");
+  ok(
+    BrowserTestUtils.is_visible(
+      searchBookmarksButton,
+      "'Search Bookmarks Button' is visible."
+    )
+  );
+  EventUtils.synthesizeMouseAtCenter(searchBookmarksButton, {});
+
+  await new Promise(resolve => {
+    window.gURLBar.controller.addQueryListener({
+      onViewOpen() {
+        window.gURLBar.controller.removeQueryListener(this);
+        resolve();
+      },
+    });
+  });
+
+  // Verify URLBar is in search mode with correct restriction
+  is(
+    gURLBar.searchMode?.source,
+    UrlbarUtils.RESULT_SOURCE.BOOKMARKS,
+    "Addressbar in correct mode."
+  );
+
+  CustomizableUI.reset();
+});
