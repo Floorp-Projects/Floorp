9be5ef7ca7061d24337841a4cdb5742b0d954d78
serge-sans-paille <sguelton@mozilla.com>
2023-04-18 09:42:14 +0000

    Bug 1826752 - Make media/gmp-clearkey/0.1 buildable outside of a unified build environment r=andi
    Differential Revision: https://phabricator.services.mozilla.com/D174878

--- media/gmp-clearkey/0.1/WMFUtils.h
+++ media/gmp-clearkey/0.1/WMFUtils.h
@@ -28,6 +28,7 @@
 #include <string>
 
 #include "VideoLimits.h"
+#include "content_decryption_module.h"
 #include "gmp-platform.h"
 #include "mozilla/Attributes.h"
 
@@ -124,8 +125,12 @@ typedef int64_t Microseconds;
     }                                                          \
   }
 
-#define STATUS_SUCCEEDED(x) ((x) == Status::kSuccess)
-#define STATUS_FAILED(x) ((x) != Status::kSuccess)
+inline bool STATUS_SUCCEEDED(cdm::Status status) {
+  return status == cdm::Status::kSuccess;
+}
+inline bool STATUS_FAILED(cdm::Status status) {
+  return !STATUS_SUCCEEDED(status);
+}
 
 #define MFPLAT_FUNC(_func, _dllname) extern decltype(::_func)* _func;
 #include "WMFSymbols.h"
