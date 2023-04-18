f56285726876140b97fce8c2e02b48f32aaa00ff
anayo <anayocrescent9@gmail.com>
2023-04-18 16:01:17 +0000

    Bug 1820654 - Replace the label link under manualUpdate with html element. r=tgiles,flod,application-update-reviewers,bytesized
    Differential Revision: https://phabricator.services.mozilla.com/D175011

--- browser/locales/en-US/browser/aboutDialog.ftl
+++ browser/locales/en-US/browser/aboutDialog.ftl
@@ -29,7 +29,7 @@ update-noUpdatesFound = { -brand-short-name } is up to date
 aboutdialog-update-checking-failed = Failed to check for updates.
 update-otherInstanceHandlingUpdates = { -brand-short-name } is being updated by another instance
 
-update-manual = Updates available at <label data-l10n-name="manual-link"/>
+aboutdialog-update-manual = Updates available at <a data-l10n-name="manual-link"/>
 
 update-unsupported = You can not perform further updates on this system. <label data-l10n-name="unsupported-link">Learn more</label>
 
--- toolkit/mozapps/update/tests/browser/head.js
+++ toolkit/mozapps/update/tests/browser/head.js
@@ -1116,6 +1116,12 @@ function runAboutPrefsUpdateTest(params, steps) {
             if (selectedPanel.id == "downloadFailed") {
               selector = "a.text-link";
             }
+            // The manualUpdate panel in about:preferences uses
+            // the moz-support-link element which doesn't have
+            // the .text-link class.
+            if (selectedPanel.id == "manualUpdate") {
+              selector = "a.manualLink";
+            }
             let link = selectedPanel.querySelector(selector);
             is(
               link.href,
