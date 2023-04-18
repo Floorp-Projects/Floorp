85c3bf414d18b994b6518909b874394aa9b4f13c
Brian <brianpt1106@gmail.com>
2023-04-18 14:09:31 +0000

    Bug 1824640 - Write a test that ensures that if there are no browser profile migrators available, the new Migration Wizard is sent to the NO_BROWSERS_FOUND page. r=mconley.
    Differential Revision: https://phabricator.services.mozilla.com/D175149

---
+++ browser/components/migration/tests/browser/browser_no_browsers_state.js
@@ -0,0 +1,44 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests that the wizard switches to the NO_BROWSERS_FOUND page
+ * when no migrators are detected.
+ */
+add_task(async function test_browser_no_programs() {
+  let sandbox = sinon.createSandbox();
+  registerCleanupFunction(() => {
+    sandbox.restore();
+  });
+
+  sandbox.stub(MigrationUtils, "availableMigratorKeys").get(() => {
+    return [];
+  });
+
+  await withMigrationWizardDialog(async prefsWin => {
+    let dialog = prefsWin.document.querySelector("#migrationWizardDialog");
+    let wizard = dialog.querySelector("migration-wizard");
+    let shadow = wizard.openOrClosedShadowRoot;
+    let deck = shadow.querySelector("#wizard-deck");
+
+    await BrowserTestUtils.waitForMutationCondition(
+      deck,
+      { attributeFilter: ["selected-view"] },
+      () => {
+        return (
+          deck.getAttribute("selected-view") ==
+          "page-" + MigrationWizardConstants.PAGES.NO_BROWSERS_FOUND
+        );
+      }
+    );
+
+    Assert.ok(
+      true,
+      "Went to no browser page after attempting to search for migrators."
+    );
+  });
+
+  sandbox.restore();
+});
