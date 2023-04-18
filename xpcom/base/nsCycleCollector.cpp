a6b1daa1bb156129ba5d5b0c6969da961c3b5835
Andrew McCreight <continuation@gmail.com>
2023-04-18 22:46:15 +0000

    Bug 1828747 - Add a way to skip some shutdown CC logs. r=smaug
    If MOZ_CC_LOG_SHUTDOWN_SKIP is set to a non-negative integer
    value, then skip that many of the initial shutdown CC logs.
    This is useful to reduce total log size if you know the
    initial few logs aren't useful, because they are the largest.
    
    Differential Revision: https://phabricator.services.mozilla.com/D175797

--- xpcom/base/nsCycleCollector.cpp
+++ xpcom/base/nsCycleCollector.cpp
@@ -242,6 +242,12 @@ static void SuspectUsingNurseryPurpleBuffer(
 //
 // MOZ_CC_LOG_SHUTDOWN: If defined, log cycle collector heaps at shutdown.
 //
+// MOZ_CC_LOG_SHUTDOWN_SKIP: If set to a non-negative integer value n, then
+// skip logging for the first n shutdown CCs. This implies MOZ_CC_LOG_SHUTDOWN.
+// The first log or two are much larger than the rest, so it can be useful to
+// reduce the total size of logs if you know already that the initial logs
+// aren't interesting.
+//
 // MOZ_CC_LOG_THREAD: If set to "main", only automatically log main thread
 // CCs. If set to "worker", only automatically log worker CCs. If set to "all",
 // log either. The default value is "all". This must be used with either
@@ -274,12 +280,23 @@ struct nsCycleCollectorParams {
   bool mAllTracesAll;
   bool mAllTracesShutdown;
   bool mLogThisThread;
+  int32_t mLogShutdownSkip = 0;
 
   nsCycleCollectorParams()
       : mLogAll(PR_GetEnv("MOZ_CC_LOG_ALL") != nullptr),
         mLogShutdown(PR_GetEnv("MOZ_CC_LOG_SHUTDOWN") != nullptr),
         mAllTracesAll(false),
         mAllTracesShutdown(false) {
+    if (const char* lssEnv = PR_GetEnv("MOZ_CC_LOG_SHUTDOWN_SKIP")) {
+      mLogShutdown = true;
+      nsDependentCString lssString(lssEnv);
+      nsresult rv;
+      int32_t lss = lssString.ToInteger(&rv);
+      if (NS_SUCCEEDED(rv) && lss >= 0) {
+        mLogShutdownSkip = lss;
+      }
+    }
+
     const char* logThreadEnv = PR_GetEnv("MOZ_CC_LOG_THREAD");
     bool threadLogging = true;
     if (logThreadEnv && !!strcmp(logThreadEnv, "all")) {
@@ -317,8 +334,20 @@ struct nsCycleCollectorParams {
     }
   }
 
-  bool LogThisCC(bool aIsShutdown) {
-    return (mLogAll || (aIsShutdown && mLogShutdown)) && mLogThisThread;
+  // aShutdownCount is how many shutdown CCs we've started.
+  // For non-shutdown CCs, we'll pass in 0.
+  // For the first shutdown CC, we'll pass in 1.
+  bool LogThisCC(int32_t aShutdownCount) {
+    if (mLogAll) {
+      return mLogThisThread;
+    }
+    if (aShutdownCount == 0 || !mLogShutdown) {
+      return false;
+    }
+    if (aShutdownCount <= mLogShutdownSkip) {
+      return false;
+    }
+    return mLogThisThread;
   }
 
   bool AllTracesThisCC(bool aIsShutdown) {
@@ -1102,6 +1131,7 @@ class nsCycleCollector : public nsIMemoryReporter {
   CycleCollectedJSRuntime* mCCJSRuntime;
 
   ccPhase mIncrementalPhase;
+  int32_t mShutdownCount = 0;
   CCGraph mGraph;
   UniquePtr<CCGraphBuilder> mBuilder;
   RefPtr<nsCycleCollectorLogger> mLogger;
@@ -3592,6 +3622,9 @@ void nsCycleCollector::BeginCollection(
   }
 
   bool isShutdown = (aReason == CCReason::SHUTDOWN);
+  if (isShutdown) {
+    mShutdownCount += 1;
+  }
 
   // Set up the listener for this CC.
   MOZ_ASSERT_IF(isShutdown, !aManualListener);
@@ -3602,7 +3635,7 @@ void nsCycleCollector::BeginCollection(
   }
 
   aManualListener = nullptr;
-  if (!mLogger && mParams.LogThisCC(isShutdown)) {
+  if (!mLogger && mParams.LogThisCC(mShutdownCount)) {
     mLogger = new nsCycleCollectorLogger();
     if (mParams.AllTracesThisCC(isShutdown)) {
       mLogger->SetAllTraces();
