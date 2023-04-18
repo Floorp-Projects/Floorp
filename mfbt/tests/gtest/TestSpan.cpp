40be9cdc56b05fee1198cf844fc10faf29dec978
serge-sans-paille <sguelton@mozilla.com>
2023-04-18 09:42:14 +0000

    Bug 1826756 - Make mfbt/tests/gtest buildable outside of a unified build environment r=andi
    Differential Revision: https://phabricator.services.mozilla.com/D174881

--- mfbt/tests/gtest/TestSpan.cpp
+++ mfbt/tests/gtest/TestSpan.cpp
@@ -19,6 +19,7 @@
 
 #include "gtest/gtest.h"
 
+#include "mozilla/Array.h"
 #include "mozilla/Span.h"
 
 #include "nsString.h"
--- mfbt/tests/gtest/TestTainting.cpp
+++ mfbt/tests/gtest/TestTainting.cpp
@@ -7,6 +7,7 @@
 #include "gtest/gtest.h"
 #include <math.h>
 
+#include "mozilla/Array.h"
 #include "mozilla/Assertions.h"
 #include "mozilla/Range.h"
 #include "mozilla/Tainting.h"
