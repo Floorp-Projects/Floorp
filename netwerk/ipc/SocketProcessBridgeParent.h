5197e26fa7fa572885fb8a631c04f9a2b5084e2e
Nika Layzell <nika@thelayzells.com>
2023-04-19 22:10:10 +0000

    Bug 1824465 - Part 19: Make PSocketProcessBridge refcounted, r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D173734

--- netwerk/ipc/SocketProcessBridgeParent.h
+++ netwerk/ipc/SocketProcessBridgeParent.h
@@ -17,10 +17,9 @@ namespace net {
 // called to destroy this actor.
 class SocketProcessBridgeParent final : public PSocketProcessBridgeParent {
  public:
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(SocketProcessBridgeParent)
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(SocketProcessBridgeParent, final)
 
-  explicit SocketProcessBridgeParent(
-      ProcessId aId, Endpoint<PSocketProcessBridgeParent>&& aEndpoint);
+  explicit SocketProcessBridgeParent(ProcessId aId);
 
   mozilla::ipc::IPCResult RecvTest();
   mozilla::ipc::IPCResult RecvInitBackground(
