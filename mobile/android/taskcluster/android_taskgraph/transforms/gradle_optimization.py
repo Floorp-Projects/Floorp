4b36c91e06e3a9b2f4cc6908ceeebd31acba58f0
Geoff Brown <gbrown@mozilla.com>
2023-04-19 15:08:15 -0600

    Bug 1825116 - Add license notice where missing (https://github.com/mozilla-mobile/firefox-android/pull/1637)
    * Bug 1825116 - Add license notice where missing
    
    * Bug 1825116 - Revert changes to strings.xml
    
    * Bug 1825116 - Restore ic_cc*.xml

--- mobile/android/taskcluster/android_taskgraph/transforms/gradle_optimization.py
+++ mobile/android/taskcluster/android_taskgraph/transforms/gradle_optimization.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from taskgraph.transforms.base import TransformSequence
 
 from ..build_config import get_path, get_upstream_deps_for_all_gradle_projects
