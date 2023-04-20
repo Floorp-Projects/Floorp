c7fcfdae0962494f0e5a3c3db5507a11fdf683df
Michael Froman <mfroman@mozilla.com>
2023-04-20 03:26:28 +0000

    Bug 1825620 - pt3 - prep_repo.sh updates r=ng DONTBUILD
    - use fetch_github_repo.py to get a fresh repro, that is ready for
      restoring the patch-stack.
    - use moz-patch-stack directory rather than previous github branch to
      restore previous patch-stack.
    - since we can now easily restore a mid-fast-forward patch-stack, we
      can default to storing moz-libwebrtc under STATE_DIR
    
    Depends on D175688
    
    Differential Revision: https://phabricator.services.mozilla.com/D175689

--- dom/media/webrtc/third_party_build/prep_repo.sh
+++ dom/media/webrtc/third_party_build/prep_repo.sh
@@ -15,7 +15,6 @@ source dom/media/webrtc/third_party_build/use_config_env.sh
 echo "MOZ_LIBWEBRTC_SRC: $MOZ_LIBWEBRTC_SRC"
 echo "MOZ_LIBWEBRTC_BRANCH: $MOZ_LIBWEBRTC_BRANCH"
 echo "MOZ_FASTFORWARD_BUG: $MOZ_FASTFORWARD_BUG"
-echo "MOZ_PRIOR_LIBWEBRTC_BRANCH: $MOZ_PRIOR_LIBWEBRTC_BRANCH"
 
 # After this point:
 # * eE: All commands should succeed.
@@ -33,46 +32,20 @@ if [ ! -f $STATE_DIR/milestone.cache ]; then
   cp $SCRIPT_DIR/pre-warmed-milestone.cache $STATE_DIR/milestone.cache
 fi
 
-# read the last line of README.moz-ff-commit to retrieve our current base
-# commit in moz-libwebrtc
-MOZ_LIBWEBRTC_BASE=`tail -1 third_party/libwebrtc/README.moz-ff-commit`
+# fetch the github repro
+./mach python $SCRIPT_DIR/fetch_github_repo.py \
+    --repo-path $MOZ_LIBWEBRTC_SRC \
+    --state-path $STATE_DIR
 
 CURRENT_DIR=`pwd`
 cd $MOZ_LIBWEBRTC_SRC
-
-# do a sanity fetch in case this was not a freshly cloned copy of the
-# repo, meaning it may not have all the mozilla branches present.
-git fetch --all
-
-# pull new upstream commits
-if [ "0" == `grep "https://webrtc.googlesource.com/src" .git/config | wc -l | tr -d " " || true` ]; then
-  echo "Setting up upstream remote (https://webrtc.googlesource.com/src)."
-  git checkout master
-  git remote add upstream https://webrtc.googlesource.com/src
-  git fetch upstream
-  git merge upstream/master
-else
-  echo "Upstream remote (https://webrtc.googlesource.com/src) already configured."
-fi
-
-# fetch upstream's branch-heads/xxxx
-if [ "0" == `grep "refs/remotes/branch-heads" .git/config | wc -l | tr -d " " || true` ]; then
-  echo "Setting up upstream remote branch-heads."
-  git config --local --add remote.upstream.fetch '+refs/branch-heads/*:refs/remotes/branch-heads/*'
-  git fetch upstream
-else
-  echo "Upstream remote branch-heads already configured."
-fi
-
-# checkout our previous branch to make sure commits are visible in the repo
-git checkout $MOZ_PRIOR_LIBWEBRTC_BRANCH
 
 # clear any possible previous patches
 rm -f *.patch
 
 # create a new work branch and "export" a new patch stack to rebase
-# find the common commit between our previous work branch and trunk
-CHERRY_PICK_BASE=`git merge-base $MOZ_PRIOR_LIBWEBRTC_BRANCH master`
+# find the common commit between our upstream release branch and trunk
+CHERRY_PICK_BASE=`git merge-base branch-heads/$MOZ_PRIOR_UPSTREAM_BRANCH_HEAD_NUM master`
 echo "common commit: $CHERRY_PICK_BASE"
 
 # create a new branch at the common commit and checkout the new branch
@@ -91,6 +64,9 @@ git branch $MOZ_LIBWEBRTC_BRANCH $CHERRY_PICK_BASE
 ERROR_HELP=""
 git checkout $MOZ_LIBWEBRTC_BRANCH
 
+# make sure we're starting with a clean tmp directory
+rm -f $TMP_DIR/*.patch $TMP_DIR/*.patch.bak
+
 # grab the patches for all the commits in chrome's release branch for libwebrtc
 git format-patch -o $TMP_DIR -k $CHERRY_PICK_BASE..branch-heads/$MOZ_PRIOR_UPSTREAM_BRANCH_HEAD_NUM
 # tweak the release branch commit summaries to show they were cherry picked
@@ -99,10 +75,15 @@ git am $TMP_DIR/*.patch # applies to branch mozpatches
 rm $TMP_DIR/*.patch $TMP_DIR/*.patch.bak
 
 # grab all the moz patches and apply
-# git format-patch -k $MOZ_LIBWEBRTC_BASE..$MOZ_PRIOR_LIBWEBRTC_BRANCH
-git format-patch -o $TMP_DIR -k branch-heads/$MOZ_PRIOR_UPSTREAM_BRANCH_HEAD_NUM..$MOZ_PRIOR_LIBWEBRTC_BRANCH
-git am $TMP_DIR/*.patch # applies to branch mozpatches
-rm $TMP_DIR/*.patch
+git am $CURRENT_DIR/third_party/libwebrtc/moz-patch-stack/*.patch
+
+# cp all the no-op files to STATE_DIR
+NO_OP_FILE_COUNT=`ls third_party/libwebrtc/moz-patch-stack \
+    | grep "no-op-cherry-pick-msg" | wc -l | tr -d " " || true`
+if [ "x$NO_OP_FILE_COUNT" != "x0" ]; then
+  cp $CURRENT_DIR/third_party/libwebrtc/moz-patch-stack/*.no-op-cherry-pick-msg \
+     $STATE_DIR
+fi
 
 cd $CURRENT_DIR
 
