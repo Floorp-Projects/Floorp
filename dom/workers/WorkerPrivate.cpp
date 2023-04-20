a0a6c2a933539ee0a7ffb7cd32ebdae8cd92c11a
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2023-04-20 17:41:47 +0000

    Bug 1824498 - Call shutdown when there isn't any ongoing fetching modules. r=jonco, a=RyanVM
    In the test file, worker_bug1824498.mjs, it imports two module scripts,
    "foo" and "bar", both are invalid specifiers.
    
    The ModuleLoadRequest of "foo" will be canceled, which in turn will:
    1. Cancel all imports of the parent ModuleLoadRequest (worker_bug1824498.mjs)
    2. Cancel the ModuleLoadRequest of "bar".
    
    After the step 1, WorkerModuleLoader::OnModuleLoadComplete will be
    called, and will shutdown the script loader.
    
    The shutdown causes two problems:
    1. When step 2 is executed, it will reject the mReady promise in
    ModuleLoadRequest, however when the MozPromise is dispatched, its event
    target has been shutdown so an assertion failure is triggered.
    
    2. Also when the ScriptLoaderRunnable of "bar" is received, it also
    triggers the assertion failure of the valid SyncLoopEventTarget.
    
    To fix the problem, we delay the shutdown until there's no ongoing module
    requests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D175476

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -5943,7 +5943,8 @@ WorkerPrivate::EventTarget::IsOnCurrentThread(bool* aIsOnCurrentThread) {
   MutexAutoLock lock(mMutex);
 
   if (mShutdown) {
-    NS_WARNING("A worker's event target was used after the worker has !");
+    NS_WARNING(
+        "A worker's event target was used after the worker has shutdown!");
     return NS_ERROR_UNEXPECTED;
   }
 
@@ -5960,7 +5961,8 @@ WorkerPrivate::EventTarget::IsOnCurrentThreadInfallible() {
   MutexAutoLock lock(mMutex);
 
   if (mShutdown) {
-    NS_WARNING("A worker's event target was used after the worker has !");
+    NS_WARNING(
+        "A worker's event target was used after the worker has shutdown!");
     return false;
   }
 
---
+++ dom/workers/test/worker_bug1824498.mjs
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-unassigned-import */
+/* eslint-disable import/no-unresolved */
+import {} from "./foo";
+import {} from "./bar";
