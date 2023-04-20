a5472643bfea3cafa29eab34b9c4760773086e7b
Michael Froman <mfroman@mozilla.com>
2023-04-20 11:32:50 -0500

    Bug 1828517 - Vendor libwebrtc from 4e83af3a9a
    Upstream commit: https://webrtc.googlesource.com/src/+/4e83af3a9a4443f275375d44c746d921dc076d55
        Adjust RTP header extension overhead for RRID
    
        which needs to be taken into account separately if the
        primary SSRC has been acknowledged but the RTX SSRC has
        not.
    
        If nothing has been acknowledged, mid+rid are sent on the primary SSRC and mid+rrid are sent on the RTX SSRC.
        If the primary SSRC has been acknowledged, no extensions are sent on the primary SSRC and mid+rrid are sent on the RTX SSRC.
        If both the primary SSRC and the RTX SSRC have been ack'd, no extensions are sent on either primary or RTX SSRC.
    
        BUG=webrtc:13896
    
        Change-Id: Ice251fae23a881ee9c9edc71b5d5c45a32ac76d2
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/288980
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Commit-Queue: Philipp Hancke <phancke@microsoft.com>
        Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#38949}

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_sender.cc
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_sender.cc
@@ -115,6 +115,7 @@ bool IsNonVolatile(RTPExtensionType type) {
     case kRtpExtensionTransportSequenceNumber:
     case kRtpExtensionTransportSequenceNumber02:
     case kRtpExtensionRtpStreamId:
+    case kRtpExtensionRepairedRtpStreamId:
     case kRtpExtensionMid:
     case kRtpExtensionGenericFrameDescriptor00:
     case kRtpExtensionGenericFrameDescriptor02:
@@ -126,7 +127,6 @@ bool IsNonVolatile(RTPExtensionType type) {
     case kRtpExtensionVideoContentType:
     case kRtpExtensionVideoLayersAllocation:
     case kRtpExtensionVideoTiming:
-    case kRtpExtensionRepairedRtpStreamId:
     case kRtpExtensionColorSpace:
     case kRtpExtensionVideoFrameTrackingId:
       return false;
@@ -755,25 +755,33 @@ void RTPSender::UpdateHeaderSizes() {
       rtp_header_length + RtpHeaderExtensionSize(kFecOrPaddingExtensionSizes,
                                                  rtp_header_extension_map_);
 
-  // RtpStreamId and Mid are treated specially in that we check if they
-  // currently are being sent. RepairedRtpStreamId is ignored because it is sent
-  // instead of RtpStreamId on rtx packets and require the same size.
+  // RtpStreamId, Mid and RepairedRtpStreamId are treated specially in that
+  // we check if they currently are being sent. RepairedRtpStreamId can be
+  // sent instead of RtpStreamID on RTX packets and may share the same space.
+  // When the primary SSRC has already been acked but the RTX SSRC has not
+  // yet been acked, RepairedRtpStreamId needs to be taken into account
+  // separately.
   const bool send_mid_rid_on_rtx =
-      rtx_ssrc_.has_value() && !rtx_ssrc_has_acked_;
-  const bool send_mid_rid =
-      always_send_mid_and_rid_ || !ssrc_has_acked_ || send_mid_rid_on_rtx;
+      rtx_ssrc_.has_value() &&
+      (always_send_mid_and_rid_ || !rtx_ssrc_has_acked_);
+  const bool send_mid_rid = always_send_mid_and_rid_ || !ssrc_has_acked_;
   std::vector<RtpExtensionSize> non_volatile_extensions;
   for (auto& extension :
        audio_configured_ ? AudioExtensionSizes() : VideoExtensionSizes()) {
     if (IsNonVolatile(extension.type)) {
       switch (extension.type) {
         case RTPExtensionType::kRtpExtensionMid:
-          if (send_mid_rid && !mid_.empty()) {
+          if ((send_mid_rid || send_mid_rid_on_rtx) && !mid_.empty()) {
             non_volatile_extensions.push_back(extension);
           }
           break;
         case RTPExtensionType::kRtpExtensionRtpStreamId:
           if (send_mid_rid && !rid_.empty()) {
+            non_volatile_extensions.push_back(extension);
+          }
+          break;
+        case RTPExtensionType::kRtpExtensionRepairedRtpStreamId:
+          if (send_mid_rid_on_rtx && !send_mid_rid && !rid_.empty()) {
             non_volatile_extensions.push_back(extension);
           }
           break;
