e756c9d9a37b151d9d3434c0687173ccb33ccca1
Alexandre Poirot <poirot.alex@gmail.com>
2023-04-21 18:00:42 +0000

    Bug 1827909 - [devtools] Use mutable map for generated to original source mapping. r=devtools-reviewers,nchevobbe
    This map is only used for removeThread action and we never return the
    content of this map as-is. So we can use mutable data here.
    For the map and the array values.
    
    Differential Revision: https://phabricator.services.mozilla.com/D175640

--- devtools/client/debugger/src/selectors/sources.js
+++ devtools/client/debugger/src/selectors/sources.js
@@ -347,7 +347,9 @@ export function getSourcesToRemoveForThread(state, threadActorID) {
       sourcesToRemove.push(state.sources.mutableSources.get(sourceId));
 
       // Also remove any original sources related to this generated source
-      const originalSourceIds = state.sources.originalSources[sourceId];
+      const originalSourceIds = state.sources.mutableOriginalSources.get(
+        sourceId
+      );
       if (originalSourceIds?.length > 0) {
         for (const originalSourceId of originalSourceIds) {
           sourcesToRemove.push(
