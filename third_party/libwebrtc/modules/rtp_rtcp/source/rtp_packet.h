10937ce258c9f01215d39fb2eb8b567370524923
Michael Froman <mfroman@mozilla.com>
2023-04-20 21:48:26 -0500

    Bug 1828517 - Vendor libwebrtc from 885ededbb8
    Upstream commit: https://webrtc.googlesource.com/src/+/885ededbb80257bdf6d5a103275ed3c46fb42b26
        Add move constructor and assign operator to RtpPacket
    
        RtpPacket has CopyOnWriteBuffer and std::vector that can be moved more
        efficiently than copied, thus move of the RtpPacket is also more efficient
    
        Bug: None
        Change-Id: I5509346e426cd32d0fb0649ef1a6883b7176df1d
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/290726
        Reviewed-by: Per Kjellander <perkj@webrtc.org>
        Commit-Queue: Per Kjellander <perkj@webrtc.org>
        Auto-Submit: Danil Chapovalov <danilchap@webrtc.org>
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39053}

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_packet.h
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_packet.h
@@ -34,11 +34,14 @@ class RtpPacket {
   // stored.
   RtpPacket();
   explicit RtpPacket(const ExtensionManager* extensions);
-  RtpPacket(const RtpPacket&);
   RtpPacket(const ExtensionManager* extensions, size_t capacity);
-  ~RtpPacket();
 
-  RtpPacket& operator=(const RtpPacket&) = default;
+  RtpPacket(const RtpPacket&);
+  RtpPacket(RtpPacket&&);
+  RtpPacket& operator=(const RtpPacket&);
+  RtpPacket& operator=(RtpPacket&&);
+
+  ~RtpPacket();
 
   // Parse and copy given buffer into Packet.
   // Does not require extension map to be registered (map is only required to
