66f94179f32006d1045fe6a1670aa3f6516f5bc8
Michael Froman <mfroman@mozilla.com>
2023-04-21 12:02:30 -0500

    Bug 1828517 - Vendor libwebrtc from e6b3f48a06
    Upstream commit: https://webrtc.googlesource.com/src/+/e6b3f48a06be05c82e8eacdfccf8f0e7720c99f3
        Reland "Move leb128 helper functions into own build target"
    
        This is a reland of commit fa962ffc698bda5bc7306ac5c3fd626eef737775
    
        Original change's description:
        > Move leb128 helper functions into own build target
        >
        > to remove duplicated implementation of these functions between av1 packetizer, av1 depacketizer and video allocation rtp header extension
        >
        > Bug: None
        > Change-Id: I30049f31c289bdb9e0aad6520f5145d1f999e635
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/290731
        > Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        > Reviewed-by: Philip Eliasson <philipel@webrtc.org>
        > Cr-Commit-Position: refs/heads/main@{#39069}
    
        Bug: None
        Change-Id: I091276868599a6716407db2972457507ddd46a8c
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/290961
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Reviewed-by: Philip Eliasson <philipel@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39135}

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_packetizer_av1.cc
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_packetizer_av1.cc
@@ -16,6 +16,7 @@
 
 #include "api/array_view.h"
 #include "api/video/video_frame_type.h"
+#include "modules/rtp_rtcp/source/leb128.h"
 #include "modules/rtp_rtcp/source/rtp_packet_to_send.h"
 #include "rtc_base/byte_buffer.h"
 #include "rtc_base/checks.h"
@@ -23,8 +24,6 @@
 
 namespace webrtc {
 namespace {
-// TODO(danilchap): Some of the helpers/constants are same as in
-// rtp_depacketizer_av1. Move them to common av1 file.
 constexpr int kAggregationHeaderSize = 1;
 // when there are 3 or less OBU (fragments) in a packet, size of the last one
 // can be omited.
@@ -45,29 +44,6 @@ bool ObuHasSize(uint8_t obu_header) {
 
 int ObuType(uint8_t obu_header) {
   return (obu_header & 0b0'1111'000) >> 3;
-}
-
-int Leb128Size(int value) {
-  RTC_DCHECK_GE(value, 0);
-  int size = 0;
-  while (value >= 0x80) {
-    ++size;
-    value >>= 7;
-  }
-  return size + 1;
-}
-
-// Returns number of bytes consumed.
-int WriteLeb128(uint32_t value, uint8_t* buffer) {
-  int size = 0;
-  while (value >= 0x80) {
-    buffer[size] = 0x80 | (value & 0x7F);
-    ++size;
-    value >>= 7;
-  }
-  buffer[size] = value;
-  ++size;
-  return size;
 }
 
 // Given `remaining_bytes` free bytes left in a packet, returns max size of an
