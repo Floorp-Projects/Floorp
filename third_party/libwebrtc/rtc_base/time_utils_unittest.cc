b8f86cf61b1ef3bcedd66f29089fccb924213f97
Michael Froman <mfroman@mozilla.com>
2023-04-20 22:53:39 -0500

    Bug 1828517 - Vendor libwebrtc from d8b6b06e70
    Upstream commit: https://webrtc.googlesource.com/src/+/d8b6b06e70718e4fd9bde65d6c5951860e7affae
        [Unwrap] Delete rtc::TimestampWrapAroundHandler
    
        Bug: webrtc:13982
        Change-Id: Ia2999e952a55d97dbd69ff19cf12c8f712b1a62f
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/290882
        Commit-Queue: Evan Shrubsole <eshr@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39087}

--- third_party/libwebrtc/rtc_base/time_utils_unittest.cc
+++ third_party/libwebrtc/rtc_base/time_utils_unittest.cc
@@ -63,59 +63,6 @@ TEST(TimeTest, TestTimeDiff64) {
   EXPECT_EQ(-ts_diff, rtc::TimeDiff(ts_earlier, ts_later));
 }
 
-class TimestampWrapAroundHandlerTest : public ::testing::Test {
- public:
-  TimestampWrapAroundHandlerTest() {}
-
- protected:
-  TimestampWrapAroundHandler wraparound_handler_;
-};
-
-TEST_F(TimestampWrapAroundHandlerTest, Unwrap) {
-  // Start value.
-  int64_t ts = 2;
-  EXPECT_EQ(ts,
-            wraparound_handler_.Unwrap(static_cast<uint32_t>(ts & 0xffffffff)));
-
-  // Wrap backwards.
-  ts = -2;
-  EXPECT_EQ(ts,
-            wraparound_handler_.Unwrap(static_cast<uint32_t>(ts & 0xffffffff)));
-
-  // Forward to 2 again.
-  ts = 2;
-  EXPECT_EQ(ts,
-            wraparound_handler_.Unwrap(static_cast<uint32_t>(ts & 0xffffffff)));
-
-  // Max positive skip ahead, until max value (0xffffffff).
-  for (uint32_t i = 0; i <= 0xf; ++i) {
-    ts = (i << 28) + 0x0fffffff;
-    EXPECT_EQ(
-        ts, wraparound_handler_.Unwrap(static_cast<uint32_t>(ts & 0xffffffff)));
-  }
-
-  // Wrap around.
-  ts += 2;
-  EXPECT_EQ(ts,
-            wraparound_handler_.Unwrap(static_cast<uint32_t>(ts & 0xffffffff)));
-
-  // Max wrap backward...
-  ts -= 0x0fffffff;
-  EXPECT_EQ(ts,
-            wraparound_handler_.Unwrap(static_cast<uint32_t>(ts & 0xffffffff)));
-
-  // ...and back again.
-  ts += 0x0fffffff;
-  EXPECT_EQ(ts,
-            wraparound_handler_.Unwrap(static_cast<uint32_t>(ts & 0xffffffff)));
-}
-
-TEST_F(TimestampWrapAroundHandlerTest, NoNegativeStart) {
-  int64_t ts = 0xfffffff0;
-  EXPECT_EQ(ts,
-            wraparound_handler_.Unwrap(static_cast<uint32_t>(ts & 0xffffffff)));
-}
-
 class TmToSeconds : public ::testing::Test {
  public:
   TmToSeconds() {
