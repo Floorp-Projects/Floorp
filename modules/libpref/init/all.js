e43f16db378bb902495dea10498d456d7a10239c
jneuberger <jneuberger@mozilla.com>
2023-04-24 08:36:43 +0000

    Bug 1828678 - Set signon.signupDetection.enabled to true in Nightly r=credential-management-reviewers,sgalich
    Differential Revision: https://phabricator.services.mozilla.com/D176021

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3192,7 +3192,11 @@ pref("signon.firefoxRelay.base_url", "https://relay.firefox.com/api/v1/");
 pref("signon.firefoxRelay.learn_more_url", "https://support.mozilla.org/1/firefox/%VERSION%/%OS%/%LOCALE%/firefox-relay-integration");
 pref("signon.firefoxRelay.manage_url", "https://relay.firefox.com");
 pref("signon.signupDetection.confidenceThreshold",     "0.75");
-pref("signon.signupDetection.enabled",                 false);
+#ifdef NIGHTLY_BUILD
+  pref("signon.signupDetection.enabled", true);
+#else
+  pref("signon.signupDetection.enabled", false);
+#endif
 
 // Satchel (Form Manager) prefs
 pref("browser.formfill.debug",            false);
