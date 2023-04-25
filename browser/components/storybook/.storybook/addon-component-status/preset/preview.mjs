b5255a354667620c28565d024b9a7b195d101677
Hanna Jones <hjones@mozilla.com>
2023-04-25 18:24:06 +0000

    Bug 1805491 - create addon to show component status in Storybook r=tgiles,mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D175931

---
+++ browser/components/storybook/.storybook/addon-component-status/preset/preview.mjs
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export const globalTypes = {
+  componentStatus: {
+    name: "Component status",
+    description:
+      "Provides a visual indicator of the component's readiness status.",
+    defaultValue: "default",
+  },
+};
