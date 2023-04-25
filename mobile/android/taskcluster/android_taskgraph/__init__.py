9c8e18a50cd36a68c023615ea01fabe1450d088a
Julien Cristau <julien+github@cristau.org>
2023-04-25 15:42:18 +0200

    Revert "Bug 1805651 - Update taskgraph to 5.1.0 and use index-path-regexes"
    This reverts commit d8939c35d509cb4839030a318db97025674aea20.

--- mobile/android/taskcluster/android_taskgraph/__init__.py
+++ mobile/android/taskcluster/android_taskgraph/__init__.py
@@ -27,7 +27,7 @@ def register(graph_config):
     Import all modules that are siblings of this one, triggering decorators in
     the process.
     """
-    _import_modules(["job", "routes", "target_tasks", "worker_types", "release_promotion"])
+    _import_modules(["job", "routes", "target_tasks", "worker_types", "release_promotion", "morph"])
 
 
 def _import_modules(modules):
