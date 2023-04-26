841ab3a366ec845921f3831bd773f2bc9f139de5
Lee Salzman <lsalzman@mozilla.com>
2023-04-26 16:58:44 +0000

    Bug 1830006 - Do not use malloc.h on OpenBSD. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D176556

--- gfx/skia/skia/src/base/SkContainers.cpp
+++ gfx/skia/skia/src/base/SkContainers.cpp
@@ -14,7 +14,7 @@
 
 #if defined(SK_BUILD_FOR_MAC) || defined(SK_BUILD_FOR_IOS)
 #include <malloc/malloc.h>
-#elif defined(SK_BUILD_FOR_ANDROID) || defined(SK_BUILD_FOR_UNIX)
+#elif defined(SK_BUILD_FOR_ANDROID) || (defined(SK_BUILD_FOR_UNIX) && !defined(__OpenBSD__))
 #include <malloc.h>
 #elif defined(SK_BUILD_FOR_WIN)
 #include <malloc.h>
@@ -38,7 +38,7 @@ SkSpan<std::byte> complete_size(void* ptr, size_t size) {
     #elif defined(SK_BUILD_FOR_ANDROID) && __ANDROID_API__ >= 17
         completeSize = malloc_usable_size(ptr);
         SkASSERT(completeSize >= size);
-    #elif defined(SK_BUILD_FOR_UNIX)
+    #elif defined(SK_BUILD_FOR_UNIX) && !defined(__OpenBSD__)
         completeSize = malloc_usable_size(ptr);
         SkASSERT(completeSize >= size);
     #elif defined(SK_BUILD_FOR_WIN)
