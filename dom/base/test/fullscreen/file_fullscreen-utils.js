63d3f30a51e430ddc12fd249206270b92416d476
Brad Werth <bwerth@mozilla.com>
2023-04-26 01:09:17 +0000

    Bug 1776738: Make fullscreen and pointerlock mochitests use document.fullscreenElement to determine fullscreen-ness. r=edgar
    With these changes, it's no longer necessary to store the normalSize on
    the window, since we never check it, nor check for resize events. This
    also removes the flaky timeouts, and replaces them with an executeSoon.
    
    For some of the fullscreen tests, the number of calls to enter and exit
    fullscreen are not matched. Since that count is essential for the checks
    of whether or not the screen is in the expected fullscreen, this defines a
    method that can force the count to a specific value. This is used in some
    of the fullscreen tests to get the expected results.
    
    Differential Revision: https://phabricator.services.mozilla.com/D172852

--- dom/base/test/fullscreen/file_fullscreen-utils.js
+++ dom/base/test/fullscreen/file_fullscreen-utils.js
@@ -1,90 +1,70 @@
-// Returns true if the window occupies the entire screen.
-// Note this only returns true once the transition from normal to
-// fullscreen mode is complete.
-function inFullscreenMode(win) {
-  return (
-    win.innerWidth == win.screen.width && win.innerHeight == win.screen.height
-  );
+// Keep track of how many fullscreenChange enters we've received, so that
+// we can balance them with the number of exits we receive. We reset this
+// to 0 when we load a test.
+var fullscreenChangeEnters = 0;
+
+addLoadEvent(function() {
+  info(`Resetting fullscreen enter count.`);
+  fullscreenChangeEnters = 0;
+});
+
+// This can be used to force a certain value for fullscreenChangeEnters
+// to handle unusual conditions -- such as exiting multiple levels of
+// fullscreen forcibly.
+function setFullscreenChangeEnters(enters) {
+  info(`Setting fullscreen enter count to ${enters}.`);
+  fullscreenChangeEnters = enters;
 }
 
-// Returns true if the window is in normal mode, i.e. non fullscreen mode.
-// Note this only returns true once the transition from fullscreen back to
-// normal mode is complete.
-function inNormalMode(win) {
-  return (
-    win.innerWidth == win.normalSize.w && win.innerHeight == win.normalSize.h
-  );
+// Returns true if the window believes it is in fullscreen. This may be true even
+// before an asynchronous fullscreen transition is complete.
+function inFullscreenMode(win) {
+  return win.document.fullscreenElement;
 }
 
 // Adds a listener that will be called once a fullscreen transition
 // is complete. When type==='enter', callback is called when we've
 // received a fullscreenchange event, and the fullscreen transition is
 // complete. When type==='exit', callback is called when we've
-// received a fullscreenchange event and the window dimensions match
-// the window dimensions when the window opened (so don't resize the
-// window while running your test!). inDoc is the document which
-// the listeners are added on, if absent, the listeners are added to
+// received a fullscreenchange event and the window is out of
+// fullscreen. inDoc is the document which the listeners are added on,
+// if absent, the listeners are added to the current document.
 // the current document.
 function addFullscreenChangeContinuation(type, callback, inDoc) {
   var doc = inDoc || document;
   var topWin = doc.defaultView.top;
-  // Remember the window size in non-fullscreen mode.
-  if (!topWin.normalSize) {
-    topWin.normalSize = {
-      w: window.innerWidth,
-      h: window.innerHeight,
-    };
-  }
   function checkCondition() {
     if (type == "enter") {
+      fullscreenChangeEnters++;
       return inFullscreenMode(topWin);
     } else if (type == "exit") {
-      // If we just revert the state to a previous fullscreen state,
-      // the window won't back to the normal mode. Hence we check
-      // fullscreenElement first here. Note that we need to check
-      // the fullscreen element of the outmost document here instead
-      // of the current one.
-      return topWin.document.fullscreenElement || inNormalMode(topWin);
+      fullscreenChangeEnters--;
+      return fullscreenChangeEnters
+        ? inFullscreenMode(topWin)
+        : !inFullscreenMode(topWin);
     }
     throw new Error("'type' must be either 'enter', or 'exit'.");
   }
-  function invokeCallback(event) {
-    // Use async call after a paint to workaround unfinished fullscreen
-    // change even when the window size has changed on Linux.
-    requestAnimationFrame(() => setTimeout(() => callback(event), 0), 0);
-  }
   function onFullscreenChange(event) {
     doc.removeEventListener("fullscreenchange", onFullscreenChange);
-    if (checkCondition()) {
-      invokeCallback(event);
-      return;
-    }
-    function onResize() {
-      if (checkCondition()) {
-        topWin.removeEventListener("resize", onResize);
-        invokeCallback(event);
-      }
-    }
-    topWin.addEventListener("resize", onResize);
+    ok(checkCondition(), `Should ${type} fullscreen.`);
+    // Delay invocation so other listeners have a chance to respond before
+    // we continue.
+    requestAnimationFrame(() => setTimeout(() => callback(event), 0), 0);
   }
   doc.addEventListener("fullscreenchange", onFullscreenChange);
 }
 
 // Calls |callback| when the next fullscreenerror is dispatched to inDoc||document.
 function addFullscreenErrorContinuation(callback, inDoc) {
-  return new Promise(resolve => {
-    let doc = inDoc || document;
-    let listener = function(event) {
-      doc.removeEventListener("fullscreenerror", listener);
-      setTimeout(function() {
-        if (callback) {
-          callback(event);
-        }
-        resolve();
-      }, 0);
-    };
-    doc.addEventListener("fullscreenerror", listener);
-  });
+  let doc = inDoc || document;
+  let listener = function(event) {
+    doc.removeEventListener("fullscreenerror", listener);
+    // Delay invocation so other listeners have a chance to respond before
+    // we continue.
+    requestAnimationFrame(() => setTimeout(() => callback(event), 0), 0);
+  };
+  doc.addEventListener("fullscreenerror", listener);
 }
 
 // Waits until the window has both the load event and a MozAfterPaint called on
