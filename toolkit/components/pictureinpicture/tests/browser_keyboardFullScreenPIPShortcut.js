f697ca3e733b490994a82fcfc73bf944d9455715
Bilal <bnasar@mozilla.com>
2023-04-27 19:12:51 +0000

    Bug 1724915 - Added a keyboard shortcut to enable full screen mode in PiP. r=pip-reviewers,kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D176568

---
+++ toolkit/components/pictureinpicture/tests/browser_keyboardFullScreenPIPShortcut.js
@@ -0,0 +1,41 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * tests that the F-key would enter and exit full screen mode in PiP.
+ */
+add_task(async () => {
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, TEST_PAGE);
+  let browser = tab.linkedBrowser;
+  let videoID = "with-controls";
+
+  let pipWin = await triggerPictureInPicture(browser, videoID);
+  ok(pipWin, "The Picture-in-Picture window is there.");
+
+  ok(
+    !pipWin.document.fullscreenElement,
+    "PiP should not yet be in fullscreen."
+  );
+
+  await promiseFullscreenEntered(pipWin, async () => {
+    EventUtils.synthesizeKey("f", {}, pipWin);
+  });
+
+  ok(
+    pipWin.document.fullscreenElement == pipWin.document.body,
+    "F-key should have caused to enter fullscreen."
+  );
+
+  await promiseFullscreenExited(pipWin, async () => {
+    EventUtils.synthesizeKey("f", {}, pipWin);
+  });
+
+  ok(
+    !pipWin.document.fullscreenElement,
+    "F-key should have caused to leave fullscreen."
+  );
+
+  await BrowserTestUtils.removeTab(tab);
+});
