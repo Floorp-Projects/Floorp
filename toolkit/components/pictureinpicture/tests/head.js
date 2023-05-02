851311e9007b73754deef27de59932ad679fdaab
DJ <dj@walker.dev>
2023-05-02 14:32:11 +0000

    Bug 1754549 - enable displaying hidden textTracks in player for specific sites. r=kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D174905

--- toolkit/components/pictureinpicture/tests/head.js
+++ toolkit/components/pictureinpicture/tests/head.js
@@ -946,17 +946,19 @@ async function isVideoMuted(browser, videoID) {
  * @param {Element} browser The <xul:browser> hosting the <video>
  * @param {String} videoID The ID of the video being checked
  * @param {Integer} defaultTrackIndex The index of the track to be loaded, or none if -1
+ * @param {String} trackMode the mode that the video's textTracks should be set to
  */
 async function prepareVideosAndWebVTTTracks(
   browser,
   videoID,
-  defaultTrackIndex = 0
+  defaultTrackIndex = 0,
+  trackMode = "showing"
 ) {
   info("Preparing video and initial text tracks");
   await ensureVideosReady(browser);
   await SpecialPowers.spawn(
     browser,
-    [{ videoID, defaultTrackIndex }],
+    [{ videoID, defaultTrackIndex, trackMode }],
     async args => {
       let video = content.document.getElementById(args.videoID);
       let tracks = video.textTracks;
@@ -967,8 +969,8 @@ async function prepareVideosAndWebVTTTracks(
       if (args.defaultTrackIndex >= 0) {
         info(`Loading track ${args.defaultTrackIndex + 1}`);
         let track = tracks[args.defaultTrackIndex];
-        tracks.mode = "showing";
-        track.mode = "showing";
+        tracks.mode = args.trackMode;
+        track.mode = args.trackMode;
       }
 
       // Briefly play the video to load text tracks onto the pip window.
