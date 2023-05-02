f82c985bfdd3da2c9ff7ad6dc69f822976a5e334
Chris H-C <chutten@mozilla.com>
2023-05-02 13:13:41 +0000

    Bug 1672273 - Provide enums in Rust and C++ for labeled_* metrics with static labels r=janerik
    First we have to replace the venerable metric hashmaps with metric `match`
    statements (like events). This is because, by adding the enum to the Labeled<>
    type, we've made it impossible to use HashMap (since its type requires a known
    Value type `V`, and Labeled<Counter, $SomeEnumType> is incomplete).
    
    Then we had to generate meaningful enums. The order of the values is important,
    as I foresee a future in which we may wish to use the enum variant value
    (its "discriminant") to index into the glean-core `labels` list.
    
    The enums can't have naming collisions. In Rust this is easy, so long as we
    stay away from reserved keywords (bug 1814767). In C++ we are using `enum
    class` so the variants' identifiers are scoped. However, on my system at least,
    this is still susceptible to collision with preprocessor defines. So we prefix
    the variants with the letter `e`. So far so good.
    
    C++ Labeled metric implementations is regrettably moved from Labeled.cpp to
    Labeled.h, as the template specialization is no longer full.
    
    No decision about JS is made or implied by its exclusion from this commit.
    This includes support for runtime registration (via JOG).
    
    As for Rust, this patch maintains the string-only Rust API advertised by the
    SDK's `Labeled<U>` trait. No decision has been made about whether to support
    the now-codegen'd Rust enum labels in FOG only through implementation against
    FOG's `LabeledMetric<U, E>` or by extending support into the SDK itself.
    
    The use of strings for labeled_* metrics is preserved mostly unchanged.
    
    The transformation from enum back to string label is performed in Rust during
    `enum_get`. This is an improvement over the current situation as the strings
    in use are guaranteed to not be allocated. However, I suspect we can improve
    even more by moving this translation lower (into the SDK, ideally).
    
    GIFFT support requires getting the label string from the enum, which is exposed
    on the FFI.
    
    Depends on D170108
    
    Differential Revision: https://phabricator.services.mozilla.com/D170109

--- toolkit/components/glean/api/src/ffi/labeled.rs
+++ toolkit/components/glean/api/src/ffi/labeled.rs
@@ -4,8 +4,15 @@
 
 #![cfg(feature = "with_gecko")]
 
+use crate::metrics::__glean_metric_maps as metric_maps;
 use nsstring::nsACString;
 use std::sync::atomic::Ordering;
+
+#[no_mangle]
+pub extern "C" fn fog_labeled_enum_to_str(id: u32, label: u16, value: &mut nsACString) {
+    let val = metric_maps::labeled_enum_to_str(id, label);
+    value.assign(&val);
+}
 
 #[no_mangle]
 pub extern "C" fn fog_labeled_boolean_get(id: u32, label: &nsACString) -> u32 {
@@ -13,6 +20,18 @@ pub extern "C" fn fog_labeled_boolean_get(id: u32, label: &nsACString) -> u32 {
         id,
         label,
         LABELED_BOOLEAN_MAP,
+        labeled_boolean_get,
+        BOOLEAN_MAP,
+        LabeledBooleanMetric
+    )
+}
+
+#[no_mangle]
+pub extern "C" fn fog_labeled_boolean_enum_get(id: u32, label: u16) -> u32 {
+    labeled_submetric_enum_get!(
+        id,
+        label,
+        labeled_boolean_enum_get,
         BOOLEAN_MAP,
         LabeledBooleanMetric
     )
@@ -24,6 +43,18 @@ pub extern "C" fn fog_labeled_counter_get(id: u32, label: &nsACString) -> u32 {
         id,
         label,
         LABELED_COUNTER_MAP,
+        labeled_counter_get,
+        COUNTER_MAP,
+        LabeledCounterMetric
+    )
+}
+
+#[no_mangle]
+pub extern "C" fn fog_labeled_counter_enum_get(id: u32, label: u16) -> u32 {
+    labeled_submetric_enum_get!(
+        id,
+        label,
+        labeled_counter_enum_get,
         COUNTER_MAP,
         LabeledCounterMetric
     )
@@ -35,6 +66,18 @@ pub extern "C" fn fog_labeled_string_get(id: u32, label: &nsACString) -> u32 {
         id,
         label,
         LABELED_STRING_MAP,
+        labeled_string_get,
+        STRING_MAP,
+        LabeledStringMetric
+    )
+}
+
+#[no_mangle]
+pub extern "C" fn fog_labeled_string_enum_get(id: u32, label: u16) -> u32 {
+    labeled_submetric_enum_get!(
+        id,
+        label,
+        labeled_string_enum_get,
         STRING_MAP,
         LabeledStringMetric
     )
