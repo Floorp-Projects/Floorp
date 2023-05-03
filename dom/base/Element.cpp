d38f7163df102bee92f52fa2ed1f055aa77e0d00
Boris Chiou <boris.chiou@gmail.com>
2023-05-03 22:49:21 +0000

    Bug 1677844 - Let getAnimations() return scroll animations with null timeline. r=webidl,smaug,emilio
    This is a tentative way to avoid any timeout in WPT because other
    brower vendors may expect getAnimations() works for scroll animations.
    
    The original implementation is error-prone because the user can use the
    declarative way to create a CSS animation associated with the document-timeline,
    and then changes animation-timeline property to any other scroll-timeline.
    If the user calls getAnimations() before changing its style, we still expose
    the Animation objects in JS and may crash or have other unexpected bahaviors
    if the user tries to access Animation.timeline.
    
    Besides, another benefit of this patch is that we can use other Web Animation
    APIs for scroll animations in WPT, e.g. Animation.ready.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176966

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -3780,13 +3780,6 @@ void Element::GetAnimationsUnsorted(Element* aElement,
                "added to an element's effect set");
     Animation* animation = effect->GetAnimation();
 
-    // FIXME: Bug 1676795: Don't expose scroll-driven animations because we are
-    // not ready.
-    if (animation->GetTimeline() &&
-        animation->GetTimeline()->IsScrollTimeline()) {
-      continue;
-    }
-
     MOZ_ASSERT(animation->IsRelevant(),
                "Only relevant animations should be added to an element's "
                "effect set");
