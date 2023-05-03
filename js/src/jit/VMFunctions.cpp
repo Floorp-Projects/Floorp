a84d06eb94914d2fb826ab18cc6f78b930d8cfaa
Doug Thayer <dothayer@mozilla.com>
2023-05-03 21:12:39 +0000

    Bug 1828496 - Don't bother with receiver param for SetElementMegamorphic r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D175680

--- js/src/jit/VMFunctions.cpp
+++ js/src/jit/VMFunctions.cpp
@@ -2093,8 +2093,7 @@ static bool TryAddOrSetPlainObjectProperty(JSContext* cx,
 }
 
 bool SetElementMegamorphic(JSContext* cx, HandleObject obj, HandleValue index,
-                           HandleValue value, HandleValue receiver,
-                           bool strict) {
+                           HandleValue value, bool strict) {
   if (obj->is<PlainObject>()) {
     bool optimized = false;
     if (!TryAddOrSetPlainObjectProperty<false>(cx, obj.as<PlainObject>(), index,
@@ -2105,12 +2104,13 @@ bool SetElementMegamorphic(JSContext* cx, HandleObject obj, HandleValue index,
       return true;
     }
   }
+  Rooted<Value> receiver(cx, ObjectValue(*obj));
   return SetObjectElementWithReceiver(cx, obj, index, value, receiver, strict);
 }
 
 bool SetElementMegamorphicCached(JSContext* cx, HandleObject obj,
                                  HandleValue index, HandleValue value,
-                                 HandleValue receiver, bool strict) {
+                                 bool strict) {
   if (obj->is<PlainObject>()) {
     bool optimized = false;
     if (!TryAddOrSetPlainObjectProperty<true>(cx, obj.as<PlainObject>(), index,
@@ -2121,6 +2121,7 @@ bool SetElementMegamorphicCached(JSContext* cx, HandleObject obj,
       return true;
     }
   }
+  Rooted<Value> receiver(cx, ObjectValue(*obj));
   return SetObjectElementWithReceiver(cx, obj, index, value, receiver, strict);
 }
 
