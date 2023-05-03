b4671d5c5b209df825a07ba4d08ef73defa11dab
anonymous0000007 <shahrushil09@gmail.com>
2023-05-03 13:25:42 +0000

    Bug 1710232 - remove dead code unused function UseSiteForFirstPartyDomain in OriginAttributes.h. r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D176790

--- caps/OriginAttributes.h
+++ caps/OriginAttributes.h
@@ -113,13 +113,6 @@ class OriginAttributes : public dom::OriginAttributesDictionary {
     return StaticPrefs::privacy_firstparty_isolate();
   }
 
-  static inline bool UseSiteForFirstPartyDomain() {
-    if (IsFirstPartyEnabled()) {
-      return StaticPrefs::privacy_firstparty_isolate_use_site();
-    }
-    return StaticPrefs::privacy_dynamic_firstparty_use_site();
-  }
-
   // check if the access of window.opener across different FPDs is restricted.
   // We only restrict the access of window.opener when first party isolation
   // is enabled and "privacy.firstparty.isolate.restrict_opener_access" is on.
