88237456180fb7455c15e86783d44006809dadbf
Kagami Sascha Rosylight <krosylight@mozilla.com>
2023-05-03 07:33:10 +0000

    Bug 1830384 - Skip cleanup when no sanitization happened. r=hpeuckmann, a=RyanVM
    D176147 falsely let the process always run; this should now skip it when not needed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176709

--- browser/modules/Sanitizer.sys.mjs
+++ browser/modules/Sanitizer.sys.mjs
@@ -963,10 +963,12 @@ async function maybeSanitizeSessionPrincipals(progress, principals, flags) {
   });
 
   progress.step = "promises:" + promises.length;
-  await Promise.all(promises);
-  await new Promise(resolve =>
-    Services.clearData.cleanupAfterDeletionAtShutdown(flags, resolve)
-  );
+  if (promises.length) {
+    await Promise.all(promises);
+    await new Promise(resolve =>
+      Services.clearData.cleanupAfterDeletionAtShutdown(flags, resolve)
+    );
+  }
   progress.step = "promises resolved";
 }
 
--- toolkit/components/cleardata/tests/marionette/test_moved_origin_directory_cleanup.py
+++ toolkit/components/cleardata/tests/marionette/test_moved_origin_directory_cleanup.py
@@ -21,7 +21,35 @@ class MovedOriginDirectoryCleanupTestCase(MarionetteTestCase):
         self.moved_origin_directory = (
             Path(self.marionette.profile_path) / "storage" / "to-be-removed" / "foo"
         )
-        self.moved_origin_directory.mkdir(parents=True)
+        self.moved_origin_directory.mkdir(parents=True, exist_ok=True)
+
+        # Add a cookie to get a principal to be cleaned up
+        with self.marionette.using_context("chrome"):
+            self.marionette.execute_script(
+                """
+                Services.cookies.add(
+                    "example.local",
+                    "path",
+                    "name",
+                    "value",
+                    false,
+                    false,
+                    false,
+                    Math.floor(Date.now() / 1000) + 24 * 60 * 60,
+                    {},
+                    Ci.nsICookie.SAMESITE_NONE,
+                    Ci.nsICookie.SCHEME_UNSET
+                );
+                """
+            )
+
+    def removeAllCookies(self):
+        with self.marionette.using_context("chrome"):
+            self.marionette.execute_script(
+                """
+                Services.cookies.removeAll();
+                """
+            )
 
     def test_ensure_cleanup_by_quit(self):
         self.assertTrue(
@@ -74,4 +102,33 @@ class MovedOriginDirectoryCleanupTestCase(MarionetteTestCase):
         Wait(self.marionette).until(
             lambda _: not self.moved_origin_directory.exists(),
             message="to-be-removed subdirectory must disappear",
+        )
+
+    def test_ensure_no_cleanup_when_disabled(self):
+        self.assertTrue(
+            self.moved_origin_directory.exists(),
+            "to-be-removed subdirectory must exist",
+        )
+
+        self.marionette.set_pref("privacy.sanitize.sanitizeOnShutdown", False)
+        self.marionette.quit()
+
+        self.assertTrue(
+            self.moved_origin_directory.exists(),
+            "to-be-removed subdirectory must not disappear",
+        )
+
+    def test_ensure_no_cleanup_when_no_cookie(self):
+        self.assertTrue(
+            self.moved_origin_directory.exists(),
+            "to-be-removed subdirectory must exist",
+        )
+
+        self.removeAllCookies()
+
+        self.marionette.quit()
+
+        self.assertTrue(
+            self.moved_origin_directory.exists(),
+            "to-be-removed subdirectory must not disappear",
         )
