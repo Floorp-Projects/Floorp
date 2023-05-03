200dce84689701fde72e4801bbfa02cb526e9fc1
Valentin Gosu <valentin.gosu@gmail.com>
2023-05-03 12:40:54 +0000

    Bug 1818714 - Remove cachePurge lock file after running BackgroundTask_removeDirectory r=necko-reviewers,saschanaz,jesup
    Differential Revision: https://phabricator.services.mozilla.com/D176548

--- netwerk/test/marionette/test_purge_http_cache_at_shutdown.py
+++ netwerk/test/marionette/test_purge_http_cache_at_shutdown.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
 from pathlib import Path
 
 from marionette_driver import Wait
@@ -34,8 +35,28 @@ class PurgeHTTPCacheAtShutdownTestCase(MarionetteTestCase):
             child.name.endswith(".purge.bg_rm") for child in self.profile_path.iterdir()
         )
 
-    def test_ensure_cache_purge_after_in_app_quit(self):
-        self.assertTrue(self.cacheDirExists(), "Cache directory must exist")
+    def initLockDir(self):
+        self.lock_dir = None
+        with self.marionette.using_context("chrome"):
+            path = self.marionette.execute_script(
+                """
+              return Services.dirsvc.get("UpdRootD", Ci.nsIFile).parent.parent.path;
+            """
+            )
+            self.lock_dir = Path(path)
+
+    def assertNoLocks(self):
+        locks = [
+            x
+            for x in self.lock_dir.iterdir()
+            if x.is_file() and "-cachePurge" in x.name
+        ]
+        self.assertEqual(locks, [], "All locks should have been removed")
+
+    # Tests are run in lexicographical order, so test_a* is run first to
+    # cleanup locks that may be there from previous runs.
+    def test_asetup(self):
+        self.initLockDir()
 
         self.marionette.quit()
 
@@ -44,8 +65,33 @@ class PurgeHTTPCacheAtShutdownTestCase(MarionetteTestCase):
             message="Cache directory must be removed after orderly shutdown",
         )
 
+        # delete locks from previous runs
+        locks = [
+            x
+            for x in self.lock_dir.iterdir()
+            if x.is_file() and "-cachePurge" in x.name
+        ]
+        for lock in locks:
+            os.remove(lock)
+        # all locks should have been removed successfully.
+        self.assertNoLocks()
+
+    def test_ensure_cache_purge_after_in_app_quit(self):
+        self.assertTrue(self.cacheDirExists(), "Cache directory must exist")
+        self.initLockDir()
+
+        self.marionette.quit()
+
+        Wait(self.marionette, timeout=60).until(
+            lambda _: not self.cacheDirExists() and not self.renamedDirExists(),
+            message="Cache directory must be removed after orderly shutdown",
+        )
+
+        self.assertNoLocks()
+
     def test_longstanding_cache_purge_after_in_app_quit(self):
         self.assertTrue(self.cacheDirExists(), "Cache directory must exist")
+        self.initLockDir()
 
         self.marionette.set_pref(
             "toolkit.background_tasks.remove_directory.testing.sleep_ms", 5000
@@ -58,12 +104,15 @@ class PurgeHTTPCacheAtShutdownTestCase(MarionetteTestCase):
             message="Cache directory must be removed after orderly shutdown",
         )
 
+        self.assertNoLocks()
+
     def test_ensure_cache_purge_after_forced_restart(self):
         """
         Doing forced restart here to prevent the shutdown phase purging and only allow startup
         phase one, via `CacheFileIOManager::OnDelayedStartupFinished`.
         """
         self.profile_path.joinpath("foo.purge.bg_rm").mkdir()
+        self.initLockDir()
 
         self.marionette.restart(in_app=False)
 
@@ -72,3 +121,5 @@ class PurgeHTTPCacheAtShutdownTestCase(MarionetteTestCase):
             message="Directories with .purge.bg_rm postfix must be removed at startup after"
             "disorderly shutdown",
         )
+
+        self.assertNoLocks()
--- toolkit/xre/MultiInstanceLock.cpp
+++ toolkit/xre/MultiInstanceLock.cpp
@@ -31,8 +31,9 @@
 
 namespace mozilla {
 
-static bool GetLockFileName(const char* nameToken, const char16_t* installPath,
-                            nsCString& filePath) {
+bool GetMultiInstanceLockFileName(const char* nameToken,
+                                  const char16_t* installPath,
+                                  nsCString& filePath) {
 #ifdef XP_WIN
   // On Windows, the lock file is placed at the path
   // [updateDirectory]\[nameToken]-[pathHash], so first we need to get the
@@ -108,7 +109,7 @@ static bool GetLockFileName(const char* nameToken, const char16_t* installPath,
 MultiInstLockHandle OpenMultiInstanceLock(const char* nameToken,
                                           const char16_t* installPath) {
   nsCString filePath;
-  if (!GetLockFileName(nameToken, installPath, filePath)) {
+  if (!GetMultiInstanceLockFileName(nameToken, installPath, filePath)) {
     return MULTI_INSTANCE_LOCK_HANDLE_ERROR;
   }
 
