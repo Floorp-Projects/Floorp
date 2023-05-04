6a6c220ac4ea11de2d8b4e23bbba007474e7a8eb
Mark Banner <standard8@mozilla.com>
2023-05-04 17:21:03 +0000

    Bug 1830265 - Tidy up .prettierignore. r=Gijs,sync-reviewers,markh
    Enables Prettier for (these don't need to be excluded):
    - extensions/pref/autoconfig/test/unit/autoconfig.js
    - services/sync/tests/unit/prefs_test_prefs_store.js
    
    Removes duplications where the files are already ignored in the .eslintignore section.
    
    Re-organises the structure of the remaining parts to make it clearer why they are being excluded.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176636

--- extensions/pref/autoconfig/test/unit/autoconfig.js
+++ extensions/pref/autoconfig/test/unit/autoconfig.js
@@ -3,4 +3,3 @@ pref("general.config.sandbox_enabled", true);
 pref("general.config.filename", "autoconfig.cfg");
 pref("general.config.vendor", "autoconfig");
 pref("general.config.obscure_value", 0);
-
