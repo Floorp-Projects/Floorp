b86c34c1776365df6fa6f821f067948ea91da964
Edgar Chen <echen@mozilla.com>
2023-05-04 18:45:41 +0000

    Bug 1830721 - Update non-client margins before changing fullscreen mode; r=rkraesig, a=RyanVM
    We were replying on the resize event fired for fullscreen mode change to
    trigger reflow. But in
    https://phabricator.services.mozilla.com/D175215,
    we started notifying sizemodechange after fullscreen mode has changed,
    which could result in the layout not being updated properly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D177103

---
+++ widget/tests/browser/browser_test_fullscreen_size.js
@@ -0,0 +1,66 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+"use strict";
+
+function waitForReflow(aWindow) {
+  return new Promise(resolve => {
+    aWindow.requestAnimationFrame(() => {
+      aWindow.requestAnimationFrame(resolve);
+    });
+  });
+}
+
+add_task(async function fullscreen_size() {
+  let win = await BrowserTestUtils.openNewBrowserWindow({});
+  win.gBrowser.selectedBrowser.focus();
+
+  info("Enter browser fullscreen mode");
+  let promise = Promise.all([
+    BrowserTestUtils.waitForEvent(win, "fullscreen"),
+    BrowserTestUtils.waitForEvent(win, "resize"),
+  ]);
+  win.fullScreen = true;
+  await promise;
+
+  info("Await reflow of the chrome window");
+  await waitForReflow(win);
+
+  is(win.innerHeight, win.outerHeight, "Check height");
+  is(win.innerWidth, win.outerWidth, "Check width");
+
+  await BrowserTestUtils.closeWindow(win);
+});
+
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1830721
+add_task(async function fullscreen_size_moz_appearance() {
+  const win = await BrowserTestUtils.openNewBrowserWindow({});
+  win.gBrowser.selectedBrowser.focus();
+
+  info("Add -moz-appearance style to chrome document");
+  const style = win.document.createElement("style");
+  style.innerHTML = `
+    #main-window {
+      -moz-appearance: -moz-win-borderless-glass;
+    }
+  `;
+  win.document.head.appendChild(style);
+
+  info("Await reflow of the chrome window");
+  await waitForReflow(win);
+
+  info("Enter browser fullscreen mode");
+  let promise = Promise.all([
+    BrowserTestUtils.waitForEvent(win, "fullscreen"),
+    BrowserTestUtils.waitForEvent(win, "resize"),
+  ]);
+  win.fullScreen = true;
+  await promise;
+
+  info("Await reflow of the chrome window");
+  await waitForReflow(win);
+
+  is(win.innerHeight, win.outerHeight, "Check height");
+  is(win.innerWidth, win.outerWidth, `Check width`);
+
+  await BrowserTestUtils.closeWindow(win);
+});
