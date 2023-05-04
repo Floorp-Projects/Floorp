6f52a4f0b1ecfe0b9c26ad8a9c1a692a48c7b821
Mark Banner <standard8@mozilla.com>
2023-05-04 17:21:03 +0000

    Bug 1830265 - Enable ESLint and Prettier on .babel-eslint.rc.js. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D176637

--- .babel-eslint.rc.js
+++ .babel-eslint.rc.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* eslint-env node */
+
 module.exports = {
-  plugins: [
-    "@babel/plugin-syntax-jsx",
-  ],
+  plugins: ["@babel/plugin-syntax-jsx"],
 };
