8fc4f989846d5049570ec440e82f9429092fccde
Stanca Serban <sstanca@mozilla.com>
2023-05-05 04:28:52 +0300

    Backed out changeset 72f882230f63 (bug 1829039) for causing mochitests failures in browser_aboutwelcome_multistage_mr.js. CLOSED TREE

--- browser/components/newtab/test/browser/browser_aboutwelcome_multistage_mr.js
+++ browser/components/newtab/test/browser/browser_aboutwelcome_multistage_mr.js
@@ -40,12 +40,6 @@ function initSandbox({ pin = true, isDefault = false } = {}) {
   return sandbox;
 }
 
-add_setup(async function() {
-  await SpecialPowers.pushPrefEnv({
-    set: [["browser.aboutwelcome.transitions", false]],
-  });
-});
-
 /**
  * Test MR message telemetry
  */
@@ -354,7 +348,6 @@ add_task(async function test_aboutwelcome_gratitude() {
   // make sure the button navigates to newtab
   await test_screen_content(
     browser,
-    "home",
     //Expected selectors
     ["body.activity-stream"],
 
@@ -467,23 +460,12 @@ add_task(async function test_aboutwelcome_embedded_migration() {
 
     // Recalculate the <panel-list> rect top value relative to the top-left
     // of the selectorRect. We expect the <panel-list> to be tightly anchored
-    // to the bottom of the <button>, so we expect this new value to be close to 0,
-    // to account for subpixel rounding
+    // to the bottom of the <button>, so we expect this new value to be 0.
     let panelTopLeftRelativeToAnchorTopLeft =
       panelRect.top - selectorRect.top - selectorRect.height;
-
-    function isfuzzy(actual, expected, epsilon, msg) {
-      if (actual >= expected - epsilon && actual <= expected + epsilon) {
-        ok(true, msg);
-      } else {
-        is(actual, expected, msg);
-      }
-    }
-
-    isfuzzy(
+    Assert.equal(
       panelTopLeftRelativeToAnchorTopLeft,
       0,
-      1,
       "Panel should be tightly anchored to the bottom of the button shadow node."
     );
   });
