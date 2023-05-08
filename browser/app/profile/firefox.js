f63f980c4347e755a7c91ead2d3db681fd9ca450
Drew Willcoxon <adw@mozilla.com>
2023-05-08 09:08:43 +0000

    Bug 1831689 - Add a pref for the weather suggestion to ignore VPNs and fetch anyway. r=daisuke a=pascalc
    Depends on D177218
    
    Differential Revision: https://phabricator.services.mozilla.com/D177339

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -410,6 +410,10 @@ pref("browser.urlbar.suggest.calculator",           false);
 // Feature gate pref for weather suggestions in the urlbar.
 pref("browser.urlbar.weather.featureGate", false);
 
+// When false, the weather suggestion will not be fetched when a VPN is
+// detected. When true, it will be fetched anyway.
+pref("browser.urlbar.weather.ignoreVPN", false);
+
 // The minimum prefix length of a weather keyword the user must type to trigger
 // the suggestion. 0 means the min length should be taken from Nimbus or remote
 // settings.
