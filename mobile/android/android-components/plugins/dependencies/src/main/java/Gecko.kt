cc0d13d7845c251e143a1534cc0d3a2004fd0b09
DonalMe <76789979+DonalMe@users.noreply.github.com>
2023-05-08 17:00:42 -0400

    Update GeckoView (Nightly) to 115.0.20230508173503

--- mobile/android/android-components/plugins/dependencies/src/main/java/Gecko.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "114.0.20230508094425"
+    const val version = "115.0.20230508173503"
 
     /**
      * GeckoView channel
