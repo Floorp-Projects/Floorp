d3d71398bcffd69a1c4fa560955ff362a477418f
Morgan Rae Reschenberg <mreschenberg@mozilla.com>
2023-05-08 20:51:12 +0000

    Bug 1828373: Create accs for elements with overflow:scroll and overflow:auto styling r=nlapre,Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D176202

--- accessible/tests/browser/tree/browser_general.js
+++ accessible/tests/browser/tree/browser_general.js
@@ -41,3 +41,88 @@ addAccessibleTask(`<p>hello world</p>`, async function(browser, docAcc) {
     DOCUMENT: [{ PARAGRAPH: [{ TEXT_LEAF: [] }] }, { TEXT_CONTAINER: [] }],
   });
 });
+
+/**
+ * Verify adding `overflow:scroll;` styling to a div causes
+ * it to  get an accessible.
+ */
+addAccessibleTask(`<p>hello world</p>`, async function(browser, docAcc) {
+  const originalTree = { DOCUMENT: [{ PARAGRAPH: [{ TEXT_LEAF: [] }] }] };
+
+  testAccessibleTree(docAcc, originalTree);
+  info("Adding div element");
+  await contentSpawnMutation(
+    browser,
+    { unexpected: [[EVENT_REORDER, docAcc]] },
+    function() {
+      const d = content.document.createElement("div");
+      content.document.body.appendChild(d);
+    }
+  );
+
+  testAccessibleTree(docAcc, originalTree);
+  info("Adding overflow:scroll styling to div");
+  await contentSpawnMutation(
+    browser,
+    { expected: [[EVENT_REORDER, docAcc]] },
+    function() {
+      content.document.body.lastElementChild.setAttribute(
+        "style",
+        "overflow:scroll;"
+      );
+    }
+  );
+
+  testAccessibleTree(docAcc, {
+    DOCUMENT: [{ PARAGRAPH: [{ TEXT_LEAF: [] }] }, { TEXT_CONTAINER: [] }],
+  });
+});
+
+/**
+ * Verify adding `overflow:auto;` styling to a div causes
+ * it to get an accessible, but `overflow:visible` does not.
+ */
+addAccessibleTask(`<p>hello world</p>`, async function(browser, docAcc) {
+  const originalTree = { DOCUMENT: [{ PARAGRAPH: [{ TEXT_LEAF: [] }] }] };
+
+  testAccessibleTree(docAcc, originalTree);
+  info("Adding div element");
+  await contentSpawnMutation(
+    browser,
+    { unexpected: [[EVENT_REORDER, docAcc]] },
+    function() {
+      const d = content.document.createElement("div");
+      content.document.body.appendChild(d);
+    }
+  );
+
+  testAccessibleTree(docAcc, originalTree);
+  info("Adding overflow:visible styling to div");
+  await contentSpawnMutation(
+    browser,
+    { unexpected: [[EVENT_REORDER, docAcc]] },
+    function() {
+      content.document.body.lastElementChild.setAttribute(
+        "style",
+        "overflow:visible;"
+      );
+    }
+  );
+
+  testAccessibleTree(docAcc, originalTree);
+  info("Adding overflow:auto styling to div");
+  await contentSpawnMutation(
+    browser,
+    { expected: [[EVENT_REORDER, docAcc]] },
+    function() {
+      content.document.body.lastElementChild.setAttribute(
+        "style",
+        "overflow:auto;"
+      );
+    }
+  );
+
+  testAccessibleTree(docAcc, {
+    DOCUMENT: [{ PARAGRAPH: [{ TEXT_LEAF: [] }] }, { TEXT_CONTAINER: [] }],
+  });
+});
