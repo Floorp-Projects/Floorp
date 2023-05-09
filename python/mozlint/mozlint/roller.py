6047186b420eff159b4ae329842548a8d0d5db6d
Mark Banner <standard8@mozilla.com>
2023-05-09 13:11:21 +0000

    Bug 1757373 - Enable linting to run in parallel when '.' is specified as the path. r=linter-reviewers,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D177260

--- python/mozlint/mozlint/roller.py
+++ python/mozlint/mozlint/roller.py
@@ -281,6 +281,11 @@ class LintRoller(object):
                     "warning: {} support-file modified, linting entire tree "
                     "(press ctrl-c to cancel)".format(linter["name"])
                 )
+            elif paths == {self.root}:
+                # If the command line is ".", the path will match with the root
+                # directory. In this case, get all the paths, so that we can
+                # benefit from chunking below.
+                lpaths = __get_current_paths()
             else:
                 lpaths = paths.union(vcs_paths)
 
