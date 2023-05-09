d03ae484847527553b3c0f68b4cfb761664d9ff9
Jan de Mooij <jdemooij@mozilla.com>
2023-05-09 14:40:12 +0000

    Bug 1830107 - Remove an invalid debug assertion. r=iain
    In debug builds, `LInt32ToIntPtr` checks that the int32 input isn't negative if
    that's what we determined earlier. However this check is invalid when we hoist the
    conversion before a bounds check because in this case the uses will never see a
    negative value but the conversion can still see one.
    
    It seems best to remove this check because if we have an `index >= INT32_MAX` we'd
    typically crash immediately anyway, and this makes the code for debug vs non-debug
    more similar.
    
    Differential Revision: https://phabricator.services.mozilla.com/D177153

--- js/src/jit/shared/Lowering-shared-inl.h
+++ js/src/jit/shared/Lowering-shared-inl.h
@@ -341,10 +341,13 @@ static inline bool IsCompatibleLIRCoercion(MIRType to, MIRType from) {
       (from == MIRType::Int32 || from == MIRType::Boolean)) {
     return true;
   }
-#  ifndef JS_64BIT
   // On 32-bit platforms Int32 can be redefined as IntPtr and vice versa.
-  if ((to == MIRType::Int32 || to == MIRType::IntPtr) &&
-      (from == MIRType::IntPtr || from == MIRType::Int32)) {
+  // On 64-bit platforms we can redefine non-negative Int32 values as IntPtr.
+  if (from == MIRType::Int32 && to == MIRType::IntPtr) {
+    return true;
+  }
+#  ifndef JS_64BIT
+  if (from == MIRType::IntPtr && to == MIRType::Int32) {
     return true;
   }
 #  endif
