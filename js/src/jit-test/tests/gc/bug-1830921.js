674d24b9aa463c609605b181a07cd2caaa5c1d60
Jon Coppeard <jcoppeard@mozilla.com>
2023-05-09 08:47:59 +0000

    Bug 1830921 - Discard JIT code when disabling or enabling the nursery r=jandem, a=dmeehan
    This makes it so we always discard JIT code when the kinds of GC thing we can
    allocate in a zone changes. Previously this happened in two separate places for
    pretenuring, and didn't happen when we enabled/disabled the nursery.
    
    Differential Revision: https://phabricator.services.mozilla.com/D177272

---
+++ js/src/jit-test/tests/gc/bug-1830921.js
@@ -0,0 +1,15 @@
+gczeal(4);
+x = [0, 0, 0, 0, 0];
+for (let i = 0; i < 5; ++i) {
+  for (let j = 0; j < x[i]; ++j) {}
+}
+gczeal(0);
+let y = [];
+for (let k = 0; k < 9999; ++k) {
+  try {
+    throw z;
+  } catch (e) {
+    y.push("" + e);
+  }
+}
+print(y);
