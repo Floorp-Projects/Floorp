ec72119001fe0fa6f8b2503a5ee226cdf13f7a1f
Cristian Tuns <ctuns@mozilla.com>
2023-05-10 14:49:41 -0400

    Backed out changeset 0054de12b39c (bug 1425310) while the issue is being investigated(Bug 1832361) CLOSED TREE

--- xpcom/ds/StaticAtoms.py
+++ xpcom/ds/StaticAtoms.py
@@ -679,7 +679,6 @@ STATIC_ATOMS = [
     Atom("mixed", "mixed"),
     Atom("messagemanagergroup", "messagemanagergroup"),
     Atom("mod", "mod"),
-    Atom("_module", "module"),
     Atom("mode", "mode"),
     Atom("modifiers", "modifiers"),
     Atom("monochrome", "monochrome"),
