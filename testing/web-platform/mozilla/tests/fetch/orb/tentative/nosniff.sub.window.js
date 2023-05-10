88f58cf5207105e5524d714474b63b7781c5db85
Andreas Farre <farre@mozilla.com>
2023-05-10 14:35:52 +0000

    Bug 1828375 - Do gradual ORB transition. r=sefeng,necko-reviewers
    Add a separate check for spec breaking allows of certain MIME
    types. Having this separated out means that we can make the rest of
    the implementation behave exactly like spec.
    
    Some tradeoffs that we need in the current state are:
    
    * Allowing "application/dash+xml"
    * Allowing "application/vnd.apple.mpegurl"
    * Allowing "text/vtt"
    * Allow all MIME types beginning with "audio/mpeg"
    * Allow "text/plain" when there is a no-sniff header.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176821

--- testing/web-platform/mozilla/tests/fetch/orb/tentative/nosniff.sub.window.js
+++ testing/web-platform/mozilla/tests/fetch/orb/tentative/nosniff.sub.window.js
@@ -3,6 +3,20 @@
 
 const path = "http://{{domains[www1]}}:{{ports[http][0]}}/fetch/orb/resources";
 
+// This is an exception that allow more MIME types than the ORB spec does.
+// This is due to web compatibility, but might be removed in the future.
+// See Bug 1828375
+promise_test(
+  async () =>
+    await fetchORB(
+      `${path}/text.txt`,
+      null,
+      contentType("text/plain"),
+      contentTypeOptions("nosniff")
+    ),
+  "ORB shouldn't block opaque text/plain with nosniff"
+);
+
 // Due to web compatibility we filter opaque Response object from the
 // fetch() function in the Fetch specification. See Bug 1823877. This
 // might be removed in the future.
