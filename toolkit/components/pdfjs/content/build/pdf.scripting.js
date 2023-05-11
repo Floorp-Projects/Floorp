596badfa0aca51a7a57932f1d1b0742df5a19360
Calixte <cdenizet@mozilla.com>
2023-05-11 11:43:25 +0000

    Bug 1832514 - Update PDF.js to new version d520754bcf32990259503682956bc24399c6bc76 r=pdfjs-reviewers,marco
    Differential Revision: https://phabricator.services.mozilla.com/D177736

--- toolkit/components/pdfjs/content/build/pdf.scripting.js
+++ toolkit/components/pdfjs/content/build/pdf.scripting.js
@@ -4258,8 +4258,8 @@ Object.defineProperty(exports, "initSandbox", ({
   }
 }));
 var _initialization = __w_pdfjs_require__(1);
-const pdfjsVersion = '3.7.11';
-const pdfjsBuild = '8f2d8f62f';
+const pdfjsVersion = '3.7.17';
+const pdfjsBuild = 'd520754bc';
 })();
 
 /******/ 	return __webpack_exports__;
