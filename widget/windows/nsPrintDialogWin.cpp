6d8f0705fd08bb59d8f135634ce28094d5b8e72c
Ray Kraesig <rkraesig@mozilla.com>
2023-05-11 01:13:05 +0000

    Bug 1832168 - [1/3] Remove print dialog's handling of embedded Gecko windows  r=emilio
    Simplify `GetHWNDForDOMWindow` significantly:
    
    0. Remove a 21-year-old code path [*] apparently intended to handle the
       case of an embedded Gecko window.
    
       We can safely infer that this code path has been unused for at least
       the last 13 years, when the patchset for bug 588735 introduced an
       unconditional `::DestroyWindow` of this function's return value.
       (This would have been the embedded window in question, if this code
       path had ever been taken.)
    
    1. Reduce remaining code to convert an `mozIDOMWindowProxy` to an
       `nsIWidget` via the presumably-better-tested mechanism used by the
       color picker and file picker.
    
    [*] Note that this code was moved in 2017 in a way that prevents
    Searchfox from following it back. For archaeological purposes, a link to
    its position at that time follows:
    
    https://searchfox.org/mozilla-central/rev/bcd8f8c970f88a51744e70d19987d76309eb2b02/toolkit/components/printingui/win/nsPrintingPromptService.cpp
    
    Differential Revision: https://phabricator.services.mozilla.com/D177566

--- widget/windows/nsPrintDialogWin.cpp
+++ widget/windows/nsPrintDialogWin.cpp
@@ -22,6 +22,8 @@
 #include "nsServiceManagerUtils.h"
 #include "nsPIDOMWindow.h"
 #include "nsQueryObject.h"
+#include "WidgetUtils.h"
+#include "WinUtils.h"
 
 static const char* kPageSetupDialogURL =
     "chrome://global/content/printPageSetup.xhtml";
@@ -142,40 +144,8 @@ nsresult nsPrintDialogServiceWin::DoDialog(mozIDOMWindowProxy* aParent,
 }
 
 HWND nsPrintDialogServiceWin::GetHWNDForDOMWindow(mozIDOMWindowProxy* aWindow) {
-  nsCOMPtr<nsIWebBrowserChrome> chrome;
-
-  // We might be embedded so check this path first
-  if (mWatcher) {
-    nsCOMPtr<mozIDOMWindowProxy> fosterParent;
-    // it will be a dependent window. try to find a foster parent.
-    if (!aWindow) {
-      mWatcher->GetActiveWindow(getter_AddRefs(fosterParent));
-      aWindow = fosterParent;
-    }
-    mWatcher->GetChromeForWindow(aWindow, getter_AddRefs(chrome));
-  }
-
-  if (chrome) {
-    nsCOMPtr<nsIBaseWindow> site(do_QueryInterface(chrome));
-    if (site) {
-      HWND w;
-      site->GetParentNativeWindow(reinterpret_cast<void**>(&w));
-      return w;
-    }
-  }
-
-  // Now we might be the Browser so check this path
-  nsCOMPtr<nsPIDOMWindowOuter> window = nsPIDOMWindowOuter::From(aWindow);
-
-  nsCOMPtr<nsIWebBrowserChrome> webBrowserChrome =
-      window->GetWebBrowserChrome();
-  if (!webBrowserChrome) return nullptr;
-
-  nsCOMPtr<nsIBaseWindow> baseWin(do_QueryInterface(webBrowserChrome));
-  if (!baseWin) return nullptr;
-
-  nsCOMPtr<nsIWidget> widget;
-  baseWin->GetMainWidget(getter_AddRefs(widget));
+  nsCOMPtr<nsIWidget> widget =
+      WidgetUtils::DOMWindowToWidget(nsPIDOMWindowOuter::From(aWindow));
   if (!widget) return nullptr;
 
   return (HWND)widget->GetNativeData(NS_NATIVE_TMP_WINDOW);
