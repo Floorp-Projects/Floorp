41bad247554244960540c2868a0095a04130e330
Mike Kaply <mozilla@kaply.com>
2023-05-12 18:39:12 +0000

    Bug 1824438 - Implement a module system for Firefox View. r=kcochrane,jsudiaman,fxview-reviewers,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D175794

---
+++ browser/components/firefoxview/history.mjs
@@ -0,0 +1,41 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { html } from "chrome://global/content/vendor/lit.all.mjs";
+import { ViewPage } from "./viewpage.mjs";
+
+class HistoryInView extends ViewPage {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  disconnectedCallback() {}
+
+  render() {
+    if (!this.selectedTab && !this.overview) {
+      return null;
+    }
+    let numRows = this.overview ? 5 : 10;
+    const itemTemplates = [];
+
+    for (let i = 1; i <= numRows; i++) {
+      itemTemplates.push(
+        html`
+          <p>History Row ${i}</p>
+        `
+      );
+    }
+
+    return html`
+      <ul>
+        ${itemTemplates}
+      </ul>
+    `;
+  }
+}
+customElements.define("view-history", HistoryInView);
