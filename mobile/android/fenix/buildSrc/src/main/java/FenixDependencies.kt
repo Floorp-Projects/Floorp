890f40b1ee1d0a3af956c2bc0ec4459e883f7864
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-05-13 21:50:22 -0400

    Bug 1833292 - Update Google Accompanist to version 0.30.1

--- mobile/android/fenix/buildSrc/src/main/java/FenixDependencies.kt
+++ mobile/android/fenix/buildSrc/src/main/java/FenixDependencies.kt
@@ -42,7 +42,7 @@ object FenixVersions {
     const val androidx_datastore = "1.0.0"
     const val androidx_datastore_preferences = "1.0.0"
     const val google_material = "1.9.0"
-    const val google_accompanist = "0.28.0"
+    const val google_accompanist = "0.30.1"
 
     const val adjust = "4.33.0"
     const val installreferrer = "2.2"
