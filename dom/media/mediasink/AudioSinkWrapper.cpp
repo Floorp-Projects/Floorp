9456afc8c2268bc2c952a876a21c109a45f08f3f
alwu <alwu@mozilla.com>
2023-05-15 16:49:14 +0000

    Bug 1829086 - part6 : check audio end promise even if there is no audio sample in the audio queue. r=padenot
    If the playback starts from the position where the audio track has ended
    but the video track is not, then there won't be any data in the audio
    queue. Therefore, we should check the end promise for the muted audio
    track no matter the audio queue is empty or not.
    
    Differential Revision: https://phabricator.services.mozilla.com/D177441

--- dom/media/mediasink/AudioSinkWrapper.cpp
+++ dom/media/mediasink/AudioSinkWrapper.cpp
@@ -98,11 +98,13 @@ TimeUnit AudioSinkWrapper::GetPosition(TimeStamp* aTimeStamp) {
     pos = GetSystemClockPosition(t);
     LOGV("%p: Getting position from the system clock %lf", this,
          pos.ToSeconds());
-    if (mAudioQueue.GetSize() > 0 && IsMuted()) {
-      // audio track, but it's muted and won't be dequeued, discard packets that
-      // are behind the current media time, to keep the queue size under
-      // control.
-      DropAudioPacketsIfNeeded(pos);
+    if (IsMuted()) {
+      if (mAudioQueue.GetSize() > 0) {
+        // audio track, but it's muted and won't be dequeued, discard packets
+        // that are behind the current media time, to keep the queue size under
+        // control.
+        DropAudioPacketsIfNeeded(pos);
+      }
       // If muted, it's necessary to manually check if the audio has "ended",
       // meaning that all the audio packets have been consumed, to resolve the
       // ended promise.
