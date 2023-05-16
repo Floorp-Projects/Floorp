098652d7ecb8076452fcc153f729a3c9e5c13d98
Jari Jalkanen <jjalkanen@mozilla.com>
2023-05-16 16:07:16 +0000

    Bug 1826780 - Introduce content type guessing function. r=dom-storage-reviewers,janv
    Differential Revision: https://phabricator.services.mozilla.com/D177014

---
+++ dom/fs/parent/FileSystemContentTypeGuess.h
@@ -0,0 +1,21 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef DOM_FS_PARENT_FILESYSTEMCONTENTTYPEGUESS_H_
+#define DOM_FS_PARENT_FILESYSTEMCONTENTTYPEGUESS_H_
+
+#include "mozilla/dom/FileSystemTypes.h"
+#include "nsStringFwd.h"
+
+namespace mozilla::dom::fs {
+
+struct FileSystemContentTypeGuess {
+  static ContentType FromPath(const Name& aPath);
+};
+
+}  // namespace mozilla::dom::fs
+
+#endif  // DOM_FS_PARENT_FILESYSTEMCONTENTTYPEGUESS_H_
