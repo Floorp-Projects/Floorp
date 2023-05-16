336416bd8c63efab57005221907c5aad7c5883f4
Jan-Ivar Bruaroey <jib@mozilla.com>
2023-05-16 03:55:45 +0000

    Bug 1528042 - Fix navigator.mozGetUserMedia to also trigger device exposure. r=karlt
    Differential Revision: https://phabricator.services.mozilla.com/D154175

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -1321,11 +1321,15 @@ void Navigator::MozGetUserMedia(const MediaStreamConstraints& aConstraints,
                                 NavigatorUserMediaErrorCallback& aOnError,
                                 CallerType aCallerType, ErrorResult& aRv) {
   MOZ_ASSERT(NS_IsMainThread());
-
   if (!mWindow || !mWindow->IsFullyActive()) {
     aRv.ThrowInvalidStateError("The document is not fully active.");
     return;
   }
+  GetMediaDevices(aRv);
+  if (aRv.Failed()) {
+    return;
+  }
+  MOZ_ASSERT(mMediaDevices);
   if (Document* doc = mWindow->GetExtantDoc()) {
     if (!mWindow->IsSecureContext()) {
       doc->SetUseCounter(eUseCounter_custom_MozGetUserMediaInsec);
@@ -1339,7 +1343,7 @@ void Navigator::MozGetUserMedia(const MediaStreamConstraints& aConstraints,
                                   "audio and/or video is required"),
         __func__);
   } else {
-    sp = MediaManager::Get()->GetUserMedia(mWindow, aConstraints, aCallerType);
+    sp = mMediaDevices->GetUserMedia(mWindow, aConstraints, aCallerType);
   }
   RefPtr<NavigatorUserMediaSuccessCallback> onsuccess(&aOnSuccess);
   RefPtr<NavigatorUserMediaErrorCallback> onerror(&aOnError);
