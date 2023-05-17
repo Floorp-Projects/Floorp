339328dd61c0f88bfb75bade6c1bec42a334f24a
Paul Adenot <paul@paul.cx>
2023-05-17 15:47:05 +0000

    Bug 1817997 - Reduce precision when storing time related to the played attribute. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D173528

--- dom/media/VideoUtils.h
+++ dom/media/VideoUtils.h
@@ -109,6 +109,7 @@ class MediaResource;
 media::TimeIntervals GetEstimatedBufferedTimeRanges(
     mozilla::MediaResource* aStream, int64_t aDurationUsecs);
 
+double ToMicrosecondResolution(double aSeconds);
 // Converts from number of audio frames (aFrames) to microseconds, given
 // the specified audio rate (aRate).
 CheckedInt64 FramesToUsecs(int64_t aFrames, uint32_t aRate);
