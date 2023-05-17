420be58a4c7df0a32d9f5bb22141df7a40bbf266
Paul Adenot <paul@paul.cx>
2023-05-17 15:46:57 +0000

    Bug 1703812 - Part 11 - Fix static-analysis issues in android/RemoteDataDecoder.cpp. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D167680

--- dom/media/platforms/android/AndroidDecoderModule.h
+++ dom/media/platforms/android/AndroidDecoderModule.h
@@ -53,7 +53,7 @@ class AndroidDecoderModule : public PlatformDecoderModule {
 
 extern LazyLogModule sAndroidDecoderModuleLog;
 
-const nsCString TranslateMimeType(const nsACString& aMimeType);
+nsCString TranslateMimeType(const nsACString& aMimeType);
 
 }  // namespace mozilla
 
