42aded9773c4868d85ef6be5384543f2fea11308
Dimi <dlee@mozilla.com>
2023-05-17 08:44:39 +0000

    Bug 1820522 - Implement heuristics to determine whether two city names are the same, different, or mergeable r=mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D173806

---
+++ browser/extensions/formautofill/test/unit/test_addressComponent_city.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const VALID_TESTS = [["New York City", true]];
+
+const COMPARE_TESTS = [
+  ["New York City", "New York City", SAME],
+  ["New York City", "new york city", SAME],
+  ["New York City", "New York  City", SIMILAR], // Merge whitespace
+  ["Happy Valley-Goose Bay", "Happy Valley Goose Bay", SIMILAR], // Replace punctuation with whitespace
+  ["New York City", "New York", A_CONTAINS_B],
+  ["New York", "NewYork", DIFFERENT],
+  ["New York City", "City New York", DIFFERENT],
+];
+
+const TEST_FIELD_NAME = "City";
+
+add_task(async function test_isValid() {
+  runIsValidTest(VALID_TESTS, TEST_FIELD_NAME, value => {
+    return { "address-level2": value };
+  });
+});
+
+add_task(async function test_compare() {
+  runCompareTest(COMPARE_TESTS, TEST_FIELD_NAME, value => {
+    return { "address-level2": value };
+  });
+});
