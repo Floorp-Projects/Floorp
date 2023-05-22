e220f6b35ece3909a6e8b94828abdfb9d470630d
mcarare <48995920+mcarare@users.noreply.github.com>
2023-05-22 13:37:54 +0300

    Bug 1831352 - Install Java 17.

--- mobile/android/taskcluster/scripts/toolchain/repack-android-sdk-linux.sh
+++ mobile/android/taskcluster/scripts/toolchain/repack-android-sdk-linux.sh
@@ -2,11 +2,11 @@
 
 export ANDROID_SDK_ROOT=$MOZ_FETCHES_DIR
 
-JAVA11PATH="/usr/lib/jvm/java-11-openjdk-amd64/bin/:$PATH"
+JAVA17PATH="/usr/lib/jvm/java-17-openjdk-amd64/bin/:$PATH"
 
-yes | PATH=$JAVA11PATH "${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager" --licenses --sdk_root="${ANDROID_SDK_ROOT}"
+yes | PATH=$JAVA17PATH "${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager" --licenses --sdk_root="${ANDROID_SDK_ROOT}"
 
 # It's nice to have the build logs include the state of the world upon completion.
-PATH=$JAVA11PATH "${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager" --list --sdk_root="${ANDROID_SDK_ROOT}"
+PATH=$JAVA17PATH "${ANDROID_SDK_ROOT}/cmdline-tools/bin/sdkmanager" --list --sdk_root="${ANDROID_SDK_ROOT}"
 
 tar cf - -C "$ANDROID_SDK_ROOT" . --transform 's,^\./,android-sdk-linux/,' | xz > "$UPLOAD_DIR/android-sdk-linux.tar.xz"
