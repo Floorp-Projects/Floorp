6bbc8e8bcb0c4571a8b3a7f8dfb30fb223926212
David Turner <david.turner@raspberrypi.com>
2023-05-22 15:38:43 +0000

    Bug 1833354 - Extend FFmpeg video frame pool to support V4L2. r=stransky,media-playback-reviewers
    Currently the video frame pool used to manage hardware-accelerated
    buffers for FFmpeg only supports VAAPI.  Extend it to support V4L2 by
    adding a new overload of GetVideoFrameSurface which takes an
    AVDRMFrameDescriptor (which wraps a DRM-PRIME buffer handle).  The
    AVDRMFrameDescriptor is converted to a VADRMPRIMESurfaceDescriptor so we
    can use the existing machinery in DMABufSurface and avoid duplication
    there.
    
    This depends on the drm_fourcc.h header from libdrm, I use the vendored
    version of libdrm instead of adding a system dependency.
    
    Differential Revision: https://phabricator.services.mozilla.com/D178349

--- dom/media/platforms/ffmpeg/FFmpegVideoFramePool.cpp
+++ dom/media/platforms/ffmpeg/FFmpegVideoFramePool.cpp
@@ -11,6 +11,14 @@
 #include "libavutil/pixfmt.h"
 #include "mozilla/StaticPrefs_media.h"
 #include "mozilla/gfx/gfxVars.h"
+#include "mozilla/widget/va_drmcommon.h"
+
+// DMABufLibWrapper defines its own version of this which collides with the
+// official version in drm_fourcc.h
+#ifdef DRM_FORMAT_MOD_INVALID
+#  undef DRM_FORMAT_MOD_INVALID
+#endif
+#include "drm_fourcc.h"
 
 #ifdef MOZ_LOGGING
 #  undef DMABUF_LOG
@@ -57,14 +65,26 @@ VideoFrameSurface<LIBAV_VER>::~VideoFrameSurface() {
 void VideoFrameSurface<LIBAV_VER>::LockVAAPIData(
     AVCodecContext* aAVCodecContext, AVFrame* aAVFrame,
     FFmpegLibWrapper* aLib) {
-  MOZ_DIAGNOSTIC_ASSERT(aAVCodecContext->hw_frames_ctx);
   mLib = aLib;
-  mAVHWFrameContext = aLib->av_buffer_ref(aAVCodecContext->hw_frames_ctx);
-  mHWAVBuffer = aLib->av_buffer_ref(aAVFrame->buf[0]);
-  DMABUF_LOG(
-      "VideoFrameSurface: VAAPI locking dmabuf surface UID %d FFMPEG ID 0x%x "
-      "mAVHWFrameContext %p mHWAVBuffer %p",
-      mSurface->GetUID(), mFFMPEGSurfaceID, mAVHWFrameContext, mHWAVBuffer);
+
+  // V4L2 frames don't have hw_frames_ctx because the v4l2-wrapper codecs
+  // don't actually use hwaccel.  In this case we don't need to add a
+  // HW frame context reference
+  if (aAVCodecContext->hw_frames_ctx) {
+    mAVHWFrameContext = aLib->av_buffer_ref(aAVCodecContext->hw_frames_ctx);
+    mHWAVBuffer = aLib->av_buffer_ref(aAVFrame->buf[0]);
+    DMABUF_LOG(
+        "VideoFrameSurface: VAAPI locking dmabuf surface UID %d FFMPEG ID 0x%x "
+        "mAVHWFrameContext %p mHWAVBuffer %p",
+        mSurface->GetUID(), mFFMPEGSurfaceID, mAVHWFrameContext, mHWAVBuffer);
+  } else {
+    mAVHWFrameContext = nullptr;
+    mHWAVBuffer = aLib->av_buffer_ref(aAVFrame->buf[0]);
+    DMABUF_LOG(
+        "VideoFrameSurface: V4L2 locking dmabuf surface UID %d FFMPEG ID 0x%x "
+        "mHWAVBuffer %p",
+        mSurface->GetUID(), mFFMPEGSurfaceID, mHWAVBuffer);
+  }
 }
 
 void VideoFrameSurface<LIBAV_VER>::ReleaseVAAPIData(bool aForFrameRecycle) {
@@ -81,7 +101,9 @@ void VideoFrameSurface<LIBAV_VER>::ReleaseVAAPIData(bool aForFrameRecycle) {
   // is closed.
   if (mLib) {
     mLib->av_buffer_unref(&mHWAVBuffer);
-    mLib->av_buffer_unref(&mAVHWFrameContext);
+    if (mAVHWFrameContext) {
+      mLib->av_buffer_unref(&mAVHWFrameContext);
+    }
     mLib = nullptr;
   }
 
@@ -206,6 +228,141 @@ VideoFramePool<LIBAV_VER>::GetVideoFrameSurface(
     DMABUF_LOG("  DMABuf texture copy is broken");
     copySurface = mTextureCopyWorks = false;
     if (!surface->UpdateYUVData(aVaDesc, aWidth, aHeight, copySurface)) {
+      return nullptr;
+    }
+  }
+
+  if (MOZ_UNLIKELY(!mTextureCreationWorks)) {
+    mTextureCreationWorks = Some(surface->VerifyTextureCreation());
+    if (!*mTextureCreationWorks) {
+      DMABUF_LOG("  failed to create texture over DMABuf memory!");
+      return nullptr;
+    }
+  }
+
+  videoSurface->MarkAsUsed(ffmpegSurfaceID);
+
+  if (!copySurface) {
+    // Check that newly added ffmpeg surface isn't already used by different
+    // VideoFrameSurface.
+    CheckNewFFMPEGSurface(ffmpegSurfaceID);
+    videoSurface->LockVAAPIData(aAVCodecContext, aAVFrame, aLib);
+  }
+  return videoSurface;
+}
+
+// Convert an FFmpeg-specific DRM descriptor into a
+// VADRMPRIMESurfaceDescriptor.  There is no fundamental difference between
+// the descriptor structs and using the latter means this can use all the
+// existing machinery in DMABufSurfaceYUV.
+static Maybe<VADRMPRIMESurfaceDescriptor> FFmpegDescToVA(
+    AVDRMFrameDescriptor& aDesc, AVFrame* aAVFrame) {
+  VADRMPRIMESurfaceDescriptor vaDesc{};
+
+  if (aAVFrame->format != AV_PIX_FMT_DRM_PRIME) {
+    DMABUF_LOG("Got non-DRM-PRIME frame from FFmpeg V4L2");
+    return Nothing();
+  }
+
+  if (aDesc.layers[0].format == DRM_FORMAT_YUV420) {
+    vaDesc.fourcc = VA_FOURCC_YV12;
+
+    if (aAVFrame->crop_top != 0 || aAVFrame->crop_left != 0) {
+      DMABUF_LOG("Top and left-side cropping are not supported");
+      return Nothing();
+    }
+
+    // Width and height after crop
+    vaDesc.width = aAVFrame->width;
+    vaDesc.height = aAVFrame->height - aAVFrame->crop_bottom;
+
+    // V4L2 expresses YUV420 as a single contiguous buffer containing
+    // all three planes.  DMABufSurfaceYUV expects the three planes
+    // separately, so we have to split them out
+    MOZ_ASSERT(aDesc.nb_objects == 1);
+    MOZ_ASSERT(aDesc.nb_layers == 1);
+
+    vaDesc.num_objects = 1;
+    vaDesc.objects[0].drm_format_modifier = aDesc.objects[0].format_modifier;
+    vaDesc.objects[0].size = aDesc.objects[0].size;
+    vaDesc.objects[0].fd = aDesc.objects[0].fd;
+
+    vaDesc.num_layers = 3;
+    for (int i = 0; i < 3; i++) {
+      vaDesc.layers[i].drm_format = DRM_FORMAT_R8;
+      vaDesc.layers[i].num_planes = 1;
+      vaDesc.layers[i].object_index[0] = 0;
+    }
+
+    // Native width and height before crop is applied
+    unsigned int uncrop_width = aDesc.layers[0].planes[0].pitch;
+    unsigned int uncrop_height = aAVFrame->height;
+
+    // Offset of the start of YUV420 data
+    unsigned int offset = aDesc.layers[0].planes[0].offset;
+
+    vaDesc.layers[0].offset[0] = offset;
+    vaDesc.layers[0].pitch[0] = uncrop_width;
+    vaDesc.layers[1].offset[0] = offset + uncrop_width * uncrop_height;
+    vaDesc.layers[1].pitch[0] = uncrop_width / 2;
+    vaDesc.layers[2].offset[0] = offset + uncrop_width * uncrop_height * 5 / 4;
+    vaDesc.layers[2].pitch[0] = uncrop_width / 2;
+  } else {
+    DMABUF_LOG("Don't know how to deal with FOURCC 0x%x",
+               aDesc.layers[0].format);
+    return Nothing();
+  }
+
+  return Some(vaDesc);
+}
+
+RefPtr<VideoFrameSurface<LIBAV_VER>>
+VideoFramePool<LIBAV_VER>::GetVideoFrameSurface(AVDRMFrameDescriptor& aDesc,
+                                                int aWidth, int aHeight,
+                                                AVCodecContext* aAVCodecContext,
+                                                AVFrame* aAVFrame,
+                                                FFmpegLibWrapper* aLib) {
+  MOZ_ASSERT(aDesc.nb_layers > 0);
+
+  auto layerDesc = FFmpegDescToVA(aDesc, aAVFrame);
+  if (layerDesc.isNothing()) {
+    return nullptr;
+  }
+
+  // Width and height, after cropping
+  int crop_width = (int)layerDesc->width;
+  int crop_height = (int)layerDesc->height;
+
+  // Use the descriptor's address as an id to track ffmpeg surfaces
+  unsigned int ffmpegSurfaceID = (uintptr_t)&aDesc;
+
+  MutexAutoLock lock(mSurfaceLock);
+
+  RefPtr<DMABufSurfaceYUV> surface;
+  RefPtr<VideoFrameSurface<LIBAV_VER>> videoSurface =
+      GetFreeVideoFrameSurface();
+  if (!videoSurface) {
+    surface = new DMABufSurfaceYUV();
+    videoSurface = new VideoFrameSurface<LIBAV_VER>(surface);
+    mDMABufSurfaces.AppendElement(videoSurface);
+  } else {
+    surface = videoSurface->GetDMABufSurface();
+  }
+  DMABUF_LOG("Using V4L2 DMABufSurface UID %d FFMPEG ID 0x%x",
+             surface->GetUID(), ffmpegSurfaceID);
+
+  bool copySurface = mTextureCopyWorks && ShouldCopySurface();
+  if (!surface->UpdateYUVData(layerDesc.value(), crop_width, crop_height,
+                              copySurface)) {
+    if (!copySurface) {
+      // Failed without texture copy. We can't do more here.
+      return nullptr;
+    }
+    // Try again without texture copy
+    DMABUF_LOG("  DMABuf texture copy is broken");
+    copySurface = mTextureCopyWorks = false;
+    if (!surface->UpdateYUVData(layerDesc.value(), crop_width, crop_height,
+                                copySurface)) {
       return nullptr;
     }
   }
