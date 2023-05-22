b7bfc01095416fa80c211a7f42518f3a16433cfd
Erik Nordin <enordin@mozilla.com>
2023-05-22 17:43:10 +0000

    Bug 1829691 - Add Translations settings panel to about:preferences r=gregtatum,fluent-reviewers,settings-reviewers,Gijs,flod
    Adds a new button and panel for managing Translations
    settings to the about:preferences panel.
    
    Manages always-translate languages, never-translate
    languages, and never-translate sites.
    
    Depends on D178228
    
    Differential Revision: https://phabricator.services.mozilla.com/D177854

--- browser/components/preferences/dialogs/translations.js
+++ browser/components/preferences/dialogs/translations.js
@@ -5,8 +5,20 @@
 
 "use strict";
 
-const kPermissionType = "translate";
-const kLanguagesPref = "browser.translation.neverForLanguages";
+/**
+ * The permission type to give to Services.perms for Translations.
+ */
+const TRANSLATIONS_PERMISSION = "translations";
+/**
+ * The list of BCP-47 language tags that will trigger auto-translate.
+ */
+const ALWAYS_TRANSLATE_LANGS_PREF =
+  "browser.translations.alwaysTranslateLanguages";
+/**
+ * The list of BCP-47 language tags that will prevent showing Translations UI.
+ */
+const NEVER_TRANSLATE_LANGS_PREF =
+  "browser.translations.neverTranslateLanguages";
 
 function Tree(aId, aData) {
   this._data = aData;
@@ -83,101 +95,207 @@ Lang.prototype = {
   },
 };
 
-var gTranslationExceptions = {
+var gTranslationsSettings = {
   onLoad() {
-    if (this._siteTree) {
+    if (this._neverTranslateSiteTree) {
       // Re-using an open dialog, clear the old observers.
-      this.uninit();
+      this.removeObservers();
     }
 
     // Load site permissions into an array.
-    this._sites = [];
-    for (let perm of Services.perms.all) {
-      if (
-        perm.type == kPermissionType &&
-        perm.capability == Services.perms.DENY_ACTION
-      ) {
-        this._sites.push(perm.principal.origin);
+    this._neverTranslateSites = [];
+    for (const perm of Services.perms.getAllByTypes([
+      TRANSLATIONS_PERMISSION,
+    ])) {
+      if (perm.capability === Services.perms.DENY_ACTION) {
+        this._neverTranslateSites.push(perm.principal.origin);
       }
     }
+    let stripProtocol = s => s?.replace(/^\w+:/, "") || "";
+    this._neverTranslateSites.sort((a, b) => {
+      return stripProtocol(a).localeCompare(stripProtocol(b));
+    });
+
+    // Load language tags into arrays.
+    this._alwaysTranslateLangs = this.getAlwaysTranslateLanguages();
+    this._neverTranslateLangs = this.getNeverTranslateLanguages();
+
+    // Add observers for relevant prefs and permissions.
     Services.obs.addObserver(this, "perm-changed");
-    this._sites.sort();
+    Services.prefs.addObserver(ALWAYS_TRANSLATE_LANGS_PREF, this);
+    Services.prefs.addObserver(NEVER_TRANSLATE_LANGS_PREF, this);
 
-    this._siteTree = new Tree("sitesTree", this._sites);
-    this.onSiteSelected();
+    // Build trees from the arrays.
+    this._alwaysTranslateLangsTree = new Tree(
+      "alwaysTranslateLanguagesTree",
+      this._alwaysTranslateLangs
+    );
+    this._neverTranslateLangsTree = new Tree(
+      "neverTranslateLanguagesTree",
+      this._neverTranslateLangs
+    );
+    this._neverTranslateSiteTree = new Tree(
+      "neverTranslateSitesTree",
+      this._neverTranslateSites
+    );
 
-    this._langs = this.getLanguageExceptions();
-    Services.prefs.addObserver(kLanguagesPref, this);
-    this._langTree = new Tree("languagesTree", this._langs);
-    this.onLanguageSelected();
+    // Ensure the UI for each group is in the correct state.
+    this.onSelectAlwaysTranslateLanguage();
+    this.onSelectNeverTranslateLanguage();
+    this.onSelectNeverTranslateSite();
   },
 
-  // Get the list of languages we don't translate as an array.
-  getLanguageExceptions() {
-    let langs = Services.prefs.getCharPref(kLanguagesPref);
-    if (!langs) {
+  /**
+   * Retrieves the value of a char-pref splits its value into an
+   * array delimited by commas.
+   *
+   * This is used for the translations preferences which are comma-
+   * separated lists of BCP-47 language tags.
+   *
+   * @param {string} pref
+   * @returns {Array<string>}
+   */
+  getLangsFromPref(pref) {
+    let rawLangs = Services.prefs.getCharPref(pref);
+    if (!rawLangs) {
       return [];
     }
 
-    let langArr = langs.split(",");
+    let langArr = rawLangs.split(",");
     let displayNames = Services.intl.getLanguageDisplayNames(
       undefined,
       langArr
     );
-    let result = langArr.map((lang, i) => new Lang(lang, displayNames[i]));
-    result.sort();
+    let langs = langArr.map((lang, i) => new Lang(lang, displayNames[i]));
+    langs.sort();
 
-    return result;
+    return langs;
   },
 
+  /**
+   * Retrieves the always-translate language tags as an array.
+   * @returns {Array<string>}
+   */
+  getAlwaysTranslateLanguages() {
+    return this.getLangsFromPref(ALWAYS_TRANSLATE_LANGS_PREF);
+  },
+
+  /**
+   * Retrieves the never-translate language tags as an array.
+   * @returns {Array<string>}
+   */
+  getNeverTranslateLanguages() {
+    return this.getLangsFromPref(NEVER_TRANSLATE_LANGS_PREF);
+  },
+
+  /**
+   * Handles updating the UI components on pref or permission changes.
+   */
   observe(aSubject, aTopic, aData) {
-    if (aTopic == "perm-changed") {
-      if (aData == "cleared") {
-        if (!this._sites.length) {
+    if (aTopic === "perm-changed") {
+      if (aData === "cleared") {
+        // Permissions have been cleared
+        if (!this._neverTranslateSites.length) {
+          // There were no sites with permissions set, nothing to do.
           return;
         }
-        let removed = this._sites.splice(0, this._sites.length);
-        this._siteTree.tree.rowCountChanged(0, -removed.length);
+        // Update the tree based on the amount of permissions removed.
+        let removed = this._neverTranslateSites.splice(
+          0,
+          this._neverTranslateSites.length
+        );
+        this._neverTranslateSiteTree.tree.rowCountChanged(0, -removed.length);
       } else {
         let perm = aSubject.QueryInterface(Ci.nsIPermission);
-        if (perm.type != kPermissionType) {
+        if (perm.type != TRANSLATIONS_PERMISSION) {
+          // The updated permission was not for Translations, nothing to do.
           return;
         }
-
-        if (aData == "added") {
+        if (aData === "added") {
           if (perm.capability != Services.perms.DENY_ACTION) {
+            // We are only showing data for sites we should never translate.
+            // If the permission is not DENY_ACTION, we don't care about it here.
             return;
           }
-          this._sites.push(perm.principal.origin);
-          this._sites.sort();
-          let tree = this._siteTree.tree;
+          this._neverTranslateSites.push(perm.principal.origin);
+          this._neverTranslateSites.sort();
+          let tree = this._neverTranslateSiteTree.tree;
           tree.rowCountChanged(0, 1);
           tree.invalidate();
         } else if (aData == "deleted") {
-          let index = this._sites.indexOf(perm.principal.origin);
+          let index = this._neverTranslateSites.indexOf(perm.principal.origin);
           if (index == -1) {
+            // The deleted permission was not in the tree, nothing to do.
             return;
           }
-          this._sites.splice(index, 1);
-          this._siteTree.tree.rowCountChanged(index, -1);
-          this.onSiteSelected();
-          return;
+          this._neverTranslateSites.splice(index, 1);
+          this._neverTranslateSiteTree.tree.rowCountChanged(index, -1);
         }
       }
-      this.onSiteSelected();
-    } else if (aTopic == "nsPref:changed") {
-      this._langs = this.getLanguageExceptions();
-      let change = this._langs.length - this._langTree.rowCount;
-      this._langTree._data = this._langs;
-      let tree = this._langTree.tree;
-      if (change) {
-        tree.rowCountChanged(0, change);
+      // Ensure the UI updates to the changes.
+      this.onSelectNeverTranslateSite();
+    } else if (aTopic === "nsPref:changed") {
+      switch (aData) {
+        case ALWAYS_TRANSLATE_LANGS_PREF: {
+          this._alwaysTranslateLangs = this.getAlwaysTranslateLanguages();
+
+          let alwaysTranslateLangsChange =
+            this._alwaysTranslateLangs.length -
+            this._alwaysTranslateLangsTree.rowCount;
+
+          this._alwaysTranslateLangsTree._data = this._alwaysTranslateLangs;
+          let alwaysTranslateLangsTree = this._alwaysTranslateLangsTree.tree;
+
+          if (alwaysTranslateLangsChange) {
+            alwaysTranslateLangsTree.rowCountChanged(
+              0,
+              alwaysTranslateLangsChange
+            );
+          }
+
+          alwaysTranslateLangsTree.invalidate();
+
+          // Ensure the UI updates to the changes.
+          this.onSelectAlwaysTranslateLanguage();
+          break;
+        }
+        case NEVER_TRANSLATE_LANGS_PREF: {
+          this._neverTranslateLangs = this.getNeverTranslateLanguages();
+
+          let neverTranslateLangsChange =
+            this._neverTranslateLangs.length -
+            this._neverTranslateLangsTree.rowCount;
+
+          this._neverTranslateLangsTree._data = this._neverTranslateLangs;
+          let neverTranslateLangsTree = this._neverTranslateLangsTree.tree;
+
+          if (neverTranslateLangsChange) {
+            neverTranslateLangsTree.rowCountChanged(
+              0,
+              neverTranslateLangsChange
+            );
+          }
+
+          neverTranslateLangsTree.invalidate();
+
+          // Ensure the UI updates to the changes.
+          this.onSelectNeverTranslateLanguage();
+          break;
+        }
       }
-      tree.invalidate();
-      this.onLanguageSelected();
     }
   },
 
+  /**
+   * Ensures that buttons states are enabled/disabled accordingly based on the
+   * content of the trees.
+   *
+   * The remove button should be enabled only if an item is selected.
+   * The removeAll button should be enabled any time the tree has content.
+   *
+   * @param {Tree} aTree
+   * @param {string} aIdPart
+   */
   _handleButtonDisabling(aTree, aIdPart) {
     let empty = aTree.isEmpty;
     document.getElementById("removeAll" + aIdPart + "s").disabled = empty;
@@ -185,70 +303,163 @@ var gTranslationExceptions = {
       empty || !aTree.hasSelection;
   },
 
-  onLanguageSelected() {
-    this._handleButtonDisabling(this._langTree, "Language");
+  /**
+   * Updates the UI state for the always-translate languages section.
+   */
+  onSelectAlwaysTranslateLanguage() {
+    this._handleButtonDisabling(
+      this._alwaysTranslateLangsTree,
+      "AlwaysTranslateLanguage"
+    );
   },
 
-  onSiteSelected() {
-    this._handleButtonDisabling(this._siteTree, "Site");
+  /**
+   * Updates the UI state for the never-translate languages section.
+   */
+  onSelectNeverTranslateLanguage() {
+    this._handleButtonDisabling(
+      this._neverTranslateLangsTree,
+      "NeverTranslateLanguage"
+    );
   },
 
-  onLanguageDeleted() {
-    let langs = Services.prefs.getCharPref(kLanguagesPref);
+  /**
+   * Updates the UI state for the never-translate sites section.
+   */
+  onSelectNeverTranslateSite() {
+    this._handleButtonDisabling(
+      this._neverTranslateSiteTree,
+      "NeverTranslateSite"
+    );
+  },
+
+  /**
+   * Updates the value of a language pref to match when a language is removed
+   * through the UI.
+   *
+   * @param {string} pref
+   * @param {Tree} tree
+   */
+  _onRemoveLanguage(pref, tree) {
+    let langs = Services.prefs.getCharPref(pref);
     if (!langs) {
       return;
     }
 
-    let removed = this._langTree.getSelectedItems().map(l => l.langCode);
+    let removed = tree.getSelectedItems().map(l => l.langCode);
 
     langs = langs.split(",").filter(l => !removed.includes(l));
-    Services.prefs.setCharPref(kLanguagesPref, langs.join(","));
+    Services.prefs.setCharPref(pref, langs.join(","));
   },
 
-  onAllLanguagesDeleted() {
-    Services.prefs.setCharPref(kLanguagesPref, "");
+  /**
+   * Updates the never-translate language pref when a never-translate language
+   * is removed via the UI.
+   */
+  onRemoveAlwaysTranslateLanguage() {
+    this._onRemoveLanguage(
+      ALWAYS_TRANSLATE_LANGS_PREF,
+      this._alwaysTranslateLangsTree
+    );
   },
 
-  onSiteDeleted() {
-    let removedSites = this._siteTree.getSelectedItems();
-    for (let origin of removedSites) {
+  /**
+   * Updates the always-translate language pref when a always-translate language
+   * is removed via the UI.
+   */
+  onRemoveNeverTranslateLanguage() {
+    this._onRemoveLanguage(
+      NEVER_TRANSLATE_LANGS_PREF,
+      this._neverTranslateLangsTree
+    );
+  },
+
+  /**
+   * Updates the permissions for a never-translate site when it is removed via the UI.
+   */
+  onRemoveNeverTranslateSite() {
+    let removedNeverTranslateSites =
+      this._neverTranslateSiteTree.getSelectedItems();
+    for (let origin of removedNeverTranslateSites) {
       let principal =
         Services.scriptSecurityManager.createContentPrincipalFromOrigin(origin);
-      Services.perms.removeFromPrincipal(principal, kPermissionType);
+      Services.perms.removeFromPrincipal(principal, TRANSLATIONS_PERMISSION);
     }
   },
 
-  onAllSitesDeleted() {
-    if (this._siteTree.isEmpty) {
+  /**
+   * Clears the always-translate languages pref when the list is cleared in the UI.
+   */
+  onRemoveAllAlwaysTranslateLanguages() {
+    Services.prefs.setCharPref(ALWAYS_TRANSLATE_LANGS_PREF, "");
+  },
+
+  /**
+   * Clears the never-translate languages pref when the list is cleared in the UI.
+   */
+  onRemoveAllNeverTranslateLanguages() {
+    Services.prefs.setCharPref(NEVER_TRANSLATE_LANGS_PREF, "");
+  },
+
+  /**
+   * Clears the never-translate sites pref when the list is cleared in the UI.
+   */
+  onRemoveAllNeverTranslateSites() {
+    if (this._neverTranslateSiteTree.isEmpty) {
       return;
     }
 
-    let removedSites = this._sites.splice(0, this._sites.length);
-    this._siteTree.tree.rowCountChanged(0, -removedSites.length);
+    let removedNeverTranslateSites = this._neverTranslateSites.splice(
+      0,
+      this._neverTranslateSites.length
+    );
+    this._neverTranslateSiteTree.tree.rowCountChanged(
+      0,
+      -removedNeverTranslateSites.length
+    );
 
-    for (let origin of removedSites) {
+    for (let origin of removedNeverTranslateSites) {
       let principal =
         Services.scriptSecurityManager.createContentPrincipalFromOrigin(origin);
-      Services.perms.removeFromPrincipal(principal, kPermissionType);
+      Services.perms.removeFromPrincipal(principal, TRANSLATIONS_PERMISSION);
     }
 
-    this.onSiteSelected();
+    this.onSelectNeverTranslateSite();
   },
 
-  onSiteKeyPress(aEvent) {
+  /**
+   * Handles removing a selected always-translate language via the keyboard.
+   */
+  onAlwaysTranslateLanguageKeyPress(aEvent) {
     if (aEvent.keyCode == KeyEvent.DOM_VK_DELETE) {
-      this.onSiteDeleted();
+      this.onRemoveAlwaysTranslateLanguage();
     }
   },
 
-  onLanguageKeyPress(aEvent) {
+  /**
+   * Handles removing a selected never-translate language via the keyboard.
+   */
+  onNeverTranslateLanguageKeyPress(aEvent) {
     if (aEvent.keyCode == KeyEvent.DOM_VK_DELETE) {
-      this.onLanguageDeleted();
+      this.onRemoveNeverTranslateLanguage();
     }
   },
 
-  uninit() {
+  /**
+   * Handles removing a selected never-translate site via the keyboard.
+   */
+  onNeverTranslateSiteKeyPress(aEvent) {
+    if (aEvent.keyCode == KeyEvent.DOM_VK_DELETE) {
+      this.onRemoveNeverTranslateSite();
+    }
+  },
+
+  /**
+   * Removes any active preference and permissions observers.
+   */
+  removeObservers() {
     Services.obs.removeObserver(this, "perm-changed");
-    Services.prefs.removeObserver(kLanguagesPref, this);
+    Services.prefs.removeObserver(ALWAYS_TRANSLATE_LANGS_PREF, this);
+    Services.prefs.removeObserver(NEVER_TRANSLATE_LANGS_PREF, this);
   },
 };
