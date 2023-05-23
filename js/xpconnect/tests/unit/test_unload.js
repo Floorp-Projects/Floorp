47f1bf49b603a793ee81b5bc79e4163f5a7c539c
Mark Banner <standard8@mozilla.com>
2023-05-23 08:13:07 +0000

    Bug 1834176 - Avoid using NetUtil as a test file in js/xpconnect/tests. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D178588

--- js/xpconnect/tests/unit/test_unload.js
+++ js/xpconnect/tests/unit/test_unload.js
@@ -4,25 +4,25 @@
 
 function run_test() {
   var scope1 = {};
-  var exports1 = ChromeUtils.import("resource://gre/modules/NetUtil.jsm", scope1);
+  var exports1 = ChromeUtils.import("resource://test/TestFile.jsm", scope1);
 
   var scope2 = {};
-  var exports2 = ChromeUtils.import("resource://gre/modules/NetUtil.jsm", scope2);
+  var exports2 = ChromeUtils.import("resource://test/TestFile.jsm", scope2);
 
   Assert.ok(exports1 === exports2);
-  Assert.ok(scope1.NetUtil === scope2.NetUtil);
+  Assert.ok(scope1.TestFile === scope2.TestFile);
 
-  Cu.unload("resource://gre/modules/NetUtil.jsm");
+  Cu.unload("resource://test/TestFile.jsm");
 
   var scope3 = {};
-  var exports3 = ChromeUtils.import("resource://gre/modules/NetUtil.jsm", scope3);
+  var exports3 = ChromeUtils.import("resource://test/TestFile.jsm", scope3);
 
   Assert.equal(false, exports1 === exports3);
-  Assert.equal(false, scope1.NetUtil === scope3.NetUtil);
+  Assert.equal(false, scope1.TestFile === scope3.TestFile);
 
   // When the jsm was unloaded, the value of all its global's properties were
   // set to undefined. While it must be safe (not crash) to call into the
   // module, we expect it to throw an error (e.g., when trying to use Ci).
-  try { scope1.NetUtil.newURI("http://www.example.com"); } catch (e) {}
-  try { scope3.NetUtil.newURI("http://www.example.com"); } catch (e) {}
+  try { scope1.TestFile.doTest(() => {}); } catch (e) {}
+  try { scope3.TestFile.doTest(() => {}); } catch (e) {}
 }
