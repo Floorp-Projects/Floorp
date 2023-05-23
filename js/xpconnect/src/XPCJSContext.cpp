56700b42a83e441e594d3d1bda3caec41ca37414
Matthew Gaudet <mgaudet@mozilla.com>
2023-05-23 15:23:04 +0000

    Bug 1795816 - Ship Array.fromAsync r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D178482

--- js/xpconnect/src/XPCJSContext.cpp
+++ js/xpconnect/src/XPCJSContext.cpp
@@ -785,7 +785,7 @@ static mozilla::Atomic<bool> sWellFormedUnicodeStringsEnabled(false);
 #ifdef ENABLE_CHANGE_ARRAY_BY_COPY
 static mozilla::Atomic<bool> sChangeArrayByCopyEnabled(false);
 #endif
-static mozilla::Atomic<bool> sArrayFromAsyncEnabled(false);
+static mozilla::Atomic<bool> sArrayFromAsyncEnabled(true);
 #ifdef ENABLE_NEW_SET_METHODS
 static mozilla::Atomic<bool> sEnableNewSetMethods(false);
 #endif
