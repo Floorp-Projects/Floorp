5492c271da39b2a913ad97f8f0df6ed78caad100
Paul Adenot <paul@paul.cx>
2023-05-24 13:18:43 +0000

    Bug 1817997 - Limit time resolution to microsecond on values exposed to script. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D176041

--- dom/media/mediasource/MediaSource.cpp
+++ dom/media/mediasource/MediaSource.cpp
@@ -391,7 +391,8 @@ void MediaSource::EndOfStream(
   SetReadyState(MediaSourceReadyState::Ended);
   mSourceBuffers->Ended();
   if (!aError.WasPassed()) {
-    DurationChange(mSourceBuffers->GetHighestBufferedEndTime(), aRv);
+    DurationChange(mSourceBuffers->GetHighestBufferedEndTime().ToBase(1000000),
+                   aRv);
     // Notify reader that all data is now available.
     mDecoder->Ended(true);
     return;
