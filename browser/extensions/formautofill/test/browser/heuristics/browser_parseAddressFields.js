a5a4f7cbcfeac6399c89954ee63c13e75a33bdb7
Dimi <dlee@mozilla.com>
2023-05-24 07:07:26 +0000

    Bug 1833613 - Implement heuristic to interpret `street-address` as `address-line1` r=credential-management-reviewers,sgalich
    A new heuristic has been introduced that interprets "street-address" as "address-line1".
    This is triggered when "address-line2" is detected, and the field preceding "address-line2" is "street-address".
    
    This modification addresses an issue with certain websites incorrectly utilizing
    "street-address" in place of "address-line1".
    
    Differential Revision: https://phabricator.services.mozilla.com/D178663

--- browser/extensions/formautofill/test/browser/heuristics/browser_parseAddressFields.js
+++ browser/extensions/formautofill/test/browser/heuristics/browser_parseAddressFields.js
@@ -5,37 +5,21 @@ http://creativecommons.org/publicdomain/zero/1.0/ */
 
 "use strict";
 
-// This bug happens only when the last element is address-lineX and
-// the field is identified by regular expressions in `HeuristicsRegExp` but is not
-// identified by regular expressions defined in `_parseAddressFields`
-const markup1 = `
-  <html>
-  <body>
-    <form>
-      <p><label>country: <input type="text" id="country" name="country" /></label></p>
-      <p><label>tel: <input type="text" id="tel" name="tel" /></label></p>
-      <p><label><input type="text" id="housenumber" /></label></p>
-    </form>
-  </body>
-  </html>
-`;
-
-const markup2 = `
-  <html>
-  <body>
-    <form>
-      <p><label>country: <input type="text" id="country" name="country" /></label></p>
-      <p><label>tel: <input type="text" id="tel" name="tel" /></label></p>
-      <p><label><input type="text" id="housenumber" /></label></p>
-      <p><label><input type="text" id="addrcomplement" /></label></p>
-    </form>
-  </body>
-  </html>
-`;
-
 add_heuristic_tests([
   {
-    fixtureData: markup1,
+    // This bug happens only when the last element is address-lineX and
+    // the field is identified by regular expressions in `HeuristicsRegExp` but is not
+    // identified by regular expressions defined in `_parseAddressFields`
+    fixtureData: `
+        <html>
+        <body>
+          <form>
+            <p><label>country: <input type="text" id="country" name="country" /></label></p>
+            <p><label>tel: <input type="text" id="tel" name="tel" /></label></p>
+            <p><label><input type="text" id="housenumber" /></label></p>
+          </form>
+        </body>
+        </html>`,
     expectedResult: [
       {
         description:
@@ -52,7 +36,17 @@ add_heuristic_tests([
     ],
   },
   {
-    fixtureData: markup2,
+    fixtureData: `
+        <html>
+        <body>
+          <form>
+            <p><label>country: <input type="text" id="country" name="country" /></label></p>
+            <p><label>tel: <input type="text" id="tel" name="tel" /></label></p>
+            <p><label><input type="text" id="housenumber" /></label></p>
+            <p><label><input type="text" id="addrcomplement" /></label></p>
+          </form>
+        </body>
+        </html>`,
     expectedResult: [
       {
         description:
@@ -65,6 +59,78 @@ add_heuristic_tests([
           { fieldName: "tel" },
           { fieldName: "address-line1" },
           { fieldName: "address-line2" },
+        ],
+      },
+    ],
+  },
+  {
+    // Bug 1833613
+    description:
+      "street-address field is treated as address-line1 when address-line2 is present while adddress-line1 is not",
+    fixtureData: `
+        <html>
+        <body>
+          <form>
+            <input type="text" id="street-address" autocomplete="street-address"/>
+            <input type="text" id="address-line2" autocomplete="address-line2"/>
+            <input type="text" id="email" autocomplete="email"/>
+          </form>
+        </body>
+        </html>`,
+    expectedResult: [
+      {
+        fields: [
+          { fieldName: "address-line1", reason: "regexp-heuristic" },
+          { fieldName: "address-line2", reason: "autocomplete" },
+          { fieldName: "email", reason: "autocomplete" },
+        ],
+      },
+    ],
+  },
+  {
+    // Bug 1833613
+    description:
+      "street-address field should not be treated as address-line1 when address-line2 is not present",
+    fixtureData: `
+        <html>
+        <body>
+          <form>
+            <input type="text" id="street-address" autocomplete="street-address"/>
+            <input type="text" id="address-line3" autocomplete="address-line3"/>
+            <input type="text" id="email" autocomplete="email"/>
+          </form>
+        </body>
+        </html>`,
+    expectedResult: [
+      {
+        fields: [
+          { fieldName: "street-address", reason: "autocomplete" },
+          { fieldName: "address-line3", reason: "autocomplete" },
+          { fieldName: "email", reason: "autocomplete" },
+        ],
+      },
+    ],
+  },
+  {
+    // Bug 1833613
+    description:
+      "street-address field should not be treated as address-line1 when address-line1 is present",
+    fixtureData: `
+        <html>
+        <body>
+          <form>
+            <input type="text" id="street-address" autocomplete="street-address"/>
+            <input type="text" id="address-line1" autocomplete="address-line1"/>
+            <input type="text" id="email" autocomplete="email"/>
+          </form>
+        </body>
+        </html>`,
+    expectedResult: [
+      {
+        fields: [
+          { fieldName: "street-address", reason: "autocomplete" },
+          { fieldName: "address-line1", reason: "autocomplete" },
+          { fieldName: "email", reason: "autocomplete" },
         ],
       },
     ],
