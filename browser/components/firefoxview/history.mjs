918e2b85c1c85fcc12f31502e7059643e543eac3
Kelly Cochrane <kcochrane@mozilla.com>
2023-05-25 13:14:18 +0000

    Bug 1833218 - Create card container component to be used in Firefox View r=sclements
    Creating new revision as a workaround for bug 1834868. The original revision was already R+ed here: https://phabricator.services.mozilla.com/D178473
    
    Differential Revision: https://phabricator.services.mozilla.com/D179073

--- browser/components/firefoxview/history.mjs
+++ browser/components/firefoxview/history.mjs
@@ -4,6 +4,8 @@
 
 import { html } from "chrome://global/content/vendor/lit.all.mjs";
 import { ViewPage } from "./viewpage.mjs";
+// eslint-disable-next-line import/no-unassigned-import
+import "chrome://browser/content/firefoxview/card-container.mjs";
 
 class HistoryInView extends ViewPage {
   constructor() {
@@ -28,9 +30,15 @@ class HistoryInView extends ViewPage {
     }
 
     return html`
-      <ul>
-        ${itemTemplates}
-      </ul>
+      <card-container
+        .viewAllPage=${this.overview ? "history" : null}
+        ?preserveCollapseState=${this.overview ? true : null}
+      >
+        <h2 slot="header" data-l10n-id="firefoxview-history-header"></h2>
+        <ul slot="main">
+          ${itemTemplates}
+        </ul>
+      </card-container>
     `;
   }
 }
