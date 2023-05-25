99c1eb967c3bd6077c97de49c0ddda53308cf269
Eitan Isaacson <eitan@monotonous.org>
2023-05-25 20:38:29 +0000

    Bug 1832353 - P2: Convert editable text tests to browser tests. r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D178715

--- accessible/tests/browser/text/head.js
+++ accessible/tests/browser/text/head.js
@@ -7,7 +7,9 @@
 /* exported createTextLeafPoint, DIRECTION_NEXT, DIRECTION_PREVIOUS,
  BOUNDARY_FLAG_DEFAULT, BOUNDARY_FLAG_INCLUDE_ORIGIN,
  BOUNDARY_FLAG_STOP_IN_EDITABLE, BOUNDARY_FLAG_SKIP_LIST_ITEM_MARKER,
- readablePoint, testPointEqual, textBoundaryGenerator, testBoundarySequence */
+ readablePoint, testPointEqual, textBoundaryGenerator, testBoundarySequence,
+ isFinalValueCorrect, isFinalValueCorrect, testInsertText, testDeleteText,
+ testCopyText, testPasteText, testCutText, testSetTextContents */
 
 // Load the shared-head file first.
 Services.scriptloader.loadSubScript(
@@ -17,9 +19,13 @@ Services.scriptloader.loadSubScript(
 
 // Loading and common.js from accessible/tests/mochitest/ for all tests, as
 // well as promisified-events.js.
+
+/* import-globals-from ../../mochitest/role.js */
+
 loadScripts(
   { name: "common.js", dir: MOCHITESTS_DIR },
   { name: "text.js", dir: MOCHITESTS_DIR },
+  { name: "role.js", dir: MOCHITESTS_DIR },
   { name: "promisified-events.js", dir: MOCHITESTS_DIR }
 );
 
@@ -130,4 +136,141 @@ function testBoundarySequence(
     expectedSequence,
     msg
   );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Editable text
+
+async function waitForCopy(browser) {
+  await BrowserTestUtils.waitForContentEvent(browser, "copy", false, evt => {
+    return true;
+  });
+
+  let clipboardData = await invokeContentTask(browser, [], async () => {
+    let text = await content.navigator.clipboard.readText();
+    return text;
+  });
+
+  return clipboardData;
+}
+
+async function isFinalValueCorrect(
+  browser,
+  acc,
+  expectedTextLeafs,
+  msg = "Value is correct"
+) {
+  let value =
+    acc.role == ROLE_ENTRY
+      ? acc.value
+      : await invokeContentTask(browser, [], () => {
+          return content.document.body.textContent;
+        });
+
+  let [before, text, after] = expectedTextLeafs;
+  let finalValue =
+    before && after && !text
+      ? [before, after].join(" ")
+      : [before, text, after].join("");
+
+  is(value.replace("\xa0", " "), finalValue, msg);
+}
+
+function waitForTextChangeEvents(acc, eventSeq) {
+  let events = eventSeq.map(eventType => {
+    return [eventType, acc];
+  });
+
+  if (acc.role == ROLE_ENTRY) {
+    events.push([EVENT_TEXT_VALUE_CHANGE, acc]);
+  }
+
+  return waitForEvents(events);
+}
+
+async function testSetTextContents(acc, text, staticContentOffset, events) {
+  acc.QueryInterface(nsIAccessibleEditableText);
+  let evtPromise = waitForTextChangeEvents(acc, events);
+  acc.setTextContents(text);
+  let evt = (await evtPromise)[0];
+  evt.QueryInterface(nsIAccessibleTextChangeEvent);
+  is(evt.start, staticContentOffset);
+}
+
+async function testInsertText(
+  acc,
+  textToInsert,
+  insertOffset,
+  staticContentOffset
+) {
+  acc.QueryInterface(nsIAccessibleEditableText);
+
+  let evtPromise = waitForTextChangeEvents(acc, [EVENT_TEXT_INSERTED]);
+  acc.insertText(textToInsert, staticContentOffset + insertOffset);
+  let evt = (await evtPromise)[0];
+  evt.QueryInterface(nsIAccessibleTextChangeEvent);
+  is(evt.start, staticContentOffset + insertOffset);
+}
+
+async function testDeleteText(
+  acc,
+  startOffset,
+  endOffset,
+  staticContentOffset
+) {
+  acc.QueryInterface(nsIAccessibleEditableText);
+
+  let evtPromise = waitForTextChangeEvents(acc, [EVENT_TEXT_REMOVED]);
+  acc.deleteText(
+    staticContentOffset + startOffset,
+    staticContentOffset + endOffset
+  );
+  let evt = (await evtPromise)[0];
+  evt.QueryInterface(nsIAccessibleTextChangeEvent);
+  is(evt.start, staticContentOffset + startOffset);
+}
+
+async function testCopyText(
+  acc,
+  startOffset,
+  endOffset,
+  staticContentOffset,
+  browser,
+  aExpectedClipboard = null
+) {
+  acc.QueryInterface(nsIAccessibleEditableText);
+  let copied = waitForCopy(browser);
+  acc.copyText(
+    staticContentOffset + startOffset,
+    staticContentOffset + endOffset
+  );
+  let clipboardText = await copied;
+  if (aExpectedClipboard != null) {
+    is(clipboardText, aExpectedClipboard, "Correct text in clipboard");
+  }
+}
+
+async function testPasteText(acc, insertOffset, staticContentOffset) {
+  acc.QueryInterface(nsIAccessibleEditableText);
+  let evtPromise = waitForTextChangeEvents(acc, [EVENT_TEXT_INSERTED]);
+  acc.pasteText(staticContentOffset + insertOffset);
+
+  let evt = (await evtPromise)[0];
+  evt.QueryInterface(nsIAccessibleTextChangeEvent);
+  // XXX: In non-headless mode pasting text produces several text leaves
+  // and the offset is not what we expect.
+  // is(evt.start, staticContentOffset + insertOffset);
+}
+
+async function testCutText(acc, startOffset, endOffset, staticContentOffset) {
+  acc.QueryInterface(nsIAccessibleEditableText);
+  let evtPromise = waitForTextChangeEvents(acc, [EVENT_TEXT_REMOVED]);
+  acc.cutText(
+    staticContentOffset + startOffset,
+    staticContentOffset + endOffset
+  );
+
+  let evt = (await evtPromise)[0];
+  evt.QueryInterface(nsIAccessibleTextChangeEvent);
+  is(evt.start, staticContentOffset + startOffset);
 }
