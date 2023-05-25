3c36a0d4ad83a97ec7042bff4da5ce334fb01d02
Emilio Cobos Álvarez <emilio@crisal.io>
2023-05-25 16:50:22 +0000

    Bug 1834960 - Avoid some code duplication from the previous patches. r=zrhoffman
    This makes me a bit less uncomfortable about the previous changes :)
    
    Note that some of this code gets modified in the following patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D179059

--- layout/style/ServoBindingTypes.h
+++ layout/style/ServoBindingTypes.h
@@ -107,14 +107,13 @@ class Element;
     static void Release(type_* aPtr) { Servo_##name_##_Release(aPtr); } \
   };                                                                    \
   }
-#define SERVO_ARC_TYPE_WITH_PREFIX(name_, style_prefix_) \
-  namespace mozilla {                                    \
-  struct Style##style_prefix_##name_;                    \
-  }                                                      \
-  SERVO_ARC_TYPE(name_, mozilla::Style##style_prefix_##name_)
-#define SERVO_LOCKED_ARC_TYPE(name_) SERVO_ARC_TYPE_WITH_PREFIX(name_, Locked)
+#define SERVO_LOCKED_ARC_TYPE(name_) \
+  namespace mozilla {                \
+  struct StyleLocked##name_;         \
+  }                                  \
+  SERVO_ARC_TYPE(name_, mozilla::StyleLocked##name_)
 #include "mozilla/ServoLockedArcTypeList.h"
-SERVO_ARC_TYPE_WITH_PREFIX(PropertyRule, )
+SERVO_ARC_TYPE(PropertyRule, mozilla::StylePropertyRule)
 SERVO_ARC_TYPE(AnimationValue, mozilla::StyleAnimationValue)
 SERVO_ARC_TYPE(ComputedStyle, mozilla::ComputedStyle)
 SERVO_ARC_TYPE(CssUrlData, mozilla::StyleCssUrlData)
