dc24e79719ca112c797c474efc47131b818f1058
Luca Greco <lgreco@mozilla.com>
2023-05-26 18:39:33 +0000

    Bug 1835013 - Include quarantinedDomains.enabled pref in about:support. r=willdurand
    Differential Revision: https://phabricator.services.mozilla.com/D179058

--- toolkit/modules/Troubleshoot.sys.mjs
+++ toolkit/modules/Troubleshoot.sys.mjs
@@ -63,6 +63,7 @@ const PREFS_FOR_DISPLAY = [
   "extensions.formautofill.",
   "extensions.lastAppVersion",
   "extensions.manifestV3.enabled",
+  "extensions.quarantinedDomains.enabled",
   "extensions.InstallTrigger.enabled",
   "extensions.InstallTriggerImpl.enabled",
   "fission.autostart",
