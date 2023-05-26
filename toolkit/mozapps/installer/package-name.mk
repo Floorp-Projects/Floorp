8e9014ced86a64f8148f69494d74e60ea28007fb
Mike Hommey <mh+mozilla@glandium.org>
2023-05-26 22:55:28 +0000

    Bug 1835189 - Make TARGET_OS in the build system mean the same as target.os in configure. r=firefox-build-system-reviewers,nalexander
    The situation is confusing enough.
    
    Eventually, we'll want to have TARGET_FOO mean the same as target.foo,
    but let's start with the easy one.
    
    Differential Revision: https://phabricator.services.mozilla.com/D179178

--- toolkit/mozapps/installer/package-name.mk
+++ toolkit/mozapps/installer/package-name.mk
@@ -15,13 +15,13 @@ MOZ_PKG_VERSION = $(MOZ_APP_VERSION)
 endif
 
 ifndef MOZ_PKG_PLATFORM
-MOZ_PKG_PLATFORM := $(TARGET_OS)-$(TARGET_CPU)
+MOZ_PKG_PLATFORM := $(TARGET_RAW_OS)-$(TARGET_CPU)
 
 ifeq ($(MOZ_BUILD_APP),mobile/android)
 MOZ_PKG_PLATFORM := android-$(TARGET_CPU)
 endif
 
-# TARGET_OS/TARGET_CPU may be unintuitive, so we hardcode some special formats
+# TARGET_RAW_OS/TARGET_CPU may be unintuitive, so we hardcode some special formats
 ifeq ($(OS_ARCH),WINNT)
 ifeq ($(CPU_ARCH),x86)
 MOZ_PKG_PLATFORM := win32
@@ -36,7 +36,7 @@ endif
 ifeq ($(OS_ARCH),Darwin)
 MOZ_PKG_PLATFORM := mac
 endif
-ifeq ($(TARGET_OS),linux-gnu)
+ifeq ($(TARGET_RAW_OS),linux-gnu)
 MOZ_PKG_PLATFORM := linux-$(TARGET_CPU)
 endif
 endif #MOZ_PKG_PLATFORM
