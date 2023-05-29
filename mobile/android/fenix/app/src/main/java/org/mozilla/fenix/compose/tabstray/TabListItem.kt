a6bf6e97a44748f60ded1537e4c43e6e09678dc8
DreVla <vlad.dreghici@gmail.com>
2023-05-29 11:34:26 +0300

    Bug 1828493 - Apply purple overlay on list item when in multi-select
    When having the list layout for tabs tray and entering multi-select
    mode, the selected list items should have a purple non opaque overlay
    on the thumbnail, as it was before in the XML implementation.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/compose/tabstray/TabListItem.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/compose/tabstray/TabListItem.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -22,6 +23,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -159,6 +161,13 @@ private fun Thumbnail(
         )
 
         if (isSelected) {
+            Box(
+                modifier = Modifier
+                    .size(width = 92.dp, height = 72.dp)
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(FirefoxTheme.colors.layerAccentNonOpaque),
+            )
+
             Card(
                 modifier = Modifier
                     .size(size = 40.dp)
