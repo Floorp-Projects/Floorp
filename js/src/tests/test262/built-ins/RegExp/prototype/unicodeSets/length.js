63cb19d42ef653f1fcc4936a3fb12e18c2d67123
Daniel Minor <dminor@mozilla.com>
2023-05-30 19:31:19 +0000

    Bug 1826574 - Enable test262 tests for Unicode Sets; r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D178941

--- js/src/tests/test262/built-ins/RegExp/prototype/unicodeSets/length.js
+++ js/src/tests/test262/built-ins/RegExp/prototype/unicodeSets/length.js
@@ -1,4 +1,3 @@
-// |reftest| skip -- regexp-v-flag is not supported
 // Copyright (C) 2022 Mathias Bynens, Ron Buckton, and André Bargull. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
