0f02f61228a7183abcf86f92d30c245bc2b75e41
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2023-05-30 23:57:40 +0000

    Bug 1827583 - Set event listener to current media control after switching process. r=geckoview-reviewers,amejiamarmol,jonalmeida a=pascalc
    This issue is a timing issue of loading content via about:config etc.
    
    Although `GeckoViewMediaControl` registers event listeners for
    `MediaController` when enabling `MediaSessionDelegate`, if process
    switching occurs, `browsingContenxt` and `MediaController` are re-created.
    
    It means that we has to register newer `MediaController`.
    
    To detect destroying current browser, I will add `onDestroyBrowser` to
    `GeckoViewModule` again (This was removed by bug 1672262).
    
    Differential Revision: https://phabricator.services.mozilla.com/D179177

--- mobile/android/chrome/geckoview/geckoview.js
+++ mobile/android/chrome/geckoview/geckoview.js
@@ -403,6 +403,9 @@ class ModuleInfo {
    * Called before the browser is removed
    */
   onDestroyBrowser() {
+    if (this._impl) {
+      this._impl.onDestroyBrowser();
+    }
     this._contentModuleLoaded = false;
   }
 
