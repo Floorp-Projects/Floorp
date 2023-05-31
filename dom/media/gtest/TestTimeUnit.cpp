f0c699ef575d6ddcc629499ffc38ca450562d032
Natalia Csoregi <ncsoregi@mozilla.com>
2023-05-31 14:45:40 +0300

    Backed out changeset 87967198f72a (bug 1833981) for failures on test_DrainOnMissingData_mp4.html. CLOSED TREE

--- dom/media/gtest/TestTimeUnit.cpp
+++ dom/media/gtest/TestTimeUnit.cpp
@@ -64,7 +64,7 @@ TEST(TimeUnit, BasicArithmetic)
     TimeUnit c = TimeUnit(9001, 90000);
     TimeUnit d = (b - c).ToBase(90000);
     EXPECT_EQ(d.mBase, 90000);
-    EXPECT_EQ(d.mTicks.value(), 530999);
+    EXPECT_EQ(d.mTicks.value(), 530998);
   }
 }
 
@@ -237,27 +237,4 @@ TEST(TimeUnit, InfinityMath)
   EXPECT_EQ(finite - posInf, negInf);
   EXPECT_EQ(finite + negInf, negInf);
   EXPECT_EQ(finite - negInf, posInf);
-}
-
-TEST(TimeUnit, BaseConversion)
-{
-  const int64_t packetSize = 1024;  // typical for AAC
-  int64_t sampleRates[] = {16000, 44100, 48000, 88200, 96000};
-  const double hnsPerSeconds = 10000000.;
-  for (auto sampleRate : sampleRates) {
-    int64_t frameCount = 0;
-    TimeUnit pts;
-    do {
-      // Compute a time in hundreds of nanoseconds based of frame count, typical
-      // on Windows platform, checking that it round trips properly.
-      int64_t hns = AssertedCast<int64_t>(
-          std::round(hnsPerSeconds * static_cast<double>(frameCount) /
-                     static_cast<double>(sampleRate)));
-      pts = TimeUnit::FromHns(hns, sampleRate);
-      EXPECT_EQ(
-          AssertedCast<int64_t>(std::round(pts.ToSeconds() * hnsPerSeconds)),
-          hns);
-      frameCount += packetSize;
-    } while (pts.ToSeconds() < 36000);
-  }
 }
