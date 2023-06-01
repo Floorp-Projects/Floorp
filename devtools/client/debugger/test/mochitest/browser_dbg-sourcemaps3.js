13a88ab5c4e3972be0530046eb8828f3d65124b5
Alexandre Poirot <poirot.alex@gmail.com>
2023-06-01 16:41:21 +0000

    Bug 1836251 - [devtools] Always pass real location object to getBreakpoint. r=devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D178396

--- devtools/client/debugger/test/mochitest/browser_dbg-sourcemaps3.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-sourcemaps3.js
@@ -28,7 +28,9 @@ add_task(async function () {
   await addBreakpoint(dbg, sortedSrc, 9, 4);
   is(dbg.selectors.getBreakpointCount(), 1, "One breakpoint exists");
   ok(
-    dbg.selectors.getBreakpoint({ sourceId: sortedSrc.id, line: 9, column: 4 }),
+    dbg.selectors.getBreakpoint(
+      createLocation({ source: sortedSrc, line: 9, column: 4 })
+    ),
     "Breakpoint has correct line"
   );
 
