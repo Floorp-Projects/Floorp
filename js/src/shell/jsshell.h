0d611c97405080609d3fdf8ffd22202260970d8e
Chris Fallin <chris@cfallin.org>
2023-06-01 13:37:35 +0000

    Bug 1831030 - JS shell on WASI: add basic Wizer integration. r=jandem
    This patch allows the use of a JS shell binary, compiled to
    `wasm32-wasi` (WASI ABI on WebAssembly), to make use of the Wizer
    snapshotting tool to embed JavaScript source with SpiderMonkey to run in
    a standalone Wasm module.
    
    If enabled with `--enable-js-shell-wizer`, the shell no longer provides
    an interactive REPL; instead, when invoked during the "initialization"
    phase of Wizer, it reads JS source on standard input, then
    saves JS state (the global, the context, etc) for later. When the
    execution is snapshotted and the snapshot is later executed as an
    ordinary WASI command, the resume entry-point invokes a JS function
    named `main` in the global scope, if any.
    
    An example of how to use this follows:
    
    ```
    $ echo "function main() { print('hello world'); }" | \
      wizer --allow-wasi -r _start=wizer.resume obj-release/dist/bin/js \
      -o snapshotted.wasm
    
    $ wasmtime snapshotted.wasm
    hello world
    $
    ```
    
    Differential Revision: https://phabricator.services.mozilla.com/D176979

--- js/src/shell/jsshell.h
+++ js/src/shell/jsshell.h
@@ -19,6 +19,7 @@
 #include "threading/LockGuard.h"
 #include "threading/Mutex.h"
 #include "threading/Thread.h"
+#include "util/CompleteFile.h"  // js::FileContents
 #include "vm/GeckoProfiler.h"
 #include "vm/Monitor.h"
 
@@ -252,10 +253,26 @@ struct ShellContext {
   JS::PersistentRooted<FunctionVector> finalizationRegistryCleanupCallbacks;
 };
 
+// A JSContext, the associated global object, the ShellContext, and
+// other miscellaneous bits that need to live as long as the
+// shell. This is used to wrap up the shell state when it needs to be
+// persisted (e.g., in a Wizer snapshot).
+struct JSAndShellContext {
+  JSContext* cx;
+  JSObject* glob;
+  UniquePtr<ShellContext> shellCx;
+  mozilla::Maybe<js::FileContents> selfHostedXDRBuffer;
+  RCFile rcStdout;
+  RCFile rcStderr;
+};
+
 extern ShellContext* GetShellContext(JSContext* cx);
 
 [[nodiscard]] extern bool PrintStackTrace(JSContext* cx,
                                           JS::Handle<JSObject*> stackObj);
+
+mozilla::Variant<JSAndShellContext, int> ShellMain(int argc, char** argv,
+                                                   bool retainContext);
 
 } /* namespace shell */
 } /* namespace js */
