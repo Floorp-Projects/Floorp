c0de474a845065e06586031dba1d56fdb4e27cc3
Noemi Erli <nerli@mozilla.com>
2023-06-02 15:39:26 +0300

    Backed out 3 changesets (bug 1760334) for causing SM bustages in ScopeBindingCache.h CLOSED TREE
    Backed out changeset 5a4374cfd962 (bug 1760334)
    Backed out changeset bbd5d4b8e2eb (bug 1760334)
    Backed out changeset 998204890aed (bug 1760334)

--- js/src/frontend/ScopeBindingCache.h
+++ js/src/frontend/ScopeBindingCache.h
@@ -26,7 +26,6 @@ namespace frontend {
 struct CompilationAtomCache;
 struct CompilationStencil;
 struct ScopeStencilRef;
-struct FakeStencilGlobalScope;
 struct CompilationStencilMerger;
 
 // Generic atom wrapper which provides a way to interpret any Atom given
@@ -82,10 +81,6 @@ struct GenericAtom {
   // already compiled script.
   GenericAtom(const CompilationStencil& context, TaggedParserAtomIndex index);
   GenericAtom(ScopeStencilRef& scope, TaggedParserAtomIndex index);
-  GenericAtom(const FakeStencilGlobalScope& scope, TaggedParserAtomIndex index)
-      : ref((JSAtom*)nullptr) {
-    MOZ_MAKE_COMPILER_ASSUME_IS_UNREACHABLE();
-  }
 
   // Constructor for atoms managed by the Garbage Collector, while providing
   // contextual scope information when delazifying functions on the main thread.
@@ -110,10 +105,6 @@ struct BindingHasher<TaggedParserAtomIndex> {
     GenericAtom other;
 
     Lookup(ScopeStencilRef& scope_ref, const GenericAtom& other);
-    Lookup(const FakeStencilGlobalScope& scope_ref, const GenericAtom& other)
-        : keyStencil(*(const CompilationStencil*)nullptr), other(other) {
-      MOZ_MAKE_COMPILER_ASSUME_IS_UNREACHABLE();
-    }
   };
 
   static HashNumber hash(const Lookup& aLookup) { return aLookup.other.hash; }
@@ -186,15 +177,12 @@ class ScopeBindingCache {
   // of scope given as arguments.
   virtual bool canCacheFor(Scope* ptr);
   virtual bool canCacheFor(ScopeStencilRef ref);
-  virtual bool canCacheFor(const FakeStencilGlobalScope& ref);
 
   // Create a new BindingMap cache for a given scope. This cache should then be
   // filled with all names which might be looked up.
   virtual BindingMap<JSAtom*>* createCacheFor(Scope* ptr);
   virtual BindingMap<TaggedParserAtomIndex>* createCacheFor(
       ScopeStencilRef ref);
-  virtual BindingMap<TaggedParserAtomIndex>* createCacheFor(
-      const FakeStencilGlobalScope& ref);
 
   // Return the BindingMap created for the associated scope, unless the
   // generation value does not match the one stored internally, in which case a
@@ -202,8 +190,6 @@ class ScopeBindingCache {
   virtual BindingMap<JSAtom*>* lookupScope(Scope* ptr, CacheGeneration gen);
   virtual BindingMap<TaggedParserAtomIndex>* lookupScope(ScopeStencilRef ref,
                                                          CacheGeneration gen);
-  virtual BindingMap<TaggedParserAtomIndex>* lookupScope(
-      const FakeStencilGlobalScope& ref, CacheGeneration gen);
 };
 
 // NoScopeBindingCache is a no-op which does not implement a ScopeBindingCache.
@@ -216,7 +202,6 @@ class NoScopeBindingCache final : public ScopeBindingCache {
 
   bool canCacheFor(Scope* ptr) override;
   bool canCacheFor(ScopeStencilRef ref) override;
-  bool canCacheFor(const FakeStencilGlobalScope& ref) override;
 };
 
 // StencilScopeBindingCache provides an interface to cache the bindings provided
@@ -248,17 +233,10 @@ class MOZ_STACK_CLASS StencilScopeBindingCache final
   CacheGeneration getCurrentGeneration() const override { return 1; }
 
   bool canCacheFor(ScopeStencilRef ref) override;
-  bool canCacheFor(const FakeStencilGlobalScope& ref) override;
-
   BindingMap<TaggedParserAtomIndex>* createCacheFor(
       ScopeStencilRef ref) override;
-  BindingMap<TaggedParserAtomIndex>* createCacheFor(
-      const FakeStencilGlobalScope& ref) override;
-
   BindingMap<TaggedParserAtomIndex>* lookupScope(ScopeStencilRef ref,
                                                  CacheGeneration gen) override;
-  BindingMap<TaggedParserAtomIndex>* lookupScope(
-      const FakeStencilGlobalScope& ref, CacheGeneration gen) override;
 };
 
 // RuntimeScopeBindingCache is used to hold the binding map for each scope which
