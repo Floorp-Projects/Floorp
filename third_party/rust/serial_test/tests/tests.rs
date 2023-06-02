826722207ae0b5c1c6b894e69411ce7ac952cc87
Mike Hommey <mh+mozilla@glandium.org>
2023-06-02 21:49:16 +0000

    Bug 1836419 - Remove the serial_test dependency. r=nordzilla,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D179754

--- third_party/rust/serial_test/tests/tests.rs
+++
@@ -1,8 +0,0 @@
-use serial_test::local_serial_core;
-
-#[test]
-fn test_empty_serial_call() {
-    local_serial_core("beta", || {
-        println!("Bar");
-    });
-}
--- third_party/rust/serial_test_derive/tests/broken/test_serial_async_before_wrapper.rs
+++
@@ -1,7 +0,0 @@
-use serial_test_derive::serial;
-
-#[serial]
-#[actix_rt::test]
-async fn test_async_serial_no_arg_actix() {}
-
-fn main() {}
