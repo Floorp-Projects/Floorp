b8efcb74fd53ede2d05a7b8f9068c5144184198d
Mike Hommey <mh+mozilla@glandium.org>
2023-06-05 22:49:51 +0000

    Bug 1836420 - Upgrade displaydoc to 0.2.4. r=emilio,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D179756

--- third_party/rust/displaydoc/tests/std/multiple.rs
+++ third_party/rust/displaydoc/tests/std/multiple.rs
@@ -29,10 +29,10 @@ use displaydoc::Display;
 #[derive(Display)]
 struct FakeType;
 
-static_assertions::assert_impl_all!(label; FakeType, core::fmt::Display);
+static_assertions::assert_impl_all!(FakeType: core::fmt::Display);
 
 /// this type is pretty swell2
 #[derive(Display)]
 struct FakeType2;
 
-static_assertions::assert_impl_all!(label2; FakeType2, core::fmt::Display);
+static_assertions::assert_impl_all!(FakeType2: core::fmt::Display);
