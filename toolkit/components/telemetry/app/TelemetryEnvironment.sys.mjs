2438480b7fb0a7da832bdc236b7fffdb1b2601da
Sergey Galich <sgalich@mozilla.com>
2023-06-05 03:21:23 +0000

    Bug 1836554 - Removing extensions.formautofill.creditCards.used preference tracking r=credential-management-reviewers,issammani
    Differential Revision: https://phabricator.services.mozilla.com/D179854

--- toolkit/components/telemetry/app/TelemetryEnvironment.sys.mjs
+++ toolkit/components/telemetry/app/TelemetryEnvironment.sys.mjs
@@ -290,7 +290,6 @@ const DEFAULT_ENVIRONMENT_PREFS = new Map([
     "extensions.formautofill.creditCards.available",
     { what: RECORD_PREF_VALUE },
   ],
-  ["extensions.formautofill.creditCards.used", { what: RECORD_PREF_VALUE }],
   ["extensions.manifestV3.enabled", { what: RECORD_PREF_VALUE }],
   ["extensions.quarantinedDomains.enabled", { what: RECORD_PREF_VALUE }],
   ["extensions.strictCompatibility", { what: RECORD_PREF_VALUE }],
