d227ba4fb0ca2290f82909416d114ea49381b3e0
Job Bautista <jobbautista9@protonmail.com>
2023-06-05 19:40:38 +0800

    [image] Delete duplicated IMAGE_JXL entry in extraMimeEntries[]
    Got overlooked while backporting from UXP. It should be put behind the MOZ_JXL
    flag anyway.

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -533,7 +533,6 @@ static const nsExtraMimeTypeEntry extraMimeEntries[] = {
     {IMAGE_SVG_XML, "svg", "Scalable Vector Graphics"},
     {IMAGE_WEBP, "webp", "WebP Image"},
     {IMAGE_AVIF, "avif", "AV1 Image File"},
-    {IMAGE_JXL, "jxl", "JPEG XL Image File"},
 
     {MESSAGE_RFC822, "eml", "RFC-822 data"},
     {TEXT_PLAIN, "txt,text", "Text File"},
