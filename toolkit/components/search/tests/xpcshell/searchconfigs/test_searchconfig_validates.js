d6fbe483827c98c987351acec7740fc71af77856
Mark Banner <standard8@mozilla.com>
2023-06-06 16:43:14 +0000

    Bug 1835824 - Add a test to validate search-config against the schema. r=search-reviewers,mcheang,daleharvey
    This will also ensure that if we add new properties to the search-config, we'll also need to add them to the schema.
    
    Differential Revision: https://phabricator.services.mozilla.com/D179496

---
+++ toolkit/components/search/tests/xpcshell/searchconfigs/test_searchconfig_validates.js
@@ -0,0 +1,67 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+ChromeUtils.defineESModuleGetters(this, {
+  JsonSchema: "resource://gre/modules/JsonSchema.sys.mjs",
+});
+
+/**
+ * Checks to see if a value is an object or not.
+ *
+ * @param {*} value
+ *   The value to check.
+ * @returns {boolean}
+ */
+function isObject(value) {
+  return value != null && typeof value == "object" && !Array.isArray(value);
+}
+
+/**
+ * This function modifies the schema to prevent allowing additional properties
+ * on objects. This is used to enforce that the schema contains everything that
+ * we deliver via the search configuration.
+ *
+ * These checks are not enabled in-product, as we want to allow older versions
+ * to keep working if we add new properties for whatever reason.
+ *
+ * @param {object} section
+ *   The section to check to see if an additionalProperties flag should be added.
+ */
+function disallowAdditionalProperties(section) {
+  if (section.type == "object") {
+    section.additionalProperties = false;
+  }
+  for (let value of Object.values(section)) {
+    if (isObject(value)) {
+      disallowAdditionalProperties(value);
+    }
+  }
+}
+
+add_task(async function test_search_config_validates_to_schema() {
+  let schema = await IOUtils.readJSON(
+    PathUtils.join(do_get_cwd().path, "search-engine-config-schema.json")
+  );
+  disallowAdditionalProperties(schema);
+
+  let selector = new SearchEngineSelector(() => {});
+  let searchConfig = await selector.getEngineConfiguration();
+  let validator = new JsonSchema.Validator(schema);
+
+  for (let entry of searchConfig) {
+    // Records in Remote Settings contain additional properties independent of
+    // the schema. Hence, we don't want to validate their presence.
+    delete entry.schema;
+    delete entry.id;
+    delete entry.last_modified;
+
+    let result = validator.validate(entry);
+    let message = `Should validate ${entry.webExtension?.id}`;
+    if (!result.valid) {
+      message += `:\n${JSON.stringify(result.errors, null, 2)}`;
+    }
+    Assert.ok(result.valid, message);
+  }
+});
