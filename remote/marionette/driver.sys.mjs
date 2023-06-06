89700cc1906e97cdcab2f1e7180515b591ae8571
Julian Descottes <jdescottes@mozilla.com>
2023-06-06 13:55:39 +0000

    Bug 1832891 - [remote] Set timeout to 5s for waitForInitialNavigation for new contexts r=webdriver-reviewers,whimboo, a=dmeehan
    Extending the timeout only when we explicitly create a new window or tab should be safe.
    On the other hand, initial windows from the browser or new tabs created by the browser UI can remain on the initial document forever, for instance on Android.
    So for those cases we keep the current unloadTimeout value.
    
    Differential Revision: https://phabricator.services.mozilla.com/D179710

--- remote/marionette/driver.sys.mjs
+++ remote/marionette/driver.sys.mjs
@@ -2290,7 +2290,10 @@ GeckoDriver.prototype.newWindow = async function (cmd) {
   // Actors need the new window to be loaded to safely execute queries.
   // Wait until the initial page load has been finished.
   await lazy.waitForInitialNavigationCompleted(
-    contentBrowser.browsingContext.webProgress
+    contentBrowser.browsingContext.webProgress,
+    {
+      unloadTimeout: 5000,
+    }
   );
 
   const id = lazy.TabManager.getIdForBrowser(contentBrowser);
