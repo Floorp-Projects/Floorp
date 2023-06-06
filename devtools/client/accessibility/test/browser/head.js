c5c84a77dc3f1cc4a140ced28a50e5056dee508d
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-06-06 13:33:47 +0000

    Bug 1767748 - [devtools] Fix browser_accessibility_sidebar_checks.js intermittent. r=devtools-reviewers,ochameau.
    The test failure screenshots show that the accessibility panel is not focused
    and the document node not selected, so there's no data being displayed in the
    sidebar.
    Let's try to focus the a11y panel to see if this fixes the problem.
    
    Also, we remove an unnecessary check on `accessibility-enable-button` as the
    element does not exist anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D179915

--- devtools/client/accessibility/test/browser/head.js
+++ devtools/client/accessibility/test/browser/head.js
@@ -104,11 +104,7 @@ async function addTestTab(url) {
   const doc = win.document;
   const store = win.view.store;
 
-  const enableButton = doc.getElementById("accessibility-enable-button");
-  // If enable button is not found, asume the tool is already enabled.
-  if (enableButton) {
-    EventUtils.sendMouseEvent({ type: "click" }, enableButton, win);
-  }
+  win.focus();
 
   await waitUntilState(
     store,
