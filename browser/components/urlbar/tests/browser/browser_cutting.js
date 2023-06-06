5261d60d73d771fcca4efc8ca26bb7408cc7bf57
Narcis Beleuzu <nbeleuzu@mozilla.com>
2023-06-06 19:05:07 +0300

    Backed out changeset 862bd1ab18fd (bug 1833091) for bc failure on browser_ext_webNavigation_urlbar_transitions.js . CLOSED TREE

--- browser/components/urlbar/tests/browser/browser_cutting.js
+++ browser/components/urlbar/tests/browser/browser_cutting.js
@@ -2,16 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-add_task(async function test() {
+function test() {
   gURLBar.focus();
   gURLBar.inputField.value = "https://example.com/";
   gURLBar.selectionStart = 4;
   gURLBar.selectionEnd = 5;
   goDoCommand("cmd_cut");
   is(
-    gURLBar.value,
+    gURLBar.inputField.value,
     "http://example.com/",
     "location bar value after cutting 's' from https"
   );
   gURLBar.handleRevert();
-});
+}
