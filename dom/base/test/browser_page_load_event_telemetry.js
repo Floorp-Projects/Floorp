f1dcfdbfbb48340e15769d439bcb88f8c0d96fad
Chris H-C <chutten@mozilla.com>
2023-06-06 20:04:32 +0000

    Bug 1836686 - Skip browser_page_load_event_telemetry in artifact builds r=perry.mcmanis
    This test attempts to test in JS a ping submitted by C++, and that goes awry
    when in artifact mode.
    
    Differential Revision: https://phabricator.services.mozilla.com/D180096

--- dom/base/test/browser_page_load_event_telemetry.js
+++ dom/base/test/browser_page_load_event_telemetry.js
@@ -7,6 +7,11 @@ const { TelemetryTestUtils } = ChromeUtils.importESModule(
 const ALL_CHANNELS = Ci.nsITelemetry.DATASET_ALL_CHANNELS;
 
 add_task(async function () {
+  if (Services.prefs.getBoolPref("telemetry.fog.artifact_build", false)) {
+    Assert.ok(true, "Test skipped in artifact builds. See bug 1836686.");
+    return;
+  }
+
   let tab = await BrowserTestUtils.openNewForegroundTab({
     gBrowser,
     waitForLoad: true,
