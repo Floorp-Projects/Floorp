7ff7ad6eb83ef9dd04fed06b95a4393f13d01e48
Nico Grunbaum <na-g@nostrum.com>
2023-06-07 00:29:15 -0700

    Bug 1833237 - Vendor libwebrtc from a09f21b207
    Upstream commit: https://webrtc.googlesource.com/src/+/a09f21b2074b8058f92714eaa426b7a72fd85570
        Introduce capture_time_identifier in webrtc::EncodedImage
    
        This CL propagates capture_time_identifier introduced in
        webrtc::VideoFrame and propagates it to EncodedImage. For use cases
        involving EncodedTransforms, this identifier is further propagated to
        TransformableVideoSenderFrame.
    
        VideoEncoder::Encode function is overriden by each encoder. Each of
        these overriden functions needs to be changed so that they can handle
        this new identifier and propagate its value in the created EncodedImage.
    
        Change-Id: I5bea4c5a3fe714f1198e497a4bcb5fd059afe516
        Bug: webrtc:14878
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/291800
        Reviewed-by: Tony Herre <herre@google.com>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Palak Agarwal <agpalak@google.com>
        Cr-Commit-Position: refs/heads/main@{#39374}

--- third_party/libwebrtc/modules/video_coding/codecs/av1/libaom_av1_encoder_unittest.cc
+++ third_party/libwebrtc/modules/video_coding/codecs/av1/libaom_av1_encoder_unittest.cc
@@ -263,5 +263,37 @@ TEST(LibaomAv1EncoderTest, RtpTimestampWrap) {
               Eq(VideoFrameType::kVideoFrameDelta));
 }
 
+TEST(LibaomAv1EncoderTest, TestCaptureTimeId) {
+  std::unique_ptr<VideoEncoder> encoder = CreateLibaomAv1Encoder();
+  const Timestamp capture_time_id = Timestamp::Micros(2000);
+  VideoCodec codec_settings = DefaultCodecSettings();
+  codec_settings.SetScalabilityMode(ScalabilityMode::kL2T1);
+  ASSERT_EQ(encoder->InitEncode(&codec_settings, DefaultEncoderSettings()),
+            WEBRTC_VIDEO_CODEC_OK);
+
+  VideoEncoder::RateControlParameters rate_parameters;
+  rate_parameters.framerate_fps = 30;
+  rate_parameters.bitrate.SetBitrate(/*spatial_index=*/0, /*temporal_index=*/0,
+                                     300'000);
+  rate_parameters.bitrate.SetBitrate(/*spatial_index=*/1, /*temporal_index=*/0,
+                                     300'000);
+  encoder->SetRates(rate_parameters);
+
+  std::vector<EncodedVideoFrameProducer::EncodedFrame> encoded_frames =
+      EncodedVideoFrameProducer(*encoder)
+          .SetNumInputFrames(1)
+          .SetCaptureTimeIdentifier(capture_time_id)
+          .Encode();
+  ASSERT_THAT(encoded_frames, SizeIs(2));
+  ASSERT_TRUE(
+      encoded_frames[0].encoded_image.CaptureTimeIdentifier().has_value());
+  ASSERT_TRUE(
+      encoded_frames[1].encoded_image.CaptureTimeIdentifier().has_value());
+  EXPECT_EQ(encoded_frames[0].encoded_image.CaptureTimeIdentifier()->us(),
+            capture_time_id.us());
+  EXPECT_EQ(encoded_frames[1].encoded_image.CaptureTimeIdentifier()->us(),
+            capture_time_id.us());
+}
+
 }  // namespace
 }  // namespace webrtc
