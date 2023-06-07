f2b7f33e9d80a4cde7346c37d24c425c0c01f9ca
Mike Hommey <mh+mozilla@glandium.org>
2023-06-07 07:28:31 +0000

    Bug 1749856 - Use GeneratedFile for rlbox.wasm.c. r=firefox-build-system-reviewers,andi
    This moves the creation of both rlbox.wasm.c and rlbox.wasm.h to
    pre-compile, which allows static-analysis to create both files (but more
    importantly the .h) without also compiling rlbox.wasm.c, which takes
    forever.
    
    Differential Revision: https://phabricator.services.mozilla.com/D180173

---
+++ config/wasm2c.py
@@ -0,0 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import subprocess
+
+
+def wasm2c(output, wasm2c_bin, wasm_lib):
+    output.close()
+    return subprocess.run(
+        [wasm2c_bin, "-o", output.name, "--disable-simd", wasm_lib]
+    ).returncode
