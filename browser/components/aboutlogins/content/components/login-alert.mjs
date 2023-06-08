2ccc657d20dcd7b32410f759f654dda0ac1842c5
Issam Mani <imani@mozilla.com>
2023-06-08 19:13:53 +0000

    Bug 1810214 - P4. Move styles to external css file. r=credential-management-reviewers,mtigley,desktop-theme-reviewers,dao
    Depends on D178795
    
    Differential Revision: https://phabricator.services.mozilla.com/D180146

--- browser/components/aboutlogins/content/components/login-alert.mjs
+++ browser/components/aboutlogins/content/components/login-alert.mjs
@@ -2,11 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {
-  html,
-  css,
-  ifDefined,
-} from "chrome://global/content/vendor/lit.all.mjs";
+import { html, ifDefined } from "chrome://global/content/vendor/lit.all.mjs";
 
 import { MozLitElement } from "chrome://global/content/lit-utils.mjs";
 
@@ -19,55 +15,13 @@ export class LoginAlert extends MozLitElement {
     };
   }
 
-  static styles = css`
-    :host {
-      display: grid;
-      column-gap: 16px;
-      grid-template-areas: "icon title action" "icon content content";
-      grid-template-columns: min-content 1fr auto;
-      padding: 16px 32px;
-      color: var(--in-content-text-color);
-      background: var(--in-content-box-background);
-      border-radius: 4px;
-      border: 1px solid var(--in-content-border-color);
-      box-shadow: 0 2px 8px 0 var(--grey-90-a10);
-    }
-
-    :host([variant="info"]) {
-      background: var(--in-content-box-background);
-    }
-
-    :host([variant="error"]) {
-      background: var(--red-70);
-    }
-
-    img {
-      grid-area: icon;
-      width: 24px;
-      -moz-context-properties: fill;
-      fill: currentColor;
-    }
-
-    h3 {
-      grid-area: title;
-      font-size: 22px;
-      font-weight: normal;
-      line-height: 1em;
-      margin: 0;
-      padding: 0;
-    }
-
-    slot[name="action"] {
-      grid-area: action;
-    }
-
-    slot[name="content"] {
-      grid-area: content;
-    }
-  `;
+  static stylesheetUrl = window.IS_STORYBOOK
+    ? "./login-alert.css"
+    : "chrome://browser/content/aboutlogins/components/login-alert.css";
 
   render() {
     return html`
+      <link rel="stylesheet" href=${this.constructor.stylesheetUrl} />
       <img src=${ifDefined(this.icon)} />
       <h3 data-l10n-id=${ifDefined(this.titleId)}></h3>
       <div>
@@ -86,20 +40,9 @@ export class LoginBreachAlert extends MozLitElement {
     };
   }
 
-  static styles = css`
-    div[slot="content"] {
-      margin-block-start: 8px;
-    }
-
-    h4 {
-      margin: 0;
-    }
-
-    a {
-      font-weight: 600;
-      color: inherit;
-    }
-  `;
+  static stylesheetUrl = window.IS_STORYBOOK
+    ? "./login-alert.css"
+    : "chrome://browser/content/aboutlogins/components/login-alert.css";
 
   constructor() {
     super();
@@ -109,6 +52,7 @@ export class LoginBreachAlert extends MozLitElement {
 
   render() {
     return html`
+      <link rel="stylesheet" href=${this.constructor.stylesheetUrl} />
       <login-alert
         variant="error"
         icon="chrome://browser/content/aboutlogins/icons/breached-website.svg"
