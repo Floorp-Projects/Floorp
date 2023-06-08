ab181396b829b6731f6b9408da6dd9f2ab291998
donal meehan <dmeehan@mozilla.com>
2023-06-08 15:30:26 -0400

    Backed out changeset c9fd0bb67278 (bug 1837201) for causing build bustage in beta

--- dom/quota/ActorsParent.cpp
+++ dom/quota/ActorsParent.cpp
@@ -4437,9 +4437,6 @@ Result<FullOriginMetadata, nsresult> QuotaManager::LoadFullOriginMetadata(
   PrincipalInfo principalInfo;
   QM_TRY(MOZ_TO_RESULT(PrincipalToPrincipalInfo(principal, &principalInfo)));
 
-  QM_TRY(MOZ_TO_RESULT(IsPrincipalInfoValid(principalInfo)),
-         Err(NS_ERROR_MALFORMED_URI));
-
   QM_TRY_UNWRAP(auto principalMetadata,
                 GetInfoFromValidatedPrincipalInfo(principalInfo));
 
@@ -4527,9 +4524,6 @@ Result<OriginMetadata, nsresult> QuotaManager::GetOriginMetadata(
   PrincipalInfo principalInfo;
   QM_TRY(MOZ_TO_RESULT(PrincipalToPrincipalInfo(principal, &principalInfo)));
 
-  QM_TRY(MOZ_TO_RESULT(IsPrincipalInfoValid(principalInfo)),
-         Err(NS_ERROR_MALFORMED_URI));
-
   QM_TRY_UNWRAP(auto principalMetadata,
                 GetInfoFromValidatedPrincipalInfo(principalInfo));
 
@@ -4600,28 +4594,8 @@ nsresult QuotaManager::InitializeRepository(PersistenceType aPersistenceType,
                     switch (dirEntryKind) {
                       case nsIFileKind::ExistsAsDirectory: {
                         QM_TRY_UNWRAP(
-                            auto maybeMetadata,
-                            QM_OR_ELSE_WARN_IF(
-                                // Expression
-                                LoadFullOriginMetadataWithRestore(
-                                    childDirectory)
-                                    .map([](auto metadata)
-                                             -> Maybe<FullOriginMetadata> {
-                                      return Some(std::move(metadata));
-                                    }),
-                                // Predicate.
-                                IsSpecificError<NS_ERROR_MALFORMED_URI>,
-                                // Fallback.
-                                ErrToDefaultOk<Maybe<FullOriginMetadata>>));
-
-                        if (!maybeMetadata) {
-                          // Unknown directories during initialization are
-                          // allowed. Just warn if we find them.
-                          UNKNOWN_FILE_WARNING(leafName);
-                          break;
-                        }
-
-                        auto metadata = maybeMetadata.extract();
+                            auto metadata,
+                            LoadFullOriginMetadataWithRestore(childDirectory));
 
                         MOZ_ASSERT(metadata.mPersistenceType ==
                                    aPersistenceType);
@@ -6576,8 +6550,7 @@ QuotaManager::GetInfoFromValidatedPrincipalInfo(
     }
 
     default: {
-      MOZ_ASSERT_UNREACHABLE("Should never get here!");
-      return Err(NS_ERROR_UNEXPECTED);
+      MOZ_CRASH("Should never get here!");
     }
   }
 }
@@ -8518,30 +8491,8 @@ nsresult GetUsageOp::ProcessOrigin(QuotaManager& aQuotaManager,
                                    const PersistenceType aPersistenceType) {
   AssertIsOnIOThread();
 
-  QM_TRY_UNWRAP(auto maybeMetadata,
-                QM_OR_ELSE_WARN_IF(
-                    // Expression
-                    aQuotaManager.LoadFullOriginMetadataWithRestore(&aOriginDir)
-                        .map([](auto metadata) -> Maybe<FullOriginMetadata> {
-                          return Some(std::move(metadata));
-                        }),
-                    // Predicate.
-                    IsSpecificError<NS_ERROR_MALFORMED_URI>,
-                    // Fallback.
-                    ErrToDefaultOk<Maybe<FullOriginMetadata>>));
-
-  if (!maybeMetadata) {
-    // Unknown directories during getting usage are allowed. Just warn if we
-    // find them.
-    QM_TRY_INSPECT(const auto& leafName,
-                   MOZ_TO_RESULT_INVOKE_MEMBER_TYPED(nsAutoString, aOriginDir,
-                                                     GetLeafName));
-
-    UNKNOWN_FILE_WARNING(leafName);
-    return NS_OK;
-  }
-
-  auto metadata = maybeMetadata.extract();
+  QM_TRY_INSPECT(const auto& metadata,
+                 aQuotaManager.LoadFullOriginMetadataWithRestore(&aOriginDir));
 
   QM_TRY_INSPECT(const auto& usageInfo,
                  GetUsageForOrigin(aQuotaManager, aPersistenceType, metadata));
@@ -9220,33 +9171,10 @@ void ClearRequestBase::DeleteFiles(QuotaManager& aQuotaManager,
            this](nsCOMPtr<nsIFile>&& file) -> mozilla::Result<Ok, nsresult> {
             QM_TRY_INSPECT(const auto& dirEntryKind, GetDirEntryKind(*file));
 
-            QM_TRY_INSPECT(const auto& leafName,
-                           MOZ_TO_RESULT_INVOKE_MEMBER_TYPED(nsAutoString, file,
-                                                             GetLeafName));
-
             switch (dirEntryKind) {
               case nsIFileKind::ExistsAsDirectory: {
-                QM_TRY_UNWRAP(
-                    auto maybeMetadata,
-                    QM_OR_ELSE_WARN_IF(
-                        // Expression
-                        aQuotaManager.GetOriginMetadata(file).map(
-                            [](auto metadata) -> Maybe<OriginMetadata> {
-                              return Some(std::move(metadata));
-                            }),
-                        // Predicate.
-                        IsSpecificError<NS_ERROR_MALFORMED_URI>,
-                        // Fallback.
-                        ErrToDefaultOk<Maybe<OriginMetadata>>));
-
-                if (!maybeMetadata) {
-                  // Unknown directories during clearing are allowed. Just warn
-                  // if we find them.
-                  UNKNOWN_FILE_WARNING(leafName);
-                  break;
-                }
-
-                auto metadata = maybeMetadata.extract();
+                QM_TRY_INSPECT(const auto& metadata,
+                               aQuotaManager.GetOriginMetadata(file));
 
                 MOZ_ASSERT(metadata.mPersistenceType == aPersistenceType);
 
@@ -9308,6 +9236,10 @@ void ClearRequestBase::DeleteFiles(QuotaManager& aQuotaManager,
               }
 
               case nsIFileKind::ExistsAsFile: {
+                QM_TRY_INSPECT(const auto& leafName,
+                               MOZ_TO_RESULT_INVOKE_MEMBER_TYPED(
+                                   nsAutoString, file, GetLeafName));
+
                 // Unknown files during clearing are allowed. Just warn if we
                 // find them.
                 if (!IsOSMetadata(leafName)) {
@@ -9764,30 +9696,7 @@ nsresult ListOriginsOp::ProcessOrigin(QuotaManager& aQuotaManager,
                                       const PersistenceType aPersistenceType) {
   AssertIsOnIOThread();
 
-  QM_TRY_UNWRAP(auto maybeMetadata,
-                QM_OR_ELSE_WARN_IF(
-                    // Expression
-                    aQuotaManager.GetOriginMetadata(&aOriginDir)
-                        .map([](auto metadata) -> Maybe<OriginMetadata> {
-                          return Some(std::move(metadata));
-                        }),
-                    // Predicate.
-                    IsSpecificError<NS_ERROR_MALFORMED_URI>,
-                    // Fallback.
-                    ErrToDefaultOk<Maybe<OriginMetadata>>));
-
-  if (!maybeMetadata) {
-    // Unknown directories during listing are allowed. Just warn if we find
-    // them.
-    QM_TRY_INSPECT(const auto& leafName,
-                   MOZ_TO_RESULT_INVOKE_MEMBER_TYPED(nsAutoString, aOriginDir,
-                                                     GetLeafName));
-
-    UNKNOWN_FILE_WARNING(leafName);
-    return NS_OK;
-  }
-
-  auto metadata = maybeMetadata.extract();
+  QM_TRY_UNWRAP(auto metadata, aQuotaManager.GetOriginMetadata(&aOriginDir));
 
   if (aQuotaManager.IsOriginInternal(metadata.mOrigin)) {
     return NS_OK;
@@ -9990,18 +9899,6 @@ nsresult StorageOperationBase::ProcessOriginDirectories() {
         QM_TRY(
             MOZ_TO_RESULT(PrincipalToPrincipalInfo(principal, &principalInfo)));
 
-        QM_WARNONLY_TRY_UNWRAP(
-            auto valid,
-            MOZ_TO_RESULT(quotaManager->IsPrincipalInfoValid(principalInfo)));
-
-        if (!valid) {
-          // Unknown directories during upgrade are allowed. Just warn if we
-          // find them.
-          UNKNOWN_FILE_WARNING(originProps.mLeafName);
-          originProps.mIgnore = true;
-          break;
-        }
-
         QM_TRY_UNWRAP(
             auto principalMetadata,
             quotaManager->GetInfoFromValidatedPrincipalInfo(principalInfo));
@@ -10031,7 +9928,7 @@ nsresult StorageOperationBase::ProcessOriginDirectories() {
       MOZ_ASSERT(originProps.mOriginMetadata.mOrigin.IsEmpty());
 
       QM_TRY(MOZ_TO_RESULT(RemoveObsoleteOrigin(originProps)));
-    } else if (!originProps.mIgnore) {
+    } else {
       MOZ_ASSERT(!originProps.mOriginMetadata.mGroup.IsEmpty());
       MOZ_ASSERT(!originProps.mOriginMetadata.mOrigin.IsEmpty());
 
