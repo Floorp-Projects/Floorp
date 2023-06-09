713e9abcffa98dc16682fed909bc0ac1c12eff68
Mike Hommey <mh+mozilla@glandium.org>
2023-06-09 20:30:29 +0000

    Bug 1837353 - Don't build the mozannotation crates when the crash reporter is disabled. r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D180395

--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -92,7 +92,9 @@ extern crate gecko_logger;
 #[cfg(feature = "oxidized_breakpad")]
 extern crate rust_minidump_writer_linux;
 
+#[cfg(feature = "crashreporter")]
 extern crate mozannotation_client;
+#[cfg(feature = "crashreporter")]
 extern crate mozannotation_server;
 
 #[cfg(feature = "webmidi_midir_impl")]
