19424d7e831b558a42cbf67f433b639e4dcbe42c
Marco Bonardo <mbonardo@mozilla.com>
2023-06-09 07:56:19 +0000

    Bug 1837170 - Add Nimbus variables to control alternative pages frecency. r=daisuke
    Differential Revision: https://phabricator.services.mozilla.com/D180211

--- browser/components/urlbar/tests/unit/test_frecency_alternative_nimbus.js
+++ browser/components/urlbar/tests/unit/test_frecency_alternative_nimbus.js
@@ -1,10 +1,35 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-const ORIGINS_FEATUREGATE = "places.frecency.origins.alternative.featureGate";
-const ORIGINS_DAYSCUTOFF = "places.frecency.origins.alternative.daysCutOff";
-
 add_task(async function () {
+  const tests = [
+    {
+      enableVariable: "originsAlternativeEnable",
+      enablePref: "places.frecency.origins.alternative.featureGate",
+      variables: {
+        originsDaysCutOff: "places.frecency.origins.alternative.daysCutOff",
+      },
+    },
+    {
+      enableVariable: "pagesAlternativeEnable",
+      enablePref: "places.frecency.pages.alternative.featureGate",
+      variables: {
+        pagesNumSampledVisits:
+          "places.frecency.pages.alternative.numSampledVisits",
+        pagesHalfLifeDays: "places.frecency.pages.alternative.halfLifeDays",
+        pagesHighWeight: "places.frecency.pages.alternative.highWeight",
+        pagesMediumWeight: "places.frecency.pages.alternative.mediumWeight",
+        pagesLowWeight: "places.frecency.pages.alternative.lowWeight",
+      },
+    },
+  ];
+  for (let test of tests) {
+    await doTest(test.enableVariable, test.enablePref, test.variables);
+  }
+});
+
+async function doTest(enableVariable, enablePref, otherVariables) {
+  info(`Testing ${enableVariable}`);
   let reset = await UrlbarTestUtils.initNimbusFeature(
     {
       // Empty for sanity check.
@@ -12,39 +37,41 @@ add_task(async function () {
     "frecency",
     "config"
   );
-  Assert.equal(Services.prefs.getBoolPref(ORIGINS_FEATUREGATE), false);
-  Assert.throws(
-    () => Services.prefs.getIntPref(ORIGINS_DAYSCUTOFF),
-    /NS_ERROR_UNEXPECTED/
-  );
+  Assert.ok(!Services.prefs.prefHasUserValue(enablePref));
+  Assert.ok(!Services.prefs.getBoolPref(enablePref, false));
+  for (let pref of Object.values(otherVariables)) {
+    Assert.ok(!Services.prefs.prefHasUserValue(pref));
+  }
   await reset();
 
   reset = await UrlbarTestUtils.initNimbusFeature(
     {
-      originsAlternativeEnable: true,
+      [enableVariable]: true,
     },
     "frecency",
     "config"
   );
-  Assert.equal(Services.prefs.getBoolPref(ORIGINS_FEATUREGATE), true);
-  Assert.ok(Services.prefs.prefHasUserValue(ORIGINS_FEATUREGATE));
-  Assert.throws(
-    () => Services.prefs.getIntPref(ORIGINS_DAYSCUTOFF),
-    /NS_ERROR_UNEXPECTED/
-  );
+  Assert.ok(Services.prefs.prefHasUserValue(enablePref));
+  Assert.equal(Services.prefs.getBoolPref(enablePref), true);
+  for (let pref of Object.values(otherVariables)) {
+    Assert.ok(!Services.prefs.prefHasUserValue(pref));
+  }
   await reset();
 
-  reset = await UrlbarTestUtils.initNimbusFeature(
-    {
-      originsAlternativeEnable: true,
-      originsDaysCutOff: 60,
-    },
-    "frecency",
-    "config"
-  );
-  Assert.equal(Services.prefs.getBoolPref(ORIGINS_FEATUREGATE), true);
-  Assert.ok(Services.prefs.prefHasUserValue(ORIGINS_FEATUREGATE));
-  Assert.equal(Services.prefs.getIntPref(ORIGINS_DAYSCUTOFF, 90), 60);
-  Assert.ok(Services.prefs.prefHasUserValue(ORIGINS_DAYSCUTOFF));
+  const FAKE_VALUE = 777;
+  let config = {
+    [enableVariable]: true,
+  };
+  for (let variable of Object.keys(otherVariables)) {
+    config[variable] = FAKE_VALUE;
+  }
+  reset = await UrlbarTestUtils.initNimbusFeature(config, "frecency", "config");
+  Assert.ok(Services.prefs.prefHasUserValue(enablePref));
+  Assert.equal(Services.prefs.getBoolPref(enablePref), true);
+  for (let pref of Object.values(otherVariables)) {
+    Assert.ok(Services.prefs.prefHasUserValue(pref));
+    Assert.equal(Services.prefs.getIntPref(pref, 90), FAKE_VALUE);
+  }
+
   await reset();
-});
+}
