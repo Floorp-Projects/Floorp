5780ef129b8e733661f2f32ee67f4ad8747f1c67
Tom Schuster <tschuster@mozilla.com>
2023-06-09 07:52:50 +0000

    Bug 1409200 - Implement CSP-3 support for hashes matching external resources with an integrity attribute. r=freddyb
    Differential Revision: https://phabricator.services.mozilla.com/D179824

--- dom/security/nsCSPContext.h
+++ dom/security/nsCSPContext.h
@@ -154,11 +154,10 @@ class nsCSPContext : public nsIContentSecurityPolicy {
   bool permitsInternal(CSPDirective aDir,
                        mozilla::dom::Element* aTriggeringElement,
                        nsICSPEventListener* aCSPEventListener,
-                       nsIURI* aContentLocation, nsIURI* aOriginalURIIfRedirect,
-                       const nsAString& aNonce, bool aSpecific,
+                       nsILoadInfo* aLoadInfo, nsIURI* aContentLocation,
+                       nsIURI* aOriginalURIIfRedirect, bool aSpecific,
                        bool aSendViolationReports,
-                       bool aSendContentLocationInViolationReports,
-                       bool aParserCreated);
+                       bool aSendContentLocationInViolationReports);
 
   // helper to report inline script/style violations
   void reportInlineViolation(CSPDirective aDirective,
