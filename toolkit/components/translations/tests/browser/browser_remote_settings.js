4289ee65a2f35b9d17c2328f905b6f94bfae1173
Erik Nordin <enordin@mozilla.com>
2023-06-09 15:56:03 +0000

    Bug 1837389 - Update failing CI test case r=gregtatum a=test-only
    Fixes a test that works for firefox versions <= the
    major version, but fails when bumping the minor version.
    This was a logic error in the test that was not exposed
    until a minor version bump.
    
    Differential Revision: https://phabricator.services.mozilla.com/D180357

--- toolkit/components/translations/tests/browser/browser_remote_settings.js
+++ toolkit/components/translations/tests/browser/browser_remote_settings.js
@@ -13,7 +13,7 @@ const { RemoteSettings } = ChromeUtils.import(
 );
 
 // The full Firefox version string.
-const firefoxFullVersion = AppConstants.MOZ_APP_VERSION;
+const firefoxFullVersion = AppConstants.MOZ_APP_VERSION_DISPLAY;
 
 // The Firefox major version string (i.e. the first set of digits).
 const firefoxMajorVersion = firefoxFullVersion.match(/\d+/);
@@ -77,8 +77,8 @@ add_task(async function test_filter_current_firefox_version() {
       filter_expression: ``,
     },
     {
-      name: `env.version <= ${firefoxMajorVersion}`,
-      filter_expression: `env.version|versionCompare('${firefoxMajorVersion}') <= 0`,
+      name: `env.version == ${firefoxFullVersion}`,
+      filter_expression: `env.version|versionCompare('${firefoxFullVersion}') == 0`,
     },
     {
       name: `env.version > ${firefoxAlphaZeroVersion}`,
