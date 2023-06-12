39aed19ed8316a8fe8cf93f2b2360a8ec5131f10
Alexandre Poirot <poirot.alex@gmail.com>
2023-06-12 10:01:58 +0000

    Bug 1837048 - [devtools] Remove unused component-panel debugger preference. r=devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D180194

--- devtools/client/debugger/src/utils/prefs.js
+++ devtools/client/debugger/src/utils/prefs.js
@@ -49,7 +49,6 @@ if (isNode()) {
   pref("devtools.debugger.features.wasm", true);
   pref("devtools.debugger.features.map-scopes", true);
   pref("devtools.debugger.features.code-folding", false);
-  pref("devtools.debugger.features.component-pane", false);
   pref("devtools.debugger.features.autocomplete-expressions", false);
   pref("devtools.debugger.features.map-expression-bindings", true);
   pref("devtools.debugger.features.map-await-expression", true);
@@ -124,7 +123,6 @@ export const features = new PrefsHelper("devtools.debugger.features", {
   autocompleteExpression: ["Bool", "autocomplete-expressions"],
   mapExpressionBindings: ["Bool", "map-expression-bindings"],
   mapAwaitExpression: ["Bool", "map-await-expression"],
-  componentPane: ["Bool", "component-pane"],
   logPoints: ["Bool", "log-points"],
   inlinePreview: ["Bool", "inline-preview"],
   windowlessServiceWorkers: ["Bool", "windowless-service-workers"],
