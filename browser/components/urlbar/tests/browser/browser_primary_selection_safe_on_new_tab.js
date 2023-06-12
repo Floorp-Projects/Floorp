4e1a5acabc562f7e3ca8df96d32366eae41c2594
Marc Seibert <mseibert@mozilla.com>
2023-06-12 13:43:39 +0000

    Bug 1833091 - Replacing all get refs of UrlbarInput 'inputField.value' with '.value'.r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D180085

--- browser/components/urlbar/tests/browser/browser_primary_selection_safe_on_new_tab.js
+++ browser/components/urlbar/tests/browser/browser_primary_selection_safe_on_new_tab.js
@@ -24,11 +24,8 @@ add_task(async function () {
 
   // Bug 1457355 reproduced only when the url had a non-empty selection.
   gURLBar.select();
-  Assert.equal(gURLBar.inputField.selectionStart, 0);
-  Assert.equal(
-    gURLBar.inputField.selectionEnd,
-    gURLBar.inputField.value.length
-  );
+  Assert.equal(gURLBar.selectionStart, 0);
+  Assert.equal(gURLBar.selectionEnd, gURLBar.value.length);
 
   if (supportsPrimary) {
     clipboardHelper.copyStringToClipboard(
