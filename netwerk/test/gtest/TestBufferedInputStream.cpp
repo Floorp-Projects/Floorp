84040b0a914d46d69dea6a12d1e8851581517a80
Valentin Gosu <valentin.gosu@gmail.com>
2023-06-12 17:08:54 +0000

    Bug 1837399 - Propagate stream error through nsBufferedInputStream::ReadSegments r=necko-reviewers,kershaw, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D180322

--- netwerk/test/gtest/TestBufferedInputStream.cpp
+++ netwerk/test/gtest/TestBufferedInputStream.cpp
@@ -2,8 +2,9 @@
 
 #include "mozilla/SpinEventLoopUntil.h"
 #include "nsBufferedStreams.h"
-#include "nsStreamUtils.h"
 #include "nsIThread.h"
+#include "nsNetUtil.h"
+#include "nsStreamUtils.h"
 #include "nsThreadUtils.h"
 #include "Helpers.h"
 
@@ -192,4 +193,60 @@ TEST(TestBufferedInputStream, AsyncLengthWait_after_close)
       "TEST(TestBufferedInputStream, AsyncLengthWait_after_close) 2"_ns,
       [&]() { return cb->Called(); }));
   ASSERT_TRUE(cb->Called());
+}
+
+// This stream returns a few bytes on the first read, and error on the second.
+class BrokenInputStream : public nsIInputStream {
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSIINPUTSTREAM
+ private:
+  virtual ~BrokenInputStream() = default;
+  bool mFirst = true;
+};
+
+NS_IMPL_ISUPPORTS(BrokenInputStream, nsIInputStream)
+
+NS_IMETHODIMP BrokenInputStream::Close(void) { return NS_OK; }
+
+NS_IMETHODIMP BrokenInputStream::Available(uint64_t* _retval) {
+  *_retval = 100;
+  return NS_OK;
+}
+
+NS_IMETHODIMP BrokenInputStream::StreamStatus(void) { return NS_OK; }
+
+NS_IMETHODIMP BrokenInputStream::Read(char* aBuf, uint32_t aCount,
+                                      uint32_t* _retval) {
+  if (mFirst) {
+    aBuf[0] = 'h';
+    aBuf[1] = 'e';
+    aBuf[2] = 'l';
+    aBuf[3] = 0;
+    *_retval = 4;
+    mFirst = false;
+    return NS_OK;
+  }
+  return NS_ERROR_CORRUPTED_CONTENT;
+}
+
+NS_IMETHODIMP BrokenInputStream::ReadSegments(nsWriteSegmentFun aWriter,
+                                              void* aClosure, uint32_t aCount,
+                                              uint32_t* _retval) {
+  return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+NS_IMETHODIMP BrokenInputStream::IsNonBlocking(bool* _retval) {
+  *_retval = false;
+  return NS_OK;
+}
+
+// Check that the error from BrokenInputStream::Read is propagated
+// through NS_ReadInputStreamToString
+TEST(TestBufferedInputStream, BrokenInputStreamToBuffer)
+{
+  nsAutoCString out;
+  RefPtr<BrokenInputStream> stream = new BrokenInputStream();
+
+  nsresult rv = NS_ReadInputStreamToString(stream, out, -1);
+  ASSERT_EQ(rv, NS_ERROR_CORRUPTED_CONTENT);
 }
