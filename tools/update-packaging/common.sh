88a18ca7f39caa1570080f4fd8438e83e8a25c9b
Julien Cristau <jcristau@mozilla.com>
2023-06-12 13:14:53 +0000

    Bug 1837939 - update-packaging: quote filenames that can contain spaces. r=releng-reviewers,bhearsum DONTBUILD
    Bug 1837329 added a new "Media Plugin Helper" that caused errors in repack logs
    (though as far as I can tell had no effect on the mar files themselves).
    
    Differential Revision: https://phabricator.services.mozilla.com/D180669

--- tools/update-packaging/common.sh
+++ tools/update-packaging/common.sh
@@ -92,8 +92,8 @@ make_add_instruction() {
 check_for_add_if_not_update() {
   add_if_not_file_chk="$1"
 
-  if [ `basename $add_if_not_file_chk` = "channel-prefs.js" -o \
-       `basename $add_if_not_file_chk` = "update-settings.ini" ]; then
+  if [ "$(basename "$add_if_not_file_chk")" = "channel-prefs.js" -o \
+       "$(basename "$add_if_not_file_chk")" = "update-settings.ini" ]; then
     ## "true" *giggle*
     return 0;
   fi
