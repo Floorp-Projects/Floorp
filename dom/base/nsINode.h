ba793505a20e48258f29802b1b5c6909db0b4c50
David Shin <dshin@mozilla.com>
2023-06-13 13:21:42 +0000

    Bug 1835037: Move `ANCHORS_RELATIVE_SELECTOR` out of nsINode flags. r=emilio
    Move the flag to ComputedValueFlags, like `CONSIDERED_RELATIVE_SELECTOR`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D180726

--- dom/base/nsINode.h
+++ dom/base/nsINode.h
@@ -178,40 +178,37 @@ enum : uint32_t {
       NODE_HAS_EDGE_CHILD_SELECTOR | NODE_HAS_SLOW_SELECTOR_LATER_SIBLINGS |
       NODE_HAS_SLOW_SELECTOR_NTH_OF,
 
-  // Set if this node tried anchoring a relative selector.
-  NODE_ANCHORS_RELATIVE_SELECTOR = NODE_FLAG_BIT(11),
-
   // This node needs to go through frame construction to get a frame (or
   // undisplayed entry).
-  NODE_NEEDS_FRAME = NODE_FLAG_BIT(12),
+  NODE_NEEDS_FRAME = NODE_FLAG_BIT(11),
 
   // At least one descendant in the flattened tree has NODE_NEEDS_FRAME set.
   // This should be set on every node on the flattened tree path between the
   // node(s) with NODE_NEEDS_FRAME and the root content.
-  NODE_DESCENDANTS_NEED_FRAMES = NODE_FLAG_BIT(13),
+  NODE_DESCENDANTS_NEED_FRAMES = NODE_FLAG_BIT(12),
 
   // Set if the node has the accesskey attribute set.
-  NODE_HAS_ACCESSKEY = NODE_FLAG_BIT(14),
+  NODE_HAS_ACCESSKEY = NODE_FLAG_BIT(13),
 
   // Set if the node has right-to-left directionality
-  NODE_HAS_DIRECTION_RTL = NODE_FLAG_BIT(15),
+  NODE_HAS_DIRECTION_RTL = NODE_FLAG_BIT(14),
 
   // Set if the node has left-to-right directionality
-  NODE_HAS_DIRECTION_LTR = NODE_FLAG_BIT(16),
+  NODE_HAS_DIRECTION_LTR = NODE_FLAG_BIT(15),
 
   NODE_ALL_DIRECTION_FLAGS = NODE_HAS_DIRECTION_LTR | NODE_HAS_DIRECTION_RTL,
 
-  NODE_HAS_BEEN_IN_UA_WIDGET = NODE_FLAG_BIT(17),
+  NODE_HAS_BEEN_IN_UA_WIDGET = NODE_FLAG_BIT(16),
 
   // Set if the node has a nonce value and a header delivered CSP.
-  NODE_HAS_NONCE_AND_HEADER_CSP = NODE_FLAG_BIT(18),
+  NODE_HAS_NONCE_AND_HEADER_CSP = NODE_FLAG_BIT(17),
 
-  NODE_KEEPS_DOMARENA = NODE_FLAG_BIT(19),
+  NODE_KEEPS_DOMARENA = NODE_FLAG_BIT(18),
 
-  NODE_MAY_HAVE_ELEMENT_CHILDREN = NODE_FLAG_BIT(20),
+  NODE_MAY_HAVE_ELEMENT_CHILDREN = NODE_FLAG_BIT(19),
 
   // Remaining bits are node type specific.
-  NODE_TYPE_SPECIFIC_BITS_OFFSET = 21
+  NODE_TYPE_SPECIFIC_BITS_OFFSET = 20
 };
 
 // Make sure we have space for our bits
