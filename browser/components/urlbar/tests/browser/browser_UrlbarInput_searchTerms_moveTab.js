312ab832d95e35e51152e885ab7df66b946c5520
James Teow <jteow@mozilla.com>
2023-06-14 20:42:52 +0000

    Bug 1801334 - Disable the persisted search tip in the move window test - r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D180761

--- browser/components/urlbar/tests/browser/browser_UrlbarInput_searchTerms_moveTab.js
+++ browser/components/urlbar/tests/browser/browser_UrlbarInput_searchTerms_moveTab.js
@@ -19,7 +19,10 @@ const SEARCH_STRING = "chocolate cake";
 
 add_setup(async function () {
   await SpecialPowers.pushPrefEnv({
-    set: [["browser.urlbar.showSearchTerms.featureGate", true]],
+    set: [
+      ["browser.urlbar.showSearchTerms.featureGate", true],
+      ["browser.urlbar.tipShownCount.searchTip_persist", 999],
+    ],
   });
 
   await SearchTestUtils.installSearchExtension({
