d463f82752a03427de0f4f16f707fd33ecb54689
James Teh <jteh@mozilla.com>
2023-06-14 03:31:45 +0000

    Bug 1838151: Remove unused a11y::filters::GetRow/GetCell. r=nlapre
    Differential Revision: https://phabricator.services.mozilla.com/D180784

--- accessible/base/Filters.cpp
+++ accessible/base/Filters.cpp
@@ -25,21 +25,3 @@ uint32_t filters::GetSelectable(LocalAccessible* aAccessible) {
 
   return eSkip;
 }
-
-uint32_t filters::GetRow(LocalAccessible* aAccessible) {
-  if (aAccessible->IsTableRow()) return eMatch | eSkipSubtree;
-
-  // Look for rows inside rowgroup or wrapping text containers.
-  a11y::role role = aAccessible->Role();
-  const nsRoleMapEntry* roleMapEntry = aAccessible->ARIARoleMap();
-  if (role == roles::GROUPING ||
-      (aAccessible->IsGenericHyperText() && !roleMapEntry)) {
-    return eSkip;
-  }
-
-  return eSkipSubtree;
-}
-
-uint32_t filters::GetCell(LocalAccessible* aAccessible) {
-  return aAccessible->IsTableCell() ? eMatch : eSkipSubtree;
-}
