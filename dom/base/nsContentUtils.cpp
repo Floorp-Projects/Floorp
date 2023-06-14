1ef8bc1220a5702bf97d12a8d0293f0a957b7af4
Tom Schuster <tschuster@mozilla.com>
2023-06-14 13:34:59 +0000

    Bug 1837153 - Make RFPTarget parameter of ShouldResistFingerprinting with nsILoadInfo non-optional. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D180431

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -2364,8 +2364,8 @@ bool nsContentUtils::ShouldResistFingerprinting_dangerous(
 }
 
 /* static */
-bool nsContentUtils::ShouldResistFingerprinting(
-    nsILoadInfo* aLoadInfo, RFPTarget aTarget /* = RFPTarget::Unknown */) {
+bool nsContentUtils::ShouldResistFingerprinting(nsILoadInfo* aLoadInfo,
+                                                RFPTarget aTarget) {
   MOZ_ASSERT(aLoadInfo->GetExternalContentPolicyType() !=
                  ExtContentPolicy::TYPE_DOCUMENT &&
              aLoadInfo->GetExternalContentPolicyType() !=
