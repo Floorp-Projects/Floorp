e09d67278ef221e1ceb128aea73e70376ca6d515
Aaron Train <aaron.train@gmail.com>
2023-06-14 11:02:25 -0400

    Bug 1838358 - Create a startup UI test task for Focus

--- mobile/android/taskcluster/android_taskgraph/transforms/push_apk.py
+++ mobile/android/taskcluster/android_taskgraph/transforms/push_apk.py
@@ -43,4 +43,7 @@ def add_startup_test(config, tasks):
             yield task
             continue
         task["dependencies"]["startup-test"] = "startup-test-nightly-arm"
+        for dep_label, dep_task in config.kind_dependencies_tasks.items():
+            if dep_task.kind == "startup-test" and dep_task.attributes['shipping-product'] == task['attributes']['shipping-product']:
+                task["dependencies"]["startup-test"] = dep_label
         yield task
