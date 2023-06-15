52b9a5e94370db452a0de6855750d241fa411904
Cristian Tuns <ctuns@mozilla.com>
2023-06-14 23:37:36 -0400

    Backed out 8 changesets (bug 1830070) for causing build bustages in nsContentUtils.cpp CLOSED TREE
    Backed out changeset 4726b70822cf (bug 1830070)
    Backed out changeset dd73f3c56f5e (bug 1830070)
    Backed out changeset 3628e8b8d28a (bug 1830070)
    Backed out changeset dfbd51c0a3a6 (bug 1830070)
    Backed out changeset 3d372cedcfdb (bug 1830070)
    Backed out changeset 443d83b8fe70 (bug 1830070)
    Backed out changeset 3ed6c302f108 (bug 1830070)
    Backed out changeset 919b884d5a2b (bug 1830070)

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -2166,7 +2166,6 @@ bool nsContentUtils::ShouldResistFingerprinting(nsIGlobalObject* aGlobalObject,
 }
 
 // Newer Should RFP Functions ----------------------------------
-// Utilities ---------------------------------------------------
 
 inline void LogDomainAndPrefList(const char* exemptedDomainsPrefName,
                                  nsAutoCString& url, bool isExemptDomain) {
@@ -2201,27 +2200,8 @@ inline bool CookieJarSettingsSaysShouldResistFingerprinting(
   return cookieJarSettings->GetShouldResistFingerprinting();
 }
 
-inline bool SchemeSaysShouldNotResistFingerprinting(nsIURI* aURI) {
-  return aURI->SchemeIs("chrome") || aURI->SchemeIs("resource") ||
-         aURI->SchemeIs("view-source") || aURI->SchemeIs("moz-extension") ||
-         (aURI->SchemeIs("about") && !NS_IsContentAccessibleAboutURI(aURI));
-}
-
-inline bool SchemeSaysShouldNotResistFingerprinting(nsIPrincipal* aPrincipal) {
-  nsCOMPtr<nsIURI> prinURI;
-  Unused << aPrincipal->GetURI(getter_AddRefs(prinURI));
-
-  return aPrincipal->SchemeIs("chrome") || aPrincipal->SchemeIs("resource") ||
-         aPrincipal->SchemeIs("view-source") ||
-         aPrincipal->SchemeIs("moz-extension") ||
-         (prinURI->SchemeIs("about") &&
-          !NS_IsContentAccessibleAboutURI(prinURI));
-}
-
 const char* kExemptedDomainsPrefName =
     "privacy.resistFingerprinting.exemptedDomains";
-
-// Functions ---------------------------------------------------
 
 /* static */
 bool nsContentUtils::ShouldResistFingerprinting(const char* aJustification,
@@ -2360,7 +2340,9 @@ bool nsContentUtils::ShouldResistFingerprinting_dangerous(
   }
 
   // Exclude internal schemes and web extensions
-  if (SchemeSaysShouldNotResistFingerprinting(aURI)) {
+  if (aURI->SchemeIs("about") || aURI->SchemeIs("chrome") ||
+      aURI->SchemeIs("resource") || aURI->SchemeIs("view-source") ||
+      aURI->SchemeIs("moz-extension")) {
     return false;
   }
 
@@ -2441,8 +2423,9 @@ bool nsContentUtils::ShouldResistFingerprinting_dangerous(
     }
   }
 
-  // Exclude internal schemes and web extensions
-  if (SchemeSaysShouldNotResistFingerprinting(aPrincipal)) {
+  // Exclude internal schemes
+  if (aPrincipal->SchemeIs("about") || aPrincipal->SchemeIs("chrome") ||
+      aPrincipal->SchemeIs("resource") || aPrincipal->SchemeIs("view-source")) {
     return false;
   }
 
@@ -2470,6 +2453,7 @@ bool nsContentUtils::ShouldResistFingerprinting_dangerous(
   // So perform this last-ditch check for that scenario.
   // We arbitrarily use https as the scheme, but it doesn't matter.
   nsCOMPtr<nsIURI> uri;
+  nsresult rv;
   if (isExemptDomain && StaticPrefs::privacy_firstparty_isolate() &&
       !originAttributes.mFirstPartyDomain.IsEmpty()) {
     rv = NS_NewURI(getter_AddRefs(uri),
@@ -2489,8 +2473,6 @@ bool nsContentUtils::ShouldResistFingerprinting_dangerous(
 
   return !isExemptDomain;
 }
-
-// --------------------------------------------------------------------
 
 /* static */
 void nsContentUtils::CalcRoundedWindowSizeForResistingFingerprinting(
