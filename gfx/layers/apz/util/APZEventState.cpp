c541bfe106604a44c9ea811667c75bdcbf300146
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2023-06-15 11:43:44 +0000

    Bug 1829967 - Fire a touchcanel event when a context menu opens rather than the context menu doesn't open. r=botond
    The modified test, helper_long_tap.html, is mostly now same as the Android one.
    A difference is on the Linux/Mac version we observe a mouselongtap event after a
    contextmenu event wasn't preventDefaulted.
    
    Depends on D180636
    
    Differential Revision: https://phabricator.services.mozilla.com/D180637

--- gfx/layers/apz/util/APZEventState.cpp
+++ gfx/layers/apz/util/APZEventState.cpp
@@ -295,26 +295,29 @@ void APZEventState::ProcessLongTap(PresShell* aPresShell,
   mContentReceivedInputBlockCallback(
       aInputBlockId, preventDefaultResult != PreventDefaultResult::No);
 
-  const bool eventHandled =
-#ifdef MOZ_WIDGET_ANDROID
+  const bool contextmenuOpen =
+#ifdef XP_WIN
+      // On Windows context menu will never be opened by long tap events, the
+      // menu will open after the user lifts their finger.
+      false;
+#elif defined(MOZ_WIDGET_ANDROID)
       // On Android, GeckoView calls preventDefault() in a JSActor
       // (ContentDelegateChild.jsm) when opening context menu so that we can
       // tell whether contextmenu opens in response to the contextmenu event by
       // checking where preventDefault() got called.
       preventDefaultResult == PreventDefaultResult::ByChrome;
 #else
-      // Unfortunately on desktop platforms other than Windows we can't use
-      // the same approach for Android since we no longer call preventDefault()
-      // since bug 1558506. So for now, we keep the current behavior that is
-      // sending a touchcancel event if the contextmenu event was
-      // preventDefault-ed in an event handler in the content itself.
-      preventDefaultResult == PreventDefaultResult::ByContent;
+      // On desktop platforms (other than Windows) unlike Android, context menu
+      // can be opened anywhere even if, for example, there's no link under the
+      // touch point. So we can assume that "not preventDefault" means a context
+      // menu is open.
+      preventDefaultResult == PreventDefaultResult::No;
 #endif
-  if (eventHandled) {
+  if (contextmenuOpen) {
     // Also send a touchcancel to content
     //  a) on Android if browser's contextmenu is open
-    //  b) on desktop platforms other than Windows if preventDefault() was
-    //     called for the contextmenu event
+    //  b) on desktop platforms other than Windows if browser's contextmenu is
+    //     open
     // so that listeners that might be waiting for a touchend don't trigger.
     WidgetTouchEvent cancelTouchEvent(true, eTouchCancel, widget.get());
     cancelTouchEvent.mModifiers = aModifiers;
