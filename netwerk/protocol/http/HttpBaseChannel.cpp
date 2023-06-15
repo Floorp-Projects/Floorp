00c97398d95d7d1c57365d39a29cb29ad1e52543
Sean Feng <sefeng@mozilla.com>
2023-06-15 13:07:43 +0000

    Bug 1837585 - Make the COEP:Credentialless check ignores the subdocument request if it's a redirect r=valentin,necko-reviewers, a=dmeehan
    We shouldn't block the iframe if the initial request doesn't have valid
    headers, we should only check the final request.
    
    Differential Revision: https://phabricator.services.mozilla.com/D180485

--- netwerk/protocol/http/HttpBaseChannel.cpp
+++ netwerk/protocol/http/HttpBaseChannel.cpp
@@ -2428,6 +2428,7 @@ nsresult HttpBaseChannel::ProcessCrossOriginEmbedderPolicyHeader() {
   // https://html.spec.whatwg.org/multipage/origin.html#coep
   if (mLoadInfo->GetExternalContentPolicyType() ==
           ExtContentPolicy::TYPE_SUBDOCUMENT &&
+      !nsHttpChannel::IsRedirectStatus(mResponseHead->Status()) &&
       mLoadInfo->GetLoadingEmbedderPolicy() !=
           nsILoadInfo::EMBEDDER_POLICY_NULL &&
       resultPolicy != nsILoadInfo::EMBEDDER_POLICY_REQUIRE_CORP &&
