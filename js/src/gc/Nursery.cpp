fdf77e13bd05e57d32b13a716c0fc719f2e8710e
Steve Fink <sfink@mozilla.com>
2023-06-15 19:56:47 +0000

    Bug 1837408 - Skip atoms zone when updating nursery flags r=jandem a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D180403

--- js/src/gc/Nursery.cpp
+++ js/src/gc/Nursery.cpp
@@ -367,7 +367,10 @@ void js::Nursery::disable() {
   position_ = 0;
   gc->storeBuffer().disable();
 
-  updateAllZoneAllocFlags();
+  if (gc->wasInitialized()) {
+    // This assumes there is an atoms zone.
+    updateAllZoneAllocFlags();
+  }
 }
 
 void js::Nursery::enableStrings() {
@@ -399,7 +402,9 @@ void js::Nursery::disableBigInts() {
 }
 
 void js::Nursery::updateAllZoneAllocFlags() {
-  for (AllZonesIter zone(gc); !zone.done(); zone.next()) {
+  // The alloc flags are not relevant for the atoms zone, and flushing
+  // jit-related information can be problematic for the atoms zone.
+  for (ZonesIter zone(gc, SkipAtoms); !zone.done(); zone.next()) {
     updateAllocFlagsForZone(zone);
   }
 }
