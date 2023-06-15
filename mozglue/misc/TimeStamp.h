b71210a27e5421984300e4335bbe137c5654bae7
Justin Link <jlink@mozilla.com>
2023-06-15 14:59:10 +0000

    Bug 1829983 - Disabled assert that's invalid on Windows r=dthayer
    Differential Revision: https://phabricator.services.mozilla.com/D180882

--- mozglue/misc/TimeStamp.h
+++ mozglue/misc/TimeStamp.h
@@ -369,6 +369,20 @@ typedef BaseTimeDuration<TimeDurationValueCalculator> TimeDuration;
  * Note that, since TimeStamp objects are small, prefer to pass them by value
  * unless there is a specific reason not to do so.
  */
+#if defined(XP_WIN)
+// If this static_assert fails then possibly the warning comment below is no
+// longer valid and should be removed.
+static_assert(sizeof(TimeStampValue) > 8);
+#endif
+/*
+ * WARNING: On Windows, each TimeStamp is represented internally by two
+ * different raw values (one from GTC and one from QPC) and which value gets
+ * used for a given operation depends on whether both operands have QPC values
+ * or not. This duality of values can lead to some surprising results when
+ * mixing TimeStamps with and without QPC values, such as comparisons being
+ * non-transitive (ie, a > b > c might not imply a > c). See bug 1829983 for
+ * more details/an example.
+ */
 class TimeStamp {
  public:
   /**
