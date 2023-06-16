e2ab4b228fedfebe04aaa4de59017b27e303e786
Kelsey Gilbert <kelsey.gilbert@mozilla.com>
2023-06-16 18:16:28 +0000

    Bug 1838739 - Initialize result of SetAsGPUOutOfMemoryError. r=webgpu-reviewers,nical
    Differential Revision: https://phabricator.services.mozilla.com/D181161

--- dom/webgpu/OutOfMemoryError.h
+++ dom/webgpu/OutOfMemoryError.h
@@ -20,13 +20,13 @@ class OutOfMemoryError final : public nsWrapperCache, public ChildOf<Device> {
  public:
   GPU_DECL_CYCLE_COLLECTION(OutOfMemoryError)
   GPU_DECL_JS_WRAP(OutOfMemoryError)
-  OutOfMemoryError() = delete;
+
+  explicit OutOfMemoryError(const RefPtr<Device>& aParent)
+      : ChildOf<Device>(aParent) {}
 
  private:
   virtual ~OutOfMemoryError();
   void Cleanup() {}
-
- public:
 };
 
 }  // namespace webgpu
