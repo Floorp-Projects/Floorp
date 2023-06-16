84fd0ef4ad4b7b310e1b6b69d17b49a7b8dfe026
surapunoyousei <ryosukeusuusu@gmail.com>
2023-06-16 21:51:03 +0900

    [FB] userChromeJS | Update configs

--- extensions/pref/autoconfig/src/legacy_component.js
+++ extensions/pref/autoconfig/src/legacy_component.js
@@ -1,26 +1,27 @@
-//This config file is made by xiaoxiaoflood. Thanks. Pelase read about:license. 
+// skip 1st line. This config file is made by xiaoxiaoflood. Thanks. Pelase read about:license.
 
-const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+let enabled = Services.prefs.getBoolPref("toolkit.legacyUserProfileCustomizations.script", false);
+ChromeUtils.import('resource://gre/modules/Services.jsm');
 
-if (Services.prefs.getBoolPref("toolkit.legacyUserProfileCustomizations.script", false)) {
-  lockPref('xpinstall.signatures.required', false);
-  lockPref('extensions.install_origins.enabled', false);
-
-  const Constants = ChromeUtils.import('resource://gre/modules/AppConstants.jsm');
-  const temp = Object.assign({}, Constants.AppConstants);
-  temp.MOZ_REQUIRE_SIGNING = false
-  Constants.AppConstants = Object.freeze(temp);
-  
-  try {
-    let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
+if (enabled) {
+    lockPref('xpinstall.signatures.required', false);
+    lockPref('extensions.install_origins.enabled', false);
+ try {
+    const cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
     cmanifest.append('utils');
     cmanifest.append('chrome.manifest');
     Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
-  
+
+    const objRef = ChromeUtils.import('resource://gre/modules/addons/AddonSettings.jsm');
+    const temp = Object.assign({}, Object.getOwnPropertyDescriptors(objRef.AddonSettings), {
+      REQUIRE_SIGNING: { value: false }
+    });
+    objRef.AddonSettings = Object.defineProperties({}, temp);
+
     Cu.import('chrome://userchromejs/content/BootstrapLoader.jsm');
   } catch (ex) {};
-  
+
   try {
     Cu.import('chrome://userchromejs/content/userChrome.jsm');
   } catch (ex) {};
-} 
+}
