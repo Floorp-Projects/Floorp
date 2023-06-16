2e19750c08e6bb292805eb8aca4181bb72b11b0f
Adam Vandolder <adam.vandolder@gmail.com>
2023-06-16 15:02:11 +0000

    Bug 1556358 - Part 3: Restore FACE state in SessionStore. r=edgar,farre
    Depends on D174115
    
    Differential Revision: https://phabricator.services.mozilla.com/D179278

--- browser/components/sessionstore/SessionStore.sys.mjs
+++ browser/components/sessionstore/SessionStore.sys.mjs
@@ -3425,8 +3425,16 @@ var SessionStoreInternal = {
   },
 
   getClosedTabDataForWindow: function ssi_getClosedTabDataForWindow(aWindow) {
+    // We need to enable wrapping reflectors in order to allow the cloning of
+    // objects containing FormDatas, which could be stored by
+    // form-associated custom elements.
+    let options = { wrapReflectors: true };
     if ("__SSi" in aWindow) {
-      return Cu.cloneInto(this._windows[aWindow.__SSi]._closedTabs, {});
+      return Cu.cloneInto(
+        this._windows[aWindow.__SSi]._closedTabs,
+        {},
+        options
+      );
     }
 
     if (!DyingWindowCache.has(aWindow)) {
@@ -3437,7 +3445,7 @@ var SessionStoreInternal = {
     }
 
     let data = DyingWindowCache.get(aWindow);
-    return Cu.cloneInto(data._closedTabs, {});
+    return Cu.cloneInto(data._closedTabs, {}, options);
   },
 
   undoCloseTab: function ssi_undoCloseTab(aWindow, aIndex) {
@@ -6289,6 +6297,13 @@ var SessionStoreInternal = {
                 value.selectedIndex,
                 value.value
               );
+              break;
+            }
+            if (
+              value.hasOwnProperty("value") &&
+              value.hasOwnProperty("state")
+            ) {
+              root.addCustomElement(isXpath, key, value.value, value.state);
               break;
             }
             if (
