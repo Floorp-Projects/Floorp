7d16332e6b83319e76392bc2fa29253c9630033d
Gregory Pappas <gp3033@protonmail.com>
2023-06-17 23:34:36 +0000

    Bug 1837813 - Remove unused accessibility.* prefs r=geckoview-reviewers,eeejay,m_kato
    Depends on D180597
    
    Differential Revision: https://phabricator.services.mozilla.com/D180598

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -316,16 +316,6 @@ pref("media.realtime_decoder.enabled", true);
 
 pref("full-screen-api.enabled", true);
 
-// Enable accessibility mode if platform accessibility is enabled.
-pref("accessibility.accessfu.activate", 2);
-pref("accessibility.accessfu.quicknav_modes", "Link,Heading,FormElement,Landmark,ListItem");
-// Active quicknav mode, index value of list from quicknav_modes
-pref("accessibility.accessfu.quicknav_index", 0);
-// Setting for an utterance order (0 - description first, 1 - description last).
-pref("accessibility.accessfu.utterance", 1);
-// Whether to skip images with empty alt text
-pref("accessibility.accessfu.skip_empty_images", true);
-
 // Transmit UDP busy-work to the LAN when anticipating low latency
 // network reads and on wifi to mitigate 802.11 Power Save Polling delays
 pref("network.tickle-wifi.enabled", true);
