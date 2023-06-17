1af810bb841de051958f6d3b7c0dc9ead497c4a2
Gregory Pappas <gp3033@protonmail.com>
2023-06-17 19:02:56 +0000

    Bug 1837813 - Remove unused app.* prefs r=geckoview-reviewers,m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D180597

--- layout/tools/reftest/remotereftest.py
+++ layout/tools/reftest/remotereftest.py
@@ -336,7 +336,6 @@ class RemoteReftest(RefTest):
         )
         profileDir = profile.profile
         prefs = {}
-        prefs["app.update.url.android"] = ""
         prefs["reftest.remote"] = True
         prefs["datareporting.policy.dataSubmissionPolicyBypassAcceptance"] = True
         # move necko cache to a location that can be cleaned up
