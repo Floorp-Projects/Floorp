97838e61b817a21d07fe38bd9f67dca6ff4bbd64
Ryosuke Asano <ryosukeusuusu@gmail.com>
2023-06-18 22:10:54 +0900

    [FB] Search Engine | Add SearXNG Search Engine
    #270

--- toolkit/components/search/SearchService.sys.mjs
+++ toolkit/components/search/SearchService.sys.mjs
@@ -2284,6 +2284,7 @@ export class SearchService {
       { webExtension: { id: "ddg@search.mozilla.org",       locale: "default"}},
       { webExtension: { id: "you.com@search.mozilla.org",   locale: "default"}},
       { webExtension: { id: "yahoo-jp@search.mozilla.org",  locale: "default"}},
+      { webExtension: { id: "searxng@search.mozilla.org",   locale: "default"}},
     ];
 
     const privateDefault = (
