3321312f72682e83c08301b608a9a52ccfd36cb3
Glenn Watson <git@intuitionlibrary.com>
2023-06-19 02:31:03 +0000

    Bug 1837103 - Support perspective transforms with new WR clip mask code path r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D180166

--- gfx/wr/webrender/src/prepare.rs
+++ gfx/wr/webrender/src/prepare.rs
@@ -108,22 +108,9 @@ pub fn prepare_primitives(
 fn can_use_clip_chain_for_quad_path(
     clip_chain: &ClipChainInstance,
     prim_spatial_node_index: SpatialNodeIndex,
-    raster_spatial_node_index: SpatialNodeIndex,
     clip_store: &ClipStore,
     data_stores: &DataStores,
-    spatial_tree: &SpatialTree,
 ) -> bool {
-    let map_prim_to_surface = spatial_tree.get_relative_transform(
-        prim_spatial_node_index,
-        raster_spatial_node_index,
-    );
-    if map_prim_to_surface.is_perspective() {
-        return false;
-    }
-
-    // TODO(gw): Temporarily disable the new clip-mask rendering path for now, while
-    //           investigating a driver-specific shader optimization regression.
-
     if !clip_chain.needs_mask {
         return true;
     }
@@ -170,7 +157,7 @@ fn get_prim_render_strategy(
     clip_chain: &ClipChainInstance,
     clip_store: &ClipStore,
     data_stores: &DataStores,
-    prim_is_2d_axis_aligned: bool,
+    can_use_nine_patch: bool,
 ) -> QuadRenderStrategy {
     if clip_chain.needs_mask {
         fn tile_count_for_size(size: f32) -> u16 {
@@ -183,7 +170,7 @@ fn get_prim_render_strategy(
         let try_split_prim = x_tiles > 1 || y_tiles > 1;
 
         if try_split_prim {
-            if prim_is_2d_axis_aligned {
+            if can_use_nine_patch {
                 if clip_chain.clips_range.count == 1 {
                     let clip_instance = clip_store.get_instance_from_range(&clip_chain.clips_range, 0);
                     let clip_node = &data_stores.clip[clip_instance.handle];
@@ -306,10 +293,8 @@ fn prepare_prim_for_render(
                 *use_legacy_path = !can_use_clip_chain_for_quad_path(
                     &prim_instance.vis.clip_chain,
                     cluster.spatial_node_index,
-                    pic_context.raster_spatial_node_index,
                     frame_state.clip_store,
                     data_stores,
-                    frame_context.spatial_tree,
                 );
 
                 *use_legacy_path
@@ -693,12 +678,13 @@ fn prepare_interned_prim_for_render(
                     pic_context.raster_spatial_node_index,
                 );
                 let prim_is_2d_axis_aligned = map_prim_to_surface.is_2d_axis_aligned();
+                let prim_has_perspective = map_prim_to_surface.is_perspective();
 
                 let strategy = get_prim_render_strategy(
                     &prim_instance.vis.clip_chain,
                     frame_state.clip_store,
                     data_stores,
-                    prim_is_2d_axis_aligned,
+                    prim_is_2d_axis_aligned && !prim_has_perspective,
                 );
 
                 let prim_data = &data_stores.prim[*data_handle];
@@ -721,7 +707,8 @@ fn prepare_interned_prim_for_render(
                 if is_opaque {
                     quad_flags |= QuadFlags::IS_OPAQUE;
                 }
-                if prim_is_2d_axis_aligned {
+                let needs_scissor = !prim_is_2d_axis_aligned || prim_has_perspective;
+                if !needs_scissor {
                     quad_flags |= QuadFlags::APPLY_DEVICE_CLIP;
                 }
 
@@ -794,7 +781,7 @@ fn prepare_interned_prim_for_render(
                             aa_flags,
                             quad_flags,
                             device_pixel_scale,
-                            !prim_is_2d_axis_aligned,
+                            needs_scissor,
                             frame_state,
                         );
 
@@ -877,7 +864,7 @@ fn prepare_interned_prim_for_render(
                                     aa_flags,
                                     quad_flags,
                                     device_pixel_scale,
-                                    !prim_is_2d_axis_aligned,
+                                    needs_scissor,
                                     frame_state,
                                 );
                                 scratch.quad_segments.push(segment);
