93a0312c3956d7e544ca07430c47afe9f0c4aa1f
Andrew Halberstadt <ahal@mozilla.com>
2023-06-19 14:39:40 +0000

    Bug 1838018 - [ci] Handle Wayland worker-type in test transforms, r=taskgraph-reviewers,jcristau, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D180710

--- taskcluster/gecko_taskgraph/transforms/test/worker.py
+++ taskcluster/gecko_taskgraph/transforms/test/worker.py
@@ -174,7 +174,9 @@ def set_worker_type(config, tasks):
         elif test_platform.startswith("android-em-7.0-x86"):
             task["worker-type"] = "t-linux-kvm"
         elif test_platform.startswith("linux") or test_platform.startswith("android"):
-            if task.get("suite", "") in ["talos", "raptor"] and not task[
+            if "wayland" in test_platform:
+                task["worker-type"] = "t-linux-wayland"
+            elif task.get("suite", "") in ["talos", "raptor"] and not task[
                 "build-platform"
             ].startswith("linux64-ccov"):
                 task["worker-type"] = "t-linux-talos-1804"
