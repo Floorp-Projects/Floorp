272e267e5a462215a8d5bf172060c4d227c31290
stransky <stransky@redhat.com>
2023-06-20 10:12:49 +0000

    Bug 1836021 [Linux] Probe VA-API devices if HW acceleration is force enabled (beta) r=emilio, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D181450

--- toolkit/mozapps/extensions/test/xpcshell/rs-blocklist/test_gfxBlacklist_Version.js
+++ toolkit/mozapps/extensions/test/xpcshell/rs-blocklist/test_gfxBlacklist_Version.js
@@ -154,9 +154,13 @@ async function run_test() {
       failureId
     );
     if (OS == "Linux" && status != Ci.nsIGfxInfo.FEATURE_STATUS_OK) {
-      // Disabled on testsuite due to old Mesa version there.
-      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_BLOCKED_DRIVER_VERSION);
-      Assert.equal(failureId.value, "FEATURE_HARDWARE_VIDEO_DECODING_MESA");
+      // Linux test suite is running on SW OpenGL backend and we disable
+      // HW video decoding there.
+      Assert.equal(status, Ci.nsIGfxInfo.FEATURE_BLOCKED_PLATFORM_TEST);
+      Assert.equal(
+        failureId.value,
+        "FEATURE_FAILURE_VIDEO_DECODING_TEST_FAILED"
+      );
     } else {
       Assert.equal(status, Ci.nsIGfxInfo.FEATURE_STATUS_OK);
     }
