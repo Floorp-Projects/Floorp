c8cb00b14ca8a262fd51ff4604ca4604315a8cb7
Erik Nordin <enordin@mozilla.com>
2023-06-20 16:00:22 +0000

    Bug 1835571 - Add tests for reenabling translations r=gregtatum
    Adds tests that ensure the correct page actions occur
    when translations is reenabled via the translations
    panel settings menu.
    
    Depends on D179330
    
    Differential Revision: https://phabricator.services.mozilla.com/D179332

---
+++ browser/components/translations/tests/browser/browser_translations_panel_app_menu_never_translate_language.js
@@ -0,0 +1,52 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests the effect of unchecking the never-translate-language menuitem, removing
+ * the language from the never-translate languages list.
+ * The translations button should reappear.
+ */
+add_task(async function test_uncheck_never_translate_language_shows_button() {
+  const { cleanup, runInPage } = await loadTestPage({
+    page: SPANISH_PAGE_URL,
+    languagePairs: LANGUAGE_PAIRS,
+    prefs: [["browser.translations.neverTranslateLanguages", "es"]],
+  });
+
+  await assertTranslationsButton(
+    { button: false },
+    "The translations button is hidden"
+  );
+
+  info(
+    "The page should still be in its original, untranslated form because " +
+      "the document language is in the neverTranslateLanguages pref"
+  );
+  await runInPage(async TranslationsTest => {
+    const { getH1 } = TranslationsTest.getSelectors();
+    await TranslationsTest.assertTranslationResult(
+      "The page's H1 is in Spanish.",
+      getH1,
+      "Don Quijote de La Mancha"
+    );
+  });
+
+  info(
+    "Simulate clicking always-translate-language in the settings menu, " +
+      "adding the document language to the alwaysTranslateLanguages pref"
+  );
+  await openTranslationsSettingsMenuViaAppMenu();
+
+  await assertIsNeverTranslateLanguage("es", true);
+  await toggleNeverTranslateLanguage();
+  await assertIsNeverTranslateLanguage("es", false);
+
+  await assertTranslationsButton(
+    { button: true, circleArrows: false, locale: false, icon: true },
+    "The translations button is now available"
+  );
+
+  await cleanup();
+});
