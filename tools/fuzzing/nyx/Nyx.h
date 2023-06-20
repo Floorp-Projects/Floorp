5ca31b3358c7ef50f2a6c0bbfba9e49537d89aca
Christian Holler (:decoder) <choller@mozilla.com>
2023-06-20 19:51:47 +0000

    Bug 1838596 - Nyx: Add an API to retrieve the raw fuzzing buffer. r=truber
    Depends on D181057
    
    Differential Revision: https://phabricator.services.mozilla.com/D181058

--- tools/fuzzing/nyx/Nyx.h
+++ tools/fuzzing/nyx/Nyx.h
@@ -34,6 +34,7 @@ class Nyx {
   bool is_enabled(const char* identifier);
   bool is_replay();
   uint32_t get_data(uint8_t* data, uint32_t size);
+  uint32_t get_raw_data(uint8_t* data, uint32_t size);
   void release(uint32_t iterations = 1);
   void handle_event(const char* type, const char* file, int line,
                     const char* reason);
@@ -43,6 +44,7 @@ class Nyx {
 
   std::atomic<bool> mReplayMode;
   std::list<Vector<uint8_t, 0, MallocAllocPolicy>*> mReplayBuffers;
+  Vector<uint8_t, 0, MallocAllocPolicy>* mRawReplayBuffer;
 
   Nyx();
   NYX_DISALLOW_COPY_AND_ASSIGN(Nyx);
