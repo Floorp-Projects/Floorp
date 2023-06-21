e0ab0451963131dedfed83ec07353b98a8ca71b2
André Bargull <andre.bargull@gmail.com>
2023-06-21 20:27:41 +0000

    Bug 1519167 - Part 24: Implement Temporal.ZonedDateTime constructor. r=spidermonkey-reviewers,allstarschh
    Implement the `Temporal.ZonedDateTime` constructor and the corresponding
    CreateTemporalZonedDateTime abstract operation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174280

--- js/src/builtin/temporal/ZonedDateTime.cpp
+++ js/src/builtin/temporal/ZonedDateTime.cpp
@@ -7,22 +7,175 @@
 #include "builtin/temporal/ZonedDateTime.h"
 
 #include "mozilla/Assertions.h"
+#include "mozilla/CheckedInt.h"
+#include "mozilla/FloatingPoint.h"
+#include "mozilla/Likely.h"
+
+#include <cstdlib>
+#include <initializer_list>
+#include <utility>
 
 #include "jspubtd.h"
 #include "NamespaceImports.h"
 
+#include "builtin/temporal/Calendar.h"
+#include "builtin/temporal/Instant.h"
+#include "builtin/temporal/TimeZone.h"
+#include "ds/IdValuePair.h"
 #include "gc/AllocKind.h"
+#include "gc/Barrier.h"
+#include "js/AllocPolicy.h"
+#include "js/CallArgs.h"
+#include "js/CallNonGenericMethod.h"
 #include "js/Class.h"
+#include "js/ComparisonOperators.h"
+#include "js/Conversions.h"
+#include "js/ErrorReport.h"
+#include "js/friend/ErrorMessages.h"
+#include "js/GCVector.h"
+#include "js/Id.h"
+#include "js/Printer.h"
+#include "js/PropertyDescriptor.h"
 #include "js/PropertySpec.h"
+#include "js/RootingAPI.h"
+#include "js/TracingAPI.h"
 #include "js/TypeDecls.h"
+#include "js/Utility.h"
+#include "js/Value.h"
+#include "util/StringBuffer.h"
+#include "vm/BigIntType.h"
+#include "vm/Compartment.h"
 #include "vm/GlobalObject.h"
+#include "vm/JSAtomState.h"
+#include "vm/JSObject.h"
 #include "vm/PlainObject.h"
+#include "vm/StringType.h"
+
+#include "vm/JSContext-inl.h"
+#include "vm/JSObject-inl.h"
+#include "vm/NativeObject-inl.h"
+#include "vm/ObjectOperations-inl.h"
 
 using namespace js;
 using namespace js::temporal;
 
+/**
+ * CreateTemporalZonedDateTime ( epochNanoseconds, timeZone, calendar [ ,
+ * newTarget ] )
+ */
+static ZonedDateTimeObject* CreateTemporalZonedDateTime(
+    JSContext* cx, const CallArgs& args, Handle<BigInt*> epochNanoseconds,
+    Handle<JSObject*> timeZone, Handle<JSObject*> calendar) {
+  // Step 1.
+  MOZ_ASSERT(IsValidEpochNanoseconds(epochNanoseconds));
+
+  // Steps 3-4.
+  Rooted<JSObject*> proto(cx);
+  if (!GetPrototypeFromBuiltinConstructor(cx, args, JSProto_ZonedDateTime,
+                                          &proto)) {
+    return nullptr;
+  }
+
+  auto* obj = NewObjectWithClassProto<ZonedDateTimeObject>(cx, proto);
+  if (!obj) {
+    return nullptr;
+  }
+
+  // Step 4.
+  auto instant = ToInstant(epochNanoseconds);
+  obj->setFixedSlot(ZonedDateTimeObject::SECONDS_SLOT,
+                    NumberValue(instant.seconds));
+  obj->setFixedSlot(ZonedDateTimeObject::NANOSECONDS_SLOT,
+                    Int32Value(instant.nanoseconds));
+
+  // Step 5.
+  obj->setFixedSlot(ZonedDateTimeObject::TIMEZONE_SLOT, ObjectValue(*timeZone));
+
+  // Step 6.
+  obj->setFixedSlot(ZonedDateTimeObject::CALENDAR_SLOT, ObjectValue(*calendar));
+
+  // Step 7.
+  return obj;
+}
+
+/**
+ * CreateTemporalZonedDateTime ( epochNanoseconds, timeZone, calendar [ ,
+ * newTarget ] )
+ */
+ZonedDateTimeObject* js::temporal::CreateTemporalZonedDateTime(
+    JSContext* cx, const Instant& instant, Handle<JSObject*> timeZone,
+    Handle<JSObject*> calendar) {
+  // Step 1.
+  MOZ_ASSERT(IsValidEpochInstant(instant));
+
+  // Steps 2-3.
+  auto* obj = NewBuiltinClassInstance<ZonedDateTimeObject>(cx);
+  if (!obj) {
+    return nullptr;
+  }
+
+  // Step 4.
+  obj->setFixedSlot(ZonedDateTimeObject::SECONDS_SLOT,
+                    NumberValue(instant.seconds));
+  obj->setFixedSlot(ZonedDateTimeObject::NANOSECONDS_SLOT,
+                    Int32Value(instant.nanoseconds));
+
+  // Step 5.
+  obj->setFixedSlot(ZonedDateTimeObject::TIMEZONE_SLOT, ObjectValue(*timeZone));
+
+  // Step 6.
+  obj->setFixedSlot(ZonedDateTimeObject::CALENDAR_SLOT, ObjectValue(*calendar));
+
+  // Step 7.
+  return obj;
+}
+
+/**
+ * Temporal.ZonedDateTime ( epochNanoseconds, timeZoneLike [ , calendarLike ] )
+ */
 static bool ZonedDateTimeConstructor(JSContext* cx, unsigned argc, Value* vp) {
-  MOZ_CRASH("NYI");
+  CallArgs args = CallArgsFromVp(argc, vp);
+
+  // Step 1.
+  if (!ThrowIfNotConstructing(cx, args, "Temporal.ZonedDateTime")) {
+    return false;
+  }
+
+  // Step 2.
+  Rooted<BigInt*> epochNanoseconds(cx, js::ToBigInt(cx, args.get(0)));
+  if (!epochNanoseconds) {
+    return false;
+  }
+
+  // Step 3.
+  if (!IsValidEpochNanoseconds(epochNanoseconds)) {
+    JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                              JSMSG_TEMPORAL_INSTANT_INVALID);
+    return false;
+  }
+
+  // Step 4.
+  Rooted<JSObject*> timeZone(cx, ToTemporalTimeZone(cx, args.get(1)));
+  if (!timeZone) {
+    return false;
+  }
+
+  // Step 5.
+  Rooted<JSObject*> calendar(cx,
+                             ToTemporalCalendarWithISODefault(cx, args.get(2)));
+  if (!calendar) {
+    return false;
+  }
+
+  // Step 6.
+  auto* obj = CreateTemporalZonedDateTime(cx, args, epochNanoseconds, timeZone,
+                                          calendar);
+  if (!obj) {
+    return false;
+  }
+
+  args.rval().setObject(*obj);
+  return true;
 }
 
 const JSClass ZonedDateTimeObject::class_ = {
