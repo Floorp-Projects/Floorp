ea7acd531a440be9d8ffddcfb4f9913cb3a1dae4
Norisz Fay <nfay@mozilla.com>
2023-06-22 02:05:28 +0300

    Backed out changeset c910266f6c42 (bug 1825088) for causing SM failures on br-on-cast.js CLOSED TREE

--- js/src/jit-test/tests/wasm/gc/supertype_later_in_group.js
+++ js/src/jit-test/tests/wasm/gc/supertype_later_in_group.js
@@ -6,10 +6,10 @@
 wasmValidateText(`
 (module
     (rec
-        (type $a (sub (struct (field (ref $notParsedYet)))))
+        (type $a (struct (field (ref $notParsedYet))))
         (type $b (sub $a (struct (field (ref $notParsedYet2)))))
 
-        (type $notParsedYet (sub (struct)))
+        (type $notParsedYet (struct))
         (type $notParsedYet2 (sub $notParsedYet (struct (field i32))))
     )
 )`);
