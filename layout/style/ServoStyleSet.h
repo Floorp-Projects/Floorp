7019b249578898ef81035daec2ba00b32a911f0b
Emily McDonough <emcdonough@mozilla.com>
2023-06-21 19:02:23 +0000

    Bug 1833471 Part 2 - Export @page pseudo-class flags to Gecko style code r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D178869

--- layout/style/ServoStyleSet.h
+++ layout/style/ServoStyleSet.h
@@ -35,6 +35,7 @@ enum class StyleRuleChangeKind : uint32_t;
 
 template <typename Integer, typename Number, typename LinearStops>
 struct StyleTimingFunction;
+struct StylePagePseudoClassFlags;
 struct StylePiecewiseLinearFunction;
 using StyleComputedTimingFunction =
     StyleTimingFunction<int32_t, float, StylePiecewiseLinearFunction>;
@@ -254,9 +255,10 @@ class ServoStyleSet {
       PseudoStyleType aType);
 
   // Get a ComputedStyle for a pageContent box with the specified page-name.
-  // A page name that is null or the empty atom gets the global page style.
+  // A page name that is null or the empty atom and has no pseudo classes gets
+  // the global page style.
   already_AddRefed<ComputedStyle> ResolvePageContentStyle(
-      const nsAtom* aPageName);
+      const nsAtom* aPageName, const StylePagePseudoClassFlags& aPseudo);
 
   already_AddRefed<ComputedStyle> ResolveXULTreePseudoStyle(
       dom::Element* aParentElement, nsCSSAnonBoxPseudoStaticAtom* aPseudoTag,
