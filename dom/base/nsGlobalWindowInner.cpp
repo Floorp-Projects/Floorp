036486f1c474dc324f17bc3735517f3dcb7617bd
Tom Schuster <tschuster@mozilla.com>
2023-06-21 07:01:24 +0000

    Bug 1838415 - Introduce RFPTarget::WindowDevicePixelRatio. r=tjr
    Depends on D181232
    
    Differential Revision: https://phabricator.services.mozilla.com/D181233

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -3504,8 +3504,8 @@ double nsGlobalWindowInner::GetDevicePixelRatio(CallerType aCallerType,
     return 1.0;
   }
 
-  if (nsIGlobalObject::ShouldResistFingerprinting(aCallerType,
-                                                  RFPTarget::Unknown)) {
+  if (nsIGlobalObject::ShouldResistFingerprinting(
+          aCallerType, RFPTarget::WindowDevicePixelRatio)) {
     // Spoofing the DevicePixelRatio causes blurriness in some situations
     // on HiDPI displays. pdf.js is a non-system caller; but it can't
     // expose the fingerprintable information, so we can safely disable
