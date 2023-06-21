7198b2f42a95dd54589c01b6621ad4db5b058f5e
Emily McDonough <emcdonough@mozilla.com>
2023-06-21 02:13:01 +0000

    Bug 1833471 Part 1 - Implement @page pseudo-class parsing and rule handling r=emilio
    This currently will not match page-rules based on any selectors with any
    pseudo-classes present.
    
    Differential Revision: https://phabricator.services.mozilla.com/D178252

--- servo/components/style/stylesheets/page_rule.rs
+++ servo/components/style/stylesheets/page_rule.rs
@@ -12,28 +12,205 @@ use crate::shared_lock::{DeepCloneParams, DeepCloneWithLock, Locked};
 use crate::shared_lock::{SharedRwLock, SharedRwLockReadGuard, ToCssWithGuard};
 use crate::str::CssStringWriter;
 use crate::values::{AtomIdent, CustomIdent};
-use cssparser::{Parser, SourceLocation};
+use cssparser::{Parser, SourceLocation, Token};
 #[cfg(feature = "gecko")]
 use malloc_size_of::{MallocSizeOf, MallocSizeOfOps, MallocUnconditionalShallowSizeOf};
 use servo_arc::Arc;
 use std::fmt::{self, Write};
+use smallvec::SmallVec;
 use style_traits::{CssWriter, ParseError, ToCss};
+
+macro_rules! page_pseudo_classes {
+    ($($(#[$($meta:tt)+])* $id:ident => $val:literal,)+) => {
+        /// [`@page`][page] rule pseudo-classes.
+        ///
+        /// https://drafts.csswg.org/css-page-3/#page-selectors
+        #[derive(Clone, Copy, Debug, Eq, MallocSizeOf, PartialEq, ToShmem)]
+        #[repr(u8)]
+        pub enum PagePseudoClass {
+            $($(#[$($meta)+])* $id,)+
+        }
+        impl PagePseudoClass {
+            fn parse<'i, 't>(
+                input: &mut Parser<'i, 't>,
+            ) -> Result<Self, ParseError<'i>> {
+                let loc = input.current_source_location();
+                let colon = input.next_including_whitespace()?;
+                if *colon != Token::Colon {
+                    return Err(loc.new_unexpected_token_error(colon.clone()));
+                }
+
+                let ident = input.next_including_whitespace()?;
+                if let Token::Ident(s) = ident {
+                    return match_ignore_ascii_case! { &**s,
+                        $($val => Ok(PagePseudoClass::$id),)+
+                        _ => Err(loc.new_unexpected_token_error(Token::Ident(s.clone()))),
+                    };
+                }
+                Err(loc.new_unexpected_token_error(ident.clone()))
+            }
+            #[inline]
+            fn to_str(&self) -> &'static str {
+                match *self {
+                    $(PagePseudoClass::$id => concat!(':', $val),)+
+                }
+            }
+        }
+    }
+}
+
+page_pseudo_classes! {
+    /// [`:first`][first] pseudo-class
+    ///
+    /// [first] https://drafts.csswg.org/css-page-3/#first-pseudo
+    First => "first",
+    /// [`:blank`][blank] pseudo-class
+    ///
+    /// [blank] https://drafts.csswg.org/css-page-3/#blank-pseudo
+    Blank => "blank",
+    /// [`:left`][left] pseudo-class
+    ///
+    /// [left]: https://drafts.csswg.org/css-page-3/#spread-pseudos
+    Left => "left",
+    /// [`:right`][right] pseudo-class
+    ///
+    /// [right]: https://drafts.csswg.org/css-page-3/#spread-pseudos
+    Right => "right",
+}
+
+bitflags! {
+    /// Bit-flags for pseudo-class. This should only be used for querying if a
+    /// page-rule applies.
+    ///
+    /// https://drafts.csswg.org/css-page-3/#page-selectors
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct PagePseudoClassFlags : u8 {
+        /// No pseudo-classes
+        const NONE = 0;
+        /// Flag for PagePseudoClass::First
+        const FIRST = 1 << 0;
+        /// Flag for PagePseudoClass::Blank
+        const BLANK = 1 << 1;
+        /// Flag for PagePseudoClass::Left
+        const LEFT = 1 << 2;
+        /// Flag for PagePseudoClass::Right
+        const RIGHT = 1 << 3;
+    }
+}
+
+impl PagePseudoClassFlags {
+    /// Creates a pseudo-class flags object with a single pseudo-class.
+    #[inline]
+    pub fn new(other: &PagePseudoClass) -> Self {
+        match *other {
+            PagePseudoClass::First => PagePseudoClassFlags::FIRST,
+            PagePseudoClass::Blank => PagePseudoClassFlags::BLANK,
+            PagePseudoClass::Left => PagePseudoClassFlags::LEFT,
+            PagePseudoClass::Right => PagePseudoClassFlags::RIGHT,
+        }
+    }
+    /// Checks if the given pseudo class applies to this set of flags.
+    #[inline]
+    pub fn contains_class(self, other: &PagePseudoClass) -> bool {
+        self.intersects(PagePseudoClassFlags::new(other))
+    }
+}
+
+type PagePseudoClasses = SmallVec<[PagePseudoClass; 4]>;
 
 /// Type of a single [`@page`][page selector]
 ///
-/// We do not support pseudo selectors yet.
 /// [page-selectors]: https://drafts.csswg.org/css2/page.html#page-selectors
-#[derive(Clone, Debug, Eq, MallocSizeOf, PartialEq, SpecifiedValueInfo, ToCss, ToShmem)]
-pub struct PageSelector(pub AtomIdent);
+#[derive(Clone, Debug, MallocSizeOf, ToShmem)]
+pub struct PageSelector{
+    /// Page name
+    ///
+    /// https://drafts.csswg.org/css-page-3/#page-type-selector
+    pub name: AtomIdent,
+    /// Pseudo-classes for [`@page`][page-selectors]
+    ///
+    /// [page-selectors]: https://drafts.csswg.org/css2/page.html#page-selectors
+    pub pseudos: PagePseudoClasses,
+}
 
 impl PageSelector {
     /// Checks if the ident matches a page-name's ident.
     ///
-    /// This does not currently take pseudo selectors into account.
+    /// This does not take pseudo selectors into account.
     #[inline]
     pub fn ident_matches(&self, other: &CustomIdent) -> bool {
-        self.0 .0 == other.0
+        self.name.0 == other.0
     }
+
+    /// Checks that this selector matches the ident and all pseudo classes are
+    /// present in the provided flags.
+    #[inline]
+    pub fn matches(&self, name: &CustomIdent, flags: &PagePseudoClassFlags) -> bool {
+        self.ident_matches(name) && self.flags_match(flags)
+    }
+
+    /// Checks that all pseudo classes in this selector are present in the
+    /// provided flags.
+    ///
+    /// Equivalent to, but may be more efficient than:
+    ///
+    /// ```
+    /// match_specificity(flags).is_some()
+    /// ```
+    pub fn flags_match(&self, flags: &PagePseudoClassFlags) -> bool {
+        self.pseudos.iter().all(|pc| flags.contains_class(pc))
+    }
+
+    /// Implements specificity calculation for a page selector given a set of
+    /// page pseudo-classes to match with.
+    /// If this selector includes any pseudo-classes that are not in the flags,
+    /// then this will return None.
+    ///
+    /// To fit the specificity calculation into a 32-bit value, this limits the
+    /// maximum count of :first and :blank to 32767, and the maximum count of
+    /// :left and :right to 65535.
+    ///
+    /// https://drafts.csswg.org/css-page-3/#cascading-and-page-context
+    pub fn match_specificity(&self, flags: &PagePseudoClassFlags) -> Option<u32> {
+        let mut g: usize = 0;
+        let mut h: usize = 0;
+        for pc in self.pseudos.iter() {
+            if !flags.contains_class(pc) {
+                return None
+            }
+            match pc {
+                PagePseudoClass::First |
+                PagePseudoClass::Blank => g += 1,
+                PagePseudoClass::Left |
+                PagePseudoClass::Right => h += 1,
+            }
+        }
+        let h = h.min(0xFFFF) as u32;
+        let g = (g.min(0x7FFF) as u32) << 16;
+        let f = if self.name.0.is_empty() { 0 } else { 0x80000000 };
+        Some(h + g + f)
+    }
+}
+
+impl ToCss for PageSelector {
+    fn to_css<W>(&self, dest: &mut CssWriter<W>) -> fmt::Result
+    where
+        W: Write
+    {
+        self.name.to_css(dest)?;
+        for pc in self.pseudos.iter() {
+            dest.write_str(pc.to_str())?;
+        }
+        Ok(())
+    }
+}
+
+fn parse_page_name<'i, 't>(
+    input: &mut Parser<'i, 't>
+) -> Result<AtomIdent, ParseError<'i>> {
+    let s = input.expect_ident()?;
+    Ok(AtomIdent::from(&**s))
 }
 
 impl Parse for PageSelector {
@@ -41,8 +218,12 @@ impl Parse for PageSelector {
         _context: &ParserContext,
         input: &mut Parser<'i, 't>,
     ) -> Result<Self, ParseError<'i>> {
-        let s = input.expect_ident()?;
-        Ok(PageSelector(AtomIdent::from(&**s)))
+        let name = input.try_parse(parse_page_name).unwrap_or(AtomIdent(atom!("")));
+        let mut pseudos = PagePseudoClasses::default();
+        while let Ok(pc) = input.try_parse(PagePseudoClass::parse) {
+            pseudos.push(pc);
+        }
+        Ok(PageSelector{name, pseudos})
     }
 }
 
@@ -107,6 +288,22 @@ impl PageRule {
         self.block.unconditional_shallow_size_of(ops) +
             self.block.read_with(guard).size_of(ops) +
             self.selectors.size_of(ops)
+    }
+    /// Computes the specificity of this page rule when matched with flags.
+    ///
+    /// Computing this value has linear-complexity with the size of the
+    /// selectors, so the caller should usually call this once and cache the
+    /// result.
+    ///
+    /// Returns None if the flags do not match this page rule.
+    ///
+    /// The return type is ordered by page-rule specificity.
+    pub fn match_specificity(&self, flags: &PagePseudoClassFlags) -> Option<u32> {
+        let mut specificity = None;
+        for s in self.selectors.0.iter().map(|s| s.match_specificity(flags)) {
+            specificity = s.max(specificity);
+        }
+        specificity
     }
 }
 
