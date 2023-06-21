63424b99a415cfbe732a8c3ee3bb2e403f11cd78
André Bargull <andre.bargull@gmail.com>
2023-06-21 20:27:39 +0000

    Bug 1519167 - Part 9: Implement Temporal.Calendar constructor and some simple methods. r=spidermonkey-reviewers,dminor
    Differential Revision: https://phabricator.services.mozilla.com/D174161

--- js/src/builtin/temporal/Calendar.cpp
+++ js/src/builtin/temporal/Calendar.cpp
@@ -7,23 +7,278 @@
 #include "builtin/temporal/Calendar.h"
 
 #include "mozilla/Assertions.h"
+#include "mozilla/Range.h"
+#include "mozilla/RangedPtr.h"
+#include "mozilla/TextUtils.h"
 
+#include <algorithm>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "jsfriendapi.h"
+#include "jsnum.h"
 #include "jspubtd.h"
 #include "jstypes.h"
 #include "NamespaceImports.h"
 
+#include "builtin/Array.h"
+#include "builtin/String.h"
+#include "gc/Allocator.h"
 #include "gc/AllocKind.h"
+#include "gc/Barrier.h"
+#include "js/AllocPolicy.h"
+#include "js/CallArgs.h"
+#include "js/CallNonGenericMethod.h"
 #include "js/Class.h"
+#include "js/ComparisonOperators.h"
+#include "js/Conversions.h"
+#include "js/ErrorReport.h"
+#include "js/ForOfIterator.h"
+#include "js/friend/ErrorMessages.h"
+#include "js/GCAPI.h"
+#include "js/GCHashTable.h"
+#include "js/GCVector.h"
+#include "js/HashTable.h"
+#include "js/Id.h"
+#include "js/Printer.h"
+#include "js/PropertyDescriptor.h"
 #include "js/PropertySpec.h"
+#include "js/RootingAPI.h"
 #include "js/TypeDecls.h"
+#include "js/Utility.h"
+#include "js/Value.h"
+#include "util/StringBuffer.h"
+#include "util/Text.h"
+#include "vm/ArrayObject.h"
+#include "vm/BytecodeUtil.h"
+#include "vm/Compartment.h"
 #include "vm/GlobalObject.h"
+#include "vm/Interpreter.h"
+#include "vm/JSAtomState.h"
+#include "vm/JSContext.h"
+#include "vm/JSObject.h"
 #include "vm/PlainObject.h"
+#include "vm/PropertyInfo.h"
+#include "vm/PropertyKey.h"
+#include "vm/Shape.h"
+#include "vm/Stack.h"
+#include "vm/StringType.h"
+
+#include "vm/JSAtom-inl.h"
+#include "vm/JSObject-inl.h"
+#include "vm/NativeObject-inl.h"
+#include "vm/ObjectOperations-inl.h"
+#include "vm/Realm-inl.h"
 
 using namespace js;
 using namespace js::temporal;
 
+static inline bool IsCalendar(Handle<Value> v) {
+  return v.isObject() && v.toObject().is<CalendarObject>();
+}
+
+#ifdef DEBUG
+template <typename CharT>
+static bool StringIsAsciiLowerCase(mozilla::Range<CharT> str) {
+  return std::all_of(str.begin().get(), str.end().get(), [](CharT ch) {
+    return mozilla::IsAscii(ch) && !mozilla::IsAsciiUppercaseAlpha(ch);
+  });
+}
+
+static bool StringIsAsciiLowerCase(JSLinearString* str) {
+  JS::AutoCheckCannotGC nogc;
+  return str->hasLatin1Chars()
+             ? StringIsAsciiLowerCase(str->latin1Range(nogc))
+             : StringIsAsciiLowerCase(str->twoByteRange(nogc));
+}
+#endif
+
+/**
+ * IsBuiltinCalendar ( id )
+ */
+static bool IsBuiltinCalendar(JSLinearString* id) {
+  // Callers must convert to lower case.
+  MOZ_ASSERT(StringIsAsciiLowerCase(id));
+
+  // Steps 1-3.
+  return StringEqualsLiteral(id, "iso8601");
+}
+
+static JSLinearString* ThrowIfNotBuiltinCalendar(JSContext* cx,
+                                                 Handle<JSLinearString*> id) {
+  if (!StringIsAscii(id)) {
+    if (auto chars = QuoteString(cx, id)) {
+      JS_ReportErrorNumberUTF8(cx, GetErrorMessage, nullptr,
+                               JSMSG_TEMPORAL_CALENDAR_INVALID_ID, chars.get());
+    }
+    return nullptr;
+  }
+
+  JSString* lower = StringToLowerCase(cx, id);
+  if (!lower) {
+    return nullptr;
+  }
+
+  JSLinearString* linear = lower->ensureLinear(cx);
+  if (!linear) {
+    return nullptr;
+  }
+
+  if (!IsBuiltinCalendar(linear)) {
+    if (auto chars = QuoteString(cx, id)) {
+      JS_ReportErrorNumberUTF8(cx, GetErrorMessage, nullptr,
+                               JSMSG_TEMPORAL_CALENDAR_INVALID_ID, chars.get());
+    }
+    return nullptr;
+  }
+
+  return linear;
+}
+
+/**
+ * CreateTemporalCalendar ( identifier [ , newTarget ] )
+ */
+static CalendarObject* CreateTemporalCalendar(
+    JSContext* cx, const CallArgs& args, Handle<JSLinearString*> identifier) {
+  // Step 1.
+  MOZ_ASSERT(IsBuiltinCalendar(identifier));
+
+  // Steps 2-3.
+  Rooted<JSObject*> proto(cx);
+  if (!GetPrototypeFromBuiltinConstructor(cx, args, JSProto_Calendar, &proto)) {
+    return nullptr;
+  }
+
+  auto* obj = NewObjectWithClassProto<CalendarObject>(cx, proto);
+  if (!obj) {
+    return nullptr;
+  }
+
+  // Step 4.
+  obj->setFixedSlot(CalendarObject::IDENTIFIER_SLOT, StringValue(identifier));
+
+  // Step 5.
+  return obj;
+}
+
+static bool Calendar_toString(JSContext* cx, unsigned argc, Value* vp);
+
+JSString* js::temporal::CalendarToString(JSContext* cx,
+                                         Handle<JSObject*> calendar) {
+  if (calendar->is<CalendarObject>() &&
+      HasNoToPrimitiveMethodPure(calendar, cx) &&
+      HasNativeMethodPure(calendar, cx->names().toString, Calendar_toString,
+                          cx)) {
+    JSString* id = calendar->as<CalendarObject>().identifier();
+    MOZ_ASSERT(id);
+    return id;
+  }
+
+  Rooted<Value> calendarValue(cx, ObjectValue(*calendar));
+  return JS::ToString(cx, calendarValue);
+}
+
+/**
+ * Temporal.Calendar ( id )
+ */
 static bool CalendarConstructor(JSContext* cx, unsigned argc, Value* vp) {
-  MOZ_CRASH("NYI");
+  CallArgs args = CallArgsFromVp(argc, vp);
+
+  // Step 1.
+  if (!ThrowIfNotConstructing(cx, args, "Temporal.Calendar")) {
+    return false;
+  }
+
+  // Step 2.
+  JSString* id = JS::ToString(cx, args.get(0));
+  if (!id) {
+    return false;
+  }
+
+  Rooted<JSLinearString*> linear(cx, id->ensureLinear(cx));
+  if (!linear) {
+    return false;
+  }
+
+  // Step 3.
+  linear = ThrowIfNotBuiltinCalendar(cx, linear);
+  if (!linear) {
+    return false;
+  }
+
+  // Step 4.
+  auto* calendar = CreateTemporalCalendar(cx, args, linear);
+  if (!calendar) {
+    return false;
+  }
+
+  args.rval().setObject(*calendar);
+  return true;
+}
+
+/**
+ * get Temporal.Calendar.prototype.id
+ */
+static bool Calendar_id(JSContext* cx, const CallArgs& args) {
+  auto* calendar = &args.thisv().toObject().as<CalendarObject>();
+
+  // Step 3.
+  args.rval().setString(calendar->identifier());
+  return true;
+}
+
+/**
+ * get Temporal.Calendar.prototype.id
+ */
+static bool Calendar_id(JSContext* cx, unsigned argc, Value* vp) {
+  // Steps 1-2.
+  CallArgs args = CallArgsFromVp(argc, vp);
+  return CallNonGenericMethod<IsCalendar, Calendar_id>(cx, args);
+}
+
+/**
+ * Temporal.Calendar.prototype.toString ( )
+ */
+static bool Calendar_toString(JSContext* cx, const CallArgs& args) {
+  auto* calendar = &args.thisv().toObject().as<CalendarObject>();
+
+  // Step 3.
+  args.rval().setString(calendar->identifier());
+  return true;
+}
+
+/**
+ * Temporal.Calendar.prototype.toString ( )
+ */
+static bool Calendar_toString(JSContext* cx, unsigned argc, Value* vp) {
+  // Steps 1-2.
+  CallArgs args = CallArgsFromVp(argc, vp);
+  return CallNonGenericMethod<IsCalendar, Calendar_toString>(cx, args);
+}
+
+/**
+ * Temporal.Calendar.prototype.toJSON ( )
+ */
+static bool Calendar_toJSON(JSContext* cx, const CallArgs& args) {
+  Rooted<JSObject*> calendar(cx, &args.thisv().toObject());
+
+  // Step 3.
+  JSString* str = CalendarToString(cx, calendar);
+  if (!str) {
+    return false;
+  }
+
+  args.rval().setString(str);
+  return true;
+}
+
+/**
+ * Temporal.Calendar.prototype.toJSON ( )
+ */
+static bool Calendar_toJSON(JSContext* cx, unsigned argc, Value* vp) {
+  // Steps 1-2.
+  CallArgs args = CallArgsFromVp(argc, vp);
+  return CallNonGenericMethod<IsCalendar, Calendar_toJSON>(cx, args);
 }
 
 const JSClass CalendarObject::class_ = {
@@ -41,10 +296,14 @@ static const JSFunctionSpec Calendar_methods[] = {
 };
 
 static const JSFunctionSpec Calendar_prototype_methods[] = {
+    JS_FN("toString", Calendar_toString, 0, 0),
+    JS_FN("toJSON", Calendar_toJSON, 0, 0),
     JS_FS_END,
 };
 
 static const JSPropertySpec Calendar_prototype_properties[] = {
+    JS_PSG("id", Calendar_id, 0),
+    JS_STRING_SYM_PS(toStringTag, "Temporal.Calendar", JSPROP_READONLY),
     JS_PS_END,
 };
 
