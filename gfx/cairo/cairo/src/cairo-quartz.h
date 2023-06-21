d8ad98c7c7fd0551ce11dc36fe7dce2e7ef55723
Mike Hommey <mh+mozilla@glandium.org>
2023-06-21 21:41:02 +0000

    Bug 1834933 - Apply patch from https://gitlab.freedesktop.org/cairo/cairo/-/issues/293. r=gfx-reviewers,jrmuizel
    With some minor change to remove the unused Carbon include.
    
    Differential Revision: https://phabricator.services.mozilla.com/D179518

--- gfx/cairo/cairo/src/cairo-quartz.h
+++ gfx/cairo/cairo/src/cairo-quartz.h
@@ -40,7 +40,14 @@
 
 #if CAIRO_HAS_QUARTZ_SURFACE
 
+#if CAIRO_HAS_QUARTZ_CORE_GRAPHICS
+#include <CoreGraphics/CoreGraphics.h>
+#include <CoreText/CoreText.h>
+#endif
+
+#if CAIRO_HAS_QUARTZ_APPLICATION_SERVICES
 #include <ApplicationServices/ApplicationServices.h>
+#endif
 
 CAIRO_BEGIN_DECLS
 
@@ -75,8 +82,10 @@ cairo_quartz_surface_get_image (cairo_surface_t *surface);
 cairo_public cairo_font_face_t *
 cairo_quartz_font_face_create_for_cgfont (CGFontRef font);
 
+#if CAIRO_HAS_QUARTZ_ATSUFONTID
 cairo_public cairo_font_face_t *
 cairo_quartz_font_face_create_for_atsu_font_id (ATSUFontID font_id);
+#endif /* CAIRO_HAS_QUARTZ_ATSUFONTID */
 
 #endif /* CAIRO_HAS_QUARTZ_FONT */
 
