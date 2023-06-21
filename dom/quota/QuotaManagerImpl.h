a4294f632bc93c1f24bb26efe1f2288a0d824021
Jan Varga <jvarga@mozilla.com>
2023-06-21 03:15:33 +0000

    Bug 1839417 - Move QuotaManager::CollectPendingOriginsForListing implementation to QuotaManagerImpl.h; r=dom-storage-reviewers,asuth
    QuotaManager::CollectPendingOriginsForListing is a public template function so
    it shouldn't be implemented in a cpp file.
    (needed for moving operations outside ActorsParent.cpp)
    
    Differential Revision: https://phabricator.services.mozilla.com/D181478

--- dom/quota/QuotaManagerImpl.h
+++ dom/quota/QuotaManagerImpl.h
@@ -7,9 +7,34 @@
 #ifndef DOM_QUOTA_QUOTAMANAGERIMPL_H_
 #define DOM_QUOTA_QUOTAMANAGERIMPL_H_
 
-#include "QuotaManager.h"
+#include "GroupInfo.h"
+#include "GroupInfoPair.h"
+#include "mozilla/dom/quota/QuotaManager.h"
+#include "OriginInfo.h"
 
 namespace mozilla::dom::quota {
+
+template <typename P>
+void QuotaManager::CollectPendingOriginsForListing(P aPredicate) {
+  MutexAutoLock lock(mQuotaMutex);
+
+  for (const auto& entry : mGroupInfoPairs) {
+    const auto& pair = entry.GetData();
+
+    MOZ_ASSERT(!entry.GetKey().IsEmpty());
+    MOZ_ASSERT(pair);
+
+    RefPtr<GroupInfo> groupInfo =
+        pair->LockedGetGroupInfo(PERSISTENCE_TYPE_DEFAULT);
+    if (groupInfo) {
+      for (const auto& originInfo : groupInfo->mOriginInfos) {
+        if (!originInfo->mDirectoryExists) {
+          aPredicate(originInfo);
+        }
+      }
+    }
+  }
+}
 
 template <typename F>
 void QuotaManager::MaybeRecordQuotaManagerShutdownStepWith(F&& aFunc) {
