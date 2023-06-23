18c72dda2e188b06955b4dde0fe41f8f3c0be448
Andrew McCreight <continuation@gmail.com>
2023-06-23 17:59:17 +0000

    Bug 1840088 - Add back type check to HTMLTableElement::BuildInheritedAttributes(). r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D181902

--- dom/html/HTMLTableElement.cpp
+++ dom/html/HTMLTableElement.cpp
@@ -935,7 +935,7 @@ void HTMLTableElement::BuildInheritedAttributes() {
     return;
   }
   const nsAttrValue* value = GetParsedAttr(nsGkAtoms::cellpadding);
-  if (!value) {
+  if (!value || value->Type() != nsAttrValue::eInteger) {
     return;
   }
   // We have cellpadding.  This will override our padding values if we don't
