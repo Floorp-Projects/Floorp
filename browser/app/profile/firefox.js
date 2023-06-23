9843aec7b16c62c0631502a9e72cb03380e96228
Benjamin VanderSloot <bvandersloot@mozilla.com>
2023-06-23 12:14:18 +0000

    Bug 1836792, part 5 - Stop the use of 3rdPartyFrameStorage permissions beyond Nightly - r=anti-tracking-reviewers,timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D180454

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1874,6 +1874,7 @@ pref("privacy.trackingprotection.cryptomining.enabled", true);
 pref("browser.contentblocking.database.enabled", true);
 
 pref("dom.storage_access.enabled", true);
+pref("dom.storage_access.frame_only", true);
 
 // Enable URL query stripping and strip on share in Nightly.
 #ifdef NIGHTLY_BUILD
