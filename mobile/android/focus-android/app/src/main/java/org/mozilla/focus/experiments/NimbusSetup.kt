feab2e3bfdeccc04fc43ec8ec255c5e0c93d744d
Roger Yang <royang@mozilla.com>
2023-06-23 14:36:26 -0400

    Bug 1840151 - Remove telemetry probes expiring in v118

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/experiments/NimbusSetup.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/experiments/NimbusSetup.kt
@@ -13,7 +13,6 @@ import org.json.JSONObject
 import org.mozilla.experiments.nimbus.NimbusInterface
 import org.mozilla.experiments.nimbus.internal.NimbusException
 import org.mozilla.focus.BuildConfig
-import org.mozilla.focus.GleanMetrics.NimbusExperiments
 import org.mozilla.focus.R
 import org.mozilla.focus.ext.components
 import org.mozilla.focus.ext.settings
@@ -89,14 +88,12 @@ internal fun finishNimbusInitialization(experiments: NimbusApi) =
             register(
                 object : NimbusInterface.Observer {
                     override fun onExperimentsFetched() {
-                        NimbusExperiments.nimbusInitialFetch.stop()
                         applyPendingExperiments()
                         // Remove lingering observer when we're done fetching experiments on startup.
                         unregister(this)
                     }
                 },
             )
-            NimbusExperiments.nimbusInitialFetch.start()
         }
         fetchExperiments()
     }
