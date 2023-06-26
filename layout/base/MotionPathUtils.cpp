5305ac622154b54d023488c22c63f55856b20c43
Boris Chiou <boris.chiou@gmail.com>
2023-06-26 23:23:54 +0000

    Bug 1598156 - Part 2: Rename Path as Shape in OffsetPathData. r=emilio
    Because we'd like to use it for all basic shapes.
    
    No behavior change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D181314

--- layout/base/MotionPathUtils.cpp
+++ layout/base/MotionPathUtils.cpp
@@ -284,8 +284,8 @@ Maybe<ResolvedMotionPathData> MotionPathUtils::ResolveMotionPath(
   // rotate.
   double directionAngle = 0.0;
   gfx::Point point;
-  if (aPath.IsPath()) {
-    const auto& path = aPath.AsPath();
+  if (aPath.IsShape()) {
+    const auto& path = aPath.AsShape();
     if (!path.mGfxPath) {
       // Empty gfx::Path means it is path('') (i.e. empty path string).
       return Nothing();
@@ -404,7 +404,7 @@ static OffsetPathData GenerateOffsetPathData(const nsIFrame* aFrame) {
         aFrame->GetProperty(nsIFrame::OffsetPathCache());
     MOZ_ASSERT(gfxPath || pathData._0.IsEmpty(),
                "Should have a valid cached gfx::Path or an empty path string");
-    return OffsetPathData::Path(pathData, gfxPath.forget());
+    return OffsetPathData::Shape(pathData, gfxPath.forget());
   }
 
   // FIXME: Bug 1598156. Handle other basic shapes.
@@ -459,7 +459,7 @@ static OffsetPathData GenerateOffsetPathData(
           MotionPathUtils::GetCompositorPathBuilder();
       path = MotionPathUtils::BuildPath(pathData, builder);
     }
-    return OffsetPathData::Path(pathData, path.forget());
+    return OffsetPathData::Shape(pathData, path.forget());
   }
 
   // FIXME: Bug 1598156. Handle other basic shapes.
