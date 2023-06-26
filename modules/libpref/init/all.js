ab7fdc401ccf38af263c5e5886ca2e7fe251a7f0
Gregory Pappas <gp3033@protonmail.com>
2023-06-26 22:05:37 +0000

    Bug 1837784 - Remove unused media.webrtc.debug.* prefs r=mjf
    Differential Revision: https://phabricator.services.mozilla.com/D180568

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -277,8 +277,6 @@ pref("media.videocontrols.keyboard-tab-to-all-controls", true);
   #endif
 
   pref("media.peerconnection.sdp.disable_stereo_fmtp", false);
-  pref("media.webrtc.debug.trace_mask", 0);
-  pref("media.webrtc.debug.multi_log", false);
   pref("media.webrtc.debug.log_file", "");
   pref("media.webrtc.debug.aec_dump_max_size", 4194304); // 4MB
 
