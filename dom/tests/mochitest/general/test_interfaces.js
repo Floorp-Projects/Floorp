19b05029179cfdcda3c721ef9c804570cc725c0e
Emilio Cobos Álvarez <emilio@crisal.io>
2023-06-27 11:10:33 +0000

    Bug 1840480 - [css-properties-values-api] Enable on nightly. r=zrhoffman,webidl,peterv
    Even though it still requires significant work, this allows us to keep
    track of progress more properly in wpt.fyi.
    
    Since we expect implementation progress to improve quite a bit in H2,
    and this shouldn't be an stability hazard, this seems fine unless the
    missing bits cause big compat issues.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182111

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -316,6 +316,8 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSPageRule", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "CSSPropertyRule", insecureContext: true, nightly: true },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSPseudoElement", insecureContext: true, disabled: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSRule", insecureContext: true },
