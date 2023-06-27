5d943204d31aefb429146204ba8f782504bcfa35
Cristian Tuns <ctuns@mozilla.com>
2023-06-27 01:25:48 -0400

    Backed out 3 changesets (bug 1829068) for causing wpt failures in setSinkId-with-MediaElementAudioSourceNode.https.html CLOSED TREE
    Backed out changeset 40cf2b588396 (bug 1829068)
    Backed out changeset 1a8f8cda9b25 (bug 1829068)
    Backed out changeset 5eb680967020 (bug 1829068)

--- dom/media/mediasink/DecodedStream.cpp
+++ dom/media/mediasink/DecodedStream.cpp
@@ -465,7 +465,8 @@ DecodedStream::DecodedStream(
       mPlaybackRate(aPlaybackRate),
       mPreservesPitch(aPreservesPitch),
       mAudioQueue(aAudioQueue),
-      mVideoQueue(aVideoQueue) {}
+      mVideoQueue(aVideoQueue),
+      mAudioDevice(std::move(aAudioDevice)) {}
 
 DecodedStream::~DecodedStream() {
   MOZ_ASSERT(mStartTime.isNothing(), "playback should've ended.");
@@ -719,12 +720,6 @@ void DecodedStream::SetPreservesPitch(bool aPreservesPitch) {
   if (mData && mData->mAudioTrack) {
     mData->mAudioTrack->SetPreservesPitch(aPreservesPitch);
   }
-}
-
-RefPtr<GenericPromise> DecodedStream::SetAudioDevice(
-    RefPtr<AudioDeviceInfo> aDevice) {
-  // All audio is captured, so nothing is actually played out, so nothing to do.
-  return GenericPromise::CreateAndResolve(true, __func__);
 }
 
 double DecodedStream::PlaybackRate() const {
