c7392e4f30651d823c4febbca5cccba432c3af73
Mike Kaply <mozilla@kaply.com>
2023-06-27 18:47:49 +0000

    Bug 1837037 - Add a pref for enabling the new Firefox View. r=kcochrane,fxview-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D182087

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -832,6 +832,7 @@ pref("browser.tabs.tooltipsShowPidAndActiveness", false);
 #endif
 
 pref("browser.tabs.firefox-view", true);
+pref("browser.tabs.firefox-view-next", false);
 pref("browser.tabs.firefox-view.logLevel", "Warn");
 pref("browser.tabs.firefox-view.notify-for-tabs", false);
 
