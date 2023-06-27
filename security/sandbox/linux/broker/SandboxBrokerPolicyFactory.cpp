5c4770cad783aef2accb8c6501b7cb9bd1015652
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2023-06-27 12:31:59 +0000

    Bug 1840371 - Allow /nix/store for loading ffmpeg dependencies r=gcp
    Differential Revision: https://phabricator.services.mozilla.com/D182011

--- security/sandbox/linux/broker/SandboxBrokerPolicyFactory.cpp
+++ security/sandbox/linux/broker/SandboxBrokerPolicyFactory.cpp
@@ -1036,6 +1036,9 @@ SandboxBrokerPolicyFactory::GetUtilityProcessPolicy(int aPid) {
   policy->AddDir(rdonly, "/usr/share");
   policy->AddDir(rdonly, "/usr/local/share");
   policy->AddDir(rdonly, "/etc");
+  // Required to make sure ffmpeg loads properly, this is already existing on
+  // Content and RDD
+  policy->AddDir(rdonly, "/nix/store");
 
   // glibc will try to stat64("/") while populating nsswitch database
   // https://sourceware.org/git/?p=glibc.git;a=blob;f=nss/nss_database.c;h=cf0306adc47f12d9bc761ab1b013629f4482b7e6;hb=9826b03b747b841f5fc6de2054bf1ef3f5c4bdf3#l396
