d02382c30a81466035a6867534d6b9f0f000689f
Chun-Min Chang <chun.m.chang@gmail.com>
2023-06-27 17:24:13 +0000

    Bug 1839270 - Prevent overflow in ComputeLayoutAndAllocationSize r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D181589

--- dom/media/webcodecs/VideoFrame.cpp
+++ dom/media/webcodecs/VideoFrame.cpp
@@ -558,22 +558,48 @@ static Result<CombinedBufferLayout, nsCString> ComputeLayoutAndAllocationSize(
   for (size_t i = 0; i < planes.Length(); ++i) {
     const VideoFrame::Format::Plane& p = planes[i];
     const gfx::IntSize sampleSize = aFormat.SampleSize(p);
+    MOZ_RELEASE_ASSERT(!sampleSize.IsEmpty());
+
+    // aRect's x, y, width, and height are int32_t, and sampleSize's width and
+    // height >= 1, so (aRect.* / sampleSize.*) must be in int32_t range.
+
+    CheckedUint32 sourceTop(aRect.Y());
+    sourceTop /= sampleSize.Height();
+    MOZ_RELEASE_ASSERT(sourceTop.isValid());
+
+    CheckedUint32 sourceHeight(aRect.Height());
+    sourceHeight /= sampleSize.Height();
+    MOZ_RELEASE_ASSERT(sourceHeight.isValid());
+
+    CheckedUint32 sourceLeftBytes(aRect.X());
+    sourceLeftBytes /= sampleSize.Width();
+    MOZ_RELEASE_ASSERT(sourceLeftBytes.isValid());
+    sourceLeftBytes *= aFormat.SampleBytes(p);
+    if (!sourceLeftBytes.isValid()) {
+      return Err(nsPrintfCString(
+          "The parsed-rect's x-offset is too large for %s plane",
+          aFormat.PlaneName(p)));
+    }
+
+    CheckedUint32 sourceWidthBytes(aRect.Width());
+    sourceWidthBytes /= sampleSize.Width();
+    MOZ_RELEASE_ASSERT(sourceWidthBytes.isValid());
+    sourceWidthBytes *= aFormat.SampleBytes(p);
+    if (!sourceWidthBytes.isValid()) {
+      return Err(
+          nsPrintfCString("The parsed-rect's width is too large for %s plane",
+                          aFormat.PlaneName(p)));
+    }
 
     // TODO: Spec here is wrong so we do differently:
     // https://github.com/w3c/webcodecs/issues/511
     // This comment should be removed once the issue is resolved.
-    ComputedPlaneLayout layout{
-        .mDestinationOffset = 0,
-        .mDestinationStride = 0,
-        .mSourceTop = (CheckedUint32(aRect.Y()) / sampleSize.Height()).value(),
-        .mSourceHeight =
-            (CheckedUint32(aRect.Height()) / sampleSize.Height()).value(),
-        .mSourceLeftBytes = (CheckedUint32(aRect.X()) / sampleSize.Width() *
-                             aFormat.SampleBytes(p))
-                                .value(),
-        .mSourceWidthBytes = (CheckedUint32(aRect.Width()) /
-                              sampleSize.Width() * aFormat.SampleBytes(p))
-                                 .value()};
+    ComputedPlaneLayout layout{.mDestinationOffset = 0,
+                               .mDestinationStride = 0,
+                               .mSourceTop = sourceTop.value(),
+                               .mSourceHeight = sourceHeight.value(),
+                               .mSourceLeftBytes = sourceLeftBytes.value(),
+                               .mSourceWidthBytes = sourceWidthBytes.value()};
     if (aPlaneLayouts) {
       const PlaneLayout& planeLayout = aPlaneLayouts->ElementAt(i);
       if (planeLayout.mStride < layout.mSourceWidthBytes) {
