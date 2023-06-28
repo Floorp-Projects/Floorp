d4ac971a6a70dffeea7b5d1f9076f5e427b8126b
Jon Coppeard <jcoppeard@mozilla.com>
2023-06-28 08:43:43 +0000

    Bug 1803984 - Part 3: Add tests for the interaction between speculative preloading and module import maps r=allstarschh
    Depends on D178787
    
    Differential Revision: https://phabricator.services.mozilla.com/D178775

---
+++ dom/base/test/jsmodules/importmaps/module_importMap_with_external_script_1.js
@@ -0,0 +1,2 @@
+// Missing file ./bad/module_1.js should be mapped to ./good/module_1.js.
+import {} from "./bad/module_1.js";
---
+++ dom/base/test/jsmodules/importmaps/module_importMap_with_external_script_2.js
@@ -0,0 +1,2 @@
+// Existing file ./bad/module_2.js should be mapped to ./good/module_2.js.
+import {} from "./bad/module_2.js";
