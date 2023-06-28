d61d5dff75d49f53e30f0ae7a6ab17e33799a25f
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-06-28 18:17:11 +0000

    Bug 1839575 - [devtools] Refresh ancestor rules when updating nested rule. r=devtools-reviewers,ochameau.
    When modifying a rule in "authored" mode, the rule's text is updated.
    If we're dealing with a nested rule, that means we need to update the authored
    text of all its ancestor rules as well.
    
    A test is added to ensure the references are properly updated.
    
    Differential Revision: https://phabricator.services.mozilla.com/D181676

---
+++ devtools/client/inspector/rules/test/browser_rules_edit-property-nested-rules.js
@@ -0,0 +1,104 @@
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Testing editing nested rules in the rule view.
+
+const STYLE = `
+    main {
+      background-color: tomato;
+      & > .foo {
+        background-color: teal;
+        &.foo {
+          color: gold;
+        }
+      }
+    }`;
+
+const HTML = `
+  <main>
+    Hello
+    <div class=foo>Nested</div>
+  </main>`;
+
+const TEST_URI_INLINE_SHEET = `
+  <style>${STYLE}</style>
+  ${HTML}`;
+
+const TEST_URI_CONSTRUCTED_SHEET = `
+  ${HTML}
+  <script>
+    const sheet = new CSSStyleSheet();
+    sheet.replaceSync(\`${STYLE}\`);
+    document.adoptedStyleSheets.push(sheet);
+  </script>
+`;
+
+add_task(async function test_inline_sheet() {
+  info("Run test with inline stylesheet");
+  await runTest(TEST_URI_INLINE_SHEET);
+});
+
+add_task(async function test_constructed_sheet() {
+  info("Run test with constructed stylesheet");
+  await runTest(TEST_URI_CONSTRUCTED_SHEET);
+});
+
+async function runTest(uri) {
+  await pushPref("layout.css.nesting.enabled", true);
+  await addTab(`data:text/html,<meta charset=utf8>${encodeURIComponent(uri)}`);
+  const { inspector, view } = await openRuleView();
+
+  await selectNode(".foo", inspector);
+
+  info(`Modify color in "&.foo" rule`);
+  await updateDeclaration(view, 1, { color: "gold" }, { color: "white" });
+  is(
+    await getComputedStyleProperty(".foo", null, "color"),
+    "rgb(255, 255, 255)",
+    "color was set to white on .foo"
+  );
+
+  info(`Modify background-color in "& > .foo" rule`);
+  await updateDeclaration(
+    view,
+    2,
+    { "background-color": "teal" },
+    { "background-color": "blue" }
+  );
+  is(
+    await getComputedStyleProperty(".foo", null, "background-color"),
+    "rgb(0, 0, 255)",
+    "background-color was set to blue on .foo…"
+  );
+  is(
+    await getComputedStyleProperty(".foo", null, "color"),
+    "rgb(255, 255, 255)",
+    "…and color is still white"
+  );
+
+  await selectNode("main", inspector);
+  info(`Modify background-color in "main" rule`);
+  await updateDeclaration(
+    view,
+    1,
+    { "background-color": "tomato" },
+    { "background-color": "red" }
+  );
+  is(
+    await getComputedStyleProperty("main", null, "background-color"),
+    "rgb(255, 0, 0)",
+    "background-color was set to red on <main>…"
+  );
+  is(
+    await getComputedStyleProperty(".foo", null, "background-color"),
+    "rgb(0, 0, 255)",
+    "…background-color is still blue on .foo…"
+  );
+  is(
+    await getComputedStyleProperty(".foo", null, "color"),
+    "rgb(255, 255, 255)",
+    "…and color is still white"
+  );
+}
---
+++ devtools/client/inspector/rules/test/browser_rules_edit-selector-nested-rules.js
@@ -0,0 +1,61 @@
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Testing editing nested rules selector in the rule view.
+
+const STYLE = `
+    h1 {
+      color: lime;
+      &.foo {
+        color: red;
+      }
+    }`;
+
+const HTML = `<h1 class=foo>Nested</h1>`;
+
+const TEST_URI_INLINE_SHEET = `
+  <style>${STYLE}</style>
+  ${HTML}`;
+
+add_task(async function test_inline_sheet() {
+  info("Run test with inline stylesheet");
+  await runTest(TEST_URI_INLINE_SHEET);
+});
+
+async function runTest(uri) {
+  await pushPref("layout.css.nesting.enabled", true);
+  await addTab(`data:text/html,<meta charset=utf8>${encodeURIComponent(uri)}`);
+  const { inspector, view } = await openRuleView();
+
+  await selectNode("h1", inspector);
+
+  is(
+    await getComputedStyleProperty("h1", null, "color"),
+    "rgb(255, 0, 0)",
+    "h1 color is red initially"
+  );
+
+  info(`Modify "&.foo" selector into "&.bar"`);
+  const ruleEditor = getRuleViewRuleEditor(view, 1);
+  const editor = await focusEditableField(view, ruleEditor.selectorText);
+  const onRuleViewChanged = view.once("ruleview-changed");
+  editor.input.value = "&.bar";
+  EventUtils.synthesizeKey("KEY_Enter");
+  await onRuleViewChanged;
+
+  is(
+    await getComputedStyleProperty("h1", null, "color"),
+    "rgb(0, 255, 0)",
+    "h1 color is now lime, as the new selector does not match the element"
+  );
+
+  info(`Modify color in "h1" rule to blue`);
+  await updateDeclaration(view, 2, { color: "lime" }, { color: "blue" });
+  is(
+    await getComputedStyleProperty("h1", null, "color"),
+    "rgb(0, 0, 255)",
+    "h1 color is now blue"
+  );
+}
