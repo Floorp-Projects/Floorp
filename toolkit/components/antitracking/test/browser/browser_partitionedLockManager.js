a16361de007409df835b04978e33eddc8fdf44df
Kagami Sascha Rosylight <krosylight@mozilla.com>
2023-06-28 20:48:14 +0000

    Bug 1798493 - Part 2: Use ShouldPartitionStorage in LockManager::Request r=asuth,anti-tracking-reviewers,pbz
    For now this only partitions third party contexts but not the first party context with third party ancestors. The latter will keep getting the first party storage access.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174263

---
+++ toolkit/components/antitracking/test/browser/browser_partitionedLockManager.js
@@ -0,0 +1,24 @@
+/* import-globals-from partitionedstorage_head.js */
+
+PartitionedStorageHelper.runTest(
+  "LockManager works in both first and third party contexts",
+  async (win3rdParty, win1stParty, allowed) => {
+    let locks = [];
+    ok(win1stParty.isSecureContext, "1st party is in a secure context");
+    ok(win3rdParty.isSecureContext, "3rd party is in a secure context");
+    await win1stParty.navigator.locks.request("foo", lock => {
+      locks.push(lock);
+      ok(true, "locks.request succeeded for 1st party");
+    });
+
+    await win3rdParty.navigator.locks.request("foo", lock => {
+      locks.push(lock);
+      ok(true, "locks.request succeeded for 3rd party");
+    });
+
+    is(locks.length, 2, "We should have granted 2 lock requests at this point");
+  },
+  /* cleanupFunction */ undefined,
+  /* extraPrefs */ undefined,
+  { runInSecureContext: true }
+);
