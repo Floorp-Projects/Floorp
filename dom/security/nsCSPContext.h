98ec0ec8ba0030659f50747bd7bf67176aa19652
Tom Schuster <tschuster@mozilla.com>
2023-06-28 08:12:20 +0000

    Bug 1839165 - Throttle the number of CSP reports that are send. r=freddyb
    Differential Revision: https://phabricator.services.mozilla.com/D181392

--- dom/security/nsCSPContext.h
+++ dom/security/nsCSPContext.h
@@ -151,6 +151,10 @@ class nsCSPContext : public nsIContentSecurityPolicy {
  private:
   void EnsureIPCPoliciesRead();
 
+  bool ShouldThrottleReport(
+      const mozilla::dom::SecurityPolicyViolationEventInit&
+          aViolationEventInit);
+
   bool permitsInternal(CSPDirective aDir,
                        mozilla::dom::Element* aTriggeringElement,
                        nsICSPEventListener* aCSPEventListener,
@@ -192,6 +196,10 @@ class nsCSPContext : public nsIContentSecurityPolicy {
   nsTArray<ConsoleMsgQueueElem> mConsoleMsgQueue;
   bool mQueueUpMessages;
   nsCOMPtr<nsIEventTarget> mEventTarget;
+
+  mozilla::TimeStamp mSendReportLimitSpanStart;
+  uint32_t mSendReportLimitCount = 1;
+  bool mWarnedAboutTooManyReports = false;
 };
 
 // Class that listens to violation report transmission and logs errors.
