cab63f2fa6dfe327ca96f0383471a3001e5f1764
Stanca Serban <sstanca@mozilla.com>
2023-06-29 00:46:26 +0300

    Backed out 3 changesets (bug 1838694) for causing mochitests failures in test_interfaces_secureContext.html. CLOSED TREE
    Backed out changeset 892fed91da4e (bug 1838694)
    Backed out changeset f6477420370e (bug 1838694)
    Backed out changeset 109ebd10d0c3 (bug 1838694)

--- dom/webgpu/ValidationError.cpp
+++ dom/webgpu/ValidationError.cpp
@@ -5,16 +5,36 @@
 
 #include "ValidationError.h"
 #include "mozilla/dom/WebGPUBinding.h"
+#include "mozilla/ErrorResult.h"
+#include "nsIGlobalObject.h"
+#include "nsReadableUtils.h"
 
 namespace mozilla::webgpu {
 
+GPU_IMPL_CYCLE_COLLECTION(ValidationError, mGlobal)
 GPU_IMPL_JS_WRAP(ValidationError)
+
+ValidationError::ValidationError(nsIGlobalObject* aGlobal,
+                                 const nsACString& aMessage)
+    : mGlobal(aGlobal) {
+  CopyUTF8toUTF16(aMessage, mMessage);
+}
+
+ValidationError::ValidationError(nsIGlobalObject* aGlobal,
+                                 const nsAString& aMessage)
+    : mGlobal(aGlobal), mMessage(aMessage) {}
+
+ValidationError::~ValidationError() = default;
 
 already_AddRefed<ValidationError> ValidationError::Constructor(
     const dom::GlobalObject& aGlobal, const nsAString& aString,
     ErrorResult& aRv) {
   nsCOMPtr<nsIGlobalObject> global = do_QueryInterface(aGlobal.GetAsSupports());
-  MOZ_RELEASE_ASSERT(global);
+  if (!global) {
+    aRv.ThrowInvalidStateError("aGlobal is not nsIGlobalObject");
+    return nullptr;
+  }
+
   return MakeAndAddRef<ValidationError>(global, aString);
 }
 
