72b740257e0db5121e15082d9a200f85f35a584b
Manuel Bucher <manuel@mozilla.com>
2023-06-29 14:23:47 +0000

    Bug 1715401 - Fix highly compressed brotli responses with unknown content-type being rejected r=necko-reviewers,valentin
    Highly compressed brotli files contain meta blocks that don't produce
    output from the first 512 Bytes. That is because the meta block header
    might be longer than 512 Bytes.[1] NBLTYPESL, NBLTYPESI, NBLTYPESD,
    NTREESL, NTREESD all can blow up the header size. Each at least by
    2*256 bits.
    
    With this solution the Content-Type would be always detected as
    `octet-stream`. That might not be the correct one. If a different
    Content-Type is required, the server can still specify it with the
    `Content-Type`-Header.[2]
    
    This solution looks directly at the error code to determine invalid
    encoding instead of relying on number of output bytes.
    
    This also doesn't throw an error anymore for truncated brotli documents
    that don't produce any output. But since we generally allow truncated
    formats[3], it makes the implementation a bit more consistent.
    
    [1]: https://www.rfc-editor.org/rfc/rfc7932#section-9.2
    [2]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
    [3]: https://bugzilla.mozilla.org/show_bug.cgi?id=1242904#c4
    
    Differential Revision: https://phabricator.services.mozilla.com/D175460

---
+++ netwerk/test/unit/test_brotli_unknown_content_type.js
@@ -0,0 +1,72 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This test file makes sure that we can decode brotli files when the
+// Content-Type header is missing (Bug 1715401)
+
+"use strict";
+
+function emptyBrotli(metadata, response) {
+  response.setHeader("Content-Encoding", "br", false);
+  response.write("\x01\x03\x06\x03");
+}
+
+function largeEmptyBrotli(metadata, response) {
+  response.setHeader("Content-Encoding", "br", false);
+  response.write("\x01\x03" + "\x06".repeat(600) + "\x03");
+}
+
+const { HttpServer } = ChromeUtils.import("resource://testing-common/httpd.js");
+
+XPCOMUtils.defineLazyGetter(this, "URL_EMPTY_BROTLI", function () {
+  return (
+    "http://localhost:" + httpServer.identity.primaryPort + "/empty-brotli"
+  );
+});
+
+XPCOMUtils.defineLazyGetter(this, "URL_LARGE_EMPTY_BROTLI", function () {
+  return (
+    "http://localhost:" +
+    httpServer.identity.primaryPort +
+    "/large-empty-brotli"
+  );
+});
+
+var httpServer = null;
+
+add_task(async function check_brotli() {
+  httpServer = new HttpServer();
+  httpServer.registerPathHandler("/empty-brotli", emptyBrotli);
+  httpServer.registerPathHandler("/large-empty-brotli", largeEmptyBrotli);
+  httpServer.start(-1);
+
+  async function test(url) {
+    let chan = NetUtil.newChannel({ uri: url, loadUsingSystemPrincipal: true });
+    let [, response] = await new Promise(resolve => {
+      chan.asyncOpen(
+        new ChannelListener(
+          (req, buff) => {
+            resolve([req, buff]);
+          },
+          null,
+          CL_IGNORE_CL
+        )
+      );
+    });
+    return response;
+  }
+  equal(
+    await test(URL_EMPTY_BROTLI),
+    "",
+    "Should decode brotli even when brotli output is empty"
+  );
+  equal(
+    await test(URL_LARGE_EMPTY_BROTLI),
+    "",
+    "Should decode brotli even when the nsUnknownDecoder can't get any decoded output"
+  );
+  Services.prefs.clearUserPref("network.http.accept-encoding");
+  Services.prefs.clearUserPref("network.http.encoding.trustworthy_is_https");
+  await httpServer.stop();
+});
