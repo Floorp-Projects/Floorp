668f321bf60e88c02d72aa3f07962ce3395bb82a
Chun-Min Chang <chun.m.chang@gmail.com>
2023-06-29 22:33:14 +0000

    Bug 1831451 - Expose VideoDecoder in test_(worker_)interfaces.html r=webidl,saschanaz,padenot
    Depends on D181327
    
    Differential Revision: https://phabricator.services.mozilla.com/D181328

--- dom/workers/test/test_worker_interfaces.js
+++ dom/workers/test/test_worker_interfaces.js
@@ -348,6 +348,8 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "VideoColorSpace", insecureContext: true, nightly: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "VideoDecoder", nightly: true },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "VideoFrame", insecureContext: true, nightly: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "WebGL2RenderingContext", insecureContext: true },
