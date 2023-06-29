499d69aa0328a73f3e1e690aa17842a823192914
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-06-29 10:46:40 +0000

    Bug 1840862 - [devtools] Enable nesting in browser_resources_css_messages.js and browser_webconsole_css_error_impacted_elements.js. r=devtools-reviewers,ochameau.
    Depends on D182384
    
    Differential Revision: https://phabricator.services.mozilla.com/D182385

--- devtools/shared/commands/resource/tests/browser_resources_css_messages.js
+++ devtools/shared/commands/resource/tests/browser_resources_css_messages.js
@@ -25,6 +25,7 @@ httpServer.registerPathHandler(`/test_css_messages.html`, (req, res) => {
 const TEST_URI = `http://localhost:${httpServer.identity.primaryPort}/test_css_messages.html`;
 
 add_task(async function () {
+  await pushPref("layout.css.nesting.enabled", true);
   await testWatchingCssMessages();
   await testWatchingCachedCssMessages();
 });
