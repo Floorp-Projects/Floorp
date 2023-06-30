786ea4489799345434f2c4f79cfad41a61c9d603
Jan-Ivar Bruaroey <jib@mozilla.com>
2023-06-30 15:06:55 -0400

    Bug 1839451 - Vendor libwebrtc from b17957ac52
    Upstream commit: https://webrtc.googlesource.com/src/+/b17957ac52c833396dc220fa6bf61d02b8f5cbc1
        Use an audio file when AudioConfig::input_file_name is specified.
    
        Change-Id: Ib4d400c8fbefc13556f9b47c62505cbb7f7c6dbc
        Bug: None
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/298741
        Commit-Queue: Jeremy Leconte <jleconte@google.com>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39643}

--- third_party/libwebrtc/test/pc/e2e/peer_params_preprocessor.cc
+++ third_party/libwebrtc/test/pc/e2e/peer_params_preprocessor.cc
@@ -200,12 +200,8 @@ void PeerParamsPreprocessor::ValidateParams(const PeerConfigurer& peer) {
              "video and one audio). Duplicate audio_config.sync_group="
           << p.audio_config->sync_group.value();
     }
-    // Check that if mode input file name specified only if mode is kFile.
-    if (p.audio_config.value().mode == AudioConfig::Mode::kGenerated) {
-      RTC_CHECK(!p.audio_config.value().input_file_name);
-    }
-    if (p.audio_config.value().mode == AudioConfig::Mode::kFile) {
-      RTC_CHECK(p.audio_config.value().input_file_name);
+    // Check that if input file name specified, the file actually exists.
+    if (p.audio_config.value().input_file_name) {
       RTC_CHECK(
           test::FileExists(p.audio_config.value().input_file_name.value()))
           << p.audio_config.value().input_file_name.value() << " doesn't exist";
