20598ecf05690aea487848b73fe4672d0a43e9ff
Noemi Erli <nerli@mozilla.com>
2023-07-02 08:09:16 +0300

    Backed out 2 changesets (bug 1806897, bug 1824230) for causing webredenr failures CLOSED TREE
    Backed out changeset 8a3e28c800cf (bug 1824230)
    Backed out changeset 83439fa6f77c (bug 1806897)

--- testing/web-platform/tests/webdriver/tests/bidi/input/__init__.py
+++ testing/web-platform/tests/webdriver/tests/bidi/input/__init__.py
@@ -2,19 +2,15 @@ import json
 
 from webdriver.bidi.modules.script import ContextTarget
 
-async def get_object_from_context(bidi_session, context, object_path):
-    """Return a plain JS object from a given context, accessible at the given object_path"""
-    events_str = await bidi_session.script.evaluate(
-        expression=f"JSON.stringify({object_path})",
-        target=ContextTarget(context),
-        await_promise=False,
-    )
-    return json.loads(events_str["value"])
-
 
 async def get_events(bidi_session, context):
     """Return list of key events recorded on the test_actions.html page."""
-    events = await get_object_from_context(bidi_session, context, "allEvents.events")
+    events_str = await bidi_session.script.evaluate(
+        expression="JSON.stringify(allEvents.events)",
+        target=ContextTarget(context),
+        await_promise=False,
+    )
+    events = json.loads(events_str["value"])
 
     # `key` values in `allEvents` may be escaped (see `escapeSurrogateHalf` in
     # test_actions.html), so this converts them back into unicode literals.
--- testing/web-platform/tests/webdriver/tests/bidi/input/perform_actions/__init__.py
+++ testing/web-platform/tests/webdriver/tests/bidi/input/perform_actions/__init__.py
@@ -1,23 +1,6 @@
 from webdriver.bidi.modules.script import ContextTarget
 
 from ... import get_viewport_dimensions, remote_mapping_to_dict
-from .. import get_object_from_context
-
-async def assert_pointer_events(
-    bidi_session, context, expected_events, target, pointer_type
-):
-    events = await get_object_from_context(
-        bidi_session, context["context"], "window.recordedEvents"
-    )
-
-    assert len(events) == len(expected_events)
-    event_types = [e["type"] for e in events]
-    assert expected_events == event_types
-
-    for e in events:
-        assert e["target"] == target
-        assert e["pointerType"] == pointer_type
-
 
 async def get_inview_center_bidi(bidi_session, context, element):
     elem_rect = await get_element_rect(bidi_session, context=context, element=element)
@@ -56,31 +39,3 @@ el => el.getBoundingClientRect().toJSON()
     )
 
     return remote_mapping_to_dict(result["value"])
-
-
-async def record_pointer_events(bidi_session, context, container, selector):
-    # Record basic mouse / pointer events on the element matching the given
-    # selector in the container.
-    # The serialized element will be returned
-    target = await bidi_session.script.call_function(
-        function_declaration=f"""container => {{
-            const target = container.querySelector("{selector}");
-            window.recordedEvents = [];
-            function onPointerEvent(event) {{
-                window.recordedEvents.push({{
-                    "type": event.type,
-                    "pointerType": event.pointerType,
-                    "target": event.target.id
-                }});
-            }}
-            target.addEventListener("pointerdown", onPointerEvent);
-            target.addEventListener("pointerup", onPointerEvent);
-            return target;
-        }}
-        """,
-        arguments=[container],
-        target=ContextTarget(context["context"]),
-        await_promise=False,
-    )
-
-    return target
