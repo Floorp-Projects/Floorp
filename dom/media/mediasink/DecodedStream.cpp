0ad23eb4aec5a124d48d87b6f9e8ed9fc6771f62
Karl Tomlinson <karlt+@karlt.net>
2023-07-03 19:31:28 +0000

    Bug 1838365 use minimum TrackTime duration for extra end-of-track frame r=padenot, a=dsmith
    Rounding could lead to a zero duration from the previous logic if
    mStartTime's denominator is not a multiple of the TrackRate.
    
    The TimeStamp passed to WriteVideoToSegment usually corresponds to the aStart
    TimeUnit [1] (unless avoiding a previous overlapping frame), and when this was
    changed from aEnd [2], no reason was given for leaving the TimeStamp for the
    EOS frame different [3], so the TimeStamp for the EOS frame now corresponds to
    aStart rather than aEnd.
    
    [1] https://searchfox.org/mozilla-central/rev/3b707c8fd7e978eebf24279ee51ccf07895cfbcb/dom/media/mediasink/DecodedStream.cpp#974,982-984,991
    [2] https://hg.mozilla.org/mozilla-central/rev/7b980f1c50a127050f831b0a7d580eddc2939d27#l5.72
    [3] https://hg.mozilla.org/mozilla-central/rev/7b980f1c50a127050f831b0a7d580eddc2939d27#l5.97
    
    Differential Revision: https://phabricator.services.mozilla.com/D182556

--- dom/media/mediasink/DecodedStream.cpp
+++ dom/media/mediasink/DecodedStream.cpp
@@ -1018,22 +1018,20 @@ void DecodedStream::SendVideo(const PrincipalHandle& aPrincipalHandle) {
     }
     if (compensateEOS) {
       VideoSegment endSegment;
-      // Calculate the deviation clock time from DecodedStream.
-      // We round the nr of microseconds up, because WriteVideoToSegment
-      // will round the conversion from microseconds to TrackTime down.
-      auto deviation = TimeUnit::FromMicroseconds(
-          mData->mVideoTrack->TrackTimeToMicroseconds(1) + 1);
       auto start = mData->mLastVideoEndTime.valueOr(mStartTime.ref());
       mData->WriteVideoToSegment(
-          mData->mLastVideoImage, start, start + deviation,
+          mData->mLastVideoImage, start, start,
           mData->mLastVideoImageDisplaySize,
-          currentTime + (start + deviation - currentPosition).ToTimeDuration(),
-          &endSegment, aPrincipalHandle, mPlaybackRate);
+          currentTime + (start - currentPosition).ToTimeDuration(), &endSegment,
+          aPrincipalHandle, mPlaybackRate);
+      // ForwardedInputTrack drops zero duration frames, even at the end of
+      // the track.  Give the frame a minimum duration so that it is not
+      // dropped.
+      endSegment.ExtendLastFrameBy(1);
       LOG_DS(LogLevel::Debug,
-             "compensateEOS: deviation %s, start %s, duration %" PRId64
+             "compensateEOS: start %s, duration %" PRId64
              ", mPlaybackRate %lf, sample rate %" PRId32,
-             deviation.ToString().get(), start.ToString().get(),
-             endSegment.GetDuration(), mPlaybackRate,
+             start.ToString().get(), endSegment.GetDuration(), mPlaybackRate,
              mData->mVideoTrack->mSampleRate);
       MOZ_ASSERT(endSegment.GetDuration() > 0);
       if (forceBlack) {
