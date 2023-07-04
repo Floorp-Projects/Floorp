8f689b45fe739e8940f9593147ead6add777fe82
Luca Greco <lgreco@mozilla.com>
2023-07-04 14:43:12 +0000

    Bug 1830767 - Forcefully set EventManager resetEventOnIdle to false for instances not related to an event page. r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D178762

--- toolkit/components/extensions/test/xpcshell/test_ext_experiments.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_experiments.js
@@ -30,7 +30,10 @@ let fooExperimentAPIs = {
     child: {
       scopes: ["addon_child"],
       script: "child.js",
-      paths: [["experiments", "foo", "child"]],
+      paths: [
+        ["experiments", "foo", "child"],
+        ["experiments", "foo", "onChildEvent"],
+      ],
     },
   },
 };
@@ -60,6 +63,13 @@ let fooExperimentFiles = {
           returns: { type: "string" },
         },
       ],
+      events: [
+        {
+          name: "onChildEvent",
+          type: "function",
+          parameters: [],
+        },
+      ],
     },
   ]),
 
@@ -83,12 +93,48 @@ let fooExperimentFiles = {
   "child.js": () => {
     this.foo = class extends ExtensionAPI {
       getAPI(context) {
+        const EventManagerWithAssertions = class extends ExtensionCommon.EventManager {
+          constructor(...args) {
+            super(...args);
+            this.assertResetOnIdleOnEvent();
+          }
+
+          assertResetOnIdleOnEvent() {
+            const expectResetIdleOnEventFalse =
+              this.context.envType !== "addon_parent" ||
+              !this.context.isBackgroundContext ||
+              this.context.extension.persistentBackground;
+            if (expectResetIdleOnEventFalse && this.resetIdleOnEvent) {
+              const details = {
+                eventManagerName: this.name,
+                resetIdleOnEvent: this.resetIdleOnEvent,
+                envType: this.context.envType,
+                viewType: this.context.viewType,
+                isBackgroundContext: this.context.isBackgroundContext,
+                persistentBackground:
+                  this.context.extension.persistentBackground,
+              };
+              throw new Error(
+                `EventManagerWithAssertions: resetIdleOnEvent should be forcefully set to false - ${JSON.stringify(
+                  details
+                )}`
+              );
+            }
+          }
+        };
         return {
           experiments: {
             foo: {
               child() {
                 return "child";
               },
+              onChildEvent: new EventManagerWithAssertions({
+                context,
+                name: `experiments.foo.onChildEvent`,
+                register: fire => {
+                  return () => {};
+                },
+              }).api(),
             },
           },
         };
@@ -374,4 +420,34 @@ add_task(async function test_unbundled_experiments() {
 
   await extension.unload();
   await apiExtension.unload();
+});
+
+add_task(async function test_eventpage_with_experiments_resetOnIdleAssert() {
+  async function event_page() {
+    browser.test.log("EventPage startup");
+    // We expect EventManagerWithAssertions instance to throw
+    // here if the resetIdleOnEvent didn't got forcefully
+    // set to false for the EventManager instantiated in
+    // the child process.
+    browser.experiments.foo.onChildEvent.addListener(() => {});
+    browser.test.sendMessage("eventpage:ready");
+  }
+
+  const extension = ExtensionTestUtils.loadExtension({
+    isPrivileged: true,
+    manifest: {
+      experiment_apis: fooExperimentAPIs,
+      background: { persistent: false },
+    },
+
+    background: event_page,
+
+    files: fooExperimentFiles,
+  });
+
+  await extension.startup();
+
+  await extension.awaitMessage("eventpage:ready");
+
+  await extension.unload();
 });
