f9438324f20255b6f0b450084443cf59ed8e4d69
Rob Buis <rbuis@igalia.com>
2023-07-05 11:48:16 +0000

    Bug 1835813 - support contain-intrinsic-size: auto none r=Oriol,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D182462

--- servo/components/style/values/generics/box.rs
+++ servo/components/style/values/generics/box.rs
@@ -98,6 +98,8 @@ impl<L> ToAnimatedZero for VerticalAlign<L> {
 pub enum GenericContainIntrinsicSize<L> {
     /// The keyword `none`.
     None,
+    /// The keywords 'auto none',
+    AutoNone,
     /// A non-negative length.
     Length(L),
     /// "auto <Length>"
@@ -113,6 +115,7 @@ impl<L: ToCss> ToCss for ContainIntrinsicSize<L> {
     {
         match *self {
             Self::None => dest.write_str("none"),
+            Self::AutoNone => dest.write_str("auto none"),
             Self::Length(ref l) => l.to_css(dest),
             Self::AutoLength(ref l) => {
                 dest.write_str("auto ")?;
