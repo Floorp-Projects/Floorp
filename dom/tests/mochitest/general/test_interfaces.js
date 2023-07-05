40bf83dc2cfaae8483fa3008431c9d4682f765ed
Erich Gubler <erichdongubler@gmail.com>
2023-07-05 14:16:40 +0000

    Bug 1838694: fix(webgpu): impl. correct `GPUError` types r=jgilbert,webgpu-reviewers,webidl,peterv
    Differential Revision: https://phabricator.services.mozilla.com/D181690

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -514,6 +514,10 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "GPUDeviceLostInfo", nightly: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "GPUError", nightly: true },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
+  { name: "GPUInternalError", nightly: true },
+  // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "GPUMapMode", nightly: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "GPUOutOfMemoryError", nightly: true },
