0d74f46ea6993784e59a73ded3f8d91bdc30d02a
Dão Gottwald <dao@mozilla.com>
2023-07-06 13:39:15 +0000

    Bug 713713 - Expand BrowserWindowTracker.openWindow to cover the use cases of OpenBrowserWindow, and remove BrowserUIUtils.openNewBrowserWindow. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D182557

--- browser/base/content/test/menubar/browser_search_bookmarks.js
+++ browser/base/content/test/menubar/browser_search_bookmarks.js
@@ -12,26 +12,30 @@ add_task(async function test_menu_search_bookmarks_with_window_open() {
 });
 
 add_task(async function test_menu_search_bookmarks_in_new_window() {
-  info("Opening bookmarks menu");
-  let newWindow = await BrowserUIUtils.openNewBrowserWindow();
+  let newWindowPromise = TestUtils.topicObserved(
+    "browser-delayed-startup-finished"
+  );
 
+  info("Executing command in untracked browser window.");
+  BrowserWindowTracker.untrackForTestsOnly(window);
   let searchBookmarksMenuEntry = document.getElementById(
     "menu_searchBookmarks"
   );
-
-  info(`Executing command.`);
   searchBookmarksMenuEntry.doCommand();
+  BrowserWindowTracker.track(window);
 
-  is(
-    newWindow,
-    BrowserWindowTracker.getTopWindow(),
-    "New window is top window."
-  );
+  info("Waiting for new window to open.");
+  let [newWindow] = await newWindowPromise;
   await isUrlbarInBookmarksSearchMode(newWindow);
   await BrowserTestUtils.closeWindow(newWindow);
 });
 
 async function isUrlbarInBookmarksSearchMode(targetWin) {
+  is(
+    targetWin,
+    BrowserWindowTracker.getTopWindow(),
+    "Target window is top window."
+  );
   await new Promise(resolve => {
     targetWin.gURLBar.controller.addQueryListener({
       onViewOpen() {
@@ -42,7 +46,6 @@ async function isUrlbarInBookmarksSearchMode(targetWin) {
   });
 
   // Verify URLBar is in search mode with correct restriction
-  is(targetWin != null, true, "top window is not empty");
   is(
     targetWin.gURLBar.searchMode?.source,
     UrlbarUtils.RESULT_SOURCE.BOOKMARKS,
