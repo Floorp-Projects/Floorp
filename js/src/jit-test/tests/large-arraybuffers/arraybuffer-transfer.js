eea8ede8947d1df582e67be6319cd77f56280e5b
André Bargull <andre.bargull@gmail.com>
2023-07-07 13:07:56 +0000

    Bug 1841113 - Part 3: Add additional ArrayBuffer transfer proposal tests. r=spidermonkey-reviewers,jandem
    Add tests not covered by test262 and which are SpiderMonkey-specific, for
    example different ArrayBuffer storage types and cross-compartment wrappers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182481

---
+++ js/src/jit-test/tests/large-arraybuffers/arraybuffer-transfer.js
@@ -0,0 +1,66 @@
+// |jit-test| --enable-arraybuffer-transfer; skip-if: !ArrayBuffer.prototype.transfer
+
+const gb = 1 * 1024 * 1024 * 1024;
+
+const smallByteLength = 1024;
+const largeByteLength = 4 * gb;
+
+// Fast alternative to |assertEq| to avoid a slow VM-call.
+// Should only be used when looping over the TypedArray contents to avoid
+// unnecessary test slowdowns.
+function assertEqNum(e, a) {
+  if (e !== a) {
+    assertEq(e, a);
+  }
+}
+
+{
+  let smallBuffer = new ArrayBuffer(smallByteLength);
+
+  let i8 = new Uint8Array(smallBuffer);
+  for (let i = 0; i < smallByteLength; ++i) {
+    assertEqNum(i8[i], 0);
+    i8[i] = i;
+  }
+
+  assertEq(smallBuffer.byteLength, smallByteLength);
+  assertEq(smallBuffer.detached, false);
+
+  // Copy from a small into a large buffer.
+  let largeBuffer = smallBuffer.transfer(largeByteLength);
+
+  assertEq(smallBuffer.byteLength, 0);
+  assertEq(smallBuffer.detached, true);
+
+  assertEq(largeBuffer.byteLength, largeByteLength);
+  assertEq(largeBuffer.detached, false);
+
+  i8 = new Uint8Array(largeBuffer);
+  for (let i = 0; i < smallByteLength; ++i) {
+    assertEqNum(i8[i], i & 0xff);
+  }
+
+  // Test the first 100 new bytes.
+  for (let i = smallByteLength; i < smallByteLength + 100; ++i) {
+    assertEqNum(i8[i], 0);
+  }
+
+  // And the last 100 new bytes.
+  for (let i = largeByteLength - 100; i < largeByteLength; ++i) {
+    assertEqNum(i8[i], 0);
+  }
+
+  // Copy back from a large into a small buffer.
+  smallBuffer = largeBuffer.transfer(smallByteLength);
+
+  assertEq(largeBuffer.byteLength, 0);
+  assertEq(largeBuffer.detached, true);
+
+  assertEq(smallBuffer.byteLength, smallByteLength);
+  assertEq(smallBuffer.detached, false);
+
+  i8 = new Uint8Array(smallBuffer);
+  for (let i = 0; i < smallByteLength; ++i) {
+    assertEqNum(i8[i], i & 0xff);
+  }
+}
---
+++ js/src/jit-test/tests/typedarray/arraybuffer-transfer-coerce-large-bytelength.js
@@ -0,0 +1,15 @@
+// |jit-test| allow-oom; --enable-arraybuffer-transfer; skip-if: (!ArrayBuffer.prototype.transfer || getBuildConfiguration().tsan)
+
+let buffer = new ArrayBuffer(0);
+
+let result = null;
+try {
+  result = buffer.transfer(2 ** 32);
+} catch {
+  // Intentionally ignore allocation failure.
+}
+
+// If the allocation succeeded, |result| has the correct size.
+if (result) {
+  assertEq(result.byteLength, 2 ** 32);
+}
