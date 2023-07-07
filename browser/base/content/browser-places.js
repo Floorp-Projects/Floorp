adf1b7b09f70e508f3f66312fb05e44ec3aae1b1
Norisz Fay <nfay@mozilla.com>
2023-07-07 05:57:07 +0300

    Backed out changeset 909fc50c54a1 (bug 1838743) for causing failures on browser_947914_button_paste.js CLOSED TREE

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -812,8 +812,6 @@ var BookmarksEventHandler = {
       // Call onCommand in the cases where it's not called automatically:
       // Middle-clicks outside of menus.
       this.onCommand(aEvent);
-      aEvent.preventDefault();
-      aEvent.stopPropagation();
     }
   },
 
