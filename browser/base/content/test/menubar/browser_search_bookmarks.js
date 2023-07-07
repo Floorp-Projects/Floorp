a01907fb904b355a634989edadaa04ab79a79733
Marco Bonardo <mak77@bonardo.net>
2023-07-07 10:06:57 +0000

    Bug 1840880 - Workaround Intermittent leak in browser_search_bookmarks.js. r=mseibert, a=test-only
    This is not solving the underlying issue, but should allow this test to not leak
    intermittently by using the same helpers as other urlbar tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182739

--- browser/base/content/test/menubar/browser_search_bookmarks.js
+++ browser/base/content/test/menubar/browser_search_bookmarks.js
@@ -1,5 +1,18 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * Tests the Search Bookmarks option from the menubar starts Address Bar search
+ * mode for bookmarks.
+ */
+ChromeUtils.defineLazyGetter(this, "UrlbarTestUtils", () => {
+  const { UrlbarTestUtils: module } = ChromeUtils.importESModule(
+    "resource://testing-common/UrlbarTestUtils.sys.mjs"
+  );
+  module.init(this);
+  return module;
+});
+
 add_task(async function test_menu_search_bookmarks_with_window_open() {
   info("Opening bookmarks menu");
   let searchBookmarksMenuEntry = document.getElementById(
@@ -8,14 +21,16 @@ add_task(async function test_menu_search_bookmarks_with_window_open() {
 
   searchBookmarksMenuEntry.doCommand();
 
-  await isUrlbarInBookmarksSearchMode(window);
+  let searchMode = UrlbarUtils.searchModeForToken("*");
+  searchMode.entry = "bookmarkmenu";
+  await UrlbarTestUtils.assertSearchMode(window, searchMode);
 });
 
 add_task(async function test_menu_search_bookmarks_in_new_window() {
   info("Opening bookmarks menu");
   let newWindow = await BrowserUIUtils.openNewBrowserWindow();
 
-  let searchBookmarksMenuEntry = document.getElementById(
+  let searchBookmarksMenuEntry = newWindow.document.getElementById(
     "menu_searchBookmarks"
   );
 
@@ -27,25 +42,8 @@ add_task(async function test_menu_search_bookmarks_in_new_window() {
     BrowserWindowTracker.getTopWindow(),
     "New window is top window."
   );
-  await isUrlbarInBookmarksSearchMode(newWindow);
+  let searchMode = UrlbarUtils.searchModeForToken("*");
+  searchMode.entry = "bookmarkmenu";
+  await UrlbarTestUtils.assertSearchMode(newWindow, searchMode);
   await BrowserTestUtils.closeWindow(newWindow);
 });
-
-async function isUrlbarInBookmarksSearchMode(targetWin) {
-  await new Promise(resolve => {
-    targetWin.gURLBar.controller.addQueryListener({
-      onViewOpen() {
-        targetWin.gURLBar.controller.removeQueryListener(this);
-        resolve();
-      },
-    });
-  });
-
-  // Verify URLBar is in search mode with correct restriction
-  is(targetWin != null, true, "top window is not empty");
-  is(
-    targetWin.gURLBar.searchMode?.source,
-    UrlbarUtils.RESULT_SOURCE.BOOKMARKS,
-    "Addressbar in correct mode."
-  );
-}
