6f2ab58c3230617576f4dede020f389051dc8ba0
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2023-07-07 09:40:04 +0000

    Bug 1841425 - Hard code UA string's Windows version as 10.0. r=necko-reviewers,kershaw
    Firefox 115 is the last version to support Windows versions < 10.0 (bug
    1594270). Firefox already caps the UA string's Windows version at 10.0
    to avoid webcompat breakage from sites not expecting Windows versions >
    10.0 (bug 1693295).
    
    Since the UA string's Windows version will no longer be < 10.0 or >
    10.0, we can now hard code the UA string's Windows version as 10.0.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182631

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -813,7 +813,13 @@ void nsHttpHandler::BuildUserAgent() {
 }
 
 #ifdef XP_WIN
-#  define OSCPU_WINDOWS "Windows NT %ld.%ld"
+// Hardcode the reported Windows version to 10.0. This way, Microsoft doesn't
+// get to change Web compat-sensitive values without our veto. The compat-
+// sensitivity keeps going up as 10.0 stays as the current value for longer
+// and longer. If the system-reported version ever changes, we'll be able to
+// take our time to evaluate the Web compat impact instead of having to
+// scramble to react like happened with macOS changing from 10.x to 11.x.
+#  define OSCPU_WINDOWS "Windows NT 10.0"
 #  define OSCPU_WIN64 OSCPU_WINDOWS "; Win64; x64"
 #endif
 
@@ -884,39 +890,29 @@ void nsHttpHandler::InitUserAgentComponents() {
 
   // Gather OS/CPU.
 #if defined(XP_WIN)
-  OSVERSIONINFO info = {sizeof(OSVERSIONINFO)};
-  if (!GetVersionEx(&info) || info.dwMajorVersion >= 10) {
-    // Cap the reported Windows version to 10.0. This way, Microsoft doesn't
-    // get to change Web compat-sensitive values without our veto. The
-    // compat-sensitivity keeps going up as 10.0 stays as the current value
-    // for longer and longer. If the system-reported version ever changes,
-    // we'll be able to take our time to evaluate the Web compat impact
-    // instead of having to scramble to react like happened with macOS
-    // changing from 10.x to 11.x.
-    info.dwMajorVersion = 10;
-    info.dwMinorVersion = 0;
-  }
 
-  const char* format;
 #  if defined _M_X64 || defined _M_AMD64
-  format = OSCPU_WIN64;
+  mOscpu.AssignLiteral(OSCPU_WIN64);
 #  elif defined(_ARM64_)
   // Report ARM64 Windows 11+ as x86_64 and Windows 10 as x86. Windows 11+
   // supports x86_64 emulation, but Windows 10 only supports x86 emulation.
-  format = IsWin11OrLater() ? OSCPU_WIN64 : OSCPU_WINDOWS;
+  if (IsWin11OrLater()) {
+    mOscpu.AssignLiteral(OSCPU_WIN64);
+  } else {
+    mOscpu.AssignLiteral(OSCPU_WINDOWS);
+  }
 #  else
   BOOL isWow64 = FALSE;
   if (!IsWow64Process(GetCurrentProcess(), &isWow64)) {
     isWow64 = FALSE;
   }
-  format = isWow64 ? OSCPU_WIN64 : OSCPU_WINDOWS;
+  if (isWow64) {
+    mOscpu.AssignLiteral(OSCPU_WIN64);
+  } else {
+    mOscpu.AssignLiteral(OSCPU_WINDOWS);
+  }
 #  endif
 
-  SmprintfPointer buf =
-      mozilla::Smprintf(format, info.dwMajorVersion, info.dwMinorVersion);
-  if (buf) {
-    mOscpu = buf.get();
-  }
 #elif defined(XP_MACOSX)
   mOscpu.AssignLiteral("Intel Mac OS X 10.15");
 #elif defined(XP_UNIX)
