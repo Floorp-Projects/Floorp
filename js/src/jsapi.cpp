a311b897fcd669f60730a1f4fb205f6b803c2a28
Iulian Moraru <imoraru@mozilla.com>
2023-07-08 00:43:15 +0300

    Backed out changeset c989d589866f (bug 1840996) for causing wasm crashes. a=backout

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -2302,12 +2302,15 @@ void JS::TransitiveCompileOptions::copyPODTransitiveOptions(
   mutedErrors_ = rhs.mutedErrors_;
   forceStrictMode_ = rhs.forceStrictMode_;
   alwaysUseFdlibm_ = rhs.alwaysUseFdlibm_;
+  sourcePragmas_ = rhs.sourcePragmas_;
   skipFilenameValidation_ = rhs.skipFilenameValidation_;
   hideScriptFromDebugger_ = rhs.hideScriptFromDebugger_;
   deferDebugMetadata_ = rhs.deferDebugMetadata_;
   eagerDelazificationStrategy_ = rhs.eagerDelazificationStrategy_;
 
   selfHostingMode = rhs.selfHostingMode;
+  asmJSOption = rhs.asmJSOption;
+  throwOnAsmJSValidationFailureOption = rhs.throwOnAsmJSValidationFailureOption;
   forceAsync = rhs.forceAsync;
   discardSource = rhs.discardSource;
   sourceIsLazy = rhs.sourceIsLazy;
@@ -2315,13 +2318,12 @@ void JS::TransitiveCompileOptions::copyPODTransitiveOptions(
   nonSyntacticScope = rhs.nonSyntacticScope;
 
   topLevelAwait = rhs.topLevelAwait;
+  importAssertions = rhs.importAssertions;
 
   borrowBuffer = rhs.borrowBuffer;
   usePinnedBytecode = rhs.usePinnedBytecode;
   allocateInstantiationStorage = rhs.allocateInstantiationStorage;
   deoptimizeModuleGlobalVars = rhs.deoptimizeModuleGlobalVars;
-
-  prefableOptions_ = rhs.prefableOptions_;
 
   introductionType = rhs.introductionType;
   introductionLineno = rhs.introductionLineno;
@@ -2407,16 +2409,23 @@ bool JS::OwningCompileOptions::copy(JS::FrontendContext* fc,
 }
 
 JS::CompileOptions::CompileOptions(JSContext* cx) : ReadOnlyCompileOptions() {
-  prefableOptions_ = cx->options().compileOptions();
-
-  if (cx->options().asmJSOption() == AsmJSOption::Enabled) {
-    if (!js::IsAsmJSCompilationAvailable(cx)) {
-      prefableOptions_.setAsmJSOption(AsmJSOption::DisabledByNoWasmCompiler);
-    } else if (cx->realm() && (cx->realm()->debuggerObservesWasm() ||
-                               cx->realm()->debuggerObservesAsmJS())) {
-      prefableOptions_.setAsmJSOption(AsmJSOption::DisabledByDebugger);
-    }
+  if (!js::IsAsmJSCompilationAvailable(cx)) {
+    // Distinguishing the cases is just for error reporting.
+    asmJSOption = !cx->options().asmJS()
+                      ? AsmJSOption::DisabledByAsmJSPref
+                      : AsmJSOption::DisabledByNoWasmCompiler;
+  } else if (cx->realm() && (cx->realm()->debuggerObservesWasm() ||
+                             cx->realm()->debuggerObservesAsmJS())) {
+    asmJSOption = AsmJSOption::DisabledByDebugger;
+  } else {
+    asmJSOption = AsmJSOption::Enabled;
   }
+  throwOnAsmJSValidationFailureOption =
+      cx->options().throwOnAsmJSValidationFailure();
+
+  importAssertions = cx->options().importAssertions();
+
+  sourcePragmas_ = cx->options().sourcePragmas();
 
   // Certain modes of operation force strict-mode in general.
   forceStrictMode_ = cx->options().strictMode();
