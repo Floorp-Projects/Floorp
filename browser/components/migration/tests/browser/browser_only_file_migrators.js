55bee439b7801aec6d37a204c700fe4470f91849
Mike Conley <mconley@mozilla.com>
2023-07-10 13:34:04 +0000

    Bug 1823489 - Have the migration-wizard element create the panel-list in its shadow root. r=kpatenio,negin,desktop-theme-reviewers,dao
    This involves opening the shadow root of the migration wizard, as the panel-list
    really isn't designed to handle being embedded within a closed shadow root.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182484

--- browser/components/migration/tests/browser/browser_only_file_migrators.js
+++ browser/components/migration/tests/browser/browser_only_file_migrators.js
@@ -56,13 +56,13 @@ add_task(async function test_only_file_migrators() {
     await changedToSelectionPage;
 
     // No browser migrators should be listed.
-    let browserMigratorItems = wizard.querySelectorAll(
+    let browserMigratorItems = shadow.querySelectorAll(
       `panel-item[type="${MigrationWizardConstants.MIGRATOR_TYPES.BROWSER}"]`
     );
     Assert.ok(!browserMigratorItems.length, "No browser migrators listed.");
 
     // Check to make sure there's at least one file migrator listed.
-    let fileMigratorItems = wizard.querySelectorAll(
+    let fileMigratorItems = shadow.querySelectorAll(
       `panel-item[type="${MigrationWizardConstants.MIGRATOR_TYPES.FILE}"]`
     );
 
--- browser/components/newtab/test/browser/browser_aboutwelcome_multistage_mr.js
+++ browser/components/newtab/test/browser/browser_aboutwelcome_multistage_mr.js
@@ -516,7 +516,7 @@ add_task(async function test_aboutwelcome_embedded_migration() {
       Assert.ok(true, "Selection page is being shown in the migration wizard.");
 
       // Now let's make sure that the <panel-list> can appear.
-      let panelList = wizard.querySelector("panel-list");
+      let panelList = shadow.querySelector("panel-list");
       Assert.ok(panelList, "Found the <panel-list>.");
 
       // The "shown" event from the panel-list is coming from a lower level
@@ -569,7 +569,7 @@ add_task(async function test_aboutwelcome_embedded_migration() {
         "Panel should be tightly anchored to the bottom of the button shadow node."
       );
 
-      let panelItem = wizard.querySelector(menuitemSelector);
+      let panelItem = shadow.querySelector(menuitemSelector);
       panelItem.click();
 
       let importButton = shadow.querySelector("#import");
