ae02e7369e2bad852083c990e8ac09b67c09805a
Tooru Fujisawa <arai_a@mac.com>
2023-07-11 10:41:25 +0000

    Bug 1842441 - Part 4: Add the documentation and testcase for conflicting names in bindings object and variables. r=jandem
    Also fixed the wrong order of environment chain in E.1. direct eval case.
    
    Depends on D183133
    
    Differential Revision: https://phabricator.services.mozilla.com/D183134

--- js/src/jit-test/tests/debug/envChain_frame-evalWithBindings-with-eval.js
+++ js/src/jit-test/tests/debug/envChain_frame-evalWithBindings-with-eval.js
@@ -12,15 +12,29 @@ dbg.onEnterFrame = frame => {
 
   const bindings = {
     bindings_prop: 50,
+
+    bindings_prop_var: 61,
+    bindings_prop_lexical: 71,
+    bindings_prop_unqualified: 81,
   };
 
-  const envs = JSON.parse(frame.evalWithBindings(`
+  const {envs, vars} = JSON.parse(frame.evalWithBindings(`
 // Put direct eval to de-optimize lexical.
 eval("");
 var qualified = 10;
 unqualified = 20;
 let lexical = 30;
 this.prop = 40;
+
+var bindings_prop_var = 60;
+let bindings_prop_lexical = 70;
+bindings_prop_unqualified = 80;
+
+const vars = {
+  bindings_prop_var,
+  bindings_prop_lexical,
+  bindings_prop_unqualified,
+};
 
 const envs = [];
 let env = getInnerMostEnvironmentObject();
@@ -32,12 +46,33 @@ while (env) {
     lexical: !!Object.getOwnPropertyDescriptor(env, "lexical"),
     prop: !!Object.getOwnPropertyDescriptor(env, "prop"),
     bindings_prop: !!Object.getOwnPropertyDescriptor(env, "bindings_prop"),
+
+    bindings_prop_var: !!Object.getOwnPropertyDescriptor(env, "bindings_prop_var"),
+    bindings_prop_var_value: Object.getOwnPropertyDescriptor(env, "bindings_prop_var")?.value,
+    bindings_prop_lexical: !!Object.getOwnPropertyDescriptor(env, "bindings_prop_lexical"),
+    bindings_prop_lexical_value: Object.getOwnPropertyDescriptor(env, "bindings_prop_lexical")?.value,
+    bindings_prop_unqualified: !!Object.getOwnPropertyDescriptor(env, "bindings_prop_unqualified"),
+    bindings_prop_unqualified_value: Object.getOwnPropertyDescriptor(env, "bindings_prop_unqualified")?.value,
   });
 
   env = getEnclosingEnvironmentObject(env);
 }
-JSON.stringify(envs);
+JSON.stringify({envs, vars});
 `, bindings).return);
+
+  assertEq(vars.bindings_prop_var, 60,
+           "qualified var should read the value set by the declaration");
+  assertEq(vars.bindings_prop_lexical, 70,
+           "lexical should read the value set by the declaration");
+  assertEq(vars.bindings_prop_unqualified, 80,
+           "unqualified name should read the value set by the assignment");
+
+  assertEq(bindings.bindings_prop_var, 61,
+          "the original bindings property must not be overwritten for var");
+  assertEq(bindings.bindings_prop_lexical, 71,
+          "the original bindings property must not be overwritten for lexical");
+  assertEq(bindings.bindings_prop_unqualified, 81,
+          "the original bindings property must not be overwritten for unqualified");
 
   assertEq(envs.length, 5);
 
@@ -50,6 +85,12 @@ JSON.stringify(envs);
   assertEq(env.lexical, true, "lexical must live in the BlockLexicalEnvironmentObject");
   assertEq(env.prop, false);
 
+  assertEq(env.bindings_prop_var, false);
+  assertEq(env.bindings_prop_lexical, true,
+           "lexical must live in the BlockLexicalEnvironmentObject even if it conflicts with the bindings object property");
+  assertEq(env.bindings_prop_lexical_value, 70);
+  assertEq(env.bindings_prop_unqualified, false);
+
   env = envs[i]; i++;
   assertEq(env.type, "WithEnvironmentObject");
   assertEq(env.qualified, false);
@@ -57,6 +98,19 @@ JSON.stringify(envs);
   assertEq(env.lexical, false);
   assertEq(env.prop, false);
   assertEq(env.bindings_prop, true, "bindings property must live in the with env for bindings");
+
+  assertEq(env.bindings_prop_var, true,
+           "bindings property must live in the with env for bindings");
+  assertEq(env.bindings_prop_var_value, 60,
+           "bindings property must be overwritten for var");
+  assertEq(env.bindings_prop_lexical, true,
+           "bindings property must live in the with env for bindings");
+  assertEq(env.bindings_prop_lexical_value, 71,
+           "bindings property must not be overwritten for lexical");
+  assertEq(env.bindings_prop_unqualified, true,
+           "bindings property must live in the with env for bindings");
+  assertEq(env.bindings_prop_unqualified_value, 80,
+           "bindings property must not be overwritten for unqualified");
 
   env = envs[i]; i++;
   assertEq(env.type, "[DebugProxy] CallObject");
@@ -66,6 +120,13 @@ JSON.stringify(envs);
   assertEq(env.prop, false);
   assertEq(env.bindings_prop, false);
 
+  assertEq(env.bindings_prop_var, true,
+           "qualified var binding must be created in the CallObject even if it conflicts with the bindings object property");
+  assertEq(env.bindings_prop_var_value, undefined,
+           "qualified var value must not be set if it conflicts with the bindings object property");
+  assertEq(env.bindings_prop_lexical, false);
+  assertEq(env.bindings_prop_unqualified, false);
+
   env = envs[i]; i++;
   assertEq(env.type, "[DebugProxy] GlobalLexicalEnvironmentObject");
   assertEq(env.qualified, false);
@@ -74,6 +135,10 @@ JSON.stringify(envs);
   assertEq(env.prop, false);
   assertEq(env.bindings_prop, false);
 
+  assertEq(env.bindings_prop_var, false);
+  assertEq(env.bindings_prop_lexical, false);
+  assertEq(env.bindings_prop_unqualified, false);
+
   env = envs[i]; i++;
   assertEq(env.type, "*global*");
   assertEq(env.qualified, false);
@@ -81,6 +146,10 @@ JSON.stringify(envs);
   assertEq(env.lexical, false);
   assertEq(env.prop, true, "this property must live in the global");
   assertEq(env.bindings_prop, false);
+
+  assertEq(env.bindings_prop_var, false);
+  assertEq(env.bindings_prop_lexical, false);
+  assertEq(env.bindings_prop_unqualified, false);
 };
 
 g.eval(`
--- js/src/jit-test/tests/debug/envChain_frame-evalWithBindings.js
+++ js/src/jit-test/tests/debug/envChain_frame-evalWithBindings.js
@@ -12,13 +12,27 @@ dbg.onEnterFrame = frame => {
 
   const bindings = {
     bindings_prop: 50,
+
+    bindings_prop_var: 61,
+    bindings_prop_lexical: 71,
+    bindings_prop_unqualified: 81,
   };
 
-  const envs = JSON.parse(frame.evalWithBindings(`
+  const {envs, vars} = JSON.parse(frame.evalWithBindings(`
 var qualified = 10;
 unqualified = 20;
 let lexical = 30;
 this.prop = 40;
+
+var bindings_prop_var = 60;
+let bindings_prop_lexical = 70;
+bindings_prop_unqualified = 80;
+
+const vars = {
+  bindings_prop_var,
+  bindings_prop_lexical,
+  bindings_prop_unqualified,
+};
 
 const envs = [];
 let env = getInnerMostEnvironmentObject();
@@ -30,18 +44,40 @@ while (env) {
     lexical: !!Object.getOwnPropertyDescriptor(env, "lexical"),
     prop: !!Object.getOwnPropertyDescriptor(env, "prop"),
     bindings_prop: !!Object.getOwnPropertyDescriptor(env, "bindings_prop"),
+
+    bindings_prop_var: !!Object.getOwnPropertyDescriptor(env, "bindings_prop_var"),
+    bindings_prop_var_value: Object.getOwnPropertyDescriptor(env, "bindings_prop_var")?.value,
+    bindings_prop_lexical: !!Object.getOwnPropertyDescriptor(env, "bindings_prop_lexical"),
+    bindings_prop_lexical_value: Object.getOwnPropertyDescriptor(env, "bindings_prop_lexical")?.value,
+    bindings_prop_unqualified: !!Object.getOwnPropertyDescriptor(env, "bindings_prop_unqualified"),
+    bindings_prop_unqualified_value: Object.getOwnPropertyDescriptor(env, "bindings_prop_unqualified")?.value,
   });
 
   env = getEnclosingEnvironmentObject(env);
 }
-JSON.stringify(envs);
+JSON.stringify({envs, vars});
 `, bindings).return);
+
+  assertEq(vars.bindings_prop_var, 60,
+           "qualified var should read the value set by the declaration");
+  assertEq(vars.bindings_prop_lexical, 70,
+           "lexical should read the value set by the declaration");
+  assertEq(vars.bindings_prop_unqualified, 80,
+           "unqualified name should read the value set by the assignment");
+
+  assertEq(bindings.bindings_prop_var, 61,
+          "the original bindings property must not be overwritten for var");
+  assertEq(bindings.bindings_prop_lexical, 71,
+          "the original bindings property must not be overwritten for lexical");
+  assertEq(bindings.bindings_prop_unqualified, 81,
+          "the original bindings property must not be overwritten for unqualified");
 
   assertEq(envs.length, 4);
 
   let i = 0, env;
 
-  // NOTE: lexical is optimized and uses frame slot.
+  // NOTE: lexical is optimized and uses frame slot, even if it conflicts with
+  //       bindings object properties.
 
   env = envs[i]; i++;
   assertEq(env.type, "WithEnvironmentObject");
@@ -51,6 +87,19 @@ JSON.stringify(envs);
   assertEq(env.prop, false);
   assertEq(env.bindings_prop, true, "bindings property must live in the with env for bindings");
 
+  assertEq(env.bindings_prop_var, true,
+           "bindings property must live in the with env for bindings");
+  assertEq(env.bindings_prop_var_value, 60,
+           "bindings property must be overwritten for var");
+  assertEq(env.bindings_prop_lexical, true,
+           "bindings property must live in the with env for bindings");
+  assertEq(env.bindings_prop_lexical_value, 71,
+           "bindings property must not be overwritten for lexical");
+  assertEq(env.bindings_prop_unqualified, true,
+           "bindings property must live in the with env for bindings");
+  assertEq(env.bindings_prop_unqualified_value, 80,
+           "bindings property must be overwritten for unqualified");
+
   env = envs[i]; i++;
   assertEq(env.type, "[DebugProxy] CallObject");
   assertEq(env.qualified, true, "qualified var must live in the CallObject");
@@ -58,6 +107,13 @@ JSON.stringify(envs);
   assertEq(env.lexical, false);
   assertEq(env.prop, false);
   assertEq(env.bindings_prop, false);
+
+  assertEq(env.bindings_prop_var, true,
+           "qualified var binding must be created in the CallObject even if it conflicts with the bindings object property");
+  assertEq(env.bindings_prop_var_value, undefined,
+           "qualified var value must not be set if it conflicts with the bindings object property");
+  assertEq(env.bindings_prop_lexical, false);
+  assertEq(env.bindings_prop_unqualified, false);
 
   env = envs[i]; i++;
   assertEq(env.type, "[DebugProxy] GlobalLexicalEnvironmentObject");
@@ -67,6 +123,10 @@ JSON.stringify(envs);
   assertEq(env.prop, false);
   assertEq(env.bindings_prop, false);
 
+  assertEq(env.bindings_prop_var, false);
+  assertEq(env.bindings_prop_lexical, false);
+  assertEq(env.bindings_prop_unqualified, false);
+
   env = envs[i]; i++;
   assertEq(env.type, "*global*");
   assertEq(env.qualified, false);
@@ -74,6 +134,10 @@ JSON.stringify(envs);
   assertEq(env.lexical, false);
   assertEq(env.prop, true, "this property must live in the global");
   assertEq(env.bindings_prop, false);
+
+  assertEq(env.bindings_prop_var, false);
+  assertEq(env.bindings_prop_lexical, false);
+  assertEq(env.bindings_prop_unqualified, false);
 };
 
 g.eval(`
