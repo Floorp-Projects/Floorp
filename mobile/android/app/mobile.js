b2089dd2fe168a6d3f212bc397f05491f3eafb45
Kershaw Chang <kershaw@mozilla.com>
2023-07-11 08:54:41 +0000

    Bug 1839054 - Enable QPACK on Android, r=necko-reviewers,geckoview-reviewers,jesup,amejiamarmol
    Differential Revision: https://phabricator.services.mozilla.com/D182707

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -38,9 +38,6 @@ pref("network.http.max-persistent-connections-per-proxy", 20);
 pref("network.http.http2.push-allowance", 32768);
 pref("network.http.http2.default-hpack-buffer", 4096); // 4k
 
-// http3
-pref("network.http.http3.default-qpack-table-size", 0);
-
 // See bug 545869 for details on why these are set the way they are
 pref("network.buffer.cache.size",  16384);
 
