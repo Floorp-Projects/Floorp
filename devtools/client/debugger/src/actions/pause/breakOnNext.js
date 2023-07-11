f736b127610f675545772ca11c0871f4a419d7b1
Alexandre Poirot <poirot.alex@gmail.com>
2023-07-11 14:36:26 +0000

    Bug 1841967 - [devtools] Align breakOnNext with step and resume actions. r=devtools-reviewers,bomsy
    Step and resume actions (command action) are acting on the currently selected thread,
    so that there is no need to pass a context. This action could also query the selectors
    to guess what is the currently selected thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182874

--- devtools/client/debugger/src/actions/pause/breakOnNext.js
+++ devtools/client/debugger/src/actions/pause/breakOnNext.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
+import { getCurrentThread } from "../../selectors";
 /**
  * Debugger breakOnNext command.
  * It's different from the comand action because we also want to
@@ -10,9 +11,10 @@
  * @memberof actions/pause
  * @static
  */
-export function breakOnNext(cx) {
+export function breakOnNext() {
   return async ({ dispatch, getState, client }) => {
-    await client.breakOnNext(cx.thread);
-    return dispatch({ type: "BREAK_ON_NEXT", thread: cx.thread });
+    const thread = getCurrentThread(getState());
+    await client.breakOnNext(thread);
+    return dispatch({ type: "BREAK_ON_NEXT", thread });
   };
 }
