9050374690cc3622482c97429e75ea9bf207fb9f
Tooru Fujisawa <arai_a@mac.com>
2023-07-12 21:12:37 +0000

    Bug 1840839 - Add xpc::SetPrefableCompileOptions and move related prefs to StaticPrefs. r=mccr8
    Moved the following prefs to StaticPrefs with "mirror: always":
      * javascript.options.source_pragmas
      * javascript.options.asmjs
      * javascript.options.throw_on_asmjs_validation_failure
    
    javascript.options.experimental.import_assertions is already defined in
    StaticPrefs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182459

--- js/xpconnect/src/XPCJSContext.cpp
+++ js/xpconnect/src/XPCJSContext.cpp
@@ -824,9 +824,20 @@ void xpc::SetPrefableRealmOptions(JS::RealmOptions& options) {
       ;
 }
 
+void xpc::SetPrefableCompileOptions(JS::PrefableCompileOptions& options) {
+  options
+      .setSourcePragmas(StaticPrefs::javascript_options_source_pragmas())
+#ifdef NIGHTLY_BUILD
+      .setImportAssertions(
+          StaticPrefs::javascript_options_experimental_import_assertions())
+#endif
+      .setAsmJS(StaticPrefs::javascript_options_asmjs())
+      .setThrowOnAsmJSValidationFailure(
+          StaticPrefs::javascript_options_throw_on_asmjs_validation_failure());
+}
+
 void xpc::SetPrefableContextOptions(JS::ContextOptions& options) {
   options
-      .setAsmJS(Preferences::GetBool(JS_OPTIONS_DOT_STR "asmjs"))
 #ifdef FUZZING
       .setFuzzing(Preferences::GetBool(JS_OPTIONS_DOT_STR "fuzzing.enabled"))
 #endif
@@ -842,18 +853,11 @@ void xpc::SetPrefableContextOptions(JS::ContextOptions& options) {
           JS_FOR_WASM_FEATURES(WASM_FEATURE)
 #undef WASM_FEATURE
       .setWasmVerbose(Preferences::GetBool(JS_OPTIONS_DOT_STR "wasm_verbose"))
-      .setThrowOnAsmJSValidationFailure(Preferences::GetBool(
-          JS_OPTIONS_DOT_STR "throw_on_asmjs_validation_failure"))
-      .setSourcePragmas(
-          Preferences::GetBool(JS_OPTIONS_DOT_STR "source_pragmas"))
       .setAsyncStack(Preferences::GetBool(JS_OPTIONS_DOT_STR "asyncstack"))
       .setAsyncStackCaptureDebuggeeOnly(Preferences::GetBool(
-          JS_OPTIONS_DOT_STR "asyncstack_capture_debuggee_only"))
-#ifdef NIGHTLY_BUILD
-      .setImportAssertions(Preferences::GetBool(
-          JS_OPTIONS_DOT_STR "experimental.import_assertions"))
-#endif
-      ;
+          JS_OPTIONS_DOT_STR "asyncstack_capture_debuggee_only"));
+
+  SetPrefableCompileOptions(options.compileOptions());
 }
 
 // Mirrored value of javascript.options.self_hosted.use_shared_memory.
