2fdcd39b8ef0d278dbc017a7118a658907ad7119
Alexandre Poirot <poirot.alex@gmail.com>
2023-07-12 16:14:21 +0000

    Bug 1822310 - [devtools] Use the context middleware to cancel outdated fetch and map scope actions. r=devtools-reviewers,bomsy
    The current "thread context" is too loose. These actions are specific to the currently selected frame.
    If we step to another frame, or select another frame in the call stack, or select another thread, we can cancel these async actions.
    
    But instead of using the "context" pattern, only identify the "selectedFrame" attribute passed
    and ensure it is still relevant.
    
    Also correctly await for async actions from selectFrame.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182550

--- devtools/client/debugger/src/actions/pause/fetchScopes.js
+++ devtools/client/debugger/src/actions/pause/fetchScopes.js
@@ -2,45 +2,33 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-import {
-  getSelectedFrame,
-  getGeneratedFrameScope,
-  getOriginalFrameScope,
-} from "../../selectors";
+import { getGeneratedFrameScope, getOriginalFrameScope } from "../../selectors";
 import { mapScopes } from "./mapScopes";
 import { generateInlinePreview } from "./inlinePreview";
 import { PROMISE } from "../utils/middleware/promise";
 
-export function fetchScopes(cx) {
+export function fetchScopes(selectedFrame) {
   return async function ({ dispatch, getState, client }) {
-    const frame = getSelectedFrame(getState(), cx.thread);
-    // Ignore the request if there is no selected frame
-    // as scopes are related to a particular frame.
-    if (!frame) {
-      return;
-    }
     // See if we already fetched the scopes.
     // We may have pause on multiple thread and re-select a paused thread
     // for which we already fetched the scopes.
     // Ignore pending scopes as the previous action may have been cancelled
     // by context assertions.
-    let scopes = getGeneratedFrameScope(getState(), frame);
+    let scopes = getGeneratedFrameScope(getState(), selectedFrame);
     if (!scopes?.scope) {
       scopes = dispatch({
         type: "ADD_SCOPES",
-        cx,
-        thread: cx.thread,
-        frame,
-        [PROMISE]: client.getFrameScopes(frame),
+        selectedFrame,
+        [PROMISE]: client.getFrameScopes(selectedFrame),
       });
 
       scopes.then(() => {
-        dispatch(generateInlinePreview(cx, frame));
+        dispatch(generateInlinePreview(selectedFrame));
       });
     }
 
-    if (!getOriginalFrameScope(getState(), frame)) {
-      await dispatch(mapScopes(cx, scopes, frame));
+    if (!getOriginalFrameScope(getState(), selectedFrame)) {
+      await dispatch(mapScopes(selectedFrame, scopes));
     }
   };
 }
