40b8a24c03df4fb85c232c6aa6cf8c222dde6c27
alwu <alwu@mozilla.com>
2023-07-12 23:05:02 +0000

    Bug 1843189 - add wmfme in chunking.py. r=taskgraph-reviewers,jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D183436

--- taskcluster/gecko_taskgraph/util/chunking.py
+++ taskcluster/gecko_taskgraph/util/chunking.py
@@ -59,6 +59,7 @@ def guess_mozinfo_from_task(task, repo=""):
         in setting["runtime"].keys(),
         "nightly_build": repo in ["mozilla-central", "autoland", "try", ""],  # trunk
         "http3": "http3" in setting["runtime"].keys(),
+        "wmfme": "wmf-media-engine" in setting["runtime"].keys(),
     }
     for platform in ("android", "linux", "mac", "win"):
         if p_os["name"].startswith(platform):
