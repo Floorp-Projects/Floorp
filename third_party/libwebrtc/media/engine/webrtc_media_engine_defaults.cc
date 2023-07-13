10c54732c92ba19a421ac10c3fd497d8d77a3fcb
Michael Froman <mfroman@mozilla.com>
2023-07-13 17:20:16 -0500

    Bug 1843113 - Vendor libwebrtc from 3beacb7a87
    Upstream commit: https://webrtc.googlesource.com/src/+/3beacb7a871db95671f10c5160e8ded45d722f68
        Create default video factories directly instead of through legacy public helpers
    
        Bug: webrtc:13573
        Change-Id: If8ab26dc45cce2dac17572772bb21806a54ed3e3
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/299660
        Reviewed-by: Philip Eliasson <philipel@webrtc.org>
        Auto-Submit: Danil Chapovalov <danilchap@webrtc.org>
        Reviewed-by: Per Kjellander <perkj@webrtc.org>
        Commit-Queue: Per Kjellander <perkj@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39729}

--- third_party/libwebrtc/media/engine/webrtc_media_engine_defaults.cc
+++ third_party/libwebrtc/media/engine/webrtc_media_engine_defaults.cc
@@ -9,12 +9,14 @@
  */
 #include "media/engine/webrtc_media_engine_defaults.h"
 
+#include <memory>
+
 #include "api/audio_codecs/builtin_audio_decoder_factory.h"
 #include "api/audio_codecs/builtin_audio_encoder_factory.h"
 #include "api/task_queue/default_task_queue_factory.h"
 #include "api/video/builtin_video_bitrate_allocator_factory.h"
-#include "api/video_codecs/builtin_video_decoder_factory.h"
-#include "api/video_codecs/builtin_video_encoder_factory.h"
+#include "media/engine/internal_decoder_factory.h"
+#include "media/engine/internal_encoder_factory.h"
 #include "modules/audio_processing/include/audio_processing.h"
 #include "rtc_base/checks.h"
 
@@ -35,9 +37,9 @@ void SetMediaEngineDefaults(cricket::MediaEngineDependencies* deps) {
     deps->audio_processing = AudioProcessingBuilder().Create();
 
   if (deps->video_encoder_factory == nullptr)
-    deps->video_encoder_factory = CreateBuiltinVideoEncoderFactory();
+    deps->video_encoder_factory = std::make_unique<InternalEncoderFactory>();
   if (deps->video_decoder_factory == nullptr)
-    deps->video_decoder_factory = CreateBuiltinVideoDecoderFactory();
+    deps->video_decoder_factory = std::make_unique<InternalDecoderFactory>();
 }
 
 }  // namespace webrtc
