77e1d6a49998c43ba36871c6c2de00b9fe9602bd
Michael Froman <mfroman@mozilla.com>
2023-07-13 17:59:56 -0500

    Bug 1843113 - Vendor libwebrtc from 755ffa7e8a
    Upstream commit: https://webrtc.googlesource.com/src/+/755ffa7e8a26896a29851bba3a09c25152fd68a3
        Remove unused field trial parameters from AimdRateControl.
    
        ratio and ignore_acked where added here https://webrtc-review.googlesource.com/c/src/+/252442
        ignore_decr  https://webrtc-review.googlesource.com/c/src/+/253901
    
        Bug: none
        Change-Id: I85008dfa70bf57bfefb453f9f1d1929510c7b825
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/298045
        Reviewed-by: Diep Bui <diepbp@webrtc.org>
        Commit-Queue: Per Kjellander <perkj@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39751}

--- third_party/libwebrtc/modules/remote_bitrate_estimator/aimd_rate_control.h
+++ third_party/libwebrtc/modules/remote_bitrate_estimator/aimd_rate_control.h
@@ -103,16 +103,8 @@ class AimdRateControl {
   // Allow the delay based estimate to only increase as long as application
   // limited region (alr) is not detected.
   const bool no_bitrate_increase_in_alr_;
-  // If false, uses estimated link capacity upper bound *
-  // `estimate_bounded_increase_ratio_` as upper limit for the estimate.
+  // If "Disabled",  estimated link capacity is not used as upper bound.
   FieldTrialFlag disable_estimate_bounded_increase_{"Disabled"};
-  FieldTrialParameter<double> estimate_bounded_increase_ratio_{"ratio", 1.0};
-  FieldTrialParameter<bool> ignore_throughput_limit_if_network_estimate_{
-      "ignore_acked", false};
-  FieldTrialParameter<bool> increase_to_network_estimate_{"immediate_incr",
-                                                          false};
-  FieldTrialParameter<bool> ignore_network_estimate_decrease_{"ignore_decr",
-                                                              false};
   absl::optional<DataRate> last_decrease_;
   FieldTrialOptional<TimeDelta> initial_backoff_interval_;
   FieldTrialFlag link_capacity_fix_;
