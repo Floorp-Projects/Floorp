482752362d3dded4326b0d15b3c1a516abd22ecc
Kagami Sascha Rosylight <krosylight@mozilla.com>
2023-07-13 12:32:19 +0000

    Bug 1842289 - Part 1: Register listeners inside the factory method instead of the constructor r=media-playback-reviewers,karlt
    Differential Revision: https://phabricator.services.mozilla.com/D183047

--- dom/media/encoder/MediaEncoder.cpp
+++ dom/media/encoder/MediaEncoder.cpp
@@ -438,37 +438,38 @@ MediaEncoder::MediaEncoder(
       mStarted(false),
       mCompleted(false),
       mError(false) {
+  if (!mAudioEncoder) {
+    mMuxedAudioEndTime = TimeUnit::FromInfinity();
+    mEncodedAudioQueue->Finish();
+  }
+  if (!mVideoEncoder) {
+    mMuxedVideoEndTime = TimeUnit::FromInfinity();
+    mEncodedVideoQueue->Finish();
+  }
+}
+
+void MediaEncoder::RegisterListeners() {
   if (mAudioEncoder) {
     mAudioPushListener = mEncodedAudioQueue->PushEvent().Connect(
         mEncoderThread, this, &MediaEncoder::OnEncodedAudioPushed);
     mAudioFinishListener = mEncodedAudioQueue->FinishEvent().Connect(
         mEncoderThread, this, &MediaEncoder::MaybeShutdown);
-    nsresult rv = mEncoderThread->Dispatch(NS_NewRunnableFunction(
+    MOZ_ALWAYS_SUCCEEDS(mEncoderThread->Dispatch(NS_NewRunnableFunction(
         "mozilla::AudioTrackEncoder::RegisterListener",
         [self = RefPtr<MediaEncoder>(this), this] {
           mAudioEncoder->RegisterListener(mEncoderListener);
-        }));
-    MOZ_DIAGNOSTIC_ASSERT(NS_SUCCEEDED(rv));
-    Unused << rv;
-  } else {
-    mMuxedAudioEndTime = TimeUnit::FromInfinity();
-    mEncodedAudioQueue->Finish();
+        })));
   }
   if (mVideoEncoder) {
     mVideoPushListener = mEncodedVideoQueue->PushEvent().Connect(
         mEncoderThread, this, &MediaEncoder::OnEncodedVideoPushed);
     mVideoFinishListener = mEncodedVideoQueue->FinishEvent().Connect(
         mEncoderThread, this, &MediaEncoder::MaybeShutdown);
-    nsresult rv = mEncoderThread->Dispatch(NS_NewRunnableFunction(
+    MOZ_ALWAYS_SUCCEEDS(mEncoderThread->Dispatch(NS_NewRunnableFunction(
         "mozilla::VideoTrackEncoder::RegisterListener",
         [self = RefPtr<MediaEncoder>(this), this] {
           mVideoEncoder->RegisterListener(mEncoderListener);
-        }));
-    MOZ_DIAGNOSTIC_ASSERT(NS_SUCCEEDED(rv));
-    Unused << rv;
-  } else {
-    mMuxedVideoEndTime = TimeUnit::FromInfinity();
-    mEncodedVideoQueue->Finish();
+        })));
   }
 }
 
@@ -727,11 +728,15 @@ already_AddRefed<MediaEncoder> MediaEncoder::CreateEncoder(
       videoEncoder->SetBitrate(aVideoBitrate);
     }
   }
-  return MakeAndAddRef<MediaEncoder>(
+  RefPtr<MediaEncoder> encoder = new MediaEncoder(
       std::move(aEncoderThread), std::move(driftCompensator), std::move(writer),
       std::move(audioEncoder), std::move(videoEncoder),
       std::move(encodedAudioQueue), std::move(encodedVideoQueue), aTrackRate,
       aMimeType, aMaxMemory, aTimeslice);
+
+  encoder->RegisterListeners();
+
+  return encoder.forget();
 }
 
 nsresult MediaEncoder::GetEncodedData(
