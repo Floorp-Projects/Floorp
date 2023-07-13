9c6d54355db89f43f9b0469737ea5391cd4b2207
Michael Froman <mfroman@mozilla.com>
2023-07-13 18:08:16 -0500

    Bug 1843113 - Vendor libwebrtc from 8481f6358e
    Upstream commit: https://webrtc.googlesource.com/src/+/8481f6358e46f05c9b85874694c692c9f8555dd2
        Remove IsSinglecastOrAllNonFirstLayersInactive() helper.
    
        As of recent changes, we can simply look at numberOfSimulcastStreams
        because in the {active,inactive,inactive} case we get a single
        webrtc::VideoStream here[1] which results in numberOfSimulcastStreams
        being 1 here[2].
    
        Looking at numberOfSimulcastStreams instead of using a helper is
        preferred because it is more descriptive and in the future, when
        {inactive,active,inactive} or {inactive,inactive,active} cases of VP9
        simulcast is also supported (webrtc:15046) then this gating will work
        even when the first layer is not the active one.
    
        [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/video/config/encoder_stream_factory.cc;l=146;drc=c99753ac8f051e379ae68e281aaef04b0a5ca8f2
    
        [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/modules/video_coding/video_codec_initializer.cc;l=77;drc=4baea5b07f2fd309892845cf2d1c0f4ca77862d3
    
        # No need to wait for win chrome bot, everything else green
        NOTRY=True
    
        Bug: webrtc:15046
        Change-Id: I8aaea2e8cc350bd01fb00cc7fd85032b7fdfe24d
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/299942
        Reviewed-by: Philip Eliasson <philipel@webrtc.org>
        Commit-Queue: Henrik Boström <hbos@webrtc.org>
        Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39759}

--- third_party/libwebrtc/api/video_codecs/video_codec.h
+++ third_party/libwebrtc/api/video_codecs/video_codec.h
@@ -130,8 +130,6 @@ class RTC_EXPORT VideoCodec {
   bool GetFrameDropEnabled() const;
   void SetFrameDropEnabled(bool enabled);
 
-  bool IsSinglecastOrAllNonFirstLayersInactive() const;
-
   // Public variables. TODO(hta): Make them private with accessors.
   VideoCodecType codecType;
 
