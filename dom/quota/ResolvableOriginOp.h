63acdb3db95d63d80b4acbd48baff8060c7d7df6
Jan Varga <jvarga@mozilla.com>
2023-07-13 09:57:16 +0000

    Bug 1840545 - Create a base class for resolvable normal origin operations; r=dom-storage-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D182183

---
+++ dom/quota/ResolvableOriginOp.h
@@ -0,0 +1,59 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef DOM_QUOTA_RESOLVABLEORIGINOP_H_
+#define DOM_QUOTA_RESOLVABLEORIGINOP_H_
+
+#include "NormalOriginOperationBase.h"
+
+#include "mozilla/MozPromise.h"
+
+namespace mozilla::dom::quota {
+
+template <typename T>
+class ResolvableOriginOp : public NormalOriginOperationBase {
+ public:
+  using PromiseType = MozPromise<T, nsresult, false>;
+
+  RefPtr<PromiseType> OnResults() {
+    AssertIsOnOwningThread();
+
+    return mPromiseHolder.Ensure(__func__);
+  }
+
+ protected:
+  ResolvableOriginOp(const char* aRunnableName,
+                     const Nullable<PersistenceType>& aPersistenceType,
+                     const OriginScope& aOriginScope,
+                     const Nullable<Client::Type> aClientType, bool aExclusive)
+      : NormalOriginOperationBase(aRunnableName, aPersistenceType, aOriginScope,
+                                  aClientType, aExclusive) {
+    AssertIsOnOwningThread();
+  }
+
+  virtual ~ResolvableOriginOp() = default;
+
+  virtual T GetResolveValue() = 0;
+
+ private:
+  void SendResults() override {
+#ifdef DEBUG
+    NoteActorDestroyed();
+#endif
+
+    if (NS_SUCCEEDED(mResultCode)) {
+      mPromiseHolder.ResolveIfExists(GetResolveValue(), __func__);
+    } else {
+      mPromiseHolder.RejectIfExists(mResultCode, __func__);
+    }
+  }
+
+  MozPromiseHolder<PromiseType> mPromiseHolder;
+};
+
+}  // namespace mozilla::dom::quota
+
+#endif  // DOM_QUOTA_RESOLVABLEORIGINOP_H_
