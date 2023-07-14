b7d1c7bc00bf52a32e85150fa04dd35207941597
Michael Froman <mfroman@mozilla.com>
2023-07-13 19:13:15 -0500

    Bug 1843113 - Vendor libwebrtc from 779aadeb2e
    Upstream commit: https://webrtc.googlesource.com/src/+/779aadeb2e2041d5ae18439cf26aa61f591d2556
        Revert "[WebRTC-SendPacketsOnWorkerThread] Cleanup VideoSendStream(Impl)"
    
        This reverts commit 77c47947ad098e4182a6244cb998e4fa8c7bd37e.
    
        Reason for revert: Breaks downstream project.
    
        Original change's description:
        > [WebRTC-SendPacketsOnWorkerThread] Cleanup VideoSendStream(Impl)
        >
        > Cleanup and remove usage of MaybeWorkerThread from VideoSendStream.
        > VideoSendStream is now created and lives on the worker thread.
        >
        > Bug: webrtc:14502
        > Change-Id: I81ccf6b9fc6e8889db81b09bd4a75a3831a003e2
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/300842
        > Reviewed-by: Erik Språng <sprang@webrtc.org>
        > Commit-Queue: Per Kjellander <perkj@webrtc.org>
        > Cr-Commit-Position: refs/heads/main@{#39814}
    
        Bug: webrtc:14502
        Change-Id: Ic969071d8797204851ecbaeea3b37f9256303d3d
        No-Presubmit: true
        No-Tree-Checks: true
        No-Try: true
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/300962
        Owners-Override: Mirko Bonadei <mbonadei@webrtc.org>
        Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Auto-Submit: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39819}

--- third_party/libwebrtc/video/video_send_stream_impl.h
+++ third_party/libwebrtc/video/video_send_stream_impl.h
@@ -121,14 +121,14 @@ class VideoSendStreamImpl : public webrtc::BitrateAllocatorObserver,
   void StartupVideoSendStream();
   // Removes the bitrate observer, stops monitoring and notifies the video
   // encoder of the bitrate update.
-  void StopVideoSendStream() RTC_RUN_ON(thread_checker_);
+  void StopVideoSendStream() RTC_RUN_ON(rtp_transport_queue_);
 
   void ConfigureProtection();
   void ConfigureSsrcs();
   void SignalEncoderTimedOut();
   void SignalEncoderActive();
   MediaStreamAllocationConfig GetAllocationConfig() const
-      RTC_RUN_ON(thread_checker_);
+      RTC_RUN_ON(rtp_transport_queue_);
 
   RTC_NO_UNIQUE_ADDRESS SequenceChecker thread_checker_;
   Clock* const clock_;
@@ -138,30 +138,31 @@ class VideoSendStreamImpl : public webrtc::BitrateAllocatorObserver,
   SendStatisticsProxy* const stats_proxy_;
   const VideoSendStream::Config* const config_;
 
-  TaskQueueBase* const worker_queue_;
+  MaybeWorkerThread* const rtp_transport_queue_;
 
   RepeatingTaskHandle check_encoder_activity_task_
-      RTC_GUARDED_BY(thread_checker_);
+      RTC_GUARDED_BY(rtp_transport_queue_);
 
   std::atomic_bool activity_;
-  bool timed_out_ RTC_GUARDED_BY(thread_checker_);
+  bool timed_out_ RTC_GUARDED_BY(rtp_transport_queue_);
 
   RtpTransportControllerSendInterface* const transport_;
   BitrateAllocatorInterface* const bitrate_allocator_;
 
-  bool disable_padding_ RTC_GUARDED_BY(thread_checker_);
-  int max_padding_bitrate_ RTC_GUARDED_BY(thread_checker_);
-  int encoder_min_bitrate_bps_ RTC_GUARDED_BY(thread_checker_);
-  uint32_t encoder_max_bitrate_bps_ RTC_GUARDED_BY(thread_checker_);
-  uint32_t encoder_target_rate_bps_ RTC_GUARDED_BY(thread_checker_);
-  double encoder_bitrate_priority_ RTC_GUARDED_BY(thread_checker_);
+  bool disable_padding_;
+  int max_padding_bitrate_;
+  int encoder_min_bitrate_bps_;
+  uint32_t encoder_max_bitrate_bps_;
+  uint32_t encoder_target_rate_bps_;
+  double encoder_bitrate_priority_;
 
   VideoStreamEncoderInterface* const video_stream_encoder_;
 
   RtcpBandwidthObserver* const bandwidth_observer_;
   RtpVideoSenderInterface* const rtp_video_sender_;
 
-  ScopedTaskSafety worker_queue_safety_;
+  rtc::scoped_refptr<PendingTaskSafetyFlag> transport_queue_safety_ =
+      PendingTaskSafetyFlag::CreateDetached();
 
   // Context for the most recent and last sent video bitrate allocation. Used to
   // throttle sending of similar bitrate allocations.
@@ -171,7 +172,7 @@ class VideoSendStreamImpl : public webrtc::BitrateAllocatorObserver,
     int64_t last_send_time_ms;
   };
   absl::optional<VbaSendContext> video_bitrate_allocation_context_
-      RTC_GUARDED_BY(thread_checker_);
+      RTC_GUARDED_BY(rtp_transport_queue_);
   const absl::optional<float> configured_pacing_factor_;
 };
 }  // namespace internal
