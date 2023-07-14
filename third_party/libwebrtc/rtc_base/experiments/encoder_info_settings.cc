4199fc54c57ff5e3b90947b3ae9a7bcc97703047
Michael Froman <mfroman@mozilla.com>
2023-07-14 11:43:11 -0500

    Bug 1843113 - Vendor libwebrtc from 8a10dca8ff
    Upstream commit: https://webrtc.googlesource.com/src/+/8a10dca8ff1f274945ea70d55503fd2af9779d5a
        Set AV1 default bitrate limits.
    
        Bug: webrtc:15097
        Change-Id: I9106665ccb9e980e9c28f56c8f376c6c0a3c9d35
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/301802
        Commit-Queue: Michael Horowitz <mhoro@webrtc.org>
        Reviewed-by: Åsa Persson <asapersson@webrtc.org>
        Reviewed-by: Sergey Silkin <ssilkin@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39898}

--- third_party/libwebrtc/rtc_base/experiments/encoder_info_settings.cc
+++ third_party/libwebrtc/rtc_base/experiments/encoder_info_settings.cc
@@ -47,10 +47,6 @@ EncoderInfoSettings::GetDefaultSinglecastBitrateLimits(
             {960 * 540, 350000, 30000, 1000000},
             {1280 * 720, 480000, 30000, 1500000}};
   }
-  // Don't override existing AV1 limits with default values.
-  if (codec_type == kVideoCodecAV1) {
-    return {};
-  }
 
   return {{320 * 180, 0, 30000, 300000},
           {480 * 270, 200000, 30000, 500000},
