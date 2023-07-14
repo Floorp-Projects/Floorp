ae11cd49dfd7809241be33aac8c8a4542e9a6c9b
Michael Froman <mfroman@mozilla.com>
2023-07-14 12:57:41 -0500

    Bug 1843113 - Vendor libwebrtc from d3416972dd
    Upstream commit: https://webrtc.googlesource.com/src/+/d3416972dddb77c01eabc4859617e222baf03131
        Video: add new metric for VP9/AV1 hw encoding with softwareBRC
    
        Record the RMSE of encoding bitrate diff from output stream to target
        bitrate set by WebRTC when the AV1/VP9 stream is removed.
    
        Design doc: https://docs.google.com/document/d/1tE7Fquj1U6CwZ01QxxjoCaJS2o0LFYyIxvstLDx0qB0/edit#heading=h.c11ebhaja9tg
    
        Bug: chromium:1358750
        Change-Id: Ifa6f5aacfa8f6b0835f4609f102ae3ae1ee2897e
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/298684
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Commit-Queue: Erik Språng <sprang@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39907}

--- third_party/libwebrtc/video/encoder_bitrate_adjuster.cc
+++ third_party/libwebrtc/video/encoder_bitrate_adjuster.cc
@@ -47,7 +47,9 @@ EncoderBitrateAdjuster::EncoderBitrateAdjuster(const VideoCodec& codec_settings)
     : utilize_bandwidth_headroom_(RateControlSettings::ParseFromFieldTrials()
                                       .BitrateAdjusterCanUseNetworkHeadroom()),
       frames_since_layout_change_(0),
-      min_bitrates_bps_{} {
+      min_bitrates_bps_{},
+      codec_(codec_settings.codecType),
+      codec_mode_(codec_settings.mode) {
   // TODO(https://crbug.com/webrtc/14891): If we want to support simulcast of
   // SVC streams, EncoderBitrateAdjuster needs to be updated to care about both
   // `simulcastStream` and `spatialLayers` at the same time.
@@ -90,7 +92,9 @@ VideoBitrateAllocation EncoderBitrateAdjuster::AdjustRateAllocation(
         ++active_tls[si];
         if (!overshoot_detectors_[si][ti]) {
           overshoot_detectors_[si][ti] =
-              std::make_unique<EncoderOvershootDetector>(kWindowSizeMs);
+              std::make_unique<EncoderOvershootDetector>(
+                  kWindowSizeMs, codec_,
+                  codec_mode_ == VideoCodecMode::kScreensharing);
           frames_since_layout_change_ = 0;
         }
       } else if (overshoot_detectors_[si][ti]) {
