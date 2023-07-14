7a5a6a5c3e872b1cb56e4d86830942435bcf751f
Michael Froman <mfroman@mozilla.com>
2023-07-13 19:38:54 -0500

    Bug 1843113 - Vendor libwebrtc from 5248228d79
    Upstream commit: https://webrtc.googlesource.com/src/+/5248228d79f8894b93374a33444ab1e150baac34
        Do not use deprecated IPv6 on Windows, and set temporary address attribute for IPv6
    
        Currently webrtc does not have info about interface flags, thus it can use deprecated IPv6 and does not prefer temporary addresses as mentioned in https://www.rfc-editor.org/rfc/rfc8835#name-usage-of-temporary-ipv6-add
    
        Test: not able to test it because cannot mock GetAdaptersAddresses system call on Windows. However, it should be correct because the implementation is the same as chromium code. https://source.chromium.org/chromium/chromium/src/+/main:net/base/network_interfaces_win.cc;l=182
    
        Bug: webrtc:14334
        Change-Id: Iae696b00368ae2f9480b542d2ddbc036338081f1
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/300965
        Commit-Queue: Diep Bui <diepbp@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39839}

--- third_party/libwebrtc/rtc_base/network.h
+++ third_party/libwebrtc/rtc_base/network.h
@@ -361,6 +361,9 @@ class RTC_EXPORT BasicNetworkManager : public NetworkManagerBase,
   bool sent_first_update_ = true;
   int start_count_ = 0;
 
+  webrtc::AlwaysValidPointer<const webrtc::FieldTrialsView,
+                             webrtc::FieldTrialBasedConfig>
+      field_trials_;
   std::vector<std::string> network_ignore_list_;
   NetworkMonitorFactory* const network_monitor_factory_;
   SocketFactory* const socket_factory_;
