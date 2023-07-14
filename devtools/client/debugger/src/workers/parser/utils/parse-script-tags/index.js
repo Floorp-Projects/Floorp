65cfd7c6ecc3d15ac15a964c7150054b756e2632
Hubert Boma Manilla <hmanilla@mozilla.com>
2023-07-14 17:34:27 +0000

    Bug 1724562 - [devtools] Move parse-script-tags module to the parser utils file r=devtools-reviewers,ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D182665

---
+++ devtools/client/debugger/src/workers/parser/utils/parse-script-tags/index.js
@@ -0,0 +1,62 @@
+/**
+ *  https://github.com/ryanjduffy/parse-script-tags
+ *
+ * Copyright (c) by Ryan Duff
+ * Licensed under the MIT License.
+ */
+
+import * as types from "@babel/types";
+import * as babelParser from "@babel/parser";
+
+import {
+  generateWhitespace,
+  getCandidateScriptLocations,
+  parseScripts as customParseScripts,
+  parseScriptTags as customParseScriptTags,
+} from "./customParse.js";
+
+function parseScript({ source, line }) {
+  // remove empty or only whitespace scripts
+  if (source.length === 0 || /^\s+$/.test(source)) {
+    return null;
+  }
+
+  try {
+    return babelParser.parse(source, {
+      sourceType: "script",
+      startLine: line,
+    });
+  } catch (e) {
+    return null;
+  }
+}
+
+function parseScripts(locations, parser = parseScript) {
+  return customParseScripts(locations, parser);
+}
+
+function extractScriptTags(source) {
+  return parseScripts(getCandidateScriptLocations(source), loc => {
+    const ast = parseScript(loc);
+
+    if (ast) {
+      return loc;
+    }
+
+    return null;
+  }).filter(types.isFile);
+}
+
+function parseScriptTags(source, parser = parseScript) {
+  return customParseScriptTags(source, parser);
+}
+
+export default parseScriptTags;
+export {
+  extractScriptTags,
+  generateWhitespace,
+  getCandidateScriptLocations,
+  parseScript,
+  parseScripts,
+  parseScriptTags,
+};
