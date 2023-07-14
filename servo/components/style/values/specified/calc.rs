32d9c8746fd2d50abc92cdc933f3d767d951760d
Tiaan Louw <tlouw@mozilla.com>
2023-07-14 11:29:55 +0000

    Bug 1843527 - Move abs() function behind a pref. r=emilio, a=dsmith
    Differential Revision: https://phabricator.services.mozilla.com/D183574

--- servo/components/style/values/specified/calc.rs
+++ servo/components/style/values/specified/calc.rs
@@ -914,6 +914,8 @@ impl CalcNode {
             static_prefs::pref!("layout.css.mod-rem.enabled")
         } else if matches!(function, Pow | Sqrt | Hypot | Log | Exp) {
             static_prefs::pref!("layout.css.exp.enabled")
+        } else if matches!(function, Abs) {
+            static_prefs::pref!("layout.css.abs-sign.enabled")
         } else {
             true
         };
