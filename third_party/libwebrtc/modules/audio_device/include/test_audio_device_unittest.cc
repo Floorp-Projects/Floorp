8174aeae3ee6f0e16266f6a5728015421f0d2466
Michael Froman <mfroman@mozilla.com>
2023-07-14 10:15:54 -0500

    Bug 1843113 - Vendor libwebrtc from e42bf81486
    Upstream commit: https://webrtc.googlesource.com/src/+/e42bf81486d2f08b6dcbf1442287202e937ce52b
        Migrate TestAudioDeviceModule on AudioDeviceModuleImpl
    
        Bug: b/272350185
        Change-Id: Ia3d85d6fa3b0d4809e987a39d60d3eb022687132
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/300363
        Commit-Queue: Artem Titov <titovartem@webrtc.org>
        Reviewed-by: Henrik Andreassson <henrika@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#39877}

--- third_party/libwebrtc/modules/audio_device/include/test_audio_device_unittest.cc
+++ third_party/libwebrtc/modules/audio_device/include/test_audio_device_unittest.cc
@@ -12,17 +12,26 @@
 
 #include <algorithm>
 #include <array>
+#include <memory>
+#include <utility>
 
+#include "absl/types/optional.h"
 #include "api/array_view.h"
+#include "api/task_queue/task_queue_factory.h"
+#include "api/units/time_delta.h"
+#include "api/units/timestamp.h"
 #include "common_audio/wav_file.h"
 #include "common_audio/wav_header.h"
+#include "modules/audio_device/include/audio_device_defines.h"
+#include "rtc_base/checks.h"
 #include "rtc_base/logging.h"
+#include "rtc_base/synchronization/mutex.h"
 #include "test/gmock.h"
 #include "test/gtest.h"
 #include "test/testsupport/file_utils.h"
+#include "test/time_controller/simulated_time_controller.h"
 
 namespace webrtc {
-
 namespace {
 
 void RunTest(const std::vector<int16_t>& input_samples,
@@ -64,7 +73,6 @@ void RunTest(const std::vector<int16_t>& input_samples,
 
   remove(output_filename.c_str());
 }
-}  // namespace
 
 TEST(BoundedWavFileWriterTest, NoSilence) {
   static const std::vector<int16_t> kInputSamples = {
@@ -189,4 +197,185 @@ TEST(PulsedNoiseCapturerTest, SetMaxAmplitude) {
   EXPECT_GT(max_sample, kAmplitude);
 }
 
+using ::testing::ElementsAre;
+
+constexpr Timestamp kStartTime = Timestamp::Millis(10000);
+
+class TestAudioTransport : public AudioTransport {
+ public:
+  enum class Mode { kPlaying, kRecording };
+
+  explicit TestAudioTransport(Mode mode) : mode_(mode) {}
+  ~TestAudioTransport() override = default;
+
+  int32_t RecordedDataIsAvailable(
+      const void* audioSamples,
+      size_t samples_per_channel,
+      size_t bytes_per_sample,
+      size_t number_of_channels,
+      uint32_t samples_per_second,
+      uint32_t total_delay_ms,
+      int32_t clock_drift,
+      uint32_t current_mic_level,
+      bool key_pressed,
+      uint32_t& new_mic_level,
+      absl::optional<int64_t> estimated_capture_time_ns) override {
+    new_mic_level = 1;
+
+    if (mode_ != Mode::kRecording) {
+      EXPECT_TRUE(false)
+          << "NeedMorePlayData mustn't be called when mode isn't kRecording";
+      return -1;
+    }
+
+    MutexLock lock(&mutex_);
+    samples_per_channel_.push_back(samples_per_channel);
+    number_of_channels_.push_back(number_of_channels);
+    bytes_per_sample_.push_back(bytes_per_sample);
+    samples_per_second_.push_back(samples_per_second);
+    return 0;
+  }
+
+  int32_t NeedMorePlayData(size_t samples_per_channel,
+                           size_t bytes_per_sample,
+                           size_t number_of_channels,
+                           uint32_t samples_per_second,
+                           void* audio_samples,
+                           size_t& samples_out,
+                           int64_t* elapsed_time_ms,
+                           int64_t* ntp_time_ms) override {
+    const size_t num_bytes = samples_per_channel * number_of_channels;
+    std::memset(audio_samples, 1, num_bytes);
+    samples_out = samples_per_channel * number_of_channels;
+    *elapsed_time_ms = 0;
+    *ntp_time_ms = 0;
+
+    if (mode_ != Mode::kPlaying) {
+      EXPECT_TRUE(false)
+          << "NeedMorePlayData mustn't be called when mode isn't kPlaying";
+      return -1;
+    }
+
+    MutexLock lock(&mutex_);
+    samples_per_channel_.push_back(samples_per_channel);
+    number_of_channels_.push_back(number_of_channels);
+    bytes_per_sample_.push_back(bytes_per_sample);
+    samples_per_second_.push_back(samples_per_second);
+    return 0;
+  }
+
+  int32_t RecordedDataIsAvailable(const void* audio_samples,
+                                  size_t samples_per_channel,
+                                  size_t bytes_per_sample,
+                                  size_t number_of_channels,
+                                  uint32_t samples_per_second,
+                                  uint32_t total_delay_ms,
+                                  int32_t clockDrift,
+                                  uint32_t current_mic_level,
+                                  bool key_pressed,
+                                  uint32_t& new_mic_level) override {
+    RTC_CHECK(false) << "This methods should be never executed";
+  }
+
+  void PullRenderData(int bits_per_sample,
+                      int sample_rate,
+                      size_t number_of_channels,
+                      size_t number_of_frames,
+                      void* audio_data,
+                      int64_t* elapsed_time_ms,
+                      int64_t* ntp_time_ms) override {
+    RTC_CHECK(false) << "This methods should be never executed";
+  }
+
+  std::vector<size_t> samples_per_channel() const {
+    MutexLock lock(&mutex_);
+    return samples_per_channel_;
+  }
+  std::vector<size_t> number_of_channels() const {
+    MutexLock lock(&mutex_);
+    return number_of_channels_;
+  }
+  std::vector<size_t> bytes_per_sample() const {
+    MutexLock lock(&mutex_);
+    return bytes_per_sample_;
+  }
+  std::vector<size_t> samples_per_second() const {
+    MutexLock lock(&mutex_);
+    return samples_per_second_;
+  }
+
+ private:
+  const Mode mode_;
+
+  mutable Mutex mutex_;
+  std::vector<size_t> samples_per_channel_ RTC_GUARDED_BY(mutex_);
+  std::vector<size_t> number_of_channels_ RTC_GUARDED_BY(mutex_);
+  std::vector<size_t> bytes_per_sample_ RTC_GUARDED_BY(mutex_);
+  std::vector<size_t> samples_per_second_ RTC_GUARDED_BY(mutex_);
+};
+
+TEST(TestAudioDeviceModuleTest, CreatedADMCanRecord) {
+  GlobalSimulatedTimeController time_controller(kStartTime);
+  TestAudioTransport audio_transport(TestAudioTransport::Mode::kRecording);
+  std::unique_ptr<TestAudioDeviceModule::PulsedNoiseCapturer> capturer =
+      TestAudioDeviceModule::CreatePulsedNoiseCapturer(
+          /*max_amplitude=*/1000,
+          /*sampling_frequency_in_hz=*/48000, /*num_channels=*/2);
+
+  rtc::scoped_refptr<AudioDeviceModule> adm = TestAudioDeviceModule::Create(
+      time_controller.GetTaskQueueFactory(), std::move(capturer),
+      /*renderer=*/nullptr);
+
+  ASSERT_EQ(adm->RegisterAudioCallback(&audio_transport), 0);
+  ASSERT_EQ(adm->Init(), 0);
+
+  EXPECT_FALSE(adm->RecordingIsInitialized());
+  ASSERT_EQ(adm->InitRecording(), 0);
+  EXPECT_TRUE(adm->RecordingIsInitialized());
+  ASSERT_EQ(adm->StartRecording(), 0);
+  time_controller.AdvanceTime(TimeDelta::Millis(10));
+  ASSERT_TRUE(adm->Recording());
+  time_controller.AdvanceTime(TimeDelta::Millis(10));
+  ASSERT_EQ(adm->StopRecording(), 0);
+
+  EXPECT_THAT(audio_transport.samples_per_channel(),
+              ElementsAre(480, 480, 480));
+  EXPECT_THAT(audio_transport.number_of_channels(), ElementsAre(2, 2, 2));
+  EXPECT_THAT(audio_transport.bytes_per_sample(), ElementsAre(4, 4, 4));
+  EXPECT_THAT(audio_transport.samples_per_second(),
+              ElementsAre(48000, 48000, 48000));
+}
+
+TEST(TestAudioDeviceModuleTest, CreatedADMCanPlay) {
+  GlobalSimulatedTimeController time_controller(kStartTime);
+  TestAudioTransport audio_transport(TestAudioTransport::Mode::kPlaying);
+  std::unique_ptr<TestAudioDeviceModule::Renderer> renderer =
+      TestAudioDeviceModule::CreateDiscardRenderer(
+          /*sampling_frequency_in_hz=*/48000, /*num_channels=*/2);
+
+  rtc::scoped_refptr<AudioDeviceModule> adm =
+      TestAudioDeviceModule::Create(time_controller.GetTaskQueueFactory(),
+                                    /*capturer=*/nullptr, std::move(renderer));
+
+  ASSERT_EQ(adm->RegisterAudioCallback(&audio_transport), 0);
+  ASSERT_EQ(adm->Init(), 0);
+
+  EXPECT_FALSE(adm->PlayoutIsInitialized());
+  ASSERT_EQ(adm->InitPlayout(), 0);
+  EXPECT_TRUE(adm->PlayoutIsInitialized());
+  ASSERT_EQ(adm->StartPlayout(), 0);
+  time_controller.AdvanceTime(TimeDelta::Millis(10));
+  ASSERT_TRUE(adm->Playing());
+  time_controller.AdvanceTime(TimeDelta::Millis(10));
+  ASSERT_EQ(adm->StopPlayout(), 0);
+
+  EXPECT_THAT(audio_transport.samples_per_channel(),
+              ElementsAre(480, 480, 480));
+  EXPECT_THAT(audio_transport.number_of_channels(), ElementsAre(2, 2, 2));
+  EXPECT_THAT(audio_transport.bytes_per_sample(), ElementsAre(4, 4, 4));
+  EXPECT_THAT(audio_transport.samples_per_second(),
+              ElementsAre(48000, 48000, 48000));
+}
+
+}  // namespace
 }  // namespace webrtc
