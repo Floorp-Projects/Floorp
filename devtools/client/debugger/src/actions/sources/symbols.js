a88c5bd153caa4974d25a72e40257d00d0bfc360
Alexandre Poirot <poirot.alex@gmail.com>
2023-07-15 12:37:41 +0000

    Bug 1822310 - [devtools] Move from "context" to location assertion for setSymbols action. r=devtools-reviewers,bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D182802

--- devtools/client/debugger/src/actions/sources/symbols.js
+++ devtools/client/debugger/src/actions/sources/symbols.js
@@ -10,23 +10,18 @@ import { loadSourceText } from "./loadSourceText";
 import { memoizeableAction } from "../../utils/memoizableAction";
 import { fulfilled } from "../../utils/async-value";
 
-async function doSetSymbols(
-  cx,
-  location,
-  { dispatch, getState, parserWorker }
-) {
+async function doSetSymbols(location, { dispatch, getState, parserWorker }) {
   await dispatch(loadSourceText(location.source, location.sourceActor));
 
   await dispatch({
     type: "SET_SYMBOLS",
-    cx,
     location,
     [PROMISE]: parserWorker.getSymbols(location.source.id),
   });
 }
 
 export const setSymbols = memoizeableAction("setSymbols", {
-  getValue: ({ location }, { getState, parserWorker }) => {
+  getValue: (location, { getState, parserWorker }) => {
     if (!parserWorker.isLocationSupported(location)) {
       return fulfilled(null);
     }
@@ -38,7 +33,6 @@ export const setSymbols = memoizeableAction("setSymbols", {
 
     return fulfilled(symbols);
   },
-  createKey: ({ location }) => location.source.id,
-  action: ({ cx, location }, thunkArgs) =>
-    doSetSymbols(cx, location, thunkArgs),
+  createKey: location => location.source.id,
+  action: (location, thunkArgs) => doSetSymbols(location, thunkArgs),
 });
