ddfabedbaf8333848783666609564922f55094aa
Otto Länd <bind-autoland@mozilla.com>
2023-07-15 09:54:59 +0000

    Bug 1843499: apply code formatting via Lando
    # ignore-this-changeset

--- js/src/jit/x64/MacroAssembler-x64.cpp
+++ js/src/jit/x64/MacroAssembler-x64.cpp
@@ -512,7 +512,7 @@ void MacroAssemblerX64::handleFailureWithHandlerTail(Label* profilerExitTail,
   movq(rsp, rax);
 
   // Call the handler.
-  using Fn = void (*)(ResumeFromException * rfe);
+  using Fn = void (*)(ResumeFromException* rfe);
   asMasm().setupUnalignedABICall(rcx);
   asMasm().passABIArg(rax);
   asMasm().callWithABI<Fn, HandleException>(
