2603b7e9dd0f25827c5221b508ae3860ab24228e
Masayuki Nakano <masayuki@d-toybox.com>
2023-07-16 02:19:23 +0000

    Bug 1232915 - Update `KeyNameList.h` to same key name list as the spec r=smaug
    The multimedia keys selection has `MediaClose`:
    https://w3c.github.io/uievents-key/#keys-multimedia
    
    And `MozPrintableKey` was used while we were implementing `.key` for printable
    keys.  Therefore, we can remove it now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D183479

--- dom/events/KeyNameList.h
+++ dom/events/KeyNameList.h
@@ -28,7 +28,6 @@ DEFINE_KEYNAME_WITH_SAME_NAME(Unidentified)
 /******************************************************************************
  * Our Internal Key Values (must have "Moz" prefix)
  *****************************************************************************/
-DEFINE_KEYNAME_INTERNAL(PrintableKey, "MozPrintableKey")
 DEFINE_KEYNAME_INTERNAL(SoftLeft, "MozSoftLeft")
 DEFINE_KEYNAME_INTERNAL(SoftRight, "MozSoftRight")
 
@@ -212,6 +211,7 @@ DEFINE_KEYNAME_WITH_SAME_NAME(Close)
 DEFINE_KEYNAME_WITH_SAME_NAME(MailForward)
 DEFINE_KEYNAME_WITH_SAME_NAME(MailReply)
 DEFINE_KEYNAME_WITH_SAME_NAME(MailSend)
+DEFINE_KEYNAME_WITH_SAME_NAME(MediaClose)
 DEFINE_KEYNAME_WITH_SAME_NAME(MediaFastForward)
 DEFINE_KEYNAME_WITH_SAME_NAME(MediaPause)
 DEFINE_KEYNAME_WITH_SAME_NAME(MediaPlay)
