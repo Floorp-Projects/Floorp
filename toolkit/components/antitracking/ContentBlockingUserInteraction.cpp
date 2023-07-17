82ec63656cbfb6f9c1d360077fb33f134220a660
Paul Zuehlcke <pbz@mozilla.com>
2023-07-17 17:54:51 +0000

    Bug 1839916 - Extend ContentBlockingUserInteraction to call BounceTrackingProtection on user interaction. r=bvandersloot,anti-tracking-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D182344

--- toolkit/components/antitracking/ContentBlockingUserInteraction.cpp
+++ toolkit/components/antitracking/ContentBlockingUserInteraction.cpp
@@ -8,6 +8,7 @@
 #include "ContentBlockingUserInteraction.h"
 #include "AntiTrackingUtils.h"
 
+#include "mozilla/BounceTrackingProtection.h"
 #include "mozilla/dom/ContentChild.h"
 #include "mozilla/PermissionManager.h"
 #include "nsIPrincipal.h"
@@ -25,6 +26,17 @@ void ContentBlockingUserInteraction::Observe(nsIPrincipal* aPrincipal) {
 
   if (XRE_IsParentProcess()) {
     LOG_PRIN(("Saving the userInteraction for %s", _spec), aPrincipal);
+
+    // The bounce tracking protection has its own interaction store.
+    RefPtr<BounceTrackingProtection> bounceTrackingProtection =
+        BounceTrackingProtection::GetSingleton();
+    // May be nullptr if the feature is disabled.
+    if (bounceTrackingProtection) {
+      nsresult rv = bounceTrackingProtection->RecordUserActivation(aPrincipal);
+      if (NS_WARN_IF(NS_FAILED(rv))) {
+        LOG(("BounceTrackingProtection::RecordUserActivation failed."));
+      }
+    }
 
     PermissionManager* permManager = PermissionManager::GetInstance();
     if (NS_WARN_IF(!permManager)) {
