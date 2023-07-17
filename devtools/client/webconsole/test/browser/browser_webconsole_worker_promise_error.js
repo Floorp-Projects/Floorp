d413f6b81db9ae5ea80c582be31591a4c596fcac
Cristian Tuns <ctuns@mozilla.com>
2023-07-17 04:56:49 -0400

    Backed out changeset 16c42273dc28 (bug 1630497) for causing mochitest failures in dom/worklet/tests/* CLOSED TREE

--- devtools/client/webconsole/test/browser/browser_webconsole_worker_promise_error.js
+++ devtools/client/webconsole/test/browser/browser_webconsole_worker_promise_error.js
@@ -11,6 +11,13 @@ const TEST_URI =
   "test/browser/test-worker-promise-error.html";
 
 add_task(async function () {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["dom.audioworklet.enabled", true],
+      ["dom.worklet.enabled", true],
+    ],
+  });
+
   const hud = await openNewTabAndConsole(TEST_URI);
   await waitFor(() =>
     findErrorMessage(hud, "uncaught exception: worker-error")
