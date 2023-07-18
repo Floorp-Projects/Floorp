572b4478a09ccf470eaa152707b61db5046ea7c7
Benjamin VanderSloot <bvandersloot@mozilla.com>
2023-07-18 19:14:31 +0000

    Bug 1805860 - Add "storage-access" permission to dom::Permissions - r=anti-tracking-reviewers,webidl,saschanaz,pbz,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D182246

--- dom/permission/PermissionUtils.cpp
+++ dom/permission/PermissionUtils.cpp
@@ -18,7 +18,8 @@ static const nsLiteralCString kPermissionTypes[] = {
     "persistent-storage"_ns,
     // "midi" is the only public permission but internally we have both "midi"
     // and "midi-sysex" (and yes, this is confusing).
-    "midi"_ns
+    "midi"_ns,
+    "storage-access"_ns
     // clang-format on
 };
 
@@ -37,6 +38,12 @@ Maybe<PermissionName> TypeToPermissionName(const nsACString& aType) {
   // name is "midi" so we have to special-case it here...
   if (aType.Equals("midi-sysex"_ns)) {
     return Some(PermissionName::Midi);
+  }
+
+  // "storage-access" permissions are also annoying and require a special case.
+  if (StringBeginsWith(aType, "3rdPartyStorage^"_ns) ||
+      StringBeginsWith(aType, "3rdPartyFrameStorage^"_ns)) {
+    return Some(PermissionName::Storage_access);
   }
 
   for (size_t i = 0; i < ArrayLength(kPermissionTypes); ++i) {
