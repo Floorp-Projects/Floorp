3c4c9fa08b1460e5b6e015c21f435e0b200b83d5
AndiAJ <andiaj@users.noreply.github.com>
2023-07-18 15:47:23 +0300

    Bug 1840994 - Fix systemNotificationCantBeDismissedWhileDownloadingTest UI test

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/DownloadTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/DownloadTest.kt
@@ -8,7 +8,6 @@ import androidx.core.net.toUri
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.customannotations.SmokeTest
@@ -351,7 +350,6 @@ class DownloadTest {
         deleteDownloadedFileOnStorage(secondDownloadedFile)
     }
 
-    @Ignore("Failing, see: https://bugzilla.mozilla.org/show_bug.cgi?id=1840994")
     @Test
     fun systemNotificationCantBeDismissedWhileDownloadingTest() {
         // Clear the "Firefox Fenix default browser notification"
@@ -373,13 +371,6 @@ class DownloadTest {
             verifySystemNotificationExists("Firefox Fenix")
             expandNotificationMessage()
             swipeDownloadNotification("Left", false)
-            verifySystemNotificationExists("Firefox Fenix")
-        }.closeNotificationTray {
-        }.openNotificationShade {
-            verifySystemNotificationExists("Firefox Fenix")
-            expandNotificationMessage()
-            swipeDownloadNotification("Right", false)
-            verifySystemNotificationExists("Firefox Fenix")
             clickDownloadNotificationControlButton("CANCEL")
         }
         deleteDownloadedFileOnStorage(downloadFile)
