3d959196d5f5e02747c6da5a7c09f1214375f624
Erik Nordin <enordin@mozilla.com>
2023-07-19 20:41:10 +0000

    Bug 1841366 - Utilize flowId in existing Translations telemetry r=gregtatum
    Instruments all pre-existing Translations telemetry  events
    to utilize the flowId event extra key.
    
    Depends on D183044
    
    Differential Revision: https://phabricator.services.mozilla.com/D183045

--- toolkit/components/translations/TranslationsTelemetry.sys.mjs
+++ toolkit/components/translations/TranslationsTelemetry.sys.mjs
@@ -7,11 +7,41 @@
  */
 export class TranslationsTelemetry {
   /**
+   * A cached value to hold the current flowId.
+   */
+  static #flowId = null;
+
+  /**
    * Telemetry functions for the Translations panel.
    * @returns {Panel}
    */
   static panel() {
     return Panel;
+  }
+
+  /**
+   * Forces the creation of a new Translations telemetry flowId and returns it.
+   * @returns {string}
+   */
+  static createFlowId() {
+    const flowId = crypto.randomUUID();
+    TranslationsTelemetry.#flowId = flowId;
+    return flowId;
+  }
+
+  /**
+   * Returns a Translations telemetry flowId by retrieving the cached value
+   * if available, or creating a new one otherwise.
+   * @returns {string}
+   */
+  static getOrCreateFlowId() {
+    // If we have the flowId cached, return it.
+    if (TranslationsTelemetry.#flowId) {
+      return TranslationsTelemetry.#flowId;
+    }
+
+    // If no flowId exists, create one.
+    return TranslationsTelemetry.createFlowId();
   }
 
   /**
@@ -22,6 +52,7 @@ export class TranslationsTelemetry {
   static onError(errorMessage) {
     Glean.translations.errorRate.addToNumerator(1);
     Glean.translations.error.record({
+      flow_id: TranslationsTelemetry.getOrCreateFlowId(),
       reason: errorMessage,
     });
   }
@@ -37,6 +68,7 @@ export class TranslationsTelemetry {
   static onTranslate(data) {
     Glean.translations.requestsCount.add(1);
     Glean.translations.translationRequest.record({
+      flow_id: TranslationsTelemetry.getOrCreateFlowId(),
       from_language: data.fromLanguage,
       to_language: data.toLanguage,
       auto_translate: data.autoTranslate,
@@ -55,6 +87,7 @@ class Panel {
    */
   static onOpen(openedFromAppMenu) {
     Glean.translationsPanel.open.record({
+      flow_id: TranslationsTelemetry.createFlowId(),
       opened_from: openedFromAppMenu ? "appMenu" : "translationsButton",
     });
   }
