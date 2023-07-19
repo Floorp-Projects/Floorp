ca254349609ca1863210c3693e367936c9f9ea5a
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2023-07-19 10:05:40 +0000

    Bug 1844275 - Stop using floating-point types in PRMJ_Now on Windows. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D183964

--- js/src/vm/Time.cpp
+++ js/src/vm/Time.cpp
@@ -53,7 +53,7 @@ extern "C" WINBASEAPI void WINAPI GetSystemTimePreciseAsFileTime(LPFILETIME);
 #  endif
 
 // Returns the number of microseconds since the Unix epoch.
-static double FileTimeToUnixMicroseconds(const FILETIME& ft) {
+static int64_t FileTimeToUnixMicroseconds(const FILETIME& ft) {
   // Get the time in 100ns intervals.
   int64_t t = (int64_t(ft.dwHighDateTime) << 32) | int64_t(ft.dwLowDateTime);
 
@@ -63,13 +63,13 @@ static double FileTimeToUnixMicroseconds(const FILETIME& ft) {
   t -= TimeToEpochIn100ns;
 
   // Divide by 10 to convert to microseconds.
-  return double(t) * 0.1;
+  return t / 10;
 }
 
 int64_t PRMJ_Now() {
   FILETIME ft;
   GetSystemTimePreciseAsFileTime(&ft);
-  return int64_t(FileTimeToUnixMicroseconds(ft));
+  return FileTimeToUnixMicroseconds(ft);
 }
 #endif
 
