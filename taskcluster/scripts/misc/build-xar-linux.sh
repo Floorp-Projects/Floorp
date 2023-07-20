e01d710ab4d4ff1f798a8a7bfb16d2efdfbcb6f0
Mike Hommey <mh+mozilla@glandium.org>
2023-07-20 20:39:38 +0000

    Bug 1844243 - Statically link libcrypto to xar. r=firefox-build-system-reviewers,sergesanspaille
    When it's not built on the same kind of docker image that runs it,
    there's a possibility of incompatibility because of the recent
    change in openssl library version. This happens when crossing between
    debian 11 and debian 12, for instance.
    
    Differential Revision: https://phabricator.services.mozilla.com/D183944

--- taskcluster/scripts/misc/build-xar-linux.sh
+++ taskcluster/scripts/misc/build-xar-linux.sh
@@ -8,6 +8,13 @@ export PATH=$PATH:$MOZ_FETCHES_DIR/clang/bin
 cd $MOZ_FETCHES_DIR/xar/xar
 
 ./autogen.sh --prefix=/builds/worker --enable-static
+
+# Force statically-linking to libcrypto. pkg-config --static will tell
+# us the extra flags that are needed (in practice, -ldl -pthread),
+# and -lcrypto, which we need to change to actually link statically.
+CRYPTO=$(pkg-config --static --libs libcrypto | sed 's/-lcrypto/-l:libcrypto.a/')
+sed -i "s/-lcrypto/$CRYPTO/" src/Makefile.inc
+
 make_flags="-j$(nproc)"
 make $make_flags
 
