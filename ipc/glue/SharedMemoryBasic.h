79240de11b198b936b67a8071a261a93b0b340a5
Nika Layzell <nika@thelayzells.com>
2023-07-20 20:23:41 +0000

    Bug 1454808 - Remove SharedMemoryBasic_android, r=jld
    The special handling for android is in chromium's SharedMemory type,
    meaning we can use SharedMemoryBasic_chromium now on Android.
    
    This patch removes the android special casing to use the chromium
    backend on that platform as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D182808

--- ipc/glue/SharedMemoryBasic.h
+++ ipc/glue/SharedMemoryBasic.h
@@ -7,9 +7,7 @@
 #ifndef mozilla_ipc_SharedMemoryBasic_h
 #define mozilla_ipc_SharedMemoryBasic_h
 
-#ifdef ANDROID
-#  include "mozilla/ipc/SharedMemoryBasic_android.h"
-#elif defined(XP_DARWIN)
+#ifdef XP_DARWIN
 #  include "mozilla/ipc/SharedMemoryBasic_mach.h"
 #else
 #  include "mozilla/ipc/SharedMemoryBasic_chromium.h"
