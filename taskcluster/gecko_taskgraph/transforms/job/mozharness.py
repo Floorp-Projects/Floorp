da695b5c2baedf7b76fddd5220e892bf32ed3d2a
Mike Hommey <mh+mozilla@glandium.org>
2023-07-20 20:39:38 +0000

    Bug 1844243 - Change the default docker image for mozharness-based tasks to a debian 12-based one. r=taskgraph-reviewers,jcristau
    This changes the docker image used for:
      - artifact-build-linux64-artifact/opt
      - l10n-*-shippable/opt
      - openh264-plugin-{linux,macosx}*
      - repackage-{l10n,linux,macosx,msix,win}*
      - repackage-shippable-l10n-msix*
      - shippable-l10n*
    
    While here, because builds are also mozharness-based, removed the
    duplicate default from the build transform.
    
    Differential Revision: https://phabricator.services.mozilla.com/D183928

--- taskcluster/gecko_taskgraph/transforms/job/mozharness.py
+++ taskcluster/gecko_taskgraph/transforms/job/mozharness.py
@@ -130,9 +130,9 @@ def mozharness_on_docker_worker_setup(config, job, taskdesc):
         )
 
     # Running via mozharness assumes an image that contains build.sh:
-    # by default, debian11-amd64-build, but it could be another image (like
+    # by default, debian12-amd64-build, but it could be another image (like
     # android-build).
-    worker.setdefault("docker-image", {"in-tree": "debian11-amd64-build"})
+    worker.setdefault("docker-image", {"in-tree": "debian12-amd64-build"})
 
     worker.setdefault("artifacts", []).append(
         {
