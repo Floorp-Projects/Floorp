963c5a7e195fc859494f3bf3d5190283477e7c8b
James Teh <jteh@mozilla.com>
2023-07-21 04:03:39 +0000

    Bug 1843389: HTMLTableCellAccessible::Col/RowExtent: Get the table frame from layout instead of walking the a11y tree. r=nlapre
    In some edge cases (such as in accessible/tests/crashtests/1472024-1.html), there can be an outer HTML table which contains an inner table according to layout, but the inner table isn't included in the a11y tree.
    The inner table has no semantic value, so including it wouldn't be appropriate.
    However, HTMLTableAccessible::Col/RowExtent previously retrieved the table using the a11y tree, which meant we used the layout frame for the outer table instead of the inner table.
    This resulted in assertions.
    To fix this, always ask the cell's layout frame for the correct table frame, rather than walking the a11y tree.
    As a bonus, this simplifies and trims the code somewhat.
    
    Differential Revision: https://phabricator.services.mozilla.com/D184066

--- accessible/html/HTMLTableAccessible.cpp
+++ accessible/html/HTMLTableAccessible.cpp
@@ -28,7 +28,6 @@
 #include "nsCoreUtils.h"
 #include "nsDebug.h"
 #include "nsIHTMLCollection.h"
-#include "nsITableCellLayout.h"
 #include "nsError.h"
 #include "nsGkAtoms.h"
 #include "nsLiteralString.h"
@@ -184,54 +183,27 @@ HTMLTableAccessible* HTMLTableCellAccessible::Table() const {
 }
 
 uint32_t HTMLTableCellAccessible::ColExtent() const {
-  int32_t rowIdx = -1, colIdx = -1;
-  if (NS_FAILED(GetCellIndexes(rowIdx, colIdx))) {
+  nsTableCellFrame* cell = do_QueryFrame(GetFrame());
+  if (!cell) {
     // This probably isn't a table according to the layout engine; e.g. it has
     // display: block.
     return 1;
   }
-
-  HTMLTableAccessible* table = Table();
-  if (NS_WARN_IF(!table)) {
-    // This can happen where there is a <tr> inside a <div role="table"> such as
-    // in Monorail.
-    return 1;
-  }
-
-  return table->ColExtentAt(rowIdx, colIdx);
+  nsTableFrame* table = cell->GetTableFrame();
+  MOZ_ASSERT(table);
+  return table->GetEffectiveColSpan(*cell);
 }
 
 uint32_t HTMLTableCellAccessible::RowExtent() const {
-  int32_t rowIdx = -1, colIdx = -1;
-  if (NS_FAILED(GetCellIndexes(rowIdx, colIdx))) {
+  nsTableCellFrame* cell = do_QueryFrame(GetFrame());
+  if (!cell) {
     // This probably isn't a table according to the layout engine; e.g. it has
     // display: block.
     return 1;
   }
-
-  HTMLTableAccessible* table = Table();
-  if (NS_WARN_IF(!table)) {
-    // This can happen where there is a <tr> inside a <div role="table"> such as
-    // in Monorail.
-    return 1;
-  }
-
-  return table->RowExtentAt(rowIdx, colIdx);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// HTMLTableCellAccessible: protected implementation
-
-nsITableCellLayout* HTMLTableCellAccessible::GetCellLayout() const {
-  return do_QueryFrame(mContent->GetPrimaryFrame());
-}
-
-nsresult HTMLTableCellAccessible::GetCellIndexes(int32_t& aRowIdx,
-                                                 int32_t& aColIdx) const {
-  nsITableCellLayout* cellLayout = GetCellLayout();
-  NS_ENSURE_STATE(cellLayout);
-
-  return cellLayout->GetCellIndexes(aRowIdx, aColIdx);
+  nsTableFrame* table = cell->GetTableFrame();
+  MOZ_ASSERT(table);
+  return table->GetEffectiveRowSpan(*cell);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -445,24 +417,6 @@ uint32_t HTMLTableAccessible::ColCount() const {
 uint32_t HTMLTableAccessible::RowCount() {
   nsTableWrapperFrame* tableFrame = GetTableWrapperFrame();
   return tableFrame ? tableFrame->GetRowCount() : 0;
-}
-
-uint32_t HTMLTableAccessible::ColExtentAt(uint32_t aRowIdx, uint32_t aColIdx) {
-  nsTableWrapperFrame* tableFrame = GetTableWrapperFrame();
-  if (!tableFrame) {
-    return 1;
-  }
-
-  return tableFrame->GetEffectiveColSpanAt(aRowIdx, aColIdx);
-}
-
-uint32_t HTMLTableAccessible::RowExtentAt(uint32_t aRowIdx, uint32_t aColIdx) {
-  nsTableWrapperFrame* tableFrame = GetTableWrapperFrame();
-  if (!tableFrame) {
-    return 1;
-  }
-
-  return tableFrame->GetEffectiveRowSpanAt(aRowIdx, aColIdx);
 }
 
 bool HTMLTableAccessible::IsProbablyLayoutTable() {
