ad002fc4dfe10e10f090de106ba16dcc322ad7e6
Alex Rudenko <OrKoN@users.noreply.github.com>
2023-07-21 09:28:39 +0000

    Bug 1843506 [wpt PR 41041] - WebDriver BiDi: add tests for background in browsingContext.create, a=testonly
    Automatic update from web-platform-tests
    WebDriver BiDi: add tests for background in browsingContext.create (#41041)
    
    * WebDriver BiDi: add tests for background in browsingContext.create
    
    * add background invalid type test
    
    * add try/finally
    --
    
    wpt-commits: a74919d21cf5bd74c49328539c9e5ac2c944c7b3
    wpt-pr: 41041

--- testing/web-platform/tests/webdriver/tests/bidi/browsing_context/activate/__init__.py
+++ testing/web-platform/tests/webdriver/tests/bidi/browsing_context/activate/__init__.py
@@ -3,16 +3,6 @@ from typing import Any, Mapping
 from webdriver.bidi.modules.script import ContextTarget
 
 
-async def get_visibility_state(bidi_session, context: Mapping[str, Any]) -> str:
-    result = await bidi_session.script.call_function(
-        function_declaration="""() => {
-        return document.visibilityState;
-    }""",
-        target=ContextTarget(context["context"]),
-        await_promise=False)
-    return result["value"]
-
-
 async def is_selector_focused(bidi_session, context: Mapping[str, Any], selector: str) -> bool:
     result = await bidi_session.script.call_function(
         function_declaration="""(selector) => {
