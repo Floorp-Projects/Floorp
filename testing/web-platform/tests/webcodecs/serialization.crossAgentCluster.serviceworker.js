a442af3881be3ccbc1444aa6047c631addc8e5e0
Chun-Min Chang <chun.m.chang@gmail.com>
2023-07-21 00:44:55 +0000

    Bug 1838984 - Add service-worker WPTs for EncodedVideoChunk serialization r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D184135

--- testing/web-platform/tests/webcodecs/serialization.crossAgentCluster.serviceworker.js
+++ testing/web-platform/tests/webcodecs/serialization.crossAgentCluster.serviceworker.js
@@ -1,4 +1,6 @@
 let videoFrameMap = new Map();
+let encodedVideoChunkMap = new Map();
+
 self.onmessage = (e) => {
   if (e.data == 'create-VideoFrame') {
     let frameOrError = null;
@@ -19,12 +21,41 @@ self.onmessage = (e) => {
     e.source.postMessage(frameOrError);
     return;
   }
+
+  if (e.data == 'create-EncodedVideoChunk') {
+    let chunkOrError = null;
+    try {
+      chunkOrError = new EncodedVideoChunk({
+        type: 'key',
+        timestamp: 0,
+        duration: 1,
+        data: new Uint8Array([2, 3, 4, 5])
+      });
+    } catch (error) {
+      chunkOrError = error
+    }
+    e.source.postMessage(chunkOrError);
+    return;
+  }
+
   if (e.data.hasOwnProperty('videoFrameId')) {
     e.source.postMessage(
       videoFrameMap.get(e.data.videoFrameId) ? 'RECEIVED' : 'NOT_RECEIVED');
     return;
   }
+
+  if (e.data.hasOwnProperty('encodedVideoChunkId')) {
+    e.source.postMessage(
+      encodedVideoChunkMap.get(e.data.encodedVideoChunkId) ? 'RECEIVED' : 'NOT_RECEIVED');
+    return;
+  }
+
   if (e.data.toString() == '[object VideoFrame]') {
     videoFrameMap.set(e.data.timestamp, e.data);
+    return;
+  }
+
+  if (e.data.toString() == '[object EncodedVideoChunk]') {
+    encodedVideoChunkMap.set(e.data.timestamp, e.data);
   }
 };
