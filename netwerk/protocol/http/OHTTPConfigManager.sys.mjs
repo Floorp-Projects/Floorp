39323d16d630fea5e2ad5bad483ce5f52b43fc41
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2023-07-21 08:22:36 +0000

    Bug 1843067 - manage config reuse for OHTTP in the networking code, r=kershaw,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D183727

---
+++ netwerk/protocol/http/OHTTPConfigManager.sys.mjs
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+let knownConfigs = new Map();
+
+export class OHTTPConfigManager {
+  static async get(aURL, aOptions = {}) {
+    try {
+      let config = await this.#getInternal(aURL, aOptions);
+      return new Uint8Array(config);
+    } catch (ex) {
+      console.error(ex);
+      return null;
+    }
+  }
+
+  static async #getInternal(aURL, aOptions = {}) {
+    let { maxAge = -1 } = aOptions;
+    let knownConfig = knownConfigs.get(aURL);
+    if (
+      knownConfig &&
+      (maxAge < 0 || Date.now() - knownConfig.fetchDate < maxAge)
+    ) {
+      return knownConfig.config;
+    }
+    return this.fetchAndStore(aURL, aOptions);
+  }
+
+  static async fetchAndStore(aURL, aOptions = {}) {
+    let fetchDate = Date.now();
+    let resp = await fetch(aURL);
+    if (!resp?.ok) {
+      throw new Error(
+        `Fetching OHTTP config from ${aURL} failed with error ${resp.status}`
+      );
+    }
+    let config = await resp.blob().then(b => b.arrayBuffer());
+    knownConfigs.set(aURL, { config, fetchDate });
+    return config;
+  }
+}
---
+++ netwerk/test/unit/test_ohttp_config_manager.js
@@ -0,0 +1,112 @@
+/* Any copyright is dedicated to the Public Domain.
+http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+let { OHTTPConfigManager } = ChromeUtils.importESModule(
+  "resource://gre/modules/OHTTPConfigManager.sys.mjs"
+);
+
+const { HttpServer } = ChromeUtils.importESModule(
+  "resource://testing-common/httpd.sys.mjs"
+);
+
+let gHttpServer;
+let gValidRequestCount = 0;
+let gFickleIsWorking = true;
+
+add_setup(async function () {
+  gHttpServer = new HttpServer();
+  let invalidHandler = (req, res) => {
+    res.setStatusLine(req.httpVersion, 500, "Oh no, it broke");
+    res.write("Uh oh, it broke.");
+  };
+  let validHandler = (req, res) => {
+    res.setHeader("Content-Type", "application/ohttp-keys");
+    res.write("1234");
+    gValidRequestCount++;
+  };
+
+  gHttpServer.registerPathHandler("/.wellknown/invalid", invalidHandler);
+  gHttpServer.registerPathHandler("/.wellknown/valid", validHandler);
+
+  gHttpServer.registerPathHandler("/.wellknown/fickle", (req, res) => {
+    if (gFickleIsWorking) {
+      return validHandler(req, res);
+    }
+    return invalidHandler(req, res);
+  });
+
+  gHttpServer.start(-1);
+});
+
+function getLocalURL(path) {
+  return `http://localhost:${gHttpServer.identity.primaryPort}/.wellknown/${path}`;
+}
+
+add_task(async function test_broken_url_returns_null() {
+  Assert.equal(await OHTTPConfigManager.get(getLocalURL("invalid")), null);
+});
+
+add_task(async function test_working_url_returns_data() {
+  Assert.deepEqual(
+    await OHTTPConfigManager.get(getLocalURL("valid")),
+    new TextEncoder().encode("1234")
+  );
+});
+
+add_task(async function test_we_only_request_once() {
+  Assert.deepEqual(
+    await OHTTPConfigManager.get(getLocalURL("valid")),
+    new TextEncoder().encode("1234")
+  );
+  let oldRequestCount = gValidRequestCount;
+
+  Assert.deepEqual(
+    await OHTTPConfigManager.get(getLocalURL("valid")),
+    new TextEncoder().encode("1234")
+  );
+  Assert.equal(
+    oldRequestCount,
+    gValidRequestCount,
+    "Shouldn't have made another request."
+  );
+});
+
+add_task(async function test_maxAge_forces_refresh() {
+  Assert.deepEqual(
+    await OHTTPConfigManager.get(getLocalURL("valid")),
+    new TextEncoder().encode("1234")
+  );
+  let oldRequestCount = gValidRequestCount;
+
+  Assert.deepEqual(
+    await OHTTPConfigManager.get(getLocalURL("valid"), { maxAge: 0 }),
+    new TextEncoder().encode("1234")
+  );
+  Assert.equal(
+    oldRequestCount + 1,
+    gValidRequestCount,
+    "Should have made another request due to maxAge."
+  );
+});
+
+add_task(async function test_maxAge_handling_of_invalid_requests() {
+  Assert.deepEqual(
+    await OHTTPConfigManager.get(getLocalURL("fickle")),
+    new TextEncoder().encode("1234")
+  );
+
+  gFickleIsWorking = false;
+
+  Assert.deepEqual(
+    await OHTTPConfigManager.get(getLocalURL("fickle"), { maxAge: 0 }),
+    null
+  );
+
+  Assert.deepEqual(
+    await OHTTPConfigManager.get(getLocalURL("fickle")),
+    new TextEncoder().encode("1234"),
+    "Should still have the cached config if no max age is passed."
+  );
+});
