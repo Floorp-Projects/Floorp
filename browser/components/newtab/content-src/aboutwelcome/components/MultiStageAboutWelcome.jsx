eb39cc9a6513664f3edba673a4450dbb1443b206
Emily McMinn <emcminn@mozilla.com>
2023-07-24 18:59:03 +0000

    Bug 1840517 - Render addons screen & tiles, and allow addons to be intalled from addonspicker component r=desktop-theme-reviewers,omc-reviewers,dao,aminomancer
    Differential Revision: https://phabricator.services.mozilla.com/D182128

--- browser/components/newtab/content-src/aboutwelcome/components/MultiStageAboutWelcome.jsx
+++ browser/components/newtab/content-src/aboutwelcome/components/MultiStageAboutWelcome.jsx
@@ -260,10 +260,19 @@ export const SecondaryCTA = props => {
   let buttonStyling = props.content.secondary_button?.has_arrow_icon
     ? `secondary arrow-icon`
     : `secondary`;
-  const isTextLink = props.content.position !== "split";
+  const isTextLink =
+    props.content.position !== "split" &&
+    props.content.tiles?.type !== "addons-picker";
+  const isPrimary = props.content.secondary_button?.style === "primary";
 
   if (isTextLink) {
     buttonStyling += " text-link";
+  }
+
+  if (isPrimary) {
+    buttonStyling = props.content.secondary_button?.has_arrow_icon
+      ? `primary arrow-icon`
+      : `primary`;
   }
 
   return (
