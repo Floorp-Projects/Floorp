98df68a734b6512d76f31216fada8d27d9d900f3
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2023-07-24 08:06:35 +0000

    Bug 1820720 - Part 1. Import https://gitlab.freedesktop.org/pixman/pixman/-/merge_requests/71. r=gfx-reviewers,lsalzman
    Actually, we need GNU AS to build the assembler files in pixman. This fix
    removes GNU AS dependency for aarch64 assembler files.
    
    Differential Revision: https://phabricator.services.mozilla.com/D183969

--- gfx/cairo/libpixman/src/pixman-arm-asm.h
+++ gfx/cairo/libpixman/src/pixman-arm-asm.h
@@ -26,12 +26,28 @@
  */
 
 /* Supplementary macro for setting function attributes */
-.macro pixman_asm_function fname
-	.func fname
-	.global fname
-#ifdef __ELF__
-	.hidden fname
-	.type fname, %function
+.macro pixman_asm_function_impl fname
+#ifdef ASM_HAVE_FUNC_DIRECTIVE
+	.func \fname
 #endif
-fname:
+	.global \fname
+#ifdef __ELF__
+	.hidden \fname
+	.type \fname, %function
+#endif
+\fname:
+.endm
+
+.macro pixman_asm_function fname
+#ifdef ASM_LEADING_UNDERSCORE
+	pixman_asm_function_impl _\fname
+#else
+	pixman_asm_function_impl \fname
+#endif
+.endm
+
+.macro pixman_end_asm_function
+#ifdef ASM_HAVE_FUNC_DIRECTIVE
+	.endfunc
+#endif
 .endm
