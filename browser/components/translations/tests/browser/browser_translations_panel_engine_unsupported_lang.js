cdf5b87908b3869883e38658df18c07b8dfd6102
Greg Tatum <tatum.creative@gmail.com>
2023-07-24 18:59:49 +0000

    Bug 1831341 - Add the learn more link to the unsupported languages; r=nordzilla
    Differential Revision: https://phabricator.services.mozilla.com/D183030

---
+++ browser/components/translations/tests/browser/browser_translations_panel_engine_unsupported_lang.js
@@ -0,0 +1,60 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests how the unsupported language flow works.
+ */
+add_task(async function test_unsupported_lang() {
+  info("Start at a page in Spanish.");
+  const { cleanup } = await loadTestPage({
+    page: SPANISH_PAGE_URL,
+    languagePairs: [
+      // Do not include Spanish.
+      { fromLang: "fr", toLang: "en", isBeta: false },
+      { fromLang: "en", toLang: "fr", isBeta: false },
+    ],
+  });
+
+  const appMenuButton = getById("PanelUI-menu-button");
+
+  click(appMenuButton, "Opening the app menu");
+  await BrowserTestUtils.waitForEvent(window.PanelUI.mainView, "ViewShown");
+
+  await waitForTranslationsPopupEvent("popupshown", () => {
+    click(getByL10nId("appmenuitem-translate"), "Opening the popup");
+  });
+
+  ok(
+    getByL10nId("translations-panel-error-unsupported"),
+    "The unsupported title is shown."
+  );
+  ok(
+    getByL10nId("translations-panel-error-unsupported-hint-known"),
+    "The unsupported language message is shown."
+  );
+  ok(
+    getByL10nId("translations-panel-learn-more-link"),
+    "The learn more link is available"
+  );
+
+  click(
+    getByL10nId("translations-panel-error-change-button"),
+    "Change the languages."
+  );
+
+  info("Waiting to find the translations panel header.");
+  const header = await waitForCondition(() =>
+    maybeGetByL10nId("translations-panel-header")
+  );
+  ok(
+    header,
+    "The original panel header is there and we are ready to translate again."
+  );
+  ok(
+    !maybeGetByL10nId("translations-panel-error-unsupported"),
+    "The unsupported title is hidden."
+  );
+  await cleanup();
+});
