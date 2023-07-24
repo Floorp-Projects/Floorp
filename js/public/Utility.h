c3b1dc615cbd1f8d3a3af094f4d98a62e8b95bdb
serge-sans-paille <sguelton@mozilla.com>
2023-07-24 08:31:10 +0000

    Bug 1844520 - Forward AutoEnterOOMUnsafeRegion::crash to a static implementation r=nbp
    Interestingly, this removes a reference to `this` which in turns unlocks
    a few compiler optimization.
    
    More specifically, -ftrivial-auto-var-init always initialize the empty
    class with one byte because the object has its address taken by a method
    that does not need it.
    
    Keeping a member function that forwards the call to a static one keeps
    the best of both world: it informs the compiler `this` is not used,
    while making it impossible to call `crash` without a live object.
    
    This patch removes the need for that class to have an address.
    
    Differential Revision: https://phabricator.services.mozilla.com/D184083

--- js/public/Utility.h
+++ js/public/Utility.h
@@ -307,8 +307,10 @@ namespace js {
 
 /* Disable OOM testing in sections which are not OOM safe. */
 struct MOZ_RAII JS_PUBLIC_DATA AutoEnterOOMUnsafeRegion {
-  MOZ_NORETURN MOZ_COLD void crash(const char* reason);
-  MOZ_NORETURN MOZ_COLD void crash(size_t size, const char* reason);
+  MOZ_NORETURN MOZ_COLD void crash(const char* reason) { crash_impl(reason); }
+  MOZ_NORETURN MOZ_COLD void crash(size_t size, const char* reason) {
+    crash_impl(reason);
+  }
 
   using AnnotateOOMAllocationSizeCallback = void (*)(size_t);
   static mozilla::Atomic<AnnotateOOMAllocationSizeCallback, mozilla::Relaxed>
@@ -340,6 +342,9 @@ struct MOZ_RAII JS_PUBLIC_DATA AutoEnterOOMUnsafeRegion {
 
   bool oomEnabled_;
 #  endif
+ private:
+  static MOZ_NORETURN MOZ_COLD void crash_impl(const char* reason);
+  static MOZ_NORETURN MOZ_COLD void crash_impl(size_t size, const char* reason);
 };
 
 } /* namespace js */
