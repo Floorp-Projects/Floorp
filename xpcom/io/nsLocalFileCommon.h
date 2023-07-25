6ac55a7e18e5a95c3aa67a862b41e7a08b378b68
Marco Bonardo <mbonardo@mozilla.com>
2023-07-25 08:16:08 +0000

    Bug 1843758. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D184233

--- xpcom/io/nsLocalFileCommon.h
+++ xpcom/io/nsLocalFileCommon.h
@@ -8,9 +8,9 @@
 #define _NS_LOCAL_FILE_COMMON_H_
 
 #ifdef MOZ_ESR
-extern const char* const sExecutableExts[102];
-#else
 extern const char* const sExecutableExts[103];
+#else
+extern const char* const sExecutableExts[104];
 #endif
 
 #endif
