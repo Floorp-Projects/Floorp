206f7c044cc020775b2bc42e458cbe012c783640
MickeyMoz <sebastian@mozilla.com>
2023-07-25 01:33:38 +0000

    Update GeckoView (Nightly) to 117.0.20230724215726.

--- mobile/android/android-components/plugins/dependencies/src/main/java/Gecko.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "117.0.20230724091621"
+    const val version = "117.0.20230724215726"
 
     /**
      * GeckoView channel
