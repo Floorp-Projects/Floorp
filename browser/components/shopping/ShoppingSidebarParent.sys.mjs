82cb5ec1c199d0bcfef1d2c7ec564d287721e58a
Jared Hirsch <jhirsch@mozilla.com>
2023-07-25 13:29:58 +0000

    Bug 1840847 - Switch shopping sidebar to use Actors for product URL messaging. r=Gijs
    Also closes bug 1844101 and bug 1844097.
    
    Differential Revision: https://phabricator.services.mozilla.com/D184048

---
+++ browser/components/shopping/ShoppingSidebarParent.sys.mjs
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export class ShoppingSidebarParent extends JSWindowActorParent {
+  updateProductURL(aURI) {
+    let url = aURI?.spec || null;
+    this.sendAsyncMessage("ShoppingSidebar:UpdateProductURL", {
+      url,
+    });
+  }
+
+  async receiveMessage(message) {
+    let win = this.browsingContext.top.embedderElement.ownerGlobal;
+    switch (message.name) {
+      case "GetProductURL":
+        let url = win.gBrowser.selectedBrowser.currentURI.spec;
+        return url;
+    }
+    return null;
+  }
+}
