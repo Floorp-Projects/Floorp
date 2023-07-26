67c24c022843d09919f6faf202e1508645a7dfed
Kagami Sascha Rosylight <krosylight@mozilla.com>
2023-07-26 22:23:57 +0000

    Bug 1843453 - Add MozPromiseRejectOnDestruction r=bvandersloot,xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D183778

--- dom/credentialmanagement/identity/IdentityNetworkHelpers.h
+++ dom/credentialmanagement/identity/IdentityNetworkHelpers.h
@@ -40,10 +40,13 @@ RefPtr<TPromise> FetchJSONStructure(Request* aRequest) {
     return resultPromise;
   }
 
+  RefPtr<PromiseNativeHandler> reject =
+      new MozPromiseRejectOnDestruction(resultPromise, __func__);
+
   // Handle the response
   fetchPromise->AddCallbacksWithCycleCollectedArgs(
-      [resultPromise](JSContext* aCx, JS::Handle<JS::Value> aValue,
-                      ErrorResult&) {
+      [resultPromise, reject](JSContext* aCx, JS::Handle<JS::Value> aValue,
+                              ErrorResult&) {
         // Get the Response object from the argument to the callback
         if (NS_WARN_IF(!aValue.isObject())) {
           resultPromise->Reject(NS_ERROR_FAILURE, __func__);
@@ -93,11 +96,13 @@ RefPtr<TPromise> FetchJSONStructure(Request* aRequest) {
                   Promise::TryExtractNSResultFromRejectionValue(aValue),
                   __func__);
             });
+        jsonPromise->AppendNativeHandler(reject);
       },
       [resultPromise](JSContext*, JS::Handle<JS::Value> aValue, ErrorResult&) {
         resultPromise->Reject(
             Promise::TryExtractNSResultFromRejectionValue(aValue), __func__);
       });
+  fetchPromise->AppendNativeHandler(reject);
 
   return resultPromise;
 }
