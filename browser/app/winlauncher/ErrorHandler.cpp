f02bd90f8681b9e03d099efeab646d4e70a68073
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2023-07-26 08:52:50 +0000

    Bug 1844908 - Remove pre-Win10-specific codepath from browser/app/winlauncher/. r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D184287

--- browser/app/winlauncher/ErrorHandler.cpp
+++ browser/app/winlauncher/ErrorHandler.cpp
@@ -15,7 +15,6 @@
 #include "mozilla/UniquePtr.h"
 #include "mozilla/Unused.h"
 #include "mozilla/WinTokenUtils.h"
-#include "mozilla/WindowsVersion.h"
 #include "mozilla/XREAppData.h"
 #include "mozilla/glue/WindowsDllServices.h"
 #include "mozilla/mscom/ProcessRuntime.h"
@@ -291,12 +290,6 @@ static const ProviderKey gProvKeys[] = {
     {WSC_SECURITY_PROVIDER_FIREWALL, "firewall"}};
 
 static bool AddWscInfo(mozilla::JSONWriter& aJson) {
-  if (!mozilla::IsWin8OrLater()) {
-    // We haven't written anything yet, so we can return true here and continue
-    // capturing data.
-    return true;
-  }
-
   // We need COM for this. Using ProcessRuntime so that process-global COM
   // configuration is done correctly
   mozilla::mscom::ProcessRuntime mscom(
