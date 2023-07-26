5326a68120da2b6848467192637fa59a01743b9d
Tim Huang <tihuang@mozilla.com>
2023-07-26 10:14:38 +0000

    Bug 1842030 - Part 2: Check the global private browsing state for HttpBaseChannel::IsBrowsingContextDiscarded() if the loadGroup is not avaiable. r=necko-reviewers,jesup
    The HttpBaseChannel::IsBrowsingContextDiscarded() did always return
    false if the loadGroup is not avaiable. But, this may not be correct for
    the private channels if the private session has been ended. To fix this,
    we make the function to check the global private browsing state
    if the loadGroup is not available for private channels.
    
    Depends on D184010
    
    Differential Revision: https://phabricator.services.mozilla.com/D184479

--- netwerk/test/unit_ipc/test_cookiejars_wrap.js
+++ netwerk/test/unit_ipc/test_cookiejars_wrap.js
@@ -4,6 +4,10 @@ function run_test() {
     "network.cookieJarSettings.unblocked_for_testing",
     true
   );
+  Services.prefs.setBoolPref(
+    "network.cookie.skip_browsing_context_check_in_parent_for_testing",
+    true
+  );
   Services.prefs.setIntPref("network.cookie.cookieBehavior", 0);
   run_test_in_child("../unit/test_cookiejars.js");
 }
