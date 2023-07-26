07c1b978e788c0f612ae171e7af9f8fbf715a0d2
Jared Hirsch <jhirsch@mozilla.com>
2023-07-26 15:01:07 +0000

    Bug 1842913 - Update parent window code to check optedIn pref r=shopping-reviewers,Gijs
    Also fixes bug 1844106, "Update shopping container to display onboarding
    based on optedIn pref".
    
    Differential Revision: https://phabricator.services.mozilla.com/D184129

--- browser/components/shopping/ShoppingSidebarParent.sys.mjs
+++ browser/components/shopping/ShoppingSidebarParent.sys.mjs
@@ -3,11 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 export class ShoppingSidebarParent extends JSWindowActorParent {
-  updateProductURL(aURI) {
-    let url = aURI?.spec || null;
-    this.sendAsyncMessage("ShoppingSidebar:UpdateProductURL", {
-      url,
-    });
+  updateProductURL() {
+    this.sendAsyncMessage("ShoppingSidebar:UpdateProductURL");
   }
 
   async receiveMessage(message) {
