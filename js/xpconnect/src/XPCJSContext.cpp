120b334e147512f632ceae8eea3db5a06c006b97
Tooru Fujisawa <arai_a@mac.com>
2023-07-26 02:26:15 +0000

    Bug 1839102 - Use enum class for Task kind. r=bas
    Differential Revision: https://phabricator.services.mozilla.com/D182980

--- js/xpconnect/src/XPCJSContext.cpp
+++ js/xpconnect/src/XPCJSContext.cpp
@@ -1184,7 +1184,8 @@ class HelperThreadTaskHandler : public Task {
     JS::RunHelperThreadTask();
     return true;
   }
-  explicit HelperThreadTaskHandler() : Task(false, EventQueuePriority::Normal) {
+  explicit HelperThreadTaskHandler()
+      : Task(Kind::OffMainThreadOnly, EventQueuePriority::Normal) {
     // Bug 1703185: Currently all tasks are run at the same priority.
   }
 
