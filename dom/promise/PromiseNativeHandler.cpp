c0d2236aec7762101a9dd03663938884c1bb9cac
Norisz Fay <nfay@mozilla.com>
2023-07-27 03:39:33 +0300

    Backed out changeset f7db385873e9 (bug 1843453) for causing bustages on IdentityNetworkHelpers.h CLOSED TREE

--- dom/promise/PromiseNativeHandler.cpp
+++ dom/promise/PromiseNativeHandler.cpp
@@ -13,8 +13,6 @@
 
 namespace mozilla::dom {
 
-NS_IMPL_ISUPPORTS0(MozPromiseRejectOnDestructionBase)
-
 NS_IMPL_ISUPPORTS0(DomPromiseListener)
 
 DomPromiseListener::DomPromiseListener(CallbackTypeResolved&& aResolve,
