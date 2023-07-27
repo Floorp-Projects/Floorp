6418993dc39b02d98358fba9d4a49dbf1348d197
Emilio Cobos Álvarez <emilio@crisal.io>
2023-07-27 17:54:47 +0200

    Bug 1845623 - Fix windows build because of wrongly-split patch.
    MANUAL PUSH: Trivial fix CLOSED TREE

--- widget/windows/nsNativeThemeWin.cpp
+++ widget/windows/nsNativeThemeWin.cpp
@@ -1852,7 +1852,6 @@ nsresult nsNativeThemeWin::ClassicGetThemePartAndState(
     case StyleAppearance::Tab:
     case StyleAppearance::Tabpanel:
     case StyleAppearance::Tabpanels:
-    case StyleAppearance::Groupbox:
       // these don't use DrawFrameControl
       return NS_OK;
     case StyleAppearance::MozMenulistArrowButton: {
