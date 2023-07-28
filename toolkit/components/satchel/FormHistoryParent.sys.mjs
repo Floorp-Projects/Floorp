1f96caf048e0b1c6f880511c3ff520adab32aaf4
Sergey Galich <sgalich@mozilla.com>
2023-07-28 13:54:25 +0000

    Bug 1835306 - Injecting Firefox Relay integration into Form History r=credential-management-reviewers,mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D183713

--- toolkit/components/satchel/FormHistoryParent.sys.mjs
+++ toolkit/components/satchel/FormHistoryParent.sys.mjs
@@ -2,25 +2,46 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { FirefoxRelayTelemetry } from "resource://gre/modules/FirefoxRelayTelemetry.mjs";
+
 const lazy = {};
 
 ChromeUtils.defineESModuleGetters(lazy, {
+  FirefoxRelay: "resource://gre/modules/FirefoxRelay.sys.mjs",
   FormHistory: "resource://gre/modules/FormHistory.sys.mjs",
+  LoginHelper: "resource://gre/modules/LoginHelper.sys.mjs",
 });
 
 export class FormHistoryParent extends JSWindowActorParent {
-  receiveMessage(message) {
-    switch (message.name) {
+  receiveMessage({ name, data }) {
+    switch (name) {
       case "FormHistory:FormSubmitEntries":
-        this.#onFormSubmitEntries(message.data);
+        this.#onFormSubmitEntries(data);
         break;
 
       case "FormHistory:AutoCompleteSearchAsync":
-        return this.#onAutoCompleteSearch(message.data);
+        return this.#onAutoCompleteSearch(data);
 
       case "FormHistory:RemoveEntry":
-        this.#onRemoveEntry(message.data);
+        this.#onRemoveEntry(data);
         break;
+
+      case "PasswordManager:offerRelayIntegration": {
+        FirefoxRelayTelemetry.recordRelayOfferedEvent(
+          "clicked",
+          data.telemetry.flowId,
+          data.telemetry.scenarioName
+        );
+        return this.#offerRelayIntegration();
+      }
+
+      case "PasswordManager:generateRelayUsername": {
+        FirefoxRelayTelemetry.recordRelayUsernameFilledEvent(
+          "clicked",
+          data.telemetry.flowId
+        );
+        return this.#generateRelayUsername();
+      }
     }
 
     return undefined;
@@ -36,8 +57,29 @@ export class FormHistoryParent extends JSWindowActorParent {
     lazy.FormHistory.update(changes);
   }
 
-  #onAutoCompleteSearch({ searchString, params }) {
-    return lazy.FormHistory.getAutoCompleteResults(searchString, params);
+  get formOrigin() {
+    return lazy.LoginHelper.getLoginOrigin(
+      this.manager.documentPrincipal?.originNoSuffix
+    );
+  }
+
+  async #onAutoCompleteSearch({ searchString, params, scenarioName }) {
+    const formHistoryPromise = lazy.FormHistory.getAutoCompleteResults(
+      searchString,
+      params
+    );
+
+    const relayPromise = lazy.FirefoxRelay.autocompleteItemsAsync({
+      formOrigin: this.formOrigin,
+      scenarioName,
+      hasInput: !!searchString.length,
+    });
+    const [formHistoryEntries, externalEntries] = await Promise.all([
+      formHistoryPromise,
+      relayPromise,
+    ]);
+
+    return { formHistoryEntries, externalEntries };
   }
 
   #onRemoveEntry({ inputName, value, guid }) {
@@ -47,5 +89,19 @@ export class FormHistoryParent extends JSWindowActorParent {
       value,
       guid,
     });
+  }
+
+  getRootBrowser() {
+    return this.browsingContext.topFrameElement;
+  }
+
+  async #offerRelayIntegration() {
+    const browser = this.getRootBrowser();
+    return lazy.FirefoxRelay.offerRelayIntegration(browser, this.formOrigin);
+  }
+
+  async #generateRelayUsername() {
+    const browser = this.getRootBrowser();
+    return lazy.FirefoxRelay.generateUsername(browser, this.formOrigin);
   }
 }
