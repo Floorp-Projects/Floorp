6425ba0125d6952732ac2761b7f063985d60e9c1
Frederic Wang <fred.wang@free.fr>
2023-07-28 03:22:47 +0000

    Bug 1845461 - Add deprecation warning and use counter for deprecated mathvariant usages. r=emilio,flod
    Differential Revision: https://phabricator.services.mozilla.com/D184561

--- browser/base/content/test/static/browser_misused_characters_in_strings.js
+++ browser/base/content/test/static/browser_misused_characters_in_strings.js
@@ -57,6 +57,17 @@ let gExceptionsList = [
     key: "ImportMapExternalNotSupported",
     type: "single-quote",
   },
+  {
+    file: "dom.properties",
+    key: "MathML_DeprecatedMathVariantWarning",
+    type: "single-quote",
+  },
+  // dom.properties is packaged twice so we need to have two exceptions for this string.
+  {
+    file: "dom.properties",
+    key: "MathML_DeprecatedMathVariantWarning",
+    type: "single-quote",
+  },
 ];
 
 /**
