bd9d6c73c7df48cfc04865469c42a9bbd2fdef26
Paul Adenot <paul@paul.cx>
2023-08-01 12:41:05 +0000

    Bug 1826382 - Vendor necessary files to decode all pcm types needed using ffmpeg. r=alwu,media-playback-reviewers DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D174655

--- media/ffvpx/libavcodec/codec_list.c
+++ media/ffvpx/libavcodec/codec_list.c
@@ -20,4 +20,25 @@ static const FFCodec * const codec_list[] = {
 #if CONFIG_LIBVORBIS_DECODER
     &ff_libvorbis_decoder,
 #endif
+#if CONFIG_PCM_ALAW_DECODER
+    &ff_pcm_alaw_decoder,
+#endif
+#if CONFIG_PCM_F32LE_DECODER
+    &ff_pcm_f32le_decoder,
+#endif
+#if CONFIG_PCM_MULAW_DECODER
+    &ff_pcm_mulaw_decoder,
+#endif
+#if CONFIG_PCM_S16LE_DECODER
+    &ff_pcm_s16le_decoder,
+#endif
+#if CONFIG_PCM_S24LE_DECODER
+    &ff_pcm_s24le_decoder,
+#endif
+#if CONFIG_PCM_S32LE_DECODER
+    &ff_pcm_s32le_decoder,
+#endif
+#if CONFIG_PCM_U8_DECODER
+    &ff_pcm_u8_decoder,
+#endif
     NULL };
