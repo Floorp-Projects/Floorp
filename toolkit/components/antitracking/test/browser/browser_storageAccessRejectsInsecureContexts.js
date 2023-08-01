27790348384d3236321b1b87933cc6bd9b70ce56
Harshit Sohaney <hsohaney@mozilla.com>
2023-08-01 17:29:50 +0000

    Bug 1798407 - Restrict Storage Access API usage to within secure contexts. Set the Static Pref to pass all tests which use insecure contexts. r=bvandersloot,anti-tracking-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D182366

---
+++ toolkit/components/antitracking/test/browser/browser_storageAccessRejectsInsecureContexts.js
@@ -0,0 +1,38 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/browser/modules/test/browser/head.js",
+  this
+);
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/toolkit/components/antitracking/test/browser/storage_access_head.js",
+  this
+);
+
+add_task(async function testInsecureContext() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["dom.storage_access.dont_grant_insecure_contexts", true]],
+  });
+
+  await setPreferences();
+
+  await openPageAndRunCode(
+    TEST_TOP_PAGE_HTTPS,
+    getExpectPopupAndClick("accept"),
+    TEST_3RD_PARTY_PAGE,
+    requestStorageAccessAndExpectSuccess
+  );
+
+  await openPageAndRunCode(
+    TEST_TOP_PAGE,
+    expectNoPopup,
+    TEST_3RD_PARTY_PAGE_HTTP,
+    requestStorageAccessAndExpectFailure
+  );
+
+  await cleanUpData();
+  await SpecialPowers.flushPrefEnv();
+});
