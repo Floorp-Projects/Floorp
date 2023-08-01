9c71fb8fc0cbf435b150f50bf6545ead073f604c
Tooru Fujisawa <arai_a@mac.com>
2023-08-01 17:15:22 +0000

    Bug 1846487 - Treat all non-syntactic top-level lexical bindings to be aliased. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D185037

---
+++ js/src/jit-test/tests/debug/Environment-setVariable-19.js
@@ -0,0 +1,22 @@
+// Access on non-syntactic lexical environment's binding.
+
+var g = newGlobal();
+g.evaluate(`
+(function () {
+    const otherDebugger = newGlobal({ sameZoneAs: this }).Debugger;
+    const dbg = otherDebugger(this);
+
+    const env = dbg.getNewestFrame().callee.environment;
+    var ran = false;
+    try {
+        env.setVariable("tdz_variable", 10);
+        ran = true;
+    } catch (e) { }
+    assertEq(ran, false);
+})();
+
+// We shouldn't crash when initializing this.
+let tdz_variable = 10;
+`, {
+  envChainObject: {},
+});
