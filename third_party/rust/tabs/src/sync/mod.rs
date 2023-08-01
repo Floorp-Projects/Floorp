f8d6fce4d4dacf907132216ff45320183b7f3c30
Mark Hammond <mhammond@skippinet.com.au>
2023-08-01 15:45:56 +0000

    Bug 1846347 - update application-services to fix tab syncing. r=skhamis
    Differential Revision: https://phabricator.services.mozilla.com/D184986

--- third_party/rust/tabs/src/sync/mod.rs
+++ third_party/rust/tabs/src/sync/mod.rs
@@ -5,31 +5,3 @@
 pub(crate) mod bridge;
 pub(crate) mod engine;
 pub(crate) mod record;
-
-#[cfg(feature = "full-sync")]
-pub mod full_sync;
-
-// When full-sync isn't enabled we need stub versions for these UDL exposed functions.
-#[cfg(not(feature = "full-sync"))]
-impl crate::TabsStore {
-    pub fn reset(self: std::sync::Arc<Self>) -> crate::error::ApiResult<()> {
-        log::warn!("reset: feature not enabled");
-        Err(crate::error::TabsApiError::SyncError {
-            reason: "reset".to_string(),
-        })
-    }
-
-    pub fn sync(
-        self: std::sync::Arc<Self>,
-        _key_id: String,
-        _access_token: String,
-        _sync_key: String,
-        _tokenserver_url: String,
-        _local_id: String,
-    ) -> crate::error::ApiResult<String> {
-        log::warn!("sync: feature not enabled");
-        Err(crate::error::TabsApiError::SyncError {
-            reason: "sync".to_string(),
-        })
-    }
-}
