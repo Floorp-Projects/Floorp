7bd7d19efd92a577ac09b8e3669664bfd703b958
Cristian Tuns <ctuns@mozilla.com>
2023-08-01 09:45:35 -0400

    Bug 1836204 - Fix prettier lint failures. r=fix CLOSED TREE

--- browser/components/translations/content/translationsPanel.js
+++ browser/components/translations/content/translationsPanel.js
@@ -1055,14 +1055,12 @@ var TranslationsPanel = new (class {
     const { panel, appMenuButton } = this.elements;
     const openedFromAppMenu = target.id === appMenuButton.id;
 
-    panel.addEventListener(
-      "ViewShown",
-      () => {
-        TranslationsParent.telemetry().panel().onOpen({
-          maintainFlow,
-          openedFromAppMenu,
-          isFirstUserInteraction,
-        })
+    panel.addEventListener("ViewShown", () => {
+      TranslationsParent.telemetry().panel().onOpen({
+        maintainFlow,
+        openedFromAppMenu,
+        isFirstUserInteraction,
+      });
     });
     PanelMultiView.openPopup(panel, target, {
       position: "bottomright topright",
