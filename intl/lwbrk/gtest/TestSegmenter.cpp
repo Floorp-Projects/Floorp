5bf18e128b1961a66720357258b7452ff4136cb1
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2023-08-02 10:32:50 +0000

    Bug 1719535 - Part 5. Add ICU4X based segmenter modules. r=TYLin,jfkthame
    Depends on D167673
    
    Differential Revision: https://phabricator.services.mozilla.com/D167675

--- intl/lwbrk/gtest/TestSegmenter.cpp
+++ intl/lwbrk/gtest/TestSegmenter.cpp
@@ -7,11 +7,15 @@
 #include "gtest/gtest.h"
 
 #include "mozilla/intl/Segmenter.h"
+#include "mozilla/Preferences.h"
 
 namespace mozilla::intl {
 
-TEST(IntlSegmenter, TestLineBreakIteratorUtf16)
+TEST(IntlSegmenter, TestLineBreakIteratorUtf16SeekOld)
 {
+  nsresult rv = Preferences::SetBool("intl.icu4x.segmenter.enabled", false);
+  EXPECT_TRUE(rv == NS_OK);
+
   const SegmenterOptions options{SegmenterGranularity::Line};
   auto result = Segmenter::TryCreate("en", options);
   ASSERT_TRUE(result.isOk());
@@ -30,7 +34,50 @@ TEST(IntlSegmenter, TestLineBreakIteratorUtf16)
   ASSERT_EQ(segIter->Seek(0u), Nothing());
 }
 
-TEST(IntlSegmenter, TestWordBreakIteratorUtf16)
+TEST(IntlSegmenter, TestLineBreakIteratorUtf16Seek)
+{
+  nsresult rv = Preferences::SetBool("intl.icu4x.segmenter.enabled", true);
+  EXPECT_TRUE(rv == NS_OK);
+
+  const SegmenterOptions options{SegmenterGranularity::Line};
+  auto result = Segmenter::TryCreate("en", options);
+  ASSERT_TRUE(result.isOk());
+  auto lineSegmenter = result.unwrap();
+
+  const char16_t text[] = u"hello world";
+  UniquePtr<SegmentIteratorUtf16> segIter =
+      lineSegmenter->Segment(MakeStringSpan(text));
+
+  // Seek to space between "hello" and "world".
+  // UAX#14 rule returns before "w".
+  ASSERT_EQ(segIter->Seek(5u), Some(6u));
+
+  ASSERT_EQ(segIter->Next(), Some(11u));
+
+  ASSERT_EQ(segIter->Next(), Nothing());
+
+  // Same as calling Next().
+  ASSERT_EQ(segIter->Seek(0u), Nothing());
+}
+
+TEST(IntlSegmenter, TestWordBreakIteratorUtf16Simple)
+{
+  const SegmenterOptions options{SegmenterGranularity::Word};
+  auto result = Segmenter::TryCreate("en", options);
+  ASSERT_TRUE(result.isOk());
+  auto wordSegmenter = result.unwrap();
+
+  const char16_t text[] = u"hello world";
+  UniquePtr<SegmentIteratorUtf16> segIter =
+      wordSegmenter->Segment(MakeStringSpan(text));
+
+  ASSERT_EQ(segIter->Next(), Some(5u));
+  ASSERT_EQ(segIter->Next(), Some(6u));
+  ASSERT_EQ(segIter->Next(), Some(11u));
+  ASSERT_EQ(segIter->Next(), Nothing());
+}
+
+TEST(IntlSegmenter, TestWordBreakIteratorUtf16Seek)
 {
   const SegmenterOptions options{SegmenterGranularity::Word};
   auto result = Segmenter::TryCreate("en", options);
@@ -51,7 +98,32 @@ TEST(IntlSegmenter, TestWordBreakIteratorUtf16)
   ASSERT_EQ(segIter->Seek(0u), Nothing());
 }
 
-TEST(IntlSegmenter, TestGraphemeClusterBreakIteratorUtf16)
+TEST(IntlSegmenter, TestGraphemeClusterBreakIteratorUtf16Simple)
+{
+  SegmenterOptions options{SegmenterGranularity::Grapheme};
+  auto result = Segmenter::TryCreate("en", options);
+  ASSERT_TRUE(result.isOk());
+  auto graphemeClusterSegmenter = result.unwrap();
+
+  const char16_t text[] = u"hello world";
+  UniquePtr<SegmentIteratorUtf16> segIter =
+      graphemeClusterSegmenter->Segment(MakeStringSpan(text));
+
+  ASSERT_EQ(segIter->Next(), Some(1u));
+  ASSERT_EQ(segIter->Next(), Some(2u));
+  ASSERT_EQ(segIter->Next(), Some(3u));
+  ASSERT_EQ(segIter->Next(), Some(4u));
+  ASSERT_EQ(segIter->Next(), Some(5u));
+  ASSERT_EQ(segIter->Next(), Some(6u));
+  ASSERT_EQ(segIter->Next(), Some(7u));
+  ASSERT_EQ(segIter->Next(), Some(8u));
+  ASSERT_EQ(segIter->Next(), Some(9u));
+  ASSERT_EQ(segIter->Next(), Some(10u));
+  ASSERT_EQ(segIter->Next(), Some(11u));
+  ASSERT_EQ(segIter->Next(), Nothing());
+}
+
+TEST(IntlSegmenter, TestGraphemeClusterBreakIteratorUtf16Seek)
 {
   SegmenterOptions options{SegmenterGranularity::Grapheme};
   auto result = Segmenter::TryCreate("en", options);
@@ -97,9 +169,41 @@ TEST(IntlSegmenter, TestGraphemeClusterBreakReverseIteratorUtf16)
 
 TEST(IntlSegmenter, TestSentenceBreakIteratorUtf16)
 {
+  nsresult rv = Preferences::SetBool("intl.icu4x.segmenter.enabled", true);
+  EXPECT_TRUE(rv == NS_OK);
+
   SegmenterOptions options{SegmenterGranularity::Sentence};
   auto result = Segmenter::TryCreate("en", options);
-  ASSERT_TRUE(result.isErr());
+  ASSERT_TRUE(result.isOk());
+  auto sentenceSegmenter = result.unwrap();
+
+  const char16_t text[] = u"Hello world. Hello world.";
+  UniquePtr<SegmentIteratorUtf16> segIter =
+      sentenceSegmenter->Segment(MakeStringSpan(text));
+
+  ASSERT_EQ(segIter->Next(), Some(13u));
+  ASSERT_EQ(segIter->Next(), Some(25u));
+  ASSERT_EQ(segIter->Next(), Nothing());
+
+  // Same as calling Next().
+  ASSERT_EQ(segIter->Seek(0u), Nothing());
+}
+
+TEST(IntlSegmenter, TestSentenceBreakIteratorUtf16Seek)
+{
+  nsresult rv = Preferences::SetBool("intl.icu4x.segmenter.enabled", true);
+  EXPECT_TRUE(rv == NS_OK);
+
+  SegmenterOptions options{SegmenterGranularity::Sentence};
+  auto result = Segmenter::TryCreate("en", options);
+  ASSERT_TRUE(result.isOk());
+  auto sentenceSegmenter = result.unwrap();
+
+  const char16_t text[] = u"Hello world. Hello world.";
+  UniquePtr<SegmentIteratorUtf16> segIter =
+      sentenceSegmenter->Segment(MakeStringSpan(text));
+
+  ASSERT_EQ(segIter->Seek(5u), Some(13u));
 }
 
 }  // namespace mozilla::intl
