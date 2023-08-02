e3491e5a078981f80598cea7517c22216b1945ae
Brad Werth <bwerth@mozilla.com>
2023-08-02 02:26:14 +0000

    Bug 1845144 Part 2: Remove macOS available and builtin_available directives for always-supported versions of macOS. r=mac-reviewers,mstange,keeler,padenot
    This also does minimal refactoring of cases where the directives were
    protecting a simple expression that could be refactored back to the
    callers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D184399

--- widget/cocoa/nsLookAndFeel.mm
+++ widget/cocoa/nsLookAndFeel.mm
@@ -104,14 +104,6 @@ nscolor nsLookAndFeel::ProcessSelectionBackground(nscolor aColor, ColorScheme aS
 nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme, nscolor& aColor) {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK
 
-  if (@available(macOS 10.14, *)) {
-    // No-op. macOS 10.14+ supports dark mode, so currentAppearance can be set
-    // to either Light or Dark.
-  } else {
-    // System colors before 10.14 are always Light.
-    aScheme = ColorScheme::Light;
-  }
-
   NSAppearance.currentAppearance = NSAppearanceForColorScheme(aScheme);
 
   nscolor color = 0;
@@ -248,13 +240,7 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme, nscolor
       color = GetColorFromNSColor(NSColor.windowFrameColor);
       break;
     case ColorID::Window: {
-      if (@available(macOS 10.14, *)) {
-        color = GetColorFromNSColor(NSColor.windowBackgroundColor);
-      } else {
-        // On 10.13 and below, NSColor.windowBackgroundColor is transparent black.
-        // Use a light grey instead (taken from macOS 11.5).
-        color = NS_RGB(0xF6, 0xF6, 0xF6);
-      }
+      color = GetColorFromNSColor(NSColor.windowBackgroundColor);
       break;
     }
     case ColorID::Field:
@@ -319,7 +305,7 @@ nsresult nsLookAndFeel::NativeGetColor(ColorID aID, ColorScheme aScheme, nscolor
     case ColorID::MozMacActiveMenuitem:
     case ColorID::MozMacActiveSourceListSelection:
     case ColorID::Accentcolor:
-      color = GetColorFromNSColor(ControlAccentColor());
+      color = GetColorFromNSColor([NSColor controlAccentColor]);
       break;
     case ColorID::Marktext:
     case ColorID::Mark:
@@ -522,14 +508,10 @@ nsresult nsLookAndFeel::NativeGetFloat(FloatID aID, float& aResult) {
 }
 
 bool nsLookAndFeel::SystemWantsDarkTheme() {
-  // This returns true if the macOS system appearance is set to dark mode on
-  // 10.14+, false otherwise.
-  if (@available(macOS 10.14, *)) {
-    NSAppearanceName aquaOrDarkAqua = [NSApp.effectiveAppearance
-        bestMatchFromAppearancesWithNames:@[ NSAppearanceNameAqua, NSAppearanceNameDarkAqua ]];
-    return [aquaOrDarkAqua isEqualToString:NSAppearanceNameDarkAqua];
-  }
-  return false;
+  // This returns true if the macOS system appearance is set to dark mode, false otherwise.
+  NSAppearanceName aquaOrDarkAqua = [NSApp.effectiveAppearance
+      bestMatchFromAppearancesWithNames:@[ NSAppearanceNameAqua, NSAppearanceNameDarkAqua ]];
+  return [aquaOrDarkAqua isEqualToString:NSAppearanceNameDarkAqua];
 }
 
 /*static*/
@@ -584,19 +566,11 @@ void nsLookAndFeel::RecordAccessibilityTelemetry() {
                                              name:NSSystemColorsDidChangeNotification
                                            object:nil];
 
-  if (@available(macOS 10.14, *)) {
-    [NSWorkspace.sharedWorkspace.notificationCenter
-        addObserver:self
-           selector:@selector(mediaQueriesChanged)
-               name:NSWorkspaceAccessibilityDisplayOptionsDidChangeNotification
-             object:nil];
-  } else {
-    [NSNotificationCenter.defaultCenter
-        addObserver:self
-           selector:@selector(mediaQueriesChanged)
-               name:NSWorkspaceAccessibilityDisplayOptionsDidChangeNotification
-             object:nil];
-  }
+  [NSWorkspace.sharedWorkspace.notificationCenter
+      addObserver:self
+         selector:@selector(mediaQueriesChanged)
+             name:NSWorkspaceAccessibilityDisplayOptionsDidChangeNotification
+           object:nil];
 
   [NSNotificationCenter.defaultCenter addObserver:self
                                          selector:@selector(scrollbarsChanged)
