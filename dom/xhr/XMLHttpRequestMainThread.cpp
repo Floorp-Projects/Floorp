d5025096ce0e77d6a652b220d8a2789b242937fa
Thomas Wisniewski <twisniewski@mozilla.com>
2023-08-03 03:57:55 +0000

    Bug 1846699 - fix a logic error in bug 1784880 which causes Raptor and streaming videos to break; r=dlrobertson
    Differential Revision: https://phabricator.services.mozilla.com/D185243

--- dom/xhr/XMLHttpRequestMainThread.cpp
+++ dom/xhr/XMLHttpRequestMainThread.cpp
@@ -815,9 +815,6 @@ bool XMLHttpRequestMainThread::IsDeniedCrossSiteCORSRequest() {
 Maybe<nsBaseChannel::ContentRange>
 XMLHttpRequestMainThread::GetRequestedContentRange() const {
   MOZ_ASSERT(mChannel);
-  if (!IsBlobURI(mRequestURL)) {
-    return mozilla::Nothing();
-  }
   nsBaseChannel* baseChan = static_cast<nsBaseChannel*>(mChannel.get());
   if (!baseChan) {
     return mozilla::Nothing();
@@ -1915,7 +1912,7 @@ XMLHttpRequestMainThread::OnStartRequest(nsIRequest* request) {
 
   // If we were asked for a bad range on a blob URL, but we're async,
   // we should throw now in order to fire an error progress event.
-  if (GetRequestedContentRange().isNothing() &&
+  if (IsBlobURI(mRequestURL) && GetRequestedContentRange().isNothing() &&
       mAuthorRequestHeaders.Has("range")) {
     return NS_ERROR_NET_PARTIAL_TRANSFER;
   }
