ac45bff11f740d221e64caa59d012df43c012755
Andrew Halberstadt <ahal@mozilla.com>
2023-08-03 20:39:59 +0000

    Bug 1839891 - [ci] Convert geckodriver-signing tasks to 'from_deps' transforms, r=releng-reviewers,taskgraph-reviewers,bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D185084

--- taskcluster/gecko_taskgraph/transforms/geckodriver_signing.py
+++ taskcluster/gecko_taskgraph/transforms/geckodriver_signing.py
@@ -6,29 +6,44 @@ Transform the repackage signing task into an actual task description.
 """
 
 from taskgraph.transforms.base import TransformSequence
+from taskgraph.util.dependencies import get_primary_dependency
+from taskgraph.util.schema import Schema
 from voluptuous import Optional
 
-from gecko_taskgraph.loader.single_dep import schema
 from gecko_taskgraph.transforms.task import task_description_schema
 from gecko_taskgraph.util.attributes import copy_attributes_from_dependent_job
 from gecko_taskgraph.util.scriptworker import get_signing_cert_scope_per_platform
 
-repackage_signing_description_schema = schema.extend(
+repackage_signing_description_schema = Schema(
     {
         Optional("label"): str,
+        Optional("attributes"): task_description_schema["attributes"],
+        Optional("dependencies"): task_description_schema["dependencies"],
         Optional("treeherder"): task_description_schema["treeherder"],
         Optional("shipping-phase"): task_description_schema["shipping-phase"],
+        Optional("job-from"): task_description_schema["job-from"],
     }
 )
 
 transforms = TransformSequence()
+
+
+@transforms.add
+def remove_name(config, jobs):
+    for job in jobs:
+        if "name" in job:
+            del job["name"]
+        yield job
+
+
 transforms.add_validate(repackage_signing_description_schema)
 
 
 @transforms.add
 def make_signing_description(config, jobs):
     for job in jobs:
-        dep_job = job["primary-dependency"]
+        dep_job = get_primary_dependency(config, job)
+        assert dep_job
 
         attributes = copy_attributes_from_dependent_job(dep_job)
         attributes["repackage_type"] = "repackage-signing"
