30f5a47a02383a680b7ce47f8447e2a8d50f471d
Timothy Nikkel <tnikkel@gmail.com>
2023-08-03 10:50:40 +0000

    Bug 1809492. Clear pointer to nsTreeBodyFrame on XULTreeElement after any possible calls that can set it. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D184318

--- layout/xul/tree/nsTreeBodyFrame.cpp
+++ layout/xul/tree/nsTreeBodyFrame.cpp
@@ -314,9 +314,7 @@ void nsTreeBodyFrame::DestroyFrom(nsIFrame* aDestructRoot,
 
   if (mColumns) mColumns->SetTree(nullptr);
 
-  if (mTree) {
-    mTree->BodyDestroyed(mTopRowIndex);
-  }
+  RefPtr tree = mTree;
 
   if (nsCOMPtr<nsITreeView> view = std::move(mView)) {
     nsCOMPtr<nsITreeSelection> sel;
@@ -325,6 +323,15 @@ void nsTreeBodyFrame::DestroyFrom(nsIFrame* aDestructRoot,
       sel->SetTree(nullptr);
     }
     view->SetTree(nullptr);
+  }
+
+  // Make this call now because view->SetTree can run js which can undo this
+  // call.
+  if (tree) {
+    tree->BodyDestroyed(mTopRowIndex);
+  }
+  if (mTree && mTree != tree) {
+    mTree->BodyDestroyed(mTopRowIndex);
   }
 
   SimpleXULLeafFrame::DestroyFrom(aDestructRoot, aPostDestroyData);
