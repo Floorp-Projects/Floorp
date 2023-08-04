88c7ce5120dba7034cda6ed1de29a56dfb75bdbc
James Teow <jteow@mozilla.com>
2023-08-04 15:04:02 +0000

    Bug 1836156 - Remove extraPageRegexps from search-telemetry-v2 - r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D180862

--- browser/components/search/SearchSERPTelemetry.sys.mjs
+++ browser/components/search/SearchSERPTelemetry.sys.mjs
@@ -295,11 +295,6 @@ class TelemetryHandler {
           r => new RegExp(r)
         );
       }
-      if (provider.extraPageRegexps) {
-        newProvider.extraPageRegexps = provider.extraPageRegexps.map(
-          r => new RegExp(r)
-        );
-      }
 
       newProvider.nonAdsLinkRegexps = provider.nonAdsLinkRegexps?.length
         ? provider.nonAdsLinkRegexps.map(r => new RegExp(r))
