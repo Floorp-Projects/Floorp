95d1e43cb683ca742973e582e8e45ef49fee0b9b
Jonathan Almeida <jalmeida@mozilla.com>
2023-08-04 15:17:57 -0400

    Bug 1847282 - Change the default for pull-to-refresh to disabled
    Changing the default for the feature while we work on fixing some of the
    known glaring bugs that users are facing.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -1425,7 +1425,7 @@ class Settings(private val appContext: Context) : PreferencesHolder {
 
     var isPullToRefreshEnabledInBrowser by booleanPreference(
         appContext.getPreferenceKey(R.string.pref_key_website_pull_to_refresh),
-        default = true,
+        default = Config.channel.isNightlyOrDebug,
     )
 
     var isDynamicToolbarEnabled by booleanPreference(
