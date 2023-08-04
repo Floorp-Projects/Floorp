da0923e8a18966e8a7a17fddd86938b823ac9540
William Durand <will+git@drnd.me>
2023-08-04 14:58:45 +0000

    Bug 1847173 - Add new GeckoRuntimeSettings for the add-on manager web API. r=amejiamarmol,geckoview-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D185396

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/WebExtensionTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/WebExtensionTest.kt
@@ -3077,4 +3077,21 @@ class WebExtensionTest : BaseSessionTest() {
         // Uninstall the add-on before exiting the test.
         sessionRule.waitForResult(controller.uninstall(borderify))
     }
+
+    @Test
+    fun testMozAddonManagerSetting() {
+        val settings = GeckoRuntimeSettings.Builder().build()
+        assertThat(
+            "Extension web API setting should be set to false",
+            settings.extensionsWebAPIEnabled,
+            equalTo(false),
+        )
+
+        val geckoPrefs = sessionRule.getPrefs("extensions.webapi.enabled")
+        assertThat(
+            "extensionsWebAPIEnabled matches Gecko pref value",
+            settings.extensionsWebAPIEnabled,
+            equalTo(geckoPrefs[0] as Boolean),
+        )
+    }
 }
