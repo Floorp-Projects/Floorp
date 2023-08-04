92fd84ee783464c350cdb20f942db6175609c9e6
Greg Tatum <tatum.creative@gmail.com>
2023-08-04 15:17:02 +0000

    Bug 1846828 - Ensure only valid translations will be offered; r=nordzilla, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D185213

--- toolkit/components/translations/actors/TranslationsParent.sys.mjs
+++ toolkit/components/translations/actors/TranslationsParent.sys.mjs
@@ -360,6 +360,18 @@ export class TranslationsParent extends JSWindowActorParent {
       return;
     }
 
+    if (
+      !detectedLanguages.docLangTag ||
+      !detectedLanguages.userLangTag ||
+      !detectedLanguages.isDocLangTagSupported
+    ) {
+      lazy.console.log(
+        "maybeOfferTranslations - The detected languages were not supported.",
+        detectedLanguages
+      );
+      return;
+    }
+
     let host;
     try {
       host = documentURI.host;
@@ -1957,7 +1969,7 @@ export class TranslationsParent extends JSWindowActorParent {
       isDocLangTagSupported: false,
     };
     if (!TranslationsParent.getIsTranslationsEngineSupported()) {
-      return langTags;
+      return null;
     }
 
     if (documentElementLang === undefined) {
