fd2ce5b2824ec0f70a259441f6e437a20eafbb20
Perry McManis <pmcmanis@mozilla.com>
2023-08-04 20:00:16 +0000

    Bug 1845822 - Instrument the shopping opt-out settings r=chutten,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D184770

--- browser/components/shopping/ShoppingUtils.sys.mjs
+++ browser/components/shopping/ShoppingUtils.sys.mjs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { XPCOMUtils } from "resource://gre/modules/XPCOMUtils.sys.mjs";
+
 const lazy = {};
 
 ChromeUtils.defineESModuleGetters(lazy, {
@@ -15,8 +17,10 @@ export const ShoppingUtils = {
   onNimbusUpdate() {
     if (lazy.NimbusFeatures.shopping2023.getVariable("enabled")) {
       ShoppingUtils.init();
+      Glean.shoppingSettings.nimbusDisabledShopping.set(false);
     } else {
       ShoppingUtils.uninit();
+      Glean.shoppingSettings.nimbusDisabledShopping.set(true);
     }
   },
 
@@ -40,6 +44,15 @@ export const ShoppingUtils = {
     // Do startup-time stuff here, like recording startup-time glean events
     // or adjusting onboarding-related prefs once per session.
 
+    XPCOMUtils.defineLazyPreferenceGetter(
+      this,
+      "optedIn",
+      "browser.shopping.experience2023.optedIn",
+      0,
+      this.setOnUpdate
+    );
+    this.setOnUpdate(undefined, undefined, this.optedIn);
+
     this.initialized = true;
   },
 
@@ -55,5 +68,10 @@ export const ShoppingUtils = {
     // prefs for onboarding.
 
     this.initialized = false;
+  },
+
+  setOnUpdate(_pref, _prev, current) {
+    Glean.shoppingSettings.componentOptedOut.set(current === 2);
+    Glean.shoppingSettings.hasOnboarded.set(current > 0);
   },
 };
