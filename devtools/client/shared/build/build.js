1547457a528ff128db3c5bb764b1fef1fb56c55c
Hubert Boma Manilla <hmanilla@mozilla.com>
2023-08-07 07:40:18 +0000

    Bug 1828573 - [devtools] Remove the jsx plugin from the build script r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D185049

--- devtools/client/shared/build/build.js
+++ devtools/client/shared/build/build.js
@@ -14,7 +14,7 @@ const Babel = require("./babel");
 const fs = require("fs");
 const _path = require("path");
 
-const defaultPlugins = ["proposal-class-properties", "transform-react-jsx"];
+const defaultPlugins = ["proposal-class-properties"];
 
 function transform(filePath) {
   // Use the extra plugins only for the debugger
