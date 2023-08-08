be31e3b9e190d7a3c67f03e8731963e49950ac8a
Paul Adenot <paul@paul.cx>
2023-08-08 12:12:41 +0000

    Bug 1826382 - Vendor necessary files to decode all pcm types needed using ffmpeg. r=alwu,media-playback-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D174655

--- dom/media/platforms/ffmpeg/FFmpegLibWrapper.cpp
+++ dom/media/platforms/ffmpeg/FFmpegLibWrapper.cpp
@@ -182,6 +182,8 @@ FFmpegLibWrapper::LinkResult FFmpegLibWrapper::Link() {
            AV_FUNC_AVUTIL_58 | AV_FUNC_AVUTIL_59 | AV_FUNC_AVUTIL_60))
   AV_FUNC(av_image_check_size, AV_FUNC_AVUTIL_ALL)
   AV_FUNC(av_image_get_buffer_size, AV_FUNC_AVUTIL_ALL)
+  AV_FUNC_OPTION(av_channel_layout_default, AV_FUNC_AVUTIL_60)
+  AV_FUNC_OPTION(av_channel_layout_from_mask, AV_FUNC_AVUTIL_60)
   AV_FUNC_OPTION(av_buffer_get_opaque,
                  (AV_FUNC_AVUTIL_56 | AV_FUNC_AVUTIL_57 | AV_FUNC_AVUTIL_58 |
                   AV_FUNC_AVUTIL_59 | AV_FUNC_AVUTIL_60))
