e8d8f42d519750fde78e72e83d1eec6d266c4ca2
Dana Keeler <dkeeler@mozilla.com>
2023-08-09 20:47:56 +0000

    Bug 1676679 - support virtual authenticator functions in webdriver r=webdriver-reviewers,jgraham,whimboo
    Depends on D185198
    
    Differential Revision: https://phabricator.services.mozilla.com/D162624

--- testing/webdriver/src/httpapi.rs
+++ testing/webdriver/src/httpapi.rs
@@ -308,6 +308,41 @@ pub fn standard_routes<U: WebDriverExtensionRoute>() -> Vec<(Method, &'static st
             Route::ReleaseActions,
         ),
         (Method::POST, "/session/{sessionId}/print", Route::Print),
+        (
+            Method::POST,
+            "/sessions/{sessionId}/webauthn/authenticator",
+            Route::WebAuthnAddVirtualAuthenticator,
+        ),
+        (
+            Method::DELETE,
+            "/sessions/{sessionId}/webauthn/authenticator/{authenticatorId}",
+            Route::WebAuthnRemoveVirtualAuthenticator,
+        ),
+        (
+            Method::POST,
+            "/sessions/{sessionId}/webauthn/authenticator/{authenticatorId}/credential",
+            Route::WebAuthnAddCredential,
+        ),
+        (
+            Method::GET,
+            "/sessions/{sessionId}/webauthn/authenticator/{authenticatorId}/credentials",
+            Route::WebAuthnGetCredentials,
+        ),
+        (
+            Method::DELETE,
+            "/sessions/{sessionId}/webauthn/authenticator/{authenticatorId}/credentials/{credentialId}",
+            Route::WebAuthnRemoveCredential,
+        ),
+        (
+            Method::DELETE,
+            "/sessions/{sessionId}/webauthn/authenticator/{authenticatorId}/credentials",
+            Route::WebAuthnRemoveAllCredentials,
+        ),
+        (
+            Method::POST,
+            "/sessions/{sessionId}/webauthn/authenticator/{authenticatorId}/uv",
+            Route::WebAuthnSetUserVerified,
+        ),
         (Method::GET, "/status", Route::Status),
     ]
 }
@@ -381,6 +416,13 @@ pub enum Route<U: WebDriverExtensionRoute> {
     Print,
     Status,
     Extension(U),
+    WebAuthnAddVirtualAuthenticator,
+    WebAuthnRemoveVirtualAuthenticator,
+    WebAuthnAddCredential,
+    WebAuthnGetCredentials,
+    WebAuthnRemoveCredential,
+    WebAuthnRemoveAllCredentials,
+    WebAuthnSetUserVerified,
 }
 
 pub trait WebDriverExtensionRoute: Clone + Send + PartialEq {
