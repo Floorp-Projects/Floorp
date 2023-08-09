f67c5b097793cf8d776d3fced39f4f6eecbb20eb
Paul Adenot <paul@paul.cx>
2023-08-09 07:51:58 +0000

    Bug 1847754 - Make the precision of current position in the state machine always use the same precision. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D185667

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -3739,7 +3739,9 @@ void MediaDecoderStateMachine::UpdatePlaybackPositionInternal(
   MOZ_ASSERT(OnTaskQueue());
   LOGV("UpdatePlaybackPositionInternal(%" PRId64 ")", aTime.ToMicroseconds());
 
-  mCurrentPosition = aTime;
+  // Ensure the position has a precision that matches other TimeUnit such as
+  // buffering ranges and duration.
+  mCurrentPosition = aTime.ToBase(1000000);
   NS_ASSERTION(mCurrentPosition.Ref() >= TimeUnit::Zero(),
                "CurrentTime should be positive!");
   if (mDuration.Ref().ref() < mCurrentPosition.Ref()) {
