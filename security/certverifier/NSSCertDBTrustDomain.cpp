3b79fcd403e327012128eebfd8c7ccd4b3516e07
Chris Peterson <cpeterson@mozilla.com>
2023-08-10 05:07:07 +0000

    Bug 1847154 - security: remove unnecessary OnMojaveOrLater() and OnCatalinaOrLater() version checks. r=keeler,spohl
    Firefox versions >= 116 only support macOS >= 10.15. Mojave is macOS 10.14. Catalina is macOS 10.15.
    
    Also stop including unused "nsCocoaFeatures.h" in EnterpriseRoots.cpp, which has no macOS version checks.
    
    Differential Revision: https://phabricator.services.mozilla.com/D185504

--- security/certverifier/NSSCertDBTrustDomain.cpp
+++ security/certverifier/NSSCertDBTrustDomain.cpp
@@ -49,10 +49,6 @@
 #include "secder.h"
 #include "secerr.h"
 
-#ifdef MOZ_WIDGET_COCOA
-#  include "nsCocoaFeatures.h"
-#endif
-
 #include "TrustOverrideUtils.h"
 #include "TrustOverride-AppleGoogleDigiCertData.inc"
 #include "TrustOverride-SymantecData.inc"
@@ -1796,12 +1792,6 @@ bool LoadIPCClientCertsModule(const nsCString& dir) {
 const char* kOSClientCertsModuleName = "OS Client Cert Module";
 
 bool LoadOSClientCertsModule(const nsCString& dir) {
-#ifdef MOZ_WIDGET_COCOA
-  // osclientcerts requires macOS 10.14 or later
-  if (!nsCocoaFeatures::OnMojaveOrLater()) {
-    return false;
-  }
-#endif
   nsLiteralCString params =
       StaticPrefs::security_osclientcerts_assume_rsa_pss_support()
           ? "RSA-PSS"_ns
