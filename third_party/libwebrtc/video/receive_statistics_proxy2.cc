b378062e77b6ffd4d6ce8eec5b1bac61fa1cb097
Michael Froman <mfroman@mozilla.com>
2023-08-10 13:23:49 -0500

    Bug 1847074 - Vendor libwebrtc from 0df40d1d14
    Upstream commit: https://webrtc.googlesource.com/src/+/0df40d1d14be4a92f0a9e94a186401d0b0b267b8
        RSP2: Rename `delay_counter_` -> `oneway_delay_counter_`
    
        There is a lot of different delays tracked by this class. Renaming this
        member clarifies what delay it actually tracks.
    
        Bug: webrtc:15085
        Change-Id: I8b038ecf84ca262efdc9f69c0f9a2a9eaad81d37
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/304402
        Commit-Queue: Rasmus Brandt <brandtr@webrtc.org>
        Reviewed-by: Åsa Persson <asapersson@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40007}

--- third_party/libwebrtc/video/receive_statistics_proxy2.cc
+++ third_party/libwebrtc/video/receive_statistics_proxy2.cc
@@ -277,7 +277,7 @@ void ReceiveStatisticsProxy::UpdateHistograms(
                                *current_delay_ms);
     log_stream << "WebRTC.Video.CurrentDelayInMs " << *current_delay_ms << '\n';
   }
-  absl::optional<int> delay_ms = delay_counter_.Avg(kMinRequiredSamples);
+  absl::optional<int> delay_ms = oneway_delay_counter_.Avg(kMinRequiredSamples);
   if (delay_ms)
     RTC_HISTOGRAM_COUNTS_10000("WebRTC.Video.OnewayDelayInMs", *delay_ms);
 
@@ -680,9 +680,9 @@ void ReceiveStatisticsProxy::OnFrameBufferTimingsUpdated(
   jitter_buffer_delay_counter_.Add(jitter_buffer_ms);
   target_delay_counter_.Add(target_delay_ms);
   current_delay_counter_.Add(current_delay_ms);
-  // Network delay (rtt/2) + target_delay_ms (jitter delay + decode time +
-  // render delay).
-  delay_counter_.Add(target_delay_ms + avg_rtt_ms_ / 2);
+  // Estimated one-way delay: network delay (rtt/2) + target_delay_ms (jitter
+  // delay + decode time + render delay).
+  oneway_delay_counter_.Add(target_delay_ms + avg_rtt_ms_ / 2);
 }
 
 void ReceiveStatisticsProxy::OnUniqueFramesCounted(int num_unique_frames) {
