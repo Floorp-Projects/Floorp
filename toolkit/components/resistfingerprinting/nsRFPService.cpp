58f7ced89fafde929c309759120f8510213cc140
Tom Schuster <tschuster@mozilla.com>
2023-08-11 09:14:20 +0000

    Bug 1847921 - Support bitwise operations on RFPTarget. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D185804

--- toolkit/components/resistfingerprinting/nsRFPService.cpp
+++ toolkit/components/resistfingerprinting/nsRFPService.cpp
@@ -99,8 +99,8 @@ static constexpr uint32_t kVideoDroppedRatio = 5;
 
 // Fingerprinting protections that are enabled by default. This can be
 // overridden using the privacy.fingerprintingProtection.overrides pref.
-const uint64_t kDefaultFingerintingProtections =
-    uint64_t(RFPTarget::CanvasRandomization);
+const RFPTarget kDefaultFingerintingProtections =
+    RFPTarget::CanvasRandomization;
 
 // ============================================================================
 // ============================================================================
@@ -113,7 +113,7 @@ static StaticRefPtr<nsRFPService> sRFPService;
 static bool sInitialized = false;
 
 // Actually enabled fingerprinting protections.
-static Atomic<uint64_t> sEnabledFingerintingProtections;
+static Atomic<RFPTarget> sEnabledFingerintingProtections;
 
 /* static */
 nsRFPService* nsRFPService::GetOrCreate() {
@@ -184,7 +184,7 @@ bool nsRFPService::IsRFPEnabledFor(RFPTarget aTarget) {
     if (aTarget == RFPTarget::IsAlwaysEnabledForPrecompute) {
       return true;
     }
-    return sEnabledFingerintingProtections & uint64_t(aTarget);
+    return bool(sEnabledFingerintingProtections & aTarget);
   }
 
   return false;
@@ -200,7 +200,7 @@ void nsRFPService::UpdateFPPOverrideList() {
     return;
   }
 
-  uint64_t enabled = kDefaultFingerintingProtections;
+  RFPTarget enabled = kDefaultFingerintingProtections;
   for (const nsAString& each : targetOverrides.Split(',')) {
     Maybe<RFPTarget> mappedValue =
         nsRFPService::TextToRFPTarget(Substring(each, 1, each.Length() - 1));
@@ -211,13 +211,13 @@ void nsRFPService::UpdateFPPOverrideList() {
                 ("RFPTarget::%s is not a valid value",
                  NS_ConvertUTF16toUTF8(each).get()));
       } else if (each[0] == '+') {
-        enabled |= uint64_t(target);
+        enabled |= target;
         MOZ_LOG(gResistFingerprintingLog, LogLevel::Warning,
                 ("Mapped value %s (0x%" PRIx64
                  "), to an addition, now we have 0x%" PRIx64,
                  NS_ConvertUTF16toUTF8(each).get(), uint64_t(target), enabled));
       } else if (each[0] == '-') {
-        enabled &= ~uint64_t(target);
+        enabled &= ~target;
         MOZ_LOG(gResistFingerprintingLog, LogLevel::Warning,
                 ("Mapped value %s (0x%" PRIx64
                  ") to a subtraction, now we have 0x%" PRIx64,
