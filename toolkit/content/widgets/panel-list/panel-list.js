8f3262fdecc3b5eb11c351e1dfdac97683ca72b9
Mike Kaply <mozilla@kaply.com>
2023-08-14 18:07:33 +0000

    Bug 1846302 - Remove unnecessary code to fix CSP error and add a null check. r=jsudiaman,mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D185929

--- toolkit/content/widgets/panel-list/panel-list.js
+++ toolkit/content/widgets/panel-list/panel-list.js
@@ -41,16 +41,6 @@
     constructor() {
       super();
       this.attachShadow({ mode: "open" });
-      // Ensure that the element is hidden even if its main stylesheet hasn't
-      // loaded yet. On initial load, or with cache disabled, the element could
-      // briefly flicker before the stylesheet is loaded without this.
-      let style = document.createElement("style");
-      style.textContent = `
-        :host(:not([open])) {
-          display: none;
-        }
-      `;
-      this.shadowRoot.appendChild(style);
       this.shadowRoot.appendChild(this.constructor.fragment);
     }
 
