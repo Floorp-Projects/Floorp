9e97b11211f1866c672291720083e5658147695a
Jon Coppeard <jcoppeard@mozilla.com>
2023-08-15 09:45:20 +0000

    Bug 1847397 - Get the string heap to use after possible GC when executing regular expressions. r=jandem, a=RyanVM
    The problem here is that initialStringHeap can be stale by the time it's used
    in UpdateRegExpStatics. GenerateRegExpMatchStubShared can allocate a shape
    which can trigger GC which can re-enable previously disabled nursery string
    allocation. This means we may incorrectly elide post barriers here.
    
    The fix is to get the initial heap setting after potential GC.
    
    Differential Revision: https://phabricator.services.mozilla.com/D186012

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -2440,9 +2440,7 @@ static void CreateMatchResultFallback(MacroAssembler& masm, Register object,
 // Generate the RegExpMatcher and RegExpExecMatch stubs. These are very similar,
 // but RegExpExecMatch also has to load and update .lastIndex for global/sticky
 // regular expressions.
-static JitCode* GenerateRegExpMatchStubShared(JSContext* cx,
-                                              gc::Heap initialStringHeap,
-                                              bool isExecMatch) {
+static JitCode* GenerateRegExpMatchStubShared(JSContext* cx, bool isExecMatch) {
   if (isExecMatch) {
     JitSpew(JitSpew_Codegen, "# Emitting RegExpExecMatch stub");
   } else {
@@ -2511,6 +2509,9 @@ static JitCode* GenerateRegExpMatchStubShared(JSContext* cx,
   // The InputOutputData is placed above the frame pointer and return address on
   // the stack.
   int32_t inputOutputDataStartOffset = 2 * sizeof(void*);
+
+  JS::AutoAssertNoGC nogc(cx);
+  gc::Heap initialStringHeap = cx->realm()->jitRealm()->getInitialStringHeap();
 
   Label notFound, oolEntry;
   if (!PrepareAndExecuteRegExp(cx, masm, regexp, input, lastIndex, temp1, temp2,
@@ -2803,13 +2804,11 @@ static JitCode* GenerateRegExpMatchStubShared(JSContext* cx,
 }
 
 JitCode* JitRealm::generateRegExpMatcherStub(JSContext* cx) {
-  return GenerateRegExpMatchStubShared(cx, initialStringHeap,
-                                       /* isExecMatch = */ false);
+  return GenerateRegExpMatchStubShared(cx, /* isExecMatch = */ false);
 }
 
 JitCode* JitRealm::generateRegExpExecMatchStub(JSContext* cx) {
-  return GenerateRegExpMatchStubShared(cx, initialStringHeap,
-                                       /* isExecMatch = */ true);
+  return GenerateRegExpMatchStubShared(cx, /* isExecMatch = */ true);
 }
 
 class OutOfLineRegExpMatcher : public OutOfLineCodeBase<CodeGenerator> {
