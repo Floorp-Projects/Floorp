e0455d68dc0e9c3f55cdb20ff989087f237530ed
scottdowne <sdowne@mozilla.com>
2023-08-15 14:44:05 +0000

    Bug 1847787 - Pocket newtab sponsored topsites enabled by default r=nanj,webdriver-reviewers,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D185694

--- testing/profiles/xpcshell/user.js
+++ testing/profiles/xpcshell/user.js
@@ -33,5 +33,7 @@ user_pref("gfx.color_management.force_srgb", true);
 user_pref("gfx.color_management.mode", 1);
 // Don't enable remote tiles on new-tab pages in xpcshell
 user_pref("browser.topsites.contile.enabled", false);
+// Don't pull sponsored Top Sites content from the network
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 user_pref("security.turn_off_all_security_so_that_viruses_can_take_over_this_computer", true);
 user_pref("preferences.force-disable.check.once.policy", true);
