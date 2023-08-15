27a287f032b85c1ded2324dcfba0045c7214a02f
Narcis Beleuzu <nbeleuzu@mozilla.com>
2023-08-15 18:53:19 +0300

    Backed out changeset 181edf360c56 (bug 1847787) for bc failure on browser_newtab_ping.js

--- testing/profiles/common/user.js
+++ testing/profiles/common/user.js
@@ -77,8 +77,6 @@ user_pref("browser.region.network.url", "");
 user_pref("browser.tabs.unloadOnLowMemory", false);
 // Don't pull Top Sites content from the network
 user_pref("browser.topsites.contile.enabled", false);
-// Don't pull sponsored Top Sites content from the network
-user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 // Default Glean to "record but don't report" mode. Docs:
 // https://firefox-source-docs.mozilla.org/toolkit/components/glean/dev/preferences.html
 user_pref("telemetry.fog.test.localhost_port", -1);
