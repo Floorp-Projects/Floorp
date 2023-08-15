11817a34ed1478ec6164e384d6bb027c089fcfcc
Benjamin VanderSloot <bvandersloot@mozilla.com>
2023-08-15 13:04:04 +0000

    Bug 1835907, part 1 - Add has storage access bit and triggering window id to the LoadInfo - r=smaug,necko-reviewers,kershaw,pbz
    In the Storage Access API's latest draft, a few items were added to the user-agent state. Relevant here,
    the source snapshot params gained two fields that are initialized from the sourceDocument during
    snapshotting source params while navigating: "has storage access" and "environment id".
    
    https://privacycg.github.io/storage-access/#ua-state
    
    These are used to identify self-initiated navigations that come from documents that have obtained storage access.
    Combined with a same-origin check, this determines if the destination document of the navigation should start
    with storage access.
    
    This is stricter than the current behavior, where if the permission is available, all documents start with storage access.
    Instead, now a document will only have storage access if it requests it explicitly or if a same-origin document that has
    storage access navigates itself to that document. This is seen as a security win.
    
    Security discussion of this change was here: https://github.com/privacycg/storage-access/issues/113
    Artur at Google wrote up a great summary here: https://docs.google.com/document/d/1AsrETl-7XvnZNbG81Zy9BcZfKbqACQYBSrjM3VsIpjY/edit#
    
    Differential Revision: https://phabricator.services.mozilla.com/D184821

--- netwerk/base/LoadInfo.h
+++ netwerk/base/LoadInfo.h
@@ -212,7 +212,8 @@ class LoadInfo final : public nsILoadInfo {
       const Maybe<mozilla::dom::ClientInfo>& aInitialClientInfo,
       const Maybe<mozilla::dom::ServiceWorkerDescriptor>& aController,
       nsSecurityFlags aSecurityFlags, uint32_t aSandboxFlags,
-      uint32_t aTriggeringSandboxFlags, nsContentPolicyType aContentPolicyType,
+      uint32_t aTriggeringSandboxFlags, uint64_t aTriggeringWindowId,
+      bool aTriggeringStorageAccess, nsContentPolicyType aContentPolicyType,
       LoadTainting aTainting, bool aBlockAllMixedContent,
       bool aUpgradeInsecureRequests, bool aBrowserUpgradeInsecureRequests,
       bool aBrowserDidUpgradeInsecureRequests,
@@ -306,6 +307,8 @@ class LoadInfo final : public nsILoadInfo {
   nsSecurityFlags mSecurityFlags;
   uint32_t mSandboxFlags;
   uint32_t mTriggeringSandboxFlags = 0;
+  uint64_t mTriggeringWindowId = 0;
+  bool mTriggeringStorageAccess = false;
   nsContentPolicyType mInternalContentPolicyType;
   LoadTainting mTainting = LoadTainting::Basic;
   bool mBlockAllMixedContent = false;
