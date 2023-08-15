3fc8f84388b614cca760f630bfc34469aaffaf45
Ben Dean-Kawamura <bdk@mozilla.com>
2023-08-15 16:41:16 +0000

    Bug 1848041 - Fix variable name mismatch in UniFFIFixtureScaffolding.cpp.  r=markh
    Differential Revision: https://phabricator.services.mozilla.com/D185857

--- toolkit/components/uniffi-bindgen-gecko-js/src/templates/UniFFIScaffolding.cpp
+++ toolkit/components/uniffi-bindgen-gecko-js/src/templates/UniFFIScaffolding.cpp
@@ -52,7 +52,7 @@ extern "C" int {{ cbi.c_handler(prefix) }}(uint64_t aHandle, uint32_t aMethod, c
     // use case for callbacks is logging, which fits very well with this model.
     //
     // So, here we simple queue the callback and return immediately.
-    mozilla::uniffi::QueueCallback({{ callback_ids.get(ci, cbi) }}, aHandle, aMethod, argsData, argsLen);
+    mozilla::uniffi::QueueCallback({{ callback_ids.get(ci, cbi) }}, aHandle, aMethod, aArgsData, aArgsLen);
     return CALLBACK_INTERFACE_SUCCESS;
 }
 static StaticRefPtr<dom::UniFFICallbackHandler> {{ cbi.js_handler() }};
--- toolkit/components/uniffi-js/UniFFIFixtureScaffolding.cpp
+++ toolkit/components/uniffi-js/UniFFIFixtureScaffolding.cpp
@@ -153,7 +153,7 @@ extern "C" int UniFFIFixturesCallbackHandlerLogger(uint64_t aHandle, uint32_t aM
     // use case for callbacks is logging, which fits very well with this model.
     //
     // So, here we simple queue the callback and return immediately.
-    mozilla::uniffi::QueueCallback(0, aHandle, aMethod, argsData, argsLen);
+    mozilla::uniffi::QueueCallback(0, aHandle, aMethod, aArgsData, aArgsLen);
     return CALLBACK_INTERFACE_SUCCESS;
 }
 static StaticRefPtr<dom::UniFFICallbackHandler> JS_CALLBACK_HANDLER_LOGGER;
