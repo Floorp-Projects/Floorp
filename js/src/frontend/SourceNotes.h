c390c7e1856661df80c2f078fb8b8c1be3b8cdb2
Tooru Fujisawa <arai_a@mac.com>
2023-08-16 10:26:32 +0000

    Bug 1847469 - Part 22: Use column number types in SrcNote::ColSpan. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D185760

--- js/src/frontend/SourceNotes.h
+++ js/src/frontend/SourceNotes.h
@@ -13,7 +13,8 @@
 #include <stddef.h>   // ptrdiff_t, size_t
 #include <stdint.h>   // int8_t, uint8_t, uint32_t
 
-#include "jstypes.h"  // js::{Bit, BitMask}
+#include "jstypes.h"          // js::{Bit, BitMask}
+#include "js/ColumnNumber.h"  // JS::ColumnNumberOffset
 
 namespace js {
 
@@ -210,29 +211,30 @@ class SrcNote {
      */
     static constexpr ptrdiff_t ColSpanSignBit = 1 << (OperandBits - 1);
 
-    static inline ptrdiff_t fromOperand(ptrdiff_t operand) {
+    static inline JS::ColumnNumberOffset fromOperand(ptrdiff_t operand) {
       // There should be no bits set outside the field we're going to
       // sign-extend.
       MOZ_ASSERT(!(operand & ~((1U << OperandBits) - 1)));
 
       // Sign-extend the least significant OperandBits bits.
-      return (operand ^ ColSpanSignBit) - ColSpanSignBit;
+      return JS::ColumnNumberOffset((operand ^ ColSpanSignBit) -
+                                    ColSpanSignBit);
     }
 
    public:
     static constexpr ptrdiff_t MinColSpan = -ColSpanSignBit;
     static constexpr ptrdiff_t MaxColSpan = ColSpanSignBit - 1;
 
-    static inline ptrdiff_t toOperand(ptrdiff_t colspan) {
+    static inline ptrdiff_t toOperand(JS::ColumnNumberOffset colspan) {
       // Truncate the two's complement colspan, for storage as an operand.
-      ptrdiff_t operand = colspan & ((1U << OperandBits) - 1);
+      ptrdiff_t operand = colspan.value() & ((1U << OperandBits) - 1);
 
       // When we read this back, we'd better get the value we stored.
       MOZ_ASSERT(fromOperand(operand) == colspan);
       return operand;
     }
 
-    static inline ptrdiff_t getSpan(const SrcNote* sn);
+    static inline JS::ColumnNumberOffset getSpan(const SrcNote* sn);
   };
 
   class SetLine {
@@ -365,7 +367,7 @@ class SrcNoteReader {
 };
 
 /* static */
-inline ptrdiff_t SrcNote::ColSpan::getSpan(const SrcNote* sn) {
+inline JS::ColumnNumberOffset SrcNote::ColSpan::getSpan(const SrcNote* sn) {
   return fromOperand(SrcNoteReader::getOperand(sn, unsigned(Operands::Span)));
 }
 
