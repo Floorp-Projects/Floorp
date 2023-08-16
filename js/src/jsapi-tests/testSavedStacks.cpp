be9222a92ad58c012fcf457eeeecf07076895f56
Tooru Fujisawa <arai_a@mac.com>
2023-08-16 17:31:38 +0000

    Bug 1847469 - Part 9: Use column number types in SavedFrame API. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D185747

--- js/src/jsapi-tests/testSavedStacks.cpp
+++ js/src/jsapi-tests/testSavedStacks.cpp
@@ -7,6 +7,7 @@
 #include "mozilla/Utf8.h"  // mozilla::Utf8Unit
 
 #include "builtin/TestingFunctions.h"
+#include "js/ColumnNumber.h"  // JS::LimitedColumnNumberOneOrigin, JS::TaggedColumnNumberOneOrigin
 #include "js/CompilationAndEvaluation.h"  // JS::Evaluate
 #include "js/Exception.h"
 #include "js/SavedFrameAPI.h"
@@ -46,10 +47,10 @@ BEGIN_TEST(testSavedStacks_ApiDefaultValues) {
   CHECK(line == 0);
 
   // Column
-  uint32_t column = 123;
+  JS::TaggedColumnNumberOneOrigin column(JS::LimitedColumnNumberOneOrigin(123));
   result = JS::GetSavedFrameColumn(cx, principals, savedFrame, &column);
   CHECK(result == JS::SavedFrameResult::AccessDenied);
-  CHECK(column == 0);
+  CHECK(column == JS::TaggedColumnNumberOneOrigin());
 
   // Function display name
   result =
@@ -256,11 +257,11 @@ BEGIN_TEST(testSavedStacks_selfHostedFrames) {
   CHECK_EQUAL(line, 3U);
 
   // Column
-  uint32_t column = 123;
+  JS::TaggedColumnNumberOneOrigin column(JS::LimitedColumnNumberOneOrigin(123));
   result = JS::GetSavedFrameColumn(cx, principals, selfHostedFrame, &column,
                                    JS::SavedFrameSelfHosted::Exclude);
   CHECK(result == JS::SavedFrameResult::Ok);
-  CHECK_EQUAL(column, 9U);
+  CHECK_EQUAL(column.oneOriginValue(), 9U);
 
   // Function display name
   result = JS::GetSavedFrameFunctionDisplayName(
@@ -360,11 +361,12 @@ BEGIN_TEST(test_GetPendingExceptionStack) {
     CHECK_EQUAL(line, expected[i].line);
 
     // Column
-    uint32_t column = 123;
+    JS::TaggedColumnNumberOneOrigin column(
+        JS::LimitedColumnNumberOneOrigin(123));
     result = JS::GetSavedFrameColumn(cx, principals, frame, &column,
                                      JS::SavedFrameSelfHosted::Exclude);
     CHECK(result == JS::SavedFrameResult::Ok);
-    CHECK_EQUAL(column, expected[i].column);
+    CHECK_EQUAL(column.oneOriginValue(), expected[i].column);
 
     // Source
     JS::RootedString str(cx);
