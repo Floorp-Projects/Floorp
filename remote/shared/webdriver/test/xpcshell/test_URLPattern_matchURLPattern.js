fdf495605ae706a6acbe73ff56dd9e20287e7021
Julian Descottes <jdescottes@mozilla.com>
2023-08-16 11:49:37 +0000

    Bug 1847736 - [remote] Create URLPattern module to match simple url patterns r=webdriver-reviewers,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D185452

---
+++ remote/shared/webdriver/test/xpcshell/test_URLPattern_matchURLPattern.js
@@ -0,0 +1,582 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { matchURLPattern, parseURLPattern } = ChromeUtils.importESModule(
+  "chrome://remote/content/shared/webdriver/URLPattern.sys.mjs"
+);
+
+// Test several variations which should match a string based http://example.com
+// pattern.
+add_task(async function test_matchURLPattern_url_variations() {
+  const pattern = parseURLPattern({
+    type: "string",
+    pattern: "http://example.com",
+  });
+
+  const urls = [
+    "http://example.com",
+    "http://EXAMPLE.com",
+    "http://user:password@example.com",
+    "http://example.com:80",
+    "http://example.com/",
+    "http://example.com/#some-hash",
+    "http:example.com",
+    "http:/example.com",
+    // TODO: URLs with a non-empty/non-null search should not match.
+    // https://github.com/w3c/webdriver-bidi/pull/429#discussion_r1284394991
+    "http://example.com?",
+    "http://example.com/?",
+    "http://example.com/?abc",
+  ];
+  for (const url of urls) {
+    ok(
+      matchURLPattern(pattern, url),
+      `url "${url}" should match pattern "http://example.com"`
+    );
+  }
+
+  // Test URLs close to http://example.com but which should not match.
+  const failingUrls = [
+    "https://example.com",
+    "http://example.com:88",
+    "http://example.com/a",
+  ];
+  for (const url of failingUrls) {
+    ok(
+      !matchURLPattern(pattern, url),
+      `url "${url}" should not match pattern "http://example.com"`
+    );
+  }
+});
+
+add_task(async function test_matchURLPattern_stringPatterns() {
+  const tests = [
+    {
+      pattern: "http://example.com",
+      url: "http://example.com",
+      match: true,
+    },
+    {
+      pattern: "HTTP://example.com:80",
+      url: "http://example.com",
+      match: true,
+    },
+    {
+      pattern: "http://example.com:80",
+      url: "http://example.com",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/path",
+      url: "http://example.com/path",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/PATH_CASE",
+      url: "http://example.com/path_case",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/path_single_segment",
+      url: "http://example.com/path_single_segment/",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/path",
+      url: "http://example.com/path_continued",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/path_two_segments/",
+      url: "http://example.com/path_two_segments/",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/path_two_segments/",
+      url: "http://example.com/path_two_segments",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/emptysearch?",
+      url: "http://example.com/emptysearch?",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/emptysearch?",
+      url: "http://example.com/emptysearch",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/emptysearch?",
+      url: "http://example.com/emptysearch??",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/emptysearch?",
+      url: "http://example.com/emptysearch?a",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/search?param",
+      url: "http://example.com/search?param",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/search?param",
+      url: "http://example.com/search?param=value",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/search?param=value",
+      url: "http://example.com/search?param=value",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/search?a=b&c=d",
+      url: "http://example.com/search?a=b&c=d",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/search?a=b&c=d",
+      url: "http://example.com/search?c=d&a=b",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/search?param",
+      url: "http://example.com/search?param#ref",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/search?param#ref",
+      url: "http://example.com/search?param#ref",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/search?param#ref",
+      url: "http://example.com/search?param",
+      match: true,
+    },
+    {
+      pattern: "http://example.com/search?param",
+      url: "http://example.com/search?parameter",
+      match: false,
+    },
+    {
+      pattern: "http://example.com/search?parameter",
+      url: "http://example.com/search?param",
+      match: false,
+    },
+    {
+      pattern: "https://example.com:80",
+      url: "https://example.com",
+      match: false,
+    },
+    {
+      pattern: "https://example.com:443",
+      url: "https://example.com",
+      match: true,
+    },
+    {
+      pattern: "ws://example.com",
+      url: "ws://example.com:80",
+      match: true,
+    },
+  ];
+
+  runMatchPatternTests(tests, "string");
+});
+
+add_task(async function test_patternPatterns_protocol() {
+  const tests = [
+    // Test protocol
+    {
+      pattern: {
+        protocol: "http",
+      },
+      url: "http://example.com",
+      match: true,
+    },
+    {
+      pattern: {
+        protocol: "HTTP",
+      },
+      url: "http://example.com",
+      match: true,
+    },
+    {
+      pattern: {
+        protocol: "http",
+      },
+      url: "http://example.com:80",
+      match: true,
+    },
+    {
+      pattern: {
+        protocol: "http",
+      },
+      url: "http://example.com:1234",
+      // TODO: Port was not specified in the pattern, so this should match.
+      // https://github.com/w3c/webdriver-bidi/pull/429#discussion_r1282752003
+      match: false,
+    },
+    {
+      pattern: {
+        protocol: "http",
+        port: "80",
+      },
+      url: "http://example.com:80",
+      match: true,
+    },
+    {
+      pattern: {
+        protocol: "http",
+        port: "1234",
+      },
+      url: "http://example.com:1234",
+      match: true,
+    },
+    {
+      pattern: {
+        protocol: "http",
+        port: "1234",
+      },
+      url: "http://example.com",
+      match: false,
+    },
+    {
+      pattern: {
+        protocol: "http",
+      },
+      url: "https://wrong-scheme.com",
+      match: false,
+    },
+    {
+      pattern: {
+        protocol: "http",
+      },
+      url: "http://whatever.com/?search#ref",
+      match: true,
+    },
+    {
+      pattern: {
+        protocol: "http",
+      },
+      url: "http://example.com/a",
+      // TODO: pathname was not specified in the pattern, so this should match.
+      // https://github.com/w3c/webdriver-bidi/pull/429/files#r1284443254
+      match: false,
+    },
+    {
+      pattern: {
+        protocol: "http",
+      },
+      url: "http://whatever.com/path?search#ref",
+      // TODO: pathname/search were not specified in the pattern, so this should match.
+      // https://github.com/w3c/webdriver-bidi/pull/429/files#r1284443254
+      match: false,
+    },
+  ];
+
+  runMatchPatternTests(tests, "pattern");
+});
+
+add_task(async function test_patternPatterns_port() {
+  const tests = [
+    {
+      pattern: {
+        protocol: "http",
+        port: "80",
+      },
+      url: "http://abc.com/",
+      match: true,
+    },
+    {
+      pattern: {
+        port: "1234",
+      },
+      url: "http://a.com:1234",
+      match: true,
+    },
+    {
+      pattern: {
+        port: "1234",
+      },
+      url: "https://a.com:1234",
+      match: true,
+    },
+  ];
+
+  runMatchPatternTests(tests, "pattern");
+});
+
+add_task(async function test_patternPatterns_hostname() {
+  const tests = [
+    {
+      pattern: {
+        hostname: "example.com",
+      },
+      url: "http://example.com",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "example.com",
+      },
+      url: "http://example.com:80",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "example.com",
+      },
+      url: "https://example.com",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "example.com",
+      },
+      url: "https://example.com:443",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "example.com",
+      },
+      url: "ws://example.com",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "example.com",
+      },
+      url: "ws://example.com:80",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "example.com",
+      },
+      url: "http://example.com/path",
+      match: false,
+    },
+    {
+      pattern: {
+        hostname: "example.com",
+      },
+      url: "http://example.com/?search",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "example\\{.com",
+      },
+      url: "http://example{.com/",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "example\\{.com",
+      },
+      url: "http://example\\{.com/",
+      match: false,
+    },
+    {
+      pattern: {
+        hostname: "127.0.0.1",
+      },
+      url: "http://127.0.0.1/",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "127.0.0.1",
+      },
+      url: "http://127.0.0.2/",
+      match: false,
+    },
+    {
+      pattern: {
+        hostname: "[2001:db8::1]",
+      },
+      url: "http://[2001:db8::1]/",
+      match: true,
+    },
+    {
+      pattern: {
+        hostname: "[::AB:1]",
+      },
+      url: "http://[::ab:1]/",
+      match: true,
+    },
+  ];
+
+  runMatchPatternTests(tests, "pattern");
+});
+
+add_task(async function test_patternPatterns_hostname() {
+  const tests = [
+    {
+      pattern: {
+        pathname: "/",
+      },
+      url: "http://example.com",
+      match: true,
+    },
+    {
+      pattern: {
+        pathname: "/",
+      },
+      url: "http://example.com/",
+      match: true,
+    },
+    {
+      pattern: {
+        pathname: "/",
+      },
+      url: "http://example.com/?",
+      match: true,
+    },
+    {
+      pattern: {
+        pathname: "path",
+      },
+      url: "http://example.com/path",
+      match: true,
+    },
+    {
+      pattern: {
+        pathname: "/path",
+      },
+      url: "http://example.com/path",
+      match: true,
+    },
+    {
+      pattern: {
+        pathname: "path",
+      },
+      url: "http://example.com/path/",
+      match: false,
+    },
+    {
+      pattern: {
+        pathname: "path",
+      },
+      url: "http://example.com/path_continued",
+      match: false,
+    },
+    {
+      pattern: {
+        pathname: "/",
+      },
+      url: "http://example.com/path",
+      match: false,
+    },
+  ];
+
+  runMatchPatternTests(tests, "pattern");
+});
+
+add_task(async function test_patternPatterns_hostname() {
+  const tests = [
+    {
+      pattern: {
+        search: "",
+      },
+      url: "http://example.com/?",
+      match: true,
+    },
+    {
+      pattern: {
+        search: "",
+      },
+      url: "http://example.com/",
+      match: false,
+    },
+    {
+      pattern: {
+        search: "",
+      },
+      url: "http://example.com/?#",
+      match: true,
+    },
+    {
+      pattern: {
+        search: "?",
+      },
+      url: "http://example.com/?",
+      match: true,
+    },
+    {
+      pattern: {
+        search: "?a",
+      },
+      url: "http://example.com/?a",
+      match: true,
+    },
+    {
+      pattern: {
+        search: "?",
+      },
+      url: "http://example.com/??",
+      match: false,
+    },
+    {
+      pattern: {
+        search: "query",
+      },
+      url: "http://example.com/?query",
+      match: true,
+    },
+    {
+      pattern: {
+        search: "?query",
+      },
+      url: "http://example.com/?query",
+      match: true,
+    },
+    {
+      pattern: {
+        search: "query=value",
+      },
+      url: "http://example.com/?query=value",
+      match: true,
+    },
+    {
+      pattern: {
+        search: "query",
+      },
+      url: "http://example.com/?query=value",
+      match: false,
+    },
+    {
+      pattern: {
+        search: "query",
+      },
+      url: "http://example.com/?query#value",
+      match: true,
+    },
+  ];
+
+  runMatchPatternTests(tests, "pattern");
+});
+
+function runMatchPatternTests(tests, type) {
+  for (const test of tests) {
+    let pattern;
+    if (type == "pattern") {
+      pattern = parseURLPattern({ type: "pattern", ...test.pattern });
+    } else {
+      pattern = parseURLPattern({ type: "string", pattern: test.pattern });
+    }
+
+    equal(
+      matchURLPattern(pattern, test.url),
+      test.match,
+      `url "${test.url}" ${
+        test.match ? "should" : "should not"
+      } match pattern ${JSON.stringify(test.pattern)}`
+    );
+  }
+}
---
+++ remote/shared/webdriver/test/xpcshell/test_URLPattern_parseURLPattern.js
@@ -0,0 +1,369 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { parseURLPattern } = ChromeUtils.importESModule(
+  "chrome://remote/content/shared/webdriver/URLPattern.sys.mjs"
+);
+
+add_task(async function test_parseURLPattern_stringPatterns() {
+  const STRING_PATTERN_TESTS = [
+    {
+      input: "http://example.com",
+      protocol: "http",
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      input: "http://example.com/",
+      protocol: "http",
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      input: "http://EXAMPLE.com",
+      protocol: "http",
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      input: "http://example%2Ecom",
+      protocol: "http",
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+
+    {
+      input: "http://example.com:80",
+      protocol: "http",
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      input: "http://example.com:8888",
+      protocol: "http",
+      hostname: "example.com",
+      port: "8888",
+      pathname: "/",
+      search: null,
+    },
+    {
+      input: "http://example.com/a////b",
+      protocol: "http",
+      hostname: "example.com",
+      port: "",
+      pathname: "/a////b",
+      search: null,
+    },
+    {
+      input: "http://example.com/?",
+      protocol: "http",
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: "",
+    },
+    {
+      input: "http://example.com/??",
+      protocol: "http",
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: "?",
+    },
+    {
+      input: "http://example.com/?/",
+      protocol: "http",
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: "/",
+    },
+    {
+      input: "file:///testfolder/test.zip",
+      protocol: "file",
+      hostname: "",
+      port: null,
+      pathname: "/testfolder/test.zip",
+      search: null,
+    },
+    {
+      input: "http://example\\{.com/",
+      protocol: "http",
+      hostname: "example{.com",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      input: "http://[2001:db8::1]/",
+      protocol: "http",
+      hostname: "[2001:db8::1]",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      input: "http://127.0.0.1/",
+      protocol: "http",
+      hostname: "127.0.0.1",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+  ];
+
+  for (const test of STRING_PATTERN_TESTS) {
+    const pattern = parseURLPattern({
+      type: "string",
+      pattern: test.input,
+    });
+
+    equal(pattern.protocol, "protocol" in test ? test.protocol : null);
+    equal(pattern.hostname, "hostname" in test ? test.hostname : null);
+    equal(pattern.port, "port" in test ? test.port : null);
+    equal(pattern.pathname, "pathname" in test ? test.pathname : null);
+    equal(pattern.search, "search" in test ? test.search : null);
+  }
+});
+
+add_task(async function test_parseURLPattern_patternPatterns() {
+  const PATTERN_PATTERN_TESTS = [
+    {
+      pattern: {
+        protocol: "http",
+      },
+      protocol: "http",
+      hostname: null,
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      pattern: {
+        protocol: "HTTP",
+      },
+      protocol: "http",
+      hostname: null,
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      pattern: {
+        hostname: "example.com",
+      },
+      protocol: null,
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      pattern: {
+        hostname: "EXAMPLE.com",
+      },
+      protocol: null,
+      hostname: "example.com",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      pattern: {
+        hostname: "127.0.0.1",
+      },
+      protocol: null,
+      hostname: "127.0.0.1",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      pattern: {
+        hostname: "[2001:db8::1]",
+      },
+      protocol: null,
+      hostname: "[2001:db8::1]",
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      pattern: {
+        port: "80",
+      },
+      protocol: null,
+      hostname: null,
+      port: "",
+      pathname: "/",
+      search: null,
+    },
+    {
+      pattern: {
+        port: "1234",
+      },
+      protocol: null,
+      hostname: null,
+      port: "1234",
+      pathname: "/",
+      search: null,
+    },
+    {
+      pattern: {
+        pathname: "path/to",
+      },
+      protocol: null,
+      hostname: null,
+      port: "",
+      pathname: "/path/to",
+      search: null,
+    },
+    {
+      pattern: {
+        pathname: "/path/to",
+      },
+      protocol: null,
+      hostname: null,
+      port: "",
+      pathname: "/path/to",
+      search: null,
+    },
+    {
+      pattern: {
+        pathname: "/path/to/",
+      },
+      protocol: null,
+      hostname: null,
+      port: "",
+      pathname: "/path/to/",
+      search: null,
+    },
+    {
+      pattern: {
+        search: "?search",
+      },
+      protocol: null,
+      hostname: null,
+      port: "",
+      pathname: "/",
+      search: "search",
+    },
+    {
+      pattern: {
+        search: "search",
+      },
+      protocol: null,
+      hostname: null,
+      port: "",
+      pathname: "/",
+      search: "search",
+    },
+    {
+      pattern: {
+        search: "?search=something",
+      },
+      protocol: null,
+      hostname: null,
+      port: "",
+      pathname: "/",
+      search: "search=something",
+    },
+    {
+      pattern: {
+        search: "search=something",
+      },
+      protocol: null,
+      hostname: null,
+      port: "",
+      pathname: "/",
+      search: "search=something",
+    },
+  ];
+
+  for (const test of PATTERN_PATTERN_TESTS) {
+    const pattern = parseURLPattern({
+      type: "pattern",
+      ...test.pattern,
+    });
+
+    equal(pattern.protocol, "protocol" in test ? test.protocol : null);
+    equal(pattern.hostname, "hostname" in test ? test.hostname : null);
+    equal(pattern.port, "port" in test ? test.port : null);
+    equal(pattern.pathname, "pathname" in test ? test.pathname : null);
+    equal(pattern.search, "search" in test ? test.search : null);
+  }
+});
+
+add_task(async function test_parseURLPattern_invalid_type() {
+  const values = [null, undefined, 1, [], "string"];
+  for (const value of values) {
+    Assert.throws(() => parseURLPattern(value), /InvalidArgumentError/);
+  }
+});
+
+add_task(async function test_parseURLPattern_invalid_type_type() {
+  const values = [null, undefined, 1, {}, []];
+  for (const type of values) {
+    Assert.throws(() => parseURLPattern({ type }), /InvalidArgumentError/);
+  }
+});
+
+add_task(async function test_parseURLPattern_invalid_type_value() {
+  const values = ["", "unknownType"];
+  for (const type of values) {
+    Assert.throws(() => parseURLPattern({ type }), /InvalidArgumentError/);
+  }
+});
+
+add_task(async function test_parseURLPattern_invalid_stringPatternType() {
+  const values = [null, undefined, 1, {}, []];
+  for (const pattern of values) {
+    Assert.throws(
+      () => parseURLPattern({ type: "string", pattern }),
+      /InvalidArgumentError/
+    );
+  }
+});
+
+add_task(async function test_parseURLPattern_invalid_stringPattern() {
+  const values = [
+    "foo",
+    "*",
+    "(",
+    ")",
+    "{",
+    "}",
+    "http\\{s\\}://example.com",
+    "https://example.com:port/",
+  ];
+  for (const pattern of values) {
+    Assert.throws(
+      () => parseURLPattern({ type: "string", pattern }),
+      /InvalidArgumentError/
+    );
+  }
+});
+
+add_task(async function test_parseURLPattern_invalid_patternPattern_type() {
+  const properties = ["protocol", "hostname", "port", "pathname", "search"];
+  const values = [false, 42, [], {}];
+  for (const property of properties) {
+    for (const value of values) {
+      Assert.throws(
+        () => parseURLPattern({ type: "pattern", [property]: value }),
+        /InvalidArgumentError/
+      );
+    }
+  }
+});
