305c9fc33759b5c97eabbd55108bc65b3b01268d
Rob Lemley <rob@thunderbird.net>
2023-08-17 14:44:06 +0000

    Bug 1848262 - Set appdir from mozharness config when processing coverage files. r=jmaher
    LCovFileRewriter uses "dist/bin/browser/" for the "appdir" parameter when no value
    is passed. That's correct for Firefox, but Thunderbird needs to set appdir to
    "dist/bin/".
    In comm-central, "appdir" will be set in the Thunderbird mozharness unittest
    config file so that appdir will be set appropriately. This allows for other
    products to use code-coverage in the future as well.
    
    Drive-by fix adds a \n when logging removed records for readability.
    
    Differential Revision: https://phabricator.services.mozilla.com/D186459

--- python/mozbuild/mozbuild/codecoverage/lcov_rewriter.py
+++ python/mozbuild/mozbuild/codecoverage/lcov_rewriter.py
@@ -676,7 +676,7 @@ class LcovFileRewriter(object):
                     # encoded with the stdout encoding.
                     sys.stdout.buffer.write(
                         (
-                            "Error: %s.\nCouldn't find source info for %s, removing record"
+                            "Error: %s.\nCouldn't find source info for %s, removing record\n"
                             % (e, url)
                         ).encode(sys.stdout.encoding, errors="replace")
                     )
