980ce099e5d77f670c02ddbbaa10b2610664e20f
Jonathan Kew <jkew@mozilla.com>
2023-08-18 14:30:44 +0000

    Bug 1849332 - Ensure URL::SetPort rejects values with leading non-numeric junk. r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D186536

--- dom/url/URL.cpp
+++ dom/url/URL.cpp
@@ -315,8 +315,13 @@ void URL::SetPort(const nsAString& aPort) {
   int32_t port = -1;
 
   // nsIURI uses -1 as default value.
+  portStr.StripTaggedASCII(ASCIIMask::MaskCRLFTab());
   if (!portStr.IsEmpty()) {
-    portStr.StripTaggedASCII(ASCIIMask::MaskCRLFTab());
+    // To be valid, the port must start with an ASCII digit.
+    // (nsAString::ToInteger ignores leading junk, so check before calling.)
+    if (!IsAsciiDigit(portStr[0])) {
+      return;
+    }
     port = portStr.ToInteger(&rv);
     if (NS_FAILED(rv)) {
       return;
