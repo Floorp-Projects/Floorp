fcf82ca9cf4fa2da0bc9dd703fb49bda0c762499
Norisz Fay <nfay@mozilla.com>
2023-08-18 08:20:02 +0300

    Backed out changeset b790e8b2cdef (bug 1849087) for causing gtest failures on psm_HMAC.Test CLOSED TREE

--- security/manager/ssl/tests/gtest/HMACTest.cpp
+++ security/manager/ssl/tests/gtest/HMACTest.cpp
@@ -13,10 +13,12 @@ const unsigned char kTestInput[] = "what do ya want for nothing?";
 
 struct HMACTestCase {
   SECOidTag hashAlg;
-  std::string expectedOutput;
+  std::basic_string<uint8_t> expectedOutput;
 };
 
-#define EXPECTED_RESULT(val) std::string(val, sizeof(val) - 1)
+#define EXPECTED_RESULT(val)                                        \
+  std::basic_string<uint8_t>(reinterpret_cast<const uint8_t*>(val), \
+                             sizeof(val) - 1)
 
 static const HMACTestCase HMACTestCases[] = {
     {
