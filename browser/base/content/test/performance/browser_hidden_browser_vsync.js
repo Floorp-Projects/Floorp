91faba9e00f9761f65eeb6af18ab9fa2b06d5039
Emilio Cobos Álvarez <emilio@crisal.io>
2023-08-18 16:17:31 +0000

    Bug 1847584 - Account for IsUnderHiddenEmbedderElement() for BrowserChild visibility. r=smaug
    Test by Gijs. Note that I removed the .isActive assertion, because this doesn't
    affect activeness.
    
    We could make that change too, but that's a bigger change, and I think we want
    to do this regardless.
    
    The CAN_RUN_SCRIPT annotations are obsolete (they come from bug 1582042
    which directly fired docshell activity events, but that's not true
    anymore, bug 1635914 changed the setup a bit later).
    
    Differential Revision: https://phabricator.services.mozilla.com/D185643

---
+++ browser/base/content/test/performance/browser_hidden_browser_vsync.js
@@ -0,0 +1,56 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+add_task(async function test_hidden_sidebar() {
+  let b = document.createXULElement("browser");
+  for (let [k, v] of Object.entries({
+    type: "content",
+    disablefullscreen: "true",
+    disablehistory: "true",
+    flex: "1",
+    style: "min-width: 300px",
+    message: "true",
+    remote: "true",
+    maychangeremoteness: "true",
+  })) {
+    b.setAttribute(k, v);
+  }
+  let mainBrowser = gBrowser.selectedBrowser;
+  let panel = gBrowser.getPanel(mainBrowser);
+  panel.append(b);
+  let loaded = BrowserTestUtils.browserLoaded(b);
+  BrowserTestUtils.loadURIString(
+    b,
+    `data:text/html,<!doctype html><style>
+  @keyframes fade-in {
+   from {
+     opacity: .25;
+   }
+   to {
+     opacity: 1;
+   }
+  </style>
+  <div style="
+    animation-name: fade-in;
+    animation-direction: alternate;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+    background-color: red;
+    height: 500px;
+    width: 100%;
+  "></div>`
+  );
+  await loaded;
+  ok(b, "Browser was created.");
+  await SpecialPowers.spawn(b, [], async () => {
+    await new Promise(r =>
+      content.requestAnimationFrame(() => content.requestAnimationFrame(r))
+    );
+  });
+  b.hidden = true;
+  ok(b.hidden, "Browser should be hidden.");
+  // Now the framework will test to see vsync goes away
+});
