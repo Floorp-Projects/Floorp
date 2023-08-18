2ba749702eab0265726a1945988d4c0002bf1250
Julian Descottes <jdescottes@mozilla.com>
2023-08-18 08:18:31 +0000

    Bug 1848950 - [devtools] Wait for source URL in debugger test helper waitForSelectedSource r=ochameau,devtools-reviewers
    Depends on D186153
    
    Differential Revision: https://phabricator.services.mozilla.com/D186335

--- devtools/client/debugger/test/mochitest/browser_dbg-inspector-integration.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-inspector-integration.js
@@ -115,7 +115,6 @@ add_task(async function () {
 
   findElement(dbg, "frame", 2).focus();
   clickElement(dbg, "frame", 2);
-  await waitForPaused(dbg);
   await waitForSelectedSource(dbg, "doc-event-handler.html");
 
   // Hover over the token to launch preview popup
