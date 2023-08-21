5dc7c25a6ef8128f7e3e4d386fadfba5e88f120b
André Bargull <andre.bargull@gmail.com>
2023-08-21 08:32:10 +0000

    Bug 1843824: Handle dead wrappers. r=iain a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D183714

--- js/src/builtin/Promise.cpp
+++ js/src/builtin/Promise.cpp
@@ -568,7 +568,11 @@ JSObject* PromiseObject::allocationSite() {
 JSObject* PromiseObject::resolutionSite() {
   auto debugInfo = PromiseDebugInfo::FromPromise(this);
   if (debugInfo) {
-    return debugInfo->resolutionSite();
+    JSObject* site = debugInfo->resolutionSite();
+    if (site && !JS_IsDeadWrapper(site)) {
+      MOZ_ASSERT(UncheckedUnwrap(site)->is<SavedFrame>());
+      return site;
+    }
   }
   return nullptr;
 }
