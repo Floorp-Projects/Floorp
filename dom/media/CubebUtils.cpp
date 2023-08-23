34c7e463bc30ffbe9933fb65a2aae3c11977ce36
Karl Tomlinson <karlt+@karlt.net>
2023-08-23 05:43:21 +0000

    Bug 1846191 delete previous cubeb context when ForceSetCubebContext() r=padenot
    This resolves leaks when GTests create multiple MockCubebs.
    
    It also checks that a previous MockCubeb does not still have streams running,
    as asserted in MockCubeb::Destroy().
    
    Differential Revision: https://phabricator.services.mozilla.com/D185028

--- dom/media/CubebUtils.cpp
+++ dom/media/CubebUtils.cpp
@@ -301,6 +301,9 @@ cubeb* GetCubebContext() {
 // This is only exported when running tests.
 void ForceSetCubebContext(cubeb* aCubebContext) {
   StaticMutexAutoLock lock(sMutex);
+  if (sCubebContext) {
+    cubeb_destroy(sCubebContext);
+  }
   sCubebContext = aCubebContext;
   sCubebState = CubebState::Initialized;
 }
