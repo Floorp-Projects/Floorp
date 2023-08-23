b3823bc463f1325984db683078279251e5ff5576
Greg Tatum <tatum.creative@gmail.com>
2023-08-23 15:04:36 +0000

    Bug 1849756 - Disable translations on Fx117 for the Nimbus holdout. r=nordzilla, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D186660

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3642,7 +3642,7 @@ pref("browser.translations.useHTML", false);
 // required.
 pref("browser.translations.autoTranslate", false);
 // Automatically popup an offer to translate on sites.
-pref("browser.translations.automaticallyPopup", true);
+pref("browser.translations.automaticallyPopup", false);
 // Simulate the behavior of using a device that does not support the translations engine.
 // Requires restart.
 pref("browser.translations.simulateUnsupportedEngine", false);
