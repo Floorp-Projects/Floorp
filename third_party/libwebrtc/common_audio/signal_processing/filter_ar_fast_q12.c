6fde35c3b852211a36d21dc4977ada2137fb7f5e
Michael Froman <mfroman@mozilla.com>
2023-08-23 17:45:11 +0000

    Bug 1849646 - remove unnecessary libwebrtc change in filter_ar_fast_q12.c r=ng,webrtc-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D186635

--- third_party/libwebrtc/common_audio/signal_processing/filter_ar_fast_q12.c
+++ third_party/libwebrtc/common_audio/signal_processing/filter_ar_fast_q12.c
@@ -8,7 +8,7 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include <stddef.h>
+#include "stddef.h"
 
 #include "rtc_base/checks.h"
 #include "common_audio/signal_processing/include/signal_processing_library.h"
