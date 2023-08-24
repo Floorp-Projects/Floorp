e0e78f3fd6a57c64e0abe2c8ab00708aafe3b953
Daniel Holbert <dholbert@cs.stanford.edu>
2023-08-24 13:08:00 +0000

    Bug 1849898: Null-check the refresh driver's mPresContext before using it to reevaluate media queries. r=emilio
    We use mPresContext in Tick()'s call to EvaluateMediaQueriesAndReportChanges(),
    but it's apparently possible for this pointer to have been nulled out as part
    of handling events, earlier in Tick(). So, the code in
    EvaluateMediaQueriesAndReportChanges() needs to check this pointer before using
    it.
    
    Note that Tick() itself has a null-check for this same mPresContext pointer,
    shortly after its call to EvaluateMediaQueriesAndReportChanges(). So, it's
    reasonable to be suspicious that it could be null a little earlier, within
    EvaluateMediaQueriesAndReportChanges(), as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D186744

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -1597,6 +1597,9 @@ void nsRefreshDriver::EvaluateMediaQueriesAndReportChanges() {
     return;
   }
   mMightNeedMediaQueryListenerUpdate = false;
+  if (!mPresContext) {
+    return;
+  }
   AUTO_PROFILER_LABEL_RELEVANT_FOR_JS(
       "Evaluate media queries and report changes", LAYOUT);
   RefPtr<Document> doc = mPresContext->Document();
