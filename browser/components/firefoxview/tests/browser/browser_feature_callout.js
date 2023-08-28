8c85348a92e7cf060ee93a91d9f7c8e46a443824
Cristian Tuns <ctuns@mozilla.com>
2023-08-28 17:53:19 -0400

    Backed out changeset c35a3bb94b22 (bug 1845356) for causing bc failures in browser_feature_callout_position.js CLOSED TREE

--- browser/components/firefoxview/tests/browser/browser_feature_callout.js
+++ browser/components/firefoxview/tests/browser/browser_feature_callout.js
@@ -219,7 +219,7 @@ add_task(async function feature_callout_arrow_is_not_flipped_on_ltr() {
   const testMessage = getCalloutMessageById("FIREFOX_VIEW_FEATURE_TOUR");
   testMessage.message.content.screens[0].anchors[0].arrow_position = "start";
   testMessage.message.content.screens[0].anchors[0].selector =
-    "span.brand-feature-name";
+    "span.brand-icon";
   const sandbox = createSandboxWithCalloutTriggerStub(testMessage);
   await BrowserTestUtils.withNewTab(
     {
--- browser/locales/en-US/browser/firefoxView.ftl
+++ browser/locales/en-US/browser/firefoxView.ftl
@@ -2,9 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-toolbar-button-firefox-view-2 =
+toolbar-button-firefox-view =
   .label = { -firefoxview-brand-name }
-  .tooltiptext = View recent browsing across windows and devices
+  .tooltiptext = { -firefoxview-brand-name }
 
 menu-tools-firefox-view =
   .label = { -firefoxview-brand-name }
