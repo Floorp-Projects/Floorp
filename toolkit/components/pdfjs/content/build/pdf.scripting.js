dbe689133c9c2a077a513f3edcdbb4b8fa3a31ae
Updatebot <updatebot@mozilla.com>
2023-08-28 16:16:54 +0000

    Bug 1850112 - Update PDF.js to 598421b11f067dacfb2cf81897c19b10db3c9988 r=pdfjs-reviewers,calixte
    Differential Revision: https://phabricator.services.mozilla.com/D186838

--- toolkit/components/pdfjs/content/build/pdf.scripting.js
+++ toolkit/components/pdfjs/content/build/pdf.scripting.js
@@ -3417,7 +3417,7 @@ class Doc extends _pdf_object.PDFObject {
   removeWeblinks() {}
   replacePages() {}
   resetForm(aFields = null) {
-    if (aFields && typeof aFields === "object") {
+    if (aFields && typeof aFields === "object" && !Array.isArray(aFields)) {
       aFields = aFields.aFields;
     }
     if (aFields && !Array.isArray(aFields)) {
@@ -4250,8 +4250,8 @@ Object.defineProperty(exports, "initSandbox", ({
   }
 }));
 var _initialization = __w_pdfjs_require__(1);
-const pdfjsVersion = '3.10.86';
-const pdfjsBuild = 'c72cb5436';
+const pdfjsVersion = '3.10.109';
+const pdfjsBuild = '598421b11';
 })();
 
 /******/ 	return __webpack_exports__;
