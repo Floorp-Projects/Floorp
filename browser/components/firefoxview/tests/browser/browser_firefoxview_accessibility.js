ac182b4208319410d580a6cf6bd983c620de5b0c
Sarah Clements <sclements@mozilla.com>
2023-08-29 15:21:41 +0000

    Bug 1847981 - Flip pref to make firefoxview-next the default r=fxview-reviewers,sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D186849

--- browser/components/firefoxview/tests/browser/browser_firefoxview_accessibility.js
+++ browser/components/firefoxview/tests/browser/browser_firefoxview_accessibility.js
@@ -14,7 +14,13 @@ add_setup(async function setup() {
   // Without resetting the view-count pref it gets surfaced after
   // the third click on the fx view toolbar button.
   await SpecialPowers.pushPrefEnv({
-    set: [["browser.firefox-view.view-count", 0]],
+    set: [
+      ["browser.firefox-view.view-count", 0],
+      ["browser.tabs.firefox-view-next", false],
+    ],
+  });
+  registerCleanupFunction(async () => {
+    await SpecialPowers.popPrefEnv();
   });
 });
 
@@ -72,7 +78,6 @@ add_task(async function test_keyboard_focus_after_tab_pickup_opened() {
 
   // cleanup time
   await tearDown(sandbox);
-  await SpecialPowers.popPrefEnv();
 });
 
 add_task(async function test_keyboard_accessibility_tab_pickup() {
