6f1b14a56589c2548fe979358a12b9d16318cf5c
Emilio Cobos Álvarez <emilio@crisal.io>
2023-08-29 00:16:54 +0000

    Bug 1850293 - Make <meter> styles not intrinsic. r=edgar
    This one needs extra code because we have an UpdateState call after
    AfterSetAttr which took care of it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D186895

--- dom/base/rust/lib.rs
+++ dom/base/rust/lib.rs
@@ -81,6 +81,8 @@ bitflags! {
         const SUB_OPTIMUM = 1 << 29;
         /// Non-standard & undocumented.
         const SUB_SUB_OPTIMUM = 1 << 30;
+        /// All the above <meter> bits in one place.
+        const METER_OPTIMUM_STATES = Self::OPTIMUM.bits | Self::SUB_OPTIMUM.bits | Self::SUB_SUB_OPTIMUM.bits;
         /// Non-standard & undocumented.
         const INCREMENT_SCRIPT_LEVEL = 1u64 << 31;
         /// <https://drafts.csswg.org/selectors-4/#the-focus-visible-pseudo>
@@ -160,6 +162,7 @@ bitflags! {
             Self::DIR_STATES.bits |
             Self::DISABLED_STATES.bits |
             Self::REQUIRED_STATES.bits |
+            Self::METER_OPTIMUM_STATES.bits |
             Self::ACTIVE.bits |
             Self::DEFINED.bits |
             Self::DRAGOVER.bits |
