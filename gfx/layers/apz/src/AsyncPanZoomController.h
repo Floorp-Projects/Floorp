99ef30eb965e978cacf8e3513f02b4f02dc39847
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2023-08-29 08:52:05 +0000

    Bug 1649440 - Defer the calculation of ZoomToFocusInput's target rect to APZ. r=botond
    Depends on D185246
    
    Differential Revision: https://phabricator.services.mozilla.com/D184440

--- gfx/layers/apz/src/AsyncPanZoomController.h
+++ gfx/layers/apz/src/AsyncPanZoomController.h
@@ -1274,6 +1274,11 @@ class AsyncPanZoomController {
     return mScrollMetadata.GetMetrics().GetScrollableRect();
   }
 
+  CSSToParentLayerScale GetZoom() const {
+    RecursiveMutexAutoLock lock(mRecursiveMutex);
+    return Metrics().GetZoom();
+  }
+
   // Returns the delta for the given InputData.
   ParentLayerPoint GetDeltaForEvent(const InputData& aEvent) const;
 
