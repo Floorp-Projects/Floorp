a66e1333a6d63b28c882ea67abba9f9a9afdf9bd
Yury Delendik <ydelendik@mozilla.com>
2023-08-31 19:32:45 +0000

    Bug 1846762 - Import return_call_ref spec test. r=bvisness
    Differential Revision: https://phabricator.services.mozilla.com/D185722

--- js/src/jit-test/tests/wasm/spec/function-references/call_ref.wast.js
+++ js/src/jit-test/tests/wasm/spec/function-references/call_ref.wast.js
@@ -39,6 +39,74 @@ let $0 = instantiate(`(module
   (func (export "null") (result i32)
     (call_ref $$ii (i32.const 1) (ref.null $$ii))
   )
+
+  ;; Recursion
+
+  (type $$ll (func (param i64) (result i64)))
+  (type $$lll (func (param i64 i64) (result i64)))
+
+  (elem declare func $$fac)
+  (global $$fac (ref $$ll) (ref.func $$fac))
+
+  (func $$fac (export "fac") (type $$ll)
+    (if (result i64) (i64.eqz (local.get 0))
+      (then (i64.const 1))
+      (else
+        (i64.mul
+          (local.get 0)
+          (call_ref $$ll (i64.sub (local.get 0) (i64.const 1)) (global.get $$fac))
+        )
+      )
+    )
+  )
+
+  (elem declare func $$fac-acc)
+  (global $$fac-acc (ref $$lll) (ref.func $$fac-acc))
+
+  (func $$fac-acc (export "fac-acc") (type $$lll)
+    (if (result i64) (i64.eqz (local.get 0))
+      (then (local.get 1))
+      (else
+        (call_ref $$lll
+          (i64.sub (local.get 0) (i64.const 1))
+          (i64.mul (local.get 0) (local.get 1))
+          (global.get $$fac-acc)
+        )
+      )
+    )
+  )
+
+  (elem declare func $$fib)
+  (global $$fib (ref $$ll) (ref.func $$fib))
+
+  (func $$fib (export "fib") (type $$ll)
+    (if (result i64) (i64.le_u (local.get 0) (i64.const 1))
+      (then (i64.const 1))
+      (else
+        (i64.add
+          (call_ref $$ll (i64.sub (local.get 0) (i64.const 2)) (global.get $$fib))
+          (call_ref $$ll (i64.sub (local.get 0) (i64.const 1)) (global.get $$fib))
+        )
+      )
+    )
+  )
+
+  (elem declare func $$even $$odd)
+  (global $$even (ref $$ll) (ref.func $$even))
+  (global $$odd (ref $$ll) (ref.func $$odd))
+
+  (func $$even (export "even") (type $$ll)
+    (if (result i64) (i64.eqz (local.get 0))
+      (then (i64.const 44))
+      (else (call_ref $$ll (i64.sub (local.get 0) (i64.const 1)) (global.get $$odd)))
+    )
+  )
+  (func $$odd (export "odd") (type $$ll)
+    (if (result i64) (i64.eqz (local.get 0))
+      (then (i64.const 99))
+      (else (call_ref $$ll (i64.sub (local.get 0) (i64.const 1)) (global.get $$even)))
+    )
+  )
 )`);
 
 // ./test/core/call_ref.wast:94
@@ -49,6 +117,69 @@ assert_return(() => invoke($0, `run`, [3]), [value("i32", -9)]);
 
 // ./test/core/call_ref.wast:97
 assert_trap(() => invoke($0, `null`, []), `null function`);
+
+// ./test/core/call_ref.wast:99
+assert_return(() => invoke($0, `fac`, [0n]), [value("i64", 1n)]);
+
+// ./test/core/call_ref.wast:100
+assert_return(() => invoke($0, `fac`, [1n]), [value("i64", 1n)]);
+
+// ./test/core/call_ref.wast:101
+assert_return(() => invoke($0, `fac`, [5n]), [value("i64", 120n)]);
+
+// ./test/core/call_ref.wast:102
+assert_return(() => invoke($0, `fac`, [25n]), [value("i64", 7034535277573963776n)]);
+
+// ./test/core/call_ref.wast:103
+assert_return(() => invoke($0, `fac-acc`, [0n, 1n]), [value("i64", 1n)]);
+
+// ./test/core/call_ref.wast:104
+assert_return(() => invoke($0, `fac-acc`, [1n, 1n]), [value("i64", 1n)]);
+
+// ./test/core/call_ref.wast:105
+assert_return(() => invoke($0, `fac-acc`, [5n, 1n]), [value("i64", 120n)]);
+
+// ./test/core/call_ref.wast:106
+assert_return(() => invoke($0, `fac-acc`, [25n, 1n]), [value("i64", 7034535277573963776n)]);
+
+// ./test/core/call_ref.wast:111
+assert_return(() => invoke($0, `fib`, [0n]), [value("i64", 1n)]);
+
+// ./test/core/call_ref.wast:112
+assert_return(() => invoke($0, `fib`, [1n]), [value("i64", 1n)]);
+
+// ./test/core/call_ref.wast:113
+assert_return(() => invoke($0, `fib`, [2n]), [value("i64", 2n)]);
+
+// ./test/core/call_ref.wast:114
+assert_return(() => invoke($0, `fib`, [5n]), [value("i64", 8n)]);
+
+// ./test/core/call_ref.wast:115
+assert_return(() => invoke($0, `fib`, [20n]), [value("i64", 10946n)]);
+
+// ./test/core/call_ref.wast:117
+assert_return(() => invoke($0, `even`, [0n]), [value("i64", 44n)]);
+
+// ./test/core/call_ref.wast:118
+assert_return(() => invoke($0, `even`, [1n]), [value("i64", 99n)]);
+
+// ./test/core/call_ref.wast:119
+assert_return(() => invoke($0, `even`, [100n]), [value("i64", 44n)]);
+
+// ./test/core/call_ref.wast:120
+assert_return(() => invoke($0, `even`, [77n]), [value("i64", 99n)]);
+
+// ./test/core/call_ref.wast:121
+assert_return(() => invoke($0, `odd`, [0n]), [value("i64", 99n)]);
+
+// ./test/core/call_ref.wast:122
+assert_return(() => invoke($0, `odd`, [1n]), [value("i64", 44n)]);
+
+// ./test/core/call_ref.wast:123
+assert_return(() => invoke($0, `odd`, [200n]), [value("i64", 99n)]);
+
+// ./test/core/call_ref.wast:124
+assert_return(() => invoke($0, `odd`, [77n]), [value("i64", 44n)]);
 
 // ./test/core/call_ref.wast:129
 let $1 = instantiate(`(module
