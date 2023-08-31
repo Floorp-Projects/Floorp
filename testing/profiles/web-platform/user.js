be6f7f67d83fa43b78b37928e9e12bec7f574330
Henrik Skupin <mail@hskupin.info>
2023-08-31 12:41:53 +0000

    Bug 1821981 - [wpt] Do not download intermediate certificates for wpt tests. r=keeler,webdriver-reviewers,Sasha a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D186968

--- testing/profiles/web-platform/user.js
+++ testing/profiles/web-platform/user.js
@@ -83,5 +83,7 @@ user_pref("remote.experimental.enabled", true);
 user_pref("privacy.partition.always_partition_third_party_non_cookie_storage", false);
 // Disable OCSP checks in WPT (webtransport triggers these occasionally)
 user_pref("security.OCSP.enabled", 0);
+// Disable download of intermediate certificates.
+user_pref("security.remote_settings.intermediates.enabled", false);
 // Disable prefers-reduced-motion to ensure that smooth scrolls can be tested.
 user_pref("general.smoothScroll", true);
