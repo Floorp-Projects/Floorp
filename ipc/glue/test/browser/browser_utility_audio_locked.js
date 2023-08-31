296d36c81ee16f87107fe118dfeea8a12a077dcb
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2023-08-31 06:55:22 +0000

    Bug 1845666 - Lock the media.utility-* prefs r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D184734

---
+++ ipc/glue/test/browser/browser_utility_audio_locked.js
@@ -0,0 +1,24 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* import-globals-from head-multiple.js */
+
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/ipc/glue/test/browser/head-multiple.js",
+  this
+);
+
+add_setup(async function setup() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["media.utility-process.enabled", false]],
+  });
+});
+
+add_task(async function testAudioDecodingInUtility() {
+  // TODO: When getting rid of audio decoding on non utility at all, this
+  // should be removed
+  const verifyForUtility = !!isNightly();
+  await runTest({ expectUtility: verifyForUtility });
+});
