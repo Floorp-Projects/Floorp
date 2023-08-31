5143bd493697a99508e3bbf88f04da3c4231c2f4
Iulian Moraru <imoraru@mozilla.com>
2023-09-01 02:16:43 +0300

    Backed out 9 changesets (bug 1838754, bug 1778754) for causing multiple build bustages. CLOSED TREE
    Backed out changeset d412fdc431d3 (bug 1778754)
    Backed out changeset b07c43c2d0e3 (bug 1838754)
    Backed out changeset 744552ffb916 (bug 1838754)
    Backed out changeset 19a063fe4fc3 (bug 1838754)
    Backed out changeset 85eb99b90c26 (bug 1838754)
    Backed out changeset 626918ba3444 (bug 1838754)
    Backed out changeset c4064dbb85a6 (bug 1838754)
    Backed out changeset af624d29c748 (bug 1838754)
    Backed out changeset 5a50ccbb53b0 (bug 1838754)

--- toolkit/mozapps/defaultagent/DefaultBrowser.h
+++ toolkit/mozapps/defaultagent/DefaultBrowser.h
@@ -12,10 +12,9 @@
 #include "mozilla/DefineEnum.h"
 #include "mozilla/WinHeaderOnlyUtils.h"
 
-MOZ_DEFINE_ENUM_CLASS(Browser,
-                      (Error, Unknown, Firefox, Chrome, EdgeWithEdgeHTML,
-                       EdgeWithBlink, InternetExplorer, Opera, Brave, Yandex,
-                       QQBrowser, _360Browser, Sogou));
+MOZ_DEFINE_ENUM_CLASS(Browser, (Unknown, Firefox, Chrome, EdgeWithEdgeHTML,
+                                EdgeWithBlink, InternetExplorer, Opera, Brave,
+                                Yandex, QQBrowser, _360Browser, Sogou));
 
 struct DefaultBrowserInfo {
   Browser currentDefaultBrowser;
