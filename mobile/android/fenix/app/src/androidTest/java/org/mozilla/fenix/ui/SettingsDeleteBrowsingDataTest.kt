fdedc4ccab6f0a85aa28d6076b392b8a53b771c1
AndiAJ <andiaj@users.noreply.github.com>
2023-09-01 11:52:20 +0300

    Bug 1807268 - Disable failing deleteCachedFilesTest UI test

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/SettingsDeleteBrowsingDataTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/SettingsDeleteBrowsingDataTest.kt
@@ -7,6 +7,7 @@ package org.mozilla.fenix.ui
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.R
@@ -226,6 +227,7 @@ class SettingsDeleteBrowsingDataTest {
         }
     }
 
+    @Ignore("Failing, see: https://bugzilla.mozilla.org/show_bug.cgi?id=1807268")
     @SmokeTest
     @Test
     fun deleteCachedFilesTest() {
