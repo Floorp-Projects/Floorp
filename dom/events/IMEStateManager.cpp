974e7400e8c9dab91defdf116b8bbf5c904d534e
Mathew Hodson <mathew.hodson@gmail.com>
2023-09-04 23:26:22 +0000

    Bug 1735980 - Remove mozactionhint attribute. r=m_kato,dom-core,smaug,masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D184278

--- dom/events/IMEStateManager.cpp
+++ dom/events/IMEStateManager.cpp
@@ -1581,15 +1581,7 @@ MOZ_CAN_RUN_SCRIPT static void GetActionHint(const IMEState& aState,
     }
   }
 
-  if (!aContent.IsAnyOfHTMLElements(nsGkAtoms::input, nsGkAtoms::textarea)) {
-    return;
-  }
-
-  // XXX This is old compatibility, but we might be able to remove this.
-  aContent.AsElement()->GetAttr(nsGkAtoms::moz_action_hint, aActionHint);
-
-  if (!aActionHint.IsEmpty()) {
-    ToLowerCase(aActionHint);
+  if (!aContent.IsHTMLElement(nsGkAtoms::input)) {
     return;
   }
 
@@ -1638,6 +1630,7 @@ MOZ_CAN_RUN_SCRIPT static void GetActionHint(const IMEState& aState,
   }
 
   if (!willSubmit) {
+    aActionHint.Truncate();
     return;
   }
 
