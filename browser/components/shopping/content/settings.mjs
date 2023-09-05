041d7e97817dcaaf0d85982a10d112a3790acd51
Stanca Serban <sstanca@mozilla.com>
2023-09-05 19:54:32 +0300

    Backed out changeset 4ced56d93af6 (bug 1849159) for causing mochitests failures in browser_stale_product.js. CLOSED TREE

--- browser/components/shopping/content/settings.mjs
+++ browser/components/shopping/content/settings.mjs
@@ -21,7 +21,6 @@ class ShoppingSettings extends MozLitElement {
     return {
       recommendationsToggleEl: "#shopping-settings-recommendations-toggle",
       optOutButtonEl: "#shopping-settings-opt-out-button",
-      shoppingCardEl: "shopping-card",
     };
   }
 
@@ -35,7 +34,6 @@ class ShoppingSettings extends MozLitElement {
 
   onDisableShopping() {
     RPMSetPref("browser.shopping.experience2023.optedIn", 2);
-    RPMSetPref("browser.shopping.experience2023.active", false);
   }
 
   render() {
