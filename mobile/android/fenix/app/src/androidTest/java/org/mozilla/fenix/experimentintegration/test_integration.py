ff3dc19b4c0992e34c427f63503b68c04f6e52f9
Benjamin Forehand <bennyjr169@gmail.com>
2023-09-05 18:47:24 -0500

    Bug 1850761 - Allow for local usage of experiment integration test suite

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/experimentintegration/test_integration.py
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/experimentintegration/test_integration.py
@@ -1,3 +1,7 @@
-def test_survey_navigates_correctly(setup_experiment, gradlewbuild):
-    setup_experiment()
+import pytest
+
+
+@pytest.mark.parametrize("load_branches", [("branch")], indirect=True)
+def test_survey_navigates_correctly(setup_experiment, gradlewbuild, load_branches):
+    setup_experiment(load_branches)
     gradlewbuild.test("SurveyExperimentIntegrationTest#checkSurveyNavigatesCorrectly")
