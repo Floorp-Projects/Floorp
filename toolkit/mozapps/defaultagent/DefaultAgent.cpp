b2863a5e42feea0671c7ffa2284d9733ef05ce20
Nicholas Rishel <nrishel@mozilla.com>
2023-09-05 20:55:26 +0000

    Bug 1838754 - Part 4: Ensure default agent notifications run on an MTA thread. r=nalexander
    This is necessary as notification callbacks can not occur on an STA thread when the event loop is not running. We currently block while waiting for notification callbacks, therefore the event loop can not run.
    
    Depends on D187068
    
    Differential Revision: https://phabricator.services.mozilla.com/D187238

--- toolkit/mozapps/defaultagent/DefaultAgent.cpp
+++ toolkit/mozapps/defaultagent/DefaultAgent.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <vector>
 
+#include "mozilla/mscom/EnsureMTA.h"
 #include "nsAutoRef.h"
 #include "nsDebug.h"
 #include "nsWindowsHelpers.h"
@@ -323,8 +324,14 @@ DefaultAgent::DoTask(const nsAString& aUniqueToken, const bool aForce) {
   }
   DefaultPdfInfo pdfInfo = defaultPdfResult.unwrap();
 
-  NotificationActivities activitiesPerformed = MaybeShowNotification(
-      browserInfo, PromiseFlatString(aUniqueToken).get(), aForce);
+  NotificationActivities activitiesPerformed;
+  // We block while waiting for the notification which prevents STA thread
+  // callbacks from running as the event loop won't run. Moving notification
+  // handling to an MTA thread prevents this conflict.
+  mozilla::mscom::EnsureMTA([&] {
+    activitiesPerformed = MaybeShowNotification(
+        browserInfo, PromiseFlatString(aUniqueToken).get(), aForce);
+  });
 
   HRESULT hr =
       SendDefaultBrowserPing(browserInfo, pdfInfo, activitiesPerformed);
