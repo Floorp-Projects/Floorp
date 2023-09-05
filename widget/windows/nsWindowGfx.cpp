932bfd40a1978c329bdc5121c926d8b5954cda2c
Emilio Cobos Álvarez <emilio@crisal.io>
2023-09-05 14:31:51 +0000

    Bug 1851585 - Minor clean-ups in nsWindow::OnPaint. r=win-reviewers,gstoll
    Differential Revision: https://phabricator.services.mozilla.com/D187444

--- widget/windows/nsWindowGfx.cpp
+++ widget/windows/nsWindowGfx.cpp
@@ -214,8 +214,8 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
   }
   mLastPaintBounds = mBounds;
 
-  if (!aDC && (renderer->GetBackendType() == LayersBackend::LAYERS_NONE) &&
-      (TransparencyMode::Transparent == mTransparencyMode)) {
+  if (!aDC && renderer->GetBackendType() == LayersBackend::LAYERS_NONE &&
+      TransparencyMode::Transparent == mTransparencyMode) {
     // For layered translucent windows all drawing should go to memory DC and no
     // WM_PAINT messages are normally generated. To support asynchronous
     // painting we force generation of WM_PAINT messages by invalidating window
@@ -231,10 +231,9 @@ bool nsWindow::OnPaint(HDC aDC, uint32_t aNestingLevel) {
     aDC = mBasicLayersSurface->GetTransparentDC();
   }
 
-  HDC hDC = aDC ? aDC : (::BeginPaint(mWnd, &ps));
+  HDC hDC = aDC ? aDC : ::BeginPaint(mWnd, &ps);
 
-  bool forceRepaint =
-      aDC || (TransparencyMode::Transparent == mTransparencyMode);
+  bool forceRepaint = aDC || TransparencyMode::Transparent == mTransparencyMode;
   LayoutDeviceIntRegion region = GetRegionToPaint(forceRepaint, ps, hDC);
 
   if (knowsCompositor && layerManager) {
