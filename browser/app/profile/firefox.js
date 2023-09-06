fd75e3a4178d94da5b098f7fd6b61bbb276ff8f0
Mike Kaply <mozilla@kaply.com>
2023-09-06 13:41:27 +0000

    Bug 1851525 - Allow Firefox View icon to be set via Nimbus. r=sclements,desktop-theme-reviewers,fxview-reviewers,dao,sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D187458

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -883,6 +883,7 @@ pref("browser.tabs.tooltipsShowPidAndActiveness", false);
 
 pref("browser.tabs.firefox-view", true);
 pref("browser.tabs.firefox-view-next", true);
+pref("browser.tabs.firefox-view-newIcon", true);
 pref("browser.tabs.firefox-view.logLevel", "Warn");
 pref("browser.tabs.firefox-view.notify-for-tabs", false);
 
