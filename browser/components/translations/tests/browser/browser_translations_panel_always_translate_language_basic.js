4ec19377fc02b5d27cfe6e4338f4676fe5847a1e
Erik Nordin <enordin@mozilla.com>
2023-09-06 16:31:01 +0000

    Bug 1850303 - Split up largest Translations tests r=gregtatum
    This just splits out the two largest Translations tests
    into smaller files to prevent timeouts in CI.
    
    Depends on D187085
    
    Differential Revision: https://phabricator.services.mozilla.com/D187490

---
+++ browser/components/translations/tests/browser/browser_translations_panel_always_translate_language_basic.js
@@ -0,0 +1,68 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests the effect of toggling the always-translate-language menuitem.
+ * Checking the box on an untranslated page should immediately translate the page.
+ * Unchecking the box on a translated page should immediately restore the page.
+ */
+add_task(async function test_toggle_always_translate_language_menuitem() {
+  const { cleanup, resolveDownloads, runInPage } = await loadTestPage({
+    page: SPANISH_PAGE_URL,
+    languagePairs: LANGUAGE_PAIRS,
+    prefs: [["browser.translations.alwaysTranslateLanguages", "pl,fr"]],
+  });
+
+  await assertTranslationsButton(
+    { button: true, circleArrows: false, locale: false, icon: true },
+    "The translations button is visible."
+  );
+
+  await assertPageIsUntranslated(runInPage);
+
+  await openTranslationsPanel({ onOpenPanel: assertPanelDefaultView });
+  await openTranslationsSettingsMenu();
+
+  await assertIsAlwaysTranslateLanguage("es", { checked: false });
+  await clickAlwaysTranslateLanguage({
+    downloadHandler: resolveDownloads,
+  });
+  await assertIsAlwaysTranslateLanguage("es", { checked: true });
+
+  await assertPageIsTranslated(
+    "es",
+    "en",
+    runInPage,
+    "The page should be automatically translated."
+  );
+
+  await navigate("Navigate to a different Spanish page", {
+    url: SPANISH_PAGE_URL_DOT_ORG,
+    downloadHandler: resolveDownloads,
+  });
+
+  await assertPageIsTranslated(
+    "es",
+    "en",
+    runInPage,
+    "The page should be automatically translated."
+  );
+
+  await openTranslationsPanel({ onOpenPanel: assertPanelRevisitView });
+  await openTranslationsSettingsMenu();
+
+  await assertIsAlwaysTranslateLanguage("es", { checked: true });
+  await clickAlwaysTranslateLanguage();
+  await assertIsAlwaysTranslateLanguage("es", { checked: false });
+
+  await assertTranslationsButton(
+    { button: true, circleArrows: false, locale: false, icon: true },
+    "Only the button appears"
+  );
+
+  await assertPageIsUntranslated(runInPage);
+
+  await cleanup();
+});
---
+++ browser/components/translations/tests/browser/browser_translations_panel_never_translate_site_basic.js
@@ -0,0 +1,55 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests the effect of toggling the never-translate-site menuitem.
+ * Checking the box on an untranslated page should immediately hide the button.
+ * The button should not appear again for sites that share the same content principal
+ * of the disabled site.
+ */
+add_task(async function test_toggle_never_translate_site_menuitem() {
+  const { cleanup, runInPage } = await loadTestPage({
+    page: SPANISH_PAGE_URL,
+    languagePairs: LANGUAGE_PAIRS,
+    permissionsUrls: [SPANISH_PAGE_URL],
+  });
+
+  await assertTranslationsButton(
+    { button: true, circleArrows: false, locale: false, icon: true },
+    "The translations button is visible."
+  );
+
+  await assertPageIsUntranslated(runInPage);
+
+  await openTranslationsPanel({ onOpenPanel: assertPanelDefaultView });
+  await openTranslationsSettingsMenu();
+
+  await assertIsNeverTranslateSite(SPANISH_PAGE_URL, { checked: false });
+  await clickNeverTranslateSite();
+  await assertIsNeverTranslateSite(SPANISH_PAGE_URL, { checked: true });
+
+  await assertPageIsUntranslated(runInPage);
+
+  await navigate("Navigate to a Spanish page with the same content principal", {
+    url: SPANISH_PAGE_URL_2,
+  });
+
+  await assertPageIsUntranslated(runInPage);
+
+  await navigate(
+    "Navigate to a Spanish page with a different content principal",
+    { url: SPANISH_PAGE_URL_DOT_ORG }
+  );
+
+  await assertTranslationsButton(
+    { button: true },
+    "The translations button should be visible, because this content principal " +
+      "has not been denied translations permissions"
+  );
+
+  await assertPageIsUntranslated(runInPage);
+
+  await cleanup();
+});
