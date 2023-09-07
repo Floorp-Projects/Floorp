d1e7d59b0cfd619ed90f2f8ed6a6e4b7fb14f83d
Butkovits Atila <abutkovits@mozilla.com>
2023-09-07 18:58:05 +0300

    Backed out changeset fb19de542f01 (bug 1320607) for causing python failures complaining about test_devtools_database_parsing . CLOSED TREE

--- devtools/client/shared/sourceeditor/test/head.js
+++ devtools/client/shared/sourceeditor/test/head.js
@@ -13,6 +13,9 @@ Services.scriptloader.loadSubScript(
 );
 
 const Editor = require("resource://devtools/client/shared/sourceeditor/editor.js");
+const {
+  getClientCssProperties,
+} = require("resource://devtools/client/fronts/css-properties.js");
 
 function promiseWaitForFocus(el) {
   return new Promise(resolve => waitForFocus(resolve, el));
