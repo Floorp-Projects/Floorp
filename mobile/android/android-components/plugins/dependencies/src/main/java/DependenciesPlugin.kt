1f2b5950e19cae52d65bd05c789c82d329c3347d
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-09-07 09:56:25 -0400

    Bug 1852089 - Update AndroidX Annotation to version 1.7.0

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -59,7 +59,7 @@ object Versions {
 
     object AndroidX {
         const val activityCompose = "1.7.2"
-        const val annotation = "1.6.0"
+        const val annotation = "1.7.0"
         const val appcompat = "1.6.1"
         const val autofill = "1.1.0"
         const val browser = "1.5.0"
