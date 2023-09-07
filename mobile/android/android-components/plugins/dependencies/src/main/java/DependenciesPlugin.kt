650addc1c5a6b36770ec717c05e9e946deaa936c
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-09-07 11:58:35 -0400

    Bug 1852097 - Update AndroidX Recyclerview to version 1.3.1

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -71,7 +71,7 @@ object Versions {
         const val core = "1.12.0"
         const val drawerlayout = "1.2.0"
         const val fragment = "1.6.1"
-        const val recyclerview = "1.3.0"
+        const val recyclerview = "1.3.1"
         const val test = "1.5.0"
         const val test_ext = "1.1.5"
         const val test_runner = "1.5.2"
