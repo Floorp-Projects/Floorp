ee421a3c2e07e00a376b33146f5381d58ebad22d
serge-sans-paille <sguelton@mozilla.com>
2023-09-07 16:36:38 +0000

    Bug 1850948 - Use std::string::data() as strtok param in GetMemoryMappings, avoiding a buffer copy in the process r=emilio
    This is both beneficial for the actual code and an enhancement wrt.
    -ftrivial-auto-var-init as it gets rid of a usage of nsAutoCString.
    
    Differential Revision: https://phabricator.services.mozilla.com/D187198

--- xpcom/base/MemoryMapping.cpp
+++ xpcom/base/MemoryMapping.cpp
@@ -118,8 +118,8 @@ nsresult GetMemoryMappings(nsTArray<MemoryMapping>& aMappings, pid_t aPid) {
   }
 
   MemoryMapping* current = nullptr;
-  std::string buffer;
-  while (std::getline(stream, buffer)) {
+  std::string line;
+  while (std::getline(stream, line)) {
     size_t start, end, offset;
     char flags[4] = "---";
     char name[512];
@@ -131,7 +131,7 @@ nsresult GetMemoryMappings(nsTArray<MemoryMapping>& aMappings, pid_t aPid) {
     //
     // 1487118a7000-148711a5a000 r-xp 00000000 103:03 54004561                  /usr/lib/libc-2.27.so
     // clang-format on
-    if (sscanf(buffer.c_str(), "%zx-%zx %4c %zx %*u:%*u %*u %511s\n", &start,
+    if (sscanf(line.c_str(), "%zx-%zx %4c %zx %*u:%*u %*u %511s\n", &start,
                &end, flags, &offset, name) >= 4) {
       PermSet perms;
       if (flags[0] == 'r') {
@@ -157,9 +157,8 @@ nsresult GetMemoryMappings(nsTArray<MemoryMapping>& aMappings, pid_t aPid) {
       continue;
     }
 
-    nsAutoCStringN<128> line(buffer.c_str());
     char* savePtr;
-    char* fieldName = strtok_r(line.BeginWriting(), ":", &savePtr);
+    char* fieldName = strtok_r(line.data(), ":", &savePtr);
     if (!fieldName) {
       continue;
     }
