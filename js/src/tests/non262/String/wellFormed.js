1ed01494b81d15b4458ecd339e59abd8e0d3bf7f
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2023-09-07 07:57:59 +0000

    Bug 1850755 - Turn on well-formed-unicode-strings by default in Browser and JS Shell. r=spidermonkey-reviewers,jandem
    Well-Formed Unicode strings is in TC39 Stage 4 and has been merged into
    ECMA262 spec,
    https://github.com/tc39/ecma262/pull/3039
    
    Differential Revision: https://phabricator.services.mozilla.com/D187182

--- js/src/tests/non262/String/wellFormed.js
+++
@@ -1,9 +0,0 @@
-// Calling String.prototype.isWellFormed() or toWellFormed() should throw a
-// TypeError when the pref "enable-well-formed-unicode-strings" is off.
-//
-// This test should be removed once the pref is turned on by default.
-assertThrowsInstanceOf(() => "abc".isWellFormed(), TypeError)
-assertThrowsInstanceOf(() => "abc".toWellFormed(), TypeError)
-
-if (typeof reportCompare === "function")
-  reportCompare(0, 0);
