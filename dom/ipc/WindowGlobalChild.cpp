e956a5822b454213a518157504c31c59a8c2d971
Iulian Moraru <imoraru@mozilla.com>
2023-09-08 14:53:05 +0300

    Backed out changeset 81a597c0aa27 (bug 1783504) as per aiunusov's request. CLOSED TREE

--- dom/ipc/WindowGlobalChild.cpp
+++ dom/ipc/WindowGlobalChild.cpp
@@ -5,7 +5,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/dom/WindowGlobalChild.h"
-#include "nsScriptSecurityManager.h"
 
 #include "mozilla/AntiTrackingUtils.h"
 #include "mozilla/ClearOnShutdown.h"
@@ -573,30 +572,6 @@ void WindowGlobalChild::SetDocumentURI(nsIURI* aDocumentURI) {
       BrowsingContext()->BrowserId(), InnerWindowId(),
       nsContentUtils::TruncatedURLForDisplay(aDocumentURI, 1024),
       embedderInnerWindowID, BrowsingContext()->UsePrivateBrowsing());
-
-  auto isLoadableViaInternet = [](nsIURI* uri) {
-    return (uri && (net::SchemeIsHTTP(uri) || net::SchemeIsHTTPS(uri)));
-  };
-
-  if (isLoadableViaInternet(aDocumentURI)) {
-    nsCOMPtr<nsIURI> principalURI = mDocumentPrincipal->GetURI();
-    if (mDocumentPrincipal->GetIsNullPrincipal()) {
-      nsCOMPtr<nsIPrincipal> precursor =
-          mDocumentPrincipal->GetPrecursorPrincipal();
-      if (precursor) {
-        principalURI = precursor->GetURI();
-      }
-    }
-
-    if (isLoadableViaInternet(principalURI) &&
-        !nsScriptSecurityManager::SecurityCompareURIs(principalURI,
-                                                      aDocumentURI)) {
-      MOZ_DIAGNOSTIC_ASSERT(
-          false,
-          "Setting DocumentURI with a different origin than principal URI");
-    }
-  }
-
   mDocumentURI = aDocumentURI;
   SendUpdateDocumentURI(aDocumentURI);
 }
