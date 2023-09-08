65d2d31e9e81b2d1cdc733582c0b965998be9f47
Tom Schuster <tschuster@mozilla.com>
2023-09-08 19:24:44 +0000

    Bug 1846224 - Removed OverrideDefaultLocaleIfNeeded. r=tjr
    With this code we would continue to override all locales anyway.
    Like with timezones the possibility exists that this covered up something that we aren't spoofing now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D185432

--- toolkit/xre/nsAppRunner.h
+++ toolkit/xre/nsAppRunner.h
@@ -104,8 +104,6 @@ nsresult NS_LockProfilePath(nsIFile* aPath, nsIFile* aTempPath,
 
 void WriteConsoleLog();
 
-void OverrideDefaultLocaleIfNeeded();
-
 /**
  * Allow exit() calls to complete. This should be done from a proper Gecko
  * shutdown path. Otherwise we aim to catch improper shutdowns.
