1ebf4a7e73d509362483650126dd67fde8808a72
Henrik Skupin <mail@hskupin.info>
2023-09-11 21:21:24 +0000

    Bug 1849972 - [wdspec] mozilla/tests/webdriver/classic/new_session/binary.py has to correctly pass capabilities. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D187781

--- testing/web-platform/mozilla/tests/webdriver/classic/new_session/binary.py
+++ testing/web-platform/mozilla/tests/webdriver/classic/new_session/binary.py
@@ -1,38 +1,33 @@
 import os
-import sys
 
 from tests.support.asserts import assert_error, assert_success
 
 
-def test_bad_binary(new_session):
+def test_bad_binary(new_session, configuration):
     # skipif annotations are forbidden in wpt
     if os.path.exists("/bin/echo"):
-        response, _ = new_session(
-            {
-                "capabilities": {
-                    "alwaysMatch": {"moz:firefoxOptions": {"binary": "/bin/echo"}}
-                }
-            }
-        )
+        capabilities = configuration["capabilities"].copy()
+        capabilities["moz:firefoxOptions"]["binary"] = "/bin/echo"
+
+        response, _ = new_session({"capabilities": {"alwaysMatch": capabilities}})
         assert_error(response, "invalid argument")
 
 
 def test_shell_script_binary(new_session, configuration):
     # skipif annotations are forbidden in wpt
-    if sys.platform == "linux":
+    if os.path.exists("/bin/bash"):
         capabilities = configuration["capabilities"].copy()
         binary = configuration["browser"]["binary"]
-        script = f"""#!/bin/bash
 
-{binary} $@"""
         path = os.path.abspath("firefox.sh")
         assert not os.path.exists(path)
         try:
+            script = f"""#!/bin/bash\n\n"{binary}" $@\n"""
             with open("firefox.sh", "w") as f:
                 f.write(script)
             os.chmod(path, 0o744)
             capabilities["moz:firefoxOptions"]["binary"] = path
-            response, _ = new_session({"capabilities": capabilities})
+            response, _ = new_session({"capabilities": {"alwaysMatch": capabilities}})
             assert_success(response)
         finally:
             os.unlink(path)
