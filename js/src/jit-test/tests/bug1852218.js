b20fe9d817e57a391d0aae2244dd5433f26b192f
Iain Ireland <iireland@mozilla.com>
2023-09-11 22:23:58 +0000

    Bug 1852218: Remove old hints after adding new hints r=dpalmeiro a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D187807

---
+++ js/src/jit-test/tests/bug1852218.js
@@ -0,0 +1,17 @@
+// |jit-test| skip-if: getBuildConfiguration()["arm64-simulator"] === true
+// This test times out in ARM64 simulator builds.
+
+function makeIonCompiledScript(n) {
+  let src = "";
+  for (var i = 0; i < n; i++) {
+    src += "\n";
+  }
+  src += "function f() {}";
+  eval(src);
+  f();
+  return f;
+}
+
+for (var i = 0; i < 5010; i++) {
+  makeIonCompiledScript(i);
+}
