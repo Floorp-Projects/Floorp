98121b6b37bd6946ed588189ea38c5eacf755668
Henrik Skupin <mail@hskupin.info>
2023-09-12 08:45:44 +0000

    Bug 1852526 - [devtools] Remove "domstyleruleOrActorid" type. r=devtools-backward-compat-reviewers,devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D187894

--- devtools/shared/specs/style/style-types.js
+++ devtools/shared/specs/style/style-types.js
@@ -6,28 +6,7 @@
 const { RetVal, types } = require("resource://devtools/shared/protocol.js");
 
 // Predeclare the domstylerule actor type
-const domstyleruleType = types.addActorType("domstylerule");
-
-// @backward-compat { version 117 } The domstyleruleOrActorid type is only needed to handle
-// older server as some properties. When 117 hits release, it can be removed
-const domstyleruleactoridType = types.getType("domstylerule#actorid");
-types.addType("appliedstyledomstyleruleOrActorid", {
-  write: (value, context, detail) => {
-    // This write function will only be used on newer server, and so we can always write
-    // a proper domstylerule.
-    return domstyleruleType.write(value, context, detail);
-  },
-  read: (value, context, detail) => {
-    // When reading values, we might get a string, as older version of the server used
-    // to only return an actorId.
-    // In such case, we read with the domstylerule#actorid (that's what was used before)
-    if (typeof value === "string") {
-      return domstyleruleactoridType.read(value, context, detail);
-    }
-    // Otherwise, we can read it with the domstylerule type
-    return domstyleruleType.read(value, context, detail);
-  },
-});
+types.addActorType("domstylerule");
 
 /**
  * When asking for the styles applied to a node, we return a list of
@@ -35,13 +14,9 @@ types.addType("appliedstyledomstyleruleOrActorid", {
  * and which element they were inherited from (if any).
  */
 types.addDictType("appliedstyle", {
-  // @backward-compat { version 117 } When 117 hits release,
-  // this should use the "domstylerule" type
-  rule: "appliedstyledomstyleruleOrActorid",
+  rule: "domstylerule",
   inherited: "nullable:domnode#actorid",
-  // @backward-compat { version 117 } When 117 hits release,
-  // this should use the "nullable:domstylerule" type
-  keyframes: "nullable:appliedstyledomstyleruleOrActorid",
+  keyframes: "nullable:domstylerule",
 });
 
 types.addDictType("matchedselector", {
@@ -53,13 +28,6 @@ types.addDictType("matchedselector", {
 
 types.addDictType("appliedStylesReturn", {
   entries: "array:appliedstyle",
-  // @backward-compat { version 117 } Newer server does not return this "rules" property.
-  // We still need it though as older server send it, and they also send only actor ids
-  // for some properties in "entries". Those can only be turned into functioning fronts
-  // if the actor form is properly handled at some point, which is why we need to keep
-  // this "rules" property here.
-  // Once 117 hits release, this can be removed.
-  rules: "nullable:array:domstylerule",
 });
 
 types.addDictType("modifiedStylesReturn", {
