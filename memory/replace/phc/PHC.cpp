a31d9ffc106b2a0681a009faa04fb25566c0a827
Stanca Serban <sstanca@mozilla.com>
2023-09-12 07:54:18 +0300

    Backed out 2 changesets (bug 1814798) for multiple phc related failures.
    Backed out changeset 50b72ab948b2 (bug 1814798)
    Backed out changeset bf7d386ad43a (bug 1814798)

--- memory/replace/phc/PHC.cpp
+++ memory/replace/phc/PHC.cpp
@@ -323,9 +323,6 @@ static const size_t kAllPagesSize = kNumAllPages * kPageSize;
 // AllocAllPages() for more information.
 static const size_t kAllPagesJemallocSize = kAllPagesSize - kPageSize;
 
-// The default state for PHC.  Either Enabled or OnlyFree.
-#define DEFAULT_STATE mozilla::phc::OnlyFree
-
 // The junk value used to fill new allocation in debug builds. It's same value
 // as the one used by mozjemalloc. PHC applies it unconditionally in debug
 // builds. Unlike mozjemalloc, PHC doesn't consult the MALLOC_OPTIONS
@@ -877,14 +874,6 @@ class GMut {
   }
 #endif
 
-  // Should we make new PHC allocations?
-  bool ShouldMakeNewAllocations() const {
-    return mPhcState == mozilla::phc::Enabled;
-  }
-
-  using PHCState = mozilla::phc::PHCState;
-  void SetState(PHCState aState) { mPhcState = aState; }
-
  private:
   template <int N>
   uint64_t RandomSeed() {
@@ -944,11 +933,6 @@ class GMut {
   size_t mPageAllocHits = 0;
   size_t mPageAllocMisses = 0;
 #endif
-
-  // This will only ever be updated from one thread.  The other threads should
-  // eventually get the update.
-  Atomic<PHCState, Relaxed> mPhcState =
-      Atomic<PHCState, Relaxed>(DEFAULT_STATE);
 };
 
 Mutex GMut::sMutex;
@@ -1087,11 +1071,6 @@ static void* MaybePageAlloc(const Maybe<arena_id_t>& aArenaId, size_t aReqSize,
   MOZ_ASSERT(IsPowerOfTwo(aAlignment));
 
   if (aReqSize > kPageSize) {
-    return nullptr;
-  }
-
-  MOZ_ASSERT(gMut);
-  if (!gMut->ShouldMakeNewAllocations()) {
     return nullptr;
   }
 
@@ -1365,7 +1344,7 @@ MOZ_ALWAYS_INLINE static void* PageRealloc(const Maybe<arena_id_t>& aArenaId,
   // Check for realloc() of a freed block.
   gMut->EnsureValidAndInUse(lock, aOldPtr, index);
 
-  if (aNewSize <= kPageSize && gMut->ShouldMakeNewAllocations()) {
+  if (aNewSize <= kPageSize) {
     // A page-to-page transition. Just keep using the page allocation. We do
     // this even if the thread is disabled, because it doesn't create a new
     // page allocation. Note that ResizePageInUse() checks aArenaId.
@@ -1707,12 +1686,6 @@ class PHCBridge : public ReplaceMallocBridge {
     } else {
       aMemoryUsage.mFragmentationBytes = 0;
     }
-  }
-
-  // Enable or Disable PHC at runtime.  If PHC is disabled it will still trap
-  // bad uses of previous allocations, but won't track any new allocations.
-  virtual void SetPHCState(mozilla::phc::PHCState aState) override {
-    gMut->SetState(aState);
   }
 };
 
