801fcb75a6292d45d2c8b564e96deb030c21839e
Malte Juergens <mjurgens@mozilla.com>
2023-09-13 13:16:59 +0000

    Bug 1850200 - Add delay to HTTPS-Only "Continue to HTTPS Site" button r=freddyb
    Differential Revision: https://phabricator.services.mozilla.com/D187887

--- toolkit/components/httpsonlyerror/content/errorpage.js
+++ toolkit/components/httpsonlyerror/content/errorpage.js
@@ -34,6 +34,11 @@ function initPage() {
     .getElementById("openInsecure")
     .addEventListener("click", onOpenInsecureButtonClick);
 
+  const delay = RPMGetIntPref("security.dialog_enable_delay", 1000);
+  setTimeout(() => {
+    document.getElementById("openInsecure").removeAttribute("inert");
+  }, delay);
+
   if (window.top == window) {
     document
       .getElementById("goBack")
