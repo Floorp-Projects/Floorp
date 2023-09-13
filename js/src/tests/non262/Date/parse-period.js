112c4f27ae75a657d6c72a8ba4d03aa81cd51832
Vinny Diehl <vinny.diehl@gmail.com>
2023-09-13 08:32:26 +0000

    Bug 449921 - Loosen requirements for characters directly following numbers r=arai
    This patch newly allows cases such as MonDDYYYY and Mon.DD.YYYY.
    In adding tests for this patch, I noticed that the tests for parsing
    periods were broken and could not fail the way that they were written,
    so I fixed them.
    
    This patch also fixes a couple of other standing bugs...
    
    fixes Bug 485639 - for cases like "Aug 15 2015 10:00am"
    fixes Bug 1760290 - for YYYY.MM.DD
    
    Differential Revision: https://phabricator.services.mozilla.com/D186054

--- js/src/tests/non262/Date/parse-period.js
+++ js/src/tests/non262/Date/parse-period.js
@@ -1,3 +1,4 @@
+// |reftest| skip-if(xulRuntime.OS=="WINNT") -- Windows doesn't accept IANA names for the TZ env variable
 /* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -6,16 +7,26 @@
 const tests = [
   "Aug. 15, 2015",
   "Aug.. 15, 2015",
+  "Aug.15.2015",
+  "15.Aug.2015",
   "Aug 15 2015 12:00 am.",
   "Sat. Aug 15 2015",
-]
+  "2015.08.15",
+  // These look weird but are accepted for Chrome parity
+  "2015./08/15 00:00:00",
+  "2015/08./15 00:00:00",
+];
 
 for (const test of tests) {
-  assertEq(Date.parse("Aug 15, 2015"), Date.parse(test));
+  assertEq(new Date(test).getTime(),
+           new Date(2015, Month.August, 15).getTime(),
+           `"${test}" should be accepted.`);
 }
 
-assertEq(Date.parse("Aug 15 2015 GMT."),
-         Date.parse("Aug 15 2015 GMT"));
+inTimeZone("Etc/GMT-1", () => {
+  let dt = new Date("Aug 15 2015 GMT.");
+  assertEq(dt.getTime(), new Date(2015, Month.August, 15, 1).getTime());
+});
 
 if (typeof reportCompare === "function")
   reportCompare(true, true);
