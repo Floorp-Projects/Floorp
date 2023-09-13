ddf1f8f2b8b6bbd61af2add7838919d6e0ee76ae
Narcis Beleuzu <nbeleuzu@mozilla.com>
2023-09-13 04:51:24 +0300

    Backed out changeset c74fb301e2f4 (bug 1851609) for bc failures on browser_shopping_integration.js . CLOSED TREE

--- browser/components/shopping/content/unanalyzed.mjs
+++ browser/components/shopping/content/unanalyzed.mjs
@@ -10,6 +10,8 @@ import { MozLitElement } from "chrome://global/content/lit-utils.mjs";
 // eslint-disable-next-line import/no-unassigned-import
 import "chrome://browser/content/shopping/shopping-card.mjs";
 
+import { FAKESPOT_ANALYSIS_URL } from "chrome://global/content/shopping/ProductConfig.mjs";
+
 class UnanalyzedProductCard extends MozLitElement {
   static properties = {
     productURL: { type: String, reflect: true },
@@ -53,6 +55,10 @@ class UnanalyzedProductCard extends MozLitElement {
           <a
             id="unanalyzed-product-analysis-link"
             data-l10n-id="shopping-unanalyzed-product-analyze-link"
+            target="_blank"
+            href="${FAKESPOT_ANALYSIS_URL}${encodeURIComponent(
+              this.productURL
+            )}"
             @click=${this.onClickAnalysisLink}
           ></a>
         </div>
