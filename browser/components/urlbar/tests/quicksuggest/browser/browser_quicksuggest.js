d78b1385b1b07118ff9e33cd3ebb54f3da367fac
Daisuke Akatsuka <daisuke@birchill.co.jp>
2023-09-14 05:14:48 +0000

    Bug 1852187: Move sponsored suggestions higher r=adw,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D187873

--- browser/components/urlbar/tests/quicksuggest/browser/browser_quicksuggest.js
+++ browser/components/urlbar/tests/quicksuggest/browser/browser_quicksuggest.js
@@ -86,3 +86,77 @@ add_task(async function nonSponsored() {
   });
   await UrlbarTestUtils.promisePopupClose(window);
 });
+
+// Tests sponsored priority feature.
+add_task(async function sponsoredPriority() {
+  const cleanUpNimbus = await UrlbarTestUtils.initNimbusFeature({
+    quickSuggestSponsoredPriority: true,
+  });
+
+  await UrlbarTestUtils.promiseAutocompleteResultPopup({
+    window,
+    value: "fra",
+  });
+  await QuickSuggestTestUtils.assertIsQuickSuggest({
+    window,
+    index: 1,
+    isSponsored: true,
+    isBestMatch: true,
+    isEmptyDescription: true,
+    url: `${TEST_URL}?q=frabbits`,
+  });
+
+  let row = await UrlbarTestUtils.waitForAutocompleteResultAt(window, 1);
+  Assert.equal(
+    row.querySelector(".urlbarView-title").firstChild.textContent,
+    "fra",
+    "The part of the keyword that matches users input is not bold."
+  );
+  Assert.equal(
+    row.querySelector(".urlbarView-title > strong").textContent,
+    "b",
+    "The auto completed section of the keyword is bolded."
+  );
+
+  // Group label.
+  let before = window.getComputedStyle(row, "::before");
+  Assert.equal(before.content, "attr(label)", "::before.content is enabled");
+  Assert.equal(
+    row.getAttribute("label"),
+    "Sponsored",
+    "Row has 'Sponsored' group label"
+  );
+
+  await UrlbarTestUtils.promisePopupClose(window);
+  await cleanUpNimbus();
+});
+
+// Tests sponsored priority feature does not affect to non-sponsored suggestion.
+add_task(async function sponsoredPriorityButNotSponsoredSuggestion() {
+  const cleanUpNimbus = await UrlbarTestUtils.initNimbusFeature({
+    quickSuggestSponsoredPriority: true,
+  });
+
+  await UrlbarTestUtils.promiseAutocompleteResultPopup({
+    window,
+    value: "nonspon",
+  });
+  await QuickSuggestTestUtils.assertIsQuickSuggest({
+    window,
+    index: 1,
+    isSponsored: false,
+    url: `${TEST_URL}?q=nonsponsored`,
+  });
+
+  let row = await UrlbarTestUtils.waitForAutocompleteResultAt(window, 1);
+  let before = window.getComputedStyle(row, "::before");
+  Assert.equal(before.content, "attr(label)", "::before.content is enabled");
+  Assert.equal(
+    row.getAttribute("label"),
+    "Firefox Suggest",
+    "Row has general group label for quick suggest"
+  );
+
+  await UrlbarTestUtils.promisePopupClose(window);
+  await cleanUpNimbus();
+});
