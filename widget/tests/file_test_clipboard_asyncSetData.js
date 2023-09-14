91c367845e6e112e4acf67e7168dbccbb1c12f84
Edgar Chen <echen@mozilla.com>
2023-09-14 09:42:52 +0000

    Bug 1852783 - Part 2: Make clipboard tests reusable; r=spohl
    Differential Revision: https://phabricator.services.mozilla.com/D187995

---
+++ widget/tests/file_test_clipboard_asyncSetData.js
@@ -0,0 +1,173 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/* import-globals-from clipboard_helper.js */
+
+"use strict";
+
+clipboardTypes.forEach(function (type) {
+  if (clipboard.isClipboardTypeSupported(type)) {
+    clipboardTypes.forEach(function (otherType) {
+      if (clipboard.isClipboardTypeSupported(otherType)) {
+        [true, false].forEach(function (async) {
+          add_task(async function test_clipboard_pending_asyncSetData() {
+            info(
+              `Test having a pending asyncSetData request on ${type} and then make a new ${
+                async ? "asyncSetData" : "setData"
+              } request on ${otherType}`
+            );
+
+            // Create a pending asyncSetData request
+            let priorResult;
+            let priorRequest = clipboard.asyncSetData(type, {
+              QueryInterface: ChromeUtils.generateQI([
+                "nsIAsyncSetClipboardDataCallback",
+              ]),
+              onComplete(rv) {
+                priorResult = rv;
+              },
+            });
+
+            // Create a new request
+            let str = writeRandomStringToClipboard(
+              "text/plain",
+              otherType,
+              null,
+              async
+            );
+
+            if (type === otherType) {
+              info("The new request should cancel the prior pending request");
+              is(
+                priorResult,
+                Cr.NS_ERROR_ABORT,
+                "The pending asyncSetData request should be canceled"
+              );
+              try {
+                priorRequest.setData(
+                  generateNewTransferable("text/plain", generateRandomString())
+                );
+                ok(
+                  false,
+                  "An error should be thrown if setData is called on a canceled clipboard request"
+                );
+              } catch (e) {
+                is(
+                  e.result,
+                  Cr.NS_ERROR_FAILURE,
+                  "An error should be thrown if setData is called on a canceled clipboard request"
+                );
+              }
+            } else {
+              info(
+                "The new request should be used to cancel the prior pending request for different clipboard types"
+              );
+              is(
+                priorResult,
+                undefined,
+                "The pending asyncSetData request should not be canceled"
+              );
+              str = generateRandomString();
+              priorRequest.setData(
+                generateNewTransferable("text/plain", str),
+                null
+              );
+              try {
+                priorRequest.setData(
+                  generateNewTransferable("text/plain", generateRandomString())
+                );
+                ok(
+                  false,
+                  "Calling setData multiple times should throw an error"
+                );
+              } catch (e) {
+                is(
+                  e.result,
+                  Cr.NS_ERROR_FAILURE,
+                  "Calling setData multiple times should throw an error"
+                );
+              }
+            }
+
+            // Test clipboard data.
+            is(
+              getClipboardData("text/plain", type),
+              str,
+              `Test clipboard data for type ${type}`
+            );
+
+            // Clean clipboard data.
+            cleanupAllClipboard();
+          });
+        });
+      }
+    });
+
+    add_task(async function test_clipboard_asyncSetData_abort() {
+      info(`Test abort asyncSetData request on ${type}`);
+
+      // Create a pending asyncSetData request
+      let result;
+      let request = clipboard.asyncSetData(type, {
+        QueryInterface: ChromeUtils.generateQI([
+          "nsIAsyncSetClipboardDataCallback",
+        ]),
+        onComplete(rv) {
+          result = rv;
+        },
+      });
+
+      // Abort with NS_OK.
+      try {
+        request.abort(Cr.NS_OK);
+        ok(false, "Throw an error when attempting to abort with NS_OK");
+      } catch (e) {
+        is(
+          e.result,
+          Cr.NS_ERROR_FAILURE,
+          "Should throw an error when attempting to abort with NS_OK"
+        );
+      }
+      is(result, undefined, "The asyncSetData request should not be canceled");
+
+      // Abort with NS_ERROR_ABORT.
+      request.abort(Cr.NS_ERROR_ABORT);
+      is(
+        result,
+        Cr.NS_ERROR_ABORT,
+        "The asyncSetData request should be canceled"
+      );
+      try {
+        request.abort(Cr.NS_ERROR_FAILURE);
+        ok(false, "Throw an error when attempting to abort again");
+      } catch (e) {
+        is(
+          e.result,
+          Cr.NS_ERROR_FAILURE,
+          "Should throw an error when attempting to abort again"
+        );
+      }
+      is(
+        result,
+        Cr.NS_ERROR_ABORT,
+        "The callback should not be notified again"
+      );
+
+      try {
+        request.setData(
+          generateNewTransferable("text/plain", generateRandomString())
+        );
+        ok(
+          false,
+          "An error should be thrown if setData is called on a canceled clipboard request"
+        );
+      } catch (e) {
+        is(
+          e.result,
+          Cr.NS_ERROR_FAILURE,
+          "An error should be thrown if setData is called on a canceled clipboard request"
+        );
+      }
+    });
+  }
+});
