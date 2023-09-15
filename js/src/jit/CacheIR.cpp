d8d3060ad4dbcca3f665308bf22a0fcc3276c058
Iain Ireland <iireland@mozilla.com>
2023-09-15 18:28:54 +0000

    Bug 1853180: Don't support proxy targets in ScriptedProxy stub r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D188299

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -1566,6 +1566,8 @@ AttachDecision GetPropIRGenerator::tryAttachScriptedProxy(
   ObjOperandId handlerObjId = writer.guardToObject(handlerValId);
   ObjOperandId targetObjId = writer.loadWrapperTarget(objId);
 
+  writer.guardIsNativeObject(targetObjId);
+
   if (trapKind == NativeGetPropKind::Missing) {
     EmitMissingPropGuard(writer, nHandlerObj, handlerObjId);
     if (cacheKind_ == CacheKind::GetProp) {
