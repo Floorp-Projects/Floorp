44ba3d383a1ad86f3da063d3e376b9ee3297af97
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2023-09-15 03:19:26 +0000

    Bug 1852806 - Remove macros for older Windows SDK from browser/components/shell/. r=nalexander
    Depends on D188019
    
    Differential Revision: https://phabricator.services.mozilla.com/D188020

--- browser/components/shell/WindowsDefaultBrowser.cpp
+++ browser/components/shell/WindowsDefaultBrowser.cpp
@@ -15,15 +15,8 @@
 #include "mozilla/WindowsVersion.h"
 #include "mozilla/WinHeaderOnlyUtils.h"
 
-// Needed for access to IApplicationActivationManager
 // This must be before any other includes that might include shlobj.h
-#ifdef _WIN32_WINNT
-#  undef _WIN32_WINNT
-#endif
-#define _WIN32_WINNT 0x0600
 #define INITGUID
-#undef NTDDI_VERSION
-#define NTDDI_VERSION NTDDI_WIN8
 #include <shlobj.h>
 
 #include <lm.h>
