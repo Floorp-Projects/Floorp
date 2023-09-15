4eb43ef12386360af7c7ac4a803d1b9859f79936
Emilio Cobos Álvarez <emilio@crisal.io>
2023-09-15 15:37:15 +0000

    Bug 1853323 - Remove browser.underline_anchors. r=settings-reviewers,mconley
    This is weirdly placed in the colors dialog, and adds a global selector
    to the UA sheet for no great reason.
    
    This sub-setting doesn't really make a lot of sense. We don't let you
    force underlines in any way, so any site CSS will override it (unlike
    colors, where you can actually override the site).
    
    Just move this to the UA sheet properly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D188326

--- browser/components/preferences/dialogs/colors.js
+++ browser/components/preferences/dialogs/colors.js
@@ -12,7 +12,6 @@ Preferences.addAll([
   { id: "browser.display.document_color_use", type: "int" },
   { id: "browser.anchor_color", type: "string" },
   { id: "browser.visited_color", type: "string" },
-  { id: "browser.underline_anchors", type: "bool" },
   { id: "browser.display.foreground_color", type: "string" },
   { id: "browser.display.background_color", type: "string" },
   { id: "browser.display.use_system_colors", type: "bool" },
