ae03610adcdc1eae5a995c8b69648f1d179fa53e
Tooru Fujisawa <arai_a@mac.com>
2023-09-15 04:15:50 +0000

    Bug 1849294 - Part 2: Remove SrcNoteType::AssignOp and GetSrcNote. r=bthrall
    GetSrcNote and GSNCache is used only for SrcNoteType::AssignOp.
    
    Differential Revision: https://phabricator.services.mozilla.com/D186917

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -12381,11 +12381,6 @@ bool BytecodeEmitter::addTryNote(TryNoteKind kind, uint32_t stackDepth,
 }
 
 bool BytecodeEmitter::newSrcNote(SrcNoteType type, unsigned* indexp) {
-  // Non-gettable source notes such as column/lineno and debugger should not be
-  // emitted for prologue / self-hosted.
-  MOZ_ASSERT_IF(skipLocationSrcNotes() || skipBreakpointSrcNotes(),
-                type <= SrcNoteType::LastGettable);
-
   SrcNotesVector& notes = bytecodeSection().notes();
   unsigned index;
 
