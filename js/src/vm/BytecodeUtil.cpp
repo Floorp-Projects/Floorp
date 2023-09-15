575516a931d03d4d5835a6c76e124a323ea5373c
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2023-09-15 13:33:39 +0000

    Bug 1839396 part 6 - Add Sprinter::length. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D181491

--- js/src/vm/BytecodeUtil.cpp
+++ js/src/vm/BytecodeUtil.cpp
@@ -1365,7 +1365,7 @@ static unsigned Disassemble1(JSContext* cx, HandleScript script, jsbytecode* pc,
     }
   }
 
-  size_t before = sp->stringEnd() - sp->string();
+  size_t before = sp->length();
   bool stackDumped = false;
   auto dumpStack = [&cx, &script, &pc, &parser, &sp, &before, &stackDumped]() {
     if (!parser) {
@@ -1376,7 +1376,7 @@ static unsigned Disassemble1(JSContext* cx, HandleScript script, jsbytecode* pc,
     }
     stackDumped = true;
 
-    size_t after = sp->stringEnd() - sp->string();
+    size_t after = sp->length();
     MOZ_ASSERT(after >= before);
 
     static const size_t stack_column = 40;
