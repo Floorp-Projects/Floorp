73c00ca360cdf7df70041294915f95ffa92148de
Otto Länd <bind-autoland@mozilla.com>
2023-09-15 01:36:30 +0000

    Bug 1850028: apply code formatting via Lando
    # ignore-this-changeset

--- js/src/wasm/WasmFrameIter.cpp
+++ js/src/wasm/WasmFrameIter.cpp
@@ -729,9 +729,9 @@ void wasm::GenerateFunctionPrologue(MacroAssembler& masm,
               subTypingDepth);
 
           // Perform the check
-          masm.branchWasmSTVIsSubtypeDynamicDepth(
-              scratch1, WasmTableCallSigReg, subTypingDepth, scratch2,
-              &functionBody, true);
+          masm.branchWasmSTVIsSubtypeDynamicDepth(scratch1, WasmTableCallSigReg,
+                                                  subTypingDepth, scratch2,
+                                                  &functionBody, true);
         }
 
         masm.bind(&fail);
