53612df2f356defc8fa808669f1b362aea84c309
Iulian Moraru <imoraru@mozilla.com>
2023-09-16 05:14:15 +0300

    Backed out changeset 163d24542c19 (bug 1853216) for causing reftest failures on 1853216-1.html.

--- gfx/thebes/gfxTextRun.h
+++ gfx/thebes/gfxTextRun.h
@@ -264,7 +264,6 @@ class gfxTextRun : public gfxShapedText {
     mozilla::SVGContextPaint* contextPaint = nullptr;
     gfxTextRunDrawCallbacks* callbacks = nullptr;
     bool allowGDI = true;
-    bool hasTextShadow = false;
     explicit DrawParams(gfxContext* aContext) : context(aContext) {}
   };
 
