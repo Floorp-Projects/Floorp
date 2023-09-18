c03af6a28d7e3dc70bd1988ffe0b0e74167bc382
Jon Coppeard <jcoppeard@mozilla.com>
2023-09-18 08:32:55 +0000

    Bug 1853305 - Part 1: Add AutoSelectGCHeap and use it when creating property iterators r=sfink
    This class observes nursery collections and encasuplates the logic of switching
    heap after a specified number of collections occur.
    
    Differential Revision: https://phabricator.services.mozilla.com/D188333

--- js/src/jsnum.cpp
+++ js/src/jsnum.cpp
@@ -808,6 +808,12 @@ MOZ_ALWAYS_INLINE static T* BackfillInt32InBuffer(int32_t si, T* buffer,
 
 template <AllowGC allowGC>
 JSLinearString* js::Int32ToString(JSContext* cx, int32_t si) {
+  return js::Int32ToStringWithHeap<allowGC>(cx, si, gc::Heap::Default);
+}
+
+template <AllowGC allowGC>
+JSLinearString* js::Int32ToStringWithHeap(JSContext* cx, int32_t si,
+                                          gc::Heap heap) {
   if (JSLinearString* str = LookupInt32ToString(cx, si)) {
     return str;
   }
@@ -818,8 +824,7 @@ JSLinearString* js::Int32ToString(JSContext* cx, int32_t si) {
       BackfillInt32InBuffer(si, buffer, std::size(buffer), &length);
 
   mozilla::Range<const Latin1Char> chars(start, length);
-  JSInlineString* str =
-      NewInlineString<allowGC>(cx, chars, js::gc::Heap::Default);
+  JSInlineString* str = NewInlineString<allowGC>(cx, chars, heap);
   if (!str) {
     return nullptr;
   }
