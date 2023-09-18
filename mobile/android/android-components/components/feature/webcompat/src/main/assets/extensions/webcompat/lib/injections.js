eedeb5ef85e6fdc377ac6438a25e0db8e15eb371
Thomas Wisniewski <wisniewskit@gmail.com>
2023-09-18 16:53:35 -0400

    Bug 1853013 - Ship 118.3.0 of feature-webcompat

--- mobile/android/android-components/components/feature/webcompat/src/main/assets/extensions/webcompat/lib/injections.js
+++ mobile/android/android-components/components/feature/webcompat/src/main/assets/extensions/webcompat/lib/injections.js
@@ -14,7 +14,24 @@ class Injections {
 
     this._availableInjections = availableInjections;
     this._activeInjections = new Set();
+    // Only used if this.shouldUseScriptingAPI is false and we are falling back
+    // to use the contentScripts API.
+    this._activeInjectionHandles = new Map();
     this._customFunctions = customFunctions;
+
+    this.shouldUseScriptingAPI =
+      browser.aboutConfigPrefs.getBoolPrefSync("useScriptingAPI");
+    // Debug log emit only on nightly (similarly to the debug
+    // helper used in shims.js for similar purpose).
+    browser.appConstants.getReleaseBranch().then(releaseBranch => {
+      if (releaseBranch !== "release_or_beta") {
+        console.debug(
+          `WebCompat Injections will be injected using ${
+            this.shouldUseScriptingAPI ? "scripting" : "contentScripts"
+          } API`
+        );
+      }
+    });
   }
 
   bindAboutCompatBroker(broker) {
@@ -83,7 +100,9 @@ class Injections {
       platformInfo.os == "android" ? "android" : "desktop",
     ];
 
-    let registeredScriptIds = await this.getPromiseRegisteredScriptIds();
+    let registeredScriptIds = this.shouldUseScriptingAPI
+      ? await this.getPromiseRegisteredScriptIds()
+      : [];
 
     for (const injection of this._availableInjections) {
       if (platformMatches.includes(injection.platform)) {
@@ -103,30 +122,33 @@ class Injections {
   buildContentScriptRegistrations(contentScripts) {
     let finalConfig = Object.assign({}, contentScripts);
 
-    // Don't persist the content scripts across browser restarts
-    // (at least not yet, we would need to apply some more changes
-    // to adjust webcompat for accounting for the scripts to be
-    // already registered).
-    //
-    // NOTE: scripting API has been introduced in Gecko 102,
-    // prior to Gecko 105 persistAcrossSessions option was required
-    // and only accepted false persistAcrossSessions, after Gecko 105
-    // is optional and defaults to true.
-    finalConfig.persistAcrossSessions = false;
-
     if (!finalConfig.runAt) {
       finalConfig.runAt = "document_start";
     }
 
-    // Convert js/css from contentScripts.register API method
-    // format to scripting.registerContentScripts API method
-    // format.
-    if (Array.isArray(finalConfig.js)) {
-      finalConfig.js = finalConfig.js.map(e => e.file);
-    }
+    if (this.shouldUseScriptingAPI) {
+      // Don't persist the content scripts across browser restarts
+      // (at least not yet, we would need to apply some more changes
+      // to adjust webcompat for accounting for the scripts to be
+      // already registered).
+      //
+      // NOTE: scripting API has been introduced in Gecko 102,
+      // prior to Gecko 105 persistAcrossSessions option was required
+      // and only accepted false persistAcrossSessions, after Gecko 105
+      // is optional and defaults to true.
 
-    if (Array.isArray(finalConfig.css)) {
-      finalConfig.css = finalConfig.css.map(e => e.file);
+      finalConfig.persistAcrossSessions = false;
+
+      // Convert js/css from contentScripts.register API method
+      // format to scripting.registerContentScripts API method
+      // format.
+      if (Array.isArray(finalConfig.js)) {
+        finalConfig.js = finalConfig.js.map(e => e.file);
+      }
+
+      if (Array.isArray(finalConfig.css)) {
+        finalConfig.css = finalConfig.css.map(e => e.file);
+      }
     }
 
     return finalConfig;
@@ -159,22 +181,31 @@ class Injections {
     let injectProps;
     try {
       const { id } = injection;
-      // enableContentScripts receives a registeredScriptIds already
-      // pre-computed once from registerContentScripts to register all
-      // the injection, whereas it does not expect to receive one when
-      // it is called from the AboutCompatBroker to re-enable one specific
-      // injection.
-      let activeScriptIds = Array.isArray(registeredScriptIds)
-        ? registeredScriptIds
-        : await this.getPromiseRegisteredScriptIds([id]);
-      injectProps = this.buildContentScriptRegistrations(
-        injection.contentScripts
-      );
-      injectProps.id = id;
-      if (!activeScriptIds.includes(id)) {
-        await browser.scripting.registerContentScripts([injectProps]);
+      if (this.shouldUseScriptingAPI) {
+        // enableContentScripts receives a registeredScriptIds already
+        // pre-computed once from registerContentScripts to register all
+        // the injection, whereas it does not expect to receive one when
+        // it is called from the AboutCompatBroker to re-enable one specific
+        // injection.
+        let activeScriptIds = Array.isArray(registeredScriptIds)
+          ? registeredScriptIds
+          : await this.getPromiseRegisteredScriptIds([id]);
+        injectProps = this.buildContentScriptRegistrations(
+          injection.contentScripts
+        );
+        injectProps.id = id;
+        if (!activeScriptIds.includes(id)) {
+          await browser.scripting.registerContentScripts([injectProps]);
+        }
+        this._activeInjections.add(id);
+      } else {
+        const handle = await browser.contentScripts.register(
+          this.buildContentScriptRegistrations(injection.contentScripts)
+        );
+        this._activeInjections.add(id);
+        this._activeInjectionHandles.set(id, handle);
       }
-      this._activeInjections.add(id);
+
       injection.active = true;
     } catch (ex) {
       console.error(
@@ -223,7 +254,15 @@ class Injections {
 
   async disableContentScripts(injection) {
     if (this._activeInjections.has(injection.id)) {
-      await browser.scripting.unregisterContentScripts({ ids: [injection.id] });
+      if (this.shouldUseScriptingAPI) {
+        await browser.scripting.unregisterContentScripts({
+          ids: [injection.id],
+        });
+      } else {
+        const handle = this._activeInjectionHandles.get(injection.id);
+        await handle.unregister();
+        this._activeInjectionHandles.delete(injection.id);
+      }
       this._activeInjections.delete(injection);
     }
     injection.active = false;
