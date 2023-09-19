695a5cba3d5373d789b75bbeb6ea73d7af3418c5
Andrew Osmond <aosmond@mozilla.com>
2023-09-19 19:23:58 +0000

    Bug 1844651 - Gracefully handle GMP service shutdown barrier related initialization failures. r=media-playback-reviewers,alwu a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D188600

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -1699,7 +1699,9 @@ void ContentParent::Init() {
 
   RefPtr<GeckoMediaPluginServiceParent> gmps(
       GeckoMediaPluginServiceParent::GetSingleton());
-  gmps->UpdateContentProcessGMPCapabilities(this);
+  if (gmps) {
+    gmps->UpdateContentProcessGMPCapabilities(this);
+  }
 
   // Flush any pref updates that happened during launch and weren't
   // included in the blobs set up in BeginSubprocessLaunch.
