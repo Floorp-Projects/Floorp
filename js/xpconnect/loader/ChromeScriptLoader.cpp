deaf9a7c7f48fb38bd447509181dd54009d32ffd
Tooru Fujisawa <arai_a@mac.com>
2023-09-19 09:46:25 +0000

    Bug 1853480 - Part 3: Use TaskController thread stack size as JS stack quota in ChromeUtils.compileScript off-thread compilation. r=nbp
    Depends on D188557
    
    Differential Revision: https://phabricator.services.mozilla.com/D188558

--- js/xpconnect/loader/ChromeScriptLoader.cpp
+++ js/xpconnect/loader/ChromeScriptLoader.cpp
@@ -68,10 +68,17 @@ class AsyncScriptCompileTask final : public Task {
   }
 
  private:
+  static size_t ThreadStackQuotaForSize(size_t size) {
+    // Set the stack quota to 10% less that the actual size.
+    // NOTE: This follows what JS helper thread does.
+    return size_t(double(size) * 0.9);
+  }
+
   void Compile() {
     // NOTE: The stack limit must be set from the same thread that compiles.
-    const size_t kDefaultStackQuota = 128 * sizeof(size_t) * 1024;
-    JS::SetNativeStackQuota(mFrontendContext, kDefaultStackQuota);
+    size_t stackSize = TaskController::GetThreadStackSize();
+    JS::SetNativeStackQuota(mFrontendContext,
+                            ThreadStackQuotaForSize(stackSize));
 
     JS::CompilationStorage compileStorage;
     mStencil = JS::CompileGlobalScriptToStencil(mFrontendContext, mOptions,
