8891df017f60a91efdfa70e2c26db5b308a9d4ec
Tooru Fujisawa <arai_a@mac.com>
2023-09-19 09:46:25 +0000

    Bug 1853480 - Part 4: Use TaskController thread stack size as JS stack quota in ScriptPreloader off-thread decode. r=nbp
    Depends on D188558
    
    Differential Revision: https://phabricator.services.mozilla.com/D188559

--- js/xpconnect/loader/ScriptPreloader.cpp
+++ js/xpconnect/loader/ScriptPreloader.cpp
@@ -1200,6 +1200,12 @@ bool ScriptPreloader::StartDecodeTask(
   return NS_SUCCEEDED(rv);
 }
 
+static size_t ThreadStackQuotaForSize(size_t size) {
+  // Set the stack quota to 10% less that the actual size.
+  // NOTE: This follows what JS helper thread does.
+  return size_t(double(size) * 0.9);
+}
+
 NS_IMETHODIMP ScriptPreloader::DecodeTask::Run() {
   auto failure = [&]() {
     RefPtr<JS::Stencil> stencil;
@@ -1216,9 +1222,8 @@ NS_IMETHODIMP ScriptPreloader::DecodeTask::Run() {
 
   auto cleanup = MakeScopeExit([&]() { JS::DestroyFrontendContext(fc); });
 
-  const size_t kDefaultStackQuota = 128 * sizeof(size_t) * 1024;
-
-  JS::SetNativeStackQuota(fc, kDefaultStackQuota);
+  size_t stackSize = TaskController::GetThreadStackSize();
+  JS::SetNativeStackQuota(fc, ThreadStackQuotaForSize(stackSize));
 
   size_t remaining = mDecodingSources.length();
   for (auto& source : mDecodingSources) {
