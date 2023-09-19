7c982eb4696b101eea07e1fbf78ddb8541782412
Dan Baker <dbaker@mozilla.com>
2023-09-19 14:26:16 -0600

    Bug 1851693 - Vendor libwebrtc from 17ec0569c6
    Upstream commit: https://webrtc.googlesource.com/src/+/17ec0569c6e453c2fcb293c71a3da4eb6fced83f
        ICE: adjust priority of non-relay candidates
    
        Introduces a field trial
          WebRTC-IncreaseIceCandidatePriorityHostSrflx
        that adjusts the priority of non-relay candidates such that the STUN priority attribute calculated as
          (prflx-type-preference << 24) | (priority & 0x00FFFFFF)
        as described in
          https://www.rfc-editor.org/rfc/rfc5245#section-7.1.2.1
        will satisfy the condition that the STUN priority of server-reflexive candidates will always be higher than the STUN priority of relay candidates.
    
        Previously this was not the case because the TURN relay preference was added to the local_preference for relay candidates, making it higher than the local_preference of srflx candidates gathered from the same interface.
        This led to cases where the resulting candidate pair priority of a srflx-relay pair was higher than the candidate pair priority of a srflx-srflx pair, i.e. using a TURN server in cases that work using a direct P2P connection.
    
        Whether the field trial is active can be observed by checking that
          priority-of-srflx-candidate&0x00FFFFFF
        is greater than
          priority-of-relay-candidate&0x00FFFFFF
    
        BUG=webrtc:13195,webrtc:5813,webrtc:15020
    
        Change-Id: Ib91708fbe7310b6454f93158a45c9d77da009091
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/292700
        Reviewed-by: Jonas Oreland <jonaso@webrtc.org>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Philipp Hancke <phancke@microsoft.com>
        Cr-Commit-Position: refs/heads/main@{#40311}

--- third_party/libwebrtc/p2p/base/port.cc
+++ third_party/libwebrtc/p2p/base/port.cc
@@ -270,9 +270,11 @@ void Port::AddAddress(const rtc::SocketAddress& address,
       ComputeFoundation(type, protocol, relay_protocol, base_address);
   Candidate c(component_, protocol, address, 0U, username_fragment(), password_,
               type, generation_, foundation, network_->id(), network_cost_);
-  c.set_priority(
-      c.GetPriority(type_preference, network_->preference(), relay_preference));
   c.set_relay_protocol(relay_protocol);
+  c.set_priority(
+      c.GetPriority(type_preference, network_->preference(), relay_preference,
+                    field_trials_->IsEnabled(
+                        "WebRTC-IncreaseIceCandidatePriorityHostSrflx")));
   c.set_tcptype(tcptype);
   c.set_network_name(network_->name());
   c.set_network_type(network_->type());
