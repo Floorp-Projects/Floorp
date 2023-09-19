e55e001e9696c2f0a619f4c2100e575b2fa41906
Dan Baker <dbaker@mozilla.com>
2023-09-19 12:15:36 -0600

    Bug 1851693 - Vendor libwebrtc from de923386a0
    Upstream commit: https://webrtc.googlesource.com/src/+/de923386a0327e702d3854bd2be2fe9c0b2ccd22
        Update parameters' type from NSString to AVAudioSession*.
    
        Bug: webrtc:15233
        Change-Id: I110a3fb1e992ff07aebe21881ee31d55d39db60b
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/308520
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40258}

--- third_party/libwebrtc/sdk/objc/components/audio/RTCAudioSession.mm
+++ third_party/libwebrtc/sdk/objc/components/audio/RTCAudioSession.mm
@@ -407,8 +407,8 @@ ABSL_CONST_INIT thread_local bool mutex_locked = false;
   return success;
 }
 
-- (BOOL)setCategory:(NSString *)category
-               mode:(NSString *)mode
+- (BOOL)setCategory:(AVAudioSessionCategory)category
+               mode:(AVAudioSessionMode)mode
             options:(AVAudioSessionCategoryOptions)options
               error:(NSError **)outError {
   if (![self checkLock:outError]) {
@@ -417,7 +417,7 @@ ABSL_CONST_INIT thread_local bool mutex_locked = false;
   return [self.session setCategory:category mode:mode options:options error:outError];
 }
 
-- (BOOL)setCategory:(NSString *)category
+- (BOOL)setCategory:(AVAudioSessionCategory)category
         withOptions:(AVAudioSessionCategoryOptions)options
               error:(NSError **)outError {
   if (![self checkLock:outError]) {
@@ -426,7 +426,7 @@ ABSL_CONST_INIT thread_local bool mutex_locked = false;
   return [self.session setCategory:category withOptions:options error:outError];
 }
 
-- (BOOL)setMode:(NSString *)mode error:(NSError **)outError {
+- (BOOL)setMode:(AVAudioSessionMode)mode error:(NSError **)outError {
   if (![self checkLock:outError]) {
     return NO;
   }
