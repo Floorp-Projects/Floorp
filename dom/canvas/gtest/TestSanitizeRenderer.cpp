9e48775e3a59f34a6aa74ca2f096265c562e10ec
Sandor Molnar <smolnar@mozilla.com>
2023-09-19 03:22:53 +0300

    Backed out 3 changesets (bug 1826040) for causing mochitest failures on dom/canvas/test/webgl-mochitest/test_renderer_strings.html CLOSED TREE
    Backed out changeset 8ee7e958a5af (bug 1826040)
    Backed out changeset 8aa3a71df9ef (bug 1826040)
    Backed out changeset bf56f53cd44a (bug 1826040)

--- dom/canvas/gtest/TestSanitizeRenderer.cpp
+++ dom/canvas/gtest/TestSanitizeRenderer.cpp
@@ -13,7 +13,7 @@ TEST(SanitizeRenderer, TestLinuxRadeon)
 {
   const std::string renderer(
       "AMD Radeon RX 5700 (NAVI10, DRM 3.35.0, 5.4.0-65-generic, LLVM 11.0.0)");
-  const std::string expectation("Radeon R9 200 Series, or similar");
+  const std::string expectation("Radeon R9 200 Series");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -23,7 +23,7 @@ TEST(SanitizeRenderer, TestLinuxRadeonTM)
   const std::string renderer(
       "AMD Radeon (TM) RX 460 Graphics (POLARIS11, DRM 3.35.0, "
       "5.4.0-65-generic, LLVM 11.0.0)");
-  const std::string expectation("Radeon R9 200 Series, or similar");
+  const std::string expectation("Radeon R9 200 Series");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -32,7 +32,7 @@ TEST(SanitizeRenderer, TestLinuxRadeonWithoutGeneration)
 {
   const std::string renderer(
       "AMD Radeon RX 5700 (DRM 3.35.0, 5.4.0-65-generic, LLVM 11.0.0)");
-  const std::string expectation("Radeon R9 200 Series, or similar");
+  const std::string expectation("Radeon R9 200 Series");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -41,7 +41,7 @@ TEST(SanitizeRenderer, TestLinuxRenoir)
 {
   const std::string renderer(
       "AMD RENOIR (DRM 3.41.0, 5.13.4-1-default, LLVM 11.1.0)");
-  const std::string expectation("Radeon R9 200 Series, or similar");
+  const std::string expectation("Radeon R9 200 Series");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -49,7 +49,7 @@ TEST(SanitizeRenderer, TestLinuxRenoir)
 TEST(SanitizeRenderer, TestWindowsVega)
 {
   const std::string renderer("Radeon RX Vega");
-  const std::string expectation("Radeon R9 200 Series, or similar");
+  const std::string expectation("Radeon R9 200 Series");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -57,7 +57,7 @@ TEST(SanitizeRenderer, TestWindowsVega)
 TEST(SanitizeRenderer, TestMacAmd)
 {
   const std::string renderer("AMD Radeon Pro 5300M OpenGL Engine");
-  const std::string expectation("Radeon R9 200 Series, or similar");
+  const std::string expectation("Radeon R9 200 Series");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -66,16 +66,7 @@ TEST(SanitizeRenderer, TestLinuxAruba)
 {
   const std::string renderer(
       "AMD ARUBA (DRM 2.50.0 / 5.10.42-calculate, LLVM 12.0.1)");
-  const std::string expectation("Radeon HD 5850, or similar");
-  const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
-  EXPECT_EQ(sanitized, expectation);
-}
-
-TEST(SanitizeRenderer, TestLinuxRembrandt)
-{
-  const std::string renderer(
-      "REMBRANDT (rembrandt, LLVM 14.0.6, DRM 3.49, 6.2.9)");
-  const std::string expectation("Radeon R9 200 Series, or similar");
+  const std::string expectation("Radeon HD 5850");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -83,14 +74,14 @@ TEST(SanitizeRenderer, TestLinuxRembrandt)
 TEST(SanitizeRenderer, TestLinuxIntel620)
 {
   const std::string renderer("Mesa Intel(R) UHD Graphics 620 (KBL GT2)");
-  const std::string expectation("Intel(R) HD Graphics 400, or similar");
+  const std::string expectation("Intel(R) HD Graphics 400");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
 TEST(SanitizeRenderer, TestLinuxIntel4000)
 {
   const std::string renderer("Mesa DRI Intel(R) HD Graphics 4000 (IVB GT2)");
-  const std::string expectation("Intel(R) HD Graphics, or similar");
+  const std::string expectation("Intel(R) HD Graphics");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -98,7 +89,7 @@ TEST(SanitizeRenderer, TestLinuxIntel4000)
 TEST(SanitizeRenderer, TestMacIntel)
 {
   const std::string renderer("Intel(R) HD Graphics 530");
-  const std::string expectation("Intel(R) HD Graphics 400, or similar");
+  const std::string expectation("Intel(R) HD Graphics 400");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -106,7 +97,7 @@ TEST(SanitizeRenderer, TestMacIntel)
 TEST(SanitizeRenderer, TestMacApple)
 {
   const std::string renderer("Apple M1");
-  const std::string expectation("Apple M1, or similar");
+  const std::string expectation("Apple M1");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -114,7 +105,7 @@ TEST(SanitizeRenderer, TestMacApple)
 TEST(SanitizeRenderer, TestPipe)
 {
   const std::string renderer("llvmpipe (LLVM 11.0.0, 128 bits)");
-  const std::string expectation("llvmpipe, or similar");
+  const std::string expectation("llvmpipe");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -127,7 +118,7 @@ TEST(SanitizeRenderer, TestAngleVega)
       "ANGLE (AMD, Radeon RX Vega Direct3D11 vs_5_0 ps_5_0, "
       "D3D11-27.20.22025.1006)");
   const std::string expectation(
-      "ANGLE (AMD, Radeon R9 200 Series Direct3D11 vs_5_0 ps_5_0), or similar");
+      "ANGLE (AMD, Radeon R9 200 Series Direct3D11 vs_5_0 ps_5_0)");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -138,8 +129,7 @@ TEST(SanitizeRenderer, TestAngleRadeon)
       "ANGLE (AMD, AMD Radeon(TM) Graphics Direct3D11 vs_5_0 ps_5_0, "
       "D3D11-27.20.1020.2002)");
   const std::string expectation(
-      "ANGLE (AMD, Radeon HD 3200 Graphics Direct3D11 vs_5_0 ps_5_0), or "
-      "similar");
+      "ANGLE (AMD, Radeon HD 3200 Graphics Direct3D11 vs_5_0 ps_5_0)");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -150,8 +140,7 @@ TEST(SanitizeRenderer, TestAngleWarp)
       "ANGLE (Unknown, Microsoft Basic Render Driver Direct3D11 vs_5_0 ps_5_0, "
       "D3D11-10.0.22000.1)");
   const std::string expectation =
-      "ANGLE (Unknown, Microsoft Basic Render Driver Direct3D11 vs_5_0 "
-      "ps_5_0), or similar";
+      "ANGLE (Unknown, Microsoft Basic Render Driver Direct3D11 vs_5_0 ps_5_0)";
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -162,8 +151,7 @@ TEST(SanitizeRenderer, TestAngle3070)
       "ANGLE (NVIDIA, NVIDIA GeForce RTX 3070 Direct3D11 vs_5_0 ps_5_0, "
       "D3D11-10.0.22000.1)");
   const std::string expectation(
-      "ANGLE (NVIDIA, NVIDIA GeForce GTX 980 Direct3D11 vs_5_0 ps_5_0), or "
-      "similar");
+      "ANGLE (NVIDIA, NVIDIA GeForce GTX 980 Direct3D11 vs_5_0 ps_5_0)");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -171,7 +159,7 @@ TEST(SanitizeRenderer, TestAngle3070)
 TEST(SanitizeRenderer, TestWindows3070)
 {
   const std::string renderer("GeForce RTX 3070/PCIe/SSE2");
-  const std::string expectation("GeForce GTX 980, or similar");
+  const std::string expectation("GeForce GTX 980/PCIe/SSE2");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -182,8 +170,7 @@ TEST(SanitizeRenderer, TestAngleK600)
       "ANGLE (NVIDIA, NVIDIA Quadro K600 Direct3D11 vs_5_0 ps_5_0, somethig "
       "like D3D11-10.0.22000.1)");
   const std::string expectation(
-      "ANGLE (NVIDIA, NVIDIA GeForce GTX 480 Direct3D11 vs_5_0 ps_5_0), or "
-      "similar");
+      "ANGLE (NVIDIA, NVIDIA GeForce GTX 480 Direct3D11 vs_5_0 ps_5_0)");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -191,7 +178,7 @@ TEST(SanitizeRenderer, TestAngleK600)
 TEST(SanitizeRenderer, TestLinuxK600)
 {
   const std::string renderer("NVE7");
-  const std::string expectation("GeForce GTX 480, or similar");
+  const std::string expectation("GeForce GTX 480");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -199,7 +186,7 @@ TEST(SanitizeRenderer, TestLinuxK600)
 TEST(SanitizeRenderer, TestLinux980)
 {
   const std::string renderer("NV124");
-  const std::string expectation("GeForce GTX 980, or similar");
+  const std::string expectation("GeForce GTX 980");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -207,7 +194,7 @@ TEST(SanitizeRenderer, TestLinux980)
 TEST(SanitizeRenderer, TestAdreno512)
 {
   const std::string renderer("Adreno (TM) 512");
-  const std::string expectation("Adreno (TM) 540, or similar");
+  const std::string expectation("Adreno (TM) 540");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -220,7 +207,7 @@ TEST(SanitizeRenderer, TestAdreno512)
 TEST(SanitizeRenderer, TestCiAndroid)
 {
   const std::string renderer("Adreno (TM) 540");
-  const std::string expectation("Adreno (TM) 540, or similar");
+  const std::string expectation("Adreno (TM) 540");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -228,7 +215,7 @@ TEST(SanitizeRenderer, TestCiAndroid)
 TEST(SanitizeRenderer, TestCiLinux)
 {
   const std::string renderer("llvmpipe (LLVM 10.0.0, 256 bits)");
-  const std::string expectation("llvmpipe, or similar");
+  const std::string expectation("llvmpipe");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -236,7 +223,7 @@ TEST(SanitizeRenderer, TestCiLinux)
 TEST(SanitizeRenderer, TestCiMac)
 {
   const std::string renderer("Intel(R) UHD Graphics 630");
-  const std::string expectation("Intel(R) HD Graphics 400, or similar");
+  const std::string expectation("Intel(R) HD Graphics 400");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -244,7 +231,7 @@ TEST(SanitizeRenderer, TestCiMac)
 TEST(SanitizeRenderer, TestCiMac2)
 {
   const std::string renderer("Apple M1");
-  const std::string expectation("Apple M1, or similar");
+  const std::string expectation("Apple M1");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
@@ -255,8 +242,7 @@ TEST(SanitizeRenderer, TestCiWindows)
       "ANGLE (NVIDIA, NVIDIA Tesla M60 Direct3D11 vs_5_0 ps_5_0, "
       "D3D11-23.21.13.9181)");
   const std::string expectation(
-      "ANGLE (NVIDIA, NVIDIA GeForce 8800 GTX Direct3D11 vs_5_0 ps_5_0), or "
-      "similar");
+      "ANGLE (NVIDIA, NVIDIA GeForce 8800 GTX Direct3D11 vs_5_0 ps_5_0)");
   const auto sanitized = mozilla::webgl::SanitizeRenderer(renderer);
   EXPECT_EQ(sanitized, expectation);
 }
