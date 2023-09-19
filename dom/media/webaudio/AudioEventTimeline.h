82be9d2014c32a0a726b0959fd230dcc9c45ef05
Karl Tomlinson <karlt+@karlt.net>
2023-09-19 07:56:26 +0000

    Bug 1853630 use last value of previous event for start of SetTargetAtTime() curves r=padenot
    Previously this was happening only after CleanupEventsOlderThan() had been
    called between blocks.
    
    t == t0 means the exponential is 1 and the curve uses the value from the curve
    of the previous event.
    When the time constant is zero and t == t0, the exponential argument is
    undefined, but v0 at t0 is the limit as the time constant approaches zero.
    
    Differential Revision: https://phabricator.services.mozilla.com/D188437

--- dom/media/webaudio/AudioEventTimeline.h
+++ dom/media/webaudio/AudioEventTimeline.h
@@ -114,9 +114,7 @@ inline int64_t AudioTimelineEvent::Time<int64_t>() const {
 class AudioEventTimeline {
  public:
   explicit AudioEventTimeline(float aDefaultValue)
-      : mValue(aDefaultValue),
-        mComputedValue(aDefaultValue),
-        mLastComputedValue(aDefaultValue) {}
+      : mValue(aDefaultValue), mSetTargetStartValue(aDefaultValue) {}
 
   bool ValidateEvent(const AudioTimelineEvent& aEvent, ErrorResult& aRv) const {
     MOZ_ASSERT(NS_IsMainThread());
@@ -238,7 +236,7 @@ class AudioEventTimeline {
   void SetValue(float aValue) {
     // Silently don't change anything if there are any events
     if (mEvents.IsEmpty()) {
-      mLastComputedValue = mComputedValue = mValue = aValue;
+      mSetTargetStartValue = mValue = aValue;
     }
   }
 
@@ -339,7 +337,7 @@ class AudioEventTimeline {
   void CleanupEventsOlderThan(TimeType aTime) {
     while (mEvents.Length() > 1 && aTime > mEvents[1].Time<TimeType>()) {
       if (mEvents[1].mType == AudioTimelineEvent::SetTarget) {
-        mLastComputedValue = GetValuesAtTimeHelperInternal(
+        mSetTargetStartValue = GetValuesAtTimeHelperInternal(
             mEvents[1].Time<TimeType>(), &mEvents[0], nullptr);
       }
 
@@ -357,7 +355,8 @@ class AudioEventTimeline {
                              const size_t aSize);
 
   template <class TimeType>
-  float GetValueAtTimeOfEvent(const AudioTimelineEvent* aNext);
+  float GetValueAtTimeOfEvent(const AudioTimelineEvent* aEvent,
+                              const AudioTimelineEvent* aPrevious);
 
   template <class TimeType>
   float GetValuesAtTimeHelperInternal(TimeType aTime,
@@ -375,11 +374,9 @@ class AudioEventTimeline {
   // array ends up being a bottleneck.
   nsTArray<AudioTimelineEvent> mEvents;
   float mValue;
-  // This is the value of this AudioParam we computed at the last tick.
-  float mComputedValue;
-  // This is the value of this AudioParam at the last tick of the previous
-  // event.
-  float mLastComputedValue;
+  // This is the value of this AudioParam at the end of the previous
+  // event for SetTarget curves.
+  float mSetTargetStartValue;
 };
 
 }  // namespace dom
