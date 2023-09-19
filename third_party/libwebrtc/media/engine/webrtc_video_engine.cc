9e19b1d877069755a594f5be85aa12cb35ff44e9
Dan Baker <dbaker@mozilla.com>
2023-09-19 12:04:15 -0600

    Bug 1851693 - Vendor libwebrtc from 682755e49e
    Upstream commit: https://webrtc.googlesource.com/src/+/682755e49e786d230b23f49f1df157656b8a45b9
        Do not support frame tracking id extension in production
    
        Pushing it to the list of extensions to negotiate could result
        in enabling it in production.
    
        BUG=None
    
        Change-Id: I98599e9fbac7e2b81b3f2ad0c7759bb052d9d9d1
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/306101
        Commit-Queue: Philipp Hancke <phancke@microsoft.com>
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40250}

--- third_party/libwebrtc/media/engine/webrtc_video_engine.cc
+++ third_party/libwebrtc/media/engine/webrtc_video_engine.cc
@@ -912,12 +912,11 @@ WebRtcVideoEngine::GetRtpHeaderExtensions() const {
           ? webrtc::RtpTransceiverDirection::kSendRecv
           : webrtc::RtpTransceiverDirection::kStopped);
 
-  result.emplace_back(
-      webrtc::RtpExtension::kVideoFrameTrackingIdUri, id++,
-      IsEnabled(trials_, "WebRTC-VideoFrameTrackingIdAdvertised")
-          ? webrtc::RtpTransceiverDirection::kSendRecv
-          : webrtc::RtpTransceiverDirection::kStopped);
-
+  // VideoFrameTrackingId is a test-only extension.
+  if (IsEnabled(trials_, "WebRTC-VideoFrameTrackingIdAdvertised")) {
+    result.emplace_back(webrtc::RtpExtension::kVideoFrameTrackingIdUri, id++,
+                        webrtc::RtpTransceiverDirection::kSendRecv);
+  }
   return result;
 }
 
