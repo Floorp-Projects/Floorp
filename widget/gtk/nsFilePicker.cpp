665719f7167d8edbf2225466ce663ea458fc5dec
Mike Conley <mconley@mozilla.com>
2023-09-19 19:44:51 +0000

    Bug 1852839 - Part 2: Implement nsIFilePicker.isModeSupported for the GTK backend. r=emilio
    Folder selection is only possible in newer versions of xdg-desktop-portal, so
    we implement isModeSupported so that we can check for this capability at runtime.
    
    Differential Revision: https://phabricator.services.mozilla.com/D188359

--- widget/gtk/nsFilePicker.cpp
+++ widget/gtk/nsFilePicker.cpp
@@ -10,6 +10,7 @@
 #include <unistd.h>
 
 #include "mozilla/Types.h"
+#include "AsyncDBus.h"
 #include "nsGtkUtils.h"
 #include "nsIFileURL.h"
 #include "nsIGIOService.h"
@@ -19,6 +20,7 @@
 #include "nsIStringBundle.h"
 #include "mozilla/Components.h"
 #include "mozilla/Preferences.h"
+#include "mozilla/dom/Promise.h"
 
 #include "nsArrayEnumerator.h"
 #include "nsEnumeratorUtils.h"
@@ -41,6 +43,7 @@ extern mozilla::LazyLogModule gWidgetLog;
 #endif /* MOZ_LOGGING */
 
 using namespace mozilla;
+using mozilla::dom::Promise;
 
 #define MAX_PREVIEW_SIZE 180
 // bug 1184009
@@ -230,6 +233,71 @@ void nsFilePicker::ReadValuesFromFileChooser(void* file_chooser) {
 void nsFilePicker::InitNative(nsIWidget* aParent, const nsAString& aTitle) {
   mParentWidget = aParent;
   mTitle.Assign(aTitle);
+}
+
+NS_IMETHODIMP
+nsFilePicker::IsModeSupported(nsIFilePicker::Mode aMode, JSContext* aCx,
+                              Promise** aRetPromise) {
+#ifdef MOZ_ENABLE_DBUS
+  if (!widget::ShouldUsePortal(widget::PortalKind::FilePicker) ||
+      aMode != nsIFilePicker::modeGetFolder) {
+    return nsBaseFilePicker::IsModeSupported(aMode, aCx, aRetPromise);
+  }
+
+  const char kFreedesktopPortalName[] = "org.freedesktop.portal.Desktop";
+  const char kFreedesktopPortalPath[] = "/org/freedesktop/portal/desktop";
+  const char kFreedesktopPortalFileChooser[] =
+      "org.freedesktop.portal.FileChooser";
+
+  MOZ_ASSERT(aCx);
+  MOZ_ASSERT(aRetPromise);
+
+  nsIGlobalObject* globalObject = xpc::CurrentNativeGlobal(aCx);
+  if (NS_WARN_IF(!globalObject)) {
+    return NS_ERROR_FAILURE;
+  }
+
+  ErrorResult result;
+  RefPtr<Promise> retPromise = Promise::Create(globalObject, result);
+  if (NS_WARN_IF(result.Failed())) {
+    return result.StealNSResult();
+  }
+
+  widget::CreateDBusProxyForBus(
+      G_BUS_TYPE_SESSION,
+      GDBusProxyFlags(G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS),
+      /* aInterfaceInfo = */ nullptr, kFreedesktopPortalName,
+      kFreedesktopPortalPath, kFreedesktopPortalFileChooser)
+      ->Then(
+          GetCurrentSerialEventTarget(), __func__,
+          [retPromise](RefPtr<GDBusProxy>&& aProxy) {
+            const char kFreedesktopPortalVersionProperty[] = "version";
+            // Folder selection was added in version 3 of xdg-desktop-portal
+            const uint32_t kFreedesktopPortalMinimumVersion = 3;
+            uint32_t foundVersion = 0;
+
+            RefPtr<GVariant> property =
+                dont_AddRef(g_dbus_proxy_get_cached_property(
+                    aProxy, kFreedesktopPortalVersionProperty));
+
+            if (property) {
+              foundVersion = g_variant_get_uint32(property);
+              LOG(("Found portal version: %u", foundVersion));
+            }
+
+            retPromise->MaybeResolve(foundVersion >=
+                                     kFreedesktopPortalMinimumVersion);
+          },
+          [retPromise](GUniquePtr<GError>&& aError) {
+            g_printerr("Failed to create DBUS proxy: %s\n", aError->message);
+            retPromise->MaybeReject(NS_ERROR_FAILURE);
+          });
+
+  retPromise.forget(aRetPromise);
+  return NS_OK;
+#else
+  return nsBaseFilePicker::IsModeSupported(aMode, aCx, aRetPromise);
+#endif
 }
 
 NS_IMETHODIMP
