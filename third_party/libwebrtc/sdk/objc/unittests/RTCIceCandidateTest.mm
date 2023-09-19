9420263a98bc3d10e10ddfa934ed0fe316683991
Dan Baker <dbaker@mozilla.com>
2023-09-19 14:29:53 -0600

    Bug 1851693 - Vendor libwebrtc from e5ee43787a
    Upstream commit: https://webrtc.googlesource.com/src/+/e5ee43787af67dcc43ff930c19427dd801a58fcb
        Fix candidate leak with initWithNativeCandidate.
    
        [RTCIceCandidate initWithNativeCandidate:] does not take ownership on
        candidate, so it must be released by caller.
    
        Bug: None
        Change-Id: I516e740e81a7aec04556f5fa71cbbecf3be6deb7
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/308500
        Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
        Commit-Queue: Kári Helgason <kthelgason@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40314}

--- third_party/libwebrtc/sdk/objc/unittests/RTCIceCandidateTest.mm
+++ third_party/libwebrtc/sdk/objc/unittests/RTCIceCandidateTest.mm
@@ -46,11 +46,12 @@
   std::string sdp("candidate:4025901590 1 udp 2122265343 "
                   "fdff:2642:12a6:fe38:c001:beda:fcf9:51aa "
                   "59052 typ host generation 0");
-  webrtc::IceCandidateInterface *nativeCandidate =
-      webrtc::CreateIceCandidate("audio", 0, sdp, nullptr);
+  std::unique_ptr<webrtc::IceCandidateInterface> nativeCandidate(
+      webrtc::CreateIceCandidate("audio", 0, sdp, nullptr));
 
   RTC_OBJC_TYPE(RTCIceCandidate) *iceCandidate =
-      [[RTC_OBJC_TYPE(RTCIceCandidate) alloc] initWithNativeCandidate:nativeCandidate];
+      [[RTC_OBJC_TYPE(RTCIceCandidate) alloc] initWithNativeCandidate:nativeCandidate.get()];
+  EXPECT_NE(nativeCandidate.get(), iceCandidate.nativeCandidate.get());
   EXPECT_TRUE([@"audio" isEqualToString:iceCandidate.sdpMid]);
   EXPECT_EQ(0, iceCandidate.sdpMLineIndex);
 
