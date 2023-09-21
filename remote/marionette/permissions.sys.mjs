de2558d8dc6e9d5fddfa0c45b28669e80997c28d
Harshit Sohaney <hsohaney@mozilla.com>
2023-09-21 01:48:46 +0000

    Bug 1835920 - Adding support for set_permission for storage-access-api wpt. r=webdriver-reviewers,bvandersloot,jdescottes,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D183890

--- remote/marionette/permissions.sys.mjs
+++ remote/marionette/permissions.sys.mjs
@@ -5,6 +5,7 @@
 const lazy = {};
 
 ChromeUtils.defineESModuleGetters(lazy, {
+  assert: "chrome://remote/content/shared/webdriver/Assert.sys.mjs",
   error: "chrome://remote/content/shared/webdriver/Errors.sys.mjs",
   MarionettePrefs: "chrome://remote/content/marionette/prefs.sys.mjs",
 });
@@ -22,21 +23,24 @@ export const permissions = {};
  *     State of the permission. It can be `granted`, `denied` or `prompt`.
  * @param {boolean} oneRealm
  *     Currently ignored
- *
+ * @param {browsingContext=} thirdPartyBrowsingContext
+ *     3rd party browsing context object
+ * @param {browsingContext=} topLevelBrowsingContext
+ *     Top level browsing context object
  * @throws {UnsupportedOperationError}
  *     If `marionette.setpermission.enabled` is not set or
  *     an unsupported permission is used.
  */
-permissions.set = function (descriptor, state, oneRealm) {
+permissions.set = function (
+  descriptor,
+  state,
+  oneRealm,
+  thirdPartyBrowsingContext,
+  topLevelBrowsingContext
+) {
   if (!lazy.MarionettePrefs.setPermissionEnabled) {
     throw new lazy.error.UnsupportedOperationError(
       "'Set Permission' is not available"
-    );
-  }
-
-  if (state === "prompt") {
-    throw new lazy.error.UnsupportedOperationError(
-      "'Set Permission' doesn't support prompt"
     );
   }
 
@@ -45,6 +49,12 @@ permissions.set = function (descriptor, state, oneRealm) {
   // that use `set_permission()` not fail.
   // Each test needs the corresponding testing pref to make it actually work.
   const { name } = descriptor;
+
+  if (state === "prompt" && name !== "storage-access") {
+    throw new lazy.error.UnsupportedOperationError(
+      "'Set Permission' doesn't support prompt"
+    );
+  }
   if (["clipboard-write", "clipboard-read"].includes(name)) {
     if (
       Services.prefs.getBoolPref("dom.events.testing.asyncClipboard", false)
@@ -61,6 +71,49 @@ permissions.set = function (descriptor, state, oneRealm) {
     throw new lazy.error.UnsupportedOperationError(
       "'Set Permission' expected notification.prompt.testing to be set"
     );
+  } else if (name === "storage-access") {
+    // Check if browsing context has a window global object
+    lazy.assert.open(thirdPartyBrowsingContext);
+    lazy.assert.open(topLevelBrowsingContext);
+
+    const thirdPartyURI =
+      thirdPartyBrowsingContext.currentWindowGlobal.documentURI;
+    const topLevelURI = topLevelBrowsingContext.currentWindowGlobal.documentURI;
+
+    const thirdPartyPrincipalSite = Services.eTLD.getSite(thirdPartyURI);
+
+    const topLevelPrincipal =
+      Services.scriptSecurityManager.createContentPrincipal(topLevelURI, {});
+
+    switch (state) {
+      case "granted": {
+        Services.perms.addFromPrincipal(
+          topLevelPrincipal,
+          "3rdPartyFrameStorage^" + thirdPartyPrincipalSite,
+          Services.perms.ALLOW_ACTION
+        );
+        return;
+      }
+      case "denied": {
+        Services.perms.addFromPrincipal(
+          topLevelPrincipal,
+          "3rdPartyFrameStorage^" + thirdPartyPrincipalSite,
+          Services.perms.DENY_ACTION
+        );
+        return;
+      }
+      case "prompt": {
+        Services.perms.removeFromPrincipal(
+          topLevelPrincipal,
+          "3rdPartyFrameStorage^" + thirdPartyPrincipalSite
+        );
+        return;
+      }
+      default:
+        throw new lazy.error.UnsupportedOperationError(
+          `'Set Permission' did not work for storage-access'`
+        );
+    }
   }
 
   throw new lazy.error.UnsupportedOperationError(
