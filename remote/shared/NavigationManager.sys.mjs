f3a02467aada82ab4bea1a895400cfb95b955733
Julian Descottes <jdescottes@mozilla.com>
2023-09-22 15:33:52 +0000

    Bug 1756595 - [remote] NavigationManager should not emit navigation-started/stopped for same document navigations r=webdriver-reviewers,Sasha,whimboo
    Depends on D163993
    
    Differential Revision: https://phabricator.services.mozilla.com/D188950

--- remote/shared/NavigationManager.sys.mjs
+++ remote/shared/NavigationManager.sys.mjs
@@ -157,11 +157,8 @@ class NavigationRegistry extends EventEmitter {
     const navigation = { finished: true, navigationId, url };
     this.#navigations.set(navigable, navigation);
 
-    // Same document navigations are immediately done, fire both started and
-    // stopped events immediately, on top of a dedicated event.
-    this.emit("navigation-started", { navigationId, navigableId, url });
+    // Same document navigations are immediately done, fire a single event.
     this.emit("location-changed", { navigationId, navigableId, url });
-    this.emit("navigation-stopped", { navigationId, navigableId, url });
 
     return navigation;
   }
