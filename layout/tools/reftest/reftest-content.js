f0146ff0e14f64775ba5ce444874b2acbfafd1b1
Mark Banner <standard8@mozilla.com>
2023-09-24 10:38:54 +0000

    Bug 1596056 - Remove editor modelines from reftest harness files. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D189056

--- layout/tools/reftest/reftest-content.js
+++ layout/tools/reftest/reftest-content.js
@@ -1,5 +1,3 @@
-/* -*- indent-tabs-mode: nil; js-indent-level: 4 -*- /
-/* vim: set shiftwidth=4 tabstop=8 autoindent cindent expandtab: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
