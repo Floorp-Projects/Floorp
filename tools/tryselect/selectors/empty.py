b5836e90e93c4b3b7fd8db7ed6a81f4d91886def
Julien Cristau <jcristau@mozilla.com>
2023-09-25 11:52:24 +0000

    Bug 1854364 - add common options to "empty" try selector. r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D188824

--- tools/tryselect/selectors/empty.py
+++ tools/tryselect/selectors/empty.py
@@ -10,14 +10,31 @@ from ..push import generate_try_task_config, push_to_try
 class EmptyParser(BaseTryParser):
     name = "empty"
     common_groups = ["push"]
+    task_configs = [
+        "artifact",
+        "browsertime",
+        "chemspill-prio",
+        "disable-pgo",
+        "env",
+        "gecko-profile",
+        "pernosco",
+        "routes",
+        "worker-overrides",
+    ]
 
 
-def run(message="{msg}", stage_changes=False, dry_run=False, closed_tree=False):
+def run(
+    message="{msg}",
+    try_config=None,
+    stage_changes=False,
+    dry_run=False,
+    closed_tree=False,
+):
     msg = 'No try selector specified, use "Add New Jobs" to select tasks.'
     return push_to_try(
         "empty",
         message.format(msg=msg),
-        try_task_config=generate_try_task_config("empty", []),
+        try_task_config=generate_try_task_config("empty", [], try_config),
         stage_changes=stage_changes,
         dry_run=dry_run,
         closed_tree=closed_tree,
