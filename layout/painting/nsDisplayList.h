c82920a1e70cd985107c0d4ea70b762b9778f0fc
Markus Stange <mstange.moz@gmail.com>
2023-09-25 23:36:28 +0000

    Bug 1853712 - Rename MustPaintOnContentSide to ShouldUseBlobRenderingForFallback (with inverted meaning). r=nical
    Content-side isn't a good name anymore, now that it's only used on the parent process main thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D188493

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -2600,11 +2600,14 @@ class nsDisplayItem {
   virtual bool NeedsGeometryUpdates() const { return false; }
 
   /**
-   * Some items such as those calling into the native themed widget machinery
-   * have to be painted on the content process. In this case it is best to avoid
-   * allocating layers that serializes and forwards the work to the compositor.
+   * When this item is rendered using fallback rendering, whether it should use
+   * blob rendering (i.e. a recording DrawTarget), as opposed to a pixel-backed
+   * DrawTarget.
+   * Some items, such as those calling into the native themed widget machinery,
+   * are more efficiently painted without blob recording. Those should return
+   * false here.
    */
-  virtual bool MustPaintOnContentSide() const { return false; }
+  virtual bool ShouldUseBlobRenderingForFallback() const { return true; }
 
   /**
    * If this has a child list where the children are in the same coordinate
@@ -4299,7 +4302,9 @@ class nsDisplayThemedBackground : public nsPaintedDisplayItem {
       layers::RenderRootStateManager* aManager,
       nsDisplayListBuilder* aDisplayListBuilder) override;
 
-  bool MustPaintOnContentSide() const override { return XRE_IsParentProcess(); }
+  bool ShouldUseBlobRenderingForFallback() const override {
+    return !XRE_IsParentProcess();
+  }
 
   /**
    * GetBounds() returns the background painting area.
@@ -4639,10 +4644,10 @@ class nsDisplayOutline final : public nsPaintedDisplayItem {
 
   NS_DISPLAY_DECL_NAME("Outline", TYPE_OUTLINE)
 
-  bool MustPaintOnContentSide() const override {
+  bool ShouldUseBlobRenderingForFallback() const override {
     MOZ_ASSERT(IsThemedOutline(),
                "The only fallback path we have is for themed outlines");
-    return XRE_IsParentProcess();
+    return !XRE_IsParentProcess();
   }
 
   bool CreateWebRenderCommands(
