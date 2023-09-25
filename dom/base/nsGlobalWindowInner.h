a3832b5182704fbc09b8ecb00b9fa80dfd4a6b06
Olli Pettay <Olli.Pettay@helsinki.fi>
2023-09-25 21:09:01 +0000

    Bug 1843477, non-virtual AddRef/Release for EventTarget and common subclasses, r=mccr8
    We might want to consider changing the setup for RefPtrTraits, so that it could work well with certain types of classes,
    but this patch is taking the approach where one needs to explicitly opt-in classes.
    
    Also, see the patch changing nsCOMPtr which makes also that to use same AddRef/Release mechanism.
    That doesn't not only give consistent behavior, but also helps with performance.
    
    Depends on D187147
    
    Differential Revision: https://phabricator.services.mozilla.com/D187148

--- dom/base/nsGlobalWindowInner.h
+++ dom/base/nsGlobalWindowInner.h
@@ -217,7 +217,8 @@ class nsGlobalWindowInner final : public mozilla::dom::EventTarget,
       mozilla::dom::WindowGlobalChild* aActor);
 
   // nsISupports
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_IMETHOD_(void) DeleteCycleCollectable() override;
 
   // nsWrapperCache
   virtual JSObject* WrapObject(JSContext* cx,
--- dom/events/EventTarget.h
+++ dom/events/EventTarget.h
@@ -52,6 +52,13 @@ class EventTarget : public nsISupports, public nsWrapperCache {
  public:
   NS_DECLARE_STATIC_IID_ACCESSOR(NS_EVENTTARGET_IID)
 
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+
+#ifndef NS_BUILD_REFCNT_LOGGING
+  MozExternalRefCountType NonVirtualAddRef();
+  MozExternalRefCountType NonVirtualRelease();
+#endif
+
   // WebIDL API
   static already_AddRefed<EventTarget> Constructor(const GlobalObject& aGlobal,
                                                    ErrorResult& aRv);
@@ -428,5 +435,22 @@ NS_DEFINE_STATIC_IID_ACCESSOR(EventTarget, NS_EVENTTARGET_IID)
 
 }  // namespace dom
 }  // namespace mozilla
+
+#ifdef NS_BUILD_REFCNT_LOGGING
+#  define NON_VIRTUAL_ADDREF_RELEASE(class_) /* Nothing */
+#else
+#  define NON_VIRTUAL_ADDREF_RELEASE(class_)                                 \
+    namespace mozilla {                                                      \
+    template <>                                                              \
+    class RefPtrTraits<class_> {                                             \
+     public:                                                                 \
+      static void Release(class_* aObject) { aObject->NonVirtualRelease(); } \
+      static void AddRef(class_* aObject) { aObject->NonVirtualAddRef(); }   \
+    };                                                                       \
+    }
+
+#endif
+
+NON_VIRTUAL_ADDREF_RELEASE(mozilla::dom::EventTarget)
 
 #endif  // mozilla_dom_EventTarget_h_
