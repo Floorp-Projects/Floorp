88bbe382ec85dd7e2fe29b359ca4187c65da2bcb
Michael Froman <mfroman@mozilla.com>
2023-09-25 15:15:02 +0000

    Bug 1854463 - verify_vendoring.sh - better detection of last patch-stack update. r=dbaker DONTBUILD
    Depends on D188880
    
    Differential Revision: https://phabricator.services.mozilla.com/D188881

--- dom/media/webrtc/third_party_build/verify_vendoring.sh
+++ dom/media/webrtc/third_party_build/verify_vendoring.sh
@@ -11,10 +11,29 @@ ERROR_HELP=""
 trap 'show_error_msg $LINENO' ERR
 
 source dom/media/webrtc/third_party_build/use_config_env.sh
+export HGPLAIN=1
 
 echo "MOZ_LIBWEBRTC_SRC: $MOZ_LIBWEBRTC_SRC"
 echo "MOZ_LIBWEBRTC_BRANCH: $MOZ_LIBWEBRTC_BRANCH"
 echo "MOZ_FASTFORWARD_BUG: $MOZ_FASTFORWARD_BUG"
+
+LAST_PATCHSTACK_UPDATE_COMMIT=`hg log --template "{node|short} {desc|firstline}\n" \
+    --include "third_party/libwebrtc/moz-patch-stack/*.patch" | head -1`
+echo "LAST_PATCHSTACK_UPDATE_COMMIT: $LAST_PATCHSTACK_UPDATE_COMMIT"
+
+TIP_SHA=`hg id -r tip | awk '{ print $1; }'`
+echo "TIP_SHA: $TIP_SHA"
+
+LAST_PATCHSTACK_UPDATE_COMMIT_SHA=`echo $LAST_PATCHSTACK_UPDATE_COMMIT \
+    | awk '{ print $1; }'`
+echo "LAST_PATCHSTACK_UPDATE_COMMIT_SHA: $LAST_PATCHSTACK_UPDATE_COMMIT_SHA"
+
+if [ "x$TIP_SHA" == "x$LAST_PATCHSTACK_UPDATE_COMMIT_SHA" ]; then
+  COMMIT_AFTER_PATCHSTACK_UPDATE="{start-commit-sha}"
+else
+  COMMIT_AFTER_PATCHSTACK_UPDATE=`hg id -r $LAST_PATCHSTACK_UPDATE_COMMIT_SHA~-1 || true`
+  echo "COMMIT_AFTER_PATCHSTACK_UPDATE: $COMMIT_AFTER_PATCHSTACK_UPDATE"
+fi
 
 # After this point:
 # * eE: All commands should succeed.
@@ -47,7 +66,7 @@ information on commits that need to be added to the patch-stack.  After
 identifying a commit or commit range from mercurial that should be added
 to the git patch-stack, the following commands should help remedy the
 situation:
-  ./mach python $SCRIPT_DIR/extract-for-git.py {start-commit-sha}::{end-commit-sha}
+  ./mach python $SCRIPT_DIR/extract-for-git.py $COMMIT_AFTER_PATCHSTACK_UPDATE::tip
   mv mailbox.patch $MOZ_LIBWEBRTC_SRC
   (cd $MOZ_LIBWEBRTC_SRC && \\
    git am mailbox.patch)
