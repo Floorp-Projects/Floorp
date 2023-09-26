91ca109d0ba916a4156b3097f923cc1b7402b611
Karl Tomlinson <karlt+@karlt.net>
2023-09-26 21:27:16 +0000

    Bug 1414444 include existing Sample count in Moof::mIndex.SetCapacity() r=kinetik
    so that AppendElement() always succeeds, as expected.
    
    A single Moof can parse multiple truns.
    
    The comment is removed because I'd prefer to keep mIndex as a FallibleTArray
    to remind anyone changing the code to keep allocations fallible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D187172

--- dom/media/mp4/MoofParser.cpp
+++ dom/media/mp4/MoofParser.cpp
@@ -783,7 +783,7 @@ Result<Ok, nsresult> Moof::ParseTrun(Box& aBox, Mvhd& aMvhd, Mdhd& aMdhd,
   nsTArray<MP4Interval<TimeUnit>> timeRanges;
   uint64_t decodeTime = *aDecodeTime;
 
-  if (!mIndex.SetCapacity(sampleCount, fallible)) {
+  if (!mIndex.SetCapacity(mIndex.Length() + sampleCount, fallible)) {
     LOG_ERROR(Moof, "Out of Memory");
     return Err(NS_ERROR_FAILURE);
   }
@@ -826,7 +826,6 @@ Result<Ok, nsresult> Moof::ParseTrun(Box& aBox, Mvhd& aMvhd, Mdhd& aMdhd,
       // because every audio sample is a keyframe.
       sample.mSync = !(sampleFlags & 0x1010000) || aIsAudio;
 
-      // FIXME: Make this infallible after bug 968520 is done.
       MOZ_ALWAYS_TRUE(mIndex.AppendElement(sample, fallible));
 
       mMdatRange = mMdatRange.Span(sample.mByteRange);
