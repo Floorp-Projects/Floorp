4be7e7e33e670b1d19c73fab790f8c58de98ac25
Jan Varga <jvarga@mozilla.com>
2023-09-26 14:51:39 +0000

    Bug 1854302 - Add testing for mozIStorageConnection::GetQuotaObjects; r=hsingh
    Differential Revision: https://phabricator.services.mozilla.com/D188793

--- dom/quota/test/gtest/QuotaManagerDependencyFixture.h
+++ dom/quota/test/gtest/QuotaManagerDependencyFixture.h
@@ -10,6 +10,7 @@
 #include "gtest/gtest.h"
 #include "mozilla/MozPromise.h"
 #include "mozilla/SpinEventLoopUntil.h"
+#include "mozilla/dom/quota/DirectoryLock.h"
 #include "mozilla/dom/quota/ForwardDecls.h"
 #include "mozilla/dom/quota/QuotaManager.h"
 
@@ -40,7 +41,7 @@ class QuotaManagerDependencyFixture : public testing::Test {
         std::bind(std::forward<Invokable>(aInvokable),
                   std::forward<Args>(aArgs)...);
     InvokeAsync(BackgroundTargetStrongRef(), __func__,
-                [boundTask = std::move(boundTask)] {
+                [boundTask = std::move(boundTask)]() mutable {
                   boundTask();
                   return BoolPromise::CreateAndResolve(true, __func__);
                 })
@@ -65,7 +66,7 @@ class QuotaManagerDependencyFixture : public testing::Test {
         std::bind(std::forward<Invokable>(aInvokable),
                   std::forward<Args>(aArgs)...);
     InvokeAsync(quotaManager->IOThread(), __func__,
-                [boundTask = std::move(boundTask)]() {
+                [boundTask = std::move(boundTask)]() mutable {
                   boundTask();
                   return BoolPromise::CreateAndResolve(true, __func__);
                 })
@@ -75,6 +76,43 @@ class QuotaManagerDependencyFixture : public testing::Test {
                });
 
     SpinEventLoopUntil("Promise is fulfilled"_ns, [&done]() { return done; });
+  }
+
+  template <class Task>
+  static void PerformClientDirectoryTest(const ClientMetadata& aClientMetadata,
+                                         Task&& aTask) {
+    PerformOnBackgroundThread([clientMetadata = aClientMetadata,
+                               task = std::forward<Task>(aTask)]() mutable {
+      RefPtr<ClientDirectoryLock> directoryLock;
+
+      QuotaManager* quotaManager = QuotaManager::Get();
+      ASSERT_TRUE(quotaManager);
+
+      bool done = false;
+
+      quotaManager->OpenClientDirectory(clientMetadata)
+          ->Then(
+              GetCurrentSerialEventTarget(), __func__,
+              [&directoryLock,
+               &done](RefPtr<ClientDirectoryLock> aResolveValue) {
+                directoryLock = std::move(aResolveValue);
+
+                done = true;
+              },
+              [&done](const nsresult aRejectValue) {
+                ASSERT_TRUE(false);
+
+                done = true;
+              });
+
+      SpinEventLoopUntil("Promise is fulfilled"_ns, [&done]() { return done; });
+
+      ASSERT_TRUE(directoryLock);
+
+      PerformOnIOThread(std::move(task), directoryLock->Id());
+
+      directoryLock = nullptr;
+    });
   }
 
   static const nsCOMPtr<nsISerialEventTarget>& BackgroundTargetStrongRef() {
