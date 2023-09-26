8bcd6c953f38ea937fe3b0054f5422ea653fa26d
Paul Zuehlcke <pbz@mozilla.com>
2023-09-26 09:20:24 +0000

    Bug 1846498 - Reset PBM feature tests. r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D187776

---
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_resetPBM.js
@@ -0,0 +1,471 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const PREF_ID_ALWAYS_ASK =
+  "browser.privatebrowsing.resetPBM.showConfirmationDialog";
+
+const SELECTOR_TOOLBAR_BUTTON = "#reset-pbm-toolbar-button";
+
+const SELECTOR_PANELVIEW = "panel #reset-pbm-panel";
+const SELECTOR_PANEL_HEADING = "#reset-pbm-panel-header > description";
+const SELECTOR_PANEL_DESCRIPTION = "#reset-pbm-panel-description";
+const SELECTOR_PANEL_CHECKBOX = "#reset-pbm-panel-checkbox";
+const SELECTOR_PANEL_CONFIRM_BTN = "#reset-pbm-panel-confirm-button";
+const SELECTOR_PANEL_CANCEL_BTN = "#reset-pbm-panel-cancel-button";
+
+const SELECTOR_PANEL_COMPLETION_TOAST = "#confirmation-hint";
+
+/**
+ * Wait for the reset pbm confirmation panel to open. May also be called if the
+ * panel is already open.
+ * @param {ChromeWindow} win - Chrome window in which the panel is embedded.
+ * @returns {Promise} - Promise which resolves once the panel has been shown.
+ * Resolves directly if the panel is already visible.
+ */
+async function waitForConfirmPanelShow(win) {
+  // Check for the panel, if it's not present yet wait for it to be inserted.
+  let panelview = win.document.querySelector(SELECTOR_PANELVIEW);
+  if (!panelview) {
+    let navToolbox = win.document.getElementById("navigator-toolbox");
+    await BrowserTestUtils.waitForMutationCondition(
+      navToolbox,
+      { childList: true, subtree: true },
+      () => {
+        panelview = win.document.querySelector(SELECTOR_PANELVIEW);
+        return !!panelview;
+      }
+    );
+  }
+
+  // Panel already visible, we can exit early.
+  if (BrowserTestUtils.is_visible(panelview)) {
+    return;
+  }
+
+  // Wait for panel shown event.
+  await BrowserTestUtils.waitForEvent(panelview.closest("panel"), "popupshown");
+}
+
+/**
+ * Hides the completion toast which is shown after the reset action has been
+ * completed.
+ * @param {ChromeWindow} win - Chrome window the toast is shown in.
+ */
+async function hideCompletionToast(win) {
+  let promiseHidden = BrowserTestUtils.waitForEvent(
+    win.ConfirmationHint._panel,
+    "popuphidden"
+  );
+
+  win.ConfirmationHint._panel.hidePopup();
+
+  await promiseHidden;
+}
+
+/**
+ * Trigger the reset pbm toolbar button which may open the confirm panel in the
+ * given window.
+ * @param {nsIDOMWindow} win - PBM window to trigger the button in.
+ * @param {boolean} [expectPanelOpen] - After the button action: whether the
+ * panel is expected to open (true) or remain closed (false).
+ * @returns {Promise} - Promise which resolves once the button has been
+ * triggered and, if applicable, the panel has been opened.
+ */
+async function triggerResetBtn(win, expectPanelOpen = true) {
+  Assert.ok(
+    PrivateBrowsingUtils.isWindowPrivate(win),
+    "Window to open panel is in PBM."
+  );
+
+  let shownPromise;
+  if (expectPanelOpen) {
+    shownPromise = waitForConfirmPanelShow(win);
+  }
+
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    SELECTOR_TOOLBAR_BUTTON,
+    {},
+    win.browsingContext
+  );
+
+  // Promise may not be defined at this point in which case this is a no-op.
+  await shownPromise;
+}
+
+/**
+ * Provides a promise that resolves once the reset confirmation panel has been hidden.
+ * @param nsIDOMWindow win - Chrome window that has the panel.
+ * @returns {Promise}
+ */
+function waitForConfirmPanelHidden(win) {
+  return BrowserTestUtils.waitForEvent(
+    win.document.querySelector(SELECTOR_PANELVIEW).closest("panel"),
+    "popuphidden"
+  );
+}
+
+/**
+ * Provides a promise that resolves once the completion toast has been shown.
+ * @param nsIDOMWindow win - Chrome window that has the panel.
+ * @returns {Promise}
+ */
+function waitForCompletionToastShown(win) {
+  // Init the confirmation hint panel so we can listen for show events.
+  win.ConfirmationHint._ensurePanel();
+  return BrowserTestUtils.waitForEvent(
+    win.document.querySelector(SELECTOR_PANEL_COMPLETION_TOAST),
+    "popupshown"
+  );
+}
+
+/**
+ * Wait for private browsing data clearing to be triggered.
+ * Clearing is not guaranteed to be done at this point. Bug 1846494 will add a
+ * promise based mechanism and potentially a new triggering method for clearing,
+ * at which point this helper should be updated.
+ * @returns {Promise} Promise which resolves when the last-pb-context-exited
+ * message has been dispatched.
+ */
+function waitForPBMDataClear() {
+  return TestUtils.topicObserved("last-pb-context-exited");
+}
+
+/**
+ * Test panel visibility.
+ * @param {nsIDOMWindow} win - Chrome window which is the parent of the panel.
+ * @param {string} selector - Query selector for the panel.
+ * @param {boolean} expectVisible - Whether the panel should be visible (true) or invisible or not present (false).
+ */
+function assertPanelVisibility(win, selector, expectVisible) {
+  let panelview = win.document.querySelector(selector);
+
+  if (expectVisible) {
+    Assert.ok(panelview, `Panelview element ${selector} should exist.`);
+    Assert.ok(
+      BrowserTestUtils.is_visible(panelview),
+      `Panelview ${selector} should be visible.`
+    );
+    return;
+  }
+
+  Assert.ok(
+    !panelview || !BrowserTestUtils.is_visible(panelview),
+    `Panelview ${selector} should be invisible or non-existent.`
+  );
+}
+
+/**
+ * Tests that the reset button is only visible in private browsing windows and
+ * when the feature is enabled.
+ */
+add_task(async function test_toolbar_button_visibility() {
+  for (let isEnabled of [false, true]) {
+    await SpecialPowers.pushPrefEnv({
+      set: [["browser.privatebrowsing.resetPBM.enabled", isEnabled]],
+    });
+
+    info(
+      "Test that the toolbar button is never visible in a normal browsing window."
+    );
+    let toolbarButtonNormalBrowsing = document.querySelector(
+      SELECTOR_TOOLBAR_BUTTON
+    );
+    Assert.equal(
+      !!toolbarButtonNormalBrowsing,
+      isEnabled,
+      "Normal browsing toolbar button element exists, depending on enabled pref state."
+    );
+    if (toolbarButtonNormalBrowsing) {
+      Assert.ok(
+        !BrowserTestUtils.is_visible(toolbarButtonNormalBrowsing),
+        "Toolbar button is not visible in normal browsing"
+      );
+    }
+
+    info(
+      "Test that the toolbar button is visible in a private browsing window, depending on enabled pref state."
+    );
+    let privateWindow = await BrowserTestUtils.openNewBrowserWindow({
+      private: true,
+    });
+
+    let toolbarButtonPrivateBrowsing = privateWindow.document.querySelector(
+      SELECTOR_TOOLBAR_BUTTON
+    );
+    Assert.equal(
+      !!toolbarButtonPrivateBrowsing,
+      isEnabled,
+      "Private browsing toolbar button element exists, depending on enabled pref state."
+    );
+    if (toolbarButtonPrivateBrowsing) {
+      Assert.equal(
+        BrowserTestUtils.is_visible(toolbarButtonPrivateBrowsing),
+        isEnabled,
+        "Toolbar button is visible in private browsing if enabled."
+      );
+    }
+
+    await BrowserTestUtils.closeWindow(privateWindow);
+
+    await SpecialPowers.popPrefEnv();
+  }
+});
+
+/**
+ * Tests the panel UI, the 'always ask' checkbox and the confirm and cancel
+ * actions.
+ */
+add_task(async function test_panel() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.privatebrowsing.resetPBM.enabled", true]],
+  });
+
+  let privateWin = await BrowserTestUtils.openNewBrowserWindow({
+    private: true,
+  });
+
+  // Panel does either not exist (because it's lazy and hasn't been opened yet),
+  // or it is invisible.
+  assertPanelVisibility(privateWin, SELECTOR_PANELVIEW, false);
+  assertPanelVisibility(privateWin, SELECTOR_PANEL_COMPLETION_TOAST, false);
+
+  info("Open the panel.");
+  await triggerResetBtn(privateWin);
+
+  info("Check that all expected elements are present and visible.");
+  [
+    SELECTOR_PANEL_HEADING,
+    SELECTOR_PANEL_DESCRIPTION,
+    SELECTOR_PANEL_CHECKBOX,
+    SELECTOR_PANEL_CONFIRM_BTN,
+    SELECTOR_PANEL_CANCEL_BTN,
+  ].forEach(elSelector => {
+    let el = privateWin.document.querySelector(elSelector);
+    Assert.ok(el, `Panel element ${elSelector} exists.`);
+    if (el) {
+      Assert.ok(
+        BrowserTestUtils.is_visible(el),
+        `Panel element ${elSelector} is visible.`
+      );
+    }
+  });
+
+  info("Inspect checkbox and pref state.");
+  let checkbox = privateWin.document.querySelector(SELECTOR_PANEL_CHECKBOX);
+  Assert.ok(
+    checkbox.checked,
+    "The 'always ask' checkbox should be checked initially."
+  );
+  Assert.ok(
+    Services.prefs.getBoolPref(
+      "browser.privatebrowsing.resetPBM.showConfirmationDialog"
+    ),
+    "The always ask pref should be true."
+  );
+
+  info("Click the checkbox to uncheck it.");
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    SELECTOR_PANEL_CHECKBOX,
+    {},
+    privateWin.browsingContext
+  );
+  Assert.ok(
+    !checkbox.checked,
+    "The 'always ask' checkbox should no longer be checked."
+  );
+  info(
+    "The pref shouldn't update after clicking the checkbox. It only updates on panel confirm."
+  );
+  Assert.ok(
+    Services.prefs.getBoolPref(
+      "browser.privatebrowsing.resetPBM.showConfirmationDialog"
+    ),
+    "The always ask pref should still be true."
+  );
+
+  info("Close the panel via cancel.");
+  let promisePanelHidden = waitForConfirmPanelHidden(privateWin);
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    SELECTOR_PANEL_CANCEL_BTN,
+    {},
+    privateWin.browsingContext
+  );
+  await promisePanelHidden;
+
+  assertPanelVisibility(privateWin, SELECTOR_PANELVIEW, false);
+  assertPanelVisibility(privateWin, SELECTOR_PANEL_COMPLETION_TOAST, false);
+
+  info("Reopen the panel.");
+  await triggerResetBtn(privateWin);
+
+  assertPanelVisibility(privateWin, SELECTOR_PANELVIEW, true);
+  assertPanelVisibility(privateWin, SELECTOR_PANEL_COMPLETION_TOAST, false);
+  Assert.ok(
+    checkbox.checked,
+    "The 'always ask' checkbox should be checked again."
+  );
+  Assert.ok(
+    Services.prefs.getBoolPref(PREF_ID_ALWAYS_ASK),
+    "The always ask pref should be true."
+  );
+
+  info("Test the checkbox on confirm.");
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    SELECTOR_PANEL_CHECKBOX,
+    {},
+    privateWin.browsingContext
+  );
+  Assert.ok(
+    !checkbox.checked,
+    "The 'always ask' checkbox should no longer be checked."
+  );
+
+  info("Close the panel via confirm.");
+  let promiseDataCleared = waitForPBMDataClear();
+  promisePanelHidden = waitForConfirmPanelHidden(privateWin);
+  let promiseCompletionToastShown = waitForCompletionToastShown(privateWin);
+
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    SELECTOR_PANEL_CONFIRM_BTN,
+    {},
+    privateWin.browsingContext
+  );
+  await promisePanelHidden;
+  await promiseCompletionToastShown;
+  assertPanelVisibility(privateWin, SELECTOR_PANELVIEW, false);
+  assertPanelVisibility(privateWin, SELECTOR_PANEL_COMPLETION_TOAST, true);
+
+  await promiseDataCleared;
+
+  Assert.ok(
+    !Services.prefs.getBoolPref(PREF_ID_ALWAYS_ASK),
+    "The always ask pref should now be false."
+  );
+
+  // Need to hide the completion toast, otherwise the next click on the toolbar
+  // button won't work.
+  info("Hide the completion toast.");
+  await hideCompletionToast(privateWin);
+
+  info(
+    "Simulate a click on the toolbar button. This time the panel should not open - we have unchecked 'always ask'."
+  );
+  promiseDataCleared = waitForPBMDataClear();
+  promiseCompletionToastShown = waitForCompletionToastShown(privateWin);
+
+  await triggerResetBtn(privateWin, false);
+
+  info("Waiting for PBM session to end.");
+  await promiseDataCleared;
+  info("Data has been cleared.");
+
+  assertPanelVisibility(privateWin, SELECTOR_PANELVIEW, false);
+
+  info("Waiting for the completion toast to show.");
+  await promiseCompletionToastShown;
+
+  assertPanelVisibility(privateWin, SELECTOR_PANELVIEW, false);
+  assertPanelVisibility(privateWin, SELECTOR_PANEL_COMPLETION_TOAST, true);
+
+  await BrowserTestUtils.closeWindow(privateWin);
+  Services.prefs.clearUserPref(PREF_ID_ALWAYS_ASK);
+});
+
+/**
+ * Tests that the reset action closes all other private browsing windows and
+ * tabs and triggers private browsing data clearing.
+ */
+add_task(async function test_reset_action() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.privatebrowsing.resetPBM.enabled", true]],
+  });
+
+  info("Open a few private browsing windows.");
+  let windowOpenPromises = [];
+  for (let i = 0; i < 3; i += 1) {
+    windowOpenPromises.push(
+      BrowserTestUtils.openNewBrowserWindow({
+        private: true,
+      })
+    );
+  }
+
+  info(
+    "Open an additional normal browsing window. It should remain open on reset PBM action."
+  );
+  let additionalNormalWindow = await BrowserTestUtils.openNewBrowserWindow({
+    private: false,
+  });
+
+  info(
+    "Wait for all windows to open and use one to trigger the PBM restart action."
+  );
+  let privateBrowsingWindows = await Promise.all(windowOpenPromises);
+  let [win] = privateBrowsingWindows;
+  win.focus();
+
+  Assert.ok(
+    PrivateBrowsingUtils.isWindowPrivate(win),
+    "Window for PBM reset trigger is private window."
+  );
+
+  info("Load a bunch of tabs in the private window.");
+  let loadPromises = [
+    "https://example.com",
+    "https://example.org",
+    "https://example.net",
+  ].map(async url => {
+    let tab = BrowserTestUtils.addTab(win.gBrowser, url);
+    await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+  });
+  await Promise.all(loadPromises);
+
+  info("Switch to the last tab.");
+  win.gBrowser.selectedTab = win.gBrowser.tabs[win.gBrowser.tabs.length - 1];
+  Assert.ok(
+    win.gBrowser.selectedBrowser.currentURI.spec != "about:privatebrowsing",
+    "The selected tab should not show about:privatebrowsing."
+  );
+
+  let windowClosePromises = [
+    ...privateBrowsingWindows.filter(w => win != w),
+  ].map(w => BrowserTestUtils.windowClosed(w));
+
+  // Create promises for tab close for all tabs in the triggering private browsing window.
+  let promisesTabsClosed = win.gBrowser.tabs.map(tab =>
+    BrowserTestUtils.waitForTabClosing(tab)
+  );
+
+  info("Trigger the restart PBM action");
+
+  let promiseDataClear = waitForPBMDataClear();
+  await ResetPBMPanel._restartPBM(win);
+
+  info(
+    "Wait for all the windows but the default normal window and the private window which triggered the reset action to be closed."
+  );
+  await Promise.all(windowClosePromises);
+
+  info("Wait for tabs in the trigger private window to close.");
+  await Promise.all(promisesTabsClosed);
+
+  info("Wait for data to be cleared.");
+  await promiseDataClear;
+
+  Assert.equal(
+    win.gBrowser.tabs.length,
+    1,
+    "Should only have 1 tab remaining."
+  );
+  Assert.equal(
+    win.gBrowser.selectedBrowser.currentURI.spec,
+    "about:privatebrowsing",
+    "The remaining tab should point to about:privatebrowsing."
+  );
+
+  // Close the private window that remained open.
+  await BrowserTestUtils.closeWindow(win);
+  await BrowserTestUtils.closeWindow(additionalNormalWindow);
+});
