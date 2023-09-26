6964bead9380117cdc0552bbd23b5ed29cadf524
mcarare <48995920+mcarare@users.noreply.github.com>
2023-09-26 15:54:37 +0300

    Bug 1854936 - Send additional notification when recording devices are in use.
    A second notification is sent if the first is dismissed, on Android 14+.

--- mobile/android/android-components/components/feature/media/src/main/java/mozilla/components/feature/media/middleware/RecordingDevicesMiddleware.kt
+++ mobile/android/android-components/components/feature/media/src/main/java/mozilla/components/feature/media/middleware/RecordingDevicesMiddleware.kt
@@ -5,11 +5,14 @@
 package mozilla.components.feature.media.middleware
 
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.annotation.VisibleForTesting
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import mozilla.components.browser.state.action.BrowserAction
 import mozilla.components.browser.state.action.ContentAction
 import mozilla.components.browser.state.action.CustomTabListAction
@@ -21,13 +24,19 @@ import mozilla.components.feature.media.notification.MediaNotificationChannel
 import mozilla.components.lib.state.Middleware
 import mozilla.components.lib.state.MiddlewareContext
 import mozilla.components.support.base.android.NotificationsDelegate
+import mozilla.components.support.base.android.OnPermissionGranted
 import mozilla.components.support.base.ids.SharedIdsHelper
 import mozilla.components.support.utils.PendingIntentUtils
+import mozilla.components.support.utils.ThreadUtils
+import mozilla.components.support.utils.ext.registerReceiverCompat
 import mozilla.components.ui.icons.R as iconsR
 
 private const val NOTIFICATION_TAG = "mozac.feature.media.recordingDevices"
 private const val NOTIFICATION_ID = 1
 private const val PENDING_INTENT_TAG = "mozac.feature.media.pendingintent"
+private const val ACTION_RECORDING_DEVICES_NOTIFICATION_DISMISSED =
+    "mozac.feature.media.recordingDevices.notificationDismissed"
+private const val NOTIFICATION_REMINDER_DELAY_MS = 5 * 1000L
 
 /**
  * Middleware for displaying an ongoing notification while recording devices (camera, microphone)
@@ -53,12 +62,15 @@ class RecordingDevicesMiddleware(
             action is TabListAction ||
             action is CustomTabListAction
         ) {
-            process(context.state)
+            process(context, false)
         }
     }
 
-    private fun process(state: BrowserState) {
-        val devices = state.tabs
+    private fun process(
+        middlewareContext: MiddlewareContext<BrowserState, BrowserAction>,
+        isReminder: Boolean,
+    ) {
+        val devices = middlewareContext.state.tabs
             .map { tab -> tab.content.recordingDevices }
             .flatten()
             .filter { device -> device.status == RecordingDevice.Status.RECORDING }
@@ -74,25 +86,57 @@ class RecordingDevicesMiddleware(
             else -> RecordingState.None
         }
 
-        updateNotification(recordingState)
+        updateNotification(
+            recordingState,
+            isReminder,
+            processRecordingState = {
+                isShowingNotification = false
+                process(middlewareContext, true)
+            },
+        )
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    internal fun updateNotification(recordingState: RecordingState) {
+    internal fun updateNotification(
+        recordingState: RecordingState,
+        isReminder: Boolean = false,
+        processRecordingState: () -> Unit = {},
+    ) {
         if (recordingState.isRecording && !isShowingNotification) {
-            showNotification(recordingState)
+            showNotification(
+                context,
+                recordingState,
+                notificationsDelegate,
+                isReminder,
+                processRecordingState,
+            ) {
+                isShowingNotification = true
+            }
         } else if (!recordingState.isRecording && isShowingNotification) {
             hideNotification()
             isShowingNotification = false
         }
     }
 
-    private fun showNotification(recordingState: RecordingState) {
+    private fun hideNotification() {
+        NotificationManagerCompat.from(context)
+            .cancel(NOTIFICATION_TAG, NOTIFICATION_ID)
+    }
+
+    private fun showNotification(
+        context: Context,
+        recordingState: RecordingState,
+        notificationsDelegate: NotificationsDelegate,
+        isReminder: Boolean = false,
+        processRecordingState: () -> Unit,
+        onPermissionGranted: OnPermissionGranted,
+    ) {
         val channelId = MediaNotificationChannel.ensureChannelExists(context)
 
-        val intent = context.packageManager.getLaunchIntentForPackage(context.packageName)?.apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        } ?: throw IllegalStateException("Package has no launcher intent")
+        val intent =
+            context.packageManager.getLaunchIntentForPackage(context.packageName)?.apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            } ?: throw IllegalStateException("Package has no launcher intent")
 
         val pendingIntent = PendingIntent.getActivity(
             context,
@@ -101,27 +145,60 @@ class RecordingDevicesMiddleware(
             PendingIntentUtils.defaultFlags or PendingIntent.FLAG_UPDATE_CURRENT,
         )
 
+        val dismissPendingIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            Intent(ACTION_RECORDING_DEVICES_NOTIFICATION_DISMISSED),
+            PendingIntentUtils.defaultFlags,
+        )
+
+        val broadcastReceiver = NotificationDismissedReceiver(processRecordingState)
+
+        context.registerReceiverCompat(
+            broadcastReceiver,
+            IntentFilter(ACTION_RECORDING_DEVICES_NOTIFICATION_DISMISSED),
+            ContextCompat.RECEIVER_NOT_EXPORTED,
+        )
+
+        val textResource = if (isReminder) {
+            context.getString(
+                recordingState.reminderTextResource,
+                context.packageManager.getApplicationLabel(context.applicationInfo).toString(),
+            )
+        } else {
+            context.getString(recordingState.textResource)
+        }
+
         val notification = NotificationCompat.Builder(context, channelId)
             .setSmallIcon(recordingState.iconResource)
             .setContentTitle(context.getString(recordingState.titleResource))
-            .setContentText(context.getString(recordingState.textResource))
+            .setContentText(textResource)
             .setPriority(NotificationCompat.PRIORITY_MAX)
             .setCategory(NotificationCompat.CATEGORY_CALL)
             .setContentIntent(pendingIntent)
             .setOngoing(true)
+            .setDeleteIntent(dismissPendingIntent)
             .build()
 
         notificationsDelegate.notify(
             NOTIFICATION_TAG,
             NOTIFICATION_ID,
             notification,
-            onPermissionGranted = { isShowingNotification = true },
+            onPermissionGranted = onPermissionGranted,
         )
     }
 
-    private fun hideNotification() {
-        NotificationManagerCompat.from(context)
-            .cancel(NOTIFICATION_TAG, NOTIFICATION_ID)
+    internal class NotificationDismissedReceiver(
+        private val processRecordingState: () -> Unit,
+    ) : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            ThreadUtils.postToMainThreadDelayed(
+                {
+                    processRecordingState.invoke()
+                },
+                NOTIFICATION_REMINDER_DELAY_MS,
+            )
+        }
     }
 }
 
@@ -129,6 +206,7 @@ internal sealed class RecordingState {
     abstract val iconResource: Int
     abstract val titleResource: Int
     abstract val textResource: Int
+    abstract val reminderTextResource: Int
 
     val isRecording
         get() = this !is None
@@ -137,18 +215,21 @@ internal sealed class RecordingState {
         override val iconResource = iconsR.drawable.mozac_ic_camera_24
         override val titleResource = R.string.mozac_feature_media_sharing_camera_and_microphone
         override val textResource = R.string.mozac_feature_media_sharing_camera_and_microphone_text
+        override val reminderTextResource = R.string.mozac_feature_media_sharing_camera_and_microphone_reminder_text
     }
 
     object Camera : RecordingState() {
         override val iconResource = iconsR.drawable.mozac_ic_camera_24
         override val titleResource = R.string.mozac_feature_media_sharing_camera
         override val textResource = R.string.mozac_feature_media_sharing_camera_text
+        override val reminderTextResource = R.string.mozac_feature_media_sharing_camera_reminder_text
     }
 
     object Microphone : RecordingState() {
         override val iconResource = iconsR.drawable.mozac_ic_microphone_24
         override val titleResource = R.string.mozac_feature_media_sharing_microphone
         override val textResource = R.string.mozac_feature_media_sharing_microphone_text
+        override val reminderTextResource = R.string.mozac_feature_media_sharing_microphone_reminder_text
     }
 
     object None : RecordingState() {
@@ -158,6 +239,8 @@ internal sealed class RecordingState {
         override val titleResource: Int
             get() = throw UnsupportedOperationException()
         override val textResource: Int
+            get() = throw UnsupportedOperationException()
+        override val reminderTextResource: Int
             get() = throw UnsupportedOperationException()
     }
 }
