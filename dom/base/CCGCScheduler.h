c086dee1c438fdc3e6850635688e585b7309bde7
serge-sans-paille <sguelton@mozilla.com>
2023-09-27 07:16:11 +0000

    Bug 1854410 - Move global ctor from dom/base/CCGCScheduler.h out of the header to avoid duplication r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D188842

--- dom/base/CCGCScheduler.h
+++ dom/base/CCGCScheduler.h
@@ -16,50 +16,45 @@
 
 namespace mozilla {
 
-static const TimeDuration kOneMinute = TimeDuration::FromSeconds(60.0f);
+extern const TimeDuration kOneMinute;
 
 // The amount of time we wait between a request to CC (after GC ran)
 // and doing the actual CC.
-static const TimeDuration kCCDelay = TimeDuration::FromSeconds(6);
+extern const TimeDuration kCCDelay;
 
-static const TimeDuration kCCSkippableDelay =
-    TimeDuration::FromMilliseconds(250);
+extern const TimeDuration kCCSkippableDelay;
 
 // In case the cycle collector isn't run at all, we don't want forget skippables
 // to run too often. So limit the forget skippable cycle to start at earliest 2
 // seconds after the end of the previous cycle.
-static const TimeDuration kTimeBetweenForgetSkippableCycles =
-    TimeDuration::FromSeconds(2);
+extern const TimeDuration kTimeBetweenForgetSkippableCycles;
 
 // ForgetSkippable is usually fast, so we can use small budgets.
 // This isn't a real budget but a hint to IdleTaskRunner whether there
 // is enough time to call ForgetSkippable.
-static const TimeDuration kForgetSkippableSliceDuration =
-    TimeDuration::FromMilliseconds(2);
+extern const TimeDuration kForgetSkippableSliceDuration;
 
 // Maximum amount of time that should elapse between incremental CC slices
-static const TimeDuration kICCIntersliceDelay =
-    TimeDuration::FromMilliseconds(64);
+extern const TimeDuration kICCIntersliceDelay;
 
 // Time budget for an incremental CC slice when using timer to run it.
-static const TimeDuration kICCSliceBudget = TimeDuration::FromMilliseconds(3);
+extern const TimeDuration kICCSliceBudget;
 // Minimum budget for an incremental CC slice when using idle time to run it.
-static const TimeDuration kIdleICCSliceBudget =
-    TimeDuration::FromMilliseconds(2);
+extern const TimeDuration kIdleICCSliceBudget;
 
 // Maximum total duration for an ICC
-static const TimeDuration kMaxICCDuration = TimeDuration::FromSeconds(2);
+extern const TimeDuration kMaxICCDuration;
 
 // Force a CC after this long if there's more than NS_CC_FORCED_PURPLE_LIMIT
 // objects in the purple buffer.
-static const TimeDuration kCCForced = kOneMinute * 2;
-static const uint32_t kCCForcedPurpleLimit = 10;
+extern const TimeDuration kCCForced;
+constexpr uint32_t kCCForcedPurpleLimit = 10;
 
 // Don't allow an incremental GC to lock out the CC for too long.
-static const TimeDuration kMaxCCLockedoutTime = TimeDuration::FromSeconds(30);
+extern const TimeDuration kMaxCCLockedoutTime;
 
 // Trigger a CC if the purple buffer exceeds this size when we check it.
-static const uint32_t kCCPurpleLimit = 200;
+constexpr uint32_t kCCPurpleLimit = 200;
 
 // Actions performed by the GCRunner state machine.
 enum class GCRunnerAction {
