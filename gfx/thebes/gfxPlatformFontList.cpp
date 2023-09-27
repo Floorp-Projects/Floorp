e02eb1fc61c4167b6c7c27cc33ccbcd6cace31bb
Jonathan Kew <jkew@mozilla.com>
2023-09-27 10:38:51 +0000

    Bug 1852596 r=gfx-reviewers,aosmond, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D189243

--- gfx/thebes/gfxPlatformFontList.cpp
+++ gfx/thebes/gfxPlatformFontList.cpp
@@ -1310,6 +1310,7 @@ class StartCmapLoadingRunnable : public mozilla::Runnable {
 };
 
 void gfxPlatformFontList::StartCmapLoadingFromFamily(uint32_t aStartIndex) {
+  AutoLock lock(mLock);
   if (aStartIndex > mStartedLoadingCmapsFrom) {
     // We already initiated cmap-loading from somewhere earlier in the list;
     // no need to do it again here.
