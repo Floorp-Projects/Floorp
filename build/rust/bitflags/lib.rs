3c185960cfe2771f0e38e68b68eb2465da4956d6
Mike Hommey <mh+mozilla@glandium.org>
2023-09-27 19:56:17 +0000

    Bug 1852209 - Switch to bitflags 2 r=supply-chain-reviewers,emilio
    We currently are using a fake bitflags 2, derived from bitflags 1. More
    and more crates are using it, and we're at a sweet spot where flipping
    things around makes sense: using a fake bitflags 1, derived from
    bitflags 2.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189316

--- build/rust/bitflags/lib.rs
+++ build/rust/bitflags/lib.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-pub use bitflags::*;
+pub use bitflags::bitflags as bitflags2;
 
 // Copy of the macro from bitflags 1.3.2, with the implicit derives
 // removed, because in 2.0, they're expected to be explicitly given
@@ -29,29 +29,16 @@ macro_rules! bitflags {
 
         $($t:tt)*
     ) => {
-        __impl_bitflags_remove_derive_debug! {
-            $(#[$($outer)+])*
+        $(#[$($outer)+])*
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        $vis struct $BitFlags($T);
 
-            $vis struct $BitFlags {
-                bits: $T,
-            }
-        }
-
-        __impl_bitflags! {
-            $BitFlags: $T {
+        bitflags2! {
+            impl $BitFlags: $T {
                 $(
                     $(#[$inner $($args)*])*
-                    $Flag = $value;
+                    const $Flag = $value;
                 )*
-            }
-        }
-
-        impl $BitFlags {
-            /// Convert from underlying bit representation, preserving all
-            /// bits (even those not corresponding to a defined flag).
-            #[inline]
-            pub const fn from_bits_retain(bits: $T) -> Self {
-                Self { bits }
             }
         }
 
@@ -60,66 +47,4 @@ macro_rules! bitflags {
         }
     };
     () => {};
-}
-
-#[macro_export(local_inner_macros)]
-#[doc(hidden)]
-macro_rules! __impl_bitflags_remove_derive_debug {
-    (
-        $(@ $(#[$keep_outer:meta])* @)?
-        #[derive(@ $($keep_derives:ident),+ @)]
-        $(#[$($other_outer:tt)+])*
-        $vis:vis struct $BitFlags:ident { $($t:tt)* }
-    ) => {
-        __impl_bitflags_remove_derive_debug! {
-            @ $($(#[$keep_outer])*)? #[derive($($keep_derives),+)] @
-            $(#[$($other_outer)+])*
-            $vis struct $BitFlags { $($t)* }
-        }
-    };
-    (
-        $(@ $(#[$keep_outer:meta])* @)?
-        #[derive($(@ $($keep_derives:ident),+ @)? Debug $(,$derives:ident)*)]
-        $(#[$($other_outer:tt)+])*
-        $vis:vis struct $BitFlags:ident { $($t:tt)* }
-    ) => {
-        __impl_bitflags_remove_derive_debug! {
-            @ $($(#[$keep_outer])*)? @
-            #[derive($(@ $($keep_derives),+ @)? $($derives),*)]
-            $(#[$($other_outer)+])*
-            $vis struct $BitFlags { $($t)* }
-        }
-    };
-    (
-        $(@ $(#[$keep_outer:meta])* @)?
-        #[derive($(@ $($keep_derives:ident),+ @)? $derive:ident $(,$derives:ident)*)]
-        $(#[$($other_outer:tt)+])*
-        $vis:vis struct $BitFlags:ident { $($t:tt)* }
-    ) => {
-        __impl_bitflags_remove_derive_debug! {
-            @ $($(#[$keep_outer])*)? @
-            #[derive(@ $($($keep_derives,)+)? $derive @ $($derives),*)]
-            $(#[$($other_outer)+])*
-            $vis struct $BitFlags { $($t)* }
-        }
-    };
-    (
-        $(@ $(#[$keep_outer:meta])* @)?
-        #[$outer:meta]
-        $(#[$($other_outer:tt)+])*
-        $vis:vis struct $BitFlags:ident { $($t:tt)* }
-    ) => {
-        __impl_bitflags_remove_derive_debug! {
-            @ $($(#[$keep_outer])*)? #[$outer] @
-            $(#[$($other_outer)+])*
-            $vis struct $BitFlags { $($t)* }
-        }
-    };
-    (
-        @ $(#[$keep_outer:meta])* @
-        $vis:vis struct $BitFlags:ident { $($t:tt)* }
-    ) => {
-        $(#[$keep_outer])*
-        $vis struct $BitFlags { $($t)* }
-    };
 }
--- third_party/rust/bitflags/README.md
+++ third_party/rust/bitflags/README.md
@@ -2,14 +2,24 @@ bitflags
 ========
 
 [![Rust](https://github.com/bitflags/bitflags/workflows/Rust/badge.svg)](https://github.com/bitflags/bitflags/actions)
-[![Join the chat at https://gitter.im/bitflags/Lobby](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/bitflags/Lobby?utm_source=badge&utm_medium=badge&utm_content=badge)
 [![Latest version](https://img.shields.io/crates/v/bitflags.svg)](https://crates.io/crates/bitflags)
 [![Documentation](https://docs.rs/bitflags/badge.svg)](https://docs.rs/bitflags)
 ![License](https://img.shields.io/crates/l/bitflags.svg)
 
-A Rust macro to generate structures which behave like a set of bitflags
+`bitflags` generates flags enums with well-defined semantics and ergonomic end-user APIs.
+
+You can use `bitflags` to:
+
+- provide more user-friendly bindings to C APIs where flags may or may not be fully known in advance.
+- generate efficient options types with string parsing and formatting support.
+
+You can't use `bitflags` to:
+
+- guarantee only bits corresponding to defined flags will ever be set. `bitflags` allows access to the underlying bits type so arbitrary bits may be set.
+- define bitfields. `bitflags` only generates types where set bits denote the presence of some combination of flags.
 
 - [Documentation](https://docs.rs/bitflags)
+- [Specification](https://github.com/bitflags/bitflags/blob/main/spec.md)
 - [Release notes](https://github.com/bitflags/bitflags/releases)
 
 ## Usage
@@ -18,7 +28,7 @@ Add this to your `Cargo.toml`:
 
 ```toml
 [dependencies]
-bitflags = "1.3"
+bitflags = "2.4.0"
 ```
 
 and this to your source code:
@@ -27,6 +37,35 @@ and this to your source code:
 use bitflags::bitflags;
 ```
 
+## Example
+
+Generate a flags structure:
+
+```rust
+use bitflags::bitflags;
+
+// The `bitflags!` macro generates `struct`s that manage a set of flags.
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    struct Flags: u32 {
+        const A = 0b00000001;
+        const B = 0b00000010;
+        const C = 0b00000100;
+        const ABC = Self::A.bits() | Self::B.bits() | Self::C.bits();
+    }
+}
+
+fn main() {
+    let e1 = Flags::A | Flags::C;
+    let e2 = Flags::B | Flags::C;
+    assert_eq!((e1 | e2), Flags::ABC);   // union
+    assert_eq!((e1 & e2), Flags::C);     // intersection
+    assert_eq!((e1 - e2), Flags::A);     // set difference
+    assert_eq!(!e2, Flags::A);           // set complement
+}
+```
+
 ## Rust Version Support
 
-The minimum supported Rust version is 1.46 due to use of associated constants and const functions.
+The minimum supported Rust version is documented in the `Cargo.toml` file.
+This may be bumped in minor releases as necessary.
