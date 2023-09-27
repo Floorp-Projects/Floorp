95a7d222ebcebd56d0432e5c924512fdf77c01ae
Ryosuke Asano <ryosukeusuusu@gmail.com>
2023-09-27 17:28:11 +0900

    [FB] Floorp Core | Remove Floorp Core

--- floorp/browser/extensions/webextensions/floorp-system/shared/gesturefy/background.js
+++
@@ -1,9 +0,0 @@
-browser.runtime.onMessage.addListener(async (val) => {
-    switch(val.type){
-        case "l10n":
-            return await browser.browserL10n.getFloorpL10nValues(val.data)
-        case "extensionsInSidebar":
-            return await browser.sidebar.getExtensionsInSidebar()
-    }
-
-})
