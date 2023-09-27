9a363a2fb8d642b9d21eb0ccda1f42b6bf716aa9
James Graham <james@hoppipolla.co.uk>
2023-09-27 12:16:32 +0000

    Bug 1854749 [wpt PR 42116] - Set original error as context when raising a different error, a=testonly
    Automatic update from web-platform-tests
    Set original error as context when raising a different error
    
    --
    
    wpt-commits: 5f0fba3215492e89b9021bf3bfb9dac63e8fd7c9
    wpt-pr: 42116

--- testing/web-platform/tests/tools/wptrunner/wptrunner/executors/base.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/executors/base.py
@@ -733,8 +733,8 @@ class CallbackHandler:
         self.logger.debug("Got async callback: %s" % result[1])
         try:
             callback = self.callbacks[command]
-        except KeyError:
-            raise ValueError("Unknown callback type %r" % result[1])
+        except KeyError as e:
+            raise ValueError("Unknown callback type %r" % result[1]) from e
         return callback(url, payload)
 
     def process_complete(self, url, payload):
@@ -747,8 +747,8 @@ class CallbackHandler:
         self.logger.debug(f"Got action: {action}")
         try:
             action_handler = self.actions[action]
-        except KeyError:
-            raise ValueError(f"Unknown action {action}")
+        except KeyError as e:
+            raise ValueError(f"Unknown action {action}") from e
         try:
             with ActionContext(self.logger, self.protocol, payload.get("context")):
                 result = action_handler(payload)
