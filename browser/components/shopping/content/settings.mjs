b4a706d04fcf2b6f139ff83dc9188770f2d400c3
Jared Hirsch <jhirsch@mozilla.com>
2023-09-27 19:17:31 +0000

    Bug 1854243 - Display shopping button even after opting out of the experiment. r=shopping-reviewers,niklas
    Differential Revision: https://phabricator.services.mozilla.com/D189271

--- browser/components/shopping/content/settings.mjs
+++ browser/components/shopping/content/settings.mjs
@@ -36,8 +36,11 @@ class ShoppingSettings extends MozLitElement {
   }
 
   onDisableShopping() {
-    RPMSetPref("browser.shopping.experience2023.optedIn", 2);
+    // Unfortunately, order matters here. As soon as we set the optedIn pref
+    // to the opted out state, the sidebar gets torn down, and the active pref
+    // is never flipped, leaving the toolbar button in the active state.
     RPMSetPref("browser.shopping.experience2023.active", false);
+    RPMSetPref("browser.shopping.experience2023.optedIn", 2);
   }
 
   fakespotLinkClicked(e) {
