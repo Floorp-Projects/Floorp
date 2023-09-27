3ab4b95656ad77e29f47f5aef7b69bc2b1c3a5d7
Weizhong Xia <77710146+WeizhongX@users.noreply.github.com>
2023-09-27 12:01:34 +0000

    Bug 1852648 [wpt PR 41924] - Support fully parallel in Wptrunner, a=testonly
    Automatic update from web-platform-tests
    Support fully parallel in Wptrunner (#41924)
    
    * Support fully parallel in Wptrunner
    
    One typical usage for devs is to run WPTs from a few local directories.
    Current grouping method tends to put tests into a few large groups,
    which limits the total number of test runners we could launched, dispite
    the number of cores the machine had.
    
    The FullyParallelGroupedSource chuck every tests into a separate group,
    thus could utilize as much as possible power the local host had. Test
    shows running ~500 tests from the same directory now takes ~ 1min,
    reduced from previous > 2 minutes.
    
    Bug: 41870
    --
    
    wpt-commits: e0f01ebf0aca4eca3bd202533bfe1fbf5c4f13f5
    wpt-pr: 41924

--- testing/web-platform/tests/tools/wptrunner/wptrunner/testloader.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/testloader.py
@@ -482,7 +482,9 @@ def get_test_src(**kwargs):
     test_source_kwargs = {"processes": kwargs["processes"],
                           "logger": kwargs["logger"]}
     chunker_kwargs = {}
-    if kwargs["run_by_dir"] is not False:
+    if kwargs["fully_parallel"]:
+        test_source_cls = FullyParallelGroupedSource
+    elif kwargs["run_by_dir"] is not False:
         # A value of None indicates infinite depth
         test_source_cls = PathGroupedSource
         test_source_kwargs["depth"] = kwargs["run_by_dir"]
@@ -638,6 +640,17 @@ class PathGroupedSource(TestSource):
     @classmethod
     def group_metadata(cls, state):
         return {"scope": "/%s" % "/".join(state["prev_group_key"][2])}
+
+
+class FullyParallelGroupedSource(PathGroupedSource):
+    # Chuck every test into a different group, so that they can run
+    # fully parallel with each other. Useful to run a lot of tests
+    # clustered in a few directories.
+    @classmethod
+    def new_group(cls, state, subsuite, test_type, test, **kwargs):
+        path = urlsplit(test.url).path.split("/")[1:-1]
+        state["prev_group_key"] = (subsuite, test_type, path)
+        return True
 
 
 class GroupFileTestSource(TestSource):
