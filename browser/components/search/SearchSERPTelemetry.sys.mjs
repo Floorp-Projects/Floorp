dbdb14f49c1081d44351ccc005c1133fefbaabc0
Stephanie Cunnane <scunnane@mozilla.com>
2023-09-28 18:11:56 +0000

    Bug 1853757 - Don't report SERP categorization for clients that haven't downloaded Remote Setting attachments. r=jteow
    Differential Revision: https://phabricator.services.mozilla.com/D188748

--- browser/components/search/SearchSERPTelemetry.sys.mjs
+++ browser/components/search/SearchSERPTelemetry.sys.mjs
@@ -1397,7 +1397,7 @@ class ContentHandler {
     if (lazy.serpEventTelemetryCategorization && telemetryState) {
       let provider = item?.info.provider;
       if (provider) {
-        SearchSERPCategorization.categorizeDomainsFromProvider(
+        SearchSERPCategorization.maybeCategorizeAndReportDomainsFromProvider(
           info.nonAdDomains,
           info.adDomains,
           provider
@@ -1416,7 +1416,9 @@ class ContentHandler {
  */
 class DomainCategorizer {
   /**
-   * Categorizes domains extracted from SERPs.
+   * Categorizes and reports domains extracted from SERPs. Note that we don't
+   * process domains if the domain-to-categories map is empty (if the client
+   * couldn't download Remote Settings attachments, for example).
    *
    * @param {Set} nonAdDomains
    *   The non-ad domains extracted from the page.
@@ -1425,15 +1427,28 @@ class DomainCategorizer {
    * @param {string} provider
    *   The provider associated with the page.
    */
-  categorizeDomainsFromProvider(nonAdDomains, adDomains, provider) {
+  maybeCategorizeAndReportDomainsFromProvider(
+    nonAdDomains,
+    adDomains,
+    provider
+  ) {
     for (let domains of [nonAdDomains, adDomains]) {
+      // We don't want to generate and report telemetry if a client was unable
+      // to download the domain-to-categories mapping from Remote Settings.
+      if (SearchSERPDomainToCategoriesMap.empty) {
+        continue;
+      }
       domains = this.processDomains(domains, provider);
       let resultsToReport = this.applyCategorizationLogic(domains);
-      this.dummyLogger(domains, resultsToReport);
+      this.dummyLogger(
+        domains,
+        resultsToReport,
+        SearchSERPDomainToCategoriesMap.version
+      );
     }
   }
 
-  // TODO: check with DS to get the final aggregation logic.
+  // TODO: check with DS to get the final aggregation logic. (Bug 1854196)
   /**
    * Applies the logic for reducing extracted domains to a single category for
    * the SERP.
@@ -1505,8 +1520,9 @@ class DomainCategorizer {
   }
 
   // TODO: replace this method once we know where to send the categorized
-  // domains and overall SERP category.
-  dummyLogger(domains, resultsToReport) {
+  // domains and overall SERP category. (Bug 1854692)
+  dummyLogger(domains, resultsToReport, version) {
+    lazy.logConsole.debug("Version of the attachments:", version);
     lazy.logConsole.debug("Domains extracted from SERP:", [...domains]);
     lazy.logConsole.debug(
       "Categorization results to report to Glean:",
@@ -1792,13 +1808,6 @@ class DomainToCategoriesMap {
 
     if (!records?.length) {
       lazy.logConsole.debug("No records found for domain-to-categories map.");
-      return;
-    }
-
-    if (!records.length) {
-      lazy.logConsole.error(
-        "No valid attachments available for domain-to-categories map."
-      );
       return;
     }
 
