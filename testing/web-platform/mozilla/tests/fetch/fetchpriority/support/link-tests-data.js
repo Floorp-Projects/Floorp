fb9061cf76dafcb186ed268bb4915aaab52dd623
Mirko Brodesser <mbrodesser@igalia.com>
2023-09-28 12:10:22 +0000

    Bug 1852754: part 7) Add tests for dynamically loading <link rel="preload"> elements. r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D189471

--- testing/web-platform/mozilla/tests/fetch/fetchpriority/support/link-tests-data.js
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/link-tests-data.js
@@ -15,6 +15,57 @@ const kExpectedRequestsOfLoadStylesheet = [
     }
 ];
 
+const kExpectedRequestsOfLinkPreload = [
+    {   fileNameAndSuffix: "dummy.txt?1",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.txt?2",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.txt?3",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.txt?4",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.font?1",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.font?2",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.font?3",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.font?4",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.image?1",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.image?2",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.image?3",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.image?4",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.css?1",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.css?2",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGHEST
+    },
+    {   fileNameAndSuffix: "dummy.css?3",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGHEST
+    },
+    {   fileNameAndSuffix: "dummy.css?4",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGHEST
+    },
+];
+
 // The expected internal priorites of the test data are specified as
 // implementation-defined. See step 11. of
 // <https://html.spec.whatwg.org/#create-a-link-request> and step 15. of
@@ -35,56 +86,10 @@ export const kTestData = [
         expectedRequests: kExpectedRequestsOfLoadStylesheet
     },
     {   testFileName: "link-initial-preload.h2.html",
-        expectedRequests: [
-        {   fileNameAndSuffix: "dummy.txt?1",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
-        },
-        {   fileNameAndSuffix: "dummy.txt?2",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
-        },
-        {   fileNameAndSuffix: "dummy.txt?3",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
-        },
-        {   fileNameAndSuffix: "dummy.txt?4",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
-        },
-        {   fileNameAndSuffix: "dummy.font?1",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
-        },
-        {   fileNameAndSuffix: "dummy.font?2",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
-        },
-        {   fileNameAndSuffix: "dummy.font?3",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
-        },
-        {   fileNameAndSuffix: "dummy.font?4",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
-        },
-        {   fileNameAndSuffix: "dummy.image?1",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
-        },
-        {   fileNameAndSuffix: "dummy.image?2",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
-        },
-        {   fileNameAndSuffix: "dummy.image?3",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
-        },
-        {   fileNameAndSuffix: "dummy.image?4",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
-        },
-        {   fileNameAndSuffix: "dummy.css?1",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
-        },
-        {   fileNameAndSuffix: "dummy.css?2",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGHEST
-        },
-        {   fileNameAndSuffix: "dummy.css?3",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGHEST
-        },
-        {   fileNameAndSuffix: "dummy.css?4",
-            internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGHEST
-        },
-        ]
+        expectedRequests: kExpectedRequestsOfLinkPreload
+    },
+    {   testFileName: "link-dynamic-preload.h2.html",
+        expectedRequests: kExpectedRequestsOfLinkPreload
     },
     {   testFileName: "link-initial-modulepreload.h2.html",
         expectedRequests: [
