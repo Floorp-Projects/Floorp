2d1b593c07af365c1bf2967af91ac3afd86d12ee
Natalia Csoregi <ncsoregi@mozilla.com>
2023-09-28 08:03:34 +0300

    Backed out 3 changesets (bug 1848282) for causing gv-junit failures. CLOSED TREE
    Backed out changeset d1fc71cc3817 (bug 1848282)
    Backed out changeset 3446c1367bfd (bug 1848282)
    Backed out changeset c800ae9f7943 (bug 1848282)

--- accessible/tests/browser/mac/browser_text_basics.js
+++ accessible/tests/browser/mac/browser_text_basics.js
@@ -248,14 +248,11 @@ function testMarkerIntegrity(accDoc, expectedMarkerValues) {
 // Run tests with old word segmenter
 addAccessibleTask("mac/doc_textmarker_test.html", async (browser, accDoc) => {
   await SpecialPowers.pushPrefEnv({
-    set: [
-      ["intl.icu4x.segmenter.enabled", false],
-      ["layout.word_select.stop_at_punctuation", true], // This is default
-    ],
+    set: [["intl.icu4x.segmenter.enabled", false]],
   });
 
   const expectedValues = await SpecialPowers.spawn(browser, [], async () => {
-    return content.wrappedJSObject.getExpected(false, true);
+    return content.wrappedJSObject.getExpected(false);
   });
 
   testMarkerIntegrity(accDoc, expectedValues);
@@ -263,35 +260,13 @@ addAccessibleTask("mac/doc_textmarker_test.html", async (browser, accDoc) => {
   await SpecialPowers.popPrefEnv();
 });
 
-// new UAX#14 segmenter without stop_at_punctuation.
 addAccessibleTask("mac/doc_textmarker_test.html", async (browser, accDoc) => {
   await SpecialPowers.pushPrefEnv({
-    set: [
-      ["intl.icu4x.segmenter.enabled", true],
-      ["layout.word_select.stop_at_punctuation", false],
-    ],
+    set: [["intl.icu4x.segmenter.enabled", true]],
   });
 
   const expectedValues = await SpecialPowers.spawn(browser, [], async () => {
-    return content.wrappedJSObject.getExpected(true, false);
-  });
-
-  testMarkerIntegrity(accDoc, expectedValues);
-
-  await SpecialPowers.popPrefEnv();
-});
-
-// new UAX#14 segmenter with stop_at_punctuation
-addAccessibleTask("mac/doc_textmarker_test.html", async (browser, accDoc) => {
-  await SpecialPowers.pushPrefEnv({
-    set: [
-      ["intl.icu4x.segmenter.enabled", true],
-      ["layout.word_select.stop_at_punctuation", true], // this is default
-    ],
-  });
-
-  const expectedValues = await SpecialPowers.spawn(browser, [], async () => {
-    return content.wrappedJSObject.getExpected(true, true);
+    return content.wrappedJSObject.getExpected(true);
   });
 
   testMarkerIntegrity(accDoc, expectedValues);
