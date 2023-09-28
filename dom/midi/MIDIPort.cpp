1e245a7a59e7a3ca0e1fd94c59ff9792584924d6
Natalia Csoregi <ncsoregi@mozilla.com>
2023-09-28 13:23:02 +0300

    Backed out changeset ac2c12d8a02d (bug 1851829) for causing failures on 1851829.html. CLOSED TREE

--- dom/midi/MIDIPort.cpp
+++ dom/midi/MIDIPort.cpp
@@ -254,10 +254,8 @@ void MIDIPort::Receive(const nsTArray<MIDIMessage>& aMsg) {
 }
 
 void MIDIPort::DisconnectFromOwner() {
-  if (Port()) {
-    Port()->SendClose();
-  }
   DontKeepAliveOnStatechange();
+  Port()->SendClose();
 
   DOMEventTargetHelper::DisconnectFromOwner();
 }
