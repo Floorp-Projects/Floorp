f2cfb8ccd8e91e7a5b471cace51e0d82f6a1445b
Stephanie Cunnane <scunnane@mozilla.com>
2023-09-28 16:42:14 +0000

    Bug 1854495 - Add SERP categorization feature flag to Nimbus. r=jteow
    Differential Revision: https://phabricator.services.mozilla.com/D189431

--- browser/components/search/test/browser/telemetry/browser_search_glean_serp_event_telemetry_enabled_by_nimbus_variable.js
+++ browser/components/search/test/browser/telemetry/browser_search_glean_serp_event_telemetry_enabled_by_nimbus_variable.js
@@ -1,8 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-// Test to verify we can toggle the Glean SERP telemetry feature via a Nimbus
-// variable.
+// Test to verify we can toggle the Glean SERP event telemetry feature via a
+// Nimbus variable.
 
 const lazy = {};
 
