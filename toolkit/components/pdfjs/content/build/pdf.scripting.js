51c061712ecd6c07a8365061f85f6e0fe09f09cf
Calixte <cdenizet@mozilla.com>
2023-09-29 08:19:19 +0000

    Bug 1855641 - StructParents entry isn't required on pages with no tagged contents r=#pdfjs-reviewers, a=dsmith
    Cherry pick of f2196f78036fe2266d86fa68a56ab3b82a68c088.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189621

--- toolkit/components/pdfjs/content/build/pdf.scripting.js
+++ toolkit/components/pdfjs/content/build/pdf.scripting.js
@@ -4250,8 +4250,8 @@ Object.defineProperty(exports, "initSandbox", ({
   }
 }));
 var _initialization = __w_pdfjs_require__(1);
-const pdfjsVersion = '3.11.182';
-const pdfjsBuild = '3f7060e77';
+const pdfjsVersion = '3.11.183';
+const pdfjsBuild = 'd37cd65c5';
 })();
 
 /******/ 	return __webpack_exports__;
