a0b892a4abde84e0e0dcce1b7d3932ac3de54ce5
az <azebrowski@mozilla.com>
2023-09-29 21:10:47 +0000

    Bug 1855058 - Part 1 - Ensure SW/HW support entries for all media codecs are displayed on about:support and improve diagnostic string formatting when copying data to the clipboard r=media-playback-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D188892

--- dom/media/platforms/MediaCodecsSupport.cpp
+++ dom/media/platforms/MediaCodecsSupport.cpp
@@ -108,20 +108,27 @@ void MCSInfo::GetMediaCodecsSupportedString(
     CODEC_SUPPORT_LOG("Can't get codec support string w/o a MCSInfo instance!");
     return;
   }
-  for (const auto& it : aSupportedCodecs) {
-    if (!instance->mHashTableMCS->Get(it, &supportInfo)) {
-      CODEC_SUPPORT_LOG("Can't find string for MediaCodecsSupported enum: %d",
-                        static_cast<int>(it));
-      aSupportString.Append("Unknown codec entry found!\n"_ns);
+  for (const auto& it : GetAllCodecDefinitions()) {
+    if (it.codec == MediaCodec::SENTINEL) {
+      break;
+    }
+    if (!instance->mHashTableCodec->Get(it.codec, &supportInfo)) {
+      CODEC_SUPPORT_LOG("Can't find codec for MediaCodecsSupported enum: %d",
+                        static_cast<int>(it.codec));
       continue;
     }
-    // Get codec name string and append SW/HW support info
     aSupportString.Append(supportInfo.commonName);
-    if (it == supportInfo.swDecodeSupport) {
+    bool foundSupport = false;
+    if (aSupportedCodecs.contains(it.swDecodeSupport)) {
       aSupportString.Append(" SW"_ns);
+      foundSupport = true;
     }
-    if (it == supportInfo.hwDecodeSupport) {
+    if (aSupportedCodecs.contains(it.hwDecodeSupport)) {
       aSupportString.Append(" HW"_ns);
+      foundSupport = true;
+    }
+    if (!foundSupport) {
+      aSupportString.Append(" NONE"_ns);
     }
     aSupportString.Append("\n"_ns);
   }
