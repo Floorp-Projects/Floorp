584b77c74bf53368d695f1f4b256e25a00c2d154
Norisz Fay <nfay@mozilla.com>
2023-10-02 16:11:21 +0300

    Backed out 2 changesets (bug 1156659) for causing dt failures on browser_net_throttle.js CLOSED TREE
    Backed out changeset af64e5a0109a (bug 1156659)
    Backed out changeset 7e9b86bce76b (bug 1156659)

--- devtools/client/netmonitor/src/connector/index.js
+++ devtools/client/netmonitor/src/connector/index.js
@@ -512,9 +512,6 @@ class Connector {
   async updateNetworkThrottling(enabled, profile) {
     if (!enabled) {
       this.networkFront.clearNetworkThrottling();
-      await this.commands.targetConfigurationCommand.updateConfiguration({
-        setTabOffline: false,
-      });
     } else {
       // The profile can be either a profile id which is used to
       // search the predefined throttle profiles or a profile object
@@ -523,9 +520,6 @@ class Connector {
         profile = throttlingProfiles.find(({ id }) => id == profile);
       }
       const { download, upload, latency } = profile;
-      await this.commands.targetConfigurationCommand.updateConfiguration({
-        setTabOffline: !download,
-      });
       await this.networkFront.setNetworkThrottling({
         downloadThroughput: download,
         uploadThroughput: upload,
