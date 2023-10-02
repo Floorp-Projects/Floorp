aa1622f8617837dcfdca00014d75cd0294cf8776
Ziran Sun <zsun@igalia.com>
2023-10-02 15:21:27 +0000

    Bug 1850341 - Check if a dialog is opened as a popover in ESC key handling.  r=emilio
    https://github.com/whatwg/html/pull/9121
    
    Differential Revision: https://phabricator.services.mozilla.com/D189632

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -14423,16 +14423,15 @@ void Document::SetFullscreenRoot(Document* aRoot) {
 void Document::HandleEscKey() {
   for (const nsWeakPtr& weakPtr : Reversed(mTopLayer)) {
     nsCOMPtr<Element> element(do_QueryReferent(weakPtr));
-    if (auto* dialog = HTMLDialogElement::FromNodeOrNull(element)) {
-      dialog->QueueCancelDialog();
-      break;
-    }
-    if (RefPtr<nsGenericHTMLElement> popoverHTMLEl =
-            nsGenericHTMLElement::FromNodeOrNull(element)) {
+    if (RefPtr popoverHTMLEl = nsGenericHTMLElement::FromNodeOrNull(element)) {
       if (element->IsAutoPopover() && element->IsPopoverOpen()) {
         popoverHTMLEl->HidePopover(IgnoreErrors());
         break;
       }
+    }
+    if (auto* dialog = HTMLDialogElement::FromNodeOrNull(element)) {
+      dialog->QueueCancelDialog();
+      break;
     }
   }
 }
