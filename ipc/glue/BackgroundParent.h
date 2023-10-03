e29c1659aaba74b1ca3a21f3197fa55afd0dea22
Jan Varga <jvarga@mozilla.com>
2023-10-03 18:40:30 +0000

    Bug 1855138 - Add BackgroundParent::KillHardAsync method; r=nika,ipc-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D189482

--- ipc/glue/BackgroundParent.h
+++ ipc/glue/BackgroundParent.h
@@ -77,6 +77,9 @@ class BackgroundParent final {
 
   static uint64_t GetChildID(PBackgroundParent* aBackgroundActor);
 
+  static void KillHardAsync(PBackgroundParent* aBackgroundActor,
+                            const char* aReason);
+
  private:
   // Only called by ContentParent for cross-process actors.
   static bool AllocStarter(ContentParent* aContent,
