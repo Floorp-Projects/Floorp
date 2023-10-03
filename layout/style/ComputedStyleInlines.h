dc70f67ea2d7d1c1b1d577140491a8b056760784
Emilio Cobos Álvarez <emilio@crisal.io>
2023-10-03 21:04:07 +0000

    Bug 1799036 - Make top layer elements be fixed-pos containing blocks. r=dholbert
    This is the most reasonable behavior to preserve our layout invariants.
    
    Once https://github.com/w3c/csswg-drafts/issues/8040 gets resolved we
    can reconsider this.
    
    I think at the very least we should promote fixed-pos elements inside
    the top layer to the top layer, but that has its own set of
    implications.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189924

--- layout/style/ComputedStyleInlines.h
+++ layout/style/ComputedStyleInlines.h
@@ -90,6 +90,10 @@ bool ComputedStyle::IsFixedPosContainingBlockForNonSVGTextFrames() const {
     return true;
   }
 
+  if (disp.mTopLayer == StyleTopLayer::Top) {
+    return true;
+  }
+
   const auto& effects = *StyleEffects();
   return effects.HasFilters() || effects.HasBackdropFilters();
 }
