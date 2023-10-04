314f4291204b76dce2b029e545d7facbf2f88828
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2023-10-04 15:18:21 +0000

    Bug 1839396 part 20 - Rename Sprinter to StringPrinter and create 2 variants. r=sfink
    In the previous patches, Sprinter was given the ability to generate JS strings,
    in order to avoid some of the use case where Sprinter allocated a buffer, just
    to see it being copied once more to make a JSString out of the generated
    content. Except that when compiling against jemalloc, we allocate JSString in a
    different arena than the default one.
    
    This patch is a follow up, which rename Sprinter to StringPrinter and create 2
    variants named Sprinter (String allocation) and JSSprinter (JSString
    allocation).
    
    In addition to the renaming, we can now specialize the JSSprinter to use the
    StringBufferArena as well as enforcing the use of a JSContext and the reporting
    of out-of-memory. As opposed to the Sprinter interface which keeps the old
    flexibility.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189150

--- js/src/vm/BytecodeUtil.h
+++ js/src/vm/BytecodeUtil.h
@@ -32,7 +32,7 @@
 #include "vm/ThrowMsgKind.h"   // ThrowMsgKind, ThrowCondition
 
 namespace js {
-class JS_PUBLIC_API Sprinter;
+class JS_PUBLIC_API StringPrinter;
 }  // namespace js
 
 /* Shorthand for type from format. */
@@ -650,11 +650,12 @@ enum class DisassembleSkeptically { No, Yes };
  * Disassemblers, for debugging only.
  */
 [[nodiscard]] extern bool Disassemble(
-    JSContext* cx, JS::Handle<JSScript*> script, bool lines, Sprinter* sp,
+    JSContext* cx, JS::Handle<JSScript*> script, bool lines, StringPrinter* sp,
     DisassembleSkeptically skeptically = DisassembleSkeptically::No);
 
 unsigned Disassemble1(JSContext* cx, JS::Handle<JSScript*> script,
-                      jsbytecode* pc, unsigned loc, bool lines, Sprinter* sp);
+                      jsbytecode* pc, unsigned loc, bool lines,
+                      StringPrinter* sp);
 
 #endif
 
