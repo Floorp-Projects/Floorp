b6b03809afb4dd74db6da2e3136d2b8fb467e5c7
Gregory Pappas <gp3033@protonmail.com>
2023-10-04 17:59:19 +0000

    Bug 1856506 - Enable dom.ipc.processPriorityManager.enabled in StaticPrefs r=gsvelto,geckoview-reviewers,amejiamarmol
    Differential Revision: https://phabricator.services.mozilla.com/D189884

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2157,11 +2157,6 @@ pref("browser.tabs.crashReporting.includeURL", false);
 // nightly and developer edition.
 pref("extensions.experiments.enabled", false);
 
-#if defined(XP_LINUX) || defined(XP_WIN) || defined(XP_MACOSX)
-  // Allows us to adjust the priority of child processes at the OS level
-  pref("dom.ipc.processPriorityManager.enabled", true);
-#endif
-
 #if defined(XP_WIN)
   pref("dom.ipc.processPriorityManager.backgroundUsesEcoQoS", true);
 #endif
