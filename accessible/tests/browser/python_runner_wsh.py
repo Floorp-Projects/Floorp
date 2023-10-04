b7e6b6a7bea046dbc1e56ace4b363247e4b1ac3e
James Teh <jteh@mozilla.com>
2023-10-04 23:58:44 +0000

    Bug 1852190 part 1: Provide the ability for a11y browser tests to run Python code. r=eeejay,jmaher
    Our mochitest harness always runs a pywebsocket3 server which supports handlers as files in any test directory.
    We use this to implement a WebSocket which takes Python code as input and returns a JSON serialised result or exception.
    The Python code is wrapped in a function so that it can return a result by simply using the return statement.
    Python code run via the WebSocket has its own globals namespace.
    OS specific things will be placed in this namespace for convenience in future patches.
    Tests might also use Python globals to persist things they don't want to keep retrieving every call.
    A runPython function has been added to shared-head.js to be called by browser tests.
    This manages connection to the WebSocket, sending the code, asynchronously handling the response, etc.
    It simply takes Python code as a string and returns the deserialised result.
    
    Differential Revision: https://phabricator.services.mozilla.com/D187744

---
+++ accessible/tests/browser/python_runner_wsh.py
@@ -0,0 +1,64 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""A pywebsocket3 handler which runs arbitrary Python code and returns the
+result.
+This is used to test OS specific accessibility APIs which can't be tested in JS.
+It is intended to be called from JS browser tests.
+"""
+
+import json
+import traceback
+
+from mod_pywebsocket import msgutil
+
+
+def web_socket_do_extra_handshake(request):
+    pass
+
+
+def web_socket_transfer_data(request):
+    def send(*args):
+        """Send a response to the client as a JSON array."""
+        msgutil.send_message(request, json.dumps(args))
+
+    # XXX Anything that should be accessible to code run
+    # via the WebSocket should be added to cleanNamespace.
+    cleanNamespace = {}
+
+    def info(message):
+        """Log an info message."""
+        send("info", str(message))
+
+    cleanNamespace["info"] = info
+    namespace = cleanNamespace.copy()
+
+    # Keep handling messages until the WebSocket is closed.
+    while True:
+        code = msgutil.receive_message(request)
+        if not code:
+            return
+        if code == "__reset__":
+            namespace = cleanNamespace.copy()
+            continue
+
+        # Wrap the code in a function called run(). This allows the code to
+        # return a result by simply using the return statement.
+        if "\n" not in code and not code.lstrip().startswith("return "):
+            # Single line without return. Assume this is an expression. We use
+            # a lambda to return the result.
+            code = f"run = lambda: {code}"
+        else:
+            lines = ["def run():"]
+            # Indent each line inside the function.
+            lines.extend(f"  {line}" for line in code.splitlines())
+            code = "\n".join(lines)
+        try:
+            # Execute this Python code, which will define the run() function.
+            exec(code, namespace)
+            # Run the function we just defined.
+            ret = namespace["run"]()
+            send("return", ret)
+        except Exception:
+            send("exception", traceback.format_exc())
