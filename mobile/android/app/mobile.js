8cd410ea126a5c740691a49469aba2702a7eec3d
Gregory Pappas <gp3033@protonmail.com>
2023-10-04 19:03:52 +0000

    Bug 1856512 - Lock toolkit.defaultChromeURI pref r=geckoview-reviewers,calu
    Differential Revision: https://phabricator.services.mozilla.com/D189887

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -12,7 +12,7 @@
 // improves readability, particular for conditional blocks that exceed a single
 // screen.
 
-pref("toolkit.defaultChromeURI", "chrome://geckoview/content/geckoview.xhtml");
+pref("toolkit.defaultChromeURI", "chrome://geckoview/content/geckoview.xhtml", locked);
 
 pref("toolkit.zoomManager.zoomValues", ".2,.3,.5,.67,.8,.9,1,1.1,1.2,1.33,1.5,1.7,2,2.4,3,4");
 
