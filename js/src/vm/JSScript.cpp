bf95367c644a5b56673bfc143f19c66523b3a821
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2023-10-04 15:18:21 +0000

    Bug 1839396 part 20 - Rename Sprinter to StringPrinter and create 2 variants. r=sfink
    In the previous patches, Sprinter was given the ability to generate JS strings,
    in order to avoid some of the use case where Sprinter allocated a buffer, just
    to see it being copied once more to make a JSString out of the generated
    content. Except that when compiling against jemalloc, we allocate JSString in a
    different arena than the default one.
    
    This patch is a follow up, which rename Sprinter to StringPrinter and create 2
    variants named Sprinter (String allocation) and JSSprinter (JSString
    allocation).
    
    In addition to the renaming, we can now specialize the JSSprinter to use the
    StringBufferArena as well as enforcing the use of a JSContext and the reporting
    of out-of-memory. As opposed to the Sprinter interface which keeps the old
    flexibility.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189150

--- js/src/vm/JSScript.cpp
+++ js/src/vm/JSScript.cpp
@@ -3348,7 +3348,7 @@ static void DumpMutableScriptFlags(js::JSONPrinter& json,
 
 /* static */
 bool JSScript::dump(JSContext* cx, JS::Handle<JSScript*> script,
-                    DumpOptions& options, js::Sprinter* sp) {
+                    DumpOptions& options, js::StringPrinter* sp) {
   {
     JSONPrinter json(*sp);
 
@@ -3450,7 +3450,7 @@ bool JSScript::dump(JSContext* cx, JS::Handle<JSScript*> script,
 
 /* static */
 bool JSScript::dumpSrcNotes(JSContext* cx, JS::Handle<JSScript*> script,
-                            js::Sprinter* sp) {
+                            js::GenericPrinter* sp) {
   sp->put("\nSource notes:\n");
   sp->printf("%4s %4s %6s %5s %6s %-16s %s\n", "ofs", "line", "column",
              "pc", "delta", "desc", "args");
@@ -3538,7 +3538,7 @@ static const char* TryNoteName(TryNoteKind kind) {
 
 /* static */
 bool JSScript::dumpTryNotes(JSContext* cx, JS::Handle<JSScript*> script,
-                            js::Sprinter* sp) {
+                            js::GenericPrinter* sp) {
   sp->put("\nException table:\nkind               stack    start      end\n");
 
   for (const js::TryNote& tn : script->trynotes()) {
@@ -3550,7 +3550,7 @@ bool JSScript::dumpTryNotes(JSContext* cx, JS::Handle<JSScript*> script,
 
 /* static */
 bool JSScript::dumpScopeNotes(JSContext* cx, JS::Handle<JSScript*> script,
-                              js::Sprinter* sp) {
+                              js::GenericPrinter* sp) {
   sp->put("\nScope notes:\n   index   parent    start      end\n");
 
   for (const ScopeNote& note : script->scopeNotes()) {
@@ -3571,7 +3571,7 @@ bool JSScript::dumpScopeNotes(JSContext* cx, JS::Handle<JSScript*> script,
 
 /* static */
 bool JSScript::dumpGCThings(JSContext* cx, JS::Handle<JSScript*> script,
-                            js::Sprinter* sp) {
+                            js::GenericPrinter* sp) {
   sp->put("\nGC things:\n   index   type       value\n");
 
   size_t i = 0;
