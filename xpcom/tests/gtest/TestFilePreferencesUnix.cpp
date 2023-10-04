7eaa72152fa52a603cf6272bdc02687a584a1448
Vinny Diehl <vinny.diehl@gmail.com>
2023-10-04 16:48:47 +0000

    Bug 1847572 - Expand home directory paths for arbitrary users r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D185999

--- xpcom/tests/gtest/TestFilePreferencesUnix.cpp
+++ xpcom/tests/gtest/TestFilePreferencesUnix.cpp
@@ -160,6 +160,31 @@ TEST(TestFilePreferencesUnix, Simple)
   rv = newPath->AppendNative(".."_ns);
   ASSERT_EQ(rv, NS_ERROR_FILE_UNRECOGNIZED_PATH);
 
+#if defined(XP_UNIX) && !defined(ANDROID)
+  nsAutoCString homePath;
+  NS_GetSpecialDirectory(NS_OS_HOME_DIR, getter_AddRefs(newPath));
+  newPath->GetNativePath(homePath);
+
+  newPath->InitWithNativePath("~"_ns);
+  ASSERT_TRUE(newPath->NativePath().Equals(homePath));
+
+  newPath->InitWithNativePath("~/foo"_ns);
+  ASSERT_TRUE(newPath->NativePath().Equals(homePath + "/foo"_ns));
+
+  nsLiteralCString homeBase =
+#  ifdef XP_MACOSX
+      "/Users"_ns;
+#  else
+      "/home"_ns;
+#  endif
+
+  newPath->InitWithNativePath("~foo"_ns);
+  ASSERT_TRUE(newPath->NativePath().Equals(homeBase + "/foo"_ns));
+
+  newPath->InitWithNativePath("~foo/bar"_ns);
+  ASSERT_TRUE(newPath->NativePath().Equals(homeBase + "/foo/bar"_ns));
+#endif
+
   nsAutoCString trickyPath(tempPath);
   trickyPath.AppendLiteral("/allowed/../forbidden_dir/file");
   rv = newPath->InitWithNativePath(trickyPath);
