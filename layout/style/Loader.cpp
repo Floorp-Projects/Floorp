b76caf16f251fee29ae7affb9fa1b83c998b2988
Mirko Brodesser <mbrodesser@igalia.com>
2023-10-05 09:07:02 +0000

    Bug 1856942: part 2) Factor sync loading of sheet out of `Loader::LoadSheet`. r=emilio
    In another patch, `::LoadSheet` needs to be extended to support the
    `fetchpriority` attribute (see
    https://bugzilla.mozilla.org/show_bug.cgi?id=1839315).
    
    `Loader::LoadSheet` is already 350 lines long
    (https://searchfox.org/mozilla-central/rev/29bdf6ff9965a647c6f64d63fed2b5bd094532c7/layout/style/Loader.cpp#1147-1495).
    To avoid further growing `Loader::LoadSheet`, details are abstract away
    in this and following parts.
    
    This is in line with
    <https://google.github.io/styleguide/cppguide.html#Write_Short_Functions>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D190045

--- layout/style/Loader.cpp
+++ layout/style/Loader.cpp
@@ -1136,6 +1136,102 @@ void Loader::InsertChildSheet(StyleSheet& aSheet, StyleSheet& aParentSheet) {
   LOG(("  Inserting into parent sheet"));
 }
 
+nsresult Loader::LoadSheetSyncInternal(SheetLoadData& aLoadData,
+                                       SheetState aSheetState) {
+  LOG(("  Synchronous load"));
+  MOZ_ASSERT(!aLoadData.mObserver, "Observer for a sync load?");
+  MOZ_ASSERT(aSheetState == SheetState::NeedsParser,
+             "Sync loads can't reuse existing async loads");
+
+  nsINode* requestingNode = aLoadData.GetRequestingNode();
+
+  nsresult rv = NS_OK;
+
+  // Create a StreamLoader instance to which we will feed
+  // the data from the sync load.  Do this before creating the
+  // channel to make error recovery simpler.
+  auto streamLoader = MakeRefPtr<StreamLoader>(aLoadData);
+
+  if (mDocument) {
+    net::PredictorLearn(aLoadData.mURI, mDocument->GetDocumentURI(),
+                        nsINetworkPredictor::LEARN_LOAD_SUBRESOURCE, mDocument);
+  }
+
+  // Synchronous loads should only be used internally. Therefore no CORS
+  // policy is needed.
+  nsSecurityFlags securityFlags =
+      nsContentSecurityManager::ComputeSecurityFlags(
+          CORSMode::CORS_NONE, nsContentSecurityManager::CORSSecurityMapping::
+                                   CORS_NONE_MAPS_TO_INHERITED_CONTEXT);
+
+  securityFlags |= nsILoadInfo::SEC_ALLOW_CHROME;
+
+  nsContentPolicyType contentPolicyType =
+      aLoadData.mPreloadKind == StylePreloadKind::None
+          ? nsIContentPolicy::TYPE_INTERNAL_STYLESHEET
+          : nsIContentPolicy::TYPE_INTERNAL_STYLESHEET_PRELOAD;
+
+  // Just load it
+  nsCOMPtr<nsIChannel> channel;
+  // Note that we are calling NS_NewChannelWithTriggeringPrincipal() with both
+  // a node and a principal.
+  // This is because of a case where the node is the document being styled and
+  // the principal is the stylesheet (perhaps from a different origin) that is
+  // applying the styles.
+  if (requestingNode) {
+    rv = NS_NewChannelWithTriggeringPrincipal(
+        getter_AddRefs(channel), aLoadData.mURI, requestingNode,
+        aLoadData.mTriggeringPrincipal, securityFlags, contentPolicyType);
+  } else {
+    MOZ_ASSERT(aLoadData.mTriggeringPrincipal->Equals(LoaderPrincipal()));
+    auto result = URLPreloader::ReadURI(aLoadData.mURI);
+    if (result.isOk()) {
+      nsCOMPtr<nsIInputStream> stream;
+      MOZ_TRY(
+          NS_NewCStringInputStream(getter_AddRefs(stream), result.unwrap()));
+
+      rv = NS_NewInputStreamChannel(
+          getter_AddRefs(channel), aLoadData.mURI, stream.forget(),
+          aLoadData.mTriggeringPrincipal, securityFlags, contentPolicyType);
+    } else {
+      rv = NS_NewChannel(getter_AddRefs(channel), aLoadData.mURI,
+                         aLoadData.mTriggeringPrincipal, securityFlags,
+                         contentPolicyType);
+    }
+  }
+  if (NS_FAILED(rv)) {
+    LOG_ERROR(("  Failed to create channel"));
+    streamLoader->ChannelOpenFailed(rv);
+    SheetComplete(aLoadData, rv);
+    return rv;
+  }
+
+  nsCOMPtr<nsILoadInfo> loadInfo = channel->LoadInfo();
+  loadInfo->SetCspNonce(aLoadData.Nonce());
+
+  nsCOMPtr<nsIInputStream> stream;
+  rv = channel->Open(getter_AddRefs(stream));
+
+  if (NS_FAILED(rv)) {
+    LOG_ERROR(("  Failed to open URI synchronously"));
+    streamLoader->ChannelOpenFailed(rv);
+    SheetComplete(aLoadData, rv);
+    return rv;
+  }
+
+  // Force UA sheets to be UTF-8.
+  // XXX this is only necessary because the default in
+  // SheetLoadData::OnDetermineCharset is wrong (bug 521039).
+  channel->SetContentCharset("UTF-8"_ns);
+
+  // Manually feed the streamloader the contents of the stream.
+  // This will call back into OnStreamComplete
+  // and thence to ParseSheet.  Regardless of whether this fails,
+  // SheetComplete has been called.
+  return nsSyncLoadService::PushSyncStreamToListener(stream.forget(),
+                                                     streamLoader, channel);
+}
+
 /**
  * LoadSheet handles the actual load of a sheet.  If the load is
  * supposed to be synchronous it just opens a channel synchronously
@@ -1185,95 +1281,7 @@ nsresult Loader::LoadSheet(SheetLoadData& aLoadData, SheetState aSheetState,
   nsINode* requestingNode = aLoadData.GetRequestingNode();
 
   if (aLoadData.mSyncLoad) {
-    LOG(("  Synchronous load"));
-    MOZ_ASSERT(!aLoadData.mObserver, "Observer for a sync load?");
-    MOZ_ASSERT(aSheetState == SheetState::NeedsParser,
-               "Sync loads can't reuse existing async loads");
-
-    // Create a StreamLoader instance to which we will feed
-    // the data from the sync load.  Do this before creating the
-    // channel to make error recovery simpler.
-    auto streamLoader = MakeRefPtr<StreamLoader>(aLoadData);
-
-    if (mDocument) {
-      net::PredictorLearn(aLoadData.mURI, mDocument->GetDocumentURI(),
-                          nsINetworkPredictor::LEARN_LOAD_SUBRESOURCE,
-                          mDocument);
-    }
-
-    // Synchronous loads should only be used internally. Therefore no CORS
-    // policy is needed.
-    nsSecurityFlags securityFlags =
-        nsContentSecurityManager::ComputeSecurityFlags(
-            CORSMode::CORS_NONE, nsContentSecurityManager::CORSSecurityMapping::
-                                     CORS_NONE_MAPS_TO_INHERITED_CONTEXT);
-
-    securityFlags |= nsILoadInfo::SEC_ALLOW_CHROME;
-
-    nsContentPolicyType contentPolicyType =
-        aLoadData.mPreloadKind == StylePreloadKind::None
-            ? nsIContentPolicy::TYPE_INTERNAL_STYLESHEET
-            : nsIContentPolicy::TYPE_INTERNAL_STYLESHEET_PRELOAD;
-
-    // Just load it
-    nsCOMPtr<nsIChannel> channel;
-    // Note that we are calling NS_NewChannelWithTriggeringPrincipal() with both
-    // a node and a principal.
-    // This is because of a case where the node is the document being styled and
-    // the principal is the stylesheet (perhaps from a different origin) that is
-    // applying the styles.
-    if (requestingNode) {
-      rv = NS_NewChannelWithTriggeringPrincipal(
-          getter_AddRefs(channel), aLoadData.mURI, requestingNode,
-          aLoadData.mTriggeringPrincipal, securityFlags, contentPolicyType);
-    } else {
-      MOZ_ASSERT(aLoadData.mTriggeringPrincipal->Equals(LoaderPrincipal()));
-      auto result = URLPreloader::ReadURI(aLoadData.mURI);
-      if (result.isOk()) {
-        nsCOMPtr<nsIInputStream> stream;
-        MOZ_TRY(
-            NS_NewCStringInputStream(getter_AddRefs(stream), result.unwrap()));
-
-        rv = NS_NewInputStreamChannel(
-            getter_AddRefs(channel), aLoadData.mURI, stream.forget(),
-            aLoadData.mTriggeringPrincipal, securityFlags, contentPolicyType);
-      } else {
-        rv = NS_NewChannel(getter_AddRefs(channel), aLoadData.mURI,
-                           aLoadData.mTriggeringPrincipal, securityFlags,
-                           contentPolicyType);
-      }
-    }
-    if (NS_FAILED(rv)) {
-      LOG_ERROR(("  Failed to create channel"));
-      streamLoader->ChannelOpenFailed(rv);
-      SheetComplete(aLoadData, rv);
-      return rv;
-    }
-
-    nsCOMPtr<nsILoadInfo> loadInfo = channel->LoadInfo();
-    loadInfo->SetCspNonce(aLoadData.Nonce());
-
-    nsCOMPtr<nsIInputStream> stream;
-    rv = channel->Open(getter_AddRefs(stream));
-
-    if (NS_FAILED(rv)) {
-      LOG_ERROR(("  Failed to open URI synchronously"));
-      streamLoader->ChannelOpenFailed(rv);
-      SheetComplete(aLoadData, rv);
-      return rv;
-    }
-
-    // Force UA sheets to be UTF-8.
-    // XXX this is only necessary because the default in
-    // SheetLoadData::OnDetermineCharset is wrong (bug 521039).
-    channel->SetContentCharset("UTF-8"_ns);
-
-    // Manually feed the streamloader the contents of the stream.
-    // This will call back into OnStreamComplete
-    // and thence to ParseSheet.  Regardless of whether this fails,
-    // SheetComplete has been called.
-    return nsSyncLoadService::PushSyncStreamToListener(stream.forget(),
-                                                       streamLoader, channel);
+    return LoadSheetSyncInternal(aLoadData, aSheetState);
   }
 
   SheetLoadDataHashKey key(aLoadData);
