5156f0778e716ab9ab9b04274632443439954acf
Ben Hearsum <ben@mozilla.com>
2023-10-05 23:30:57 +0000

    Bug 1816998: Replace macOS quarantine database-based attribution with extended attribute attribution r=nalexander
    With extended attribute attribution using exactly the same attribution strings as Windows (eg: no URLs), this turns out to be quite straightforward. Once we've pulled in the attribution string from the extended attribute, the existing parsing, validation, etc. just works.
    
    The only wrinkle is that the extended attributes may have nul bytes or tabs that we need to strip away. (Tabs may be present because we use them to pad the attribution area when we prepare the DMG for attribution. Nul bytes may be present because we overwrite the entire attribution before updating the attribution data.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D189258

--- browser/components/attribution/AttributionCode.sys.mjs
+++ browser/components/attribution/AttributionCode.sys.mjs
@@ -191,43 +191,6 @@ export var AttributionCode = {
   },
 
   /**
-   * Returns an object containing a key-value pair for each piece of attribution
-   * data included in the passed-in URL containing a query string encoding an
-   * attribution code.
-   *
-   * We have less control of the attribution codes on macOS so we accept more
-   * URLs than we accept attribution codes on Windows.
-   *
-   * If the URL is empty, returns an empty object.
-   *
-   * If the URL doesn't parse, throws.
-   */
-  parseAttributionCodeFromUrl(url) {
-    if (!url) {
-      return {};
-    }
-
-    let parsed = {};
-
-    let params = new URL(url).searchParams;
-    for (let key of ATTR_CODE_KEYS) {
-      // We support the key prefixed with utm_ or not, but intentionally
-      // choose non-utm params over utm params.
-      for (let paramKey of [`utm_${key}`, `funnel_${key}`, key]) {
-        if (params.has(paramKey)) {
-          // We expect URI-encoded components in our attribution codes.
-          let value = encodeURIComponent(params.get(paramKey));
-          if (value && ATTR_CODE_VALUE_REGEX.test(value)) {
-            parsed[key] = value;
-          }
-        }
-      }
-    }
-
-    return parsed;
-  },
-
-  /**
    * Returns a string serializing the given attribution data.
    *
    * It is expected that the given values are already URL-encoded.
@@ -285,14 +248,15 @@ export var AttributionCode = {
         `getAttrDataAsync: macOS && !exists("${attributionFile.path}")`
       );
 
-      // On macOS, we fish the attribution data from the system quarantine DB.
+      // On macOS, we fish the attribution data from an extended attribute on
+      // the .app bundle directory.
       try {
-        let referrer = await lazy.MacAttribution.getReferrerUrl();
+        let attrStr = await lazy.MacAttribution.getAttributionString();
         lazy.log.debug(
-          `getAttrDataAsync: macOS attribution getReferrerUrl: "${referrer}"`
+          `getAttrDataAsync: macOS attribution getAttributionString: "${attrStr}"`
         );
 
-        gCachedAttrData = this.parseAttributionCodeFromUrl(referrer);
+        gCachedAttrData = this.parseAttributionCode(attrStr);
       } catch (ex) {
         // Avoid partial attribution data.
         gCachedAttrData = {};
@@ -315,8 +279,7 @@ export var AttributionCode = {
         `macOS attribution data is ${JSON.stringify(gCachedAttrData)}`
       );
 
-      // We only want to try to fetch the referrer from the quarantine
-      // database once on macOS.
+      // We only want to try to fetch the attribution string once on macOS
       try {
         let code = this.serializeAttributionData(gCachedAttrData);
         lazy.log.debug(`macOS attribution data serializes as "${code}"`);
--- browser/components/attribution/test/browser/browser_AttributionCode_telemetry.js
+++ browser/components/attribution/test/browser/browser_AttributionCode_telemetry.js
@@ -7,15 +7,10 @@ const { AttributionIOUtils } = ChromeUtils.importESModule(
 
 add_task(async function test_parse_error() {
   if (AppConstants.platform == "macosx") {
-    // On macOS, the underlying data is the OS-level quarantine
-    // database.  We need to start from nothing to isolate the cache.
     const { MacAttribution } = ChromeUtils.importESModule(
       "resource:///modules/MacAttribution.sys.mjs"
     );
-    let attributionSvc = Cc["@mozilla.org/mac-attribution;1"].getService(
-      Ci.nsIMacAttributionService
-    );
-    attributionSvc.setReferrerUrl(MacAttribution.applicationPath, "", true);
+    MacAttribution.setAttributionString("");
   }
 
   registerCleanupFunction(async () => {
