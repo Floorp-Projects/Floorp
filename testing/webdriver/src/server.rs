6e6eebceed6fd5eedcb41ba2833bd0d0065c2465
James Graham <james@hoppipolla.co.uk>
2023-10-06 07:35:43 +0000

    Bug 1857193 - Make passing --port=0 select a free port, r=webdriver-reviewers,jdescottes
    Previously this bound to a free port, but we weren't allowing
    connections because we checked the Host header against the specified
    port (i.e. 0) rather than the bound port.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189925

--- testing/webdriver/src/server.rs
+++ testing/webdriver/src/server.rs
@@ -203,7 +203,7 @@ impl Drop for Listener {
 }
 
 pub fn start<T, U>(
-    address: SocketAddr,
+    mut address: SocketAddr,
     allow_hosts: Vec<Host>,
     allow_origins: Vec<Url>,
     handler: T,
@@ -216,6 +216,11 @@ where
     let listener = StdTcpListener::bind(address)?;
     listener.set_nonblocking(true)?;
     let addr = listener.local_addr()?;
+    if address.port() == 0 {
+        // If we passed in 0 as the port number the OS will assign an unused port;
+        // we want to update the address to the actual used port
+        address.set_port(addr.port())
+    }
     let (msg_send, msg_recv) = channel();
 
     let builder = thread::Builder::new().name("webdriver server".to_string());
