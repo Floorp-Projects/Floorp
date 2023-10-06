714a40e23bc936fe0a9861c87fb2faefa807c5b7
Jamie Nicol <jnicol@mozilla.com>
2023-10-06 15:12:48 +0000

    Bug 1857447 - Avoid redundant UniformData calls in DrawTargetWebgl. r=lsalzman
    Calling ClientWebGLContext::UniformData() many times causes the
    command buffer to fill up and we spend a fair amount of time flushing
    the old buffer and allocating a new one, as well as serializing the
    values.
    
    The uniforms themselves are very small but they add up over a large
    number of calls. We already have some code to track whether the
    uniform values are dirty to avoid some redundancy, but a) this doesn't
    cover every uniform, and b) we invalidate them all when switching
    program.
    
    This patch makes us track the value of every uniform that gets set
    dynamically, and tracks the values separately for each program
    used. It then uses these to avoid calling UniformData redundantly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D190269

--- dom/canvas/DrawTargetWebgl.h
+++ dom/canvas/DrawTargetWebgl.h
@@ -7,6 +7,8 @@
 #ifndef _MOZILLA_GFX_DRAWTARGETWEBGL_H
 #define _MOZILLA_GFX_DRAWTARGETWEBGL_H
 
+#include "GLTypes.h"
+#include "mozilla/Array.h"
 #include "mozilla/gfx/2D.h"
 #include "mozilla/gfx/PathSkia.h"
 #include "mozilla/LinkedList.h"
@@ -180,12 +182,6 @@ class DrawTargetWebgl : public DrawTarget, public SupportsWeakPtr {
     RefPtr<WebGLProgramJS> mLastProgram;
     RefPtr<WebGLTextureJS> mLastTexture;
     RefPtr<WebGLTextureJS> mLastClipMask;
-    // Whether the shader viewport state requires updating.
-    bool mDirtyViewport = true;
-    // Whether the shader anti-aliasing state requires updating.
-    bool mDirtyAA = true;
-    // Whether the shader clip AA bounds require updating.
-    bool mDirtyClip = true;
 
     // WebGL shader resources
     RefPtr<WebGLBufferJS> mPathVertexBuffer;
@@ -220,6 +216,25 @@ class DrawTargetWebgl : public DrawTarget, public SupportsWeakPtr {
     RefPtr<WebGLUniformLocationJS> mImageProgramSampler;
     RefPtr<WebGLUniformLocationJS> mImageProgramClipMask;
     RefPtr<WebGLUniformLocationJS> mImageProgramClipBounds;
+
+    struct SolidProgramUniformState {
+      Maybe<Array<float, 2>> mViewport;
+      Maybe<Array<float, 1>> mAA;
+      Maybe<Array<float, 6>> mTransform;
+      Maybe<Array<float, 4>> mColor;
+      Maybe<Array<float, 4>> mClipBounds;
+    } mSolidProgramUniformState;
+
+    struct ImageProgramUniformState {
+      Maybe<Array<float, 2>> mViewport;
+      Maybe<Array<float, 1>> mAA;
+      Maybe<Array<float, 6>> mTransform;
+      Maybe<Array<float, 6>> mTexMatrix;
+      Maybe<Array<float, 4>> mTexBounds;
+      Maybe<Array<float, 4>> mColor;
+      Maybe<Array<float, 1>> mSwizzle;
+      Maybe<Array<float, 4>> mClipBounds;
+    } mImageProgramUniformState;
 
     // Scratch framebuffer used to wrap textures for miscellaneous utility ops.
     RefPtr<WebGLFramebufferJS> mScratchFramebuffer;
@@ -292,6 +307,13 @@ class DrawTargetWebgl : public DrawTarget, public SupportsWeakPtr {
     bool HasClipMask() const {
       return mLastClipMask && mLastClipMask != mNoClipMask;
     }
+
+    // Avoids redundant UniformData calls by caching the previously set value.
+    template <class T, size_t N>
+    void MaybeUniformData(GLenum aFuncElemType,
+                          const WebGLUniformLocationJS* const aLoc,
+                          const Array<T, N>& aData,
+                          Maybe<Array<T, N>>& aCached);
 
     bool IsCurrentTarget(DrawTargetWebgl* aDT) const {
       return aDT == mCurrentTarget;
