c083851dad67fa62fba18dc46840f749c683745f
Logan Rosen <loganrosen@gmail.com>
2023-10-06 16:30:47 +0000

    Bug 1852645 - fix mach try chooser regression r=ahal,jgraham
    With the fix to Bug 1786490, we are now using Werkzeug 2.1.2, which removed the shutdown function that the chooser was relying on to shut down the Flask server after selecting tasks. To remedy this, I followed the instructions [here](https://werkzeug.palletsprojects.com/en/2.3.x/serving/#shutting-down-the-server) for shutting down the server using multiprocessing instead.
    
    Since multiprocessing needs to pickle the objects with the spawn method (fork is not available on Windows and is not used by default on macOS), I needed to make ReadOnlyDict picklable by adding a `__reduce__` method like what is implemented [here](https://github.com/Marco-Sulla/python-frozendict/blob/master/src/frozendict/core.py#L142), as it is used in the task object that's passed to the Flask app.
    
    ***
    
    Differential Revision: https://phabricator.services.mozilla.com/D187952

--- tools/tryselect/selectors/chooser/__init__.py
+++ tools/tryselect/selectors/chooser/__init__.py
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import multiprocessing
 import os
+import time
 import webbrowser
 from threading import Timer
 
@@ -69,10 +71,11 @@ def run(
         for task in excluded_tasks:
             tg.tasks.pop(task)
 
-    app = create_application(tg)
+    queue = multiprocessing.Queue()
 
     if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
         # we are in the reloader process, don't open the browser or do any try stuff
+        app = create_application(tg, queue)
         app.run()
         return
 
@@ -80,9 +83,16 @@ def run(
     url = "http://127.0.0.1:5000"
     Timer(1, lambda: webbrowser.open(url)).start()
     print("Starting trychooser on {}".format(url))
-    app.run()
+    process = multiprocessing.Process(
+        target=create_and_run_application, args=(tg, queue)
+    )
+    process.start()
 
-    selected = app.tasks
+    selected = queue.get()
+
+    # Allow the close page to render before terminating the process.
+    time.sleep(1)
+    process.terminate()
     if not selected:
         print("no tasks selected")
         return
@@ -97,3 +107,11 @@ def run(
         closed_tree=closed_tree,
         push_to_lando=push_to_lando,
     )
+
+
+def create_and_run_application(tg, queue: multiprocessing.Queue):
+    from .app import create_application
+
+    app = create_application(tg, queue)
+
+    app.run()
