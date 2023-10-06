1ad07fc116333fb7c0530d7040e6ba4ac0c1dab0
Jan de Mooij <jdemooij@mozilla.com>
2023-10-06 11:27:49 +0000

    Bug 1857237 - Use the external string cache also for the inline string path. r=jonco
    In `NewMaybeExternalString` we have a fast path for short Latin1 strings. This case is
    very common.
    
    Changing the cache to handle these too is very effective: the `lookupInline` method
    has a hit rate of at least 90% on Speedometer 3.
    
    Differential Revision: https://phabricator.services.mozilla.com/D190206

--- js/src/gc/GC.cpp
+++ js/src/gc/GC.cpp
@@ -2221,11 +2221,8 @@ void ArenaLists::checkEmptyArenaList(AllocKind kind) {
 }
 
 void GCRuntime::purgeRuntimeForMinorGC() {
-  // If external strings become nursery allocable, remember to call
-  // zone->externalStringCache().purge() (and delete this assert.)
-  MOZ_ASSERT(!IsNurseryAllocable(AllocKind::EXTERNAL_STRING));
-
   for (ZonesIter zone(this, SkipAtoms); !zone.done(); zone.next()) {
+    zone->externalStringCache().purge();
     zone->functionToStringCache().purge();
   }
 }
