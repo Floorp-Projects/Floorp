9ef1490b613194f29a5564bfed56923ff64bd918
Dan Robertson <drobertson@mozilla.com>
2023-10-07 16:06:21 +0000

    Bug 1857547 - Mark APZCCallbackHelper::FireSingleTapEvent as MOZ_CAN_RUN_SCRIPT. r=hiro
    APZCCallbackHelper::FireSingleTapEvent dispatches synthesized mouse
    events for a single tap user gesture. Since the method fires events,
    the method should be marked as MOZ_CAN_RUN_SCRIPT.
    
    Differential Revision: https://phabricator.services.mozilla.com/D190305

--- gfx/layers/apz/util/APZCCallbackHelper.h
+++ gfx/layers/apz/util/APZCCallbackHelper.h
@@ -104,6 +104,7 @@ class APZCCallbackHelper {
 
   /* Synthesize a mouse event with the given parameters, and dispatch it
    * via the given widget. */
+  MOZ_CAN_RUN_SCRIPT
   static nsEventStatus DispatchSynthesizedMouseEvent(
       EventMessage aMsg, const LayoutDevicePoint& aRefPoint,
       Modifiers aModifiers, int32_t aClickCount, nsIWidget* aWidget);
@@ -121,6 +122,7 @@ class APZCCallbackHelper {
 
   /* Fire a single-tap event at the given point. The event is dispatched
    * via the given widget. */
+  MOZ_CAN_RUN_SCRIPT
   static void FireSingleTapEvent(const LayoutDevicePoint& aPoint,
                                  Modifiers aModifiers, int32_t aClickCount,
                                  nsIWidget* aWidget);
