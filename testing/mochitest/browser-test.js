2a507cb9ddef18b22173a29eb37dacc5184aebf9
James Teh <jteh@mozilla.com>
2023-10-09 18:20:11 +0000

    Bug 1692110 part 1: Perform automatic accessibility checks by capturing click events. r=ayeddi,Gijs
    Previously, we ran a11y checks in EventUtils.sendMouseEvent.
    Unfortunately, many tests don't use this, instead using some other method of clicking.
    This meant that a11y checks lacked a lot of coverage.
    Rather than trying to adjust all existing tests to use sendMouseEvent and trying to maintain this somehow for future tests, we instead capture click events and run a11y checks then.
    
    Differential Revision: https://phabricator.services.mozilla.com/D118126

--- testing/mochitest/browser-test.js
+++ testing/mochitest/browser-test.js
@@ -166,6 +166,8 @@ function Tester(aTests, structuredLogger, aCallback) {
   );
   this.AccessibilityUtils = this.EventUtils.AccessibilityUtils;
 
+  this.AccessibilityUtils.init();
+
   // Make sure our SpecialPowers actor is instantiated, in case it was
   // registered after our DOMWindowCreated event was fired (which it
   // most likely was).
@@ -524,6 +526,8 @@ Tester.prototype = {
     TabDestroyObserver.destroy();
     Services.console.unregisterListener(this);
 
+    this.AccessibilityUtils.uninit();
+
     // It's important to terminate the module to avoid crashes on shutdown.
     this.PromiseTestUtils.uninit();
 
@@ -549,7 +553,6 @@ Tester.prototype = {
 
     // Tests complete, notify the callback and return
     this.callback(this.tests);
-    this.accService = null;
     this.callback = null;
     this.tests = null;
   },
