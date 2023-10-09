5a4d59e04daf9c5c8a09cfb428ace78400618cae
Jonathan Kew <jkew@mozilla.com>
2023-10-09 18:53:01 +0000

    Bug 1856035 - Backed out changeset 57fec34232bd for regression behavior on macOS 11 and 12.
    Not sure why the new implementation doesn't work on older OS versions,
    but these APIs are inadequately documented and liable to change behavior
    between releases. So just go with what works, as shown by testing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D190500

--- gfx/thebes/gfxCoreTextShaper.cpp
+++ gfx/thebes/gfxCoreTextShaper.cpp
@@ -9,7 +9,6 @@
 #include "gfxFontUtils.h"
 #include "gfxTextRun.h"
 #include "mozilla/gfx/2D.h"
-#include "mozilla/gfx/ScaledFontMac.h"
 #include "mozilla/UniquePtrExtensions.h"
 
 #include <algorithm>
@@ -17,7 +16,6 @@
 #include <dlfcn.h>
 
 using namespace mozilla;
-using namespace mozilla::gfx;
 
 // standard font descriptors that we construct the first time they're needed
 CTFontDescriptorRef gfxCoreTextShaper::sFeaturesDescriptor[kMaxFontInstances];
@@ -633,8 +631,11 @@ CTFontDescriptorRef gfxCoreTextShaper::GetFeaturesDescriptor(
 
 CTFontRef gfxCoreTextShaper::CreateCTFontWithFeatures(
     CGFloat aSize, CTFontDescriptorRef aDescriptor) {
+  const gfxFontEntry* fe = mFont->GetFontEntry();
+  bool isInstalledFont = !fe->IsUserFont() || fe->IsLocalUserFont();
   CGFontRef cgFont = static_cast<gfxMacFont*>(mFont)->GetCGFontRef();
-  return CreateCTFontFromCGFontWithVariations(cgFont, aSize, aDescriptor);
+  return gfxMacFont::CreateCTFontFromCGFontWithVariations(
+      cgFont, aSize, isInstalledFont, aDescriptor);
 }
 
 void gfxCoreTextShaper::Shutdown()  // [static]
