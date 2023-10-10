38bc9cac52f40362eec10377f7b18a252472dd0a
Natalia Csoregi <ncsoregi@mozilla.com>
2023-10-10 23:31:06 +0300

    Backed out changeset f7da4696baf1 (bug 1857948) for causing failures on test_webauthn_authenticator_selection.html. CLOSED TREE

--- dom/webauthn/WebAuthnTransactionParent.cpp
+++ dom/webauthn/WebAuthnTransactionParent.cpp
@@ -350,12 +350,6 @@ void WebAuthnTransactionParent::ActorDestroy(ActorDestroyReason aWhy) {
   mRegisterPromiseRequest.DisconnectIfExists();
   mSignPromiseRequest.DisconnectIfExists();
   mTransactionId.reset();
-
-  nsCOMPtr<nsIWebAuthnService> webauthnService(
-      do_GetService("@mozilla.org/webauthn/service;1"));
-  if (webauthnService) {
-    webauthnService->Reset();
-  }
 }
 
 }  // namespace mozilla::dom
