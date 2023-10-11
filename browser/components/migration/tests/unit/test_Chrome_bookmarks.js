5085c317abb7cb0887f03caaee2ff58c26264d50
Mike Conley <mconley@mozilla.com>
2023-10-11 20:09:21 +0000

    Bug 1818237 - Make it possible to request permission to access Chrome-browser profiles. r=mstriemer,fluent-reviewers,desktop-theme-reviewers
    This is needed for Ubuntu Linux when Firefox is installed as a Snap package.
    Under those conditions, Firefox doesn't have the ability to read the contents
    of the other browsers' data directories.
    
    We work around by showing the user a native file picker and instructing them
    to browse to that data directory in folder picker mode - doing so gives
    the Firefox process access to the folder (through a temporary symlink).
    
    Another restriction we must overcome is Sqlite's inability to read from
    Sqlite databases through that symlinked folder. We work around this by
    copying the databases we're importing from to the system temporary
    directory, querying it, and then deleting those copies.
    
    Differential Revision: https://phabricator.services.mozilla.com/D188874

--- browser/components/migration/tests/unit/test_Chrome_bookmarks.js
+++ browser/components/migration/tests/unit/test_Chrome_bookmarks.js
@@ -49,11 +49,16 @@ async function testBookmarks(migratorKey, subDirs) {
   }
 
   let target = rootDir.clone();
+
   // Pretend this is the default profile
-  subDirs.push("Default");
   while (subDirs.length) {
     target.append(subDirs.shift());
   }
+
+  let localStatePath = PathUtils.join(target.path, "Local State");
+  await IOUtils.writeJSON(localStatePath, []);
+
+  target.append("Default");
 
   await IOUtils.makeDirectory(target.path, {
     createAncestor: true,
@@ -80,6 +85,7 @@ async function testBookmarks(migratorKey, subDirs) {
   await IOUtils.writeJSON(target.path, bookmarksData);
 
   let migrator = await MigrationUtils.getMigrator(migratorKey);
+  Assert.ok(await migrator.hasPermissions(), "Has permissions");
   // Sanity check for the source.
   Assert.ok(await migrator.isSourceAvailable());
 
