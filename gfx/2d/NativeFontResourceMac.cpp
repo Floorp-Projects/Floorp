0f5261c39ce1dc60449ceb59ff90caf1f1b580e3
Jonathan Kew <jkew@mozilla.com>
2023-10-12 11:54:08 +0000

    Bug 1803406 - Ensure consistent font lists across macOS processes, and use the kCTFontOptionsPreferSystemFont option when instantiating Core Text fonts. r=gfx-reviewers,lsalzman
    This aims to avoid conflicts with user-installed fonts that shadow system fonts,
    where there is a risk that different processes end up using different, incompatible
    versions of "the same" font (i.e. they resolve the same font descriptor to different
    resources).
    
    Differential Revision: https://phabricator.services.mozilla.com/D170286

--- gfx/2d/NativeFontResourceMac.cpp
+++ gfx/2d/NativeFontResourceMac.cpp
@@ -112,7 +112,8 @@ already_AddRefed<NativeFontResourceMac> NativeFontResourceMac::Create(
 
   // creating the CGFontRef via the CTFont avoids the data being held alive
   // in a cache.
-  CTFontRef ctFont = CTFontCreateWithFontDescriptor(ctFontDesc, 0, NULL);
+  CTFontRef ctFont = CTFontCreateWithFontDescriptorAndOptions(
+      ctFontDesc, 0, NULL, kCTFontOptionsPreferSystemFont);
 
   // Creating the CGFont from the CTFont prevents the font data from being
   // held in the TDescriptorSource cache. This appears to be true even
