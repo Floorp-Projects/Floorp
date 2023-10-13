b743b485cf3b988d3ece33278da95c9f62ff4268
Tom Schuster <tschuster@mozilla.com>
2023-10-13 18:03:14 +0000

    Bug 1846893 - Add JSContext checks. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D185993

--- js/src/vm/Iterator.cpp
+++ js/src/vm/Iterator.cpp
@@ -13,6 +13,7 @@
 #include "vm/ObjectOperations.h"
 #include "vm/SelfHosting.h"
 #include "vm/StringType.h"
+#include "vm/JSContext-inl.h"  // JSContext::check
 #include "vm/JSObject-inl.h"
 
 using namespace js;
@@ -22,6 +23,8 @@ namespace JS {
 // https://tc39.es/ecma262/#sec-getiterator
 // GetIterator(obj [, hint [, method]])
 JSObject* GetIteratorObject(JSContext* cx, HandleValue obj, bool isAsync) {
+  cx->check(obj);
+
   FixedInvokeArgs<3> args(cx);
   args[0].set(obj);
   args[1].setBoolean(isAsync);
@@ -40,6 +43,8 @@ JSObject* GetIteratorObject(JSContext* cx, HandleValue obj, bool isAsync) {
 // https://tc39.es/ecma262/#sec-iteratornext
 bool IteratorNext(JSContext* cx, HandleObject iteratorRecord,
                   MutableHandleValue result) {
+  cx->check(iteratorRecord);
+
   FixedInvokeArgs<1> args(cx);
   args[0].setObject(*iteratorRecord);
   return CallSelfHostedFunction(cx, cx->names().IteratorNext,
@@ -48,6 +53,8 @@ bool IteratorNext(JSContext* cx, HandleObject iteratorRecord,
 
 // https://tc39.es/ecma262/#sec-iteratorcomplete
 bool IteratorComplete(JSContext* cx, HandleObject iterResult, bool* done) {
+  cx->check(iterResult);
+
   RootedValue doneV(cx);
   if (!GetProperty(cx, iterResult, iterResult, cx->names().done, &doneV)) {
     return false;
@@ -60,15 +67,18 @@ bool IteratorComplete(JSContext* cx, HandleObject iterResult, bool* done) {
 // https://tc39.es/ecma262/#sec-iteratorvalue
 bool IteratorValue(JSContext* cx, HandleObject iterResult,
                    MutableHandleValue value) {
+  cx->check(iterResult);
   return GetProperty(cx, iterResult, iterResult, cx->names().value, value);
 }
 
 bool GetIteratorRecordIterator(JSContext* cx, HandleObject iteratorRecord,
                                MutableHandleValue iterator) {
+  cx->check(iteratorRecord);
   return GetProperty(cx, iteratorRecord, iteratorRecord, cx->names().iterator,
                      iterator);
 }
 
+// https://tc39.es/ecma262/#sec-getmethod
 static bool GetMethod(JSContext* cx, HandleValue v, Handle<PropertyName*> name,
                       MutableHandleValue result) {
   // Step 1. Let func be ? GetV(V, P).
@@ -95,6 +105,7 @@ static bool GetMethod(JSContext* cx, HandleValue v, Handle<PropertyName*> name,
 
 bool GetReturnMethod(JSContext* cx, HandleValue iterator,
                      MutableHandleValue result) {
+  cx->check(iterator);
   // Step 2. Let returnMethod be GetMethod(iterator, "return").
   return GetMethod(cx, iterator, cx->names().return_, result);
 }
