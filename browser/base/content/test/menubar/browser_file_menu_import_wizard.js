453355f874ddc0506788b75b6595f827d54ed802
Mike Conley <mconley@mozilla.com>
2023-10-16 18:24:42 +0000

    Bug 1855280 - Remove the ability to switch to the old migration wizard. r=kpatenio,omc-reviewers,settings-reviewers,credential-management-reviewers,negin,aminomancer,mtigley
    This removes the pref to re-enable the legacy wizard, and all of the things that used
    to read from it.
    
    It also disables the ability to use the legacy wizard from about:welcome with the
    aboutWelcomeBehavior feature.
    
    It also removes the closeMigrationWizard utility from BrowserTestUtils, since that
    effectively became a thin-wrapper around BrowserTestUtils.removeTab.
    
    Finally, this patch also removes some documentation about the legacy migration wizard,
    and tests that specifically check the legacy dialog behaviour.
    
    Differential Revision: https://phabricator.services.mozilla.com/D190716

--- browser/base/content/test/menubar/browser_file_menu_import_wizard.js
+++ browser/base/content/test/menubar/browser_file_menu_import_wizard.js
@@ -4,24 +4,20 @@
 "use strict";
 
 add_setup(async () => {
-  // Load the initial tab at example.com. This makes it so that if
-  // we're using the new migration wizard, we'll load the about:preferences
-  // page in a new tab rather than overtaking the initial one. This
-  // makes it easier to be consistent with closing and opening
-  // behaviours between the two kinds of migration wizards.
+  // Load the initial tab at example.com. This makes it so that when the
+  // about:preferences hosting the migration wizard opens, we'll load
+  // the about:preferences page in a new tab rather than overtaking the
+  // initial one. This makes it easier to be more explicit when cleaning
+  // up because we can just remove the opened tab.
   let browser = gBrowser.selectedBrowser;
   BrowserTestUtils.startLoadingURIString(browser, "https://example.com");
   await BrowserTestUtils.browserLoaded(browser);
 });
 
 add_task(async function file_menu_import_wizard() {
-  // We can't call this code directly or our JS execution will get blocked on Windows/Linux where
-  // the dialog is modal.
-  executeSoon(() =>
-    document.getElementById("menu_importFromAnotherBrowser").doCommand()
-  );
-
-  let wizard = await BrowserTestUtils.waitForMigrationWizard(window);
-  ok(wizard, "Migrator window opened");
-  await BrowserTestUtils.closeMigrationWizard(wizard);
+  let wizardTabPromise = BrowserTestUtils.waitForMigrationWizard(window);
+  document.getElementById("menu_importFromAnotherBrowser").doCommand();
+  let wizardTab = await wizardTabPromise;
+  ok(wizardTab, "Migration wizard tab opened");
+  BrowserTestUtils.removeTab(wizardTab);
 });
--- browser/components/migration/tests/browser/browser_entrypoint_telemetry.js
+++ browser/components/migration/tests/browser/browser_entrypoint_telemetry.js
@@ -3,58 +3,27 @@
 
 "use strict";
 
-const CONTENT_MODAL_ENABLED_PREF = "browser.migrate.content-modal.enabled";
 const HISTOGRAM_ID = "FX_MIGRATION_ENTRY_POINT_CATEGORICAL";
-const LEGACY_HISTOGRAM_ID = "FX_MIGRATION_ENTRY_POINT";
 
 async function showThenCloseMigrationWizardViaEntrypoint(entrypoint) {
   let openedPromise = BrowserTestUtils.waitForMigrationWizard(window);
 
-  // On some platforms, this call blocks, so in order to let the test proceed, we
-  // run it on the next tick of the event loop.
-  executeSoon(() => {
-    MigrationUtils.showMigrationWizard(window, {
-      entrypoint,
-    });
+  MigrationUtils.showMigrationWizard(window, {
+    entrypoint,
   });
 
-  let wizard = await openedPromise;
-  Assert.ok(wizard, "Migration wizard opened.");
+  let wizardTab = await openedPromise;
+  Assert.ok(wizardTab, "Migration wizard opened.");
 
-  if (!Services.prefs.getBoolPref(CONTENT_MODAL_ENABLED_PREF)) {
-    // This scalar gets written to asynchronously. The old wizard will be going
-    // away soon, so we'll just poll for it to be set rather than doing anything
-    // fancier.
-    await BrowserTestUtils.waitForCondition(() => {
-      // We should make sure that the migration.time_to_produce_legacy_migrator_list
-      // scalar was set, since we know that at least one legacy migration wizard has
-      // been opened.
-      let scalars = TelemetryTestUtils.getProcessScalars(
-        "parent",
-        false,
-        false
-      );
-      if (!scalars["migration.time_to_produce_legacy_migrator_list"]) {
-        return false;
-      }
-
-      Assert.ok(
-        scalars["migration.time_to_produce_legacy_migrator_list"] > 0,
-        "Non-zero scalar value recorded for migration.time_to_produce_migrator_list"
-      );
-      return true;
-    });
-  }
-
-  await BrowserTestUtils.closeMigrationWizard(wizard);
+  await BrowserTestUtils.removeTab(wizardTab);
 }
 
 add_setup(async () => {
   // Load the initial tab at example.com. This makes it so that if
-  // we're using the new migration wizard, we'll load the about:preferences
-  // page in a new tab rather than overtaking the initial one. This
-  // makes it easier to be consistent with closing and opening
-  // behaviours between the two kinds of migration wizards.
+  // when we load the wizard in about:preferences, we'll load the
+  // about:preferences page in a new tab rather than overtaking the
+  // initial one. This makes cleanup of the wizard more explicit, since
+  // we can just close the tab.
   let browser = gBrowser.selectedBrowser;
   BrowserTestUtils.startLoadingURIString(browser, "https://example.com");
   await BrowserTestUtils.browserLoaded(browser);
@@ -62,76 +31,42 @@ add_setup(async () => {
 
 /**
  * Tests that the entrypoint passed to MigrationUtils.showMigrationWizard gets
- * written to both the FX_MIGRATION_ENTRY_POINT_CATEGORICAL histogram, as well
- * as the legacy FX_MIGRATION_ENTRY_POINT histogram (but only if using the old
- * wizard window).
+ * written to both the FX_MIGRATION_ENTRY_POINT_CATEGORICAL histogram.
  */
-add_task(async function test_legacy_wizard() {
-  for (let contentModalEnabled of [true, false]) {
-    info("Testing with content modal enabled: " + contentModalEnabled);
-    await SpecialPowers.pushPrefEnv({
-      set: [[CONTENT_MODAL_ENABLED_PREF, contentModalEnabled]],
-    });
+add_task(async function test_entrypoints() {
+  let histogram = TelemetryTestUtils.getAndClearHistogram(HISTOGRAM_ID);
 
-    let histogram = TelemetryTestUtils.getAndClearHistogram(HISTOGRAM_ID);
-    let legacyHistogram =
-      TelemetryTestUtils.getAndClearHistogram(LEGACY_HISTOGRAM_ID);
-
-    // Let's arbitrarily pick the "Bookmarks" entrypoint, and make sure this
-    // is recorded.
-    await showThenCloseMigrationWizardViaEntrypoint(
-      MigrationUtils.MIGRATION_ENTRYPOINTS.BOOKMARKS
-    );
-    let entrypointId = MigrationUtils.getLegacyMigrationEntrypoint(
-      MigrationUtils.MIGRATION_ENTRYPOINTS.BOOKMARKS
-    );
-
-    TelemetryTestUtils.assertHistogram(histogram, entrypointId, 1);
-
-    if (!contentModalEnabled) {
-      TelemetryTestUtils.assertHistogram(legacyHistogram, entrypointId, 1);
-    }
-
-    histogram = TelemetryTestUtils.getAndClearHistogram(HISTOGRAM_ID);
-    legacyHistogram =
-      TelemetryTestUtils.getAndClearHistogram(LEGACY_HISTOGRAM_ID);
-
-    // Now let's pick the "Preferences" entrypoint, and make sure this
-    // is recorded.
-    await showThenCloseMigrationWizardViaEntrypoint(
-      MigrationUtils.MIGRATION_ENTRYPOINTS.PREFERENCES
-    );
-    entrypointId = MigrationUtils.getLegacyMigrationEntrypoint(
-      MigrationUtils.MIGRATION_ENTRYPOINTS.PREFERENCES
-    );
-
-    TelemetryTestUtils.assertHistogram(histogram, entrypointId, 1);
-    if (!contentModalEnabled) {
-      TelemetryTestUtils.assertHistogram(legacyHistogram, entrypointId, 1);
-    }
-
-    histogram = TelemetryTestUtils.getAndClearHistogram(HISTOGRAM_ID);
-    legacyHistogram =
-      TelemetryTestUtils.getAndClearHistogram(LEGACY_HISTOGRAM_ID);
-
-    // Finally, check the fallback by passing in something invalid as an entrypoint.
-    await showThenCloseMigrationWizardViaEntrypoint(undefined);
-    entrypointId = MigrationUtils.getLegacyMigrationEntrypoint(
-      MigrationUtils.MIGRATION_ENTRYPOINTS.UNKNOWN
-    );
-
-    TelemetryTestUtils.assertHistogram(histogram, entrypointId, 1);
-    if (!contentModalEnabled) {
-      TelemetryTestUtils.assertHistogram(legacyHistogram, entrypointId, 1);
-    }
-  }
-
-  // We should make sure that the migration.time_to_produce_legacy_migrator_list
-  // scalar was set, since we know that at least one legacy migration wizard has
-  // been opened.
-  let scalars = TelemetryTestUtils.getProcessScalars("parent", false, false);
-  Assert.ok(
-    scalars["migration.time_to_produce_legacy_migrator_list"] > 0,
-    "Non-zero scalar value recorded for migration.time_to_produce_migrator_list"
+  // Let's arbitrarily pick the "Bookmarks" entrypoint, and make sure this
+  // is recorded.
+  await showThenCloseMigrationWizardViaEntrypoint(
+    MigrationUtils.MIGRATION_ENTRYPOINTS.BOOKMARKS
   );
+  let entrypointId = MigrationUtils.getLegacyMigrationEntrypoint(
+    MigrationUtils.MIGRATION_ENTRYPOINTS.BOOKMARKS
+  );
+
+  TelemetryTestUtils.assertHistogram(histogram, entrypointId, 1);
+
+  histogram = TelemetryTestUtils.getAndClearHistogram(HISTOGRAM_ID);
+
+  // Now let's pick the "Preferences" entrypoint, and make sure this
+  // is recorded.
+  await showThenCloseMigrationWizardViaEntrypoint(
+    MigrationUtils.MIGRATION_ENTRYPOINTS.PREFERENCES
+  );
+  entrypointId = MigrationUtils.getLegacyMigrationEntrypoint(
+    MigrationUtils.MIGRATION_ENTRYPOINTS.PREFERENCES
+  );
+
+  TelemetryTestUtils.assertHistogram(histogram, entrypointId, 1);
+
+  histogram = TelemetryTestUtils.getAndClearHistogram(HISTOGRAM_ID);
+
+  // Finally, check the fallback by passing in something invalid as an entrypoint.
+  await showThenCloseMigrationWizardViaEntrypoint(undefined);
+  entrypointId = MigrationUtils.getLegacyMigrationEntrypoint(
+    MigrationUtils.MIGRATION_ENTRYPOINTS.UNKNOWN
+  );
+
+  TelemetryTestUtils.assertHistogram(histogram, entrypointId, 1);
 });
