09454678f1986053fc2110f39acff3d2ca2891c8
Andreas Pehrson <apehrson@mozilla.com>
2023-10-16 10:42:21 +0200

    Bug 1857862 - Vendor libwebrtc from 58e97b8600
    Upstream commit: https://webrtc.googlesource.com/src/+/58e97b86003e1b34677eaf0d9aae556c4cf7b2f6
        Removes AllowWgcDesktopCapturer feature flag
    
        This flag is no longer used in Chrome and can now be removed.
    
        Bug: chromium:1314868
        Change-Id: Id91b3352dc7ec0543d54894cc206a6e0c7667e9e
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/309960
        Commit-Queue: Alexander Cooper <alcooper@chromium.org>
        Reviewed-by: Alexander Cooper <alcooper@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#40337}

--- third_party/libwebrtc/modules/desktop_capture/desktop_capture_options.h
+++ third_party/libwebrtc/modules/desktop_capture/desktop_capture_options.h
@@ -159,10 +159,6 @@ class RTC_EXPORT DesktopCaptureOptions {
   }
 
 #if defined(RTC_ENABLE_WIN_WGC)
-  // TODO(henrika): to be removed.
-  bool allow_wgc_capturer() const { return allow_wgc_capturer_; }
-  void set_allow_wgc_capturer(bool allow) { allow_wgc_capturer_ = allow; }
-
   // This flag enables the WGC capturer for capturing the screen.
   // This capturer should offer similar or better performance than the cropping
   // capturer without the disadvantages listed above. However, the WGC capturer
@@ -248,7 +244,6 @@ class RTC_EXPORT DesktopCaptureOptions {
   bool allow_directx_capturer_ = false;
   bool allow_cropping_window_capturer_ = false;
 #if defined(RTC_ENABLE_WIN_WGC)
-  bool allow_wgc_capturer_ = false;
   bool allow_wgc_screen_capturer_ = false;
   bool allow_wgc_window_capturer_ = false;
   bool allow_wgc_capturer_fallback_ = false;
