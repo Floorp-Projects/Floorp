706e7d5bc03ae69fa839a403c379e3f3b622e8df
Andreas Pehrson <apehrson@mozilla.com>
2023-10-16 12:45:22 +0200

    Bug 1857862 - Vendor libwebrtc from becfe2e571
    Upstream commit: https://webrtc.googlesource.com/src/+/becfe2e5712d3f446c03348d0af1501747a65345
        Make opengl targets ios-only.
    
        Bug: b/288827308
        Change-Id: I5d37db079646eb8276d4f66a0fc33a585aad38e9
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/311100
        Commit-Queue: Mirko Bonadei <mbonadei@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40385}

--- third_party/libwebrtc/sdk/objc/components/renderer/opengl/RTCOpenGLDefines.h
+++ third_party/libwebrtc/sdk/objc/components/renderer/opengl/RTCOpenGLDefines.h
@@ -10,7 +10,6 @@
 
 #import <Foundation/Foundation.h>
 
-#if TARGET_OS_IPHONE
 #define RTC_PIXEL_FORMAT GL_LUMINANCE
 #define SHADER_VERSION
 #define VERTEX_SHADER_IN "attribute"
@@ -22,16 +21,3 @@
 
 @class EAGLContext;
 typedef EAGLContext GlContextType;
-#else
-#define RTC_PIXEL_FORMAT GL_RED
-#define SHADER_VERSION "#version 150\n"
-#define VERTEX_SHADER_IN "in"
-#define VERTEX_SHADER_OUT "out"
-#define FRAGMENT_SHADER_IN "in"
-#define FRAGMENT_SHADER_OUT "out vec4 fragColor;\n"
-#define FRAGMENT_SHADER_COLOR "fragColor"
-#define FRAGMENT_SHADER_TEXTURE "texture"
-
-@class NSOpenGLContext;
-typedef NSOpenGLContext GlContextType;
-#endif
