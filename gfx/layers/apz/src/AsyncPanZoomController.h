96fa6a17a37e943c5b46417583c11ad7087370c9
Natalia Csoregi <ncsoregi@mozilla.com>
2023-10-16 05:52:53 +0300

    Backed out 3 changesets (bug 1827975, bug 1831580) for causing scrollend failures. CLOSED TREE
    Backed out changeset f3de6f924333 (bug 1831580)
    Backed out changeset d9d3c8186de0 (bug 1827975)
    Backed out changeset 93a891338556 (bug 1827975)

--- gfx/layers/apz/src/AsyncPanZoomController.h
+++ gfx/layers/apz/src/AsyncPanZoomController.h
@@ -1477,13 +1477,6 @@ class AsyncPanZoomController {
    */
   void DispatchStateChangeNotification(PanZoomState aOldState,
                                        PanZoomState aNewState);
-
-  /**
-   * Send a TransformBegin notification followed by a TransformEnd
-   * notification.
-   */
-  void SendTransformBeginAndEnd();
-
   /**
    * Internal helpers for checking general state of this apzc.
    */
@@ -1610,8 +1603,6 @@ class AsyncPanZoomController {
   void SmoothScrollTo(CSSSnapDestination&& aDestination,
                       ScrollTriggeredByScript aTriggeredByScript,
                       const ScrollOrigin& aOrigin);
-
-  ParentLayerPoint ConvertDestinationToDelta(CSSPoint& aDestination) const;
 
   // Start a smooth-scrolling animation to the given destination, with MSD
   // physics that is suited for scroll-snapping.
