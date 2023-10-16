6f07a8932778bb004f791ac29f3ba50a79dc184d
Emilio Cobos Álvarez <emilio@crisal.io>
2023-10-16 17:15:50 +0000

    Bug 1857743 - Fix inactive color computation for windows-accent-in-titlebar. r=win-reviewers,rkraesig
    This is more like what other browsers do, and makes more sense, it
    actually blends the text-color with that opacity, which is what the
    front-end uses.
    
    Differential Revision: https://phabricator.services.mozilla.com/D190955

--- widget/windows/nsLookAndFeel.cpp
+++ widget/windows/nsLookAndFeel.cpp
@@ -887,18 +887,28 @@ auto nsLookAndFeel::ComputeTitlebarColors() -> TitlebarColors {
     result.mInactiveLight.mFg = result.mInactiveDark.mFg =
         *result.mAccentInactiveText;
   } else {
-    // The 153 matches the .6 opacity from browser-aero.css, which says it
-    // was calculated to match the opacity change of Windows Explorer
-    // titlebar text change for inactive windows.
+    // The 153 matches the .6 opacity the front-end uses, which was calculated
+    // to match the opacity change of Windows Explorer titlebar text change
+    // for inactive windows.
+    constexpr uint8_t kTextAlpha = 153;
+    // This is hand-picked to .8 to change the accent color a bit but not too
+    // much.
+    constexpr uint8_t kBgAlpha = 208;
+    const auto BlendWithAlpha = [](nscolor aBg, nscolor aFg,
+                                   uint8_t aAlpha) -> nscolor {
+      return NS_ComposeColors(
+          aBg, NS_RGBA(NS_GET_R(aFg), NS_GET_G(aFg), NS_GET_B(aFg), aAlpha));
+    };
+
     result.mInactiveLight.mBg =
-        NS_ComposeColors(*result.mAccent, NS_RGBA(255, 255, 255, 153));
+        BlendWithAlpha(NS_RGB(255, 255, 255), *result.mAccent, kBgAlpha);
     result.mInactiveLight.mFg =
-        NS_ComposeColors(*result.mAccentText, NS_RGBA(255, 255, 255, 153));
+        BlendWithAlpha(*result.mAccent, *result.mAccentText, kTextAlpha);
 
     result.mInactiveDark.mBg =
-        NS_ComposeColors(*result.mAccent, NS_RGBA(0, 0, 0, 153));
+        BlendWithAlpha(NS_RGB(0, 0, 0), *result.mAccent, kBgAlpha);
     result.mInactiveDark.mFg =
-        NS_ComposeColors(*result.mAccentText, NS_RGBA(0, 0, 0, 153));
+        BlendWithAlpha(*result.mAccent, *result.mAccentText, kTextAlpha);
   }
   return result;
 }
