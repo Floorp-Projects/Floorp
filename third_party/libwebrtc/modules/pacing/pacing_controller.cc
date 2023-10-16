b05b877aa93a69c76aba9da9ca6a3041c7fcc612
Andreas Pehrson <apehrson@mozilla.com>
2023-10-16 13:44:31 +0200

    Bug 1857862 - Vendor libwebrtc from 6a4f409241
    Upstream commit: https://webrtc.googlesource.com/src/+/6a4f409241c434fb34faf99a025179c1a674f393
        pacing_controller: add millisecond units to warning message
    
        Bug: None
        Change-Id: I8a5c5ca6a641a74213116a734f3c19c6972e5916
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/311400
        Reviewed-by: Philip Eliasson <philipel@webrtc.org>
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40418}

--- third_party/libwebrtc/modules/pacing/pacing_controller.cc
+++ third_party/libwebrtc/modules/pacing/pacing_controller.cc
@@ -291,9 +291,9 @@ TimeDelta PacingController::UpdateTimeAndGetElapsed(Timestamp now) {
   TimeDelta elapsed_time = now - last_process_time_;
   last_process_time_ = now;
   if (elapsed_time > kMaxElapsedTime) {
-    RTC_LOG(LS_WARNING) << "Elapsed time (" << elapsed_time.ms()
-                        << " ms) longer than expected, limiting to "
-                        << kMaxElapsedTime.ms();
+    RTC_LOG(LS_WARNING) << "Elapsed time (" << ToLogString(elapsed_time)
+                        << ") longer than expected, limiting to "
+                        << ToLogString(kMaxElapsedTime);
     elapsed_time = kMaxElapsedTime;
   }
   return elapsed_time;
