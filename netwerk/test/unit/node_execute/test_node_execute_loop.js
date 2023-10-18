8478cbd6c6e7717ff17a4f11f7a5d490916fdba5
Valentin Gosu <valentin.gosu@gmail.com>
2023-10-18 10:07:18 +0000

    Bug 1772329 - Force kill node process and subprocesses in xpcshell tests at the end of the test r=jmaher,necko-reviewers,kershaw
    Added the start_new_session=True argument to Popen so that the node instance gets its own pgid.
    On POSIX platforms the server shutdown will now be done using os.killpg() instead of process.terminate()
    
    Differential Revision: https://phabricator.services.mozilla.com/D189212

---
+++ netwerk/test/unit/node_execute/test_node_execute_loop.js
@@ -0,0 +1,20 @@
+// This test checks that the interaction between NodeServer.execute defined in
+// httpd.js and the node server that we're interacting with defined in
+// moz-http2.js is working properly.
+// This test spawns a node server that loops on while true and makes sure
+// the the process group is killed by runxpcshelltests.py at exit.
+// See bug 1855174
+
+"use strict";
+
+const { NodeServer } = ChromeUtils.importESModule(
+  "resource://testing-common/httpd.sys.mjs"
+);
+
+add_task(async function killOnEnd() {
+  let id = await NodeServer.fork();
+  // Make the forked subprocess hang forever.
+  NodeServer.execute(id, "while (true) {}").catch(e => {});
+  await new Promise(resolve => do_timeout(10, resolve));
+  // Should get killed at the end of the test by the harness.
+});
