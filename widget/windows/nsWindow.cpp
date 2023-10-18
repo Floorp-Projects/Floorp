bebceb1b2144fd03d22d988ec383a797a6133f01
Tooru Fujisawa <arai_a@mac.com>
2023-10-18 01:54:43 +0000

    Bug 1846407 - Use enum class for Task::Run return value. r=bas,win-reviewers,rkraesig
    Differential Revision: https://phabricator.services.mozilla.com/D185043

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -608,17 +608,17 @@ class InitializeVirtualDesktopManagerTask : public Task {
   }
 #endif
 
-  virtual bool Run() override {
+  virtual TaskResult Run() override {
     RefPtr<IVirtualDesktopManager> desktopManager;
     HRESULT hr = ::CoCreateInstance(
         CLSID_VirtualDesktopManager, NULL, CLSCTX_INPROC_SERVER,
         __uuidof(IVirtualDesktopManager), getter_AddRefs(desktopManager));
     if (FAILED(hr)) {
-      return true;
+      return TaskResult::Complete;
     }
 
     gVirtualDesktopManager = desktopManager;
-    return true;
+    return TaskResult::Complete;
   }
 };
 
