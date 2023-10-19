74132d3d22abf723bb31be49895f013b3af8efba
Emilio Cobos Álvarez <emilio@crisal.io>
2023-10-19 17:24:23 +0200

    Bug 1855763 - Add zoom entry to property_database.js
    MANUAL PUSH: Trivial orange fix CLOSED TREE

--- layout/style/test/property_database.js
+++ layout/style/test/property_database.js
@@ -14010,6 +14010,24 @@ if (IsCSSPropertyPrefEnabled("layout.css.prefixes.transforms")) {
   });
 }
 
+if (IsCSSPropertyPrefEnabled("layout.css.zoom.enabled")) {
+  Object.assign(gCSSProperties, {
+    "zoom": {
+      domProp: "zoom",
+      inherited: false,
+      type: CSS_TYPE_LONGHAND,
+      initial_values: ["normal", "1", "100%", "0", "0%"],
+      other_values: [
+        "1.5",
+        "2",
+        "150%",
+        "200%",
+      ],
+      invalid_values: ["-1", "-40%"],
+    }
+  });
+}
+
 // Copy aliased properties' fields from their alias targets. Keep this logic
 // at the bottom of this file to ensure all the aliased properties are
 // processed.
