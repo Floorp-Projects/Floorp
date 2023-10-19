fc5629ebe5b404292f4af65f332f16b002b1d89a
Andreas Pehrson <apehrson@mozilla.com>
2023-10-19 22:17:52 +0000

    Bug 1859880 - Fix hw decode of WebRTC streams with WMF on Windows. r=media-playback-reviewers,padenot
    This patch should fix hw decode with WebRTC for all codecs supported by WMF.
    
    Differential Revision: https://phabricator.services.mozilla.com/D191447

--- dom/media/platforms/wmf/WMFVideoMFTManager.cpp
+++ dom/media/platforms/wmf/WMFVideoMFTManager.cpp
@@ -412,9 +412,11 @@ WMFVideoMFTManager::SetDecoderMediaTypes() {
 
   UINT32 fpsDenominator = 1000;
   UINT32 fpsNumerator = static_cast<uint32_t>(mFramerate * fpsDenominator);
-  hr = MFSetAttributeRatio(inputType, MF_MT_FRAME_RATE, fpsNumerator,
-                           fpsDenominator);
-  NS_ENSURE_TRUE(SUCCEEDED(hr), hr);
+  if (fpsNumerator > 0) {
+    hr = MFSetAttributeRatio(inputType, MF_MT_FRAME_RATE, fpsNumerator,
+                             fpsDenominator);
+    NS_ENSURE_TRUE(SUCCEEDED(hr), hr);
+  }
 
   RefPtr<IMFMediaType> outputType;
   hr = wmf::MFCreateMediaType(getter_AddRefs(outputType));
@@ -428,9 +430,11 @@ WMFVideoMFTManager::SetDecoderMediaTypes() {
                           mVideoInfo.ImageRect().height);
   NS_ENSURE_TRUE(SUCCEEDED(hr), hr);
 
-  hr = MFSetAttributeRatio(outputType, MF_MT_FRAME_RATE, fpsNumerator,
-                           fpsDenominator);
-  NS_ENSURE_TRUE(SUCCEEDED(hr), hr);
+  if (fpsNumerator > 0) {
+    hr = MFSetAttributeRatio(outputType, MF_MT_FRAME_RATE, fpsNumerator,
+                             fpsDenominator);
+    NS_ENSURE_TRUE(SUCCEEDED(hr), hr);
+  }
 
   GUID outputSubType = [&]() {
     switch (mVideoInfo.mColorDepth) {
