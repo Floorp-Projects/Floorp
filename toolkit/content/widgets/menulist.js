a35fdc6b1c3e999aa9e041c36b3bcac062b53e13
Emilio Cobos Álvarez <emilio@crisal.io>
2023-10-23 16:01:46 +0000

    Bug 1860385 - Clean up menulist styling. r=desktop-theme-reviewers,dao
    * Rather than inheriting the native attribute, use :host([native]).
     * Remove unused background-color from native rules (background doesn't
       show because of native appearance).
     * Remove unnecessary !important.
     * Refactor high contrast backgrounds on Windows to avoid !important.
     * Make high contrast on windows not alter sizing.
     * Remove default namespaces. This sheet applies in a shadow tree, so
       there's no random stuff.
    
    Depends on D191561
    
    Differential Revision: https://phabricator.services.mozilla.com/D191562

--- toolkit/content/widgets/menulist.js
+++ toolkit/content/widgets/menulist.js
@@ -87,12 +87,10 @@
 
     static get inheritedAttributes() {
       return {
-        "#label-box": "native",
-        image: "src=image,native",
-        "#label": "value=label,crop,accesskey,highlightable,native",
-        "#highlightable-label":
-          "text=label,crop,accesskey,highlightable,native",
-        dropmarker: "disabled,open,native",
+        image: "src=image",
+        "#label": "value=label,crop,accesskey",
+        "#highlightable-label": "text=label,crop,accesskey",
+        dropmarker: "disabled,open",
       };
     }
 
