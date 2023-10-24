a2d185867d44480e4df4711dcb7bfce0cf31bbb5
Brad Werth <bwerth@mozilla.com>
2023-10-24 20:01:49 +0000

    Bug 1658432: Don't remove wrapping CALayer from its superview outside of Commit. r=mac-reviewers,mstange, a=dsmith
    This change lets NativeLayerRootCA::Representation::Commit be the only
    point where sublayers are added and removed from the root CALayer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D191673

--- gfx/layers/NativeLayerCA.mm
+++ gfx/layers/NativeLayerCA.mm
@@ -1638,7 +1638,6 @@ bool NativeLayerCA::Representation::ApplyChanges(
     mContentCALayer = nil;
     [mOpaquenessTintLayer release];
     mOpaquenessTintLayer = nil;
-    [mWrappingCALayer removeFromSuperlayer];
     [mWrappingCALayer release];
     mWrappingCALayer = nil;
   }
