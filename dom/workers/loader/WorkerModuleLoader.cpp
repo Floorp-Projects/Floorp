d155a7b1a0e393700bbd670bc2c84dbf04f0830a
Andrew Sutherland <asutherland@asutherland.org>
2023-10-24 20:34:25 +0000

    Bug 1856090 - Improve ServiceWorker state propagation.  a=RyanVM
    Original Revision: https://phabricator.services.mozilla.com/D190245
    
    Differential Revision: https://phabricator.services.mozilla.com/D194843

--- dom/workers/loader/WorkerModuleLoader.cpp
+++ dom/workers/loader/WorkerModuleLoader.cpp
@@ -49,9 +49,10 @@ already_AddRefed<ModuleLoadRequest> WorkerModuleLoader::CreateStaticImport(
   // See Discussion in https://github.com/w3c/webappsec-csp/issues/336
   Maybe<ClientInfo> clientInfo = GetGlobalObject()->GetClientInfo();
 
-  RefPtr<WorkerLoadContext> loadContext =
-      new WorkerLoadContext(WorkerLoadContext::Kind::StaticImport, clientInfo,
-                            aParent->GetWorkerLoadContext()->mScriptLoader);
+  RefPtr<WorkerLoadContext> loadContext = new WorkerLoadContext(
+      WorkerLoadContext::Kind::StaticImport, clientInfo,
+      aParent->GetWorkerLoadContext()->mScriptLoader,
+      aParent->GetWorkerLoadContext()->mOnlyExistingCachedResourcesAllowed);
   RefPtr<ModuleLoadRequest> request = new ModuleLoadRequest(
       aURI, aParent->mFetchOptions, SRIMetadata(), aParent->mURI, loadContext,
       false, /* is top level */
@@ -90,6 +91,8 @@ already_AddRefed<ModuleLoadRequest> WorkerModuleLoader::CreateDynamicImport(
   }
 
   // Not supported for Service Workers.
+  // https://github.com/w3c/ServiceWorker/issues/1585 covers existing discussion
+  // about potentially supporting use of import().
   if (workerPrivate->IsServiceWorker()) {
     return nullptr;
   }
@@ -110,9 +113,16 @@ already_AddRefed<ModuleLoadRequest> WorkerModuleLoader::CreateDynamicImport(
 
   Maybe<ClientInfo> clientInfo = GetGlobalObject()->GetClientInfo();
 
-  RefPtr<WorkerLoadContext> context =
-      new WorkerLoadContext(WorkerLoadContext::Kind::DynamicImport, clientInfo,
-                            GetCurrentScriptLoader());
+  RefPtr<WorkerLoadContext> context = new WorkerLoadContext(
+      WorkerLoadContext::Kind::DynamicImport, clientInfo,
+      GetCurrentScriptLoader(),
+      // When dynamic import is supported in ServiceWorkers,
+      // the current plan in onlyExistingCachedResourcesAllowed
+      // is that only existing cached resources will be
+      // allowed.  (`import()` will not be used for caching
+      // side effects, but instead a specific method will be
+      // used during installation.)
+      true);
 
   RefPtr<ModuleLoadRequest> request = new ModuleLoadRequest(
       aURI, options, SRIMetadata(), baseURL, context, true,
