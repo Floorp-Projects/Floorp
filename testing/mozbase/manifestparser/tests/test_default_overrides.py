69d342407ae3330336fe474f35e1519e699e37fc
Mike Hommey <mh+mozilla@glandium.org>
2023-10-24 21:36:23 +0000

    Bug 1859997 - Switch manifestparser to toml instead of tomlkit. r=jmaher,ahal,mach-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D191394

--- testing/mozbase/manifestparser/tests/test_default_overrides.py
+++ testing/mozbase/manifestparser/tests/test_default_overrides.py
@@ -78,34 +78,6 @@ class TestDefaultSkipif(unittest.TestCase):
                     deepstrip(test["skip-if"]), "os == 'win' && debug\ndebug"
                 )
 
-    def test_defaults_toml_multiline(self):
-        default = os.path.join(here, "default-skipif-multiline.toml")
-        parser = ManifestParser(manifests=(default,), use_toml=True)
-        for test in parser.tests:
-            if test["name"] == "test1":
-                self.assertEqual(
-                    deepstrip(test["skip-if"]), "os == 'win' && debug\ndebug"
-                )
-            elif test["name"] == "test2":
-                self.assertEqual(
-                    deepstrip(test["skip-if"]), "os == 'win' && debug\nos == 'linux'"
-                )
-            elif test["name"] == "test3":
-                self.assertEqual(
-                    deepstrip(test["skip-if"]), "os == 'win' && debug\nos == 'win'"
-                )
-            elif test["name"] == "test4":
-                self.assertEqual(
-                    deepstrip(test["skip-if"]),
-                    "os == 'win' && debug\nos == 'win' && debug",
-                )
-            elif test["name"] == "test5":
-                self.assertEqual(deepstrip(test["skip-if"]), "os == 'win' && debug")
-            elif test["name"] == "test6":
-                self.assertEqual(
-                    deepstrip(test["skip-if"]), "os == 'win' && debug\ndebug"
-                )
-
 
 class TestDefaultSupportFiles(unittest.TestCase):
     """Tests combining support-files field in [DEFAULT] with the value for a test"""
