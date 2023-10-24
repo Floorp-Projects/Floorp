5857fdad80f8a193a3f8f78d48991ba8c762b6bb
alwu <alwu@mozilla.com>
2023-10-24 20:42:12 +0000

    Bug 1844169 - part1 : move ScopedHString to WMFUtils. r=media-playback-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D183932

--- dom/media/platforms/wmf/MFContentProtectionManager.cpp
+++ dom/media/platforms/wmf/MFContentProtectionManager.cpp
@@ -4,7 +4,6 @@
 
 #include "MFContentProtectionManager.h"
 
-#include <hstring.h>
 #include <winnt.h>
 
 #include "MFMediaEngineUtils.h"
@@ -17,18 +16,6 @@ using Microsoft::WRL::ComPtr;
 #define LOG(msg, ...)                         \
   MOZ_LOG(gMFMediaEngineLog, LogLevel::Debug, \
           ("MFContentProtectionManager=%p, " msg, this, ##__VA_ARGS__))
-
-class ScopedHString final {
- public:
-  explicit ScopedHString(const WCHAR aCharArray[]) {
-    WindowsCreateString(aCharArray, wcslen(aCharArray), &mString);
-  }
-  ~ScopedHString() { WindowsDeleteString(mString); }
-  const HSTRING& Get() { return mString; }
-
- private:
-  HSTRING mString;
-};
 
 MFContentProtectionManager::MFContentProtectionManager() {
   MOZ_COUNT_CTOR(MFContentProtectionManager);
