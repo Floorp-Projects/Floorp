ecaa3d70ebe3492530411b816c6e715be9531f02
Mike Kaply <mozilla@kaply.com>
2023-10-25 12:49:40 +0000

    Bug 1860856 - Allow cert/devices dialogs to be disabled by policy. r=keeler
    Differential Revision: https://phabricator.services.mozilla.com/D191770

--- browser/components/enterprisepolicies/Policies.sys.mjs
+++ browser/components/enterprisepolicies/Policies.sys.mjs
@@ -1741,6 +1741,8 @@ export var Policies = {
       const allowedSecurityPrefs = [
         "security.block_fileuri_script_with_wrong_mime",
         "security.default_personal_cert",
+        "security.disable_button.openCertManager",
+        "security.disable_button.openDeviceManager",
         "security.insecure_connection_text.enabled",
         "security.insecure_connection_text.pbmode.enabled",
         "security.mixed_content.block_active_content",
