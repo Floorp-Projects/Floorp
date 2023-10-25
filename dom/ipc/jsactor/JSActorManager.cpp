9c5eef32d0dc8ce15343b30641155c490a49d791
Greg Tatum <tatum.creative@gmail.com>
2023-10-25 22:15:11 +0000

    Bug 1815339 - Ensure dangling MessagePorts are cleaned up in JSActorManager; r=asuth
    The previous behavior was triggering an assertion in the
    StructuredCloneHolder destructor. In this particular case, the dangling
    MessagePorts can safely ignored.
    
    Differential Revision: https://phabricator.services.mozilla.com/D191472

--- dom/ipc/jsactor/JSActorManager.cpp
+++ dom/ipc/jsactor/JSActorManager.cpp
@@ -8,6 +8,7 @@
 
 #include "mozilla/dom/AutoEntryScript.h"
 #include "mozilla/dom/JSActorService.h"
+#include "mozilla/dom/MessagePort.h"
 #include "mozilla/dom/PWindowGlobal.h"
 #include "mozilla/ipc/ProtocolUtils.h"
 #include "mozilla/AppShutdown.h"
@@ -188,6 +189,16 @@ void JSActorManager::ReceiveRawMessage(
   JS::Rooted<JS::Value> data(cx);
   if (aData) {
     aData->Read(cx, &data, error);
+    // StructuredCloneHolder populates an array of ports for MessageEvent.ports
+    // which we don't need, but which StructuredCloneHolder's destructor will
+    // assert on for thread safety reasons (that do not apply in this case) if
+    // we do not consume the array.  It's possible for the Read call above to
+    // populate this array even in event of an error, so we must consume the
+    // array before processing the error.
+    nsTArray<RefPtr<MessagePort>> ports = aData->TakeTransferredPorts();
+    // Cast to void so that the ports will actually be moved, and then
+    // discarded.
+    (void)ports;
     if (error.Failed()) {
       CHILD_DIAGNOSTIC_ASSERT(CycleCollectedJSRuntime::Get()->OOMReported(),
                               "Should not receive non-decodable data");
