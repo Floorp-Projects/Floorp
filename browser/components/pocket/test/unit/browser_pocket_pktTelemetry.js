3354d4b582c5c666e232b0d272edff0bb888a689
Chris H-C <chutten@mozilla.com>
2023-10-26 07:20:42 +0000

    Bug 1857324 - Add new "pocket-button" ping r=thecount
    Depends on D190857
    
    Differential Revision: https://phabricator.services.mozilla.com/D190858

--- browser/components/pocket/test/unit/browser_pocket_pktTelemetry.js
+++ browser/components/pocket/test/unit/browser_pocket_pktTelemetry.js
@@ -52,3 +52,70 @@ test_runner(async function test_generateStructuredIngestionEndpoint({
     "https://incoming.telemetry.mozilla.org/submit/activity-stream/pocket-button/1/7fd5a1ac-6089-4212-91a7-fcdec1d2f533"
   );
 });
+
+test_runner(async function test_submitPocketButtonPing({ sandbox }) {
+  const creationDate = "19640";
+  const impressionId = "{422e3da9-c694-4fd2-b676-8ae070156128}";
+  sandbox.stub(pktTelemetry, "impressionId").value(impressionId);
+  sandbox.stub(pktTelemetry, "_profileCreationDate").returns(creationDate);
+
+  const eventAction = "some action like 'click'";
+  const eventSource = "some source like 'save_button'";
+
+  const assertConstantStuff = () => {
+    Assert.equal(
+      "{" + Glean.pocketButton.impressionId.testGetValue() + "}",
+      impressionId
+    );
+    Assert.equal(Glean.pocketButton.pocketLoggedInStatus.testGetValue(), false);
+    Assert.equal(
+      Glean.pocketButton.profileCreationDate.testGetValue(),
+      creationDate
+    );
+
+    Assert.equal(Glean.pocketButton.eventAction.testGetValue(), eventAction);
+    Assert.equal(Glean.pocketButton.eventSource.testGetValue(), eventSource);
+  };
+
+  let submitted = false;
+  GleanPings.pocketButton.testBeforeNextSubmit(() => {
+    submitted = true;
+    assertConstantStuff();
+    Assert.equal(Glean.pocketButton.eventPosition.testGetValue(), null);
+    Assert.equal(Glean.pocketButton.model.testGetValue(), null);
+  });
+
+  pktTelemetry.submitPocketButtonPing(eventAction, eventSource);
+  Assert.ok(submitted, "Ping submitted successfully");
+
+  submitted = false;
+  GleanPings.pocketButton.testBeforeNextSubmit(() => {
+    submitted = true;
+    assertConstantStuff();
+    Assert.equal(Glean.pocketButton.eventPosition.testGetValue(), 0);
+    Assert.equal(Glean.pocketButton.model.testGetValue(), null);
+  });
+
+  pktTelemetry.submitPocketButtonPing(eventAction, eventSource, 0, null);
+  Assert.ok(submitted, "Ping submitted successfully");
+
+  submitted = false;
+  GleanPings.pocketButton.testBeforeNextSubmit(() => {
+    submitted = true;
+    assertConstantStuff();
+    // falsey but not undefined positions will be omitted.
+    Assert.equal(Glean.pocketButton.eventPosition.testGetValue(), null);
+    Assert.equal(
+      Glean.pocketButton.model.testGetValue(),
+      "some-really-groovy-model"
+    );
+  });
+
+  pktTelemetry.submitPocketButtonPing(
+    eventAction,
+    eventSource,
+    false,
+    "some-really-groovy-model"
+  );
+  Assert.ok(submitted, "Ping submitted successfully");
+});
