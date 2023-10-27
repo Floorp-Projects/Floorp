9f11b7b281b04ada97a024faf4d45b157efa6dd7
Byron Campen <docfaraday@gmail.com>
2023-10-27 14:39:07 -0500

    Bug 1860685 - Vendor libwebrtc from 45a985c71d
    Upstream commit: https://webrtc.googlesource.com/src/+/45a985c71d8c0a999d28b20303baf0b5ddf5bb8c
        Check use_rtx() in PeerConnectionFactory::GetRtpSenderCapabilities
    
        Following https://webrtc-review.googlesource.com/c/src/+/262666, use_rtx() was checked in PeerConnectionFactory::GetRtpReceiverCapabilities but was missed in GetRtpSenderCapabilities. Therefore clients that hardcode use_rtx = false end up in an inconsistent state where RTX is not fully disabled.
    
        Bug: None
        Change-Id: Ice5f29a77c59e9081f9dd72c13c819024a34a7dd
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/316243
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40625}

--- third_party/libwebrtc/pc/connection_context.h
+++ third_party/libwebrtc/pc/connection_context.h
@@ -100,7 +100,10 @@ class ConnectionContext final
   // use RTX, but so far, no code has been found that sets it to false.
   // Kept in the API in order to ease introduction if we want to resurrect
   // the functionality.
-  bool use_rtx() { return true; }
+  bool use_rtx() { return use_rtx_; }
+
+  // For use by tests.
+  void set_use_rtx(bool use_rtx) { use_rtx_ = use_rtx; }
 
  protected:
   explicit ConnectionContext(PeerConnectionFactoryDependencies* dependencies);
@@ -139,6 +142,10 @@ class ConnectionContext final
   std::unique_ptr<rtc::PacketSocketFactory> default_socket_factory_
       RTC_GUARDED_BY(signaling_thread_);
   std::unique_ptr<SctpTransportFactoryInterface> const sctp_factory_;
+
+  // Controls whether to announce support for the the rfc4588 payload format
+  // for retransmitted video packets.
+  bool use_rtx_;
 };
 
 }  // namespace webrtc
