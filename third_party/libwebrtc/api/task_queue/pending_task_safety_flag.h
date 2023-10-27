1b35bd04f799f23c85fa85e3460fe8b13e4bb612
Byron Campen <docfaraday@gmail.com>
2023-10-27 14:31:42 -0500

    Bug 1860685 - Vendor libwebrtc from 66bf3f472c
    Upstream commit: https://webrtc.googlesource.com/src/+/66bf3f472c3cbf3872b77ff15c2b02bd1b3d88db
        Make PendingTaskSafetyFlag compatible with component builds
    
        Bug: chromium:1470992
        Change-Id: I06cec9cda36c9de75b970eaf709f9ed3b9f466b4
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/317620
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40618}

--- third_party/libwebrtc/api/task_queue/pending_task_safety_flag.h
+++ third_party/libwebrtc/api/task_queue/pending_task_safety_flag.h
@@ -19,6 +19,7 @@
 #include "api/sequence_checker.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/system/no_unique_address.h"
+#include "rtc_base/system/rtc_export.h"
 
 namespace webrtc {
 
@@ -58,7 +59,7 @@ namespace webrtc {
 //
 //   my_task_queue_->PostTask(SafeTask(safety_flag_, [this] { MyMethod(); }));
 //
-class PendingTaskSafetyFlag final
+class RTC_EXPORT PendingTaskSafetyFlag final
     : public rtc::RefCountedNonVirtual<PendingTaskSafetyFlag> {
  public:
   static rtc::scoped_refptr<PendingTaskSafetyFlag> Create();
@@ -116,7 +117,7 @@ class PendingTaskSafetyFlag final
 // This should be used by the class that wants tasks dropped after destruction.
 // The requirement is that the instance has to be constructed and destructed on
 // the same thread as the potentially dropped tasks would be running on.
-class ScopedTaskSafety final {
+class RTC_EXPORT ScopedTaskSafety final {
  public:
   ScopedTaskSafety() = default;
   explicit ScopedTaskSafety(rtc::scoped_refptr<PendingTaskSafetyFlag> flag)
@@ -140,7 +141,7 @@ class ScopedTaskSafety final {
 
 // Like ScopedTaskSafety, but allows construction on a different thread than
 // where the flag will be used.
-class ScopedTaskSafetyDetached final {
+class RTC_EXPORT ScopedTaskSafetyDetached final {
  public:
   ScopedTaskSafetyDetached() = default;
   ~ScopedTaskSafetyDetached() { flag_->SetNotAlive(); }
