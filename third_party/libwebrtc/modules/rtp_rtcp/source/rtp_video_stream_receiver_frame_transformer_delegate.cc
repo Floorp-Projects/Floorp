65e6f831114a1b2040d50971a16429af4bffb44d
Byron Campen <docfaraday@gmail.com>
2023-10-27 11:26:06 -0500

    Bug 1860685 - Vendor libwebrtc from 86162d94d5
    Upstream commit: https://webrtc.googlesource.com/src/+/86162d94d5b95db13e1c8e588ed19a88e1e3fca9
        Implement setMetadata for receiver encoded video frames
    
        This change adds a new function to RTPFrameObject to allow setting the
        RTPVideoHeader from VideoFrameMetadata.
    
        The setMetadata function in TransformableVideoReceiverFrame disallows
        changing anything other than frameID and dependencies.
    
        Change-Id: I74e55ffbe1f426b660c2e243b20358c6a6cc2ffd
        Bug: chromium:1464853
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/314963
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Commit-Queue: Palak Agarwal <agpalak@google.com>
        Reviewed-by: Tony Herre <herre@google.com>
        Cr-Commit-Position: refs/heads/main@{#40530}

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_video_stream_receiver_frame_transformer_delegate.cc
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_video_stream_receiver_frame_transformer_delegate.cc
@@ -46,7 +46,7 @@ class TransformableVideoReceiverFrame
   }
 
   uint8_t GetPayloadType() const override { return frame_->PayloadType(); }
-  uint32_t GetSsrc() const override { return metadata_.GetSsrc(); }
+  uint32_t GetSsrc() const override { return Metadata().GetSsrc(); }
   uint32_t GetTimestamp() const override { return frame_->Timestamp(); }
   void SetRTPTimestamp(uint32_t timestamp) override {
     frame_->SetTimestamp(timestamp);
@@ -63,9 +63,16 @@ class TransformableVideoReceiverFrame
 
   VideoFrameMetadata Metadata() const override { return metadata_; }
 
-  void SetMetadata(const VideoFrameMetadata&) override {
-    RTC_DCHECK_NOTREACHED()
-        << "TransformableVideoReceiverFrame::SetMetadata is not implemented";
+  void SetMetadata(const VideoFrameMetadata& metadata) override {
+    // Create |new_metadata| from existing metadata and change only frameId and
+    // dependencies.
+    VideoFrameMetadata new_metadata = Metadata();
+    new_metadata.SetFrameId(metadata.GetFrameId());
+    new_metadata.SetFrameDependencies(metadata.GetFrameDependencies());
+    RTC_DCHECK(new_metadata == metadata)
+        << "TransformableVideoReceiverFrame::SetMetadata can be only used to "
+           "change frameID and dependencies";
+    frame_->SetHeaderFromMetadata(new_metadata);
   }
 
   std::unique_ptr<RtpFrameObject> ExtractFrame() && {
