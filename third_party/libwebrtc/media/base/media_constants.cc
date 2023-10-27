a96bda8d0b07835bc3f97ce42e3c5f0ceb1f741c
Byron Campen <docfaraday@gmail.com>
2023-10-27 15:23:14 -0500

    Bug 1860685 - Vendor libwebrtc from 44943c8064
    Upstream commit: https://webrtc.googlesource.com/src/+/44943c8064b93a366cb8c8afdb881c009957e3df
        Add H265 codec name and profile/tier/level utils.
    
        This adds H265 codec name and profile/tier/level handling needed for
        H265 SDP negotiation.
    
        Bug: webrtc:13485
        Change-Id: I838b910042ce36f8ae3979c41a73ee46935c57d8
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/315900
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Reviewed-by: Sergey Silkin <ssilkin@webrtc.org>
        Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
        Cr-Commit-Position: refs/heads/main@{#40661}

--- third_party/libwebrtc/media/base/media_constants.cc
+++ third_party/libwebrtc/media/base/media_constants.cc
@@ -103,6 +103,7 @@ const char kVp8CodecName[] = "VP8";
 const char kVp9CodecName[] = "VP9";
 const char kAv1CodecName[] = "AV1";
 const char kH264CodecName[] = "H264";
+const char kH265CodecName[] = "H265";
 
 // RFC 6184 RTP Payload Format for H.264 video
 const char kH264FmtpProfileLevelId[] = "profile-level-id";
@@ -112,6 +113,16 @@ const char kH264FmtpSpropParameterSets[] = "sprop-parameter-sets";
 const char kH264FmtpSpsPpsIdrInKeyframe[] = "sps-pps-idr-in-keyframe";
 const char kH264ProfileLevelConstrainedBaseline[] = "42e01f";
 const char kH264ProfileLevelConstrainedHigh[] = "640c1f";
+
+// RFC 7798 RTP Payload Format for H.265 video
+const char kH265FmtpProfileSpace[] = "profile-space";
+const char kH265FmtpTierFlag[] = "tier-flag";
+const char kH265FmtpProfileId[] = "profile-id";
+const char kH265FmtpLevelId[] = "level-id";
+const char kH265FmtpProfileCompatibilityIndicator[] =
+    "profile-compatibility-indicator";
+const char kH265FmtpInteropConstraints[] = "interop-constraints";
+const char kH265FmtpTxMode[] = "tx-mode";
 
 const char kVP9ProfileId[] = "profile-id";
 
