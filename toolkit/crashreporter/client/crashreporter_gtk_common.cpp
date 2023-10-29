2b44c6ed89a8e56ba3f03ea2acb803582d4a65f3
freyalaluna <freya.laluna@colostate.edu>
2023-10-29 18:31:25 +0000

    Bug 1857749 - Removed duplicate inclusion of 'crashreporter.h' in crashreporter_gtk_common.cpp r=sylvestre DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D191809

--- toolkit/crashreporter/client/crashreporter_gtk_common.cpp
+++ toolkit/crashreporter/client/crashreporter_gtk_common.cpp
@@ -3,8 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "crashreporter.h"
-
 #include <unistd.h>
 #include <dlfcn.h>
 #include <errno.h>
