9ed80e0a7e342755378faeacee6ad5aef98daa54
Olli Pettay <Olli.Pettay@helsinki.fi>
2023-10-31 23:05:59 +0000

    Bug 1862311 - Clean up the comments in nsHtml5AtomTable.h and increase the cache size a tiny bit, r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D192392

--- parser/html/nsHtml5AtomTable.h
+++ parser/html/nsHtml5AtomTable.h
@@ -10,35 +10,11 @@
 #include "nsAtom.h"
 #include "nsISerialEventTarget.h"
 
-#define RECENTLY_USED_PARSER_ATOMS_SIZE 31
+#define RECENTLY_USED_PARSER_ATOMS_SIZE 37
 
-/**
- * nsHtml5AtomTable provides non-locking lookup and creation of atoms for
- * nsHtml5Parser or nsHtml5StreamParser.
- *
- * The hashtable holds dynamically allocated atoms that are private to an
- * instance of nsHtml5Parser or nsHtml5StreamParser. (Static atoms are used on
- * interned nsHtml5ElementNames and interned nsHtml5AttributeNames. Also, when
- * the doctype name is 'html', that identifier needs to be represented as a
- * static atom.)
- *
- * Each instance of nsHtml5Parser has a single instance of nsHtml5AtomTable,
- * and each instance of nsHtml5StreamParser has a single instance of
- * nsHtml5AtomTable. Dynamic atoms obtained from an nsHtml5AtomTable are valid
- * for == comparison with each other or with atoms declared in nsHtml5Atoms
- * within the nsHtml5Tokenizer and the nsHtml5TreeBuilder instances owned by
- * the same nsHtml5Parser/nsHtml5StreamParser instance that owns the
- * nsHtml5AtomTable instance.
- *
- * Dynamic atoms (atoms whose IsStatic() returns false) obtained from
- * nsHtml5AtomTable must be re-obtained from another atom table when there's a
- * need to migrate atoms from an nsHtml5Parser to its nsHtml5StreamParser
- * (re-obtain from the other nsHtml5AtomTable), from an nsHtml5Parser to its
- * owner nsHtml5Parser (re-obtain from the other nsHtml5AtomTable) or from the
- * parser to the DOM (re-obtain from the application-wide atom table). To
- * re-obtain an atom from another atom table, obtain a string from the atom
- * using ToString(nsAString&) and look up an atom in the other table using that
- * string.
+/*
+ * nsHtml5AtomTable provides an atom cache for nsHtml5Parser and
+ * nsHtml5StreamParser.
  *
  * An instance of nsHtml5AtomTable that belongs to an nsHtml5Parser is only
  * accessed from the main thread. An instance of nsHtml5AtomTable that belongs
@@ -52,8 +28,8 @@
  * thread working for the nsHtml5StreamParser when nsHtml5AtomTable belongs to
  * an nsHtml5StreamParser.
  *
- * Dynamic atoms obtained from nsHtml5AtomTable are deleted when the
- * nsHtml5AtomTable itself is destructed, which happens when the owner
+ * Atoms cached by nsHtml5AtomTable are released when Clear() is called or when
+ * the nsHtml5AtomTable itself is destructed, which happens when the owner
  * nsHtml5Parser or nsHtml5StreamParser is destructed.
  */
 class nsHtml5AtomTable {
