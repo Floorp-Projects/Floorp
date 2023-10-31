b6d4b835b90fc6d14169b429ff9e8a8613288271
Emilio Cobos Álvarez <emilio@crisal.io>
2023-10-31 10:07:14 +0000

    Bug 1852721 - Null-check parent before emitting signal. r=Jamie
    This avoids warning spam on GTK with a11y enabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D187971

--- accessible/atk/AccessibleWrap.cpp
+++ accessible/atk/AccessibleWrap.cpp
@@ -1171,6 +1171,14 @@ static const char* kMutationStrings[2][2] = {
 
 void MaiAtkObject::FireAtkShowHideEvent(AtkObject* aParent, bool aIsAdded,
                                         bool aFromUser) {
+  if (!aParent) {
+    // XXX ATK needs a parent for these events. However, we might have already
+    // unbound from the parent by the time we fire a hide event. Ideally, we
+    // need to find a way to keep the parent around, but ATK clients don't seem
+    // to care about these missing events.
+    MOZ_ASSERT(!aIsAdded);
+    return;
+  }
   int32_t indexInParent = getIndexInParentCB(&this->parent);
   const char* signal_name = kMutationStrings[aFromUser][aIsAdded];
   g_signal_emit_by_name(aParent, signal_name, indexInParent, this, nullptr);
