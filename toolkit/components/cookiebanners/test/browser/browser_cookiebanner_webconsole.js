faabce1b4b0e90e22ea287c248e09e9a2b53b18b
Paul Zuehlcke <pbz@mozilla.com>
2023-10-31 20:18:48 +0000

    Bug 1821731 - Log a message to the website console if Gecko handled a cookie banner on behalf of the user. r=anti-tracking-reviewers,fluent-reviewers,bvandersloot
    Differential Revision: https://phabricator.services.mozilla.com/D191531

---
+++ toolkit/components/cookiebanners/test/browser/browser_cookiebanner_webconsole.js
@@ -0,0 +1,95 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const { SiteDataTestUtils } = ChromeUtils.importESModule(
+  "resource://testing-common/SiteDataTestUtils.sys.mjs"
+);
+
+/**
+ * Registers a console listener and waits for the cookie banner handled message
+ * to be logged.
+ * @returns {Promise} - Promise which resolves once the message has been logged.
+ */
+async function waitForCookieBannerHandledConsoleMsg() {
+  let msg;
+  let checkFn = msg =>
+    msg && msg.match(/handled a cookie banner on behalf of the user./);
+  await new Promise(resolve => {
+    SpecialPowers.registerConsoleListener(consoleMsg => {
+      msg = consoleMsg.message;
+      if (checkFn(msg)) {
+        resolve();
+      }
+    });
+  });
+  SpecialPowers.postConsoleSentinel();
+
+  ok(checkFn(msg), "Observed cookie banner handled console message.");
+}
+
+add_setup(clickTestSetup);
+
+/**
+ * Tests that when we handle a banner via clicking a message is logged to the
+ * website console.
+ */
+add_task(async function test_banner_clicking_log_web_console() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [
+        "cookiebanners.service.mode",
+        Ci.nsICookieBannerService.MODE_REJECT_OR_ACCEPT,
+      ],
+    ],
+  });
+
+  insertTestClickRules();
+
+  let consoleMsgPromise = waitForCookieBannerHandledConsoleMsg();
+
+  info("Handle the banner via click and wait for console message to appear.");
+  await openPageAndVerify({
+    domain: TEST_DOMAIN_A,
+    testURL: TEST_PAGE_A,
+    visible: false,
+    expected: "OptOut",
+  });
+
+  await consoleMsgPromise;
+});
+
+/**
+ * Tests that when we handle a banner via cookie injection a message is logged
+ * to the website console.
+ */
+add_task(async function test_cookie_injection_log_web_console() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [
+        "cookiebanners.service.mode",
+        Ci.nsICookieBannerService.MODE_REJECT_OR_ACCEPT,
+      ],
+      ["cookiebanners.cookieInjector.enabled", true],
+    ],
+  });
+
+  insertTestCookieRules();
+
+  let consoleMsgPromise = waitForCookieBannerHandledConsoleMsg();
+
+  info(
+    "Handle the banner via cookie injection and wait for console message to appear."
+  );
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    TEST_ORIGIN_A
+  );
+  BrowserTestUtils.removeTab(tab);
+
+  await consoleMsgPromise;
+
+  // Clear injected cookies.
+  await SiteDataTestUtils.clear();
+});
