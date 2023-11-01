8dc7695179ed21694f309c854f8e1130e07145a1
James Teh <jteh@mozilla.com>
2023-11-01 00:40:00 +0000

    Bug 1775683: Request a longer timeout for accessible/tests/browser/e10s/browser_caching_name.js. r=eeejay
    These timeouts are most likely due to slowness from TSAN overhead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D192305

--- accessible/tests/browser/e10s/browser_caching_name.js
+++ accessible/tests/browser/e10s/browser_caching_name.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 "use strict";
+requestLongerTimeout(2);
 
 /* import-globals-from ../../mochitest/name.js */
 loadScripts({ name: "name.js", dir: MOCHITESTS_DIR });
