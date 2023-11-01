0e7a86916c30099eae4b3c5d877aa288298ab930
Norisz Fay <nfay@mozilla.com>
2023-11-01 09:47:26 +0200

    Backed out changeset 79de70379650 (bug 1832198) for causing failures on browser_quicksuggest_mdn.js CLOSED TREE

--- browser/components/urlbar/private/AdmWikipedia.sys.mjs
+++ browser/components/urlbar/private/AdmWikipedia.sys.mjs
@@ -57,7 +57,6 @@ export class AdmWikipedia extends BaseFeature {
       lazy.QuickSuggest.jsBackend.register(this);
     } else {
       lazy.QuickSuggest.jsBackend.unregister(this);
-      this.#suggestionsMap.clear();
     }
   }
 
