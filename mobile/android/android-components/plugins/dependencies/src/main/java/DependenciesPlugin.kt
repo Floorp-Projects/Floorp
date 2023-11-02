a98dca9e687193be14e35396e045e29a6a9eedfa
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2023-11-02 15:13:55 -0400

    Bug 1862744 - Update JaCoCo to version 0.8.11

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -23,7 +23,7 @@ object Versions {
     const val robolectric = "4.11.1"
     const val mockito = "5.7.0"
     const val maven_ant_tasks = "2.1.3"
-    const val jacoco = "0.8.10"
+    const val jacoco = "0.8.11"
     const val okhttp = "4.11.0"
 
     const val android_gradle_plugin = "7.4.2"
