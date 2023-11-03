9a8730723e3492bf1fb41af75ed7ed01e06fd3ab
alwu <alwu@mozilla.com>
2023-11-03 15:58:31 +0000

    Bug 1862526 - enable GetDebugInfo on non-debug build as well. r=media-playback-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D192516

--- dom/media/eme/KeySystemConfig.h
+++ dom/media/eme/KeySystemConfig.h
@@ -155,9 +155,7 @@ struct KeySystemConfig {
   KeySystemConfig(KeySystemConfig&&) = default;
   KeySystemConfig& operator=(KeySystemConfig&&) = default;
 
-#ifdef DEBUG
   nsString GetDebugInfo() const;
-#endif
 
   // Return true if the given key system is equal to `mKeySystem`, or it can be
   // mapped to the same key system
