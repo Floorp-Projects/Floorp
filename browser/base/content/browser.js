4470a84f8fc6fccdcc0d70c634f847bb1f98f82e
Greg Tatum <tatum.creative@gmail.com>
2023-11-04 14:08:53 +0000

    Bug 1815339 - Make the simd detection synchronous; r=nordzilla
    Differential Revision: https://phabricator.services.mozilla.com/D192701

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -5392,13 +5392,11 @@ var XULBrowserWindow = {
       this._menuItemForTranslations.removeAttribute("disabled");
     }
     if (gTranslationsEnabled) {
-      TranslationsParent.onIsTranslationsEngineSupported(isSupported => {
-        if (isSupported) {
-          this._menuItemForTranslations.removeAttribute("hidden");
-        } else {
-          this._menuItemForTranslations.setAttribute("hidden", "true");
-        }
-      });
+      if (TranslationsParent.getIsTranslationsEngineSupported()) {
+        this._menuItemForTranslations.removeAttribute("hidden");
+      } else {
+        this._menuItemForTranslations.setAttribute("hidden", "true");
+      }
     } else {
       this._menuItemForTranslations.setAttribute("hidden", "true");
     }
