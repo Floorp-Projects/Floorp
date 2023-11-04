c962e1526b9889388c920098d9ee05eef733bfdc
Mark Banner <standard8@mozilla.com>
2023-11-04 09:29:27 +0000

    Bug 1858148 - Change ESLint rule reject-importGlobalProperties to reject all imports in sjs files. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D190760

--- tools/lint/eslint/eslint-plugin-mozilla/lib/configs/recommended.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/configs/recommended.js
@@ -105,10 +105,9 @@ module.exports = {
       },
       files: ["**/*.sjs"],
       rules: {
-        // TODO Bug 1501127: sjs files have their own sandbox, and do not inherit
-        // the Window backstage pass directly. Turn this rule off for sjs files for
-        // now until we develop a solution.
-        "mozilla/reject-importGlobalProperties": "off",
+        // For sjs files, reject everything as we should update the sandbox
+        // to include the globals we need, as these are test-only files.
+        "mozilla/reject-importGlobalProperties": ["error", "everything"],
       },
     },
     {
