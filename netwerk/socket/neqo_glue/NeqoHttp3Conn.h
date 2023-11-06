01936465c5bdfe17a6d7a2fd312e46e3eb9d0d1b
Randell Jesup <rjesup@mozilla.com>
2023-11-06 03:51:56 +0000

    Bug 1853444: Set/Get SendOrder for WebTransportSendStreams r=kershaw,necko-reviewers,webidl,smaug
    Allow dynamic changes to steram sendOrder to comply with updated
    spec.
    
    Differential Revision: https://phabricator.services.mozilla.com/D188377

--- netwerk/socket/neqo_glue/NeqoHttp3Conn.h
+++ netwerk/socket/neqo_glue/NeqoHttp3Conn.h
@@ -149,9 +149,10 @@ class NeqoHttp3Conn final {
                                                          aResult);
   }
 
-  nsresult WebTransportSetSendOrder(uint64_t aSessionId, int64_t aSendOrder) {
+  nsresult WebTransportSetSendOrder(uint64_t aSessionId,
+                                    Maybe<int64_t> aSendOrder) {
     return neqo_http3conn_webtransport_set_sendorder(this, aSessionId,
-                                                     aSendOrder);
+                                                     aSendOrder.ptrOr(nullptr));
   }
 
  private:
