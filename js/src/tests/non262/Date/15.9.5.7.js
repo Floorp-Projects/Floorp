e6591d94b29d2e83717039f3b1095c4ee99762ce
Vinny Diehl <vinny.diehl@gmail.com>
2023-11-06 06:24:14 +0000

    Bug 1863197 - Fix intermittent failure caused by DST r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D192804

--- js/src/tests/non262/Date/15.9.5.7.js
+++ js/src/tests/non262/Date/15.9.5.7.js
@@ -1,3 +1,4 @@
+// |reftest| skip-if(xulRuntime.OS=="WINNT") -- Windows doesn't accept IANA names for the TZ env variable
 /* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -15,7 +16,7 @@
 
    new Date(d.toDateString() + " " + d.toLocaleTimeString()) ==  d
 
-   Author:  pschwartau@netscape.com                            
+   Author:  pschwartau@netscape.com
    Date:    14 november 2000
    Revised: 07 january 2002  because of a change in JS Date format:
    Revised: 21 November 2005 since the string comparison stuff is horked.
@@ -24,12 +25,12 @@
    See http://bugzilla.mozilla.org/show_bug.cgi?id=118266 (SpiderMonkey)
    See http://bugzilla.mozilla.org/show_bug.cgi?id=118636 (Rhino)
 */
-//-----------------------------------------------------------------------------
+
 var SECTION = "15.9.5.7";
 var TITLE   = "Date.prototype.toLocaleTimeString()";
-  
+
 var status = '';
-var actual = ''; 
+var actual = '';
 var expect = '';
 var givenDate;
 var year = '';
@@ -37,75 +38,73 @@ var regexp = '';
 var TimeString = '';
 var reducedDateString = '';
 var hopeThisIsLocaleTimeString = '';
-var cnERR ='OOPS! FATAL ERROR: no regexp match in extractLocaleTimeString()';
+var cnERR = 'OOPS! FATAL ERROR: no regexp match in extractLocaleTimeString()';
 
-writeHeaderToLog( SECTION + " "+ TITLE);
+writeHeaderToLog(SECTION + " " + TITLE);
 
-var now = new Date();
+inTimeZone("PST", () => {
+  var now = new Date();
 
-// valueOf() is to accurate to the millisecond,
-// Date.parse() is accurate only to the second
-var TIME_NOW = now.valueOf();
+  // valueOf() is to accurate to the millisecond,
+  // Date.parse() is accurate only to the second
+  var TIME_NOW = now.valueOf();
 
-// first, a couple generic tests -
+  // first, a couple generic tests -
 
-status = "typeof (now.toLocaleTimeString())"; 
-actual =   typeof (now.toLocaleTimeString());
-expect = "string";
-addTestCase();
-
-status = "Date.prototype.toLocaleTimeString.length";  
-actual =  Date.prototype.toLocaleTimeString.length;
-expect =  0;  
-addTestCase();
-
-// 1970
-addDateTestCase(0);
-addDateTestCase(TZ_ADJUST);
-  
-// 1900
-addDateTestCase(UTC_01_JAN_1900);
-addDateTestCase(UTC_01_JAN_1900 - TZ_ADJUST);
-
-// 2000
-addDateTestCase(UTC_01_JAN_2000);
-addDateTestCase(UTC_01_JAN_2000 - TZ_ADJUST);
-   
-// 29 Feb 2000
-addDateTestCase(UTC_29_FEB_2000);
-addDateTestCase(UTC_29_FEB_2000 - 1000);
-addDateTestCase(UTC_29_FEB_2000 - TZ_ADJUST);
-
-// Now
-addDateTestCase( TIME_NOW);
-addDateTestCase( TIME_NOW - TZ_ADJUST);
-
-// 2005
-addDateTestCase(UTC_01_JAN_2005);
-addDateTestCase(UTC_01_JAN_2005 - 1000);
-addDateTestCase(UTC_01_JAN_2005 - TZ_ADJUST);
-
-test();
-
-function addTestCase()
-{
-  new TestCase(
-    status,
-    expect,
-    actual);
-}
-
-
-function addDateTestCase(date_given_in_milliseconds)
-{
-  var s = 'new Date(' +  date_given_in_milliseconds + ')';
-  givenDate = new Date(date_given_in_milliseconds);
-  
-  status = 'd = ' + s +
-    '; d == new Date(d.toDateString() + " " + d.toLocaleTimeString())';  
-  expect = givenDate.toString();
-  actual = new Date(givenDate.toDateString() +
-                    ' ' + givenDate.toLocaleTimeString()).toString();
+  status = "typeof (now.toLocaleTimeString())";
+  actual =   typeof (now.toLocaleTimeString());
+  expect = "string";
   addTestCase();
-}
 
+  status = "Date.prototype.toLocaleTimeString.length";
+  actual =  Date.prototype.toLocaleTimeString.length;
+  expect =  0;
+  addTestCase();
+
+  // 1970
+  addDateTestCase(0);
+  addDateTestCase(TZ_ADJUST);
+
+  // 1900
+  addDateTestCase(UTC_01_JAN_1900);
+  addDateTestCase(UTC_01_JAN_1900 - TZ_ADJUST);
+
+  // 2000
+  addDateTestCase(UTC_01_JAN_2000);
+  addDateTestCase(UTC_01_JAN_2000 - TZ_ADJUST);
+
+  // 29 Feb 2000
+  addDateTestCase(UTC_29_FEB_2000);
+  addDateTestCase(UTC_29_FEB_2000 - 1000);
+  addDateTestCase(UTC_29_FEB_2000 - TZ_ADJUST);
+
+  // Now
+  addDateTestCase(TIME_NOW);
+  addDateTestCase(TIME_NOW - TZ_ADJUST);
+
+  // 2005
+  addDateTestCase(UTC_01_JAN_2005);
+  addDateTestCase(UTC_01_JAN_2005 - 1000);
+  addDateTestCase(UTC_01_JAN_2005 - TZ_ADJUST);
+
+  test();
+
+  function addTestCase() {
+    new TestCase(
+      status,
+      expect,
+      actual);
+  }
+
+  function addDateTestCase(date_given_in_milliseconds) {
+    var s = 'new Date(' +  date_given_in_milliseconds + ')';
+    givenDate = new Date(date_given_in_milliseconds);
+
+    status = 'd = ' + s +
+      '; d == new Date(d.toDateString() + " " + d.toLocaleTimeString())';
+    expect = givenDate.toString();
+    actual = new Date(givenDate.toDateString() +
+                      ' ' + givenDate.toLocaleTimeString()).toString();
+    addTestCase();
+  }
+});
