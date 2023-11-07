a7b06eb2a6e79c57e3c9656e38ea879368c0a258
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-11-07 05:54:52 +0000

    Bug 1862963 - [devtools] Use strictRequires option for commonjs rollup plugin. r=devtools-reviewers,ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D192770

--- devtools/client/debugger/dist/search-worker.js
+++ devtools/client/debugger/dist/search-worker.js
@@ -233,162 +233,169 @@
 
     var workerUtils = {exports: {}};
 
-    (function (module) {
+    var hasRequiredWorkerUtils;
 
-    	class WorkerDispatcher {
-    	  #msgId = 1;
-    	  #worker = null;
-    	  // Map of message ids -> promise resolution functions, for dispatching worker responses
-    	  #pendingCalls = new Map();
-    	  #url = "";
+    function requireWorkerUtils () {
+    	if (hasRequiredWorkerUtils) return workerUtils.exports;
+    	hasRequiredWorkerUtils = 1;
+    	(function (module) {
 
-    	  constructor(url) {
-    	    this.#url = url;
-    	  }
+    		class WorkerDispatcher {
+    		  #msgId = 1;
+    		  #worker = null;
+    		  // Map of message ids -> promise resolution functions, for dispatching worker responses
+    		  #pendingCalls = new Map();
+    		  #url = "";
 
-    	  start() {
-    	    // When running in debugger jest test, we don't have access to ChromeWorker
-    	    if (typeof ChromeWorker == "function") {
-    	      this.#worker = new ChromeWorker(this.#url);
-    	    } else {
-    	      this.#worker = new Worker(this.#url);
-    	    }
-    	    this.#worker.onerror = err => {
-    	      console.error(`Error in worker ${this.#url}`, err.message);
-    	    };
-    	    this.#worker.addEventListener("message", this.#onMessage);
-    	  }
+    		  constructor(url) {
+    		    this.#url = url;
+    		  }
 
-    	  stop() {
-    	    if (!this.#worker) {
-    	      return;
-    	    }
+    		  start() {
+    		    // When running in debugger jest test, we don't have access to ChromeWorker
+    		    if (typeof ChromeWorker == "function") {
+    		      this.#worker = new ChromeWorker(this.#url);
+    		    } else {
+    		      this.#worker = new Worker(this.#url);
+    		    }
+    		    this.#worker.onerror = err => {
+    		      console.error(`Error in worker ${this.#url}`, err.message);
+    		    };
+    		    this.#worker.addEventListener("message", this.#onMessage);
+    		  }
 
-    	    this.#worker.removeEventListener("message", this.#onMessage);
-    	    this.#worker.terminate();
-    	    this.#worker = null;
-    	    this.#pendingCalls.clear();
-    	  }
+    		  stop() {
+    		    if (!this.#worker) {
+    		      return;
+    		    }
 
-    	  task(method, { queue = false } = {}) {
-    	    const calls = [];
-    	    const push = args => {
-    	      return new Promise((resolve, reject) => {
-    	        if (queue && calls.length === 0) {
-    	          Promise.resolve().then(flush);
-    	        }
+    		    this.#worker.removeEventListener("message", this.#onMessage);
+    		    this.#worker.terminate();
+    		    this.#worker = null;
+    		    this.#pendingCalls.clear();
+    		  }
 
-    	        calls.push({ args, resolve, reject });
+    		  task(method, { queue = false } = {}) {
+    		    const calls = [];
+    		    const push = args => {
+    		      return new Promise((resolve, reject) => {
+    		        if (queue && calls.length === 0) {
+    		          Promise.resolve().then(flush);
+    		        }
 
-    	        if (!queue) {
-    	          flush();
-    	        }
-    	      });
-    	    };
+    		        calls.push({ args, resolve, reject });
 
-    	    const flush = () => {
-    	      const items = calls.slice();
-    	      calls.length = 0;
+    		        if (!queue) {
+    		          flush();
+    		        }
+    		      });
+    		    };
 
-    	      if (!this.#worker) {
-    	        this.start();
-    	      }
+    		    const flush = () => {
+    		      const items = calls.slice();
+    		      calls.length = 0;
 
-    	      const id = this.#msgId++;
-    	      this.#worker.postMessage({
-    	        id,
-    	        method,
-    	        calls: items.map(item => item.args),
-    	      });
+    		      if (!this.#worker) {
+    		        this.start();
+    		      }
 
-    	      this.#pendingCalls.set(id, items);
-    	    };
+    		      const id = this.#msgId++;
+    		      this.#worker.postMessage({
+    		        id,
+    		        method,
+    		        calls: items.map(item => item.args),
+    		      });
 
-    	    return (...args) => push(args);
-    	  }
+    		      this.#pendingCalls.set(id, items);
+    		    };
 
-    	  invoke(method, ...args) {
-    	    return this.task(method)(...args);
-    	  }
+    		    return (...args) => push(args);
+    		  }
 
-    	  #onMessage = ({ data: result }) => {
-    	    const items = this.#pendingCalls.get(result.id);
-    	    this.#pendingCalls.delete(result.id);
-    	    if (!items) {
-    	      return;
-    	    }
+    		  invoke(method, ...args) {
+    		    return this.task(method)(...args);
+    		  }
 
-    	    if (!this.#worker) {
-    	      return;
-    	    }
+    		  #onMessage = ({ data: result }) => {
+    		    const items = this.#pendingCalls.get(result.id);
+    		    this.#pendingCalls.delete(result.id);
+    		    if (!items) {
+    		      return;
+    		    }
 
-    	    result.results.forEach((resultData, i) => {
-    	      const { resolve, reject } = items[i];
+    		    if (!this.#worker) {
+    		      return;
+    		    }
 
-    	      if (resultData.error) {
-    	        const err = new Error(resultData.message);
-    	        err.metadata = resultData.metadata;
-    	        reject(err);
-    	      } else {
-    	        resolve(resultData.response);
-    	      }
-    	    });
-    	  };
-    	}
+    		    result.results.forEach((resultData, i) => {
+    		      const { resolve, reject } = items[i];
 
-    	function workerHandler(publicInterface) {
-    	  return function (msg) {
-    	    const { id, method, calls } = msg.data;
+    		      if (resultData.error) {
+    		        const err = new Error(resultData.message);
+    		        err.metadata = resultData.metadata;
+    		        reject(err);
+    		      } else {
+    		        resolve(resultData.response);
+    		      }
+    		    });
+    		  };
+    		}
 
-    	    Promise.all(
-    	      calls.map(args => {
-    	        try {
-    	          const response = publicInterface[method].apply(undefined, args);
-    	          if (response instanceof Promise) {
-    	            return response.then(
-    	              val => ({ response: val }),
-    	              err => asErrorMessage(err)
-    	            );
-    	          }
-    	          return { response };
-    	        } catch (error) {
-    	          return asErrorMessage(error);
-    	        }
-    	      })
-    	    ).then(results => {
-    	      globalThis.postMessage({ id, results });
-    	    });
-    	  };
-    	}
+    		function workerHandler(publicInterface) {
+    		  return function (msg) {
+    		    const { id, method, calls } = msg.data;
 
-    	function asErrorMessage(error) {
-    	  if (typeof error === "object" && error && "message" in error) {
-    	    // Error can't be sent via postMessage, so be sure to convert to
-    	    // string.
-    	    return {
-    	      error: true,
-    	      message: error.message,
-    	      metadata: error.metadata,
-    	    };
-    	  }
+    		    Promise.all(
+    		      calls.map(args => {
+    		        try {
+    		          const response = publicInterface[method].apply(undefined, args);
+    		          if (response instanceof Promise) {
+    		            return response.then(
+    		              val => ({ response: val }),
+    		              err => asErrorMessage(err)
+    		            );
+    		          }
+    		          return { response };
+    		        } catch (error) {
+    		          return asErrorMessage(error);
+    		        }
+    		      })
+    		    ).then(results => {
+    		      globalThis.postMessage({ id, results });
+    		    });
+    		  };
+    		}
 
-    	  return {
-    	    error: true,
-    	    message: error == null ? error : error.toString(),
-    	    metadata: undefined,
-    	  };
-    	}
+    		function asErrorMessage(error) {
+    		  if (typeof error === "object" && error && "message" in error) {
+    		    // Error can't be sent via postMessage, so be sure to convert to
+    		    // string.
+    		    return {
+    		      error: true,
+    		      message: error.message,
+    		      metadata: error.metadata,
+    		    };
+    		  }
 
-    	// Might be loaded within a worker thread where `module` isn't available.
-    	{
-    	  module.exports = {
-    	    WorkerDispatcher,
-    	    workerHandler,
-    	  };
-    	} 
-    } (workerUtils));
+    		  return {
+    		    error: true,
+    		    message: error == null ? error : error.toString(),
+    		    metadata: undefined,
+    		  };
+    		}
 
-    var workerUtilsExports = workerUtils.exports;
+    		// Might be loaded within a worker thread where `module` isn't available.
+    		{
+    		  module.exports = {
+    		    WorkerDispatcher,
+    		    workerHandler,
+    		  };
+    		} 
+    	} (workerUtils));
+    	return workerUtils.exports;
+    }
+
+    var workerUtilsExports = requireWorkerUtils();
 
     self.onmessage = workerUtilsExports.workerHandler({ getMatches, findSourceMatches });
 
