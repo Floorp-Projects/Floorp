cc6aeb13caab2b2b0db517bd79e2169f274cf24b
Erik Nordin <enordin@mozilla.com>
2023-11-07 01:34:01 +0000

    Bug 1861516 - Remove Translations useFastText pref r=gregtatum
    Removes the browser.translations.languageIdentification.useFastText
    pref, making CLD2 the only option for language identification
    for Translations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D192656

--- toolkit/components/translations/tests/browser/browser_about_translations_translations.js
+++ toolkit/components/translations/tests/browser/browser_about_translations_translations.js
@@ -179,7 +179,6 @@ add_task(async function test_about_translations_language_identification() {
       { fromLang: "en", toLang: "fr" },
       { fromLang: "fr", toLang: "en" },
     ],
-    prefs: [["browser.translations.languageIdentification.useFastText", true]],
     runInPage: async ({ selectors }) => {
       const { document, window } = content;
       Cu.waiveXrays(window).DEBOUNCE_DELAY = 5; // Make the timer run faster for tests.
