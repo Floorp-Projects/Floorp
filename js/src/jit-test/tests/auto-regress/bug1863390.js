09253f7d74b5341bb9b08fe4dfd6ad42a92dd840
Stanca Serban <sstanca@mozilla.com>
2023-11-08 13:06:44 +0200

    Backed out changeset 2b10a37f9a23 (bug 1863390) for causing JSRope related SM bustages. CLOSED TREE

--- js/src/jit-test/tests/auto-regress/bug1863390.js
+++
@@ -1,6 +0,0 @@
-var left = "left-rope-child";
-var right = newString("right-rope-child", {twoByte: true});
-var rope = newRope(left, right);
-var result = rope.substring(0, left.length + 1);
-
-assertEq(result, "left-rope-childr");
