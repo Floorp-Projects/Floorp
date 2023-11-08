3e6ae6be5767754e917382e061f0a261dd851c20
Erik Nordin <enordin@mozilla.com>
2023-11-08 23:38:37 +0000

    Bug 1861516 - Remove Translations fastText LanguageIdEngine r=gregtatum
    Removes the source code for the Translations LanguageIdEngine
    which utilized fastText as its underlying mechanism.
    
    Depends on D192656
    
    Differential Revision: https://phabricator.services.mozilla.com/D192657

--- browser/base/content/test/performance/browser_startup_content.js
+++ browser/base/content/test/performance/browser_startup_content.js
@@ -72,7 +72,6 @@ const intermittently_loaded_scripts = {
     // Translations code which may be preffed on.
     "resource://gre/actors/TranslationsChild.sys.mjs",
     "resource://gre/modules/translation/LanguageDetector.sys.mjs",
-    "chrome://global/content/translations/language-id-engine.sys.mjs",
     "resource://gre/modules/ConsoleAPIStorage.sys.mjs", // Logging related.
 
     // Session store.
