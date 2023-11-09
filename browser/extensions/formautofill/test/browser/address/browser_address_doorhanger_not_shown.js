f1dbb99fdeb0bdd2df7fb262aad4d537abd75e8d
Dimi <dlee@mozilla.com>
2023-11-09 09:02:32 +0000

    Bug 1860619 - Match label or value when setting the default value of country and address-level1 menulist r=joschmidt
    Differential Revision: https://phabricator.services.mozilla.com/D192246

--- browser/extensions/formautofill/test/browser/address/browser_address_doorhanger_not_shown.js
+++ browser/extensions/formautofill/test/browser/address/browser_address_doorhanger_not_shown.js
@@ -44,12 +44,23 @@ const TESTCASES = [
       "#tel": "1-650-903-0800",
     },
   },
+  {
+    description: "Should not trigger the address save doorhanger when pref off",
+    document: DEFAULT_TEST_DOC,
+    targetElementId: TARGET_ELEMENT_ID,
+    formValue: {
+      "#street-addr": "331 E. Evelyn Avenue",
+      "#email": "test@mozilla.org",
+      "#tel": "1-650-903-0800",
+    },
+    prefs: [["extensions.formautofill.addresses.capture.v2.enabled", false]],
+  },
 ];
 
 add_setup(async function () {
   await SpecialPowers.pushPrefEnv({
     set: [
-      ["extensions.formautofill.addresses.capture.enabled", true],
+      ["extensions.formautofill.addresses.capture.v2.enabled", true],
       ["extensions.formautofill.addresses.supported", "on"],
     ],
   });
@@ -58,6 +69,11 @@ add_setup(async function () {
 add_task(async function test_save_doorhanger_not_shown() {
   for (const TEST of TESTCASES) {
     info(`Test ${TEST.description}`);
+    if (TEST.prefs) {
+      await SpecialPowers.pushPrefEnv({
+        set: TEST.prefs,
+      });
+    }
 
     await BrowserTestUtils.withNewTab(EMPTY_URL, async function (browser) {
       await SpecialPowers.spawn(browser, [TEST.document], doc => {
@@ -73,5 +89,9 @@ add_task(async function test_save_doorhanger_not_shown() {
 
       await ensureNoDoorhanger(browser);
     });
+
+    if (TEST.prefs) {
+      await SpecialPowers.popPrefEnv();
+    }
   }
 });
