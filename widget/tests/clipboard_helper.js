21b5a66c6c0d020b1dc98c1c6ca52873e628293c
Noemi Erli <nerli@mozilla.com>
2023-11-09 12:32:56 +0200

    Backed out changeset 7355802744d1 (bug 1852947) for causing failures in test_clipboard_asyncGetData.html CLOSED TREE

--- widget/tests/clipboard_helper.js
+++ widget/tests/clipboard_helper.js
@@ -14,25 +14,13 @@ const clipboardTypes = [
   clipboard.kSelectionCache,
 ];
 
-function emptyClipboardData(aType) {
-  // XXX gtk doesn't support emptying clipboard data which is stored from
-  // other application (bug 1853884). As a workaround, we set dummy data
-  // to the clipboard first to ensure the subsequent emptyClipboard call
-  // works.
-  if (navigator.platform.includes("Linux")) {
-    writeStringToClipboard("foo", "text/plain", aType);
-  }
-
-  clipboard.emptyClipboard(aType);
-}
-
 function cleanupAllClipboard() {
-  for (const type of clipboardTypes) {
+  clipboardTypes.forEach(function (type) {
     if (clipboard.isClipboardTypeSupported(type)) {
       info(`cleanup clipboard ${type}`);
-      emptyClipboardData(type);
+      clipboard.emptyClipboard(type);
     }
-  }
+  });
 }
 
 function generateRandomString() {
@@ -58,14 +46,6 @@ function generateNewTransferable(aFlavor, aStr) {
 function addStringToTransferable(aFlavor, aStr, aTrans) {
   aTrans.addDataFlavor(aFlavor);
 
-  let supportsStr = Cc["@mozilla.org/supports-string;1"].createInstance(
-    Ci.nsISupportsString
-  );
-  supportsStr.data = aStr;
-  aTrans.setTransferData(aFlavor, supportsStr);
-}
-
-function updateStringToTransferable(aFlavor, aStr, aTrans) {
   let supportsStr = Cc["@mozilla.org/supports-string;1"].createInstance(
     Ci.nsISupportsString
   );
@@ -134,74 +114,4 @@ function getClipboardData(aFlavor, aClipboardType) {
     // If the clipboard is empty getTransferData will throw.
     return null;
   }
-}
-
-function asyncGetClipboardData(aClipboardType) {
-  return new Promise((resolve, reject) => {
-    try {
-      clipboard.asyncGetData(
-        ["text/plain", "text/html", "image/png"],
-        aClipboardType,
-        {
-          QueryInterface: SpecialPowers.ChromeUtils.generateQI([
-            "nsIAsyncClipboardGetCallback",
-          ]),
-          // nsIAsyncClipboardGetCallback
-          onSuccess: SpecialPowers.wrapCallback(function (
-            aAsyncGetClipboardData
-          ) {
-            resolve(aAsyncGetClipboardData);
-          }),
-          onError: SpecialPowers.wrapCallback(function (aResult) {
-            reject(aResult);
-          }),
-        }
-      );
-    } catch (e) {
-      ok(false, `asyncGetData should not throw`);
-      reject(e);
-    }
-  });
-}
-
-function asyncClipboardRequestGetData(aRequest, aFlavor, aThrows = false) {
-  return new Promise((resolve, reject) => {
-    var trans = Cc["@mozilla.org/widget/transferable;1"].createInstance(
-      Ci.nsITransferable
-    );
-    trans.init(null);
-    trans.addDataFlavor(aFlavor);
-    try {
-      aRequest.getData(trans, aResult => {
-        if (aResult != Cr.NS_OK) {
-          reject(aResult);
-          return;
-        }
-
-        try {
-          var data = SpecialPowers.createBlankObject();
-          trans.getTransferData(aFlavor, data);
-          resolve(data.value.QueryInterface(Ci.nsISupportsString).data);
-        } catch (ex) {
-          // XXX: should widget set empty string to transferable when there no
-          // data in system clipboard?
-          resolve("");
-        }
-      });
-      ok(
-        !aThrows,
-        `nsIAsyncGetClipboardData.getData should ${
-          aThrows ? "throw" : "success"
-        }`
-      );
-    } catch (e) {
-      ok(
-        aThrows,
-        `nsIAsyncGetClipboardData.getData should ${
-          aThrows ? "throw" : "success"
-        }`
-      );
-      reject(e);
-    }
-  });
 }
