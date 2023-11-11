c4d04c31b23d21a60576ee34652a607125a36118
Vinny Diehl <vinny.diehl@gmail.com>
2023-11-11 12:32:12 +0000

    Bug 746529 - Truncate milliseconds after 3 digits r=arai
    Previously, we would round up on the 17th digit, this change was made
    for Chrome parity.
    
    Differential Revision: https://phabricator.services.mozilla.com/D193345

--- js/src/tests/non262/Date/parse-milliseconds.js
+++ js/src/tests/non262/Date/parse-milliseconds.js
@@ -8,7 +8,6 @@ const accepted = {
   "Sep 26 1995 12:34:56.7": "1995-09-26T12:34:56.700",
   "Sep 26 1995 12:34:56.78": "1995-09-26T12:34:56.780",
   "Sep 26 1995 12:34:56.001": "1995-09-26T12:34:56.001",
-  "Sep 26 1995 12:34:56.0001": "1995-09-26T12:34:56",
 
   "Sep 26 1995 12:34:56.789Z": "1995-09-26T12:34:56.789Z",
   "Sep 26 1995 12:34:56.789 -0500": "1995-09-26T12:34:56.789-0500",
@@ -18,12 +17,28 @@ const accepted = {
   // Rejecting this case could be done trivially, but Chrome allows it,
   // so we will allow it for parity.
   "Sep 26 1995 12:34:56.789.": "1995-09-26T12:34:56.789",
+
+  // Truncate after 3 digits
+  "Sep 26 1995 12:34:56.0001": "1995-09-26T12:34:56",
+  "Sep 26 1995 12:34:56.0009": "1995-09-26T12:34:56",
+  "Sep 26 1995 12:34:56.999": "1995-09-26T12:34:56.999",
+  "Sep 26 1995 12:34:56.9990": "1995-09-26T12:34:56.999",
+  "Sep 26 1995 12:34:56.9999": "1995-09-26T12:34:56.999",
+  // Before bug 746529, it would have rounded up here:
+  "Sep 26 1995 12:34:56.99999999999999996": "1995-09-26T12:34:56.999",
+  "Sep 26 1995 12:34:56.50099999999999996": "1995-09-26T12:34:56.500",
+  "Sep 26 1995 12:34:56.00099999999999996": "1995-09-26T12:34:56",
+  "Sep 26 1995 12:34:56.000999999999999999999": "1995-09-26T12:34:56",
 };
 const rejected = [
   "Sep 26 1995 12:34:56.",
   "Sep 26 1995 12:34:56:789",
   "Sep 26 1995 12:34:56..789",
 ];
+
+// Sanity check to make sure these are being parsed to the right precision
+// Otherwise, the comparisons in the above object could still pass
+assertEq(Date.parse("1970-01-01T00:00:00.99999999999999996Z"), 999);
 
 for (const [test, expected] of Object.entries(accepted)) {
   const testDate = new Date(test);
