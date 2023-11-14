072217b570bea7f962e69214eea06ab72855acba
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2023-11-14 10:30:16 +0000

    Bug 1854434 - cache privacy level pref, r=sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D193205

--- toolkit/modules/sessionstore/PrivacyLevel.sys.mjs
+++ toolkit/modules/sessionstore/PrivacyLevel.sys.mjs
@@ -2,23 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const PREF = "browser.sessionstore.privacy_level";
+import { XPCOMUtils } from "resource://gre/modules/XPCOMUtils.sys.mjs";
 
 // The following constants represent the different possible privacy levels that
 // can be set by the user and that we need to consider when collecting text
 // data, and cookies.
 //
 // Collect data from all sites (http and https).
-// const PRIVACY_NONE = 0;
+const PRIVACY_NONE = 0;
 // Collect data from unencrypted sites (http), only.
 const PRIVACY_ENCRYPTED = 1;
 // Collect no data.
 const PRIVACY_FULL = 2;
 
-/**
- * The external API as exposed by this module.
- */
-export var PrivacyLevel = Object.freeze({
+export var PrivacyLevel = {
   /**
    * Returns whether the current privacy level allows saving data for the given
    * |url|.
@@ -37,18 +34,30 @@ export var PrivacyLevel = Object.freeze({
    * @return {bool} Whether we can save data for the specified site.
    */
   canSave(isHttps) {
-    let level = Services.prefs.getIntPref(PREF);
-
     // Never save any data when full privacy is requested.
-    if (level == PRIVACY_FULL) {
+    if (this.privacyLevel == PRIVACY_FULL) {
       return false;
     }
 
     // Don't save data for encrypted sites when requested.
-    if (isHttps && level == PRIVACY_ENCRYPTED) {
+    if (isHttps && this.privacyLevel == PRIVACY_ENCRYPTED) {
       return false;
     }
 
     return true;
   },
-});
+
+  canSaveAnything() {
+    return this.privacyLevel != PRIVACY_FULL;
+  },
+
+  shouldSaveEverything() {
+    return this.privacyLevel == PRIVACY_NONE;
+  },
+};
+
+XPCOMUtils.defineLazyPreferenceGetter(
+  PrivacyLevel,
+  "privacyLevel",
+  "browser.sessionstore.privacy_level"
+);
