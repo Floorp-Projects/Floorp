e3fcda6db2ab233eb9502620eb5536621e75e570
Robert Longson <longsonr@gmail.com>
2023-11-15 19:27:43 +0000

    Bug 1864039 - clip-path path reference box is incorrect with stroke r=boris
    Differential Revision: https://phabricator.services.mozilla.com/D193643

--- layout/svg/CSSClipPathInstance.h
+++ layout/svg/CSSClipPathInstance.h
@@ -55,7 +55,8 @@ class MOZ_STACK_CLASS CSSClipPathInstance {
   already_AddRefed<Path> CreateClipPathInset(DrawTarget* aDrawTarget,
                                              const nsRect& aRefBox);
 
-  already_AddRefed<Path> CreateClipPathPath(DrawTarget* aDrawTarget);
+  already_AddRefed<Path> CreateClipPathPath(DrawTarget* aDrawTarget,
+                                            const nsRect& aRefBox);
 
   /**
    * The frame for the element that is currently being clipped.
