ecea4271fabc114eae3c034962c3701fd884aa96
Paul Zuehlcke <pbz@mozilla.com>
2023-11-15 09:49:25 +0000

    Bug 1861377 - Enable global cookie banner rules by default. r=timhuang
    When the feature is enabled this also enables global rules used to handle
    CMPs by default.
    
    Differential Revision: https://phabricator.services.mozilla.com/D191948

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2853,7 +2853,6 @@ pref("browser.pdfjs.feature-tour", "{\"screen\":\"\",\"complete\":false}");
 // StaticPrefList.yaml for a description of the prefs.
 #ifdef NIGHTLY_BUILD
   pref("cookiebanners.service.mode.privateBrowsing", 1);
-  pref("cookiebanners.service.enableGlobalRules", true);
 #endif
 
 #if defined(EARLY_BETA_OR_EARLIER)
