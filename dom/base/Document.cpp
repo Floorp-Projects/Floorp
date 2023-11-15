7f8a3ccc5ea8efe09c87b36b27563e7c7239e643
Stanca Serban <sstanca@mozilla.com>
2023-11-15 23:02:32 +0200

    Backed out 7 changesets (bug 1852098) for causing build bustages in UseCounterMetrics.cpp. CLOSED TREE
    Backed out changeset 2caabd4bfe86 (bug 1852098)
    Backed out changeset 5c174b0ee1ab (bug 1852098)
    Backed out changeset 05ba23510b93 (bug 1852098)
    Backed out changeset 256984effaa2 (bug 1852098)
    Backed out changeset 8bed08cf2a68 (bug 1852098)
    Backed out changeset 16b0698a239d (bug 1852098)
    Backed out changeset 6e8d54a51fb7 (bug 1852098)

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -232,7 +232,6 @@
 #include "mozilla/dom/TreeOrderedArrayInlines.h"
 #include "mozilla/dom/TreeWalker.h"
 #include "mozilla/dom/URL.h"
-#include "mozilla/dom/UseCounterMetrics.h"
 #include "mozilla/dom/UserActivation.h"
 #include "mozilla/dom/WindowBinding.h"
 #include "mozilla/dom/WindowContext.h"
@@ -16152,7 +16151,6 @@ void Document::ReportDocumentUseCounters() {
   // TOP_LEVEL_CONTENT_DOCUMENTS_DESTROYED is recorded in
   // WindowGlobalParent::FinishAccumulatingPageUseCounters.
   Telemetry::Accumulate(Telemetry::CONTENT_DOCUMENTS_DESTROYED, 1);
-  glean::use_counter::content_documents_destroyed.Add();
 
   // Ask all of our resource documents to report their own document use
   // counters.
@@ -16185,7 +16183,6 @@ void Document::ReportDocumentUseCounters() {
                     Telemetry::GetHistogramName(id), urlForLogging->get());
     }
     Telemetry::Accumulate(id, 1);
-    IncrementUseCounter(uc, /* aIsPage = */ false);
   }
 }
 
