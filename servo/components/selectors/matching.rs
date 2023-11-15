41c9bb99f7f686c8ce4f6a26d26bf94dced089a2
Emilio Cobos Álvarez <emilio@crisal.io>
2023-11-15 17:21:24 +0000

    Bug 1864647 - Don't crash when finding nested relative selectors. r=dshin
    For now, do the same as pseudo-elements, which is effectively to keep
    them in the replaced selector but don't match them. Avoid crashing in
    this case.
    
    I filed https://github.com/w3c/csswg-drafts/issues/9600 since this issue
    isn't specific about :has(), and depending on the result of that
    discussion, the "proper" solution might be different.
    
    Differential Revision: https://phabricator.services.mozilla.com/D193663

--- servo/components/selectors/matching.rs
+++ servo/components/selectors/matching.rs
@@ -525,8 +525,27 @@ fn relative_selector_match_early<E: Element>(
     None
 }
 
+fn match_relative_selectors<E: Element>(
+    selectors: &[RelativeSelector<E::Impl>],
+    element: &E,
+    context: &mut MatchingContext<E::Impl>,
+    rightmost: Rightmost,
+) -> bool {
+    if context.relative_selector_anchor().is_some() {
+        // FIXME(emilio): This currently can happen with nesting, and it's not fully
+        // correct, arguably. But the ideal solution isn't super-clear either. For now,
+        // cope with it and explicitly reject it at match time. See [1] for discussion.
+        //
+        // [1]: https://github.com/w3c/csswg-drafts/issues/9600
+        return false;
+    }
+    context.nest_for_relative_selector(element.opaque(), |context| {
+        do_match_relative_selectors(selectors, element, context, rightmost)
+    })
+}
+
 /// Matches a relative selector in a list of relative selectors.
-fn matches_relative_selectors<E: Element>(
+fn do_match_relative_selectors<E: Element>(
     selectors: &[RelativeSelector<E::Impl>],
     element: &E,
     context: &mut MatchingContext<E::Impl>,
@@ -1088,11 +1107,9 @@ where
         Component::Negation(ref list) => context.shared.nest_for_negation(|context| {
             !matches_complex_selector_list(list.slice(), element, context, rightmost)
         }),
-        Component::Has(ref relative_selectors) => context
-            .shared
-            .nest_for_relative_selector(element.opaque(), |context| {
-                matches_relative_selectors(relative_selectors, element, context, rightmost)
-            }),
+        Component::Has(ref relative_selectors) => {
+            match_relative_selectors(relative_selectors, element, context.shared, rightmost)
+        },
         Component::Combinator(_) => unsafe {
             debug_unreachable!("Shouldn't try to selector-match combinators")
         },
