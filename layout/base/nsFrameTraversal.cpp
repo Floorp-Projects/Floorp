44ea06c342b5b4827b875f0fb316b8c3ad1174aa
Norisz Fay <nfay@mozilla.com>
2023-11-16 16:05:55 +0200

    Backed out changeset d3fec9e5f456 (bug 1856539) for causing bustage on nsFrameTraversal.cpp CLOSED TREE

--- layout/base/nsFrameTraversal.cpp
+++ layout/base/nsFrameTraversal.cpp
@@ -13,10 +13,8 @@
 #include "nsPlaceholderFrame.h"
 #include "nsPresContext.h"
 #include "nsContainerFrame.h"
-#include "mozilla/dom/Element.h"
 
 using namespace mozilla;
-using namespace mozilla::dom;
 
 class nsFrameIterator : public nsIFrameEnumerator {
  public:
@@ -92,8 +90,6 @@ class nsFrameIterator : public nsIFrameEnumerator {
    */
   nsIFrame* GetPlaceholderFrame(nsIFrame* aFrame);
   bool IsPopupFrame(nsIFrame* aFrame);
-
-  bool IsInvokerOpenPopoverFrame(nsIFrame* aFrame);
 
   nsPresContext* const mPresContext;
   const bool mLockScroll;
@@ -357,11 +353,8 @@ nsIFrame* nsFrameIterator::GetFirstChild(nsIFrame* aFrame) {
   if (result && mFollowOOFs) {
     result = nsPlaceholderFrame::GetRealFrameFor(result);
 
-    if (IsPopupFrame(result) || IsInvokerOpenPopoverFrame(result)) {
-      result = GetNextSibling(result);
-    }
+    if (IsPopupFrame(result)) result = GetNextSibling(result);
   }
-
   return result;
 }
 
@@ -371,11 +364,8 @@ nsIFrame* nsFrameIterator::GetLastChild(nsIFrame* aFrame) {
   if (result && mFollowOOFs) {
     result = nsPlaceholderFrame::GetRealFrameFor(result);
 
-    if (IsPopupFrame(result) || IsInvokerOpenPopoverFrame(result)) {
-      result = GetPrevSibling(result);
-    }
+    if (IsPopupFrame(result)) result = GetPrevSibling(result);
   }
-
   return result;
 }
 
@@ -385,13 +375,11 @@ nsIFrame* nsFrameIterator::GetNextSibling(nsIFrame* aFrame) {
   if (aFrame == mLimiter) return nullptr;
   if (aFrame) {
     result = GetNextSiblingInner(aFrame);
-    if (result && mFollowOOFs) {
+    if (result && mFollowOOFs)
       result = nsPlaceholderFrame::GetRealFrameFor(result);
-      if (IsPopupFrame(result) || IsInvokerOpenPopoverFrame(result)) {
-        result = GetNextSibling(result);
-      }
-    }
   }
+
+  if (mFollowOOFs && IsPopupFrame(result)) result = GetNextSibling(result);
 
   return result;
 }
@@ -402,13 +390,11 @@ nsIFrame* nsFrameIterator::GetPrevSibling(nsIFrame* aFrame) {
   if (aFrame == mLimiter) return nullptr;
   if (aFrame) {
     result = GetPrevSiblingInner(aFrame);
-    if (result && mFollowOOFs) {
+    if (result && mFollowOOFs)
       result = nsPlaceholderFrame::GetRealFrameFor(result);
-      if (IsPopupFrame(result) || IsInvokerOpenPopoverFrame(result)) {
-        result = GetPrevSibling(result);
-      }
-    }
   }
+
+  if (mFollowOOFs && IsPopupFrame(result)) result = GetPrevSibling(result);
 
   return result;
 }
@@ -443,16 +429,6 @@ bool nsFrameIterator::IsPopupFrame(nsIFrame* aFrame) {
     return false;
   }
   return aFrame && aFrame->IsMenuPopupFrame();
-}
-
-bool nsFrameIterator::IsInvokerOpenPopoverFrame(nsIFrame* aFrame) {
-  if (const nsIContent* currentContent = aFrame->GetContent()) {
-    if (const auto* popover = Element::FromNode(currentContent)) {
-      return popover && popover->IsPopoverOpen() &&
-             popover->GetPopoverData()->GetInvoker();
-    }
-  }
-  return false;
 }
 
 // nsVisualIterator implementation
