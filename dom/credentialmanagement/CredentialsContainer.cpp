1dd1fb337f20666b9fb628a777d2a75ad3dcde0f
John Schanck <jschanck@mozilla.com>
2023-11-17 22:43:40 +0000

    Bug 1838932 - implement WebAuthn conditional mediation. r=keeler
    Differential Revision: https://phabricator.services.mozilla.com/D192764

--- dom/credentialmanagement/CredentialsContainer.cpp
+++ dom/credentialmanagement/CredentialsContainer.cpp
@@ -163,9 +163,8 @@ already_AddRefed<Promise> CredentialsContainer::Get(
       return CreateAndRejectWithNotAllowed(mParent, aRv);
     }
 
-    if (conditionallyMediated) {
-      // Conditional mediation for WebAuthn Get() will be implemented in
-      // Bug 1838932.
+    if (conditionallyMediated &&
+        !StaticPrefs::security_webauthn_enable_conditional_mediation()) {
       RefPtr<Promise> promise = CreatePromise(mParent, aRv);
       if (!promise) {
         return nullptr;
@@ -176,8 +175,8 @@ already_AddRefed<Promise> CredentialsContainer::Get(
     }
 
     EnsureWebAuthnManager();
-    return mManager->GetAssertion(aOptions.mPublicKey.Value(), aOptions.mSignal,
-                                  aRv);
+    return mManager->GetAssertion(aOptions.mPublicKey.Value(),
+                                  conditionallyMediated, aOptions.mSignal, aRv);
   }
 
   if (aOptions.mIdentity.WasPassed() &&
