86011b46436b2814c5db415087f2849e563b8c54
Thomas Wisniewski <twisniewski@mozilla.com>
2023-11-19 15:34:40 +0000

    Bug 1852340 - part 1: Add a new Gecko component for reporting broken websites; r=ayeddi,fluent-reviewers,mossop,flod,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D190647

---
+++ browser/components/reportbrokensite/test/browser/browser_reason_dropdown.js
@@ -0,0 +1,65 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/* Tests to ensure that the reason dropdown is shown or hidden
+ * based on its pref, and that its optional and required modes affect
+ * the Send button and report appropriately.
+ */
+
+"use strict";
+
+add_common_setup();
+
+async function clickSendAndCheckPing(rbs, expectedReason = null) {
+  const pingCheck = new Promise(resolve => {
+    GleanPings.brokenSiteReport.testBeforeNextSubmit(() => {
+      Assert.equal(
+        Glean.brokenSiteReport.breakageCategory.testGetValue(),
+        expectedReason
+      );
+      resolve();
+    });
+  });
+  await rbs.clickSend();
+  return pingCheck;
+}
+
+add_task(async function testReasonDropdown() {
+  ensureReportBrokenSitePreffedOn();
+
+  await BrowserTestUtils.withNewTab(
+    REPORTABLE_PAGE_URL,
+    async function (browser) {
+      ensureReasonDisabled();
+
+      let rbs = await AppMenu().openReportBrokenSite();
+      await rbs.isReasonHidden();
+      await rbs.isSendButtonEnabled();
+      await clickSendAndCheckPing(rbs);
+      await rbs.clickOkay();
+
+      ensureReasonOptional();
+      rbs = await AppMenu().openReportBrokenSite();
+      await rbs.isReasonOptional();
+      await rbs.isSendButtonEnabled();
+      await clickSendAndCheckPing(rbs);
+      await rbs.clickOkay();
+
+      rbs = await AppMenu().openReportBrokenSite();
+      await rbs.isReasonOptional();
+      rbs.chooseReason("slow");
+      await rbs.isSendButtonEnabled();
+      await clickSendAndCheckPing(rbs, "slow");
+      await rbs.clickOkay();
+
+      ensureReasonRequired();
+      rbs = await AppMenu().openReportBrokenSite();
+      await rbs.isReasonRequired();
+      await rbs.isSendButtonDisabled();
+      rbs.chooseReason("media");
+      await rbs.isSendButtonEnabled();
+      await clickSendAndCheckPing(rbs, "media");
+      await rbs.clickOkay();
+    }
+  );
+});
