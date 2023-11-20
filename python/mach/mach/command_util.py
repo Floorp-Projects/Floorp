29c905d8a92438878bc506b557b368335a329939
ahochheiden <ahochheiden@mozilla.com>
2023-11-20 18:05:57 +0000

    Bug 1857279 - Load `@SettingsProvider` settings early so that we can parse set aliases and load the correct virtualenv r=ahal
    Aliased commands have not loaded the virtualenv associated with a
    command/subcommand since bug 1695312. It has been defaulting to the
    `common` virtualenv, which worked most of the time since most commands
    use `common` and it also contains a large share of our dependencies.
    
    Differential Revision: https://phabricator.services.mozilla.com/D192376

--- python/mach/mach/command_util.py
+++ python/mach/mach/command_util.py
@@ -341,6 +341,13 @@ class DetermineCommandVenvAction(argparse.Action):
             return
 
         command = values[0]
+
+        aliases = namespace.mach_command_aliases
+
+        if command in aliases:
+            alias = aliases[command]
+            command = alias
+
         setattr(namespace, "command_name", command)
 
         # the "help" command does not have a module file, it's handled
--- tools/tryselect/test/conftest.py
+++ tools/tryselect/test/conftest.py
@@ -54,13 +54,11 @@ def patch_vcs(monkeypatch):
 @pytest.fixture(scope="session")
 def run_mach():
     import mach_initialize
-    from mach.config import ConfigSettings
     from tryselect.tasks import build
 
     mach = mach_initialize.initialize(build.topsrcdir)
 
     def inner(args):
-        mach.settings = ConfigSettings()
         return mach.run(args)
 
     return inner
