0f979f867bf3f1be2abaf7ab1d805889c08cf824
Thiago Perrotta <tperrotta@chromium.org>
2023-11-20 10:49:19 +0000

    Bug 1864467 [wpt PR 43116] - [wdspec] remove network intercept: use browsingContext.load, a=testonly
    Automatic update from web-platform-tests
    [wdspec] remove network intercept: use browsingContext.load (#43116)
    
    Follow-up-of: #42969
    --
    
    wpt-commits: 95577b3f9fa6276728f895a8be4730083a471563
    wpt-pr: 43116

--- testing/web-platform/tests/webdriver/tests/bidi/network/remove_intercept/remove_intercept.py
+++ testing/web-platform/tests/webdriver/tests/bidi/network/remove_intercept/remove_intercept.py
@@ -1,7 +1,5 @@
 # META: timeout=long
 
-import asyncio
-
 import pytest
 
 from .. import (
@@ -20,7 +18,7 @@ PAGE_OTHER_TEXT = "/webdriver/tests/bidi/network/support/other.txt"
     "responseStarted",
 ])
 async def test_remove_intercept(
-    bidi_session, wait_for_event, url, setup_network_test, add_intercept, top_context, wait_for_future_safe, phase
+    bidi_session, wait_for_event, url, setup_network_test, add_intercept, subscribe_events, top_context, wait_for_future_safe, phase
 ):
     network_events = await setup_network_test(
         events=[
@@ -41,13 +39,18 @@ async def test_remove_intercept(
 
     on_network_event = wait_for_event(f"network.{phase}")
 
+    await subscribe_events(events=["browsingContext.load"], contexts=[top_context["context"]])
+
+    browsing_context_load_events = []
+
+    async def on_browsing_context_load_event(method, data):
+        browsing_context_load_events.append(data)
+
+    remove_listener = bidi_session.add_event_listener("browsingContext.load", on_browsing_context_load_event)
+
     # Request to top_context should be blocked.
     # TODO(https://github.com/w3c/webdriver-bidi/issues/188): Use a timeout argument when available.
-    with pytest.raises(asyncio.TimeoutError):
-        await asyncio.wait_for(
-            asyncio.shield(bidi_session.browsing_context.navigate(context=top_context["context"], url=text_url, wait="complete")),
-            timeout=2.0,
-        )
+    await bidi_session.browsing_context.navigate(context=top_context["context"], url=text_url, wait="complete")
 
     await wait_for_future_safe(on_network_event)
 
@@ -91,6 +94,9 @@ async def test_remove_intercept(
 
     assert len(response_completed_events) == 1
     assert_response_event(response_completed_events[0], is_blocked=False)
+
+    assert len(browsing_context_load_events) == 0
+    remove_listener()
 
 
 @pytest.mark.asyncio
