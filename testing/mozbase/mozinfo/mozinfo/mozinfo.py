dadd77303c2382fa49a5f8ce0273d8ef42c0b84f
Sebastian Hengst <archaeopteryx@coole-files.de>
2023-11-20 15:26:19 +0000

    Bug 1865645 - drop pre-Windows 10 code from mozinfo.py, add support for OS version 11. r=jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D194087

--- testing/mozbase/mozinfo/mozinfo/mozinfo.py
+++ testing/mozbase/mozinfo/mozinfo/mozinfo.py
@@ -37,37 +37,6 @@ class unknown(object):
 unknown = unknown()  # singleton
 
 
-def get_windows_version():
-    import ctypes
-
-    class OSVERSIONINFOEXW(ctypes.Structure):
-        _fields_ = [
-            ("dwOSVersionInfoSize", ctypes.c_ulong),
-            ("dwMajorVersion", ctypes.c_ulong),
-            ("dwMinorVersion", ctypes.c_ulong),
-            ("dwBuildNumber", ctypes.c_ulong),
-            ("dwPlatformId", ctypes.c_ulong),
-            ("szCSDVersion", ctypes.c_wchar * 128),
-            ("wServicePackMajor", ctypes.c_ushort),
-            ("wServicePackMinor", ctypes.c_ushort),
-            ("wSuiteMask", ctypes.c_ushort),
-            ("wProductType", ctypes.c_byte),
-            ("wReserved", ctypes.c_byte),
-        ]
-
-    os_version = OSVERSIONINFOEXW()
-    os_version.dwOSVersionInfoSize = ctypes.sizeof(os_version)
-    retcode = ctypes.windll.Ntdll.RtlGetVersion(ctypes.byref(os_version))
-    if retcode != 0:
-        raise OSError
-
-    return (
-        os_version.dwMajorVersion,
-        os_version.dwMinorVersion,
-        os_version.dwBuildNumber,
-    )
-
-
 # get system information
 info = {
     "os": unknown,
@@ -92,16 +61,11 @@ if system in ["Microsoft", "Windows"]:
     else:
         processor = os.environ.get("PROCESSOR_ARCHITECTURE", processor)
     system = os.environ.get("OS", system).replace("_", " ")
-    (major, minor, _, _, service_pack) = os.sys.getwindowsversion()
+    (major, minor, build_number, _, service_pack) = os.sys.getwindowsversion()
     info["service_pack"] = service_pack
-    if major >= 6 and minor >= 2:
-        # On windows >= 8.1 the system call that getwindowsversion uses has
-        # been frozen to always return the same values. In this case we call
-        # the RtlGetVersion API directly, which still provides meaningful
-        # values, at least for now.
-        major, minor, build_number = get_windows_version()
-        version = "%d.%d.%d" % (major, minor, build_number)
-
+    version = "%d.%d.%d" % (major, minor, build_number)
+    if major == 10 and minor == 0 and build_number >= 22000:
+        major = 11
     os_version = "%d.%d" % (major, minor)
 elif system.startswith(("MINGW", "MSYS_NT")):
     # windows/mingw python build (msys)
