9c37ae9f9cc4a80f02684fb91fbfa19cb897acc3
Yury Delendik <ydelendik@mozilla.com>
2023-11-21 15:24:50 +0000

    Bug 1862089 - Delay I64 box creations in wasm::Instance::callImport. r=rhunt
    Differential Revision: https://phabricator.services.mozilla.com/D192330

--- js/src/wasm/WasmValue.cpp
+++ js/src/wasm/WasmValue.cpp
@@ -356,6 +356,8 @@ template bool wasm::ToJSValue<DebugCodegenVal>(JSContext* cx, const void* src,
                                                FieldType type,
                                                MutableHandleValue dst,
                                                CoercionLevel level);
+template bool wasm::ToJSValueMayGC<NoDebug>(FieldType type);
+template bool wasm::ToJSValueMayGC<DebugCodegenVal>(FieldType type);
 
 template bool wasm::ToWebAssemblyValue<NoDebug>(JSContext* cx, HandleValue val,
                                                 ValType type, void* loc,
@@ -373,6 +375,8 @@ template bool wasm::ToJSValue<DebugCodegenVal>(JSContext* cx, const void* src,
                                                ValType type,
                                                MutableHandleValue dst,
                                                CoercionLevel level);
+template bool wasm::ToJSValueMayGC<NoDebug>(ValType type);
+template bool wasm::ToJSValueMayGC<DebugCodegenVal>(ValType type);
 
 template <typename Debug = NoDebug>
 bool ToWebAssemblyValue_i8(JSContext* cx, HandleValue val, int8_t* loc) {
@@ -852,9 +856,19 @@ bool wasm::ToJSValue(JSContext* cx, const void* src, FieldType type,
 }
 
 template <typename Debug>
+bool wasm::ToJSValueMayGC(FieldType type) {
+  return type.kind() == FieldType::I64;
+}
+
+template <typename Debug>
 bool wasm::ToJSValue(JSContext* cx, const void* src, ValType type,
                      MutableHandleValue dst, CoercionLevel level) {
   return wasm::ToJSValue(cx, src, FieldType(type.packed()), dst, level);
+}
+
+template <typename Debug>
+bool wasm::ToJSValueMayGC(ValType type) {
+  return wasm::ToJSValueMayGC(FieldType(type.packed()));
 }
 
 /* static */
