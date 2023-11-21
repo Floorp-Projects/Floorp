0f82854c5dd5ec329ffc208decb4d4afbf1fa4d7
alwu <alwu@mozilla.com>
2023-11-21 22:43:30 +0000

    Bug 1858546 - part1 : ensure LPAC permission on Widevine L1 path. r=bobowen,aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D192888

--- dom/media/gmp/GMPServiceParent.h
+++ dom/media/gmp/GMPServiceParent.h
@@ -51,6 +51,9 @@ class GeckoMediaPluginServiceParent final
   NS_IMETHOD HasPluginForAPI(const nsACString& aAPI,
                              const nsTArray<nsCString>& aTags,
                              bool* aRetVal) override;
+  NS_IMETHOD FindPluginDirectoryForAPI(const nsACString& aAPI,
+                                       const nsTArray<nsCString>& aTags,
+                                       nsIFile** aDirectory) override;
   NS_IMETHOD GetNodeId(const nsAString& aOrigin,
                        const nsAString& aTopLevelOrigin,
                        const nsAString& aGMPName,
