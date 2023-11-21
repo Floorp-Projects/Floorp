ad22ad47e4567a7eb3b613ef53ff7f5a991960ab
Mirko Brodesser <mbrodesser@igalia.com>
2023-11-21 08:39:32 +0000

    Bug 1865610: part 7) Add test for internally assigned priority of font-face loaded from `<head>`. r=jfkthame
    Tests existing behavior. It differs from Chromium's, which assigns
    highest priority instead of high.
    
    The test is added for the fetchpriority feature because loading a
    font-face font has overlapping code with linking fonts via `<link
    rel=preload as=font>` in `FontPreloader`
    (https://searchfox.org/mozilla-central/rev/d6576544301cacc0e393fbc919c53e4e6b0d46ec/layout/style/FontPreloader.h#25).
    
    I checked that no other tests already test this.
    
    Daniel: see parts 8 and 9 too.
    
    Differential Revision: https://phabricator.services.mozilla.com/D193550

---
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/font-face-tests-data.js
@@ -0,0 +1,13 @@
+const kExpectedRequests = [
+    {   fileNameAndSuffix: "dummy.font",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+];
+
+export const kTestFolderName = "font-face-tests";
+
+export const kTestData = [
+    {   testFileName: "load-font-face-from-head.h2.html",
+        expectedRequests: kExpectedRequests
+    },
+];
