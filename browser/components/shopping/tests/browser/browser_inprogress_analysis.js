b56100d719d2cc50a95fdd4a0c1530309a453759
Fred Chasen <fchasen@gmail.com>
2023-11-22 23:00:06 +0000

    Bug 1861195 - Pass analysis progress from pollForAnalysisCompleted and display progress in sidebar message. r=shopping-reviewers,kpatenio
    - Makes the ShoppingProduct extend EventEmitter and emit an `analysis-progress` event when the is progress reported during polling for analysis statuses.
    - Passes that event down to shopping message bar with a `progress` attribute.
    - Passes the progress event as l10n args to the message bar header.
    - Displays the progress in the message bar icon by setting it in a `--analysis-progress-pcent` variable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D193831

--- browser/components/shopping/tests/browser/browser_inprogress_analysis.js
+++ browser/components/shopping/tests/browser/browser_inprogress_analysis.js
@@ -63,7 +63,8 @@ add_task(async function test_in_progress_analysis_unanalyzed() {
 });
 
 /**
- * Tests that the correct shopping-message-bar component appears after re-requesting analysis for a stale product.
+ * Tests that the correct shopping-message-bar component appears after re-requesting analysis for a stale product,
+ * and that component shows progress percentage.
  */
 add_task(async function test_in_progress_analysis_stale() {
   await BrowserTestUtils.withNewTab(
@@ -105,6 +106,8 @@ add_task(async function test_in_progress_analysis_stale() {
 
           // Mock the response from analysis status being "pending"
           shoppingContainer.isAnalysisInProgress = true;
+          // Mock the analysis status response with progress.
+          shoppingContainer.analysisProgress = 50;
           // Add data back, as it was unset as due to the lack of mock APIs.
           // TODO: Support for the mocks will be added in Bug 1853474.
           shoppingContainer.data = Cu.cloneInto(mockData, content);
@@ -112,10 +115,34 @@ add_task(async function test_in_progress_analysis_stale() {
           await messageBarVisiblePromise;
           await shoppingContainer.updateComplete;
 
+          let shoppingMessageBarEl = shoppingContainer.shoppingMessageBarEl;
           is(
-            shoppingContainer.shoppingMessageBarEl?.getAttribute("type"),
+            shoppingMessageBarEl?.getAttribute("type"),
             "reanalysis-in-progress",
             "shopping-message-bar type should be correct"
+          );
+          is(
+            shoppingMessageBarEl?.getAttribute("progress"),
+            "50",
+            "shopping-message-bar should have progress"
+          );
+
+          let messageBarEl =
+            shoppingMessageBarEl?.shadowRoot.querySelector("message-bar");
+          is(
+            messageBarEl?.getAttribute("style"),
+            "--analysis-progress-pcent: 50%;",
+            "message-bar should have progress set as a CSS variable"
+          );
+
+          let messageBarContainerEl =
+            shoppingMessageBarEl?.shadowRoot.querySelector(
+              "#message-bar-container"
+            );
+          is(
+            messageBarContainerEl.querySelector("#header")?.dataset.l10nArgs,
+            `{"percentage":50}`,
+            "message-bar-container header should have progress set as a l10n arg"
           );
         }
       );
--- browser/components/storybook/stories/shopping-message-bar.stories.mjs
+++ browser/components/storybook/stories/shopping-message-bar.stories.mjs
@@ -7,6 +7,9 @@ import { html, ifDefined } from "lit.all.mjs";
 // eslint-disable-next-line import/no-unassigned-import
 import "browser/components/shopping/content/shopping-message-bar.mjs";
 
+window.MozXULElement.insertFTLIfNeeded("browser/shopping.ftl");
+window.MozXULElement.insertFTLIfNeeded("toolkit/branding/brandings.ftl");
+
 export default {
   title: "Domain-specific UI Widgets/Shopping/Shopping Message Bar",
   component: "shopping-message-bar",
@@ -34,41 +37,18 @@ export default {
     actions: {
       handles: ["click"],
     },
-    fluent: `
-shopping-message-bar-warning-stale-analysis-message-2 = New info to check
-shopping-message-bar-warning-stale-analysis-button = Check now
-shopping-message-bar-generic-error =
-  .heading = No info available right now
-  .message = We’re working to resolve the issue. Please check back soon.
-shopping-message-bar-warning-not-enough-reviews =
-  .heading = Not enough reviews yet
-  .message = When this product has more reviews, we’ll be able to check their quality.
-shopping-message-bar-warning-product-not-available =
-  .heading = Product is not available
-  .message = If you see this product is back in stock, report it and we’ll work on checking the reviews.
-shopping-message-bar-warning-product-not-available-button2 = Report product is in stock
-shopping-message-bar-thanks-for-reporting =
-  .heading = Thanks for reporting!
-  .message = We should have info about this product’s reviews within 24 hours. Please check back.
-shopping-message-bar-warning-product-not-available-reported =
-  .heading = Info coming soon
-  .message = We should have info about this product’s reviews within 24 hours. Please check back.
-shopping-message-bar-analysis-in-progress-title2 = Checking review quality
-shopping-message-bar-analysis-in-progress-message2 = This could take about 60 seconds.
-shopping-survey-thanks =
-  .heading = Thanks for your feedback!
-shopping-message-bar-page-not-supported =
-  .heading = We can’t check these reviews
-  .message = Unfortunately, we can’t check the review quality for certain types of products. For example, gift cards and streaming video, music, and games.
-    `,
   },
 };
 
-const Template = ({ type }) => html`
-  <shopping-message-bar type=${ifDefined(type)}></shopping-message-bar>
+const Template = ({ type, progress }) => html`
+  <shopping-message-bar
+    type=${ifDefined(type)}
+    progress=${ifDefined(progress)}
+  ></shopping-message-bar>
 `;
 
 export const DefaultShoppingMessageBar = Template.bind({});
 DefaultShoppingMessageBar.args = {
   type: "stale",
+  progress: 0,
 };
