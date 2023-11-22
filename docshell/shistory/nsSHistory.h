61ceb81b8121c8838691b97eabc231db531ca1f2
Jonathan Watt <jwatt@jwatt.org>
2023-11-22 08:08:13 +0000

    Bug 1865891 p5. Rename EvictContentViewerForEntry to match nsIDocumentViewer's new name. r=emilio
    Depends on D194277
    
    Differential Revision: https://phabricator.services.mozilla.com/D194278

--- docshell/shistory/nsSHistory.h
+++ docshell/shistory/nsSHistory.h
@@ -250,7 +250,7 @@ class nsSHistory : public mozilla::LinkedListElement<nsSHistory>,
   // Evict content viewers in this window which don't lie in the "safe" range
   // around aIndex.
   virtual void EvictOutOfRangeWindowDocumentViewers(int32_t aIndex);
-  void EvictContentViewerForEntry(nsISHEntry* aEntry);
+  void EvictDocumentViewerForEntry(nsISHEntry* aEntry);
   static void GloballyEvictContentViewers();
   static void GloballyEvictAllContentViewers();
 
