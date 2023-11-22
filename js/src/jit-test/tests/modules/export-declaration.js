dedd558552d532523daa4e76f89140d9fc2c88a4
Tooru Fujisawa <arai_a@mac.com>
2023-11-22 11:13:56 +0000

    Bug 1862814 - Part 1: Use 1-origin column number in Reflect.parse. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D193375

--- js/src/jit-test/tests/modules/export-declaration.js
+++ js/src/jit-test/tests/modules/export-declaration.js
@@ -460,9 +460,9 @@ var loc = parseAsModule("export { a as b } from 'c'", {
 }).body[0].loc;
 
 assertEq(loc.start.line, 1);
-assertEq(loc.start.column, 0);
+assertEq(loc.start.column, 1);
 assertEq(loc.start.line, 1);
-assertEq(loc.end.column, 26);
+assertEq(loc.end.column, 27);
 
 assertThrowsInstanceOf(function () {
    parseAsModule("function f() { export a }");
