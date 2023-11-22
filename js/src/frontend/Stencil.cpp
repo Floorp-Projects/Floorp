22b9ae6db82776c573189ebf09b4243da35c4696
Tooru Fujisawa <arai_a@mac.com>
2023-11-22 11:13:57 +0000

    Bug 1862814 - Part 2: Use 1-origin column number in script-related dump. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D193376

--- js/src/frontend/Stencil.cpp
+++ js/src/frontend/Stencil.cpp
@@ -4428,7 +4428,7 @@ void ScriptStencilExtra::dumpFields(js::JSONPrinter& json) const {
   json.property("toStringStart", extent.toStringStart);
   json.property("toStringEnd", extent.toStringEnd);
   json.property("lineno", extent.lineno);
-  json.property("column", extent.column.zeroOriginValue());
+  json.property("column", extent.column.oneOriginValue());
   json.endObject();
 
   json.property("memberInitializers", memberInitializers_);
@@ -4591,7 +4591,7 @@ static void DumpInputScriptFields(js::JSONPrinter& json,
     json.property("toStringStart", extent.toStringStart);
     json.property("toStringEnd", extent.toStringEnd);
     json.property("lineno", extent.lineno);
-    json.property("column", extent.column.zeroOriginValue());
+    json.property("column", extent.column.oneOriginValue());
   }
   json.endObject();
 
