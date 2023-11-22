e4d4b7b23d7dc91b40efc61c1ecc15ac8820762f
Tooru Fujisawa <arai_a@mac.com>
2023-11-22 11:13:57 +0000

    Bug 1862814 - Part 4: Use 1-origin column number in testing function compile options. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D193378

--- js/src/jit-test/tests/debug/Source-startLine-startColumn.js
+++ js/src/jit-test/tests/debug/Source-startLine-startColumn.js
@@ -7,8 +7,7 @@ g.evaluate("function f(x) {}");
 var fw = gw.getOwnPropertyDescriptor("f").value;
 assertEq(fw.script.source.startLine, 1);
 assertEq(fw.script.source.startColumn, 1);
-// NOTE: evaluate function takes 0-origin column number.
 g.evaluate("function g(x) {}", { lineNumber: 10, columnNumber: 5 });
 var gw = gw.getOwnPropertyDescriptor("g").value;
 assertEq(gw.script.source.startLine, 10);
-assertEq(gw.script.source.startColumn, 6);
+assertEq(gw.script.source.startColumn, 5);
--- js/src/jit-test/tests/parser/bug-1161312.js
+++ js/src/jit-test/tests/parser/bug-1161312.js
@@ -1,3 +1,3 @@
 // Test that lazy scripts can handle OOB column numbers.
 
-assertEq(evaluate(`var f = x=>saveStack().column; f()`, { columnNumber: 1729 }), 1741);
+assertEq(evaluate(`var f = x=>saveStack().column; f()`, { columnNumber: 1730 }), 1741);
