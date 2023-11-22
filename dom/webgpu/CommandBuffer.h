287470a50cd1d8c5621e9b2e89e41efccb21f061
Nicolas Silva <nsilva@mozilla.com>
2023-11-22 14:15:35 +0000

    Bug 1861985 - Handle CommandBuffer/CommandBuffer lifecycle correctly. r=webgpu-reviewers,ErichDonGubler
    In wgpu, command encoders and command buffers actually share the same identity and resource, so dropping one drops the other. This commit makes it so that our gecko wrappers take that into account. The lifetime is now tied to the encoder which may be held alive by a command buffer if there is one.
    
    Differential Revision: https://phabricator.services.mozilla.com/D193313

--- dom/webgpu/CommandBuffer.h
+++ dom/webgpu/CommandBuffer.h
@@ -22,7 +22,8 @@ class CommandBuffer final : public ObjectBase, public ChildOf<Device> {
   GPU_DECL_JS_WRAP(CommandBuffer)
 
   CommandBuffer(Device* const aParent, RawId aId,
-                nsTArray<WeakPtr<CanvasContext>>&& aTargetContexts);
+                nsTArray<WeakPtr<CanvasContext>>&& aTargetContexts,
+                RefPtr<CommandEncoder>&& aEncoder);
 
   Maybe<RawId> Commit();
 
@@ -33,6 +34,11 @@ class CommandBuffer final : public ObjectBase, public ChildOf<Device> {
 
   const RawId mId;
   const nsTArray<WeakPtr<CanvasContext>> mTargetContexts;
+  // Command buffers and encoders share the same identity (this is a
+  // simplifcation currently made by wgpu). To avoid dropping the same ID twice,
+  // the wgpu resource lifetime is tied to the encoder which is held alive by
+  // the command buffer.
+  RefPtr<CommandEncoder> mEncoder;
 };
 
 }  // namespace mozilla::webgpu
