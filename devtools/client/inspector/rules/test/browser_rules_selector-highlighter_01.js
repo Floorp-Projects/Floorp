f597224482ab78a8155340b7e5a6e0a4bfcaa154
Nicolas Chevobbe <nchevobbe@mozilla.com>
2023-11-23 11:14:52 +0000

    Bug 1844053 - [devtools] Make selector highlighter icon keyboard accessible. r=devtools-reviewers,ochameau.
    Differential Revision: https://phabricator.services.mozilla.com/D190262

--- devtools/client/inspector/rules/test/browser_rules_selector-highlighter_01.js
+++ devtools/client/inspector/rules/test/browser_rules_selector-highlighter_01.js
@@ -18,6 +18,8 @@ const TEST_URI = `
 add_task(async function () {
   await addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
   const { inspector, view } = await openRuleView();
+  const { waitForHighlighterTypeShown, waitForHighlighterTypeHidden } =
+    getHighlighterTestHelpers(inspector);
 
   const activeHighlighter = inspector.highlighters.getActiveHighlighter(
     inspector.highlighters.TYPES.SELECTOR
@@ -25,8 +27,60 @@ add_task(async function () {
   ok(!activeHighlighter, "No selector highlighter is active");
 
   info("Clicking on a selector icon");
-  const { highlighter, isShown } = await clickSelectorIcon(view, "body, p, td");
+  let data = await clickSelectorIcon(view, "body, p, td");
 
-  ok(highlighter, "The selector highlighter instance was created");
-  ok(isShown, "The selector highlighter was shown");
+  ok(data.highlighter, "The selector highlighter instance was created");
+  ok(data.isShown, "The selector highlighter was shown");
+
+  info("Click on the same icon to disable highlighter");
+  data = await clickSelectorIcon(view, "body, p, td");
+  ok(!data.isShown, "The highlighter is not visible anymore");
+
+  info("Check that the selector highlighter can be toggled from the keyboard");
+  const ruleEl = getRuleViewRule(view, "body, p, td", 0);
+  const selectorContainerEl = ruleEl.querySelector(
+    ".ruleview-selectors-container"
+  );
+  const selectorHighlighterIcon = ruleEl.querySelector(
+    ".ruleview-selectorhighlighter"
+  );
+  is(
+    selectorHighlighterIcon.getAttribute("aria-pressed"),
+    "false",
+    "selector highlighter icon is not pressed by default"
+  );
+  selectorContainerEl.focus();
+  EventUtils.synthesizeKey("VK_TAB", {}, selectorContainerEl.ownerGlobal);
+  await waitFor(
+    () =>
+      selectorContainerEl.ownerDocument.activeElement ===
+      selectorHighlighterIcon
+  );
+  ok(true, "selector highlighter button can be focused");
+
+  const onHighlighterShown = waitForHighlighterTypeShown(
+    inspector.highlighters.TYPES.SELECTOR
+  );
+  EventUtils.synthesizeKey("VK_RETURN", {}, selectorContainerEl.ownerGlobal);
+  data = await onHighlighterShown;
+
+  ok(true, "The selector highlighter was shown from the keyboard");
+  ok(data.highlighter, "The selector highlighter instance was created");
+
+  await waitFor(
+    () => selectorHighlighterIcon.getAttribute("aria-pressed") === "true"
+  );
+  ok(true, "selector highlighter icon is pressed");
+
+  const onHighlighterHidden = waitForHighlighterTypeHidden(
+    inspector.highlighters.TYPES.SELECTOR
+  );
+  EventUtils.synthesizeKey("VK_RETURN", {}, selectorContainerEl.ownerGlobal);
+  await onHighlighterHidden;
+  ok(true, "The selector highlighter was hidden from the keyboard");
+  is(
+    selectorHighlighterIcon.getAttribute("aria-pressed"),
+    "false",
+    "selector highlighter icon is no longer pressed"
+  );
 });
