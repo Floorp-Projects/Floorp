333989cbd76648a92599e6b5b8e7f352172964f0
Tim Huang <tihuang@mozilla.com>
2023-11-24 21:04:32 +0000

    Bug 1865627 - Add cloudfront.net to the tracking-annotation skip urls. r=anti-tracking-reviewers,pbz
    Differential Revision: https://phabricator.services.mozilla.com/D194610

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3360,7 +3360,7 @@ pref("urlclassifier.features.emailtracking.datacollection.allowlistTables", "moz
 pref("urlclassifier.disallow_completions", "goog-downloadwhite-digest256,base-track-digest256,mozstd-trackwhite-digest256,content-track-digest256,mozplugin-block-digest256,mozplugin2-block-digest256,ads-track-digest256,social-track-digest256,analytics-track-digest256,base-fingerprinting-track-digest256,content-fingerprinting-track-digest256,base-cryptomining-track-digest256,content-cryptomining-track-digest256,fanboyannoyance-ads-digest256,fanboysocial-ads-digest256,easylist-ads-digest256,easyprivacy-ads-digest256,adguard-ads-digest256,social-tracking-protection-digest256,social-tracking-protection-facebook-digest256,social-tracking-protection-linkedin-digest256,social-tracking-protection-twitter-digest256,base-email-track-digest256,content-email-track-digest256");
 
 // Workaround for Google Recaptcha
-pref("urlclassifier.trackingAnnotationSkipURLs", "google.com/recaptcha/,*.google.com/recaptcha/");
+pref("urlclassifier.trackingAnnotationSkipURLs", "google.com/recaptcha/,*.google.com/recaptcha/,d3vox9szr7t2nm.cloudfront.net");
 pref("privacy.rejectForeign.allowList", "");
 
 // The list of email webapp sites
