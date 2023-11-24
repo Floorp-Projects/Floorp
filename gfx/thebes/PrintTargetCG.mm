073ad22f3e057920a5f1c0da98a9a07e83cc8969
Jonathan Watt <jwatt@jwatt.org>
2023-11-24 14:02:22 +0000

    Bug 1866272. Support CSS page-size in WPT print reftests on macOS. r=emilio, a=RyanVM
    This WPT RFC proposes adding support for CSS page-size in WPT Print Reftests:
    https://github.com/web-platform-tests/rfcs/pull/173
    
    I've made the necessary changes to enable that in bug 1866319, but it turns out
    that this change doesn't work on Mac if/when we take the nsIOutputStream code
    path (as we do when generating PDF output for WPT Print Reftests).
    
    This fixes the nsIOutputStream code path in our Mac printing code to support
    setting page sizes in PDF output, just as the non-nsIOutputStream code path
    does.
    
    Differential Revision: https://phabricator.services.mozilla.com/D194534

--- gfx/thebes/PrintTargetCG.mm
+++ gfx/thebes/PrintTargetCG.mm
@@ -227,18 +227,42 @@ nsresult PrintTargetCG::AbortPrinting() {
 nsresult PrintTargetCG::BeginPage(const IntSize& aSizeInPoints) {
   NS_OBJC_BEGIN_TRY_BLOCK_RETURN;
 
+  unsigned int width;
+  unsigned int height;
+  if (StaticPrefs::layout_css_page_orientation_enabled()) {
+    width = static_cast<unsigned int>(aSizeInPoints.width);
+    height = static_cast<unsigned int>(aSizeInPoints.height);
+  } else {
+    width = static_cast<unsigned int>(mSize.width);
+    height = static_cast<unsigned int>(mSize.height);
+  }
+
   CGContextRef context;
   if (mPrintToStreamContext) {
-    CGContextBeginPage(mPrintToStreamContext, nullptr);
+    CGRect bounds = CGRectMake(0, 0, width, height);
+    CGContextBeginPage(mPrintToStreamContext, &bounds);
     context = mPrintToStreamContext;
   } else {
     // XXX Why are we calling this if we don't check the return value?
     PMSessionError(mPrintSession);
 
+    // XXX For mixed sheet sizes that aren't simply an orientation switch, we
+    // will want to be able to pass a sheet size here, using something like:
+    //   PMRect bounds = { 0, 0, double(height), double(width) };
+    // But the docs for PMSessionBeginPageNoDialog's `pageFrame` parameter say:
+    //   "You should pass NULL, as this parameter is currentlyunsupported."
+    // https://developer.apple.com/documentation/applicationservices/1463416-pmsessionbeginpagenodialog?language=objc
+    // And indeed, it doesn't appear to do anything.
+    // (It seems weird that CGContextBeginPage (above) supports passing a rect,
+    // and that that works for setting sheet sizes in PDF output, but the Core
+    // Printing API does not.)
+    // We can always switch to PrintTargetPDF - we use that for Windows/Linux
+    // anyway. But Core Graphics output is better than Cairo's in some cases.
+    //
+    // For now, we support switching sheet orientation only:
     if (StaticPrefs::layout_css_page_orientation_enabled()) {
       ::PMOrientation pageOrientation =
-          aSizeInPoints.width < aSizeInPoints.height ? kPMPortrait
-                                                     : kPMLandscape;
+          width < height ? kPMPortrait : kPMLandscape;
       ::PMSetOrientation(mPageFormat, pageOrientation, kPMUnlocked);
       // We don't need to reset the orientation, since we set it for every page.
     }
@@ -255,16 +279,6 @@ nsresult PrintTargetCG::BeginPage(const IntSize& aSizeInPoints) {
     if (!context) {
       return NS_ERROR_FAILURE;
     }
-  }
-
-  unsigned int width;
-  unsigned int height;
-  if (StaticPrefs::layout_css_page_orientation_enabled()) {
-    width = static_cast<unsigned int>(aSizeInPoints.width);
-    height = static_cast<unsigned int>(aSizeInPoints.height);
-  } else {
-    width = static_cast<unsigned int>(mSize.width);
-    height = static_cast<unsigned int>(mSize.height);
   }
 
   // Initially, origin is at bottom-left corner of the paper.
