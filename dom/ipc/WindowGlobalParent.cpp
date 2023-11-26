82c8c809332bb4e3f37e46c1effc6f49f9072731
Chris H-C <chutten@mozilla.com>
2023-11-26 19:55:30 +0000

    Bug 1852098 - Increment Glean use counter metrics and submit the use-counters ping during shutdown  a=RyanVM
    Original Revision: https://phabricator.services.mozilla.com/D193250
    Depends on D193249
    
    Depends on D194230
    
    Differential Revision: https://phabricator.services.mozilla.com/D194231

--- dom/ipc/WindowGlobalParent.cpp
+++ dom/ipc/WindowGlobalParent.cpp
@@ -26,8 +26,10 @@
 #include "mozilla/dom/MediaController.h"
 #include "mozilla/dom/WindowGlobalChild.h"
 #include "mozilla/dom/ChromeUtils.h"
+#include "mozilla/dom/UseCounterMetrics.h"
 #include "mozilla/dom/ipc/IdType.h"
 #include "mozilla/dom/ipc/StructuredCloneData.h"
+#include "mozilla/glean/GleanMetrics.h"
 #include "mozilla/Components.h"
 #include "mozilla/ScopeExit.h"
 #include "mozilla/ServoCSSParser.h"
@@ -1117,6 +1119,7 @@ void WindowGlobalParent::FinishAccumulatingPageUseCounters() {
     }
 
     Telemetry::Accumulate(Telemetry::TOP_LEVEL_CONTENT_DOCUMENTS_DESTROYED, 1);
+    glean::use_counter::top_level_content_documents_destroyed.Add();
 
     bool any = false;
     for (int32_t c = 0; c < eUseCounter_Count; ++c) {
@@ -1132,6 +1135,7 @@ void WindowGlobalParent::FinishAccumulatingPageUseCounters() {
                       Telemetry::GetHistogramName(id), urlForLogging->get());
       }
       Telemetry::Accumulate(id, 1);
+      IncrementUseCounter(uc, /* aIsPage = */ true);
     }
 
     if (!any) {
