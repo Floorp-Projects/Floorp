c4ecf938c29e45e90674ebc9cd72ebbfa79c0450
Mike Conley <mconley@mozilla.com>
2023-11-27 16:42:20 +0000

    Bug 1860668 - Adjust Istanbul coverage thresholds for Store.jsm and TopSitesFeed.jsm. r=thecount
    These are now tested via xpcshell, and their coverage is no longer reported
    up through Istanbul.
    
    Depends on D193488
    
    Differential Revision: https://phabricator.services.mozilla.com/D193489

--- browser/components/newtab/karma.mc.config.js
+++ browser/components/newtab/karma.mc.config.js
@@ -136,17 +136,29 @@ module.exports = function (config) {
               functions: 100,
               branches: 75,
             },
-            "lib/TopSitesFeed.jsm": {
-              statements: 70,
-              lines: 75,
-              functions: 80,
-              branches: 60,
-            },
             "lib/Screenshots.jsm": {
               statements: 94,
               lines: 94,
               functions: 75,
               branches: 84,
+            },
+            /**
+             * Store.jsm is tested via an xpcshell test
+             */
+            "lib/Store.jsm": {
+              statements: 8,
+              lines: 8,
+              functions: 0,
+              branches: 0,
+            },
+            /**
+             * TopSitesFeed.jsm is tested via an xpcshell test
+             */
+            "lib/TopSitesFeed.jsm": {
+              statements: 9,
+              lines: 9,
+              functions: 5,
+              branches: 0,
             },
             /**
              * TopStoresFeed.jsm is not tested in automation and is slated
