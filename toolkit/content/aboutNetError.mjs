e616c4a2d5e0674e988d03e23a7abffc72eb88b5
Iulian Moraru <imoraru@mozilla.com>
2023-11-27 15:38:16 +0200

    Backed out changeset 7c7a7ff17005 (bug 1866503) for causing conflicts with backout of Bug 1866503.

--- toolkit/content/aboutNetError.mjs
+++ toolkit/content/aboutNetError.mjs
@@ -10,6 +10,11 @@ import {
   pemToDER,
 } from "chrome://global/content/certviewer/certDecoder.mjs";
 
+window.addEventListener("load", console.log);
+window.addEventListener("load", console.log, true);
+document.addEventListener("load", console.log);
+document.addEventListener("load", console.log, true);
+
 const formatter = new Intl.DateTimeFormat();
 
 const HOST_NAME = getHostName();
