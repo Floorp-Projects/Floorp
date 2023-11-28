62129447cd1af10a6bc51188fd2153feba3ead27
serge-sans-paille <sguelton@mozilla.com>
2023-11-28 11:32:56 +0000

    Bug 1862309 - Add speedometer3 to the PGO training set r=smaug,sparky,jnicol
    This is done for both Android and regular builds, and it brings a nice
    1% perf improvement on Windows, and 2% on Android.
    
    Do not get rid of Speedometer2 yet, adding Speedometer3 already brings
    an interesting performance boost.
    
    To update to the latest version run
    
            > ./mach vendor third_party/webkit/PerformanceTests/Speedometer3/moz.yaml
    
    Differential Revision: https://phabricator.services.mozilla.com/D194040

---
+++ third_party/webkit/PerformanceTests/Speedometer3/resources/todomvc/architecture-examples/preact-complex/scripts/build.js
@@ -0,0 +1,16 @@
+/**
+ * Build the TodoMVC Preact Complex DOM example.
+ */
+const path = require("path");
+const { buildComplex } = require("big-dom-generator/utils/buildComplex");
+
+const options = {
+    callerDirectory: path.resolve(__dirname),
+    sourceDirectory: path.join("..", "node_modules", "todomvc-preact", "dist"),
+    title: "TodoMVC: Preact Complex DOM",
+    filesToMove: ["node_modules/big-dom-generator/dist/big-dom-generator.css", "node_modules/big-dom-generator/dist/logo.png", "node_modules/big-dom-generator/utils/app.css"],
+    standaloneDirectory: path.resolve(__dirname, "..", "..", "preact"),
+    complexDirectory: path.resolve(__dirname, ".."),
+};
+
+buildComplex(options);
