a5f26cb8381272b90a5094a40188c5f14125ebed
KS <kshampur@mozilla.com>
2023-11-28 15:33:43 +0000

    Bug 1846849 - Update chromium cron to include ARM relevant fetches. r=perftest-reviewers,taskgraph-reviewers,afinder,ahal
    Update the chrommium fetch cron to now include the ARM64 Mac OS relevant
    files. This ensures we will have available the correct chromedriver for
    CaR on Apple Silicon.
    
    Differential Revision: https://phabricator.services.mozilla.com/D193865

--- taskcluster/gecko_taskgraph/target_tasks.py
+++ taskcluster/gecko_taskgraph/target_tasks.py
@@ -1055,9 +1055,11 @@ def target_tasks_chromium_update(full_task_graph, parameters, graph_config):
         "fetch-win32-chromium",
         "fetch-win64-chromium",
         "fetch-mac-chromium",
+        "fetch-mac-chromium-arm",
         "toolchain-linux64-custom-car",
         "toolchain-win64-custom-car",
         "toolchain-macosx64-custom-car",
+        "toolchain-macosx-arm64-custom-car",
         "toolchain-android-custom-car",
     ]
 
