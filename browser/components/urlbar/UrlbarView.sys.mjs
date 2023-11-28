221ee286cff6eefcc8e468f6406da20dff8cf8c2
Dão Gottwald <dao@mozilla.com>
2023-11-28 07:56:47 +0000

    Bug 1866620 - [Firefox Suggest Opt-in Experiment] Bring narrow layout for topPosition=false in line with topPosition=true. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D194691

--- browser/components/urlbar/UrlbarView.sys.mjs
+++ browser/components/urlbar/UrlbarView.sys.mjs
@@ -2723,10 +2723,9 @@ export class UrlbarView {
   }
 
   #enableOrDisableRowWrap() {
-    this.#rows.toggleAttribute(
-      "wrap",
-      getBoundsWithoutFlushing(this.input.textbox).width < 650
-    );
+    let wrap = getBoundsWithoutFlushing(this.input.textbox).width < 650;
+    this.#rows.toggleAttribute("wrap", wrap);
+    this.oneOffSearchButtons.container.toggleAttribute("wrap", wrap);
   }
 
   /**
