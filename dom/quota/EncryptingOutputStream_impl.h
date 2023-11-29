ac88e97d0d911cdcf3973630b439e82372fc0062
Jan Varga <jvarga@mozilla.com>
2023-11-29 22:46:17 +0000

    Bug 1864123 - Optimize flushing to base stream;  a=RyanVM
    Original Revision: https://phabricator.services.mozilla.com/D193286
    
    Differential Revision: https://phabricator.services.mozilla.com/D195043

--- dom/quota/EncryptingOutputStream_impl.h
+++ dom/quota/EncryptingOutputStream_impl.h
@@ -18,6 +18,8 @@
 #include "nsDebug.h"
 #include "nsError.h"
 #include "nsIAsyncOutputStream.h"
+#include "nsIRandomGenerator.h"
+#include "nsServiceManagerUtils.h"
 
 namespace mozilla::dom::quota {
 template <typename CipherStrategy>
@@ -205,6 +207,26 @@ nsresult EncryptingOutputStream<CipherStrategy>::FlushToBaseStream() {
   if (!mNextByte) {
     // Nothing to do.
     return NS_OK;
+  }
+
+  if (mNextByte < mEncryptedBlock->MaxPayloadLength()) {
+    if (!mRandomGenerator) {
+      mRandomGenerator =
+          do_GetService("@mozilla.org/security/random-generator;1");
+      if (NS_WARN_IF(!mRandomGenerator)) {
+        return NS_ERROR_FAILURE;
+      }
+    }
+
+    const auto payload = mEncryptedBlock->MutablePayload();
+
+    const auto unusedPayload = payload.From(mNextByte);
+
+    nsresult rv = mRandomGenerator->GenerateRandomBytesInto(
+        unusedPayload.Elements(), unusedPayload.Length());
+    if (NS_WARN_IF(NS_FAILED(rv))) {
+      return rv;
+    }
   }
 
   // XXX The compressing stream implementation this was based on wrote a stream
