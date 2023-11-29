dc03eaf2f0ea2d04789c39fd4f7332819980be0c
Michael Froman <mfroman@mozilla.com>
2023-11-29 12:31:53 -0600

    Bug 1867099 - Vendor libwebrtc from 19ff1ad237
    Upstream commit: https://webrtc.googlesource.com/src/+/19ff1ad237e3b77f15cc5aa427941b9f0a324f94
        Reland "Always use AV1 specific bitrate limits when spatial layers are used."
    
        This reverts commit 030c6ff43fe407f87ae329512ebb87604b253074.
    
        Reason for revert: reland with fix
    
        Original change's description:
        > Revert "Always use AV1 specific bitrate limits when spatial layers are used."
        >
        > This reverts commit d2d165d47cc7a2aaa53596ad8055ddc30b76101b.
        >
        > Reason for revert: All the regressions!
        >
        > Original change's description:
        > > Always use AV1 specific bitrate limits when spatial layers are used.
        > >
        > > Bug: b/295129711
        > > Change-Id: I93569027bea34c43e2a3c4de0875e8bbddd5b64e
        > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/319283
        > > Reviewed-by: Michael Horowitz <mhoro@webrtc.org>
        > > Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        > > Reviewed-by: Sergey Silkin <ssilkin@webrtc.org>
        > > Cr-Commit-Position: refs/heads/main@{#40719}
        >
        > Bug: b/295129711
        > Change-Id: I5776edbaba33e86eb10414062ef2b29510f40b8d
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/319880
        > Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
        > Cr-Commit-Position: refs/heads/main@{#40730}
    
        Bug: b/295129711
        Change-Id: I5fe84184d3f3780fdc4e9c1d43c4989d333d44a7
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/319881
        Reviewed-by: Michael Horowitz <mhoro@webrtc.org>
        Commit-Queue: Philip Eliasson <philipel@webrtc.org>
        Reviewed-by: Sergey Silkin <ssilkin@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40739}

--- third_party/libwebrtc/modules/video_coding/codecs/av1/av1_svc_config.cc
+++ third_party/libwebrtc/modules/video_coding/codecs/av1/av1_svc_config.cc
@@ -85,6 +85,9 @@ bool SetAv1SvcConfig(VideoCodec& video_codec,
     }
   }
 
+  bool configured_for_single_spatial_layer =
+      ScalabilityModeToNumSpatialLayers(*scalability_mode) == 1;
+
   if (ScalabilityMode reduced = LimitNumSpatialLayers(
           *scalability_mode,
           GetLimitedNumSpatialLayers(video_codec.width, video_codec.height));
@@ -118,7 +121,7 @@ bool SetAv1SvcConfig(VideoCodec& video_codec,
     spatial_layer.active = true;
   }
 
-  if (info.num_spatial_layers == 1) {
+  if (configured_for_single_spatial_layer) {
     SpatialLayer& spatial_layer = video_codec.spatialLayers[0];
     spatial_layer.minBitrate = video_codec.minBitrate;
     spatial_layer.maxBitrate = video_codec.maxBitrate;
@@ -129,10 +132,8 @@ bool SetAv1SvcConfig(VideoCodec& video_codec,
 
   for (int sl_idx = 0; sl_idx < info.num_spatial_layers; ++sl_idx) {
     SpatialLayer& spatial_layer = video_codec.spatialLayers[sl_idx];
-    // minBitrate and maxBitrate formulas are copied from vp9 settings and
-    // are not yet tuned for av1.
     const int num_pixels = spatial_layer.width * spatial_layer.height;
-    int min_bitrate_kbps = (600.0 * std::sqrt(num_pixels) - 95'000.0) / 1000.0;
+    int min_bitrate_kbps = (480.0 * std::sqrt(num_pixels) - 95'000.0) / 1000.0;
     spatial_layer.minBitrate = std::max(min_bitrate_kbps, 20);
     spatial_layer.maxBitrate = 50 + static_cast<int>(1.6 * num_pixels / 1000.0);
     spatial_layer.targetBitrate =
