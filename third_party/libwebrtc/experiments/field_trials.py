5f4c74be0f3ce51c5496e2db10c2f9ecf1a403c7
Michael Froman <mfroman@mozilla.com>
2023-11-29 17:28:34 -0600

    Bug 1867099 - Vendor libwebrtc from 7d81e18ac0
    Upstream commit: https://webrtc.googlesource.com/src/+/7d81e18ac0d3c91ce77e0eeee4f1baa783ed2306
        Reformat field_trials.py to follow PEP-8
    
        Bug: None
        Change-Id: I29fffef124e3ca54ec2075bbae005fd5fdd75e83
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/321181
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Jeremy Leconte <jleconte@google.com>
        Commit-Queue: Emil Lundmark <lndmrk@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40815}

--- third_party/libwebrtc/experiments/field_trials.py
+++ third_party/libwebrtc/experiments/field_trials.py
@@ -18,12 +18,12 @@ import dataclasses
 # TODO(bugs.webrtc.org/14154): End date and bug should also be stored.
 @dataclasses.dataclass(frozen=True)
 class FieldTrial:
-  """Representation of all attributes associated with a field trial.
+    """Representation of all attributes associated with a field trial.
 
-  Attributes:
-    key: Field trial key.
-  """
-  key: str
+    Attributes:
+      key: Field trial key.
+    """
+    key: str
 
 
 # As per the policy in `g3doc/field-trials.md`, all field trials should be
@@ -33,82 +33,83 @@ REGISTERED_FIELD_TRIALS: Set[FieldTrial] = {
 }
 
 
-def RegistryHeader(field_trials: Set[FieldTrial] = None) -> str:
-  """Generates a C++ header with all field trial keys.
+def registry_header(field_trials: Set[FieldTrial] = None) -> str:
+    """Generates a C++ header with all field trial keys.
 
-  Args:
-    field_trials: Field trials to include in the header.
+    Args:
+      field_trials: Field trials to include in the header.
 
-  Returns:
-    String representation of a C++ header file containing all field trial keys.
+    Returns:
+      String representation of a C++ header file containing all field trial
+      keys.
 
-  >>> trials = {FieldTrial('B'), FieldTrial('A'), FieldTrial('B')}
-  >>> print(RegistryHeader(trials))
-  // This file was automatically generated. Do not edit.
-  <BLANKLINE>
-  #ifndef GEN_REGISTERED_FIELD_TRIALS_H_
-  #define GEN_REGISTERED_FIELD_TRIALS_H_
-  <BLANKLINE>
-  #include "absl/strings/string_view.h"
-  <BLANKLINE>
-  namespace webrtc {
-  <BLANKLINE>
-  inline constexpr absl::string_view kRegisteredFieldTrials[] = {
-      "A",
-      "B",
-  };
-  <BLANKLINE>
-  }  // namespace webrtc
-  <BLANKLINE>
-  #endif  // GEN_REGISTERED_FIELD_TRIALS_H_
-  <BLANKLINE>
-  """
-  if not field_trials:
-    field_trials = REGISTERED_FIELD_TRIALS
-  registered_keys = [f.key for f in field_trials]
-  keys = '\n'.join(f'    "{k}",' for k in sorted(registered_keys))
-  return ('// This file was automatically generated. Do not edit.\n'
-          '\n'
-          '#ifndef GEN_REGISTERED_FIELD_TRIALS_H_\n'
-          '#define GEN_REGISTERED_FIELD_TRIALS_H_\n'
-          '\n'
-          '#include "absl/strings/string_view.h"\n'
-          '\n'
-          'namespace webrtc {\n'
-          '\n'
-          'inline constexpr absl::string_view kRegisteredFieldTrials[] = {\n'
-          f'{keys}\n'
-          '};\n'
-          '\n'
-          '}  // namespace webrtc\n'
-          '\n'
-          '#endif  // GEN_REGISTERED_FIELD_TRIALS_H_\n')
+    >>> trials = {FieldTrial('B'), FieldTrial('A'), FieldTrial('B')}
+    >>> print(registry_header(trials))
+    // This file was automatically generated. Do not edit.
+    <BLANKLINE>
+    #ifndef GEN_REGISTERED_FIELD_TRIALS_H_
+    #define GEN_REGISTERED_FIELD_TRIALS_H_
+    <BLANKLINE>
+    #include "absl/strings/string_view.h"
+    <BLANKLINE>
+    namespace webrtc {
+    <BLANKLINE>
+    inline constexpr absl::string_view kRegisteredFieldTrials[] = {
+        "A",
+        "B",
+    };
+    <BLANKLINE>
+    }  // namespace webrtc
+    <BLANKLINE>
+    #endif  // GEN_REGISTERED_FIELD_TRIALS_H_
+    <BLANKLINE>
+    """
+    if not field_trials:
+        field_trials = REGISTERED_FIELD_TRIALS
+    registered_keys = [f.key for f in field_trials]
+    keys = '\n'.join(f'    "{k}",' for k in sorted(registered_keys))
+    return ('// This file was automatically generated. Do not edit.\n'
+            '\n'
+            '#ifndef GEN_REGISTERED_FIELD_TRIALS_H_\n'
+            '#define GEN_REGISTERED_FIELD_TRIALS_H_\n'
+            '\n'
+            '#include "absl/strings/string_view.h"\n'
+            '\n'
+            'namespace webrtc {\n'
+            '\n'
+            'inline constexpr absl::string_view kRegisteredFieldTrials[] = {\n'
+            f'{keys}\n'
+            '};\n'
+            '\n'
+            '}  // namespace webrtc\n'
+            '\n'
+            '#endif  // GEN_REGISTERED_FIELD_TRIALS_H_\n')
 
 
-def CmdHeader(args: argparse.Namespace) -> None:
-  args.output.write(RegistryHeader())
+def cmd_header(args: argparse.Namespace) -> None:
+    args.output.write(registry_header())
 
 
 def main() -> None:
-  parser = argparse.ArgumentParser()
-  subcommand = parser.add_subparsers(dest='cmd')
-  parser_header = subcommand.add_parser(
-      'header',
-      help='generate C++ header file containing registered field trial keys')
-  parser_header.add_argument('--output',
-                             default=sys.stdout,
-                             type=argparse.FileType('w'),
-                             required=False,
-                             help='output file')
-  parser_header.set_defaults(cmd=CmdHeader)
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    subcommand = parser.add_subparsers(dest='cmd')
+    parser_header = subcommand.add_parser(
+        'header',
+        help='generate C++ header file containing registered field trial keys')
+    parser_header.add_argument('--output',
+                               default=sys.stdout,
+                               type=argparse.FileType('w'),
+                               required=False,
+                               help='output file')
+    parser_header.set_defaults(cmd=cmd_header)
+    args = parser.parse_args()
 
-  if not args.cmd:
-    parser.print_help(sys.stderr)
-    sys.exit(1)
+    if not args.cmd:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
-  args.cmd(args)
+    args.cmd(args)
 
 
 if __name__ == '__main__':
-  main()
+    main()
