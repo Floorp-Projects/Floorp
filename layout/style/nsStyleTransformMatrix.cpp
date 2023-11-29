55c55f1ae99a83d22cb298b51171b13aa78a8746
Jonathan Watt <jwatt@jwatt.org>
2023-11-29 00:42:17 +0000

    Bug 1864396. Prevent transform-box:stroke-box crash. r=emilio,layout-reviewers,longsonr
    Differential Revision: https://phabricator.services.mozilla.com/D194712

--- layout/style/nsStyleTransformMatrix.cpp
+++ layout/style/nsStyleTransformMatrix.cpp
@@ -49,10 +49,24 @@ static nsRect GetSVGBox(const nsIFrame* aFrame) {
     return nsRect(-aFrame->GetPosition(), CSSPixel::ToAppUnits(size));
   };
 
+  auto transformBox = aFrame->StyleDisplay()->mTransformBox;
+  if ((transformBox == StyleTransformBox::StrokeBox ||
+       transformBox == StyleTransformBox::BorderBox) &&
+      aFrame->StyleSVGReset()->HasNonScalingStroke()) {
+    // To calculate stroke bounds for an element with `non-scaling-stroke` we
+    // need to resolve its transform to its outer-svg, but to resolve that
+    // transform when it has `transform-box:stroke-box` (or `border-box`)
+    // may require its stroke bounds. There's no ideal way to break this
+    // cyclical dependency, but we break it by converting to
+    // `transform-box:fill-box` here.
+    // https://github.com/w3c/csswg-drafts/issues/9640
+    transformBox = StyleTransformBox::FillBox;
+  }
+
   // For SVG elements without associated CSS layout box, the used value for
   // content-box is fill-box and for border-box is stroke-box.
   // https://drafts.csswg.org/css-transforms-1/#transform-box
-  switch (aFrame->StyleDisplay()->mTransformBox) {
+  switch (transformBox) {
     case StyleTransformBox::ContentBox:
     case StyleTransformBox::FillBox: {
       // Percentages in transforms resolve against the SVG bbox, and the
