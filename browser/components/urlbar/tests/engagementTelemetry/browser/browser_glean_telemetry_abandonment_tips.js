497a10837c07ea2df80c8579898cb934f7ec9333
Anna Yeddi <ayeddi@mozilla.com>
2023-11-29 00:00:24 +0000

    Bug 1864941 - Add exceptions from a11y_checks for 3 tests clicking outside of the urlbar in Search and Address Bar. r=adw
    It appears that `browser/components/urlbar/tests/engagementTelemetry/browser/browser_glean_telemetry_engagement_edge_cases.js` and  `browser/components/urlbar/tests/engagementTelemetry/browser/browser_glean_telemetry_abandonment_tips.js` tests the mouse/touch-specific behavior: the test cases are [sending a click event somewhere on the window to test that the tooltip would, in fact, disappear or not](https://searchfox.org/mozilla-central/source/browser/components/urlbar/tests/engagementTelemetry/browser/browser_glean_telemetry_abandonment_tips.js#61) (depending on the test case), while there are other ways to dismiss the tooltip that are accessible (i.e. by pressing `Esc` key, testing which is included in one of the test cases).
    
    Similarly, `browser/components/urlbar/tests/browser/browser_view_selectionByMouse.js` sends a `mouseup` events to the `<html:body>` to dismiss the url bar with a mouse-specific tests.
    
    These clicks are not expected or do not have to be done by a keyboard-only user and it does not have to confirm the keyboard focusability as part of the accessibility of these elements.
    
    We adding an exception from a11y_checks for these clicks, because the clicks are testing mouse-specific behavior where keyboard accessible alternative is available and is tested, and we removing the `fail-if` notation that was added by bug 1854660 before the investigation in the meta bug 1854661.
    
    Differential Revision: https://phabricator.services.mozilla.com/D193699

--- browser/components/urlbar/tests/engagementTelemetry/browser/browser_glean_telemetry_abandonment_tips.js
+++ browser/components/urlbar/tests/engagementTelemetry/browser/browser_glean_telemetry_abandonment_tips.js
@@ -58,7 +58,15 @@ add_task(async function mouse_down_with_tip() {
   await doTest(async browser => {
     await showPersistSearchTip("test");
     await UrlbarTestUtils.promisePopupClose(window, () => {
+      // We intentionally turn off this a11y check, because the following click
+      // is sent to test the telemetry behavior using an alternative way of the
+      // urlbar dismissal, where other ways are accessible, therefore this test
+      // can be ignored.
+      AccessibilityUtils.setEnv({
+        mustHaveAccessibleRule: false,
+      });
       EventUtils.synthesizeMouseAtCenter(browser, {});
+      AccessibilityUtils.resetEnv();
     });
 
     assertAbandonmentTelemetry([{ results: "tip_persist" }]);
@@ -67,7 +75,15 @@ add_task(async function mouse_down_with_tip() {
 
 add_task(async function mouse_down_without_tip() {
   await doTest(async browser => {
+    // We intentionally turn off this a11y check, because the following click
+    // is sent to test the telemetry behavior using an alternative way of the
+    // urlbar dismissal, where other ways are accessible, therefore this test
+    // can be ignored.
+    AccessibilityUtils.setEnv({
+      mustHaveAccessibleRule: false,
+    });
     EventUtils.synthesizeMouseAtCenter(browser, {});
+    AccessibilityUtils.resetEnv();
 
     assertAbandonmentTelemetry([]);
   });
