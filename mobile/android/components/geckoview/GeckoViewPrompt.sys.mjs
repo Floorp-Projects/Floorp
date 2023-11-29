70bb95b93d6bc7d5d38a6578bc29f92e797bc0f2
CanadaHonk <oj@oojmed.com>
2023-11-29 14:15:36 +0000

    Bug 1830909 - Implement <hr> in <select> r=hsivonen,emilio,geckoview-reviewers,desktop-theme-reviewers,Jamie,owlish
    Updated HTML parser to allow <hr> in <select>.
    
    Updated internal toolkit UI for <select> dropdown to create
    menuseperators for hrs.
    
    Updated WPT expectations:
     - HTML5Lib WebKit parsing for it now passes 100%
    
    Also includes Android support, but Fenix does not support separators
    in the menus used (single/multiple) yet so they are not rendered.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189065

--- mobile/android/components/geckoview/GeckoViewPrompt.sys.mjs
+++ mobile/android/components/geckoview/GeckoViewPrompt.sys.mjs
@@ -101,6 +101,8 @@ export class PromptFactory {
         } else if (win.HTMLOptionElement.isInstance(child)) {
           item.label = child.label || child.text;
           item.selected = child.selected;
+        } else if (win.HTMLHRElement.isInstance(child)) {
+          item.separator = true;
         } else {
           continue;
         }
