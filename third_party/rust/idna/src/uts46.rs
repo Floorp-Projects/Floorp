94568140db6b9b966de42262b44b6bbc465afb1f
Valentin Gosu <valentin.gosu@gmail.com>
2023-11-29 10:09:05 +0000

    Bug 1866135 - Upgrade rust-url to 2.5.0 r=necko-reviewers,supply-chain-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D194461

--- third_party/rust/idna/src/uts46.rs
+++ third_party/rust/idna/src/uts46.rs
@@ -453,7 +453,7 @@ impl Idna {
             return Errors::default();
         }
         let mut errors = processing(domain, self.config, &mut self.normalized, out);
-        self.output = std::mem::replace(out, String::with_capacity(out.len()));
+        self.output = core::mem::replace(out, String::with_capacity(out.len()));
         let mut first = true;
         for label in self.output.split('.') {
             if !first {
@@ -509,6 +509,7 @@ impl Idna {
 }
 
 #[derive(Clone, Copy)]
+#[must_use]
 pub struct Config {
     use_std3_ascii_rules: bool,
     transitional_processing: bool,
--- third_party/rust/url/src/lib.rs
+++ third_party/rust/url/src/lib.rs
@@ -134,7 +134,7 @@ url = { version = "2", features = ["debugger_visualizer"] }
 
 */
 
-#![doc(html_root_url = "https://docs.rs/url/2.4.1")]
+#![doc(html_root_url = "https://docs.rs/url/2.5.0")]
 #![cfg_attr(
     feature = "debugger_visualizer",
     debugger_visualizer(natvis_file = "../../debug_metadata/url.natvis")
@@ -205,6 +205,7 @@ pub struct Url {
 
 /// Full configuration for the URL parser.
 #[derive(Copy, Clone)]
+#[must_use]
 pub struct ParseOptions<'a> {
     base_url: Option<&'a Url>,
     encoding_override: EncodingOverride<'a>,
@@ -1566,7 +1567,9 @@ impl Url {
             });
         } else {
             self.query_start = None;
-            self.strip_trailing_spaces_from_opaque_path();
+            if fragment.is_none() {
+                self.strip_trailing_spaces_from_opaque_path();
+            }
         }
 
         self.restore_already_parsed_fragment(fragment);
@@ -2697,7 +2700,7 @@ impl Ord for Url {
 impl PartialOrd for Url {
     #[inline]
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        self.serialization.partial_cmp(&other.serialization)
+        Some(self.cmp(other))
     }
 }
 
