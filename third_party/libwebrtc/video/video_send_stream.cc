1340cc67e3280c9aa3e9477d204f3f0dd649e072
Michael Froman <mfroman@mozilla.com>
2023-11-29 12:37:22 -0600

    Bug 1867099 - Vendor libwebrtc from 6ba7feb302
    Upstream commit: https://webrtc.googlesource.com/src/+/6ba7feb302b478d13a586e3a35b6ecb28dd9977e
        Make video encoder reconfiguration logging more verbose
    
        logging the configuration, in particular the content type which
        together with RTP configuration information like the ssrcs helps differentiating between encoders.
    
        BUG=None
    
        Change-Id: I1b4b2ec2bffea338cc73c3a9c6a3f775d8f1c26b
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/319560
        Reviewed-by: Henrik Boström <hbos@webrtc.org>
        Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
        Commit-Queue: Philipp Hancke <phancke@microsoft.com>
        Cr-Commit-Position: refs/heads/main@{#40744}

--- third_party/libwebrtc/video/video_send_stream.cc
+++ third_party/libwebrtc/video/video_send_stream.cc
@@ -282,6 +282,8 @@ void VideoSendStream::ReconfigureVideoEncoder(VideoEncoderConfig config,
                                               SetParametersCallback callback) {
   RTC_DCHECK_RUN_ON(&thread_checker_);
   RTC_DCHECK_EQ(content_type_, config.content_type);
+  RTC_LOG(LS_VERBOSE) << "Encoder config: " << config.ToString()
+                      << " VideoSendStream config: " << config_.ToString();
   video_stream_encoder_->ConfigureEncoder(
       std::move(config),
       config_.rtp.max_packet_size - CalculateMaxHeaderSize(config_.rtp),
