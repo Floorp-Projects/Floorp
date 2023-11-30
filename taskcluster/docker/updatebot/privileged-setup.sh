d3c741fbbb40667d81de71682ff7409735016d98
Mike Hommey <mh+mozilla@glandium.org>
2023-11-30 08:45:30 +0000

    Bug 1867143 - Upgrade push-to-try docker image to Debian 12. r=releng-reviewers,bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D194958

--- taskcluster/docker/updatebot/privileged-setup.sh
+++ taskcluster/docker/updatebot/privileged-setup.sh
@@ -27,6 +27,7 @@ apt-get install -y --no-install-recommends \
     python3-minimal \
     python3-wheel \
     python3-pip \
+    python3-poetry \
     python3-venv \
     python3-requests \
     python3-requests-unixsocket \
@@ -75,8 +76,5 @@ git checkout "$UPDATEBOT_REVISION"
 cd /builds/worker/
 chown -R worker:worker .
 chown -R worker:worker .*
-
-python3 -m pip install -U pip
-python3 -m pip install poetry==1.2.2
 
 rm -rf /setup
