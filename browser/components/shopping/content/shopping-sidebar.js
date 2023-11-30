9d392f8dca9e8dbcd899f597f1b6497d5bb9c9cc
Cosmin Sabou <csabou@mozilla.com>
2023-11-30 23:00:03 +0200

    Backed out 2 changesets (bug 1858762, bug 1867336) as requested by jhirsch. a=backout
    Backed out changeset 5df38003afc9 (bug 1867336)
    Backed out changeset 7ec919f740b9 (bug 1858762)

--- browser/components/shopping/content/shopping-sidebar.js
+++ browser/components/shopping/content/shopping-sidebar.js
@@ -9,9 +9,6 @@
 {
   const SHOPPING_SIDEBAR_WIDTH_PREF =
     "browser.shopping.experience2023.sidebarWidth";
-  const SHOPPING_SIDEBAR_WIDTH_VAR = "--shopping-sidebar-width";
-  const SHOPPINGS_SIDEBAR_WIDTH_TRANSLATE_X_VAR =
-    "--shopping-sidebar-width-translate-x";
   class ShoppingSidebar extends MozXULElement {
     #browser;
     #initialized;
@@ -55,10 +52,7 @@
         0
       );
       if (previousWidth > 0) {
-        this.style.setProperty(
-          SHOPPING_SIDEBAR_WIDTH_VAR,
-          `${previousWidth}px`
-        );
+        this.style.width = `${previousWidth}px`;
       }
 
       this.resizeObserver = new ResizeObserver(this.resizeObserverFn);
@@ -69,14 +63,6 @@
 
     resizeObserverFn() {
       Services.prefs.setIntPref(SHOPPING_SIDEBAR_WIDTH_PREF, this.scrollWidth);
-
-      /* Setting `--shopping-sidebar-width` directly would cause the resize observer to loop.
-       * Update `shopping-sidebar-width-translate-x` instead to prevent this
-       * after opening or closing the sidebar. */
-      this.style.setProperty(
-        SHOPPINGS_SIDEBAR_WIDTH_TRANSLATE_X_VAR,
-        `${this.scrollWidth}px`
-      );
     }
   }
 
