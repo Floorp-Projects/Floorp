9961aeffabdb8cdea13419598e381126bdde5c8b
Narcis Beleuzu <nbeleuzu@mozilla.com>
2023-11-30 18:03:36 +0200

    Backed out changeset a6b49e33322e (bug 1845455) for bustages on FormAutofillNative.cpp . CLOSED TREE

--- toolkit/components/formautofill/FormAutofillNative.cpp
+++ toolkit/components/formautofill/FormAutofillNative.cpp
@@ -390,7 +390,6 @@ const Rule kCreditCardRules[] = {
     {RegexKey::CC_NAME,
      // Firefox-specific rules
      "account.*holder.*name"
-     "|^(credit[-\s]?card|card).*name"
      // de-DE
      "|^(kredit)?(karten|konto)inhaber"
      "|^(name).*karte"
