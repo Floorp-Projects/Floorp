90c2dca1e56619ff7f7ea8a50da51de1e241759b
Ray Kraesig <rkraesig@mozilla.com>
2023-11-30 19:59:15 +0000

    Bug 1858225 - [2/9] inline `nsFilePicker::...Impl()` functions  r=win-reviewers,mhowell
    These functions are essentially sprues, left over from earlier
    development. Trim them away.
    
    No functional changes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D193734

--- widget/windows/nsFilePicker.h
+++ widget/windows/nsFilePicker.h
@@ -83,13 +83,6 @@ class nsFilePicker : public nsBaseWinFilePicker {
   bool ShowFilePicker(const nsString& aInitialDir);
 
  private:
-  // Show the dialog (by default, remotely falling back to locally, or whatever
-  // is specified by the current config).
-  static Result<Maybe<Results>> ShowFilePickerImpl(
-      HWND aParent, FileDialogType type, nsTArray<Command> const& commands);
-  static Result<Maybe<nsString>> ShowFolderPickerImpl(
-      HWND aParent, nsTArray<Command> const& commands);
-
   // Show the dialog out-of-process.
   static Result<Maybe<Results>> ShowFilePickerRemote(
       HWND aParent, FileDialogType type, nsTArray<Command> const& commands);
