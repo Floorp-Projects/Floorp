55021f5329b63b068d19daf14059c0858d4a25f8
Mike Conley <mconley@mozilla.com>
2023-12-01 00:33:02 +0000

    Bug 1867340 - Enable the new Windows Jump List backend on Nightly only. r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D195010

--- browser/modules/WindowsJumpLists.sys.mjs
+++ browser/modules/WindowsJumpLists.sys.mjs
@@ -277,6 +277,8 @@ var Builder = class {
           customDescriptions
         );
       }
+    } catch (e) {
+      console.error("buildList failed: ", e);
     } finally {
       this._isBuilding = false;
     }
