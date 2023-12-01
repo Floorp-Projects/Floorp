b7a822c5bc50a419fbc4af05b0646152dde3d571
Paul Zuehlcke <pbz@mozilla.com>
2023-12-01 21:22:41 +0000

    Bug 1865914 - Part 3, r=Gijs,edgar a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D194886

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -6044,6 +6044,19 @@ nsIPrincipal* nsGlobalWindowInner::GetClientPrincipal() {
   return mClientSource ? mClientSource->GetPrincipal() : nullptr;
 }
 
+bool nsGlobalWindowInner::IsInFullScreenTransition() {
+  if (!mIsChrome) {
+    return false;
+  }
+
+  nsGlobalWindowOuter* outerWindow = GetOuterWindowInternal();
+  if (!outerWindow) {
+    return false;
+  }
+
+  return outerWindow->mIsInFullScreenTransition;
+}
+
 //*****************************************************************************
 // nsGlobalWindowInner: Timeout Functions
 //*****************************************************************************
--- toolkit/modules/PopupNotifications.sys.mjs
+++ toolkit/modules/PopupNotifications.sys.mjs
@@ -18,6 +18,7 @@ const ICON_ATTRIBUTE_SHOWING = "showing";
 const ICON_ANCHOR_ATTRIBUTE = "popupnotificationanchor";
 
 const PREF_SECURITY_DELAY = "security.notification_enable_delay";
+const FULLSCREEN_TRANSITION_TIME_SHOWN_OFFSET_MS = 2000;
 
 // Enumerated values for the POPUP_NOTIFICATION_STATS telemetry histogram.
 const TELEMETRY_STAT_OFFERED = 0;
@@ -308,6 +309,12 @@ export function PopupNotifications(tabbrowser, panel, iconBox, options = {}) {
     true
   );
 
+  Services.obs.addObserver(this, "fullscreen-transition-start");
+
+  this.window.addEventListener("unload", () => {
+    Services.obs.removeObserver(this, "fullscreen-transition-start");
+  });
+
   this.window.addEventListener("activate", this, true);
   if (this.tabbrowser.tabContainer) {
     this.tabbrowser.tabContainer.addEventListener("TabSelect", this, true);
@@ -352,6 +359,18 @@ PopupNotifications.prototype = {
   },
   get iconBox() {
     return this._iconBox;
+  },
+
+  observe(subject, topic) {
+    if (topic == "fullscreen-transition-start") {
+      // Extend security delay if the panel is open.
+      if (this.isPanelOpen) {
+        let notification = this.panel.firstChild?.notification;
+        if (notification) {
+          this._extendSecurityDelay([notification]);
+        }
+      }
+    }
   },
 
   /**
@@ -1216,6 +1235,13 @@ PopupNotifications.prototype = {
     }
   },
 
+  _extendSecurityDelay(notifications) {
+    let now = this.window.performance.now();
+    notifications.forEach(n => {
+      n.timeShown = now + FULLSCREEN_TRANSITION_TIME_SHOWN_OFFSET_MS;
+    });
+  },
+
   _showPanel: function PopupNotifications_showPanel(
     notificationsToShow,
     anchorElement
@@ -1309,6 +1335,12 @@ PopupNotifications.prototype = {
         // shown with "options.dismissed" will be recorded in a separate bucket.
         n._recordTelemetryStat(TELEMETRY_STAT_OFFERED);
       }, this);
+
+      // We're about to open the panel while in a full screen transition. Extend
+      // the security delay.
+      if (this.window.isInFullScreenTransition) {
+        this._extendSecurityDelay(notificationsToShow);
+      }
 
       let target = this.panel;
       if (target.parentNode) {
