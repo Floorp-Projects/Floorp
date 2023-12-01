750e3fd7b889c70c3b18785c271229e1168c843c
Mike Conley <mconley@mozilla.com>
2023-12-01 00:33:02 +0000

    Bug 1867340 - Enable the new Windows Jump List backend on Nightly only. r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D195010

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1446,6 +1446,15 @@ pref("browser.bookmarks.editDialog.maxRecentFolders", 7);
   pref("browser.taskbar.previews.max", 20);
   pref("browser.taskbar.previews.cachetime", 5);
   pref("browser.taskbar.lists.legacyBackend", true);
+
+  // We'll only enable the new off-main-thread jumplist backend on Nightly for
+  // now while we test it.
+#ifdef NIGHTLY_BUILD
+  pref("browser.taskbar.lists.legacyBackend", false);
+#else
+  pref("browser.taskbar.lists.legacyBackend", true);
+#endif
+
   pref("browser.taskbar.lists.enabled", true);
   pref("browser.taskbar.lists.frequent.enabled", true);
   pref("browser.taskbar.lists.recent.enabled", false);
