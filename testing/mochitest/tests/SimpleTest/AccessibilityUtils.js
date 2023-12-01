51ef5d2275a81ea57a4ec95458ac6c1d123a88d6
James Teh <jteh@mozilla.com>
2023-12-01 04:23:50 +0000

    Bug 1867170: a11y checks: Check the correct node in some cases involving shadow DOM. r=ayeddi
    First, if a click is synthesised at the centre of a custom element containing a slot, the click might target a text node which is a child of the slot.
    In that case, the composedTarget for the click event will be the slot, since click events on text nodes are retargeted to the parent node.
    The slot itself isn't rendered, so we were returning early due to the 0 bounds check, hiding potential failures.
    To fix this, walk to the slot's flattened tree parent.
    
    Second, if a click occurs on a node which doesn't have an Accessible, we try to walk to the nearest Accessible ancestor.
    If the node is in shadow DOM, the nearest Accessible might be outside of the shadow DOM.
    Previously, we would stop at the shadow root.
    Now, we walk flattened tree parents, which will walk us out of the shadow root.
    
    Differential Revision: https://phabricator.services.mozilla.com/D194944

--- testing/mochitest/tests/SimpleTest/AccessibilityUtils.js
+++ testing/mochitest/tests/SimpleTest/AccessibilityUtils.js
@@ -679,7 +679,7 @@ this.AccessibilityUtils = (function () {
   function findInteractiveAccessible(node) {
     let acc;
     // Walk DOM ancestors until we find one with an accessible.
-    for (; node && !acc; node = node.parentNode) {
+    for (; node && !acc; node = node.flattenedTreeParentNode) {
       acc = getAccessible(node);
     }
     if (!acc) {
@@ -793,6 +793,13 @@ this.AccessibilityUtils = (function () {
     handleEvent({ composedTarget }) {
       if (!this._shouldHandleClicks) {
         return;
+      }
+      if (composedTarget.tagName.toLowerCase() == "slot") {
+        // The click occurred on a text node inside a slot. Since events don't
+        // target text nodes, the event was retargeted to the slot. However, a
+        // slot isn't itself rendered. To deal with this, use the slot's parent
+        // instead.
+        composedTarget = composedTarget.flattenedTreeParentNode;
       }
       const bounds =
         composedTarget.ownerGlobal?.windowUtils?.getBoundsWithoutFlushing(
