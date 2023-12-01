443e61156b24f4e0c86c1af516496a30f3e7f565
Butkovits Atila <abutkovits@mozilla.com>
2023-12-01 08:32:00 +0200

    Backed out 2 changesets (bug 1865815) for causing failures at browser_parent_menuitems.js. CLOSED TREE
    Backed out changeset 727a1d40fbf1 (bug 1865815)
    Backed out changeset 385880f4db2b (bug 1865815)

--- browser/components/reportbrokensite/test/browser/browser_parent_menuitems.js
+++ browser/components/reportbrokensite/test/browser/browser_parent_menuitems.js
@@ -3,96 +3,79 @@
 
 /* Test that the Report Broken Site menu items are disabled
  * when the active tab is not on a reportable URL, and is hidden
- * when the feature is disabled via pref. Also make sure that
- * it does not appear on the App menu's Help sub-view.
+ * when the feature is disabled via pref.
  */
 
 "use strict";
 
 add_common_setup();
 
-requestLongerTimeout(10);
-
 add_task(async function test() {
   ensureReportBrokenSitePreffedOff();
 
-  const menus = [
-    [AppMenu(), false],
-    [AppMenuHelpSubmenu(), true],
-    [ProtectionsPanel(), false],
-    [HelpMenu(), false],
-  ];
+  const appMenu = AppMenu();
+  const menus = [appMenu, ProtectionsPanel(), HelpMenu()];
+
+  async function forceMenuItemStateUpdate() {
+    window.ReportBrokenSite.enableOrDisableMenuitems(window);
+
+    // the hidden/disabled state of all of the menuitems may not update until one
+    // is rendered; then the related <command>'s state is propagated to them all.
+    await appMenu.open();
+    await appMenu.close();
+  }
 
   await BrowserTestUtils.withNewTab("about:blank", async function () {
-    for (const [menu] of menus) {
-      await menu.open();
+    await forceMenuItemStateUpdate();
+    for (const { menuDescription, reportBrokenSite } of menus) {
       isMenuItemHidden(
-        menu.reportBrokenSite,
-        `${menu.menuDescription} option hidden on invalid page when preffed off`
+        reportBrokenSite,
+        `${menuDescription} option hidden on invalid page when preffed off`
       );
-      await menu.close();
     }
   });
 
   await BrowserTestUtils.withNewTab(REPORTABLE_PAGE_URL, async function () {
-    for (const [menu] of menus) {
-      await menu.open();
+    await forceMenuItemStateUpdate();
+    for (const { menuDescription, reportBrokenSite } of menus) {
       isMenuItemHidden(
-        menu.reportBrokenSite,
-        `${menu.menuDescription} option hidden on valid page when preffed off`
+        reportBrokenSite,
+        `${menuDescription} option hidden on valid page when preffed off`
       );
-      await menu.close();
     }
   });
 
   ensureReportBrokenSitePreffedOn();
 
   await BrowserTestUtils.withNewTab("about:blank", async function () {
-    for (const [menu, alwaysHidden] of menus) {
-      await menu.open();
-      if (alwaysHidden) {
-        isMenuItemHidden(
-          menu.reportBrokenSite,
-          `${menu.menuDescription} option hidden on invalid page when preffed on`
-        );
-      } else {
-        isMenuItemDisabled(
-          menu.reportBrokenSite,
-          `${menu.menuDescription} option disabled on invalid page when preffed on`
-        );
-      }
-      await menu.close();
+    await forceMenuItemStateUpdate();
+    for (const { menuDescription, reportBrokenSite } of menus) {
+      isMenuItemDisabled(
+        reportBrokenSite,
+        `${menuDescription} option disabled on invalid page when preffed on`
+      );
     }
   });
 
   await BrowserTestUtils.withNewTab(REPORTABLE_PAGE_URL, async function () {
-    for (const [menu, alwaysHidden] of menus) {
-      await menu.open();
-      if (alwaysHidden) {
-        isMenuItemHidden(
-          menu.reportBrokenSite,
-          `${menu.menuDescription} option hidden on valid page when preffed on`
-        );
-      } else {
-        isMenuItemEnabled(
-          menu.reportBrokenSite,
-          `${menu.menuDescription} option enabled on valid page when preffed on`
-        );
-      }
-      await menu.close();
+    await forceMenuItemStateUpdate();
+    for (const { menuDescription, reportBrokenSite } of menus) {
+      isMenuItemEnabled(
+        reportBrokenSite,
+        `${menuDescription} option enabled on valid page when preffed on`
+      );
     }
   });
 
   ensureReportBrokenSitePreffedOff();
 
   await BrowserTestUtils.withNewTab(REPORTABLE_PAGE_URL, async function () {
-    for (const [menu] of menus) {
-      await menu.open();
+    await forceMenuItemStateUpdate();
+    for (const { menuDescription, reportBrokenSite } of menus) {
       isMenuItemHidden(
-        menu.reportBrokenSite,
-        `${menu.menuDescription} option hidden again when pref toggled back off`
+        reportBrokenSite,
+        `${menuDescription} option hidden again when pref toggled back off`
       );
-      await menu.close();
     }
   });
 });
