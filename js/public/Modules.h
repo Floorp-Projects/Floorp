55738aee24f9345defe3c4fc2f552f823847691b
Jon Coppeard <jcoppeard@mozilla.com>
2023-12-02 08:49:59 +0000

    Bug 1865410 - Part 2: Removed unused module APIs following previous patch r=allstarschh a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D194767

--- js/public/Modules.h
+++ js/public/Modules.h
@@ -303,11 +303,6 @@ extern JS_PUBLIC_API JSObject* GetModuleEnvironment(
  */
 extern JS_PUBLIC_API void ClearModuleEnvironment(JSObject* moduleObj);
 
-/*
- * Diagnostic assert that the module is has status |Unlinked|.
- */
-extern JS_PUBLIC_API void AssertModuleUnlinked(JSObject* moduleObj);
-
 }  // namespace JS
 
 #endif  // js_Modules_h
