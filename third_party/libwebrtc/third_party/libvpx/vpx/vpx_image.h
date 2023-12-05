d8a031fb023c47a3741059cead92d0704179dcd1
Chun-Min Chang <chun.m.chang@gmail.com>
2023-12-05 20:08:22 +0000

    Bug 1864008 - Move libvpx to libgkcodecs, part 2 r=webrtc-reviewers,padenot,mjf
    This patch addes trampoline headers for libvpx.
    
    To follow the libwebrtc merge procedure, the vpx headers are silently
    replaced with "trampoline" headers, which do nothing but include real
    VPX headers. This makes the libwebrtc-merge process easier without
    worrying headers' paths.
    
    On the other hand, the `rtc_build_libvpx` is set to `true` in
    webrtc.gni, so moz.build file for third_party/libvpx can be generated by
    the gn_processor in the next patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195495

---
+++ third_party/libwebrtc/third_party/libvpx/vpx/vpx_image.h
@@ -0,0 +1,22 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ *
+ * Copyright 2023 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+// This is a trampoline header meant to redirect from where libwebrtc code
+// expects third_party headers to where those headers exist in the Mozilla
+// Firefox repository.
+#include <vpx/vpx_image.h>
