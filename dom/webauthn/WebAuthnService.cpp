5b69a542a97f4af06e09ea9ee3209956e9bc5641
John Schanck <jschanck@mozilla.com>
2023-12-05 18:42:05 +0000

    Bug 1867847 - part 1: simplify internal service selection in WebAuthnService. r=keeler
    Differential Revision: https://phabricator.services.mozilla.com/D195281

--- dom/webauthn/WebAuthnService.cpp
+++ dom/webauthn/WebAuthnService.cpp
@@ -22,11 +22,7 @@ WebAuthnService::MakeCredential(uint64_t aTransactionId,
                                 uint64_t browsingContextId,
                                 nsIWebAuthnRegisterArgs* aArgs,
                                 nsIWebAuthnRegisterPromise* aPromise) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->MakeCredential(aTransactionId, browsingContextId,
-                                        aArgs, aPromise);
-  }
-  return mPlatformService->MakeCredential(aTransactionId, browsingContextId,
+  return DefaultService()->MakeCredential(aTransactionId, browsingContextId,
                                           aArgs, aPromise);
 }
 
@@ -35,15 +31,8 @@ WebAuthnService::GetAssertion(uint64_t aTransactionId,
                               uint64_t browsingContextId,
                               nsIWebAuthnSignArgs* aArgs,
                               nsIWebAuthnSignPromise* aPromise) {
-  nsresult rv;
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    rv = mTestService->GetAssertion(aTransactionId, browsingContextId, aArgs,
-                                    aPromise);
-  } else {
-    rv = mPlatformService->GetAssertion(aTransactionId, browsingContextId,
-                                        aArgs, aPromise);
-  }
-
+  nsresult rv = DefaultService()->GetAssertion(
+      aTransactionId, browsingContextId, aArgs, aPromise);
   if (NS_FAILED(rv)) {
     return rv;
   }
@@ -68,91 +57,57 @@ WebAuthnService::GetAssertion(uint64_t aTransactionId,
 
 NS_IMETHODIMP
 WebAuthnService::GetIsUVPAA(bool* aAvailable) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->GetIsUVPAA(aAvailable);
-  }
-  return mPlatformService->GetIsUVPAA(aAvailable);
+  return DefaultService()->GetIsUVPAA(aAvailable);
 }
 
 NS_IMETHODIMP
 WebAuthnService::HasPendingConditionalGet(uint64_t aBrowsingContextId,
                                           const nsAString& aOrigin,
                                           uint64_t* aRv) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->HasPendingConditionalGet(aBrowsingContextId, aOrigin,
-                                                  aRv);
-  }
-  return mPlatformService->HasPendingConditionalGet(aBrowsingContextId, aOrigin,
+  return DefaultService()->HasPendingConditionalGet(aBrowsingContextId, aOrigin,
                                                     aRv);
 }
 
 NS_IMETHODIMP
 WebAuthnService::GetAutoFillEntries(
     uint64_t aTransactionId, nsTArray<RefPtr<nsIWebAuthnAutoFillEntry>>& aRv) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->GetAutoFillEntries(aTransactionId, aRv);
-  }
-  return mPlatformService->GetAutoFillEntries(aTransactionId, aRv);
+  return DefaultService()->GetAutoFillEntries(aTransactionId, aRv);
 }
 
 NS_IMETHODIMP
 WebAuthnService::SelectAutoFillEntry(uint64_t aTransactionId,
                                      const nsTArray<uint8_t>& aCredentialId) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->SelectAutoFillEntry(aTransactionId, aCredentialId);
-  }
-  return mPlatformService->SelectAutoFillEntry(aTransactionId, aCredentialId);
+  return DefaultService()->SelectAutoFillEntry(aTransactionId, aCredentialId);
 }
 
 NS_IMETHODIMP
 WebAuthnService::ResumeConditionalGet(uint64_t aTransactionId) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->ResumeConditionalGet(aTransactionId);
-  }
-  return mPlatformService->ResumeConditionalGet(aTransactionId);
+  return DefaultService()->ResumeConditionalGet(aTransactionId);
 }
 
 NS_IMETHODIMP
-WebAuthnService::Reset() {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->Reset();
-  }
-  return mPlatformService->Reset();
-}
+WebAuthnService::Reset() { return DefaultService()->Reset(); }
 
 NS_IMETHODIMP
 WebAuthnService::Cancel(uint64_t aTransactionId) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->Cancel(aTransactionId);
-  }
-  return mPlatformService->Cancel(aTransactionId);
+  return DefaultService()->Cancel(aTransactionId);
 }
 
 NS_IMETHODIMP
 WebAuthnService::PinCallback(uint64_t aTransactionId, const nsACString& aPin) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->PinCallback(aTransactionId, aPin);
-  }
-  return mPlatformService->PinCallback(aTransactionId, aPin);
+  return DefaultService()->PinCallback(aTransactionId, aPin);
 }
 
 NS_IMETHODIMP
 WebAuthnService::ResumeMakeCredential(uint64_t aTransactionId,
                                       bool aForceNoneAttestation) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->ResumeMakeCredential(aTransactionId,
-                                              aForceNoneAttestation);
-  }
-  return mPlatformService->ResumeMakeCredential(aTransactionId,
+  return DefaultService()->ResumeMakeCredential(aTransactionId,
                                                 aForceNoneAttestation);
 }
 
 NS_IMETHODIMP
 WebAuthnService::SelectionCallback(uint64_t aTransactionId, uint64_t aIndex) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->SelectionCallback(aTransactionId, aIndex);
-  }
-  return mPlatformService->SelectionCallback(aTransactionId, aIndex);
+  return DefaultService()->SelectionCallback(aTransactionId, aIndex);
 }
 
 NS_IMETHODIMP
@@ -160,22 +115,14 @@ WebAuthnService::AddVirtualAuthenticator(
     const nsACString& protocol, const nsACString& transport,
     bool hasResidentKey, bool hasUserVerification, bool isUserConsenting,
     bool isUserVerified, uint64_t* retval) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->AddVirtualAuthenticator(
-        protocol, transport, hasResidentKey, hasUserVerification,
-        isUserConsenting, isUserVerified, retval);
-  }
-  return mPlatformService->AddVirtualAuthenticator(
+  return DefaultService()->AddVirtualAuthenticator(
       protocol, transport, hasResidentKey, hasUserVerification,
       isUserConsenting, isUserVerified, retval);
 }
 
 NS_IMETHODIMP
 WebAuthnService::RemoveVirtualAuthenticator(uint64_t authenticatorId) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->RemoveVirtualAuthenticator(authenticatorId);
-  }
-  return mPlatformService->RemoveVirtualAuthenticator(authenticatorId);
+  return DefaultService()->RemoveVirtualAuthenticator(authenticatorId);
 }
 
 NS_IMETHODIMP
@@ -186,12 +133,7 @@ WebAuthnService::AddCredential(uint64_t authenticatorId,
                                const nsACString& privateKey,
                                const nsACString& userHandle,
                                uint32_t signCount) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->AddCredential(authenticatorId, credentialId,
-                                       isResidentCredential, rpId, privateKey,
-                                       userHandle, signCount);
-  }
-  return mPlatformService->AddCredential(authenticatorId, credentialId,
+  return DefaultService()->AddCredential(authenticatorId, credentialId,
                                          isResidentCredential, rpId, privateKey,
                                          userHandle, signCount);
 }
@@ -200,52 +142,32 @@ NS_IMETHODIMP
 WebAuthnService::GetCredentials(
     uint64_t authenticatorId,
     nsTArray<RefPtr<nsICredentialParameters>>& retval) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->GetCredentials(authenticatorId, retval);
-  }
-  return mPlatformService->GetCredentials(authenticatorId, retval);
+  return DefaultService()->GetCredentials(authenticatorId, retval);
 }
 
 NS_IMETHODIMP
 WebAuthnService::RemoveCredential(uint64_t authenticatorId,
                                   const nsACString& credentialId) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->RemoveCredential(authenticatorId, credentialId);
-  }
-  return mPlatformService->RemoveCredential(authenticatorId, credentialId);
+  return DefaultService()->RemoveCredential(authenticatorId, credentialId);
 }
 
 NS_IMETHODIMP
 WebAuthnService::RemoveAllCredentials(uint64_t authenticatorId) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->RemoveAllCredentials(authenticatorId);
-  }
-  return mPlatformService->RemoveAllCredentials(authenticatorId);
+  return DefaultService()->RemoveAllCredentials(authenticatorId);
 }
 
 NS_IMETHODIMP
 WebAuthnService::SetUserVerified(uint64_t authenticatorId,
                                  bool isUserVerified) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->SetUserVerified(authenticatorId, isUserVerified);
-  }
-  return mPlatformService->SetUserVerified(authenticatorId, isUserVerified);
+  return DefaultService()->SetUserVerified(authenticatorId, isUserVerified);
 }
 
 NS_IMETHODIMP
-WebAuthnService::Listen() {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->Listen();
-  }
-  return mPlatformService->Listen();
-}
+WebAuthnService::Listen() { return DefaultService()->Listen(); }
 
 NS_IMETHODIMP
 WebAuthnService::RunCommand(const nsACString& cmd) {
-  if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
-    return mTestService->RunCommand(cmd);
-  }
-  return mPlatformService->RunCommand(cmd);
+  return DefaultService()->RunCommand(cmd);
 }
 
 }  // namespace mozilla::dom
--- dom/webauthn/WebAuthnService.h
+++ dom/webauthn/WebAuthnService.h
@@ -30,12 +30,12 @@ class WebAuthnService final : public nsIWebAuthnService {
   NS_DECL_NSIWEBAUTHNSERVICE
 
   WebAuthnService() {
-    Unused << authrs_service_constructor(getter_AddRefs(mTestService));
+    Unused << authrs_service_constructor(getter_AddRefs(mAuthrsService));
 #if defined(XP_WIN)
     if (WinWebAuthnService::AreWebAuthNApisAvailable()) {
       mPlatformService = new WinWebAuthnService();
     } else {
-      mPlatformService = mTestService;
+      mPlatformService = mAuthrsService;
     }
 #elif defined(MOZ_WIDGET_ANDROID)
     mPlatformService = new AndroidWebAuthnService();
@@ -44,18 +44,27 @@ class WebAuthnService final : public nsIWebAuthnService {
       mPlatformService = NewMacOSWebAuthnServiceIfAvailable();
     }
     if (!mPlatformService) {
-      mPlatformService = mTestService;
+      mPlatformService = mAuthrsService;
     }
 #else
-    mPlatformService = mTestService;
+    mPlatformService = mAuthrsService;
 #endif
   }
 
  private:
   ~WebAuthnService() = default;
 
+  nsIWebAuthnService* DefaultService() {
+    if (StaticPrefs::security_webauth_webauthn_enable_softtoken()) {
+      return mAuthrsService;
+    }
+    return mPlatformService;
+  }
+
+  nsIWebAuthnService* AuthrsService() { return mAuthrsService; }
+
+  nsCOMPtr<nsIWebAuthnService> mAuthrsService;
   nsCOMPtr<nsIWebAuthnService> mPlatformService;
-  nsCOMPtr<nsIWebAuthnService> mTestService;
 };
 
 }  // namespace mozilla::dom
