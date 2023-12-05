0bd1f0550731ecee4a13de32630f42cd3a42f2ef
Jed Davis <jld@mozilla.com>
2023-12-05 18:13:46 +0000

    Bug 1508072 - Restrict the sandbox rule for mremap as used for wasm array buffers. r=gcp
    Note that mremap calls from libc realloc implementations are now
    handled in SandboxPolicyCommon (for all process types), while
    ContentSandboxPolicy now handles only the wasm-specific use case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D192029

--- security/sandbox/common/test/SandboxTestingChildTests.h
+++ security/sandbox/common/test/SandboxTestingChildTests.h
@@ -17,6 +17,7 @@
 #    include <linux/mempolicy.h>
 #    include <sched.h>
 #    include <sys/ioctl.h>
+#    include <sys/mman.h>
 #    include <sys/prctl.h>
 #    include <sys/resource.h>
 #    include <sys/socket.h>
@@ -68,6 +69,10 @@ namespace ApplicationServices {
 // Defined in <linux/watch_queue.h> which was added in 5.8
 #  ifndef O_NOTIFICATION_PIPE
 #    define O_NOTIFICATION_PIPE O_EXCL
+#  endif
+// Added in 5.7.
+#  ifndef MREMAP_DONTUNMAP
+#    define MREMAP_DONTUNMAP 4
 #  endif
 #endif
 
@@ -526,6 +531,31 @@ void RunTestsContent(SandboxTestingChild* child) {
     }
   }
 
+  {
+    static constexpr size_t kMapSize = 65536;
+    void* mapping = mmap(nullptr, kMapSize, PROT_READ | PROT_WRITE,
+                         MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    MOZ_ASSERT(mapping != MAP_FAILED);
+    child->ErrnoTest("mremap-zero"_ns, true, [&] {
+      void* rv = mremap(mapping, kMapSize, kMapSize, 0);
+      if (rv == MAP_FAILED) {
+        return -1;
+      }
+      MOZ_ASSERT(rv == mapping);
+      return 0;
+    });
+
+    child->ErrnoValueTest("mremap-forbidden"_ns, ENOSYS, [&] {
+      void* rv = mremap(mapping, kMapSize, kMapSize, MREMAP_DONTUNMAP);
+      // This is an invalid flag combination (DONTUNMAP requires
+      // MAYMOVE) so it will always fail with *something*.
+      MOZ_ASSERT(rv == MAP_FAILED);
+      return -1;
+    });
+
+    munmap(mapping, kMapSize);
+  }
+
 #  endif  // XP_LINUX
 
 #  ifdef XP_MACOSX
@@ -770,6 +800,36 @@ void RunTestsGMPlugin(SandboxTestingChild* child) {
     errno = savedErrno;
     return rv;
   });
+
+  {
+    static constexpr size_t kMapSize = 65536;
+    void* mapping = mmap(nullptr, kMapSize, PROT_READ | PROT_WRITE,
+                         MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    MOZ_ASSERT(mapping != MAP_FAILED);
+
+#    ifndef MOZ_MEMORY
+    child->ErrnoTest("mremap-move"_ns, true, [&] {
+      void* rv = mremap(mapping, kMapSize, kMapSize, MREMAP_MAYMOVE);
+      if (rv == MAP_FAILED) {
+        return -1;
+      }
+      // It *may* move the mapping, but when the size doesn't change
+      // it's not expected to:
+      MOZ_ASSERT(rv == mapping);
+      return 0;
+    });
+#    endif
+
+    child->ErrnoValueTest("mremap-forbidden"_ns, ENOSYS, [&] {
+      void* rv = mremap(mapping, kMapSize, kMapSize, MREMAP_DONTUNMAP);
+      // This is an invalid flag combination (DONTUNMAP requires
+      // MAYMOVE) so it will always fail with *something*.
+      MOZ_ASSERT(rv == MAP_FAILED);
+      return -1;
+    });
+
+    munmap(mapping, kMapSize);
+  }
 
 #  elif XP_MACOSX  // XP_LINUX
   RunMacTestLaunchProcess(child);
