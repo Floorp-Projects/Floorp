f7d3a85f7e79bfc62c9e65db5862b43e17204ad8
Iulian Moraru <imoraru@mozilla.com>
2023-12-05 02:18:39 +0200

    Backed out changeset 07de2cf13c82 (bug 1675646) for causing dt failures on browser_net_http3_request_details.js. CLOSED TREE

--- devtools/client/netmonitor/test/head.js
+++ devtools/client/netmonitor/test/head.js
@@ -1445,12 +1445,6 @@ const clickOnSidebarTab = (doc, name) => {
  *     The netmonitor instance.
  */
 async function addBlockedRequest(pattern, monitor) {
-  const { store, windowRequire } = monitor.panelWin;
-  const Actions = windowRequire("devtools/client/netmonitor/src/actions/index");
-
-  // Open the request blocking panel
-  store.dispatch(Actions.toggleRequestBlockingPanel());
-
   info("Add a blocked request for the URL pattern " + pattern);
   const doc = monitor.panelWin.document;
 
@@ -1468,9 +1462,6 @@ async function addBlockedRequest(pattern, monitor) {
 
   typeInNetmonitor(pattern, monitor);
   EventUtils.synthesizeKey("KEY_Enter");
-
-  // close the request blocking panel
-  store.dispatch(Actions.toggleRequestBlockingPanel());
 }
 
 /**
