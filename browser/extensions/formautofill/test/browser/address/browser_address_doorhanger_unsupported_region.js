4605722246c6b573c25e60a2c4b0d98383a2f72b
Dimi <dlee@mozilla.com>
2023-12-06 11:37:37 +0000

    Bug 1868090 - Do not show the address capture doorhanger when the country field of the address is in an unsupported region r=credential-management-reviewers,issammani
    Differential Revision: https://phabricator.services.mozilla.com/D195415

---
+++ browser/extensions/formautofill/test/browser/address/browser_address_doorhanger_unsupported_region.js
@@ -0,0 +1,85 @@
+"use strict";
+
+const { Region } = ChromeUtils.importESModule(
+  "resource://gre/modules/Region.sys.mjs"
+);
+add_setup(async function () {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["extensions.formautofill.addresses.capture.v2.enabled", true],
+      ["extensions.formautofill.addresses.supported", "detect"],
+      ["extensions.formautofill.addresses.supportedCountries", "US,CA"],
+    ],
+  });
+});
+
+add_task(async function test_save_doorhanger_supported_region() {
+  await BrowserTestUtils.withNewTab(
+    { gBrowser, url: ADDRESS_FORM_URL },
+    async function (browser) {
+      const onPopupShown = waitForPopupShown();
+
+      await focusUpdateSubmitForm(browser, {
+        focusSelector: "#given-name",
+        newValues: {
+          "#given-name": "John Doe",
+          "#organization": "Mozilla",
+          "#street-address": "123 Sesame Street",
+          "#country": "US",
+        },
+      });
+      await onPopupShown;
+    }
+  );
+});
+
+/**
+ * Do not display the address capture doorhanger if the country field of the
+ * submitted form is not on the supported list."
+ */
+add_task(async function test_save_doorhanger_unsupported_region_from_record() {
+  await BrowserTestUtils.withNewTab(
+    { gBrowser, url: ADDRESS_FORM_URL },
+    async function (browser) {
+      await focusUpdateSubmitForm(browser, {
+        focusSelector: "#given-name",
+        newValues: {
+          "#given-name": "John Doe",
+          "#organization": "Mozilla",
+          "#street-address": "123 Sesame Street",
+          "#country": "DE",
+        },
+      });
+
+      await ensureNoDoorhanger(browser);
+    }
+  );
+});
+
+add_task(async function test_save_doorhanger_unsupported_region_from_pref() {
+  const initialHomeRegion = Region._home;
+  const initialCurrentRegion = Region._current;
+
+  const region = "FR";
+  Region._setCurrentRegion(region);
+  Region._setHomeRegion(region);
+
+  await BrowserTestUtils.withNewTab(
+    { gBrowser, url: ADDRESS_FORM_URL },
+    async function (browser) {
+      await focusUpdateSubmitForm(browser, {
+        focusSelector: "#given-name",
+        newValues: {
+          "#given-name": "John Doe",
+          "#organization": "Mozilla",
+          "#street-address": "123 Sesame Street",
+        },
+      });
+
+      await ensureNoDoorhanger(browser);
+    }
+  );
+
+  Region._setCurrentRegion(initialHomeRegion);
+  Region._setHomeRegion(initialCurrentRegion);
+});
