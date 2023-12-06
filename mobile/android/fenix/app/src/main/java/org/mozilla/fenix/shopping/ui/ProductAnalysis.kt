f6f590f1844ee9b29a24965fd38227eafc35a2b6
DreVla <vlad.dreghici@gmail.com>
2023-12-06 15:18:42 +0200

    Bug 1864365 - Change Review Checker adjusted rating description
    Previously, the adjusted rating text was "Unreliable reviews removed",
    which could have cause confusion. This was replaced with
    "Based on reliable reviews".

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/shopping/ui/ProductAnalysis.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/shopping/ui/ProductAnalysis.kt
@@ -247,7 +247,7 @@ private fun AdjustedProductRatingCard(
         }
 
         Text(
-            text = stringResource(R.string.review_quality_check_adjusted_rating_description),
+            text = stringResource(R.string.review_quality_check_adjusted_rating_description_2),
             color = FirefoxTheme.colors.textPrimary,
             style = FirefoxTheme.typography.caption,
         )
