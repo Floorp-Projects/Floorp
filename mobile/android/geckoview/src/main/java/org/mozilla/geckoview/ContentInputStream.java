5454a9516274bc97ad0e8e1a223db905f6b783b8
Calixte <cdenizet@mozilla.com>
2023-12-06 22:20:57 +0000

    Bug 1868382 - Check if a provider belongs to an allow-list if it isn't exported when reading pdfs in GV r=geckoview-reviewers,owlish
    Some Google apps which are files provider are exported or not, hence checking for
    the exported flag is likely too restrictive.
    So for the case where exported flag is false, we check that the authority starts
    with a string belonging to an allow list.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195556

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/ContentInputStream.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/ContentInputStream.java
@@ -28,6 +28,8 @@ import org.mozilla.gecko.annotation.WrapForJNI;
 
   private static final byte[][] HEADERS = {{'%', 'P', 'D', 'F', '-'}};
 
+  private static final String[] ALLOWED_AUTHORITIES = {"com.google"};
+
   private AssetFileDescriptor mFd;
 
   ContentInputStream(final @NonNull String aUri) {
@@ -63,16 +65,6 @@ import org.mozilla.gecko.annotation.WrapForJNI;
     super.close();
   }
 
-  private static boolean wasGrantedPermission(
-      final @NonNull Context aCtx, final @NonNull Uri aUri) {
-    // For reference:
-    //   https://developer.android.com/topic/security/risks/content-resolver#mitigations_2
-    final int pid = Process.myPid();
-    final int uid = Process.myUid();
-    return aCtx.checkUriPermission(aUri, pid, uid, Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        == PackageManager.PERMISSION_GRANTED;
-  }
-
   private static boolean isExported(final @NonNull Context aCtx, final @NonNull Uri aUri) {
     // For reference:
     //   https://developer.android.com/topic/security/risks/content-resolver#mitigations_2
@@ -89,6 +81,40 @@ import org.mozilla.gecko.annotation.WrapForJNI;
     // We check that the provider is exported:
     //   https://developer.android.com/reference/android/content/pm/ComponentInfo?hl=en#exported
     return info.exported;
+  }
+
+  private static boolean isInAllowList(final @NonNull Context aCtx, final @NonNull Uri aUri) {
+    // For reference:
+    //   https://developer.android.com/topic/security/risks/content-resolver#mitigations_2
+    final String authority = aUri.getAuthority();
+    final PackageManager packageManager = aCtx.getPackageManager();
+    if (authority == null || packageManager == null) {
+      return false;
+    }
+    // Make sure we've a provider for this authority.
+    final ProviderInfo info = packageManager.resolveContentProvider(authority, 0);
+    if (info == null) {
+      return false;
+    }
+
+    for (final String allowedAuthority : ALLOWED_AUTHORITIES) {
+      if (info.authority.startsWith(allowedAuthority)) {
+        return true;
+      }
+    }
+
+    Log.d(LOGTAG, "The authority isn't in the allow list: " + info.authority);
+    return false;
+  }
+
+  private static boolean wasGrantedPermission(
+      final @NonNull Context aCtx, final @NonNull Uri aUri) {
+    // For reference:
+    //   https://developer.android.com/topic/security/risks/content-resolver#mitigations_2
+    final int pid = Process.myPid();
+    final int uid = Process.myUid();
+    return aCtx.checkUriPermission(aUri, pid, uid, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        == PackageManager.PERMISSION_GRANTED;
   }
 
   private static boolean belongsToCurrentApplication(
@@ -117,10 +143,12 @@ import org.mozilla.gecko.annotation.WrapForJNI;
     final Context context = GeckoAppShell.getApplicationContext();
 
     try {
-      if ((isExported(context, uri) && wasGrantedPermission(context, uri))
+      if ((isExported(context, uri) || isInAllowList(context, uri))
+              && wasGrantedPermission(context, uri)
           || belongsToCurrentApplication(context, uri)) {
         final ContentResolver cr = context.getContentResolver();
         cr.openAssetFileDescriptor(uri, "r").close();
+        Log.d(LOGTAG, "The uri is readable: " + uri);
         return true;
       }
     } catch (final IOException | SecurityException e) {
@@ -128,6 +156,8 @@ import org.mozilla.gecko.annotation.WrapForJNI;
       // we're in an isolated process.
       Log.e(LOGTAG, "Cannot read the uri: " + uri, e);
     }
+
+    Log.d(LOGTAG, "The uri isn't readable: " + uri);
     return false;
   }
 
