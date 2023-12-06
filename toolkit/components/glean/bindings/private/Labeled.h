aa1ee0ac03dd67a772d55fd1bf8fd128e5bd64c2
Jan-Erik Rediger <jrediger@mozilla.com>
2023-12-06 09:02:13 +0000

    Bug 1865421 - Refactor: Use existing method instead of repeating code. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D194372

--- toolkit/components/glean/bindings/private/Labeled.h
+++ toolkit/components/glean/bindings/private/Labeled.h
@@ -112,11 +112,7 @@ class Labeled<CounterMetric, E> {
     // to the label string and mirrored scalar so we can mirror its operations.
     auto mirrorId = ScalarIdForMetric(mId);
     if (mirrorId) {
-      GetLabeledMirrorLock().apply([&](auto& lock) {
-        auto tuple = std::make_tuple<Telemetry::ScalarID, nsString>(
-            mirrorId.extract(), NS_ConvertUTF8toUTF16(aLabel));
-        lock.ref()->InsertOrUpdate(submetricId, std::move(tuple));
-      });
+      UpdateLabeledMirror(mirrorId.extract(), submetricId, aLabel);
     }
     return CounterMetric(submetricId);
   }
@@ -196,11 +192,7 @@ class Labeled<CounterMetric, DynamicLabel> {
     // to the label string and mirrored scalar so we can mirror its operations.
     auto mirrorId = ScalarIdForMetric(mId);
     if (mirrorId) {
-      GetLabeledMirrorLock().apply([&](auto& lock) {
-        auto tuple = std::make_tuple<Telemetry::ScalarID, nsString>(
-            mirrorId.extract(), NS_ConvertUTF8toUTF16(aLabel));
-        lock.ref()->InsertOrUpdate(submetricId, std::move(tuple));
-      });
+      UpdateLabeledMirror(mirrorId.extract(), submetricId, aLabel);
     }
     return CounterMetric(submetricId);
   }
