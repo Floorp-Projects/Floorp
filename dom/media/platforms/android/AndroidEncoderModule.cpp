68f09fdaaacd50bbb5eec3a99312e1f89f31cdc2
Paul Adenot <paul@paul.cx>
2023-12-07 13:55:05 +0000

    Bug 1867542 - Reflect PEM changes to AndroidEncoderModule. r=jolin
    There contains lots of changes but no functional changes is intended.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195137

--- dom/media/platforms/android/AndroidEncoderModule.cpp
+++ dom/media/platforms/android/AndroidEncoderModule.cpp
@@ -5,8 +5,6 @@
 #include "AndroidEncoderModule.h"
 
 #include "AndroidDataEncoder.h"
-#include "MP4Decoder.h"
-#include "VPXDecoder.h"
 
 #include "mozilla/Logging.h"
 #include "mozilla/java/HardwareCodecCapabilityUtilsWrappers.h"
@@ -18,38 +16,27 @@ extern LazyLogModule sPEMLog;
       sPEMLog, mozilla::LogLevel::Debug, \
       ("AndroidEncoderModule(%p)::%s: " arg, this, __func__, ##__VA_ARGS__))
 
-bool AndroidEncoderModule::SupportsMimeType(const nsACString& aMimeType) const {
-  return (MP4Decoder::IsH264(aMimeType) &&
+bool AndroidEncoderModule::SupportsCodec(CodecType aCodec) const {
+  return (aCodec == CodecType::H264 &&
           java::HardwareCodecCapabilityUtils::HasHWH264(true /* encoder */)) ||
-         (VPXDecoder::IsVP8(aMimeType) &&
+         (aCodec == CodecType::VP8 &&
           java::HardwareCodecCapabilityUtils::HasHWVP8(true /* encoder */)) ||
-         (VPXDecoder::IsVP9(aMimeType) &&
+         (aCodec == CodecType::VP9 &&
           java::HardwareCodecCapabilityUtils::HasHWVP9(true /* encoder */));
 }
 
-already_AddRefed<MediaDataEncoder> AndroidEncoderModule::CreateVideoEncoder(
-    const CreateEncoderParams& aParams, const bool aHardwareNotAllowed) const {
-  // TODO: extend AndroidDataEncoder and Java codec to accept this option.
-  MOZ_ASSERT(!aHardwareNotAllowed);
+bool AndroidEncoderModule::Supports(const EncoderConfig& aConfig) const {
+  // No deep check here for now
+  return SupportsCodec(aConfig.mCodec);
+}
 
-  RefPtr<MediaDataEncoder> encoder;
-  switch (CreateEncoderParams::CodecTypeForMime(aParams.mConfig.mMimeType)) {
-    case MediaDataEncoder::CodecType::H264:
-      return MakeRefPtr<AndroidDataEncoder<MediaDataEncoder::H264Config>>(
-                 aParams.ToH264Config(), aParams.mTaskQueue)
-          .forget();
-    case MediaDataEncoder::CodecType::VP8:
-      return MakeRefPtr<AndroidDataEncoder<MediaDataEncoder::VP8Config>>(
-                 aParams.ToVP8Config(), aParams.mTaskQueue)
-          .forget();
-    case MediaDataEncoder::CodecType::VP9:
-      return MakeRefPtr<AndroidDataEncoder<MediaDataEncoder::VP9Config>>(
-                 aParams.ToVP9Config(), aParams.mTaskQueue)
-          .forget();
-    default:
-      AND_PEM_LOG("Unsupported MIME type:%s", aParams.mConfig.mMimeType.get());
-      return nullptr;
+already_AddRefed<MediaDataEncoder> AndroidEncoderModule::CreateVideoEncoder(
+    const EncoderConfig& aConfig, const RefPtr<TaskQueue>& aTaskQueue) const {
+  if (!Supports(aConfig)) {
+    AND_PEM_LOG("Unsupported codec type: %d", static_cast<int>(aConfig.mCodec));
+    return nullptr;
   }
+  return MakeRefPtr<AndroidDataEncoder>(aConfig, aTaskQueue).forget();
 }
 
 }  // namespace mozilla
