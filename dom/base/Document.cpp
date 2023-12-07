c50dd48fb59e857f9970e8192f341ac6cef34769
Adam Vandolder <avandolder@mozilla.com>
2023-12-07 21:27:10 +0000

    Bug 1712140 - Part 3: Add support for parsing and building Declarative ShadowDOMs. r=dom-core,webidl,saschanaz,hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D193675

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -1394,6 +1394,7 @@ Document::Document(const char* aContentType)
       mHasUserInteractionTimerScheduled(false),
       mShouldResistFingerprinting(false),
       mCloningForSVGUse(false),
+      mAllowDeclarativeShadowRoots(false),
       mXMLDeclarationBits(0),
       mOnloadBlockCount(0),
       mWriteLevel(0),
@@ -18964,4 +18965,14 @@ void Document::UpdateHiddenByContentVisibilityForAnimations() {
     timeline->UpdateHiddenByContentVisibility();
   }
 }
+
+void Document::SetAllowDeclarativeShadowRoots(
+    bool aAllowDeclarativeShadowRoots) {
+  mAllowDeclarativeShadowRoots = aAllowDeclarativeShadowRoots;
+}
+
+bool Document::AllowsDeclarativeShadowRoots() const {
+  return mAllowDeclarativeShadowRoots;
+}
+
 }  // namespace mozilla::dom
