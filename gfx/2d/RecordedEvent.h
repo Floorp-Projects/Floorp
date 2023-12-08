a44f01de92dd29bb0518db681ddadc6e794f6906
Narcis Beleuzu <nbeleuzu@mozilla.com>
2023-12-08 13:18:52 +0200

    Backed out changeset 71b6060b6015 (bug 1863914) for causing Bug 1868934. a=backout

--- gfx/2d/RecordedEvent.h
+++ gfx/2d/RecordedEvent.h
@@ -214,7 +214,7 @@ struct SizeCollector {
 };
 
 struct MemWriter {
-  constexpr explicit MemWriter(char* aPtr) : mPtr(aPtr) {}
+  explicit MemWriter(char* aPtr) : mPtr(aPtr) {}
   void write(const char* aData, size_t aSize) {
     memcpy(mPtr, aData, aSize);
     mPtr += aSize;
@@ -222,30 +222,13 @@ struct MemWriter {
   char* mPtr;
 };
 
-class ContiguousBuffer {
- public:
-  ContiguousBuffer(char* aStart, size_t aSize)
-      : mWriter(aStart), mEnd(aStart + aSize) {}
-
-  constexpr MOZ_IMPLICIT ContiguousBuffer(std::nullptr_t) : mWriter(nullptr) {}
-
-  MemWriter& Writer() { return mWriter; }
-
-  size_t SizeRemaining() { return mWriter.mPtr ? mEnd - mWriter.mPtr : 0; }
-
-  bool IsValid() { return !!mWriter.mPtr; }
-
- private:
-  MemWriter mWriter;
-  char* mEnd = nullptr;
-};
-
-// Allows a derived class to provide guaranteed contiguous buffer.
-class ContiguousBufferStream {
+// This is a simple interface for an EventRingBuffer, so we can use it in the
+// RecordedEvent reading and writing machinery.
+class EventRingBuffer {
  public:
   /**
-   * Templated RecordEvent function so that we can record into the buffer
-   * quickly using MemWriter.
+   * Templated RecordEvent function so that when we have enough contiguous
+   * space we can record into the buffer quickly using MemWriter.
    *
    * @param aRecordedEvent the event to record
    */
@@ -254,25 +237,56 @@ class ContiguousBufferStream {
     SizeCollector size;
     WriteElement(size, aRecordedEvent->GetType());
     aRecordedEvent->Record(size);
-    auto& buffer = GetContiguousBuffer(size.mTotalSize);
-    if (!buffer.IsValid()) {
-      return;
+    if (size.mTotalSize > mAvailable) {
+      WaitForAndRecalculateAvailableSpace();
     }
-
-    MOZ_ASSERT(size.mTotalSize <= buffer.SizeRemaining());
-
-    WriteElement(buffer.Writer(), aRecordedEvent->GetType());
-    aRecordedEvent->Record(buffer.Writer());
-    IncrementEventCount();
+    if (size.mTotalSize <= mAvailable) {
+      MemWriter writer(mBufPos);
+      WriteElement(writer, aRecordedEvent->GetType());
+      aRecordedEvent->Record(writer);
+      UpdateWriteTotalsBy(size.mTotalSize);
+    } else {
+      WriteElement(*this, aRecordedEvent->GetType());
+      aRecordedEvent->Record(*this);
+    }
   }
+
+  /**
+   * Simple write function required by WriteElement.
+   *
+   * @param aData the data to be written to the buffer
+   * @param aSize the number of chars to write
+   */
+  virtual void write(const char* const aData, const size_t aSize) = 0;
+
+  /**
+   * Simple read function required by ReadElement.
+   *
+   * @param aOut the pointer to read into
+   * @param aSize the number of chars to read
+   */
+  virtual void read(char* const aOut, const size_t aSize) = 0;
+
+  virtual bool good() const = 0;
+
+  virtual void SetIsBad() = 0;
 
  protected:
   /**
-   * Provide a contiguous buffer with at least aSize remaining.
+   * Wait until space is available for writing and then set mBufPos and
+   * mAvailable.
    */
-  virtual ContiguousBuffer& GetContiguousBuffer(size_t aSize) = 0;
+  virtual bool WaitForAndRecalculateAvailableSpace() = 0;
 
-  virtual void IncrementEventCount() = 0;
+  /**
+   * Update write count, mBufPos and mAvailable.
+   *
+   * @param aCount number of bytes written
+   */
+  virtual void UpdateWriteTotalsBy(uint32_t aCount) = 0;
+
+  char* mBufPos = nullptr;
+  uint32_t mAvailable = 0;
 };
 
 struct MemStream {
@@ -416,7 +430,7 @@ class RecordedEvent {
 
   virtual void RecordToStream(std::ostream& aStream) const = 0;
   virtual void RecordToStream(EventStream& aStream) const = 0;
-  virtual void RecordToStream(ContiguousBufferStream& aStream) const = 0;
+  virtual void RecordToStream(EventRingBuffer& aStream) const = 0;
   virtual void RecordToStream(MemStream& aStream) const = 0;
 
   virtual void OutputSimpleEventInfo(std::stringstream& aStringStream) const {}
@@ -445,6 +459,9 @@ class RecordedEvent {
                           const std::function<bool(RecordedEvent*)>& aAction);
   static bool DoWithEventFromStream(
       EventStream& aStream, EventType aType,
+      const std::function<bool(RecordedEvent*)>& aAction);
+  static bool DoWithEventFromStream(
+      EventRingBuffer& aStream, EventType aType,
       const std::function<bool(RecordedEvent*)>& aAction);
 
   EventType GetType() const { return (EventType)mType; }
@@ -478,7 +495,7 @@ class RecordedEventDerived : public RecordedEvent {
     WriteElement(aStream, this->mType);
     static_cast<const Derived*>(this)->Record(aStream);
   }
-  void RecordToStream(ContiguousBufferStream& aStream) const final {
+  void RecordToStream(EventRingBuffer& aStream) const final {
     aStream.RecordEvent(static_cast<const Derived*>(this));
   }
   void RecordToStream(MemStream& aStream) const override {
