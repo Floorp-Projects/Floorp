a88286e935b8e455472b98fe78f000abc865471e
Jan Varga <jvarga@mozilla.com>
2023-12-08 05:55:05 +0000

    Bug 1808294 - Add DirectoryLock::Acquired method; r=dom-storage-reviewers,jari
    Differential Revision: https://phabricator.services.mozilla.com/D192444

--- dom/quota/DirectoryLock.h
+++ dom/quota/DirectoryLock.h
@@ -26,6 +26,8 @@ class NS_NO_VTABLE DirectoryLock {
 
   virtual int64_t Id() const = 0;
 
+  virtual bool Acquired() const = 0;
+
   // XXX This method is now deprecated, use the one which returns the
   // `BoolPromise`
   virtual void Acquire(RefPtr<OpenDirectoryListener> aOpenListener) = 0;
