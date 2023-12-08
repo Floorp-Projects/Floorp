d0c34b17b6ce969331e699b681da55198b227a5d
Stanca Serban <sstanca@mozilla.com>
2023-12-08 16:52:18 +0200

    Backed out changeset ad72b273da7f (bug 1856732) for causing bc failures in browser_canvas_randomization.js. CLOSED TREE

--- dom/canvas/CanvasUtils.cpp
+++ dom/canvas/CanvasUtils.cpp
@@ -48,16 +48,6 @@
 
 using namespace mozilla::gfx;
 
-static bool IsUnrestrictedPrincipal(nsIPrincipal& aPrincipal) {
-  // The system principal can always extract canvas data.
-  if (aPrincipal.IsSystemPrincipal()) {
-    return true;
-  }
-
-  // Allow extension principals.
-  return aPrincipal.GetIsAddonOrExpandedAddonPrincipal();
-}
-
 namespace mozilla::CanvasUtils {
 
 bool IsImageExtractionAllowed(dom::Document* aDocument, JSContext* aCx,
@@ -113,8 +103,14 @@ bool IsImageExtractionAllowed(dom::Document* aDocument, JSContext* aCx,
     return false;
   }
 
-  // The system and extension principals can always extract canvas data.
-  if (IsUnrestrictedPrincipal(aPrincipal)) {
+  // The system principal can always extract canvas data.
+  if (aPrincipal.IsSystemPrincipal()) {
+    return true;
+  }
+
+  // Allow extension principals.
+  auto* principal = BasePrincipal::Cast(&aPrincipal);
+  if (principal->AddonPolicy() || principal->ContentScriptAddonPolicy()) {
     return true;
   }
 
@@ -180,7 +176,7 @@ bool IsImageExtractionAllowed(dom::Document* aDocument, JSContext* aCx,
   // Either permit or block extraction if a stored permission setting exists.
   uint32_t permission;
   rv = permissionManager->TestPermissionFromPrincipal(
-      &aPrincipal, PERMISSION_CANVAS_EXTRACT_DATA, &permission);
+      principal, PERMISSION_CANVAS_EXTRACT_DATA, &permission);
   NS_ENSURE_SUCCESS(rv, false);
   switch (permission) {
     case nsIPermissionManager::ALLOW_ACTION:
@@ -242,7 +238,7 @@ bool IsImageExtractionAllowed(dom::Document* aDocument, JSContext* aCx,
   // maybe not
   nsPIDOMWindowOuter* win = aDocument->GetWindow();
   nsAutoCString origin;
-  rv = aPrincipal.GetOrigin(origin);
+  rv = principal->GetOrigin(origin);
   NS_ENSURE_SUCCESS(rv, false);
 
   if (XRE_IsContentProcess()) {
@@ -264,45 +260,6 @@ bool IsImageExtractionAllowed(dom::Document* aDocument, JSContext* aCx,
 
   // We don't extract the image for now -- user may override at prompt.
   return false;
-}
-
-ImageExtraction ImageExtractionResult(dom::HTMLCanvasElement* aCanvasElement,
-                                      JSContext* aCx,
-                                      nsIPrincipal& aPrincipal) {
-  if (IsUnrestrictedPrincipal(aPrincipal)) {
-    return ImageExtraction::Unrestricted;
-  }
-
-  nsCOMPtr<dom::Document> ownerDoc = aCanvasElement->OwnerDoc();
-  if (!IsImageExtractionAllowed(ownerDoc, aCx, aPrincipal)) {
-    return ImageExtraction::Placeholder;
-  }
-
-  if (ownerDoc->ShouldResistFingerprinting(RFPTarget::CanvasRandomization)) {
-    return ImageExtraction::Randomize;
-  }
-
-  return ImageExtraction::Unrestricted;
-}
-
-ImageExtraction ImageExtractionResult(dom::OffscreenCanvas* aOffscreenCanvas,
-                                      JSContext* aCx,
-                                      nsIPrincipal& aPrincipal) {
-  if (IsUnrestrictedPrincipal(aPrincipal)) {
-    return ImageExtraction::Unrestricted;
-  }
-
-  if (aOffscreenCanvas->ShouldResistFingerprinting(
-          RFPTarget::CanvasImageExtractionPrompt)) {
-    return ImageExtraction::Placeholder;
-  }
-
-  if (aOffscreenCanvas->ShouldResistFingerprinting(
-          RFPTarget::CanvasRandomization)) {
-    return ImageExtraction::Randomize;
-  }
-
-  return ImageExtraction::Unrestricted;
 }
 
 bool GetCanvasContextType(const nsAString& str,
