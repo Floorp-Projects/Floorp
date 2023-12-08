8d6339b4f6a5557ce195828143f0de7d9433e849
Sergey Galich <sgalich@mozilla.com>
2023-12-08 00:07:33 +0000

    Bug 1868630 - Removing custom lint rules for Form Autofill, satchel & Login Manager r=mtigley,credential-management-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D195694

--- toolkit/components/satchel/test/unit/head_satchel.js
+++ toolkit/components/satchel/test/unit/head_satchel.js
@@ -2,13 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* eslint
-  "no-unused-vars": ["error", {
-    vars: "local",
-    args: "none",
-  }],
-*/
-
 const CURRENT_SCHEMA = 5;
 const PR_HOURS = 60 * 60 * 1000000;
 
