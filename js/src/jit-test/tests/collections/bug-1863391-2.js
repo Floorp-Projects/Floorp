6c14142e39743c5c8958619ffde2569af32a5f05
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2023-12-11 14:32:45 +0000

    Bug 1869318 - Update test in bug-1863391. r=jonco
    Add '--enable-symbols-as-weakmap-keys' pref, and skip if it's a
    release_or_beta build.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196053

--- js/src/jit-test/tests/collections/bug-1863391-2.js
+++ js/src/jit-test/tests/collections/bug-1863391-2.js
@@ -1,4 +1,4 @@
-// |jit-test| skip-if: helperThreadCount() === 0
+// |jit-test| --enable-symbols-as-weakmap-keys; skip-if: helperThreadCount() === 0 || getBuildConfiguration("release_or_beta")
 evalInWorker(`
   a = new WeakMap
   b = Symbol.hasInstance;
