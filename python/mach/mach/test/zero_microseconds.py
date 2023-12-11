b34126488159f8842ed0d37e26ba1d03e67204dc
ogiorgis <olivier.giorgis@logilab.fr>
2023-12-11 10:09:09 +0000

    Bug 1749473 - fix autodoc import warning r=sylvestre DONTBUILD
    NameError: name 'self' is not defined
    
    Depends on D196011
    
    Differential Revision: https://phabricator.services.mozilla.com/D196012

--- python/mach/mach/test/zero_microseconds.py
+++ python/mach/mach/test/zero_microseconds.py
@@ -1,6 +1,10 @@
+# Make flake8, ruff and autodoc happy
+if "self" not in globals():
+    self = None
+
 # This code is loaded via `mach python --exec-file`, so it runs in the scope of
 # the `mach python` command.
-old = self._mach_context.post_dispatch_handler  # noqa: F821
+old = self._mach_context.post_dispatch_handler
 
 
 def handler(context, handler, instance, result, start_time, end_time, depth, args):
@@ -9,4 +13,4 @@ def handler(context, handler, instance, result, start_time, end_time, depth, arg
     old(context, handler, instance, result, int(start_time), end_time, depth, args)
 
 
-self._mach_context.post_dispatch_handler = handler  # noqa: F821
+self._mach_context.post_dispatch_handler = handler
