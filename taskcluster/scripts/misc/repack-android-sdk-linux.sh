dd247c2aa7ef4af14414d9c32f321595e99c7f6d
Geoff Brown <gbrown@mozilla.com>
2023-12-12 00:12:48 +0000

    Bug 1869407 - Pin android emulator in android-sdk-linux fetch r=bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D196104

--- taskcluster/scripts/misc/repack-android-sdk-linux.sh
+++ taskcluster/scripts/misc/repack-android-sdk-linux.sh
@@ -10,6 +10,13 @@ mkdir -p $UPLOAD_DIR
 cd $GECKO_PATH
 ./mach python python/mozboot/mozboot/android.py --artifact-mode --no-interactive --list-packages
 
+# Bug 1869407: override emulator to a known working version
+curl -L http://dl.google.com/android/repository/emulator-linux_x64-10696886.zip > /tmp/emulator.zip
+cd /builds/worker/.mozbuild/android-sdk-linux
+rm -rf emulator
+unzip /tmp/emulator.zip
+cd $GECKO_PATH
+
 tar cavf $UPLOAD_DIR/android-sdk-linux.tar.zst -C /builds/worker/.mozbuild android-sdk-linux bundletool.jar
 
 ls -al $UPLOAD_DIR
