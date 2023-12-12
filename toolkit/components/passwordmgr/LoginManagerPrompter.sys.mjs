5c3aeae79b61e3946efba7182816bf261b286b10
Johannes J. Schmidt <joschmidt@mozilla.com>
2023-12-12 11:00:03 +0000

    Bug 1867011 - Update Strings for about:logins - Remove passwords - r=jneuberger,fluent-reviewers,kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D194980

--- toolkit/components/passwordmgr/LoginManagerPrompter.sys.mjs
+++ toolkit/components/passwordmgr/LoginManagerPrompter.sys.mjs
@@ -617,7 +617,7 @@ export class LoginManagerPrompter {
           lazy.log.debug("Showing the ConfirmationHint");
           anchor.ownerGlobal.ConfirmationHint.show(
             anchor,
-            "confirmation-hint-login-removed"
+            "confirmation-hint-password-removed"
           );
         },
       });
