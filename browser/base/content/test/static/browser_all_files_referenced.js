9f2d1909c49597e23e1c14a0676b29527f4dd357
jneuberger <jneuberger@mozilla.com>
2023-12-13 23:02:42 +0000

    Bug 1867022 - Update strings of os prompt dialog before using payment method r=credential-management-reviewers,fluent-reviewers,dimi,flod
    Differential Revision: https://phabricator.services.mozilla.com/D195659

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -44,6 +44,8 @@ var gExceptionPaths = [
   // Exclude the form autofill path that has been moved out of the extensions to
   // toolkit, see bug 1691821.
   "resource://gre-resources/autofill/",
+  // Localization file added programatically in FormAutofillSection.sys.mjs
+  "resource://gre/localization/en-US/toolkit/formautofill",
 
   // Exclude all search-extensions because they aren't referenced by filename
   "resource://search-extensions/",
