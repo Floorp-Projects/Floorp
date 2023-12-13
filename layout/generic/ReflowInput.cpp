ddf5d31d312e043828850ee02eab5acbd9ff63c7
Ting-Yu Lin <tlin@mozilla.com>
2023-12-13 22:50:36 +0000

    Bug 1869722 Part 1 - Cleanup after fixing tables as flex items. r=dholbert
    Flex containers do not need the `alignCB` tweak for table frames in
    `ReflowInput::InitConstraints`, so I remove the comment, and improve it so that
    we don't need to call `alignCB->GetParent()` multiple times.
    
    This patch doesn't change behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196257

--- layout/generic/ReflowInput.cpp
+++ layout/generic/ReflowInput.cpp
@@ -2365,11 +2365,10 @@ void ReflowInput::InitConstraints(
       }
 
       nsIFrame* alignCB = mFrame->GetParent();
-      if (alignCB->IsTableWrapperFrame() && alignCB->GetParent()) {
-        // XXX grid-specific for now; maybe remove this check after we address
-        // bug 799725
-        if (alignCB->GetParent()->IsGridContainerFrame()) {
-          alignCB = alignCB->GetParent();
+      if (alignCB->IsTableWrapperFrame()) {
+        nsIFrame* alignCBParent = alignCB->GetParent();
+        if (alignCBParent && alignCBParent->IsGridContainerFrame()) {
+          alignCB = alignCBParent;
         }
       }
       if (alignCB->IsGridContainerFrame()) {
