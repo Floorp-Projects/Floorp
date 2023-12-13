858c8e5a82ef051047193f9a6cb339ca4554da19
Masayuki Nakano <masayuki@d-toybox.com>
2023-12-13 07:13:03 +0000

    Bug 1868283 - part 2: Make `browser_tooltip.js` synthesize mouse events via the parent process r=daisuke,devtools-reviewers,nchevobbe
    The popup manager is in the parent process and the parent process may store
    the last mouse cursor position which is set by a preceding test, but the
    test synthesizes mouse events only in the content process.  Therefore, the
    parent may have unexpected state.
    
    For solving these issues, this patch makes the test synthesize the events via
    the parent process with enabling the async event dispatcher.
    
    Then, new issue appeared. When I run this test in the verify mode, it
    high-frequently failed with no mouse event logs.  Therefore, this patch makes
    the content process waits first `mousemove` outside the target before starting
    the test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195637

--- devtools/client/responsive/test/browser/browser_tooltip.js
+++ devtools/client/responsive/test/browser/browser_tooltip.js
@@ -9,13 +9,10 @@ const TEST_URL = `data:text/html;charset=utf-8,${TEST_CONTENT}`;
 // Test for the tooltip coordinate on the browsing document in RDM.
 
 addRDMTask(TEST_URL, async ({ ui }) => {
-  // On ubuntu1804, the test fails if the real mouse cursor is on the test document.
-  // See Bug 1600183
-  info("Disable non test mouse event");
-  window.windowUtils.disableNonTestMouseEvents(true);
-  registerCleanupFunction(() => {
-    window.windowUtils.disableNonTestMouseEvents(false);
-  });
+  // Make this synthesize mouse events via the parent process because some
+  // code may cache the mouse cursor position like PresShell.  That may prevent
+  // unexpected DOM events coming from the parent process.
+  await pushPref("test.events.async.enabled", true);
 
   info("Create a promise which waits until the tooltip will be shown");
   const tooltip = ui.browserWindow.gBrowser.ownerDocument.getElementById(
@@ -27,6 +24,55 @@ addRDMTask(TEST_URL, async ({ ui }) => {
   info("Show a tooltip");
   await spawnViewportTask(ui, {}, async () => {
     const target = content.document.querySelector("h1");
+
+    // First, make sure to move mouse cursor outside the target.
+    info("Waiting for initial mousemove");
+    {
+      // FYI: Some `mousemove` events may not be sent to the content process
+      // until all preparations done in the main process and the content
+      // process. Therefore, we need to synthesize `mousemove`s until we
+      // get it in the remote process at least once.  Therefore, we cannot
+      // use a promise to wait a `mousemove` here and we need to use the while
+      // loop below.
+      let mouseMoveFired = false;
+      content.document.addEventListener(
+        "mousemove",
+        event => {
+          isnot(
+            event.target,
+            target,
+            "The first mousemove should be fired outside the target"
+          );
+          mouseMoveFired = true;
+        },
+        {
+          once: true,
+        }
+      );
+      while (!mouseMoveFired) {
+        await EventUtils.synthesizeMouse(
+          target,
+          -2,
+          -2,
+          { type: "mousemove" },
+          content
+        );
+        await EventUtils.synthesizeMouse(
+          target,
+          -1,
+          -1,
+          { type: "mousemove" },
+          content
+        );
+        // Wait for the tooltip for the target is hidden even if it was visible.
+        await new Promise(resolve =>
+          content.window.requestAnimationFrame(() =>
+            content.window.requestAnimationFrame(resolve)
+          )
+        );
+      }
+    }
+
     const eventLogger = event =>
       info(
         `${event.type}: path=[${event.composedPath()}], outerHTML=${
@@ -36,14 +82,6 @@ addRDMTask(TEST_URL, async ({ ui }) => {
     target.addEventListener("mouseover", eventLogger);
     target.addEventListener("mouseout", eventLogger);
     content.document.addEventListener("mousemove", eventLogger);
-    // First, make sure to move mouse cursor outside the target.
-    await EventUtils.synthesizeMouse(
-      target,
-      -1,
-      -1,
-      { type: "mousemove", isSynthesized: false },
-      content
-    );
     // Then, move cursor over the target.
     await EventUtils.synthesizeMouse(
       target,
