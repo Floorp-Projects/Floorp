a888f07e0112c2809a043d8c8da256b98d126809
Emilio Cobos Álvarez <emilio@crisal.io>
2023-12-13 12:53:58 +0000

    Bug 1869299 - More modern Linux theme colors and looks. r=dao,stransky,desktop-theme-reviewers,rmader
    Override Adwaita / Yaru colors with colors from the libadwaita
    documentation.
    
    Do a couple minor tweaks to the urlbar too to match both our default
    themes in other platforms and Adwaita.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196153

--- widget/nsXPLookAndFeel.cpp
+++ widget/nsXPLookAndFeel.cpp
@@ -500,6 +500,8 @@ static constexpr struct {
     // Affects zoom settings which includes text and full zoom.
     {"browser.display.os-zoom-behavior"_ns,
      widget::ThemeChangeKind::StyleAndLayout},
+    // This affects system colors on Linux.
+    {"widget.gtk.libadwaita-colors.enabled"_ns, widget::ThemeChangeKind::Style},
     // This affects not only the media query, but also the native theme, so we
     // need to re-layout.
     {"browser.theme.toolbar-theme"_ns, widget::ThemeChangeKind::AllBits},
