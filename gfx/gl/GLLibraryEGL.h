6216f87a9f4a3ff041ff899579c0862dc2036948
Stanca Serban <sstanca@mozilla.com>
2023-12-13 12:29:58 +0200

    Backed out changeset 5105ca2d1171 (bug 1868825) for causing bp-nu bustages in GLLibraryEGL.h. CLOSED TREE

--- gfx/gl/GLLibraryEGL.h
+++ gfx/gl/GLLibraryEGL.h
@@ -28,7 +28,6 @@
 
 #ifdef MOZ_WIDGET_ANDROID
 #  include "mozilla/ProfilerLabels.h"
-#  include "AndroidBuild.h"
 #endif
 
 #if defined(MOZ_X11)
@@ -691,21 +690,6 @@ class GLLibraryEGL final {
   } mSymbols = {};
 };
 
-static bool ShouldLeakEglDisplay() {
-  // We are seeing crashes in eglTerminate on the Samsung S22 family of devices
-  // running Android 14, so we leak the EGLDisplay rather than call
-  // eglTerminate.
-#ifdef MOZ_WIDGET_ANDROID
-  if (jni::GetAPIVersion() >= 34) {
-    const auto board = java::sdk::Build::BOARD()->ToString();
-    if (board.EqualsASCII("s5e9925")) {
-      return true;
-    }
-  }
-#endif
-  return false;
-}
-
 class EglDisplay final {
  public:
   const RefPtr<GLLibraryEGL> mLib;
@@ -755,13 +739,7 @@ class EglDisplay final {
 
   // -
 
-  EGLBoolean fTerminate() {
-    static const bool shouldLeak = ShouldLeakEglDisplay();
-    if (shouldLeak) {
-      return LOCAL_EGL_TRUE;
-    }
-    return mLib->fTerminate(mDisplay);
-  }
+  EGLBoolean fTerminate() { return mLib->fTerminate(mDisplay); }
 
   EGLBoolean fMakeCurrent(EGLSurface draw, EGLSurface read,
                           EGLContext ctx) const {
