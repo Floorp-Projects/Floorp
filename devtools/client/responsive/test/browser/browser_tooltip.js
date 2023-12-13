ea23cadd0ed2ec1eeb67aae06f963aff0f17d55a
Masayuki Nakano <masayuki@d-toybox.com>
2023-12-13 07:13:03 +0000

    Bug 1868283 - part 1: Make `browser_tooltip.js` do not synthesize `mouseover` directly r=daisuke,devtools-reviewers,nchevobbe
    `mouseover` should be fired by `EventStateManager` when it gets `mousemove`
    events at that time, `PresShell` and `EventStateManager` update their member
    to dispatch `mouseout` etc later correctly. So, the test should synthesize
    `mousemove` instead.
    
    Additionally, for making it easier to debug, this patch adds event logger
    too. Number of logging events are just a few so that it's not problem from
    the log size point of view.
    
    Finally, this adds a clean up code to hide the tooltip which was shown by
    this test.  That may help to make it stable when continuously run the test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195636

--- devtools/client/responsive/test/browser/browser_tooltip.js
+++ devtools/client/responsive/test/browser/browser_tooltip.js
@@ -3,7 +3,7 @@
 
 "use strict";
 
-const TEST_CONTENT = `<h1 title="test title">test h1</h1>`;
+const TEST_CONTENT = `<h1 title="test title" style="margin: 5px">test h1</h1>`;
 const TEST_URL = `data:text/html;charset=utf-8,${TEST_CONTENT}`;
 
 // Test for the tooltip coordinate on the browsing document in RDM.
@@ -22,15 +22,34 @@ addRDMTask(TEST_URL, async ({ ui }) => {
     "remoteBrowserTooltip"
   );
   const onTooltipShown = BrowserTestUtils.waitForEvent(tooltip, "popupshown");
+  const onTooltipHidden = BrowserTestUtils.waitForEvent(tooltip, "popuphidden");
 
   info("Show a tooltip");
   await spawnViewportTask(ui, {}, async () => {
     const target = content.document.querySelector("h1");
+    const eventLogger = event =>
+      info(
+        `${event.type}: path=[${event.composedPath()}], outerHTML=${
+          event.target.outerHTML
+        }, relatedTarget=${event.relatedTarget?.outerHTML}`
+      );
+    target.addEventListener("mouseover", eventLogger);
+    target.addEventListener("mouseout", eventLogger);
+    content.document.addEventListener("mousemove", eventLogger);
+    // First, make sure to move mouse cursor outside the target.
+    await EventUtils.synthesizeMouse(
+      target,
+      -1,
+      -1,
+      { type: "mousemove", isSynthesized: false },
+      content
+    );
+    // Then, move cursor over the target.
     await EventUtils.synthesizeMouse(
       target,
       1,
       1,
-      { type: "mouseover", isSynthesized: false },
+      { type: "mousemove", isSynthesized: false },
       content
     );
     await EventUtils.synthesizeMouse(
@@ -54,4 +73,19 @@ addRDMTask(TEST_URL, async ({ ui }) => {
 
   info("Test the X coordinate of the tooltip");
   isnot(tooltip.screenX, 0, "The X coordinate of tooltip should not be 0");
+
+  // Finally, clean up the tooltip if we're running in the verify mode.
+  info("Wait for hiding the tooltip");
+  await spawnViewportTask(ui, {}, async () => {
+    info("Cleaning up the tooltip with moving the cursor");
+    const target = content.document.querySelector("h1");
+    await EventUtils.synthesizeMouse(
+      target,
+      -1,
+      -1,
+      { type: "mousemove", isSynthesized: false },
+      content
+    );
+  });
+  await onTooltipHidden;
 });
