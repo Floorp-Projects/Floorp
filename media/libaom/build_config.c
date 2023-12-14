ffd78038690ac5bad9a7adcf7f0c9614739df8c9
Paul Adenot <paul@paul.cx>
2023-12-14 09:12:34 +0000

    Bug 1869332 - Write a placeholder implementation for getting the buildconfig. r=chunmin
    This is expected to be present in ffmpeg, but is only for displaying things on
    the command-line, we don't need a "real" implementation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196062

---
+++ media/libaom/build_config.c
@@ -0,0 +1,8 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Placeholder implementation -- need to be present but not used
+const char *aom_codec_build_config(void) {return "It's a good config.";}
