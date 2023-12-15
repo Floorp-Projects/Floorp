d634e55f4f62201c5da2fb4de6789ef1ed9afff9
AndiAJ <andiaj@users.noreply.github.com>
2023-12-15 15:11:00 +0200

    Bug 1814032 - Re-enable verifyMultipleAddressesSelectionTest UI test

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/AddressAutofillTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/AddressAutofillTest.kt
@@ -7,7 +7,6 @@ package org.mozilla.fenix.ui
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.customannotations.SmokeTest
@@ -265,7 +264,6 @@ class AddressAutofillTest {
     }
 
     // TestRail link: https://testrail.stage.mozaws.net/index.php?/cases/view/1836849
-    @Ignore("Failing, see: https://bugzilla.mozilla.org/show_bug.cgi?id=1814032")
     @Test
     fun verifyMultipleAddressesSelectionTest() {
         val addressFormPage =
