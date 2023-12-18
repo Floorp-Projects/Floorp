53e55e93de91ea320204b9286db914e05385a524
Ting-Yu Lin <tlin@mozilla.com>
2023-12-18 22:12:43 +0000

    Bug 1350037 Part 3 - Prevent table caption from honoring justify-*/align-* when table is a grid item. r=dholbert
    The old code used to apply ShrinkWrap flag to both the inner table and the
    caption if the table wrapper is a stretched grid item. However, per analysis in
    bug 1350037 comment 4, when we reflow table captions, we'd like it *not* to
    shrink-wrap, but to stretch its inline-size to at least as wide as the inner
    table frame [1].
    
    This patch moves the logic that computes whether a table is a stretched grid
    item from `ReflowInput` to `nsTableWrapperFrame`, and apply the `ShrinkWrap`
    flag to inner table if needed.
    
    [1] https://searchfox.org/mozilla-central/rev/54d6d78c3b33c92fd9eef9c0555a153bf8e4f2b4/layout/tables/nsTableWrapperFrame.cpp#760-763
    
    Differential Revision: https://phabricator.services.mozilla.com/D196705

--- layout/tables/nsTableWrapperFrame.h
+++ layout/tables/nsTableWrapperFrame.h
@@ -5,6 +5,7 @@
 #ifndef nsTableWrapperFrame_h__
 #define nsTableWrapperFrame_h__
 
+#include "LayoutConstants.h"
 #include "mozilla/Attributes.h"
 #include "mozilla/Maybe.h"
 #include "nscore.h"
@@ -202,6 +203,15 @@ class nsTableWrapperFrame : public nsContainerFrame {
                       const mozilla::LogicalSize& aInnerSize,
                       mozilla::LogicalPoint& aOrigin,
                       mozilla::WritingMode aWM) const;
+
+  // This is a helper for CreateReflowInputForInnerTable() and
+  // ComputeAutoSize(). It computes whether we need shrink-wrap behavior for
+  // children.
+  //
+  // Note: We don't need to call this in CreateReflowInputForCaption() because
+  // when we reflow the captions, we want them to stretch their inline-sizes to
+  // be at least as wide as the inner table frame.
+  mozilla::ComputeSizeFlags CreateComputeSizeFlagsForChild() const;
 
   // Create and init the child reflow input, using passed-in aChildRI, so that
   // caller can use it after we return.
