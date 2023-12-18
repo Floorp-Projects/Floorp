252f9e50c488f5cd8176353afd84d0baf1955882
Mark Banner <standard8@mozilla.com>
2023-12-18 12:01:34 +0000

    Bug 1868749 - Remove support for moz:date replacement in search engine urls. r=search-reviewers,daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D195767

--- toolkit/components/search/tests/xpcshell/test_getSubmission_params.js
+++ toolkit/components/search/tests/xpcshell/test_getSubmission_params.js
@@ -52,26 +52,6 @@ add_task(async function test_paramSubstitution() {
   check("{startPage?}", "");
 
   check("{moz:locale}", Services.locale.requestedLocale);
-
-  url.template = prefix + "{moz:date}";
-  let params = new URLSearchParams(engine.getSubmission(searchTerms).uri.query);
-  Assert.ok(params.has("search"), "Should have a search option");
-
-  let [, year, month, day, hour] = params
-    .get("search")
-    .match(/^(\d{4})(\d{2})(\d{2})(\d{2})/);
-  let date = new Date(year, month - 1, day, hour);
-
-  // We check the time is within an hour of now as the parameter is only
-  // precise to an hour. Checking the difference also should cope with date
-  // changes etc.
-  let difference = Date.now() - date;
-  Assert.lessOrEqual(
-    difference,
-    60 * 60 * 1000,
-    "Should have set the date within an hour"
-  );
-  Assert.greaterOrEqual(difference, 0, "Should not have a time in the past.");
 });
 
 add_task(async function test_mozParamsFailForNonAppProvided() {
