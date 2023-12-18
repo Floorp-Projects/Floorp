0492eb0673cc2e25730c2013528b5132deed2b48
mcheang <mcheang@mozilla.com>
2023-12-18 13:43:06 +0000

    Bug 1870420 - Add a description for search-config-v2 fetch engine selector tests. r=Standard8 DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D196648

--- toolkit/components/search/tests/xpcshell/test_engine_selector_defaults.js
+++ toolkit/components/search/tests/xpcshell/test_engine_selector_defaults.js
@@ -1,6 +1,11 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+/**
+ * This tests the SearchEngineSelector in finding the correct default engine
+ * and private default engine based on the user's environment.
+ */
+
 "use strict";
 
 ChromeUtils.defineESModuleGetters(this, {
