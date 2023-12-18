e49d4d88d43b8956a70625248afd622b2d7c0587
Lee Salzman <lsalzman@mozilla.com>
2023-12-18 18:10:46 +0000

    Bug 1829026 - Allow invalidating the current TLS context on establishing TLS scope. r=aosmond
    On a given thread, if there are outside users of OpenGL (such as WebRender) that don't go
    through the GLContext interface to set the current context, the TLS current context value
    may be incorrect. To solve this, we need to assume that on establishing some TLS scopes,
    that the current context value is unreliable and invalidate it so that it gets properly
    reset.
    
    Differential Revision: https://phabricator.services.mozilla.com/D194350

--- gfx/gl/GLContext.cpp
+++ gfx/gl/GLContext.cpp
@@ -309,6 +309,13 @@ GLContext::~GLContext() {
 }
 
 /*static*/
+void GLContext::InvalidateCurrentContext() {
+  if (sCurrentContext.init()) {
+    sCurrentContext.set(nullptr);
+  }
+}
+
+/*static*/
 void GLContext::StaticDebugCallback(GLenum source, GLenum type, GLuint id,
                                     GLenum severity, GLsizei length,
                                     const GLchar* message,
@@ -2406,7 +2413,7 @@ bool GLContext::MakeCurrent(bool aForce) const {
     }
     if (MOZ_LIKELY(isCurrent)) {
       MOZ_ASSERT(IsCurrentImpl() ||
-                 !MakeCurrentImpl());  // Might have lost context.
+                 MakeCurrentImpl());  // Might have lost context.
       return true;
     }
   }
