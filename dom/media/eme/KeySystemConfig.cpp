8de1886d1a76e0dc0ce0c6effa10ea50f1b35d9e
alwu <alwu@mozilla.com>
2023-12-18 20:52:11 +0000

    Bug 1863800 - part2 : check if Widevine L1 is installed. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D195831

--- dom/media/eme/KeySystemConfig.cpp
+++ dom/media/eme/KeySystemConfig.cpp
@@ -30,25 +30,33 @@ namespace mozilla {
 
 /* static */
 bool KeySystemConfig::Supports(const nsAString& aKeySystem) {
-  nsCString api = nsLiteralCString(CHROMIUM_CDM_API);
-  nsCString name = NS_ConvertUTF16toUTF8(aKeySystem);
-
-  if (HaveGMPFor(api, {name})) {
-    return true;
+  // Check if Widevine L3 or Clearkey has been downloaded via GMP downloader.
+  if (IsWidevineKeySystem(aKeySystem) || IsClearkeyKeySystem(aKeySystem)) {
+    return HaveGMPFor(nsCString(CHROMIUM_CDM_API),
+                      {NS_ConvertUTF16toUTF8(aKeySystem)});
   }
+
 #ifdef MOZ_WIDGET_ANDROID
   // Check if we can use MediaDrm for this keysystem.
-  if (mozilla::java::MediaDrmProxy::IsSchemeSupported(name)) {
+  if (mozilla::java::MediaDrmProxy::IsSchemeSupported(
+          NS_ConvertUTF16toUTF8(aKeySystem))) {
     return true;
   }
 #endif
+
 #if MOZ_WMF_CDM
-  if ((IsPlayReadyKeySystemAndSupported(aKeySystem) ||
-       IsWidevineExperimentKeySystemAndSupported(aKeySystem)) &&
+  // Check if Widevine L1 has been downloaded via GMP downloader.
+  if (IsWidevineExperimentKeySystemAndSupported(aKeySystem)) {
+    return HaveGMPFor(nsCString(kWidevineExperimentAPIName),
+                      {nsCString(kWidevineExperimentKeySystemName)});
+  }
+
+  if ((IsPlayReadyKeySystemAndSupported(aKeySystem)) &&
       WMFCDMImpl::Supports(aKeySystem)) {
     return true;
   }
 #endif
+
   return false;
 }
 
