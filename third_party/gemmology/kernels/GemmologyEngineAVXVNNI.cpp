3389b1eb865b8cea874b8d2ce64fa972027de18e
serge-sans-paille <sguelton@mozilla.com>
2023-12-19 13:04:19 +0000

    Bug 1868949 - Add support for AVX VNNI in cpu detection and IntegerGemm r=marco
    This make it possible to use the recently added changes in gemmology.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196425

---
+++ third_party/gemmology/kernels/GemmologyEngineAVXVNNI.cpp
@@ -0,0 +1,19 @@
+/* -*- mode: c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* this source code form is subject to the terms of the mozilla public
+ * license, v. 2.0. if a copy of the mpl was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <gemmology.h>
+
+namespace gemmology {
+template struct Engine<xsimd::avxvnni>;
+template void Engine<xsimd::avxvnni>::SelectColumnsB(int8_t const*, int8_t*,
+                                                  size_t, uint32_t const*,
+                                                  uint32_t const*);
+template void Engine<xsimd::avxvnni>::Shift::Multiply(
+    uint8_t const*, int8_t const*, size_t, size_t, size_t,
+    gemmology::callbacks::UnquantizeAndAddBiasAndWrite);
+template void Engine<xsimd::avxvnni>::Shift::PrepareBias(
+    int8_t const*, size_t, size_t,
+    gemmology::callbacks::UnquantizeAndAddBiasAndWrite);
+}  // namespace gemmology
