f714b61e9197c6a73078502cbd9d8dc83183a5d7
Mark Banner <standard8@mozilla.com>
2023-12-20 13:28:23 +0000

    Bug 1870644 - Merge test_multipleIcons.js into test_opensearch_icon.js. r=search-reviewers,mcheang
    test_multipleIcons.js is basically testing OpenSearch icons, so we should make that clear.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196731

--- toolkit/components/search/tests/xpcshell/test_opensearch_icon.js
+++ toolkit/components/search/tests/xpcshell/test_opensearch_icon.js
@@ -10,7 +10,7 @@ add_setup(async function () {
   await Services.search.init();
 });
 
-const tests = [
+const ICON_TESTS = [
   {
     name: "Big Icon",
     image: "bigIcon.ico",
@@ -28,8 +28,8 @@ const tests = [
   },
 ];
 
-for (const test of tests) {
-  add_task(async function () {
+add_task(async function test_icon_types() {
+  for (let test of ICON_TESTS) {
     info(`Testing ${test.name}`);
 
     let promiseEngineAdded = SearchTestUtils.promiseSearchNotification(
@@ -54,10 +54,57 @@ for (const test of tests) {
     let engine = await promiseEngineAdded;
     await promiseEngineChanged;
 
-    Assert.ok(engine.iconURI, "the engine has an icon");
+    Assert.ok(engine.iconURI, `${test.name} engine has an icon`);
     Assert.ok(
       engine.iconURI.spec.startsWith(test.expected),
-      "the icon is saved as an x-icon data url"
+      `${test.name} iconURI starts with the expected information`
     );
+  }
+});
+
+add_task(async function test_multiple_icons_in_file() {
+  let engine = await SearchTestUtils.promiseNewSearchEngine({
+    url: `${gDataUrl}engineImages.xml`,
   });
-}
+
+  info("The default should be the 16x16 icon");
+  Assert.ok(engine.iconURI.spec.includes("ico16"));
+
+  Assert.ok(engine.getIconURLBySize(16, 16).includes("ico16"));
+  Assert.ok(engine.getIconURLBySize(32, 32).includes("ico32"));
+  Assert.ok(engine.getIconURLBySize(74, 74).includes("ico74"));
+
+  info("Invalid dimensions should return null until bug 1655070 is fixed.");
+  Assert.equal(null, engine.getIconURLBySize(50, 50));
+
+  let allIcons = engine.getIcons();
+
+  info("Check that allIcons contains expected icon sizes");
+  Assert.equal(allIcons.length, 3);
+  let expectedWidths = [16, 32, 74];
+  Assert.ok(
+    allIcons.every(item => {
+      let width = item.width;
+      Assert.notEqual(expectedWidths.indexOf(width), -1);
+      Assert.equal(width, item.height);
+
+      let icon = item.url.split(",").pop();
+      Assert.equal(icon, "ico" + width);
+
+      return true;
+    })
+  );
+});
+
+add_task(async function test_icon_not_in_opensearch_file() {
+  let engineUrl = gDataUrl + "engine-fr.xml";
+  let engine = await Services.search.addOpenSearchEngine(
+    engineUrl,
+    "data:image/x-icon;base64,ico16"
+  );
+
+  // Even though the icon wasn't specified inside the XML file, it should be
+  // available both in the iconURI attribute and with getIconURLBySize.
+  Assert.ok(engine.iconURI.spec.includes("ico16"));
+  Assert.ok(engine.getIconURLBySize(16, 16).includes("ico16"));
+});
