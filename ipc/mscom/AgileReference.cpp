6f90016dbbe91dc8a86fdaf03992e1814eef63b1
Ray Kraesig <rkraesig@mozilla.com>
2023-12-20 17:06:21 +0000

    Bug 1869053 - [2/5] eliminate all implicit conversions between RefPtr and AgileReference  r=handyman,win-reviewers
    Remove all of the code in `RefPtr` and `AgileReference` that allowed
    for implicitly converting between the two in via constructors or the
    assignment operator.
    
    Replace these with a slightly-less-convenient-but-substantially-more-
    explicit `Resolve` function family.
    
    (This also eliminates the dependency that `class RefPtr` had on
    `class AgileReference`.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D196365

--- ipc/mscom/AgileReference.cpp
+++ ipc/mscom/AgileReference.cpp
@@ -87,7 +87,7 @@ AgileReference& AgileReference::operator=(AgileReference&& aOther) noexcept {
 }
 
 HRESULT
-AgileReference::Resolve(REFIID aIid, void** aOutInterface) const {
+AgileReference::ResolveRaw(REFIID aIid, void** aOutInterface) const {
   MOZ_ASSERT(aOutInterface);
   MOZ_ASSERT(mAgileRef);
   MOZ_ASSERT(IsCOMInitializedOnCurrentThread());
