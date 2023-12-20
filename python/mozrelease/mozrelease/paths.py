798b412f174c357533e8d074a229c6cadf64255d
Logan Rosen <loganrosen@gmail.com>
2023-12-20 03:05:48 +0000

    Bug 1870213 - Remove usage of six in python/mozrelease r=mach-reviewers,ahochheiden
    Differential Revision: https://phabricator.services.mozilla.com/D196527

--- python/mozrelease/mozrelease/paths.py
+++ python/mozrelease/mozrelease/paths.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from six.moves.urllib.parse import urlunsplit
+from urllib.parse import urlunsplit
 
 product_ftp_map = {
     "fennec": "mobile",
