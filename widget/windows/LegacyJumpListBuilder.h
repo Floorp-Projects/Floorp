c2c631b9eb2fc1c1fce5cff4443462ca35e11a34
Ray Kraesig <rkraesig@mozilla.com>
2023-12-20 17:06:23 +0000

    Bug 1869053 - [5/5] templatize and further simplify AgileReference  r=handyman,win-reviewers
    Lift `aIid` to compile-time, as a template parameter `InterfaceT`. This
    simplifies the common case for using `Resolve()`, where the desired and
    supplied interfaces are the same. (For the as-yet-unattested case where
    they're not, retain the old functionality by means of a small family of
    `ResolveAs()` functions.)
    
    Additionally, to eliminate a swath of custom logic and magic-number
    choices surrounding `mHResult`, eliminate `mHResult` itself as well.
    Instead, since its value was derived from the creation of the underlying
    `IAgileReference`, any callers that might care can acquire it as an
    additional return value from a named-constructor function.
    
    These collectively trim `AgileReference`'s footprint down to a single
    `RefPtr`, with all its special member functions having only default
    implementations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196513

--- widget/windows/LegacyJumpListBuilder.h
+++ widget/windows/LegacyJumpListBuilder.h
@@ -46,7 +46,8 @@ class LegacyJumpListBuilder : public nsILegacyJumpListBuilder,
   static Atomic<bool> sBuildingList;
 
  private:
-  mscom::AgileReference mJumpListMgr MOZ_GUARDED_BY(mMonitor);
+  mscom::AgileReference<ICustomDestinationList> mJumpListMgr
+      MOZ_GUARDED_BY(mMonitor);
   uint32_t mMaxItems MOZ_GUARDED_BY(mMonitor);
   bool mHasCommit;
   RefPtr<LazyIdleThread> mIOThread;
