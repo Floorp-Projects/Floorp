3084dc439db1e3d53dfae41470114bddc1d71655
Julien Cristau <jcristau@mozilla.com>
2023-12-20 09:20:08 +0000

    Bug 1866178 - fix taskcluster-load-image mach command. r=firefox-build-system-reviewers,taskgraph-reviewers,bhearsum,glandium
    Use standalone taskgraph's load_image_by_task_id, which shells out to
    docker instead of talking to its socket directly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196849

--- taskcluster/gecko_taskgraph/docker.py
+++ taskcluster/gecko_taskgraph/docker.py
@@ -3,15 +3,13 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-import json
 import os
-import tarfile
 from io import BytesIO
 
+from taskgraph.docker import load_image_by_task_id
 from taskgraph.generator import load_tasks_for_kind
 from taskgraph.optimize.strategies import IndexSearch
 from taskgraph.parameters import Parameters
-from taskgraph.util.taskcluster import get_artifact_url, get_session
 
 from gecko_taskgraph.util import docker
 
@@ -48,18 +46,6 @@ def load_image_by_name(image_name, tag=None):
         return False
 
     return load_image_by_task_id(task_id, tag)
-
-
-def load_image_by_task_id(task_id, tag=None):
-    artifact_url = get_artifact_url(task_id, "public/image.tar.zst")
-    result = load_image(artifact_url, tag)
-    print("Found docker image: {}:{}".format(result["image"], result["tag"]))
-    if tag:
-        print(f"Re-tagged as: {tag}")
-    else:
-        tag = "{}:{}".format(result["image"], result["tag"])
-    print(f"Try: docker run -ti --rm {tag} bash")
-    return True
 
 
 def build_context(name, outputFile, args=None):
@@ -103,95 +89,3 @@ def build_image(name, tag, args=None):
         print("Create an image suitable for deploying/pushing by creating")
         print("a VERSION file in the image directory.")
         print("*" * 50)
-
-
-def load_image(url, imageName=None, imageTag=None):
-    """
-    Load docker image from URL as imageName:tag, if no imageName or tag is given
-    it will use whatever is inside the zstd compressed tarball.
-
-    Returns an object with properties 'image', 'tag' and 'layer'.
-    """
-    import zstandard as zstd
-
-    # If imageName is given and we don't have an imageTag
-    # we parse out the imageTag from imageName, or default it to 'latest'
-    # if no imageName and no imageTag is given, 'repositories' won't be rewritten
-    if imageName and not imageTag:
-        if ":" in imageName:
-            imageName, imageTag = imageName.split(":", 1)
-        else:
-            imageTag = "latest"
-
-    info = {}
-
-    def download_and_modify_image():
-        # This function downloads and edits the downloaded tar file on the fly.
-        # It emits chunked buffers of the editted tar file, as a generator.
-        print(f"Downloading from {url}")
-        # get_session() gets us a requests.Session set to retry several times.
-        req = get_session().get(url, stream=True)
-        req.raise_for_status()
-
-        with zstd.ZstdDecompressor().stream_reader(req.raw) as ifh:
-            tarin = tarfile.open(
-                mode="r|",
-                fileobj=ifh,
-                bufsize=zstd.DECOMPRESSION_RECOMMENDED_OUTPUT_SIZE,
-            )
-
-            # Stream through each member of the downloaded tar file individually.
-            for member in tarin:
-                # Non-file members only need a tar header. Emit one.
-                if not member.isfile():
-                    yield member.tobuf(tarfile.GNU_FORMAT)
-                    continue
-
-                # Open stream reader for the member
-                reader = tarin.extractfile(member)
-
-                # If member is `repositories`, we parse and possibly rewrite the
-                # image tags.
-                if member.name == "repositories":
-                    # Read and parse repositories
-                    repos = json.loads(reader.read())
-                    reader.close()
-
-                    # If there is more than one image or tag, we can't handle it
-                    # here.
-                    if len(repos.keys()) > 1:
-                        raise Exception("file contains more than one image")
-                    info["image"] = image = list(repos.keys())[0]
-                    if len(repos[image].keys()) > 1:
-                        raise Exception("file contains more than one tag")
-                    info["tag"] = tag = list(repos[image].keys())[0]
-                    info["layer"] = layer = repos[image][tag]
-
-                    # Rewrite the repositories file
-                    data = json.dumps({imageName or image: {imageTag or tag: layer}})
-                    reader = BytesIO(data.encode("utf-8"))
-                    member.size = len(data)
-
-                # Emit the tar header for this member.
-                yield member.tobuf(tarfile.GNU_FORMAT)
-                # Then emit its content.
-                remaining = member.size
-                while remaining:
-                    length = min(remaining, zstd.DECOMPRESSION_RECOMMENDED_OUTPUT_SIZE)
-                    buf = reader.read(length)
-                    remaining -= len(buf)
-                    yield buf
-                # Pad to fill a 512 bytes block, per tar format.
-                remainder = member.size % 512
-                if remainder:
-                    yield ("\0" * (512 - remainder)).encode("utf-8")
-
-                reader.close()
-
-    docker.post_to_docker(download_and_modify_image(), "/images/load", quiet=0)
-
-    # Check that we found a repositories file
-    if not info.get("image") or not info.get("tag") or not info.get("layer"):
-        raise Exception("No repositories file found!")
-
-    return info
