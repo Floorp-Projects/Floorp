c5e7936d09889fd3163013f7d2b1835fea23e647
Paul Adenot <paul@paul.cx>
2023-12-22 21:39:09 +0000

    Bug 1749047 - Add EncoderTraits. r=chunmin
    I don't think it's terribly useful but it's there to call into the PEM to check
    support. We'll see if we keep it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D195711

---
+++ dom/media/EncoderTraits.cpp
@@ -0,0 +1,19 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "PEMFactory.h"
+
+namespace mozilla::EncodeTraits {
+
+// Returns true if it is possible to encode to a particular configuration, false
+// otherwise.
+bool Supports(const EncoderConfig& aConfig) {
+  RefPtr<PEMFactory> pem = new PEMFactory();
+  return pem->Supports(aConfig);
+}
+}
+
+
