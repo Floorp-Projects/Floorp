3373b9806b3408f464ade742eb1d446ad34a927d
Barret Rennie <barret@brennie.ca>
2023-12-22 19:08:53 +0000

    Bug 1860940 - Stub ExperimentStore.addEnrollment with a sync fn in tests r=emcminn
    Bug 1773583 replaced the innards of the stub with a sync fn, but we're still
    stubbing with an async fn (and awaiting a sync fn call). This should have no
    ordering-related side effects since awaiting a sync fn call returns immediately
    without yielding.
    
    Differential Revision: https://phabricator.services.mozilla.com/D197201

--- toolkit/components/nimbus/test/NimbusTestUtils.sys.mjs
+++ toolkit/components/nimbus/test/NimbusTestUtils.sys.mjs
@@ -185,8 +185,8 @@ export const ExperimentFakes = {
     // We want calls to `store.addEnrollment` to implicitly validate the
     // enrollment before saving to store
     let origAddExperiment = manager.store.addEnrollment.bind(manager.store);
-    sandbox.stub(manager.store, "addEnrollment").callsFake(async enrollment => {
-      await ExperimentTestUtils.validateEnrollment(enrollment);
+    sandbox.stub(manager.store, "addEnrollment").callsFake(enrollment => {
+      ExperimentTestUtils.validateEnrollment(enrollment);
       return origAddExperiment(enrollment);
     });
 
