254da3bf4c772ab1c857dcd31d9bee742ab9f89d
Jonathan Kew <jkew@mozilla.com>
2023-12-23 13:05:33 +0000

    Bug 1870973 - Back out changeset 3ff9d913d60a (bug 1862249) due to line-wrapping regressions.  a=dmeehan
    This reverts the change to make ideographic space trimmable, because of unintended impact on line
    breaking and measurement in other cases. We'll need a more carefully targeted fix for the original
    issue in bug 1862249, as well as better test coverage when landing a new patch there.
    
    (Given that we've had multiple regression reports about this, I think we should uplift the backout
    to 122. We could even consider it for a 121.x dot-release if one happens, as it's just reverting
    to longstanding behavior here.)
    
    Original Revision: https://phabricator.services.mozilla.com/D197150
    
    Differential Revision: https://phabricator.services.mozilla.com/D197196

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -744,16 +744,13 @@ static bool IsCSSWordSpacingSpace(const nsTextFragment* aFrag, uint32_t aPos,
 }
 
 constexpr char16_t kOghamSpaceMark = 0x1680;
-constexpr char16_t kIdeographicSpace = 0x3000;
 
 // Check whether the string aChars/aLength starts with space that's
 // trimmable according to CSS 'white-space:normal/nowrap'.
-static bool IsTrimmableSpaceAtStart(const char16_t* aChars, uint32_t aLength) {
+static bool IsTrimmableSpace(const char16_t* aChars, uint32_t aLength) {
   NS_ASSERTION(aLength > 0, "No text for IsSpace!");
 
   char16_t ch = *aChars;
-  // Note that Ideographic Space is NOT treated as trimmable here, because
-  // (unlike ASCII space) it does not collapse at start-of-line.
   if (ch == ' ' || ch == kOghamSpaceMark) {
     return !nsTextFrameUtils::IsSpaceCombiningSequenceTail(aChars + 1,
                                                            aLength - 1);
@@ -775,7 +772,6 @@ static bool IsTrimmableSpace(const nsTextFragment* aFrag, uint32_t aPos,
   switch (aFrag->CharAt(aPos)) {
     case ' ':
     case kOghamSpaceMark:
-    case kIdeographicSpace:
       return (!aStyleText->WhiteSpaceIsSignificant() || aAllowHangingWS) &&
              !IsSpaceCombiningSequenceTail(aFrag, aPos + 1);
     case '\n':
@@ -824,7 +820,7 @@ static uint32_t GetTrimmableWhitespaceCount(const nsTextFragment* aFrag,
     const char16_t* str = aFrag->Get2b() + aStartOffset;
     int32_t fragLen = aFrag->GetLength() - aStartOffset;
     for (; count < aLength; ++count) {
-      if (!IsTrimmableSpaceAtStart(str, fragLen)) {
+      if (!IsTrimmableSpace(str, fragLen)) {
         break;
       }
       str += aDirection;
