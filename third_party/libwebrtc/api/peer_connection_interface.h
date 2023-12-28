2071e3afa5f55b02d79866b895e14d8b26a6eefe
Michael Froman <mfroman@mozilla.com>
2023-12-28 16:00:16 -0600

    Bug 1871981 - Vendor libwebrtc from 4408575d18
    Upstream commit: https://webrtc.googlesource.com/src/+/4408575d18b6a6869ef9dbfa4d4a2b3615767576
        Reland "Enable SRTP GCM ciphers by default"
    
        This is a reland of commit d8633868b34dc1d841f0a9fd1afe2bc22aa8bde6
    
        Original change's description:
        > Enable SRTP GCM ciphers by default
        >
        > Bug: webrtc:15178
        > Change-Id: I0216ce8f194fffc820723d82b9c04a76573c2f4f
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/305381
        > Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        > Commit-Queue: Philipp Hancke <phancke@microsoft.com>
        > Reviewed-by: Victor Boivie <boivie@webrtc.org>
        > Cr-Commit-Position: refs/heads/main@{#40828}
    
        Bug: webrtc:15178
        Change-Id: I5ea939ed6263547ebc177d9dd1763ba888936866
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/321961
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Reviewed-by: Victor Boivie <boivie@webrtc.org>
        Commit-Queue: Harald Alvestrand <hta@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40862}

--- third_party/libwebrtc/api/peer_connection_interface.h
+++ third_party/libwebrtc/api/peer_connection_interface.h
@@ -1493,7 +1493,7 @@ class RTC_EXPORT PeerConnectionFactoryInterface
     rtc::SSLProtocolVersion ssl_max_version = rtc::SSL_PROTOCOL_DTLS_12;
 
     // Sets crypto related options, e.g. enabled cipher suites.
-    CryptoOptions crypto_options = CryptoOptions::NoGcm();
+    CryptoOptions crypto_options = {};
   };
 
   // Set the options to be used for subsequently created PeerConnections.
