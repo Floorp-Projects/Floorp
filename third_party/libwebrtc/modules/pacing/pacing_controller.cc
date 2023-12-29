91d0f022702388330b79a7cb500da719aba818a7
Michael Froman <mfroman@mozilla.com>
2023-12-29 17:18:45 -0600

    Bug 1871981 - Vendor libwebrtc from 0b554e7004
    Upstream commit: https://webrtc.googlesource.com/src/+/0b554e7004be0b4bea85c8b0833cfc4ff056500b
        Upper limit pacer send bursts to about 63Kbyte
    
        The purpose is to ensure send socket buffers are not overfilled at high
        pacing rates.
    
        Bug: chromium:1354491
        Change-Id: Ic6f473080292f84a2a099b85fb5817f7e14e7355
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/323000
        Commit-Queue: Per Kjellander <perkj@webrtc.org>
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40911}

--- third_party/libwebrtc/modules/pacing/pacing_controller.cc
+++ third_party/libwebrtc/modules/pacing/pacing_controller.cc
@@ -17,6 +17,8 @@
 
 #include "absl/cleanup/cleanup.h"
 #include "absl/strings/match.h"
+#include "api/units/data_size.h"
+#include "api/units/time_delta.h"
 #include "modules/pacing/bitrate_prober.h"
 #include "modules/pacing/interval_budget.h"
 #include "rtc_base/checks.h"
@@ -343,9 +345,13 @@ Timestamp PacingController::NextSendTime() const {
     // debt is allowed to grow up to one packet more than what can be sent
     // during 'send_burst_period_'.
     TimeDelta drain_time = media_debt_ / adjusted_media_rate_;
+    // Ensure that a burst of sent packet is not larger than kMaxBurstSize in
+    // order to not risk overfilling socket buffers at high bitrate.
+    TimeDelta send_burst_interval =
+        std::min(send_burst_interval_, kMaxBurstSize / adjusted_media_rate_);
     next_send_time =
         last_process_time_ +
-        ((send_burst_interval_ > drain_time) ? TimeDelta::Zero() : drain_time);
+        ((send_burst_interval > drain_time) ? TimeDelta::Zero() : drain_time);
   } else if (padding_rate_ > DataRate::Zero() && packet_queue_.Empty()) {
     // If we _don't_ have pending packets, check how long until we have
     // bandwidth for padding packets. Both media and padding debts must
