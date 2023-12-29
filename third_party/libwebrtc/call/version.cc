bcc9b3eaa3621d554399d200fbc0f236e5b1a1fa
Michael Froman <mfroman@mozilla.com>
2023-12-29 11:36:38 -0600

    Bug 1871981 - Vendor libwebrtc from 3c3e205d1e
    Upstream commit: https://webrtc.googlesource.com/src/+/3c3e205d1ed488f69ee07148f9a8f9314d96faed
        Update WebRTC code version (2023-10-07T04:03:53).
    
        Bug: None
        Change-Id: I7467696806d3f0765b97cc67d81686ed9e849dbe
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/322591
        Commit-Queue: webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com <webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com>
        Bot-Commit: webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com <webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com>
        Cr-Commit-Position: refs/heads/main@{#40886}

--- third_party/libwebrtc/call/version.cc
+++ third_party/libwebrtc/call/version.cc
@@ -13,7 +13,7 @@
 namespace webrtc {
 
 // The timestamp is always in UTC.
-const char* const kSourceTimestamp = "WebRTC source stamp 2023-10-05T04:12:20";
+const char* const kSourceTimestamp = "WebRTC source stamp 2023-10-07T04:03:53";
 
 void LoadWebRTCVersionInRegister() {
   // Using volatile to instruct the compiler to not optimize `p` away even
