34b1884ee882085134ab760ff554f4c436cd49a2
Dão Gottwald <dao@mozilla.com>
2023-12-30 10:49:36 +0000

    Bug 1843922 - Fix and refactor browser_new_tab_bookmarks_toolbar_height.js to be less flaky. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D183872

--- browser/base/content/test/tabs/browser_new_tab_bookmarks_toolbar_height.js
+++ browser/base/content/test/tabs/browser_new_tab_bookmarks_toolbar_height.js
@@ -7,6 +7,60 @@
 // Tests that showing the bookmarks toolbar for new tabs only doesn't affect
 // the view port height in background tabs.
 
+let gHeightChanges = 0;
+async function expectHeightChanges(tab, expectedNewHeightChanges, msg) {
+  let contentObservedHeightChanges = await ContentTask.spawn(
+    tab.linkedBrowser,
+    null,
+    async args => {
+      await new Promise(resolve => content.requestAnimationFrame(resolve));
+      return content.document.body.innerText;
+    }
+  );
+  is(
+    contentObservedHeightChanges - gHeightChanges,
+    expectedNewHeightChanges,
+    msg
+  );
+  gHeightChanges = contentObservedHeightChanges;
+}
+
+async function expectBmToolbarVisibilityChange(triggerFn, visible, msg) {
+  let collapsedState = BrowserTestUtils.waitForMutationCondition(
+    BookmarkingUI.toolbar,
+    { attributes: true, attributeFilter: ["collapsed"] },
+    () => BookmarkingUI.toolbar.collapsed != visible
+  );
+  let toolbarItemsVisibilityUpdated = visible
+    ? BrowserTestUtils.waitForEvent(
+        BookmarkingUI.toolbar,
+        "BookmarksToolbarVisibilityUpdated"
+      )
+    : null;
+  triggerFn();
+  await collapsedState;
+  is(
+    BookmarkingUI.toolbar.getAttribute("collapsed"),
+    (!visible).toString(),
+    `${msg}; collapsed attribute state`
+  );
+  if (visible) {
+    info(`${msg}; waiting for toolbar items to become visible`);
+    await toolbarItemsVisibilityUpdated;
+    isnot(
+      BookmarkingUI.toolbar.getBoundingClientRect().height,
+      0,
+      `${msg}; should have a height`
+    );
+  } else {
+    is(
+      BookmarkingUI.toolbar.getBoundingClientRect().height,
+      0,
+      `${msg}; should have zero height`
+    );
+  }
+}
+
 add_task(async function () {
   registerCleanupFunction(() => {
     setToolbarVisibility(
@@ -17,11 +71,10 @@ add_task(async function () {
     );
   });
 
-  setToolbarVisibility(BookmarkingUI.toolbar, false, false, false);
-  is(
-    BookmarkingUI.toolbar.getAttribute("collapsed"),
-    "true",
-    "bookmarks toolbar is hidden"
+  await expectBmToolbarVisibilityChange(
+    () => setToolbarVisibility(BookmarkingUI.toolbar, false, false, false),
+    false,
+    "bookmarks toolbar is hidden initially"
   );
 
   let pageURL = getRootDirectory(gTestPath).replace(
@@ -31,62 +84,44 @@ add_task(async function () {
   pageURL = `${pageURL}file_observe_height_changes.html`;
   let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, pageURL);
 
-  let expectedHeightChanges = 0;
-  let assertHeightChanges = async msg => {
-    let heightChanges = await ContentTask.spawn(
-      tab.linkedBrowser,
-      null,
-      async args => {
-        await new Promise(resolve => content.requestAnimationFrame(resolve));
-        return content.document.body.innerText;
-      }
-    );
-    is(heightChanges, expectedHeightChanges, msg);
-  };
-  let assertBmToolbarVisibility = (visible, msg) => {
-    is(
-      BookmarkingUI.toolbar.getAttribute("collapsed"),
-      (!visible).toString(),
-      "bookmarks toolbar collapsed attribute state"
-    );
-    if (visible) {
-      isnot(
-        BookmarkingUI.toolbar.getBoundingClientRect().height,
-        0,
-        "bookmarks toolbar should have a height"
-      );
-    } else {
-      is(
-        BookmarkingUI.toolbar.getBoundingClientRect().height,
-        0,
-        "bookmarks toolbar height should be 0"
-      );
-    }
-  };
-
-  setToolbarVisibility(BookmarkingUI.toolbar, true, false, false);
-  assertBmToolbarVisibility(true, "bookmarks toolbar is visible");
-  expectedHeightChanges++;
-  assertHeightChanges(
-    "height changes when showing the toolbar without the animation"
+  await expectBmToolbarVisibilityChange(
+    () => setToolbarVisibility(BookmarkingUI.toolbar, true, false, false),
+    true,
+    "bookmarks toolbar is visible after explicitly showing it for tab with content loaded"
+  );
+  await expectHeightChanges(
+    tab,
+    1,
+    "content area height changes when showing the toolbar without the animation"
   );
 
-  setToolbarVisibility(BookmarkingUI.toolbar, "newtab", false, false);
-  assertBmToolbarVisibility(
+  await expectBmToolbarVisibilityChange(
+    () => setToolbarVisibility(BookmarkingUI.toolbar, "newtab", false, false),
     false,
-    "bookmarks toolbar is hidden for non-new tab"
+    "bookmarks toolbar is hidden for non-new tab after setting it to only show for new tabs"
   );
-  expectedHeightChanges++;
-  assertHeightChanges(
-    "height changes when hiding the toolbar without the animation"
+  await expectHeightChanges(
+    tab,
+    1,
+    "content area height changes when hiding the toolbar without the animation"
   );
 
   info("Opening a new tab, making the previous tab non-selected");
-  BrowserOpenTab();
-  ok(!tab.selected, "non-new tab is in the background (not the selected tab)");
-  assertBmToolbarVisibility(true, "bookmarks toolbar is visible for new tab");
-  assertHeightChanges(
-    "no additional height change in background tab when showing the bookmarks toolbar in new tab"
+  await expectBmToolbarVisibilityChange(
+    () => {
+      BrowserOpenTab();
+      ok(
+        !tab.selected,
+        "non-new tab is in the background (not the selected tab)"
+      );
+    },
+    true,
+    "bookmarks toolbar is visible for new tab after setting it to only show for new tabs"
+  );
+  await expectHeightChanges(
+    tab,
+    0,
+    "no additional content area height change in background tab when showing the bookmarks toolbar in new tab"
   );
 
   gBrowser.removeCurrentTab();
