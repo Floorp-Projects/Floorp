b678f695cbb11afcd54bf1d3a9c676f536dc8834
Cosmin Sabou <csabou@mozilla.com>
2023-12-30 14:52:41 +0200

    Backed out changeset 22c07e92d841 (bug 1868928) for causing memory leaks.

--- gfx/layers/wr/AsyncImagePipelineManager.cpp
+++ gfx/layers/wr/AsyncImagePipelineManager.cpp
@@ -12,7 +12,6 @@
 #include "CompositableHost.h"
 #include "gfxEnv.h"
 #include "mozilla/gfx/gfxVars.h"
-#include "mozilla/layers/AsyncImagePipelineOp.h"
 #include "mozilla/layers/CompositorThread.h"
 #include "mozilla/layers/RemoteTextureHostWrapper.h"
 #include "mozilla/layers/SharedSurfacesParent.h"
@@ -477,9 +476,7 @@ void AsyncImagePipelineManager::ApplyAsyncImageForPipeline(
 
 void AsyncImagePipelineManager::ApplyAsyncImageForPipeline(
     const wr::PipelineId& aPipelineId, wr::TransactionBuilder& aTxn,
-    wr::TransactionBuilder& aTxnForImageBridge,
-    AsyncImagePipelineOps* aPendingOps,
-    RemoteTextureInfoList* aPendingRemoteTextures) {
+    wr::TransactionBuilder& aTxnForImageBridge, RemoteTextureInfoList* aList) {
   AsyncImagePipeline* pipeline =
       mAsyncImagePipelines.Get(wr::AsUint64(aPipelineId));
   if (!pipeline) {
@@ -489,7 +486,7 @@ void AsyncImagePipelineManager::ApplyAsyncImageForPipeline(
   // ready event of RemoteTexture that uses ImageBridge do not need to be
   // checked here.
   if (pipeline->mImageHost->GetAsyncRef()) {
-    aPendingRemoteTextures = nullptr;
+    aList = nullptr;
   }
 
   wr::TransactionBuilder fastTxn(mApi, /* aUseSceneBuilderThread */ false);
@@ -515,36 +512,14 @@ void AsyncImagePipelineManager::ApplyAsyncImageForPipeline(
   auto* wrapper = texture ? texture->AsRemoteTextureHostWrapper() : nullptr;
 
   // Store pending remote texture that is used for waiting at WebRenderAPI.
-  if (aPendingRemoteTextures && texture &&
-      texture != pipeline->mCurrentTexture && texture->NumSubTextures() != 0 &&
-      wrapper && wrapper->IsReadyForRendering()) {
-    aPendingRemoteTextures->mList.emplace(wrapper->mTextureId,
-                                          wrapper->mOwnerId, wrapper->mForPid);
-  }
-
-  if (aPendingOps && !pipeline->mImageHost->GetAsyncRef()) {
-    aPendingOps->mList.emplace(AsyncImagePipelineOp::ApplyAsyncImageForPipeline(
-        this, aPipelineId, texture));
-    return;
+  if (aList && texture && wrapper && texture != pipeline->mCurrentTexture &&
+      texture->NumSubTextures() != 0 && wrapper->IsReadyForRendering()) {
+    aList->mList.emplace(wrapper->mTextureId, wrapper->mOwnerId,
+                         wrapper->mForPid);
   }
 
   ApplyAsyncImageForPipeline(epoch, aPipelineId, pipeline, texture,
                              sceneBuilderTxn, maybeFastTxn);
-}
-
-void AsyncImagePipelineManager::ApplyAsyncImageForPipeline(
-    const wr::PipelineId& aPipelineId, TextureHost* aTexture,
-    wr::TransactionBuilder& aTxn) {
-  AsyncImagePipeline* pipeline =
-      mAsyncImagePipelines.Get(wr::AsUint64(aPipelineId));
-  if (!pipeline) {
-    return;
-  }
-  MOZ_ASSERT(!pipeline->mImageHost->GetAsyncRef());
-
-  wr::Epoch epoch = GetNextImageEpoch();
-  ApplyAsyncImageForPipeline(epoch, aPipelineId, pipeline, aTexture, aTxn,
-                             aTxn);
 }
 
 void AsyncImagePipelineManager::SetEmptyDisplayList(
