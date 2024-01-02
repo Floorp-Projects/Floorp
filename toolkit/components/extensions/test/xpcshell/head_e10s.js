1901c9d000bb5912175de3a429fa619c41be7300
Gregory Pappas <gp3033@protonmail.com>
2024-01-02 11:17:09 +0000

    Bug 1724089 - Remove browser.tabs.remote.autostart pref r=webdriver-reviewers,perftest-reviewers,geckoview-reviewers,jgraham,extension-reviewers,mossop,sparky,robwu,owlish
    Differential Revision: https://phabricator.services.mozilla.com/D192136

--- toolkit/components/extensions/test/xpcshell/head_e10s.js
+++ toolkit/components/extensions/test/xpcshell/head_e10s.js
@@ -2,7 +2,4 @@
 
 /* globals ExtensionTestUtils */
 
-// xpcshell disables e10s by default. Turn it on.
-Services.prefs.setBoolPref("browser.tabs.remote.autostart", true);
-
-ExtensionTestUtils.remoteContentScripts = true;
+// TODO: Remove head_e10s.js and remoteContentScripts (XPCShellContentUtils.sys.mjs)
