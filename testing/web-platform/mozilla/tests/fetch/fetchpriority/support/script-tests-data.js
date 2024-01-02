baff4c93ade2c7e66b09df041d377a38bc3dc40e
Mirko Brodesser <mbrodesser@igalia.com>
2024-01-02 14:12:50 +0000

    Bug 1861061: change internal priority to low for async/deferred scripts with `fetchpriority=auto` and to high for module-scripts and scripts from head. r=smaug
    For web-compatibility; match Chromium's behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D196954

--- testing/web-platform/mozilla/tests/fetch/fetchpriority/support/script-tests-data.js
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/script-tests-data.js
@@ -3,6 +3,12 @@ const kFetchPriorityHighRequestFileNameAndSuffix = "dummy.js?2";
 const kFetchPriorityAutoRequestFileNameAndSuffix = "dummy.js?3";
 const kNoFetchPriorityRequestFileNameAndSuffix = "dummy.js?4";
 
+// Mapping fetchpriority's values to internal priorities is specified as
+// implementation-defined (https://fetch.spec.whatwg.org/#concept-fetch, step
+// 15). For web-compatibility, Chromium's desired mapping is chosen, see
+// <https://web.dev/articles/fetch-priority#browser_priority_and_fetchpriority>.
+// Exceptions are commented below.
+
 const kExpectedRequestsForScriptsInHead = [
     {   fileNameAndSuffix: kFetchPriorityLowRequestFileNameAndSuffix,
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
@@ -11,21 +17,13 @@ const kExpectedRequestsForScriptsInHead = [
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
     },
     {   fileNameAndSuffix: kFetchPriorityAutoRequestFileNameAndSuffix,
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
     },
     {   fileNameAndSuffix: kNoFetchPriorityRequestFileNameAndSuffix,
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
     }
 ];
 
-// Mapping fetchpriority's values to internal priorities is specified as
-// implementation-defined (https://fetch.spec.whatwg.org/#concept-fetch, step
-// 15). For web-compatibility, Chromium's mapping is chosen, see
-// <https://web.dev/articles/fetch-priority#browser_priority_and_fetchpriority>.
-//
-// The difference of the internal priorities for late- and early-in-body scripts
-// is considered important for optimizing the LCP
-// (https://developer.mozilla.org/en-US/docs/Glossary/Largest_contentful_paint).
 const kExpectedRequestsForScriptsInBody = [
     {   fileNameAndSuffix: "dummy.js?1",
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
@@ -33,11 +31,12 @@ const kExpectedRequestsForScriptsInBody = [
     {   fileNameAndSuffix: "dummy.js?2",
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
     },
+    // Bug 1872654: Chromium's behavior here differs.
     {   fileNameAndSuffix: "dummy.js?3",
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
     },
     {   fileNameAndSuffix: "dummy.js?4",
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
     },
     {   fileNameAndSuffix: "dummy.js?5",
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
@@ -55,6 +54,53 @@ const kExpectedRequestsForScriptsInBody = [
 
 export const kTestFolderName = "script-tests";
 
+const kExpectedRequestsForNonModuleAsyncAndDeferredScripts = [
+    {   fileNameAndSuffix: "dummy.js?1",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.js?2",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.js?3",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.js?4",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+]
+
+const kExpectedRequestsForDynamicNonModuleScripts = [
+    {   fileNameAndSuffix: "dummy.js?1",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.js?2",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.js?3",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.js?4",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+];
+
+// Chromium's desired behavior is under discussion:
+// <https://bugs.chromium.org/p/chromium/issues/detail?id=1475635>.
+const kExpectedRequestsForModuleScripts = [
+    {   fileNameAndSuffix: "dummy.js?1",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "dummy.js?2",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.js?3",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.js?4",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+]
+
 export const kTestData = [
     {   testFileName: "script-initial-load-head.h2.html",
         expectedRequests: kExpectedRequestsForScriptsInHead
@@ -63,24 +109,24 @@ export const kTestData = [
         expectedRequests: kExpectedRequestsForScriptsInBody
     },
     {   testFileName: "async-script-initial-load.h2.html",
-        expectedRequests: kExpectedRequestsForScriptsInHead
+        expectedRequests: kExpectedRequestsForNonModuleAsyncAndDeferredScripts
     },
     {   testFileName: "deferred-script-initial-load.h2.html",
-        expectedRequests: kExpectedRequestsForScriptsInHead
+        expectedRequests: kExpectedRequestsForNonModuleAsyncAndDeferredScripts
     },
     {   testFileName: "module-script-initial-load.h2.html",
-        expectedRequests: kExpectedRequestsForScriptsInHead
+        expectedRequests: kExpectedRequestsForModuleScripts
     },
     {   testFileName: "async-module-script-initial-load.h2.html",
-        expectedRequests: kExpectedRequestsForScriptsInHead
+        expectedRequests: kExpectedRequestsForModuleScripts
     },
     // Dynamic insertion executes non-speculative-parsing
     // (https://developer.mozilla.org/en-US/docs/Glossary/speculative_parsing)
-    // code paths.
+    // code paths. Moreover such inserted scripts are loaded asynchronously.
     {   testFileName: "script-dynamic-insertion.h2.html",
-        expectedRequests: kExpectedRequestsForScriptsInHead
+        expectedRequests: kExpectedRequestsForNonModuleAsyncAndDeferredScripts
     },
     {   testFileName: "module-script-dynamic-insertion.h2.html",
-        expectedRequests: kExpectedRequestsForScriptsInHead
+        expectedRequests: kExpectedRequestsForModuleScripts
     }
 ];
