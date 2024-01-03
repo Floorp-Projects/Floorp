22f7c8b81537f4523119e4e7a7e30d8349292301
Michael Hughes <mhughes@mozilla.com>
2024-01-03 21:50:04 +0000

    Bug 1830725: Synchronous `nsWindowsPackageManager::GetCampaignId` blocks main thread for many seconds r=nrishel
    Made GetCampaignId asynchronous to not block anything.
    
    Differential Revision: https://phabricator.services.mozilla.com/D188520

--- browser/components/attribution/test/browser/browser_AttributionCode_telemetry.js
+++ browser/components/attribution/test/browser/browser_AttributionCode_telemetry.js
@@ -80,9 +80,17 @@ add_task(async function test_read_error() {
     throw new Error("read_error");
   };
 
+  // On MSIX builds, AttributionIOUtils.read is not used; AttributionIOUtils.msixCampaignId is.
+  // Ensure we override that as well.
+  let oldMsixCampaignId = AttributionIOUtils.msixCampaignId;
+  AttributionIOUtils.msixCampaignId = async () => {
+    throw new Error("read_error");
+  };
+
   registerCleanupFunction(() => {
     AttributionIOUtils.exists = oldExists;
     AttributionIOUtils.read = oldRead;
+    AttributionIOUtils.msixCampaignId = oldMsixCampaignId;
   });
 
   // Try to read the file
