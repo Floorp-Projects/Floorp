7bb7c66501c745797e03112e1e5c14251c903dd2
Michael Froman <mfroman@mozilla.com>
2024-01-03 11:16:42 -0600

    Bug 1871981 - Vendor libwebrtc from 8ef094f66a
    Upstream commit: https://webrtc.googlesource.com/src/+/8ef094f66aa17a39dd71dcdb2bb37cf6425982a6
        Use acked bitrate as lower bound of loss based BWE.
    
        This cl/ makes sure that the estimate cannot go lower than a factor of acked bitrate. The current flag LowerBoundByAckedRateFactor is set to 0, means we dont use it.
    
    
        Bug: webrtc:12707
        Change-Id: I75d5881f0b85a374af3f7039b82c71aee97fb7b0
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/323881
        Reviewed-by: Per Kjellander <perkj@webrtc.org>
        Commit-Queue: Diep Bui <diepbp@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#40958}

--- third_party/libwebrtc/modules/congestion_controller/goog_cc/loss_based_bwe_v2.cc
+++ third_party/libwebrtc/modules/congestion_controller/goog_cc/loss_based_bwe_v2.cc
@@ -164,11 +164,14 @@ LossBasedBweV2::Result LossBasedBweV2::GetLossBasedResult() const {
 
   if (IsValid(delay_based_estimate_)) {
     result.bandwidth_estimate =
-        std::min({current_estimate_.loss_limited_bandwidth,
-                  GetInstantUpperBound(), delay_based_estimate_});
+        std::max(GetInstantLowerBound(),
+                 std::min({current_estimate_.loss_limited_bandwidth,
+                           GetInstantUpperBound(), delay_based_estimate_}));
   } else {
-    result.bandwidth_estimate = std::min(
-        current_estimate_.loss_limited_bandwidth, GetInstantUpperBound());
+    result.bandwidth_estimate =
+        std::max(GetInstantLowerBound(),
+                 std::min(current_estimate_.loss_limited_bandwidth,
+                          GetInstantUpperBound()));
   }
   return result;
 }
@@ -176,6 +179,7 @@ LossBasedBweV2::Result LossBasedBweV2::GetLossBasedResult() const {
 void LossBasedBweV2::SetAcknowledgedBitrate(DataRate acknowledged_bitrate) {
   if (IsValid(acknowledged_bitrate)) {
     acknowledged_bitrate_ = acknowledged_bitrate;
+    CalculateInstantLowerBound();
   } else {
     RTC_LOG(LS_WARNING) << "The acknowledged bitrate must be finite: "
                         << ToString(acknowledged_bitrate);
@@ -195,6 +199,7 @@ void LossBasedBweV2::SetMinMaxBitrate(DataRate min_bitrate,
                                       DataRate max_bitrate) {
   if (IsValid(min_bitrate)) {
     min_bitrate_ = min_bitrate;
+    CalculateInstantLowerBound();
   } else {
     RTC_LOG(LS_WARNING) << "The min bitrate must be finite: "
                         << ToString(min_bitrate);
@@ -393,6 +398,8 @@ absl::optional<LossBasedBweV2::Config> LossBasedBweV2::CreateConfig(
                                                       true);
   FieldTrialParameter<bool> use_in_start_phase("UseInStartPhase", false);
   FieldTrialParameter<int> min_num_observations("MinNumObservations", 3);
+  FieldTrialParameter<double> lower_bound_by_acked_rate_factor(
+      "LowerBoundByAckedRateFactor", 0.0);
   if (key_value_config) {
     ParseFieldTrial({&enabled,
                      &bandwidth_rampup_upper_bound_factor,
@@ -428,7 +435,8 @@ absl::optional<LossBasedBweV2::Config> LossBasedBweV2::CreateConfig(
                      &slope_of_bwe_high_loss_func,
                      &not_use_acked_rate_in_alr,
                      &use_in_start_phase,
-                     &min_num_observations},
+                     &min_num_observations,
+                     &lower_bound_by_acked_rate_factor},
                     key_value_config->Lookup("WebRTC-Bwe-LossBasedBweV2"));
   }
 
@@ -488,6 +496,8 @@ absl::optional<LossBasedBweV2::Config> LossBasedBweV2::CreateConfig(
   config->not_use_acked_rate_in_alr = not_use_acked_rate_in_alr.Get();
   config->use_in_start_phase = use_in_start_phase.Get();
   config->min_num_observations = min_num_observations.Get();
+  config->lower_bound_by_acked_rate_factor =
+      lower_bound_by_acked_rate_factor.Get();
 
   return config;
 }
@@ -671,6 +681,13 @@ bool LossBasedBweV2::IsConfigValid() const {
   if (config_->min_num_observations <= 0) {
     RTC_LOG(LS_WARNING) << "The min number of observations must be positive: "
                         << config_->min_num_observations;
+    valid = false;
+  }
+  if (config_->lower_bound_by_acked_rate_factor < 0.0) {
+    RTC_LOG(LS_WARNING)
+        << "The estimate lower bound by acknowledged rate factor must be "
+           "non-negative: "
+        << config_->lower_bound_by_acked_rate_factor;
     valid = false;
   }
   return valid;
@@ -911,6 +928,24 @@ void LossBasedBweV2::CalculateInstantUpperBound() {
   }
 
   cached_instant_upper_bound_ = instant_limit;
+}
+
+DataRate LossBasedBweV2::GetInstantLowerBound() const {
+  return cached_instant_lower_bound_.value_or(DataRate::Zero());
+}
+
+void LossBasedBweV2::CalculateInstantLowerBound() {
+  DataRate instance_lower_bound = DataRate::Zero();
+  if (IsValid(acknowledged_bitrate_) &&
+      config_->lower_bound_by_acked_rate_factor > 0.0) {
+    instance_lower_bound = config_->lower_bound_by_acked_rate_factor *
+                           acknowledged_bitrate_.value();
+  }
+
+  if (IsValid(min_bitrate_)) {
+    instance_lower_bound = std::max(instance_lower_bound, min_bitrate_);
+  }
+  cached_instant_lower_bound_ = instance_lower_bound;
 }
 
 void LossBasedBweV2::CalculateTemporalWeights() {
