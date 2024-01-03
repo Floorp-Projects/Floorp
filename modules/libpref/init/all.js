62d3f55fddd4097911f17ad01b24f0a641bb1fc5
Kagami Sascha Rosylight <saschanaz@outlook.com>
2024-01-03 12:06:08 +0000

    Bug 1872510 - Add SamsungColorEmoji to font.name-list.emoji for Android r=jfkthame, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D197525

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2898,7 +2898,7 @@ pref("font.size.monospace.x-math", 13);
 #if defined(ANDROID)
   // We use the bundled Charis SIL Compact as serif font for Firefox for Android
 
-  pref("font.name-list.emoji", "Noto Color Emoji");
+  pref("font.name-list.emoji", "SamsungColorEmoji, Noto Color Emoji");
 
   pref("font.name-list.serif.ar", "Noto Naskh Arabic, Noto Serif, Droid Serif");
   pref("font.name-list.sans-serif.ar", "Noto Naskh Arabic, Roboto, Google Sans, Droid Sans");
