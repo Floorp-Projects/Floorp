d5efaffc38eb9de3070a4bef069922eba0eb0ee1
Kelsey Gilbert <kelsey.gilbert@mozilla.com>
2024-01-04 18:38:52 +0000

    Bug 1655101 - Allow webgl.texImage gpu-blit for RGB8 if supported. r=gfx-reviewers,aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D194943

--- dom/canvas/WebGLContext.h
+++ dom/canvas/WebGLContext.h
@@ -317,6 +317,7 @@ class WebGLContext : public VRefCounted, public SupportsWeakPtr {
                                      webgl::InitContextResult* out);
 
  private:
+  bool mIsRgb8Renderable = false;
   void FinishInit();
 
  protected:
