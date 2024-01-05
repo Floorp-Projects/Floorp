a3a90e0df932ac2e964119785cf31eb6041b8a49
Mark Banner <standard8@mozilla.com>
2024-01-05 09:22:35 +0000

    Bug 1864821 - Remove PromiseUtils.defer(). r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D197490

--- toolkit/modules/PromiseUtils.sys.mjs
+++ toolkit/modules/PromiseUtils.sys.mjs
@@ -3,16 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 export var PromiseUtils = {
-  /*
-   * Creates a new pending Promise and provide methods to resolve and reject this Promise.
-   *
-   * @return {Deferred} an object consisting of a pending Promise "promise"
-   * and methods "resolve" and "reject" to change its state.
-   */
-  defer() {
-    return new Deferred();
-  },
-
   /**
    * Requests idle dispatch to the main thread for the given callback,
    * and returns a promise which resolves to the callback's return value
@@ -37,35 +27,3 @@ export var PromiseUtils = {
     });
   },
 };
-
-/**
- * The definition of Deferred object which is returned by PromiseUtils.defer(),
- * It contains a Promise and methods to resolve/reject it.
- */
-function Deferred() {
-  /* A method to resolve the associated Promise with the value passed.
-   * If the promise is already settled it does nothing.
-   *
-   * @param {anything} value : This value is used to resolve the promise
-   * If the value is a Promise then the associated promise assumes the state
-   * of Promise passed as value.
-   */
-  this.resolve = null;
-
-  /* A method to reject the assocaited Promise with the value passed.
-   * If the promise is already settled it does nothing.
-   *
-   * @param {anything} reason: The reason for the rejection of the Promise.
-   * Generally its an Error object. If however a Promise is passed, then the Promise
-   * itself will be the reason for rejection no matter the state of the Promise.
-   */
-  this.reject = null;
-
-  /* A newly created Pomise object.
-   * Initially in pending state.
-   */
-  this.promise = new Promise((resolve, reject) => {
-    this.resolve = resolve;
-    this.reject = reject;
-  });
-}
