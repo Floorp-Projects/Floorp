4f7590dfd4322b17d0751ce4ca099579dfbab797
Dana Keeler <dkeeler@mozilla.com>
2024-01-05 17:00:36 +0000

    Bug 1871201 - remove expired data_storage.migration metric r=nkulatova
    The metric indicates migration has largely been successful. It can now be
    removed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D197709

--- security/manager/ssl/data_storage/src/lib.rs
+++ security/manager/ssl/data_storage/src/lib.rs
@@ -399,7 +399,6 @@ impl DataStorageInner {
     fn read_old_format(&mut self, path: PathBuf) -> Result<(), nsresult> {
         let file = File::open(path).map_err(|_| NS_ERROR_FAILURE)?;
         let reader = BufReader::new(file);
-        let mut migration_okay = true;
         // First read each line in the old file into the persistent slots list.
         // The old format was limited to 1024 lines, so only expect that many.
         for line in reader.lines().flatten().take(1024) {
@@ -407,19 +406,16 @@ impl DataStorageInner {
                 Ok(entry) => {
                     if self.persistent_slots.len() >= MAX_SLOTS {
                         warn!("too many lines in old DataStorage format");
-                        migration_okay = false;
                         break;
                     }
                     if !entry.is_empty() {
                         self.persistent_slots.push(entry);
                     } else {
                         warn!("empty entry in old DataStorage format?");
-                        migration_okay = false;
                     }
                 }
                 Err(_) => {
                     warn!("failed to migrate a line from old DataStorage format");
-                    migration_okay = false;
                 }
             }
         }
@@ -432,7 +428,6 @@ impl DataStorageInner {
             .collect();
         // Finally, write out the migrated data to the new backing file.
         self.async_write_entries(self.persistent_slots.clone())?;
-        data_storage::migration.get(&self.name).set(migration_okay);
         data_storage::entries
             .get(&self.name)
             .add(self.persistent_table.len() as i32);
