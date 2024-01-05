7c7669d705c82004713e57d88f2d2f216630a98a
Olivia Hall <ohall@mozilla.com>
2024-01-05 14:42:35 +0000

    Bug 1871203 - Removing Expiring dotprint GeckoView Telemetry r=geckoview-reviewers,amejiamarmol
    This bug removes telemetry expiring in 124 that was for GeckoView's
    use of `window.print()`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D197732

--- mobile/android/actors/GeckoViewPrintDelegateParent.sys.mjs
+++ mobile/android/actors/GeckoViewPrintDelegateParent.sys.mjs
@@ -24,51 +24,8 @@ export class GeckoViewPrintDelegateParent extends GeckoViewActorParent {
     this.browserStaticClone = null;
   }
 
-  telemetryDotPrintRequested() {
-    Glean.dotprint.requested.add(1);
-  }
-
-  telemetryDotPrintPdfCompleted(status) {
-    if (status.isPdfSuccessful) {
-      Glean.dotprint.androidDialogRequested.add(1);
-    } else {
-      var reason = "";
-      switch (status.errorReason) {
-        // ERROR_PRINT_SETTINGS_SERVICE_NOT_AVAILABLE
-        case -1: {
-          reason = "no_settings_service";
-          break;
-        }
-        // ERROR_UNABLE_TO_CREATE_PRINT_SETTINGS
-        case -2: {
-          reason = "no_settings";
-          break;
-        }
-        // ERROR_UNABLE_TO_RETRIEVE_CANONICAL_BROWSING_CONTEXT
-        case -3: {
-          reason = "no_canonical_context";
-          break;
-        }
-        // ERROR_NO_ACTIVITY_CONTEXT_DELEGATE
-        case -4: {
-          reason = "no_activity_context_delegate";
-          break;
-        }
-        // ERROR_NO_ACTIVITY_CONTEXT
-        case -5: {
-          reason = "no_activity_context";
-          break;
-        }
-        default:
-          reason = "unknown";
-      }
-      Glean.dotprint.failure[reason].add(1);
-    }
-  }
-
   printRequest() {
     if (this.browserStaticClone != null) {
-      this.telemetryDotPrintRequested();
       this.eventDispatcher.sendRequest({
         type: "GeckoView:DotPrintRequest",
         canonicalBrowsingContextId: this.browserStaticClone.browsingContext.id,
