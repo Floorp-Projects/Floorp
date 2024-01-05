40a35c6952c6ed9128b9fcc785761ae4721b7b77
Jon Coppeard <jcoppeard@mozilla.com>
2024-01-05 14:55:24 +0000

    Bug 1872982 - Part 1: Use more sepecific types in InnerViewTable r=jandem
    To make it clearer what's going on this replaces JSObject with
    ArrayBufferObject and ArrayBufferViewObject where possible and does some other
    tidy ups.
    
    Differential Revision: https://phabricator.services.mozilla.com/D197775

--- js/src/vm/ArrayBufferObject.h
+++ js/src/vm/ArrayBufferObject.h
@@ -551,12 +551,12 @@ ArrayBufferObjectMaybeShared* CreateWasmBuffer(JSContext* cx,
 // Per-compartment table that manages the relationship between array buffers
 // and the views that use their storage.
 class InnerViewTable {
- public:
-  using ViewVector = GCVector<UnsafeBarePtr<JSObject*>, 1, ZoneAllocPolicy>;
+  using ViewVector =
+      GCVector<UnsafeBarePtr<ArrayBufferViewObject*>, 1, ZoneAllocPolicy>;
 
-  friend class ArrayBufferObject;
-
- private:
+  // For all objects sharing their storage with some other view, this maps
+  // the object to the list of such views. All entries in this map are weak.
+  //
   // This key is a raw pointer and not a WeakHeapPtr because the post-barrier
   // would hold nursery-allocated entries live unconditionally. It is a very
   // common pattern in low-level and performance-oriented JavaScript to create
@@ -565,29 +565,23 @@ class InnerViewTable {
   // regression. Thus, it is vital that nursery pointers in this map not be held
   // live. Special support is required in the minor GC, implemented in
   // sweepAfterMinorGC.
-  using Map = GCHashMap<UnsafeBarePtr<JSObject*>, ViewVector,
-                        StableCellHasher<JSObject*>, ZoneAllocPolicy>;
-
-  // For all objects sharing their storage with some other view, this maps
-  // the object to the list of such views. All entries in this map are weak.
-  Map map;
+  using ArrayBufferViewMap =
+      GCHashMap<UnsafeBarePtr<ArrayBufferObject*>, ViewVector,
+                StableCellHasher<JSObject*>, ZoneAllocPolicy>;
+  ArrayBufferViewMap map;
 
   // List of keys from innerViews where either the source or at least one
   // target is in the nursery. The raw pointer to a JSObject is allowed here
   // because this vector is cleared after every minor collection. Users in
   // sweepAfterMinorCollection must be careful to use MaybeForwarded before
   // touching these pointers.
-  Vector<JSObject*, 0, SystemAllocPolicy> nurseryKeys;
+  Vector<ArrayBufferObject*, 0, SystemAllocPolicy> nurseryKeys;
 
   // Whether nurseryKeys is a complete list.
-  bool nurseryKeysValid;
-
-  bool addView(JSContext* cx, ArrayBufferObject* buffer, JSObject* view);
-  ViewVector* maybeViewsUnbarriered(ArrayBufferObject* obj);
-  void removeViews(ArrayBufferObject* obj);
+  bool nurseryKeysValid = true;
 
  public:
-  explicit InnerViewTable(Zone* zone) : map(zone), nurseryKeysValid(true) {}
+  explicit InnerViewTable(Zone* zone) : map(zone) {}
 
   // Remove references to dead objects in the table and update table entries
   // to reflect moved objects.
@@ -601,6 +595,13 @@ class InnerViewTable {
   }
 
   size_t sizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf);
+
+ private:
+  friend class ArrayBufferObject;
+  bool addView(JSContext* cx, ArrayBufferObject* buffer,
+               ArrayBufferViewObject* view);
+  ViewVector* maybeViewsUnbarriered(ArrayBufferObject* buffer);
+  void removeViews(ArrayBufferObject* buffer);
 };
 
 template <typename Wrapper>
