a6f1ca049a99b2bdacffe71bb440b0be08dab26d
Matthew Gaudet <mgaudet@mozilla.com>
2024-01-08 17:30:16 +0000

    Bug 1870931 - Mark non262/TypedArray/large-arrays.js as slow r=iain
    Takes 45s locally for --jitflags=jstests
    
    Depends on D196917
    
    Differential Revision: https://phabricator.services.mozilla.com/D196918

--- js/src/tests/non262/TypedArray/large-arrays.js
+++ js/src/tests/non262/TypedArray/large-arrays.js
@@ -1,4 +1,4 @@
-// |reftest| skip-if(!xulRuntime.shell)
+// |reftest| slow skip-if(!xulRuntime.shell)
 
 // Test that we can access TypedArrays beyond the 4GB mark, if large buffers are
 // supported.
