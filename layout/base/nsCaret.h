a4d781eb8a1313cdc90ec578280c9e60effc5731
Sean Feng <sefeng@mozilla.com>
2024-01-10 17:03:38 +0000

    Bug 1860328 - Fix a bug where caret misses when dragging and dropping r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D192377

--- layout/base/nsCaret.h
+++ layout/base/nsCaret.h
@@ -12,6 +12,7 @@
 #include "mozilla/MemoryReporting.h"
 #include "mozilla/dom/Selection.h"
 #include "nsCoord.h"
+#include "nsIFrame.h"
 #include "nsISelectionListener.h"
 #include "nsIWeakReferenceUtils.h"
 #include "nsPoint.h"
@@ -196,6 +197,10 @@ class nsCaret final : public nsISelectionListener {
 
   size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf) const;
 
+  void SetLastCaretFrame(nsIFrame* aLastCaretFrame) {
+    mLastCaretFrame = aLastCaretFrame;
+  }
+
  protected:
   static void CaretBlinkCallback(nsITimer* aTimer, void* aClosure);
 
@@ -279,6 +284,11 @@ class nsCaret final : public nsISelectionListener {
    * it's in non-user-modifiable content.
    */
   bool mIgnoreUserModify;
+
+  /**
+   * mLastCaretFrame is the last caret frame that was scheduled to paint.
+   */
+  WeakFrame mLastCaretFrame;
 };
 
 #endif  // nsCaret_h__
