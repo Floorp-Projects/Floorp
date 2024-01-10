955777580b78e9db72aa0f72a74b22e6e35bb1ac
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2024-01-10 17:13:31 -0500

    Bug 1874076 - Update AndroidX Lifecycle to version 2.7.0

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -79,7 +79,7 @@ object Versions {
         const val paging = "3.2.1"
         const val palette = "1.0.0"
         const val preferences = "1.2.1"
-        const val lifecycle = "2.6.2"
+        const val lifecycle = "2.7.0"
         const val media = "1.7.0"
         const val navigation = "2.5.3"
         const val work = "2.7.1"
