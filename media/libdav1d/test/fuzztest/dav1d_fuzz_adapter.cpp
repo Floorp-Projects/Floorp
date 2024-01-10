a212702708ab277751a41ecb2e28db90205386ae
Jesse Schwartzentruber <truber@mozilla.com>
2024-01-10 18:15:24 +0000

    Bug 1873745 - Fix Dav1d libFuzzer target r=decoder
    Differential Revision: https://phabricator.services.mozilla.com/D198177

--- media/libdav1d/test/fuzztest/dav1d_fuzz_adapter.cpp
+++ media/libdav1d/test/fuzztest/dav1d_fuzz_adapter.cpp
@@ -1,9 +1,15 @@
 
 #include "FuzzingInterface.h"
 
-namespace dav1dfuzz {
+#define LLVMFuzzerInitialize Dav1dFuzzerInitialize
+#define LLVMFuzzerTestOneInput Dav1dFuzzerTestOneInput
+
+extern "C" {
 #include "tests/libfuzzer/dav1d_fuzzer.c"
 }
 
-MOZ_FUZZING_INTERFACE_RAW(nullptr, dav1dfuzz::LLVMFuzzerTestOneInput,
+#undef LLVMFuzzerInitialize
+#undef LLVMFuzzerTestOneInput
+
+MOZ_FUZZING_INTERFACE_RAW(Dav1dFuzzerInitialize, Dav1dFuzzerTestOneInput,
                           Dav1dDecode);
