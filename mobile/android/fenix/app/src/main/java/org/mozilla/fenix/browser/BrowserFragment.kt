d81c3616b8a727ce19966178608d2fe5ce7e6cfd
Harrison Oglesby <oglesby.harrison@gmail.com>
2024-01-11 16:08:40 -0800

    Bug 1874298 - Add Reload Page action to PageActions in Toolbar

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/BrowserFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/BrowserFragment.kt
@@ -166,6 +166,7 @@ class BrowserFragment : BaseBrowserFragment(), UserInteractionHandler {
         initTranslationsAction(context, view)
         initSharePageAction(context)
         initReviewQualityCheck(context, view)
+        initReloadAction(context)
 
         thumbnailsFeature.set(
             feature = BrowserThumbnails(context, binding.engineView, components.core.store),
@@ -346,6 +347,49 @@ class BrowserFragment : BaseBrowserFragment(), UserInteractionHandler {
                 owner = this,
                 view = view,
             )
+        }
+    }
+
+    private fun initReloadAction(context: Context) {
+        if (!IncompleteRedesignToolbarFeature(context.settings()).isEnabled || refreshAction != null) {
+            return
+        }
+
+        refreshAction =
+            BrowserToolbar.TwoStateButton(
+                primaryImage = AppCompatResources.getDrawable(
+                    context,
+                    R.drawable.mozac_ic_arrow_clockwise_24,
+                )!!,
+                primaryContentDescription = context.getString(R.string.browser_menu_refresh),
+                primaryImageTintResource = ThemeManager.resolveAttribute(R.attr.textPrimary, context),
+                isInPrimaryState = {
+                    getCurrentTab()?.content?.loading == false
+                },
+                secondaryImage = AppCompatResources.getDrawable(
+                    context,
+                    R.drawable.mozac_ic_stop,
+                )!!,
+                secondaryContentDescription = context.getString(R.string.browser_menu_stop),
+                disableInSecondaryState = false,
+                longClickListener = {
+                    browserToolbarInteractor.onBrowserToolbarMenuItemTapped(
+                        ToolbarMenu.Item.Reload(bypassCache = true),
+                    )
+                },
+                listener = {
+                    if (getCurrentTab()?.content?.loading == true) {
+                        browserToolbarInteractor.onBrowserToolbarMenuItemTapped(ToolbarMenu.Item.Stop)
+                    } else {
+                        browserToolbarInteractor.onBrowserToolbarMenuItemTapped(
+                            ToolbarMenu.Item.Reload(bypassCache = false),
+                        )
+                    }
+                },
+            )
+
+        refreshAction?.let {
+            browserToolbarView.view.addPageAction(it)
         }
     }
 
