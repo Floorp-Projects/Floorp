94c4d5c6b3e2b80de4f90b83db2c5822e5a40d8e
asthetik <asthetiklee@163.com>
2024-01-13 10:41:31 +0000

    Bug 1868293 - Fix clippy warnings. r=sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D198488

--- toolkit/components/glean/api/src/ffi/event.rs
+++ toolkit/components/glean/api/src/ffi/event.rs
@@ -59,7 +59,6 @@ pub extern "C" fn fog_event_record(
             Some(m) => m.record_raw(extra),
             None => panic!("No (dynamic) metric for event with id {}", id),
         }
-        return;
     } else {
         match metric_maps::record_event_by_id(id, extra) {
             Ok(()) => {}
@@ -148,7 +147,7 @@ pub extern "C" fn fog_event_test_get_value(
     };
 
     for event in events {
-        let extra = event.extra.unwrap_or_else(HashMap::new);
+        let extra = event.extra.unwrap_or_else_default();
         let extra_len = extra.len();
         let mut extras = ThinVec::with_capacity(extra_len * 2);
         for (k, v) in extra.into_iter() {
