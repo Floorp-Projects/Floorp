6d88d8328fdf0d6e763e28910a49b5ae53ab77ec
Karl Tomlinson <karlt+@karlt.net>
2024-01-15 22:15:30 +0000

    Bug 1873806 add a second pulseaudio null sink with --use-test-media-devices r=padenot,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D198133

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -904,29 +904,7 @@ def findTestMediaDevices(log):
     )
     info["video"] = {"name": name, "process": process}
 
-    # check if PulseAudio module-null-sink is loaded
-    pactl = spawn.find_executable("pactl")
-
-    if not pactl:
-        log.error("Could not find pactl on system")
-        return None
-
-    try:
-        o = subprocess.check_output([pactl, "list", "short", "modules"])
-    except subprocess.CalledProcessError:
-        log.error("Could not list currently loaded modules")
-        return None
-
-    null_sink = [x for x in o.splitlines() if b"module-null-sink" in x]
-
-    if not null_sink:
-        try:
-            subprocess.check_call([pactl, "load-module", "module-null-sink"])
-        except subprocess.CalledProcessError:
-            log.error("Could not load module-null-sink")
-            return None
-
-    # Hardcode the name since it's always the same.
+    # Hardcode the PulseAudio module-null-sink name since it's always the same.
     info["audio"] = {"name": "Monitor of Null Output"}
     return info
 
@@ -2598,14 +2576,14 @@ toolbar#nav-bar {
                 )
         options.manifestFile = None
 
-        if hasattr(self, "virtualInputDeviceIdList"):
+        if hasattr(self, "virtualDeviceIdList"):
             pactl = spawn.find_executable("pactl")
 
             if not pactl:
                 self.log.error("Could not find pactl on system")
                 return None
 
-            for id in self.virtualInputDeviceIdList:
+            for id in self.virtualDeviceIdList:
                 try:
                     subprocess.check_call([pactl, "unload-module", str(id)])
                 except subprocess.CalledProcessError:
@@ -2614,7 +2592,7 @@ toolbar#nav-bar {
                     )
                     return None
 
-            self.virtualInputDeviceIdList = []
+            self.virtualDeviceIdList = []
 
     def dumpScreen(self, utilityPath):
         if self.haveDumpedScreen:
@@ -3068,10 +3046,11 @@ toolbar#nav-bar {
         options.manifestFile = None
         options.profilePath = None
 
-    def initializeVirtualInputDevices(self):
+    def initializeVirtualAudioDevices(self):
         """
-        Configure the system to have a number of virtual audio input devices, that
-        each produce a tone at a particular frequency.
+        Configure the system to have a number of virtual audio devices:
+        2 output devices, and
+        4 input devices that each produce a tone at a particular frequency.
 
         This method is only currently implemented for Linux.
         """
@@ -3084,34 +3063,62 @@ toolbar#nav-bar {
             self.log.error("Could not find pactl on system")
             return
 
-        DEVICES_COUNT = 4
-        DEVICES_BASE_FREQUENCY = 110  # Hz
-        self.virtualInputDeviceIdList = []
-        # If the device are already present, find their id and return early
-        o = subprocess.check_output([pactl, "list", "modules", "short"])
-        found_devices = 0
-        for input in o.splitlines():
-            device = input.decode().split("\t")
-            if device[1] == "module-sine-source":
-                self.virtualInputDeviceIdList.append(int(device[0]))
-                found_devices += 1
+        def getModuleIds(moduleName):
+            o = subprocess.check_output([pactl, "list", "modules", "short"])
+            list = []
+            for input in o.splitlines():
+                device = input.decode().split("\t")
+                if device[1] == moduleName:
+                    list.append(int(device[0]))
+            return list
 
-        if found_devices == DEVICES_COUNT:
+        OUTPUT_DEVICES_COUNT = 2
+        INPUT_DEVICES_COUNT = 4
+        DEVICES_BASE_FREQUENCY = 110  # Hz
+        # If the device are already present, find their id and return early
+        outputDeviceIdList = getModuleIds("module-null-sink")
+        inputDeviceIdList = getModuleIds("module-sine-source")
+
+        if (
+            len(outputDeviceIdList) == OUTPUT_DEVICES_COUNT
+            and len(inputDeviceIdList) == INPUT_DEVICES_COUNT
+        ):
+            self.virtualDeviceIdList = outputDeviceIdList + inputDeviceIdList
             return
-        elif found_devices != 0:
-            # Remove all devices and reinitialize them properly
-            for id in self.virtualInputDeviceIdList:
+        else:
+            # Remove any existing devices and reinitialize properly
+            for id in outputDeviceIdList + inputDeviceIdList:
                 try:
                     subprocess.check_call([pactl, "unload-module", str(id)])
                 except subprocess.CalledProcessError:
                     log.error("Could not remove pulse module with id {}".format(id))
                     return None
 
+        idList = []
+        command = [pactl, "load-module", "module-null-sink"]
+        try:  # device for "media.audio_loopback_dev" pref
+            o = subprocess.check_output(command + ["rate=44100"])
+            idList.append(int(o))
+        except subprocess.CalledProcessError:
+            self.log.error("Could not load module-null-sink")
+
+        try:
+            o = subprocess.check_output(
+                command
+                + [
+                    "rate=48000",
+                    "sink_properties='device.description=\"48000 Hz Null Output\"'",
+                ]
+            )
+            idList.append(int(o))
+        except subprocess.CalledProcessError:
+            self.log.error("Could not load module-null-sink at rate=48000")
+
         # We want quite a number of input devices, each with a different tone
         # frequency and device name so that we can recognize them easily during
         # testing.
         command = [pactl, "load-module", "module-sine-source", "rate=44100"]
-        for i in range(1, DEVICES_COUNT + 1):
+        for i in range(1, INPUT_DEVICES_COUNT + 1):
             freq = i * DEVICES_BASE_FREQUENCY
             complete_command = command + [
                 "source_name=sine-{}".format(freq),
@@ -3119,13 +3126,15 @@ toolbar#nav-bar {
             ]
             try:
                 o = subprocess.check_output(complete_command)
-                self.virtualInputDeviceIdList.append(o)
+                idList.append(int(o))
 
             except subprocess.CalledProcessError:
                 self.log.error(
                     "Could not create device with module-sine-source"
                     " (freq={})".format(freq)
                 )
+
+        self.virtualDeviceIdList = idList
 
     def normalize_paths(self, paths):
         # Normalize test paths so they are relative to test root
@@ -3594,12 +3603,12 @@ toolbar#nav-bar {
             )
 
         if options.useTestMediaDevices:
+            self.initializeVirtualAudioDevices()
             devices = findTestMediaDevices(self.log)
             if not devices:
                 self.log.error("Could not find test media devices to use")
                 return 1
             self.mediaDevices = devices
-            self.initializeVirtualInputDevices()
 
         # See if we were asked to run on Valgrind
         valgrindPath = None
