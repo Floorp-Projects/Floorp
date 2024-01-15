e85b019e9faca58f4be28d008837b3cf92314eed
Emilio Cobos Álvarez <emilio@crisal.io>
2024-01-15 15:03:19 +0000

    Bug 1874701 - Convert aboutNetError to an html page. r=Gijs,valentin
    While I'm looking at this / bug 1866884, this it probably makes sense,
    wdyt?
    
    Differential Revision: https://phabricator.services.mozilla.com/D198571

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -53,7 +53,7 @@ static const RedirEntry kRedirMap[] = {
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::URI_CAN_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
          nsIAboutModule::HIDE_FROM_ABOUTABOUT},
-    {"certerror", "chrome://global/content/aboutNetError.xhtml",
+    {"certerror", "chrome://global/content/aboutNetError.html",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::URI_CAN_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
          nsIAboutModule::HIDE_FROM_ABOUTABOUT},
