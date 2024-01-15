319ea438438ad64d2296080fbef330b94afb098d
André Bargull <andre.bargull@gmail.com>
2024-01-15 12:18:56 +0000

    Bug 1870251 - Part 12: Add parent class to Instant and InstantSpan. r=allstarschh
    Add a parent class to avoid code duplication. In a future update for normalized
    time duration, another class will also inherit from `SecondsAndNanoseconds`.
    
    Depends on D196569
    
    Differential Revision: https://phabricator.services.mozilla.com/D196570

--- js/src/builtin/temporal/TemporalTypes.h
+++ js/src/builtin/temporal/TemporalTypes.h
@@ -35,34 +35,23 @@ namespace js::temporal {
     } while (false)
 #endif
 
-struct InstantSpan;
-
 /**
- * Instant represents a time since the epoch value, measured in nanoseconds.
- *
- * Instant supports a range of ±8.64 × 10^21 nanoseconds, covering ±10^8 days
- * in either direction relative to midnight at the beginning of 1 January 1970
- * UTC. The range also exactly matches the supported range of JavaScript Date
- * objects.
- *
- * C++ doesn't provide a built-in type capable of storing an integer in the
- * range ±8.64 × 10^21, therefore we need to create our own abstraction. This
- * struct follows the design of `std::timespec` and splits the instant into a
- * signed seconds part and an unsigned nanoseconds part.
+ * Struct to represent a seconds and nanoseconds value. The nanoseconds value
+ * is normalized to the range [0, 999'999'999].
  */
-struct Instant final {
-  // Seconds part in the range [-8'640'000'000'000, +8'640'000'000'000] for
-  // valid epoch instant values.
+template <typename Derived>
+struct SecondsAndNanoseconds {
+  // Seconds part in the range allowed by the derived class.
   int64_t seconds = 0;
 
   // Nanoseconds part in the range [0, 999'999'999].
   int32_t nanoseconds = 0;
 
-  bool operator==(const Instant& other) const {
+  constexpr bool operator==(const SecondsAndNanoseconds& other) const {
     return seconds == other.seconds && nanoseconds == other.nanoseconds;
   }
 
-  bool operator<(const Instant& other) const {
+  constexpr bool operator<(const SecondsAndNanoseconds& other) const {
     // The compiler can optimize expressions like |instant < Instant{}| to a
     // single right-shift operation when we propagate the range of nanoseconds.
     JS_ASSUME(nanoseconds >= 0);
@@ -72,182 +61,31 @@ struct Instant final {
   }
 
   // Other operators are implemented in terms of operator== and operator<.
-  bool operator!=(const Instant& other) const { return !(*this == other); }
-  bool operator>(const Instant& other) const { return other < *this; }
-  bool operator<=(const Instant& other) const { return !(other < *this); }
-  bool operator>=(const Instant& other) const { return !(*this < other); }
-
-  inline Instant& operator+=(const InstantSpan& other);
-  inline Instant& operator-=(const InstantSpan& other);
-
-  inline Instant operator+(const InstantSpan& other) const;
-  inline Instant operator-(const InstantSpan& other) const;
-
-  inline InstantSpan operator-(const Instant& other) const;
-
-  /**
-   * Return this instant as seconds from the start of the epoch. (Rounds towards
-   * zero.)
-   */
-  int64_t toSeconds() const {
-    int64_t sec = seconds;
-    int64_t nanos = nanoseconds;
-    if (sec < 0 && nanos > 0) {
-      sec += 1;
-    }
-    return sec;
+  constexpr bool operator!=(const SecondsAndNanoseconds& other) const {
+    return !(*this == other);
+  }
+  constexpr bool operator>(const SecondsAndNanoseconds& other) const {
+    return other < *this;
+  }
+  constexpr bool operator<=(const SecondsAndNanoseconds& other) const {
+    return !(other < *this);
+  }
+  constexpr bool operator>=(const SecondsAndNanoseconds& other) const {
+    return !(*this < other);
   }
 
-  /**
-   * Return this instant as milliseconds from the start of the epoch. (Rounds
-   * towards zero.)
-   */
-  int64_t toMilliseconds() const {
-    int64_t sec = seconds;
-    int64_t nanos = nanoseconds;
-    if (sec < 0 && nanos > 0) {
-      sec += 1;
-      nanos -= 1'000'000'000;
-    }
-    return (sec * 1'000) + (nanos / 1'000'000);
-  }
-
-  /**
-   * Return this instant as microseconds from the start of the epoch. (Rounds
-   * towards zero.)
-   */
-  int64_t toMicroseconds() const {
-    int64_t sec = seconds;
-    int64_t nanos = nanoseconds;
-    if (sec < 0 && nanos > 0) {
-      sec += 1;
-      nanos -= 1'000'000'000;
-    }
-    return (sec * 1'000'000) + (nanos / 1'000);
-  }
-
-  /**
-   * Return this instant as nanoseconds from the start of the epoch.
-   *
-   * The returned nanoseconds amount can be invalid on overflow. The caller is
-   * responsible for handling the overflow case.
-   */
-  mozilla::CheckedInt64 toNanoseconds() const {
-    mozilla::CheckedInt64 nanos = seconds;
-    nanos *= ToNanoseconds(TemporalUnit::Second);
-    nanos += nanoseconds;
-    return nanos;
-  }
-
-  /**
-   * Return this instant as microseconds from the start of the epoch. (Rounds
-   * towards negative infinity.)
-   */
-  int64_t floorToMicroseconds() const {
-    return (seconds * 1'000'000) + (nanoseconds / 1'000);
-  }
-
-  /**
-   * Return this instant as milliseconds from the start of the epoch. (Rounds
-   * towards negative infinity.)
-   */
-  int64_t floorToMilliseconds() const {
-    return (seconds * 1'000) + (nanoseconds / 1'000'000);
-  }
-
-  /**
-   * Return this instant as milliseconds from the start of the epoch. (Rounds
-   * towards positive infinity.)
-   */
-  int64_t ceilToMilliseconds() const {
-    return floorToMilliseconds() + int64_t(nanoseconds % 1'000'000 != 0);
-  }
-
-  /**
-   * Create an instant from a seconds from the epoch value.
-   */
-  static constexpr Instant fromSeconds(int64_t seconds) { return {seconds, 0}; }
-
-  /**
-   * Create an instant from a milliseconds from the epoch value.
-   */
-  static constexpr Instant fromMilliseconds(int64_t milliseconds) {
-    int64_t seconds = milliseconds / 1'000;
-    int32_t millis = milliseconds % 1'000;
-    if (millis < 0) {
-      seconds -= 1;
-      millis += 1'000;
-    }
-    return {seconds, millis * 1'000'000};
-  }
-
-  /**
-   * Create an instant from a microseconds from the epoch value.
-   */
-  static constexpr Instant fromMicroseconds(int64_t microseconds) {
-    int64_t seconds = microseconds / 1'000'000;
-    int32_t micros = microseconds % 1'000'000;
-    if (micros < 0) {
-      seconds -= 1;
-      micros += 1'000'000;
-    }
-    return {seconds, micros * 1'000};
-  }
-
-  /**
-   * Create an instant from a nanoseconds from the epoch value.
-   */
-  static constexpr Instant fromNanoseconds(int64_t nanoseconds) {
-    int64_t seconds = nanoseconds / 1'000'000'000;
-    int32_t nanos = nanoseconds % 1'000'000'000;
-    if (nanos < 0) {
-      seconds -= 1;
-      nanos += 1'000'000'000;
-    }
-    return {seconds, nanos};
-  }
-};
-
-/**
- * InstantSpan represents a span of time between two Instants, measured in
- * nanoseconds.
- */
-struct InstantSpan final {
-  // Seconds part in the range [2 × -8'640'000'000'000, 2 × +8'640'000'000'000]
-  // for valid epoch instant span values.
-  int64_t seconds = 0;
-
-  // Nanoseconds part in the range [0, 999'999'999].
-  int32_t nanoseconds = 0;
-
-  bool operator==(const InstantSpan& other) const {
-    return seconds == other.seconds && nanoseconds == other.nanoseconds;
-  }
-
-  bool operator<(const InstantSpan& other) const {
-    // The compiler can optimize expressions like |instant < InstantSpan{}| to a
-    // single right-shift operation when we propagate the range of nanoseconds.
-    JS_ASSUME(nanoseconds >= 0);
-    JS_ASSUME(other.nanoseconds >= 0);
-    return (seconds < other.seconds) ||
-           (seconds == other.seconds && nanoseconds < other.nanoseconds);
-  }
-
-  // Other operators are implemented in terms of operator== and operator<.
-  bool operator!=(const InstantSpan& other) const { return !(*this == other); }
-  bool operator>(const InstantSpan& other) const { return other < *this; }
-  bool operator<=(const InstantSpan& other) const { return !(other < *this); }
-  bool operator>=(const InstantSpan& other) const { return !(*this < other); }
-
-  InstantSpan& operator+=(const InstantSpan& other) {
+ protected:
+  template <typename T, typename U, class R = Derived>
+  static constexpr R add(const SecondsAndNanoseconds<T>& self,
+                         const SecondsAndNanoseconds<U>& other) {
     // The caller needs to make sure integer overflow won't happen. CheckedInt
     // will assert on overflow and we intentionally don't try to recover from
     // overflow in this method.
 
-    mozilla::CheckedInt64 secs = seconds;
+    mozilla::CheckedInt64 secs = self.seconds;
     secs += other.seconds;
 
-    mozilla::CheckedInt32 nanos = nanoseconds;
+    mozilla::CheckedInt32 nanos = self.nanoseconds;
     nanos += other.nanoseconds;
 
     if (nanos.value() >= 1'000'000'000) {
@@ -256,20 +94,20 @@ struct InstantSpan final {
     }
     MOZ_ASSERT(0 <= nanos.value() && nanos.value() < 1'000'000'000);
 
-    seconds = secs.value();
-    nanoseconds = nanos.value();
-    return *this;
+    return {secs.value(), nanos.value()};
   }
 
-  InstantSpan& operator-=(const InstantSpan& other) {
+  template <class T, class U, class R = Derived>
+  static constexpr R subtract(const SecondsAndNanoseconds<T>& self,
+                              const SecondsAndNanoseconds<U>& other) {
     // The caller needs to make sure integer underflow won't happen. CheckedInt
     // will assert on underflow and we intentionally don't try to recover from
     // underflow in this method.
 
-    mozilla::CheckedInt64 secs = seconds;
+    mozilla::CheckedInt64 secs = self.seconds;
     secs -= other.seconds;
 
-    mozilla::CheckedInt32 nanos = nanoseconds;
+    mozilla::CheckedInt32 nanos = self.nanoseconds;
     nanos -= other.nanoseconds;
 
     if (nanos.value() < 0) {
@@ -278,27 +116,18 @@ struct InstantSpan final {
     }
     MOZ_ASSERT(0 <= nanos.value() && nanos.value() < 1'000'000'000);
 
-    seconds = secs.value();
-    nanoseconds = nanos.value();
-    return *this;
+    return {secs.value(), nanos.value()};
   }
 
-  InstantSpan operator+(const InstantSpan& other) const {
-    auto result = *this;
-    result += other;
-    return result;
+  static constexpr Derived negate(const Derived& self) {
+    return subtract(Derived{}, self);
   }
 
-  InstantSpan operator-(const InstantSpan& other) const {
-    auto result = *this;
-    result -= other;
-    return result;
-  }
-
+ public:
   /**
-   * Return the absolute value of this instant span.
+   * Return the absolute value.
    */
-  InstantSpan abs() const {
+  constexpr Derived abs() const {
     int64_t sec = seconds;
     int32_t nanos = nanoseconds;
     if (sec < 0) {
@@ -313,12 +142,50 @@ struct InstantSpan final {
   }
 
   /**
-   * Return this instant span as nanoseconds.
+   * Return the seconds value, rounded towards zero.
+   */
+  constexpr int64_t toSeconds() const {
+    int64_t sec = seconds;
+    int32_t nanos = nanoseconds;
+    if (sec < 0 && nanos > 0) {
+      sec += 1;
+    }
+    return sec;
+  }
+
+  /**
+   * Return the milliseconds value, rounded towards zero.
+   */
+  constexpr int64_t toMilliseconds() const {
+    int64_t sec = seconds;
+    int32_t nanos = nanoseconds;
+    if (sec < 0 && nanos > 0) {
+      sec += 1;
+      nanos -= 1'000'000'000;
+    }
+    return (sec * 1'000) + (nanos / 1'000'000);
+  }
+
+  /**
+   * Return the microseconds value, rounded towards zero.
+   */
+  constexpr int64_t toMicroseconds() const {
+    int64_t sec = seconds;
+    int32_t nanos = nanoseconds;
+    if (sec < 0 && nanos > 0) {
+      sec += 1;
+      nanos -= 1'000'000'000;
+    }
+    return (sec * 1'000'000) + (nanos / 1'000);
+  }
+
+  /**
+   * Return the nanoseconds value.
    *
    * The returned nanoseconds amount can be invalid on overflow. The caller is
    * responsible for handling the overflow case.
    */
-  mozilla::CheckedInt64 toNanoseconds() const {
+  constexpr mozilla::CheckedInt64 toNanoseconds() const {
     mozilla::CheckedInt64 nanos = seconds;
     nanos *= ToNanoseconds(TemporalUnit::Second);
     nanos += nanoseconds;
@@ -326,16 +193,21 @@ struct InstantSpan final {
   }
 
   /**
-   * Create an instant span from a minutes value.
+   * Create from a minutes value.
    */
-  static constexpr InstantSpan fromMinutes(int64_t minutes) {
+  static constexpr Derived fromMinutes(int64_t minutes) {
     return {minutes * 60, 0};
   }
 
   /**
-   * Create an instant span from a milliseconds value.
+   * Create from a seconds value.
    */
-  static constexpr InstantSpan fromMilliseconds(int64_t milliseconds) {
+  static constexpr Derived fromSeconds(int64_t seconds) { return {seconds, 0}; }
+
+  /**
+   * Create from a milliseconds value.
+   */
+  static constexpr Derived fromMilliseconds(int64_t milliseconds) {
     int64_t seconds = milliseconds / 1'000;
     int32_t millis = milliseconds % 1'000;
     if (millis < 0) {
@@ -346,9 +218,22 @@ struct InstantSpan final {
   }
 
   /**
-   * Create an instant span from a nanoseconds value.
+   * Create from a microseconds value.
    */
-  static constexpr InstantSpan fromNanoseconds(int64_t nanoseconds) {
+  static constexpr Derived fromMicroseconds(int64_t microseconds) {
+    int64_t seconds = microseconds / 1'000'000;
+    int32_t micros = microseconds % 1'000'000;
+    if (micros < 0) {
+      seconds -= 1;
+      micros += 1'000'000;
+    }
+    return {seconds, micros * 1'000};
+  }
+
+  /**
+   * Create from a nanoseconds value.
+   */
+  static constexpr Derived fromNanoseconds(int64_t nanoseconds) {
     int64_t seconds = nanoseconds / 1'000'000'000;
     int32_t nanos = nanoseconds % 1'000'000'000;
     if (nanos < 0) {
@@ -359,69 +244,126 @@ struct InstantSpan final {
   }
 };
 
-Instant& Instant::operator+=(const InstantSpan& other) {
-  // The caller needs to make sure integer overflow won't happen. CheckedInt
-  // will assert on overflow and we intentionally don't try to recover from
-  // overflow in this method.
-
-  mozilla::CheckedInt64 secs = seconds;
-  secs += other.seconds;
-
-  mozilla::CheckedInt32 nanos = nanoseconds;
-  nanos += other.nanoseconds;
-
-  if (nanos.value() >= 1'000'000'000) {
-    secs += 1;
-    nanos -= 1'000'000'000;
-  }
-  MOZ_ASSERT(0 <= nanos.value() && nanos.value() < 1'000'000'000);
-
-  seconds = secs.value();
-  nanoseconds = nanos.value();
-  return *this;
-}
-
-Instant& Instant::operator-=(const InstantSpan& other) {
-  // The caller needs to make sure integer underflow won't happen. CheckedInt
-  // will assert on underflow and we intentionally don't try to recover from
-  // underflow in this method.
-
-  mozilla::CheckedInt64 secs = seconds;
-  secs -= other.seconds;
-
-  mozilla::CheckedInt32 nanos = nanoseconds;
-  nanos -= other.nanoseconds;
-
-  if (nanos.value() < 0) {
-    secs -= 1;
-    nanos += 1'000'000'000;
-  }
-  MOZ_ASSERT(0 <= nanos.value() && nanos.value() < 1'000'000'000);
-
-  seconds = secs.value();
-  nanoseconds = nanos.value();
-  return *this;
-}
-
-Instant Instant::operator+(const InstantSpan& other) const {
-  auto result = *this;
-  result += other;
-  return result;
-}
-
-Instant Instant::operator-(const InstantSpan& other) const {
-  auto result = *this;
-  result -= other;
-  return result;
-}
-
-InstantSpan Instant::operator-(const Instant& other) const {
-  InstantSpan result{seconds, nanoseconds};
-  result -= InstantSpan{other.seconds, other.nanoseconds};
-  return result;
-}
-
 #undef JS_ASSUME
+
+/**
+ * InstantSpan represents a span of time between two Instants, measured in
+ * nanoseconds.
+ */
+struct InstantSpan final : SecondsAndNanoseconds<InstantSpan> {
+  constexpr InstantSpan& operator+=(const InstantSpan& other) {
+    *this = add(*this, other);
+    return *this;
+  }
+
+  constexpr InstantSpan& operator-=(const InstantSpan& other) {
+    *this = subtract(*this, other);
+    return *this;
+  }
+
+  constexpr InstantSpan operator+(const InstantSpan& other) const {
+    return add(*this, other);
+  }
+
+  constexpr InstantSpan operator-(const InstantSpan& other) const {
+    return subtract(*this, other);
+  }
+
+  constexpr InstantSpan operator-() const { return negate(*this); }
+
+  /**
+   * Returns the maximum instant span value.
+   */
+  static constexpr InstantSpan max() {
+    // The limit is 2×8.64 × 10^21 nanoseconds, which is 2×8.64 × 10^12 seconds.
+    constexpr int64_t seconds = 2 * 8'640'000'000'000;
+    constexpr int64_t nanos = 0;
+    return {seconds, nanos};
+  }
+
+  /**
+   * Returns the minimum instant span value.
+   */
+  static constexpr InstantSpan min() { return -max(); }
+};
+
+/**
+ * Instant represents a time since the epoch value, measured in nanoseconds.
+ *
+ * Instant supports a range of ±8.64 × 10^21 nanoseconds, covering ±10^8 days
+ * in either direction relative to midnight at the beginning of 1 January 1970
+ * UTC. The range also exactly matches the supported range of JavaScript Date
+ * objects.
+ *
+ * C++ doesn't provide a built-in type capable of storing an integer in the
+ * range ±8.64 × 10^21, therefore we need to create our own abstraction. This
+ * struct follows the design of `std::timespec` and splits the instant into a
+ * signed seconds part and an unsigned nanoseconds part.
+ */
+struct Instant final : SecondsAndNanoseconds<Instant> {
+  constexpr Instant& operator+=(const InstantSpan& other) {
+    *this = add(*this, other);
+    return *this;
+  }
+
+  constexpr Instant& operator-=(const InstantSpan& other) {
+    *this = subtract(*this, other);
+    return *this;
+  }
+
+  constexpr Instant operator+(const InstantSpan& other) const {
+    return add(*this, other);
+  }
+
+  constexpr Instant operator-(const InstantSpan& other) const {
+    return subtract(*this, other);
+  }
+
+  constexpr InstantSpan operator-(const Instant& other) const {
+    return subtract<Instant, Instant, InstantSpan>(*this, other);
+  }
+
+  constexpr Instant operator-() const { return negate(*this); }
+
+  /**
+   * Return this instant as microseconds from the start of the epoch. (Rounds
+   * towards negative infinity.)
+   */
+  constexpr int64_t floorToMicroseconds() const {
+    return (seconds * 1'000'000) + (nanoseconds / 1'000);
+  }
+
+  /**
+   * Return this instant as milliseconds from the start of the epoch. (Rounds
+   * towards negative infinity.)
+   */
+  constexpr int64_t floorToMilliseconds() const {
+    return (seconds * 1'000) + (nanoseconds / 1'000'000);
+  }
+
+  /**
+   * Return this instant as milliseconds from the start of the epoch. (Rounds
+   * towards positive infinity.)
+   */
+  constexpr int64_t ceilToMilliseconds() const {
+    return floorToMilliseconds() + int64_t(nanoseconds % 1'000'000 != 0);
+  }
+
+  /**
+   * Returns the maximum instant value.
+   */
+  static constexpr Instant max() {
+    // The limit is 8.64 × 10^21 nanoseconds, which is 8.64 × 10^12 seconds.
+    constexpr int64_t seconds = 8'640'000'000'000;
+    constexpr int64_t nanos = 0;
+    return {seconds, nanos};
+  }
+
+  /**
+   * Returns the minimum instant value.
+   */
+  static constexpr Instant min() { return -max(); }
+};
 
 /**
  * Plain date represents a date in the ISO 8601 calendar.
