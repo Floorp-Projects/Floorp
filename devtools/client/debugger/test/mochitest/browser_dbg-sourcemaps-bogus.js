ab15a5dca1c59ec78469af1377ee6795ddf50779
Alexandre Poirot <poirot.alex@gmail.com>
2024-01-15 12:57:19 +0000

    Bug 1834725 - [devtools] Surface source map errors in Debugger frontend. r=devtools-reviewers,nchevobbe
    I'm introducing a new dedicated method on SourceMapLoader so that we can:
    * ensure retrieving the resolved source map URL, even in case of errors
      so that we can easily open the source map file from the frontend.
    * pass all useful data in one worker call (ignore urls + sources + error + resolve source map url)
    
    Display the source map error in a new dedicated warning footer in the bottom of the editor pane.
    
    (Removing failing jest test. Mochitests should cover the source maps.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D188586

--- devtools/client/debugger/test/mochitest/browser_dbg-sourcemaps-bogus.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-sourcemaps-bogus.js
@@ -20,12 +20,18 @@ add_task(async function () {
     "doc-sourcemap-bogus.html",
     "non-existant-map.js",
     "map-with-failed-original-request.js",
-    "map-with-failed-original-request.original.js"
+    "map-with-failed-original-request.original.js",
+    "invalid-json-map.js"
   );
   // Make sure there is only the expected sources and we miss some original sources.
-  is(dbg.selectors.getSourceCount(), 3, "Only 3 source exists");
+  is(dbg.selectors.getSourceCount(), 4, "Only 4 source exists");
 
   await selectSource(dbg, "non-existant-map.js");
+  is(
+    findFooterNotificationMessage(dbg),
+    "Source Map Error: request failed with status 404",
+    "There is a warning about the missing source map file"
+  );
 
   // We should still be able to set breakpoints and pause in the
   // generated source.
@@ -39,7 +45,20 @@ add_task(async function () {
   );
   await resume(dbg);
 
+  // Test a Source Map with invalid JSON
+  await selectSource(dbg, "invalid-json-map.js");
+  is(
+    findFooterNotificationMessage(dbg),
+    "Source Map Error: JSON.parse: expected property name or '}' at line 2 column 3 of the JSON data",
+    "There is a warning about the missing source map file"
+  );
+
+  // Test a Source Map with missing original text content
   await selectSource(dbg, "map-with-failed-original-request.js");
+  ok(
+    !findElement(dbg, "editorNotificationFooter"),
+    "The source-map is valid enough to not spawn a warning message"
+  );
   await addBreakpoint(dbg, "map-with-failed-original-request.js", 7);
   invokeInTab("changeStyleAttribute");
   await waitForPaused(dbg);
@@ -54,6 +73,15 @@ add_task(async function () {
   // The original file is visible in the source tree and can be selected,
   // but its content can't be displayed
   await selectSource(dbg, "map-with-failed-original-request.original.js");
+  const notificationMessage = DEBUGGER_L10N.getFormatStr(
+    "editorNotificationFooter.noOriginalScopes",
+    DEBUGGER_L10N.getStr("scopes.showOriginalScopes")
+  );
+  is(
+    findFooterNotificationMessage(dbg),
+    notificationMessage,
+    "There is no warning about source-map but rather one about original scopes"
+  );
   is(
     getCM(dbg).getValue(),
     `Error while fetching an original source: request failed with status 404\nSource URL: ${EXAMPLE_URL}map-with-failed-original-request.original.js`
