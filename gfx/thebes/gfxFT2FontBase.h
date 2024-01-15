0d2d3455fc254d6bf1046d856166722845697665
Jonathan Kew <jkew@mozilla.com>
2024-01-15 09:14:09 +0000

    Bug 1873708 - patch 1 - Synthesize fallback glyph extents for color glyphs if FreeType returned empty bounds, to avoid clipped rendering. r=gfx-reviewers,aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D198278

--- gfx/thebes/gfxFT2FontBase.h
+++ gfx/thebes/gfxFT2FontBase.h
@@ -80,7 +80,7 @@ class gfxFT2FontBase : public gfxFont {
   // Get advance (and optionally bounds) of a single glyph from FreeType,
   // and return true, or return false if we failed.
   bool GetFTGlyphExtents(uint16_t aGID, int32_t* aWidth,
-                         mozilla::gfx::IntRect* aBounds = nullptr) const;
+                         mozilla::gfx::IntRect* aBounds = nullptr);
 
  protected:
   ~gfxFT2FontBase() override;
@@ -143,10 +143,10 @@ class gfxFT2FontBase : public gfxFont {
   };
 
   const GlyphMetrics& GetCachedGlyphMetrics(
-      uint16_t aGID, mozilla::gfx::IntRect* aBounds = nullptr) const;
+      uint16_t aGID, mozilla::gfx::IntRect* aBounds = nullptr);
 
-  mutable mozilla::UniquePtr<nsTHashMap<nsUint32HashKey, GlyphMetrics>>
-      mGlyphMetrics MOZ_GUARDED_BY(mLock);
+  mozilla::UniquePtr<nsTHashMap<nsUint32HashKey, GlyphMetrics>> mGlyphMetrics
+      MOZ_GUARDED_BY(mLock);
 };
 
 #endif /* GFX_FT2FONTBASE_H */
