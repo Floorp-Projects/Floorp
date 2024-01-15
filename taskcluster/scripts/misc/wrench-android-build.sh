c572ae79882c88d63825e1b598f16e39ca7a0bea
Mike Hommey <mh+mozilla@glandium.org>
2024-01-15 22:28:28 +0000

    Bug 1874281 - Switch cargo-apk to a toolchain task. r=firefox-build-system-reviewers,ahochheiden
    Differential Revision: https://phabricator.services.mozilla.com/D198340

--- taskcluster/scripts/misc/wrench-android-build.sh
+++ taskcluster/scripts/misc/wrench-android-build.sh
@@ -4,7 +4,7 @@ set -x -e -v
 MODE=${1?"First argument must be debug|release"}
 
 pushd "${MOZ_FETCHES_DIR}"
-mv wrench-deps/{vendor,.cargo,cargo-apk} ${GECKO_PATH}/gfx/wr
+mv wrench-deps/{vendor,.cargo} ${GECKO_PATH}/gfx/wr
 popd
 
 pushd "${GECKO_PATH}/gfx/wr/wrench"
@@ -16,9 +16,9 @@ export ANDROID_SDK_ROOT="${MOZ_FETCHES_DIR}/android-sdk-linux"
 export ANDROID_NDK_ROOT="${MOZ_FETCHES_DIR}/android-ndk"
 
 if [ "$MODE" == "debug" ]; then
-    ../cargo-apk/bin/cargo-apk apk build --frozen --verbose --lib
+    $MOZ_FETCHES_DIR/cargo-apk/cargo-apk apk build --frozen --verbose --lib
 elif [ "$MODE" == "release" ]; then
-    ../cargo-apk/bin/cargo-apk apk build --frozen --verbose --lib --release
+    $MOZ_FETCHES_DIR/cargo-apk/cargo-apk apk build --frozen --verbose --lib --release
 else
     echo "Unknown mode '${MODE}'; must be 'debug' or 'release'"
     exit 1
--- taskcluster/scripts/misc/wrench-deps-vendoring.sh
+++ taskcluster/scripts/misc/wrench-deps-vendoring.sh
@@ -15,12 +15,6 @@ mkdir .cargo
 cargo vendor --locked --sync ./Cargo.toml > .cargo/config
 mkdir wrench-deps
 mv vendor .cargo wrench-deps/
-mkdir wrench-deps/cargo-apk
-# Until there's a version of cargo-apk published on crates.io that has
-# https://github.com/rust-windowing/android-ndk-rs/pull/236, we need to use
-# an unpublished version. Additionally, until we update the NDK version used
-# in gecko we must use our own patched version. See bug 1615148.
-cargo install --path $MOZ_FETCHES_DIR/android-ndk-rs/cargo-apk --root wrench-deps/cargo-apk cargo-apk
 
 ci-scripts/install-meson.sh
 mv meson wrench-deps/meson
