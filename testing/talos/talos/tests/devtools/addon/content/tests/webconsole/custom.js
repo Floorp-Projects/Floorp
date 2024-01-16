48ba67d5a44cad0be5cb4259c4641f6c0be4abd7
Alexandre Poirot <poirot.alex@gmail.com>
2024-01-16 22:40:53 +0000

    Bug 1874696 - [devtools] Better cover webconsole when reaching the maximum number of logged messages (10k). r=devtools-reviewers,perftest-reviewers,nchevobbe,sparky
    DAMP tests weren't logging enough messages to reach the maximum number of displayed messages,
    which is currently set to 10k. Some extra computation happens to prune old, removed messages
    which appear to be sometime slow.
    
    Also log many messages during the batch iteration so that we removes lots of messages
    on each new console update.
    
    Differential Revision: https://phabricator.services.mozilla.com/D198570

--- testing/talos/talos/tests/devtools/addon/content/tests/webconsole/custom.js
+++ testing/talos/talos/tests/devtools/addon/content/tests/webconsole/custom.js
@@ -21,26 +21,41 @@ const TEST_URL = PAGES_BASE_URL + "custom/console/index.html";
 
 module.exports = async function () {
   // These numbers controls the number of console api calls we do in the test
-  const sync = 500,
-    stream = 250,
-    batch = 500,
-    simple = 5000;
+  // Use 10k to reach the visible limit of messages reducer when logging sync (which is significantly faster than strem/batch)
+  // Use limited number of async logging (stream/batch) as that is slow
+  const sync = 10000,
+    // Stream uses request animation frame while batch uses idle request
+    // and stream only log one message on each iteration
+    stream = 100,
+    // Number of batch sent after idle
+    batch = 10,
+    // Number of messages emitted on each batch iteration
+    batchSize = 500;
 
-  const params = `?sync=${sync}&stream=${stream}&batch=${batch}&simple=${simple}`;
+  const params = `?sync=${sync}&stream=${stream}&batch=${batch}&batchSize=${batchSize}`;
   const url = TEST_URL + params;
   await testSetup(url, { disableCache: true });
 
   const toolbox = await openToolboxAndLog("custom.webconsole", "webconsole");
+  const { hud } = toolbox.getPanel("webconsole");
+  // Wait for very last async messages to show up
+  await waitForConsoleOutputChildListChange(hud, consoleOutput => {
+    const messages = consoleOutput.querySelectorAll(".message-body");
+    return (
+      messages &&
+      messages[messages.length - 1]?.textContent.includes("very last message")
+    );
+  });
   // With virtualization, we won't have all the messages rendered in the DOM, so we only
-  // wait for the last message to be displayed ("simple log 4999").
+  // wait for the last message to be displayed ("batch log 249").
   await reloadConsoleAndLog("custom", toolbox, [
     {
-      text: "simple log " + (simple - 1),
+      text: "very last message",
     },
   ]);
 
   dump("Clear console\n");
-  const { hud } = toolbox.getPanel("webconsole");
+
   const onMessageCleared = waitForConsoleOutputChildListChange(
     hud,
     consoleOutput => consoleOutput.querySelector(".message-body") == null
