9678f5caab98856ab51eef900a25eed84879c16f
Nicolas Chevobbe <nchevobbe@mozilla.com>
2024-01-16 13:44:51 +0000

    Bug 1874363 - [devtools] Remove build-debugger source-editor  mapping. r=devtools-reviewers,ochameau.
    Also remove devtools/client/debugger/src/utils/editor/source-editor.js which wasn't
    used an look like a relic of ancient times.
    
    Differential Revision: https://phabricator.services.mozilla.com/D198409

--- devtools/client/debugger/src/utils/editor/tests/create-editor.spec.js
+++ devtools/client/debugger/src/utils/editor/tests/create-editor.spec.js
@@ -3,23 +3,20 @@
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
 import { createEditor } from "../create-editor";
-import SourceEditor from "../source-editor";
 
 import { features } from "../../prefs";
 
 describe("createEditor", () => {
-  test("Returns a SourceEditor", () => {
+  test("SourceEditor default config", () => {
     const editor = createEditor();
-    expect(editor).toBeInstanceOf(SourceEditor);
-    expect(editor.opts).toMatchSnapshot();
-    expect(editor.opts.gutters).not.toContain("CodeMirror-foldgutter");
+    expect(editor.config).toMatchSnapshot();
+    expect(editor.config.gutters).not.toContain("CodeMirror-foldgutter");
   });
 
   test("Adds codeFolding", () => {
     features.codeFolding = true;
     const editor = createEditor();
-    expect(editor).toBeInstanceOf(SourceEditor);
-    expect(editor.opts).toMatchSnapshot();
-    expect(editor.opts.gutters).toContain("CodeMirror-foldgutter");
+    expect(editor.config).toMatchSnapshot();
+    expect(editor.config.gutters).toContain("CodeMirror-foldgutter");
   });
 });
