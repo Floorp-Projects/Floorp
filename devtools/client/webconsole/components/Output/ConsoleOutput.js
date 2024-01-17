a92d67ff42da1c7e7ca7f678ff3c37febf996285
Alexandre Poirot <poirot.alex@gmail.com>
2024-01-17 15:10:46 +0000

    Bug 1873066 - [devtools] Avoid some computation when receiving new messages. r=devtools-reviewers,nchevobbe
    The "is in opening group" computation, which goes through all message arrays
    can be avoided until it is meaningful to check for this particular edge case.
    Also we can remove the redundant check against `nextProps.messagesUi`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D197770

--- devtools/client/webconsole/components/Output/ConsoleOutput.js
+++ devtools/client/webconsole/components/Output/ConsoleOutput.js
@@ -164,13 +164,6 @@ class ConsoleOutput extends Component {
     const bottomBuffer = this.lazyMessageListRef.current.bottomBuffer;
     this.scrollDetectionIntersectionObserver.unobserve(bottomBuffer);
 
-    // We need to scroll to the bottom if:
-    // - we are reacting to "initialize" action, and we are already scrolled to the bottom
-    // - the number of messages displayed changed and we are already scrolled to the
-    //   bottom, but not if we are reacting to a group opening.
-    // - the number of messages in the store changed and the new message is an evaluation
-    //   result.
-
     const visibleMessagesDelta =
       nextProps.visibleMessages.length - this.props.visibleMessages.length;
     const messagesDelta = nextProps.messageCount - this.props.messageCount;
@@ -182,24 +175,34 @@ class ConsoleOutput extends Component {
       nextProps.mutableMessages.get(nextProps.lastMessageId)?.type ===
         MESSAGE_TYPE.RESULT;
 
-    const messagesUiDelta =
-      nextProps.messagesUi.length - this.props.messagesUi.length;
-    const isOpeningGroup =
-      messagesUiDelta > 0 &&
-      nextProps.messagesUi.some(
-        id =>
-          !this.props.messagesUi.includes(id) &&
-          nextProps.messagesUi.includes(id) &&
-          this.props.visibleMessages.includes(id) &&
-          nextProps.visibleMessages.includes(id)
+    // Use an inline function in order to avoid executing the expensive Array.some()
+    // unless condition are meant to do this additional check.
+    const isOpeningGroup = () => {
+      const messagesUiDelta =
+        nextProps.messagesUi.length - this.props.messagesUi.length;
+      return (
+        messagesUiDelta > 0 &&
+        nextProps.messagesUi.some(
+          id =>
+            !this.props.messagesUi.includes(id) &&
+            this.props.visibleMessages.includes(id) &&
+            nextProps.visibleMessages.includes(id)
+        )
       );
+    };
 
+    // We need to scroll to the bottom if:
     this.shouldScrollBottom =
+      // - we are reacting to "initialize" action, and we are already scrolled to the bottom
       (!this.props.initialized &&
         nextProps.initialized &&
         this.scrolledToBottom) ||
+      // - the number of messages in the store changed and the new message is an evaluation
+      //   result.
       isNewMessageEvaluationResult ||
-      (this.scrolledToBottom && visibleMessagesDelta > 0 && !isOpeningGroup);
+      // - the number of messages displayed changed and we are already scrolled to the
+      //   bottom, but not if we are reacting to a group opening.
+      (this.scrolledToBottom && visibleMessagesDelta > 0 && !isOpeningGroup());
   }
 
   componentDidUpdate(prevProps) {
