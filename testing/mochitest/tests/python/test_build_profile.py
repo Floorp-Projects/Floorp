f39ef9fc577c89e9523d8a64a1a9840e79cec379
Gregory Pappas <gp3033@protonmail.com>
2024-01-18 11:51:13 +0000

    Bug 1724089 - Remove browser.tabs.remote.autostart pref r=webdriver-reviewers,perftest-reviewers,geckoview-reviewers,jgraham,extension-reviewers,mossop,sparky,robwu,owlish,necko-reviewers,jmaher,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D192136

--- testing/mochitest/tests/python/test_build_profile.py
+++ testing/mochitest/tests/python/test_build_profile.py
@@ -43,10 +43,7 @@ def profile_data_dir():
 
 
 def test_common_prefs_are_all_set(build_profile, profile_data_dir):
-    # We set e10s=False here because MochitestDesktop.buildProfile overwrites
-    # the value defined in the base profile.
-    # TODO stop setting browser.tabs.remote.autostart in the base profile
-    md, result = build_profile(e10s=False)
+    md, result = build_profile()
 
     with open(os.path.join(profile_data_dir, "profiles.json"), "r") as fh:
         base_profiles = json.load(fh)["mochitest"]
