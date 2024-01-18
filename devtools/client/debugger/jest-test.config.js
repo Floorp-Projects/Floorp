25eadf5ed20c98874b5a9b9a7678d6e1af3c0a60
Nicolas Chevobbe <nchevobbe@mozilla.com>
2024-01-18 07:00:40 +0000

    Bug 1875029 - [devtools] Cleanup debugger jest-test.config.js. r=devtools-reviewers,bomsy.
    Depends on D198774
    
    Differential Revision: https://phabricator.services.mozilla.com/D198779

--- devtools/client/debugger/jest-test.config.js
+++ devtools/client/debugger/jest-test.config.js
@@ -8,44 +8,21 @@
 
 const sharedJestConfig = require(`${__dirname}/../shared/test-helpers/shared-jest.config`);
 
-const { resolve } = require("path");
-const rootDir = resolve(__dirname);
 module.exports = {
-  rootDir,
-  displayName: "test",
-  testURL: "http://localhost/",
   testEnvironment: "jsdom",
   testPathIgnorePatterns: [
     "/node_modules/",
     "/helpers/",
     "/fixtures/",
     "src/test/mochitest/examples/",
-    "<rootDir>/firefox",
     "package.json",
   ],
   modulePathIgnorePatterns: ["test/mochitest"],
-  collectCoverageFrom: [
-    "src/**/*.js",
-    "!src/**/fixtures/*.js",
-    "!src/test/**/*.js",
-    "!src/components/stories/**/*.js",
-    "!**/*.mock.js",
-    "!**/*.spec.js",
-  ],
-  transform: {
-    "\\.[jt]sx?$": "babel-jest",
-  },
-  transformIgnorePatterns: ["node_modules/(?!(devtools-|react-aria-))"],
   setupFilesAfterEnv: ["<rootDir>/src/test/tests-setup.js"],
   setupFiles: ["<rootDir>/src/test/shim.js", "jest-localstorage-mock"],
-  snapshotSerializers: [
-    "jest-serializer-babel-ast",
-    "enzyme-to-json/serializer",
-  ],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
   moduleNameMapper: {
     ...sharedJestConfig.moduleNameMapper,
-    "\\.css$": "<rootDir>/../shared/test-helpers/jest-fixtures/empty-module",
-    "\\.svg$": "<rootDir>/../shared/test-helpers/jest-fixtures/svgMock.js",
     "react-dom-factories": "<rootDir>/../shared/vendor/react-dom-factories.js",
   },
 };
