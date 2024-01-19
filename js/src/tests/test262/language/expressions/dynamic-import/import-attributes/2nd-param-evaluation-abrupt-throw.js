9a6c1e7f10c5e30207924bd3be9401fe420ff13b
Jonatan Klemets <jonatan.r.klemets@gmail.com>
2024-01-19 01:01:32 +0000

    Bug 1835669 - Update test262 r=mgaudet
    Depends on D194267
    
    Differential Revision: https://phabricator.services.mozilla.com/D194268

--- js/src/tests/test262/language/expressions/dynamic-import/import-attributes/2nd-param-evaluation-abrupt-throw.js
+++ js/src/tests/test262/language/expressions/dynamic-import/import-attributes/2nd-param-evaluation-abrupt-throw.js
@@ -1,4 +1,4 @@
-// |reftest| skip -- import-attributes is not supported
+// |reftest| shell-option(--enable-import-attributes) skip-if(!xulRuntime.shell) -- requires shell-options
 // Copyright (C) 2021 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
