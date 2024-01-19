03c30fdfcc02a31543ea5735640599506162c618
Nika Layzell <nika@thelayzells.com>
2024-01-19 20:23:20 +0000

    Bug 1874739 - Part 7: Make PRemoteWorkerController refcounted, r=dom-worker-reviewers,janv,asuth
    This is part of removing [ManualDealloc] from all protocols which manage other
    protocols.
    
    Differential Revision: https://phabricator.services.mozilla.com/D198617

--- dom/serviceworkers/ServiceWorkerPrivate.cpp
+++ dom/serviceworkers/ServiceWorkerPrivate.cpp
@@ -1156,14 +1156,6 @@ nsresult ServiceWorkerPrivate::SpawnWorkerIfNeeded() {
     return NS_ERROR_DOM_INVALID_STATE_ERR;
   }
 
-  /**
-   * Manually `AddRef()` because `DeallocPRemoteWorkerControllerChild()`
-   * calls `Release()` and the `AllocPRemoteWorkerControllerChild()` function
-   * is not called.
-   */
-  // NOLINTNEXTLINE(readability-redundant-smartptr-get)
-  controllerChild.get()->AddRef();
-
   mControllerChild = new RAIIActorPtrHolder(controllerChild.forget());
 
   // Update Running count here because we may Terminate before we get
