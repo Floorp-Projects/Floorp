599cb0b8ad4e8ddff6b60c46262ecd5826dab5e0
Steve Fink <sfink@mozilla.com>
2024-01-20 00:02:11 +0000

    Bug 1874785 - Add JSRuntime::atExit(func, data) for registering cleanup function to be called during destruction r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D198743

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -18,6 +18,7 @@
 #include "mozilla/XorShift128PlusRNG.h"
 
 #include <algorithm>
+#include <utility>
 
 #ifdef JS_HAS_INTL_API
 #  include "builtin/intl/SharedIntlData.h"
@@ -665,8 +666,19 @@ struct JSRuntime {
   /* Code coverage output. */
   js::UnprotectedData<js::coverage::LCovRuntime> lcovOutput_;
 
+  /* Functions to call, together with data, when the runtime is being torn down.
+   */
+  js::MainThreadData<mozilla::Vector<std::pair<void (*)(void*), void*>, 4>>
+      cleanupClosures;
+
  public:
   js::coverage::LCovRuntime& lcovOutput() { return lcovOutput_.ref(); }
+
+  /* Register a cleanup function to be called during runtime shutdown. Do not
+   * depend on the ordering of cleanup calls. */
+  bool atExit(void (*function)(void*), void* data) {
+    return cleanupClosures.ref().append(std::pair(function, data));
+  }
 
  private:
   js::UnprotectedData<js::jit::JitRuntime*> jitRuntime_;
