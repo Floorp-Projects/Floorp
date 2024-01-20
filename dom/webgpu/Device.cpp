479581f823d395c7887e3c941b33639809988158
Nicolas Silva <nsilva@mozilla.com>
2024-01-20 01:18:57 +0000

    Bug 1873047 - Generate a valid BindGroup object on the JS side even if IPC is down. r=webgpu-reviewers,ErichDonGubler
    Depends on D198383
    
    Differential Revision: https://phabricator.services.mozilla.com/D198384

--- dom/webgpu/Device.cpp
+++ dom/webgpu/Device.cpp
@@ -405,10 +405,48 @@ already_AddRefed<PipelineLayout> Device::CreatePipelineLayout(
 }
 already_AddRefed<BindGroup> Device::CreateBindGroup(
     const dom::GPUBindGroupDescriptor& aDesc) {
-  RawId id = 0;
-  if (mBridge->CanSend()) {
-    id = mBridge->DeviceCreateBindGroup(mId, aDesc);
+  nsTArray<ffi::WGPUBindGroupEntry> entries(aDesc.mEntries.Length());
+  for (const auto& entry : aDesc.mEntries) {
+    ffi::WGPUBindGroupEntry e = {};
+    e.binding = entry.mBinding;
+    if (entry.mResource.IsGPUBufferBinding()) {
+      const auto& bufBinding = entry.mResource.GetAsGPUBufferBinding();
+      if (!bufBinding.mBuffer->mId) {
+        NS_WARNING("Buffer binding has no id -- ignoring.");
+        continue;
+      }
+      e.buffer = bufBinding.mBuffer->mId;
+      e.offset = bufBinding.mOffset;
+      e.size = bufBinding.mSize.WasPassed() ? bufBinding.mSize.Value() : 0;
+    } else if (entry.mResource.IsGPUTextureView()) {
+      e.texture_view = entry.mResource.GetAsGPUTextureView()->mId;
+    } else if (entry.mResource.IsGPUSampler()) {
+      e.sampler = entry.mResource.GetAsGPUSampler()->mId;
+    } else {
+      // Not a buffer, nor a texture view, nor a sampler. If we pass
+      // this to wgpu_client, it'll panic. Log a warning instead and
+      // ignore this entry.
+      NS_WARNING("Bind group entry has unknown type.");
+      continue;
+    }
+    entries.AppendElement(e);
   }
+
+  ffi::WGPUBindGroupDescriptor desc = {};
+
+  webgpu::StringHelper label(aDesc.mLabel);
+  desc.label = label.Get();
+  desc.layout = aDesc.mLayout->mId;
+  desc.entries = entries.Elements();
+  desc.entries_length = entries.Length();
+
+  ipc::ByteBuf bb;
+  RawId id = ffi::wgpu_client_create_bind_group(mBridge->GetClient(), mId,
+                                                &desc, ToFFI(&bb));
+  if (mBridge->CanSend()) {
+    mBridge->SendDeviceAction(mId, std::move(bb));
+  }
+
   RefPtr<BindGroup> object = new BindGroup(this, id);
   return object.forget();
 }
