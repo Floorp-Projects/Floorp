5690905a5a80183164d5d5ee4770e5c789aab2eb
Perry McManis <pmcmanis@mozilla.com>
2024-01-22 15:45:27 +0000

    Bug 1874923 - Update Glean to 56.1.0 r=janerik,supply-chain-reviewers,mach-reviewers,ahochheiden
    Differential Revision: https://phabricator.services.mozilla.com/D199093

--- third_party/rust/glean-core/src/lib_unit_tests.rs
+++ third_party/rust/glean-core/src/lib_unit_tests.rs
@@ -1077,11 +1077,13 @@ fn records_database_file_size() {
     let dir = tempfile::tempdir().unwrap();
     let tmpname = dir.path().display().to_string();
 
-    // Initialize Glean once to ensure we create the database.
+    // Initialize Glean once to ensure we create the database and did not error.
     let glean = Glean::with_options(&tmpname, GLOBAL_APPLICATION_ID, true);
     let database_size = &glean.database_metrics.size;
     let data = database_size.get_value(&glean, "metrics");
+
     assert!(data.is_none());
+
     drop(glean);
 
     // Initialize Glean again to record file size.
@@ -1094,6 +1096,11 @@ fn records_database_file_size() {
 
     // We should see the database containing some data.
     assert!(data.sum > 0);
+
+    let rkv_load_state = &glean.database_metrics.rkv_load_error;
+    let rkv_load_error = rkv_load_state.get_value(&glean, "metrics");
+
+    assert_eq!(rkv_load_error, None);
 }
 
 #[cfg(not(target_os = "windows"))]
