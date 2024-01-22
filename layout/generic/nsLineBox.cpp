e88b0806b63b9f2ef3f7e6bc2e2c13e22776e3df
Ting-Yu Lin <tlin@mozilla.com>
2024-01-22 19:50:48 +0000

    Bug 1865012 Part 2 - Add nsFrameList::RemoveLastChild, and destroy frame list in reverse order. r=dholbert
    With only the Part 1, `layout/generic/crashtests/1683126.html` will timeout.
    It can be reproduced locally via
    
    ```
    ./mach crashtest layout/generic/crashtests/1683126.html
    ```
    
    It is because if we destroy a frame list from the first child to the last child,
    each time we destroy the first child [1], the second child become the new first
    child, and we have to update the first-in-flow and first-continuation cache for
    later continuations, which is a linear time operation.
    
    [1] See nsSplittableFrame::RemoveFromFlow() with the argument being the first
    child in a frame list.
    
    Differential Revision: https://phabricator.services.mozilla.com/D197967

--- layout/generic/nsLineBox.cpp
+++ layout/generic/nsLineBox.cpp
@@ -359,10 +359,8 @@ void nsLineBox::DeleteLineList(nsPresContext* aPresContext, nsLineList& aLines,
       line->SwitchToCounter();  // Avoid expensive has table removals.
     }
     while (line->GetChildCount() > 0) {
-      nsIFrame* child = aFrames->RemoveFirstChild();
+      nsIFrame* child = aFrames->RemoveLastChild();
       MOZ_DIAGNOSTIC_ASSERT(child->PresContext() == aPresContext);
-      MOZ_DIAGNOSTIC_ASSERT(child == line->mFirstChild, "Lines out of sync");
-      line->mFirstChild = aFrames->FirstChild();
       line->NoteFrameRemoved(child);
       child->Destroy(aContext);
     }
