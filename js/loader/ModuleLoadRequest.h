8d9ed15dd60c473a86ba933422a2ccaead3bb45f
Tooru Fujisawa <arai_a@mac.com>
2024-01-23 23:29:53 +0000

    Bug 1875836 - Part 1: Add ModuleLoadRequest::SetDynamicImport method. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D199278

--- js/loader/ModuleLoadRequest.h
+++ js/loader/ModuleLoadRequest.h
@@ -69,6 +69,10 @@ class ModuleLoadRequest final : public ScriptLoadRequest {
 
   void SetReady() override;
   void Cancel() override;
+
+  void SetDynamicImport(LoadedScript* aReferencingScript,
+                        JS::Handle<JSString*> aSpecifier,
+                        JS::Handle<JSObject*> aPromise);
   void ClearDynamicImport();
 
   void ModuleLoaded();
