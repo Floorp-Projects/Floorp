592c02dcee145c22214f3f20e134fc60c1b82679
Vincent Hilla <vhilla@mozilla.com>
2024-01-23 08:01:16 +0000

    Bug 1874881 - Remove pref dom.events.phases.correctOrderOnTarget. r=dom-core,edgar
    Differential Revision: https://phabricator.services.mozilla.com/D198691

--- dom/events/EventDispatcher.cpp
+++ dom/events/EventDispatcher.cpp
@@ -585,12 +585,7 @@ void EventTargetChainItem::HandleEventTargetChain(
   }
 
   // Target
-  const bool prefCorrectOrder =
-      StaticPrefs::dom_events_phases_correctOrderOnTarget();
   aVisitor.mEvent->mFlags.mInTargetPhase = true;
-  if (!prefCorrectOrder) {
-    aVisitor.mEvent->mFlags.mInBubblingPhase = true;
-  }
   EventTargetChainItem& targetItem = chain[firstCanHandleEventTargetIdx];
   // Need to explicitly retarget touch targets so that initial targets get set
   // properly in case nothing else retargeted touches.
@@ -602,23 +597,18 @@ void EventTargetChainItem::HandleEventTargetChain(
        targetItem.ForceContentDispatch())) {
     targetItem.HandleEvent(aVisitor, aCd);
   }
-  if (prefCorrectOrder) {
-    aVisitor.mEvent->mFlags.mInCapturePhase = false;
-    aVisitor.mEvent->mFlags.mInBubblingPhase = true;
-    if (!aVisitor.mEvent->PropagationStopped() &&
-        (!aVisitor.mEvent->mFlags.mNoContentDispatch ||
-         targetItem.ForceContentDispatch())) {
-      targetItem.HandleEvent(aVisitor, aCd);
-    }
+  aVisitor.mEvent->mFlags.mInCapturePhase = false;
+  aVisitor.mEvent->mFlags.mInBubblingPhase = true;
+  if (!aVisitor.mEvent->PropagationStopped() &&
+      (!aVisitor.mEvent->mFlags.mNoContentDispatch ||
+       targetItem.ForceContentDispatch())) {
+    targetItem.HandleEvent(aVisitor, aCd);
   }
 
   if (aVisitor.mEvent->mFlags.mInSystemGroup) {
     targetItem.PostHandleEvent(aVisitor);
   }
   aVisitor.mEvent->mFlags.mInTargetPhase = false;
-  if (!prefCorrectOrder) {
-    aVisitor.mEvent->mFlags.mInCapturePhase = false;
-  }
 
   // Bubble
   for (uint32_t i = firstCanHandleEventTargetIdx + 1; i < chainLength; ++i) {
