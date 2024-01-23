8b1a2404f7fcba1ac75d517636f6c40eab31aa72
mcarare <mcarare@mozilla.com>
2024-01-23 14:50:12 +0000

    Bug 1873677 - Upgrade Gradle to 8.5 version r=geckoview-reviewers,calu
    Gradle 8.5 is still compatible with AGP 7.4.2, so upgrading AGP can be done as a follow-up.
    
    Differential Revision: https://phabricator.services.mozilla.com/D198584

--- taskcluster/scripts/misc/android-gradle-dependencies/after.sh
+++ taskcluster/scripts/misc/android-gradle-dependencies/after.sh
@@ -20,12 +20,10 @@ cp -R ${NEXUS_WORK}/storage/gradle-plugins android-gradle-dependencies
 
 # The Gradle wrapper will have downloaded and verified the hash of exactly one
 # Gradle distribution.  It will be located in $GRADLE_USER_HOME, like
-# ~/.gradle/wrapper/dists/gradle-2.7-all/$PROJECT_HASH/gradle-2.7-all.zip.  We
+# ~/.gradle/wrapper/dists/gradle-8.5-bin/$PROJECT_HASH/gradle-8.5.  We
 # want to remove the version from the internal directory for use via tooltool in
 # a mozconfig.
-cp ${GRADLE_USER_HOME}/wrapper/dists/gradle-*-*/*/gradle-*-*.zip gradle.zip
-unzip -q gradle.zip
-mv gradle-* android-gradle-dependencies/gradle-dist
+cp -a ${GRADLE_USER_HOME}/wrapper/dists/gradle-*-*/*/gradle-*/ android-gradle-dependencies/gradle-dist
 
 tar cavf /builds/worker/artifacts/android-gradle-dependencies.tar.zst android-gradle-dependencies
 
