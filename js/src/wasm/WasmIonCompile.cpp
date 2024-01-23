7f95803c3b2a7bc7e513f3579a341856e811b4ab
Ryan Hunt <rhunt@eqrion.net>
2024-01-23 22:03:04 +0000

    Bug 1873776 - wasm: Fix Ion bug with throw_ref inside a try. r=jseward
    A throw_ref in Ion inside of a `try` or `try_table` would emit a call
    to Instance::throwException to set the pending exception and
    trigger the unwinding. It does this instead of using the Ion
    FunctionCompiler::throwFrom helper, because we don't have the
    tag of the exnref laying around, and getting it is difficult.
    
    However, Ion MIR generation has an invariant that function calls
    that throw generate a control flow graph edge to their nearest
    enclosing try. This is setup today for wasm calls, but not for
    wasm instance method calls. This commit updates wasm instance
    method calls to generate the right MIR graph.
    
    Differential Revision: https://phabricator.services.mozilla.com/D198093

--- js/src/wasm/WasmIonCompile.cpp
+++ js/src/wasm/WasmIonCompile.cpp
@@ -2399,14 +2399,30 @@ class FunctionCompiler {
     }
 
     CallSiteDesc desc(lineOrBytecode, CallSiteDesc::Symbolic);
+    MWasmCallTryDesc tryDesc;
+    if (!beginTryCall(&tryDesc)) {
+      return false;
+    }
+
     MInstruction* ins;
-    ins = MWasmCallUncatchable::NewBuiltinInstanceMethodCall(
-        alloc(), desc, builtin.identity, builtin.failureMode, call.instanceArg_,
-        call.regArgs_, StackArgAreaSizeUnaligned(builtin));
+    if (tryDesc.inTry) {
+      ins = MWasmCallCatchable::NewBuiltinInstanceMethodCall(
+          alloc(), desc, builtin.identity, builtin.failureMode,
+          call.instanceArg_, call.regArgs_, StackArgAreaSizeUnaligned(builtin),
+          tryDesc);
+    } else {
+      ins = MWasmCallUncatchable::NewBuiltinInstanceMethodCall(
+          alloc(), desc, builtin.identity, builtin.failureMode,
+          call.instanceArg_, call.regArgs_, StackArgAreaSizeUnaligned(builtin));
+    }
     if (!ins) {
       return false;
     }
     curBlock_->add(ins);
+
+    if (!finishTryCall(&tryDesc)) {
+      return false;
+    }
 
     if (!def) {
       return true;
@@ -3659,8 +3675,8 @@ class FunctionCompiler {
       return false;
     }
 
-    // If there is no surrounding catching block, call an instance method to
-    // throw the exception.
+    // Call Instance::throwException to perform tag unpacking and throw the
+    // exception
     if (!emitInstanceCall1(readBytecodeOffset(), SASigThrowException, exnRef)) {
       return false;
     }
