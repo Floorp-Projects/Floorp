ad430cbf577bac51cb128849ffb09dd5f74758b2
Anna Yeddi <ayeddi@mozilla.com>
2024-01-23 00:55:16 +0000

    Bug 1874290 - Add exceptions from a11y_checks for clicks on non-interactive chrome UI in Search and Address Bar. r=adw
    The `browser/components/urlbar/tests/browser-tips/browser_searchTips_interaction.js` and `browser/components/urlbar/tests/browser/browser_closePanelOnClick.js` tests send a click event on a non-interactive `<toolbarspring>` element to test that the URL Bar panel and a search tip would, in fact, be closed.
    
    For users of keyboards and assistive technology another way like pressing `Esc` key is available to dismiss the dialog, thus we need to add an exception from the a11y_checks for this tests via `setEnv` and remove the `fail-if` notations from their test manifests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D198411

--- browser/components/urlbar/tests/browser/browser_closePanelOnClick.js
+++ browser/components/urlbar/tests/browser/browser_closePanelOnClick.js
@@ -8,6 +8,23 @@
 
 "use strict";
 
+add_setup(function () {
+  // We intentionally turn off this a11y check, because the following
+  // clicks is purposefully targeting non-interactive elements to dismiss
+  // the opened URL Bar with a mouse which can be done by assistive
+  // technology and keyboard by pressing `Esc` key, this rule check shall
+  // be ignored by a11y_checks suite.
+  AccessibilityUtils.setEnv({ mustHaveAccessibleRule: false });
+
+  registerCleanupFunction(async () => {
+    // Usually, the AccessibilityUtils environment should be reset right after
+    // the click, but in this case there are no other testable interactions
+    // between iterations of the use case task besides those clicks that we are
+    // setting the environment with.
+    AccessibilityUtils.resetEnv();
+  });
+});
+
 add_task(async function () {
   await BrowserTestUtils.withNewTab("about:robots", async () => {
     for (let elt of [
