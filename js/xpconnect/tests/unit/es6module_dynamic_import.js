50184c6b2d9fd57dd0cd55e2a2b192111a88b164
Tooru Fujisawa <arai_a@mac.com>
2024-01-23 23:29:55 +0000

    Bug 1875836 - Part 3: Add tests. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D199280

--- js/xpconnect/tests/unit/es6module_dynamic_import.js
+++ js/xpconnect/tests/unit/es6module_dynamic_import.js
@@ -1,7 +1,25 @@
+import { getCounter, setCounter } from "./es6module_dynamic_import_static.js";
+
 let resolve;
 
 export const result = new Promise(r => { resolve = r; });
 
-import("./es6module_dynamic_import2.js").then(ns => {}, e => {
-  resolve(e);
+import("./es6module_dynamic_import2.js").then(ns => {
+  resolve(ns);
 });
+
+export function doImport() {
+  return import("./es6module_dynamic_import3.js");
+}
+
+export function callGetCounter() {
+  return getCounter();
+}
+
+export function callSetCounter(v) {
+  setCounter(v);
+}
+
+export function doImportStatic() {
+  return import("./es6module_dynamic_import_static.js");
+}
---
+++ js/xpconnect/tests/unit/es6module_dynamic_import_runtime_error.js
@@ -0,0 +1,17 @@
+let resolve;
+
+export const result = new Promise(r => { resolve = r; });
+
+import("./es6module_dynamic_import_runtime_error2.js").then(ns => {}, e => {
+  resolve(e);
+});
+
+export async function doImport() {
+  try {
+    await import("./es6module_dynamic_import_runtime_error3.js");
+  } catch (e) {
+    return e;
+  }
+
+  return null;
+}
