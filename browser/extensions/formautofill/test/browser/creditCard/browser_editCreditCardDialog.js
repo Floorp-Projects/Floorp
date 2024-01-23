e1473b6d11ed93b688f787eecb4a183024347eab
Micah Tigley <mtigley@mozilla.com>
2024-01-23 18:13:22 +0000

    Bug 1820287 - Use moz-button-group in the add/edit credit card dialog in about:preferences. r=hjones,credential-management-reviewers,desktop-theme-reviewers,mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D174069

--- browser/extensions/formautofill/test/browser/creditCard/browser_editCreditCardDialog.js
+++ browser/extensions/formautofill/test/browser/creditCard/browser_editCreditCardDialog.js
@@ -27,6 +27,7 @@ add_task(async function test_saveCreditCard() {
         .textContent.includes("Add"),
       "Add card dialog title is correct"
     );
+
     EventUtils.synthesizeKey("VK_TAB", {}, win);
     EventUtils.synthesizeKey(TEST_CREDIT_CARD_1["cc-number"], {}, win);
     EventUtils.synthesizeKey("VK_TAB", {}, win);
@@ -48,11 +49,16 @@ add_task(async function test_saveCreditCard() {
     );
     EventUtils.synthesizeKey("VK_TAB", {}, win);
     EventUtils.synthesizeKey(TEST_CREDIT_CARD_1["cc-name"], {}, win);
-    EventUtils.synthesizeKey("VK_TAB", {}, win);
-    EventUtils.synthesizeKey("VK_TAB", {}, win);
+    if (AppConstants.platform != "win") {
+      EventUtils.synthesizeKey("VK_TAB", {}, win);
+      EventUtils.synthesizeKey("VK_TAB", {}, win);
+    } else {
+      EventUtils.synthesizeKey("VK_TAB", {}, win);
+    }
     info("saving credit card");
     EventUtils.synthesizeKey("VK_RETURN", {}, win);
   });
+
   let creditCards = await getCreditCards();
 
   is(creditCards.length, 1, "only one credit card is in storage");
@@ -84,7 +90,12 @@ add_task(async function test_saveCreditCardWithMaxYear() {
     );
     EventUtils.synthesizeKey("VK_TAB", {}, win);
     EventUtils.synthesizeKey(TEST_CREDIT_CARD_2["cc-name"], {}, win);
-    EventUtils.synthesizeKey("VK_TAB", {}, win);
+    if (AppConstants.platform != "win") {
+      EventUtils.synthesizeKey("VK_TAB", {}, win);
+      EventUtils.synthesizeKey("VK_TAB", {}, win);
+    } else {
+      EventUtils.synthesizeKey("VK_TAB", {}, win);
+    }
     info("saving credit card");
     EventUtils.synthesizeKey("VK_RETURN", {}, win);
   });
@@ -129,7 +140,9 @@ add_task(async function test_saveCreditCardWithBillingAddress() {
     EventUtils.synthesizeKey(TEST_CREDIT_CARD["cc-name"], {}, win);
     EventUtils.synthesizeKey("VK_TAB", {}, win);
     EventUtils.synthesizeKey(billingAddress["given-name"], {}, win);
-    EventUtils.synthesizeKey("VK_TAB", {}, win);
+    if (AppConstants.platform != "win") {
+      EventUtils.synthesizeKey("VK_TAB", {}, win);
+    }
     info("saving credit card");
     EventUtils.synthesizeKey("VK_RETURN", {}, win);
   });
