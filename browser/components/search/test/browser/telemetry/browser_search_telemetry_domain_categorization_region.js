9db1bc99003b8dabacece9d813d243148d46b7a4
James Teow <jteow@mozilla.com>
2024-01-23 14:02:57 +0000

    Bug 1875483 - Remove locale and change app version to only report its major app version - r=scunnane
    Differential Revision: https://phabricator.services.mozilla.com/D199103

--- browser/components/search/test/browser/telemetry/browser_search_telemetry_domain_categorization_region.js
+++ browser/components/search/test/browser/telemetry/browser_search_telemetry_domain_categorization_region.js
@@ -4,9 +4,9 @@
 "use strict";
 
 /*
- * These tests check that changing the locale and region actually results in
- * reporting the correct changes. Other tests that include the locale and region
- * just report the locale/region used by the test.
+ * These tests check that changing the region actually results in reporting the
+ * correct changes. Other tests that include region just report the default
+ * used by the test.
  */
 
 ChromeUtils.defineESModuleGetters(this, {
@@ -62,9 +62,6 @@ const TEST_PROVIDER_INFO = [
 const originalHomeRegion = Region.home;
 const originalCurrentRegion = Region.current;
 
-const originalAvailableLocales = Services.locale.availableLocales;
-const originalRequestedLocales = Services.locale.requestedLocales;
-
 add_setup(async function () {
   SearchSERPTelemetry.overrideSearchTelemetryForTests(TEST_PROVIDER_INFO);
   await waitForIdle();
@@ -80,55 +77,44 @@ add_setup(async function () {
   Region._setHomeRegion("DE", false);
   Assert.equal(Region.home, "DE", "Region");
 
-  info("Change locale to DE.");
-  Services.locale.availableLocales = ["de"];
-  Services.locale.requestedLocales = ["de"];
-  Assert.equal(Services.locale.appLocaleAsBCP47, "de", "Locale");
-
   registerCleanupFunction(async () => {
     Region._setHomeRegion(originalHomeRegion);
     Region._setCurrentRegion(originalCurrentRegion);
-
-    Services.locale.availableLocales = originalAvailableLocales;
-    Services.locale.requestedLocales = originalRequestedLocales;
 
     SearchSERPTelemetry.overrideSearchTelemetryForTests();
     resetTelemetry();
   });
 });
 
-add_task(
-  async function test_categorize_page_with_different_region_and_locale() {
-    resetTelemetry();
+add_task(async function test_categorize_page_with_different_region() {
+  resetTelemetry();
 
-    let url = getSERPUrl("searchTelemetryDomainCategorizationReporting.html");
-    info("Load a SERP with organic and sponsored results.");
-    let promise = waitForPageWithCategorizedDomains();
-    let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, url);
-    await promise;
+  let url = getSERPUrl("searchTelemetryDomainCategorizationReporting.html");
+  info("Load a SERP with organic and sponsored results.");
+  let promise = waitForPageWithCategorizedDomains();
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, url);
+  await promise;
 
-    await BrowserTestUtils.removeTab(tab);
-    assertCategorizationValues([
-      {
-        organic_category: "3",
-        organic_num_domains: "1",
-        organic_num_inconclusive: "0",
-        organic_num_unknown: "0",
-        sponsored_category: "4",
-        sponsored_num_domains: "2",
-        sponsored_num_inconclusive: "0",
-        sponsored_num_unknown: "0",
-        mappings_version: "1",
-        app_version: APP_VERSION,
-        channel: CHANNEL,
-        locale: "de",
-        region: "DE",
-        partner_code: "ff",
-        provider: "example",
-        tagged: "true",
-        num_ads_clicked: "0",
-        num_ads_visible: "2",
-      },
-    ]);
-  }
-);
+  await BrowserTestUtils.removeTab(tab);
+  assertCategorizationValues([
+    {
+      organic_category: "3",
+      organic_num_domains: "1",
+      organic_num_inconclusive: "0",
+      organic_num_unknown: "0",
+      sponsored_category: "4",
+      sponsored_num_domains: "2",
+      sponsored_num_inconclusive: "0",
+      sponsored_num_unknown: "0",
+      mappings_version: "1",
+      app_version: APP_MAJOR_VERSION,
+      channel: CHANNEL,
+      region: "DE",
+      partner_code: "ff",
+      provider: "example",
+      tagged: "true",
+      num_ads_clicked: "0",
+      num_ads_visible: "2",
+    },
+  ]);
+});
