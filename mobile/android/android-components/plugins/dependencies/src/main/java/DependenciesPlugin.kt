f3acaf069be55ddfcbbe62379b5732f8c62a8289
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2024-01-24 16:52:23 -0500

    Bug 1876397 - Update Mockito to version 5.10.0

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -21,7 +21,7 @@ object Versions {
 
     const val junit = "4.13.2"
     const val robolectric = "4.11.1"
-    const val mockito = "5.9.0"
+    const val mockito = "5.10.0"
     const val maven_ant_tasks = "2.1.3"
     const val jacoco = "0.8.11"
     const val okhttp = "4.11.0"
