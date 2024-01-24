9645395cfee4c609c45dd346086d01e28b6af89c
Julian Descottes <jdescottes@mozilla.com>
2024-01-24 14:15:41 +0000

    Bug 1870848 - [wdspec] Add create_user_context command to bidi browser module r=webdriver-reviewers,whimboo
    Depends on D198968
    
    Differential Revision: https://phabricator.services.mozilla.com/D198969

--- testing/web-platform/tests/tools/webdriver/webdriver/bidi/modules/browser.py
+++ testing/web-platform/tests/tools/webdriver/webdriver/bidi/modules/browser.py
@@ -7,3 +7,14 @@ class Browser(BidiModule):
     @command
     def close(self) -> Mapping[str, Any]:
         return {}
+
+    @command
+    def create_user_context(self) -> Mapping[str, Any]:
+        return {}
+
+    @create_user_context.result
+    def _create_user_context(self, result: Mapping[str, Any]) -> Any:
+        assert result["userContext"] is not None
+        assert isinstance(result["userContext"], str)
+
+        return result["userContext"]
