704b167503ef6f659c9f49f60e744e9115c5ec59
Stanca Serban <sstanca@mozilla.com>
2024-01-24 21:06:05 +0200

    Backed out changeset f66c3a3de349 (bug 1873105) for causing xpcshell failures.

--- dom/canvas/test/webgl-conf/generate-wrappers-and-manifest.py
+++ dom/canvas/test/webgl-conf/generate-wrappers-and-manifest.py
@@ -13,9 +13,9 @@ from pathlib import Path
 
 # All paths in this file are based where this file is run.
 WRAPPER_TEMPLATE_FILE = "mochi-wrapper.html.template"
-MANIFEST_TEMPLATE_FILE = "mochitest.toml.template"
-ERRATA_FILE = "mochitest-errata.toml"
-DEST_MANIFEST_PATHSTR = "generated-mochitest.toml"
+MANIFEST_TEMPLATE_FILE = "mochitest.ini.template"
+ERRATA_FILE = "mochitest-errata.ini"
+DEST_MANIFEST_PATHSTR = "generated-mochitest.ini"
 
 BASE_TEST_LIST_PATHSTR = "checkout/00_test_list.txt"
 GENERATED_PATHSTR = "generated"
@@ -411,21 +411,20 @@ def WriteManifest(wrapperPathStrList, supportPathStrList):
     # SUPPORT_FILES
     supportPathStrList = [ManifestPathStr(x) for x in supportPathStrList]
     supportPathStrList = sorted(supportPathStrList)
-    supportFilesStr = '",\n  "'.join(supportPathStrList)
-    supportFilesStr = '[\n  "' + supportFilesStr + '",\n]'
+    supportFilesStr = "\n".join(supportPathStrList)
 
     # MANIFEST_TESTS
     manifestTestLineList = []
     wrapperPathStrList = sorted(wrapperPathStrList)
     for wrapperPathStr in wrapperPathStrList:
         wrapperManifestPathStr = ManifestPathStr(wrapperPathStr)
-        sectionName = '\n["' + wrapperManifestPathStr + '"]'
+        sectionName = "[" + wrapperManifestPathStr + "]"
         manifestTestLineList.append(sectionName)
 
         errataLines = []
 
         subsuite = ChooseSubsuite(wrapperPathStr)
-        errataLines.append('subsuite = "' + subsuite + '"')
+        errataLines.append("subsuite = " + subsuite)
 
         if wrapperPathStr in errataMap:
             assert subsuite
@@ -459,58 +458,59 @@ def WriteManifest(wrapperPathStrList, supportPathStrList):
 # Internals
 
 
-def LoadTOML(path):
+kManifestHeaderRegex = re.compile(r"\[([^]]*)\]")
+
+
+def LoadINI(path):
     curSectionName = None
     curSectionMap = {}
+
     lineNum = 0
+
     ret = {}
     ret[curSectionName] = (lineNum, curSectionMap)
-    multiLineVal = False
-    key = ""
-    val = ""
 
     with open(path, "r") as f:
-        for rawLine in f:
+        for line in f:
             lineNum += 1
-            if multiLineVal:
-                val += "\n" + rawLine.rstrip()
-                if rawLine.find("]") >= 0:
-                    multiLineVal = False
-                    curSectionMap[key] = (lineNum, val)
-            else:
-                line = rawLine.strip()
-                if not line:
-                    continue
-                if line[0] in [";", "#"]:
-                    continue
-                if line[0] == "[":
-                    assert line[-1] == "]", "{}:{}".format(path, lineNum)
-                    curSectionName = line[1:-1].strip('"')
-                    assert (
-                        curSectionName not in ret
-                    ), "Line {}: Duplicate section: {}".format(lineNum, line)
-                    curSectionMap = {}
-                    ret[curSectionName] = (lineNum, curSectionMap)
-                    continue
-                split = line.split("=", 1)
-                key = split[0].strip()
-                val = ""
-                if len(split) == 2:
-                    val = split[1].strip()
-                if val.find("[") >= 0 and val.find("]") < 0:
-                    multiLineVal = True
-                else:
-                    curSectionMap[key] = (lineNum, val)
+
+            line = line.strip()
+            if not line:
+                continue
+
+            if line[0] in [";", "#"]:
+                continue
+
+            if line[0] == "[":
+                assert line[-1] == "]", "{}:{}".format(path, lineNum)
+
+                curSectionName = line[1:-1]
+                assert (
+                    curSectionName not in ret
+                ), "Line {}: Duplicate section: {}".format(lineNum, line)
+
+                curSectionMap = {}
+                ret[curSectionName] = (lineNum, curSectionMap)
+                continue
+
+            split = line.split("=", 1)
+            key = split[0].strip()
+            val = ""
+            if len(split) == 2:
+                val = split[1].strip()
+
+            curSectionMap[key] = (lineNum, val)
+            continue
 
     return ret
 
 
 def LoadErrata():
-    tomlMap = LoadTOML(ERRATA_FILE)
+    iniMap = LoadINI(ERRATA_FILE)
 
     ret = {}
 
-    for sectionName, (sectionLineNum, sectionMap) in tomlMap.items():
+    for sectionName, (sectionLineNum, sectionMap) in iniMap.items():
         curLines = []
 
         if sectionName is None:
