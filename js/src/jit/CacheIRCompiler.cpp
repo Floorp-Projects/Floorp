a05fb4830106d8b45edf2064ddf96e3ff0d0e5b3
André Bargull <andre.bargull@gmail.com>
2024-01-25 15:49:48 +0000

    Bug 1842773 - Part 16: Replace TypedArrayObject with FixedLengthTypedArrayObject in the JITs. r=sfink
    Replace TypedArrayObject with FixedLengthTypedArrayObject for part 18, when
    `TypedArrayObject::length()` gets changed to handle out-of-bounds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D183332

--- js/src/jit/CacheIRCompiler.cpp
+++ js/src/jit/CacheIRCompiler.cpp
@@ -8836,8 +8836,8 @@ bool CacheIRCompiler::emitAtomicsCompareExchangeResult(
     masm.Push(index);
     masm.Push(obj);
 
-    using Fn = BigInt* (*)(JSContext*, TypedArrayObject*, size_t, const BigInt*,
-                           const BigInt*);
+    using Fn = BigInt* (*)(JSContext*, FixedLengthTypedArrayObject*, size_t,
+                           const BigInt*, const BigInt*);
     callvm->call<Fn, jit::AtomicsCompareExchange64>();
     return true;
   }
@@ -9091,7 +9091,7 @@ bool CacheIRCompiler::emitAtomicsLoadResult(ObjOperandId objId,
     masm.Push(index);
     masm.Push(obj);
 
-    using Fn = BigInt* (*)(JSContext*, TypedArrayObject*, size_t);
+    using Fn = BigInt* (*)(JSContext*, FixedLengthTypedArrayObject*, size_t);
     callvm->call<Fn, jit::AtomicsLoad64>();
     return true;
   }
@@ -9172,7 +9172,7 @@ bool CacheIRCompiler::emitAtomicsStoreResult(ObjOperandId objId,
     volatileRegs.takeUnchecked(scratch);
     masm.PushRegsInMask(volatileRegs);
 
-    using Fn = void (*)(TypedArrayObject*, size_t, const BigInt*);
+    using Fn = void (*)(FixedLengthTypedArrayObject*, size_t, const BigInt*);
     masm.setupUnalignedABICall(scratch);
     masm.passABIArg(obj);
     masm.passABIArg(index);
