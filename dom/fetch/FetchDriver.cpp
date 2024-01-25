0f4c09c9b014ba2cd9450718c0b8016f63166643
Norisz Fay <nfay@mozilla.com>
2024-01-25 18:35:45 +0200

    Backed out changeset 07a3a86e82ee (bug 1871498) for causing multiple leakcheck failures CLOSED TREE

--- dom/fetch/FetchDriver.cpp
+++ dom/fetch/FetchDriver.cpp
@@ -970,7 +970,6 @@ void FetchDriver::FailWithNetworkError(nsresult rv) {
 
   // mObserver could be null after OnResponseAvailable().
   if (mObserver) {
-    mObserver->OnReportPerformanceTiming();
     mObserver->OnResponseEnd(FetchDriverObserver::eByNetworking,
                              JS::UndefinedHandleValue);
     mObserver = nullptr;
