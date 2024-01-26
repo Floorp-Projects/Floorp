f2cdf26b8d57672904aa92f17db3c44edd565e9c
André Bargull <andre.bargull@gmail.com>
2024-01-26 08:17:47 +0000

    Bug 1842773 - Part 35: Reimport test262 tests. r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D183352

--- js/src/tests/test262/built-ins/DataView/prototype/setInt32/resizable-buffer.js
+++ js/src/tests/test262/built-ins/DataView/prototype/setInt32/resizable-buffer.js
@@ -1,4 +1,4 @@
-// |reftest| skip -- resizable-arraybuffer is not supported
+// |reftest| shell-option(--enable-arraybuffer-resizable) skip-if(!ArrayBuffer.prototype.resize||!xulRuntime.shell) -- resizable-arraybuffer is not enabled unconditionally, requires shell-options
 // Copyright (C) 2021 the V8 project authors. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
