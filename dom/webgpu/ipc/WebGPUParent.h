567429f7fbe625ad3e6dec3dd762cf6fcfc68d07
Brad Werth <bwerth@mozilla.com>
2024-01-26 20:03:17 +0000

    Bug 1865921 Part 1: Make WebGPUParent supply a device lost callback. r=ErichDonGubler,webgpu-reviewers,nical
    This patch makes several changes:
    
    1) It modifies WebGPUParent::RecvAdapterRequestDevice to additionally
    send a device lost callback to wgpu. The user data for this callback is
    stored within WebGPUParent as a hash of unique_ptr memory, which allows
    for the possibility that wgpu will not call the callback (though it
    should). When the callback is received, this memory is cleared. When the
    WebGPUParent is destroyed, it asserts that there is no remaining held
    memory for pending callbacks. These callbacks also check a WeakPtr to
    the parent as additional safety.
    
    2) It corrects some behavior in Device::ResolveLost where checking the
    existence of the underlying JS object is moved earlier, since later
    operations on the promise require the JS object to exist.
    
    Differential Revision: https://phabricator.services.mozilla.com/D194310

--- dom/webgpu/ipc/WebGPUParent.h
+++ dom/webgpu/ipc/WebGPUParent.h
@@ -8,6 +8,7 @@
 
 #include <unordered_map>
 
+#include "mozilla/WeakPtr.h"
 #include "mozilla/webgpu/ffi/wgpu.h"
 #include "mozilla/webgpu/PWebGPUParent.h"
 #include "mozilla/webrender/WebRenderAPI.h"
@@ -40,7 +41,7 @@ class PresentationData;
 //   needs to be dropped when the last reference to it dies on the child
 //   process.
 
-class WebGPUParent final : public PWebGPUParent {
+class WebGPUParent final : public PWebGPUParent, public SupportsWeakPtr {
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(WebGPUParent, override)
 
  public:
@@ -169,6 +170,8 @@ class WebGPUParent final : public PWebGPUParent {
  private:
   static void MapCallback(ffi::WGPUBufferMapAsyncStatus aStatus,
                           uint8_t* aUserData);
+  static void DeviceLostCallback(uint8_t* aUserData, uint8_t aReason,
+                                 const char* aMessage);
   void DeallocBufferShmem(RawId aBufferId);
 
   void RemoveExternalTexture(RawId aTextureId);
@@ -215,6 +218,16 @@ class WebGPUParent final : public PWebGPUParent {
 
   // Shared handle of wgpu device's fence.
   RefPtr<gfx::FileHandleWrapper> mFenceHandle;
+
+  // Store DeviceLostRequest structs for each device as unique_ptrs mapped
+  // to their device ids. We keep these unique_ptrs alive as long as the
+  // device is alive.
+  struct DeviceLostRequest {
+    WeakPtr<WebGPUParent> mParent;
+    RawId mDeviceId;
+  };
+  std::unordered_map<RawId, std::unique_ptr<DeviceLostRequest>>
+      mDeviceLostRequests;
 };
 
 }  // namespace webgpu
