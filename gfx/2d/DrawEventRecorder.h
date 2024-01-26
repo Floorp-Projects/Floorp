5bfb0fcad17fc3606f7c817039ac7d7168b2750f
Andrew Osmond <aosmond@mozilla.com>
2024-01-26 14:18:03 +0000

    Bug 1876538 - Backout changes from bug 1875364 to cache current filter for recordings. r=gfx-reviewers,jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D199723

--- gfx/2d/DrawEventRecorder.h
+++ gfx/2d/DrawEventRecorder.h
@@ -25,7 +25,6 @@ namespace mozilla {
 namespace gfx {
 
 class DrawTargetRecording;
-class FilterNodeRecording;
 class PathRecording;
 
 class DrawEventRecorderPrivate : public DrawEventRecorder {
@@ -87,54 +86,18 @@ class DrawEventRecorderPrivate : public DrawEventRecorder {
   void RecordEvent(DrawTargetRecording* aDT, const RecordedEvent& aEvent) {
     ReferencePtr dt = aDT;
     if (mCurrentDT != dt) {
-      RecordSetCurrentDrawTarget(dt);
+      SetDrawTarget(dt);
     }
     RecordEvent(aEvent);
   }
 
-  void RecordSetCurrentDrawTarget(ReferencePtr aDT);
+  void SetDrawTarget(ReferencePtr aDT);
 
-  void SetCurrentDrawTarget(DrawTargetRecording* aDT) { mCurrentDT = aDT; }
-
-  void ClearCurrentDrawTarget(DrawTargetRecording* aDT) {
+  void ClearDrawTarget(DrawTargetRecording* aDT) {
     ReferencePtr dt = aDT;
     if (mCurrentDT == dt) {
       mCurrentDT = nullptr;
     }
-  }
-
-  void RecordEvent(FilterNodeRecording* aFilter, const RecordedEvent& aEvent) {
-    ReferencePtr filter = aFilter;
-    if (mCurrentFilter != filter) {
-      RecordSetCurrentFilterNode(filter);
-    }
-    RecordEvent(aEvent);
-  }
-
-  void RecordSetCurrentFilterNode(ReferencePtr aFilter);
-
-  void SetCurrentFilterNode(FilterNodeRecording* aFilter) {
-    mCurrentFilter = aFilter;
-  }
-
-  void ClearCurrentFilterNode(FilterNodeRecording* aFilter) {
-    ReferencePtr filter = aFilter;
-    if (mCurrentFilter == filter) {
-      mCurrentFilter = nullptr;
-    }
-  }
-
-  void RecordEvent(DrawTargetRecording* aDT, FilterNodeRecording* aFilter,
-                   const RecordedEvent& aEvent) {
-    ReferencePtr dt = aDT;
-    if (mCurrentDT != dt) {
-      RecordSetCurrentDrawTarget(dt);
-    }
-    ReferencePtr filter = aFilter;
-    if (mCurrentFilter != filter) {
-      RecordSetCurrentFilterNode(filter);
-    }
-    RecordEvent(aEvent);
   }
 
   void AddStoredObject(const ReferencePtr aObject) {
@@ -289,7 +252,6 @@ class DrawEventRecorderPrivate : public DrawEventRecorder {
   nsTHashMap<void*, ThreadSafeWeakPtr<SourceSurface>> mStoredSurfaces;
 
   ReferencePtr mCurrentDT;
-  ReferencePtr mCurrentFilter;
   ExternalSurfacesHolder mExternalSurfaces;
   bool mExternalFonts;
 };
