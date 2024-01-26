565d2ce88e55553ff456ef8661b292560209c16c
Kelsey Gilbert <kelsey.gilbert@mozilla.com>
2024-01-26 21:05:30 +0000

    Bug 1873769 - Add TempPtrToSetter(UniquePtr<T>*) -> T**-ish for safe out-ptr RAII. r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D198087

--- mfbt/tests/TestUniquePtr.cpp
+++ mfbt/tests/TestUniquePtr.cpp
@@ -6,6 +6,7 @@
 
 #include <stddef.h>
 
+#include <memory>  // For unique_ptr
 #include <type_traits>
 #include <utility>
 
@@ -544,6 +545,36 @@ static bool TestVoid() {
   return true;
 }
 
+static bool TestTempPtrToSetter() {
+  static int sFooRefcount = 0;
+  struct Foo {
+    Foo() { sFooRefcount += 1; }
+
+    ~Foo() { sFooRefcount -= 1; }
+  };
+
+  const auto AllocByOutvar = [](Foo** out) -> bool {
+    *out = new Foo;
+    return true;
+  };
+
+  {
+    UniquePtr<Foo> f;
+    (void)AllocByOutvar(mozilla::TempPtrToSetter(&f));
+    CHECK(sFooRefcount == 1);
+  }
+  CHECK(sFooRefcount == 0);
+
+  {
+    std::unique_ptr<Foo> f;
+    (void)AllocByOutvar(mozilla::TempPtrToSetter(&f));
+    CHECK(sFooRefcount == 1);
+  }
+  CHECK(sFooRefcount == 0);
+
+  return true;
+}
+
 int main() {
   TestDeleterType();
 
@@ -569,6 +600,9 @@ int main() {
     return 1;
   }
   if (!TestVoid()) {
+    return 1;
+  }
+  if (!TestTempPtrToSetter()) {
     return 1;
   }
   return 0;
