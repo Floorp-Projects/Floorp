29ef807844cf816a6fa3c6154698bf0d7850fd20
Malte Juergens <mjurgens@mozilla.com>
2024-01-26 11:02:02 +0000

    Bug 1824325 - Remove unnescessary code introduced by D198575 r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D199708

--- netwerk/base/LoadInfo.cpp
+++ netwerk/base/LoadInfo.cpp
@@ -1568,9 +1568,6 @@ already_AddRefed<nsIPrincipal> CreateTruncatedPrincipal(
     }
     uriString += scheme + separator + hostPort + path;
 
-    nsAutoCString principalSpec;
-    aPrincipal->GetSpec(principalSpec);
-
     nsCOMPtr<nsIURI> truncatedURI;
     nsresult rv = NS_NewURI(getter_AddRefs(truncatedURI), uriString);
     if (NS_FAILED(rv)) {
