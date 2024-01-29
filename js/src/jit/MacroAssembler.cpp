a1225a2ea7c98b3ada591cf9bcb681dd6d4cc2f9
André Bargull <andre.bargull@gmail.com>
2024-01-29 10:44:27 +0000

    Bug 1842773 - Part 19: Add guard instruction for fixed-length typed arrays. r=sfink,jandem
    Part 14 only disallowed attaching resizable typed arrays in CacheIR, but we
    still need to guard the object is also a fixed-length typed array at runtime.
    
    Differential Revision: https://phabricator.services.mozilla.com/D183335

--- js/src/jit/MacroAssembler.cpp
+++ js/src/jit/MacroAssembler.cpp
@@ -7531,6 +7531,21 @@ void MacroAssembler::branchIfClassIsNotTypedArray(Register clasp,
             notTypedArray);
 }
 
+void MacroAssembler::branchIfClassIsNotFixedLengthTypedArray(
+    Register clasp, Label* notTypedArray) {
+  // Inline implementation of IsFixedLengthTypedArrayClass().
+
+  const auto* firstTypedArrayClass =
+      std::begin(TypedArrayObject::fixedLengthClasses);
+  const auto* lastTypedArrayClass =
+      std::prev(std::end(TypedArrayObject::fixedLengthClasses));
+
+  branchPtr(Assembler::Below, clasp, ImmPtr(firstTypedArrayClass),
+            notTypedArray);
+  branchPtr(Assembler::Above, clasp, ImmPtr(lastTypedArrayClass),
+            notTypedArray);
+}
+
 void MacroAssembler::branchIfHasDetachedArrayBuffer(Register obj, Register temp,
                                                     Label* label) {
   // Inline implementation of ArrayBufferViewObject::hasDetachedBuffer().
