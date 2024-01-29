c8e5518bc0039e8e511205eaff04dae63c5eb573
Brad Werth <bwerth@mozilla.com>
2024-01-29 16:57:22 +0000

    Bug 1874740: Make callers to NotifyCompositorSessionLost hold a reference to widget.  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D199035
    
    Differential Revision: https://phabricator.services.mozilla.com/D199374

--- gfx/ipc/RemoteCompositorSession.cpp
+++ gfx/ipc/RemoteCompositorSession.cpp
@@ -46,10 +46,13 @@ RemoteCompositorSession::~RemoteCompositorSession() {
 }
 
 void RemoteCompositorSession::NotifySessionLost() {
+  // Hold a reference to mWidget since NotifyCompositorSessionLost may
+  // release the last reference mid-execution.
+  RefPtr<nsBaseWidget> widget(mWidget);
   // Re-entrancy should be impossible: when we are being notified of a lost
   // session, we have by definition not shut down yet. We will shutdown, but
   // then will be removed from the notification list.
-  mWidget->NotifyCompositorSessionLost(this);
+  widget->NotifyCompositorSessionLost(this);
 }
 
 CompositorBridgeParent* RemoteCompositorSession::GetInProcessBridge() const {
