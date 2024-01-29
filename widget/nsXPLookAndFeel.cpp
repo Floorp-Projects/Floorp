d5ab24629bf71cf385756e77d486f9409e5ee84c
Emilio Cobos Álvarez <emilio@crisal.io>
2024-01-29 19:17:40 +0000

    Bug 1877246 - Add color-scheme aware appworkspace/scrollbar colors. r=ayeddi
    Otherwise we fall back to the light color, causing white-text on white
    background on dark mode.
    
    Differential Revision: https://phabricator.services.mozilla.com/D199910

--- widget/nsXPLookAndFeel.cpp
+++ widget/nsXPLookAndFeel.cpp
@@ -728,6 +728,8 @@ Maybe<nscolor> nsXPLookAndFeel::GenericDarkColor(ColorID aID) {
   switch (aID) {
     case ColorID::Window:  // --in-content-page-background
     case ColorID::Background:
+    case ColorID::Appworkspace:
+    case ColorID::Scrollbar:
       color = kWindowBackground;
       break;
 
