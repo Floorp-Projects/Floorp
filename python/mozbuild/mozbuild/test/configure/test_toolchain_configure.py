5e004449383db5a273dd7484013e173c843645ac
Mike Hommey <mh+mozilla@glandium.org>
2024-01-31 21:50:25 +0000

    Bug 1877546 - Refresh rust targets in configure tests. r=firefox-build-system-reviewers,sergesanspaille
    The oldest version of rust we support being newer than any of the
    conditionals we had, we can remove them all, and use the actual full
    list from rustc 1.70.0 onwards.
    
    We also add a test with the latest existing version.
    
    Differential Revision: https://phabricator.services.mozilla.com/D200088

--- python/mozbuild/mozbuild/test/configure/test_toolchain_configure.py
+++ python/mozbuild/mozbuild/test/configure/test_toolchain_configure.py
@@ -1678,43 +1678,84 @@ def gen_invoke_rustc(version, rustup_wrapper=False):
                 "",
             )
         if args == ("--print", "target-list"):
-            # Raw list returned by rustc version 1.32, + ios, which somehow
-            # don't appear in the default list.
-            # https://github.com/rust-lang/rust/issues/36156
+            # Raw list returned by rustc version 1.70
             rust_targets = [
+                "aarch64-apple-darwin",
                 "aarch64-apple-ios",
+                "aarch64-apple-ios-macabi",
+                "aarch64-apple-ios-sim",
+                "aarch64-apple-tvos",
+                "aarch64-apple-watchos-sim",
                 "aarch64-fuchsia",
+                "aarch64-kmc-solid_asp3",
                 "aarch64-linux-android",
+                "aarch64-nintendo-switch-freestanding",
+                "aarch64-pc-windows-gnullvm",
                 "aarch64-pc-windows-msvc",
-                "aarch64-unknown-cloudabi",
                 "aarch64-unknown-freebsd",
+                "aarch64-unknown-fuchsia",
                 "aarch64-unknown-hermit",
                 "aarch64-unknown-linux-gnu",
+                "aarch64-unknown-linux-gnu_ilp32",
                 "aarch64-unknown-linux-musl",
+                "aarch64-unknown-linux-ohos",
                 "aarch64-unknown-netbsd",
                 "aarch64-unknown-none",
+                "aarch64-unknown-none-softfloat",
+                "aarch64-unknown-nto-qnx710",
                 "aarch64-unknown-openbsd",
+                "aarch64-unknown-redox",
+                "aarch64-unknown-uefi",
+                "aarch64-uwp-windows-msvc",
+                "aarch64-wrs-vxworks",
+                "aarch64_be-unknown-linux-gnu",
+                "aarch64_be-unknown-linux-gnu_ilp32",
                 "arm-linux-androideabi",
                 "arm-unknown-linux-gnueabi",
                 "arm-unknown-linux-gnueabihf",
                 "arm-unknown-linux-musleabi",
                 "arm-unknown-linux-musleabihf",
+                "arm64_32-apple-watchos",
+                "armeb-unknown-linux-gnueabi",
                 "armebv7r-none-eabi",
                 "armebv7r-none-eabihf",
+                "armv4t-none-eabi",
                 "armv4t-unknown-linux-gnueabi",
+                "armv5te-none-eabi",
                 "armv5te-unknown-linux-gnueabi",
                 "armv5te-unknown-linux-musleabi",
+                "armv5te-unknown-linux-uclibceabi",
+                "armv6-unknown-freebsd",
                 "armv6-unknown-netbsd-eabihf",
+                "armv6k-nintendo-3ds",
+                "armv7-apple-ios",
                 "armv7-linux-androideabi",
-                "armv7-unknown-cloudabi-eabihf",
+                "armv7-sony-vita-newlibeabihf",
+                "armv7-unknown-freebsd",
+                "armv7-unknown-linux-gnueabi",
                 "armv7-unknown-linux-gnueabihf",
+                "armv7-unknown-linux-musleabi",
                 "armv7-unknown-linux-musleabihf",
+                "armv7-unknown-linux-ohos",
+                "armv7-unknown-linux-uclibceabi",
+                "armv7-unknown-linux-uclibceabihf",
                 "armv7-unknown-netbsd-eabihf",
+                "armv7-wrs-vxworks-eabihf",
+                "armv7a-kmc-solid_asp3-eabi",
+                "armv7a-kmc-solid_asp3-eabihf",
+                "armv7a-none-eabi",
+                "armv7a-none-eabihf",
+                "armv7k-apple-watchos",
                 "armv7r-none-eabi",
                 "armv7r-none-eabihf",
                 "armv7s-apple-ios",
                 "asmjs-unknown-emscripten",
+                "avr-unknown-gnu-atmega328",
+                "bpfeb-unknown-none",
+                "bpfel-unknown-none",
+                "hexagon-unknown-linux-musl",
                 "i386-apple-ios",
+                "i586-pc-nto-qnx700",
                 "i586-pc-windows-msvc",
                 "i586-unknown-linux-gnu",
                 "i586-unknown-linux-musl",
@@ -1722,140 +1763,187 @@ def gen_invoke_rustc(version, rustup_wrapper=False):
                 "i686-linux-android",
                 "i686-pc-windows-gnu",
                 "i686-pc-windows-msvc",
-                "i686-unknown-cloudabi",
-                "i686-unknown-dragonfly",
                 "i686-unknown-freebsd",
                 "i686-unknown-haiku",
                 "i686-unknown-linux-gnu",
                 "i686-unknown-linux-musl",
                 "i686-unknown-netbsd",
                 "i686-unknown-openbsd",
+                "i686-unknown-uefi",
+                "i686-uwp-windows-gnu",
+                "i686-uwp-windows-msvc",
+                "i686-wrs-vxworks",
+                "loongarch64-unknown-linux-gnu",
+                "m68k-unknown-linux-gnu",
                 "mips-unknown-linux-gnu",
                 "mips-unknown-linux-musl",
                 "mips-unknown-linux-uclibc",
+                "mips64-openwrt-linux-musl",
                 "mips64-unknown-linux-gnuabi64",
+                "mips64-unknown-linux-muslabi64",
                 "mips64el-unknown-linux-gnuabi64",
+                "mips64el-unknown-linux-muslabi64",
+                "mipsel-sony-psp",
+                "mipsel-sony-psx",
                 "mipsel-unknown-linux-gnu",
                 "mipsel-unknown-linux-musl",
                 "mipsel-unknown-linux-uclibc",
+                "mipsel-unknown-none",
+                "mipsisa32r6-unknown-linux-gnu",
+                "mipsisa32r6el-unknown-linux-gnu",
+                "mipsisa64r6-unknown-linux-gnuabi64",
+                "mipsisa64r6el-unknown-linux-gnuabi64",
                 "msp430-none-elf",
+                "nvptx64-nvidia-cuda",
+                "powerpc-unknown-freebsd",
                 "powerpc-unknown-linux-gnu",
                 "powerpc-unknown-linux-gnuspe",
                 "powerpc-unknown-linux-musl",
                 "powerpc-unknown-netbsd",
+                "powerpc-unknown-openbsd",
+                "powerpc-wrs-vxworks",
+                "powerpc-wrs-vxworks-spe",
+                "powerpc64-ibm-aix",
+                "powerpc64-unknown-freebsd",
                 "powerpc64-unknown-linux-gnu",
                 "powerpc64-unknown-linux-musl",
+                "powerpc64-unknown-openbsd",
+                "powerpc64-wrs-vxworks",
+                "powerpc64le-unknown-freebsd",
                 "powerpc64le-unknown-linux-gnu",
                 "powerpc64le-unknown-linux-musl",
+                "riscv32gc-unknown-linux-gnu",
+                "riscv32gc-unknown-linux-musl",
+                "riscv32i-unknown-none-elf",
+                "riscv32im-unknown-none-elf",
                 "riscv32imac-unknown-none-elf",
+                "riscv32imac-unknown-xous-elf",
+                "riscv32imc-esp-espidf",
                 "riscv32imc-unknown-none-elf",
+                "riscv64gc-unknown-freebsd",
+                "riscv64gc-unknown-fuchsia",
+                "riscv64gc-unknown-linux-gnu",
+                "riscv64gc-unknown-linux-musl",
+                "riscv64gc-unknown-none-elf",
+                "riscv64gc-unknown-openbsd",
+                "riscv64imac-unknown-none-elf",
                 "s390x-unknown-linux-gnu",
+                "s390x-unknown-linux-musl",
                 "sparc-unknown-linux-gnu",
                 "sparc64-unknown-linux-gnu",
                 "sparc64-unknown-netbsd",
+                "sparc64-unknown-openbsd",
                 "sparcv9-sun-solaris",
+                "thumbv4t-none-eabi",
+                "thumbv5te-none-eabi",
                 "thumbv6m-none-eabi",
                 "thumbv7a-pc-windows-msvc",
+                "thumbv7a-uwp-windows-msvc",
                 "thumbv7em-none-eabi",
                 "thumbv7em-none-eabihf",
                 "thumbv7m-none-eabi",
+                "thumbv7neon-linux-androideabi",
+                "thumbv7neon-unknown-linux-gnueabihf",
+                "thumbv7neon-unknown-linux-musleabihf",
                 "thumbv8m.base-none-eabi",
-                "wasm32-experimental-emscripten",
+                "thumbv8m.main-none-eabi",
+                "thumbv8m.main-none-eabihf",
                 "wasm32-unknown-emscripten",
                 "wasm32-unknown-unknown",
+                "wasm32-wasi",
+                "wasm64-unknown-unknown",
                 "x86_64-apple-darwin",
                 "x86_64-apple-ios",
+                "x86_64-apple-ios-macabi",
+                "x86_64-apple-tvos",
+                "x86_64-apple-watchos-sim",
                 "x86_64-fortanix-unknown-sgx",
                 "x86_64-fuchsia",
                 "x86_64-linux-android",
+                "x86_64-pc-nto-qnx710",
+                "x86_64-pc-solaris",
                 "x86_64-pc-windows-gnu",
+                "x86_64-pc-windows-gnullvm",
                 "x86_64-pc-windows-msvc",
-                "x86_64-rumprun-netbsd",
                 "x86_64-sun-solaris",
-                "x86_64-unknown-bitrig",
-                "x86_64-unknown-cloudabi",
                 "x86_64-unknown-dragonfly",
                 "x86_64-unknown-freebsd",
+                "x86_64-unknown-fuchsia",
                 "x86_64-unknown-haiku",
                 "x86_64-unknown-hermit",
+                "x86_64-unknown-illumos",
                 "x86_64-unknown-l4re-uclibc",
                 "x86_64-unknown-linux-gnu",
                 "x86_64-unknown-linux-gnux32",
                 "x86_64-unknown-linux-musl",
                 "x86_64-unknown-netbsd",
+                "x86_64-unknown-none",
                 "x86_64-unknown-openbsd",
                 "x86_64-unknown-redox",
+                "x86_64-unknown-uefi",
+                "x86_64-uwp-windows-gnu",
+                "x86_64-uwp-windows-msvc",
+                "x86_64-wrs-vxworks",
             ]
-            # Additional targets from 1.33
-            if Version(version) >= "1.33.0":
+            # Additional targets from 1.71
+            if Version(version) >= "1.71.0":
                 rust_targets += [
-                    "thumbv7neon-linux-androideabi",
-                    "thumbv7neon-unknown-linux-gnueabihf",
-                    "x86_64-unknown-uefi",
-                    "thumbv8m.main-none-eabi",
-                    "thumbv8m.main-none-eabihf",
+                    "x86_64h-apple-darwin",
                 ]
-            # Additional targets from 1.34
-            if Version(version) >= "1.34.0":
+            # Additional targets from 1.72
+            if Version(version) >= "1.72.0":
                 rust_targets += [
-                    "nvptx64-nvidia-cuda",
-                    "powerpc64-unknown-freebsd",
-                    "riscv64gc-unknown-none-elf",
-                    "riscv64imac-unknown-none-elf",
+                    "aarch64_be-unknown-netbsd",
+                    "loongarch64-unknown-none",
+                    "loongarch64-unknown-none-softfloat",
+                    "riscv32imac-esp-espidf",
+                    "riscv64gc-unknown-netbsd",
                 ]
-            # Additional targets from 1.35
-            if Version(version) >= "1.35.0":
+            # Additional targets from 1.73
+            if Version(version) >= "1.73.0":
                 rust_targets += [
-                    "armv6-unknown-freebsd",
-                    "armv7-unknown-freebsd",
-                    "mipsisa32r6-unknown-linux-gnu",
-                    "mipsisa32r6el-unknown-linux-gnu",
-                    "mipsisa64r6-unknown-linux-gnuabi64",
-                    "mipsisa64r6el-unknown-linux-gnuabi64",
-                    "wasm32-unknown-wasi",
+                    "aarch64-unknown-teeos",
+                    "csky-unknown-linux-gnuabiv2",
+                    "riscv64-linux-android",
+                    "riscv64gc-unknown-hermit",
+                    "sparc-unknown-none-elf",
+                    "wasm32-wasi-preview1-threads",
+                    "x86_64-unikraft-linux-musl",
+                    "x86_64-unknown-linux-ohos",
                 ]
-            # Additional targets from 1.36
-            if Version(version) >= "1.36.0":
-                rust_targets += ["wasm32-wasi"]
-                rust_targets.remove("wasm32-unknown-wasi")
-                rust_targets.remove("x86_64-unknown-bitrig")
-            # Additional targets from 1.37
-            if Version(version) >= "1.37.0":
-                rust_targets += ["x86_64-pc-solaris"]
-            # Additional targets from 1.38
-            if Version(version) >= "1.38.0":
+            # Additional targets from 1.74
+            if Version(version) >= "1.74.0":
                 rust_targets += [
-                    "aarch64-unknown-redox",
-                    "aarch64-wrs-vxworks",
-                    "armv7-unknown-linux-gnueabi",
-                    "armv7-unknown-linux-musleabi",
-                    "armv7-wrs-vxworks",
-                    "hexagon-unknown-linux-musl",
-                    "i586-wrs-vxworks",
-                    "i686-uwp-windows-gnu",
-                    "i686-wrs-vxworks",
-                    "powerpc-wrs-vxworks",
-                    "powerpc-wrs-vxworks-spe",
-                    "powerpc64-wrs-vxworks",
-                    "riscv32i-unknown-none-elf",
-                    "x86_64-uwp-windows-gnu",
-                    "x86_64-wrs-vxworks",
+                    "i686-pc-windows-gnullvm",
+                    "i686-unknown-hurd-gnu",
                 ]
-            # Additional targets from 1.38
-            if Version(version) >= "1.39.0":
+                rust_targets.remove("armv7-apple-ios")
+            # Additional targets from 1.75
+            if Version(version) >= "1.75.0":
                 rust_targets += [
-                    "aarch64-uwp-windows-msvc",
-                    "armv7-wrs-vxworks-eabihf",
-                    "i686-unknown-uefi",
-                    "i686-uwp-windows-msvc",
-                    "mips64-unknown-linux-muslabi64",
-                    "mips64el-unknown-linux-muslabi64",
-                    "sparc64-unknown-openbsd",
-                    "x86_64-linux-kernel",
-                    "x86_64-uwp-windows-msvc",
+                    "aarch64-apple-tvos-sim",
+                    "csky-unknown-linux-gnuabiv2hf",
+                    "i586-unknown-netbsd",
+                    "mipsel-unknown-netbsd",
                 ]
-                rust_targets.remove("armv7-wrs-vxworks")
-                rust_targets.remove("i586-wrs-vxworks")
+            # Additional targets from 1.76
+            if Version(version) >= "1.76.0":
+                rust_targets += [
+                    "aarch64-apple-watchos",
+                    "arm64e-apple-darwin",
+                    "arm64e-apple-ios",
+                    "i686-win7-windows-msvc",
+                    "riscv32imafc-unknown-none-elf",
+                    "x86_64-win7-windows-msvc",
+                ]
+                rust_targets.remove("asmjs-unknown-emscripten")
+                rust_targets.remove("x86_64-sun-solaris")
+            # Additional targets from 1.77
+            if Version(version) >= "1.77.0":
+                rust_targets += [
+                    "aarch64-unknown-illumos",
+                    "hexagon-unknown-none-elf",
+                ]
 
             return 0, "\n".join(sorted(rust_targets)), ""
         if (
@@ -1873,16 +1961,16 @@ def gen_invoke_rustc(version, rustup_wrapper=False):
 
 
 class RustTest(BaseConfigureTest):
-    def get_rust_target(
-        self, target, compiler_type="gcc", version=MINIMUM_RUST_VERSION, arm_target=None
-    ):
+    VERSION = MINIMUM_RUST_VERSION
+
+    def get_rust_target(self, target, compiler_type="gcc", arm_target=None):
         environ = {
             "PATH": os.pathsep.join(mozpath.abspath(p) for p in ("/bin", "/usr/bin"))
         }
 
         paths = {
-            mozpath.abspath("/usr/bin/cargo"): gen_invoke_cargo(version),
-            mozpath.abspath("/usr/bin/rustc"): gen_invoke_rustc(version),
+            mozpath.abspath("/usr/bin/cargo"): gen_invoke_cargo(self.VERSION),
+            mozpath.abspath("/usr/bin/rustc"): gen_invoke_rustc(self.VERSION),
         }
 
         self.TARGET = target
@@ -1944,7 +2032,12 @@ class RustTest(BaseConfigureTest):
             ("x86_64-unknown-linux-android", "x86_64-linux-android"),
             ("x86_64-pc-linux-gnu", "x86_64-unknown-linux-gnu"),
             ("sparcv9-sun-solaris2", "sparcv9-sun-solaris"),
-            ("x86_64-sun-solaris2", "x86_64-sun-solaris"),
+            (
+                "x86_64-sun-solaris2",
+                "x86_64-sun-solaris"
+                if Version(self.VERSION) < "1.76.0"
+                else "x86_64-pc-solaris",
+            ),
         ):
             self.assertEqual(self.get_rust_target(autoconf), rust)
 
@@ -2050,6 +2143,10 @@ class RustTest(BaseConfigureTest):
             ),
             "armv4t-unknown-linux-gnueabi",
         )
+
+
+class Rust177Test(RustTest):
+    VERSION = "1.77.0"
 
 
 if __name__ == "__main__":
