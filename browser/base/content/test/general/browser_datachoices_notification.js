d8ac7220d2605886a9c4f9c5fffa76cda2a9e802
Hanna Jones <hjones@mozilla.com>
2024-01-31 14:52:03 +0000

    Bug 1874019 - Wait for about:preferences to open rather than relying on next tick r=reusable-components-reviewers,tgiles
    The fact that adding an infobar notificaiton is async now seems to have messed with some of the timing assumptions baked into this test. Explicitly waiting for the about:preferences URI to load, rather than just waiting for a tick seems to make things more reliable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D198636

--- browser/base/content/test/general/browser_datachoices_notification.js
+++ browser/base/content/test/general/browser_datachoices_notification.js
@@ -104,11 +104,16 @@ var checkInfobarButton = async function (aNotification) {
   );
   let button = buttons[0];
 
+  let openPrefsPromise = BrowserTestUtils.waitForLocationChange(
+    gBrowser,
+    "about:preferences#privacy"
+  );
+
   // Click on the button.
   button.click();
 
   // Wait for the preferences panel to open.
-  await promiseNextTick();
+  await openPrefsPromise;
 };
 
 add_setup(async function () {
