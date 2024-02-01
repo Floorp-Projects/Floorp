9b00bb2bfed419782d74c4dc8f751960387c8d75
Kagami Sascha Rosylight <saschanaz@outlook.com>
2024-02-01 11:03:59 +0000

    Bug 1876536 - Part 1: Remove self refcounts within constructor in dom/canvas r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D199626

--- dom/canvas/SourceSurfaceWebgl.h
+++ dom/canvas/SourceSurfaceWebgl.h
@@ -24,8 +24,6 @@ class SourceSurfaceWebgl : public DataSourceSurface {
   MOZ_DECLARE_REFCOUNTED_VIRTUAL_TYPENAME(SourceSurfaceWebgl, override)
 
   explicit SourceSurfaceWebgl(DrawTargetWebgl* aDT);
-  SourceSurfaceWebgl(const RefPtr<TextureHandle>& aHandle,
-                     const RefPtr<SharedContextWebgl>& aSharedContext);
   virtual ~SourceSurfaceWebgl();
 
   SurfaceType GetType() const override { return SurfaceType::WEBGL; }
@@ -46,11 +44,15 @@ class SourceSurfaceWebgl : public DataSourceSurface {
   friend class DrawTargetWebgl;
   friend class SharedContextWebgl;
 
+  explicit SourceSurfaceWebgl(const RefPtr<SharedContextWebgl>& aSharedContext);
+
   bool EnsureData();
 
   void DrawTargetWillChange(bool aNeedHandle);
 
   void GiveTexture(RefPtr<TextureHandle> aHandle);
+
+  void SetHandle(TextureHandle* aHandle);
 
   void OnUnlinkTexture(SharedContextWebgl* aContext);
 
