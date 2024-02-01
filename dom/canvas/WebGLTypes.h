f33a2060c7df07ae4d828664a5f6a025d97992be
Kelsey Gilbert <kelsey.gilbert@mozilla.com>
2024-02-01 23:32:14 +0000

    Bug 1873866 - RawBuffer now forbids null/size-only. r=gfx-reviewers,ahale
    Differential Revision: https://phabricator.services.mozilla.com/D199939

--- dom/canvas/WebGLTypes.h
+++ dom/canvas/WebGLTypes.h
@@ -853,33 +853,31 @@ struct VertAttribPointerCalculated final {
 
 }  // namespace webgl
 
-/**
- * Represents a block of memory that it may or may not own.  The
- * inner data type must be trivially copyable by memcpy.
- */
+// TODO: s/RawBuffer/Span/
 template <typename T = uint8_t>
 class RawBuffer final {
   const T* mBegin = nullptr;
   size_t mLen = 0;
-  UniqueBuffer mOwned;
 
  public:
   using ElementType = T;
 
-  /**
-   * If aTakeData is true, RawBuffer will delete[] the memory when destroyed.
-   */
-  explicit RawBuffer(const Range<const T>& data, UniqueBuffer&& owned = {})
-      : mBegin(data.begin().get()),
-        mLen(data.length()),
-        mOwned(std::move(owned)) {}
-
-  explicit RawBuffer(const size_t len) : mLen(len) {}
+  explicit RawBuffer(const Range<const T>& data)
+      : mBegin(data.begin().get()), mLen(data.length()) {
+    if (mLen) {
+      MOZ_ASSERT(mBegin);
+    }
+  }
 
   ~RawBuffer() = default;
 
-  Range<const T> Data() const { return {mBegin, mLen}; }
-  const auto& begin() const { return mBegin; }
+  Range<const T> Data() const { return {begin(), mLen}; }
+  const auto& begin() const {
+    if (mLen) {
+      MOZ_RELEASE_ASSERT(mBegin);
+    }
+    return mBegin;
+  }
   const auto& size() const { return mLen; }
 
   void Shrink(const size_t newLen) {
