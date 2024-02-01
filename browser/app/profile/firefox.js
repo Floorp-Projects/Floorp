927c046ee44157b36b7e187f32ca2edf68ff9547
Thomas Wisniewski <twisniewski@mozilla.com>
2024-02-01 16:36:03 +0000

    Bug 1876880 - put the randomization of Report Broken Site reason-dropdown behind a pref, and base its ordering on ClientEnvironment.randomizationId; r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D199790

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2973,6 +2973,8 @@ pref("ui.new-webcompat-reporter.send-more-info-link", false);
 # 0 = disabled, 1 = reason optional, 2 = reason required.
 pref("ui.new-webcompat-reporter.reason-dropdown", 0);
 
+pref("ui.new-webcompat-reporter.reason-dropdown.randomized", true);
+
 // Reset Private Browsing Session feature
 #if defined(NIGHTLY_BUILD)
   pref("browser.privatebrowsing.resetPBM.enabled", true);
