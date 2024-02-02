4ab601c14f544dc7a3d542e9e28f794480bf042a
Geoff Brown <gbrown@mozilla.com>
2024-02-02 18:27:49 +0000

    Bug 1877861 - run cppunittest against android x86 builds r=jcristau,taskgraph-reviewers,jmaher
    Currently there are no tests run against geckoview x86 builds. One consequence
    of this is that geckoview x86 artifact builds are not available. Since there
    are no x86 artifact builds, build-fat-aar must wait for the non-artifact x86
    build to complete after the x86_64, arm7, and aarch64 artifact builds complete.
    The planned fenix and other firefox-android builds coming to mozilla-central
    soon will rely on build-fat-aar, so we want these builds to be efficient.
    This patch adds a minimal test set for geckoview x86 to enable x86 artifact
    builds. cppunit tests are selected because they are one of the few test suites
    which run trouble-free on x86, they are conceivably abi-sensitive, and they
    add minimal test load (they complete quickly).
    
    Differential Revision: https://phabricator.services.mozilla.com/D200406

--- taskcluster/gecko_taskgraph/transforms/test/other.py
+++ taskcluster/gecko_taskgraph/transforms/test/other.py
@@ -169,6 +169,10 @@ def set_treeherder_machine_platform(config, tasks):
             task["treeherder-machine-platform"] = task["test-platform"].replace(
                 ".", "-"
             )
+        elif "android-em-7.0-x86-qr" in task["test-platform"]:
+            task["treeherder-machine-platform"] = task["test-platform"].replace(
+                ".", "-"
+            )
         elif "-qr" in task["test-platform"]:
             task["treeherder-machine-platform"] = task["test-platform"]
         elif "android-hw" in task["test-platform"]:
@@ -755,7 +759,7 @@ test_setting_description_schema = Schema(
     {
         Required("_hash"): str,
         "platform": {
-            Required("arch"): Any("32", "64", "aarch64", "arm7", "x86_64"),
+            Required("arch"): Any("32", "64", "aarch64", "arm7", "x86", "x86_64"),
             Required("os"): {
                 Required("name"): Any("android", "linux", "macosx", "windows"),
                 Required("version"): str,
