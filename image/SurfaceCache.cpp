6af6d20ebec71965d954b22cfb97d17f6d1a02db
Norisz Fay <nfay@mozilla.com>
2024-02-02 04:48:43 +0200

    Backed out changeset c5ac0dc2aa69 (bug 1864550) for causing multiple image.mem.max_legal_imgframe_size_kb related failures CLOSED TREE

--- image/SurfaceCache.cpp
+++ image/SurfaceCache.cpp
@@ -1899,7 +1899,8 @@ bool SurfaceCache::IsLegalSize(const IntSize& aSize) {
     NS_WARNING("width or height too large");
     return false;
   }
-  const int32_t maxSize = StaticPrefs::image_mem_max_legal_imgframe_size_kb();
+  const int32_t maxSize =
+      StaticPrefs::image_mem_max_legal_imgframe_size_kb_AtStartup();
   if (MOZ_UNLIKELY(maxSize > 0 && requiredBytes.value() / 1024 > maxSize)) {
     return false;
   }
