5b5021b0a6d599e6e1d251914b2bfd4d45c8575c
Jan de Mooij <jdemooij@mozilla.com>
2024-02-02 09:19:34 +0000

    Bug 1877586 - Fix OOM simulation check in maxPromiseHelperThreads. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D200295

---
+++ js/src/jit-test/tests/basic/bug1877586.js
@@ -0,0 +1,9 @@
+// |jit-test| skip-if: !('oomAtAllocation' in this); allow-oom
+try {
+    for (let i = 0; i < 5; i++) {
+        WebAssembly.instantiateStreaming(
+            wasmTextToBinary('(module (func) (export "" (func 0)))')
+        );
+    }
+} catch (e) {}
+oomAtAllocation(7, 7);
