086be20f6f8789c2aee711be69ec2faaee2c49b9
Tooru Fujisawa <arai_a@mac.com>
2024-02-03 04:50:41 +0000

    Bug 1877019 - Reflect user activation modifiers for keyboard event only if it likely activates a link or a link-like button. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D200202

--- browser/base/content/test/tabs/browser_window_open_modifiers.js
+++ browser/base/content/test/tabs/browser_window_open_modifiers.js
@@ -15,29 +15,66 @@ add_task(async function () {
       const metaShiftEvent = { [metaKey]: true, shiftKey: true };
 
       const tests = [
-        // id, options, result
-        ["#instant", normalEvent, "tab"],
-        ["#instant", shiftEvent, "window"],
-        ["#instant", metaEvent, "tab-bg"],
-        ["#instant", metaShiftEvent, "tab"],
+        // type, id, options, result
+        ["mouse", "#instant", normalEvent, "tab"],
+        ["mouse", "#instant", shiftEvent, "window"],
+        ["mouse", "#instant", metaEvent, "tab-bg"],
+        ["mouse", "#instant", metaShiftEvent, "tab"],
 
-        ["#instant-popup", normalEvent, "popup"],
-        ["#instant-popup", shiftEvent, "window"],
-        ["#instant-popup", metaEvent, "tab-bg"],
-        ["#instant-popup", metaShiftEvent, "tab"],
+        ["mouse", "#instant-popup", normalEvent, "popup"],
+        ["mouse", "#instant-popup", shiftEvent, "window"],
+        ["mouse", "#instant-popup", metaEvent, "tab-bg"],
+        ["mouse", "#instant-popup", metaShiftEvent, "tab"],
 
-        ["#delayed", normalEvent, "tab"],
-        ["#delayed", shiftEvent, "window"],
-        ["#delayed", metaEvent, "tab-bg"],
-        ["#delayed", metaShiftEvent, "tab"],
+        ["mouse", "#delayed", normalEvent, "tab"],
+        ["mouse", "#delayed", shiftEvent, "window"],
+        ["mouse", "#delayed", metaEvent, "tab-bg"],
+        ["mouse", "#delayed", metaShiftEvent, "tab"],
 
-        ["#delayed-popup", normalEvent, "popup"],
-        ["#delayed-popup", shiftEvent, "window"],
-        ["#delayed-popup", metaEvent, "tab-bg"],
-        ["#delayed-popup", metaShiftEvent, "tab"],
+        ["mouse", "#delayed-popup", normalEvent, "popup"],
+        ["mouse", "#delayed-popup", shiftEvent, "window"],
+        ["mouse", "#delayed-popup", metaEvent, "tab-bg"],
+        ["mouse", "#delayed-popup", metaShiftEvent, "tab"],
+
+        // NOTE: meta+keyboard doesn't activate.
+
+        ["VK_SPACE", "#instant", normalEvent, "tab"],
+        ["VK_SPACE", "#instant", shiftEvent, "window"],
+
+        ["VK_SPACE", "#instant-popup", normalEvent, "popup"],
+        ["VK_SPACE", "#instant-popup", shiftEvent, "window"],
+
+        ["VK_SPACE", "#delayed", normalEvent, "tab"],
+        ["VK_SPACE", "#delayed", shiftEvent, "window"],
+
+        ["VK_SPACE", "#delayed-popup", normalEvent, "popup"],
+        ["VK_SPACE", "#delayed-popup", shiftEvent, "window"],
+
+        ["KEY_Enter", "#link-instant", normalEvent, "tab"],
+        ["KEY_Enter", "#link-instant", shiftEvent, "window"],
+
+        ["KEY_Enter", "#link-instant-popup", normalEvent, "popup"],
+        ["KEY_Enter", "#link-instant-popup", shiftEvent, "window"],
+
+        ["KEY_Enter", "#link-delayed", normalEvent, "tab"],
+        ["KEY_Enter", "#link-delayed", shiftEvent, "window"],
+
+        ["KEY_Enter", "#link-delayed-popup", normalEvent, "popup"],
+        ["KEY_Enter", "#link-delayed-popup", shiftEvent, "window"],
+
+        // Trigger user-defined shortcut key, where modifiers shouldn't affect.
+
+        ["x", "#instant", normalEvent, "tab"],
+        ["x", "#instant", shiftEvent, "tab"],
+        ["x", "#instant", metaEvent, "tab"],
+        ["x", "#instant", metaShiftEvent, "tab"],
+
+        ["y", "#instant", normalEvent, "popup"],
+        ["y", "#instant", shiftEvent, "popup"],
+        ["y", "#instant", metaEvent, "popup"],
+        ["y", "#instant", metaShiftEvent, "popup"],
       ];
-
-      for (const [id, event, result] of tests) {
+      for (const [type, id, event, result] of tests) {
         const eventStr = JSON.stringify(event);
 
         let openPromise;
@@ -53,7 +90,29 @@ add_task(async function () {
           });
         }
 
-        BrowserTestUtils.synthesizeMouseAtCenter(id, { ...event }, browser);
+        if (type == "mouse") {
+          BrowserTestUtils.synthesizeMouseAtCenter(id, { ...event }, browser);
+        } else {
+          // Make sure the keyboard activates a simple button on the page.
+          await ContentTask.spawn(browser, id, elementId => {
+            content.document.querySelector("#focus-result").value = "";
+            content.document.querySelector("#focus-check").focus();
+          });
+          BrowserTestUtils.synthesizeKey("VK_SPACE", {}, browser);
+          await ContentTask.spawn(browser, {}, async () => {
+            await ContentTaskUtils.waitForCondition(
+              () =>
+                content.document.querySelector("#focus-result").value === "ok"
+            );
+          });
+
+          // Once confirmed the keyboard event works, send the actual event
+          // that calls window.open.
+          await ContentTask.spawn(browser, id, elementId => {
+            content.document.querySelector(elementId).focus();
+          });
+          BrowserTestUtils.synthesizeKey(type, { ...event }, browser);
+        }
 
         const openedThing = await openPromise;
 
@@ -64,13 +123,13 @@ add_task(async function () {
             Assert.equal(
               gBrowser.selectedTab,
               newTab,
-              `${id} with ${eventStr} opened a foreground tab`
+              `${id} with ${type} and ${eventStr} opened a foreground tab`
             );
           } else {
             Assert.notEqual(
               gBrowser.selectedTab,
               newTab,
-              `${id} with ${eventStr} opened a background tab`
+              `${id} with ${type} and ${eventStr} opened a background tab`
             );
           }
 
@@ -82,15 +141,30 @@ add_task(async function () {
           if (result == "window") {
             ok(
               !tabs.collapsed,
-              `${id} with ${eventStr} opened a regular window`
+              `${id} with ${type} and ${eventStr} opened a regular window`
             );
           } else {
-            ok(tabs.collapsed, `${id} with ${eventStr} opened a popup window`);
+            ok(
+              tabs.collapsed,
+              `${id} with ${type} and ${eventStr} opened a popup window`
+            );
           }
 
           const closedPopupPromise = BrowserTestUtils.windowClosed(newWindow);
           newWindow.close();
           await closedPopupPromise;
+
+          // Make sure the focus comes back to this window before proceeding
+          // to the next test.
+          if (Services.focus.focusedWindow != window) {
+            const focusBack = BrowserTestUtils.waitForEvent(
+              window,
+              "focus",
+              true
+            );
+            window.focus();
+            await focusBack;
+          }
         }
       }
     }
