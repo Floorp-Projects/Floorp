dbb8257bab163375443c704598e4891b36923380
Edgar Chen <echen@mozilla.com>
2024-02-05 21:41:00 +0000

    Bug 1828259 - Close autocomplete popup when window is resized; r=dimi
    Differential Revision: https://phabricator.services.mozilla.com/D199857

--- toolkit/components/satchel/nsFormFillController.cpp
+++ toolkit/components/satchel/nsFormFillController.cpp
@@ -925,6 +925,9 @@ nsFormFillController::HandleEvent(Event* aEvent) {
         RemoveForDocument(doc);
       }
     } break;
+    case eResize:
+      SetPopupOpen(false);
+      break;
     default:
       // Handling the default case to shut up stupid -Wswitch warnings.
       // One day compilers will be smarter...
@@ -952,6 +955,7 @@ void nsFormFillController::AttachListeners(EventTarget* aEventTarget) {
                               TrustedEventsAtCapture());
   elm->AddEventListenerByType(this, u"contextmenu"_ns,
                               TrustedEventsAtCapture());
+  elm->AddEventListenerByType(this, u"resize"_ns, TrustedEventsAtCapture());
 }
 
 void nsFormFillController::RemoveForDocument(Document* aDoc) {
