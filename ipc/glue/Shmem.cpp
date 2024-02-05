374ee5c8c550e556bf10601a559d444068274a1b
Nika Layzell <nika@thelayzells.com>
2024-02-05 18:43:43 +0000

    Bug 1873295 - Part 1: Relax Shmem DEBUG behaviour around actor destruction, r=ipc-reviewers,jld
    Currently when in a DEBUG build, Shmem has a bunch of extra behaviour
    and checks, which can occasionally make it more difficult to use. This
    bug is to track removing some of the unnecessary checks being performed
    which can make the type more complex to use safely.
    
    Specifically, when the Shmem is destroyed at the actor level using
    IPrococol::DeallocShmem or because the actor has died, in DEBUG builds
    the memory mapping used by the Shmem is immediately mapped as
    non-readable and non-writable. As this can happen at any time, this can
    make using one of these Shmems from a different thread than the actor it
    was originally bound to quite difficult. As an example, in bug 1812498,
    special logic was added to VideoBridgeParent::OnChannelError to block
    the actor's thread until off-thread uses of the Shmem were complete.
    
    This behaviour was always debug-only, and is actually unnecessary. The
    actual Shmem mapping is not managed by the actor, only the ability to
    send it between processes, so it should be reasonable for one of these
    objects to be used after the actor it was sent over is destroyed. This
    patch does not change the behaviour of mapping the shared memory pages
    as inaccessible for non-unsafe shmems while they are "owned" by another
    process.
    
    In the future, we may want to change the name "DeallocShmem" to be
    something more representative of the true behaviour of the function
    (which is to prevent further sending of the Shmem object over IPDL,
    freeing IPDL's reference to the underlying shared memory segment).
    
    Differential Revision: https://phabricator.services.mozilla.com/D197828

--- ipc/glue/Shmem.cpp
+++ ipc/glue/Shmem.cpp
@@ -62,15 +62,19 @@ class ShmemDestroyed : public IPC::Message {
   }
 };
 
-static SharedMemory* NewSegment() { return new SharedMemoryBasic; }
+static already_AddRefed<SharedMemory> NewSegment() {
+  return MakeAndAddRef<SharedMemoryBasic>();
+}
 
-static already_AddRefed<SharedMemory> CreateSegment(size_t aNBytes,
-                                                    size_t aExtraSize) {
+static already_AddRefed<SharedMemory> CreateSegment(size_t aNBytes) {
+  if (!aNBytes) {
+    return nullptr;
+  }
   RefPtr<SharedMemory> segment = NewSegment();
   if (!segment) {
     return nullptr;
   }
-  size_t size = SharedMemory::PageAlignedSize(aNBytes + aExtraSize);
+  size_t size = SharedMemory::PageAlignedSize(aNBytes);
   if (!segment->Create(size) || !segment->Map(size)) {
     return nullptr;
   }
@@ -78,8 +82,7 @@ static already_AddRefed<SharedMemory> CreateSegment(size_t aNBytes,
 }
 
 static already_AddRefed<SharedMemory> ReadSegment(
-    const IPC::Message& aDescriptor, Shmem::id_t* aId, size_t* aNBytes,
-    size_t aExtraSize) {
+    const IPC::Message& aDescriptor, Shmem::id_t* aId, size_t* aNBytes) {
   if (SHMEM_CREATED_MESSAGE_TYPE != aDescriptor.type()) {
     NS_ERROR("expected 'shmem created' message");
     return nullptr;
@@ -97,7 +100,10 @@ static already_AddRefed<SharedMemory> ReadSegment(
     return nullptr;
   }
   reader.EndRead();
-  size_t size = SharedMemory::PageAlignedSize(*aNBytes + aExtraSize);
+  if (!*aNBytes) {
+    return nullptr;
+  }
+  size_t size = SharedMemory::PageAlignedSize(*aNBytes);
   if (!segment->Map(size)) {
     return nullptr;
   }
@@ -106,53 +112,7 @@ static already_AddRefed<SharedMemory> ReadSegment(
   return segment.forget();
 }
 
-static void DestroySegment(SharedMemory* aSegment) {
-  // the SharedMemory dtor closes and unmaps the actual OS shmem segment
-  if (aSegment) {
-    aSegment->Release();
-  }
-}
-
 #if defined(DEBUG)
-
-static const char sMagic[] =
-    "This little piggy went to market.\n"
-    "This little piggy stayed at home.\n"
-    "This little piggy has roast beef,\n"
-    "This little piggy had none.\n"
-    "And this little piggy cried \"Wee! Wee! Wee!\" all the way home";
-
-struct Header {
-  // Don't use size_t or bool here because their size depends on the
-  // architecture.
-  uint32_t mSize;
-  uint32_t mUnsafe;
-  char mMagic[sizeof(sMagic)];
-};
-
-static void GetSections(Shmem::SharedMemory* aSegment, Header** aHeader,
-                        char** aFrontSentinel, char** aData,
-                        char** aBackSentinel) {
-  MOZ_ASSERT(aSegment && aFrontSentinel && aData && aBackSentinel,
-             "null param(s)");
-
-  *aFrontSentinel = reinterpret_cast<char*>(aSegment->memory());
-  MOZ_ASSERT(*aFrontSentinel, "null memory()");
-
-  *aHeader = reinterpret_cast<Header*>(*aFrontSentinel);
-
-  size_t pageSize = Shmem::SharedMemory::SystemPageSize();
-  *aData = *aFrontSentinel + pageSize;
-
-  *aBackSentinel = *aFrontSentinel + aSegment->Size() - pageSize;
-}
-
-static Header* GetHeader(Shmem::SharedMemory* aSegment) {
-  Header* header;
-  char* dontcare;
-  GetSections(aSegment, &header, &dontcare, &dontcare, &dontcare);
-  return header;
-}
 
 static void Protect(SharedMemory* aSegment) {
   MOZ_ASSERT(aSegment, "null segment");
@@ -164,90 +124,6 @@ static void Unprotect(SharedMemory* aSegment) {
   MOZ_ASSERT(aSegment, "null segment");
   aSegment->Protect(reinterpret_cast<char*>(aSegment->memory()),
                     aSegment->Size(), RightsRead | RightsWrite);
-}
-
-//
-// In debug builds, we specially allocate shmem segments.  The layout
-// is as follows
-//
-//   Page 0: "front sentinel"
-//     size of mapping
-//     magic bytes
-//   Page 1 through n-1:
-//     user data
-//   Page n: "back sentinel"
-//     [nothing]
-//
-// The mapping can be in one of the following states, wrt to the
-// current process.
-//
-//   State "unmapped": all pages are mapped with no access rights.
-//
-//   State "mapping": all pages are mapped with read/write access.
-//
-//   State "mapped": the front and back sentinels are mapped with no
-//     access rights, and all the other pages are mapped with
-//     read/write access.
-//
-// When a SharedMemory segment is first allocated, it starts out in
-// the "mapping" state for the process that allocates the segment, and
-// in the "unmapped" state for the other process.  The allocating
-// process will then create a Shmem, which takes the segment into the
-// "mapped" state, where it can be accessed by clients.
-//
-// When a Shmem is sent to another process in an IPDL message, the
-// segment transitions into the "unmapped" state for the sending
-// process, and into the "mapping" state for the receiving process.
-// The receiving process will then create a Shmem from the underlying
-// segment, and take the segment into the "mapped" state.
-//
-// In the "mapping" state, we use the front sentinel to verify the
-// integrity of the shmem segment.  If valid, it has a size_t
-// containing the number of bytes the user allocated followed by the
-// magic bytes above.
-//
-// In the "mapped" state, the front and back sentinels have no access
-// rights.  They act as guards against buffer overflows and underflows
-// in client code; if clients touch a sentinel, they die with SIGSEGV.
-//
-// The "unmapped" state is used to enforce single-owner semantics of
-// the shmem segment.  If a process other than the current owner tries
-// to touch the segment, it dies with SIGSEGV.
-//
-
-Shmem::Shmem(SharedMemory* aSegment, id_t aId)
-    : mSegment(aSegment), mData(nullptr), mSize(0) {
-  MOZ_ASSERT(mSegment, "null segment");
-  MOZ_ASSERT(aId != 0, "invalid ID");
-
-  Unprotect(mSegment);
-
-  Header* header;
-  char* frontSentinel;
-  char* data;
-  char* backSentinel;
-  GetSections(aSegment, &header, &frontSentinel, &data, &backSentinel);
-
-  // do a quick validity check to avoid weird-looking crashes in libc
-  char check = *frontSentinel;
-  (void)check;
-
-  MOZ_ASSERT(!strncmp(header->mMagic, sMagic, sizeof(sMagic)),
-             "invalid segment");
-  mSize = static_cast<size_t>(header->mSize);
-
-  size_t pageSize = SharedMemory::SystemPageSize();
-  MOZ_ASSERT(mSegment->Size() - (2 * pageSize) >= mSize,
-             "illegal size in shared memory segment");
-
-  // transition into the "mapped" state by protecting the front and
-  // back sentinels (which guard against buffer under/overflows)
-  mSegment->Protect(frontSentinel, pageSize, RightsNone);
-  mSegment->Protect(backSentinel, pageSize, RightsNone);
-
-  // don't set these until we know they're valid
-  mData = data;
-  mId = aId;
 }
 
 void Shmem::AssertInvariants() const {
@@ -267,124 +143,32 @@ void Shmem::AssertInvariants() const {
 void Shmem::RevokeRights() {
   AssertInvariants();
 
-  size_t pageSize = SharedMemory::SystemPageSize();
-  Header* header = GetHeader(mSegment);
-
-  // Open this up for reading temporarily
-  mSegment->Protect(reinterpret_cast<char*>(header), pageSize, RightsRead);
-
-  if (!header->mUnsafe) {
+  // When sending a non-unsafe shmem, remove read/write rights from the local
+  // mapping of the segment.
+  if (!mUnsafe) {
     Protect(mSegment);
-  } else {
-    mSegment->Protect(reinterpret_cast<char*>(header), pageSize, RightsNone);
   }
 }
 
-// static
-already_AddRefed<Shmem::SharedMemory> Shmem::Alloc(size_t aNBytes, bool aUnsafe,
-                                                   bool aProtect) {
-  NS_ASSERTION(aNBytes <= UINT32_MAX, "Will truncate shmem segment size!");
-  MOZ_ASSERT(!aProtect || !aUnsafe, "protect => !unsafe");
+#endif  // if defined(DEBUG)
 
-  size_t pageSize = SharedMemory::SystemPageSize();
-  // |2*pageSize| is for the front and back sentinel
-  RefPtr<SharedMemory> segment = CreateSegment(aNBytes, 2 * pageSize);
-  if (!segment) {
-    return nullptr;
-  }
+Shmem::Shmem(SharedMemory* aSegment, id_t aId, size_t aSize, bool aUnsafe)
+    : mSegment(aSegment), mData(aSegment->memory()), mSize(aSize), mId(aId) {
+#ifdef DEBUG
+  mUnsafe = aUnsafe;
+  Unprotect(mSegment);
+#endif
 
-  Header* header;
-  char* frontSentinel;
-  char* data;
-  char* backSentinel;
-  GetSections(segment, &header, &frontSentinel, &data, &backSentinel);
-
-  // initialize the segment with Shmem-internal information
-
-  // NB: this can't be a static assert because technically pageSize
-  // isn't known at compile time, event though in practice it's always
-  // going to be 4KiB
-  MOZ_ASSERT(sizeof(Header) <= pageSize, "Shmem::Header has gotten too big");
-  memcpy(header->mMagic, sMagic, sizeof(sMagic));
-  header->mSize = static_cast<uint32_t>(aNBytes);
-  header->mUnsafe = aUnsafe;
-
-  if (aProtect) Protect(segment);
-
-  return segment.forget();
-}
-
-// static
-already_AddRefed<Shmem::SharedMemory> Shmem::OpenExisting(
-    const IPC::Message& aDescriptor, id_t* aId, bool aProtect) {
-  size_t size;
-  size_t pageSize = SharedMemory::SystemPageSize();
-  // |2*pageSize| is for the front and back sentinels
-  RefPtr<SharedMemory> segment =
-      ReadSegment(aDescriptor, aId, &size, 2 * pageSize);
-  if (!segment) {
-    return nullptr;
-  }
-
-  Header* header = GetHeader(segment);
-
-  if (size != header->mSize) {
-    // Deallocation should zero out the header, so check for that.
-    if (header->mSize || header->mUnsafe || header->mMagic[0] ||
-        memcmp(header->mMagic, &header->mMagic[1],
-               sizeof(header->mMagic) - 1)) {
-      NS_ERROR("Wrong size for this Shmem!");
-    } else {
-      NS_WARNING("Shmem was deallocated");
-    }
-    return nullptr;
-  }
-
-  // The caller of this function may not know whether the segment is
-  // unsafe or not
-  if (!header->mUnsafe && aProtect) Protect(segment);
-
-  return segment.forget();
-}
-
-// static
-void Shmem::Dealloc(SharedMemory* aSegment) {
-  if (!aSegment) return;
-
-  size_t pageSize = SharedMemory::SystemPageSize();
-  Header* header;
-  char* frontSentinel;
-  char* data;
-  char* backSentinel;
-  GetSections(aSegment, &header, &frontSentinel, &data, &backSentinel);
-
-  aSegment->Protect(frontSentinel, pageSize, RightsWrite | RightsRead);
-  memset(header->mMagic, 0, sizeof(sMagic));
-  header->mSize = 0;
-  header->mUnsafe = false;  // make it "safe" so as to catch errors
-
-  DestroySegment(aSegment);
-}
-
-#else  // !defined(DEBUG)
-
-Shmem::Shmem(SharedMemory* aSegment, id_t aId)
-    : mSegment(aSegment), mData(aSegment->memory()), mSize(0), mId(aId) {
-  mSize = static_cast<size_t>(*PtrToSize(mSegment));
-  MOZ_RELEASE_ASSERT(mSegment->Size() - sizeof(uint32_t) >= mSize,
+  MOZ_RELEASE_ASSERT(mSegment->Size() >= mSize,
                      "illegal size in shared memory segment");
 }
 
 // static
-already_AddRefed<Shmem::SharedMemory> Shmem::Alloc(size_t aNBytes,
-                                                   bool /*unused*/,
-                                                   bool /*unused*/) {
-  RefPtr<SharedMemory> segment = CreateSegment(aNBytes, sizeof(uint32_t));
+already_AddRefed<Shmem::SharedMemory> Shmem::Alloc(size_t aNBytes) {
+  RefPtr<SharedMemory> segment = CreateSegment(aNBytes);
   if (!segment) {
     return nullptr;
   }
-
-  *PtrToSize(segment) = static_cast<uint32_t>(aNBytes);
 
   return segment.forget();
 }
@@ -393,24 +177,13 @@ already_AddRefed<Shmem::SharedMemory> Shmem::Alloc(size_t aNBytes,
 already_AddRefed<Shmem::SharedMemory> Shmem::OpenExisting(
     const IPC::Message& aDescriptor, id_t* aId, bool /*unused*/) {
   size_t size;
-  RefPtr<SharedMemory> segment =
-      ReadSegment(aDescriptor, aId, &size, sizeof(uint32_t));
+  RefPtr<SharedMemory> segment = ReadSegment(aDescriptor, aId, &size);
   if (!segment) {
-    return nullptr;
-  }
-
-  // this is the only validity check done in non-DEBUG builds
-  if (size != static_cast<size_t>(*PtrToSize(segment))) {
     return nullptr;
   }
 
   return segment.forget();
 }
-
-// static
-void Shmem::Dealloc(SharedMemory* aSegment) { DestroySegment(aSegment); }
-
-#endif  // if defined(DEBUG)
 
 UniquePtr<IPC::Message> Shmem::MkCreatedMessage(int32_t routingId) {
   AssertInvariants();
@@ -433,6 +206,10 @@ UniquePtr<IPC::Message> Shmem::MkDestroyedMessage(int32_t routingId) {
 void IPDLParamTraits<Shmem>::Write(IPC::MessageWriter* aWriter,
                                    IProtocol* aActor, Shmem&& aParam) {
   WriteIPDLParam(aWriter, aActor, aParam.mId);
+  WriteIPDLParam(aWriter, aActor, uint32_t(aParam.mSize));
+#ifdef DEBUG
+  WriteIPDLParam(aWriter, aActor, aParam.mUnsafe);
+#endif
 
   aParam.RevokeRights();
   aParam.forget();
@@ -441,13 +218,26 @@ void IPDLParamTraits<Shmem>::Write(IPC::MessageWriter* aWriter,
 bool IPDLParamTraits<Shmem>::Read(IPC::MessageReader* aReader,
                                   IProtocol* aActor, paramType* aResult) {
   paramType::id_t id;
-  if (!ReadIPDLParam(aReader, aActor, &id)) {
+  uint32_t size;
+  if (!ReadIPDLParam(aReader, aActor, &id) ||
+      !ReadIPDLParam(aReader, aActor, &size)) {
     return false;
   }
 
+  bool unsafe = false;
+#ifdef DEBUG
+  if (!ReadIPDLParam(aReader, aActor, &unsafe)) {
+    return false;
+  }
+#endif
+
   Shmem::SharedMemory* rawmem = aActor->LookupSharedMemory(id);
   if (rawmem) {
-    *aResult = Shmem(rawmem, id);
+    if (size > rawmem->Size()) {
+      return false;
+    }
+
+    *aResult = Shmem(rawmem, id, size, unsafe);
     return true;
   }
   *aResult = Shmem();
