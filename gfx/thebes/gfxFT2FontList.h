595e406488b081b718e757404e9de0e74ed53fcb
Tom Ritter <tom@mozilla.com>
2024-02-05 19:30:40 +0000

    Bug 1826412: Apply Font Visibility Restrictions to Android r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D200363

--- gfx/thebes/gfxFT2FontList.h
+++ gfx/thebes/gfxFT2FontList.h
@@ -187,6 +187,8 @@ class gfxFT2FontList final : public gfxPlatformFontList {
   // initialize font lists
   nsresult InitFontListForPlatform() MOZ_REQUIRES(mLock) override;
 
+  FontVisibility GetVisibilityForFamily(const nsACString& aName) const;
+
   void AppendFaceFromFontListEntry(const FontListEntry& aFLE,
                                    StandardFile aStdFile) MOZ_REQUIRES(mLock);
 
