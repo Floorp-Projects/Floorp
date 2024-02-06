f1f6ae473f0bd4aa9baea8c2411d3c314f4c355e
Jonathan Kew <jkew@mozilla.com>
2024-02-06 11:41:08 +0000

    Bug 1824671 - patch 1a - Bump unicode-bidi to v0.3.15 to pick up bug-fix #119 and extended get_base_direction_full API. r=supply-chain-reviewers,dminor
    Differential Revision: https://phabricator.services.mozilla.com/D197888

--- third_party/rust/unicode-bidi/src/prepare.rs
+++ third_party/rust/unicode-bidi/src/prepare.rs
@@ -59,7 +59,17 @@ pub fn isolating_run_sequences(
         assert!(!stack.is_empty());
 
         let start_class = original_classes[run.start];
-        let end_class = original_classes[run.end - 1];
+        // > In rule X10, [..] skip over any BNs when [..].
+        // > Do the same when determining if the last character of the sequence is an isolate initiator.
+        //
+        // <https://www.unicode.org/reports/tr9/#Retaining_Explicit_Formatting_Characters>
+        let end_class = original_classes[run.start..run.end]
+            .iter()
+            .copied()
+            .rev()
+            .filter(not_removed_by_x9)
+            .next()
+            .unwrap_or(start_class);
 
         let mut sequence = if start_class == PDI && stack.len() > 1 {
             // Continue a previous sequence interrupted by an isolate.
