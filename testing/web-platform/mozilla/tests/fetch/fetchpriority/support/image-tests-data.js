e1e8bb9943ce1ca3d5d2efcafbc1be7cc49ee8ff
Frédéric Wang <fwang@igalia.com>
2024-02-06 18:57:43 +0000

    Bug 1878831 - Add tests for dynamically loaded images with fetchpriority disabled. r=valentin,necko-reviewers
    These tests were added in D194874 but currently fail because
    fetchpriority is not implemented yet for images (bug 1839313). We make
    these tests run with `network.fetchpriority` disabled with the current
    behavior (always use PRIORITY_LOW), so we can check whether it changes
    when we implement support for that case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D200782

--- testing/web-platform/mozilla/tests/fetch/fetchpriority/support/image-tests-data.js
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/image-tests-data.js
@@ -19,8 +19,29 @@ const kExpectedRequests = [
     },
 ];
 
+const kExpectedRequestsDisabled = [
+    {   fileNameAndSuffix: "square_25px_x_25px.png?1",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "square_25px_x_25px.png?2",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "square_25px_x_25px.png?3",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+    {   fileNameAndSuffix: "square_25px_x_25px.png?4",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
+    },
+];
+
 export const kTestData = [
     {   testFileName: "image-dynamic-load.h2.html",
         expectedRequests: kExpectedRequests
+    },
+];
+
+export const kTestDataDisabled = [
+    {   testFileName: "image-dynamic-load.h2.html",
+        expectedRequests: kExpectedRequestsDisabled
     },
 ];
