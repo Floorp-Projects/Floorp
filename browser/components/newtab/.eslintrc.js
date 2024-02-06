41bdb282285785a6c2276f2d9f4ac6dfb2bed44b
Mark Banner <standard8@mozilla.com>
2024-02-06 22:32:53 +0000

    Bug 1878554 - Remove ESLint rule require-await for aboutwelcome/asrouter/newtab code. r=aminomancer,omc-reviewers
    We decided not to use the rule in bug 1381030.
    
    Differential Revision: https://phabricator.services.mozilla.com/D200587

--- browser/components/newtab/.eslintrc.js
+++ browser/components/newtab/.eslintrc.js
@@ -88,7 +88,6 @@ module.exports = {
       rules: {
         "func-name-matching": 0,
         "lines-between-class-members": 0,
-        "require-await": 0,
       },
     },
   ],
@@ -185,7 +184,6 @@ module.exports = {
     "prefer-spread": "error",
     "prefer-template": "error",
     radix: ["error", "always"],
-    "require-await": "error",
     "sort-vars": "error",
     "symbol-description": "error",
     "vars-on-top": "error",
