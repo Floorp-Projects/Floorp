1505a000a78ca3043b0b65a641f73838b0798070
Jan de Mooij <jdemooij@mozilla.com>
2024-02-06 12:51:31 +0000

    Bug 1877193 part 10 - Use JS::Prefs for experimental.shadow_realms pref. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D200001

--- js/xpconnect/src/XPCJSContext.cpp
+++ js/xpconnect/src/XPCJSContext.cpp
@@ -780,7 +780,6 @@ static mozilla::Atomic<bool> sStreamsEnabled(false);
 
 static mozilla::Atomic<bool> sWeakRefsEnabled(false);
 static mozilla::Atomic<bool> sWeakRefsExposeCleanupSome(false);
-static mozilla::Atomic<bool> sShadowRealmsEnabled(false);
 #ifdef NIGHTLY_BUILD
 static mozilla::Atomic<bool> sNewSetMethodsEnabled(false);
 static mozilla::Atomic<bool> sArrayBufferResizableEnabled(false);
@@ -806,7 +805,6 @@ void xpc::SetPrefableRealmOptions(JS::RealmOptions& options) {
           StaticPrefs::browser_tabs_remote_useCrossOriginOpenerPolicy() &&
           StaticPrefs::browser_tabs_remote_useCrossOriginEmbedderPolicy())
       .setWeakRefsEnabled(GetWeakRefsEnabled())
-      .setShadowRealmsEnabled(sShadowRealmsEnabled)
 #ifdef NIGHTLY_BUILD
       .setNewSetMethodsEnabled(sNewSetMethodsEnabled)
       .setArrayBufferResizableEnabled(sArrayBufferResizableEnabled)
@@ -1014,8 +1012,6 @@ static void ReloadPrefsCallback(const char* pref, void* aXpccx) {
   sWeakRefsEnabled = Preferences::GetBool(JS_OPTIONS_DOT_STR "weakrefs");
   sWeakRefsExposeCleanupSome = Preferences::GetBool(
       JS_OPTIONS_DOT_STR "experimental.weakrefs.expose_cleanupSome");
-  sShadowRealmsEnabled =
-      Preferences::GetBool(JS_OPTIONS_DOT_STR "experimental.shadow_realms");
 #ifdef NIGHTLY_BUILD
   sNewSetMethodsEnabled =
       Preferences::GetBool(JS_OPTIONS_DOT_STR "experimental.new_set_methods");
