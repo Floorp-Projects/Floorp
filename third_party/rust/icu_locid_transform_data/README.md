8d621dd715cf099ca7723619b1b3b58bf0ece289
Jonathan Kew <jkew@mozilla.com>
2024-02-06 11:41:11 +0000

    Bug 1824671 - patch 9 - Run ./mach vendor rust to bring in icu_properties. r=supply-chain-reviewers,dminor
    Differential Revision: https://phabricator.services.mozilla.com/D198448

---
+++ third_party/rust/icu_locid_transform_data/README.md
@@ -0,0 +1,11 @@
+# icu_locid_transform_data [![crates.io](https://img.shields.io/crates/v/icu_locid_transform_data)](https://crates.io/crates/icu_locid_transform_data)
+
+<!-- cargo-rdme start -->
+
+Data for the icu_locid_transform crate
+
+<!-- cargo-rdme end -->
+
+## More Information
+
+For more information on development, authorship, contributing etc. please visit [`ICU4X home page`](https://github.com/unicode-org/icu4x).
---
+++ third_party/rust/icu_locid_transform_data/data/macros.rs
@@ -0,0 +1,66 @@
+// @generated
+/// Marks a type as a data provider. You can then use macros like
+/// `impl_core_helloworld_v1` to add implementations.
+///
+/// ```ignore
+/// struct MyProvider;
+/// const _: () = {
+///     include!("path/to/generated/macros.rs");
+///     make_provider!(MyProvider);
+///     impl_core_helloworld_v1!(MyProvider);
+/// }
+/// ```
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __make_provider {
+    ($ name : ty) => {
+        #[clippy::msrv = "1.67"]
+        impl $name {
+            #[doc(hidden)]
+            #[allow(dead_code)]
+            pub const MUST_USE_MAKE_PROVIDER_MACRO: () = ();
+        }
+    };
+}
+#[doc(inline)]
+pub use __make_provider as make_provider;
+#[macro_use]
+#[path = "macros/fallback_likelysubtags_v1.rs.data"]
+mod fallback_likelysubtags_v1;
+#[doc(inline)]
+pub use __impl_fallback_likelysubtags_v1 as impl_fallback_likelysubtags_v1;
+#[macro_use]
+#[path = "macros/fallback_parents_v1.rs.data"]
+mod fallback_parents_v1;
+#[doc(inline)]
+pub use __impl_fallback_parents_v1 as impl_fallback_parents_v1;
+#[macro_use]
+#[path = "macros/fallback_supplement_co_v1.rs.data"]
+mod fallback_supplement_co_v1;
+#[doc(inline)]
+pub use __impl_fallback_supplement_co_v1 as impl_fallback_supplement_co_v1;
+#[macro_use]
+#[path = "macros/locid_transform_aliases_v1.rs.data"]
+mod locid_transform_aliases_v1;
+#[doc(inline)]
+pub use __impl_locid_transform_aliases_v1 as impl_locid_transform_aliases_v1;
+#[macro_use]
+#[path = "macros/locid_transform_likelysubtags_ext_v1.rs.data"]
+mod locid_transform_likelysubtags_ext_v1;
+#[doc(inline)]
+pub use __impl_locid_transform_likelysubtags_ext_v1 as impl_locid_transform_likelysubtags_ext_v1;
+#[macro_use]
+#[path = "macros/locid_transform_likelysubtags_l_v1.rs.data"]
+mod locid_transform_likelysubtags_l_v1;
+#[doc(inline)]
+pub use __impl_locid_transform_likelysubtags_l_v1 as impl_locid_transform_likelysubtags_l_v1;
+#[macro_use]
+#[path = "macros/locid_transform_likelysubtags_sr_v1.rs.data"]
+mod locid_transform_likelysubtags_sr_v1;
+#[doc(inline)]
+pub use __impl_locid_transform_likelysubtags_sr_v1 as impl_locid_transform_likelysubtags_sr_v1;
+#[macro_use]
+#[path = "macros/locid_transform_script_dir_v1.rs.data"]
+mod locid_transform_script_dir_v1;
+#[doc(inline)]
+pub use __impl_locid_transform_script_dir_v1 as impl_locid_transform_script_dir_v1;
