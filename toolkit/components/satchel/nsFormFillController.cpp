9d8f7548bfea7d685d52d1ad1f4c7cb9ca821789
pstanciu <pstanciu@mozilla.com>
2024-02-07 15:41:35 +0200

    Backed out changeset 5c3a58685415 (bug 1828259) for causing test failures on the GV bump on firefox-android. a=backout CLOSED TREE

--- toolkit/components/satchel/nsFormFillController.cpp
+++ toolkit/components/satchel/nsFormFillController.cpp
@@ -895,9 +895,6 @@ nsFormFillController::HandleEvent(Event* aEvent) {
         RemoveForDocument(doc);
       }
     } break;
-    case eResize:
-      SetPopupOpen(false);
-      break;
     default:
       // Handling the default case to shut up stupid -Wswitch warnings.
       // One day compilers will be smarter...
@@ -925,7 +922,6 @@ void nsFormFillController::AttachListeners(EventTarget* aEventTarget) {
                               TrustedEventsAtCapture());
   elm->AddEventListenerByType(this, u"contextmenu"_ns,
                               TrustedEventsAtCapture());
-  elm->AddEventListenerByType(this, u"resize"_ns, TrustedEventsAtCapture());
 }
 
 void nsFormFillController::RemoveForDocument(Document* aDoc) {
