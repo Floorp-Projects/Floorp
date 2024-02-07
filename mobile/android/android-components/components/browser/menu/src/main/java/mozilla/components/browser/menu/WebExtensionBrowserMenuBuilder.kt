df3f5e2a966caf4c3908d7059d22457632a8165d
William Durand <will+git@drnd.me>
2024-02-07 16:01:41 +0100

    Bug 1870319 - Announce title in add-ons sub-menu and use "navigate back" instead of "navigate up"

--- mobile/android/android-components/components/browser/menu/src/main/java/mozilla/components/browser/menu/WebExtensionBrowserMenuBuilder.kt
+++ mobile/android/android-components/components/browser/menu/src/main/java/mozilla/components/browser/menu/WebExtensionBrowserMenuBuilder.kt
@@ -91,7 +91,7 @@ class WebExtensionBrowserMenuBuilder(
     ): List<BrowserMenuItem> {
         val addonsMenuItem = if (filteredExtensionMenuItems.isNotEmpty()) {
             val backPressMenuItem = BackPressMenuItem(
-                contentDescription = context.getString(R.string.action_bar_up_description),
+                contentDescription = context.getString(R.string.mozac_browser_menu_addons_description),
                 label = context.getString(R.string.mozac_browser_menu_addons),
                 imageResource = style.backPressMenuItemDrawableRes,
                 iconTintColorResource = style.webExtIconTintColorResource,
