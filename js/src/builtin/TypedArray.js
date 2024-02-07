1f3a3c106c98f07149b74be15d442792a4b2813f
André Bargull <andre.bargull@gmail.com>
2024-02-07 13:02:49 +0000

    Bug 1878658 - Part 3: Fix variable name typo in TypedArrayJoin. r=spidermonkey-reviewers,sfink
    Depends on D200637
    
    Differential Revision: https://phabricator.services.mozilla.com/D200638

--- js/src/builtin/TypedArray.js
+++ js/src/builtin/TypedArray.js
@@ -701,7 +701,7 @@ function TypedArrayJoin(separator) {
   // ToString() might have detached or resized the underlying ArrayBuffer. To avoid
   // checking for this condition when looping in step 8.c, do it once here.
   if (limit === 0) {
-    return callFunction(String_repeat, separator, len - 1);
+    return callFunction(String_repeat, sep, len - 1);
   }
 
   assert(
@@ -730,7 +730,7 @@ function TypedArrayJoin(separator) {
   }
 
   if (limit < len) {
-    R += callFunction(String_repeat, separator, len - limit);
+    R += callFunction(String_repeat, sep, len - limit);
   }
 
   // Step 9.
