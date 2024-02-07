e8f88fac9c43de93a0c68012222c5b608394a2b4
Cristian Tuns <ctuns@mozilla.com>
2024-02-07 01:56:15 -0500

    Backed out 7 changesets (bug 1869371) for causing xpcshell failures in /test_telemetry.js CLOSED TREE
    Backed out changeset 1a5a7790c117 (bug 1869371)
    Backed out changeset b1e169c2ffb2 (bug 1869371)
    Backed out changeset ab8c2d6a01ba (bug 1869371)
    Backed out changeset 4f2ac5fe4356 (bug 1869371)
    Backed out changeset b2211ce1cbcc (bug 1869371)
    Backed out changeset 51827faf0339 (bug 1869371)
    Backed out changeset 673ddb6bc4b3 (bug 1869371)

--- toolkit/components/extensions/test/xpcshell/test_native_manifests.js
+++ toolkit/components/extensions/test/xpcshell/test_native_manifests.js
@@ -313,7 +313,7 @@ add_task(async function test_manifest_with_invalid_utf_8() {
   );
   equal(result, null, "lookupApplication should reject file with invalid UTF8");
   let errorPattern =
-    /NotReadableError: Could not read `.*': file is not UTF-8 encoded/;
+    /NotReadableError: Could not read file.* because it is not UTF-8 encoded/;
   let utf8Errors = messages.filter(({ message }) => errorPattern.test(message));
   equal(utf8Errors.length, 1, "lookupApplication logs error about UTF-8");
 });
