a37ae20735ea2b57fdca0d7a459c8b9700a162a2
Nicolas Chevobbe <nchevobbe@mozilla.com>
2024-02-08 14:39:41 +0000

    Bug 1877989 - [devtools] Add CodeMirror 6 in tree. r=devtools-reviewers,bomsy.
    Differential Revision: https://phabricator.services.mozilla.com/D200305

---
+++ devtools/client/shared/sourceeditor/codemirror6/index.mjs
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import * as codemirror from "codemirror";
+import * as codemirrorView from "@codemirror/view";
+import * as codemirrorState from "@codemirror/state";
+import * as codemirrorLanguage from "@codemirror/language";
+import * as codemirrorLangJavascript from "@codemirror/lang-javascript";
+import * as lezerHighlight from "@lezer/highlight";
+
+// XXX When adding new exports, you need to update the codemirror6.bundle.js file,
+// running `npm install && npm run build-cm6` from the devtools/client/shared/sourceeditor folder
+
+export {
+  codemirror,
+  codemirrorView,
+  codemirrorState,
+  codemirrorLanguage,
+  codemirrorLangJavascript,
+  lezerHighlight,
+};
---
+++ devtools/client/shared/sourceeditor/rollup.config.mjs
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* eslint-disable import/no-unresolved */
+import terser from "@rollup/plugin-terser";
+import nodeResolve from "@rollup/plugin-node-resolve";
+
+export default function (commandLineArgs) {
+  const plugins = [nodeResolve()];
+  if (commandLineArgs.minified) {
+    plugins.push(terser());
+  }
+
+  return {
+    input: "codemirror6/index.mjs",
+    output: {
+      file: "codemirror6/codemirror6.bundle.js",
+      format: "iife",
+      name: "CodeMirror",
+    },
+    plugins,
+  };
+}
