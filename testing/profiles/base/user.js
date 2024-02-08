178c00bf64e015c947a38ce4ed76300097be8958
Issam Mani <imani@mozilla.com>
2024-02-08 21:14:20 +0000

    Bug 1865705 - Use nimbus flag to control address autofill. r=credential-management-reviewers,dimi
    Differential Revision: https://phabricator.services.mozilla.com/D194149

--- testing/profiles/base/user.js
+++ testing/profiles/base/user.js
@@ -8,3 +8,6 @@
 user_pref("gfx.webrender.all", true);
 user_pref("dom.input_events.security.minNumTicks", 0);
 user_pref("dom.input_events.security.minTimeElapsedInMS", 0);
+
+// Set address autofill to true for tests
+user_pref("extensions.formautofill.addresses.experiments.enabled", true);
