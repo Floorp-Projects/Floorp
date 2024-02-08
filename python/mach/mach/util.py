f8a8bbcdae63ea6bd5d94b221c6457c7f59f39fb
ahochheiden <ahochheiden@mozilla.com>
2024-02-08 21:07:26 +0000

    Bug 1877347 - Replace `distutils.util.strtobool` with a custom `strtobool` within `mach` r=firefox-build-system-reviewers,glandium
    The migration advice (https://peps.python.org/pep-0632/#migration-advice)
    suggests reimplementing the functionality as per the docs (https://docs.python.org/3.9/distutils/apiref.html#distutils.util.strtobool)
    which is exactly what I've done here.
    
    Differential Revision: https://phabricator.services.mozilla.com/D199950

--- python/mach/mach/util.py
+++ python/mach/mach/util.py
@@ -115,3 +115,18 @@ def to_optional_str(path: Optional[Path]):
         return str(path)
     else:
         return None
+
+
+def strtobool(value: str):
+    # Reimplementation of distutils.util.strtobool
+    # https://docs.python.org/3.9/distutils/apiref.html#distutils.util.strtobool
+    true_vals = ("y", "yes", "t", "true", "on", "1")
+    false_vals = ("n", "no", "f", "false", "off", "0")
+
+    value = value.lower()
+    if value in true_vals:
+        return 1
+    if value in false_vals:
+        return 0
+
+    raise ValueError(f'Expected one of: {", ".join(true_vals + false_vals)}')
