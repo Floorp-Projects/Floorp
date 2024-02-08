5de7131ae3095ef9bb564c6f5a6a82a6cc0d0d36
Karandeep <klubana@mozilla.com>
2024-02-08 16:28:42 +0000

    Bug 1852051 - Remove the Search Bar from the toolbar for users who haven't used it in 120 days. r=dao,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D195001

--- browser/components/customizableui/SearchWidgetTracker.sys.mjs
+++ browser/components/customizableui/SearchWidgetTracker.sys.mjs
@@ -31,6 +31,7 @@ export const SearchWidgetTracker = {
     Services.prefs.addObserver(PREF_NAME, () =>
       this.syncWidgetWithPreference()
     );
+    this._updateSearchBarVisibilityBasedOnUsage();
   },
 
   onWidgetAdded(widgetId, area) {
@@ -91,6 +92,23 @@ export const SearchWidgetTracker = {
         null,
         "searchpref"
       );
+    }
+  },
+
+  _updateSearchBarVisibilityBasedOnUsage() {
+    let searchBarLastUsed = Services.prefs.getStringPref(
+      "browser.search.widget.lastUsed",
+      ""
+    );
+    if (searchBarLastUsed) {
+      const removeAfterDaysUnused = Services.prefs.getIntPref(
+        "browser.search.widget.removeAfterDaysUnused"
+      );
+      let saerchBarUnusedThreshold =
+        removeAfterDaysUnused * 24 * 60 * 60 * 1000;
+      if (new Date() - new Date(searchBarLastUsed) > saerchBarUnusedThreshold) {
+        Services.prefs.setBoolPref("browser.search.widget.inNavBar", false);
+      }
     }
   },
 
