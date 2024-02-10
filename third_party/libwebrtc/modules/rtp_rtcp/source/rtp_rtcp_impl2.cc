2b39a3fc08f149684b423ea672c3248331b5f80e
Jan-Ivar Bruaroey <jib@mozilla.com>
2024-02-10 13:30:54 -0500

    Bug 1876843 - Vendor libwebrtc from f1df16ceea
    Upstream commit: https://webrtc.googlesource.com/src/+/f1df16ceeaa5618762ac9949142bbdac9eca0bed
        Per default enable WebRTC-PaddingMode-RecentLargePacket
    
        This means that RtpPacketHistory::PaddingMode::kRecentLargePacket is
        used per default.
    
        Bug: webrtc:15201, b/284281602
        Change-Id: If8feb66105a9b1e13ae4cb28a44a74c8839b72e1
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/327602
        Commit-Queue: Per Kjellander <perkj@webrtc.org>
        Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
        Auto-Submit: Per Kjellander <perkj@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#41215}

--- third_party/libwebrtc/modules/rtp_rtcp/source/rtp_rtcp_impl2.cc
+++ third_party/libwebrtc/modules/rtp_rtcp/source/rtp_rtcp_impl2.cc
@@ -53,8 +53,8 @@ RTCPSender::Configuration AddRtcpSendEvaluationCallback(
 
 RtpPacketHistory::PaddingMode GetPaddingMode(
     const FieldTrialsView* field_trials) {
-  if (field_trials &&
-      field_trials->IsEnabled("WebRTC-PaddingMode-RecentLargePacket")) {
+  if (!field_trials ||
+      !field_trials->IsDisabled("WebRTC-PaddingMode-RecentLargePacket")) {
     return RtpPacketHistory::PaddingMode::kRecentLargePacket;
   }
   return RtpPacketHistory::PaddingMode::kPriority;
