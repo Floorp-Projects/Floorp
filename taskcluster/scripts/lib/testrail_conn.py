6a9c111474c1fc00dcf9a8a0d8f54c2b4c6198ea
Julien Cristau <jcristau@mozilla.com>
2024-02-12 12:05:27 +0100

    Bug 1825116 - add license header to testrail_conn script. r=jajohnson,bhearsum,releng-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D201564

--- taskcluster/scripts/lib/testrail_conn.py
+++ taskcluster/scripts/lib/testrail_conn.py
@@ -1,4 +1,9 @@
 # flake8: noqa
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """TestRail API binding for Python 3.x.
 
 (API v2, available since TestRail 3.0)
