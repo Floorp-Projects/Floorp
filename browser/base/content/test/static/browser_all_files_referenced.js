a8d79313539a95bc050578c8c65679615b5a8c31
Karandeep <klubana@mozilla.com>
2024-02-13 16:44:12 +0000

    Bug 1878264 - Remove ext-urlbar.js once condprof profiles are regenerated. r=mak,extension-reviewers,robwu
    Differential Revision: https://phabricator.services.mozilla.com/D200443

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -285,10 +285,6 @@ var allowlist = [
   { file: "chrome://browser/content/screenshots/copy.svg" },
   { file: "chrome://browser/content/screenshots/download.svg" },
   { file: "chrome://browser/content/screenshots/download-white.svg" },
-
-  // The file is temporarily left until a new conditioned profile is created,
-  // to be removed with bug 1878264
-  { file: "chrome://browser/content/parent/ext-urlbar.js" },
 ];
 
 if (AppConstants.platform != "win") {
