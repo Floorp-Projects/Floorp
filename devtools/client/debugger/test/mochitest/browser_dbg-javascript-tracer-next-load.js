300b31c25c1ac35e69032ba0099f3f880c144caf
Alexandre Poirot <poirot.alex@gmail.com>
2024-02-13 09:38:50 +0000

    Bug 1832135 - [devtools] Split JavaScript tracer test in many tests. r=devtools-reviewers,nchevobbe
    Doing all the tests in one-go would go over the timeout in debug builds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D201588

---
+++ devtools/client/debugger/test/mochitest/browser_dbg-javascript-tracer-next-load.js
@@ -0,0 +1,131 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// Tests tracing only on next page load
+
+"use strict";
+
+add_task(async function testTracingOnNextLoad() {
+  await pushPref("devtools.debugger.features.javascript-tracing", true);
+
+  await pushPref(
+    "devtools.debugger.features.javascript-tracing-log-method",
+    "console"
+  );
+  // Cover tracing function argument values
+  const jsCode = `function foo() {}; function bar() {}; foo(); dump("plop\\n")`;
+  const dbg = await initDebuggerWithAbsoluteURL(
+    "data:text/html," +
+      encodeURIComponent(`<script>${jsCode}</script><body></body>`)
+  );
+
+  const traceButton = findElement(dbg, "trace");
+
+  await openContextMenuInDebugger(dbg, "trace");
+  let toggled = waitForDispatch(
+    dbg.store,
+    "TOGGLE_JAVASCRIPT_TRACING_ON_NEXT_LOAD"
+  );
+  selectContextMenuItem(dbg, `#debugger-trace-menu-item-next-load`);
+  await toggled;
+  ok(true, "Toggled the trace on next load");
+
+  await clickElement(dbg, "trace");
+
+  ok(
+    !traceButton.classList.contains("pending"),
+    "Before toggling the trace button, it has no particular state"
+  );
+
+  info(
+    "Wait for the split console to be automatically displayed when toggling this setting"
+  );
+  await dbg.toolbox.getPanelWhenReady("webconsole");
+
+  invokeInTab("bar");
+
+  // Let some time for the call to be traced
+  await wait(500);
+
+  is(
+    (await findConsoleMessages(dbg.toolbox, "λ bar")).length,
+    0,
+    "The code isn't logged as the tracer shouldn't be started yet"
+  );
+
+  // Wait for the trace button to be highlighted
+  await waitFor(() => {
+    return traceButton.classList.contains("pending");
+  }, "The tracer button is pending before reloading the page");
+
+  info("Add an iframe to trigger a target creation");
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [], async function () {
+    const iframe = content.document.createElement("iframe");
+    iframe.src = "data:text/html,iframe";
+    content.document.body.appendChild(iframe);
+  });
+
+  // Let some time to regress and start the tracer
+  await wait(500);
+
+  ok(
+    traceButton.classList.contains("pending"),
+    "verify that the tracer is still pending after the iframe creation"
+  );
+
+  // Reload the page to trigger the tracer
+  await reload(dbg);
+
+  let topLevelThreadActorID =
+    dbg.toolbox.commands.targetCommand.targetFront.threadFront.actorID;
+  info("Wait for tracing to be enabled after page reload");
+  await waitForState(dbg, state => {
+    return dbg.selectors.getIsThreadCurrentlyTracing(topLevelThreadActorID);
+  });
+  ok(
+    traceButton.classList.contains("active"),
+    "The tracer button is now active after reload"
+  );
+
+  info("Wait for the split console to be displayed");
+  await dbg.toolbox.getPanelWhenReady("webconsole");
+
+  // Ensure that the very early code is traced
+  await hasConsoleMessage(dbg, "λ foo");
+
+  is(
+    (await findConsoleMessages(dbg.toolbox, "λ bar")).length,
+    0,
+    "The code ran before the reload isn't logged"
+  );
+
+  await clickElement(dbg, "trace");
+
+  topLevelThreadActorID =
+    dbg.toolbox.commands.targetCommand.targetFront.threadFront.actorID;
+  info("Wait for tracing to be disabled");
+  await waitForState(dbg, state => {
+    return dbg.selectors.getIsThreadCurrentlyTracing(topLevelThreadActorID);
+  });
+  await waitFor(() => {
+    return !traceButton.classList.contains("active");
+  }, "The tracer button is no longer active after stop request");
+
+  info("Toggle the setting back off");
+  await openContextMenuInDebugger(dbg, "trace");
+  toggled = waitForDispatch(
+    dbg.store,
+    "TOGGLE_JAVASCRIPT_TRACING_ON_NEXT_LOAD"
+  );
+  selectContextMenuItem(dbg, `#debugger-trace-menu-item-next-load`);
+  await toggled;
+  await waitFor(() => {
+    return !traceButton.classList.contains("pending");
+  }, "The tracer button is no longer pending after toggling the setting");
+
+  // Reset the trace on next interaction setting
+  Services.prefs.clearUserPref(
+    "devtools.debugger.javascript-tracing-on-next-interaction"
+  );
+});
---
+++ devtools/client/debugger/test/mochitest/browser_dbg-javascript-tracer-worker.js
@@ -0,0 +1,63 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// Tests tracing web workers
+
+"use strict";
+add_task(async function testTracingWorker() {
+  await pushPref("devtools.debugger.features.javascript-tracing", true);
+
+  // We have to enable worker targets and disable this pref to have functional tracing for workers
+  await pushPref("dom.worker.console.dispatch_events_to_main_thread", false);
+
+  const dbg = await initDebugger("doc-scripts.html");
+
+  info("Instantiate a worker");
+  const { targetCommand } = dbg.toolbox.commands;
+  let onAvailable;
+  const onNewTarget = new Promise(resolve => {
+    onAvailable = ({ targetFront }) => {
+      resolve(targetFront);
+    };
+  });
+  await targetCommand.watchTargets({
+    types: [targetCommand.TYPES.FRAME],
+    onAvailable,
+  });
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [], async function () {
+    content.worker = new content.Worker("simple-worker.js");
+  });
+  info("Wait for the worker target");
+  const workerTarget = await onNewTarget;
+
+  await waitFor(
+    () => findAllElements(dbg, "threadsPaneItems").length == 2,
+    "Wait for the two threads to be displayed in the thread pane"
+  );
+  const threadsEl = findAllElements(dbg, "threadsPaneItems");
+  is(threadsEl.length, 2, "There are two threads in the thread panel");
+
+  info("Enable tracing on all threads");
+  await clickElement(dbg, "trace");
+  info("Wait for tracing to be enabled for the worker");
+  await waitForState(dbg, state => {
+    return dbg.selectors.getIsThreadCurrentlyTracing(
+      workerTarget.threadFront.actorID
+    );
+  });
+
+  // `timer` is called within the worker via a setInterval of 1 second
+  await hasConsoleMessage(dbg, "setIntervalCallback");
+  await hasConsoleMessage(dbg, "λ timer");
+
+  // Also verify that postMessage are traced
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [], async function () {
+    content.worker.postMessage("foo");
+  });
+
+  await hasConsoleMessage(dbg, "DOM(message)");
+  await hasConsoleMessage(dbg, "λ onmessage");
+
+  await dbg.toolbox.closeToolbox();
+});
