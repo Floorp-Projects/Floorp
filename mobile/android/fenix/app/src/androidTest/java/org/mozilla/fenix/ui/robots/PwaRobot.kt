99f6434e7ac2ba0507c97daa31193df6e6f1fb43
AndiAJ <andiaj@users.noreply.github.com>
2024-02-13 14:21:00 +0200

    Bug 1880064 - Add logs to PwaRobot

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/robots/PwaRobot.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/robots/PwaRobot.kt
@@ -4,24 +4,32 @@
 
 package org.mozilla.fenix.ui.robots
 
+import android.util.Log
 import androidx.test.uiautomator.UiSelector
 import org.junit.Assert.assertTrue
 import org.mozilla.fenix.helpers.AppAndSystemHelper.isExternalAppBrowserActivityInCurrentTask
+import org.mozilla.fenix.helpers.Constants
+import org.mozilla.fenix.helpers.Constants.TAG
 import org.mozilla.fenix.helpers.MatcherHelper.assertUIObjectExists
+import org.mozilla.fenix.helpers.MatcherHelper.itemWithResId
 import org.mozilla.fenix.helpers.TestHelper.mDevice
 import org.mozilla.fenix.helpers.TestHelper.packageName
 
 class PwaRobot {
-    fun verifyCustomTabToolbarIsNotDisplayed() = assertUIObjectExists(customTabToolbar(), exists = false)
-    fun verifyPwaActivityInCurrentTask() = assertTrue(isExternalAppBrowserActivityInCurrentTask())
+    fun verifyCustomTabToolbarIsNotDisplayed() = assertUIObjectExists(itemWithResId("$packageName:id/toolbar"), exists = false)
+    fun verifyPwaActivityInCurrentTask() {
+        Log.i(TAG, "Trying to verify that the latest activity of the application is used for custom tabs or PWAs")
+        assertTrue(isExternalAppBrowserActivityInCurrentTask())
+        Log.i(TAG, "Verified that the latest activity of the application is used for custom tabs or PWAs")
+    }
 
     class Transition
 }
 
 fun pwaScreen(interact: PwaRobot.() -> Unit): PwaRobot.Transition {
+    Log.i(TAG, "pwaScreen: Trying to find the engine view")
     mDevice.findObject(UiSelector().resourceId("$packageName:id/engineView"))
+    Log.i(Constants.TAG, "pwaScreen: Found the engine view")
     PwaRobot().interact()
     return PwaRobot.Transition()
 }
-
-private fun customTabToolbar() = mDevice.findObject(UiSelector().resourceId("$packageName:id/toolbar"))
