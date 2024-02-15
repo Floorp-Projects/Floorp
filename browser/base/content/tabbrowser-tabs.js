75ef6626fde61f0fcb0836176ccbdc405912b364
Natalia Csoregi <ncsoregi@mozilla.com>
2024-02-15 08:45:45 +0200

    Backed out changeset aa5fc411278d (bug 1876522) for causing failures on browser_tab_tooltips.js. CLOSED TREE

--- browser/base/content/tabbrowser-tabs.js
+++ browser/base/content/tabbrowser-tabs.js
@@ -24,7 +24,6 @@
       this.addEventListener("TabUnpinned", this);
       this.addEventListener("TabHoverStart", this);
       this.addEventListener("TabHoverEnd", this);
-      this.addEventListener("mouseleave", this);
       this.addEventListener("transitionend", this);
       this.addEventListener("dblclick", this);
       this.addEventListener("click", this);
@@ -1833,14 +1832,6 @@
               "tabbrowser-tab-preview"
             );
             previewContainer.tab = null;
-          }
-          break;
-        case "mouseleave":
-          if (this._showCardPreviews) {
-            const previewContainer = document.getElementById(
-              "tabbrowser-tab-preview"
-            );
-            previewContainer.resetDelay();
           }
           break;
         case "mouseout":
