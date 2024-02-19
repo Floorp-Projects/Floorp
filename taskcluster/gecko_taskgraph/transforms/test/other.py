30dd22e0329b62167769bca1be6fd3b8bfa9c6c0
Mike Hommey <mh+mozilla@glandium.org>
2024-02-19 22:19:23 +0000

    Bug 1875239 - Switch most tasks to node 18. r=jmaher,firefox-build-system-reviewers,sergesanspaille,perftest-reviewers,sparky
    Differential Revision: https://phabricator.services.mozilla.com/D201187

--- taskcluster/gecko_taskgraph/transforms/test/other.py
+++ taskcluster/gecko_taskgraph/transforms/test/other.py
@@ -308,25 +308,25 @@ def setup_browsertime(config, tasks):
 
         ts = {
             "by-test-platform": {
-                "android.*": ["browsertime", "linux64-geckodriver", "linux64-node-16"],
-                "linux.*": ["browsertime", "linux64-geckodriver", "linux64-node-16"],
+                "android.*": ["browsertime", "linux64-geckodriver", "linux64-node"],
+                "linux.*": ["browsertime", "linux64-geckodriver", "linux64-node"],
                 "macosx1015.*": [
                     "browsertime",
                     "macosx64-geckodriver",
-                    "macosx64-node-16",
+                    "macosx64-node",
                 ],
                 "macosx1300.*": [
                     "browsertime",
                     "macosx64-aarch64-geckodriver",
-                    "macosx64-aarch64-node-16",
+                    "macosx64-aarch64-node",
                 ],
                 "windows.*aarch64.*": [
                     "browsertime",
                     "win32-geckodriver",
-                    "win32-node-16",
+                    "win32-node",
                 ],
-                "windows.*-32.*": ["browsertime", "win32-geckodriver", "win32-node-16"],
-                "windows.*-64.*": ["browsertime", "win64-geckodriver", "win64-node-16"],
+                "windows.*-32.*": ["browsertime", "win32-geckodriver", "win32-node"],
+                "windows.*-64.*": ["browsertime", "win64-geckodriver", "win64-node"],
             },
         }
 
