01ebe15220126886efcf47017f0c97c41c61a4e8
Sebastian Hengst <archaeopteryx@coole-files.de>
2024-02-19 19:11:05 +0100

    Backed out changeset bef8b1b461b8 (bug 1822171) for causing frequent connection issues to Android in testing. CLOSED TREE

--- python/mozboot/mozboot/android.py
+++ python/mozboot/mozboot/android.py
@@ -21,10 +21,10 @@ from tqdm import tqdm
 from mozboot.bootstrap import MOZCONFIG_SUGGESTION_TEMPLATE
 
 NDK_VERSION = "r26c"
-CMDLINE_TOOLS_VERSION_STRING = "12.0"
-CMDLINE_TOOLS_VERSION = "11076708"
+CMDLINE_TOOLS_VERSION_STRING = "11.0"
+CMDLINE_TOOLS_VERSION = "9644228"
 
-BUNDLETOOL_VERSION = "1.15.6"
+BUNDLETOOL_VERSION = "1.15.2"
 
 # We expect the emulator AVD definitions to be platform agnostic
 LINUX_X86_64_ANDROID_AVD = "linux64-android-avd-x86_64-repack"
