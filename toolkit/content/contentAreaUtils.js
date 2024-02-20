3d2b6440786e7a95aa28cc6622b417572d35e42a
echrs <echrs@outlook.com>
2024-02-20 20:33:24 +0000

    Bug 1878635 - Remove Deprecated.sys.mjs r=Standard8,devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D201737

--- toolkit/content/contentAreaUtils.js
+++ toolkit/content/contentAreaUtils.js
@@ -11,7 +11,6 @@ var { XPCOMUtils } = ChromeUtils.importESModule(
 
 ChromeUtils.defineESModuleGetters(this, {
   BrowserUtils: "resource://gre/modules/BrowserUtils.sys.mjs",
-  Deprecated: "resource://gre/modules/Deprecated.sys.mjs",
   DownloadLastDir: "resource://gre/modules/DownloadLastDir.sys.mjs",
   DownloadPaths: "resource://gre/modules/DownloadPaths.sys.mjs",
   Downloads: "resource://gre/modules/Downloads.sys.mjs",
