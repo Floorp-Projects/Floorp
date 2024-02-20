c926a6a10660d21c57b832a7df21bd4276536640
Byron Campen <docfaraday@gmail.com>
2024-02-20 15:40:48 +0000

    Bug 1874471: Disable payload-type matching for filters that know an ssrc. r=mjf
    Depends on D201766
    
    Differential Revision: https://phabricator.services.mozilla.com/D201767

--- media/webrtc/signaling/gtest/mediapipeline_unittest.cpp
+++ media/webrtc/signaling/gtest/mediapipeline_unittest.cpp
@@ -598,6 +598,15 @@ TEST_F(MediaPipelineFilterTest, TestSSRCFilter) {
   EXPECT_FALSE(Filter(filter, 556, 110));
 }
 
+TEST_F(MediaPipelineFilterTest, TestSSRCFilterOverridesPayloadTypeFilter) {
+  MediaPipelineFilter filter;
+  filter.AddRemoteSSRC(555);
+  filter.AddUniqueReceivePT(110);
+  // We have a configured ssrc; do not allow payload type matching.
+  EXPECT_FALSE(Filter(filter, 556, 110));
+  EXPECT_TRUE(Filter(filter, 555, 110));
+}
+
 #define SSRC(ssrc)                                                    \
   ((ssrc >> 24) & 0xFF), ((ssrc >> 16) & 0xFF), ((ssrc >> 8) & 0xFF), \
       (ssrc & 0xFF)
@@ -630,6 +639,11 @@ TEST_F(MediaPipelineFilterTest, TestPayloadTypeFilter) {
   filter.AddUniqueReceivePT(110);
   EXPECT_TRUE(Filter(filter, 555, 110));
   EXPECT_FALSE(Filter(filter, 556, 111));
+  // Matching based on unique payload type causes us to learn the ssrc.
+  EXPECT_TRUE(Filter(filter, 555, 98));
+  // Once we have learned an SSRC, do _not_ learn new ones based on payload
+  // type.
+  EXPECT_FALSE(Filter(filter, 557, 110));
 }
 
 TEST_F(MediaPipelineFilterTest, TestSSRCMovedWithMid) {
