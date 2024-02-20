f64137abdbf18002e8ee3d43870b3553c5e8670e
Natalia Csoregi <ncsoregi@mozilla.com>
2024-02-20 19:08:00 +0200

    Backed out changeset 623e19026939 (bug 1855264) for causing crashes on test_sync_auth_manager.js. CLOSED TREE

--- services/sync/tests/unit/test_sync_auth_manager.js
+++ services/sync/tests/unit/test_sync_auth_manager.js
@@ -37,8 +37,9 @@ const { TokenServerClient, TokenServerClientServerError } =
   ChromeUtils.importESModule(
     "resource://services-common/tokenserverclient.sys.mjs"
   );
-const { AccountState, ERROR_INVALID_ACCOUNT_STATE } =
-  ChromeUtils.importESModule("resource://gre/modules/FxAccounts.sys.mjs");
+const { AccountState } = ChromeUtils.importESModule(
+  "resource://gre/modules/FxAccounts.sys.mjs"
+);
 
 const SECOND_MS = 1000;
 const MINUTE_MS = SECOND_MS * 60;
@@ -191,11 +192,8 @@ add_task(async function test_initialializeWithAuthErrorAndDeletedAccount() {
 
   await Assert.rejects(
     syncAuthManager._ensureValidToken(),
-    err => {
-      Assert.equal(err.message, ERROR_INVALID_ACCOUNT_STATE);
-      return true; // expected error
-    },
-    "should reject because the account was deleted"
+    AuthenticationError,
+    "should reject due to an auth error"
   );
 
   Assert.ok(accessTokenWithSessionTokenCalled);
