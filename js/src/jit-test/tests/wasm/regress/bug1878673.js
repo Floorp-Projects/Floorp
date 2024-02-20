4728061096275ae19727995627297a76d3703448
Yury Delendik <ydelendik@mozilla.com>
2024-02-20 04:17:27 +0000

    Bug 1878673 - Properly handle OOM for SIMD loads. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D201973

---
+++ js/src/jit-test/tests/wasm/regress/bug1878673.js
@@ -0,0 +1,15 @@
+// |jit-test| skip-if: !('oomTest' in this)
+
+// Check proper handling of OOM in SIMD loads.
+
+oomTest(function () {
+    let x = wasmTextToBinary(`(module
+        (memory 1 1)
+        (func
+          i32.const 16
+          v128.load8x8_s
+          i16x8.abs
+          drop)
+        )`);
+    new WebAssembly.Module(x);
+});
