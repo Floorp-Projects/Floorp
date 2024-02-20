fb918be171fe7781657c9d86ff8b27109c1b71d6
Greg Tatum <tatum.creative@gmail.com>
2024-02-20 18:20:06 +0000

    Bug 1873743 - Stub out the toolkit/components/ml files; r=translations-reviewers,nordzilla
    Differential Revision: https://phabricator.services.mozilla.com/D198071

---
+++ toolkit/components/ml/actors/MLEngineParent.sys.mjs
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * The ML engine is in its own content process. This actor handles the
+ * marshalling of the data such as the engine payload.
+ */
+export class MLEngineParent extends JSWindowActorParent {
+  // TODO
+}
