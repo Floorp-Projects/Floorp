b34967992a44c6cf198f5dfd03ab50d666bb5144
Ryan VanderMeulen <ryanvm@gmail.com>
2024-02-20 14:10:06 +0000

    Bug 1880925 - Pin platform-tools to r34.0.5. r=releng-reviewers,jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D202185

--- taskcluster/scripts/misc/repack-android-sdk-linux.sh
+++ taskcluster/scripts/misc/repack-android-sdk-linux.sh
@@ -11,10 +11,17 @@ cd $GECKO_PATH
 ./mach python python/mozboot/mozboot/android.py --artifact-mode --no-interactive --list-packages
 
 # Bug 1869407: override emulator to a known working version
-curl -L http://dl.google.com/android/repository/emulator-linux_x64-10696886.zip > /tmp/emulator.zip
+curl --fail -L http://dl.google.com/android/repository/emulator-linux_x64-10696886.zip > /tmp/emulator.zip
 cd /builds/worker/.mozbuild/android-sdk-linux
 rm -rf emulator
 unzip /tmp/emulator.zip
+cd $GECKO_PATH
+
+# Bug 1880925: override platform-tools to a known working version
+curl --fail -L https://dl.google.com/android/repository/platform-tools_r34.0.5-linux.zip > /tmp/platform-tools.zip
+cd /builds/worker/.mozbuild/android-sdk-linux
+rm -rf platform-tools
+unzip /tmp/platform-tools.zip
 cd $GECKO_PATH
 
 tar cavf $UPLOAD_DIR/android-sdk-linux.tar.zst -C /builds/worker/.mozbuild android-sdk-linux bundletool.jar
