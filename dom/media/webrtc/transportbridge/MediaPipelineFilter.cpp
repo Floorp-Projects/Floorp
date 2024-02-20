a39d1f550ed0ecdc2e81ce98b792372776923b21
Byron Campen <docfaraday@gmail.com>
2024-02-20 15:40:48 +0000

    Bug 1874471: Disable payload-type matching for filters that know an ssrc. r=mjf
    Depends on D201766
    
    Differential Revision: https://phabricator.services.mozilla.com/D201767

--- dom/media/webrtc/transportbridge/MediaPipelineFilter.cpp
+++ dom/media/webrtc/transportbridge/MediaPipelineFilter.cpp
@@ -85,20 +85,24 @@ bool MediaPipelineFilter::Filter(const webrtc::RTPHeader& header) {
   // Remote SSRC based filtering
   //
 
-  if (remote_ssrc_set_.count(header.ssrc)) {
+  if (!remote_ssrc_set_.empty()) {
+    if (remote_ssrc_set_.count(header.ssrc)) {
+      DEBUG_LOG(
+          ("MediaPipelineFilter SSRC: %u matched remote SSRC set."
+           " passing packet",
+           header.ssrc));
+      return true;
+    }
     DEBUG_LOG(
-        ("MediaPipelineFilter SSRC: %u matched remote SSRC set."
-         " passing packet",
-         header.ssrc));
-    return true;
+        ("MediaPipelineFilter SSRC: %u did not match any of %zu"
+         " remote SSRCS.",
+         header.ssrc, remote_ssrc_set_.size()));
+    return false;
   }
-  DEBUG_LOG(
-      ("MediaPipelineFilter SSRC: %u did not match any of %zu"
-       " remote SSRCS.",
-       header.ssrc, remote_ssrc_set_.size()));
 
   //
-  // PT, payload type, last ditch effort filtering
+  // PT, payload type, last ditch effort filtering. We only try this if we do
+  // not have any ssrcs configured (either by learning them, or negotiation).
   //
 
   if (receive_payload_type_set_.count(header.payloadType)) {
