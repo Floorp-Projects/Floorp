eaf1fb93da3f48e2db511eea11e220e78fc7adf1
Greg Tatum <tatum.creative@gmail.com>
2024-02-21 02:58:51 +0000

    Bug 1873743 - Stub out enough of remote settings to download; r=translations-reviewers,nordzilla
    Differential Revision: https://phabricator.services.mozilla.com/D198072

--- toolkit/components/ml/tests/browser/browser_ml_engine.js
+++ toolkit/components/ml/tests/browser/browser_ml_engine.js
@@ -7,3 +7,22 @@ add_task(async function test_ml_engine_exists() {
   const response = await fetch("chrome://global/content/ml/MLEngine.html");
   ok(response.ok, "The ml engine can be fetched.");
 });
+
+add_task(async function test_summarization() {
+  const { removeMocks, remoteClients } = await createAndMockMLRemoteSettings({
+    autoDownloadFromRemoteSettings: true,
+  });
+
+  const summarizePromise = SummarizerModel.summarize("This gets cut in half.");
+
+  await remoteClients.models.resolvePendingDownloads(1);
+  await remoteClients.wasm.resolvePendingDownloads(1);
+
+  is(
+    await summarizePromise,
+    "This gets c",
+    "The text gets cut in half simulating summarizing"
+  );
+
+  removeMocks();
+});
