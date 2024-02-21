5b573b2fd2fb36e583b203a4956f5a0a890ecccd
Nerixyz <nerixdev@outlook.de>
2024-02-21 21:11:12 +0000

    Bug 1689538 - Implement current playback position algorithm for PositionState. r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D198849

--- dom/media/mediasession/MediaSession.cpp
+++ dom/media/mediasession/MediaSession.cpp
@@ -20,6 +20,29 @@
 
 namespace mozilla::dom {
 
+double PositionState::CurrentPlaybackPosition(TimeStamp aNow) const {
+  // https://w3c.github.io/mediasession/#current-playback-position
+
+  // Set time elapsed to the system time in seconds minus the last position
+  // updated time.
+  auto timeElapsed = aNow - mPositionUpdatedTime;
+  // Mutliply time elapsed with actual playback rate.
+  timeElapsed = timeElapsed.MultDouble(mPlaybackRate);
+  // Set position to time elapsed added to last reported playback position.
+  auto position = timeElapsed.ToSeconds() + mLastReportedPlaybackPosition;
+
+  // If position is less than zero, return zero.
+  if (position < 0.0) {
+    return 0.0;
+  }
+  // If position is greater than duration, return duration.
+  if (position > mDuration) {
+    return mDuration;
+  }
+  // Return position.
+  return position;
+}
+
 // We don't use NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE because we need to
 // unregister MediaSession from document's activity listeners.
 NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_CLASS(MediaSession)
@@ -176,8 +199,8 @@ void MediaSession::SetPositionState(const MediaPositionState& aState,
 
   // Update the position state and last position updated time.
   MOZ_ASSERT(aState.mDuration.WasPassed());
-  mPositionState =
-      Some(PositionState(aState.mDuration.Value(), playbackRate, position));
+  mPositionState = Some(PositionState(aState.mDuration.Value(), playbackRate,
+                                      position, TimeStamp::Now()));
   NotifyPositionStateChanged();
 }
 
--- dom/media/mediasession/MediaSession.h
+++ dom/media/mediasession/MediaSession.h
@@ -11,6 +11,7 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/dom/MediaSessionBinding.h"
 #include "mozilla/EnumeratedArray.h"
+#include "mozilla/TimeStamp.h"
 #include "nsCycleCollectionParticipant.h"
 #include "nsIDocumentActivity.h"
 #include "nsWrapperCache.h"
@@ -29,13 +30,21 @@ class MediaMetadata;
 struct PositionState {
   PositionState() = default;
   PositionState(double aDuration, double aPlaybackRate,
-                double aLastReportedTime)
+                double aLastReportedTime, TimeStamp aPositionUpdatedTime)
       : mDuration(aDuration),
         mPlaybackRate(aPlaybackRate),
-        mLastReportedPlaybackPosition(aLastReportedTime) {}
-  double mDuration;
-  double mPlaybackRate;
-  double mLastReportedPlaybackPosition;
+        mLastReportedPlaybackPosition(aLastReportedTime),
+        mPositionUpdatedTime(aPositionUpdatedTime) {}
+
+  double mDuration = 0.0;
+  double mPlaybackRate = 0.0;
+  double mLastReportedPlaybackPosition = 0.0;
+  TimeStamp mPositionUpdatedTime;
+
+  // Returns the playback position in seconds (from 0 to mDuration)
+  // at the current time (aNow).
+  // https://w3c.github.io/mediasession/#current-playback-position
+  double CurrentPlaybackPosition(TimeStamp aNow = TimeStamp::Now()) const;
 };
 
 class MediaSession final : public nsIDocumentActivity, public nsWrapperCache {
