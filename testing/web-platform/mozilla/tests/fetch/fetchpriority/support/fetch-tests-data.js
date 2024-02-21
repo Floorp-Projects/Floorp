9c52a91fd95df69a8db0c086a635aa49f29184f6
Ziran Sun <zsun@igalia.com>
2024-02-21 12:03:28 +0000

    Bug 1839319 - Refine the tests for Fetch() API with fetchpriority. r=fredw
    This is to be in line with the comment at D200920. This change is also based on D200921.
    
    Differential Revision: https://phabricator.services.mozilla.com/D202223

--- testing/web-platform/mozilla/tests/fetch/fetchpriority/support/fetch-tests-data.js
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/fetch-tests-data.js
@@ -1,11 +1,12 @@
 export const kTestFolderName = "fetch-tests";
 
+// Use RequestInit's priority if specified, or RequestionInfo's priority otherwise.
 const kExpectedRequestsOfFetchAPI = [
     {   fileNameAndSuffix: "dummy.css?1",
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
     },
     {   fileNameAndSuffix: "dummy.css?2",
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
     },
     {   fileNameAndSuffix: "dummy.css?3",
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
@@ -14,23 +15,41 @@ const kExpectedRequestsOfFetchAPI = [
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
     },
     {   fileNameAndSuffix: "dummy.css?5",
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
     },
     {   fileNameAndSuffix: "dummy.css?6",
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
-    },
-    {   fileNameAndSuffix: "dummy.css?7",
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
     },
-    {   fileNameAndSuffix: "dummy.css?8",
+    {   fileNameAndSuffix: "dummy.css?7",
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?8",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
     },
     {   fileNameAndSuffix: "dummy.css?9",
-        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
     },
     {   fileNameAndSuffix: "dummy.css?10",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.css?11",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?12",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?13",
         internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_LOW
-    }
+    },
+    {   fileNameAndSuffix: "dummy.css?14",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_HIGH
+    },
+    {   fileNameAndSuffix: "dummy.css?15",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?16",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
 ];
 
 export const kTestData = [
