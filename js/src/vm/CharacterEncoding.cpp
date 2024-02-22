4e38da7730eb94d5e286540835be14190bcb8cbb
Steve Fink <sfink@mozilla.com>
2024-02-22 19:36:48 +0000

    Bug 1879997 - use CountLeadingZeroes32 r=spidermonkey-reviewers,arai
    Differential Revision: https://phabricator.services.mozilla.com/D201887

--- js/src/vm/CharacterEncoding.cpp
+++ js/src/vm/CharacterEncoding.cpp
@@ -286,11 +286,6 @@ static bool InflateUTF8ToUTF16(JSContext* cx, const UTF8Chars& src,
         break;
       }
     } else {
-      // Non-ASCII code unit.  Determine its length in bytes (n).
-      uint32_t n = 1;
-      while (v & (0x80 >> n)) {
-        n++;
-      }
 
 #define INVALID(report, arg, n2)                                    \
   do {                                                              \
@@ -314,6 +309,14 @@ static bool InflateUTF8ToUTF16(JSContext* cx, const UTF8Chars& src,
       goto invalidMultiByteCodeUnit;                                \
     }                                                               \
   } while (0)
+
+      // Non-ASCII code unit. Determine its length in bytes (n).
+      //
+      // Avoid undefined behavior from passing in 0
+      // (https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html#index-_005f_005fbuiltin_005fclz)
+      // by turning on the low bit so that 0xff will set n=31-24=7, which will
+      // be detected as an invalid character.
+      uint32_t n = mozilla::CountLeadingZeroes32(~int8_t(src[i]) | 0x1) - 24;
 
       // Check the leading byte.
       if (n < 2 || n > 4) {
