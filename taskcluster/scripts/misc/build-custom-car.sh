b79a40f59ec193fc86b3c528633e79eebeaf4a42
Mike Hommey <mh+mozilla@glandium.org>
2024-02-22 00:53:26 +0000

    Bug 1880919 - Refresh Visual Studio artifacts. r=firefox-build-system-reviewers,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D202182

--- taskcluster/scripts/misc/build-custom-car.sh
+++ taskcluster/scripts/misc/build-custom-car.sh
@@ -106,7 +106,7 @@ if [[ $(uname -o) == "Msys" ]]; then
   pushd "$WINDOWSSDKDIR"
   mkdir -p Debuggers/x64/
   popd
-  mv $MOZ_FETCHES_DIR/VS/VC/Redist/MSVC/14.38.33130/x64/Microsoft.VC143.CRT/* chrome_dll/system32/
+  mv $MOZ_FETCHES_DIR/VS/VC/Redist/MSVC/14.38.33135/x64/Microsoft.VC143.CRT/* chrome_dll/system32/
   mv "$WINDOWSSDKDIR/App Certification Kit/"* "$WINDOWSSDKDIR"/Debuggers/x64/
   export WINDIR="$PWD/chrome_dll"
 
