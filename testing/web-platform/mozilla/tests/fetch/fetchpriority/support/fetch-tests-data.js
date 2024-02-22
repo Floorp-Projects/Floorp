6a59fa81bb2babad5e044f698eb0174a723059ca
Ziran Sun <zsun@igalia.com>
2024-02-22 09:37:19 +0000

    Bug 1839319 - Add tests for Fetch() API with fetchpriority disabled. r=valentin,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D200920

--- testing/web-platform/mozilla/tests/fetch/fetchpriority/support/fetch-tests-data.js
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/fetch-tests-data.js
@@ -52,8 +52,65 @@ const kExpectedRequestsOfFetchAPI = [
     },
 ];
 
+const kExpectedRequestsOfFetchAPIDisabled = [
+    {   fileNameAndSuffix: "dummy.css?1",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?2",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?3",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?4",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?5",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?6",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?7",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?8",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?9",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?10",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?11",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?12",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?13",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?14",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?15",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+    {   fileNameAndSuffix: "dummy.css?16",
+        internalPriority: SpecialPowers.Ci.nsISupportsPriority.PRIORITY_NORMAL
+    },
+];
+
 export const kTestData = [
     {   testFileName: "fetch-init.h2.html",
         expectedRequests: kExpectedRequestsOfFetchAPI
+    }
+];
+
+export const kTestDataDisabled = [
+    {   testFileName: "fetch-init.h2.html",
+        expectedRequests: kExpectedRequestsOfFetchAPIDisabled
     }
 ];
