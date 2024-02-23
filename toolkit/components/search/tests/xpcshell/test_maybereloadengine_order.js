3de3ce83fed6b9a49f39971095df1312857464e5
mcheang <mcheang@mozilla.com>
2024-02-23 01:38:27 +0000

    Bug 1870684 - Ensure tests using test-extensions are passing when newSearchConfig.enabled PREF is ON. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D200395

--- toolkit/components/search/tests/xpcshell/test_maybereloadengine_order.js
+++ toolkit/components/search/tests/xpcshell/test_maybereloadengine_order.js
@@ -1,56 +1,14 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+/*
+ * Test engine order is not set after engine reload.
+ */
+
 "use strict";
 
-const TEST_CONFIG = [
-  {
-    webExtension: {
-      id: "plainengine@search.mozilla.org",
-      name: "Plain",
-      search_url: "https://duckduckgo.com/",
-      params: [
-        {
-          name: "q",
-          value: "{searchTerms}",
-        },
-      ],
-      suggest_url: "https://ac.duckduckgo.com/ac/q={searchTerms}&type=list",
-    },
-    appliesTo: [{ included: { everywhere: true } }],
-  },
-  {
-    webExtension: {
-      id: "special-engine@search.mozilla.org",
-      name: "Special",
-      search_url: "https://www.google.com/search",
-      params: [
-        {
-          name: "q",
-          value: "{searchTerms}",
-        },
-        {
-          name: "client",
-          condition: "purpose",
-          purpose: "keyword",
-          value: "firefox-b-1-ab",
-        },
-        {
-          name: "client",
-          condition: "purpose",
-          purpose: "searchbar",
-          value: "firefox-b-1",
-        },
-      ],
-      suggest_url:
-        "https://www.google.com/complete/search?client=firefox&q={searchTerms}",
-    },
-    appliesTo: [{ default: "yes", included: { regions: ["FR"] } }],
-  },
-];
-
 add_setup(async function () {
-  await SearchTestUtils.useTestEngines("test-extensions", null, TEST_CONFIG);
+  await SearchTestUtils.useTestEngines("test-extensions");
   await AddonTestUtils.promiseStartupManager();
 
   registerCleanupFunction(AddonTestUtils.promiseShutdownManager);
@@ -59,7 +17,7 @@ add_setup(async function () {
 add_task(async function basic_multilocale_test() {
   let resolver;
   let initPromise = new Promise(resolve => (resolver = resolve));
-  useCustomGeoServer("FR", initPromise);
+  useCustomGeoServer("TR", initPromise);
 
   await Services.search.init();
   await Services.search.getAppProvidedEngines();
