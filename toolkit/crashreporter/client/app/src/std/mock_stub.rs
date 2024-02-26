ad78fa1ab52e7eaa417d410d41e523975fccfb18
Alex Franchuk <afranchuk@mozilla.com>
2024-02-26 19:12:37 +0000

    Bug 1759175 pt10 - Add additional documentation for the crashreporter client r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D201993

--- toolkit/crashreporter/client/app/src/std/mock_stub.rs
+++ toolkit/crashreporter/client/app/src/std/mock_stub.rs
@@ -3,11 +3,17 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #![allow(dead_code)]
 
+//! Stubs used when mocking isn't enabled.
+
+/// Create a mock hook with the given name. When mocking isn't enabled, the given value will be
+/// used instead. Panics if the hook isn't set.
 #[inline(always)]
 pub fn hook<T: std::any::Any + Send + Sync + Clone>(normally: T, _name: &'static str) -> T {
     normally
 }
 
+/// Create a mock hook with the given name. When mocking isn't enabled or the hook hasn't been set,
+/// the given value will be used instead.
 #[inline(always)]
 pub fn try_hook<T: std::any::Any + Send + Sync + Clone>(fallback: T, _name: &'static str) -> T {
     fallback
