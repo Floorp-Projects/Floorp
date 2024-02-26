0f95749c770109f35231b834a1715a9d1cc71289
Tiaan Louw <tlouw@mozilla.com>
2024-02-26 14:55:00 +0000

    Bug 1862928 - Part 1 - Move color serialization code to separate file. r=layout-reviewers,emilio
    Moving code to a separate file to allow adding more logic to it in the
    next commit.
    
    Differential Revision: https://phabricator.services.mozilla.com/D192672

--- servo/components/style/color/mod.rs
+++ servo/components/style/color/mod.rs
@@ -8,10 +8,9 @@
 pub mod convert;
 pub mod mix;
 pub mod parsing;
+mod to_css;
 
 use cssparser::color::PredefinedColorSpace;
-use std::fmt::{self, Write};
-use style_traits::{CssWriter, ToCss};
 
 /// The 3 components that make up a color.  (Does not include the alpha component)
 #[derive(Copy, Clone, Debug, MallocSizeOf, PartialEq, ToShmem)]
@@ -537,77 +536,6 @@ impl From<PredefinedColorSpace> for ColorSpace {
             PredefinedColorSpace::Rec2020 => ColorSpace::Rec2020,
             PredefinedColorSpace::XyzD50 => ColorSpace::XyzD50,
             PredefinedColorSpace::XyzD65 => ColorSpace::XyzD65,
-        }
-    }
-}
-
-impl ToCss for AbsoluteColor {
-    fn to_css<W>(&self, dest: &mut CssWriter<W>) -> fmt::Result
-    where
-        W: Write,
-    {
-        match self.color_space {
-            ColorSpace::Srgb if self.flags.contains(ColorFlags::IS_LEGACY_SRGB) => {
-                // The "none" keyword is not supported in the rgb/rgba legacy syntax.
-                cssparser::ToCss::to_css(
-                    &parsing::RgbaLegacy::from_floats(
-                        self.components.0,
-                        self.components.1,
-                        self.components.2,
-                        self.alpha,
-                    ),
-                    dest,
-                )
-            },
-            ColorSpace::Hsl | ColorSpace::Hwb => self.into_srgb_legacy().to_css(dest),
-            ColorSpace::Lab => cssparser::ToCss::to_css(
-                &parsing::Lab::new(self.c0(), self.c1(), self.c2(), self.alpha()),
-                dest,
-            ),
-            ColorSpace::Lch => cssparser::ToCss::to_css(
-                &parsing::Lch::new(self.c0(), self.c1(), self.c2(), self.alpha()),
-                dest,
-            ),
-            ColorSpace::Oklab => cssparser::ToCss::to_css(
-                &parsing::Oklab::new(self.c0(), self.c1(), self.c2(), self.alpha()),
-                dest,
-            ),
-            ColorSpace::Oklch => cssparser::ToCss::to_css(
-                &parsing::Oklch::new(self.c0(), self.c1(), self.c2(), self.alpha()),
-                dest,
-            ),
-            _ => {
-                let color_space = match self.color_space {
-                    ColorSpace::Srgb => {
-                        debug_assert!(
-                            !self.flags.contains(ColorFlags::IS_LEGACY_SRGB),
-                            "legacy srgb is not a color function"
-                        );
-                        PredefinedColorSpace::Srgb
-                    },
-                    ColorSpace::SrgbLinear => PredefinedColorSpace::SrgbLinear,
-                    ColorSpace::DisplayP3 => PredefinedColorSpace::DisplayP3,
-                    ColorSpace::A98Rgb => PredefinedColorSpace::A98Rgb,
-                    ColorSpace::ProphotoRgb => PredefinedColorSpace::ProphotoRgb,
-                    ColorSpace::Rec2020 => PredefinedColorSpace::Rec2020,
-                    ColorSpace::XyzD50 => PredefinedColorSpace::XyzD50,
-                    ColorSpace::XyzD65 => PredefinedColorSpace::XyzD65,
-
-                    _ => {
-                        unreachable!("other color spaces do not support color() syntax")
-                    },
-                };
-
-                let color_function = parsing::ColorFunction {
-                    color_space,
-                    c1: self.c0(),
-                    c2: self.c1(),
-                    c3: self.c2(),
-                    alpha: self.alpha(),
-                };
-                let color = parsing::Color::ColorFunction(color_function);
-                cssparser::ToCss::to_css(&color, dest)
-            },
         }
     }
 }
