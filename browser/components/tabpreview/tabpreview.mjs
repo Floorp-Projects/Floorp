bc30c1592e1968bcf3f3ad88c3622f7cd649605e
Dave Townsend <dtownsend@oxymoronical.com>
2024-02-26 12:16:45 +0000

    Bug 1875831: Don't use arrow positioning for the tab preview panel. r=tabbrowser-reviewers,dao
    The panel isn't an arrow panel and the standard popup positioning appears to
    work fine. It also seems that something about the arrow positioning logic
    causes background windows to raise.
    
    Differential Revision: https://phabricator.services.mozilla.com/D202709

--- browser/components/tabpreview/tabpreview.mjs
+++ browser/components/tabpreview/tabpreview.mjs
@@ -63,7 +63,6 @@ export default class TabPreview extends MozLitElement {
     this.panel.setAttribute("norolluponanchor", true);
     this.panel.setAttribute("consumeoutsideclicks", "never");
     this.panel.setAttribute("level", "parent");
-    this.panel.setAttribute("type", "arrow");
     this.shadowRoot.append(this.panel);
     return this.panel;
   }
