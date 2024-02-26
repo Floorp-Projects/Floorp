56a817b92e90178be37c25a29c4b818e4d754f84
Valentin Gosu <valentin.gosu@gmail.com>
2024-02-26 10:34:20 +0000

    Bug 1760077 - Fix racy test_channel_close.js r=necko-reviewers,kershaw
    ChannelListener throws an exception if the channel gets closed unexpectedly.
    Since this test was racing live_channels[1] vs local_channel, sometimes
    the server would get closed before local_channel completed, causing an
    exception.
    
    This patch introduces SimpleChannelListener which doesn't include so much
    logic and may be a better fit when a channel might succeed or fail depending
    on certain circumstances.
    
    Differential Revision: https://phabricator.services.mozilla.com/D202413

--- netwerk/test/unit/head_channels.js
+++ netwerk/test/unit/head_channels.js
@@ -525,3 +525,27 @@ function makeHTTPChannel(url, with_proxy) {
     loadUsingSystemPrincipal: true,
   }).QueryInterface(Ci.nsIHttpChannel);
 }
+
+// Like ChannelListener but does not throw an exception if something
+// goes wrong. Callback is supposed to do all the work.
+class SimpleChannelListener {
+  constructor(callback) {
+    this._onStopCallback = callback;
+    this._buffer = "";
+  }
+  get QueryInterface() {
+    return ChromeUtils.generateQI(["nsIStreamListener", "nsIRequestObserver"]);
+  }
+
+  onStartRequest(request) {}
+
+  onDataAvailable(request, stream, offset, count) {
+    this._buffer = this._buffer.concat(read_stream(stream, count));
+  }
+
+  onStopRequest(request, status) {
+    if (this._onStopCallback) {
+      this._onStopCallback(request, this._buffer);
+    }
+  }
+}
