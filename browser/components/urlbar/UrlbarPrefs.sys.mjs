a66db50e7d0281945ee700d335b0c34e26a22002
Daisuke Akatsuka <daisuke@birchill.co.jp>
2024-02-28 22:36:29 +0000

    Bug 1882220: Support simple UI weather suggestion r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D202786

--- browser/components/urlbar/UrlbarPrefs.sys.mjs
+++ browser/components/urlbar/UrlbarPrefs.sys.mjs
@@ -525,6 +525,7 @@ const NIMBUS_DEFAULTS = {
   weatherKeywords: null,
   weatherKeywordsMinimumLength: 0,
   weatherKeywordsMinimumLengthCap: 0,
+  weatherSimpleUI: false,
 };
 
 // Maps preferences under browser.urlbar.suggest to behavior names, as defined
