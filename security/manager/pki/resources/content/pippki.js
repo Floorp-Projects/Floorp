560107d6a743b1e6f80a75efd41283e2a72551be
Gregory Pappas <gp3033@protonmail.com>
2024-02-28 15:07:46 +0000

    Bug 1878401 - part 5 - Update nsIFilePicker users in security/ to pass BrowsingContext to init r=keeler
    Depends on D200549
    
    Differential Revision: https://phabricator.services.mozilla.com/D200550

--- security/manager/pki/resources/content/pippki.js
+++ security/manager/pki/resources/content/pippki.js
@@ -127,7 +127,7 @@ async function exportToFile(parent, cert) {
   );
 
   var fp = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
-  fp.init(parent, saveCertAs, Ci.nsIFilePicker.modeSave);
+  fp.init(parent.browsingContext, saveCertAs, Ci.nsIFilePicker.modeSave);
   fp.defaultString = certToFilename(cert);
   fp.defaultExtension = DEFAULT_CERT_EXTENSION;
   for (let format of Object.values(formats)) {
