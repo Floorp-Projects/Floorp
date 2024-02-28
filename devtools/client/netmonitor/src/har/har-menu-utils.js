67bf0fdf61046db8ce34bfd972b99b154dd7f96a
Gregory Pappas <gp3033@protonmail.com>
2024-02-28 21:29:38 +0000

    Bug 1878401 - part 4 - Update nsIFilePicker users in devtools/ to pass BrowsingContext to init r=profiler-reviewers,devtools-reviewers,nchevobbe,julienw
    Depends on D200548
    
    Differential Revision: https://phabricator.services.mozilla.com/D200549

--- devtools/client/netmonitor/src/har/har-menu-utils.js
+++ devtools/client/netmonitor/src/har/har-menu-utils.js
@@ -57,7 +57,7 @@ var HarMenuUtils = {
   openHarFile(actions, openSplitConsole) {
     const fp = Cc["@mozilla.org/filepicker;1"].createInstance(Ci.nsIFilePicker);
     fp.init(
-      window,
+      window.browsingContext,
       L10N.getStr("netmonitor.har.importHarDialogTitle"),
       Ci.nsIFilePicker.modeOpen
     );
