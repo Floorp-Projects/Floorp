26f86dc1887c36ba360a879d6b113c7da0c795ea
Nicolas Silva <nsilva@mozilla.com>
2024-02-28 08:15:40 +0000

    Bug 1882318 - Add some documentation to quad shaders. r=gfx-reviewers,gw
    Differential Revision: https://phabricator.services.mozilla.com/D202831

--- gfx/wr/webrender/res/ps_quad_textured.glsl
+++ gfx/wr/webrender/res/ps_quad_textured.glsl
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/// This shader renders solid colors or simple images in a color or alpha target.
+
 #include ps_quad
 
 #ifndef SWGL_ANTIALIAS
