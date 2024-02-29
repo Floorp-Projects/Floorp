bb9dadf3eac0eeb3881841ce4cddb4b2fe6c700b
Issam Mani <imani@mozilla.com>
2024-02-29 08:00:01 +0000

    Bug 1882587 - Check supportedRegions as well as experiment flags for address autofill. r=dimi
    Differential Revision: https://phabricator.services.mozilla.com/D203034

--- browser/extensions/formautofill/test/unit/test_isAddressAutofillAvailable.js
+++ browser/extensions/formautofill/test/unit/test_isAddressAutofillAvailable.js
@@ -11,21 +11,31 @@ const { FormAutofill } = ChromeUtils.importESModule(
   "resource://autofill/FormAutofill.sys.mjs"
 );
 
+add_setup(async () => {
+  Services.prefs.setBoolPref(
+    "extensions.formautofill.addresses.experiments.enabled",
+    false
+  );
+  registerCleanupFunction(function cleanupRegion() {
+    Services.prefs.clearUserPref(
+      "extensions.formautofill.addresses.experiments.enabled"
+    );
+  });
+});
+
 add_task(async function test_defaultTestEnvironment() {
   Assert.equal(
     Services.prefs.getCharPref("extensions.formautofill.addresses.supported"),
     "on"
   );
-
-  Assert.equal(
-    Services.prefs.getBoolPref(
-      "extensions.formautofill.addresses.experiments.enabled"
-    ),
-    true
-  );
 });
 
-add_task(async function test_default_supported_address_region() {
+add_task(async function test_default_supported_module_and_autofill_region() {
+  Services.prefs.setCharPref("browser.search.region", "US");
+  registerCleanupFunction(function cleanupRegion() {
+    Services.prefs.clearUserPref("browser.search.region");
+  });
+
   let addon = await AddonManager.getAddonByID(EXTENSION_ID);
   await addon.reload();
 
@@ -33,15 +43,44 @@ add_task(async function test_default_supported_address_region() {
   Assert.equal(FormAutofill.isAutofillAddressesEnabled, true);
 });
 
-add_task(async function test_unsupported_address_region() {
-  const addon = await AddonManager.getAddonByID(EXTENSION_ID);
+add_task(
+  async function test_supported_creditCard_region_unsupported_address_region() {
+    Services.prefs.setCharPref(
+      "extensions.formautofill.addresses.supported",
+      "detect"
+    );
+    Services.prefs.setCharPref(
+      "extensions.formautofill.creditCards.supported",
+      "detect"
+    );
+    Services.prefs.setCharPref("browser.search.region", "FR");
+    Services.prefs.setCharPref(
+      "extensions.formautofill.addresses.supportedCountries",
+      "US,CA"
+    );
+    Services.prefs.setCharPref(
+      "extensions.formautofill.creditCards.supportedCountries",
+      "US,CA,FR"
+    );
+    registerCleanupFunction(function cleanupPrefs() {
+      Services.prefs.clearUserPref("browser.search.region");
+      Services.prefs.clearUserPref(
+        "extensions.formautofill.addresses.supportedCountries"
+      );
+      Services.prefs.clearUserPref(
+        "extensions.formautofill.addresses.supported"
+      );
+      Services.prefs.clearUserPref(
+        "extensions.formautofill.creditCards.supported"
+      );
+    });
 
-  Services.prefs.setBoolPref(
-    "extensions.formautofill.addresses.experiments.enabled",
-    false
-  );
-
-  await addon.reload();
-  Assert.equal(FormAutofill.isAutofillAddressesAvailable, false);
-  Assert.equal(FormAutofill.isAutofillAddressesEnabled, false);
-});
+    let addon = await AddonManager.getAddonByID(EXTENSION_ID);
+    await addon.reload();
+    Assert.ok(
+      Services.prefs.getBoolPref("extensions.formautofill.creditCards.enabled")
+    );
+    Assert.equal(FormAutofill.isAutofillAddressesAvailable, false);
+    Assert.equal(FormAutofill.isAutofillAddressesEnabled, false);
+  }
+);
