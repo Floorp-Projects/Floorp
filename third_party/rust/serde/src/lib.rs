288cbac703730937cb670cf888eac1997b00c3a7
Erich Gubler <erichdongubler@gmail.com>
2024-03-01 20:44:17 +0000

    Bug 1882956: build(rust): update `serde` 1.0.195 → 1.0.197 r=supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D203228

--- third_party/rust/serde/src/lib.rs
+++ third_party/rust/serde/src/lib.rs
@@ -95,7 +95,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 // Serde types in rustdoc of other crates get linked to here.
-#![doc(html_root_url = "https://docs.rs/serde/1.0.195")]
+#![doc(html_root_url = "https://docs.rs/serde/1.0.197")]
 // Support using Serde without the standard library!
 #![cfg_attr(not(feature = "std"), no_std)]
 // Show which crate feature enables conditionally compiled APIs in documentation.
@@ -130,6 +130,7 @@
     clippy::derive_partial_eq_without_eq,
     clippy::enum_glob_use,
     clippy::explicit_auto_deref,
+    clippy::incompatible_msrv,
     clippy::let_underscore_untyped,
     clippy::map_err_ignore,
     clippy::new_without_default,
@@ -178,16 +179,16 @@ mod lib {
     pub use self::core::{cmp, mem, slice};
 
     pub use self::core::cell::{Cell, RefCell};
-    pub use self::core::clone::{self, Clone};
+    pub use self::core::clone;
     pub use self::core::cmp::Reverse;
-    pub use self::core::convert::{self, From, Into};
-    pub use self::core::default::{self, Default};
-    pub use self::core::fmt::{self, Debug, Display};
+    pub use self::core::convert;
+    pub use self::core::default;
+    pub use self::core::fmt::{self, Debug, Display, Write as FmtWrite};
     pub use self::core::marker::{self, PhantomData};
     pub use self::core::num::Wrapping;
     pub use self::core::ops::{Bound, Range, RangeFrom, RangeInclusive, RangeTo};
-    pub use self::core::option::{self, Option};
-    pub use self::core::result::{self, Result};
+    pub use self::core::option;
+    pub use self::core::result;
     pub use self::core::time::Duration;
 
     #[cfg(all(feature = "alloc", not(feature = "std")))]
