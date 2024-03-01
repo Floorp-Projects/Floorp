5579c4b72a1ff29ae1c73d4fcbca30bb035de800
Ben Hearsum <ben@mozilla.com>
2024-03-01 15:08:34 +0000

    Bug 1883046: make nightly decision tasks more resilient to unexpected Balrog release data r=releng-reviewers,taskgraph-reviewers,jcristau
    This fixes the specific issue we just had with a release only containing `partials`, but also deals with a potential future problem if `buildID` is missing. (Less likely...but still possible?)
    
    Differential Revision: https://phabricator.services.mozilla.com/D203278

--- taskcluster/gecko_taskgraph/test/test_util_partials.py
+++ taskcluster/gecko_taskgraph/test/test_util_partials.py
@@ -30,18 +30,34 @@ release_blob = {
 
 
 def nightly_blob(release):
-    return {
-        "platforms": {
-            "WINNT_x86_64-msvc": {
-                "locales": {
-                    "en-US": {
-                        "buildID": release[-14:],
-                        "completes": [{"fileUrl": release}],
+    # Added for bug 1883046, where we identified a case where a Balrog release
+    # that does not contain completes will throw an unnecessary exception.
+    if release == "Firefox-mozilla-central-nightly-20211001214601":
+        return {
+            "platforms": {
+                "WINNT_x86_64-msvc": {
+                    "locales": {
+                        "en-US": {
+                            "buildID": release[-14:],
+                            "partials": [{"fileUrl": release}],
+                        }
                     }
                 }
             }
         }
-    }
+    else:
+        return {
+            "platforms": {
+                "WINNT_x86_64-msvc": {
+                    "locales": {
+                        "en-US": {
+                            "buildID": release[-14:],
+                            "completes": [{"fileUrl": release}],
+                        }
+                    }
+                }
+            }
+        }
 
 
 class TestReleaseHistory(unittest.TestCase):
