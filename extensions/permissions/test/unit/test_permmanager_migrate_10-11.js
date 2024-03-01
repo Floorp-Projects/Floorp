df63f2a45a3f8fba2594b9170bdc6f542b6421fd
Dave Townsend <dtownsend@oxymoronical.com>
2024-03-01 18:28:35 +0000

    Bug 1864896: Autofix unused function arguments (extensions/permissions). r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D202966

--- extensions/permissions/test/unit/test_permmanager_migrate_10-11.js
+++ extensions/permissions/test/unit/test_permmanager_migrate_10-11.js
@@ -109,7 +109,7 @@ add_task(async function test() {
     ["http://foo.com^inBrowser=1", "A", 2, 0, 0, 0],
   ];
 
-  let found = expected.map(it => 0);
+  let found = expected.map(() => 0);
 
   // Add some places to the places database
   await PlacesTestUtils.addVisits(
