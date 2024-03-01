481b89a92aab61be5ff6bb895961bec99023d0f5
Nikki Sharpley <nsharpley@mozilla.com>
2024-03-01 16:50:59 +0000

    Bug 1882605 - Fix intermittent browser_tab_list_keyboard_navigation failure due to reordering of tabs r=fxview-reviewers,kcochrane,jsudiaman
    This patch also disables `browser_tab_list_keyboard_navigation` a11y failing tests noted in Bug 1882597
    until Bug 1850591 resolves a11y checks for the `moz-page-nav-button` button to be focusable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203182

--- browser/components/firefoxview/tests/browser/browser_tab_list_keyboard_navigation.js
+++ browser/components/firefoxview/tests/browser/browser_tab_list_keyboard_navigation.js
@@ -80,8 +80,9 @@ add_task(async function test_open_tab_row_with_sound_navigation() {
 
     await navigateToViewAndWait(document, "opentabs");
     const openTabs = document.querySelector("view-opentabs[name=opentabs]");
-    await TestUtils.waitForCondition(() =>
-      tabWithSound.hasAttribute("soundplaying")
+    await TestUtils.waitForCondition(
+      () => tabWithSound.hasAttribute("soundplaying"),
+      "Tab is playing sound"
     );
     await tabsUpdated;
     await openTabs.updateComplete;
@@ -91,8 +92,10 @@ add_task(async function test_open_tab_row_with_sound_navigation() {
       "The tab list hasn't rendered"
     );
 
-    // Focus tab row
-    let tabRow = openTabs.viewCards[0].tabList.rowEls[1];
+    // Focus tab row with sound playing
+    let tabRow = openTabs.viewCards[0].tabList.rowEls.find(rowEl =>
+      rowEl.indicators.includes("soundplaying")
+    );
     let tabRowFocused = BrowserTestUtils.waitForEvent(tabRow, "focus", win);
     tabRow.focus();
     await tabRowFocused;
