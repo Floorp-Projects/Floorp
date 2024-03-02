eede185935d29eae8a096f6c958a189dbf6cb111
Peter Van der Beken <peterv@propagandism.org>
2024-03-02 07:50:23 +0000

    Bug 1640839 - Stop using ImageBitmapFormat::EndGuard_. r=jgilbert
    Differential Revision: https://phabricator.services.mozilla.com/D201340

--- dom/media/ImageToI420.cpp
+++ dom/media/ImageToI420.cpp
@@ -11,6 +11,7 @@
 #include "mozilla/dom/ImageUtils.h"
 #include "mozilla/gfx/Point.h"
 #include "mozilla/RefPtr.h"
+#include "mozilla/Result.h"
 #include "nsThreadUtils.h"
 
 using mozilla::ImageFormat;
@@ -75,7 +76,12 @@ nsresult ConvertToI420(Image* aImage, uint8_t* aDestY, int aDestStrideY,
 
   if (const PlanarYCbCrData* data = GetPlanarYCbCrData(aImage)) {
     const ImageUtils imageUtils(aImage);
-    switch (imageUtils.GetFormat()) {
+    Maybe<dom::ImageBitmapFormat> format = imageUtils.GetFormat();
+    if (format.isNothing()) {
+      MOZ_ASSERT_UNREACHABLE("YUV format conversion not implemented");
+      return NS_ERROR_NOT_IMPLEMENTED;
+    }
+    switch (format.value()) {
       case ImageBitmapFormat::YUV420P:
         return MapRv(libyuv::I420ToI420(
             data->mYChannel, data->mYStride, data->mCbChannel,
