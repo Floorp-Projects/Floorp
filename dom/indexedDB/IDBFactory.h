20e7dc734f14c0e4d31260ac82b5e8e58a35c8c3
Jan Varga <jvarga@mozilla.com>
2024-03-04 17:35:45 +0000

    Bug 934640 - Extract background actor creation into a separate function; r=dom-storage-reviewers,jari
    EnsureBackgroundActor will be also called from IDBFactory::Databases.
    
    Differential Revision: https://phabricator.services.mozilla.com/D190017

--- dom/indexedDB/IDBFactory.h
+++ dom/indexedDB/IDBFactory.h
@@ -196,6 +196,8 @@ class IDBFactory final : public GlobalTeardownObserver, public nsWrapperCache {
   static nsresult AllowedForWindowInternal(nsPIDOMWindowInner* aWindow,
                                            nsCOMPtr<nsIPrincipal>* aPrincipal);
 
+  nsresult EnsureBackgroundActor();
+
   [[nodiscard]] RefPtr<IDBOpenDBRequest> OpenInternal(
       JSContext* aCx, nsIPrincipal* aPrincipal, const nsAString& aName,
       const Optional<uint64_t>& aVersion, bool aDeleting,
