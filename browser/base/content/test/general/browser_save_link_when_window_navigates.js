f9ff487c03ef07911b5adcc9fe6e417a5a34b329
Cosmin Sabou <csabou@mozilla.com>
2024-03-04 16:24:27 +0200

    Backed out changeset a89ec49d7645 (bug 1877195) for causing wpt failures on iframe_sandbox_window_open_download_allow_downloads. CLOSED TREE

--- browser/base/content/test/general/browser_save_link_when_window_navigates.js
+++ browser/base/content/test/general/browser_save_link_when_window_navigates.js
@@ -36,7 +36,7 @@ function triggerSave(aWindow, aCallback) {
   var fileName;
   let testBrowser = aWindow.gBrowser.selectedBrowser;
   let testURI =
-    "https://example.com/browser/browser/base/content/test/general/navigating_window_with_download.html";
+    "http://mochi.test:8888/browser/browser/base/content/test/general/navigating_window_with_download.html";
 
   // Only observe the UTC dialog if it's enabled by pref
   if (Services.prefs.getBoolPref(ALWAYS_ASK_PREF)) {
