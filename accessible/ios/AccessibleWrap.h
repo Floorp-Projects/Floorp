6a90a3b62301c535d4bf994175851e45be2aff33
Eitan Isaacson <eitan@monotonous.org>
2024-03-04 21:11:49 +0000

    Bug 1851273 - P1: Add accessible/ios directory. r=Jamie,morgan,nlapre
    Differential Revision: https://phabricator.services.mozilla.com/D187328

---
+++ accessible/ios/AccessibleWrap.h
@@ -0,0 +1,30 @@
+/* clang-format off */
+/* -*- Mode: Objective-C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* clang-format on */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* For documentation of the accessibility architecture,
+ * see https://firefox-source-docs.mozilla.org/accessible/index.html
+ */
+
+#ifndef mozilla_a11y_AccessibleWrap_h_
+#define mozilla_a11y_AccessibleWrap_h_
+
+#include "nsCOMPtr.h"
+#include "LocalAccessible.h"
+
+namespace mozilla {
+namespace a11y {
+
+class AccessibleWrap : public LocalAccessible {
+ public:  // construction, destruction
+  AccessibleWrap(nsIContent* aContent, DocAccessible* aDoc);
+  virtual ~AccessibleWrap() = default;
+};
+
+}  // namespace a11y
+}  // namespace mozilla
+
+#endif
