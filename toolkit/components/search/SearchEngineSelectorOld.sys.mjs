bbdf41da6cd79ed00fac1f9920c1f942b7e29cdd
Mark Banner <standard8@mozilla.com>
2024-03-05 18:39:41 +0000

    Bug 1880432 - When resetting the search service in tests, also reset the engine selector listeners.  a=diannaS
    This avoids multiple notifications when resetting the SearchService and then performing configuration updates.
    
    Original Revision: https://phabricator.services.mozilla.com/D203003
    
    Differential Revision: https://phabricator.services.mozilla.com/D203435

--- toolkit/components/search/SearchEngineSelectorOld.sys.mjs
+++ toolkit/components/search/SearchEngineSelectorOld.sys.mjs
@@ -84,6 +84,20 @@ export class SearchEngineSelectorOld {
   }
 
   /**
+   * Resets the remote settings listeners.
+   */
+  reset() {
+    if (this._listenerAdded) {
+      this._remoteConfig.off("sync", this._onConfigurationUpdated);
+      this._remoteConfigOverrides.off(
+        "sync",
+        this._onConfigurationOverridesUpdated
+      );
+      this._listenerAdded = false;
+    }
+  }
+
+  /**
    * Handles getting the configuration from remote settings.
    */
   async getEngineConfiguration() {
