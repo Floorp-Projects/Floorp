49e92523502583b7c78e78bdf0b63afec9fb95b7
Noemi Erli <nerli@mozilla.com>
2024-03-06 01:15:23 +0200

    Backed out 3 changesets (bug 1879156, bug 1879624) for causing bustages in nsUserCharacteristics.cpp CLOSED TREE
    Backed out changeset 681ebb76953a (bug 1879624)
    Backed out changeset e7861bf5ec6e (bug 1879156)
    Backed out changeset 1e78188b948b (bug 1879156)

--- toolkit/components/resistfingerprinting/tests/gtest/test_usercharping.cpp
+++ toolkit/components/resistfingerprinting/tests/gtest/test_usercharping.cpp
@@ -17,25 +17,16 @@ const auto* const kUUIDPref =
 
 TEST(ResistFingerprinting, UserCharacteristics_Simple)
 {
-  mozilla::glean::characteristics::max_touch_points.Set(7);
-
   bool submitted = false;
   mozilla::glean_pings::UserCharacteristics.TestBeforeNextSubmit(
-      [&submitted](const nsACString& aReason) {
-        submitted = true;
-
-        ASSERT_EQ(
-            7, mozilla::glean::characteristics::max_touch_points.TestGetValue()
-                   .unwrap()
-                   .ref());
-      });
+      [&submitted](const nsACString& aReason) { submitted = true; });
   mozilla::glean_pings::UserCharacteristics.Submit();
   ASSERT_TRUE(submitted);
 }
 
 TEST(ResistFingerprinting, UserCharacteristics_Complex)
 {
-  nsUserCharacteristics::PopulateData(true);
+  nsUserCharacteristics::PopulateData();
 
   bool submitted = false;
   mozilla::glean_pings::UserCharacteristics.TestBeforeNextSubmit(
@@ -61,11 +52,6 @@ TEST(ResistFingerprinting, UserCharacteristics_Complex)
                 .unwrap()
                 .value()
                 .get());
-        ASSERT_EQ(
-            testing::MaxTouchPoints(),
-            mozilla::glean::characteristics::max_touch_points.TestGetValue()
-                .unwrap()
-                .ref());
       });
   nsUserCharacteristics::SubmitPing();
   ASSERT_TRUE(submitted);
@@ -102,7 +88,7 @@ TEST(ResistFingerprinting, UserCharacteristics_ClearPref)
                 .value()
                 .get());
       });
-  nsUserCharacteristics::PopulateData(true);
+  nsUserCharacteristics::PopulateData();
   nsUserCharacteristics::SubmitPing();
 
   auto original_value =
@@ -135,7 +121,7 @@ TEST(ResistFingerprinting, UserCharacteristics_ClearPref)
         Preferences::GetCString(kUUIDPref, uuidValue);
         ASSERT_STRNE("", uuidValue.get());
       });
-  nsUserCharacteristics::PopulateData(true);
+  nsUserCharacteristics::PopulateData();
   nsUserCharacteristics::SubmitPing();
 
   Preferences::SetBool("datareporting.healthreport.uploadEnabled",
