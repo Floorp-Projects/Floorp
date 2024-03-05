90b63cce54ce767a8593666ad866d724465ca00f
Thomas Wisniewski <wisniewskit@gmail.com>
2024-03-04 20:58:59 -0500

    Bug 1881803 - update webcompat-reporter to version 2.1.0

--- mobile/android/android-components/components/feature/webcompat-reporter/src/main/assets/extensions/webcompat-reporter/background.js
+++ mobile/android/android-components/components/feature/webcompat-reporter/src/main/assets/extensions/webcompat-reporter/background.js
@@ -6,226 +6,88 @@
 
 /* globals browser */
 
-const Config = {
-  newIssueEndpoint: "https://webcompat.com/issues/new",
-  newIssueEndpointPref: "newIssueEndpoint",
-  productName: "",
-  screenshotFormat: {
-    format: "jpeg",
-    quality: 75,
-  },
+const desktopReporterConfig = {
+  src: "desktop-reporter",
+  utm_campaign: "report-site-issue-button",
+  utm_source: "desktop-reporter",
 };
 
-const FRAMEWORK_KEYS = ["hasFastClick", "hasMobify", "hasMarfeel"];
+const androidReporterConfig = {
+  src: "android-components-reporter",
+  utm_campaign: "report-site-issue-button",
+  utm_source: "android-components-reporter",
+};
 
-browser.pageAction.onClicked.addListener(tab => {
-  return getWebCompatInfoForTab(tab).then(
-    info => {
-      return openWebCompatTab(info);
-    },
-    err => {
-      console.error("WebCompat Reporter: unexpected error", err);
-    }
-  );
-});
+const getReporterConfig = (() => {
+  let promise;
+  return async () => {
+    promise ??= new Promise(resolve => {
+      browser.permissions
+        .contains({ permissions: ["nativeMessaging"] })
+        .then(needProductName => {
+          if (needProductName) {
+            const port = browser.runtime.connectNative(
+              "mozacWebcompatReporter"
+            );
+            port.onMessage.addListener(message => {
+              if ("productName" in message) {
+                androidReporterConfig.productName = message.productName;
+                resolve(androidReporterConfig);
 
-browser.aboutConfigPrefs.onEndpointPrefChange.addListener(checkEndpointPref);
-
-checkEndpointPref();
-
-async function checkEndpointPref() {
-  const value = await browser.aboutConfigPrefs.getEndpointPref();
-  if (value === undefined) {
-    browser.aboutConfigPrefs.setEndpointPref(Config.newIssueEndpoint);
-  } else {
-    Config.newIssueEndpoint = value;
-  }
-}
-
-(() => {
-  let port = browser.runtime.connectNative("mozacWebcompatReporter");
-  port.onMessage.addListener(message => {
-    if ("productName" in message) {
-      Config.productName = message.productName;
-
-      // For now, setting the productName is the only use for this port, and that's only happening
-      // once after startup, so let's disconnect the port when we're done.
-      port.disconnect();
-    }
-  });
+                // For now, setting the productName is the only use for this port, and that's only happening
+                // once after startup, so let's disconnect the port when we're done.
+                port.disconnect();
+              }
+            });
+          } else {
+            resolve(desktopReporterConfig);
+          }
+        });
+    });
+    return promise;
+  };
 })();
 
-function hasFastClickPageScript() {
-  const win = window.wrappedJSObject;
-
-  if (win.FastClick) {
-    return true;
-  }
-
-  for (const property in win) {
-    try {
-      const proto = win[property].prototype;
-      if (proto && proto.needsClick) {
-        return true;
+async function loadTab(url) {
+  const newTab = await browser.tabs.create({ url });
+  return new Promise(resolve => {
+    const listener = (tabId, changeInfo, tab) => {
+      if (
+        tabId == newTab.id &&
+        tab.url !== "about:blank" &&
+        changeInfo.status == "complete"
+      ) {
+        browser.tabs.onUpdated.removeListener(listener);
+        resolve(newTab);
       }
-    } catch (_) {}
-  }
-
-  return false;
-}
-
-function hasMobifyPageScript() {
-  const win = window.wrappedJSObject;
-  return !!(win.Mobify && win.Mobify.Tag);
-}
-
-function hasMarfeelPageScript() {
-  const win = window.wrappedJSObject;
-  return !!win.marfeel;
-}
-
-function checkForFrameworks(tabId) {
-  return browser.tabs
-    .executeScript(tabId, {
-      code: `
-      (function() {
-        ${hasFastClickPageScript};
-        ${hasMobifyPageScript};
-        ${hasMarfeelPageScript};
-
-        const result = {
-          hasFastClick: hasFastClickPageScript(),
-          hasMobify: hasMobifyPageScript(),
-          hasMarfeel: hasMarfeelPageScript(),
-        }
-
-        return result;
-      })();
-    `,
-    })
-    .then(([results]) => results)
-    .catch(() => false);
-}
-
-function getWebCompatInfoForTab(tab) {
-  const { id, url } = tab;
-  return Promise.all([
-    browser.browserInfo.getBlockList(),
-    browser.browserInfo.getBuildID(),
-    browser.browserInfo.getGraphicsPrefs(),
-    browser.browserInfo.getUpdateChannel(),
-    browser.browserInfo.hasTouchScreen(),
-    browser.tabExtras.getWebcompatInfo(id),
-    browser.browserInfo.getAdditionalData(),
-    checkForFrameworks(id),
-    browser.tabs.captureTab(id, Config.screenshotFormat).catch(e => {
-      console.error("WebCompat Reporter: getting a screenshot failed", e);
-      return Promise.resolve(undefined);
-    }),
-  ]).then(
-    ([
-      blockList,
-      buildID,
-      graphicsPrefs,
-      channel,
-      hasTouchScreen,
-      frameInfo,
-      additionalData,
-      frameworks,
-      screenshot,
-    ]) => {
-      if (channel !== "linux") {
-        delete graphicsPrefs["layers.acceleration.force-enabled"];
-      }
-
-      const consoleLog = frameInfo.log;
-      delete frameInfo.log;
-
-      additionalData.isPB = frameInfo.isPB;
-      additionalData.prefs = { ...additionalData.prefs, ...graphicsPrefs };
-      additionalData.hasMixedActiveContentBlocked =
-        frameInfo.hasMixedActiveContentBlocked;
-      additionalData.hasMixedDisplayContentBlocked =
-        frameInfo.hasMixedDisplayContentBlocked;
-      additionalData.hasTrackingContentBlocked =
-        !!frameInfo.hasTrackingContentBlocked;
-
-      return Object.assign(frameInfo, {
-        tabId: id,
-        blockList,
-        details: Object.assign(graphicsPrefs, {
-          buildID,
-          channel,
-          consoleLog,
-          frameworks,
-          additionalData,
-          hasTouchScreen,
-          "mixed active content blocked":
-            frameInfo.hasMixedActiveContentBlocked,
-          "mixed passive content blocked":
-            frameInfo.hasMixedDisplayContentBlocked,
-          "tracking content blocked": frameInfo.hasTrackingContentBlocked
-            ? `true (${blockList})`
-            : "false",
-        }),
-        screenshot,
-        url,
-      });
-    }
-  );
-}
-
-function stripNonASCIIChars(str) {
-  // eslint-disable-next-line no-control-regex
-  return str.replace(/[^\x00-\x7F]/g, "");
-}
-
-async function openWebCompatTab(compatInfo) {
-  const url = new URL(Config.newIssueEndpoint);
-  const { details } = compatInfo;
-  const params = {
-    url: `${compatInfo.url}`,
-    utm_source: "android-components-reporter",
-    utm_campaign: "report-site-issue-button",
-    src: "android-components-reporter",
-    details,
-    extra_labels: [`browser-${Config.productName}`],
-  };
-
-  for (let framework of FRAMEWORK_KEYS) {
-    if (details.frameworks[framework]) {
-      params.details[framework] = true;
-      params.extra_labels.push(
-        framework.replace(/^has/, "type-").toLowerCase()
-      );
-    }
-  }
-  delete details.frameworks;
-
-  if (details["gfx.webrender.all"] || details["gfx.webrender.enabled"]) {
-    params.extra_labels.push("type-webrender-enabled");
-  }
-  if (compatInfo.hasTrackingContentBlocked) {
-    params.extra_labels.push(
-      `type-tracking-protection-${compatInfo.blockList}`
-    );
-  }
-
-  const json = stripNonASCIIChars(JSON.stringify(params));
-  const tab = await browser.tabs.create({ url: url.href });
-  await browser.tabs.executeScript(tab.id, {
-    runAt: "document_end",
-    code: `(function() {
-      async function postMessageData(dataURI, metadata) {
-        const res = await fetch(dataURI);
-        const blob = await res.blob();
-        const data = {
-           screenshot: blob,
-           message: metadata
-        };
-        postMessage(data, "${url.origin}");
-      }
-      postMessageData("${compatInfo.screenshot}", ${json});
-    })()`,
+    };
+    browser.tabs.onUpdated.addListener(listener);
   });
+}
+
+async function captureAndSendReport(tab) {
+  const { id, url } = tab;
+  try {
+    const { endpointUrl, webcompatInfo } =
+      await browser.tabExtras.getWebcompatInfo(id);
+    const reporterConfig = await getReporterConfig();
+    const dataToSend = {
+      endpointUrl,
+      reportUrl: url,
+      reporterConfig,
+      webcompatInfo,
+    };
+    const newTab = await loadTab(endpointUrl);
+    browser.tabExtras.sendWebcompatInfo(newTab.id, dataToSend);
+  } catch (err) {
+    console.error("WebCompat Reporter: unexpected error", err);
+  }
+}
+
+if ("helpMenu" in browser) {
+  // desktop
+  browser.helpMenu.onHelpMenuCommand.addListener(captureAndSendReport);
+} else {
+  // Android
+  browser.pageAction.onClicked.addListener(captureAndSendReport);
 }
