b32f15f0451900e69b001020f92a5f9e78e9cfc4
Niklas Baumgardner <nbaumgardner@mozilla.com>
2024-03-05 16:50:31 +0000

    Bug 1883143 - Initial profiles setup. r=jhirsch,mossop
    Differential Revision: https://phabricator.services.mozilla.com/D203299

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -3024,3 +3024,6 @@ pref("browser.mailto.dualPrompt.os", false);
 pref("browser.mailto.prompt.os", true);
 
 pref("browser.backup.enabled", false);
+
+// Pref to enable the new profiles
+pref("browser.profiles.enabled", false);
