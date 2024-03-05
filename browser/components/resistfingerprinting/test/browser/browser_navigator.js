8dd46277f1b2d941fe661bd194cd5ce6204ce95e
Chris Peterson <cpeterson@mozilla.com>
2024-03-05 01:25:54 +0000

    Bug 1860417 - Add "Linux" to Firefox's Android UA string. r=necko-reviewers,geckoview-reviewers,kershaw,amejiamarmol
    Chrome's Android UA string lists "Linux; Android", while Firefox's lists only "Android". This has caused webcompat problems for at least one website (samsung.com) because it assumes every browser UA string will include one of "Windows", "Mac", or "Linux". Let's add "Linux; Android" to Firefox's UA string, like Chrome's, to satisfy that website assumption.
    
    Firefox's current Android UA string:
    
    `Mozilla/5.0 (Android 10; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0`
    
    Firefox's new Android UA string:
    
    `Mozilla/5.0 (Linux; Android 10; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0`
    
    For comparison, an example of Chrome's Android UA string:
    
    `Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36`
    
    Differential Revision: https://phabricator.services.mozilla.com/D202653

--- browser/components/resistfingerprinting/test/browser/browser_navigator.js
+++ browser/components/resistfingerprinting/test/browser/browser_navigator.js
@@ -20,7 +20,7 @@ const DEFAULT_APPVERSION = {
   linux: "5.0 (X11)",
   win: "5.0 (Windows)",
   macosx: "5.0 (Macintosh)",
-  android: `5.0 (Android ${osVersion})`,
+  android: `5.0 (Linux; Android ${osVersion})`,
   other: "5.0 (X11)",
 };
 
@@ -28,7 +28,7 @@ const SPOOFED_APPVERSION = {
   linux: "5.0 (X11)",
   win: "5.0 (Windows)",
   macosx: "5.0 (Macintosh)",
-  android: "5.0 (Android 10)",
+  android: "5.0 (Linux; Android 10)",
   other: "5.0 (X11)",
 };
 
@@ -103,7 +103,7 @@ const SPOOFED_OSCPU = {
 const DEFAULT_UA_OS = {
   linux: `X11; Linux ${defaultLinuxCpu}`,
   macosx: "Macintosh; Intel Mac OS X 10.15",
-  android: `Android ${osVersion}; Mobile`,
+  android: `Linux; Android ${osVersion}; Mobile`,
   other: `X11; Linux ${defaultLinuxCpu}`,
 };
 
@@ -111,14 +111,14 @@ const SPOOFED_UA_NAVIGATOR_OS = {
   linux: "X11; Linux x86_64",
   win: "Windows NT 10.0; Win64; x64",
   macosx: "Macintosh; Intel Mac OS X 10.15",
-  android: "Android 10; Mobile",
+  android: "Linux; Android 10; Mobile",
   other: "X11; Linux x86_64",
 };
 const SPOOFED_UA_HTTPHEADER_OS = {
   linux: "Windows NT 10.0",
   win: "Windows NT 10.0",
   macosx: "Windows NT 10.0",
-  android: "Android 10; Mobile",
+  android: "Linux; Android 10; Mobile",
   other: "Windows NT 10.0",
 };
 const SPOOFED_HW_CONCURRENCY = 2;
--- browser/components/resistfingerprinting/test/browser/browser_navigator_iframes.js
+++ browser/components/resistfingerprinting/test/browser/browser_navigator_iframes.js
@@ -35,7 +35,7 @@ const DEFAULT_APPVERSION = {
   linux: "5.0 (X11)",
   win: "5.0 (Windows)",
   macosx: "5.0 (Macintosh)",
-  android: `5.0 (Android ${osVersion})`,
+  android: `5.0 (Linux; Android ${osVersion})`,
   other: "5.0 (X11)",
 };
 
@@ -43,7 +43,7 @@ const SPOOFED_APPVERSION = {
   linux: "5.0 (X11)",
   win: "5.0 (Windows)",
   macosx: "5.0 (Macintosh)",
-  android: "5.0 (Android 10)",
+  android: "5.0 (Linux; Android 10)",
   other: "5.0 (X11)",
 };
 
@@ -118,7 +118,7 @@ const SPOOFED_OSCPU = {
 const DEFAULT_UA_OS = {
   linux: `X11; Linux ${defaultLinuxCpu}`,
   macosx: "Macintosh; Intel Mac OS X 10.15",
-  android: `Android ${osVersion}; Mobile`,
+  android: `Linux; Android ${osVersion}; Mobile`,
   other: `X11; Linux ${defaultLinuxCpu}`,
 };
 
@@ -126,14 +126,14 @@ const SPOOFED_UA_NAVIGATOR_OS = {
   linux: "X11; Linux x86_64",
   win: "Windows NT 10.0; Win64; x64",
   macosx: "Macintosh; Intel Mac OS X 10.15",
-  android: "Android 10; Mobile",
+  android: "Linux; Android 10; Mobile",
   other: "X11; Linux x86_64",
 };
 const SPOOFED_UA_HTTPHEADER_OS = {
   linux: "Windows NT 10.0",
   win: "Windows NT 10.0",
   macosx: "Windows NT 10.0",
-  android: "Android 10; Mobile",
+  android: "Linux; Android 10; Mobile",
   other: "Windows NT 10.0",
 };
 const SPOOFED_HW_CONCURRENCY = 2;
