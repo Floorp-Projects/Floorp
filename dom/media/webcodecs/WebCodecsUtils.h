187b4384d84757dc846d5fecadd90efb95dbfd76
Paul Adenot <paul@paul.cx>
2024-03-05 15:52:17 +0000

    Bug 1749044 - Move GetExtraData from VideoDecoder.cpp to WebCodecsUtils.cpp. r=chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D192352

--- dom/media/webcodecs/WebCodecsUtils.h
+++ dom/media/webcodecs/WebCodecsUtils.h
@@ -8,6 +8,7 @@
 #define MOZILLA_DOM_WEBCODECS_WEBCODECSUTILS_H
 
 #include "ErrorList.h"
+#include "MediaData.h"
 #include "js/TypeDecls.h"
 #include "mozilla/Maybe.h"
 #include "mozilla/MozPromise.h"
@@ -87,6 +88,9 @@ Result<Ok, nsresult> CloneBuffer(
     JSContext* aCx,
     OwningMaybeSharedArrayBufferViewOrMaybeSharedArrayBuffer& aDest,
     const OwningMaybeSharedArrayBufferViewOrMaybeSharedArrayBuffer& aSrc);
+
+Result<RefPtr<MediaByteBuffer>, nsresult> GetExtraDataFromArrayBuffer(
+    const OwningMaybeSharedArrayBufferViewOrMaybeSharedArrayBuffer& aBuffer);
 
 /*
  * The following are utilities to convert between VideoColorSpace values to
