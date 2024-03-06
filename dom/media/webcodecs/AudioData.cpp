44a49575b1631333210d0ed97de32e57e0311f9a
Paul Adenot <paul@paul.cx>
2024-03-06 11:14:19 +0000

    Bug 1858958 - Test more conversions, clearly mark planar->interleaved conversion as outside of spec. r=chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D192339

--- dom/media/webcodecs/AudioData.cpp
+++ dom/media/webcodecs/AudioData.cpp
@@ -443,6 +443,7 @@ void CopySamples(Span<S> aSource, Span<D> aDest, uint32_t aSourceChannelCount,
   }
 
   if (!IsInterleaved(aSourceFormat) && IsInterleaved(aCopyToSpec.mFormat)) {
+    MOZ_CRASH("This should never be hit -- current spec doesn't support it");
     // Planar to interleaved -- copy of all channels of the source into the
     // destination buffer.
     MOZ_ASSERT(aCopyToSpec.mPlaneIndex == 0);
