f95ead7b49ea90ef405ef5e1dc18ba7f0c07abe5
Barret Rennie <barret@brennie.ca>
2024-03-07 14:00:04 +0000

    Bug 1883672 - Remove isEarlyStartup flag from jsParallelParsing r=denispal
    This flag was only used to set the setPref prefs on the user branch. Bug
    1875404 updated the setPref annotation to specify the branch for each pref, so
    the isEarlyStartup flag was not doing anything for this feature.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203662

--- toolkit/components/nimbus/generate/generate_feature_manifest.py
+++ toolkit/components/nimbus/generate/generate_feature_manifest.py
@@ -29,7 +29,6 @@ ALLOWED_ISEARLYSTARTUP_FEATURE_IDS = {
     "dapTelemetry",
     "deviceMigration",
     "gleanInternalSdk",
-    "jsParallelParsing",
     "majorRelease2022",
     "migrationWizard",
     "newtab",
