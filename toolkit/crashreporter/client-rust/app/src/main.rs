f1043cdefaa5e9c3550d66ee07d3e164079a7ada
Alex Franchuk <afranchuk@mozilla.com>
2024-03-07 19:47:09 +0000

    Bug 1759175 pt3 - Crashreporter business logic r=gsvelto,fluent-reviewers,flod,eemeli,cmartin
    Differential Revision: https://phabricator.services.mozilla.com/D185942

--- toolkit/crashreporter/client-rust/app/src/main.rs
+++ toolkit/crashreporter/client-rust/app/src/main.rs
@@ -6,6 +6,10 @@
 // application.
 #![cfg_attr(windows, windows_subsystem = "windows")]
 
+use crate::std::sync::Arc;
+use anyhow::Context;
+use config::Config;
+
 /// cc is short for Clone Capture, a shorthand way to clone a bunch of values before an expression
 /// (particularly useful for closures).
 ///
@@ -19,11 +23,85 @@ macro_rules! cc {
     }
 }
 
+mod async_task;
+mod config;
 mod data;
+mod lang;
+mod logging;
+mod logic;
+mod net;
+mod process;
+mod settings;
+mod std;
+mod thread_bound;
 mod ui;
 
-use ui::*;
-
 fn main() {
-    todo!();
+    let log_target = logging::init();
+
+    let mut config = Config::new();
+    let config_result = config.read_from_environment();
+    config.log_target = Some(log_target);
+
+    let mut config = Arc::new(config);
+
+    let result = config_result.and_then(|()| {
+        let attempted_send = try_run(&mut config)?;
+        if !attempted_send {
+            // Exited without attempting to send the crash report; delete files.
+            config.delete_files();
+        }
+        Ok(())
+    });
+
+    if let Err(message) = result {
+        // TODO maybe errors should also delete files?
+        log::error!("exiting with error: {message}");
+        if !config.auto_submit {
+            // Only show a dialog if auto_submit is disabled.
+            ui::error_dialog(&config, message);
+        }
+        std::process::exit(1);
+    }
+}
+
+fn try_run(config: &mut Arc<Config>) -> anyhow::Result<bool> {
+    if config.dump_file.is_none() {
+        if !config.auto_submit {
+            Err(anyhow::anyhow!(config.string("crashreporter-information")))
+        } else {
+            Ok(false)
+        }
+    } else {
+        // Run minidump-analyzer to gather stack traces.
+        {
+            let analyzer_path = config.sibling_program_path("minidump-analyzer");
+            let mut cmd = crate::process::background_command(&analyzer_path);
+            if config.dump_all_threads {
+                cmd.arg("--full");
+            }
+            cmd.arg(config.dump_file());
+            let output = cmd
+                .output()
+                .with_context(|| config.string("crashreporter-error-minidump-analyzer"))?;
+            if !output.status.success() {
+                log::warn!(
+                    "minidump-analyzer failed to run ({});\n\nstderr: {}\n\nstdout: {}",
+                    output.status,
+                    String::from_utf8_lossy(&output.stderr),
+                    String::from_utf8_lossy(&output.stdout),
+                );
+            }
+        }
+
+        let extra = {
+            // Perform a few things which may change the config, then treat is as immutable.
+            let config = Arc::get_mut(config).expect("unexpected config references");
+            let extra = config.load_extra_file()?;
+            config.move_crash_data_to_pending()?;
+            extra
+        };
+
+        logic::ReportCrash::new(config.clone(), extra)?.run()
+    }
 }
