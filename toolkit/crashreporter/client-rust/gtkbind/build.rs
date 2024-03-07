1a30a52c0ff6cbcc905f6b3217a6fe0131d708f1
Alex Franchuk <afranchuk@mozilla.com>
2024-03-07 19:47:08 +0000

    Bug 1759175 pt1 - Repository integration r=glandium,supply-chain-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D174916

---
+++ toolkit/crashreporter/client-rust/gtkbind/build.rs
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+use mozbuild::config::{
+    CC_BASE_FLAGS as CFLAGS, MOZ_GTK3_CFLAGS as GTK_CFLAGS, MOZ_GTK3_LIBS as GTK_LIBS,
+};
+
+fn main() {
+    let bindings = bindgen::Builder::default()
+        .header_contents("gtk_bindings.h", r#"#include "gtk/gtk.h""#)
+        .clang_args(CFLAGS)
+        .clang_args(GTK_CFLAGS)
+        .allowlist_function("gtk_.*")
+        .allowlist_function("g_(application|main_context|object|signal|timeout)_.*")
+        // The gtk/glib valist functions generate FFI-unsafe signatures on aarch64 which cause
+        // compile errors. We don't use them anyway.
+        .blocklist_function(".*_valist")
+        .derive_default(true)
+        .generate()
+        .expect("unable to generate gtk bindings");
+    for flag in GTK_LIBS {
+        if let Some(lib) = flag.strip_prefix("-l") {
+            println!("cargo:rustc-link-lib={lib}");
+        } else if let Some(path) = flag.strip_prefix("-L") {
+            println!("cargo:rustc-link-search={path}");
+        }
+    }
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("gtk_bindings.rs"))
+        .expect("failed to write gtk bindings");
+}
