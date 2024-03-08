bda9f1263a99d24deafb65b0193788000650f2b6
Punam Dahiya <pdahiya@mozilla.com>
2024-03-08 23:06:14 +0000

    Bug 1877210 - Remove unused asrouter components r=omc-reviewers,aminomancer
    Differential Revision: https://phabricator.services.mozilla.com/D204122

--- browser/components/asrouter/content-src/components/ConditionalWrapper/ConditionalWrapper.jsx
+++
@@ -1,9 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-// lifted from https://gist.github.com/kitze/23d82bb9eb0baabfd03a6a720b1d637f
-const ConditionalWrapper = ({ condition, wrap, children }) =>
-  condition && wrap ? wrap(children) : children;
-
-export default ConditionalWrapper;
