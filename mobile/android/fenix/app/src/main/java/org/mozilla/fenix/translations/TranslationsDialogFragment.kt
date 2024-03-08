f246886e7464b89b6d3b3e6e02c0f8232febfffe
iorgamgabriel <iorgamgabriel@yahoo.com>
2024-03-08 17:46:30 +0200

    Bug 1883465 - Bottom Sheet in landscape should span the full screen

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/translations/TranslationsDialogFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/translations/TranslationsDialogFragment.kt
@@ -74,6 +74,7 @@ class TranslationsDialogFragment : BottomSheetDialogFragment() {
                 val bottomSheet = findViewById<View?>(R.id.design_bottom_sheet)
                 bottomSheet?.setBackgroundResource(android.R.color.transparent)
                 behavior = BottomSheetBehavior.from(bottomSheet)
+                behavior?.peekHeight = resources.displayMetrics.heightPixels
                 behavior?.state = BottomSheetBehavior.STATE_EXPANDED
             }
         }
