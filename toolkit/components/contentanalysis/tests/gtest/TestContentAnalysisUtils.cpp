8f2cbb8158aa0b37f527bedcee627e020b6ff907
David P <daparks@mozilla.com>
2024-03-08 20:28:09 +0000

    Bug 1879181: Unit test ContentAnalysis URL filter lists r=gstoll
    Would be nice to check all of AnalyzeContentRequest but proper CA unit tests
    are still wip.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203509

--- toolkit/components/contentanalysis/tests/gtest/TestContentAnalysisUtils.cpp
+++ toolkit/components/contentanalysis/tests/gtest/TestContentAnalysisUtils.cpp
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "TestContentAnalysis.h"
+#include "TestContentAnalysisAgent.h"
 #include <combaseapi.h>
 #include <pathcch.h>
 #include <shlwapi.h>
