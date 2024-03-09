c00eee27fa500cc634d6fcef6125d2a36b438910
Martin Robinson <mrobinson@igalia.com>
2024-03-09 15:30:27 +0000

    Bug 1884403 - WebRender should only depend on `glean` when building for Gecko r=jrmuizel
    Make glean an optional dependency that is only enabled when building
    WebRender for Gecko.
    
    Differential Revision: https://phabricator.services.mozilla.com/D204059

--- gfx/wr/webrender/src/telemetry.rs
+++ gfx/wr/webrender/src/telemetry.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#[cfg(feature = "gecko")]
 use glean::TimerId;
 #[cfg(feature = "gecko")]
 use firefox_on_glean::metrics::wr;
@@ -9,16 +10,19 @@ use std::time::Duration;
 
 pub struct Telemetry;
 
+#[cfg(not(feature = "gecko"))]
+pub struct TimerId;
+
 /// Defines the interface for hooking up an external telemetry reporter to WR.
 #[cfg(not(feature = "gecko"))]
 impl Telemetry {
     pub fn record_rasterize_blobs_time(_duration: Duration) { }
-    pub fn start_framebuild_time() -> TimerId { TimerId { id: 0 } }
+    pub fn start_framebuild_time() -> TimerId { TimerId }
     pub fn stop_and_accumulate_framebuild_time(_id: TimerId) { }
     pub fn record_renderer_time(_duration: Duration) { }
     pub fn record_renderer_time_no_sc(_duration: Duration) { }
     pub fn record_scenebuild_time(_duration: Duration) { }
-    pub fn start_sceneswap_time() -> TimerId { TimerId { id: 0 } }
+    pub fn start_sceneswap_time() -> TimerId { TimerId }
     pub fn stop_and_accumulate_sceneswap_time(_id: TimerId) { }
     pub fn cancel_sceneswap_time(_id: TimerId) { }
     pub fn record_texture_cache_update_time(_duration: Duration) { }
