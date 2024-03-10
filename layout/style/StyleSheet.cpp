97971f8448b3e2b3dc3ea0a62e0b8ad3ebcbb0f0
sunil mayya <smayya@mozilla.com>
2024-03-10 14:12:04 +0000

    Bug 1864817 - implement OnDataFinished for CSSLoader. r=jesup,necko-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D189403

--- layout/style/StyleSheet.cpp
+++ layout/style/StyleSheet.cpp
@@ -28,7 +28,6 @@
 #include "mozilla/css/SheetLoadData.h"
 
 #include "mozAutoDocUpdate.h"
-#include "SheetLoadData.h"
 
 namespace mozilla {
 
@@ -41,6 +40,7 @@ StyleSheet::StyleSheet(css::SheetParsingMode aParsingMode, CORSMode aCORSMode,
       mDocumentOrShadowRoot(nullptr),
       mParsingMode(aParsingMode),
       mState(static_cast<State>(0)),
+      mMutex("StyleSheet::mMutex"),
       mInner(new StyleSheetInfo(aCORSMode, aIntegrity, aParsingMode)) {
   mInner->AddSheet(this);
 }
@@ -54,6 +54,7 @@ StyleSheet::StyleSheet(const StyleSheet& aCopy, StyleSheet* aParentSheetToUse,
       mDocumentOrShadowRoot(aDocOrShadowRootToUse),
       mParsingMode(aCopy.mParsingMode),
       mState(aCopy.mState),
+      mMutex("StyleSheet::mMutex"),
       // Shallow copy, but concrete subclasses will fix up.
       mInner(aCopy.mInner) {
   MOZ_ASSERT(!aConstructorDocToUse || aCopy.IsConstructed());
@@ -71,6 +72,10 @@ StyleSheet::StyleSheet(const StyleSheet& aCopy, StyleSheet* aParentSheetToUse,
     mState &= ~(State::ForcedUniqueInner | State::ModifiedRules |
                 State::ModifiedRulesForDevtools);
   }
+
+  // we could have cloned sheet which has not been parsed yet
+  // reset the parsing state flag
+  mState &= ~State::AsyncParseOngoing;
 
   if (aCopy.mMedia) {
     // XXX This is wrong; we should be keeping @import rules and
@@ -740,7 +745,9 @@ already_AddRefed<dom::Promise> StyleSheet::Replace(const nsACString& aText,
   loadData->mIsBeingParsed = true;
   MOZ_ASSERT(!mReplacePromise);
   mReplacePromise = promise;
-  ParseSheet(*loader, aText, *loadData)
+  RefPtr<css::SheetLoadDataHolder> holder(
+      new css::SheetLoadDataHolder(__func__, loadData, false));
+  ParseSheet(*loader, aText, holder)
       ->Then(
           target, __func__,
           [loadData] { loadData->SheetFinishedParsingAsync(); },
@@ -1159,28 +1166,22 @@ already_AddRefed<StyleSheet> StyleSheet::CreateEmptyChildSheet(
   return child.forget();
 }
 
-// We disable parallel stylesheet parsing if the browser is recording CSS errors
-// (which parallel parsing can't handle).
-static bool AllowParallelParse(css::Loader& aLoader, URLExtraData* aUrlData) {
-  Document* doc = aLoader.GetDocument();
-  if (doc && css::ErrorReporter::ShouldReportErrors(*doc)) {
-    return false;
-  }
-  // Otherwise we can parse in parallel.
-  return true;
-}
-
 RefPtr<StyleSheetParsePromise> StyleSheet::ParseSheet(
     css::Loader& aLoader, const nsACString& aBytes,
-    css::SheetLoadData& aLoadData) {
+    const RefPtr<css::SheetLoadDataHolder>& aLoadData) {
   MOZ_ASSERT(mParsePromise.IsEmpty());
   RefPtr<StyleSheetParsePromise> p = mParsePromise.Ensure(__func__);
-  if (!aLoadData.ShouldDefer()) {
+  if (!aLoadData->get()->ShouldDefer()) {
     mParsePromise.SetTaskPriority(nsIRunnablePriority::PRIORITY_RENDER_BLOCKING,
                                   __func__);
   }
   SetURLExtraData();
-
+  MOZ_ASSERT(!IsAsyncParseOngoing());
+  {
+    MutexAutoLock lock(mMutex);
+    mState |= State::AsyncParseOngoing;
+  }
+  MOZ_ASSERT_IF(NS_IsMainThread(), !HasParsePromiseResolutionBlocked());
   // @import rules are disallowed due to this decision:
   // https://github.com/WICG/construct-stylesheets/issues/119#issuecomment-588352418
   // We may allow @import rules again in the future.
@@ -1191,26 +1192,26 @@ RefPtr<StyleSheetParsePromise> StyleSheet::ParseSheet(
   const bool shouldRecordCounters =
       aLoader.GetDocument() && aLoader.GetDocument()->GetStyleUseCounters() &&
       !urlData->ChromeRulesEnabled();
-  if (!AllowParallelParse(aLoader, urlData)) {
+
+  if (aLoadData->get()->mRecordErrors) {
+    MOZ_ASSERT(NS_IsMainThread());
     UniquePtr<StyleUseCounters> counters;
     if (shouldRecordCounters) {
       counters.reset(Servo_UseCounters_Create());
     }
-
     RefPtr<StyleStylesheetContents> contents =
         Servo_StyleSheet_FromUTF8Bytes(
-            &aLoader, this, &aLoadData, &aBytes, mParsingMode, urlData,
-            aLoadData.mCompatMode,
+            &aLoader, this, aLoadData->get(), &aBytes, mParsingMode, urlData,
+            aLoadData->get()->mCompatMode,
             /* reusable_sheets = */ nullptr, counters.get(), allowImportRules,
             StyleSanitizationKind::None,
             /* sanitized_output = */ nullptr)
             .Consume();
     FinishAsyncParse(contents.forget(), std::move(counters));
   } else {
-    auto holder = MakeRefPtr<css::SheetLoadDataHolder>(__func__, &aLoadData);
-    Servo_StyleSheet_FromUTF8BytesAsync(holder, urlData, &aBytes, mParsingMode,
-                                        aLoadData.mCompatMode,
-                                        shouldRecordCounters, allowImportRules);
+    Servo_StyleSheet_FromUTF8BytesAsync(
+        aLoadData, urlData, &aBytes, mParsingMode,
+        aLoadData->get()->mCompatMode, shouldRecordCounters, allowImportRules);
   }
 
   return p;
@@ -1219,12 +1220,13 @@ RefPtr<StyleSheetParsePromise> StyleSheet::ParseSheet(
 void StyleSheet::FinishAsyncParse(
     already_AddRefed<StyleStylesheetContents> aSheetContents,
     UniquePtr<StyleUseCounters> aUseCounters) {
+  mState &= ~State::AsyncParseOngoing;
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(!mParsePromise.IsEmpty());
   Inner().mContents = aSheetContents;
   Inner().mUseCounters = std::move(aUseCounters);
   FixUpRuleListAfterContentsChangeIfNeeded();
-  mParsePromise.Resolve(true, __func__);
+  MayBeResolveParsePromise();
 }
 
 void StyleSheet::ParseSheetSync(
@@ -1351,6 +1353,7 @@ already_AddRefed<StyleSheet> StyleSheet::Clone(
     dom::DocumentOrShadowRoot* aCloneDocumentOrShadowRoot) const {
   MOZ_ASSERT(!IsConstructed(),
              "Cannot create a non-constructed sheet from a constructed sheet");
+  MutexAutoLock lock(mMutex);
   RefPtr<StyleSheet> clone =
       new StyleSheet(*this, aCloneParent, aCloneDocumentOrShadowRoot,
                      /* aConstructorDocToUse */ nullptr);
