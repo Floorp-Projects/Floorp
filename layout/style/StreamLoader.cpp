e6269d2d2514f401a2e92be8d309cf481c3c00fe
Cristian Tuns <ctuns@mozilla.com>
2024-03-10 14:05:10 -0400

    Backed out changeset 19762bd6f793 (bug 1883391) for causing build bustages in StreamLoader.cpp CLOSED TREE

--- layout/style/StreamLoader.cpp
+++ layout/style/StreamLoader.cpp
@@ -116,13 +116,6 @@ StreamLoader::OnStopRequest(nsIRequest* aRequest, nsresult aStatus) {
   // Resolution of parse promise fires onLoadEvent and this should not happen
   // before main thread OnStopRequest is dispatched.
   if (NS_IsMainThread()) {
-    if (mOnDataFinishedTime) {
-      // collect telemetry for the delta between OnDataFinished and
-      // OnStopRequest
-      TimeDuration delta = (TimeStamp::Now() - mOnDataFinishedTime);
-      glean::networking::http_content_cssloader_ondatafinished_to_onstop_delay
-          .AccumulateRawDuration(delta);
-    }
     mSheetLoadData->mSheet->BlockOrUnblockParsePromise(false);
   }
 
@@ -237,9 +230,6 @@ void StreamLoader::HandleBOM() {
 NS_IMETHODIMP
 StreamLoader::OnDataFinished(nsresult aResult) {
   if (StaticPrefs::network_send_OnDataFinished_cssLoader()) {
-    MOZ_ASSERT(mOnDataFinishedTime.IsNull(),
-               "OnDataFinished should only be called once");
-    mOnDataFinishedTime = TimeStamp::Now();
     return OnStopRequest(mRequest, aResult);
   }
 
