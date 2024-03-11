31afa4866947a408db39b7a54319854acad3f9b6
mcheang <mcheang@mozilla.com>
2024-03-11 17:07:12 +0000

    Bug 1870685 - Ensure tests outside of search pass with the new search configuration enabled. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D202637

--- toolkit/components/telemetry/tests/unit/test_TelemetryEnvironment_search.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryEnvironment_search.js
@@ -7,6 +7,10 @@ const { TelemetryEnvironment } = ChromeUtils.importESModule(
 const { SearchTestUtils } = ChromeUtils.importESModule(
   "resource://testing-common/SearchTestUtils.sys.mjs"
 );
+
+const { SearchUtils } = ChromeUtils.importESModule(
+  "resource://gre/modules/SearchUtils.sys.mjs"
+);
 const { TelemetryEnvironmentTesting } = ChromeUtils.importESModule(
   "resource://testing-common/TelemetryEnvironmentTesting.sys.mjs"
 );
@@ -140,10 +144,13 @@ async function checkDefaultSearch(privateOn, reInitSearchService) {
   Assert.equal(data.settings.defaultSearchEngine, "telemetrySearchIdentifier");
   let expectedSearchEngineData = {
     name: "telemetrySearchIdentifier",
-    loadPath: "[addon]telemetrySearchIdentifier@search.mozilla.org",
+    loadPath: SearchUtils.newSearchConfigEnabled
+      ? "[app]telemetrySearchIdentifier@search.mozilla.org"
+      : "[addon]telemetrySearchIdentifier@search.mozilla.org",
     origin: "default",
-    submissionURL:
-      "https://ar.wikipedia.org/wiki/%D8%AE%D8%A7%D8%B5:%D8%A8%D8%AD%D8%AB?search=&sourceId=Mozilla-search",
+    submissionURL: SearchUtils.newSearchConfigEnabled
+      ? "https://ar.wikipedia.org/wiki/%D8%AE%D8%A7%D8%B5:%D8%A8%D8%AD%D8%AB?sourceId=Mozilla-search&search="
+      : "https://ar.wikipedia.org/wiki/%D8%AE%D8%A7%D8%B5:%D8%A8%D8%AD%D8%AB?search=&sourceId=Mozilla-search",
   };
   Assert.deepEqual(
     data.settings.defaultSearchEngineData,
