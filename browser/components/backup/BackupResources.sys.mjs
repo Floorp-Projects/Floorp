6cbcd5b68e829f13f4e7bb1836828ea0e063e406
Fred Chasen <fchasen@gmail.com>
2024-03-13 16:02:07 +0000

    Bug 1884995 - Add BackupResource abstract class. r=mconley,backup-reviewers
    Adds a `BackupResource` abstract class to be extended by more specific resource handlers and a `BackupResources` module which resources can be registered with.
    
    The BackupResource base includes helpers to get the size of files and directories.
    
    All registed resources will be provided to the `BackupService` constructor for it instantiate them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203795

---
+++ browser/components/backup/BackupResources.sys.mjs
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+/**
+ * Classes exported here are registered as a resource that can be
+ * backed up and restored in the BackupService.
+ *
+ * They must extend the BackupResource base class.
+ */
+export {};
