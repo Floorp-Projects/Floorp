386b33a0cce019d4b1eaf63203edfac5840eb64c
alwu <alwu@mozilla.com>
2024-03-13 17:58:04 +0000

    Bug 1884719 - part2 : only use media engine playback when hardware video decoding is allowed. r=media-playback-reviewers,jolin
    Differential Revision: https://phabricator.services.mozilla.com/D204293

--- dom/media/platforms/wmf/MFMediaEngineDecoderModule.h
+++ dom/media/platforms/wmf/MFMediaEngineDecoderModule.h
@@ -10,6 +10,8 @@
 
 namespace mozilla {
 
+// MFMediaEngineDecoderModule is used for the media engine playback, which only
+// supports hardware decoding.
 class MFMediaEngineDecoderModule final : public PlatformDecoderModule {
  public:
   static void Init();
