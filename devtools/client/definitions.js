0f7d9b617394b62163a8caa7506bdb88cac9b03c
artem manushenkov <artemmanusenkov@gmail.com>
2024-03-13 12:47:28 +0000

    Bug 1763961 [devtools] Make unchecking "Pick an element from the page" in "Available Toolbox Buttons" disable hotkey for opening element picker/inspector r=jdescottes,devtools-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D203948

--- devtools/client/definitions.js
+++ devtools/client/definitions.js
@@ -161,7 +161,11 @@ Tools.inspector = {
   // that trigger the element picker.
   preventRaisingOnKey: true,
   onkey(panel, toolbox) {
-    toolbox.nodePicker.togglePicker();
+    if (
+      Services.prefs.getBoolPref("devtools.command-button-pick.enabled", false)
+    ) {
+      toolbox.nodePicker.togglePicker();
+    }
   },
 
   isToolSupported(toolbox) {
