37b56985d0b14eba8a2f52d45fc26d53b58e1818
Jan Varga <jvarga@mozilla.com>
2024-03-14 07:04:30 +0000

    Bug 1858989 - Bring back owning thread assertions; r=dom-storage-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D203292

--- dom/quota/CachingDatabaseConnection.h
+++ dom/quota/CachingDatabaseConnection.h
@@ -67,7 +67,7 @@ class CachingDatabaseConnection {
   class LazyStatement;
 
   void AssertIsOnConnectionThread() const {
-#ifdef CACHING_DB_CONNECTION_CHECK_THREAD_OWNERSHIP
+#ifdef MOZ_THREAD_SAFETY_OWNERSHIP_CHECKS_SUPPORTED
     mOwningThread->AssertOwnership("CachingDatabaseConnection not thread-safe");
 #endif
   }
@@ -125,7 +125,7 @@ class CachingDatabaseConnection {
   void Close();
 
  private:
-#ifdef CACHING_DB_CONNECTION_CHECK_THREAD_OWNERSHIP
+#ifdef MOZ_THREAD_SAFETY_OWNERSHIP_CHECKS_SUPPORTED
   LazyInitializedOnce<const nsAutoOwningThread> mOwningThread;
 #endif
 
