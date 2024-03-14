bb4e93def49ba3f59ec8bddd68dac23d4d4d6665
Andrew Halberstadt <ahal@mozilla.com>
2024-03-14 15:00:57 +0000

    Bug 1884364 - Convert gecko_taskgraph/test/test_decision.py to pytest format, r=taskgraph-reviewers,gbrown
    I intially converted this because I was having problems getting 'responses' to
    work with the unittest style tests. But then later I ended up not using 'responses'
    at all.
    
    Oh well, I think this looks much better anyway.
    
    Differential Revision: https://phabricator.services.mozilla.com/D204126

--- taskcluster/gecko_taskgraph/test/test_decision.py
+++ taskcluster/gecko_taskgraph/test/test_decision.py
@@ -7,7 +7,6 @@ import json
 import os
 import shutil
 import tempfile
-import unittest
 from unittest.mock import patch
 
 import pytest
@@ -18,6 +17,7 @@ from gecko_taskgraph import decision
 from gecko_taskgraph.parameters import register_parameters
 
 FAKE_GRAPH_CONFIG = {"product-dir": "browser", "taskgraph": {}}
+TTC_FILE = os.path.join(os.getcwd(), "try_task_config.json")
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -25,150 +25,168 @@ def register():
     register_parameters()
 
 
-class TestDecision(unittest.TestCase):
-    def test_write_artifact_json(self):
-        data = [{"some": "data"}]
-        tmpdir = tempfile.mkdtemp()
-        try:
-            decision.ARTIFACTS_DIR = os.path.join(tmpdir, "artifacts")
-            decision.write_artifact("artifact.json", data)
-            with open(os.path.join(decision.ARTIFACTS_DIR, "artifact.json")) as f:
-                self.assertEqual(json.load(f), data)
-        finally:
-            if os.path.exists(tmpdir):
-                shutil.rmtree(tmpdir)
-            decision.ARTIFACTS_DIR = "artifacts"
-
-    def test_write_artifact_yml(self):
-        data = [{"some": "data"}]
-        tmpdir = tempfile.mkdtemp()
-        try:
-            decision.ARTIFACTS_DIR = os.path.join(tmpdir, "artifacts")
-            decision.write_artifact("artifact.yml", data)
-            self.assertEqual(load_yaml(decision.ARTIFACTS_DIR, "artifact.yml"), data)
-        finally:
-            if os.path.exists(tmpdir):
-                shutil.rmtree(tmpdir)
-            decision.ARTIFACTS_DIR = "artifacts"
+@pytest.fixture(scope="module")
+def options():
+    return {
+        "base_repository": "https://hg.mozilla.org/mozilla-unified",
+        "head_repository": "https://hg.mozilla.org/mozilla-central",
+        "head_rev": "abcd",
+        "head_ref": "ef01",
+        "head_tag": "",
+        "message": "",
+        "project": "mozilla-central",
+        "pushlog_id": "143",
+        "pushdate": 1503691511,
+        "owner": "nobody@mozilla.com",
+        "repository_type": "hg",
+        "tasks_for": "hg-push",
+        "level": "3",
+    }
 
 
-class TestGetDecisionParameters(unittest.TestCase):
-    ttc_file = os.path.join(os.getcwd(), "try_task_config.json")
+def test_write_artifact_json():
+    data = [{"some": "data"}]
+    tmpdir = tempfile.mkdtemp()
+    try:
+        decision.ARTIFACTS_DIR = os.path.join(tmpdir, "artifacts")
+        decision.write_artifact("artifact.json", data)
+        with open(os.path.join(decision.ARTIFACTS_DIR, "artifact.json")) as f:
+            assert json.load(f) == data
+    finally:
+        if os.path.exists(tmpdir):
+            shutil.rmtree(tmpdir)
+        decision.ARTIFACTS_DIR = "artifacts"
 
-    def setUp(self):
-        self.options = {
-            "base_repository": "https://hg.mozilla.org/mozilla-unified",
-            "head_repository": "https://hg.mozilla.org/mozilla-central",
-            "head_rev": "abcd",
-            "head_ref": "ef01",
-            "head_tag": "",
-            "message": "",
-            "project": "mozilla-central",
-            "pushlog_id": "143",
-            "pushdate": 1503691511,
-            "owner": "nobody@mozilla.com",
-            "repository_type": "hg",
-            "tasks_for": "hg-push",
-            "level": "3",
-        }
 
-    @patch("gecko_taskgraph.decision.get_hg_revision_branch")
-    @patch("gecko_taskgraph.decision._determine_more_accurate_base_rev")
-    def test_simple_options(
-        self, mock_determine_more_accurate_base_rev, mock_get_hg_revision_branch
-    ):
-        mock_get_hg_revision_branch.return_value = "default"
-        mock_determine_more_accurate_base_rev.return_value = "baserev"
-        with MockedOpen({self.ttc_file: None}):
-            params = decision.get_decision_parameters(FAKE_GRAPH_CONFIG, self.options)
-        self.assertEqual(params["pushlog_id"], "143")
-        self.assertEqual(params["build_date"], 1503691511)
-        self.assertEqual(params["hg_branch"], "default")
-        self.assertEqual(params["moz_build_date"], "20170825200511")
-        self.assertEqual(params["try_mode"], None)
-        self.assertEqual(params["try_options"], None)
-        self.assertEqual(params["try_task_config"], {})
+def test_write_artifact_yml():
+    data = [{"some": "data"}]
+    tmpdir = tempfile.mkdtemp()
+    try:
+        decision.ARTIFACTS_DIR = os.path.join(tmpdir, "artifacts")
+        decision.write_artifact("artifact.yml", data)
+        assert load_yaml(decision.ARTIFACTS_DIR, "artifact.yml") == data
+    finally:
+        if os.path.exists(tmpdir):
+            shutil.rmtree(tmpdir)
+        decision.ARTIFACTS_DIR = "artifacts"
 
-    @patch("gecko_taskgraph.decision.get_hg_revision_branch")
-    @patch("gecko_taskgraph.decision._determine_more_accurate_base_rev")
-    def test_no_email_owner(
-        self, mock_determine_more_accurate_base_rev, mock_get_hg_revision_branch
-    ):
-        mock_get_hg_revision_branch.return_value = "default"
-        mock_determine_more_accurate_base_rev.return_value = "baserev"
-        self.options["owner"] = "ffxbld"
-        with MockedOpen({self.ttc_file: None}):
-            params = decision.get_decision_parameters(FAKE_GRAPH_CONFIG, self.options)
-        self.assertEqual(params["owner"], "ffxbld@noreply.mozilla.org")
 
-    @patch("gecko_taskgraph.decision.get_hg_revision_branch")
-    @patch("gecko_taskgraph.decision.get_hg_commit_message")
-    @patch("gecko_taskgraph.decision._determine_more_accurate_base_rev")
-    def test_try_options(
-        self,
-        mock_determine_more_accurate_base_rev,
-        mock_get_hg_commit_message,
-        mock_get_hg_revision_branch,
-    ):
-        mock_get_hg_commit_message.return_value = "try: -b do -t all --artifact"
-        mock_get_hg_revision_branch.return_value = "default"
-        mock_determine_more_accurate_base_rev.return_value = "baserev"
-        self.options["project"] = "try"
-        with MockedOpen({self.ttc_file: None}):
-            params = decision.get_decision_parameters(FAKE_GRAPH_CONFIG, self.options)
-        self.assertEqual(params["try_mode"], "try_option_syntax")
-        self.assertEqual(params["try_options"]["build_types"], "do")
-        self.assertEqual(params["try_options"]["unittests"], "all")
-        self.assertEqual(
-            params["try_task_config"],
+@patch("gecko_taskgraph.decision.get_hg_revision_branch")
+@patch("gecko_taskgraph.decision.get_hg_commit_message")
+@patch("gecko_taskgraph.decision._determine_more_accurate_base_rev")
+@pytest.mark.parametrize(
+    "extra_options,commit_msg,ttc,expected",
+    (
+        pytest.param(
+            {},
+            None,
+            None,
             {
-                "gecko-profile": False,
-                "use-artifact-builds": True,
-                "env": {},
+                "pushlog_id": "143",
+                "build_date": 1503691511,
+                "hg_branch": "default",
+                "moz_build_date": "20170825200511",
+                "try_mode": None,
+                "try_options": None,
+                "try_task_config": {},
             },
-        )
+            id="simple_options",
+        ),
+        pytest.param(
+            {"owner": "ffxbld"},
+            None,
+            None,
+            {
+                "owner": "ffxbld@noreply.mozilla.org",
+            },
+            id="no_email_owner",
+        ),
+        pytest.param(
+            {"project": "try"},
+            "try: -b do -t all --artifact",
+            None,
+            {
+                "try_mode": "try_option_syntax",
+                "try_options": {
+                    "build_types": "do",
+                    "include_nightly": False,
+                    "interactive": False,
+                    "jobs": None,
+                    "no_retry": False,
+                    "notifications": None,
+                    "platforms": "all",
+                    "raptor": "none",
+                    "raptor_trigger_tests": 1,
+                    "tag": None,
+                    "talos": "all",
+                    "talos_trigger_tests": 1,
+                    "taskcluster_worker": False,
+                    "trigger_tests": 1,
+                    "unittests": "all",
+                },
+                "try_task_config": {
+                    "gecko-profile": False,
+                    "use-artifact-builds": True,
+                    "env": {},
+                },
+            },
+            id="try_options",
+        ),
+        pytest.param(
+            {
+                "project": "try",
+            },
+            "Fuzzy query=foo",
+            {"tasks": ["a", "b"]},
+            {
+                "try_mode": "try_task_config",
+                "try_options": None,
+                "try_task_config": {"tasks": ["a", "b"]},
+            },
+            id="try_task_config",
+        ),
+    ),
+)
+def test_get_decision_parameters(
+    mock_determine_more_accurate_base_rev,
+    mock_get_hg_commit_message,
+    mock_get_hg_revision_branch,
+    options,
+    extra_options,
+    commit_msg,
+    ttc,
+    expected,
+):
+    mock_get_hg_revision_branch.return_value = "default"
+    mock_get_hg_commit_message.return_value = commit_msg or "commit message"
+    mock_determine_more_accurate_base_rev.return_value = "baserev"
 
-    @patch("gecko_taskgraph.decision.get_hg_revision_branch")
-    @patch("gecko_taskgraph.decision.get_hg_commit_message")
-    @patch("gecko_taskgraph.decision._determine_more_accurate_base_rev")
-    def test_try_task_config(
-        self,
-        mock_get_hg_commit_message,
-        mock_get_hg_revision_branch,
-        mock_determine_more_accurate_base_rev,
-    ):
-        mock_get_hg_commit_message.return_value = "Fuzzy query=foo"
-        mock_get_hg_revision_branch.return_value = "default"
-        mock_determine_more_accurate_base_rev.return_value = "baserev"
-        ttc = {"tasks": ["a", "b"]}
-        self.options["project"] = "try"
-        with MockedOpen({self.ttc_file: json.dumps(ttc)}):
-            params = decision.get_decision_parameters(FAKE_GRAPH_CONFIG, self.options)
-            self.assertEqual(params["try_mode"], "try_task_config")
-            self.assertEqual(params["try_options"], None)
-            self.assertEqual(params["try_task_config"], ttc)
+    options.update(extra_options)
+    contents = None
+    if ttc:
+        contents = json.dumps(ttc)
+    with MockedOpen({TTC_FILE: contents}):
+        params = decision.get_decision_parameters(FAKE_GRAPH_CONFIG, options)
 
-    def test_try_syntax_from_message_empty(self):
-        self.assertEqual(decision.try_syntax_from_message(""), "")
+    for key in expected:
+        assert params[key] == expected[key], f"key {key} does not match!"
 
-    def test_try_syntax_from_message_no_try_syntax(self):
-        self.assertEqual(decision.try_syntax_from_message("abc | def"), "")
 
-    def test_try_syntax_from_message_initial_try_syntax(self):
-        self.assertEqual(
-            decision.try_syntax_from_message("try: -f -o -o"), "try: -f -o -o"
-        )
-
-    def test_try_syntax_from_message_initial_try_syntax_multiline(self):
-        self.assertEqual(
-            decision.try_syntax_from_message("try: -f -o -o\nabc\ndef"), "try: -f -o -o"
-        )
-
-    def test_try_syntax_from_message_embedded_try_syntax_multiline(self):
-        self.assertEqual(
-            decision.try_syntax_from_message("some stuff\ntry: -f -o -o\nabc\ndef"),
+@pytest.mark.parametrize(
+    "msg, expected",
+    (
+        pytest.param("", "", id="empty"),
+        pytest.param("abc | def", "", id="no_try_syntax"),
+        pytest.param("try: -f -o -o", "try: -f -o -o", id="initial_try_syntax"),
+        pytest.param(
+            "some stuff\ntry: -f -o -o\nabc\ndef",
             "try: -f -o -o",
-        )
+            id="embedded_try_syntax_multiline",
+        ),
+    ),
+)
+def test_try_syntax_from_message(msg, expected):
+    assert decision.try_syntax_from_message(msg) == expected
 
 
 if __name__ == "__main__":
