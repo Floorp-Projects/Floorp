2f9a1712073eaebb24e86ef14f18073094a7c271
Dan Baker <dbaker@mozilla.com>
2024-03-15 16:49:52 -0600

    Bug 1883116 - Vendor libwebrtc from 1d6bf3156b
    Upstream commit: https://webrtc.googlesource.com/src/+/1d6bf3156bbe6d729bdcaf767ed664e0085c79af
        Use propagated instead of global field trials in FecControllerDefault
    
        Bug: webrtc:10335
        Change-Id: Ia559ae2655b39e7093cfdb9ed669f3463ef90054
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/333842
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Reviewed-by: Philip Eliasson <philipel@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#41483}

--- third_party/libwebrtc/call/call.cc
+++ third_party/libwebrtc/call/call.cc
@@ -909,7 +909,7 @@ webrtc::VideoSendStream* Call::CreateVideoSendStream(
   std::unique_ptr<FecController> fec_controller =
       config_.fec_controller_factory
           ? config_.fec_controller_factory->CreateFecController(env_)
-          : std::make_unique<FecControllerDefault>(&env_.clock());
+          : std::make_unique<FecControllerDefault>(env_);
   return CreateVideoSendStream(std::move(config), std::move(encoder_config),
                                std::move(fec_controller));
 }
