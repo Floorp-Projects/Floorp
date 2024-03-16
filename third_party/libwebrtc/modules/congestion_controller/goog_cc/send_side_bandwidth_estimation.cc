f9b2071137d9a0d2ca77646de3abbe228f8c3f55
Dan Baker <dbaker@mozilla.com>
2024-03-15 19:47:06 -0600

    Bug 1883116 - Vendor libwebrtc from 3b500e60e8
    Upstream commit: https://webrtc.googlesource.com/src/+/3b500e60e857eb799123d784a3af790bb4761550
        Fixing a crash in SendSideBandwidthEstmation
    
        This CL addresses a crash we started seeing in M121 where a
        function is being called on loss_based_bandwidth_estimator_v2_
        without checking whether it is enabled (it's not) which leads
        to absl::optional<> throwing since config_ is not valid.
    
        Bug: chromium:1518852
        Change-Id: Iffef1051fe7988046e33a709ce281aebefd2bcd7
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/334103
        Commit-Queue: Joe Downing <joedow@google.com>
        Reviewed-by: Per Kjellander <perkj@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#41538}

--- third_party/libwebrtc/modules/congestion_controller/goog_cc/send_side_bandwidth_estimation.cc
+++ third_party/libwebrtc/modules/congestion_controller/goog_cc/send_side_bandwidth_estimation.cc
@@ -281,7 +281,8 @@ void SendSideBandwidthEstimation::OnRouteChange() {
   uma_update_state_ = kNoUpdate;
   uma_rtt_state_ = kNoUpdate;
   last_rtc_event_log_ = Timestamp::MinusInfinity();
-  if (loss_based_bandwidth_estimator_v2_->UseInStartPhase()) {
+  if (LossBasedBandwidthEstimatorV2Enabled() &&
+      loss_based_bandwidth_estimator_v2_->UseInStartPhase()) {
     loss_based_bandwidth_estimator_v2_.reset(
         new LossBasedBweV2(key_value_config_));
   }
