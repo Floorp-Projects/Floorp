7129ac7d3e6cd88891e22ea1064dbf5f894c5156
kpatenio <kpatenio@mozilla.com>
2024-03-18 18:31:10 +0000

    Bug 1883642 - Add disk size measurements for places.sqlite and favicons.sqlite. r=backup-reviewers,places-reviewers,mconley,mak
    Differential Revision: https://phabricator.services.mozilla.com/D204528

---
+++ browser/components/backup/tests/xpcshell/head.js
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const BYTES_IN_KB = 1000;
+
+/**
+ * Create a file of a given size in kilobytes.
+ *
+ * @param {string} path the path where the file will be created.
+ * @param {number} sizeInKB size file in Kilobytes.
+ * @returns {Promise<undefined>}
+ */
+async function createKilobyteSizedFile(path, sizeInKB) {
+  let bytes = new Uint8Array(sizeInKB * BYTES_IN_KB);
+  await IOUtils.write(path, bytes);
+}
