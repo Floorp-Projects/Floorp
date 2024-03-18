f39caff25018ddd0f82d525bedfe66185d7901ac
Sam Foster <sfoster@mozilla.com>
2024-03-18 21:08:12 +0000

    Bug 1877678 - Split out the some of the open tabs tests into their own file. r=fxview-reviewers,sclements
    Differential Revision: https://phabricator.services.mozilla.com/D202486

--- browser/components/firefoxview/tests/browser/browser_opentabs_cards.js
+++ browser/components/firefoxview/tests/browser/browser_opentabs_cards.js
@@ -15,23 +15,6 @@ add_setup(function () {
   gInitialTabURL = gBrowser.selectedBrowser.currentURI.spec;
 });
 
-async function navigateToOpenTabs(browser) {
-  const document = browser.contentDocument;
-  if (document.querySelector("named-deck").selectedViewName != "opentabs") {
-    await navigateToViewAndWait(browser.contentDocument, "opentabs");
-  }
-}
-
-function getOpenTabsComponent(browser) {
-  return browser.contentDocument.querySelector("named-deck > view-opentabs");
-}
-
-function getCards(browser) {
-  return getOpenTabsComponent(browser).shadowRoot.querySelectorAll(
-    "view-opentabs-card"
-  );
-}
-
 async function cleanup() {
   await SimpleTest.promiseFocus(window);
   await promiseAllButPrimaryWindowClosed();
@@ -55,11 +38,6 @@ async function cleanup() {
   );
 }
 
-async function getRowsForCard(card) {
-  await TestUtils.waitForCondition(() => card.tabList.rowEls.length);
-  return card.tabList.rowEls;
-}
-
 /**
  * Verify that there are the expected number of cards, and that each card has
  * the expected URLs in order.
@@ -70,10 +48,10 @@ async function getRowsForCard(card) {
  *   The expected URLs for each card.
  */
 async function checkTabLists(browser, expected) {
-  const cards = getCards(browser);
+  const cards = getOpenTabsCards(getOpenTabsComponent(browser));
   is(cards.length, expected.length, `There are ${expected.length} windows.`);
   for (let i = 0; i < cards.length; i++) {
-    const tabItems = await getRowsForCard(cards[i]);
+    const tabItems = await getTabRowsForCard(cards[i]);
     const actual = Array.from(tabItems).map(({ url }) => url);
     Assert.deepEqual(
       actual,
@@ -119,8 +97,8 @@ add_task(async function open_tab_same_window() {
       browser.contentDocument,
       "visibilitychange"
     );
-    const cards = getCards(browser);
-    const tabItems = await getRowsForCard(cards[0]);
+    const cards = getOpenTabsCards(openTabs);
+    const tabItems = await getTabRowsForCard(cards[0]);
     tabItems[0].mainEl.click();
     await promiseHidden;
   });
@@ -132,8 +110,8 @@ add_task(async function open_tab_same_window() {
 
   await openFirefoxViewTab(window).then(async viewTab => {
     const browser = viewTab.linkedBrowser;
-    const cards = getCards(browser);
-    let tabItems = await getRowsForCard(cards[0]);
+    const cards = getOpenTabsCards(getOpenTabsComponent(browser));
+    let tabItems = await getTabRowsForCard(cards[0]);
 
     let promiseHidden = BrowserTestUtils.waitForEvent(
       browser.contentDocument,
@@ -169,8 +147,8 @@ add_task(async function open_tab_same_window() {
     await tabChangeRaised;
 
     await checkTabLists(browser, [[gInitialTabURL]]);
-    const [card] = getCards(browser);
-    const [row] = await getRowsForCard(card);
+    const [card] = getOpenTabsCards(getOpenTabsComponent(browser));
+    const [row] = await getTabRowsForCard(card);
     ok(
       !row.shadowRoot.getElementById("fxview-tab-row-url").hidden,
       "The URL is displayed, since we have one window."
@@ -202,8 +180,8 @@ add_task(async function open_tab_new_window() {
       [gInitialTabURL, TEST_URL],
       [gInitialTabURL],
     ]);
-    const cards = getCards(browser);
-    const originalWinRows = await getRowsForCard(cards[1]);
+    const cards = getOpenTabsCards(openTabs);
+    const originalWinRows = await getTabRowsForCard(cards[1]);
     const [row] = originalWinRows;
     ok(
       row.shadowRoot.getElementById("fxview-tab-row-url").hidden,
@@ -233,9 +211,9 @@ add_task(async function open_tab_new_window() {
     await openTabs.openTabsTarget.readyWindowsPromise;
     await openTabs.updateComplete;
 
-    const cards = getCards(browser);
+    const cards = getOpenTabsCards(openTabs);
     is(cards.length, 2, "There are two windows.");
-    const newWinRows = await getRowsForCard(cards[1]);
+    const newWinRows = await getTabRowsForCard(cards[1]);
 
     info("Select a tab from the new window.");
     winFocused = BrowserTestUtils.waitForEvent(win, "focus", true);
@@ -262,7 +240,7 @@ add_task(async function open_tab_new_private_window() {
     await openTabs.openTabsTarget.readyWindowsPromise;
     await openTabs.updateComplete;
 
-    const cards = getCards(browser);
+    const cards = getOpenTabsCards(openTabs);
     is(cards.length, 1, "The private window is not displayed.");
   });
   await cleanup();
@@ -295,8 +273,8 @@ add_task(async function open_tab_new_window_sort_by_recency() {
     await BrowserTestUtils.switchTab(newWindow.gBrowser, tabs[0]);
     await SimpleTest.promiseFocus(window);
     await TestUtils.waitForCondition(async () => {
-      const [, secondCard] = getCards(linkedBrowser);
-      const tabItems = await getRowsForCard(secondCard);
+      const [, secondCard] = getOpenTabsCards(openTabs);
+      const tabItems = await getTabRowsForCard(secondCard);
       return tabItems[0].url === gInitialTabURL;
     });
     await checkTabLists(linkedBrowser, [
@@ -374,252 +352,5 @@ add_task(async function styling_for_multiple_windows() {
       "The container shows three columns when three windows are open."
     );
   });
-  await cleanup();
-});
-
-add_task(async function toggle_show_more_link() {
-  const tabEntry = url => ({
-    entries: [{ url, triggeringPrincipal_base64 }],
-  });
-  const NUMBER_OF_WINDOWS = 4;
-  const NUMBER_OF_TABS = 42;
-  const browserState = { windows: [] };
-  for (let windowIndex = 0; windowIndex < NUMBER_OF_WINDOWS; windowIndex++) {
-    const winData = { tabs: [] };
-    let tabCount = windowIndex == NUMBER_OF_WINDOWS - 1 ? NUMBER_OF_TABS : 1;
-    for (let i = 0; i < tabCount; i++) {
-      winData.tabs.push(tabEntry(`data:,Window${windowIndex}-Tab${i}`));
-    }
-    winData.selected = winData.tabs.length;
-    browserState.windows.push(winData);
-  }
-  // use Session restore to batch-open windows and tabs
-  await SessionStoreTestUtils.promiseBrowserState(browserState);
-  // restoring this state requires an update to the initial tab globals
-  // so cleanup expects the right thing
-  gInitialTab = gBrowser.selectedTab;
-  gInitialTabURL = gBrowser.selectedBrowser.currentURI.spec;
-
-  const windows = Array.from(Services.wm.getEnumerator("navigator:browser"));
-  is(windows.length, NUMBER_OF_WINDOWS, "There are four browser windows.");
-
-  const tab = (win = window) => {
-    info("Tab");
-    EventUtils.synthesizeKey("KEY_Tab", {}, win);
-  };
-
-  const enter = (win = window) => {
-    info("Enter");
-    EventUtils.synthesizeKey("KEY_Enter", {}, win);
-  };
-
-  let lastCard;
-
-  SimpleTest.promiseFocus(window);
-  await openFirefoxViewTab(window).then(async viewTab => {
-    const browser = viewTab.linkedBrowser;
-    await navigateToOpenTabs(browser);
-    const openTabs = getOpenTabsComponent(browser);
-    await openTabs.openTabsTarget.readyWindowsPromise;
-    await openTabs.updateComplete;
-
-    const cards = getCards(browser);
-    is(cards.length, NUMBER_OF_WINDOWS, "There are four windows.");
-    lastCard = cards[NUMBER_OF_WINDOWS - 1];
-  });
-
-  await openFirefoxViewTab(window).then(async viewTab => {
-    const browser = viewTab.linkedBrowser;
-    const openTabs = getOpenTabsComponent(browser);
-    await openTabs.openTabsTarget.readyWindowsPromise;
-    await openTabs.updateComplete;
-    Assert.less(
-      (await getRowsForCard(lastCard)).length,
-      NUMBER_OF_TABS,
-      "Not all tabs are shown yet."
-    );
-    info("Toggle the Show More link.");
-    lastCard.shadowRoot.querySelector("div[slot=footer]").click();
-    await BrowserTestUtils.waitForMutationCondition(
-      lastCard.shadowRoot,
-      { childList: true, subtree: true },
-      async () => (await getRowsForCard(lastCard)).length === NUMBER_OF_TABS
-    );
-
-    info("Toggle the Show Less link.");
-    lastCard.shadowRoot.querySelector("div[slot=footer]").click();
-    await BrowserTestUtils.waitForMutationCondition(
-      lastCard.shadowRoot,
-      { childList: true, subtree: true },
-      async () => (await getRowsForCard(lastCard)).length < NUMBER_OF_TABS
-    );
-
-    // Setting this pref allows the test to run as expected with a keyboard on MacOS
-    await SpecialPowers.pushPrefEnv({
-      set: [["accessibility.tabfocus", 7]],
-    });
-
-    info("Toggle the Show More link with keyboard.");
-    lastCard.shadowRoot.querySelector("card-container").summaryEl.focus();
-    // Tab to first item in the list
-    tab();
-    // Tab to the footer
-    tab();
-    enter();
-    await BrowserTestUtils.waitForMutationCondition(
-      lastCard.shadowRoot,
-      { childList: true, subtree: true },
-      async () => (await getRowsForCard(lastCard)).length === NUMBER_OF_TABS
-    );
-
-    info("Toggle the Show Less link with keyboard.");
-    lastCard.shadowRoot.querySelector("card-container").summaryEl.focus();
-    // Tab to first item in the list
-    tab();
-    // Tab to the footer
-    tab();
-    enter();
-    await BrowserTestUtils.waitForMutationCondition(
-      lastCard.shadowRoot,
-      { childList: true, subtree: true },
-      async () => (await getRowsForCard(lastCard)).length < NUMBER_OF_TABS
-    );
-
-    await SpecialPowers.popPrefEnv();
-  });
-  await cleanup();
-});
-
-add_task(async function search_open_tabs() {
-  // Open a new window and navigate to TEST_URL. Then, when we search for
-  // TEST_URL, it should show a search result in the new window's card.
-  const win = await BrowserTestUtils.openNewBrowserWindow();
-  await BrowserTestUtils.openNewForegroundTab(win.gBrowser, TEST_URL);
-
-  await SpecialPowers.pushPrefEnv({
-    set: [["browser.firefox-view.search.enabled", true]],
-  });
-  await openFirefoxViewTab(window).then(async viewTab => {
-    const browser = viewTab.linkedBrowser;
-    await navigateToOpenTabs(browser);
-    const openTabs = getOpenTabsComponent(browser);
-    await openTabs.openTabsTarget.readyWindowsPromise;
-    await openTabs.updateComplete;
-
-    const cards = getCards(browser);
-    is(cards.length, 2, "There are two windows.");
-    const winTabs = await getRowsForCard(cards[0]);
-    const newWinTabs = await getRowsForCard(cards[1]);
-
-    info("Input a search query.");
-    EventUtils.synthesizeMouseAtCenter(openTabs.searchTextbox, {}, content);
-    EventUtils.sendString(TEST_URL, content);
-    await TestUtils.waitForCondition(
-      () => openTabs.viewCards[0].tabList.rowEls.length === 0,
-      "There are no matching search results in the original window."
-    );
-    await TestUtils.waitForCondition(
-      () => openTabs.viewCards[1].tabList.rowEls.length === 1,
-      "There is one matching search result in the new window."
-    );
-
-    info("Clear the search query.");
-    EventUtils.synthesizeMouseAtCenter(
-      openTabs.searchTextbox.clearButton,
-      {},
-      content
-    );
-    await TestUtils.waitForCondition(
-      () => openTabs.viewCards[0].tabList.rowEls.length === winTabs.length,
-      "The original window's list is restored."
-    );
-    await TestUtils.waitForCondition(
-      () => openTabs.viewCards[1].tabList.rowEls.length === newWinTabs.length,
-      "The new window's list is restored."
-    );
-    openTabs.searchTextbox.blur();
-
-    info("Input a search query with keyboard.");
-    EventUtils.synthesizeKey("f", { accelKey: true }, content);
-    EventUtils.sendString(TEST_URL, content);
-    await TestUtils.waitForCondition(
-      () => openTabs.viewCards[0].tabList.rowEls.length === 0,
-      "There are no matching search results in the original window."
-    );
-    await TestUtils.waitForCondition(
-      () => openTabs.viewCards[1].tabList.rowEls.length === 1,
-      "There is one matching search result in the new window."
-    );
-
-    info("Clear the search query with keyboard.");
-    is(
-      openTabs.shadowRoot.activeElement,
-      openTabs.searchTextbox,
-      "Search input is focused"
-    );
-    EventUtils.synthesizeKey("KEY_Tab", {}, content);
-    ok(
-      openTabs.searchTextbox.clearButton.matches(":focus-visible"),
-      "Clear Search button is focused"
-    );
-    EventUtils.synthesizeKey("KEY_Enter", {}, content);
-    await TestUtils.waitForCondition(
-      () => openTabs.viewCards[0].tabList.rowEls.length === winTabs.length,
-      "The original window's list is restored."
-    );
-    await TestUtils.waitForCondition(
-      () => openTabs.viewCards[1].tabList.rowEls.length === newWinTabs.length,
-      "The new window's list is restored."
-    );
-  });
-
-  await SpecialPowers.popPrefEnv();
-  await cleanup();
-});
-
-add_task(async function search_open_tabs_recent_browsing() {
-  const NUMBER_OF_TABS = 6;
-  const win = await BrowserTestUtils.openNewBrowserWindow();
-  for (let i = 0; i < NUMBER_OF_TABS; i++) {
-    await BrowserTestUtils.openNewForegroundTab(win.gBrowser, TEST_URL);
-  }
-  await SpecialPowers.pushPrefEnv({
-    set: [["browser.firefox-view.search.enabled", true]],
-  });
-  await openFirefoxViewTab(window).then(async viewTab => {
-    const browser = viewTab.linkedBrowser;
-    await navigateToViewAndWait(browser.contentDocument, "recentbrowsing");
-    const recentBrowsing = browser.contentDocument.querySelector(
-      "view-recentbrowsing"
-    );
-
-    info("Input a search query.");
-    EventUtils.synthesizeMouseAtCenter(
-      recentBrowsing.searchTextbox,
-      {},
-      content
-    );
-    EventUtils.sendString(TEST_URL, content);
-    const slot = recentBrowsing.querySelector("[slot='opentabs']");
-    await TestUtils.waitForCondition(
-      () => slot.viewCards[0].tabList.rowEls.length === 5,
-      "Not all search results are shown yet."
-    );
-
-    info("Click the Show All link.");
-    const showAllLink = await TestUtils.waitForCondition(() => {
-      const elt = slot.viewCards[0].shadowRoot.querySelector(
-        "[data-l10n-id='firefoxview-show-all']"
-      );
-      EventUtils.synthesizeMouseAtCenter(elt, {}, content);
-      if (slot.viewCards[0].tabList.rowEls.length === NUMBER_OF_TABS) {
-        return elt;
-      }
-      return false;
-    }, "All search results are shown.");
-    is(showAllLink.role, "link", "The show all control is a link.");
-    ok(BrowserTestUtils.isHidden(showAllLink), "The show all link is hidden.");
-  });
-  await SpecialPowers.popPrefEnv();
   await cleanup();
 });
