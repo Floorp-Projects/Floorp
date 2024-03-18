19a2c7c3b4146cbddc54ddcbc0f7e9233c18f091
Mike Conley <mconley@mozilla.com>
2024-03-18 17:54:19 +0000

    Bug 1885366 - Add BackupResource to the backups source docs. r=backup-reviewers,kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D204824

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,7 @@ extensions = [
 # excluded from valid-jsdoc in the top-level .eslintrc.js.
 js_source_path = [
     "../browser/components/backup",
+    "../browser/components/backup/resources",
     "../browser/components/extensions",
     "../browser/components/migration",
     "../browser/components/migration/content",
