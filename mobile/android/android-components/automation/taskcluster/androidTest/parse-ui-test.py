7f6f937c2019ffa48f935339b4888752cb2dc9f5
Geoff Brown <gbrown@mozilla.com>
2024-03-18 18:40:51 +0000

    Bug 1882087 - Improve ui-test failure messages for treeherder r=aaronmt
    Recall that `test-lab.py` uses `parse-ui-test.py` and `parse-ui-test-fromfile.py` to parse test results, depending on the test type. `parse-ui-test-fromfile` can produce better failure messages, including the test class and test name.
    
    The strategy here is to first run `parse-ui-test-fromfile` (subject to test type, as before), noting whether it produced a treeherder-friendly failure message; then run `parse-ui-test` (for all tests, as before) passing a new argument to print any failures in the treeherder-friendly way, only if `parse-ui-test-fromfile` did not report failures. Most of the complexity is to avoid duplicate reporting, favouring the more granular failure messages from `parse-ui-test-fromfile`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D204684

--- mobile/android/android-components/automation/taskcluster/androidTest/parse-ui-test.py
+++ mobile/android/android-components/automation/taskcluster/androidTest/parse-ui-test.py
@@ -31,6 +31,12 @@ def parse_args(cmdln_args):
         "--exit-code", type=int, help="Exit code of flank.", required=True
     )
     parser.add_argument("--device-type", help="Type of device ", required=True)
+    parser.add_argument(
+        "--report-treeherder-failures",
+        help="Report failures in treeherder format.",
+        required=False,
+    )
+
     return parser.parse_args(args=cmdln_args)
 
 
@@ -62,7 +68,11 @@ def main():
                 **matrix_result
             )
         )
-        if matrix_result["outcome"] != "success":
+        if (
+            args.report_treeherder_failures
+            and matrix_result["outcome"] != "success"
+            and matrix_result["outcome"] != "flaky"
+        ):
             # write failures to test log in format known to treeherder logviewer
             sys.stdout.write(
                 f"TEST-UNEXPECTED-FAIL | {matrix_result['outcome']} | {matrix_result['webLink']} | {matrix_result['axes'][0]['details']}\n"
