a7f93031d87347991169a44bc6a597e9c90faa9f
Cosmin Sabou <csabou@mozilla.com>
2024-03-19 14:26:40 +0200

    Backed out changeset 3d46becb9ecd (bug 1879987) for mochitest failures on test_popupReflowPos. CLOSED TREE

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -3165,31 +3165,23 @@ void nsWindow::SetFocus(Raise aRaise, mozilla::dom::CallerType aCallerType) {
   LOG("  widget now has focus in SetFocus()");
 }
 
-void nsWindow::ResetScreenBounds() { mGdkWindowRootOrigin.reset(); }
-
 LayoutDeviceIntRect nsWindow::GetScreenBounds() {
   if (!mGdkWindow) {
     return mBounds;
   }
 
   const LayoutDeviceIntPoint origin = [&] {
-    GdkPoint origin;
-
-    if (mGdkWindowRootOrigin.isSome()) {
-      origin = mGdkWindowRootOrigin.value();
-    } else {
-      gdk_window_get_root_origin(mGdkWindow, &origin.x, &origin.y);
-      mGdkWindowRootOrigin = Some(origin);
-    }
+    gint x, y;
+    gdk_window_get_root_origin(mGdkWindow, &x, &y);
 
     // Workaround for https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/4820
     // Bug 1775017 Gtk < 3.24.35 returns scaled values for
     // override redirected window on X11.
     if (gtk_check_version(3, 24, 35) != nullptr && GdkIsX11Display() &&
         gdk_window_get_window_type(mGdkWindow) == GDK_WINDOW_TEMP) {
-      return LayoutDeviceIntPoint(origin.x, origin.y);
+      return LayoutDeviceIntPoint(x, y);
     }
-    return GdkPointToDevicePixels(origin);
+    return GdkPointToDevicePixels({x, y});
   }();
 
   // mBounds.Size() is the window bounds, not the window-manager frame
@@ -4055,8 +4047,6 @@ gboolean nsWindow::OnConfigureEvent(GtkWidget* aWidget,
   if (mPendingConfigures > 0) {
     mPendingConfigures--;
   }
-
-  ResetScreenBounds();
 
   // Don't fire configure event for scale changes, we handle that
   // OnScaleChanged event. Skip that for toplevel windows only.
