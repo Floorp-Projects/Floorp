07d6103f7d523ba7818239b9c1c6cc3eb52c439e
sunil mayya <smayya@mozilla.com>
2024-03-19 12:41:10 +0000

    Bug 1884208 -  Remove negative probes for measuring delta between OnDataFinished and OnStopRequest in HttpChannelChild and HTML5Parser. r=jesup,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D204057

--- parser/html/nsHtml5StreamParser.cpp
+++ parser/html/nsHtml5StreamParser.cpp
@@ -1421,16 +1421,10 @@ nsresult nsHtml5StreamParser::OnStopRequest(
   }
   if (!mOnStopRequestTime.IsNull() && !mOnDataFinishedTime.IsNull()) {
     TimeDuration delta = (mOnStopRequestTime - mOnDataFinishedTime);
-    if (delta.ToMilliseconds() < 0) {
-      // Because Telemetry can't handle negatives
-      delta = -delta;
-      glean::networking::
-          http_content_html5parser_ondatafinished_to_onstop_delay_negative
-              .AccumulateRawDuration(delta);
-    } else {
-      glean::networking::http_content_html5parser_ondatafinished_to_onstop_delay
-          .AccumulateRawDuration(delta);
-    }
+    MOZ_ASSERT((delta.ToMilliseconds() >= 0),
+               "OnDataFinished after OnStopRequest");
+    glean::networking::http_content_html5parser_ondatafinished_to_onstop_delay
+        .AccumulateRawDuration(delta);
   }
   return NS_OK;
 }
