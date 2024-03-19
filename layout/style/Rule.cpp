ad95c77de83abb28bea81bb7b9da082a3c499c63
David Shin <dshin@mozilla.com>
2024-03-19 13:36:48 +0000

    Bug 1882581: Implement `@scope` parsing. r=firefox-style-system-reviewers,saschanaz,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D203153

--- layout/style/Rule.cpp
+++ layout/style/Rule.cpp
@@ -100,13 +100,13 @@ void Rule::AssertParentRuleType() {
   // this->Type() here since it's virtual.
   if (mParentRule) {
     auto type = mParentRule->Type();
-    MOZ_ASSERT(type == StyleCssRuleType::Media ||
-               type == StyleCssRuleType::Style ||
-               type == StyleCssRuleType::Document ||
-               type == StyleCssRuleType::Supports ||
-               type == StyleCssRuleType::Keyframes ||
-               type == StyleCssRuleType::LayerBlock ||
-               type == StyleCssRuleType::Container);
+    MOZ_ASSERT(
+        type == StyleCssRuleType::Media || type == StyleCssRuleType::Style ||
+        type == StyleCssRuleType::Document ||
+        type == StyleCssRuleType::Supports ||
+        type == StyleCssRuleType::Keyframes ||
+        type == StyleCssRuleType::LayerBlock ||
+        type == StyleCssRuleType::Container || type == StyleCssRuleType::Scope);
   }
 }
 #endif
