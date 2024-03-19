9ccd8510b3dd8f49faae9137ae0babaf05dbbee8
Julien Cristau <jcristau@mozilla.com>
2024-03-19 17:00:32 +0000

    Bug 1885917 - Backed out changeset a13052a3e0b9 (bug 1839869), served its purpose. r=releng-reviewers,gbrown DONTBUILD
    This backs out "Bug 1839869 - push fenix/focus nightly to the internal track on
    play store.", and reverts to pushing to the default track instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D204934

--- taskcluster/android_taskgraph/worker_types.py
+++ taskcluster/android_taskgraph/worker_types.py
@@ -76,7 +76,6 @@ def build_scriptworker_beetmover_payload(config, task, task_def):
         Required("commit"): bool,
         Required("product"): str,
         Required("dep"): bool,
-        Optional("google-play-track"): Any(str, None),
     },
 )
 def build_push_apk_payload(config, task, task_def):
@@ -90,8 +89,6 @@ def build_push_apk_payload(config, task, task_def):
         "commit": worker["commit"],
         "upstreamArtifacts": worker["upstream-artifacts"],
     }
-    if worker.get("google-play-track"):
-        task_def["payload"]["google_play_track"] = worker["google-play-track"]
 
     scope_prefix = config.graph_config["scriptworker"]["scope-prefix"]
     task_def["scopes"].append(
