841ff2bf0b2ad3121c752cb06d41e49f78694d31
Alex Franchuk <afranchuk@mozilla.com>
2024-03-20 14:59:44 +0000

    Bug 1759175 pt3 - Crashreporter business logic r=gsvelto,fluent-reviewers,flod,eemeli,cmartin
    Differential Revision: https://phabricator.services.mozilla.com/D185942

---
+++ toolkit/crashreporter/client-rust/app/src/process.rs
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+//! Process utility functions.
+
+use crate::std::{ffi::OsStr, process::Command};
+
+/// Return a command configured to run in the background.
+///
+/// This means that no associated console will be opened, when applicable.
+pub fn background_command<S: AsRef<OsStr>>(program: S) -> Command {
+    #[allow(unused_mut)]
+    let mut cmd = Command::new(program);
+    #[cfg(windows)]
+    {
+        use std::os::windows::process::CommandExt;
+        use windows_sys::Win32::System::Threading::CREATE_NO_WINDOW;
+        cmd.creation_flags(CREATE_NO_WINDOW);
+    }
+    cmd
+}
---
+++ toolkit/crashreporter/client-rust/app/src/settings.rs
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+//! Persistent settings of the application.
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Settings {
+    /// Whether a crash report should be sent.
+    pub submit_report: bool,
+    /// Whether the URL that was open should be included in a sent report.
+    pub include_url: bool,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Settings {
+            submit_report: true,
+            include_url: false,
+        }
+    }
+}
+
+impl Settings {
+    /// Write the settings to the given writer.
+    pub fn to_writer<W: std::io::Write>(&self, writer: W) -> anyhow::Result<()> {
+        Ok(serde_json::to_writer_pretty(writer, self)?)
+    }
+
+    /// Read the settings from the given reader.
+    pub fn from_reader<R: std::io::Read>(reader: R) -> anyhow::Result<Self> {
+        Ok(serde_json::from_reader(reader)?)
+    }
+
+    #[cfg(test)]
+    pub fn to_string(&self) -> String {
+        serde_json::to_string_pretty(self).unwrap()
+    }
+}
