a2f3edcd3b02dec4435b19c73e3434ab74032650
kpatenio <kpatenio@mozilla.com>
2024-03-20 15:53:24 +0000

    Bug 1883736 - Add disk size measurements for all logins, keys, certificates and payment methods stores. r=backup-reviewers,mconley
    Differential Revision: https://phabricator.services.mozilla.com/D204948

--- browser/components/backup/BackupResources.sys.mjs
+++ browser/components/backup/BackupResources.sys.mjs
@@ -2,12 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { CredentialsAndSecurityBackupResource } from "resource:///modules/backup/CredentialsAndSecurityBackupResource.sys.mjs";
+import { PlacesBackupResource } from "resource:///modules/backup/PlacesBackupResource.sys.mjs";
+
 /**
  * Classes exported here are registered as a resource that can be
  * backed up and restored in the BackupService.
  *
  * They must extend the BackupResource base class.
  */
-import { PlacesBackupResource } from "resource:///modules/backup/PlacesBackupResource.sys.mjs";
 
-export { PlacesBackupResource };
+export { CredentialsAndSecurityBackupResource, PlacesBackupResource };
