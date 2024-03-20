f4b32a35b19f326130b26e0ba39e55dd4d96e65b
Noemi Erli <nerli@mozilla.com>
2024-03-21 01:16:11 +0200

    Backed out changeset 4816ddcf03ba (bug 1883739) for causing failures in test_measurements.js CLOSED TREE
    browser/components/backup/metrics.yaml HG: changed browser/components/backup/moz.build HG: changed browser/components/backup/tests/xpcshell/test_measurements.js HG: changed toolkit/components/telemetry/Scalars.yaml HG: removed
    browser/components/backup/resources/PreferencesBackupResource.sys.mjs

--- browser/components/backup/BackupResources.sys.mjs
+++ browser/components/backup/BackupResources.sys.mjs
@@ -2,18 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { CredentialsAndSecurityBackupResource } from "resource:///modules/backup/CredentialsAndSecurityBackupResource.sys.mjs";
+import { PlacesBackupResource } from "resource:///modules/backup/PlacesBackupResource.sys.mjs";
+
 /**
  * Classes exported here are registered as a resource that can be
  * backed up and restored in the BackupService.
  *
  * They must extend the BackupResource base class.
  */
-import { CredentialsAndSecurityBackupResource } from "resource:///modules/backup/CredentialsAndSecurityBackupResource.sys.mjs";
-import { PlacesBackupResource } from "resource:///modules/backup/PlacesBackupResource.sys.mjs";
-import { PreferencesBackupResource } from "resource:///modules/backup/PreferencesBackupResource.sys.mjs";
 
-export {
-  CredentialsAndSecurityBackupResource,
-  PlacesBackupResource,
-  PreferencesBackupResource,
-};
+export { CredentialsAndSecurityBackupResource, PlacesBackupResource };
