4cab34feb50c6f26fdc53cc54e5d9f559cecc032
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2024-03-20 06:28:39 +0000

    Bug 1877703 - Part 5: Call SetHadImportMap when the module script is created. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D204937

---
+++ dom/base/test/jsmodules/importmaps/module_importMap_with_nonexisting_module.mjs
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-unassigned-import, import/no-unresolved */
+// Bareword specifier should be mapped to ./good/module_0.mjs.
+import {} from "bare";
+import * as test from "nonexistingmodule";
