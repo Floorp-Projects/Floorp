1afaaf066589c29ad35b53288b51a892f37a1e8c
Alex Franchuk <afranchuk@mozilla.com>
2024-03-20 14:59:43 +0000

    Bug 1759175 pt2 - UI definition r=gsvelto,cmartin,flod
    This defines the UI and data binding abstraction layer that is used across all platforms.
    
    Differential Revision: https://phabricator.services.mozilla.com/D174917

--- toolkit/crashreporter/client-rust/app/src/main.rs
+++ toolkit/crashreporter/client-rust/app/src/main.rs
@@ -5,6 +5,25 @@
 // Use the WINDOWS windows subsystem. This prevents a console window from opening with the
 // application.
 #![cfg_attr(windows, windows_subsystem = "windows")]
+
+/// cc is short for Clone Capture, a shorthand way to clone a bunch of values before an expression
+/// (particularly useful for closures).
+///
+/// It is defined here to allow it to be used in all submodules (textual scope lookup).
+macro_rules! cc {
+    ( ($($c:ident),*) $e:expr ) => {
+        {
+            $(let $c = $c.clone();)*
+            $e
+        }
+    }
+}
+
+mod data;
+mod ui;
+
+use ui::*;
+
 fn main() {
-    todo!()
+    todo!();
 }
