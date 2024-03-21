853e463cdd4fa7af40be420c04dcf12b14aaae21
Brad Werth <bwerth@mozilla.com>
2024-03-21 18:03:25 +0000

    Bug 1654398: Make ts_paint measurement check firstPaint2 timings instead of firstPaint. r=perftest-reviewers,sparky
    As noted in Bug 1556568, firstPaint records when we complete the first
    render that the OS tells us to do (in nsViewManager::Refresh). This
    sometimes can happen much later than we would like, skewing results. It
    also is sensitive to whether or not the call to FlushRendering is sync
    or async, since it fires as soon as the call is complete. For these
    reasons, we'd like ts_paint to measure something more consistent, which
    this patch accomplishes by changing the measurement to use firstPaint2.
    
    firstPaint2 records in nsView::DidCompositeWindow, which measures that
    the pixels have been put on the screen, no matter who asked us to do it.
    This solves both problems and is the superior measure.
    
    Because this most always measure ts_paint at a later time than current
    code, this will get flagged as a performance regression. We need to
    instead treat it as a measurement recalibration.
    
    Differential Revision: https://phabricator.services.mozilla.com/D204825

--- testing/talos/talos/talos-powers/api.js
+++ testing/talos/talos/talos-powers/api.js
@@ -332,9 +332,9 @@ TalosPowersService.prototype = {
     let mm = message.target.messageManager;
     let startupInfo = Services.startup.getStartupInfo();
 
-    if (!startupInfo.firstPaint) {
+    if (!startupInfo.firstPaint2) {
       // It's possible that we were called early enough that
-      // the firstPaint measurement hasn't been set yet. In
+      // the firstPaint2 measurement hasn't been set yet. In
       // that case, we set up an observer for the next time
       // a window is painted and re-retrieve the startup info.
       let obs = function (subject, topic) {
