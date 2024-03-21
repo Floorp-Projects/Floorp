d332423c6e57776b238476b8c934095efa3a7db3
Calixte <cdenizet@mozilla.com>
2024-03-21 17:15:26 +0000

    Bug 1886796 - Enable pdfjs.enableHighlightFloatingButton in nightly and early beta r=marco
    Differential Revision: https://phabricator.services.mozilla.com/D205343

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -188,11 +188,13 @@ pref("pdfjs.enableXfa", true);
 // Enable adding an image in a pdf.
 pref("pdfjs.enableStampEditor", true);
 
-// Enable adding an image in a pdf.
+// Enable highlighting in a pdf.
 #if defined(EARLY_BETA_OR_EARLIER)
   pref("pdfjs.enableHighlightEditor", true);
+  pref("pdfjs.enableHighlightFloatingButton", true);
 #else
   pref("pdfjs.enableHighlightEditor", false);
+  pref("pdfjs.enableHighlightFloatingButton", false);
 #endif
 
 // Disable support for MathML
