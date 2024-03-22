02d401bec4891f1afa7fbe8615a7b4c75d83ef0c
Paul Adenot <paul@paul.cx>
2024-03-22 14:16:42 +0000

    Bug 1860492 - Stop using Theora files in WPT (Mozilla tests). r=jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D205093

--- testing/web-platform/mozilla/tests/service-workers/resources/fetch_video.py
+++ testing/web-platform/mozilla/tests/service-workers/resources/fetch_video.py
@@ -2,11 +2,11 @@ import os
 
 
 def main(request, response):
-    filename = os.path.join(request.doc_root, "media", "2x2-green.ogv")
+    filename = os.path.join(request.doc_root, "media", "2x2-green.webm")
     body = open(filename, "rb").read()
     length = len(body)
     headers = [
-        (b"Content-Type", b"video/ogg"),
+        (b"Content-Type", b"video/webm"),
         (b"Accept-Ranges", b"bytes"),
         (b"Content-Length", b"%d" % length),
         (b"Content-Range", b"bytes 0-%d/%d" % (length - 1, length)),
