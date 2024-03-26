99376fd94cca6b49a5fe00fe9d4e0883a99042be
Emilio Cobos Álvarez <emilio@crisal.io>
2024-03-26 09:17:06 +0000

    Bug 1870380 - Don't use handled hints for table captions. r=tnikkel,layout-reviewers
    Table captions have a similar issue as column spanners, where their
    parent might not be the in the subtree of the style frame of its
    ancestors. In particular, a repaint posted to a table that
    doesn't cause a repaint in the table wrapper might not cause a repaint
    of its captions.
    
    Handle table captions like we treat out of flows and spanners, and add
    more comments around this set-up.
    
    Differential Revision: https://phabricator.services.mozilla.com/D205504

--- layout/base/RestyleManager.h
+++ layout/base/RestyleManager.h
@@ -66,13 +66,11 @@ class ServoRestyleState {
   // our children too if we're out of flow since they aren't necessarily
   // parented in DOM order, and thus a change handled by a DOM ancestor doesn't
   // necessarily mean that it's handled for an ancestor frame.
-  enum class Type {
-    InFlow,
-    OutOfFlow,
-  };
+  enum class CanUseHandledHints : bool { No = false, Yes };
 
   ServoRestyleState(const nsIFrame& aOwner, ServoRestyleState& aParentState,
-                    nsChangeHint aHintForThisFrame, Type aType,
+                    nsChangeHint aHintForThisFrame,
+                    CanUseHandledHints aCanUseHandledHints,
                     bool aAssertWrapperRestyleLength = true)
       : mStyleSet(aParentState.mStyleSet),
         mChangeList(aParentState.mChangeList),
@@ -81,7 +79,7 @@ class ServoRestyleState {
             aParentState.mPendingScrollAnchorSuppressions),
         mPendingWrapperRestyleOffset(
             aParentState.mPendingWrapperRestyles.Length()),
-        mChangesHandled(aType == Type::InFlow
+        mChangesHandled(bool(aCanUseHandledHints)
                             ? aParentState.mChangesHandled | aHintForThisFrame
                             : aHintForThisFrame)
 #ifdef DEBUG
@@ -90,7 +88,7 @@ class ServoRestyleState {
         mAssertWrapperRestyleLength(aAssertWrapperRestyleLength)
 #endif
   {
-    if (aType == Type::InFlow) {
+    if (bool(aCanUseHandledHints)) {
       AssertOwner(aParentState);
     }
   }
