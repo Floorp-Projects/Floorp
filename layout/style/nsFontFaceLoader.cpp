7ec818defc429a0d4fd4d300864671f86511b1e5
Jonathan Kew <jkew@mozilla.com>
2024-03-26 09:44:38 +0000

    Bug 1808813 - Ensure font loaders are cancelled properly if we refresh the font list. r=gfx-reviewers,lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D205376

--- layout/style/nsFontFaceLoader.cpp
+++ layout/style/nsFontFaceLoader.cpp
@@ -360,6 +360,7 @@ void nsFontFaceLoader::Cancel() {
   if (doc) {
     doc->UnblockOnload(false);
   }
+  mFontFaceSet->RemoveLoader(this);
   mFontFaceSet = nullptr;
   if (mLoadTimer) {
     mLoadTimer->Cancel();
