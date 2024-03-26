e94987e2b45ff9d0c5b4d0c8b6daa4474455c967
abhishekmadan30 <148475204+abhishekmadan30@users.noreply.github.com>
2024-03-26 14:42:07 +0000

    Bug 1884941 - Add an option to URLParams::Parse() for if it should decode the parameters or not. r=anti-tracking-reviewers,necko-reviewers,valentin,timhuang.
    Differential Revision: https://phabricator.services.mozilla.com/D204914

--- caps/OriginAttributes.cpp
+++ caps/OriginAttributes.cpp
@@ -324,7 +324,7 @@ bool OriginAttributes::PopulateFromSuffix(const nsACString& aStr) {
   MOZ_RELEASE_ASSERT(mPartitionKey.IsEmpty());
 
   return URLParams::Parse(
-      Substring(aStr, 1, aStr.Length() - 1),
+      Substring(aStr, 1, aStr.Length() - 1), true,
       [this](const nsAString& aName, const nsAString& aValue) {
         if (aName.EqualsLiteral("inBrowser")) {
           if (!aValue.EqualsLiteral("1")) {
