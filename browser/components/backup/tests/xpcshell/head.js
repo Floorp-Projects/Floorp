a82e08ef06f62eff058b81c9cf07804ab9122aa7
Fred Chasen <fchasen@gmail.com>
2024-03-26 20:15:48 +0000

    Bug 1883740 - Add disk size measurements for cookies, form history and sessions. data-review=jhirsch r=backup-reviewers,mconley
    - Adds `CookiesBackupResource`, `FormHistoryBackupResource` and `SessionsStoreBackupResource` classes with measurement methods for those resources.
    - Exports `BYTES_IN_KB` and new `bytesToFuzzyKilobytes` methods from `BackupResource`
    - Fix filename for `BackupResource` tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D205254

--- browser/components/backup/tests/xpcshell/head.js
+++ browser/components/backup/tests/xpcshell/head.js
@@ -17,3 +17,25 @@ async function createKilobyteSizedFile(path, sizeInKB) {
   let bytes = new Uint8Array(sizeInKB * BYTES_IN_KB);
   await IOUtils.write(path, bytes);
 }
+
+/**
+ * Remove a file if it exists and is unlocked.
+ *
+ * @param {string} path path to the file to remove.
+ */
+async function maybeRemoveFile(path) {
+  if (await IOUtils.exists(path)) {
+    return;
+  }
+
+  try {
+    await IOUtils.remove(path);
+  } catch (error) {
+    // Sometimes remove() throws when the file is not unlocked soon
+    // enough.
+    if (error.name == "NS_ERROR_FILE_IS_LOCKED") {
+      return;
+    }
+    throw error;
+  }
+}
