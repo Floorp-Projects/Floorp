f3d83af18190345e1d4afaf8f7ea6fa08747b362
Norisz Fay <nfay@mozilla.com>
2024-03-27 21:08:57 +0200

    Backed out changeset 66d93ead16c4 (bug 1886856) for causing node newtab failure in bundles CLOSED TREE

--- browser/components/messagepreview/messagepreview.js
+++ browser/components/messagepreview/messagepreview.js
@@ -6,25 +6,13 @@
 
 "use strict";
 
-// decode a 16-bit string in which only one byte of each
-// 16-bit unit is occupied, to UTF-8. This is necessary to
-// comply with `btoa` API constraints.
-function fromBinary(encoded) {
-  const binary = atob(decodeURIComponent(encoded));
-  const bytes = new Uint8Array(binary.length);
-  for (let i = 0; i < bytes.length; i++) {
-    bytes[i] = binary.charCodeAt(i);
-  }
-  return String.fromCharCode(...new Uint16Array(bytes.buffer));
-}
-
 function decodeMessageFromUrl() {
   const url = new URL(document.location.href);
 
   if (url.searchParams.has("json")) {
     const encodedMessage = url.searchParams.get("json");
 
-    return fromBinary(encodedMessage);
+    return atob(encodedMessage);
   }
   return null;
 }
