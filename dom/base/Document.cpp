a6f6d6fc1d31ee950ce0782720ea1af58a344d5e
Tim Huang <tihuang@mozilla.com>
2024-03-27 15:24:46 +0000

    Bug 1873682 - Fallback to use partitionKey as the top URI when getting the fingerprinting override if the top-level window has been navigated away. r=tjr,anti-tracking-reviewers,cookie-reviewers,pbz
    This patch introduces a mTopLevelWindowContextId field to
    cookieJarSettings. The field caches the top-level window context id. We
    use this field to check whether the top-level window is navigated away.
    If so, we fallback to use partitionKey as the top URI when getting the
    fingerprinting override because the top-level URI could be different in
    this case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D203889

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -18632,12 +18632,24 @@ nsICookieJarSettings* Document::CookieJarSettings() {
         net::CookieJarSettings::Cast(mCookieJarSettings)
             ->SetFingerprintingRandomizationKey(randomKey);
       }
+
+      // Inerit the top level windowContext id from the parent.
+      net::CookieJarSettings::Cast(mCookieJarSettings)
+          ->SetTopLevelWindowContextId(
+              net::CookieJarSettings::Cast(inProcessParent->CookieJarSettings())
+                  ->GetTopLevelWindowContextId());
     } else {
       mCookieJarSettings = net::CookieJarSettings::Create(NodePrincipal());
+
+      if (IsTopLevelContentDocument()) {
+        net::CookieJarSettings::Cast(mCookieJarSettings)
+            ->SetTopLevelWindowContextId(InnerWindowID());
+      }
     }
 
     if (auto* wgc = GetWindowGlobalChild()) {
       net::CookieJarSettingsArgs csArgs;
+
       net::CookieJarSettings::Cast(mCookieJarSettings)->Serialize(csArgs);
       // Update cookie settings in the parent process
       if (!wgc->SendUpdateCookieJarSettings(csArgs)) {
