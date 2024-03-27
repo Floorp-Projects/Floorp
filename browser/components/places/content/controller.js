121d55f44846ff692f5fa2189827c61736e20c19
Marco Bonardo <mbonardo@mozilla.com>
2024-03-27 08:54:46 +0000

    Bug 1887736 - Fix bookmark removal signature to use the proper guid properties. r=daisuke,places-reviewers
    nsINavHistoryResultNode.guid doesn't exist, so this always falls back to uris;
    unfortunately separators don't have a uri.
    
    Differential Revision: https://phabricator.services.mozilla.com/D205716

--- browser/components/places/content/controller.js
+++ browser/components/places/content/controller.js
@@ -379,7 +379,9 @@ PlacesController.prototype = {
   _isRepeatedRemoveOperation() {
     let lastRemoveOperationFingerprint = this._lastRemoveOperationFingerprint;
     this._lastRemoveOperationFingerprint = PlacesUtils.sha256(
-      this._view.selectedNodes.map(n => n.guid ?? n.uri).join()
+      this._view.selectedNodes
+        .map(n => n.bookmarkGuid ?? n.pageGuid ?? n.uri)
+        .join()
     );
     return (
       lastRemoveOperationFingerprint == this._lastRemoveOperationFingerprint
