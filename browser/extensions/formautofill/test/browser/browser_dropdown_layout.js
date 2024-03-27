9afed23adf7c36f15ae6bb3b4eebd3ae095f1e97
Cristina Horotan <chorotan@mozilla.com>
2024-03-28 01:47:01 +0200

    Backed out 2 changesets (bug 1883549) for causing mochitest failures at test_basic_creditcard_autocomplete_form.html
    Backed out changeset 068452b591ba (bug 1883549)
    Backed out changeset 985c0e227dd9 (bug 1883549)

--- browser/extensions/formautofill/test/browser/browser_dropdown_layout.js
+++ browser/extensions/formautofill/test/browser/browser_dropdown_layout.js
@@ -7,6 +7,22 @@ add_task(async function setup_storage() {
   await setStorage(TEST_ADDRESS_1, TEST_ADDRESS_2, TEST_ADDRESS_3);
 });
 
+async function reopenPopupWithResizedInput(browser, selector, newSize) {
+  await closePopup(browser);
+  /* eslint no-shadow: ["error", { "allow": ["selector", "newSize"] }] */
+  await SpecialPowers.spawn(
+    browser,
+    [{ selector, newSize }],
+    async function ({ selector, newSize }) {
+      const input = content.document.querySelector(selector);
+
+      input.style.boxSizing = "border-box";
+      input.style.width = newSize + "px";
+    }
+  );
+  await openPopupOn(browser, selector);
+}
+
 add_task(async function test_address_dropdown() {
   await BrowserTestUtils.withNewTab(
     { gBrowser, url: URL },
@@ -16,6 +32,20 @@ add_task(async function test_address_dropdown() {
       const firstItem = getDisplayedPopupItems(browser)[0];
 
       is(firstItem.getAttribute("ac-image"), "", "Should not show icon");
+
+      // The breakpoint of two-lines layout is 150px
+      await reopenPopupWithResizedInput(browser, focusInput, 140);
+      is(
+        firstItem._itemBox.getAttribute("size"),
+        "small",
+        "Show two-lines layout"
+      );
+      await reopenPopupWithResizedInput(browser, focusInput, 160);
+      is(
+        firstItem._itemBox.hasAttribute("size"),
+        false,
+        "Show one-line layout"
+      );
 
       await closePopup(browser);
     }
