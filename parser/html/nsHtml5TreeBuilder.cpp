d47cb52e633d44a58ee1dfc1bee5b8a002d50fd2
Henri Sivonen <hsivonen@hsivonen.fi>
2024-03-28 00:32:21 +0000

    Bug 1807017 - Consider to reuse the memory when nsHtml5Tokenizer::end() is called, r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D201982

--- parser/html/nsHtml5TreeBuilder.cpp
+++ parser/html/nsHtml5TreeBuilder.cpp
@@ -100,6 +100,18 @@ static const char* const QUIRKY_PUBLIC_IDS_DATA[] = {
     "-//webtechs//dtd mozilla html//"};
 staticJArray<const char*, int32_t> nsHtml5TreeBuilder::QUIRKY_PUBLIC_IDS = {
     QUIRKY_PUBLIC_IDS_DATA, MOZ_ARRAY_LENGTH(QUIRKY_PUBLIC_IDS_DATA)};
+void nsHtml5TreeBuilder::setKeepBuffer(bool keepBuffer) {
+  this->keepBuffer = keepBuffer;
+}
+
+bool nsHtml5TreeBuilder::dropBufferIfLongerThan(int32_t length) {
+  if (charBuffer.length > length) {
+    charBuffer = nullptr;
+    return true;
+  }
+  return false;
+}
+
 void nsHtml5TreeBuilder::startTokenization(nsHtml5Tokenizer* self) {
   tokenizer = self;
   stackNodes = jArray<nsHtml5StackNode*, int32_t>::newJArray(64);
@@ -118,7 +130,9 @@ void nsHtml5TreeBuilder::startTokenization(nsHtml5Tokenizer* self) {
   headPointer = nullptr;
   start(fragment);
   charBufferLen = 0;
-  charBuffer = nullptr;
+  if (!keepBuffer) {
+    charBuffer = nullptr;
+  }
   framesetOk = true;
   if (fragment) {
     nsIContentHandle* elt;
@@ -652,7 +666,9 @@ void nsHtml5TreeBuilder::endTokenization() {
     stackNodesIdx = 0;
     stackNodes = nullptr;
   }
-  charBuffer = nullptr;
+  if (!keepBuffer) {
+    charBuffer = nullptr;
+  }
   end();
 }
 
