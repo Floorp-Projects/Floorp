c978329289b6ec8e6997fcd96279f06e85184d48
Sandor Molnar <smolnar@mozilla.com>
2024-03-29 15:49:14 +0200

    Backed out 5 changesets (bug 1874741) for causing assertion failures @ toolkit/components/cookiebanners/nsCookieBannerTelemetryService.cpp CLOSED TREE
    Backed out changeset adfe9bc39245 (bug 1874741)
    Backed out changeset 6b18dcced252 (bug 1874741)
    Backed out changeset 7976b0cb00d6 (bug 1874741)
    Backed out changeset 1aa93711fe2f (bug 1874741)
    Backed out changeset 77de059a4857 (bug 1874741)

--- toolkit/components/protobuf/regenerate_cpp_files.sh
+++ toolkit/components/protobuf/regenerate_cpp_files.sh
@@ -25,7 +25,6 @@ cd ../../.. # Top level.
 regenerate devtools/shared/heapsnapshot/ CoreDump.proto
 regenerate toolkit/components/reputationservice/chromium/chrome/common/safe_browsing/ csd.proto
 regenerate toolkit/components/url-classifier/chromium/ safebrowsing.proto
-regenerate toolkit/components/cookiebanners/ cookieBanner.proto
 command cp third_party/content_analysis_sdk/proto/content_analysis/sdk/analysis.proto toolkit/components/contentanalysis/content_analysis/sdk/analysis.proto
 regenerate toolkit/components/contentanalysis/content_analysis/sdk/ analysis.proto
 command cp third_party/rust/viaduct/src/fetch_msg_types.proto toolkit/components/viaduct/fetch_msg_types.proto
