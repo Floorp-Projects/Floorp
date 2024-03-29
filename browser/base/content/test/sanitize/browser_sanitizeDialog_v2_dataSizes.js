880ddc0c8f1f65cddcfe948a4cbfcd08af13be17
Harshit Sohaney <hsohaney@mozilla.com>
2024-03-29 12:22:36 +0000

    Bug 1881327 - (part 2) Testing for clear history dialog resizing. r=pbz
    Differential Revision: https://phabricator.services.mozilla.com/D203674

--- browser/base/content/test/sanitize/browser_sanitizeDialog_v2_dataSizes.js
+++ browser/base/content/test/sanitize/browser_sanitizeDialog_v2_dataSizes.js
@@ -10,6 +10,14 @@ ChromeUtils.defineESModuleGetters(this, {
   Sanitizer: "resource:///modules/Sanitizer.sys.mjs",
 });
 
+const LARGE_USAGE_NUM = 100000000000000000000000000000000000000000000000000;
+
+function isIframeOverflowing(win) {
+  return (
+    win.scrollWidth > win.clientWidth || win.scrollHeight > win.clientHeight
+  );
+}
+
 add_setup(async function () {
   await blankSlate();
   registerCleanupFunction(async function () {
@@ -304,6 +312,61 @@ add_task(async function testClearingBeforeDataSizesLoad() {
     !(await SiteDataTestUtils.hasIndexedDB("https://example.com")),
     "We don't have indexedDB data for example.com"
   );
+
+  // Restore the sandbox after the test is complete
+  sandbox.restore();
+});
+
+// tests the dialog resizing upon wrapping of text
+// so that the clear buttons do not get cut out of the dialog.
+add_task(async function testPossibleWrappingOfDialog() {
+  await blankSlate();
+
+  let dh = new ClearHistoryDialogHelper({
+    checkingDataSizes: true,
+  });
+  // Create a sandbox for isolated stubbing within the test
+  let sandbox = sinon.createSandbox();
+  sandbox
+    .stub(SiteDataManager, "getQuotaUsageForTimeRanges")
+    .callsFake(async () => {
+      info("stubbed getQuotaUsageForTimeRanges called");
+
+      return {
+        TIMESPAN_HOUR: 0,
+        TIMESPAN_2HOURS: 0,
+        TIMESPAN_4HOURS: LARGE_USAGE_NUM,
+        TIMESPAN_TODAY: 0,
+        TIMESPAN_EVERYTHING: 0,
+      };
+    });
+
+  dh.onload = async function () {
+    let windowObj =
+      window.browsingContext.topChromeWindow.gDialogBox._dialog._frame
+        .contentWindow;
+    let promise = new Promise(resolve => {
+      windowObj.addEventListener("resize", resolve);
+    });
+    this.selectDuration(Sanitizer.TIMESPAN_4HOURS);
+
+    await promise;
+    ok(
+      !isIframeOverflowing(windowObj.document.getElementById("SanitizeDialog")),
+      "There should be no overflow on wrapping in the dialog"
+    );
+
+    this.selectDuration(Sanitizer.TIMESPAN_2HOURS);
+    await promise;
+    ok(
+      !isIframeOverflowing(windowObj.document.getElementById("SanitizeDialog")),
+      "There should be no overflow on wrapping in the dialog"
+    );
+
+    this.cancelDialog();
+  };
+  dh.open();
+  await dh.promiseClosed;
 
   // Restore the sandbox after the test is complete
   sandbox.restore();
