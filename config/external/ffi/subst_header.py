50fc26096f3e07ef2870845e2152915f5c74fd52
Andrew McCreight <continuation@gmail.com>
2024-04-01 18:07:23 +0000

    Bug 1888842 - Fix typos in MPL source code notices. r=sylvestre DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D206241

--- config/external/ffi/subst_header.py
+++ config/external/ffi/subst_header.py
@@ -1,6 +1,6 @@
 # -*- Mode: python; indent-tabs-mode: nil; tab-width: 40 -*-
 # vim: set filetype=python:
-# This Souce Code Form is subject to the terms of the Mozilla Public
+# This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distibuted with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
