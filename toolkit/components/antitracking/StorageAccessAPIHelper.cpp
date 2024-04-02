f6fa95ad96f009c2c2293c3e70214f081e2ec3be
Benjamin VanderSloot <bvandersloot@mozilla.com>
2024-04-02 12:50:01 +0000

    Bug 1876575, part 1 - Make AntiTracking::IsThirdParty* aware of ABA case - r=anti-tracking-reviewers,cookie-reviewers,timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D203180

--- toolkit/components/antitracking/StorageAccessAPIHelper.cpp
+++ toolkit/components/antitracking/StorageAccessAPIHelper.cpp
@@ -475,7 +475,6 @@ StorageAccessAPIHelper::CompleteAllowAccessForOnParentProcess(
       [aParentContext, aTopLevelWindowId, trackingOrigin, trackingPrincipal,
        aCookieBehavior,
        aReason](int aAllowMode) -> RefPtr<StorageAccessPermissionGrantPromise> {
-    MOZ_ASSERT(!aParentContext->IsInProcess());
     // We don't have the window, send an IPC to the content process that
     // owns the parent window. But there is a special case, for window.open,
     // we'll return to the content process we need to inform when this
@@ -1060,12 +1059,7 @@ StorageAccessAPIHelper::CheckSameSiteCallingContextDecidesStorageAccessAPI(
     }
   }
 
-  nsIChannel* chan = aDocument->GetChannel();
-  if (!chan) {
-    return Some(false);
-  }
-  nsCOMPtr<nsILoadInfo> loadInfo = chan->LoadInfo();
-  if (loadInfo->GetIsThirdPartyContextToTopWindow()) {
+  if (AntiTrackingUtils::IsThirdPartyDocument(aDocument)) {
     return Some(false);
   }
 
