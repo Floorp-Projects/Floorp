752b76a10342fc5a9ecdf2200a2a23d93856a122
Iulian Moraru <imoraru@mozilla.com>
2024-04-03 03:56:57 +0300

    Backed out changeset dec3e6cf26ed (bug 1888941) for causing wr failures on text-transform-full-size-kana-007.html.

--- layout/generic/nsTextRunTransformations.h
+++ layout/generic/nsTextRunTransformations.h
@@ -202,7 +202,6 @@ class nsTransformedTextRun final : public gfxTextRun {
         mOwnsFactory(aOwnsFactory),
         mNeedsRebuild(true) {
     mCharacterGlyphs = reinterpret_cast<CompressedGlyph*>(this + 1);
-    SetupClusterBoundaries(0, aString, aLength);
   }
 };
 
