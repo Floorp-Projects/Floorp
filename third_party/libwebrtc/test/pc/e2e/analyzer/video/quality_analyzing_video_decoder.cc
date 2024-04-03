33515e1ca99182a93cd60b7782e675e92833dbbd
Andreas Pehrson <apehrson@mozilla.com>
2024-04-03 22:47:42 +0200

    Bug 1888181 - Vendor libwebrtc from 62cee88e4b
    Upstream commit: https://webrtc.googlesource.com/src/+/62cee88e4b02527385ef89397601fb9649f3bcd5
        Propagate Environment through QualityAnalyzingVideoDecoderFactory
    
        Bug: webrtc:15791
        Change-Id: I9eddf7bf9fb66ee70495e9bc3c810126e2015287
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/336800
        Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
        Reviewed-by: Jeremy Leconte <jleconte@webrtc.org>
        Auto-Submit: Danil Chapovalov <danilchap@webrtc.org>
        Commit-Queue: Jeremy Leconte <jleconte@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#41630}

--- third_party/libwebrtc/test/pc/e2e/analyzer/video/quality_analyzing_video_decoder.cc
+++ third_party/libwebrtc/test/pc/e2e/analyzer/video/quality_analyzing_video_decoder.cc
@@ -259,10 +259,10 @@ QualityAnalyzingVideoDecoderFactory::GetSupportedFormats() const {
   return delegate_->GetSupportedFormats();
 }
 
-std::unique_ptr<VideoDecoder>
-QualityAnalyzingVideoDecoderFactory::CreateVideoDecoder(
+std::unique_ptr<VideoDecoder> QualityAnalyzingVideoDecoderFactory::Create(
+    const Environment& env,
     const SdpVideoFormat& format) {
-  std::unique_ptr<VideoDecoder> decoder = delegate_->CreateVideoDecoder(format);
+  std::unique_ptr<VideoDecoder> decoder = delegate_->Create(env, format);
   return std::make_unique<QualityAnalyzingVideoDecoder>(
       peer_name_, std::move(decoder), extractor_, analyzer_);
 }
