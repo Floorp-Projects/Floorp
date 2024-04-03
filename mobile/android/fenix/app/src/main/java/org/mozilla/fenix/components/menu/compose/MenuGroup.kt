1d4fb5ea408c908738a9a432fea6ecdb493c7b53
Gabriel Luong <gabriel.luong@gmail.com>
2024-04-03 07:50:26 +0000

    Bug 1885580 - Add a MenuGroup component for the menu redesign r=android-reviewers,007
    Source: https://www.figma.com/file/RFz9fYtotQCQuinwcZujZt/Menu-Redesign?type=design&node-id=10107-73271&mode=dev
    
    Differential Revision: https://phabricator.services.mozilla.com/D206252

---
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/MenuGroup.kt
@@ -0,0 +1,75 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.components.menu.compose
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.mozilla.fenix.R
+import org.mozilla.fenix.compose.Divider
+import org.mozilla.fenix.compose.annotation.LightDarkPreview
+import org.mozilla.fenix.compose.list.IconListItem
+import org.mozilla.fenix.theme.FirefoxTheme
+
+private val ROUNDED_CORNER_SHAPE = RoundedCornerShape(12.dp)
+
+/**
+ * A menu group container.
+ *
+ * @param content The children composable to be laid out.
+ */
+@Composable
+internal fun MenuGroup(content: @Composable () -> Unit) {
+    Column(
+        modifier = Modifier
+            .background(
+                color = FirefoxTheme.colors.layer2,
+                shape = ROUNDED_CORNER_SHAPE,
+            )
+            .border(
+                border = BorderStroke(
+                    width = 0.5.dp,
+                    color = FirefoxTheme.colors.borderPrimary,
+                ),
+                shape = ROUNDED_CORNER_SHAPE,
+            ),
+    ) {
+        content()
+    }
+}
+
+@LightDarkPreview
+@Composable
+private fun MenuGroupPreview() {
+    FirefoxTheme {
+        Column(
+            modifier = Modifier
+                .background(color = FirefoxTheme.colors.layer3)
+                .padding(16.dp),
+        ) {
+            MenuGroup {
+                IconListItem(
+                    label = stringResource(id = R.string.browser_menu_add_to_homescreen),
+                    beforeIconPainter = painterResource(id = R.drawable.mozac_ic_plus_24),
+                )
+
+                Divider(color = FirefoxTheme.colors.borderSecondary)
+
+                IconListItem(
+                    label = stringResource(id = R.string.browser_menu_add_to_homescreen),
+                    beforeIconPainter = painterResource(id = R.drawable.mozac_ic_plus_24),
+                )
+            }
+        }
+    }
+}
