f0a5962b2afa617ec4292b1a303013b5d597d12e
Tom Ritter <tom@mozilla.com>
2024-04-03 23:43:17 +0000

    Bug 1882422: Change all http references in the license to https r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D204407

--- toolkit/components/resistfingerprinting/RFPTargetIPCUtils.h
+++ toolkit/components/resistfingerprinting/RFPTargetIPCUtils.h
@@ -2,7 +2,7 @@
 /* vim: set ts=8 sts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef __RFPTargetIPCUtils_h__
 #define __RFPTargetIPCUtils_h__
--- toolkit/components/resistfingerprinting/tests/browser/browser_canvas_randomization.js
+++ toolkit/components/resistfingerprinting/tests/browser/browser_canvas_randomization.js
@@ -1,6 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 /**
  * Bug 1816189 - Testing canvas randomization on canvas data extraction.
