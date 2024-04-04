a8abdc6e3a211a55d316813211780ca6d839be91
Boris Chiou <boris.chiou@gmail.com>
2024-04-04 19:39:29 +0000

    Bug 1889496 - Part 2: Implement CSSStartingStyleRule interface. r=webidl,firefox-style-system-reviewers,layout-reviewers,emilio
    Introduce CSSStartingStyleRule and add one extra parsing test for
    @starting-style rule.
    
    Differential Revision: https://phabricator.services.mozilla.com/D206564

--- layout/style/ServoStyleSet.cpp
+++ layout/style/ServoStyleSet.cpp
@@ -43,6 +43,7 @@
 #include "mozilla/dom/CSSPropertyRule.h"
 #include "mozilla/dom/CSSScopeRule.h"
 #include "mozilla/dom/CSSSupportsRule.h"
+#include "mozilla/dom/CSSStartingStyleRule.h"
 #include "mozilla/dom/FontFaceSet.h"
 #include "mozilla/dom/Element.h"
 #include "mozilla/dom/ElementInlines.h"
@@ -1004,9 +1005,7 @@ void ServoStyleSet::RuleChangedInternal(StyleSheet& aSheet, css::Rule& aRule,
     CASE_FOR(LayerStatement, LayerStatement)
     CASE_FOR(Container, Container)
     CASE_FOR(Scope, Scope)
-    case StyleCssRuleType::StartingStyle:
-      // TODO: Implement this in the following patch.
-      break;
+    CASE_FOR(StartingStyle, StartingStyle)
     // @namespace can only be inserted / removed when there are only other
     // @namespace and @import rules, and can't be mutated.
     case StyleCssRuleType::Namespace:
