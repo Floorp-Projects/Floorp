7b61999d68e8625f1938833f874813b44c1cde75
Nan Jiang <njiang028@gmail.com>
2024-04-05 19:33:15 +0000

    Bug 1886601 - Wire up the relevancy Rust component r=bdk,ttran,firefox-desktop-core-reviewers ,mconley
    Differential Revision: https://phabricator.services.mozilla.com/D205708

---
+++ toolkit/components/contentrelevancy/tests/xpcshell/head.js
@@ -0,0 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// This requires the profile directory for Places and the content relevancy
+// component needs a profile directory for storage.
+do_get_profile();
