7cffb0bcf480aac04952a70887dfdb0142acc1c7
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2024-04-05 17:46:38 +0000

    Bug 1889508 - Don't override InputConnection.commitText on Samsung Galaxy J7. r=geckoview-reviewers,ohall a=RyanVM
    It seems to occur unexpected crash on Android 6 with Samsung Galaxy J7
    only.
    
    I don't know why Editable.replace throws null pointer exception. As long
    as AOSP code, there is no reason to throw it. I guess this causes Samsung's
    customizations etc.
    
    So I would like to turn off the optimization by bug 1837931 on Galaxy J7
    only.
    
    Differential Revision: https://phabricator.services.mozilla.com/D206519

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/InputMethods.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/InputMethods.java
@@ -6,11 +6,13 @@
 package org.mozilla.gecko;
 
 import android.content.Context;
+import android.os.Build;
 import android.provider.Settings.Secure;
 import android.view.View;
 import android.view.inputmethod.InputMethodInfo;
 import android.view.inputmethod.InputMethodManager;
 import java.util.Collection;
+import java.util.Locale;
 
 public final class InputMethods {
   public static final String METHOD_ANDROID_LATINIME = "com.android.inputmethod.latin/.LatinIME";
@@ -97,6 +99,13 @@ public final class InputMethods {
   public static boolean needsRestartOnReplaceRemove(final Context context) {
     final String inputMethod = getCurrentInputMethod(context);
     return METHOD_SONY.equals(inputMethod);
+  }
+
+  // Workaround for bug 1818268 - Unexpected crash on Galaxy J7
+  public static boolean dontOverrideCommitText() {
+    return Build.VERSION.SDK_INT == 23
+        && Build.MANUFACTURER.toLowerCase(Locale.ROOT).equals("samsung")
+        && Build.MODEL.startsWith("SM-J700F");
   }
 
   // TODO: Replace usages by definition in EditorInfoCompat once available (bug 1385726).
