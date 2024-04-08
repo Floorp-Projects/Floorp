7d9f17f390c3a5c2a2f64626d14276efa3a9a0ec
keithamus <keithamus@users.noreply.github.com>
2024-04-08 13:15:23 +0000

    Bug 1887543 - Enable CustomStateSet by default. r=webidl,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D206883

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -344,7 +344,7 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CustomEvent", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "CustomStateSet", insecureContext: true, nightly: true },
+  { name: "CustomStateSet", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "DecompressionStream", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
