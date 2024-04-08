a332c6082e3d19683896bf27d3b33f1d7b07636e
Greg Stoll <gstoll@mozilla.com>
2024-04-08 23:53:58 +0000

    Bug 1875481 - Part 1: move MOZ_CAN_RUN_SCRIPT_BOUNDARY_LAMBDA into a shared header r=handyman
    Differential Revision: https://phabricator.services.mozilla.com/D200977

--- mfbt/Attributes.h
+++ mfbt/Attributes.h
@@ -1031,4 +1031,12 @@
 #  define MOZ_EMPTY_BASES
 #endif
 
+// XXX: GCC somehow does not allow attributes before lambda return types, while
+// clang requires so. See also bug 1627007.
+#ifdef __clang__
+#  define MOZ_CAN_RUN_SCRIPT_BOUNDARY_LAMBDA MOZ_CAN_RUN_SCRIPT_BOUNDARY
+#else
+#  define MOZ_CAN_RUN_SCRIPT_BOUNDARY_LAMBDA
+#endif
+
 #endif /* mozilla_Attributes_h */
