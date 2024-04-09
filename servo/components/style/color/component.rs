788b2180baffd59d5313835d652051e449ff82fe
Tiaan Louw <tlouw@mozilla.com>
2024-04-09 14:00:00 +0000

    Bug 1890502 - Unify the color component parsing functions r=layout-reviewers,emilio
    No behaviour is changed and the functions are only merged to a single
    more generic version.  Avoid code duplication and will make future
    changes much easier and less error prone.
    
    Differential Revision: https://phabricator.services.mozilla.com/D206993

--- servo/components/style/color/component.rs
+++ servo/components/style/color/component.rs
@@ -4,6 +4,16 @@
 
 //! Parse/serialize and resolve a single color component.
 
+use crate::{
+    parser::ParserContext,
+    values::{
+        generics::calc::CalcUnits,
+        specified::calc::{CalcNode as SpecifiedCalcNode, Leaf as SpecifiedLeaf},
+    },
+};
+use cssparser::{Parser, Token};
+use style_traits::{ParseError, StyleParseErrorKind};
+
 /// A single color component.
 #[derive(Clone, MallocSizeOf, PartialEq, ToShmem)]
 pub enum ColorComponent<ValueType> {
@@ -43,6 +53,52 @@ impl<ValueType> ColorComponent<ValueType> {
         match self {
             Self::None => default,
             Self::Value(value) => value,
+        }
+    }
+}
+
+/// An utility trait that allows the construction of [ColorComponent]
+/// `ValueType`'s after parsing a color component.
+pub trait ColorComponentType: Sized {
+    /// Return the [CalcUnits] flags that the impl can handle.
+    fn units() -> CalcUnits;
+
+    /// Try to create a new component from the given token.
+    fn try_from_token(token: &Token) -> Result<Self, ()>;
+
+    /// Try to create a new component from the given [CalcNodeLeaf] that was
+    /// resolved from a [CalcNode].
+    fn try_from_leaf(leaf: &SpecifiedLeaf) -> Result<Self, ()>;
+}
+
+impl<ValueType: ColorComponentType> ColorComponent<ValueType> {
+    /// Parse a single [ColorComponent].
+    pub fn parse<'i, 't>(
+        context: &ParserContext,
+        input: &mut Parser<'i, 't>,
+        allow_none: bool,
+    ) -> Result<Self, ParseError<'i>> {
+        let location = input.current_source_location();
+
+        match *input.next()? {
+            Token::Ident(ref value) if allow_none && value.eq_ignore_ascii_case("none") => {
+                Ok(ColorComponent::None)
+            },
+            Token::Function(ref name) => {
+                let function = SpecifiedCalcNode::math_function(context, name, location)?;
+                let node = SpecifiedCalcNode::parse(context, input, function, ValueType::units())?;
+
+                let Ok(resolved_leaf) = node.resolve() else {
+                    return Err(location.new_custom_error(StyleParseErrorKind::UnspecifiedError));
+                };
+
+                ValueType::try_from_leaf(&resolved_leaf)
+                    .map(Self::Value)
+                    .map_err(|_| location.new_custom_error(StyleParseErrorKind::UnspecifiedError))
+            },
+            ref t => ValueType::try_from_token(t)
+                .map(Self::Value)
+                .map_err(|_| location.new_unexpected_token_error(t.clone())),
         }
     }
 }
