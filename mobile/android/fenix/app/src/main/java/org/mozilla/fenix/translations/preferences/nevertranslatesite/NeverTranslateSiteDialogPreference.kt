b1851db3e90c009c6a6cc16038f66c5c6742fd5e
ohall-m <ohall@mozilla.com>
2024-04-09 13:15:49 +0000

    Bug 1890407 - Fixes Translations Button Casing in Dialogs r=android-reviewers,giorga
    A few translations pop-up dialogs needed casing changes, including:
    
    * "Cancel" and "Delete" button case in popup for deleting an item in "Never translate these sites"
    * "Cancel" and "Delete" button case in popup for deleting an item in "Download languages"
    * Low data popup
    
    Differential Revision: https://phabricator.services.mozilla.com/D206944

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/translations/preferences/nevertranslatesite/NeverTranslateSiteDialogPreference.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/translations/preferences/nevertranslatesite/NeverTranslateSiteDialogPreference.kt
@@ -46,12 +46,14 @@ fun NeverTranslateSiteDialogPreference(
         confirmButton = {
             TextButton(
                 text = stringResource(id = R.string.never_translate_site_dialog_confirm_delete_preference),
+                upperCaseText = false,
                 onClick = { onConfirmDelete() },
             )
         },
         dismissButton = {
             TextButton(
                 text = stringResource(id = R.string.never_translate_site_dialog_cancel_preference),
+                upperCaseText = false,
                 onClick = { onCancel() },
             )
         },
