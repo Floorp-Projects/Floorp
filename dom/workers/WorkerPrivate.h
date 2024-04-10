09b11042cdcbf457f5caaddd6b9e582c5ce433ff
Paul Zuehlcke <pbz@mozilla.com>
2024-04-10 14:12:39 +0000

    Bug 1848406 - Keep track of storage access in web workers. r=bvandersloot,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D203640

--- dom/workers/WorkerPrivate.h
+++ dom/workers/WorkerPrivate.h
@@ -996,6 +996,8 @@ class WorkerPrivate final
 
   void PropagateStorageAccessPermissionGranted();
 
+  void NotifyStorageKeyUsed();
+
   void EnableDebugger();
 
   void DisableDebugger();
@@ -1618,6 +1620,10 @@ class WorkerPrivate final
 
   // The flag indicates if the worke is idle for events in the main event loop.
   bool mWorkerLoopIsIdle MOZ_GUARDED_BY(mMutex){false};
+
+  // This flag is used to ensure we only call NotifyStorageKeyUsed once per
+  // global.
+  bool hasNotifiedStorageKeyUsed{false};
 };
 
 class AutoSyncLoopHolder {
