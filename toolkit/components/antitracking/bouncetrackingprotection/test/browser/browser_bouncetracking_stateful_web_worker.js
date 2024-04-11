7398a745e047702481159aa8d4ba0456f3ddda21
Paul Zuehlcke <pbz@mozilla.com>
2024-04-11 19:03:16 +0000

    Bug 1848406 - Keep track of storage access in web workers. r=bvandersloot,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D203640

---
+++ toolkit/components/antitracking/bouncetrackingprotection/test/browser/browser_bouncetracking_stateful_web_worker.js
@@ -0,0 +1,27 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+let bounceTrackingProtection;
+
+add_setup(async function () {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["privacy.bounceTrackingProtection.requireStatefulBounces", true],
+      ["privacy.bounceTrackingProtection.bounceTrackingGracePeriodSec", 0],
+    ],
+  });
+  bounceTrackingProtection = Cc[
+    "@mozilla.org/bounce-tracking-protection;1"
+  ].getService(Ci.nsIBounceTrackingProtection);
+});
+
+add_task(async function test_bounce_stateful_indexedDB() {
+  info("Client bounce with indexedDB.");
+  await runTestBounce({
+    bounceType: "client",
+    setState: "indexedDB",
+    setStateInWebWorker: true,
+  });
+});
