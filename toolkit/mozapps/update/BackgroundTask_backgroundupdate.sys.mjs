a0513d84ab42f53ab11b46fc804e82ed6ec147d7
Jan-Erik Rediger <jrediger@mozilla.com>
2024-04-11 15:41:03 +0000

    Bug 1855257 - Disable internal Glean pings for WDBA r=chutten,application-update-reviewers,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D201079

--- toolkit/mozapps/update/BackgroundTask_backgroundupdate.sys.mjs
+++ toolkit/mozapps/update/BackgroundTask_backgroundupdate.sys.mjs
@@ -344,7 +344,8 @@ export async function runBackgroundTask(commandLine) {
   );
   Services.fog.initializeFOG(
     gleanRoot.path,
-    "firefox.desktop.background.update"
+    "firefox.desktop.background.update",
+    /* disableInternalPings */ true
   );
 
   // For convenience, mirror our loglevel.
