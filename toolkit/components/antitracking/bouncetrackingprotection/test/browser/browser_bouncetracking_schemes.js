4ff6971c779b0d4e35fed2c1b298f9540f7e8557
Paul Zuehlcke <pbz@mozilla.com>
2024-04-11 19:03:18 +0000

    Bug 1888500 - Add a test to ensure we only record user activation for http(s) schemes. r=bvandersloot,anti-tracking-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D206506

---
+++ toolkit/components/antitracking/bouncetrackingprotection/test/browser/browser_bouncetracking_schemes.js
@@ -0,0 +1,67 @@
+/* Any copyright is dedicated to the Public Domain.
+   https://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+let bounceTrackingProtection;
+
+add_setup(async function () {
+  bounceTrackingProtection = Cc[
+    "@mozilla.org/bounce-tracking-protection;1"
+  ].getService(Ci.nsIBounceTrackingProtection);
+  bounceTrackingProtection.clearAll();
+});
+
+async function testInteractWithSite(origin, expectRecorded) {
+  is(
+    bounceTrackingProtection.testGetUserActivationHosts({}).length,
+    0,
+    "No user activation hosts initially"
+  );
+
+  let baseDomain;
+  let scheme;
+
+  await BrowserTestUtils.withNewTab(origin, async browser => {
+    baseDomain = browser.contentPrincipal.baseDomain;
+    scheme = browser.contentPrincipal.URI.scheme;
+
+    info(
+      `Trigger a user activation, which should ${
+        expectRecorded ? "" : "not "
+      }be recorded.`
+    );
+    await BrowserTestUtils.synthesizeMouseAtPoint(50, 50, {}, browser);
+  });
+  if (expectRecorded) {
+    Assert.deepEqual(
+      bounceTrackingProtection.testGetUserActivationHosts({}),
+      [baseDomain],
+      `User activation should be recorded for ${scheme} scheme.`
+    );
+  } else {
+    Assert.deepEqual(
+      bounceTrackingProtection.testGetUserActivationHosts({}),
+      [],
+      `User activation should not be recorded for ${scheme} scheme.`
+    );
+  }
+
+  bounceTrackingProtection.clearAll();
+}
+
+/**
+ * Test that we only record user activation for supported schemes.
+ */
+add_task(async function test_userActivationSchemes() {
+  // eslint-disable-next-line @microsoft/sdl/no-insecure-url
+  await testInteractWithSite("http://example.com", true);
+  await testInteractWithSite("https://example.com", true);
+
+  await testInteractWithSite("about:blank", false);
+  await testInteractWithSite("about:robots", false);
+  await testInteractWithSite(
+    "file://" + Services.dirsvc.get("TmpD", Ci.nsIFile).path,
+    false
+  );
+});
