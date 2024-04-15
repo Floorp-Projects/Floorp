3b11fe29578d806a2dceaacaf7844e4fb708ec97
Ben Dean-Kawamura <bdk@mozilla.com>
2024-04-15 18:07:00 +0000

    Bug 1890866 - Reorganize the UniFFI fixtures/examples.  r=markh
    Differential Revision: https://phabricator.services.mozilla.com/D207208

---
+++ toolkit/components/uniffi-fixtures/sprites/tests/bindings/test_sprites.py
@@ -0,0 +1,16 @@
+from sprites import Point, Sprite, Vector
+
+sempty = Sprite(None)
+assert sempty.get_position() == Point(x=0, y=0)
+
+s = Sprite(Point(x=0, y=1))
+assert s.get_position() == Point(x=0, y=1)
+
+s.move_to(Point(x=1, y=2))
+assert s.get_position() == Point(x=1, y=2)
+
+s.move_by(Vector(dx=-4, dy=2))
+assert s.get_position() == Point(x=-3, y=4)
+
+srel = Sprite.new_relative_to(Point(x=0, y=1), Vector(dx=1, dy=1.5))
+assert srel.get_position() == Point(x=1, y=2.5)
