7d66c71fb309b6c06610408ff4c0dd4371424330
Tamas Szentpeteri <tszentpeteri@mozilla.com>
2024-04-17 18:32:19 +0300

    Backed out changeset 4bbc6fe9fdf1 (bug 1883668) for causing xpcshell failures in test_dap.js. CLOSED TREE

--- toolkit/components/nimbus/generate/generate_feature_manifest.py
+++ toolkit/components/nimbus/generate/generate_feature_manifest.py
@@ -25,6 +25,7 @@ NIMBUS_FALLBACK_PREFS = (
 # See https://bugzilla.mozilla.org/show_bug.cgi?id=1875331 for details.
 ALLOWED_ISEARLYSTARTUP_FEATURE_IDS = {
     "aboutwelcome",
+    "gleanInternalSdk",
     "majorRelease2022",
     "newtab",
     "pocketNewtab",
