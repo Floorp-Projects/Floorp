0ecdb4107798c5ce0d977d667ade8a8380ef750f
Chris Peterson <cpeterson@mozilla.com>
2024-04-17 21:07:31 +0000

    Bug 1891332 - Replace debug MOZ_ASSERT with MOZ_RELEASE_ASSERT in Windows tests. r=win-reviewers,rkraesig
    And MOZ_ASSERT_UNREACHABLE with MOZ_CRASH.
    
    MOZ_ASSERT and MOZ_ASSERT_UNREACHABLE are only checked in debug builds, so release builds' tests are not checking these assertions (though these tests could still have failed later with less-obvious errors).
    
    Depends on D207377
    
    Differential Revision: https://phabricator.services.mozilla.com/D207378

--- browser/app/winlauncher/test/TestSameBinary.cpp
+++ browser/app/winlauncher/test/TestSameBinary.cpp
@@ -193,8 +193,7 @@ static int ParentMain(int argc, wchar_t* argv[]) {
     return 1;
   }
 
-  MOZ_ASSERT_UNREACHABLE("This process should be terminated by now");
-  return 0;
+  MOZ_CRASH("This process should be terminated by now");
 }
 
 static int MonitorMain(int argc, wchar_t* argv[]) {
