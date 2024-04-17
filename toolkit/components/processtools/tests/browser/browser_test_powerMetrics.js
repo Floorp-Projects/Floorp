1eace53086561a6f663f92ec7645b0613553ff8e
Malte Juergens <mjurgens@mozilla.com>
2024-04-17 12:17:37 +0000

    Bug 1835983 - Update tests in toolkit/components/ to work with https-first enabled r=freddyb
    Differential Revision: https://phabricator.services.mozilla.com/D204662

--- toolkit/components/processtools/tests/browser/browser_test_powerMetrics.js
+++ toolkit/components/processtools/tests/browser/browser_test_powerMetrics.js
@@ -12,7 +12,7 @@ function GetTestWebBasedURL(fileName) {
   return (
     getRootDirectory(gTestPath).replace(
       "chrome://mochitests/content",
-      "http://example.org"
+      "https://example.org"
     ) + fileName
   );
 }
