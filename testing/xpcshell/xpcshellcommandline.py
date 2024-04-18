1c02724953683d255187a466f356083de92d16b2
Joel Maher <jmaher@mozilla.com>
2024-04-18 02:22:45 +0000

    Bug 1891629 - make xpcshell msix a pure variant. r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D207496

--- testing/xpcshell/xpcshellcommandline.py
+++ testing/xpcshell/xpcshellcommandline.py
@@ -23,6 +23,20 @@ def add_common_arguments(parser):
         help="application directory (as opposed to XRE directory)",
     )
     parser.add_argument(
+        "--msix-app-path",
+        type=str,
+        dest="msixAppPath",
+        default=None,
+        help="application directory for msix mode (as opposed to XRE directory)",
+    )
+    parser.add_argument(
+        "--msix-app-binary",
+        type=str,
+        dest="msix_app_binary",
+        default=None,
+        help="path to application binary for msix mode (eg: c:\\program files\\mozilla firefox\\firefox.exe)",
+    )
+    parser.add_argument(
         "--interactive",
         action="store_true",
         dest="interactive",
@@ -143,6 +157,15 @@ def add_common_arguments(parser):
         help="absolute path to directory containing XRE (probably xulrunner)",
     )
     parser.add_argument(
+        "--msix-xre-path",
+        action="store",
+        type=str,
+        dest="msixXrePath",
+        # individual scripts will set a sane default
+        default=None,
+        help="absolute path to directory containing XRE in msix mode (probably xulrunner)",
+    )
+    parser.add_argument(
         "--symbols-path",
         action="store",
         type=str,
@@ -246,6 +269,12 @@ def add_common_arguments(parser):
         help="override the number of jobs (threads) when running tests "
         "in parallel, the default is CPU x 1.5 when running via mach "
         "and CPU x 4 when running in automation",
+    )
+    parser.add_argument(
+        "--variant",
+        action="store",
+        default="",
+        help="use specified variant for any harness level changes",
     )
     parser.add_argument(
         "--setpref",
