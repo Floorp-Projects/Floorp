da7e971e1cf0b7cc89df90f910ec0426be0fbfe6
Sandor Molnar <smolnar@mozilla.com>
2024-04-18 08:23:25 +0300

    Backed out changeset f85447f6f56d (bug 1891145) for causing mochitest failures @ browser_visituri_restriction.js CLOSED TREE

--- toolkit/components/places/History.h
+++ toolkit/components/places/History.h
@@ -196,16 +196,6 @@ class History final : public BaseHistory,
   };
 
   nsTHashMap<nsURIHashKey, RecentURIVisit> mRecentlyVisitedURIs;
-
-  struct OriginFloodingRestriction {
-    TimeStamp mLastVisitTimeStamp;
-    unsigned int mExpireIntervalSeconds;
-    unsigned int mAllowedVisitCount;
-  };
-  nsTHashMap<nsCStringHashKey, OriginFloodingRestriction>
-      mOriginFloodingRestrictions;
-  void UpdateOriginFloodingRestriction(nsACString& aOrigin);
-  bool IsRestrictedOrigin(nsACString& aOrigin);
 };
 
 }  // namespace mozilla::places
