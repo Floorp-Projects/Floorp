21c58a0027ebe34a19807f02c0e83d3de5d9a420
Ryan VanderMeulen <ryanvm@gmail.com>
2024-04-23 21:19:48 +0000

    Bug 1892371 - Update to JDK 17.0.11. r=geckoview-reviewers,amejiamarmol
    Differential Revision: https://phabricator.services.mozilla.com/D208279

--- python/mozboot/mozboot/android.py
+++ python/mozboot/mozboot/android.py
@@ -42,8 +42,8 @@ AVD_MANIFEST_ARM = Path(__file__).resolve().parent / "android-avds/arm.json"
 AVD_MANIFEST_ARM64 = Path(__file__).resolve().parent / "android-avds/arm64.json"
 
 JAVA_VERSION_MAJOR = "17"
-JAVA_VERSION_MINOR = "0.10"
-JAVA_VERSION_PATCH = "7"
+JAVA_VERSION_MINOR = "0.11"
+JAVA_VERSION_PATCH = "9"
 
 ANDROID_NDK_EXISTS = """
 Looks like you have the correct version of the Android NDK installed at:
@@ -824,7 +824,7 @@ def ensure_java(os_name, os_arch):
 
     if not java_path.exists():
         # e.g. https://github.com/adoptium/temurin17-binaries/releases/
-        #      download/jdk-17.0.10%2B7/OpenJDK17U-jre_x64_linux_hotspot_17.0.10_7.tar.gz
+        #      download/jdk-17.0.11%2B9/OpenJDK17U-jre_x64_linux_hotspot_17.0.11_9.tar.gz
         java_url = (
             "https://github.com/adoptium/temurin{major}-binaries/releases/"
             "download/jdk-{major}.{minor}%2B{patch}/"
@@ -842,7 +842,7 @@ def ensure_java(os_name, os_arch):
 
 
 def java_bin_path(os_name, toolchain_path: Path):
-    # Like jdk-17.0.10+7
+    # Like jdk-17.0.11+9
     jdk_folder = "jdk-{major}.{minor}+{patch}".format(
         major=JAVA_VERSION_MAJOR, minor=JAVA_VERSION_MINOR, patch=JAVA_VERSION_PATCH
     )
