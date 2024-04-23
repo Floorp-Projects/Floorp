0abfb7c1f9c011582076a89f12397df6b7e71fa5
Nicolas Chevobbe <nchevobbe@mozilla.com>
2024-04-23 08:43:03 +0000

    Bug 1889363 - [devtools] Implement performEOFFixup for InspectorCSSParser. r=devtools-reviewers,ochameau.
    Differential Revision: https://phabricator.services.mozilla.com/D206748

--- devtools/shared/css/lexer.js
+++ devtools/shared/css/lexer.js
@@ -1514,11 +1514,16 @@ Scanner.prototype = {
  *
  * @param {String} input the CSS text to lex
  * @param {Boolean} useInspectorCSSParser Set to true to use InspectorCSSParser.
+ * @param {Boolean} trackEOFChars Set to true if performEOFFixup will be called.
  * @return {CSSLexer} the new lexer
  */
-function getCSSLexer(input, useInspectorCSSParser = false) {
+function getCSSLexer(
+  input,
+  useInspectorCSSParser = false,
+  trackEOFChars = false
+) {
   if (useInspectorCSSParser) {
-    return new InspectorCSSParserWrapper(input);
+    return new InspectorCSSParserWrapper(input, { trackEOFChars });
   }
   return new Scanner(input);
 }
@@ -1531,8 +1536,18 @@ exports.getCSSLexer = getCSSLexer;
  */
 class InspectorCSSParserWrapper {
   #offset = 0;
-  constructor(input) {
+  #trackEOFChars;
+  #eofCharacters = eEOFCharacters_None;
+
+  /**
+   *
+   * @param {String} input: The CSS text to lex
+   * @param {Object} options
+   * @param {Boolean} options.trackEOFChars: Set to true if performEOFFixup will be called.
+   */
+  constructor(input, options = {}) {
     this.parser = new InspectorCSSParser(input);
+    this.#trackEOFChars = options.trackEOFChars;
   }
 
   get lineNumber() {
@@ -1549,6 +1564,49 @@ class InspectorCSSParserWrapper {
       return token;
     }
 
+    if (this.#trackEOFChars) {
+      const { tokenType, text } = token;
+      const lastChar = text[text.length - 1];
+      if (tokenType === "Comment" && lastChar !== `/`) {
+        if (lastChar === `*`) {
+          this.#eofCharacters = eEOFCharacters_Slash;
+        } else {
+          this.#eofCharacters = eEOFCharacters_Asterisk | eEOFCharacters_Slash;
+        }
+      } else if (tokenType === "QuotedString" || tokenType === "BadString") {
+        if (lastChar === "\\") {
+          this.#eofCharacters =
+            this.#eofCharacters | eEOFCharacters_DropBackslash;
+        }
+        if (text[0] !== lastChar) {
+          this.#eofCharacters =
+            this.#eofCharacters |
+            (text[0] === `"`
+              ? eEOFCharacters_DoubleQuote
+              : eEOFCharacters_SingleQuote);
+        }
+      } else {
+        if (lastChar === "\\") {
+          this.#eofCharacters = eEOFCharacters_ReplacementChar;
+        }
+
+        // For some reason, we only automatically close `url`, other functions
+        // will have their opening parenthesis escaped.
+        if (
+          (tokenType === "Function" && token.value === "url") ||
+          tokenType === "BadUrl" ||
+          (tokenType === "UnquotedUrl" && lastChar !== ")")
+        ) {
+          this.#eofCharacters = this.#eofCharacters | eEOFCharacters_CloseParen;
+        }
+
+        if (tokenType === "CloseParenthesis") {
+          this.#eofCharacters =
+            this.#eofCharacters & ~eEOFCharacters_CloseParen;
+        }
+      }
+    }
+
     // At the moment, InspectorCSSParser doesn't expose offsets, so we need to compute
     // them manually here.
     // We can do that because we are retrieving every token in the input string, and so the
@@ -1557,5 +1615,78 @@ class InspectorCSSParserWrapper {
     this.#offset += token.text.length;
     token.endOffset = this.#offset;
     return token;
+  }
+
+  /**
+   * When EOF is reached, the last token might be unterminated in some
+   * ways.  This method takes an input string and appends the needed
+   * terminators.  In particular:
+   *
+   * 1. If EOF occurs mid-string, this will append the correct quote.
+   * 2. If EOF occurs in a url token, this will append the close paren.
+   * 3. If EOF occurs in a comment this will append the comment closer.
+   *
+   * A trailing backslash might also have been present in the input
+   * string.  This is handled in different ways, depending on the
+   * context and arguments.
+   *
+   * If preserveBackslash is true, then the existing backslash at the
+   * end of inputString is preserved, and a new backslash is appended.
+   * That is, the input |\| is transformed to |\\|, and the
+   * input |'\| is transformed to |'\\'|.
+   *
+   * Otherwise, preserveBackslash is false:
+   * If the backslash appears in a string context, then the trailing
+   * backslash is dropped from inputString.  That is, |"\| is
+   * transformed to |""|.
+   * If the backslash appears outside of a string context, then
+   * U+FFFD is appended.  That is, |\| is transformed to a string
+   * with two characters: backslash followed by U+FFFD.
+   *
+   * Passing false for preserveBackslash makes the result conform to
+   * the CSS Syntax specification.  However, passing true may give
+   * somewhat more intuitive behavior.
+   *
+   * @param inputString the input string
+   * @param preserveBackslash how to handle trailing backslashes
+   * @return the input string with the termination characters appended
+   */
+  performEOFFixup(inputString, preserveBackslash) {
+    let result = inputString;
+
+    let eofChars = this.#eofCharacters;
+    if (
+      preserveBackslash &&
+      (eofChars &
+        (eEOFCharacters_DropBackslash | eEOFCharacters_ReplacementChar)) !=
+        0
+    ) {
+      eofChars &= ~(
+        eEOFCharacters_DropBackslash | eEOFCharacters_ReplacementChar
+      );
+      result += "\\";
+    }
+
+    if (
+      (eofChars & eEOFCharacters_DropBackslash) != 0 &&
+      !!result.length &&
+      result.endsWith("\\")
+    ) {
+      result = result.slice(0, -1);
+    }
+
+    // First, ignore eEOFCharacters_DropBackslash.
+    let c = eofChars >> 1;
+
+    // All of the remaining EOFCharacters bits represent appended characters,
+    // and the bits are in the order that they need appending.
+    for (const p of kImpliedEOFCharacters) {
+      if (c & 1) {
+        result += String.fromCharCode(p);
+      }
+      c >>= 1;
+    }
+
+    return result;
   }
 }
