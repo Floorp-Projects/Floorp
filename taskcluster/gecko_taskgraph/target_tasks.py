188b79877a41224a947a8bff9545d065959c936f
Erich Gubler <erichdongubler@gmail.com>
2024-04-23 20:47:30 +0000

    Bug 1873687 - test(webgpu): split (most) tests from `webgpu` into `webgpu-backlog` r=webgpu-reviewers,taskgraph-reviewers,bhearsum,nical
    Differential Revision: https://phabricator.services.mozilla.com/D207224

--- taskcluster/gecko_taskgraph/target_tasks.py
+++ taskcluster/gecko_taskgraph/target_tasks.py
@@ -43,7 +43,7 @@ UNCOMMON_TRY_TASK_LABELS = [
     r"linux-",  # hide all linux32 tasks by default - bug 1599197
     r"linux1804-32",  # hide linux32 tests - bug 1599197
     # Test tasks
-    r"web-platform-tests.*backlog",  # hide wpt jobs that are not implemented yet - bug 1572820
+    r"web-platform-tests(?!-webgpu).*backlog",  # hide wpt jobs that are not implemented yet - bug 1572820
     r"-ccov",
     r"-profiling-",  # talos/raptor profiling jobs are run too often
     r"-32-.*-webgpu",  # webgpu gets little benefit from these tests.
