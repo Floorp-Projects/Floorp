51e48cbe3396575c1a9142ca18fa4ce5b3217fc7
James Teh <jteh@mozilla.com>
2024-04-23 06:07:43 +0000

    Bug 1887799 part 2: Implement the UIA GridItem pattern. r=nlapre
    Differential Revision: https://phabricator.services.mozilla.com/D208275

---
+++ accessible/windows/uia/UiaGridItem.cpp
@@ -0,0 +1,96 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "ia2AccessibleTableCell.h"
+#include "mozilla/a11y/TableAccessible.h"
+#include "mozilla/a11y/TableCellAccessible.h"
+#include "UiaGridItem.h"
+
+using namespace mozilla;
+using namespace mozilla::a11y;
+
+// UiaGridItem
+
+TableCellAccessible* UiaGridItem::CellAcc() {
+  auto* derived = static_cast<ia2AccessibleTableCell*>(this);
+  Accessible* acc = derived->Acc();
+  return acc ? acc->AsTableCell() : nullptr;
+}
+
+// IGridItemProvider methods
+
+STDMETHODIMP
+UiaGridItem::get_Row(__RPC__out int* aRetVal) {
+  if (!aRetVal) {
+    return E_INVALIDARG;
+  }
+  TableCellAccessible* cell = CellAcc();
+  if (!cell) {
+    return CO_E_OBJNOTCONNECTED;
+  }
+  *aRetVal = cell->RowIdx();
+  return S_OK;
+}
+
+STDMETHODIMP
+UiaGridItem::get_Column(__RPC__out int* aRetVal) {
+  if (!aRetVal) {
+    return E_INVALIDARG;
+  }
+  TableCellAccessible* cell = CellAcc();
+  if (!cell) {
+    return CO_E_OBJNOTCONNECTED;
+  }
+  *aRetVal = cell->ColIdx();
+  return S_OK;
+}
+
+STDMETHODIMP
+UiaGridItem::get_RowSpan(__RPC__out int* aRetVal) {
+  if (!aRetVal) {
+    return E_INVALIDARG;
+  }
+  TableCellAccessible* cell = CellAcc();
+  if (!cell) {
+    return CO_E_OBJNOTCONNECTED;
+  }
+  *aRetVal = cell->RowExtent();
+  return S_OK;
+}
+
+STDMETHODIMP
+UiaGridItem::get_ColumnSpan(__RPC__out int* aRetVal) {
+  if (!aRetVal) {
+    return E_INVALIDARG;
+  }
+  TableCellAccessible* cell = CellAcc();
+  if (!cell) {
+    return CO_E_OBJNOTCONNECTED;
+  }
+  *aRetVal = cell->ColExtent();
+  return S_OK;
+}
+
+STDMETHODIMP
+UiaGridItem::get_ContainingGrid(
+    __RPC__deref_out_opt IRawElementProviderSimple** aRetVal) {
+  if (!aRetVal) {
+    return E_INVALIDARG;
+  }
+  *aRetVal = nullptr;
+  TableCellAccessible* cell = CellAcc();
+  if (!cell) {
+    return CO_E_OBJNOTCONNECTED;
+  }
+  TableAccessible* table = cell->Table();
+  if (!table) {
+    return E_FAIL;
+  }
+  Accessible* tableAcc = table->AsAccessible();
+  RefPtr<IRawElementProviderSimple> uia = MsaaAccessible::GetFrom(tableAcc);
+  uia.forget(aRetVal);
+  return S_OK;
+}
---
+++ accessible/windows/uia/UiaGridItem.h
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_a11y_UiaGridItem_h__
+#define mozilla_a11y_UiaGridItem_h__
+
+#include "objbase.h"
+#include "uiautomation.h"
+
+namespace mozilla::a11y {
+class TableCellAccessible;
+
+/**
+ * IGridItemProvider implementation.
+ */
+class UiaGridItem : public IGridItemProvider {
+ public:
+  // IGridItemProvider
+  virtual /* [propget] */ HRESULT STDMETHODCALLTYPE get_Row(
+      /* [retval][out] */ __RPC__out int* aRetVal);
+
+  virtual /* [propget] */ HRESULT STDMETHODCALLTYPE get_Column(
+      /* [retval][out] */ __RPC__out int* aRetVal);
+
+  virtual /* [propget] */ HRESULT STDMETHODCALLTYPE get_RowSpan(
+      /* [retval][out] */ __RPC__out int* aRetVal);
+
+  virtual /* [propget] */ HRESULT STDMETHODCALLTYPE get_ColumnSpan(
+      /* [retval][out] */ __RPC__out int* aRetVal);
+
+  virtual /* [propget] */ HRESULT STDMETHODCALLTYPE get_ContainingGrid(
+      /* [retval][out] */ __RPC__deref_out_opt IRawElementProviderSimple**
+          aRetVal);
+
+ private:
+  TableCellAccessible* CellAcc();
+};
+
+}  // namespace mozilla::a11y
+
+#endif
