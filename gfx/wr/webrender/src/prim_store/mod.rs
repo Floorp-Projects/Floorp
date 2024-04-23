406a2bfc94efd6e976fe950af35555fb2ec522c0
Nicolas Silva <nsilva@mozilla.com>
2024-04-23 07:42:45 +0000

    Bug 1892398 - Handle quad nine-patches in a single loop. r=gw
    This patch mainly extracts the direct/indirect quad segment separation in the scratch buffers from D207657
    
    Differential Revision: https://phabricator.services.mozilla.com/D208104

--- gfx/wr/webrender/src/prim_store/mod.rs
+++ gfx/wr/webrender/src/prim_store/mod.rs
@@ -1215,8 +1215,9 @@ pub struct PrimitiveScratchBuffer {
     /// Set of sub-graphs that are required, determined during visibility pass
     pub required_sub_graphs: FastHashSet<PictureIndex>,
 
-    /// Temporary buffer for building segments in to during prepare pass
-    pub quad_segments: Vec<QuadSegment>,
+    /// Temporary buffers for building segments in to during prepare pass
+    pub quad_direct_segments: Vec<QuadSegment>,
+    pub quad_indirect_segments: Vec<QuadSegment>,
 }
 
 impl Default for PrimitiveScratchBuffer {
@@ -1231,7 +1232,8 @@ impl Default for PrimitiveScratchBuffer {
             debug_items: Vec::new(),
             messages: Vec::new(),
             required_sub_graphs: FastHashSet::default(),
-            quad_segments: Vec::new(),
+            quad_direct_segments: Vec::new(),
+            quad_indirect_segments: Vec::new(),
         }
     }
 }
@@ -1245,7 +1247,8 @@ impl PrimitiveScratchBuffer {
         self.segment_instances.recycle(recycler);
         self.gradient_tiles.recycle(recycler);
         recycler.recycle_vec(&mut self.debug_items);
-        recycler.recycle_vec(&mut self.quad_segments);
+        recycler.recycle_vec(&mut self.quad_direct_segments);
+        recycler.recycle_vec(&mut self.quad_indirect_segments);
     }
 
     pub fn begin_frame(&mut self) {
@@ -1254,7 +1257,8 @@ impl PrimitiveScratchBuffer {
         // location.
         self.clip_mask_instances.clear();
         self.clip_mask_instances.push(ClipMaskKind::None);
-        self.quad_segments.clear();
+        self.quad_direct_segments.clear();
+        self.quad_indirect_segments.clear();
 
         self.border_cache_handles.clear();
 
