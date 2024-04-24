8ac1db6c96883bf5b4e83d559f30ca01e5e63ad5
Timothy Nikkel <tnikkel@gmail.com>
2024-04-24 01:57:41 +0000

    Bug 1892678. Rename pref image.decode-sync.enabled to image.testing.decode-sync.enabled. r=gfx-reviewers,nical
    The reason I want to rename this so that it is clear that it is intended for tests only is for bug 1866411. If we see that this pref is set then we can assume that it isn't being flipped during the loading of images, and therefore we can avoid the extra invalidates that sync decode paints would normally incur.
    
    Differential Revision: https://phabricator.services.mozilla.com/D208111

--- testing/profiles/reftest/user.js
+++ testing/profiles/reftest/user.js
@@ -43,7 +43,7 @@ user_pref("gfx.logging.level", 1);
 user_pref("image.downscale-during-decode.enabled", false);
 // We do the capturing the reftest results with sync decoding
 // so we want to be consistent.
-user_pref("image.decode-sync.enabled", true);
+user_pref("image.testing.decode-sync.enabled", true);
 // Disable interruptible reflow since (1) it's normally not going to
 // happen, but (2) it might happen if we somehow end up with both
 // pending user events and clock skew.  So to avoid having to change
