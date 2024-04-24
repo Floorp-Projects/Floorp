b62c83d22be8593139aa19fb8ce66cd7d6fcfd4c
Timothy Nikkel <tnikkel@gmail.com>
2024-04-24 09:16:43 +0000

    Bug 1892867. Remove some unused view/ code. r=hiro
    Differential Revision: https://phabricator.services.mozilla.com/D208289

--- view/nsView.cpp
+++ view/nsView.cpp
@@ -902,28 +902,6 @@ bool nsView::IsRoot() const {
   return mViewManager->GetRootView() == this;
 }
 
-nsRect nsView::GetBoundsInParentUnits() const {
-  nsView* parent = GetParent();
-  nsViewManager* VM = GetViewManager();
-  if (this != VM->GetRootView() || !parent) {
-    return mDimBounds;
-  }
-  int32_t ourAPD = VM->AppUnitsPerDevPixel();
-  int32_t parentAPD = parent->GetViewManager()->AppUnitsPerDevPixel();
-  return mDimBounds.ScaleToOtherAppUnitsRoundOut(ourAPD, parentAPD);
-}
-
-nsPoint nsView::ConvertFromParentCoords(nsPoint aPt) const {
-  const nsView* parent = GetParent();
-  if (parent) {
-    aPt = aPt.ScaleToOtherAppUnits(
-        parent->GetViewManager()->AppUnitsPerDevPixel(),
-        GetViewManager()->AppUnitsPerDevPixel());
-  }
-  aPt -= GetPosition();
-  return aPt;
-}
-
 static bool IsPopupWidget(nsIWidget* aWidget) {
   return aWidget->GetWindowType() == WindowType::Popup;
 }
