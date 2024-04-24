15fb60fb928055b4511757f49a36ee311a34a792
Jonathan Kew <jkew@mozilla.com>
2024-04-24 12:54:46 +0000

    Bug 1878199 - patch 1 - Don't create an additional Mutex in FontFaceImpl::Entry, just use the base gfxFontEntry's existing RWLock.  a=dmeehan
    No functional change here, just cleanup in preparation for the following patch.
    We don't need FontFaceImpl to create a mutex, as there's already a lock (RWLock)
    in its base class gfxFontEntry, and we can use that to guard the fields here.
    
    Original Revision: https://phabricator.services.mozilla.com/D207295
    
    Differential Revision: https://phabricator.services.mozilla.com/D208097

--- layout/style/FontFaceImpl.cpp
+++ layout/style/FontFaceImpl.cpp
@@ -737,7 +737,7 @@ void FontFaceImpl::Entry::SetLoadState(UserFontLoadState aLoadState) {
 
   nsTArray<RefPtr<FontFaceImpl>> fontFaces;
   {
-    MutexAutoLock lock(mMutex);
+    AutoReadLock lock(mLock);
     fontFaces.SetCapacity(mFontFaces.Length());
     for (FontFaceImpl* f : mFontFaces) {
       fontFaces.AppendElement(f);
@@ -759,7 +759,7 @@ void FontFaceImpl::Entry::SetLoadState(UserFontLoadState aLoadState) {
 /* virtual */
 void FontFaceImpl::Entry::GetUserFontSets(
     nsTArray<RefPtr<gfxUserFontSet>>& aResult) {
-  MutexAutoLock lock(mMutex);
+  AutoReadLock lock(mLock);
 
   aResult.Clear();
 
@@ -784,7 +784,7 @@ void FontFaceImpl::Entry::GetUserFontSets(
 
 /* virtual */ already_AddRefed<gfxUserFontSet>
 FontFaceImpl::Entry::GetUserFontSet() const {
-  MutexAutoLock lock(mMutex);
+  AutoReadLock lock(mLock);
   if (mFontSet) {
     return do_AddRef(mFontSet);
   }
@@ -817,7 +817,7 @@ void FontFaceImpl::Entry::CheckUserFontSetLocked() {
 }
 
 void FontFaceImpl::Entry::FindFontFaceOwners(nsTHashSet<FontFace*>& aOwners) {
-  MutexAutoLock lock(mMutex);
+  AutoReadLock lock(mLock);
   for (FontFaceImpl* f : mFontFaces) {
     if (FontFace* owner = f->GetOwner()) {
       aOwners.Insert(owner);
@@ -826,13 +826,13 @@ void FontFaceImpl::Entry::FindFontFaceOwners(nsTHashSet<FontFace*>& aOwners) {
 }
 
 void FontFaceImpl::Entry::AddFontFace(FontFaceImpl* aFontFace) {
-  MutexAutoLock lock(mMutex);
+  AutoWriteLock lock(mLock);
   mFontFaces.AppendElement(aFontFace);
   CheckUserFontSetLocked();
 }
 
 void FontFaceImpl::Entry::RemoveFontFace(FontFaceImpl* aFontFace) {
-  MutexAutoLock lock(mMutex);
+  AutoWriteLock lock(mLock);
   mFontFaces.RemoveElement(aFontFace);
   CheckUserFontSetLocked();
 }
