042556261f8b44051e84f5e261cf5ddd795f3e54
Matthew Gaudet <mgaudet@mozilla.com>
2024-04-24 01:54:14 +0000

    Bug 1892699 - Disable arguments.length optimization in generators and async functions r=jandem
    This could be potentially backed out if Bug 1892802 were fixed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D208206

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -10955,6 +10955,12 @@ GeneralParser<ParseHandler, Unit>::memberPropertyAccess(
   if (handler_.isArgumentsName(lhs) && handler_.isLengthName(name)) {
     MOZ_ASSERT(pc_->numberOfArgumentsNames > 0);
     pc_->numberOfArgumentsNames--;
+    // Currently when resuming Generators don't get their argument length set
+    // in the interpreter frame (see InterpreterStack::resumeGeneratorCallFrame,
+    // and its call to initCallFrame).
+    if (pc_->isGeneratorOrAsync()) {
+      pc_->sc()->setIneligibleForArgumentsLength();
+    }
     return handler_.newArgumentsLength(lhs, name);
   }
 
