cbdc085f99a7ab4ec4541538216e138a84be25ee
AndiAJ <andiaj@users.noreply.github.com>
2024-04-26 08:06:25 +0000

    Bug 1807268 - Fix verifyOpenAllInNewTabsOptionTest UI test r=ohorvath
    Similar situation with the one encountered with [[ https://phabricator.services.mozilla.com/D208505 | verifyOpenAllInPrivateTabsTest ]], the UI test was disabled a while ago due to the Home Activity ANR.
    This always happened after performing the close tabs action.
    
    After taking a closer look, I've noticed that we tried to open the main menu robot instead of the home screen robot (closing all the tabs will result in a transition to the home screen)
    I've also noticed that the closing of the open tabs action was redundant, and wasn't really needed to test this specific scenario so I've removed it.
    
    ✅ The UI test successfully passed 200x on Firebase.
    
    Differential Revision: https://phabricator.services.mozilla.com/D208722

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/BookmarksTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/ui/BookmarksTest.kt
@@ -9,7 +9,6 @@ import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import kotlinx.coroutines.runBlocking
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.mozilla.fenix.R
@@ -237,7 +236,6 @@ class BookmarksTest : TestSetup() {
     }
 
     // TestRail link: https://testrail.stage.mozaws.net/index.php?/cases/view/1919261
-    @Ignore("Failing, see: https://bugzilla.mozilla.org/show_bug.cgi?id=1807268")
     @Test
     fun verifyOpenAllInNewTabsOptionTest() {
         val webPages = listOf(
@@ -261,11 +259,6 @@ class BookmarksTest : TestSetup() {
             createBookmark(webPages[1].url, "root")
             createBookmark(webPages[2].url, "root")
             createBookmark(webPages[3].url, "sub")
-        }.openTabDrawer {
-            closeTab()
-        }
-
-        browserScreen {
         }.openThreeDotMenu {
         }.openBookmarks {
         }.openThreeDotMenu("root") {
