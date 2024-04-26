7f35d71b1c352665faefb6c15778a65897668e7b
Dan Minor <dminor@mozilla.com>
2024-04-26 13:18:03 +0000

    Bug 1833647 - Reimport test262 with Float16Array tests enabled; r=anba
    Differential Revision: https://phabricator.services.mozilla.com/D205261

--- js/src/tests/test262/harness/testTypedArray-conversions.js
+++ js/src/tests/test262/harness/testTypedArray-conversions.js
@@ -1,3 +1,4 @@
+// |reftest| shell-option(--enable-float16array)
 // Copyright (c) 2017 Rick Waldron.  All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
