cffbf6100a6f262f0ddeaba95318b3725f1efc46
Julien Cristau <jcristau@mozilla.com>
2024-04-29 14:06:17 +0000

    Bug 1889299 - switch windows signing to autograph_authenticode_202404 format. r=taskgraph-reviewers,releng-reviewers,bhearsum
    This switches us away from the code signing certificate expiring in
    June, to a new one valid until 2027.
    
    Differential Revision: https://phabricator.services.mozilla.com/D208633

--- taskcluster/gecko_taskgraph/transforms/repackage_signing_partner.py
+++ taskcluster/gecko_taskgraph/transforms/repackage_signing_partner.py
@@ -93,7 +93,7 @@ def make_repackage_signing_description(config, jobs):
                     "paths": [
                         get_artifact_path(dep_job, f"{repack_id}/target.installer.exe"),
                     ],
-                    "formats": ["autograph_authenticode_sha2", "autograph_gpg"],
+                    "formats": ["autograph_authenticode_202404", "autograph_gpg"],
                 }
             ]
 
@@ -113,7 +113,7 @@ def make_repackage_signing_description(config, jobs):
                                 f"{repack_id}/target.stub-installer.exe",
                             ),
                         ],
-                        "formats": ["autograph_authenticode_sha2", "autograph_gpg"],
+                        "formats": ["autograph_authenticode_202404", "autograph_gpg"],
                     }
                 )
         elif "mac" in build_platform:
