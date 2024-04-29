88f543e5502b3aece64a25ac0bad23b59b513155
Jan de Mooij <jdemooij@mozilla.com>
2024-04-29 12:51:19 +0000

    Bug 1889685 part 3 - Remove empty builtin/Sorting.js file. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D208620

--- js/src/builtin/Sorting.js
+++
@@ -1,7 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-// We use varying sorts across the self-hosted codebase. All sorts are
-// consolidated here to avoid confusion and re-implementation of existing
-// algorithms.
