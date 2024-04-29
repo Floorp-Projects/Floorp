0c172174b76dcd0b0d2b703a1db428d420d233c7
t-p-white <towhite@mozilla.com>
2024-04-29 15:53:05 +0000

    Bug 1893174 - Added microsurvey prompt Compose UI r=android-reviewers,vdreghici
    Differential Revision: https://phabricator.services.mozilla.com/D208768

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/microsurvey/ui/MicrosurveyRequestPrompt.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/microsurvey/ui/MicrosurveyRequestPrompt.kt
@@ -6,11 +6,11 @@ package org.mozilla.fenix.microsurvey.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,34 +18,48 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import org.mozilla.fenix.R
 import org.mozilla.fenix.compose.annotation.LightDarkPreview
+import org.mozilla.fenix.compose.button.PrimaryButton
 import org.mozilla.fenix.theme.FirefoxTheme
 
 /**
- * The header UI used for micro-survey.
+ * Initial microsurvey prompt displayed to the user to request completion of feedback.
  *
- * @param title The text that will be visible on header.
- * @param onCloseButtonClick Invoked when the close button is clicked.
+ * @param title The prompt header title.
  */
 @Composable
-fun MicroSurveyHeader(
+fun MicrosurveyRequestPrompt(
+    title: String = "Help make printing in Firefox better. It only takes a sec.",
+) {  // todo this is the message title FXDROID-1918)
+    Column(
+        modifier = Modifier
+            .background(color = FirefoxTheme.colors.layer1)
+            .padding(all = 16.dp),
+    ) {
+        Header(title)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        PrimaryButton(text = stringResource(id = R.string.micro_survey_continue_button_label)) {}
+    }
+}
+
+@Composable
+private fun Header(
     title: String,
-    onCloseButtonClick: () -> Unit,
 ) {
     Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.fillMaxWidth(),
     ) {
         Image(
             painter = painterResource(R.drawable.ic_firefox),
-            contentDescription = null, // todo update to string res once a11y strings are available.
+            contentDescription = null, // todo update to string res once a11y strings are available FXDROID-1919.
             modifier = Modifier.size(24.dp),
         )
 
@@ -58,31 +72,24 @@ fun MicroSurveyHeader(
             modifier = Modifier.weight(1f),
         )
 
-        IconButton(onClick = onCloseButtonClick) {
+        IconButton(
+            onClick = {},// todo FXDROID-1947
+            modifier = Modifier.size(20.dp)
+        ) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_close),
-                contentDescription = null, // todo update to string res once a11y strings are available.
+                contentDescription = null, // todo update to string res once a11y strings are available FXDROID-1919.
                 tint = FirefoxTheme.colors.iconPrimary,
-                modifier = Modifier
-                    .size(20.dp)
             )
         }
     }
 }
 
-/**
- * The preview for micro-survey header
- */
-@Composable
+@PreviewScreenSizes
 @LightDarkPreview
-fun MicroSurveyHeaderPreview() {
+@Composable
+private fun MicrosurveyRequestPromptPreview() {
     FirefoxTheme {
-        Box(
-            modifier = Modifier
-                .background(color = FirefoxTheme.colors.layer1)
-                .padding(16.dp),
-        ) {
-            MicroSurveyHeader(stringResource(R.string.micro_survey_survey_header)) {}
-        }
+        MicrosurveyRequestPrompt()
     }
 }
