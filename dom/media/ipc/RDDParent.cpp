ae15c5491f14588ffb76c62d7540fa0d69f990fe
alwu <alwu@mozilla.com>
2024-04-29 17:46:29 +0000

    Bug 1892516 - part4 : no need to reinit WMF PDM in the RDD process because we only do hardware decoding via WMF on GPU process. r=media-playback-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D208715

--- dom/media/ipc/RDDParent.cpp
+++ dom/media/ipc/RDDParent.cpp
@@ -167,18 +167,6 @@ mozilla::ipc::IPCResult RDDParent::RecvInit(
 }
 
 IPCResult RDDParent::RecvUpdateVar(const GfxVarUpdate& aUpdate) {
-#if defined(XP_WIN)
-  auto scopeExit = MakeScopeExit(
-      [couldUseHWDecoder = gfx::gfxVars::CanUseHardwareVideoDecoding()] {
-        if (couldUseHWDecoder != gfx::gfxVars::CanUseHardwareVideoDecoding()) {
-          // The capabilities of the system may have changed, force a refresh by
-          // re-initializing the WMF PDM.
-          WMFDecoderModule::Init();
-          Unused << RDDParent::GetSingleton()->SendUpdateMediaCodecsSupported(
-              PDMFactory::Supported(true /* force refresh */));
-        }
-      });
-#endif
   gfxVars::ApplyUpdate(aUpdate);
   return IPC_OK();
 }
