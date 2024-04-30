de56ae88bf17f74207c40a627be93ccf1bcd2df6
Edgar Chen <echen@mozilla.com>
2024-04-30 11:49:29 +0000

    Bug 1887845 - Enabled `dom.events.asyncClipboard.clipboardItem` by default; r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D207973

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -257,7 +257,7 @@ let interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "ClipboardEvent", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "ClipboardItem", earlyBetaOrEarlier: true },
+  { name: "ClipboardItem" },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CloseEvent", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
