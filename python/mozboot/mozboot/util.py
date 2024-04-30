19c9fbd1ed562a9086621e3ae4110910bd58944f
Mike Hommey <mh+mozilla@glandium.org>
2024-04-30 07:29:43 +0000

    Bug 1890935 - Bump the minimum version of Rust to 1.76. r=firefox-build-system-reviewers,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D207239

--- python/mozboot/mozboot/util.py
+++ python/mozboot/mozboot/util.py
@@ -11,7 +11,7 @@ import certifi
 from mach.site import PythonVirtualenv
 from mach.util import get_state_dir
 
-MINIMUM_RUST_VERSION = "1.74.0"
+MINIMUM_RUST_VERSION = "1.76.0"
 
 
 def get_tools_dir(srcdir=False):
