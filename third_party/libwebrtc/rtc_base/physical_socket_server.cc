491f976f0444e906442075b651ed5f021a298e37
Nico Grunbaum <na-g@nostrum.com>
2024-05-02 00:53:38 -0700

    Bug 1892299 - Vendor libwebrtc from 4473d75651
    Upstream commit: https://webrtc.googlesource.com/src/+/4473d75651e6805b633349113ecea98a80491840
        Add TCP keep-alive options to rtc::Socket
    
        Enabling Socket options on keep-alive related function that may enable clients to detect any stale connection early on.
    
        Bug: webrtc:15866
        Change-Id: Ib4f15e0c933aeb6cf4fd18ff8cc708d118ea8645
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/342223
        Reviewed-by: Harald Alvestrand <hta@webrtc.org>
        Commit-Queue: Tim Na <natim@webrtc.org>
        Cr-Commit-Position: refs/heads/main@{#41888}

--- third_party/libwebrtc/rtc_base/physical_socket_server.cc
+++ third_party/libwebrtc/rtc_base/physical_socket_server.cc
@@ -785,6 +785,35 @@ int PhysicalSocket::TranslateOption(Option opt, int* slevel, int* sopt) {
 #endif
     case OPT_RTP_SENDTIME_EXTN_ID:
       return -1;  // No logging is necessary as this not a OS socket option.
+    case OPT_KEEPALIVE:
+      *slevel = SOL_SOCKET;
+      *sopt = SO_KEEPALIVE;
+      break;
+    case OPT_TCP_KEEPCNT:
+      *slevel = IPPROTO_TCP;
+      *sopt = TCP_KEEPCNT;
+      break;
+    case OPT_TCP_KEEPIDLE:
+      *slevel = IPPROTO_TCP;
+#if !defined(WEBRTC_MAC)
+      *sopt = TCP_KEEPIDLE;
+#else
+      *sopt = TCP_KEEPALIVE;
+#endif
+      break;
+    case OPT_TCP_KEEPINTVL:
+      *slevel = IPPROTO_TCP;
+      *sopt = TCP_KEEPINTVL;
+      break;
+    case OPT_TCP_USER_TIMEOUT:
+#if defined(WEBRTC_LINUX) || defined(WEBRTC_ANDROID)
+      *slevel = IPPROTO_TCP;
+      *sopt = TCP_USER_TIMEOUT;
+      break;
+#else
+      RTC_LOG(LS_WARNING) << "Socket::OPT_TCP_USER_TIMEOUT not supported.";
+      return -1;
+#endif
     default:
       RTC_DCHECK_NOTREACHED();
       return -1;
