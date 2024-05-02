02c5f4fadfe8ed2391ccb3d37f0cbfbd380c8ed0
Nico Grunbaum <na-g@nostrum.com>
2024-05-02 01:19:44 -0700

    Bug 1892299 - Vendor libwebrtc from d1ba1dc9c7
    Upstream commit: https://webrtc.googlesource.com/src/+/d1ba1dc9c7f5f29da56763a7ebe26b13310fb874
        Update includes to use <> instead of ""
    
        Webrtc is build with FFmpeg sources on defined in the include path
        through the -I flag, so they should be included this way instead. This
        would otherwise cause a conflict when the chromium ffmpeg sources move
        from third_party/ffmpeg/* to third_party/ffmpeg/src/*
    
        BUG: chromium:329282834
        Change-Id: Id8f7e91446bdc536db77e74388a73e51f5111ffc
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/342820
        Reviewed-by: Erik Språng <sprang@webrtc.org>
        Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
        Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#41899}

--- third_party/libwebrtc/modules/video_coding/codecs/h264/h264_decoder_impl.cc
+++ third_party/libwebrtc/modules/video_coding/codecs/h264/h264_decoder_impl.cc
@@ -16,15 +16,15 @@
 
 #include "modules/video_coding/codecs/h264/h264_decoder_impl.h"
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/imgutils.h>
+}  // extern "C"
+
 #include <algorithm>
 #include <limits>
 #include <memory>
-
-extern "C" {
-#include "third_party/ffmpeg/libavcodec/avcodec.h"
-#include "third_party/ffmpeg/libavformat/avformat.h"
-#include "third_party/ffmpeg/libavutil/imgutils.h"
-}  // extern "C"
 
 #include "api/video/color_space.h"
 #include "api/video/i010_buffer.h"
