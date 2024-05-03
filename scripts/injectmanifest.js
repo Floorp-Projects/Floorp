197a7b3e66d8df5aa1d562fd3cebfde149499dc0
NyanRus <68762426+nyanrus@users.noreply.github.com>
2024-05-03 17:50:35 +0900

    linux support in inject manifest

--- scripts/injectmanifest.js
+++ scripts/injectmanifest.js
@@ -1,15 +1,26 @@
 import * as fs from "node:fs/promises";
 
 export async function injectManifest() {
-  const manifest_chrome = (await fs.readFile("dist/bin/chrome.manifest")).toString();
+  const manifest_chrome = (
+    await fs.readFile("dist/bin/chrome.manifest")
+  ).toString();
 
   if (!manifest_chrome.includes("manifest noraneko/noraneko.manifest")) {
-    await fs.writeFile("dist/bin/chrome.manifest", manifest_chrome + "\nmanifest noraneko/noraneko.manifest");
+    await fs.writeFile(
+      "dist/bin/chrome.manifest",
+      `${manifest_chrome}\nmanifest noraneko/noraneko.manifest`,
+    );
   }
 
   try {
     await fs.access("dist/bin/noraneko");
     await fs.rm("dist/bin/noraneko");
   } catch {}
-  await fs.symlink("../noraneko", "dist/bin/noraneko", "junction");
+  const isWin = process.platform === "win32";
+
+  await fs.symlink(
+    "../noraneko",
+    "dist/bin/noraneko",
+    isWin ? "junction" : undefined,
+  );
 }
