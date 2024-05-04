75dbe36b3657feb91a5eb08c32f7f12365be2c0e
donal meehan <dmeehan@mozilla.com>
2024-05-04 08:40:32 -0400

    Backed out 2 changesets (bug 1892947) due to critical failure with clear on shutdown
    Backed out changeset b3259c59eabe (bug 1892947)
    Backed out changeset 90770625d9e3 (bug 1892947)

--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -25,6 +25,18 @@ const CONTENT_BLOCKING_PREFS = [
   "privacy.fingerprintingProtection.pbmode",
 ];
 
+/*
+ * Prefs that are unique to sanitizeOnShutdown and are not shared
+ * with the deleteOnClose mechanism like privacy.clearOnShutdown.cookies, -cache and -offlineApps
+ */
+const SANITIZE_ON_SHUTDOWN_PREFS_ONLY = [
+  "privacy.clearOnShutdown.history",
+  "privacy.clearOnShutdown.downloads",
+  "privacy.clearOnShutdown.sessions",
+  "privacy.clearOnShutdown.formdata",
+  "privacy.clearOnShutdown.siteSettings",
+];
+
 const PREF_OPT_OUT_STUDIES_ENABLED = "app.shield.optoutstudies.enabled";
 const PREF_NORMANDY_ENABLED = "app.normandy.enabled";
 
@@ -79,34 +91,6 @@ XPCOMUtils.defineLazyPreferenceGetter(
 ChromeUtils.defineESModuleGetters(this, {
   DoHConfigController: "resource:///modules/DoHConfig.sys.mjs",
 });
-
-const SANITIZE_ON_SHUTDOWN_MAPPINGS = {
-  history: "privacy.clearOnShutdown.history",
-  downloads: "privacy.clearOnShutdown.downloads",
-  formdata: "privacy.clearOnShutdown.formdata",
-  sessions: "privacy.clearOnShutdown.sessions",
-  siteSettings: "privacy.clearOnShutdown.siteSettings",
-  cookies: "privacy.clearOnShutdown.cookies",
-  cache: "privacy.clearOnShutdown.cache",
-  offlineApps: "privacy.clearOnShutdown.offlineApps",
-};
-
-/*
- * Prefs that are unique to sanitizeOnShutdown and are not shared
- * with the deleteOnClose mechanism like privacy.clearOnShutdown.cookies, -cache and -offlineApps
- */
-const SANITIZE_ON_SHUTDOWN_PREFS_ONLY = [
-  "privacy.clearOnShutdown.history",
-  "privacy.clearOnShutdown.downloads",
-  "privacy.clearOnShutdown.sessions",
-  "privacy.clearOnShutdown.formdata",
-  "privacy.clearOnShutdown.siteSettings",
-];
-
-const SANITIZE_ON_SHUTDOWN_PREFS_ONLY_V2 = [
-  "privacy.clearOnShutdown_v2.historyFormDataAndDownloads",
-  "privacy.clearOnShutdown_v2.siteSettings",
-];
 
 Preferences.addAll([
   // Content blocking / Tracking Protection
@@ -171,20 +155,13 @@ Preferences.addAll([
   { id: "privacy.sanitize.sanitizeOnShutdown", type: "bool" },
   { id: "privacy.sanitize.timeSpan", type: "int" },
   { id: "privacy.clearOnShutdown.cookies", type: "bool" },
-  { id: "privacy.clearOnShutdown_v2.cookiesAndStorage", type: "bool" },
   { id: "privacy.clearOnShutdown.cache", type: "bool" },
-  { id: "privacy.clearOnShutdown_v2.cache", type: "bool" },
   { id: "privacy.clearOnShutdown.offlineApps", type: "bool" },
   { id: "privacy.clearOnShutdown.history", type: "bool" },
-  {
-    id: "privacy.clearOnShutdown_v2.historyFormDataAndDownloads",
-    type: "bool",
-  },
   { id: "privacy.clearOnShutdown.downloads", type: "bool" },
   { id: "privacy.clearOnShutdown.sessions", type: "bool" },
   { id: "privacy.clearOnShutdown.formdata", type: "bool" },
   { id: "privacy.clearOnShutdown.siteSettings", type: "bool" },
-  { id: "privacy.clearOnShutdown_v2.siteSettings", type: "bool" },
 
   // Do not track
   { id: "privacy.donottrackheader.enabled", type: "bool" },
@@ -2117,25 +2094,11 @@ var gPrivacyPane = {
    */
   initDeleteOnCloseBox() {
     let deleteOnCloseBox = document.getElementById("deleteOnClose");
-
-    // We have to branch between the old clear on shutdown prefs and new prefs after the clear history revamp (Bug 1853996)
-    // Once the old dialog is deprecated, we can remove these branches.
-    let isCookiesAndStorageClearingOnShutdown;
-    if (useOldClearHistoryDialog) {
-      isCookiesAndStorageClearingOnShutdown =
-        Preferences.get("privacy.sanitize.sanitizeOnShutdown").value &&
+    deleteOnCloseBox.checked =
+      (Preferences.get("privacy.sanitize.sanitizeOnShutdown").value &&
         Preferences.get("privacy.clearOnShutdown.cookies").value &&
         Preferences.get("privacy.clearOnShutdown.cache").value &&
-        Preferences.get("privacy.clearOnShutdown.offlineApps").value;
-    } else {
-      isCookiesAndStorageClearingOnShutdown =
-        Preferences.get("privacy.sanitize.sanitizeOnShutdown").value &&
-        Preferences.get("privacy.clearOnShutdown_v2.cookiesAndStorage").value &&
-        Preferences.get("privacy.clearOnShutdown_v2.cache").value;
-    }
-
-    deleteOnCloseBox.checked =
-      isCookiesAndStorageClearingOnShutdown ||
+        Preferences.get("privacy.clearOnShutdown.offlineApps").value) ||
       Preferences.get("browser.privatebrowsing.autostart").value;
   },
 
@@ -2148,17 +2111,12 @@ var gPrivacyPane = {
     let sanitizeOnShutdownPref = Preferences.get(
       "privacy.sanitize.sanitizeOnShutdown"
     );
-
     // ClearOnClose cleaning categories
-    let cookiePref = useOldClearHistoryDialog
-      ? Preferences.get("privacy.clearOnShutdown.cookies")
-      : Preferences.get("privacy.clearOnShutdown_v2.cookiesAndStorage");
-    let cachePref = useOldClearHistoryDialog
-      ? Preferences.get("privacy.clearOnShutdown.cache")
-      : Preferences.get("privacy.clearOnShutdown_v2.cache");
-    let offlineAppsPref = useOldClearHistoryDialog
-      ? Preferences.get("privacy.clearOnShutdown.offlineApps")
-      : Preferences.get("privacy.clearOnShutdown_v2.cookiesAndStorage");
+    let cookiePref = Preferences.get("privacy.clearOnShutdown.cookies");
+    let cachePref = Preferences.get("privacy.clearOnShutdown.cache");
+    let offlineAppsPref = Preferences.get(
+      "privacy.clearOnShutdown.offlineApps"
+    );
 
     // Sync the cleaning prefs with the deleteOnClose box
     deleteOnCloseBox.addEventListener("command", () => {
@@ -2198,32 +2156,18 @@ var gPrivacyPane = {
    */
   _onSanitizePrefChangeSyncClearOnClose() {
     let deleteOnCloseBox = document.getElementById("deleteOnClose");
-
-    // We have to branch between the old clear on shutdown prefs and new prefs after the clear history revamp (Bug 1853996)
-    // Once the old dialog is deprecated, we can remove these branches.
-    if (useOldClearHistoryDialog) {
-      deleteOnCloseBox.checked =
-        Preferences.get("privacy.sanitize.sanitizeOnShutdown").value &&
-        Preferences.get("privacy.clearOnShutdown.cookies").value &&
-        Preferences.get("privacy.clearOnShutdown.cache").value &&
-        Preferences.get("privacy.clearOnShutdown.offlineApps").value;
-    } else {
-      deleteOnCloseBox.checked =
-        Preferences.get("privacy.sanitize.sanitizeOnShutdown").value &&
-        Preferences.get("privacy.clearOnShutdown_v2.cookiesAndStorage").value &&
-        Preferences.get("privacy.clearOnShutdown_v2.cache").value;
-    }
+    deleteOnCloseBox.checked =
+      Preferences.get("privacy.clearOnShutdown.cookies").value &&
+      Preferences.get("privacy.clearOnShutdown.cache").value &&
+      Preferences.get("privacy.clearOnShutdown.offlineApps").value &&
+      Preferences.get("privacy.sanitize.sanitizeOnShutdown").value;
   },
 
   /*
    * Unsets cleaning prefs that do not belong to DeleteOnClose
    */
   _resetCleaningPrefs() {
-    let sanitizeOnShutdownPrefsArray = useOldClearHistoryDialog
-      ? SANITIZE_ON_SHUTDOWN_PREFS_ONLY
-      : SANITIZE_ON_SHUTDOWN_PREFS_ONLY_V2;
-
-    return sanitizeOnShutdownPrefsArray.forEach(
+    SANITIZE_ON_SHUTDOWN_PREFS_ONLY.forEach(
       pref => (Preferences.get(pref).value = false)
     );
   },
@@ -2232,11 +2176,7 @@ var gPrivacyPane = {
    Checks if the user set cleaning prefs that do not belong to DeleteOnClose
    */
   _isCustomCleaningPrefPresent() {
-    let sanitizeOnShutdownPrefsArray = useOldClearHistoryDialog
-      ? SANITIZE_ON_SHUTDOWN_PREFS_ONLY
-      : SANITIZE_ON_SHUTDOWN_PREFS_ONLY_V2;
-
-    return sanitizeOnShutdownPrefsArray.some(
+    return SANITIZE_ON_SHUTDOWN_PREFS_ONLY.some(
       pref => Preferences.get(pref).value
     );
   },
