97b75de67ed2643e4e02166270ff2f19616bd846
Gabriel Luong <gabriel.luong@gmail.com>
2024-05-05 08:20:50 +0000

    Bug 1885647 - Part 3: Implement a placeholder Extensions submenu component r=android-reviewers,skhan
    Design: https://www.figma.com/file/RFz9fYtotQCQuinwcZujZt/Menu-Redesign?type=design&node-id=11203-151061&mode=design&t=s2P4UpMXAhoVojlM-4
    
    Differential Revision: https://phabricator.services.mozilla.com/D208227

---
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/ExtensionsSubmenu.kt
@@ -0,0 +1,98 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.components.menu.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.mozilla.fenix.R
+import org.mozilla.fenix.components.menu.compose.header.SubmenuHeader
+import org.mozilla.fenix.compose.annotation.LightDarkPreview
+import org.mozilla.fenix.compose.list.TextListItem
+import org.mozilla.fenix.theme.FirefoxTheme
+import org.mozilla.fenix.theme.Theme
+
+@Composable
+internal fun ExtensionsSubmenu(
+    onBackButtonClick: () -> Unit,
+    onManageExtensionsMenuClick: () -> Unit,
+    onDiscoverMoreExtensionsMenuClick: () -> Unit,
+) {
+    Column {
+        SubmenuHeader(
+            header = stringResource(id = R.string.browser_menu_extensions),
+            onClick = onBackButtonClick,
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Column(
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    top = 12.dp,
+                    end = 16.dp,
+                    bottom = 32.dp,
+                ),
+            verticalArrangement = Arrangement.spacedBy(32.dp),
+        ) {
+            MenuGroup {
+                MenuItem(
+                    label = stringResource(id = R.string.browser_menu_manage_extensions),
+                    beforeIconPainter = painterResource(id = R.drawable.mozac_ic_extension_cog_24),
+                    onClick = onManageExtensionsMenuClick,
+                )
+            }
+
+            MenuGroup {
+                TextListItem(
+                    label = stringResource(id = R.string.browser_menu_discover_more_extensions),
+                    iconPainter = painterResource(R.drawable.mozac_ic_external_link_24),
+                    onClick = onDiscoverMoreExtensionsMenuClick,
+                )
+            }
+        }
+    }
+}
+
+@LightDarkPreview
+@Composable
+private fun ExtensionsSubmenuPreview() {
+    FirefoxTheme {
+        Column(
+            modifier = Modifier.background(color = FirefoxTheme.colors.layer3),
+        ) {
+            ExtensionsSubmenu(
+                onBackButtonClick = {},
+                onManageExtensionsMenuClick = {},
+                onDiscoverMoreExtensionsMenuClick = {},
+            )
+        }
+    }
+}
+
+@LightDarkPreview
+@Composable
+private fun ExtensionsSubmenuPrivatePreview() {
+    FirefoxTheme(theme = Theme.Private) {
+        Column(
+            modifier = Modifier.background(color = FirefoxTheme.colors.layer3),
+        ) {
+            ExtensionsSubmenu(
+                onBackButtonClick = {},
+                onManageExtensionsMenuClick = {},
+                onDiscoverMoreExtensionsMenuClick = {},
+            )
+        }
+    }
+}
