dbf6608bc7a607bbf901611ffe05113241ea5930
Mike Conley <mconley@mozilla.com>
2024-05-06 15:18:40 +0000

    Bug 1892312 - Add recoveredFromBackup to TelemetryEnvironment profile structure. data-review=chutten, r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D209306

--- toolkit/components/telemetry/app/TelemetryEnvironment.sys.mjs
+++ toolkit/components/telemetry/app/TelemetryEnvironment.sys.mjs
@@ -1672,6 +1672,7 @@ EnvironmentCache.prototype = {
     let creationDate = await profileAccessor.created;
     let resetDate = await profileAccessor.reset;
     let firstUseDate = await profileAccessor.firstUse;
+    let recoveredFromBackup = await profileAccessor.recoveredFromBackup;
 
     this._currentEnvironment.profile.creationDate =
       Utils.millisecondsToDays(creationDate);
@@ -1682,6 +1683,10 @@ EnvironmentCache.prototype = {
     if (firstUseDate) {
       this._currentEnvironment.profile.firstUseDate =
         Utils.millisecondsToDays(firstUseDate);
+    }
+    if (recoveredFromBackup) {
+      this._currentEnvironment.profile.recoveredFromBackup =
+        Utils.millisecondsToDays(recoveredFromBackup);
     }
   },
 
