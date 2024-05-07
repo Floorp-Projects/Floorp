6ee3ace30927b4a3500b1cb282d7890dbad2f549
Ryan VanderMeulen <ryanvm@gmail.com>
2024-05-07 13:30:02 +0000

    Bug 1894919 - Update Compose Compiler to version 1.5.13. r=android-reviewers,mcarare
    Differential Revision: https://phabricator.services.mozilla.com/D209378

--- mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ mobile/android/android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -51,7 +51,7 @@ object Versions {
 
     // see https://android-developers.googleblog.com/2022/06/independent-versioning-of-Jetpack-Compose-libraries.html
     // for Jetpack Compose libraries versioning
-    const val compose_compiler = "1.5.12"
+    const val compose_compiler = "1.5.13"
 
     object AndroidX {
         const val activityCompose = "1.7.2"
