57b17216b2a84399adf8998418490602da60648d
Joel Maher <jmaher@mozilla.com>
2024-05-07 11:42:53 +0000

    Bug 1894665 - move linux/ccov jittest to workers that have scratch disk to avoid worker exceptions. r=aryx,taskgraph-reviewers,jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D209052

--- taskcluster/gecko_taskgraph/transforms/test/other.py
+++ taskcluster/gecko_taskgraph/transforms/test/other.py
@@ -576,6 +576,8 @@ def enable_code_coverage(config, tasks):
                 continue
             task["mozharness"].setdefault("extra-options", []).append("--code-coverage")
             task["instance-size"] = "xlarge-noscratch"
+            if "jittest" in task["test-name"]:
+                task["instance-size"] = "xlarge"
 
             # Temporarily disable Mac tests on mozilla-central
             if "mac" in task["build-platform"]:
