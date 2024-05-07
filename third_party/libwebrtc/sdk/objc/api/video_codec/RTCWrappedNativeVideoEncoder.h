293a6f23d71399c0a3db712ca19da51010ffe972
Nico Grunbaum <na-g@nostrum.com>
2024-05-07 15:09:57 -0700

    Bug 1895602 - Cherry-pick upstream libwebrtc commit d7e0981281 r?mjf,pehrsons
    Upstream commit: https://webrtc.googlesource.com/src/+/d7e0981281fc8a94043c8a0cd72fffd28cd2df0d
           Propagate webrtc::Environment through objc VideoEncoderFactory
    
           Bug: webrtc:15860
           Change-Id: I9e7ee89e1ac9f950d38734510cf843e144108d24
           Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/342800
           Reviewed-by: Mirko Bonadei <mbonadei@webrtc.org>
           Reviewed-by: Kári Helgason <kthelgason@webrtc.org>
           Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
           Cr-Commit-Position: refs/heads/main@{#41933}
    
    Differential Revision: https://phabricator.services.mozilla.com/D210235

--- third_party/libwebrtc/sdk/objc/api/video_codec/RTCWrappedNativeVideoEncoder.h
+++ third_party/libwebrtc/sdk/objc/api/video_codec/RTCWrappedNativeVideoEncoder.h
@@ -10,6 +10,7 @@
 
 #import <Foundation/Foundation.h>
 
+#import "RTCNativeVideoEncoder.h"
 #import "base/RTCMacros.h"
 #import "base/RTCVideoEncoder.h"
 
@@ -17,7 +18,8 @@
 #include "api/video_codecs/video_encoder.h"
 #include "media/base/codec.h"
 
-@interface RTC_OBJC_TYPE (RTCWrappedNativeVideoEncoder) : NSObject <RTC_OBJC_TYPE (RTCVideoEncoder)>
+// TODO: bugs.webrtc.org/15860 - Remove in favor of the RTCNativeVideoEncoderBuilder
+@interface RTC_OBJC_TYPE (RTCWrappedNativeVideoEncoder) : RTC_OBJC_TYPE (RTCNativeVideoEncoder)
 
 - (instancetype)initWithNativeEncoder:(std::unique_ptr<webrtc::VideoEncoder>)encoder;
 
