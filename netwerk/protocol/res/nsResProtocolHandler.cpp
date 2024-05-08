d2dc5eccdb6d9127de05477572bab59e9c4d8f1b
Sean <sekim@mozilla.com>
2024-05-08 08:50:39 +0000

    Bug 1557480 - Refactor nsResProtocolHandler::GetSubstitutionInternal into two methods r=valentin,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D209583

--- netwerk/protocol/res/nsResProtocolHandler.cpp
+++ netwerk/protocol/res/nsResProtocolHandler.cpp
@@ -123,20 +123,23 @@ nsResProtocolHandler::AllowContentToAccess(nsIURI* aURI, bool* aResult) {
   return NS_OK;
 }
 
+uint32_t nsResProtocolHandler::GetJARFlags(const nsACString& aRoot) {
+  if (aRoot.Equals(kAndroid)) {
+    return nsISubstitutingProtocolHandler::RESOLVE_JAR_URI;
+  }
+
+  // 0 implies no content access.
+  return 0;
+}
+
 nsresult nsResProtocolHandler::GetSubstitutionInternal(const nsACString& aRoot,
-                                                       nsIURI** aResult,
-                                                       uint32_t* aFlags) {
+                                                       nsIURI** aResult) {
   nsAutoCString uri;
 
   if (!ResolveSpecialCases(aRoot, "/"_ns, "/"_ns, uri)) {
     return NS_ERROR_NOT_AVAILABLE;
   }
 
-  if (aRoot.Equals(kAndroid)) {
-    *aFlags = nsISubstitutingProtocolHandler::RESOLVE_JAR_URI;
-  } else {
-    *aFlags = 0;  // No content access.
-  }
   return NS_NewURI(aResult, uri);
 }
 
