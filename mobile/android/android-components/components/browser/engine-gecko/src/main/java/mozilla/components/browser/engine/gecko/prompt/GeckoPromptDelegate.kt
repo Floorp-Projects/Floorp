f083596d3554b5296b909afeef6fd88bd5622483
alexandra.virvara <avirvara@mozilla.com>
2024-05-08 07:57:27 +0000

    Bug 1893930: show the suggest string password prompt when tapping on the password field while being on a sign up page r=boek,android-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D208907

--- mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/prompt/GeckoPromptDelegate.kt
+++ mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/prompt/GeckoPromptDelegate.kt
@@ -282,17 +282,16 @@ internal class GeckoPromptDelegate(private val geckoEngineSession: GeckoEngineSe
         return geckoResult
     }
 
+    @Suppress("MaxLineLength")
     override fun onLoginSelect(
         session: GeckoSession,
         prompt: AutocompleteRequest<Autocomplete.LoginSelectOption>,
     ): GeckoResult<PromptResponse>? {
         val promptOptions = prompt.options
+
         val generatedPassword =
-            if (promptOptions.isNotEmpty() && promptOptions.first().hint == Autocomplete.SelectOption.Hint.GENERATED) {
-                promptOptions.first().value.password
-            } else {
-                null
-            }
+            promptOptions.firstOrNull { option -> option.hint == Autocomplete.SelectOption.Hint.GENERATED }?.value?.password
+
         val geckoResult = GeckoResult<PromptResponse>()
         val onConfirmSelect: (Login) -> Unit = { login ->
             if (!prompt.isComplete) {
