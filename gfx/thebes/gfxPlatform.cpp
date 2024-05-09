ca914eeed57286ce2739314dcfead3983568e1c8
Cristian Tuns <ctuns@mozilla.com>
2024-05-09 17:14:40 -0400

    Backed out changeset 51e29aa932f4 (bug 1896006) for causing build bustages in gfxPlatform.cpp CLOSED TREE

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -874,7 +874,8 @@ void gfxPlatform::Init() {
     // Prefs that don't fit into any of the other sections
     forcedPrefs.AppendPrintf("-T%d%d%d) ",
                              StaticPrefs::gfx_android_rgb16_force_AtStartup(),
-                             StaticPrefs::gfx_canvas_accelerated());
+                             StaticPrefs::gfx_canvas_accelerated(),
+                             StaticPrefs::layers_force_shmem_tiles_AtStartup());
     ScopedGfxFeatureReporter::AppNote(forcedPrefs);
   }
 
