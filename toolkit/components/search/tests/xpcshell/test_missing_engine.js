efb14fc8dd8d6150625af886228f015352b34171
Mark Banner <standard8@mozilla.com>
2024-05-09 15:34:58 +0000

    Bug 1895718 - Fix test_missing_engine.js to work when old search configuration is disabled. r=search-reviewers,jteow
    Also changes it to only run with the old search configuration, as the test is to do with the built-in WebExtensions which is not
    relevant to the new search configuration.
    
    Differential Revision: https://phabricator.services.mozilla.com/D209931

--- toolkit/components/search/tests/xpcshell/test_missing_engine.js
+++ toolkit/components/search/tests/xpcshell/test_missing_engine.js
@@ -2,7 +2,8 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
 // This test is designed to check the search service keeps working if there's
-// a built-in engine missing from the configuration.
+// an application provided WebExtension missing that is referenced from the
+// configuration. Only applies to the old search configuration.
 
 "use strict";
 
@@ -55,64 +56,16 @@ const BAD_CONFIG = [
   },
 ];
 
-const CONFIG_V2 = [
-  {
-    recordType: "engine",
-    identifier: "engine",
-    base: {
-      name: "Test search engine",
-      urls: {
-        search: {
-          base: "https://www.google.com/search",
-          params: [
-            {
-              name: "channel",
-              searchAccessPoint: {
-                addressbar: "fflb",
-                contextmenu: "rcs",
-              },
-            },
-          ],
-          searchTermParamName: "q",
-        },
-        suggestions: {
-          base: "https://suggestqueries.google.com/complete/search?output=firefox&client=firefox&hl={moz:locale}",
-          searchTermParamName: "q",
-        },
-      },
-    },
-    variants: [
-      {
-        environment: { allRegionsAndLocales: true },
-      },
-    ],
-  },
-  {
-    recordType: "defaultEngines",
-    specificDefaults: [],
-  },
-  {
-    recordType: "engineOrders",
-    orders: [],
-  },
-];
-
 add_setup(async function () {
   SearchTestUtils.useMockIdleService();
   await AddonTestUtils.promiseStartupManager();
 
   // This test purposely attempts to load a missing engine.
-  consoleAllowList.push(
-    "Could not load engine engine-missing@search.mozilla.org"
-  );
+  consoleAllowList.push("Could not load engine");
 });
 
 add_task(async function test_startup_with_missing() {
-  await SearchTestUtils.useTestEngines(
-    "data",
-    null,
-    SearchUtils.newSearchConfigEnabled ? CONFIG_V2 : BAD_CONFIG
-  );
+  await SearchTestUtils.useTestEngines("data", null, BAD_CONFIG);
 
   const result = await Services.search.init();
   Assert.ok(
@@ -135,7 +88,7 @@ add_task(async function test_update_with_missing() {
 
   await RemoteSettings(SearchUtils.SETTINGS_KEY).emit("sync", {
     data: {
-      current: SearchUtils.newSearchConfigEnabled ? CONFIG_V2 : GOOD_CONFIG,
+      current: GOOD_CONFIG,
     },
   });
 
@@ -156,7 +109,7 @@ add_task(async function test_update_with_missing() {
 
   await RemoteSettings(SearchUtils.SETTINGS_KEY).emit("sync", {
     data: {
-      current: SearchUtils.newSearchConfigEnabled ? CONFIG_V2 : BAD_CONFIG,
+      current: BAD_CONFIG,
     },
   });
 
