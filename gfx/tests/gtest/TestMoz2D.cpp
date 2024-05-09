e2df7431fc9be0f88495cfa1f27cea399c3e95d1
flowejam <91450942+flowejam@users.noreply.github.com>
2024-05-09 00:02:55 +0000

    Bug 1843950: Change BasePoint::Length() return type to avoid truncation to int. r=botond
    Created a new metafunction, FloatType, which will prevent truncation of the
    decimal portion of the Length method result. Added GTest unit tests, and converted
    old unit tests in gfx/2d/unittest/TestPoint.cpp to gtests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D208306

--- gfx/tests/gtest/TestMoz2D.cpp
+++ gfx/tests/gtest/TestMoz2D.cpp
@@ -6,23 +6,12 @@
 
 #include "gtest/gtest.h"
 #include "TestBase.h"
-#include "TestPoint.h"
 #include "TestScaling.h"
 #include "TestBugs.h"
 
 TEST(Moz2D, Bugs)
 {
   TestBugs* test = new TestBugs();
-  int failures = 0;
-  test->RunTests(&failures);
-  delete test;
-
-  ASSERT_EQ(failures, 0);
-}
-
-TEST(Moz2D, Point)
-{
-  TestBase* test = new TestPoint();
   int failures = 0;
   test->RunTests(&failures);
   delete test;
