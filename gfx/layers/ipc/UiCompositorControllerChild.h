37d94d4a2df72ec0c420d055f4d4c344e1df668f
Jamie Nicol <jnicol@mozilla.com>
2024-05-13 16:52:13 +0000

    Bug 1880503 - Handle sync IPC timeout in UiCompositorControllerChild. r=aosmond
    Extend the sync IPC timeout mechanism in CompositorManagerChild to
    additionally cover UiCompositorControllerChild. As
    UiCompositorControllerChild runs on the Android UI thread, we ensure
    GPUProcessManager::KillProcess dispatches to the gecko main thread.
    
    Along with the previous patch in this series this should provide us
    with crash reports when the Android UI thread is hung waiting for the
    GPU process to reply.
    
    Differential Revision: https://phabricator.services.mozilla.com/D202167

--- gfx/layers/ipc/UiCompositorControllerChild.h
+++ gfx/layers/ipc/UiCompositorControllerChild.h
@@ -94,6 +94,9 @@ class UiCompositorControllerChild final
   void OpenForGPUProcess(Endpoint<PUiCompositorControllerChild>&& aEndpoint);
   void SendCachedValues();
 
+  void SetReplyTimeout();
+  bool ShouldContinueFromReplyTimeout() override;
+
   bool mIsOpen;
   uint64_t mProcessToken;
   Maybe<gfx::IntRect> mResize;
