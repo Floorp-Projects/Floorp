7fa86d813e2228daa7283683d8585559777ee375
Masayuki Nakano <masayuki@d-toybox.com>
2024-05-14 01:08:37 +0000

    Bug 1840568 - Make `TSFTextStore` allow to flush layout at query content only when it's not called recursively r=m_kato
    By default, query content allows to flushing layout.  In most cases, this should
    not cause recursive call of the caching content/selection methods.  However,
    according to a crash report, this may occur with no chance to avoid the loop
    from `TSFTextStore` point of view.  Fortunately, the recursive call may happen
    only when an element in chrome in the main process has focus.  Therefore, I
    think that we can keep current `MOZ_DIAGNOSTIC_ASSERT`s in the methods, but the
    nested call should not allow to flush pending layout to avoid an infinite loop
    in the release channel.
    
    Differential Revision: https://phabricator.services.mozilla.com/D209992

--- widget/windows/TSFTextStore.h
+++ widget/windows/TSFTextStore.h
@@ -1003,7 +1003,9 @@ class TSFTextStore final : public ITextStoreACP,
 
   // While mContentForTSF is valid, this returns the text stored by it.
   // Otherwise, return the current text content retrieved by eQueryTextContent.
-  bool GetCurrentText(nsAString& aTextContent);
+  enum class AllowToFlushLayoutIfNoCache { No, Yes };
+  bool GetCurrentText(nsAString& aTextContent,
+                      AllowToFlushLayoutIfNoCache aAllowToFlushLayoutIfNoCache);
 
   class MouseTracker final {
    public:
