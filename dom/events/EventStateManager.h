7ed8d5df0b247d4e9407af588a966b06bcfcf509
Masayuki Nakano <masayuki@d-toybox.com>
2024-05-14 01:37:35 +0000

    Bug 1888018 - part 3: Make `OverOutElementsWrapper` restore original `mouseover` target if it's removed temporarily r=smaug
    Chromium stores the last "over" event target before dispatching the boundary
    events [1][2].  However, Chrome and Safari do not fire mouse boundary events
    when the last over target is removed temporarily.  Therefore, I think that
    they consider whether the target is removed not immediately after removing
    the node.  Currently, we consider whether we should dispatch mouse boundary
    events for the layout/DOM changes with next mouse event or next synthesized
    `eMouseMove` which is enqueued when the last `mouseover` target is removed [3]
    (i.e., it's considered at latest next animation frame after removing the
    target).  Therefore, we can restore the original `mouseover` target if it's
    reconnected to the DOM again before that.  Although the timing of considering
    that could be incompatible with the other browsers in strictly speaking, but I
    think that this is enough because the new tests pass on Firefox and Chrome and
    I don't have any ideas about more time-sensitive test cases.
    
    About `pointerover`, we don't need to change the behavior because it's well
    defined by the Pointer Events spec and the behavior is compatible between
    browsers.
    
    Note that the new test,
    `mouse_boundary_events_after_reappending_last_over_target.tentative.html` is not
    compatible with the expectation of mouse boundary events in
    `pointerevent_after_target_appended.html` which is in the scope of Interop-2024.
    Therefore, there are some new failures of its result (note that the tests were
    not passed before D202907 (D202376 is adding the new behavior and it's enabled
    by D202907).  The new failure results are, we stop dispatching `mouseover` and
    `mouseenter` after `mousedown` or `mouseup` whose event listener moves its event
    target because it's reconnected to different position in same parent immediately
    and element under the cursor is not changed by the temporary removal.  Safari
    behaves same as our new behavior [4][5] and this is consistent with the result
    of new tests.  Therefore, I think that Chrome should fix it, so I'll file a new
    spec issue about this to discuss what's the best.
    
    (Chrome passes most tests in
    `mouse_boundary_events_after_reappending_last_over_target.tentative.html`.
    They fail the cases moving the last `mouseover` event target outside the deepest
    connected `mouseenter` event target but it stays under the cursor because they
    don't dispatch mouse boundary events and fails.  This means that they won't
    dispatch `mouseleave` event on the ex-parent of the target.  And also they fail
    the cases moving the `mouseover` event target outside the deepest connected
    `mouseenter` event target and changing the element under the cursor because
    they dispatch `mouseout` and `mouseleave` on the moved target.  Finally, they
    fail the case re-appending the target in a next animation after removal because
    they dispatch redundant `mouseenter` events on all ancestors.)
    
    1. https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/input/mouse_event_manager.cc;l=415;drc=5742c2e870cdd5e453212d92f13a854993cd60bf
    2. https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/input/mouse_event_manager.cc;l=451;drc=5742c2e870cdd5e453212d92f13a854993cd60bf
    3. https://searchfox.org/mozilla-central/rev/294e1fbdcc9ca0c328c372392e03bb49df4ee77e/dom/events/EventStateManager.cpp#6273-6276
    4. https://wpt.fyi/results/pointerevents/pointerevent_after_target_appended.html%3Fmouse?run_id=6221043298992128&run_id=5090975864586240&run_id=5106342989135872&run_id=5101843876675584
    5. https://wpt.fyi/results/pointerevents/pointerevent_after_target_appended.html%3Ftouch?run_id=6221043298992128&run_id=5090975864586240&run_id=5106342989135872&run_id=5101843876675584
    
    Differential Revision: https://phabricator.services.mozilla.com/D205536

--- dom/events/EventStateManager.h
+++ dom/events/EventStateManager.h
@@ -118,12 +118,42 @@ class OverOutElementsWrapper final : public nsISupports {
                : nullptr;
   }
 
+  /**
+   * Called when EventStateManager::PreHandleEvent() receives an event which
+   * should be treated as the deadline to restore the last "over" event target
+   * as the next "out" event target and for avoiding to dispatch redundant
+   * "over" event on the same target again when it was removed but reconnected.
+   * If the last "over" event target was reconnected under the last deepest
+   * "enter" event target, this restores the last "over" event target.
+   * Otherwise, makes the instance forget the last "over" target because the
+   * user maybe has seen that the last "over" target is completely removed from
+   * the tree.
+   *
+   * @param aEvent      The event which the caller received.  If this is set to
+   *                    nullptr or not a mouse event, this forgets the pending
+   *                    last "over" event target.
+   */
+  void TryToRestorePendingRemovedOverTarget(const WidgetEvent* aEvent);
+
+  /**
+   * Return true if we have a pending removing last "over" event target at least
+   * for the weak reference to it.  In other words, when this returns true, we
+   * need to handle the pending removing "over" event target.
+   */
+  [[nodiscard]] bool MaybeHasPendingRemovingOverEventTarget() const {
+    return mPendingRemovingOverEventTarget;
+  }
+
  private:
   /**
    * Whether the last "over" event target is the target of "out" event if you
    * dispatch "out" event.
    */
   [[nodiscard]] bool LastOverEventTargetIsOutEventTarget() const {
+    MOZ_ASSERT_IF(mDeepestEnterEventTargetIsOverEventTarget,
+                  mDeepestEnterEventTarget);
+    MOZ_ASSERT_IF(mDeepestEnterEventTargetIsOverEventTarget,
+                  !MaybeHasPendingRemovingOverEventTarget());
     return mDeepestEnterEventTargetIsOverEventTarget;
   }
 
@@ -131,11 +161,28 @@ class OverOutElementsWrapper final : public nsISupports {
       nsIContent* aOverEventTargetAndDeepestEnterEventTarget);
   void UpdateDeepestEnterEventTarget(nsIContent* aDeepestEnterEventTarget);
 
+  nsCOMPtr<nsIContent> GetPendingRemovingOverEventTarget() const {
+    nsCOMPtr<nsIContent> pendingRemovingOverEventTarget =
+        do_QueryReferent(mPendingRemovingOverEventTarget);
+    return pendingRemovingOverEventTarget.forget();
+  }
+
   // The deepest event target of the last "enter" event.  If
   // mDeepestEnterEventTargetIsOverEventTarget is true, this is the last "over"
   // event target too.  If it's set to false, this is an ancestor of the last
-  // "over" event target which has not been removed from the DOM tree.
+  // "over" event target which is not removed from the DOM tree.
   nsCOMPtr<nsIContent> mDeepestEnterEventTarget;
+
+  // The last "over" event target which will be considered as disconnected or
+  // connected later because web apps may remove the "over" event target
+  // temporarily and reconnect it to the deepest "enter" target immediately.
+  // In such case, we should keep treating it as the last "over" event target
+  // as the next "out" event target.
+  // FYI: This needs to be a weak pointer.  Otherwise, the leak window checker
+  // of mochitests will detect windows in the closed tabs which ran tests
+  // synthesizing mouse moves because while a <browser> is stored with a strong
+  // pointer, the child window is also grabbed by the element.
+  nsWeakPtr mPendingRemovingOverEventTarget;
 
   // While we're dispatching "over" and "enter" events, this is set to the
   // "over" event target.  If it's removed from the DOM tree, this is set to
@@ -158,7 +205,7 @@ class OverOutElementsWrapper final : public nsISupports {
   // to false.  Then, mDeepestEnterEventTarget may be an ancestor of the
   // "over" element which should be the deepest target of next "leave"
   // element but shouldn't be target of "out" event.
-  bool mDeepestEnterEventTargetIsOverEventTarget = true;
+  bool mDeepestEnterEventTargetIsOverEventTarget = false;
 };
 
 class EventStateManager : public nsSupportsWeakReference, public nsIObserver {
