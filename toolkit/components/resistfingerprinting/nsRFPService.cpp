ed94e02658d9c6453f905ebd58e41912a25b8716
Fatih <fkilic@mozilla.com>
2024-05-14 17:55:52 +0000

    Bug 1896372: Extract defaults to RFPTargetsDefault.inc. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D210167

--- toolkit/components/resistfingerprinting/nsRFPService.cpp
+++ toolkit/components/resistfingerprinting/nsRFPService.cpp
@@ -126,12 +126,21 @@ static constexpr uint32_t kVideoDroppedRatio = 5;
 // Fingerprinting protections that are enabled by default. This can be
 // overridden using the privacy.fingerprintingProtection.overrides pref.
 #if defined(MOZ_WIDGET_ANDROID)
-const RFPTarget kDefaultFingerprintingProtections =
-    RFPTarget::CanvasRandomization;
+// NOLINTNEXTLINE(bugprone-macro-parentheses)
+#  define ANDROID_DEFAULT(name) RFPTarget::name |
+#  define DESKTOP_DEFAULT(name)
 #else
-const RFPTarget kDefaultFingerprintingProtections =
-    RFPTarget::CanvasRandomization | RFPTarget::FontVisibilityLangPack;
+#  define ANDROID_DEFAULT(name)
+// NOLINTNEXTLINE(bugprone-macro-parentheses)
+#  define DESKTOP_DEFAULT(name) RFPTarget::name |
 #endif
+
+const RFPTarget kDefaultFingerprintingProtections =
+#include "RFPTargetsDefault.inc"
+    static_cast<RFPTarget>(0);
+
+#undef ANDROID_DEFAULT
+#undef DESKTOP_DEFAULT
 
 static constexpr uint32_t kSuspiciousFingerprintingActivityThreshold = 1;
 
