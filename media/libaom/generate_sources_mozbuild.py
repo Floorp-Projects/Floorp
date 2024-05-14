5fe4d17f357a43b72e7f1483c5ee5ee0bfe2236a
Chun-Min Chang <chun.m.chang@gmail.com>
2024-05-14 00:56:39 +0000

    Bug 1895635 - Fix syntax warning in libaom update script r=media-playback-reviewers,padenot
    This patch fixes the syntax warning below:
    ```
    SyntaxWarning: "is" with a literal. Did you mean "=="?
    ```
    in the generate_sources_mozbuild.py
    
    Differential Revision: https://phabricator.services.mozilla.com/D209994

--- media/libaom/generate_sources_mozbuild.py
+++ media/libaom/generate_sources_mozbuild.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
 
         # Windows x86_64 needs this -- all other source files are shared
         # between OSes
-        if cpu is 'x86_64' and system is 'win':
+        if cpu == 'x86_64' and system == 'win':
           f.write('  \'X64_WIN_SOURCES\': [\n')
           f.write('    \'%s\',\n' % variables['AOM_PORTS_ASM_X86'])
           f.write("  ],\n")
