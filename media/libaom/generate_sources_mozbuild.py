4733afc6e6dd975daeebf674984bbd95e3918d55
Chun-Min Chang <chun.m.chang@gmail.com>
2024-05-14 00:56:40 +0000

    Bug 1895635 - Generate optional arguments for arm64 rtcd headers by aom_config.h r=media-playback-reviewers,alwu
    This patch introduces a script to generate optional arguments for arm64
    rtcd headers, utilizing the configurations specified in the associated
    aom_config.h file.
    
    Depends on D209995
    
    Differential Revision: https://phabricator.services.mozilla.com/D210227

--- media/libaom/generate_sources_mozbuild.py
+++ media/libaom/generate_sources_mozbuild.py
@@ -24,13 +24,6 @@ def write_aom_config(system, arch, variables, cache_variables):
     # inherit this from the mozilla build config
     cache_variables.remove('HAVE_PTHREAD_H')
 
-    if arch == 'arm64':
-        print("Please ensure the generated arm64 rtcd headers follow the feature settings here")
-        # The features below are copied from the "ARM64_FLAVORS" in AOM_DIR/build/cmake/cpu.cmake.
-        arm64_flavors = ["NEON", "ARM_CRC32", "NEON_DOTPROD", "NEON_I8MM", "SVE", "SVE2"]
-        for flavor in arm64_flavors:
-            print("ENABLE_{}: {}, HAVE_{}: {}".format(flavor, variables['ENABLE_' + flavor], flavor, variables['HAVE_' + flavor]))
-
     outdir = os.path.join('config', system, arch, 'config')
     try:
         os.makedirs(outdir)
