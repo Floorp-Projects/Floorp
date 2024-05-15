74d14ae4590d3ac783e45addc8410ea254d38db4
Nipun Shukla <nshukla@mozilla.com>
2024-05-15 15:34:43 +0000

    Bug 1893341 - Change Firefox launch on login to use Windows StartupTask functions on MSIX r=nalexander,nrishel,settings-reviewers,omc-reviewers,firefox-desktop-core-reviewers ,aminomancer
    Differential Revision: https://phabricator.services.mozilla.com/D208567

--- browser/components/preferences/tests/browser_windows_launch_on_login.js
+++ browser/components/preferences/tests/browser_windows_launch_on_login.js
@@ -18,7 +18,25 @@ const { ExperimentFakes } = ChromeUtils.importESModule(
   "resource://testing-common/NimbusTestUtils.sys.mjs"
 );
 
-add_task(async function test_check_checkbox() {
+const { MockRegistry } = ChromeUtils.importESModule(
+  "resource://testing-common/MockRegistry.sys.mjs"
+);
+
+let registry = null;
+add_setup(() => {
+  registry = new MockRegistry();
+  registry.setValue(
+    Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+    "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",
+    "",
+    ""
+  );
+  registerCleanupFunction(() => {
+    registry.shutdown();
+  });
+});
+
+add_task(async function test_check_uncheck_checkbox() {
   await ExperimentAPI.ready();
   let doCleanup = await ExperimentFakes.enrollWithFeatureConfig({
     featureId: "windowsLaunchOnLogin",
@@ -40,25 +58,6 @@ add_task(async function test_check_checkbox() {
       "Key exists"
     );
 
-    gBrowser.removeCurrentTab();
-  });
-  await doCleanup();
-});
-
-add_task(async function test_uncheck_checkbox() {
-  await ExperimentAPI.ready();
-  let doCleanup = await ExperimentFakes.enrollWithFeatureConfig({
-    featureId: "windowsLaunchOnLogin",
-    value: { enabled: true },
-  });
-  await WindowsLaunchOnLogin.withLaunchOnLoginRegistryKey(async wrk => {
-    // Open preferences to general pane
-    await openPreferencesViaOpenPreferencesAPI("paneGeneral", {
-      leaveOpen: true,
-    });
-    let doc = gBrowser.contentDocument;
-
-    let launchOnLoginCheckbox = doc.getElementById("windowsLaunchOnLogin");
     launchOnLoginCheckbox.click();
     ok(!launchOnLoginCheckbox.checked, "Autostart checkbox unchecked");
 
@@ -73,12 +72,20 @@ add_task(async function test_uncheck_checkbox() {
 });
 
 add_task(async function create_external_regkey() {
+  if (Services.sysinfo.getProperty("hasWinPackageId")) {
+    return;
+  }
   await ExperimentAPI.ready();
   let doCleanup = await ExperimentFakes.enrollWithFeatureConfig({
     featureId: "windowsLaunchOnLogin",
     value: { enabled: true },
   });
   await WindowsLaunchOnLogin.withLaunchOnLoginRegistryKey(async wrk => {
+    // Delete any existing entries before testing
+    // Both functions are install specific so it's safe to run them
+    // like this.
+    wrk.removeValue(WindowsLaunchOnLogin.getLaunchOnLoginRegistryName());
+    await WindowsLaunchOnLogin.removeLaunchOnLoginShortcuts();
     // Create registry key without using settings pane to check if
     // this is reflected in the settings
     let autostartPath =
@@ -108,6 +115,9 @@ add_task(async function create_external_regkey() {
 });
 
 add_task(async function delete_external_regkey() {
+  if (Services.sysinfo.getProperty("hasWinPackageId")) {
+    return;
+  }
   await ExperimentAPI.ready();
   let doCleanup = await ExperimentFakes.enrollWithFeatureConfig({
     featureId: "windowsLaunchOnLogin",
@@ -136,6 +146,7 @@ add_task(async function delete_external_regkey() {
 });
 
 registerCleanupFunction(async function () {
+  await WindowsLaunchOnLogin.removeLaunchOnLoginShortcuts();
   await WindowsLaunchOnLogin.withLaunchOnLoginRegistryKey(async wrk => {
     let registryName = WindowsLaunchOnLogin.getLaunchOnLoginRegistryName();
     if (wrk.hasValue(registryName)) {
