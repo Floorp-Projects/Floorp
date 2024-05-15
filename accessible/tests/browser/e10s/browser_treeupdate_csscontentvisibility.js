10f5c21b0ecaa44cdd80efc3930159d006704ad4
Emilio Cobos Álvarez <emilio@crisal.io>
2024-05-15 09:36:52 +0000

    Bug 1896763 - Make display observer timing happen after all layout changes are possibly done. r=smaug,Jamie
    Not sure how testable this is, for a11y purposes. The other consumer is
    some XRSession stuff which doesn't seem to care too much about the
    timing of this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D210384

--- accessible/tests/browser/e10s/browser_treeupdate_csscontentvisibility.js
+++ accessible/tests/browser/e10s/browser_treeupdate_csscontentvisibility.js
@@ -56,23 +56,7 @@ add_setup(async function () {
 });
 
 async function setContentVisibility(browser, value) {
-  let mutationPromise = (() => {
-    switch (value) {
-      case "hidden":
-        return waitForEvent(EVENT_REORDER, "target");
-      case "auto":
-        return waitForEvents({
-          expected: [
-            [EVENT_REORDER, "child"],
-            [EVENT_REORDER, "content-child"],
-            [EVENT_REORDER, "shadowDiv"],
-            [EVENT_REORDER, "target"],
-          ],
-        });
-      default:
-        throw new Error(`unexpected content-visibility: ${value}`);
-    }
-  })();
+  let mutationPromise = waitForEvent(EVENT_REORDER, "target");
 
   // Change the value of `content-visibility` property for the target
   info(`Setting content-visibility: ${value}`);
