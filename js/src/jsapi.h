4073d0bdf256d05011c3e0f8395dfd901297c372
Daniel Minor <dminor@mozilla.com>
2024-05-15 19:26:47 +0000

    Bug 1826573 - Add pref for duplicate named capture groups; r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D209744

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -861,7 +861,8 @@ extern JS_PUBLIC_API void JS_SetOffthreadIonCompilationEnabled(JSContext* cx,
   Register(WASM_FOLD_OFFSETS, "wasm.fold-offsets") \
   Register(WASM_DELAY_TIER2, "wasm.delay-tier2") \
   Register(WASM_JIT_BASELINE, "wasm.baseline") \
-  Register(WASM_JIT_OPTIMIZING, "wasm.optimizing")  // clang-format on
+  Register(WASM_JIT_OPTIMIZING, "wasm.optimizing") \
+  Register(REGEXP_DUPLICATE_NAMED_GROUPS, "regexp.duplicate-named-groups")  // clang-format on
 
 typedef enum JSJitCompilerOption {
 #define JIT_COMPILER_DECLARE(key, str) JSJITCOMPILER_##key,
