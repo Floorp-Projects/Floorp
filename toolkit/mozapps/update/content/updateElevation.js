e58f96019c1af14e377bda463e53353495569d2e
Robin Steuber <bytesized@mozilla.com>
2024-05-15 17:06:13 +0000

    Bug 1875502 - Pre: Make AUS's readyUpdate async r=nalexander,settings-reviewers,application-update-reviewers
    Aside from tests (which will be in the next patch in the stack), this specifically leaves two instances of readyUpdate unchanged - the one in BrowserContentHandler's `getArgs` and the one in UpdatePing's `_getActiveUpdate`. In both of these cases what is actually wanted is the update that was just installed. It's only because of the (fairly arbitrary) order that things are started up in that `readyUpdate` still happens to contain this value when these things run. Later in this patch stack we will add something more appropriate for this. But in the meantime, it doesn't make sense to convert functions to be asynchronous that will not ultimately need to be asynchronous.
    
    Differential Revision: https://phabricator.services.mozilla.com/D209115

--- toolkit/mozapps/update/content/updateElevation.js
+++ toolkit/mozapps/update/content/updateElevation.js
@@ -30,7 +30,7 @@ const gUpdateElevationDialog = {
     button.label = label;
     button.setAttribute("accesskey", this.getAUSString(string + ".accesskey"));
   },
-  onLoad() {
+  async onLoad() {
     this.strings = document.getElementById("updateStrings");
     this.brandName = document
       .getElementById("brandStrings")
@@ -39,7 +39,7 @@ const gUpdateElevationDialog = {
     let um = Cc["@mozilla.org/updates/update-manager;1"].getService(
       Ci.nsIUpdateManager
     );
-    let update = um.readyUpdate;
+    let update = await um.getReadyUpdate();
     let updateFinishedName = document.getElementById("updateFinishedName");
     updateFinishedName.value = update.name;
 
