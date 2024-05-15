03f7bd9e4abf77b0e2073187fa419db3edd1a7de
Jonathan Kew <jkew@mozilla.com>
2024-05-15 10:39:38 +0000

    Bug 1896117 - Let the unicode-bidi implementation of the intl::Bidi component ride the train to release. r=platform-i18n-reviewers,nordzilla
    Differential Revision: https://phabricator.services.mozilla.com/D210039

--- intl/components/src/Bidi.h
+++ intl/components/src/Bidi.h
@@ -7,14 +7,10 @@
 #include "mozilla/intl/BidiEmbeddingLevel.h"
 #include "mozilla/intl/ICU4CGlue.h"
 
-// Restrict use of the Rust unicode-bidi implementation to Nightly builds,
-// pending investigation of perf regressions; Beta/Release builds will
-// continue to use the ICU4C implementation for now.
-#ifdef NIGHTLY_BUILD
-#  define USE_RUST_UNICODE_BIDI 1
-#else
-#  define USE_RUST_UNICODE_BIDI 0
-#endif
+// Use the Rust unicode-bidi crate to back the Bidi component.
+// (Define to 0 to use the legacy ICU4C implementation instead,
+// until that code is removed altogether.)
+#define USE_RUST_UNICODE_BIDI 1
 
 #if USE_RUST_UNICODE_BIDI
 #  include "mozilla/intl/unicode_bidi_ffi_generated.h"
