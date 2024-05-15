af3d72cdf8afd2315423343ac1f09a5095e7bd85
Tim Huang <tihuang@mozilla.com>
2024-05-15 16:48:58 +0000

    Bug 1885936 - Part 2: Add data collecting functions to UserCharacteristicsChild. r=tschuster
    We might need to call ChromeOnly functions to collect user
    characteristics data. To achieve this, this patches introduces two
    functions to UserCharacteristicsChild, collectUserCharacteristicsData()
    and collectUserCharacteristicsDataWithDelay() for different collecting
    timing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D207980

--- toolkit/actors/UserCharacteristicsChild.sys.mjs
+++ toolkit/actors/UserCharacteristicsChild.sys.mjs
@@ -5,6 +5,10 @@
 
 const lazy = {};
 
+ChromeUtils.defineESModuleGetters(lazy, {
+  setTimeout: "resource://gre/modules/Timer.sys.mjs",
+});
+
 ChromeUtils.defineLazyGetter(lazy, "console", () => {
   return console.createInstance({
     prefix: "UserCharacteristicsPage",
@@ -13,14 +17,52 @@ ChromeUtils.defineLazyGetter(lazy, "console", () => {
 });
 
 export class UserCharacteristicsChild extends JSWindowActorChild {
-  handleEvent(event) {
+  /**
+   * A placeholder for the collected data.
+   *
+   * @typedef {Object} userDataDetails
+   *   @property {string} debug - The debug messages.
+   *   @property {Object} output - The user characteristics data.
+   */
+  userDataDetails;
+
+  collectingDelay = 1000; // Collecting delay for 1000 ms.
+
+  // Please add data collection here if the collection requires privilege
+  // access, such as accessing ChromeOnly functions. The function is called
+  // right after the content page finishes.
+  async collectUserCharacteristicsData() {
+    lazy.console.debug("Calling collectUserCharacteristicsData()");
+  }
+
+  // This function is similar to the above function, but we call this function
+  // with a delay after the content page finished. This function is for data
+  // that requires loading time.
+  async collectUserCharacteristicsDataWithDelay() {
+    lazy.console.debug("Calling collectUserCharacteristicsDataWithDelay()");
+  }
+
+  async handleEvent(event) {
     lazy.console.debug("Got ", event.type);
     switch (event.type) {
       case "UserCharacteristicsDataDone":
+        this.userDataDetails = event.detail;
+
+        await this.collectUserCharacteristicsData();
+
+        await new Promise(resolve => {
+          lazy.setTimeout(resolve, this.collectingDelay);
+        });
+
+        await this.collectUserCharacteristicsDataWithDelay();
+
         lazy.console.debug("creating IdleDispatch");
         ChromeUtils.idleDispatch(() => {
           lazy.console.debug("sending PageReady");
-          this.sendAsyncMessage("UserCharacteristics::PageReady", event.detail);
+          this.sendAsyncMessage(
+            "UserCharacteristics::PageReady",
+            this.userDataDetails
+          );
         });
         break;
     }
