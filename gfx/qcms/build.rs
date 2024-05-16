3f7b25f6d9b59deed9695a06dc2fce40532f1dc7
Mike Hommey <mh+mozilla@glandium.org>
2024-05-16 01:58:17 +0000

    Bug 1897029 - Add rustc-check-cfg to qcms for rustc 1.80. r=gfx-reviewers,jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D210590

--- gfx/qcms/build.rs
+++ gfx/qcms/build.rs
@@ -1,7 +1,10 @@
 extern crate version_check as rustc;
 
 fn main() {
+    if rustc::is_min_version("1.80.0").unwrap_or(false) {
+        println!("cargo::rustc-check-cfg=cfg(stdsimd_split)");
+    }
     if rustc::is_min_version("1.78.0").unwrap_or(false) {
-        println!("cargo:rustc-cfg=stdsimd_split");
+        println!("cargo::rustc-cfg=stdsimd_split");
     }
 }
