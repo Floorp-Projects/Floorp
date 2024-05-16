89b510e20d5ba86859342ad7f6018d8e3b799e0b
Robin Steuber <bytesized@mozilla.com>
2024-05-16 20:01:50 +0000

    Bug 1875502 - Pre: Make testPostUpdateProcessing async r=nalexander,application-update-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D209119

--- toolkit/mozapps/update/tests/data/shared.js
+++ toolkit/mozapps/update/tests/data/shared.js
@@ -176,7 +176,7 @@ function waitForEvent(topic, status = null) {
 }
 
 /* Triggers post-update processing */
-function testPostUpdateProcessing() {
+async function testPostUpdateProcessing() {
   gAUS.observe(null, "test-post-update-processing", "");
 }
 
