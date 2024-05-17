35f34ff785e49ecea8f18973e4eeaf0fe3b65f26
Julien Cristau <jcristau@mozilla.com>
2024-05-17 14:09:22 +0000

    Bug 1841030 - cache the thread id in thread-local storage r=profiler-reviewers,canaltinova
    Differential Revision: https://phabricator.services.mozilla.com/D205838

--- tools/profiler/core/ProfilerUtils.cpp
+++ tools/profiler/core/ProfilerUtils.cpp
@@ -67,8 +67,12 @@ ProfilerThreadId profiler_current_thread_id() {
 #    include <sys/syscall.h>
 
 ProfilerThreadId profiler_current_thread_id() {
-  // glibc doesn't provide a wrapper for gettid() until 2.30
-  return ProfilerThreadId::FromNativeId(syscall(SYS_gettid));
+  static thread_local pid_t tid;
+  if (!tid) {
+    // glibc doesn't provide a wrapper for gettid() until 2.30
+    tid = static_cast<pid_t>(syscall(SYS_gettid));
+  }
+  return ProfilerThreadId::FromNativeId(tid);
 }
 
 // ------------------------------------------------------- FreeBSD
