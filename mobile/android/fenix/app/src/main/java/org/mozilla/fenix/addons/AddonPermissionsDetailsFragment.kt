ec71a8838c440bfbfb95b93bba59e1279e2e8d82
Zac McKenney <zmckenney@mozilla.com>
2024-05-17 02:11:47 +0000

    Bug 1811563 - Redesign permissions screen for addons r=android-reviewers,amejiamarmol,gl
    Differential Revision: https://phabricator.services.mozilla.com/D205398

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/addons/AddonPermissionsDetailsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/addons/AddonPermissionsDetailsFragment.kt
@@ -4,31 +4,169 @@
 
 package org.mozilla.fenix.addons
 
-import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import mozilla.components.feature.addons.Addon
+import mozilla.components.feature.addons.Addon.Companion.isAllURLsPermission
 import mozilla.components.feature.addons.ui.translateName
 import org.mozilla.fenix.BrowserDirection
 import org.mozilla.fenix.HomeActivity
-import org.mozilla.fenix.R
-import org.mozilla.fenix.databinding.FragmentAddOnPermissionsBinding
+import org.mozilla.fenix.addons.ui.AddonPermissionsScreen
+import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.showToolbar
+import org.mozilla.fenix.theme.FirefoxTheme
 
 /**
- * A fragment to show the permissions of an add-on.
+ * Optional [Addon.Permission.name] lists for requesting permission updates for an [Addon].
  */
-class AddonPermissionsDetailsFragment :
-    Fragment(R.layout.fragment_add_on_permissions),
-    AddonPermissionsDetailsInteractor {
+data class AddonPermissionsUpdateRequest(
+    val optionalPermissions: List<String> = emptyList(),
+    val originPermissions: List<String> = emptyList(),
+)
+
+/**
+ * A fragment to show and allow a user to change permissions for an addon.
+ */
+class AddonPermissionsDetailsFragment : Fragment() {
 
     private val args by navArgs<AddonPermissionsDetailsFragmentArgs>()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentAddOnPermissionsBinding.bind(view)
-        AddonPermissionDetailsBindingDelegate(binding, interactor = this).bind(args.addon)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+        setContent {
+            val optionalPermissions = rememberSaveable {
+                mutableStateOf(args.addon.translateOptionalPermissions(requireContext()))
+            }
+
+            val originPermissions = rememberSaveable {
+                mutableStateOf(
+                    args.addon.optionalOrigins
+                        .map {
+                            Addon.LocalizedPermission(it.name, it)
+                        },
+                )
+            }
+
+            // Note: Even if <all_urls> is in optionalPermissions of the extension manifest, it is found in
+            // originPermissions of the Addon
+            val allSitesHostPermissionsList = rememberSaveable {
+                mutableStateOf(
+                    args.addon.optionalOrigins.getAllSitesPermissionsList(),
+                )
+            }
+
+            // Update all of the mutable states when an addon is returned from updating permissions
+            val onUpdatePermissionsSuccess: (Addon) -> Unit = { updatedAddon ->
+                optionalPermissions.value =
+                    updatedAddon.translateOptionalPermissions(requireContext())
+                originPermissions.value = updatedAddon.optionalOrigins.map {
+                    Addon.LocalizedPermission(it.name, it)
+                }
+                allSitesHostPermissionsList.value =
+                    updatedAddon.optionalOrigins.getAllSitesPermissionsList()
+            }
+
+            FirefoxTheme {
+                AddonPermissionsScreen(
+                    permissions = args.addon.translatePermissions(requireContext()),
+                    optionalPermissions = optionalPermissions.value,
+                    originPermissions = originPermissions.value,
+                    isAllSitesSwitchVisible = allSitesHostPermissionsList.value.isNotEmpty(),
+                    isAllSitesEnabled = allSitesHostPermissionsList.value.getOrNull(0)?.granted
+                        ?: false,
+                    onAddOptionalPermissions = { permissionRequest ->
+                        addOptionalPermissions(permissionRequest, onUpdatePermissionsSuccess)
+                    },
+                    onRemoveOptionalPermissions = { permissionRequest ->
+                        removeOptionalPermission(permissionRequest, onUpdatePermissionsSuccess)
+                    },
+                    onAddAllSitesPermissions = {
+                        addOptionalPermissions(
+                            AddonPermissionsUpdateRequest(
+                                originPermissions = allSitesHostPermissionsList.value.map { it.name },
+                            ),
+                            onUpdatePermissionsSuccess,
+                        )
+                    },
+                    onRemoveAllSitesPermissions = {
+                        removeOptionalPermission(
+                            AddonPermissionsUpdateRequest(
+                                originPermissions = allSitesHostPermissionsList.value.map { it.name },
+                            ),
+                            onUpdatePermissionsSuccess,
+                        )
+                    },
+                    onLearnMoreClick = { learnMoreUrl ->
+                        openWebsite(learnMoreUrl)
+                    },
+                )
+            }
+        }
+    }
+
+    private fun addOptionalPermissions(
+        addPermissionsRequest: AddonPermissionsUpdateRequest,
+        onUpdatePermissionsSuccess: (Addon) -> Unit,
+    ) {
+        requireContext().components.addonManager.addOptionalPermission(
+            args.addon,
+            addPermissionsRequest.optionalPermissions,
+            addPermissionsRequest.originPermissions,
+            onSuccess = {
+                onUpdatePermissionsSuccess(it)
+            },
+            onError = {
+                /** No-Op **/
+            },
+        )
+    }
+
+    private fun removeOptionalPermission(
+        removePermissionsRequest: AddonPermissionsUpdateRequest,
+        onUpdatePermissionsSuccess: (Addon) -> Unit,
+    ) {
+        requireContext().components.addonManager.removeOptionalPermission(
+            args.addon,
+            removePermissionsRequest.optionalPermissions,
+            removePermissionsRequest.originPermissions,
+            onSuccess = {
+                onUpdatePermissionsSuccess(it)
+            },
+            onError = {
+                /** No-Op **/
+            },
+        )
+    }
+
+    private fun openWebsite(addonSiteUrl: String) {
+        (activity as HomeActivity).openToBrowserAndLoad(
+            searchTermOrURL = addonSiteUrl,
+            newTab = true,
+            from = BrowserDirection.FromAddonPermissionsDetailsFragment,
+        )
+    }
+
+    private fun <T : List<Addon.Permission>> T.getAllSitesPermissionsList(): List<Addon.Permission> {
+        return this.mapNotNull {
+            if (it.isAllURLsPermission()) {
+                it
+            } else {
+                null
+            }
+        }
     }
 
     override fun onResume() {
@@ -36,13 +174,5 @@ class AddonPermissionsDetailsFragment :
         context?.let {
             showToolbar(title = args.addon.translateName(it))
         }
-    }
-
-    override fun openWebsite(addonSiteUrl: Uri) {
-        (activity as HomeActivity).openToBrowserAndLoad(
-            searchTermOrURL = addonSiteUrl.toString(),
-            newTab = true,
-            from = BrowserDirection.FromAddonPermissionsDetailsFragment,
-        )
     }
 }
