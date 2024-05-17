608e693ad31fc1e5a7760f0afc90501efa30d4e0
James Teh <jteh@mozilla.com>
2024-05-17 00:24:14 +0000

    Bug 1896047 part 2: Introduce CssAltContent class to query alternative text specified in the CSS content property. r=eeejay
    This allows us to check for alt text and also to output all of it to a string.
    For now, this only supports plain strings, but this will be extended in a subsequent patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D210014

---
+++ accessible/base/CssAltContent.h
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef CssAltContent_h_
+#define CssAltContent_h_
+
+#include "nsStyleStruct.h"
+
+namespace mozilla::a11y {
+
+/**
+ * Queries alternative text specified in the CSS content property.
+ */
+class MOZ_STACK_CLASS CssAltContent {
+ public:
+  explicit CssAltContent(nsIContent* aContent);
+
+  /**
+   * Checks whether any CSS alt text has been specified. For example:
+   * if (CssAltContent(someContentNode)) ...
+   */
+  explicit operator bool() const { return !mItems.IsEmpty(); }
+
+  /**
+   * Append all CSS alt text to a string.
+   */
+  void AppendToString(nsAString& aOut);
+
+ private:
+  mozilla::Span<const mozilla::StyleContentItem> mItems;
+};
+
+}  // namespace mozilla::a11y
+
+#endif
