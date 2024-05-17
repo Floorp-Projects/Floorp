093f21ba43c7399dbce0af81ba1331d4e84066dd
Michael Froman <mfroman@mozilla.com>
2024-05-17 15:53:40 +0000

    Bug 1896162 - add default directory to process in commit-build-file-changes.sh r=dbaker DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D210056

--- dom/media/webrtc/third_party_build/commit-build-file-changes.sh
+++ dom/media/webrtc/third_party_build/commit-build-file-changes.sh
@@ -10,6 +10,11 @@ ERROR_HELP=""
 # Print an Error message if `set -eE` causes the script to exit due to a failed command
 trap 'show_error_msg $LINENO' ERR
 
+if [ "x$PROCESS_DIR" = "x" ]; then
+  PROCESS_DIR=third_party/libwebrtc
+  echo "Default directory: $PROCESS_DIR"
+fi
+
 # All commands should be printed as they are executed
 # set -x
 
@@ -19,7 +24,12 @@ trap 'show_error_msg $LINENO' ERR
 # * o pipefail: All stages of all pipes should succede.
 set -eEuo pipefail
 
-MOZ_BUILD_CHANGE_CNT=`hg status --include="**moz.build" third_party/libwebrtc | wc -l | tr -d " "`
+if [ ! -d $PROCESS_DIR ]; then
+  echo "No directory found for '$PROCESS_DIR'"
+  exit 1
+fi
+
+MOZ_BUILD_CHANGE_CNT=`hg status --include="**moz.build" $PROCESS_DIR | wc -l | tr -d " "`
 echo "MOZ_BUILD_CHANGE_CNT: $MOZ_BUILD_CHANGE_CNT"
 if [ "x$MOZ_BUILD_CHANGE_CNT" != "x0" ]; then
   CURRENT_COMMIT_SHA=`hg id -i | sed 's/+//'`
@@ -28,20 +38,20 @@ if [ "x$MOZ_BUILD_CHANGE_CNT" != "x0" ]; then
   # since we have build file changes, touch the CLOBBER file
   cat CLOBBER | egrep "^#|^$" > CLOBBER.new
   mv CLOBBER.new CLOBBER
-  echo "Modified build files in third_party/libwebrtc - $COMMIT_DESC" >> CLOBBER
+  echo "Modified build files in $PROCESS_DIR - $COMMIT_DESC" >> CLOBBER
 
-  ADD_CNT=`hg status --include="**moz.build" -nu third_party/libwebrtc | wc -l | tr -d " "`
-  DEL_CNT=`hg status --include="**moz.build" -nd third_party/libwebrtc | wc -l | tr -d " "`
+  ADD_CNT=`hg status --include="**moz.build" -nu $PROCESS_DIR | wc -l | tr -d " "`
+  DEL_CNT=`hg status --include="**moz.build" -nd $PROCESS_DIR | wc -l | tr -d " "`
   if [ "x$ADD_CNT" != "x0" ]; then
-    hg status --include="**moz.build" -nu third_party/libwebrtc | xargs hg add
+    hg status --include="**moz.build" -nu $PROCESS_DIR | xargs hg add
   fi
   if [ "x$DEL_CNT" != "x0" ]; then
-    hg status --include="**moz.build" -nd third_party/libwebrtc | xargs hg rm
+    hg status --include="**moz.build" -nd $PROCESS_DIR | xargs hg rm
   fi
 
   hg commit -m \
     "$COMMIT_DESC - moz.build file updates" \
-    --include="**moz.build" --include="CLOBBER" third_party/libwebrtc CLOBBER
+    --include="**moz.build" --include="CLOBBER" $PROCESS_DIR CLOBBER
 fi
 
 echo "Done in $0"
