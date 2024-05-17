463aa5c102b7ca9b133b942d05ee9bf62375b14c
Iulian Moraru <imoraru@mozilla.com>
2024-05-18 01:31:43 +0300

    Backed out changeset 1db41dc8d778 (bug 1833735) for causing xpchsell failures on ensureExperimentToRolloutTransitionPerformed.js. CLOSED TREE

--- toolkit/components/backgroundtasks/BackgroundTasksManager.sys.mjs
+++ toolkit/components/backgroundtasks/BackgroundTasksManager.sys.mjs
@@ -4,7 +4,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const lazy = {};
-import { DevToolsSocketStatus } from "resource://devtools/shared/security/DevToolsSocketStatus.sys.mjs";
 
 ChromeUtils.defineESModuleGetters(lazy, {
   setTimeout: "resource://gre/modules/Timer.sys.mjs",
@@ -230,32 +229,22 @@ export class BackgroundTasksManager {
       if (taskModule.backgroundTaskTimeoutSec) {
         timingSettings.maxTaskRuntimeSec = taskModule.backgroundTaskTimeoutSec;
       }
+
       try {
         let minimumReached = false;
-        let minRuntime;
-        let maxRuntime;
-        if (DevToolsSocketStatus.hasSocketOpened()) {
-          lazy.log.info(
-            `Setting background task timeout period to indefinite because a DevTools server is listening.`
-          );
-          minimumReached = true;
-          maxRuntime = new Promise(() => {});
-        } else {
-          minRuntime = new Promise(resolve =>
-            lazy.setTimeout(() => {
-              minimumReached = true;
-              resolve(true);
-            }, timingSettings.minTaskRuntimeMS)
-          );
-          maxRuntime = new Promise(resolve =>
+        let minRuntime = new Promise(resolve =>
+          lazy.setTimeout(() => {
+            minimumReached = true;
+            resolve(true);
+          }, timingSettings.minTaskRuntimeMS)
+        );
+        exitCode = await Promise.race([
+          new Promise(resolve =>
             lazy.setTimeout(() => {
               lazy.log.error(`Background task named '${name}' timed out`);
               resolve(EXIT_CODE.TIMEOUT);
             }, timingSettings.maxTaskRuntimeSec * 1000)
-          );
-        }
-        exitCode = await Promise.race([
-          maxRuntime,
+          ),
           taskModule.runBackgroundTask(commandLine),
         ]);
         if (!minimumReached) {
