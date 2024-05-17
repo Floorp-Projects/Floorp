cd32dc3a7459110c34a5731e9fcf600780e53c5f
KS <kshampur@mozilla.com>
2024-05-17 16:17:12 +0000

    Bug 1890597 - Add chromedriver 125. r=perftest-reviewers,taskgraph-reviewers,sparky,gbrown
    Differential Revision: https://phabricator.services.mozilla.com/D210810

--- taskcluster/gecko_taskgraph/transforms/test/other.py
+++ taskcluster/gecko_taskgraph/transforms/test/other.py
@@ -395,24 +395,24 @@ def setup_browsertime(config, tasks):
 
         cd_fetches = {
             "android.*": [
-                "linux64-chromedriver-122",
                 "linux64-chromedriver-123",
                 "linux64-chromedriver-124",
+                "linux64-chromedriver-125",
             ],
             "linux.*": [
-                "linux64-chromedriver-122",
                 "linux64-chromedriver-123",
                 "linux64-chromedriver-124",
+                "linux64-chromedriver-125",
             ],
             "macosx1015.*": [
-                "mac64-chromedriver-122",
                 "mac64-chromedriver-123",
                 "mac64-chromedriver-124",
+                "mac64-chromedriver-125",
             ],
             "macosx1400.*": [
-                "mac-arm-chromedriver-122",
                 "mac-arm-chromedriver-123",
                 "mac-arm-chromedriver-124",
+                "mac-arm-chromedriver-125",
             ],
             "windows.*aarch64.*": [
                 "win32-chromedriver-121",
@@ -420,8 +420,8 @@ def setup_browsertime(config, tasks):
                 "win32-chromedriver-123",
             ],
             "windows.*-64.*": [
-                "win64-chromedriver-123",
                 "win64-chromedriver-124",
+                "win64-chromedriver-125",
             ],
         }
 
