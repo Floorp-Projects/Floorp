4e8d27b8397cbc4ffb7e6765cd233eadc935febe
Tomislav Jovanovic <tomica@gmail.com>
2024-05-19 01:47:55 +0000

    Bug 1874406 - Refactor, optimize and cleanup event page idle management r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D210220

--- toolkit/components/extensions/ExtensionParent.sys.mjs
+++ toolkit/components/extensions/ExtensionParent.sys.mjs
@@ -1206,7 +1206,7 @@ ParentAPIManager = {
         !context.extension.persistentBackground
       ) {
         context.extension.emit("background-script-reset-idle", {
-          reason: "parentApiCall",
+          reason: "parentapicall",
           path: data.path,
         });
       }
