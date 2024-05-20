fb4cfde8074d0b7badefee9c657fd35f892f234c
Alexandre Poirot <poirot.alex@gmail.com>
2024-05-20 16:03:27 +0000

    Bug 1881503 - [devtools] Re-enable browser toolbox DAMP tests. r=devtools-reviewers,perftest-reviewers,nchevobbe,afinder
    They seem to not longer fail on Mac/Windows.
    
    Differential Revision: https://phabricator.services.mozilla.com/D210896

--- testing/talos/talos/tests/devtools/addon/content/damp-tests.js
+++ testing/talos/talos/tests/devtools/addon/content/damp-tests.js
@@ -5,7 +5,6 @@
 "use strict";
 
 const isWindows = Services.appinfo.OS === "WINNT";
-const isMacOS = Services.appinfo.OS === "Darwin";
 
 // DAMP is split in sub-suites to run the tests faster on continuous integration.
 // See the initial patches in Bug 1749928 if we need to add more suites.
@@ -233,8 +232,6 @@ module.exports = {
     {
       name: "browser-toolbox",
       path: "toolbox/browser-toolbox.js",
-      // Bug 1881503 - disabled toolbox/browser-toolbox on windows and mac for causing permafails
-      disabled: isWindows || isMacOS,
     },
     {
       name: "server.protocoljs",
