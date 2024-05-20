2bd3784e7f3640b824f20bff74aa0ce7c85b7990
Mark Banner <standard8@mozilla.com>
2024-05-20 13:49:44 +0000

    Bug 1897770 - Move ESLint no-insecure-url definitions to general-test.js. r=frontend-codestyle-reviewers,Gijs
    This moves the general test definition to be first, so that the other test types can build on it.
    It also re-arranges the no-insecure-url rule definitions to be defined in a single place in general-test.
    
    This also exposes a few more places where we're not currently meeting the no-insecure-url rule, so we allowlist
    those as well for now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D210927

--- tools/lint/eslint/eslint-plugin-mozilla/lib/configs/general-test.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/configs/general-test.js
@@ -1,8 +1,32 @@
-// Parent config file for all xpcshell files.
+// Parent config file for all test files.
+// This should be applied by the configuration before any other test
+// configurations.
 "use strict";
 
 module.exports = {
   rules: {
+    // No using of insecure url, so no http urls.
+    // Note: This is turned off for xpcshell-tests as it is not considered
+    // necessary for xpcshell level tests.
+    "@microsoft/sdl/no-insecure-url": [
+      "error",
+      {
+        exceptions: [
+          "^http:\\/\\/mochi\\.test?.*",
+          "^http:\\/\\/mochi\\.xorigin-test?.*",
+          "^http:\\/\\/localhost?.*",
+          "^http:\\/\\/127\\.0\\.0\\.1?.*",
+          // Exempt xmlns urls
+          "^http:\\/\\/www\\.w3\\.org?.*",
+          "^http:\\/\\/www\\.mozilla\\.org\\/keymaster\\/gatekeeper?.*",
+          // Exempt urls that start with ftp or ws.
+          "^ws:?.*",
+          "^ftp:?.*",
+        ],
+        varExceptions: ["insecure?.*"],
+      },
+    ],
+
     "mozilla/import-headjs-globals": "error",
     "mozilla/mark-test-function-used": "error",
     "mozilla/no-arbitrary-setTimeout": "error",
