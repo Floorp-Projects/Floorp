7c07e66e350042b2cbbc67b7541098838ba9565e
Alexandre Poirot <poirot.alex@gmail.com>
2024-05-21 08:32:13 +0000

    Bug 1832824 - [devtools] Fix browser_command_line_urls.js intermittent. r=devtools-reviewers,nchevobbe
    View source may update the selection late.
    
    Differential Revision: https://phabricator.services.mozilla.com/D210974

--- devtools/startup/tests/browser/browser_command_line_urls.js
+++ devtools/startup/tests/browser/browser_command_line_urls.js
@@ -85,6 +85,9 @@ add_task(async function openingWithDevToolsButUnknownSource() {
   );
 
   await SpecialPowers.spawn(gBrowser.selectedBrowser, [], async function () {
+    // View source may not have updated the selection just yet
+    ContentTaskUtils.waitForCondition(() => !!content.getSelection());
+
     const selection = content.getSelection();
     Assert.equal(
       selection.toString(),
