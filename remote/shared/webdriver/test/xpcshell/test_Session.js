487ee2dc064ed8ea17cc03d402168165dbdad346
Henrik Skupin <mail@hskupin.info>
2024-05-23 18:30:05 +0000

    Bug 1884650 - [remote] Add support for user prompt handler. r=webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D210730

--- remote/shared/webdriver/test/xpcshell/test_Session.js
+++ remote/shared/webdriver/test/xpcshell/test_Session.js
@@ -40,7 +40,7 @@ add_task(function test_WebDriverSession_getters() {
   );
   equal(session.timeouts, session.capabilities.get("timeouts"));
   equal(
-    session.unhandledPromptBehavior,
+    session.userPromptHandler,
     session.capabilities.get("unhandledPromptBehavior")
   );
 });
