4f8c686e751087b148f7f4ea17b1c72354024b46
Andrew Osmond <aosmond@mozilla.com>
2024-05-23 22:58:18 +0000

    Bug 1897201 - Return identity transform while recovering from context loss with canvas. r=gfx-reviewers,lsalzman a=pascalc
    When we lose the context for a 2D canvas, we need to silently fail any
    canvas calls until the context is recovered. Unfortunately that meant we
    returned a nullptr in CanvasRenderingContext2D::GetTransform which is
    unsupported by the WebIDL. For now, let's just return an identity
    transform as a placeholder for these situations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211471

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -2217,14 +2217,16 @@ void CanvasRenderingContext2D::Transform(double aM11, double aM12, double aM21,
 
 already_AddRefed<DOMMatrix> CanvasRenderingContext2D::GetTransform(
     ErrorResult& aError) {
-  if (!EnsureTarget(aError)) {
+  // If we are silently failing, then we still need to return a transform while
+  // we are in the process of recovering.
+  Matrix transform;
+  if (EnsureTarget(aError)) {
+    transform = mTarget->GetTransform();
+  } else if (aError.Failed()) {
     return nullptr;
   }
 
-  MOZ_ASSERT(IsTargetValid());
-
-  RefPtr<DOMMatrix> matrix =
-      new DOMMatrix(GetParentObject(), mTarget->GetTransform());
+  RefPtr<DOMMatrix> matrix = new DOMMatrix(GetParentObject(), transform);
   return matrix.forget();
 }
 
