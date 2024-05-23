2f51aa6177279f5c5942efa8607f4aa8d8f4fb87
Chun-Min Chang <chun.m.chang@gmail.com>
2024-05-23 05:21:18 +0000

    Bug 1891117 - Use const ConfigInternal& in DecodedDataToOutputType r=media-playback-reviewers,padenot
    Depends on D210847
    
    Differential Revision: https://phabricator.services.mozilla.com/D210848

--- dom/media/webcodecs/VideoDecoder.h
+++ dom/media/webcodecs/VideoDecoder.h
@@ -71,7 +71,7 @@ class VideoDecoder final : public DecoderTemplate<VideoDecoderTraits> {
 
   virtual nsTArray<RefPtr<VideoFrame>> DecodedDataToOutputType(
       nsIGlobalObject* aGlobalObject, const nsTArray<RefPtr<MediaData>>&& aData,
-      VideoDecoderConfigInternal& aConfig) override;
+      const VideoDecoderConfigInternal& aConfig) override;
 };
 
 }  // namespace mozilla::dom
