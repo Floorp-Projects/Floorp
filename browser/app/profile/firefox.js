1f7f49d26023f40e01419550834bc567d230f4b5
Sammy Khamis <skhamis@mozilla.com>
2024-05-23 20:18:02 +0000

    Bug 1898580: Flip the FxA CTA panel pref to be default on r=markh
    Differential Revision: https://phabricator.services.mozilla.com/D211435

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2532,7 +2532,7 @@ pref("identity.fxaccounts.toolbar.defaultVisible", true);
 
 // Prefs to control Firefox Account panels that shows call to actions
 // for other supported Mozilla products
-pref("identity.fxaccounts.toolbar.pxiToolbarEnabled", false);
+pref("identity.fxaccounts.toolbar.pxiToolbarEnabled", true);
 pref("identity.fxaccounts.toolbar.pxiToolbarEnabled.monitorEnabled", true);
 pref("identity.fxaccounts.toolbar.pxiToolbarEnabled.relayEnabled", true);
 pref("identity.fxaccounts.toolbar.pxiToolbarEnabled.vpnEnabled", true);
