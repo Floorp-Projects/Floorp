bfad4103bd92e7ee6f47b1c7e8ae268c83256ea8
alwu <alwu@mozilla.com>
2024-05-23 19:11:24 +0000

    Bug 1897636 - allow the initialization for PDMs on any thread. r=media-playback-reviewers,padenot
    Since bug 1885671, we no longer need to run the initialization for
    decoder module on the main thread, so we should allow running
    `InitPDMs()` on non-main threads as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211142

--- dom/media/platforms/PDMFactory.cpp
+++ dom/media/platforms/PDMFactory.cpp
@@ -192,8 +192,9 @@ StaticMutex PDMInitializer::sMonitor;
 /* static */
 void PDMInitializer::InitPDMs() {
   StaticMutexAutoLock mon(sMonitor);
-  MOZ_DIAGNOSTIC_ASSERT(NS_IsMainThread());
-  MOZ_ASSERT(!sHasInitializedPDMs);
+  if (sHasInitializedPDMs) {
+    return;
+  }
   if (XRE_IsGPUProcess()) {
     PDM_INIT_LOG("Init PDMs in GPU process");
     InitGpuPDMs();
@@ -306,26 +307,28 @@ void PDMFactory::EnsureInit() {
   if (PDMInitializer::HasInitializedPDMs()) {
     return;
   }
-  auto initalization = []() {
+  static Atomic<bool> sInitGfxVarsAndPreferences(false);
+  auto initalizationGfxVarsAndPreferences = []() {
     MOZ_DIAGNOSTIC_ASSERT(NS_IsMainThread());
-    if (!PDMInitializer::HasInitializedPDMs()) {
+    if (!sInitGfxVarsAndPreferences) {
       // Ensure that all system variables are initialized.
       gfx::gfxVars::Initialize();
       // Prime the preferences system from the main thread.
       Unused << BrowserTabsRemoteAutostart();
-      PDMInitializer::InitPDMs();
+      sInitGfxVarsAndPreferences = true;
     }
   };
-  // If on the main thread, then initialize PDMs. Otherwise, do a sync-dispatch
-  // to main thread.
+  // There are some initialization needed to be done on the main thread.
   if (NS_IsMainThread()) {
-    initalization();
-    return;
+    initalizationGfxVarsAndPreferences();
+  } else {
+    nsCOMPtr<nsIEventTarget> mainTarget = GetMainThreadSerialEventTarget();
+    nsCOMPtr<nsIRunnable> runnable =
+        NS_NewRunnableFunction("PDMFactory::EnsureInit",
+                               std::move(initalizationGfxVarsAndPreferences));
+    SyncRunnable::DispatchToThread(mainTarget, runnable);
   }
-  nsCOMPtr<nsIEventTarget> mainTarget = GetMainThreadSerialEventTarget();
-  nsCOMPtr<nsIRunnable> runnable = NS_NewRunnableFunction(
-      "PDMFactory::EnsureInit", std::move(initalization));
-  SyncRunnable::DispatchToThread(mainTarget, runnable);
+  PDMInitializer::InitPDMs();
 }
 
 RefPtr<PlatformDecoderModule::CreateDecoderPromise> PDMFactory::CreateDecoder(
