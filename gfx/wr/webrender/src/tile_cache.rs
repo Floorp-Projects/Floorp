c58f82b2556a739cd8c60a4d8b28102aa014937a
Brad Werth <bwerth@mozilla.com>
2024-05-23 20:51:59 +0000

    Bug 1872892: Allow YuvImage underlays in more circumstances. r=gw
    This patch makes two changes:
    
    1) Backdrops will be considered spanning as long as they cover the
    visible local clip rect. Formerly, they had to span the entire visible
    local rect, which meant that undrawn areas had to also be covered.
    
    2) Image prims will not promote until all of the promotable YuvImage
    prims have been considered for promotion. This is similar to the
    original implementation of underlays in Bug 1849680, where the presence
    of any YuvImage prim would prevent *all* overlay promotion (which is the
    only type considered for Image prims). The new behavior will allow Image
    prims to be promoted as long as all the YuvImages have been processed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211297

--- gfx/wr/webrender/src/tile_cache.rs
+++ gfx/wr/webrender/src/tile_cache.rs
@@ -618,7 +618,8 @@ fn create_tile_cache(
         shared_clip_node_id,
         shared_clip_leaf_id,
         virtual_surface_size: frame_builder_config.compositor_kind.get_virtual_surface_size(),
-        overlay_surface_count: prim_list.overlay_surface_count,
+        image_surface_count: prim_list.image_surface_count,
+        yuv_image_surface_count: prim_list.yuv_image_surface_count,
     });
 
     let pic_index = prim_store.pictures.alloc().init(PicturePrimitive::new_image(
