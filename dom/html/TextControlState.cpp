a21958bc408cd1a7b8d04463573a3d72ca0607b2
Olli Pettay <Olli.Pettay@helsinki.fi>
2024-05-23 20:18:47 +0000

    Bug 1898343 - Don't fire selectionchange if there is one pending, r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D211263

--- dom/html/TextControlState.cpp
+++ dom/html/TextControlState.cpp
@@ -2116,8 +2116,10 @@ void TextControlState::SetSelectionRange(uint32_t aStart, uint32_t aEnd,
   // XXX(krosylight): Shouldn't it fire before select event?
   // Currently Gecko and Blink both fire selectionchange after select.
   if (IsSelectionCached() &&
-      StaticPrefs::dom_select_events_textcontrols_selectionchange_enabled()) {
-    asyncDispatcher = new AsyncEventDispatcher(
+      StaticPrefs::dom_select_events_textcontrols_selectionchange_enabled() &&
+      !mTextCtrlElement->HasScheduledSelectionChangeEvent()) {
+    mTextCtrlElement->SetHasScheduledSelectionChangeEvent();
+    asyncDispatcher = new AsyncSelectionChangeEventDispatcher(
         mTextCtrlElement, eSelectionChange, CanBubble::eYes);
     asyncDispatcher->PostDOMEvent();
   }
