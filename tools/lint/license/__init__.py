55b566e4379bdf9a75cd26ab11a394a4c2eae113
Mark Banner <standard8@mozilla.com>
2024-05-24 11:20:04 +0000

    Bug 1898558 - Switch the license linter to use an exclusion list for tests that is defined in the yaml rather than in the linter itself. r=linter-reviewers,sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D211408

--- tools/lint/license/__init__.py
+++ tools/lint/license/__init__.py
@@ -229,10 +229,6 @@ def lint(paths, config, fix=None, **lintargs):
 
     licenses = load_valid_license()
     for f in files:
-        if is_test(f):
-            # For now, do not do anything with test (too many)
-            continue
-
         if not is_valid_license(licenses, f):
             if fix and fix_me(log, f):
                 fixed += 1
