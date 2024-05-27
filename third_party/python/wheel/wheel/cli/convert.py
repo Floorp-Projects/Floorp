bd17394bd9c6a4b8d09f1840845a4fcdea513da2
ahochheiden <ahochheiden@mozilla.com>
2024-05-27 23:06:19 +0000

    Bug 1857470 - Vendor `wheel` at version `0.43.0` r=firefox-build-system-reviewers,mach-reviewers,sergesanspaille,ahal
    We need to use `wheel` in mozharness to use `--no-use-pep517` (so we can
    use a modern `pip`). We don't explicitly need to upgrade `wheel`, but it
    seems sensible to upgrade it to a similar baseline as both `pip` and
    `setuptools` (which we also need are are upgrading with this stack).
    
    Differential Revision: https://phabricator.services.mozilla.com/D211594

--- third_party/python/wheel/wheel/cli/convert.py
+++ third_party/python/wheel/wheel/cli/convert.py
@@ -1,21 +1,29 @@
+from __future__ import annotations
+
 import os.path
 import re
 import shutil
-import sys
 import tempfile
 import zipfile
-from distutils import dist
 from glob import iglob
 
 from ..bdist_wheel import bdist_wheel
 from ..wheelfile import WheelFile
-from . import WheelError, require_pkgresources
+from . import WheelError
 
-egg_info_re = re.compile(r'''
+try:
+    from setuptools import Distribution
+except ImportError:
+    from distutils.dist import Distribution
+
+egg_info_re = re.compile(
+    r"""
     (?P<name>.+?)-(?P<ver>.+?)
     (-(?P<pyver>py\d\.\d+)
      (-(?P<arch>.+?))?
-    )?.egg$''', re.VERBOSE)
+    )?.egg$""",
+    re.VERBOSE,
+)
 
 
 class _bdist_wheel_tag(bdist_wheel):
@@ -34,11 +42,11 @@ class _bdist_wheel_tag(bdist_wheel):
             return bdist_wheel.get_tag(self)
 
 
-def egg2wheel(egg_path, dest_dir):
+def egg2wheel(egg_path: str, dest_dir: str) -> None:
     filename = os.path.basename(egg_path)
     match = egg_info_re.match(filename)
     if not match:
-        raise WheelError('Invalid egg file name: {}'.format(filename))
+        raise WheelError(f"Invalid egg file name: {filename}")
 
     egg_info = match.groupdict()
     dir = tempfile.mkdtemp(suffix="_e2w")
@@ -55,34 +63,34 @@ def egg2wheel(egg_path, dest_dir):
             else:
                 shutil.copytree(src, os.path.join(dir, pth))
 
-    pyver = egg_info['pyver']
+    pyver = egg_info["pyver"]
     if pyver:
-        pyver = egg_info['pyver'] = pyver.replace('.', '')
+        pyver = egg_info["pyver"] = pyver.replace(".", "")
 
-    arch = (egg_info['arch'] or 'any').replace('.', '_').replace('-', '_')
+    arch = (egg_info["arch"] or "any").replace(".", "_").replace("-", "_")
 
     # assume all binary eggs are for CPython
-    abi = 'cp' + pyver[2:] if arch != 'any' else 'none'
+    abi = "cp" + pyver[2:] if arch != "any" else "none"
 
-    root_is_purelib = egg_info['arch'] is None
+    root_is_purelib = egg_info["arch"] is None
     if root_is_purelib:
-        bw = bdist_wheel(dist.Distribution())
+        bw = bdist_wheel(Distribution())
     else:
-        bw = _bdist_wheel_tag(dist.Distribution())
+        bw = _bdist_wheel_tag(Distribution())
 
     bw.root_is_pure = root_is_purelib
     bw.python_tag = pyver
     bw.plat_name_supplied = True
-    bw.plat_name = egg_info['arch'] or 'any'
+    bw.plat_name = egg_info["arch"] or "any"
     if not root_is_purelib:
         bw.full_tag_supplied = True
         bw.full_tag = (pyver, abi, arch)
 
-    dist_info_dir = os.path.join(dir, '{name}-{ver}.dist-info'.format(**egg_info))
-    bw.egg2dist(os.path.join(dir, 'EGG-INFO'), dist_info_dir)
-    bw.write_wheelfile(dist_info_dir, generator='egg2wheel')
-    wheel_name = '{name}-{ver}-{pyver}-{}-{}.whl'.format(abi, arch, **egg_info)
-    with WheelFile(os.path.join(dest_dir, wheel_name), 'w') as wf:
+    dist_info_dir = os.path.join(dir, "{name}-{ver}.dist-info".format(**egg_info))
+    bw.egg2dist(os.path.join(dir, "EGG-INFO"), dist_info_dir)
+    bw.write_wheelfile(dist_info_dir, generator="egg2wheel")
+    wheel_name = "{name}-{ver}-{pyver}-{}-{}.whl".format(abi, arch, **egg_info)
+    with WheelFile(os.path.join(dest_dir, wheel_name), "w") as wf:
         wf.write_files(dir)
 
     shutil.rmtree(dir)
@@ -125,38 +133,38 @@ def parse_wininst_info(wininfo_name, egginfo_name):
     if egginfo_name:
         egginfo = egg_info_re.search(egginfo_name)
         if not egginfo:
-            raise ValueError("Egg info filename %s is not valid" % (egginfo_name,))
+            raise ValueError(f"Egg info filename {egginfo_name} is not valid")
 
     # Parse the wininst filename
     # 1. Distribution name (up to the first '-')
-    w_name, sep, rest = wininfo_name.partition('-')
+    w_name, sep, rest = wininfo_name.partition("-")
     if not sep:
-        raise ValueError("Installer filename %s is not valid" % (wininfo_name,))
+        raise ValueError(f"Installer filename {wininfo_name} is not valid")
 
     # Strip '.exe'
     rest = rest[:-4]
     # 2. Python version (from the last '-', must start with 'py')
-    rest2, sep, w_pyver = rest.rpartition('-')
-    if sep and w_pyver.startswith('py'):
+    rest2, sep, w_pyver = rest.rpartition("-")
+    if sep and w_pyver.startswith("py"):
         rest = rest2
-        w_pyver = w_pyver.replace('.', '')
+        w_pyver = w_pyver.replace(".", "")
     else:
         # Not version specific - use py2.py3. While it is possible that
         # pure-Python code is not compatible with both Python 2 and 3, there
         # is no way of knowing from the wininst format, so we assume the best
         # here (the user can always manually rename the wheel to be more
         # restrictive if needed).
-        w_pyver = 'py2.py3'
+        w_pyver = "py2.py3"
     # 3. Version and architecture
-    w_ver, sep, w_arch = rest.rpartition('.')
+    w_ver, sep, w_arch = rest.rpartition(".")
     if not sep:
-        raise ValueError("Installer filename %s is not valid" % (wininfo_name,))
+        raise ValueError(f"Installer filename {wininfo_name} is not valid")
 
     if egginfo:
-        w_name = egginfo.group('name')
-        w_ver = egginfo.group('ver')
+        w_name = egginfo.group("name")
+        w_ver = egginfo.group("ver")
 
-    return {'name': w_name, 'ver': w_ver, 'arch': w_arch, 'pyver': w_pyver}
+    return {"name": w_name, "ver": w_ver, "arch": w_arch, "pyver": w_pyver}
 
 
 def wininst2wheel(path, dest_dir):
@@ -164,7 +172,7 @@ def wininst2wheel(path, dest_dir):
         # Search for egg-info in the archive
         egginfo_name = None
         for filename in bdw.namelist():
-            if '.egg-info' in filename:
+            if ".egg-info" in filename:
                 egginfo_name = filename
                 break
 
@@ -172,27 +180,27 @@ def wininst2wheel(path, dest_dir):
 
         root_is_purelib = True
         for zipinfo in bdw.infolist():
-            if zipinfo.filename.startswith('PLATLIB'):
+            if zipinfo.filename.startswith("PLATLIB"):
                 root_is_purelib = False
                 break
         if root_is_purelib:
-            paths = {'purelib': ''}
+            paths = {"purelib": ""}
         else:
-            paths = {'platlib': ''}
+            paths = {"platlib": ""}
 
-        dist_info = "%(name)s-%(ver)s" % info
+        dist_info = "{name}-{ver}".format(**info)
         datadir = "%s.data/" % dist_info
 
         # rewrite paths to trick ZipFile into extracting an egg
         # XXX grab wininst .ini - between .exe, padding, and first zip file.
         members = []
-        egginfo_name = ''
+        egginfo_name = ""
         for zipinfo in bdw.infolist():
-            key, basename = zipinfo.filename.split('/', 1)
+            key, basename = zipinfo.filename.split("/", 1)
             key = key.lower()
             basepath = paths.get(key, None)
             if basepath is None:
-                basepath = datadir + key.lower() + '/'
+                basepath = datadir + key.lower() + "/"
             oldname = zipinfo.filename
             newname = basepath + basename
             zipinfo.filename = newname
@@ -203,66 +211,62 @@ def wininst2wheel(path, dest_dir):
                 members.append(newname)
             # Remember egg-info name for the egg2dist call below
             if not egginfo_name:
-                if newname.endswith('.egg-info'):
+                if newname.endswith(".egg-info"):
                     egginfo_name = newname
-                elif '.egg-info/' in newname:
-                    egginfo_name, sep, _ = newname.rpartition('/')
+                elif ".egg-info/" in newname:
+                    egginfo_name, sep, _ = newname.rpartition("/")
         dir = tempfile.mkdtemp(suffix="_b2w")
         bdw.extractall(dir, members)
 
     # egg2wheel
-    abi = 'none'
-    pyver = info['pyver']
-    arch = (info['arch'] or 'any').replace('.', '_').replace('-', '_')
+    abi = "none"
+    pyver = info["pyver"]
+    arch = (info["arch"] or "any").replace(".", "_").replace("-", "_")
     # Wininst installers always have arch even if they are not
     # architecture-specific (because the format itself is).
     # So, assume the content is architecture-neutral if root is purelib.
     if root_is_purelib:
-        arch = 'any'
+        arch = "any"
     # If the installer is architecture-specific, it's almost certainly also
     # CPython-specific.
-    if arch != 'any':
-        pyver = pyver.replace('py', 'cp')
-    wheel_name = '-'.join((dist_info, pyver, abi, arch))
+    if arch != "any":
+        pyver = pyver.replace("py", "cp")
+    wheel_name = "-".join((dist_info, pyver, abi, arch))
     if root_is_purelib:
-        bw = bdist_wheel(dist.Distribution())
+        bw = bdist_wheel(Distribution())
     else:
-        bw = _bdist_wheel_tag(dist.Distribution())
+        bw = _bdist_wheel_tag(Distribution())
 
     bw.root_is_pure = root_is_purelib
     bw.python_tag = pyver
     bw.plat_name_supplied = True
-    bw.plat_name = info['arch'] or 'any'
+    bw.plat_name = info["arch"] or "any"
 
     if not root_is_purelib:
         bw.full_tag_supplied = True
         bw.full_tag = (pyver, abi, arch)
 
-    dist_info_dir = os.path.join(dir, '%s.dist-info' % dist_info)
+    dist_info_dir = os.path.join(dir, "%s.dist-info" % dist_info)
     bw.egg2dist(os.path.join(dir, egginfo_name), dist_info_dir)
-    bw.write_wheelfile(dist_info_dir, generator='wininst2wheel')
+    bw.write_wheelfile(dist_info_dir, generator="wininst2wheel")
 
     wheel_path = os.path.join(dest_dir, wheel_name)
-    with WheelFile(wheel_path, 'w') as wf:
+    with WheelFile(wheel_path, "w") as wf:
         wf.write_files(dir)
 
     shutil.rmtree(dir)
 
 
 def convert(files, dest_dir, verbose):
-    # Only support wheel convert if pkg_resources is present
-    require_pkgresources('wheel convert')
-
     for pat in files:
         for installer in iglob(pat):
-            if os.path.splitext(installer)[1] == '.egg':
+            if os.path.splitext(installer)[1] == ".egg":
                 conv = egg2wheel
             else:
                 conv = wininst2wheel
 
             if verbose:
-                print("{}... ".format(installer))
-                sys.stdout.flush()
+                print(f"{installer}... ", flush=True)
 
             conv(installer, dest_dir)
             if verbose:
--- third_party/python/wheel/wheel/metadata.py
+++ third_party/python/wheel/wheel/metadata.py
@@ -2,132 +2,179 @@
 Tools for converting old- to new-style metadata.
 """
 
+from __future__ import annotations
+
+import functools
+import itertools
 import os.path
+import re
 import textwrap
+from email.message import Message
+from email.parser import Parser
+from typing import Iterator
 
-import pkg_resources
-
-from .pkginfo import read_pkg_info
+from .vendored.packaging.requirements import Requirement
 
 
-def requires_to_requires_dist(requirement):
+def _nonblank(str):
+    return str and not str.startswith("#")
+
+
+@functools.singledispatch
+def yield_lines(iterable):
+    r"""
+    Yield valid lines of a string or iterable.
+    >>> list(yield_lines(''))
+    []
+    >>> list(yield_lines(['foo', 'bar']))
+    ['foo', 'bar']
+    >>> list(yield_lines('foo\nbar'))
+    ['foo', 'bar']
+    >>> list(yield_lines('\nfoo\n#bar\nbaz #comment'))
+    ['foo', 'baz #comment']
+    >>> list(yield_lines(['foo\nbar', 'baz', 'bing\n\n\n']))
+    ['foo', 'bar', 'baz', 'bing']
+    """
+    return itertools.chain.from_iterable(map(yield_lines, iterable))
+
+
+@yield_lines.register(str)
+def _(text):
+    return filter(_nonblank, map(str.strip, text.splitlines()))
+
+
+def split_sections(s):
+    """Split a string or iterable thereof into (section, content) pairs
+    Each ``section`` is a stripped version of the section header ("[section]")
+    and each ``content`` is a list of stripped lines excluding blank lines and
+    comment-only lines.  If there are any such lines before the first section
+    header, they're returned in a first ``section`` of ``None``.
+    """
+    section = None
+    content = []
+    for line in yield_lines(s):
+        if line.startswith("["):
+            if line.endswith("]"):
+                if section or content:
+                    yield section, content
+                section = line[1:-1].strip()
+                content = []
+            else:
+                raise ValueError("Invalid section heading", line)
+        else:
+            content.append(line)
+
+    # wrap up last segment
+    yield section, content
+
+
+def safe_extra(extra):
+    """Convert an arbitrary string to a standard 'extra' name
+    Any runs of non-alphanumeric characters are replaced with a single '_',
+    and the result is always lowercased.
+    """
+    return re.sub("[^A-Za-z0-9.-]+", "_", extra).lower()
+
+
+def safe_name(name):
+    """Convert an arbitrary string to a standard distribution name
+    Any runs of non-alphanumeric/. characters are replaced with a single '-'.
+    """
+    return re.sub("[^A-Za-z0-9.]+", "-", name)
+
+
+def requires_to_requires_dist(requirement: Requirement) -> str:
     """Return the version specifier for a requirement in PEP 345/566 fashion."""
-    if getattr(requirement, 'url', None):
+    if getattr(requirement, "url", None):
         return " @ " + requirement.url
 
     requires_dist = []
-    for op, ver in requirement.specs:
-        requires_dist.append(op + ver)
-    if not requires_dist:
-        return ''
-    return " (%s)" % ','.join(sorted(requires_dist))
+    for spec in requirement.specifier:
+        requires_dist.append(spec.operator + spec.version)
+
+    if requires_dist:
+        return " " + ",".join(sorted(requires_dist))
+    else:
+        return ""
 
 
-def convert_requirements(requirements):
+def convert_requirements(requirements: list[str]) -> Iterator[str]:
     """Yield Requires-Dist: strings for parsed requirements strings."""
     for req in requirements:
-        parsed_requirement = pkg_resources.Requirement.parse(req)
+        parsed_requirement = Requirement(req)
         spec = requires_to_requires_dist(parsed_requirement)
-        extras = ",".join(sorted(parsed_requirement.extras))
+        extras = ",".join(sorted(safe_extra(e) for e in parsed_requirement.extras))
         if extras:
-            extras = "[%s]" % extras
-        yield (parsed_requirement.project_name + extras + spec)
+            extras = f"[{extras}]"
+
+        yield safe_name(parsed_requirement.name) + extras + spec
 
 
-def generate_requirements(extras_require):
+def generate_requirements(
+    extras_require: dict[str, list[str]],
+) -> Iterator[tuple[str, str]]:
     """
-    Convert requirements from a setup()-style dictionary to ('Requires-Dist', 'requirement')
-    and ('Provides-Extra', 'extra') tuples.
+    Convert requirements from a setup()-style dictionary to
+    ('Requires-Dist', 'requirement') and ('Provides-Extra', 'extra') tuples.
 
     extras_require is a dictionary of {extra: [requirements]} as passed to setup(),
     using the empty extra {'': [requirements]} to hold install_requires.
     """
     for extra, depends in extras_require.items():
-        condition = ''
-        extra = extra or ''
-        if ':' in extra:  # setuptools extra:condition syntax
-            extra, condition = extra.split(':', 1)
+        condition = ""
+        extra = extra or ""
+        if ":" in extra:  # setuptools extra:condition syntax
+            extra, condition = extra.split(":", 1)
 
-        extra = pkg_resources.safe_extra(extra)
+        extra = safe_extra(extra)
         if extra:
-            yield 'Provides-Extra', extra
+            yield "Provides-Extra", extra
             if condition:
                 condition = "(" + condition + ") and "
             condition += "extra == '%s'" % extra
 
         if condition:
-            condition = ' ; ' + condition
+            condition = " ; " + condition
 
         for new_req in convert_requirements(depends):
-            yield 'Requires-Dist', new_req + condition
+            yield "Requires-Dist", new_req + condition
 
 
-def pkginfo_to_metadata(egg_info_path, pkginfo_path):
+def pkginfo_to_metadata(egg_info_path: str, pkginfo_path: str) -> Message:
     """
     Convert .egg-info directory with PKG-INFO to the Metadata 2.1 format
     """
-    pkg_info = read_pkg_info(pkginfo_path)
-    pkg_info.replace_header('Metadata-Version', '2.1')
+    with open(pkginfo_path, encoding="utf-8") as headers:
+        pkg_info = Parser().parse(headers)
+
+    pkg_info.replace_header("Metadata-Version", "2.1")
     # Those will be regenerated from `requires.txt`.
-    del pkg_info['Provides-Extra']
-    del pkg_info['Requires-Dist']
-    requires_path = os.path.join(egg_info_path, 'requires.txt')
+    del pkg_info["Provides-Extra"]
+    del pkg_info["Requires-Dist"]
+    requires_path = os.path.join(egg_info_path, "requires.txt")
     if os.path.exists(requires_path):
-        with open(requires_path) as requires_file:
+        with open(requires_path, encoding="utf-8") as requires_file:
             requires = requires_file.read()
 
-        parsed_requirements = sorted(pkg_resources.split_sections(requires),
-                                     key=lambda x: x[0] or '')
+        parsed_requirements = sorted(split_sections(requires), key=lambda x: x[0] or "")
         for extra, reqs in parsed_requirements:
             for key, value in generate_requirements({extra: reqs}):
                 if (key, value) not in pkg_info.items():
                     pkg_info[key] = value
 
-    description = pkg_info['Description']
+    description = pkg_info["Description"]
     if description:
-        pkg_info.set_payload(dedent_description(pkg_info))
-        del pkg_info['Description']
+        description_lines = pkg_info["Description"].splitlines()
+        dedented_description = "\n".join(
+            # if the first line of long_description is blank,
+            # the first line here will be indented.
+            (
+                description_lines[0].lstrip(),
+                textwrap.dedent("\n".join(description_lines[1:])),
+                "\n",
+            )
+        )
+        pkg_info.set_payload(dedented_description)
+        del pkg_info["Description"]
 
     return pkg_info
-
-
-def pkginfo_unicode(pkg_info, field):
-    """Hack to coax Unicode out of an email Message() - Python 3.3+"""
-    text = pkg_info[field]
-    field = field.lower()
-    if not isinstance(text, str):
-        for item in pkg_info.raw_items():
-            if item[0].lower() == field:
-                text = item[1].encode('ascii', 'surrogateescape') \
-                    .decode('utf-8')
-                break
-
-    return text
-
-
-def dedent_description(pkg_info):
-    """
-    Dedent and convert pkg_info['Description'] to Unicode.
-    """
-    description = pkg_info['Description']
-
-    # Python 3 Unicode handling, sorta.
-    surrogates = False
-    if not isinstance(description, str):
-        surrogates = True
-        description = pkginfo_unicode(pkg_info, 'Description')
-
-    description_lines = description.splitlines()
-    description_dedent = '\n'.join(
-        # if the first line of long_description is blank,
-        # the first line here will be indented.
-        (description_lines[0].lstrip(),
-         textwrap.dedent('\n'.join(description_lines[1:])),
-         '\n'))
-
-    if surrogates:
-        description_dedent = description_dedent \
-            .encode("utf8") \
-            .decode("ascii", "surrogateescape")
-
-    return description_dedent
