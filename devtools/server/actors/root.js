2ec7fad516b2e6d949f102fdc98ca410c471b9b6
Sandor Molnar <smolnar@mozilla.com>
2024-05-28 18:54:54 +0300

    Backed out changeset 6f7a33d823af (bug 1824726) for causing dt failures @ browser_webconsole_message_categories.js CLOSED TREE

--- devtools/server/actors/root.js
+++ devtools/server/actors/root.js
@@ -135,8 +135,6 @@ class RootActor extends Actor {
             "dom.worker.console.dispatch_events_to_main_thread"
           )
         : true,
-      // @backward-compat { version 126 } The server started throttling ressource emission
-      throttledResources: true,
     };
   }
 
