f40369f5fea97fe8942b863db6abba526ed725f6
Sidharth Sachdev <ssachdev@mozilla.com>
2024-05-28 19:22:30 +0000

    Bug 1898323 - Fix OsAuth migration.  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D211245
    
    Differential Revision: https://phabricator.services.mozilla.com/D211879

---
+++ browser/components/tests/unit/test_browserGlue_migration_osauth.js
@@ -0,0 +1,119 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TOPIC_BROWSERGLUE_TEST = "browser-glue-test";
+const TOPICDATA_BROWSERGLUE_TEST = "force-ui-migration";
+const gBrowserGlue = Cc["@mozilla.org/browser/browserglue;1"].getService(
+  Ci.nsIObserver
+);
+const UI_VERSION = 147;
+
+const { LoginHelper } = ChromeUtils.importESModule(
+  "resource://gre/modules/LoginHelper.sys.mjs"
+);
+const { FormAutofillUtils } = ChromeUtils.importESModule(
+  "resource://gre/modules/shared/FormAutofillUtils.sys.mjs"
+);
+
+const CC_OLD_PREF = "extensions.formautofill.reauth.enabled";
+const CC_TYPO_PREF = "extensions.formautofill.creditcards.reauth.optout";
+const CC_NEW_PREF = FormAutofillUtils.AUTOFILL_CREDITCARDS_REAUTH_PREF;
+
+const PASSWORDS_OLD_PREF = "signon.management.page.os-auth.enabled";
+const PASSWORDS_NEW_PREF = LoginHelper.OS_AUTH_FOR_PASSWORDS_PREF;
+
+add_task(async function setup() {
+  registerCleanupFunction(() => {
+    Services.prefs.clearUserPref("browser.migration.version");
+    Services.prefs.clearUserPref(CC_OLD_PREF);
+    Services.prefs.clearUserPref(CC_TYPO_PREF);
+    Services.prefs.clearUserPref(CC_NEW_PREF);
+    Services.prefs.clearUserPref(PASSWORDS_OLD_PREF);
+    Services.prefs.clearUserPref(PASSWORDS_NEW_PREF);
+    Services.prefs.clearUserPref("browser.startup.homepage_override.mstone");
+  });
+});
+
+add_task(async function test_pref_migration_old_pref_os_auth_disabled() {
+  Services.prefs.setIntPref("browser.migration.version", UI_VERSION - 1);
+  Services.prefs.setBoolPref(CC_OLD_PREF, false);
+  Services.prefs.setBoolPref(PASSWORDS_OLD_PREF, false);
+  // Simulate a migration.
+  gBrowserGlue.observe(
+    null,
+    TOPIC_BROWSERGLUE_TEST,
+    TOPICDATA_BROWSERGLUE_TEST
+  );
+  Assert.ok(
+    !FormAutofillUtils.getOSAuthEnabled(CC_NEW_PREF),
+    "OS Auth should be disabled for credit cards since it was disabled before migration."
+  );
+  Assert.ok(
+    !LoginHelper.getOSAuthEnabled(PASSWORDS_NEW_PREF),
+    "OS Auth should be disabled for passwords since it was disabled before migration."
+  );
+});
+
+add_task(async function test_pref_migration_old_pref_os_auth_enabled() {
+  Services.prefs.setIntPref("browser.migration.version", UI_VERSION - 1);
+  Services.prefs.setBoolPref(CC_OLD_PREF, true);
+  Services.prefs.setBoolPref(PASSWORDS_OLD_PREF, true);
+  // Simulate a migration.
+  gBrowserGlue.observe(
+    null,
+    TOPIC_BROWSERGLUE_TEST,
+    TOPICDATA_BROWSERGLUE_TEST
+  );
+  Assert.ok(
+    FormAutofillUtils.getOSAuthEnabled(CC_NEW_PREF),
+    "OS Auth should be enabled for credit cards since it was enabled before migration."
+  );
+  Assert.ok(
+    LoginHelper.getOSAuthEnabled(PASSWORDS_NEW_PREF),
+    "OS Auth should be enabled for passwords since it was enabled before migration."
+  );
+});
+
+add_task(
+  async function test_creditCards_pref_migration_typo_pref_os_auth_disabled() {
+    Services.prefs.setIntPref("browser.migration.version", UI_VERSION - 1);
+    Services.prefs.setCharPref(
+      "browser.startup.homepage_override.mstone",
+      "127.0b6"
+    );
+    FormAutofillUtils.setOSAuthEnabled(CC_TYPO_PREF, false);
+    // Simulate a migration.
+    gBrowserGlue.observe(
+      null,
+      TOPIC_BROWSERGLUE_TEST,
+      TOPICDATA_BROWSERGLUE_TEST
+    );
+    Assert.ok(
+      !FormAutofillUtils.getOSAuthEnabled(CC_NEW_PREF),
+      "OS Auth should be disabled for credit cards since it was disabled before migration."
+    );
+  }
+);
+
+add_task(
+  async function test_creditCards_pref_migration_typo_pref_os_auth_enabled() {
+    Services.prefs.setIntPref("browser.migration.version", UI_VERSION - 1);
+    Services.prefs.setCharPref(
+      "browser.startup.homepage_override.mstone",
+      "127.0b6"
+    );
+    FormAutofillUtils.setOSAuthEnabled(CC_TYPO_PREF, true);
+    // Simulate a migration.
+    gBrowserGlue.observe(
+      null,
+      TOPIC_BROWSERGLUE_TEST,
+      TOPICDATA_BROWSERGLUE_TEST
+    );
+    Assert.ok(
+      FormAutofillUtils.getOSAuthEnabled(CC_NEW_PREF),
+      "OS Auth should be enabled for credit cards since it was enabled before migration."
+    );
+  }
+);
