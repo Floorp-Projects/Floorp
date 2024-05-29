26f1857569fd82b42754023ab6d8a3cec3a7a151
Abhishek Madan <amadan@mozilla.com>
2024-05-29 17:36:37 +0000

    Bug 1872993 - Part 2: Added Tests.r=pbz,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D210079

--- browser/base/content/test/popupNotifications/head.js
+++ browser/base/content/test/popupNotifications/head.js
@@ -365,3 +365,31 @@ function triggerSecondaryCommand(popup, index) {
     altKey: !navigator.platform.includes("Mac"),
   });
 }
+
+/**
+ * The security delay calculation in PopupNotification.sys.mjs is dependent on
+ * the monotonically increasing value of Cu.now. This timestamp is
+ * not relative to a fixed date, but to runtime.
+ * We need to wait for the value Cu.now() to be larger than the
+ * security delay in order to observe the bug. Only then does the
+ * timeSinceShown check in PopupNotifications.sys.mjs lead to a timeSinceShown
+ * value that is unconditionally greater than lazy.buttonDelay for
+ * notification.timeShown = null = 0.
+ * See: https://searchfox.org/mozilla-central/rev/f32d5f3949a3f4f185122142b29f2e3ab776836e/toolkit/modules/PopupNotifications.sys.mjs#1870-1872
+ *
+ * When running in automation as part of a larger test suite Cu.now()
+ * should usually be already sufficiently high in which case this check should
+ * directly resolve.
+ */
+async function ensureSecurityDelayReady(timeNewWindowOpened = 0) {
+  let secDelay = Services.prefs.getIntPref(
+    "security.notification_enable_delay"
+  );
+
+  await TestUtils.waitForCondition(
+    () => Cu.now() - timeNewWindowOpened > secDelay,
+    "Wait for performance.now() > SECURITY_DELAY",
+    500,
+    50
+  );
+}
