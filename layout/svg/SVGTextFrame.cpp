b6e03d00b3f68a3da17755a42307a96635459393
longsonr <longsonr@gmail.com>
2024-05-30 14:26:29 +0000

    Bug 1899525 - Stop lengthAdjust="spacingAndGlyphs" clipping the last character r=jfkthame
    Reverts a small part of bug 1621502
    
    Differential Revision: https://phabricator.services.mozilla.com/D212091

--- layout/svg/SVGTextFrame.cpp
+++ layout/svg/SVGTextFrame.cpp
@@ -869,12 +869,6 @@ SVGBBox TextRenderedRun::GetRunUserSpaceRect(nsPresContext* aContext,
               fillInAppUnits.height),
       aContext);
 
-  if (vertical) {
-    fill.Scale(1.0, mLengthAdjustScaleFactor);
-  } else {
-    fill.Scale(mLengthAdjustScaleFactor, 1.0);
-  }
-
   // Scale the rectangle up due to any mFontSizeScaleFactor.
   fill.Scale(1.0 / mFontSizeScaleFactor);
 
