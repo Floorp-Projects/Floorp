1e61b33af7c190bab1128ad3c595610d562a2781
Sandor Molnar <smolnar@mozilla.com>
2024-05-31 00:47:15 +0300

    Backed out changeset 2c914d67e8d8 (bug 1835987) for causing Spider-monkey failures

--- browser/components/resistfingerprinting/test/browser/browser_timezone.js
+++ browser/components/resistfingerprinting/test/browser/browser_timezone.js
@@ -1,7 +1,6 @@
 /**
  * Bug 1330890 - A test case for verifying Date() object of javascript will use
- *               Atlantic/Reykjavik timezone (GMT and "real" equivalent to UTC)
- *               after fingerprinting resistance is enabled.
+ *               UTC timezone after fingerprinting resistance is enabled.
  */
 
 async function verifySpoofed() {
@@ -17,15 +16,13 @@ async function verifySpoofed() {
   // Running in content:
   function test() {
     let date = new Date();
-    const TZ_NAME = "Atlantic/Reykjavik";
-    const TZ_SUFFIX = "Greenwich Mean Time";
     ok(
-      date.toString().endsWith(`(${TZ_SUFFIX})`),
-      `The date toString() is in ${TZ_NAME} timezone.`
+      date.toString().endsWith("(Coordinated Universal Time)"),
+      "The date toString() is in UTC timezone."
     );
     ok(
-      date.toTimeString().endsWith(`(${TZ_SUFFIX})`),
-      `The date toTimeString() is in ${TZ_NAME} timezone.`
+      date.toTimeString().endsWith("(Coordinated Universal Time)"),
+      "The date toTimeString() is in UTC timezone."
     );
     let dateTimeFormat = Intl.DateTimeFormat("en-US", {
       dateStyle: "full",
@@ -33,12 +30,12 @@ async function verifySpoofed() {
     });
     is(
       dateTimeFormat.resolvedOptions().timeZone,
-      TZ_NAME,
-      `The Intl.DateTimeFormat is in ${TZ_NAME} timezone.`
+      "UTC",
+      "The Intl.DateTimeFormat is in UTC timezone."
     );
     ok(
-      dateTimeFormat.format(date).endsWith(TZ_SUFFIX),
-      `The Intl.DateTimeFormat is formatting with the ${TZ_NAME} timezone.`
+      dateTimeFormat.format(date).endsWith("Coordinated Universal Time"),
+      "The Intl.DateTimeFormat is formatting with the UTC timezone."
     );
     is(
       date.getFullYear(),
