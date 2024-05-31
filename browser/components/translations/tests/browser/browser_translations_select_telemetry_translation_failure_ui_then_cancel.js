df5f2f9807124cf80114deda878b47de891637f2
Erik Nordin <enordin@mozilla.com>
2024-05-31 14:01:27 +0000

    Bug 1898598 - Split up translation_failure_ui test r=translations-reviewers,gregtatum
    Splits up the test case in browser_translations_select_telemetry_translation_failure_ui.js
    in hopes that it cuts down on the intermittent failure in CI.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211927

--- browser/components/translations/tests/browser/browser_translations_select_telemetry_translation_failure_ui_then_cancel.js
+++ browser/components/translations/tests/browser/browser_translations_select_telemetry_translation_failure_ui_then_cancel.js
@@ -5,16 +5,15 @@
 
 /**
  * This test case verifies the counts and extra data sent from telemetry events when interacting
- * with the SelectTranslationsPanel's translation-failure UI states.
+ * with the SelectTranslationsPanel's translation-failure UI and the user cancels after retrying.
  */
 add_task(
-  async function test_select_translations_panel_telemetry_translation_failure_ui() {
-    const { cleanup, runInPage, rejectDownloads, resolveDownloads } =
-      await loadTestPage({
-        page: SELECT_TEST_PAGE_URL,
-        languagePairs: LANGUAGE_PAIRS,
-        prefs: [["browser.translations.select.enable", true]],
-      });
+  async function test_select_translations_panel_telemetry_translation_failure_then_cancel() {
+    const { cleanup, runInPage, rejectDownloads } = await loadTestPage({
+      page: SELECT_TEST_PAGE_URL,
+      languagePairs: LANGUAGE_PAIRS,
+      prefs: [["browser.translations.select.enable", true]],
+    });
 
     await SelectTranslationsTestUtils.openPanel(runInPage, {
       selectFrenchSection: true,
@@ -120,102 +119,6 @@ add_task(
       Glean.translationsSelectTranslationsPanel.close,
       {
         expectedEventCount: 1,
-      }
-    );
-
-    await SelectTranslationsTestUtils.openPanel(runInPage, {
-      selectFrenchSection: true,
-      openAtFrenchSection: true,
-      expectedFromLanguage: "fr",
-      expectedToLanguage: "en",
-      downloadHandler: rejectDownloads,
-      onOpenPanel:
-        SelectTranslationsTestUtils.assertPanelViewTranslationFailure,
-    });
-    await TestTranslationsTelemetry.assertEvent(
-      Glean.translationsSelectTranslationsPanel.open,
-      {
-        expectedEventCount: 2,
-        expectNewFlowId: true,
-        assertForMostRecentEvent: {
-          document_language: "es",
-          from_language: "fr",
-          to_language: "en",
-          top_preferred_language: "en",
-        },
-      }
-    );
-    await TestTranslationsTelemetry.assertEvent(
-      Glean.translations.translationRequest,
-      {
-        expectedEventCount: 3,
-        assertForMostRecentEvent: {
-          document_language: "es",
-          from_language: "fr",
-          to_language: "en",
-          top_preferred_language: "en",
-          request_target: "select",
-          auto_translate: false,
-          source_text_code_units:
-            AppConstants.platform === "win"
-              ? 1616 // With carriage returns
-              : 1607, // No carriage returns
-          source_text_word_count: 257,
-        },
-      }
-    );
-    await TestTranslationsTelemetry.assertEvent(
-      Glean.translationsSelectTranslationsPanel.translationFailureMessage,
-      {
-        expectedEventCount: 3,
-        assertForMostRecentEvent: {
-          from_language: "fr",
-          to_language: "en",
-        },
-      }
-    );
-
-    await SelectTranslationsTestUtils.clickTryAgainButton({
-      downloadHandler: resolveDownloads,
-      viewAssertion: SelectTranslationsTestUtils.assertPanelViewTranslated,
-    });
-    await TestTranslationsTelemetry.assertEvent(
-      Glean.translationsSelectTranslationsPanel.tryAgainButton,
-      {
-        expectedEventCount: 2,
-      }
-    );
-    await TestTranslationsTelemetry.assertEvent(
-      Glean.translations.translationRequest,
-      {
-        expectedEventCount: 4,
-        assertForMostRecentEvent: {
-          document_language: "es",
-          from_language: "fr",
-          to_language: "en",
-          top_preferred_language: "en",
-          request_target: "select",
-          auto_translate: false,
-          source_text_code_units:
-            AppConstants.platform === "win"
-              ? 1616 // With carriage returns
-              : 1607, // No carriage returns
-          source_text_word_count: 257,
-        },
-      }
-    );
-
-    await SelectTranslationsTestUtils.clickDoneButton();
-    await TestTranslationsTelemetry.assertEvent(
-      Glean.translationsSelectTranslationsPanel.doneButton,
-      {
-        expectedEventCount: 1,
-      }
-    );
-    await TestTranslationsTelemetry.assertEvent(
-      Glean.translationsSelectTranslationsPanel.close,
-      {
-        expectedEventCount: 2,
       }
     );
 
