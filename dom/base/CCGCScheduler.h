99f004b8ec3828a2fb960a49f117d0c288242745
Steve Fink <sfink@mozilla.com>
2024-05-31 17:33:57 +0000

    Bug 1899615 - Make the slice budget pref and parameter mean the actual budget, rather than half of it r=pbone
    Differential Revision: https://phabricator.services.mozilla.com/D212042

--- dom/base/CCGCScheduler.h
+++ dom/base/CCGCScheduler.h
@@ -535,7 +535,7 @@ class CCGCScheduler {
 
   // Configuration parameters
 
-  TimeDuration mActiveIntersliceGCBudget = TimeDuration::FromMilliseconds(5);
+  TimeDuration mActiveIntersliceGCBudget = TimeDuration::FromMilliseconds(10);
 };
 
 }  // namespace mozilla
