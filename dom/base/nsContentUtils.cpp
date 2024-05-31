6ca8c13ace9acbc0413650783b1f89d62df54e7c
Nika Layzell <nika@thelayzells.com>
2024-05-31 17:30:22 +0000

    Bug 1718673 - Allow loading text/event-stream in-browser as plain-text, r=smaug,necko-reviewers,kershaw
    This patch also updates some documentation which was telling folks supporting new content types to update dead code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D211797

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -4627,8 +4627,8 @@ bool nsContentUtils::IsChildOfSameType(Document* aDoc) {
 }
 
 bool nsContentUtils::IsPlainTextType(const nsACString& aContentType) {
-  // NOTE: if you add a type here, add it to the CONTENTDLF_CATEGORIES
-  // define in nsContentDLF.h as well.
+  // NOTE: if you add a type here, add it to the content_types array in
+  // layout/build/components.conf as well.
   return aContentType.EqualsLiteral(TEXT_PLAIN) ||
          aContentType.EqualsLiteral(TEXT_CSS) ||
          aContentType.EqualsLiteral(TEXT_CACHE_MANIFEST) ||
@@ -4639,7 +4639,8 @@ bool nsContentUtils::IsPlainTextType(const nsACString& aContentType) {
          aContentType.EqualsLiteral(APPLICATION_ECMASCRIPT) ||
          aContentType.EqualsLiteral(TEXT_JAVASCRIPT) ||
          aContentType.EqualsLiteral(APPLICATION_JSON) ||
-         aContentType.EqualsLiteral(TEXT_JSON);
+         aContentType.EqualsLiteral(TEXT_JSON) ||
+         aContentType.EqualsLiteral(TEXT_EVENT_STREAM);
 }
 
 bool nsContentUtils::IsUtf8OnlyPlainTextType(const nsACString& aContentType) {
