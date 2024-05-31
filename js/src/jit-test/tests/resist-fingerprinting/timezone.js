f35f65021fd79bfe3ef77d5a1b747d474895bd19
hackademix <giorgio@maone.net>
2024-05-31 14:16:36 +0000

    Bug 1835987 - Change RFP-spoofed TZ to Atlantic/Reykjavik. r=tjr
    Atlantic/Reykjavik stays on UTC during all the year, but it is less
    likely to be blocked than plan UTC.
    
    Differential Revision: https://phabricator.services.mozilla.com/D212131

--- js/src/jit-test/tests/resist-fingerprinting/timezone.js
+++ js/src/jit-test/tests/resist-fingerprinting/timezone.js
@@ -2,7 +2,10 @@
 
 let tzRE = /\(([^\)]+)\)/;
 
-// Make sure we aren't already running with UTC
+const SPOOFED_TZ_NAME = "Atlantic/Reykjavik";
+const SPOOFED_TZ_GENERIC = "Greenwich Mean Time";
+
+// Make sure we aren't already running with spoofed TZ
 let original = new Date(0);
 assertEq(tzRE.exec(original.toString())[1], "Pacific Standard Time");
 
@@ -16,8 +19,8 @@ assertEq(originalDT.resolvedOptions().timeZone, "PST8PDT");
 let global = newGlobal({forceUTC: true});
 
 let date = new global.Date();
-assertEq(tzRE.exec(date.toString())[1], "Coordinated Universal Time");
-assertEq(tzRE.exec(date.toTimeString())[1], "Coordinated Universal Time");
+assertEq(tzRE.exec(date.toString())[1], SPOOFED_TZ_GENERIC);
+assertEq(tzRE.exec(date.toTimeString())[1], SPOOFED_TZ_GENERIC);
 assertEq(date.getFullYear(), date.getUTCFullYear());
 assertEq(date.getMonth(), date.getUTCMonth());
 assertEq(date.getDate(), date.getUTCDate());
@@ -29,5 +32,5 @@ let dt = global.Intl.DateTimeFormat("en-US", {
   dateStyle: "full",
   timeStyle: "full",
 });
-assertEq(dt.format(date).endsWith("Coordinated Universal Time"), true);
-assertEq(dt.resolvedOptions().timeZone, "UTC");
+assertEq(dt.format(date).endsWith(SPOOFED_TZ_GENERIC), true);
+assertEq(dt.resolvedOptions().timeZone, SPOOFED_TZ_NAME);
