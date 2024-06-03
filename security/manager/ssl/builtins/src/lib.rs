5858336093a054f6ea5a6bdac5d50d5d4a31e68a
John Schanck <jschanck@mozilla.com>
2024-06-03 18:08:05 +0000

    Bug 1899656 - add a mock builtins module for tests. r=glandium,keeler
    Differential Revision: https://phabricator.services.mozilla.com/D212066

--- security/manager/ssl/builtins/src/lib.rs
+++ security/manager/ssl/builtins/src/lib.rs
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// certdata may include dead code depending on the contents of certdata.txt
+#[allow(dead_code)]
 mod certdata;
 mod internal;
 mod pkcs11;
---
+++ security/manager/ssl/tests/unit/test_builtins.js
@@ -0,0 +1,34 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+"use strict";
+
+// Tests that use a mock builtins module.
+
+// Ensure that the appropriate initialization has happened.
+do_get_profile();
+
+add_setup(function load_nssckbi_testlib() {
+  let moduleName = "Mock Builtins";
+  let libraryName = "test_builtins";
+
+  checkPKCS11ModuleNotPresent(moduleName, libraryName);
+
+  let libraryFile = Services.dirsvc.get("CurWorkD", Ci.nsIFile);
+  libraryFile.append("test_builtins");
+  libraryFile.append(ctypes.libraryName(libraryName));
+  loadPKCS11Module(libraryFile, moduleName, true);
+  let testModule = checkPKCS11ModuleExists(moduleName, libraryName);
+
+  // Check that listing the slots for the test module works.
+  let testModuleSlotNames = Array.from(
+    testModule.listSlots(),
+    slot => slot.name
+  );
+  testModuleSlotNames.sort();
+  const expectedSlotNames = ["NSS Builtin Objects"];
+  deepEqual(
+    testModuleSlotNames,
+    expectedSlotNames,
+    "Actual and expected slot names should be equal"
+  );
+});
