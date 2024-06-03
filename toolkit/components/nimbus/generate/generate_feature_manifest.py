9ad9de10cb6deb7a08ca1a428cd810e22873d74f
Barret Rennie <barret@brennie.ca>
2024-06-03 19:07:46 +0000

    Bug 1900401 - Fix path for prefFlips schema in FeatureManifest r=emcminn
    The feature manifest validation logic now checks that schema paths exist.
    
    A test has been added to ensure all schema URIs can be resolved with fetch() in
    the browser.
    
    Differential Revision: https://phabricator.services.mozilla.com/D212449

--- toolkit/components/nimbus/generate/generate_feature_manifest.py
+++ toolkit/components/nimbus/generate/generate_feature_manifest.py
@@ -5,6 +5,7 @@
 import json
 import sys
 from pathlib import Path
+from urllib.parse import urlparse
 
 import jsonschema
 import yaml
@@ -38,6 +39,8 @@ def write_fm_headers(fd):
 
 
 def validate_feature_manifest(schema_path, manifest_path, manifest):
+    TOPSRCDIR = Path(__file__).parent.parent.parent.parent.parent
+
     with open(schema_path, "r") as f:
         schema = json.load(f)
 
@@ -116,6 +119,17 @@ def validate_feature_manifest(schema_path, manifest_path, manifest):
                             f"{pref}"
                         )
                         raise Exception("Set prefs and fallback prefs cannot overlap")
+
+            if "schema" in feature:
+                schema_path = TOPSRCDIR / feature["schema"]["path"]
+                if not schema_path.exists():
+                    raise Exception(f"Schema does not exist at {schema_path}")
+
+                uri = urlparse(feature["schema"]["uri"])
+                if uri.scheme not in ("resource", "chrome"):
+                    raise Exception(
+                        "Only resource:// and chrome:// URIs are supported for schemas"
+                    )
 
         except Exception as e:
             print("Error while validating FeatureManifest.yaml")
