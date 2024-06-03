67af0f079650201257a9786dd728de85657f78f6
Nicolas Chevobbe <nchevobbe@mozilla.com>
2024-06-03 05:43:10 +0000

    Bug 1899489 - [devtools] Display registered property information in var() tooltip. r=devtools-reviewers,bomsy.
    Differential Revision: https://phabricator.services.mozilla.com/D212092

--- devtools/client/inspector/rules/test/browser_rules_registered-custom-properties.js
+++ devtools/client/inspector/rules/test/browser_rules_registered-custom-properties.js
@@ -143,35 +143,56 @@ add_task(async function () {
     "h1",
     "background-color",
     // The variable value is the initial value since the variable does not inherit
-    `--css-no-inherit = ${CSS_NO_INHERIT_INITIAL_VALUE}`
+    `--css-no-inherit = ${CSS_NO_INHERIT_INITIAL_VALUE}`,
+    [
+      `syntax:"<color>"`,
+      `inherits:false`,
+      `initial-value:${CSS_NO_INHERIT_INITIAL_VALUE}`,
+    ]
   );
   await checkVariableTooltipForProperty(
     view,
     "h1",
     "color",
     // The variable value is the value set in the main selector, since the variable does inherit
-    `--css-inherit = ${CSS_INHERIT_MAIN_VALUE}`
+    `--css-inherit = ${CSS_INHERIT_MAIN_VALUE}`,
+    [
+      `syntax:"<color>"`,
+      `inherits:true`,
+      `initial-value:${CSS_INHERIT_INITIAL_VALUE}`,
+    ]
   );
   await checkVariableTooltipForProperty(
     view,
     "h1",
     "border-color",
     // The variable value is the initial value since the variable is not set
-    `--css-not-defined = ${CSS_NOT_DEFINED_INITIAL_VALUE}`
+    `--css-not-defined = ${CSS_NOT_DEFINED_INITIAL_VALUE}`,
+    [
+      `syntax:"<color>"`,
+      `inherits:true`,
+      `initial-value:${CSS_NOT_DEFINED_INITIAL_VALUE}`,
+    ]
   );
   await checkVariableTooltipForProperty(
     view,
     "h1",
     "height",
     // The variable value is the initial value since the variable does not inherit
-    `--js-no-inherit = ${JS_NO_INHERIT_INITIAL_VALUE}`
+    `--js-no-inherit = ${JS_NO_INHERIT_INITIAL_VALUE}`,
+    [
+      `syntax:"<length>"`,
+      `inherits:false`,
+      `initial-value:${JS_NO_INHERIT_INITIAL_VALUE}`,
+    ]
   );
   await checkVariableTooltipForProperty(
     view,
     "h1",
     "width",
     // The variable value is the value set in the main selector, since the variable does inherit
-    `--js-inherit = ${JS_INHERIT_MAIN_VALUE}`
+    `--js-inherit = ${JS_INHERIT_MAIN_VALUE}`,
+    [`syntax:"*"`, `inherits:true`]
   );
 
   info(
@@ -214,7 +235,8 @@ add_task(async function () {
     view,
     "h1",
     "caret-color",
-    `--css-dynamic-registered = orchid`
+    `--css-dynamic-registered = orchid`,
+    [`syntax:"<color>"`, `inherits:false`, `initial-value:orchid`]
   );
 
   info("Check that updating property does update rules view");
@@ -253,7 +275,8 @@ add_task(async function () {
     view,
     "h1",
     "caret-color",
-    `--css-dynamic-registered = purple`
+    `--css-dynamic-registered = purple`,
+    [`syntax:"<color>"`, `inherits:true`, `initial-value:purple`]
   );
 
   info("Check that removing property does update rules view");
@@ -324,7 +347,8 @@ add_task(async function () {
     view,
     "h1",
     "outline",
-    `--constructed = aqua`
+    `--constructed = aqua`,
+    [`syntax:"<color>"`, `inherits:true`, `initial-value:aqua`]
   );
 
   info(
@@ -463,13 +487,15 @@ function checkRegisteredProperties(view, expectedProperties) {
  * @param {CssRuleView} view
  * @param {String} ruleSelector
  * @param {String} propertyName
- * @param {String} expectedTooltipContent
+ * @param {String} expectedTooltipValueContent
+ * @param {Array<String>} expectedTooltipRegisteredPropertyContent
  */
 async function checkVariableTooltipForProperty(
   view,
   ruleSelector,
   propertyName,
-  expectedTooltipContent
+  expectedTooltipValueContent,
+  expectedTooltipRegisteredPropertyContent
 ) {
   // retrieve tooltip target
   const variableEl = await waitFor(() =>
@@ -481,10 +507,26 @@ async function checkVariableTooltipForProperty(
   );
 
   const previewTooltip = await assertShowPreviewTooltip(view, variableEl);
+  const [valueEl, registeredPropertyEl] = previewTooltip.panel.querySelectorAll(
+    ".variable-value,.registered-property"
+  );
   is(
-    previewTooltip.panel.textContent,
-    expectedTooltipContent,
+    valueEl.textContent,
+    expectedTooltipValueContent,
     `CSS variable preview tooltip shows the expected value for ${propertyName} in ${ruleSelector}`
   );
+  if (!expectedTooltipRegisteredPropertyContent) {
+    is(
+      registeredPropertyEl,
+      undefined,
+      `CSS variable preview tooltip doesn't have registered property section for ${propertyName} in ${ruleSelector}`
+    );
+  } else {
+    is(
+      registeredPropertyEl.innerText,
+      expectedTooltipRegisteredPropertyContent.join("\n"),
+      `CSS variable preview tooltip has expected registered property section for ${propertyName} in ${ruleSelector}`
+    );
+  }
   await assertTooltipHiddenOnMouseOut(previewTooltip, variableEl);
 }
