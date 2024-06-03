6c37c9c5217dfc57f842881280c5bff5697ad6f9
Otto Länd <bind-autoland@mozilla.com>
2024-06-03 14:05:37 +0000

    Bug 1892009, 1892010, 1892013, 1892014, 1892015, 1892020, 1895750, 1896920: apply code formatting via Lando
    # ignore-this-changeset

--- toolkit/components/credentialmanagement/IdentityCredentialStorageService.h
+++ toolkit/components/credentialmanagement/IdentityCredentialStorageService.h
@@ -46,7 +46,7 @@ class IdentityCredentialStorageService final
  private:
   IdentityCredentialStorageService()
       : mMonitor("mozilla::IdentityCredentialStorageService::mMonitor"),
-        mPendingWrites(0){};
+        mPendingWrites(0) {};
   ~IdentityCredentialStorageService() = default;
 
   // Spins up the service. This includes firing off async work in a worker
