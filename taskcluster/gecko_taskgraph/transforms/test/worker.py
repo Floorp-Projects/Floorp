12a85c395556219ec901593ee36c880ebd3bad31
Joel Maher <jmaher@mozilla.com>
2024-06-03 20:09:46 +0000

    Bug 1900155 - do not add requires-gpu for hardware tasks. r=aryx,taskgraph-reviewers,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D212357

--- taskcluster/gecko_taskgraph/transforms/test/worker.py
+++ taskcluster/gecko_taskgraph/transforms/test/worker.py
@@ -142,8 +142,10 @@ def set_worker_type(config, tasks):
                 if task[
                     "virtualization"
                 ] == "virtual-with-gpu" and test_platform.startswith("windows1"):
-                    # add in `--requires-gpu` to the mozharness options
-                    task["mozharness"]["extra-options"].append("--requires-gpu")
+                    # some unittests can run on hardware, no need for --requires-gpu
+                    if not test_platform.startswith("windows11-64-2009-hw-ref"):
+                        # add in `--requires-gpu` to the mozharness options
+                        task["mozharness"]["extra-options"].append("--requires-gpu")
 
             # now we have the right platform set the worker type accordingly
             task["worker-type"] = win_worker_type_platform[task["virtualization"]]
