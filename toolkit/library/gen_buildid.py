42efff29a6873fe027dcf92d94b67be74c7440ed
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2024-06-04 18:29:13 +0000

    Bug 1876590 - Place gToolkitBuildID in a specific section r=firefox-build-system-reviewers,nalexander,glandium
    For ELF we directly create a section with the value inside it
    
    Differential Revision: https://phabricator.services.mozilla.com/D199658

--- toolkit/library/gen_buildid.py
+++ toolkit/library/gen_buildid.py
@@ -20,13 +20,80 @@ def main(output, input_file):
     with open(input_file) as fh:
         objs = [l.strip() for l in fh.readlines()]
 
-    pp = Preprocessor()
-    pp.out = StringIO()
-    pp.do_include(os.path.join(buildconfig.topobjdir, "buildid.h"))
-    buildid = pp.context["MOZ_BUILDID"]
-    output.write('extern const char gToolkitBuildID[] = "%s";' % buildid)
+    write_file(output, None)
+
     return set(
         os.path.join("build", o)
         for o in objs
         if os.path.splitext(os.path.basename(o))[0] != "buildid"
     )
+
+
+def tests(output, buildid):
+    write_file(output, buildid)
+
+
+def write_file(output, maybe_buildid):
+    pp = Preprocessor()
+    pp.out = StringIO()
+    pp.do_include(os.path.join(buildconfig.topobjdir, "buildid.h"))
+    buildid = pp.context["MOZ_BUILDID"] if maybe_buildid is None else maybe_buildid
+
+    keyword_extern = "extern" if maybe_buildid is None else ""
+    attribute_used = "__attribute__((used))" if maybe_buildid is not None else ""
+
+    output.write(
+        """
+#include "buildid_section.h"
+
+#if defined(XP_DARWIN) || defined(XP_WIN)
+#define SECTION_NAME_ATTRIBUTE __attribute__((section(MOZ_BUILDID_SECTION_NAME)))
+#else
+#define SECTION_NAME_ATTRIBUTE
+#endif
+
+{extern} const char gToolkitBuildID[] SECTION_NAME_ATTRIBUTE {used} = "{buildid}";
+""".format(
+            extern=keyword_extern,
+            used=attribute_used,
+            buildid=buildid,
+        )
+    )
+
+    if buildconfig.substs.get("TARGET_KERNEL") not in (
+        "Darwin",
+        "WINNT",
+    ):
+        elf_note = """
+#include <elf.h>
+
+#define note_name "mzbldid"
+#define note_desc "{buildid}"
+
+// This is not defined on Android?
+// Android also hardcodes "1"
+// https://android.googlesource.com/platform/ndk/+/refs/tags/ndk-r26c/sources/crt/crtbrand.S#35
+#ifndef NT_VERSION
+#define NT_VERSION 1
+#endif
+
+struct note {{
+    Elf32_Nhdr header; // Elf32 or Elf64 doesn't matter, they're the same size
+    char name[(sizeof(note_name) + 3) / 4 * 4];
+    char desc[(sizeof(note_desc) + 3) / 4 * 4];
+}};
+
+{extern} const struct note gNoteToolkitBuildID __attribute__((section(MOZ_BUILDID_SECTION_NAME), aligned(4), used)) = {{
+    {{ sizeof(note_name), sizeof(note_desc), NT_VERSION }},
+    note_name,
+    note_desc
+}};
+"""
+        output.write(
+            "{}".format(
+                elf_note.format(
+                    extern=keyword_extern,
+                    buildid=buildid,
+                )
+            )
+        )
