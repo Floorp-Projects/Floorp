5856042085e12369f326e129dd3f6730c439cf5f
Ting-Yu Lin <tlin@mozilla.com>
2024-06-04 17:54:50 +0000

    Bug 1900522 Part 6 - Remove a SizeComputationInput::ComputeISizeValue() variant. r=layout-reviewers,emilio
    This removed one is just forwarding the call to nsIFrame, and used internally
    within ReflowInput.
    
    While I'm here, rename local variables `inside*` and `outside*` to make them
    clearer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D212509

--- layout/generic/ReflowInput.h
+++ layout/generic/ReflowInput.h
@@ -220,14 +220,6 @@ struct SizeComputationInput {
    * sizes.
    */
   template <typename SizeOrMaxSize>
-  inline nscoord ComputeISizeValue(const WritingMode aWM,
-                                   const LogicalSize& aContainingBlockSize,
-                                   const LogicalSize& aContentEdgeToBoxSizing,
-                                   nscoord aBoxSizingToMarginEdge,
-                                   const SizeOrMaxSize&) const;
-  // same as previous, but using mComputedBorderPadding, mComputedPadding,
-  // and mComputedMargin
-  template <typename SizeOrMaxSize>
   inline nscoord ComputeISizeValue(const LogicalSize& aContainingBlockSize,
                                    mozilla::StyleBoxSizing aBoxSizing,
                                    const SizeOrMaxSize&) const;
