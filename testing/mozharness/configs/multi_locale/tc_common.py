393c9ca708142c97b926ec1afe758c807066b039
Ben Hearsum <ben@mozilla.com>
2024-06-04 00:25:07 +0000

    Bug 1900059: Support l10n strings from github in multilocale build script r=releng-reviewers,jcristau
    This script ends up using the same mixin that was patched in https://bugzilla.mozilla.org/show_bug.cgi?id=1881272, so all we need to do is make gittool.py available to it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D212389

---
+++ testing/mozharness/configs/multi_locale/tc_common.py
@@ -0,0 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os.path
+
+import mozharness
+
+external_tools_path = os.path.join(
+    os.path.abspath(os.path.dirname(os.path.dirname(mozharness.__file__))),
+    "external_tools",
+)
+
+config = {
+    "exes": {
+        "gittool.py": [os.path.join(external_tools_path, "gittool.py")],
+    },
+}
