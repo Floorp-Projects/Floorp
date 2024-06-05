2b20997a97db95d1ac0a868a663308b4546e0e1a
Jonathan Kew <jkew@mozilla.com>
2024-06-05 17:28:36 +0000

    Bug 1670993 - For webcompat, make -webkit-font-smoothing:antialiased behave like -moz-osx-font-smoothing:grayscale. r=firefox-style-system-reviewers,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D212130

--- servo/components/style/properties/longhands/font.mako.rs
+++ servo/components/style/properties/longhands/font.mako.rs
@@ -474,11 +474,13 @@ pub mod system_font {
 
 ${helpers.single_keyword(
     "-moz-osx-font-smoothing",
-    "auto grayscale",
+    "auto grayscale subpixel-antialiased",
     engines="gecko",
     gecko_constant_prefix="NS_FONT_SMOOTHING",
     gecko_ffi_name="mFont.smoothing",
     gecko_pref="layout.css.osx-font-smoothing.enabled",
+    aliases="-webkit-font-smoothing:layout.css.osx-font-smoothing.enabled",
+    gecko_aliases="antialiased=grayscale",
     has_effect_on_gecko_scrollbars=False,
     spec="Nonstandard (https://developer.mozilla.org/en-US/docs/Web/CSS/font-smooth)",
     animation_value_type="discrete",
