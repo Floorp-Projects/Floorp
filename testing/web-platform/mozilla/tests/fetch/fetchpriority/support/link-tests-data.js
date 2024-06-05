b52d37b5490fdf07106ee078033515e260bd11bb
Frédéric Wang <fwang@igalia.com>
2024-06-05 08:04:06 +0000

    Bug 1882084 - Add WPT tests for fetchpriority in early hints preload. r=valentin
    The logic to handle Link headers [1] is the same for 103 Early hints
    reponses [2] as for 200 OK reponses. Tests are added to verify that the
    fetchpriority attribute is properly handled in early hints:
    
    1. A new test preload-fetchpriority.html verifies that the presence
       of fetchpriority=low/high/auto does not affect proper handling of
       early hints. This test is copied from preload-initiator-type.html.
       For that purpose, the helper scripts early-hints-helpers.sub.js
       and early-hints-test-loader.h2.py are adjusted so that they accept
       a fetchpriority_attr parameter.
    
    2. link-tests-data.js (used by fetchpriority-disabled.h2.html and
       fetchpriority.h2.html) is extended to handle the cases when
       Link as=font/image/fetch/script/style headers are added in early
       hints. The corresponding link-early-hints-*.h2.html pages don't
       check whether the initiator type is "early-hints". Instead,
       the helper scripts early-hints-helpers.sub.js and
       early-hints-test-loader.h2.py are adjusted so that accept a new
       boolean allowing to exclude the preload headers from the
       200 OK response.
    
    [1] https://html.spec.whatwg.org/multipage/semantics.html#processing-link-headers
    [2] https://html.spec.whatwg.org/multipage/semantics.html#early-hints
    
    Differential Revision: https://phabricator.services.mozilla.com/D209632

--- testing/web-platform/mozilla/tests/fetch/fetchpriority/support/link-tests-data.js
+++ testing/web-platform/mozilla/tests/fetch/fetchpriority/support/link-tests-data.js
@@ -392,7 +392,22 @@ export const kTestData = [
     },
     {   testFileName: "link-header.h2.html?pipe" + kPipeHeaderPrefetchLinks,
         expectedRequests: kExpectedRequestsOfPrefetch
-    }
+    },
+    {   testFileName: "link-early-hints-preload-font.h2.html",
+         expectedRequests: kExpectedRequestsOfLinkPreloadFont
+    },
+    {   testFileName: "link-early-hints-preload-image.h2.html",
+        expectedRequests: kExpectedRequestsOfLinkPreloadImage
+    },
+    {   testFileName: "link-early-hints-preload-fetch.h2.html",
+         expectedRequests: kExpectedRequestsOfLinkPreloadFetch
+    },
+    {   testFileName: "link-early-hints-preload-script.h2.html",
+         expectedRequests: kExpectedRequestsOfPreloadScript
+    },
+    {   testFileName: "link-early-hints-preload-style.h2.html",
+        expectedRequests: kExpectedRequestsOfLinkPreloadStyle
+    },
 ];
 
 export const kTestDataDisabled = [
@@ -467,5 +482,20 @@ export const kTestDataDisabled = [
     },
     {   testFileName: "link-header.h2.html?pipe" + kPipeHeaderModulepreloadLinks,
         expectedRequests: kExpectedRequestsOfModulepreloadDisabled
+    },
+    {   testFileName: "link-early-hints-preload-font.h2.html",
+         expectedRequests: kExpectedRequestsOfLinkPreloadFontDisabled
+    },
+    {   testFileName: "link-early-hints-preload-image.h2.html",
+        expectedRequests: kExpectedRequestsOfLinkPreloadImageDisabled
+    },
+    {   testFileName: "link-early-hints-preload-fetch.h2.html",
+         expectedRequests: kExpectedRequestsOfLinkPreloadFetchDisabled
+    },
+    {   testFileName: "link-early-hints-preload-script.h2.html",
+         expectedRequests: kExpectedRequestsOfPreloadScriptDisabled
+    },
+    {   testFileName: "link-early-hints-preload-style.h2.html",
+        expectedRequests: kExpectedRequestsOfLinkPreloadStyleDisabled
     },
 ];
