278feea5b59336d6b9bce40871cdb8e9a62a76e1
Tamas Szentpeteri <tszentpeteri@mozilla.com>
2024-06-06 18:36:45 +0300

    Backed out changeset aed45d6328cd (bug 1900526) for causing SM bustages related to bug1900526.js. CLOSED TREE

--- js/src/jit-test/tests/wasm/memory64/bug1900526.js
+++
@@ -1,6 +0,0 @@
-// |jit-test| allow-oom; skip-if: !canRunHugeMemoryTests()
-
-const maxPages = wasmMaxMemoryPages("i64");
-const m = new WebAssembly.Memory({initial: 0, index: "i64"});
-m.grow(maxPages);
-assertEq(m.buffer.byteLength, maxPages * PageSizeInBytes);
