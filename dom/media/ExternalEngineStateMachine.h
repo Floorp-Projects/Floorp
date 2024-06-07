02d2c20ae5d3bab16730cd9cf369ca7786365e09
alwu <alwu@mozilla.com>
2024-06-07 17:58:22 +0000

    Bug 1901110 - perform a simple audible check for the external engine state machine. r=media-playback-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D212847

--- dom/media/ExternalEngineStateMachine.h
+++ dom/media/ExternalEngineStateMachine.h
@@ -181,17 +181,17 @@ class ExternalEngineStateMachine final
     // crashes.
     struct RecoverEngine : public InitEngine {};
 
-    StateObject() : mData(ReadingMetadata()), mName(State::ReadingMetadata){};
+    StateObject() : mData(ReadingMetadata()), mName(State::ReadingMetadata) {};
     explicit StateObject(InitEngine&& aArg)
-        : mData(std::move(aArg)), mName(State::InitEngine){};
+        : mData(std::move(aArg)), mName(State::InitEngine) {};
     explicit StateObject(RunningEngine&& aArg)
-        : mData(std::move(aArg)), mName(State::RunningEngine){};
+        : mData(std::move(aArg)), mName(State::RunningEngine) {};
     explicit StateObject(SeekingData&& aArg)
-        : mData(std::move(aArg)), mName(State::SeekingData){};
+        : mData(std::move(aArg)), mName(State::SeekingData) {};
     explicit StateObject(ShutdownEngine&& aArg)
-        : mData(std::move(aArg)), mName(State::ShutdownEngine){};
+        : mData(std::move(aArg)), mName(State::ShutdownEngine) {};
     explicit StateObject(RecoverEngine&& aArg)
-        : mData(std::move(aArg)), mName(State::RecoverEngine){};
+        : mData(std::move(aArg)), mName(State::RecoverEngine) {};
 
     bool IsInitEngine() const { return mData.is<InitEngine>(); }
     bool IsReadingMetadata() const { return mData.is<ReadingMetadata>(); }
@@ -300,6 +300,8 @@ class ExternalEngineStateMachine final
   void ReportTelemetry(const MediaResult& aError);
 
   void DecodeError(const MediaResult& aError) override;
+
+  void NotifyAudibleStateChangeIfNeeded();
 
   UniquePtr<ExternalPlaybackEngine> mEngine;
 
