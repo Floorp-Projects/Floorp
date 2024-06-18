d69fd82528dfb440bb02447c7040af355185df12
Ben Dean-Kawamura <bdk@mozilla.com>
2024-06-18 13:36:32 +0000

    Backout dd8cfe92844a9af9ff5b357ffb197d0d970f9f55 (Bug 1900928), r=lina,android-reviewers,jonalmeida, a=dmeehan
    This was a hotfix that's no longer needed now that
    https://bugzilla.mozilla.org/show_bug.cgi?id=1900837 has been fixed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D213847

--- mobile/android/android-components/components/feature/fxsuggest/src/main/java/mozilla/components/feature/fxsuggest/FxSuggestIngestionWorker.kt
+++ mobile/android/android-components/components/feature/fxsuggest/src/main/java/mozilla/components/feature/fxsuggest/FxSuggestIngestionWorker.kt
@@ -22,10 +22,16 @@ internal class FxSuggestIngestionWorker(
     private val logger = Logger("FxSuggestIngestionWorker")
 
     override suspend fun doWork(): Result {
-        // Disable periodic ingestion until we figure out
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=1900837
-        logger.info("Skipping ingesting new suggestions")
-        return Result.success()
+        logger.info("Ingesting new suggestions")
+        val storage = GlobalFxSuggestDependencyProvider.requireStorage()
+        val success = storage.ingest()
+        return if (success) {
+            logger.info("Successfully ingested new suggestions")
+            Result.success()
+        } else {
+            logger.error("Failed to ingest new suggestions")
+            Result.retry()
+        }
     }
 
     internal companion object {
