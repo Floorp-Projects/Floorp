f83c570038415a6444c901a09685d9a2c610bd9f
Ben Hearsum <ben@mozilla.com>
2024-06-20 13:54:19 +0000

    Bug 1900421: Switch l10n string fetching to github  a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D213830

--- testing/mozharness/scripts/desktop_l10n.py
+++ testing/mozharness/scripts/desktop_l10n.py
@@ -89,7 +89,7 @@ class DesktopSingleLocale(LocalesMixin, AutomationMixin, VCSMixin, BaseScript):
                 "log_name": "single_locale",
                 "hg_l10n_base": "https://hg.mozilla.org/l10n-central",
                 # If set, takes precedence over `hg_l10n_base`
-                "git_repository": "",
+                "git_repository": "https://github.com/mozilla-l10n/firefox-l10n",
             },
         }
 
