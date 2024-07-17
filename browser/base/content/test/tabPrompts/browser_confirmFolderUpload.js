837416bddf31fb2ba7a4509d79b5e665c7c66e01
Oliver Medhurst <omedhurst@mozilla.com>
2024-07-17 12:27:36 +0000

    Bug 1877969 - Consume user activation when showing any picker  a=dmeehan
    Consume transient user activation when showing file/color picker
    at any time. Do not open if there is no transent user activation.
    
    Fixed various internal tests broken by this change.
    
    Spec PR: https://github.com/whatwg/html/pull/10344
    
    Original Revision: https://phabricator.services.mozilla.com/D201096
    
    Differential Revision: https://phabricator.services.mozilla.com/D216792

--- browser/base/content/test/tabPrompts/browser_confirmFolderUpload.js
+++ browser/base/content/test/tabPrompts/browser_confirmFolderUpload.js
@@ -95,6 +95,12 @@ async function testUploadPrompt(confirmUpload) {
       MockFilePicker.useDirectory(args.path);
 
       let input = content.document.getElementById("filepicker");
+
+      // Activate the page to allow opening the file picker.
+      content.SpecialPowers.wrap(
+        content.document
+      ).notifyUserGestureActivation();
+
       input.click();
     });
 
