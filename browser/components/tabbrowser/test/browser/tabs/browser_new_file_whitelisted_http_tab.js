e73abe2c578de5b1c89479b1aa01dbe7f2e1891e
Nika Layzell <nika@thelayzells.com>
2024-07-19 14:38:20 +0000

    Bug 1834864 - Select BCG more consistently during COOP+COEP process switches, r=smaug,tabbrowser-reviewers,mak, a=dmeehan
    Previously it was possible to bypass specific BCG selection based on
    cross-origin isolated status if the site was allowed to load file URIs
    using enterprise policies, which could lead to a crash.
    
    This patch changes the behaviour such that BCG selection now happens
    correctly. The site will still not be cross-origin isolated due to being
    loaded into a file content process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D217007

--- browser/components/tabbrowser/test/browser/tabs/browser_new_file_whitelisted_http_tab.js
+++ browser/components/tabbrowser/test/browser/tabs/browser_new_file_whitelisted_http_tab.js
@@ -1,8 +1,15 @@
 /* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
 /* vim: set ft=javascript ts=2 et sw=2 tw=80: */
 
-// eslint-disable-next-line @microsoft/sdl/no-insecure-url
-const TEST_HTTP = "http://example.org/";
+const TEST_HTTP_DOMAIN = "https://example.org/";
+
+const TEST_ROOT = getRootDirectory(gTestPath).replace(
+  "chrome://mochitests/content",
+  "https://example.org"
+);
+
+const TEST_NORMAL = `${TEST_ROOT}dummy_page.html`;
+const TEST_COOP_COEP = `${TEST_ROOT}file_coop_coep.html`;
 
 // Test for bug 1378377.
 add_task(async function () {
@@ -11,27 +18,72 @@ add_task(async function () {
     set: [["browser.tabs.remote.separateFileUriProcess", true]],
   });
 
-  await BrowserTestUtils.withNewTab(TEST_HTTP, async function (fileBrowser) {
+  await BrowserTestUtils.withNewTab(TEST_NORMAL, async function (fileBrowser) {
     ok(
       E10SUtils.isWebRemoteType(fileBrowser.remoteType),
       "Check that tab normally has web remote type."
     );
+
+    await SpecialPowers.spawn(fileBrowser, [], () => {
+      ok(
+        !content.crossOriginIsolated,
+        "Tab content is not cross-origin isolated"
+      );
+    });
   });
+
+  await BrowserTestUtils.withNewTab(
+    TEST_COOP_COEP,
+    async function (fileBrowser) {
+      ok(
+        fileBrowser.remoteType.startsWith(
+          E10SUtils.WEB_REMOTE_COOP_COEP_TYPE_PREFIX
+        ),
+        "Check that COOP+COEP tab normally has webCOOP+COEP remote type."
+      );
+      await SpecialPowers.spawn(fileBrowser, [], () => {
+        ok(content.crossOriginIsolated, "Tab content is cross-origin isolated");
+      });
+    }
+  );
 
   // Set prefs to whitelist TEST_HTTP for file:// URI use.
   await SpecialPowers.pushPrefEnv({
     set: [
       ["capability.policy.policynames", "allowFileURI"],
-      ["capability.policy.allowFileURI.sites", TEST_HTTP],
+      ["capability.policy.allowFileURI.sites", TEST_HTTP_DOMAIN],
       ["capability.policy.allowFileURI.checkloaduri.enabled", "allAccess"],
     ],
   });
 
-  await BrowserTestUtils.withNewTab(TEST_HTTP, async function (fileBrowser) {
+  await BrowserTestUtils.withNewTab(TEST_NORMAL, async function (fileBrowser) {
     is(
       fileBrowser.remoteType,
       E10SUtils.FILE_REMOTE_TYPE,
       "Check that tab now has file remote type."
     );
+    await SpecialPowers.spawn(fileBrowser, [], () => {
+      ok(
+        !content.crossOriginIsolated,
+        "Tab content is not cross-origin isolated"
+      );
+    });
   });
+
+  await BrowserTestUtils.withNewTab(
+    TEST_COOP_COEP,
+    async function (fileBrowser) {
+      is(
+        fileBrowser.remoteType,
+        E10SUtils.FILE_REMOTE_TYPE,
+        "Check that tab now has file remote type."
+      );
+      await SpecialPowers.spawn(fileBrowser, [], () => {
+        ok(
+          !content.crossOriginIsolated,
+          "Tab content is not cross-origin isolated"
+        );
+      });
+    }
+  );
 });
