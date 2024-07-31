0950280c54781de46ca0e6921528cc677bf9987f
Alex Franchuk <afranchuk@mozilla.com>
2024-07-31 14:17:19 +0000

    Bug 506009 - Crash reporter client: support profiles on different volumes than the home directory r=gsvelto a=RyanVM
    Rather than renaming files (which must be on the same
    volume/filesystem), we copy and delete them. This is less efficient,
    however the files in question are fairly small, so there's probably no
    need to rename _when we can_ as an optimization.
    
    Differential Revision: https://phabricator.services.mozilla.com/D217727

--- toolkit/crashreporter/client/app/src/config.rs
+++ toolkit/crashreporter/client/app/src/config.rs
@@ -237,12 +237,22 @@ impl Config {
 
         let move_file = |from: &Path| -> anyhow::Result<PathBuf> {
             let to = pending_crashes_dir.join(from.file_name().unwrap());
-            std::fs::rename(from, &to).with_context(|| {
-                self.build_string("crashreporter-error-moving-path")
-                    .arg("from", from.display().to_string())
-                    .arg("to", to.display().to_string())
-                    .get()
-            })?;
+            // Try to rename, but copy and remove if it fails. `rename` won't work across
+            // mount points. (bug 506009)
+            if let Err(e) = std::fs::rename(from, &to) {
+                log::warn!("failed to move {} to {}: {e}", from.display(), to.display());
+                log::info!("trying to copy and remove instead");
+
+                std::fs::copy(from, &to).with_context(|| {
+                    self.build_string("crashreporter-error-moving-path")
+                        .arg("from", from.display().to_string())
+                        .arg("to", to.display().to_string())
+                        .get()
+                })?;
+                if let Err(e) = std::fs::remove_file(from) {
+                    log::warn!("failed to remove {}: {e}", from.display());
+                }
+            }
             Ok(to)
         };
 
