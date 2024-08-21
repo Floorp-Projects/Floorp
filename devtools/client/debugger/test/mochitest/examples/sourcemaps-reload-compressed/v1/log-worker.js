3d89cab17db12fb05348857eb87f8fa7e18f9f92
Butkovits Atila <abutkovits@mozilla.com>
2024-08-21 03:57:07 +0300

    Backed out 2 changesets (bug 1908095) for causing failures at browser_dbg-features-breakpoints.js. a=backout
    Backed out changeset 21bf8880f772 (bug 1908095)
    Backed out changeset 85060db8ce99 (bug 1908095)

--- devtools/client/debugger/test/mochitest/examples/sourcemaps-reload-compressed/v1/log-worker.js
+++
@@ -1,3 +0,0 @@
-self.onmessage = function onmessage() {
-  console.log("hi");
-};
--- devtools/client/debugger/test/mochitest/examples/sourcemaps-reload-compressed/v1/react-component-module.js
+++
@@ -1,2 +0,0 @@
-// Use a code pattern identified by the parser worker to classify this module as a React Component
-var isReactComponent = true;
