afd0a2c3faceffc445ac2b92bed4eb8b8d03c23f
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2024-08-28 19:30:05 +0900

    [FB] CSK | 検索できるように

--- apps/main/about/preferences/hashchange.ts
+++ apps/main/about/preferences/hashchange.ts
@@ -2,9 +2,18 @@ import { createSignal } from "solid-js";
 
 export const [hash, setHash] = createSignal("");
 
-const onHashChange = (ev: Event | null) => setHash(window.location.hash);
+const onHashChange = (ev: Event | null) => {
+  setHash(window.location.hash);
+  console.log("ChangeHash");
+  if(window.location.hash === "#csk") {
+    document.getElementById("cskCategory").hidden = false;
+  } else {
+    document.getElementById("cskCategory").hidden = true;
+  }
+};
 
 export function initHashChange() {
   window.addEventListener("hashchange", onHashChange);
+  window.addEventListener("load", onHashChange);
   onHashChange(null);
 }
