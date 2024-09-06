c68f8919d39f9ce18d1749d0d44323b20d897603
David Parks <davidp99@gmail.com>
2024-09-06 14:44:22 +0000

    Bug 1912340: Run drag-drop test with dom.events.dataTransfer.protected.enabled true and false  a=diannaS
    This patch is separated from the previous to ease reading -- the code changes in
    this patch are minor so the file split is mostly transparent. We also run the
    test for dom.events.dataTransfer.protected.enabled = true. The test is split
    into two to avoid timeouts.
    
    Original Revision: https://phabricator.services.mozilla.com/D219076
    
    Differential Revision: https://phabricator.services.mozilla.com/D221183

---
+++ dom/events/test/browser_dragdrop_unprotected.js
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Test that drag and drop events are sent at the right time.
+// Includes tests for dragging between domains, windows and iframes.
+
+"use strict";
+
+add_setup(async function () {
+  await SpecialPowers.pushPrefEnv({
+    set: [["dom.events.dataTransfer.protected.enabled", false]],
+  });
+  registerCleanupFunction(async function () {
+    SpecialPowers.popPrefEnv();
+  });
+
+  await setup();
+});
+
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/dom/events/test/browser_dragdrop_impl.js",
+  this
+);
