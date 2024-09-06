09e9a2761bf99860cd2cff2239931431688e533e
Greg Stoll <gstoll@mozilla.com>
2024-09-06 14:44:08 +0000

    Bug 1907303 - make Content Analysis check native clipboard format as well  a=diannaS
    - Check for native Windows clipboard format (kNativeHTMLMime) as well as plain text and HTML
    - Make CheckClipboardContentAnalysisAsText() check for nsISupportsCString as well as nsISupportsString, as the kNativeHTMLMime format uses nsISupportsCString.
    - Update test to correctly check pasting when copying from Firefox - if allowPaste is false, the paste should not happen. This test was wrong before this change and would have shown this problem if it had been correct.
    
    Original Revision: https://phabricator.services.mozilla.com/D216358
    
    Differential Revision: https://phabricator.services.mozilla.com/D221149

--- toolkit/components/contentanalysis/tests/browser/browser_clipboard_content_analysis.js
+++ toolkit/components/contentanalysis/tests/browser/browser_clipboard_content_analysis.js
@@ -127,21 +127,29 @@ async function testClipboardWithContentAnalysis(allowPaste) {
 
   await sendKey("v");
   await pastePromise;
-  // 2 calls because there are two formats on the clipboard
-  is(mockCA.calls.length, 2, "Correct number of calls to Content Analysis");
+  // 3 calls because there are three formats on the clipboard
+  is(mockCA.calls.length, 3, "Correct number of calls to Content Analysis");
   assertContentAnalysisRequest(
     mockCA.calls[0],
     htmlPrefix + "t <b>Bold</b>" + htmlPostfix
   );
   assertContentAnalysisRequest(mockCA.calls[1], "t Bold");
+  assertContentAnalysisRequest(mockCA.calls[2], null);
+  // This is a complicated format, just make sure it has the text we expect
+  ok(
+    mockCA.calls[2].textContent.includes("t <b>Bold</b>"),
+    "request textContent should contain HTML"
+  );
   mockCA.clearCalls();
 
-  let copyPromise = SpecialPowers.spawn(browser, [], () => {
+  let copyPromise = SpecialPowers.spawn(browser, [allowPaste], allowPaste => {
     var main = content.document.getElementById("main");
 
     Assert.equal(
       main.innerHTML,
-      "Test <b>Bold</b> After Textt <b>Bold</b>",
+      allowPaste
+        ? "Test <b>Bold</b> After Textt <b>Bold</b>"
+        : "Test <b>Bold</b> After Text",
       "Copy and paste html"
     );
 
@@ -226,20 +234,28 @@ async function testClipboardWithContentAnalysis(allowPaste) {
 
   await sendKey("v");
   await pastePromise;
-  // 2 calls because there are two formats on the clipboard
-  is(mockCA.calls.length, 2, "Correct number of calls to Content Analysis");
+  // 3 calls because there are three formats on the clipboard
+  is(mockCA.calls.length, 3, "Correct number of calls to Content Analysis");
   assertContentAnalysisRequest(
     mockCA.calls[0],
     htmlPrefix + "<i>Italic</i> " + htmlPostfix
   );
   assertContentAnalysisRequest(mockCA.calls[1], "Some text");
+  assertContentAnalysisRequest(mockCA.calls[2], null);
+  // This is a complicated format, just make sure it has the text we expect
+  ok(
+    mockCA.calls[2].textContent.includes("<i>Italic</i>"),
+    "request textContent should contain HTML"
+  );
   mockCA.clearCalls();
 
-  await SpecialPowers.spawn(browser, [], () => {
+  await SpecialPowers.spawn(browser, [allowPaste], allowPaste => {
     var main = content.document.getElementById("main");
     Assert.equal(
       main.innerHTML,
-      "<i>Italic</i> Test <b>Bold</b> After<b></b>",
+      allowPaste
+        ? "<i>Italic</i> Test <b>Bold</b> After<b></b>"
+        : "Test <b>Bold</b>",
       "Copy and paste html 2"
     );
   });
@@ -308,23 +324,34 @@ async function testClipboardWithContentAnalysis(allowPaste) {
   await SpecialPowers.spawn(browser, [], () => {});
   await sendKey("v");
   await pastePromise;
-  is(mockCA.calls.length, 1, "Correct number of calls to Content Analysis");
+  is(mockCA.calls.length, 2, "Correct number of calls to Content Analysis");
   assertContentAnalysisRequest(
     mockCA.calls[0],
     htmlPrefix +
       '<img id="img" tabindex="1" src="http://example.org/browser/browser/base/content/test/general/moz.png">' +
       htmlPostfix
   );
+  // This is the CF_HTML format
+  assertContentAnalysisRequest(mockCA.calls[1], null);
+  // This is a complicated format, just make sure it has the text we expect
+  ok(
+    mockCA.calls[1].textContent.includes(
+      '<img id="img" tabindex="1" src="http://example.org/browser/browser/base/content/test/general/moz.png">'
+    ),
+    "request textContent should contain HTML"
+  );
   mockCA.clearCalls();
 
   // The new content should now include an image.
-  await SpecialPowers.spawn(browser, [], () => {
+  await SpecialPowers.spawn(browser, [allowPaste], allowPaste => {
     var main = content.document.getElementById("main");
     Assert.equal(
       main.innerHTML,
-      '<i>Italic</i> <img id="img" tabindex="1" ' +
-        'src="http://example.org/browser/browser/base/content/test/general/moz.png">' +
-        "Test <b>Bold</b> After<b></b>",
+      allowPaste
+        ? '<i>Italic</i> <img id="img" tabindex="1" ' +
+            'src="http://example.org/browser/browser/base/content/test/general/moz.png">' +
+            "Test <b>Bold</b> After<b></b>"
+        : "Test <b>Bold</b>",
       "Paste after copy image"
     );
   });
@@ -349,7 +376,9 @@ function assertContentAnalysisRequest(request, expectedText) {
     "request has correct operationTypeForDisplay"
   );
   is(request.filePath, "", "request filePath should be empty");
-  is(request.textContent, expectedText, "request textContent should match");
+  if (expectedText !== null) {
+    is(request.textContent, expectedText, "request textContent should match");
+  }
   is(request.printDataHandle, 0, "request printDataHandle should not be 0");
   is(request.printDataSize, 0, "request printDataSize should not be 0");
   ok(!!request.requestToken.length, "request requestToken should not be empty");
