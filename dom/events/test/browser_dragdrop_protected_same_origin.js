70fbae66e7a3794a020a91390c8aa2dc830949c4
David Parks <davidp99@gmail.com>
2024-09-06 14:44:22 +0000

    Bug 1912340: Further split up browser_dragdrop_[un]protected tests  a=diannaS
    No substantive changes.  This avoids timeouts in automation.
    
    Original Revision: https://phabricator.services.mozilla.com/D219548
    
    Differential Revision: https://phabricator.services.mozilla.com/D221184

--- dom/events/test/browser_dragdrop_protected_same_origin.js
+++ dom/events/test/browser_dragdrop_protected_same_origin.js
@@ -2,10 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Test that drag and drop events are sent at the right time.
-// Includes tests for dragging between domains, windows and iframes.
+// Test that drag and drop events are sent at the right time. Includes tests for
+// dragging between windows and iframes, all of the same origin.
+// dom.events.dataTransfer.protected.enabled = true
 
 "use strict";
+
+const OUTER_BASE_1 = "https://example.org/browser/dom/events/test/";
+const OUTER_BASE_2 = "https://example.org/browser/dom/events/test/";
+
+// iframe domains
+const INNER_BASE_1 = OUTER_BASE_1;
+const INNER_BASE_2 = OUTER_BASE_2;
 
 add_setup(async function () {
   await SpecialPowers.pushPrefEnv({
