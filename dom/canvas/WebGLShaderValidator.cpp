71e8c006223e887fb01d67f9e8930268eb2c6459
Kelsey Gilbert <kelsey.gilbert@mozilla.com>
2024-09-17 16:56:43 +0000

    Bug 1912404 - Remove unneeded 16kiB var limit for Mesa. r=gfx-reviewers,lsalzman a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D222010

--- dom/canvas/WebGLShaderValidator.cpp
+++ dom/canvas/WebGLShaderValidator.cpp
@@ -208,10 +208,6 @@ std::unique_ptr<webgl::ShaderValidator> WebGLContext::CreateShaderValidator(
       return static_cast<size_t>(bytes);
     }
 
-    if (gl->IsMesa()) {
-      return 4 * 4 * 1024;  // 4K words
-    }
-
     if (kIsMacOS) {
       return 128 * 1024;  // 8k vec4s
     }
