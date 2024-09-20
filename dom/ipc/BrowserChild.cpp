62c1e89c70e1ad545f9cff9a8e3d7f95c42defed
David Parks <davidp99@gmail.com>
2024-09-20 23:34:59 +0000

    Bug 1918907: Don't clear drag session target BrowserChild on eDragLeave/eDragExit  a=diannaS
    The BrowserChild should only be cleared on EndDragSession, which is sent when
    the user finishes or cancels dragging. This is reciprocal to it being set in
    StartDragSession.
    
    The change to nsDragSessionProxy::EndDragSessionImpl is non-functional -- it
    highlights the BrowserChild symmetry above.
    
    Original Revision: https://phabricator.services.mozilla.com/D222389
    
    Differential Revision: https://phabricator.services.mozilla.com/D222947

--- dom/ipc/BrowserChild.cpp
+++ dom/ipc/BrowserChild.cpp
@@ -1901,15 +1901,6 @@ mozilla::ipc::IPCResult BrowserChild::RecvRealDragEvent(
   }
 
   DispatchWidgetEventViaAPZ(localEvent);
-
-  if (aEvent.mMessage == eDragLeave || aEvent.mMessage == eDragExit) {
-    // If session is still active, remove its target.
-    dragSession = GetDragSession();
-    if (dragSession) {
-      static_cast<nsDragSessionProxy*>(dragSession.get())
-          ->SetDragTarget(nullptr);
-    }
-  }
   return IPC_OK();
 }
 
