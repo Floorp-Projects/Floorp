5759400a051dce0bc90737319555ef50cd5a6bbb
Andrew Osmond <aosmond@mozilla.com>
2024-09-26 21:01:38 +0000

    Bug 1898773 - Ensure we also check for ffmpeg + OpenH264 when determining codec support. r=media-playback-reviewers,padenot a=RyanVM
    When we create a video decoder with ffmpeg, we check if the underlying
    decoder implementation is supplied by OpenH264. If so, and the
    media.ffmpeg.allow-openh264 is set to false (the default), we refuse to
    create the video decoder. This is a problem because the decoder module
    for ffmpeg declared support for H264, so we did not try falling back to
    our own OpenH264 plugin exposed via GMP.
    
    This patch duplicates the logic check for ffmpeg + OpenH264 in
    FFmpegDecoderModule::Supports to ensure we fallback properly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D223843

--- dom/media/platforms/ffmpeg/FFmpegDataDecoder.cpp
+++ dom/media/platforms/ffmpeg/FFmpegDataDecoder.cpp
@@ -87,8 +87,10 @@ MediaResult FFmpegDataDecoder<LIBAV_VER>::InitDecoder(AVDictionary** aOptions) {
     return MediaResult(NS_ERROR_DOM_MEDIA_FATAL_ERR,
                        RESULT_DETAIL("unable to find codec"));
   }
-  // openh264 has broken decoding of some h264 videos so
-  // don't use it unless explicitly allowed for now.
+  // This logic is mirrored in FFmpegDecoderModule::Supports. We prefer to use
+  // our own OpenH264 decoder through the plugin over ffmpeg by default due to
+  // broken decoding with some versions. openh264 has broken decoding of some
+  // h264 videos so don't use it unless explicitly allowed for now.
   if (!strcmp(codec->name, "libopenh264") &&
       !StaticPrefs::media_ffmpeg_allow_openh264()) {
     FFMPEG_LOG("  unable to find codec (openh264 disabled by pref)");
