6a9fbbb2dcb4dcc5a7a354bd9b74de5ced381b90
Mike Hommey <mh+mozilla@glandium.org>
2024-10-15 13:32:39 +0000

    Bug 1924031 - Replace use of pipes.quote with shlex.quote.  a=RyanVM
    It was literally a reexport before the pipes module was removed in
    python 3.13.
    https://github.com/python/cpython/blob/3.12/Lib/pipes.py#L66
    
    Original Revision: https://phabricator.services.mozilla.com/D225319
    
    Differential Revision: https://phabricator.services.mozilla.com/D225607

--- taskcluster/android_taskgraph/job.py
+++ taskcluster/android_taskgraph/job.py
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-from pipes import quote as shell_quote
+from shlex import quote as shell_quote
 
 from gecko_taskgraph.transforms.job import configure_taskdesc_for_run, run_job_using
 from taskgraph.util import path
