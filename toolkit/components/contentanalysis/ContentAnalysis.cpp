bb62f7dbf0ef5e6dd22486cfd61044433bd7df50
David Parks <davidp99@gmail.com>
2024-11-05 15:35:37 +0000

    Bug 1910452: Part 5 - Release content analysis cache request on main thread  a=dmeehan
    Without this, we assert when releasing the request on the background thread when
    we hit an error, such as the DLP agent not running.
    
    Original Revision: https://phabricator.services.mozilla.com/D220617
    
    Differential Revision: https://phabricator.services.mozilla.com/D227591

--- toolkit/components/contentanalysis/ContentAnalysis.cpp
+++ toolkit/components/contentanalysis/ContentAnalysis.cpp
@@ -1321,6 +1321,11 @@ void ContentAnalysis::DoAnalyzeRequest(
     nsCOMPtr<nsIContentAnalysisRequest> aRequestToCache,
     const std::shared_ptr<content_analysis::sdk::Client>& aClient) {
   MOZ_ASSERT(!NS_IsMainThread());
+  auto threadsafeErrorHandler = MakeScopeExit([&]() {
+    // Make sure the cache request is destroyed on the main thread.
+    NS_DispatchToMainThread(NS_NewCancelableRunnableFunction(
+        "CARequestErrorCleanup", [aRTC = std::move(aRequestToCache)]() {}));
+  });
   RefPtr<ContentAnalysis> owner =
       ContentAnalysis::GetContentAnalysisFromService();
   if (!owner) {
@@ -1420,6 +1425,7 @@ void ContentAnalysis::DoAnalyzeRequest(
         }
         owner->IssueResponse(response);
       }));
+  threadsafeErrorHandler.release();
 }
 
 void ContentAnalysis::SendWarnResponse(
