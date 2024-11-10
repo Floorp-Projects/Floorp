cdda904fdecc56341f4cb63f61e52927c158c7f7
Sean <sekim@mozilla.com>
2024-11-10 15:15:22 +0000

    Bug 1929622 - Improve error handling in SSO Cookie Parsing  a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D228389

--- netwerk/protocol/http/MicrosoftEntraSSOUtils.mm
+++ netwerk/protocol/http/MicrosoftEntraSSOUtils.mm
@@ -102,23 +102,53 @@ class API_AVAILABLE(macos(13.3)) MicrosoftEntraSSOUtils final {
                          error:&err];
 
         if (!err) {
-          NSArray* allHeaders = @[
-            ssoCookiesDict[@"device_headers"], ssoCookiesDict[@"prt_headers"]
-          ];
-          // Append cookie headers retrieved from MS Broker
-          for (NSArray* headerArray in allHeaders) {
-            for (NSDictionary* headerDict in headerArray) {
-              NSDictionary* headers = headerDict[@"header"];
-              for (NSString* key in headers) {
-                NSString* value = headers[key];
-                nsAutoString nsKey;
-                nsAutoString nsValue;
-                mozilla::CopyNSStringToXPCOMString(key, nsKey);
-                mozilla::CopyNSStringToXPCOMString(value, nsValue);
-                mCallback->AddRequestHeader(NS_ConvertUTF16toUTF8(nsKey),
-                                            NS_ConvertUTF16toUTF8(nsValue));
+          NSMutableArray* allHeaders = [NSMutableArray array];
+
+          if (ssoCookiesDict[@"device_headers"]) {
+            [allHeaders addObject:ssoCookiesDict[@"device_headers"]];
+          } else {
+            MOZ_LOG(gMacOSWebAuthnServiceLog, mozilla::LogLevel::Debug,
+                    ("SSORequestDelegate::didCompleteWithAuthorization: "
+                     "Missing device_headers"));
+          }
+
+          if (ssoCookiesDict[@"prt_headers"]) {
+            [allHeaders addObject:ssoCookiesDict[@"prt_headers"]];
+          } else {
+            MOZ_LOG(gMacOSWebAuthnServiceLog, mozilla::LogLevel::Debug,
+                    ("SSORequestDelegate::didCompleteWithAuthorization: "
+                     "Missing prt_headers"));
+          }
+
+          // We would like to have both device_headers and prt_headers before
+          // attaching the headers
+          if (allHeaders.count == 2) {
+            // Append cookie headers retrieved from MS Broker
+            for (NSArray* headerArray in allHeaders) {
+              if (headerArray) {
+                for (NSDictionary* headerDict in headerArray) {
+                  NSDictionary* headers = headerDict[@"header"];
+                  if (headers) {
+                    for (NSString* key in headers) {
+                      NSString* value = headers[key];
+                      if (value) {
+                        nsAutoString nsKey;
+                        nsAutoString nsValue;
+                        mozilla::CopyNSStringToXPCOMString(key, nsKey);
+                        mozilla::CopyNSStringToXPCOMString(value, nsValue);
+                        mCallback->AddRequestHeader(
+                            NS_ConvertUTF16toUTF8(nsKey),
+                            NS_ConvertUTF16toUTF8(nsValue));
+                      }
+                    }
+                  }
+                }
               }
             }
+          } else {
+            MOZ_LOG(gMacOSWebAuthnServiceLog, mozilla::LogLevel::Debug,
+                    ("SSORequestDelegate::didCompleteWithAuthorization: "
+                     "sso_cookies has missing headers"));
           }
         } else {
           MOZ_LOG(gMacOSWebAuthnServiceLog, mozilla::LogLevel::Debug,
