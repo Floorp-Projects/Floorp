f8bf9c12ff673477609fa050a48401df52232d01
David P <daparks@mozilla.com>
2024-11-26 09:12:06 -0500

    Bug 1921759: Log the process that holds the clipboard mutex on Windows a=dmeehan When Windows keeps us from getting access to the clipboard because another application is using it, this will report the exe that currently holds the mutex.
    Differential Revision: https://phabricator.services.mozilla.com/D227742

--- widget/windows/WinUtils.h
+++ widget/windows/WinUtils.h
@@ -566,6 +566,8 @@ class WinUtils {
 
   static void GetClipboardFormatAsString(UINT aFormat, nsAString& aOutput);
 
+  static nsresult GetProcessImageName(DWORD aProcessId, nsAString& aName);
+
  private:
   static WhitelistVec BuildWhitelist();
 
