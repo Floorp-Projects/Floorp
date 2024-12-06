484e2850d2d626673f5b740273c88a6f6e4beb6a
Mike Hommey <mh+mozilla@glandium.org>
2024-12-06 09:35:39 +0000

    Bug 1906930 - Fix SrcRepository.get_files when the SrcRepository is given a path other than the current directory.  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D216502
    
    Differential Revision: https://phabricator.services.mozilla.com/D231335

--- python/mozversioncontrol/mozversioncontrol/__init__.py
+++ python/mozversioncontrol/mozversioncontrol/__init__.py
@@ -1019,9 +1019,10 @@ class SrcRepository(Repository):
         """
         res = []
         # move away the .git or .hg folder from path to more easily test in a hg/git repo
-        for root, dirs, files in os.walk("."):
+        for root, dirs, files in os.walk(self.path):
+            base = os.path.relpath(root, self.path)
             for name in files:
-                res.append(os.path.join(root, name))
+                res.append(os.path.join(base, name))
         return res
 
     def get_tracked_files_finder(self, path):
