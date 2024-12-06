27178d3773dc137831a06143b7814378257dae8f
NyanRus <68762426+nyanrus@users.noreply.github.com>
2024-12-07 00:37:58 +0900

    fix(cache): disable refreshing cache

--- src/apps/main/core/common/update-refresh-cache/index.ts
+++ src/apps/main/core/common/update-refresh-cache/index.ts
@@ -2,30 +2,30 @@ export function init() {
   const { NoranekoConstants } = ChromeUtils.importESModule(
     "resource://noraneko/modules/NoranekoConstants.sys.mjs",
   );
-  if (
-    Services.prefs.getStringPref("noraneko.version2", "") !==
-    NoranekoConstants.version2
-  ) {
-    console.debug(
-      "[noraneko updater] pref 'noraneko.version2' !== NoranekoConstants.version2",
-    );
+  // if (
+  //   Services.prefs.getStringPref("noraneko.version2", "") !==
+  //   NoranekoConstants.version2
+  // ) {
+  //   console.debug(
+  //     "[noraneko updater] pref 'noraneko.version2' !== NoranekoConstants.version2",
+  //   );
 
-    //set version2
-    if (Services.prefs.prefHasDefaultValue("noraneko.version2")) {
-      Services.prefs.unlockPref("noraneko.version2");
-    }
+  //   //set version2
+  //   if (Services.prefs.prefHasDefaultValue("noraneko.version2")) {
+  //     Services.prefs.unlockPref("noraneko.version2");
+  //   }
 
-    Services.prefs
-      .getDefaultBranch(null as unknown as string)
-      .setStringPref("noraneko.version2", NoranekoConstants.version2);
-    Services.prefs.lockPref("noraneko.version2");
+  //   Services.prefs
+  //     .getDefaultBranch(null as unknown as string)
+  //     .setStringPref("noraneko.version2", NoranekoConstants.version2);
+  //   Services.prefs.lockPref("noraneko.version2");
 
-    Services.prefs.savePrefFile(null);
+  //   Services.prefs.savePrefFile(null);
 
-    //invalidate cache and restart
-    Services.appinfo.invalidateCachesOnRestart();
-    // Services.startup.quit(
-    //   Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit,
-    // );
-  }
+  //   //invalidate cache and restart
+  //   Services.appinfo.invalidateCachesOnRestart();
+  //   // Services.startup.quit(
+  //   //   Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit,
+  //   // );
+  // }
 }
