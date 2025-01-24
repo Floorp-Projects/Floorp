33135f7e3f1b9284c62b578884525bed03d05d29
Narcis Beleuzu <nbeleuzu@mozilla.com>
2025-01-24 05:18:58 +0200

    Backed out changeset 0c6183497ae4 (bug 1943011) for xpcshell failure on test_trr_https_fallback.js . a=backout

--- netwerk/test/unit/test_trr_https_fallback.js
+++ netwerk/test/unit/test_trr_https_fallback.js
@@ -799,7 +799,6 @@ add_task(async function testHttp3ExcludedList() {
           name: "www.h3_fail.org",
           values: [
             { key: "alpn", value: "h3-29" },
-            { key: "no-default-alpn" },
             { key: "port", value: h3Port },
           ],
         },
@@ -902,7 +901,6 @@ add_task(async function testAllRecordsInHttp3ExcludedList() {
           name: "www.h3_fail1.org",
           values: [
             { key: "alpn", value: "h3-29" },
-            { key: "no-default-alpn" },
             { key: "port", value: h3Port },
             { key: "echconfig", value: "456..." },
           ],
