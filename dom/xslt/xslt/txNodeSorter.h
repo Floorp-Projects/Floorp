7eae5e06a94a3f7c0cd94c581e13d0a23d9e3a26
Peter Van der Beken <peterv@propagandism.org>
2025-02-04 17:28:39 +0000

    Bug 1944313 - Propagate XSLT expression failure correctly. r=farre, a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D235895

--- dom/xslt/xslt/txNodeSorter.h
+++ dom/xslt/xslt/txNodeSorter.h
@@ -36,7 +36,7 @@ class txNodeSorter {
   struct SortData {
     txNodeSorter* mNodeSorter;
     txNodeSetContext* mContext;
-    txObject** mSortValues;
+    mozilla::UniquePtr<txObject>* mSortValues;
     nsresult mRv;
   };
   struct SortKey {
@@ -46,8 +46,9 @@ class txNodeSorter {
 
   static int compareNodes(uint32_t aIndexA, uint32_t aIndexB,
                           SortData& aSortData);
-  static bool calcSortValue(txObject*& aSortValue, SortKey* aKey,
-                            SortData* aSortData, uint32_t aNodeIndex);
+  static mozilla::UniquePtr<txObject> calcSortValue(SortKey* aKey,
+                                                    SortData* aSortData,
+                                                    uint32_t aNodeIndex);
   txList mSortKeys;
   unsigned int mNKeys;
 };
