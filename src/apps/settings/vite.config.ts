710e2902982cfc3cef25b65fedba17300e0637c2
NyanRus <68762426+nyanrus@users.noreply.github.com>
2025-02-18 00:30:26 +0900

    fix(build): jar.mn for settings

--- src/apps/settings/vite.config.ts
+++ src/apps/settings/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import path from "node:path"
-//import { generateJarManifest } from "../common/scripts/gen_jarmanifest.ts";
+import path from "node:path";
+import { generateJarManifest } from "../common/scripts/gen_jarmanifest.ts";
 
 export default defineConfig({
   server: {
@@ -25,27 +25,27 @@ export default defineConfig({
     tsconfigPaths(),
     react(),
     tailwindcss(),
-    // {
-    //   name: "gen_jarmn",
-    //   enforce: "post",
-    //   async generateBundle(options, bundle, isWrite) {
-    //     this.emitFile({
-    //       type: "asset",
-    //       fileName: "jar.mn",
-    //       needsCodeReference: false,
-    //       source: await generateJarManifest(bundle, {
-    //         prefix: "content",
-    //         namespace: "noraneko-settings",
-    //         register_type: "content",
-    //       }),
-    //     });
-    //     this.emitFile({
-    //       type: "asset",
-    //       fileName: "moz.build",
-    //       needsCodeReference: false,
-    //       source: `JAR_MANIFESTS += ["jar.mn"]`,
-    //     });
-    //   },
-    // },
+    {
+      name: "gen_jarmn",
+      enforce: "post",
+      async generateBundle(options, bundle, isWrite) {
+        this.emitFile({
+          type: "asset",
+          fileName: "jar.mn",
+          needsCodeReference: false,
+          source: await generateJarManifest(bundle, {
+            prefix: "content",
+            namespace: "noraneko-settings",
+            register_type: "content",
+          }),
+        });
+        this.emitFile({
+          type: "asset",
+          fileName: "moz.build",
+          needsCodeReference: false,
+          source: `JAR_MANIFESTS += ["jar.mn"]`,
+        });
+      },
+    },
   ],
 });
