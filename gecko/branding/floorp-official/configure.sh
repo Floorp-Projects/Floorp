c2b485eeb2e3d56637343ace6eca98ebac5c1c26
Ryosuke Asano <s1F102400011@iniad.org>
2025-04-02 11:22:19 +0900

    feat(branding): update configure.sh to use quotes for branding variables

--- gecko/branding/floorp-official/configure.sh
+++ gecko/branding/floorp-official/configure.sh
@@ -2,13 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=Floorp
-MOZ_APP_NAME=Floorp
-MOZ_APP_PROFILE=Floorp
+MOZ_APP_DISPLAYNAME="Floorp"
+MOZ_APP_NAME="Floorp"
+MOZ_APP_PROFILE="Floorp"
 MOZ_APP_UA_NAME="Firefox"
 
 MOZ_APP_VENDOR="Ablaze"
-MOZ_APP_REMOTINGNAME=floorp-default
+MOZ_APP_REMOTINGNAME="floorp-default"
 
 MOZ_REQUIRE_SIGNING=
 MOZ_TELEMETRY_REPORTING=
