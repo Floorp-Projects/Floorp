0cc952d11222a9829a26dba29a3e6b110f7f5216
Ryosuke Asano <s1F102400011@iniad.org>
2025-04-02 11:20:37 +0900

    feat(branding): update configuration for Floorp Daylight with additional vendor and remoting name

--- gecko/branding/floorp-daylight/configure.sh
+++ gecko/branding/floorp-daylight/configure.sh
@@ -3,5 +3,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 MOZ_APP_DISPLAYNAME="Floorp Daylight"
-MOZ_APP_PROFILE=Floorp
-MOZ_APP_UA_NAME=Firefox
+MOZ_APP_NAME="Floorp"
+MOZ_APP_PROFILE="Floorp"
+MOZ_APP_UA_NAME="Firefox"
+
+MOZ_APP_VENDOR="Ablaze"
+MOZ_APP_REMOTINGNAME="floorp-daylight"
+
+MOZ_REQUIRE_SIGNING=
+MOZ_TELEMETRY_REPORTING=
