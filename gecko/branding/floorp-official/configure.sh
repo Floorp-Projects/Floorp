b368e1b8b8ddeb04fb46fd8266db5dc2bea29cb2
Ryosuke Asano <s1F102400011@iniad.org>
2025-04-02 11:16:59 +0900

    feat(branding): update application configuration for Floorp branding

--- gecko/branding/floorp-official/configure.sh
+++ gecko/branding/floorp-official/configure.sh
@@ -3,5 +3,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 MOZ_APP_DISPLAYNAME=Floorp
+MOZ_APP_NAME=Floorp
 MOZ_APP_PROFILE=Floorp
-MOZ_APP_UA_NAME=Firefox
+MOZ_APP_UA_NAME="Firefox"
+
+MOZ_APP_VENDOR="Ablaze"
+MOZ_APP_REMOTINGNAME=floorp-default
+
+MOZ_REQUIRE_SIGNING=
+MOZ_TELEMETRY_REPORTING=
