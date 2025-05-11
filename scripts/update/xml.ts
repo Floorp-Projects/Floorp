4b0aefe6b11933128f67f80e14d09cb96052255b
NyanRus <68762426+nyanrus@users.noreply.github.com>
2025-05-11 22:39:51 +0900

    rftr: xml.ts

--- scripts/update/xml.ts
+++ scripts/update/xml.ts
@@ -1,52 +1,28 @@
-import { json2xml } from "xml-js";
+import { z } from "zod";
 
-const meta = JSON.parse(
-  await fs.readFile(Deno.args[0], { encoding: "utf-8" }),
-) as {
-  version_display: string;
-  version: string;
-  noraneko_version: string;
-  mar_size: string;
-  mar_shasum: string;
-  buildid: string;
-  noraneko_buildid: string;
-};
+const zMeta = z.object({
+  version: z.string(),
+  version_display: z.string(),
+  noraneko_version: z.string(),
+  mar_size: z.string(),
+  mar_shasum: z.string(),
+  buildid: z.string(),
+  noraneko_buildid: z.string(),
+});
 
-const update = {
-  _declaration: {
-    _attributes: {
-      version: "1.0",
-      encoding: "utf-8",
-    },
-  },
-  updates: {
-    update: {
-      _attributes: {
-        type: "minor",
-        displayVersion: meta.version_display,
-        appVersion: meta.version,
-        platformVersion: meta.version,
-        buildID: meta.buildid,
-        appVersion2: meta.noraneko_version,
-        buildID2: meta.noraneko_buildid,
-      },
-      patch: [
-        {
-          _attributes: {
-            type: "complete",
-            URL:
-              "http://github.com/nyanrus/noraneko/releases/download/alpha/noraneko-win-amd64-full.mar",
-            hashFunctions: "sha512",
-            hashValue: meta.mar_shasum,
-            size: meta.mar_size,
-          },
-        },
-      ],
-    },
-  },
-};
-import fs from "node:fs/promises";
-await fs.writeFile(
+const meta = zMeta.parse(JSON.parse(
+  await Deno.readTextFile(Deno.args[0]),
+));
+
+const patchUrl =
+  "http://github.com/nyanrus/noraneko/releases/download/alpha/noraneko-win-amd64-full.mar";
+
+await Deno.writeTextFile(
   Deno.args[1],
-  json2xml(JSON.stringify(update), { "compact": true, spaces: 4 }),
+  `<?xml version="1.0" encoding="UTF-8"?>
+<updates>
+  <update type="minor" displayVersion="${meta.version_display}" appVersion="${meta.version}" platformVersion="${meta.version}" buildID="${meta.buildid}" appVersion2="${meta.noraneko_version}">
+    <patch type="complete" URL="${patchUrl}" size="${meta.mar_size} hashFunction="sha512" hashValue="${meta.mar_shasum}"/>
+  </update>
+</updates>`,
 );
