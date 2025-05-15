25e452aa570e62cc0e40993c735d0767048b81b9
Ben Visness <bvisness@mozilla.com>
2025-05-15 23:55:46 +0000

    Bug 1962634: Add missing types to uncheckedReadValType.  a=RyanVM
    This method is only called by the debugger, and was missing various
    reference types.
    
    Original Revision: https://phabricator.services.mozilla.com/D246752
    
    Differential Revision: https://phabricator.services.mozilla.com/D249599

---
+++ js/src/jit-test/tests/wasm/gc/bug-1962634.js
@@ -0,0 +1,42 @@
+// |jit-test| skip-if: !wasmDebuggingEnabled()
+
+const f = newGlobal({ newCompartment: true });
+f.g = this;
+f.eval(`
+  Debugger(g).onEnterFrame = function(frame) { frame.environment }
+`);
+
+const types = [
+  "anyref", "(ref any)",
+  "eqref", "(ref eq)",
+  "i31ref", "(ref i31)",
+  "structref", "(ref struct)",
+  "arrayref", "(ref array)",
+  "nullref", "(ref none)",
+
+  "funcref", "(ref func)",
+  "nullfuncref", "(ref nofunc)",
+
+  "externref", "(ref extern)",
+  "nullexternref", "(ref noextern)",
+
+  "(ref null $s)", "(ref $s)",
+];
+
+if (wasmExnRefEnabled()) {
+  types.push(
+    "exnref", "(ref exn)",
+    "nullexnref", "(ref noexn)",
+  )
+}
+
+for (const type of types) {
+  const m = new WebAssembly.Module(wasmTextToBinary(`(module
+    (type $s (struct))
+    (func (export "test")
+      (local ${type})
+    )
+  )`));
+  const { test } = new WebAssembly.Instance(m).exports;
+  test();
+}
