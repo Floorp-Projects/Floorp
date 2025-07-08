ca3d498d61ad4f7fa4f6784d224169c71b1a136c
Ryosuke Asano <s1F102400011@iniad.org>
2025-07-08 20:45:31 +0900

    feat(chrome-css): re-enable chromeCSS feature by fixing path excusing fixing

--- src/apps/main/core/common/chrome-css/index.ts
+++ src/apps/main/core/common/chrome-css/index.ts
@@ -14,7 +14,6 @@ export default class ChromeCSS extends NoraComponentBase {
   static ctx: ChromeCSSService | null = null;
 
   init(): void {
-    return;
     const cssService = ChromeCSSService.getInstance();
     cssService.init();
     ChromeCSS.ctx = cssService;
